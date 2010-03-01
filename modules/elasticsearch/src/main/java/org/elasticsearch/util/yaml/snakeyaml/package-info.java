7d3b642326aee421996ac65e07a6b5b5a1cf0cf8
kimchy <kimchy@gmail.com>
2010-03-01 15:53:15 +0200

    merge snakeyaml into source

--- modules/elasticsearch/src/main/java/org/elasticsearch/util/yaml/snakeyaml/package-info.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/util/yaml/snakeyaml/package-info.java
@@ -16,4 +16,5 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.elasticsearch.util.yaml.snakeyaml;
+
+ackage org.elasticsearch.util.yaml.snakeyaml;

0f29a4fc8ce934d3e453092edd0b7f31efac5f49
kimchy <kimchy@gmail.com>
2010-05-18 18:23:01 +0300

    add anotehr test

--- modules/elasticsearch/src/test/java/org/elasticsearch/index/cache/filter/FilterCacheTests.java
+++ modules/elasticsearch/src/test/java/org/elasticsearch/index/cache/filter/FilterCacheTests.java
@@ -23,7 +23,9 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.ConstantScoreQuery;
+import org.apache.lucene.search.FilteredQuery;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 import org.elasticsearch.index.Index;
@@ -73,12 +75,13 @@ public class FilterCacheTests {
         reader = refreshReader(reader);
         IndexSearcher searcher = new IndexSearcher(reader);
         assertThat(Lucene.count(searcher, new ConstantScoreQuery(filterCache.cache(new TermFilter(new Term("id", "1")))), -1), equalTo(1l));
+        assertThat(Lucene.count(searcher, new FilteredQuery(new MatchAllDocsQuery(), filterCache.cache(new TermFilter(new Term("id", "1")))), -1), equalTo(1l));
 
         indexWriter.deleteDocuments(new Term("id", "1"));
         reader = refreshReader(reader);
         searcher = new IndexSearcher(reader);
         assertThat(Lucene.count(searcher, new ConstantScoreQuery(filterCache.cache(new TermFilter(new Term("id", "1")))), -1), equalTo(0l));
-
+        assertThat(Lucene.count(searcher, new FilteredQuery(new MatchAllDocsQuery(), filterCache.cache(new TermFilter(new Term("id", "1")))), -1), equalTo(0l));
 
         indexWriter.close();
     }

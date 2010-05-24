2cdb7bc05a35ee4299e1d5b32d2e69c0f9811530
kimchy <kimchy@gmail.com>
2010-05-24 10:24:16 +0300

    change jclouds to use cached thread pool

--- plugins/cloud/src/main/java/org/elasticsearch/cloud/jclouds/JCloudsUtils.java
+++ plugins/cloud/src/main/java/org/elasticsearch/cloud/jclouds/JCloudsUtils.java
@@ -22,7 +22,11 @@ package org.elasticsearch.cloud.jclouds;
 import com.google.inject.Module;
 import org.elasticsearch.cloud.jclouds.logging.JCloudsLoggingModule;
 import org.elasticsearch.util.collect.ImmutableList;
+import org.elasticsearch.util.concurrent.DynamicExecutors;
 import org.elasticsearch.util.settings.Settings;
+import org.jclouds.concurrent.config.ExecutorServiceModule;
+
+import java.util.concurrent.Executors;
 
 /**
  * @author kimchy (shay.banon)
@@ -30,6 +34,9 @@ import org.elasticsearch.util.settings.Settings;
 public class JCloudsUtils {
 
     public static Iterable<? extends Module> buildModules(Settings settings) {
-        return ImmutableList.of(new JCloudsLoggingModule(settings));
+        return ImmutableList.of(new JCloudsLoggingModule(settings),
+                new ExecutorServiceModule(
+                        Executors.newCachedThreadPool(DynamicExecutors.daemonThreadFactory(settings, "jclouds-user")),
+                        Executors.newCachedThreadPool(DynamicExecutors.daemonThreadFactory(settings, "jclouds-io"))));
     }
 }

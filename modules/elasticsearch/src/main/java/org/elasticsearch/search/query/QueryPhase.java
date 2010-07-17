6716202aaf95a20f23aaa21c18f76380958d15d0
kimchy <kimchy@gmail.com>
2010-07-17 19:38:04 +0300

    fix optimization to not do sorting when searching with score "desc" sorting

--- modules/elasticsearch/src/main/java/org/elasticsearch/search/query/QueryPhase.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/search/query/QueryPhase.java
@@ -102,7 +102,7 @@ public class QueryPhase implements SearchPhase {
             boolean sort = false;
             // try and optimize for a case where the sorting is based on score, this is how we work by default!
             if (searchContext.sort() != null) {
-                if (searchContext.sort().getSort().length > 0) {
+                if (searchContext.sort().getSort().length > 1) {
                     sort = true;
                 } else {
                     SortField sortField = searchContext.sort().getSort()[0];

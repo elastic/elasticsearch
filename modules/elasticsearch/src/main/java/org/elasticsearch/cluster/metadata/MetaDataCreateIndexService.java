8b1e7c3811627bc8af86ae532d6ea54077b59565
kimchy <kimchy@gmail.com>
2010-07-29 19:22:21 +0300

    fix test for existance of directory based mappings when creating an index

--- modules/elasticsearch/src/main/java/org/elasticsearch/cluster/metadata/MetaDataCreateIndexService.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/cluster/metadata/MetaDataCreateIndexService.java
@@ -127,11 +127,11 @@ public class MetaDataCreateIndexService extends AbstractComponent {
                     File mappingsDir = new File(environment.configFile(), "mappings");
                     if (mappingsDir.exists() && mappingsDir.isDirectory()) {
                         File defaultMappingsDir = new File(mappingsDir, "_default");
-                        if (mappingsDir.exists() && mappingsDir.isDirectory()) {
+                        if (defaultMappingsDir.exists() && defaultMappingsDir.isDirectory()) {
                             addMappings(mappings, defaultMappingsDir);
                         }
                         File indexMappingsDir = new File(mappingsDir, request.index);
-                        if (mappingsDir.exists() && mappingsDir.isDirectory()) {
+                        if (indexMappingsDir.exists() && indexMappingsDir.isDirectory()) {
                             addMappings(mappings, indexMappingsDir);
                         }
                     }

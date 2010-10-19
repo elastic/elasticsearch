982fae88c4a7729b5358399157140dadbf2346f8
kimchy <kimchy@gmail.com>
2010-10-19 18:03:48 +0200

    don't use the index serivce to get the stored blobs in shared gateway, move it to upper node level

--- modules/elasticsearch/src/main/java/org/elasticsearch/index/gateway/blobstore/BlobStoreIndexGateway.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/index/gateway/blobstore/BlobStoreIndexGateway.java
@@ -20,12 +20,8 @@
 package org.elasticsearch.index.gateway.blobstore;
 
 import org.elasticsearch.ElasticSearchException;
-import org.elasticsearch.common.blobstore.BlobMetaData;
 import org.elasticsearch.common.blobstore.BlobPath;
 import org.elasticsearch.common.blobstore.BlobStore;
-import org.elasticsearch.common.blobstore.ImmutableBlobContainer;
-import org.elasticsearch.common.collect.ImmutableMap;
-import org.elasticsearch.common.collect.Lists;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.gateway.Gateway;
@@ -33,13 +29,8 @@ import org.elasticsearch.gateway.blobstore.BlobStoreGateway;
 import org.elasticsearch.gateway.none.NoneGateway;
 import org.elasticsearch.index.AbstractIndexComponent;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.index.gateway.CommitPoint;
-import org.elasticsearch.index.gateway.CommitPoints;
 import org.elasticsearch.index.gateway.IndexGateway;
 import org.elasticsearch.index.settings.IndexSettings;
-
-import java.io.IOException;
-import java.util.List;
 
 /**
  * @author kimchy (shay.banon)
@@ -69,26 +60,6 @@ public abstract class BlobStoreIndexGateway extends AbstractIndexComponent imple
         this.indexPath = this.gateway.basePath().add("indices").add(index.name());
     }
 
-    public CommitPoint findCommitPoint(int shardId) throws IOException {
-        ImmutableBlobContainer container = blobStore.immutableBlobContainer(shardPath(shardId));
-        ImmutableMap<String, BlobMetaData> blobs = container.listBlobs();
-        List<CommitPoint> commitPointsList = Lists.newArrayList();
-        for (String name : blobs.keySet()) {
-            if (name.startsWith("commit-")) {
-                try {
-                    commitPointsList.add(CommitPoints.fromXContent(container.readBlobFully(name)));
-                } catch (Exception e) {
-                    logger.warn("failed to read commit point [{}]", e, name);
-                }
-            }
-        }
-        CommitPoints commitPoints = new CommitPoints(commitPointsList);
-        if (commitPoints.commits().isEmpty()) {
-            return null;
-        }
-        return commitPoints.commits().get(0);
-    }
-
     @Override public String toString() {
         return type() + "://" + blobStore + "/" + indexPath;
     }
@@ -103,6 +74,10 @@ public abstract class BlobStoreIndexGateway extends AbstractIndexComponent imple
 
     public BlobPath shardPath(int shardId) {
         return indexPath.add(Integer.toString(shardId));
+    }
+
+    public static BlobPath shardPath(BlobPath basePath, String index, int shardId) {
+        return basePath.add("indices").add(index).add(Integer.toString(shardId));
     }
 
     @Override public void close(boolean delete) throws ElasticSearchException {

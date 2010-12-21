59ee8493cedd0145bea392d3c7be61ae3122ca65
kimchy <kimchy@gmail.com>
2010-12-21 23:43:05 +0200

    use public terms filter in all places

--- modules/elasticsearch/src/main/java/org/apache/lucene/search/PublicTermsFilter.java
+++ modules/elasticsearch/src/main/java/org/apache/lucene/search/PublicTermsFilter.java
@@ -25,18 +25,51 @@ import org.apache.lucene.index.TermDocs;
 import org.apache.lucene.util.OpenBitSet;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author kimchy (shay.banon)
  */
-public class PublicTermsFilter extends TermsFilter {
+// LUCENE MONITOR: Against TermsFilter
+public class PublicTermsFilter extends Filter {
+
+    Set<Term> terms = new TreeSet<Term>();
+
+    /**
+     * Adds a term to the list of acceptable terms
+     *
+     * @param term
+     */
+    public void addTerm(Term term) {
+        terms.add(term);
+    }
 
     public Set<Term> getTerms() {
         return terms;
     }
 
-    // override default impl here to use fastSet...
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if ((obj == null) || (obj.getClass() != this.getClass()))
+            return false;
+        PublicTermsFilter test = (PublicTermsFilter) obj;
+        return (terms == test.terms ||
+                (terms != null && terms.equals(test.terms)));
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 9;
+        for (Iterator<Term> iter = terms.iterator(); iter.hasNext();) {
+            Term term = iter.next();
+            hash = 31 * hash + term.hashCode();
+        }
+        return hash;
+    }
 
     @Override
     public DocIdSet getDocIdSet(IndexReader reader) throws IOException {
@@ -49,8 +82,7 @@ public class PublicTermsFilter extends TermsFilter {
                     result.fastSet(td.doc());
                 }
             }
-        }
-        finally {
+        } finally {
             td.close();
         }
         return result;

d523104b91af4a8dff1c4b2691efdc6f857c8c3d
kimchy <kimchy@gmail.com>
2011-01-01 23:48:25 +0200

    add some sanity tests with joda for date time manipulation

--- modules/elasticsearch/src/test/java/org/elasticsearch/deps/joda/SimpleJodaTests.java
+++ modules/elasticsearch/src/test/java/org/elasticsearch/deps/joda/SimpleJodaTests.java
@@ -70,6 +70,10 @@ public class SimpleJodaTests {
         } catch (IllegalArgumentException e) {
             // all is well
         }
+
+        // test offset in format
+        millis = formatter.parseMillis("1970-01-01T00:00:00-02:00");
+        assertThat(millis, equalTo(TimeValue.timeValueHours(2).millis()));
     }
 
     @Test public void testIsoVsCustom() {

17e6a9e8cc79f2a010eee952d5af3081c8d5be5d
harryf <harry@local.ch>
2011-01-06 01:44:11 +0100

    Add Snowball stemmer as analyzer and filter

---
+++ modules/elasticsearch/src/main/java/org/elasticsearch/index/analysis/SnowballAnalyzerProvider.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to Elastic Search and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Elastic Search licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.index.analysis;
+
+import org.apache.lucene.analysis.StopAnalyzer;
+import org.apache.lucene.analysis.de.GermanAnalyzer;
+import org.apache.lucene.analysis.fr.FrenchAnalyzer;
+import org.apache.lucene.analysis.nl.DutchAnalyzer;
+import org.apache.lucene.analysis.snowball.SnowballAnalyzer;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.inject.assistedinject.Assisted;
+import org.elasticsearch.common.lucene.Lucene;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.Index;
+import org.elasticsearch.index.settings.IndexSettings;
+
+import java.util.Set;
+
+/**
+ * Creates a SnowballAnalyzer initialized with stopwords and Snowball filter. Only
+ * supports Dutch, English (default), French, German and German2 where stopwords
+ * are readily available. For other languages available with the Lucene Snowball
+ * Stemmer, use them directly with the SnowballFilter and a CustomAnalyzer.
+ * Configuration of language is done with the "language" attribute or the analyzer.
+ * Also supports additional stopwords via "stopwords" attribute
+ *
+ * The SnowballAnalyzer comes with a StandardFilter, LowerCaseFilter, StopFilter
+ * and the SnowballFilter.
+ *
+ * @author harryf (Harry Fuecks)
+ */
+public class SnowballAnalyzerProvider extends AbstractIndexAnalyzerProvider<SnowballAnalyzer> {
+
+    private enum SupportedAnalyzer {
+        DUTCH {
+            public Set<?> getStopwords() {
+                return DutchAnalyzer.getDefaultStopSet();
+            }
+        },
+        ENGLISH {
+            public Set<?> getStopwords() {
+                return StopAnalyzer.ENGLISH_STOP_WORDS_SET;
+            }
+        },
+        FRENCH {
+            public Set<?> getStopwords() {
+                return FrenchAnalyzer.getDefaultStopSet();
+            }
+        },
+        GERMAN {
+            public Set<?> getStopwords() {
+                return GermanAnalyzer.getDefaultStopSet();
+            }
+        },
+        GERMAN2 {
+            public Set<?> getStopwords() {
+                return GermanAnalyzer.getDefaultStopSet();
+            }
+        };
+
+        public abstract Set<?> getStopwords();
+    }
+
+    private final SnowballAnalyzer analyzer;
+
+    @Inject public SnowballAnalyzerProvider(Index index, @IndexSettings Settings indexSettings, @Assisted String name, @Assisted Settings settings) {
+        super(index, indexSettings, name);
+
+        String language = settings.get("language", "English");
+        Set<?> stopWords = Analysis.parseStopWords(settings,
+                SupportedAnalyzer.valueOf(language.toUpperCase()).getStopwords());
+
+        analyzer = new SnowballAnalyzer(Lucene.VERSION, language, stopWords);
+    }
+
+    @Override public SnowballAnalyzer get() {
+        return this.analyzer;
+    }
+}

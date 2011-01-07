8b8bad757224db04d58f785fd65ea78accf918d4
kimchy <kimchy@gmail.com>
2011-01-07 14:34:34 +0200

    build on aferreira improvements to handle empty parameters with no =, and improve component decoding logic

--- modules/elasticsearch/src/test/java/org/elasticsearch/rest/util/RestUtilsTests.java
+++ modules/elasticsearch/src/test/java/org/elasticsearch/rest/util/RestUtilsTests.java
@@ -84,37 +84,40 @@ public class RestUtilsTests {
         params.clear();
         uri = "something?=";
         RestUtils.decodeQueryString(uri, uri.indexOf('?') + 1, params);
-        assertThat(params.size(), equalTo(1));
-        assertThat(params.get(""), equalTo(""));
+        assertThat(params.size(), equalTo(0));
 
         params.clear();
         uri = "something?&=";
         RestUtils.decodeQueryString(uri, uri.indexOf('?') + 1, params);
-        assertThat(params.size(), equalTo(1));
-        assertThat(params.get(""), equalTo(""));
+        assertThat(params.size(), equalTo(0));
 
         params.clear();
         uri = "something?a";
         RestUtils.decodeQueryString(uri, uri.indexOf('?') + 1, params);
-        assertThat(params.size(), equalTo(0));
+        assertThat(params.size(), equalTo(1));
+        assertThat(params.get("a"), equalTo(""));
 
         params.clear();
         uri = "something?p=v&a";
         RestUtils.decodeQueryString(uri, uri.indexOf('?') + 1, params);
-        assertThat(params.size(), equalTo(1));
+        assertThat(params.size(), equalTo(2));
+        assertThat(params.get("a"), equalTo(""));
         assertThat(params.get("p"), equalTo("v"));
 
         params.clear();
         uri = "something?p=v&a&p1=v1";
         RestUtils.decodeQueryString(uri, uri.indexOf('?') + 1, params);
-        assertThat(params.size(), equalTo(2));
+        assertThat(params.size(), equalTo(3));
+        assertThat(params.get("a"), equalTo(""));
         assertThat(params.get("p"), equalTo("v"));
         assertThat(params.get("p1"), equalTo("v1"));
 
         params.clear();
         uri = "something?p=v&a&b&p1=v1";
         RestUtils.decodeQueryString(uri, uri.indexOf('?') + 1, params);
-        assertThat(params.size(), equalTo(2));
+        assertThat(params.size(), equalTo(4));
+        assertThat(params.get("a"), equalTo(""));
+        assertThat(params.get("b"), equalTo(""));
         assertThat(params.get("p"), equalTo("v"));
         assertThat(params.get("p1"), equalTo("v1"));
     }

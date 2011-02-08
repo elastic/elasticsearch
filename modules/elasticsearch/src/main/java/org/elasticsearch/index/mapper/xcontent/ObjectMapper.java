a03405cdccab0b5a47a94f1d1b75486ca0160fb3
kimchy <kimchy@gmail.com>
2011-02-08 11:38:02 +0200

    dynamic flag on object type should lower case itself when serialized back to its json representation

--- modules/elasticsearch/src/main/java/org/elasticsearch/index/mapper/xcontent/ObjectMapper.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/index/mapper/xcontent/ObjectMapper.java
@@ -140,7 +140,7 @@ public class ObjectMapper implements XContentMapper, IncludeInAllMapper {
 
                 if (fieldName.equals("dynamic")) {
                     String value = fieldNode.toString();
-                    if (value.equals("strict")) {
+                    if (value.equalsIgnoreCase("strict")) {
                         builder.dynamic(Dynamic.STRICT);
                     } else {
                         builder.dynamic(nodeBooleanValue(fieldNode) ? Dynamic.TRUE : Dynamic.FALSE);
@@ -590,11 +590,11 @@ public class ObjectMapper implements XContentMapper, IncludeInAllMapper {
         // inherit the root behavior
         if (this instanceof RootObjectMapper) {
             if (dynamic != Dynamic.TRUE) {
-                builder.field("dynamic", dynamic);
+                builder.field("dynamic", dynamic.name().toLowerCase());
             }
         } else {
             if (dynamic != Defaults.DYNAMIC) {
-                builder.field("dynamic", dynamic);
+                builder.field("dynamic", dynamic.name().toLowerCase());
             }
         }
         if (enabled != Defaults.ENABLED) {

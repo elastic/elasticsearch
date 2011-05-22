93c4217f63c2b6cb1773cbd963377d2cf17d05f1
kimchy <kimchy@gmail.com>
2011-05-22 15:15:44 +0300

    remove bytes caching for stream copy

--- modules/elasticsearch/src/main/java/org/elasticsearch/common/xcontent/json/JsonXContentGenerator.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/common/xcontent/json/JsonXContentGenerator.java
@@ -19,7 +19,6 @@
 
 package org.elasticsearch.common.xcontent.json;
 
-import org.elasticsearch.common.CacheRecycler;
 import org.elasticsearch.common.io.Streams;
 import org.elasticsearch.common.jackson.JsonGenerator;
 import org.elasticsearch.common.xcontent.*;
@@ -214,8 +213,7 @@ public class JsonXContentGenerator implements XContentGenerator {
         generator.writeRaw(fieldName);
         generator.writeRaw("\" : ");
         flush();
-        byte[] bytes = CacheRecycler.popBytes();
-        Streams.copy(content, bos, bytes);
+        Streams.copy(content, bos);
     }
 
     @Override public void copyCurrentStructure(XContentParser parser) throws IOException {

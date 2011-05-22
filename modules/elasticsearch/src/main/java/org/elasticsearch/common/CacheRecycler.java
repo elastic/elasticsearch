5a95ee7b58bc8551bc4b3def220fbb25a80a94cd
kimchy <kimchy@gmail.com>
2011-05-22 15:15:44 +0300

    remove bytes caching for stream copy

--- modules/elasticsearch/src/main/java/org/elasticsearch/common/CacheRecycler.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/common/CacheRecycler.java
@@ -34,7 +34,6 @@ public class CacheRecycler {
 
     public static void clear() {
         BufferRecycler.clean();
-        bytes.remove();
         doubleObjectHashMap.remove();
         longObjectHashMap.remove();
         longLongHashMap.remove();
@@ -45,27 +44,6 @@ public class CacheRecycler {
         longIntHashMap.remove();
         objectIntHashMap.remove();
         intArray.remove();
-    }
-
-    // Bytes
-    private static ThreadLocal<SoftReference<byte[]>> bytes = new ThreadLocal<SoftReference<byte[]>>();
-
-    public static byte[] popBytes() {
-        SoftReference<byte[]> ref = bytes.get();
-        byte[] bb = ref == null ? null : ref.get();
-        if (bb == null) {
-            bb = new byte[1024];
-            bytes.set(new SoftReference<byte[]>(bb));
-        }
-        return bb;
-    }
-
-    public static void pushBytes(byte[] bb) {
-        SoftReference<byte[]> ref = bytes.get();
-        byte[] bb1 = ref == null ? null : ref.get();
-        if (bb1 != null && bb1.length < bb.length) {
-            bytes.set(new SoftReference<byte[]>(bb));
-        }
     }
 
     // ----- ExtTHashMap -----

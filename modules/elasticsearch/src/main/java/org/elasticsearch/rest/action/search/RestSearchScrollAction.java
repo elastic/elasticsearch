b1e1f48b187d15f6112f97cdf84c4d62b84a2438
kimchy <kimchy@gmail.com>
2011-06-15 16:28:53 +0300

    Search: When a search request failed completely (all shards fail) return a proper HTTP status code, closes #1035.

--- modules/elasticsearch/src/main/java/org/elasticsearch/rest/action/search/RestSearchScrollAction.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/rest/action/search/RestSearchScrollAction.java
@@ -27,7 +27,12 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.rest.*;
+import org.elasticsearch.rest.BaseRestHandler;
+import org.elasticsearch.rest.RestChannel;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.rest.XContentRestResponse;
+import org.elasticsearch.rest.XContentThrowableRestResponse;
 import org.elasticsearch.search.Scroll;
 
 import java.io.IOException;
@@ -86,7 +91,7 @@ public class RestSearchScrollAction extends BaseRestHandler {
                     builder.startObject();
                     response.toXContent(builder, request);
                     builder.endObject();
-                    channel.sendResponse(new XContentRestResponse(request, OK, builder));
+                    channel.sendResponse(new XContentRestResponse(request, response.status(), builder));
                 } catch (Exception e) {
                     onFailure(e);
                 }

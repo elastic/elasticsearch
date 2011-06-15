ff98deee4e589d068af58300cc73609b64835fad
kimchy <kimchy@gmail.com>
2011-06-15 14:11:15 +0300

    add a narrowing search scan test

--- modules/test/integration/src/test/java/org/elasticsearch/test/integration/search/scan/SearchScanTests.java
+++ modules/test/integration/src/test/java/org/elasticsearch/test/integration/search/scan/SearchScanTests.java
@@ -33,6 +33,7 @@ import org.testng.annotations.Test;
 
 import java.util.Set;
 
+import static org.elasticsearch.common.xcontent.XContentFactory.*;
 import static org.elasticsearch.index.query.QueryBuilders.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
@@ -56,160 +57,45 @@ public class SearchScanTests extends AbstractNodesTests {
         return client("node1");
     }
 
-    @Test public void shard1docs100size3() throws Exception {
-        testScroll(1, 100, 3);
-    }
-
-    @Test public void shard1docs100size7() throws Exception {
-        testScroll(1, 100, 7);
-    }
-
-    @Test public void shard1docs100size13() throws Exception {
-        testScroll(1, 100, 13);
-    }
-
-    @Test public void shard1docs100size24() throws Exception {
-        testScroll(1, 100, 24);
-    }
-
-    @Test public void shard1docs100size45() throws Exception {
-        testScroll(1, 100, 45);
-    }
-
-    @Test public void shard1docs100size63() throws Exception {
-        testScroll(1, 100, 63);
-    }
-
-    @Test public void shard1docs100size89() throws Exception {
-        testScroll(1, 100, 89);
-    }
-
-    @Test public void shard1docs100size99() throws Exception {
-        testScroll(1, 100, 99);
-    }
-
-    @Test public void shard1docs100size100() throws Exception {
-        testScroll(1, 100, 100);
-    }
-
-    @Test public void shard1docs100size101() throws Exception {
-        testScroll(1, 100, 101);
-    }
-
-    @Test public void shard1docs100size120() throws Exception {
-        testScroll(1, 100, 120);
-    }
-
-    @Test public void shard3docs100size3() throws Exception {
-        testScroll(3, 100, 3);
-    }
-
-    @Test public void shard3docs100size7() throws Exception {
-        testScroll(3, 100, 7);
-    }
-
-    @Test public void shard3docs100size13() throws Exception {
-        testScroll(3, 100, 13);
-    }
-
-    @Test public void shard3docs100size24() throws Exception {
-        testScroll(3, 100, 24);
-    }
-
-    @Test public void shard3docs100size45() throws Exception {
-        testScroll(3, 100, 45);
-    }
-
-    @Test public void shard3docs100size63() throws Exception {
-        testScroll(3, 100, 63);
-    }
-
-    @Test public void shard3docs100size89() throws Exception {
-        testScroll(3, 100, 89);
-    }
-
-    @Test public void shard3docs100size120() throws Exception {
-        testScroll(3, 100, 120);
-    }
-
-    @Test public void shard3docs100size3Unbalanced() throws Exception {
-        testScroll(3, 100, 3, true);
-    }
-
-    @Test public void shard3docs100size7Unbalanced() throws Exception {
-        testScroll(3, 100, 7, true);
-    }
-
-    @Test public void shard3docs100size13Unbalanced() throws Exception {
-        testScroll(3, 100, 13, true);
-    }
-
-    @Test public void shard3docs100size24Unbalanced() throws Exception {
-        testScroll(3, 100, 24, true);
-    }
-
-    @Test public void shard3docs100size45Unbalanced() throws Exception {
-        testScroll(3, 100, 45, true);
-    }
-
-    @Test public void shard3docs100size63Unbalanced() throws Exception {
-        testScroll(3, 100, 63, true);
-    }
-
-    @Test public void shard3docs100size89Unbalanced() throws Exception {
-        testScroll(3, 100, 89, true);
-    }
-
-    @Test public void shard3docs100size120Unbalanced() throws Exception {
-        testScroll(3, 100, 120);
-    }
-
-    private void testScroll(int numberOfShards, long numberOfDocs, int size) throws Exception {
-        testScroll(numberOfShards, numberOfDocs, size, false);
-    }
-
-    private void testScroll(int numberOfShards, long numberOfDocs, int size, boolean unbalanced) throws Exception {
+    @Test public void testNarrowingQuery() throws Exception {
         try {
             client.admin().indices().prepareDelete("test").execute().actionGet();
         } catch (Exception e) {
             // ignore
         }
-        client.admin().indices().prepareCreate("test").setSettings(ImmutableSettings.settingsBuilder().put("index.number_of_shards", numberOfShards)).execute().actionGet();
+        client.admin().indices().prepareCreate("test").setSettings(ImmutableSettings.settingsBuilder().put("index.number_of_shards", 5)).execute().actionGet();
         client.admin().cluster().prepareHealth().setWaitForGreenStatus().execute().actionGet();
+
 
         Set<String> ids = Sets.newHashSet();
         Set<String> expectedIds = Sets.newHashSet();
-        for (int i = 0; i < numberOfDocs; i++) {
-            String id = Integer.toString(i);
-            expectedIds.add(id);
-            String routing = null;
-            if (unbalanced) {
-                if (i < (numberOfDocs * 0.6)) {
-                    routing = "0";
-                } else if (i < (numberOfDocs * 0.9)) {
-                    routing = "1";
-                } else {
-                    routing = "2";
-                }
-            }
-            client.prepareIndex("test", "type1", id).setRouting(routing).setSource("field", i).execute().actionGet();
+
+        for (int i = 0; i < 100; i++) {
+            expectedIds.add(Integer.toString(i));
+            client.prepareIndex("test", "tweet", Integer.toString(i)).setSource(
+                    jsonBuilder().startObject().field("user", "kimchy1").field("postDate", System.currentTimeMillis()).field("message", "test").endObject()).execute().actionGet();
+        }
+
+        for (int i = 100; i < 200; i++) {
+            client.prepareIndex("test", "tweet", Integer.toString(i)).setSource(
+                    jsonBuilder().startObject().field("user", "kimchy2").field("postDate", System.currentTimeMillis()).field("message", "test").endObject()).execute().actionGet();
         }
 
         client.admin().indices().prepareRefresh().execute().actionGet();
 
         SearchResponse searchResponse = client.prepareSearch()
                 .setSearchType(SearchType.SCAN)
-                .setQuery(matchAllQuery())
-                .setSize(size)
+                .setQuery(termQuery("user", "kimchy1"))
+                .setSize(35)
                 .setScroll(TimeValue.timeValueMinutes(2))
                 .execute().actionGet();
 
-        assertThat(searchResponse.hits().totalHits(), equalTo(numberOfDocs));
+        assertThat(searchResponse.hits().totalHits(), equalTo(100l));
 
         // start scrolling, until we get not results
         while (true) {
             searchResponse = client.prepareSearchScroll(searchResponse.scrollId()).setScroll(TimeValue.timeValueMinutes(2)).execute().actionGet();
-            assertThat(searchResponse.hits().totalHits(), equalTo(numberOfDocs));
+            assertThat(searchResponse.hits().totalHits(), equalTo(100l));
             assertThat(searchResponse.failedShards(), equalTo(0));
             for (SearchHit hit : searchResponse.hits()) {
                 assertThat(hit.id() + "should not exists in the result set", ids.contains(hit.id()), equalTo(false));

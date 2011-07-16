83ca40ed0a4b7e962f2812dd76387ac80b76c685
kimchy <kimchy@gmail.com>
2011-07-16 03:35:39 +0300

    Allow to configure node name using `node.name` (on top of current `name`), closes #1126.

--- modules/elasticsearch/src/main/java/org/elasticsearch/cluster/node/DiscoveryNode.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/cluster/node/DiscoveryNode.java
@@ -48,6 +48,7 @@ public class DiscoveryNode implements Streamable, Serializable {
 
     public static Map<String, String> buildCommonNodesAttributes(Settings settings) {
         Map<String, String> attributes = Maps.newHashMap(settings.getByPrefix("node.").getAsMap());
+        attributes.remove("name"); // name is extracted in other places
         if (attributes.containsKey("client")) {
             if (attributes.get("client").equals("false")) {
                 attributes.remove("client"); // this is the default

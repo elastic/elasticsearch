0972af4adec47a7187d6e8c331c6cd4755da72b0
Shay Banon <kimchy@gmail.com>
2011-09-09 14:06:54 +0300

    don't do double int[] lookup for ordinal

--- modules/elasticsearch/src/main/java/org/elasticsearch/index/field/data/shorts/MultiValueShortFieldData.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/index/field/data/shorts/MultiValueShortFieldData.java
@@ -160,7 +160,7 @@ public class MultiValueShortFieldData extends ShortFieldData {
             int loc = ordinal[docId];
             if (loc != 0) {
                 found = true;
-                proc.onOrdinal(docId, ordinal[docId]);
+                proc.onOrdinal(docId, loc);
             }
         }
         if (!found) {

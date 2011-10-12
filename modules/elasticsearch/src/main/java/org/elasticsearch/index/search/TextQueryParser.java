036710342323b14315b7f683299324ab5f2d62b4
Shay Banon <kimchy@gmail.com>
2011-10-12 22:14:03 +0200

    Text Query does not use search_analyzer on field that is prefixed with the type name, closes #1390.

--- modules/elasticsearch/src/main/java/org/elasticsearch/index/search/TextQueryParser.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/index/search/TextQueryParser.java
@@ -130,6 +130,9 @@ public class TextQueryParser {
             if (mapper != null) {
                 analyzer = mapper.searchAnalyzer();
             }
+            if (analyzer == null && smartNameFieldMappers != null && smartNameFieldMappers.docMapper() != null) {
+                analyzer = smartNameFieldMappers.docMapper().searchAnalyzer();
+            }
             if (analyzer == null) {
                 analyzer = parseContext.mapperService().searchAnalyzer();
             }

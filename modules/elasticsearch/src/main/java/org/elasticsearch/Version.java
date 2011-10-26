1a4ef3b80909088107553d9bcc39b0741e313e8b
Shay Banon <kimchy@gmail.com>
2011-10-27 01:43:08 +0200

    add 0.18.2 option

--- modules/elasticsearch/src/main/java/org/elasticsearch/Version.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/Version.java
@@ -38,6 +38,8 @@ public class Version {
     public static final Version V_0_18_0 = new Version(V_0_18_0_ID, false);
     public static final int V_0_18_1_ID = /*00*/180199;
     public static final Version V_0_18_1 = new Version(V_0_18_1_ID, false);
+    public static final int V_0_18_2_ID = /*00*/180299;
+    public static final Version V_0_18_2 = new Version(V_0_18_2_ID, false);
 
     public static final int V_0_19_0_ID = /*00*/190099;
     public static final Version V_0_19_0 = new Version(V_0_19_0_ID, true);
@@ -54,6 +56,8 @@ public class Version {
                 return V_0_18_0;
             case V_0_18_1_ID:
                 return V_0_18_1;
+            case V_0_18_2_ID:
+                return V_0_18_2;
             case V_0_19_0_ID:
                 return V_0_19_0;
             default:

b48fec06a6f4f23e96afcbae7551de675ca0d154
Shay Banon <kimchy@gmail.com>
2011-11-13 19:16:01 +0200

    don't use CLASSPATH in plugin, and simplify plugin.bat

--- bin/plugin.bat
+++ bin/plugin.bat
@@ -8,10 +8,7 @@ set SCRIPT_DIR=%~dp0
 for %%I in ("%SCRIPT_DIR%..") do set ES_HOME=%%~dpfI
 
 
-set ES_CLASSPATH=%CLASSPATH%;%ES_HOME%/lib/*
-set ES_PARAMS=-Delasticsearch -Des.path.home="%ES_HOME%"
-
-"%JAVA_HOME%\bin\java" %JAVA_OPTS% %ES_JAVA_OPTS% %ES_PARAMS% -cp "%ES_CLASSPATH%" "org.elasticsearch.plugins.PluginManager" %*
+"%JAVA_HOME%\bin\java" -Des.path.home="%ES_HOME%" -cp "%ES_HOME%/lib/*" "org.elasticsearch.plugins.PluginManager" %*
 goto finally
 
 

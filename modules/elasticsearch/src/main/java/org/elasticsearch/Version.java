3cd9fcddefae019c00b004ea0db60cb3c6914a50
Shay Banon <kimchy@gmail.com>
2011-11-29 13:52:30 +0200

    release 0.18.5

--- modules/elasticsearch/src/main/java/org/elasticsearch/Version.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/Version.java
@@ -45,7 +45,7 @@ public class Version {
     public static final int V_0_18_4_ID = /*00*/180499;
     public static final Version V_0_18_4 = new Version(V_0_18_4_ID, false);
     public static final int V_0_18_5_ID = /*00*/180599;
-    public static final Version V_0_18_5 = new Version(V_0_18_5_ID, true);
+    public static final Version V_0_18_5 = new Version(V_0_18_5_ID, false);
 
     public static final Version CURRENT = V_0_18_5;
 

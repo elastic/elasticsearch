346d2fc28c33c0660d43eff7200b3a3157efdbf4
Shay Banon <kimchy@gmail.com>
2011-12-12 20:53:47 +0200

    remove unused setting

--- src/main/java/org/elasticsearch/indices/recovery/RecoverySource.java
+++ src/main/java/org/elasticsearch/indices/recovery/RecoverySource.java
@@ -23,7 +23,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import org.apache.lucene.store.IndexInput;
 import org.elasticsearch.ElasticSearchException;
-import org.elasticsearch.cluster.metadata.MetaData;
 import org.elasticsearch.common.StopWatch;
 import org.elasticsearch.common.component.AbstractComponent;
 import org.elasticsearch.common.inject.Inject;
@@ -52,10 +51,6 @@ import java.util.concurrent.atomic.AtomicReference;
  * source shard to the target shard.
  */
 public class RecoverySource extends AbstractComponent {
-
-    static {
-        MetaData.addDynamicSettings("index.shard.recovery.concurrent_streams");
-    }
 
     public static class Actions {
         public static final String START_RECOVERY = "index/shard/recovery/startRecovery";

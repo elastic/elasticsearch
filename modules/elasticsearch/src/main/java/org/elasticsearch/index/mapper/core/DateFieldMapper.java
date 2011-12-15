166e4b00676e2a59392f07abc8a3c4485ec01a1d
Shay Banon <kimchy@gmail.com>
2011-12-15 20:50:43 +0200

    timestamp term match broken in 0.18.5, closes #1543.

--- modules/elasticsearch/src/main/java/org/elasticsearch/index/mapper/core/DateFieldMapper.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/index/mapper/core/DateFieldMapper.java
@@ -26,6 +26,7 @@ import org.apache.lucene.search.NumericRangeFilter;
 import org.apache.lucene.search.NumericRangeQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.util.NumericUtils;
+import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.Numbers;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.joda.FormatDateTimeFormatter;
@@ -44,6 +45,7 @@ import org.elasticsearch.index.mapper.MapperParsingException;
 import org.elasticsearch.index.mapper.MergeContext;
 import org.elasticsearch.index.mapper.MergeMappingException;
 import org.elasticsearch.index.mapper.ParseContext;
+import org.elasticsearch.index.query.QueryParseContext;
 import org.elasticsearch.index.search.NumericRangeFieldDataFilter;
 
 import java.io.IOException;
@@ -219,11 +221,23 @@ public class DateFieldMapper extends NumberFieldMapper<Long> {
                 true, true);
     }
 
+    @Override public Query fieldQuery(String value, @Nullable QueryParseContext context) {
+        long lValue = parseStringValue(value);
+        return NumericRangeQuery.newLongRange(names.indexName(), precisionStep,
+                lValue, lValue, true, true);
+    }
+
     @Override public Query rangeQuery(String lowerTerm, String upperTerm, boolean includeLower, boolean includeUpper) {
         return NumericRangeQuery.newLongRange(names.indexName(), precisionStep,
                 lowerTerm == null ? null : parseStringValue(lowerTerm),
                 upperTerm == null ? null : includeUpper ? parseUpperInclusiveStringValue(upperTerm) : parseStringValue(upperTerm),
                 includeLower, includeUpper);
+    }
+
+    @Override public Filter fieldFilter(String value, @Nullable QueryParseContext context) {
+        long lValue = parseStringValue(value);
+        return NumericRangeFilter.newLongRange(names.indexName(), precisionStep,
+                lValue, lValue, true, true);
     }
 
     @Override public Filter rangeFilter(String lowerTerm, String upperTerm, boolean includeLower, boolean includeUpper) {

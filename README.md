e26f62a71e1825879f819d6fcc58f830bdfea9f1
Shay Banon <kimchy@gmail.com>
2011-12-29 11:58:31 +0200

    first!

---
+++ README.md
@@ -0,0 +1,16 @@
+ICU Analysis for ElasticSearch
+==================================
+
+The Smart Chinese Analysis plugin integrates Lucene Smart Chinese analysis module into elasticsearch.
+
+In order to install the plugin, simply run: `bin/plugin -install elasticsearch/elasticsearch-smartcn-icu/1.0.0`.
+
+    --------------------------------------------------
+    | Smart Chinese Analysis Plugin | ElasticSearch  |
+    --------------------------------------------------
+    | master                        | 0.18 -> master |
+    --------------------------------------------------
+    | 1.0.0                         | 0.18 -> master |
+    --------------------------------------------------
+
+The plugin includes the `smartcn` analyzer, `smartcn_sentence` tokenizer, and `smartcn_word` token filter.
---
+++ src/test/java/org/elasticsearch/index/analysis/SimpleSmartChineseAnalysisTests.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to ElasticSearch and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. ElasticSearch licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.index.analysis;
+
+import org.elasticsearch.common.inject.Injector;
+import org.elasticsearch.common.inject.ModulesBuilder;
+import org.elasticsearch.common.settings.SettingsModule;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.env.EnvironmentModule;
+import org.elasticsearch.index.Index;
+import org.elasticsearch.index.IndexNameModule;
+import org.elasticsearch.index.settings.IndexSettingsModule;
+import org.elasticsearch.indices.analysis.IndicesAnalysisModule;
+import org.elasticsearch.indices.analysis.IndicesAnalysisService;
+import org.hamcrest.MatcherAssert;
+import org.testng.annotations.Test;
+
+import static org.elasticsearch.common.settings.ImmutableSettings.Builder.EMPTY_SETTINGS;
+import static org.hamcrest.Matchers.instanceOf;
+
+/**
+ */
+public class SimpleSmartChineseAnalysisTests {
+
+    @Test
+    public void testDefaultsIcuAnalysis() {
+        Index index = new Index("test");
+
+        Injector parentInjector = new ModulesBuilder().add(new SettingsModule(EMPTY_SETTINGS), new EnvironmentModule(new Environment(EMPTY_SETTINGS)), new IndicesAnalysisModule()).createInjector();
+        Injector injector = new ModulesBuilder().add(
+                new IndexSettingsModule(index, EMPTY_SETTINGS),
+                new IndexNameModule(index),
+                new AnalysisModule(EMPTY_SETTINGS, parentInjector.getInstance(IndicesAnalysisService.class)).addProcessor(new SmartChineseAnalysisBinderProcessor()))
+                .createChildInjector(parentInjector);
+
+        AnalysisService analysisService = injector.getInstance(AnalysisService.class);
+
+        TokenizerFactory tokenizerFactory = analysisService.tokenizer("smartcn_sentence");
+        MatcherAssert.assertThat(tokenizerFactory, instanceOf(SmartChineseSentenceTokenizerFactory.class));
+
+        TokenFilterFactory filterFactory = analysisService.tokenFilter("smartcn_word");
+        MatcherAssert.assertThat(filterFactory, instanceOf(SmartChineseWordTokenFilterFactory.class));
+    }
+}

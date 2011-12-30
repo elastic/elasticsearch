3c88eacb01b53085b83b0533a4f5d9a5425e2b3f
Shay Banon <kimchy@gmail.com>
2011-12-30 13:42:08 +0200

    bind transport validate query actions as singletons

--- src/main/java/org/elasticsearch/action/TransportActionModule.java
+++ src/main/java/org/elasticsearch/action/TransportActionModule.java
@@ -52,6 +52,7 @@ import org.elasticsearch.action.admin.indices.stats.TransportIndicesStatsAction;
 import org.elasticsearch.action.admin.indices.status.TransportIndicesStatusAction;
 import org.elasticsearch.action.admin.indices.template.delete.TransportDeleteIndexTemplateAction;
 import org.elasticsearch.action.admin.indices.template.put.TransportPutIndexTemplateAction;
+import org.elasticsearch.action.admin.indices.validate.query.TransportValidateQueryAction;
 import org.elasticsearch.action.bulk.TransportBulkAction;
 import org.elasticsearch.action.bulk.TransportShardBulkAction;
 import org.elasticsearch.action.count.TransportCountAction;
@@ -110,6 +111,7 @@ public class TransportActionModule extends AbstractModule {
         bind(TransportAnalyzeAction.class).asEagerSingleton();
         bind(TransportPutIndexTemplateAction.class).asEagerSingleton();
         bind(TransportDeleteIndexTemplateAction.class).asEagerSingleton();
+        bind(TransportValidateQueryAction.class).asEagerSingleton();
 
         bind(TransportGatewaySnapshotAction.class).asEagerSingleton();
 

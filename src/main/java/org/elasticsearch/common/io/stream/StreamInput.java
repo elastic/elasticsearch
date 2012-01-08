38853aab2b26d9e539a0874da0aafa9bdae15a64
Shay Banon <kimchy@gmail.com>
2012-01-08 15:26:33 +0200

    Improve serialization (stream) of UTF strings, note, requires flush when upgrading, closes #1595.

--- src/main/java/org/elasticsearch/common/io/stream/StreamInput.java
+++ src/main/java/org/elasticsearch/common/io/stream/StreamInput.java
@@ -24,19 +24,12 @@ import org.elasticsearch.common.Nullable;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UTFDataFormatException;
 import java.util.*;
 
 /**
  *
  */
 public abstract class StreamInput extends InputStream {
-
-    /**
-     * working arrays initialized on demand by readUTF
-     */
-    private byte bytearr[] = new byte[80];
-    protected char chararr[] = new char[80];
 
     /**
      * Reads and returns a single byte.
@@ -154,35 +147,12 @@ public abstract class StreamInput extends InputStream {
         return i | ((b & 0x7FL) << 56);
     }
 
-    // COPIED from DataInputStream
-
     public String readUTF() throws IOException {
-        int utflen = readInt();
-        if (utflen == 0) {
-            return "";
-        }
-        if (bytearr.length < utflen) {
-            bytearr = new byte[utflen * 2];
-            chararr = new char[utflen * 2];
-        }
-        char[] chararr = this.chararr;
-        byte[] bytearr = this.bytearr;
-
-        int c, char2, char3;
-        int count = 0;
-        int chararr_count = 0;
-
-        readBytes(bytearr, 0, utflen);
-
-        while (count < utflen) {
-            c = (int) bytearr[count] & 0xff;
-            if (c > 127) break;
-            count++;
-            chararr[chararr_count++] = (char) c;
-        }
-
-        while (count < utflen) {
-            c = (int) bytearr[count] & 0xff;
+        int charCount = readVInt();
+        char[] chars = CachedStreamInput.getCharArray(charCount);
+        int c, charIndex = 0;
+        while (charIndex < charCount) {
+            c = readByte() & 0xff;
             switch (c >> 4) {
                 case 0:
                 case 1:
@@ -192,47 +162,18 @@ public abstract class StreamInput extends InputStream {
                 case 5:
                 case 6:
                 case 7:
-                    /* 0xxxxxxx*/
-                    count++;
-                    chararr[chararr_count++] = (char) c;
+                    chars[charIndex++] = (char) c;
                     break;
                 case 12:
                 case 13:
-                    /* 110x xxxx   10xx xxxx*/
-                    count += 2;
-                    if (count > utflen)
-                        throw new UTFDataFormatException(
-                                "malformed input: partial character at end");
-                    char2 = (int) bytearr[count - 1];
-                    if ((char2 & 0xC0) != 0x80)
-                        throw new UTFDataFormatException(
-                                "malformed input around byte " + count);
-                    chararr[chararr_count++] = (char) (((c & 0x1F) << 6) |
-                            (char2 & 0x3F));
+                    chars[charIndex++] = (char) ((c & 0x1F) << 6 | readByte() & 0x3F);
                     break;
                 case 14:
-                    /* 1110 xxxx  10xx xxxx  10xx xxxx */
-                    count += 3;
-                    if (count > utflen)
-                        throw new UTFDataFormatException(
-                                "malformed input: partial character at end");
-                    char2 = (int) bytearr[count - 2];
-                    char3 = (int) bytearr[count - 1];
-                    if (((char2 & 0xC0) != 0x80) || ((char3 & 0xC0) != 0x80))
-                        throw new UTFDataFormatException(
-                                "malformed input around byte " + (count - 1));
-                    chararr[chararr_count++] = (char) (((c & 0x0F) << 12) |
-                            ((char2 & 0x3F) << 6) |
-                            ((char3 & 0x3F) << 0));
+                    chars[charIndex++] = (char) ((c & 0x0F) << 12 | (readByte() & 0x3F) << 6 | (readByte() & 0x3F) << 0);
                     break;
-                default:
-                    /* 10xx xxxx,  1111 xxxx */
-                    throw new UTFDataFormatException(
-                            "malformed input around byte " + count);
             }
         }
-        // The number of chars produced may be less than utflen
-        return new String(chararr, 0, chararr_count);
+        return new String(chars, 0, charCount);
     }
 
 

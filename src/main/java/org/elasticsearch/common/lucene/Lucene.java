2c3332132e740d57364f04f66aab632811754cf5
Shay Banon <kimchy@gmail.com>
2012-01-16 13:40:49 +0200

    lucene 3.5 verison

--- src/main/java/org/elasticsearch/common/lucene/Lucene.java
+++ src/main/java/org/elasticsearch/common/lucene/Lucene.java
@@ -44,7 +44,7 @@ import java.util.Map;
  */
 public class Lucene {
 
-    public static final Version VERSION = Version.LUCENE_34;
+    public static final Version VERSION = Version.LUCENE_35;
     public static final Version ANALYZER_VERSION = VERSION;
     public static final Version QUERYPARSER_VERSION = VERSION;
 
@@ -58,6 +58,9 @@ public class Lucene {
     public static Version parseVersion(@Nullable String version, Version defaultVersion, ESLogger logger) {
         if (version == null) {
             return defaultVersion;
+        }
+        if ("3.5".equals(version)) {
+            return Version.LUCENE_35;
         }
         if ("3.4".equals(version)) {
             return Version.LUCENE_34;

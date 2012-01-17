534f487de3edd7b6a4ca6d6aed7bdbc5b0ae2b7a
Shay Banon <kimchy@gmail.com>
2012-01-18 01:08:35 +0200

    Local Gateway: Move shard state to be stored under each shard, and not globally under _state, closes #1618.

--- src/main/java/org/elasticsearch/gateway/local/LocalGatewayModule.java
+++ src/main/java/org/elasticsearch/gateway/local/LocalGatewayModule.java
@@ -24,6 +24,8 @@ import org.elasticsearch.common.inject.AbstractModule;
 import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.common.inject.PreProcessModule;
 import org.elasticsearch.gateway.Gateway;
+import org.elasticsearch.gateway.local.state.shards.LocalGatewayShardsState;
+import org.elasticsearch.gateway.local.state.shards.TransportNodesListGatewayStartedShards;
 
 /**
  *
@@ -33,6 +35,7 @@ public class LocalGatewayModule extends AbstractModule implements PreProcessModu
     @Override
     protected void configure() {
         bind(Gateway.class).to(LocalGateway.class).asEagerSingleton();
+        bind(LocalGatewayShardsState.class).asEagerSingleton();
         bind(TransportNodesListGatewayMetaState.class).asEagerSingleton();
         bind(TransportNodesListGatewayStartedShards.class).asEagerSingleton();
     }

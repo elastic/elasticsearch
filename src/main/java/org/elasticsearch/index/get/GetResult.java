dc09191afc843ed9677d5e42c25f3f6543076d36
Shay Banon <kimchy@gmail.com>
2012-03-18 12:06:16 +0200

    Get/MultiGet API with no type provided and doc does not exists causes a failure, closes #1794.

--- src/main/java/org/elasticsearch/index/get/GetResult.java
+++ src/main/java/org/elasticsearch/index/get/GetResult.java
@@ -309,7 +309,7 @@ public class GetResult implements Streamable, Iterable<GetField>, ToXContent {
     @Override
     public void readFrom(StreamInput in) throws IOException {
         index = in.readUTF();
-        type = in.readUTF();
+        type = in.readOptionalUTF();
         id = in.readUTF();
         version = in.readLong();
         exists = in.readBoolean();
@@ -334,7 +334,7 @@ public class GetResult implements Streamable, Iterable<GetField>, ToXContent {
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         out.writeUTF(index);
-        out.writeUTF(type);
+        out.writeOptionalUTF(type);
         out.writeUTF(id);
         out.writeLong(version);
         out.writeBoolean(exists);

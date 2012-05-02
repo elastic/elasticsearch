df55fdfc76cbada2cabeab5bdf2b50b49b2382cf
Shay Banon <kimchy@gmail.com>
2012-05-02 20:52:54 +0300

    Index Merge Scheduler: Configuring using `serial` fails to load, closes #1901.

--- src/main/java/org/apache/lucene/index/TrackingSerialMergeScheduler.java
+++ src/main/java/org/apache/lucene/index/TrackingSerialMergeScheduler.java
@@ -90,8 +90,9 @@ public class TrackingSerialMergeScheduler extends MergeScheduler {
             currentMergesNumDocs.inc(totalNumDocs);
             currentMergesSizeInBytes.inc(totalSizeInBytes);
 
+            // sadly, segment name is not available since mergeInit is called from merge itself...
             if (logger.isTraceEnabled()) {
-                logger.trace("merge [{}] starting..., merging [{}]/[{}], into [{}]", merge.info.name, totalNumDocs, new ByteSizeValue(totalSizeInBytes), new ByteSizeValue(merge.estimatedMergeBytes));
+                logger.trace("merge [{}] starting..., merging [{}] segments, [{}] docs, [{}] size, into [{}] estimated_size", merge.info == null ? "_na_" : merge.info.name, merge.segments.size(), totalNumDocs, new ByteSizeValue(totalSizeInBytes), new ByteSizeValue(merge.estimatedMergeBytes));
             }
             try {
                 TrackingMergeScheduler.setCurrentMerge(merge);
@@ -108,9 +109,9 @@ public class TrackingSerialMergeScheduler extends MergeScheduler {
                 totalMergesSizeInBytes.inc(totalSizeInBytes);
                 totalMerges.inc(took);
                 if (took > 20000) { // if more than 20 seconds, DEBUG log it
-                    logger.debug("merge [{}] done, took [{}]", merge.info.name, TimeValue.timeValueMillis(took));
+                    logger.debug("merge [{}] done, took [{}]", merge.info == null ? "_na_" : merge.info.name, TimeValue.timeValueMillis(took));
                 } else if (logger.isTraceEnabled()) {
-                    logger.trace("merge [{}] done, took [{}]", merge.info.name, TimeValue.timeValueMillis(took));
+                    logger.trace("merge [{}] done, took [{}]", merge.info == null ? "_na_" : merge.info.name, TimeValue.timeValueMillis(took));
                 }
             }
         }

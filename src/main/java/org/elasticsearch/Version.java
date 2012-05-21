11c9c404e9cbe414934c66f67e49c51ead12676c
Shay Banon <kimchy@gmail.com>
2012-05-21 22:08:14 +0200

    add 0.19.5

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -73,6 +73,8 @@ public class Version implements Serializable {
     public static final Version V_0_19_3 = new Version(V_0_19_3_ID, false);
     public static final int V_0_19_4_ID = /*00*/190499;
     public static final Version V_0_19_4 = new Version(V_0_19_4_ID, false);
+    public static final int V_0_19_5_ID = /*00*/190599;
+    public static final Version V_0_19_5 = new Version(V_0_19_5_ID, false);
 
     public static final int V_0_20_0_Beta1_ID = /*00*/200001;
     public static final Version V_0_20_0_Beta1 = new Version(V_0_20_0_Beta1_ID, true);
@@ -120,6 +122,8 @@ public class Version implements Serializable {
                 return V_0_19_3;
             case V_0_19_4_ID:
                 return V_0_19_4;
+            case V_0_19_5_ID:
+                return V_0_19_5;
 
             case V_0_20_0_Beta1_ID:
                 return V_0_20_0_Beta1;

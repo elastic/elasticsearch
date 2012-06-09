026aaaf72dc13e17644e3f1305e41a5f396865df
Shay Banon <kimchy@gmail.com>
2012-06-10 00:42:54 +0200

    Upgrade to latest jst166y and jsr166e                                                                                                                                                                                            Embed the code now in our source, since jsr166e jar generation with 1.6 instead of 1.7 is complicated when doing it on its own as it relies on ThreadLocalRandom, and we have it in jsr166y

---
+++ src/main/java/jsr166e/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Written by Doug Lea with assistance from members of JCP JSR-166
+ * Expert Group and released to the public domain, as explained at
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+
+// Built on 2012-06-09
+package jsr166e;

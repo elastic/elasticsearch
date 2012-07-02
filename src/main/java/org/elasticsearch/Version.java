d0a1eb7f1fabf4597819be452a142b0a905ff73f
Shay Banon <kimchy@gmail.com>
2012-07-02 22:09:40 +0200

    move to 0.19.9 snap

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -81,8 +81,10 @@ public class Version implements Serializable {
     public static final Version V_0_19_7 = new Version(V_0_19_7_ID, false);
     public static final int V_0_19_8_ID = /*00*/190899;
     public static final Version V_0_19_8 = new Version(V_0_19_8_ID, false);
+    public static final int V_0_19_9_ID = /*00*/190999;
+    public static final Version V_0_19_9 = new Version(V_0_19_9_ID, true);
 
-    public static final Version CURRENT = V_0_19_8;
+    public static final Version CURRENT = V_0_19_9;
 
     public static Version readVersion(StreamInput in) throws IOException {
         return fromId(in.readVInt());
@@ -133,6 +135,8 @@ public class Version implements Serializable {
                 return V_0_19_7;
             case V_0_19_8_ID:
                 return V_0_19_8;
+            case V_0_19_9_ID:
+                return V_0_19_9;
             default:
                 return new Version(id, null);
         }

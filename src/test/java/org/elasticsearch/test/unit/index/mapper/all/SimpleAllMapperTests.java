9aae62b4a60718f4a7147f12a60485f1d379dbde
Shay Banon <kimchy@gmail.com>
2012-08-20 15:07:32 +0200

    All Field: Automatically detect when field level boosting is used, and optimize when its not, closes #2189.

--- src/test/java/org/elasticsearch/test/unit/index/mapper/all/SimpleAllMapperTests.java
+++ src/test/java/org/elasticsearch/test/unit/index/mapper/all/SimpleAllMapperTests.java
@@ -29,7 +29,6 @@ import org.elasticsearch.common.lucene.all.AllTermQuery;
 import org.elasticsearch.common.lucene.all.AllTokenStream;
 import org.elasticsearch.index.mapper.DocumentMapper;
 import org.elasticsearch.index.mapper.FieldMapper;
-import org.elasticsearch.index.mapper.FieldMappers;
 import org.elasticsearch.test.unit.index.mapper.MapperTests;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
@@ -60,7 +59,23 @@ public class SimpleAllMapperTests {
         FieldMapper mapper = docMapper.mappers().smartNameFieldMapper("_all");
         assertThat(mapper.queryStringTermQuery(new Term("_all", "foobar")), Matchers.instanceOf(AllTermQuery.class));
     }
-    
+
+    @Test
+    public void testAllMappersNoBoost() throws Exception {
+        String mapping = copyToStringFromClasspath("/org/elasticsearch/test/unit/index/mapper/all/noboost-mapping.json");
+        DocumentMapper docMapper = MapperTests.newParser().parse(mapping);
+        byte[] json = copyToBytesFromClasspath("/org/elasticsearch/test/unit/index/mapper/all/test1.json");
+        Document doc = docMapper.parse(new BytesArray(json)).rootDoc();
+        AllField field = (AllField) doc.getFieldable("_all");
+        AllEntries allEntries = ((AllTokenStream) field.tokenStreamValue()).allEntries();
+        assertThat(allEntries.fields().size(), equalTo(3));
+        assertThat(allEntries.fields().contains("address.last.location"), equalTo(true));
+        assertThat(allEntries.fields().contains("name.last"), equalTo(true));
+        assertThat(allEntries.fields().contains("simple1"), equalTo(true));
+        FieldMapper mapper = docMapper.mappers().smartNameFieldMapper("_all");
+        assertThat(mapper.queryStringTermQuery(new Term("_all", "foobar")), Matchers.instanceOf(TermQuery.class));
+    }
+
     @Test
     public void testAllMappersTermQuery() throws Exception {
         String mapping = copyToStringFromClasspath("/org/elasticsearch/test/unit/index/mapper/all/mapping_omit_positions_on_all.json");

6e66f45f58db2361540b1952b2e50d3983c3bc85
Shay Banon <kimchy@gmail.com>
2012-09-23 21:31:13 +0200

    minor geo shape fetch improvements

--- src/main/java/org/elasticsearch/index/query/GeoShapeFilterParser.java
+++ src/main/java/org/elasticsearch/index/query/GeoShapeFilterParser.java
@@ -1,9 +1,28 @@
+/*
+ * Licensed to ElasticSearch and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. ElasticSearch licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.elasticsearch.index.query;
 
 import com.spatial4j.core.shape.Shape;
 import org.apache.lucene.search.Filter;
-import org.elasticsearch.common.geo.ShapeRelation;
 import org.elasticsearch.common.geo.GeoJSONShapeParser;
+import org.elasticsearch.common.geo.ShapeRelation;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.internal.Nullable;
 import org.elasticsearch.common.xcontent.XContentParser;
@@ -19,12 +38,13 @@ import static org.elasticsearch.index.query.support.QueryParsers.wrapSmartNameFi
 
 /**
  * {@link FilterParser} for filtering Documents based on {@link Shape}s.
- *
+ * <p/>
  * Only those fields mapped using {@link GeoShapeFieldMapper} can be filtered
  * using this parser.
- *
+ * <p/>
  * Format supported:
- *
+ * <p/>
+ * <pre>
  * "field" : {
  *     "relation" : "intersects",
  *     "shape" : {
@@ -34,6 +54,7 @@ import static org.elasticsearch.index.query.support.QueryParsers.wrapSmartNameFi
  *         ]
  *     }
  * }
+ * </pre>
  */
 public class GeoShapeFilterParser implements FilterParser {
 
@@ -88,7 +109,7 @@ public class GeoShapeFilterParser implements FilterParser {
                             if (shapeRelation == null) {
                                 throw new QueryParsingException(parseContext.index(), "Unknown shape operation [" + parser.text() + " ]");
                             }
-                        } else if ("indexed_shape".equals(currentFieldName)) {
+                        } else if ("indexed_shape".equals(currentFieldName) || "indexedShape".equals(currentFieldName)) {
                             while ((token = parser.nextToken()) != XContentParser.Token.END_OBJECT) {
                                 if (token == XContentParser.Token.FIELD_NAME) {
                                     currentFieldName = parser.currentName();

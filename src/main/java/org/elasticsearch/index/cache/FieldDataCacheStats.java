b3387fe6bc5e43cab8a440065a3b01d7a10bc1a0
Andreas Sinz <andreas.sinz@aon.at>
2012-09-26 11:47:46 +0200

    Moved a string into a private class and documented the FieldDataCacheStats class a bit

--- src/main/java/org/elasticsearch/index/cache/FieldDataCacheStats.java
+++ src/main/java/org/elasticsearch/index/cache/FieldDataCacheStats.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.elasticsearch.common.xcontent.ToXContent;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentBuilderString;
 import org.elasticsearch.index.cache.field.data.support.AbstractConcurrentMapFieldDataCache;
 import org.elasticsearch.index.service.IndexService;
 
@@ -22,27 +23,35 @@ public class FieldDataCacheStats implements ToXContent {
 
 		AbstractConcurrentMapFieldDataCache fieldCache = (AbstractConcurrentMapFieldDataCache) indexService.cache().fieldData();
 
+		//Get the cache size for all fields of the given index
 		for (String elem : fieldCache.getFieldNames()) {
 			long cacheSize = fieldCache.sizeInBytes(elem);
 			tmp.put(elem, cacheSize);
 		}
 
+		//Put the whole fieldcache map into the global map
 		fieldCaches.put(indexService.index().getName(), tmp);
 	}
 
 	@Override
 	public XContentBuilder toXContent(XContentBuilder builder, Params params)
 			throws IOException {
-		builder.startObject("fieldDataCaches");
+		builder.startObject(Fields.FIELDDATACACHES);
 
 		Iterator<String> iterator = this.fieldCaches.keySet().iterator();
 		while(iterator.hasNext()) {
 			String key = iterator.next();
 			HashMap<String, Object> tmp = this.fieldCaches.get(key);
+			
+			//Append the map with all the fieldcaches and their size
 			builder.field(key, tmp);
 		}
 
 		builder.endObject();
 		return builder;
+	}
+	
+	static final class Fields {
+		static final XContentBuilderString FIELDDATACACHES = new XContentBuilderString("fieldDataCaches");
 	}
 }

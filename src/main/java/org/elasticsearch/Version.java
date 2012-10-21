6ec0071fa49afe6ca06b33ad9d68cc6e17a6ed31
Shay Banon <kimchy@gmail.com>
2012-10-22 01:51:20 +0200

    move master to be 0.21.0 Beta1 snap

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -89,7 +89,10 @@ public class Version implements Serializable {
     public static final int V_0_20_0_RC1_ID = /*00*/200051;
     public static final Version V_0_20_0_RC1 = new Version(V_0_20_0_RC1_ID, false);
 
-    public static final Version CURRENT = V_0_20_0_RC1;
+    public static final int V_0_21_0_Beta1_ID = /*00*/210001;
+    public static final Version V_0_21_0_Beta1 = new Version(V_0_21_0_Beta1_ID, false);
+
+    public static final Version CURRENT = V_0_21_0_Beta1;
 
     public static Version readVersion(StreamInput in) throws IOException {
         return fromId(in.readVInt());
@@ -97,6 +100,9 @@ public class Version implements Serializable {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_0_21_0_Beta1_ID:
+                return V_0_21_0_Beta1;
+
             case V_0_20_0_RC1_ID:
                 return V_0_20_0_RC1;
 

7434169f9a6d372f7c9d40255724d7d0d6bba610
Chris Male <gento0nz@gmail.com>
2012-10-25 15:36:02 +1300

    Changed SpatialRelation contains to within

--- src/main/java/org/elasticsearch/common/lucene/spatial/SpatialStrategy.java
+++ src/main/java/org/elasticsearch/common/lucene/spatial/SpatialStrategy.java
@@ -77,8 +77,8 @@ public abstract class SpatialStrategy {
         switch (relation) {
             case INTERSECTS:
                 return createIntersectsFilter(shape);
-            case CONTAINS:
-                return createContainsFilter(shape);
+            case WITHIN:
+                return createWithinFilter(shape);
             case DISJOINT:
                 return createDisjointFilter(shape);
             default:
@@ -98,8 +98,8 @@ public abstract class SpatialStrategy {
         switch (relation) {
             case INTERSECTS:
                 return createIntersectsQuery(shape);
-            case CONTAINS:
-                return createContainsQuery(shape);
+            case WITHIN:
+                return createWithinQuery(shape);
             case DISJOINT:
                 return createDisjointQuery(shape);
             default:
@@ -151,7 +151,7 @@ public abstract class SpatialStrategy {
      * @param shape Shape to find the contained Shapes of
      * @return Filter for finding the contained indexed Shapes
      */
-    public abstract Filter createContainsFilter(Shape shape);
+    public abstract Filter createWithinFilter(Shape shape);
 
     /**
      * Creates a Query that will find all indexed Shapes that are properly
@@ -161,7 +161,7 @@ public abstract class SpatialStrategy {
      * @param shape Shape to find the contained Shapes of
      * @return Query for finding the contained indexed Shapes
      */
-    public abstract Query createContainsQuery(Shape shape);
+    public abstract Query createWithinQuery(Shape shape);
 
     /**
      * Returns the name of the field this Strategy applies to

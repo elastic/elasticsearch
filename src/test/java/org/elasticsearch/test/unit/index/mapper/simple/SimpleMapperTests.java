5c0ef796e8ee48cd22eaa384436553ce4f94a6b8
Martijn van Groningen <martijn.v.groningen@gmail.com>
2012-11-02 15:48:27 +0100

    lucene 4: Upgraded BoostMappingTests + SimpleMapperTests

--- src/test/java/org/elasticsearch/test/unit/index/mapper/simple/SimpleMapperTests.java
+++ src/test/java/org/elasticsearch/test/unit/index/mapper/simple/SimpleMapperTests.java
@@ -20,7 +20,7 @@
 package org.elasticsearch.test.unit.index.mapper.simple;
 
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexableField;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.index.mapper.DocumentMapper;
@@ -29,7 +29,6 @@ import org.elasticsearch.index.mapper.Uid;
 import org.elasticsearch.test.unit.index.mapper.MapperTests;
 import org.testng.annotations.Test;
 
-import static org.apache.lucene.document.Field.Store.YES;
 import static org.elasticsearch.common.io.Streams.copyToBytesFromClasspath;
 import static org.elasticsearch.common.io.Streams.copyToStringFromClasspath;
 import static org.elasticsearch.index.mapper.MapperBuilders.*;
@@ -53,7 +52,10 @@ public class SimpleMapperTests {
         BytesReference json = new BytesArray(copyToBytesFromClasspath("/org/elasticsearch/test/unit/index/mapper/simple/test1.json"));
         Document doc = docMapper.parse("person", "1", json).rootDoc();
 
-        assertThat((double) doc.getBoost(), closeTo(3.7, 0.01));
+        assertThat(doc.getFields().size(), equalTo(14));
+        for (IndexableField field : doc.getFields()) {
+            assertThat((double) field.boost(), closeTo(3.7, 0.01));
+        }
         assertThat(doc.get(docMapper.mappers().name("first").mapper().names().indexName()), equalTo("shay"));
         assertThat(docMapper.mappers().name("first").mapper().names().fullName(), equalTo("name.first"));
 //        System.out.println("Document: " + doc);
@@ -74,7 +76,10 @@ public class SimpleMapperTests {
         BytesReference json = new BytesArray(copyToBytesFromClasspath("/org/elasticsearch/test/unit/index/mapper/simple/test1.json"));
         Document doc = builtDocMapper.parse(json).rootDoc();
         assertThat(doc.get(docMapper.uidMapper().names().indexName()), equalTo(Uid.createUid("person", "1")));
-        assertThat((double) doc.getBoost(), closeTo(3.7, 0.01));
+        assertThat(doc.getFields().size(), equalTo(14));
+        for (IndexableField field : doc.getFields()) {
+            assertThat((double) field.boost(), closeTo(3.7, 0.01));
+        }
         assertThat(doc.get(docMapper.mappers().name("first").mapper().names().indexName()), equalTo("shay"));
 //        System.out.println("Document: " + doc);
 //        System.out.println("Json: " + docMapper.sourceMapper().value(doc));
@@ -90,7 +95,10 @@ public class SimpleMapperTests {
         BytesReference json = new BytesArray(copyToBytesFromClasspath("/org/elasticsearch/test/unit/index/mapper/simple/test1.json"));
         Document doc = docMapper.parse(json).rootDoc();
         assertThat(doc.get(docMapper.uidMapper().names().indexName()), equalTo(Uid.createUid("person", "1")));
-        assertThat((double) doc.getBoost(), closeTo(3.7, 0.01));
+        assertThat(doc.getFields().size(), equalTo(14));
+        for (IndexableField field : doc.getFields()) {
+            assertThat((double) field.boost(), closeTo(3.7, 0.01));
+        }
         assertThat(doc.get(docMapper.mappers().name("first").mapper().names().indexName()), equalTo("shay"));
 //        System.out.println("Document: " + doc);
 //        System.out.println("Json: " + docMapper.sourceMapper().value(doc));
@@ -103,7 +111,10 @@ public class SimpleMapperTests {
         BytesReference json = new BytesArray(copyToBytesFromClasspath("/org/elasticsearch/test/unit/index/mapper/simple/test1-notype-noid.json"));
         Document doc = docMapper.parse("person", "1", json).rootDoc();
         assertThat(doc.get(docMapper.uidMapper().names().indexName()), equalTo(Uid.createUid("person", "1")));
-        assertThat((double) doc.getBoost(), closeTo(3.7, 0.01));
+        assertThat(doc.getFields().size(), equalTo(14));
+        for (IndexableField field : doc.getFields()) {
+            assertThat((double) field.boost(), closeTo(3.7, 0.01));
+        }
         assertThat(doc.get(docMapper.mappers().name("first").mapper().names().indexName()), equalTo("shay"));
 //        System.out.println("Document: " + doc);
 //        System.out.println("Json: " + docMapper.sourceMapper().value(doc));

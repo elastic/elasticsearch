f8365fead7f933d44a1dd9857542216de5bb04ba
synhershko <itamar@code972.com>
2012-11-07 18:00:54 +0200

    Correcting REST responses

--- src/main/java/org/elasticsearch/rest/action/bulk/RestBulkAction.java
+++ src/main/java/org/elasticsearch/rest/action/bulk/RestBulkAction.java
@@ -30,10 +30,12 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.client.Requests;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentBuilderString;
 import org.elasticsearch.index.percolator.PercolatorExecutor;
 import org.elasticsearch.rest.*;
+import org.elasticsearch.search.highlight.HighlightField;
 
 import java.io.IOException;
 
@@ -122,7 +124,27 @@ public class RestBulkAction extends BaseRestHandler {
                             if (indexResponse.matches() != null) {
                                 builder.startArray(Fields.MATCHES);
                                 for (PercolatorExecutor.PercolationMatch match : indexResponse.matches()) {
-                                    builder.value(match); // TODO
+                                    builder.startObject();
+                                    builder.field("match", match.getMatch());
+
+                                    if (match.getHighlightFields() != null && !match.getHighlightFields().isEmpty()) {
+                                        builder.startObject("highlight");
+                                        for (HighlightField field : match.getHighlightFields().values()) {
+                                            builder.field(field.name());
+                                            if (field.fragments() == null) {
+                                                builder.nullValue();
+                                            } else {
+                                                builder.startArray();
+                                                for (Text fragment : field.fragments()) {
+                                                    builder.value(fragment);
+                                                }
+                                                builder.endArray();
+                                            }
+                                        }
+                                        builder.endObject();
+                                    }
+
+                                    builder.endObject();
                                 }
                                 builder.endArray();
                             }
--- src/main/java/org/elasticsearch/rest/action/update/RestUpdateAction.java
+++ src/main/java/org/elasticsearch/rest/action/update/RestUpdateAction.java
@@ -29,6 +29,7 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentBuilderString;
 import org.elasticsearch.index.VersionType;
@@ -36,6 +37,7 @@ import org.elasticsearch.index.percolator.PercolatorExecutor;
 import org.elasticsearch.rest.*;
 import org.elasticsearch.rest.action.support.RestActions;
 import org.elasticsearch.rest.action.support.RestXContentBuilder;
+import org.elasticsearch.search.highlight.HighlightField;
 
 import java.io.IOException;
 import java.util.Map;
@@ -144,7 +146,27 @@ public class RestUpdateAction extends BaseRestHandler {
                     if (response.matches() != null) {
                         builder.startArray(Fields.MATCHES);
                         for (PercolatorExecutor.PercolationMatch match : response.matches()) {
-                            builder.value(match); // TODO
+                            builder.startObject();
+                            builder.field("match", match.getMatch());
+
+                            if (match.getHighlightFields() != null && !match.getHighlightFields().isEmpty()) {
+                                builder.startObject("highlight");
+                                for (HighlightField field : match.getHighlightFields().values()) {
+                                    builder.field(field.name());
+                                    if (field.fragments() == null) {
+                                        builder.nullValue();
+                                    } else {
+                                        builder.startArray();
+                                        for (Text fragment : field.fragments()) {
+                                            builder.value(fragment);
+                                        }
+                                        builder.endArray();
+                                    }
+                                }
+                                builder.endObject();
+                            }
+
+                            builder.endObject();
                         }
                         builder.endArray();
                     }

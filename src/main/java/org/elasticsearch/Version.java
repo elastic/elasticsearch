9c0ae8b3bab310d20f71acf7d295709ab7f492d4
Shay Banon <kimchy@gmail.com>
2012-12-07 23:09:39 +0100

    release 0.20.0

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -95,10 +95,10 @@ public class Version implements Serializable {
     public static final int V_0_20_0_RC1_ID = /*00*/200051;
     public static final Version V_0_20_0_RC1 = new Version(V_0_20_0_RC1_ID, false);
 
-    public static final int V_0_20_0_RC2_ID = /*00*/200052;
-    public static final Version V_0_20_0_RC2 = new Version(V_0_20_0_RC2_ID, true);
+    public static final int V_0_20_0_ID = /*00*/200099;
+    public static final Version V_0_20_0 = new Version(V_0_20_0_ID, false);
 
-    public static final Version CURRENT = V_0_20_0_RC2;
+    public static final Version CURRENT = V_0_20_0;
 
     public static Version readVersion(StreamInput in) throws IOException {
         return fromId(in.readVInt());
@@ -106,8 +106,8 @@ public class Version implements Serializable {
 
     public static Version fromId(int id) {
         switch (id) {
-            case V_0_20_0_RC2_ID:
-                return V_0_20_0_RC2;
+            case V_0_20_0_ID:
+                return V_0_20_0;
             case V_0_20_0_RC1_ID:
                 return V_0_20_0_RC1;
 

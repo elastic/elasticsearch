0b55fd1fcb4808fa59e957da559944ecabc37b7c
Igor Motov <igor@motovs.org>
2013-01-31 09:10:01 -0500

    Improve RecoverAfterNodes tests

--- src/test/java/org/elasticsearch/test/integration/gateway/none/RecoverAfterNodesTests.java
+++ src/test/java/org/elasticsearch/test/integration/gateway/none/RecoverAfterNodesTests.java
@@ -20,6 +20,7 @@
 package org.elasticsearch.test.integration.gateway.none;
 
 import org.elasticsearch.cluster.block.ClusterBlockLevel;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.gateway.GatewayService;
 import org.elasticsearch.test.integration.AbstractNodesTests;
 import org.testng.annotations.AfterMethod;
@@ -35,13 +36,15 @@ import static org.hamcrest.Matchers.hasItem;
  */
 public class RecoverAfterNodesTests extends AbstractNodesTests {
 
+    private final static TimeValue BLOCK_WAIT_TIMEOUT = TimeValue.timeValueSeconds(1);
+
     @AfterMethod
     public void closeNodes() {
         closeAllNodes();
     }
 
     @Test
-    public void testRecoverAfterNodes() {
+    public void testRecoverAfterNodes() throws Exception {
         logger.info("--> start node (1)");
         startNode("node1", settingsBuilder().put("gateway.recover_after_nodes", 3));
         assertThat(client("node1").admin().cluster().prepareState().setLocal(true).execute().actionGet()
@@ -50,6 +53,8 @@ public class RecoverAfterNodesTests extends AbstractNodesTests {
 
         logger.info("--> start node (2)");
         startNode("node2", settingsBuilder().put("gateway.recover_after_nodes", 3));
+        // Sleeping here for the same time that we wait to check for empty blocks
+        Thread.sleep(BLOCK_WAIT_TIMEOUT.millis());
         assertThat(client("node1").admin().cluster().prepareState().setLocal(true).execute().actionGet()
                 .state().blocks().global(ClusterBlockLevel.METADATA),
                 hasItem(GatewayService.STATE_NOT_RECOVERED_BLOCK));
@@ -60,15 +65,9 @@ public class RecoverAfterNodesTests extends AbstractNodesTests {
         logger.info("--> start node (3)");
         startNode("node3", settingsBuilder().put("gateway.recover_after_nodes", 3));
 
-        assertThat(client("node1").admin().cluster().prepareState().setLocal(true).execute().actionGet()
-                .state().blocks().global(ClusterBlockLevel.METADATA).isEmpty(),
-                equalTo(true));
-        assertThat(client("node2").admin().cluster().prepareState().setLocal(true).execute().actionGet()
-                .state().blocks().global(ClusterBlockLevel.METADATA).isEmpty(),
-                equalTo(true));
-        assertThat(client("node3").admin().cluster().prepareState().setLocal(true).execute().actionGet()
-                .state().blocks().global(ClusterBlockLevel.METADATA).isEmpty(),
-                equalTo(true));
+        assertThat(waitForNoBlocks(BLOCK_WAIT_TIMEOUT, "node1").isEmpty(), equalTo(true));
+        assertThat(waitForNoBlocks(BLOCK_WAIT_TIMEOUT, "node2").isEmpty(), equalTo(true));
+        assertThat(waitForNoBlocks(BLOCK_WAIT_TIMEOUT, "node3").isEmpty(), equalTo(true));
     }
 
     @Test
@@ -102,23 +101,14 @@ public class RecoverAfterNodesTests extends AbstractNodesTests {
 
         logger.info("--> start master_node (2)");
         startNode("master2", settingsBuilder().put("gateway.recover_after_master_nodes", 2).put("node.data", false).put("node.master", true));
-        Thread.sleep(300);
-        assertThat(client("master1").admin().cluster().prepareState().setLocal(true).execute().actionGet()
-                .state().blocks().global(ClusterBlockLevel.METADATA).isEmpty(),
-                equalTo(true));
-        assertThat(client("master2").admin().cluster().prepareState().setLocal(true).execute().actionGet()
-                .state().blocks().global(ClusterBlockLevel.METADATA).isEmpty(),
-                equalTo(true));
-        assertThat(client("data1").admin().cluster().prepareState().setLocal(true).execute().actionGet()
-                .state().blocks().global(ClusterBlockLevel.METADATA).isEmpty(),
-                equalTo(true));
-        assertThat(client("data2").admin().cluster().prepareState().setLocal(true).execute().actionGet()
-                .state().blocks().global(ClusterBlockLevel.METADATA).isEmpty(),
-                equalTo(true));
+        assertThat(waitForNoBlocks(BLOCK_WAIT_TIMEOUT, "master1").isEmpty(), equalTo(true));
+        assertThat(waitForNoBlocks(BLOCK_WAIT_TIMEOUT, "master2").isEmpty(), equalTo(true));
+        assertThat(waitForNoBlocks(BLOCK_WAIT_TIMEOUT, "data1").isEmpty(), equalTo(true));
+        assertThat(waitForNoBlocks(BLOCK_WAIT_TIMEOUT, "data2").isEmpty(), equalTo(true));
     }
 
     @Test
-    public void testRecoverAfterDataNodes() {
+    public void testRecoverAfterDataNodes() throws Exception {
         logger.info("--> start master_node (1)");
         startNode("master1", settingsBuilder().put("gateway.recover_after_data_nodes", 2).put("node.data", false).put("node.master", true));
         assertThat(client("master1").admin().cluster().prepareState().setLocal(true).execute().actionGet()
@@ -148,17 +138,9 @@ public class RecoverAfterNodesTests extends AbstractNodesTests {
 
         logger.info("--> start data_node (2)");
         startNode("data2", settingsBuilder().put("gateway.recover_after_data_nodes", 2).put("node.data", true).put("node.master", false));
-        assertThat(client("master1").admin().cluster().prepareState().setLocal(true).execute().actionGet()
-                .state().blocks().global(ClusterBlockLevel.METADATA).isEmpty(),
-                equalTo(true));
-        assertThat(client("master2").admin().cluster().prepareState().setLocal(true).execute().actionGet()
-                .state().blocks().global(ClusterBlockLevel.METADATA).isEmpty(),
-                equalTo(true));
-        assertThat(client("data1").admin().cluster().prepareState().setLocal(true).execute().actionGet()
-                .state().blocks().global(ClusterBlockLevel.METADATA).isEmpty(),
-                equalTo(true));
-        assertThat(client("data2").admin().cluster().prepareState().setLocal(true).execute().actionGet()
-                .state().blocks().global(ClusterBlockLevel.METADATA).isEmpty(),
-                equalTo(true));
+        assertThat(waitForNoBlocks(BLOCK_WAIT_TIMEOUT, "master1").isEmpty(), equalTo(true));
+        assertThat(waitForNoBlocks(BLOCK_WAIT_TIMEOUT, "master2").isEmpty(), equalTo(true));
+        assertThat(waitForNoBlocks(BLOCK_WAIT_TIMEOUT, "data1").isEmpty(), equalTo(true));
+        assertThat(waitForNoBlocks(BLOCK_WAIT_TIMEOUT, "data2").isEmpty(), equalTo(true));
     }
 }

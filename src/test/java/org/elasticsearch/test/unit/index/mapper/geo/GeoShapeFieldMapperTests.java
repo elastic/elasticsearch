8db436f1071ae15586cfc3e348059e2e6708bdec
Simon Willnauer <simonw@apache.org>
2013-02-15 20:54:19 +0100

    Remove backported Lucene 4 spatial code in favor of the released version in Lucene 4.1

--- src/test/java/org/elasticsearch/test/unit/index/mapper/geo/GeoShapeFieldMapperTests.java
+++ src/test/java/org/elasticsearch/test/unit/index/mapper/geo/GeoShapeFieldMapperTests.java
@@ -1,20 +1,21 @@
 package org.elasticsearch.test.unit.index.mapper.geo;
 
-import org.elasticsearch.common.lucene.spatial.SpatialStrategy;
-import org.elasticsearch.common.lucene.spatial.prefix.tree.GeohashPrefixTree;
-import org.elasticsearch.common.lucene.spatial.prefix.tree.QuadPrefixTree;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+
+import java.io.IOException;
+
+import org.apache.lucene.spatial.SpatialStrategy;
+import org.apache.lucene.spatial.prefix.PrefixTreeStrategy;
+import org.apache.lucene.spatial.prefix.tree.GeohashPrefixTree;
+import org.apache.lucene.spatial.prefix.tree.QuadPrefixTree;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.index.mapper.DocumentMapper;
 import org.elasticsearch.index.mapper.FieldMapper;
 import org.elasticsearch.index.mapper.geo.GeoShapeFieldMapper;
 import org.elasticsearch.test.unit.index.mapper.MapperTests;
 import org.testng.annotations.Test;
-
-import java.io.IOException;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.instanceOf;
 
 public class GeoShapeFieldMapperTests {
 
@@ -31,11 +32,11 @@ public class GeoShapeFieldMapperTests {
         assertThat(fieldMapper, instanceOf(GeoShapeFieldMapper.class));
 
         GeoShapeFieldMapper geoShapeFieldMapper = (GeoShapeFieldMapper) fieldMapper;
-        SpatialStrategy strategy = geoShapeFieldMapper.spatialStrategy();
+        PrefixTreeStrategy strategy = geoShapeFieldMapper.spatialStrategy();
 
-        assertThat(strategy.getDistanceErrorPct(), equalTo(GeoShapeFieldMapper.Defaults.DISTANCE_ERROR_PCT));
-        assertThat(strategy.getPrefixTree(), instanceOf(GeohashPrefixTree.class));
-        assertThat(strategy.getPrefixTree().getMaxLevels(), equalTo(GeoShapeFieldMapper.Defaults.GEOHASH_LEVELS));
+        assertThat(strategy.getDistErrPct(), equalTo(GeoShapeFieldMapper.Defaults.DISTANCE_ERROR_PCT));
+        assertThat(strategy.getGrid(), instanceOf(GeohashPrefixTree.class));
+        assertThat(strategy.getGrid().getMaxLevels(), equalTo(GeoShapeFieldMapper.Defaults.GEOHASH_LEVELS));
     }
 
     @Test
@@ -54,11 +55,11 @@ public class GeoShapeFieldMapperTests {
         assertThat(fieldMapper, instanceOf(GeoShapeFieldMapper.class));
 
         GeoShapeFieldMapper geoShapeFieldMapper = (GeoShapeFieldMapper) fieldMapper;
-        SpatialStrategy strategy = geoShapeFieldMapper.spatialStrategy();
+        PrefixTreeStrategy strategy = geoShapeFieldMapper.spatialStrategy();
 
-        assertThat(strategy.getDistanceErrorPct(), equalTo(0.1));
-        assertThat(strategy.getPrefixTree(), instanceOf(GeohashPrefixTree.class));
-        assertThat(strategy.getPrefixTree().getMaxLevels(), equalTo(4));
+        assertThat(strategy.getDistErrPct(), equalTo(0.1));
+        assertThat(strategy.getGrid(), instanceOf(GeohashPrefixTree.class));
+        assertThat(strategy.getGrid().getMaxLevels(), equalTo(4));
     }
 
     @Test
@@ -77,10 +78,10 @@ public class GeoShapeFieldMapperTests {
         assertThat(fieldMapper, instanceOf(GeoShapeFieldMapper.class));
 
         GeoShapeFieldMapper geoShapeFieldMapper = (GeoShapeFieldMapper) fieldMapper;
-        SpatialStrategy strategy = geoShapeFieldMapper.spatialStrategy();
+        PrefixTreeStrategy strategy = geoShapeFieldMapper.spatialStrategy();
 
-        assertThat(strategy.getDistanceErrorPct(), equalTo(0.5));
-        assertThat(strategy.getPrefixTree(), instanceOf(QuadPrefixTree.class));
-        assertThat(strategy.getPrefixTree().getMaxLevels(), equalTo(6));
+        assertThat(strategy.getDistErrPct(), equalTo(0.5));
+        assertThat(strategy.getGrid(), instanceOf(QuadPrefixTree.class));
+        assertThat(strategy.getGrid().getMaxLevels(), equalTo(6));
     }
 }

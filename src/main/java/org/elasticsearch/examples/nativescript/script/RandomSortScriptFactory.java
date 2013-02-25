25b15650e34d149b3915b7a7808ff97c9c88a65a
Igor Motov <igor@motovs.org>
2013-02-24 22:58:42 -0500

    Add random sort order native script

---
+++ src/main/java/org/elasticsearch/examples/nativescript/script/RandomSortScriptFactory.java
@@ -0,0 +1,76 @@
+package org.elasticsearch.examples.nativescript.script;
+
+import org.elasticsearch.ElasticSearchIllegalArgumentException;
+import org.elasticsearch.common.Nullable;
+import org.elasticsearch.common.xcontent.support.XContentMapValues;
+import org.elasticsearch.index.field.data.DocFieldData;
+import org.elasticsearch.index.mapper.internal.UidFieldMapper;
+import org.elasticsearch.script.AbstractFloatSearchScript;
+import org.elasticsearch.script.AbstractLongSearchScript;
+import org.elasticsearch.script.ExecutableScript;
+import org.elasticsearch.script.NativeScriptFactory;
+import sun.security.provider.MD5;
+
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * This script demonstrates how native scripts can be used to create custom sort order.
+ * Since sort operation is expecting float parameter, the {@link AbstractFloatSearchScript} can be used.
+ *
+ * The script accepts one optional parameter salt. If parameter is specified, a pseudo random sort order is used.
+ * Otherwise, a random sort order is used.
+ */
+public class RandomSortScriptFactory implements NativeScriptFactory {
+
+    /**
+     * This method is called for every search on every shard.
+     *
+     * @param params list of script parameters passed with the query
+     * @return new native script
+     */
+    @Override
+    public ExecutableScript newScript(@Nullable Map<String, Object> params) {
+        String salt = params == null ? null : XContentMapValues.nodeStringValue(params.get("salt"), null);
+        if (salt == null) {
+            return new RandomSortScript();
+        } else {
+            return new PseudoRandomSortScript(salt);
+        }
+    }
+
+    private static class RandomSortScript extends AbstractLongSearchScript {
+        private final Random random;
+
+        private RandomSortScript() {
+            random = new Random();
+        }
+
+        @Override
+        public long runAsLong() {
+            return random.nextLong();
+        }
+    }
+
+    private static class PseudoRandomSortScript extends AbstractLongSearchScript {
+        private final String salt;
+
+        private PseudoRandomSortScript(String salt) {
+            this.salt = salt;
+        }
+
+        @Override
+        public long runAsLong() {
+            DocFieldData fieldData = doc().field(UidFieldMapper.NAME);
+            byte[] sort = org.elasticsearch.common.Digest.md5(fieldData.stringValue() + salt);
+            return (sort[0] & 0xFFL) << 56
+                    | (sort[1] & 0xFFL) << 48
+                    | (sort[2] & 0xFFL) << 40
+                    | (sort[3] & 0xFFL) << 32
+                    | (sort[4] & 0xFFL) << 24
+                    | (sort[5] & 0xFFL) << 16
+                    | (sort[6] & 0xFFL) << 8
+                    | (sort[7] & 0xFFL);
+        }
+    }
+}
---
+++ src/test/java/org/elasticsearch/examples/nativescript/script/RandomSortScriptTests.java
@@ -0,0 +1,103 @@
+package org.elasticsearch.examples.nativescript.script;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.common.collect.MapBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.indices.IndexMissingException;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+
+/**
+ */
+public class RandomSortScriptTests extends AbstractSearchScriptTests {
+    @Test
+    public void testPseudoRandomScript() throws Exception {
+        // Delete the old index
+        try {
+            node.client().admin().indices().prepareDelete("test").execute().actionGet();
+        } catch (IndexMissingException ex) {
+            // Ignore
+        }
+
+        // Create a new index
+        String mapping = XContentFactory.jsonBuilder().startObject().startObject("type")
+                .startObject("properties")
+                .startObject("name").field("type", "string").endObject()
+                .endObject().endObject().endObject()
+                .string();
+        node.client().admin().indices().prepareCreate("test")
+                .addMapping("type", mapping)
+                .execute().actionGet();
+
+        // Index 100 records (0..99)
+        for (int i = 0; i < 100; i++) {
+            node.client().prepareIndex("test", "type", Integer.toString(i))
+                    .setSource(XContentFactory.jsonBuilder().startObject()
+                            .field("name", "rec " + i)
+                            .endObject())
+                    .execute().actionGet();
+        }
+        node.client().admin().indices().prepareRefresh("test").execute().actionGet();
+
+
+        // Retrieve first 10 records
+        SearchResponse searchResponse = node.client().prepareSearch("test")
+                .setQuery(matchAllQuery())
+                .addField("name")
+                .setSize(10)
+                .addSort(SortBuilders.scriptSort("random", "number").lang("native").setParams(MapBuilder.<String, Object>newMapBuilder().put("salt", "1234").map()))
+                .execute().actionGet();
+        assertThat(Arrays.toString(searchResponse.shardFailures()), searchResponse.failedShards(), equalTo(0));
+
+        // Check that random order was applied
+        assertThat(searchResponse.hits().getAt(0).field("name").getValue().toString(), not(equalTo("rec0")));
+
+        String[] records = new String[10];
+
+        // Store sort order
+        for (int i = 0; i < 10; i++) {
+            records[i] = searchResponse.hits().getAt(i).field("name").getValue().toString();
+        }
+
+        // Retrieve first 10 records again
+        searchResponse = node.client().prepareSearch("test")
+                .setQuery(matchAllQuery())
+                .addField("name")
+                .setSize(10)
+                .addSort(SortBuilders.scriptSort("random", "number").lang("native").setParams(MapBuilder.<String, Object>newMapBuilder().put("salt", "1234").map()))
+                .execute().actionGet();
+        assertThat(Arrays.toString(searchResponse.shardFailures()), searchResponse.failedShards(), equalTo(0));
+
+        // Verify the same sort order
+        for (int i = 0; i < 10; i++) {
+            assertThat(searchResponse.hits().getAt(i).field("name").getValue().toString(), equalTo(records[i]));
+        }
+
+        // Retrieve first 10 records without salt
+        searchResponse = node.client().prepareSearch("test")
+                .setQuery(matchAllQuery())
+                .addField("name")
+                .setSize(10)
+                .addSort(SortBuilders.scriptSort("random", "number").lang("native"))
+                .execute().actionGet();
+        assertThat(Arrays.toString(searchResponse.shardFailures()), searchResponse.failedShards(), equalTo(0));
+
+        // Verify different sort order
+        boolean different = false;
+        for (int i = 0; i < 10; i++) {
+            if (!records[i].equals(searchResponse.hits().getAt(i).field("name").getValue().toString())) {
+                different = true;
+                break;
+            }
+        }
+        assertThat(different, equalTo(true));
+
+    }
+}

355f80adc92cc69ed35cbc4936f0f8334ee55307
Simon Willnauer <simonw@apache.org>
2013-04-26 19:16:44 +0200

    Added temporary fix for LUCENE-4899 where FastVectorHighlihgter failed with StringIndexOutOfBoundsException if a single highlight phrase or term was greater than the fragCharSize producing negative string offsets
    The fixed BaseFragListBuilder was added as XSimpleFragListBuilder which triggers an assert once Elasticsearch
    upgrades to Lucene 4.3

--- src/test/java/org/elasticsearch/test/integration/search/highlight/HighlighterSearchTests.java
+++ src/test/java/org/elasticsearch/test/integration/search/highlight/HighlighterSearchTests.java
@@ -19,6 +19,12 @@
 
 package org.elasticsearch.test.integration.search.highlight;
 
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.PhraseQuery;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.BooleanClause.Occur;
+import org.apache.lucene.search.vectorhighlight.FieldQuery;
 import org.elasticsearch.ElasticSearchException;
 import org.elasticsearch.action.search.SearchPhaseExecutionException;
 import org.elasticsearch.action.search.SearchResponse;
@@ -31,6 +37,7 @@ import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.MatchQueryBuilder.Type;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.indices.IndexMissingException;
 import org.elasticsearch.rest.RestStatus;
@@ -81,6 +88,69 @@ public class HighlighterSearchTests extends AbstractNodesTests {
         return client("server1");
     }
 
+    
+    @Test
+    public void testEnsureNoNegativeOffsets() throws Exception {
+        try {
+            client.admin().indices().prepareDelete("test").execute().actionGet();
+        } catch (Exception e) {
+            // ignore
+        }
+
+        client.admin().indices().prepareCreate("test").setSettings(ImmutableSettings.settingsBuilder().put("index.number_of_shards", 2))
+                .addMapping("type1", jsonBuilder().startObject().startObject("type1").startObject("properties")
+                        // we don't store title, now lets see if it works...
+                        .startObject("no_long_term").field("type", "string").field("store", "no").field("term_vector", "with_positions_offsets").endObject()
+                        .startObject("long_term").field("type", "string").field("store", "no").field("term_vector", "with_positions_offsets").endObject()
+                        .endObject().endObject().endObject())
+                .execute().actionGet();
+
+
+        client.prepareIndex("test", "type1", "1")
+                .setSource(XContentFactory.jsonBuilder().startObject()
+                        .startArray("no_long_term")
+                        .value("This is a test where foo is highlighed and should be highlighted")
+                        .endArray()
+                        .startArray("long_term")
+                        .value("This is a test thisisaverylongwordandmakessurethisfails where foo is highlighed and should be highlighted")
+                        .endArray()
+                        .endObject())
+                .setRefresh(true).execute().actionGet();
+
+
+        SearchResponse search = client.prepareSearch()
+                .setQuery(matchQuery("long_term", "thisisaverylongwordandmakessurethisfails foo highlighed"))
+                .addHighlightedField("long_term", 18, 1)
+                .execute().actionGet();
+
+        assertThat(search.getHits().totalHits(), equalTo(1l));
+        assertThat(search.getHits().hits().length, equalTo(1));
+
+        assertThat(search.getHits().hits()[0].highlightFields().get("long_term").fragments().length, equalTo(1));
+        assertThat(search.getHits().hits()[0].highlightFields().get("long_term").fragments()[0].string(), equalTo("<em>thisisaverylongwordandmakessurethisfails</em>"));
+        
+        
+        search = client.prepareSearch()
+                .setQuery(matchQuery("no_long_term", "test foo highlighed").type(Type.PHRASE).slop(3))
+                .addHighlightedField("no_long_term", 18, 1).setHighlighterPostTags("</b>").setHighlighterPreTags("<b>")
+                .execute().actionGet();
+        assertThat(search.getHits().totalHits(), equalTo(1l));
+        assertThat(search.getHits().hits().length, equalTo(1));
+        assertThat(search.getHits().hits()[0].highlightFields().size(), equalTo(0));
+       
+
+        search = client.prepareSearch()
+                .setQuery(matchQuery("no_long_term", "test foo highlighed").type(Type.PHRASE).slop(3))
+                .addHighlightedField("no_long_term", 30, 1).setHighlighterPostTags("</b>").setHighlighterPreTags("<b>")
+                .execute().actionGet();
+        
+        assertThat(search.getHits().totalHits(), equalTo(1l));
+        assertThat(search.getHits().hits().length, equalTo(1));
+
+        assertThat(search.getHits().hits()[0].highlightFields().get("no_long_term").fragments().length, equalTo(1));
+        assertThat(search.getHits().hits()[0].highlightFields().get("no_long_term").fragments()[0].string(), equalTo("a <b>test</b> where <b>foo</b> is <b>highlighed</b> and"));
+    }
+    
     @Test
     public void testSourceLookupHighlightingUsingPlainHighlighter() throws Exception {
         try {

f5665275133eac1d6071cbf2391796cb0527358d
Shay Banon <kimchy@gmail.com>
2013-05-06 12:55:32 +0200

    Rest Get Source Allow to get the source directly using a specific REST endpoint without any additional content around it, the endpoint is `{index}/{type}/{id}/_source`. Note, HEAD now also support the _source endpoint. closes #2993, closes #2995

--- src/main/java/org/elasticsearch/rest/action/get/RestGetSourceAction.java
+++ src/main/java/org/elasticsearch/rest/action/get/RestGetSourceAction.java
@@ -19,30 +19,32 @@
 
 package org.elasticsearch.rest.action.get;
 
-import org.elasticsearch.ExceptionsHelper;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.rest.*;
+import org.elasticsearch.rest.action.support.RestXContentBuilder;
 
-import static org.elasticsearch.rest.RestRequest.Method.HEAD;
+import java.io.IOException;
+
+import static org.elasticsearch.rest.RestRequest.Method.GET;
 import static org.elasticsearch.rest.RestStatus.NOT_FOUND;
 import static org.elasticsearch.rest.RestStatus.OK;
+import static org.elasticsearch.rest.action.support.RestXContentBuilder.restContentBuilder;
 
 /**
  *
  */
-public class RestHeadAction extends BaseRestHandler {
+public class RestGetSourceAction extends BaseRestHandler {
 
     @Inject
-    public RestHeadAction(Settings settings, Client client, RestController controller) {
+    public RestGetSourceAction(Settings settings, Client client, RestController controller) {
         super(settings, client);
-        controller.registerHandler(HEAD, "/{index}/{type}/{id}", this);
-        controller.registerHandler(HEAD, "/{index}/{type}/{id}/_source", this);
+        controller.registerHandler(GET, "/{index}/{type}/{id}/_source", this);
     }
 
     @Override
@@ -51,22 +53,22 @@ public class RestHeadAction extends BaseRestHandler {
         getRequest.listenerThreaded(false);
         getRequest.operationThreaded(true);
         getRequest.refresh(request.paramAsBoolean("refresh", getRequest.refresh()));
+        getRequest.routing(request.param("routing"));  // order is important, set it after routing, so it will set the routing
         getRequest.parent(request.param("parent"));
-        getRequest.routing(request.param("routing"));
         getRequest.preference(request.param("preference"));
         getRequest.realtime(request.paramAsBooleanOptional("realtime", null));
-        // don't get any fields back...
-        getRequest.fields(Strings.EMPTY_ARRAY);
-        // TODO we can also just return the document size as Content-Length
 
         client.get(getRequest, new ActionListener<GetResponse>() {
             @Override
             public void onResponse(GetResponse response) {
+
                 try {
+                    XContentBuilder builder = restContentBuilder(request, response.getSourceInternal());
                     if (!response.isExists()) {
-                        channel.sendResponse(new StringRestResponse(NOT_FOUND));
+                        channel.sendResponse(new XContentRestResponse(request, NOT_FOUND, builder));
                     } else {
-                        channel.sendResponse(new StringRestResponse(OK));
+                        RestXContentBuilder.directSource(response.getSourceInternal(), builder, request);
+                        channel.sendResponse(new XContentRestResponse(request, OK, builder));
                     }
                 } catch (Throwable e) {
                     onFailure(e);
@@ -76,8 +78,8 @@ public class RestHeadAction extends BaseRestHandler {
             @Override
             public void onFailure(Throwable e) {
                 try {
-                    channel.sendResponse(new StringRestResponse(ExceptionsHelper.status(e)));
-                } catch (Exception e1) {
+                    channel.sendResponse(new XContentThrowableRestResponse(request, e));
+                } catch (IOException e1) {
                     logger.error("Failed to send failure response", e1);
                 }
             }

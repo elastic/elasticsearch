3394b7960630d1a02b232e82389042d6f23082f1
Igor Motov <igor@motovs.org>
2013-05-08 18:21:47 -0400

    Fix DfsSearchResult method names in AggregatedDfs

--- src/main/java/org/elasticsearch/search/dfs/AggregatedDfs.java
+++ src/main/java/org/elasticsearch/search/dfs/AggregatedDfs.java
@@ -72,7 +72,7 @@ public class AggregatedDfs implements Streamable {
             Term term = new Term(in.readString(), in.readBytesRef());
             TermStatistics stats = new TermStatistics(in.readBytesRef(), 
                     in.readVLong(), 
-                    DfsSearchResult.toNotAvailable(in.readVLong()));
+                    DfsSearchResult.subOne(in.readVLong()));
             termStatistics.put(term, stats);
         }
         fieldStatistics = DfsSearchResult.readFieldStats(in);
@@ -89,7 +89,7 @@ public class AggregatedDfs implements Streamable {
             TermStatistics stats = termTermStatisticsEntry.getValue();
             out.writeBytesRef(stats.term());
             out.writeVLong(stats.docFreq());
-            out.writeVLong(DfsSearchResult.plusOne(stats.totalTermFreq()));
+            out.writeVLong(DfsSearchResult.addOne(stats.totalTermFreq()));
         }
         DfsSearchResult.writeFieldStats(out, fieldStatistics);
         out.writeVLong(maxDoc);

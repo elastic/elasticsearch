27b27036d206137ffe1267a03c5414cf5c56f0f0
Simon Willnauer <simonw@apache.org>
2013-05-14 15:08:14 +0200

    Use Recovery Throtteling by default.
    To prevent to extensive resource use during recovery we use
    recovery throtteling by default to prevent unexpected peak load
    on clusters. The default is set to 20 MB/sec.
    
    Closes #3035

--- src/main/java/org/elasticsearch/cluster/settings/ClusterDynamicSettingsModule.java
+++ src/main/java/org/elasticsearch/cluster/settings/ClusterDynamicSettingsModule.java
@@ -63,6 +63,7 @@ public class ClusterDynamicSettingsModule extends AbstractModule {
         clusterDynamicSettings.addDynamicSetting(RecoverySettings.INDICES_RECOVERY_TRANSLOG_SIZE, Validator.BYTES_SIZE);
         clusterDynamicSettings.addDynamicSetting(RecoverySettings.INDICES_RECOVERY_COMPRESS);
         clusterDynamicSettings.addDynamicSetting(RecoverySettings.INDICES_RECOVERY_CONCURRENT_STREAMS, Validator.POSITIVE_INTEGER);
+        clusterDynamicSettings.addDynamicSetting(RecoverySettings.INDICES_RECOVERY_MAX_BYTES_PER_SEC, Validator.BYTES_SIZE);
         clusterDynamicSettings.addDynamicSetting(RecoverySettings.INDICES_RECOVERY_MAX_SIZE_PER_SEC, Validator.BYTES_SIZE);
         clusterDynamicSettings.addDynamicSetting(ThreadPool.THREADPOOL_GROUP + "*");
         clusterDynamicSettings.addDynamicSetting(ThrottlingAllocationDecider.CLUSTER_ROUTING_ALLOCATION_NODE_INITIAL_PRIMARIES_RECOVERIES, Validator.INTEGER);

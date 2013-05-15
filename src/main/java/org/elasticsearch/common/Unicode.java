f92eed8591ade784d19740735ee069b8c8305292
Shay Banon <kimchy@gmail.com>
2013-05-15 12:13:13 +0200

    clean thread locals without needing a wrapper clean thread locals smartly by identifying "our" classes, and removing them, so there is no need to wrap it in our our clenable values

--- src/main/java/org/elasticsearch/common/Unicode.java
+++ src/main/java/org/elasticsearch/common/Unicode.java
@@ -21,7 +21,6 @@ package org.elasticsearch.common;
 
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.UnicodeUtil;
-import org.elasticsearch.common.util.concurrent.ThreadLocals;
 
 import java.util.Arrays;
 
@@ -30,17 +29,17 @@ import java.util.Arrays;
  */
 public class Unicode {
 
-    private static ThreadLocal<ThreadLocals.CleanableValue<BytesRef>> cachedUtf8Result = new ThreadLocal<ThreadLocals.CleanableValue<BytesRef>>() {
+    private static ThreadLocal<BytesRef> cachedUtf8Result = new ThreadLocal<BytesRef>() {
         @Override
-        protected ThreadLocals.CleanableValue<BytesRef> initialValue() {
-            return new ThreadLocals.CleanableValue<BytesRef>(new BytesRef());
+        protected BytesRef initialValue() {
+            return new BytesRef();
         }
     };
 
-    private static ThreadLocal<ThreadLocals.CleanableValue<UTF16Result>> cachedUtf16Result = new ThreadLocal<ThreadLocals.CleanableValue<UTF16Result>>() {
+    private static ThreadLocal<UTF16Result> cachedUtf16Result = new ThreadLocal<UTF16Result>() {
         @Override
-        protected ThreadLocals.CleanableValue<UTF16Result> initialValue() {
-            return new ThreadLocals.CleanableValue<UTF16Result>(new UTF16Result());
+        protected UTF16Result initialValue() {
+            return new UTF16Result();
         }
     };
 
@@ -73,7 +72,7 @@ public class Unicode {
         if (source == null) {
             return null;
         }
-        BytesRef result = cachedUtf8Result.get().get();
+        BytesRef result = cachedUtf8Result.get();
         UnicodeUtil.UTF16toUTF8(source, 0, source.length(), result);
         return result;
     }
@@ -111,7 +110,7 @@ public class Unicode {
         if (source == null) {
             return null;
         }
-        UTF16Result result = cachedUtf16Result.get().get();
+        UTF16Result result = cachedUtf16Result.get();
         UTF8toUTF16(source, offset, length, result);
         return result;
     }

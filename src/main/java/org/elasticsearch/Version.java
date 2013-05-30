d24a96f07fbe9e27a3f22f8feca89cc5d0bd1f05
Shay Banon <kimchy@gmail.com>
2013-05-30 14:11:04 +0200

    release 0.90.1

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -121,12 +121,12 @@ public class Version implements Serializable {
     public static final int V_0_90_0_ID = /*00*/900099;
     public static final Version V_0_90_0 = new Version(V_0_90_0_ID, false, org.apache.lucene.util.Version.LUCENE_42);
     public static final int V_0_90_1_ID = /*00*/900199;
-    public static final Version V_0_90_1 = new Version(V_0_90_1_ID, true, org.apache.lucene.util.Version.LUCENE_43);
+    public static final Version V_0_90_1 = new Version(V_0_90_1_ID, false, org.apache.lucene.util.Version.LUCENE_43);
 
     public static final Version CURRENT = V_0_90_1;
-    
+
     static {
-        assert CURRENT.luceneVersion == Lucene.VERSION: "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
+        assert CURRENT.luceneVersion == Lucene.VERSION : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
     }
 
     public static Version readVersion(StreamInput in) throws IOException {

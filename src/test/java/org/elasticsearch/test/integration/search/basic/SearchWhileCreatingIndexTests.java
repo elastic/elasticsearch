bbe391ab978968fe6d0c6152dd600ff2469b0a42
Shay Banon <kimchy@gmail.com>
2013-07-28 15:27:15 +0200

    improve test the test checks that even though some shards might not be active, search will still work

--- src/test/java/org/elasticsearch/test/integration/search/basic/SearchWhileCreatingIndexTests.java
+++ src/test/java/org/elasticsearch/test/integration/search/basic/SearchWhileCreatingIndexTests.java
@@ -19,21 +19,24 @@
 
 package org.elasticsearch.test.integration.search.basic;
 
-import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilder;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
-import java.util.Arrays;
-
+import org.elasticsearch.action.admin.indices.refresh.RefreshResponse;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.test.integration.AbstractSharedClusterTest;
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 
 public class SearchWhileCreatingIndexTests extends AbstractSharedClusterTest {
 
+    protected int numberOfNodes() {
+        return 1;
+    }
 
     /**
      * This test basically verifies that search with a single shard active (cause we indexed to it) and other
@@ -42,11 +45,12 @@ public class SearchWhileCreatingIndexTests extends AbstractSharedClusterTest {
     @Test
     public void searchWhileCreatingIndex() {
         for (int i = 0; i < 20; i++) {
-            run(prepareCreate("test").setSettings(settingsBuilder().put("index.number_of_shards", 10)));
-            run(client().prepareIndex("test", "type1", "id:" + i).setSource("field", "test"));
-            refresh();
+            prepareCreate("test").setSettings(settingsBuilder().put("index.number_of_shards", 10));
+            client().prepareIndex("test", "type1", "id:" + i).setSource("field", "test").execute().actionGet();
+            RefreshResponse refreshResponse = client().admin().indices().prepareRefresh("test").execute().actionGet();
+            assertThat(refreshResponse.getSuccessfulShards(), greaterThanOrEqualTo(1)); // at least one shard should be successful when refreshing
             SearchResponse searchResponse = client().prepareSearch("test").setQuery(QueryBuilders.termQuery("field", "test")).execute().actionGet();
-            assertThat("Found unexpected number of hits ShardFailures:" + Arrays.toString(searchResponse.getShardFailures()) + " id: " + i, searchResponse.getHits().totalHits(), equalTo(1l));
+            assertThat("found unexpected number of hits, shard_failures (we expected to potentially non active ones!):" + Arrays.toString(searchResponse.getShardFailures()) + " id: " + i, searchResponse.getHits().totalHits(), equalTo(1l));
             wipeIndex("test");
         }
     }

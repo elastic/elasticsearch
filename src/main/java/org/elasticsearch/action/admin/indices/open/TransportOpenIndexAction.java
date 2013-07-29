406611b284d09bc470ae61891f1c4fd6f7b114b7
Luca Cavanna <cavannaluca@gmail.com>
2013-07-29 15:46:50 +0200

    completed renaming of MetaDataStateIndexService to MetaDataIndexStateService

--- src/main/java/org/elasticsearch/action/admin/indices/open/TransportOpenIndexAction.java
+++ src/main/java/org/elasticsearch/action/admin/indices/open/TransportOpenIndexAction.java
@@ -37,13 +37,13 @@ import org.elasticsearch.transport.TransportService;
  */
 public class TransportOpenIndexAction extends TransportMasterNodeOperationAction<OpenIndexRequest, OpenIndexResponse> {
 
-    private final MetaDataIndexStateService stateIndexService;
+    private final MetaDataIndexStateService indexStateService;
 
     @Inject
     public TransportOpenIndexAction(Settings settings, TransportService transportService, ClusterService clusterService,
-                                    ThreadPool threadPool, MetaDataIndexStateService stateIndexService) {
+                                    ThreadPool threadPool, MetaDataIndexStateService indexStateService) {
         super(settings, transportService, clusterService, threadPool);
-        this.stateIndexService = stateIndexService;
+        this.indexStateService = indexStateService;
     }
 
     @Override
@@ -80,7 +80,7 @@ public class TransportOpenIndexAction extends TransportMasterNodeOperationAction
 
     @Override
     protected void masterOperation(final OpenIndexRequest request, final ClusterState state, final ActionListener<OpenIndexResponse> listener) throws ElasticSearchException {
-        stateIndexService.openIndex(new MetaDataIndexStateService.Request(request.indices()).timeout(request.timeout()).masterTimeout(request.masterNodeTimeout()), new MetaDataIndexStateService.Listener() {
+        indexStateService.openIndex(new MetaDataIndexStateService.Request(request.indices()).timeout(request.timeout()).masterTimeout(request.masterNodeTimeout()), new MetaDataIndexStateService.Listener() {
             @Override
             public void onResponse(MetaDataIndexStateService.Response response) {
                 listener.onResponse(new OpenIndexResponse(response.acknowledged()));

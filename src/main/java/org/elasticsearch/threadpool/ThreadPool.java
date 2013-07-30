1fbd6e303905aa2aa091fc43f01b49f02a9a985b
Martijn van Groningen <martijn.v.groningen@gmail.com>
2013-07-30 09:48:05 +0200

    Lowered the default size of the percolate thread pool

--- src/main/java/org/elasticsearch/threadpool/ThreadPool.java
+++ src/main/java/org/elasticsearch/threadpool/ThreadPool.java
@@ -106,7 +106,7 @@ public class ThreadPool extends AbstractComponent {
                 .put(Names.BULK, settingsBuilder().put("type", "fixed").put("size", availableProcessors).build())
                 .put(Names.GET, settingsBuilder().put("type", "fixed").put("size", availableProcessors).build())
                 .put(Names.SEARCH, settingsBuilder().put("type", "fixed").put("size", availableProcessors * 3).put("queue_size", 1000).build())
-                .put(Names.PERCOLATE, settingsBuilder().put("type", "fixed").put("size", availableProcessors * 3).put("queue_size", 1000).build())
+                .put(Names.PERCOLATE, settingsBuilder().put("type", "fixed").put("size", availableProcessors).put("queue_size", 1000).build())
                 .put(Names.MANAGEMENT, settingsBuilder().put("type", "scaling").put("keep_alive", "5m").put("size", 5).build())
                 .put(Names.FLUSH, settingsBuilder().put("type", "scaling").put("keep_alive", "5m").put("size", halfProcMaxAt5).build())
                 .put(Names.MERGE, settingsBuilder().put("type", "scaling").put("keep_alive", "5m").put("size", halfProcMaxAt5).build())

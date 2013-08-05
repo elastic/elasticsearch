094c10d62d120db71210922ec926ef01c263d15e
Simon Willnauer <simonw@apache.org>
2013-08-05 09:41:32 +0200

    Added busy waiting util and add suite timeout.
    Some rare tests require to busy-wait a short time until a given
    condition occurs for instance until a threadpool scaled down the
    number of threads. This commit adds a util that waits a give time
    until a condition is met, in contrast to Thread.sleep this method
    waits increases the wait time by doubleling the waiting time
    iterativly by doubeling it to prevent fast tests to always wait
    a given sleep interval.
    
    This commit also adds a suite timeout to fail a test if the test
    times out. The test infrastructure will provide thread stack traces
    if the timeout kicks in. The default timeout is set to 1h.

--- src/test/java/org/elasticsearch/test/integration/ElasticsearchTestCase.java
+++ src/test/java/org/elasticsearch/test/integration/ElasticsearchTestCase.java
@@ -21,18 +21,19 @@ package org.elasticsearch.test.integration;
 import com.carrotsearch.randomizedtesting.JUnit4MethodProvider;
 import com.carrotsearch.randomizedtesting.RandomizedTest;
 import com.carrotsearch.randomizedtesting.ThreadFilter;
-import com.carrotsearch.randomizedtesting.annotations.Listeners;
-import com.carrotsearch.randomizedtesting.annotations.TestMethodProviders;
-import com.carrotsearch.randomizedtesting.annotations.ThreadLeakFilters;
-import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
+import com.carrotsearch.randomizedtesting.annotations.*;
 import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope.Scope;
+import com.google.common.base.Predicate;
 import org.apache.lucene.util.LuceneJUnit3MethodProvider;
 import org.apache.lucene.util.LuceneTestCase;
+import org.apache.lucene.util.TimeUnits;
 import org.apache.lucene.util.Version;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.junit.listerners.ReproduceInfoPrinter;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
 
 @TestMethodProviders({
         LuceneJUnit3MethodProvider.class,
@@ -43,14 +44,13 @@ import org.junit.runner.RunWith;
 })
 @ThreadLeakFilters(defaultFilters = true, filters = {ElasticsearchTestCase.ElasticSearchThreadFilter.class})
 @ThreadLeakScope(Scope.NONE)
+@TimeoutSuite(millis = TimeUnits.HOUR) // timeout the suite after 1h and fail the test.
 @RunWith(value = com.carrotsearch.randomizedtesting.RandomizedRunner.class)
 public abstract class ElasticsearchTestCase extends RandomizedTest {
 
     public static final Version TEST_VERSION_CURRENT = LuceneTestCase.TEST_VERSION_CURRENT;
 
     protected final ESLogger logger = Loggers.getLogger(getClass());
-
-    public static final boolean NIGHLY = Boolean.parseBoolean(System.getProperty("es.tests.nighly", "false")); // disabled by default
 
     public static final String CHILD_VM_ID = System.getProperty("junit4.childvm.id", "" + System.currentTimeMillis());
 
@@ -62,6 +62,29 @@ public abstract class ElasticsearchTestCase extends RandomizedTest {
 
             return true;
         }
+    }
+    
+    public void awaitBusy(Predicate<?> breakPredicate) throws InterruptedException {
+        awaitBusy(breakPredicate, 10, TimeUnit.SECONDS);
+    }
+
+    
+    public void awaitBusy(Predicate<?> breakPredicate, long maxWaitTime, TimeUnit unit) throws InterruptedException {
+        long maxTimeInMillis = TimeUnit.MILLISECONDS.convert(maxWaitTime, unit);
+        long iterations = Math.max(Math.round(Math.log10(maxTimeInMillis) / Math.log10(2)), 1);
+        long timeInMillis = 1;
+        long sum = 0;
+        for (int i = 0; i < iterations; i++) {
+            if (breakPredicate.apply(null)) {
+                return;
+            }
+            sum += timeInMillis;
+            Thread.sleep(timeInMillis);
+            timeInMillis *= 2;
+        }
+        timeInMillis = maxTimeInMillis - sum;
+        Thread.sleep(Math.max(timeInMillis, 0));
+        
     }
 
 }

026fffdf11aac99fca6f5c7927ce5fcbb8757829
Shay Banon <kimchy@gmail.com>
2013-08-06 15:28:02 +0200

    add 0.90.4

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -128,6 +128,8 @@ public class Version implements Serializable {
     public static final Version V_0_90_2 = new Version(V_0_90_2_ID, false, org.apache.lucene.util.Version.LUCENE_43);
     public static final int V_0_90_3_ID = /*00*/900399;
     public static final Version V_0_90_3 = new Version(V_0_90_3_ID, false, org.apache.lucene.util.Version.LUCENE_44);
+    public static final int V_0_90_4_ID = /*00*/900499;
+    public static final Version V_0_90_4 = new Version(V_0_90_4_ID, false, org.apache.lucene.util.Version.LUCENE_44);
 
     public static final int V_1_0_0_Beta1_ID = /*00*/1000001;
     public static final Version V_1_0_0_Beta1 = new Version(V_1_0_0_Beta1_ID, true, org.apache.lucene.util.Version.LUCENE_44);
@@ -147,6 +149,8 @@ public class Version implements Serializable {
             case V_1_0_0_Beta1_ID:
                 return V_1_0_0_Beta1;
 
+            case V_0_90_4_ID:
+                return V_0_90_4;
             case V_0_90_3_ID:
                 return V_0_90_3;
             case V_0_90_2_ID:

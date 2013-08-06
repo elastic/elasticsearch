13b8acba4d99700475fccd7f504b9043e85b2c40
Shay Banon <kimchy@gmail.com>
2013-08-06 15:27:16 +0200

    move to 0.90.4 snap

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -128,8 +128,10 @@ public class Version implements Serializable {
     public static final Version V_0_90_2 = new Version(V_0_90_2_ID, false, org.apache.lucene.util.Version.LUCENE_43);
     public static final int V_0_90_3_ID = /*00*/900399;
     public static final Version V_0_90_3 = new Version(V_0_90_3_ID, false, org.apache.lucene.util.Version.LUCENE_44);
+    public static final int V_0_90_4_ID = /*00*/900499;
+    public static final Version V_0_90_4 = new Version(V_0_90_4_ID, true, org.apache.lucene.util.Version.LUCENE_44);
 
-    public static final Version CURRENT = V_0_90_3;
+    public static final Version CURRENT = V_0_90_4;
 
     static {
         assert CURRENT.luceneVersion == Lucene.VERSION : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
@@ -141,6 +143,8 @@ public class Version implements Serializable {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_0_90_4_ID:
+                return V_0_90_4;
             case V_0_90_3_ID:
                 return V_0_90_3;
             case V_0_90_2_ID:

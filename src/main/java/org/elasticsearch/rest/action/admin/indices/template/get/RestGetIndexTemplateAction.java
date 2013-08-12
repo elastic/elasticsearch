45c0d1de0446dd348bc6501c63dc34652a5c9401
Alexander Reelsen <alexander@reelsen.net>
2013-08-12 12:57:15 +0200

    Added IndicesAdminClient.getIndexTemplates()
    In addition to creating and removing a template, one can now receive index templates as well.
    Simple regexes like template* are supported.
    
    Closes #3439

--- src/main/java/org/elasticsearch/rest/action/admin/indices/template/get/RestGetIndexTemplateAction.java
+++ src/main/java/org/elasticsearch/rest/action/admin/indices/template/get/RestGetIndexTemplateAction.java
@@ -20,22 +20,19 @@
 package org.elasticsearch.rest.action.admin.indices.template.get;
 
 import com.google.common.collect.Maps;
+import org.elasticsearch.ExceptionsHelper;
 import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.action.admin.cluster.state.ClusterStateRequest;
-import org.elasticsearch.action.admin.cluster.state.ClusterStateResponse;
+import org.elasticsearch.action.admin.indices.template.get.GetIndexTemplatesRequest;
+import org.elasticsearch.action.admin.indices.template.get.GetIndexTemplatesResponse;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.client.Requests;
 import org.elasticsearch.cluster.metadata.IndexTemplateMetaData;
-import org.elasticsearch.cluster.metadata.MetaData;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.settings.SettingsFilter;
 import org.elasticsearch.common.xcontent.ToXContent;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.rest.*;
 import org.elasticsearch.rest.action.support.RestXContentBuilder;
 
-import java.io.IOException;
 import java.util.Map;
 
 import static org.elasticsearch.rest.RestRequest.Method.GET;
@@ -47,47 +44,36 @@ import static org.elasticsearch.rest.RestStatus.OK;
  */
 public class RestGetIndexTemplateAction extends BaseRestHandler {
 
-    private final SettingsFilter settingsFilter;
-
     @Inject
-    public RestGetIndexTemplateAction(Settings settings, Client client, RestController controller,
-                                      SettingsFilter settingsFilter) {
+    public RestGetIndexTemplateAction(Settings settings, Client client, RestController controller) {
         super(settings, client);
-        this.settingsFilter = settingsFilter;
 
         controller.registerHandler(GET, "/_template/{name}", this);
     }
 
     @Override
     public void handleRequest(final RestRequest request, final RestChannel channel) {
-        ClusterStateRequest clusterStateRequest = Requests.clusterStateRequest()
-                .filterRoutingTable(true)
-                .filterNodes(true)
-                .filteredIndexTemplates(request.param("name"))
-                .filterOutIndices();
+        GetIndexTemplatesRequest getIndexTemplatesRequest = new GetIndexTemplatesRequest(request.param("name"));
+        getIndexTemplatesRequest.listenerThreaded(false);
 
-        clusterStateRequest.listenerThreaded(false);
-
-        client.admin().cluster().state(clusterStateRequest, new ActionListener<ClusterStateResponse>() {
+        client.admin().indices().getTemplates(getIndexTemplatesRequest, new ActionListener<GetIndexTemplatesResponse>() {
             @Override
-            public void onResponse(ClusterStateResponse response) {
-                Map<String, String> paramsMap = Maps.newHashMap();
-                paramsMap.put("reduce_mappings", "true");
-                ToXContent.Params params = new ToXContent.DelegatingMapParams(paramsMap, request);
-
+            public void onResponse(GetIndexTemplatesResponse getIndexTemplatesResponse) {
                 try {
-                    MetaData metaData = response.getState().metaData();
+                    boolean templateExists = getIndexTemplatesResponse.getIndexTemplates().size() > 0;
 
-                    if (metaData.templates().values().size() == 0) {
+                    if (!templateExists) {
                         channel.sendResponse(new StringRestResponse(NOT_FOUND));
                     } else {
+                        Map<String, String> paramsMap = Maps.newHashMap();
+                        paramsMap.put("reduce_mappings", "true");
+                        ToXContent.Params params = new ToXContent.DelegatingMapParams(paramsMap, request);
+
                         XContentBuilder builder = RestXContentBuilder.restContentBuilder(request);
                         builder.startObject();
-
-                        for (IndexTemplateMetaData indexMetaData : metaData.templates().values()) {
-                            IndexTemplateMetaData.Builder.toXContent(indexMetaData, builder, params);
+                        for (IndexTemplateMetaData indexTemplateMetaData : getIndexTemplatesResponse.getIndexTemplates()) {
+                            IndexTemplateMetaData.Builder.toXContent(indexTemplateMetaData, builder, params);
                         }
-
                         builder.endObject();
 
                         channel.sendResponse(new XContentRestResponse(request, OK, builder));
@@ -100,8 +86,8 @@ public class RestGetIndexTemplateAction extends BaseRestHandler {
             @Override
             public void onFailure(Throwable e) {
                 try {
-                    channel.sendResponse(new XContentThrowableRestResponse(request, e));
-                } catch (IOException e1) {
+                    channel.sendResponse(new StringRestResponse(ExceptionsHelper.status(e)));
+                } catch (Exception e1) {
                     logger.error("Failed to send failure response", e1);
                 }
             }

d14576609c94f8613b3231c3dcb4233cad98b441
Luca Cavanna <cavannaluca@gmail.com>
2013-09-09 14:12:13 +0200

    Fixed typo: renamed test wamer package to warmer

--- src/test/java/org/elasticsearch/test/integration/indices/warmer/LocalGatewayIndicesWarmerTests.java
+++ src/test/java/org/elasticsearch/test/integration/indices/warmer/LocalGatewayIndicesWarmerTests.java
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-package org.elasticsearch.test.integration.indices.wamer;
+package org.elasticsearch.test.integration.indices.warmer;
 
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.cluster.ClusterState;

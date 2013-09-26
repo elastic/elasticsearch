b66f3c68341b29a92a69a5f049bcd2134133c9db
Boaz Leskes <b.leskes@gmail.com>
2013-09-26 14:31:07 +0200

    Send the update-mapping events before actually indexing into the shard, because the latter may generate exceptions (like when the shard is not yet ready to accept new docs).
    Also improved the SimpleDeleteMappingTest, as it make take a while until the update-mapping event is processed.
    
    Closes #3782

--- src/test/java/org/elasticsearch/indices/mapping/SimpleDeleteMappingTests.java
+++ src/test/java/org/elasticsearch/indices/mapping/SimpleDeleteMappingTests.java
@@ -51,12 +51,10 @@ public class SimpleDeleteMappingTests extends AbstractIntegrationTest {
         }
 
         ClusterState clusterState = client().admin().cluster().prepareState().execute().actionGet().getState();
-        if (!clusterState.metaData().index("test").mappings().containsKey("type1")) {
-            logger.info("mapping of type [{}] is not in the clusterstate version: [{}] localNode: [{}] nodes: [{}]", "type1", 
-                    clusterState.version(), clusterState.nodes().localNode(), clusterState.nodes().dataNodes().values());
-            logger.info("Current mappings in clusterstate: [{}]", clusterState.metaData().index("test").mappings().keySet());
-        }
-        assertThat(clusterState.metaData().index("test").mappings().containsKey("type1"), equalTo(true));
+        for (int i = 0; i < 10 && !clusterState.metaData().index("test").mappings().containsKey("type1"); i++, Thread.sleep(100)) ;
+
+        assertThat(clusterState.metaData().index("test").mappings(), hasKey("type1"));
+
         GetMappingsResponse mappingsResponse = client().admin().indices().prepareGetMappings("test").setTypes("type1").execute().actionGet();
         assertThat(mappingsResponse.getMappings().get("test").get("type1"), notNullValue());
 

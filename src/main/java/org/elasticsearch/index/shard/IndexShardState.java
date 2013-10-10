e7bf55ef0a2f562268df4f452b38d3c067b123d8
Martijn van Groningen <martijn.v.groningen@gmail.com>
2013-10-10 16:15:33 +0200

    Fixed serialization error. POST_RECOVERY is now also serialized

--- src/main/java/org/elasticsearch/index/shard/IndexShardState.java
+++ src/main/java/org/elasticsearch/index/shard/IndexShardState.java
@@ -32,6 +32,14 @@ public enum IndexShardState {
     RELOCATED((byte) 4),
     CLOSED((byte) 5);
 
+    private static final IndexShardState[] ORDS = new IndexShardState[IndexShardState.values().length];
+
+    static {
+        for (IndexShardState state : IndexShardState.values()) {
+            ORDS[state.id()] = state;
+        }
+    }
+
     private final byte id;
 
     IndexShardState(byte id) {
@@ -43,17 +51,9 @@ public enum IndexShardState {
     }
 
     public static IndexShardState fromId(byte id) throws ElasticSearchIllegalArgumentException {
-        if (id == 0) {
-            return CREATED;
-        } else if (id == 1) {
-            return RECOVERING;
-        } else if (id == 2) {
-            return STARTED;
-        } else if (id == 3) {
-            return RELOCATED;
-        } else if (id == 4) {
-            return CLOSED;
+        if (id < ORDS[0].id && id > ORDS[ORDS.length - 1].id) {
+            throw new ElasticSearchIllegalArgumentException("No mapping for id [" + id + "]");
         }
-        throw new ElasticSearchIllegalArgumentException("No mapping for id [" + id + "]");
+        return ORDS[id];
     }
 }

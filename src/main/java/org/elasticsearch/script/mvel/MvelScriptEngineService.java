bb62cc7cd84e3919167179433757e39f4de188a4
Britta Weber <britta.weber@elasticsearch.com>
2013-10-24 19:09:02 +0200

    remove trove import from mvel script
    all subclasses of trove maps have been removed in 088e05b368

--- src/main/java/org/elasticsearch/script/mvel/MvelScriptEngineService.java
+++ src/main/java/org/elasticsearch/script/mvel/MvelScriptEngineService.java
@@ -54,7 +54,6 @@ public class MvelScriptEngineService extends AbstractComponent implements Script
 
         parserConfiguration = new ParserConfiguration();
         parserConfiguration.addPackageImport("java.util");
-        parserConfiguration.addPackageImport("gnu.trove");
         parserConfiguration.addPackageImport("org.joda");
         parserConfiguration.addImport("time", MVEL.getStaticMethod(System.class, "currentTimeMillis", new Class[0]));
         // unboxed version of Math, better performance since conversion from boxed to unboxed my mvel is not needed

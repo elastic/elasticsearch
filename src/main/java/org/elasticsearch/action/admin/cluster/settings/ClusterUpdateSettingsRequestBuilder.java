353f2b7ab8f695c0b875ab15bcf626c4754e60bd
Luca Cavanna <cavannaluca@gmail.com>
2013-10-28 17:46:25 +0100

    Added support for acknowledgements to update cluster settings api
    As a side note, the internal reroute call is now part of the ack mechanism. That means that if the response contains acknowledged flag, the internal reroute that was eventually issued was acknowledged too. Also, even if the request is not acknowledged, the reroute is issued before returning, which means that there is no need to manually call reroute afterwards to make sure the new settings are immediately applied.
    
    Closes #3995

--- src/main/java/org/elasticsearch/action/admin/cluster/settings/ClusterUpdateSettingsRequestBuilder.java
+++ src/main/java/org/elasticsearch/action/admin/cluster/settings/ClusterUpdateSettingsRequestBuilder.java
@@ -20,7 +20,7 @@
 package org.elasticsearch.action.admin.cluster.settings;
 
 import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.action.support.master.MasterNodeOperationRequestBuilder;
+import org.elasticsearch.action.support.master.AcknowledgedRequestBuilder;
 import org.elasticsearch.client.ClusterAdminClient;
 import org.elasticsearch.client.internal.InternalClusterAdminClient;
 import org.elasticsearch.common.settings.Settings;
@@ -28,48 +28,73 @@ import org.elasticsearch.common.settings.Settings;
 import java.util.Map;
 
 /**
+ * Builder for a cluster update settings request
  */
-public class ClusterUpdateSettingsRequestBuilder extends MasterNodeOperationRequestBuilder<ClusterUpdateSettingsRequest, ClusterUpdateSettingsResponse, ClusterUpdateSettingsRequestBuilder> {
+public class ClusterUpdateSettingsRequestBuilder extends AcknowledgedRequestBuilder<ClusterUpdateSettingsRequest, ClusterUpdateSettingsResponse, ClusterUpdateSettingsRequestBuilder> {
 
     public ClusterUpdateSettingsRequestBuilder(ClusterAdminClient clusterClient) {
         super((InternalClusterAdminClient) clusterClient, new ClusterUpdateSettingsRequest());
     }
 
+    /**
+     * Sets the transient settings to be updated. They will not survive a full cluster restart
+     */
     public ClusterUpdateSettingsRequestBuilder setTransientSettings(Settings settings) {
         request.transientSettings(settings);
         return this;
     }
 
+    /**
+     * Sets the transient settings to be updated. They will not survive a full cluster restart
+     */
     public ClusterUpdateSettingsRequestBuilder setTransientSettings(Settings.Builder settings) {
         request.transientSettings(settings);
         return this;
     }
 
+    /**
+     * Sets the source containing the transient settings to be updated. They will not survive a full cluster restart
+     */
     public ClusterUpdateSettingsRequestBuilder setTransientSettings(String settings) {
         request.transientSettings(settings);
         return this;
     }
 
+    /**
+     * Sets the transient settings to be updated. They will not survive a full cluster restart
+     */
     public ClusterUpdateSettingsRequestBuilder setTransientSettings(Map settings) {
         request.transientSettings(settings);
         return this;
     }
 
+    /**
+     * Sets the persistent settings to be updated. They will get applied cross restarts
+     */
     public ClusterUpdateSettingsRequestBuilder setPersistentSettings(Settings settings) {
         request.persistentSettings(settings);
         return this;
     }
 
+    /**
+     * Sets the persistent settings to be updated. They will get applied cross restarts
+     */
     public ClusterUpdateSettingsRequestBuilder setPersistentSettings(Settings.Builder settings) {
         request.persistentSettings(settings);
         return this;
     }
 
+    /**
+     * Sets the source containing the persistent settings to be updated. They will get applied cross restarts
+     */
     public ClusterUpdateSettingsRequestBuilder setPersistentSettings(String settings) {
         request.persistentSettings(settings);
         return this;
     }
 
+    /**
+     * Sets the persistent settings to be updated. They will get applied cross restarts
+     */
     public ClusterUpdateSettingsRequestBuilder setPersistentSettings(Map settings) {
         request.persistentSettings(settings);
         return this;

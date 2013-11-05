552852f299a817534f5655e8e5bc2bf5d8c79806
Simon Willnauer <simonw@apache.org>
2013-11-05 10:34:44 +0100

    Remove unreleased versions from master & fix 0.90.6 Lucene Version

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -133,9 +133,7 @@ public class Version implements Serializable {
     public static final int V_0_90_5_ID = /*00*/900599;
     public static final Version V_0_90_5 = new Version(V_0_90_5_ID, false, org.apache.lucene.util.Version.LUCENE_44);
     public static final int V_0_90_6_ID = /*00*/900699;
-    public static final Version V_0_90_6 = new Version(V_0_90_6_ID, false, org.apache.lucene.util.Version.LUCENE_44);
-    public static final int V_0_90_7_ID = /*00*/900799;
-    public static final Version V_0_90_7 = new Version(V_0_90_7_ID, false, org.apache.lucene.util.Version.LUCENE_45);
+    public static final Version V_0_90_6 = new Version(V_0_90_6_ID, false, org.apache.lucene.util.Version.LUCENE_45);
 
     public static final int V_1_0_0_Beta1_ID = /*00*/1000001;
     public static final Version V_1_0_0_Beta1 = new Version(V_1_0_0_Beta1_ID, true, org.apache.lucene.util.Version.LUCENE_45);
@@ -154,8 +152,6 @@ public class Version implements Serializable {
         switch (id) {
             case V_1_0_0_Beta1_ID:
                 return V_1_0_0_Beta1;
-            case V_0_90_7_ID:
-                return V_0_90_7;
             case V_0_90_6_ID:
                 return V_0_90_6;
             case V_0_90_5_ID:

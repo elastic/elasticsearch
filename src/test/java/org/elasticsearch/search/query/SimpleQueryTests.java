0569a737a5971c3e6b88811da62d18bef943a88a
Olivier Favre <olivier@yakaz.com>
2013-11-06 15:52:29 +0100

    Indices query/filter skip parsing altogether for irrelevant indices - fix #2416

--- src/test/java/org/elasticsearch/search/query/SimpleQueryTests.java
+++ src/test/java/org/elasticsearch/search/query/SimpleQueryTests.java
@@ -26,6 +26,8 @@ import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.search.SearchPhaseExecutionException;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.action.search.ShardSearchFailure;
+import org.elasticsearch.common.Priority;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
@@ -35,8 +37,10 @@ import org.elasticsearch.index.query.MatchQueryBuilder.Type;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.SearchParseException;
 import org.elasticsearch.search.facet.FacetBuilders;
 import org.elasticsearch.test.AbstractIntegrationTest;
+import org.elasticsearch.transport.RemoteTransportException;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.ISODateTimeFormat;
@@ -1889,5 +1893,180 @@ public class SimpleQueryTests extends AbstractIntegrationTest {
         searchResponse = client().prepareSearch("test")
                 .setQuery(QueryBuilders.multiMatchQuery("value2", "field2^2").lenient(true)).get();
         assertHitCount(searchResponse, 1l);
+    }
+
+    @Test
+    public void testIndicesQuery() throws Exception {
+        createIndex("index1", "index2");
+        ensureGreen();
+
+        client().prepareIndex("index1", "type1").setId("1").setSource("text", "value").get();
+        client().prepareIndex("index2", "type2").setId("2").setSource("text", "value").get();
+        refresh();
+
+        SearchResponse response = client().prepareSearch("index1", "index2")
+                .setQuery(indicesQuery(matchQuery("text", "value"), "index1")
+                        .noMatchQuery(matchQuery("text", "value"))).get();
+        assertHitCount(response, 2l);
+        assertThat(response.getHits().getAt(0).getId(), either(equalTo("1")).or(equalTo("2")));
+        assertThat(response.getHits().getAt(1).getId(), either(equalTo("1")).or(equalTo("2")));
+
+        response = client().prepareSearch("index1", "index2")
+                .setQuery(indicesQuery(matchQuery("text", "value"), "index1")).get();
+        assertHitCount(response, 2l);
+        assertThat(response.getHits().getAt(0).getId(), either(equalTo("1")).or(equalTo("2")));
+        assertThat(response.getHits().getAt(1).getId(), either(equalTo("1")).or(equalTo("2")));
+
+        response = client().prepareSearch("index1", "index2")
+                .setQuery(indicesQuery(matchQuery("text", "value"), "index1")
+                        .noMatchQuery("all")).get();
+        assertHitCount(response, 2l);
+        assertThat(response.getHits().getAt(0).getId(), either(equalTo("1")).or(equalTo("2")));
+        assertThat(response.getHits().getAt(1).getId(), either(equalTo("1")).or(equalTo("2")));
+
+        response = client().prepareSearch("index1", "index2")
+                .setQuery(indicesQuery(matchQuery("text", "value"), "index1")
+                        .noMatchQuery("none")).get();
+        assertHitCount(response, 1l);
+        assertThat(response.getHits().getAt(0).getId(), equalTo("1"));
+    }
+
+    @Test
+    public void testIndicesFilter() throws Exception {
+        createIndex("index1", "index2");
+        ensureGreen();
+
+        client().prepareIndex("index1", "type1").setId("1").setSource("text", "value").get();
+        client().prepareIndex("index2", "type2").setId("2").setSource("text", "value").get();
+        refresh();
+
+        SearchResponse response = client().prepareSearch("index1", "index2")
+                .setFilter(indicesFilter(termFilter("text", "value"), "index1")
+                        .noMatchFilter(termFilter("text", "value"))).get();
+        assertHitCount(response, 2l);
+        assertThat(response.getHits().getAt(0).getId(), either(equalTo("1")).or(equalTo("2")));
+        assertThat(response.getHits().getAt(1).getId(), either(equalTo("1")).or(equalTo("2")));
+
+        response = client().prepareSearch("index1", "index2")
+                .setFilter(indicesFilter(termFilter("text", "value"), "index1")).get();
+        assertHitCount(response, 2l);
+        assertThat(response.getHits().getAt(0).getId(), either(equalTo("1")).or(equalTo("2")));
+        assertThat(response.getHits().getAt(1).getId(), either(equalTo("1")).or(equalTo("2")));
+
+        response = client().prepareSearch("index1", "index2")
+                .setFilter(indicesFilter(termFilter("text", "value"), "index1")
+                        .noMatchFilter("all")).get();
+        assertHitCount(response, 2l);
+        assertThat(response.getHits().getAt(0).getId(), either(equalTo("1")).or(equalTo("2")));
+        assertThat(response.getHits().getAt(1).getId(), either(equalTo("1")).or(equalTo("2")));
+
+        response = client().prepareSearch("index1", "index2")
+                .setFilter(indicesFilter(termFilter("text", "value"), "index1")
+                        .noMatchFilter("none")).get();
+        assertHitCount(response, 1l);
+        assertThat(response.getHits().getAt(0).getId(), equalTo("1"));
+    }
+
+    @Test // https://github.com/elasticsearch/elasticsearch/issues/2416
+    public void testIndicesQueryHideParsingExceptions() throws Exception {
+        client().admin().indices().prepareCreate("simple")
+                .addMapping("lone", jsonBuilder().startObject().startObject("lone").endObject().endObject())
+                .get();
+        client().admin().indices().prepareCreate("related")
+                .addMapping("parent", jsonBuilder().startObject().startObject("parent").endObject().endObject())
+                .addMapping("child", jsonBuilder().startObject().startObject("child").startObject("_parent").field("type", "parent")
+                        .endObject().endObject().endObject())
+                .get();
+        ensureGreen();
+
+        client().prepareIndex("simple", "lone").setId("1").setSource("text", "value").get();
+        client().prepareIndex("related", "parent").setId("2").setSource("text", "parent").get();
+        client().prepareIndex("related", "child").setId("3").setParent("2").setSource("text", "value").get();
+        refresh();
+
+        SearchResponse response = client().prepareSearch("related")
+                .setQuery(hasChildQuery("child", matchQuery("text", "value"))).get();
+        assertHitCount(response, 1l);
+        assertThat(response.getHits().getAt(0).getId(), equalTo("2"));
+
+        response = client().prepareSearch("simple")
+                .setQuery(matchQuery("text", "value")).get();
+        assertHitCount(response, 1l);
+        assertThat(response.getHits().getAt(0).getId(), equalTo("1"));
+
+        try {
+            client().prepareSearch("simple")
+                    .setQuery(hasChildQuery("child", matchQuery("text", "value"))).get();
+            fail("Should have failed with a SearchPhaseExecutionException because all shards failed with a nested QueryParsingException");
+            // If no failure happens, the HasChildQuery may have changed behavior when provided with wrong types
+        } catch (SearchPhaseExecutionException e) {
+            // There is no easy way to ensure we got a QueryParsingException
+        }
+
+        response = client().prepareSearch("related", "simple")
+                .setQuery(indicesQuery(matchQuery("text", "parent"), "related")
+                        .noMatchQuery(matchQuery("text", "value"))).get();
+        assertHitCount(response, 2l);
+        assertThat(response.getHits().getAt(0).getId(), either(equalTo("1")).or(equalTo("2")));
+        assertThat(response.getHits().getAt(1).getId(), either(equalTo("1")).or(equalTo("2")));
+
+        response = client().prepareSearch("related", "simple")
+                .setQuery(indicesQuery(hasChildQuery("child", matchQuery("text", "value")), "related")
+                        .noMatchQuery(matchQuery("text", "value"))).get();
+        assertHitCount(response, 2l);
+        assertThat(response.getHits().getAt(0).getId(), either(equalTo("1")).or(equalTo("2")));
+        assertThat(response.getHits().getAt(1).getId(), either(equalTo("1")).or(equalTo("2")));
+    }
+
+
+    @Test // https://github.com/elasticsearch/elasticsearch/issues/2416
+    public void testIndicesFilterHideParsingExceptions() throws Exception {
+        client().admin().indices().prepareCreate("simple")
+                .addMapping("lone", jsonBuilder().startObject().startObject("lone").endObject().endObject())
+                .get();
+        client().admin().indices().prepareCreate("related")
+                .addMapping("parent", jsonBuilder().startObject().startObject("parent").endObject().endObject())
+                .addMapping("child", jsonBuilder().startObject().startObject("child").startObject("_parent").field("type", "parent")
+                        .endObject().endObject().endObject())
+                .get();
+        ensureGreen();
+
+        client().prepareIndex("simple", "lone").setId("1").setSource("text", "value").get();
+        client().prepareIndex("related", "parent").setId("2").setSource("text", "parent").get();
+        client().prepareIndex("related", "child").setId("3").setParent("2").setSource("text", "value").get();
+        refresh();
+
+        SearchResponse response = client().prepareSearch("related")
+                .setFilter(hasChildFilter("child", termFilter("text", "value"))).get();
+        assertHitCount(response, 1l);
+        assertThat(response.getHits().getAt(0).getId(), equalTo("2"));
+
+        response = client().prepareSearch("simple")
+                .setFilter(termFilter("text", "value")).get();
+        assertHitCount(response, 1l);
+        assertThat(response.getHits().getAt(0).getId(), equalTo("1"));
+
+        try {
+            client().prepareSearch("simple")
+                    .setFilter(hasChildFilter("child", termFilter("text", "value"))).get();
+            fail("Should have failed with a SearchPhaseExecutionException because all shards failed with a nested QueryParsingException");
+            // If no failure happens, the HasChildQuery may have changed behavior when provided with wrong types
+        } catch (SearchPhaseExecutionException e) {
+            // There is no easy way to ensure we got a QueryParsingException
+        }
+
+        response = client().prepareSearch("related", "simple")
+                .setFilter(indicesFilter(termFilter("text", "parent"), "related")
+                        .noMatchFilter(termFilter("text", "value"))).get();
+        assertHitCount(response, 2l);
+        assertThat(response.getHits().getAt(0).getId(), either(equalTo("1")).or(equalTo("2")));
+        assertThat(response.getHits().getAt(1).getId(), either(equalTo("1")).or(equalTo("2")));
+
+        response = client().prepareSearch("related", "simple")
+                .setFilter(indicesFilter(hasChildFilter("child", termFilter("text", "value")), "related")
+                        .noMatchFilter(termFilter("text", "value"))).get();
+        assertHitCount(response, 2l);
+        assertThat(response.getHits().getAt(0).getId(), either(equalTo("1")).or(equalTo("2")));
+        assertThat(response.getHits().getAt(1).getId(), either(equalTo("1")).or(equalTo("2")));
     }
 }

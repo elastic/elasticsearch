3145ffd3728fcd24e8b05a34d157d6eedbc5368c
Alexander Reelsen <alexander@reelsen.net>
2013-11-23 07:40:29 +0100

    Added mlockall setting to process info output

--- src/main/java/org/elasticsearch/common/jna/Natives.java
+++ src/main/java/org/elasticsearch/common/jna/Natives.java
@@ -19,11 +19,11 @@
 
 package org.elasticsearch.common.jna;
 
-import java.util.Locale;
-
 import com.sun.jna.Native;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
+
+import java.util.Locale;
 
 /**
  *
@@ -31,13 +31,18 @@ import org.elasticsearch.common.logging.Loggers;
 public class Natives {
 
     private static ESLogger logger = Loggers.getLogger(Natives.class);
+    // Set to true, in case native mlockall call was successful
+    public static boolean LOCAL_MLOCKALL = false;
 
     public static void tryMlockall() {
         int errno = Integer.MIN_VALUE;
         try {
             int result = CLibrary.mlockall(CLibrary.MCL_CURRENT);
-            if (result != 0)
+            if (result != 0) {
                 errno = Native.getLastError();
+            } else {
+                LOCAL_MLOCKALL = true;
+            }
         } catch (UnsatisfiedLinkError e) {
             // this will have already been logged by CLibrary, no need to repeat it
             return;

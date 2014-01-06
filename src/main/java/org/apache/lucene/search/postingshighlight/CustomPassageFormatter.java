3ff7011b752c1e431698e7de6fe15587f8cdd3eb
Simon Willnauer <simonw@apache.org>
2014-01-06 22:48:02 +0100

    Fix ASL Header in source files to reflect s/ElasticSearch/Elasticsearch
    This commit also removes the license to Shay Banon in favor of soley
    Elasticsearch. Thanks Shay for this awesome product you took it far!
    
    Closes #4636

--- src/main/java/org/apache/lucene/search/postingshighlight/CustomPassageFormatter.java
+++ src/main/java/org/apache/lucene/search/postingshighlight/CustomPassageFormatter.java
@@ -1,8 +1,8 @@
 /*
- * Licensed to ElasticSearch and Shay Banon under one
+ * Licensed to Elasticsearch under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership. ElasticSearch licenses this
+ * regarding copyright ownership. Elasticsearch licenses this
  * file to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at

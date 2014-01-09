982c848ba762027193f2eec16343da866a03f82a
Alexander Reelsen <alexander@reelsen.net>
2014-01-09 11:22:49 +0100

    [DOCS] Added documentation for SameShardAllocationDecider
    Closes #4615

--- src/main/java/org/elasticsearch/cluster/routing/allocation/decider/SameShardAllocationDecider.java
+++ src/main/java/org/elasticsearch/cluster/routing/allocation/decider/SameShardAllocationDecider.java
@@ -27,10 +27,10 @@ import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 
 /**
- * An allocation decider that prevents the more than prevents multiple instances
- * of the same shard to be allocated on a single <tt>host</tt>. The cluster setting can
- * be modified in real-time by updating the {@value #SAME_HOST_SETTING} value of
- * cluster setting API. The default is <code>false</code>.
+ * An allocation decider that prevents multiple instances of the same shard to be
+ * allocated on a single <tt>host</tt>. The cluster setting can be modified in
+ * real-time by updating the {@value #SAME_HOST_SETTING} value of cluster setting
+ * API. The default is <code>false</code>.
  * <p>
  * Note: this setting only applies if multiple nodes are started on the same
  * <tt>host</tt>. Multiple allocations of the same shard on the same <tt>node</tt> are

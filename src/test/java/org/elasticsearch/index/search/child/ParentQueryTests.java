cfe92bacd8c58a263adea5ab4c02d28519d4dfe8
Adrien Grand <jpountz@gmail.com>
2014-01-20 11:56:03 +0100

    Fix test bug: test expects at least one child document.

--- src/test/java/org/elasticsearch/index/search/child/ParentQueryTests.java
+++ src/test/java/org/elasticsearch/index/search/child/ParentQueryTests.java
@@ -98,6 +98,10 @@ public class ParentQueryTests extends ElasticsearchLuceneTestCase {
             } else {
                 numChildDocs = random().nextInt(TEST_NIGHTLY ? 40 : 10);
             }
+            if (parentDocId == numParentDocs - 1 && childIdToParentId.isEmpty()) {
+                // ensure there is at least one child in the index
+                numChildDocs = Math.max(1, numChildDocs);
+            }
             for (int i = 0; i < numChildDocs; i++) {
                 String child = Integer.toString(childDocId++);
                 boolean markChildAsDeleted = rarely();

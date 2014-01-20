a7ddce94fa535ba92e8d61117c971419b025b85c
David Pilato <david@pilato.fr>
2014-01-20 11:05:26 +0100

    Update PR after @javanna review

--- src/test/java/org/elasticsearch/river/RiverTests.java
+++ src/test/java/org/elasticsearch/river/RiverTests.java
@@ -32,6 +32,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.Matchers.equalTo;
 
+@ElasticsearchIntegrationTest.ClusterScope(scope = ElasticsearchIntegrationTest.Scope.SUITE)
 public class RiverTests extends ElasticsearchIntegrationTest {
 
     @Test

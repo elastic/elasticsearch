9c067054eef34f00c552fae219e2dc90470dd1ed
Igor Motov <igor@motovs.org>
2014-01-29 06:49:36 -0500

    Improve test stability

--- src/test/java/org/elasticsearch/index/mapper/copyto/CopyToMapperIntegrationTests.java
+++ src/test/java/org/elasticsearch/index/mapper/copyto/CopyToMapperIntegrationTests.java
@@ -44,6 +44,7 @@ public class CopyToMapperIntegrationTests extends ElasticsearchIntegrationTest {
                 client().admin().indices().prepareCreate("test-idx")
                         .addMapping("doc", createDynamicTemplateMapping())
         );
+        ensureGreen("test-idx");
 
         int recordCount = between(1, 200);
 

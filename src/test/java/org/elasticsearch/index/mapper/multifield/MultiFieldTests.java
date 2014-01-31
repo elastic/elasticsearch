406903032247783c0785bc930f66446e24eaebb6
Martijn van Groningen <martijn.v.groningen@gmail.com>
2014-01-31 12:50:40 +0100

    Added `fields` support to `geo_point` and `completion` field type.
    Upgrading 0.90.x `multi_field` type that has a `geo_point` or `completion` field type as default field would otherwise fail.
    Also it make sense to support these field types, because both support specifying the actual values as string.

--- src/test/java/org/elasticsearch/index/mapper/multifield/MultiFieldTests.java
+++ src/test/java/org/elasticsearch/index/mapper/multifield/MultiFieldTests.java
@@ -27,15 +27,14 @@ import org.elasticsearch.index.mapper.DocumentMapperParser;
 import org.elasticsearch.index.mapper.FieldMapper;
 import org.elasticsearch.index.mapper.MapperTestUtils;
 import org.elasticsearch.index.mapper.ParseContext.Document;
-import org.elasticsearch.index.mapper.core.DateFieldMapper;
-import org.elasticsearch.index.mapper.core.LongFieldMapper;
-import org.elasticsearch.index.mapper.core.StringFieldMapper;
-import org.elasticsearch.index.mapper.core.TokenCountFieldMapper;
+import org.elasticsearch.index.mapper.core.*;
+import org.elasticsearch.index.mapper.geo.GeoPointFieldMapper;
 import org.elasticsearch.test.ElasticsearchTestCase;
 import org.junit.Test;
 
 import static org.elasticsearch.common.io.Streams.copyToBytesFromClasspath;
 import static org.elasticsearch.common.io.Streams.copyToStringFromClasspath;
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.elasticsearch.index.mapper.MapperBuilders.*;
 import static org.hamcrest.Matchers.*;
 
@@ -235,5 +234,175 @@ public class MultiFieldTests extends ElasticsearchTestCase {
         assertThat(docMapper.mappers().fullName("age.stored").mapper().fieldType().indexed(), equalTo(true));
         assertThat(docMapper.mappers().fullName("age.stored").mapper().fieldType().stored(), equalTo(true));
         assertThat(docMapper.mappers().fullName("age.stored").mapper().fieldType().tokenized(), equalTo(false));
+    }
+
+    @Test
+    public void testConvertMultiFieldGeoPoint() throws Exception {
+        String mapping = copyToStringFromClasspath("/org/elasticsearch/index/mapper/multifield/test-multi-field-type-geo_point.json");
+        DocumentMapper docMapper = MapperTestUtils.newParser().parse(mapping);
+
+        assertThat(docMapper.mappers().fullName("a").mapper(), notNullValue());
+        assertThat(docMapper.mappers().fullName("a").mapper(), instanceOf(StringFieldMapper.class));
+        assertThat(docMapper.mappers().fullName("a").mapper().fieldType().indexed(), equalTo(true));
+        assertThat(docMapper.mappers().fullName("a").mapper().fieldType().stored(), equalTo(false));
+        assertThat(docMapper.mappers().fullName("a").mapper().fieldType().tokenized(), equalTo(false));
+
+        assertThat(docMapper.mappers().fullName("a.b").mapper(), notNullValue());
+        assertThat(docMapper.mappers().fullName("a.b").mapper(), instanceOf(GeoPointFieldMapper.class));
+        assertThat(docMapper.mappers().fullName("a.b").mapper().fieldType().indexed(), equalTo(true));
+        assertThat(docMapper.mappers().fullName("a.b").mapper().fieldType().stored(), equalTo(false));
+        assertThat(docMapper.mappers().fullName("a.b").mapper().fieldType().tokenized(), equalTo(false));
+
+        BytesReference json = jsonBuilder().startObject()
+                .field("_id", "1")
+                .field("a", "-1,-1")
+                .endObject().bytes();
+        Document doc = docMapper.parse(json).rootDoc();
+
+        IndexableField f = doc.getField("a");
+        assertThat(f, notNullValue());
+        assertThat(f.name(), equalTo("a"));
+        assertThat(f.stringValue(), equalTo("-1,-1"));
+        assertThat(f.fieldType().stored(), equalTo(false));
+        assertThat(f.fieldType().indexed(), equalTo(true));
+
+        f = doc.getField("a.b");
+        assertThat(f, notNullValue());
+        assertThat(f.name(), equalTo("a.b"));
+        assertThat(f.stringValue(), equalTo("-1.0,-1.0"));
+        assertThat(f.fieldType().stored(), equalTo(false));
+        assertThat(f.fieldType().indexed(), equalTo(true));
+
+        assertThat(docMapper.mappers().fullName("b").mapper(), notNullValue());
+        assertThat(docMapper.mappers().fullName("b").mapper(), instanceOf(GeoPointFieldMapper.class));
+        assertThat(docMapper.mappers().fullName("b").mapper().fieldType().indexed(), equalTo(true));
+        assertThat(docMapper.mappers().fullName("b").mapper().fieldType().stored(), equalTo(false));
+        assertThat(docMapper.mappers().fullName("b").mapper().fieldType().tokenized(), equalTo(false));
+
+        assertThat(docMapper.mappers().fullName("b.a").mapper(), notNullValue());
+        assertThat(docMapper.mappers().fullName("b.a").mapper(), instanceOf(StringFieldMapper.class));
+        assertThat(docMapper.mappers().fullName("b.a").mapper().fieldType().indexed(), equalTo(true));
+        assertThat(docMapper.mappers().fullName("b.a").mapper().fieldType().stored(), equalTo(false));
+        assertThat(docMapper.mappers().fullName("b.a").mapper().fieldType().tokenized(), equalTo(false));
+
+        json = jsonBuilder().startObject()
+                .field("_id", "1")
+                .field("b", "-1,-1")
+                .endObject().bytes();
+        doc = docMapper.parse(json).rootDoc();
+
+        f = doc.getField("b");
+        assertThat(f, notNullValue());
+        assertThat(f.name(), equalTo("b"));
+        assertThat(f.stringValue(), equalTo("-1.0,-1.0"));
+        assertThat(f.fieldType().stored(), equalTo(false));
+        assertThat(f.fieldType().indexed(), equalTo(true));
+
+        f = doc.getField("b.a");
+        assertThat(f, notNullValue());
+        assertThat(f.name(), equalTo("b.a"));
+        assertThat(f.stringValue(), equalTo("-1,-1"));
+        assertThat(f.fieldType().stored(), equalTo(false));
+        assertThat(f.fieldType().indexed(), equalTo(true));
+
+        json = jsonBuilder().startObject()
+                .field("_id", "1")
+                .startArray("b").startArray().value(-1).value(-1).endArray().startArray().value(-2).value(-2).endArray().endArray()
+                .endObject().bytes();
+        doc = docMapper.parse(json).rootDoc();
+
+        f = doc.getFields("b")[0];
+        assertThat(f, notNullValue());
+        assertThat(f.name(), equalTo("b"));
+        assertThat(f.stringValue(), equalTo("-1.0,-1.0"));
+        assertThat(f.fieldType().stored(), equalTo(false));
+        assertThat(f.fieldType().indexed(), equalTo(true));
+
+        f = doc.getFields("b")[1];
+        assertThat(f, notNullValue());
+        assertThat(f.name(), equalTo("b"));
+        assertThat(f.stringValue(), equalTo("-2.0,-2.0"));
+        assertThat(f.fieldType().stored(), equalTo(false));
+        assertThat(f.fieldType().indexed(), equalTo(true));
+
+        f = doc.getField("b.a");
+        assertThat(f, notNullValue());
+        assertThat(f.name(), equalTo("b.a"));
+        // NOTE: "]" B/c the lat,long aren't specified as a string, we miss the actual values when parsing the multi
+        // fields. We already skipped over the coordinates values and can't get to the coordinates.
+        // This happens if coordinates are specified as array and object.
+        assertThat(f.stringValue(), equalTo("]"));
+        assertThat(f.fieldType().stored(), equalTo(false));
+        assertThat(f.fieldType().indexed(), equalTo(true));
+    }
+
+    @Test
+    public void testConvertMultiFieldCompletion() throws Exception {
+        String mapping = copyToStringFromClasspath("/org/elasticsearch/index/mapper/multifield/test-multi-field-type-completion.json");
+        DocumentMapper docMapper = MapperTestUtils.newParser().parse(mapping);
+
+        assertThat(docMapper.mappers().fullName("a").mapper(), notNullValue());
+        assertThat(docMapper.mappers().fullName("a").mapper(), instanceOf(StringFieldMapper.class));
+        assertThat(docMapper.mappers().fullName("a").mapper().fieldType().indexed(), equalTo(true));
+        assertThat(docMapper.mappers().fullName("a").mapper().fieldType().stored(), equalTo(false));
+        assertThat(docMapper.mappers().fullName("a").mapper().fieldType().tokenized(), equalTo(false));
+
+        assertThat(docMapper.mappers().fullName("a.b").mapper(), notNullValue());
+        assertThat(docMapper.mappers().fullName("a.b").mapper(), instanceOf(CompletionFieldMapper.class));
+        assertThat(docMapper.mappers().fullName("a.b").mapper().fieldType().indexed(), equalTo(true));
+        assertThat(docMapper.mappers().fullName("a.b").mapper().fieldType().stored(), equalTo(false));
+        assertThat(docMapper.mappers().fullName("a.b").mapper().fieldType().tokenized(), equalTo(true));
+
+        BytesReference json = jsonBuilder().startObject()
+                .field("_id", "1")
+                .field("a", "complete me")
+                .endObject().bytes();
+        Document doc = docMapper.parse(json).rootDoc();
+
+        IndexableField f = doc.getField("a");
+        assertThat(f, notNullValue());
+        assertThat(f.name(), equalTo("a"));
+        assertThat(f.stringValue(), equalTo("complete me"));
+        assertThat(f.fieldType().stored(), equalTo(false));
+        assertThat(f.fieldType().indexed(), equalTo(true));
+
+        f = doc.getField("a.b");
+        assertThat(f, notNullValue());
+        assertThat(f.name(), equalTo("a.b"));
+        assertThat(f.stringValue(), equalTo("complete me"));
+        assertThat(f.fieldType().stored(), equalTo(false));
+        assertThat(f.fieldType().indexed(), equalTo(true));
+
+        assertThat(docMapper.mappers().fullName("b").mapper(), notNullValue());
+        assertThat(docMapper.mappers().fullName("b").mapper(), instanceOf(CompletionFieldMapper.class));
+        assertThat(docMapper.mappers().fullName("b").mapper().fieldType().indexed(), equalTo(true));
+        assertThat(docMapper.mappers().fullName("b").mapper().fieldType().stored(), equalTo(false));
+        assertThat(docMapper.mappers().fullName("b").mapper().fieldType().tokenized(), equalTo(true));
+
+        assertThat(docMapper.mappers().fullName("b.a").mapper(), notNullValue());
+        assertThat(docMapper.mappers().fullName("b.a").mapper(), instanceOf(StringFieldMapper.class));
+        assertThat(docMapper.mappers().fullName("b.a").mapper().fieldType().indexed(), equalTo(true));
+        assertThat(docMapper.mappers().fullName("b.a").mapper().fieldType().stored(), equalTo(false));
+        assertThat(docMapper.mappers().fullName("b.a").mapper().fieldType().tokenized(), equalTo(false));
+
+        json = jsonBuilder().startObject()
+                .field("_id", "1")
+                .field("b", "complete me")
+                .endObject().bytes();
+        doc = docMapper.parse(json).rootDoc();
+
+        f = doc.getField("b");
+        assertThat(f, notNullValue());
+        assertThat(f.name(), equalTo("b"));
+        assertThat(f.stringValue(), equalTo("complete me"));
+        assertThat(f.fieldType().stored(), equalTo(false));
+        assertThat(f.fieldType().indexed(), equalTo(true));
+
+        f = doc.getField("b.a");
+        assertThat(f, notNullValue());
+        assertThat(f.name(), equalTo("b.a"));
+        assertThat(f.stringValue(), equalTo("complete me"));
+        assertThat(f.fieldType().stored(), equalTo(false));
+        assertThat(f.fieldType().indexed(), equalTo(true));
     }
 }

1abf919b8955cfc23fac48a39d225bed1ec80731
Adrien Grand <jpountz@gmail.com>
2014-02-04 11:52:45 +0100

    Add script support to value_count aggregations.
    Close #5001

--- src/main/java/org/elasticsearch/search/aggregations/metrics/valuecount/ValueCountBuilder.java
+++ src/main/java/org/elasticsearch/search/aggregations/metrics/valuecount/ValueCountBuilder.java
@@ -19,31 +19,15 @@
 
 package org.elasticsearch.search.aggregations.metrics.valuecount;
 
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.search.aggregations.metrics.MetricsAggregationBuilder;
-
-import java.io.IOException;
+import org.elasticsearch.search.aggregations.metrics.ValuesSourceMetricsAggregationBuilder;
 
 /**
  *
  */
-public class ValueCountBuilder extends MetricsAggregationBuilder<ValueCountBuilder> {
-
-    private String field;
+public class ValueCountBuilder extends ValuesSourceMetricsAggregationBuilder<ValueCountBuilder> {
 
     public ValueCountBuilder(String name) {
         super(name, InternalValueCount.TYPE.name());
     }
 
-    public ValueCountBuilder field(String field) {
-        this.field = field;
-        return this;
-    }
-
-    @Override
-    protected void internalXContent(XContentBuilder builder, Params params) throws IOException {
-        if (field != null) {
-            builder.field("field", field);
-        }
-    }
 }

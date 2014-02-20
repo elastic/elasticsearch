968d372c99668ed2a4f5b2528c06d0ff66573bd8
Isabel Drost-Fromm <isabel.drostfromm@elasticsearch.com>
2014-02-20 14:35:57 +0100

    Fixing license headers.

--- src/main/java/org/elasticsearch/index/query/TemplateQueryBuilder.java
+++ src/main/java/org/elasticsearch/index/query/TemplateQueryBuilder.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to Elasticsearch under one or more contributor
  * license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright

8aa6d01f3fb4602d0e0cd728a26a23051d536e45
Zachary Tong <zacharyjtong@gmail.com>
2014-02-21 16:50:50 -0500

    Enable filter profiling in Visitor

--- src/main/java/org/elasticsearch/common/lucene/search/profile/ProfileQueryVisitor.java
+++ src/main/java/org/elasticsearch/common/lucene/search/profile/ProfileQueryVisitor.java
@@ -1,18 +1,20 @@
 package org.elasticsearch.common.lucene.search.profile;
 
 
+import org.apache.lucene.queryparser.surround.query.AndQuery;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Filter;
 import org.apache.lucene.search.Query;
 import org.elasticsearch.common.lucene.search.XFilteredQuery;
 
 /**
  * This class walks a query and wraps all applicable queries in ProfileQuery queries
  */
-public class ProfileQueryVisitor extends Visitor<Query, ProfileQuery> {
+public class ProfileQueryVisitor extends Visitor<Object, ProfileComponent> {
 
     public ProfileQueryVisitor() {
-        super(ProfileQueryVisitor.class, Query.class, ProfileQuery.class);
+        super(ProfileQueryVisitor.class, Object.class, ProfileComponent.class);
     }
 
     public ProfileQuery visit(BooleanQuery booleanQuery) {
@@ -21,7 +23,7 @@ public class ProfileQueryVisitor extends Visitor<Query, ProfileQuery> {
         BooleanQuery newQuery = new BooleanQuery(booleanQuery.isCoordDisabled());
 
         for (BooleanClause clause : booleanQuery.clauses()) {
-            ProfileQuery pQuery = apply(clause.getQuery());
+            ProfileQuery pQuery = (ProfileQuery) apply(clause.getQuery());
             newQuery.add(pQuery, clause.getOccur());
         }
 
@@ -29,11 +31,18 @@ public class ProfileQueryVisitor extends Visitor<Query, ProfileQuery> {
     }
 
     public ProfileQuery visit(XFilteredQuery query) {
-        XFilteredQuery newQuery = new XFilteredQuery(apply(query.getQuery()), query.getFilter());
+        ProfileQuery pQuery = (ProfileQuery) apply(query.getQuery());
+        ProfileFilter pFilter = (ProfileFilter) apply(query.getFilter());
+
+        XFilteredQuery newQuery = new XFilteredQuery(pQuery, pFilter);
         return new ProfileQuery(newQuery);
     }
 
     public ProfileQuery visit(Query query) {
         return new ProfileQuery(query);
+    }
+
+    public ProfileFilter visit(Filter filter) {
+        return new ProfileFilter(filter);
     }
 }

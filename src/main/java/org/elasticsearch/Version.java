26feed79983063ae83bfa11bd4ce214b1f45c884
javanna <cavannaluca@gmail.com>
2014-02-25 15:37:55 +0000

    release [0.90.12]

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -145,7 +145,7 @@ public class Version implements Serializable {
     public static final int V_0_90_11_ID = /*00*/901199;
     public static final Version V_0_90_11 = new Version(V_0_90_11_ID, false, org.apache.lucene.util.Version.LUCENE_46);
     public static final int V_0_90_12_ID = /*00*/901299;
-    public static final Version V_0_90_12 = new Version(V_0_90_12_ID, true, org.apache.lucene.util.Version.LUCENE_46);
+    public static final Version V_0_90_12 = new Version(V_0_90_12_ID, false, org.apache.lucene.util.Version.LUCENE_46);
 
     public static final Version CURRENT = V_0_90_12;
 

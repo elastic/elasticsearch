bc5281109288a28968fefa4f78eaee651ead7318
Costin Leau <costin.leau@gmail.com>
2014-02-28 16:19:23 +0200

    fix failing IsolatedPluginTests (change numNodes to 0)
    (cherry picked from commit f26a369a78ade032df44fcf331f378644f74b9ea)

--- src/test/java/org/elasticsearch/plugins/IsolatedPluginTests.java
+++ src/test/java/org/elasticsearch/plugins/IsolatedPluginTests.java
@@ -43,7 +43,7 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 
 // NB: the tests uses System Properties to pass the information from different plugins (loaded in separate CLs) to the test.
 // hence the use of try/finally blocks to clean these up after the test has been executed as otherwise the test framework will trigger a failure
-@ClusterScope(scope = Scope.TEST, numNodes = 1)
+@ClusterScope(scope = Scope.TEST, numNodes = 0)
 public class IsolatedPluginTests extends ElasticsearchIntegrationTest {
 
     private static final Settings SETTINGS;
@@ -113,7 +113,7 @@ public class IsolatedPluginTests extends ElasticsearchIntegrationTest {
     @Test
     public void testIsolatedPluginProperties() throws Exception {
         try {
-            cluster().client();
+            client();
             Properties p = System.getProperties();
             assertThat(p.getProperty("es.test.isolated.plugin.count"), equalTo("2"));
             String prop = p.getProperty("es.test.isolated.plugin.instantiated.hashes");

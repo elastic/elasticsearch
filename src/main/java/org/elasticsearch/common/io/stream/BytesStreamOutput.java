68cb9a8c4d4edfc161805f37555f0c9c7bf10447
Holger Hoffstätte <holger.hoffstaette@googlemail.com>
2014-03-04 13:55:34 +0100

    Rewrite BytesStreamOutput on top of BigArrays/ByteArray.
    Fix for #5159

--- src/main/java/org/elasticsearch/common/io/stream/BytesStreamOutput.java
+++ src/main/java/org/elasticsearch/common/io/stream/BytesStreamOutput.java
@@ -19,38 +19,52 @@
 
 package org.elasticsearch.common.io.stream;
 
-import org.apache.lucene.util.ArrayUtil;
+import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.io.BytesStream;
+import org.elasticsearch.common.util.BigArrays;
+import org.elasticsearch.common.util.ByteArray;
 
 import java.io.IOException;
 
 /**
- *
+ * A @link {@link StreamOutput} that uses{@link BigArrays} to acquire pages of
+ * bytes, which avoids frequent reallocation & copying of the internal data.
  */
 public class BytesStreamOutput extends StreamOutput implements BytesStream {
 
-    public static final int DEFAULT_SIZE = 2 * 1024;
-
-    public static final int OVERSIZE_LIMIT = 256 * 1024;
+    /**
+     * Factory/manager for our ByteArray
+     */
+    private final BigArrays bigarrays;
 
     /**
-     * The buffer where data is stored.
+     * The internal list of pages.
      */
-    protected byte buf[];
+    private ByteArray bytes;
 
     /**
      * The number of valid bytes in the buffer.
      */
-    protected int count;
+    private int count;
 
+    /**
+     * Create a nonrecycling {@link BytesStreamOutput} with 1 initial page acquired.
+     */
     public BytesStreamOutput() {
-        this(DEFAULT_SIZE);
+        this(BigArrays.PAGE_SIZE_IN_BYTES);
     }
 
-    public BytesStreamOutput(int size) {
-        this.buf = new byte[size];
+    /**
+     * Create a nonrecycling {@link BytesStreamOutput} with enough initial pages acquired
+     * to satisfy the capacity given by {@link expectedSize}.
+     * 
+     * @param expectedSize the expected maximum size of the stream in bytes.
+     */
+    public BytesStreamOutput(int expectedSize) {
+        bigarrays = BigArrays.NON_RECYCLING_INSTANCE;
+        bytes = bigarrays.newByteArray(expectedSize);
     }
 
     @Override
@@ -64,87 +78,89 @@ public class BytesStreamOutput extends StreamOutput implements BytesStream {
     }
 
     @Override
-    public void seek(long position) throws IOException {
-        if (position > Integer.MAX_VALUE) {
-            throw new UnsupportedOperationException();
-        }
-        count = (int) position;
-    }
-
-    @Override
     public void writeByte(byte b) throws IOException {
-        int newcount = count + 1;
-        if (newcount > buf.length) {
-            buf = grow(newcount);
-        }
-        buf[count] = b;
-        count = newcount;
-    }
-
-    public void skip(int length) {
-        int newcount = count + length;
-        if (newcount > buf.length) {
-            buf = grow(newcount);
-        }
-        count = newcount;
+        ensureCapacity(count+1);
+        bytes.set(count, b);
+        count++;
     }
 
     @Override
     public void writeBytes(byte[] b, int offset, int length) throws IOException {
+        // nothing to copy
         if (length == 0) {
             return;
         }
-        int newcount = count + length;
-        if (newcount > buf.length) {
-            buf = grow(newcount);
-        }
-        System.arraycopy(b, offset, buf, count, length);
-        count = newcount;
-    }
 
-    private byte[] grow(int newCount) {
-        // try and grow faster while we are small...
-        if (newCount < OVERSIZE_LIMIT) {
-            newCount = Math.max(buf.length << 1, newCount);
+        // illegal args: offset and/or length exceed array size
+        if (b.length < (offset + length)) {
+            throw new IllegalArgumentException("Illegal offset " + offset + "/length " + length + " for byte[] of length " + b.length);
         }
-        return ArrayUtil.grow(buf, newCount);
-    }
 
-    public void seek(int seekTo) {
-        count = seekTo;
+        // get enough pages for new size
+        ensureCapacity(count+length);
+
+        // bulk copy
+        bytes.set(count, b, offset, length);
+
+        // advance
+        count += length;
     }
 
     public void reset() {
-        count = 0;
-    }
+        // shrink list of pages
+        if (bytes.size() > BigArrays.PAGE_SIZE_IN_BYTES) {
+            bytes = bigarrays.resize(bytes, BigArrays.PAGE_SIZE_IN_BYTES);
+        }
 
-    public int bufferSize() {
-        return buf.length;
+        // go back to start
+        count = 0;
     }
 
     @Override
     public void flush() throws IOException {
-        // nothing to do there
+        // nothing to do
+    }
+
+    @Override
+    public void seek(long position) throws IOException {
+        if (position > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("position " + position + " > Integer.MAX_VALUE");
+        }
+
+        count = (int)position;
+        ensureCapacity(count);
+    }
+
+    public void skip(int length) {
+        count += length;
+        ensureCapacity(count);
     }
 
     @Override
     public void close() throws IOException {
-        // nothing to do here
-    }
-
-    @Override
-    public BytesReference bytes() {
-        return new BytesArray(buf, 0, count);
+        // empty for now.
     }
 
     /**
      * Returns the current size of the buffer.
-     *
-     * @return the value of the <code>count</code> field, which is the number
-     *         of valid bytes in this output stream.
+     * 
+     * @return the value of the <code>count</code> field, which is the number of valid
+     *         bytes in this output stream.
      * @see java.io.ByteArrayOutputStream#count
      */
     public int size() {
         return count;
     }
+
+    @Override
+    public BytesReference bytes() {
+        BytesRef bref = new BytesRef();
+        bytes.get(0, count, bref);
+        return new BytesArray(bref, false);
+    }
+
+    private void ensureCapacity(int offset) {
+        bytes = bigarrays.grow(bytes, offset);
+    }
+
 }

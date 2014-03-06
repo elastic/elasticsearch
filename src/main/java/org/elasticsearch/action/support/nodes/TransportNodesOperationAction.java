56fa5458e622367c1475ff84e319571cd0221538
Shay Banon <kimchy@gmail.com>
2014-03-06 10:08:32 +0000

    Nicer failure message when node should not connect to another in nodes API There are cases where nodes should not connect to another node. For example, client nodes do not connect to other client nodes. When executing a nodes level API, we should have a nicer failure indicating that, and not log it by default, as its not a real failure

--- src/main/java/org/elasticsearch/action/support/nodes/TransportNodesOperationAction.java
+++ src/main/java/org/elasticsearch/action/support/nodes/TransportNodesOperationAction.java
@@ -163,6 +163,8 @@ public abstract class TransportNodesOperationAction<Request extends NodesOperati
                     } else {
                         if (node == null) {
                             onFailure(idx, nodeId, new NoSuchNodeException(nodeId));
+                        } else if (!clusterService.localNode().shouldConnectTo(node)) {
+                            onFailure(idx, nodeId, new NodeShouldNotConnectException(clusterService.localNode(), node));
                         } else {
                             NodeRequest nodeRequest = newNodeRequest(nodeId, request);
                             transportService.sendRequest(node, transportNodeAction, nodeRequest, transportRequestOptions, new BaseTransportResponseHandler<NodeResponse>() {
@@ -202,7 +204,7 @@ public abstract class TransportNodesOperationAction<Request extends NodesOperati
         }
 
         private void onFailure(int idx, String nodeId, Throwable t) {
-            if (logger.isDebugEnabled()) {
+            if (logger.isDebugEnabled() && !(t instanceof NodeShouldNotConnectException)) {
                 logger.debug("failed to execute on node [{}]", t, nodeId);
             }
             if (accumulateExceptions()) {

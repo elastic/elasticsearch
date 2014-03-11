5924e0890caaad6aaba08e160f6df83b88d07010
Derek Slife <derek@slife.net>
2014-03-11 14:17:31 -0700

    Corrected issue with throttle type setting not respected upon updates

--- src/main/java/org/elasticsearch/index/store/support/AbstractIndexStore.java
+++ src/main/java/org/elasticsearch/index/store/support/AbstractIndexStore.java
@@ -45,7 +45,7 @@ public abstract class AbstractIndexStore extends AbstractIndexComponent implemen
     class ApplySettings implements IndexSettingsService.Listener {
         @Override
         public void onRefreshSettings(Settings settings) {
-            String rateLimitingType = indexSettings.get(INDEX_STORE_THROTTLE_TYPE, AbstractIndexStore.this.rateLimitingType);
+            String rateLimitingType = settings.get(INDEX_STORE_THROTTLE_TYPE, AbstractIndexStore.this.rateLimitingType);
             if (!rateLimitingType.equals(AbstractIndexStore.this.rateLimitingType)) {
                 logger.info("updating index.store.throttle.type from [{}] to [{}]", AbstractIndexStore.this.rateLimitingType, rateLimitingType);
                 if (rateLimitingType.equalsIgnoreCase("node")) {

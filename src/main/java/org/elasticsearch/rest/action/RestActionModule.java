a7cadf71e3901c18df1f8a78a857a195d6e84067
Zachary Tong <zacharyjtong@gmail.com>
2014-03-17 15:09:47 -0400

    Add profile endpoint

--- src/main/java/org/elasticsearch/rest/action/RestActionModule.java
+++ src/main/java/org/elasticsearch/rest/action/RestActionModule.java
@@ -94,6 +94,7 @@ import org.elasticsearch.rest.action.main.RestMainAction;
 import org.elasticsearch.rest.action.mlt.RestMoreLikeThisAction;
 import org.elasticsearch.rest.action.percolate.RestMultiPercolateAction;
 import org.elasticsearch.rest.action.percolate.RestPercolateAction;
+import org.elasticsearch.rest.action.profile.RestProfileAction;
 import org.elasticsearch.rest.action.search.RestClearScrollAction;
 import org.elasticsearch.rest.action.search.RestMultiSearchAction;
 import org.elasticsearch.rest.action.search.RestSearchAction;
@@ -201,6 +202,7 @@ public class RestActionModule extends AbstractModule {
         bind(RestMultiPercolateAction.class).asEagerSingleton();
 
         bind(RestSearchAction.class).asEagerSingleton();
+        bind(RestProfileAction.class).asEagerSingleton();
         bind(RestSearchScrollAction.class).asEagerSingleton();
         bind(RestClearScrollAction.class).asEagerSingleton();
         bind(RestMultiSearchAction.class).asEagerSingleton();

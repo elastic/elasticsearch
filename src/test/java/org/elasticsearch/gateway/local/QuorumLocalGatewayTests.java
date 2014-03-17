2a31bd83ef7fc80a3407da6205e04a7e16e846d3
Boaz Leskes <b.leskes@gmail.com>
2014-03-17 11:01:43 +0100

    [Tests] QuorumLocalGatewayTests - Set mini_master_nodes earlier to make sure it has effect.  ClusterServiceTests - remove a thread.sleep in favor of awaitBusy

--- src/test/java/org/elasticsearch/gateway/local/QuorumLocalGatewayTests.java
+++ src/test/java/org/elasticsearch/gateway/local/QuorumLocalGatewayTests.java
@@ -129,6 +129,8 @@ public class QuorumLocalGatewayTests extends ElasticsearchIntegrationTest {
         cluster().startNode(settingsBuilder().put("gateway.type", "local").build());
         cluster().startNode(settingsBuilder().put("gateway.type", "local").build());
         cluster().startNode(settingsBuilder().put("gateway.type", "local").build());
+        // we are shutting down nodes - make sure we don't have 2 clusters if we test network
+        setMinimumMasterNodes(2);
 
         createIndex("test");
         ensureGreen();
@@ -145,9 +147,6 @@ public class QuorumLocalGatewayTests extends ElasticsearchIntegrationTest {
         for (int i = 0; i < 10; i++) {
             assertHitCount(client().prepareCount().setQuery(matchAllQuery()).get(), 2l);
         }
-        client().admin().cluster().prepareUpdateSettings().setTransientSettings(settingsBuilder()
-                .put("discovery.zen.minimum_master_nodes", 2)) // we are shutting down nodes - make sure we don't have 2 clusters if we test network
-                .get();
         logger.info("--> restart all nodes");
         cluster().fullRestart(new RestartCallback() {
             @Override

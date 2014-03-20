9e0ca46d3fc889c6896c32a26319ecd421bf428b
Simon Willnauer <simonw@apache.org>
2014-03-20 13:05:04 +0100

    Add BoolFilterBuilder#hasClauses to be consitent with BoolQueryBuilder
    Closes #5472

--- src/main/java/org/elasticsearch/index/query/BoolQueryBuilder.java
+++ src/main/java/org/elasticsearch/index/query/BoolQueryBuilder.java
@@ -119,10 +119,11 @@ public class BoolQueryBuilder extends BaseQueryBuilder implements BoostableQuery
     }
 
     /**
-     * Return <code>true</code> if the query being built has no clause yet
+     * Returns <code>true</code> iff this query builder has at least one should, must or mustNot clause.
+     * Otherwise <code>false</code>.
      */
     public boolean hasClauses() {
-        return !mustClauses.isEmpty() || !mustNotClauses.isEmpty() || !shouldClauses.isEmpty();
+        return !(mustClauses.isEmpty() && shouldClauses.isEmpty() && mustNotClauses.isEmpty());
     }
     
     /**

b70399b5629d2ab6a5f834aa916f6003fec429ee
Simon Willnauer <simonw@apache.org>
2014-03-20 17:16:04 +0100

    Add simple escape method for special characters to template query
    The default mustache engine was using HTML escaping which breaks queries
    if used with JSON etc. This commit adds escaping for:
    
    ```
    \b  Backspace (ascii code 08)
    \f  Form feed (ascii code 0C)
    \n  New line
    \r  Carriage return
    \t  Tab
    \v  Vertical tab
    \"  Double quote
    \\  Backslash
    ```
    
    Closes #5473

--- src/test/java/org/elasticsearch/script/mustache/MustacheScriptEngineTest.java
+++ src/test/java/org/elasticsearch/script/mustache/MustacheScriptEngineTest.java
@@ -18,24 +18,26 @@
  */
 package org.elasticsearch.script.mustache;
 
+import com.carrotsearch.randomizedtesting.generators.RandomPicks;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.test.ElasticsearchTestCase;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.equalTo;
+
 /**
  * Mustache based templating test
- * */
+ */
 public class MustacheScriptEngineTest extends ElasticsearchTestCase {
     private MustacheScriptEngineService qe;
-
-    private static String TEMPLATE = "GET _search {\"query\": " + "{\"boosting\": {" + "\"positive\": {\"match\": {\"body\": \"gift\"}},"
-            + "\"negative\": {\"term\": {\"body\": {\"value\": \"solr\"}" + "}}, \"negative_boost\": {{boost_val}} } }}";
 
     @Before
     public void setup() {
@@ -44,12 +46,70 @@ public class MustacheScriptEngineTest extends ElasticsearchTestCase {
 
     @Test
     public void testSimpleParameterReplace() {
-        Map<String, Object> vars = new HashMap<String, Object>();
-        vars.put("boost_val", "0.3");
-        BytesReference o = (BytesReference) qe.execute(qe.compile(TEMPLATE), vars);
-        assertEquals("GET _search {\"query\": {\"boosting\": {\"positive\": {\"match\": {\"body\": \"gift\"}},"
-                + "\"negative\": {\"term\": {\"body\": {\"value\": \"solr\"}}}, \"negative_boost\": 0.3 } }}",
-                new String(o.toBytes(), Charset.forName("UTF-8")));
+        {
+            String template = "GET _search {\"query\": " + "{\"boosting\": {" + "\"positive\": {\"match\": {\"body\": \"gift\"}},"
+                    + "\"negative\": {\"term\": {\"body\": {\"value\": \"solr\"}" + "}}, \"negative_boost\": {{boost_val}} } }}";
+            Map<String, Object> vars = new HashMap<String, Object>();
+            vars.put("boost_val", "0.3");
+            BytesReference o = (BytesReference) qe.execute(qe.compile(template), vars);
+            assertEquals("GET _search {\"query\": {\"boosting\": {\"positive\": {\"match\": {\"body\": \"gift\"}},"
+                    + "\"negative\": {\"term\": {\"body\": {\"value\": \"solr\"}}}, \"negative_boost\": 0.3 } }}",
+                    new String(o.toBytes(), Charset.forName("UTF-8")));
+        }
+        {
+            String template = "GET _search {\"query\": " + "{\"boosting\": {" + "\"positive\": {\"match\": {\"body\": \"gift\"}},"
+                    + "\"negative\": {\"term\": {\"body\": {\"value\": \"{{body_val}}\"}" + "}}, \"negative_boost\": {{boost_val}} } }}";
+            Map<String, Object> vars = new HashMap<String, Object>();
+            vars.put("boost_val", "0.3");
+            vars.put("body_val", "\"quick brown\"");
+            BytesReference o = (BytesReference) qe.execute(qe.compile(template), vars);
+            assertEquals("GET _search {\"query\": {\"boosting\": {\"positive\": {\"match\": {\"body\": \"gift\"}},"
+                    + "\"negative\": {\"term\": {\"body\": {\"value\": \"\\\"quick brown\\\"\"}}}, \"negative_boost\": 0.3 } }}",
+                    new String(o.toBytes(), Charset.forName("UTF-8")));
+        }
+    }
+
+    @Test
+    public void testEscapeJson() throws IOException {
+        {
+            StringWriter writer = new StringWriter();
+            JsonEscapingMustacheFactory.escape("hello \n world", writer);
+            assertThat(writer.toString(), equalTo("hello \\\n world"));
+        }
+        {
+            StringWriter writer = new StringWriter();
+            JsonEscapingMustacheFactory.escape("\n", writer);
+            assertThat(writer.toString(), equalTo("\\\n"));
+        }
+
+        Character[] specialChars = new Character[]{'\f', '\n', '\r', '"', '\\', (char) 11, '\t', '\b' };
+        int iters = scaledRandomIntBetween(100, 1000);
+        for (int i = 0; i < iters; i++) {
+            int rounds = scaledRandomIntBetween(1, 20);
+            StringWriter escaped = new StringWriter();
+            StringWriter writer = new StringWriter();
+            for (int j = 0; j < rounds; j++) {
+                String s = getChars();
+                writer.write(s);
+                escaped.write(s);
+                char c = RandomPicks.randomFrom(getRandom(), specialChars);
+                writer.append(c);
+                escaped.append('\\');
+                escaped.append(c);
+            }
+            StringWriter target = new StringWriter();
+            assertThat(escaped.toString(), equalTo(JsonEscapingMustacheFactory.escape(writer.toString(), target).toString()));
+        }
+    }
+
+    private String getChars() {
+        String string = randomRealisticUnicodeOfCodepointLengthBetween(0, 10);
+        for (int i = 0; i < string.length(); i++) {
+            if (JsonEscapingMustacheFactory.isEscapeChar(string.charAt(i))) {
+                return string.substring(0, i);
+            }
+        }
+        return string;
     }
 
 }

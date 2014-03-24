2a7b965caa3d8676124250a36943b56bbe8b3cf2
Adrien Grand <jpountz@gmail.com>
2014-03-24 16:03:39 +0100

    BytesReference.Helper should never materialize a byte[] array.
    The current implementations of BytesReference.Helper.bytesEquals and
    bytesHashCode materialize a byte[] when passed an instance that returns `false`
    in `hasArray()`. They should instead do a byte-by-byte comparison/hashcode
    computation without materializing the array.
    
    Close #5517

--- src/main/java/org/elasticsearch/common/util/UnsafeUtils.java
+++ src/main/java/org/elasticsearch/common/util/UnsafeUtils.java
@@ -73,13 +73,18 @@ public enum UnsafeUtils {
 
     /** Compare the two given {@link BytesRef}s for equality. */
     public static boolean equals(BytesRef b1, BytesRef b2) {
-        int len = b1.length;
-        if (b2.length != len) {
+        if (b1.length != b2.length) {
             return false;
         }
-        int o1 = b1.offset, o2 = b2.offset;
+        return equals(b1.bytes, b1.offset, b2.bytes, b2.offset, b1.length);
+    }
+
+    /**
+     * Compare <code>b1[o1:o1+len)</code>against <code>b1[o2:o2+len)</code>.
+     */
+    public static boolean equals(byte[] b1, int o1, byte[] b2, int o2, int len) {
         while (len >= 8) {
-            if (readLong(b1.bytes, o1) != readLong(b2.bytes, o2)) {
+            if (readLong(b1, o1) != readLong(b2, o2)) {
                 return false;
             }
             len -= 8;
@@ -87,7 +92,7 @@ public enum UnsafeUtils {
             o2 += 8;
         }
         if (len >= 4) {
-            if (readInt(b1.bytes, o1) != readInt(b2.bytes, o2)) {
+            if (readInt(b1, o1) != readInt(b2, o2)) {
                 return false;
             }
             len -= 4;
@@ -95,7 +100,7 @@ public enum UnsafeUtils {
             o2 += 4;
         }
         if (len >= 2) {
-            if (readShort(b1.bytes, o1) != readShort(b2.bytes, o2)) {
+            if (readShort(b1, o1) != readShort(b2, o2)) {
                 return false;
             }
             len -= 2;
@@ -103,7 +108,7 @@ public enum UnsafeUtils {
             o2 += 2;
         }
         if (len == 1) {
-            if (readByte(b1.bytes, o1) != readByte(b2.bytes, o2)) {
+            if (readByte(b1, o1) != readByte(b2, o2)) {
                 return false;
             }
         } else {

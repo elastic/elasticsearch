6e68a8ae8907fa4afb1089bf402b6be043568847
Boaz Leskes <b.leskes@gmail.com>
2014-03-25 17:35:21 +0100

    Bumped version to 0.90.14-SNAP

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -148,8 +148,10 @@ public class Version implements Serializable {
     public static final Version V_0_90_12 = new Version(V_0_90_12_ID, false, org.apache.lucene.util.Version.LUCENE_46);
     public static final int V_0_90_13_ID = /*00*/901399;
     public static final Version V_0_90_13 = new Version(V_0_90_13_ID, false, org.apache.lucene.util.Version.LUCENE_46);
+    public static final int V_0_90_14_ID = /*00*/901499;
+    public static final Version V_0_90_14 = new Version(V_0_90_14_ID, true, org.apache.lucene.util.Version.LUCENE_46);
 
-    public static final Version CURRENT = V_0_90_13;
+    public static final Version CURRENT = V_0_90_14;
 
     static {
         assert CURRENT.luceneVersion == Lucene.VERSION : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";

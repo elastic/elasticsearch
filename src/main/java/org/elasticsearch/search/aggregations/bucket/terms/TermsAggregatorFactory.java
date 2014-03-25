31b2e7024c31de19839805d7a0c488382d726abc
Adrien Grand <jpountz@gmail.com>
2014-03-25 10:13:42 +0100

    Refactor common code for unmapped aggregators into `NonCollectingAggregator`.
    Our aggregators that are dedicated to unmapped fields all look more or less
    the same so this hopefully helps remove some spaghetti code.
    
    Close #5528

--- src/main/java/org/elasticsearch/search/aggregations/bucket/terms/TermsAggregatorFactory.java
+++ src/main/java/org/elasticsearch/search/aggregations/bucket/terms/TermsAggregatorFactory.java
@@ -21,8 +21,7 @@ package org.elasticsearch.search.aggregations.bucket.terms;
 import org.apache.lucene.index.AtomicReaderContext;
 import org.elasticsearch.ElasticsearchIllegalArgumentException;
 import org.elasticsearch.common.ParseField;
-import org.elasticsearch.search.aggregations.AggregationExecutionException;
-import org.elasticsearch.search.aggregations.Aggregator;
+import org.elasticsearch.search.aggregations.*;
 import org.elasticsearch.search.aggregations.Aggregator.BucketAggregationMode;
 import org.elasticsearch.search.aggregations.AggregatorFactories;
 import org.elasticsearch.search.aggregations.bucket.terms.support.IncludeExclude;
@@ -107,7 +106,13 @@ public class TermsAggregatorFactory extends ValueSourceAggregatorFactory {
 
     @Override
     protected Aggregator createUnmapped(AggregationContext aggregationContext, Aggregator parent) {
-        return new UnmappedTermsAggregator(name, order, requiredSize, minDocCount, aggregationContext, parent);
+        final InternalAggregation aggregation = new UnmappedTerms(name, order, requiredSize, minDocCount);
+        return new NonCollectingAggregator(name, aggregationContext, parent) {
+            @Override
+            public InternalAggregation buildEmptyAggregation() {
+                return aggregation;
+            }
+        };
     }
 
     private static boolean hasParentBucketAggregator(Aggregator parent) {

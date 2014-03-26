96c280f43ddb96bc0f72849fd47c0347a9a96a6f
David Pilato <david@pilato.fr>
2014-03-26 19:33:12 +0100

    Update to elasticsearch 1.1.0
    Closes #11.

--- src/main/java/org/elasticsearch/discovery/azure/AzureDiscovery.java
+++ src/main/java/org/elasticsearch/discovery/azure/AzureDiscovery.java
@@ -28,6 +28,7 @@ import org.elasticsearch.common.collect.ImmutableList;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.network.NetworkService;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.discovery.DiscoverySettings;
 import org.elasticsearch.discovery.zen.ZenDiscovery;
 import org.elasticsearch.discovery.zen.ping.ZenPing;
 import org.elasticsearch.discovery.zen.ping.ZenPingService;
@@ -46,8 +47,10 @@ public class AzureDiscovery extends ZenDiscovery {
     @Inject
     public AzureDiscovery(Settings settings, ClusterName clusterName, ThreadPool threadPool, TransportService transportService,
                           ClusterService clusterService, NodeSettingsService nodeSettingsService, ZenPingService pingService,
-                          DiscoveryNodeService discoveryNodeService, AzureComputeService azureService, NetworkService networkService) {
-        super(settings, clusterName, threadPool, transportService, clusterService, nodeSettingsService, discoveryNodeService, pingService, Version.CURRENT);
+                          DiscoveryNodeService discoveryNodeService, AzureComputeService azureService, NetworkService networkService,
+                          DiscoverySettings discoverySettings) {
+        super(settings, clusterName, threadPool, transportService, clusterService, nodeSettingsService,
+                discoveryNodeService, pingService, Version.CURRENT, discoverySettings);
         if (settings.getAsBoolean("cloud.enabled", true)) {
             ImmutableList<? extends ZenPing> zenPings = pingService.zenPings();
             UnicastZenPing unicastZenPing = null;

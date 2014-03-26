7f271fd37aff3ad4c03a0d1cc418ed3dca592cbb
Konrad Beiske <konrad@beiske.org>
2014-03-26 18:52:22 +0100

    Add per repository credentials
    Changed AwsS3Service to use one client per region and credentials combination.
    Made S3Repository specify credentials if such exists in the repository settings.
    
    Updated readme with repository specific credentials settings.
    
    Closes #54.
    Closes #55.
    Closes #56.
    (cherry picked from commit d4ea2dd)

--- src/test/java/org/elasticsearch/repositories/s3/S3SnapshotRestoreTest.java
+++ src/test/java/org/elasticsearch/repositories/s3/S3SnapshotRestoreTest.java
@@ -23,6 +23,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.DeleteObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+
 import org.elasticsearch.action.admin.cluster.repositories.put.PutRepositoryResponse;
 import org.elasticsearch.action.admin.cluster.snapshots.create.CreateSnapshotResponse;
 import org.elasticsearch.action.admin.cluster.snapshots.restore.RestoreSnapshotResponse;
@@ -33,6 +34,7 @@ import org.elasticsearch.cloud.aws.AwsS3Service;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.concurrent.UncategorizedExecutionException;
 import org.elasticsearch.repositories.RepositoryMissingException;
 import org.elasticsearch.snapshots.SnapshotState;
 import org.elasticsearch.test.ElasticsearchIntegrationTest.ClusterScope;
@@ -50,7 +52,7 @@ import static org.hamcrest.Matchers.greaterThan;
 /**
  */
 @AwsTest
-@ClusterScope(scope = Scope.TEST, numNodes = 2)
+@ClusterScope(scope = Scope.SUITE, numNodes = 2)
 public class S3SnapshotRestoreTest extends AbstractAwsTest {
 
     @Override
@@ -152,6 +154,117 @@ public class S3SnapshotRestoreTest extends AbstractAwsTest {
     }
 
     /**
+     * This test verifies that the test configuration is set up in a manner that
+     * does not make the test {@link #testRepositoryWithCustomCredentials()} pointless.
+     */
+    @Test(expected = UncategorizedExecutionException.class)
+    public void assertRepositoryWithCustomCredentialsIsNotAccessibleByDefaultCredentials() {
+        Client client = client();
+        Settings bucketSettings = cluster().getInstance(Settings.class).getByPrefix("repositories.s3.private-bucket.");
+        logger.info("-->  creating s3 repository with bucket[{}] and path [{}]", bucketSettings.get("bucket"), basePath);
+        PutRepositoryResponse putRepositoryResponse = client.admin().cluster().preparePutRepository("test-repo")
+                .setType("s3").setSettings(ImmutableSettings.settingsBuilder()
+                        .put("base_path", basePath)
+                        .put("bucket", bucketSettings.get("bucket"))
+                ).get();
+        assertThat(putRepositoryResponse.isAcknowledged(), equalTo(true));
+
+        assertRepositoryIsOperational(client, "test-repo");
+    }
+
+    @Test
+    public void testRepositoryWithCustomCredentials() {
+        Client client = client();
+        Settings bucketSettings = cluster().getInstance(Settings.class).getByPrefix("repositories.s3.private-bucket.");
+        logger.info("-->  creating s3 repository with bucket[{}] and path [{}]", bucketSettings.get("bucket"), basePath);
+        PutRepositoryResponse putRepositoryResponse = client.admin().cluster().preparePutRepository("test-repo")
+                .setType("s3").setSettings(ImmutableSettings.settingsBuilder()
+                        .put("base_path", basePath)
+                        .put("access_key", bucketSettings.get("access_key"))
+                        .put("secret_key", bucketSettings.get("secret_key"))
+                        .put("bucket", bucketSettings.get("bucket"))
+                ).get();
+        assertThat(putRepositoryResponse.isAcknowledged(), equalTo(true));
+
+        assertRepositoryIsOperational(client, "test-repo");
+    }
+
+    /**
+     * This test verifies that the test configuration is set up in a manner that
+     * does not make the test {@link #testRepositoryInRemoteRegion()} pointless.
+     */
+    @Test(expected = UncategorizedExecutionException.class)
+    public void assertRepositoryInRemoteRegionIsRemote() {
+        Client client = client();
+        Settings bucketSettings = cluster().getInstance(Settings.class).getByPrefix("repositories.s3.remote-bucket.");
+        logger.info("-->  creating s3 repository with bucket[{}] and path [{}]", bucketSettings.get("bucket"), basePath);
+        PutRepositoryResponse putRepositoryResponse = client.admin().cluster().preparePutRepository("test-repo")
+                .setType("s3").setSettings(ImmutableSettings.settingsBuilder()
+                        .put("base_path", basePath)
+                        .put("bucket", bucketSettings.get("bucket"))
+// Below setting intentionally omitted to assert bucket is not available in default region.
+//                        .put("region", privateBucketSettings.get("region"))
+                ).get();
+        assertThat(putRepositoryResponse.isAcknowledged(), equalTo(true));
+
+        assertRepositoryIsOperational(client, "test-repo");
+    }
+
+    @Test
+    public void testRepositoryInRemoteRegion() {
+        Client client = client();
+        Settings settings = cluster().getInstance(Settings.class);
+        Settings bucketSettings = settings.getByPrefix("repositories.s3.remote-bucket.");
+        logger.info("-->  creating s3 repository with bucket[{}] and path [{}]", bucketSettings.get("bucket"), basePath);
+        PutRepositoryResponse putRepositoryResponse = client.admin().cluster().preparePutRepository("test-repo")
+                .setType("s3").setSettings(ImmutableSettings.settingsBuilder()
+                        .put("base_path", basePath)
+                        .put("bucket", bucketSettings.get("bucket"))
+                        .put("region", bucketSettings.get("region"))
+                ).get();
+        assertThat(putRepositoryResponse.isAcknowledged(), equalTo(true));
+
+        assertRepositoryIsOperational(client, "test-repo");
+    }
+
+    private void assertRepositoryIsOperational(Client client, String repository) {
+        createIndex("test-idx-1");
+        ensureGreen();
+
+        logger.info("--> indexing some data");
+        for (int i = 0; i < 100; i++) {
+            index("test-idx-1", "doc", Integer.toString(i), "foo", "bar" + i);
+        }
+        refresh();
+        assertThat(client.prepareCount("test-idx-1").get().getCount(), equalTo(100L));
+
+        logger.info("--> snapshot");
+        CreateSnapshotResponse createSnapshotResponse = client.admin().cluster().prepareCreateSnapshot(repository, "test-snap").setWaitForCompletion(true).setIndices("test-idx-*").get();
+        assertThat(createSnapshotResponse.getSnapshotInfo().successfulShards(), greaterThan(0));
+        assertThat(createSnapshotResponse.getSnapshotInfo().successfulShards(), equalTo(createSnapshotResponse.getSnapshotInfo().totalShards()));
+
+        assertThat(client.admin().cluster().prepareGetSnapshots(repository).setSnapshots("test-snap").get().getSnapshots().get(0).state(), equalTo(SnapshotState.SUCCESS));
+
+        logger.info("--> delete some data");
+        for (int i = 0; i < 50; i++) {
+            client.prepareDelete("test-idx-1", "doc", Integer.toString(i)).get();
+        }
+        refresh();
+        assertThat(client.prepareCount("test-idx-1").get().getCount(), equalTo(50L));
+
+        logger.info("--> close indices");
+        client.admin().indices().prepareClose("test-idx-1").get();
+
+        logger.info("--> restore all indices from the snapshot");
+        RestoreSnapshotResponse restoreSnapshotResponse = client.admin().cluster().prepareRestoreSnapshot(repository, "test-snap").setWaitForCompletion(true).execute().actionGet();
+        assertThat(restoreSnapshotResponse.getRestoreInfo().totalShards(), greaterThan(0));
+
+        ensureGreen();
+        assertThat(client.prepareCount("test-idx-1").get().getCount(), equalTo(100L));
+    }
+
+
+    /**
      * Deletes repositories, supports wildcard notation.
      */
     public static void wipeRepositories(String... repositories) {
@@ -172,45 +285,55 @@ public class S3SnapshotRestoreTest extends AbstractAwsTest {
      * Deletes content of the repository files in the bucket
      */
     public void cleanRepositoryFiles(String basePath) {
-        String bucket = cluster().getInstance(Settings.class).get("repositories.s3.bucket");
-        AmazonS3 client = cluster().getInstance(AwsS3Service.class).client();
-        try {
-            ObjectListing prevListing = null;
-            //From http://docs.amazonwebservices.com/AmazonS3/latest/dev/DeletingMultipleObjectsUsingJava.html
-            //we can do at most 1K objects per delete
-            //We don't know the bucket name until first object listing
-            DeleteObjectsRequest multiObjectDeleteRequest = null;
-            ArrayList<DeleteObjectsRequest.KeyVersion> keys = new ArrayList<DeleteObjectsRequest.KeyVersion>();
-            while (true) {
-                ObjectListing list;
-                if (prevListing != null) {
-                    list = client.listNextBatchOfObjects(prevListing);
-                } else {
-                    list = client.listObjects(bucket, basePath);
-                    multiObjectDeleteRequest = new DeleteObjectsRequest(list.getBucketName());
-                }
-                for (S3ObjectSummary summary : list.getObjectSummaries()) {
-                    keys.add(new DeleteObjectsRequest.KeyVersion(summary.getKey()));
-                    //Every 500 objects batch the delete request
-                    if (keys.size() > 500) {
-                        multiObjectDeleteRequest.setKeys(keys);
-                        client.deleteObjects(multiObjectDeleteRequest);
+        Settings settings = cluster().getInstance(Settings.class);
+        Settings[] buckets = {
+                settings.getByPrefix("repositories.s3."),
+                settings.getByPrefix("repositories.s3.private-bucket."),
+                settings.getByPrefix("repositories.s3.remote-bucket.")
+            };
+        for (Settings bucket : buckets) {
+            AmazonS3 client = cluster().getInstance(AwsS3Service.class).client(
+                    bucket.get("region", settings.get("repositories.s3.region")),
+                    bucket.get("access_key", settings.get("cloud.aws.access_key")),
+                    bucket.get("secret_key", settings.get("cloud.aws.secret_key")));
+            try {
+                ObjectListing prevListing = null;
+                //From http://docs.amazonwebservices.com/AmazonS3/latest/dev/DeletingMultipleObjectsUsingJava.html
+                //we can do at most 1K objects per delete
+                //We don't know the bucket name until first object listing
+                DeleteObjectsRequest multiObjectDeleteRequest = null;
+                ArrayList<DeleteObjectsRequest.KeyVersion> keys = new ArrayList<DeleteObjectsRequest.KeyVersion>();
+                while (true) {
+                    ObjectListing list;
+                    if (prevListing != null) {
+                        list = client.listNextBatchOfObjects(prevListing);
+                    } else {
+                        list = client.listObjects(bucket.get("bucket"), basePath);
                         multiObjectDeleteRequest = new DeleteObjectsRequest(list.getBucketName());
-                        keys.clear();
+                    }
+                    for (S3ObjectSummary summary : list.getObjectSummaries()) {
+                        keys.add(new DeleteObjectsRequest.KeyVersion(summary.getKey()));
+                        //Every 500 objects batch the delete request
+                        if (keys.size() > 500) {
+                            multiObjectDeleteRequest.setKeys(keys);
+                            client.deleteObjects(multiObjectDeleteRequest);
+                            multiObjectDeleteRequest = new DeleteObjectsRequest(list.getBucketName());
+                            keys.clear();
+                        }
+                    }
+                    if (list.isTruncated()) {
+                        prevListing = list;
+                    } else {
+                        break;
                     }
                 }
-                if (list.isTruncated()) {
-                    prevListing = list;
-                } else {
-                    break;
+                if (!keys.isEmpty()) {
+                    multiObjectDeleteRequest.setKeys(keys);
+                    client.deleteObjects(multiObjectDeleteRequest);
                 }
+            } catch (Throwable ex) {
+                logger.warn("Failed to delete S3 repository", ex);
             }
-            if (!keys.isEmpty()) {
-                multiObjectDeleteRequest.setKeys(keys);
-                client.deleteObjects(multiObjectDeleteRequest);
-            }
-        } catch (Throwable ex) {
-            logger.warn("Failed to delete S3 repository", ex);
         }
     }
 }

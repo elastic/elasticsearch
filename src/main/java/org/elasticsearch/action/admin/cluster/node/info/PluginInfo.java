70c089de0adc866326a6804c556c2c91647a3860
Chris Earle <pickypg@gmail.com>
2014-03-30 00:04:02 -0400

    Replaces usage of `StringBuffer` with `StringBuilder` and fixes stray single quote in the same `PluginInfo#toString()` method.
    Closes #5605

--- src/main/java/org/elasticsearch/action/admin/cluster/node/info/PluginInfo.java
+++ src/main/java/org/elasticsearch/action/admin/cluster/node/info/PluginInfo.java
@@ -207,13 +207,13 @@ public class PluginInfo implements Streamable, Serializable, ToXContent {
 
     @Override
     public String toString() {
-        final StringBuffer sb = new StringBuffer("PluginInfo{");
+        final StringBuilder sb = new StringBuilder("PluginInfo{");
         sb.append("name='").append(name).append('\'');
         sb.append(", description='").append(description).append('\'');
         sb.append(", site=").append(site);
         sb.append(", jvm=").append(jvm);
         sb.append(", version='").append(version).append('\'');
-        sb.append(", isolation='").append(isolation);
+        sb.append(", isolation=").append(isolation);
         sb.append('}');
         return sb.toString();
     }

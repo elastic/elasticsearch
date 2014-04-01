196b5776b7c1af9214d7546d851dcb890c62d151
javanna <cavannaluca@gmail.com>
2014-04-01 18:43:14 +0200

    [TEST] fixed typo in ReproduceInfoPrinter (s/nighly/nightly)

--- src/test/java/org/elasticsearch/test/junit/listeners/ReproduceInfoPrinter.java
+++ src/test/java/org/elasticsearch/test/junit/listeners/ReproduceInfoPrinter.java
@@ -116,7 +116,7 @@ public class ReproduceInfoPrinter extends RunListener {
 
         public ReproduceErrorMessageBuilder appendESProperties() {
             appendProperties("es.logger.level", "es.node.mode", "es.node.local", TESTS_CLUSTER, TestCluster.TESTS_ENABLE_MOCK_MODULES,
-                    "tests.assertion.disabled", "tests.security.manager", "tests.nighly", "tests.jvms", "tests.client.ratio", "tests.heap.size");
+                    "tests.assertion.disabled", "tests.security.manager", "tests.nightly", "tests.jvms", "tests.client.ratio", "tests.heap.size");
             if (System.getProperty("tests.jvm.argline") != null && !System.getProperty("tests.jvm.argline").isEmpty()) {
                 appendOpt("tests.jvm.argline", "\"" + System.getProperty("tests.jvm.argline") + "\"");
             }

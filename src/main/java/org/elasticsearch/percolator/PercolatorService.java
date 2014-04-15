905c843f5af401cf3408aab99da74a9717424d10
Simon Willnauer <simonw@apache.org>
2014-04-15 22:50:33 +0200

    [TEST] Fix PercolatorTests#testSimple2
    This test requires a mapping since otherwise if there is no mapping
    added the percolator query might not be parsed as a query on a numeric
    field since the query might arrive on a node before the dynamic mapping
    reached that node.
    
    This commit also moves the `indexService.readAllowed()` call up before
    the number of percolation queries is check to make sure we fail if reads
    are not allowed - there might be a query in-flight which means we need
    to check another node rather than return an empty result.

--- src/main/java/org/elasticsearch/percolator/PercolatorService.java
+++ src/main/java/org/elasticsearch/percolator/PercolatorService.java
@@ -169,6 +169,7 @@ public class PercolatorService extends AbstractComponent {
     public PercolateShardResponse percolate(PercolateShardRequest request) {
         IndexService percolateIndexService = indicesService.indexServiceSafe(request.index());
         IndexShard indexShard = percolateIndexService.shardSafe(request.shardId());
+        indexShard.readAllowed(); // check if we can read the shard...
 
         ShardPercolateService shardPercolateService = indexShard.shardPercolateService();
         shardPercolateService.prePercolate();
@@ -229,8 +230,6 @@ public class PercolatorService extends AbstractComponent {
             context.percolatorTypeId = action.id();
 
             percolatorIndex.prepare(context, parsedDocument);
-
-            indexShard.readAllowed();
             return action.doPercolate(request, context);
         } finally {
             context.close();

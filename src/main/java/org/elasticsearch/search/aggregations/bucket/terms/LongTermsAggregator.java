f2749006d2a9d6e9675c905c35047a135ff1e5cc
Martijn van Groningen <martijn.v.groningen@gmail.com>
2014-04-28 00:50:46 +0700

    Cut other aggregations over to use collectExistingBucket() if a bucket ord has been hit, that already exists.

--- src/main/java/org/elasticsearch/search/aggregations/bucket/terms/LongTermsAggregator.java
+++ src/main/java/org/elasticsearch/search/aggregations/bucket/terms/LongTermsAggregator.java
@@ -83,8 +83,10 @@ public class LongTermsAggregator extends BucketsAggregator {
             long bucketOrdinal = bucketOrds.add(val);
             if (bucketOrdinal < 0) { // already seen
                 bucketOrdinal = - 1 - bucketOrdinal;
+                collectExistingBucket(doc, bucketOrdinal);
+            } else {
+                collectBucket(doc, bucketOrdinal);
             }
-            collectBucket(doc, bucketOrdinal);
         }
     }
 

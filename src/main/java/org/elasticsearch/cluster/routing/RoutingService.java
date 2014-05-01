95d85bb01134aabd2d88b42416b0db769b2acf66
Boaz Leskes <b.leskes@gmail.com>
2014-05-01 21:36:48 +0200

    RoutingService background reroute task should check if node is still master when its run
    It may be that between the moment the task was added to the queue the current node stepped down from being master (due to minimum_master_nodes settings).

--- src/main/java/org/elasticsearch/cluster/routing/RoutingService.java
+++ src/main/java/org/elasticsearch/cluster/routing/RoutingService.java
@@ -141,6 +141,11 @@ public class RoutingService extends AbstractLifecycleComponent<RoutingService> i
             clusterService.submitStateUpdateTask(CLUSTER_UPDATE_TASK_SOURCE, Priority.HIGH, new ClusterStateUpdateTask() {
                 @Override
                 public ClusterState execute(ClusterState currentState) {
+                    // double check we are still master, this may have changed.
+                    if (!currentState.nodes().localNodeMaster()) {
+                        return currentState;
+                    }
+
                     RoutingAllocation.Result routingResult = allocationService.reroute(currentState);
                     if (!routingResult.changed()) {
                         // no state changed

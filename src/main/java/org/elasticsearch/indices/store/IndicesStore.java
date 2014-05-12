ade3b9a5209bebfb37f5c35b612b38c5fa3ce4bb
mikemccand <mail@mikemccandless.com>
2014-05-12 14:42:26 -0400

    #6081: set IO throttling back to 20 MB/sec now that #6018 is fixed

--- src/main/java/org/elasticsearch/indices/store/IndicesStore.java
+++ src/main/java/org/elasticsearch/indices/store/IndicesStore.java
@@ -95,7 +95,7 @@ public class IndicesStore extends AbstractComponent implements ClusterStateListe
         // we limit with 20MB / sec by default with a default type set to merge sice 0.90.1
         this.rateLimitingType = componentSettings.get("throttle.type", StoreRateLimiting.Type.MERGE.name());
         rateLimiting.setType(rateLimitingType);
-        this.rateLimitingThrottle = componentSettings.getAsBytesSize("throttle.max_bytes_per_sec", new ByteSizeValue(50, ByteSizeUnit.MB));
+        this.rateLimitingThrottle = componentSettings.getAsBytesSize("throttle.max_bytes_per_sec", new ByteSizeValue(20, ByteSizeUnit.MB));
         rateLimiting.setMaxRate(rateLimitingThrottle);
 
         logger.debug("using indices.store.throttle.type [{}], with index.store.throttle.max_bytes_per_sec [{}]", rateLimitingType, rateLimitingThrottle);

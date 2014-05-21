a03d66ece0c02672689607c220c3111ebbb2334b
Adrien Grand <jpountz@gmail.com>
2014-05-21 15:08:34 +0200

    Fail queries that have two aggregations with the same name.
    Close #6255

--- src/test/java/org/elasticsearch/search/aggregations/ParsingTests.java
+++ src/test/java/org/elasticsearch/search/aggregations/ParsingTests.java
@@ -19,6 +19,7 @@
 
 package org.elasticsearch.search.aggregations;
 
+import com.carrotsearch.randomizedtesting.generators.RandomStrings;
 import org.elasticsearch.action.search.SearchPhaseExecutionException;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.test.ElasticsearchIntegrationTest;
@@ -109,6 +110,26 @@ public class ParsingTests extends ElasticsearchIntegrationTest {
                             .endObject()
                         .endObject()
                     .endObject()
+            .endObject()).execute().actionGet();
+    }
+
+    @Test(expected=SearchPhaseExecutionException.class)
+    public void testSameAggregationName() throws Exception {
+        createIndex("idx");
+        ensureGreen();
+        final String name = RandomStrings.randomAsciiOfLength(getRandom(), 10);
+        client().prepareSearch("idx").setAggregations(JsonXContent.contentBuilder()
+            .startObject()
+                .startObject(name)
+                    .startObject("terms")
+                        .field("field", "a")
+                    .endObject()
+                .endObject()
+                .startObject(name)
+                    .startObject("terms")
+                        .field("field", "b")
+                    .endObject()
+                .endObject()
             .endObject()).execute().actionGet();
     }
 

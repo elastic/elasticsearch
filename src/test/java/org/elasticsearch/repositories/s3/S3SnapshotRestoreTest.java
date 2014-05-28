c743f067025412c5c1ecc44b0f06ad82392116ec
Wilkes Joiner <wilkesjoiner@gmail.com>
2014-05-28 22:48:34 +0200

    Add Server Side Encryption support for S3 repository
    For legal reasons, we need to store our ES backups in S3 using server side encryption.
    
    Closes #78.
    
    (cherry picked from commit b3f9e12)

--- src/test/java/org/elasticsearch/repositories/s3/S3SnapshotRestoreTest.java
+++ src/test/java/org/elasticsearch/repositories/s3/S3SnapshotRestoreTest.java
@@ -44,6 +44,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.ArrayList;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -151,6 +152,93 @@ public class S3SnapshotRestoreTest extends AbstractAwsTest {
         ClusterState clusterState = client.admin().cluster().prepareState().get().getState();
         assertThat(clusterState.getMetaData().hasIndex("test-idx-1"), equalTo(true));
         assertThat(clusterState.getMetaData().hasIndex("test-idx-2"), equalTo(false));
+    }
+    
+    @Test
+    public void testEncryption() {
+	Client client = client();
+	logger.info("-->  creating s3 repository with bucket[{}] and path [{}]", cluster().getInstance(Settings.class).get("repositories.s3.bucket"), basePath);
+	PutRepositoryResponse putRepositoryResponse = client.admin().cluster().preparePutRepository("test-repo")
+		.setType("s3").setSettings(ImmutableSettings.settingsBuilder()
+			.put("base_path", basePath)
+			.put("chunk_size", randomIntBetween(1000, 10000))
+			.put("server_side_encryption", true)
+		).get();
+	assertThat(putRepositoryResponse.isAcknowledged(), equalTo(true));
+
+	createIndex("test-idx-1", "test-idx-2", "test-idx-3");
+	ensureGreen();
+
+	logger.info("--> indexing some data");
+	for (int i = 0; i < 100; i++) {
+	    index("test-idx-1", "doc", Integer.toString(i), "foo", "bar" + i);
+	    index("test-idx-2", "doc", Integer.toString(i), "foo", "baz" + i);
+	    index("test-idx-3", "doc", Integer.toString(i), "foo", "baz" + i);
+	}
+	refresh();
+	assertThat(client.prepareCount("test-idx-1").get().getCount(), equalTo(100L));
+	assertThat(client.prepareCount("test-idx-2").get().getCount(), equalTo(100L));
+	assertThat(client.prepareCount("test-idx-3").get().getCount(), equalTo(100L));
+
+	logger.info("--> snapshot");
+	CreateSnapshotResponse createSnapshotResponse = client.admin().cluster().prepareCreateSnapshot("test-repo", "test-snap").setWaitForCompletion(true).setIndices("test-idx-*", "-test-idx-3").get();
+	assertThat(createSnapshotResponse.getSnapshotInfo().successfulShards(), greaterThan(0));
+	assertThat(createSnapshotResponse.getSnapshotInfo().successfulShards(), equalTo(createSnapshotResponse.getSnapshotInfo().totalShards()));
+
+	assertThat(client.admin().cluster().prepareGetSnapshots("test-repo").setSnapshots("test-snap").get().getSnapshots().get(0).state(), equalTo(SnapshotState.SUCCESS));
+
+	Settings settings = cluster().getInstance(Settings.class);
+	Settings bucket = settings.getByPrefix("repositories.s3.");
+	AmazonS3 s3Client = cluster().getInstance(AwsS3Service.class).client(
+		bucket.get("region", settings.get("repositories.s3.region")),
+		bucket.get("access_key", settings.get("cloud.aws.access_key")),
+		bucket.get("secret_key", settings.get("cloud.aws.secret_key")));
+
+    String bucketName = bucket.get("bucket");
+	logger.info("--> verify encryption for bucket [{}], prefix [{}]", bucketName, basePath);
+	List<S3ObjectSummary> summaries = s3Client.listObjects(bucketName, basePath).getObjectSummaries();
+	for (S3ObjectSummary summary : summaries) {
+	    assertThat(s3Client.getObjectMetadata(bucketName, summary.getKey()).getServerSideEncryption(), equalTo("AES256"));
+	}
+
+	logger.info("--> delete some data");
+	for (int i = 0; i < 50; i++) {
+	    client.prepareDelete("test-idx-1", "doc", Integer.toString(i)).get();
+	}
+	for (int i = 50; i < 100; i++) {
+	    client.prepareDelete("test-idx-2", "doc", Integer.toString(i)).get();
+	}
+	for (int i = 0; i < 100; i += 2) {
+	    client.prepareDelete("test-idx-3", "doc", Integer.toString(i)).get();
+	}
+	refresh();
+	assertThat(client.prepareCount("test-idx-1").get().getCount(), equalTo(50L));
+	assertThat(client.prepareCount("test-idx-2").get().getCount(), equalTo(50L));
+	assertThat(client.prepareCount("test-idx-3").get().getCount(), equalTo(50L));
+
+	logger.info("--> close indices");
+	client.admin().indices().prepareClose("test-idx-1", "test-idx-2").get();
+
+	logger.info("--> restore all indices from the snapshot");
+	RestoreSnapshotResponse restoreSnapshotResponse = client.admin().cluster().prepareRestoreSnapshot("test-repo", "test-snap").setWaitForCompletion(true).execute().actionGet();
+	assertThat(restoreSnapshotResponse.getRestoreInfo().totalShards(), greaterThan(0));
+
+	ensureGreen();
+	assertThat(client.prepareCount("test-idx-1").get().getCount(), equalTo(100L));
+	assertThat(client.prepareCount("test-idx-2").get().getCount(), equalTo(100L));
+	assertThat(client.prepareCount("test-idx-3").get().getCount(), equalTo(50L));
+
+	// Test restore after index deletion
+	logger.info("--> delete indices");
+	cluster().wipeIndices("test-idx-1", "test-idx-2");
+	logger.info("--> restore one index after deletion");
+	restoreSnapshotResponse = client.admin().cluster().prepareRestoreSnapshot("test-repo", "test-snap").setWaitForCompletion(true).setIndices("test-idx-*", "-test-idx-2").execute().actionGet();
+	assertThat(restoreSnapshotResponse.getRestoreInfo().totalShards(), greaterThan(0));
+	ensureGreen();
+	assertThat(client.prepareCount("test-idx-1").get().getCount(), equalTo(100L));
+	ClusterState clusterState = client.admin().cluster().prepareState().get().getState();
+	assertThat(clusterState.getMetaData().hasIndex("test-idx-1"), equalTo(true));
+	assertThat(clusterState.getMetaData().hasIndex("test-idx-2"), equalTo(false));
     }
 
     /**

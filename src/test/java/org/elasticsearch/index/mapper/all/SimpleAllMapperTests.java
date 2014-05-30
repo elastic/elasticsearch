dafba2e19057e54bb787243777433c594f50154a
Britta Weber <britta.weber@elasticsearch.com>
2014-05-30 14:19:39 +0200

    Object and Type parsing: Fix include_in_all in type
    include_in_all can also be set on type level (root object).
    This fixes a regression introduced  in #6093
    
    closes #6304

--- src/test/java/org/elasticsearch/index/mapper/all/SimpleAllMapperTests.java
+++ src/test/java/org/elasticsearch/index/mapper/all/SimpleAllMapperTests.java
@@ -413,6 +413,13 @@ public class SimpleAllMapperTests extends ElasticsearchTestCase {
         rootTypes.put(IndexFieldMapper.NAME, "{\"enabled\" : true}");
         rootTypes.put(SourceFieldMapper.NAME, "{\"enabled\" : true}");
         rootTypes.put(TypeFieldMapper.NAME, "{\"store\" : true}");
+        rootTypes.put("include_in_all", "true");
+        rootTypes.put("index_analyzer", "\"standard\"");
+        rootTypes.put("search_analyzer", "\"standard\"");
+        rootTypes.put("analyzer", "\"standard\"");
+        rootTypes.put("dynamic_date_formats", "[\"yyyy-MM-dd\", \"dd-MM-yyyy\"]");
+        rootTypes.put("numeric_detection", "true");
+        rootTypes.put("dynamic_templates", "[]");
         for (String key : rootTypes.keySet()) {
             mapping += "\"" + key+ "\"" + ":" + rootTypes.get(key) + ",\n";
         }

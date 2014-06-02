535fc046322370aea796f2f6a1e24e979b4a176a
Simon Willnauer <simonw@apache.org>
2014-06-02 11:57:32 +0200

    [TEST] Ensure cluster size reflected in the cluster state
    We perform some management operations that require the cluster to be
    consistent with respect to the number of nodes in the cluster state
    / visible to the master in order to rely on the ack mechanism. This
    only applies to the test infrastructure when nodes are not explicitly
    started / stopped as well as while tearing down the cluster and wiping
    indices after the tests.

--- src/test/java/org/elasticsearch/test/TestCluster.java
+++ src/test/java/org/elasticsearch/test/TestCluster.java
@@ -94,6 +94,7 @@ import static org.apache.lucene.util.LuceneTestCase.rarely;
 import static org.apache.lucene.util.LuceneTestCase.usually;
 import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilder;
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertNoTimeout;
 
 /**
  * TestCluster manages a set of JVM private nodes and allows convenient access to them.
@@ -396,6 +397,11 @@ public final class TestCluster extends ImmutableTestCluster {
         } catch (Exception e) {
             throw new ElasticsearchException("failed to start nodes", e);
         }
+        if (!futures.isEmpty()) {
+            synchronized (this) {
+                assertNoTimeout(client().admin().cluster().prepareHealth().setWaitForNodes(Integer.toString(nodes.size())).get());
+            }
+        }
     }
 
     /**
@@ -422,6 +428,9 @@ public final class TestCluster extends ImmutableTestCluster {
         }
         for (NodeAndClient toRemove : nodesToRemove) {
             nodes.remove(toRemove.name);
+        }
+        if (!nodesToRemove.isEmpty() && size() > 0) {
+            assertNoTimeout(client().admin().cluster().prepareHealth().setWaitForNodes(Integer.toString(nodes.size())).get());
         }
     }
 

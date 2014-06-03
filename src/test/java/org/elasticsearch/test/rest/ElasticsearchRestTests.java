e04d3b9727a36079a9648098d8d8bc03c81c7261
javanna <cavannaluca@gmail.com>
2014-06-03 09:21:49 +0200

    [TEST] make sure that the -Dtests.rest.blacklist parameter works on windows too
    Some reserved characters need to be replaced in the test section names, which gets parsed as a path although it isn't a filename

--- src/test/java/org/elasticsearch/test/rest/ElasticsearchRestTests.java
+++ src/test/java/org/elasticsearch/test/rest/ElasticsearchRestTests.java
@@ -177,7 +177,10 @@ public class ElasticsearchRestTests extends ElasticsearchIntegrationTest {
     public void reset() throws IOException, RestException {
         //skip test if it matches one of the blacklist globs
         for (PathMatcher blacklistedPathMatcher : blacklistPathMatchers) {
-            assumeFalse("[" + testCandidate.getTestPath() + "] skipped, reason: blacklisted", blacklistedPathMatcher.matches(Paths.get(testCandidate.getTestPath())));
+            //we need to replace a few characters otherwise the test section name can't be parsed as a path on windows
+            String testSection = testCandidate.getTestSection().getName().replace("*", "").replace("\\", "/").replaceAll("\\s+/", "/").trim();
+            String testPath = testCandidate.getSuitePath() + "/" + testSection;
+            assumeFalse("[" + testCandidate.getTestPath() + "] skipped, reason: blacklisted", blacklistedPathMatcher.matches(Paths.get(testPath)));
         }
 
         restTestExecutionContext.resetClient(immutableCluster().httpAddresses());

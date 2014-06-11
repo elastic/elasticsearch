673ef3db3ff4f8153db6081d399970d8f08ef5ae
Clinton Gormley <clint@traveljury.com>
2014-06-11 12:30:16 +0200

    The StemmerTokenFilter had a number of issues:
    * `english` returned the slow snowball English stemmer
    * `porter2` returned the snowball Porter stemmer (v1)
    * `portuguese` was used twice, preventing the second version from working
    
    Changes:
    
    * `english` now returns the fast PorterStemmer (for indices created from v1.3.0 onwards)
    * `porter2` now returns the snowball English stemmer (for indices created from v1.3.0 onwards)
    * `light_english` now returns the `kstem` stemmer (`kstem` still works)
    * `portuguese_rslp` returns the PortugueseStemmer
    * `dutch_kp` is a synonym for `kp`
    
    Tests and docs updated
    
    Fixes #6345
    Fixes #6213
    Fixes #6330

--- src/main/java/org/elasticsearch/index/analysis/StemmerTokenFilterFactory.java
+++ src/main/java/org/elasticsearch/index/analysis/StemmerTokenFilterFactory.java
@@ -47,6 +47,8 @@ import org.apache.lucene.analysis.pt.PortugueseStemFilter;
 import org.apache.lucene.analysis.ru.RussianLightStemFilter;
 import org.apache.lucene.analysis.snowball.SnowballFilter;
 import org.apache.lucene.analysis.sv.SwedishLightStemFilter;
+import org.elasticsearch.Version;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.assistedinject.Assisted;
@@ -69,6 +71,8 @@ public class StemmerTokenFilterFactory extends AbstractTokenFilterFactory {
 
     @Override
     public TokenStream create(TokenStream tokenStream) {
+        final Version indexVersion = indexSettings.getAsVersion(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT);
+
         if ("arabic".equalsIgnoreCase(language)) {
             return new ArabicStemFilter(tokenStream);
         } else if ("armenian".equalsIgnoreCase(language)) {
@@ -85,90 +89,129 @@ public class StemmerTokenFilterFactory extends AbstractTokenFilterFactory {
             return new CzechStemFilter(tokenStream);
         } else if ("danish".equalsIgnoreCase(language)) {
             return new SnowballFilter(tokenStream, new DanishStemmer());
+
+            // Dutch stemmers
         } else if ("dutch".equalsIgnoreCase(language)) {
             return new SnowballFilter(tokenStream, new DutchStemmer());
-        } else if ("english".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new EnglishStemmer());
-        } else if ("finnish".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new FinnishStemmer());
-        } else if ("french".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new FrenchStemmer());
-        } else if ("german".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new GermanStemmer());
-        } else if ("german2".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new German2Stemmer());
-        } else if ("hungarian".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new HungarianStemmer());
-        } else if ("italian".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new ItalianStemmer());
-        } else if ("kp".equalsIgnoreCase(language)) {
+        } else if ("dutch_kp".equalsIgnoreCase(language) || "dutchKp".equalsIgnoreCase(language) || "kp".equalsIgnoreCase(language)) {
             return new SnowballFilter(tokenStream, new KpStemmer());
-        } else if ("kstem".equalsIgnoreCase(language)) {
+
+            // English stemmers
+        } else if ("english".equalsIgnoreCase(language)) {
+            if (indexVersion.onOrAfter(Version.V_1_3_0)) {
+                return new PorterStemFilter(tokenStream);
+            } else {
+                return new SnowballFilter(tokenStream, new EnglishStemmer());
+            }
+        } else if ("light_english".equalsIgnoreCase(language) || "lightEnglish".equalsIgnoreCase(language)
+                || "kstem".equalsIgnoreCase(language)) {
             return new KStemFilter(tokenStream);
         } else if ("lovins".equalsIgnoreCase(language)) {
             return new SnowballFilter(tokenStream, new LovinsStemmer());
-        } else if ("latvian".equalsIgnoreCase(language)) {
-            return new LatvianStemFilter(tokenStream);
-        } else if ("norwegian".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new NorwegianStemmer());
-        } else if ("minimal_norwegian".equalsIgnoreCase(language) || "minimalNorwegian".equals(language)) {
-            return new NorwegianMinimalStemFilter(tokenStream);
         } else if ("porter".equalsIgnoreCase(language)) {
             return new PorterStemFilter(tokenStream);
         } else if ("porter2".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new PorterStemmer());
-        } else if ("portuguese".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new PortugueseStemmer());
-        } else if ("romanian".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new RomanianStemmer());
-        } else if ("russian".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new RussianStemmer());
-        } else if ("spanish".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new SpanishStemmer());
-        } else if ("swedish".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new SwedishStemmer());
-        } else if ("turkish".equalsIgnoreCase(language)) {
-            return new SnowballFilter(tokenStream, new TurkishStemmer());
+            if (indexVersion.onOrAfter(Version.V_1_3_0)) {
+                return new SnowballFilter(tokenStream, new EnglishStemmer());
+            } else {
+                return new SnowballFilter(tokenStream, new PorterStemmer());
+            }
         } else if ("minimal_english".equalsIgnoreCase(language) || "minimalEnglish".equalsIgnoreCase(language)) {
             return new EnglishMinimalStemFilter(tokenStream);
         } else if ("possessive_english".equalsIgnoreCase(language) || "possessiveEnglish".equalsIgnoreCase(language)) {
             return new EnglishPossessiveFilter(version, tokenStream);
+
+            // Finnish stemmers
+        } else if ("finnish".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new FinnishStemmer());
         } else if ("light_finish".equalsIgnoreCase(language) || "lightFinish".equalsIgnoreCase(language)) {
             // leaving this for backward compatibility
             return new FinnishLightStemFilter(tokenStream);
         } else if ("light_finnish".equalsIgnoreCase(language) || "lightFinnish".equalsIgnoreCase(language)) {
             return new FinnishLightStemFilter(tokenStream);
+
+            // French stemmers
+        } else if ("french".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new FrenchStemmer());
         } else if ("light_french".equalsIgnoreCase(language) || "lightFrench".equalsIgnoreCase(language)) {
             return new FrenchLightStemFilter(tokenStream);
         } else if ("minimal_french".equalsIgnoreCase(language) || "minimalFrench".equalsIgnoreCase(language)) {
             return new FrenchMinimalStemFilter(tokenStream);
+
+            // German stemmers
+        } else if ("german".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new GermanStemmer());
+        } else if ("german2".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new German2Stemmer());
         } else if ("light_german".equalsIgnoreCase(language) || "lightGerman".equalsIgnoreCase(language)) {
             return new GermanLightStemFilter(tokenStream);
         } else if ("minimal_german".equalsIgnoreCase(language) || "minimalGerman".equalsIgnoreCase(language)) {
             return new GermanMinimalStemFilter(tokenStream);
+
+        } else if ("greek".equalsIgnoreCase(language)) {
+            return new GreekStemFilter(tokenStream);
         } else if ("hindi".equalsIgnoreCase(language)) {
             return new HindiStemFilter(tokenStream);
+
+            // Hungarian stemmers
+        } else if ("hungarian".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new HungarianStemmer());
         } else if ("light_hungarian".equalsIgnoreCase(language) || "lightHungarian".equalsIgnoreCase(language)) {
             return new HungarianLightStemFilter(tokenStream);
+
         } else if ("indonesian".equalsIgnoreCase(language)) {
             return new IndonesianStemFilter(tokenStream);
+
+            // Italian stemmers
+        } else if ("italian".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new ItalianStemmer());
         } else if ("light_italian".equalsIgnoreCase(language) || "lightItalian".equalsIgnoreCase(language)) {
             return new ItalianLightStemFilter(tokenStream);
+
+        } else if ("latvian".equalsIgnoreCase(language)) {
+            return new LatvianStemFilter(tokenStream);
+
+            // Norwegian stemmers
+        } else if ("norwegian".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new NorwegianStemmer());
+        } else if ("minimal_norwegian".equalsIgnoreCase(language) || "minimalNorwegian".equals(language)) {
+            return new NorwegianMinimalStemFilter(tokenStream);
+
+            // Portuguese stemmers
+        } else if ("portuguese".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new PortugueseStemmer());
         } else if ("light_portuguese".equalsIgnoreCase(language) || "lightPortuguese".equalsIgnoreCase(language)) {
             return new PortugueseLightStemFilter(tokenStream);
         } else if ("minimal_portuguese".equalsIgnoreCase(language) || "minimalPortuguese".equalsIgnoreCase(language)) {
             return new PortugueseMinimalStemFilter(tokenStream);
-        } else if ("portuguese".equalsIgnoreCase(language)) {
+        } else if ("portuguese_rslp".equalsIgnoreCase(language)) {
             return new PortugueseStemFilter(tokenStream);
+
+        } else if ("romanian".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new RomanianStemmer());
+
+            // Russian stemmers
+        } else if ("russian".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new RussianStemmer());
         } else if ("light_russian".equalsIgnoreCase(language) || "lightRussian".equalsIgnoreCase(language)) {
             return new RussianLightStemFilter(tokenStream);
+
+            // Spanish stemmers
+        } else if ("spanish".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new SpanishStemmer());
         } else if ("light_spanish".equalsIgnoreCase(language) || "lightSpanish".equalsIgnoreCase(language)) {
             return new SpanishLightStemFilter(tokenStream);
+
+            // Swedish stemmers
+        } else if ("swedish".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new SwedishStemmer());
         } else if ("light_swedish".equalsIgnoreCase(language) || "lightSwedish".equalsIgnoreCase(language)) {
             return new SwedishLightStemFilter(tokenStream);
-        } else if ("greek".equalsIgnoreCase(language)) {
-            return new GreekStemFilter(tokenStream);
+
+        } else if ("turkish".equalsIgnoreCase(language)) {
+            return new SnowballFilter(tokenStream, new TurkishStemmer());
         }
+
         return new SnowballFilter(tokenStream, language);
     }
 

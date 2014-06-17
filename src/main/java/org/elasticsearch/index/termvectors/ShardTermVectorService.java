f22f3db30f9dcbedf1b1a4991b948a2ad8097830
Alex Ksikes <alex.ksikes@gmail.com>
2014-06-17 19:08:38 +0200

    Term Vectors API: Computes term vectors on the fly if not stored in the index.
    Adds the ability to the Term Vector API to generate term vectors for some
    chosen fields, even though they haven't been explicitely stored in the index.
    
    Relates to #5184
    Closes #6567

--- src/main/java/org/elasticsearch/index/termvectors/ShardTermVectorService.java
+++ src/main/java/org/elasticsearch/index/termvectors/ShardTermVectorService.java
@@ -19,24 +19,30 @@
 
 package org.elasticsearch.index.termvectors;
 
-import org.apache.lucene.index.Fields;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.MultiFields;
-import org.apache.lucene.index.Term;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.index.*;
+import org.apache.lucene.index.memory.MemoryIndex;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.termvector.TermVectorRequest;
 import org.elasticsearch.action.termvector.TermVectorResponse;
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.lucene.uid.Versions;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.engine.Engine;
-import org.elasticsearch.index.mapper.MapperService;
+import org.elasticsearch.index.get.GetField;
+import org.elasticsearch.index.get.GetResult;
+import org.elasticsearch.index.mapper.FieldMapper;
 import org.elasticsearch.index.mapper.Uid;
+import org.elasticsearch.index.mapper.core.StringFieldMapper;
 import org.elasticsearch.index.mapper.internal.UidFieldMapper;
 import org.elasticsearch.index.settings.IndexSettings;
 import org.elasticsearch.index.shard.AbstractIndexShardComponent;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.index.shard.service.IndexShard;
+
+import java.io.IOException;
+import java.util.*;
 
 /**
  */
@@ -44,10 +50,9 @@ import org.elasticsearch.index.shard.service.IndexShard;
 public class ShardTermVectorService extends AbstractIndexShardComponent {
 
     private IndexShard indexShard;
-    private MapperService mapperService;
 
     @Inject
-    public ShardTermVectorService(ShardId shardId, @IndexSettings Settings indexSettings, MapperService mapperService) {
+    public ShardTermVectorService(ShardId shardId, @IndexSettings Settings indexSettings) {
         super(shardId, indexSettings);
     }
 
@@ -66,8 +71,11 @@ public class ShardTermVectorService extends AbstractIndexShardComponent {
             Fields topLevelFields = MultiFields.getFields(topLevelReader);
             Versions.DocIdAndVersion docIdAndVersion = Versions.loadDocIdAndVersion(topLevelReader, uidTerm);
             if (docIdAndVersion != null) {
-
                 Fields termVectorsByField = docIdAndVersion.context.reader().getTermVectors(docIdAndVersion.docId);
+                /* generate term vectors if not available */
+                if (request.selectedFields() != null) {
+                    termVectorsByField = generateTermVectorsIfNeeded(termVectorsByField, request, uidTerm, false);
+                }
                 termVectorResponse.setFields(termVectorsByField, request.selectedFields(), request.getFlags(), topLevelFields);
                 termVectorResponse.setExists(true);
                 termVectorResponse.setDocVersion(docIdAndVersion.version);
@@ -80,5 +88,103 @@ public class ShardTermVectorService extends AbstractIndexShardComponent {
             searcher.close();
         }
         return termVectorResponse;
+    }
+
+    private Fields generateTermVectorsIfNeeded(Fields termVectorsByField, TermVectorRequest request, Term uidTerm, boolean realTime) throws IOException {
+        List<String> validFields = new ArrayList<>();
+        for (String field : request.selectedFields()) {
+            FieldMapper fieldMapper = indexShard.mapperService().smartNameFieldMapper(field);
+            if (!(fieldMapper instanceof StringFieldMapper)) {
+                continue;
+            }
+            if (fieldMapper.fieldType().storeTermVectors()) {
+                continue;
+            }
+            // only disallow fields which are not indexed
+            if (!fieldMapper.fieldType().indexed()) {
+                continue;
+            }
+            validFields.add(field);
+        }
+        if (validFields.isEmpty()) {
+            return termVectorsByField;
+        }
+
+        Engine.GetResult get = indexShard.get(new Engine.Get(realTime, uidTerm));
+        Fields generatedTermVectors;
+        try {
+            if (!get.exists()) {
+                return termVectorsByField;
+            }
+            // TODO: support for fetchSourceContext?
+            GetResult getResult = indexShard.getService().get(
+                    get, request.id(), request.type(), validFields.toArray(Strings.EMPTY_ARRAY), null);
+            generatedTermVectors = generateTermVectors(getResult.getFields().values(), request.offsets());
+        } finally {
+            get.release();
+        }
+        if (termVectorsByField == null) {
+            return generatedTermVectors;
+        } else {
+            return mergeFields(request.selectedFields().toArray(Strings.EMPTY_ARRAY), termVectorsByField, generatedTermVectors);
+        }
+    }
+
+    private Fields generateTermVectors(Collection<GetField> getFields, boolean withOffsets) throws IOException {
+        // store document in memory index
+        MemoryIndex index = new MemoryIndex(withOffsets);
+        for (GetField getField : getFields) {
+            String field = getField.getName();
+            Analyzer analyzer = indexShard.mapperService().smartNameFieldMapper(field).indexAnalyzer();
+            if (analyzer == null) {
+                analyzer = indexShard.mapperService().analysisService().defaultIndexAnalyzer();
+            }
+            for (Object text : getField.getValues()) {
+                index.addField(field, text.toString(), analyzer);
+            }
+        }
+        // and read vectors from it
+        return MultiFields.getFields(index.createSearcher().getIndexReader());
+    }
+
+    private Fields mergeFields(String[] fieldNames, Fields... fieldsObject) throws IOException {
+        ParallelFields parallelFields = new ParallelFields();
+        for (Fields fieldObject : fieldsObject) {
+            assert fieldObject != null;
+            for (String fieldName : fieldNames) {
+                Terms terms = fieldObject.terms(fieldName);
+                if (terms != null) {
+                    parallelFields.addField(fieldName, terms);
+                }
+            }
+        }
+        return parallelFields;
+    }
+
+    // Poached from Lucene ParallelAtomicReader
+    private static final class ParallelFields extends Fields {
+        final Map<String,Terms> fields = new TreeMap<>();
+
+        ParallelFields() {
+        }
+
+        void addField(String fieldName, Terms terms) {
+            fields.put(fieldName, terms);
+        }
+
+        @Override
+        public Iterator<String> iterator() {
+            return Collections.unmodifiableSet(fields.keySet()).iterator();
+        }
+
+        @Override
+        public Terms terms(String field) {
+            return fields.get(field);
+        }
+
+        @Override
+        public int size() {
+            return fields.size();
+        }
     }
 }

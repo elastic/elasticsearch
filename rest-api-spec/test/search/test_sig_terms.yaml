elastic/elasticsearch

commit fbd7c9aa5d6c1c730ab73887ca275b319a588804

Aggregations: Fix reducing of range aggregations.
Under some rare circumstances:
 - local transport,
 - the range aggregation has both a parent and a child aggregation,
 - the range aggregation got no documents on one shard or more and several
   documents on one shard or more.
the range aggregation could return incorrect counts and sub aggregations.

The root cause is that since the reduce happens in-place and since the range
aggregation uses the same instance for all sub-aggregation in case of an
empty bucket, sometimes non-empty buckets would have been reduced into this
shared instance.

In order to avoid similar bugs in the future, aggregations have been updated
to return a new instance when reducing instead of doing it in-place.

Close #6435

src/main/java/org/elasticsearch/search/aggregations/bucket/significant/SignificantLongTerms.java
@@ -83,6 +83,11 @@ public class SignificantLongTerms extends InternalSignificantTerms {
             return Long.toString(term);
         }
 
+        @Override
+        Bucket newBucket(long subsetDf, long subsetSize, long supersetDf, long supersetSize, InternalAggregations aggregations) {
+            return new Bucket(subsetDf, subsetSize, supersetDf, supersetSize, term, aggregations);
+        }
+
     }
 
     private ValueFormatter formatter;
@@ -99,6 +104,12 @@ public class SignificantLongTerms extends InternalSignificantTerms {
     @Override
     public Type type() {
         return TYPE;
+    }
+
+    @Override
+    InternalSignificantTerms newAggregation(long subsetSize, long supersetSize,
+            List<InternalSignificantTerms.Bucket> buckets) {
+        return new SignificantLongTerms(subsetSize, supersetSize, getName(), formatter, requiredSize, supersetSize, buckets);
     }
 
     @Override

src/main/java/org/elasticsearch/search/aggregations/bucket/significant/SignificantStringTerms.java
@@ -85,6 +85,10 @@ public class SignificantStringTerms extends InternalSignificantTerms {
             return termBytes.utf8ToString();
         }
 
+        @Override
+        Bucket newBucket(long subsetDf, long subsetSize, long supersetDf, long supersetSize, InternalAggregations aggregations) {
+            return new Bucket(termBytes, subsetDf, subsetSize, supersetDf, supersetSize, aggregations);
+        }
     }
 
     SignificantStringTerms() {} // for serialization
@@ -97,6 +101,12 @@ public class SignificantStringTerms extends InternalSignificantTerms {
     @Override
     public Type type() {
         return TYPE;
+    }
+
+    @Override
+    InternalSignificantTerms newAggregation(long subsetSize, long supersetSize,
+            List<InternalSignificantTerms.Bucket> buckets) {
+        return new SignificantStringTerms(subsetSize, supersetSize, getName(), requiredSize, supersetSize, buckets);
     }
 
     @Override

src/main/java/org/elasticsearch/search/aggregations/InternalAggregations.java
@@ -61,12 +61,6 @@ public class InternalAggregations implements Aggregations, ToXContent, Streamabl
         this.aggregations = aggregations;
     }
 
-    /** Resets the internal addAggregation */
-    void reset(List<InternalAggregation> aggregations) {
-        this.aggregations = aggregations;
-        this.aggregationsAsMap = null;
-    }
-
     /**
      * Iterates over the {@link Aggregation}s.
      */
@@ -145,20 +139,7 @@ public class InternalAggregations implements Aggregations, ToXContent, Streamabl
             InternalAggregation first = aggregations.get(0); // the list can't be empty as it's created on demand
             reducedAggregations.add(first.reduce(new InternalAggregation.ReduceContext(aggregations, bigArrays)));
         }
-        InternalAggregations result = aggregationsList.get(0);
-        result.reset(reducedAggregations);
-        return result;
-    }
-
-    /**
-     * Reduces this aggregations, effectively propagates the reduce to all the sub aggregations
-     * @param cacheRecycler
-     */
-    public void reduce(BigArrays bigArrays) {
-        for (int i = 0; i < aggregations.size(); i++) {
-            InternalAggregation aggregation = aggregations.get(i);
-            aggregations.set(i, aggregation.reduce(new InternalAggregation.ReduceContext(ImmutableList.of(aggregation), bigArrays)));
-        }
+        return new InternalAggregations(reducedAggregations);
     }
 
     /** The fields required to write this addAggregation to xcontent */

src/main/java/org/elasticsearch/search/aggregations/bucket/InternalSingleBucketAggregation.java
@@ -33,8 +33,8 @@ import java.util.List;
  */
 public abstract class InternalSingleBucketAggregation extends InternalAggregation implements SingleBucketAggregation {
 
-    protected long docCount;
-    protected InternalAggregations aggregations;
+    private long docCount;
+    private InternalAggregations aggregations;
 
     protected InternalSingleBucketAggregation() {} // for serialization
 
@@ -61,26 +61,23 @@ public abstract class InternalSingleBucketAggregation extends InternalAggregatio
         return aggregations;
     }
 
+    /**
+     * Create a <b>new</b> empty sub aggregation. This must be a new instance on each call.
+     */
+    protected abstract InternalSingleBucketAggregation newAggregation(String name, long docCount, InternalAggregations subAggregations);
+
     @Override
     public InternalAggregation reduce(ReduceContext reduceContext) {
         List<InternalAggregation> aggregations = reduceContext.aggregations();
-        if (aggregations.size() == 1) {
-            InternalSingleBucketAggregation reduced = ((InternalSingleBucketAggregation) aggregations.get(0));
-            reduced.aggregations.reduce(reduceContext.bigArrays());
-            return reduced;
-        }
-        InternalSingleBucketAggregation reduced = null;
+        long docCount = 0L;
         List<InternalAggregations> subAggregationsList = new ArrayList<>(aggregations.size());
         for (InternalAggregation aggregation : aggregations) {
-            if (reduced == null) {
-                reduced = (InternalSingleBucketAggregation) aggregation;
-            } else {
-                this.docCount += ((InternalSingleBucketAggregation) aggregation).docCount;
-            }
+            assert aggregation.getName().equals(getName());
+            docCount += ((InternalSingleBucketAggregation) aggregation).docCount;
             subAggregationsList.add(((InternalSingleBucketAggregation) aggregation).aggregations);
         }
-        reduced.aggregations = InternalAggregations.reduce(subAggregationsList, reduceContext.bigArrays());
-        return reduced;
+        final InternalAggregations aggs = InternalAggregations.reduce(subAggregationsList, reduceContext.bigArrays());
+        return newAggregation(getName(), docCount, aggs);
     }
 
     @Override

src/main/java/org/elasticsearch/search/aggregations/bucket/filter/InternalFilter.java
@@ -56,4 +56,8 @@ public class InternalFilter extends InternalSingleBucketAggregation implements F
         return TYPE;
     }
 
+    @Override
+    protected InternalSingleBucketAggregation newAggregation(String name, long docCount, InternalAggregations subAggregations) {
+        return new InternalFilter(name, docCount, subAggregations);
+    }
 }

src/main/java/org/elasticsearch/search/aggregations/bucket/geogrid/InternalGeoHashGrid.java
@@ -106,24 +106,14 @@ public class InternalGeoHashGrid extends InternalAggregation implements GeoHashG
         }
 
         public Bucket reduce(List<? extends Bucket> buckets, BigArrays bigArrays) {
-            if (buckets.size() == 1) {
-                // we still need to reduce the sub aggs
-                Bucket bucket = buckets.get(0);
-                bucket.aggregations.reduce(bigArrays);
-                return bucket;
-            }
-            Bucket reduced = null;
             List<InternalAggregations> aggregationsList = new ArrayList<>(buckets.size());
+            long docCount = 0;
             for (Bucket bucket : buckets) {
-                if (reduced == null) {
-                    reduced = bucket;
-                } else {
-                    reduced.docCount += bucket.docCount;
-                }
+                docCount += bucket.docCount;
                 aggregationsList.add(bucket.aggregations);
             }
-            reduced.aggregations = InternalAggregations.reduce(aggregationsList, bigArrays);
-            return reduced;
+            final InternalAggregations aggs = InternalAggregations.reduce(aggregationsList, bigArrays);
+            return new Bucket(geohashAsLong, docCount, aggs);
         }
 
         @Override
@@ -181,19 +171,10 @@ public class InternalGeoHashGrid extends InternalAggregation implements GeoHashG
     @Override
     public InternalGeoHashGrid reduce(ReduceContext reduceContext) {
         List<InternalAggregation> aggregations = reduceContext.aggregations();
-        if (aggregations.size() == 1) {
-            InternalGeoHashGrid grid = (InternalGeoHashGrid) aggregations.get(0);
-            grid.reduceAndTrimBuckets(reduceContext.bigArrays());
-            return grid;
-        }
-        InternalGeoHashGrid reduced = null;
 
         LongObjectPagedHashMap<List<Bucket>> buckets = null;
         for (InternalAggregation aggregation : aggregations) {
             InternalGeoHashGrid grid = (InternalGeoHashGrid) aggregation;
-            if (reduced == null) {
-                reduced = grid;
-            }
             if (buckets == null) {
                 buckets = new LongObjectPagedHashMap<>(grid.buckets.size(), reduceContext.bigArrays());
             }
@@ -207,12 +188,6 @@ public class InternalGeoHashGrid extends InternalAggregation implements GeoHashG
             }
         }
 
-        if (reduced == null) {
-            // there are only unmapped terms, so we just return the first one (no need to reduce)
-            return (InternalGeoHashGrid) aggregations.get(0);
-        }
-
-        // TODO: would it be better to sort the backing array buffer of the hppc map directly instead of using a PQ?
         final int size = (int) Math.min(requiredSize, buckets.size());
         BucketPriorityQueue ordered = new BucketPriorityQueue(size);
         for (LongObjectPagedHashMap.Cursor<List<Bucket>> cursor : buckets) {
@@ -224,28 +199,7 @@ public class InternalGeoHashGrid extends InternalAggregation implements GeoHashG
         for (int i = ordered.size() - 1; i >= 0; i--) {
             list[i] = ordered.pop();
         }
-        reduced.buckets = Arrays.asList(list);
-        return reduced;
-    }
-
-    protected void reduceAndTrimBuckets(BigArrays bigArrays) {
-
-        if (requiredSize > buckets.size()) { // nothing to trim
-            for (Bucket bucket : buckets) {
-                bucket.aggregations.reduce(bigArrays);
-            }
-            return;
-        }
-
-        List<Bucket> trimmedBuckets = new ArrayList<>(requiredSize);
-        for (Bucket bucket : buckets) {
-            if (trimmedBuckets.size() >= requiredSize) {
-                break;
-            }
-            bucket.aggregations.reduce(bigArrays);
-            trimmedBuckets.add(bucket);
-        }
-        buckets = trimmedBuckets;
+        return new InternalGeoHashGrid(getName(), requiredSize, Arrays.asList(list));
     }
 
     @Override

src/main/java/org/elasticsearch/search/aggregations/bucket/global/InternalGlobal.java
@@ -56,4 +56,9 @@ public class InternalGlobal extends InternalSingleBucketAggregation implements G
     public Type type() {
         return TYPE;
     }
+
+    @Override
+    protected InternalSingleBucketAggregation newAggregation(String name, long docCount, InternalAggregations subAggregations) {
+        return new InternalGlobal(name, docCount, subAggregations);
+    }
 }

src/main/java/org/elasticsearch/search/aggregations/bucket/histogram/InternalDateHistogram.java
@@ -58,6 +58,11 @@ public class InternalDateHistogram extends InternalHistogram<InternalDateHistogr
         }
 
         @Override
+        protected InternalHistogram.Factory<Bucket> getFactory() {
+            return FACTORY;
+        }
+
+        @Override
         public String getKey() {
             return formatter != null ? formatter.format(key) : ValueFormatter.DateTime.DEFAULT.format(key);
         }
@@ -107,6 +112,11 @@ public class InternalDateHistogram extends InternalHistogram<InternalDateHistogr
     @Override
     public Type type() {
         return TYPE;
+    }
+
+    @Override
+    protected InternalHistogram.Factory<Bucket> getFactory() {
+        return FACTORY;
     }
 
     @Override

commit 49f40d5a814e18e5ee70ca6c84492c044f05f092

significant terms: fix json response
Commit fbd7c9aa5d6c1c introduced a regression that caused
the min_doc_count to be equal to the number of documents in the
background set. As a result no buckets were built when the
response for significant terms was created.
This only affected the final XContent response.

src/main/java/org/elasticsearch/search/aggregations/bucket/significant/SignificantLongTerms.java
@@ -109,7 +109,7 @@ public class SignificantLongTerms extends InternalSignificantTerms {
     @Override
     InternalSignificantTerms newAggregation(long subsetSize, long supersetSize,
             List<InternalSignificantTerms.Bucket> buckets) {
-        return new SignificantLongTerms(subsetSize, supersetSize, getName(), formatter, requiredSize, supersetSize, buckets);
+        return new SignificantLongTerms(subsetSize, supersetSize, getName(), formatter, requiredSize, minDocCount, buckets);
     }
 
     @Override

src/main/java/org/elasticsearch/search/aggregations/bucket/significant/SignificantStringTerms.java
@@ -106,7 +106,7 @@ public class SignificantStringTerms extends InternalSignificantTerms {
     @Override
     InternalSignificantTerms newAggregation(long subsetSize, long supersetSize,
             List<InternalSignificantTerms.Bucket> buckets) {
-        return new SignificantStringTerms(subsetSize, supersetSize, getName(), requiredSize, supersetSize, buckets);
+        return new SignificantStringTerms(subsetSize, supersetSize, getName(), requiredSize, minDocCount, buckets);
     }
 
     @Override

rest-api-spec/test/search/test_sig_terms.yaml
@@ -1,0 +1,74 @@
+---
+"Default index":
+  - do:
+      indices.create:
+          index:  goodbad
+          body:
+            settings:
+                number_of_shards: "1"
+
+  - do:
+      index:
+          index:  goodbad
+          type:   doc
+          id:     1
+          body:   { text: "good", class: "good" }
+  - do:
+      index:
+          index:  goodbad
+          type:   doc
+          id:     2
+          body:   { text: "good", class: "good" }
+  - do:
+      index:
+          index:  goodbad
+          type:   doc
+          id:     3
+          body:   { text: "bad", class: "bad" }
+  - do:
+      index:
+          index:  goodbad
+          type:   doc
+          id:     4
+          body:   { text: "bad", class: "bad" }
+  - do:
+      index:
+          index:  goodbad
+          type:   doc
+          id:     5
+          body:   { text: "good bad", class: "good" }
+  - do:
+      index:
+          index:  goodbad
+          type:   doc
+          id:     6
+          body:   { text: "good bad", class: "bad" }
+  - do:
+      index:
+          index:  goodbad
+          type:   doc
+          id:     7
+          body:   { text: "bad", class: "bad" }
+
+
+
+  - do:
+      indices.refresh:
+        index: [goodbad]
+
+  - do:
+      search:
+        index: goodbad
+        type:  doc
+
+  - match: {hits.total: 7}
+  
+  - do:
+      search:
+        index: goodbad
+        type:  doc
+        body: {"aggs": {"class": {"terms": {"field": "class"},"aggs": {"sig_terms": {"significant_terms": {"field": "text"}}}}}}
+
+  - match: {aggregations.class.buckets.0.sig_terms.buckets.0.key: "bad"}
+  - match: {aggregations.class.buckets.0.sig_terms.buckets.0.key: "good"}
+  

src/test/java/org/elasticsearch/search/aggregations/bucket/SignificantTermsTests.java
@@ -23,12 +23,18 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.bucket.significant.SignificantStringTerms;
 import org.elasticsearch.search.aggregations.bucket.significant.SignificantTerms;
 import org.elasticsearch.search.aggregations.bucket.significant.SignificantTerms.Bucket;
 import org.elasticsearch.search.aggregations.bucket.significant.SignificantTermsAggregatorFactory.ExecutionMode;
 import org.elasticsearch.search.aggregations.bucket.significant.SignificantTermsBuilder;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsBuilder;
 import org.elasticsearch.test.ElasticsearchIntegrationTest;
@@ -36,6 +42,7 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import static org.elasticsearch.cluster.metadata.IndexMetaData.SETTING_NUMBER_OF_REPLICAS;
@@ -300,4 +307,58 @@ public class SignificantTermsTests extends ElasticsearchIntegrationTest {
         assertEquals(4, kellyTerm.getSupersetDf());
     }
 
+    @Test
+    public void testXContentResponse() throws Exception {
+        String type = randomBoolean() ? "string" : "long";
+        prepareGoodBad(type);
+        SearchResponse response = client().prepareSearch("goodbad").setTypes("doc")
+                .addAggregation(new TermsBuilder("class").field("class").subAggregation(new SignificantTermsBuilder("sig_terms").field("text")))
+                .execute()
+                .actionGet();
+        assertSearchResponse(response);
+        StringTerms classes = (StringTerms) response.getAggregations().get("class");
+        assertThat(classes.getBuckets().size(), equalTo(2));
+        for (Terms.Bucket classBucket : classes.getBuckets()) {
+            Map<String, Aggregation> aggs = classBucket.getAggregations().asMap();
+            assertTrue(aggs.containsKey("sig_terms"));
+            SignificantTerms agg = (SignificantTerms) aggs.get("sig_terms");
+            assertThat(agg.getBuckets().size(), equalTo(1));
+            String term = agg.iterator().next().getKey();
+            String classTerm = classBucket.getKey();
+            assertTrue(term.equals(classTerm));
+        }
+
+        XContentBuilder responseBuilder = XContentFactory.jsonBuilder();
+        classes.toXContent(responseBuilder, null);
+        String result = null;
+        if (type.equals("long")) {
+            result = "\"class\"{\"buckets\":[{\"key\":\"0\",\"doc_count\":4,\"sig_terms\":{\"doc_count\":4,\"buckets\":[{\"key\":0,\"key_as_string\":\"0\",\"doc_count\":4,\"score\":0.39999999999999997,\"bg_count\":5}]}},{\"key\":\"1\",\"doc_count\":3,\"sig_terms\":{\"doc_count\":3,\"buckets\":[{\"key\":1,\"key_as_string\":\"1\",\"doc_count\":3,\"score\":0.75,\"bg_count\":4}]}}]}";
+        } else {
+            result = "\"class\"{\"buckets\":[{\"key\":\"0\",\"doc_count\":4,\"sig_terms\":{\"doc_count\":4,\"buckets\":[{\"key\":\"0\",\"doc_count\":4,\"score\":0.39999999999999997,\"bg_count\":5}]}},{\"key\":\"1\",\"doc_count\":3,\"sig_terms\":{\"doc_count\":3,\"buckets\":[{\"key\":\"1\",\"doc_count\":3,\"score\":0.75,\"bg_count\":4}]}}]}";
+        }
+        assertThat(responseBuilder.string(), equalTo(result));
+
+    }
+
+    private void prepareGoodBad(String type) {
+        String mappings = "{\"doc\": {\"properties\":{\"text\": {\"type\":\"" + type + "\"}}}}";
+        logger.info(mappings);
+        assertAcked(prepareCreate("goodbad").setSettings(SETTING_NUMBER_OF_SHARDS, 1, SETTING_NUMBER_OF_REPLICAS, 0).addMapping("doc", mappings));
+        String[] gb = {"0", "1"};
+        client().prepareIndex("goodbad", "doc", "1")
+                .setSource("text", "1", "class", "1").get();
+        client().prepareIndex("goodbad", "doc", "2")
+                .setSource("text", "1", "class", "1").get();
+        client().prepareIndex("goodbad", "doc", "3")
+                .setSource("text", "0", "class", "0").get();
+        client().prepareIndex("goodbad", "doc", "4")
+                .setSource("text", "0", "class", "0").get();
+        client().prepareIndex("goodbad", "doc", "5")
+                .setSource("text", gb, "class", "1").get();
+        client().prepareIndex("goodbad", "doc", "6")
+                .setSource("text", gb, "class", "0").get();
+        client().prepareIndex("goodbad", "doc", "7")
+                .setSource("text", "0", "class", "0").get();
+        refresh();
+    }
 }
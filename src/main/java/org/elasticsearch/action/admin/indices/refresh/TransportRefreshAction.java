134732075242d61ca5ebc7468e7f3df443e6236d
Boaz Leskes <b.leskes@gmail.com>
2014-06-19 11:10:13 +0200

    [TEST] Added (trace) logging to testGetFields_complexField

--- src/main/java/org/elasticsearch/action/admin/indices/refresh/TransportRefreshAction.java
+++ src/main/java/org/elasticsearch/action/admin/indices/refresh/TransportRefreshAction.java
@@ -122,7 +122,6 @@ public class TransportRefreshAction extends TransportBroadcastOperationAction<Re
      */
     @Override
     protected GroupShardsIterator shards(ClusterState clusterState, RefreshRequest request, String[] concreteIndices) {
-        logger.trace("resolving shards to refresh based on cluster state version [{}]", clusterState.version());
         return clusterState.routingTable().allAssignedShardsGrouped(concreteIndices, true, true);
     }
 

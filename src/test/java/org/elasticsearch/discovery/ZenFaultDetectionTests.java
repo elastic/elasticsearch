838e2c877df354052081184fc18f064d7aa87f25
Boaz Leskes <b.leskes@gmail.com>
2014-06-26 10:58:15 +0200

    Fixed compilation issue caused by the lack of a thread pool name

--- src/test/java/org/elasticsearch/discovery/ZenFaultDetectionTests.java
+++ src/test/java/org/elasticsearch/discovery/ZenFaultDetectionTests.java
@@ -60,7 +60,7 @@ public class ZenFaultDetectionTests extends ElasticsearchTestCase {
     @Before
     public void setUp() throws Exception {
         super.setUp();
-        threadPool = new ThreadPool();
+        threadPool = new ThreadPool(getClass().getName());
         serviceA = build(ImmutableSettings.builder().put("name", "TS_A").build(), version0);
         nodeA = new DiscoveryNode("TS_A", "TS_A", serviceA.boundAddress().publishAddress(), ImmutableMap.<String, String>of(), version0);
         serviceB = build(ImmutableSettings.builder().put("name", "TS_B").build(), version1);

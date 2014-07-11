2c0e9b819e0e430b18e23c88d70483df58c9d32a
Areek Zillur <areek.zillur@elasticsearch.com>
2014-07-11 17:07:38 -0400

    Stats: Expose IndexWriter and VersionMap RAM usage to ShardStats and _cat endpoint
    This commit adds the RAM usage of IndexWriter and VersionMap
    
    Closes #6483

--- src/main/java/org/elasticsearch/rest/action/cat/RestNodesAction.java
+++ src/main/java/org/elasticsearch/rest/action/cat/RestNodesAction.java
@@ -169,6 +169,8 @@ public class RestNodesAction extends AbstractCatAction {
 
         table.addCell("segments.count", "alias:sc,segmentsCount;default:false;text-align:right;desc:number of segments");
         table.addCell("segments.memory", "alias:sm,segmentsMemory;default:false;text-align:right;desc:memory used by segments");
+        table.addCell("segments.index_writer_memory", "alias:siwm,segmentsIndexWriterMemory;default:false;text-align:right;desc:memory used by index writer");
+        table.addCell("segments.version_map_memory", "alias:svmm,segmentsVersionMapMemory;default:false;text-align:right;desc:memory used by version map");
 
         table.addCell("suggest.current", "alias:suc,suggestCurrent;default:false;text-align:right;desc:number of current suggest ops");
         table.addCell("suggest.time", "alias:suti,suggestTime;default:false;text-align:right;desc:time spend in suggest");
@@ -271,6 +273,8 @@ public class RestNodesAction extends AbstractCatAction {
 
             table.addCell(stats == null ? null : stats.getIndices().getSegments().getCount());
             table.addCell(stats == null ? null : stats.getIndices().getSegments().getMemory());
+            table.addCell(stats == null ? null : stats.getIndices().getSegments().getIndexWriterMemory());
+            table.addCell(stats == null ? null : stats.getIndices().getSegments().getVersionMapMemory());
 
             table.addCell(stats == null ? null : stats.getIndices().getSuggest().getCurrent());
             table.addCell(stats == null ? null : stats.getIndices().getSuggest().getTime());

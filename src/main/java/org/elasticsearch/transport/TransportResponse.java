071b51f992bf30bdf7e4af27a241097add593b07
uboness <uboness@gmail.com>
2014-07-11 14:40:51 +0200

    Cleanup of the transport request/response messages
    Now both TransportRequest and TransportResponse inherit from a base TransportMessage that holds the message headers and also now added the remote transport address (where this message came from).

--- src/main/java/org/elasticsearch/transport/TransportResponse.java
+++ src/main/java/org/elasticsearch/transport/TransportResponse.java
@@ -19,18 +19,9 @@
 
 package org.elasticsearch.transport;
 
-import com.google.common.collect.Maps;
-import org.elasticsearch.common.io.stream.StreamInput;
-import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.common.io.stream.Streamable;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  */
-public abstract class TransportResponse implements Streamable {
+public abstract class TransportResponse extends TransportMessage<TransportResponse> {
 
     public static class Empty extends TransportResponse {
 
@@ -45,55 +36,11 @@ public abstract class TransportResponse implements Streamable {
         }
     }
 
-    private Map<String, Object> headers;
-
     protected TransportResponse() {
-
     }
 
-    protected TransportResponse(TransportResponse request) {
-        // create a new copy of the headers, since we are creating a new request which might have
-        // its headers changed in the context of that specific request
-        if (request.getHeaders() != null) {
-            this.headers = new HashMap<>(request.getHeaders());
-        }
+    protected TransportResponse(TransportResponse response) {
+        super(response);
     }
 
-    @SuppressWarnings("unchecked")
-    public final TransportResponse putHeader(String key, Object value) {
-        if (headers == null) {
-            headers = Maps.newHashMap();
-        }
-        headers.put(key, value);
-        return this;
-    }
-
-    @SuppressWarnings("unchecked")
-    public final <V> V getHeader(String key) {
-        if (headers == null) {
-            return null;
-        }
-        return (V) headers.get(key);
-    }
-
-    public Map<String, Object> getHeaders() {
-        return this.headers;
-    }
-
-    @Override
-    public void readFrom(StreamInput in) throws IOException {
-        if (in.readBoolean()) {
-            headers = in.readMap();
-        }
-    }
-
-    @Override
-    public void writeTo(StreamOutput out) throws IOException {
-        if (headers == null) {
-            out.writeBoolean(false);
-        } else {
-            out.writeBoolean(true);
-            out.writeMap(headers);
-        }
-    }
 }

15362a38ec71a802e5a2af1686e6bcfcabbc6054
uboness <uboness@gmail.com>
2014-07-12 16:43:35 +0200

    Added more utility methods to Settings
    - names() to return the direct settings names
    - getAsSettings(String) to return the settings mapped to the given name (like getByPrefix(...) except no need to provide a tailing '.')

--- src/main/java/org/elasticsearch/common/settings/ImmutableSettings.java
+++ src/main/java/org/elasticsearch/common/settings/ImmutableSettings.java
@@ -219,6 +219,11 @@ public class ImmutableSettings implements Settings {
     }
 
     @Override
+    public Settings getAsSettings(String setting) {
+        return getByPrefix(setting + ".");
+    }
+
+    @Override
     public String get(String setting) {
         String retVal = settings.get(setting);
         if (retVal != null) {
@@ -561,6 +566,20 @@ public class ImmutableSettings implements Settings {
         } catch (Exception e) {
             throw new SettingsException("Failed to parse version setting [" + setting + "] with value [" + sValue + "]", e);
         }
+    }
+
+    @Override
+    public Set<String> names() {
+        Set<String> names = new HashSet<>();
+        for (String key : settings.keySet()) {
+            int i = key.indexOf(".");
+            if (i < 0) {
+                names.add(key);
+            } else {
+                names.add(key.substring(0, i));
+            }
+        }
+        return names;
     }
 
     @Override

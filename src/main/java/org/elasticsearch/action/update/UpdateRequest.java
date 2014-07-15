37c7b777b7582a9b8f3ced72cb3cbb85f48ad3a9
Simon Willnauer <simonw@apache.org>
2014-07-15 12:13:45 +0200

    Added missing version based serialization

--- src/main/java/org/elasticsearch/action/update/UpdateRequest.java
+++ src/main/java/org/elasticsearch/action/update/UpdateRequest.java
@@ -651,7 +651,7 @@ public class UpdateRequest extends InstanceShardOperationRequest<UpdateRequest> 
         out.writeString(id);
         out.writeOptionalString(routing);
         out.writeOptionalString(script);
-        if (script != null) {
+        if (script != null && out.getVersion().onOrAfter(Version.V_1_3_0)) {
             ScriptService.ScriptType.writeTo(scriptType, out);
         }
         out.writeOptionalString(scriptLang);

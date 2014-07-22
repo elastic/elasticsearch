c55197baf2c4627e01178ac157deb0fdc3eab423
Brian Murphy <brian.p.murphy@gmail.com>
2014-07-22 14:22:57 +0100

    [FIX] Fix update parser to accept script_id

--- src/main/java/org/elasticsearch/action/update/UpdateRequest.java
+++ src/main/java/org/elasticsearch/action/update/UpdateRequest.java
@@ -574,6 +574,10 @@ public class UpdateRequest extends InstanceShardOperationRequest<UpdateRequest> 
                     currentFieldName = parser.currentName();
                 } else if ("script".equals(currentFieldName)) {
                     script = parser.textOrNull();
+                    scriptType = ScriptService.ScriptType.INLINE;
+                } else if ("script_id".equals(currentFieldName)) {
+                    script = parser.textOrNull();
+                    scriptType = ScriptService.ScriptType.INDEXED;
                 } else if ("params".equals(currentFieldName)) {
                     scriptParams = parser.map();
                 } else if ("lang".equals(currentFieldName)) {

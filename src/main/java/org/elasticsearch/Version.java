3c9eac908b688f1849c66073250c25d288934d20
Simon Willnauer <simonw@apache.org>
2014-07-23 13:44:06 +0000

    release [1.3.0]

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -187,7 +187,7 @@ public class Version implements Serializable {
     public static final int V_1_2_3_ID = /*00*/1020399;
     public static final Version V_1_2_3 = new Version(V_1_2_3_ID, false, org.apache.lucene.util.Version.LUCENE_4_8);
     public static final int V_1_3_0_ID = /*00*/1030099;
-    public static final Version V_1_3_0 = new Version(V_1_3_0_ID, true, org.apache.lucene.util.Version.LUCENE_4_9);
+    public static final Version V_1_3_0 = new Version(V_1_3_0_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
 
     public static final Version CURRENT = V_1_3_0;
 

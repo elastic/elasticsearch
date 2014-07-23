8b15947e8a00133dc70ade206db6620f299573d1
Simon Willnauer <simonw@apache.org>
2014-07-23 16:13:00 +0200

    move to [1.2.4-SNAPSHOT]

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -184,8 +184,10 @@ public class Version implements Serializable {
     public static final Version V_1_2_2 = new Version(V_1_2_2_ID, false, org.apache.lucene.util.Version.LUCENE_48);
     public static final int V_1_2_3_ID = /*00*/1020399;
     public static final Version V_1_2_3 = new Version(V_1_2_3_ID, false, org.apache.lucene.util.Version.LUCENE_48);
+    public static final int V_1_2_4_ID = /*00*/1020499;
+    public static final Version V_1_2_4 = new Version(V_1_2_4_ID, true, org.apache.lucene.util.Version.LUCENE_48);
 
-    public static final Version CURRENT = V_1_2_3;
+    public static final Version CURRENT = V_1_2_4;
 
     static {
         assert CURRENT.luceneVersion == Lucene.VERSION : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
@@ -197,6 +199,8 @@ public class Version implements Serializable {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_1_2_4_ID:
+                return V_1_2_4;
             case V_1_2_3_ID:
                 return V_1_2_3;
             case V_1_2_2_ID:

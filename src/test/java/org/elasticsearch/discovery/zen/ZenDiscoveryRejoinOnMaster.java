24dd1467d6f103072169643d065a88898aee8b03
Martijn van Groningen <martijn.v.groningen@gmail.com>
2014-07-25 17:31:45 +0200

    [TEST] Added test that verifies that no shard relocations happen during / after a master re-election.

--- src/test/java/org/elasticsearch/discovery/zen/ZenDiscoveryRejoinOnMaster.java
+++ src/test/java/org/elasticsearch/discovery/zen/ZenDiscoveryRejoinOnMaster.java
@@ -19,12 +19,17 @@
 
 package org.elasticsearch.discovery.zen;
 
+import com.google.common.base.Predicate;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.action.admin.indices.recovery.RecoveryResponse;
+import org.elasticsearch.common.Priority;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.discovery.Discovery;
 import org.elasticsearch.test.ElasticsearchIntegrationTest;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 /**
@@ -33,7 +38,7 @@ import static org.hamcrest.Matchers.is;
 public class ZenDiscoveryRejoinOnMaster extends ElasticsearchIntegrationTest {
 
     @Test
-    public void testChangeRejoinOnMaster() throws Exception {
+    public void testChangeRejoinOnMasterOptionIsDynamic() throws Exception {
         Settings nodeSettings = ImmutableSettings.settingsBuilder()
                 .put("discovery.type", "zen") // <-- To override the local setting if set externally
                 .build();
@@ -46,6 +51,53 @@ public class ZenDiscoveryRejoinOnMaster extends ElasticsearchIntegrationTest {
                 .get();
 
         assertThat(zenDiscovery.isRejoinOnMasterGone(), is(false));
+    }
+
+    @Test
+    public void testNoShardRelocationsOccurWhenElectedMasterNodeFails() throws Exception {
+        Settings defaultSettings = ImmutableSettings.builder()
+                .put("discovery.zen.fd.ping_timeout", "1s")
+                .put("discovery.zen.fd.ping_retries", "1")
+                .put("discovery.type", "zen")
+                .build();
+
+        Settings masterNodeSettings = ImmutableSettings.builder()
+                .put("node.data", false)
+                .put(defaultSettings)
+                .build();
+        internalCluster().startNodesAsync(2, masterNodeSettings).get();
+        Settings dateNodeSettings = ImmutableSettings.builder()
+                .put("node.master", false)
+                .put(defaultSettings)
+                .build();
+        internalCluster().startNodesAsync(2, dateNodeSettings).get();
+        ClusterHealthResponse clusterHealthResponse = client().admin().cluster().prepareHealth()
+                .setWaitForEvents(Priority.LANGUID)
+                .setWaitForNodes("4")
+                .setWaitForRelocatingShards(0)
+                .get();
+        assertThat(clusterHealthResponse.isTimedOut(), is(false));
+
+        createIndex("test");
+        ensureSearchable("test");
+        RecoveryResponse r = client().admin().indices().prepareRecoveries("test").get();
+        int numRecoveriesBeforeNewMaster = r.shardResponses().get("test").size();
+
+        final String oldMaster = internalCluster().getMasterName();
+        internalCluster().stopCurrentMasterNode();
+        boolean result = awaitBusy(new Predicate<Object>() {
+            @Override
+            public boolean apply(Object input) {
+                String current = internalCluster().getMasterName();
+                return current != null && !current.equals(oldMaster);
+            }
+        });
+        assertTrue(result);
+        ensureSearchable("test");
+
+        r = client().admin().indices().prepareRecoveries("test").get();
+        int numRecoveriesAfterNewMaster = r.shardResponses().get("test").size();
+        assertThat(numRecoveriesAfterNewMaster, equalTo(numRecoveriesBeforeNewMaster));
     }
 
 }

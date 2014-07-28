f264bb0417a780b239a19b788765d94fe4de6b62
Lee Hinman <lee@writequit.org>
2014-07-28 12:10:13 +0200

    Change logging level for circuit breaking to warn

--- src/main/java/org/elasticsearch/common/breaker/ChildMemoryCircuitBreaker.java
+++ src/main/java/org/elasticsearch/common/breaker/ChildMemoryCircuitBreaker.java
@@ -139,7 +139,7 @@ public class ChildMemoryCircuitBreaker implements CircuitBreaker {
                             newUsedWithOverhead, new ByteSizeValue(newUsedWithOverhead));
                 }
                 if (memoryBytesLimit > 0 && newUsedWithOverhead > memoryBytesLimit) {
-                    logger.error("[{}] New used memory {} [{}] from field [{}] would be larger than configured breaker: {} [{}], breaking",
+                    logger.warn("[{}] New used memory {} [{}] from field [{}] would be larger than configured breaker: {} [{}], breaking",
                             this.name,
                             newUsedWithOverhead, new ByteSizeValue(newUsedWithOverhead), label,
                             memoryBytesLimit, new ByteSizeValue(memoryBytesLimit));

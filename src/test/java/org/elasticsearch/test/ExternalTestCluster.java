150117ef4a53a028c85f85869de971ddd5db2793
Martijn van Groningen <martijn.v.groningen@gmail.com>
2014-07-29 19:13:03 +0200

    Percolator should cache index field data instances.
    Before the index reader used by the percolator didn't allow to register a CoreCloseListener, but now it does, making it safe to cache index field data cache entries.
    Creating field data structures is relatively expensive and caching them can save a lot of noise if many queries are evaluated in a percolator call.
    
    Closes #6806
    Closes #7081

--- src/test/java/org/elasticsearch/test/ExternalTestCluster.java
+++ src/test/java/org/elasticsearch/test/ExternalTestCluster.java
@@ -120,13 +120,16 @@ public final class ExternalTestCluster extends TestCluster {
     public void ensureEstimatedStats() {
         if (size() > 0) {
             NodesStatsResponse nodeStats = client().admin().cluster().prepareNodesStats()
-                    .clear().setBreaker(true).execute().actionGet();
+                    .clear().setBreaker(true).setIndices(true).execute().actionGet();
             for (NodeStats stats : nodeStats.getNodes()) {
                 assertThat("Fielddata breaker not reset to 0 on node: " + stats.getNode(),
                         stats.getBreaker().getStats(CircuitBreaker.Name.FIELDDATA).getEstimated(), equalTo(0L));
                 // ExternalTestCluster does not check the request breaker,
                 // because checking it requires a network request, which in
                 // turn increments the breaker, making it non-0
+
+                assertThat("Fielddata size must be 0 on node: " + stats.getNode(), stats.getIndices().getFieldData().getMemorySizeInBytes(), equalTo(0l));
+                assertThat("Filter cache size must be 0 on node: " + stats.getNode(), stats.getIndices().getFilterCache().getMemorySizeInBytes(), equalTo(0l));
             }
         }
     }

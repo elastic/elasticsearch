ae9d6092bdb00e8a31faf227e87d4838e1fe527b
Martijn van Groningen <martijn.v.groningen@gmail.com>
2014-08-07 23:50:23 +0200

    [TEST] Associate type with percolate query

--- src/test/java/org/elasticsearch/percolator/PercolatorTests.java
+++ src/test/java/org/elasticsearch/percolator/PercolatorTests.java
@@ -1720,14 +1720,12 @@ public class PercolatorTests extends ElasticsearchIntegrationTest {
 
         assertMatchCount(percolateResponse, 0l);
         assertThat(percolateResponse.getMatches(), arrayWithSize(0));
-
-        ensureYellow("idx");
         waitForConcreteMappingsOnAll("idx", "type", "custom.color");
 
         // The previous percolate request introduced the custom.color field, so now we register the query again
         // and the field name `color` will be resolved to `custom.color` field in mapping via smart field mapping resolving.
         client().prepareIndex("idx", PercolatorService.TYPE_NAME, "2")
-                .setSource(jsonBuilder().startObject().field("query", QueryBuilders.queryString("color:blue")).endObject())
+                .setSource(jsonBuilder().startObject().field("query", QueryBuilders.queryString("color:blue")).field("type", "type").endObject())
                 .get();
 
         // The second request will yield a match, since the query during the proper field during parsing.

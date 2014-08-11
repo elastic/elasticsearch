218ca660e7e3fdb8e69801539c306835e0d91d3c
Martijn van Groningen <martijn.v.groningen@gmail.com>
2014-08-11 17:14:07 +0200

    Mappings: Make sure that multi fields are serialized in alphabetic order to ensure that the source is always the same.
    Closes #7215

--- src/main/java/org/elasticsearch/index/mapper/core/AbstractFieldMapper.java
+++ src/main/java/org/elasticsearch/index/mapper/core/AbstractFieldMapper.java
@@ -59,10 +59,7 @@ import org.elasticsearch.index.similarity.SimilarityLookupService;
 import org.elasticsearch.index.similarity.SimilarityProvider;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 /**
  *
@@ -991,9 +988,17 @@ public abstract class AbstractFieldMapper<T> implements FieldMapper<T> {
                 builder.field("path", pathType.name().toLowerCase(Locale.ROOT));
             }
             if (!mappers.isEmpty()) {
+                // sort the mappers so we get consistent serialization format
+                Mapper[] sortedMappers = mappers.values().toArray(Mapper.class);
+                Arrays.sort(sortedMappers, new Comparator<Mapper>() {
+                    @Override
+                    public int compare(Mapper o1, Mapper o2) {
+                        return o1.name().compareTo(o2.name());
+                    }
+                });
                 builder.startObject("fields");
-                for (ObjectCursor<Mapper> cursor : mappers.values()) {
-                    cursor.value.toXContent(builder, params);
+                for (Mapper mapper : sortedMappers) {
+                    mapper.toXContent(builder, params);
                 }
                 builder.endObject();
             }

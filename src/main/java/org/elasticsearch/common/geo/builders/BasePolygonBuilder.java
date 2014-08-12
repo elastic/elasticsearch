9211d0459285e04b0238e718a519ac089d40cce4
Colin Goodheart-Smithe <colin.goodheart-smithe@elasticsearch.com>
2014-08-12 11:04:07 +0100

    Fix for failing BasePolygonBuilder

--- src/main/java/org/elasticsearch/common/geo/builders/BasePolygonBuilder.java
+++ src/main/java/org/elasticsearch/common/geo/builders/BasePolygonBuilder.java
@@ -359,7 +359,7 @@ public abstract class BasePolygonBuilder<E extends BasePolygonBuilder<E>> extend
             current.intersect = current.coordinate;
             final int intersections = intersections(current.coordinate.x, edges);
             final int pos = Arrays.binarySearch(edges, 0, intersections, current, INTERSECTION_ORDER);
-            if (pos < 0) {
+            if (pos >= 0) {
                 throw new ElasticsearchParseException("Invaild shape: Hole is not within polygon");
             }
             final int index = -(pos+2);

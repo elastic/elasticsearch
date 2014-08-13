cc819f206ba5261e0be3d716f38d7273f65b7cce
Ryan Ernst <ryan@iernst.net>
2014-08-13 08:02:55 -0700

    Add [1.3.3] and [1.2.5] version constants.

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -188,12 +188,16 @@ public class Version implements Serializable {
     public static final Version V_1_2_3 = new Version(V_1_2_3_ID, false, org.apache.lucene.util.Version.LUCENE_4_8);
     public static final int V_1_2_4_ID = /*00*/1020499;
     public static final Version V_1_2_4 = new Version(V_1_2_4_ID, false, org.apache.lucene.util.Version.LUCENE_4_8);
+    public static final int V_1_2_5_ID = /*00*/1020599;
+    public static final Version V_1_2_5 = new Version(V_1_2_5_ID, false, org.apache.lucene.util.Version.LUCENE_4_8);
     public static final int V_1_3_0_ID = /*00*/1030099;
     public static final Version V_1_3_0 = new Version(V_1_3_0_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
     public static final int V_1_3_1_ID = /*00*/1030199;
     public static final Version V_1_3_1 = new Version(V_1_3_1_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
     public static final int V_1_3_2_ID = /*00*/1030299;
     public static final Version V_1_3_2 = new Version(V_1_3_2_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
+    public static final int V_1_3_3_ID = /*00*/1030399;
+    public static final Version V_1_3_3 = new Version(V_1_3_3_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
     public static final int V_1_4_0_ID = /*00*/1040099;
     public static final Version V_1_4_0 = new Version(V_1_4_0_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
     public static final int V_2_0_0_ID = /*00*/2000099;
@@ -215,12 +219,16 @@ public class Version implements Serializable {
                 return V_2_0_0;
             case V_1_4_0_ID:
                 return V_1_4_0;
+            case V_1_3_3_ID:
+                return V_1_3_3;
             case V_1_3_2_ID:
                 return V_1_3_2;
             case V_1_3_1_ID:
                 return V_1_3_1;
             case V_1_3_0_ID:
                 return V_1_3_0;
+            case V_1_2_5_ID:
+                return V_1_2_5;
             case V_1_2_4_ID:
                 return V_1_2_4;
             case V_1_2_3_ID:

bfbdc61e3b5be07b38f5ef5255f4f144e6501fd6
Ryan Ernst <ryan@iernst.net>
2014-08-13 08:02:55 -0700

    Add [1.2.5] version constant.

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -186,8 +186,10 @@ public class Version implements Serializable {
     public static final Version V_1_2_3 = new Version(V_1_2_3_ID, false, org.apache.lucene.util.Version.LUCENE_48);
     public static final int V_1_2_4_ID = /*00*/1020499;
     public static final Version V_1_2_4 = new Version(V_1_2_4_ID, false, org.apache.lucene.util.Version.LUCENE_48);
+    public static final int V_1_2_5_ID = /*00*/1020599;
+    public static final Version V_1_2_5 = new Version(V_1_2_5_ID, false, org.apache.lucene.util.Version.LUCENE_48);
 
-    public static final Version CURRENT = V_1_2_4;
+    public static final Version CURRENT = V_1_2_5;
 
     static {
         assert CURRENT.luceneVersion == Lucene.VERSION : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
@@ -199,6 +201,8 @@ public class Version implements Serializable {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_1_2_5_ID:
+                return V_1_2_5;
             case V_1_2_4_ID:
                 return V_1_2_4;
             case V_1_2_3_ID:

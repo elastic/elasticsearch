dee175dbe2f254f3f26992f5d7591939aaefd12f
Kevin Kluge <kevin.kluge@elasticsearch.com>
2014-08-13 14:27:50 +0000

    release [1.3.2]

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -193,7 +193,7 @@ public class Version implements Serializable {
     public static final int V_1_3_1_ID = /*00*/1030199;
     public static final Version V_1_3_1 = new Version(V_1_3_1_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
     public static final int V_1_3_2_ID = /*00*/1030299;
-    public static final Version V_1_3_2 = new Version(V_1_3_2_ID, true, org.apache.lucene.util.Version.LUCENE_4_9);
+    public static final Version V_1_3_2 = new Version(V_1_3_2_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
 
     public static final Version CURRENT = V_1_3_2;
 

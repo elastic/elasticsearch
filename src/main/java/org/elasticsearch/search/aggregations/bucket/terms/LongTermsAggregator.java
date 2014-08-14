537bf7d3cc74b60742a515cac3efcd5c36020926
Adrien Grand <jpountz@gmail.com>
2014-08-14 13:46:26 +0200

    Aggregations: Merge LongTermsAggregator and DoubleTermsAggregator.
    These two aggregators basically do exactly the same thing, they just interpret
    bytes differently. This refactoring found an (unreleased) bug in the long terms
    aggregator which didn't work correctly with duplicate values.
    
    Close #7279

--- src/main/java/org/elasticsearch/search/aggregations/bucket/terms/LongTermsAggregator.java
+++ src/main/java/org/elasticsearch/search/aggregations/bucket/terms/LongTermsAggregator.java
@@ -63,9 +63,13 @@ public class LongTermsAggregator extends TermsAggregator {
         return true;
     }
 
+    protected SortedNumericDocValues getValues(ValuesSource.Numeric valuesSource) {
+        return valuesSource.longValues();
+    }
+
     @Override
     public void setNextReader(AtomicReaderContext reader) {
-        values = valuesSource.longValues();
+        values = getValues(valuesSource);
     }
 
     @Override
@@ -77,14 +81,14 @@ public class LongTermsAggregator extends TermsAggregator {
         long previous = Long.MAX_VALUE;
         for (int i = 0; i < valuesCount; ++i) {
             final long val = values.valueAt(i);
-            if (previous != val || i != 0) {
-            long bucketOrdinal = bucketOrds.add(val);
-            if (bucketOrdinal < 0) { // already seen
-                bucketOrdinal = - 1 - bucketOrdinal;
-                collectExistingBucket(doc, bucketOrdinal);
-            } else {
-                collectBucket(doc, bucketOrdinal);
-            }
+            if (previous != val || i == 0) {
+                long bucketOrdinal = bucketOrds.add(val);
+                if (bucketOrdinal < 0) { // already seen
+                    bucketOrdinal = - 1 - bucketOrdinal;
+                    collectExistingBucket(doc, bucketOrdinal);
+                } else {
+                    collectBucket(doc, bucketOrdinal);
+                }
                 previous = val;
             }
         }

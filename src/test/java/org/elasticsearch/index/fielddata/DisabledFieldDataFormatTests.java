bd91ed375ca2041979350112218847ab72fbe4f7
Adrien Grand <jpountz@gmail.com>
2014-08-14 15:26:15 +0200

    [TESTS] Make DisabledFieldDataFormatTests less evil.

--- src/test/java/org/elasticsearch/index/fielddata/DisabledFieldDataFormatTests.java
+++ src/test/java/org/elasticsearch/index/fielddata/DisabledFieldDataFormatTests.java
@@ -23,32 +23,16 @@ import org.elasticsearch.action.search.SearchPhaseExecutionException;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.index.mapper.MapperService.SmartNameFieldMappers;
-import org.elasticsearch.index.service.IndexService;
-import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.Aggregator.SubAggCollectionMode;
-import org.elasticsearch.test.ElasticsearchIntegrationTest;
-import org.elasticsearch.test.ElasticsearchIntegrationTest.ClusterScope;
-
-import java.util.Set;
-import java.util.concurrent.Callable;
+import org.elasticsearch.test.ElasticsearchSingleNodeTest;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.*;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
 
-@ClusterScope(randomDynamicTemplates = false)
-public class DisabledFieldDataFormatTests extends ElasticsearchIntegrationTest {
-
-    @Override
-    protected int numberOfReplicas() {
-        return 0;
-    }
+public class DisabledFieldDataFormatTests extends ElasticsearchSingleNodeTest {
 
     public void test() throws Exception {
-        prepareCreate("test").addMapping("type", "s", "type=string").execute().actionGet();
-        ensureGreen();
+        createIndex("test", ImmutableSettings.EMPTY, "type", "s", "type=string");
         logger.info("indexing data start");
         for (int i = 0; i < 10; ++i) {
             client().prepareIndex("test", "type", Integer.toString(i)).setSource("s", "value" + i).execute().actionGet();
@@ -57,7 +41,7 @@ public class DisabledFieldDataFormatTests extends ElasticsearchIntegrationTest {
 
         final int searchCycles = 1;
 
-        refresh();
+        client().admin().indices().prepareRefresh().execute().actionGet();
 
         // disable field data
         updateFormat("disabled");
@@ -97,7 +81,7 @@ public class DisabledFieldDataFormatTests extends ElasticsearchIntegrationTest {
 
         // but add more docs and the new segment won't be loaded
         client().prepareIndex("test", "type", "-1").setSource("s", "value").execute().actionGet();
-        refresh();
+        client().admin().indices().prepareRefresh().execute().actionGet();
         for (int i = 0; i < searchCycles; i++) {
             try {
                 resp = client().prepareSearch("test").setPreference(Integer.toString(i)).addAggregation(AggregationBuilders.terms("t").field("s")
@@ -124,24 +108,6 @@ public class DisabledFieldDataFormatTests extends ElasticsearchIntegrationTest {
                         .endObject()
                         .endObject()).get());
         logger.info(">> put mapping end {}", format);
-        assertBusy(new Callable<Object>() {
-            @Override
-            public Object call() throws Exception {
-                Set<String> nodes = internalCluster().nodesInclude("test");
-                assertFalse(nodes.isEmpty());
-                for (String node : nodes) {
-                    IndicesService indicesService = internalCluster().getInstance(IndicesService.class, node);
-                    IndexService indexService = indicesService.indexService("test");
-                    assertThat(indexService, notNullValue());
-                    final SmartNameFieldMappers mappers = indexService.mapperService().smartName("s");
-                    assertThat(mappers, notNullValue());
-                    assertTrue(mappers.hasMapper());
-                    final String currentFormat = mappers.mapper().fieldDataType().getFormat(ImmutableSettings.EMPTY);
-                    assertThat(currentFormat, equalTo(format));
-                }
-                return null;
-            }
-        });
     }
 
 }

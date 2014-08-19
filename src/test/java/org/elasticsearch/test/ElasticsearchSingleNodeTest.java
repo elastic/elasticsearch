238efe505b6a511e324073d4ecfd7c6ec675a618
Britta Weber <britta.weber@elasticsearch.com>
2014-08-19 22:48:15 +0200

    bool query: parser should return match_all in case there are no clauses
    This also fixes has_parent filters with a nested empty bool filter
    (see test SimpleChildQuerySearchTests#test6722, the test should actually expect
    either 0 results when searching for has_parent "test" or one result when
    search for has_parent "foo")
    
    closes #7240
    closes #7347

--- src/test/java/org/elasticsearch/test/ElasticsearchSingleNodeTest.java
+++ src/test/java/org/elasticsearch/test/ElasticsearchSingleNodeTest.java
@@ -160,7 +160,7 @@ public abstract class ElasticsearchSingleNodeTest extends ElasticsearchTestCase 
         return createIndex(index, createIndexRequestBuilder);
     }
 
-    private static IndexService createIndex(String index, CreateIndexRequestBuilder createIndexRequestBuilder) {
+    protected static IndexService createIndex(String index, CreateIndexRequestBuilder createIndexRequestBuilder) {
         assertAcked(createIndexRequestBuilder.get());
         // Wait for the index to be allocated so that cluster state updates don't override
         // changes that would have been done locally

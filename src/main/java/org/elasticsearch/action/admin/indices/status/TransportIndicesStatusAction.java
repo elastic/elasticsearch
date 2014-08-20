7c58b3b71072cf98f5f24d9df189b6db11e453e2
javanna <cavannaluca@gmail.com>
2014-08-20 12:11:07 +0200

    Internal: adjusted internal requests visibility from public to package private

--- src/main/java/org/elasticsearch/action/admin/indices/status/TransportIndicesStatusAction.java
+++ src/main/java/org/elasticsearch/action/admin/indices/status/TransportIndicesStatusAction.java
@@ -239,7 +239,7 @@ public class TransportIndicesStatusAction extends TransportBroadcastOperationAct
         return shardStatus;
     }
 
-    public static class IndexShardStatusRequest extends BroadcastShardOperationRequest {
+    static class IndexShardStatusRequest extends BroadcastShardOperationRequest {
 
         boolean recovery;
 

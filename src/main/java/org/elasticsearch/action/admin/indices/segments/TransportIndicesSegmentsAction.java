abdbfe768b52fe46d14864c35008fcf6c8dc5902
javanna <cavannaluca@gmail.com>
2014-08-20 21:12:10 +0200

    Internal: adjusted internal requests visibility from public to package private (redo)
    was just reverted by mistake in the failed attempt of isolating the change and taking it out of #7319

--- src/main/java/org/elasticsearch/action/admin/indices/segments/TransportIndicesSegmentsAction.java
+++ src/main/java/org/elasticsearch/action/admin/indices/segments/TransportIndicesSegmentsAction.java
@@ -134,7 +134,7 @@ public class TransportIndicesSegmentsAction extends TransportBroadcastOperationA
         return new ShardSegments(indexShard.routingEntry(), indexShard.engine().segments());
     }
 
-    public static class IndexShardSegmentRequest extends BroadcastShardOperationRequest {
+    static class IndexShardSegmentRequest extends BroadcastShardOperationRequest {
 
         IndexShardSegmentRequest() {
         }

elastic/elasticsearch

commit 6950c38a0436ec937797f01fba8d7d95e6d6225f

Tests: Improve test coverage.
Close #7428

src/main/java/org/elasticsearch/indices/fielddata/cache/IndicesFieldDataCache.java
@@ -32,7 +32,10 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.index.fielddata.*;
+import org.elasticsearch.index.fielddata.AtomicFieldData;
+import org.elasticsearch.index.fielddata.FieldDataType;
+import org.elasticsearch.index.fielddata.IndexFieldData;
+import org.elasticsearch.index.fielddata.IndexFieldDataCache;
 import org.elasticsearch.index.mapper.FieldMapper;
 import org.elasticsearch.index.service.IndexService;
 import org.elasticsearch.index.shard.ShardId;
@@ -110,10 +113,7 @@ public class IndicesFieldDataCache extends AbstractComponent implements RemovalL
         IndexFieldCache indexCache = key.indexCache;
         long sizeInBytes = key.sizeInBytes;
         final Accountable value = notification.getValue();
-        assert sizeInBytes >= 0 || value != null : "Expected size [" + sizeInBytes + "] to be positive or value [" + value + "] to be non-null";
-        if (sizeInBytes == -1 && value != null) {
-            sizeInBytes = value.ramBytesUsed();
-        }
+        assert value == null || sizeInBytes > 0 && sizeInBytes == value.ramBytesUsed() : "Expected size [" + sizeInBytes + "] to be positive or value [" + value + "] to be non-null";
         for (IndexFieldDataCache.Listener listener : key.listeners) {
             try {
                 listener.onUnload(indexCache.fieldNames, indexCache.fieldDataType, notification.wasEvicted(), sizeInBytes);
@@ -198,7 +198,6 @@ public class IndicesFieldDataCache extends AbstractComponent implements RemovalL
 
         public <FD extends AtomicFieldData, IFD extends IndexFieldData.Global<FD>> IFD load(final IndexReader indexReader, final IFD indexFieldData) throws Exception {
             final Key key = new Key(this, indexReader.getCoreCacheKey());
-
             //noinspection unchecked
             final Accountable accountable = cache.get(key, new Callable<Accountable>() {
                 @Override
@@ -221,6 +220,7 @@ public class IndicesFieldDataCache extends AbstractComponent implements RemovalL
                             logger.error("Failed to call listener on global ordinals loading", e);
                         }
                     }
+                    key.sizeInBytes = ifd.ramBytesUsed();
                     return ifd;
                 }
             });

rest-api-spec/test/cat.indices/10_basic.yml
@@ -1,0 +1,26 @@
+---
+"Test cat indices output":
+
+  - do:
+      cat.indices: {}
+
+  - match:
+      $body: |
+               /^$/
+
+  - do:
+      indices.create:
+        index: index1
+        body:
+          settings:
+            number_of_shards: "1"
+            number_of_replicas: "0"
+  - do:
+       cluster.health:
+         wait_for_status: yellow
+  - do:
+      cat.indices: {}
+
+  - match:
+      $body: |
+               /^(green \s+ index1 \s+ 1 \s+ 0 \s+ 0 \s+ 0 \s+ (\d+|\d+[.]\d+)(kb|b) \s+ (\d+|\d+[.]\d+)(kb|b))$/

src/main/java/org/elasticsearch/common/Booleans.java
@@ -24,6 +24,11 @@ package org.elasticsearch.common;
  */
 public class Booleans {
 
+    /**
+     * Returns <code>true</code> iff the sequence is neither of the following:
+     *   <tt>false</tt>, <tt>0</tt>, <tt>off</tt>, <tt>no</tt>,
+     *   otherwise <code>false</code>
+     */
     public static boolean parseBoolean(char[] text, int offset, int length, boolean defaultValue) {
         if (text == null || length == 0) {
             return defaultValue;
@@ -73,7 +78,17 @@ public class Booleans {
         return false;
     }
 
-
+    public static Boolean parseBoolean(String value, Boolean defaultValue) {
+        if (value == null) { // only for the null case we do that here!
+            return defaultValue;
+        }
+        return parseBoolean(value, false);
+    }
+    /**
+     * Returns <code>true</code> iff the value is neither of the following:
+     *   <tt>false</tt>, <tt>0</tt>, <tt>off</tt>, <tt>no</tt>
+     *   otherwise <code>false</code>
+     */
     public static boolean parseBoolean(String value, boolean defaultValue) {
         if (value == null) {
             return defaultValue;
@@ -81,19 +96,22 @@ public class Booleans {
         return !(value.equals("false") || value.equals("0") || value.equals("off") || value.equals("no"));
     }
 
-    public static Boolean parseBoolean(String value, Boolean defaultValue) {
-        if (value == null) {
-            return defaultValue;
-        }
-        return !(value.equals("false") || value.equals("0") || value.equals("off") || value.equals("no"));
-    }
-
+    /**
+     * Returns <code>true</code> iff the value is either of the following:
+     *   <tt>false</tt>, <tt>0</tt>, <tt>off</tt>, <tt>no</tt>
+     *   otherwise <code>false</code>
+     */
     public static boolean isExplicitFalse(String value) {
-        return (value.equals("false") || value.equals("0") || value.equals("off") || value.equals("no"));
+        return value != null && (value.equals("false") || value.equals("0") || value.equals("off") || value.equals("no"));
     }
 
+    /**
+     * Returns <code>true</code> iff the value is either of the following:
+     *   <tt>true</tt>, <tt>1</tt>, <tt>on</tt>, <tt>yes</tt>
+     *   otherwise <code>false</code>
+     */
     public static boolean isExplicitTrue(String value) {
-        return (value.equals("true") || value.equals("1") || value.equals("on") || value.equals("yes"));
+        return value != null && (value.equals("true") || value.equals("1") || value.equals("on") || value.equals("yes"));
     }
 
 }

src/main/java/org/elasticsearch/common/Preconditions.java
@@ -23,7 +23,6 @@ import org.elasticsearch.ElasticsearchIllegalArgumentException;
 import org.elasticsearch.ElasticsearchIllegalStateException;
 import org.elasticsearch.ElasticsearchNullPointerException;
 
-import java.util.Collection;
 import java.util.NoSuchElementException;
 
 /**
@@ -244,204 +243,6 @@ public final class Preconditions {
                     format(errorMessageTemplate, errorMessageArgs));
         }
         return reference;
-    }
-
-    /**
-     * Ensures that an {@code Iterable} object passed as a parameter to the
-     * calling method is not null and contains no null elements.
-     *
-     * @param iterable the iterable to check the contents of
-     * @return the non-null {@code iterable} reference just validated
-     * @throws org.elasticsearch.ElasticsearchNullPointerException
-     *          if {@code iterable} is null or contains at
-     *          least one null element
-     */
-    public static <T extends Iterable<?>> T checkContentsNotNull(T iterable) {
-        if (containsOrIsNull(iterable)) {
-            throw new ElasticsearchNullPointerException();
-        }
-        return iterable;
-    }
-
-    /**
-     * Ensures that an {@code Iterable} object passed as a parameter to the
-     * calling method is not null and contains no null elements.
-     *
-     * @param iterable     the iterable to check the contents of
-     * @param errorMessage the exception message to use if the check fails; will
-     *                     be converted to a string using {@link String#valueOf(Object)}
-     * @return the non-null {@code iterable} reference just validated
-     * @throws org.elasticsearch.ElasticsearchNullPointerException
-     *          if {@code iterable} is null or contains at
-     *          least one null element
-     */
-    public static <T extends Iterable<?>> T checkContentsNotNull(
-            T iterable, Object errorMessage) {
-        if (containsOrIsNull(iterable)) {
-            throw new ElasticsearchNullPointerException(String.valueOf(errorMessage));
-        }
-        return iterable;
-    }
-
-    /**
-     * Ensures that an {@code Iterable} object passed as a parameter to the
-     * calling method is not null and contains no null elements.
-     *
-     * @param iterable             the iterable to check the contents of
-     * @param errorMessageTemplate a template for the exception message should the
-     *                             check fail. The message is formed by replacing each {@code %s}
-     *                             placeholder in the template with an argument. These are matched by
-     *                             position - the first {@code %s} gets {@code errorMessageArgs[0]}, etc.
-     *                             Unmatched arguments will be appended to the formatted message in square
-     *                             braces. Unmatched placeholders will be left as-is.
-     * @param errorMessageArgs     the arguments to be substituted into the message
-     *                             template. Arguments are converted to strings using
-     *                             {@link String#valueOf(Object)}.
-     * @return the non-null {@code iterable} reference just validated
-     * @throws org.elasticsearch.ElasticsearchNullPointerException
-     *          if {@code iterable} is null or contains at
-     *          least one null element
-     */
-    public static <T extends Iterable<?>> T checkContentsNotNull(T iterable,
-                                                                 String errorMessageTemplate, Object... errorMessageArgs) {
-        if (containsOrIsNull(iterable)) {
-            throw new ElasticsearchNullPointerException(
-                    format(errorMessageTemplate, errorMessageArgs));
-        }
-        return iterable;
-    }
-
-    private static boolean containsOrIsNull(Iterable<?> iterable) {
-        if (iterable == null) {
-            return true;
-        }
-
-        if (iterable instanceof Collection) {
-            Collection<?> collection = (Collection<?>) iterable;
-            try {
-                return collection.contains(null);
-            } catch (ElasticsearchNullPointerException e) {
-                // A NPE implies that the collection doesn't contain null.
-                return false;
-            }
-        } else {
-            for (Object element : iterable) {
-                if (element == null) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-
-    /**
-     * Ensures that {@code index} specifies a valid <i>element</i> in an array,
-     * list or string of size {@code size}. An element index may range from zero,
-     * inclusive, to {@code size}, exclusive.
-     *
-     * @param index a user-supplied index identifying an element of an array, list
-     *              or string
-     * @param size  the size of that array, list or string
-     * @throws IndexOutOfBoundsException if {@code index} is negative or is not
-     *                                   less than {@code size}
-     * @throws org.elasticsearch.ElasticsearchIllegalArgumentException
-     *                                   if {@code size} is negative
-     */
-    public static void checkElementIndex(int index, int size) {
-        checkElementIndex(index, size, "index");
-    }
-
-    /**
-     * Ensures that {@code index} specifies a valid <i>element</i> in an array,
-     * list or string of size {@code size}. An element index may range from zero,
-     * inclusive, to {@code size}, exclusive.
-     *
-     * @param index a user-supplied index identifying an element of an array, list
-     *              or string
-     * @param size  the size of that array, list or string
-     * @param desc  the text to use to describe this index in an error message
-     * @throws IndexOutOfBoundsException if {@code index} is negative or is not
-     *                                   less than {@code size}
-     * @throws org.elasticsearch.ElasticsearchIllegalArgumentException
-     *                                   if {@code size} is negative
-     */
-    public static void checkElementIndex(int index, int size, String desc) {
-        checkArgument(size >= 0, "negative size: %s", size);
-        if (index < 0) {
-            throw new IndexOutOfBoundsException(
-                    format("%s (%s) must not be negative", desc, index));
-        }
-        if (index >= size) {
-            throw new IndexOutOfBoundsException(
-                    format("%s (%s) must be less than size (%s)", desc, index, size));
-        }
-    }
-
-    /**
-     * Ensures that {@code index} specifies a valid <i>position</i> in an array,
-     * list or string of size {@code size}. A position index may range from zero
-     * to {@code size}, inclusive.
-     *
-     * @param index a user-supplied index identifying a position in an array, list
-     *              or string
-     * @param size  the size of that array, list or string
-     * @throws IndexOutOfBoundsException if {@code index} is negative or is
-     *                                   greater than {@code size}
-     * @throws org.elasticsearch.ElasticsearchIllegalArgumentException
-     *                                   if {@code size} is negative
-     */
-    public static void checkPositionIndex(int index, int size) {
-        checkPositionIndex(index, size, "index");
-    }
-
-    /**
-     * Ensures that {@code index} specifies a valid <i>position</i> in an array,
-     * list or string of size {@code size}. A position index may range from zero
-     * to {@code size}, inclusive.
-     *
-     * @param index a user-supplied index identifying a position in an array, list
-     *              or string
-     * @param size  the size of that array, list or string
-     * @param desc  the text to use to describe this index in an error message
-     * @throws IndexOutOfBoundsException if {@code index} is negative or is
-     *                                   greater than {@code size}
-     * @throws org.elasticsearch.ElasticsearchIllegalArgumentException
-     *                                   if {@code size} is negative
-     */
-    public static void checkPositionIndex(int index, int size, String desc) {
-        checkArgument(size >= 0, "negative size: %s", size);
-        if (index < 0) {
-            throw new IndexOutOfBoundsException(format(
-                    "%s (%s) must not be negative", desc, index));
-        }
-        if (index > size) {
-            throw new IndexOutOfBoundsException(format(
-                    "%s (%s) must not be greater than size (%s)", desc, index, size));
-        }
-    }
-
-    /**
-     * Ensures that {@code start} and {@code end} specify a valid <i>positions</i>
-     * in an array, list or string of size {@code size}, and are in order. A
-     * position index may range from zero to {@code size}, inclusive.
-     *
-     * @param start a user-supplied index identifying a starting position in an
-     *              array, list or string
-     * @param end   a user-supplied index identifying a ending position in an array,
-     *              list or string
-     * @param size  the size of that array, list or string
-     * @throws IndexOutOfBoundsException if either index is negative or is
-     *                                   greater than {@code size}, or if {@code end} is less than {@code start}
-     * @throws org.elasticsearch.ElasticsearchIllegalArgumentException
-     *                                   if {@code size} is negative
-     */
-    public static void checkPositionIndexes(int start, int end, int size) {
-        checkPositionIndex(start, size, "start index");
-        checkPositionIndex(end, size, "end index");
-        if (end < start) {
-            throw new IndexOutOfBoundsException(format(
-                    "end index (%s) must not be less than start index (%s)", end, start));
-        }
     }
 
     /**

src/main/java/org/elasticsearch/common/geo/GeoHashUtils.java
@@ -21,7 +21,6 @@ import org.elasticsearch.ElasticsearchIllegalArgumentException;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 
 
 /**
@@ -119,23 +118,6 @@ public class GeoHashUtils {
      */
     public static Collection<? extends CharSequence> neighbors(String geohash) {
         return addNeighbors(geohash, geohash.length(), new ArrayList<CharSequence>(8));
-    }
-
-    /**
-     * Create an {@link Iterable} which allows to iterate over the cells that
-     * contain a given geohash
-     *   
-     * @param geohash Geohash of a cell
-     * 
-     * @return {@link Iterable} of path
-     */
-    public static Iterable<String> path(final String geohash) {
-        return new Iterable<String>() {
-            @Override
-            public Iterator<String> iterator() {
-                return new GeohashPathIterator(geohash);
-            }
-        };
     }
     
     /**
@@ -329,19 +311,6 @@ public class GeoHashUtils {
     public static GeoPoint decode(String geohash, GeoPoint ret) {
         double[] interval = decodeCell(geohash);
         return ret.reset((interval[0] + interval[1]) / 2D, (interval[2] + interval[3]) / 2D);
-    }
-
-    /**
-     * Decodes the given geohash into a geohash cell defined by the points nothWest and southEast
-     *
-     * @param geohash   Geohash to deocde
-     * @param northWest the point north/west of the cell
-     * @param southEast the point south/east of the cell
-     */
-    public static void decodeCell(String geohash, GeoPoint northWest, GeoPoint southEast) {
-        double[] interval = decodeCell(geohash);
-        northWest.reset(interval[1], interval[2]);
-        southEast.reset(interval[0], interval[3]);
     }
 
     private static double[] decodeCell(String geohash) {

src/main/java/org/elasticsearch/common/geo/GeoPoint.java
@@ -78,11 +78,6 @@ public final class GeoPoint {
         return this;
     }
 
-    void latlon(double lat, double lon) {
-        this.lat = lat;
-        this.lon = lon;
-    }
-
     public final double lat() {
         return this.lat;
     }

src/main/java/org/elasticsearch/common/geo/GeoUtils.java
@@ -421,4 +421,7 @@ public class GeoUtils {
             throw new ElasticsearchParseException("geo_point expected");
         }
     }
+
+    private GeoUtils() {
+    }
 }

commit 15a6a4e3559fdb097f20e9b1e043ebe401d41efa

[TEST] fix issue clearing fielddata breaker introduced in 6950c38a0436ec937797f01fba8d7d95e6d6225f

src/main/java/org/elasticsearch/indices/fielddata/cache/IndicesFieldDataCache.java
@@ -113,7 +113,10 @@ public class IndicesFieldDataCache extends AbstractComponent implements RemovalL
         IndexFieldCache indexCache = key.indexCache;
         long sizeInBytes = key.sizeInBytes;
         final Accountable value = notification.getValue();
-        assert value == null || sizeInBytes > 0 && sizeInBytes == value.ramBytesUsed() : "Expected size [" + sizeInBytes + "] to be positive or value [" + value + "] to be non-null";
+        assert sizeInBytes >= 0 || value != null : "Expected size [" + sizeInBytes + "] to be positive or value [" + value + "] to be non-null";
+        if (sizeInBytes == -1 && value != null) {
+            sizeInBytes = value.ramBytesUsed();
+        }
         for (IndexFieldDataCache.Listener listener : key.listeners) {
             try {
                 listener.onUnload(indexCache.fieldNames, indexCache.fieldDataType, notification.wasEvicted(), sizeInBytes);
@@ -220,7 +223,6 @@ public class IndicesFieldDataCache extends AbstractComponent implements RemovalL
                             logger.error("Failed to call listener on global ordinals loading", e);
                         }
                     }
-                    key.sizeInBytes = ifd.ramBytesUsed();
                     return ifd;
                 }
             });
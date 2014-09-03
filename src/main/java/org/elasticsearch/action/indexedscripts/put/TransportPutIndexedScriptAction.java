851cb3ae8aac7041e1a2b3838053d6c52011aa22
javanna <cavannaluca@gmail.com>
2014-09-03 11:55:42 +0200

    Internal: fix members visibility, remove unused constant and needless try catch in indexed scripts transport actions

--- src/main/java/org/elasticsearch/action/indexedscripts/put/TransportPutIndexedScriptAction.java
+++ src/main/java/org/elasticsearch/action/indexedscripts/put/TransportPutIndexedScriptAction.java
@@ -20,9 +20,9 @@
 package org.elasticsearch.action.indexedscripts.put;
 
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.DelegatingActionListener;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.HandledTransportAction;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.inject.Inject;
@@ -36,8 +36,8 @@ import org.elasticsearch.transport.TransportService;
  */
 public class TransportPutIndexedScriptAction extends HandledTransportAction<PutIndexedScriptRequest, PutIndexedScriptResponse> {
 
-    private ScriptService scriptService;
-    private Client client;
+    private final ScriptService scriptService;
+    private final Client client;
 
     @Inject
     public TransportPutIndexedScriptAction(Settings settings, ThreadPool threadPool,
@@ -57,7 +57,7 @@ public class TransportPutIndexedScriptAction extends HandledTransportAction<PutI
     protected void doExecute(final PutIndexedScriptRequest request, final ActionListener<PutIndexedScriptResponse> listener) {
         scriptService.putScriptToIndex(client, request.safeSource(), request.scriptLang(), request.id(), null, request.opType().toString(), request.version(), request.versionType(), new DelegatingActionListener<IndexResponse,PutIndexedScriptResponse>(listener) {
             @Override
-                public PutIndexedScriptResponse getDelegatedFromInstigator(IndexResponse indexResponse){
+            public PutIndexedScriptResponse getDelegatedFromInstigator(IndexResponse indexResponse){
                 return new PutIndexedScriptResponse(indexResponse.getType(),indexResponse.getId(),indexResponse.getVersion(),indexResponse.isCreated());
             }
         });

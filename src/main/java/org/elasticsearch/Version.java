fea7cb45876a73c339d9fac40c5881e95cb88c64
Martijn van Groningen <martijn.v.groningen@gmail.com>
2014-09-08 17:32:03 +0200

    Core: 1.2.5 is unreleased so it should be snapshot.

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -187,7 +187,7 @@ public class Version implements Serializable {
     public static final int V_1_2_4_ID = /*00*/1020499;
     public static final Version V_1_2_4 = new Version(V_1_2_4_ID, false, org.apache.lucene.util.Version.LUCENE_48);
     public static final int V_1_2_5_ID = /*00*/1020599;
-    public static final Version V_1_2_5 = new Version(V_1_2_5_ID, false, org.apache.lucene.util.Version.LUCENE_48);
+    public static final Version V_1_2_5 = new Version(V_1_2_5_ID, true, org.apache.lucene.util.Version.LUCENE_48);
 
     public static final Version CURRENT = V_1_2_5;
 

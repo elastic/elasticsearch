69e8e72ec3e6b5c8a5c77e7236480b8cb52547bc
javanna <cavannaluca@gmail.com>
2014-09-11 15:14:13 +0200

    More Like This API: remove unused search_query_hint parameter
    Closes #7691

--- src/main/java/org/elasticsearch/rest/action/mlt/RestMoreLikeThisAction.java
+++ src/main/java/org/elasticsearch/rest/action/mlt/RestMoreLikeThisAction.java
@@ -70,7 +70,6 @@ public class RestMoreLikeThisAction extends BaseRestHandler {
         mltRequest.searchType(SearchType.fromString(request.param("search_type")));
         mltRequest.searchIndices(request.paramAsStringArray("search_indices", null));
         mltRequest.searchTypes(request.paramAsStringArray("search_types", null));
-        mltRequest.searchQueryHint(request.param("search_query_hint"));
         mltRequest.searchSize(request.paramAsInt("search_size", mltRequest.searchSize()));
         mltRequest.searchFrom(request.paramAsInt("search_from", mltRequest.searchFrom()));
         String searchScroll = request.param("search_scroll");

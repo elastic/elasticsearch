4f791b06db666564d47390d86a7ce4ccbb3f45ca
Brian Murphy <brian.p.murphy@gmail.com>
2014-09-19 10:27:28 +0100

    Revert "Bulk API: Do not fail whole request on closed index"
    This reverts commit 405e5816b827db5823e6c2ed9c1bbe9fa4629355.

--- src/main/java/org/elasticsearch/action/index/IndexRequest.java
+++ src/main/java/org/elasticsearch/action/index/IndexRequest.java
@@ -25,7 +25,6 @@ import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionRequestValidationException;
 import org.elasticsearch.action.RoutingMissingException;
 import org.elasticsearch.action.TimestampParsingException;
-import org.elasticsearch.action.DocumentRequest;
 import org.elasticsearch.action.support.replication.ShardReplicationOperationRequest;
 import org.elasticsearch.client.Requests;
 import org.elasticsearch.cluster.metadata.MappingMetaData;
@@ -65,7 +64,7 @@ import static org.elasticsearch.action.ValidateActions.addValidationError;
  * @see org.elasticsearch.client.Requests#indexRequest(String)
  * @see org.elasticsearch.client.Client#index(IndexRequest)
  */
-public class IndexRequest extends ShardReplicationOperationRequest<IndexRequest> implements DocumentRequest<IndexRequest> {
+public class IndexRequest extends ShardReplicationOperationRequest<IndexRequest> {
 
     /**
      * Operation type controls if the type of the index operation.

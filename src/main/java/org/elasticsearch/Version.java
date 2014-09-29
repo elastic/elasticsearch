ddf796dad5ef2e4910791c294f29aca137993ee1
Michael McCandless <mail@mikemccandless.com>
2014-09-29 13:37:19 +0000

    release [1.3.3]

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -197,7 +197,7 @@ public class Version implements Serializable {
     public static final int V_1_3_2_ID = /*00*/1030299;
     public static final Version V_1_3_2 = new Version(V_1_3_2_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
     public static final int V_1_3_3_ID = /*00*/1030399;
-    public static final Version V_1_3_3 = new Version(V_1_3_3_ID, true, org.apache.lucene.util.Version.LUCENE_4_9);
+    public static final Version V_1_3_3 = new Version(V_1_3_3_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
 
     public static final Version CURRENT = V_1_3_3;
 

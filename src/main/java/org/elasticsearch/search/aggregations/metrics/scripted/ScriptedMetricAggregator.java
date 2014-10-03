a546c80ba4419016d715f756aa32c8518fd81a7d
Colin Goodheart-Smithe <colin.goodheart-smithe@elasticsearch.com>
2014-10-03 11:10:52 +0100

    Aggregations: makes script params consistent with other APIs in scripted_metric
    This change removes the script_type parameter form the Scripted Metric Aggregation and adds support for _file and _id suffixes to the init_script, map_script, combine_script and reduce_script parameters to make defining the source of the script consistent with the other APIs which use the ScriptService

--- src/main/java/org/elasticsearch/search/aggregations/metrics/scripted/ScriptedMetricAggregator.java
+++ src/main/java/org/elasticsearch/search/aggregations/metrics/scripted/ScriptedMetricAggregator.java
@@ -46,15 +46,15 @@ public class ScriptedMetricAggregator extends MetricsAggregator {
     // initial parameters for {reduce}
     private final Map<String, Object> reduceParams;
     private ScriptService scriptService;
-    private ScriptType scriptType;
+    private ScriptType reduceScriptType;
 
-    protected ScriptedMetricAggregator(String name, String scriptLang, ScriptType scriptType, String initScript, String mapScript,
-            String combineScript, String reduceScript, Map<String, Object> params, Map<String, Object> reduceParams,
-            AggregationContext context, Aggregator parent) {
+    protected ScriptedMetricAggregator(String name, String scriptLang, ScriptType initScriptType, String initScript,
+            ScriptType mapScriptType, String mapScript, ScriptType combineScriptType, String combineScript, ScriptType reduceScriptType,
+            String reduceScript, Map<String, Object> params, Map<String, Object> reduceParams, AggregationContext context, Aggregator parent) {
         super(name, 1, context, parent);
         this.scriptService = context.searchContext().scriptService();
         this.scriptLang = scriptLang;
-        this.scriptType = scriptType;
+        this.reduceScriptType = reduceScriptType;
         if (params == null) {
             this.params = new HashMap<>();
             this.params.put("_agg", new HashMap<>());
@@ -67,11 +67,11 @@ public class ScriptedMetricAggregator extends MetricsAggregator {
             this.reduceParams = reduceParams;
         }
         if (initScript != null) {
-            scriptService.executable(scriptLang, initScript, scriptType, this.params).run();
+            scriptService.executable(scriptLang, initScript, initScriptType, this.params).run();
         }
-        this.mapScript = scriptService.search(context.searchContext().lookup(), scriptLang, mapScript, scriptType, this.params);
+        this.mapScript = scriptService.search(context.searchContext().lookup(), scriptLang, mapScript, mapScriptType, this.params);
         if (combineScript != null) {
-            this.combineScript = scriptService.executable(scriptLang, combineScript, scriptType, this.params);
+            this.combineScript = scriptService.executable(scriptLang, combineScript, combineScriptType, this.params);
         } else {
             this.combineScript = null;
         }
@@ -102,18 +102,21 @@ public class ScriptedMetricAggregator extends MetricsAggregator {
         } else {
             aggregation = params.get("_agg");
         }
-        return new InternalScriptedMetric(name, aggregation, scriptLang, scriptType, reduceScript, reduceParams);
+        return new InternalScriptedMetric(name, aggregation, scriptLang, reduceScriptType, reduceScript, reduceParams);
     }
 
     @Override
     public InternalAggregation buildEmptyAggregation() {
-        return new InternalScriptedMetric(name, null, scriptLang, scriptType, reduceScript, reduceParams);
+        return new InternalScriptedMetric(name, null, scriptLang, reduceScriptType, reduceScript, reduceParams);
     }
 
     public static class Factory extends AggregatorFactory {
 
         private String scriptLang;
-        private ScriptType scriptType;
+        private ScriptType initScriptType;
+        private ScriptType mapScriptType;
+        private ScriptType combineScriptType;
+        private ScriptType reduceScriptType;
         private String initScript;
         private String mapScript;
         private String combineScript;
@@ -121,11 +124,14 @@ public class ScriptedMetricAggregator extends MetricsAggregator {
         private Map<String, Object> params;
         private Map<String, Object> reduceParams;
 
-        public Factory(String name, String scriptLang, ScriptType scriptType, String initScript, String mapScript, String combineScript, String reduceScript,
+        public Factory(String name, String scriptLang, ScriptType initScriptType, String initScript, ScriptType mapScriptType, String mapScript, ScriptType combineScriptType, String combineScript, ScriptType reduceScriptType, String reduceScript,
                 Map<String, Object> params, Map<String, Object> reduceParams) {
             super(name, InternalScriptedMetric.TYPE.name());
             this.scriptLang = scriptLang;
-            this.scriptType = scriptType;
+            this.initScriptType = initScriptType;
+            this.mapScriptType = mapScriptType;
+            this.combineScriptType = combineScriptType;
+            this.reduceScriptType = reduceScriptType;
             this.initScript = initScript;
             this.mapScript = mapScript;
             this.combineScript = combineScript;
@@ -136,8 +142,8 @@ public class ScriptedMetricAggregator extends MetricsAggregator {
 
         @Override
         public Aggregator create(AggregationContext context, Aggregator parent, long expectedBucketsCount) {
-            return new ScriptedMetricAggregator(name, scriptLang, scriptType, initScript, mapScript, combineScript, reduceScript, params,
-                    reduceParams, context, parent);
+            return new ScriptedMetricAggregator(name, scriptLang, initScriptType, initScript, mapScriptType, mapScript, combineScriptType,
+                    combineScript, reduceScriptType, reduceScript, params, reduceParams, context, parent);
         }
 
     }

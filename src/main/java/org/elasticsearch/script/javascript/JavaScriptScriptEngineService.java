37bac2d4b2ca1eddc98f8f20dd0ccc97b54deab3
Britta Weber <britta.weber@elasticsearch.com>
2014-10-07 12:29:13 +0200

    Remove setNextScore in SearchScript
    Due to a change in elasticsearch 1.4.0, we need to apply a similar patch here.
    
    See https://github.com/elasticsearch/elasticsearch/pull/6864
    See https://github.com/elasticsearch/elasticsearch/pull/7819
    
    Closes #23.
    (cherry picked from commit 5e5c373)

--- src/main/java/org/elasticsearch/script/javascript/JavaScriptScriptEngineService.java
+++ src/main/java/org/elasticsearch/script/javascript/JavaScriptScriptEngineService.java
@@ -26,6 +26,7 @@ import org.elasticsearch.common.component.AbstractComponent;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.script.ExecutableScript;
+import org.elasticsearch.script.ScoreAccessor;
 import org.elasticsearch.script.ScriptEngineService;
 import org.elasticsearch.script.SearchScript;
 import org.elasticsearch.script.javascript.support.NativeList;
@@ -221,7 +222,9 @@ public class JavaScriptScriptEngineService extends AbstractComponent implements 
 
         @Override
         public void setScorer(Scorer scorer) {
-            lookup.setScorer(scorer);
+            Context ctx = Context.enter();
+            ScriptableObject.putProperty(scope, "_score", wrapFactory.wrapAsJavaObject(ctx, scope, new ScoreAccessor(scorer), ScoreAccessor.class));
+            Context.exit();
         }
 
         @Override

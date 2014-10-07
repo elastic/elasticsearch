09ff3724ee5ced68053275a06d7a3fa52ff87372
David Pilato <david@pilato.fr>
2014-10-07 16:15:40 +0200

    plugins: disable support for config dir
    When removing and installing again the plugin all configuration files will be removed in `config/pluginname` dir.
    This is bad as users may have set and added specific configuration files.
    
    During an install, if we detect already existing files in `config/pluginname` directory, we simply copy the new file to the same dir but we append a `.new` at the end.
    
    Related to  #5064.
    
    (cherry picked from commit 5da028f)
    (cherry picked from commit 4cb1f95)

--- src/main/java/org/elasticsearch/common/io/FileSystemUtils.java
+++ src/main/java/org/elasticsearch/common/io/FileSystemUtils.java
@@ -25,6 +25,11 @@ import org.elasticsearch.common.logging.ESLogger;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+import static java.nio.file.FileVisitResult.SKIP_SUBTREE;
 
 /**
  *
@@ -183,4 +188,121 @@ public class FileSystemUtils {
             // ignore
         }
     }
+
+    /**
+     * This utility copy a full directory content (excluded) under
+     * a new directory but without overwriting existing files.
+     *
+     * When a file already exists in destination dir, the source file is copied under
+     * destination directory but with a suffix appended if set or source file is ignored
+     * if suffix is not set (null).
+     * @param source Source directory (for example /tmp/es/src)
+     * @param destination Destination directory (destination directory /tmp/es/dst)
+     * @param suffix When not null, files are copied with a suffix appended to the original name (eg: ".new")
+     *               When null, files are ignored
+     */
+    public static void moveFilesWithoutOverwriting(File source, final File destination, final String suffix) throws IOException {
+
+        // Create destination dir
+        FileSystemUtils.mkdirs(destination);
+
+        final int configPathRootLevel = source.toPath().getNameCount();
+
+        // We walk through the file tree from
+        Files.walkFileTree(source.toPath(), new SimpleFileVisitor<Path>() {
+            private Path buildPath(Path path) {
+                return destination.toPath().resolve(path);
+            }
+
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                // We are now in dir. We need to remove root of config files to have a relative path
+
+                // If we are not walking in root dir, we might be able to copy its content
+                // if it does not already exist
+                if (configPathRootLevel != dir.getNameCount()) {
+                    Path subpath = dir.subpath(configPathRootLevel, dir.getNameCount());
+                    Path path = buildPath(subpath);
+                    if (!Files.exists(path)) {
+                        // We just move the structure to new dir
+                        if (!dir.toFile().renameTo(path.toFile())) {
+                            throw new IOException("Could not move [" + dir + "] to [" + path + "]");
+                        }
+
+                        // We just ignore sub files from here
+                        return FileVisitResult.SKIP_SUBTREE;
+                    }
+                }
+
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Path subpath = null;
+
+                if (configPathRootLevel != file.getNameCount()) {
+                    subpath = file.subpath(configPathRootLevel, file.getNameCount());
+                }
+                Path path = buildPath(subpath);
+
+                if (!Files.exists(path)) {
+                    // We just move the new file to new dir
+                    Files.move(file, path);
+                } else if (suffix != null) {
+                    // If it already exists we try to copy this new version appending suffix to its name
+                    path = Paths.get(path.toString().concat(suffix));
+                    // We just move the file to new dir but with a new name (appended with suffix)
+                    Files.move(file, path, StandardCopyOption.REPLACE_EXISTING);
+                }
+
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    /**
+     * Copy recursively a dir to a new location
+     * @param source source dir
+     * @param destination destination dir
+     */
+    public static void copyDirectoryRecursively(File source, File destination) throws IOException {
+        Files.walkFileTree(source.toPath(),
+                new TreeCopier(source.toPath(), destination.toPath()));
+    }
+
+    static class TreeCopier extends SimpleFileVisitor<Path> {
+        private final Path source;
+        private final Path target;
+
+        TreeCopier(Path source, Path target) {
+            this.source = source;
+            this.target = target;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+            Path newDir = target.resolve(source.relativize(dir));
+            try {
+                Files.copy(dir, newDir);
+            } catch (FileAlreadyExistsException x) {
+                // We ignore this
+            } catch (IOException x) {
+                return SKIP_SUBTREE;
+            }
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Path newFile = target.resolve(source.relativize(file));
+            try {
+                Files.copy(file, newFile);
+            } catch (IOException x) {
+                // We ignore this
+            }
+            return CONTINUE;
+        }
+    }
+
 }

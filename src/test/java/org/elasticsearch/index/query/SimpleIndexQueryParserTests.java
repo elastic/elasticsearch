617815d4590cfc7a9c6dc8570b97ca034b65f9c7
Britta Weber <britta.weber@elasticsearch.com>
2014-10-08 17:45:16 +0200

    [TEST] fix test after pr #8020

--- src/test/java/org/elasticsearch/index/query/SimpleIndexQueryParserTests.java
+++ src/test/java/org/elasticsearch/index/query/SimpleIndexQueryParserTests.java
@@ -2357,7 +2357,7 @@ public class SimpleIndexQueryParserTests extends ElasticsearchSingleNodeTest {
         assertThat(parsedQuery, instanceOf(XConstantScoreQuery.class));
         assertThat(((XConstantScoreQuery) parsedQuery).getFilter(), instanceOf(CustomQueryWrappingFilter.class));
         assertThat(((CustomQueryWrappingFilter) ((XConstantScoreQuery) parsedQuery).getFilter()).getQuery(), instanceOf(ParentConstantScoreQuery.class));
-        assertThat(((CustomQueryWrappingFilter) ((XConstantScoreQuery) parsedQuery).getFilter()).getQuery().toString(), equalTo("parent_filter[foo](*:*)"));
+        assertThat(((CustomQueryWrappingFilter) ((XConstantScoreQuery) parsedQuery).getFilter()).getQuery().toString(), equalTo("parent_filter[foo](filtered(*:*)->cache(_type:foo))"));
         SearchContext.removeCurrent();
     }
 }

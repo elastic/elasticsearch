f2ebb9fc866b72cb01d2a907a4a45408a0e263ba
Simon Willnauer <simonw@apache.org>
2014-10-10 22:45:51 +0200

    use integration test for backwards testing

--- src/test/java/org/elasticsearch/rest/action/admin/indices/upgrade/UpgradeReallyOldIndexTest.java
+++ src/test/java/org/elasticsearch/rest/action/admin/indices/upgrade/UpgradeReallyOldIndexTest.java
@@ -23,7 +23,10 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.lucene.util.TestUtil;
 import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
+import org.elasticsearch.action.admin.indices.get.GetIndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.common.io.FileSystemUtils;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
@@ -31,56 +34,75 @@ import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
 import org.elasticsearch.node.internal.InternalNode;
+import org.elasticsearch.test.ElasticsearchIntegrationTest;
 import org.elasticsearch.test.ElasticsearchTestCase;
 import org.elasticsearch.test.rest.client.http.HttpRequestBuilder;
 import org.elasticsearch.test.rest.client.http.HttpResponse;
 import org.junit.Before;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-public class UpgradeReallyOldIndexTest extends ElasticsearchTestCase {
-    Node node;
-    
-    @Before
-    public void setup() {
-        
-    }
-    
-    public void test() throws Exception {
-        File dataDir = newTempDir();
-        File oldIndex = new File(getClass().getResource("index-0.20.zip").toURI());
-        TestUtil.unzip(oldIndex, dataDir);
-        
-        
-        
-        NodeBuilder nodeBuilder = NodeBuilder.nodeBuilder();
-        nodeBuilder.clusterName("oldindexcluster");
-        Settings settings = ImmutableSettings.builder()
-            .put("path.data", dataDir.getPath())
-            .put("node.mode", "network")
-            .put(InternalNode.HTTP_ENABLED, true)
-            .build();
-        Node node = nodeBuilder.settings(settings).node();
-        
-        try {
-            node.start();
-            NodeInfo info = nodeInfo(node.client());
-            info.getHttp().address().boundAddress();
-            TransportAddress publishAddress = info.getTransport().address().publishAddress();
-            assertEquals(1, publishAddress.uniqueAddressTypeId());
-            InetSocketAddress address = ((InetSocketTransportAddress) publishAddress).address();
-            HttpRequestBuilder httpClient = new HttpRequestBuilder(HttpClients.createDefault()).host(address.getHostName()).port(address.getPort());
-            HttpResponse rsp = httpClient.method("GET").path("/_upgrade").addParam("pretty", "true").execute();
-            System.out.println("RESPONSE: \n" + rsp.getBody());
-        } finally {
-            node.stop();
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
+
+@ElasticsearchIntegrationTest.ClusterScope(scope = ElasticsearchIntegrationTest.Scope.TEST, numDataNodes = 0, minNumDataNodes = 0, maxNumDataNodes = 0)
+public class UpgradeReallyOldIndexTest extends ElasticsearchIntegrationTest {
+
+    // this can maybe go into  ElasticsearchIntegrationTest
+    public File prepareBackwardsDataDir(File backwardsIndex) throws IOException {
+        File dataDir = new File(newTempDir(), "data");
+        TestUtil.unzip(backwardsIndex, dataDir.getParentFile());
+        assertThat(dataDir.exists(), is(true));
+        String[] list = dataDir.list();
+        if (list == null || list.length > 1) {
+            throw new IllegalStateException("Backwards index must contain exactly one cluster");
         }
+        File src = new File(dataDir, list[0]);
+        File dest = new File(dataDir, internalCluster().getClusterName());
+        assertThat(src.exists(), is(true));
+        src.renameTo(dest);
+        assertThat(src.exists(), is(false));
+        assertThat(dest.exists(), is(true));
+        return dataDir;
     }
+
+
+    public void test() throws Exception {
+        File dataDir = prepareBackwardsDataDir(new File(getClass().getResource("index-0.20.zip").toURI()));
+        internalCluster().startNode(ImmutableSettings.builder()
+                .put("path.data", dataDir.getPath())
+                .put("node.mode", "network")
+                .put("gateway.type", "local") // this is important we need to recover from gateway
+                .put(InternalNode.HTTP_ENABLED, true)
+                .build());
+        NodeInfo info = nodeInfo(client());
+        info.getHttp().address().boundAddress();
+        TransportAddress publishAddress = info.getHttp().address().publishAddress();
+        assertEquals(1, publishAddress.uniqueAddressTypeId());
+        GetIndexResponse getIndexResponse = client().admin().indices().prepareGetIndex().get();
+        logger.info("Found indices: {}", Arrays.toString(getIndexResponse.indices()));
+        assertThat(getIndexResponse.indices().length, equalTo(1));
+        assertThat(getIndexResponse.indices()[0], equalTo("test"));
+        ensureYellow("test");
+        SearchResponse test = client().prepareSearch("test").get();
+        assertThat(test.getHits().getTotalHits(), greaterThanOrEqualTo(1l));
+        InetSocketAddress address = ((InetSocketTransportAddress) publishAddress).address();
+        HttpRequestBuilder httpClient = new HttpRequestBuilder(HttpClients.createDefault()).host(address.getHostName()).port(address.getPort());
+        HttpResponse rsp = httpClient.method("GET").path("/_upgrade").addParam("pretty", "true").execute();
+        logger.info("RESPONSE: \n" + rsp.getBody());
+    }
+
+
 
     static NodeInfo nodeInfo(final Client client) {
         final NodesInfoResponse nodeInfos = client.admin().cluster().prepareNodesInfo().get();

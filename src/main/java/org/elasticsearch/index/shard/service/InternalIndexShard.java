46d5ef20e54e609052dbea0df6eb0949880bc614
Michael McCandless <mail@mikemccandless.com>
2014-10-15 04:32:10 -0400

    Core: only schedule another refresh if refresh_interval is positive
    If a refresh was already running when the refresh_interval is
    dynamically updated down to a non-positive value (0, -1, etc.), then
    it's was possible for the refresh thread to go into while (true)
    refresh() loop.
    
    Closes #8085

--- src/main/java/org/elasticsearch/index/shard/service/InternalIndexShard.java
+++ src/main/java/org/elasticsearch/index/shard/service/InternalIndexShard.java
@@ -946,11 +946,7 @@ public class InternalIndexShard extends AbstractIndexShardComponent implements I
         public void run() {
             // we check before if a refresh is needed, if not, we reschedule, otherwise, we fork, refresh, and then reschedule
             if (!engine().refreshNeeded()) {
-                synchronized (mutex) {
-                    if (state != IndexShardState.CLOSED) {
-                        refreshScheduledFuture = threadPool.schedule(refreshInterval, ThreadPool.Names.SAME, this);
-                    }
-                }
+                reschedule();
                 return;
             }
             threadPool.executor(ThreadPool.Names.REFRESH).execute(new Runnable() {
@@ -979,13 +975,19 @@ public class InternalIndexShard extends AbstractIndexShardComponent implements I
                             logger.warn("Failed to perform scheduled engine refresh", e);
                         }
                     }
-                    synchronized (mutex) {
-                        if (state != IndexShardState.CLOSED) {
-                            refreshScheduledFuture = threadPool.schedule(refreshInterval, ThreadPool.Names.SAME, EngineRefresher.this);
-                        }
-                    }
+
+                    reschedule();
                 }
             });
+        }
+
+        /** Schedules another (future) refresh, if refresh_interval is still enabled. */
+        private void reschedule() {
+            synchronized (mutex) {
+                if (state != IndexShardState.CLOSED && refreshInterval.millis() > 0) {
+                    refreshScheduledFuture = threadPool.schedule(refreshInterval, ThreadPool.Names.SAME, this);
+                }
+            }
         }
     }
 

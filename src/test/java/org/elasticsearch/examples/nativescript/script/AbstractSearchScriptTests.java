c5ed8ee067aac77ddc9f01785d9f6f9ba6b5de38
Igor Motov <igor@motovs.org>
2014-10-16 16:57:59 -0400

    Upgrade to elasticsearch 1.3.4

--- src/test/java/org/elasticsearch/examples/nativescript/script/AbstractSearchScriptTests.java
+++ src/test/java/org/elasticsearch/examples/nativescript/script/AbstractSearchScriptTests.java
@@ -2,14 +2,13 @@ package org.elasticsearch.examples.nativescript.script;
 
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.plugins.PluginsService;
 import org.elasticsearch.test.ElasticsearchIntegrationTest;
 import org.elasticsearch.test.ElasticsearchIntegrationTest.ClusterScope;
 import org.elasticsearch.test.ElasticsearchIntegrationTest.Scope;
-import org.junit.Test;
 
 import static org.elasticsearch.cluster.metadata.IndexMetaData.SETTING_NUMBER_OF_REPLICAS;
 import static org.elasticsearch.cluster.metadata.IndexMetaData.SETTING_NUMBER_OF_SHARDS;
-import static org.hamcrest.Matchers.equalTo;
 
 /**
  */
@@ -28,6 +27,7 @@ public class AbstractSearchScriptTests extends ElasticsearchIntegrationTest {
     protected Settings nodeSettings(int nodeOrdinal) {
         return ImmutableSettings.settingsBuilder()
                 .put("gateway.type", "none")
+                .put("plugins." + PluginsService.LOAD_PLUGIN_FROM_CLASSPATH, true)
                 .put(super.nodeSettings(nodeOrdinal))
                 .build();
     }

03acfd35220f55995da9b0415df4ddf50863b027
Alex Ksikes <alex.ksikes@gmail.com>
2014-10-27 15:15:04 +0100

    MLT Field Query: remove it from master
    The MLT field query is simply replaced by a MLT query set to specififc field.
    To simplify code maintenance we should deprecate it in 1.4 and remove it in
    2.0.
    
    Closes #8238

--- src/main/java/org/elasticsearch/indices/query/IndicesQueriesModule.java
+++ src/main/java/org/elasticsearch/indices/query/IndicesQueriesModule.java
@@ -94,7 +94,6 @@ public class IndicesQueriesModule extends AbstractModule {
         qpBinders.addBinding().to(SpanNearQueryParser.class).asEagerSingleton();
         qpBinders.addBinding().to(SpanOrQueryParser.class).asEagerSingleton();
         qpBinders.addBinding().to(MoreLikeThisQueryParser.class).asEagerSingleton();
-        qpBinders.addBinding().to(MoreLikeThisFieldQueryParser.class).asEagerSingleton();
         qpBinders.addBinding().to(FuzzyLikeThisQueryParser.class).asEagerSingleton();
         qpBinders.addBinding().to(FuzzyLikeThisFieldQueryParser.class).asEagerSingleton();
         qpBinders.addBinding().to(WrapperQueryParser.class).asEagerSingleton();

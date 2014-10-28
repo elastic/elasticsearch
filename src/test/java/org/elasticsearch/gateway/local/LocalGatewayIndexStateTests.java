a34b3a890f10ce161e381c79d6d3ce5f1132cbe7
Philipp Bogensberger <philipp@crate.io>
2014-10-28 15:36:53 +0100

    Fix: If dangling_timeout was set to 0 and auto_import_dangled was set to yes, dangling indices were deleted by mistake, because a RemoveDanglingIndices runnable was added to every dangling indices, without considering the auto_import_dangled setting.

--- src/test/java/org/elasticsearch/gateway/local/LocalGatewayIndexStateTests.java
+++ src/test/java/org/elasticsearch/gateway/local/LocalGatewayIndexStateTests.java
@@ -277,6 +277,7 @@ public class LocalGatewayIndexStateTests extends ElasticsearchIntegrationTest {
     public void testDanglingIndicesAutoImportYes() throws Exception {
         Settings settings = settingsBuilder()
                 .put("gateway.type", "local").put("gateway.local.auto_import_dangled", "yes")
+                .put("gateway.local.dangling_timeout", randomIntBetween(0, 120))
                 .build();
         logger.info("--> starting two nodes");
         final String node_1 = internalCluster().startNode(settings);

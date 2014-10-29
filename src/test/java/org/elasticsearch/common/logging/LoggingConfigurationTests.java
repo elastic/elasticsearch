f8ea7c15d7b018334b7f0b3a92ba60e7c073c168
Colin Goodheart-Smithe <colin.goodheart-smithe@elasticsearch.com>
2014-10-29 13:03:13 +0000

    [TEST] Added awaits fix to failing LoggingConfigurationTests

--- src/test/java/org/elasticsearch/common/logging/LoggingConfigurationTests.java
+++ src/test/java/org/elasticsearch/common/logging/LoggingConfigurationTests.java
@@ -21,6 +21,7 @@ package org.elasticsearch.common.logging;
 
 import org.apache.log4j.Appender;
 import org.apache.log4j.Logger;
+import org.apache.lucene.util.LuceneTestCase.AwaitsFix;
 import org.elasticsearch.common.logging.log4j.Log4jESLogger;
 import org.elasticsearch.common.logging.log4j.Log4jESLoggerFactory;
 import org.elasticsearch.common.logging.log4j.LogConfigurator;
@@ -39,6 +40,7 @@ import static org.hamcrest.Matchers.notNullValue;
  */
 public class LoggingConfigurationTests extends ElasticsearchTestCase {
 
+    @AwaitsFix(bugUrl = "relates to commit 4ebbb657")
     @Test
     public void testMultipleConfigs() throws Exception {
         File configDir = resolveConfigDir();

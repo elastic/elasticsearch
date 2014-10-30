89009d2a4df82674e2456f1b7f220ffd04b170a6
Zachary Tong <zacharyjtong@gmail.com>
2014-10-30 16:22:03 -0400

    Re-use existing Avg classes

--- src/main/java/org/elasticsearch/search/reducers/metric/avg/InternalAvg.java
+++ src/main/java/org/elasticsearch/search/reducers/metric/avg/InternalAvg.java
@@ -19,79 +19,18 @@
 
 package org.elasticsearch.search.reducers.metric.avg;
 
-import org.elasticsearch.common.io.stream.StreamInput;
-import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.search.aggregations.AggregationStreams;
 import org.elasticsearch.search.aggregations.InternalAggregation;
-import org.elasticsearch.search.aggregations.metrics.InternalNumericMetricsAggregation;
+import org.elasticsearch.search.aggregations.metrics.avg.Avg;
 
-import java.io.IOException;
 
-public class InternalAvg extends InternalNumericMetricsAggregation.SingleValue implements Avg {
-
-    public static final Type TYPE = new Type("avg");
-
-    public static final AggregationStreams.Stream STREAM = new AggregationStreams.Stream() {
-        @Override
-        public InternalAvg readResult(StreamInput in) throws IOException {
-            InternalAvg avg = new InternalAvg();
-            avg.readFrom(in);
-            return avg;
-        }
-    };
-
-    public static void registerStreams() {
-        AggregationStreams.registerStream(STREAM, TYPE.stream());
-    }
-
-    private double value;
-
-    public InternalAvg() {
-        super();
-    }
+public class InternalAvg extends org.elasticsearch.search.aggregations.metrics.max.InternalMax implements Avg {
 
     public InternalAvg(String name, double avg) {
-        super(name);
-        this.value = avg;
+        super(name, avg);
     }
 
     @Override
-    public Type type() {
-        return TYPE;
-    }
-
-    @Override
-    public double getValue() {
-        return value();
-    }
-
-    @Override
-    public double value() {
-        return value;
-    }
-
-    
-    @Override
-    public void readFrom(StreamInput in) throws IOException {
-        name = in.readString();
-        value = in.readDouble();
-    }
-
-    @Override
-    public void writeTo(StreamOutput out) throws IOException {
-        out.writeString(name);
-        out.writeDouble(value);
-    }
-
-    @Override
-    public XContentBuilder doXContentBody(XContentBuilder builder, Params params) throws IOException {
-        builder.field("value", value);
-        return builder;
-    }
-
-    @Override
-    public InternalAggregation reduce(ReduceContext reduceContext) {
+    public org.elasticsearch.search.aggregations.metrics.max.InternalMax reduce(InternalAggregation.ReduceContext reduceContext) {
         throw new UnsupportedOperationException("Not supported");
     }
 

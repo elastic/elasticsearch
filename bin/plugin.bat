b48c0c3b92436721838574c09e87fc1caba0b71a
Alexander Reelsen <alexander@reelsen.net>
2014-10-30 12:32:53 +0100

    Plugins: Support usage of ES_JAVA_OPTS in plugin commands
    As that environment variable can contain elasticsearch specific configuration
    like the path to a configuration, it makes sense to support in both
    elasticsearch commands.
    
    Closes #8288

--- bin/plugin.bat
+++ bin/plugin.bat
@@ -9,7 +9,7 @@ for %%I in ("%SCRIPT_DIR%..") do set ES_HOME=%%~dpfI
 
 TITLE Elasticsearch Plugin Manager ${project.version}
 
-"%JAVA_HOME%\bin\java" %JAVA_OPTS% -Xmx64m -Xms16m -Des.path.home="%ES_HOME%" -cp "%ES_HOME%/lib/*;" "org.elasticsearch.plugins.PluginManager" %*
+"%JAVA_HOME%\bin\java" %JAVA_OPTS% %ES_JAVA_OPTS% -Xmx64m -Xms16m -Des.path.home="%ES_HOME%" -cp "%ES_HOME%/lib/*;" "org.elasticsearch.plugins.PluginManager" %*
 goto finally
 
 

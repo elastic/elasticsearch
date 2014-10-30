28ab948e99e3ea4497c9b1e468384806ba7e1790
Simon Willnauer <simonw@apache.org>
2014-10-30 17:34:58 +0100

    don't advance iterator in a match(doc) method

--- src/main/java/org/elasticsearch/index/search/child/ChildrenConstantScoreQuery.java
+++ src/main/java/org/elasticsearch/index/search/child/ChildrenConstantScoreQuery.java
@@ -302,11 +302,15 @@ public class ChildrenConstantScoreQuery extends Query {
         @Override
         protected boolean match(int doc) {
             if (parentWeight.remaining == 0) {
-                try {
-                    advance(DocIdSetIterator.NO_MORE_DOCS);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                // nocommit - this tires to exhaust the iterator but this causes an assertion error
+                // we should never modify the iterator in this method -- we should put an assertion in lucene to make sure the iterator is not advacned in
+                // this method
+                // another way to do this would be to extend lucene to return an enum here just like FilterTermsEnum#SeekStatus works like found|not_found|end
+//                try {
+//                    advance(DocIdSetIterator.NO_MORE_DOCS);
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
                 return false;
             }
 

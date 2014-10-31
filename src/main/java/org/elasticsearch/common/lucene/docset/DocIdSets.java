46a5cc5732dea096c0c80ae5ce42911c9c51e44e
Simon Willnauer <simonw@apache.org>
2014-10-31 13:00:16 +0100

    I hate it but P/C now passes

--- src/main/java/org/elasticsearch/common/lucene/docset/DocIdSets.java
+++ src/main/java/org/elasticsearch/common/lucene/docset/DocIdSets.java
@@ -81,13 +81,13 @@ public class DocIdSets {
             return set;
         }
         // TODO: should we use RoaringDocIdSet like Lucene?
-        it = set.iterator();
-        if (it == null) {
-            return DocIdSet.EMPTY;
-        }
-        FixedBitSet fixedBitSet = new FixedBitSet(reader.maxDoc());
+        final FixedBitSet fixedBitSet = new FixedBitSet(reader.maxDoc());
+        // nocommit - this is fucked up due to ParentChild - we can't to a simple or here since we can't pull the set.iterator() twice
         long cost = it.cost();
-        fixedBitSet.or(it);
+        do {
+            fixedBitSet.set(doc);
+            doc = it.nextDoc();
+        } while (doc != DocIdSetIterator.NO_MORE_DOCS);
         return new BitDocIdSet(fixedBitSet, cost);
     }
 

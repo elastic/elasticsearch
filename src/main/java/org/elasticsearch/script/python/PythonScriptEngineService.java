8cbdf5302f7caebe0d168fd9e5c4212d8d21bb89
Robert Muir <rmuir@apache.org>
2014-11-05 17:18:37 -0500

    upgrade to lucene 5 snapshot

--- src/main/java/org/elasticsearch/script/python/PythonScriptEngineService.java
+++ src/main/java/org/elasticsearch/script/python/PythonScriptEngineService.java
@@ -19,7 +19,7 @@
 
 package org.elasticsearch.script.python;
 
-import org.apache.lucene.index.AtomicReaderContext;
+import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.search.Scorer;
 import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.component.AbstractComponent;
@@ -171,7 +171,7 @@ public class PythonScriptEngineService extends AbstractComponent implements Scri
         }
 
         @Override
-        public void setNextReader(AtomicReaderContext context) {
+        public void setNextReader(LeafReaderContext context) {
             lookup.setNextReader(context);
         }
 

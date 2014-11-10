d973a446334b153fb9e92206d1a4e361d7d37203
Vidar Kongsli <vidarkongsli@yahoo.com>
2014-11-10 23:10:24 +0100

    Added quotes to allow spaces in installation path

--- bin/elasticsearch.bat
+++ bin/elasticsearch.bat
@@ -3,7 +3,7 @@
 SETLOCAL
 TITLE Elasticsearch ${project.version}
 
-CALL %~dp0elasticsearch.in.bat
+CALL "%~dp0elasticsearch.in.bat"
 
 "%JAVA_HOME%\bin\java" %JAVA_OPTS% %ES_JAVA_OPTS% %ES_PARAMS% %* -cp "%ES_CLASSPATH%" "org.elasticsearch.bootstrap.Elasticsearch"
 

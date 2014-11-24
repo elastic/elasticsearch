ecbb1b5fda820214076839c2b523ee641423eb28
Chris Earle <chris.earle@elasticsearch.com>
2014-11-24 12:19:20 -0500

    Update to Jackson 2.4.3
    - Update pom to 2.4.3 from 2.4.2
    - Enable the CBOR data header (aka tag) from the CBOR Generator to provide binary identification like the Smile format
    - Check for the CBOR header and ensure that the data sent in represents a "major type" that is an object
    - Cleans up `JsonVsCborTests` unused imports
    
    Closes #7640
    
    Conflicts:
    	pom.xml

--- src/test/java/org/elasticsearch/common/xcontent/cbor/JsonVsCborTests.java
+++ src/test/java/org/elasticsearch/common/xcontent/cbor/JsonVsCborTests.java
@@ -19,10 +19,6 @@
 
 package org.elasticsearch.common.xcontent.cbor;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentGenerator;
@@ -31,7 +27,6 @@ import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.test.ElasticsearchTestCase;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import static org.hamcrest.Matchers.equalTo;

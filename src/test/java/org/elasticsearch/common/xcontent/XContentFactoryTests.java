f210aec896b68d7e804c0e04733468539cac1358
Chris Earle <chris.earle@elasticsearch.com>
2014-11-25 16:44:49 -0500

    Revert "Update to Jackson 2.4.3"
    This reverts commit ecbb1b5fda820214076839c2b523ee641423eb28.

--- src/test/java/org/elasticsearch/common/xcontent/XContentFactoryTests.java
+++ src/test/java/org/elasticsearch/common/xcontent/XContentFactoryTests.java
@@ -29,45 +29,37 @@ import java.io.IOException;
 import static org.hamcrest.Matchers.equalTo;
 
 /**
- * Tests {@link XContentFactory} type generation.
+ *
  */
 public class XContentFactoryTests extends ElasticsearchTestCase {
 
 
     @Test
     public void testGuessJson() throws IOException {
-        assertType(XContentType.JSON);
+        testGuessType(XContentType.JSON);
     }
 
     @Test
     public void testGuessSmile() throws IOException {
-        assertType(XContentType.SMILE);
+        testGuessType(XContentType.SMILE);
     }
 
     @Test
     public void testGuessYaml() throws IOException {
-        assertType(XContentType.YAML);
+        testGuessType(XContentType.YAML);
     }
 
     @Test
     public void testGuessCbor() throws IOException {
-        assertType(XContentType.CBOR);
+        testGuessType(XContentType.CBOR);
     }
 
-    private void assertType(XContentType type) throws IOException {
-        for (XContentBuilder builder : generateBuilders(type)) {
-            assertBuilderType(builder, type);
-        }
-    }
+    private void testGuessType(XContentType type) throws IOException {
+        XContentBuilder builder = XContentFactory.contentBuilder(type);
+        builder.startObject();
+        builder.field("field1", "value1");
+        builder.endObject();
 
-    /**
-     * Assert the {@code builder} maps to the appropriate {@code type}.
-     *
-     * @param builder Builder to check.
-     * @param type Type to match.
-     * @throws IOException if any error occurs while checking the builder
-     */
-    private void assertBuilderType(XContentBuilder builder, XContentType type) throws IOException {
         assertThat(XContentFactory.xContentType(builder.bytes()), equalTo(type));
         BytesArray bytesArray = builder.bytes().toBytesArray();
         assertThat(XContentFactory.xContentType(new BytesStreamInput(bytesArray.array(), bytesArray.arrayOffset(), bytesArray.length(), false)), equalTo(type));
@@ -76,31 +68,5 @@ public class XContentFactoryTests extends ElasticsearchTestCase {
         if (type != XContentType.CBOR) {
             assertThat(XContentFactory.xContentType(builder.string()), equalTo(type));
         }
-    }
-
-    /**
-     * Generate builders to test various use cases to check.
-     *
-     * @param type The type to use.
-     * @return Never {@code null} array of unique {@link XContentBuilder}s testing different edge cases.
-     * @throws IOException if any error occurs while generating builders
-     */
-    private XContentBuilder[] generateBuilders(XContentType type) throws IOException {
-        XContentBuilder[] builders = new XContentBuilder[] {
-            XContentFactory.contentBuilder(type), XContentFactory.contentBuilder(type)
-        };
-
-        // simple object
-        builders[0].startObject();
-        builders[0].field("field1", "value1");
-            builders[0].startObject("object1");
-            builders[0].field("field2", "value2");
-            builders[0].endObject();
-        builders[0].endObject();
-
-        // empty object
-        builders[1].startObject().endObject();
-
-        return builders;
     }
 }

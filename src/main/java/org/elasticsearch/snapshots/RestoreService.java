f7177983b8087f6a2fcbcbb9831d538f08bea618
Igor Motov <igor@motovs.org>
2014-11-24 21:17:03 -0500

    Snapshot/Restore: fix compilation issue for JDK 1.7

--- src/main/java/org/elasticsearch/snapshots/RestoreService.java
+++ src/main/java/org/elasticsearch/snapshots/RestoreService.java
@@ -374,7 +374,7 @@ public class RestoreService extends AbstractComponent implements ClusterStateLis
         clusterService.submitStateUpdateTask("update snapshot state", new ProcessedClusterStateUpdateTask() {
 
             private RestoreInfo restoreInfo = null;
-            private HashMap<ShardId, ShardRestoreStatus> shards = null;
+            private Map<ShardId, ShardRestoreStatus> shards = null;
 
             @Override
             public ClusterState execute(ClusterState currentState) {
@@ -389,7 +389,7 @@ public class RestoreService extends AbstractComponent implements ClusterStateLis
                         if (entry.snapshotId().equals(request.snapshotId())) {
                             assert !found;
                             found = true;
-                            HashMap<ShardId, ShardRestoreStatus> shards = newHashMap(entry.shards());
+                            Map<ShardId, ShardRestoreStatus> shards = newHashMap(entry.shards());
                             logger.trace("[{}] Updating shard [{}] with status [{}]", request.snapshotId(), request.shardId(), request.status().state());
                             shards.put(request.shardId(), request.status());
                             if (!completed(shards)) {
@@ -423,7 +423,7 @@ public class RestoreService extends AbstractComponent implements ClusterStateLis
                 if (restoreInfo != null) {
                     RoutingTable routingTable = newState.getRoutingTable();
                     final List<ShardId> waitForStarted = newArrayList();
-                    for (HashMap.Entry<ShardId, ShardRestoreStatus> shard : shards.entrySet()) {
+                    for (Map.Entry<ShardId, ShardRestoreStatus> shard : shards.entrySet()) {
                         if (shard.getValue().state() == RestoreMetaData.State.SUCCESS ) {
                             ShardId shardId = shard.getKey();
                             ShardRouting shardRouting = findPrimaryShard(routingTable, shardId);

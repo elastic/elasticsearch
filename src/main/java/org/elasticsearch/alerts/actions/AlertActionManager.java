27f83bb6218160e6fea5e9d790a9fe41dfb9e56d
Martijn van Groningen <martijn.v.groningen@gmail.com>
2014-11-27 12:14:30 +0100

    Clear the queue once the reader thread sees that it needs to stop.
    Original commit: elastic/x-pack-elasticsearch@b70a224ff1463dd2de334ddd59c4d8950a2095cf

--- src/main/java/org/elasticsearch/alerts/actions/AlertActionManager.java
+++ src/main/java/org/elasticsearch/alerts/actions/AlertActionManager.java
@@ -132,7 +132,6 @@ public class AlertActionManager extends AbstractComponent {
 
     public void stop() {
         if (started.compareAndSet(true, false)) {
-            actionsToBeProcessed.clear();
             actionsToBeProcessed.add(END_ENTRY);
             logger.info("Stopped job queue");
         }
@@ -357,6 +356,7 @@ public class AlertActionManager extends AbstractComponent {
                 while (started()) {
                     AlertActionEntry entry = actionsToBeProcessed.take();
                     if (!started() || entry == END_ENTRY) {
+                        actionsToBeProcessed.clear();
                         logger.info("Stopping thread to read from the job queue");
                         return;
                     }

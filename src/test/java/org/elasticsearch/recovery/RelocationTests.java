5c6c7f23baddb24c890463c5e97faa00dea85118
Simon Willnauer <simonw@apache.org>
2014-11-28 11:01:15 +0100

    [TEST] add back accidetially removed test logging

--- src/test/java/org/elasticsearch/recovery/RelocationTests.java
+++ src/test/java/org/elasticsearch/recovery/RelocationTests.java
@@ -61,6 +61,7 @@ import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.test.BackgroundIndexer;
 import org.elasticsearch.test.ElasticsearchIntegrationTest;
 import org.elasticsearch.test.ElasticsearchIntegrationTest.ClusterScope;
+import org.elasticsearch.test.junit.annotations.TestLogging;
 import org.elasticsearch.test.transport.MockTransportService;
 import org.elasticsearch.transport.*;
 import org.junit.Test;
@@ -87,6 +88,7 @@ import static org.hamcrest.Matchers.*;
 /**
  */
 @ClusterScope(scope = Scope.TEST, numDataNodes = 0)
+@TestLogging("indices.recovery:TRACE,index.shard.service:TRACE")
 public class RelocationTests extends ElasticsearchIntegrationTest {
     private final TimeValue ACCEPTABLE_RELOCATION_TIME = new TimeValue(5, TimeUnit.MINUTES);
 

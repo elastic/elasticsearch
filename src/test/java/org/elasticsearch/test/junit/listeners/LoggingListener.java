d0d434cd3a0222a8fcf86fee0c8d8617a247408a
javanna <cavannaluca@gmail.com>
2014-12-09 13:36:42 +0100

    [TEST] LoggingListener to restore the initial logger levels after any modification
    Modifications to LoggingListener pushed with #8820 caused the original logger levels not to be reset after modifications, as the new state was saved for restore instead of the previous one.
    
     Added unit tests for LoggingListener as well.
    
    Closes #8845

--- src/test/java/org/elasticsearch/test/junit/listeners/LoggingListener.java
+++ src/test/java/org/elasticsearch/test/junit/listeners/LoggingListener.java
@@ -47,7 +47,7 @@ public class LoggingListener extends RunListener {
 
     @Override
     public void testRunStarted(Description description) throws Exception {
-        previousPackageLoggingMap = processTestLogging( description.getTestClass().getPackage().getAnnotation(TestLogging.class));
+        previousPackageLoggingMap = processTestLogging(description.getTestClass().getPackage().getAnnotation(TestLogging.class));
         previousClassLoggingMap = processTestLogging(description.getAnnotation(TestLogging.class));
     }
 
@@ -80,11 +80,13 @@ public class LoggingListener extends RunListener {
         if (map == null) {
             return null;
         }
+        Map<String, String> previousValues = new HashMap<>();
         for (Map.Entry<String, String> entry : map.entrySet()) {
             ESLogger esLogger = resolveLogger(entry.getKey());
+            previousValues.put(entry.getKey(), esLogger.getLevel());
             esLogger.setLevel(entry.getValue());
         }
-        return map;
+        return previousValues;
     }
 
     public static Map<String, String> getLoggersAndLevelsFromAnnotation(TestLogging testLogging) {

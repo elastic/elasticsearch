59f408cd577347335b1d107ffd9a78f4d6f4d675
Zachary Tong <zacharyjtong@gmail.com>
2014-12-15 15:10:29 -0500

    Move random query generation to own class
    The random query / filter generation deserves it's own class in the testing infrastructure, rather
    than a utility method of the profile query tests.  Has more widespread utility.
    
    Also tried to generify the methods a little, to make them easier to use outside profile query tests

--- src/test/java/org/elasticsearch/search/profile/ProfileQueryTests.java
+++ src/test/java/org/elasticsearch/search/profile/ProfileQueryTests.java
@@ -33,129 +33,15 @@ import org.elasticsearch.index.query.*;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.elasticsearch.test.query.RandomQueryGenerator.randomQueryBuilder;
+
 
 @ElasticsearchIntegrationTest.ClusterScope(scope=ElasticsearchIntegrationTest.Scope.TEST)
 public class ProfileQueryTests extends ElasticsearchIntegrationTest {
-
-    private QueryBuilder randomQueryBuilder(int numDocs, int depth) {
-
-        if (depth == 0) {
-            return QueryBuilders.termQuery("field1", English.intToEnglish(randomIntBetween(0, numDocs)));
-        }
-
-        QueryBuilder q;
-        switch (randomIntBetween(0,8)) {
-            case 0:
-                q = QueryBuilders.filteredQuery(randomQueryBuilder(numDocs, depth -1), randomFilterBuilder(numDocs, depth -1));
-                break;
-            case 1:
-                q = QueryBuilders.termQuery("field1", English.intToEnglish(randomIntBetween(0,numDocs)));
-                break;
-            case 2:
-                q = QueryBuilders.termsQuery("field1", English.intToEnglish(randomIntBetween(0,numDocs)),English.intToEnglish(randomIntBetween(0,numDocs)));
-                break;
-            case 3:
-                q = QueryBuilders.rangeQuery("field2").from(randomIntBetween(0,numDocs/2 - 1)).to(randomIntBetween(numDocs/2, numDocs));
-                break;
-            case 4:
-                q = QueryBuilders.boolQuery();
-                int numClause = randomIntBetween(0,5);
-                for (int i = 0; i < numClause; i++) {
-                    ((BoolQueryBuilder)q).must(randomQueryBuilder(numDocs, depth -1));
-                }
-                numClause = randomIntBetween(0,5);
-                for (int i = 0; i < numClause; i++) {
-                    ((BoolQueryBuilder)q).should(randomQueryBuilder(numDocs, depth -1));
-                }
-                numClause = randomIntBetween(0,5);
-                for (int i = 0; i < numClause; i++) {
-                    ((BoolQueryBuilder)q).mustNot(randomQueryBuilder(numDocs, depth -1));
-                }
-                break;
-            case 5:
-                q = QueryBuilders.matchAllQuery();
-                break;
-            case 6:
-                q = QueryBuilders.boostingQuery().boost(randomFloat())
-                        .positive(randomQueryBuilder(numDocs, depth - 1))
-                        .negativeBoost(randomFloat())
-                        .negative(randomQueryBuilder(numDocs, depth - 1)); 
-                break;
-            case 7:
-                if (randomBoolean()) {
-                    q = QueryBuilders.constantScoreQuery(randomQueryBuilder(numDocs, depth - 1));
-                } else {
-                    q = QueryBuilders.constantScoreQuery(randomFilterBuilder(numDocs, depth - 1));
-                }
-                break;
-            case 8:
-                q = QueryBuilders.commonTermsQuery("field1", English.intToEnglish(randomIntBetween(0,numDocs)));
-                break;
-            default:
-                q = QueryBuilders.termQuery("field1", English.intToEnglish(randomIntBetween(0,numDocs)));
-                break;
-        }
-
-        return q;
-    }
-
-    private FilterBuilder randomFilterBuilder(int numDocs, int depth) {
-        if (depth == 0) {
-            return FilterBuilders.termFilter("field1", English.intToEnglish(randomIntBetween(0,numDocs)));
-        } 
-        FilterBuilder f;
-        switch (randomIntBetween(0,7)) {
-            case 0:
-                f = FilterBuilders.matchAllFilter();
-                break;
-            case 1:
-                f = FilterBuilders.termFilter("field1", English.intToEnglish(randomIntBetween(0,numDocs)));
-                break;
-            case 2:
-                f = FilterBuilders.termsFilter("field1", English.intToEnglish(randomIntBetween(0,numDocs)),English.intToEnglish(randomIntBetween(0,numDocs)));
-                break;
-            case 3:
-                f = FilterBuilders.rangeFilter("field2").from(randomIntBetween(0,numDocs/2 - 1)).to(randomIntBetween(numDocs/2, numDocs));
-                break;
-            case 4:
-                f = FilterBuilders.boolFilter();
-                int numClause = randomIntBetween(0,5);
-                for (int i = 0; i < numClause; i++) {
-                    ((BoolFilterBuilder)f).must(randomFilterBuilder(numDocs, depth -1));
-                }
-                numClause = randomIntBetween(0,5);
-                for (int i = 0; i < numClause; i++) {
-                    ((BoolFilterBuilder)f).should(randomFilterBuilder(numDocs, depth -1));
-                }
-                numClause = randomIntBetween(0,5);
-                for (int i = 0; i < numClause; i++) {
-                    ((BoolFilterBuilder)f).mustNot(randomFilterBuilder(numDocs, depth -1));
-                }
-                break;
-            case 5:
-                f = FilterBuilders.andFilter();
-                numClause = randomIntBetween(0,5);
-                for (int i = 0; i < numClause; i++) {
-                    ((AndFilterBuilder)f).add(randomFilterBuilder(numDocs, depth -1));
-                }
-                break;
-            case 6:
-                f = FilterBuilders.orFilter();
-                numClause = randomIntBetween(0,5);
-                for (int i = 0; i < numClause; i++) {
-                    ((OrFilterBuilder)f).add(randomFilterBuilder(numDocs, depth -1));
-                }
-                break;
-            case 7:
-                f = FilterBuilders.notFilter(randomFilterBuilder(numDocs, depth -1));
-                break;
-            default:
-                f = FilterBuilders.termFilter("field1", English.intToEnglish(randomIntBetween(0,numDocs)));
-                break;
-        }
-
-        return f;
-    }
 
 
     @Test
@@ -176,12 +62,15 @@ public class ProfileQueryTests extends ElasticsearchIntegrationTest {
             );
         }
 
+        List<String> stringFields = Arrays.asList("field1");
+        List<String> numericFields = Arrays.asList("field2");
+
         indexRandom(true, docs);
 
         refresh();
         int iters = between(20, 100);
         for (int i = 0; i < iters; i++) {
-            QueryBuilder q = randomQueryBuilder(numDocs, 3);
+            QueryBuilder q = randomQueryBuilder(stringFields, numericFields, numDocs, 3);
             logger.info(q.toString());
 
             SearchResponse resp = client().prepareSearch().setQuery(q).setProfile(true).setSearchType(SearchType.QUERY_THEN_FETCH).execute().actionGet();
---
+++ src/test/java/org/elasticsearch/test/query/RandomQueryGenerator.java
@@ -0,0 +1,268 @@
+package org.elasticsearch.test.query;
+
+import org.apache.lucene.util.English;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.carrotsearch.randomizedtesting.RandomizedTest.*;
+
+
+public class RandomQueryGenerator {
+    public static QueryBuilder randomQueryBuilder(List<String> stringFields, List<String> numericFields, int numDocs, int depth) {
+        assertTrue("Must supply at least one string field", stringFields.size() > 0);
+        assertTrue("Must supply at least one numeric field", numericFields.size() > 0);
+
+        if (depth == 0) {
+            return randomTerminalQuery(stringFields, numericFields, numDocs);
+        }
+
+        switch (randomIntBetween(0,4)) {
+            case 0:
+                return randomTerminalQuery(stringFields, numericFields, numDocs);
+            case 1:
+                return QueryBuilders.filteredQuery(randomQueryBuilder(stringFields, numericFields, numDocs, depth -1), randomFilterBuilder(numDocs, depth -1));
+            case 2:
+                return randomBoolQuery(stringFields, numericFields, numDocs, depth);
+            case 3:
+                return randomBoostingQuery(stringFields, numericFields, numDocs, depth);
+            case 4:
+                return randomConstantScoreQuery(stringFields, numericFields, numDocs, depth);
+            default:
+                return randomTerminalQuery(stringFields, numericFields, numDocs);
+        }
+    }
+
+    private static QueryBuilder randomTerminalQuery(List<String> stringFields, List<String> numericFields, int numDocs) {
+        switch (randomIntBetween(0,4)) {
+            case 0:
+                return randomTermQuery(stringFields, numDocs);
+            case 1:
+                return randomTermsQuery(stringFields, numDocs);
+            case 2:
+                return randomRangeQuery(numericFields, numDocs);
+            case 3:
+                return QueryBuilders.matchAllQuery();
+            case 4:
+                return randomCommonTermsQuery(stringFields, numDocs);
+            case 5:
+                return randomFuzzyQuery(stringFields, numDocs);
+            default:
+                return randomTermQuery(stringFields, numDocs);
+        }
+    }
+
+    public static FilterBuilder randomFilterBuilder(int numDocs, int depth) {
+        if (depth == 0) {
+            return FilterBuilders.termFilter("field1", English.intToEnglish(randomInt(numDocs)));
+        }
+        FilterBuilder f;
+        switch (randomIntBetween(0,7)) {
+            case 0:
+                f = FilterBuilders.matchAllFilter();
+                break;
+            case 1:
+                f = FilterBuilders.termFilter("field1", English.intToEnglish(randomInt(numDocs)));
+                break;
+            case 2:
+                f = FilterBuilders.termsFilter("field1", English.intToEnglish(randomInt(numDocs)),English.intToEnglish(randomInt(numDocs)));
+                break;
+            case 3:
+                f = FilterBuilders.rangeFilter("field2").from(randomIntBetween(0,numDocs/2 - 1)).to(randomIntBetween(numDocs/2, numDocs));
+                break;
+            case 4:
+                f = FilterBuilders.boolFilter();
+                int numClause = randomIntBetween(0,5);
+                for (int i = 0; i < numClause; i++) {
+                    ((BoolFilterBuilder)f).must(randomFilterBuilder(numDocs, depth -1));
+                }
+                numClause = randomIntBetween(0,5);
+                for (int i = 0; i < numClause; i++) {
+                    ((BoolFilterBuilder)f).should(randomFilterBuilder(numDocs, depth -1));
+                }
+                numClause = randomIntBetween(0,5);
+                for (int i = 0; i < numClause; i++) {
+                    ((BoolFilterBuilder)f).mustNot(randomFilterBuilder(numDocs, depth -1));
+                }
+                break;
+            case 5:
+                f = FilterBuilders.andFilter();
+                numClause = randomIntBetween(0,5);
+                for (int i = 0; i < numClause; i++) {
+                    ((AndFilterBuilder)f).add(randomFilterBuilder(numDocs, depth -1));
+                }
+                break;
+            case 6:
+                f = FilterBuilders.orFilter();
+                numClause = randomIntBetween(0,5);
+                for (int i = 0; i < numClause; i++) {
+                    ((OrFilterBuilder)f).add(randomFilterBuilder(numDocs, depth -1));
+                }
+                break;
+            case 7:
+                f = FilterBuilders.notFilter(randomFilterBuilder(numDocs, depth -1));
+                break;
+            default:
+                f = FilterBuilders.termFilter("field1", English.intToEnglish(randomInt(numDocs)));
+                break;
+        }
+
+        return f;
+    }
+
+    private static String randomQueryString(int max) {
+        StringBuilder qsBuilder = new StringBuilder();
+
+        for (int i = 0; i < max; i++) {
+            qsBuilder.append(English.intToEnglish(randomInt(max)));
+            qsBuilder.append(" ");
+        }
+
+        return qsBuilder.toString();
+    }
+
+    private static String randomField(List<String> fields) {
+        return fields.get(randomInt(fields.size() - 1));
+    }
+
+
+
+    private static QueryBuilder randomTermQuery(List<String> fields, int numDocs) {
+        return QueryBuilders.termQuery(randomField(fields), randomQueryString(1));
+    }
+
+    private static QueryBuilder randomTermsQuery(List<String> fields, int numDocs) {
+        int numTerms = randomInt(numDocs);
+        ArrayList<String> terms = new ArrayList<>(numTerms);
+
+        for (int i = 0; i < numTerms; i++) {
+            terms.add(randomQueryString(1));
+        }
+
+        return QueryBuilders.termsQuery(randomField(fields), terms);
+    }
+
+    private static QueryBuilder randomRangeQuery(List<String> fields, int numDocs) {
+        QueryBuilder q =  QueryBuilders.rangeQuery(randomField(fields));
+
+        if (randomBoolean()) {
+            ((RangeQueryBuilder)q).from(randomIntBetween(0, numDocs / 2 - 1));
+        }
+        if (randomBoolean()) {
+            ((RangeQueryBuilder)q).to(randomIntBetween(numDocs / 2, numDocs));
+        }
+
+        return q;
+    }
+
+    private static QueryBuilder randomBoolQuery(List<String> stringFields, List<String> numericFields, int numDocs, int depth) {
+        QueryBuilder q = QueryBuilders.boolQuery();
+        int numClause = randomIntBetween(0,5);
+        for (int i = 0; i < numClause; i++) {
+            ((BoolQueryBuilder)q).must(randomQueryBuilder(stringFields, numericFields,numDocs, depth -1));
+        }
+
+        numClause = randomIntBetween(0,5);
+        for (int i = 0; i < numClause; i++) {
+            ((BoolQueryBuilder)q).should(randomQueryBuilder(stringFields, numericFields,numDocs, depth -1));
+        }
+
+        numClause = randomIntBetween(0,5);
+        for (int i = 0; i < numClause; i++) {
+            ((BoolQueryBuilder)q).mustNot(randomQueryBuilder(stringFields, numericFields, numDocs, depth -1));
+        }
+
+        return q;
+    }
+
+    private static QueryBuilder randomBoostingQuery(List<String> stringFields, List<String> numericFields, int numDocs, int depth) {
+        return QueryBuilders.boostingQuery().boost(randomFloat())
+                .positive(randomQueryBuilder(stringFields, numericFields, numDocs, depth - 1))
+                .negativeBoost(randomFloat())
+                .negative(randomQueryBuilder(stringFields, numericFields, numDocs, depth - 1));
+    }
+
+    private static QueryBuilder randomConstantScoreQuery(List<String> stringFields, List<String> numericFields, int numDocs, int depth) {
+        if (randomBoolean()) {
+            return QueryBuilders.constantScoreQuery(randomQueryBuilder(stringFields, numericFields, numDocs, depth - 1));
+        } else {
+            return QueryBuilders.constantScoreQuery(randomFilterBuilder(numDocs, depth - 1));
+        }
+    }
+
+    private static QueryBuilder randomCommonTermsQuery(List<String> fields, int numDocs) {
+        int numTerms = randomInt(numDocs);
+
+        QueryBuilder q = QueryBuilders.commonTermsQuery(randomField(fields), randomQueryString(numTerms));
+        if (randomBoolean()) {
+            ((CommonTermsQueryBuilder)q).boost(randomFloat());
+        }
+
+        if (randomBoolean()) {
+            ((CommonTermsQueryBuilder)q).cutoffFrequency(randomFloat());
+        }
+
+        if (randomBoolean()) {
+            ((CommonTermsQueryBuilder)q).highFreqMinimumShouldMatch(Integer.toString(randomInt(numTerms)))
+                    .highFreqOperator(randomBoolean() ? CommonTermsQueryBuilder.Operator.AND : CommonTermsQueryBuilder.Operator.OR);
+        }
+
+        if (randomBoolean()) {
+            ((CommonTermsQueryBuilder)q).lowFreqMinimumShouldMatch(Integer.toString(randomInt(numTerms)))
+                    .lowFreqOperator(randomBoolean() ? CommonTermsQueryBuilder.Operator.AND : CommonTermsQueryBuilder.Operator.OR);
+        }
+
+        return q;
+    }
+
+    private static QueryBuilder randomFuzzyQuery(List<String> fields, int numDocs) {
+
+        QueryBuilder q = QueryBuilders.fuzzyQuery(randomField(fields), randomQueryString(1));
+
+        if (randomBoolean()) {
+            ((FuzzyQueryBuilder)q).boost(randomFloat());
+        }
+
+        if (randomBoolean()) {
+            switch (randomIntBetween(0, 5)) {
+                case 0:
+                    ((FuzzyQueryBuilder)q).fuzziness(Fuzziness.AUTO);
+                    break;
+                case 1:
+                    ((FuzzyQueryBuilder)q).fuzziness(Fuzziness.ONE);
+                    break;
+                case 2:
+                    ((FuzzyQueryBuilder)q).fuzziness(Fuzziness.TWO);
+                    break;
+                case 3:
+                    ((FuzzyQueryBuilder)q).fuzziness(Fuzziness.ZERO);
+                    break;
+                case 4:
+                    ((FuzzyQueryBuilder)q).fuzziness(Fuzziness.fromEdits(randomIntBetween(0,2)));
+                    break;
+                case 5:
+                    ((FuzzyQueryBuilder)q).fuzziness(Fuzziness.fromSimilarity(randomFloat()));
+                    break;
+                default:
+                    ((FuzzyQueryBuilder)q).fuzziness(Fuzziness.AUTO);
+                    break;
+            }
+        }
+
+        if (randomBoolean()) {
+            ((FuzzyQueryBuilder)q).maxExpansions(randomInt());
+        }
+
+        if (randomBoolean()) {
+            ((FuzzyQueryBuilder)q).prefixLength(randomInt());
+        }
+
+        if (randomBoolean()) {
+            ((FuzzyQueryBuilder)q).transpositions(randomBoolean());
+        }
+
+        return q;
+    }
+}

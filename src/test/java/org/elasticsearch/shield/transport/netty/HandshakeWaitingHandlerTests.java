b4b3101b636783c89922b0155817a9deaac255f7
jaymode <jay.modi@elasticsearch.com>
2014-12-31 14:21:49 -0500

    Fix compilation error by removing use of SecureRandom.getInstanceStrong()
    Original commit: elastic/x-pack-elasticsearch@3a5d1c0e89a80c0d1995756d45aef22c8b708868

--- src/test/java/org/elasticsearch/shield/transport/netty/HandshakeWaitingHandlerTests.java
+++ src/test/java/org/elasticsearch/shield/transport/netty/HandshakeWaitingHandlerTests.java
@@ -217,7 +217,7 @@ public class HandshakeWaitingHandlerTests extends ElasticsearchTestCase {
         @Override
         public ChannelFuture call() throws Exception {
             ChannelBuffer buffer = ChannelBuffers.buffer(8);
-            buffer.writeLong(SecureRandom.getInstanceStrong().nextLong());
+            buffer.writeLong(SecureRandom.getInstance("SHA1PRNG").nextLong());
 
             // Connect and wait, then immediately start writing
             ChannelFuture future = bootstrap.connect(new InetSocketAddress("localhost", port));

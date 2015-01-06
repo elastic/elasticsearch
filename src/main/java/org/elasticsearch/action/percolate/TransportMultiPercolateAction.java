5b733efb79da7c9ba11558ba272673e2011c1447
Martijn van Groningen <martijn.v.groningen@gmail.com>
2015-01-06 15:17:20 +0100

    Fixed a bug that was caused by specifying routing on a multi percolate request causing an ArrayIndexOutOfBoundsException.
    The multi percolate shard responses are collected in an atomic array which uses the shard id is used as index, but the number of shards the multi percolate request was meant to go to was used as size of this array instead the total number of shards an index has. This caused the exception when routing was used.
    
    Closes #6214

--- src/main/java/org/elasticsearch/action/percolate/TransportMultiPercolateAction.java
+++ src/main/java/org/elasticsearch/action/percolate/TransportMultiPercolateAction.java
@@ -136,7 +136,7 @@ public class TransportMultiPercolateAction extends HandledTransportAction<MultiP
 
     }
 
-    private class ASyncAction {
+    private final class ASyncAction {
 
         final ActionListener<MultiPercolateResponse> finalListener;
         final Map<ShardId, TransportShardMultiPercolateAction.Request> requestsByShard;
@@ -188,7 +188,9 @@ public class TransportMultiPercolateAction extends HandledTransportAction<MultiP
                         continue;
                     }
 
-                    responsesByItemAndShard.set(slot, new AtomicReferenceArray(shards.size()));
+                    // The shard id is used as index in the atomic ref array, so we need to find out how many shards there are regardless of routing:
+                    int numShards = clusterService.operationRouting().searchShardsCount(clusterState, percolateRequest.indices(), concreteIndices, null, null);
+                    responsesByItemAndShard.set(slot, new AtomicReferenceArray(numShards));
                     expectedOperationsPerItem.set(slot, new AtomicInteger(shards.size()));
                     for (ShardIterator shard : shards) {
                         ShardId shardId = shard.shardId();

c29f4db6057accc7965f89c986d821674ab6ead0
Simon Willnauer <simonw@apache.org>
2015-01-08 15:36:36 +0100

    [TEST] Ensure shard lock is acquired before we try the timeout version

--- src/test/java/org/elasticsearch/env/NodeEnvironmentTests.java
+++ src/test/java/org/elasticsearch/env/NodeEnvironmentTests.java
@@ -25,11 +25,9 @@ import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.AbstractRunnable;
-import org.elasticsearch.common.util.concurrent.CountDown;
 import org.elasticsearch.index.Index;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.test.ElasticsearchTestCase;
-import org.elasticsearch.test.junit.annotations.TestLogging;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -154,7 +152,6 @@ public class NodeEnvironmentTests extends ElasticsearchTestCase {
     }
 
     @Test
-    @TestLogging("env:TRACE")
     public void testDeleteSafe() throws IOException, InterruptedException {
         Settings settings = nodeEnvSettings(tmpPaths());
         final NodeEnvironment env = new NodeEnvironment(settings, new Environment(settings));
@@ -202,6 +199,8 @@ public class NodeEnvironmentTests extends ElasticsearchTestCase {
 
         final AtomicReference<Throwable> threadException = new AtomicReference<>();
         final CountDownLatch latch = new CountDownLatch(1);
+        final CountDownLatch blockLatch = new CountDownLatch(1);
+        final CountDownLatch start = new CountDownLatch(1);
         if (randomBoolean()) {
             Thread t = new Thread(new AbstractRunnable() {
                 @Override
@@ -209,12 +208,15 @@ public class NodeEnvironmentTests extends ElasticsearchTestCase {
                     logger.error("unexpected error", t);
                     threadException.set(t);
                     latch.countDown();
+                    blockLatch.countDown();
                 }
 
                 @Override
                 protected void doRun() throws Exception {
-                    try (ShardLock fooLock = env.shardLock(new ShardId("foo", 1))) {
-                        Thread.sleep(100);
+                    start.await();
+                    try (ShardLock _ = env.shardLock(new ShardId("foo", 1))) {
+                        blockLatch.countDown();
+                        Thread.sleep(randomIntBetween(1, 10));
                     }
                     latch.countDown();
                 }
@@ -222,7 +224,10 @@ public class NodeEnvironmentTests extends ElasticsearchTestCase {
             t.start();
         } else {
             latch.countDown();
+            blockLatch.countDown();
         }
+        start.countDown();
+        blockLatch.await();
 
         env.deleteIndexDirectorySafe(new Index("foo"), 5000, idxSettings);
 

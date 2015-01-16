1d040c4c234f2943f3663b644f4c6e1bcff7ab1e
uboness <uboness@gmail.com>
2015-01-16 02:53:23 +0100

    [Cleanup] - removed redundant overridden methods in esusers realm
    Original commit: elastic/x-pack-elasticsearch@98ef6a9953d4ce14664f412dbf5338a38083e5dc

--- src/main/java/org/elasticsearch/shield/authc/esusers/ESUsersRealm.java
+++ src/main/java/org/elasticsearch/shield/authc/esusers/ESUsersRealm.java
@@ -10,14 +10,11 @@ import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.rest.RestController;
-import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.shield.User;
-import org.elasticsearch.shield.authc.AuthenticationToken;
 import org.elasticsearch.shield.authc.Realm;
 import org.elasticsearch.shield.authc.support.CachingUsernamePasswordRealm;
 import org.elasticsearch.shield.authc.support.RefreshListener;
 import org.elasticsearch.shield.authc.support.UsernamePasswordToken;
-import org.elasticsearch.transport.TransportMessage;
 import org.elasticsearch.watcher.ResourceWatcherService;
 
 /**
@@ -37,26 +34,6 @@ public class ESUsersRealm extends CachingUsernamePasswordRealm {
         userPasswdStore.addListener(listener);
         this.userRolesStore = userRolesStore;
         userRolesStore.addListener(listener);
-    }
-
-    @Override
-    public String type() {
-        return TYPE;
-    }
-
-    @Override
-    public UsernamePasswordToken token(RestRequest request) {
-        return UsernamePasswordToken.extractToken(request, null);
-    }
-
-    @Override
-    public UsernamePasswordToken token(TransportMessage<?> message) {
-        return UsernamePasswordToken.extractToken(message, null);
-    }
-
-    @Override
-    public boolean supports(AuthenticationToken token) {
-        return token instanceof UsernamePasswordToken;
     }
 
     @Override

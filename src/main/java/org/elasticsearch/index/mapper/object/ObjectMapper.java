46b055c907c4eea423bc659a10556305c199ed22
simaov <simonchuk.ov@gmail.com>
2015-01-27 16:49:40 +0200

    #9444 throw StrictDynamicMappingException exception if dynamic is 'strict' and undeclared field value is NULL, test for this
    Fixes #9445

--- src/main/java/org/elasticsearch/index/mapper/object/ObjectMapper.java
+++ src/main/java/org/elasticsearch/index/mapper/object/ObjectMapper.java
@@ -538,6 +538,10 @@ public class ObjectMapper implements Mapper, AllFieldMapper.IncludeInAll {
                 }
             }
             mapper.parse(context);
+        } else {
+            if (dynamic == Dynamic.STRICT) {
+                throw new StrictDynamicMappingException(fullPath, lastFieldName);
+            }
         }
     }
 

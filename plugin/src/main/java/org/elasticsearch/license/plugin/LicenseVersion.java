8ef2df3f1b33937ac7b729da4d568ec78d166b31
Areek Zillur <areek.zillur@elasticsearch.com>
2015-01-26 21:45:36 -0500

    moving version to 2.0.0-SNAPSHOT
    Original commit: elastic/x-pack-elasticsearch@e6dd782df96a476a3c4f85a0a165d8663f54438a

--- plugin/src/main/java/org/elasticsearch/license/plugin/LicenseVersion.java
+++ plugin/src/main/java/org/elasticsearch/license/plugin/LicenseVersion.java
@@ -23,9 +23,11 @@ public class LicenseVersion implements Serializable {
     // the (internal) format of the id is there so we can easily do after/before checks on the id
 
     public static final int V_1_0_0_ID = /*00*/1000099;
+    public static final int V_2_0_0_ID = /*00*/2000099;
     public static final LicenseVersion V_1_0_0 = new LicenseVersion(V_1_0_0_ID, false, License.VERSION_START, Version.V_1_4_0_Beta1);
+    public static final LicenseVersion V_2_0_0 = new LicenseVersion(V_2_0_0_ID, true, License.VERSION_START, Version.V_1_4_0_Beta1);
 
-    public static final LicenseVersion CURRENT = V_1_0_0;
+    public static final LicenseVersion CURRENT = V_2_0_0;
 
     public static LicenseVersion readVersion(StreamInput in) throws IOException {
         return fromId(in.readVInt());

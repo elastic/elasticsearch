1695f76f68dede73143a4bfbd539d2d750bf5bca
Boaz Leskes <b.leskes@gmail.com>
2015-01-28 11:28:35 +0100

    Test: testOldIndexes should disable merging
    It verifies some segments need to be upgraded, but if they are merged away, there are upgraded implicitly

--- src/test/java/org/elasticsearch/bwcompat/OldIndexBackwardsCompatibilityTests.java
+++ src/test/java/org/elasticsearch/bwcompat/OldIndexBackwardsCompatibilityTests.java
@@ -26,22 +26,19 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.merge.policy.MergePolicyModule;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.node.internal.InternalNode;
 import org.elasticsearch.rest.action.admin.indices.upgrade.UpgradeTest;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.sort.SortOrder;
 import org.elasticsearch.test.hamcrest.ElasticsearchAssertions;
+import org.elasticsearch.test.index.merge.NoMergePolicyProvider;
 import org.elasticsearch.test.rest.client.http.HttpRequestBuilder;
 import org.hamcrest.Matchers;
 
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 
@@ -134,7 +131,8 @@ public class OldIndexBackwardsCompatibilityTests extends StaticIndexBackwardComp
     void assertOldIndexWorks(String index) throws Exception {
         Settings settings = ImmutableSettings.builder()
             .put(InternalNode.HTTP_ENABLED, true) // for _upgrade
-            .build();
+                .put(MergePolicyModule.MERGE_POLICY_TYPE_KEY, NoMergePolicyProvider.class) // disable merging so no segments will be upgraded
+                .build();
         loadIndex(index, settings);
         logMemoryStats();
         assertBasicSearchWorks();

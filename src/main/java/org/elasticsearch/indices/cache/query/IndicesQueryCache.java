15a766084da3c3e9d1982dcdbbc9cfd233e1fc82
Simon Willnauer <simonw@apache.org>
2015-01-29 10:39:17 +0100

    [CACHE] Use correct number of bytes in query cache accounting
    today we use the length of the BytesReference which is misleading since
    the reference is paged such that the length != ramBytesUsed. This can lead
    to a way higher memory consuption than expected if query results are tiny
    since each query result requires at least 16kb. Yet, we should rethink this
    strategy for query results that are very small ie. less than 20% of the ramBytesUsed
    but this commit first tries to make the acocunting correct.

--- src/main/java/org/elasticsearch/indices/cache/query/IndicesQueryCache.java
+++ src/main/java/org/elasticsearch/indices/cache/query/IndicesQueryCache.java
@@ -38,14 +38,18 @@ import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.cluster.ClusterService;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.bytes.PagedBytesReference;
+import org.elasticsearch.common.bytes.ReleasablePagedBytesReference;
 import org.elasticsearch.common.component.AbstractComponent;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
+import org.elasticsearch.common.io.stream.ReleasableBytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.MemorySizeValue;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.util.BigArrays;
 import org.elasticsearch.common.util.concurrent.ConcurrentCollections;
 import org.elasticsearch.common.util.concurrent.EsRejectedExecutionException;
 import org.elasticsearch.index.shard.IndexShard;
@@ -82,7 +86,7 @@ import static org.elasticsearch.common.Strings.hasLength;
  * There are still several TODOs left in this class, some easily addressable, some more complex, but the support
  * is functional.
  */
-public class IndicesQueryCache extends AbstractComponent implements RemovalListener<IndicesQueryCache.Key, BytesReference> {
+public class IndicesQueryCache extends AbstractComponent implements RemovalListener<IndicesQueryCache.Key, IndicesQueryCache.Value> {
 
     /**
      * A setting to enable or disable query caching on an index level. Its dynamic by default
@@ -110,7 +114,7 @@ public class IndicesQueryCache extends AbstractComponent implements RemovalListe
     private final TimeValue expire;
     private final int concurrencyLevel;
 
-    private volatile Cache<Key, BytesReference> cache;
+    private volatile Cache<Key, Value> cache;
 
     @Inject
     public IndicesQueryCache(Settings settings, ClusterService clusterService, ThreadPool threadPool) {
@@ -135,7 +139,7 @@ public class IndicesQueryCache extends AbstractComponent implements RemovalListe
     private void buildCache() {
         long sizeInBytes = MemorySizeValue.parseBytesSizeValueOrHeapRatio(size).bytes();
 
-        CacheBuilder<Key, BytesReference> cacheBuilder = CacheBuilder.newBuilder()
+        CacheBuilder<Key, Value> cacheBuilder = CacheBuilder.newBuilder()
                 .maximumWeight(sizeInBytes).weigher(new QueryCacheWeigher()).removalListener(this);
         cacheBuilder.concurrencyLevel(concurrencyLevel);
 
@@ -146,12 +150,11 @@ public class IndicesQueryCache extends AbstractComponent implements RemovalListe
         cache = cacheBuilder.build();
     }
 
-    private static class QueryCacheWeigher implements Weigher<Key, BytesReference> {
+    private static class QueryCacheWeigher implements Weigher<Key, Value> {
 
         @Override
-        public int weigh(Key key, BytesReference value) {
-            // TODO add sizeInBytes to BytesReference, since it might be paged.... (Accountable)
-            return (int) (key.ramBytesUsed() + value.length());
+        public int weigh(Key key, Value value) {
+            return (int) (key.ramBytesUsed() + value.ramBytesUsed());
         }
     }
 
@@ -170,7 +173,7 @@ public class IndicesQueryCache extends AbstractComponent implements RemovalListe
     }
 
     @Override
-    public void onRemoval(RemovalNotification<Key, BytesReference> notification) {
+    public void onRemoval(RemovalNotification<Key, Value> notification) {
         if (notification.getKey() == null) {
             return;
         }
@@ -222,7 +225,7 @@ public class IndicesQueryCache extends AbstractComponent implements RemovalListe
         assert canCache(request, context);
         Key key = buildKey(request, context);
         Loader loader = new Loader(queryPhase, context, key);
-        BytesReference value = cache.get(key, loader);
+        Value value = cache.get(key, loader);
         if (loader.isLoaded()) {
             key.shard.queryCache().onMiss();
             // see if its the first time we see this reader, and make sure to register a cleanup key
@@ -238,10 +241,10 @@ public class IndicesQueryCache extends AbstractComponent implements RemovalListe
         }
 
         // try and be smart, and reuse an already loaded and constructed QueryResult of in VM execution
-        return new BytesQuerySearchResult(context.id(), context.shardTarget(), value, loader.isLoaded() ? context.queryResult() : null);
+        return new BytesQuerySearchResult(context.id(), context.shardTarget(), value.reference, loader.isLoaded() ? context.queryResult() : null);
     }
 
-    private static class Loader implements Callable<BytesReference> {
+    private static class Loader implements Callable<Value> {
 
         private final QueryPhase queryPhase;
         private final SearchContext context;
@@ -259,17 +262,39 @@ public class IndicesQueryCache extends AbstractComponent implements RemovalListe
         }
 
         @Override
-        public BytesReference call() throws Exception {
+        public Value call() throws Exception {
             queryPhase.execute(context);
-            BytesStreamOutput out = new BytesStreamOutput();
-            context.queryResult().writeToNoId(out);
-            // for now, keep the paged data structure, which might have unused bytes to fill a page, but better to keep
-            // the memory properly paged instead of having varied sized bytes
-            BytesReference value = out.bytes();
-            assert verifyCacheSerializationSameAsQueryResult(value, context, context.queryResult());
-            loaded = true;
-            key.shard.queryCache().onCached(key, value);
-            return value;
+            try (BytesStreamOutput out = new BytesStreamOutput()) {
+                context.queryResult().writeToNoId(out);
+                // for now, keep the paged data structure, which might have unused bytes to fill a page, but better to keep
+                // the memory properly paged instead of having varied sized bytes
+                final BytesReference reference = out.bytes();
+                assert verifyCacheSerializationSameAsQueryResult(reference, context, context.queryResult());
+                loaded = true;
+                Value value = new Value(reference, out.ramBytesUsed());
+                key.shard.queryCache().onCached(key, value);
+                return value;
+            }
+        }
+    }
+
+    public static class Value implements Accountable {
+        final BytesReference reference;
+        final long ramBytesUsed;
+
+        public Value(BytesReference reference, long ramBytesUsed) {
+            this.reference = reference;
+            this.ramBytesUsed = ramBytesUsed;
+        }
+
+        @Override
+        public long ramBytesUsed() {
+            return ramBytesUsed;
+        }
+
+        @Override
+        public Collection<Accountable> getChildResources() {
+            return Collections.emptyList();
         }
     }
 

8c79b08311697523c168a3ed4d88435e0304030c
Christoph Büscher <christoph.buescher@elasticsearch.com>
2015-02-02 17:46:08 +0100

    Aggregations: Add 'offset' option to histogram aggregation
    Histogram aggregation supports an 'offset' option to move bucket boundaries.
    In a histogram with buckets of size X these can be moved from 0, X, 2X, 3X,...
    by an offset value of Y to Y, X+Y, 2X+Y, 3X+Y... by using the 'offset' option.
    The previous 'pre_offset' and 'post_offset' options are removed in favour of
    the simplified 'offset' option.
    
    Closes #9417
    Closes #9505

--- src/test/java/org/elasticsearch/common/rounding/RoundingTests.java
+++ src/test/java/org/elasticsearch/common/rounding/RoundingTests.java
@@ -20,25 +20,82 @@
 package org.elasticsearch.common.rounding;
 
 import org.elasticsearch.test.ElasticsearchTestCase;
+import org.junit.Test;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
-
-
 public class RoundingTests extends ElasticsearchTestCase {
 
+    /**
+     * simple testcase to ilustrate how Rounding.Interval works on readable input
+     */
+    @Test
     public void testInterval() {
+        int interval = 10;
+        Rounding.Interval rounding = new Rounding.Interval(interval);
+        int value = 24;
+        final long key = rounding.roundKey(24);
+        final long r = rounding.round(24);
+        String message = "round(" + value + ", interval=" + interval + ") = " + r;
+        assertEquals(value/interval, key);
+        assertEquals(value/interval * interval, r);
+        assertEquals(message, 0, r % interval);
+    }
+
+    @Test
+    public void testIntervalRandom() {
         final long interval = randomIntBetween(1, 100);
         Rounding.Interval rounding = new Rounding.Interval(interval);
         for (int i = 0; i < 1000; ++i) {
             long l = Math.max(randomLong(), Long.MIN_VALUE + interval);
+            final long key = rounding.roundKey(l);
             final long r = rounding.round(l);
             String message = "round(" + l + ", interval=" + interval + ") = " + r;
             assertEquals(message, 0, r % interval);
             assertThat(message, r, lessThanOrEqualTo(l));
             assertThat(message, r + interval, greaterThan(l));
+            assertEquals(message, r, key*interval);
         }
     }
 
+    /**
+     * Simple testcase to ilustrate how Rounding.Pre works on readable input.
+     * preOffset shifts input value before rounding (so here 24 -> 31)
+     * postOffset shifts rounded Value after rounding (here 30 -> 35)
+     */
+    @Test
+    public void testPrePostRounding() {
+        int interval = 10;
+        int value = 24;
+        int preOffset = 7;
+        int postOffset = 5;
+        Rounding.PrePostRounding rounding = new Rounding.PrePostRounding(new Rounding.Interval(interval), preOffset, postOffset);
+        final long key = rounding.roundKey(24);
+        final long roundedValue = rounding.round(24);
+        String message = "round(" + value + ", interval=" + interval + ") = " + roundedValue;
+        assertEquals(3, key);
+        assertEquals(35, roundedValue);
+        assertEquals(message, postOffset, roundedValue % interval);
+    }
+
+    @Test
+    public void testPrePostRoundingRandom() {
+        final long interval = randomIntBetween(1, 100);
+        Rounding.Interval internalRounding = new Rounding.Interval(interval);
+        final long preRounding = randomIntBetween(-100, 100);
+        final long postRounding = randomIntBetween(-100, 100);
+        Rounding.PrePostRounding  prePost = new Rounding.PrePostRounding(new Rounding.Interval(interval), preRounding, postRounding);
+        long safetyMargin = Math.abs(interval) + Math.abs(preRounding) + Math.abs(postRounding); // to prevent range overflow / underflow
+        for (int i = 0; i < 1000; ++i) {
+            long l = Math.max(randomLong() - safetyMargin, Long.MIN_VALUE + safetyMargin);
+            final long key = prePost.roundKey(l);
+            final long r = prePost.round(l);
+            String message = "round(" + l + ", interval=" + interval + ") = "+ r;
+            assertEquals(message, internalRounding.round(l+preRounding), r - postRounding);
+            assertThat(message, r - postRounding, lessThanOrEqualTo(l + preRounding));
+            assertThat(message, r + interval - postRounding, greaterThan(l + preRounding));
+            assertEquals(message, r, key*interval + postRounding);
+        }
+    }
 }

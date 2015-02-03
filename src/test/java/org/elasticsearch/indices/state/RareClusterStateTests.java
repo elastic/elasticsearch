896e8657ea498c074e9b7661d71f698b5cd23e94
Boaz Leskes <b.leskes@gmail.com>
2015-02-03 09:28:02 +0100

    Discovery: check index uuid when merging incoming cluster state into local
    In big deployment ClusterState can be large. To make sure we keep reusing objects that were promoted to the Old Gen, ZenDiscovery has an optimization where it tries to reuse existing IndexMetaData object (containing among other things the mappings) from the current cluster state if they didn't change. The comparison currently uses the index name and the metadata version. This is however not enough and we should also check the index uuid. In extreme cases, where cluster state processing is slow and the index in question is deleted and recreated and these operations are batch processed together, we can use the wrong meta data if the version is also identical. This can happen if people create the index with all meta data predefined and no settings were changed.
    
    Closes #9489
    Closes #9541

--- src/test/java/org/elasticsearch/indices/state/RareClusterStateTests.java
+++ src/test/java/org/elasticsearch/indices/state/RareClusterStateTests.java
@@ -23,6 +23,7 @@ import com.google.common.collect.ImmutableMap;
 import org.elasticsearch.cluster.ClusterInfo;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.DiskUsage;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.cluster.routing.RoutingNodes;
 import org.elasticsearch.cluster.routing.RoutingTable;
@@ -30,14 +31,25 @@ import org.elasticsearch.cluster.routing.allocation.RoutingAllocation;
 import org.elasticsearch.cluster.routing.allocation.decider.AllocationDecider;
 import org.elasticsearch.cluster.routing.allocation.decider.AllocationDeciders;
 import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.discovery.DiscoveryModule;
+import org.elasticsearch.discovery.DiscoverySettings;
 import org.elasticsearch.gateway.GatewayAllocator;
 import org.elasticsearch.test.ElasticsearchIntegrationTest;
+import org.elasticsearch.test.disruption.BlockClusterStateProcessing;
+import org.elasticsearch.test.junit.annotations.TestLogging;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertHitCount;
 
 /**
  */
-@ElasticsearchIntegrationTest.ClusterScope(scope = ElasticsearchIntegrationTest.Scope.SUITE, numDataNodes = 1, numClientNodes = 0, transportClientRatio = 0)
+@ElasticsearchIntegrationTest.ClusterScope(scope = ElasticsearchIntegrationTest.Scope.TEST, numDataNodes = 0, numClientNodes = 0, transportClientRatio = 0)
 public class RareClusterStateTests extends ElasticsearchIntegrationTest {
 
     @Override
@@ -52,6 +64,7 @@ public class RareClusterStateTests extends ElasticsearchIntegrationTest {
 
     @Test
     public void testUnassignedShardAndEmptyNodesInRoutingTable() throws Exception {
+        internalCluster().startNode();
         createIndex("a");
         ensureSearchable("a");
         ClusterState current = clusterService().state();
@@ -66,8 +79,40 @@ public class RareClusterStateTests extends ElasticsearchIntegrationTest {
         );
         ClusterInfo clusterInfo = new ClusterInfo(ImmutableMap.<String, DiskUsage>of(), ImmutableMap.<String, Long>of());
 
-        RoutingAllocation routingAllocation =  new RoutingAllocation(allocationDeciders, routingNodes, current.nodes(), clusterInfo);
+        RoutingAllocation routingAllocation = new RoutingAllocation(allocationDeciders, routingNodes, current.nodes(), clusterInfo);
         allocator.allocateUnassigned(routingAllocation);
+    }
+
+    @Test
+    @TestLogging(value = "cluster.service:TRACE")
+    public void testDeleteCreateInOneBulk() throws Exception {
+        internalCluster().startNodesAsync(2, ImmutableSettings.builder()
+                .put(DiscoveryModule.DISCOVERY_TYPE_KEY, "zen")
+                .build()).get();
+        assertFalse(client().admin().cluster().prepareHealth().setWaitForNodes("2").get().isTimedOut());
+        prepareCreate("test").setSettings(IndexMetaData.SETTING_AUTO_EXPAND_REPLICAS, true).addMapping("type").get();
+        ensureGreen("test");
+
+        // now that the cluster is stable, remove publishing timeout
+        assertAcked(client().admin().cluster().prepareUpdateSettings().setTransientSettings(ImmutableSettings.builder().put(DiscoverySettings.PUBLISH_TIMEOUT, "0")));
+
+        Set<String> nodes = new HashSet<>(Arrays.asList(internalCluster().getNodeNames()));
+        nodes.remove(internalCluster().getMasterName());
+
+        // block none master node.
+        BlockClusterStateProcessing disruption = new BlockClusterStateProcessing(nodes.iterator().next(), getRandom());
+        internalCluster().setDisruptionScheme(disruption);
+        logger.info("--> indexing a doc");
+        index("test", "type", "1");
+        refresh();
+        disruption.startDisrupting();
+        logger.info("--> delete index and recreate it");
+        assertFalse(client().admin().indices().prepareDelete("test").setTimeout("200ms").get().isAcknowledged());
+        assertFalse(prepareCreate("test").setTimeout("200ms").setSettings(IndexMetaData.SETTING_AUTO_EXPAND_REPLICAS, true).get().isAcknowledged());
+        logger.info("--> letting cluster proceed");
+        disruption.stopDisrupting();
+        ensureGreen(TimeValue.timeValueMinutes(30), "test");
+        assertHitCount(client().prepareSearch("test").get(), 0);
     }
 
 }

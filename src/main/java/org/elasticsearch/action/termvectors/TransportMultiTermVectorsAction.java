455a85dc3b9a3adf3f9562405fe6778a0701565b
Alex Ksikes <alex.ksikes@gmail.com>
2015-02-05 16:36:08 +0100

    Term Vectors: requests are now timed
    When asking for term statistics, generating term vectors on the fly or with
    `dfs` set to `true`, some requests may take a while, so it is useful to know
    exactly how long.
    
    Closes #9583

--- src/main/java/org/elasticsearch/action/termvectors/TransportMultiTermVectorsAction.java
+++ src/main/java/org/elasticsearch/action/termvectors/TransportMultiTermVectorsAction.java
@@ -62,6 +62,7 @@ public class TransportMultiTermVectorsAction extends HandledTransportAction<Mult
         Map<ShardId, MultiTermVectorsShardRequest> shardRequests = new HashMap<>();
         for (int i = 0; i < request.requests.size(); i++) {
             TermVectorsRequest termVectorsRequest = request.requests.get(i);
+            termVectorsRequest.startTime = System.currentTimeMillis();
             termVectorsRequest.routing(clusterState.metaData().resolveIndexRouting(termVectorsRequest.routing(), termVectorsRequest.index()));
             if (!clusterState.metaData().hasConcreteIndex(termVectorsRequest.index())) {
                 responses.set(i, new MultiTermVectorsItemResponse(null, new MultiTermVectorsResponse.Failure(termVectorsRequest.index(),
@@ -80,7 +81,6 @@ public class TransportMultiTermVectorsAction extends HandledTransportAction<Mult
             if (shardRequest == null) {
                 shardRequest = new MultiTermVectorsShardRequest(request, shardId.index().name(), shardId.id());
                 shardRequest.preference(request.preference);
-
                 shardRequests.put(shardId, shardRequest);
             }
             shardRequest.add(i, termVectorsRequest);

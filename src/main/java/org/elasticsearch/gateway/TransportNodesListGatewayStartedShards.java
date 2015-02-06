e684d7fde432a713332cab1e20a5c2aacd96b7be
Boaz Leskes <b.leskes@gmail.com>
2015-02-06 21:44:57 +0100

    Logging: improve logging messages added in #9562 & #9562
    Closes #9603

--- src/main/java/org/elasticsearch/gateway/TransportNodesListGatewayStartedShards.java
+++ src/main/java/org/elasticsearch/gateway/TransportNodesListGatewayStartedShards.java
@@ -117,13 +117,13 @@ public class TransportNodesListGatewayStartedShards extends TransportNodesOperat
     @Override
     protected NodeGatewayStartedShards nodeOperation(NodeRequest request) throws ElasticsearchException {
         try {
-            logger.trace("loading shard state info for {}", request.shardId);
+            logger.trace("{} loading local shard state info", request.shardId);
             ShardStateInfo shardStateInfo = shardsState.loadShardInfo(request.shardId);
             if (shardStateInfo != null) {
                 logger.debug("{} shard state info found: [{}]", request.shardId, shardStateInfo);
                 return new NodeGatewayStartedShards(clusterService.localNode(), shardStateInfo.version);
             }
-            logger.trace("no shard info found for {}", request.shardId);
+            logger.trace("{} no local shard info found", request.shardId);
             return new NodeGatewayStartedShards(clusterService.localNode(), -1);
         } catch (Exception e) {
             throw new ElasticsearchException("failed to load started shards", e);

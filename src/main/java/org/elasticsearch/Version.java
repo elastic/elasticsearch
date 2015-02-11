475733ee0837fba18c00c3ee76cd49a08755550c
Martijn van Groningen <martijn.v.groningen@gmail.com>
2015-02-11 14:44:18 +0000

    release [1.3.8]

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -207,7 +207,7 @@ public class Version implements Serializable {
     public static final int V_1_3_7_ID = /*00*/1030799;
     public static final Version V_1_3_7 = new Version(V_1_3_7_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
     public static final int V_1_3_8_ID = /*00*/1030899;
-    public static final Version V_1_3_8 = new Version(V_1_3_8_ID, true, org.apache.lucene.util.Version.LUCENE_4_9);
+    public static final Version V_1_3_8 = new Version(V_1_3_8_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
 
     public static final Version CURRENT = V_1_3_8;
 

9f4c56a7b81046e126c7b40f91b772009b26e937
Adrien Grand <jpountz@gmail.com>
2015-02-12 14:52:40 +0100

    Internal: Remove XCollector.
    We don't actually need this interface, we can just run the post-collection
    operation once collection is finished on impls that need it.
    
    Close #9677

--- src/main/java/org/elasticsearch/common/lucene/search/FilteredCollector.java
+++ src/main/java/org/elasticsearch/common/lucene/search/FilteredCollector.java
@@ -28,7 +28,7 @@ import java.io.IOException;
 /**
  *
  */
-public class FilteredCollector implements XCollector {
+public class FilteredCollector implements Collector {
 
     private final Collector collector;
     private final Filter filter;
@@ -39,58 +39,14 @@ public class FilteredCollector implements XCollector {
     }
 
     @Override
-    public void postCollection() throws IOException {
-        if (collector instanceof XCollector) {
-            ((XCollector) collector).postCollection();
-        }
-    }
-
-    @Override
     public LeafCollector getLeafCollector(LeafReaderContext context) throws IOException {
         final DocIdSet set = filter.getDocIdSet(context, null);
         final LeafCollector in = collector.getLeafCollector(context);
-        final Bits bits = set == null ? null : set.bits();
-
-        if (bits != null) {
-            // the filter supports random-access
-            return new FilterLeafCollector(in) {
-                public void collect(int doc) throws IOException {
-                    if (bits.get(doc)) {
-                        in.collect(doc);
-                    }
-                }
-            };
-        }
-
-        // No random-access support, use the iterator and force in-order scoring
-        final DocIdSetIterator iterator;
-        if (DocIdSets.isEmpty(set)) {
-            iterator = null;
-        } else {
-            // DIS.iterator might still return null here
-            iterator = set.iterator();
-        }
-
-        if (iterator == null) {
-            return new FilterLeafCollector(in) {
-                @Override
-                public void collect(int doc) throws IOException {
-                    // no-op
-                }
-            };
-        }
+        final Bits bits = DocIdSets.asSequentialAccessBits(context.reader().maxDoc(), set);
 
         return new FilterLeafCollector(in) {
-            @Override
             public void collect(int doc) throws IOException {
-                final int itDoc = iterator.docID();
-                if (itDoc > doc) {
-                    return;
-                } else if (itDoc < doc) {
-                    if (iterator.advance(doc) == doc) {
-                        in.collect(doc);
-                    }
-                } else {
+                if (bits.get(doc)) {
                     in.collect(doc);
                 }
             }

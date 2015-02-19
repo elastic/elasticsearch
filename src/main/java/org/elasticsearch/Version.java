1b54c22e9423d6722c13aaef6be70429dea71f68
Alexander Reelsen <alexander@reelsen.net>
2015-02-19 14:55:43 +0100

    Versioning: Adding 1.3.9 and 1.4.5 versions

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -209,8 +209,10 @@ public class Version {
     public static final Version V_1_3_7 = new Version(V_1_3_7_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
     public static final int V_1_3_8_ID = 1030899;
     public static final Version V_1_3_8 = new Version(V_1_3_8_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
-    public static final int V_1_3_9_ID = /*00*/1030999;
-    public static final Version V_1_3_9 = new Version(V_1_3_9_ID, true, org.apache.lucene.util.Version.LUCENE_4_9);
+    public static final int V_1_3_9_ID = 1030999;
+    public static final Version V_1_3_9 = new Version(V_1_3_9_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
+    public static final int V_1_3_10_ID = /*00*/1031099;
+    public static final Version V_1_3_10 = new Version(V_1_3_10_ID, true, org.apache.lucene.util.Version.LUCENE_4_9);
     public static final int V_1_4_0_Beta1_ID = 1040001;
     public static final Version V_1_4_0_Beta1 = new Version(V_1_4_0_Beta1_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_1);
     public static final int V_1_4_0_ID = 1040099;
@@ -222,7 +224,9 @@ public class Version {
     public static final int V_1_4_3_ID = 1040399;
     public static final Version V_1_4_3 = new Version(V_1_4_3_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_3);
     public static final int V_1_4_4_ID = 1040499;
-    public static final Version V_1_4_4 = new Version(V_1_4_4_ID, true, org.apache.lucene.util.Version.LUCENE_4_10_3);
+    public static final Version V_1_4_4 = new Version(V_1_4_4_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_3);
+    public static final int V_1_4_5_ID = 1040599;
+    public static final Version V_1_4_5 = new Version(V_1_4_5_ID, true, org.apache.lucene.util.Version.LUCENE_4_10_3);
     public static final int V_1_5_0_ID = 1050099;
     public static final Version V_1_5_0 = new Version(V_1_5_0_ID, true, org.apache.lucene.util.Version.LUCENE_4_10_3);
     public static final int V_2_0_0_ID = 2000099;
@@ -244,6 +248,8 @@ public class Version {
                 return V_2_0_0;
             case V_1_5_0_ID:
                 return V_1_5_0;
+            case V_1_4_5_ID:
+                return V_1_4_5;
             case V_1_4_4_ID:
                 return V_1_4_4;
             case V_1_4_3_ID:
@@ -256,6 +262,8 @@ public class Version {
                 return V_1_4_0;
             case V_1_4_0_Beta1_ID:
                 return V_1_4_0_Beta1;
+            case V_1_3_10_ID:
+                return V_1_3_10;
             case V_1_3_9_ID:
                 return V_1_3_9;
             case V_1_3_8_ID:

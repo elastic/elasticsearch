0915c7306e6264ba21a6cb7609b93545ccc32ef1
Alexander Reelsen <alexander@reelsen.net>
2015-02-19 12:33:25 +0000

    release [1.3.9]

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -209,7 +209,7 @@ public class Version implements Serializable {
     public static final int V_1_3_8_ID = /*00*/1030899;
     public static final Version V_1_3_8 = new Version(V_1_3_8_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
     public static final int V_1_3_9_ID = /*00*/1030999;
-    public static final Version V_1_3_9 = new Version(V_1_3_9_ID, true, org.apache.lucene.util.Version.LUCENE_4_9);
+    public static final Version V_1_3_9 = new Version(V_1_3_9_ID, false, org.apache.lucene.util.Version.LUCENE_4_9);
 
     public static final Version CURRENT = V_1_3_9;
 

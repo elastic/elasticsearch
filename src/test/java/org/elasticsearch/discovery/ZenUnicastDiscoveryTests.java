efbda318d061867c098be5112561f8099227b9d4
jaymode <jay.modi@elasticsearch.com>
2015-02-25 13:26:21 -0500

    Tests: check node ports availability when using unicast discovery
    Some tests failures are seen when a node attempts to use a port that is already bound
    by some other process on the test machine. This commit adds a bind to test port availability
    and iterates over the port range until an available port is found. This reduces the likelihood
    of a test node failing to start up due to the port already being bound.

--- src/test/java/org/elasticsearch/discovery/ZenUnicastDiscoveryTests.java
+++ src/test/java/org/elasticsearch/discovery/ZenUnicastDiscoveryTests.java
@@ -59,7 +59,7 @@ public class ZenUnicastDiscoveryTests extends ElasticsearchIntegrationTest {
         for (int i = 0; i < unicastHostOrdinals.length; i++) {
             unicastHostOrdinals[i] = i;
         }
-        discoveryConfig = new ClusterDiscoveryConfiguration.UnicastZen(currentNumNodes, unicastHostOrdinals, Scope.TEST);
+        discoveryConfig = new ClusterDiscoveryConfiguration.UnicastZen(currentNumNodes, unicastHostOrdinals);
 
         // start the unicast hosts
         internalCluster().startNodesAsync(unicastHostOrdinals.length).get();
@@ -81,7 +81,7 @@ public class ZenUnicastDiscoveryTests extends ElasticsearchIntegrationTest {
         int currentNumNodes = randomIntBetween(3, 5);
         int currentNumOfUnicastHosts = randomIntBetween(1, currentNumNodes);
         final Settings settings = ImmutableSettings.settingsBuilder().put("discovery.zen.minimum_master_nodes", currentNumNodes / 2 + 1).build();
-        discoveryConfig = new ClusterDiscoveryConfiguration.UnicastZen(currentNumNodes, currentNumOfUnicastHosts, settings, Scope.TEST);
+        discoveryConfig = new ClusterDiscoveryConfiguration.UnicastZen(currentNumNodes, currentNumOfUnicastHosts, settings);
 
         List<String> nodes = internalCluster().startNodesAsync(currentNumNodes).get();
 

5cd109cab9639869229879e625a9c19e1653f908
Ryan Ernst <ryan@iernst.net>
2015-02-27 22:05:42 -0800

    Test: ShadowEngineTests - close translogs on tear down here too

--- src/test/java/org/elasticsearch/index/engine/ShadowEngineTests.java
+++ src/test/java/org/elasticsearch/index/engine/ShadowEngineTests.java
@@ -154,6 +154,9 @@ public class ShadowEngineTests extends ElasticsearchLuceneTestCase {
         super.tearDown();
         replicaEngine.close();
         storeReplica.close();
+        
+        translog.close();
+        replicaTranslog.close();
 
         primaryEngine.close();
         store.close();

a7175198db7223614f99204a637553bb3a8a280d
mikemccand <mike@elasticsearch.com>
2015-03-13 10:35:58 -0700

    don't re-throw exception on merge

--- src/main/java/org/elasticsearch/index/merge/scheduler/ConcurrentMergeSchedulerProvider.java
+++ src/main/java/org/elasticsearch/index/merge/scheduler/ConcurrentMergeSchedulerProvider.java
@@ -141,7 +141,9 @@ public class ConcurrentMergeSchedulerProvider extends MergeSchedulerProvider {
         protected void handleMergeException(Directory dir, Throwable exc) {
             logger.warn("failed to merge", exc);
             provider.failedMerge(new MergePolicy.MergeException(exc, dir));
-            super.handleMergeException(dir, exc);
+            // NOTE: do not call super.handleMergeException here, which would just re-throw the exception
+            // and let Java's thread exc handler see it / log it to stderr, but we already 1) logged it
+            // and 2) handled the exception by failing the engine
         }
 
         @Override

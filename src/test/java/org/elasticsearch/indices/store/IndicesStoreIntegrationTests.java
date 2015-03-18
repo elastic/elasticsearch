e35608ca2a164ce463ccd9c782728e4cb1a508c7
Britta Weber <britta.weber@elasticsearch.com>
2015-03-17 17:01:59 -0700

    [TEST] increase test logging

--- src/test/java/org/elasticsearch/indices/store/IndicesStoreIntegrationTests.java
+++ src/test/java/org/elasticsearch/indices/store/IndicesStoreIntegrationTests.java
@@ -38,6 +38,7 @@ import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.test.ElasticsearchIntegrationTest;
 import org.elasticsearch.test.ElasticsearchIntegrationTest.ClusterScope;
 import org.elasticsearch.test.InternalTestCluster;
+import org.elasticsearch.test.junit.annotations.TestLogging;
 import org.junit.Test;
 
 import java.io.File;
@@ -60,6 +61,7 @@ public class IndicesStoreIntegrationTests extends ElasticsearchIntegrationTest {
     private static final Settings SETTINGS = settingsBuilder().put("gateway.type", "local").build();
 
     @Test
+    @TestLogging("indices:TRACE")
     public void indexCleanup() throws Exception {
         final String masterNode = internalCluster().startNode(ImmutableSettings.builder().put(SETTINGS).put("node.data", false));
         final String node_1 = internalCluster().startNode(ImmutableSettings.builder().put(SETTINGS).put("node.master", false));

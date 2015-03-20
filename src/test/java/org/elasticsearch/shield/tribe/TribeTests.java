eedb677dc2b7d88d5c4190674babb24c1a53f993
jaymode <jay.modi@elasticsearch.com>
2015-03-20 07:01:57 -0400

    fix compile error due to new signature of InternalTestCluster
    Original commit: elastic/x-pack-elasticsearch@bc8a9099dc6e9bc6d128021be8647bc1cad12b10

--- src/test/java/org/elasticsearch/shield/tribe/TribeTests.java
+++ src/test/java/org/elasticsearch/shield/tribe/TribeTests.java
@@ -65,7 +65,7 @@ public class TribeTests extends ShieldIntegrationTest {
             String cluster2Name = clusterName(Scope.SUITE.name(), Integer.toString(CHILD_JVM_ID), randomLong());
             //no port conflicts as this test uses the global cluster and a suite cluster that gets manually created
             ShieldSettingsSource cluster2SettingsSource = new ShieldSettingsSource(2, sslTransportEnabled, systemKey, newTempDir(LifecycleScope.SUITE), Scope.SUITE);
-            cluster2 = new InternalTestCluster(randomLong(), 2, 2, cluster2Name, cluster2SettingsSource, 0, false, false, CHILD_JVM_ID, SECOND_CLUSTER_NODE_PREFIX);
+            cluster2 = new InternalTestCluster(randomLong(), 2, 2, cluster2Name, cluster2SettingsSource, 0, false, CHILD_JVM_ID, SECOND_CLUSTER_NODE_PREFIX);
 
             assert tribeSettingsSource == null;
             //given the low (2 and 1) number of nodes that the 2 SUITE clusters will have, we are not going to have port conflicts
@@ -143,7 +143,7 @@ public class TribeTests extends ShieldIntegrationTest {
 
         //we need to recreate the tribe node after each test otherwise ensureClusterSizeConsistency barfs
         String tribeClusterName = clusterName(Scope.SUITE.name(), Integer.toString(CHILD_JVM_ID), randomLong());
-        tribeNodeCluster = new InternalTestCluster(randomLong(), 1, 1, tribeClusterName, tribeSettingsSource, 0, false, false, CHILD_JVM_ID, TRIBE_CLUSTER_NODE_PREFIX);
+        tribeNodeCluster = new InternalTestCluster(randomLong(), 1, 1, tribeClusterName, tribeSettingsSource, 0, false, CHILD_JVM_ID, TRIBE_CLUSTER_NODE_PREFIX);
         tribeNodeCluster.beforeTest(getRandom(), 0.5);
         awaitSameNodeCounts();
     }

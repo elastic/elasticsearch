1fe2ad72234670083aec164436b97212645343a2
Robert Muir <rmuir@apache.org>
2015-03-21 02:47:44 -0400

    [Bootstrap] Throw exception if the JVM will corrupt data.
    Detect the worst-offenders, all IBM versions and several known hotspot
    versions that can cause index corruption, and fail on startup.
    
    Provide/detect compiler workarounds when they exist, but warn about
    performance degradation.
    
    In all cases the check can be bypassed completely with a safety
    switch via undocumented system property (es.bypass.vm.check=true)
    
    Closes #7580

--- src/main/java/org/elasticsearch/bootstrap/Bootstrap.java
+++ src/main/java/org/elasticsearch/bootstrap/Bootstrap.java
@@ -200,6 +200,10 @@ public class Bootstrap {
                 Loggers.disableConsoleLogging();
                 System.out.close();
             }
+
+            // fail if using broken version
+            JVMCheck.check();
+
             bootstrap.setup(true, tuple);
 
             stage = "Startup";

2d77bdbeb6ae3cea70ebb35e6e57b92642dc96af
Martijn van Groningen <martijn.v.groningen@gmail.com>
2015-03-23 15:00:54 +0100

    undo elastic elasticsearch

--- dev-tools/build_release.py
+++ dev-tools/build_release.py
@@ -61,7 +61,7 @@ Once it's done it will print all the remaining steps.
 """
 env = os.environ
 
-PLUGINS = [('license', 'elastic/license/latest'),
+PLUGINS = [('license', 'elasticsearch/license/latest'),
            ('bigdesk', 'lukas-vlcek/bigdesk'),
            ('paramedic', 'karmi/elasticsearch-paramedic'),
            ('segmentspy', 'polyfractal/elasticsearch-segmentspy'),

5dedb8fc90e675b231effbbbf1348b9f1ab67a15
Martijn van Groningen <martijn.v.groningen@gmail.com>
2015-03-23 16:18:28 +0100

    Set next development version

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -226,8 +226,10 @@ public class Version {
     public static final Version V_1_4_5 = new Version(V_1_4_5_ID, true, org.apache.lucene.util.Version.LUCENE_4_10_4);
     public static final int V_1_5_0_ID = 1050099;
     public static final Version V_1_5_0 = new Version(V_1_5_0_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_4);
+    public static final int V_1_5_1_ID = 1050199;
+    public static final Version V_1_5_1 = new Version(V_1_5_1_ID, true, org.apache.lucene.util.Version.LUCENE_4_10_4);
 
-    public static final Version CURRENT = V_1_5_0;
+    public static final Version CURRENT = V_1_5_1;
 
     static {
         assert CURRENT.luceneVersion.equals(Lucene.VERSION) : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
@@ -239,6 +241,8 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_1_5_1_ID:
+                return V_1_5_1;
             case V_1_5_0_ID:
                 return V_1_5_0;
             case V_1_4_5_ID:

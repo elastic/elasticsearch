8df409651a150a0aaf42cd5e921f4f92426c55c5
Ryan Ernst <ryan@iernst.net>
2015-03-23 08:04:05 -0700

    Tests: Add static bwc index for 1.5.0

--- dev-tools/create-bwc-index.py
+++ dev-tools/create-bwc-index.py
@@ -151,7 +151,7 @@ def generate_index(client):
     }
     # completion type was added in 0.90.3
     if not version.startswith('0.20') and not version in ['0.90.0.Beta1', '0.90.0.RC1', '0.90.0.RC2', '0.90.0', '0.90.1', '0.90.2']:
-      mappings['analyzer_1']['properties']['completion_with_index_analyzer'] = {
+      mappings['analyzer_type1']['properties']['completion_with_index_analyzer'] = {
         'type': 'completion',
         'index_analyzer': 'standard'
       }

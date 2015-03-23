31f3afe6844078c7abc253dd7ad879e02a971f21
jaymode <jay.modi@elasticsearch.com>
2015-03-23 16:26:41 -0400

    use the BytesStreamInput with BytesArray
    This resolves a compatibility issue with the current builds of elasticsearch
    1.6.0.
    
    Closes elastic/elasticsearch#774
    
    Original commit: elastic/x-pack-elasticsearch@0ee97afa53c17e5f15f2d6fb7e3bb527bf71f854

--- src/main/java/org/elasticsearch/shield/authc/InternalAuthenticationService.java
+++ src/main/java/org/elasticsearch/shield/authc/InternalAuthenticationService.java
@@ -7,6 +7,7 @@ package org.elasticsearch.shield.authc;
 
 import org.apache.commons.codec.binary.Base64;
 import org.elasticsearch.common.Nullable;
+import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.component.AbstractComponent;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.io.stream.BytesStreamInput;
@@ -116,7 +117,7 @@ public class InternalAuthenticationService extends AbstractComponent implements 
     static User decodeUser(String text) {
         byte[] bytes = Base64.decodeBase64(text);
         try {
-            BytesStreamInput input = new BytesStreamInput(bytes, true);
+            BytesStreamInput input = new BytesStreamInput(new BytesArray(bytes));
             return User.readFrom(input);
         } catch (IOException ioe) {
             throw new AuthenticationException("could not read authenticated user", ioe);

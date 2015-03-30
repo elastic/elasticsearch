a7ced907ab2779c9d06dd7c6afdb116d3a961451
Michael McCandless <mail@mikemccandless.com>
2015-03-30 10:25:45 -0400

    Core: deletions (alone) should also force a refresh when version map is using too much RAM
    We already force a refresh in index/create ops, to clear version map
    when it's using too much RAM, but we were failing to do this for
    deletes, so an app that does tons of deletes with no indexing, and has
    set refresh_interval to -1, would have version map using unbounded
    RAM.
    
    Closes #10312

--- src/main/java/org/elasticsearch/index/engine/InternalEngine.java
+++ src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -461,6 +461,7 @@ public class InternalEngine extends Engine {
         }
 
         maybePruneDeletedTombstones();
+        checkVersionMapRefresh();
     }
 
     private void maybePruneDeletedTombstones() {

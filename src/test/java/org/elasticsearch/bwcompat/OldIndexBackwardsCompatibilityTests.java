9d0764e5df67f570a6696d10d227a9db44bc5baf
Ryan Ernst <ryan@iernst.net>
2015-04-09 13:31:08 -0700

    Tests: increasing timeout for replicas in static bwc tests, to account for reallySlowJenkinsWhenIndexHasLotsOfSegments

--- src/test/java/org/elasticsearch/bwcompat/OldIndexBackwardsCompatibilityTests.java
+++ src/test/java/org/elasticsearch/bwcompat/OldIndexBackwardsCompatibilityTests.java
@@ -305,7 +305,7 @@ public class OldIndexBackwardsCompatibilityTests extends ElasticsearchIntegratio
         assertAcked(client().admin().indices().prepareUpdateSettings(indexName).setSettings(ImmutableSettings.builder()
                         .put("number_of_replicas", numReplicas)
         ).execute().actionGet());
-        ensureGreen(TimeValue.timeValueMinutes(1), indexName);
+        ensureGreen(TimeValue.timeValueMinutes(2), indexName);
         logger.debug("--> index [{}] is green, took [{}]", indexName, TimeValue.timeValueMillis(System.currentTimeMillis() - startTime));
         logger.debug("--> recovery status:\n{}", XContentHelper.toString(client().admin().indices().prepareRecoveries(indexName).get()));
 

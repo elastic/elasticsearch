b81e8ad2cee13bf3604ba5f7a38b22bca146f63a
javanna <cavannaluca@gmail.com>
2015-04-09 16:04:50 +0200

    Prepare next development iteration

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -228,8 +228,10 @@ public class Version {
     public static final Version V_1_5_0 = new Version(V_1_5_0_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_4);
     public static final int V_1_5_1_ID = 1050199;
     public static final Version V_1_5_1 = new Version(V_1_5_1_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_4);
+    public static final int V_1_5_2_ID = 1050299;
+    public static final Version V_1_5_2 = new Version(V_1_5_2_ID, true, org.apache.lucene.util.Version.LUCENE_4_10_4);
 
-    public static final Version CURRENT = V_1_5_1;
+    public static final Version CURRENT = V_1_5_2;
 
     static {
         assert CURRENT.luceneVersion.equals(Lucene.VERSION) : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
@@ -241,6 +243,8 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_1_5_2_ID:
+                return V_1_5_2;
             case V_1_5_1_ID:
                 return V_1_5_1;
             case V_1_5_0_ID:

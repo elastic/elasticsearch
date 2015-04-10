27ce47b503ddda3809811a184788e043b22690b9
Lee Hinman <lee@writequit.org>
2015-04-10 16:38:45 -0600

    [CORE] Log sending translog operation batches to nodes

--- src/main/java/org/elasticsearch/indices/recovery/RecoverySourceHandler.java
+++ src/main/java/org/elasticsearch/indices/recovery/RecoverySourceHandler.java
@@ -610,6 +610,10 @@ public class RecoverySourceHandler implements Engine.RecoveryHandler {
                 .withType(TransportRequestOptions.Type.RECOVERY)
                 .withTimeout(recoverySettings.internalActionLongTimeout());
 
+        if (operation == null) {
+            logger.trace("[{}][{}] no translog operations to send to {}",
+                    indexName, shardId, request.targetNode());
+        }
         while (operation != null) {
             if (shard.state() == IndexShardState.CLOSED) {
                 throw new IndexShardClosedException(request.shardId());
@@ -642,6 +646,12 @@ public class RecoverySourceHandler implements Engine.RecoveryHandler {
                                 recoveryOptions, EmptyTransportResponseHandler.INSTANCE_SAME).txGet();
                     }
                 });
+                if (logger.isTraceEnabled()) {
+                    logger.trace("[{}][{}] sent batch of [{}][{}] (total: [{}]) translog operations to {}",
+                            indexName, shardId, ops, new ByteSizeValue(size),
+                            shard.translog().estimatedNumberOfOperations(),
+                            request.targetNode());
+                }
 
                 ops = 0;
                 size = 0;
@@ -664,6 +674,12 @@ public class RecoverySourceHandler implements Engine.RecoveryHandler {
                 }
             });
 
+        }
+        if (logger.isTraceEnabled()) {
+            logger.trace("[{}][{}] sent final batch of [{}][{}] (total: [{}]) translog operations to {}",
+                    indexName, shardId, ops, new ByteSizeValue(size),
+                    shard.translog().estimatedNumberOfOperations(),
+                    request.targetNode());
         }
         return totalOperations;
     }

8bd131eac2bc2d6c7050a96e91ccf2e73e5e5938
David Pilato <david@pilato.fr>
2015-04-29 12:00:55 +0200

    [core] Simplify IndexStore and friends
    Related to https://github.com/elastic/elasticsearch/pull/10773
    
    Closes #81.

--- src/main/java/org/elasticsearch/index/store/smbmmapfs/SmbMmapFsIndexStore.java
+++ src/main/java/org/elasticsearch/index/store/smbmmapfs/SmbMmapFsIndexStore.java
@@ -21,19 +21,19 @@ package org.elasticsearch.index.store.smbmmapfs;
 
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.env.NodeEnvironment;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.index.IndexService;
 import org.elasticsearch.index.settings.IndexSettings;
+import org.elasticsearch.index.settings.IndexSettingsService;
 import org.elasticsearch.index.store.DirectoryService;
-import org.elasticsearch.index.store.support.AbstractIndexStore;
+import org.elasticsearch.index.store.IndexStore;
 import org.elasticsearch.indices.store.IndicesStore;
 
-public class SmbMmapFsIndexStore extends AbstractIndexStore {
+public class SmbMmapFsIndexStore extends IndexStore {
 
     @Inject
-    public SmbMmapFsIndexStore(Index index, @IndexSettings Settings indexSettings, IndexService indexService, IndicesStore indicesStore, NodeEnvironment nodeEnv) {
-        super(index, indexSettings, indexService, indicesStore, nodeEnv);
+    public SmbMmapFsIndexStore(Index index, @IndexSettings Settings indexSettings,
+                               IndexSettingsService indexSettingsService, IndicesStore indicesStore) {
+        super(index, indexSettings, indexSettingsService, indicesStore);
     }
 
     @Override

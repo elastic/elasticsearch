dc2eed9681d6d5a3eec43f4b3d777e73effe8efd
Britta Weber <britta.weber@elasticsearch.com>
2015-05-05 17:45:41 +0200

    fix after merge with master

--- src/main/java/org/elasticsearch/indices/recovery/RecoverySourceHandler.java
+++ src/main/java/org/elasticsearch/indices/recovery/RecoverySourceHandler.java
@@ -417,7 +417,7 @@ public class RecoverySourceHandler {
                     try {
                         final Store.MetadataSnapshot remainingFilesAfterCleanup = recoverWithSyncId? request.metadataSnapshot(): recoverySourceMetadata;
                         transportService.submitRequest(request.targetNode(), RecoveryTarget.Actions.CLEAN_FILES,
-                                new RecoveryCleanFilesRequest(request.recoveryId(), shard.shardId(), recoverySourceMetadata, translogView.totalOperations()),
+                                new RecoveryCleanFilesRequest(request.recoveryId(), shard.shardId(), remainingFilesAfterCleanup, translogView.totalOperations()),
                                 TransportRequestOptions.options().withTimeout(recoverySettings.internalActionTimeout()),
                                 EmptyTransportResponseHandler.INSTANCE_SAME).txGet();
                     } catch (RemoteTransportException remoteException) {

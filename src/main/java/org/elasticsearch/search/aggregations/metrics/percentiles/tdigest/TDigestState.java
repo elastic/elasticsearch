a2dcf107092a450d0f70d561085aa31a55bac2b2
Emmanuel Bourg <ebourg@apache.org>
2015-05-11 10:35:55 +0200

    Upgrade the dependency on t-digest (3.0 -> 3.1)

--- src/main/java/org/elasticsearch/search/aggregations/metrics/percentiles/tdigest/TDigestState.java
+++ src/main/java/org/elasticsearch/search/aggregations/metrics/percentiles/tdigest/TDigestState.java
@@ -43,7 +43,7 @@ public class TDigestState extends AVLTreeDigest {
 
     public static void write(TDigestState state, StreamOutput out) throws IOException {
         out.writeDouble(state.compression);
-        out.writeVInt(state.centroidCount());
+        out.writeVInt(state.centroids().size());
         for (Centroid centroid : state.centroids()) {
             out.writeDouble(centroid.mean());
             out.writeVLong(centroid.count());

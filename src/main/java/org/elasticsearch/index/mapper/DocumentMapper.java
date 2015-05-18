85977c411cdb52f02fe73c84fde6f3cac0d4e6ac
Adrien Grand <jpountz@gmail.com>
2015-05-18 16:05:26 +0200

    Mappings: Make mapping updates atomic wrt document parsing.
    When mapping updates happen concurrently with document parsing, bad things can
    happen. For instance, when applying a mapping update we first update the Mapping
    object which is used for parsing and then FieldNameAnalyzer which is used by
    IndexWriter for analysis. So if you are unlucky, it could happen that a document
    was parsed successfully without introducing dynamic updates yet IndexWriter does
    not see its analyzer yet.
    
    In order to fix this issue, mapping updates are now protected by a write lock
    and document parsing is protected by the read lock associated with this write
    lock. This ensures that no documents will be parsed while a mapping update is
    being applied, so document parsing will either see none of the update or all of
    it.

--- src/main/java/org/elasticsearch/index/mapper/DocumentMapper.java
+++ src/main/java/org/elasticsearch/index/mapper/DocumentMapper.java
@@ -23,6 +23,7 @@ import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
+
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.search.DocIdSet;
@@ -75,7 +76,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  *
@@ -162,7 +163,7 @@ public class DocumentMapper implements ToXContent {
 
         public DocumentMapper build(MapperService mapperService, DocumentMapperParser docMapperParser) {
             Preconditions.checkNotNull(rootObjectMapper, "Mapper builder must have the root object mapper set");
-            return new DocumentMapper(mapperService, index, indexSettings, docMapperParser, rootObjectMapper, meta, rootMappers, sourceTransforms);
+            return new DocumentMapper(mapperService, index, indexSettings, docMapperParser, rootObjectMapper, meta, rootMappers, sourceTransforms, mapperService.mappingLock);
         }
     }
 
@@ -185,12 +186,14 @@ public class DocumentMapper implements ToXContent {
 
     private final Query typeFilter;
 
-    private final Object mappersMutex = new Object();
+    private final ReentrantReadWriteLock mappingLock;
 
     public DocumentMapper(MapperService mapperService, String index, @Nullable Settings indexSettings, DocumentMapperParser docMapperParser,
                           RootObjectMapper rootObjectMapper,
                           ImmutableMap<String, Object> meta,
-                          Map<Class<? extends RootMapper>, RootMapper> rootMappers, List<SourceTransform> sourceTransforms) {
+                          Map<Class<? extends RootMapper>, RootMapper> rootMappers,
+                          List<SourceTransform> sourceTransforms,
+                          ReentrantReadWriteLock mappingLock) {
         this.mapperService = mapperService;
         this.type = rootObjectMapper.name();
         this.typeText = new StringAndBytesText(this.type);
@@ -200,9 +203,10 @@ public class DocumentMapper implements ToXContent {
                 rootMappers.values().toArray(new RootMapper[rootMappers.values().size()]),
                 sourceTransforms.toArray(new SourceTransform[sourceTransforms.size()]),
                 meta);
-        this.documentParser = new DocumentParser(index, indexSettings, docMapperParser, this);
+        this.documentParser = new DocumentParser(index, indexSettings, docMapperParser, this, mappingLock.readLock());
 
         this.typeFilter = typeMapper().termQuery(type, null);
+        this.mappingLock = mappingLock;
 
         if (rootMapper(ParentFieldMapper.class).active()) {
             // mark the routing field mapper as required
@@ -332,10 +336,6 @@ public class DocumentMapper implements ToXContent {
         return this.objectMappers;
     }
 
-    public ParsedDocument parse(BytesReference source) throws MapperParsingException {
-        return parse(SourceToParse.source(source));
-    }
-
     public ParsedDocument parse(String type, String id, BytesReference source) throws MapperParsingException {
         return parse(SourceToParse.source(source).type(type).id(id));
     }
@@ -412,24 +412,22 @@ public class DocumentMapper implements ToXContent {
         return DocumentParser.transformSourceAsMap(mapping, sourceAsMap);
     }
 
-    public void addFieldMappers(Collection<FieldMapper<?>> fieldMappers) {
-        synchronized (mappersMutex) {
-            this.fieldMappers = this.fieldMappers.copyAndAllAll(fieldMappers);
-        }
+    private void addFieldMappers(Collection<FieldMapper<?>> fieldMappers) {
+        assert mappingLock.isWriteLockedByCurrentThread();
+        this.fieldMappers = this.fieldMappers.copyAndAllAll(fieldMappers);
         mapperService.addFieldMappers(fieldMappers);
     }
 
     private void addObjectMappers(Collection<ObjectMapper> objectMappers) {
-        synchronized (mappersMutex) {
-            MapBuilder<String, ObjectMapper> builder = MapBuilder.newMapBuilder(this.objectMappers);
-            for (ObjectMapper objectMapper : objectMappers) {
-                builder.put(objectMapper.fullPath(), objectMapper);
-                if (objectMapper.nested().isNested()) {
-                    hasNestedObjects = true;
-                }
+        assert mappingLock.isWriteLockedByCurrentThread();
+        MapBuilder<String, ObjectMapper> builder = MapBuilder.newMapBuilder(this.objectMappers);
+        for (ObjectMapper objectMapper : objectMappers) {
+            builder.put(objectMapper.fullPath(), objectMapper);
+            if (objectMapper.nested().isNested()) {
+                hasNestedObjects = true;
             }
-            this.objectMappers = builder.immutableMap();
         }
+        this.objectMappers = builder.immutableMap();
         mapperService.addObjectMappers(objectMappers);
     }
 
@@ -480,15 +478,20 @@ public class DocumentMapper implements ToXContent {
         };
     }
 
-    public synchronized MergeResult merge(Mapping mapping, boolean simulate) {
-        final MergeResult mergeResult = newMergeContext(simulate);
-        this.mapping.merge(mapping, mergeResult);
-        if (simulate == false) {
-            addFieldMappers(mergeResult.getNewFieldMappers());
-            addObjectMappers(mergeResult.getNewObjectMappers());
-            refreshSource();
+    public MergeResult merge(Mapping mapping, boolean simulate) {
+        mappingLock.writeLock().lock();
+        try {
+            final MergeResult mergeResult = newMergeContext(simulate);
+            this.mapping.merge(mapping, mergeResult);
+            if (simulate == false) {
+                addFieldMappers(mergeResult.getNewFieldMappers());
+                addObjectMappers(mergeResult.getNewObjectMappers());
+                refreshSource();
+            }
+            return mergeResult;
+        } finally {
+            mappingLock.writeLock().unlock();
         }
-        return mergeResult;
     }
 
     public CompressedString refreshSource() throws ElasticsearchGenerationException {

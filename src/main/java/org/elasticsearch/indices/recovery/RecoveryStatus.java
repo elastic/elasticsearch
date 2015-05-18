8aa2f6271367ce6203b60ea8c135c2d7b05bbac7
Simon Willnauer <simonw@apache.org>
2015-05-18 14:19:05 +0200

    Decrement reference even if IndexShard#postRecovery barfs
    This can cause a reference leak if we call `IndexShard#postRecovery`
    on an already closed shard.

--- src/main/java/org/elasticsearch/indices/recovery/RecoveryStatus.java
+++ src/main/java/org/elasticsearch/indices/recovery/RecoveryStatus.java
@@ -188,9 +188,14 @@ public class RecoveryStatus extends AbstractRefCounted {
     public void markAsDone() {
         if (finished.compareAndSet(false, true)) {
             assert tempFileNames.isEmpty() : "not all temporary files are renamed";
-            indexShard.postRecovery("peer recovery done");
-            // release the initial reference. recovery files will be cleaned as soon as ref count goes to zero, potentially now
-            decRef();
+            try {
+                // this might still throw an exception ie. if the shard is CLOSED due to some other event.
+                // it's safer to decrement the reference in a try finally here.
+                indexShard.postRecovery("peer recovery done");
+            } finally {
+                // release the initial reference. recovery files will be cleaned as soon as ref count goes to zero, potentially now
+                decRef();
+            }
             listener.onRecoveryDone(state());
         }
     }

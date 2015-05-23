543f572d80b0494c4f620eb2e6ad3fb8d1fdd89b
javanna <cavannaluca@gmail.com>
2015-05-23 11:15:27 +0200

    Highlighting: keep track of the original query only in HighlighterContext
    We used to keep track of the rewritten query in the highlighter context to support custom rewriting done by our own postings highlighter fork. Now that we rely on lucene implementation, no rewrite happens, we can simply keep track of the original query and simplify code around it.
    
    Closes #11317

--- src/main/java/org/elasticsearch/search/highlight/HighlighterContext.java
+++ src/main/java/org/elasticsearch/search/highlight/HighlighterContext.java
@@ -18,7 +18,6 @@
  */
 package org.elasticsearch.search.highlight;
 
-import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.search.Query;
 import org.elasticsearch.index.mapper.FieldMapper;
 import org.elasticsearch.search.fetch.FetchSubPhase;
@@ -34,39 +33,15 @@ public class HighlighterContext {
     public final FieldMapper mapper;
     public final SearchContext context;
     public final FetchSubPhase.HitContext hitContext;
-    public final HighlightQuery query;
+    public final Query query;
 
     public HighlighterContext(String fieldName, SearchContextHighlight.Field field, FieldMapper mapper, SearchContext context,
-            FetchSubPhase.HitContext hitContext, HighlightQuery query) {
+            FetchSubPhase.HitContext hitContext, Query query) {
         this.fieldName = fieldName;
         this.field = field;
         this.mapper = mapper;
         this.context = context;
         this.hitContext = hitContext;
         this.query = query;
-    }
-
-    public static class HighlightQuery {
-        private final Query originalQuery;
-        private final Query query;
-        private final boolean queryRewritten;
-
-        protected HighlightQuery(Query originalQuery, Query query, boolean queryRewritten) {
-            this.originalQuery = originalQuery;
-            this.query = query;
-            this.queryRewritten = queryRewritten;
-        }
-
-        public boolean queryRewritten() {
-            return queryRewritten;
-        }
-
-        public Query originalQuery() {
-            return originalQuery;
-        }
-
-        public Query query() {
-            return query;
-        }
     }
 }

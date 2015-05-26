97e60161378ab98843e07d33005c8c143b41bbf9
Michael McCandless <mail@mikemccandless.com>
2015-05-26 08:05:53 -0400

    Absorb ImmutableSettings into Settings

--- src/test/java/org/elasticsearch/index/analysis/SimpleIcuAnalysisTests.java
+++ src/test/java/org/elasticsearch/index/analysis/SimpleIcuAnalysisTests.java
@@ -23,7 +23,7 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.test.ElasticsearchTestCase;
 import org.junit.Test;
 
-import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilder;
+import static org.elasticsearch.common.settings.Settings.settingsBuilder;
 import static org.elasticsearch.index.analysis.AnalysisTestUtils.createAnalysisService;
 import static org.hamcrest.Matchers.instanceOf;
 /**

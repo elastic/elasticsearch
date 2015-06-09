cdd3ac4dde4f69524ec0a14de3828cb95bbb86d0
Alexander Reelsen <alexander@reelsen.net>
2015-06-09 13:35:08 +0000

    release [1.6.0]

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -235,7 +235,7 @@ public class Version {
     public static final int V_1_5_3_ID = 1050399;
     public static final Version V_1_5_3 = new Version(V_1_5_3_ID, true, org.apache.lucene.util.Version.LUCENE_4_10_4);
     public static final int V_1_6_0_ID = 1060099;
-    public static final Version V_1_6_0 = new Version(V_1_6_0_ID, true, org.apache.lucene.util.Version.LUCENE_4_10_4);
+    public static final Version V_1_6_0 = new Version(V_1_6_0_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_4);
 
     public static final Version CURRENT = V_1_6_0;
 

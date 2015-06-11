97740b9411e408585cb57df422d7bf8d78e5df10
Shaunak Kashyap <ycombinator@gmail.com>
2015-06-11 06:03:09 -0700

    Removing top-level filter parameter from search API

--- core/src/main/java/org/elasticsearch/search/query/QueryPhase.java
+++ core/src/main/java/org/elasticsearch/search/query/QueryPhase.java
@@ -65,7 +65,6 @@ public class QueryPhase implements SearchPhase {
                 .put("query", new QueryParseElement())
                 .put("queryBinary", new QueryBinaryParseElement())
                 .put("query_binary", new QueryBinaryParseElement())
-                .put("filter", new PostFilterParseElement()) // For bw comp reason, should be removed in version 1.1
                 .put("post_filter", new PostFilterParseElement())
                 .put("postFilter", new PostFilterParseElement())
                 .put("filterBinary", new FilterBinaryParseElement())

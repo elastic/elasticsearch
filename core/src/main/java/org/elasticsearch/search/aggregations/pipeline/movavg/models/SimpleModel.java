ae742c4a038b5a1f7cbfdf1697845cd8ea4d71ac
Zachary Tong <zacharyjtong@gmail.com>
2015-06-19 10:36:15 -0400

    Aggregations: moving_avg model parser should accept any numeric, not just doubles
    Also changes the models to throw ParseExceptions instead of SearchParseExceptions, so that
    the validation can be unit-tested.
    
    Fixes #11487

--- core/src/main/java/org/elasticsearch/search/aggregations/pipeline/movavg/models/SimpleModel.java
+++ core/src/main/java/org/elasticsearch/search/aggregations/pipeline/movavg/models/SimpleModel.java
@@ -28,6 +28,7 @@ import org.elasticsearch.search.aggregations.pipeline.movavg.MovAvgParser;
 import org.elasticsearch.search.internal.SearchContext;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Collection;
 import java.util.Map;
 
@@ -72,7 +73,7 @@ public class SimpleModel extends MovAvgModel {
         }
 
         @Override
-        public MovAvgModel parse(@Nullable Map<String, Object> settings, String pipelineName,  SearchContext context, int windowSize) {
+        public MovAvgModel parse(@Nullable Map<String, Object> settings, String pipelineName, int windowSize) throws ParseException {
             return new SimpleModel();
         }
     }

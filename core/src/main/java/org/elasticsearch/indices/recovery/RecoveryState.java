68d658a30ffd85e9ecf1838c82bffe11d0df845e
szroland <szroland@freemail.hu>
2015-06-27 23:49:39 +0200

    Recovery: Fix wrong reused file bytes in Recovery API reports
    Simple snapshot.restore test case that also exposes the bug mentioned in #11876
    
    Fix #11876
    Closes #11965

--- core/src/main/java/org/elasticsearch/indices/recovery/RecoveryState.java
+++ core/src/main/java/org/elasticsearch/indices/recovery/RecoveryState.java
@@ -942,7 +942,7 @@ public class RecoveryState implements ToXContent, Streamable {
             // stream size first, as it matters more and the files section can be long
             builder.startObject(Fields.SIZE);
             builder.byteSizeField(Fields.TOTAL_IN_BYTES, Fields.TOTAL, totalBytes());
-            builder.byteSizeField(Fields.REUSED_IN_BYTES, Fields.REUSED, totalBytes());
+            builder.byteSizeField(Fields.REUSED_IN_BYTES, Fields.REUSED, reusedBytes());
             builder.byteSizeField(Fields.RECOVERED_IN_BYTES, Fields.RECOVERED, recoveredBytes());
             builder.field(Fields.PERCENT, String.format(Locale.ROOT, "%1.1f%%", recoveredBytesPercent()));
             builder.endObject();

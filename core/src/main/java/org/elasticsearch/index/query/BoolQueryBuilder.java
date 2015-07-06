60fa8908d8ab901de4537bfb14e4d12fbeb2facb
javanna <cavannaluca@gmail.com>
2015-07-06 11:00:54 +0200

    BoolQueryBuilder#hasClauses to take into account filterClauses too

--- core/src/main/java/org/elasticsearch/index/query/BoolQueryBuilder.java
+++ core/src/main/java/org/elasticsearch/index/query/BoolQueryBuilder.java
@@ -196,7 +196,7 @@ public class BoolQueryBuilder extends AbstractQueryBuilder<BoolQueryBuilder> {
      * Otherwise <code>false</code>.
      */
     public boolean hasClauses() {
-        return !(mustClauses.isEmpty() && shouldClauses.isEmpty() && mustNotClauses.isEmpty());
+        return !(mustClauses.isEmpty() && shouldClauses.isEmpty() && mustNotClauses.isEmpty() && filterClauses.isEmpty());
     }
 
     /**

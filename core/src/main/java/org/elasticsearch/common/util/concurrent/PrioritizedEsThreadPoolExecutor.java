e40eee557fa0eb9449672cf9bd8a89eea3bc9d4b
Boaz Leskes <b.leskes@gmail.com>
2015-07-17 11:11:41 +0200

    ThreadPools: schedule a timeout check after adding command to queue
    Our thread pools have support for timeout on a task. To support this, a special background task is schedule to run at timeout. That background task fires and check if the main task is still in the executor queue and then cancels it if needed. Currently we schedule this background task before adding the main task to the queue. If the timeout is very small (in tests we often use numbers like 2 ms)  the background task can fire before the main one is added to the queue causing the timeout to be missed.
    
    See http://build-us-00.elastic.co/job/es_g1gc_master_metal/11780/testReport/junit/org.elasticsearch.cluster/ClusterServiceTests/testTimeoutUpdateTask/
    
    Closes #12319

--- core/src/main/java/org/elasticsearch/common/util/concurrent/PrioritizedEsThreadPoolExecutor.java
+++ core/src/main/java/org/elasticsearch/common/util/concurrent/PrioritizedEsThreadPoolExecutor.java
@@ -105,6 +105,7 @@ public class PrioritizedEsThreadPoolExecutor extends EsThreadPoolExecutor {
         } else if (!(command instanceof PrioritizedFutureTask)) { // it might be a callable wrapper...
             command = new TieBreakingPrioritizedRunnable(command, Priority.NORMAL, insertionOrder.incrementAndGet());
         }
+        super.execute(command);
         if (timeout.nanos() >= 0) {
             if (command instanceof TieBreakingPrioritizedRunnable) {
                 ((TieBreakingPrioritizedRunnable) command).scheduleTimeout(timer, timeoutCallback, timeout);
@@ -114,7 +115,6 @@ public class PrioritizedEsThreadPoolExecutor extends EsThreadPoolExecutor {
                 throw new UnsupportedOperationException("Execute with timeout is not supported for future tasks");
             }
         }
-        super.execute(command);
     }
 
     @Override
@@ -161,7 +161,8 @@ public class PrioritizedEsThreadPoolExecutor extends EsThreadPoolExecutor {
 
         private Runnable runnable;
         private final long insertionOrder;
-        private ScheduledFuture<?> timeoutFuture;
+        private volatile ScheduledFuture<?> timeoutFuture;
+        private volatile boolean started = false;
 
         TieBreakingPrioritizedRunnable(PrioritizedRunnable runnable, long insertionOrder) {
             this(runnable, runnable.priority(), insertionOrder);
@@ -175,6 +176,9 @@ public class PrioritizedEsThreadPoolExecutor extends EsThreadPoolExecutor {
 
         @Override
         public void run() {
+            // make the task as stared. This is needed for synchronization with the timeout handling
+            // see  #scheduleTimeout()
+            started = true;
             FutureUtils.cancel(timeoutFuture);
             runAndClean(runnable);
         }
@@ -197,6 +201,10 @@ public class PrioritizedEsThreadPoolExecutor extends EsThreadPoolExecutor {
                     }
                 }
             }, timeValue.nanos(), TimeUnit.NANOSECONDS);
+            if (started) {
+                // if the actual action already it might have missed the setting of the future. Clean it ourselves.
+                FutureUtils.cancel(timeoutFuture);
+            }
         }
 
         /**

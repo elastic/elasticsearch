e11e75e3ec9f7d7bd3375146f237c949264bc0b9
Christoph Büscher <christoph@elastic.co>
2015-07-22 15:56:25 +0200

    Fix edge case for SpanMultiTermQueryBuilderTest

--- core/src/test/java/org/elasticsearch/index/query/SpanMultiTermQueryBuilderTest.java
+++ core/src/test/java/org/elasticsearch/index/query/SpanMultiTermQueryBuilderTest.java
@@ -68,8 +68,8 @@ public class SpanMultiTermQueryBuilderTest extends BaseQueryTestCase<SpanMultiTe
     @Test
     public void testUnsupportedInnerQueryType() throws IOException {
         QueryParseContext parseContext = createContext();
-        // test makes only sense if date field is mapped
-        if (parseContext.fieldMapper(DATE_FIELD_NAME) != null) {
+        // test makes only sense if we have at least one type registered with date field mapping
+        if (getCurrentTypes().length > 0 && parseContext.fieldMapper(DATE_FIELD_NAME) != null) {
             try {
                 RangeQueryBuilder query = new RangeQueryBuilder(DATE_FIELD_NAME);
                 new SpanMultiTermQueryBuilder(query).toQuery(createContext());

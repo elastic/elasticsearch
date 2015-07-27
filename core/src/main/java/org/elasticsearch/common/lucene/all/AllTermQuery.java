d7491515b21fb4b3c94956c75bcb74b8a5c863ae
Nik Everett <nik9000@gmail.com>
2015-07-27 15:36:34 -0400

    _all: Stop NPE querying _all when it doesn't exist
    This can happen in two ways:
    1. The _all field is disabled.
    2. There are documents in the index, the _all field is enabled, but there are
    no fields in any of the documents.
    
    In both of these cases we now rewrite the query to a MatchNoDocsQuery which
    should be safe because there isn't anything to match.
    
    Closes #12439

--- core/src/main/java/org/elasticsearch/common/lucene/all/AllTermQuery.java
+++ core/src/main/java/org/elasticsearch/common/lucene/all/AllTermQuery.java
@@ -25,6 +25,7 @@ import org.apache.lucene.index.PostingsEnum;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.index.Terms;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchNoDocsQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.payloads.AveragePayloadFunction;
@@ -124,13 +125,20 @@ public final class AllTermQuery extends PayloadTermQuery {
 
     @Override
     public Query rewrite(IndexReader reader) throws IOException {
+        boolean fieldExists = false;
         boolean hasPayloads = false;
         for (LeafReaderContext context : reader.leaves()) {
             final Terms terms = context.reader().terms(term.field());
-            if (terms.hasPayloads()) {
-                hasPayloads = true;
-                break;
+            if (terms != null) {
+                fieldExists = true;
+                if (terms.hasPayloads()) {
+                    hasPayloads = true;
+                    break;
+                }
             }
+        }
+        if (fieldExists == false) {
+            return new MatchNoDocsQuery();
         }
         if (hasPayloads == false) {
             TermQuery rewritten = new TermQuery(term);

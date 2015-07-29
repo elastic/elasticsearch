410b676c645e5a731b7aaf72b3ba5d56bb8f0791
Alexander Reelsen <alexander@reelsen.net>
2015-07-29 12:12:12 +0200

    Preparation for 1.6.3-SNAPSHOT, added bwc compat indices

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -240,8 +240,10 @@ public class Version {
     public static final Version V_1_6_1 = new Version(V_1_6_1_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_4);
     public static final int V_1_6_2_ID = 1060299;
     public static final Version V_1_6_2 = new Version(V_1_6_2_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_4);
+    public static final int V_1_6_3_ID = 1060399;
+    public static final Version V_1_6_3 = new Version(V_1_6_3_ID, true, org.apache.lucene.util.Version.LUCENE_4_10_4);
 
-    public static final Version CURRENT = V_1_6_2;
+    public static final Version CURRENT = V_1_6_3;
 
     static {
         assert CURRENT.luceneVersion.equals(Lucene.VERSION) : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
@@ -253,6 +255,8 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_1_6_3_ID:
+                return V_1_6_3;
             case V_1_6_2_ID:
                 return V_1_6_2;
             case V_1_6_1_ID:

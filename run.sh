bf579381a413bbce2ac99b13f8819fbc68b89973
Robert Muir <rmuir@apache.org>
2015-07-29 14:32:05 -0400

    explicitly add dev-tools, it doesnt use our parent, which might be scarier

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # turn off tests and other validation to speed it up
 # TODO: can be sped up more, if shading is moved out of core/
 # TODO: this will work on windows too. feel free to make a .bat
-mvn -am -pl distribution/zip package -DskipTests -Drun -Pdev
+mvn -am -pl dev-tools,distribution/zip package -DskipTests -Drun -Pdev

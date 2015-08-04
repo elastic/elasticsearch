20a5fc7e30e00c33012e108c84b5bb8aabb618ea
Clinton Gormley <clint@traveljury.com>
2015-08-04 13:07:58 +0200

    The check-license script now accepts either a directory or a .zip file only
    Called as:
    
        check_license_and_sha.pl --check path/to/licenses path/to/extracted/package/
    
    or
    
        check_license_and_sha.pl --check path/to/licenses path/to/file.zip

--- dev-tools/src/main/resources/license-check/check_license_and_sha.pl
+++ dev-tools/src/main/resources/license-check/check_license_and_sha.pl
@@ -6,37 +6,29 @@ use v5.10;
 
 use FindBin qw($RealBin);
 use lib "$RealBin/lib";
-use Archive::Ar();
-use Cwd();
 use File::Spec();
-use Digest::SHA qw(sha1);
 use File::Temp();
+use Digest::SHA qw(sha1);
 use File::Basename qw(basename);
 use Archive::Extract();
 $Archive::Extract::PREFER_BIN = 1;
-
-our %Extract_Package = (
-    zip => \&extract_zip,
-    gz  => \&extract_tar_gz,
-    rpm => \&extract_rpm,
-    deb => \&extract_deb
-);
 
 my $mode = shift(@ARGV) || "";
 die usage() unless $mode =~ /^--(check|update)$/;
 
 my $License_Dir = shift(@ARGV) || die usage();
-my $Package     = shift(@ARGV) || die usage();
+my $Source      = shift(@ARGV) || die usage();
 $License_Dir = File::Spec->rel2abs($License_Dir) . '/';
-$Package     = File::Spec->rel2abs($Package);
+$Source      = File::Spec->rel2abs($Source);
 
 die "License dir is not a directory: $License_Dir\n" . usage()
     unless -d $License_Dir;
 
-die "Package is not a file: $Package\n" . usage()
-    unless -f $Package;
+my %shas
+    = -f $Source ? jars_from_zip($Source)
+    : -d $Source ? jars_from_dir($Source)
+    :   die "Source is neither a directory nor a zip file: $Source" . usage();
 
-my %shas = get_shas_from_package($Package);
 $mode eq '--check'
     ? exit check_shas_and_licenses(%shas)
     : exit write_shas(%shas);
@@ -109,12 +101,14 @@ sub check_shas_and_licenses {
 
     my @unused_licenses = grep { !$licenses{$_} } keys %licenses;
     if (@unused_licenses) {
+        $error++;
         say STDERR "Extra LICENCE file present: " . join ", ",
             sort @unused_licenses;
     }
 
     my @unused_notices = grep { !$notices{$_} } keys %notices;
     if (@unused_notices) {
+        $error++;
         say STDERR "Extra NOTICE file present: " . join ", ",
             sort @unused_notices;
     }
@@ -124,7 +118,7 @@ sub check_shas_and_licenses {
 
 You can update the SHA files by running:
 
-$0 --update $License_Dir $Package
+$0 --update $License_Dir $Source
 
 SHAS
     }
@@ -194,82 +188,37 @@ sub get_sha_files {
 }
 
 #===================================
-sub get_shas_from_package {
+sub jars_from_zip {
 #===================================
-    my $package = shift;
-    my ($type) = ( $package =~ /\.(\w+)$/ );
-    die "Unrecognised package type: $package"
-        unless $type && $Extract_Package{$type};
-
+    my ($source) = @_;
     my $temp_dir = File::Temp->newdir;
-    my $files
-        = eval { $Extract_Package{$type}->( $package, $temp_dir->dirname ) }
-        or die "Couldn't extract $package: $@";
-
-    my @jars = map {"$temp_dir/$_"}
-        grep { /\.jar$/ && !/elasticsearch[^\/]*$/ } @$files;
+    my $dir_name = $temp_dir->dirname;
+    my $archive = Archive::Extract->new( archive => $source, type => 'zip' );
+    $archive->extract( to => $dir_name ) || die $archive->error;
+    my @jars = map { File::Spec->rel2abs( $_, $dir_name ) }
+        grep { /\.jar$/ && !/elasticsearch[^\/]*$/ } @{ $archive->files };
+    die "No JARS found in: $source\n"
+        unless @jars;
     return calculate_shas(@jars);
 }
 
 #===================================
-sub extract_zip {
+sub jars_from_dir {
 #===================================
-    my ( $package, $dir ) = @_;
-    my $archive = Archive::Extract->new( archive => $package, type => 'zip' );
-    $archive->extract( to => $dir ) || die $archive->error;
-    return $archive->files;
-}
-
-#===================================
-sub extract_tar_gz {
-#===================================
-    my ( $package, $dir ) = @_;
-    my $archive = Archive::Extract->new( archive => $package, type => 'tgz' );
-    $archive->extract( to => $dir ) || die $archive->error;
-    return $archive->files;
-}
-
-#===================================
-sub extract_rpm {
-#===================================
-    my ( $package, $dir ) = @_;
-    my $cwd = Cwd::cwd();
-    my @files;
-    eval {
-        chdir $dir;
-        say "Trying with rpm2cpio";
-        my $out = eval {`rpm2cpio '$package'  | cpio -idmv --quiet`};
-        unless ($out) {
-            say "Trying with rpm2cpio.pl";
-            $out = eval {`rpm2cpio.pl '$package'  | cpio -idmv --quiet`};
-        }
-        @files = split "\n", $out if $out;
-    };
-    chdir $cwd;
-    die $@ if $@;
-    die "Couldn't extract $package\n" unless @files;
-    return \@files;
-}
-
-#===================================
-sub extract_deb {
-#===================================
-    my ( $package, $dir ) = @_;
-    my $archive = Archive::Ar->new;
-    $archive->read($package) || die $archive->error;
-    my $cwd = Cwd::cwd();
-    eval {
-        chdir $dir;
-        $archive->extract('data.tar.gz') || die $archive->error;
-    };
-    chdir $cwd;
-    die $@ if $@;
-    $archive = Archive::Extract->new(
-        archive => $dir . '/data.tar.gz',
-        type    => 'tgz'
+    my $source = shift;
+    my @jars;
+    File::Find::find(
+        {   wanted => sub {
+                push @jars, File::Spec->rel2abs( $_, $source )
+                    if /\.jar$/ && !/elasticsearch[^\/]*$/;
+            },
+            no_chdir => 1
+        },
+        $source
     );
-    $archive->extract( to => $dir ) || die $archive->error;
-    return $archive->files;
+    die "No JARS found in: $source\n"
+        unless @jars;
+    return calculate_shas(@jars);
 }
 
 #===================================
@@ -291,11 +240,13 @@ sub usage {
 
 USAGE:
 
-    # check the sha1 and LICENSE files for each jar in the zip|gz|deb|rpm
+    # check the sha1 and LICENSE files for each jar in the zip or directory
     $0 --check  path/to/licenses/ path/to/package.zip
+    $0 --check  path/to/licenses/ path/to/dir/
 
-    # updates the sha1s for each jar in the zip|gz|deb|rpm
+    # updates the sha1s for each jar in the zip or directory
     $0 --update path/to/licenses/ path/to/package.zip
+    $0 --update path/to/licenses/ path/to/dir/
 
 USAGE
 

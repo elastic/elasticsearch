12e9dcc6842c1c3797d623891ae14638f336b87c
Adrien Grand <jpountz@gmail.com>
2015-08-11 10:23:46 +0200

    Add Shield testing to qa/smoke-test-plugins.
    Original commit: elastic/x-pack-elasticsearch@fbbc6cefda0aa34f06bc435a325274565e6fc43b

--- qa/smoke-test-plugins/src/test/java/org/elasticsearch/smoketest/SmokeTestPluginsIT.java
+++ qa/smoke-test-plugins/src/test/java/org/elasticsearch/smoketest/SmokeTestPluginsIT.java
@@ -7,13 +7,23 @@ package org.elasticsearch.smoketest;
 
 import com.carrotsearch.randomizedtesting.annotations.Name;
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
+
+import org.elasticsearch.client.support.Headers;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.shield.ShieldPlugin;
+import org.elasticsearch.shield.authc.support.SecuredString;
 import org.elasticsearch.test.rest.ESRestTestCase;
 import org.elasticsearch.test.rest.RestTestCandidate;
 import org.elasticsearch.test.rest.parser.RestTestParseException;
 
 import java.io.IOException;
 
+import static org.elasticsearch.shield.authc.support.UsernamePasswordToken.basicAuthHeaderValue;
+
 public class SmokeTestPluginsIT extends ESRestTestCase {
+
+    private static final String USER = "test_user";
+    private static final String PASS = "changeme";
 
     public SmokeTestPluginsIT(@Name("yaml") RestTestCandidate testCandidate) {
         super(testCandidate);
@@ -23,5 +33,22 @@ public class SmokeTestPluginsIT extends ESRestTestCase {
     public static Iterable<Object[]> parameters() throws IOException, RestTestParseException {
         return ESRestTestCase.createParameters(0, 1);
     }
+
+    @Override
+    protected Settings restClientSettings() {
+        String token = basicAuthHeaderValue(USER, new SecuredString(PASS.toCharArray()));
+        return Settings.builder()
+                .put(Headers.PREFIX + ".Authorization", token)
+                .build();
+    }
+
+    @Override
+    protected Settings externalClusterClientSettings() {
+        return Settings.builder()
+                .put("shield.user", USER + ":" + PASS)
+                .put("plugin.types", ShieldPlugin.class.getName())
+                .build();
+    }
+
 }
 

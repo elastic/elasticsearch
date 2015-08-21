db8a882517e07556c49488139636bfd8dae6d212
Jason Tedor <jason@tedor.me>
2015-08-21 09:19:11 -0400

    Limit type name length
    This commit backports the fix #13036 for #13021 to 2.0 from master.

--- core/src/test/java/org/elasticsearch/index/mapper/MapperServiceTest.java
+++ core/src/test/java/org/elasticsearch/index/mapper/MapperServiceTest.java
@@ -19,12 +19,18 @@
 
 package org.elasticsearch.index.mapper;
 
+import org.elasticsearch.Version;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.test.ESSingleNodeTestCase;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import static org.elasticsearch.test.VersionUtils.getFirstVersion;
+import static org.elasticsearch.test.VersionUtils.getPreviousVersion;
+import static org.elasticsearch.test.VersionUtils.randomVersionBetween;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasToString;
 
 public class MapperServiceTest extends ESSingleNodeTestCase {
@@ -38,6 +44,41 @@ public class MapperServiceTest extends ESSingleNodeTestCase {
         String index = "test-index";
         String type = ".test-type";
         String field = "field";
+        client()
+                .admin()
+                .indices()
+                .prepareCreate(index)
+                .addMapping(type, field, "type=string")
+                .execute()
+                .actionGet();
+    }
+
+    @Test
+    public void testThatLongTypeNameIsNotRejectedOnPreElasticsearchVersionTwo() {
+        String index = "text-index";
+        String field = "field";
+        String type = new String(new char[256]).replace("\0", "a");
+
+        CreateIndexResponse response =
+                client()
+                        .admin()
+                        .indices()
+                        .prepareCreate(index)
+                        .setSettings(settings(randomVersionBetween(random(), getFirstVersion(), getPreviousVersion(Version.V_2_0_0_beta1))))
+                        .addMapping(type, field, "type=string")
+                        .execute()
+                        .actionGet();
+        assertNotNull(response);
+    }
+
+    @Test
+    public void testTypeNameTooLong() {
+        String index = "text-index";
+        String field = "field";
+        String type = new String(new char[256]).replace("\0", "a");
+
+        expectedException.expect(MapperParsingException.class);
+        expectedException.expect(hasToString(containsString("mapping type name [" + type + "] is too long; limit is length 255 but was [256]")));
         client()
                 .admin()
                 .indices()

8cc9531bcbdfab41b8e3aa7f6373c75e1f055426
Jason Tedor <jason@tedor.me>
2015-08-30 11:54:14 -0400

    Update for API changes in 5e2efcfe091c62ef215016e2981d05339ea945a3
    Original commit: elastic/x-pack-elasticsearch@5cf5f71ba6c4695501b1406e247a9a0343343ea0

--- marvel/src/test/java/org/elasticsearch/marvel/agent/renderer/indices/IndexRecoveryRendererTests.java
+++ marvel/src/test/java/org/elasticsearch/marvel/agent/renderer/indices/IndexRecoveryRendererTests.java
@@ -7,7 +7,6 @@ package org.elasticsearch.marvel.agent.renderer.indices;
 
 import org.elasticsearch.Version;
 import org.elasticsearch.action.admin.indices.recovery.RecoveryResponse;
-import org.elasticsearch.action.admin.indices.recovery.ShardRecoveryResponse;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.common.transport.DummyTransportAddress;
 import org.elasticsearch.index.shard.ShardId;
@@ -36,19 +35,17 @@ public class IndexRecoveryRendererTests extends ESTestCase {
         DiscoveryNode source = new DiscoveryNode("node-src", DummyTransportAddress.INSTANCE, Version.CURRENT);
         DiscoveryNode target = new DiscoveryNode("node-tgt", DummyTransportAddress.INSTANCE, Version.CURRENT);
 
-        List<ShardRecoveryResponse> shards = new ArrayList<>();
+        List<RecoveryState> shards = new ArrayList<>();
 
         // Shard 0
-        ShardRecoveryResponse shard0 = new ShardRecoveryResponse();
-        shard0.recoveryState(new RecoveryState(new ShardId(indexName, 0), true, RecoveryState.Type.RELOCATION, source, target));
+        RecoveryState shard0 = new RecoveryState(new ShardId(indexName, 0), true, RecoveryState.Type.RELOCATION, source, target);
         shards.add(shard0);
 
         // Shard 1
-        ShardRecoveryResponse shard1 = new ShardRecoveryResponse();
-        shard1.recoveryState(new RecoveryState(new ShardId(indexName, 1), true, RecoveryState.Type.STORE, source, target));
+        RecoveryState shard1 = new RecoveryState(new ShardId(indexName, 1), true, RecoveryState.Type.STORE, source, target);
         shards.add(shard1);
 
-        Map<String, List<ShardRecoveryResponse>> shardResponses = new HashMap<>(1);
+        Map<String, List<RecoveryState>> shardResponses = new HashMap<>(1);
         shardResponses.put(indexName, shards);
 
         RecoveryResponse recoveryResponse = new RecoveryResponse(2, 2, 2, false, shardResponses, null);

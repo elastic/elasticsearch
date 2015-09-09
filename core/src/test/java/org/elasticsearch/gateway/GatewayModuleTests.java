bfdb5284b4996870320f4fba452f8ffb8ca542a2
Boaz Leskes <b.leskes@gmail.com>
2015-09-09 12:13:17 +0200

    remove unused code

--- core/src/test/java/org/elasticsearch/gateway/GatewayModuleTests.java
+++ core/src/test/java/org/elasticsearch/gateway/GatewayModuleTests.java
@@ -23,16 +23,7 @@ import org.elasticsearch.cluster.ClusterService;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.ModuleTestCase;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.env.NodeEnvironment;
-import org.elasticsearch.node.Node;
-import org.elasticsearch.node.NodeBuilder;
-import org.elasticsearch.node.internal.InternalSettingsPreparer;
-import org.elasticsearch.plugins.Plugin;
-import org.elasticsearch.test.ESTestCase;
-import org.elasticsearch.test.InternalTestCluster;
-
-import static org.hamcrest.Matchers.instanceOf;
 
 public class GatewayModuleTests extends ModuleTestCase {
 
@@ -45,44 +36,6 @@ public class GatewayModuleTests extends ModuleTestCase {
     public void testDefaultGateway() {
         GatewayModule gatewayModule = new GatewayModule(Settings.EMPTY);
         assertBinding(gatewayModule, Gateway.class, Gateway.class);
-    }
-
-    protected Node buildNode(String gateway) {
-        final Settings.Builder settings = Settings.builder()
-                .put(ClusterName.SETTING, InternalTestCluster.clusterName("custom-gateway", randomLong()))
-                .put("path.home", createTempDir())
-                .put("path.shared_data", createTempDir().getParent())
-                .put("node.name", "mock_gateway")
-                .put(EsExecutors.PROCESSORS, 1) // limit the number of threads created
-                .put("http.enabled", false)
-                .put(InternalSettingsPreparer.IGNORE_SYSTEM_PROPERTIES_SETTING, true);
-        if (gateway != null) {
-            settings.put(GatewayModule.GATEWAY_TYPE_KEY, gateway);
-            settings.putArray("plugin.types", TestPlugin.class.getName());
-        }
-        return NodeBuilder.nodeBuilder().local(true).data(true).settings(settings // make sure we get what we set :)
-        ).build().start();
-    }
-
-    public static class TestPlugin extends Plugin {
-        @Override
-        public String name() {
-            return "mock-gateway";
-        }
-
-        @Override
-        public String description() {
-            return "injecting custom MockGateway testing";
-        }
-
-        public void onModule(GatewayModule gatewayModule) {
-            gatewayModule.registerGatewayType("mock", MockGateway.class);
-        }
-
-        @Override
-        public Settings additionalSettings() {
-            return Settings.builder().put(GatewayModule.GATEWAY_TYPE_KEY, "mock").build();
-        }
     }
 
     public static class MockGateway extends Gateway {

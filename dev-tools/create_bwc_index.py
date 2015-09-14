a96350d785f86364e174ac9b5cef8738bc7040b9
Alexander Reelsen <alexander@reelsen.net>
2015-09-14 12:37:02 +0200

    Release: Add 1.7.2 bwc indices/versions
    * Added BWC indices
    * Added snapshot version to Version.java
    * Fixed create_bwc_index to use localhost instead of localhost and 127.0.0.1 (problem with ipv4/6 setup)

--- dev-tools/create_bwc_index.py
+++ dev-tools/create_bwc_index.py
@@ -154,7 +154,7 @@ def create_client(http_port=DEFAULT_HTTP_TCP_PORT, timeout=30):
   for _ in range(0, timeout):
     # TODO: ask Honza if there is a better way to do this?
     try:
-      client = Elasticsearch([{'host': '127.0.0.1', 'port': http_port}])
+      client = Elasticsearch([{'host': 'localhost', 'port': http_port}])
       client.cluster.health(wait_for_nodes=1)
       client.count() # can we actually search or do we get a 503? -- anyway retry
       return client

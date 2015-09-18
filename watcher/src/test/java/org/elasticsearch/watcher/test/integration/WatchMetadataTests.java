288274c3c245247bde64fff8523ad9ce99710bd8
Martijn van Groningen <martijn.v.groningen@gmail.com>
2015-09-18 11:07:35 +0200

    test: removed test logging
    Original commit: elastic/x-pack-elasticsearch@9ce8f5e7d8aae83ee8320231b68ac83aa8df49f2

--- watcher/src/test/java/org/elasticsearch/watcher/test/integration/WatchMetadataTests.java
+++ watcher/src/test/java/org/elasticsearch/watcher/test/integration/WatchMetadataTests.java
@@ -80,7 +80,6 @@ public class WatchMetadataTests extends AbstractWatcherIntegrationTestCase {
     }
 
     @Test
-    @TestLogging("watcher:TRACE")
     public void testWatchMetadataAvailableAtExecution() throws Exception {
         Map<String, Object> metadata = new HashMap<>();
         metadata.put("foo", "bar");

047022ab0f03871f558b8ec91bc58e07224ca394
Tanguy Leroux <tlrx.dev@gmail.com>
2015-09-25 18:05:31 +0200

    Move MarvelRestIT.java in a rest package
    Original commit: elastic/x-pack-elasticsearch@b2e90bb3ea05ba8ef60bb64aecc9534b17b21247

--- marvel/src/test/java/org/elasticsearch/marvel/rest/MarvelRestIT.java
+++ marvel/src/test/java/org/elasticsearch/marvel/rest/MarvelRestIT.java
@@ -3,19 +3,16 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-package org.elasticsearch.marvel;
+package org.elasticsearch.marvel.rest;
 
 import com.carrotsearch.randomizedtesting.annotations.Name;
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
-import org.apache.lucene.util.LuceneTestCase;
 import org.elasticsearch.test.rest.ESRestTestCase;
 import org.elasticsearch.test.rest.RestTestCandidate;
 import org.elasticsearch.test.rest.parser.RestTestParseException;
-import org.junit.Ignore;
 
 import java.io.IOException;
 
-@LuceneTestCase.AwaitsFix(bugUrl = "why do we have this rest test in the first place???")
 public class MarvelRestIT extends ESRestTestCase {
 
     public MarvelRestIT(@Name("yaml") RestTestCandidate testCandidate) {

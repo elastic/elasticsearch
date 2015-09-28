9b4a2c906e50b737f0ca2f4a8a34312afb87c7be
Tanguy Leroux <tlrx.dev@gmail.com>
2015-09-28 11:02:32 +0200

    Marvel: Mute ClusterInfoCollectorTests until licensing stuff is updated
    Original commit: elastic/x-pack-elasticsearch@d1b03e7cd3642803378083fd0f51d07415f042cf

--- marvel/src/test/java/org/elasticsearch/marvel/agent/collector/cluster/ClusterInfoCollectorTests.java
+++ marvel/src/test/java/org/elasticsearch/marvel/agent/collector/cluster/ClusterInfoCollectorTests.java
@@ -20,7 +20,10 @@ import static org.hamcrest.Matchers.*;
 
 public class ClusterInfoCollectorTests extends AbstractCollectorTestCase {
 
-    @Test
+    // This test is muted for now until licensing stuff is updated.
+    // Right now AbstractCollectorTestCase only registers license for Marvel,
+    // so license count at line 56 will always be 1 even when Shield is enabled
+    @Test @AwaitsFix(bugUrl = "https://github.com/elastic/x-plugins/issues/683")
     public void testClusterInfoCollector() throws Exception {
         Collection<MarvelDoc> results = newClusterInfoCollector().doCollect();
         assertThat(results, hasSize(1));

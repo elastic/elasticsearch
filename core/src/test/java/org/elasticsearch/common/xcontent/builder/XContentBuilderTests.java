74fa85c52714e7f45ec8ae5dada8c2e359676788
Simon Willnauer <simon.willnauer@elasticsearch.com>
2015-09-29 14:28:03 +0200

    Merge pull request #13816 from s1monw/fix_jackson_line_feed
    Ensure XContent is consistent across platforms

--- core/src/test/java/org/elasticsearch/common/xcontent/builder/XContentBuilderTests.java
+++ core/src/test/java/org/elasticsearch/common/xcontent/builder/XContentBuilderTests.java
@@ -320,6 +320,25 @@ public class XContentBuilderTests extends ESTestCase {
         assertThat(pathBuilder.string(), equalTo(stringBuilder.string()));
     }
 
+    public void testIndentIsPlatformIndependent() throws IOException {
+        XContentBuilder builder = XContentFactory.contentBuilder(XContentType.JSON).prettyPrint();
+        builder.startObject().field("test","foo").startObject("foo").field("foobar", "boom").endObject().endObject();
+        String string = builder.string();
+        assertEquals("{\n" +
+                "  \"test\" : \"foo\",\n" +
+                "  \"foo\" : {\n" +
+                "    \"foobar\" : \"boom\"\n" +
+                "  }\n" +
+                "}", string);
+
+        builder = XContentFactory.contentBuilder(XContentType.YAML).prettyPrint();
+        builder.startObject().field("test","foo").startObject("foo").field("foobar", "boom").endObject().endObject();
+        string = builder.string();
+        assertEquals("---\n" +
+                "test: \"foo\"\n" +
+                "foo:\n" +
+                "  foobar: \"boom\"\n", string);
+    }
 
     public void testRenderGeoPoint() throws IOException {
         XContentBuilder builder = XContentFactory.contentBuilder(XContentType.JSON).prettyPrint();

ea494fc69cdfb690a18a4cf545bbb0809c7d7c5e
Ryan Ernst <ryan@iernst.net>
2015-09-30 12:34:05 +0200

    fix incorrect maven group id for zip distribution

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/test/ClusterFormationTasks.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/test/ClusterFormationTasks.groovy
@@ -146,7 +146,7 @@ class ClusterFormationTasks {
             elasticsearchZip
         }
         project.dependencies {
-            elasticsearchZip "org.elasticsearch.distributions.zip:elasticsearch:${elasticsearchVersion}"
+            elasticsearchZip "org.elasticsearch.distribution.zip:elasticsearch:${elasticsearchVersion}"
         }
     }
 }

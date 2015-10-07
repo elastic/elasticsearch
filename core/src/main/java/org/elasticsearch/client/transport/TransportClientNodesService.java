1f7a7bacbb2f6f72949401e854a7572deb3feb4f
Itamar Syn-Hershko <itamar@code972.com>
2015-10-08 01:08:35 +0300

    Update TransportClientNodesService.java

--- core/src/main/java/org/elasticsearch/client/transport/TransportClientNodesService.java
+++ core/src/main/java/org/elasticsearch/client/transport/TransportClientNodesService.java
@@ -53,7 +53,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;c
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CountDownLatch;

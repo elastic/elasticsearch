0dca49fae4b9571a25dd5b00769594fb15ca1407
Ryan Ernst <ryan@iernst.net>
2015-10-30 21:32:16 -0700

    fix eclipse config for qa projects

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/test/RestTestPlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/test/RestTestPlugin.groovy
@@ -30,7 +30,6 @@ class RestTestPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.pluginManager.apply('java-base')
         project.pluginManager.apply('carrotsearch.randomized-testing')
-        project.pluginManager.apply('idea')
 
         // remove some unnecessary tasks for a qa test
         project.tasks.removeAll { it.name in ['assemble', 'buildDependents'] }
@@ -53,6 +52,7 @@ class RestTestPlugin implements Plugin<Project> {
         project.eclipse {
             classpath {
                 sourceSets = [project.sourceSets.test]
+                plusConfigurations = [project.configurations.testRuntime]
             }
         }
     }

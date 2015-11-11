ac0af568cd1cb253ad8765a20cd257890fb55f09
David Pursehouse <david.pursehouse@sonymobile.com>
2015-11-10 22:22:33 -0800

    Update Lucene verion in Version
    Change-Id: I7b3ea40b29fdb37f0d6ee1f3736ef5ff00ad0c38

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -268,7 +268,7 @@ public class Version {
     public static final int V_2_0_1_ID = 2000199;
     public static final Version V_2_0_1 = new Version(V_2_0_1_ID, true, org.apache.lucene.util.Version.LUCENE_5_2_1);
     public static final int V_2_1_0_ID = 2010099;
-    public static final Version V_2_1_0 = new Version(V_2_1_0_ID, true, org.apache.lucene.util.Version.LUCENE_5_3_0);
+    public static final Version V_2_1_0 = new Version(V_2_1_0_ID, true, org.apache.lucene.util.Version.LUCENE_5_3_1);
 
     public static final Version CURRENT = V_2_1_0;
 

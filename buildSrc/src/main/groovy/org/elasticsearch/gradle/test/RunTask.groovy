50b48e7d8ccf8a7c978e23e96ba8ab5157f2a0dd
Ryan Ernst <ryan@iernst.net>
2015-11-16 16:02:44 -0800

    Build: Make run command work within plugins
    We recently got a run command with gradle, but it is sometimes useful to
    run ES with a specific plugin. This is a start, by making each esplugin
    have a run command which installs the plugin and runs elasticsearch in
    the foreground.

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/test/RunTask.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/test/RunTask.groovy
@@ -1,12 +1,23 @@
 package org.elasticsearch.gradle.test
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.Project
 
 class RunTask extends DefaultTask {
+
     ClusterConfiguration clusterConfig = new ClusterConfiguration(httpPort: 9200, transportPort: 9300, daemonize: false)
 
     RunTask() {
-        ClusterFormationTasks.setup(project, this, clusterConfig)
+        project.afterEvaluate {
+            ClusterFormationTasks.setup(project, this, clusterConfig)
+        }
+    }
+
+    static void configure(Project project) {
+        RunTask task = project.tasks.create(
+            name: 'run',
+            type: RunTask,
+            description: "Runs elasticsearch with '${project.path}'",
+            group: 'Verification')
     }
 }

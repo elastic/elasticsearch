7df641c1b308a17a104a56558f1640356447c580
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2015-11-18 15:07:50 +0000

    Aggregations Refactor: Refactor Percentiles and Percentile Ranks Aggregation

--- core/src/main/java/org/elasticsearch/search/aggregations/bucket/range/ipv4/IpRangeParser.java
+++ core/src/main/java/org/elasticsearch/search/aggregations/bucket/range/ipv4/IpRangeParser.java
@@ -142,7 +142,7 @@ public class IpRangeParser implements Aggregator.Parser {
 
     // NORELEASE implement this method when refactoring this aggregation
     @Override
-    public AggregatorFactory getFactoryPrototype() {
+    public AggregatorFactory[] getFactoryPrototypes() {
         return null;
     }
 

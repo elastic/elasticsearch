af3be95ba5f2d139776816937af11ff0a7bc77f4
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2015-11-20 15:26:28 +0000

    Aggregations Refactor: Refactor Cardinality Aggregation

--- core/src/main/java/org/elasticsearch/search/aggregations/metrics/cardinality/CardinalityAggregatorFactory.java
+++ core/src/main/java/org/elasticsearch/search/aggregations/metrics/cardinality/CardinalityAggregatorFactory.java
@@ -19,29 +19,61 @@
 
 package org.elasticsearch.search.aggregations.metrics.cardinality;
 
+import org.elasticsearch.common.ParseField;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.search.aggregations.Aggregator;
 import org.elasticsearch.search.aggregations.bucket.SingleBucketAggregator;
 import org.elasticsearch.search.aggregations.pipeline.PipelineAggregator;
 import org.elasticsearch.search.aggregations.support.AggregationContext;
+import org.elasticsearch.search.aggregations.support.ValueType;
 import org.elasticsearch.search.aggregations.support.ValuesSource;
 import org.elasticsearch.search.aggregations.support.ValuesSourceAggregatorFactory;
-import org.elasticsearch.search.aggregations.support.ValuesSourceParser;
+import org.elasticsearch.search.aggregations.support.ValuesSourceType;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
-final class CardinalityAggregatorFactory extends ValuesSourceAggregatorFactory.LeafOnly<ValuesSource> {
+public final class CardinalityAggregatorFactory<VS extends ValuesSource> extends ValuesSourceAggregatorFactory.LeafOnly<VS> {
 
-    private final long precisionThreshold;
+    public static final ParseField PRECISION_THRESHOLD_FIELD = new ParseField("precision_threshold");
 
-    CardinalityAggregatorFactory(String name, ValuesSourceParser.Input<ValuesSource> input, long precisionThreshold) {
-        super(name, InternalCardinality.TYPE, input);
+    private Long precisionThreshold = null;
+
+    public CardinalityAggregatorFactory(String name, ValuesSourceType valuesSourceType, ValueType valueType) {
+        super(name, InternalCardinality.TYPE, valuesSourceType, valueType);
+    }
+
+    /**
+     * Set a precision threshold. Higher values improve accuracy but also
+     * increase memory usage.
+     */
+    public void precisionThreshold(long precisionThreshold) {
         this.precisionThreshold = precisionThreshold;
     }
 
+    /**
+     * Get the precision threshold. Higher values improve accuracy but also
+     * increase memory usage. Will return <code>null</code> if the
+     * precisionThreshold has not been set yet.
+     */
+    public Long precisionThreshold() {
+        return precisionThreshold;
+    }
+
+    /**
+     * @deprecated no replacement - values will always be rehashed
+     */
+    @Deprecated
+    public void rehash(boolean rehash) {
+        // Deprecated all values are already rehashed so do nothing
+    }
+
     private int precision(Aggregator parent) {
-        return precisionThreshold < 0 ? defaultPrecision(parent) : HyperLogLogPlusPlus.precisionFromThreshold(precisionThreshold);
+        return precisionThreshold == null ? defaultPrecision(parent) : HyperLogLogPlusPlus.precisionFromThreshold(precisionThreshold);
     }
 
     @Override
@@ -51,10 +83,48 @@ final class CardinalityAggregatorFactory extends ValuesSourceAggregatorFactory.L
     }
 
     @Override
-    protected Aggregator doCreateInternal(ValuesSource valuesSource, AggregationContext context, Aggregator parent,
+    protected Aggregator doCreateInternal(VS valuesSource, AggregationContext context, Aggregator parent,
             boolean collectsFromSingleBucket, List<PipelineAggregator> pipelineAggregators, Map<String, Object> metaData) throws IOException {
         return new CardinalityAggregator(name, valuesSource, precision(parent), config.formatter(), context, parent, pipelineAggregators,
                 metaData);
+    }
+
+    @Override
+    protected ValuesSourceAggregatorFactory<VS> innerReadFrom(String name, ValuesSourceType valuesSourceType,
+            ValueType targetValueType, StreamInput in) throws IOException {
+        CardinalityAggregatorFactory<VS> factory = new CardinalityAggregatorFactory<>(name, valuesSourceType, targetValueType);
+        if (in.readBoolean()) {
+            factory.precisionThreshold = in.readLong();
+        }
+        return factory;
+    }
+
+    @Override
+    protected void innerWriteTo(StreamOutput out) throws IOException {
+        boolean hasPrecisionThreshold = precisionThreshold != null;
+        out.writeBoolean(hasPrecisionThreshold);
+        if (hasPrecisionThreshold) {
+            out.writeLong(precisionThreshold);
+        }
+    }
+
+    @Override
+    public XContentBuilder doXContentBody(XContentBuilder builder, Params params) throws IOException {
+        if (precisionThreshold != null) {
+            builder.field(PRECISION_THRESHOLD_FIELD.getPreferredName(), precisionThreshold);
+        }
+        return builder;
+    }
+
+    @Override
+    protected int innerHashCode() {
+        return Objects.hash(precisionThreshold);
+    }
+
+    @Override
+    protected boolean innerEquals(Object obj) {
+        CardinalityAggregatorFactory<ValuesSource> other = (CardinalityAggregatorFactory<ValuesSource>) obj;
+        return Objects.equals(precisionThreshold, other.precisionThreshold);
     }
 
     /*

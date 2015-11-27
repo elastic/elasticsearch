3bbb65efd783da96573af0bb2b98e68de193629b
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2015-11-27 10:00:13 +0000

    Aggregations Refactor: Refactor Serial Differencing Aggregation

--- core/src/test/java/org/elasticsearch/search/aggregations/BasePipelineAggregationTestCase.java
+++ core/src/test/java/org/elasticsearch/search/aggregations/BasePipelineAggregationTestCase.java
@@ -32,6 +32,7 @@ import org.elasticsearch.common.io.stream.NamedWriteableAwareStreamInput;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsFilter;
 import org.elasticsearch.common.settings.SettingsModule;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentParser;
@@ -101,7 +102,7 @@ public abstract class BasePipelineAggregationTestCase<AF extends PipelineAggrega
         index = new Index("test");
         injector = new ModulesBuilder().add(
                 new EnvironmentModule(new Environment(settings)),
-                new SettingsModule(settings),
+                new SettingsModule(settings, new SettingsFilter(settings)),
                 new ThreadPoolModule(new ThreadPool(settings)),
                 new ScriptModule(settings),
                 new IndicesModule() {

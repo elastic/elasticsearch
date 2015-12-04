784ebb1e1b9691225120ca9eac5c5b3473d853fc
Robert Muir <rmuir@apache.org>
2015-12-04 03:25:43 -0500

    fix bats tests

--- qa/vagrant/src/test/resources/packaging/scripts/plugin_test_cases.bash
+++ qa/vagrant/src/test/resources/packaging/scripts/plugin_test_cases.bash
@@ -223,14 +223,6 @@ fi
     install_and_check_plugin discovery multicast
 }
 
-@test "[$GROUP] install lang-expression plugin" {
-    install_and_check_plugin lang expression
-}
-
-@test "[$GROUP] install lang-groovy plugin" {
-    install_and_check_plugin lang groovy
-}
-
 @test "[$GROUP] install javascript plugin" {
     install_and_check_plugin lang javascript rhino-*.jar
 }
@@ -329,14 +321,6 @@ fi
 
 @test "[$GROUP] remove multicast discovery plugin" {
     remove_plugin discovery-multicast
-}
-
-@test "[$GROUP] remove lang-expression plugin" {
-    remove_plugin lang-expression
-}
-
-@test "[$GROUP] remove lang-groovy plugin" {
-    remove_plugin lang-groovy
 }
 
 @test "[$GROUP] remove javascript plugin" {

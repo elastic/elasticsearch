4cdd0f3bc267b99076dbeb24e90d9e8c4ee3b660
Lukas Olson <olson.lukas@gmail.com>
2015-12-07 08:38:36 -0700

    Move to new directory structure
    Original commit: elastic/x-pack-elasticsearch@de5f703b6d1a98bfbcdb494491077fad48d0bae2

--- shield/kibana/server/lib/get_auth_header.js
+++
@@ -1,4 +0,0 @@
-module.exports = (username, password) => {
-  const auth = new Buffer(`${username}:${password}`, 'utf8').toString('base64');
-  return {'Authorization': `Basic ${auth}`};
-};
--- shield/kibana/server/lib/is_valid_user.js
+++
@@ -1,5 +0,0 @@
-const getAuthHeader = require('./get_auth_header');
-
-module.exports = (client) => (username, password) => client.info({
-  headers: getAuthHeader(username, password)
-});

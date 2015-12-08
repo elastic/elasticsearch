e2401524c374f4ad7d9e03e241665f3085e5b1fc
Britta Weber <brwe@users.noreply.github.com>
2015-12-08 10:24:32 +0100

    Merge pull request #15213 from brwe/copy-to-in-multi-fields-exception
    throw exception if a copy_to is within a multi field
    
    Copy to within multi field is ignored from 2.0 on, see #10802.
    Instead of just ignoring it, we should throw an exception if this
    is found in the mapping when a mapping is added. For already
    existing indices we should at least log a warning.
    
    related to #14946

--- core/src/main/java/org/elasticsearch/index/mapper/Mapper.java
+++ core/src/main/java/org/elasticsearch/index/mapper/Mapper.java
@@ -134,6 +134,24 @@ public abstract class Mapper implements ToXContent, Iterable<Mapper> {
             public ParseFieldMatcher parseFieldMatcher() {
                 return parseFieldMatcher;
             }
+
+            public boolean isWithinMultiField() { return false; }
+
+            protected Map<String, TypeParser> typeParsers() { return typeParsers; }
+
+            public ParserContext createMultiFieldContext(ParserContext in) {
+                return new MultiFieldParserContext(in) {
+                    @Override
+                    public boolean isWithinMultiField() { return true; }
+                };
+            }
+
+            class MultiFieldParserContext extends ParserContext {
+                MultiFieldParserContext(ParserContext in) {
+                    super(in.type(), in.analysisService, in.similarityLookupService(), in.mapperService(), ImmutableMap.<String, TypeParser>builder().putAll(in.typeParsers()).build(), in.indexVersionCreated(), in.parseFieldMatcher());
+                }
+            }
+
         }
 
         Mapper.Builder<?,?> parse(String name, Map<String, Object> node, ParserContext parserContext) throws MapperParsingException;

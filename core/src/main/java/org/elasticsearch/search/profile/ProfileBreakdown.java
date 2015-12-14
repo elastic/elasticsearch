fde3b0587911f0b5f15e779c671d0510cbd568a9
Zachary Tong <zacharyjtong@gmail.com>
2015-12-14 10:28:23 -0500

    Documentation tweaks, renaming build_weight -> create_weight

--- core/src/main/java/org/elasticsearch/search/profile/ProfileBreakdown.java
+++ core/src/main/java/org/elasticsearch/search/profile/ProfileBreakdown.java
@@ -33,7 +33,7 @@ public final class ProfileBreakdown {
 
     /** Enumeration of all supported timing types. */
     public enum TimingType {
-        BUILD_WEIGHT,
+        CREATE_WEIGHT,
         BUILD_SCORER,
         NEXT_DOC,
         ADVANCE,

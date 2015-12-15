5c58b391f15626e4abc4a866591d971ff54dcef0
Simon Willnauer <simonw@apache.org>
2015-12-15 14:22:23 +0100

    Bump version to 1.7.5-SNAPSHOT

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -252,8 +252,9 @@ public class Version {
     public static final Version V_1_7_3 = new Version(V_1_7_3_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_4);
     public static final int V_1_7_4_ID = 1070499;
     public static final Version V_1_7_4 = new Version(V_1_7_4_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_4);
-
-    public static final Version CURRENT = V_1_7_4;
+    public static final int V_1_7_5_ID = 1070599;
+    public static final Version V_1_7_5 = new Version(V_1_7_5_ID, true, org.apache.lucene.util.Version.LUCENE_4_10_4);
+    public static final Version CURRENT = V_1_7_5;
 
     static {
         assert CURRENT.luceneVersion.equals(Lucene.VERSION) : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
@@ -265,6 +266,8 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_1_7_5_ID:
+                return V_1_7_5;
             case V_1_7_4_ID:
                 return V_1_7_4;
             case V_1_7_3_ID:

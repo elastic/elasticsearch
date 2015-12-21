85dba5f0a9c237543a086dd6cf5a570a9a678803
Simon Willnauer <simonw@apache.org>
2015-12-21 15:45:13 +0100

    Move to 2.3.0-SNAPSHOT

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -279,8 +279,10 @@ public class Version {
     public static final Version V_2_1_2 = new Version(V_2_1_2_ID, true, org.apache.lucene.util.Version.LUCENE_5_3_1);
     public static final int V_2_2_0_ID = 2020099;
     public static final Version V_2_2_0 = new Version(V_2_2_0_ID, true, org.apache.lucene.util.Version.LUCENE_5_4_0);
+    public static final int V_2_3_0_ID = 2030099;
+    public static final Version V_2_3_0 = new Version(V_2_3_0_ID, true, org.apache.lucene.util.Version.LUCENE_5_4_0);
 
-    public static final Version CURRENT = V_2_2_0;
+    public static final Version CURRENT = V_2_3_0;
 
     static {
         assert CURRENT.luceneVersion.equals(Lucene.VERSION) : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
@@ -292,6 +294,8 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_2_3_0_ID:
+                return V_2_3_0;
             case V_2_2_0_ID:
                 return V_2_2_0;
             case V_2_1_2_ID:

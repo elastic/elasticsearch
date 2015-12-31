174012174b5db8c47ff49821da8d0a5f54cd9bd1
Nicholas Knize <nknize@gmail.com>
2015-12-31 10:56:04 -0600

    Reconcile GeoPoint toString and fromString methods
    GeoPoint.toString prints as a json array of values, but resetFromString expects comma delimited. This commit reconciles the methods.

--- core/src/test/java/org/elasticsearch/index/mapper/multifield/MultiFieldsIntegrationIT.java
+++ core/src/test/java/org/elasticsearch/index/mapper/multifield/MultiFieldsIntegrationIT.java
@@ -26,6 +26,7 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.cluster.metadata.MappingMetaData;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.geo.GeoPoint;
 import org.elasticsearch.common.unit.DistanceUnit;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
@@ -127,12 +128,13 @@ public class MultiFieldsIntegrationIT extends ESIntegTestCase {
         assertThat(bField.get("type").toString(), equalTo("string"));
         assertThat(bField.get("index").toString(), equalTo("not_analyzed"));
 
+        GeoPoint point = new GeoPoint(51, 19);
         client().prepareIndex("my-index", "my-type", "1").setSource("a", "51,19").setRefresh(true).get();
         CountResponse countResponse = client().prepareCount("my-index")
                 .setQuery(constantScoreQuery(geoDistanceQuery("a").point(51, 19).distance(50, DistanceUnit.KILOMETERS)))
                 .get();
         assertThat(countResponse.getCount(), equalTo(1l));
-        countResponse = client().prepareCount("my-index").setQuery(matchQuery("a.b", "51,19")).get();
+        countResponse = client().prepareCount("my-index").setQuery(matchQuery("a.b", point.toString())).get();
         assertThat(countResponse.getCount(), equalTo(1l));
     }
 

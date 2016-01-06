6ddd233a0283e12634a89a566f34ab645c4dba20
Nik Everett <nik9000@gmail.com>
2016-01-06 12:12:20 -0500

    Don't override originalQuery with request filters
    These filters leak into highlighting and probably other places and cause
    things like the type name to be highlighted when using
    requireFieldMatch=false. We could have special hacks to keep them out of
    highlighting but it feals better to keep them out of any variable named
    "originalQuery".
    
    Closes #15689

--- core/src/test/java/org/elasticsearch/validate/SimpleValidateQueryIT.java
+++ core/src/test/java/org/elasticsearch/validate/SimpleValidateQueryIT.java
@@ -108,7 +108,7 @@ public class SimpleValidateQueryIT extends ESIntegTestCase {
         }
 
         for (Client client : internalCluster()) {
-                ValidateQueryResponse response = client.admin().indices().prepareValidateQuery("test")
+            ValidateQueryResponse response = client.admin().indices().prepareValidateQuery("test")
                     .setQuery(QueryBuilders.queryStringQuery("foo"))
                     .setExplain(true)
                     .execute().actionGet();

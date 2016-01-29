763b0a532349d66b5be5732ff994a972a1f7d859
Lee Hinman <lee@writequit.org>
2016-01-29 16:22:37 -0700

    Add Exception class name to message in `NotSerializableExceptionWrapper`
    Previously it's possible to get errors like:
    
    ```
    Caused by: NotSerializableExceptionWrapper[d:\shared_data\afs-issue-1-1\23]
    ```
    
    Which doesn't tell us what the underlying exception type was that could
    not be serialized.
    
    This changes the message to prepend the
    `ElasticsearchException.getExceptionName()` of the exception (which is a
    underscore case of the class with the leading 'Elasticsearch' removed)

--- core/src/test/java/org/elasticsearch/transport/AbstractSimpleTransportTestCase.java
+++ core/src/test/java/org/elasticsearch/transport/AbstractSimpleTransportTestCase.java
@@ -375,7 +375,7 @@ public abstract class AbstractSimpleTransportTestCase extends ESTestCase {
 
                     @Override
                     public void handleException(TransportException exp) {
-                        assertThat("bad message !!!", equalTo(exp.getCause().getMessage()));
+                        assertThat("runtime_exception: bad message !!!", equalTo(exp.getCause().getMessage()));
                     }
                 });
 
@@ -383,7 +383,7 @@ public abstract class AbstractSimpleTransportTestCase extends ESTestCase {
             res.txGet();
             fail("exception should be thrown");
         } catch (Exception e) {
-            assertThat(e.getCause().getMessage(), equalTo("bad message !!!"));
+            assertThat(e.getCause().getMessage(), equalTo("runtime_exception: bad message !!!"));
         }
 
         serviceA.removeHandler("sayHelloException");

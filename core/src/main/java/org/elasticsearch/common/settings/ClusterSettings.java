d13739fcb94e7da84d34ef779ff96ddce8781e97
Jason Tedor <jason@tedor.me>
2016-01-29 06:31:44 -0500

    OS monitor refresh interval setting
    This commit converts the OS monitor refresh interval setting
    "monitor.os.refresh_interval" to the new settings infrastructure.

--- core/src/main/java/org/elasticsearch/common/settings/ClusterSettings.java
+++ core/src/main/java/org/elasticsearch/common/settings/ClusterSettings.java
@@ -67,6 +67,7 @@ import org.elasticsearch.indices.fielddata.cache.IndicesFieldDataCache;
 import org.elasticsearch.indices.recovery.RecoverySettings;
 import org.elasticsearch.indices.store.IndicesStore;
 import org.elasticsearch.indices.ttl.IndicesTTLService;
+import org.elasticsearch.monitor.os.OsService;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.internal.InternalSettingsPreparer;
 import org.elasticsearch.repositories.fs.FsRepository;
@@ -317,5 +318,7 @@ public final class ClusterSettings extends AbstractScopedSettings {
                     ESLoggerFactory.LOG_LEVEL_SETTING,
                     NodeEnvironment.MAX_LOCAL_STORAGE_NODES_SETTING,
                     NodeEnvironment.ENABLE_LUCENE_SEGMENT_INFOS_TRACE_SETTING,
-                    NodeEnvironment.ADD_NODE_ID_TO_CUSTOM_PATH)));
+                    NodeEnvironment.ADD_NODE_ID_TO_CUSTOM_PATH,
+                    OsService.REFRESH_INTERVAL_SETTING
+                )));
 }

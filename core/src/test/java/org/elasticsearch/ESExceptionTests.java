3b8434c4d1a245923a5eab91739156dec20e4fc0
Lee Hinman <lee@writequit.org>
2016-01-29 16:22:37 -0700

    Add Exception class name to message in `NotSerializableExceptionWrapper`
    Previously it's possible to get errors like:
    
    ```
    Caused by: NotSerializableExceptionWrapper[d:\shared_data\afs-issue-1-1\23]
    ```
    
    Which doesn't tell us what the underlying exception type was that could
    not be serialized.
    
    This changes the message to prepend the
    `ElasticsearchException.getExceptionName()` of the exception (which is a
    underscore case of the class with the leading 'Elasticsearch' removed)

--- core/src/test/java/org/elasticsearch/ESExceptionTests.java
+++ core/src/test/java/org/elasticsearch/ESExceptionTests.java
@@ -276,12 +276,12 @@ public class ESExceptionTests extends ESTestCase {
     public void testSerializeUnknownException() throws IOException {
         BytesStreamOutput out = new BytesStreamOutput();
         QueryParsingException queryParsingException = new QueryParsingException(new Index("foo"), 1, 2, "foobar", null);
-        Throwable ex = new Throwable("wtf", queryParsingException);
+        Throwable ex = new Throwable("eggplant", queryParsingException);
         out.writeThrowable(ex);
 
         StreamInput in = StreamInput.wrap(out.bytes());
         Throwable throwable = in.readThrowable();
-        assertEquals("wtf", throwable.getMessage());
+        assertEquals("throwable: eggplant", throwable.getMessage());
         assertTrue(throwable instanceof ElasticsearchException);
         QueryParsingException e = (QueryParsingException)throwable.getCause();
                 assertEquals(queryParsingException.getIndex(), e.getIndex());
@@ -319,7 +319,9 @@ public class ESExceptionTests extends ESTestCase {
             StreamInput in = StreamInput.wrap(out.bytes());
             ElasticsearchException e = in.readThrowable();
             assertEquals(e.getMessage(), ex.getMessage());
-            assertEquals(ex.getCause().getClass().getName(), e.getCause().getMessage(), ex.getCause().getMessage());
+            assertTrue("Expected: " + e.getCause().getMessage() + " to contain: " +
+                            ex.getCause().getClass().getName() + " but it didn't",
+                    e.getCause().getMessage().contains(ex.getCause().getMessage()));
             if (ex.getCause().getClass() != Throwable.class) { // throwable is not directly mapped
                 assertEquals(e.getCause().getClass(), ex.getCause().getClass());
             } else {

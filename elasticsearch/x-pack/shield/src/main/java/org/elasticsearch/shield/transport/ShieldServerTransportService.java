8fe5afd8a744559716c9dd0d4c7d4374b2e7fc31
Nik Everett <nik9000@gmail.com>
2016-02-01 18:18:45 -0500

    Support task status
    This is the x-content buddy of https://github.com/elastic/elasticsearch/issues/16344
    
    Original commit: elastic/x-pack-elasticsearch@db216f7b468fb3ab790ebec04fcf8a50d11ae095

--- elasticsearch/x-pack/shield/src/main/java/org/elasticsearch/shield/transport/ShieldServerTransportService.java
+++ elasticsearch/x-pack/shield/src/main/java/org/elasticsearch/shield/transport/ShieldServerTransportService.java
@@ -7,6 +7,7 @@ package org.elasticsearch.shield.transport;
 
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.shield.action.ShieldActionMapper;
@@ -66,8 +67,9 @@ public class ShieldServerTransportService extends TransportService {
                                         AuthorizationService authzService,
                                         ShieldActionMapper actionMapper,
                                         ClientTransportFilter clientTransportFilter,
-                                        ShieldLicenseState licenseState) {
-        super(settings, transport, threadPool);
+                                        ShieldLicenseState licenseState,
+                                        NamedWriteableRegistry namedWriteableRegistry) {
+        super(settings, transport, threadPool, namedWriteableRegistry);
         this.authcService = authcService;
         this.authzService = authzService;
         this.actionMapper = actionMapper;

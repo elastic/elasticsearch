7ca2651a7bd16a349062addcb929cc3fb003111a
Nik Everett <nik9000@gmail.com>
2016-02-10 15:41:40 -0500

    Wrap another lone line

--- test/framework/src/main/java/org/elasticsearch/test/client/NoOpClient.java
+++ test/framework/src/main/java/org/elasticsearch/test/client/NoOpClient.java
@@ -38,7 +38,10 @@ public class NoOpClient extends AbstractClient {
     }
 
     @Override
-    protected <Request extends ActionRequest<Request>, Response extends ActionResponse, RequestBuilder extends ActionRequestBuilder<Request, Response, RequestBuilder>> void doExecute(Action<Request, Response, RequestBuilder> action, Request request, ActionListener<Response> listener) {
+    protected <Request extends ActionRequest<Request>,
+                    Response extends ActionResponse,
+                    RequestBuilder extends ActionRequestBuilder<Request, Response, RequestBuilder>>
+            void doExecute(Action<Request, Response, RequestBuilder> action, Request request, ActionListener<Response> listener) {
         listener.onResponse(null);
     }
 

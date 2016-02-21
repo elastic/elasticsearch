b5f40adb12891da1c747be3305cb88adc10adf3b
Tanguy Leroux <tlrx.dev@gmail.com>
2016-02-21 14:39:52 -0800

    Marvel: Add stats for primary shards
    closes elastic/elasticsearch#1198
    
    Original commit: elastic/x-pack-elasticsearch@e823d01397a70dd633f9cef03a827878f41d5bc8

--- elasticsearch/x-pack/marvel/src/test/java/org/elasticsearch/marvel/agent/renderer/indices/IndexStatsRendererTests.java
+++ elasticsearch/x-pack/marvel/src/test/java/org/elasticsearch/marvel/agent/renderer/indices/IndexStatsRendererTests.java
@@ -49,16 +49,23 @@ public class IndexStatsRendererTests extends ESTestCase {
                         return stats;
                     }
 
-                @Override
-                public CommonStats getPrimaries() {
-                    // Primaries will be filtered out by the renderer
-                    CommonStats stats = new CommonStats();
-                    stats.docs = new DocsStats(345678L, randomLong());
-                    stats.store = new StoreStats(randomLong(), randomLong());
-                    stats.indexing = new IndexingStats(new IndexingStats.Stats(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, true, randomLong()), null);
-                    return stats;
-                }
-            });
+                    @Override
+                    public CommonStats getPrimaries() {
+                        // Primaries will be filtered out by the renderer
+                        CommonStats stats = new CommonStats();
+                        stats.docs = new DocsStats(345678L, randomLong());
+                        stats.store = new StoreStats(randomLong(), randomLong());
+                        stats.indexing = new IndexingStats(new IndexingStats.Stats(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, true, randomLong()), null);
+                        stats.search = new SearchStats(new SearchStats.Stats(1L, 7L, 0L, 0L, 0L, 0L, 0L, 0L, 0L), 0L, null);
+                        stats.merge = new MergeStats();
+                        stats.merge.add(0L, 0L, 0L, 42L, 0L, 0L, 0L, 0L, 0L, 0L);
+                        stats.refresh = new RefreshStats(0L, 978L);
+                        stats.fieldData = new FieldDataStats(123456L, 0L, null);
+                        stats.segments = new SegmentsStats();
+                        stats.segments.add(0, 87965412L);
+                        return stats;
+                    }
+                });
 
         logger.debug("--> rendering the document");
         Renderer renderer = new IndexStatsRenderer();

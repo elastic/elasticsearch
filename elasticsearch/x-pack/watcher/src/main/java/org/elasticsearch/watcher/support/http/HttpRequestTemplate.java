47f1c2daa574b78cf1f3559665e8d2c5f30106af
Alexander Reelsen <alexander@reelsen.net>
2016-02-25 17:29:36 -0800

    Watcher: Throw exception when empty URL is handed in http requests
    This ensures that invalid watches are not even added and rejected on
    index time.
    
    Closes elastic/elasticsearch#1510
    
    Original commit: elastic/x-pack-elasticsearch@d18e0c8ef6a3841a5dda6c4c7182100173a28475

--- elasticsearch/x-pack/watcher/src/main/java/org/elasticsearch/watcher/support/http/HttpRequestTemplate.java
+++ elasticsearch/x-pack/watcher/src/main/java/org/elasticsearch/watcher/support/http/HttpRequestTemplate.java
@@ -248,6 +248,10 @@ public class HttpRequestTemplate implements ToXContent {
         return new Builder(host, port);
     }
 
+    static Builder builder() {
+        return new Builder();
+    }
+
     public static class Parser {
 
         private final HttpAuthRegistry httpAuthRegistry;
@@ -477,8 +481,15 @@ public class HttpRequestTemplate implements ToXContent {
         }
 
         public Builder fromUrl(String supposedUrl) {
+            if (Strings.hasLength(supposedUrl) == false) {
+                throw new ElasticsearchParseException("Configured URL is empty, please configure a valid URL");
+            }
+
             try {
                 URI uri = new URI(supposedUrl);
+                if (Strings.hasLength(uri.getScheme()) == false) {
+                    throw new ElasticsearchParseException("URL [{}] does not contain a scheme", uri);
+                }
                 scheme = Scheme.parse(uri.getScheme());
                 port = uri.getPort() > 0 ? uri.getPort() : scheme.defaultPort();
                 host = uri.getHost();
@@ -495,7 +506,7 @@ public class HttpRequestTemplate implements ToXContent {
                     }
                 }
             } catch (URISyntaxException e) {
-                throw new ElasticsearchParseException("Malformed URI [{}]", supposedUrl);
+                throw new ElasticsearchParseException("Malformed URL [{}]", supposedUrl);
             }
             return this;
         }

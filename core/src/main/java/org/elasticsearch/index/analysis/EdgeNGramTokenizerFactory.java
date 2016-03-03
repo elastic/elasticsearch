6837c6eabf96075f743649da9b9b52dd39611c58
Adrien Grand <jpountz@gmail.com>
2016-03-03 10:50:59 +0100

    Fix the edge ngram tokenizer/filter.

--- core/src/main/java/org/elasticsearch/index/analysis/EdgeNGramTokenizerFactory.java
+++ core/src/main/java/org/elasticsearch/index/analysis/EdgeNGramTokenizerFactory.java
@@ -21,9 +21,7 @@ package org.elasticsearch.index.analysis;
 
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.ngram.EdgeNGramTokenizer;
-import org.apache.lucene.analysis.ngram.Lucene43EdgeNGramTokenizer;
 import org.apache.lucene.analysis.ngram.NGramTokenizer;
-import org.apache.lucene.util.Version;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
@@ -33,55 +31,33 @@ import static org.elasticsearch.index.analysis.NGramTokenizerFactory.parseTokenC
 /**
  *
  */
-@SuppressWarnings("deprecation")
 public class EdgeNGramTokenizerFactory extends AbstractTokenizerFactory {
 
     private final int minGram;
 
     private final int maxGram;
 
-    private final Lucene43EdgeNGramTokenizer.Side side;
-
     private final CharMatcher matcher;
-    
-    protected org.elasticsearch.Version esVersion;
 
 
     public EdgeNGramTokenizerFactory(IndexSettings indexSettings, Environment environment, String name, Settings settings) {
         super(indexSettings, name, settings);
         this.minGram = settings.getAsInt("min_gram", NGramTokenizer.DEFAULT_MIN_NGRAM_SIZE);
         this.maxGram = settings.getAsInt("max_gram", NGramTokenizer.DEFAULT_MAX_NGRAM_SIZE);
-        this.side = Lucene43EdgeNGramTokenizer.Side.getSide(settings.get("side", Lucene43EdgeNGramTokenizer.DEFAULT_SIDE.getLabel()));
         this.matcher = parseTokenChars(settings.getAsArray("token_chars"));
-        this.esVersion = indexSettings.getIndexVersionCreated();
     }
 
     @Override
     public Tokenizer create() {
-        if (version.onOrAfter(Version.LUCENE_4_3) && esVersion.onOrAfter(org.elasticsearch.Version.V_0_90_2)) {
-            /*
-             * We added this in 0.90.2 but 0.90.1 used LUCENE_43 already so we can not rely on the lucene version.
-             * Yet if somebody uses 0.90.2 or higher with a prev. lucene version we should also use the deprecated version.
-             */
-            if (side == Lucene43EdgeNGramTokenizer.Side.BACK) {
-                throw new IllegalArgumentException("side=back is not supported anymore. Please fix your analysis chain or use"
-                        + " an older compatibility version (<=4.2) but beware that it might cause highlighting bugs." 
-                        + " To obtain the same behavior as the previous version please use \"edgeNGram\" filter which still supports side=back" 
-                        + " in combination with a \"keyword\" tokenizer");
-            }
-            final Version version = this.version == Version.LUCENE_4_3 ? Version.LUCENE_4_4 : this.version; // always use 4.4 or higher
-            if (matcher == null) {
-                return new EdgeNGramTokenizer(minGram, maxGram);
-            } else {
-                return new EdgeNGramTokenizer(minGram, maxGram) {
-                    @Override
-                    protected boolean isTokenChar(int chr) {
-                        return matcher.isTokenChar(chr);
-                    }
-                };
-            }
+        if (matcher == null) {
+            return new EdgeNGramTokenizer(minGram, maxGram);
         } else {
-            return new Lucene43EdgeNGramTokenizer(side, minGram, maxGram);
+            return new EdgeNGramTokenizer(minGram, maxGram) {
+                @Override
+                protected boolean isTokenChar(int chr) {
+                    return matcher.isTokenChar(chr);
+                }
+            };
         }
     }
 }

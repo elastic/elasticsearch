fbe0aefa0c6f5f4d4a501a783aff1cf0e69c4fd5
Martijn van Groningen <martijn.v.groningen@gmail.com>
2016-03-10 13:22:59 +0100

    fix compile erors due to upstream changes in node stats
    Original commit: elastic/x-pack-elasticsearch@a0a600cb688ca33eee3092c2c6f65548469ad66b

--- elasticsearch/x-pack/marvel/src/test/java/org/elasticsearch/marvel/agent/resolver/cluster/ClusterStatsResolverTests.java
+++ elasticsearch/x-pack/marvel/src/test/java/org/elasticsearch/marvel/agent/resolver/cluster/ClusterStatsResolverTests.java
@@ -129,7 +129,7 @@ public class ClusterStatsResolverTests extends MonitoringIndexNameResolverTestCa
         statsByShard.put(index, Collections.singletonList(new IndexShardStats(new ShardId(index, 0), randomShardStats())));
         return new NodeStats(new DiscoveryNode("node_0", DummyTransportAddress.INSTANCE, Version.CURRENT), 0,
                 new NodeIndicesStats(new CommonStats(), statsByShard), null, null, null, null,
-                new FsInfo(0, pathInfo), null, null, null, null, null);
+                new FsInfo(0, pathInfo), null, null, null, null, null, null);
     }
 
     /**

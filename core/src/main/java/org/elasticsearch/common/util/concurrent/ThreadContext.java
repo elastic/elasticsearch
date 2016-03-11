80f638b56a1c1551b391ce67ae381ca47ee8a641
Nik Everett <nik9000@gmail.com>
2016-03-11 13:00:47 -0500

    Support scheduled commands in current context
    Adds support for scheduling commands to run at a later time on another
    thread pool in the current thread's context:
    
    ```java
    Runnable someCommand = () -> {System.err.println("Demo");};
    someCommand = threadPool.getThreadContext().preserveContext(someCommand);
    threadPool.schedule(timeValueMinutes(1), Names.GENERAL, someCommand);
    ```
    
    This happens automatically for calls to `threadPool.execute` but `schedule`
    and `scheduleWithFixedDelay` don't do that, presumably because scheduled
    tasks are usually context-less. Rather than preserve the current context
    on all scheduled tasks this just makes it possible to preserve it using
    the syntax above.
    
    To make this all go it moves the Runnables that wrap the commands from
    EsThreadPoolExecutor into ThreadContext.
    
    This, or something like it, is required to support reindex throttling.

--- core/src/main/java/org/elasticsearch/common/util/concurrent/ThreadContext.java
+++ core/src/main/java/org/elasticsearch/common/util/concurrent/ThreadContext.java
@@ -200,6 +200,36 @@ public final class ThreadContext implements Closeable, Writeable<ThreadContext.T
         return (T) threadLocal.get().transientHeaders.get(key);
     }
 
+    /**
+     * Saves the current thread context and wraps command in a Runnable that restores that context before running command. If
+     * <code>command</code> has already been passed through this method then it is returned unaltered rather than wrapped twice.
+     */
+    public Runnable preserveContext(Runnable command) {
+        if (command instanceof ContextPreservingAbstractRunnable) {
+            return command;
+        }
+        if (command instanceof ContextPreservingRunnable) {
+            return command;
+        }
+        if (command instanceof AbstractRunnable) {
+            return new ContextPreservingAbstractRunnable((AbstractRunnable) command);
+        }
+        return new ContextPreservingRunnable(command);
+    }
+
+    /**
+     * Unwraps a command that was previously wrapped by {@link #preserveContext(Runnable)}.
+     */
+    public Runnable unwrap(Runnable command) {
+        if (command instanceof ContextPreservingAbstractRunnable) {
+            return ((ContextPreservingAbstractRunnable) command).unwrap();
+        }
+        if (command instanceof ContextPreservingRunnable) {
+            return ((ContextPreservingRunnable) command).unwrap();
+        }
+        return command;
+    }
+
     public interface StoredContext extends AutoCloseable {
         @Override
         void close();
@@ -354,6 +384,106 @@ public final class ThreadContext implements Closeable, Writeable<ThreadContext.T
             if (closed.compareAndSet(false, true)) {
                 super.close();
             }
+        }
+    }
+
+    /**
+     * Wraps a Runnable to preserve the thread context.
+     */
+    class ContextPreservingRunnable implements Runnable {
+        private final Runnable in;
+        private final ThreadContext.StoredContext ctx;
+
+        ContextPreservingRunnable(Runnable in) {
+            ctx = newStoredContext();
+            this.in = in;
+        }
+
+        @Override
+        public void run() {
+            boolean whileRunning = false;
+            try (ThreadContext.StoredContext ingore = stashContext()){
+                ctx.restore();
+                whileRunning = true;
+                in.run();
+                whileRunning = false;
+            } catch (IllegalStateException ex) {
+                if (whileRunning || threadLocal.closed.get() == false) {
+                    throw ex;
+                }
+                // if we hit an ISE here we have been shutting down
+                // this comes from the threadcontext and barfs if
+                // our threadpool has been shutting down
+            }
+        }
+
+        @Override
+        public String toString() {
+            return in.toString();
+        }
+
+        public Runnable unwrap() {
+            return in;
+        }
+    }
+
+    /**
+     * Wraps an AbstractRunnable to preserve the thread context.
+     */
+    public class ContextPreservingAbstractRunnable extends AbstractRunnable {
+        private final AbstractRunnable in;
+        private final ThreadContext.StoredContext ctx;
+
+        private ContextPreservingAbstractRunnable(AbstractRunnable in) {
+            ctx = newStoredContext();
+            this.in = in;
+        }
+
+        @Override
+        public boolean isForceExecution() {
+            return in.isForceExecution();
+        }
+
+        @Override
+        public void onAfter() {
+            in.onAfter();
+        }
+
+        @Override
+        public void onFailure(Throwable t) {
+            in.onFailure(t);
+        }
+
+        @Override
+        public void onRejection(Throwable t) {
+            in.onRejection(t);
+        }
+
+        @Override
+        protected void doRun() throws Exception {
+            boolean whileRunning = false;
+            try (ThreadContext.StoredContext ingore = stashContext()){
+                ctx.restore();
+                whileRunning = true;
+                in.doRun();
+                whileRunning = false;
+            } catch (IllegalStateException ex) {
+                if (whileRunning || threadLocal.closed.get() == false) {
+                    throw ex;
+                }
+                // if we hit an ISE here we have been shutting down
+                // this comes from the threadcontext and barfs if
+                // our threadpool has been shutting down
+            }
+        }
+
+        @Override
+        public String toString() {
+            return in.toString();
+        }
+
+        public AbstractRunnable unwrap() {
+            return in;
         }
     }
 }

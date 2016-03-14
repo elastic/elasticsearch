0ebcef0bb47154cf5582c9722490ea43d1d3345a
Simon Willnauer <simonw@apache.org>
2016-03-14 14:51:43 +0100

    wrap line after 140 chars

--- core/src/main/java/org/elasticsearch/action/admin/indices/mapping/put/PutMappingRequest.java
+++ core/src/main/java/org/elasticsearch/action/admin/indices/mapping/put/PutMappingRequest.java
@@ -94,7 +94,8 @@ public class PutMappingRequest extends AcknowledgedRequest<PutMappingRequest> im
             validationException = addValidationError("mapping source is empty", validationException);
         }
         if (concreteIndex != null && (indices != null && indices.length > 0)) {
-            validationException = addValidationError("either concreteIndices or unresolved indices can be set concrete: [" + concreteIndex + "] and indices: " + indices , validationException);
+            validationException = addValidationError("either concreteIndices or unresolved indices can be set concrete: [" + concreteIndex
+                + "] and indices: " + indices , validationException);
         }
         return validationException;
     }

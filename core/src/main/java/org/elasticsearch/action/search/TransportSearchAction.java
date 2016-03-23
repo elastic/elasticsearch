de78621c95b71485552c2171f5785b70ea7fc2ac
Areek Zillur <areek.zillur@elasticsearch.com>
2016-03-23 12:24:18 -0400

    simplify handling suggest-only search request

--- core/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
+++ core/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
@@ -37,8 +37,6 @@ import org.elasticsearch.transport.TransportService;
 import java.util.Map;
 import java.util.Set;
 
-import static org.elasticsearch.action.search.SearchType.DFS_QUERY_AND_FETCH;
-import static org.elasticsearch.action.search.SearchType.DFS_QUERY_THEN_FETCH;
 import static org.elasticsearch.action.search.SearchType.QUERY_AND_FETCH;
 import static org.elasticsearch.action.search.SearchType.QUERY_THEN_FETCH;
 
@@ -71,18 +69,20 @@ public class TransportSearchAction extends HandledTransportAction<SearchRequest,
             Map<String, Set<String>> routingMap = indexNameExpressionResolver.resolveSearchRouting(clusterState,
                     searchRequest.routing(), searchRequest.indices());
             int shardCount = clusterService.operationRouting().searchShardsCount(clusterState, concreteIndices, routingMap);
-            boolean hasOnlySuggest = searchRequest.hasOnlySuggest();
             if (shardCount == 1) {
                 // if we only have one group, then we always want Q_A_F, no need for DFS, and no need to do THEN since we hit one shard
                 searchRequest.searchType(QUERY_AND_FETCH);
-            } else if (hasOnlySuggest &&
-                (searchRequest.searchType() == DFS_QUERY_AND_FETCH || searchRequest.searchType() == DFS_QUERY_THEN_FETCH)) {
-                // convert to Q_T_F if we have only suggest
-                searchRequest.searchType(QUERY_THEN_FETCH);
             }
-            if (hasOnlySuggest && (searchRequest.requestCache() == null || searchRequest.requestCache())) {
+            if (searchRequest.isSuggestOnly()) {
                 // disable request cache if we have only suggest
                 searchRequest.requestCache(false);
+                switch (searchRequest.searchType()) {
+                    case DFS_QUERY_AND_FETCH:
+                    case DFS_QUERY_THEN_FETCH:
+                        // convert to Q_T_F if we have only suggest
+                        searchRequest.searchType(QUERY_THEN_FETCH);
+                        break;
+                }
             }
         } catch (IndexNotFoundException | IndexClosedException e) {
             // ignore these failures, we will notify the search response if its really the case from the actual action

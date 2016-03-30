390d1352b4b2c355c38caa7c71a3fb106cc5d234
Clinton Gormley <clint@traveljury.com>
2016-03-30 18:51:29 +0200

    Bumped version to 2.3.1-SNAPSHOT and added bwc indices for 2.3.0 and 2.2.2

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -288,11 +288,15 @@ public class Version {
     public static final int V_2_2_1_ID = 2020199;
     public static final Version V_2_2_1 = new Version(V_2_2_1_ID, false, org.apache.lucene.util.Version.LUCENE_5_4_1);
     public static final int V_2_2_2_ID = 2020299;
-    public static final Version V_2_2_2 = new Version(V_2_2_2_ID, true, org.apache.lucene.util.Version.LUCENE_5_4_1);
+    public static final Version V_2_2_2 = new Version(V_2_2_2_ID, false, org.apache.lucene.util.Version.LUCENE_5_4_1);
+    public static final int V_2_2_3_ID = 2020399;
+    public static final Version V_2_2_3 = new Version(V_2_2_3_ID, true, org.apache.lucene.util.Version.LUCENE_5_4_1);
     public static final int V_2_3_0_ID = 2030099;
-    public static final Version V_2_3_0 = new Version(V_2_3_0_ID, true, org.apache.lucene.util.Version.LUCENE_5_5_0);
+    public static final Version V_2_3_0 = new Version(V_2_3_0_ID, false, org.apache.lucene.util.Version.LUCENE_5_5_0);
+    public static final int V_2_3_1_ID = 2030199;
+    public static final Version V_2_3_1 = new Version(V_2_3_1_ID, true, org.apache.lucene.util.Version.LUCENE_5_5_0);
 
-    public static final Version CURRENT = V_2_3_0;
+    public static final Version CURRENT = V_2_3_1;
 
     static {
         assert CURRENT.luceneVersion.equals(Lucene.VERSION) : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
@@ -304,8 +308,12 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_2_3_1_ID:
+                return V_2_3_1;
             case V_2_3_0_ID:
                 return V_2_3_0;
+            case V_2_2_3_ID:
+                return V_2_2_3;
             case V_2_2_2_ID:
                 return V_2_2_2;
             case V_2_2_1_ID:

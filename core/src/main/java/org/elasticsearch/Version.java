ecca2e37a8f2ab0346362fb9bf2ce6fa1e338019
Clinton Gormley <clint@traveljury.com>
2016-04-04 17:39:44 +0200

    Bumped current version to 2.3.2-SNAPSHOT and added bwc indices for 2.3.1

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -294,9 +294,11 @@ public class Version {
     public static final int V_2_3_0_ID = 2030099;
     public static final Version V_2_3_0 = new Version(V_2_3_0_ID, false, org.apache.lucene.util.Version.LUCENE_5_5_0);
     public static final int V_2_3_1_ID = 2030199;
-    public static final Version V_2_3_1 = new Version(V_2_3_1_ID, true, org.apache.lucene.util.Version.LUCENE_5_5_0);
+    public static final Version V_2_3_1 = new Version(V_2_3_1_ID, false, org.apache.lucene.util.Version.LUCENE_5_5_0);
+    public static final int V_2_3_2_ID = 2030299;
+    public static final Version V_2_3_2 = new Version(V_2_3_2_ID, true, org.apache.lucene.util.Version.LUCENE_5_5_0);
 
-    public static final Version CURRENT = V_2_3_1;
+    public static final Version CURRENT = V_2_3_2;
 
     static {
         assert CURRENT.luceneVersion.equals(Lucene.VERSION) : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
@@ -308,6 +310,8 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_2_3_2_ID:
+                return V_2_3_2;
             case V_2_3_1_ID:
                 return V_2_3_1;
             case V_2_3_0_ID:

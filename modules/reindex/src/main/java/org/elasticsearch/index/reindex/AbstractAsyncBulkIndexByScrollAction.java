c2e745bf3ba2eaf6ca4eb540594b97662b68d70f
Nik Everett <nik9000@gmail.com>
2016-04-12 07:32:29 -0400

    reindex: Guard against user disabling fields

--- modules/reindex/src/main/java/org/elasticsearch/index/reindex/AbstractAsyncBulkIndexByScrollAction.java
+++ modules/reindex/src/main/java/org/elasticsearch/index/reindex/AbstractAsyncBulkIndexByScrollAction.java
@@ -87,6 +87,11 @@ public abstract class AbstractAsyncBulkIndexByScrollAction<
 
         for (SearchHit doc : docs) {
             if (doc.isSourceEmpty()) {
+                /*
+                 * Either the document didn't store _source or we didn't fetch it for some reason. Since we don't allow the user to
+                 * change the "fields" part of the search request it is unlikely that we got here because we didn't fetch _source.
+                 * Thus the error message assumes that it wasn't stored.
+                 */
                 throw new IllegalArgumentException("[" + doc.index() + "][" + doc.type() + "][" + doc.id() + "] didn't store _source");
             }
             IndexRequest index = buildIndexRequest(doc);

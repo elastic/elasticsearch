2b1c191b121c1e5ecdec329639625bb701beb7b1
Jason Tedor <jason@tedor.me>
2016-04-12 12:21:40 -0400

    Add comments to JVM options parsing for Windows
    This commit adds some comments to the portion of bin/elasticsearch.bat
    that is responsible for parsing the JVM options.

--- distribution/src/main/resources/bin/elasticsearch.bat
+++ distribution/src/main/resources/bin/elasticsearch.bat
@@ -36,10 +36,13 @@ FOR /F "usebackq tokens=1* delims= " %%A IN (!params!) DO (
 SET HOSTNAME=%COMPUTERNAME%
 
 if "%ES_JVM_OPTIONS%" == "" (
+rem '0' is the batch file, '~dp' appends the drive and path
 set ES_JVM_OPTIONS="%~dp0\..\config\jvm.options"
 )
 
 @setlocal
+rem extract the options from the JVM options file %ES_JVM_OPTIONS%
+rem such options are the lines beginning with '-', thus "findstr /b"
 for /F "usebackq delims=" %%a in (`findstr /b \- "%ES_JVM_OPTIONS%"`) do set JVM_OPTIONS=!JVM_OPTIONS! %%a
 @endlocal & set ES_JAVA_OPTS=%JVM_OPTIONS% %ES_JAVA_OPTS%
 

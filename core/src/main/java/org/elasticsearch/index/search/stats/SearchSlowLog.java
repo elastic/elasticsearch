a265786531b66f45dd4f747504308cc8d3ee2f0c
Jim Ferenczi <jim.ferenczi@elastic.co>
2016-04-18 12:13:10 +0200

    Add missing index name to search slow log. This commits adds the index name as part of the logging message. Closes #17025 Backport of #17818

--- core/src/main/java/org/elasticsearch/index/search/stats/SearchSlowLog.java
+++ core/src/main/java/org/elasticsearch/index/search/stats/SearchSlowLog.java
@@ -159,7 +159,7 @@ public final class SearchSlowLog{
         }
     }
 
-    private static class SlowLogSearchContextPrinter {
+    static class SlowLogSearchContextPrinter {
         private final SearchContext context;
         private final long tookInNanos;
         private final boolean reformat;
@@ -173,6 +173,7 @@ public final class SearchSlowLog{
         @Override
         public String toString() {
             StringBuilder sb = new StringBuilder();
+            sb.append(context.indexShard().indexService().index());
             sb.append("took[").append(TimeValue.timeValueNanos(tookInNanos)).append("], took_millis[").append(TimeUnit.NANOSECONDS.toMillis(tookInNanos)).append("], ");
             if (context.types() == null) {
                 sb.append("types[], ");

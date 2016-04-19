cdf4778483e41f020ecd2879fb213170b9abcfa1
Nik Everett <nik9000@gmail.com>
2016-04-19 18:15:15 -0400

    Fix javadoc

--- core/src/main/java/org/elasticsearch/action/admin/cluster/node/tasks/cancel/TransportCancelTasksAction.java
+++ core/src/main/java/org/elasticsearch/action/admin/cluster/node/tasks/cancel/TransportCancelTasksAction.java
@@ -56,7 +56,7 @@ import java.util.function.Consumer;
  * Transport action that can be used to cancel currently running cancellable tasks.
  * <p>
  * For a task to be cancellable it has to return an instance of
- * {@link CancellableTask} from {@link TransportRequest#createTask(long, String, String)}
+ * {@link CancellableTask} from {@link TransportRequest#createTask(long, String, String, TaskId)}
  */
 public class TransportCancelTasksAction extends TransportTasksAction<CancellableTask, CancelTasksRequest, CancelTasksResponse, TaskInfo> {
 

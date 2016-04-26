23787d5dd144ba7083783bcfd3ee564e2e7e9587
Clinton Gormley <clint@traveljury.com>
2016-04-26 13:51:37 +0200

    Added 2.3.3-SNAPSHOT and bwc indices for 2.3.2

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -296,7 +296,9 @@ public class Version {
     public static final int V_2_3_1_ID = 2030199;
     public static final Version V_2_3_1 = new Version(V_2_3_1_ID, false, org.apache.lucene.util.Version.LUCENE_5_5_0);
     public static final int V_2_3_2_ID = 2030299;
-    public static final Version V_2_3_2 = new Version(V_2_3_2_ID, true, org.apache.lucene.util.Version.LUCENE_5_5_0);
+    public static final Version V_2_3_2 = new Version(V_2_3_2_ID, false, org.apache.lucene.util.Version.LUCENE_5_5_0);
+    public static final int V_2_3_3_ID = 2030399;
+    public static final Version V_2_3_3 = new Version(V_2_3_3_ID, true, org.apache.lucene.util.Version.LUCENE_5_5_0);
     public static final int V_2_4_0_ID = 2040099;
     public static final Version V_2_4_0 = new Version(V_2_4_0_ID, true, org.apache.lucene.util.Version.LUCENE_5_5_0);
 
@@ -314,6 +316,8 @@ public class Version {
         switch (id) {
             case V_2_4_0_ID:
                 return V_2_4_0;
+            case V_2_3_3_ID:
+                return V_2_3_3;
             case V_2_3_2_ID:
                 return V_2_3_2;
             case V_2_3_1_ID:

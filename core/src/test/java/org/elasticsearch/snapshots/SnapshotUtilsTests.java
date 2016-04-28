8ae751b9c08c87f6d5ce7a624e21814843955c12
Mathias Fußenegger <mfussenegger@users.noreply.github.com>
2016-04-28 14:10:01 +0200

    Handle indices=["_all"] when restoring a snapshot (#18025)
    Setting indices=["_all"] when restoring a snapshot would throw an IndexNotFoundException instead of selecting all indices as in other APIs.

--- core/src/test/java/org/elasticsearch/snapshots/SnapshotUtilsTests.java
+++ core/src/test/java/org/elasticsearch/snapshots/SnapshotUtilsTests.java
@@ -34,6 +34,7 @@ public class SnapshotUtilsTests extends ESTestCase {
     public void testIndexNameFiltering() {
         assertIndexNameFiltering(new String[]{"foo", "bar", "baz"}, new String[]{}, new String[]{"foo", "bar", "baz"});
         assertIndexNameFiltering(new String[]{"foo", "bar", "baz"}, new String[]{"*"}, new String[]{"foo", "bar", "baz"});
+        assertIndexNameFiltering(new String[]{"foo", "bar", "baz"}, new String[]{"_all"}, new String[]{"foo", "bar", "baz"});
         assertIndexNameFiltering(new String[]{"foo", "bar", "baz"}, new String[]{"foo", "bar", "baz"}, new String[]{"foo", "bar", "baz"});
         assertIndexNameFiltering(new String[]{"foo", "bar", "baz"}, new String[]{"foo"}, new String[]{"foo"});
         assertIndexNameFiltering(new String[]{"foo", "bar", "baz"}, new String[]{"baz", "not_available"}, new String[]{"baz"});

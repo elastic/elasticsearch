2f43179ddae5029324aaa7514ad0b3bc46baa21c
Lee Hinman <lee@writequit.org>
2016-05-13 10:29:35 -0600

    Fix compilation for only one script language extension
    Relates to https://github.com/elastic/elasticsearch/pull/18332
    
    Original commit: elastic/x-pack-elasticsearch@32f0bd6c4fdfc65227827c92faf3ae9dc450a9e8

--- elasticsearch/x-pack/watcher/src/test/java/org/elasticsearch/script/SleepScriptEngine.java
+++ elasticsearch/x-pack/watcher/src/test/java/org/elasticsearch/script/SleepScriptEngine.java
@@ -22,8 +22,6 @@ public class SleepScriptEngine implements ScriptEngineService {
 
     public static final String NAME = "sleep";
 
-    public static final List<String> TYPES = Collections.singletonList(NAME);
-
     public static class TestPlugin extends Plugin {
 
         public TestPlugin() {
@@ -52,8 +50,8 @@ public class SleepScriptEngine implements ScriptEngineService {
     }
 
     @Override
-    public List<String> getExtensions() {
-        return TYPES;
+    public String getExtension() {
+        return NAME;
     }
 
     @Override

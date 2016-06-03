6c28235b03fdad898986f613e198c24ca3067e4d
Simon Willnauer <simon.willnauer@elasticsearch.com>
2016-06-03 13:42:09 +0200

    Fix merge stats rendering in RestIndicesAction (#18720)
    give the table description:
    ```
            table.addCell("merges.total", "sibling:pri;alias:mt,mergesTotal;default:false;text-align:right;desc:number of completed merge ops");
            table.addCell("pri.merges.total", "default:false;text-align:right;desc:number of completed merge ops");
    
            table.addCell("merges.total_docs", "sibling:pri;alias:mtd,mergesTotalDocs;default:false;text-align:right;desc:docs merged");
            table.addCell("pri.merges.total_docs", "default:false;text-align:right;desc:docs merged");
    
            table.addCell("merges.total_size", "sibling:pri;alias:mts,mergesTotalSize;default:false;text-align:right;desc:size merged");
            table.addCell("pri.merges.total_size", "default:false;text-align:right;desc:size merged");
    ```
    this is how it should be.

--- core/src/main/java/org/elasticsearch/rest/action/cat/RestIndicesAction.java
+++ core/src/main/java/org/elasticsearch/rest/action/cat/RestIndicesAction.java
@@ -412,13 +412,13 @@ public class RestIndicesAction extends AbstractCatAction {
             table.addCell(indexStats == null ? null : indexStats.getPrimaries().getIndexing().getTotal().getIndexFailedCount());
 
             table.addCell(indexStats == null ? null : indexStats.getTotal().getMerge().getCurrent());
-            table.addCell(indexStats == null ? null : indexStats.getPrimaries().getMerge().getCurrentSize());
+            table.addCell(indexStats == null ? null : indexStats.getPrimaries().getMerge().getCurrent());
 
             table.addCell(indexStats == null ? null : indexStats.getTotal().getMerge().getCurrentNumDocs());
             table.addCell(indexStats == null ? null : indexStats.getPrimaries().getMerge().getCurrentNumDocs());
 
             table.addCell(indexStats == null ? null : indexStats.getTotal().getMerge().getCurrentSize());
-            table.addCell(indexStats == null ? null : indexStats.getPrimaries().getMerge().getCurrent());
+            table.addCell(indexStats == null ? null : indexStats.getPrimaries().getMerge().getCurrentSize());
 
             table.addCell(indexStats == null ? null : indexStats.getTotal().getMerge().getTotal());
             table.addCell(indexStats == null ? null : indexStats.getPrimaries().getMerge().getTotal());

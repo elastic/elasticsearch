5d1d7c9082914d129859dc3194411c05c864449e
Stefan Scherer <scherer_stefan@icloud.com>
2016-06-03 19:01:58 +0200

    Remove cont label

--- distribution/src/main/resources/bin/elasticsearch.in.bat
+++ distribution/src/main/resources/bin/elasticsearch.in.bat
@@ -5,12 +5,11 @@ IF DEFINED JAVA_HOME (
 ) ELSE (
   FOR %%I IN (java.exe) DO set JAVA=%%~$PATH:I
 )
-IF EXIST "%JAVA%" GOTO cont
+IF NOT EXIST "%JAVA%" (
+  ECHO Could not find any executable java binary. Please install java in your PATH or set JAVA_HOME 1>&2
+  EXIT /B 1
+)
 
-ECHO Could not find any executable java binary. Please install java in your PATH or set JAVA_HOME 1>&2
-EXIT /B 1
-
-:cont
 set SCRIPT_DIR=%~dp0
 for %%I in ("%SCRIPT_DIR%..") do set ES_HOME=%%~dpfI
 

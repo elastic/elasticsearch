ab457be9d80aa147259cebc3e83a7ada425b74c2
Robert Muir <rmuir@apache.org>
2016-06-11 13:48:04 -0400

    remove unnecessary semicolon and return

--- modules/lang-painless/src/test/java/org/elasticsearch/painless/FunctionRefTests.java
+++ modules/lang-painless/src/test/java/org/elasticsearch/painless/FunctionRefTests.java
@@ -112,13 +112,13 @@ public class FunctionRefTests extends ScriptTestCase {
     public void testOwnStaticMethodReference() {
         //System.out.println(Debugger.toString("int mycompare(int i, int j) { return j - i; } " +
         //                     "List l = new ArrayList(); l.add(2); l.add(1); l.sort(this::mycompare); return l.get(0);"));
-        assertEquals(2, exec("int mycompare(int i, int j) { return j - i; } " +
+        assertEquals(2, exec("int mycompare(int i, int j) { j - i } " +
                              "List l = new ArrayList(); l.add(2); l.add(1); l.sort(this::mycompare); return l.get(0);"));
     }
     
     @AwaitsFix(bugUrl = "working on it")
     public void testOwnStaticMethodReferenceDef() {
-        assertEquals(2, exec("int mycompare(int i, int j) { return j - i; } " +
+        assertEquals(2, exec("int mycompare(int i, int j) { j - i } " +
                              "def l = new ArrayList(); l.add(2); l.add(1); l.sort(this::mycompare); return l.get(0);"));
     }
 

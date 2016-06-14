0c6938ba912b86857c268fdbe500901d4da8e106
Isabel Drost-Fromm <isabel.drostfromm@elasticsearch.com>
2016-06-14 11:38:59 +0200

    Example for request json.

--- modules/rank-eval/src/main/java/org/elasticsearch/index/rankeval/RestRankEvalAction.java
+++ modules/rank-eval/src/main/java/org/elasticsearch/index/rankeval/RestRankEvalAction.java
@@ -19,6 +19,122 @@
 
 package org.elasticsearch.index.rankeval;
 
-public class RestRankEvalAction {
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.rest.BaseRestHandler;
+import org.elasticsearch.rest.RestChannel;
+import org.elasticsearch.rest.RestRequest;
+
+/**
+ * Accepted input format:
+ * 
+ * General Format:
+ * 
+ * 
+  {
+    "user_requests": [{
+        "id": "human_readable_id",
+        "request_params": { ... map of template params ... },
+        "doc_ratings": { ... mapping from doc id to rating value ... }
+     }],
+
+    "translation_spec": {
+        "id": "human_readable_id",
+        "request_template": {... query template to evaluate ...}
+    },
+    "evaluation_metric": {
+        "... metric_name... ": {
+            "... metric_parameter_key ...": ...metric_parameter_value...
+        }
+    }
+  }
+
+ * 
+ * Example: 
+ * 
+ * 
+   {
+    "user_requests": [{
+        "id": "amsterdam_query",
+        "request_params": {
+            "querystring": "coffee",
+            "browser": "chrome",
+            "time_of_day": "morning",
+            "ip_location": "ams"
+        },
+        "doc_ratings": {
+            "1": 1,
+            "2": 0,
+            "3": 1,
+            "4": 1
+        }
+    }, {
+        "id": "berlin_query",
+        "request_params": {
+            "querystring": "club mate",
+            "browser": "safari",
+            "time_of_day": "evening",
+            "ip_location": "ber"
+        },
+        "doc_ratings": {
+            "1": 0,
+            "5": 1,
+            "6": 1
+        }
+    }],
+
+    "translation_spec": {
+        "id": "huge_weight_on_city",
+        "request_template": {
+            "inline": {
+                "query": {
+                    "bool": {
+                        "must": [{
+                            "match": {
+                                "beverage": "{{querystring}}"
+                            }
+                        }, {
+                            "term": {
+                                "browser": {
+                                    "value": "{{browser}}"
+                                }
+                            }
+                        }, {
+                            "term": {
+                                "time_of_day": {
+                                    "value": "{{time_of_day}}",
+                                    "boost": 2
+                                }
+                            }
+                        }, {
+                            "term": {
+                                "ip_location": {
+                                    "value": "{{ip_location}}",
+                                    "boost": 10
+                                }
+                            }
+                        }]
+                    }
+                },
+                "size": 10
+            }
+        }
+    },
+    "evaluation_metric": {
+        "precisionAtN": {
+            "size": 10
+        }
+    }
+  } 
+ * */
+public class RestRankEvalAction extends BaseRestHandler {
+
+    protected RestRankEvalAction(Settings settings, Client client) {
+        super(settings, client);
+    }
+
+    @Override
+    protected void handleRequest(RestRequest request, RestChannel channel, Client client) throws Exception {
+    }
 
 }

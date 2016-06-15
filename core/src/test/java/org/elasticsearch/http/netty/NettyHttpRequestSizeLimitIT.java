7ef13cd4f306b3bb65b23f2948aa2d7bfbfd390e
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-06-15 18:02:31 +0200

    Mute NHRSLIT while investigating

--- core/src/test/java/org/elasticsearch/http/netty/NettyHttpRequestSizeLimitIT.java
+++ core/src/test/java/org/elasticsearch/http/netty/NettyHttpRequestSizeLimitIT.java
@@ -92,6 +92,7 @@ public class NettyHttpRequestSizeLimitIT extends ESIntegTestCase {
         }
     }
 
+    @AwaitsFix(bugUrl = "muted while investigating")
     public void testDoesNotLimitExcludedRequests() throws Exception {
         ensureGreen();
 

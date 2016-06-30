66e3b15d21e183d652cce79c38301feedf7eb0a3
David Pilato <david@pilato.fr>
2016-06-30 11:12:20 +0200

    Fix NPE when GCE region is empty
    When GCE region is empty we get back from the API something like:
    
    ```
    {
      "id": "dummy"
    }
    ```
    
    instead of:
    
    ```
    {
      "id": "dummy",
      "items":[ ]
    }
    ```
    
    This generates a NPE when we aggregate all the lists into a single one.
    
    Closes #16967.

--- plugins/discovery-gce/src/test/java/org/elasticsearch/discovery/gce/GceDiscoveryTests.java
+++ plugins/discovery-gce/src/test/java/org/elasticsearch/discovery/gce/GceDiscoveryTests.java
@@ -254,4 +254,17 @@ public class GceDiscoveryTests extends ESTestCase {
             assertThat(expected.getMessage(), containsString("one or more gce discovery settings are missing."));
         }
     }
+
+    /**
+     * For issue https://github.com/elastic/elasticsearch/issues/16967
+     */
+    public void testEmptyRegion16967() {
+        Settings nodeSettings = Settings.builder()
+            .put(GceComputeService.PROJECT_SETTING.getKey(), projectName)
+            .putArray(GceComputeService.ZONE_SETTING.getKey(), "europe-west1-b", "us-central1-a")
+            .build();
+        mock = new GceComputeServiceMock(nodeSettings, networkService);
+        List<DiscoveryNode> discoveryNodes = buildDynamicNodes(mock, nodeSettings);
+        assertThat(discoveryNodes, hasSize(1));
+    }
 }

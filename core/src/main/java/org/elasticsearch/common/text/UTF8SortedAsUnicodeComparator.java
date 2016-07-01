99c1d54bc562b37feaebd11287bdce25c445ae76
Simon Willnauer <simonw@apache.org>
2016-07-01 09:17:40 +0200

    make args final

--- core/src/main/java/org/elasticsearch/common/text/UTF8SortedAsUnicodeComparator.java
+++ core/src/main/java/org/elasticsearch/common/text/UTF8SortedAsUnicodeComparator.java
@@ -76,7 +76,7 @@ public final class UTF8SortedAsUnicodeComparator implements Comparator<BytesRefe
         }
     }
 
-    private static final void advance(BytesRef ref, int length) {
+    private static final void advance(final BytesRef ref, final int length) {
         assert ref.length >= length : " ref.length: " + ref.length + " length: " + length;
         assert ref.offset+length < ref.bytes.length || (ref.offset+length == ref.bytes.length && ref.length-length == 0)
             : "offset: " + ref.offset + " ref.bytes.length: " + ref.bytes.length + " length: " + length + " ref.length: " + ref.length;

9dab9eb76256d2a3b1fd83e180ec62c2991782fa
Simon Willnauer <simonw@apache.org>
2016-07-11 12:16:53 +0200

    Followup from elastic/elasticsearchelastic/elasticsearch#19332
    Added missing IOException and simplified filtering out mock transports
    for security integration tests.
    
    Relates to elastic/elasticseachelastic/elasticsearch#19332
    
    Original commit: elastic/x-pack-elasticsearch@8a5c015c3d6041100f46b00b795a77fdcdfd8650

--- elasticsearch/x-pack/security/src/test/java/org/elasticsearch/test/SecurityIntegTestCase.java
+++ elasticsearch/x-pack/security/src/test/java/org/elasticsearch/test/SecurityIntegTestCase.java
@@ -16,6 +16,7 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.transport.MockTcpTransportPlugin;
 import org.elasticsearch.xpack.security.InternalClient;
 import org.elasticsearch.xpack.security.Security;
 import org.elasticsearch.xpack.security.authc.support.SecuredString;
@@ -177,11 +178,8 @@ public abstract class SecurityIntegTestCase extends ESIntegTestCase {
     }
 
     @Override
-    protected Collection<Class<? extends Plugin>> getMockPlugins() {
-        Set<Class<? extends Plugin>> plugins = new HashSet<>(super.getMockPlugins());
-        plugins.remove(MockTransportService.TestPlugin.class); // security has its own transport service
-        plugins.remove(AssertingLocalTransport.TestPlugin.class); // security has its own transport
-        return plugins;
+    protected boolean addMockTransportService() {
+        return false; // security has its own transport service
     }
 
     @Override
@@ -353,10 +351,6 @@ public abstract class SecurityIntegTestCase extends ESIntegTestCase {
 
     protected InternalClient internalClient() {
         return internalCluster().getInstance(InternalClient.class);
-    }
-
-    protected InternalClient internalClient(String node) {
-        return internalCluster().getInstance(InternalClient.class, node);
     }
 
     protected SecurityClient securityClient() {

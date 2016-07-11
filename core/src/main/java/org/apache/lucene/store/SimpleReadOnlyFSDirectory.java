08428b42c2be11ff5cef3de4b0ff96ba09c6121d
Yannick Welsch <yannick@welsch.lu>
2016-07-11 09:43:47 +0200

    Add copy of SimpleFSIndexInput

--- core/src/main/java/org/apache/lucene/store/SimpleReadOnlyFSDirectory.java
+++ core/src/main/java/org/apache/lucene/store/SimpleReadOnlyFSDirectory.java
@@ -18,7 +18,9 @@
  */
 package org.apache.lucene.store;
 
+import java.io.EOFException;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -80,11 +82,136 @@ public class SimpleReadOnlyFSDirectory extends BaseDirectory {
         ensureOpen();
         Path path = directory.resolve(name);
         SeekableByteChannel channel = Files.newByteChannel(path, StandardOpenOption.READ);
-        return new SimpleFSDirectory.SimpleFSIndexInput("SimpleFSIndexInput(path=\"" + path + "\")", channel, context);
+        return new SimpleFSIndexInput("SimpleFSIndexInput(path=\"" + path + "\")", channel, context);
     }
 
     @Override
     public void close() throws IOException {
         isOpen = false;
+    }
+
+    /**
+     * Copy of SimpleFSDirectory.SimpleFSIndexInput which is package-private class.
+     *
+     * Reads bytes with {@link SeekableByteChannel#read(ByteBuffer)}
+     */
+    static final class SimpleFSIndexInput extends BufferedIndexInput {
+        /**
+         * The maximum chunk size for reads of 16384 bytes.
+         */
+        private static final int CHUNK_SIZE = 16384;
+
+        /** the channel we will read from */
+        protected final SeekableByteChannel channel;
+        /** is this instance a clone and hence does not own the file to close it */
+        boolean isClone = false;
+        /** start offset: non-zero in the slice case */
+        protected final long off;
+        /** end offset (start+length) */
+        protected final long end;
+
+        private ByteBuffer byteBuf; // wraps the buffer for NIO
+
+        public SimpleFSIndexInput(String resourceDesc, SeekableByteChannel channel, IOContext context) throws IOException {
+            super(resourceDesc, context);
+            this.channel = channel;
+            this.off = 0L;
+            this.end = channel.size();
+        }
+
+        public SimpleFSIndexInput(String resourceDesc, SeekableByteChannel channel, long off, long length, int bufferSize) {
+            super(resourceDesc, bufferSize);
+            this.channel = channel;
+            this.off = off;
+            this.end = off + length;
+            this.isClone = true;
+        }
+
+        @Override
+        public void close() throws IOException {
+            if (!isClone) {
+                channel.close();
+            }
+        }
+
+        @Override
+        public SimpleFSIndexInput clone() {
+            SimpleFSIndexInput clone = (SimpleFSIndexInput)super.clone();
+            clone.isClone = true;
+            return clone;
+        }
+
+        @Override
+        public IndexInput slice(String sliceDescription, long offset, long length) throws IOException {
+            if (offset < 0 || length < 0 || offset + length > this.length()) {
+                throw new IllegalArgumentException("slice() " + sliceDescription + " out of bounds: offset=" + offset + ",length=" +
+                    length + ",fileLength="  + this.length() + ": "  + this);
+            }
+            return new SimpleFSIndexInput(getFullSliceDescription(sliceDescription), channel, off + offset, length, getBufferSize());
+        }
+
+        @Override
+        public final long length() {
+            return end - off;
+        }
+
+        @Override
+        protected void newBuffer(byte[] newBuffer) {
+            super.newBuffer(newBuffer);
+            byteBuf = ByteBuffer.wrap(newBuffer);
+        }
+
+        @Override
+        protected void readInternal(byte[] b, int offset, int len) throws IOException {
+            final ByteBuffer bb;
+
+            // Determine the ByteBuffer we should use
+            if (b == buffer) {
+                // Use our own pre-wrapped byteBuf:
+                assert byteBuf != null;
+                bb = byteBuf;
+                byteBuf.clear().position(offset);
+            } else {
+                bb = ByteBuffer.wrap(b, offset, len);
+            }
+
+            synchronized(channel) {
+                long pos = getFilePointer() + off;
+
+                if (pos + len > end) {
+                    throw new EOFException("read past EOF: " + this);
+                }
+
+                try {
+                    channel.position(pos);
+
+                    int readLength = len;
+                    while (readLength > 0) {
+                        final int toRead = Math.min(CHUNK_SIZE, readLength);
+                        bb.limit(bb.position() + toRead);
+                        assert bb.remaining() == toRead;
+                        final int i = channel.read(bb);
+                        if (i < 0) { // be defensive here, even though we checked before hand, something could have changed
+                            throw new EOFException("read past EOF: " + this + " off: " + offset + " len: " + len + " pos: " + pos +
+                                " chunkLen: " + toRead + " end: " + end);
+                        }
+                        assert i > 0 : "SeekableByteChannel.read with non zero-length bb.remaining() must always read at least one byte " +
+                            "(Channel is in blocking mode, see spec of ReadableByteChannel)";
+                        pos += i;
+                        readLength -= i;
+                    }
+                    assert readLength == 0;
+                } catch (IOException ioe) {
+                    throw new IOException(ioe.getMessage() + ": " + this, ioe);
+                }
+            }
+        }
+
+        @Override
+        protected void seekInternal(long pos) throws IOException {
+            if (pos > length()) {
+                throw new EOFException("read past EOF: pos=" + pos + " vs length=" + length() + ": " + this);
+            }
+        }
     }
 }

8e0da2602df3477cfb1e4c22c8a787b919f18e68
Ryan Ernst <ryan@iernst.net>
2016-07-25 20:12:42 -0700

    Fix license ack tests to ensure license change always results in ack messages
    Original commit: elastic/x-pack-elasticsearch@25029eb83ed171803103d028e1bf274223ebc9a0

--- elasticsearch/x-pack/license-plugin/src/test/java/org/elasticsearch/license/plugin/core/LicensesAcknowledgementTests.java
+++ elasticsearch/x-pack/license-plugin/src/test/java/org/elasticsearch/license/plugin/core/LicensesAcknowledgementTests.java
@@ -27,17 +27,18 @@ public class LicensesAcknowledgementTests extends AbstractLicenseServiceTestCase
         setInitialState(TestUtils.generateSignedLicense("trial", TimeValue.timeValueHours(2)), licenseState);
         licenseService.start();
         // try installing a signed license
-        License signedLicense = generateSignedLicense(TimeValue.timeValueHours(10));
+        License signedLicense = generateSignedLicense("basic", TimeValue.timeValueHours(10));
         PutLicenseRequest putLicenseRequest = new PutLicenseRequest().license(signedLicense);
         // ensure acknowledgement message was part of the response
         licenseService.registerLicense(putLicenseRequest, new AssertingLicensesUpdateResponse(false, LicensesStatus.VALID, true));
         assertThat(licenseService.getLicense(), not(signedLicense));
+        verify(clusterService, times(0)).submitStateUpdateTask(any(String.class), any(ClusterStateUpdateTask.class));
 
         // try installing a signed license with acknowledgement
         putLicenseRequest = new PutLicenseRequest().license(signedLicense).acknowledge(true);
         // ensure license was installed and no acknowledgment message was returned
         licenseService.registerLicense(putLicenseRequest, new AssertingLicensesUpdateResponse(true, LicensesStatus.VALID, false));
-        verify(clusterService, times(2)).submitStateUpdateTask(any(String.class), any(ClusterStateUpdateTask.class));
+        verify(clusterService, times(1)).submitStateUpdateTask(any(String.class), any(ClusterStateUpdateTask.class));
     }
 
     private static class AssertingLicensesUpdateResponse implements ActionListener<PutLicenseResponse> {
@@ -61,6 +62,7 @@ public class LicensesAcknowledgementTests extends AbstractLicenseServiceTestCase
 
         @Override
         public void onFailure(Exception throwable) {
+            throw new RuntimeException(throwable);
         }
     }
 }

9894f0c73bb6051d2cba53ed8d504088953b69c2
javanna <cavannaluca@gmail.com>
2016-07-26 15:41:11 +0200

    fix javadocs

--- client/sniffer/src/main/java/org/elasticsearch/client/sniff/ElasticsearchHostsSniffer.java
+++ client/sniffer/src/main/java/org/elasticsearch/client/sniff/ElasticsearchHostsSniffer.java
@@ -56,7 +56,7 @@ public final class ElasticsearchHostsSniffer implements HostsSniffer {
 
     /**
      * Creates a new instance of the Elasticsearch sniffer. It will use the provided {@link RestClient} to fetch the hosts
-     * through the nodes info api. Will use the default sniff request timeout value (@link {@link #DEFAULT_SNIFF_REQUEST_TIMEOUT}
+     * through the nodes info api. Will use the default sniff request timeout value {@link #DEFAULT_SNIFF_REQUEST_TIMEOUT}
      * and http as the scheme for all the hosts.
      */
     public ElasticsearchHostsSniffer(RestClient restClient) {

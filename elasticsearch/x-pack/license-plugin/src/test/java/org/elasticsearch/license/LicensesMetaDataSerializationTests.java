92e357f838a2d09e03cb025fcd3730d9b5be3434
Areek Zillur <areek.zillur@elasticsearch.com>
2016-07-27 11:56:13 -0400

    Remove bwc code to read 1.x licenses from cluster state
    In 2.x, 1.x license format in cluster state was upgraded
    to the 2.x format.  This commit removes the code to read
    1.x license format from cluster state in 5.x, as 2.x clusters
    will already upgrade the license format to 2.x format.
    
    Original commit: elastic/x-pack-elasticsearch@77f18ffc7643541f57a7904fe1ed83a132da3957

--- elasticsearch/x-pack/license-plugin/src/test/java/org/elasticsearch/license/LicensesMetaDataSerializationTests.java
+++ elasticsearch/x-pack/license-plugin/src/test/java/org/elasticsearch/license/LicensesMetaDataSerializationTests.java
@@ -5,13 +5,10 @@
  */
 package org.elasticsearch.license;
 
-import org.elasticsearch.Version;
 import org.elasticsearch.cluster.metadata.MetaData;
 import org.elasticsearch.cluster.metadata.RepositoriesMetaData;
 import org.elasticsearch.cluster.metadata.RepositoryMetaData;
 import org.elasticsearch.common.bytes.BytesReference;
-import org.elasticsearch.common.io.stream.BytesStreamOutput;
-import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.ToXContent;
@@ -23,11 +20,9 @@ import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.license.core.License;
 import org.elasticsearch.test.ESTestCase;
 
-import java.util.Base64;
 import java.util.Collections;
 import java.util.UUID;
 
-import static org.elasticsearch.license.core.CryptUtils.encrypt;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
@@ -88,120 +83,6 @@ public class LicensesMetaDataSerializationTests extends ESTestCase {
         builder.endObject();
         LicensesMetaData licensesMetaDataFromXContent = getLicensesMetaDataFromXContent(builder.bytes());
         assertThat(licensesMetaDataFromXContent.getLicense(), equalTo(trialLicense));
-    }
-
-    public void test1xLicensesMetaDataFromXContent() throws Exception {
-        License signedLicense = TestUtils.generateSignedLicense(TimeValue.timeValueHours(2));
-        long issueDate = signedLicense.issueDate() - TimeValue.timeValueMillis(200).getMillis();
-        License.Builder specBuilder = License.builder()
-                .uid(UUID.randomUUID().toString())
-                .issuedTo("customer")
-                .maxNodes(5)
-                .issueDate(issueDate)
-                .expiryDate(issueDate + TimeValue.timeValueHours(2).getMillis());
-        final License trialLicense = TrialLicense.create(specBuilder);
-        // trial license
-        XContentBuilder builder = XContentFactory.jsonBuilder();
-        builder.startObject();
-        builder.startObject("licenses");
-        builder.startArray("trial_licenses");
-        XContentBuilder contentBuilder = XContentFactory.contentBuilder(XContentType.JSON);
-        trialLicense.toXContent(contentBuilder, new ToXContent.MapParams(Collections.singletonMap(License.LICENSE_SPEC_VIEW_MODE, "true")));
-        builder.value(Base64.getEncoder().encodeToString(encrypt(BytesReference.toBytes(contentBuilder.bytes()))));
-        builder.endArray();
-        builder.startArray("signed_licenses");
-        builder.endArray();
-        builder.endObject();
-        builder.endObject();
-        LicensesMetaData licensesMetaDataFromXContent = getLicensesMetaDataFromXContent(builder.bytes());
-        assertThat(licensesMetaDataFromXContent.getLicense(), equalTo(trialLicense));
-
-        // signed license
-        builder = XContentFactory.jsonBuilder();
-        builder.startObject();
-        builder.startObject("licenses");
-        builder.startArray("trial_licenses");
-        builder.endArray();
-        builder.startArray("signed_licenses");
-        signedLicense.toXContent(builder, ToXContent.EMPTY_PARAMS);
-        builder.endArray();
-        builder.endObject();
-        builder.endObject();
-        licensesMetaDataFromXContent = getLicensesMetaDataFromXContent(builder.bytes());
-        assertThat(licensesMetaDataFromXContent.getLicense(), equalTo(signedLicense));
-
-        // trial and signed license
-        builder = XContentFactory.jsonBuilder();
-        builder.startObject();
-        builder.startObject("licenses");
-        builder.startArray("trial_licenses");
-        contentBuilder = XContentFactory.contentBuilder(XContentType.JSON);
-        trialLicense.toXContent(contentBuilder, new ToXContent.MapParams(Collections.singletonMap(License.LICENSE_SPEC_VIEW_MODE, "true")));
-        builder.value(Base64.getEncoder().encodeToString(encrypt(BytesReference.toBytes(contentBuilder.bytes()))));
-        builder.endArray();
-        builder.startArray("signed_licenses");
-        signedLicense.toXContent(builder, ToXContent.EMPTY_PARAMS);
-        builder.endArray();
-        builder.endObject();
-        builder.endObject();
-        licensesMetaDataFromXContent = getLicensesMetaDataFromXContent(builder.bytes());
-        assertThat(licensesMetaDataFromXContent.getLicense(), equalTo(signedLicense));
-
-        // license with later issue date is selected
-        long laterIssueDate = trialLicense.issueDate() + TimeValue.timeValueHours(2).getMillis();
-        License signedLicenseIssuedLater = TestUtils.generateSignedLicense(laterIssueDate, TimeValue.timeValueHours(2));
-        builder = XContentFactory.jsonBuilder();
-        builder.startObject();
-        builder.startObject("licenses");
-        builder.startArray("trial_licenses");
-        contentBuilder = XContentFactory.contentBuilder(XContentType.JSON);
-        trialLicense.toXContent(contentBuilder, new ToXContent.MapParams(Collections.singletonMap(License.LICENSE_SPEC_VIEW_MODE, "true")));
-        builder.value(Base64.getEncoder().encodeToString(encrypt(BytesReference.toBytes(contentBuilder.bytes()))));
-        builder.endArray();
-        builder.startArray("signed_licenses");
-        signedLicense.toXContent(builder, ToXContent.EMPTY_PARAMS);
-        signedLicenseIssuedLater.toXContent(builder, ToXContent.EMPTY_PARAMS);
-        builder.endArray();
-        builder.endObject();
-        builder.endObject();
-        licensesMetaDataFromXContent = getLicensesMetaDataFromXContent(builder.bytes());
-        assertThat(licensesMetaDataFromXContent.getLicense(), equalTo(signedLicenseIssuedLater));
-
-    }
-
-    public void test1xLicensesMetaDataFromStream() throws Exception {
-        long issueDate = System.currentTimeMillis();
-        License.Builder specBuilder = License.builder()
-                .uid(UUID.randomUUID().toString())
-                .issuedTo("customer")
-                .maxNodes(5)
-                .issueDate(issueDate)
-                .expiryDate(issueDate + TimeValue.timeValueHours(1).getMillis());
-        final License trialLicense = TrialLicense.create(specBuilder);
-        // trial license
-        BytesStreamOutput output = new BytesStreamOutput();
-        output.writeVInt(0);
-        output.writeVInt(1);
-        XContentBuilder contentBuilder = XContentFactory.contentBuilder(XContentType.JSON);
-        trialLicense.toXContent(contentBuilder, new ToXContent.MapParams(Collections.singletonMap(License.LICENSE_SPEC_VIEW_MODE, "true")));
-        output.writeString(Base64.getEncoder().encodeToString(encrypt(BytesReference.toBytes(contentBuilder.bytes()))));
-        try (StreamInput input = output.bytes().streamInput()) {
-            input.setVersion(Version.V_2_0_0_beta1);
-            LicensesMetaData licensesMetaData = LicensesMetaData.PROTO.readFrom(input);
-            assertThat(licensesMetaData.getLicense(), equalTo(trialLicense));
-        }
-
-        // signed license
-        License signedLicense = TestUtils.generateSignedLicense(TimeValue.timeValueHours(2));
-        output = new BytesStreamOutput();
-        output.writeVInt(1);
-        signedLicense.writeTo(output);
-        output.writeVInt(0);
-        try (StreamInput input = output.bytes().streamInput()) {
-            input.setVersion(Version.V_2_0_0_beta1);
-            LicensesMetaData licensesMetaData = LicensesMetaData.PROTO.readFrom(input);
-            assertThat(licensesMetaData.getLicense(), equalTo(signedLicense));
-        }
     }
 
     public void testLicenseTombstoneFromXContext() throws Exception {

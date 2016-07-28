bdebd02d8cac79e146e9ecdd5155a5800676e926
Nik Everett <nik9000@gmail.com>
2016-07-28 14:00:17 -0400

    Only write forced_refresh if we forced a refresh
    Otherwise it just adds noise to the response.
    
    Closes #19629

--- core/src/test/java/org/elasticsearch/action/DocWriteResponseTests.java
+++ core/src/test/java/org/elasticsearch/action/DocWriteResponseTests.java
@@ -20,8 +20,19 @@
 package org.elasticsearch.action;
 
 import org.elasticsearch.action.DocWriteResponse.Operation;
+import org.elasticsearch.action.support.replication.ReplicationResponse.ShardInfo;
+import org.elasticsearch.common.xcontent.ToXContent;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.test.ESTestCase;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 
 public class DocWriteResponseTests extends ESTestCase {
     public void testGetLocation() {
@@ -30,5 +41,34 @@ public class DocWriteResponseTests extends ESTestCase {
         };
         assertEquals("/index/type/id", response.getLocation(null));
         assertEquals("/index/type/id?routing=test_routing", response.getLocation("test_routing"));
+    }
+
+    /**
+     * Tests that {@link DocWriteResponse#toXContent(XContentBuilder, ToXContent.Params)} doesn't include {@code forced_refresh} unless it
+     * is true. We can't assert this in the yaml tests because "not found" is also "false" there....
+     */
+    public void testToXContentDoesntIncludeForcedRefreshUnlessForced() throws IOException {
+        DocWriteResponse response = new DocWriteResponse(new ShardId("index", "uuid", 0), "type", "id", 0, Operation.CREATE) {
+         // DocWriteResponse is abstract so we have to sneak a subclass in here to test it.
+        };
+        response.setShardInfo(new ShardInfo(1, 1));
+        response.setForcedRefresh(false);
+        try (XContentBuilder builder = JsonXContent.contentBuilder()) {
+            builder.startObject();
+            response.toXContent(builder, ToXContent.EMPTY_PARAMS);
+            builder.endObject();
+            try (XContentParser parser = JsonXContent.jsonXContent.createParser(builder.bytes())) {
+                assertThat(parser.map(), not(hasKey("forced_refresh")));
+            }
+        }
+        response.setForcedRefresh(true);
+        try (XContentBuilder builder = JsonXContent.contentBuilder()) {
+            builder.startObject();
+            response.toXContent(builder, ToXContent.EMPTY_PARAMS);
+            builder.endObject();
+            try (XContentParser parser = JsonXContent.jsonXContent.createParser(builder.bytes())) {
+                assertThat(parser.map(), hasEntry("forced_refresh", true));
+            }
+        }
     }
 }

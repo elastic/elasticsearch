76591e54f6542717769f0933458a9c5aeb3fbb58
jaymode <jay.modi@elasticsearch.com>
2016-08-03 07:27:37 -0400

    mock channelhandlercontext instead of a null implementation
    Original commit: elastic/x-pack-elasticsearch@e4ba6b96cc23755c85eee1199e0ea29c51cb3dfc

--- elasticsearch/x-pack/security/src/test/java/org/elasticsearch/xpack/security/transport/netty4/IpFilterRemoteAddressFilterTests.java
+++ elasticsearch/x-pack/security/src/test/java/org/elasticsearch/xpack/security/transport/netty4/IpFilterRemoteAddressFilterTests.java
@@ -5,21 +5,7 @@
  */
 package org.elasticsearch.xpack.security.transport.netty4;
 
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelConfig;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelId;
-import io.netty.channel.ChannelMetadata;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.ChannelProgressivePromise;
-import io.netty.channel.ChannelPromise;
-import io.netty.channel.EventLoop;
-import io.netty.util.Attribute;
-import io.netty.util.AttributeKey;
-import io.netty.util.concurrent.EventExecutor;
 import org.elasticsearch.common.component.Lifecycle;
 import org.elasticsearch.common.network.InetAddresses;
 import org.elasticsearch.common.settings.ClusterSettings;
@@ -34,14 +20,10 @@ import org.elasticsearch.transport.Transport;
 import org.elasticsearch.transport.TransportSettings;
 import org.elasticsearch.xpack.security.audit.AuditTrailService;
 import org.elasticsearch.xpack.security.transport.filter.IPFilter;
-import org.elasticsearch.xpack.security.transport.netty3.IPFilterNetty3UpstreamHandler;
-import org.jboss.netty.channel.ChannelEvent;
-import org.jboss.netty.channel.UpstreamMessageEvent;
 import org.junit.Before;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
@@ -96,219 +78,9 @@ public class IpFilterRemoteAddressFilterTests extends ESTestCase {
 
     public void testThatFilteringWorksByIp() throws Exception {
         InetSocketAddress localhostAddr = new InetSocketAddress(InetAddresses.forString("127.0.0.1"), 12345);
-        assertThat(handler.accept(new NullChannelHandlerContext(), localhostAddr), is(true));
+        assertThat(handler.accept(mock(ChannelHandlerContext.class), localhostAddr), is(true));
 
         InetSocketAddress remoteAddr = new InetSocketAddress(InetAddresses.forString("10.0.0.8"), 12345);
-        assertThat(handler.accept(new NullChannelHandlerContext(), remoteAddr), is(false));
+        assertThat(handler.accept(mock(ChannelHandlerContext.class), remoteAddr), is(false));
     }
-
-    private static class NullChannelHandlerContext implements ChannelHandlerContext {
-
-        @Override
-        public Channel channel() {
-            return null;
-        }
-
-        @Override
-        public EventExecutor executor() {
-            return null;
-        }
-
-        @Override
-        public String name() {
-            return null;
-        }
-
-        @Override
-        public ChannelHandler handler() {
-            return null;
-        }
-
-        @Override
-        public boolean isRemoved() {
-            return false;
-        }
-
-        @Override
-        public ChannelHandlerContext fireChannelRegistered() {
-            return null;
-        }
-
-        @Override
-        public ChannelHandlerContext fireChannelUnregistered() {
-            return null;
-        }
-
-        @Override
-        public ChannelHandlerContext fireChannelActive() {
-            return null;
-        }
-
-        @Override
-        public ChannelHandlerContext fireChannelInactive() {
-            return null;
-        }
-
-        @Override
-        public ChannelHandlerContext fireExceptionCaught(Throwable cause) {
-            return null;
-        }
-
-        @Override
-        public ChannelHandlerContext fireUserEventTriggered(Object evt) {
-            return null;
-        }
-
-        @Override
-        public ChannelHandlerContext fireChannelRead(Object msg) {
-            return null;
-        }
-
-        @Override
-        public ChannelHandlerContext fireChannelReadComplete() {
-            return null;
-        }
-
-        @Override
-        public ChannelHandlerContext fireChannelWritabilityChanged() {
-            return null;
-        }
-
-        @Override
-        public ChannelHandlerContext read() {
-            return null;
-        }
-
-        @Override
-        public ChannelHandlerContext flush() {
-            return null;
-        }
-
-        @Override
-        public ChannelPipeline pipeline() {
-            return null;
-        }
-
-        @Override
-        public ByteBufAllocator alloc() {
-            return null;
-        }
-
-        @Override
-        public <T> Attribute<T> attr(AttributeKey<T> key) {
-            return null;
-        }
-
-        @Override
-        public <T> boolean hasAttr(AttributeKey<T> key) {
-            return false;
-        }
-
-        @Override
-        public ChannelFuture bind(SocketAddress localAddress) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture connect(SocketAddress remoteAddress) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture connect(SocketAddress remoteAddress, SocketAddress localAddress) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture disconnect() {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture close() {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture deregister() {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture bind(SocketAddress localAddress, ChannelPromise promise) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture connect(SocketAddress remoteAddress, ChannelPromise promise) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture connect(SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture disconnect(ChannelPromise promise) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture close(ChannelPromise promise) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture deregister(ChannelPromise promise) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture write(Object msg) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture write(Object msg, ChannelPromise promise) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture writeAndFlush(Object msg, ChannelPromise promise) {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture writeAndFlush(Object msg) {
-            return null;
-        }
-
-        @Override
-        public ChannelPromise newPromise() {
-            return null;
-        }
-
-        @Override
-        public ChannelProgressivePromise newProgressivePromise() {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture newSucceededFuture() {
-            return null;
-        }
-
-        @Override
-        public ChannelFuture newFailedFuture(Throwable cause) {
-            return null;
-        }
-
-        @Override
-        public ChannelPromise voidPromise() {
-            return null;
-        }
-
-    }
-
 }

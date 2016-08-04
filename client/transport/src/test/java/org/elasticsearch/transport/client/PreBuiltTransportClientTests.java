f1110f6f2a831ce06609a83189e2896bb03813cb
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2016-08-04 16:45:05 +0100

    fix import statements

--- client/transport/src/test/java/org/elasticsearch/transport/client/PreBuiltTransportClientTests.java
+++ client/transport/src/test/java/org/elasticsearch/transport/client/PreBuiltTransportClientTests.java
@@ -32,8 +32,8 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class PreBuiltTransportClientTests extends RandomizedTest {

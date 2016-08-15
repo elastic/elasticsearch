fdd50612ae4ba1ecd4a704a128a8f0aa5af0673f
Nik Everett <nik9000@gmail.com>
2016-08-15 15:51:24 -0400

    Fix reindex under the transport client
    The big change here is cleaning up the `TaskListResponse` so it doesn't
    have a breaky `toString` implementation. That was causing the reindex
    tests to break.
    
    Also removed `NetworkModule#registerTaskStatus` which is part of the
    Plugin API. Use `Plugin#getNamedWriteables` instead.

--- core/src/test/java/org/elasticsearch/tasks/ListTasksResponseTests.java
+++ core/src/test/java/org/elasticsearch/tasks/ListTasksResponseTests.java
@@ -20,29 +20,23 @@
 package org.elasticsearch.tasks;
 
 import org.elasticsearch.action.admin.cluster.node.tasks.list.ListTasksResponse;
-import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.test.ESTestCase;
-import org.hamcrest.Matchers;
 
-import java.util.Collections;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 
 public class ListTasksResponseTests extends ESTestCase {
 
-    public void testToStringNoTask() {
-        ListTasksResponse tasksResponse = new ListTasksResponse();
-        String string = tasksResponse.toString();
-        assertThat(string, Matchers.containsString("nodes"));
+    public void testEmptyToString() {
+        assertEquals("{\"tasks\":{}}", new ListTasksResponse().toString());
     }
 
-    public void testToString() {
+    public void testNonEmptyToString() {
         TaskInfo info = new TaskInfo(
             new TaskId("node1", 1), "dummy-type", "dummy-action", "dummy-description", null, 0, 1, true, new TaskId("node1", 0));
-
-        DiscoveryNodes nodes = DiscoveryNodes.builder().build();
-        ListTasksResponse tasksResponse = new ListTasksResponse(Collections.singletonList(info), Collections.emptyList(),
-            Collections.emptyList(), nodes);
-
-        String string = tasksResponse.toString();
-        assertThat(string, Matchers.containsString("\"type\":\"dummy-type\""));
+        ListTasksResponse tasksResponse = new ListTasksResponse(singletonList(info), emptyList(), emptyList());
+        assertEquals("{\"tasks\":{\"node1:1\":{\"node\":\"node1\",\"id\":1,\"type\":\"dummy-type\",\"action\":\"dummy-action\","
+                + "\"description\":\"dummy-description\",\"start_time_in_millis\":0,\"running_time_in_nanos\":1,\"cancellable\":true,"
+                + "\"parent_task_id\":\"node1:0\"}}}", tasksResponse.toString());
     }
 }

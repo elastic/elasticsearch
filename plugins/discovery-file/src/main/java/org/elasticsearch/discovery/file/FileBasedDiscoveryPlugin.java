3e8fbecce3842a0b98a1a831f7ccd520b4b11e78
Ali Beyad <ali@elastic.co>
2016-09-08 17:06:05 -0400

    File-based discovery plugin
    This commit introduces a new plugin for file-based unicast hosts
    discovery. This allows specifying the unicast hosts participating
    in discovery through a `unicast_hosts.txt` file located in the
    config directory. The plugin will use the hosts specified in this
    file as the set of hosts to ping during discovery.
    
    The format of the `unicast_hosts.txt` file is to have one host/port
    entry per line. The hosts file is read and parsed every time
    discovery makes ping requests, thus a new version of the file that
    is published to the config directory will automatically be picked
    up.
    
    Closes #20323

---
+++ plugins/discovery-file/src/main/java/org/elasticsearch/discovery/file/FileBasedDiscoveryPlugin.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.discovery.file;
+
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.discovery.DiscoveryModule;
+import org.elasticsearch.discovery.zen.ZenDiscovery;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.plugins.DiscoveryPlugin;
+import org.elasticsearch.plugins.Plugin;
+
+/**
+ * Plugin for providing file-based unicast hosts discovery. The list of unicast hosts
+ * is obtained by reading the {@link FileBasedUnicastHostsProvider#UNICAST_HOSTS_FILE} in
+ * the {@link Environment#configFile()} directory.
+ */
+public class FileBasedDiscoveryPlugin extends Plugin implements DiscoveryPlugin {
+
+    private static final Logger logger = Loggers.getLogger(FileBasedDiscoveryPlugin.class);
+    private static final String DISCOVERY_FILE = "file";
+
+    private final Settings settings;
+
+    public FileBasedDiscoveryPlugin(Settings settings) {
+        this.settings = settings;
+        logger.trace("starting file-based discovery plugin...");
+    }
+
+    public void onModule(DiscoveryModule discoveryModule) {
+        logger.debug("register file-based unicast hosts provider");
+        discoveryModule.addDiscoveryType(DISCOVERY_FILE, ZenDiscovery.class);
+        discoveryModule.addUnicastHostProvider(DISCOVERY_FILE, FileBasedUnicastHostsProvider.class);
+    }
+}
---
+++ plugins/discovery-file/src/test/java/org/elasticsearch/discovery/file/FileBasedUnicastHostsProviderTests.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.discovery.file;
+
+import org.elasticsearch.Version;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.test.transport.MockTransportService;
+import org.elasticsearch.threadpool.TestThreadPool;
+import org.elasticsearch.threadpool.ThreadPool;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+
+/**
+ * Tests for {@link FileBasedUnicastHostsProvider}.
+ */
+public class FileBasedUnicastHostsProviderTests extends ESTestCase {
+
+    private static ThreadPool threadPool;
+    private MockTransportService transportService;
+
+    @BeforeClass
+    public static void createThreadPool() {
+        threadPool = new TestThreadPool(FileBasedUnicastHostsProviderTests.class.getName());
+    }
+
+    @AfterClass
+    public static void stopThreadPool() {
+        if (threadPool != null) {
+            threadPool.shutdownNow();
+            threadPool = null;
+        }
+    }
+
+    @Before
+    public void createTransportSvc() {
+        transportService = MockTransportService.local(Settings.EMPTY, Version.CURRENT, threadPool);
+    }
+
+    public void testBuildDynamicNodes() throws Exception {
+        final List<String> hostEntries = Arrays.asList("192.168.0.1", "192.168.0.2:9305", "255.255.23.15");
+        final Settings settings = setupUnicastHosts(hostEntries);
+        final FileBasedUnicastHostsProvider provider = new FileBasedUnicastHostsProvider(settings, transportService);
+        final List<DiscoveryNode> nodes = provider.buildDynamicNodes();
+        assertEquals(hostEntries.size(), nodes.size());
+        for (int i = 0; i < hostEntries.size(); i++) {
+            assertThat(nodes.get(i).getAddress().toString(), containsString(hostEntries.get(i)));
+        }
+    }
+
+    public void testEmptyUnicastHostsFile() throws Exception {
+        final List<String> hostEntries = Collections.emptyList();
+        final Settings settings = setupUnicastHosts(hostEntries);
+        final FileBasedUnicastHostsProvider provider = new FileBasedUnicastHostsProvider(settings, transportService);
+        final List<DiscoveryNode> nodes = provider.buildDynamicNodes();
+        assertEquals(0, nodes.size());
+    }
+
+    public void testUnicastHostsDoesNotExist() throws Exception {
+        final Settings settings = Settings.builder()
+                                      .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir())
+                                      .build();
+        final FileBasedUnicastHostsProvider provider = new FileBasedUnicastHostsProvider(settings, transportService);
+        final List<DiscoveryNode> nodes = provider.buildDynamicNodes();
+        assertEquals(0, nodes.size());
+    }
+
+    // sets up the config dir and writes to the unicast hosts file in the config dir
+    private Settings setupUnicastHosts(final List<String> hostEntries) throws IOException {
+        final Path homeDir = createTempDir();
+        final Settings settings = Settings.builder()
+                                      .put(Environment.PATH_HOME_SETTING.getKey(), homeDir)
+                                      .build();
+        final Path configDir = homeDir.resolve("config");
+        Files.createDirectory(configDir);
+        final Path unicastHostsPath = configDir.resolve(FileBasedUnicastHostsProvider.UNICAST_HOSTS_FILE);
+        try (BufferedWriter writer = Files.newBufferedWriter(unicastHostsPath)) {
+            writer.write(String.join("\n", hostEntries));
+        }
+        return settings;
+    }
+}

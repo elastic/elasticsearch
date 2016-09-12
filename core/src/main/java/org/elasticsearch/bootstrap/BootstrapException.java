ffbf7d30271379a43d761ad7c4d60564f0580749
Tanguy Leroux <tlrx.dev@gmail.com>
2016-09-12 16:56:13 +0200

    Add quiet option to disable console logging (#20422)
    This commit adds a -q/--quiet option to Elasticsearch so that it does not log anything in the console and closes stdout & stderr streams. This is useful for SystemD to avoid duplicate logs in both journalctl and /var/log/elasticsearch/elasticsearch.log while still allows the JVM to print error messages in stdout/stderr if needed.
    
    closes #17220
    
    (cherry picked from commit 6090c51)

--- core/src/main/java/org/elasticsearch/bootstrap/BootstrapException.java
+++ core/src/main/java/org/elasticsearch/bootstrap/BootstrapException.java
@@ -26,7 +26,7 @@ import java.util.Map;
  * Wrapper exception for checked exceptions thrown during the bootstrap process. Methods invoked
  * during bootstrap should explicitly declare the checked exceptions that they can throw, rather
  * than declaring the top-level checked exception {@link Exception}. This exception exists to wrap
- * these checked exceptions so that {@link Bootstrap#init(boolean, Path, Map)} does not have to
+ * these checked exceptions so that {@link Bootstrap#init(boolean, Path, boolean, Map)} does not have to
  * declare all of these checked exceptions.
  */
 class BootstrapException extends Exception {

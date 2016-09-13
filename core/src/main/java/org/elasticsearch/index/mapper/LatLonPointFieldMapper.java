8ce3ba32bb3dcdaa0456733439220486d7fc6440
Nicholas Knize <nknize@gmail.com>
2016-09-13 12:25:55 -0500

    update LAT_LON_FIELD_VERSION
    Update to 5.0.0-beta1

--- core/src/main/java/org/elasticsearch/index/mapper/LatLonPointFieldMapper.java
+++ core/src/main/java/org/elasticsearch/index/mapper/LatLonPointFieldMapper.java
@@ -44,7 +44,8 @@ import java.util.Map;
  */
 public class LatLonPointFieldMapper extends BaseGeoPointFieldMapper {
     public static final String CONTENT_TYPE = "geo_point";
-    public static final Version LAT_LON_FIELD_VERSION = Version.V_5_0_0_alpha6;
+    // TODO replace this once 5.0.0-beta1 is released
+    public static final Version LAT_LON_FIELD_VERSION = Version.fromString("5.0.0-beta1");
 
     public static class Defaults extends BaseGeoPointFieldMapper.Defaults {
         public static final LatLonPointFieldType FIELD_TYPE = new LatLonPointFieldType();

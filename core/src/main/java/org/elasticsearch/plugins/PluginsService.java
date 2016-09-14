48a40d4de07ef988ab54c9d1b37e619a66d4b69a
Yannick Welsch <yannick@welsch.lu>
2016-09-14 22:58:12 +0200

    Fix wrong logger usages
    These misusages were found by the logger usage checker that was re-enabled in the previous commit.

--- core/src/main/java/org/elasticsearch/plugins/PluginsService.java
+++ core/src/main/java/org/elasticsearch/plugins/PluginsService.java
@@ -245,7 +245,7 @@ public class PluginsService extends AbstractComponent {
                         try {
                             reference.onModuleMethod.invoke(plugin.v2(), module);
                         } catch (IllegalAccessException | InvocationTargetException e) {
-                            logger.warn("plugin {}, failed to invoke custom onModule method", e, plugin.v1().getName());
+                            logger.warn((Supplier<?>) () -> new ParameterizedMessage("plugin {}, failed to invoke custom onModule method", plugin.v1().getName()), e);
                             throw new ElasticsearchException("failed to invoke onModule", e);
                         } catch (Exception e) {
                             logger.warn((Supplier<?>) () -> new ParameterizedMessage("plugin {}, failed to invoke custom onModule method", plugin.v1().getName()), e);

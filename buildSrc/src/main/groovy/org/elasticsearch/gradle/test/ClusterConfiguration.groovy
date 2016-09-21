ac42c91376383070e7b9be6677ee9929dccd8a6b
Jason Tedor <jason@tedor.me>
2016-09-21 16:33:15 -0400

    Fix cluster wait condition
    Today we define a cluster wait condition to try to wait at least a
    certain number of nodes when running integration tests. Alas, the wait
    condition is incorrect because wait_for_nodes>=${numNodes} will be split
    by parameter parsing on the equals sign so the request looks like it has
    a parameter named wait_for_nodes>. The fact that REST param parsing is
    lenient leads to this being undiscovered. This commit fixes this issue.
    
    Relates #20601

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/test/ClusterConfiguration.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/test/ClusterConfiguration.groovy
@@ -95,11 +95,11 @@ class ClusterConfiguration {
     @Input
     Closure waitCondition = { NodeInfo node, AntBuilder ant ->
         File tmpFile = new File(node.cwd, 'wait.success')
-        ant.echo("==> [${new Date()}] checking health: http://${node.httpUri()}/_cluster/health?wait_for_nodes>=${numNodes}")
+        ant.echo("==> [${new Date()}] checking health: http://${node.httpUri()}/_cluster/health?wait_for_nodes=>=${numNodes}")
         // checking here for wait_for_nodes to be >= the number of nodes because its possible
         // this cluster is attempting to connect to nodes created by another task (same cluster name),
         // so there will be more nodes in that case in the cluster state
-        ant.get(src: "http://${node.httpUri()}/_cluster/health?wait_for_nodes>=${numNodes}",
+        ant.get(src: "http://${node.httpUri()}/_cluster/health?wait_for_nodes=>=${numNodes}",
                 dest: tmpFile.toString(),
                 ignoreerrors: true, // do not fail on error, so logging buffers can be flushed by the wait task
                 retries: 10)

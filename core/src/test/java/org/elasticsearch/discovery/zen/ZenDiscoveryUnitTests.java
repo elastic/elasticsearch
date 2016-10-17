1d5885577547ecd798e7674949c37b5eb406ecf6
Christoph Büscher <christoph@elastic.co>
2016-10-17 13:05:03 +0200

    Use expectThrows() instead of try-catch blocks for testing expected exceptions

--- core/src/test/java/org/elasticsearch/discovery/zen/ZenDiscoveryUnitTests.java
+++ core/src/test/java/org/elasticsearch/discovery/zen/ZenDiscoveryUnitTests.java
@@ -87,7 +87,7 @@ public class ZenDiscoveryUnitTests extends ESTestCase {
 
         currentNodes = DiscoveryNodes.builder();
         currentNodes.masterNodeId("b").add(new DiscoveryNode("b", buildNewFakeTransportAddress(), emptyMap(), emptySet(), Version.CURRENT));
-        ;
+
         // version isn't taken into account, so randomize it to ensure this.
         if (randomBoolean()) {
             currentState.version(2);

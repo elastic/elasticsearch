7caf6b70c3d4e2a742f4152b6179687c28502174
Ryan Ernst <ryan@iernst.net>
2016-10-20 09:13:59 -0700

    Plugins: Make UnicastHostsProvider extension pull based (#21036)
    This change moves providing UnicastHostsProvider for zen discovery to be
    pull based, adding a getter in DiscoveryPlugin. A new setting is added,
    discovery.zen.hosts_provider, to separate the discovery type from the
    hosts provider for zen when it is selected. Unfortunately existing
    plugins added ZenDiscovery with their own name in order to just provide
    a hosts provider, so there are already many users setting the hosts
    provider through discovery.type. This change also includes backcompat,
    falling back to discovery.type when discovery.zen.hosts_provider is not
    set.

--- core/src/test/java/org/elasticsearch/discovery/zen/UnicastZenPingTests.java
+++ core/src/test/java/org/elasticsearch/discovery/zen/UnicastZenPingTests.java
@@ -61,6 +61,8 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 
 public class UnicastZenPingTests extends ESTestCase {
+    private static final UnicastHostsProvider EMPTY_HOSTS_PROVIDER = Collections::emptyList;
+
     public void testSimplePings() throws InterruptedException {
         int startPort = 11000 + randomIntBetween(0, 1000);
         int endPort = startPort + 10;
@@ -95,7 +97,7 @@ public class UnicastZenPingTests extends ESTestCase {
                 .build();
 
         Settings hostsSettingsMismatch = Settings.builder().put(hostsSettings).put(settingsMismatch).build();
-        UnicastZenPing zenPingA = new UnicastZenPing(hostsSettings, threadPool, handleA.transportService, null);
+        UnicastZenPing zenPingA = new UnicastZenPing(hostsSettings, threadPool, handleA.transportService, EMPTY_HOSTS_PROVIDER);
         zenPingA.setPingContextProvider(new PingContextProvider() {
             @Override
             public DiscoveryNodes nodes() {
@@ -109,7 +111,7 @@ public class UnicastZenPingTests extends ESTestCase {
         });
         zenPingA.start();
 
-        UnicastZenPing zenPingB = new UnicastZenPing(hostsSettings, threadPool, handleB.transportService, null);
+        UnicastZenPing zenPingB = new UnicastZenPing(hostsSettings, threadPool, handleB.transportService, EMPTY_HOSTS_PROVIDER);
         zenPingB.setPingContextProvider(new PingContextProvider() {
             @Override
             public DiscoveryNodes nodes() {
@@ -123,7 +125,7 @@ public class UnicastZenPingTests extends ESTestCase {
         });
         zenPingB.start();
 
-        UnicastZenPing zenPingC = new UnicastZenPing(hostsSettingsMismatch, threadPool, handleC.transportService, null) {
+        UnicastZenPing zenPingC = new UnicastZenPing(hostsSettingsMismatch, threadPool, handleC.transportService, EMPTY_HOSTS_PROVIDER) {
             @Override
             protected Version getVersion() {
                 return versionD;
@@ -142,7 +144,7 @@ public class UnicastZenPingTests extends ESTestCase {
         });
         zenPingC.start();
 
-        UnicastZenPing zenPingD = new UnicastZenPing(hostsSettingsMismatch, threadPool, handleD.transportService, null);
+        UnicastZenPing zenPingD = new UnicastZenPing(hostsSettingsMismatch, threadPool, handleD.transportService, EMPTY_HOSTS_PROVIDER);
         zenPingD.setPingContextProvider(new PingContextProvider() {
             @Override
             public DiscoveryNodes nodes() {

326a665509b66a0fe15135e1dd4b9a486b97433e
Simon Willnauer <simonw@apache.org>
2016-10-28 15:21:43 +0200

    [TEST] Only set 0s state timeout if we have more than one node

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/test/ClusterFormationTasks.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/test/ClusterFormationTasks.groovy
@@ -259,8 +259,10 @@ class ClusterFormationTasks {
         // basically skip initial state recovery to allow the cluster to form using a realistic master election
         // this means all nodes must be up, join the seed node and do a master election. This will also allow new and
         // old nodes in the BWC case to become the master
-        esConfig['discovery.zen.minimum_master_nodes'] = node.config.numNodes
-        esConfig['discovery.initial_state_timeout'] = '0s' // don't wait for state.. just start up quickly
+        if (node.config.numNodes > 1) {
+            esConfig['discovery.zen.minimum_master_nodes'] = node.config.numNodes
+            esConfig['discovery.initial_state_timeout'] = '0s' // don't wait for state.. just start up quickly
+        }
         esConfig['node.max_local_storage_nodes'] = node.config.numNodes
         esConfig['http.port'] = node.config.httpPort
         esConfig['transport.tcp.port'] =  node.config.transportPort

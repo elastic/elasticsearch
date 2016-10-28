e95cb6efe0bcc090ed5a722b5d96883efe0e02f2
Jack Conradson <osjdconrad@gmail.com>
2016-10-28 11:11:27 -0700

    Clean up ScriptType.

--- core/src/main/java/org/elasticsearch/script/ScriptType.java
+++ core/src/main/java/org/elasticsearch/script/ScriptType.java
@@ -24,66 +24,117 @@ import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 
 import java.io.IOException;
-import java.util.Locale;
+import java.util.Objects;
 
 /**
- * The type of a script, more specifically where it gets loaded from:
- * - provided dynamically at request time
- * - loaded from an index
- * - loaded from file
+ * ScriptType represents the way a script is stored and retrieved from the {@link ScriptService}.
+ * It's also used to by {@link ScriptSettings} and {@link ScriptModes} to determine whether or not
+ * a {@link Script} is allowed to be executed based on both default and user-defined settings.
  */
 public enum ScriptType {
 
-    INLINE(0, "inline", "inline", false),
-    STORED(1, "id", "stored", false),
-    FILE(2, "file", "file", true);
+    /**
+     * INLINE scripts are specified in numerous queries and compiled on-the-fly.
+     * They will be cached based on the lang and code of the script.
+     * They are turned off by default for security purposes.
+     */
+    INLINE ( 0 , "inline" , new ParseField("inline")       , false ),
 
-    private final int val;
-    private final ParseField parseField;
-    private final String scriptType;
-    private final boolean defaultScriptEnabled;
+    /**
+     * STORED scripts are saved as part of the {@link org.elasticsearch.cluster.ClusterState}
+     * based on user requests.  They will be cached when they are first used in a query.
+     * They are turned off by default for security purposes.
+     */
+    STORED ( 1 , "stored" , new ParseField("stored", "id") , false ),
 
+    /**
+     * FILE scripts are loaded from disk either on start-up or on-the-fly depending on
+     * user-defined settings.  They will be compiled and cached as soon as they are loaded
+     * from disk.  They are turned on by default as they should always be safe to execute.
+     */
+    FILE   ( 2 , "file"   , new ParseField("file")         , true  );
+
+    /**
+     * Reads an int from the input stream and converts it to a {@link ScriptType}.
+     * @return The ScriptType read from the stream. Throws an {@link IllegalStateException} if no ScriptType is found based on the id.
+     */
     public static ScriptType readFrom(StreamInput in) throws IOException {
-        int scriptTypeVal = in.readVInt();
-        for (ScriptType type : values()) {
-            if (type.val == scriptTypeVal) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("Unexpected value read for ScriptType got [" + scriptTypeVal + "] expected one of ["
-                + INLINE.val + "," + FILE.val + "," + STORED.val + "]");
-    }
+        int id = in.readVInt();
 
-    public static void writeTo(ScriptType scriptType, StreamOutput out) throws IOException{
-        if (scriptType != null) {
-            out.writeVInt(scriptType.val);
+        if (FILE.id == id) {
+            return FILE;
+        } else if (STORED.id == id) {
+            return STORED;
+        } else if (INLINE.id == id) {
+            return INLINE;
         } else {
-            out.writeVInt(INLINE.val); //Default to inline
+            throw new IllegalStateException("Error reading ScriptType id [" + id + "] from stream, expected one of [" +
+                FILE.id + " [" + FILE.name + "], " + STORED.id + " [" + STORED.name + "], " + INLINE.id + " [" + INLINE.name + "]]");
         }
     }
 
-    ScriptType(int val, String name, String scriptType, boolean defaultScriptEnabled) {
-        this.val = val;
-        this.parseField = new ParseField(name);
-        this.scriptType = scriptType;
-        this.defaultScriptEnabled = defaultScriptEnabled;
+    /**
+     * Writes an int to the output stream based on the id of the {@link ScriptType}.
+     * @param type The {@link ScriptType}.  Must not be {@code null}.
+     */
+    public static void writeTo(ScriptType type, StreamOutput out) throws IOException {
+        Objects.requireNonNull(type);
+        out.writeVInt(type.id);
     }
 
+    private final int id;
+    private final String name;
+    private final ParseField parseField;
+    private final boolean defaultEnabled;
+
+    /**
+     * Standard constructor.
+     * @param id A unique identifier for a type that can be read/written to a stream.
+     * @param name A unique name for a type.
+     * @param parseField Specifies the name used to parse input from queries.
+     * @param defaultEnabled Whether or not an {@link ScriptType} can be run by default.
+     */
+    ScriptType(int id, String name, ParseField parseField, boolean defaultEnabled) {
+        this.id = id;
+        this.name = name;
+        this.parseField = parseField;
+        this.defaultEnabled = defaultEnabled;
+    }
+
+    /**
+     * @return The unique id for this {@link ScriptType}.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return The unique name for this {@link ScriptType}.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return Specifies the name used to parse input from queries.
+     */
     public ParseField getParseField() {
         return parseField;
     }
 
-    public boolean getDefaultScriptEnabled() {
-        return defaultScriptEnabled;
+    /**
+     * @return Whether or not a {@link ScriptType} can be run by default.  Note
+     * this can be potentially overriden by any {@link ScriptEngineService}.
+     */
+    public boolean isDefaultEnabled() {
+        return defaultEnabled;
     }
 
-    public String getScriptType() {
-        return scriptType;
-    }
-
+    /**
+     * @return The same as calling {@link #getName()}.
+     */
     @Override
     public String toString() {
-        return name().toLowerCase(Locale.ROOT);
+        return getName();
     }
-
 }

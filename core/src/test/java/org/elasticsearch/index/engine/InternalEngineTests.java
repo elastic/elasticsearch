2ba4dadea0c16fdb701caf79db81b966df58c52f
Simon Willnauer <simonw@apache.org>
2016-11-02 09:30:49 +0100

    [TEST] fix extrasFS file filtering in OldIndexUtils

--- core/src/test/java/org/elasticsearch/index/engine/InternalEngineTests.java
+++ core/src/test/java/org/elasticsearch/index/engine/InternalEngineTests.java
@@ -1825,7 +1825,7 @@ public class InternalEngineTests extends ESTestCase {
             assertEquals(Arrays.toString(tlogFiles), tlogFiles.length, 2); // ckp & tlog
             Path tlogFile = tlogFiles[0].getFileName().toString().endsWith("tlog") ? tlogFiles[0] : tlogFiles[1];
             final long size = Files.size(tlogFile);
-            logger.info("upgrading index {} file: {} size: {}", indexName, tlogFiles[0].getFileName(), size);
+            logger.debug("upgrading index {} file: {} size: {}", indexName, tlogFiles[0].getFileName(), size);
             Directory directory = newFSDirectory(src.resolve("0").resolve("index"));
             Store store = createStore(directory);
             final int iters = randomIntBetween(0, 2);

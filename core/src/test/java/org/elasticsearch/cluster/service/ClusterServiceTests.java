4ac64d7a716fe3605e783ee43991e9589a112ce1
Jason Tedor <jason@tedor.me>
2016-11-04 10:44:38 -0400

    Assert status logger does not warn on Log4j usage
    Today if you start Elasticsearch with the status logger configured to
    the warn level, or use a transport client with the default status logger
    level, you will see warn messages about deprecation loggers being
    created with different message factories and that formatting might be
    broken. This happens because the deprecation logger is constructed using
    the message factory from its parent, an artifact leftover from the first
    Log4j 2 implementation that used a custom message factory. When that
    custom message factory was removed, this constructor invocation should
    have been changed to not explicitly use the message factory from the
    parent. This commit fixes this invocation. However, we also had some
    status checking to all tests to ensure that there are no warn status log
    messages that might indicate a configuration problem with Log4j 2. These
    assertions blow up badly without the fix for the deprecation logger
    construction, and also caught a misconfiguration in one of the logging
    tests.

--- core/src/test/java/org/elasticsearch/cluster/service/ClusterServiceTests.java
+++ core/src/test/java/org/elasticsearch/cluster/service/ClusterServiceTests.java
@@ -742,6 +742,7 @@ public class ClusterServiceTests extends ESTestCase {
     @TestLogging("org.elasticsearch.cluster.service:TRACE") // To ensure that we log cluster state events on TRACE level
     public void testClusterStateUpdateLogging() throws Exception {
         MockLogAppender mockAppender = new MockLogAppender();
+        mockAppender.start();
         mockAppender.addExpectation(
                 new MockLogAppender.SeenEventExpectation(
                         "test1",
@@ -838,6 +839,7 @@ public class ClusterServiceTests extends ESTestCase {
             latch.await();
         } finally {
             Loggers.removeAppender(clusterLogger, mockAppender);
+            mockAppender.stop();
         }
         mockAppender.assertAllExpectationsMatched();
     }
@@ -845,6 +847,7 @@ public class ClusterServiceTests extends ESTestCase {
     @TestLogging("org.elasticsearch.cluster.service:WARN") // To ensure that we log cluster state events on WARN level
     public void testLongClusterStateUpdateLogging() throws Exception {
         MockLogAppender mockAppender = new MockLogAppender();
+        mockAppender.start();
         mockAppender.addExpectation(
                 new MockLogAppender.UnseenEventExpectation(
                         "test1 shouldn't see because setting is too low",
@@ -968,6 +971,7 @@ public class ClusterServiceTests extends ESTestCase {
             latch.await();
         } finally {
             Loggers.removeAppender(clusterLogger, mockAppender);
+            mockAppender.stop();
         }
         mockAppender.assertAllExpectationsMatched();
     }

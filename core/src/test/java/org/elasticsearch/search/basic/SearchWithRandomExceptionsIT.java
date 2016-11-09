62b287ac83827c7e93753d48ad2be793ad5da17d
javanna <cavannaluca@gmail.com>
2016-11-09 12:15:19 +0100

    Revert "Fix tests overriding mock engine to avoid builtin mock"
    This reverts commit 97223063451c4cea4508c4e805322bc1cd5a4d9f.

--- core/src/test/java/org/elasticsearch/search/basic/SearchWithRandomExceptionsIT.java
+++ core/src/test/java/org/elasticsearch/search/basic/SearchWithRandomExceptionsIT.java
@@ -46,10 +46,8 @@ import org.elasticsearch.test.engine.ThrowingLeafReaderWrapper;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
@@ -58,14 +56,7 @@ public class SearchWithRandomExceptionsIT extends ESIntegTestCase {
 
     @Override
     protected Collection<Class<? extends Plugin>> nodePlugins() {
-        return Arrays.asList(RandomExceptionDirectoryReaderWrapper.TestPlugin.class);
-    }
-
-    @Override
-    protected Collection<Class<? extends Plugin>> getMockPlugins() {
-        Set<Class<? extends Plugin>> mocks = new HashSet<>(super.getMockPlugins());
-        mocks.remove(MockEngineFactoryPlugin.class);
-        return mocks;
+        return Arrays.asList(RandomExceptionDirectoryReaderWrapper.TestPlugin.class, MockEngineFactoryPlugin.class);
     }
 
     public void testRandomExceptions() throws IOException, InterruptedException, ExecutionException {

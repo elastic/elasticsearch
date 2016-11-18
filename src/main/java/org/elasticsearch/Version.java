54669d69925aa12d24b5ab2303f5fe98c6552a07
Alexander Reelsen <alexander@reelsen.net>
2016-11-18 17:35:41 +0100

    Bump versions and add BWC indices

--- src/main/java/org/elasticsearch/Version.java
+++ src/main/java/org/elasticsearch/Version.java
@@ -256,7 +256,9 @@ public class Version {
     public static final Version V_1_7_5 = new Version(V_1_7_5_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_4);
     public static final int V_1_7_6_ID = 1070699;
     public static final Version V_1_7_6 = new Version(V_1_7_6_ID, false, org.apache.lucene.util.Version.LUCENE_4_10_4);
-    public static final Version CURRENT = V_1_7_6;
+    public static final int V_1_7_7_ID = 1070799;
+    public static final Version V_1_7_7 = new Version(V_1_7_7_ID, true, org.apache.lucene.util.Version.LUCENE_4_10_4);
+    public static final Version CURRENT = V_1_7_7;
 
     static {
         assert CURRENT.luceneVersion.equals(Lucene.VERSION) : "Version must be upgraded to [" + Lucene.VERSION + "] is still set to [" + CURRENT.luceneVersion + "]";
@@ -268,6 +270,8 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_1_7_7_ID:
+                return V_1_7_7;
             case V_1_7_6_ID:
                 return V_1_7_6;
             case V_1_7_5_ID:

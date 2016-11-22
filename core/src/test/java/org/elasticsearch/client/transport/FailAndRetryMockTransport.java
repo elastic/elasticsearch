8a69709911152ab0caa820146acfc244cc4d5cbe
Ryan Ernst <ryan@iernst.net>
2016-11-22 14:41:13 -0800

    Transport client: Fix remove address to actually work
    The removeTransportAddress method of TransportClient removes the address
    from the list of nodes that the client pings to sniff for nodes.
    However, it does not remove it from the list of existing connected
    nodes. This means removing a node is not possible, as long as that node
    is still up.
    
    This change removes the node from the connected nodes list before
    triggering sampling (ie sniffing).  While the fix is simple, testing was
    not because there were no existing tests for sniffing. This change also
    modifies the mocks used by transport client unit tests in order to allow
    mocking sniffing.

--- core/src/test/java/org/elasticsearch/client/transport/FailAndRetryMockTransport.java
+++ core/src/test/java/org/elasticsearch/client/transport/FailAndRetryMockTransport.java
@@ -20,8 +20,13 @@
 package org.elasticsearch.client.transport;
 
 import org.elasticsearch.action.admin.cluster.node.liveness.LivenessResponse;
+import org.elasticsearch.action.admin.cluster.node.liveness.TransportLivenessAction;
+import org.elasticsearch.action.admin.cluster.state.ClusterStateAction;
+import org.elasticsearch.action.admin.cluster.state.ClusterStateResponse;
 import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.common.component.Lifecycle;
 import org.elasticsearch.common.component.LifecycleListener;
 import org.elasticsearch.common.settings.Settings;
@@ -64,6 +69,8 @@ abstract class FailAndRetryMockTransport<Response extends TransportResponse> imp
         this.clusterName = clusterName;
     }
 
+    protected abstract ClusterState getMockClusterState(DiscoveryNode node);
+
     @Override
     @SuppressWarnings("unchecked")
     public void sendRequest(DiscoveryNode node, long requestId, String action, TransportRequest request, TransportRequestOptions options)
@@ -71,9 +78,17 @@ abstract class FailAndRetryMockTransport<Response extends TransportResponse> imp
 
         //we make sure that nodes get added to the connected ones when calling addTransportAddress, by returning proper nodes info
         if (connectMode) {
-            TransportResponseHandler transportResponseHandler = transportServiceAdapter.onResponseReceived(requestId);
-            transportResponseHandler.handleResponse(new LivenessResponse(ClusterName.CLUSTER_NAME_SETTING.getDefault(Settings.EMPTY),
-                node));
+            if (TransportLivenessAction.NAME.equals(action)) {
+                TransportResponseHandler transportResponseHandler = transportServiceAdapter.onResponseReceived(requestId);
+                transportResponseHandler.handleResponse(new LivenessResponse(ClusterName.CLUSTER_NAME_SETTING.getDefault(Settings.EMPTY),
+                    node));
+            } else if (ClusterStateAction.NAME.equals(action)) {
+                TransportResponseHandler transportResponseHandler = transportServiceAdapter.onResponseReceived(requestId);
+                ClusterState clusterState = getMockClusterState(node);
+                transportResponseHandler.handleResponse(new ClusterStateResponse(clusterName, clusterState));
+            } else {
+                throw new UnsupportedOperationException("Mock transport does not understand action " + action);
+            }
             return;
         }
 

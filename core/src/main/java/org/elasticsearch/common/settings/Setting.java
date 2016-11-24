3c5e787f245118efc49ce4770a5dcab37b3c42a7
Simon Willnauer <simonw@apache.org>
2016-11-24 20:17:42 +0100

    fix javadoc

--- core/src/main/java/org/elasticsearch/common/settings/Setting.java
+++ core/src/main/java/org/elasticsearch/common/settings/Setting.java
@@ -314,9 +314,9 @@ public class Setting<T> extends ToXContentToBytes {
     /**
      * Add this setting to the builder if it doesn't exists in the source settings.
      * The value added to the builder is taken from the given default settings object.
-     * @param builder
-     * @param source
-     * @param defaultSettings
+     * @param builder the settings builder to fill the diff into
+     * @param source the source settings object to diff
+     * @param defaultSettings the default settings object to diff against
      */
     public void diff(Settings.Builder builder, Settings source, Settings defaultSettings) {
         if (exists(source) == false) {

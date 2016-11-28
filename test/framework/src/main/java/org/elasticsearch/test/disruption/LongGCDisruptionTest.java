04ace7556b29a7c275f7559efd871ca4762e6b29
Yannick Welsch <yannick@welsch.lu>
2016-11-28 11:24:25 +0100

    Detect nodes being blocked by GC-disrupted node (#21797)
    The disruption type LongGCDisruption simulates GCs on a node by suspending all the threads of that node. If the suspended threads are in a code section with shared JVM locks, however, it can prevent the other nodes from doing their thing. The class LongGCDisruption has a list of class names for which we know that this can occur. Whenever a test using the GC disruption type fails in mysterious ways, it becomes a long guessing game to find the offending class. This commit adds code to LongGCDisruption to automatically detect these situations, fail the test early and report the offending class and all relevant context.

--- test/framework/src/main/java/org/elasticsearch/test/disruption/LongGCDisruptionTest.java
+++ test/framework/src/main/java/org/elasticsearch/test/disruption/LongGCDisruptionTest.java
@@ -18,11 +18,15 @@
  */
 package org.elasticsearch.test.disruption;
 
+import org.elasticsearch.common.Nullable;
 import org.elasticsearch.test.ESTestCase;
 
+import java.lang.management.ThreadInfo;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.regex.Pattern;
 
@@ -146,6 +150,96 @@ public class LongGCDisruptionTest extends ESTestCase {
             assertBusy(() -> assertThat(ops.get(), greaterThan(first)));
         } finally {
             stop.set(true);
+        }
+    }
+
+    public void testBlockDetection() throws Exception {
+        final String disruptedNodeName = "disrupted_node";
+        final String blockedNodeName = "blocked_node";
+        CountDownLatch waitForBlockDetectionResult = new CountDownLatch(1);
+        AtomicReference<ThreadInfo> blockDetectionResult = new AtomicReference<>();
+        LongGCDisruption disruption = new LongGCDisruption(random(), disruptedNodeName) {
+            @Override
+            protected Pattern[] getUnsafeClasses() {
+                return new Pattern[0];
+            }
+
+            @Override
+            protected void onBlockDetected(ThreadInfo blockedThread, @Nullable ThreadInfo blockingThread) {
+                blockDetectionResult.set(blockedThread);
+                waitForBlockDetectionResult.countDown();
+            }
+
+            @Override
+            protected long getBlockDetectionIntervalInMillis() {
+                return 10L;
+            }
+        };
+        if (disruption.isBlockDetectionSupported() == false) {
+            return;
+        }
+        final AtomicBoolean stop = new AtomicBoolean();
+        final CountDownLatch underLock = new CountDownLatch(1);
+        final CountDownLatch pauseUnderLock = new CountDownLatch(1);
+        final LockedExecutor lockedExecutor = new LockedExecutor();
+        final AtomicLong ops = new AtomicLong();
+        try {
+            for (int i = 0; i < 5; i++) {
+                // at least one locked and one none lock thread
+                final boolean lockedExec = (i < 4 && randomBoolean()) || i == 0;
+                Thread thread = new Thread(() -> {
+                    while (stop.get() == false) {
+                        if (lockedExec) {
+                            lockedExecutor.executeLocked(() -> {
+                                try {
+                                    underLock.countDown();
+                                    ops.incrementAndGet();
+                                    pauseUnderLock.await();
+                                } catch (InterruptedException e) {
+
+                                }
+                            });
+                        } else {
+                            ops.incrementAndGet();
+                        }
+                    }
+                });
+
+                thread.setName("[" + disruptedNodeName + "][" + i + "]");
+                thread.start();
+            }
+
+            for (int i = 0; i < 5; i++) {
+                // at least one locked and one none lock thread
+                final boolean lockedExec = (i < 4 && randomBoolean()) || i == 0;
+                Thread thread = new Thread(() -> {
+                    while (stop.get() == false) {
+                        if (lockedExec) {
+                            lockedExecutor.executeLocked(() -> {
+                                ops.incrementAndGet();
+                            });
+                        } else {
+                            ops.incrementAndGet();
+                        }
+                    }
+                });
+                thread.setName("[" + blockedNodeName + "][" + i + "]");
+                thread.start();
+            }
+            // make sure some threads of test_node are under lock
+            underLock.await();
+            disruption.startDisrupting();
+            waitForBlockDetectionResult.await(30, TimeUnit.SECONDS);
+            disruption.stopDisrupting();
+
+            ThreadInfo threadInfo = blockDetectionResult.get();
+            assertNotNull(threadInfo);
+            assertThat(threadInfo.getThreadName(), containsString("[" + blockedNodeName + "]"));
+            assertThat(threadInfo.getLockOwnerName(), containsString("[" + disruptedNodeName + "]"));
+            assertThat(threadInfo.getLockInfo().getClassName(), containsString(ReentrantLock.class.getName()));
+        } finally {
+            stop.set(true);
+            pauseUnderLock.countDown();
         }
     }
 }

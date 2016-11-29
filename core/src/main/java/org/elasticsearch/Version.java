e23a91032d71b6446ee4f4aad1a83d15d43c4718
Jim Ferenczi <jim.ferenczi@elastic.co>
2016-11-29 18:40:31 +0100

    Upgrade to lucene-6.4.0-snapshot-ec38570 (#21853)
    Set lucene version to 6.4.0-snapshot-ec38570 and update all the sha1s/license
    Fix invalid combo after upgrade in query_string query. split_on_whitespace=false is disallowed if auto_generate_phrase_queries=true
    Adapt the expectations of some tests to the new format of the Lucene explain output

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -102,7 +102,7 @@ public class Version {
     public static final int V_5_1_0_ID_UNRELEASED = 5010099;
     public static final Version V_5_1_0_UNRELEASED = new Version(V_5_1_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_3_0);
     public static final int V_5_2_0_ID_UNRELEASED = 5020099;
-    public static final Version V_5_2_0_UNRELEASED = new Version(V_5_2_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_3_0);
+    public static final Version V_5_2_0_UNRELEASED = new Version(V_5_2_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_4_0);
     public static final Version CURRENT = V_5_2_0_UNRELEASED;
 
     // unreleased versions must be added to the above list with the suffix _UNRELEASED (with the exception of CURRENT)

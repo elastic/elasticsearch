2465853548a8dee258159df54b4c4fcab3f54b51
Shai Erera <serera@gmail.com>
2016-12-01 22:11:59 +0200

    Return correct term statistics whem a field is not found in a shard
    If you ask for the term vectors of an artificial document with
    term_statistics=true, but a shard does not have any terms of the doc's
    field(s), it returns the doc's term vectors values as the shard-level
    term statistics. This commit fixes that to return 0 for ttf and also
    field-level aggregated statistics.
    
    This closes #21906

--- core/src/main/java/org/elasticsearch/action/termvectors/TermVectorsWriter.java
+++ core/src/main/java/org/elasticsearch/action/termvectors/TermVectorsWriter.java
@@ -71,7 +71,7 @@ final class TermVectorsWriter {
 
             // if no terms found, take the retrieved term vector fields for stats
             if (topLevelTerms == null) {
-                topLevelTerms = fieldTermVector;
+                topLevelTerms = EMPTY_TERMS;
             }
 
             TermsEnum topLevelIterator = topLevelTerms.iterator();
@@ -292,4 +292,30 @@ final class TermVectorsWriter {
         // further...
         output.writeVLong(Math.max(0, value + 1));
     }
+
+    /** Implements an empty {@link Terms}. */
+    private static final Terms EMPTY_TERMS = new Terms() {
+        @Override public TermsEnum iterator() throws IOException { return EMPTY_TERMS_ENUM; }
+        @Override public long size() throws IOException { return 0; }
+        @Override public long getSumTotalTermFreq() throws IOException { return 0; }
+        @Override public long getSumDocFreq() throws IOException { return 0; }
+        @Override public int getDocCount() throws IOException { return 0; }
+        @Override public boolean hasFreqs() { return false; }
+        @Override public boolean hasOffsets() { return false; }
+        @Override public boolean hasPositions() { return false; }
+        @Override public boolean hasPayloads() { return false; }
+    };
+
+    /** Implements an empty {@link TermsEnum}. */
+    private static final TermsEnum EMPTY_TERMS_ENUM = new TermsEnum() {
+        @Override public BytesRef next() throws IOException { return null; }
+        @Override public SeekStatus seekCeil(BytesRef text) throws IOException { return SeekStatus.END; }
+        @Override public void seekExact(long ord) throws IOException { }
+        @Override public BytesRef term() throws IOException { return null; }
+        @Override public long ord() throws IOException { return 0; }
+        @Override public int docFreq() throws IOException { return 0; }
+        @Override public long totalTermFreq() throws IOException { return 0; }
+        @Override public PostingsEnum postings(PostingsEnum reuse, int flags) throws IOException { return null; }
+    };
+
 }

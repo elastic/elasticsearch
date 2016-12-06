6f4aabd4f18d40d437e0db62a0c49a1983a71bf8
Christoph Büscher <christoph@elastic.co>
2016-12-06 15:39:30 +0100

    Add fromXContent to HighlightField
    This adds a fromXContent method and unit test to the HighlightField class so we
    can parse it as part of a serch response. This is part of the preparation for
    parsing search responses on the client side.

--- core/src/main/java/org/elasticsearch/search/internal/InternalSearchHit.java
+++ core/src/main/java/org/elasticsearch/search/internal/InternalSearchHit.java
@@ -471,7 +471,8 @@ public class InternalSearchHit implements SearchHit {
             builder.field(Fields._SCORE, score);
         }
         for (SearchHitField field : metaFields) {
-            builder.field(field.name(), (Object) field.value());
+            Object value = (Object) field.value();
+            builder.field(field.name(), value); 
         }
         if (source != null) {
             XContentHelper.writeRawField("_source", source, builder, params);
@@ -490,16 +491,7 @@ public class InternalSearchHit implements SearchHit {
         if (highlightFields != null && !highlightFields.isEmpty()) {
             builder.startObject(Fields.HIGHLIGHT);
             for (HighlightField field : highlightFields.values()) {
-                builder.field(field.name());
-                if (field.fragments() == null) {
-                    builder.nullValue();
-                } else {
-                    builder.startArray();
-                    for (Text fragment : field.fragments()) {
-                        builder.value(fragment);
-                    }
-                    builder.endArray();
-                }
+                field.toXContent(builder, params);
             }
             builder.endObject();
         }

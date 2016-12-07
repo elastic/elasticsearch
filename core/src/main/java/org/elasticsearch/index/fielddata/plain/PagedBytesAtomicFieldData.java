cbd43401300a76e6f4431654f8b32beb94938897
Adrien Grand <jpountz@gmail.com>
2016-12-07 10:32:11 +0100

    Expose `ip` fields as strings in scripts. (#21997)
    Currently we expose the internal representation that we use for ip addresses,
    which are the ipv6 bytes. However, this is not really usable, exposes internal
    implementation details and also does not work fine with other APIs that expect
    that the values can be `toString`'d.
    
    Closes #21977

--- core/src/main/java/org/elasticsearch/index/fielddata/plain/PagedBytesAtomicFieldData.java
+++ core/src/main/java/org/elasticsearch/index/fielddata/plain/PagedBytesAtomicFieldData.java
@@ -40,6 +40,7 @@ public class PagedBytesAtomicFieldData extends AbstractAtomicOrdinalsFieldData {
     protected final Ordinals ordinals;
 
     public PagedBytesAtomicFieldData(PagedBytes.Reader bytes, PackedLongValues termOrdToBytesOffset, Ordinals ordinals) {
+        super(DEFAULT_SCRIPT_FUNCTION);
         this.bytes = bytes;
         this.termOrdToBytesOffset = termOrdToBytesOffset;
         this.ordinals = ordinals;

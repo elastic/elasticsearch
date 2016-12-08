581b1be217a81d8c3f59e52ceb82efff4c498f04
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2016-12-08 14:58:00 +0000

    Updates aws credential variables in upload task
    Original commit: elastic/x-pack-elasticsearch@4fc9f0239282471a1419d1a808a6febfd41e1f87

--- buildSrc/src/main/groovy/org/elastic/gradle/UploadS3Task.groovy
+++ buildSrc/src/main/groovy/org/elastic/gradle/UploadS3Task.groovy
@@ -51,7 +51,7 @@ class UploadS3Task extends DefaultTask {
 
     @TaskAction
     public void uploadToS3() {
-        AWSCredentials creds = new BasicAWSCredentials(project.awsAccessKey, project.awsSecretKey)
+        AWSCredentials creds = new BasicAWSCredentials(project.prelertAwsAccessKey, project.prelertAwsSecretKey)
 
         ClientConfiguration clientConfiguration = new ClientConfiguration();
         // the response metadata cache is only there for diagnostics purposes,

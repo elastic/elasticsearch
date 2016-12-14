42d49cddd6dacaf3c39be89533829366bd097fde
Jason Tedor <jason@tedor.me>
2016-12-13 19:10:11 -0500

    Add shutdown hook for closing CLI commands
    This commit enables CLI commands to be closeable and installs a runtime
    shutdown hook to ensure that if the JVM shuts down (as opposed to
    aborting) the close method is called.
    
    It is not enough to wrap uses of commands in main methods in
    try-with-resources blocks as these will not run if, say, the virtual
    machine is terminated in response to SIGINT, or system shutdown event.
    
    Relates #22126

--- core/src/main/java/org/elasticsearch/plugins/RemovePluginCommand.java
+++ core/src/main/java/org/elasticsearch/plugins/RemovePluginCommand.java
@@ -43,7 +43,7 @@ import static org.elasticsearch.cli.Terminal.Verbosity.VERBOSE;
 /**
  * A command for the plugin cli to remove a plugin from elasticsearch.
  */
-final class RemovePluginCommand extends SettingCommand {
+class RemovePluginCommand extends SettingCommand {
 
     private final OptionSpec<String> arguments;
 

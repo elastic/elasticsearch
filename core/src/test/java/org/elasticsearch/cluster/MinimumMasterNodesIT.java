bf65a69bbf6d680e922676f6e5836fee27c4684f
Boaz Leskes <b.leskes@gmail.com>
2016-12-14 20:14:16 +0100

    Enforce min master nodes in test cluster (#22065)
    In order to start clusters with min master nodes set without setting `discovery.initial_state_timeout`, #21846 has changed the way we start nodes. Instead to the previous serial start up, we now always start the nodes in an async fashion (internally). This means that starting a cluster is unsafe without `min_master_nodes` being set. We should therefore make it mandatory.

--- core/src/test/java/org/elasticsearch/cluster/MinimumMasterNodesIT.java
+++ core/src/test/java/org/elasticsearch/cluster/MinimumMasterNodesIT.java
@@ -268,7 +268,7 @@ public class MinimumMasterNodesIT extends ESIntegTestCase {
     public void testDynamicUpdateMinimumMasterNodes() throws Exception {
         Settings settings = Settings.builder()
                 .put(ZenDiscovery.PING_TIMEOUT_SETTING.getKey(), "400ms")
-                .put("discovery.initial_state_timeout", "500ms")
+                .put(ElectMasterService.DISCOVERY_ZEN_MINIMUM_MASTER_NODES_SETTING.getKey(), "1")
                 .build();
 
         logger.info("--> start first node and wait for it to be a master");

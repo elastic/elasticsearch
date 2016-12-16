6e0cc4da01be3e5567f95fdbcb162339a68a4a36
Adrien Grand <jpountz@gmail.com>
2016-12-16 14:57:58 +0100

    Fix MapperService.allEnabled().
    It returns whether the last merged mapping has `_all` enabled rather than
    whether any of the types has `_all` enabled.

--- core/src/main/java/org/elasticsearch/index/mapper/MapperService.java
+++ core/src/main/java/org/elasticsearch/index/mapper/MapperService.java
@@ -153,7 +153,7 @@ public class MapperService extends AbstractIndexComponent implements Closeable {
     }
 
     /**
-     * Returns true if the "_all" field is enabled for the type
+     * Returns true if the "_all" field is enabled on any type.
      */
     public boolean allEnabled() {
         return this.allEnabled;
@@ -377,7 +377,7 @@ public class MapperService extends AbstractIndexComponent implements Closeable {
         this.hasNested = hasNested;
         this.fullPathObjectMappers = fullPathObjectMappers;
         this.parentTypes = parentTypes;
-        this.allEnabled = mapper.allFieldMapper().enabled();
+        this.allEnabled |= mapper.allFieldMapper().enabled();
 
         assert assertSerialization(newMapper);
         assert assertMappersShareSameFieldType();

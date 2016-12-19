621baf3cb253aa7f21d164dc5649804a40ed1ca1
Ali Beyad <ali@elastic.co>
2016-12-19 13:19:32 -0500

    Adds setting level to allocation decider explanations
    The allocation decider explanation messages where improved in #21771 to
    include the specific Elasticsearch setting that contributed to the
    decision taken by the decider.  This commit improves upon the
    explanation message output by including whether the setting was an index
    level setting or a cluster level setting.  This will further help the
    user understand and locate the setting that is the cause of shards
    remaining unassigned or remaining on their current node.

--- core/src/main/java/org/elasticsearch/cluster/routing/allocation/decider/EnableAllocationDecider.java
+++ core/src/main/java/org/elasticsearch/cluster/routing/allocation/decider/EnableAllocationDecider.java
@@ -189,10 +189,12 @@ public class EnableAllocationDecider extends AllocationDecider {
     }
 
     private static String setting(Rebalance rebalance, boolean usedIndexSetting) {
-        StringBuilder buf = new StringBuilder("[");
+        StringBuilder buf = new StringBuilder();
         if (usedIndexSetting) {
+            buf.append("index setting [");
             buf.append(INDEX_ROUTING_REBALANCE_ENABLE_SETTING.getKey());
         } else {
+            buf.append("cluster setting [");
             buf.append(CLUSTER_ROUTING_REBALANCE_ENABLE_SETTING.getKey());
         }
         buf.append("=").append(rebalance.toString().toLowerCase(Locale.ROOT)).append("]");

0eaaee160b3ca642238b765a1d719af66a6ec9da
Boaz Leskes <b.leskes@gmail.com>
2016-12-21 13:19:03 +0100

    better code sharding?

--- core/src/main/java/org/elasticsearch/action/delete/TransportDeleteAction.java
+++ core/src/main/java/org/elasticsearch/action/delete/TransportDeleteAction.java
@@ -19,30 +19,18 @@
 
 package org.elasticsearch.action.delete;
 
-import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.action.bulk.BulkShardRequest;
-import org.elasticsearch.action.bulk.BulkShardResponse;
 import org.elasticsearch.action.bulk.TransportBulkAction;
 import org.elasticsearch.action.bulk.TransportShardBulkAction;
+import org.elasticsearch.action.bulk.TransportSingleItemBulkWriteAction;
 import org.elasticsearch.action.support.ActionFilters;
-import org.elasticsearch.action.support.replication.TransportWriteAction;
 import org.elasticsearch.cluster.action.shard.ShardStateAction;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.service.ClusterService;
-import org.elasticsearch.common.collect.Tuple;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.indices.IndicesService;
-import org.elasticsearch.tasks.Task;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
-
-import static org.elasticsearch.action.bulk.SingleWriteOperationUtility.executeSingleItemBulkRequestOnPrimary;
-import static org.elasticsearch.action.bulk.SingleWriteOperationUtility.executeSingleItemBulkRequestOnReplica;
-import static org.elasticsearch.action.bulk.SingleWriteOperationUtility.toSingleItemBulkRequest;
-import static org.elasticsearch.action.bulk.SingleWriteOperationUtility.wrapBulkResponse;
-import static org.elasticsearch.action.bulk.SingleWriteOperationUtility.ResultHolder;
 
 /**
  * Performs the delete operation.
@@ -50,10 +38,7 @@ import static org.elasticsearch.action.bulk.SingleWriteOperationUtility.ResultHo
  * Deprecated use TransportBulkAction with a single item instead
  */
 @Deprecated
-public class TransportDeleteAction extends TransportWriteAction<DeleteRequest, DeleteRequest, DeleteResponse> {
-
-    private final TransportBulkAction bulkAction;
-    private final TransportShardBulkAction shardBulkAction;
+public class TransportDeleteAction extends TransportSingleItemBulkWriteAction<DeleteRequest, DeleteResponse> {
 
     @Inject
     public TransportDeleteAction(Settings settings, TransportService transportService, ClusterService clusterService,
@@ -61,41 +46,12 @@ public class TransportDeleteAction extends TransportWriteAction<DeleteRequest, D
                                  ActionFilters actionFilters, IndexNameExpressionResolver indexNameExpressionResolver,
                                  TransportBulkAction bulkAction, TransportShardBulkAction shardBulkAction) {
         super(settings, DeleteAction.NAME, transportService, clusterService, indicesService, threadPool, shardStateAction,
-                actionFilters, indexNameExpressionResolver, DeleteRequest::new, DeleteRequest::new, ThreadPool.Names.INDEX);
-        this.bulkAction = bulkAction;
-        this.shardBulkAction = shardBulkAction;
-    }
-
-    @Override
-    protected void doExecute(Task task, final DeleteRequest request, final ActionListener<DeleteResponse> listener) {
-        bulkAction.execute(task, toSingleItemBulkRequest(request), wrapBulkResponse(listener));
+            actionFilters, indexNameExpressionResolver, DeleteRequest::new, DeleteRequest::new, ThreadPool.Names.INDEX,
+            bulkAction, shardBulkAction);
     }
 
     @Override
     protected DeleteResponse newResponseInstance() {
         return new DeleteResponse();
-    }
-
-    @Override
-    protected WritePrimaryResult<DeleteRequest, DeleteResponse> shardOperationOnPrimary(
-            DeleteRequest request, IndexShard primary) throws Exception {
-        ResultHolder<DeleteResponse> resultHolder = executeSingleItemBulkRequestOnPrimary(request,
-                bulkShardRequest -> {
-                    WritePrimaryResult<BulkShardRequest, BulkShardResponse> result =
-                            shardBulkAction.shardOperationOnPrimary(bulkShardRequest, primary);
-                    return new Tuple<>(result.finalResponseIfSuccessful, result.location);
-                }
-        );
-        return new WritePrimaryResult<>(request, resultHolder.response, resultHolder.location, resultHolder.failure, primary, logger);
-    }
-
-    @Override
-    protected WriteReplicaResult<DeleteRequest> shardOperationOnReplica(
-            DeleteRequest request, IndexShard replica) throws Exception {
-        ResultHolder resultHolder = executeSingleItemBulkRequestOnReplica(request, bulkShardRequest -> {
-            WriteReplicaResult<BulkShardRequest> result = shardBulkAction.shardOperationOnReplica(bulkShardRequest, replica);
-            return result.location;
-        });
-        return new WriteReplicaResult<>(request, resultHolder.location, null, replica, logger);
     }
 }
--- core/src/main/java/org/elasticsearch/action/index/TransportIndexAction.java
+++ core/src/main/java/org/elasticsearch/action/index/TransportIndexAction.java
@@ -19,30 +19,18 @@
 
 package org.elasticsearch.action.index;
 
-import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.action.bulk.BulkShardRequest;
-import org.elasticsearch.action.bulk.BulkShardResponse;
-import org.elasticsearch.action.bulk.SingleWriteOperationUtility.ResultHolder;
 import org.elasticsearch.action.bulk.TransportBulkAction;
 import org.elasticsearch.action.bulk.TransportShardBulkAction;
+import org.elasticsearch.action.bulk.TransportSingleItemBulkWriteAction;
 import org.elasticsearch.action.support.ActionFilters;
-import org.elasticsearch.action.support.replication.TransportWriteAction;
 import org.elasticsearch.cluster.action.shard.ShardStateAction;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.service.ClusterService;
-import org.elasticsearch.common.collect.Tuple;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.indices.IndicesService;
-import org.elasticsearch.tasks.Task;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
-
-import static org.elasticsearch.action.bulk.SingleWriteOperationUtility.executeSingleItemBulkRequestOnReplica;
-import static org.elasticsearch.action.bulk.SingleWriteOperationUtility.executeSingleItemBulkRequestOnPrimary;
-import static org.elasticsearch.action.bulk.SingleWriteOperationUtility.toSingleItemBulkRequest;
-import static org.elasticsearch.action.bulk.SingleWriteOperationUtility.wrapBulkResponse;
 
 /**
  * Performs the index operation.
@@ -57,10 +45,7 @@ import static org.elasticsearch.action.bulk.SingleWriteOperationUtility.wrapBulk
  * Deprecated use TransportBulkAction with a single item instead
  */
 @Deprecated
-public class TransportIndexAction extends TransportWriteAction<IndexRequest, IndexRequest, IndexResponse> {
-
-    private final TransportBulkAction bulkAction;
-    private final TransportShardBulkAction shardBulkAction;
+public class TransportIndexAction extends TransportSingleItemBulkWriteAction<IndexRequest, IndexResponse> {
 
     @Inject
     public TransportIndexAction(Settings settings, TransportService transportService, ClusterService clusterService,
@@ -69,43 +54,12 @@ public class TransportIndexAction extends TransportWriteAction<IndexRequest, Ind
                                 ActionFilters actionFilters, IndexNameExpressionResolver indexNameExpressionResolver,
                                 TransportBulkAction bulkAction, TransportShardBulkAction shardBulkAction) {
         super(settings, IndexAction.NAME, transportService, clusterService, indicesService, threadPool, shardStateAction,
-                actionFilters, indexNameExpressionResolver, IndexRequest::new, IndexRequest::new, ThreadPool.Names.INDEX);
-        this.bulkAction = bulkAction;
-        this.shardBulkAction = shardBulkAction;
-    }
-
-    @Override
-    protected void doExecute(Task task, final IndexRequest request, final ActionListener<IndexResponse> listener) {
-        bulkAction.execute(task, toSingleItemBulkRequest(request), wrapBulkResponse(listener));
+            actionFilters, indexNameExpressionResolver, IndexRequest::new, IndexRequest::new, ThreadPool.Names.INDEX,
+            bulkAction, shardBulkAction);
     }
 
     @Override
     protected IndexResponse newResponseInstance() {
         return new IndexResponse();
-    }
-
-    @Override
-    protected WritePrimaryResult<IndexRequest, IndexResponse> shardOperationOnPrimary(
-            IndexRequest request, final IndexShard primary) throws Exception {
-        ResultHolder<IndexResponse> resultHolder = executeSingleItemBulkRequestOnPrimary(request,
-                bulkShardRequest -> {
-                    WritePrimaryResult<BulkShardRequest, BulkShardResponse> result =
-                            shardBulkAction.shardOperationOnPrimary(bulkShardRequest, primary);
-                    return new Tuple<>(result.finalResponseIfSuccessful, result.location);
-                }
-        );
-        return new WritePrimaryResult<>(request, resultHolder.response, resultHolder.location, resultHolder.failure, primary, logger);
-    }
-
-    @Override
-    protected WriteReplicaResult<IndexRequest> shardOperationOnReplica(
-            IndexRequest request, IndexShard replica) throws Exception {
-        ResultHolder resultHolder = executeSingleItemBulkRequestOnReplica(request,
-                bulkShardRequest -> {
-                    WriteReplicaResult<BulkShardRequest> result = shardBulkAction.shardOperationOnReplica(bulkShardRequest, replica);
-                    return result.location;
-                }
-        );
-        return new WriteReplicaResult<>(request, resultHolder.location, null, replica, logger);
     }
 }

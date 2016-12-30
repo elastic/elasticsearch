2b92ee71bc86eb0b958be183de3c06dfbfd8d61f
Adrien Grand <jpountz@gmail.com>
2016-12-30 10:54:20 +0100

    Fix compilation.
    Original commit: elastic/x-pack-elasticsearch@8e845fe3b3e38ce7704d6ce457d7f720346b49cf

--- elasticsearch/src/test/java/org/elasticsearch/xpack/security/authz/accesscontrol/SecurityIndexSearcherWrapperUnitTests.java
+++ elasticsearch/src/test/java/org/elasticsearch/xpack/security/authz/accesscontrol/SecurityIndexSearcherWrapperUnitTests.java
@@ -128,7 +128,7 @@ public class SecurityIndexSearcherWrapperUnitTests extends ESTestCase {
         indexSettings = IndexSettingsModule.newIndexSettings(index, Settings.EMPTY);
         NamedAnalyzer namedAnalyzer = new NamedAnalyzer("default", AnalyzerScope.INDEX, new StandardAnalyzer());
         IndexAnalyzers indexAnalyzers = new IndexAnalyzers(indexSettings, namedAnalyzer, namedAnalyzer, namedAnalyzer,
-                Collections.emptyMap());
+                Collections.emptyMap(), Collections.emptyMap());
         SimilarityService similarityService = new SimilarityService(indexSettings, Collections.emptyMap());
         mapperService = new MapperService(indexSettings, indexAnalyzers, xContentRegistry(), similarityService,
                 new IndicesModule(emptyList()).getMapperRegistry(), () -> null);

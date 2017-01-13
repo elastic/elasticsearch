f96cd45567b8902349065ff8d3c4e30bfd0f8765
Michael McCandless <mike@elastic.co>
2017-01-12 19:00:39 -0500

    Source filtering: only accept array items if the previous include pattern matches (#22593)
    Source filtering was always accepting array items even if the include pattern did not match.
    
    Closes #22557

--- core/src/main/java/org/elasticsearch/common/xcontent/support/XContentMapValues.java
+++ core/src/main/java/org/elasticsearch/common/xcontent/support/XContentMapValues.java
@@ -266,7 +266,7 @@ public class XContentMapValues {
 
                 List<Object> filteredValue = filter((Iterable<?>) value,
                         subIncludeAutomaton, subIncludeState, excludeAutomaton, excludeState, matchAllAutomaton);
-                if (includeAutomaton.isAccept(includeState) || filteredValue.isEmpty() == false) {
+                if (filteredValue.isEmpty() == false) {
                     filtered.put(key, filteredValue);
                 }
 
@@ -289,6 +289,7 @@ public class XContentMapValues {
             CharacterRunAutomaton excludeAutomaton, int initialExcludeState,
             CharacterRunAutomaton matchAllAutomaton) {
         List<Object> filtered = new ArrayList<>();
+        boolean isInclude = includeAutomaton.isAccept(initialIncludeState);
         for (Object value : iterable) {
             if (value instanceof Map) {
                 int includeState = includeAutomaton.step(initialIncludeState, '.');
@@ -307,9 +308,8 @@ public class XContentMapValues {
                 if (filteredValue.isEmpty() == false) {
                     filtered.add(filteredValue);
                 }
-            } else {
-                // TODO: we have tests relying on this behavior on arrays even
-                // if the path does not match, but this looks like a bug?
+            } else if (isInclude) {
+                // #22557: only accept this array value if the key we are on is accepted:
                 filtered.add(value);
             }
         }

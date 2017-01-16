cea70f4d4ac2455c714e1fc8b33e307c16aa55c8
Jason Tedor <jason@tedor.me>
2017-01-16 16:13:04 -0500

    Remove unneeded assertion disjunction in engine
    This commit removes an unneeded disjunction in an assertion in the
    engine.

--- core/src/main/java/org/elasticsearch/index/engine/Engine.java
+++ core/src/main/java/org/elasticsearch/index/engine/Engine.java
@@ -361,7 +361,7 @@ public abstract class Engine implements Closeable {
 
         void setTranslogLocation(Translog.Location translogLocation) {
             if (freeze.get() == null) {
-                assert failure == null || translogLocation == null: "failure has to be null to set translog location";
+                assert failure == null : "failure has to be null to set translog location";
                 this.translogLocation = translogLocation;
             } else {
                 throw new IllegalStateException("result is already frozen");

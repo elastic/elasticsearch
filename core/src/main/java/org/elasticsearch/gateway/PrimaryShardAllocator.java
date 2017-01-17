72269bdd1cf5d0a9b7eb2a55c9e5fd9d525dc49b
Boaz Leskes <b.leskes@gmail.com>
2017-01-17 18:04:53 +0100

    feedback

--- core/src/main/java/org/elasticsearch/gateway/PrimaryShardAllocator.java
+++ core/src/main/java/org/elasticsearch/gateway/PrimaryShardAllocator.java
@@ -476,7 +476,7 @@ public abstract class PrimaryShardAllocator extends BaseGatewayShardAllocator {
      * recovered on any node
      */
     private boolean recoverOnAnyNode(IndexMetaData metaData) {
-        // don't use the settings directly, not to trigger manny deprecation logging
+        // don't use the setting directly, not to trigger verbose deprecation logging
         return (IndexMetaData.isOnSharedFilesystem(metaData.getSettings()) || IndexMetaData.isOnSharedFilesystem(this.settings))
             && (metaData.getSettings().getAsBoolean(IndexMetaData.SETTING_SHARED_FS_ALLOW_RECOVERY_ON_ANY_NODE, false) ||
                 this.settings.getAsBoolean(IndexMetaData.SETTING_SHARED_FS_ALLOW_RECOVERY_ON_ANY_NODE, false));

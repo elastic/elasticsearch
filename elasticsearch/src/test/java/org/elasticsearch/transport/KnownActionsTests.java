5aae30f7220794204a7ad33434dabf655686eb29
Ali Beyad <ali@elastic.co>
2017-01-18 16:42:08 -0500

    [TEST] fixes known handler test
    Original commit: elastic/x-pack-elasticsearch@98a8f23d238ed177bab6302cf95886ef8fcb4d88

--- elasticsearch/src/test/java/org/elasticsearch/transport/KnownActionsTests.java
+++ elasticsearch/src/test/java/org/elasticsearch/transport/KnownActionsTests.java
@@ -75,7 +75,7 @@ public class KnownActionsTests extends SecurityIntegTestCase {
     public void testAllTransportHandlersAreKnown() {
         TransportService transportService = internalCluster().getDataNodeInstance(TransportService.class);
         for (String handler : transportService.requestHandlers.keySet()) {
-            if (!knownHandlers.contains(handler)) {
+            if (!knownActions.contains(handler)) {
                 assertThat("elasticsearch core transport handler [" + handler + "] is unknown to security", knownHandlers,
                         hasItem(handler));
             }

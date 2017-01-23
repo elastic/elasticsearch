23c9a8772dbedfb9897701784737a9f3712f1667
Alexander Reelsen <alexander@reelsen.net>
2017-01-23 12:20:10 +0100

    Tests: Added 2.4.4 bwc indices, removed 2.4.5 snapshot version

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -81,8 +81,6 @@ public class Version {
     public static final Version V_2_4_3 = new Version(V_2_4_3_ID, org.apache.lucene.util.Version.LUCENE_5_5_2);
     public static final int V_2_4_4_ID = 2040499;
     public static final Version V_2_4_4 = new Version(V_2_4_4_ID, org.apache.lucene.util.Version.LUCENE_5_5_2);
-    public static final int V_2_4_5_ID = 2040599;
-    public static final Version V_2_4_5 = new Version(V_2_4_5_ID, org.apache.lucene.util.Version.LUCENE_5_5_2);
     public static final int V_5_0_0_alpha1_ID = 5000001;
     public static final Version V_5_0_0_alpha1 = new Version(V_5_0_0_alpha1_ID, org.apache.lucene.util.Version.LUCENE_6_0_0);
     public static final int V_5_0_0_alpha2_ID = 5000002;
@@ -163,8 +161,6 @@ public class Version {
                 return V_5_0_0_alpha2;
             case V_5_0_0_alpha1_ID:
                 return V_5_0_0_alpha1;
-            case V_2_4_5_ID:
-                return V_2_4_5;
             case V_2_4_4_ID:
                 return V_2_4_4;
             case V_2_4_3_ID:

2bd927d8720c12aba69b255ca207f1900c3f6c53
Simon Willnauer <simonw@apache.org>
2017-01-23 10:15:51 +0100

    fix line len

--- core/src/test/java/org/elasticsearch/action/IndicesRequestIT.java
+++ core/src/test/java/org/elasticsearch/action/IndicesRequestIT.java
@@ -754,7 +754,8 @@ public class IndicesRequestIT extends ESIntegTestCase {
 
         @Override
         public <T extends TransportRequest> TransportRequestHandler<T> interceptHandler(String action, String executor,
-                                                                                        TransportRequestHandler<T> actualHandler, boolean forceExecution) {
+                                                                                        TransportRequestHandler<T> actualHandler,
+                                                                                        boolean forceExecution) {
             return new InterceptingRequestHandler<>(action, actualHandler);
         }
 

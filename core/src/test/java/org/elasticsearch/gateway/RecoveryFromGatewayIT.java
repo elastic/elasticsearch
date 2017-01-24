32c6702c6e569f67bb8aa7b382b652db47c86d06
Jason Tedor <jason@tedor.me>
2017-01-23 19:24:09 -0500

    More trace logging for test

--- core/src/test/java/org/elasticsearch/gateway/RecoveryFromGatewayIT.java
+++ core/src/test/java/org/elasticsearch/gateway/RecoveryFromGatewayIT.java
@@ -386,7 +386,7 @@ public class RecoveryFromGatewayIT extends ESIntegTestCase {
     }
 
     // test fails with seed FE6770A74885D66E
-    @TestLogging("org.elasticsearch.indices.recovery:TRACE")
+    @TestLogging("org.elasticsearch.indices.recovery:TRACE,org.elasticsearch.index.shard.StoreRecovery:TRACE")
     public void testReusePeerRecovery() throws Exception {
         final Settings settings = Settings.builder()
             .put(MockFSIndexStore.INDEX_CHECK_INDEX_ON_CLOSE_SETTING.getKey(), false)

9bd254fa95abd19ba4ec0e316192c9feadc4265c
Yannick Welsch <yannick@welsch.lu>
2017-01-24 11:03:07 +0100

    Use Gradle-version specific source sets
    Instead of using Gradle-version specific compilation options, use distinct source sets. This also allows compilation of buildSrc/build-tools under IDEs that
    don't understand the version-specific compilation options.
    
    Relates to #22669

---
+++ buildSrc/src/main/gradle-2.13-groovy/org/elasticsearch/gradle/ProgressLoggerFactoryInjection.groovy
@@ -0,0 +1,35 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.elasticsearch.gradle
+
+import org.gradle.logging.ProgressLoggerFactory
+
+import javax.inject.Inject
+
+/**
+ * Allows to inject a ProgressLoggerFactory to tasks in src/main/groovy
+ * without requiring the corresponding import of ProgressLoggerFactory,
+ * making it compatible with both Gradle 2.13 and 2.14+.
+ */
+trait ProgressLoggerFactoryInjection {
+    @Inject
+    ProgressLoggerFactory getProgressLoggerFactory() {
+        throw new UnsupportedOperationException()
+    }
+}

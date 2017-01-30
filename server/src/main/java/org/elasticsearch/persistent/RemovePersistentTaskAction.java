777b21f2ef874ed918a84a39ad8fe7bbe079fa0e
Martijn van Groningen <martijn.v.groningen@gmail.com>
2017-01-30 16:23:05 +0100

    Add a number of auxiliary methods to persistent tasks classes.
    Original commit: elastic/x-pack@7f44b41b7a5b53a27a97d54c15ebbdf52bb00a87

--- server/src/main/java/org/elasticsearch/persistent/RemovePersistentTaskAction.java
+++ server/src/main/java/org/elasticsearch/persistent/RemovePersistentTaskAction.java
@@ -112,7 +112,7 @@ public class RemovePersistentTaskAction extends Action<RemovePersistentTaskActio
     }
 
     public static class Response extends AcknowledgedResponse {
-        protected Response() {
+        public Response() {
             super();
         }
 

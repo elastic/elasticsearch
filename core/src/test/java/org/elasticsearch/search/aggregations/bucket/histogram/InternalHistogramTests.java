ee6fe29dc66ae832e03d553bf6355aa6a1889f0b
Adrien Grand <jpountz@gmail.com>
2017-02-06 17:57:34 +0100

    Fix test bug.

--- core/src/test/java/org/elasticsearch/search/aggregations/bucket/histogram/InternalHistogramTests.java
+++ core/src/test/java/org/elasticsearch/search/aggregations/bucket/histogram/InternalHistogramTests.java
@@ -19,6 +19,7 @@
 
 package org.elasticsearch.search.aggregations.bucket.histogram;
 
+import org.apache.lucene.util.TestUtil;
 import org.elasticsearch.common.io.stream.Writeable.Reader;
 import org.elasticsearch.search.DocValueFormat;
 import org.elasticsearch.search.aggregations.InternalAggregationTestCase;
@@ -26,9 +27,9 @@ import org.elasticsearch.search.aggregations.InternalAggregations;
 import org.elasticsearch.search.aggregations.pipeline.PipelineAggregator;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class InternalHistogramTests extends InternalAggregationTestCase<InternalHistogram> {
 
@@ -42,7 +43,8 @@ public class InternalHistogramTests extends InternalAggregationTestCase<Internal
         final int interval = randomIntBetween(1, 3);
         List<InternalHistogram.Bucket> buckets = new ArrayList<>();
         for (int i = 0; i < numBuckets; ++i) {
-            buckets.add(new InternalHistogram.Bucket(base + i * interval, randomInt(50), keyed, format, InternalAggregations.EMPTY));
+            final int docCount = TestUtil.nextInt(random(), 1, 50);
+            buckets.add(new InternalHistogram.Bucket(base + i * interval, docCount, keyed, format, InternalAggregations.EMPTY));
         }
         return new InternalHistogram(name, buckets, (InternalOrder) InternalHistogram.Order.KEY_ASC,
                 1, null, format, keyed, pipelineAggregators, metaData);
@@ -50,14 +52,14 @@ public class InternalHistogramTests extends InternalAggregationTestCase<Internal
 
     @Override
     protected void assertReduced(InternalHistogram reduced, List<InternalHistogram> inputs) {
-        Map<Double, Long> expectedCounts = new HashMap<>();
+        Map<Double, Long> expectedCounts = new TreeMap<>();
         for (Histogram histogram : inputs) {
             for (Histogram.Bucket bucket : histogram.getBuckets()) {
                 expectedCounts.compute((Double) bucket.getKey(),
                         (key, oldValue) -> (oldValue == null ? 0 : oldValue) + bucket.getDocCount());
             }
         }
-        Map<Double, Long> actualCounts = new HashMap<>();
+        Map<Double, Long> actualCounts = new TreeMap<>();
         for (Histogram.Bucket bucket : reduced.getBuckets()) {
             actualCounts.compute((Double) bucket.getKey(),
                     (key, oldValue) -> (oldValue == null ? 0 : oldValue) + bucket.getDocCount());

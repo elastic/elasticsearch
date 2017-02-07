e15af55d6bd010444ce4e57a32ea9b28ea6c11b8
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2017-02-07 18:07:52 +0000

    Bumps elasticsearch version to 5.4.0

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -116,7 +116,9 @@ public class Version {
     public static final Version V_5_2_1_UNRELEASED = new Version(V_5_2_1_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_4_1);
     public static final int V_5_3_0_ID_UNRELEASED = 5030099;
     public static final Version V_5_3_0_UNRELEASED = new Version(V_5_3_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_4_1);
-    public static final Version CURRENT = V_5_3_0_UNRELEASED;
+    public static final int V_5_4_0_ID_UNRELEASED = 5040099;
+    public static final Version V_5_4_0_UNRELEASED = new Version(V_5_4_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_4_1);
+    public static final Version CURRENT = V_5_4_0_UNRELEASED;
 
     // unreleased versions must be added to the above list with the suffix _UNRELEASED (with the exception of CURRENT)
 
@@ -131,6 +133,8 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_5_4_0_ID_UNRELEASED:
+                return V_5_4_0_UNRELEASED;
             case V_5_3_0_ID_UNRELEASED:
                 return V_5_3_0_UNRELEASED;
             case V_5_2_1_ID_UNRELEASED:

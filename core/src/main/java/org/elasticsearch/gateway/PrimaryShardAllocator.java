9154686623320179eec753ccbff39355346e45d4
Yannick Welsch <yannick@welsch.lu>
2017-02-08 10:00:55 +0100

    Remove legacy primary shard allocation mode based on versions (#23016)
    Elasticsearch v5.0.0 uses allocation IDs to safely allocate primary shards whereas prior versions of ES used a version-based mode instead. Elasticsearch v5 still has support for version-based primary shard allocation as it needs to be able to load 2.x shards. ES v6 can drop the legacy support.

--- core/src/main/java/org/elasticsearch/gateway/PrimaryShardAllocator.java
+++ core/src/main/java/org/elasticsearch/gateway/PrimaryShardAllocator.java
@@ -22,8 +22,6 @@ package org.elasticsearch.gateway;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.apache.logging.log4j.util.Supplier;
-import org.apache.lucene.util.CollectionUtil;
-import org.elasticsearch.Version;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.routing.RecoverySource;
@@ -37,8 +35,6 @@ import org.elasticsearch.cluster.routing.allocation.NodeAllocationResult.ShardSt
 import org.elasticsearch.cluster.routing.allocation.RoutingAllocation;
 import org.elasticsearch.cluster.routing.allocation.decider.Decision;
 import org.elasticsearch.cluster.routing.allocation.decider.Decision.Type;
-import org.elasticsearch.common.settings.Setting;
-import org.elasticsearch.common.settings.Setting.Property;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.ShardLockObtainFailedException;
 import org.elasticsearch.gateway.AsyncShardFetch.FetchResult;
@@ -52,7 +48,6 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -71,34 +66,8 @@ import java.util.stream.Stream;
  */
 public abstract class PrimaryShardAllocator extends BaseGatewayShardAllocator {
 
-    private static final Function<String, String> INITIAL_SHARDS_PARSER = (value) -> {
-        switch (value) {
-            case "quorum":
-            case "quorum-1":
-            case "half":
-            case "one":
-            case "full":
-            case "full-1":
-            case "all-1":
-            case "all":
-                return value;
-            default:
-                Integer.parseInt(value); // it can be parsed that's all we care here?
-                return value;
-        }
-    };
-
-    public static final Setting<String> NODE_INITIAL_SHARDS_SETTING =
-        new Setting<>("gateway.initial_shards", (settings) -> settings.get("gateway.local.initial_shards", "quorum"), INITIAL_SHARDS_PARSER,
-            Property.Dynamic, Property.NodeScope);
-    @Deprecated
-    public static final Setting<String> INDEX_RECOVERY_INITIAL_SHARDS_SETTING =
-        new Setting<>("index.recovery.initial_shards", (settings) -> NODE_INITIAL_SHARDS_SETTING.get(settings) , INITIAL_SHARDS_PARSER,
-            Property.Dynamic, Property.IndexScope);
-
     public PrimaryShardAllocator(Settings settings) {
         super(settings);
-        logger.debug("using initial_shards [{}]", NODE_INITIAL_SHARDS_SETTING.get(settings));
     }
 
     /**
@@ -139,34 +108,13 @@ public abstract class PrimaryShardAllocator extends BaseGatewayShardAllocator {
         final boolean snapshotRestore = unassignedShard.recoverySource().getType() == RecoverySource.Type.SNAPSHOT;
         final boolean recoverOnAnyNode = recoverOnAnyNode(indexMetaData);
 
-        final NodeShardsResult nodeShardsResult;
-        final boolean enoughAllocationsFound;
-
-        if (inSyncAllocationIds.isEmpty()) {
-            assert Version.indexCreated(indexMetaData.getSettings()).before(Version.V_5_0_0_alpha1) :
-                "trying to allocate a primary with an empty in sync allocation id set, but index is new. index: "
-                    + indexMetaData.getIndex();
-            // when we load an old index (after upgrading cluster) or restore a snapshot of an old index
-            // fall back to old version-based allocation mode
-            // Note that once the shard has been active, lastActiveAllocationIds will be non-empty
-            nodeShardsResult = buildVersionBasedNodeShardsResult(unassignedShard, snapshotRestore || recoverOnAnyNode,
-                                                                 allocation.getIgnoreNodes(unassignedShard.shardId()), shardState, logger);
-            if (snapshotRestore || recoverOnAnyNode) {
-                enoughAllocationsFound = nodeShardsResult.allocationsFound > 0;
-            } else {
-                enoughAllocationsFound = isEnoughVersionBasedAllocationsFound(indexMetaData, nodeShardsResult);
-            }
-            logger.debug("[{}][{}]: version-based allocation for pre-{} index found {} allocations of {}", unassignedShard.index(),
-                         unassignedShard.id(), Version.V_5_0_0_alpha1, nodeShardsResult.allocationsFound, unassignedShard);
-        } else {
-            assert inSyncAllocationIds.isEmpty() == false;
-            // use allocation ids to select nodes
-            nodeShardsResult = buildAllocationIdBasedNodeShardsResult(unassignedShard, snapshotRestore || recoverOnAnyNode,
-                allocation.getIgnoreNodes(unassignedShard.shardId()), inSyncAllocationIds, shardState, logger);
-            enoughAllocationsFound = nodeShardsResult.orderedAllocationCandidates.size() > 0;
-            logger.debug("[{}][{}]: found {} allocation candidates of {} based on allocation ids: [{}]", unassignedShard.index(),
-                         unassignedShard.id(), nodeShardsResult.orderedAllocationCandidates.size(), unassignedShard, inSyncAllocationIds);
-        }
+        assert inSyncAllocationIds.isEmpty() == false;
+        // use in-sync allocation ids to select nodes
+        final NodeShardsResult nodeShardsResult = buildNodeShardsResult(unassignedShard, snapshotRestore || recoverOnAnyNode,
+            allocation.getIgnoreNodes(unassignedShard.shardId()), inSyncAllocationIds, shardState, logger);
+        final boolean enoughAllocationsFound = nodeShardsResult.orderedAllocationCandidates.size() > 0;
+        logger.debug("[{}][{}]: found {} allocation candidates of {} based on allocation ids: [{}]", unassignedShard.index(),
+            unassignedShard.id(), nodeShardsResult.orderedAllocationCandidates.size(), unassignedShard, inSyncAllocationIds);
 
         if (enoughAllocationsFound == false) {
             if (snapshotRestore) {
@@ -293,10 +241,10 @@ public abstract class PrimaryShardAllocator extends BaseGatewayShardAllocator {
      * inSyncAllocationIds are added to the list. Otherwise, any node that has a shard is added to the list, but
      * entries with matching allocation id are always at the front of the list.
      */
-    protected static NodeShardsResult buildAllocationIdBasedNodeShardsResult(ShardRouting shard, boolean matchAnyShard,
-                                                                             Set<String> ignoreNodes, Set<String> inSyncAllocationIds,
-                                                                             FetchResult<NodeGatewayStartedShards> shardState,
-                                                                             Logger logger) {
+    protected static NodeShardsResult buildNodeShardsResult(ShardRouting shard, boolean matchAnyShard,
+                                                            Set<String> ignoreNodes, Set<String> inSyncAllocationIds,
+                                                            FetchResult<NodeGatewayStartedShards> shardState,
+                                                            Logger logger) {
         List<NodeGatewayStartedShards> nodeShardStates = new ArrayList<>();
         int numberOfAllocationsFound = 0;
         for (NodeGatewayStartedShards nodeShardState : shardState.getData().values()) {
@@ -308,13 +256,10 @@ public abstract class PrimaryShardAllocator extends BaseGatewayShardAllocator {
             }
 
             if (nodeShardState.storeException() == null) {
-                if (allocationId == null && nodeShardState.legacyVersion() == ShardStateMetaData.NO_VERSION) {
+                if (allocationId == null) {
                     logger.trace("[{}] on node [{}] has no shard state information", shard, nodeShardState.getNode());
-                } else if (allocationId != null) {
-                    assert nodeShardState.legacyVersion() == ShardStateMetaData.NO_VERSION : "Allocation id and legacy version cannot be both present";
-                    logger.trace("[{}] on node [{}] has allocation id [{}]", shard, nodeShardState.getNode(), allocationId);
                 } else {
-                    logger.trace("[{}] on node [{}] has no allocation id, out-dated shard (shard state version: [{}])", shard, nodeShardState.getNode(), nodeShardState.legacyVersion());
+                    logger.trace("[{}] on node [{}] has allocation id [{}]", shard, nodeShardState.getNode(), allocationId);
                 }
             } else {
                 final String finalAllocationId = allocationId;
@@ -356,37 +301,6 @@ public abstract class PrimaryShardAllocator extends BaseGatewayShardAllocator {
     }
 
     /**
-     * used by old version-based allocation
-     */
-    private boolean isEnoughVersionBasedAllocationsFound(IndexMetaData indexMetaData, NodeShardsResult nodeShardsResult) {
-        // check if the counts meets the minimum set
-        int requiredAllocation = 1;
-        // if we restore from a repository one copy is more then enough
-        String initialShards = INDEX_RECOVERY_INITIAL_SHARDS_SETTING.get(indexMetaData.getSettings(), settings);
-        if ("quorum".equals(initialShards)) {
-            if (indexMetaData.getNumberOfReplicas() > 1) {
-                requiredAllocation = ((1 + indexMetaData.getNumberOfReplicas()) / 2) + 1;
-            }
-        } else if ("quorum-1".equals(initialShards) || "half".equals(initialShards)) {
-            if (indexMetaData.getNumberOfReplicas() > 2) {
-                requiredAllocation = ((1 + indexMetaData.getNumberOfReplicas()) / 2);
-            }
-        } else if ("one".equals(initialShards)) {
-            requiredAllocation = 1;
-        } else if ("full".equals(initialShards) || "all".equals(initialShards)) {
-            requiredAllocation = indexMetaData.getNumberOfReplicas() + 1;
-        } else if ("full-1".equals(initialShards) || "all-1".equals(initialShards)) {
-            if (indexMetaData.getNumberOfReplicas() > 1) {
-                requiredAllocation = indexMetaData.getNumberOfReplicas();
-            }
-        } else {
-            requiredAllocation = Integer.parseInt(initialShards);
-        }
-
-        return nodeShardsResult.allocationsFound >= requiredAllocation;
-    }
-
-    /**
      * Split the list of node shard states into groups yes/no/throttle based on allocation deciders
      */
     private NodesToAllocate buildNodesToAllocate(RoutingAllocation allocation,
@@ -415,85 +329,6 @@ public abstract class PrimaryShardAllocator extends BaseGatewayShardAllocator {
         }
         return new NodesToAllocate(Collections.unmodifiableList(yesNodeShards), Collections.unmodifiableList(throttledNodeShards),
                                       Collections.unmodifiableList(noNodeShards));
-    }
-
-    /**
-     * Builds a list of previously started shards. If matchAnyShard is set to false, only shards with the highest shard version are added to
-     * the list. Otherwise, any existing shard is added to the list, but entries with highest version are always at the front of the list.
-     */
-    static NodeShardsResult buildVersionBasedNodeShardsResult(ShardRouting shard, boolean matchAnyShard, Set<String> ignoreNodes,
-                                                              FetchResult<NodeGatewayStartedShards> shardState, Logger logger) {
-        final List<NodeGatewayStartedShards> allocationCandidates = new ArrayList<>();
-        int numberOfAllocationsFound = 0;
-        long highestVersion = ShardStateMetaData.NO_VERSION;
-        for (NodeGatewayStartedShards nodeShardState : shardState.getData().values()) {
-            long version = nodeShardState.legacyVersion();
-            DiscoveryNode node = nodeShardState.getNode();
-
-            if (ignoreNodes.contains(node.getId())) {
-                continue;
-            }
-
-            if (nodeShardState.storeException() == null) {
-                if (version == ShardStateMetaData.NO_VERSION && nodeShardState.allocationId() == null) {
-                    logger.trace("[{}] on node [{}] has no shard state information", shard, nodeShardState.getNode());
-                } else if (version != ShardStateMetaData.NO_VERSION) {
-                    assert nodeShardState.allocationId() == null : "Allocation id and legacy version cannot be both present";
-                    logger.trace("[{}] on node [{}] has version [{}] of shard", shard, nodeShardState.getNode(), version);
-                } else {
-                    // shard was already selected in a 5.x cluster as primary for recovery, was initialized (and wrote a new state file) but
-                    // did not make it to STARTED state before the cluster crashed (otherwise list of active allocation ids would be
-                    // non-empty and allocation id - based allocation mode would be chosen).
-                    // Prefer this shard copy again.
-                    version = Long.MAX_VALUE;
-                    logger.trace("[{}] on node [{}] has allocation id [{}]", shard, nodeShardState.getNode(), nodeShardState.allocationId());
-                }
-            } else {
-                final long finalVersion = version;
-                if (nodeShardState.storeException() instanceof ShardLockObtainFailedException) {
-                    logger.trace((Supplier<?>) () -> new ParameterizedMessage("[{}] on node [{}] has version [{}] but the store can not be opened as it's locked, treating as valid shard", shard, nodeShardState.getNode(), finalVersion), nodeShardState.storeException());
-                    if (nodeShardState.allocationId() != null) {
-                        version = Long.MAX_VALUE; // shard was already selected in a 5.x cluster as primary, prefer this shard copy again.
-                    } else {
-                        version = 0L; // treat as lowest version so that this shard is the least likely to be selected as primary
-                    }
-                } else {
-                    // disregard the reported version and assign it as no version (same as shard does not exist)
-                    logger.trace((Supplier<?>) () -> new ParameterizedMessage("[{}] on node [{}] has version [{}] but the store can not be opened, treating no version", shard, nodeShardState.getNode(), finalVersion), nodeShardState.storeException());
-                    version = ShardStateMetaData.NO_VERSION;
-                }
-            }
-
-            if (version != ShardStateMetaData.NO_VERSION) {
-                numberOfAllocationsFound++;
-                // If we've found a new "best" candidate, clear the
-                // current candidates and add it
-                if (version > highestVersion) {
-                    highestVersion = version;
-                    if (matchAnyShard == false) {
-                        allocationCandidates.clear();
-                    }
-                    allocationCandidates.add(nodeShardState);
-                } else if (version == highestVersion) {
-                    // If the candidate is the same, add it to the
-                    // list, but keep the current candidate
-                    allocationCandidates.add(nodeShardState);
-                }
-            }
-        }
-        // sort array so the node with the highest version is at the beginning
-        CollectionUtil.timSort(allocationCandidates, Comparator.comparing(NodeGatewayStartedShards::legacyVersion).reversed());
-
-        if (logger.isTraceEnabled()) {
-            StringBuilder sb = new StringBuilder("[");
-            for (NodeGatewayStartedShards n : allocationCandidates) {
-                sb.append("[").append(n.getNode().getName()).append("]").append(" -> ").append(n.legacyVersion()).append(", ");
-            }
-            sb.append("]");
-            logger.trace("{} candidates for allocation: {}", shard, sb.toString());
-        }
-
-        return new NodeShardsResult(Collections.unmodifiableList(allocationCandidates), numberOfAllocationsFound);
     }
 
     /**
--- core/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
+++ core/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
@@ -158,20 +158,19 @@ public class IndexShardTests extends IndexShardTestCase {
     public void testWriteShardState() throws Exception {
         try (NodeEnvironment env = newNodeEnvironment()) {
             ShardId id = new ShardId("foo", "fooUUID", 1);
-            long version = between(1, Integer.MAX_VALUE / 2);
             boolean primary = randomBoolean();
             AllocationId allocationId = randomBoolean() ? null : randomAllocationId();
-            ShardStateMetaData state1 = new ShardStateMetaData(version, primary, "fooUUID", allocationId);
+            ShardStateMetaData state1 = new ShardStateMetaData(primary, "fooUUID", allocationId);
             write(state1, env.availableShardPaths(id));
             ShardStateMetaData shardStateMetaData = load(logger, env.availableShardPaths(id));
             assertEquals(shardStateMetaData, state1);
 
-            ShardStateMetaData state2 = new ShardStateMetaData(version, primary, "fooUUID", allocationId);
+            ShardStateMetaData state2 = new ShardStateMetaData(primary, "fooUUID", allocationId);
             write(state2, env.availableShardPaths(id));
             shardStateMetaData = load(logger, env.availableShardPaths(id));
             assertEquals(shardStateMetaData, state1);
 
-            ShardStateMetaData state3 = new ShardStateMetaData(version + 1, primary, "fooUUID", allocationId);
+            ShardStateMetaData state3 = new ShardStateMetaData(primary, "fooUUID", allocationId);
             write(state3, env.availableShardPaths(id));
             shardStateMetaData = load(logger, env.availableShardPaths(id));
             assertEquals(shardStateMetaData, state3);
@@ -236,21 +235,20 @@ public class IndexShardTests extends IndexShardTestCase {
 
     public void testShardStateMetaHashCodeEquals() {
         AllocationId allocationId = randomBoolean() ? null : randomAllocationId();
-        ShardStateMetaData meta = new ShardStateMetaData(randomLong(), randomBoolean(),
+        ShardStateMetaData meta = new ShardStateMetaData(randomBoolean(),
             randomRealisticUnicodeOfCodepointLengthBetween(1, 10), allocationId);
 
-        assertEquals(meta, new ShardStateMetaData(meta.legacyVersion, meta.primary, meta.indexUUID, meta.allocationId));
+        assertEquals(meta, new ShardStateMetaData(meta.primary, meta.indexUUID, meta.allocationId));
         assertEquals(meta.hashCode(),
-            new ShardStateMetaData(meta.legacyVersion, meta.primary, meta.indexUUID, meta.allocationId).hashCode());
+            new ShardStateMetaData(meta.primary, meta.indexUUID, meta.allocationId).hashCode());
 
-        assertFalse(meta.equals(new ShardStateMetaData(meta.legacyVersion, !meta.primary, meta.indexUUID, meta.allocationId)));
-        assertFalse(meta.equals(new ShardStateMetaData(meta.legacyVersion + 1, meta.primary, meta.indexUUID, meta.allocationId)));
-        assertFalse(meta.equals(new ShardStateMetaData(meta.legacyVersion, !meta.primary, meta.indexUUID + "foo", meta.allocationId)));
-        assertFalse(meta.equals(new ShardStateMetaData(meta.legacyVersion, !meta.primary, meta.indexUUID + "foo", randomAllocationId())));
+        assertFalse(meta.equals(new ShardStateMetaData(!meta.primary, meta.indexUUID, meta.allocationId)));
+        assertFalse(meta.equals(new ShardStateMetaData(!meta.primary, meta.indexUUID + "foo", meta.allocationId)));
+        assertFalse(meta.equals(new ShardStateMetaData(!meta.primary, meta.indexUUID + "foo", randomAllocationId())));
         Set<Integer> hashCodes = new HashSet<>();
         for (int i = 0; i < 30; i++) { // just a sanity check that we impl hashcode
             allocationId = randomBoolean() ? null : randomAllocationId();
-            meta = new ShardStateMetaData(randomLong(), randomBoolean(),
+            meta = new ShardStateMetaData(randomBoolean(),
                 randomRealisticUnicodeOfCodepointLengthBetween(1, 10), allocationId);
             hashCodes.add(meta.hashCode());
         }

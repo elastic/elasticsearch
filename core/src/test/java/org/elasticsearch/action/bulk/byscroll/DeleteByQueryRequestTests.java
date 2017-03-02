69171dfdbae92515675d1e41c2cd9f8aa7647e7e
kunal642 <kunal.kapoor@knoldus.in>
2017-03-02 16:40:55 +0530

    added unit test for DeleteByQueryRequestTests

--- core/src/test/java/org/elasticsearch/action/bulk/byscroll/DeleteByQueryRequestTests.java
+++ core/src/test/java/org/elasticsearch/action/bulk/byscroll/DeleteByQueryRequestTests.java
@@ -73,7 +73,7 @@ public class DeleteByQueryRequestTests extends AbstractBulkByScrollRequestTestCa
         // No extra assertions needed
     }
 
-    public void testTypesSetter() {
+    public void testTypesGetter() {
         int numTypes = between(1, 50);
         String[] types = new String[numTypes];
         for (int i = 0; i < numTypes; i++) {
@@ -82,6 +82,18 @@ public class DeleteByQueryRequestTests extends AbstractBulkByScrollRequestTestCa
         SearchRequest searchRequest = new SearchRequest();
         searchRequest.types(types);
         DeleteByQueryRequest request = new DeleteByQueryRequest(searchRequest);
+        assertArrayEquals(request.types(), types);
+    }
+
+    public void testTypesSetter() {
+        int numTypes = between(1, 50);
+        String[] types = new String[numTypes];
+        for (int i = 0; i < numTypes; i++) {
+            types[i] = randomSimpleString(random(), 1, 30);
+        }
+        SearchRequest searchRequest = new SearchRequest();
+        DeleteByQueryRequest request = new DeleteByQueryRequest(searchRequest);
+        request.types(types);
         assertArrayEquals(request.types(), types);
     }
 }

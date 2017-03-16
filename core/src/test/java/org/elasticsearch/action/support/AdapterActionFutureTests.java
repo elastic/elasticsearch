2e89ed79188ec963a8a4d0a9e814eae7cb8d93bd
Jason Tedor <jason@tedor.me>
2017-03-16 16:03:46 -0400

    Remove extra newline

--- core/src/test/java/org/elasticsearch/action/support/AdapterActionFutureTests.java
+++ core/src/test/java/org/elasticsearch/action/support/AdapterActionFutureTests.java
@@ -81,7 +81,6 @@ public class AdapterActionFutureTests extends ESTestCase {
         } catch (final IllegalStateException e) {
             assertTrue(Thread.currentThread().isInterrupted());
         }
-
     }
 
 }

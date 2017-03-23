041eecca8e21d1d32025cfb8b24ee0a1f9ea11d5
Shepard1212 <yingzeyu0310@foxmail.com>
2017-03-23 13:21:03 +1300

    Fix #4704
    Since the StandardHtmlAnalyzer's functionality is easy to replicate using the HTMLStripCharFilter, so I remove this analyzer.

--- core/src/main/java/org/elasticsearch/index/analysis/StandardHtmlStripAnalyzer.java
+++ core/src/main/java/org/elasticsearch/index/analysis/StandardHtmlStripAnalyzer.java
@@ -31,18 +31,6 @@ import org.apache.lucene.analysis.standard.StandardTokenizer;
 
 public class StandardHtmlStripAnalyzer extends StopwordAnalyzerBase {
 
-    /**
-     * @deprecated use {@link StandardHtmlStripAnalyzer#StandardHtmlStripAnalyzer(CharArraySet)} instead
-     */
-    @Deprecated
-    public StandardHtmlStripAnalyzer() {
-        super(StopAnalyzer.ENGLISH_STOP_WORDS_SET);
-    }
-
-    public StandardHtmlStripAnalyzer(CharArraySet stopwords) {
-        super(stopwords);
-    }
-
     @Override
     protected TokenStreamComponents createComponents(final String fieldName) {
         final Tokenizer src = new StandardTokenizer();

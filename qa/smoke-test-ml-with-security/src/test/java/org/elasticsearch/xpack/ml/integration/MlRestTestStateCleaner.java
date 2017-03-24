c0445fac4d6c818b560b71cd3ca770f859cac80c
David Roberts <dave.roberts@elastic.co>
2017-03-24 17:58:30 +0000

    [TEST] Silence failing ML security tests
    Original commit: elastic/x-pack-elasticsearch@af4ed2019d5f4c904215b6b502fa3f301ee7f342

--- qa/smoke-test-ml-with-security/src/test/java/org/elasticsearch/xpack/ml/integration/MlRestTestStateCleaner.java
+++ qa/smoke-test-ml-with-security/src/test/java/org/elasticsearch/xpack/ml/integration/MlRestTestStateCleaner.java
@@ -84,11 +84,18 @@ public class MlRestTestStateCleaner {
                     logger.error("Got status code " + statusCode + " when closing job " + jobId);
                 }
             } catch (Exception e) {
-                if (e.getMessage().contains("cannot close job, expected job state [opened], but got [closed]")
-                        || e.getMessage().contains("cannot close job, expected job state [opened], but got [closing]")) {
+                if (e.getMessage().contains("expected job state [opened], but got [closed]")
+                        || e.getMessage().contains("expected job state [opened], but got [closing]")) {
                     logger.debug("job [" + jobId + "] has already been closed", e);
                 } else {
-                    logger.warn("failed to close job [" + jobId + "]", e);
+                    logger.warn("failed to close job [" + jobId + "]. Forcing closed.", e);
+
+                    try {
+                        adminClient.performRequest("POST", "/_xpack/ml/anomaly_detectors/" + jobId + "/_close?force=true");
+                        throw new RuntimeException("Had to resort to force-closing job, something went wrong?");
+                    } catch (Exception e2) {
+                        throw new RuntimeException("Force-closing job [" + jobId + "] failed.", e2);
+                    }
                 }
             }
             int statusCode = adminClient.performRequest("DELETE", "/_xpack/ml/anomaly_detectors/" + jobId).getStatusLine().getStatusCode();

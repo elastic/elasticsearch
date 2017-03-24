207e41752b8655062c859c2a14714ac6371e63b6
Nik Everett <nik9000@gmail.com>
2017-03-24 17:50:32 -0400

    Remove leftover

--- modules/lang-mustache/src/main/java/org/elasticsearch/script/mustache/MustacheScriptEngineService.java
+++ modules/lang-mustache/src/main/java/org/elasticsearch/script/mustache/MustacheScriptEngineService.java
@@ -107,7 +107,6 @@ public final class MustacheScriptEngineService implements TemplateService.Backen
     @Override
     public ExecutableScript executable(CompiledScript compiledScript,
             @Nullable Map<String, Object> vars) {
-        ESLoggerFactory.getLogger(TemplateService.class).warn("ASDFASDF toexec [{}] [{}]", compiledScript, vars);
         return new MustacheExecutableScript(compiledScript, vars);
     }
 

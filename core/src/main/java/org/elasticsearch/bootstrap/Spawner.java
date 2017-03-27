defd0452e729617db5678fb4351b32fc403ca784
Jason Tedor <jason@tedor.me>
2017-03-27 15:52:45 -0400

    Modify permissions dialog for plugins
    This commit modifies the handling of plugins that require special
    permissions to cover a case that was not previously covered.
    
    Relates #23742

--- core/src/main/java/org/elasticsearch/bootstrap/Spawner.java
+++ core/src/main/java/org/elasticsearch/bootstrap/Spawner.java
@@ -19,9 +19,10 @@
 
 package org.elasticsearch.bootstrap;
 
-import org.apache.lucene.util.Constants;
 import org.apache.lucene.util.IOUtils;
 import org.elasticsearch.env.Environment;
+import org.elasticsearch.plugins.PluginInfo;
+import org.elasticsearch.plugins.Platforms;
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -32,97 +33,89 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * Spawns native plugin controller processes if present.  Will only work prior to a system call filter being installed.
+ * Spawns native plugin controller processes if present. Will only work prior to a system call
+ * filter being installed.
  */
 final class Spawner implements Closeable {
 
-    private static final String PROGRAM_NAME = Constants.WINDOWS ? "controller.exe" : "controller";
-    private static final String PLATFORM_NAME = makePlatformName(Constants.OS_NAME, Constants.OS_ARCH);
-    private static final String TMP_ENVVAR = "TMPDIR";
-
-    /**
+    /*
      * References to the processes that have been spawned, so that we can destroy them.
      */
     private final List<Process> processes = new ArrayList<>();
+    private AtomicBoolean spawned = new AtomicBoolean();
 
     @Override
     public void close() throws IOException {
-        try {
-            IOUtils.close(() -> processes.stream().map(s -> (Closeable)s::destroy).iterator());
-        } finally {
-            processes.clear();
-        }
+        IOUtils.close(() -> processes.stream().map(s -> (Closeable) s::destroy).iterator());
     }
 
     /**
-     * For each plugin, attempt to spawn the controller daemon.  Silently ignore any plugins
-     * that don't include a controller for the correct platform.
+     * Spawns the native controllers for each plugin
+     *
+     * @param environment the node environment
+     * @throws IOException if an I/O error occurs reading the plugins or spawning a native process
      */
-    void spawnNativePluginControllers(Environment environment) throws IOException {
-        if (Files.exists(environment.pluginsFile())) {
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(environment.pluginsFile())) {
-                for (Path plugin : stream) {
-                    Path spawnPath = makeSpawnPath(plugin);
-                    if (Files.isRegularFile(spawnPath)) {
-                        spawnNativePluginController(spawnPath, environment.tmpFile());
-                    }
+    void spawnNativePluginControllers(final Environment environment) throws IOException {
+        if (!spawned.compareAndSet(false, true)) {
+            throw new IllegalStateException("native controllers already spawned");
+        }
+        final Path pluginsFile = environment.pluginsFile();
+        if (!Files.exists(pluginsFile)) {
+            throw new IllegalStateException("plugins directory [" + pluginsFile + "] not found");
+        }
+        /*
+         * For each plugin, attempt to spawn the controller daemon. Silently ignore any plugin that
+         * don't include a controller for the correct platform.
+         */
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(pluginsFile)) {
+            for (final Path plugin : stream) {
+                final PluginInfo info = PluginInfo.readFromProperties(plugin);
+                final Path spawnPath = Platforms.nativeControllerPath(plugin);
+                if (!Files.isRegularFile(spawnPath)) {
+                    continue;
                 }
+                if (!info.hasNativeController()) {
+                    final String message = String.format(
+                            Locale.ROOT,
+                            "plugin [%s] does not have permission to fork native controller",
+                            plugin.getFileName());
+                    throw new IllegalArgumentException(message);
+                }
+                final Process process =
+                        spawnNativePluginController(spawnPath, environment.tmpFile());
+                processes.add(process);
             }
         }
     }
 
     /**
-     * Attempt to spawn the controller daemon for a given plugin.  The spawned process
-     * will remain connected to this JVM via its stdin, stdout and stderr, but the
-     * references to these streams are not available to code outside this package.
+     * Attempt to spawn the controller daemon for a given plugin. The spawned process will remain
+     * connected to this JVM via its stdin, stdout, and stderr streams, but the references to these
+     * streams are not available to code outside this package.
      */
-    private void spawnNativePluginController(Path spawnPath, Path tmpPath) throws IOException {
-        ProcessBuilder pb = new ProcessBuilder(spawnPath.toString());
+    private Process spawnNativePluginController(
+            final Path spawnPath,
+            final Path tmpPath) throws IOException {
+        final ProcessBuilder pb = new ProcessBuilder(spawnPath.toString());
 
-        // The only environment variable passes on the path to the temporary directory
+        // the only environment variable passes on the path to the temporary directory
         pb.environment().clear();
-        pb.environment().put(TMP_ENVVAR, tmpPath.toString());
+        pb.environment().put("TMPDIR", tmpPath.toString());
 
-        // The output stream of the Process object corresponds to the daemon's stdin
-        processes.add(pb.start());
+        // the output stream of the process object corresponds to the daemon's stdin
+        return pb.start();
     }
 
+    /**
+     * The collection of processes representing spawned native controllers.
+     *
+     * @return the processes
+     */
     List<Process> getProcesses() {
         return Collections.unmodifiableList(processes);
     }
 
-    /**
-     * Make the full path to the program to be spawned.
-     */
-    static Path makeSpawnPath(Path plugin) {
-        return plugin.resolve("platform").resolve(PLATFORM_NAME).resolve("bin").resolve(PROGRAM_NAME);
-    }
-
-    /**
-     * Make the platform name in the format used in Kibana downloads, for example:
-     * - darwin-x86_64
-     * - linux-x86-64
-     * - windows-x86_64
-     * For *nix platforms this is more-or-less `uname -s`-`uname -m` converted to lower case.
-     * However, for consistency between different operating systems on the same architecture
-     * "amd64" is replaced with "x86_64" and "i386" with "x86".
-     * For Windows it's "windows-" followed by either "x86" or "x86_64".
-     */
-    static String makePlatformName(String osName, String osArch) {
-        String os = osName.toLowerCase(Locale.ROOT);
-        if (os.startsWith("windows")) {
-            os = "windows";
-        } else if (os.equals("mac os x")) {
-            os = "darwin";
-        }
-        String cpu = osArch.toLowerCase(Locale.ROOT);
-        if (cpu.equals("amd64")) {
-            cpu = "x86_64";
-        } else if (cpu.equals("i386")) {
-            cpu = "x86";
-        }
-        return os + "-" + cpu;
-    }
 }

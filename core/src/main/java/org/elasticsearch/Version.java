61f3f5d7da4ef1051c2c1d737eb6d4730f16e150
Ali Beyad <ali@elastic.co>
2017-03-28 13:05:52 -0400

    Adds v5.3.1 to version constants

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -120,6 +120,8 @@ public class Version {
     public static final Version V_5_2_3_UNRELEASED = new Version(V_5_2_3_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_4_1);
     public static final int V_5_3_0_ID_UNRELEASED = 5030099;
     public static final Version V_5_3_0_UNRELEASED = new Version(V_5_3_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_4_1);
+    public static final int V_5_3_1_ID_UNRELEASED = 5030199;
+    public static final Version V_5_3_1_UNRELEASED = new Version(V_5_3_1_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_4_1);
     public static final int V_5_4_0_ID_UNRELEASED = 5040099;
     public static final Version V_5_4_0_UNRELEASED = new Version(V_5_4_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_5_0);
     public static final Version CURRENT = V_5_4_0_UNRELEASED;
@@ -139,6 +141,8 @@ public class Version {
         switch (id) {
             case V_5_4_0_ID_UNRELEASED:
                 return V_5_4_0_UNRELEASED;
+            case V_5_3_1_ID_UNRELEASED:
+                return V_5_3_1_UNRELEASED;
             case V_5_3_0_ID_UNRELEASED:
                 return V_5_3_0_UNRELEASED;
             case V_5_2_3_ID_UNRELEASED:

56716629700459a3ecdd79edd6ef1d6fffcb525e
Ali Beyad <ali@elastic.co>
2017-03-28 13:09:36 -0400

    Bumps version number to v5.3.1

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -120,7 +120,9 @@ public class Version {
     public static final Version V_5_2_3_UNRELEASED = new Version(V_5_2_3_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_4_1);
     public static final int V_5_3_0_ID_UNRELEASED = 5030099;
     public static final Version V_5_3_0_UNRELEASED = new Version(V_5_3_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_4_1);
-    public static final Version CURRENT = V_5_3_0_UNRELEASED;
+    public static final int V_5_3_1_ID_UNRELEASED = 5030199;
+    public static final Version V_5_3_1_UNRELEASED = new Version(V_5_3_1_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_4_1);
+    public static final Version CURRENT = V_5_3_1_UNRELEASED;
 
     // unreleased versions must be added to the above list with the suffix _UNRELEASED (with the exception of CURRENT)
 
@@ -135,6 +137,8 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_5_3_1_ID_UNRELEASED:
+                return V_5_3_1_UNRELEASED;
             case V_5_3_0_ID_UNRELEASED:
                 return V_5_3_0_UNRELEASED;
             case V_5_2_3_ID_UNRELEASED:

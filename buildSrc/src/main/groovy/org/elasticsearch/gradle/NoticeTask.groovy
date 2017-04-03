db9207ae2113760bbf68f952565bf062cde25f3e
David Roberts <dave.roberts@elastic.co>
2017-04-03 17:19:27 +0100

    Add the ability to include extra notices in a plugin's NOTICES file

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/NoticeTask.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/NoticeTask.groovy
@@ -22,12 +22,14 @@ package org.elasticsearch.gradle
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
+import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 /**
- * A task to create a notice file which includes dependencies' notices.
+ * A task to create a notice file which includes dependencies' notices
+ * and, optionally, notices from a list of extra directories.
  */
 public class NoticeTask extends DefaultTask {
 
@@ -40,6 +42,10 @@ public class NoticeTask extends DefaultTask {
     /** Configurations to inspect dependencies*/
     private List<Project> dependencies = new ArrayList<>()
 
+    /** Extra directories to include notices from */
+    @InputDirectory
+    private List<File> extraLicensesDirs = new ArrayList<>()
+
     public NoticeTask() {
         description = 'Create a notice file from dependencies'
     }
@@ -49,23 +55,36 @@ public class NoticeTask extends DefaultTask {
         dependencies.add(project)
     }
 
+    /** Add notices from an extra directory. */
+    public void extraLicensesDir(File extraLicensesDir) {
+        extraLicensesDirs.add(extraLicensesDir)
+    }
+
     @TaskAction
     public void generateNotice() {
         StringBuilder output = new StringBuilder()
         output.append(inputFile.getText('UTF-8'))
         output.append('\n\n')
-        Set<String> seen = new HashSet<>()
+        Map<String, File> seen = new TreeMap<>()
         for (Project dep : dependencies) {
-            File licensesDir = new File(dep.projectDir, 'licenses')
-            if (licensesDir.exists() == false) continue
-            licensesDir.eachFileMatch({ it ==~ /.*-NOTICE\.txt/ && seen.contains(it) == false}) { File file ->
-                String name = file.name.substring(0, file.name.length() - '-NOTICE.txt'.length())
-                appendFile(file, name, 'NOTICE', output)
-                appendFile(new File(file.parentFile, "${name}-LICENSE.txt"), name, 'LICENSE', output)
-                seen.add(file.name)
-            }
+            iterateLicensesDir(new File(dep.projectDir, 'licenses'), seen, true)
+        }
+        for (File extraLicensesDir : extraLicensesDirs) {
+            iterateLicensesDir(extraLicensesDir, seen, false)
+        }
+        for (File file : seen.values()) {
+            String name = file.name.substring(0, file.name.length() - '-NOTICE.txt'.length())
+            appendFile(file, name, 'NOTICE', output)
+            appendFile(new File(file.parentFile, "${name}-LICENSE.txt"), name, 'LICENSE', output)
         }
         outputFile.setText(output.toString(), 'UTF-8')
+    }
+
+    static void iterateLicensesDir(File licensesDir, Map<String, File> seen, boolean isOptional) {
+        if (isOptional && licensesDir.exists() == false) return
+        licensesDir.eachFileMatch({ it ==~ /.*-NOTICE\.txt/ }) { File file ->
+            seen.put(file.name, file)
+        }
     }
 
     static void appendFile(File file, String name, String type, StringBuilder output) {

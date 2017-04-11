a5f0a74b6704f5ac2eca0a6a474a9882c9a5a568
Dimitrios Athanasiou <dimitris@elastic.co>
2017-04-11 16:26:13 +0100

    [ML] Remove unused imports in GetDatafeedsStatsAction
    Original commit: elastic/x-pack-elasticsearch@83abaae58e7a0fa04401eb0375e439092d9b4862

--- plugin/src/main/java/org/elasticsearch/xpack/ml/action/GetDatafeedsStatsAction.java
+++ plugin/src/main/java/org/elasticsearch/xpack/ml/action/GetDatafeedsStatsAction.java
@@ -42,9 +42,7 @@ import org.elasticsearch.xpack.persistent.PersistentTasksCustomMetaData;
 import org.elasticsearch.xpack.persistent.PersistentTasksCustomMetaData.PersistentTask;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;

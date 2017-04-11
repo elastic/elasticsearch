0f6eeae66bdc9c0c688cc1b8f96f9e5bf016385e
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2017-04-11 10:42:54 +0100

    Fixes another checkstyle error

--- core/src/main/java/org/elasticsearch/index/fielddata/FieldData.java
+++ core/src/main/java/org/elasticsearch/index/fielddata/FieldData.java
@@ -500,7 +500,7 @@ public enum FieldData {
          * Advance this instance to the given document id
          * @return true if there is a value for this document
          */
-        abstract boolean advanceExact(int doc) throws IOException;
+        boolean advanceExact(int doc) throws IOException;
 
         /** Fill the list of charsquences with the list of values for the current document. */
         void get(List<CharSequence> values) throws IOException;

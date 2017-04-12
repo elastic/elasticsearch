3bc395640a9493fea450ae357a5bbcfc10ce1f4f
Adrien Grand <jpountz@gmail.com>
2017-04-12 18:28:12 +0200

    Bump version to 5.5.

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -124,7 +124,9 @@ public class Version {
     public static final Version V_5_3_1_UNRELEASED = new Version(V_5_3_1_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_4_2);
     public static final int V_5_4_0_ID_UNRELEASED = 5040099;
     public static final Version V_5_4_0_UNRELEASED = new Version(V_5_4_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_5_0);
-    public static final Version CURRENT = V_5_4_0_UNRELEASED;
+    public static final int V_5_5_0_ID_UNRELEASED = 5050099;
+    public static final Version V_5_5_0_UNRELEASED = new Version(V_5_5_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_5_0);
+    public static final Version CURRENT = V_5_5_0_UNRELEASED;
 
     // unreleased versions must be added to the above list with the suffix _UNRELEASED (with the exception of CURRENT)
 
@@ -139,6 +141,8 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_5_5_0_ID_UNRELEASED:
+                return V_5_5_0_UNRELEASED;
             case V_5_4_0_ID_UNRELEASED:
                 return V_5_4_0_UNRELEASED;
             case V_5_3_1_ID_UNRELEASED:

4245672acdc3e0906e606b712e0bceac8ba12453
Dimitrios Athanasiou <dimitris@elastic.co>
2017-04-13 18:01:51 +0100

    [TEST] Unmute tests as the fix is now picked by CI
    relates elastic/x-pack-elasticsearch#1079
    
    Original commit: elastic/x-pack-elasticsearch@15d801fed2d84d843368efe42df4312a85306efd

--- plugin/src/test/java/org/elasticsearch/xpack/ml/integration/MlJobIT.java
+++ plugin/src/test/java/org/elasticsearch/xpack/ml/integration/MlJobIT.java
@@ -86,7 +86,6 @@ public class MlJobIT extends ESRestTestCase {
         assertThat(responseAsString, containsString("\"job_id\":\"farequote\""));
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/elastic/x-pack-elasticsearch/issues/1079")
     public void testGetJobs_GivenSingleJob() throws Exception {
         createFarequoteJob();
 
@@ -107,7 +106,6 @@ public class MlJobIT extends ESRestTestCase {
         assertThat(responseAsString, containsString("\"job_id\":\"farequote\""));
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/elastic/x-pack-elasticsearch/issues/1079")
     public void testGetJobs_GivenMultipleJobs() throws Exception {
         createFarequoteJob("farequote_1");
         createFarequoteJob("farequote_2");

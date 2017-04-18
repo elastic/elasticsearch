a11f77aea0a3368cd3db8ad5e954314a1f943ef1
Chris Earle <pickypg@users.noreply.github.com>
2017-04-18 12:24:15 -0400

    [Monitoring] Convert "" _id to null (elastic/x-pack-elasticsearch#1098)
    When Logstash 5.2 - 5.3 submit documents via the `_xpack/monitoring/_bulk`
    endpoint, it sends its time-based documents with an explicit `_id` of
    `""`.
    
    This used to be automatically ignored by Monitoring, but we now accept the
    _id that we are given (including `null`). ES, prior to 5.3.1, accepted
    `""` as a valid `_id` through the `_bulk` endpoint, which means that it
    blindly accepted and overwrote documents given that ID, meaning that all
    Logstash instances "shared" the exact same document and therefore the UI
    becomes useless.
    
    This change allows `""` to be used and it simply replaces that value, and
    only that value, with `null`. This enables backwards compatibility with LS
    5.2 - 5.3.0.
    
    Original commit: elastic/x-pack-elasticsearch@889578e61eb95ab94664fd1a11fbc93394fd2072

--- plugin/src/main/java/org/elasticsearch/xpack/monitoring/exporter/MonitoringDoc.java
+++ plugin/src/main/java/org/elasticsearch/xpack/monitoring/exporter/MonitoringDoc.java
@@ -6,6 +6,7 @@
 package org.elasticsearch.xpack.monitoring.exporter;
 
 import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.inject.internal.Nullable;
 import org.elasticsearch.common.io.stream.StreamInput;
@@ -41,13 +42,15 @@ public class MonitoringDoc {
     private final BytesReference source;
     private final XContentType sourceXContentType;
 
-    protected MonitoringDoc(String monitoringId, String monitoringVersion, String type, String id,
+    protected MonitoringDoc(String monitoringId, String monitoringVersion,
+                            String type, @Nullable String id,
                             String clusterUUID, long timestamp, Node node,
                             BytesReference source, XContentType sourceXContentType) {
         this.monitoringId = monitoringId;
         this.monitoringVersion = monitoringVersion;
         this.type = type;
-        this.id = id;
+        // We allow strings to be "" because Logstash 5.2 - 5.3 would submit empty _id values for time-based documents
+        this.id = Strings.isNullOrEmpty(id) ? null : id;
         this.clusterUUID = clusterUUID;
         this.timestamp = timestamp;
         this.sourceNode = node;
@@ -62,8 +65,7 @@ public class MonitoringDoc {
 
     public MonitoringDoc(String monitoringId, String monitoringVersion, String type, String id,
                          String clusterUUID, long timestamp, DiscoveryNode discoveryNode) {
-        this(monitoringId, monitoringVersion, type, id, clusterUUID, timestamp,
-                fromDiscoveryNode(discoveryNode));
+        this(monitoringId, monitoringVersion, type, id, clusterUUID, timestamp, fromDiscoveryNode(discoveryNode));
     }
 
     public String getType() {
--- plugin/src/test/java/org/elasticsearch/xpack/monitoring/exporter/MonitoringDocTests.java
+++ plugin/src/test/java/org/elasticsearch/xpack/monitoring/exporter/MonitoringDocTests.java
@@ -7,6 +7,7 @@ package org.elasticsearch.xpack.monitoring.exporter;
 
 import org.elasticsearch.Version;
 import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.test.ESTestCase;
 
@@ -19,6 +20,20 @@ import java.util.Set;
 import static org.elasticsearch.xpack.monitoring.exporter.MonitoringDoc.Node.fromDiscoveryNode;
 
 public class MonitoringDocTests extends ESTestCase {
+
+    public void testEmptyIdBecomesNull() {
+        final String id = randomFrom("", null, randomAlphaOfLength(5));
+        final MonitoringDoc doc =
+                new MonitoringDoc("monitoringId", "monitoringVersion",
+                                  "type", id, "clusterUUID",
+                                  0, (DiscoveryNode)null);
+
+        if (Strings.isNullOrEmpty(id)) {
+            assertNull(doc.getId());
+        } else {
+            assertSame(id, doc.getId());
+        }
+    }
 
     public void testFromDiscoveryNode() {
         assertEquals(null, fromDiscoveryNode(null));
@@ -37,7 +52,7 @@ public class MonitoringDocTests extends ESTestCase {
         }
         Set<DiscoveryNode.Role> roles = new HashSet<>();
         if (randomBoolean()) {
-            randomSubsetOf(Arrays.asList(DiscoveryNode.Role.values())).forEach(roles::add);
+            roles.addAll(randomSubsetOf(Arrays.asList(DiscoveryNode.Role.values())));
         }
         final MonitoringDoc.Node expectedNode = new MonitoringDoc.Node(nodeId,
                 address.address().getHostString(), address.toString(),

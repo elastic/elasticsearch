5440dd648104708b84bf960537bbf242c887c81a
Adrien Grand <jpountz@gmail.com>
2017-04-21 10:33:02 +0200

    IndicesQueryCache should delegate the scorerSupplier method. (#24209)
    Otherwise the range improvements that we did on range queries would not work.
    This is similar to https://issues.apache.org/jira/browse/LUCENE-7749.

--- core/src/main/java/org/elasticsearch/indices/IndicesQueryCache.java
+++ core/src/main/java/org/elasticsearch/indices/IndicesQueryCache.java
@@ -27,6 +27,7 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.QueryCache;
 import org.apache.lucene.search.QueryCachingPolicy;
 import org.apache.lucene.search.Scorer;
+import org.apache.lucene.search.ScorerSupplier;
 import org.apache.lucene.search.Weight;
 import org.apache.lucene.search.XLRUQueryCache;
 import org.elasticsearch.common.component.AbstractComponent;
@@ -153,6 +154,12 @@ public class IndicesQueryCache extends AbstractComponent implements QueryCache, 
         public Scorer scorer(LeafReaderContext context) throws IOException {
             shardKeyMap.add(context.reader());
             return in.scorer(context);
+        }
+
+        @Override
+        public ScorerSupplier scorerSupplier(LeafReaderContext context) throws IOException {
+            shardKeyMap.add(context.reader());
+            return in.scorerSupplier(context);
         }
 
         @Override

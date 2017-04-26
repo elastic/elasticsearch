5d313b6e87d683cd28b52b8b63ca66db6b164627
Koen De Groote <kdg.private@gmail.com>
2017-04-26 04:15:00 +0200

    Replace alternating regex with character classes
    This commit replaces two alternating regular expressions (that is,
    regular expressions that consist of the form a|b where a and b are
    characters) with the equivalent regular expression rewritten as a
    character class (that is, [ab]) The reason this is an improvement is
    because a|b involves backtracking while [ab] does not.
    
    Relates #24316

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -292,7 +292,7 @@ public class Version {
         if (snapshot = version.endsWith("-SNAPSHOT")) {
             version = version.substring(0, version.length() - 9);
         }
-        String[] parts = version.split("\\.|\\-");
+        String[] parts = version.split("[.-]");
         if (parts.length < 3 || parts.length > 4) {
             throw new IllegalArgumentException(
                     "the version needs to contain major, minor, and revision, and optionally the build: " + version);

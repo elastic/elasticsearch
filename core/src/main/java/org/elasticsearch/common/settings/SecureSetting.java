621d4ed5273c96dd290e0ae19050674dbe71e907
Ryan Ernst <ryan@iernst.net>
2017-05-11 10:26:57 -0700

    rename sysprop

--- core/src/main/java/org/elasticsearch/common/settings/SecureSetting.java
+++ core/src/main/java/org/elasticsearch/common/settings/SecureSetting.java
@@ -35,7 +35,7 @@ import org.elasticsearch.common.util.ArrayUtils;
 public abstract class SecureSetting<T> extends Setting<T> {
 
     /** Determines whether legacy settings with sensitive values should be allowed. */
-    private static final boolean ALLOW_INSECURE_SETTINGS = Booleans.parseBoolean(System.getProperty("allow_insecure_settings", "false"));
+    private static final boolean ALLOW_INSECURE_SETTINGS = Booleans.parseBoolean(System.getProperty("es.allow_insecure_settings", "false"));
 
     private static final Set<Property> ALLOWED_PROPERTIES = EnumSet.of(Property.Deprecated, Property.Shared);
 

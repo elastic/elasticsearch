5fe1e648e6b88a8ac73133f73b804984d6c4f533
Nik Everett <nik9000@gmail.com>
2017-05-10 23:30:01 -0400

    Get more information when reindex test fails
    It looks like it leaks contexts but it isn't clear why so this
    adds a little more logging. This is the failure:
    https://elasticsearch-ci.elastic.co/job/elastic+elasticsearch+5.4+multijob-intake/94/console

--- modules/reindex/src/test/java/org/elasticsearch/index/reindex/ReindexFailureTests.java
+++ modules/reindex/src/test/java/org/elasticsearch/index/reindex/ReindexFailureTests.java
@@ -23,6 +23,7 @@ import org.elasticsearch.action.ActionRequestValidationException;
 import org.elasticsearch.action.bulk.BulkItemResponse.Failure;
 import org.elasticsearch.action.bulk.byscroll.BulkByScrollResponse;
 import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.test.junit.annotations.TestLogging;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +40,7 @@ import static org.hamcrest.Matchers.lessThanOrEqualTo;
 /**
  * Tests failure capturing and abort-on-failure behavior of reindex.
  */
+@TestLogging("_root:DEBUG")
 public class ReindexFailureTests extends ReindexTestCase {
     public void testFailuresCauseAbortDefault() throws Exception {
         /*
@@ -108,7 +110,7 @@ public class ReindexFailureTests extends ReindexTestCase {
                 logger.info("Didn't trigger a reindex failure on the {} attempt", attempt);
                 attempt++;
             } catch (ExecutionException e) {
-                logger.info("Triggered a reindex failure on the {} attempt", attempt);
+                logger.info("Triggered a reindex failure on the {} attempt: {}", attempt, e.getMessage());
                 assertThat(e.getMessage(),
                         either(containsString("all shards failed"))
                         .or(containsString("No search context found"))

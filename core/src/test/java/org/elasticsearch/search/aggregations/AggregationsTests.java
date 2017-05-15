e65f95651cd0975d3b262a2f3e247d30852f9fd8
Christoph Büscher <christoph@elastic.co>
2017-05-15 15:06:01 +0200

    Small improvement in InternalAggregationTestCase test setup after changes in master (#24675)

--- core/src/test/java/org/elasticsearch/search/aggregations/AggregationsTests.java
+++ core/src/test/java/org/elasticsearch/search/aggregations/AggregationsTests.java
@@ -127,7 +127,7 @@ public class AggregationsTests extends ESTestCase {
 
     @Override
     protected NamedXContentRegistry xContentRegistry() {
-        return new NamedXContentRegistry(InternalAggregationTestCase.getNamedXContents());
+        return new NamedXContentRegistry(InternalAggregationTestCase.getDefaultNamedXContents());
     }
 
     @Before
@@ -145,9 +145,9 @@ public class AggregationsTests extends ESTestCase {
     }
 
     public void testAllAggsAreBeingTested() {
-        assertEquals(InternalAggregationTestCase.getNamedXContents().size(), aggsTests.size());
+        assertEquals(InternalAggregationTestCase.getDefaultNamedXContents().size(), aggsTests.size());
         Set<String> aggs = aggsTests.stream().map((testCase) -> testCase.createTestInstance().getType()).collect(Collectors.toSet());
-        for (NamedXContentRegistry.Entry entry : InternalAggregationTestCase.getNamedXContents()) {
+        for (NamedXContentRegistry.Entry entry : InternalAggregationTestCase.getDefaultNamedXContents()) {
             assertTrue(aggs.contains(entry.name.getPreferredName()));
         }
     }

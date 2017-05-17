292e383d2c7a4dd59258fc3e141001d2699699fb
Simon Willnauer <simonw@apache.org>
2017-05-17 17:28:35 +0200

    Fix static / version based BWC tests (#1456)
    With the leniency in Version.java we missed to really setup BWC
    testing for static indices. This change brings back the testing and adds
    missing bwc indices.
    
    Relates to elastic/elasticsearch#24732

--- server/src/main/java/org/elasticsearch/persistent/PersistentTasksCustomMetaData.java
+++ server/src/main/java/org/elasticsearch/persistent/PersistentTasksCustomMetaData.java
@@ -196,7 +196,7 @@ public final class PersistentTasksCustomMetaData extends AbstractNamedDiffable<M
 
     @Override
     public Version getMinimalSupportedVersion() {
-        return Version.V_5_4_0_UNRELEASED;
+        return Version.V_5_4_0;
     }
 
     @Override

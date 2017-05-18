9046fca7a966e0627074ca23f0d491dacddd7f6f
Ryan Ernst <ryan@iernst.net>
2017-05-18 14:20:23 -0700

    Test: Make mixed cluster bwc test per wire compat version (#24780)
    This commit renames the backwards-5.0 qa test to mixed-cluster and
    creates a test within the project per wire compat version. Like with
    rolling upgrade tests, the integTest task will run against the most
    recent version, while all versions will be tested with the bwcTest task.

--- qa/mixed-cluster/src/test/java/org/elasticsearch/backwards/MixedClusterClientYamlTestSuiteIT.java
+++ qa/mixed-cluster/src/test/java/org/elasticsearch/backwards/MixedClusterClientYamlTestSuiteIT.java
@@ -29,9 +29,9 @@ import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
 import java.io.IOException;
 
 @TimeoutSuite(millis = 40 * TimeUnits.MINUTE) // some of the windows test VMs are slow as hell
-public class Backwards50ClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
+public class MixedClusterClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
 
-    public Backwards50ClientYamlTestSuiteIT(ClientYamlTestCandidate testCandidate) {
+    public MixedClusterClientYamlTestSuiteIT(ClientYamlTestCandidate testCandidate) {
         super(testCandidate);
     }
 

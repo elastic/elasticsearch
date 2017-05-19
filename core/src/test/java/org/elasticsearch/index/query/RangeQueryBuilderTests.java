f1ebb1773ab65b95966a44b0cc163809f3bc1b3d
Nicholas Knize <nknize@gmail.com>
2017-05-19 15:00:48 -0500

    Fix RangeFieldMapper rangeQuery to properly handle relations
    This commit fixes the RangeFieldMapper and RangeQueryBuilder to pass the correct relation to the RangeQuery when performing a range query over range fields.

--- core/src/test/java/org/elasticsearch/index/query/RangeQueryBuilderTests.java
+++ core/src/test/java/org/elasticsearch/index/query/RangeQueryBuilderTests.java
@@ -83,9 +83,6 @@ public class RangeQueryBuilderTests extends AbstractQueryTestCase<RangeQueryBuil
                         query.format("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
                     }
                 }
-                if (query.fieldName().equals(DATE_RANGE_FIELD_NAME)) {
-                    query.relation(RandomPicks.randomFrom(random(), ShapeRelation.values()).getRelationName());
-                }
                 break;
             case 2:
             default:
@@ -100,6 +97,9 @@ public class RangeQueryBuilderTests extends AbstractQueryTestCase<RangeQueryBuil
         }
         if (randomBoolean()) {
             query.to(null);
+        }
+        if (query.fieldName().equals(INT_RANGE_FIELD_NAME) || query.fieldName().equals(DATE_RANGE_FIELD_NAME)) {
+            query.relation(RandomPicks.randomFrom(random(), ShapeRelation.values()).getRelationName());
         }
         return query;
     }

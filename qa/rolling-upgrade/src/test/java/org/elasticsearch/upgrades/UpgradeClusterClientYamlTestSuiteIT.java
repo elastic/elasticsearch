34f526b60bb0637c28ff8e9469ba18d3b656006c
David Kyle <david.kyle@elastic.co>
2017-06-01 14:29:10 +0100

    [ML] Yml rolling upgrade tests (elastic/x-pack-elasticsearch#1555)
    * ML bwc jobs tests
    
    * Test for starting old data feeds
    
    Original commit: elastic/x-pack-elasticsearch@131c7b15eb2c2b6f38d11418b971c948b19e5fa0

--- qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/UpgradeClusterClientYamlTestSuiteIT.java
+++ qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/UpgradeClusterClientYamlTestSuiteIT.java
@@ -7,17 +7,48 @@ package org.elasticsearch.upgrades;
 
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 import com.carrotsearch.randomizedtesting.annotations.TimeoutSuite;
+import org.apache.http.HttpStatus;
 import org.apache.lucene.util.TimeUnits;
+import org.elasticsearch.common.CheckedFunction;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
+import org.elasticsearch.test.rest.yaml.ClientYamlTestResponse;
+import org.elasticsearch.xpack.ml.MachineLearningTemplateRegistry;
 import org.elasticsearch.xpack.security.SecurityClusterClientYamlTestCase;
+import org.junit.Before;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonMap;
 
 @TimeoutSuite(millis = 5 * TimeUnits.MINUTE) // to account for slow as hell VMs
 public class UpgradeClusterClientYamlTestSuiteIT extends SecurityClusterClientYamlTestCase {
+
+    /**
+     * Waits for the Machine Learning templates to be created by {@link MachineLearningTemplateRegistry}
+     */
+    @Before
+    public void waitForTemplates() throws Exception {
+        List<String> templates = new ArrayList<>();
+        templates.addAll(Arrays.asList(MachineLearningTemplateRegistry.TEMPLATE_NAMES));
+
+        for (String template : templates) {
+            awaitCallApi("indices.exists_template", singletonMap("name", template), emptyList(),
+                    response -> true,
+                    () -> "Exception when waiting for [" + template + "] template to be created");
+        }
+    }
 
     @Override
     protected boolean preserveIndicesUponCompletion() {
@@ -39,5 +70,36 @@ public class UpgradeClusterClientYamlTestSuiteIT extends SecurityClusterClientYa
         return Settings.builder()
                 .put(ThreadContext.PREFIX + ".Authorization", token)
                 .build();
+    }
+
+    /**
+     * Executes an API call using the admin context, waiting for it to succeed.
+     */
+    private void awaitCallApi(String apiName,
+                              Map<String, String> params,
+                              List<Map<String, Object>> bodies,
+                              CheckedFunction<ClientYamlTestResponse, Boolean, IOException> success,
+                              Supplier<String> error) throws Exception {
+
+        AtomicReference<IOException> exceptionHolder = new AtomicReference<>();
+        awaitBusy(() -> {
+            try {
+                ClientYamlTestResponse response = getAdminExecutionContext().callApi(apiName, params, bodies, Collections.emptyMap()
+                );
+                if (response.getStatusCode() == HttpStatus.SC_OK) {
+                    exceptionHolder.set(null);
+                    return success.apply(response);
+                }
+                return false;
+            } catch (IOException e) {
+                exceptionHolder.set(e);
+            }
+            return false;
+        });
+
+        IOException exception = exceptionHolder.get();
+        if (exception != null) {
+            throw new IllegalStateException(error.get(), exception);
+        }
     }
 }

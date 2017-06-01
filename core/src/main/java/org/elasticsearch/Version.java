8b390c278dc914968a16f989b691e32111be4462
Jason Tedor <jason@tedor.me>
2017-06-01 11:32:53 -0400

    Add version 5.4.2 constant
    This commit adds the version 5.4.2 constant to 5.4.

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -126,6 +126,8 @@ public class Version {
     public static final Version V_5_4_0 = new Version(V_5_4_0_ID, org.apache.lucene.util.Version.LUCENE_6_5_0);
     public static final int V_5_4_1_ID = 5040199;
     public static final Version V_5_4_1 = new Version(V_5_4_1_ID, org.apache.lucene.util.Version.LUCENE_6_5_1);
+    public static final int V_5_4_2_ID_UNRELEASED = 5040299;
+    public static final Version V_5_4_2_UNRELEASED = new Version(V_5_4_2_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_5_1);
     public static final Version CURRENT = V_5_4_1;
 
     // unreleased versions must be added to the above list with the suffix _UNRELEASED (with the exception of CURRENT)
@@ -141,6 +143,8 @@ public class Version {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_5_4_2_ID_UNRELEASED:
+                return V_5_4_2_UNRELEASED;
             case V_5_4_1_ID:
                 return V_5_4_1;
             case V_5_4_0_ID:

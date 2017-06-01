4185337df17d0716c42ce946b713c67f0f4481cf
Jason Tedor <jason@tedor.me>
2017-06-01 11:18:25 -0400

    Add version 5.3.3 constant
    This commit adds the version 5.3.3 constant to master.

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -72,6 +72,8 @@ public class Version implements Comparable<Version> {
     public static final Version V_5_3_1 = new Version(V_5_3_1_ID, org.apache.lucene.util.Version.LUCENE_6_4_2);
     public static final int V_5_3_2_ID = 5030299;
     public static final Version V_5_3_2 = new Version(V_5_3_2_ID, org.apache.lucene.util.Version.LUCENE_6_4_2);
+    public static final int V_5_3_3_ID = 5030399;
+    public static final Version V_5_3_3 = new Version(V_5_3_3_ID, org.apache.lucene.util.Version.LUCENE_6_4_2);
     public static final int V_5_4_0_ID = 5040099;
     public static final Version V_5_4_0 = new Version(V_5_4_0_ID, org.apache.lucene.util.Version.LUCENE_6_5_0);
     public static final int V_5_4_1_ID = 5040199;
@@ -109,6 +111,8 @@ public class Version implements Comparable<Version> {
                 return V_5_4_1;
             case V_5_4_0_ID:
                 return V_5_4_0;
+            case V_5_3_3_ID:
+                return V_5_3_3;
             case V_5_3_2_ID:
                 return V_5_3_2;
             case V_5_3_1_ID:

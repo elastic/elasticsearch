a85ec92efeb5ab101957dbe6ca0e52fc6f7ffc1a
Jim Ferenczi <jim.ferenczi@elastic.co>
2017-06-02 09:35:06 +0200

    Add the ability to set eager_global_ordinals in the new parent-join field

--- modules/parent-join/src/main/java/org/elasticsearch/join/mapper/ParentIdFieldMapper.java
+++ modules/parent-join/src/main/java/org/elasticsearch/join/mapper/ParentIdFieldMapper.java
@@ -73,6 +73,11 @@ public final class ParentIdFieldMapper extends FieldMapper {
             return children;
         }
 
+        public Builder eagerGlobalOrdinals(boolean eagerGlobalOrdinals) {
+            fieldType().setEagerGlobalOrdinals(eagerGlobalOrdinals);
+            return builder;
+        }
+
         @Override
         public ParentIdFieldMapper build(BuilderContext context) {
             fieldType.setName(name);

1cef531165e9488a9032596ae7b907d0e1fcc2c1
Chris Earle <pickypg@users.noreply.github.com>
2017-06-02 10:01:21 -0400

    Always Accumulate Transport Exceptions (#1619)
    This is the x-pack side of the removal of `accumulateExceptions()` for both `TransportNodesAction` and `TransportTasksAction`.
    
    There are occasional, random failures that occur during API calls that are silently ignored from the caller's perspective, which also leads to weird API responses that have no response and also no errors, which is obviously untrue.

--- server/src/test/java/org/elasticsearch/persistent/TestPersistentTasksPlugin.java
+++ server/src/test/java/org/elasticsearch/persistent/TestPersistentTasksPlugin.java
@@ -546,10 +546,6 @@ public class TestPersistentTasksPlugin extends Plugin implements ActionPlugin {
             listener.onResponse(new TestTaskResponse());
         }
 
-        @Override
-        protected boolean accumulateExceptions() {
-            return false;
-        }
     }
 
 

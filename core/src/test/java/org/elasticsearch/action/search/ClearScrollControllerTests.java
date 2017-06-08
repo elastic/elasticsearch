cd72490024841331539a726b5ea36c3909bb75b2
Simon Willnauer <simonw@apache.org>
2017-06-08 09:36:55 +0200

    fix compilation issue

--- core/src/test/java/org/elasticsearch/action/search/ClearScrollControllerTests.java
+++ core/src/test/java/org/elasticsearch/action/search/ClearScrollControllerTests.java
@@ -72,10 +72,15 @@ public class ClearScrollControllerTests extends ESTestCase {
         List<DiscoveryNode> nodesInvoked = new CopyOnWriteArrayList<>();
         SearchTransportService searchTransportService = new SearchTransportService(Settings.EMPTY, null) {
             @Override
-            public void sendClearAllScrollContexts(DiscoveryNode node, ActionListener<TransportResponse> listener) {
-                nodesInvoked.add(node);
+            public void sendClearAllScrollContexts(Transport.Connection connection, ActionListener<TransportResponse> listener) {
+                nodesInvoked.add(connection.getNode());
                 Thread t = new Thread(() -> listener.onResponse(TransportResponse.Empty.INSTANCE)); // response is unused
                 t.start();
+            }
+
+            @Override
+            Transport.Connection getConnection(String clusterAlias, DiscoveryNode node) {
+                return new SearchAsyncActionTests.MockConnection(node);
             }
         };
         ClearScrollRequest clearScrollRequest = new ClearScrollRequest();

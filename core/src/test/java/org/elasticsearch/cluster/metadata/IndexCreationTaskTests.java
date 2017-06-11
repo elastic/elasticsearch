0004b020d03728faddccdcd81e880340fef69b43
Sergey Galkin <sergey@galkin.me>
2017-06-11 10:31:23 +0300

    extract task from anonymous class to inner in onlyCreateIndex

---
+++ core/src/test/java/org/elasticsearch/cluster/metadata/IndexCreationTaskTests.java
@@ -0,0 +1,12 @@
+package org.elasticsearch.cluster.metadata;
+
+import org.elasticsearch.test.ESTestCase;
+
+public class IndexCreationTaskTests extends ESTestCase {
+
+    public void testExecute() {
+        // @todo basic run
+        // @todo run with templates
+        // @todo run with aliases
+    }
+}

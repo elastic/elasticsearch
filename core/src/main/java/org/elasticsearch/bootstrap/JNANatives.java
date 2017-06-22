97a2c4523de9801b8a0ddbe8b056b128f968e327
Jason Tedor <jason@tedor.me>
2017-06-22 07:59:58 -0400

    Get short path name for native controllers
    Due to limitations with CreateProcessW on Windows (ultimately used by
    ProcessBuilder) with respect to maximum path lengths, we need to get the
    short path name for any native controllers before trying to start them
    in case the absolute path exceeds the maximum path length. This commit
    uses JNA to invoke the necessary Windows API for this to start the
    native controller using the short path.
    
    To be precise about the limitation here, the MSDN docs for
    CreateProcessW say for the command line parameter:
    
    >The command line to be executed. The maximum length of this string is
    >32,768 characters, including the Unicode terminating null character. If
    >lpApplicationName is NULL, the module name portionof lpCommandLine is
    >limited to MAX_PATH characters.
    
    This is exactly how the Windows implementation of Process in the JDK
    invokes CreateProcessW: with the executable name (lpApplicationName) set
    to NULL.
    
    Relates #25344

--- core/src/main/java/org/elasticsearch/bootstrap/JNANatives.java
+++ core/src/main/java/org/elasticsearch/bootstrap/JNANatives.java
@@ -21,6 +21,7 @@ package org.elasticsearch.bootstrap;
 
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
+import com.sun.jna.WString;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.util.Constants;
 import org.elasticsearch.common.logging.Loggers;
@@ -191,6 +192,35 @@ class JNANatives {
             if (process != null) {
                 kernel.CloseHandle(process);
             }
+        }
+    }
+
+    /**
+     * Retrieves the short path form of the specified path.
+     *
+     * @param path the path
+     * @return the short path name (or the original path if getting the short path name fails for any reason)
+     */
+    static String getShortPathName(String path) {
+        assert Constants.WINDOWS;
+        try {
+            final WString longPath = new WString("\\\\?\\" + path);
+            // first we get the length of the buffer needed
+            final int length = JNAKernel32Library.getInstance().GetShortPathNameW(longPath, null, 0);
+            if (length == 0) {
+                logger.warn("failed to get short path name: {}", Native.getLastError());
+                return path;
+            }
+            final char[] shortPath = new char[length];
+            // knowing the length of the buffer, now we get the short name
+            if (JNAKernel32Library.getInstance().GetShortPathNameW(longPath, shortPath, length) > 0) {
+                return Native.toString(shortPath);
+            } else {
+                logger.warn("failed to get short path name: {}", Native.getLastError());
+                return path;
+            }
+        } catch (final UnsatisfiedLinkError e) {
+            return path;
         }
     }
 

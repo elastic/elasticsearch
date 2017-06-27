b710f5906f24f1c5b342cc8eb03397203ebc1ed0
Jason Tedor <jason@tedor.me>
2017-06-27 15:18:03 -0400

    Register primary context handoff as known handler
    This commit registers the primary context handoff handler as known.
    
    relates elastic/x-pack-elasticsearch#1856
    
    Original commit: elastic/x-pack-elasticsearch@f91142a52160a45c1381961cd7b144452ea16f88

--- plugin/src/test/java/org/elasticsearch/transport/KnownActionsTests.java
+++ plugin/src/test/java/org/elasticsearch/transport/KnownActionsTests.java
@@ -73,7 +73,6 @@ public class KnownActionsTests extends SecurityIntegTestCase {
         codeActions = loadCodeActions();
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/elastic/x-pack-elasticsearch/issues/1856")
     public void testAllTransportHandlersAreKnown() {
         TransportService transportService = internalCluster().getDataNodeInstance(TransportService.class);
         for (String handler : transportService.requestHandlers.keySet()) {

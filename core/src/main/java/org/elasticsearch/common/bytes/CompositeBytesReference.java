4e5868f7a6c52a6b56636056052d0a9643885435
Tim Brooks <tim@uncontended.net>
2017-06-27 10:48:22 -0500

    Remove composite buffer changes

--- core/src/main/java/org/elasticsearch/common/bytes/CompositeBytesReference.java
+++ core/src/main/java/org/elasticsearch/common/bytes/CompositeBytesReference.java
@@ -26,8 +26,6 @@ import org.apache.lucene.util.RamUsageEstimator;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 
 /**
@@ -105,7 +103,18 @@ public final class CompositeBytesReference extends BytesReference {
 
     @Override
     public BytesRef toBytesRef() {
-        return compositeReferenceToBytesRef(this);
+        BytesRefBuilder builder = new BytesRefBuilder();
+        builder.grow(length());
+        BytesRef spare;
+        BytesRefIterator iterator = iterator();
+        try {
+            while ((spare = iterator.next()) != null) {
+                builder.append(spare);
+            }
+        } catch (IOException ex) {
+            throw new AssertionError("won't happen", ex); // this is really an error since we don't do IO in our bytesreferences
+        }
+        return builder.toBytesRef();
     }
 
     @Override
@@ -132,25 +141,11 @@ public final class CompositeBytesReference extends BytesReference {
         } else {
             return () -> null;
         }
+
     }
 
     @Override
     public long ramBytesUsed() {
        return ramBytesUsed;
-    }
-
-    public static BytesRef compositeReferenceToBytesRef(BytesReference reference) {
-        BytesRefBuilder builder = new BytesRefBuilder();
-        builder.grow(reference.length());
-        BytesRef spare;
-        BytesRefIterator iterator = reference.iterator();
-        try {
-            while ((spare = iterator.next()) != null) {
-                builder.append(spare);
-            }
-        } catch (IOException ex) {
-            throw new AssertionError("won't happen", ex); // this is really an error since we don't do IO in our bytesreferences
-        }
-        return builder.toBytesRef();
     }
 }

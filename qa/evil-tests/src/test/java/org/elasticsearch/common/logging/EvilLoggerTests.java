1cbcad82eb253a1eaf08662853e0bf873ba1b2ce
Jason Tedor <jason@tedor.me>
2017-06-29 12:30:13 -0400

    Fix raw-types 2

--- qa/evil-tests/src/test/java/org/elasticsearch/common/logging/EvilLoggerTests.java
+++ qa/evil-tests/src/test/java/org/elasticsearch/common/logging/EvilLoggerTests.java
@@ -168,7 +168,7 @@ public class EvilLoggerTests extends ESTestCase {
     public void testDeprecatedSettings() throws IOException, UserException {
         setupLogging("settings");
 
-        final Setting setting = Setting.boolSetting("deprecated.foo", false, Setting.Property.Deprecated);
+        final Setting<Boolean> setting = Setting.boolSetting("deprecated.foo", false, Setting.Property.Deprecated);
         final Settings settings = Settings.builder().put("deprecated.foo", true).build();
 
         final int iterations = randomIntBetween(0, 128);

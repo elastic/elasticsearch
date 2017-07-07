e05c817e572df71bf1449f8b0b7e64ba360ed672
Boaz Leskes <b.leskes@gmail.com>
2017-07-07 17:37:51 +0200

    Add v5_5_1 constant

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -84,6 +84,8 @@ public class Version implements Comparable<Version> {
     public static final Version V_5_4_3 = new Version(V_5_4_3_ID, org.apache.lucene.util.Version.LUCENE_6_5_1);
     public static final int V_5_5_0_ID = 5050099;
     public static final Version V_5_5_0 = new Version(V_5_5_0_ID, org.apache.lucene.util.Version.LUCENE_6_6_0);
+    public static final int V_5_5_1_ID = 5050199;
+    public static final Version V_5_5_1 = new Version(V_5_5_1_ID, org.apache.lucene.util.Version.LUCENE_6_6_0);
     public static final int V_5_6_0_ID = 5060099;
     public static final Version V_5_6_0 = new Version(V_5_6_0_ID, org.apache.lucene.util.Version.LUCENE_6_6_0);
     public static final int V_6_0_0_alpha1_ID = 6000001;
@@ -118,6 +120,8 @@ public class Version implements Comparable<Version> {
                 return V_6_0_0_alpha1;
             case V_5_6_0_ID:
                 return V_5_6_0;
+            case V_5_5_1_ID:
+                return V_5_5_1;
             case V_5_5_0_ID:
                 return V_5_5_0;
             case V_5_4_3_ID:

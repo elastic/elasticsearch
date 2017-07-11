5bf876db9d9cf50d56b6c74d3c9d7aa895b5ffbc
Simon Willnauer <simonw@apache.org>
2017-07-11 16:58:02 +0200

    Ensure we rewrite common queries to `match_none` if possible
    In certain situations we can early terminate and just skip the entire
    query phase or make the lucene level rewrite very cheap if we can already
    tell that a query won't match any documents. For instance if there is a single
    `match_none` ie. due to some range rewrite in a filter or must clause of a boolean
    query it can just drop all it's other queries since it will never match.

--- core/src/main/java/org/elasticsearch/index/query/ConstantScoreQueryBuilder.java
+++ core/src/main/java/org/elasticsearch/index/query/ConstantScoreQueryBuilder.java
@@ -155,6 +155,9 @@ public class ConstantScoreQueryBuilder extends AbstractQueryBuilder<ConstantScor
     @Override
     protected QueryBuilder doRewrite(QueryRewriteContext queryRewriteContext) throws IOException {
         QueryBuilder rewrite = filterBuilder.rewrite(queryRewriteContext);
+        if (rewrite instanceof MatchNoneQueryBuilder) {
+            return rewrite; // we won't match anyway
+        }
         if (rewrite != filterBuilder) {
             return new ConstantScoreQueryBuilder(rewrite);
         }

9d8f11dc27c1fc2057e7ea5a82c0a5f364f5e8ce
Jason Tedor <jason@tedor.me>
2017-07-20 22:42:39 +0900

    Remove legacy checks for config file settings
    This commit removes legacy checks for unsupported an environment
    variable and unsupported system properties. This environment variable
    and these system properties have not been supported since 1.x so it is
    safe to stop checking for the existence of these settings.
    
    Relates #25809

--- core/src/main/java/org/elasticsearch/bootstrap/Bootstrap.java
+++ core/src/main/java/org/elasticsearch/bootstrap/Bootstrap.java
@@ -290,8 +290,6 @@ final class Bootstrap {
         } catch (IOException e) {
             throw new BootstrapException(e);
         }
-        checkForCustomConfFile();
-
         if (environment.pidFile() != null) {
             try {
                 PidFile.create(environment.pidFile(), true);
@@ -387,28 +385,6 @@ final class Bootstrap {
     @SuppressForbidden(reason = "System#err")
     private static void closeSysError() {
         System.err.close();
-    }
-
-    private static void checkForCustomConfFile() {
-        String confFileSetting = System.getProperty("es.default.config");
-        checkUnsetAndMaybeExit(confFileSetting, "es.default.config");
-        confFileSetting = System.getProperty("es.config");
-        checkUnsetAndMaybeExit(confFileSetting, "es.config");
-        confFileSetting = System.getProperty("elasticsearch.config");
-        checkUnsetAndMaybeExit(confFileSetting, "elasticsearch.config");
-    }
-
-    private static void checkUnsetAndMaybeExit(String confFileSetting, String settingName) {
-        if (confFileSetting != null && confFileSetting.isEmpty() == false) {
-            Logger logger = Loggers.getLogger(Bootstrap.class);
-            logger.info("{} is no longer supported. elasticsearch.yml must be placed in the config directory and cannot be renamed.", settingName);
-            exit(1);
-        }
-    }
-
-    @SuppressForbidden(reason = "Allowed to exit explicitly in bootstrap phase")
-    private static void exit(int status) {
-        System.exit(status);
     }
 
     private static void checkLucene() {

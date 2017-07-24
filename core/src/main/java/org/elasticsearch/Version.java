c90bc407d9302ee76072fe45fd93417d3ed53fae
Ryan Ernst <ryan@iernst.net>
2017-07-24 12:38:02 -0700

    Add version constants for 6.1 to 6.x branch

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -89,17 +89,14 @@ public class Version implements Comparable<Version> {
     public static final int V_5_6_0_ID = 5060099;
     public static final Version V_5_6_0 = new Version(V_5_6_0_ID, org.apache.lucene.util.Version.LUCENE_6_6_0);
     public static final int V_6_0_0_alpha1_ID = 6000001;
-    public static final Version V_6_0_0_alpha1 =
-            new Version(V_6_0_0_alpha1_ID, org.apache.lucene.util.Version.LUCENE_7_0_0);
+    public static final Version V_6_0_0_alpha1 = new Version(V_6_0_0_alpha1_ID, org.apache.lucene.util.Version.LUCENE_7_0_0);
     public static final int V_6_0_0_alpha2_ID = 6000002;
-    public static final Version V_6_0_0_alpha2 =
-            new Version(V_6_0_0_alpha2_ID, org.apache.lucene.util.Version.LUCENE_7_0_0);
+    public static final Version V_6_0_0_alpha2 = new Version(V_6_0_0_alpha2_ID, org.apache.lucene.util.Version.LUCENE_7_0_0);
     public static final int V_6_0_0_beta1_ID = 6000026;
-    public static final Version V_6_0_0_beta1 =
-        new Version(V_6_0_0_beta1_ID, org.apache.lucene.util.Version.LUCENE_7_0_0);
-    public static final Version CURRENT = V_6_0_0_beta1;
-
-    // unreleased versions must be added to the above list with the suffix _UNRELEASED (with the exception of CURRENT)
+    public static final Version V_6_0_0_beta1 = new Version(V_6_0_0_beta1_ID, org.apache.lucene.util.Version.LUCENE_7_0_0);
+    public static final int V_6_1_0_ID = 6010099;
+    public static final Version V_6_1_0 = new Version(V_6_1_0_ID, org.apache.lucene.util.Version.LUCENE_7_0_0);
+    public static final Version CURRENT = V_6_1_0;
 
     static {
         assert CURRENT.luceneVersion.equals(org.apache.lucene.util.Version.LATEST) : "Version must be upgraded to ["
@@ -112,6 +109,8 @@ public class Version implements Comparable<Version> {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_6_1_0_ID:
+                return V_6_1_0;
             case V_6_0_0_beta1_ID:
                 return V_6_0_0_beta1;
             case V_6_0_0_alpha2_ID:

b1f7d3c71e13bc0717df41c969e63b5d2cfdd521
Lee Hinman <lee@writequit.org>
2017-07-25 09:50:29 -0600

    Add 5.5.1 version constant and BWC indices

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -134,8 +134,10 @@ public class Version implements Comparable<Version> {
     public static final Version V_5_4_3 = new Version(V_5_4_3_ID, org.apache.lucene.util.Version.LUCENE_6_5_1);
     public static final int V_5_5_0_ID = 5050099;
     public static final Version V_5_5_0 = new Version(V_5_5_0_ID, org.apache.lucene.util.Version.LUCENE_6_6_0);
-    public static final int V_5_5_1_ID_UNRELEASED = 5050199;
-    public static final Version V_5_5_1_UNRELEASED = new Version(V_5_5_1_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_6_0);
+    public static final int V_5_5_1_ID = 5050199;
+    public static final Version V_5_5_1 = new Version(V_5_5_1_ID, org.apache.lucene.util.Version.LUCENE_6_6_0);
+    public static final int V_5_5_2_ID_UNRELEASED = 5050299;
+    public static final Version V_5_5_2_UNRELEASED = new Version(V_5_5_2_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_6_0);
     public static final int V_5_6_0_ID_UNRELEASED = 5060099;
     public static final Version V_5_6_0_UNRELEASED = new Version(V_5_6_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_6_0);
     public static final Version CURRENT = V_5_6_0_UNRELEASED;
@@ -155,8 +157,10 @@ public class Version implements Comparable<Version> {
         switch (id) {
             case V_5_6_0_ID_UNRELEASED:
                 return V_5_6_0_UNRELEASED;
-            case V_5_5_1_ID_UNRELEASED:
-                return V_5_5_1_UNRELEASED;
+            case V_5_5_2_ID_UNRELEASED:
+                return V_5_5_2_UNRELEASED;
+            case V_5_5_1_ID:
+                return V_5_5_1;
             case V_5_5_0_ID:
                 return V_5_5_0;
             case V_5_4_3_ID:

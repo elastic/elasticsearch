2147c7fc1ff2237de9fc0baff49cc754f80ee835
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2017-07-26 12:16:35 +0100

    javadocs

--- core/src/main/java/org/elasticsearch/common/xcontent/ToXContentFragment.java
+++ core/src/main/java/org/elasticsearch/common/xcontent/ToXContentFragment.java
@@ -19,6 +19,14 @@
 
 package org.elasticsearch.common.xcontent;
 
+/**
+ * An interface allowing to transfer an object to "XContent" using an
+ * {@link XContentBuilder}. The difference between {@link ToXContentFragment}
+ * and {@link ToXContentObject} is that the former outputs a fragment that
+ * requires to start and end a new anonymous object externally, while the latter
+ * guarantees that what gets printed out is fully valid syntax without any
+ * external addition.
+ */
 public interface ToXContentFragment extends ToXContent {
 
     @Override

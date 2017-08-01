7291eb55feec5a3404e934c3f15438fff992d513
Jay Modi <jaymode@users.noreply.github.com>
2017-08-01 07:36:35 -0600

    Automatically enable AES 256 bit TLS ciphers when available (elastic/x-pack-elasticsearch#2137)
    This commit adds detection of support for AES 256 bit ciphers and enables their use when the JVM
    supports them. For OpenJDK, this is often the case without any changes but for the Oracle JVM, the
    unlimited policy file needs to be installed. In order to simplify the work a user would need to do
    we can detect this support and automatically enable the AES 256 bit versions of the ciphers we
    already enable.
    
    Original commit: elastic/x-pack-elasticsearch@5f23b18a1e164372a54ad9975446061731476efe

---
+++ plugin/src/test/java/org/elasticsearch/xpack/XPackSettingsTests.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+package org.elasticsearch.xpack;
+
+import org.elasticsearch.test.ESTestCase;
+
+import javax.crypto.Cipher;
+
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
+
+public class XPackSettingsTests extends ESTestCase {
+
+    public void testDefaultSSLCiphers() throws Exception {
+        assertThat(XPackSettings.DEFAULT_CIPHERS, hasItem("TLS_RSA_WITH_AES_128_CBC_SHA"));
+
+        final boolean useAES256 = Cipher.getMaxAllowedKeyLength("AES") > 128;
+        if (useAES256) {
+            logger.info("AES 256 is available");
+            assertThat(XPackSettings.DEFAULT_CIPHERS, hasItem("TLS_RSA_WITH_AES_256_CBC_SHA"));
+        } else {
+            logger.info("AES 256 is not available");
+            assertThat(XPackSettings.DEFAULT_CIPHERS, not(hasItem("TLS_RSA_WITH_AES_256_CBC_SHA")));
+        }
+    }
+}

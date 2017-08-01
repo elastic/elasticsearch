b8c33d163fc6f74ed460f575ee1f4858317c3b28
Tim Brooks <tim@uncontended.net>
2017-08-01 14:24:45 -0500

    WIP

---
+++ core/src/main/java/org/elasticsearch/common/breaker/DebuggingBreaker.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.common.breaker;
+
+public class DebuggingBreaker implements CircuitBreaker {
+
+    private final CircuitBreaker breaker;
+
+    public DebuggingBreaker(CircuitBreaker breaker) {
+        this.breaker = breaker;
+    }
+
+    @Override
+    public void circuitBreak(String fieldName, long bytesNeeded) {
+        breaker.circuitBreak(fieldName, bytesNeeded);
+    }
+
+    @Override
+    public double addEstimateBytesAndMaybeBreak(long bytes, String label) throws CircuitBreakingException {
+        return breaker.addEstimateBytesAndMaybeBreak(bytes, label);
+    }
+
+    @Override
+    public long addWithoutBreaking(long bytes) {
+        return breaker.addWithoutBreaking(bytes);
+    }
+
+    @Override
+    public long getUsed() {
+        return breaker.getUsed();
+    }
+
+    @Override
+    public long getLimit() {
+        return breaker.getLimit();
+    }
+
+    @Override
+    public double getOverhead() {
+        return breaker.getOverhead();
+    }
+
+    @Override
+    public long getTrippedCount() {
+        return breaker.getTrippedCount();
+    }
+
+    @Override
+    public String getName() {
+        return breaker.getName();
+    }
+}

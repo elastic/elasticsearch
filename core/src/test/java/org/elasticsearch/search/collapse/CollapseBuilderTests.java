87c6e63e73a579440578ee0922413d56966faa7e
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2017-08-02 11:38:31 +0100

    Adds mutate function to various tests (#25999)
    * Adds mutate function to various tests
    
    Relates to #25929
    
    * fix test
    
    * implements mutate function for all single bucket aggs
    
    * review comments
    
    * convert getMutateFunction to mutateIInstance

--- core/src/test/java/org/elasticsearch/search/collapse/CollapseBuilderTests.java
+++ core/src/test/java/org/elasticsearch/search/collapse/CollapseBuilderTests.java
@@ -103,6 +103,32 @@ public class CollapseBuilderTests extends AbstractSerializingTestCase<CollapseBu
     }
 
     @Override
+    protected CollapseBuilder mutateInstance(CollapseBuilder instance) throws IOException {
+        CollapseBuilder newBuilder;
+        switch (between(0, 2)) {
+        case 0:
+            newBuilder = new CollapseBuilder(instance.getField() + randomAlphaOfLength(10));
+            newBuilder.setMaxConcurrentGroupRequests(instance.getMaxConcurrentGroupRequests());
+            newBuilder.setInnerHits(instance.getInnerHits());
+            break;
+        case 1:
+            newBuilder = copyInstance(instance);
+            newBuilder.setMaxConcurrentGroupRequests(instance.getMaxConcurrentGroupRequests() + between(1, 20));
+            break;
+        case 2:
+        default:
+            newBuilder = copyInstance(instance);
+            List<InnerHitBuilder> innerHits = newBuilder.getInnerHits();
+            for (int i = 0; i < between(1, 5); i++) {
+                innerHits.add(InnerHitBuilderTests.randomInnerHits());
+            }
+            newBuilder.setInnerHits(innerHits);
+            break;
+        }
+        return newBuilder;
+    }
+
+    @Override
     protected NamedWriteableRegistry getNamedWriteableRegistry() {
         return namedWriteableRegistry;
     }

1146a35870a4142b6737239977d6538362d46ab4
Martijn van Groningen <martijn.v.groningen@gmail.com>
2017-08-03 15:00:56 +0200

    Move more token filters to analysis-common module
    The following token filters were moved: arabic_stem, brazilian_stem, czech_stem, dutch_stem, french_stem, german_stem and russian_stem.
    
    Relates to #23658

--- modules/analysis-common/src/main/java/org/elasticsearch/analysis/common/GermanStemTokenFilterFactory.java
+++ modules/analysis-common/src/main/java/org/elasticsearch/analysis/common/GermanStemTokenFilterFactory.java
@@ -17,27 +17,29 @@
  * under the License.
  */
 
-package org.elasticsearch.index.analysis;
+package org.elasticsearch.analysis.common;
 
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.br.BrazilianStemFilter;
-import org.apache.lucene.analysis.miscellaneous.SetKeywordMarkerFilter;
 import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.de.GermanStemFilter;
+import org.apache.lucene.analysis.miscellaneous.SetKeywordMarkerFilter;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
+import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
+import org.elasticsearch.index.analysis.Analysis;
 
-public class BrazilianStemTokenFilterFactory extends AbstractTokenFilterFactory {
+public class GermanStemTokenFilterFactory extends AbstractTokenFilterFactory {
 
     private final CharArraySet exclusions;
 
-    public BrazilianStemTokenFilterFactory(IndexSettings indexSettings, Environment environment, String name, Settings settings) {
+    GermanStemTokenFilterFactory(IndexSettings indexSettings, Environment environment, String name, Settings settings) {
         super(indexSettings, name, settings);
         this.exclusions = Analysis.parseStemExclusion(settings, CharArraySet.EMPTY_SET);
     }
 
     @Override
     public TokenStream create(TokenStream tokenStream) {
-        return new BrazilianStemFilter(new SetKeywordMarkerFilter(tokenStream, exclusions));
+        return new GermanStemFilter(new SetKeywordMarkerFilter(tokenStream, exclusions));
     }
 }
--- modules/analysis-common/src/main/java/org/elasticsearch/analysis/common/RussianStemTokenFilterFactory.java
+++ modules/analysis-common/src/main/java/org/elasticsearch/analysis/common/RussianStemTokenFilterFactory.java
@@ -17,29 +17,23 @@
  * under the License.
  */
 
-package org.elasticsearch.index.analysis;
+package org.elasticsearch.analysis.common;
 
-import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.miscellaneous.SetKeywordMarkerFilter;
 import org.apache.lucene.analysis.snowball.SnowballFilter;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
-import org.tartarus.snowball.ext.DutchStemmer;
+import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
 
-public class DutchStemTokenFilterFactory extends AbstractTokenFilterFactory {
+public class RussianStemTokenFilterFactory extends AbstractTokenFilterFactory {
 
-    private final CharArraySet exclusions;
-
-    public DutchStemTokenFilterFactory(IndexSettings indexSettings, Environment environment, String name, Settings settings) {
+    public RussianStemTokenFilterFactory(IndexSettings indexSettings, Environment environment, String name, Settings settings) {
         super(indexSettings, name, settings);
-        this.exclusions = Analysis.parseStemExclusion(settings, CharArraySet.EMPTY_SET);
     }
 
     @Override
     public TokenStream create(TokenStream tokenStream) {
-        tokenStream = new SetKeywordMarkerFilter(tokenStream, exclusions);
-        return new SnowballFilter(tokenStream, new DutchStemmer());
+        return new SnowballFilter(tokenStream, "Russian");
     }
 }

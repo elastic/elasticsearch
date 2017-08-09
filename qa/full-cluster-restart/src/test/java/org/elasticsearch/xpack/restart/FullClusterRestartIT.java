2b2f831116e0402c07c929405954e94d5bb1455d
Nik Everett <nik9000@gmail.com>
2017-08-09 15:58:47 -0400

    Fix broke bwc test in sql
    The test was enabled for the wrong versions....
    
    Original commit: elastic/x-pack-elasticsearch@2662a11e0cc9c536f51be20a9c5215c881bbac29

--- qa/full-cluster-restart/src/test/java/org/elasticsearch/xpack/restart/FullClusterRestartIT.java
+++ qa/full-cluster-restart/src/test/java/org/elasticsearch/xpack/restart/FullClusterRestartIT.java
@@ -251,7 +251,7 @@ public class FullClusterRestartIT extends ESRestTestCase {
     public void testSqlFailsOnIndexWithTwoTypes() throws IOException {
         // TODO this isn't going to trigger until we backport to 6.1
         assumeTrue("It is only possible to build an index that sql doesn't like before 6.0.0",
-                oldClusterVersion.onOrAfter(Version.V_6_0_0_alpha1));
+                oldClusterVersion.before(Version.V_6_0_0_alpha1));
         if (runningAgainstOldCluster) {
             client().performRequest("POST", "/testsqlfailsonindexwithtwotypes/type1", emptyMap(),
                     new StringEntity("{}", ContentType.APPLICATION_JSON));

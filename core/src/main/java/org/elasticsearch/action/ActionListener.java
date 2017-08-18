691d7ed8a528687fb7e1afc2545c76fc4ed6e79f
Tim Brooks <tim@uncontended.net>
2017-08-18 10:39:07 -0500

    Fix documentation

--- core/src/main/java/org/elasticsearch/action/ActionListener.java
+++ core/src/main/java/org/elasticsearch/action/ActionListener.java
@@ -71,12 +71,11 @@ public interface ActionListener<Response> {
 
     /**
      * Creates a listener that listens for a response (or failure) and executes the
-     * corresponding consumer when the response (or failure) is received.
+     * corresponding runnable when the response (or failure) is received.
      *
-     * @param onResponse the checked consumer of the response, when the listener receives one
-     * @param onFailure the consumer of the failure, when the listener receives one
+     * @param runnable the runnable to be executed when the listener is called.
      * @param <Response> the type of the response
-     * @return a listener that listens for responses and invokes the consumer when received
+     * @return a listener that listens for responses and invokes the runnable when received
      */
     static <Response> ActionListener<Response> wrap(Runnable runnable) {
         return new ActionListener<Response>() {

5125978f600539c16d72a6c6af9b20149e9b2009
Jason Tedor <jason@tedor.me>
2017-08-17 22:50:52 -0400

    Enable command extensions
    We rely on command extensions in our scripts but we do not actually
    guarantee that they are enabled (usually they are, by default, but they
    can be disabled outside of our control). This commit ensures that they
    are enabled.
    
    Relates elastic/x-pack-elasticsearch#2307
    
    Original commit: elastic/x-pack-elasticsearch@a5eec8ca7b5db31ac76982fed00473cab4d9c70b

--- plugin/bin/x-pack/syskeygen.bat
+++ plugin/bin/x-pack/syskeygen.bat
@@ -5,6 +5,7 @@ rem or more contributor license agreements. Licensed under the Elastic License;
 rem you may not use this file except in compliance with the Elastic License.
 
 setlocal enabledelayedexpansion
+setlocal enableextensions
 
 call "%~dp0..\elasticsearch-env.bat" || exit /b 1
 
@@ -18,4 +19,5 @@ call "%~dp0x-pack-env.bat" || exit /b 1
   org.elasticsearch.xpack.security.crypto.tool.SystemKeyTool ^
   %*
 
+endlocal
 endlocal

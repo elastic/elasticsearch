f38ade6c3aa1a048e1c9bf7449a4af2b65dd18d0
Tim Brooks <tim@uncontended.net>
2017-08-22 15:16:40 -0500

    Merge tsa with ts

--- core/src/test/java/org/elasticsearch/client/transport/TransportClientNodesServiceTests.java
+++ core/src/test/java/org/elasticsearch/client/transport/TransportClientNodesServiceTests.java
@@ -61,7 +61,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
--- core/src/test/java/org/elasticsearch/indices/cluster/ClusterStateChanges.java
+++ core/src/test/java/org/elasticsearch/indices/cluster/ClusterStateChanges.java
@@ -75,7 +75,6 @@ import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexService;
 import org.elasticsearch.index.mapper.MapperService;
 import org.elasticsearch.index.shard.IndexEventListener;
-import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.test.gateway.TestGatewayAllocator;
 import org.elasticsearch.threadpool.ThreadPool;
@@ -87,7 +86,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import static com.carrotsearch.randomizedtesting.RandomizedTest.getRandom;

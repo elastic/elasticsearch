35f2b421047ba1bf2a47fd17e0bd4c24a0313d3c
Jason Tedor <jason@tedor.me>
2017-08-28 20:47:58 -0400

    Add packaging tests for keystore creation
    This commit adds a packaging test that the keystore is created when
    X-Pack is installed, and that it has the correct ownership and
    permissions.
    
    Relates elastic/x-pack-elasticsearch#2380
    
    Original commit: elastic/x-pack-elasticsearch@f7a3b3bc0e77e83da01c2655c2b36900d00b6bde

---
+++ qa/vagrant/src/test/resources/packaging/tests/keystore.bash
@@ -0,0 +1,55 @@
+#!/usr/bin/env bats
+
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+
+load $BATS_UTILS/utils.bash
+load $BATS_UTILS/plugins.bash
+
+setup() {
+    if [ $BATS_TEST_NUMBER == 1 ]; then
+        clean_before_test
+        install
+    fi
+}
+
+if [[ "$BATS_TEST_FILENAME" =~ 20_tar_keystore.bats$ ]]; then
+    load $BATS_UTILS/tar.bash
+    GROUP='TAR KEYSTORE'
+    install() {
+        install_archive
+        verify_archive_installation
+    }
+    export ESHOME=/tmp/elasticsearch
+    export_elasticsearch_paths
+    export ESPLUGIN_COMMAND_USER=elasticsearch
+else
+    load $BATS_UTILS/packages.bash
+    if is_rpm; then
+        GROUP='RPM KEYSTORE'
+    elif is_dpkg; then
+        GROUP='DEB KEYSTORE'
+    fi
+    export_elasticsearch_paths
+    export ESPLUGIN_COMMAND_USER=root
+    install() {
+        install_package
+        verify_package_installation
+    }
+fi
+
+@test "[$GROUP] keystore does not exist" {
+    assert_file_not_exist /etc/elasticsearch/elasticsearch.keystore
+}
+
+@test "[$GROUP] keystore exists after install" {
+    install_and_check_plugin x pack x-pack-*.jar
+    if [[ "$BATS_TEST_FILENAME" =~ 20_tar_keystore.bats$ ]]; then
+        assert_file "$ESCONFIG/elasticsearch.keystore" f elasticsearch elasticsearch 660
+    else
+        assert_file "$ESCONFIG/elasticsearch.keystore" f root elasticsearch 660
+    fi
+}
+
+

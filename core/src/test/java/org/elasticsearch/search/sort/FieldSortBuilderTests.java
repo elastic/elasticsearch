2140868bde13be691182163e30984d8a5d69a967
Christoph Büscher <christoph@elastic.co>
2017-08-30 11:47:02 +0200

    [Tests] Improve testing of FieldSortBuilder
    Currently we don't do much unit testing about the SortField that is created then
    calling the
    SortBuilders `build` method. Most of this is covered by integration tests
    somewhere but it
    would be good to have some basic checks in FieldSortBuilderTest as well.
    
    This adds testing for the sort order, mode, missing values and checks that
    `nested` gets set
    in the XFieldComparatorSource when `nestedPath` and `nestedFilter` are set on
    the builder.
    
    Relates to #17286

--- core/src/test/java/org/elasticsearch/search/sort/FieldSortBuilderTests.java
+++ core/src/test/java/org/elasticsearch/search/sort/FieldSortBuilderTests.java
@@ -20,15 +20,33 @@ x * Licensed to Elasticsearch under one or more contributor
 package org.elasticsearch.search.sort;
 
 import org.apache.lucene.search.SortField;
+import org.apache.lucene.search.SortedNumericSelector;
+import org.apache.lucene.search.SortedNumericSortField;
+import org.apache.lucene.search.SortedSetSelector;
+import org.apache.lucene.search.SortedSetSortField;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
+import org.elasticsearch.index.fielddata.IndexFieldData.XFieldComparatorSource;
+import org.elasticsearch.index.mapper.KeywordFieldMapper;
+import org.elasticsearch.index.mapper.MappedFieldType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.QueryShardContext;
+import org.elasticsearch.index.query.QueryShardException;
 import org.elasticsearch.search.DocValueFormat;
+import org.elasticsearch.search.MultiValueMode;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.Matchers.instanceOf;
+
 public class FieldSortBuilderTests extends AbstractSortTestCase<FieldSortBuilder> {
+
+    /**
+     * {@link #provideMappedFieldType(String)} will return a
+     */
+    private static String MAPPED_STRING_FIELDNAME = "_stringField";
 
     @Override
     protected FieldSortBuilder createTestItem() {
@@ -125,7 +143,136 @@ public class FieldSortBuilderTests extends AbstractSortTestCase<FieldSortBuilder
         assertEquals(DocValueFormat.RAW, format);
     }
 
-    public void testReverseOptionFails() throws IOException {
+    /**
+     * Test that missing values get transfered correctly to the SortField
+     */
+    public void testBuildSortFieldMissingValue() throws IOException {
+        QueryShardContext shardContextMock = createMockShardContext();
+        FieldSortBuilder fieldSortBuilder = new FieldSortBuilder("value").missing("_first");
+        SortField sortField = fieldSortBuilder.build(shardContextMock).field;
+        SortedNumericSortField expectedSortField = new SortedNumericSortField("value", SortField.Type.DOUBLE);
+        expectedSortField.setMissingValue(Double.NEGATIVE_INFINITY);
+        assertEquals(expectedSortField, sortField);
+
+        fieldSortBuilder = new FieldSortBuilder("value").missing("_last");
+        sortField = fieldSortBuilder.build(shardContextMock).field;
+        expectedSortField = new SortedNumericSortField("value", SortField.Type.DOUBLE);
+        expectedSortField.setMissingValue(Double.POSITIVE_INFINITY);
+        assertEquals(expectedSortField, sortField);
+
+        Double randomDouble = randomDouble();
+        fieldSortBuilder = new FieldSortBuilder("value").missing(randomDouble);
+        sortField = fieldSortBuilder.build(shardContextMock).field;
+        expectedSortField = new SortedNumericSortField("value", SortField.Type.DOUBLE);
+        expectedSortField.setMissingValue(randomDouble);
+        assertEquals(expectedSortField, sortField);
+
+        fieldSortBuilder = new FieldSortBuilder("value").missing(randomDouble.toString());
+        sortField = fieldSortBuilder.build(shardContextMock).field;
+        expectedSortField = new SortedNumericSortField("value", SortField.Type.DOUBLE);
+        expectedSortField.setMissingValue(randomDouble);
+        assertEquals(expectedSortField, sortField);
+    }
+
+    /**
+     * Test that the sort builder order gets transfered correctly to the SortField
+     */
+    public void testBuildSortFieldOrder() throws IOException {
+        QueryShardContext shardContextMock = createMockShardContext();
+        FieldSortBuilder fieldSortBuilder = new FieldSortBuilder("value");
+        SortField sortField = fieldSortBuilder.build(shardContextMock).field;
+        SortedNumericSortField expectedSortField = new SortedNumericSortField("value", SortField.Type.DOUBLE, false);
+        expectedSortField.setMissingValue(Double.POSITIVE_INFINITY);
+        assertEquals(expectedSortField, sortField);
+
+        fieldSortBuilder = new FieldSortBuilder("value").order(SortOrder.ASC);
+        sortField = fieldSortBuilder.build(shardContextMock).field;
+        expectedSortField = new SortedNumericSortField("value", SortField.Type.DOUBLE, false);
+        expectedSortField.setMissingValue(Double.POSITIVE_INFINITY);
+        assertEquals(expectedSortField, sortField);
+
+        fieldSortBuilder = new FieldSortBuilder("value").order(SortOrder.DESC);
+        sortField = fieldSortBuilder.build(shardContextMock).field;
+        expectedSortField = new SortedNumericSortField("value", SortField.Type.DOUBLE, true, SortedNumericSelector.Type.MAX);
+        expectedSortField.setMissingValue(Double.NEGATIVE_INFINITY);
+        assertEquals(expectedSortField, sortField);
+    }
+
+    /**
+     * Test that the sort builder mode gets transfered correctly to the SortField
+     */
+    public void testMultiValueMode() throws IOException {
+        QueryShardContext shardContextMock = createMockShardContext();
+
+        FieldSortBuilder sortBuilder = new FieldSortBuilder("value").sortMode(SortMode.MIN);
+        SortField sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField, instanceOf(SortedNumericSortField.class));
+        SortedNumericSortField numericSortField = (SortedNumericSortField) sortField;
+        assertEquals(SortedNumericSelector.Type.MIN, numericSortField.getSelector());
+
+        sortBuilder = new FieldSortBuilder("value").sortMode(SortMode.MAX);
+        sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField, instanceOf(SortedNumericSortField.class));
+        numericSortField = (SortedNumericSortField) sortField;
+        assertEquals(SortedNumericSelector.Type.MAX, numericSortField.getSelector());
+
+        sortBuilder = new FieldSortBuilder("value").sortMode(SortMode.SUM);
+        sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField.getComparatorSource(), instanceOf(XFieldComparatorSource.class));
+        XFieldComparatorSource comparatorSource = (XFieldComparatorSource) sortField.getComparatorSource();
+        assertEquals(MultiValueMode.SUM, comparatorSource.sortMode());
+
+        sortBuilder = new FieldSortBuilder("value").sortMode(SortMode.AVG);
+        sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField.getComparatorSource(), instanceOf(XFieldComparatorSource.class));
+        comparatorSource = (XFieldComparatorSource) sortField.getComparatorSource();
+        assertEquals(MultiValueMode.AVG, comparatorSource.sortMode());
+
+        sortBuilder = new FieldSortBuilder("value").sortMode(SortMode.MEDIAN);
+        sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField.getComparatorSource(), instanceOf(XFieldComparatorSource.class));
+        comparatorSource = (XFieldComparatorSource) sortField.getComparatorSource();
+        assertEquals(MultiValueMode.MEDIAN, comparatorSource.sortMode());
+
+        // sort mode should also be set by build() implicitely to MIN or MAX if not set explicitely on builder
+        sortBuilder = new FieldSortBuilder("value");
+        sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField, instanceOf(SortedNumericSortField.class));
+        numericSortField = (SortedNumericSortField) sortField;
+        assertEquals(SortedNumericSelector.Type.MIN, numericSortField.getSelector());
+
+        sortBuilder = new FieldSortBuilder("value").order(SortOrder.DESC);
+        sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField, instanceOf(SortedNumericSortField.class));
+        numericSortField = (SortedNumericSortField) sortField;
+        assertEquals(SortedNumericSelector.Type.MAX, numericSortField.getSelector());
+    }
+
+    /**
+     * Test that the sort builder nested object gets created in the SortField
+     */
+    public void testBuildNested() throws IOException {
+        QueryShardContext shardContextMock = createMockShardContext();
+
+        FieldSortBuilder sortBuilder = new FieldSortBuilder("value").setNestedPath("path");
+        SortField sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField.getComparatorSource(), instanceOf(XFieldComparatorSource.class));
+        XFieldComparatorSource comparatorSource = (XFieldComparatorSource) sortField.getComparatorSource();
+        assertNotNull(comparatorSource.nested());
+
+        sortBuilder = new FieldSortBuilder("value").setNestedPath("path").setNestedFilter(QueryBuilders.termQuery("field", 10.0));
+        sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField.getComparatorSource(), instanceOf(XFieldComparatorSource.class));
+        comparatorSource = (XFieldComparatorSource) sortField.getComparatorSource();
+        assertNotNull(comparatorSource.nested());
+
+        // if nested path is missing, we omit any filter and return a SortedNumericSortField
+        sortBuilder = new FieldSortBuilder("value").setNestedFilter(QueryBuilders.termQuery("field", 10.0));
+        sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField, instanceOf(SortedNumericSortField.class));
+    }
+
+    public void testUnknownOptionFails() throws IOException {
         String json = "{ \"post_date\" : {\"reverse\" : true} },\n";
 
         XContentParser parser = createParser(JsonXContent.jsonXContent, json);
@@ -134,14 +281,51 @@ public class FieldSortBuilderTests extends AbstractSortTestCase<FieldSortBuilder
         parser.nextToken();
         parser.nextToken();
 
-        try {
-          FieldSortBuilder.fromXContent(parser, "");
-          fail("adding reverse sorting option should fail with an exception");
-        } catch (IllegalArgumentException e) {
-            // all good
+        IllegalArgumentException e = expectThrows(IllegalArgumentException.class, () -> FieldSortBuilder.fromXContent(parser, ""));
+        assertEquals("[field_sort] unknown field [reverse], parser not found", e.getMessage());
+    }
+
+    @Override
+    protected MappedFieldType provideMappedFieldType(String name) {
+        if (name.equals(MAPPED_STRING_FIELDNAME)) {
+            KeywordFieldMapper.KeywordFieldType fieldType = new KeywordFieldMapper.KeywordFieldType();
+            fieldType.setName(name);
+            fieldType.setHasDocValues(true);
+            return fieldType;
+        } else {
+            return super.provideMappedFieldType(name);
         }
     }
 
+    /**
+     * Test that MIN, MAX mode work on non-numeric fields, but other modes throw exception
+     */
+    public void testModeNonNumericField() throws IOException {
+        QueryShardContext shardContextMock = createMockShardContext();
+
+        FieldSortBuilder sortBuilder = new FieldSortBuilder(MAPPED_STRING_FIELDNAME).sortMode(SortMode.MIN);
+        SortField sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField, instanceOf(SortedSetSortField.class));
+        assertEquals(SortedSetSelector.Type.MIN, ((SortedSetSortField) sortField).getSelector());
+
+        sortBuilder = new FieldSortBuilder(MAPPED_STRING_FIELDNAME).sortMode(SortMode.MAX);
+        sortField = sortBuilder.build(shardContextMock).field;
+        assertThat(sortField, instanceOf(SortedSetSortField.class));
+        assertEquals(SortedSetSelector.Type.MAX, ((SortedSetSortField) sortField).getSelector());
+
+        String expectedError = "we only support AVG, MEDIAN and SUM on number based fields";
+        QueryShardException e = expectThrows(QueryShardException.class,
+                () -> new FieldSortBuilder(MAPPED_STRING_FIELDNAME).sortMode(SortMode.AVG).build(shardContextMock));
+        assertEquals(expectedError, e.getMessage());
+
+        e = expectThrows(QueryShardException.class,
+                () -> new FieldSortBuilder(MAPPED_STRING_FIELDNAME).sortMode(SortMode.SUM).build(shardContextMock));
+        assertEquals(expectedError, e.getMessage());
+
+        e = expectThrows(QueryShardException.class,
+                () -> new FieldSortBuilder(MAPPED_STRING_FIELDNAME).sortMode(SortMode.MEDIAN).build(shardContextMock));
+        assertEquals(expectedError, e.getMessage());
+    }
 
     @Override
     protected FieldSortBuilder fromXContent(XContentParser parser, String fieldName) throws IOException {

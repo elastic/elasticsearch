15b262494fa1d711da4194a0d6ad871f8887f626
Jason Tedor <jason@tedor.me>
2017-08-31 08:31:00 -0400

    Clarify comment

--- core/src/main/java/org/elasticsearch/common/logging/ESLoggerFactory.java
+++ core/src/main/java/org/elasticsearch/common/logging/ESLoggerFactory.java
@@ -47,7 +47,7 @@ public final class ESLoggerFactory {
 
     public static Logger getLogger(String prefix, Class<?> clazz) {
         /*
-         * Do not use LogManager#getLogger as this now uses Class#getCanonicalName under the hood; as this returns null for local and
+         * Do not use LogManager#getLogger(Class) as this now uses Class#getCanonicalName under the hood; as this returns null for local and
          * anonymous classes, any place we create, for example, an abstract component defined as an anonymous class (e.g., in tests) will
          * result in a logger with a null name which will blow up in a lookup inside of Log4j.
          */

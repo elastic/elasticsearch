a245cf53c64d80525de07305a9b946e063d28682
Adrien Grand <jpountz@gmail.com>
2017-09-07 10:33:18 +0200

    Upgrade to Lucene-6.6.1.
    Here is the 6.6.1 changelog:
    
    Bug Fixes
    
    * LUCENE-7869: Changed MemoryIndex to sort 1d points. In case of 1d points, the PointInSetQuery.MergePointVisitor expects
      that these points are visited in ascending order. The memory index doesn't do this and this can result in document
      with multiple points that should match to not match. (Martijn van Groningen)
    
    * LUCENE-7878: Fix query builder to keep the SHOULD clause that wraps multi-word synonyms. (Jim Ferenczi)

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -141,7 +141,7 @@ public class Version implements Comparable<Version> {
     public static final int V_5_5_3_ID_UNRELEASED = 5050399;
     public static final Version V_5_5_3_UNRELEASED = new Version(V_5_5_3_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_6_0);
     public static final int V_5_6_0_ID_UNRELEASED = 5060099;
-    public static final Version V_5_6_0_UNRELEASED = new Version(V_5_6_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_6_0);
+    public static final Version V_5_6_0_UNRELEASED = new Version(V_5_6_0_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_6_1);
     public static final Version CURRENT = V_5_6_0_UNRELEASED;
 
     // unreleased versions must be added to the above list with the suffix _UNRELEASED (with the exception of CURRENT)

29e6758d9ba0f0ec158e544fbcf4856937b0503a
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2017-09-11 13:36:56 +0100

    Creates a new X-Pack feature for index lifecycle
    Feature consists of a shell of a persistant task which will later be used to inspect the index settings and apply curator like changes to the index (move from hot to warm, rollover, shrink etc.)

---
+++ x-pack/plugin/src/main/java/org/elasticsearch/xpack/indexlifecycle/IndexLifecycleFeatureSet.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+package org.elasticsearch.xpack.indexlifecycle;
+
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.common.Nullable;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.license.XPackLicenseState;
+import org.elasticsearch.xpack.XPackFeatureSet;
+import org.elasticsearch.xpack.XPackPlugin;
+import org.elasticsearch.xpack.XPackSettings;
+
+import java.util.Map;
+
+public class IndexLifecycleFeatureSet implements XPackFeatureSet {
+
+    private final boolean enabled;
+    private final XPackLicenseState licenseState;
+
+    @Inject
+    public IndexLifecycleFeatureSet(Settings settings, @Nullable XPackLicenseState licenseState) {
+        this.enabled = XPackSettings.INDEX_LIFECYCLE_ENABLED.get(settings);
+        this.licenseState = licenseState;
+    }
+
+    @Override
+    public String name() {
+        return XPackPlugin.INDEX_LIFECYCLE;
+    }
+
+    @Override
+    public String description() {
+        return "Index lifecycle management for the Elastic Stack";
+    }
+
+    @Override
+    public boolean available() {
+        return licenseState != null && licenseState.isIndexLifecycleAllowed();
+    }
+
+    @Override
+    public boolean enabled() {
+        return enabled;
+    }
+
+    @Override
+    public Map<String, Object> nativeCodeInfo() {
+        return null;
+    }
+
+    @Override
+    public void usage(ActionListener<XPackFeatureSet.Usage> listener) {
+        listener.onResponse(new Usage(name(), available(), enabled()) {
+        });
+    }
+
+}

b0678310b64c51cc30a4f936fd5429d587503450
Jason Tedor <jason@tedor.me>
2017-09-11 07:42:11 -0400

    Snapshot fallback should consider build.snapshot
    When determining if a build is a snapshot build, we look for a field in
    the JAR manifest. However, when running tests, we are not running with a
    compiled core Elasticsearch JAR, we are running with the compiled core
    classes on the classpath. We have a fallback for this, we always assume
    such a situation is a snapshot build. However, when running builds with
    -Dbuild.snapshot=false, this is not the case. As such, we need to
    fallback to the value of build.snapshot. However, there are cases where
    we are not running with a compiled core Elasticsearch JAR (e.g., when
    the transport client is embedded in a web container) so we should only
    do this fallback if we are in tests. To verify we are in tests, we check
    if randomized runner is on the classpath.
    
    Relates #26554

--- test/framework/src/main/java/org/elasticsearch/test/junit/listeners/ReproduceInfoPrinter.java
+++ test/framework/src/main/java/org/elasticsearch/test/junit/listeners/ReproduceInfoPrinter.java
@@ -142,7 +142,7 @@ public class ReproduceInfoPrinter extends RunListener {
                 appendProperties(TESTS_CLUSTER, ESIntegTestCase.TESTS_ENABLE_MOCK_MODULES);
             }
             appendProperties("tests.assertion.disabled", "tests.security.manager", "tests.nightly", "tests.jvms",
-                             "tests.client.ratio", "tests.heap.size", "tests.bwc", "tests.bwc.version");
+                             "tests.client.ratio", "tests.heap.size", "tests.bwc", "tests.bwc.version", "build.snapshot");
             if (System.getProperty("tests.jvm.argline") != null && !System.getProperty("tests.jvm.argline").isEmpty()) {
                 appendOpt("tests.jvm.argline", "\"" + System.getProperty("tests.jvm.argline") + "\"");
             }

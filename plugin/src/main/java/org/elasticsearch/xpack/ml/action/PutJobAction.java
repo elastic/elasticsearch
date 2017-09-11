4077323f87f37c077183a41d70c460a34f821eb3
David Kyle <david.kyle@elastic.co>
2017-09-11 14:53:46 +0100

    [ML] Add setting for job max model memory limit (elastic/x-pack-elasticsearch#2460)
    * Add setting for job max model memory limit
    
    * Address review comments
    
    Original commit: elastic/x-pack-elasticsearch@01b60ade95c1fa0577219a24a7958318db27f2d7

--- plugin/src/main/java/org/elasticsearch/xpack/ml/action/PutJobAction.java
+++ plugin/src/main/java/org/elasticsearch/xpack/ml/action/PutJobAction.java
@@ -72,13 +72,6 @@ public class PutJobAction extends Action<PutJobAction.Request, PutJobAction.Resp
                         jobBuilder.getId(), jobId));
             }
 
-            // Some fields cannot be set at create time
-            List<String> invalidJobCreationSettings = jobBuilder.invalidCreateTimeSettings();
-            if (invalidJobCreationSettings.isEmpty() == false) {
-                throw new IllegalArgumentException(Messages.getMessage(Messages.JOB_CONFIG_INVALID_CREATE_SETTINGS,
-                        String.join(",", invalidJobCreationSettings)));
-            }
-
             return new Request(jobBuilder);
         }
 
@@ -88,11 +81,12 @@ public class PutJobAction extends Action<PutJobAction.Request, PutJobAction.Resp
             // Validate the jobBuilder immediately so that errors can be detected prior to transportation.
             jobBuilder.validateInputFields();
 
-            // In 6.1 we want to make the model memory size limit more prominent, and also reduce the default from
-            // 4GB to 1GB.  However, changing the meaning of a null model memory limit for existing jobs would be a
-            // breaking change, so instead we add an explicit limit to newly created jobs that didn't have one when
-            // submitted
-            jobBuilder.setDefaultMemoryLimitIfUnset();
+            // Some fields cannot be set at create time
+            List<String> invalidJobCreationSettings = jobBuilder.invalidCreateTimeSettings();
+            if (invalidJobCreationSettings.isEmpty() == false) {
+                throw new IllegalArgumentException(Messages.getMessage(Messages.JOB_CONFIG_INVALID_CREATE_SETTINGS,
+                        String.join(",", invalidJobCreationSettings)));
+            }
 
             this.jobBuilder = jobBuilder;
         }

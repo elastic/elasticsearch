b8a51832adac9c4e95677a8ca837459935c35a0e
Jason Tedor <jason@tedor.me>
2017-09-15 13:24:48 -0400

    Cache global checkpoint lookup

--- core/src/main/java/org/elasticsearch/index/seqno/GlobalCheckpointTracker.java
+++ core/src/main/java/org/elasticsearch/index/seqno/GlobalCheckpointTracker.java
@@ -358,8 +358,9 @@ public class GlobalCheckpointTracker extends AbstractIndexShardComponent {
          * information is lagging compared to a replica (e.g., if a replica is promoted to primary but has stale info relative to other
          * replica shards). In these cases, the local knowledge of the global checkpoint could be higher than sync from the lagging primary.
          */
-        if (getGlobalCheckpoint() <= globalCheckpoint) {
-            logger.trace("updating global checkpoint from [{}] to [{}] due to [{}]", getGlobalCheckpoint(), globalCheckpoint, reason);
+        final long currentGlobalCheckpoint = getGlobalCheckpoint();
+        if (currentGlobalCheckpoint <= globalCheckpoint) {
+            logger.trace("updating global checkpoint from [{}] to [{}] due to [{}]", currentGlobalCheckpoint, globalCheckpoint, reason);
             final long unassignedSeqNo = SequenceNumbers.UNASSIGNED_SEQ_NO;
             final CheckpointState cps =
                     checkpoints.computeIfAbsent(allocationId, k -> new CheckpointState(unassignedSeqNo, unassignedSeqNo, true));

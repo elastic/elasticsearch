f0d3479eac9022756a4ef62bfb5654d362d2cc84
Simon Willnauer <simonw@apache.org>
2017-09-18 16:19:52 +0200

    fix imports

--- core/src/test/java/org/elasticsearch/common/io/stream/StreamTests.java
+++ core/src/test/java/org/elasticsearch/common/io/stream/StreamTests.java
@@ -19,7 +19,6 @@
 
 package org.elasticsearch.common.io.stream;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;

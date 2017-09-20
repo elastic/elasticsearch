27d3fc39a620729920f35d4f6f2ce80000e046ed
Simon Willnauer <simonw@apache.org>
2017-09-20 17:51:16 +0200

    fix broken ec2 setting

--- core/src/main/java/org/elasticsearch/common/settings/Settings.java
+++ core/src/main/java/org/elasticsearch/common/settings/Settings.java
@@ -71,7 +71,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 

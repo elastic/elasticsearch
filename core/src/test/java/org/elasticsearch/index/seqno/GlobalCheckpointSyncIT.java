cf4e67b9ddb9654239add2330c0f595dacf87749
Jason Tedor <jason@tedor.me>
2017-09-21 07:18:50 -0400

    Test iteration

--- core/src/test/java/org/elasticsearch/index/seqno/GlobalCheckpointSyncIT.java
+++ core/src/test/java/org/elasticsearch/index/seqno/GlobalCheckpointSyncIT.java
@@ -17,13 +17,15 @@
  * under the License.
  */
 
-package org.elasticsearch.index;
+package org.elasticsearch.index.seqno;
 
 import org.elasticsearch.action.admin.indices.stats.IndexShardStats;
 import org.elasticsearch.action.admin.indices.stats.IndexStats;
 import org.elasticsearch.action.admin.indices.stats.IndicesStatsResponse;
 import org.elasticsearch.action.admin.indices.stats.ShardStats;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.IndexService;
 import org.elasticsearch.index.seqno.SeqNoStats;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.test.ESIntegTestCase;
@@ -32,15 +34,27 @@ import org.elasticsearch.test.InternalSettingsPlugin;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static org.hamcrest.Matchers.equalTo;
 
-public class IndexServiceIT extends ESIntegTestCase {
+public class GlobalCheckpointSyncIT extends ESIntegTestCase {
 
-    public void testGlobalCheckpointSync() throws Exception {
+    @Override
+    protected Collection<Class<? extends Plugin>> nodePlugins() {
+        return Stream.concat(super.nodePlugins().stream(), Stream.of(InternalSettingsPlugin.class)).collect(Collectors.toList());
+    }
+
+    public void testPostOperationGlobalCheckpointSync() throws Exception {
         internalCluster().startNode();
-        final int numberOfDocuments = randomIntBetween(1, 128);
+        // set the sync interval high so it does not execute during this test
+        prepareCreate("test", Settings.builder().put(IndexService.GLOBAL_CHECKPOINT_SYNC_INTERVAL_SETTING.getKey(), "24h")).get();
+        if (randomBoolean()) {
+            ensureGreen();
+        }
+        final int numberOfDocuments = randomIntBetween(0, 128);
         for (int i = 0; i < numberOfDocuments; i++) {
             final String id = Integer.toString(i);
             client().prepareIndex("test", "test", id).setSource("{\"foo\": " + id + "}", XContentType.JSON).get();
@@ -60,6 +74,10 @@ public class IndexServiceIT extends ESIntegTestCase {
                 final SeqNoStats primarySeqNoStats = primary.getSeqNoStats();
                 for (final ShardStats shardStats : indexShardStats) {
                     final SeqNoStats seqNoStats = shardStats.getSeqNoStats();
+                    if (seqNoStats == null) {
+                        // the shard is initializing
+                        continue;
+                    }
                     assertThat(seqNoStats.getGlobalCheckpoint(), equalTo(primarySeqNoStats.getGlobalCheckpoint()));
                 }
             }

c84c48fa0123897a54396f9a2295a10d49d40be2
Albert Zaharovits <albert.zaharovits@gmail.com>
2017-09-21 11:26:28 +0300

    Usability improvement for the password bootstrap tool (reserved users) (elastic/x-pack-elasticsearch#2444)
    Generate or prompt for passwords of the reserved users, validating strength
    and typos, then issue change requests, keeping elastic user change request last.
    
    Closes: elastic/x-pack-elasticsearch#2424
    
    Original commit: elastic/x-pack-elasticsearch@1f827d393c5e68538e029b7db4033439467cf1e8

--- plugin/src/test/java/org/elasticsearch/xpack/security/authc/esnative/tool/SetupPasswordToolTests.java
+++ plugin/src/test/java/org/elasticsearch/xpack/security/authc/esnative/tool/SetupPasswordToolTests.java
@@ -10,6 +10,8 @@ import org.elasticsearch.cli.CommandTestCase;
 import org.elasticsearch.cli.ExitCodes;
 import org.elasticsearch.cli.Terminal;
 import org.elasticsearch.cli.UserException;
+import org.elasticsearch.common.CheckedConsumer;
+import org.elasticsearch.common.CheckedSupplier;
 import org.elasticsearch.common.settings.KeyStoreWrapper;
 import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
@@ -18,47 +20,48 @@ import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.xpack.security.authc.esnative.ReservedRealm;
+import org.elasticsearch.xpack.security.support.Validation;
 import org.elasticsearch.xpack.security.user.ElasticUser;
-import org.elasticsearch.xpack.security.user.KibanaUser;
-import org.elasticsearch.xpack.security.user.LogstashSystemUser;
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
 
 import java.io.IOException;
-import java.security.GeneralSecurityException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.contains;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 public class SetupPasswordToolTests extends CommandTestCase {
 
     private final String pathHomeParameter = "-Epath.home=" + createTempDir();
     private SecureString bootstrapPassword;
-    private final String ep = "elastic-password";
-    private final String kp = "kibana-password";
-    private final String lp = "logstash-password";
     private CommandLineHttpClient httpClient;
     private KeyStoreWrapper keyStore;
+    private List<String> usersInSetOrder;
 
     @Before
-    public void setSecretsAndKeyStore() throws GeneralSecurityException {
+    public void setSecretsAndKeyStore() throws Exception {
         // sometimes we fall back to the keystore seed as this is the default when a new node starts
         boolean useFallback = randomBoolean();
         bootstrapPassword = useFallback ?  new SecureString("0xCAFEBABE".toCharArray()) :
                 new SecureString("bootstrap-password".toCharArray());
         this.keyStore = mock(KeyStoreWrapper.class);
         this.httpClient = mock(CommandLineHttpClient.class);
+
         when(keyStore.isLoaded()).thenReturn(true);
         if (useFallback) {
             when(keyStore.getSettingNames()).thenReturn(new HashSet<>(Arrays.asList(ReservedRealm.BOOTSTRAP_ELASTIC_PASSWORD.getKey(),
@@ -68,14 +71,24 @@ public class SetupPasswordToolTests extends CommandTestCase {
             when(keyStore.getSettingNames()).thenReturn(Collections.singleton(KeyStoreWrapper.SEED_SETTING.getKey()));
             when(keyStore.getString(KeyStoreWrapper.SEED_SETTING.getKey())).thenReturn(bootstrapPassword);
         }
+
         when(httpClient.getDefaultURL()).thenReturn("http://localhost:9200");
 
-        terminal.addSecretInput(ep);
-        terminal.addSecretInput(ep);
-        terminal.addSecretInput(kp);
-        terminal.addSecretInput(kp);
-        terminal.addSecretInput(lp);
-        terminal.addSecretInput(lp);
+        when(httpClient.postURL(anyString(), any(URL.class), anyString(), any(SecureString.class), any(CheckedSupplier.class),
+                any(CheckedConsumer.class))).thenReturn(HttpURLConnection.HTTP_OK);
+
+        // elastic user is updated last
+        usersInSetOrder = new ArrayList<>(SetupPasswordTool.USERS);
+        for (int i = 0; i < usersInSetOrder.size() - 1; i++) {
+            if (ElasticUser.NAME.equals(usersInSetOrder.get(i))) {
+                Collections.swap(usersInSetOrder, i, i + 1);
+            }
+        }
+
+        for (String user : SetupPasswordTool.USERS) {
+            terminal.addSecretInput(user + "-password");
+            terminal.addSecretInput(user + "-password");
+        }
     }
 
     @Override
@@ -106,78 +119,110 @@ public class SetupPasswordToolTests extends CommandTestCase {
     }
 
     public void testAutoSetup() throws Exception {
+        String url = httpClient.getDefaultURL();
         execute("auto", pathHomeParameter, "-b", "true");
 
         verify(keyStore).decrypt(new char[0]);
 
-        ArgumentCaptor<String> passwordCaptor = ArgumentCaptor.forClass(String.class);
-
         InOrder inOrder = Mockito.inOrder(httpClient);
-        String elasticUrl = "http://localhost:9200/_xpack/security/user/elastic/_password";
-        inOrder.verify(httpClient).postURL(eq("PUT"), eq(elasticUrl), eq(ElasticUser.NAME), eq(bootstrapPassword),
-                passwordCaptor.capture());
 
-        String[] users = {KibanaUser.NAME, LogstashSystemUser.NAME};
-        SecureString newPassword = new SecureString(parsePassword(passwordCaptor.getValue()).toCharArray());
-        for (String user : users) {
-            String urlWithRoute = "http://localhost:9200/_xpack/security/user/" + user + "/_password";
-            inOrder.verify(httpClient).postURL(eq("PUT"), eq(urlWithRoute), eq(ElasticUser.NAME), eq(newPassword), anyString());
+        URL checkUrl = new URL(url + "/_xpack/security/_authenticate?pretty");
+        inOrder.verify(httpClient).postURL(eq("GET"), eq(checkUrl), eq(ElasticUser.NAME), eq(bootstrapPassword), any(CheckedSupplier.class),
+                any(CheckedConsumer.class));
+        for (String user : usersInSetOrder) {
+            URL urlWithRoute = new URL(url + "/_xpack/security/user/" + user + "/_password");
+            inOrder.verify(httpClient).postURL(eq("PUT"), eq(urlWithRoute), eq(ElasticUser.NAME), eq(bootstrapPassword),
+                    any(CheckedSupplier.class), any(CheckedConsumer.class));
         }
+
+    }
+
+    public void testAuthnFail() throws Exception {
+        URL authnURL = new URL(httpClient.getDefaultURL() + "/_xpack/security/_authenticate?pretty");
+        when(httpClient.postURL(eq("GET"), eq(authnURL), eq(ElasticUser.NAME), any(SecureString.class), any(CheckedSupplier.class),
+                any(CheckedConsumer.class))).thenReturn(HttpURLConnection.HTTP_UNAUTHORIZED);
+
+        try {
+            execute(randomBoolean() ? "auto" : "interactive", pathHomeParameter);
+            fail("Should have thrown exception");
+        } catch (UserException e) {
+            assertEquals(ExitCodes.CONFIG, e.exitCode);
+        }
+
     }
 
     public void testUrlOption() throws Exception {
         String url = "http://localhost:9202";
         execute("auto", pathHomeParameter, "-u", url, "-b");
 
-        ArgumentCaptor<String> passwordCaptor = ArgumentCaptor.forClass(String.class);
-
         InOrder inOrder = Mockito.inOrder(httpClient);
-        String elasticUrl = url + "/_xpack/security/user/elastic/_password";
-        inOrder.verify(httpClient).postURL(eq("PUT"), eq(elasticUrl), eq(ElasticUser.NAME), eq(bootstrapPassword),
-                passwordCaptor.capture());
 
-        String[] users = {KibanaUser.NAME, LogstashSystemUser.NAME};
-        SecureString newPassword = new SecureString(parsePassword(passwordCaptor.getValue()).toCharArray());
-        for (String user : users) {
-            String urlWithRoute = url + "/_xpack/security/user/" + user + "/_password";
-            inOrder.verify(httpClient).postURL(eq("PUT"), eq(urlWithRoute), eq(ElasticUser.NAME), eq(newPassword), anyString());
+        URL checkUrl = new URL(url + "/_xpack/security/_authenticate?pretty");
+        inOrder.verify(httpClient).postURL(eq("GET"), eq(checkUrl), eq(ElasticUser.NAME), eq(bootstrapPassword), any(CheckedSupplier.class),
+                any(CheckedConsumer.class));
+        for (String user : usersInSetOrder) {
+            URL urlWithRoute = new URL(url + "/_xpack/security/user/" + user + "/_password");
+            inOrder.verify(httpClient).postURL(eq("PUT"), eq(urlWithRoute), eq(ElasticUser.NAME), eq(bootstrapPassword),
+                    any(CheckedSupplier.class), any(CheckedConsumer.class));
         }
     }
 
     public void testInteractiveSetup() throws Exception {
+        String url = httpClient.getDefaultURL();
+
         terminal.addTextInput("Y");
+        execute("interactive", pathHomeParameter);
+
+        InOrder inOrder = Mockito.inOrder(httpClient);
+
+        URL checkUrl = new URL(url + "/_xpack/security/_authenticate?pretty");
+        inOrder.verify(httpClient).postURL(eq("GET"), eq(checkUrl), eq(ElasticUser.NAME), eq(bootstrapPassword), any(CheckedSupplier.class),
+                any(CheckedConsumer.class));
+        for (String user : usersInSetOrder) {
+            URL urlWithRoute = new URL(url + "/_xpack/security/user/" + user + "/_password");
+            ArgumentCaptor<CheckedSupplier<String, Exception>> passwordCaptor = ArgumentCaptor.forClass((Class) CheckedSupplier.class);
+            inOrder.verify(httpClient).postURL(eq("PUT"), eq(urlWithRoute), eq(ElasticUser.NAME), eq(bootstrapPassword),
+                    passwordCaptor.capture(), any(CheckedConsumer.class));
+            assertThat(passwordCaptor.getValue().get(), CoreMatchers.containsString(user + "-password"));
+        }
+    }
+
+    public void testInteractivePasswordsFatFingers() throws Exception {
+        String url = httpClient.getDefaultURL();
+
+        terminal.reset();
+        terminal.addTextInput("Y");
+        for (String user : SetupPasswordTool.USERS) {
+            // fail in strength and match
+            int failCount = randomIntBetween(3, 10);
+            while (failCount-- > 0) {
+                String password1 = randomAlphaOfLength(randomIntBetween(3, 10));
+                terminal.addSecretInput(password1);
+                Validation.Error err = Validation.Users.validatePassword(password1.toCharArray());
+                if (err == null) {
+                    // passes strength validation, fail by mismatch
+                    terminal.addSecretInput(password1 + "typo");
+                }
+            }
+            // two good passwords
+            terminal.addSecretInput(user + "-password");
+            terminal.addSecretInput(user + "-password");
+        }
 
         execute("interactive", pathHomeParameter);
 
         InOrder inOrder = Mockito.inOrder(httpClient);
-        String elasticUrl = "http://localhost:9200/_xpack/security/user/elastic/_password";
-        SecureString newPassword = new SecureString(ep.toCharArray());
-        inOrder.verify(httpClient).postURL(eq("PUT"), eq(elasticUrl), eq(ElasticUser.NAME), eq(bootstrapPassword), contains(ep));
 
-        String kibanaUrl = "http://localhost:9200/_xpack/security/user/" + KibanaUser.NAME + "/_password";
-        inOrder.verify(httpClient).postURL(eq("PUT"), eq(kibanaUrl), eq(ElasticUser.NAME), eq(newPassword), contains(kp));
-        String logstashUrl = "http://localhost:9200/_xpack/security/user/" + LogstashSystemUser.NAME + "/_password";
-        inOrder.verify(httpClient).postURL(eq("PUT"), eq(logstashUrl), eq(ElasticUser.NAME), eq(newPassword), contains(lp));
-    }
-
-    public void testInteractivePasswordsNotMatching() throws Exception {
-        String ep = "elastic-password";
-
-        terminal.reset();
-        terminal.addTextInput("Y");
-        terminal.addSecretInput(ep);
-        terminal.addSecretInput(ep + "typo");
-        String url = "http://localhost:9200";
-
-        try {
-            execute("interactive", pathHomeParameter, "-u", url);
-            fail("Should have thrown exception");
-        } catch (UserException e) {
-            assertEquals(ExitCodes.USAGE, e.exitCode);
-            assertEquals("Passwords for user [elastic] do not match", e.getMessage());
+        URL checkUrl = new URL(url + "/_xpack/security/_authenticate?pretty");
+        inOrder.verify(httpClient).postURL(eq("GET"), eq(checkUrl), eq(ElasticUser.NAME), eq(bootstrapPassword), any(CheckedSupplier.class),
+                any(CheckedConsumer.class));
+        for (String user : usersInSetOrder) {
+            URL urlWithRoute = new URL(url + "/_xpack/security/user/" + user + "/_password");
+            ArgumentCaptor<CheckedSupplier<String, Exception>> passwordCaptor = ArgumentCaptor.forClass((Class) CheckedSupplier.class);
+            inOrder.verify(httpClient).postURL(eq("PUT"), eq(urlWithRoute), eq(ElasticUser.NAME), eq(bootstrapPassword),
+                    passwordCaptor.capture(), any(CheckedConsumer.class));
+            assertThat(passwordCaptor.getValue().get(), CoreMatchers.containsString(user + "-password"));
         }
-
-        verifyZeroInteractions(httpClient);
     }
 
     private String parsePassword(String value) throws IOException {

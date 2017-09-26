5ad170a7ed6ed26d8baadec06773a78df84dea5c
Dimitrios Athanasiou <dimitris@elastic.co>
2017-09-26 12:08:15 +0100

    Fix compilation due to upstream change
    Original commit: elastic/x-pack-elasticsearch@57d834841d01b869bd3f20766db1f2742003cd20

--- plugin/src/test/java/org/elasticsearch/xpack/security/authc/esnative/ESNativeRealmMigrateToolTests.java
+++ plugin/src/test/java/org/elasticsearch/xpack/security/authc/esnative/ESNativeRealmMigrateToolTests.java
@@ -46,7 +46,9 @@ public class ESNativeRealmMigrateToolTests extends CommandTestCase {
 
                     @Override
                     protected Environment createEnv(Terminal terminal, Map<String, String> settings) throws UserException {
-                        return new Environment(Settings.builder().put(settings).build());
+                        Settings.Builder builder = Settings.builder();
+                        settings.forEach((k,v) -> builder.put(k, v));
+                        return new Environment(builder.build());
                     }
 
                 };

25a6eb577d9175ab20352c683e71e06d23466725
James Baiera <james.baiera@gmail.com>
2017-09-26 14:27:01 -0400

    Adding version id

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -144,9 +144,11 @@ public class Version implements Comparable<Version> {
     public static final Version V_5_6_0 = new Version(V_5_6_0_ID, org.apache.lucene.util.Version.LUCENE_6_6_0);
     public static final int V_5_6_1_ID = 5060199;
     public static final Version V_5_6_1 = new Version(V_5_6_1_ID, org.apache.lucene.util.Version.LUCENE_6_6_1);
-    public static final int V_5_6_2_ID_UNRELEASED = 5060299;
-    public static final Version V_5_6_2_UNRELEASED = new Version(V_5_6_2_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_6_1);
-    public static final Version CURRENT = V_5_6_2_UNRELEASED;
+    public static final int V_5_6_2_ID = 5060299;
+    public static final Version V_5_6_2 = new Version(V_5_6_2_ID, org.apache.lucene.util.Version.LUCENE_6_6_1);
+    public static final int V_5_6_3_ID_UNRELEASED = 5060399;
+    public static final Version V_5_6_3_UNRELEASED = new Version(V_5_6_3_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_6_1);
+    public static final Version CURRENT = V_5_6_3_UNRELEASED;
 
     // unreleased versions must be added to the above list with the suffix _UNRELEASED (with the exception of CURRENT)
 
@@ -161,8 +163,10 @@ public class Version implements Comparable<Version> {
 
     public static Version fromId(int id) {
         switch (id) {
-            case V_5_6_2_ID_UNRELEASED:
-                return V_5_6_2_UNRELEASED;
+            case V_5_6_3_ID_UNRELEASED:
+                return V_5_6_3_UNRELEASED;
+            case V_5_6_2_ID:
+                return V_5_6_2;
             case V_5_6_1_ID:
                 return V_5_6_1;
             case V_5_6_0_ID:

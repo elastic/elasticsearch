720e2f77c974ad79aca25a1f0e7d24169312891b
Tim Brooks <tim@uncontended.net>
2017-10-02 13:19:37 -0600

    Check channel i open before setting SO_LINGER
    This commit fixes a #26855. Right now we set SO_LINGER to 0 if we are
    stopping the transport. This can throw a ChannelClosedException if the
    raw channel is already closed. We have a number of scenarios where it is
    possible this could be called with a channel that is already closed.
    This commit fixes the issue be checking that the channel is not closed
    before attempting to set the socket option.

--- modules/transport-netty4/src/main/java/org/elasticsearch/transport/netty4/Netty4Transport.java
+++ modules/transport-netty4/src/main/java/org/elasticsearch/transport/netty4/Netty4Transport.java
@@ -338,7 +338,9 @@ public class Netty4Transport extends TcpTransport<Channel> {
                  * side otherwise the client (node) initiates the TCP closing sequence which doesn't cause these issues. Setting this
                  * by default from the beginning can have unexpected side-effects an should be avoided, our protocol is designed
                  * in a way that clients close connection which is how it should be*/
-                channel.config().setOption(ChannelOption.SO_LINGER, 0);
+                if (channel.isOpen()) {
+                    channel.config().setOption(ChannelOption.SO_LINGER, 0);
+                }
             }
         }
         if (blocking) {

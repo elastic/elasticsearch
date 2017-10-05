2b476bcc12134c11cd3813895037e36cade42c47
Martijn van Groningen <martijn.v.groningen@gmail.com>
2017-10-05 14:16:10 +0200

    fixed compile error

--- core/src/main/java/org/elasticsearch/index/analysis/BengaliAnalyzerProvider.java
+++ core/src/main/java/org/elasticsearch/index/analysis/BengaliAnalyzerProvider.java
@@ -32,7 +32,7 @@ public class BengaliAnalyzerProvider extends AbstractIndexAnalyzerProvider<Benga
     public BengaliAnalyzerProvider(IndexSettings indexSettings, Environment env, String name, Settings settings) {
         super(indexSettings, name, settings);
         analyzer = new BengaliAnalyzer(
-            Analysis.parseStopWords(env, settings, BengaliAnalyzer.getDefaultStopSet()),
+            Analysis.parseStopWords(env, indexSettings.getIndexVersionCreated(), settings, BengaliAnalyzer.getDefaultStopSet()),
             Analysis.parseStemExclusion(settings, CharArraySet.EMPTY_SET)
         );
         analyzer.setVersion(version);

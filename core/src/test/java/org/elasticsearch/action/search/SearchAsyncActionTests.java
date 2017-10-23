a30a234d44533199609d5633e67a99f2616e0987
Jason Tedor <jason@tedor.me>
2017-10-23 17:06:56 -0400

    Remove import

--- core/src/test/java/org/elasticsearch/action/search/SearchAsyncActionTests.java
+++ core/src/test/java/org/elasticsearch/action/search/SearchAsyncActionTests.java
@@ -50,7 +50,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;

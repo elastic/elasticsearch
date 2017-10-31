68c4eab0f50cf615a705ab8ccaa6efd31d121c1e
Jason Tedor <jason@tedor.me>
2017-10-31 09:21:12 -0400

    Use index more

--- core/src/main/java/org/elasticsearch/index/seqno/LocalCheckpointTracker.java
+++ core/src/main/java/org/elasticsearch/index/seqno/LocalCheckpointTracker.java
@@ -227,7 +227,7 @@ public class LocalCheckpointTracker {
         if (processedSeqNo.indexExists(index) == false) {
             processedSeqNo.indexInsert(index, bitArrayKey, new FixedBitSet(bitArraysSize));
         }
-        return processedSeqNo.get(bitArrayKey);
+        return processedSeqNo.indexGet(index);
     }
 
     /**

2b9ed1f458fa66ac3dc885083ce824fff7054eee
Jack Conradson <jack@elastic.co>
2017-10-31 17:49:52 -0700

    Add version 6.0.0

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -114,6 +114,9 @@ public class Version implements Comparable<Version> {
     public static final int V_6_0_0_rc2_ID = 6000052;
     public static final Version V_6_0_0_rc2 =
         new Version(V_6_0_0_rc2_ID, org.apache.lucene.util.Version.LUCENE_7_0_1);
+    public static final int V_6_0_0_ID = 6000099;
+    public static final Version V_6_0_0 =
+        new Version(V_6_0_0_ID, org.apache.lucene.util.Version.LUCENE_7_0_1);
     public static final int V_6_1_0_ID = 6010099;
     public static final Version V_6_1_0 = new Version(V_6_1_0_ID, org.apache.lucene.util.Version.LUCENE_7_1_0);
     public static final Version CURRENT = V_6_1_0;
@@ -131,6 +134,8 @@ public class Version implements Comparable<Version> {
         switch (id) {
             case V_6_1_0_ID:
                 return V_6_1_0;
+            case V_6_0_0_ID:
+                return V_6_0_0;
             case V_6_0_0_rc2_ID:
                 return V_6_0_0_rc2;
             case V_6_0_0_rc1_ID:

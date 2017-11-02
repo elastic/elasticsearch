a4d49edf0e034727486ddb02817f42c81cbcf5be
Jason Tedor <jason@tedor.me>
2017-11-02 09:26:05 -0400

    Revert "Revert "Remove checkpoint tracker bit sets setting""
    This reverts commit df0eae2a3cf5add16481dcf25afad792d69438d0.

--- core/src/main/java/org/elasticsearch/common/settings/IndexScopedSettings.java
+++ core/src/main/java/org/elasticsearch/common/settings/IndexScopedSettings.java
@@ -120,7 +120,6 @@ public final class IndexScopedSettings extends AbstractScopedSettings {
         IndexSettings.QUERY_STRING_LENIENT_SETTING,
         IndexSettings.ALLOW_UNMAPPED,
         IndexSettings.INDEX_CHECK_ON_STARTUP,
-        LocalCheckpointTracker.SETTINGS_BIT_ARRAYS_SIZE,
         IndexSettings.MAX_REFRESH_LISTENERS_PER_SHARD,
         IndexSettings.MAX_SLICES_PER_SCROLL,
         ShardsLimitAllocationDecider.INDEX_TOTAL_SHARDS_PER_NODE_SETTING,

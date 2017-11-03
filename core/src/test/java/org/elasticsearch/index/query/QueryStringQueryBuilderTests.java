20e8005859a800d6d08e011c43dd74c64eb7c4be
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2017-11-03 13:21:44 +0000

    Fixes QueryStringQueryBuilderTests
    Closes #27246

--- core/src/test/java/org/elasticsearch/index/query/QueryStringQueryBuilderTests.java
+++ core/src/test/java/org/elasticsearch/index/query/QueryStringQueryBuilderTests.java
@@ -806,7 +806,8 @@ public class QueryStringQueryBuilderTests extends AbstractQueryTestCase<QueryStr
         QueryStringQueryBuilder queryBuilder = new QueryStringQueryBuilder(STRING_FIELD_NAME + ":*");
         Query query = queryBuilder.toQuery(context);
         if (getCurrentTypes().length > 0) {
-            if (context.getIndexSettings().getIndexVersionCreated().onOrAfter(Version.V_6_1_0)) {
+            if (context.getIndexSettings().getIndexVersionCreated().onOrAfter(Version.V_6_1_0)
+                    && (context.fieldMapper(STRING_FIELD_NAME).omitNorms() == false)) {
                 assertThat(query, equalTo(new ConstantScoreQuery(new NormsFieldExistsQuery(STRING_FIELD_NAME))));
             } else {
                 assertThat(query, equalTo(new ConstantScoreQuery(new TermQuery(new Term("_field_names", STRING_FIELD_NAME)))));

36105231c35e62ea198df5296456aa7e55823562
Alexander Reelsen <alexander@reelsen.net>
2017-11-15 11:51:23 +0100

    Watcher: Return useful error message when no accounts are found (elastic/x-pack-elasticsearch#2897)
    When there were no accounts configured, watcher returned a cryptic
    error message containing 'null' in the description. This fix returns
    a more clear error message.
    
    On top a dedicated NotificationServiceTests class was added to remove
    redundant test cases in the hipchat/jira/slack unit tests, that all
    basically tested NotificationService capabilties.
    
    relates elastic/x-pack-elasticsearch#2666
    
    Original commit: elastic/x-pack-elasticsearch@45d0d1df318cf99ca27c2e3dca9762f0778c2b5f

--- plugin/src/test/java/org/elasticsearch/xpack/watcher/notification/hipchat/HipChatAccountsTests.java
+++ plugin/src/test/java/org/elasticsearch/xpack/watcher/notification/hipchat/HipChatAccountsTests.java
@@ -7,7 +7,6 @@ package org.elasticsearch.xpack.watcher.notification.hipchat;
 
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.settings.SettingsException;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.xpack.watcher.common.http.HttpClient;
 import org.elasticsearch.xpack.watcher.common.http.HttpProxy;
@@ -21,10 +20,7 @@ import org.mockito.ArgumentCaptor;
 import java.util.Collections;
 import java.util.HashMap;
 
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.isOneOf;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -34,110 +30,6 @@ public class HipChatAccountsTests extends ESTestCase {
     @Before
     public void init() throws Exception {
         httpClient = mock(HttpClient.class);
-    }
-
-    public void testSingleAccount() throws Exception {
-        Settings.Builder builder = Settings.builder()
-                .put("xpack.notification.hipchat.default_account", "account1");
-        addAccountSettings("account1", builder);
-        HipChatService service = new HipChatService(builder.build(), httpClient, new ClusterSettings(Settings.EMPTY, 
-                Collections.singleton(HipChatService.HIPCHAT_ACCOUNT_SETTING)));
-        HipChatAccount account = service.getAccount("account1");
-        assertThat(account, notNullValue());
-        assertThat(account.name, equalTo("account1"));
-        account = service.getAccount(null); // falling back on the default
-        assertThat(account, notNullValue());
-        assertThat(account.name, equalTo("account1"));
-    }
-
-    public void testSingleAccountNoExplicitDefault() throws Exception {
-        Settings.Builder builder = Settings.builder();
-        addAccountSettings("account1", builder);
-
-        HipChatService service = new HipChatService(builder.build(), httpClient, new ClusterSettings(Settings.EMPTY,
-                Collections.singleton(HipChatService.HIPCHAT_ACCOUNT_SETTING)));
-        HipChatAccount account = service.getAccount("account1");
-        assertThat(account, notNullValue());
-        assertThat(account.name, equalTo("account1"));
-        account = service.getAccount(null); // falling back on the default
-        assertThat(account, notNullValue());
-        assertThat(account.name, equalTo("account1"));
-    }
-
-    public void testMultipleAccounts() throws Exception {
-        Settings.Builder builder = Settings.builder()
-                .put("xpack.notification.hipchat.default_account", "account1");
-        addAccountSettings("account1", builder);
-        addAccountSettings("account2", builder);
-
-        HipChatService service = new HipChatService(builder.build(), httpClient, new ClusterSettings(Settings.EMPTY,
-                Collections.singleton(HipChatService.HIPCHAT_ACCOUNT_SETTING)));
-        HipChatAccount account = service.getAccount("account1");
-        assertThat(account, notNullValue());
-        assertThat(account.name, equalTo("account1"));
-        account = service.getAccount("account2");
-        assertThat(account, notNullValue());
-        assertThat(account.name, equalTo("account2"));
-        account = service.getAccount(null); // falling back on the default
-        assertThat(account, notNullValue());
-        assertThat(account.name, equalTo("account1"));
-    }
-
-    public void testMultipleAccountsNoExplicitDefault() throws Exception {
-        Settings.Builder builder = Settings.builder()
-                .put("xpack.notification.hipchat.default_account", "account1");
-        addAccountSettings("account1", builder);
-        addAccountSettings("account2", builder);
-
-        HipChatService service = new HipChatService(builder.build(), httpClient, new ClusterSettings(Settings.EMPTY,
-                Collections.singleton(HipChatService.HIPCHAT_ACCOUNT_SETTING)));
-        HipChatAccount account = service.getAccount("account1");
-        assertThat(account, notNullValue());
-        assertThat(account.name, equalTo("account1"));
-        account = service.getAccount("account2");
-        assertThat(account, notNullValue());
-        assertThat(account.name, equalTo("account2"));
-        account = service.getAccount(null);
-        assertThat(account, notNullValue());
-        assertThat(account.name, isOneOf("account1", "account2"));
-    }
-
-    public void testMultipleAccountsUnknownDefault() throws Exception {
-        Settings.Builder builder = Settings.builder()
-                .put("xpack.notification.hipchat.default_account", "unknown");
-        addAccountSettings("account1", builder);
-        addAccountSettings("account2", builder);
-        try {
-            new HipChatService(builder.build(), httpClient, new ClusterSettings(Settings.EMPTY,
-                    Collections.singleton(HipChatService.HIPCHAT_ACCOUNT_SETTING)));
-            fail("Expected SettingsException");
-        } catch (SettingsException e) {
-            assertThat(e.getMessage(), is("could not find default account [unknown]"));
-        }
-    }
-
-    public void testNoAccount() throws Exception {
-        Settings.Builder builder = Settings.builder();
-        HipChatService service = new HipChatService(builder.build(), httpClient, new ClusterSettings(Settings.EMPTY,
-                Collections.singleton(HipChatService.HIPCHAT_ACCOUNT_SETTING)));
-        try {
-            service.getAccount(null);
-            fail("no accounts are configured so trying to get the default account should throw an IllegalStateException");
-        } catch (IllegalArgumentException e) {
-            assertThat(e.getMessage(), is("no account found for name: [null]"));
-        }
-    }
-
-    public void testNoAccountWithDefaultAccount() throws Exception {
-        Settings.Builder builder = Settings.builder()
-                .put("xpack.notification.hipchat.default_account", "unknown");
-        try {
-            new HipChatService(builder.build(), httpClient, new ClusterSettings(Settings.EMPTY,
-                    Collections.singleton(HipChatService.HIPCHAT_ACCOUNT_SETTING)));
-            fail("Expected SettingsException");
-        } catch (SettingsException e) {
-            assertThat(e.getMessage(), is("could not find default account [unknown]"));
-        }
     }
 
     public void testProxy() throws Exception {
@@ -152,7 +44,7 @@ public class HipChatAccountsTests extends ESTestCase {
                 .addRooms(new TextTemplate("room"))
                 .setFrom("from")
                 .build();
-        HipChatMessage hipChatMessage = template.render(new MockTextTemplateEngine(), new HashMap());
+        HipChatMessage hipChatMessage = template.render(new MockTextTemplateEngine(), new HashMap<>());
 
         ArgumentCaptor<HttpRequest> argumentCaptor = ArgumentCaptor.forClass(HttpRequest.class);
         when(httpClient.execute(argumentCaptor.capture())).thenReturn(new HttpResponse(200));

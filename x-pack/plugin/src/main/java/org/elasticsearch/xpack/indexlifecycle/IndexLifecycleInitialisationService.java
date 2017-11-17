0f9342642b6e1951dd062027ae1d6084c5c24564
Tal Levy <jubboy333@gmail.com>
2017-11-17 09:22:54 -0700

    tal-temp migration to not use settings for the lifecycle policy

--- x-pack/plugin/src/main/java/org/elasticsearch/xpack/indexlifecycle/IndexLifecycleInitialisationService.java
+++ x-pack/plugin/src/main/java/org/elasticsearch/xpack/indexlifecycle/IndexLifecycleInitialisationService.java
@@ -12,11 +12,7 @@ import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.component.AbstractComponent;
 import org.elasticsearch.common.logging.ESLoggerFactory;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.index.Index;
-import org.elasticsearch.index.IndexService;
-import org.elasticsearch.index.shard.IndexEventListener;
 import org.elasticsearch.threadpool.ThreadPool;
-import org.elasticsearch.xpack.XPackPlugin;
 import org.elasticsearch.xpack.scheduler.SchedulerEngine;
 import org.elasticsearch.xpack.security.InternalClient;
 
@@ -24,16 +20,16 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.time.Clock;
 
-import static org.elasticsearch.xpack.indexlifecycle.IndexLifecycle.LIFECYCLE_TIMESERIES_SETTING;
+import static org.elasticsearch.xpack.indexlifecycle.IndexLifecycle.LIFECYCLE_TIMESERIES_NAME_SETTING;
+import static org.elasticsearch.xpack.indexlifecycle.IndexLifecycle.NAME;
 
-public class IndexLifecycleInitialisationService extends AbstractComponent implements LocalNodeMasterListener, IndexEventListener, SchedulerEngine.Listener, Closeable {
-    private static final Logger logger = ESLoggerFactory.getLogger(XPackPlugin.class);
+public class IndexLifecycleInitialisationService extends AbstractComponent implements LocalNodeMasterListener, SchedulerEngine.Listener, Closeable {
+    private static final Logger logger = ESLoggerFactory.getLogger(IndexLifecycleInitialisationService.class);
 
     private final SetOnce<SchedulerEngine> scheduler = new SetOnce<>();
     private final Clock clock;
     private InternalClient client;
     private ClusterService clusterService;
-    private boolean isMaster;
 
     public IndexLifecycleInitialisationService(Settings settings, InternalClient client, ClusterService clusterService, Clock clock) {
         super(settings);
@@ -43,67 +39,31 @@ public class IndexLifecycleInitialisationService extends AbstractComponent imple
         clusterService.addLocalNodeMasterListener(this);
     }
 
-    /**
-     * This should kick-off some stuff in the scheduler
-     * This is triggered every update to settings in the index
-     * @param settings the settings to read the lifecycle details from
-     */
-    public synchronized void setLifecycleSettings(Index index, long creationDate, Settings settings) {
-        if (isMaster) {
-            IndexLifecycleSettings lifecycleSettings = new IndexLifecycleSettings(index, creationDate, settings, client);
-            lifecycleSettings.schedulePhases(scheduler.get());
-        }
-    }
-
-    /**
-     * Called before the index gets created. Note that this is also called
-     * when the index is created on data nodes
-     * @param index The index whose settings are to be validated
-     * @param indexSettings The settings for the specified index
-     */
     @Override
-    public void beforeIndexCreated(Index index, Settings indexSettings) {
-        ESLoggerFactory.getLogger("INDEX-LIFECYCLE-PLUGIN").error("validate setting before index is created");
-        LIFECYCLE_TIMESERIES_SETTING.get(indexSettings);
-    }
-
-    @Override
-    public void afterIndexCreated(IndexService indexService) {
-        Settings indexSettings = indexService.getIndexSettings().getSettings();
-        Settings lifecycleSettings = (Settings) LIFECYCLE_TIMESERIES_SETTING.get(indexSettings);
-        if (isMaster && lifecycleSettings.size() > 0) {
-            setLifecycleSettings(indexService.index(), indexService.getMetaData().getCreationDate(),
-                indexSettings.getByPrefix(LIFECYCLE_TIMESERIES_SETTING.getKey()));
-        }
-    }
-
-    @Override
-    public void triggered(SchedulerEngine.Event event) {
-        throw new UnsupportedOperationException();
+    public String executorName() {
+        return ThreadPool.Names.MANAGEMENT;
     }
 
     @Override
     public void onMaster() {
-        isMaster = true;
         scheduler.set(new SchedulerEngine(clock));
-        clusterService.state().getMetaData().getIndices().valuesIt()
-                .forEachRemaining((idxMeta) -> {
-                    if (idxMeta.getSettings().getByPrefix(LIFECYCLE_TIMESERIES_SETTING.getKey()).size() > 0) {
-                        setLifecycleSettings(idxMeta.getIndex(), idxMeta.getCreationDate(),
-                            idxMeta.getSettings().getByPrefix(LIFECYCLE_TIMESERIES_SETTING.getKey()));
-                }
-            });
+        scheduler.get().register(this);
+        scheduler.get().add(new SchedulerEngine.Job(NAME, ((startTime, now) -> now + 1000)));
     }
 
     @Override
     public void offMaster() {
-        isMaster = false;
-        // when is this called?
     }
 
     @Override
-    public String executorName() {
-        return ThreadPool.Names.MANAGEMENT;
+    public void triggered(SchedulerEngine.Event event) {
+        clusterService.state().getMetaData().getIndices().valuesIt()
+            .forEachRemaining((idxMeta) -> {
+            if (LIFECYCLE_TIMESERIES_NAME_SETTING.get(idxMeta.getSettings()) != null) {
+                // get policy by name
+                // idxMeta.getIndex(), idxMeta.getCreationDate(),client
+                }
+            });
     }
 
     @Override

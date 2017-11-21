openeuler-mirror/poissonsearch-oss

Cross Cluster Search: make remote clusters optional (#27182)
Today Cross Cluster Search requires at least one node in each remote cluster to be up once the cross cluster search is run. Otherwise the whole search request fails despite some of the data (either local and/or remote) is available. This happens when performing the _search/shards calls to find out which remote shards the query has to be executed on. This scenario is different from shard failures that may happen later on when the query is actually executed, in case e.g. remote shards are missing, which is not going to fail the whole request but rather yield partial results, and the _shards section in the response will indicate that.

This commit introduces a boolean setting per cluster called search.remote.$cluster_alias.skip_if_disconnected, set to false by default, which allows to skip certain clusters if they are down when trying to reach them through a cross cluster search requests. By default all clusters are mandatory.

Scroll requests support such setting too when they are first initiated (first search request with scroll parameter), but subsequent scroll rounds (_search/scroll endpoint) will fail if some of the remote clusters went down meanwhile.

The search API response contains now a new _clusters section, similar to the _shards section, that gets returned whenever one or more clusters were disconnected and got skipped:

"_clusters" : {
    "total" : 3,
    "successful" : 2,
    "skipped" : 1
}
Such section won't be part of the response if no clusters have been skipped.

The per cluster skip_unavailable setting value has also been added to the output of the remote/info API.

core/src/main/java/org/elasticsearch/transport/RemoteConnectionInfo.java
@@ -18,12 +18,12 @@
  */
 package org.elasticsearch.transport;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.common.xcontent.ToXContent.Params;
 import org.elasticsearch.common.xcontent.ToXContentFragment;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 
@@ -42,17 +42,19 @@ public final class RemoteConnectionInfo implements ToXContentFragment, Writeable
     final TimeValue initialConnectionTimeout;
     final int numNodesConnected;
     final String clusterAlias;
+    final boolean skipUnavailable;
 
     RemoteConnectionInfo(String clusterAlias, List<TransportAddress> seedNodes,
                          List<TransportAddress> httpAddresses,
                          int connectionsPerCluster, int numNodesConnected,
-                         TimeValue initialConnectionTimeout) {
+                         TimeValue initialConnectionTimeout, boolean skipUnavailable) {
         this.clusterAlias = clusterAlias;
         this.seedNodes = seedNodes;
         this.httpAddresses = httpAddresses;
         this.connectionsPerCluster = connectionsPerCluster;
         this.numNodesConnected = numNodesConnected;
         this.initialConnectionTimeout = initialConnectionTimeout;
+        this.skipUnavailable = skipUnavailable;
     }
 
     public RemoteConnectionInfo(StreamInput input) throws IOException {
@@ -62,6 +64,12 @@ public final class RemoteConnectionInfo implements ToXContentFragment, Writeable
         initialConnectionTimeout = new TimeValue(input);
         numNodesConnected = input.readVInt();
         clusterAlias = input.readString();
+        //TODO update version once backported
+        if (input.getVersion().onOrAfter(Version.V_7_0_0_alpha1)) {
+            skipUnavailable = input.readBoolean();
+        } else {
+            skipUnavailable = false;
+        }
     }
 
     @Override
@@ -82,6 +90,7 @@ public final class RemoteConnectionInfo implements ToXContentFragment, Writeable
             builder.field("num_nodes_connected", numNodesConnected);
             builder.field("max_connections_per_cluster", connectionsPerCluster);
             builder.field("initial_connect_timeout", initialConnectionTimeout);
+            builder.field("skip_unavailable", skipUnavailable);
         }
         builder.endObject();
         return builder;
@@ -95,6 +104,10 @@ public final class RemoteConnectionInfo implements ToXContentFragment, Writeable
         initialConnectionTimeout.writeTo(out);
         out.writeVInt(numNodesConnected);
         out.writeString(clusterAlias);
+        //TODO update version once backported
+        if (out.getVersion().onOrAfter(Version.V_7_0_0_alpha1)) {
+            out.writeBoolean(skipUnavailable);
+        }
     }
 
     @Override
@@ -107,11 +120,13 @@ public final class RemoteConnectionInfo implements ToXContentFragment, Writeable
             Objects.equals(seedNodes, that.seedNodes) &&
             Objects.equals(httpAddresses, that.httpAddresses) &&
             Objects.equals(initialConnectionTimeout, that.initialConnectionTimeout) &&
-            Objects.equals(clusterAlias, that.clusterAlias);
+            Objects.equals(clusterAlias, that.clusterAlias) &&
+            skipUnavailable == that.skipUnavailable;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(seedNodes, httpAddresses, connectionsPerCluster, initialConnectionTimeout, numNodesConnected, clusterAlias);
+        return Objects.hash(seedNodes, httpAddresses, connectionsPerCluster, initialConnectionTimeout,
+                numNodesConnected, clusterAlias, skipUnavailable);
     }
 }

core/src/test/java/org/elasticsearch/action/search/SearchResponseTests.java
@@ -19,8 +19,14 @@
 
 package org.elasticsearch.action.search;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.io.stream.BytesStreamOutput;
+import org.elasticsearch.common.io.stream.NamedWriteableAwareStreamInput;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.common.xcontent.ToXContent;
@@ -31,6 +37,7 @@ import org.elasticsearch.rest.action.search.RestSearchAction;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.SearchHitsTests;
+import org.elasticsearch.search.SearchModule;
 import org.elasticsearch.search.aggregations.AggregationsTests;
 import org.elasticsearch.search.aggregations.InternalAggregations;
 import org.elasticsearch.search.internal.InternalSearchResponse;
@@ -40,14 +47,17 @@ import org.elasticsearch.search.suggest.Suggest;
 import org.elasticsearch.search.suggest.SuggestTests;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.InternalAggregationTestCase;
+import org.elasticsearch.test.VersionUtils;
 import org.junit.After;
 import org.junit.Before;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonMap;
 import static org.elasticsearch.test.XContentTestUtils.insertRandomFields;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertToXContentEquivalent;
@@ -61,6 +71,8 @@ public class SearchResponseTests extends ESTestCase {
         xContentRegistry = new NamedXContentRegistry(namedXContents);
     }
 
+    private final NamedWriteableRegistry namedWriteableRegistry = new NamedWriteableRegistry(
+            new SearchModule(Settings.EMPTY, false, emptyList()).getNamedWriteables());
     private AggregationsTests aggregationsTests = new AggregationsTests();
 
     @Before
@@ -112,8 +124,16 @@ public class SearchResponseTests extends ESTestCase {
         } else {
             internalSearchResponse = InternalSearchResponse.empty();
         }
+
         return new SearchResponse(internalSearchResponse, null, totalShards, successfulShards, skippedShards, tookInMillis,
-            shardSearchFailures);
+            shardSearchFailures, randomBoolean() ? randomClusters() : SearchResponse.Clusters.EMPTY);
+    }
+
+    private static SearchResponse.Clusters randomClusters() {
+        int totalClusters = randomIntBetween(0, 10);
+        int successfulClusters = randomIntBetween(0, totalClusters);
+        int skippedClusters = totalClusters - successfulClusters;
+        return new SearchResponse.Clusters(totalClusters, successfulClusters, skippedClusters);
     }
 
     /**
@@ -193,29 +213,104 @@ public class SearchResponseTests extends ESTestCase {
         SearchHit hit = new SearchHit(1, "id1", new Text("type"), Collections.emptyMap());
         hit.score(2.0f);
         SearchHit[] hits = new SearchHit[] { hit };
-        SearchResponse response = new SearchResponse(
-                new InternalSearchResponse(new SearchHits(hits, 100, 1.5f), null, null, null, false, null, 1), null, 0, 0, 0, 0,
-                new ShardSearchFailure[0]);
-        StringBuilder expectedString = new StringBuilder();
-        expectedString.append("{");
         {
-            expectedString.append("\"took\":0,");
-            expectedString.append("\"timed_out\":false,");
-            expectedString.append("\"_shards\":");
+            SearchResponse response = new SearchResponse(
+                    new InternalSearchResponse(new SearchHits(hits, 100, 1.5f), null, null, null, false, null, 1), null, 0, 0, 0, 0,
+                    ShardSearchFailure.EMPTY_ARRAY, SearchResponse.Clusters.EMPTY);
+            StringBuilder expectedString = new StringBuilder();
+            expectedString.append("{");
             {
-                expectedString.append("{\"total\":0,");
-                expectedString.append("\"successful\":0,");
-                expectedString.append("\"skipped\":0,");
-                expectedString.append("\"failed\":0},");
+                expectedString.append("\"took\":0,");
+                expectedString.append("\"timed_out\":false,");
+                expectedString.append("\"_shards\":");
+                {
+                    expectedString.append("{\"total\":0,");
+                    expectedString.append("\"successful\":0,");
+                    expectedString.append("\"skipped\":0,");
+                    expectedString.append("\"failed\":0},");
+                }
+                expectedString.append("\"hits\":");
+                {
+                    expectedString.append("{\"total\":100,");
+                    expectedString.append("\"max_score\":1.5,");
+                    expectedString.append("\"hits\":[{\"_type\":\"type\",\"_id\":\"id1\",\"_score\":2.0}]}");
+                }
             }
-            expectedString.append("\"hits\":");
+            expectedString.append("}");
+            assertEquals(expectedString.toString(), Strings.toString(response));
+        }
+        {
+            SearchResponse response = new SearchResponse(
+                    new InternalSearchResponse(new SearchHits(hits, 100, 1.5f), null, null, null, false, null, 1), null, 0, 0, 0, 0,
+                    ShardSearchFailure.EMPTY_ARRAY, new SearchResponse.Clusters(5, 3, 2));
+            StringBuilder expectedString = new StringBuilder();
+            expectedString.append("{");
             {
-                expectedString.append("{\"total\":100,");
-                expectedString.append("\"max_score\":1.5,");
-                expectedString.append("\"hits\":[{\"_type\":\"type\",\"_id\":\"id1\",\"_score\":2.0}]}");
+                expectedString.append("\"took\":0,");
+                expectedString.append("\"timed_out\":false,");
+                expectedString.append("\"_shards\":");
+                {
+                    expectedString.append("{\"total\":0,");
+                    expectedString.append("\"successful\":0,");
+                    expectedString.append("\"skipped\":0,");
+                    expectedString.append("\"failed\":0},");
+                }
+                expectedString.append("\"_clusters\":");
+                {
+                    expectedString.append("{\"total\":5,");
+                    expectedString.append("\"successful\":3,");
+                    expectedString.append("\"skipped\":2},");
+                }
+                expectedString.append("\"hits\":");
+                {
+                    expectedString.append("{\"total\":100,");
+                    expectedString.append("\"max_score\":1.5,");
+                    expectedString.append("\"hits\":[{\"_type\":\"type\",\"_id\":\"id1\",\"_score\":2.0}]}");
+                }
+            }
+            expectedString.append("}");
+            assertEquals(expectedString.toString(), Strings.toString(response));
+        }
+    }
+
+    public void testSerialization() throws IOException {
+        SearchResponse searchResponse = createTestItem(false);
+        BytesStreamOutput bytesStreamOutput = new BytesStreamOutput();
+        searchResponse.writeTo(bytesStreamOutput);
+        try (StreamInput in = new NamedWriteableAwareStreamInput(
+                StreamInput.wrap(bytesStreamOutput.bytes().toBytesRef().bytes), namedWriteableRegistry)) {
+            SearchResponse serialized = new SearchResponse();
+            serialized.readFrom(in);
+            assertEquals(searchResponse.getHits().totalHits, serialized.getHits().totalHits);
+            assertEquals(searchResponse.getHits().getHits().length, serialized.getHits().getHits().length);
+            assertEquals(searchResponse.getNumReducePhases(), serialized.getNumReducePhases());
+            assertEquals(searchResponse.getFailedShards(), serialized.getFailedShards());
+            assertEquals(searchResponse.getTotalShards(), serialized.getTotalShards());
+            assertEquals(searchResponse.getSkippedShards(), serialized.getSkippedShards());
+            assertEquals(searchResponse.getClusters(), serialized.getClusters());
+        }
+    }
+
+    public void testSerializationBwc() throws IOException {
+        final byte[] data = Base64.getDecoder().decode("AAAAAAAAAAAAAgABBQUAAAoAAAAAAAAA");
+        final Version version = VersionUtils.randomVersionBetween(random(), Version.V_5_6_0, Version.V_6_0_0_rc2);
+        try (StreamInput in = new NamedWriteableAwareStreamInput(StreamInput.wrap(data), namedWriteableRegistry)) {
+            in.setVersion(version);
+            SearchResponse deserialized = new SearchResponse();
+            deserialized.readFrom(in);
+            assertSame(SearchResponse.Clusters.EMPTY, deserialized.getClusters());
+
+            try (BytesStreamOutput out = new BytesStreamOutput()) {
+                out.setVersion(version);
+                deserialized.writeTo(out);
+                try (StreamInput in2 = new NamedWriteableAwareStreamInput(StreamInput.wrap(out.bytes().toBytesRef().bytes),
+                        namedWriteableRegistry)) {
+                    in2.setVersion(version);
+                    SearchResponse deserialized2 = new SearchResponse();
+                    deserialized2.readFrom(in2);
+                    assertSame(SearchResponse.Clusters.EMPTY, deserialized2.getClusters());
+                }
             }
         }
-        expectedString.append("}");
-        assertEquals(expectedString.toString(), Strings.toString(response));
     }
 }

elastic/elasticsearch

core/src/main/java/org/elasticsearch/transport/RemoteConnectionInfo.java
@@ -18,12 +18,12 @@
  */
 package org.elasticsearch.transport;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.common.xcontent.ToXContent.Params;
 import org.elasticsearch.common.xcontent.ToXContentFragment;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 
@@ -42,17 +42,19 @@ public final class RemoteConnectionInfo implements ToXContentFragment, Writeable
     final TimeValue initialConnectionTimeout;
     final int numNodesConnected;
     final String clusterAlias;
+    final boolean skipUnavailable;
 
     RemoteConnectionInfo(String clusterAlias, List<TransportAddress> seedNodes,
                          List<TransportAddress> httpAddresses,
                          int connectionsPerCluster, int numNodesConnected,
-                         TimeValue initialConnectionTimeout) {
+                         TimeValue initialConnectionTimeout, boolean skipUnavailable) {
         this.clusterAlias = clusterAlias;
         this.seedNodes = seedNodes;
         this.httpAddresses = httpAddresses;
         this.connectionsPerCluster = connectionsPerCluster;
         this.numNodesConnected = numNodesConnected;
         this.initialConnectionTimeout = initialConnectionTimeout;
+        this.skipUnavailable = skipUnavailable;
     }
 
     public RemoteConnectionInfo(StreamInput input) throws IOException {
@@ -62,6 +64,11 @@ public final class RemoteConnectionInfo implements ToXContentFragment, Writeable
         initialConnectionTimeout = new TimeValue(input);
         numNodesConnected = input.readVInt();
         clusterAlias = input.readString();
+        if (input.getVersion().onOrAfter(Version.V_6_1_0)) {
+            skipUnavailable = input.readBoolean();
+        } else {
+            skipUnavailable = false;
+        }
     }
 
     @Override
@@ -82,6 +89,7 @@ public final class RemoteConnectionInfo implements ToXContentFragment, Writeable
             builder.field("num_nodes_connected", numNodesConnected);
             builder.field("max_connections_per_cluster", connectionsPerCluster);
             builder.field("initial_connect_timeout", initialConnectionTimeout);
+            builder.field("skip_unavailable", skipUnavailable);
         }
         builder.endObject();
         return builder;
@@ -95,6 +103,9 @@ public final class RemoteConnectionInfo implements ToXContentFragment, Writeable
         initialConnectionTimeout.writeTo(out);
         out.writeVInt(numNodesConnected);
         out.writeString(clusterAlias);
+        if (out.getVersion().onOrAfter(Version.V_6_1_0)) {
+            out.writeBoolean(skipUnavailable);
+        }
     }
 
     @Override
@@ -107,11 +118,13 @@ public final class RemoteConnectionInfo implements ToXContentFragment, Writeable
             Objects.equals(seedNodes, that.seedNodes) &&
             Objects.equals(httpAddresses, that.httpAddresses) &&
             Objects.equals(initialConnectionTimeout, that.initialConnectionTimeout) &&
-            Objects.equals(clusterAlias, that.clusterAlias);
+            Objects.equals(clusterAlias, that.clusterAlias) &&
+            skipUnavailable == that.skipUnavailable;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(seedNodes, httpAddresses, connectionsPerCluster, initialConnectionTimeout, numNodesConnected, clusterAlias);
+        return Objects.hash(seedNodes, httpAddresses, connectionsPerCluster, initialConnectionTimeout,
+                numNodesConnected, clusterAlias, skipUnavailable);
     }
 }

core/src/test/java/org/elasticsearch/action/search/SearchResponseTests.java
@@ -19,8 +19,14 @@
 
 package org.elasticsearch.action.search;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.io.stream.BytesStreamOutput;
+import org.elasticsearch.common.io.stream.NamedWriteableAwareStreamInput;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.common.xcontent.ToXContent;
@@ -31,6 +37,7 @@ import org.elasticsearch.rest.action.search.RestSearchAction;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.SearchHitsTests;
+import org.elasticsearch.search.SearchModule;
 import org.elasticsearch.search.aggregations.AggregationsTests;
 import org.elasticsearch.search.aggregations.InternalAggregations;
 import org.elasticsearch.search.internal.InternalSearchResponse;
@@ -40,14 +47,17 @@ import org.elasticsearch.search.suggest.Suggest;
 import org.elasticsearch.search.suggest.SuggestTests;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.InternalAggregationTestCase;
+import org.elasticsearch.test.VersionUtils;
 import org.junit.After;
 import org.junit.Before;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonMap;
 import static org.elasticsearch.test.XContentTestUtils.insertRandomFields;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertToXContentEquivalent;
@@ -61,6 +71,8 @@ public class SearchResponseTests extends ESTestCase {
         xContentRegistry = new NamedXContentRegistry(namedXContents);
     }
 
+    private final NamedWriteableRegistry namedWriteableRegistry = new NamedWriteableRegistry(
+            new SearchModule(Settings.EMPTY, false, emptyList()).getNamedWriteables());
     private AggregationsTests aggregationsTests = new AggregationsTests();
 
     @Before
@@ -112,8 +124,16 @@ public class SearchResponseTests extends ESTestCase {
         } else {
             internalSearchResponse = InternalSearchResponse.empty();
         }
+
         return new SearchResponse(internalSearchResponse, null, totalShards, successfulShards, skippedShards, tookInMillis,
-            shardSearchFailures);
+            shardSearchFailures, randomBoolean() ? randomClusters() : SearchResponse.Clusters.EMPTY);
+    }
+
+    private static SearchResponse.Clusters randomClusters() {
+        int totalClusters = randomIntBetween(0, 10);
+        int successfulClusters = randomIntBetween(0, totalClusters);
+        int skippedClusters = totalClusters - successfulClusters;
+        return new SearchResponse.Clusters(totalClusters, successfulClusters, skippedClusters);
     }
 
     /**
@@ -193,29 +213,104 @@ public class SearchResponseTests extends ESTestCase {
         SearchHit hit = new SearchHit(1, "id1", new Text("type"), Collections.emptyMap());
         hit.score(2.0f);
         SearchHit[] hits = new SearchHit[] { hit };
-        SearchResponse response = new SearchResponse(
-                new InternalSearchResponse(new SearchHits(hits, 100, 1.5f), null, null, null, false, null, 1), null, 0, 0, 0, 0,
-                new ShardSearchFailure[0]);
-        StringBuilder expectedString = new StringBuilder();
-        expectedString.append("{");
         {
-            expectedString.append("\"took\":0,");
-            expectedString.append("\"timed_out\":false,");
-            expectedString.append("\"_shards\":");
+            SearchResponse response = new SearchResponse(
+                    new InternalSearchResponse(new SearchHits(hits, 100, 1.5f), null, null, null, false, null, 1), null, 0, 0, 0, 0,
+                    ShardSearchFailure.EMPTY_ARRAY, SearchResponse.Clusters.EMPTY);
+            StringBuilder expectedString = new StringBuilder();
+            expectedString.append("{");
             {
-                expectedString.append("{\"total\":0,");
-                expectedString.append("\"successful\":0,");
-                expectedString.append("\"skipped\":0,");
-                expectedString.append("\"failed\":0},");
+                expectedString.append("\"took\":0,");
+                expectedString.append("\"timed_out\":false,");
+                expectedString.append("\"_shards\":");
+                {
+                    expectedString.append("{\"total\":0,");
+                    expectedString.append("\"successful\":0,");
+                    expectedString.append("\"skipped\":0,");
+                    expectedString.append("\"failed\":0},");
+                }
+                expectedString.append("\"hits\":");
+                {
+                    expectedString.append("{\"total\":100,");
+                    expectedString.append("\"max_score\":1.5,");
+                    expectedString.append("\"hits\":[{\"_type\":\"type\",\"_id\":\"id1\",\"_score\":2.0}]}");
+                }
             }
-            expectedString.append("\"hits\":");
+            expectedString.append("}");
+            assertEquals(expectedString.toString(), Strings.toString(response));
+        }
+        {
+            SearchResponse response = new SearchResponse(
+                    new InternalSearchResponse(new SearchHits(hits, 100, 1.5f), null, null, null, false, null, 1), null, 0, 0, 0, 0,
+                    ShardSearchFailure.EMPTY_ARRAY, new SearchResponse.Clusters(5, 3, 2));
+            StringBuilder expectedString = new StringBuilder();
+            expectedString.append("{");
             {
-                expectedString.append("{\"total\":100,");
-                expectedString.append("\"max_score\":1.5,");
-                expectedString.append("\"hits\":[{\"_type\":\"type\",\"_id\":\"id1\",\"_score\":2.0}]}");
+                expectedString.append("\"took\":0,");
+                expectedString.append("\"timed_out\":false,");
+                expectedString.append("\"_shards\":");
+                {
+                    expectedString.append("{\"total\":0,");
+                    expectedString.append("\"successful\":0,");
+                    expectedString.append("\"skipped\":0,");
+                    expectedString.append("\"failed\":0},");
+                }
+                expectedString.append("\"_clusters\":");
+                {
+                    expectedString.append("{\"total\":5,");
+                    expectedString.append("\"successful\":3,");
+                    expectedString.append("\"skipped\":2},");
+                }
+                expectedString.append("\"hits\":");
+                {
+                    expectedString.append("{\"total\":100,");
+                    expectedString.append("\"max_score\":1.5,");
+                    expectedString.append("\"hits\":[{\"_type\":\"type\",\"_id\":\"id1\",\"_score\":2.0}]}");
+                }
+            }
+            expectedString.append("}");
+            assertEquals(expectedString.toString(), Strings.toString(response));
+        }
+    }
+
+    public void testSerialization() throws IOException {
+        SearchResponse searchResponse = createTestItem(false);
+        BytesStreamOutput bytesStreamOutput = new BytesStreamOutput();
+        searchResponse.writeTo(bytesStreamOutput);
+        try (StreamInput in = new NamedWriteableAwareStreamInput(
+                StreamInput.wrap(bytesStreamOutput.bytes().toBytesRef().bytes), namedWriteableRegistry)) {
+            SearchResponse serialized = new SearchResponse();
+            serialized.readFrom(in);
+            assertEquals(searchResponse.getHits().totalHits, serialized.getHits().totalHits);
+            assertEquals(searchResponse.getHits().getHits().length, serialized.getHits().getHits().length);
+            assertEquals(searchResponse.getNumReducePhases(), serialized.getNumReducePhases());
+            assertEquals(searchResponse.getFailedShards(), serialized.getFailedShards());
+            assertEquals(searchResponse.getTotalShards(), serialized.getTotalShards());
+            assertEquals(searchResponse.getSkippedShards(), serialized.getSkippedShards());
+            assertEquals(searchResponse.getClusters(), serialized.getClusters());
+        }
+    }
+
+    public void testSerializationBwc() throws IOException {
+        final byte[] data = Base64.getDecoder().decode("AAAAAAAAAAAAAgABBQUAAAoAAAAAAAAA");
+        final Version version = VersionUtils.randomVersionBetween(random(), Version.V_5_6_5, Version.V_6_0_0);
+        try (StreamInput in = new NamedWriteableAwareStreamInput(StreamInput.wrap(data), namedWriteableRegistry)) {
+            in.setVersion(version);
+            SearchResponse deserialized = new SearchResponse();
+            deserialized.readFrom(in);
+            assertSame(SearchResponse.Clusters.EMPTY, deserialized.getClusters());
+
+            try (BytesStreamOutput out = new BytesStreamOutput()) {
+                out.setVersion(version);
+                deserialized.writeTo(out);
+                try (StreamInput in2 = new NamedWriteableAwareStreamInput(StreamInput.wrap(out.bytes().toBytesRef().bytes),
+                        namedWriteableRegistry)) {
+                    in2.setVersion(version);
+                    SearchResponse deserialized2 = new SearchResponse();
+                    deserialized2.readFrom(in2);
+                    assertSame(SearchResponse.Clusters.EMPTY, deserialized2.getClusters());
+                }
             }
         }
-        expectedString.append("}");
-        assertEquals(expectedString.toString(), Strings.toString(response));
     }
 }
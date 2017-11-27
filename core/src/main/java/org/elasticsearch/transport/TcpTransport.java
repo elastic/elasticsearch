3de3a725b2f9f654f74e9436aed36b0230bab446
Jason Tedor <jason@tedor.me>
2017-11-27 13:48:04 -0500

    Bubble exceptions when closing compressible streams
    Compressible bytes output stream swallows exceptions that occur when
    closing. This commit changes this behavior so that such exceptions
    bubble up.
    
    Relates #27542

--- core/src/main/java/org/elasticsearch/transport/TcpTransport.java
+++ core/src/main/java/org/elasticsearch/transport/TcpTransport.java
@@ -47,7 +47,6 @@ import org.elasticsearch.common.io.stream.ReleasableBytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.lease.Releasable;
-import org.elasticsearch.common.lease.Releasables;
 import org.elasticsearch.common.metrics.CounterMetric;
 import org.elasticsearch.common.metrics.MeanMetric;
 import org.elasticsearch.common.network.NetworkAddress;
@@ -71,8 +70,10 @@ import org.elasticsearch.monitor.jvm.JvmInfo;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.threadpool.ThreadPool;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.StreamCorruptedException;
+import java.io.UncheckedIOException;
 import java.net.BindException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -1689,27 +1690,34 @@ public abstract class TcpTransport<Channel> extends AbstractLifecycleComponent i
     }
 
     private final class SendListener extends SendMetricListener<Channel> {
-        private final Releasable optionalReleasable;
+        private final Closeable optionalCloseable;
         private final Runnable transportAdaptorCallback;
 
-        private SendListener(Releasable optionalReleasable, Runnable transportAdaptorCallback, long messageLength) {
+        private SendListener(Closeable optionalCloseable, Runnable transportAdaptorCallback, long messageLength) {
             super(messageLength);
-            this.optionalReleasable = optionalReleasable;
+            this.optionalCloseable = optionalCloseable;
             this.transportAdaptorCallback = transportAdaptorCallback;
         }
 
         @Override
         protected void innerInnerOnResponse(Channel channel) {
-            release();
+            closeAndCallback(null);
         }
 
         @Override
         protected void innerOnFailure(Exception e) {
-            release();
+            closeAndCallback(e);
         }
 
-        private void release() {
-            Releasables.close(optionalReleasable, transportAdaptorCallback::run);
+        private void closeAndCallback(final Exception e) {
+            try {
+                IOUtils.close(optionalCloseable, transportAdaptorCallback::run);
+            } catch (final IOException inner) {
+                if (e != null) {
+                    inner.addSuppressed(e);
+                }
+                throw new UncheckedIOException(inner);
+            }
         }
     }
 

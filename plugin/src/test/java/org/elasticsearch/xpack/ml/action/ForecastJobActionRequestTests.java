3e52e0ba48bee7c92238d780fe54ea353d55e3ac
Dimitris Athanasiou <dimitris@elastic.co>
2017-11-28 10:57:48 +0000

    [ML] Validate duration and expires_in params in forecast API (elastic/x-pack-elasticsearch#3139)
    Relates elastic/machine-learning-cpp#443
    
    Original commit: elastic/x-pack-elasticsearch@f42e4490d14552ff6caa3417ccb6d99d23f03bf3

--- plugin/src/test/java/org/elasticsearch/xpack/ml/action/ForecastJobActionRequestTests.java
+++ plugin/src/test/java/org/elasticsearch/xpack/ml/action/ForecastJobActionRequestTests.java
@@ -10,6 +10,8 @@ import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.test.AbstractStreamableXContentTestCase;
 import org.elasticsearch.xpack.ml.action.ForecastJobAction.Request;
 
+import static org.hamcrest.Matchers.equalTo;
+
 public class ForecastJobActionRequestTests extends AbstractStreamableXContentTestCase<Request> {
 
     @Override
@@ -28,12 +30,35 @@ public class ForecastJobActionRequestTests extends AbstractStreamableXContentTes
         if (randomBoolean()) {
             request.setDuration(TimeValue.timeValueSeconds(randomIntBetween(1, 1_000_000)).getStringRep());
         }
-
+        if (randomBoolean()) {
+            request.setExpiresIn(TimeValue.timeValueSeconds(randomIntBetween(0, 1_000_000)).getStringRep());
+        }
         return request;
     }
 
     @Override
     protected Request createBlankInstance() {
         return new Request();
+    }
+
+    public void testSetDuration_GivenZero() {
+        IllegalArgumentException e = expectThrows(IllegalArgumentException.class, () -> new Request().setDuration("0"));
+        assertThat(e.getMessage(), equalTo("[duration] must be positive: [0ms]"));
+    }
+
+    public void testSetDuration_GivenNegative() {
+        IllegalArgumentException e = expectThrows(IllegalArgumentException.class, () -> new Request().setDuration("-1s"));
+        assertThat(e.getMessage(), equalTo("[duration] must be positive: [-1]"));
+    }
+
+    public void testSetExpiresIn_GivenZero() {
+        Request request = new Request();
+        request.setExpiresIn("0");
+        assertThat(request.getExpiresIn(), equalTo(TimeValue.ZERO));
+    }
+
+    public void testSetExpiresIn_GivenNegative() {
+        IllegalArgumentException e = expectThrows(IllegalArgumentException.class, () -> new Request().setExpiresIn("-1s"));
+        assertThat(e.getMessage(), equalTo("[expires_in] must be non-negative: [-1]"));
     }
 }

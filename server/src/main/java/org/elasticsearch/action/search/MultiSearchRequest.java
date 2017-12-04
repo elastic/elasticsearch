e7675a47128914838110365cb826cc1c3f9149c3
markharwood <markharwood@gmail.com>
2017-12-04 16:54:56 +0000

    Add allow_partial_search_results flag to search requests with default setting = false. When true will error if search either timeouts, has partial errors or has missing shards rather than returning partial search results.
    Closes #27435

--- server/src/main/java/org/elasticsearch/action/search/MultiSearchRequest.java
+++ server/src/main/java/org/elasticsearch/action/search/MultiSearchRequest.java
@@ -226,6 +226,8 @@ public class MultiSearchRequest extends ActionRequest implements CompositeIndice
                             searchRequest.preference(nodeStringValue(value, null));
                         } else if ("routing".equals(entry.getKey())) {
                             searchRequest.routing(nodeStringValue(value, null));
+                        } else if ("allow_partial_search_results".equals(entry.getKey())) {
+                            searchRequest.allowPartialSearchResults(nodeBooleanValue(value, null));
                         }
                     }
                     defaultOptions = IndicesOptions.fromMap(source, defaultOptions);
@@ -296,6 +298,9 @@ public class MultiSearchRequest extends ActionRequest implements CompositeIndice
                 }
                 if (request.routing() != null) {
                     xContentBuilder.field("routing", request.routing());
+                }
+                if (request.allowPartialSearchResults() != null) {
+                    xContentBuilder.field("allow_partial_search_results", request.allowPartialSearchResults());
                 }
                 xContentBuilder.endObject();
                 xContentBuilder.bytes().writeTo(output);

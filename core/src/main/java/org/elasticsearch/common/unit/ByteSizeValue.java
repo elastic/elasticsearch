c4d615bfe2da6e9181a59f453f90d2918c800cce
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2017-12-06 12:31:08 +0000

    Fixes ByteSizeValue to serialise correctly
    This fix makes a few fixes to ByteSizeValue to make it possible to perform round-trip serialisation:
    * Changes wire serialisation to use Zlong methods instead of VLong methods. This is needed because the value `-1` is accepted but previously if `-1` is supplied it cannot be serialised using the wire protocol.
    * Limits the supplied size to be no more than Long.MAX_VALUE when converted to bytes. Previously values greater than Long.MAX_VALUE bytes were accepted but would be silently interpreted as Long.MAX_VALUE bytes rather than erroring so the user had no idea the value was not being used the way they had intended. I consider this a bug and so fine to include this bug fix in a minor version but I am open to other points of view.
    * Adds a `getStringRep()` method that can be used when serialising the value to JSON. This will print the bytes value if the size is positive, `”0”` if the size is `0` and `”-1”` if the size is `-1`.
    * Adds logic to detect fractional values when parsing from a String and emits a deprecation warning in this case.
    * Modifies hashCode and equals methods to work with long values rather than doubles so they don’t run into precision problems when dealing with large values. Previous to this change the equals method would not detect small differences in the values (e.g. 1-1000 bytes ranges) if the actual values where very large (e.g. PBs). This was due to the values being in the order of 10^18 but doubles only maintaining a precision of ~10^15.
    
    Closes #27568

--- core/src/main/java/org/elasticsearch/common/unit/ByteSizeValue.java
+++ core/src/main/java/org/elasticsearch/common/unit/ByteSizeValue.java
@@ -20,28 +20,40 @@
 package org.elasticsearch.common.unit;
 
 import org.elasticsearch.ElasticsearchParseException;
+import org.elasticsearch.Version;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.common.logging.DeprecationLogger;
+import org.elasticsearch.common.logging.Loggers;
 
 import java.io.IOException;
 import java.util.Locale;
 import java.util.Objects;
 
 public class ByteSizeValue implements Writeable, Comparable<ByteSizeValue> {
+    private static final DeprecationLogger DEPRECATION_LOGGER = new DeprecationLogger(Loggers.getLogger(ByteSizeValue.class));
 
     private final long size;
     private final ByteSizeUnit unit;
 
     public ByteSizeValue(StreamInput in) throws IOException {
-        size = in.readVLong();
+        if (in.getVersion().before(Version.V_6_2_0)) {
+            size = in.readVLong();
+        } else {
+            size = in.readZLong();
+        }
         unit = ByteSizeUnit.BYTES;
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
-        out.writeVLong(getBytes());
+        if (out.getVersion().before(Version.V_6_2_0)) {
+            out.writeVLong(getBytes());
+        } else {
+            out.writeZLong(getBytes());
+        }
     }
 
     public ByteSizeValue(long bytes) {
@@ -51,8 +63,23 @@ public class ByteSizeValue implements Writeable, Comparable<ByteSizeValue> {
     public ByteSizeValue(long size, ByteSizeUnit unit) {
         this.size = size;
         this.unit = unit;
+        if (size > Long.MAX_VALUE / unit.toBytes(1)) {
+            throw new IllegalArgumentException(
+                    "Values greater than " + Long.MAX_VALUE + " bytes are not supported: " + size + unit.getSuffix());
+        }
     }
 
+    // For testing
+    long getSize() {
+        return size;
+    }
+
+    // For testing
+    ByteSizeUnit getUnit() {
+        return unit;
+    }
+
+    @Deprecated
     public int bytesAsInt() {
         long bytes = getBytes();
         if (bytes > Integer.MAX_VALUE) {
@@ -105,26 +132,33 @@ public class ByteSizeValue implements Writeable, Comparable<ByteSizeValue> {
         return ((double) getBytes()) / ByteSizeUnit.C5;
     }
 
+    public String getStringRep() {
+        if (size <= 0) {
+            return String.valueOf(size);
+        }
+        return getBytes() + ByteSizeUnit.BYTES.getSuffix();
+    }
+
     @Override
     public String toString() {
         long bytes = getBytes();
         double value = bytes;
-        String suffix = "b";
+        String suffix = ByteSizeUnit.BYTES.getSuffix();
         if (bytes >= ByteSizeUnit.C5) {
             value = getPbFrac();
-            suffix = "pb";
+            suffix = ByteSizeUnit.PB.getSuffix();
         } else if (bytes >= ByteSizeUnit.C4) {
             value = getTbFrac();
-            suffix = "tb";
+            suffix = ByteSizeUnit.TB.getSuffix();
         } else if (bytes >= ByteSizeUnit.C3) {
             value = getGbFrac();
-            suffix = "gb";
+            suffix = ByteSizeUnit.GB.getSuffix();
         } else if (bytes >= ByteSizeUnit.C2) {
             value = getMbFrac();
-            suffix = "mb";
+            suffix = ByteSizeUnit.MB.getSuffix();
         } else if (bytes >= ByteSizeUnit.C1) {
             value = getKbFrac();
-            suffix = "kb";
+            suffix = ByteSizeUnit.KB.getSuffix();
         }
         return Strings.format1Decimals(value, suffix);
     }
@@ -140,46 +174,59 @@ public class ByteSizeValue implements Writeable, Comparable<ByteSizeValue> {
             return defaultValue;
         }
         long bytes;
-        try {
-            String lowerSValue = sValue.toLowerCase(Locale.ROOT).trim();
-            if (lowerSValue.endsWith("k")) {
-                bytes = (long) (Double.parseDouble(lowerSValue.substring(0, lowerSValue.length() - 1)) * ByteSizeUnit.C1);
-            } else if (lowerSValue.endsWith("kb")) {
-                bytes = (long) (Double.parseDouble(lowerSValue.substring(0, lowerSValue.length() - 2)) * ByteSizeUnit.C1);
-            } else if (lowerSValue.endsWith("m")) {
-                bytes = (long) (Double.parseDouble(lowerSValue.substring(0, lowerSValue.length() - 1)) * ByteSizeUnit.C2);
-            } else if (lowerSValue.endsWith("mb")) {
-                bytes = (long) (Double.parseDouble(lowerSValue.substring(0, lowerSValue.length() - 2)) * ByteSizeUnit.C2);
-            } else if (lowerSValue.endsWith("g")) {
-                bytes = (long) (Double.parseDouble(lowerSValue.substring(0, lowerSValue.length() - 1)) * ByteSizeUnit.C3);
-            } else if (lowerSValue.endsWith("gb")) {
-                bytes = (long) (Double.parseDouble(lowerSValue.substring(0, lowerSValue.length() - 2)) * ByteSizeUnit.C3);
-            } else if (lowerSValue.endsWith("t")) {
-                bytes = (long) (Double.parseDouble(lowerSValue.substring(0, lowerSValue.length() - 1)) * ByteSizeUnit.C4);
-            } else if (lowerSValue.endsWith("tb")) {
-                bytes = (long) (Double.parseDouble(lowerSValue.substring(0, lowerSValue.length() - 2)) * ByteSizeUnit.C4);
-            } else if (lowerSValue.endsWith("p")) {
-                bytes = (long) (Double.parseDouble(lowerSValue.substring(0, lowerSValue.length() - 1)) * ByteSizeUnit.C5);
-            } else if (lowerSValue.endsWith("pb")) {
-                bytes = (long) (Double.parseDouble(lowerSValue.substring(0, lowerSValue.length() - 2)) * ByteSizeUnit.C5);
-            } else if (lowerSValue.endsWith("b")) {
-                bytes = Long.parseLong(lowerSValue.substring(0, lowerSValue.length() - 1).trim());
-            } else if (lowerSValue.equals("-1")) {
-                // Allow this special value to be unit-less:
-                bytes = -1;
-            } else if (lowerSValue.equals("0")) {
-                // Allow this special value to be unit-less:
-                bytes = 0;
-            } else {
-                // Missing units:
-                throw new ElasticsearchParseException(
-                        "failed to parse setting [{}] with value [{}] as a size in bytes: unit is missing or unrecognized",
-                        settingName, sValue);
-            }
-        } catch (NumberFormatException e) {
-            throw new ElasticsearchParseException("failed to parse [{}]", e, sValue);
+        String lowerSValue = sValue.toLowerCase(Locale.ROOT).trim();
+        if (lowerSValue.endsWith("k")) {
+            bytes = (long) (parse(sValue, lowerSValue, "k", settingName) * ByteSizeUnit.C1);
+        } else if (lowerSValue.endsWith("kb")) {
+            bytes = (long) (parse(sValue, lowerSValue, "kb", settingName) * ByteSizeUnit.C1);
+        } else if (lowerSValue.endsWith("m")) {
+            bytes = (long) (parse(sValue, lowerSValue, "m", settingName) * ByteSizeUnit.C2);
+        } else if (lowerSValue.endsWith("mb")) {
+            bytes = (long) (parse(sValue, lowerSValue, "mb", settingName) * ByteSizeUnit.C2);
+        } else if (lowerSValue.endsWith("g")) {
+            bytes = (long) (parse(sValue, lowerSValue, "g", settingName) * ByteSizeUnit.C3);
+        } else if (lowerSValue.endsWith("gb")) {
+            bytes = (long) (parse(sValue, lowerSValue, "gb", settingName) * ByteSizeUnit.C3);
+        } else if (lowerSValue.endsWith("t")) {
+            bytes = (long) (parse(sValue, lowerSValue, "t", settingName) * ByteSizeUnit.C4);
+        } else if (lowerSValue.endsWith("tb")) {
+            bytes = (long) (parse(sValue, lowerSValue, "tb", settingName) * ByteSizeUnit.C4);
+        } else if (lowerSValue.endsWith("p")) {
+            bytes = (long) (parse(sValue, lowerSValue, "p", settingName) * ByteSizeUnit.C5);
+        } else if (lowerSValue.endsWith("pb")) {
+            bytes = (long) (parse(sValue, lowerSValue, "pb", settingName) * ByteSizeUnit.C5);
+        } else if (lowerSValue.endsWith("b")) {
+            bytes = Long.parseLong(lowerSValue.substring(0, lowerSValue.length() - 1).trim());
+        } else if (lowerSValue.equals("-1")) {
+            // Allow this special value to be unit-less:
+            bytes = -1;
+        } else if (lowerSValue.equals("0")) {
+            // Allow this special value to be unit-less:
+            bytes = 0;
+        } else {
+            // Missing units:
+            throw new ElasticsearchParseException(
+                    "failed to parse setting [{}] with value [{}] as a size in bytes: unit is missing or unrecognized", settingName,
+                    sValue);
         }
         return new ByteSizeValue(bytes, ByteSizeUnit.BYTES);
+    }
+
+    private static double parse(final String initialInput, final String normalized, final String suffix, final String settingName) {
+        final String s = normalized.substring(0, normalized.length() - suffix.length()).trim();
+        try {
+            return Long.parseLong(s);
+        } catch (final NumberFormatException e) {
+            try {
+                final double doubleValue = Double.parseDouble(s);
+                DEPRECATION_LOGGER.deprecated(
+                        "Fractional bytes values are deprecated. Use non-fractional bytes values instead: [{}] found for setting [{}]",
+                        initialInput, settingName);
+                return doubleValue;
+            } catch (final NumberFormatException ignored) {
+                throw new ElasticsearchParseException("failed to parse [{}]", e, initialInput);
+            }
+        }
     }
 
     @Override
@@ -196,13 +243,13 @@ public class ByteSizeValue implements Writeable, Comparable<ByteSizeValue> {
 
     @Override
     public int hashCode() {
-        return Double.hashCode(((double) size) * unit.toBytes(1));
+        return Long.hashCode(size * unit.toBytes(1));
     }
 
     @Override
     public int compareTo(ByteSizeValue other) {
-        double thisValue = ((double) size) * unit.toBytes(1);
-        double otherValue = ((double) other.size) * other.unit.toBytes(1);
-        return Double.compare(thisValue, otherValue);
+        long thisValue = size * unit.toBytes(1);
+        long otherValue = other.size * other.unit.toBytes(1);
+        return Long.compare(thisValue, otherValue);
     }
 }

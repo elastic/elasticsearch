05e73d9844082fbe3afb4c748a1566ba456aebdf
Maxime Gréau <maxime.greau@elastic.co>
2017-12-07 15:56:41 +0100

    Build: Add 3rd party dependencies report generation
    * Adds task dependenciesInfo to BuildPlugin to generate a CSV file with dependencies information (name,version,url,license)
    * Adds `ConcatFilesTask.groovy` to concatenates multiple files into one
    * Adds task `:distribution:generateDependenciesReport` to concatenate `dependencies.csv` files into a single file (`es-dependencies.csv` by default)
    
     # Examples:
          $ gradle dependenciesInfo :distribution:generateDependenciesReport
    
     ## Use `csv` system property to customize the output file path
         $ gradle dependenciesInfo :distribution:generateDependenciesReport -Dcsv=/tmp/elasticsearch-dependencies.csv
    
     ## When branch is not master, use `build.branch` system property to generate correct licenses URLs
         $ gradle dependenciesInfo :distribution:generateDependenciesReport -Dbuild.branch=6.x -Dcsv=/tmp/elasticsearch-dependencies.csv

---
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/ConcatFilesTask.groovy
@@ -0,0 +1,70 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.gradle
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.FileTree
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+
+/**
+ * Concatenates a list of files into one.
+ */
+public class ConcatFilesTask extends DefaultTask {
+
+    /** List of files to concatenate */
+    @InputFiles
+    public FileTree files
+
+    /** line to add at the top of the target file */
+    @Input
+    @Optional
+    public String headerLine
+
+    @OutputFile
+    File target
+
+    public ConcatFilesTask() {
+        description = 'Concat a list of files into one.'
+    }
+
+    @TaskAction
+    public void concatFiles() {
+        final StringBuilder output = new StringBuilder()
+
+        if (headerLine) {
+            output.append(headerLine).append('\n')
+        }
+
+        final File globalFile = File.createTempFile('global', 'txt')
+        files.each {
+            globalFile.append(it.getText('UTF-8'))
+        }
+        final TreeSet<String> lines = globalFile.readLines('UTF-8').toSet()
+        for (String value : lines) {
+            output.append(value).append('\n')
+        }
+
+        target.setText(output.toString(), 'UTF-8')
+    }
+}

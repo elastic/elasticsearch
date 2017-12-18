b3524fe3b754c20d851f634dcf6b507053266af0
Jason Tedor <jason@tedor.me>
2017-12-18 17:50:10 -0500

    Filter current version from compatible versions
    We need to filter the current version from the list of compatible
    versions to match how we calculate the list of compatible versions in
    Gradle.

--- test/framework/src/test/java/org/elasticsearch/test/VersionUtilsTests.java
+++ test/framework/src/test/java/org/elasticsearch/test/VersionUtilsTests.java
@@ -306,6 +306,7 @@ public class VersionUtilsTests extends ESTestCase {
                 .filter(Version::isRelease)
                 .collect(toList());
         List<String> releasedIndexCompatible = released.stream()
+                .filter(v -> !Version.CURRENT.equals(v))
                 .map(Object::toString)
                 .collect(toList());
         assertEquals(releasedIndexCompatible, indexCompatible.released);
@@ -313,7 +314,7 @@ public class VersionUtilsTests extends ESTestCase {
         List<String> unreleasedIndexCompatible = VersionUtils.allUnreleasedVersions().stream()
                 /* Gradle skips the current version because being backwards compatible
                  * with yourself is implied. Java lists the version because it is useful. */
-                .filter(v -> v != Version.CURRENT)
+                .filter(v -> !Version.CURRENT.equals(v))
                 .map(Object::toString)
                 .collect(toList());
         assertEquals(unreleasedIndexCompatible, indexCompatible.unreleased);
@@ -330,6 +331,7 @@ public class VersionUtilsTests extends ESTestCase {
         // We'll fix this very, very soon. But for now, this hack.
         // end big horrible hack
         List<String> releasedWireCompatible = released.stream()
+                .filter(v -> !Version.CURRENT.equals(v))
                 .filter(v -> v.onOrAfter(minimumCompatibleVersion))
                 .map(Object::toString)
                 .collect(toList());
@@ -338,7 +340,7 @@ public class VersionUtilsTests extends ESTestCase {
         List<String> unreleasedWireCompatible = VersionUtils.allUnreleasedVersions().stream()
                 /* Gradle skips the current version because being backwards compatible
                  * with yourself is implied. Java lists the version because it is useful. */
-                .filter(v -> v != Version.CURRENT)
+                .filter(v -> !Version.CURRENT.equals(v))
                 .filter(v -> v.onOrAfter(minimumCompatibleVersion))
                 .map(Object::toString)
                 .collect(toList());

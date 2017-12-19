openeuler-mirror/poissonsearch-oss

Use port 0 InternalTestCluster nodes (#27859)
We currently have a complicated port assignment scheme to make sure that the nodes span off by the internal test cluster will be assigned fixed port ranges that will also not collide between clusters. The port ranges need to be fixed in advance so that the nodes will be able to find each other via `UnicastZenPing`.

This approach worked well for the last few years but we are now at a point that our testing has grown beyond it and we exceed the 5 reusable ranges per JVM. This means that nodes are not always assigned the first 5 ports in their range which causes cluster formation issues. On top of that, most of the clusters that are span up don't even rely on `UnicastZenPing` but rather `MockZenPings` that uses in memory maps for discovery (with the down side that they are not influenced by network disruption simulations).

This PR changes `InternalTestCluster` to use port 0 as a fixed assignment. This will allow the OS to manage ports and will ensure we don't have collisions. For tests that need to simulate network disruptions (and thus can't use `MockZenPings`), a new `UnicastHostProvider` is introduced that is based on the current state of the test cluster. Since that is only resolved at run time, it is aware of the port assignments of the OS.

Closes #27818
Closes #27762

test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
@@ -162,31 +162,8 @@ public final class InternalTestCluster extends TestCluster {
 
     private final Logger logger = Loggers.getLogger(getClass());
 
-    /**
-     * The number of ports in the range used for this JVM
-     */
-    public static final int PORTS_PER_JVM = 100;
-
-    /**
-     * The number of ports in the range used for this cluster
-     */
-    public static final int PORTS_PER_CLUSTER = 20;
-
-    private static final int GLOBAL_TRANSPORT_BASE_PORT = 9300;
-    private static final int GLOBAL_HTTP_BASE_PORT = 19200;
-
-    private static final int JVM_ORDINAL = Integer.parseInt(System.getProperty(SysGlobals.CHILDVM_SYSPROP_JVM_ID, "0"));
-
-    /**
-     * a per-JVM unique offset to be used for calculating unique port ranges.
-     */
-    public static final int JVM_BASE_PORT_OFFSET = PORTS_PER_JVM * (JVM_ORDINAL + 1);
 
     private static final AtomicInteger clusterOrdinal = new AtomicInteger();
-    private final int CLUSTER_BASE_PORT_OFFSET = JVM_BASE_PORT_OFFSET + (clusterOrdinal.getAndIncrement() * PORTS_PER_CLUSTER) % PORTS_PER_JVM;
-
-    public final int TRANSPORT_BASE_PORT = GLOBAL_TRANSPORT_BASE_PORT + CLUSTER_BASE_PORT_OFFSET;
-    public final int HTTP_BASE_PORT = GLOBAL_HTTP_BASE_PORT + CLUSTER_BASE_PORT_OFFSET;
 
 
     public static final int DEFAULT_LOW_NUM_MASTER_NODES = 1;
@@ -293,7 +270,11 @@ public final class InternalTestCluster extends TestCluster {
         this.nodePrefix = nodePrefix;
 
         assert nodePrefix != null;
+        ArrayList<Class<? extends Plugin>> tmpMockPlugins = new ArrayList<>(mockPlugins);
+
+
         this.mockPlugins = mockPlugins;
+
 
         sharedNodesSeeds = new long[numSharedDedicatedMasterNodes + numSharedDataNodes + numSharedCoordOnlyNodes];
         for (int i = 0; i < sharedNodesSeeds.length; i++) {
@@ -322,8 +303,8 @@ public final class InternalTestCluster extends TestCluster {
         builder.put(Environment.PATH_SHARED_DATA_SETTING.getKey(), baseDir.resolve("custom"));
         builder.put(Environment.PATH_HOME_SETTING.getKey(), baseDir);
         builder.put(Environment.PATH_REPO_SETTING.getKey(), baseDir.resolve("repos"));
-        builder.put(TcpTransport.PORT.getKey(), TRANSPORT_BASE_PORT + "-" + (TRANSPORT_BASE_PORT + PORTS_PER_CLUSTER));
-        builder.put("http.port", HTTP_BASE_PORT + "-" + (HTTP_BASE_PORT + PORTS_PER_CLUSTER));
+        builder.put(TcpTransport.PORT.getKey(), 0);
+        builder.put("http.port", 0);
         builder.put("http.pipelining", enableHttpPipelining);
         if (Strings.hasLength(System.getProperty("tests.es.logger.level"))) {
             builder.put("logger.level", System.getProperty("tests.es.logger.level"));

test/framework/src/main/java/org/elasticsearch/test/discovery/TestZenDiscovery.java
@@ -19,15 +19,13 @@
 
 package org.elasticsearch.test.discovery;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Supplier;
-
+import org.apache.lucene.util.SetOnce;
+import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.routing.allocation.AllocationService;
 import org.elasticsearch.cluster.service.ClusterApplier;
 import org.elasticsearch.cluster.service.MasterService;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.network.NetworkService;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
@@ -41,6 +39,14 @@ import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static org.elasticsearch.discovery.zen.UnicastZenPing.DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING;
+
 /**
  * A alternative zen discovery which allows using mocks for things like pings, as well as
  * giving access to internals.
@@ -53,9 +59,11 @@ public class TestZenDiscovery extends ZenDiscovery {
     /** A plugin which installs mock discovery and configures it to be used. */
     public static class TestPlugin extends Plugin implements DiscoveryPlugin {
         protected final Settings settings;
+        private final SetOnce<MockUncasedHostProvider> unicastHostProvider = new SetOnce<>();
         public TestPlugin(Settings settings) {
             this.settings = settings;
         }
+
         @Override
         public Map<String, Supplier<Discovery>> getDiscoveryTypes(ThreadPool threadPool, TransportService transportService,
                                                                   NamedWriteableRegistry namedWriteableRegistry,
@@ -63,8 +71,31 @@ public class TestZenDiscovery extends ZenDiscovery {
                                                                   ClusterSettings clusterSettings, UnicastHostsProvider hostsProvider,
                                                                   AllocationService allocationService) {
             return Collections.singletonMap("test-zen",
-                () -> new TestZenDiscovery(settings, threadPool, transportService, namedWriteableRegistry, masterService,
+                () -> new TestZenDiscovery(
+                    // we don't get the latest setting which were updated by the extra settings for the plugin. TODO: fix.
+                    Settings.builder().put(settings).putList(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey()).build(),
+                    threadPool, transportService, namedWriteableRegistry, masterService,
                     clusterApplier, clusterSettings, hostsProvider, allocationService));
+        }
+
+        @Override
+        public Map<String, Supplier<UnicastHostsProvider>> getZenHostsProviders(TransportService transportService,
+                                                                                NetworkService networkService) {
+            final Supplier<UnicastHostsProvider> supplier;
+            if (USE_MOCK_PINGS.get(settings)) {
+                // we have to return something in order for the unicast host provider setting to resolve to something. It will never be used
+                supplier = () -> () -> {
+                    throw new UnsupportedOperationException();
+                };
+            } else {
+                supplier = () -> {
+                    unicastHostProvider.set(
+                        new MockUncasedHostProvider(transportService::getLocalNode, ClusterName.CLUSTER_NAME_SETTING.get(settings))
+                    );
+                    return unicastHostProvider.get();
+                };
+            }
+            return Collections.singletonMap("test-zen", supplier);
         }
 
         @Override
@@ -74,7 +105,19 @@ public class TestZenDiscovery extends ZenDiscovery {
 
         @Override
         public Settings additionalSettings() {
-            return Settings.builder().put(DiscoveryModule.DISCOVERY_TYPE_SETTING.getKey(), "test-zen").build();
+            return Settings.builder()
+                .put(DiscoveryModule.DISCOVERY_TYPE_SETTING.getKey(), "test-zen")
+                .put(DiscoveryModule.DISCOVERY_HOSTS_PROVIDER_SETTING.getKey(), "test-zen")
+                .putList(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey())
+                .build();
+        }
+
+        @Override
+        public void close() throws IOException {
+            super.close();
+            if (unicastHostProvider.get() != null) {
+                unicastHostProvider.get().close();
+            }
         }
     }
 

elastic/elasticsearch

test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
@@ -162,31 +162,8 @@ public final class InternalTestCluster extends TestCluster {
 
     private final Logger logger = Loggers.getLogger(getClass());
 
-    /**
-     * The number of ports in the range used for this JVM
-     */
-    public static final int PORTS_PER_JVM = 100;
-
-    /**
-     * The number of ports in the range used for this cluster
-     */
-    public static final int PORTS_PER_CLUSTER = 20;
-
-    private static final int GLOBAL_TRANSPORT_BASE_PORT = 9300;
-    private static final int GLOBAL_HTTP_BASE_PORT = 19200;
-
-    private static final int JVM_ORDINAL = Integer.parseInt(System.getProperty(SysGlobals.CHILDVM_SYSPROP_JVM_ID, "0"));
-
-    /**
-     * a per-JVM unique offset to be used for calculating unique port ranges.
-     */
-    public static final int JVM_BASE_PORT_OFFSET = PORTS_PER_JVM * (JVM_ORDINAL + 1);
 
     private static final AtomicInteger clusterOrdinal = new AtomicInteger();
-    private final int CLUSTER_BASE_PORT_OFFSET = JVM_BASE_PORT_OFFSET + (clusterOrdinal.getAndIncrement() * PORTS_PER_CLUSTER) % PORTS_PER_JVM;
-
-    public final int TRANSPORT_BASE_PORT = GLOBAL_TRANSPORT_BASE_PORT + CLUSTER_BASE_PORT_OFFSET;
-    public final int HTTP_BASE_PORT = GLOBAL_HTTP_BASE_PORT + CLUSTER_BASE_PORT_OFFSET;
 
 
     public static final int DEFAULT_LOW_NUM_MASTER_NODES = 1;
@@ -322,8 +299,8 @@ public final class InternalTestCluster extends TestCluster {
         builder.put(Environment.PATH_SHARED_DATA_SETTING.getKey(), baseDir.resolve("custom"));
         builder.put(Environment.PATH_HOME_SETTING.getKey(), baseDir);
         builder.put(Environment.PATH_REPO_SETTING.getKey(), baseDir.resolve("repos"));
-        builder.put(TransportSettings.PORT.getKey(), TRANSPORT_BASE_PORT + "-" + (TRANSPORT_BASE_PORT + PORTS_PER_CLUSTER));
-        builder.put("http.port", HTTP_BASE_PORT + "-" + (HTTP_BASE_PORT + PORTS_PER_CLUSTER));
+        builder.put(TransportSettings.PORT.getKey(), 0);
+        builder.put("http.port", 0);
         builder.put("http.pipelining", enableHttpPipelining);
         if (Strings.hasLength(System.getProperty("tests.es.logger.level"))) {
             builder.put("logger.level", System.getProperty("tests.es.logger.level"));

test/framework/src/main/java/org/elasticsearch/test/discovery/TestZenDiscovery.java
@@ -19,13 +19,11 @@
 
 package org.elasticsearch.test.discovery;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Supplier;
-
+import org.apache.lucene.util.SetOnce;
+import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.network.NetworkService;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.discovery.Discovery;
@@ -37,6 +35,14 @@ import org.elasticsearch.plugins.DiscoveryPlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static org.elasticsearch.discovery.zen.UnicastZenPing.DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING;
 
 /**
  * A alternative zen discovery which allows using mocks for things like pings, as well as
@@ -50,15 +56,40 @@ public class TestZenDiscovery extends ZenDiscovery {
     /** A plugin which installs mock discovery and configures it to be used. */
     public static class TestPlugin extends Plugin implements DiscoveryPlugin {
         private Settings settings;
+        private final SetOnce<MockUncasedHostProvider> unicastHostProvider = new SetOnce<>();
         public TestPlugin(Settings settings) {
             this.settings = settings;
         }
+
         @Override
         public Map<String, Supplier<Discovery>> getDiscoveryTypes(ThreadPool threadPool, TransportService transportService,
                                                                   NamedWriteableRegistry namedWriteableRegistry,
                                                                   ClusterService clusterService, UnicastHostsProvider hostsProvider) {
             return Collections.singletonMap("test-zen",
-                () -> new TestZenDiscovery(settings, threadPool, transportService, namedWriteableRegistry, clusterService, hostsProvider));
+                () -> new TestZenDiscovery(
+                    // we don't get the latest setting which were updated by the extra settings for the plugin. TODO: fix.
+                    Settings.builder().put(settings).putArray(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey()).build(),
+                    threadPool, transportService, namedWriteableRegistry, clusterService, hostsProvider));
+        }
+
+        @Override
+        public Map<String, Supplier<UnicastHostsProvider>> getZenHostsProviders(TransportService transportService,
+                                                                                NetworkService networkService) {
+            final Supplier<UnicastHostsProvider> supplier;
+            if (USE_MOCK_PINGS.get(settings)) {
+                // we have to return something in order for the unicast host provider setting to resolve to something. It will never be used
+                supplier = () -> () -> {
+                    throw new UnsupportedOperationException();
+                };
+            } else {
+                supplier = () -> {
+                    unicastHostProvider.set(
+                        new MockUncasedHostProvider(transportService::getLocalNode, ClusterName.CLUSTER_NAME_SETTING.get(settings))
+                    );
+                    return unicastHostProvider.get();
+                };
+            }
+            return Collections.singletonMap("test-zen", supplier);
         }
 
         @Override
@@ -68,7 +99,19 @@ public class TestZenDiscovery extends ZenDiscovery {
 
         @Override
         public Settings additionalSettings() {
-            return Settings.builder().put(DiscoveryModule.DISCOVERY_TYPE_SETTING.getKey(), "test-zen").build();
+            return Settings.builder()
+                .put(DiscoveryModule.DISCOVERY_TYPE_SETTING.getKey(), "test-zen")
+                .put(DiscoveryModule.DISCOVERY_HOSTS_PROVIDER_SETTING.getKey(), "test-zen")
+                .putArray(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey())
+                .build();
+        }
+
+        @Override
+        public void close() throws IOException {
+            super.close();
+            if (unicastHostProvider.get() != null) {
+                unicastHostProvider.get().close();
+            }
         }
     }
 
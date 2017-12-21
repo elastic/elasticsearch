b2fefa4bc117e1c00aab497f3de46f6a10208079
Hendrik Muhs <hendrik.muhs@elastic.co>
2017-12-21 07:47:32 +0100

    [ML] allow forecast only on post 6.1 jobs (elastic/x-pack-elasticsearch#3362)
    allow forecast only on post 6.1 jobs
    
    discuss issue: elastic/machine-learning-cpp#494
    
    relates elastic/x-pack-elasticsearch#3219
    
    Original commit: elastic/x-pack-elasticsearch@ed846942250eabb9a8393c0f04c4e34e3fc8692f

--- plugin/src/test/java/org/elasticsearch/xpack/ml/action/ForecastJobActionRequestTests.java
+++ plugin/src/test/java/org/elasticsearch/xpack/ml/action/ForecastJobActionRequestTests.java
@@ -5,10 +5,19 @@
  */
 package org.elasticsearch.xpack.ml.action;
 
+import org.elasticsearch.ElasticsearchStatusException;
+import org.elasticsearch.Version;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.test.AbstractStreamableXContentTestCase;
 import org.elasticsearch.xpack.ml.action.ForecastJobAction.Request;
+import org.elasticsearch.xpack.ml.job.config.AnalysisConfig;
+import org.elasticsearch.xpack.ml.job.config.DataDescription;
+import org.elasticsearch.xpack.ml.job.config.Detector;
+import org.elasticsearch.xpack.ml.job.config.Job;
+
+import java.util.Collections;
+import java.util.Date;
 
 import static org.hamcrest.Matchers.equalTo;
 
@@ -60,5 +69,52 @@ public class ForecastJobActionRequestTests extends AbstractStreamableXContentTes
     public void testSetExpiresIn_GivenNegative() {
         IllegalArgumentException e = expectThrows(IllegalArgumentException.class, () -> new Request().setExpiresIn("-1s"));
         assertThat(e.getMessage(), equalTo("[expires_in] must be non-negative: [-1]"));
+    }
+
+    public void testValidate_jobVersionCannonBeBefore61() {
+        Job.Builder jobBuilder = createTestJob("forecast-it-test-job-version");
+
+        jobBuilder.setJobVersion(Version.V_6_0_1);
+        ForecastJobAction.Request request = new ForecastJobAction.Request();
+        Exception e = expectThrows(ElasticsearchStatusException.class,
+                () -> ForecastJobAction.TransportAction.validate(jobBuilder.build(), request));
+        assertEquals(
+                "Cannot run forecast because jobs created prior to version 6.1 are not supported",
+                e.getMessage());
+    }
+
+    public void testValidate_jobVersionCannonBeBefore61NoJobVersion() {
+        Job.Builder jobBuilder = createTestJob("forecast-it-test-job-version");
+
+        ForecastJobAction.Request request = new ForecastJobAction.Request();
+        Exception e = expectThrows(ElasticsearchStatusException.class,
+                () -> ForecastJobAction.TransportAction.validate(jobBuilder.build(), request));
+        assertEquals(
+                "Cannot run forecast because jobs created prior to version 6.1 are not supported",
+                e.getMessage());
+    }
+
+    public void testValidate_DurationCannotBeLessThanBucketSpan() {
+        Job.Builder jobBuilder = createTestJob("forecast-it-test-job-version");
+
+        ForecastJobAction.Request request = new ForecastJobAction.Request();
+        request.setDuration(TimeValue.timeValueMinutes(1));
+        Exception e = expectThrows(ElasticsearchStatusException.class,
+                () -> ForecastJobAction.TransportAction.validate(jobBuilder.build(new Date()), request));
+        assertEquals("[duration] must be greater or equal to the bucket span: [1m/1h]", e.getMessage());
+    }
+
+    private Job.Builder createTestJob(String jobId) {
+        Job.Builder jobBuilder = new Job.Builder(jobId);
+        jobBuilder.setCreateTime(new Date());
+        Detector.Builder detector = new Detector.Builder("mean", "value");
+        AnalysisConfig.Builder analysisConfig = new AnalysisConfig.Builder(Collections.singletonList(detector.build()));
+        TimeValue bucketSpan = TimeValue.timeValueHours(1);
+        analysisConfig.setBucketSpan(bucketSpan);
+        DataDescription.Builder dataDescription = new DataDescription.Builder();
+        dataDescription.setTimeFormat("epoch");
+        jobBuilder.setAnalysisConfig(analysisConfig);
+        jobBuilder.setDataDescription(dataDescription);
+        return jobBuilder;
     }
 }

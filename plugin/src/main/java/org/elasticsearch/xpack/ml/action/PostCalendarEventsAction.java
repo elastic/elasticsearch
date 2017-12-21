0950a903f27ffdad23465756e200a7e9d9d0d717
David Kyle <david.kyle@elastic.co>
2017-12-21 14:05:54 +0000

    [ML] Get events for job and from all calendars (elastic/x-pack-elasticsearch#3401)
    * Add get events for job and from all calendars
    
    Original commit: elastic/x-pack-elasticsearch@8029ffb8980d1db0a5a28ebffae39700afd51486

--- plugin/src/main/java/org/elasticsearch/xpack/ml/action/PostCalendarEventsAction.java
+++ plugin/src/main/java/org/elasticsearch/xpack/ml/action/PostCalendarEventsAction.java
@@ -5,7 +5,6 @@
  */
 package org.elasticsearch.xpack.ml.action;
 
-import org.elasticsearch.ResourceNotFoundException;
 import org.elasticsearch.action.Action;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.ActionRequest;

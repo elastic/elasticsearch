elastic/elasticsearch

commit a5e8a221ecf9cbe519049770768218d1626af204

Move GlobalCheckpointTracker and remove SequenceNumbersService (#27837)
This commit moves GlobalCheckpointTracker from the engine to IndexShard, where it better fits logically: Tracking the global checkpoint based on the local checkpoints of all shards in the replication group is not a property of the engine, but rather a property fulfilled by the current primary shard. The LocalCheckpointTracker on the other hand is driven by the contents of the local translog. By moving GlobalCheckpointTracker to IndexShard, it makes little sense to keep the SequenceNumbersService class around - it would only wrap the LocalCheckpointTracker. This commit therefore removes the class and replaces occurrences of SequenceNumbersService in the engine directly by LocalCheckpointTracker.

core/src/main/java/org/elasticsearch/index/translog/TranslogWriter.java
@@ -119,8 +119,8 @@ public class TranslogWriter extends BaseTranslogReader implements Closeable {
     }
 
     public static TranslogWriter create(ShardId shardId, String translogUUID, long fileGeneration, Path file, ChannelFactory channelFactory,
-                                        ByteSizeValue bufferSize, final LongSupplier globalCheckpointSupplier,
-                                        final long initialMinTranslogGen, final LongSupplier minTranslogGenerationSupplier)
+                                        ByteSizeValue bufferSize, final long initialMinTranslogGen, long initialGlobalCheckpoint,
+                                        final LongSupplier globalCheckpointSupplier, final LongSupplier minTranslogGenerationSupplier)
         throws IOException {
         final BytesRef ref = new BytesRef(translogUUID);
         final int firstOperationOffset = getHeaderLength(ref.length);
@@ -132,10 +132,21 @@ public class TranslogWriter extends BaseTranslogReader implements Closeable {
             writeHeader(out, ref);
             channel.force(true);
             final Checkpoint checkpoint = Checkpoint.emptyTranslogCheckpoint(firstOperationOffset, fileGeneration,
-                globalCheckpointSupplier.getAsLong(), initialMinTranslogGen);
+                initialGlobalCheckpoint, initialMinTranslogGen);
             writeCheckpoint(channelFactory, file.getParent(), checkpoint);
+            final LongSupplier writerGlobalCheckpointSupplier;
+            if (Assertions.ENABLED) {
+                writerGlobalCheckpointSupplier = () -> {
+                    long gcp = globalCheckpointSupplier.getAsLong();
+                    assert gcp >= initialGlobalCheckpoint :
+                        "global checkpoint [" + gcp + "] lower than initial gcp [" + initialGlobalCheckpoint + "]";
+                    return gcp;
+                };
+            } else {
+                writerGlobalCheckpointSupplier = globalCheckpointSupplier;
+            }
             return new TranslogWriter(channelFactory, shardId, checkpoint, channel, file, bufferSize,
-                globalCheckpointSupplier, minTranslogGenerationSupplier);
+                writerGlobalCheckpointSupplier, minTranslogGenerationSupplier);
         } catch (Exception exception) {
             // if we fail to bake the file-generation into the checkpoint we stick with the file and once we recover and that
             // file exists we remove it. We only apply this logic to the checkpoint.generation+1 any other file with a higher generation is an error condition

core/src/main/java/org/elasticsearch/action/support/replication/ReplicationOperation.java
@@ -32,7 +32,6 @@ import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.util.set.Sets;
-import org.elasticsearch.index.seqno.SequenceNumbersService;
 import org.elasticsearch.index.shard.ReplicationGroup;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.rest.RestStatus;
@@ -400,14 +399,14 @@ public class ReplicationOperation<
     public interface ReplicaResponse {
 
         /**
-         * The local checkpoint for the shard. See {@link SequenceNumbersService#getLocalCheckpoint()}.
+         * The local checkpoint for the shard.
          *
          * @return the local checkpoint
          **/
         long localCheckpoint();
 
         /**
-         * The global checkpoint for the shard. See {@link SequenceNumbersService#getGlobalCheckpoint()}.
+         * The global checkpoint for the shard.
          *
          * @return the global checkpoint
          **/

core/src/main/java/org/elasticsearch/index/engine/Engine.java
@@ -64,8 +64,8 @@ import org.elasticsearch.index.VersionType;
 import org.elasticsearch.index.mapper.ParseContext.Document;
 import org.elasticsearch.index.mapper.ParsedDocument;
 import org.elasticsearch.index.merge.MergeStats;
+import org.elasticsearch.index.seqno.LocalCheckpointTracker;
 import org.elasticsearch.index.seqno.SequenceNumbers;
-import org.elasticsearch.index.seqno.SequenceNumbersService;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.index.store.Store;
 import org.elasticsearch.index.translog.Translog;
@@ -567,7 +567,7 @@ public abstract class Engine implements Closeable {
      *
      * @return the sequence number service
      */
-    public abstract SequenceNumbersService seqNoService();
+    public abstract LocalCheckpointTracker getLocalCheckpointTracker();
 
     /**
      * Read the last segments info from the commit pointed to by the searcher manager

core/src/main/java/org/elasticsearch/index/engine/EngineConfig.java
@@ -44,6 +44,7 @@ import org.elasticsearch.threadpool.ThreadPool;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.function.LongSupplier;
 
 /*
  * Holds all the configuration that is used to create an {@link Engine}.
@@ -78,6 +79,7 @@ public final class EngineConfig {
     private final TranslogRecoveryRunner translogRecoveryRunner;
     @Nullable
     private final CircuitBreakerService circuitBreakerService;
+    private final LongSupplier globalCheckpointSupplier;
 
     /**
      * Index setting to change the low level lucene codec used for writing new segments.
@@ -124,7 +126,8 @@ public final class EngineConfig {
                         boolean forceNewHistoryUUID, TranslogConfig translogConfig, TimeValue flushMergesAfter,
                         List<ReferenceManager.RefreshListener> externalRefreshListener,
                         List<ReferenceManager.RefreshListener> internalRefreshListener, Sort indexSort,
-                        TranslogRecoveryRunner translogRecoveryRunner, CircuitBreakerService circuitBreakerService) {
+                        TranslogRecoveryRunner translogRecoveryRunner, CircuitBreakerService circuitBreakerService,
+                        LongSupplier globalCheckpointSupplier) {
         if (openMode == null) {
             throw new IllegalArgumentException("openMode must not be null");
         }
@@ -155,6 +158,7 @@ public final class EngineConfig {
         this.indexSort = indexSort;
         this.translogRecoveryRunner = translogRecoveryRunner;
         this.circuitBreakerService = circuitBreakerService;
+        this.globalCheckpointSupplier = globalCheckpointSupplier;
     }
 
     /**
@@ -225,6 +229,13 @@ public final class EngineConfig {
      */
     public Store getStore() {
         return store;
+    }
+
+    /**
+     * Returns the global checkpoint tracker
+     */
+    public LongSupplier getGlobalCheckpointSupplier() {
+        return globalCheckpointSupplier;
     }
 
     /**

core/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -49,6 +49,7 @@ import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.SuppressForbidden;
 import org.elasticsearch.common.UUIDs;
+import org.elasticsearch.common.collect.Tuple;
 import org.elasticsearch.common.lease.Releasable;
 import org.elasticsearch.common.lucene.LoggerInfoStream;
 import org.elasticsearch.common.lucene.Lucene;
@@ -67,9 +68,8 @@ import org.elasticsearch.index.mapper.ParseContext;
 import org.elasticsearch.index.mapper.UidFieldMapper;
 import org.elasticsearch.index.merge.MergeStats;
 import org.elasticsearch.index.merge.OnGoingMerge;
-import org.elasticsearch.index.seqno.SeqNoStats;
+import org.elasticsearch.index.seqno.LocalCheckpointTracker;
 import org.elasticsearch.index.seqno.SequenceNumbers;
-import org.elasticsearch.index.seqno.SequenceNumbersService;
 import org.elasticsearch.index.shard.ElasticsearchMergePolicy;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.index.translog.Translog;
@@ -123,7 +123,7 @@ public class InternalEngine extends Engine {
 
     private final IndexThrottle throttle;
 
-    private final SequenceNumbersService seqNoService;
+    private final LocalCheckpointTracker localCheckpointTracker;
 
     private final String uidField;
 
@@ -150,12 +150,12 @@ public class InternalEngine extends Engine {
     private final String historyUUID;
 
     public InternalEngine(EngineConfig engineConfig) {
-        this(engineConfig, InternalEngine::sequenceNumberService);
+        this(engineConfig, LocalCheckpointTracker::new);
     }
 
     InternalEngine(
             final EngineConfig engineConfig,
-            final BiFunction<EngineConfig, SeqNoStats, SequenceNumbersService> seqNoServiceSupplier) {
+            final BiFunction<Long, Long, LocalCheckpointTracker> localCheckpointTrackerSupplier) {
         super(engineConfig);
         openMode = engineConfig.getOpenMode();
         if (engineConfig.isAutoGeneratedIDsOptimizationEnabled() == false) {
@@ -179,14 +179,12 @@ public class InternalEngine extends Engine {
             mergeScheduler = scheduler = new EngineMergeScheduler(engineConfig.getShardId(), engineConfig.getIndexSettings());
             throttle = new IndexThrottle();
             try {
-                final SeqNoStats seqNoStats = loadSeqNoStats(openMode);
-                logger.trace("recovered [{}]", seqNoStats);
-                this.seqNoService = seqNoServiceSupplier.apply(engineConfig, seqNoStats);
-                translog = openTranslog(engineConfig, translogDeletionPolicy, seqNoService::getGlobalCheckpoint);
+                this.localCheckpointTracker = createLocalCheckpointTracker(localCheckpointTrackerSupplier);
+                translog = openTranslog(engineConfig, translogDeletionPolicy, engineConfig.getGlobalCheckpointSupplier());
                 assert translog.getGeneration() != null;
                 this.translog = translog;
                 this.snapshotDeletionPolicy = new SnapshotDeletionPolicy(
-                    new CombinedDeletionPolicy(openMode, translogDeletionPolicy, translog::getLastSyncedGlobalCheckpoint)
+                    new CombinedDeletionPolicy(openMode, translogDeletionPolicy, engineConfig.getGlobalCheckpointSupplier())
                 );
                 writer = createWriter(openMode == EngineConfig.OpenMode.CREATE_INDEX_AND_TRANSLOG);
                 updateMaxUnsafeAutoIdTimestampFromWriter(writer);
@@ -235,6 +233,27 @@ public class InternalEngine extends Engine {
             }
         }
         logger.trace("created new InternalEngine");
+    }
+
+    private LocalCheckpointTracker createLocalCheckpointTracker(
+        BiFunction<Long, Long, LocalCheckpointTracker> localCheckpointTrackerSupplier) throws IOException {
+        final long maxSeqNo;
+        final long localCheckpoint;
+        switch (openMode) {
+            case CREATE_INDEX_AND_TRANSLOG:
+                maxSeqNo = SequenceNumbers.NO_OPS_PERFORMED;
+                localCheckpoint = SequenceNumbers.NO_OPS_PERFORMED;
+                break;
+            case OPEN_INDEX_AND_TRANSLOG:
+            case OPEN_INDEX_CREATE_TRANSLOG:
+                final Tuple<Long, Long> seqNoStats = store.loadSeqNoInfo();
+                maxSeqNo = seqNoStats.v1();
+                localCheckpoint = seqNoStats.v2();
+                logger.trace("recovered maximum sequence number [{}] and local checkpoint [{}]", maxSeqNo, localCheckpoint);
+                break;
+            default: throw new IllegalArgumentException("unknown type: " + openMode);
+        }
+        return localCheckpointTrackerSupplier.apply(maxSeqNo, localCheckpoint);
     }
 
     /**
@@ -310,12 +329,12 @@ public class InternalEngine extends Engine {
     public void restoreLocalCheckpointFromTranslog() throws IOException {
         try (ReleasableLock ignored = writeLock.acquire()) {
             ensureOpen();
-            final long localCheckpoint = seqNoService.getLocalCheckpoint();
+            final long localCheckpoint = localCheckpointTracker.getCheckpoint();
             try (Translog.Snapshot snapshot = getTranslog().newSnapshotFromMinSeqNo(localCheckpoint + 1)) {
                 Translog.Operation operation;
                 while ((operation = snapshot.next()) != null) {
                     if (operation.seqNo() > localCheckpoint) {
-                        seqNoService.markSeqNoAsCompleted(operation.seqNo());
+                        localCheckpointTracker.markSeqNoAsCompleted(operation.seqNo());
                     }
                 }
             }
@@ -326,17 +345,17 @@ public class InternalEngine extends Engine {
     public int fillSeqNoGaps(long primaryTerm) throws IOException {
         try (ReleasableLock ignored = writeLock.acquire()) {
             ensureOpen();
-            final long localCheckpoint = seqNoService.getLocalCheckpoint();
-            final long maxSeqNo = seqNoService.getMaxSeqNo();
+            final long localCheckpoint = localCheckpointTracker.getCheckpoint();
+            final long maxSeqNo = localCheckpointTracker.getMaxSeqNo();
             int numNoOpsAdded = 0;
             for (
                     long seqNo = localCheckpoint + 1;
                     seqNo <= maxSeqNo;
-                    seqNo = seqNoService.getLocalCheckpoint() + 1 /* the local checkpoint might have advanced so we leap-frog */) {
+                    seqNo = localCheckpointTracker.getCheckpoint() + 1 /* the local checkpoint might have advanced so we leap-frog */) {
                 innerNoOp(new NoOp(seqNo, primaryTerm, Operation.Origin.PRIMARY, System.nanoTime(), "filling gaps"));
                 numNoOpsAdded++;
-                assert seqNo <= seqNoService.getLocalCheckpoint()
-                        : "local checkpoint did not advance; was [" + seqNo + "], now [" + seqNoService.getLocalCheckpoint() + "]";
+                assert seqNo <= localCheckpointTracker.getCheckpoint()
+                        : "local checkpoint did not advance; was [" + seqNo + "], now [" + localCheckpointTracker.getCheckpoint() + "]";
 
             }
             return numNoOpsAdded;
@@ -352,35 +371,6 @@ public class InternalEngine extends Engine {
             }
         }
         maxUnsafeAutoIdTimestamp.set(Math.max(maxUnsafeAutoIdTimestamp.get(), commitMaxUnsafeAutoIdTimestamp));
-    }
-
-    static SequenceNumbersService sequenceNumberService(
-        final EngineConfig engineConfig,
-        final SeqNoStats seqNoStats) {
-        return new SequenceNumbersService(
-            engineConfig.getShardId(),
-            engineConfig.getAllocationId(),
-            engineConfig.getIndexSettings(),
-            seqNoStats.getMaxSeqNo(),
-            seqNoStats.getLocalCheckpoint(),
-            seqNoStats.getGlobalCheckpoint());
-    }
-
-    private SeqNoStats loadSeqNoStats(EngineConfig.OpenMode openMode) throws IOException {
-        switch (openMode) {
-            case OPEN_INDEX_AND_TRANSLOG:
-                final long globalCheckpoint = Translog.readGlobalCheckpoint(engineConfig.getTranslogConfig().getTranslogPath());
-                return store.loadSeqNoStats(globalCheckpoint);
-            case OPEN_INDEX_CREATE_TRANSLOG:
-                return store.loadSeqNoStats(SequenceNumbers.UNASSIGNED_SEQ_NO);
-            case CREATE_INDEX_AND_TRANSLOG:
-                return new SeqNoStats(
-                    SequenceNumbers.NO_OPS_PERFORMED,
-                    SequenceNumbers.NO_OPS_PERFORMED,
-                    SequenceNumbers.UNASSIGNED_SEQ_NO);
-            default:
-                throw new IllegalArgumentException(openMode.toString());
-        }
     }
 
     @Override
@@ -732,7 +722,7 @@ public class InternalEngine extends Engine {
      * @return the sequence number
      */
     protected long doGenerateSeqNoForOperation(final Operation operation) {
-        return seqNoService.generateSeqNo();
+        return localCheckpointTracker.generateSeqNo();
     }
 
     @Override
@@ -803,7 +793,7 @@ public class InternalEngine extends Engine {
                     indexResult.setTranslogLocation(location);
                 }
                 if (indexResult.getSeqNo() != SequenceNumbers.UNASSIGNED_SEQ_NO) {
-                    seqNoService.markSeqNoAsCompleted(indexResult.getSeqNo());
+                    localCheckpointTracker.markSeqNoAsCompleted(indexResult.getSeqNo());
                 }
                 indexResult.setTook(System.nanoTime() - index.startTime());
                 indexResult.freeze();
@@ -834,7 +824,7 @@ public class InternalEngine extends Engine {
             // this allows to ignore the case where a document was found in the live version maps in
             // a delete state and return false for the created flag in favor of code simplicity
             final OpVsLuceneDocStatus opVsLucene;
-            if (index.seqNo() <= seqNoService.getLocalCheckpoint()){
+            if (index.seqNo() <= localCheckpointTracker.getCheckpoint()){
                 // the operation seq# is lower then the current local checkpoint and thus was already put into lucene
                 // this can happen during recovery where older operations are sent from the translog that are already
                 // part of the lucene commit (either from a peer recovery or a local translog)
@@ -1100,7 +1090,7 @@ public class InternalEngine extends Engine {
                 deleteResult.setTranslogLocation(location);
             }
             if (deleteResult.getSeqNo() != SequenceNumbers.UNASSIGNED_SEQ_NO) {
-                seqNoService.markSeqNoAsCompleted(deleteResult.getSeqNo());
+                localCheckpointTracker.markSeqNoAsCompleted(deleteResult.getSeqNo());
             }
             deleteResult.setTook(System.nanoTime() - delete.startTime());
             deleteResult.freeze();
@@ -1126,7 +1116,7 @@ public class InternalEngine extends Engine {
         // this allows to ignore the case where a document was found in the live version maps in
         // a delete state and return true for the found flag in favor of code simplicity
         final OpVsLuceneDocStatus opVsLucene;
-        if (delete.seqNo() <= seqNoService.getLocalCheckpoint()) {
+        if (delete.seqNo() <= localCheckpointTracker.getCheckpoint()) {
             // the operation seq# is lower then the current local checkpoint and thus was already put into lucene
             // this can happen during recovery where older operations are sent from the translog that are already
             // part of the lucene commit (either from a peer recovery or a local translog)
@@ -1273,7 +1263,7 @@ public class InternalEngine extends Engine {
             return noOpResult;
         } finally {
             if (seqNo != SequenceNumbers.UNASSIGNED_SEQ_NO) {
-                seqNoService.markSeqNoAsCompleted(seqNo);
+                localCheckpointTracker.markSeqNoAsCompleted(seqNo);
             }
         }
     }
@@ -2017,7 +2007,7 @@ public class InternalEngine extends Engine {
     protected void commitIndexWriter(final IndexWriter writer, final Translog translog, @Nullable final String syncId) throws IOException {
         ensureCanFlush();
         try {
-            final long localCheckpoint = seqNoService.getLocalCheckpoint();
+            final long localCheckpoint = localCheckpointTracker.getCheckpoint();
             final Translog.TranslogGeneration translogGeneration = translog.getMinGenerationForSeqNo(localCheckpoint + 1);
             final String translogFileGeneration = Long.toString(translogGeneration.translogFileGeneration);
             final String translogUUID = translogGeneration.translogUUID;
@@ -2040,7 +2030,7 @@ public class InternalEngine extends Engine {
                 if (syncId != null) {
                     commitData.put(Engine.SYNC_COMMIT_ID, syncId);
                 }
-                commitData.put(SequenceNumbers.MAX_SEQ_NO, Long.toString(seqNoService.getMaxSeqNo()));
+                commitData.put(SequenceNumbers.MAX_SEQ_NO, Long.toString(localCheckpointTracker.getMaxSeqNo()));
                 commitData.put(MAX_UNSAFE_AUTO_ID_TIMESTAMP_COMMIT_ID, Long.toString(maxUnsafeAutoIdTimestamp.get()));
                 commitData.put(HISTORY_UUID_KEY, historyUUID);
                 logger.trace("committing writer with commit data [{}]", commitData);
@@ -2104,8 +2094,8 @@ public class InternalEngine extends Engine {
         return mergeScheduler.stats();
     }
 
-    public final SequenceNumbersService seqNoService() {
-        return seqNoService;
+    public final LocalCheckpointTracker getLocalCheckpointTracker() {
+        return localCheckpointTracker;
     }
 
     /**

core/src/main/java/org/elasticsearch/index/seqno/GlobalCheckpointTracker.java
@@ -42,6 +42,7 @@ import java.util.OptionalLong;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.LongConsumer;
+import java.util.function.LongSupplier;
 import java.util.function.ToLongFunction;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
@@ -55,7 +56,7 @@ import java.util.stream.LongStream;
  * <p>
  * The global checkpoint is maintained by the primary shard and is replicated to all the replicas (via {@link GlobalCheckpointSyncAction}).
  */
-public class GlobalCheckpointTracker extends AbstractIndexShardComponent {
+public class GlobalCheckpointTracker extends AbstractIndexShardComponent implements LongSupplier {
 
     /**
      * The allocation ID for the shard to which this tracker is a component of.
@@ -214,7 +215,7 @@ public class GlobalCheckpointTracker extends AbstractIndexShardComponent {
      *
      * @return a map from allocation ID to the local knowledge of the global checkpoint for that allocation ID
      */
-    synchronized ObjectLongMap<String> getInSyncGlobalCheckpoints() {
+    public synchronized ObjectLongMap<String> getInSyncGlobalCheckpoints() {
         assert primaryMode;
         assert handoffInProgress == false;
         final ObjectLongMap<String> globalCheckpoints = new ObjectLongHashMap<>(checkpoints.size()); // upper bound on the size
@@ -329,7 +330,7 @@ public class GlobalCheckpointTracker extends AbstractIndexShardComponent {
      * @param indexSettings    the index settings
      * @param globalCheckpoint the last known global checkpoint for this shard, or {@link SequenceNumbers#UNASSIGNED_SEQ_NO}
      */
-    GlobalCheckpointTracker(
+    public GlobalCheckpointTracker(
             final ShardId shardId,
             final String allocationId,
             final IndexSettings indexSettings,
@@ -372,6 +373,11 @@ public class GlobalCheckpointTracker extends AbstractIndexShardComponent {
         final CheckpointState cps = checkpoints.get(shardAllocationId);
         assert cps != null;
         return cps.globalCheckpoint;
+    }
+
+    @Override
+    public long getAsLong() {
+        return getGlobalCheckpoint();
     }
 
     /**

core/src/main/java/org/elasticsearch/index/seqno/LocalCheckpointTracker.java
@@ -78,7 +78,7 @@ public class LocalCheckpointTracker {
      *
      * @return the next assigned sequence number
      */
-    synchronized long generateSeqNo() {
+    public synchronized long generateSeqNo() {
         return nextSeqNo++;
     }
 
@@ -109,7 +109,7 @@ public class LocalCheckpointTracker {
      *
      * @param checkpoint the local checkpoint to reset this tracker to
      */
-    synchronized void resetCheckpoint(final long checkpoint) {
+    public synchronized void resetCheckpoint(final long checkpoint) {
         assert checkpoint != SequenceNumbers.UNASSIGNED_SEQ_NO;
         assert checkpoint <= this.checkpoint;
         processedSeqNo.clear();
@@ -130,7 +130,7 @@ public class LocalCheckpointTracker {
      *
      * @return the maximum sequence number
      */
-    long getMaxSeqNo() {
+    public long getMaxSeqNo() {
         return nextSeqNo - 1;
     }
 
@@ -138,9 +138,9 @@ public class LocalCheckpointTracker {
     /**
      * constructs a {@link SeqNoStats} object, using local state and the supplied global checkpoint
      *
-     * @implNote this is needed to make sure the local checkpoint and max seq no are consistent
+     * This is needed to make sure the local checkpoint and max seq no are consistent
      */
-    synchronized SeqNoStats getStats(final long globalCheckpoint) {
+    public synchronized SeqNoStats getStats(final long globalCheckpoint) {
         return new SeqNoStats(getMaxSeqNo(), getCheckpoint(), globalCheckpoint);
     }
 
@@ -151,7 +151,7 @@ public class LocalCheckpointTracker {
      * @throws InterruptedException if the thread was interrupted while blocking on the condition
      */
     @SuppressForbidden(reason = "Object#wait")
-    synchronized void waitForOpsToComplete(final long seqNo) throws InterruptedException {
+    public synchronized void waitForOpsToComplete(final long seqNo) throws InterruptedException {
         while (checkpoint < seqNo) {
             // notified by updateCheckpoint
             this.wait();

core/src/main/java/org/elasticsearch/index/seqno/SequenceNumbers.java
@@ -19,6 +19,8 @@
 
 package org.elasticsearch.index.seqno;
 
+import org.elasticsearch.common.collect.Tuple;
+
 import java.util.Map;
 
 /**
@@ -42,15 +44,12 @@ public class SequenceNumbers {
     public static final long NO_OPS_PERFORMED = -1L;
 
     /**
-     * Reads the sequence number stats from the commit data (maximum sequence number and local checkpoint) and uses the specified global
-     * checkpoint.
+     * Reads the sequence number stats from the commit data (maximum sequence number and local checkpoint).
      *
-     * @param globalCheckpoint the global checkpoint to use
      * @param commitData       the commit data
      * @return the sequence number stats
      */
-    public static SeqNoStats loadSeqNoStatsFromLuceneCommit(
-        final long globalCheckpoint,
+    public static Tuple<Long, Long> loadSeqNoInfoFromLuceneCommit(
         final Iterable<Map.Entry<String, String>> commitData) {
         long maxSeqNo = NO_OPS_PERFORMED;
         long localCheckpoint = NO_OPS_PERFORMED;
@@ -66,7 +65,7 @@ public class SequenceNumbers {
             }
         }
 
-        return new SeqNoStats(maxSeqNo, localCheckpoint, globalCheckpoint);
+        return new Tuple<>(maxSeqNo, localCheckpoint);
     }
 
     /**

commit 4c3da53d4d13062c38bf3314f373652de76db217

Only sync translog when global checkpoint increased
Today we did not set the global checkpoint when opening an engine from
an existing store. If we are forced to close an engine before advancing
the global checkpoint, we also have to close translog which in turn sync
a new checkpoint with an unassigned global checkpoint. This is not
caught until the global checkpoint assertion was introduced in PR
#27837.

This commit tightens the syncNeeded conditions.

Relates #27970

core/src/main/java/org/elasticsearch/index/translog/TranslogWriter.java
@@ -255,7 +255,7 @@ public class TranslogWriter extends BaseTranslogReader implements Closeable {
      */
     public boolean syncNeeded() {
         return totalOffset != lastSyncedCheckpoint.offset ||
-            globalCheckpointSupplier.getAsLong() != lastSyncedCheckpoint.globalCheckpoint ||
+            globalCheckpointSupplier.getAsLong() > lastSyncedCheckpoint.globalCheckpoint ||
             minTranslogGenerationSupplier.getAsLong() != lastSyncedCheckpoint.minTranslogGeneration;
     }
 
f28722f3de7a7b2d62fe8c055e6121379dca146a
Tanguy Leroux <tlrx.dev@gmail.com>
2017-12-23 14:51:21 +0100

    [Test] Mute testAbortedSnapshotDuringInitDoesNotStart()
    The failing test is tracked in #27974

--- core/src/test/java/org/elasticsearch/snapshots/SharedClusterSnapshotRestoreIT.java
+++ core/src/test/java/org/elasticsearch/snapshots/SharedClusterSnapshotRestoreIT.java
@@ -3131,6 +3131,7 @@ public class SharedClusterSnapshotRestoreIT extends AbstractSnapshotIntegTestCas
         assertThat(shardStats.getSeqNoStats().getMaxSeqNo(), equalTo(15L));
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/27974")
     public void testAbortedSnapshotDuringInitDoesNotStart() throws Exception {
         final Client client = client();
 

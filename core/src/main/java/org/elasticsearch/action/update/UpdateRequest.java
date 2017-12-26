8610e7f04fa30bb09a7245e6bed4a998058b1ece
cybrooke <maneaiulian96@yahoo.com>
2017-12-26 18:15:28 +0000

    added toString() to UpdateRequest

--- core/src/main/java/org/elasticsearch/action/update/UpdateRequest.java
+++ core/src/main/java/org/elasticsearch/action/update/UpdateRequest.java
@@ -893,4 +893,33 @@ public class UpdateRequest extends InstanceShardOperationRequest<UpdateRequest>
         builder.endObject();
         return builder;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        String newLine = System.getProperty("line.separator");
+
+        result.append(this.getClass().getName());
+        result.append(" Object {");
+        result.append(newLine);
+
+        Field[] fields = this.getClass().getDeclaredFields();
+
+        for (Field field: fields) {
+            result.append("  ");
+            try {
+                result.append(field.getName());
+                result.append(": ");
+                result.append(field.get(this));
+            }
+            catch (IllegalAccessException ex) {
+                System.out.println(ex);
+            }
+            result.append(newLine);
+        }
+        result.append("}");
+
+        return result.toString();
+    }
+    
 }

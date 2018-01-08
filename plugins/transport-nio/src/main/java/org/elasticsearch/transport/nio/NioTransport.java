c2675cf3f783189d421469db9cb346198cf47d7c
Tim Brooks <tim@uncontended.net>
2018-01-08 09:47:38 -0700

    Remove nl

--- plugins/transport-nio/src/main/java/org/elasticsearch/transport/nio/NioTransport.java
+++ plugins/transport-nio/src/main/java/org/elasticsearch/transport/nio/NioTransport.java
@@ -153,7 +153,6 @@ public class NioTransport extends TcpTransport {
 
     private void acceptChannel(NioSocketChannel channel) {
         serverAcceptedChannel((TcpNioSocketChannel) channel);
-
     }
 
     private class TcpChannelFactory extends ChannelFactory<TcpNioServerSocketChannel, TcpNioSocketChannel> {

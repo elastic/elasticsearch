be012b132605ab0052d4ab1d7eb736d64b84a2e5
Jim Ferenczi <jim.ferenczi@elastic.co>
2018-01-15 16:47:46 +0100

    upgrade to lucene 7.2.1 (#28218)

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -146,7 +146,7 @@ public class Version implements Comparable<Version> {
     public static final Version V_6_2_0 = new Version(V_6_2_0_ID, org.apache.lucene.util.Version.LUCENE_7_2_0);
     public static final int V_7_0_0_alpha1_ID = 7000001;
     public static final Version V_7_0_0_alpha1 =
-        new Version(V_7_0_0_alpha1_ID, org.apache.lucene.util.Version.LUCENE_7_2_0);
+        new Version(V_7_0_0_alpha1_ID, org.apache.lucene.util.Version.LUCENE_7_2_1);
     public static final Version CURRENT = V_7_0_0_alpha1;
 
     static {

24c19c889eb13a66bb4ae5fbd027a359b6a6a1b8
Jim Ferenczi <jimczi@apache.org>
2018-01-15 18:27:30 +0100

    #28045 restore removed import after backport

--- server/src/main/java/org/elasticsearch/index/search/MultiMatchQuery.java
+++ server/src/main/java/org/elasticsearch/index/search/MultiMatchQuery.java
@@ -29,6 +29,7 @@ import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.util.BytesRef;
+import org.elasticsearch.ElasticsearchParseException;
 import org.elasticsearch.common.lucene.search.Queries;
 import org.elasticsearch.index.mapper.MappedFieldType;
 import org.elasticsearch.index.query.AbstractQueryBuilder;

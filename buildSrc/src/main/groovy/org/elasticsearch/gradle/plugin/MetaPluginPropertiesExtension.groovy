582910f2ddd0c40a9381f22df31ff18808084677
Ryan Ernst <ryan@iernst.net>
2018-01-17 19:47:37 -0800

    Build: Add gradle plugin for configuring meta plugin (#28276)
    This commit adds a gradle plugin to ease development of meta plugins.
    Applying the plugin will generated the meta plugin properties based on
    the es_meta_plugin configuration object, which includes name and
    description. The plugins to include within the meta plugin are
    configured through the `plugins` list. An integ test task is also
    automatically added.

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/plugin/MetaPluginPropertiesExtension.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/plugin/MetaPluginPropertiesExtension.groovy
@@ -17,12 +17,30 @@
  * under the License.
  */
 
-// A meta plugin packaging example that bundles multiple plugins in a single zip.
+package org.elasticsearch.gradle.plugin
 
-apply plugin: 'elasticsearch.es-meta-plugin'
+import org.gradle.api.Project
+import org.gradle.api.tasks.Input
 
-es_meta_plugin {
-  name 'meta-plugin'
-  description 'example meta plugin'
-  plugins = ['dummy-plugin1', 'dummy-plugin2']
+/**
+ * A container for meta plugin properties that will be written to the meta plugin descriptor, for easy
+ * manipulation in the gradle DSL.
+ */
+class MetaPluginPropertiesExtension {
+    @Input
+    String name
+
+    @Input
+    String description
+
+    /**
+     * The plugins this meta plugin wraps.
+     * Note this is not written to the plugin descriptor, but used to setup the final zip file task.
+     */
+    @Input
+    List<String> plugins
+
+    MetaPluginPropertiesExtension(Project project) {
+        name = project.name
+    }
 }

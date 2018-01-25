9e912c53eb99b57094f7901f2ed5ed0c205ae6d7
Jason Tedor <jason@tedor.me>
2018-01-25 06:30:25 -0500

    Reword comment

--- server/src/main/java/org/elasticsearch/search/aggregations/bucket/terms/StringTerms.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/bucket/terms/StringTerms.java
@@ -64,16 +64,18 @@ public class StringTerms extends InternalMappedTerms<StringTerms, StringTerms.Bu
             return getKeyAsString();
         }
 
+        // this method is needed for scripted numeric aggs
         @Override
         public Number getKeyAsNumber() {
-            // this method is needed for scripted numeric aggs
+            /*
+             * If the term is a long greater than 2^52 then parsing as a double would lose accuracy. Therefore, we first parse as a long and
+             * if this fails then we attempt to parse the term as a double.
+             */
             try {
                 return Long.parseLong(termBytes.utf8ToString());
-            } catch (NumberFormatException ignored) {
-                //first tried to parsed as an Integer in order to not lose accuracy
-                //in case the values is bigger than 2^52
+            } catch (final NumberFormatException ignored) {
+                return Double.parseDouble(termBytes.utf8ToString());
             }
-            return Double.parseDouble(termBytes.utf8ToString());
         }
 
         @Override

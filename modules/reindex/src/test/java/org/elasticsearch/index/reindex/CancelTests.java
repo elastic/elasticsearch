e9563a8bc3930847ec2c70f2de434c5f75346c27
Nik Everett <nik9000@gmail.com>
2018-01-30 17:57:55 -0500

    Reindex: Raise timeout on flaky test
    This gives the test longer to block its updates. Now that we're checking
    if the updates actually blocked saw that they may not do so in the
    normal 10 seconds on a highly loaded system. And our jenkins machines
    often function like highly loaded systems. Maybe this fixes #26758!

--- modules/reindex/src/test/java/org/elasticsearch/index/reindex/CancelTests.java
+++ modules/reindex/src/test/java/org/elasticsearch/index/reindex/CancelTests.java
@@ -120,7 +120,9 @@ public class CancelTests extends ReindexTestCase {
          * exhausted their slice while others might have quite a bit left
          * to work on. We can't control that. */
         logger.debug("waiting for updates to be blocked");
-        boolean blocked = awaitBusy(() -> ALLOWED_OPERATIONS.hasQueuedThreads() && ALLOWED_OPERATIONS.availablePermits() == 0);
+        boolean blocked = awaitBusy(
+            () -> ALLOWED_OPERATIONS.hasQueuedThreads() && ALLOWED_OPERATIONS.availablePermits() == 0,
+            1, TimeUnit.MINUTES); // 10 seconds is usually fine but on heavilly loaded machines this can wake a while
         assertTrue("updates blocked", blocked);
 
         // Status should show the task running

52e2f08fb5ff779c6e239419e0c8522d12d56318
Christoph Büscher <cbuescher@posteo.de>
2018-01-30 17:48:09 +0100

    Add ranking evaluation API to High Level Rest Client (#28357)
    This change adds support for the new ranking evaluation API to the High Level Rest Client.
    This mostly means adding support for parsing the various response objects back from the
    REST representation. It includes one change to the response syntax where previously we didn't
    print the type of the metric details section but we now need it to pick the right parser to
    parse this section back.
    
    Closes #28198

--- modules/rank-eval/src/main/java/org/elasticsearch/index/rankeval/MetricDetail.java
+++ modules/rank-eval/src/main/java/org/elasticsearch/index/rankeval/MetricDetail.java
@@ -20,11 +20,31 @@
 package org.elasticsearch.index.rankeval;
 
 import org.elasticsearch.common.io.stream.NamedWriteable;
-import org.elasticsearch.common.xcontent.ToXContent;
+import org.elasticsearch.common.xcontent.ToXContentObject;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+
+import java.io.IOException;
 
 /**
  * Details about a specific {@link EvaluationMetric} that should be included in the resonse.
  */
-public interface MetricDetails extends ToXContent, NamedWriteable {
+public interface MetricDetail extends ToXContentObject, NamedWriteable {
 
+    @Override
+    default XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.startObject();
+        builder.startObject(getMetricName());
+        innerToXContent(builder, params);
+        builder.endObject();
+        return builder.endObject();
+    };
+
+    default String getMetricName() {
+        return getWriteableName();
+    }
+
+    /**
+     * Implementations should write their own fields to the {@link XContentBuilder} passed in.
+     */
+    XContentBuilder innerToXContent(XContentBuilder builder, Params params) throws IOException;
 }

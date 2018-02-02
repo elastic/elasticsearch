340ab298cc5c8a045b33194a04ee4220fd954b11
Michael Basnight <mbasnight@gmail.com>
2018-02-02 15:57:56 -0600

    various cleanup to minimize the drift between what was there and is now

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -154,7 +154,6 @@ public class Version implements Comparable<Version> {
     public static final int V_7_0_0_alpha1_ID = 7000001;
     public static final Version V_7_0_0_alpha1 =
         new Version(V_7_0_0_alpha1_ID, org.apache.lucene.util.Version.LUCENE_7_2_1);
-
     public static final Version CURRENT = V_7_0_0_alpha1;
 
     static {

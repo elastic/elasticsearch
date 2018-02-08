d73f1bb4d29ed64f4babc0c7fe47531312901db6
Ryan Ernst <ryan@iernst.net>
2018-02-07 21:56:55 -0800

    make comileOnly deps available for testing

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
@@ -260,8 +260,9 @@ class BuildPlugin implements Plugin<Project> {
      * to iterate the transitive dependencies and add excludes.
      */
     static void configureConfigurations(Project project) {
-        // TODO: this should really be in upstream gradle, no reason provided deps for compile shouldn't be used by tests
-        project.configurations.testCompileOnly.extendsFrom(project.configurations.compileOnly)
+        // we want to test compileOnly deps!
+        project.configurations.testCompile.extendsFrom(project.configurations.compileOnly)
+
         // we are not shipping these jars, we act like dumb consumers of these things
         if (project.path.startsWith(':test:fixtures') || project.path == ':build-tools') {
             return

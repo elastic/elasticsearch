3d8b57763bac2e7bd949eb8a011294510e4c1199
Igor Motov <igor@motovs.org>
2018-02-08 13:12:46 -0500

    Add 6.2.2 version constant

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -150,6 +150,8 @@ public class Version implements Comparable<Version> {
     public static final Version V_6_2_0 = new Version(V_6_2_0_ID, org.apache.lucene.util.Version.LUCENE_7_2_1);
     public static final int V_6_2_1_ID = 6020199;
     public static final Version V_6_2_1 = new Version(V_6_2_1_ID, org.apache.lucene.util.Version.LUCENE_7_2_1);
+    public static final int V_6_2_2_ID = 6020299;
+    public static final Version V_6_2_2 = new Version(V_6_2_2_ID, org.apache.lucene.util.Version.LUCENE_7_2_1);
     public static final int V_6_3_0_ID = 6030099;
     public static final Version V_6_3_0 = new Version(V_6_3_0_ID, org.apache.lucene.util.Version.LUCENE_7_2_1);
     public static final Version CURRENT = V_6_3_0;
@@ -167,6 +169,8 @@ public class Version implements Comparable<Version> {
         switch (id) {
             case V_6_3_0_ID:
                 return V_6_3_0;
+            case V_6_2_2_ID:
+                return V_6_2_2;
             case V_6_2_1_ID:
                 return V_6_2_1;
             case V_6_2_0_ID:

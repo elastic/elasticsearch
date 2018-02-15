b042afdfdcaedf490a37f5183ff8996e48e9043f
Andy Bristol <andy.bristol@elastic.co>
2018-02-15 11:25:57 -0800

    [TEST] AwaitsFix JdbcCsvSpecIT
    For elastic/x-pack-elasticsearch#3960
    
    Original commit: elastic/x-pack-elasticsearch@c0c1ff7d07309cab340d203bc42767d89adcdda8

--- qa/sql/no-security/src/test/java/org/elasticsearch/xpack/qa/sql/nosecurity/JdbcCsvSpecIT.java
+++ qa/sql/no-security/src/test/java/org/elasticsearch/xpack/qa/sql/nosecurity/JdbcCsvSpecIT.java
@@ -5,8 +5,10 @@
  */
 package org.elasticsearch.xpack.qa.sql.nosecurity;
 
+import org.apache.lucene.util.LuceneTestCase;
 import org.elasticsearch.xpack.qa.sql.jdbc.CsvSpecTestCase;
 
+@LuceneTestCase.AwaitsFix(bugUrl = "https://github.com/elastic/x-pack-elasticsearch/issues/3960")
 public class JdbcCsvSpecIT extends CsvSpecTestCase {
     public JdbcCsvSpecIT(String fileName, String groupName, String testName, Integer lineNumber, CsvTestCase testCase) {
         super(fileName, groupName, testName, lineNumber, testCase);

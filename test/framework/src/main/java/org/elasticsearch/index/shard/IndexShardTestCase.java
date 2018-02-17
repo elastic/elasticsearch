5c50f5f361abc0194124a4531bd737c596c0675e
Nhat Nguyen <nhat.nguyen@elastic.co>
2018-02-16 21:25:58 -0500

    Separate acquiring safe commit and last commit (#28271)
    Previously we introduced a new parameter to `acquireIndexCommit` to
    allow acquire either a safe commit or a last commit. However with the
    new parameters, callers can provide a nonsense combination - flush first
    but acquire the safe commit. This commit separates acquireIndexCommit
    method into two different methods to avoid that problem. Moreover, this
    change should also improve the readability.
    
    Relates #28038

--- test/framework/src/main/java/org/elasticsearch/index/shard/IndexShardTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/index/shard/IndexShardTestCase.java
@@ -621,7 +621,7 @@ public abstract class IndexShardTestCase extends ESTestCase {
                                  final Snapshot snapshot,
                                  final Repository repository) throws IOException {
         final IndexShardSnapshotStatus snapshotStatus = IndexShardSnapshotStatus.newInitializing();
-        try (Engine.IndexCommitRef indexCommitRef = shard.acquireIndexCommit(false, true)) {
+        try (Engine.IndexCommitRef indexCommitRef = shard.acquireLastIndexCommit(true)) {
             Index index = shard.shardId().getIndex();
             IndexId indexId = new IndexId(index.getName(), index.getUUID());
 

72de14115b9303d6a7b7351eca246cdd0fa3673a
Martijn van Groningen <martijn.v.groningen@gmail.com>
2018-02-20 08:46:57 +0100

    fixed codestyle violation

--- libs/grok/src/main/java/org/elasticsearch/grok/Grok.java
+++ libs/grok/src/main/java/org/elasticsearch/grok/Grok.java
@@ -28,7 +28,11 @@ import org.joni.Region;
 import org.joni.Syntax;
 import org.joni.exception.ValueException;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Iterator;

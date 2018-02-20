65fc283410968a08df24b6c0f3e648f0dc6dd23b
Michael Basnight <mbasnight@gmail.com>
2018-02-20 11:37:45 -0600

    Handle 5.6.8 release
    Bump version constant
    Generate BWC zips

--- core/src/main/java/org/elasticsearch/Version.java
+++ core/src/main/java/org/elasticsearch/Version.java
@@ -156,9 +156,11 @@ public class Version implements Comparable<Version> {
     public static final Version V_5_6_6 = new Version(V_5_6_6_ID, org.apache.lucene.util.Version.LUCENE_6_6_1);
     public static final int V_5_6_7_ID = 5060799;
     public static final Version V_5_6_7 = new Version(V_5_6_7_ID, org.apache.lucene.util.Version.LUCENE_6_6_1);
-    public static final int V_5_6_8_ID_UNRELEASED = 5060899;
-    public static final Version V_5_6_8_UNRELEASED = new Version(V_5_6_8_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_6_1);
-    public static final Version CURRENT = V_5_6_8_UNRELEASED;
+    public static final int V_5_6_8_ID = 5060899;
+    public static final Version V_5_6_8 = new Version(V_5_6_8_ID, org.apache.lucene.util.Version.LUCENE_6_6_1);
+    public static final int V_5_6_9_ID_UNRELEASED = 5060999;
+    public static final Version V_5_6_9_UNRELEASED = new Version(V_5_6_9_ID_UNRELEASED, org.apache.lucene.util.Version.LUCENE_6_6_1);
+    public static final Version CURRENT = V_5_6_9_UNRELEASED;
 
     // unreleased versions must be added to the above list with the suffix _UNRELEASED (with the exception of CURRENT)
 
@@ -173,8 +175,10 @@ public class Version implements Comparable<Version> {
 
     public static Version fromId(int id) {
         switch (id) {
-            case V_5_6_8_ID_UNRELEASED:
-                return V_5_6_8_UNRELEASED;
+            case V_5_6_9_ID_UNRELEASED:
+                return V_5_6_9_UNRELEASED;
+            case V_5_6_8_ID:
+                return V_5_6_8;
             case V_5_6_7_ID:
                 return V_5_6_7;
             case V_5_6_6_ID:

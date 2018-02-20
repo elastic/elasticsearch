5991e977d2bc9c99d7ff0e90ea128e2b427ba882
Jim Ferenczi <jimczi@apache.org>
2018-02-20 17:24:00 +0100

    Add unreleased v6.2.3 version

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -151,6 +151,8 @@ public class Version implements Comparable<Version> {
     public static final Version V_6_2_1 = new Version(V_6_2_1_ID, org.apache.lucene.util.Version.LUCENE_7_2_1);
     public static final int V_6_2_2_ID = 6020299;
     public static final Version V_6_2_2 = new Version(V_6_2_2_ID, org.apache.lucene.util.Version.LUCENE_7_2_1);
+    public static final int V_6_2_3_ID = 6020399;
+    public static final Version V_6_2_3 = new Version(V_6_2_3_ID, org.apache.lucene.util.Version.LUCENE_7_2_1);
     public static final int V_6_3_0_ID = 6030099;
     public static final Version V_6_3_0 = new Version(V_6_3_0_ID, org.apache.lucene.util.Version.LUCENE_7_2_1);
     public static final int V_7_0_0_alpha1_ID = 7000001;
@@ -173,6 +175,8 @@ public class Version implements Comparable<Version> {
                 return V_7_0_0_alpha1;
             case V_6_3_0_ID:
                 return V_6_3_0;
+            case V_6_2_3_ID:
+                return V_6_2_3;
             case V_6_2_2_ID:
                 return V_6_2_2;
             case V_6_2_1_ID:

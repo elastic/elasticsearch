020f5496beba4d5c2ea0dafb40c4d7aa25a8f565
jaymode <jay@elastic.co>
2018-02-22 09:55:01 -0700

    Test: update IndexAuditUpgradeIT after backport
    This commit updates the IndexAuditUpgradeIT test after backporting. The
    backport fixes a bug that caused the node name field to be null, which
    affected the expected number of nodes in the tests.
    
    Original commit: elastic/x-pack-elasticsearch@9df99d88001f7d617d6b61a41c27e3abc9060d3d

--- qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/IndexAuditUpgradeIT.java
+++ qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/IndexAuditUpgradeIT.java
@@ -21,7 +21,7 @@ public class IndexAuditUpgradeIT extends AbstractUpgradeTestCase {
         assumeTrue("only runs against old cluster", clusterType == CLUSTER_TYPE.OLD);
         assertBusy(() -> {
             assertAuditDocsExist();
-            assertNumUniqueNodeNameBuckets(0); // TODO update on backport. Will become 2
+            assertNumUniqueNodeNameBuckets(2);
         });
     }
 
@@ -29,8 +29,7 @@ public class IndexAuditUpgradeIT extends AbstractUpgradeTestCase {
         assumeTrue("only runs against mixed cluster", clusterType == CLUSTER_TYPE.MIXED);
         assertBusy(() -> {
             assertAuditDocsExist();
-            // TODO update on backport. Will become 2 as new node won't index docs until a new version node is master
-            assertNumUniqueNodeNameBuckets(0);
+            assertNumUniqueNodeNameBuckets(2);
         });
     }
 
@@ -38,8 +37,7 @@ public class IndexAuditUpgradeIT extends AbstractUpgradeTestCase {
         assumeTrue("only runs against upgraded cluster", clusterType == CLUSTER_TYPE.UPGRADED);
         assertBusy(() -> {
             assertAuditDocsExist();
-            // TODO update on backport. will become 4
-            assertNumUniqueNodeNameBuckets(2);
+            assertNumUniqueNodeNameBuckets(4);
         });
     }
 

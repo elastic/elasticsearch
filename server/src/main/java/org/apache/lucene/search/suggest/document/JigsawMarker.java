bc1497d0c768718dcbb0320cd3aa8be558ef5e8a
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-03-13 20:34:00 +0100

    Fix classes headers

--- server/src/main/java/org/apache/lucene/search/suggest/document/JigsawMarker.java
+++ server/src/main/java/org/apache/lucene/search/suggest/document/JigsawMarker.java
@@ -1,11 +1,26 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.lucene.search.suggest.document;
 
 /**
- * This marker is needed because Java 9 compiler expects the current package to contains at least one class during automodule descriptor derivation process.
- * <p>
- * Created on 13/03/18.
- *
- * @author Reda.Housni-Alaoui
+ * This marker is needed because Java 9 compiler expects the current package to contain at least one class during
+ * automodule descriptor derivation process.
  */
 interface JigsawMarker {
 }

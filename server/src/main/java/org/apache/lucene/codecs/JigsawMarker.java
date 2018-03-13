3cc2298df04c379a76339eb88eea2e44d905e6fe
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-03-13 20:26:35 +0100

    Java 9: requiring elasticsearch from module-info.java fails

---
+++ server/src/main/java/org/apache/lucene/codecs/JigsawMarker.java
@@ -0,0 +1,11 @@
+package org.apache.lucene.codecs;
+
+/**
+ * This marker is needed because Java 9 compiler expects the current package to contains at least one class during automodule descriptor derivation process.
+ * <p>
+ * Created on 13/03/18.
+ *
+ * @author Reda.Housni-Alaoui
+ */
+interface JigsawMarker {
+}

71c5c32ea1b5ce5c053a0a1641fc280d6938360a
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-03-13 22:52:50 +0100

    Add test module

---
+++ test/jigsaw-consumer/src/main/java/org/elasticsearch/test/jigsaw_consumer/Foo.java
@@ -0,0 +1,14 @@
+package org.elasticsearch.test.jigsaw_consumer;
+
+import org.elasticsearch.client.Client;
+
+/**
+ * Created on 13/03/18.
+ *
+ * @author Reda.Housni-Alaoui
+ */
+public interface Foo {
+
+    Client client();
+
+}

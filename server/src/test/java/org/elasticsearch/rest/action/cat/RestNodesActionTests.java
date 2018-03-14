e726abaf01fd8458cc6ef9ca7383531253147b1c
David Kyle <david.kyle@elastic.co>
2018-03-14 13:37:42 +0000

    Use correct licence

--- server/src/test/java/org/elasticsearch/rest/action/cat/RestNodesActionTests.java
+++ server/src/test/java/org/elasticsearch/rest/action/cat/RestNodesActionTests.java
@@ -1,17 +1,22 @@
 /*
- * ELASTICSEARCH CONFIDENTIAL
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Copyright (c) 2018 Elasticsearch BV. All Rights Reserved.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Notice: this software, and all information contained
- * therein, is the exclusive property of Elasticsearch BV
- * and its licensors, if any, and is protected under applicable
- * domestic and foreign law, and international treaties.
- *
- * Reproduction, republication or distribution without the
- * express written consent of Elasticsearch BV is
- * strictly prohibited.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package org.elasticsearch.rest.action.cat;
 
 import org.elasticsearch.Version;

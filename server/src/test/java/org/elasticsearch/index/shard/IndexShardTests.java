36f8531bf463788547545b52feca588b76ad49b5
Tanguy Leroux <tlrx.dev@gmail.com>
2018-03-28 09:35:05 +0200

    Don't load global state when only restoring indices (#29239)
    Restoring a snapshot, or getting the status of finished
    snapshots, currently always load the global state metadata
     file from the repository even if it not required. This
    slows down the restore process (or listing statuses process)
     and can also be an issue if the global state cannot be
    deserialized (because it has unknown customs for example).
    
    This commit splits the Repository.getSnapshotMetadata()
    method into two distincts methods: getGlobalMetadata()
    and getIndexMetadata() that are now called only when needed.

--- server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
+++ server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
@@ -2605,7 +2605,12 @@ public class IndexShardTests extends IndexShardTestCase {
         }
 
         @Override
-        public MetaData getSnapshotMetaData(SnapshotInfo snapshot, List<IndexId> indices) throws IOException {
+        public MetaData getSnapshotGlobalMetaData(SnapshotId snapshotId) {
+            return null;
+        }
+
+        @Override
+        public IndexMetaData getSnapshotIndexMetaData(SnapshotId snapshotId, IndexId index) throws IOException {
             return null;
         }
 

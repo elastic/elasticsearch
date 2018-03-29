00a387080774654f9db997e2edb99f58e9dad691
Lee Hinman <lee@writequit.org>
2018-03-29 15:31:34 -0600

    Move the copied Regex.simpleMatch into new Glob.globMatch in core

--- libs/xcontent/src/main/java/org/elasticsearch/common/xcontent/support/filtering/FilterPath.java
+++ libs/xcontent/src/main/java/org/elasticsearch/common/xcontent/support/filtering/FilterPath.java
@@ -20,6 +20,8 @@
 
 package org.elasticsearch.common.xcontent.support.filtering;
 
+import org.elasticsearch.common.Glob;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -47,53 +49,10 @@ public class FilterPath {
     }
 
     public FilterPath matchProperty(String name) {
-        if ((next != null) && (simpleWildcard || doubleWildcard || simpleMatch(segment, name))) {
+        if ((next != null) && (simpleWildcard || doubleWildcard || Glob.globMatch(segment, name))) {
             return next;
         }
         return null;
-    }
-
-    /**
-     * Match a String against the given pattern, supporting the following simple
-     * pattern styles: "xxx*", "*xxx", "*xxx*" and "xxx*yyy" matches (with an
-     * arbitrary number of pattern parts), as well as direct equality.
-     *
-     * @param pattern the pattern to match against
-     * @param str     the String to match
-     * @return whether the String matches the given pattern
-     */
-    private static boolean simpleMatch(String pattern, String str) {
-        if (pattern == null || str == null) {
-            return false;
-        }
-        int firstIndex = pattern.indexOf('*');
-        if (firstIndex == -1) {
-            return pattern.equals(str);
-        }
-        if (firstIndex == 0) {
-            if (pattern.length() == 1) {
-                return true;
-            }
-            int nextIndex = pattern.indexOf('*', firstIndex + 1);
-            if (nextIndex == -1) {
-                return str.endsWith(pattern.substring(1));
-            } else if (nextIndex == 1) {
-                // Double wildcard "**" - skipping the first "*"
-                return simpleMatch(pattern.substring(1), str);
-            }
-            String part = pattern.substring(1, nextIndex);
-            int partIndex = str.indexOf(part);
-            while (partIndex != -1) {
-                if (simpleMatch(pattern.substring(nextIndex), str.substring(partIndex + part.length()))) {
-                    return true;
-                }
-                partIndex = str.indexOf(part, partIndex + 1);
-            }
-            return false;
-        }
-        return (str.length() >= firstIndex &&
-            pattern.substring(0, firstIndex).equals(str.substring(0, firstIndex)) &&
-            simpleMatch(pattern.substring(firstIndex), str.substring(firstIndex)));
     }
 
     public boolean matches() {

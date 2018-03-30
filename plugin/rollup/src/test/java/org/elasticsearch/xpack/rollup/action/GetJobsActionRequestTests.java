a4b96e994a898f671a02d6d1995bbb750b8c508e
Zachary Tong <polyfractal@elastic.co>
2018-03-30 06:24:29 -0700

    [Rollup] Delegate GetJobs to master (elastic/x-pack-elasticsearch#4247)
    If a job is deleted and then GetJobs API is immediately called,
    it is possible for a job to be returned in the response.  This is likely
    due to the GetJobs API being executed on a node with a slightly
    stale cluster state which shows the job as still existing.
    
    So we delegate to the master node so the list of jobs/tasks is current.
    After routing to the master, we need to check if the rollup job
    is in the PersistentTask's CS.  A job can be acknowledged canceled,
    removed from the CS, but the allocated task is still alive.  So we
    first check the CS to make sure it's really there before going to the
    allocated task to get the status.
    
    As extra precaution, when running local to the task, we also make
    sure the task isn't canceled before including it in the response.
    
    relates elastic/x-pack-elasticsearch#4041 
    
    
    Original commit: elastic/x-pack-elasticsearch@95ed0304276120abd266827a7e05f1b3d0abf4a2

--- plugin/rollup/src/test/java/org/elasticsearch/xpack/rollup/action/GetJobsActionRequestTests.java
+++ plugin/rollup/src/test/java/org/elasticsearch/xpack/rollup/action/GetJobsActionRequestTests.java
@@ -6,9 +6,19 @@
 package org.elasticsearch.xpack.rollup.action;
 
 
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.MetaData;
+import org.elasticsearch.persistent.PersistentTasksCustomMetaData;
 import org.elasticsearch.test.AbstractStreamableTestCase;
+import org.elasticsearch.xpack.core.rollup.ConfigTestHelpers;
+import org.elasticsearch.xpack.core.rollup.action.GetRollupJobsAction;
 import org.elasticsearch.xpack.core.rollup.action.GetRollupJobsAction.Request;
+import org.elasticsearch.xpack.core.rollup.job.RollupJob;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class GetJobsActionRequestTests extends AbstractStreamableTestCase<Request> {
@@ -24,6 +34,79 @@ public class GetJobsActionRequestTests extends AbstractStreamableTestCase<Reques
     @Override
     protected Request createBlankInstance() {
         return new Request();
+    }
+
+    public void testStateCheckNoPersistentTasks() {
+        GetRollupJobsAction.Request request = new GetRollupJobsAction.Request("foo");
+        ClusterState state = ClusterState.builder(new ClusterName("_name"))
+                .metaData(MetaData.builder().putCustom(PersistentTasksCustomMetaData.TYPE,
+                        new PersistentTasksCustomMetaData(0L, Collections.emptyMap())))
+                .build();
+        boolean hasRollupJobs = TransportGetRollupJobAction.stateHasRollupJobs(request, state);
+        assertFalse(hasRollupJobs);
+    }
+
+    public void testStateCheckAllNoPersistentTasks() {
+        GetRollupJobsAction.Request request = new GetRollupJobsAction.Request("_all");
+        ClusterState state = ClusterState.builder(new ClusterName("_name"))
+                .metaData(MetaData.builder().putCustom(PersistentTasksCustomMetaData.TYPE,
+                        new PersistentTasksCustomMetaData(0L, Collections.emptyMap())))
+                .build();
+        boolean hasRollupJobs = TransportGetRollupJobAction.stateHasRollupJobs(request, state);
+        assertFalse(hasRollupJobs);
+    }
+
+    public void testStateCheckNoMatchingPersistentTasks() {
+        GetRollupJobsAction.Request request = new GetRollupJobsAction.Request("foo");
+        Map<String, PersistentTasksCustomMetaData.PersistentTask<?>> tasks
+                = Collections.singletonMap("bar", new PersistentTasksCustomMetaData.PersistentTask<>("bar", "bar", null, 1, null));
+        ClusterState state = ClusterState.builder(new ClusterName("_name"))
+                .metaData(MetaData.builder().putCustom(PersistentTasksCustomMetaData.TYPE,
+                        new PersistentTasksCustomMetaData(0L, tasks)))
+                .build();
+        boolean hasRollupJobs = TransportGetRollupJobAction.stateHasRollupJobs(request, state);
+        assertFalse(hasRollupJobs);
+    }
+
+    public void testStateCheckMatchingPersistentTasks() {
+        GetRollupJobsAction.Request request = new GetRollupJobsAction.Request("foo");
+        RollupJob job = new RollupJob(ConfigTestHelpers.getRollupJob("foo").build(), Collections.emptyMap());
+        Map<String, PersistentTasksCustomMetaData.PersistentTask<?>> tasks
+                = Collections.singletonMap("foo", new PersistentTasksCustomMetaData.PersistentTask<>("foo", RollupJob.NAME, job, 1, null));
+        ClusterState state = ClusterState.builder(new ClusterName("_name"))
+                .metaData(MetaData.builder().putCustom(PersistentTasksCustomMetaData.TYPE,
+                        new PersistentTasksCustomMetaData(0L, tasks)))
+                .build();
+        boolean hasRollupJobs = TransportGetRollupJobAction.stateHasRollupJobs(request, state);
+        assertTrue(hasRollupJobs);
+    }
+
+    public void testStateCheckAllMatchingPersistentTasks() {
+        GetRollupJobsAction.Request request = new GetRollupJobsAction.Request("_all");
+        RollupJob job = new RollupJob(ConfigTestHelpers.getRollupJob("foo").build(), Collections.emptyMap());
+        Map<String, PersistentTasksCustomMetaData.PersistentTask<?>> tasks
+                = Collections.singletonMap("foo", new PersistentTasksCustomMetaData.PersistentTask<>("foo", RollupJob.NAME, job, 1, null));
+        ClusterState state = ClusterState.builder(new ClusterName("_name"))
+                .metaData(MetaData.builder().putCustom(PersistentTasksCustomMetaData.TYPE,
+                        new PersistentTasksCustomMetaData(0L, tasks)))
+                .build();
+        boolean hasRollupJobs = TransportGetRollupJobAction.stateHasRollupJobs(request, state);
+        assertTrue(hasRollupJobs);
+    }
+
+    public void testStateCheckAllWithSeveralMatchingPersistentTasks() {
+        GetRollupJobsAction.Request request = new GetRollupJobsAction.Request("_all");
+        RollupJob job = new RollupJob(ConfigTestHelpers.getRollupJob("foo").build(), Collections.emptyMap());
+        RollupJob job2 = new RollupJob(ConfigTestHelpers.getRollupJob("bar").build(), Collections.emptyMap());
+        Map<String, PersistentTasksCustomMetaData.PersistentTask<?>> tasks = new HashMap<>(2);
+        tasks.put("foo", new PersistentTasksCustomMetaData.PersistentTask<>("foo", RollupJob.NAME, job, 1, null));
+        tasks.put("bar", new PersistentTasksCustomMetaData.PersistentTask<>("bar", RollupJob.NAME, job2, 1, null));
+        ClusterState state = ClusterState.builder(new ClusterName("_name"))
+                .metaData(MetaData.builder().putCustom(PersistentTasksCustomMetaData.TYPE,
+                        new PersistentTasksCustomMetaData(0L, tasks)))
+                .build();
+        boolean hasRollupJobs = TransportGetRollupJobAction.stateHasRollupJobs(request, state);
+        assertTrue(hasRollupJobs);
     }
 }
 

b58159ddad28ef030f704bfa70177b9586ccc330
Tal Levy <jubboy333@gmail.com>
2018-04-05 17:43:32 -0700

    add more tests

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/indexlifecycle/PhaseAfterStepTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/indexlifecycle/PhaseAfterStepTests.java
@@ -6,10 +6,50 @@
 package org.elasticsearch.xpack.core.indexlifecycle;
 
 
+import org.elasticsearch.Version;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
+import org.elasticsearch.cluster.metadata.MetaData;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.Index;
 import org.elasticsearch.test.ESTestCase;
 
 public class PhaseAfterStepTests extends ESTestCase {
 
-    public void test() {
+    public void testConditionMet() {
+        long creationDate = randomNonNegativeLong();
+        IndexMetaData indexMetadata = IndexMetaData.builder(randomAlphaOfLength(5))
+            .settings(settings(Version.CURRENT).put(LifecycleSettings.LIFECYCLE_INDEX_CREATION_DATE, creationDate))
+            .creationDate(creationDate)
+            .numberOfShards(1).numberOfReplicas(0).build();
+        MetaData metaData = MetaData.builder()
+            .persistentSettings(settings(Version.CURRENT).build())
+            .put(IndexMetaData.builder(indexMetadata))
+            .build();
+        Index index = indexMetadata.getIndex();
+        ClusterState clusterState = ClusterState.builder(ClusterName.DEFAULT).metaData(metaData).build();
+        long after = randomNonNegativeLong();
+        long now = creationDate + after + randomIntBetween(0, 2);
+        PhaseAfterStep step = new PhaseAfterStep(() -> now, TimeValue.timeValueMillis(after), null, null);
+        assertTrue(step.isConditionMet(index, clusterState));
+    }
+
+    public void testConditionNotMet() {
+        long creationDate = randomNonNegativeLong();
+        IndexMetaData indexMetadata = IndexMetaData.builder(randomAlphaOfLength(5))
+            .settings(settings(Version.CURRENT).put(LifecycleSettings.LIFECYCLE_INDEX_CREATION_DATE, creationDate))
+            .creationDate(creationDate)
+            .numberOfShards(1).numberOfReplicas(0).build();
+        MetaData metaData = MetaData.builder()
+            .persistentSettings(settings(Version.CURRENT).build())
+            .put(IndexMetaData.builder(indexMetadata))
+            .build();
+        Index index = indexMetadata.getIndex();
+        ClusterState clusterState = ClusterState.builder(ClusterName.DEFAULT).metaData(metaData).build();
+        long after = randomNonNegativeLong();
+        long now = creationDate + after - randomIntBetween(1, 1000);
+        PhaseAfterStep step = new PhaseAfterStep(() -> now, TimeValue.timeValueMillis(after), null, null);
+        assertFalse(step.isConditionMet(index, clusterState));
     }
 }

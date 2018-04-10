2346f7fa89460beea0e4dc15e681fc9d1ecf4af4
Martijn van Groningen <martijn.v.groningen@gmail.com>
2018-04-10 07:44:51 +0200

    removed unused import

--- modules/percolator/src/main/java/org/elasticsearch/percolator/QueryAnalyzer.java
+++ modules/percolator/src/main/java/org/elasticsearch/percolator/QueryAnalyzer.java
@@ -55,7 +55,6 @@ import org.elasticsearch.index.search.ESToParentBlockJoinQuery;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;

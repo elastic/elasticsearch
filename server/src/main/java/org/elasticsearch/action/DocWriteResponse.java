3b480b4fccb94d2d0f23f6a8698261fbdae04cc9
Adrien Grand <jpountz@gmail.com>
2018-04-13 10:41:10 +0200

    Add the `include_type_name` option to the search and document APIs.
    This commit add the `include_type_name` option to the `index`, `update`,
    `delete`, `get`, `bulk` and `search` APIs. When set to `false`, the response
    will omit the `_type` in the response. This option doesn't work if the endpoint
    contains a type. For instance, the following call would succeed:
    
    ```
    GET index/_doc/1?include_type_name=false
    ```
    
    But the following one would fail:
    
    ```
    GET index/some_type/1?include_type_name=false
    ```
    
    Relates #15613

--- server/src/main/java/org/elasticsearch/action/DocWriteResponse.java
+++ server/src/main/java/org/elasticsearch/action/DocWriteResponse.java
@@ -296,10 +296,12 @@ public abstract class DocWriteResponse extends ReplicationResponse implements Wr
     public XContentBuilder innerToXContent(XContentBuilder builder, Params params) throws IOException {
         ReplicationResponse.ShardInfo shardInfo = getShardInfo();
         builder.field(_INDEX, shardId.getIndexName())
-                .field(_TYPE, type)
                 .field(_ID, id)
                 .field(_VERSION, version)
                 .field(RESULT, getResult().getLowercase());
+        if (params.paramAsBoolean("include_type_name", true)) {
+            builder.field(_TYPE, type);
+        }
         if (forcedRefresh) {
             builder.field(FORCED_REFRESH, true);
         }

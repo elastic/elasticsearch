f93f7bbbf224dd658ddc22a823cccba7eccb17b5
Jason Tedor <jason@tedor.me>
2018-04-13 12:41:18 -0400

    Use proper Java version for BWC builds (#29493)
    Today we have JAVA_HOME for the compiler Java home and RUNTIME_JAVA_HOME
    for the test Java home. However, when we compile BWC nodes and run them,
    neither of these Java homes might be the version that was suitable for
    that BWC node (e.g., 5.6 requires JDK 8 to compile and to run). This
    commit adds support for the environment variables JAVA\d+_HOME and uses
    the appropriate Java home based on the version of the node being
    started. We even do this for reindex-from-old which requires JDK 7 for
    these very old nodes. Note that these environment variables are not
    required if not running BWC tests, and they are strictly required if
    running BWC tests.

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/test/NodeInfo.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/test/NodeInfo.groovy
@@ -22,7 +22,9 @@ import com.sun.jna.Native
 import com.sun.jna.WString
 import org.apache.tools.ant.taskdefs.condition.Os
 import org.elasticsearch.gradle.Version
+import org.gradle.api.GradleException
 import org.gradle.api.InvalidUserDataException
+import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 
 import java.nio.file.Files
@@ -162,7 +164,25 @@ class NodeInfo {
             args.add("${esScript}")
         }
 
-        env = ['JAVA_HOME': project.runtimeJavaHome]
+        final String javaHome
+        final Map<Integer, JavaVersion> javaVersions = project.javaVersions
+        if (Version.fromString(nodeVersion).before("6.2.0")) {
+            final String java8Home = javaVersions.get(8)
+            if (java8Home == null) {
+                throw new GradleException("JAVA8_HOME must be set to run BWC tests against [" + nodeVersion + "]")
+            }
+            javaHome = java8Home
+        } else if (Version.fromString(nodeVersion).onOrAfter("6.2.0") && Version.fromString(nodeVersion).before("6.3.0")) {
+            final String java9Home = javaVersions.get(9)
+            if (java9Home == null) {
+                throw new GradleException("JAVA9_HOME must be set to run BWC tests against [" + nodeVersion + "]")
+            }
+            javaHome = java9Home
+        } else {
+            javaHome = project.compilerJavaHome
+        }
+
+        env = ['JAVA_HOME':javaHome]
         args.addAll("-E", "node.portsfile=true")
         String collectedSystemProperties = config.systemProperties.collect { key, value -> "-D${key}=${value}" }.join(" ")
         String esJavaOpts = config.jvmArgs.isEmpty() ? collectedSystemProperties : collectedSystemProperties + " " + config.jvmArgs

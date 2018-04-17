d223bcf7ab4f5754f0f89a56518760eef65dc493
Adrien Grand <jpountz@gmail.com>
2018-04-17 11:29:08 +0200

    Add the `include_type_name` option to the search and document APIs. (#29506)
    This commit add the `include_type_name` option to the `index`, `update`,
    `delete`, `get`, `bulk` and `search` APIs. When set to `false`, the response
    will omit the `_type` in the response. This option doesn't work if the endpoint
    contains a type. For instance, the following call would succeed:
    
    ```
    GET index/_doc/1?include_type_name=false
    ```
    
    But the following one would fail:
    
    ```
    GET index/some_type/1?include_type_name=false
    ```
    
    Relates #15613

--- server/src/main/java/org/elasticsearch/rest/action/document/RestUpdateAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/document/RestUpdateAction.java
@@ -25,6 +25,7 @@ import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.VersionType;
+import org.elasticsearch.index.mapper.MapperService;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.RestController;
 import org.elasticsearch.rest.RestRequest;
@@ -50,7 +51,13 @@ public class RestUpdateAction extends BaseRestHandler {
 
     @Override
     public RestChannelConsumer prepareRequest(final RestRequest request, final NodeClient client) throws IOException {
-        UpdateRequest updateRequest = new UpdateRequest(request.param("index"), request.param("type"), request.param("id"));
+        final boolean includeTypeName = request.paramAsBoolean("include_type_name", true);
+        final String type = request.param("type");
+        if (includeTypeName == false && MapperService.SINGLE_MAPPING_NAME.equals(type) == false) {
+            throw new IllegalArgumentException("You may only use the [include_type_name=false] option with the update API with the " +
+                    "[{index}/_doc/{id}/_update] endpoint.");
+        }
+        UpdateRequest updateRequest = new UpdateRequest(request.param("index"), type, request.param("id"));
         updateRequest.routing(request.param("routing"));
         updateRequest.timeout(request.paramAsTime("timeout", updateRequest.timeout()));
         updateRequest.setRefreshPolicy(request.param("refresh"));

22bbf10da16240b1f162356f81c3280c2416f704
Mayya Sharipova <mayya.sharipova@elastic.co>
2018-04-18 15:28:05 -0400

    Handle missing and multiple values in script
    Previously in script for numeric fields, there was no way to check if
    a document is missing a value. Also certain operations on multiple-
    values fields were missing.
    
    This PR adds the following:
    
    - add the following functions for multiple-valued numeric fields:
        doc['field'].min returns the minumum amoung values
        doc['field'].max returns the maximum amoung values
        doc['field'].sum returns the sum of amoung values
        doc['field'].avg returns the average of values
    
    - return null for doc['field'] if a document is missing a 'field1':
        Now we can do this:
        if (doc['field'] == null) {return -1;} return doc['field'].value;  or
        doc['field']?.value ?: -1
        This new behaviour will only work is the following system property is set:
        `export ES_JAVA_OPTS="-Des.script.null_for_missing_value=true"'
    
    Closes #29286

--- server/src/test/java/org/elasticsearch/search/aggregations/metrics/CardinalityIT.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/metrics/CardinalityIT.java
@@ -76,7 +76,7 @@ public class CardinalityIT extends ESIntegTestCase {
             scripts.put("doc['str_values'].values", vars -> {
                 Map<?, ?> doc = (Map) vars.get("doc");
                 ScriptDocValues.Strings strValue = (ScriptDocValues.Strings) doc.get("str_values");
-                return strValue.getValues();
+                if (strValue != null) { return strValue.getValues(); } else return null;
             });
 
             scripts.put("doc[' + singleNumericField() + '].value", vars -> {
@@ -86,7 +86,8 @@ public class CardinalityIT extends ESIntegTestCase {
 
             scripts.put("doc[' + multiNumericField(false) + '].values", vars -> {
                 Map<?, ?> doc = (Map) vars.get("doc");
-                return ((ScriptDocValues<?>) doc.get(multiNumericField(false))).getValues();
+                ScriptDocValues numValue = (ScriptDocValues<?>) doc.get(multiNumericField(false));
+                if (numValue != null) { return numValue.getValues(); } else return null;
             });
 
             return scripts;
@@ -129,7 +130,7 @@ public class CardinalityIT extends ESIntegTestCase {
                     .endObject()
                     .endObject().endObject().endObject()).execute().actionGet();
 
-        numDocs = randomIntBetween(2, 100);
+        numDocs = randomIntBetween(2, 3);
         precisionThreshold = randomIntBetween(0, 1 << randomInt(20));
         IndexRequestBuilder[] builders = new IndexRequestBuilder[(int) numDocs];
         for (int i = 0; i < numDocs; ++i) {

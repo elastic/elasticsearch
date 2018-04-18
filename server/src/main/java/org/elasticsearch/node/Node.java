90fc389a6143ec38cf98f33b9bb034ae25b89c0b
Ryan Ernst <ryan@iernst.net>
2018-04-17 19:56:06 -0700

    Remove remaining tribe node references
    While tribe node was removed in
    https://github.com/elastic/elasticsearch/pull/28443, there remained a
    couple lingering references to it in docs and code. This commit removes
    those remaining references.

--- server/src/main/java/org/elasticsearch/node/Node.java
+++ server/src/main/java/org/elasticsearch/node/Node.java
@@ -777,7 +777,6 @@ public class Node implements Closeable {
         logger.info("closing ...");
         List<Closeable> toClose = new ArrayList<>();
         StopWatch stopWatch = new StopWatch("node_close");
-        toClose.add(() -> stopWatch.start("tribe"));
         toClose.add(() -> stopWatch.stop().start("node_service"));
         toClose.add(nodeService);
         toClose.add(() -> stopWatch.stop().start("http"));

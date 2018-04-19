5659b3d1fb1184a8d893d473e06ef92e6fa43189
Jay Modi <jaymode@users.noreply.github.com>
2018-04-19 09:54:25 -0600

    Wait for Netty threads after Security tests (elastic/x-pack-elasticsearch#4390)
    This commit adds waits for two Netty threads that run in the background
    and require explicit calls to await inactivity in them. This shows up
    as an issue in security tests since we always use netty and do not have
    these waits so these threads can trip the thread leak detector.
    
    relates elastic/x-pack-elasticsearch#4367
    
    Original commit: elastic/x-pack-elasticsearch@619d3817fdd215637bb2ffbfb8d279e4079508cf

--- plugin/core/src/test/java/org/elasticsearch/xpack/core/TestXPackTransportClient.java
+++ plugin/core/src/test/java/org/elasticsearch/xpack/core/TestXPackTransportClient.java
@@ -5,12 +5,17 @@
  */
 package org.elasticsearch.xpack.core;
 
+import io.netty.util.ThreadDeathWatcher;
+import io.netty.util.concurrent.GlobalEventExecutor;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.network.NetworkModule;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.xpack.core.security.SecurityField;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.test.ESTestCase.getTestTransportPlugin;
 
@@ -27,5 +32,23 @@ public class TestXPackTransportClient extends TransportClient {
 
     public TestXPackTransportClient(Settings settings, Collection<Class<? extends Plugin>> plugins) {
         super(settings, Settings.EMPTY, addPlugins(plugins, getTestTransportPlugin()), null);
+    }
+
+    @Override
+    public void close() {
+        super.close();
+        if (NetworkModule.TRANSPORT_TYPE_SETTING.exists(settings) == false
+                || NetworkModule.TRANSPORT_TYPE_SETTING.get(settings).equals(SecurityField.NAME4)) {
+            try {
+                GlobalEventExecutor.INSTANCE.awaitInactivity(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            try {
+                ThreadDeathWatcher.awaitInactivity(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }

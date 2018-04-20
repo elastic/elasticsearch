0d0b1f44ee8951422ef3fb6c64e1b4aa2a09f422
Ryan Ernst <ryan@iernst.net>
2018-04-19 17:55:25 -0700

    make mock http transport more realistic

--- test/framework/src/main/java/org/elasticsearch/test/MockHttpTransport.java
+++ test/framework/src/main/java/org/elasticsearch/test/MockHttpTransport.java
@@ -22,6 +22,7 @@ package org.elasticsearch.test;
 import org.elasticsearch.common.component.AbstractLifecycleComponent;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.BoundTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.http.HttpInfo;
 import org.elasticsearch.http.HttpServerTransport;
 import org.elasticsearch.http.HttpStats;
@@ -36,6 +37,14 @@ public class MockHttpTransport extends AbstractLifecycleComponent implements Htt
      * Marker plugin used by {@link org.elasticsearch.node.MockNode} to enable {@link MockHttpTransport}.
      */
     public static class TestPlugin extends Plugin {}
+
+    // dummy address/info that can be read by code expecting objects from the relevant methods,
+    // but not actually used for a real connection
+    private static final TransportAddress DUMMY_TRANSPORT_ADDRESS = new TransportAddress(TransportAddress.META_ADDRESS, 0);
+    private static final BoundTransportAddress DUMMY_BOUND_ADDRESS = new BoundTransportAddress(
+        new TransportAddress[] { DUMMY_TRANSPORT_ADDRESS }, DUMMY_TRANSPORT_ADDRESS);
+    private static final HttpInfo DUMMY_HTTP_INFO = new HttpInfo(DUMMY_BOUND_ADDRESS, 0);
+    private static final HttpStats DUMMY_HTTP_STATS = new HttpStats(0, 0);
 
     public MockHttpTransport() {
         super(Settings.EMPTY);
@@ -52,16 +61,16 @@ public class MockHttpTransport extends AbstractLifecycleComponent implements Htt
 
     @Override
     public BoundTransportAddress boundAddress() {
-        return null;
+        return DUMMY_BOUND_ADDRESS;
     }
 
     @Override
     public HttpInfo info() {
-        return null;
+        return DUMMY_HTTP_INFO;
     }
 
     @Override
     public HttpStats stats() {
-        return null;
+        return DUMMY_HTTP_STATS;
     }
 }

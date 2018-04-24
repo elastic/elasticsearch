bdf241347d6a957911ca5b063cf04688d4265659
Jason Tedor <jason@tedor.me>
2018-04-24 18:21:37 -0400

    Add 6.4.0 version to master (#29684)
    This commit adds the 6.4.0 version constant to the master branch.

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -170,6 +170,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_6_2_5 = new Version(V_6_2_5_ID, LUCENE_7_2_1);
     public static final int V_6_3_0_ID = 6030099;
     public static final Version V_6_3_0 = new Version(V_6_3_0_ID, org.apache.lucene.util.Version.LUCENE_7_3_0);
+    public static final int V_6_4_0_ID = 6040099;
+    public static final Version V_6_4_0 = new Version(V_6_4_0_ID, org.apache.lucene.util.Version.LUCENE_7_3_0);
     public static final int V_7_0_0_alpha1_ID = 7000001;
     public static final Version V_7_0_0_alpha1 =
         new Version(V_7_0_0_alpha1_ID, org.apache.lucene.util.Version.LUCENE_7_3_0);
@@ -188,6 +190,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
         switch (id) {
             case V_7_0_0_alpha1_ID:
                 return V_7_0_0_alpha1;
+            case V_6_4_0_ID:
+                return V_6_4_0;
             case V_6_3_0_ID:
                 return V_6_3_0;
             case V_6_2_5_ID:

openeuler-mirror/poissonsearch-oss

Add additional shards routing info in ShardSearchRequest (#29533)
This commit propagates the preference and routing of the original SearchRequest in the ShardSearchRequest.
This information is then use to fix a bug in sliced scrolls when executed with a preference (or a routing).
Instead of computing the slice query from the total number of shards in the index, this commit computes this number from the number of shards per index that participates in the request.

Fixes #27550

server/src/main/java/org/elasticsearch/search/internal/ShardSearchLocalRequest.java
@@ -28,13 +28,10 @@ import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryRewriteContext;
-import org.elasticsearch.index.query.QueryShardContext;
 import org.elasticsearch.index.query.Rewriteable;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.search.Scroll;
-import org.elasticsearch.search.SearchService;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import java.io.IOException;
@@ -61,7 +58,6 @@ import java.util.Optional;
  */
 
 public class ShardSearchLocalRequest implements ShardSearchRequest {
-
     private String clusterAlias;
     private ShardId shardId;
     private int numberOfShards;
@@ -74,17 +70,18 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
     private Boolean requestCache;
     private long nowInMillis;
     private boolean allowPartialSearchResults;
-
+    private String[] indexRoutings = Strings.EMPTY_ARRAY;
+    private String preference;
     private boolean profile;
 
     ShardSearchLocalRequest() {
     }
 
     ShardSearchLocalRequest(SearchRequest searchRequest, ShardId shardId, int numberOfShards,
-                            AliasFilter aliasFilter, float indexBoost, long nowInMillis, String clusterAlias) {
+                            AliasFilter aliasFilter, float indexBoost, long nowInMillis, String clusterAlias, String[] indexRoutings) {
         this(shardId, numberOfShards, searchRequest.searchType(),
-                searchRequest.source(), searchRequest.types(), searchRequest.requestCache(), aliasFilter, indexBoost, 
-                searchRequest.allowPartialSearchResults());
+                searchRequest.source(), searchRequest.types(), searchRequest.requestCache(), aliasFilter, indexBoost,
+                searchRequest.allowPartialSearchResults(), indexRoutings, searchRequest.preference());
         // If allowPartialSearchResults is unset (ie null), the cluster-level default should have been substituted
         // at this stage. Any NPEs in the above are therefore an error in request preparation logic.
         assert searchRequest.allowPartialSearchResults() != null;
@@ -102,7 +99,8 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
     }
 
     public ShardSearchLocalRequest(ShardId shardId, int numberOfShards, SearchType searchType, SearchSourceBuilder source, String[] types,
-            Boolean requestCache, AliasFilter aliasFilter, float indexBoost, boolean allowPartialSearchResults) {
+                                   Boolean requestCache, AliasFilter aliasFilter, float indexBoost, boolean allowPartialSearchResults,
+                                   String[] indexRoutings, String preference) {
         this.shardId = shardId;
         this.numberOfShards = numberOfShards;
         this.searchType = searchType;
@@ -112,6 +110,8 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
         this.aliasFilter = aliasFilter;
         this.indexBoost = indexBoost;
         this.allowPartialSearchResults = allowPartialSearchResults;
+        this.indexRoutings = indexRoutings;
+        this.preference = preference;
     }
 
 
@@ -169,16 +169,26 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
     public Boolean requestCache() {
         return requestCache;
     }
-    
+
     @Override
     public Boolean allowPartialSearchResults() {
         return allowPartialSearchResults;
     }
-    
+
 
     @Override
     public Scroll scroll() {
         return scroll;
+    }
+
+    @Override
+    public String[] indexRoutings() {
+        return indexRoutings;
+    }
+
+    @Override
+    public String preference() {
+        return preference;
     }
 
     @Override
@@ -225,6 +235,13 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
         if (in.getVersion().onOrAfter(Version.V_6_3_0)) {
             allowPartialSearchResults = in.readOptionalBoolean();
         }
+        if (in.getVersion().onOrAfter(Version.V_7_0_0_alpha1)) {
+            indexRoutings = in.readStringArray();
+            preference = in.readOptionalString();
+        } else {
+            indexRoutings = Strings.EMPTY_ARRAY;
+            preference = null;
+        }
     }
 
     protected void innerWriteTo(StreamOutput out, boolean asKey) throws IOException {
@@ -240,7 +257,7 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
         if (out.getVersion().onOrAfter(Version.V_5_2_0)) {
             out.writeFloat(indexBoost);
         }
-        if (!asKey) {
+        if (asKey == false) {
             out.writeVLong(nowInMillis);
         }
         out.writeOptionalBoolean(requestCache);
@@ -250,7 +267,12 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
         if (out.getVersion().onOrAfter(Version.V_6_3_0)) {
             out.writeOptionalBoolean(allowPartialSearchResults);
         }
-        
+        if (asKey == false) {
+            if (out.getVersion().onOrAfter(Version.V_7_0_0_alpha1)) {
+                out.writeStringArray(indexRoutings);
+                out.writeOptionalString(preference);
+            }
+        }
     }
 
     @Override

server/src/main/java/org/elasticsearch/search/slice/SliceBuilder.java
@@ -23,6 +23,10 @@ import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.MatchNoDocsQuery;
 import org.apache.lucene.search.Query;
 import org.elasticsearch.Version;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.routing.GroupShardsIterator;
+import org.elasticsearch.cluster.routing.ShardIterator;
+import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.ParseField;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.StreamInput;
@@ -30,6 +34,7 @@ import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.logging.DeprecationLogger;
 import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.common.util.set.Sets;
 import org.elasticsearch.common.xcontent.ObjectParser;
 import org.elasticsearch.common.xcontent.ToXContentObject;
 import org.elasticsearch.common.xcontent.XContentBuilder;
@@ -39,9 +44,13 @@ import org.elasticsearch.index.fielddata.IndexNumericFieldData;
 import org.elasticsearch.index.mapper.IdFieldMapper;
 import org.elasticsearch.index.mapper.MappedFieldType;
 import org.elasticsearch.index.query.QueryShardContext;
+import org.elasticsearch.search.internal.ShardSearchRequest;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  *  A slice builder allowing to split a scroll in multiple partitions.
@@ -203,10 +212,47 @@ public class SliceBuilder implements Writeable, ToXContentObject {
         return Objects.hash(this.field, this.id, this.max);
     }
 
-    public Query toFilter(QueryShardContext context, int shardId, int numShards) {
+    /**
+     * Converts this QueryBuilder to a lucene {@link Query}.
+     *
+     * @param context Additional information needed to build the query
+     */
+    public Query toFilter(ClusterService clusterService, ShardSearchRequest request, QueryShardContext context, Version minNodeVersion) {
         final MappedFieldType type = context.fieldMapper(field);
         if (type == null) {
             throw new IllegalArgumentException("field " + field + " not found");
+        }
+
+        int shardId = request.shardId().id();
+        int numShards = context.getIndexSettings().getNumberOfShards();
+        if (minNodeVersion.onOrAfter(Version.V_7_0_0_alpha1) &&
+                (request.preference() != null || request.indexRoutings().length > 0)) {
+            GroupShardsIterator<ShardIterator> group = buildShardIterator(clusterService, request);
+            assert group.size() <= numShards : "index routing shards: " + group.size() +
+                " cannot be greater than total number of shards: " + numShards;
+            if (group.size() < numShards) {
+                /**
+                 * The routing of this request targets a subset of the shards of this index so we need to we retrieve
+                 * the original {@link GroupShardsIterator} and compute the request shard id and number of
+                 * shards from it.
+                 * This behavior has been added in {@link Version#V_7_0_0_alpha1} so if there is another node in the cluster
+                 * with an older version we use the original shard id and number of shards in order to ensure that all
+                 * slices use the same numbers.
+                 */
+                numShards = group.size();
+                int ord = 0;
+                shardId = -1;
+                // remap the original shard id with its index (position) in the sorted shard iterator.
+                for (ShardIterator it : group) {
+                    assert it.shardId().getIndex().equals(request.shardId().getIndex());
+                    if (request.shardId().equals(it.shardId())) {
+                        shardId = ord;
+                        break;
+                    }
+                    ++ord;
+                }
+                assert shardId != -1 : "shard id: " + request.shardId().getId() + " not found in index shard routing";
+            }
         }
 
         String field = this.field;
@@ -271,6 +317,17 @@ public class SliceBuilder implements Writeable, ToXContentObject {
             return new MatchNoDocsQuery("this shard is not part of the slice");
         }
         return new MatchAllDocsQuery();
+    }
+
+    /**
+     * Returns the {@link GroupShardsIterator} for the provided <code>request</code>.
+     */
+    private GroupShardsIterator<ShardIterator> buildShardIterator(ClusterService clusterService, ShardSearchRequest request) {
+        final ClusterState state = clusterService.state();
+        String[] indices = new String[] { request.shardId().getIndex().getName() };
+        Map<String, Set<String>> routingMap = request.indexRoutings().length > 0 ?
+            Collections.singletonMap(indices[0], Sets.newHashSet(request.indexRoutings())) : null;
+        return clusterService.operationRouting().searchShards(state, indices, routingMap, request.preference());
     }
 
     @Override

elastic/elasticsearch

server/src/main/java/org/elasticsearch/search/internal/ShardSearchLocalRequest.java
@@ -28,13 +28,10 @@ import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryRewriteContext;
-import org.elasticsearch.index.query.QueryShardContext;
 import org.elasticsearch.index.query.Rewriteable;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.search.Scroll;
-import org.elasticsearch.search.SearchService;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import java.io.IOException;
@@ -61,7 +58,6 @@ import java.util.Optional;
  */
 
 public class ShardSearchLocalRequest implements ShardSearchRequest {
-
     private String clusterAlias;
     private ShardId shardId;
     private int numberOfShards;
@@ -74,17 +70,18 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
     private Boolean requestCache;
     private long nowInMillis;
     private boolean allowPartialSearchResults;
-
+    private String[] indexRoutings = Strings.EMPTY_ARRAY;
+    private String preference;
     private boolean profile;
 
     ShardSearchLocalRequest() {
     }
 
     ShardSearchLocalRequest(SearchRequest searchRequest, ShardId shardId, int numberOfShards,
-                            AliasFilter aliasFilter, float indexBoost, long nowInMillis, String clusterAlias) {
+                            AliasFilter aliasFilter, float indexBoost, long nowInMillis, String clusterAlias, String[] indexRoutings) {
         this(shardId, numberOfShards, searchRequest.searchType(),
-                searchRequest.source(), searchRequest.types(), searchRequest.requestCache(), aliasFilter, indexBoost, 
-                searchRequest.allowPartialSearchResults());
+                searchRequest.source(), searchRequest.types(), searchRequest.requestCache(), aliasFilter, indexBoost,
+                searchRequest.allowPartialSearchResults(), indexRoutings, searchRequest.preference());
         // If allowPartialSearchResults is unset (ie null), the cluster-level default should have been substituted
         // at this stage. Any NPEs in the above are therefore an error in request preparation logic.
         assert searchRequest.allowPartialSearchResults() != null;
@@ -102,7 +99,8 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
     }
 
     public ShardSearchLocalRequest(ShardId shardId, int numberOfShards, SearchType searchType, SearchSourceBuilder source, String[] types,
-            Boolean requestCache, AliasFilter aliasFilter, float indexBoost, boolean allowPartialSearchResults) {
+                                   Boolean requestCache, AliasFilter aliasFilter, float indexBoost, boolean allowPartialSearchResults,
+                                   String[] indexRoutings, String preference) {
         this.shardId = shardId;
         this.numberOfShards = numberOfShards;
         this.searchType = searchType;
@@ -112,6 +110,8 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
         this.aliasFilter = aliasFilter;
         this.indexBoost = indexBoost;
         this.allowPartialSearchResults = allowPartialSearchResults;
+        this.indexRoutings = indexRoutings;
+        this.preference = preference;
     }
 
 
@@ -169,16 +169,26 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
     public Boolean requestCache() {
         return requestCache;
     }
-    
+
     @Override
     public Boolean allowPartialSearchResults() {
         return allowPartialSearchResults;
     }
-    
+
 
     @Override
     public Scroll scroll() {
         return scroll;
+    }
+
+    @Override
+    public String[] indexRoutings() {
+        return indexRoutings;
+    }
+
+    @Override
+    public String preference() {
+        return preference;
     }
 
     @Override
@@ -225,6 +235,13 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
         if (in.getVersion().onOrAfter(Version.V_6_3_0)) {
             allowPartialSearchResults = in.readOptionalBoolean();
         }
+        if (in.getVersion().onOrAfter(Version.V_6_4_0)) {
+            indexRoutings = in.readStringArray();
+            preference = in.readOptionalString();
+        } else {
+            indexRoutings = Strings.EMPTY_ARRAY;
+            preference = null;
+        }
     }
 
     protected void innerWriteTo(StreamOutput out, boolean asKey) throws IOException {
@@ -240,7 +257,7 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
         if (out.getVersion().onOrAfter(Version.V_5_2_0)) {
             out.writeFloat(indexBoost);
         }
-        if (!asKey) {
+        if (asKey == false) {
             out.writeVLong(nowInMillis);
         }
         out.writeOptionalBoolean(requestCache);
@@ -250,7 +267,12 @@ public class ShardSearchLocalRequest implements ShardSearchRequest {
         if (out.getVersion().onOrAfter(Version.V_6_3_0)) {
             out.writeOptionalBoolean(allowPartialSearchResults);
         }
-        
+        if (asKey == false) {
+            if (out.getVersion().onOrAfter(Version.V_6_4_0)) {
+                out.writeStringArray(indexRoutings);
+                out.writeOptionalString(preference);
+            }
+        }
     }
 
     @Override

server/src/main/java/org/elasticsearch/search/slice/SliceBuilder.java
@@ -23,6 +23,10 @@ import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.MatchNoDocsQuery;
 import org.apache.lucene.search.Query;
 import org.elasticsearch.Version;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.routing.GroupShardsIterator;
+import org.elasticsearch.cluster.routing.ShardIterator;
+import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.ParseField;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.StreamInput;
@@ -30,6 +34,7 @@ import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.logging.DeprecationLogger;
 import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.common.util.set.Sets;
 import org.elasticsearch.common.xcontent.ObjectParser;
 import org.elasticsearch.common.xcontent.ToXContentObject;
 import org.elasticsearch.common.xcontent.XContentBuilder;
@@ -40,9 +45,13 @@ import org.elasticsearch.index.mapper.IdFieldMapper;
 import org.elasticsearch.index.mapper.MappedFieldType;
 import org.elasticsearch.index.mapper.UidFieldMapper;
 import org.elasticsearch.index.query.QueryShardContext;
+import org.elasticsearch.search.internal.ShardSearchRequest;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  *  A slice builder allowing to split a scroll in multiple partitions.
@@ -204,10 +213,47 @@ public class SliceBuilder implements Writeable, ToXContentObject {
         return Objects.hash(this.field, this.id, this.max);
     }
 
-    public Query toFilter(QueryShardContext context, int shardId, int numShards) {
+    /**
+     * Converts this QueryBuilder to a lucene {@link Query}.
+     *
+     * @param context Additional information needed to build the query
+     */
+    public Query toFilter(ClusterService clusterService, ShardSearchRequest request, QueryShardContext context, Version minNodeVersion) {
         final MappedFieldType type = context.fieldMapper(field);
         if (type == null) {
             throw new IllegalArgumentException("field " + field + " not found");
+        }
+
+        int shardId = request.shardId().id();
+        int numShards = context.getIndexSettings().getNumberOfShards();
+        if (minNodeVersion.onOrAfter(Version.V_6_4_0) &&
+                (request.preference() != null || request.indexRoutings().length > 0)) {
+            GroupShardsIterator<ShardIterator> group = buildShardIterator(clusterService, request);
+            assert group.size() <= numShards : "index routing shards: " + group.size() +
+                " cannot be greater than total number of shards: " + numShards;
+            if (group.size() < numShards) {
+                /**
+                 * The routing of this request targets a subset of the shards of this index so we need to we retrieve
+                 * the original {@link GroupShardsIterator} and compute the request shard id and number of
+                 * shards from it.
+                 * This behavior has been added in {@link Version#V_6_4_0} so if there is another node in the cluster
+                 * with an older version we use the original shard id and number of shards in order to ensure that all
+                 * slices use the same numbers.
+                 */
+                numShards = group.size();
+                int ord = 0;
+                shardId = -1;
+                // remap the original shard id with its index (position) in the sorted shard iterator.
+                for (ShardIterator it : group) {
+                    assert it.shardId().getIndex().equals(request.shardId().getIndex());
+                    if (request.shardId().equals(it.shardId())) {
+                        shardId = ord;
+                        break;
+                    }
+                    ++ord;
+                }
+                assert shardId != -1 : "shard id: " + request.shardId().getId() + " not found in index shard routing";
+            }
         }
 
         String field = this.field;
@@ -276,6 +322,17 @@ public class SliceBuilder implements Writeable, ToXContentObject {
             return new MatchNoDocsQuery("this shard is not part of the slice");
         }
         return new MatchAllDocsQuery();
+    }
+
+    /**
+     * Returns the {@link GroupShardsIterator} for the provided <code>request</code>.
+     */
+    private GroupShardsIterator<ShardIterator> buildShardIterator(ClusterService clusterService, ShardSearchRequest request) {
+        final ClusterState state = clusterService.state();
+        String[] indices = new String[] { request.shardId().getIndex().getName() };
+        Map<String, Set<String>> routingMap = request.indexRoutings().length > 0 ?
+            Collections.singletonMap(indices[0], Sets.newHashSet(request.indexRoutings())) : null;
+        return clusterService.operationRouting().searchShards(state, indices, routingMap, request.preference());
     }
 
     @Override
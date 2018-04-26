e2a15dc747753448e166002d1fd2a8f5384326dc
Tal Levy <jubboy333@gmail.com>
2018-04-26 08:15:22 -0700

    fix license

--- x-pack/plugin/index-lifecycle/qa/src/test/java/org/elasticsearch/xpack/indexlifecycle/TimeSeriesLifecycleActionsIT.java
+++ x-pack/plugin/index-lifecycle/qa/src/test/java/org/elasticsearch/xpack/indexlifecycle/TimeSeriesLifecycleActionsIT.java
@@ -1,16 +1,7 @@
 /*
- * ELASTICSEARCH CONFIDENTIAL
- *
- * Copyright (c) 2017 Elasticsearch BV. All Rights Reserved.
- *
- * Notice: this software, and all information contained
- * therein, is the exclusive property of Elasticsearch BV
- * and its licensors, if any, and is protected under applicable
- * domestic and foreign law, and international treaties.
- *
- * Reproduction, republication or distribution without the
- * express written consent of Elasticsearch BV is
- * strictly prohibited.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
  */
 package org.elasticsearch.xpack.indexlifecycle;
 

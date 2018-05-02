916bf9d26d6292dcb9b229123c58a86f0e741ffd
Ryan Ernst <ryan@iernst.net>
2018-05-02 08:08:54 -0700

    Convert server javadoc to html5 (#30279)
    This commit converts the remaining javadocs in :server using html4 to html5.
    This was mostly converting `tt` to `{@code}`.

--- server/src/main/java/org/elasticsearch/common/network/NetworkService.java
+++ server/src/main/java/org/elasticsearch/common/network/NetworkService.java
@@ -66,12 +66,12 @@ public final class NetworkService {
      */
     public interface CustomNameResolver {
         /**
-         * Resolves the default value if possible. If not, return <tt>null</tt>.
+         * Resolves the default value if possible. If not, return {@code null}.
          */
         InetAddress[] resolveDefault();
 
         /**
-         * Resolves a custom value handling, return <tt>null</tt> if can't handle it.
+         * Resolves a custom value handling, return {@code null} if can't handle it.
          */
         InetAddress[] resolveIfPossible(String value) throws IOException;
     }

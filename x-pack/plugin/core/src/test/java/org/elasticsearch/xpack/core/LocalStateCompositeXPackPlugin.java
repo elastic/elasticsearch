69cbb956b909b0e1f167b1c7feb4c1abf4150433
Alexander Reelsen <alexander@reelsen.net>
2018-05-02 23:20:38 +0200

    use IOUtils.close

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/LocalStateCompositeXPackPlugin.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/LocalStateCompositeXPackPlugin.java
@@ -28,6 +28,7 @@ import org.elasticsearch.common.util.BigArrays;
 import org.elasticsearch.common.util.PageCacheRecycler;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
+import org.elasticsearch.core.internal.io.IOUtils;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.env.NodeEnvironment;
 import org.elasticsearch.http.HttpServerTransport;
@@ -394,9 +395,7 @@ public class LocalStateCompositeXPackPlugin extends XPackPlugin implements Scrip
 
     @Override
     public void close() throws IOException {
-        for (Plugin plugin : plugins) {
-            plugin.close();
-        }
+        IOUtils.close(plugins);
     }
 
     private <T> List<T> filterPlugins(Class<T> type) {

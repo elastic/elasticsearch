3f2ead1bc5d167781d46d4b3cb6cc9760e877245
javanna <cavannaluca@gmail.com>
2018-05-11 17:05:00 +0200

    Simplify RestGetAllSettingsAction

--- server/src/main/java/org/elasticsearch/rest/action/admin/indices/RestGetAllSettingsAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/admin/indices/RestGetAllSettingsAction.java
@@ -19,47 +19,26 @@
 
 package org.elasticsearch.rest.action.admin.indices;
 
-import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
-import org.elasticsearch.action.admin.indices.get.GetIndexRequest.Feature;
-import org.elasticsearch.action.admin.indices.get.GetIndexResponse;
-import org.elasticsearch.action.support.IndicesOptions;
+import org.elasticsearch.action.admin.indices.settings.get.GetSettingsRequest;
 import org.elasticsearch.client.node.NodeClient;
-import org.elasticsearch.cluster.metadata.AliasMetaData;
-import org.elasticsearch.common.Strings;
-import org.elasticsearch.common.settings.IndexScopedSettings;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.settings.SettingsFilter;
-import org.elasticsearch.common.xcontent.ToXContent.Params;
-import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.rest.BaseRestHandler;
-import org.elasticsearch.rest.BytesRestResponse;
 import org.elasticsearch.rest.RestController;
 import org.elasticsearch.rest.RestRequest;
-import org.elasticsearch.rest.RestResponse;
-import org.elasticsearch.rest.action.RestBuilderListener;
+import org.elasticsearch.rest.action.RestToXContentListener;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Set;
 
 import static org.elasticsearch.rest.RestRequest.Method.GET;
-import static org.elasticsearch.rest.RestRequest.Method.HEAD;
-import static org.elasticsearch.rest.RestStatus.OK;
 
 /**
  * The REST handler for retrieving all settings
  */
 public class RestGetAllSettingsAction extends BaseRestHandler {
 
-    private final IndexScopedSettings indexScopedSettings;
-    private final SettingsFilter settingsFilter;
-
-    public RestGetAllSettingsAction(final Settings settings, final RestController controller,
-                                    final IndexScopedSettings indexScopedSettings, final SettingsFilter settingsFilter) {
+    public RestGetAllSettingsAction(final Settings settings, final RestController controller) {
         super(settings);
-        this.indexScopedSettings = indexScopedSettings;
         controller.registerHandler(GET, "/_settings", this);
-        this.settingsFilter = settingsFilter;
     }
 
     @Override
@@ -69,53 +48,14 @@ public class RestGetAllSettingsAction extends BaseRestHandler {
 
     @Override
     public RestChannelConsumer prepareRequest(final RestRequest request, final NodeClient client) throws IOException {
-        final GetIndexRequest getIndexRequest = new GetIndexRequest();
-        getIndexRequest.indices(Strings.EMPTY_ARRAY);
-        getIndexRequest.features(Feature.SETTINGS);
-        getIndexRequest.indicesOptions(IndicesOptions.fromRequest(request, getIndexRequest.indicesOptions()));
-        getIndexRequest.local(request.paramAsBoolean("local", getIndexRequest.local()));
-        getIndexRequest.humanReadable(request.paramAsBoolean("human", false));
+        final boolean renderDefaults = request.paramAsBoolean("include_defaults", false);
         // This is required so the "flat_settings" parameter counts as consumed
         request.paramAsBoolean("flat_settings", false);
-        final boolean defaults = request.paramAsBoolean("include_defaults", false);
-        return channel -> client.admin().indices().getIndex(getIndexRequest, new RestBuilderListener<GetIndexResponse>(channel) {
-
-            @Override
-            public RestResponse buildResponse(final GetIndexResponse response, final XContentBuilder builder) throws Exception {
-                builder.startObject();
-                {
-                    for (final String index : response.indices()) {
-                        builder.startObject(index);
-                        {
-                            writeSettings(response.settings().get(index), builder, request, defaults);
-                        }
-                        builder.endObject();
-                    }
-                }
-                builder.endObject();
-
-                return new BytesRestResponse(OK, builder);
-            }
-
-
-            private void writeSettings(final Settings settings, final XContentBuilder builder,
-                                       final Params params, final boolean defaults) throws IOException {
-                builder.startObject("settings");
-                {
-                    settings.toXContent(builder, params);
-                }
-                builder.endObject();
-                if (defaults) {
-                    builder.startObject("defaults");
-                    {
-                        settingsFilter
-                                .filter(indexScopedSettings.diff(settings, RestGetAllSettingsAction.this.settings))
-                                .toXContent(builder, request);
-                    }
-                    builder.endObject();
-                }
-            }
-        });
+        GetSettingsRequest getSettingsRequest = new GetSettingsRequest()
+                .humanReadable(request.hasParam("human"))
+                .includeDefaults(renderDefaults);
+        getSettingsRequest.local(request.paramAsBoolean("local", getSettingsRequest.local()));
+        getSettingsRequest.masterNodeTimeout(request.paramAsTime("master_timeout", getSettingsRequest.masterNodeTimeout()));
+        return channel -> client.admin().indices().getSettings(getSettingsRequest, new RestToXContentListener<>(channel));
     }
-
 }

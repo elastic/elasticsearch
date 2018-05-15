cddfd483acf07e65925aef9a315cf9c461c2d279
Jason Tedor <jason@tedor.me>
2018-05-15 17:53:30 -0400

    Skip shard deprecation messages in REST tests
    A 6.x node can send a deprecation message that the default number of
    shards will change from five to one in 7.0.0. In a mixed cluster,
    whether or not a create index request sees five or one shard and
    produces a deprecation message depends on the version of the master
    node. This means that during BWC tests a test can see this deprecation
    message depending on the version of the master node. In 6.x when we
    introduced this deprecation message we assumed that whereever we see
    this deprecation message is expected. However, in a mixed cluster test
    we need a similar mechanism but it would only apply if the version of
    the master node is earlier than 7.0.0. This commit takes advantage of a
    recent change to expose the version of the master node to do sections of
    REST tests. With this in hand, we can skip asserting on the deprecation
    message if the version of the master node is before 7.0.0 and otherwise
    seeing that deprecation message would be completely unexpected.

--- test/framework/src/main/java/org/elasticsearch/test/rest/yaml/section/DoSection.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/yaml/section/DoSection.java
@@ -20,6 +20,7 @@
 package org.elasticsearch.test.rest.yaml.section;
 
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.Version;
 import org.elasticsearch.common.ParsingException;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.collect.Tuple;
@@ -233,7 +234,7 @@ public class DoSection implements ExecutableSection {
                 }
                 fail(formatStatusCodeMessage(response, catchStatusCode));
             }
-            checkWarningHeaders(response.getWarningHeaders());
+            checkWarningHeaders(response.getWarningHeaders(), executionContext.masterVersion());
         } catch(ClientYamlTestResponseException e) {
             ClientYamlTestResponse restTestResponse = e.getRestTestResponse();
             if (!Strings.hasLength(catchParam)) {
@@ -259,7 +260,7 @@ public class DoSection implements ExecutableSection {
     /**
      * Check that the response contains only the warning headers that we expect.
      */
-    void checkWarningHeaders(final List<String> warningHeaders) {
+    void checkWarningHeaders(final List<String> warningHeaders, final Version masterVersion) {
         final List<String> unexpected = new ArrayList<>();
         final List<String> unmatched = new ArrayList<>();
         final List<String> missing = new ArrayList<>();
@@ -269,8 +270,18 @@ public class DoSection implements ExecutableSection {
         for (final String header : warningHeaders) {
             final Matcher matcher = WARNING_HEADER_PATTERN.matcher(header);
             final boolean matches = matcher.matches();
-            if (matches) {
-                final String message = matcher.group(1);
+            final String message = matcher.group(1);
+            // noinspection StatementWithEmptyBody
+            if (masterVersion.before(Version.V_7_0_0_alpha1)
+                    && message.equals("the default number of shards will change from [5] to [1] in 7.0.0; "
+                    + "if you wish to continue using the default of [5] shards, "
+                    + "you must manage this on the create index request or with an index template")) {
+                /*
+                 * This warning header will come back in the vast majority of our tests that create an index. Rather than rewrite our
+                 * tests to assert this warning header, we assume that it is expected.
+                 */
+            }
+            else if (matches) {
                 if (expected.remove(message) == false) {
                     unexpected.add(header);
                 }

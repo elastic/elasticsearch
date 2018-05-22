241eff7424777aba43decab7166fd8eef6fe72f8
Jason Tedor <jason@tedor.me>
2018-05-22 00:46:09 -0400

    Reduce CLI scripts to one-liners on Windows
    This commit reduces the Windows CLI scripts to one-liners by moving all
    of the redundant logic to an elasticsearch-cli script. This commit is
    only the Windows side, a previous commit covered the Linux side.

--- x-pack/plugin/security/src/main/bin/elasticsearch-saml-metadata.bat
+++ x-pack/plugin/security/src/main/bin/elasticsearch-saml-metadata.bat
@@ -11,15 +11,20 @@ call "%~dp0elasticsearch-env.bat" || exit /b 1
 
 call "%~dp0x-pack-security-env.bat" || exit /b 1
 
-%JAVA% ^
-  %ES_JAVA_OPTS% ^
-  -Des.path.home="%ES_HOME%" ^
-  -Des.path.conf="%ES_PATH_CONF%" ^
-  -Des.distribution.flavor="%ES_DISTRIBUTION_FLAVOR%" ^
-  -Des.distribution.type="%ES_DISTRIBUTION_TYPE%" ^
-  -cp "%ES_CLASSPATH%" ^
+@echo off
+
+rem Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+rem or more contributor license agreements. Licensed under the Elastic License;
+rem you may not use this file except in compliance with the Elastic License.
+
+setlocal enabledelayedexpansion
+setlocal enableextensions
+
+set ES_ADDITIONAL_SOURCES=x-pack-env;x-pack-security-env
+call "%~dp0elasticsearch-cli.bat" ^
   org.elasticsearch.xpack.security.authc.saml.SamlMetadataCommand ^
-  %*
+  %* ^
+  || exit /b 1
 
 endlocal
 endlocal

07e830e75f62f8ecd931ec1b3fc7ca262586f279
Andy Bristol <andy.bristol@elastic.co>
2018-05-23 14:56:35 -0700

    [test] java tests for archive packaging (#30734)
    Ports the first couple tests for archive distributions from the old bats
    project to the new java project that includes windows platforms,
    consolidating them into one test method that tests that the
    distributions can be extracted and their contents verified. Includes the
    zip distributions which were not tested in the bats project.

--- qa/vagrant/src/main/java/org/elasticsearch/packaging/VMTestRunner.java
+++ qa/vagrant/src/main/java/org/elasticsearch/packaging/VMTestRunner.java
@@ -19,20 +19,22 @@
 
 package org.elasticsearch.packaging;
 
-import org.elasticsearch.packaging.test.OssTarTests;
-import org.elasticsearch.packaging.test.OssZipTests;
-import org.elasticsearch.packaging.test.DefaultTarTests;
-import org.elasticsearch.packaging.test.DefaultZipTests;
+import org.junit.runner.JUnitCore;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-@RunWith(Suite.class)
-@SuiteClasses({
-    DefaultTarTests.class,
-    DefaultZipTests.class,
-    OssTarTests.class,
-    OssZipTests.class
-})
-public class PackagingTests {}
+/**
+ * Ensures that the current JVM is running on a virtual machine before delegating to {@link JUnitCore}. We just check for the existence
+ * of a special file that we create during VM provisioning.
+ */
+public class VMTestRunner {
+    public static void main(String[] args) {
+        if (Files.exists(Paths.get("/is_vagrant_vm"))) {
+            JUnitCore.main(args);
+        } else {
+            throw new RuntimeException("This filesystem does not have an expected marker file indicating it's a virtual machine. These " +
+                "tests should only run in a virtual machine because they're destructive.");
+        }
+    }
+}

114f9babdf07d807254c23c46769b697574f6c7b
markharwood <markharwood@gmail.com>
2018-05-24 10:27:07 +0100

    Add MultiSearchTemplate support to High Level Rest client
    Note some changes to core classes were required to support tests:
    
    * Added SearchType.currentlySupported to list only the usable options
    * Changed SearchResponse.Clusters constructor to public
    * Added SearchRequest.DEFAULT_BATCHED_REDUCE_SIZE constant
    * Added missing "tookInMillis" to MultiSearchTemplateResponse
    * Added SearchTemplateResponse toString() for comparisons in tests

--- client/rest-high-level/src/test/java/org/elasticsearch/client/RequestConvertersTests.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/RequestConvertersTests.java
@@ -122,8 +122,12 @@ import org.elasticsearch.index.rankeval.RankEvalSpec;
 import org.elasticsearch.index.rankeval.RatedRequest;
 import org.elasticsearch.index.rankeval.RestRankEvalAction;
 import org.elasticsearch.repositories.fs.FsRepository;
+import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.rest.RestRequest.Method;
 import org.elasticsearch.rest.action.search.RestSearchAction;
 import org.elasticsearch.script.ScriptType;
+import org.elasticsearch.script.mustache.MultiSearchTemplateRequest;
+import org.elasticsearch.script.mustache.RestMultiSearchTemplateAction;
 import org.elasticsearch.script.mustache.SearchTemplateRequest;
 import org.elasticsearch.search.Scroll;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
@@ -1373,7 +1377,53 @@ public class RequestConvertersTests extends ESTestCase {
         assertEquals(Collections.emptyMap(), request.getParameters());
         assertToXContentBody(searchTemplateRequest, request.getEntity());
     }
+    
+    public void testMultiSearchTemplate() throws Exception {
+        final int numSearchRequests = randomIntBetween(1, 10);
+        MultiSearchTemplateRequest multiSearchTemplateRequest = new MultiSearchTemplateRequest();
+        
+        for (int i = 0; i < numSearchRequests; i++) {
+            // Create a random request.
+            String[] indices = randomIndicesNames(0, 5);
+            SearchRequest searchRequest = new SearchRequest(indices);
+            
+            Map<String, String> expectedParams = new HashMap<>();
+            setRandomSearchParams(searchRequest, expectedParams);
+    
+            // scroll is not supported in the current msearch or msearchtemplate api, so unset it:
+            searchRequest.scroll((Scroll) null);
+            // batched reduce size is currently not set-able on a per-request basis as it is a query string parameter only
+            searchRequest.setBatchedReduceSize(SearchRequest.DEFAULT_BATCHED_REDUCE_SIZE);
+            
+            setRandomIndicesOptions(searchRequest::indicesOptions, searchRequest::indicesOptions, expectedParams);
+            
+            SearchTemplateRequest searchTemplateRequest = new SearchTemplateRequest(searchRequest);
+    
+            searchTemplateRequest.setScript("{\"query\": { \"match\" : { \"{{field}}\" : \"{{value}}\" }}}");
+            searchTemplateRequest.setScriptType(ScriptType.INLINE);
+            searchTemplateRequest.setProfile(randomBoolean());
+    
+            Map<String, Object> scriptParams = new HashMap<>();
+            scriptParams.put("field", "name");
+            scriptParams.put("value", randomAlphaOfLengthBetween(2, 5));
+            searchTemplateRequest.setScriptParams(scriptParams);
+    
+            multiSearchTemplateRequest.add(searchTemplateRequest);            
+        }
 
+        Request multiRequest = RequestConverters.multiSearchTemplate(multiSearchTemplateRequest);
+        
+        assertEquals(HttpPost.METHOD_NAME, multiRequest.getMethod());
+        assertEquals("/_msearch/template", multiRequest.getEndpoint());
+        List<SearchTemplateRequest> searchRequests = multiSearchTemplateRequest.requests();
+        assertEquals(numSearchRequests, searchRequests.size());
+
+        HttpEntity actualEntity = multiRequest.getEntity();
+        byte[] expectedBytes = MultiSearchTemplateRequest.writeMultiLineFormat(multiSearchTemplateRequest, XContentType.JSON.xContent());
+        assertEquals(XContentType.JSON.mediaTypeWithoutParameters(), actualEntity.getContentType().getValue());
+        assertEquals(new BytesArray(expectedBytes), new BytesArray(EntityUtils.toByteArray(actualEntity)));        
+    }
+    
     public void testExistsAlias() {
         GetAliasesRequest getAliasesRequest = new GetAliasesRequest();
         String[] indices = randomBoolean() ? null : randomIndicesNames(0, 5);
@@ -2385,7 +2435,7 @@ public class RequestConvertersTests extends ESTestCase {
             expectedParams.put("preference", searchRequest.preference());
         }
         if (randomBoolean()) {
-            searchRequest.searchType(randomFrom(SearchType.values()));
+            searchRequest.searchType(randomFrom(SearchType.CURRENTLY_SUPPORTED));
         }
         expectedParams.put("search_type", searchRequest.searchType().name().toLowerCase(Locale.ROOT));
         if (randomBoolean()) {
@@ -2410,7 +2460,7 @@ public class RequestConvertersTests extends ESTestCase {
             Map<String, String> expectedParams) {
 
         if (randomBoolean()) {
-            setter.accept(IndicesOptions.fromOptions(randomBoolean(), randomBoolean(), randomBoolean(), randomBoolean()));
+             setter.accept(IndicesOptions.fromOptions(randomBoolean(), randomBoolean(), randomBoolean(), randomBoolean()));
         }
         expectedParams.put("ignore_unavailable", Boolean.toString(getter.get().ignoreUnavailable()));
         expectedParams.put("allow_no_indices", Boolean.toString(getter.get().allowNoIndices()));
--- modules/lang-mustache/src/test/java/org/elasticsearch/script/mustache/MultiSearchTemplateResponseTests.java
+++ modules/lang-mustache/src/test/java/org/elasticsearch/script/mustache/MultiSearchTemplateResponseTests.java
@@ -16,17 +16,14 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.elasticsearch.action.search;
+package org.elasticsearch.script.mustache;
 
 import org.elasticsearch.ElasticsearchException;
-import org.elasticsearch.common.bytes.BytesReference;
-import org.elasticsearch.common.xcontent.ToXContent;
-import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.ShardSearchFailure;
 import org.elasticsearch.common.xcontent.XContentParser;
-import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.search.internal.InternalSearchResponse;
 import org.elasticsearch.test.AbstractXContentTestCase;
-import org.elasticsearch.test.ESTestCase;
 
 import java.io.IOException;
 
@@ -34,12 +31,13 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.nullValue;
 
-public class MultiSearchResponseTests extends AbstractXContentTestCase<MultiSearchResponse> {
+public class MultiSearchTemplateResponseTests extends AbstractXContentTestCase<MultiSearchTemplateResponse> {
 
     @Override
-    protected MultiSearchResponse createTestInstance() {
+    protected MultiSearchTemplateResponse createTestInstance() {
         int numItems = randomIntBetween(0, 128);
-        MultiSearchResponse.Item[] items = new MultiSearchResponse.Item[numItems];
+        long overallTookInMillis = randomNonNegativeLong();
+        MultiSearchTemplateResponse.Item[] items = new MultiSearchTemplateResponse.Item[numItems];
         for (int i = 0; i < numItems; i++) {
             if (randomBoolean()) {
                 // Creating a minimal response is OK, because SearchResponse self
@@ -50,21 +48,23 @@ public class MultiSearchResponseTests extends AbstractXContentTestCase<MultiSear
                 int skippedShards = totalShards - successfulShards;
                 InternalSearchResponse internalSearchResponse = InternalSearchResponse.empty();
                 SearchResponse.Clusters clusters = new SearchResponse.Clusters(totalShards, successfulShards, skippedShards);
+                SearchTemplateResponse searchTemplateResponse = new SearchTemplateResponse();
                 SearchResponse searchResponse = new SearchResponse(internalSearchResponse, null, totalShards,
                         successfulShards, skippedShards, tookInMillis, ShardSearchFailure.EMPTY_ARRAY, clusters);
-                items[i] = new MultiSearchResponse.Item(searchResponse, null);
+                searchTemplateResponse.setResponse(searchResponse);
+                items[i] = new MultiSearchTemplateResponse.Item(searchTemplateResponse, null);
             } else {
-                items[i] = new MultiSearchResponse.Item(null, new ElasticsearchException("an error"));
+                items[i] = new MultiSearchTemplateResponse.Item(null, new ElasticsearchException("an error"));
             }
         }
-        return new MultiSearchResponse(items, randomNonNegativeLong());
+        return new MultiSearchTemplateResponse(items, overallTookInMillis);
     }
 
     @Override
-    protected MultiSearchResponse doParseInstance(XContentParser parser) throws IOException {
-        return MultiSearchResponse.fromXContext(parser);
+    protected MultiSearchTemplateResponse doParseInstance(XContentParser parser) throws IOException {
+        return MultiSearchTemplateResponse.fromXContext(parser);
     }
-    
+
     @Override
     protected boolean supportsUnknownFields() {
         return false;
@@ -74,14 +74,14 @@ public class MultiSearchResponseTests extends AbstractXContentTestCase<MultiSear
     protected boolean assertToXContentEquivalence() {
         return false;
     }
-    
+
     @Override
-    protected void assertEqualInstances(MultiSearchResponse expected, MultiSearchResponse actual) {
-        assertThat(actual.getTook(), equalTo(expected.getTook()));
-        assertThat(actual.getResponses().length, equalTo(expected.getResponses().length));
-        for (int i = 0; i < expected.getResponses().length; i++) {
-            MultiSearchResponse.Item expectedItem = expected.getResponses()[i];
-            MultiSearchResponse.Item actualItem = actual.getResponses()[i];
+    protected void assertEqualInstances(MultiSearchTemplateResponse expectedInstance, MultiSearchTemplateResponse newInstance) {
+        assertThat(newInstance.getTook(), equalTo(expectedInstance.getTook()));
+        assertThat(newInstance.getResponses().length, equalTo(expectedInstance.getResponses().length));
+        for (int i = 0; i < expectedInstance.getResponses().length; i++) {
+            MultiSearchTemplateResponse.Item expectedItem = expectedInstance.getResponses()[i];
+            MultiSearchTemplateResponse.Item actualItem = newInstance.getResponses()[i];
             if (expectedItem.isFailure()) {
                 assertThat(actualItem.getResponse(), nullValue());
                 assertThat(actualItem.getFailureMessage(), containsString(expectedItem.getFailureMessage()));

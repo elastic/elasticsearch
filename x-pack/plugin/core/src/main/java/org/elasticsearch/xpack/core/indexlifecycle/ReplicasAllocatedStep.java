abbe8ceffea14a2ab96f58db9316d8eec2d161c0
Tal Levy <JubBoy333@gmail.com>
2018-05-25 07:52:23 -0700

    remove requirement for shards/replicas in allocation check steps (#30855)
    As we are preparing to support policy updates/changes, we noticed
    that restricting allocation wait steps with pinned replicas/shard
    counts makes this difficult to continue from. For example,
    as user may update or switch a policy to increase replicas. If this
    is done, then the check will never pass and user intervention will
    be required. If we simply remove this restriction, we still check
    that the index is allocated correctly, but without depending on
    the newly configured replicas setting in the policy.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/indexlifecycle/ReplicasAllocatedStep.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/indexlifecycle/ReplicasAllocatedStep.java
@@ -21,15 +21,9 @@ import java.util.Objects;
 
 public class ReplicasAllocatedStep extends ClusterStateWaitStep {
     public static final String NAME = "enough-shards-allocated";
-    private int numberReplicas;
 
-    public ReplicasAllocatedStep(StepKey key, StepKey nextStepKey, int numberReplicas) {
+    public ReplicasAllocatedStep(StepKey key, StepKey nextStepKey) {
         super(key, nextStepKey);
-        this.numberReplicas = numberReplicas;
-    }
-
-    int getNumberReplicas() {
-        return numberReplicas;
     }
 
     @Override
@@ -41,73 +35,54 @@ public class ReplicasAllocatedStep extends ClusterStateWaitStep {
         }
         // We only want to make progress if the cluster state reflects the number of replicas change and all shards are active
         boolean allShardsActive = ActiveShardCount.ALL.enoughShardsActive(clusterState, index.getName());
-        boolean isConditionMet = idxMeta.getNumberOfReplicas() == numberReplicas && allShardsActive;
-        if (isConditionMet) {
+        if (allShardsActive) {
             return new Result(true, null);
         } else {
-            return new Result(false, new Info(numberReplicas, idxMeta.getNumberOfReplicas(), allShardsActive));
+            return new Result(false, new Info(idxMeta.getNumberOfReplicas(), allShardsActive));
         }
     }
-    
+
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), numberReplicas);
+        return super.hashCode();
     }
-    
+
     @Override
     public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        ReplicasAllocatedStep other = (ReplicasAllocatedStep) obj;
-        return super.equals(obj) &&
-                Objects.equals(numberReplicas, other.numberReplicas);
+        return obj != null && getClass() == obj.getClass() && super.equals(obj);
     }
-    
+
     public static final class Info implements ToXContentObject {
 
-        private final long expectedReplicas;
         private final long actualReplicas;
         private final boolean allShardsActive;
         private final String message;
 
-        static final ParseField EXPECTED_REPLICAS = new ParseField("expected_replicas");
         static final ParseField ACTUAL_REPLICAS = new ParseField("actual_replicas");
         static final ParseField ALL_SHARDS_ACTIVE = new ParseField("all_shards_active");
         static final ParseField MESSAGE = new ParseField("message");
         static final ConstructingObjectParser<Info, Void> PARSER = new ConstructingObjectParser<>("replicas_allocated_step_info",
-                a -> new Info((long) a[0], (long) a[1], (boolean) a[2]));
+                a -> new Info((long) a[0], (boolean) a[1]));
         static {
-            PARSER.declareLong(ConstructingObjectParser.constructorArg(), EXPECTED_REPLICAS);
             PARSER.declareLong(ConstructingObjectParser.constructorArg(), ACTUAL_REPLICAS);
             PARSER.declareBoolean(ConstructingObjectParser.constructorArg(), ALL_SHARDS_ACTIVE);
             PARSER.declareString((i, s) -> {}, MESSAGE);
         }
 
-        public Info(long expectedReplicas, long actualReplicas, boolean allShardsActive) {
-            this.expectedReplicas = expectedReplicas;
+        public Info(long actualReplicas, boolean allShardsActive) {
             this.actualReplicas = actualReplicas;
             this.allShardsActive = allShardsActive;
-            if (actualReplicas != expectedReplicas) {
-                message = "Waiting for " + IndexMetaData.SETTING_NUMBER_OF_REPLICAS + " to be updated to " + expectedReplicas;
-            } else if (allShardsActive == false) {
+            if (allShardsActive == false) {
                 message = "Waiting for all shard copies to be active";
             } else {
                 message = "";
             }
         }
 
-        public long getExpectedReplicas() {
-            return expectedReplicas;
-        }
-        
         public long getActualReplicas() {
             return actualReplicas;
         }
-        
+
         public boolean allShardsActive() {
             return allShardsActive;
         }
@@ -116,7 +91,6 @@ public class ReplicasAllocatedStep extends ClusterStateWaitStep {
         public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
             builder.startObject();
             builder.field(MESSAGE.getPreferredName(), message);
-            builder.field(EXPECTED_REPLICAS.getPreferredName(), expectedReplicas);
             builder.field(ACTUAL_REPLICAS.getPreferredName(), actualReplicas);
             builder.field(ALL_SHARDS_ACTIVE.getPreferredName(), allShardsActive);
             builder.endObject();
@@ -125,7 +99,7 @@ public class ReplicasAllocatedStep extends ClusterStateWaitStep {
 
         @Override
         public int hashCode() {
-            return Objects.hash(expectedReplicas, actualReplicas, allShardsActive);
+            return Objects.hash(actualReplicas, allShardsActive);
         }
 
         @Override
@@ -137,8 +111,7 @@ public class ReplicasAllocatedStep extends ClusterStateWaitStep {
                 return false;
             }
             Info other = (Info) obj;
-            return Objects.equals(expectedReplicas, other.expectedReplicas) &&
-                    Objects.equals(actualReplicas, other.actualReplicas) &&
+            return Objects.equals(actualReplicas, other.actualReplicas) &&
                     Objects.equals(allShardsActive, other.allShardsActive);
         }
 

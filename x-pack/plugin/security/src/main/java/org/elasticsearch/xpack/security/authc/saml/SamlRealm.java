60d554c349d9409512dea7a292301bb8913687e0
Ioannis Kakavas <ikakavas@protonmail.com>
2018-05-29 19:11:09 +0300

    Limit the scope of BouncyCastle dependency (#30358)
    Limits the scope of the runtime dependency on
    BouncyCastle so that it can be eventually removed.
    
    * Splits functionality related to reading and generating certificates
    and keys in two utility classes so that reading certificates and
    keys doesn't require BouncyCastle.
    * Implements a class for parsing PEM Encoded key material (which also
    adds support for reading PKCS8 encoded encrypted private keys).
    * Removes BouncyCastle dependency for all of our test suites(except
    for the tests that explicitly test certificate generation) by using
    pre-generated keys/certificates/keystores.

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/saml/SamlRealm.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/saml/SamlRealm.java
@@ -44,7 +44,7 @@ import org.elasticsearch.xpack.core.security.authc.RealmConfig;
 import org.elasticsearch.xpack.core.security.authc.RealmSettings;
 import org.elasticsearch.xpack.core.security.authc.saml.SamlRealmSettings;
 import org.elasticsearch.xpack.core.security.user.User;
-import org.elasticsearch.xpack.core.ssl.CertUtils;
+import org.elasticsearch.xpack.core.ssl.CertParsingUtils;
 import org.elasticsearch.xpack.core.ssl.SSLService;
 import org.elasticsearch.xpack.core.ssl.X509KeyPairSettings;
 import org.elasticsearch.xpack.security.authc.Realms;
@@ -301,7 +301,8 @@ public final class SamlRealm extends Realm implements Releasable {
 
     private static List<X509Credential> buildCredential(RealmConfig config, X509KeyPairSettings keyPairSettings,
             Setting<String> aliasSetting, final boolean allowMultiple) {
-        final X509KeyManager keyManager = CertUtils.getKeyManager(keyPairSettings, config.settings(), null, config.env());
+        final X509KeyManager keyManager = CertParsingUtils.getKeyManager(keyPairSettings, config.settings(), null, config.env());
+
         if (keyManager == null) {
             return null;
         }

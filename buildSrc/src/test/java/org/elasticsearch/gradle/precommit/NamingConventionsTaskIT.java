a1f93cfc31a0d45da38f6ae51d3995588531b20b
Alpar Torok <torokalpar@gmail.com>
2018-05-30 14:00:18 +0300

    First test to run a Gradle build

--- buildSrc/src/test/java/org/elasticsearch/gradle/precommit/NamingConventionsTaskIT.java
+++ buildSrc/src/test/java/org/elasticsearch/gradle/precommit/NamingConventionsTaskIT.java
@@ -1,7 +1,28 @@
 package org.elasticsearch.gradle.precommit;
 
 import org.elasticsearch.gradle.GradleIntegrationTests;
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.gradle.testkit.runner.TaskOutcome;
+import org.junit.Assert;
+import org.junit.Test;
 
-public abstract class NamingConventionsTaskIT extends GradleIntegrationTests {
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class NamingConventionsTaskIT extends GradleIntegrationTests {
+
+    @Test
+    public void pluginCanBeApplied() {
+        BuildResult result = GradleRunner.create()
+            .withProjectDir(getProjectDir("namingConventionsSelfTest"))
+            .withArguments("hello", "-s")
+            .withPluginClasspath()
+            .build();
+
+        assertTrue(result.getOutput().contains("build plugin can be applied"));
+        assertEquals(TaskOutcome.SUCCESS, result.task(":hello").getOutcome());
+    }
 
 }

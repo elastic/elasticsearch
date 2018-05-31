38a8ee76d35ff4b7bac42a563bb4d5d5d5581ff6
Jim Ferenczi <jimczi@apache.org>
2018-05-31 18:14:30 +0200

    fix compil after merge

--- server/src/main/java/org/elasticsearch/index/analysis/AnalysisRegistry.java
+++ server/src/main/java/org/elasticsearch/index/analysis/AnalysisRegistry.java
@@ -553,7 +553,7 @@ public final class AnalysisRegistry implements Closeable {
             TokenizerFactory tokenizerFactory,
             Map<String, TokenFilterFactory> tokenFilters,
             Map<String, CharFilterFactory> charFilters) {
-        if (keywordTokenizerFactory == null) {
+        if (tokenizerFactory == null) {
             throw new IllegalStateException("keyword tokenizer factory is null, normalizers require analysis-common module");
         }
 

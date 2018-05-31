32f3a452a34850d0527f8c6e2f1a4c9ebd32703f
Jim Ferenczi <jimczi@apache.org>
2018-05-31 17:50:33 +0200

    Remove the ability to index or query context suggestions without context
    This is a follow up of #30712 that removes the ability to index or query
    and context enabled completion field without context.
    
    Relates #30712

--- server/src/main/java/org/elasticsearch/search/suggest/completion/context/ContextMappings.java
+++ server/src/main/java/org/elasticsearch/search/suggest/completion/context/ContextMappings.java
@@ -124,7 +124,7 @@ public class ContextMappings implements ToXContent {
         private final ParseContext.Document document;
 
         TypedContextField(String name, String value, int weight, Map<String, Set<CharSequence>> contexts,
-                                 ParseContext.Document document) {
+                          ParseContext.Document document) {
             super(name, value, weight);
             this.contexts = contexts;
             this.document = document;
@@ -150,8 +150,7 @@ public class ContextMappings implements ToXContent {
                 }
             }
             if (typedContexts.isEmpty()) {
-                DEPRECATION_LOGGER.deprecated("The ability to index a suggestion with no context on a context enabled completion field" +
-                    " is deprecated and will be removed in the next major release.");
+                throw new IllegalArgumentException("Contexts are mandatory in context enabled completion field [" + name + "]");
             }
             return typedContexts;
         }
@@ -186,8 +185,7 @@ public class ContextMappings implements ToXContent {
             }
         }
         if (hasContext == false) {
-            DEPRECATION_LOGGER.deprecated("The ability to query with no context on a context enabled completion field is deprecated " +
-                "and will be removed in the next major release.");
+            throw new IllegalArgumentException("Missing mandatory contexts in context query");
         }
         return typedContextQuery;
     }

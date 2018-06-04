8f4711f3f6fc6a5a190951de4df173eeee015918
Alan Woodward <romseygeek@gmail.com>
2018-06-04 08:50:35 +0100

    Index phrases (#30450)
    Specifying `index_phrases: true` on a text field mapping will add a subsidiary
    [field]._index_phrase field, indexing two-term shingles from the parent field.
    The parent analysis chain is re-used, wrapped with a FixedShingleFilter.
    
    At query time, if a phrase match query is executed, the mapping will redirect it
    to run against the subsidiary field.
    
    This should trade faster phrase querying for a larger index and longer indexing
    times.
    
    Relates to #27049

--- server/src/main/java/org/elasticsearch/index/query/MatchPhraseQueryBuilder.java
+++ server/src/main/java/org/elasticsearch/index/query/MatchPhraseQueryBuilder.java
@@ -28,6 +28,7 @@ import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.index.mapper.MappedFieldType;
 import org.elasticsearch.index.search.MatchQuery;
 import org.elasticsearch.index.search.MatchQuery.ZeroTermsQuery;
 

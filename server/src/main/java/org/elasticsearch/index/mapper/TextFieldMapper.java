424a7e8753c4999014ab812e8d9210232ce85628
Jim Ferenczi <jimczi@apache.org>
2018-06-04 14:23:24 +0200

    Fix index prefixes to work with span_multi
    Text fields that use `index_prefixes` can rewrite `prefix` queries into
    `term` queries internally. This commit fix the handling of this rewriting
    in the `span_multi` query.
    This change also copies the index options of the text field into the
    prefix field in order to be able to run positional queries. This is mandatory
    for `span_multi` to work but this could also be useful to optimize `match_phrase_prefix`
    queries in a follow up. Note that this change can only be done on indices created
    after 6.3 since we set the index options to doc only in this version.
    
    Fixes #31056

--- server/src/main/java/org/elasticsearch/index/mapper/TextFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/TextFieldMapper.java
@@ -40,6 +40,7 @@ import org.apache.lucene.search.NormsFieldExistsQuery;
 import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
+import org.elasticsearch.Version;
 import org.elasticsearch.common.collect.Iterators;
 import org.elasticsearch.common.logging.ESLoggerFactory;
 import org.elasticsearch.common.settings.Settings;
@@ -175,7 +176,11 @@ public class TextFieldMapper extends FieldMapper {
                 if (fieldType().isSearchable() == false) {
                     throw new IllegalArgumentException("Cannot set index_prefixes on unindexed field [" + name() + "]");
                 }
-                if (fieldType.indexOptions() == IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS) {
+                // Copy the index options of the main field to allow phrase queries on
+                // the prefix field.
+                if (context.indexCreatedVersion().onOrAfter(Version.V_7_0_0_alpha1)) {
+                    prefixFieldType.setIndexOptions(fieldType.indexOptions());
+                } else if (fieldType.indexOptions() == IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS) {
                     prefixFieldType.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS);
                 }
                 if (fieldType.storeTermVectorOffsets()) {
@@ -339,7 +344,6 @@ public class TextFieldMapper extends FieldMapper {
         PrefixFieldType(String name, int minChars, int maxChars) {
             setTokenized(true);
             setOmitNorms(true);
-            setIndexOptions(IndexOptions.DOCS);
             setName(name);
             this.minChars = minChars;
             this.maxChars = maxChars;

01b5a46c24d24895aa3a4eb78917534161fb76d4
Jason Tedor <jason@tedor.me>
2018-06-06 21:57:58 -0400

    Pass main class by environment variable on Windows (#31156)
    A previous refactoring of the CLI scripts migrated all of the CLI tools
    to shell to a common script, elasticsearch-cli. This approach is fine in
    Bash where it is easy to tear arguments apart but it doesn't work so
    well on Windows where quoting is insane. To avoid having to tear the
    arguments apart to separate the first argument to elasticsearch-cli from
    the remaining arguments, we instead choose a strategy where we can avoid
    tearing the arguments apart. To do this, we will instead pass the main
    class by an environment variable and then we can pass the arguments
    straight through. This will let us avoid awful quoting issues on
    Windows. This is the Windows side of that effort and the Bash side was
    in a previous commit.

--- x-pack/plugin/watcher/src/main/bin/elasticsearch-croneval.bat
+++ x-pack/plugin/watcher/src/main/bin/elasticsearch-croneval.bat
@@ -7,9 +7,9 @@ rem you may not use this file except in compliance with the Elastic License.
 setlocal enabledelayedexpansion
 setlocal enableextensions
 
+set ES_MAIN_CLASS=org.elasticsearch.xpack.watcher.trigger.schedule.tool.CronEvalTool
 set ES_ADDITIONAL_SOURCES=x-pack-env;x-pack-watcher-env
 call "%~dp0elasticsearch-cli.bat" ^
-  org.elasticsearch.xpack.watcher.trigger.schedule.tool.CronEvalTool ^
   %%* ^
   || exit /b 1
 

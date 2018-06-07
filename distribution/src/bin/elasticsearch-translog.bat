8989682d3f02dfc9dea7aea3972a0e0ef8b9fbd0
Jason Tedor <jason@tedor.me>
2018-06-06 21:57:58 -0400

    Pass main class by environment variable on Windows (#31156)
    A previous refactoring of the CLI scripts migrated all of the CLI tools
    to shell to a common script, elasticsearch-cli. This approach is fine in
    Bash where it is easy to tear arguments apart but it doesn't work so
    well on Windows where quoting is insane. To avoid having to tear the
    arguments apart to separate the first argument to elasticsearch-cli from
    the remaining arguments, we instead choose a strategy where we can avoid
    tearing the arguments apart. To do this, we will instead pass the main
    class by an environment variable and then we can pass the arguments
    straight through. This will let us avoid awful quoting issues on
    Windows. This is the Windows side of that effort and the Bash side was
    in a previous commit.

--- distribution/src/bin/elasticsearch-translog.bat
+++ distribution/src/bin/elasticsearch-translog.bat
@@ -3,8 +3,8 @@
 setlocal enabledelayedexpansion
 setlocal enableextensions
 
+set ES_MAIN_CLASS=org.elasticsearch.index.translog.TranslogToolCli
 call "%~dp0elasticsearch-cli.bat" ^
-  org.elasticsearch.index.translog.TranslogToolCli ^
   %%* ^
   || exit /b 1
 

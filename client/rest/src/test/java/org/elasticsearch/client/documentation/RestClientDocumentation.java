25280ef46ebf3a19459457788a909d2f13315822
Nik Everett <nik9000@gmail.com>
2018-06-11 11:42:04 -0400

    Docs!

--- client/rest/src/test/java/org/elasticsearch/client/documentation/RestClientDocumentation.java
+++ client/rest/src/test/java/org/elasticsearch/client/documentation/RestClientDocumentation.java
@@ -37,6 +37,7 @@ import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.HttpAsyncResponseConsumerFactory;
+import org.elasticsearch.client.HttpAsyncResponseConsumerFactory.HeapBufferedResponseConsumerFactory;
 import org.elasticsearch.client.Node;
 import org.elasticsearch.client.NodeSelector;
 import org.elasticsearch.client.Request;
@@ -74,6 +75,19 @@ import java.util.concurrent.CountDownLatch;
  */
 @SuppressWarnings("unused")
 public class RestClientDocumentation {
+    private static final String TOKEN = "DUMMY";
+
+    // tag::rest-client-options-singleton
+    private static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+        builder.addHeader("Authorization", "Bearer " + TOKEN); // <1>
+        builder.setNodeSelector(NodeSelector.NOT_MASTER_ONLY); // <2>
+        builder.setHttpAsyncResponseConsumerFactory(           // <3>
+            new HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
+    // end::rest-client-options-singleton
 
     @SuppressWarnings("unused")
     public void testUsage() throws IOException, InterruptedException {
@@ -174,30 +188,14 @@ public class RestClientDocumentation {
             //tag::rest-client-body-shorter
             request.setJsonEntity("{\"json\":\"text\"}");
             //end::rest-client-body-shorter
-            {
-                //tag::rest-client-headers
-                RequestOptions.Builder options = request.getOptions().toBuilder();
-                options.addHeader("Accept", "text/plain");
-                options.addHeader("Cache-Control", "no-cache");
-                request.setOptions(options);
-                //end::rest-client-headers
-            }
-            {
-                //tag::rest-client-response-consumer
-                RequestOptions.Builder options = request.getOptions().toBuilder();
-                options.setHttpAsyncResponseConsumerFactory(
-                        new HttpAsyncResponseConsumerFactory.HeapBufferedResponseConsumerFactory(30 * 1024 * 1024));
-                request.setOptions(options);
-                //end::rest-client-response-consumer
-            }
-            {
-                //tag::rest-client-node-selector
-                // TODO link me to docs
-                RequestOptions.Builder options = request.getOptions().toBuilder();
-                options.setNodeSelector(NodeSelector.NOT_MASTER_ONLY);
-                request.setOptions(options);
-                //end::rest-client-node-selector
-            }
+            //tag::rest-client-options-set-singleton
+            request.setOptions(COMMON_OPTIONS);
+            //end::rest-client-options-set-singleton
+            //tag::rest-client-options-customize
+            RequestOptions.Builder options = COMMON_OPTIONS.toBuilder();
+            options.addHeader("cats", "knock things off of other things");
+            request.setOptions(options);
+            //end::rest-client-options-customize
         }
         {
             HttpEntity[] documents = new HttpEntity[10];

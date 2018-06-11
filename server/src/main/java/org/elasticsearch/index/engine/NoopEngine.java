de63a5e97427b180142810eb92d7d0e0b9d222cf
Lee Hinman <lee@writequit.org>
2018-06-11 13:19:38 -0600

    Remove unused lastGen, validate Translog UUID

--- server/src/main/java/org/elasticsearch/index/engine/NoopEngine.java
+++ server/src/main/java/org/elasticsearch/index/engine/NoopEngine.java
@@ -22,17 +22,22 @@ package org.elasticsearch.index.engine;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexCommit;
 import org.apache.lucene.index.SegmentInfos;
+import org.elasticsearch.common.Nullable;
 import org.elasticsearch.index.seqno.SeqNoStats;
 import org.elasticsearch.index.translog.Translog;
+import org.elasticsearch.index.translog.TranslogConfig;
 import org.elasticsearch.index.translog.TranslogCorruptedException;
+import org.elasticsearch.index.translog.TranslogDeletionPolicy;
 import org.elasticsearch.index.translog.TranslogStats;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.function.BiFunction;
+import java.util.function.LongSupplier;
 import java.util.stream.Stream;
 
 /**
@@ -82,8 +87,13 @@ final class NoopEngine extends Engine {
             List<IndexCommit> indexCommits = DirectoryReader.listCommits(store.directory());
             lastCommit = indexCommits.get(indexCommits.size()-1);
             historyUUID = lastCommit.getUserData().get(HISTORY_UUID_KEY);
-            // We don't want any translogs hanging around for recovery, so we need to set these accordingly
-            final long lastGen = Long.parseLong(lastCommit.getUserData().get(Translog.TRANSLOG_GENERATION_KEY));
+
+            // The deletion policy for the translog should not keep any translogs around, so the min age/size is set to -1
+            final TranslogDeletionPolicy translogDeletionPolicy = new TranslogDeletionPolicy(-1, -1);
+
+            // The translog is opened and closed to validate that the translog UUID from lucene is the same as the one in the translog
+            Translog translog = openTranslog(engineConfig, translogDeletionPolicy, engineConfig.getGlobalCheckpointSupplier());
+            translog.close();
 
             success = true;
         } catch (IOException | TranslogCorruptedException e) {
@@ -97,6 +107,27 @@ final class NoopEngine extends Engine {
             }
         }
         logger.trace("created new NoopEngine");
+    }
+
+    private Translog openTranslog(EngineConfig engineConfig, TranslogDeletionPolicy translogDeletionPolicy,
+                                  LongSupplier globalCheckpointSupplier) throws IOException {
+        final TranslogConfig translogConfig = engineConfig.getTranslogConfig();
+        final String translogUUID = loadTranslogUUIDFromLastCommit();
+        // We expect that this shard already exists, so it must already have an existing translog else something is badly wrong!
+        return new Translog(translogConfig, translogUUID, translogDeletionPolicy, globalCheckpointSupplier,
+                engineConfig.getPrimaryTermSupplier());
+    }
+
+    /**
+     * Reads the current stored translog ID from the last commit data.
+     */
+    @Nullable
+    private String loadTranslogUUIDFromLastCommit() {
+        final Map<String, String> commitUserData = lastCommittedSegmentInfos.getUserData();
+        if (commitUserData.containsKey(Translog.TRANSLOG_GENERATION_KEY) == false) {
+            throw new IllegalStateException("commit doesn't contain translog generation id");
+        }
+        return commitUserData.get(Translog.TRANSLOG_UUID_KEY);
     }
 
     @Override

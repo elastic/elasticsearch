b5c2d95b7183c1f04eb4a8c3ad343c403863530a
Christoph Büscher <cbuescher@posteo.de>
2018-06-13 22:24:48 +0200

    iter

--- client/rest-high-level/src/test/java/org/elasticsearch/client/RestHighLevelClientTests.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/RestHighLevelClientTests.java
@@ -608,7 +608,7 @@ public class RestHighLevelClientTests extends ESTestCase {
 
     public void testProvidedNamedXContents() {
         List<NamedXContentRegistry.Entry> namedXContents = RestHighLevelClient.getProvidedNamedXContents();
-        assertEquals(7, namedXContents.size());
+        assertEquals(8, namedXContents.size());
         Map<Class<?>, Integer> categories = new HashMap<>();
         List<String> names = new ArrayList<>();
         for (NamedXContentRegistry.Entry namedXContent : namedXContents) {

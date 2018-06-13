a4cbcff644b8e1fd3cf63ba07f416daaecba6696
Tanguy Leroux <tlrx.dev@gmail.com>
2018-06-13 12:40:22 +0200

    Fix AntFixture waiting condition (#31272)
    The AntFixture waiting condition is evaluated to false
    but it should be true.

--- modules/repository-url/src/test/java/org/elasticsearch/repositories/url/URLFixture.java
+++ modules/repository-url/src/test/java/org/elasticsearch/repositories/url/URLFixture.java
@@ -16,55 +16,50 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.elasticsearch.repositories.azure;
+package org.elasticsearch.repositories.url;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import org.elasticsearch.common.SuppressForbidden;
-import org.elasticsearch.common.io.Streams;
 import org.elasticsearch.mocksocket.MockHttpServer;
-import org.elasticsearch.repositories.azure.AzureStorageTestServer.Response;
 import org.elasticsearch.rest.RestStatus;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.singleton;
-import static java.util.Collections.singletonList;
+import static java.util.Collections.singletonMap;
 
 /**
- * {@link AzureStorageFixture} is a fixture that emulates an Azure Storage service.
- * <p>
- * It starts an asynchronous socket server that binds to a random local port. The server parses
- * HTTP requests and uses a {@link AzureStorageTestServer} to handle them before returning
- * them to the client as HTTP responses.
+ * This {@link URLFixture} exposes a filesystem directory over HTTP. It is used in repository-url
+ * integration tests to expose a directory created by a regular FS repository.
  */
-public class AzureStorageFixture {
+public class URLFixture {
 
     public static void main(String[] args) throws Exception {
         if (args == null || args.length != 2) {
-            throw new IllegalArgumentException("AzureStorageFixture <working directory> <container>");
+            throw new IllegalArgumentException("URLFixture <working directory> <repository directory>");
         }
 
         final InetSocketAddress socketAddress = new InetSocketAddress(InetAddress.getLoopbackAddress(), 0);
         final HttpServer httpServer = MockHttpServer.createHttp(socketAddress, 0);
 
         try {
-            final Path workingDirectory = workingDir(args[0]);
+            final Path workingDirectory = dir(args[0]);
             /// Writes the PID of the current Java process in a `pid` file located in the working directory
             writeFile(workingDirectory, "pid", ManagementFactory.getRuntimeMXBean().getName().split("@")[0]);
 
@@ -72,12 +67,8 @@ public class AzureStorageFixture {
             // Writes the address and port of the http server in a `ports` file located in the working directory
             writeFile(workingDirectory, "ports", addressAndPort);
 
-            // Emulates Azure
-            final String storageUrl = "http://" + addressAndPort;
-            final AzureStorageTestServer testServer = new AzureStorageTestServer(storageUrl);
-            testServer.createContainer(args[1]);
-
-            httpServer.createContext("/", new ResponseHandler(testServer));
+            // Exposes the repository over HTTP
+            httpServer.createContext("/", new ResponseHandler(dir(args[1])));
             httpServer.start();
 
             // Wait to be killed
@@ -89,7 +80,7 @@ public class AzureStorageFixture {
     }
 
     @SuppressForbidden(reason = "Paths#get is fine - we don't have environment here")
-    private static Path workingDir(final String dir) {
+    private static Path dir(final String dir) {
         return Paths.get(dir);
     }
 
@@ -110,40 +101,68 @@ public class AzureStorageFixture {
 
     static class ResponseHandler implements HttpHandler {
 
-        private final AzureStorageTestServer server;
+        private final Path repositoryDir;
 
-        private ResponseHandler(final AzureStorageTestServer server) {
-            this.server = server;
+        ResponseHandler(final Path repositoryDir) {
+            this.repositoryDir = repositoryDir;
         }
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            String method = exchange.getRequestMethod();
-            String path = server.getEndpoint() + exchange.getRequestURI().getRawPath();
-            String query = exchange.getRequestURI().getRawQuery();
-            Map<String, List<String>> headers = exchange.getRequestHeaders();
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            Streams.copy(exchange.getRequestBody(), out);
-
-            Response response = null;
+            Response response;
 
             final String userAgent = exchange.getRequestHeaders().getFirst("User-Agent");
             if (userAgent != null && userAgent.startsWith("Apache Ant")) {
                 // This is a request made by the AntFixture, just reply "OK"
                 response = new Response(RestStatus.OK, emptyMap(), "text/plain; charset=utf-8", "OK".getBytes(UTF_8));
-            } else {
-                // Otherwise simulate a S3 response
-                response = server.handle(method, path, query, headers, out.toByteArray());
-            }
 
-            Map<String, List<String>> responseHeaders = exchange.getResponseHeaders();
-            responseHeaders.put("Content-Type", singletonList(response.contentType));
-            response.headers.forEach((k, v) -> responseHeaders.put(k, singletonList(v)));
+            } else if ("GET".equalsIgnoreCase(exchange.getRequestMethod())) {
+                String path = exchange.getRequestURI().toString();
+                if (path.length() > 0 && path.charAt(0) == '/') {
+                    path = path.substring(1);
+                }
+
+                Path normalizedRepositoryDir = repositoryDir.normalize();
+                Path normalizedPath = normalizedRepositoryDir.resolve(path).normalize();
+
+                if (normalizedPath.startsWith(normalizedRepositoryDir)) {
+                    if (Files.exists(normalizedPath) && Files.isReadable(normalizedPath) && Files.isRegularFile(normalizedPath)) {
+                        byte[] content = Files.readAllBytes(normalizedPath);
+                        Map<String, String> headers = singletonMap("Content-Length", String.valueOf(content.length));
+                        response = new Response(RestStatus.OK, headers, "application/octet-stream", content);
+                    } else {
+                        response = new Response(RestStatus.NOT_FOUND, emptyMap(), "text/plain; charset=utf-8", new byte[0]);
+                    }
+                } else {
+                    response = new Response(RestStatus.FORBIDDEN, emptyMap(), "text/plain; charset=utf-8", new byte[0]);
+                }
+            } else {
+                response = new Response(RestStatus.INTERNAL_SERVER_ERROR, emptyMap(), "text/plain; charset=utf-8",
+                    "Unsupported HTTP method".getBytes(StandardCharsets.UTF_8));
+            }
             exchange.sendResponseHeaders(response.status.getStatus(), response.body.length);
             if (response.body.length > 0) {
                 exchange.getResponseBody().write(response.body);
             }
             exchange.close();
+        }
+    }
+
+    /**
+     * Represents a HTTP Response.
+     */
+    static class Response {
+
+        final RestStatus status;
+        final Map<String, String> headers;
+        final String contentType;
+        final byte[] body;
+
+        Response(final RestStatus status, final Map<String, String> headers, final String contentType, final byte[] body) {
+            this.status = Objects.requireNonNull(status);
+            this.headers = Objects.requireNonNull(headers);
+            this.contentType = Objects.requireNonNull(contentType);
+            this.body = Objects.requireNonNull(body);
         }
     }
 }

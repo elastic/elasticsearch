2f71509851dd5a7bd2b85c43a0aad1450c37881a
Alpar Torok <torokalpar@gmail.com>
2018-06-15 12:35:29 +0300

    fix equals check

--- server/src/main/java/org/elasticsearch/bootstrap/Security.java
+++ server/src/main/java/org/elasticsearch/bootstrap/Security.java
@@ -221,16 +221,16 @@ final class Security {
                     if (aliasProperty.equals(property) == false) {
                         propertiesSet.add(aliasProperty);
                         String previous = System.setProperty(aliasProperty, url.toString());
-                        // allow to re-set to what was previusly there. https://github.com/elastic/elasticsearch/issues/31324
-                        if (previous != null && url.toString() != previous) {
+                        // allow to re-set to what was previously there. https://github.com/elastic/elasticsearch/issues/31324
+                        if (previous != null && url.toString().equals(previous) == false) {
                             throw new IllegalStateException("codebase property already set: " + aliasProperty + " -> " + previous +
                                                             ", cannot set to " + url.toString());
                         }
                     }
                     propertiesSet.add(property);
                     String previous = System.setProperty(property, url.toString());
-                    // allow to re-set to what was previusly there. https://github.com/elastic/elasticsearch/issues/31324
-                    if (previous != null && url.toString() != previous) {
+                    // allow to re-set to what was previously there. https://github.com/elastic/elasticsearch/issues/31324
+                    if (previous != null && url.toString().equals(previous) == false) {
                         throw new IllegalStateException("codebase property already set: " + property + " -> " + previous +
                                                         ", cannot set to " + url.toString());
                     }

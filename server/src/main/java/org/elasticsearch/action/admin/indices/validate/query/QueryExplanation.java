c4f8df3ad6b8657557a7efb9399e70927e1f0340
Sohaib Iftikhar <sohaib1692@gmail.com>
2018-06-18 15:59:29 +0200

    REST high-level client: add validate query API (#31077)
    Adds the validate query API to the high level rest client.

--- server/src/main/java/org/elasticsearch/action/admin/indices/validate/query/QueryExplanation.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/validate/query/QueryExplanation.java
@@ -20,15 +20,56 @@
 package org.elasticsearch.action.admin.indices.validate.query;
 
 import org.elasticsearch.Version;
+import org.elasticsearch.common.ParseField;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Streamable;
+import org.elasticsearch.common.xcontent.ConstructingObjectParser;
+import org.elasticsearch.common.xcontent.ToXContentFragment;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentParser;
 
 import java.io.IOException;
+import java.util.Objects;
 
-public class QueryExplanation  implements Streamable {
+import static org.elasticsearch.common.xcontent.ConstructingObjectParser.constructorArg;
+import static org.elasticsearch.common.xcontent.ConstructingObjectParser.optionalConstructorArg;
+
+public class QueryExplanation  implements Streamable, ToXContentFragment {
+
+    public static final String INDEX_FIELD = "index";
+    public static final String SHARD_FIELD = "shard";
+    public static final String VALID_FIELD = "valid";
+    public static final String ERROR_FIELD = "error";
+    public static final String EXPLANATION_FIELD = "explanation";
 
     public static final int RANDOM_SHARD = -1;
+
+    @SuppressWarnings("unchecked")
+    static ConstructingObjectParser<QueryExplanation, Void> PARSER = new ConstructingObjectParser<>(
+        "query_explanation",
+        true,
+        a -> {
+            int shard = RANDOM_SHARD;
+            if (a[1] != null) {
+                shard = (int)a[1];
+            }
+            return new QueryExplanation(
+                (String)a[0],
+                shard,
+                (boolean)a[2],
+                (String)a[3],
+                (String)a[4]
+            );
+        }
+    );
+    static {
+        PARSER.declareString(optionalConstructorArg(), new ParseField(INDEX_FIELD));
+        PARSER.declareInt(optionalConstructorArg(), new ParseField(SHARD_FIELD));
+        PARSER.declareBoolean(constructorArg(), new ParseField(VALID_FIELD));
+        PARSER.declareString(optionalConstructorArg(), new ParseField(EXPLANATION_FIELD));
+        PARSER.declareString(optionalConstructorArg(), new ParseField(ERROR_FIELD));
+    }
 
     private String index;
 
@@ -109,5 +150,44 @@ public class QueryExplanation  implements Streamable {
         QueryExplanation exp = new QueryExplanation();
         exp.readFrom(in);
         return exp;
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        if (getIndex() != null) {
+            builder.field(INDEX_FIELD, getIndex());
+        }
+        if(getShard() >= 0) {
+            builder.field(SHARD_FIELD, getShard());
+        }
+        builder.field(VALID_FIELD, isValid());
+        if (getError() != null) {
+            builder.field(ERROR_FIELD, getError());
+        }
+        if (getExplanation() != null) {
+            builder.field(EXPLANATION_FIELD, getExplanation());
+        }
+        return builder;
+    }
+
+    public static QueryExplanation fromXContent(XContentParser parser) {
+        return PARSER.apply(parser, null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QueryExplanation other = (QueryExplanation) o;
+        return Objects.equals(getIndex(), other.getIndex()) &&
+            Objects.equals(getShard(), other.getShard()) &&
+            Objects.equals(isValid(), other.isValid()) &&
+            Objects.equals(getError(), other.getError()) &&
+            Objects.equals(getExplanation(), other.getExplanation());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIndex(), getShard(), isValid(), getError(), getExplanation());
     }
 }

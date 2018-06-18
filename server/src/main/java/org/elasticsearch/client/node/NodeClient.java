a703c8052aa46c2bde89a8f4a71cc1240ee933e7
Ryan Ernst <ryan@iernst.net>
2018-06-18 07:23:10 -0700

    iter?

--- server/src/main/java/org/elasticsearch/client/node/NodeClient.java
+++ server/src/main/java/org/elasticsearch/client/node/NodeClient.java
@@ -76,9 +76,9 @@ public class NodeClient extends AbstractClient {
     }
 
     /**
-     * Execute an {@link GenericAction} locally, returning that {@link Task} used to track it, and linking an {@link ActionListener}. Prefer this
-     * method if you don't need access to the task when listening for the response. This is the method used to implement the {@link Client}
-     * interface.
+     * Execute an {@link GenericAction} locally, returning that {@link Task} used to track it, and linking an {@link ActionListener}.
+     * Prefer this method if you don't need access to the task when listening for the response. This is the method used to implement
+     * the {@link Client} interface.
      */
     public <    Request extends ActionRequest,
                 Response extends ActionResponse
@@ -87,8 +87,8 @@ public class NodeClient extends AbstractClient {
     }
 
     /**
-     * Execute an {@link GenericAction} locally, returning that {@link Task} used to track it, and linking an {@link TaskListener}. Prefer this
-     * method if you need access to the task when listening for the response.
+     * Execute an {@link GenericAction} locally, returning that {@link Task} used to track it, and linking an {@link TaskListener}.
+     * Prefer this method if you need access to the task when listening for the response.
      */
     public <    Request extends ActionRequest,
                 Response extends ActionResponse

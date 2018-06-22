c991dbc04d3164b6267697d54f9eec9051ddbe97
Vladimir Dolzhenko <vladimir.dolzhenko@elastic.co>
2018-06-22 17:22:03 +0200

    unused import...

--- client/rest-high-level/src/test/java/org/elasticsearch/client/documentation/IndicesClientDocumentationIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/documentation/IndicesClientDocumentationIT.java
@@ -92,7 +92,6 @@ import org.elasticsearch.rest.RestStatus;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;

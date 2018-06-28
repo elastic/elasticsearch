6dd169da4ff963fb991d4d462a98ebbeca49bfaf
javanna <cavannaluca@gmail.com>
2018-06-28 12:06:24 +0200

    remove unused import

--- client/rest/src/test/java/org/elasticsearch/client/RestClientTests.java
+++ client/rest/src/test/java/org/elasticsearch/client/RestClientTests.java
@@ -50,7 +50,6 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 

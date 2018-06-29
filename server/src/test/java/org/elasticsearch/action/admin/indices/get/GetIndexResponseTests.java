a65bc873323a9bd59a9b13208e3b1fefbcbe7af6
Sohaib Iftikhar <sohaib1692@gmail.com>
2018-06-29 03:21:49 +0200

    Implemented toXContent and fromXContent for GetIndexResponse
    This commit does the all the server side work adding the Get Index API to the REST high-level-client.
    A follow up would be the client side changes

---
+++ server/src/test/java/org/elasticsearch/action/admin/indices/get/GetIndexResponseTests.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.action.admin.indices.get;
+
+import org.apache.lucene.util.CollectionUtil;
+import org.elasticsearch.action.admin.indices.alias.get.GetAliasesResponseTests;
+import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsResponseTests;
+import org.elasticsearch.cluster.metadata.AliasMetaData;
+import org.elasticsearch.cluster.metadata.MappingMetaData;
+import org.elasticsearch.common.collect.ImmutableOpenMap;
+import org.elasticsearch.common.settings.IndexScopedSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.index.RandomCreateIndexGenerator;
+import org.elasticsearch.test.AbstractStreamableXContentTestCase;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class GetIndexResponseTests extends AbstractStreamableXContentTestCase<GetIndexResponse> {
+    @Override
+    protected GetIndexResponse doParseInstance(XContentParser parser) throws IOException {
+        return GetIndexResponse.fromXContent(parser);
+    }
+
+    @Override
+    protected GetIndexResponse createBlankInstance() {
+        return new GetIndexResponse();
+    }
+
+    @Override
+    protected GetIndexResponse createTestInstance() {
+        String[] indices = generateRandomStringArray(5, 5, false, false);
+        ImmutableOpenMap.Builder<String, ImmutableOpenMap<String, MappingMetaData>> mappings = ImmutableOpenMap.builder();
+        ImmutableOpenMap.Builder<String, List<AliasMetaData>> aliases = ImmutableOpenMap.builder();
+        ImmutableOpenMap.Builder<String, Settings> settings = ImmutableOpenMap.builder();
+        ImmutableOpenMap.Builder<String, Settings> defaultSettings = ImmutableOpenMap.builder();
+        IndexScopedSettings indexScopedSettings = IndexScopedSettings.DEFAULT_SCOPED_SETTINGS;
+        boolean includeDefaults = randomBoolean();
+        for (String index: indices) {
+            mappings.put(index, GetMappingsResponseTests.createMappingsForIndex());
+
+            List<AliasMetaData> aliasMetaDataList = new ArrayList<>();
+            int aliasesNum = randomIntBetween(0, 3);
+            for (int i=0; i<aliasesNum; i++) {
+                aliasMetaDataList.add(GetAliasesResponseTests.createAliasMetaData());
+            }
+            CollectionUtil.timSort(aliasMetaDataList, Comparator.comparing(AliasMetaData::alias));
+            aliases.put(index, Collections.unmodifiableList(aliasMetaDataList));
+
+            Settings.Builder builder = Settings.builder();
+            builder.put(RandomCreateIndexGenerator.randomIndexSettings());
+            settings.put(index, builder.build());
+
+            if (includeDefaults) {
+                defaultSettings.put(index, indexScopedSettings.diff(settings.get(index), Settings.EMPTY));
+            }
+        }
+        return new GetIndexResponse(
+            indices, mappings.build(), aliases.build(), settings.build(), defaultSettings.build()
+        );
+    }
+
+    @Override
+    protected boolean supportsUnknownFields() {
+        return false;
+    }
+
+}

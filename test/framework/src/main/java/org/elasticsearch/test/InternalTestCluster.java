80644aadd64656ee10e867c5938afc3f7d4c2c4a
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-07-03 10:01:08 +0200

    Circuit-break based on real memory usage
    With this commit we introduce a new circuit-breaking strategy to the
    parent circuit breaker. Contrary to the current implementation which
    only accounts for memory reserved via child circuit breakers, the new
    strategy measures real heap memory usage at the time of reservation.
    This allows us to be much more aggressive with the circuit breaker limit
    so we bump it to 95% by default.
    
    The new strategy can be turned on with the new cluster setting
    `indices.breaker.total.userealmemory`. It is on by default if the heap
    size is smaller than 1GB, otherwise it is turned off.

--- test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
+++ test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
@@ -396,6 +396,14 @@ public final class InternalTestCluster extends TestCluster {
             builder.put(MappingUpdatedAction.INDICES_MAPPING_DYNAMIC_TIMEOUT_SETTING.getKey(), new TimeValue(RandomNumbers.randomIntBetween(random, 10, 30), TimeUnit.SECONDS));
         }
 
+        if (random.nextBoolean()) {
+            builder.put(HierarchyCircuitBreakerService.USE_REAL_MEMORY_USAGE_SETTING.getKey(), true);
+            // allow full allocation in tests to avoid breaking
+            builder.put(HierarchyCircuitBreakerService.TOTAL_CIRCUIT_BREAKER_LIMIT_SETTING.getKey(), "100%");
+        } else {
+            builder.put(HierarchyCircuitBreakerService.USE_REAL_MEMORY_USAGE_SETTING.getKey(), false);
+        }
+
         if (random.nextInt(10) == 0) {
             builder.put(HierarchyCircuitBreakerService.REQUEST_CIRCUIT_BREAKER_TYPE_SETTING.getKey(), "noop");
             builder.put(HierarchyCircuitBreakerService.FIELDDATA_CIRCUIT_BREAKER_TYPE_SETTING.getKey(), "noop");

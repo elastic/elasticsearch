de9e56aa0161abb30047e7d96b814ad250b69165
Costin Leau <costin@users.noreply.github.com>
2018-07-03 16:56:31 +0300

    DOC: Add examples to the SQL docs (#31633)
    Significantly improve the example snippets in the documentation.
    The examples are part of the test suite and checked nightly.
    To help readability, the existing dataset was extended (test_emp renamed
    to emp plus library).
    Improve output of JDBC tests to be consistent with the CLI
    Add lenient flag to JDBC asserts to allow type widening (a long is
    equivalent to a integer as long as the value is the same).

--- x-pack/qa/sql/no-security/src/test/java/org/elasticsearch/xpack/qa/sql/nosecurity/JdbcDocCsvSpectIT.java
+++ x-pack/qa/sql/no-security/src/test/java/org/elasticsearch/xpack/qa/sql/nosecurity/JdbcDocCsvSpectIT.java
@@ -3,13 +3,17 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-package org.elasticsearch.xpack.qa.sql.jdbc;
+package org.elasticsearch.xpack.qa.sql.nosecurity;
 
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.test.junit.annotations.TestLogging;
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.xpack.qa.sql.jdbc.CsvTestUtils.CsvTestCase;
+import org.elasticsearch.xpack.qa.sql.jdbc.DataLoader;
+import org.elasticsearch.xpack.qa.sql.jdbc.JdbcAssert;
+import org.elasticsearch.xpack.qa.sql.jdbc.SpecBaseIntegrationTestCase;
+import org.elasticsearch.xpack.qa.sql.jdbc.SqlSpecTestCase;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -20,17 +24,39 @@ import static org.elasticsearch.xpack.qa.sql.jdbc.CsvTestUtils.csvConnection;
 import static org.elasticsearch.xpack.qa.sql.jdbc.CsvTestUtils.executeCsvQuery;
 import static org.elasticsearch.xpack.qa.sql.jdbc.CsvTestUtils.specParser;
 
-@TestLogging(JdbcTestUtils.SQL_TRACE)
-public abstract class DebugCsvSpec extends SpecBaseIntegrationTestCase {
+/**
+ * CSV test specification for DOC examples.
+ * While we could use the existing tests, their purpose is to test corner-cases which
+ * gets reflected in the dataset structure.
+ * The doc tests while redundant, try to be expressive first and foremost and sometimes
+ * the dataset isn't exactly convenient.
+ * 
+ * Also looking around for the tests across the test files isn't trivial.
+ * 
+ * That's not to say the two cannot be merged however that felt like too much of an effort
+ * at this stage and, to not keep things stalling, started with this approach.
+ */
+public class JdbcDocCsvSpectIT extends SpecBaseIntegrationTestCase {
+
     private final CsvTestCase testCase;
+
+    @Override
+    protected String indexName() {
+        return "library";
+    }
+
+    @Override
+    protected void loadDataset(RestClient client) throws Exception {
+        DataLoader.loadDocsDatasetIntoEs(client);
+    }
 
     @ParametersFactory(shuffle = false, argumentFormatting = SqlSpecTestCase.PARAM_FORMATTING)
     public static List<Object[]> readScriptSpec() throws Exception {
         Parser parser = specParser();
-        return readScriptSpec("/debug.csv-spec", parser);
+        return readScriptSpec("/docs.csv-spec", parser);
     }
 
-    public DebugCsvSpec(String fileName, String groupName, String testName, Integer lineNumber, CsvTestCase testCase) {
+    public JdbcDocCsvSpectIT(String fileName, String groupName, String testName, Integer lineNumber, CsvTestCase testCase) {
         super(fileName, groupName, testName, lineNumber);
         this.testCase = testCase;
     }
@@ -42,9 +68,8 @@ public abstract class DebugCsvSpec extends SpecBaseIntegrationTestCase {
         //
         // uncomment this to printout the result set and create new CSV tests
         //
-        JdbcTestUtils.logResultSetMetadata(elastic, log);
-        JdbcTestUtils.logResultSetData(elastic, log);
-        //JdbcAssert.assertResultSets(expected, elastic, log);
+        //JdbcTestUtils.logLikeCLI(elastic, log);
+        JdbcAssert.assertResultSets(expected, elastic, log, true);
     }
 
     @Override
@@ -54,8 +79,7 @@ public abstract class DebugCsvSpec extends SpecBaseIntegrationTestCase {
 
     @Override
     protected final void doTest() throws Throwable {
-        try (Connection csv = csvConnection(testCase.expectedResults);
-             Connection es = esJdbc()) {
+        try (Connection csv = csvConnection(testCase.expectedResults); Connection es = esJdbc()) {
 
             // pass the testName as table for debugging purposes (in case the underlying reader is missing)
             ResultSet expected = executeCsvQuery(csv, testName);

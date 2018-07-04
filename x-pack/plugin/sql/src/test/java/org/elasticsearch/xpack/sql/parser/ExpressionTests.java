87dae06b4c596b0fc2515f04af049ee8c11a6dcc
Costin Leau <costin.leau@gmail.com>
2018-07-04 10:41:24 +0300

    test for negative long

--- x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/parser/ExpressionTests.java
+++ x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/parser/ExpressionTests.java
@@ -8,6 +8,7 @@ package org.elasticsearch.xpack.sql.parser;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.xpack.sql.expression.Expression;
 import org.elasticsearch.xpack.sql.expression.Literal;
+import org.elasticsearch.xpack.sql.expression.function.scalar.arithmetic.Neg;
 import org.elasticsearch.xpack.sql.type.DataType;
 
 public class ExpressionTests extends ESTestCase {
@@ -20,6 +21,16 @@ public class ExpressionTests extends ESTestCase {
         Literal l = (Literal) lt;
         assertEquals(Long.MAX_VALUE, l.value());
         assertEquals(DataType.LONG, l.dataType());
+    }
+
+    public void testLiteralLongNegative() throws Exception {
+        // Long.MIN_VALUE doesn't work since it is being interpreted as negate positive.long which is 1 higher than Long.MAX_VALUE
+        Expression lt = parser.createExpression(String.valueOf(-Long.MAX_VALUE));
+        assertEquals(Neg.class, lt.getClass());
+        Neg n = (Neg) lt;
+        assertTrue(n.foldable());
+        assertEquals(-Long.MAX_VALUE, n.fold());
+        assertEquals(DataType.LONG, n.dataType());
     }
 
     public void testLiteralInteger() throws Exception {

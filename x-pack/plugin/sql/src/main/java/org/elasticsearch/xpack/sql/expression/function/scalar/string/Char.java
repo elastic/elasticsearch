43d94e54c5b40ea95fb2911e87849d8343c50fa3
Andrei Stefan <andrei@elastic.co>
2018-07-05 19:01:38 +0300

    Added unary text manipulating functions.

---
+++ x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/expression/function/scalar/string/Char.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+package org.elasticsearch.xpack.sql.expression.function.scalar.string;
+
+import org.elasticsearch.xpack.sql.expression.Expression;
+import org.elasticsearch.xpack.sql.expression.function.scalar.string.StringProcessor.StringOperation;
+import org.elasticsearch.xpack.sql.tree.Location;
+import org.elasticsearch.xpack.sql.tree.NodeInfo;
+import org.elasticsearch.xpack.sql.type.DataType;
+
+/**
+ * Converts an int ASCII code to a character value.
+ */
+public class Char extends UnaryStringFunction {
+
+    public Char(Location location, Expression field) {
+        super(location, field);
+    }
+
+    @Override
+    protected TypeResolution resolveType() {
+        if (!childrenResolved()) {
+            return new TypeResolution("Unresolved children");
+        }
+
+        return field().dataType().isInteger ? TypeResolution.TYPE_RESOLVED : new TypeResolution(
+                "'%s' requires a integer type, received %s", operation(), field().dataType().esType);
+    }
+
+    @Override
+    protected NodeInfo<Char> info() {
+        return NodeInfo.create(this, Char::new, field());
+    }
+
+    @Override
+    protected Char replaceChild(Expression newChild) {
+        return new Char(location(), newChild);
+    }
+
+    @Override
+    protected StringOperation operation() {
+        return StringOperation.CHAR;
+    }
+
+    @Override
+    public DataType dataType() {
+        return DataType.KEYWORD;
+    }
+}
---
+++ x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/expression/function/scalar/string/Space.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+package org.elasticsearch.xpack.sql.expression.function.scalar.string;
+
+import org.elasticsearch.xpack.sql.expression.Expression;
+import org.elasticsearch.xpack.sql.expression.function.scalar.string.StringProcessor.StringOperation;
+import org.elasticsearch.xpack.sql.tree.Location;
+import org.elasticsearch.xpack.sql.tree.NodeInfo;
+import org.elasticsearch.xpack.sql.type.DataType;
+
+/**
+ * Generates a string consisting of count spaces.
+ */
+public class Space extends UnaryStringFunction {
+
+    public Space(Location location, Expression field) {
+        super(location, field);
+    }
+
+    @Override
+    protected TypeResolution resolveType() {
+        if (!childrenResolved()) {
+            return new TypeResolution("Unresolved children");
+        }
+
+        return field().dataType().isInteger ? TypeResolution.TYPE_RESOLVED : new TypeResolution(
+                "'%s' requires a integer type, received %s", operation(), field().dataType().esType);
+    }
+
+    @Override
+    protected NodeInfo<Space> info() {
+        return NodeInfo.create(this, Space::new, field());
+    }
+
+    @Override
+    protected Space replaceChild(Expression newChild) {
+        return new Space(location(), newChild);
+    }
+
+    @Override
+    protected StringOperation operation() {
+        return StringOperation.SPACE;
+    }
+
+    @Override
+    public DataType dataType() {
+        return DataType.KEYWORD;
+    }
+}

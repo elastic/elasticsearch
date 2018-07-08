de94640f8542d37ed073c345f197cacb32b36591
Nhat Nguyen <nhat.nguyen@elastic.co>
2018-07-08 15:05:08 -0400

    Upgrade to Lucene-7.5.0-snapshot-b9e064b935 (#31839)
    This snapshot includes LUCENE-8384 which blocks work in Lucene rollback.

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -171,7 +171,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final int V_6_3_2_ID = 6030299;
     public static final Version V_6_3_2 = new Version(V_6_3_2_ID, org.apache.lucene.util.Version.LUCENE_7_3_1);
     public static final int V_6_4_0_ID = 6040099;
-    public static final Version V_6_4_0 = new Version(V_6_4_0_ID, org.apache.lucene.util.Version.LUCENE_7_4_0);
+    public static final Version V_6_4_0 = new Version(V_6_4_0_ID, org.apache.lucene.util.Version.LUCENE_7_5_0);
 
     public static final Version CURRENT = V_6_4_0;
 

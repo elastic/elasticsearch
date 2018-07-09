49416db6cebe5b3708b43201a134ea829ef4c674
Alexander Reelsen <alexander@reelsen.net>
2018-07-09 09:42:13 +0200

    Watcher: Increase HttpClient parallel sent requests (#31859)
    The HTTPClient used in watcher is based on the apache http client. The
    current client is using a lot of defaults - which are not always
    optimal. Two of those defaults are the maximum number of total
    connections and the maximum number of connections to a single route.
    
    If one of those limits is reached, the HTTPClient waits for a connection
    to be finished thus acting in a blocking fashion. In order to prevent
    this when many requests are being executed, we increase the limit of
    total connections as well as the connections per route (a route is
    basically an endpoint, which also contains proxy information, not
    containing an URL, just hosts).
    
    On top of that an additional option has been set to evict
    long running connections, which can potentially be reused after some
    time. As this requires an additional background thread, this required
    some changes to ensure that the httpclient is closed properly.
    
    This is a backport of #30130

--- x-pack/plugin/watcher/src/main/java/org/elasticsearch/xpack/watcher/Watcher.java
+++ x-pack/plugin/watcher/src/main/java/org/elasticsearch/xpack/watcher/Watcher.java
@@ -30,6 +30,7 @@ import org.elasticsearch.common.settings.SettingsFilter;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
+import org.elasticsearch.core.internal.io.IOUtils;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.env.NodeEnvironment;
 import org.elasticsearch.index.IndexModule;
@@ -221,6 +222,7 @@ public class Watcher extends Plugin implements ActionPlugin, ScriptPlugin {
 
     private static final Logger logger = Loggers.getLogger(Watcher.class);
     private WatcherIndexingListener listener;
+    private HttpClient httpClient;
 
 //    public List<NamedWriteableRegistry.Entry> getNamedWriteables() {
 //        List<NamedWriteableRegistry.Entry> entries = new ArrayList<>();
@@ -287,7 +289,7 @@ public class Watcher extends Plugin implements ActionPlugin, ScriptPlugin {
         // TODO: add more auth types, or remove this indirection
         HttpAuthRegistry httpAuthRegistry = new HttpAuthRegistry(httpAuthFactories);
         HttpRequestTemplate.Parser httpTemplateParser = new HttpRequestTemplate.Parser(httpAuthRegistry);
-        final HttpClient httpClient = new HttpClient(settings, httpAuthRegistry, getSslService());
+        httpClient = new HttpClient(settings, httpAuthRegistry, getSslService());
 
         // notification
         EmailService emailService = new EmailService(settings, cryptoService, clusterService.getClusterSettings());
@@ -631,5 +633,10 @@ public class Watcher extends Plugin implements ActionPlugin, ScriptPlugin {
     @Override
     public List<ScriptContext> getContexts() {
         return Arrays.asList(Watcher.SCRIPT_SEARCH_CONTEXT, Watcher.SCRIPT_EXECUTABLE_CONTEXT, Watcher.SCRIPT_TEMPLATE_CONTEXT);
+    }
+
+    @Override
+    public void close() throws IOException {
+        IOUtils.closeWhileHandlingException(httpClient);
     }
 }

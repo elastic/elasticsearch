f1032e46933be0702023b82dc2be9aa583f6272e
Tal Levy <jubboy333@gmail.com>
2018-07-10 13:35:30 -0400

    fix checkstyle

--- server/src/main/java/org/elasticsearch/cluster/metadata/IndexNameExpressionResolver.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/IndexNameExpressionResolver.java
@@ -42,7 +42,6 @@ import org.joda.time.format.DateTimeFormatter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;

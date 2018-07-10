1f090d3e1c903316c5d5784894cd940f2b29302b
Nik Everett <nik9000@gmail.com>
2018-07-10 13:01:28 -0400

    HLRest: Move xPackInfo() to xPack().info() (#31905)
    Originally I put the X-Pack info object into the top level rest client
    object. I did that because we thought we'd like to squash `xpack` from
    the name of the X-Pack APIs now that it is part of the default
    distribution. We still kind of want to do that, but at least for now we
    feel like it is better to keep the high level rest client aligned with
    the other language clients like C# and Python. This shifts the X-Pack
    info API to align with its json spec file.
    
    Relates to #31870

--- client/rest-high-level/src/test/java/org/elasticsearch/client/documentation/MiscellaneousDocumentationIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/documentation/MiscellaneousDocumentationIT.java
@@ -87,7 +87,7 @@ public class MiscellaneousDocumentationIT extends ESRestHighLevelClientTestCase 
                     XPackInfoRequest.Category.BUILD,
                     XPackInfoRequest.Category.LICENSE,
                     XPackInfoRequest.Category.FEATURES));
-            XPackInfoResponse response = client.xPackInfo(request, RequestOptions.DEFAULT);
+            XPackInfoResponse response = client.xpack().info(request, RequestOptions.DEFAULT);
             //end::x-pack-info-execute
 
             //tag::x-pack-info-response
@@ -123,7 +123,7 @@ public class MiscellaneousDocumentationIT extends ESRestHighLevelClientTestCase 
             listener = new LatchedActionListener<>(listener, latch);
 
             // tag::x-pack-info-execute-async
-            client.xPackInfoAsync(request, RequestOptions.DEFAULT, listener); // <1>
+            client.xpack().infoAsync(request, RequestOptions.DEFAULT, listener); // <1>
             // end::x-pack-info-execute-async
 
             assertTrue(latch.await(30L, TimeUnit.SECONDS));

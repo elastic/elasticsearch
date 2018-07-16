a87ae91717a9b133a33916f2d582da2fa849cc37
Alexander Reelsen <alexander@reelsen.net>
2018-07-17 00:05:08 +0200

    Core: Backport java time date formatters (#31997)
    * Date: Add DateFormatters class that uses java.time (#31856)
    
    A newly added class called DateFormatters now contains java.time based
    builders for dates, which also intends to be fully backwards compatible,
    when the name based date formatters are picked. Also a new class named
    CompoundDateTimeFormatter for being able to parse multiple different
    formats has been added.
    
    A duelling test class has been added that ensures the same dates when
    parsing java or joda time formatted dates for the name based dates.
    
    Note, that java.time and joda time are not fully backwards compatible,
    which also means that old formats will currently not work with this
    setup.
    
    * Tests: Remove use of joda time in some tests (#31922)
    
    This also extends the dateformatters test to ensure that the printers
    are acting the same in java time and joda time.
    
    * Tests: Fix SearchFieldsIT.testDocValueFields
    
    This test produced different implementations of joda time classes,
    depending on if the data was serialized or not (DateTime vs
    MutableDateTime). This now uses a common base class to extract the
    milliseconds from the data.
    
    Closes #31992

--- server/src/test/java/org/elasticsearch/search/aggregations/bucket/MinDocCountIT.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/bucket/MinDocCountIT.java
@@ -22,10 +22,10 @@ package org.elasticsearch.search.aggregations.bucket;
 import com.carrotsearch.hppc.LongHashSet;
 import com.carrotsearch.hppc.LongSet;
 import com.carrotsearch.randomizedtesting.generators.RandomStrings;
-
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.common.time.DateFormatters;
 import org.elasticsearch.index.fielddata.ScriptDocValues;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -40,10 +40,9 @@ import org.elasticsearch.search.aggregations.bucket.terms.IncludeExclude;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
 import org.elasticsearch.test.ESIntegTestCase;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.format.DateTimeFormat;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -123,8 +122,9 @@ public class MinDocCountIT extends AbstractTermsTestCase {
                 longTerm = randomInt(cardinality * 2);
             } while (!longTerms.add(longTerm));
             double doubleTerm = longTerm * Math.PI;
-            String dateTerm = DateTimeFormat.forPattern("yyyy-MM-dd")
-                    .print(new DateTime(2014, 1, ((int) longTerm % 20) + 1, 0, 0, DateTimeZone.UTC));
+
+            ZonedDateTime time = ZonedDateTime.of(2014, 1, ((int) longTerm % 20) + 1, 0, 0, 0, 0, ZoneOffset.UTC);
+            String dateTerm = DateFormatters.forPattern("yyyy-MM-dd").format(time);
             final int frequency = randomBoolean() ? 1 : randomIntBetween(2, 20);
             for (int j = 0; j < frequency; ++j) {
                 indexRequests.add(client().prepareIndex("idx", "type").setSource(jsonBuilder()

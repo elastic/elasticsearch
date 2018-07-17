6cfa0328a5cfd8afb03548437823ea4e5c1d0ad1
Julie Tibshirani <julie.tibshirani@elastic.co>
2018-07-17 12:49:19 -0700

    Add more comprehensive tests for field aliases in queries + aggregations. (#31565)
    * Make sure that significant terms aggregations work with field aliases.
    * Add a test for ValuesSourceConfig.
    * Allow for subclasses of AggregatorTestCase to provide field aliases.
    * Add tests for nested and reverse_nested aggregations.
    * Add an integration test for nested queries.
    * Add an integration test for 'more like this' queries.
    * Add tests for querying and loading meta-fields.
    * Add unit tests for the relevant query builders.
    * Add integration tests for geo polygon and shape queries.
    * Fix static analysis violations.
    * Document that aliases cannot be used in a query lookup path.

--- server/src/test/java/org/elasticsearch/index/query/MatchQueryBuilderTests.java
+++ server/src/test/java/org/elasticsearch/index/query/MatchQueryBuilderTests.java
@@ -19,6 +19,7 @@
 
 package org.elasticsearch.index.query;
 
+import org.apache.lucene.index.Term;
 import org.apache.lucene.queries.ExtendedCommonTermsQuery;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
@@ -47,6 +48,7 @@ import org.hamcrest.Matcher;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -59,13 +61,13 @@ import static org.hamcrest.Matchers.notNullValue;
 public class MatchQueryBuilderTests extends AbstractQueryTestCase<MatchQueryBuilder> {
     @Override
     protected MatchQueryBuilder doCreateTestQueryBuilder() {
-        String fieldName = randomFrom(STRING_FIELD_NAME, BOOLEAN_FIELD_NAME, INT_FIELD_NAME,
-                DOUBLE_FIELD_NAME, DATE_FIELD_NAME);
+        String fieldName = STRING_ALIAS_FIELD_NAME; //randomFrom(STRING_FIELD_NAME, STRING_ALIAS_FIELD_NAME, BOOLEAN_FIELD_NAME,
+            //INT_FIELD_NAME, DOUBLE_FIELD_NAME, DATE_FIELD_NAME);
         if (fieldName.equals(DATE_FIELD_NAME)) {
             assumeTrue("test runs only when at least a type is registered", getCurrentTypes().length > 0);
         }
         Object value;
-        if (fieldName.equals(STRING_FIELD_NAME)) {
+        if (isTextField(fieldName)) {
             int terms = randomIntBetween(0, 3);
             StringBuilder builder = new StringBuilder();
             for (int i = 0; i < terms; i++) {
@@ -79,11 +81,11 @@ public class MatchQueryBuilderTests extends AbstractQueryTestCase<MatchQueryBuil
         MatchQueryBuilder matchQuery = new MatchQueryBuilder(fieldName, value);
         matchQuery.operator(randomFrom(Operator.values()));
 
-        if (randomBoolean() && fieldName.equals(STRING_FIELD_NAME)) {
+        if (randomBoolean() && isTextField(fieldName)) {
             matchQuery.analyzer(randomFrom("simple", "keyword", "whitespace"));
         }
 
-        if (fieldName.equals(STRING_FIELD_NAME) && randomBoolean()) {
+        if (isTextField(fieldName) && randomBoolean()) {
             matchQuery.fuzziness(randomFuzziness(fieldName));
         }
 
@@ -179,6 +181,12 @@ public class MatchQueryBuilderTests extends AbstractQueryTestCase<MatchQueryBuil
         if (query instanceof ExtendedCommonTermsQuery) {
             assertTrue(queryBuilder.cutoffFrequency() != null);
             ExtendedCommonTermsQuery ectq = (ExtendedCommonTermsQuery) query;
+            List<Term> terms = ectq.getTerms();
+            if (!terms.isEmpty()) {
+                Term term = terms.iterator().next();
+                String expectedFieldName = expectedFieldName(queryBuilder.fieldName());
+                assertThat(term.field(), equalTo(expectedFieldName));
+            }
             assertEquals(queryBuilder.cutoffFrequency(), ectq.getMaxTermFrequency(), Float.MIN_VALUE);
         }
 
@@ -195,6 +203,9 @@ public class MatchQueryBuilderTests extends AbstractQueryTestCase<MatchQueryBuil
                 termLcMatcher = either(termLcMatcher).or(equalTo(originalTermLc.substring(0, 1)));
             }
             assertThat(actualTermLc, termLcMatcher);
+
+            String expectedFieldName = expectedFieldName(queryBuilder.fieldName());
+            assertThat(expectedFieldName, equalTo(fuzzyQuery.getTerm().field()));
             assertThat(queryBuilder.prefixLength(), equalTo(fuzzyQuery.getPrefixLength()));
             assertThat(queryBuilder.fuzzyTranspositions(), equalTo(fuzzyQuery.getTranspositions()));
         }

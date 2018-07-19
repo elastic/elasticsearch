e5824d727c105798b4fd12ea0b99283ae40dac0e
Ioannis Kakavas <ioannis@elastic.co>
2018-07-19 14:18:47 +0300

    Remove BouncyCastle dependency from runtime
    This commit introduces a new project that builds a Jar of the
    classes that have a dependency on BouncyCastle and puts it in a
    subdirectory of lib (/tools/security-cli) along with the
    BouncyCastle jars. This directory is then passed in the
    ES_ADDITIONAL_CLASSPATH_DIRECTORIES of the CLI tools that use
    these classes.
    
    BouncyCastle is removed as a runtime dependency (remains
    as a compileOnly one) from x-pack core and x-pack security.

--- x-pack/plugin/security/src/main/bin/elasticsearch-certgen.bat
+++ x-pack/plugin/security/src/main/bin/elasticsearch-certgen.bat
@@ -9,6 +9,7 @@ setlocal enableextensions
 
 set ES_MAIN_CLASS=org.elasticsearch.xpack.core.ssl.CertificateGenerateTool
 set ES_ADDITIONAL_SOURCES=x-pack-env;x-pack-security-env
+set ES_ADDITIONAL_CLASSPATH_DIRECTORIES=lib/tools/security-cli
 call "%~dp0elasticsearch-cli.bat" ^
   %%* ^
   || exit /b 1

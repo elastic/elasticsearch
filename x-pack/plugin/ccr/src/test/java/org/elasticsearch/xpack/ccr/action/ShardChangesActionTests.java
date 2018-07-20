0865c8b152a5b6336ebb9a6da60cb844f813b260
Nhat Nguyen <nhat.nguyen@elastic.co>
2018-07-20 17:18:26 -0400

    TEST: Fix range issue in ShardChangesActionTests
    We modified the way we calculate to_seqno in #32121 but did not adjust
    this test accordingly. If min_seqno equals to max_seqno, the size should be
    one instead of zero.
    
    Relates #32121

--- x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/action/ShardChangesActionTests.java
+++ x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/action/ShardChangesActionTests.java
@@ -44,11 +44,11 @@ public class ShardChangesActionTests extends ESSingleNodeTestCase {
         for (int iter = 0; iter < iters; iter++) {
             int min = randomIntBetween(0, numWrites - 1);
             int max = randomIntBetween(min, numWrites - 1);
-            int size = max - min;
+            int size = max - min + 1;
             final Translog.Operation[] operations = ShardChangesAction.getOperations(indexShard,
                 indexShard.getGlobalCheckpoint(), min, size, Long.MAX_VALUE);
             final List<Long> seenSeqNos = Arrays.stream(operations).map(Translog.Operation::seqNo).collect(Collectors.toList());
-            final List<Long> expectedSeqNos = LongStream.range(min, max).boxed().collect(Collectors.toList());
+            final List<Long> expectedSeqNos = LongStream.rangeClosed(min, max).boxed().collect(Collectors.toList());
             assertThat(seenSeqNos, equalTo(expectedSeqNos));
         }
 

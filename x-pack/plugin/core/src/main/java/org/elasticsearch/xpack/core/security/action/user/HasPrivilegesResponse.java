openeuler-mirror/poissonsearch-oss

Introduce Application Privileges with support for Kibana RBAC (#32309)
This commit introduces "Application Privileges" to the X-Pack security
model.

Application Privileges are managed within Elasticsearch, and can be
tested with the _has_privileges API, but do not grant access to any
actions or resources within Elasticsearch. Their purpose is to allow
applications outside of Elasticsearch to represent and store their own
privileges model within Elasticsearch roles.

Access to manage application privileges is handled in a new way that
grants permission to specific application names only. This lays the
foundation for more OLS on cluster privileges, which is implemented by
allowing a cluster permission to inspect not just the action being
executed, but also the request to which the action is applied.
To support this, a "conditional cluster privilege" is introduced, which
is like the existing cluster privilege, except that it has a Predicate
over the request as well as over the action name.

Specifically, this adds
- GET/PUT/DELETE actions for defining application level privileges
- application privileges in role definitions
- application privileges in the has_privileges API
- changes to the cluster permission class to support checking of request
  objects
- a new "global" element on role definition to provide cluster object
  level security (only for manage application privileges)
- changes to `kibana_user`, `kibana_dashboard_only_user` and
  `kibana_system` roles to use and manage application privileges

Closes #29820
Closes #31559

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/user/HasPrivilegesResponse.java
@@ -5,6 +5,11 @@
  */
 package org.elasticsearch.xpack.core.security.action.user;
 
+import org.elasticsearch.Version;
+import org.elasticsearch.action.ActionResponse;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -14,27 +19,27 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-import org.elasticsearch.action.ActionResponse;
-import org.elasticsearch.common.io.stream.StreamInput;
-import org.elasticsearch.common.io.stream.StreamOutput;
-
 /**
  * Response for a {@link HasPrivilegesRequest}
  */
 public class HasPrivilegesResponse extends ActionResponse {
     private boolean completeMatch;
     private Map<String, Boolean> cluster;
-    private List<IndexPrivileges> index;
+    private List<ResourcePrivileges> index;
+    private Map<String, List<ResourcePrivileges>> application;
 
     public HasPrivilegesResponse() {
-        this(true, Collections.emptyMap(), Collections.emptyList());
+        this(true, Collections.emptyMap(), Collections.emptyList(), Collections.emptyMap());
     }
 
-    public HasPrivilegesResponse(boolean completeMatch, Map<String, Boolean> cluster, Collection<IndexPrivileges> index) {
+    public HasPrivilegesResponse(boolean completeMatch, Map<String, Boolean> cluster, Collection<ResourcePrivileges> index,
+                                 Map<String, Collection<ResourcePrivileges>> application) {
         super();
         this.completeMatch = completeMatch;
         this.cluster = new HashMap<>(cluster);
         this.index = new ArrayList<>(index);
+        this.application = new HashMap<>();
+        application.forEach((key, val) -> this.application.put(key, Collections.unmodifiableList(new ArrayList<>(val))));
     }
 
     public boolean isCompleteMatch() {
@@ -45,44 +50,67 @@ public class HasPrivilegesResponse extends ActionResponse {
         return Collections.unmodifiableMap(cluster);
     }
 
-    public List<IndexPrivileges> getIndexPrivileges() {
+    public List<ResourcePrivileges> getIndexPrivileges() {
         return Collections.unmodifiableList(index);
+    }
+
+    /**
+     * Retrieves the results from checking application privileges,
+     * @return A {@code Map} keyed by application-name
+     */
+    public Map<String, List<ResourcePrivileges>> getApplicationPrivileges() {
+        return Collections.unmodifiableMap(application);
     }
 
     public void readFrom(StreamInput in) throws IOException {
         super.readFrom(in);
         completeMatch = in.readBoolean();
-        int count = in.readVInt();
-        index = new ArrayList<>(count);
+        index = readResourcePrivileges(in);
+        if (in.getVersion().onOrAfter(Version.V_7_0_0_alpha1)) {
+            application = in.readMap(StreamInput::readString, HasPrivilegesResponse::readResourcePrivileges);
+        }
+    }
+
+    private static List<ResourcePrivileges> readResourcePrivileges(StreamInput in) throws IOException {
+        final int count = in.readVInt();
+        final List<ResourcePrivileges> list = new ArrayList<>(count);
         for (int i = 0; i < count; i++) {
             final String index = in.readString();
             final Map<String, Boolean> privileges = in.readMap(StreamInput::readString, StreamInput::readBoolean);
-            this.index.add(new IndexPrivileges(index, privileges));
+            list.add(new ResourcePrivileges(index, privileges));
         }
+        return list;
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         super.writeTo(out);
         out.writeBoolean(completeMatch);
-        out.writeVInt(index.size());
-        for (IndexPrivileges index : index) {
-            out.writeString(index.index);
-            out.writeMap(index.privileges, StreamOutput::writeString, StreamOutput::writeBoolean);
+        writeResourcePrivileges(out, index);
+        if (out.getVersion().onOrAfter(Version.V_7_0_0_alpha1)) {
+            out.writeMap(application, StreamOutput::writeString, HasPrivilegesResponse::writeResourcePrivileges);
         }
     }
 
-    public static class IndexPrivileges {
-        private final String index;
+    private static void writeResourcePrivileges(StreamOutput out, List<ResourcePrivileges> privileges) throws IOException {
+        out.writeVInt(privileges.size());
+        for (ResourcePrivileges priv : privileges) {
+            out.writeString(priv.resource);
+            out.writeMap(priv.privileges, StreamOutput::writeString, StreamOutput::writeBoolean);
+        }
+    }
+
+    public static class ResourcePrivileges {
+        private final String resource;
         private final Map<String, Boolean> privileges;
 
-        public IndexPrivileges(String index, Map<String, Boolean> privileges) {
-            this.index = Objects.requireNonNull(index);
+        public ResourcePrivileges(String resource, Map<String, Boolean> privileges) {
+            this.resource = Objects.requireNonNull(resource);
             this.privileges = Collections.unmodifiableMap(privileges);
         }
 
-        public String getIndex() {
-            return index;
+        public String getResource() {
+            return resource;
         }
 
         public Map<String, Boolean> getPrivileges() {
@@ -92,14 +120,14 @@ public class HasPrivilegesResponse extends ActionResponse {
         @Override
         public String toString() {
             return getClass().getSimpleName() + "{" +
-                    "index='" + index + '\'' +
+                    "resource='" + resource + '\'' +
                     ", privileges=" + privileges +
                     '}';
         }
 
         @Override
         public int hashCode() {
-            int result = index.hashCode();
+            int result = resource.hashCode();
             result = 31 * result + privileges.hashCode();
             return result;
         }
@@ -113,8 +141,8 @@ public class HasPrivilegesResponse extends ActionResponse {
                 return false;
             }
 
-            final IndexPrivileges other = (IndexPrivileges) o;
-            return this.index.equals(other.index) && this.privileges.equals(other.privileges);
+            final ResourcePrivileges other = (ResourcePrivileges) o;
+            return this.resource.equals(other.resource) && this.privileges.equals(other.privileges);
         }
     }
 }

x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/action/user/HasPrivilegesRequestTests.java
@@ -1,0 +1,125 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+package org.elasticsearch.xpack.core.security.action.user;
+
+import org.elasticsearch.Version;
+import org.elasticsearch.action.ActionRequestValidationException;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.io.stream.BytesStreamOutput;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.xpack.core.security.authz.RoleDescriptor.ApplicationResourcePrivileges;
+import org.elasticsearch.xpack.core.security.authz.RoleDescriptor.IndicesPrivileges;
+import org.elasticsearch.xpack.core.security.authz.privilege.ClusterPrivilege;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
+public class HasPrivilegesRequestTests extends ESTestCase {
+
+    public void testSerializationV7() throws IOException {
+        final HasPrivilegesRequest original = randomRequest();
+        final HasPrivilegesRequest copy = serializeAndDeserialize(original, Version.V_7_0_0_alpha1);
+
+        assertThat(copy.username(), equalTo(original.username()));
+        assertThat(copy.clusterPrivileges(), equalTo(original.clusterPrivileges()));
+        assertThat(copy.indexPrivileges(), equalTo(original.indexPrivileges()));
+        assertThat(copy.applicationPrivileges(), equalTo(original.applicationPrivileges()));
+    }
+
+    public void testSerializationV63() throws IOException {
+        final HasPrivilegesRequest original = randomRequest();
+        final HasPrivilegesRequest copy = serializeAndDeserialize(original, Version.V_6_3_0);
+
+        assertThat(copy.username(), equalTo(original.username()));
+        assertThat(copy.clusterPrivileges(), equalTo(original.clusterPrivileges()));
+        assertThat(copy.indexPrivileges(), equalTo(original.indexPrivileges()));
+        assertThat(copy.applicationPrivileges(), nullValue());
+    }
+
+    public void testValidateNullPrivileges() {
+        final HasPrivilegesRequest request = new HasPrivilegesRequest();
+        final ActionRequestValidationException exception = request.validate();
+        assertThat(exception, notNullValue());
+        assertThat(exception.validationErrors(), hasItem("clusterPrivileges must not be null"));
+        assertThat(exception.validationErrors(), hasItem("indexPrivileges must not be null"));
+        assertThat(exception.validationErrors(), hasItem("applicationPrivileges must not be null"));
+    }
+
+    public void testValidateEmptyPrivileges() {
+        final HasPrivilegesRequest request = new HasPrivilegesRequest();
+        request.clusterPrivileges(new String[0]);
+        request.indexPrivileges(new IndicesPrivileges[0]);
+        request.applicationPrivileges(new ApplicationResourcePrivileges[0]);
+        final ActionRequestValidationException exception = request.validate();
+        assertThat(exception, notNullValue());
+        assertThat(exception.validationErrors(), hasItem("must specify at least one privilege"));
+    }
+
+    public void testValidateNoWildcardApplicationPrivileges() {
+        final HasPrivilegesRequest request = new HasPrivilegesRequest();
+        request.clusterPrivileges(new String[0]);
+        request.indexPrivileges(new IndicesPrivileges[0]);
+        request.applicationPrivileges(new ApplicationResourcePrivileges[] {
+            ApplicationResourcePrivileges.builder().privileges("read").application("*").resources("item/1").build()
+        });
+        final ActionRequestValidationException exception = request.validate();
+        assertThat(exception, notNullValue());
+        assertThat(exception.validationErrors(), hasItem("Application names may not contain '*' (found '*')"));
+    }
+
+    private HasPrivilegesRequest serializeAndDeserialize(HasPrivilegesRequest original, Version version) throws IOException {
+        final BytesStreamOutput out = new BytesStreamOutput();
+        out.setVersion(version);
+        original.writeTo(out);
+
+        final HasPrivilegesRequest copy = new HasPrivilegesRequest();
+        final StreamInput in = out.bytes().streamInput();
+        in.setVersion(version);
+        copy.readFrom(in);
+        assertThat(in.read(), equalTo(-1));
+        return copy;
+    }
+
+    private HasPrivilegesRequest randomRequest() {
+        final HasPrivilegesRequest request = new HasPrivilegesRequest();
+        request.username(randomAlphaOfLength(8));
+
+        final List<String> clusterPrivileges = randomSubsetOf(Arrays.asList(ClusterPrivilege.MONITOR, ClusterPrivilege.MANAGE,
+            ClusterPrivilege.MANAGE_ML, ClusterPrivilege.MANAGE_SECURITY, ClusterPrivilege.MANAGE_PIPELINE, ClusterPrivilege.ALL))
+            .stream().flatMap(p -> p.name().stream()).collect(Collectors.toList());
+        request.clusterPrivileges(clusterPrivileges.toArray(Strings.EMPTY_ARRAY));
+
+        IndicesPrivileges[] indicesPrivileges = new IndicesPrivileges[randomInt(5)];
+        for (int i = 0; i < indicesPrivileges.length; i++) {
+            indicesPrivileges[i] = IndicesPrivileges.builder()
+                .privileges(randomFrom("read", "write", "create", "delete", "all"))
+                .indices(randomAlphaOfLengthBetween(2, 8) + (randomBoolean() ? "*" : ""))
+                .build();
+        }
+        request.indexPrivileges(indicesPrivileges);
+
+        final ApplicationResourcePrivileges[] appPrivileges = new ApplicationResourcePrivileges[randomInt(5)];
+        for (int i = 0; i < appPrivileges.length; i++) {
+            appPrivileges[i] = ApplicationResourcePrivileges.builder()
+                .application(randomAlphaOfLengthBetween(3, 8))
+                .resources(randomAlphaOfLengthBetween(5, 7) + (randomBoolean() ? "*" : ""))
+                .privileges(generateRandomStringArray(6, 7, false, false))
+                .build();
+        }
+        request.applicationPrivileges(appPrivileges);
+        return request;
+    }
+
+}

elastic/elasticsearch

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/user/HasPrivilegesResponse.java
@@ -5,6 +5,11 @@
  */
 package org.elasticsearch.xpack.core.security.action.user;
 
+import org.elasticsearch.Version;
+import org.elasticsearch.action.ActionResponse;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -14,27 +19,27 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-import org.elasticsearch.action.ActionResponse;
-import org.elasticsearch.common.io.stream.StreamInput;
-import org.elasticsearch.common.io.stream.StreamOutput;
-
 /**
  * Response for a {@link HasPrivilegesRequest}
  */
 public class HasPrivilegesResponse extends ActionResponse {
     private boolean completeMatch;
     private Map<String, Boolean> cluster;
-    private List<IndexPrivileges> index;
+    private List<ResourcePrivileges> index;
+    private Map<String, List<ResourcePrivileges>> application;
 
     public HasPrivilegesResponse() {
-        this(true, Collections.emptyMap(), Collections.emptyList());
+        this(true, Collections.emptyMap(), Collections.emptyList(), Collections.emptyMap());
     }
 
-    public HasPrivilegesResponse(boolean completeMatch, Map<String, Boolean> cluster, Collection<IndexPrivileges> index) {
+    public HasPrivilegesResponse(boolean completeMatch, Map<String, Boolean> cluster, Collection<ResourcePrivileges> index,
+                                 Map<String, Collection<ResourcePrivileges>> application) {
         super();
         this.completeMatch = completeMatch;
         this.cluster = new HashMap<>(cluster);
         this.index = new ArrayList<>(index);
+        this.application = new HashMap<>();
+        application.forEach((key, val) -> this.application.put(key, Collections.unmodifiableList(new ArrayList<>(val))));
     }
 
     public boolean isCompleteMatch() {
@@ -45,44 +50,67 @@ public class HasPrivilegesResponse extends ActionResponse {
         return Collections.unmodifiableMap(cluster);
     }
 
-    public List<IndexPrivileges> getIndexPrivileges() {
+    public List<ResourcePrivileges> getIndexPrivileges() {
         return Collections.unmodifiableList(index);
+    }
+
+    /**
+     * Retrieves the results from checking application privileges,
+     * @return A {@code Map} keyed by application-name
+     */
+    public Map<String, List<ResourcePrivileges>> getApplicationPrivileges() {
+        return Collections.unmodifiableMap(application);
     }
 
     public void readFrom(StreamInput in) throws IOException {
         super.readFrom(in);
         completeMatch = in.readBoolean();
-        int count = in.readVInt();
-        index = new ArrayList<>(count);
+        index = readResourcePrivileges(in);
+        if (in.getVersion().onOrAfter(Version.V_6_4_0)) {
+            application = in.readMap(StreamInput::readString, HasPrivilegesResponse::readResourcePrivileges);
+        }
+    }
+
+    private static List<ResourcePrivileges> readResourcePrivileges(StreamInput in) throws IOException {
+        final int count = in.readVInt();
+        final List<ResourcePrivileges> list = new ArrayList<>(count);
         for (int i = 0; i < count; i++) {
             final String index = in.readString();
             final Map<String, Boolean> privileges = in.readMap(StreamInput::readString, StreamInput::readBoolean);
-            this.index.add(new IndexPrivileges(index, privileges));
+            list.add(new ResourcePrivileges(index, privileges));
         }
+        return list;
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         super.writeTo(out);
         out.writeBoolean(completeMatch);
-        out.writeVInt(index.size());
-        for (IndexPrivileges index : index) {
-            out.writeString(index.index);
-            out.writeMap(index.privileges, StreamOutput::writeString, StreamOutput::writeBoolean);
+        writeResourcePrivileges(out, index);
+        if (out.getVersion().onOrAfter(Version.V_6_4_0)) {
+            out.writeMap(application, StreamOutput::writeString, HasPrivilegesResponse::writeResourcePrivileges);
         }
     }
 
-    public static class IndexPrivileges {
-        private final String index;
+    private static void writeResourcePrivileges(StreamOutput out, List<ResourcePrivileges> privileges) throws IOException {
+        out.writeVInt(privileges.size());
+        for (ResourcePrivileges priv : privileges) {
+            out.writeString(priv.resource);
+            out.writeMap(priv.privileges, StreamOutput::writeString, StreamOutput::writeBoolean);
+        }
+    }
+
+    public static class ResourcePrivileges {
+        private final String resource;
         private final Map<String, Boolean> privileges;
 
-        public IndexPrivileges(String index, Map<String, Boolean> privileges) {
-            this.index = Objects.requireNonNull(index);
+        public ResourcePrivileges(String resource, Map<String, Boolean> privileges) {
+            this.resource = Objects.requireNonNull(resource);
             this.privileges = Collections.unmodifiableMap(privileges);
         }
 
-        public String getIndex() {
-            return index;
+        public String getResource() {
+            return resource;
         }
 
         public Map<String, Boolean> getPrivileges() {
@@ -92,14 +120,14 @@ public class HasPrivilegesResponse extends ActionResponse {
         @Override
         public String toString() {
             return getClass().getSimpleName() + "{" +
-                    "index='" + index + '\'' +
+                    "resource='" + resource + '\'' +
                     ", privileges=" + privileges +
                     '}';
         }
 
         @Override
         public int hashCode() {
-            int result = index.hashCode();
+            int result = resource.hashCode();
             result = 31 * result + privileges.hashCode();
             return result;
         }
@@ -113,8 +141,8 @@ public class HasPrivilegesResponse extends ActionResponse {
                 return false;
             }
 
-            final IndexPrivileges other = (IndexPrivileges) o;
-            return this.index.equals(other.index) && this.privileges.equals(other.privileges);
+            final ResourcePrivileges other = (ResourcePrivileges) o;
+            return this.resource.equals(other.resource) && this.privileges.equals(other.privileges);
         }
     }
 }

x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/action/user/HasPrivilegesRequestTests.java
@@ -1,0 +1,125 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+package org.elasticsearch.xpack.core.security.action.user;
+
+import org.elasticsearch.Version;
+import org.elasticsearch.action.ActionRequestValidationException;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.io.stream.BytesStreamOutput;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.xpack.core.security.authz.RoleDescriptor.ApplicationResourcePrivileges;
+import org.elasticsearch.xpack.core.security.authz.RoleDescriptor.IndicesPrivileges;
+import org.elasticsearch.xpack.core.security.authz.privilege.ClusterPrivilege;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
+public class HasPrivilegesRequestTests extends ESTestCase {
+
+    public void testSerializationV7() throws IOException {
+        final HasPrivilegesRequest original = randomRequest();
+        final HasPrivilegesRequest copy = serializeAndDeserialize(original, Version.V_6_4_0);
+
+        assertThat(copy.username(), equalTo(original.username()));
+        assertThat(copy.clusterPrivileges(), equalTo(original.clusterPrivileges()));
+        assertThat(copy.indexPrivileges(), equalTo(original.indexPrivileges()));
+        assertThat(copy.applicationPrivileges(), equalTo(original.applicationPrivileges()));
+    }
+
+    public void testSerializationV63() throws IOException {
+        final HasPrivilegesRequest original = randomRequest();
+        final HasPrivilegesRequest copy = serializeAndDeserialize(original, Version.V_6_3_0);
+
+        assertThat(copy.username(), equalTo(original.username()));
+        assertThat(copy.clusterPrivileges(), equalTo(original.clusterPrivileges()));
+        assertThat(copy.indexPrivileges(), equalTo(original.indexPrivileges()));
+        assertThat(copy.applicationPrivileges(), nullValue());
+    }
+
+    public void testValidateNullPrivileges() {
+        final HasPrivilegesRequest request = new HasPrivilegesRequest();
+        final ActionRequestValidationException exception = request.validate();
+        assertThat(exception, notNullValue());
+        assertThat(exception.validationErrors(), hasItem("clusterPrivileges must not be null"));
+        assertThat(exception.validationErrors(), hasItem("indexPrivileges must not be null"));
+        assertThat(exception.validationErrors(), hasItem("applicationPrivileges must not be null"));
+    }
+
+    public void testValidateEmptyPrivileges() {
+        final HasPrivilegesRequest request = new HasPrivilegesRequest();
+        request.clusterPrivileges(new String[0]);
+        request.indexPrivileges(new IndicesPrivileges[0]);
+        request.applicationPrivileges(new ApplicationResourcePrivileges[0]);
+        final ActionRequestValidationException exception = request.validate();
+        assertThat(exception, notNullValue());
+        assertThat(exception.validationErrors(), hasItem("must specify at least one privilege"));
+    }
+
+    public void testValidateNoWildcardApplicationPrivileges() {
+        final HasPrivilegesRequest request = new HasPrivilegesRequest();
+        request.clusterPrivileges(new String[0]);
+        request.indexPrivileges(new IndicesPrivileges[0]);
+        request.applicationPrivileges(new ApplicationResourcePrivileges[] {
+            ApplicationResourcePrivileges.builder().privileges("read").application("*").resources("item/1").build()
+        });
+        final ActionRequestValidationException exception = request.validate();
+        assertThat(exception, notNullValue());
+        assertThat(exception.validationErrors(), hasItem("Application names may not contain '*' (found '*')"));
+    }
+
+    private HasPrivilegesRequest serializeAndDeserialize(HasPrivilegesRequest original, Version version) throws IOException {
+        final BytesStreamOutput out = new BytesStreamOutput();
+        out.setVersion(version);
+        original.writeTo(out);
+
+        final HasPrivilegesRequest copy = new HasPrivilegesRequest();
+        final StreamInput in = out.bytes().streamInput();
+        in.setVersion(version);
+        copy.readFrom(in);
+        assertThat(in.read(), equalTo(-1));
+        return copy;
+    }
+
+    private HasPrivilegesRequest randomRequest() {
+        final HasPrivilegesRequest request = new HasPrivilegesRequest();
+        request.username(randomAlphaOfLength(8));
+
+        final List<String> clusterPrivileges = randomSubsetOf(Arrays.asList(ClusterPrivilege.MONITOR, ClusterPrivilege.MANAGE,
+            ClusterPrivilege.MANAGE_ML, ClusterPrivilege.MANAGE_SECURITY, ClusterPrivilege.MANAGE_PIPELINE, ClusterPrivilege.ALL))
+            .stream().flatMap(p -> p.name().stream()).collect(Collectors.toList());
+        request.clusterPrivileges(clusterPrivileges.toArray(Strings.EMPTY_ARRAY));
+
+        IndicesPrivileges[] indicesPrivileges = new IndicesPrivileges[randomInt(5)];
+        for (int i = 0; i < indicesPrivileges.length; i++) {
+            indicesPrivileges[i] = IndicesPrivileges.builder()
+                .privileges(randomFrom("read", "write", "create", "delete", "all"))
+                .indices(randomAlphaOfLengthBetween(2, 8) + (randomBoolean() ? "*" : ""))
+                .build();
+        }
+        request.indexPrivileges(indicesPrivileges);
+
+        final ApplicationResourcePrivileges[] appPrivileges = new ApplicationResourcePrivileges[randomInt(5)];
+        for (int i = 0; i < appPrivileges.length; i++) {
+            appPrivileges[i] = ApplicationResourcePrivileges.builder()
+                .application(randomAlphaOfLengthBetween(3, 8))
+                .resources(randomAlphaOfLengthBetween(5, 7) + (randomBoolean() ? "*" : ""))
+                .privileges(generateRandomStringArray(6, 7, false, false))
+                .build();
+        }
+        request.applicationPrivileges(appPrivileges);
+        return request;
+    }
+
+}
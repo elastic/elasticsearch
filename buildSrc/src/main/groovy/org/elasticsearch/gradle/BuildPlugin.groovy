04394be7b0d6900bce049de479b0777a1d9b8e70
Alpar Torok <torokalpar@gmail.com>
2018-07-25 06:21:03 +0000

    Determine the minimum gradle version based on the wrapper (#32226)
    * Determine the minimum gradle version based on the wrapper
    
    This is restrictive and forces users of the plugin to move together with
    us, but without integration tests it's close to impossible to make sure
    that the claimed compatability is really there.
    If we do want to offer more flexibility, we should add those tests
    first.
    
    * Track gradle version in individual file
    
    * PR review

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
@@ -20,6 +20,7 @@ package org.elasticsearch.gradle
 
 import com.carrotsearch.gradle.junit4.RandomizedTestingTask
 import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
+import org.apache.commons.io.IOUtils
 import org.apache.tools.ant.taskdefs.condition.Os
 import org.eclipse.jgit.lib.Constants
 import org.eclipse.jgit.lib.RepositoryBuilder
@@ -53,6 +54,7 @@ import org.gradle.internal.jvm.Jvm
 import org.gradle.process.ExecResult
 import org.gradle.util.GradleVersion
 
+import java.nio.charset.StandardCharsets
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 /**
@@ -67,8 +69,13 @@ class BuildPlugin implements Plugin<Project> {
                 + 'elasticearch.standalone-rest-test, and elasticsearch.build '
                 + 'are mutually exclusive')
         }
-        if (GradleVersion.current() < GradleVersion.version('4.9')) {
-            throw new GradleException('Gradle 4.9+ is required to use elasticsearch.build plugin')
+        final String minimumGradleVersion
+        InputStream is = getClass().getResourceAsStream("/minimumGradleVersion")
+        try { minimumGradleVersion = IOUtils.toString(is, StandardCharsets.UTF_8.toString()) } finally { is.close() }
+        if (GradleVersion.current() < GradleVersion.version(minimumGradleVersion.trim())) {
+            throw new GradleException(
+                    "Gradle ${minimumGradleVersion}+ is required to use elasticsearch.build plugin"
+            )
         }
         project.pluginManager.apply('java')
         project.pluginManager.apply('carrotsearch.randomized-testing')
@@ -152,14 +159,6 @@ class BuildPlugin implements Plugin<Project> {
                 println "  JAVA_HOME             : ${gradleJavaHome}"
             }
             println "  Random Testing Seed   : ${project.testSeed}"
-
-            // enforce Gradle version
-            final GradleVersion currentGradleVersion = GradleVersion.current();
-
-            final GradleVersion minGradle = GradleVersion.version('4.3')
-            if (currentGradleVersion < minGradle) {
-                throw new GradleException("${minGradle} or above is required to build Elasticsearch")
-            }
 
             // enforce Java version
             if (compilerJavaVersionEnum < minimumCompilerVersion) {

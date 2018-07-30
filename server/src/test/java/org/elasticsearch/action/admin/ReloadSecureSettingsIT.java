c8b4968f604aeefb3026bf0d135d1098733dca5b
Ioannis Kakavas <ioannis@elastic.co>
2018-07-30 18:55:18 +0300

    Remove unused import

--- server/src/test/java/org/elasticsearch/action/admin/ReloadSecureSettingsIT.java
+++ server/src/test/java/org/elasticsearch/action/admin/ReloadSecureSettingsIT.java
@@ -32,7 +32,6 @@ import org.elasticsearch.plugins.PluginsService;
 import org.elasticsearch.plugins.ReloadablePlugin;
 import org.elasticsearch.test.ESIntegTestCase;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;

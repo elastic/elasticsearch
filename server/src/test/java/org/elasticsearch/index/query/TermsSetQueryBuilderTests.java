adeec7ed1b86e933554236b838cabd2740550942
Julie Tibshirani <julie.tibshirani@elastic.co>
2018-07-31 10:37:54 -0700

    Improve the error message when an index is incompatible with field aliases. (#32482)

--- server/src/test/java/org/elasticsearch/index/query/TermsSetQueryBuilderTests.java
+++ server/src/test/java/org/elasticsearch/index/query/TermsSetQueryBuilderTests.java
@@ -263,7 +263,7 @@ public class TermsSetQueryBuilderTests extends AbstractQueryTestCase<TermsSetQue
     }
 
     public void testFieldAlias() {
-        assumeTrue("Test runs only when there is a single mapping type.", isSingleType());
+        assumeTrue("Test only runs on indexes that enforce a single mapping type.", isSingleType());
 
         List<String> randomTerms = Arrays.asList(generateRandomStringArray(5, 10, false, false));
         TermsSetQueryBuilder queryBuilder = new TermsSetQueryBuilder(STRING_ALIAS_FIELD_NAME, randomTerms)

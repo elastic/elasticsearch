aacbff69cde53edf8d1073f917452b0272881183
Lee Hinman <lee@writequit.org>
2018-08-02 12:23:08 -0600

    Remove ILM constructor hacks
    This commit removes the hacks associated with mocking Response objects. Rather
    than parse a wrapped byte array, the constructors for `IndicesAliasesResponse`
    and `ResizeResponse` are made public
    
    Relates to #29823

--- server/src/main/java/org/elasticsearch/action/admin/indices/alias/IndicesAliasesResponse.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/alias/IndicesAliasesResponse.java
@@ -37,7 +37,7 @@ public class IndicesAliasesResponse extends AcknowledgedResponse {
     IndicesAliasesResponse() {
     }
 
-    IndicesAliasesResponse(boolean acknowledged) {
+    public IndicesAliasesResponse(boolean acknowledged) {
         super(acknowledged);
     }
 

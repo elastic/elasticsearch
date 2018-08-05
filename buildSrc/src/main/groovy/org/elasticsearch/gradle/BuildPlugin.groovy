2ef1e5cc2377fbf0bb1429beff38b98624211c13
Armin <me@obrown.io>
2018-08-05 11:21:03 +0200

    CR: Add backwards compatibility with by putting ctx back into params

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
@@ -782,8 +782,11 @@ class BuildPlugin implements Plugin<Project> {
                 }
             }
 
-            // TODO: remove this once joda time is removed from scriptin in 7.0
+            // TODO: remove this once joda time is removed from scripting in 7.0
             systemProperty 'es.scripting.use_java_time', 'true'
+
+            // TODO: remove this once ctx isn't added to update script params in 7.0
+            systemProperty 'es.scripting.update.ctx_in_params', 'false'
 
             // Set the system keystore/truststore password if we're running tests in a FIPS-140 JVM
             if (project.inFipsJvm) {
--- server/src/main/java/org/elasticsearch/action/update/UpdateHelper.java
+++ server/src/main/java/org/elasticsearch/action/update/UpdateHelper.java
@@ -19,6 +19,11 @@
 
 package org.elasticsearch.action.update;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.LongSupplier;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.DocWriteResponse;
@@ -47,16 +52,17 @@ import org.elasticsearch.script.ScriptService;
 import org.elasticsearch.script.UpdateScript;
 import org.elasticsearch.search.lookup.SourceLookup;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.LongSupplier;
+import static org.elasticsearch.common.Booleans.parseBoolean;
 
 /**
  * Helper for translating an update request to an index, delete request or update response.
  */
 public class UpdateHelper extends AbstractComponent {
+
+    /** Whether scripts should add the ctx variable to the params map. */
+    private static final boolean CTX_IN_PARAMS =
+        parseBoolean(System.getProperty("es.scripting.update.ctx_in_params"), true);
+
     private final ScriptService scriptService;
 
     public UpdateHelper(Settings settings, ScriptService scriptService) {
@@ -280,9 +286,16 @@ public class UpdateHelper extends AbstractComponent {
         try {
             if (scriptService != null) {
                 UpdateScript.Factory factory = scriptService.compile(script, UpdateScript.CONTEXT);
-                Map<String, Object> params = new HashMap<>();
-                params.putAll(script.getParams());
-                params.put(ContextFields.CTX, ctx);
+                final Map<String, Object> params;
+                if (CTX_IN_PARAMS) {
+                    params = new HashMap<>();
+                    params.putAll(script.getParams());
+                    params.put(ContextFields.CTX, ctx);
+                    deprecationLogger.deprecated("Exposing `ctx` under `params.ctx` is deprecated. " +
+                        "Use -Des.scripting.update.ctx_in_params=false to disable this behaviour.");
+                } else {
+                    params = script.getParams();
+                }
                 UpdateScript executableScript = factory.newInstance(params);
                 executableScript.execute(ctx);
             }

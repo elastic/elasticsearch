0ac78166184a0b464b42fcab544cdc05b2705ddd
Yannick Welsch <yannick@welsch.lu>
2018-08-06 13:33:48 +0200

    remove empty lines

--- x-pack/protocol/src/main/java/org/elasticsearch/protocol/xpack/license/DeleteLicenseRequest.java
+++ x-pack/protocol/src/main/java/org/elasticsearch/protocol/xpack/license/DeleteLicenseRequest.java
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.elasticsearch.protocol.xpack.license;
 
 import org.elasticsearch.action.ActionRequestValidationException;

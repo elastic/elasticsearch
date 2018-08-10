e92ddd4e7f3468a828d7698188d029637041c365
David Roberts <dave.roberts@elastic.co>
2018-08-10 17:09:07 +0100

    [ML] Add log structure finder functionality
    This change adds a library to ML that can be used to deduce a log
    file's structure given only a sample of the log file.
    
    Eventually this will be used to add an endpoint to ML to make the
    functionality available to end users, but this will follow in a
    separate change.
    
    The functionality is split into a library so that it can also be
    used by a command line tool without requiring the command line
    tool to include all server code.

---
+++ x-pack/plugin/ml/log-structure-finder/src/test/java/org/elasticsearch/xpack/ml/logstructure/JsonLogFileStructureFinderTests.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+package org.elasticsearch.xpack.ml.logstructure;
+
+import java.util.Collections;
+
+public class JsonLogFileStructureFinderTests extends LogFileStructureTestCase {
+
+    private LogFileStructureFinderFactory factory = new JsonLogFileStructureFinderFactory();
+
+    public void testCreateConfigsGivenGoodJson() throws Exception {
+        assertTrue(factory.canCreateFromSample(explanation, JSON_SAMPLE));
+
+        String charset = randomFrom(POSSIBLE_CHARSETS);
+        Boolean hasByteOrderMarker = randomHasByteOrderMarker(charset);
+        LogFileStructureFinder structureFinder = factory.createFromSample(explanation, JSON_SAMPLE, charset, hasByteOrderMarker);
+
+        LogFileStructure structure = structureFinder.getStructure();
+
+        assertEquals(LogFileStructure.Format.JSON, structure.getFormat());
+        assertEquals(charset, structure.getCharset());
+        if (hasByteOrderMarker == null) {
+            assertNull(structure.getHasByteOrderMarker());
+        } else {
+            assertEquals(hasByteOrderMarker, structure.getHasByteOrderMarker());
+        }
+        assertNull(structure.getExcludeLinesPattern());
+        assertNull(structure.getMultilineStartPattern());
+        assertNull(structure.getSeparator());
+        assertNull(structure.getHasHeaderRow());
+        assertNull(structure.getShouldTrimFields());
+        assertNull(structure.getGrokPattern());
+        assertEquals("timestamp", structure.getTimestampField());
+        assertEquals(Collections.singletonList("UNIX_MS"), structure.getTimestampFormats());
+    }
+}

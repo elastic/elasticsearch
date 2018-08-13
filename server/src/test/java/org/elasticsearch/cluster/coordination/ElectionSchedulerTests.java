fe1792109b52d208adf669aa40068c370ced5fcf
David Turner <david.turner@elastic.co>
2018-08-13 12:06:21 +0100

    Imports

--- server/src/test/java/org/elasticsearch/cluster/coordination/ElectionSchedulerTests.java
+++ server/src/test/java/org/elasticsearch/cluster/coordination/ElectionSchedulerTests.java
@@ -39,7 +39,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import static java.util.Collections.emptySet;

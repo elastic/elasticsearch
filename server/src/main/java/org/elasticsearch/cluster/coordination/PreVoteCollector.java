84ee0fd1c45e842e3caba10ce0ee7b85481d879c
David Turner <david.turner@elastic.co>
2018-08-14 15:19:23 +0100

    Introduce PreVoteCollector
    An election requires a node to select a term that is higher than all
    previously-seen terms.  If nodes are too enthusiastic about starting elections
    then they can effectively excludes itself from the cluster until the leader can
    bump to a still-higher term, and if this process repeats then a single faulty
    node can prevent the cluster from making useful progress.
    
    The solution is to start the election with a pre-voting round to ensure that
    there is at least a quorum of nodes who believe there to be no leader.

---
+++ server/src/main/java/org/elasticsearch/cluster/coordination/PreVoteCollector.java
@@ -0,0 +1,210 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.cluster.coordination;
+
+import org.apache.logging.log4j.message.ParameterizedMessage;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.coordination.CoordinationState.VoteCollection;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.Nullable;
+import org.elasticsearch.common.component.AbstractComponent;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.threadpool.ThreadPool.Names;
+import org.elasticsearch.transport.TransportException;
+import org.elasticsearch.transport.TransportResponseHandler;
+import org.elasticsearch.transport.TransportService;
+
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.LongConsumer;
+
+import static org.elasticsearch.cluster.coordination.CoordinationState.isElectionQuorum;
+import static org.elasticsearch.common.util.concurrent.ConcurrentCollections.newConcurrentSet;
+
+public class PreVoteCollector extends AbstractComponent {
+
+    public static final String REQUEST_PRE_VOTE_ACTION_NAME = "internal:cluster/request_pre_vote";
+
+    private final AtomicLong maxTermSeen = new AtomicLong(0);
+    private final TransportService transportService;
+    private final LongConsumer startElection; // consumes maximum known term
+    private volatile PreVoteResponse preVoteResponse;
+
+    @Nullable
+    private volatile PreVotingRound currentRound;
+    @Nullable
+    private volatile DiscoveryNode currentLeader;
+
+    private long updateMaxTermSeen(long term) {
+        return maxTermSeen.accumulateAndGet(term, Math::max);
+    }
+
+    PreVoteCollector(Settings settings, PreVoteResponse preVoteResponse, TransportService transportService, LongConsumer startElection) {
+        super(settings);
+        this.preVoteResponse = preVoteResponse;
+        this.transportService = transportService;
+        this.startElection = startElection;
+
+        transportService.registerRequestHandler(REQUEST_PRE_VOTE_ACTION_NAME, Names.GENERIC, false, false,
+            PreVoteRequest::new,
+            (request, channel, task) -> channel.sendResponse(handlePreVoteRequest(request)));
+    }
+
+    public void start(final ClusterState clusterState, final Iterable<DiscoveryNode> broadcastNodes) {
+        logger.debug("{} starting", this);
+        assert currentRound == null;
+        currentRound = new PreVotingRound(clusterState);
+        currentRound.start(broadcastNodes);
+    }
+
+    public void stop() {
+        currentRound.stop();
+        currentRound = null;
+    }
+
+    public void update(PreVoteResponse preVoteResponse, DiscoveryNode currentLeader) {
+        logger.trace("updating with preVoteResponse={}, currentLeader={}", preVoteResponse, currentLeader);
+        this.preVoteResponse = preVoteResponse;
+        this.currentLeader = currentLeader;
+    }
+
+    // package-private for testing
+    PreVoteResponse handlePreVoteRequest(PreVoteRequest request) {
+        updateMaxTermSeen(request.getCurrentTerm());
+        // TODO if we are a leader and the max term seen exceeds our term then we need to bump our term
+
+        DiscoveryNode currentLeader = this.currentLeader;
+        if (currentLeader == null || currentLeader.equals(request.getSourceNode())) {
+            return preVoteResponse;
+        } else {
+            throw new CoordinationStateRejectedException("rejecting " + request + " as there is already a leader");
+        }
+    }
+
+    // package-private for testing, actual usages TODO
+    long getMaxTermSeen() {
+        return maxTermSeen.get();
+    }
+
+    @Override
+    public String toString() {
+        return "PreVoteCollector{" +
+            "currentRound=" + currentRound +
+            ", currentLeader=" + currentLeader +
+            ", preVoteResponse=" + preVoteResponse +
+            '}';
+    }
+
+    private class PreVotingRound {
+        private final Set<DiscoveryNode> preVotesReceived = newConcurrentSet();
+        private final AtomicBoolean electionStarted = new AtomicBoolean();
+        private final PreVoteRequest preVoteRequest;
+        private final ClusterState clusterState;
+        private final AtomicBoolean isRunning = new AtomicBoolean();
+
+        PreVotingRound(ClusterState clusterState) {
+            this.clusterState = clusterState;
+            preVoteRequest = new PreVoteRequest(transportService.getLocalNode(), preVoteResponse.getCurrentTerm());
+        }
+
+        void start(final Iterable<DiscoveryNode> broadcastNodes) {
+
+            final boolean isRunningChanged = isRunning.compareAndSet(false, true);
+            assert isRunningChanged;
+
+            broadcastNodes.forEach(n -> transportService.sendRequest(n, REQUEST_PRE_VOTE_ACTION_NAME, preVoteRequest,
+                new TransportResponseHandler<PreVoteResponse>() {
+                    @Override
+                    public void handleResponse(PreVoteResponse response) {
+                        handlePreVoteResponse(response, n);
+                    }
+
+                    @Override
+                    public void handleException(TransportException exp) {
+                        if (exp.getRootCause() instanceof CoordinationStateRejectedException) {
+                            logger.debug("{} failed: {}", this, exp.getRootCause().getMessage());
+                        } else {
+                            logger.debug(new ParameterizedMessage("{} failed", this), exp);
+                        }
+                    }
+
+                    @Override
+                    public String executor() {
+                        return Names.GENERIC;
+                    }
+
+                    @Override
+                    public String toString() {
+                        return "TransportResponseHandler{" + PreVoteCollector.this + ", node=" + n + '}';
+                    }
+                }));
+        }
+
+        void stop() {
+            final boolean isRunningChanged = isRunning.compareAndSet(true, false);
+            assert isRunningChanged;
+        }
+
+        private void handlePreVoteResponse(PreVoteResponse response, DiscoveryNode sender) {
+            if (isRunning.get() == false) {
+                logger.debug("{} ignoring {} from {}, no longer running", this, response, sender);
+                return;
+            }
+
+            final long currentMaxTermSeen = updateMaxTermSeen(response.getCurrentTerm());
+
+            final PreVoteResponse currentPreVoteResponse = preVoteResponse;
+            if (response.getLastAcceptedTerm() > currentPreVoteResponse.getLastAcceptedTerm()
+                || (response.getLastAcceptedTerm() == currentPreVoteResponse.getLastAcceptedTerm()
+                && response.getLastAcceptedVersion() > currentPreVoteResponse.getLastAcceptedVersion())) {
+                logger.debug("{} ignoring {} from {} as it is fresher", this, response, sender);
+                return;
+            }
+
+            preVotesReceived.add(sender);
+            final VoteCollection voteCollection = new VoteCollection();
+            preVotesReceived.forEach(voteCollection::addVote);
+
+            if (isElectionQuorum(voteCollection, clusterState) == false) {
+                logger.debug("{} added {} from {}, no quorum yet", this, response, sender);
+                return;
+            }
+
+            if (electionStarted.compareAndSet(false, true) == false) {
+                logger.debug("{} added {} from {} but election has already started", this, response, sender);
+                return;
+            }
+
+            logger.debug("{} added {} from {}, starting election in term > {}", this, response, sender, currentMaxTermSeen);
+            startElection.accept(currentMaxTermSeen);
+        }
+
+        @Override
+        public String toString() {
+            return "PreVotingRound{" +
+                "preVotesReceived=" + preVotesReceived +
+                ", electionStarted=" + electionStarted +
+                ", preVoteRequest=" + preVoteRequest +
+                ", isRunning=" + isRunning +
+                '}';
+        }
+    }
+}
---
+++ server/src/test/java/org/elasticsearch/cluster/coordination/PreVoteCollectorTests.java
@@ -0,0 +1,239 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.cluster.coordination;
+
+import org.elasticsearch.Version;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.ClusterState.VotingConfiguration;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.test.transport.CapturingTransport;
+import org.elasticsearch.transport.ConnectTransportException;
+import org.elasticsearch.transport.Transport;
+import org.elasticsearch.transport.TransportRequest;
+import org.elasticsearch.transport.TransportService;
+import org.junit.Before;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.emptySet;
+import static org.elasticsearch.cluster.coordination.PreVoteCollector.REQUEST_PRE_VOTE_ACTION_NAME;
+import static org.elasticsearch.node.Node.NODE_NAME_SETTING;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+public class PreVoteCollectorTests extends ESTestCase {
+
+    private DeterministicTaskQueue deterministicTaskQueue;
+    private PreVoteCollector preVoteCollector;
+    private boolean electionOccurred = false;
+    private DiscoveryNode localNode;
+    private Map<DiscoveryNode, PreVoteResponse> responsesByNode = new HashMap<>();
+    private long lastElectionMaxTermSeen;
+
+    @Before
+    public void createObjects() {
+        Settings settings = Settings.builder().put(NODE_NAME_SETTING.getKey(), "node").build();
+        deterministicTaskQueue = new DeterministicTaskQueue(settings);
+        final Transport capturingTransport = new CapturingTransport() {
+            @Override
+            protected void onSendRequest(final long requestId, final String action, final TransportRequest request,
+                                         final DiscoveryNode node) {
+                super.onSendRequest(requestId, action, request, node);
+                assertThat(action, is(REQUEST_PRE_VOTE_ACTION_NAME));
+                assertThat(request, instanceOf(PreVoteRequest.class));
+                assertThat(node, not(equalTo(localNode)));
+                PreVoteRequest preVoteRequest = (PreVoteRequest) request;
+                assertThat(preVoteRequest.getSourceNode(), equalTo(localNode));
+                deterministicTaskQueue.scheduleNow(new Runnable() {
+                    @Override
+                    public void run() {
+                        final PreVoteResponse response = responsesByNode.get(node);
+                        if (response == null) {
+                            handleRemoteError(requestId, new ConnectTransportException(node, "no response"));
+                        } else {
+                            handleResponse(requestId, response);
+                        }
+                    }
+
+                    @Override
+                    public String toString() {
+                        return "response to " + request + " from " + node;
+                    }
+                });
+            }
+        };
+        localNode = new DiscoveryNode("local-node", buildNewFakeTransportAddress(), Version.CURRENT);
+        responsesByNode.put(localNode, new PreVoteResponse(3, 2, 1));
+        TransportService transportService = new TransportService(settings, capturingTransport,
+            deterministicTaskQueue.getThreadPool(), TransportService.NOOP_TRANSPORT_INTERCEPTOR,
+            boundTransportAddress -> localNode, null, emptySet());
+        transportService.start();
+        transportService.acceptIncomingRequests();
+
+        preVoteCollector = new PreVoteCollector(settings, getLocalPreVoteResponse(), transportService, maxTermSeen -> {
+            assert electionOccurred == false;
+            electionOccurred = true;
+            lastElectionMaxTermSeen = maxTermSeen;
+        });
+    }
+
+    private PreVoteResponse getLocalPreVoteResponse() {
+        return Objects.requireNonNull(responsesByNode.get(localNode));
+    }
+
+    private void startAndRunCollector(DiscoveryNode... votingNodes) {
+        startCollector(votingNodes);
+        runCollector();
+    }
+
+    private void runCollector() {
+        deterministicTaskQueue.runAllRunnableTasks(random());
+        assertFalse(deterministicTaskQueue.hasDeferredTasks());
+        assertFalse(deterministicTaskQueue.hasRunnableTasks());
+    }
+
+    private void startCollector(DiscoveryNode... votingNodes) {
+        final VotingConfiguration votingConfiguration
+            = new VotingConfiguration(Arrays.stream(votingNodes).map(DiscoveryNode::getId).collect(Collectors.toSet()));
+        final ClusterState clusterState = CoordinationStateTests.clusterState(0, 0, localNode, votingConfiguration, votingConfiguration, 0);
+
+        preVoteCollector.start(clusterState, responsesByNode.keySet());
+    }
+
+    public void testStartsElectionIfLocalNodeIsOnlyNode() {
+        startAndRunCollector(localNode);
+        assertTrue(electionOccurred);
+    }
+
+    public void testStartsElectionIfLocalNodeIsQuorum() {
+        final DiscoveryNode otherNode = new DiscoveryNode("other-node", buildNewFakeTransportAddress(), Version.CURRENT);
+        responsesByNode.put(otherNode, new PreVoteResponse(3, 2, 1));
+        startAndRunCollector(otherNode);
+        assertTrue(electionOccurred);
+    }
+
+
+    public void testStartsElectionIfOtherNodeIsQuorum() {
+        final DiscoveryNode otherNode = new DiscoveryNode("other-node", buildNewFakeTransportAddress(), Version.CURRENT);
+        responsesByNode.put(otherNode, new PreVoteResponse(3, 2, 1));
+        startAndRunCollector(otherNode);
+        assertTrue(electionOccurred);
+    }
+
+    public void testDoesNotStartsElectionIfOtherNodeIsQuorumAndDoesNotRespond() {
+        final DiscoveryNode otherNode = new DiscoveryNode("other-node", buildNewFakeTransportAddress(), Version.CURRENT);
+        responsesByNode.put(otherNode, null);
+        startAndRunCollector(otherNode);
+        assertFalse(electionOccurred);
+    }
+
+    public void testDoesNotStartElectionIfStopped() {
+        final DiscoveryNode otherNode = new DiscoveryNode("other-node", buildNewFakeTransportAddress(), Version.CURRENT);
+        responsesByNode.put(otherNode, new PreVoteResponse(3, 2, 1));
+        startCollector(otherNode);
+        preVoteCollector.stop();
+        runCollector();
+        assertFalse(electionOccurred);
+    }
+
+    public void testIgnoresPreVotesFromLaterTerms() {
+        final DiscoveryNode otherNode = new DiscoveryNode("other-node", buildNewFakeTransportAddress(), Version.CURRENT);
+        responsesByNode.put(otherNode, new PreVoteResponse(3, 3, 1));
+        startAndRunCollector(otherNode);
+        assertFalse(electionOccurred);
+    }
+
+    public void testIgnoresPreVotesFromLaterVersionInSameTerm() {
+        final DiscoveryNode otherNode = new DiscoveryNode("other-node", buildNewFakeTransportAddress(), Version.CURRENT);
+        responsesByNode.put(otherNode, new PreVoteResponse(3, 2, 2));
+        startAndRunCollector(otherNode);
+        assertFalse(electionOccurred);
+    }
+
+    public void testAcceptsPreVotesFromLaterVersionInEarlierTerms() {
+        final DiscoveryNode otherNode = new DiscoveryNode("other-node", buildNewFakeTransportAddress(), Version.CURRENT);
+        responsesByNode.put(otherNode, new PreVoteResponse(3, 1, 2));
+        startAndRunCollector(otherNode);
+        assertTrue(electionOccurred);
+        assertThat(lastElectionMaxTermSeen, is(3L));
+    }
+
+    public void testReturnsMaximumSeenTerm() {
+        final DiscoveryNode otherNode1 = new DiscoveryNode("other-node-1", buildNewFakeTransportAddress(), Version.CURRENT);
+        final DiscoveryNode otherNode2 = new DiscoveryNode("other-node-2", buildNewFakeTransportAddress(), Version.CURRENT);
+        responsesByNode.put(otherNode1, new PreVoteResponse(4, 2, 1));
+        responsesByNode.put(otherNode2, new PreVoteResponse(5, 2, 1));
+        startAndRunCollector(otherNode1, otherNode2);
+        assertTrue(electionOccurred);
+        assertThat(lastElectionMaxTermSeen, is(5L));
+    }
+
+    public void testResponseIfCandidate() {
+        final long term = randomNonNegativeLong();
+        final DiscoveryNode otherNode = new DiscoveryNode("other-node", buildNewFakeTransportAddress(), Version.CURRENT);
+
+        PreVoteResponse newPreVoteResponse = new PreVoteResponse(5, 4, 3);
+        preVoteCollector.update(newPreVoteResponse, null);
+        final PreVoteResponse preVoteResponse = preVoteCollector.handlePreVoteRequest(new PreVoteRequest(otherNode, term));
+
+        assertThat(preVoteResponse, equalTo(newPreVoteResponse));
+        assertThat(preVoteCollector.getMaxTermSeen(), is(term));
+    }
+
+    public void testResponseToNonLeaderIfNotCandidate() {
+        final long term = randomNonNegativeLong();
+        final DiscoveryNode leaderNode = new DiscoveryNode("leader-node", buildNewFakeTransportAddress(), Version.CURRENT);
+        final DiscoveryNode otherNode = new DiscoveryNode("other-node", buildNewFakeTransportAddress(), Version.CURRENT);
+
+        PreVoteResponse newPreVoteResponse = new PreVoteResponse(5, 4, 3);
+        preVoteCollector.update(newPreVoteResponse, leaderNode);
+
+        expectThrows(CoordinationStateRejectedException.class, () ->
+            preVoteCollector.handlePreVoteRequest(new PreVoteRequest(otherNode, term)));
+        assertThat(preVoteCollector.getMaxTermSeen(), is(term));
+    }
+
+    public void testResponseToRequestFromLeader() {
+        // This is a _rare_ case where our leader has detected a failure and stepped down, but we are still a
+        // follower. It's possible that the leader lost its quorum, but while we're still a follower we will not
+        // offer joins to any other node so there is no major drawback in offering a join to our old leader. The
+        // advantage of this is that it makes it slightly more likely that the leader won't change, and also that
+        // its re-election will happen more quickly than if it had to wait for a quorum of followers to also detect
+        // its failure.
+
+        final long term = randomNonNegativeLong();
+        final DiscoveryNode leaderNode = new DiscoveryNode("leader-node", buildNewFakeTransportAddress(), Version.CURRENT);
+
+        PreVoteResponse newPreVoteResponse = new PreVoteResponse(5, 4, 3);
+        preVoteCollector.update(newPreVoteResponse, leaderNode);
+        final PreVoteResponse preVoteResponse = preVoteCollector.handlePreVoteRequest(new PreVoteRequest(leaderNode, term));
+
+        assertThat(preVoteResponse, equalTo(newPreVoteResponse));
+        assertThat(preVoteCollector.getMaxTermSeen(), is(term));
+    }
+}

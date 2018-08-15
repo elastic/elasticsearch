6516a96121b4f49c6c26c43672b8b9be22451cf6
Andy Bristol <andy.bristol@elastic.co>
2018-08-14 18:21:59 -0700

    [test] mute IndexShardTests.testDocStats
    For #32766

--- server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
+++ server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
@@ -2389,6 +2389,7 @@ public class IndexShardTests extends IndexShardTestCase {
         closeShards(sourceShard, targetShard);
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/32766")
     public void testDocStats() throws IOException {
         IndexShard indexShard = null;
         try {

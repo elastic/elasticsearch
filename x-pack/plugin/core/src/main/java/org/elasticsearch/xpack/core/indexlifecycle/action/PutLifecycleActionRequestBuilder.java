8154ee8c473365b6feed8af19aea6fc7d3871146
Jason Tedor <jason@tedor.me>
2018-08-17 16:53:09 -0400

    Add license headers for ILM request builders
    This commit adds license headers for the ILM request builders. As these
    are under the x-pack directory, they implicitly carry this license but
    this commit makes that explicit.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/indexlifecycle/action/PutLifecycleActionRequestBuilder.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/indexlifecycle/action/PutLifecycleActionRequestBuilder.java
@@ -1,9 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
 package org.elasticsearch.xpack.core.indexlifecycle.action;
 
 import org.elasticsearch.action.Action;
 import org.elasticsearch.action.ActionRequestBuilder;
 import org.elasticsearch.client.ElasticsearchClient;
 import org.elasticsearch.xpack.core.indexlifecycle.LifecyclePolicy;
+import org.elasticsearch.xpack.core.indexlifecycle.action.PutLifecycleAction;
 
 public class PutLifecycleActionRequestBuilder
         extends ActionRequestBuilder<PutLifecycleAction.Request, PutLifecycleAction.Response, PutLifecycleActionRequestBuilder> {

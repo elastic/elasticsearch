946312bb91eafdcf0a5854294662273e00895c99
Jim Ferenczi <jimczi@apache.org>
2018-08-20 15:06:28 +0200

    Convert BoostingQueryBuilder to use FunctionScoreQuery.boostByQuery

--- server/src/main/java/org/apache/lucene/search/vectorhighlight/CustomFieldQuery.java
+++ server/src/main/java/org/apache/lucene/search/vectorhighlight/CustomFieldQuery.java
@@ -22,7 +22,6 @@ package org.apache.lucene.search.vectorhighlight;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queries.BlendedTermQuery;
-import org.apache.lucene.queries.BoostingQuery;
 import org.apache.lucene.search.BoostQuery;
 import org.apache.lucene.search.ConstantScoreQuery;
 import org.apache.lucene.search.MultiPhraseQuery;
@@ -74,12 +73,11 @@ public class CustomFieldQuery extends FieldQuery {
         } else if (sourceQuery instanceof BlendedTermQuery) {
             final BlendedTermQuery blendedTermQuery = (BlendedTermQuery) sourceQuery;
             flatten(blendedTermQuery.rewrite(reader), reader, flatQueries, boost);
-        } else if (sourceQuery instanceof BoostingQuery) {
-            BoostingQuery boostingQuery = (BoostingQuery) sourceQuery;
-            //flatten positive query with query boost
-            flatten(boostingQuery.getMatch(), reader, flatQueries, boost);
-            //flatten negative query with negative boost
-            flatten(boostingQuery.getContext(), reader, flatQueries, boostingQuery.getBoost());
+        } else if (sourceQuery instanceof org.apache.lucene.queries.function.FunctionScoreQuery) {
+            org.apache.lucene.queries.function.FunctionScoreQuery funcScoreQuery =
+                (org.apache.lucene.queries.function.FunctionScoreQuery) sourceQuery;
+            //flatten query with query boost
+            flatten(funcScoreQuery.getWrappedQuery(), reader, flatQueries, boost);
         } else if (sourceQuery instanceof SynonymQuery) {
             // SynonymQuery should be handled by the parent class directly.
             // This statement should be removed when https://issues.apache.org/jira/browse/LUCENE-7484 is merged.

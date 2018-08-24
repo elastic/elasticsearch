2fdfe51dae22fa0ff0ddcbc1245a529fda7cd543
Nik Everett <nik9000@gmail.com>
2018-08-24 11:44:31 -0400

    Switch back to volatile read

--- server/src/main/java/org/elasticsearch/common/logging/NodeNamePatternConverter.java
+++ server/src/main/java/org/elasticsearch/common/logging/NodeNamePatternConverter.java
@@ -26,6 +26,7 @@ import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.pattern.ConverterKeys;
 import org.apache.logging.log4j.core.pattern.LogEventPatternConverter;
 import org.apache.logging.log4j.core.pattern.PatternConverter;
+import org.apache.lucene.util.SetOnce;
 
 /**
  * Converts {@code %node_name} in log4j patterns into the current node name.
@@ -37,28 +38,15 @@ import org.apache.logging.log4j.core.pattern.PatternConverter;
 @ConverterKeys({"node_name"})
 public final class NodeNamePatternConverter extends LogEventPatternConverter {
     /**
-     * The name of this node. This is intentionally not {@code volatile} so we
-     * don't have to pay for the volatile read when we log every log line.
-     * Instead we rely on a volatile inside of log4j2 to set up the
-     * "happens before" relationship guaranteeing the correctness here.
-     * Specifically, before every log, log4j2 reads
-     * {@code Logger.privateConfiguration}. These volatiles are written to
-     * after every write to this static variable. The only two writes to this
-     * variable are both followed shortly by writes to
-     * {@code Logger.privateConfiguration}. Thus all writes all writes to this
-     * variable "happen before" all reads from this variable. So long as we
-     * don't add any more writes. And we don't plan to. We also guard against
-     * it by declaring the variable private and making the method to write to
-     * it package private. It isn't great protection, but we plan to remove
-     * the need for all of this shortly.
+     * The name of this node.
      */
-    private static String nodeName = "unknown";
+    private static final SetOnce<String> NODE_NAME = new SetOnce<>();
 
     /**
      * Set the name of this node.
      */
     static void setNodeName(String nodeName) {
-        NodeNamePatternConverter.nodeName = nodeName;
+        NODE_NAME.set(nodeName);
     }
 
     /**
@@ -78,6 +66,12 @@ public final class NodeNamePatternConverter extends LogEventPatternConverter {
 
     @Override
     public void format(LogEvent event, StringBuilder toAppendTo) {
-        toAppendTo.append(nodeName);
+        /*
+         * We're not thrilled about this volatile read on every line logged but
+         * the alternatives are slightly terrifying and/or don't work with the
+         * security manager.
+         */
+        String nodeName = NODE_NAME.get();
+        toAppendTo.append(nodeName == null ? "unknown" : nodeName);
     }
 }

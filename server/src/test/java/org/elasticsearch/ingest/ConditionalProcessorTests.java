881d6c40cc8657c3b490558d7729e7075c9a55f9
Armin Braun <me@obrown.io>
2018-08-29 14:50:16 +0200

    Add some tests for immutability

--- server/src/test/java/org/elasticsearch/ingest/ConditionalProcessorTests.java
+++ server/src/test/java/org/elasticsearch/ingest/ConditionalProcessorTests.java
@@ -19,9 +19,13 @@
 
 package org.elasticsearch.ingest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.script.MockScriptEngine;
 import org.elasticsearch.script.Script;
@@ -31,6 +35,7 @@ import org.elasticsearch.script.ScriptType;
 import org.elasticsearch.test.ESTestCase;
 
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.Is.is;
 
@@ -87,5 +92,50 @@ public class ConditionalProcessorTests extends ESTestCase {
         processor.execute(ingestDocument);
         assertThat(ingestDocument.getSourceAndMetadata().get(conditionalField), is(falseValue));
         assertThat(ingestDocument.getSourceAndMetadata(), not(hasKey("foo")));
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testActsOnImmutableData() throws Exception {
+        assertMutatingCtxThrows(ctx -> ctx.remove("foo"));
+        assertMutatingCtxThrows(ctx -> ctx.put("foo", "bar"));
+        assertMutatingCtxThrows(ctx -> ((List<Object>)ctx.get("listField")).add("bar"));
+        assertMutatingCtxThrows(ctx -> ((List<Object>)ctx.get("listField")).remove("bar"));
+    }
+
+    private static void assertMutatingCtxThrows(Consumer<Map<String, Object>> mutation) throws Exception {
+        String scriptName = "conditionalScript";
+        CompletableFuture<Exception> expectedException = new CompletableFuture<>();
+        ScriptService scriptService = new ScriptService(Settings.builder().build(),
+            Collections.singletonMap(
+                Script.DEFAULT_SCRIPT_LANG,
+                new MockScriptEngine(
+                    Script.DEFAULT_SCRIPT_LANG,
+                    Collections.singletonMap(
+                        scriptName, ctx -> {
+                            try {
+                                mutation.accept(ctx);
+                            } catch (Exception e) {
+                                expectedException.complete(e);
+                            }
+                            return false;
+                        }
+                    )
+                )
+            ),
+            new HashMap<>(ScriptModule.CORE_CONTEXTS)
+        );
+        Map<String, Object> document = new HashMap<>();
+        ConditionalProcessor processor = new ConditionalProcessor(
+            randomAlphaOfLength(10),
+            new Script(
+                ScriptType.INLINE, Script.DEFAULT_SCRIPT_LANG,
+                scriptName, Collections.emptyMap()), scriptService, null
+        );
+        IngestDocument ingestDocument = RandomDocumentPicks.randomIngestDocument(random(), document);
+        ingestDocument.setFieldValue("listField", new ArrayList<>());
+        processor.execute(ingestDocument);
+        Exception e = expectedException.get();
+        assertThat(e, instanceOf(UnsupportedOperationException.class));
+        assertEquals("Mutating ingest documents in conditionals is not supported", e.getMessage());
     }
 }

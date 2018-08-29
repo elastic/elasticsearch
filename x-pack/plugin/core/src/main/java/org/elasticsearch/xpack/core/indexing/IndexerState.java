cfc003d4855601b1f1bf2be522f1c6087feeb229
Hendrik Muhs <hendrik.muhs@elastic.co>
2018-08-29 20:28:21 +0200

    [Rollup] Re-factor Rollup Indexer into a generic indexer for re-usability   (#32743)
    This extracts a super class out of the rollup indexer called the AsyncTwoPhaseIterator. 
    The implementor of it can define the query, transformation of the response, 
    indexing and the object to persist the position/state of the indexer.
    
    The stats object used by the indexer to record progress is also now abstract, allowing
    the implementation provide custom stats beyond what the indexer provides.  It also
    allows the implementation to decide how the stats are presented (leaves toXContent()
    up to the implementation).
    
    This should allow new projects to reuse the search-then-index persistent task that Rollup
    uses, but without the restrictions/baggage of how Rollup has to work internally to
    satisfy time-based rollups.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/indexing/IndexerState.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/indexing/IndexerState.java
@@ -3,7 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-package org.elasticsearch.xpack.core.rollup.job;
+package org.elasticsearch.xpack.core.indexing;
 
 import org.elasticsearch.common.ParseField;
 import org.elasticsearch.common.io.stream.StreamInput;

c0c53cf72c8522f1df6340aab92247ca3e421c60
markharwood <markharwood@gmail.com>
2018-08-29 14:53:34 +0100

    Tests fix - Graph HLRC client overly long line and syncing core’s copy of GraphExploreResponseTests taken from protocol. Related to #33231

--- x-pack/plugin/core/src/test/java/org/elasticsearch/protocol/xpack/graph/GraphExploreResponseTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/protocol/xpack/graph/GraphExploreResponseTests.java
@@ -8,7 +8,6 @@ package org.elasticsearch.protocol.xpack.graph;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.ShardOperationFailedException;
 import org.elasticsearch.action.search.ShardSearchFailure;
-import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.xcontent.ToXContent;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.test.AbstractXContentTestCase;
@@ -63,7 +62,7 @@ public class GraphExploreResponseTests extends AbstractXContentTestCase< GraphEx
 
     @Override
     protected String[] getShuffleFieldsExceptions() {
-        return new String[]{"vertices"};
+        return new String[]{"vertices", "connections"};
     }
     
     private static   GraphExploreResponse createTestInstanceWithFailures() {
@@ -115,7 +114,8 @@ public class GraphExploreResponseTests extends AbstractXContentTestCase< GraphEx
         boolean supportsUnknownFields = true;
         //exceptions are not of the same type whenever parsed back
         boolean assertToXContentEquivalence = false;
-        AbstractXContentTestCase.testFromXContent(NUMBER_OF_TEST_RUNS, instanceSupplier, supportsUnknownFields, Strings.EMPTY_ARRAY,
+        AbstractXContentTestCase.testFromXContent(
+                NUMBER_OF_TEST_RUNS, instanceSupplier, supportsUnknownFields, getShuffleFieldsExceptions(),
                 getRandomFieldsExcludeFilterWhenResultHasErrors(), this::createParser, this::doParseInstance,
                 this::assertEqualInstances, assertToXContentEquivalence, ToXContent.EMPTY_PARAMS);
     }    

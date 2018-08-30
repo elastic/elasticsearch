ebf1e906cc7207545a84e7b31ee5ec76ccfb17b0
Armin Braun <me@obrown.io>
2018-08-30 03:46:39 +0200

    Ingest: Add conditional per processor (#32398)
    * Ingest: Add conditional per processor
    * closes #21248

--- server/src/main/java/org/elasticsearch/ingest/Pipeline.java
+++ server/src/main/java/org/elasticsearch/ingest/Pipeline.java
@@ -26,6 +26,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import org.elasticsearch.script.ScriptService;
 
 /**
  * A pipeline is a list of {@link Processor} instances grouped under a unique id.
@@ -51,16 +52,16 @@ public final class Pipeline {
         this.version = version;
     }
 
-    public static Pipeline create(String id, Map<String, Object> config, Map<String, Processor.Factory> processorFactories)
-        throws Exception {
+    public static Pipeline create(String id, Map<String, Object> config,
+        Map<String, Processor.Factory> processorFactories, ScriptService scriptService) throws Exception {
         String description = ConfigurationUtils.readOptionalStringProperty(null, null, config, DESCRIPTION_KEY);
         Integer version = ConfigurationUtils.readIntProperty(null, null, config, VERSION_KEY, null);
-        List<Map<String, Map<String, Object>>> processorConfigs =
-            ConfigurationUtils.readList(null, null, config, PROCESSORS_KEY);
-        List<Processor> processors = ConfigurationUtils.readProcessorConfigs(processorConfigs, processorFactories);
-        List<Map<String, Map<String, Object>>> onFailureProcessorConfigs =
+        List<Map<String, Object>> processorConfigs = ConfigurationUtils.readList(null, null, config, PROCESSORS_KEY);
+        List<Processor> processors = ConfigurationUtils.readProcessorConfigs(processorConfigs, scriptService, processorFactories);
+        List<Map<String, Object>> onFailureProcessorConfigs =
                 ConfigurationUtils.readOptionalList(null, null, config, ON_FAILURE_KEY);
-        List<Processor> onFailureProcessors = ConfigurationUtils.readProcessorConfigs(onFailureProcessorConfigs, processorFactories);
+        List<Processor> onFailureProcessors =
+            ConfigurationUtils.readProcessorConfigs(onFailureProcessorConfigs, scriptService, processorFactories);
         if (config.isEmpty() == false) {
             throw new ElasticsearchParseException("pipeline [" + id +
                     "] doesn't support one or more provided configuration parameters " + Arrays.toString(config.keySet().toArray()));

001b78f704d25d01d4c28a4a756c536321c8dc8a
Igor Motov <igor@motovs.org>
2018-08-31 01:27:00 +0530

    Replace IndexMetaData.Custom with Map-based custom metadata (#32749)
    This PR removes the deprecated `Custom` class in `IndexMetaData`, in favor
    of a `Map<String, DiffableStringMap>` that is used to store custom index
    metadata. As part of this, there is now no way to set this metadata in a
    template or create index request (since it's only set by plugins, or dedicated
    REST endpoints).
    
    The `Map<String, DiffableStringMap>` is intended to be a namespaced `Map<String,
    String>` (`DiffableStringMap` implements `Map<String, String>`, so the signature
    is more like `Map<String, Map<String, String>>`). This is so we can do things
    like:
    
    ``` java
    Map<String, String> ccrMeta = indexMetaData.getCustom("ccr");
    ```
    
    And then have complete control over the metadata. This also means any
    plugin/feature that uses this has to manage its own BWC, as the map is just
    serialized as a map. It also means that if metadata is put in the map that isn't
    used (for instance, if a plugin were removed), it causes no failures the way
    an unregistered `Setting` would.
    
    The reason I use a custom `DiffableStringMap` here rather than a plain
    `Map<String, String>` is so the map can be diffed with previous cluster state
    updates for serialization.
    
    Supersedes #32683

--- server/src/main/java/org/elasticsearch/cluster/metadata/MetaDataCreateIndexService.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/MetaDataCreateIndexService.java
@@ -38,7 +38,6 @@ import org.elasticsearch.cluster.ack.CreateIndexClusterStateUpdateResponse;
 import org.elasticsearch.cluster.block.ClusterBlock;
 import org.elasticsearch.cluster.block.ClusterBlockLevel;
 import org.elasticsearch.cluster.block.ClusterBlocks;
-import org.elasticsearch.cluster.metadata.IndexMetaData.Custom;
 import org.elasticsearch.cluster.metadata.IndexMetaData.State;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.cluster.routing.IndexRoutingTable;
@@ -287,7 +286,7 @@ public class MetaDataCreateIndexService extends AbstractComponent {
                 List<IndexTemplateMetaData> templates =
                         MetaDataIndexTemplateService.findTemplates(currentState.metaData(), request.index());
 
-                Map<String, Custom> customs = new HashMap<>();
+                Map<String, Map<String, String>> customs = new HashMap<>();
 
                 // add the request mapping
                 Map<String, Map<String, Object>> mappings = new HashMap<>();
@@ -298,10 +297,6 @@ public class MetaDataCreateIndexService extends AbstractComponent {
 
                 for (Map.Entry<String, String> entry : request.mappings().entrySet()) {
                     mappings.put(entry.getKey(), MapperService.parseMapping(xContentRegistry, entry.getValue()));
-                }
-
-                for (Map.Entry<String, Custom> entry : request.customs().entrySet()) {
-                    customs.put(entry.getKey(), entry.getValue());
                 }
 
                 final Index recoverFromIndex = request.recoverFrom();
@@ -318,18 +313,6 @@ public class MetaDataCreateIndexService extends AbstractComponent {
                             } else {
                                 mappings.put(cursor.key,
                                     MapperService.parseMapping(xContentRegistry, mappingString));
-                            }
-                        }
-                        // handle custom
-                        for (ObjectObjectCursor<String, Custom> cursor : template.customs()) {
-                            String type = cursor.key;
-                            IndexMetaData.Custom custom = cursor.value;
-                            IndexMetaData.Custom existing = customs.get(type);
-                            if (existing == null) {
-                                customs.put(type, custom);
-                            } else {
-                                IndexMetaData.Custom merged = existing.mergeWith(custom);
-                                customs.put(type, merged);
                             }
                         }
                         //handle aliases
@@ -519,7 +502,7 @@ public class MetaDataCreateIndexService extends AbstractComponent {
                     indexMetaDataBuilder.putAlias(aliasMetaData);
                 }
 
-                for (Map.Entry<String, Custom> customEntry : customs.entrySet()) {
+                for (Map.Entry<String, Map<String, String>> customEntry : customs.entrySet()) {
                     indexMetaDataBuilder.putCustom(customEntry.getKey(), customEntry.getValue());
                 }
 

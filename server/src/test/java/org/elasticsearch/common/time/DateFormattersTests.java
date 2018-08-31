64d4952f2254f03e679f60fadbec7ba5adf71e1f
Alexander Reelsen <alexander@reelsen.net>
2018-08-31 12:35:12 +0200

    Core: Fix epoch millis java time formatter
    The existing implemention could not deal with negative numbers as well
    as +- 999 milliseconds around the epoch.
    
    This commit uses Instant.ofEpochMilli() and tries to parse the input to
    a number instead of using a date formatter.

---
+++ server/src/test/java/org/elasticsearch/common/time/DateFormattersTests.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.common.time;
+
+import org.elasticsearch.test.ESTestCase;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+public class DateFormattersTests extends ESTestCase {
+
+    // the epoch milli parser is a bit special, as it does not use date formatter, see comments in DateFormatters
+    public void testEpochMilliParser() {
+        CompoundDateTimeFormatter formatter = DateFormatters.forPattern("epoch_millis");
+
+        DateTimeParseException e = expectThrows(DateTimeParseException.class, () -> formatter.parse("invalid"));
+        assertThat(e.getMessage(), containsString("invalid number"));
+
+        // different zone, should still yield the same output, as epoch is time zoned independent
+        ZoneId zoneId = randomZone();
+        CompoundDateTimeFormatter zonedFormatter = formatter.withZone(zoneId);
+        assertThat(zonedFormatter.printer.getZone(), is(zoneId));
+
+        // test with negative and non negative values
+        assertThatSameDateTime(formatter, zonedFormatter, String.valueOf(randomNonNegativeLong() * -1));
+        assertThatSameDateTime(formatter, zonedFormatter, String.valueOf(randomNonNegativeLong()));
+        assertThatSameDateTime(formatter, zonedFormatter, String.valueOf(0));
+        assertThatSameDateTime(formatter, zonedFormatter, String.valueOf(-1));
+        assertThatSameDateTime(formatter, zonedFormatter, String.valueOf(1));
+    }
+
+    private void assertThatSameDateTime(CompoundDateTimeFormatter formatter, CompoundDateTimeFormatter zonedFormatter, String value) {
+        ZonedDateTime formatterZonedDateTime = DateFormatters.toZonedDateTime(formatter.parse(value));
+        ZonedDateTime zonedFormatterZonedDateTime = DateFormatters.toZonedDateTime(zonedFormatter.parse(value));
+        assertThat(formatterZonedDateTime.toInstant().toEpochMilli(), is(zonedFormatterZonedDateTime.toInstant().toEpochMilli()));
+    }
+}

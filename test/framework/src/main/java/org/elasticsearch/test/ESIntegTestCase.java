11b4fc8f417eee5e32d6e0a3333cce92e44be0ed
Nhat Nguyen <nhat.nguyen@elastic.co>
2018-08-31 15:32:22 -0400

    TEST: Disable randomized soft-deletes settings
    Tracked at #33321

--- test/framework/src/main/java/org/elasticsearch/test/ESIntegTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/ESIntegTestCase.java
@@ -725,7 +725,8 @@ public abstract class ESIntegTestCase extends ESTestCase {
         }
         // always default delayed allocation to 0 to make sure we have tests are not delayed
         builder.put(UnassignedInfo.INDEX_DELAYED_NODE_LEFT_TIMEOUT_SETTING.getKey(), 0);
-        builder.put(IndexSettings.INDEX_SOFT_DELETES_SETTING.getKey(), randomBoolean());
+        //norelease - AwaitsFix: https://github.com/elastic/elasticsearch/issues/33321
+        builder.put(IndexSettings.INDEX_SOFT_DELETES_SETTING.getKey(), false);
         if (randomBoolean()) {
             builder.put(IndexSettings.INDEX_SOFT_DELETES_RETENTION_OPERATIONS_SETTING.getKey(), between(0, 1000));
         }

3448675db6d417ca20dbdac5d541dfdbaa626782
Nhat Nguyen <nhat.nguyen@elastic.co>
2018-09-01 11:34:49 -0400

    Revert "Mute testSyncerOnClosingShard"
    This reverts commit ce635f5f150d1a9b8b9e380f52aac129e79fb381.

--- server/src/test/java/org/elasticsearch/index/shard/PrimaryReplicaSyncerTests.java
+++ server/src/test/java/org/elasticsearch/index/shard/PrimaryReplicaSyncerTests.java
@@ -125,7 +125,6 @@ public class PrimaryReplicaSyncerTests extends IndexShardTestCase {
         closeShards(shard);
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/33330")
     public void testSyncerOnClosingShard() throws Exception {
         IndexShard shard = newStartedShard(true);
         AtomicBoolean syncActionCalled = new AtomicBoolean();

9576ff5f162e6caff28cf1f705b5b05b30ab41f6
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2018-09-04 19:11:27 +0100

    adds ILMClient to XPackClient

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/XPackClient.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/XPackClient.java
@@ -13,6 +13,7 @@ import org.elasticsearch.protocol.xpack.XPackInfoRequest;
 import org.elasticsearch.protocol.xpack.XPackInfoResponse;
 import org.elasticsearch.xpack.core.action.XPackInfoAction;
 import org.elasticsearch.xpack.core.action.XPackInfoRequestBuilder;
+import org.elasticsearch.xpack.core.indexlifecycle.client.ILMClient;
 import org.elasticsearch.xpack.core.ml.client.MachineLearningClient;
 import org.elasticsearch.xpack.core.monitoring.client.MonitoringClient;
 import org.elasticsearch.xpack.core.security.client.SecurityClient;
@@ -33,6 +34,7 @@ public class XPackClient {
     private final SecurityClient securityClient;
     private final WatcherClient watcherClient;
     private final MachineLearningClient machineLearning;
+    private final ILMClient ilmClient;
 
     public XPackClient(Client client) {
         this.client = client;
@@ -41,6 +43,7 @@ public class XPackClient {
         this.securityClient = new SecurityClient(client);
         this.watcherClient = new WatcherClient(client);
         this.machineLearning = new MachineLearningClient(client);
+        this.ilmClient = new ILMClient(client);
     }
 
     public Client es() {
@@ -65,6 +68,10 @@ public class XPackClient {
 
     public MachineLearningClient machineLearning() {
         return machineLearning;
+    }
+
+    public ILMClient ilmClient() {
+        return ilmClient;
     }
 
     public XPackClient withHeaders(Map<String, String> headers) {

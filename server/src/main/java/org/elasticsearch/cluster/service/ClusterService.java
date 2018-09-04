435bd33a52fcbf462aaf771f795e19e968353da7
Gordon Brown <gordon.brown@elastic.co>
2018-09-04 16:14:18 -0600

    Add user-defined cluster metadata (#33325)
    Adds a place for users to store cluster-wide data they wish to associate
    with the cluster via the Cluster Settings API. This is strictly for
    user-defined data, Elasticsearch makes no other other use of these
    settings.

--- server/src/main/java/org/elasticsearch/cluster/service/ClusterService.java
+++ server/src/main/java/org/elasticsearch/cluster/service/ClusterService.java
@@ -52,6 +52,9 @@ public class ClusterService extends AbstractLifecycleComponent {
             Setting.positiveTimeSetting("cluster.service.slow_task_logging_threshold", TimeValue.timeValueSeconds(30),
                     Property.Dynamic, Property.NodeScope);
 
+    public static final org.elasticsearch.common.settings.Setting.AffixSetting<String> USER_DEFINED_META_DATA =
+        Setting.prefixKeySetting("cluster.metadata.", (key) -> Setting.simpleString(key, Property.Dynamic, Property.NodeScope));
+
     private final ClusterName clusterName;
 
     private final OperationRouting operationRouting;
@@ -68,6 +71,8 @@ public class ClusterService extends AbstractLifecycleComponent {
         this.clusterName = ClusterName.CLUSTER_NAME_SETTING.get(settings);
         this.clusterSettings.addSettingsUpdateConsumer(CLUSTER_SERVICE_SLOW_TASK_LOGGING_THRESHOLD_SETTING,
             this::setSlowTaskLoggingThreshold);
+        // Add a no-op update consumer so changes are logged
+        this.clusterSettings.addAffixUpdateConsumer(USER_DEFINED_META_DATA, (first, second) -> {}, (first, second) -> {});
         this.initialClusterStateCustoms = initialClusterStateCustoms;
         this.clusterApplierService = new ClusterApplierService(settings, clusterSettings, threadPool, this::newClusterStateBuilder);
     }

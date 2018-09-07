8f4676a61859a11aab09f069b87f3978804fec23
Tal Levy <jubboy333@gmail.com>
2018-09-07 16:50:15 -0700

    add integration test for phase_execution_info

--- client/rest-high-level/src/test/java/org/elasticsearch/client/IndexLifecycleIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/IndexLifecycleIT.java
@@ -36,6 +36,7 @@ import org.elasticsearch.client.indexlifecycle.LifecyclePolicy;
 import org.elasticsearch.client.indexlifecycle.LifecyclePolicyMetadata;
 import org.elasticsearch.client.indexlifecycle.OperationMode;
 import org.elasticsearch.client.indexlifecycle.Phase;
+import org.elasticsearch.client.indexlifecycle.PhaseExecutionInfo;
 import org.elasticsearch.client.indexlifecycle.PutLifecyclePolicyRequest;
 import org.elasticsearch.client.indexlifecycle.RolloverAction;
 import org.elasticsearch.client.indexlifecycle.ShrinkAction;
@@ -132,7 +133,8 @@ public class IndexLifecycleIT extends ESRestHighLevelClientTestCase {
         Map<String, LifecycleAction> hotActions = Collections.singletonMap(
             RolloverAction.NAME,
             new RolloverAction(null, TimeValue.timeValueHours(50 * 24), null));
-        lifecyclePhases.put("hot", new Phase("hot", randomFrom(TimeValue.ZERO, null), hotActions));
+        Phase hotPhase = new Phase("hot", randomFrom(TimeValue.ZERO, null), hotActions);
+        lifecyclePhases.put("hot", hotPhase);
 
         Map<String, LifecycleAction> warmActions = new HashMap<>();
         warmActions.put(AllocateAction.NAME, new AllocateAction(null, null, null, Collections.singletonMap("_name", "node-1")));
@@ -182,6 +184,8 @@ public class IndexLifecycleIT extends ESRestHighLevelClientTestCase {
         assertEquals("hot", fooResponse.getPhase());
         assertEquals("rollover", fooResponse.getAction());
         assertEquals("attempt_rollover", fooResponse.getStep());
+        assertEquals(new PhaseExecutionInfo(policy.getName(), new Phase("", hotPhase.getAfter(), hotPhase.getActions()), 1L, 0L),
+            fooResponse.getPhaseExecutionInfo());
         IndexLifecycleExplainResponse bazResponse = indexResponses.get("baz-01");
         assertNotNull(bazResponse);
         assertTrue(bazResponse.managedByILM());

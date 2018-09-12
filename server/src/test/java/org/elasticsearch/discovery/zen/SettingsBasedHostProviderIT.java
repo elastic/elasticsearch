edf1feb4ee6959be32b8d9471ad444a1230561f0
David Turner <david.turner@elastic.co>
2018-09-12 13:41:42 +0200

    Just pass the settings directly to the nodes as they are started

--- server/src/test/java/org/elasticsearch/discovery/zen/SettingsBasedHostProviderIT.java
+++ server/src/test/java/org/elasticsearch/discovery/zen/SettingsBasedHostProviderIT.java
@@ -22,11 +22,7 @@ package org.elasticsearch.discovery.zen;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequest;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.settings.Settings.Builder;
 import org.elasticsearch.test.ESIntegTestCase;
-import org.junit.Before;
-
-import java.util.function.Consumer;
 
 import static org.elasticsearch.discovery.DiscoveryModule.DISCOVERY_HOSTS_PROVIDER_SETTING;
 import static org.elasticsearch.discovery.zen.SettingsBasedHostsProvider.DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING;
@@ -35,14 +31,6 @@ import static org.elasticsearch.transport.TcpTransport.PORT;
 
 @ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST, numDataNodes = 0, numClientNodes = 0)
 public class SettingsBasedHostProviderIT extends ESIntegTestCase {
-
-    private Consumer<Builder> configureDiscovery;
-
-    @Before
-    public void setDefaultDiscoveryConfiguration() {
-        configureDiscovery = b -> {
-        };
-    }
 
     @Override
     protected Settings nodeSettings(int nodeOrdinal) {
@@ -55,9 +43,9 @@ public class SettingsBasedHostProviderIT extends ESIntegTestCase {
             builder.remove(DISCOVERY_HOSTS_PROVIDER_SETTING.getKey());
         }
 
+        // super.nodeSettings sets this to an empty list, which disables any search for other nodes, but here we want this to happen:
         builder.remove(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey());
 
-        configureDiscovery.accept(builder);
         return builder.build();
     }
 
@@ -66,16 +54,19 @@ public class SettingsBasedHostProviderIT extends ESIntegTestCase {
         final NodesInfoResponse nodesInfoResponse
             = client(seedNodeName).admin().cluster().nodesInfo(new NodesInfoRequest("_local")).actionGet();
         final String seedNodeAddress = nodesInfoResponse.getNodes().get(0).getTransport().getAddress().publishAddress().toString();
-        configureDiscovery = b -> b.putList(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey(), seedNodeAddress);
         logger.info("--> using seed node address {}", seedNodeAddress);
 
         int extraNodes = randomIntBetween(1, 5);
-        internalCluster().startNodes(extraNodes);
+        internalCluster().startNodes(extraNodes,
+            Settings.builder().putList(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey(), seedNodeAddress).build());
 
         ensureStableCluster(extraNodes + 1);
     }
 
     public void testClusterFormsByScanningPorts() {
+        // This test will fail if all 4 ports just less than the one used by the first node are already bound by something else. It's hard
+        // to know how often this might happen in reality, so let's try it and see.
+
         final String seedNodeName = internalCluster().startNode();
         final NodesInfoResponse nodesInfoResponse
             = client(seedNodeName).admin().cluster().nodesInfo(new NodesInfoRequest("_local")).actionGet();
@@ -84,10 +75,7 @@ public class SettingsBasedHostProviderIT extends ESIntegTestCase {
         final String portSpec = minPort + "-" + seedNodePort;
 
         logger.info("--> using port specification [{}]", portSpec);
-
-        configureDiscovery = b -> b.put(PORT.getKey(), portSpec);
-
-        internalCluster().startNode();
+        internalCluster().startNode(Settings.builder().put(PORT.getKey(), portSpec));
         ensureStableCluster(2);
     }
 }

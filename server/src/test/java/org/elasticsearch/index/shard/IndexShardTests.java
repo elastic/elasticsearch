c79f7711bb09efe4a8d370da43eb2f3ff1817d8d
Nhat Nguyen <nhat.nguyen@elastic.co>
2018-09-12 08:24:11 -0400

    TEST: Adjust rollback condition when shard is empty
    If a shard is empty, it won't rollback its engine on promotion.
    This commit adjusts the expectation in the rollback test.
    
    Relates #33473

--- server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
+++ server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
@@ -963,7 +963,8 @@ public class IndexShardTests extends IndexShardTestCase {
         Set<String> docsBelowGlobalCheckpoint = getShardDocUIDs(indexShard).stream()
             .filter(id -> Long.parseLong(id) <= Math.max(globalCheckpointOnReplica, globalCheckpoint)).collect(Collectors.toSet());
         final CountDownLatch latch = new CountDownLatch(1);
-        final boolean shouldRollback = Math.max(globalCheckpoint, globalCheckpointOnReplica) < indexShard.seqNoStats().getMaxSeqNo();
+        final boolean shouldRollback = Math.max(globalCheckpoint, globalCheckpointOnReplica) < indexShard.seqNoStats().getMaxSeqNo()
+            && indexShard.seqNoStats().getMaxSeqNo() != SequenceNumbers.NO_OPS_PERFORMED;
         final Engine beforeRollbackEngine = indexShard.getEngine();
         indexShard.acquireReplicaOperationPermit(
                 indexShard.pendingPrimaryTerm + 1,

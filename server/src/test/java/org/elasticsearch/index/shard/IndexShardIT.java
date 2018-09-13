4d5c4b49570b1e951653d94bee3c1ac7256a39ae
Nhat Nguyen <nhat.nguyen@elastic.co>
2018-09-13 11:18:03 -0400

    TEST: decrease logging level in the flush test
    Relates #31629

--- server/src/test/java/org/elasticsearch/index/shard/IndexShardIT.java
+++ server/src/test/java/org/elasticsearch/index/shard/IndexShardIT.java
@@ -400,7 +400,7 @@ public class IndexShardIT extends ESSingleNodeTestCase {
         }
     }
 
-    @TestLogging("_root:DEBUG,org.elasticsearch.index.shard:TRACE,org.elasticsearch.index.engine:TRACE")
+    @TestLogging("org.elasticsearch.index.shard:TRACE,org.elasticsearch.index.engine:TRACE")
     public void testStressMaybeFlushOrRollTranslogGeneration() throws Exception {
         createIndex("test");
         ensureGreen();

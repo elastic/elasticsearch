94f6d4560dfc0898137c19d717d43907316c0857
David Turner <david.turner@elastic.co>
2018-09-13 07:32:33 +0200

    Revert "Mark NamedDateTimeProcessorTests as @AwaitsFix"
    This reverts commit 44a80d61b287b48e4e2a5782bfdb008d552858aa.

--- x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/expression/function/scalar/datetime/NamedDateTimeProcessorTests.java
+++ x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/expression/function/scalar/datetime/NamedDateTimeProcessorTests.java
@@ -5,7 +5,6 @@
  */
 package org.elasticsearch.xpack.sql.expression.function.scalar.datetime;
 
-import org.apache.lucene.util.LuceneTestCase.AwaitsFix;
 import org.elasticsearch.common.io.stream.Writeable.Reader;
 import org.elasticsearch.test.AbstractWireSerializingTestCase;
 import org.elasticsearch.xpack.sql.expression.function.scalar.datetime.NamedDateTimeProcessor.NameExtractor;
@@ -15,7 +14,6 @@ import org.joda.time.DateTimeZone;
 import java.io.IOException;
 import java.util.TimeZone;
 
-@AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/33621")
 public class NamedDateTimeProcessorTests extends AbstractWireSerializingTestCase<NamedDateTimeProcessor> {
     private static final TimeZone UTC = TimeZone.getTimeZone("UTC");
 

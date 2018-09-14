5e81556af645d137305758eddf36fcfb51deb104
Jason Tedor <jason@tedor.me>
2018-09-14 14:36:29 -0400

    No backticks

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -5,13 +5,13 @@ SCRIPT="$0"
 # SCRIPT might be an arbitrarily deep series of symbolic links; loop until we
 # have the concrete path
 while [ -h "$SCRIPT" ] ; do
-  ls=`ls -ld "$SCRIPT"`
+  ls=$(ls -ld "$SCRIPT")
   # Drop everything prior to ->
-  link=`expr "$ls" : '.*-> \(.*\)$'`
+  link=$(expr "$ls" : '.*-> \(.*\)$')
   if expr "$link" : '/.*' > /dev/null; then
     SCRIPT="$link"
   else
-    SCRIPT=`dirname "$SCRIPT"`/"$link"
+    SCRIPT=$(dirname "$SCRIPT")/"$link"
   fi
 done
 

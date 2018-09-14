a036702065735e633839179324e6f53e5804436f
David Turner <david.turner@elastic.co>
2018-09-14 13:49:05 +0200

    Imports

--- server/src/main/java/org/elasticsearch/discovery/PeerFinder.java
+++ server/src/main/java/org/elasticsearch/discovery/PeerFinder.java
@@ -46,7 +46,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
-import java.util.function.LongConsumer;
 import java.util.stream.Collectors;
 
 import static org.elasticsearch.common.util.concurrent.ConcurrentCollections.newConcurrentMap;

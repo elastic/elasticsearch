fa034ddf960f086448bb9f5f02be035f3b93d652
Igor Motov <igor@motovs.org>
2018-09-14 10:14:02 +0400

    Mute FullClusterRestartSettingsUpgradeIT
    Tracked by #33697

--- qa/full-cluster-restart/src/test/java/org/elasticsearch/upgrades/FullClusterRestartSettingsUpgradeIT.java
+++ qa/full-cluster-restart/src/test/java/org/elasticsearch/upgrades/FullClusterRestartSettingsUpgradeIT.java
@@ -19,6 +19,7 @@
 
 package org.elasticsearch.upgrades;
 
+import org.apache.lucene.util.LuceneTestCase.AwaitsFix;
 import org.elasticsearch.Version;
 import org.elasticsearch.action.admin.cluster.settings.ClusterGetSettingsResponse;
 import org.elasticsearch.client.Request;
@@ -39,6 +40,7 @@ import static org.elasticsearch.transport.RemoteClusterAware.SEARCH_REMOTE_CLUST
 import static org.elasticsearch.transport.RemoteClusterService.SEARCH_REMOTE_CLUSTER_SKIP_UNAVAILABLE;
 import static org.hamcrest.Matchers.equalTo;
 
+@AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/33697")
 public class FullClusterRestartSettingsUpgradeIT extends AbstractFullClusterRestartTestCase {
 
     public void testRemoteClusterSettingsUpgraded() throws IOException {

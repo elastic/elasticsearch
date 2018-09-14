4bcad95fe717714600cfe2d3d8109749ac0e00fc
Martijn van Groningen <martijn.v.groningen@gmail.com>
2018-09-14 09:59:06 +0200

    [TEST] wait for no initializing shards

--- x-pack/plugin/ccr/qa/multi-cluster-with-security/src/test/java/org/elasticsearch/xpack/ccr/FollowIndexSecurityIT.java
+++ x-pack/plugin/ccr/qa/multi-cluster-with-security/src/test/java/org/elasticsearch/xpack/ccr/FollowIndexSecurityIT.java
@@ -243,6 +243,7 @@ public class FollowIndexSecurityIT extends ESRestTestCase {
         Request request = new Request("GET", "/_cluster/health/" + index);
         request.addParameter("wait_for_status", "yellow");
         request.addParameter("wait_for_no_relocating_shards", "true");
+        request.addParameter("wait_for_no_initializing_shards", "true");
         request.addParameter("timeout", "70s");
         request.addParameter("level", "shards");
         adminClient().performRequest(request);

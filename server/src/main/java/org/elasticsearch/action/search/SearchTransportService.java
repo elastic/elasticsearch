772bb39d0bd61279c1cd91be94c7a148aec3d211
Simon Willnauer <simonw@apache.org>
2018-09-18 16:24:12 +0200

    fix imports

--- server/src/main/java/org/elasticsearch/action/search/SearchTransportService.java
+++ server/src/main/java/org/elasticsearch/action/search/SearchTransportService.java
@@ -19,8 +19,6 @@
 
 package org.elasticsearch.action.search;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.ActionListenerResponseHandler;
 import org.elasticsearch.action.IndicesRequest;

4154dded675194f47ea11293317fe0e0913dcde9
iverase <ivera@apache.org>
2018-09-21 10:42:07 +0200

    Mute ParentIdAggIT.testParentIdAggregation
    This test has failed 6 times in the last 2 days
    
    Relates: #33752

--- server/src/test/java/org/elasticsearch/search/aggregations/bucket/ParentIdAggIT.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/bucket/ParentIdAggIT.java
@@ -34,6 +34,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 
 public class ParentIdAggIT extends ESIntegTestCase {
+
+    @AwaitsFix(bugUrl="https://github.com/elastic/elasticsearch/issues/33752")
     public void testParentIdAggregation() throws IOException {
         XContentBuilder mapping = jsonBuilder().startObject()
                 .startObject("childtype")

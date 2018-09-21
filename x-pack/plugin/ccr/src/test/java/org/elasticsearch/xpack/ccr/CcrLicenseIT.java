0f7436198a6542e87abdb995968868c72f7a23f0
Martijn van Groningen <martijn.v.groningen@gmail.com>
2018-09-21 18:08:29 +0200

    [CCR] Move headers from auto follow pattern to auto follow metadata (#33846)
    This ensures that we will not serialize the headers as part of the
    auto follow pattern in the to be added get auto follow api.

--- x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/CcrLicenseIT.java
+++ x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/CcrLicenseIT.java
@@ -140,11 +140,11 @@ public class CcrLicenseIT extends ESSingleNodeTestCase {
             @Override
             public ClusterState execute(ClusterState currentState) throws Exception {
                 AutoFollowPattern autoFollowPattern =
-                    new AutoFollowPattern(Collections.singletonList("logs-*"), null, null, null, null, null, null, null, null, null);
+                    new AutoFollowPattern(Collections.singletonList("logs-*"), null, null, null, null, null, null, null, null);
                 AutoFollowMetadata autoFollowMetadata = new AutoFollowMetadata(
                     Collections.singletonMap("test_alias", autoFollowPattern),
-                    Collections.emptyMap()
-                );
+                    Collections.emptyMap(),
+                    Collections.emptyMap());
 
                 ClusterState.Builder newState = ClusterState.builder(currentState);
                 newState.metaData(MetaData.builder(currentState.getMetaData())

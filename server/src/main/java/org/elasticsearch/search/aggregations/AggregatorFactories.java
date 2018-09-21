48678001d1ed5c3c72461121d3421d7bf5213ed7
Hendrik Muhs <hendrik.muhs@elastic.co>
2018-09-21 15:39:49 +0200

    remove unused

--- server/src/main/java/org/elasticsearch/search/aggregations/AggregatorFactories.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/AggregatorFactories.java
@@ -44,7 +44,6 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;

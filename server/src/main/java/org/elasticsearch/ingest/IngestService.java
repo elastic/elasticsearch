cba63065d8330ec68c7bc2e264a36f7236ff387b
Jake Landis <jake.landis@elastic.co>
2018-09-26 15:15:22 -0500

    remove unused import

--- server/src/main/java/org/elasticsearch/ingest/IngestService.java
+++ server/src/main/java/org/elasticsearch/ingest/IngestService.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;

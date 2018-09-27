bc37d2de85134c1739b6f6e0ad1ea34110be6e32
Ryan Ernst <ryan@iernst.net>
2018-09-26 17:49:53 -0700

    Tests: Add support for custom contexts to mock scripts
    This commit adds the ability to plug in compilation of custom contexts
    in mock script engine. This is needed for testing plugins which add
    custom contexts like watcher.

--- x-pack/plugin/watcher/src/test/java/org/elasticsearch/xpack/watcher/test/integration/ExecutionVarsIntegrationTests.java
+++ x-pack/plugin/watcher/src/test/java/org/elasticsearch/xpack/watcher/test/integration/ExecutionVarsIntegrationTests.java
@@ -9,13 +9,17 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.xcontent.support.XContentMapValues;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.protocol.xpack.watcher.PutWatchResponse;
+import org.elasticsearch.script.MockScriptEngine;
 import org.elasticsearch.script.MockScriptPlugin;
+import org.elasticsearch.script.ScriptContext;
 import org.elasticsearch.xpack.core.watcher.client.WatcherClient;
 import org.elasticsearch.xpack.core.watcher.support.xcontent.ObjectPath;
 import org.elasticsearch.xpack.core.watcher.support.xcontent.XContentSource;
 import org.elasticsearch.xpack.core.watcher.transport.actions.execute.ExecuteWatchResponse;
 import org.elasticsearch.xpack.watcher.condition.ScriptCondition;
+import org.elasticsearch.xpack.watcher.condition.WatcherConditionScript;
 import org.elasticsearch.xpack.watcher.test.AbstractWatcherIntegrationTestCase;
+import org.elasticsearch.xpack.watcher.transform.script.WatcherTransformScript;
 import org.hamcrest.Matcher;
 
 import java.util.HashMap;
@@ -104,6 +108,27 @@ public class ExecutionVarsIntegrationTests extends AbstractWatcherIntegrationTes
             });
 
             return scripts;
+        }
+
+        protected Map<ScriptContext<?>, MockScriptEngine.ContextCompiler> pluginContextCompilers() {
+            HashMap<ScriptContext<?>, MockScriptEngine.ContextCompiler> compilers = new HashMap<>();
+            compilers.put(WatcherConditionScript.CONTEXT, (script, options) ->
+                (WatcherConditionScript.Factory) (params, watcherContext) ->
+                    new WatcherConditionScript(params, watcherContext) {
+                        @Override
+                        public boolean execute() {
+                            return (boolean) script.apply(getParams());
+                        }
+                    });
+            compilers.put(WatcherTransformScript.CONTEXT, (script, options) ->
+                (WatcherTransformScript.Factory) (params, watcherContext, payload) ->
+                    new WatcherTransformScript(params, watcherContext, payload) {
+                        @Override
+                        public Object execute() {
+                            return script.apply(getParams());
+                        }
+                    });
+            return compilers;
         }
     }
 

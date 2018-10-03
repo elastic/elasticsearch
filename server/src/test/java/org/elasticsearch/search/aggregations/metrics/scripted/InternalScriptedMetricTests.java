b56d0a250e7a996584eb61847537c40aed79c10b
Kazuhiro Sera <seratch@gmail.com>
2018-10-04 02:11:39 +0900

    Fix a variety of typos and misspelled words (#32792)

--- server/src/test/java/org/elasticsearch/search/aggregations/metrics/scripted/InternalScriptedMetricTests.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/metrics/scripted/InternalScriptedMetricTests.java
@@ -113,7 +113,7 @@ public class InternalScriptedMetricTests extends InternalAggregationTestCase<Int
      */
     @Override
     protected ScriptService mockScriptService() {
-        // mock script always retuns the size of the input aggs list as result
+        // mock script always returns the size of the input aggs list as result
         @SuppressWarnings("unchecked")
         MockScriptEngine scriptEngine = new MockScriptEngine(MockScriptEngine.NAME,
                 Collections.singletonMap(REDUCE_SCRIPT_NAME, script -> ((List<Object>) script.get("states")).size()),

c14cf7ae25b705445d62650ac2ec2044fd2e0607
David Turner <david.turner@elastic.co>
2018-10-03 13:09:59 +0100

    Register setting

--- server/src/main/java/org/elasticsearch/common/settings/ClusterSettings.java
+++ server/src/main/java/org/elasticsearch/common/settings/ClusterSettings.java
@@ -445,6 +445,7 @@ public final class ClusterSettings extends AbstractScopedSettings {
                     IndexGraveyard.SETTING_MAX_TOMBSTONES,
                     EnableAssignmentDecider.CLUSTER_TASKS_ALLOCATION_ENABLE_SETTING,
                     PeerFinder.DISCOVERY_FIND_PEERS_INTERVAL_SETTING,
+                    PeerFinder.DISCOVERY_REQUEST_PEERS_TIMEOUT_SETTING,
                     ElectionSchedulerFactory.ELECTION_INITIAL_TIMEOUT_SETTING,
                     ElectionSchedulerFactory.ELECTION_BACK_OFF_TIME_SETTING,
                     ElectionSchedulerFactory.ELECTION_MAX_TIMEOUT_SETTING,

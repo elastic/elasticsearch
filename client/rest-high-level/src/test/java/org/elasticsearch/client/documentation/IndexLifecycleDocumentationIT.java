10159b369a575f030da7739dd8eece95d52f1ba2
Armin Braun <me@obrown.io>
2018-10-05 16:00:32 +0200

    start documentation

---
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/documentation/IndexLifecycleDocumentationIT.java
@@ -0,0 +1,8 @@
+package org.elasticsearch.client.documentation;
+
+import org.elasticsearch.client.ESRestHighLevelClientTestCase;
+
+public class IndexLifecycleDocumentationIT extends ESRestHighLevelClientTestCase {
+
+
+}

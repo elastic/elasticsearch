2a926f38776f4fbed692afec2cc5301ff7bb8835
Christophe Bismuth <christophe.bismuth@gmail.com>
2018-10-15 16:42:38 +0200

    Include unassigned indices in index stats endpoint (#27178)

--- server/src/main/java/org/elasticsearch/action/admin/indices/stats/IndicesStatsResponse.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/stats/IndicesStatsResponse.java
@@ -21,6 +21,12 @@ package org.elasticsearch.action.admin.indices.stats;
 
 import org.elasticsearch.action.support.DefaultShardOperationFailedException;
 import org.elasticsearch.action.support.broadcast.BroadcastResponse;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.health.ClusterIndexHealth;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
+import org.elasticsearch.cluster.metadata.MetaData;
+import org.elasticsearch.cluster.routing.IndexRoutingTable;
+import org.elasticsearch.cluster.routing.RoutingTable;
 import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.StreamInput;
@@ -34,6 +40,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 import static java.util.Collections.unmodifiableMap;
@@ -44,6 +51,9 @@ public class IndicesStatsResponse extends BroadcastResponse {
 
     private Map<ShardRouting, ShardStats> shardStatsMap;
 
+    private MetaData metaData;
+    private RoutingTable routingTable;
+
     IndicesStatsResponse() {
 
     }
@@ -52,6 +62,14 @@ public class IndicesStatsResponse extends BroadcastResponse {
                          List<DefaultShardOperationFailedException> shardFailures) {
         super(totalShards, successfulShards, failedShards, shardFailures);
         this.shards = shards;
+    }
+
+    IndicesStatsResponse(ShardStats[] shards, int totalShards, int successfulShards, int failedShards,
+                         List<DefaultShardOperationFailedException> shardFailures, ClusterState clusterState) {
+        super(totalShards, successfulShards, failedShards, shardFailures);
+        this.shards = shards;
+        this.metaData = clusterState.metaData();
+        this.routingTable = clusterState.routingTable();
     }
 
     public Map<ShardRouting, ShardStats> asMap() {
@@ -171,31 +189,69 @@ public class IndicesStatsResponse extends BroadcastResponse {
 
         if ("indices".equalsIgnoreCase(level) || "shards".equalsIgnoreCase(level)) {
             builder.startObject(Fields.INDICES);
-            for (IndexStats indexStats : getIndices().values()) {
-                builder.startObject(indexStats.getIndex());
-                builder.field("uuid", indexStats.getUuid());
-                builder.startObject("primaries");
-                indexStats.getPrimaries().toXContent(builder, params);
-                builder.endObject();
+            if (getIndices().values().isEmpty() == false) {
+                for (IndexStats indexStats : getIndices().values()) {
+                    builder.startObject(indexStats.getIndex());
+                    builder.field("uuid", indexStats.getUuid());
+                    builder.startObject("primaries");
+                    indexStats.getPrimaries().toXContent(builder, params);
+                    builder.endObject();
 
-                builder.startObject("total");
-                indexStats.getTotal().toXContent(builder, params);
-                builder.endObject();
+                    builder.startObject("total");
+                    indexStats.getTotal().toXContent(builder, params);
+                    builder.endObject();
 
-                if ("shards".equalsIgnoreCase(level)) {
-                    builder.startObject(Fields.SHARDS);
-                    for (IndexShardStats indexShardStats : indexStats) {
-                        builder.startArray(Integer.toString(indexShardStats.getShardId().id()));
-                        for (ShardStats shardStats : indexShardStats) {
-                            builder.startObject();
-                            shardStats.toXContent(builder, params);
-                            builder.endObject();
+                    if ("shards".equalsIgnoreCase(level)) {
+                        builder.startObject(Fields.SHARDS);
+                        for (IndexShardStats indexShardStats : indexStats) {
+                            builder.startArray(Integer.toString(indexShardStats.getShardId().id()));
+                            for (ShardStats shardStats : indexShardStats) {
+                                builder.startObject();
+                                shardStats.toXContent(builder, params);
+                                builder.endObject();
+                            }
+                            builder.endArray();
                         }
-                        builder.endArray();
+                        builder.endObject();
                     }
                     builder.endObject();
                 }
-                builder.endObject();
+            } else if (metaData != null && routingTable != null) {
+                for (IndexRoutingTable indexRoutingTable : routingTable) {
+                    final Index index = indexRoutingTable.getIndex();
+                    if (params.param("index") == null || Objects.equals(params.param("index"), index.getName())) {
+                        final IndexMetaData indexMetaData = metaData.index(index);
+                        final ClusterIndexHealth health = new ClusterIndexHealth(indexMetaData, indexRoutingTable);
+
+                        final int totalNumberOfShards = indexMetaData.getTotalNumberOfShards();
+                        final int numberOfShards = metaData.getNumberOfShards();
+
+                        builder.startObject(index.getName());
+                        builder.field("uuid", index.getUUID());
+                        builder.field("primaries", numberOfShards);
+                        builder.field("total", totalNumberOfShards);
+
+                        if ("shards".equalsIgnoreCase(level)) {
+                            final int activeTotal = health.getActiveShards();
+                            final int activePrimaries = health.getActivePrimaryShards();
+                            final int unassignedTotal = health.getUnassignedShards() + health.getInitializingShards();
+                            final int unassignedPrimaries = numberOfShards - health.getActivePrimaryShards();
+
+                            builder.startObject("shards");
+                            builder.field("replicas", metaData.index(index).getNumberOfReplicas());
+                            builder.field("active_total", activeTotal);
+                            builder.field("active_primaries", activePrimaries);
+                            builder.field("active_replicas", activeTotal - activePrimaries);
+                            builder.field("unassigned_total", unassignedTotal);
+                            builder.field("unassigned_primaries", unassignedPrimaries);
+                            builder.field("unassigned_replicas", unassignedTotal - unassignedPrimaries);
+                            builder.field("initializing", health.getInitializingShards());
+                            builder.field("relocating", health.getRelocatingShards());
+                            builder.endObject();
+                        }
+                        builder.endObject();
+                    }
+                }
             }
             builder.endObject();
         }
--- server/src/main/java/org/elasticsearch/action/admin/indices/stats/TransportIndicesStatsAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/stats/TransportIndicesStatsAction.java
@@ -82,8 +82,11 @@ public class TransportIndicesStatsAction extends TransportBroadcastByNodeAction<
     }
 
     @Override
-    protected IndicesStatsResponse newResponse(IndicesStatsRequest request, int totalShards, int successfulShards, int failedShards, List<ShardStats> responses, List<DefaultShardOperationFailedException> shardFailures, ClusterState clusterState) {
-        return new IndicesStatsResponse(responses.toArray(new ShardStats[responses.size()]), totalShards, successfulShards, failedShards, shardFailures);
+    protected IndicesStatsResponse newResponse(IndicesStatsRequest request, int totalShards, int successfulShards, int failedShards,
+                                               List<ShardStats> responses, List<DefaultShardOperationFailedException> shardFailures,
+                                               ClusterState clusterState) {
+        return new IndicesStatsResponse(responses.toArray(new ShardStats[responses.size()]), totalShards, successfulShards, failedShards,
+            shardFailures, clusterState);
     }
 
     @Override

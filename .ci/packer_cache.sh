079b9485356e4d07e6c5b1fec3c427fb5dedc2ad
Alpar Torok <torokalpar@gmail.com>
2018-10-16 16:27:19 +0300

    Circle branches to catch all deps

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -17,17 +17,28 @@ done
 
 cd $(dirname "${SCRIPT}")/..
 
-source .ci/java-versions.properties
+resolveAllDependencies() {
+    local REFRESH_FAILED=no
+    local MAX_TRIES=3
+    for i in $(seq 1 $MAX_TRIES) ; do 
+        echo "Resolving dependencies try $i/$MAX_TRIES"
+        JAVA_HOME="${HOME}"/.java/${ES_BUILD_JAVA} ./gradlew resolveAllDependencies --parallel && break
+        REFRESH_FAILED=yes
+    done
+    if [ $REFRESH_FAILED != "no" ] ; then
+        echo "Resolving dependencies failed after 3 retries ..."
+        exit 1
+    fi   
+}
 
-REFRESH_FAILED=no
-MAX_TRIES=3
-for i in $(seq 1 $MAX_TRIES) ; do 
-    echo "Resolving dependencies try $i/$MAX_TRIES"
-    JAVA_HOME="${HOME}"/.java/${ES_BUILD_JAVA} ./gradlew resolveAllDependencies --parallel && break
-    REFRESH_FAILED=yes
+for branch in "master" "6.x" "5.6" ; do 
+    echo "Resolving dependencies for $branch"
+    if  ! git checkout $branch ; then 
+        echo "Failed to switch branch to $branch"
+        exit 2
+    fi
+    source .ci/java-versions.properties
+    resolveAllDependencies
 done
-if [ $REFRESH_FAILED != "no" ] ; then
-    echo "Resolving dependencies failed after 3 retries ..."
-    exit 1
-fi   
+
 exit 0

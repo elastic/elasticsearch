26792b192711981a5bae65977deca36f4d373253
Jim Ferenczi <jimczi@apache.org>
2018-10-16 12:08:22 +0200

    fix style

--- server/src/main/java/org/elasticsearch/search/suggest/completion/TopSuggestGroupDocsCollector.java
+++ server/src/main/java/org/elasticsearch/search/suggest/completion/TopSuggestGroupDocsCollector.java
@@ -37,17 +37,21 @@ import java.util.Map;
 /**
  *
  * Custom {@link TopSuggestDocsCollector} that returns top documents from the completion suggester.
- * When suggestions are augmented with contexts values this collector groups suggestions coming from the same document
- * but matching different contexts together.
+ * <p>
+ * TODO: this should be refactored when https://issues.apache.org/jira/browse/LUCENE-8529 is fixed.
+ * Unlike the parent class, this collector uses the surface form to tie-break suggestions with identical
+ * scores.
+ * <p>
+ * This collector groups suggestions coming from the same document but matching different contexts
+ * or surface form together. When different contexts or surface forms match the same suggestion form only
+ * the best one per document (sorted by weight) is kept.
+ * <p>
  * This collector is also able to filter duplicate suggestion coming from different documents.
- * When different contexts match the same suggestion form only the best one (sorted by weight) is kept.
  * In order to keep this feature fast, the de-duplication of suggestions with different contexts is done
  * only on the top N*num_contexts (where N is the number of documents to return) suggestions per segment.
  * This means that skip_duplicates will visit at most N*num_contexts suggestions per segment to find unique suggestions
  * that match the input. If more than N*num_contexts suggestions are duplicated with different contexts this collector
  * will not be able to return more than one suggestion even when N is greater than 1.
- *
- * TODO: this should be refactored when https://issues.apache.org/jira/browse/LUCENE-8529 is fixed.
  **/
 public class TopSuggestGroupDocsCollector extends TopSuggestDocsCollector {
     private final class SuggestScoreDocPriorityQueue extends PriorityQueue<TopSuggestDocs.SuggestScoreDoc> {
@@ -119,6 +123,9 @@ public class TopSuggestGroupDocsCollector extends TopSuggestDocsCollector {
         }
     }
 
+    /**
+     * Returns the contexts associated with the provided <code>doc</code>.
+     */
     public List<CharSequence> getContexts(int doc) {
         return docContexts.getOrDefault(doc, Collections.emptyList());
     }
@@ -145,14 +152,7 @@ public class TopSuggestGroupDocsCollector extends TopSuggestDocsCollector {
         }
     }
 
-    /**
-     * Called for every matched completion,
-     * similar to {@link org.apache.lucene.search.LeafCollector#collect(int)}
-     * but for completions.
-     *
-     * NOTE: collection at the leaf level is guaranteed to be in
-     * descending order of score
-     */
+    @Override
     public void collect(int docID, CharSequence key, CharSequence context, float score) throws IOException {
         int globalDoc = docID + docBase;
         boolean isDuplicate = docContexts.containsKey(globalDoc);
@@ -163,7 +163,7 @@ public class TopSuggestGroupDocsCollector extends TopSuggestDocsCollector {
         if (isDuplicate) {
             return;
         }
-        TopSuggestDocs.SuggestScoreDoc current = new TopSuggestDocs.SuggestScoreDoc(docBase + docID, key, context, score);
+        TopSuggestDocs.SuggestScoreDoc current = new TopSuggestDocs.SuggestScoreDoc(globalDoc, key, context, score);
         if (current == priorityQueue.insertWithOverflow(current)) {
             // if the current SuggestScoreDoc has overflown from pq,
             // we can assume all of the successive collections from
@@ -173,9 +173,7 @@ public class TopSuggestGroupDocsCollector extends TopSuggestDocsCollector {
         }
     }
 
-    /**
-     * Returns at most <code>num</code> Top scoring {@link org.apache.lucene.search.suggest.document.TopSuggestDocs}s
-     */
+    @Override
     public TopSuggestDocs get() throws IOException {
 
         TopSuggestDocs.SuggestScoreDoc[] suggestScoreDocs;

0d960201c4fbb69a6abf024568bc5c6df62a9f5a
Benjamin Trent <benjamin.trent@elastic.co>
2018-10-16 16:14:10 -0500

    fixing failing client side test with new default values

--- client/rest-high-level/src/test/java/org/elasticsearch/client/RollupIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/RollupIT.java
@@ -41,6 +41,7 @@ import org.elasticsearch.client.rollup.job.config.GroupConfig;
 import org.elasticsearch.client.rollup.job.config.MetricConfig;
 import org.elasticsearch.client.rollup.job.config.RollupJobConfig;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.mapper.DateFieldMapper;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramInterval;
@@ -161,7 +162,9 @@ public class RollupIT extends ESRestHighLevelClientTestCase {
     public void testPutAndGetRollupJob() throws Exception {
         // TODO expand this to also test with histogram and terms?
         final GroupConfig groups = new GroupConfig(new DateHistogramGroupConfig("date", DateHistogramInterval.DAY));
-        final List<MetricConfig> metrics = Collections.singletonList(new MetricConfig("value", SUPPORTED_METRICS));
+        final List<MetricConfig> metrics = Arrays.asList(
+            new MetricConfig("value", SUPPORTED_METRICS),
+            new MetricConfig("date", Arrays.asList(MaxAggregationBuilder.NAME)));
         final TimeValue timeout = TimeValue.timeValueSeconds(randomIntBetween(30, 600));
 
         PutRollupJobRequest putRollupJobRequest =
@@ -189,21 +192,28 @@ public class RollupIT extends ESRestHighLevelClientTestCase {
             assertEquals(groups.getDateHistogram().getTimeZone(), source.get("date.date_histogram.time_zone"));
 
             for (MetricConfig metric : metrics) {
-                for (String name : metric.getMetrics()) {
-                    Number value = (Number) source.get(metric.getField() + "." + name + ".value");
-                    if ("min".equals(name)) {
-                        assertEquals(min, value.intValue());
-                    } else if ("max".equals(name)) {
-                        assertEquals(max, value.intValue());
-                    } else if ("sum".equals(name)) {
-                        assertEquals(sum, value.doubleValue(), 0.0d);
-                    } else if ("avg".equals(name)) {
-                        assertEquals(sum, value.doubleValue(), 0.0d);
-                        Number avgCount = (Number) source.get(metric.getField() + "." + name + "._count");
-                        assertEquals(numDocs, avgCount.intValue());
-                    } else if ("value_count".equals(name)) {
-                        assertEquals(numDocs, value.intValue());
+                if (metric.getField().equals("value")) {
+                    for (String name : metric.getMetrics()) {
+                        Number value = (Number) source.get(metric.getField() + "." + name + ".value");
+                        if ("min".equals(name)) {
+                            assertEquals(min, value.intValue());
+                        } else if ("max".equals(name)) {
+                            assertEquals(max, value.intValue());
+                        } else if ("sum".equals(name)) {
+                            assertEquals(sum, value.doubleValue(), 0.0d);
+                        } else if ("avg".equals(name)) {
+                            assertEquals(sum, value.doubleValue(), 0.0d);
+                            Number avgCount = (Number) source.get(metric.getField() + "." + name + "._count");
+                            assertEquals(numDocs, avgCount.intValue());
+                        } else if ("value_count".equals(name)) {
+                            assertEquals(numDocs, value.intValue());
+                        }
                     }
+                } else {
+                    Number value = (Number) source.get(metric.getField() + ".max.value");
+                    assertEquals(
+                        DateFieldMapper.DEFAULT_DATE_TIME_FORMATTER.parser().parseDateTime("2018-01-01T00:59:50").getMillis(),
+                        value.longValue());
                 }
             }
         });

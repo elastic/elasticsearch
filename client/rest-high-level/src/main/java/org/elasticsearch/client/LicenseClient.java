18aa1c1381b76769ed5ebaaf4b6840aa26301d75
Andy Bristol <andy.bristol@elastic.co>
2018-10-17 08:02:04 -0700

    add start trial API to HLRC (#33406)
    Introduces client-specific request and response classes that do not
    depend on the server
    
    The `type` parameter is named `licenseType` in the response class to be
    more descriptive. The parts that make up the acknowledged-required
    response are given slightly different names than their server-response
    types to be consistent with the naming in the put license API
    
    Tests do not cover all cases because the integ test cluster starts up
    with a trial license - this will be addressed in a future commit

--- client/rest-high-level/src/main/java/org/elasticsearch/client/LicenseClient.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/LicenseClient.java
@@ -22,6 +22,8 @@ package org.elasticsearch.client;
 import org.apache.http.HttpEntity;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.license.StartTrialRequest;
+import org.elasticsearch.client.license.StartTrialResponse;
 import org.elasticsearch.client.license.StartBasicRequest;
 import org.elasticsearch.client.license.StartBasicResponse;
 import org.elasticsearch.common.Strings;
@@ -44,6 +46,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 import static java.util.Collections.emptySet;
+import static java.util.Collections.singleton;
 
 /**
  * A wrapper for the {@link RestHighLevelClient} that provides methods for
@@ -121,6 +124,30 @@ public final class LicenseClient {
     public void deleteLicenseAsync(DeleteLicenseRequest request, RequestOptions options, ActionListener<AcknowledgedResponse> listener) {
         restHighLevelClient.performRequestAsyncAndParseEntity(request, LicenseRequestConverters::deleteLicense, options,
             AcknowledgedResponse::fromXContent, listener, emptySet());
+    }
+
+    /**
+     * Starts a trial license on the cluster.
+     * @param options the request options (e.g. headers), use {@link RequestOptions#DEFAULT} if nothing needs to be customized
+     * @return the response
+     * @throws IOException in case there is a problem sending the request or parsing back the response
+     */
+    public StartTrialResponse startTrial(StartTrialRequest request, RequestOptions options) throws IOException {
+        return restHighLevelClient.performRequestAndParseEntity(request, LicenseRequestConverters::startTrial, options,
+            StartTrialResponse::fromXContent, singleton(403));
+    }
+
+    /**
+     * Asynchronously starts a trial license on the cluster.
+     * @param options the request options (e.g. headers), use {@link RequestOptions#DEFAULT} if nothing needs to be customized
+     * @param listener the listener to be notified upon request completion
+     */
+    public void startTrialAsync(StartTrialRequest request,
+                                RequestOptions options,
+                                ActionListener<StartTrialResponse> listener) {
+
+        restHighLevelClient.performRequestAsyncAndParseEntity(request, LicenseRequestConverters::startTrial, options,
+            StartTrialResponse::fromXContent, listener, singleton(403));
     }
 
     /**

0e88165bee2af5db70d8787c9a0d4f0a2f53919d
Zachary Tong <zach@elastic.co>
2018-10-18 14:32:43 -0400

    checkstyle

--- test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
@@ -71,7 +71,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.function.BooleanSupplier;
 
 import static java.util.Collections.sort;
 import static java.util.Collections.unmodifiableList;

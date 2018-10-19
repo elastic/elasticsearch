7e1ab2d9e75fb339adf54a6e7bcafd1568c20449
Nhat Nguyen <nhat.nguyen@elastic.co>
2018-10-19 13:56:00 -0400

    CCR: Replicate existing ops with old term on follower (#34412)
    Since #34288, we might hit deadlock if the FollowTask has more fetchers
    than writers. This can happen in the following scenario:
    
    Suppose the leader has two operations [seq#0, seq#1]; the FollowTask has
    two fetchers and one writer.
    
    1. The FollowTask issues two concurrent fetch requests: {from_seq_no: 0,
    num_ops:1} and {from_seq_no: 1, num_ops:1} to read seq#0 and seq#1
    respectively.
    
    2. The second request which fetches seq#1 completes before, and then it
    triggers a write request containing only seq#1.
    
    3. The primary of a follower fails after it has replicated seq#1 to
    replicas.
    
    4. Since the old primary did not respond, the FollowTask issues another
    write request containing seq#1 (resend the previous write request).
    
    5. The new primary has seq#1 already; thus it won't replicate seq#1 to
    replicas but will wait for the global checkpoint to advance at least
    seq#1.
    
    The problem is that the FollowTask has only one writer and that writer
    is waiting for seq#0 which won't be delivered until the writer completed.
    
    This PR proposes to replicate existing operations with the old primary
    term (instead of the current term) on the follower. In particular, when
    the following primary detects that it has processed an process already,
    it will look up the term of an existing operation with the same seq_no
    in the Lucene index, then rewrite that operation with the old term
    before replicating it to the following replicas. This approach is
    wait-free but requires soft-deletes on the follower.
    
    Relates #34288

--- x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/action/bulk/BulkShardOperationsTests.java
+++ x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/action/bulk/BulkShardOperationsTests.java
@@ -7,18 +7,18 @@
 package org.elasticsearch.xpack.ccr.action.bulk;
 
 import org.apache.lucene.index.Term;
-import org.elasticsearch.ElasticsearchTimeoutException;
-import org.elasticsearch.action.support.PlainActionFuture;
+import org.elasticsearch.Version;
+import org.elasticsearch.action.admin.indices.flush.FlushRequest;
 import org.elasticsearch.action.support.replication.TransportWriteAction;
+import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.common.Randomness;
-import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.index.engine.Engine;
+import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.mapper.Uid;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.index.shard.IndexShardTestCase;
 import org.elasticsearch.index.translog.Translog;
+import org.elasticsearch.indices.recovery.RecoveryState;
 import org.elasticsearch.xpack.ccr.CcrSettings;
 import org.elasticsearch.xpack.ccr.index.engine.FollowingEngineFactory;
 
@@ -29,6 +29,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.emptySet;
+import static org.elasticsearch.xpack.ccr.action.bulk.TransportBulkShardOperationsAction.rewriteOperationWithPrimaryTerm;
 import static org.hamcrest.Matchers.equalTo;
 import static org.elasticsearch.xpack.ccr.action.bulk.TransportBulkShardOperationsAction.CcrWritePrimaryResult;
 
@@ -87,60 +90,11 @@ public class BulkShardOperationsTests extends IndexShardTestCase {
         closeShards(followerPrimary);
     }
 
-    public void testPrimaryResultWaitForGlobalCheckpoint() throws Exception {
-        final Settings settings = Settings.builder().put(CcrSettings.CCR_FOLLOWING_INDEX_SETTING.getKey(), true).build();
-        final IndexShard shard = newStartedShard(false, settings, new FollowingEngineFactory());
-        int numOps = between(1, 100);
-        for (int i = 0; i < numOps; i++) {
-            final String id = Integer.toString(i);
-            final Translog.Operation op;
-            if (randomBoolean()) {
-                op = new Translog.Index("_doc", id, i, primaryTerm, 0, SOURCE, null, -1);
-            } else if (randomBoolean()) {
-                shard.advanceMaxSeqNoOfUpdatesOrDeletes(i);
-                op = new Translog.Delete("_doc", id, new Term("_id", Uid.encodeId(id)), i, primaryTerm, 0);
-            } else {
-                op = new Translog.NoOp(i, primaryTerm, "test");
-            }
-            shard.applyTranslogOperation(op, Engine.Operation.Origin.REPLICA);
-        }
-        BulkShardOperationsRequest request = new BulkShardOperationsRequest();
-        {
-            PlainActionFuture<BulkShardOperationsResponse> listener = new PlainActionFuture<>();
-            CcrWritePrimaryResult primaryResult = new CcrWritePrimaryResult(request, null, shard, -2, logger);
-            primaryResult.respond(listener);
-            assertThat("should return intermediately if waiting_global_checkpoint is not specified", listener.isDone(), equalTo(true));
-            assertThat(listener.get().getMaxSeqNo(), equalTo(shard.seqNoStats().getMaxSeqNo()));
-        }
-        {
-            PlainActionFuture<BulkShardOperationsResponse> listener = new PlainActionFuture<>();
-            long waitingForGlobalCheckpoint = randomLongBetween(shard.getGlobalCheckpoint() + 1, shard.getLocalCheckpoint());
-            CcrWritePrimaryResult primaryResult = new CcrWritePrimaryResult(request, null, shard, waitingForGlobalCheckpoint, logger);
-            primaryResult.respond(listener);
-            assertThat(listener.isDone(), equalTo(false));
-            expectThrows(ElasticsearchTimeoutException.class, () -> listener.actionGet(TimeValue.timeValueMillis(1)));
-
-            shard.updateGlobalCheckpointOnReplica(randomLongBetween(shard.getGlobalCheckpoint(), waitingForGlobalCheckpoint - 1), "test");
-            expectThrows(ElasticsearchTimeoutException.class, () -> listener.actionGet(TimeValue.timeValueMillis(1)));
-
-            shard.updateGlobalCheckpointOnReplica(randomLongBetween(waitingForGlobalCheckpoint, shard.getLocalCheckpoint()), "test");
-            assertThat(listener.get().getMaxSeqNo(), equalTo(shard.seqNoStats().getMaxSeqNo()));
-            assertThat(listener.get().getGlobalCheckpoint(), equalTo(shard.getGlobalCheckpoint()));
-        }
-        {
-            PlainActionFuture<BulkShardOperationsResponse> listener = new PlainActionFuture<>();
-            long waitingForGlobalCheckpoint = randomLongBetween(-1, shard.getGlobalCheckpoint());
-            CcrWritePrimaryResult primaryResult = new CcrWritePrimaryResult(request, null, shard, waitingForGlobalCheckpoint, logger);
-            primaryResult.respond(listener);
-            assertThat(listener.get().getMaxSeqNo(), equalTo(shard.seqNoStats().getMaxSeqNo()));
-            assertThat(listener.get().getGlobalCheckpoint(), equalTo(shard.getGlobalCheckpoint()));
-        }
-        closeShards(shard);
-    }
-
     public void testPrimaryResultIncludeOnlyAppliedOperations() throws Exception {
-        final Settings settings = Settings.builder().put(CcrSettings.CCR_FOLLOWING_INDEX_SETTING.getKey(), true).build();
-        final IndexShard primary = newStartedShard(true, settings, new FollowingEngineFactory());
+        final Settings settings = Settings.builder().put(CcrSettings.CCR_FOLLOWING_INDEX_SETTING.getKey(), true)
+            .put(IndexSettings.INDEX_SOFT_DELETES_SETTING.getKey(), true).build();
+        final IndexShard oldPrimary = newStartedShard(true, settings, new FollowingEngineFactory());
+        final long oldPrimaryTerm = oldPrimary.getOperationPrimaryTerm();
         long seqno = 0;
         List<Translog.Operation> firstBulk = new ArrayList<>();
         List<Translog.Operation> secondBulk = new ArrayList<>();
@@ -157,46 +111,41 @@ public class BulkShardOperationsTests extends IndexShardTestCase {
             } else {
                 secondBulk.add(op);
             }
+            if (rarely()) {
+                oldPrimary.refresh("test");
+            }
+            if (rarely()) {
+                oldPrimary.flush(new FlushRequest());
+            }
         }
         Randomness.shuffle(firstBulk);
         Randomness.shuffle(secondBulk);
-        primary.advanceMaxSeqNoOfUpdatesOrDeletes(seqno);
-
-        final CcrWritePrimaryResult fullResult = TransportBulkShardOperationsAction.shardOperationOnPrimary(primary.shardId(),
-            primary.getHistoryUUID(), firstBulk, seqno, primary, logger);
+        oldPrimary.advanceMaxSeqNoOfUpdatesOrDeletes(seqno);
+        final CcrWritePrimaryResult fullResult = TransportBulkShardOperationsAction.shardOperationOnPrimary(oldPrimary.shardId(),
+            oldPrimary.getHistoryUUID(), firstBulk, seqno, oldPrimary, logger);
         assertThat(fullResult.replicaRequest().getOperations(),
-            equalTo(rewriteWithPrimaryTerm(firstBulk, primary.getOperationPrimaryTerm())));
-        assertThat(fullResult.waitingForGlobalCheckpoint, equalTo(-2L));
-
-        // This bulk includes some operations from the first bulk. These operations should not be included in the result.
+            equalTo(firstBulk.stream().map(op -> rewriteOperationWithPrimaryTerm(op, oldPrimaryTerm)).collect(Collectors.toList())));
+        primaryTerm = randomLongBetween(primaryTerm, primaryTerm + 10);
+        final IndexShard newPrimary = reinitShard(oldPrimary);
+        DiscoveryNode localNode = new DiscoveryNode("foo", buildNewFakeTransportAddress(), emptyMap(), emptySet(), Version.CURRENT);
+        newPrimary.markAsRecovering("store", new RecoveryState(newPrimary.routingEntry(), localNode, null));
+        assertTrue(newPrimary.recoverFromStore());
+        IndexShardTestCase.updateRoutingEntry(newPrimary, newPrimary.routingEntry().moveToStarted());
+        newPrimary.advanceMaxSeqNoOfUpdatesOrDeletes(seqno);
+        // The second bulk includes some operations from the first bulk which were processed already;
+        // only a subset of these operations will be included the result but with the old primary term.
         final List<Translog.Operation> existingOps = randomSubsetOf(firstBulk);
-        final CcrWritePrimaryResult partialResult = TransportBulkShardOperationsAction.shardOperationOnPrimary(primary.shardId(),
-            primary.getHistoryUUID(), Stream.concat(existingOps.stream(), secondBulk.stream()).collect(Collectors.toList()),
-            seqno, primary, logger);
-        assertThat(partialResult.replicaRequest().getOperations(),
-            equalTo(rewriteWithPrimaryTerm(secondBulk, primary.getOperationPrimaryTerm())));
-        assertThat(partialResult.waitingForGlobalCheckpoint,
-            equalTo(existingOps.stream().mapToLong(Translog.Operation::seqNo).max().orElse(-2L)));
+        final CcrWritePrimaryResult partialResult = TransportBulkShardOperationsAction.shardOperationOnPrimary(newPrimary.shardId(),
+            newPrimary.getHistoryUUID(), Stream.concat(secondBulk.stream(), existingOps.stream()).collect(Collectors.toList()),
+            seqno, newPrimary, logger);
+        final long newPrimaryTerm = newPrimary.getOperationPrimaryTerm();
+        final long globalCheckpoint = newPrimary.getGlobalCheckpoint();
+        final List<Translog.Operation> appliedOperations = Stream.concat(
+            secondBulk.stream().map(op -> rewriteOperationWithPrimaryTerm(op, newPrimaryTerm)),
+            existingOps.stream().filter(op -> op.seqNo() > globalCheckpoint).map(op -> rewriteOperationWithPrimaryTerm(op, oldPrimaryTerm))
+        ).collect(Collectors.toList());
 
-        closeShards(primary);
-    }
-
-    private List<Translog.Operation> rewriteWithPrimaryTerm(List<Translog.Operation> sourceOperations, long primaryTerm) {
-        return sourceOperations.stream().map(op -> {
-            switch (op.opType()) {
-                case INDEX:
-                    final Translog.Index index = (Translog.Index) op;
-                    return new Translog.Index(index.type(), index.id(), index.seqNo(), primaryTerm,
-                        index.version(), BytesReference.toBytes(index.source()), index.routing(), index.getAutoGeneratedIdTimestamp());
-                case DELETE:
-                    final Translog.Delete delete = (Translog.Delete) op;
-                    return new Translog.Delete(delete.type(), delete.id(), delete.uid(), delete.seqNo(), primaryTerm, delete.version());
-                case NO_OP:
-                    final Translog.NoOp noOp = (Translog.NoOp) op;
-                    return new Translog.NoOp(noOp.seqNo(), primaryTerm, noOp.reason());
-                default:
-                    throw new IllegalStateException("unexpected operation type [" + op.opType() + "]");
-            }
-        }).collect(Collectors.toList());
+        assertThat(partialResult.replicaRequest().getOperations(), equalTo(appliedOperations));
+        closeShards(newPrimary);
     }
 }

60bdaf55b726b94e8ed00bfdf9ba0546f8530b60
Ryan Ernst <ryan@iernst.net>
2018-10-22 20:30:15 -0700

    Test: Fix last reference to SearchScript
    This was accidentally left over when converting to FieldScript.
    
    closes #34683

--- x-pack/plugin/ml/qa/single-node-tests/src/test/java/org/elasticsearch/xpack/ml/transforms/PainlessDomainSplitIT.java
+++ x-pack/plugin/ml/qa/single-node-tests/src/test/java/org/elasticsearch/xpack/ml/transforms/PainlessDomainSplitIT.java
@@ -180,7 +180,6 @@ public class PainlessDomainSplitIT extends ESRestTestCase {
         // remove this once one of the awaitsfixes are removed!!
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/34683")
     public void testIsolated() throws Exception {
         Settings.Builder settings = Settings.builder()
                 .put(IndexMetaData.INDEX_NUMBER_OF_SHARDS_SETTING.getKey(), 1)

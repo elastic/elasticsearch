61391a478127983c9d84286c0279d5e3bee5b5cf
Alan Woodward <romseygeek@apache.org>
2018-10-24 15:33:03 +0100

    checkstyle

--- modules/analysis-common/src/main/java/org/elasticsearch/analysis/common/SynonymTokenFilterFactory.java
+++ modules/analysis-common/src/main/java/org/elasticsearch/analysis/common/SynonymTokenFilterFactory.java
@@ -35,7 +35,6 @@ import org.elasticsearch.index.analysis.TokenizerFactory;
 
 import java.io.Reader;
 import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 

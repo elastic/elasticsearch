668466f2cddc6f4118fba08839a3309e2305ee0b
Tal Levy <jubboy333@gmail.com>
2018-10-24 17:28:24 -0700

    fix PolicyStatsTests mutateInstance
    through randomization, there is a chance that the mutateInstance
    for PolicyStatsTests does not actually mutate the original object.
    This PR aims to fix this

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/indexlifecycle/PolicyStatsTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/indexlifecycle/PolicyStatsTests.java
@@ -38,10 +38,10 @@ public class PolicyStatsTests extends AbstractWireSerializingTestCase<PolicyStat
         switch (between(0, 1)) {
         case 0:
             phaseStats = new HashMap<>(instance.getPhaseStats());
-            phaseStats.put(randomAlphaOfLengthBetween(1, 20), PhaseStatsTests.createRandomInstance());
+            phaseStats.put(randomAlphaOfLengthBetween(21, 25), PhaseStatsTests.createRandomInstance());
             break;
         case 1:
-            indicesManaged = randomIntBetween(1, 50);
+            indicesManaged = randomIntBetween(11, 50);
             break;
         default:
             throw new AssertionError("Illegal randomisation branch");

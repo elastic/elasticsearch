00773cb96e1b8fc5729b6f2e34a6533f5abe3635
Nik Everett <nik9000@gmail.com>
2018-10-26 15:40:16 -0400

    Drop deprecationLogger from AbstractComponent (#34859)
    Drops the `deprecationLogger` from `AbstractComponent`, moving it to
    places where we need it. This saves us from building a bunch of
    `DeprecationLogger`s that we don't need.
    
    Relates to #34488

--- server/src/main/java/org/elasticsearch/common/component/AbstractComponent.java
+++ server/src/main/java/org/elasticsearch/common/component/AbstractComponent.java
@@ -19,20 +19,17 @@
 
 package org.elasticsearch.common.component;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.common.logging.DeprecationLogger;
+import org.apache.logging.log4j.LogManager;
 import org.elasticsearch.common.settings.Settings;
 
 public abstract class AbstractComponent {
 
     protected final Logger logger;
-    protected final DeprecationLogger deprecationLogger;
     protected final Settings settings;
 
     public AbstractComponent(Settings settings) {
         this.logger = LogManager.getLogger(getClass());
-        this.deprecationLogger = new DeprecationLogger(logger);
         this.settings = settings;
     }
 }

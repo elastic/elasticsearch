8406300daa81a911d1377cf5a7cbc504b45186eb
Tim Brooks <tim@uncontended.net>
2018-11-05 12:08:51 -0700

    Fix checkstyle

--- server/src/main/java/org/elasticsearch/transport/TcpTransport.java
+++ server/src/main/java/org/elasticsearch/transport/TcpTransport.java
@@ -95,7 +95,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;

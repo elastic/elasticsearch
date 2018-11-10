cdaf8e42b4c65e66c34ffb4e393fd308e2d8e052
Julie Tibshirani <julie.tibshirani@elastic.co>
2018-11-09 19:11:46 -0800

    Add a test around JSON fields and source filtering. (#35399)

--- server/src/test/java/org/elasticsearch/search/source/SourceFetchingIT.java
+++ server/src/test/java/org/elasticsearch/search/source/SourceFetchingIT.java
@@ -22,6 +22,10 @@ package org.elasticsearch.search.source;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.test.ESIntegTestCase;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -94,5 +98,36 @@ public class SourceFetchingIT extends ESIntegTestCase {
         assertThat(response.getHits().getAt(0).getSourceAsString(), notNullValue());
         assertThat(response.getHits().getAt(0).getSourceAsMap().size(), equalTo(1));
         assertThat((String) response.getHits().getAt(0).getSourceAsMap().get("field"), equalTo("value"));
+    }
+
+    public void testSourceFilteringWithJsonField() {
+        prepareCreate("test").addMapping("_doc", "headers", "type=json").get();
+        ensureGreen();
+
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("content-type", "application/json");
+        headers.put("origin", "https://www.elastic.co");
+        Map<String, Object> source = Collections.singletonMap("headers", headers);
+
+        client().prepareIndex("test", "_doc", "1").setSource(source).get();
+        refresh();
+
+        SearchResponse response = client().prepareSearch("test").setFetchSource(true).get();
+        assertThat(response.getHits().getAt(0).getSourceAsMap(), equalTo(source));
+
+        // Check 'include' filtering.
+        response = client().prepareSearch("test").setFetchSource("headers", null).get();
+        assertThat(response.getHits().getAt(0).getSourceAsMap(), equalTo(source));
+
+        response = client().prepareSearch("test").setFetchSource("headers.content-type", null).get();
+        Map<String, Object> filteredSource = Collections.singletonMap("headers",
+            Collections.singletonMap("content-type", "application/json"));
+        assertThat(response.getHits().getAt(0).getSourceAsMap(), equalTo(filteredSource));
+
+        // Check 'exclude' filtering.
+        response = client().prepareSearch("test").setFetchSource(null, "headers.content-type").get();
+        filteredSource = Collections.singletonMap("headers",
+            Collections.singletonMap("origin", "https://www.elastic.co"));
+        assertThat(response.getHits().getAt(0).getSourceAsMap(), equalTo(filteredSource));
     }
 }

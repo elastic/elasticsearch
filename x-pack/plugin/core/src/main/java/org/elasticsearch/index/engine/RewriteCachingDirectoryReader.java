aaeb52a38c4629979bdb1b7d0a49644051e58e27
Simon Willnauer <simonw@apache.org>
2018-11-12 20:37:00 +0100

    fix checkstyle

--- x-pack/plugin/core/src/main/java/org/elasticsearch/index/engine/RewriteCachingDirectoryReader.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/index/engine/RewriteCachingDirectoryReader.java
@@ -26,7 +26,6 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.util.Bits;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;

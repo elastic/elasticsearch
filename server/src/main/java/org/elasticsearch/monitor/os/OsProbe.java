5c61807b4a1a15def9a3337792733cc25fdce35e
Jason Tedor <jason@tedor.me>
2018-11-12 15:54:27 -0500

    Remove import

--- server/src/main/java/org/elasticsearch/monitor/os/OsProbe.java
+++ server/src/main/java/org/elasticsearch/monitor/os/OsProbe.java
@@ -33,7 +33,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;

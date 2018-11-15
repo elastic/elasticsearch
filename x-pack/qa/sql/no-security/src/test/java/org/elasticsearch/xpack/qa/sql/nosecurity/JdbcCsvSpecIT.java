8b909eebddd539af26e9d71198e6ffa8df862bb2
Andrei Stefan <andrei@elastic.co>
2018-11-15 15:21:10 +0200

    Unmuted test

--- x-pack/qa/sql/no-security/src/test/java/org/elasticsearch/xpack/qa/sql/nosecurity/JdbcCsvSpecIT.java
+++ x-pack/qa/sql/no-security/src/test/java/org/elasticsearch/xpack/qa/sql/nosecurity/JdbcCsvSpecIT.java
@@ -5,11 +5,9 @@
  */
 package org.elasticsearch.xpack.qa.sql.nosecurity;
 
-import org.apache.lucene.util.LuceneTestCase;
 import org.elasticsearch.xpack.qa.sql.jdbc.CsvSpecTestCase;
 import org.elasticsearch.xpack.qa.sql.jdbc.CsvTestUtils.CsvTestCase;
 
-@LuceneTestCase.AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/35176")
 public class JdbcCsvSpecIT extends CsvSpecTestCase {
     public JdbcCsvSpecIT(String fileName, String groupName, String testName, Integer lineNumber, CsvTestCase testCase) {
         super(fileName, groupName, testName, lineNumber, testCase);

7f8e4b170373855b9f5b64317d5fdc96e882443a
Yogesh Gaikwad <yogesh.gaikwad@elastic.co>
2018-11-19 15:23:04 +1100

    Remove unused import.

--- client/rest-high-level/src/main/java/org/elasticsearch/client/security/PutPrivilegesResponse.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/security/PutPrivilegesResponse.java
@@ -26,7 +26,6 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 import static org.elasticsearch.common.xcontent.XContentParserUtils.ensureExpectedToken;
 

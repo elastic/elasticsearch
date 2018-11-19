8265777f7609d23b69ace62b3b17a00e59e656fc
David Kyle <david.kyle@elastic.co>
2018-11-19 14:43:39 +0000

    Rolling upgrade tests for jobs and data feeds

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/action/TransportGetJobsStatsAction.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/action/TransportGetJobsStatsAction.java
@@ -39,7 +39,6 @@ import org.elasticsearch.xpack.ml.job.process.autodetect.AutodetectProcessManage
 import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;

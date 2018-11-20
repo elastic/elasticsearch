287d640999790318e921dd206900dd7a85698a0a
Mark Tozzi <mark.tozzi@gmail.com>
2018-11-20 11:20:21 -0500

    Up the population size

--- server/src/test/java/org/elasticsearch/search/aggregations/metrics/TDigestStateTransitionalSerializationTests.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/metrics/TDigestStateTransitionalSerializationTests.java
@@ -23,7 +23,7 @@ import java.util.Set;
  */
 public class TDigestStateTransitionalSerializationTests extends ESTestCase {
 
-    private static final int POPULATION_SIZE = 1000;
+    private static final int POPULATION_SIZE = 10000;
     private static final double TOLERANCE = 0.05;
 
     private List<Double> sampleData;

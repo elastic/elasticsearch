9e922c32eb42baef90b843fa1128ac77aa8f32ba
Vladimir Dolzhenko <vladimir.dolzhenko@elastic.co>
2018-11-20 16:12:44 +0100

    imports

--- server/src/test/java/org/elasticsearch/cluster/routing/PrimaryAllocationIT.java
+++ server/src/test/java/org/elasticsearch/cluster/routing/PrimaryAllocationIT.java
@@ -59,7 +59,6 @@ import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 import static org.elasticsearch.cluster.metadata.IndexMetaData.SETTING_NUMBER_OF_REPLICAS;

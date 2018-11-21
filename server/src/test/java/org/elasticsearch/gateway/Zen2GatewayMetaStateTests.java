9362b225be29bb56938d9c3d5657c2829846187c
Andrey Ershov <andrey.ershov@elastic.co>
2018-11-21 18:08:25 +0100

    Add new tests to Zen2GatewayMetaStateTests

--- server/src/test/java/org/elasticsearch/gateway/Zen2GatewayMetaStateTests.java
+++ server/src/test/java/org/elasticsearch/gateway/Zen2GatewayMetaStateTests.java
@@ -1,8 +1,29 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.elasticsearch.gateway;
 
 import org.elasticsearch.Version;
 import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.coordination.CoordinationMetaData;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.cluster.metadata.Manifest;
 import org.elasticsearch.cluster.metadata.MetaData;
 import org.elasticsearch.cluster.metadata.MetaDataIndexUpgradeService;
@@ -20,10 +41,11 @@ import java.io.IOException;
 import java.util.Collections;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 
 public class Zen2GatewayMetaStateTests extends ESTestCase {
     private class Gateway extends Zen2GatewayMetaState {
-        public Gateway(Settings settings, NodeEnvironment nodeEnvironment, TransportService transportService) throws IOException {
+        Gateway(Settings settings, NodeEnvironment nodeEnvironment, TransportService transportService) throws IOException {
             super(settings, nodeEnvironment, new MetaStateService(nodeEnvironment, xContentRegistry()),
                     Mockito.mock(MetaDataIndexUpgradeService.class), Mockito.mock(MetaDataUpgrader.class),
                     transportService);
@@ -31,7 +53,7 @@ public class Zen2GatewayMetaStateTests extends ESTestCase {
 
         @Override
         protected void upgradeMetaData(MetaDataIndexUpgradeService metaDataIndexUpgradeService, MetaDataUpgrader metaDataUpgrader) {
-            // do nothing
+            // MetaData upgrade is tested in GatewayMetaStateTests, we override this method to NOP to make mocking easier
         }
     }
 
@@ -101,13 +123,43 @@ public class Zen2GatewayMetaStateTests extends ESTestCase {
                 build();
     }
 
+    private CoordinationMetaData createCoordinationMetaData(long term) {
+        CoordinationMetaData.Builder builder = CoordinationMetaData.builder();
+        builder.term(term);
+        builder.lastAcceptedConfiguration(
+                new CoordinationMetaData.VotingConfiguration(
+                        Sets.newHashSet(generateRandomStringArray(10, 10, false))));
+        builder.lastCommittedConfiguration(
+                new CoordinationMetaData.VotingConfiguration(
+                        Sets.newHashSet(generateRandomStringArray(10, 10, false))));
+        //TODO add voting tombstones once xcontent is properly implemented for CoordinationMetaData
+
+        return builder.build();
+    }
+
+    private IndexMetaData createIndexMetaData(String indexName, int numberOfShards, long version) {
+        return IndexMetaData.builder(indexName).settings(
+                Settings.builder()
+                        .put(IndexMetaData.SETTING_INDEX_UUID, indexName)
+                        .put(IndexMetaData.SETTING_NUMBER_OF_SHARDS, numberOfShards)
+                        .put(IndexMetaData.SETTING_NUMBER_OF_REPLICAS, 0)
+                        .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
+                        .build()
+        ).version(version).build();
+    }
+
     public void testSetLastAcceptedState() throws IOException {
         Gateway gateway = newGateway();
+        final long term = randomNonNegativeLong();
 
         for (int i = 0; i < randomIntBetween(1, 5); i++) {
             final long version = randomNonNegativeLong();
+            final String indexName = randomAlphaOfLength(10);
+            final IndexMetaData indexMetaData = createIndexMetaData(indexName, randomIntBetween(1,5), randomNonNegativeLong());
             final MetaData metaData = MetaData.builder().
                     persistentSettings(Settings.builder().put(randomAlphaOfLength(10), randomAlphaOfLength(10)).build()).
+                    coordinationMetaData(createCoordinationMetaData(term)).
+                    put(indexMetaData, false).
                     build();
             ClusterState state = createClusterState(version, metaData);
 
@@ -115,14 +167,69 @@ public class Zen2GatewayMetaStateTests extends ESTestCase {
             gateway = maybeNew(gateway);
             assertThat(gateway.getLastAcceptedState().version(), equalTo(version));
             assertTrue(MetaData.isGlobalStateEquals(gateway.getLastAcceptedState().metaData(), metaData));
+            assertThat(gateway.getLastAcceptedState().metaData().index(indexName), equalTo(indexMetaData));
         }
     }
 
-    public void testCurrentTermAndTermAreDifferent() {
-        //TODO implement once CoordinationState is merged
+    public void testSetLastAcceptedStateTermChanged() throws IOException {
+        Gateway gateway = newGateway();
+
+        final String indexName = randomAlphaOfLength(10);
+        final int numberOfShards = randomIntBetween(1, 5);
+        final long version = randomNonNegativeLong();
+        final long term = randomNonNegativeLong();
+        final IndexMetaData indexMetaData = createIndexMetaData(indexName, numberOfShards, version);
+        final ClusterState state = createClusterState(randomNonNegativeLong(),
+                MetaData.builder().coordinationMetaData(createCoordinationMetaData(term)).put(indexMetaData, false).build());
+        gateway.setLastAcceptedState(state);
+
+        gateway = maybeNew(gateway);
+        final long newTerm = randomValueOtherThan(term, () -> randomNonNegativeLong());
+        final int newNumberOfShards = randomValueOtherThan(numberOfShards, () -> randomIntBetween(1,5));
+        final IndexMetaData newIndexMetaData = createIndexMetaData(indexName, newNumberOfShards, version);
+        final ClusterState newClusterState = createClusterState(randomNonNegativeLong(),
+                MetaData.builder().coordinationMetaData(createCoordinationMetaData(newTerm)).put(newIndexMetaData, false).build());
+        gateway.setLastAcceptedState(newClusterState);
+
+        gateway = maybeNew(gateway);
+        assertThat(gateway.getLastAcceptedState().metaData().index(indexName), equalTo(newIndexMetaData));
     }
 
-    public void testMarkAcceptedConfigAsCommitted() {
-        //TODO implement once CoordinationState is merged
+    public void testCurrentTermAndTermAreDifferent() throws IOException {
+        Gateway gateway = newGateway();
+
+        long currentTerm = randomNonNegativeLong();
+        long term  = randomValueOtherThan(currentTerm, () -> randomNonNegativeLong());
+
+        gateway.setCurrentTerm(currentTerm);
+        gateway.setLastAcceptedState(createClusterState(randomNonNegativeLong(),
+                MetaData.builder().coordinationMetaData(CoordinationMetaData.builder().term(term).build()).build()));
+
+        gateway = maybeNew(gateway);
+        assertThat(gateway.getCurrentTerm(), equalTo(currentTerm));
+        assertThat(gateway.getLastAcceptedState().coordinationMetaData().term(), equalTo(term));
+    }
+
+    public void testMarkAcceptedConfigAsCommitted() throws IOException {
+        Gateway gateway = newGateway();
+
+        CoordinationMetaData coordinationMetaData = createCoordinationMetaData(randomNonNegativeLong());
+        ClusterState state = createClusterState(randomNonNegativeLong(),
+                MetaData.builder().coordinationMetaData(coordinationMetaData).build());
+        gateway.setLastAcceptedState(state);
+
+        gateway = maybeNew(gateway);
+        assertThat(gateway.getLastAcceptedState().getLastAcceptedConfiguration(),
+                not(equalTo(gateway.getLastAcceptedState().getLastCommittedConfiguration())));
+        gateway.markLastAcceptedConfigAsCommitted();
+
+        gateway = maybeNew(gateway);
+        assertThat(gateway.getLastAcceptedState().getLastAcceptedConfiguration(),
+                equalTo(gateway.getLastAcceptedState().getLastCommittedConfiguration()));
+        gateway.markLastAcceptedConfigAsCommitted();
+
+        gateway = maybeNew(gateway);
+        assertThat(gateway.getLastAcceptedState().getLastAcceptedConfiguration(),
+                equalTo(gateway.getLastAcceptedState().getLastCommittedConfiguration()));
     }
 }

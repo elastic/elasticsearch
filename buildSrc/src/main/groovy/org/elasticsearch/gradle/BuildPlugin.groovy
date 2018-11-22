d7c419f7c81c1b37c11bac4afb24c6cdf41ff40e
Alpar Torok <torokalpar@gmail.com>
2018-11-22 16:59:04 +0200

    Move back to maven central
    Jcenter should be identical but it's not and has some broken artifacts
    for 5.3.1

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
@@ -301,7 +301,7 @@ class BuildPlugin implements Plugin<Project> {
             // such that we don't have to pass hardcoded files to gradle
             repos.mavenLocal()
         }
-        repos.jcenter()
+        repos.mavenCentral()
 
         String luceneVersion = VersionProperties.lucene
         if (luceneVersion.contains('-snapshot')) {

ac11f6c4dbccf517b12aa980e9a8afe69ca923cf
Dimitris Athanasiou <dimitris@elastic.co>
2018-11-22 11:28:55 +0000

    [FEATURE][ML] Reindex data frame before starting analytics
    With this commit before we start the analytics we first reindex
    the source data frame into a new index. Note we should maintain
    the settings and the mappings of the source index.

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/action/TransportRunAnalyticsAction.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/action/TransportRunAnalyticsAction.java
@@ -2,15 +2,26 @@ package org.elasticsearch.xpack.ml.action;
 
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.ActionListenerResponseHandler;
+import org.elasticsearch.action.admin.indices.create.CreateIndexAction;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.HandledTransportAction;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
+import org.elasticsearch.cluster.metadata.MappingMetaData;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.collect.ImmutableOpenMap;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.env.Environment;
+import org.elasticsearch.index.IndexNotFoundException;
+import org.elasticsearch.index.reindex.ReindexAction;
+import org.elasticsearch.index.reindex.ReindexRequest;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
@@ -21,7 +32,9 @@ import org.elasticsearch.xpack.ml.analytics.DataFrameDataExtractor;
 import org.elasticsearch.xpack.ml.analytics.DataFrameDataExtractorFactory;
 import org.elasticsearch.xpack.ml.analytics.process.AnalyticsProcessManager;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -34,6 +47,17 @@ public class TransportRunAnalyticsAction extends HandledTransportAction<RunAnaly
     private final Environment environment;
     private final AnalyticsProcessManager analyticsProcessManager;
 
+    /**
+     * Unfortunately, getting the settings of an index include internal settings that should
+     * not be set explicitly. There is no way to filter those out. Thus, we have to maintain
+     * a list of them and filter them out manually.
+     */
+    private static final List<String> INTERNAL_SETTINGS = Arrays.asList(
+        "index.creation_date",
+        "index.provided_name",
+        "index.uuid",
+        "index.version.created"
+    );
 
     @Inject
     public TransportRunAnalyticsAction(ThreadPool threadPool, TransportService transportService, ActionFilters actionFilters,
@@ -53,7 +77,7 @@ public class TransportRunAnalyticsAction extends HandledTransportAction<RunAnaly
     protected void doExecute(Task task, RunAnalyticsAction.Request request, ActionListener<AcknowledgedResponse> listener) {
         DiscoveryNode localNode = clusterService.localNode();
         if (isMlNode(localNode)) {
-            runPipelineAnalytics(request, listener);
+            reindexDataframeAndStartAnalysis(request.getIndex(), listener);
             return;
         }
 
@@ -78,8 +102,49 @@ public class TransportRunAnalyticsAction extends HandledTransportAction<RunAnaly
         return Boolean.valueOf(enabled);
     }
 
-    private void runPipelineAnalytics(RunAnalyticsAction.Request request, ActionListener<AcknowledgedResponse> listener) {
-        String jobId = "ml-analytics-" + request.getIndex();
+    private void reindexDataframeAndStartAnalysis(String index, ActionListener<AcknowledgedResponse> listener) {
+
+        final String destinationIndex = index + "_copy";
+
+        ActionListener<CreateIndexResponse> copyIndexCreatedListener = ActionListener.wrap(
+            createIndexResponse -> {
+                ReindexRequest reindexRequest = new ReindexRequest();
+                reindexRequest.setSourceIndices(index);
+                reindexRequest.setDestIndex(destinationIndex);
+                client.execute(ReindexAction.INSTANCE, reindexRequest, ActionListener.wrap(
+                    bulkResponse -> {
+                        runPipelineAnalytics(destinationIndex, listener);
+                    },
+                    listener::onFailure
+                ));
+            }, listener::onFailure
+        );
+
+        createDestinationIndex(index, destinationIndex, copyIndexCreatedListener);
+    }
+
+    private void createDestinationIndex(String sourceIndex, String destinationIndex, ActionListener<CreateIndexResponse> listener) {
+        IndexMetaData indexMetaData = clusterService.state().getMetaData().getIndices().get(sourceIndex);
+        if (indexMetaData == null) {
+            listener.onFailure(new IndexNotFoundException(sourceIndex));
+            return;
+        }
+
+        if (indexMetaData.getMappings().size() != 1) {
+            listener.onFailure(ExceptionsHelper.badRequestException("Does not support indices with multiple types"));
+            return;
+        }
+
+        Settings.Builder settingsBuilder = Settings.builder().put(indexMetaData.getSettings());
+        INTERNAL_SETTINGS.stream().forEach(settingsBuilder::remove);
+        ImmutableOpenMap<String, MappingMetaData> mappings = indexMetaData.getMappings();
+        CreateIndexRequest createIndexRequest = new CreateIndexRequest(destinationIndex, settingsBuilder.build());
+        createIndexRequest.mapping(mappings.keysIt().next(), mappings.valuesIt().next().source().string(), XContentType.JSON);
+        client.execute(CreateIndexAction.INSTANCE, createIndexRequest, listener);
+    }
+
+    private void runPipelineAnalytics(String index, ActionListener<AcknowledgedResponse> listener) {
+        String jobId = "ml-analytics-" + index;
 
         ActionListener<DataFrameDataExtractorFactory> dataExtractorFactoryListener = ActionListener.wrap(
             dataExtractorFactory -> {
@@ -90,7 +155,6 @@ public class TransportRunAnalyticsAction extends HandledTransportAction<RunAnaly
             listener::onFailure
         );
 
-        DataFrameDataExtractorFactory.create(client, Collections.emptyMap(), request.getIndex(), dataExtractorFactoryListener);
+        DataFrameDataExtractorFactory.create(client, Collections.emptyMap(), index, dataExtractorFactoryListener);
     }
-
 }

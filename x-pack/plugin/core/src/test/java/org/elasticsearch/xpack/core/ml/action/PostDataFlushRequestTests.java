faf7f942068c0cd6d0bad0054da63a8fd6d5246a
Martijn van Groningen <martijn.v.groningen@gmail.com>
2018-11-23 13:51:06 +0100

    Fixed serialization tests

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/ml/action/PostDataFlushRequestTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/ml/action/PostDataFlushRequestTests.java
@@ -5,10 +5,11 @@
  */
 package org.elasticsearch.xpack.core.ml.action;
 
-import org.elasticsearch.test.AbstractStreamableTestCase;
+import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.test.AbstractWireSerializingTestCase;
 import org.elasticsearch.xpack.core.ml.action.FlushJobAction.Request;
 
-public class PostDataFlushRequestTests extends AbstractStreamableTestCase<Request> {
+public class PostDataFlushRequestTests extends AbstractWireSerializingTestCase<Request> {
 
     @Override
     protected Request createTestInstance() {
@@ -27,8 +28,8 @@ public class PostDataFlushRequestTests extends AbstractStreamableTestCase<Reques
     }
 
     @Override
-    protected Request createBlankInstance() {
-        return new Request();
+    protected Writeable.Reader<Request> instanceReader() {
+        return Request::new;
     }
 
     public void testNullJobIdThrows() {

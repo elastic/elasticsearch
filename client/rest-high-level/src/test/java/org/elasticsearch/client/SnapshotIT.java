8fd4dc6fba87596d19a53a9feb3186b6ca3dda8c
Sachin Araballi <sachinaraballi@gmail.com>
2018-11-25 18:50:13 +0530

    Unnecessary comparisons in boolean expression
    Java Warning : 
     Avoid unnecessary comparisons in boolean expressions, they serve no purpose and impacts readability.

--- client/rest-high-level/src/test/java/org/elasticsearch/client/SnapshotIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/SnapshotIT.java
@@ -144,7 +144,7 @@ public class SnapshotIT extends ESRestHighLevelClientTestCase {
 
         CreateSnapshotResponse response = createTestSnapshot(request);
         assertEquals(waitForCompletion ? RestStatus.OK : RestStatus.ACCEPTED, response.status());
-        if (waitForCompletion == false) {
+        if (!waitForCompletion) {
             // If we don't wait for the snapshot to complete we have to cancel it to not leak the snapshot task
             AcknowledgedResponse deleteResponse = execute(
                 new DeleteSnapshotRequest(repository, snapshot),

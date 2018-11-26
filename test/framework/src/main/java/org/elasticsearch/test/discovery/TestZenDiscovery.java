980e1a3b382eec5495c8a4d3a7630be4b6bab85b
David Turner <david.turner@elastic.co>
2018-11-26 11:11:39 +0000

    Set default for use_zen2 to true

--- test/framework/src/main/java/org/elasticsearch/test/discovery/TestZenDiscovery.java
+++ test/framework/src/main/java/org/elasticsearch/test/discovery/TestZenDiscovery.java
@@ -62,7 +62,7 @@ public class TestZenDiscovery extends ZenDiscovery {
         Setting.boolSetting("discovery.zen.use_mock_pings", true, Setting.Property.NodeScope);
 
     public static final Setting<Boolean> USE_ZEN2 =
-        Setting.boolSetting("discovery.zen.use_zen2", false, Setting.Property.NodeScope);
+        Setting.boolSetting("discovery.zen.use_zen2", true, Setting.Property.NodeScope);
 
     /** A plugin which installs mock discovery and configures it to be used. */
     public static class TestPlugin extends Plugin implements DiscoveryPlugin {

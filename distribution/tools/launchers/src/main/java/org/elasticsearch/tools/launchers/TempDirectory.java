7967b75dd6613fb633f5d49dd05e70d5294a2d56
Jason Tedor <jason@tedor.me>
2018-11-28 10:48:16 -0500

    Not unique on Windows

--- distribution/tools/launchers/src/main/java/org/elasticsearch/tools/launchers/TempDirectory.java
+++ distribution/tools/launchers/src/main/java/org/elasticsearch/tools/launchers/TempDirectory.java
@@ -20,14 +20,16 @@
 package org.elasticsearch.tools.launchers;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 /**
  * Creates a temporary directory as a sub-directory of the default temporary directory. Note that this causes the created temporary
  * directory to be a private temporary directory.
  */
-final class Mktemp {
+final class TempDirectory {
 
     /**
      * The main entry point. The exit code is 0 if we successfully created a temporary directory as a sub-directory of the default
@@ -40,7 +42,17 @@ final class Mktemp {
         if (args.length != 0) {
             throw new IllegalArgumentException("expected zero arguments but was " + Arrays.toString(args));
         }
-        final Path path = Launchers.createTempDirectory("elasticsearch-");
+        /*
+         * On Windows, we avoid creating a unique temporary directory per invocation lest we pollute the temporary directory. On other
+         * operating systems, temporary directories will be cleaned automatically via various mechanisms (e.g., systemd, or restarts).
+         */
+        final Path path;
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            path = Paths.get(System.getProperty("java.io.tmpdir"), "elasticsearch");
+            Files.createDirectories(path);
+        } else {
+            path = Launchers.createTempDirectory("elasticsearch-");
+        }
         Launchers.outPrintln(path.toString());
     }
 

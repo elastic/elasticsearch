61f2ae2600bf4ef649a8b4b741488c444bc6c5f3
David Kyle <david.kyle@elastic.co>
2018-11-29 11:57:30 +0000

    Fix finalization and test

--- x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/action/TransportFinalizeJobExecutionActionTests.java
+++ x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/action/TransportFinalizeJobExecutionActionTests.java
@@ -12,6 +12,7 @@ import org.elasticsearch.action.update.UpdateAction;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.ClusterStateUpdateTask;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.metadata.MetaData;
 import org.elasticsearch.cluster.service.ClusterService;
@@ -31,8 +32,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -43,17 +45,19 @@ public class TransportFinalizeJobExecutionActionTests extends ESTestCase {
 
     private ThreadPool threadPool;
     private Client client;
+    private ClusterService clusterService;
 
     @Before
     @SuppressWarnings("unchecked")
     private void setupMocks() {
         ExecutorService executorService = mock(ExecutorService.class);
         threadPool = mock(ThreadPool.class);
-        org.elasticsearch.mock.orig.Mockito.doAnswer(invocation -> {
+        doAnswer(invocation -> {
             ((Runnable) invocation.getArguments()[0]).run();
             return null;
         }).when(executorService).execute(any(Runnable.class));
         when(threadPool.executor(MachineLearning.UTILITY_THREAD_POOL_NAME)).thenReturn(executorService);
+        when(threadPool.getThreadContext()).thenReturn(new ThreadContext(Settings.EMPTY));
 
         client = mock(Client.class);
         doAnswer( invocationOnMock -> {
@@ -61,14 +65,19 @@ public class TransportFinalizeJobExecutionActionTests extends ESTestCase {
             listener.onResponse(null);
             return null;
         }).when(client).execute(eq(UpdateAction.INSTANCE), any(), any());
-
         when(client.threadPool()).thenReturn(threadPool);
-        when(threadPool.getThreadContext()).thenReturn(new ThreadContext(Settings.EMPTY));
+
+        clusterService = mock(ClusterService.class);
+
+        doAnswer( invocationOnMock -> {
+            ClusterStateUpdateTask updateTask = (ClusterStateUpdateTask)invocationOnMock.getArguments()[1];
+            updateTask.clusterStateProcessed(null, null, null);
+            return null;
+        }).when(clusterService).submitStateUpdateTask(anyString(), any());
     }
 
     public void testOperation_noJobsInClusterState() {
-        ClusterService clusterService = mock(ClusterService.class);
-        TransportFinalizeJobExecutionAction action = createAction(clusterService);
+        TransportFinalizeJobExecutionAction action = createAction();
 
         ClusterState clusterState = ClusterState.builder(new ClusterName("finalize-job-action-tests")).build();
 
@@ -85,8 +94,7 @@ public class TransportFinalizeJobExecutionActionTests extends ESTestCase {
     }
 
     public void testOperation_jobInClusterState() {
-        ClusterService clusterService = mock(ClusterService.class);
-        TransportFinalizeJobExecutionAction action = createAction(clusterService);
+        TransportFinalizeJobExecutionAction action = createAction();
 
         MlMetadata.Builder mlBuilder = new MlMetadata.Builder();
         mlBuilder.putJob(BaseMlIntegTestCase.createFareQuoteJob("cs-job").build(new Date()), false);
@@ -103,11 +111,37 @@ public class TransportFinalizeJobExecutionActionTests extends ESTestCase {
                 e -> fail(e.getMessage())
         ));
 
+        assertTrue(ack.get().isAcknowledged());
         verify(client, never()).execute(eq(UpdateAction.INSTANCE), any(), any());
         verify(clusterService, times(1)).submitStateUpdateTask(any(), any());
     }
 
-    private TransportFinalizeJobExecutionAction createAction(ClusterService clusterService) {
+    public void testOperation_jobsInBothClusterAndIndex() {
+        TransportFinalizeJobExecutionAction action = createAction();
+
+        MlMetadata.Builder mlBuilder = new MlMetadata.Builder();
+        mlBuilder.putJob(BaseMlIntegTestCase.createFareQuoteJob("cs-job").build(new Date()), false);
+
+        ClusterState clusterState = ClusterState.builder(new ClusterName("finalize-job-action-tests"))
+                .metaData(MetaData.builder()
+                        .putCustom(MlMetadata.TYPE, mlBuilder.build()))
+                .build();
+
+        FinalizeJobExecutionAction.Request request =
+                new FinalizeJobExecutionAction.Request(new String[]{"index-job", "cs-job"});
+        AtomicReference<AcknowledgedResponse> ack = new AtomicReference<>();
+        action.masterOperation(request, clusterState, ActionListener.wrap(
+                ack::set,
+                e -> assertNull(e.getMessage())
+        ));
+
+        assertTrue(ack.get().isAcknowledged());
+        // The job in the clusterstate should not be updated in the index
+        verify(client, times(1)).execute(eq(UpdateAction.INSTANCE), any(), any());
+        verify(clusterService, times(1)).submitStateUpdateTask(any(), any());
+    }
+
+    private TransportFinalizeJobExecutionAction createAction() {
         return new TransportFinalizeJobExecutionAction(Settings.EMPTY, mock(TransportService.class), clusterService,
                 threadPool, mock(ActionFilters.class), mock(IndexNameExpressionResolver.class), client);
 

e592bd1ddcd2fcd5b5a66e541fbbc6d1f2aca22f
Alan Woodward <romseygeek@gmail.com>
2018-11-29 10:35:38 +0000

    Ensure TokenFilters only produce single tokens when parsing synonyms (#34331)
    A number of tokenfilters can produce multiple tokens at the same position.  This
    is a problem when using token chains to parse synonym files, as the SynonymMap
    requires that there are no stacked tokens in its input.
    
    This commit ensures that when used to parse synonyms, these tokenfilters either produce
    a single version of their input token, or that they throw an error when mappings are
    generated.  In indexes created in elasticsearch 6.x deprecation warnings are emitted in place
    of the error.
    
    * asciifolding and cjk_bigram produce only the folded or bigrammed token
    * decompounders, synonyms and keyword_repeat are skipped
    * n-grams, word-delimiter-filter, multiplexer, fingerprint and phonetic throw errors
    
    Fixes #34298

--- plugins/analysis-phonetic/src/main/java/org/elasticsearch/index/analysis/PhoneticTokenFilterFactory.java
+++ plugins/analysis-phonetic/src/main/java/org/elasticsearch/index/analysis/PhoneticTokenFilterFactory.java
@@ -30,11 +30,13 @@ import org.apache.commons.codec.language.bm.Languages.LanguageSet;
 import org.apache.commons.codec.language.bm.NameType;
 import org.apache.commons.codec.language.bm.PhoneticEngine;
 import org.apache.commons.codec.language.bm.RuleType;
+import org.apache.logging.log4j.LogManager;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.phonetic.BeiderMorseFilter;
 import org.apache.lucene.analysis.phonetic.DaitchMokotoffSoundexFilter;
 import org.apache.lucene.analysis.phonetic.DoubleMetaphoneFilter;
 import org.apache.lucene.analysis.phonetic.PhoneticFilter;
+import org.elasticsearch.common.logging.DeprecationLogger;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
@@ -46,6 +48,10 @@ import java.util.HashSet;
 import java.util.List;
 
 public class PhoneticTokenFilterFactory extends AbstractTokenFilterFactory {
+
+
+    private static final DeprecationLogger DEPRECATION_LOGGER
+        = new DeprecationLogger(LogManager.getLogger(PhoneticTokenFilterFactory.class));
 
     private final Encoder encoder;
     private final boolean replace;
@@ -138,5 +144,12 @@ public class PhoneticTokenFilterFactory extends AbstractTokenFilterFactory {
             return new PhoneticFilter(tokenStream, encoder, !replace);
         }
         throw new IllegalArgumentException("encoder error");
+    }
+
+    @Override
+    public TokenFilterFactory getSynonymFilter() {
+        DEPRECATION_LOGGER.deprecatedAndMaybeLog("synonym_tokenfilters", "Token filter [" + name()
+            + "] will not be usable to parse synonyms after v7.0");
+        return this;
     }
 }

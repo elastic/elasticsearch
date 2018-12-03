3f2e9051d20bd758cfe8f057abf4995e9e1fa4ba
Tim Brooks <tim@uncontended.net>
2018-12-02 18:38:41 -0700

    WIP

--- test/framework/src/main/java/org/elasticsearch/transport/AbstractSimpleTransportTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/transport/AbstractSimpleTransportTestCase.java
@@ -2521,7 +2521,7 @@ public abstract class AbstractSimpleTransportTestCase extends ESTestCase {
             String failedMessage = "Unexpected read bytes size. The transport exception that was received=" + exception;
             // 49 bytes are the non-exception message bytes that have been received. It should include the initial
             // handshake message and the header, version, etc bytes in the exception message.
-            assertEquals(failedMessage, 53 + streamOutput.bytes().length(), stats.getRxSize().getBytes());
+            assertEquals(failedMessage, 49 + streamOutput.bytes().length(), stats.getRxSize().getBytes());
             assertEquals(109, stats.getTxSize().getBytes());
         } finally {
             serviceC.close();

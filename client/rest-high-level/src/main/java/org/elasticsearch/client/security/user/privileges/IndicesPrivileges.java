730f83537ddc405eca4e245f2c0f24677c612f8e
Albert Zaharovits <albert.zaharovits@elastic.co>
2018-12-04 12:59:25 +0200

    Unsaved trimming

--- client/rest-high-level/src/main/java/org/elasticsearch/client/security/user/privileges/IndicesPrivileges.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/security/user/privileges/IndicesPrivileges.java
@@ -58,7 +58,7 @@ public final class IndicesPrivileges implements ToXContentObject {
 
     @SuppressWarnings("unchecked")
     static final ConstructingObjectParser<IndicesPrivileges, Void> PARSER =
-        new ConstructingObjectParser<>("indices_privileges", true, constructorObjects -> {
+        new ConstructingObjectParser<>("indices_privileges", false, constructorObjects -> {
                 int i = 0;
                 final Collection<String> indices = (Collection<String>) constructorObjects[i++];
                 final Collection<String> privileges = (Collection<String>) constructorObjects[i++];
@@ -73,7 +73,7 @@ public final class IndicesPrivileges implements ToXContentObject {
     static {
         @SuppressWarnings("unchecked")
         final ConstructingObjectParser<Tuple<Collection<String>, Collection<String>>, Void> fls_parser =
-                new ConstructingObjectParser<>( "field_level_parser", true, constructorObjects -> {
+                new ConstructingObjectParser<>( "field_level_parser", false, constructorObjects -> {
                         int i = 0;
                         final Collection<String> grantFields = (Collection<String>) constructorObjects[i++];
                         final Collection<String> exceptFields = (Collection<String>) constructorObjects[i];

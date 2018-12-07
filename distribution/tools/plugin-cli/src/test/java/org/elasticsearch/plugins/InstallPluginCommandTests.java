e6ac83da3098cb3c1e845529176c91172809a99c
Alpar Torok <torokalpar@gmail.com>
2018-12-07 14:38:06 +0200

    Don't print download progress in batch mode
    With this change we will no longer provide the progress bar in batch
    mode.
    Assuming that this is mode is mainly for consumption by tools which
    will serialize the output, we shouldn't print a progress bar to be
    for every percentile.

--- distribution/tools/plugin-cli/src/test/java/org/elasticsearch/plugins/InstallPluginCommandTests.java
+++ distribution/tools/plugin-cli/src/test/java/org/elasticsearch/plugins/InstallPluginCommandTests.java
@@ -852,7 +852,7 @@ public class InstallPluginCommandTests extends ESTestCase {
         Path pluginZip = createPlugin(name, pluginDir);
         InstallPluginCommand command = new InstallPluginCommand() {
             @Override
-            Path downloadZip(Terminal terminal, String urlString, Path tmpDir) throws IOException {
+            Path downloadZip(Terminal terminal, String urlString, Path tmpDir, boolean isBatch) throws IOException {
                 assertEquals(url, urlString);
                 Path downloadedPath = tmpDir.resolve("downloaded.zip");
                 Files.copy(pluginZip, downloadedPath);

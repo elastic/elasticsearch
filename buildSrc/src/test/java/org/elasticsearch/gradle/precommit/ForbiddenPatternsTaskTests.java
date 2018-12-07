c42b799071fccf766acce4a672e641b84f932cf8
Christian Schneider <cdschneider@wisc.edu>
2018-12-06 21:51:19 -0600

    unused TemporaryFolder

--- buildSrc/src/test/java/org/elasticsearch/gradle/precommit/ForbiddenPatternsTaskTests.java
+++ buildSrc/src/test/java/org/elasticsearch/gradle/precommit/ForbiddenPatternsTaskTests.java
@@ -5,8 +5,6 @@ import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.testfixtures.ProjectBuilder;
-import org.junit.Rule;
-import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,8 +16,6 @@ import java.util.Map;
 import java.util.Optional;
 
 public class ForbiddenPatternsTaskTests extends GradleUnitTestCase {
-    @Rule
-    public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
     public void testCheckInvalidPatternsWhenNoSourceFilesExist() throws Exception {
         Project project = createProject();

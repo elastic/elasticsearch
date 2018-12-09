32c4f99238112fa4b381316c1e73d457956ef040
Yogesh Gaikwad <902768+bizybot@users.noreply.github.com>
2018-12-09 16:03:28 +1100

    [HLRC] Add support for put privileges API (#35679)
    This commit adds support for API to create or update
    application privileges in high-level rest client.

--- client/rest-high-level/src/test/java/org/elasticsearch/client/security/user/privileges/ApplicationPrivilegeTests.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/security/user/privileges/ApplicationPrivilegeTests.java
@@ -19,8 +19,12 @@
 
 package org.elasticsearch.client.security.user.privileges;
 
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.xcontent.DeprecationHandler;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
+import org.elasticsearch.common.xcontent.ToXContent;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.test.ESTestCase;
 
@@ -36,19 +40,19 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class ApplicationPrivilegeTests extends ESTestCase {
 
-    public void testFromXContent() throws IOException {
+    public void testFromXContentAndToXContent() throws IOException {
         String json =
-            "    {" +
-                "      \"application\": \"myapp\"," +
-                "      \"name\": \"read\"," +
-                "      \"actions\": [" +
-                "        \"data:read/*\"," +
-                "        \"action:login\"" +
-                "      ],\n" +
-                "      \"metadata\": {" +
-                "        \"description\": \"Read access to myapp\"" +
-                "      }" +
-                "    }";
+                "{\n"
+                + "  \"application\" : \"myapp\",\n"
+                + "  \"name\" : \"read\",\n"
+                + "  \"actions\" : [\n"
+                + "    \"data:read/*\",\n"
+                + "    \"action:login\"\n"
+                + "  ],\n"
+                + "  \"metadata\" : {\n"
+                + "    \"description\" : \"Read access to myapp\"\n"
+                + "  }\n"
+                + "}";
         final ApplicationPrivilege privilege = ApplicationPrivilege.fromXContent(XContentType.JSON.xContent().createParser(
             new NamedXContentRegistry(Collections.emptyList()), new DeprecationHandler() {
                 @Override
@@ -64,6 +68,10 @@ public class ApplicationPrivilegeTests extends ESTestCase {
         final ApplicationPrivilege expectedPrivilege =
             new ApplicationPrivilege("myapp", "read", Arrays.asList("data:read/*", "action:login"), metadata);
         assertThat(privilege, equalTo(expectedPrivilege));
+
+        XContentBuilder builder = privilege.toXContent(XContentFactory.jsonBuilder().prettyPrint(), ToXContent.EMPTY_PARAMS);
+        String toJson = Strings.toString(builder);
+        assertThat(toJson, equalTo(json));
     }
 
     public void testEmptyApplicationName() {

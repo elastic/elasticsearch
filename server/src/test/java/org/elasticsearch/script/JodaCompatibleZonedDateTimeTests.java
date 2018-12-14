971e328edffca3e4a964ed3b549756b1d789516b
Michael Basnight <mbasnight@gmail.com>
2018-12-14 13:43:22 -0600

    nit on spacing

--- server/src/test/java/org/elasticsearch/script/JodaCompatibleZonedDateTimeTests.java
+++ server/src/test/java/org/elasticsearch/script/JodaCompatibleZonedDateTimeTests.java
@@ -261,14 +261,14 @@ public class JodaCompatibleZonedDateTimeTests extends ESTestCase {
     }
 
     public void testIsAfter() {
-        long millis = randomLongBetween(0, Integer.MAX_VALUE /2);
+        long millis = randomLongBetween(0, Integer.MAX_VALUE / 2);
         JodaCompatibleZonedDateTime beforeTime = new JodaCompatibleZonedDateTime(Instant.ofEpochMilli(millis), ZoneOffset.ofHours(-7));
         millis = randomLongBetween(millis + 1, Integer.MAX_VALUE);
         JodaCompatibleZonedDateTime afterTime = new JodaCompatibleZonedDateTime(Instant.ofEpochMilli(millis), ZoneOffset.ofHours(-7));
         assertTrue(afterTime.isAfter(beforeTime));
     }
     public void testIsBefore() {
-        long millis = randomLongBetween(0, Integer.MAX_VALUE /2);
+        long millis = randomLongBetween(0, Integer.MAX_VALUE / 2);
         JodaCompatibleZonedDateTime beforeTime = new JodaCompatibleZonedDateTime(Instant.ofEpochMilli(millis), ZoneOffset.ofHours(-7));
         millis = randomLongBetween(millis + 1, Integer.MAX_VALUE);
         JodaCompatibleZonedDateTime afterTime = new JodaCompatibleZonedDateTime(Instant.ofEpochMilli(millis), ZoneOffset.ofHours(-7));

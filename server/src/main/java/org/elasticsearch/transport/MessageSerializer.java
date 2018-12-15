0d2fd14e18d0f20a556270a198bf8acbad31cbb9
Tim Brooks <tim@uncontended.net>
2018-12-14 22:31:28 -0700

    WIP

--- server/src/main/java/org/elasticsearch/transport/MessageSerializer.java
+++ server/src/main/java/org/elasticsearch/transport/MessageSerializer.java
@@ -26,10 +26,7 @@ import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.io.stream.ReleasableBytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
-import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.util.BigArrays;
-import org.elasticsearch.common.util.concurrent.ThreadContext;
-import org.elasticsearch.core.internal.io.IOUtils;
 import org.elasticsearch.threadpool.ThreadPool;
 
 import java.io.IOException;
@@ -37,104 +34,115 @@ import java.util.Set;
 
 public class MessageSerializer {
 
-    private final ThreadPool threadPool = null;
-    private final String[] features = new String[0];
-    private final Writeable message = null;
-    private final Version version = Version.CURRENT;
-    private final String action = null;
-    private final long requestId = -1;
-    private final boolean compressMessage = false;
-    private byte status = 0;
+    private static abstract class Message implements Writeable {
 
+        private final BigArrays bigArrays = BigArrays.NON_RECYCLING_INSTANCE;
 
-    public void request() throws IOException {
-        CompressibleBytesOutputStream stream = preamble();
-        if (version.onOrAfter(Version.V_6_3_0)) {
-            stream.writeStringArray(features);
-        }
-        stream.writeString(action);
+        protected final ThreadPool threadPool;
+        protected final Version version;
+        protected final long requestId;
+        protected final Writeable message;
+        protected final boolean compressMessage;
+        protected byte status = 0;
 
-        BytesReference postamble = postamble(stream);
-    }
-
-    public void response() throws IOException {
-        final boolean compress = compressMessage && canCompress(message);
-        if (compress) {
-            status = TransportStatus.setCompress(status);
+        protected Message(ThreadPool threadPool, Version version, long requestId, Writeable message, boolean compressMessage) {
+            this.threadPool = threadPool;
+            this.version = version;
+            this.requestId = requestId;
+            this.message = message;
+            this.compressMessage = compressMessage;
         }
 
-        ReleasableBytesStreamOutput bStream = new ReleasableBytesStreamOutput(bigArrays);
-        CompressibleBytesOutputStream stream = new CompressibleBytesOutputStream(bStream, compress);
+        CompressibleBytesOutputStream initStream() throws IOException {
+            final boolean compress = compressMessage && canCompress(message);
+            if (compress) {
+                status = TransportStatus.setCompress(status);
+            }
 
-        stream.setVersion(version);
-        threadPool.getThreadContext().writeTo(stream);
+            ReleasableBytesStreamOutput bStream = new ReleasableBytesStreamOutput(bigArrays);
+            final CompressibleBytesOutputStream stream = new CompressibleBytesOutputStream(bStream, compress);
 
+            stream.setVersion(version);
+            threadPool.getThreadContext().writeTo(stream);
 
-        Set<String> features = null;
-        stream.setFeatures(features);
-
-        BytesReference postamble = postamble(stream);
-    }
-
-    private final BigArrays bigArrays = BigArrays.NON_RECYCLING_INSTANCE;
-
-    private CompressibleBytesOutputStream preamble() throws IOException {
-        // only compress if asked and the request is not bytes. Otherwise only
-        // the header part is compressed, and the "body" can't be extracted as compressed
-        final boolean compress = compressMessage && canCompress(message);
-        if (compress) {
-            status = TransportStatus.setCompress(status);
+            return stream;
         }
 
-        ReleasableBytesStreamOutput bStream = new ReleasableBytesStreamOutput(bigArrays);
-        final CompressibleBytesOutputStream stream = new CompressibleBytesOutputStream(bStream, compress);
-
-        stream.setVersion(version);
-        threadPool.getThreadContext().writeTo(stream);
-
-        return stream;
-    }
-
-    private BytesReference postamble(CompressibleBytesOutputStream stream) throws IOException {
-        final BytesReference zeroCopyBuffer;
-        if (message instanceof BytesTransportRequest) { // what a shitty optimization - we should use a direct send method instead
-            BytesTransportRequest bRequest = (BytesTransportRequest) message;
-            bRequest.writeThin(stream);
-            zeroCopyBuffer = bRequest.bytes;
-        } else if (message instanceof RemoteTransportException) {
-            stream.writeException((RemoteTransportException) message);
-            zeroCopyBuffer = BytesArray.EMPTY;
-        } else {
-            message.writeTo(stream);
-            zeroCopyBuffer = BytesArray.EMPTY;
+        BytesReference finish(CompressibleBytesOutputStream stream) throws IOException {
+            final BytesReference zeroCopyBuffer;
+            if (message instanceof BytesTransportRequest) { // what a shitty optimization - we should use a direct send method instead
+                BytesTransportRequest bRequest = (BytesTransportRequest) message;
+                bRequest.writeThin(stream);
+                zeroCopyBuffer = bRequest.bytes;
+            } else if (message instanceof RemoteTransportException) {
+                stream.writeException((RemoteTransportException) message);
+                zeroCopyBuffer = BytesArray.EMPTY;
+            } else {
+                message.writeTo(stream);
+                zeroCopyBuffer = BytesArray.EMPTY;
+            }
+            // we have to call materializeBytes() here before accessing the bytes. A CompressibleBytesOutputStream
+            // might be implementing compression. And materializeBytes() ensures that some marker bytes (EOS marker)
+            // are written. Otherwise we barf on the decompressing end when we read past EOF on purpose in the
+            // #validateRequest method. this might be a problem in deflate after all but it's important to write
+            // the marker bytes.
+            final BytesReference messageBody = stream.materializeBytes();
+            final BytesReference header = buildHeader(requestId, status, stream.getVersion(), messageBody.length() + zeroCopyBuffer.length());
+            return new CompositeBytesReference(header, messageBody, zeroCopyBuffer);
         }
-        // we have to call materializeBytes() here before accessing the bytes. A CompressibleBytesOutputStream
-        // might be implementing compression. And materializeBytes() ensures that some marker bytes (EOS marker)
-        // are written. Otherwise we barf on the decompressing end when we read past EOF on purpose in the
-        // #validateRequest method. this might be a problem in deflate after all but it's important to write
-        // the marker bytes.
-        final BytesReference messageBody = stream.materializeBytes();
-        final BytesReference header = buildHeader(requestId, status, stream.getVersion(), messageBody.length() + zeroCopyBuffer.length());
-        return new CompositeBytesReference(header, messageBody, zeroCopyBuffer);
+
+        private BytesReference buildHeader(long requestId, byte status, Version protocolVersion, int length) throws IOException {
+            try (BytesStreamOutput headerOutput = new BytesStreamOutput(TcpHeader.HEADER_SIZE)) {
+                headerOutput.setVersion(protocolVersion);
+                TcpHeader.writeHeader(headerOutput, requestId, status, protocolVersion, length);
+                final BytesReference bytes = headerOutput.bytes();
+                assert bytes.length() == TcpHeader.HEADER_SIZE : "header size mismatch expected: " + TcpHeader.HEADER_SIZE + " but was: "
+                    + bytes.length();
+                return bytes;
+            }
+        }
     }
 
-    /**
-     * Writes the Tcp message header into a bytes reference.
-     *
-     * @param requestId       the request ID
-     * @param status          the request status
-     * @param protocolVersion the protocol version used to serialize the data in the message
-     * @param length          the payload length in bytes
-     * @see TcpHeader
-     */
-    private BytesReference buildHeader(long requestId, byte status, Version protocolVersion, int length) throws IOException {
-        try (BytesStreamOutput headerOutput = new BytesStreamOutput(TcpHeader.HEADER_SIZE)) {
-            headerOutput.setVersion(protocolVersion);
-            TcpHeader.writeHeader(headerOutput, requestId, status, protocolVersion, length);
-            final BytesReference bytes = headerOutput.bytes();
-            assert bytes.length() == TcpHeader.HEADER_SIZE : "header size mismatch expected: " + TcpHeader.HEADER_SIZE + " but was: "
-                + bytes.length();
-            return bytes;
+    static class Request extends Message {
+
+        private final String[] features;
+        private final String action;
+
+
+        Request(ThreadPool threadPool, String[] features, Writeable message, Version version, String action, long requestId,
+                boolean compressMessage) {
+            super(threadPool, version, requestId, message, compressMessage);
+            this.features = features;
+            this.action = action;
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) throws IOException {
+            CompressibleBytesOutputStream stream = initStream();
+            if (version.onOrAfter(Version.V_6_3_0)) {
+                stream.writeStringArray(features);
+            }
+            stream.writeString(action);
+
+            BytesReference postamble = finish(stream);
+        }
+    }
+
+    static class Response extends Message {
+
+        private final Set<String> features;
+
+        Response(ThreadPool threadPool, Set<String> features, Writeable message, Version version, String action, long requestId,
+                 boolean compressMessage) {
+            super(threadPool, version, requestId, message, compressMessage);
+            this.features = features;
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) throws IOException {
+            CompressibleBytesOutputStream stream = initStream();
+            stream.setFeatures(features);
+            BytesReference postamble = finish(stream);
         }
     }
 

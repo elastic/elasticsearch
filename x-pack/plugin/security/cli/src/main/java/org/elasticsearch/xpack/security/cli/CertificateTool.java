de70a403c2f8f6621cefecf171a5e866eb4dadcd
MiguelFerreira1998 <33258304+MiguelFerreira1998@users.noreply.github.com>
2018-12-17 03:27:23 +0000

    Removed check OpenSSL version
    (warning for all versions)

--- x-pack/plugin/security/cli/src/main/java/org/elasticsearch/xpack/security/cli/CertificateTool.java
+++ x-pack/plugin/security/cli/src/main/java/org/elasticsearch/xpack/security/cli/CertificateTool.java
@@ -46,9 +46,6 @@ import javax.security.auth.x500.X500Principal;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.InputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.nio.CharBuffer;
@@ -93,7 +90,6 @@ public class CertificateTool extends LoggingAwareMultiCommand {
     private static final String DEFAULT_CERT_ZIP = "certificate-bundle.zip";
     private static final String DEFAULT_CA_ZIP = "elastic-stack-ca.zip";
     private static final String DEFAULT_CA_P12 = "elastic-stack-ca.p12";
-    private static final String OLDER_SSL_VERSION_PASSWORD_SIZE = "1.1.0";
     private static final BouncyCastleProvider BC_PROV = new BouncyCastleProvider();
     private static final Integer MAX_SIZE_PW_BUFFER_OLDER_SSL = 50;
 
@@ -341,7 +337,7 @@ public class CertificateTool extends LoggingAwareMultiCommand {
         private CAInfo loadPkcs12CA(Terminal terminal, OptionSet options, Environment env) throws Exception {
             Path path = resolvePath(options, caPkcs12PathSpec);
             char[] passwordOption = getChars(caPasswordSpec.value(options));
-            Map<Certificate, Key> keys = withPassword("CA (" + path + ")", false, passwordOption,
+            Map<Certificate, Key> keys = withPassword("CA (" + path + ")", passwordOption,
                 terminal, password -> CertParsingUtils.readPkcs12KeyPairs(path, password, a -> password));
 
             if (keys.size() != 1) {
@@ -385,7 +381,7 @@ public class CertificateTool extends LoggingAwareMultiCommand {
                 return new CAInfo(caCert, keyPair.getPrivate(), true, password);
             }
             if (options.has(caPasswordSpec)) {
-                return withPassword("CA Private key", false, null, terminal, p -> new CAInfo(caCert, keyPair.getPrivate(), true, p.clone()));
+                return withPassword("CA Private key", null, terminal, p -> new CAInfo(caCert, keyPair.getPrivate(), true, p.clone()));
             }
             return new CAInfo(caCert, keyPair.getPrivate(), true, null);
         }
@@ -532,7 +528,7 @@ public class CertificateTool extends LoggingAwareMultiCommand {
             final String fileName = dirName + "ca.p12";
             outputStream.putNextEntry(new ZipEntry(fileName));
 
-            withPassword("Generated CA", false, info.password, terminal, caPassword -> {
+            withPassword("Generated CA", info.password, terminal, caPassword -> {
                 writePkcs12(fileName, outputStream, "ca", info.certAndKey, null, caPassword, null);
                 return null;
             });
@@ -552,18 +548,8 @@ public class CertificateTool extends LoggingAwareMultiCommand {
             final KeyStore pkcs12 = KeyStore.getInstance("PKCS12");
             pkcs12.load(null);
 
-        boolean olderVersion = false;
-        String command = "openssl version -a";
-        Process proc = Runtime.getRuntime().exec(command);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-        String line = "";
-        line = reader.readLine();
-        line = line.substring(8,13);
 
-        if(line.compareTo(OLDER_SSL_VERSION_PASSWORD_SIZE) < 0)
-            olderVersion = true;
-
-                withPassword(fileName,olderVersion, password, terminal, p12Password -> {
+                withPassword(fileName, password, terminal, p12Password -> {
                     if (isAscii(p12Password)) {
                         pkcs12.setKeyEntry(alias, pair.key, p12Password, new Certificate[]{pair.cert});
                         if (caCert != null) {
@@ -812,7 +798,7 @@ public class CertificateTool extends LoggingAwareMultiCommand {
                             outputStream.putNextEntry(new ZipEntry(keyFileName));
                             if (usePassword) {
 
-                                withPassword(keyFileName, false, outputPassword, terminal, password -> {
+                                withPassword(keyFileName, outputPassword, terminal, password -> {
                                     pemWriter.writeObject(pair.key, getEncrypter(password));
                                     return null;
                                 });
@@ -941,10 +927,11 @@ public class CertificateTool extends LoggingAwareMultiCommand {
         return new JcePEMEncryptorBuilder("DES-EDE3-CBC").setProvider(BC_PROV).build(password);
     }
 
-    private static <T, E extends Exception> T withPassword(String description, boolean long_size, char[] password, Terminal terminal,
+    private static <T, E extends Exception> T withPassword(String description, char[] password, Terminal terminal,
                                                            CheckedFunction<char[], T, E> body) throws E {
         if (password == null) {
             char[] promptedValue = terminal.readSecret("Enter password for " + description + " : ");
+            boolean long_size = true;
             while(long_size){
                 if(promptedValue.length > MAX_SIZE_PW_BUFFER_OLDER_SSL){
                     if(terminal.promptYesNo("Your password exceeds a length of 50 characters. Versions of OpenSSL older than 1.1.0 will not be able to use this certificate. Do you want to continue?", true))

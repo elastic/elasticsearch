d861476aea857b51e47cb09cb187ae6f9c7e4b89
Ioannis Kakavas <ikakavas@protonmail.com>
2018-12-18 10:05:50 +0200

    Enhance Invalidate Token API (#35388)
    This change:
    
    - Adds functionality to invalidate all (refresh+access) tokens for all users of a realm
    - Adds functionality to invalidate all (refresh+access)tokens for a user in all realms
    - Adds functionality to invalidate all (refresh+access) tokens for a user in a specific realm
    - Changes the response format for the invalidate token API to contain information about the
       number of the invalidated tokens and possible errors that were encountered.
    - Updates the API Documentation
    
    After back-porting to 6.x, the `created` field will be removed from master as a field in the
    response
    
    Resolves: #35115
    Relates: #34556

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/token/InvalidateTokenResponse.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/token/InvalidateTokenResponse.java
@@ -5,41 +5,83 @@
  */
 package org.elasticsearch.xpack.core.security.action.token;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.xcontent.ToXContent;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.xpack.core.security.authc.support.TokensInvalidationResult;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Objects;
 
 /**
- * Response for a invalidation of a token.
+ * Response for a invalidation of one or multiple tokens.
  */
-public final class InvalidateTokenResponse extends ActionResponse {
+public final class InvalidateTokenResponse extends ActionResponse implements ToXContent {
 
-    private boolean created;
+    private TokensInvalidationResult result;
 
     public InvalidateTokenResponse() {}
 
-    public InvalidateTokenResponse(boolean created) {
-        this.created = created;
+    public InvalidateTokenResponse(TokensInvalidationResult result) {
+        this.result = result;
     }
 
-    /**
-     * If the token is already invalidated then created will be <code>false</code>
-     */
-    public boolean isCreated() {
-        return created;
+    public TokensInvalidationResult getResult() {
+        return result;
+    }
+
+    private boolean isCreated() {
+        return result.getInvalidatedTokens().size() > 0
+            && result.getPreviouslyInvalidatedTokens().isEmpty()
+            && result.getErrors().isEmpty();
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         super.writeTo(out);
-        out.writeBoolean(created);
+        if (out.getVersion().before(Version.V_7_0_0)) {
+            out.writeBoolean(isCreated());
+        } else {
+            result.writeTo(out);
+        }
     }
 
     @Override
     public void readFrom(StreamInput in) throws IOException {
         super.readFrom(in);
-        created = in.readBoolean();
+        if (in.getVersion().before(Version.V_7_0_0)) {
+            final boolean created = in.readBoolean();
+            if (created) {
+                result = new TokensInvalidationResult(Arrays.asList(""), Collections.emptyList(), Collections.emptyList(), 0);
+            } else {
+                result = new TokensInvalidationResult(Collections.emptyList(), Arrays.asList(""), Collections.emptyList(), 0);
+            }
+        } else {
+            result = new TokensInvalidationResult(in);
+        }
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, ToXContent.Params params) throws IOException {
+        result.toXContent(builder, params);
+        return builder;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvalidateTokenResponse that = (InvalidateTokenResponse) o;
+        return Objects.equals(result, that.result);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(result);
     }
 }

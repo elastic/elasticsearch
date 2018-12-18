2724e1ebfcb4d8a7e713f403bdbb526a76d1c2a0
Nicholas Knize <nknize@gmail.com>
2018-12-17 23:21:05 -0600

    fix AbstractBuilderTestCase to assert geo warnings only if legacy geoshape field mapper is used

--- test/framework/src/main/java/org/elasticsearch/test/AbstractBuilderTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/AbstractBuilderTestCase.java
@@ -254,7 +254,7 @@ public abstract class AbstractBuilderTestCase extends ESTestCase {
                         AbstractBuilderTestCase.this);
                 return null;
             });
-            if (enableWarningsCheck() == true && serviceHolder.idxSettings.getIndexVersionCreated().before(Version.V_6_6_0)) {
+            if (enableWarningsCheck() == true && currentTypes.length != 0) {
                 assertDeprecatedGeoWarnings();
             }
         }

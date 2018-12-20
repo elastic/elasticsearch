openeuler-mirror/poissonsearch-oss

Initialize startup `CcrRepositories` (#36730)
Currently, the CcrRepositoryManger only listens for settings updates
and installs new repositories. It does not install the repositories that
are in the initial settings. This commit, modifies the manager to
install the initial repositories. Additionally, it modifies the ccr
integration test to configure the remote leader node at startup, instead
of using a settings update.

x-pack/plugin/ccr/src/main/java/org/elasticsearch/xpack/ccr/CcrRepositoryManager.java
@@ -10,6 +10,7 @@ import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.component.AbstractLifecycleComponent;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.transport.RemoteClusterAware;
 import org.elasticsearch.xpack.ccr.action.repositories.DeleteInternalCcrRepositoryAction;
@@ -18,31 +19,70 @@ import org.elasticsearch.xpack.ccr.action.repositories.PutInternalCcrRepositoryA
 import org.elasticsearch.xpack.ccr.action.repositories.PutInternalCcrRepositoryRequest;
 import org.elasticsearch.xpack.ccr.repository.CcrRepository;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
-class CcrRepositoryManager extends RemoteClusterAware {
+class CcrRepositoryManager extends AbstractLifecycleComponent {
 
     private final Client client;
+    private final RemoteSettingsUpdateListener updateListener;
 
     CcrRepositoryManager(Settings settings, ClusterService clusterService, Client client) {
         super(settings);
         this.client = client;
-        listenForUpdates(clusterService.getClusterSettings());
+        updateListener = new RemoteSettingsUpdateListener(settings);
+        updateListener.listenForUpdates(clusterService.getClusterSettings());
     }
 
     @Override
-    protected void updateRemoteCluster(String clusterAlias, List<String> addresses, String proxyAddress) {
-        String repositoryName = CcrRepository.NAME_PREFIX + clusterAlias;
-        if (addresses.isEmpty()) {
-            DeleteInternalCcrRepositoryRequest request = new DeleteInternalCcrRepositoryRequest(repositoryName);
-            PlainActionFuture<DeleteInternalCcrRepositoryAction.DeleteInternalCcrRepositoryResponse> f = PlainActionFuture.newFuture();
-            client.execute(DeleteInternalCcrRepositoryAction.INSTANCE, request, f);
-            assert f.isDone() : "Should be completed as it is executed synchronously";
-        } else {
-            ActionRequest request = new PutInternalCcrRepositoryRequest(repositoryName, CcrRepository.TYPE);
-            PlainActionFuture<PutInternalCcrRepositoryAction.PutInternalCcrRepositoryResponse> f = PlainActionFuture.newFuture();
-            client.execute(PutInternalCcrRepositoryAction.INSTANCE, request, f);
-            assert f.isDone() : "Should be completed as it is executed synchronously";
+    protected void doStart() {
+        updateListener.init();
+    }
+
+    @Override
+    protected void doStop() {
+    }
+
+    @Override
+    protected void doClose() throws IOException {
+    }
+
+    private void putRepository(String repositoryName) {
+        ActionRequest request = new PutInternalCcrRepositoryRequest(repositoryName, CcrRepository.TYPE);
+        PlainActionFuture<PutInternalCcrRepositoryAction.PutInternalCcrRepositoryResponse> f = PlainActionFuture.newFuture();
+        client.execute(PutInternalCcrRepositoryAction.INSTANCE, request, f);
+        assert f.isDone() : "Should be completed as it is executed synchronously";
+    }
+
+    private void deleteRepository(String repositoryName) {
+        DeleteInternalCcrRepositoryRequest request = new DeleteInternalCcrRepositoryRequest(repositoryName);
+        PlainActionFuture<DeleteInternalCcrRepositoryAction.DeleteInternalCcrRepositoryResponse> f = PlainActionFuture.newFuture();
+        client.execute(DeleteInternalCcrRepositoryAction.INSTANCE, request, f);
+        assert f.isDone() : "Should be completed as it is executed synchronously";
+    }
+
+    private class RemoteSettingsUpdateListener extends RemoteClusterAware {
+
+        private RemoteSettingsUpdateListener(Settings settings) {
+            super(settings);
+        }
+
+        void init() {
+            Set<String> clusterAliases = buildRemoteClustersDynamicConfig(settings).keySet();
+            for (String clusterAlias : clusterAliases) {
+                putRepository(CcrRepository.NAME_PREFIX + clusterAlias);
+            }
+        }
+
+        @Override
+        protected void updateRemoteCluster(String clusterAlias, List<String> addresses, String proxy) {
+            String repositoryName = CcrRepository.NAME_PREFIX + clusterAlias;
+            if (addresses.isEmpty()) {
+                deleteRepository(repositoryName);
+            } else {
+                putRepository(repositoryName);
+            }
         }
     }
 }

elastic/elasticsearch

x-pack/plugin/ccr/src/main/java/org/elasticsearch/xpack/ccr/CcrRepositoryManager.java
@@ -9,6 +9,7 @@ package org.elasticsearch.xpack.ccr;
 import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.component.AbstractLifecycleComponent;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.transport.RemoteClusterAware;
 import org.elasticsearch.xpack.ccr.action.repositories.DeleteInternalCcrRepositoryAction;
@@ -17,31 +18,70 @@ import org.elasticsearch.xpack.ccr.action.repositories.PutInternalCcrRepositoryA
 import org.elasticsearch.xpack.ccr.action.repositories.PutInternalCcrRepositoryRequest;
 import org.elasticsearch.xpack.ccr.repository.CcrRepository;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
-class CcrRepositoryManager extends RemoteClusterAware {
+class CcrRepositoryManager extends AbstractLifecycleComponent {
 
     private final NodeClient client;
+    private final RemoteSettingsUpdateListener updateListener;
 
     CcrRepositoryManager(Settings settings, ClusterService clusterService, NodeClient client) {
         super(settings);
         this.client = client;
-        listenForUpdates(clusterService.getClusterSettings());
+        updateListener = new RemoteSettingsUpdateListener(settings);
+        updateListener.listenForUpdates(clusterService.getClusterSettings());
     }
 
     @Override
-    protected void updateRemoteCluster(String clusterAlias, List<String> addresses, String proxyAddress) {
-        String repositoryName = CcrRepository.NAME_PREFIX + clusterAlias;
-        if (addresses.isEmpty()) {
-            DeleteInternalCcrRepositoryRequest request = new DeleteInternalCcrRepositoryRequest(repositoryName);
-            PlainActionFuture<DeleteInternalCcrRepositoryAction.DeleteInternalCcrRepositoryResponse> f = PlainActionFuture.newFuture();
-            client.executeLocally(DeleteInternalCcrRepositoryAction.INSTANCE, request, f);
-            assert f.isDone() : "Should be completed as it is executed synchronously";
-        } else {
-            PutInternalCcrRepositoryRequest request = new PutInternalCcrRepositoryRequest(repositoryName, CcrRepository.TYPE);
-            PlainActionFuture<PutInternalCcrRepositoryAction.PutInternalCcrRepositoryResponse> f = PlainActionFuture.newFuture();
-            client.executeLocally(PutInternalCcrRepositoryAction.INSTANCE, request, f);
-            assert f.isDone() : "Should be completed as it is executed synchronously";
+    protected void doStart() {
+        updateListener.init();
+    }
+
+    @Override
+    protected void doStop() {
+    }
+
+    @Override
+    protected void doClose() throws IOException {
+    }
+
+    private void putRepository(String repositoryName) {
+        PutInternalCcrRepositoryRequest request = new PutInternalCcrRepositoryRequest(repositoryName, CcrRepository.TYPE);
+        PlainActionFuture<PutInternalCcrRepositoryAction.PutInternalCcrRepositoryResponse> f = PlainActionFuture.newFuture();
+        client.executeLocally(PutInternalCcrRepositoryAction.INSTANCE, request, f);
+        assert f.isDone() : "Should be completed as it is executed synchronously";
+    }
+
+    private void deleteRepository(String repositoryName) {
+        DeleteInternalCcrRepositoryRequest request = new DeleteInternalCcrRepositoryRequest(repositoryName);
+        PlainActionFuture<DeleteInternalCcrRepositoryAction.DeleteInternalCcrRepositoryResponse> f = PlainActionFuture.newFuture();
+        client.executeLocally(DeleteInternalCcrRepositoryAction.INSTANCE, request, f);
+        assert f.isDone() : "Should be completed as it is executed synchronously";
+    }
+
+    private class RemoteSettingsUpdateListener extends RemoteClusterAware {
+
+        private RemoteSettingsUpdateListener(Settings settings) {
+            super(settings);
+        }
+
+        void init() {
+            Set<String> clusterAliases = buildRemoteClustersDynamicConfig(settings).keySet();
+            for (String clusterAlias : clusterAliases) {
+                putRepository(CcrRepository.NAME_PREFIX + clusterAlias);
+            }
+        }
+
+        @Override
+        protected void updateRemoteCluster(String clusterAlias, List<String> addresses, String proxy) {
+            String repositoryName = CcrRepository.NAME_PREFIX + clusterAlias;
+            if (addresses.isEmpty()) {
+                deleteRepository(repositoryName);
+            } else {
+                putRepository(repositoryName);
+            }
         }
     }
 }
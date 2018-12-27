0271e5dc8fcd1026af8dc1238ce2d30d9e367622
Jim Ferenczi <jimczi@apache.org>
2018-12-27 10:08:57 +0100

    remove obsolete javadocs

--- server/src/main/java/org/elasticsearch/search/suggest/completion/TopSuggestGroupDocsCollector.java
+++ server/src/main/java/org/elasticsearch/search/suggest/completion/TopSuggestGroupDocsCollector.java
@@ -30,21 +30,12 @@ import java.util.Map;
 /**
  *
  * Custom {@link TopSuggestDocsCollector} that returns top documents from the completion suggester.
- * <p>
- * TODO: this should be refactored when https://issues.apache.org/jira/browse/LUCENE-8529 is fixed.
- * Unlike the parent class, this collector uses the surface form to tie-break suggestions with identical
- * scores.
+ *
  * <p>
  * This collector groups suggestions coming from the same document but matching different contexts
  * or surface form together. When different contexts or surface forms match the same suggestion form only
  * the best one per document (sorted by weight) is kept.
  * <p>
- * This collector is also able to filter duplicate suggestion coming from different documents.
- * In order to keep this feature fast, the de-duplication of suggestions with different contexts is done
- * only on the top N*num_contexts (where N is the number of documents to return) suggestions per segment.
- * This means that skip_duplicates will visit at most N*num_contexts suggestions per segment to find unique suggestions
- * that match the input. If more than N*num_contexts suggestions are duplicated with different contexts this collector
- * will not be able to return more than one suggestion even when N is greater than 1.
  **/
 class TopSuggestGroupDocsCollector extends TopSuggestDocsCollector {
     private Map<Integer, List<CharSequence>> docContexts = new HashMap<>();

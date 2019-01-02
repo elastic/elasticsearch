a8ad9207edffce3bb8f4173f16d4137901751ff1
Hendrik Muhs <hendrik.muhs@elastic.co>
2019-01-02 14:04:42 +0100

    fix checkstyle

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/dataframe/DataFrameMessages.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/dataframe/DataFrameMessages.java
@@ -16,11 +16,13 @@ public class DataFrameMessages {
     public static final String REST_STOP_JOB_WAIT_FOR_COMPLETION_INTERRUPT = "Interrupted while waiting for data frame job [{0}] to stop";
     public static final String REST_PUT_DATA_FRAME_JOB_EXISTS = "Job with id [{1}] already exists";
     public static final String REST_DELETE_DATA_FRAME_UNKNOWN_JOB = "Job with id [{1}] could not be found";
-    public static final String REST_PUT_DATA_FRAME_FAILED_TO_VALIDATE_DATA_FRAME_CONFIGURATION = "Failed to validate data frame configuration";
+    public static final String REST_PUT_DATA_FRAME_FAILED_TO_VALIDATE_DATA_FRAME_CONFIGURATION =
+            "Failed to validate data frame configuration";
     public static final String REST_PUT_DATA_FRAME_FAILED_PERSIST_JOB_CONFIGURATION = "Failed to persist data frame configuration";
     public static final String REST_PUT_DATA_FRAME_FAILED_TO_DEDUCE_TARGET_MAPPINGS = "Failed to deduce target mappings";
     public static final String REST_PUT_DATA_FRAME_FAILED_TO_CREATE_TARGET_INDEX = "Failed to create target index";
-    public static final String REST_PUT_DATA_FRAME_FAILED_TO_START_PERSISTENT_TASK = "Failed to start persistent task, configuration has been cleaned up: [{0}]";
+    public static final String REST_PUT_DATA_FRAME_FAILED_TO_START_PERSISTENT_TASK =
+            "Failed to start persistent task, configuration has been cleaned up: [{0}]";
 
     public static final String FAILED_TO_CREATE_DESTINATION_INDEX = "Could not create destination index [{0}] for job[{1}]";
     public static final String FAILED_TO_LOAD_JOB_CONFIGURATION = "Failed to load data frame job configuration for job [{0}]";

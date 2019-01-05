94dc9519c179618aaa137137c1f7f050f6303f9a
Gordon Brown <gordon.brown@elastic.co>
2019-01-04 17:55:36 -0700

    [WIP] Add rollover-based test case

--- x-pack/plugin/ilm/qa/multi-cluster/src/test/java/org/elasticsearch/xpack/indexlifecycle/CCRIndexLifecycleIT.java
+++ x-pack/plugin/ilm/qa/multi-cluster/src/test/java/org/elasticsearch/xpack/indexlifecycle/CCRIndexLifecycleIT.java
@@ -12,11 +12,13 @@ import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.xpack.ccr.ESCCRRestTestCase;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.hamcrest.Matchers.equalTo;
@@ -30,20 +32,21 @@ public class CCRIndexLifecycleIT extends ESCCRRestTestCase {
     public void testBasicCCRAndILMIntegration() throws Exception {
         String indexName = "logs-1";
 
+        String policyName = "basic-test";
         if ("leader".equals(targetCluster)) {
-            putILMPolicy();
+            putILMPolicy(policyName, "50GB", null, TimeValue.timeValueHours(7*24));
             Settings indexSettings = Settings.builder()
                 .put("index.soft_deletes.enabled", true)
                 .put("index.number_of_shards", 1)
                 .put("index.number_of_replicas", 0)
-                .put("index.lifecycle.name", "my_policy")
+                .put("index.lifecycle.name", policyName)
                 .put("index.lifecycle.rollover_alias", "logs")
                 .build();
             createIndex(indexName, indexSettings, "", "\"logs\": { }");
             ensureGreen(indexName);
         } else if ("follow".equals(targetCluster)) {
             // Policy with the same name must exist in follower cluster too:
-            putILMPolicy();
+            putILMPolicy(policyName, "50GB", null, TimeValue.timeValueHours(7*24));
             followIndex(indexName, indexName);
             // Aliases are not copied from leader index, so we need to add that for the rollover action in follower cluster:
             client().performRequest(new Request("PUT", "/" + indexName + "/_alias/logs"));
@@ -57,8 +60,8 @@ public class CCRIndexLifecycleIT extends ESCCRRestTestCase {
                     // Sanity check that following_index setting has been set, so that we can verify later that this setting has been unset:
                     assertThat(getIndexSetting(client(), indexName, "index.xpack.ccr.following_index"), equalTo("true"));
 
-                    assertILMPolicy(leaderClient, indexName, "hot");
-                    assertILMPolicy(client(), indexName, "hot");
+                    assertILMPolicy(leaderClient, indexName, policyName, "hot");
+                    assertILMPolicy(client(), indexName, policyName, "hot");
                 });
 
                 updateIndexSettings(leaderClient, indexName, Settings.builder()
@@ -71,8 +74,8 @@ public class CCRIndexLifecycleIT extends ESCCRRestTestCase {
                     assertThat(getIndexSetting(leaderClient, indexName, "index.lifecycle.indexing_complete"), equalTo("true"));
                     assertThat(getIndexSetting(client(), indexName, "index.lifecycle.indexing_complete"), equalTo("true"));
 
-                    assertILMPolicy(leaderClient, indexName, "warm");
-                    assertILMPolicy(client(), indexName, "warm");
+                    assertILMPolicy(leaderClient, indexName, policyName, "warm");
+                    assertILMPolicy(client(), indexName, policyName, "warm");
 
                     // ILM should have placed both indices in the warm phase and there these indices are read-only:
                     assertThat(getIndexSetting(leaderClient, indexName, "index.blocks.write"), equalTo("true"));
@@ -87,8 +90,109 @@ public class CCRIndexLifecycleIT extends ESCCRRestTestCase {
         }
     }
 
-    private static void putILMPolicy() throws IOException {
-        final Request request = new Request("PUT", "_ilm/policy/my_policy");
+    public void testCcrAndIlmWithRollover() throws Exception {
+        String alias = "metrics";
+        String indexName = "metrics-000001";
+        String nextIndexName = "metrics-000002";
+        String policyName = "rollover-test";
+
+        if ("leader".equals(targetCluster)) {
+            // Create a policy on the leader
+            putILMPolicy(policyName, null, 1, null);
+            Request templateRequest = new Request("PUT", "_template/my_template");
+            Settings indexSettings = Settings.builder()
+                .put("index.soft_deletes.enabled", true)
+                .put("index.number_of_shards", 1)
+                .put("index.number_of_replicas", 0)
+                .put("index.lifecycle.name", policyName)
+                .put("index.lifecycle.rollover_alias", alias)
+                .build();
+            templateRequest.setJsonEntity("{\"index_patterns\":  [\"metrics-*\"], \"settings\":  " + Strings.toString(indexSettings) + "}");
+            assertOK(client().performRequest(templateRequest));
+        } else if ("follow".equals(targetCluster)) {
+            // Policy with the same name must exist in follower cluster too:
+            putILMPolicy(policyName, null, 1, null);
+
+            // Set up an auto-follow pattern
+            Request createAutoFollowRequest = new Request("PUT", "/_ccr/auto_follow/my_auto_follow_pattern");
+            createAutoFollowRequest.setJsonEntity("{\"leader_index_patterns\": [\"metrics-*\"], \"remote_cluster\": \"leader_cluster\"}");
+            assertOK(client().performRequest(createAutoFollowRequest));
+
+            try (RestClient leaderClient = buildLeaderClient()) {
+                // Create an index on the leader using the template set up above
+                Request createIndexRequest = new Request("PUT", "/" + indexName);
+                createIndexRequest.setJsonEntity("{" +
+                    "\"mappings\": {\"_doc\": {\"properties\": {\"field\": {\"type\": \"keyword\"}}}}, " +
+                    "\"aliases\": {\"" + alias + "\":  {\"is_write_index\":  true}} }");
+                assertOK(leaderClient.performRequest(createIndexRequest));
+                // Check that the new index is creeg
+                Request checkIndexRequest = new Request("GET", "/_cluster/health/" + indexName);
+                checkIndexRequest.addParameter("wait_for_status", "green");
+                checkIndexRequest.addParameter("timeout", "70s");
+                checkIndexRequest.addParameter("level", "shards");
+                assertOK(leaderClient.performRequest(checkIndexRequest));
+
+                // Check that it got replicated to the follower
+                assertBusy(() -> assertTrue(indexExists(indexName)));
+
+                // Aliases are not copied from leader index, so we need to add that for the rollover action in follower cluster:
+                client().performRequest(new Request("PUT", "/" + indexName + "/_alias/" + alias));
+
+                index(leaderClient, indexName, "1");
+                assertDocumentExists(leaderClient, indexName, "1");
+
+                assertBusy(() -> {
+                    assertDocumentExists(client(), indexName, "1");
+                    // Sanity check that following_index setting has been set, so that we can verify later that this setting has been unset:
+                    assertThat(getIndexSetting(client(), indexName, "index.xpack.ccr.following_index"), equalTo("true"));
+                });
+
+                // Wait for the index to roll over on the leader
+                assertBusy(() -> {
+                    assertOK(leaderClient.performRequest(new Request("HEAD", "/" + nextIndexName)));
+                    assertThat(getIndexSetting(leaderClient, indexName, "index.lifecycle.indexing_complete"), equalTo("true"));
+
+                });
+
+                assertBusy(() -> {
+                    // Wait for the next index should have been created on the leader
+                    assertOK(leaderClient.performRequest(new Request("HEAD", "/" + nextIndexName)));
+                    // And the old index should have a write block and indexing complete set
+                    assertThat(getIndexSetting(leaderClient, indexName, "index.blocks.write"), equalTo("true"));
+                    assertThat(getIndexSetting(leaderClient, indexName, "index.lifecycle.indexing_complete"), equalTo("true"));
+
+                });
+
+                assertBusy(() -> {
+                    // Wait for the setting to get replicated to the follower
+                    assertThat(getIndexSetting(client(), indexName, "index.lifecycle.indexing_complete"), equalTo("true"));
+                }, 2, TimeUnit.MINUTES);
+                // TODO: ^^ I don't know why but this takes like 70 seconds to happen. It doesn't seem like it should and this should
+                // definitely be fixed before merging.
+
+                assertBusy(() -> {
+                    // ILM should have unfollowed the follower index, so the following_index setting should have been removed:
+                    // (this controls whether the follow engine is used)
+                    assertThat(getIndexSetting(client(), indexName, "index.xpack.ccr.following_index"), nullValue());
+                    // The next index should have been created on the follower as well
+                    indexExists(nextIndexName);
+                });
+
+                assertBusy(() -> {
+                    // And the previously-follower index should be in the warm phase
+                    assertILMPolicy(client(), indexName, policyName, "warm");
+                });
+
+                // Clean up
+                leaderClient.performRequest(new Request("DELETE", "/_template/my_template"));
+            }
+        } else {
+            fail("unexpected target cluster [" + targetCluster + "]");
+        }
+    }
+
+    private static void putILMPolicy(String name, String maxSize, Integer maxDocs, TimeValue maxAge) throws IOException {
+        final Request request = new Request("PUT", "_ilm/policy/" + name);
         XContentBuilder builder = jsonBuilder();
         builder.startObject();
         {
@@ -101,8 +205,15 @@ public class CCRIndexLifecycleIT extends ESCCRRestTestCase {
                         builder.startObject("actions");
                         {
                             builder.startObject("rollover");
-                            builder.field("max_size", "50GB");
-                            builder.field("max_age", "7d");
+                            if (maxSize != null) {
+                                builder.field("max_size", maxSize);
+                            }
+                            if (maxAge != null) {
+                                builder.field("max_age", maxAge);
+                            }
+                            if (maxDocs != null) {
+                                builder.field("max_docs", maxDocs);
+                            }
                             builder.endObject();
                         }
                         {
@@ -143,13 +254,13 @@ public class CCRIndexLifecycleIT extends ESCCRRestTestCase {
         assertOK(client().performRequest(request));
     }
 
-    private static void assertILMPolicy(RestClient client, String index, String expectedPhase) throws IOException {
+    private static void assertILMPolicy(RestClient client, String index, String policy, String expectedPhase) throws IOException {
         final Request request = new Request("GET", "/" + index + "/_ilm/explain");
         Map<String, Object> response = toMap(client.performRequest(request));
         LOGGER.info("response={}", response);
         Map<?, ?> explanation = (Map<?, ?>) ((Map<?, ?>) response.get("indices")).get(index);
         assertThat(explanation.get("managed"), is(true));
-        assertThat(explanation.get("policy"), equalTo("my_policy"));
+        assertThat(explanation.get("policy"), equalTo(policy));
         assertThat(explanation.get("phase"), equalTo(expectedPhase));
     }
 

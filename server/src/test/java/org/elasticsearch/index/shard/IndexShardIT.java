9b3f8bf3a2229403e04e6b9cd39a074368dcb10d
Jason Tedor <jason@tedor.me>
2019-01-06 17:09:24 -0800

    Revert unrelated formatting change

--- server/src/test/java/org/elasticsearch/index/shard/IndexShardIT.java
+++ server/src/test/java/org/elasticsearch/index/shard/IndexShardIT.java
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.elasticsearch.index.shard;
 
 import org.apache.lucene.store.LockObtainFailedException;

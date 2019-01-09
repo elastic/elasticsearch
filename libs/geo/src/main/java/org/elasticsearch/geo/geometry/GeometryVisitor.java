9408af723036bcf0b23e91cf901bb66ee0ad1d8e
Igor Motov <igor@motovs.org>
2019-01-09 15:32:30 -0500

    Add javadoc with explanation of the Visitor Pattern

--- libs/geo/src/main/java/org/elasticsearch/geo/geometry/GeometryVisitor.java
+++ libs/geo/src/main/java/org/elasticsearch/geo/geometry/GeometryVisitor.java
@@ -20,7 +20,29 @@
 package org.elasticsearch.geo.geometry;
 
 /**
- * Support class for creating of geometry Visitors
+ * Support class for creating of geometry Visitors.
+ * <p>
+ * This is an implementation of the Visitor pattern. The basic idea is to simplify adding new operations on Geometries, without
+ * constantly modifying and adding new functionality to the Geometry hierarchy and keeping it as lightweight as possible.
+ * <p>
+ * It is a more object-oriented alternative to structures like this:
+ * <pre>
+ * if (obj instanceof This) {
+ *   doThis((This) obj);
+ * } elseif (obj instanceof That) {
+ *   doThat((That) obj);
+ * ...
+ * } else {
+ *   throw new IllegalArgumentException("Unknown object " + obj);
+ * }
+ * </pre>
+ * <p>
+ * The Visitor Pattern replaces this structure with Interface inheritance making it easier to identify all places that are using this
+ * structure, and making a shape a compile-time failure instead of runtime.
+ * <p>
+ * See {@link org.elasticsearch.geo.utils.WellKnownText#toWKT(Geometry, StringBuilder)} for an example of how this interface is used.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Visitor_pattern">Visitor Pattern</a>
  */
 public interface GeometryVisitor<T> {
 

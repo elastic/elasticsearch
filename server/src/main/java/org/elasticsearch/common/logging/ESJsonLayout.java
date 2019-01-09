669e9ecab04fdf7a7c64254e0cded5c7b46a1483
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2019-01-09 17:36:17 +0100

    additional json tests

--- server/src/main/java/org/elasticsearch/common/logging/ESJsonLayout.java
+++ server/src/main/java/org/elasticsearch/common/logging/ESJsonLayout.java
@@ -19,23 +19,39 @@
 
 package org.elasticsearch.common.logging;
 
+import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.Node;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
 import org.apache.logging.log4j.core.layout.AbstractStringLayout;
 import org.apache.logging.log4j.core.layout.ByteBufferDestination;
 import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.elasticsearch.common.Strings;
 
 import java.nio.charset.Charset;
 import java.util.Map;
 
-@Plugin(name = "ESJsonLayout", category = "Core", elementType = "layout", printObject = true)
+@Plugin(name = "ESJsonLayout", category = Node.CATEGORY, elementType = Layout.ELEMENT_TYPE, printObject = true)
 public class ESJsonLayout extends AbstractStringLayout {
 
+    /**
+     * type - the name of the appender
+     * timestamp - ISO8601 with additional timezone ID
+     * level - INFO, WARN etc
+     * component - logger name, most of the times class name
+     * cluster.name - taken from sys:es.logs.cluster_name system property because it is always set
+     * node.name - taken from NodeNamePatternConverter, as it can be set in runtime as hostname when not set in elasticsearch.yml
+     * node_and_cluster_id - in json as node.id and cluster.uuid - taken from NodeAndClusterIdConverter and present
+     * once clusterStateUpdate is first received
+     * message - a json escaped message. Multiline messages will be converted to single line with new line explicitly replaced to \n
+     * exceptionAsJson - in json as stacktrace. Only present when throwable is passed as a parameter to a Logger. Taken from
+     * JsonThrowablePatternConverter
+     */
     private static final String PATTERN = "{" +
-        "\"type\": \"console\", " +
-        "\"timestamp\": \"%d{yyyy-MM-dd'T'HH:mm:ss,SSSZ}\", " + // this is ISO8601 with additional timezone ID
+        "\"type\": \"${TYPE}\", " +
+        "\"timestamp\": \"%d{yyyy-MM-dd'T'HH:mm:ss,SSSZ}\", " +
         "\"level\": \"%p\", " +
         "\"component\": \"%c{1.}\", " +
         "\"cluster.name\": \"${sys:es.logs.cluster_name}\", " +
@@ -47,21 +63,25 @@ public class ESJsonLayout extends AbstractStringLayout {
 
     private final PatternLayout patternLayout;
 
-    protected ESJsonLayout(boolean locationInfo, boolean properties, boolean complete,
-                           Charset charset) {
+    protected ESJsonLayout(String typeName, Charset charset) {
         super(charset);
         this.patternLayout = PatternLayout.newBuilder()
-            .withPattern(PATTERN)
-            .withAlwaysWriteExceptions(false)
-            .build();
+                                          .withPattern(pattern(typeName))
+                                          .withAlwaysWriteExceptions(false)
+                                          .build();
+    }
+
+    private String pattern(String type) {
+        if (Strings.isEmpty(type)) {
+            throw new IllegalArgumentException("layout parameter 'type_name' cannot be empty");
+        }
+        return PATTERN.replace("${TYPE}", type);
     }
 
     @PluginFactory
-    public static ESJsonLayout createLayout(@PluginAttribute("locationInfo") boolean locationInfo,
-                                            @PluginAttribute("properties") boolean properties,
-                                            @PluginAttribute("complete") boolean complete,
+    public static ESJsonLayout createLayout(@PluginAttribute("type_name") String type,
                                             @PluginAttribute(value = "charset", defaultString = "UTF-8") Charset charset) {
-        return new ESJsonLayout(locationInfo, properties, complete, charset);
+        return new ESJsonLayout(type, charset);
     }
 
     @Override

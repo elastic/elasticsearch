ed43526630884c68499deed405e5f27236b16e85
Jason Tedor <jason@tedor.me>
2019-01-11 11:01:36 -0800

    Imports

--- server/src/test/java/org/elasticsearch/index/engine/InternalEngineTests.java
+++ server/src/test/java/org/elasticsearch/index/engine/InternalEngineTests.java
@@ -149,7 +149,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;

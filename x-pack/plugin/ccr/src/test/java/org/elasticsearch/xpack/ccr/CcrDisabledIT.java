976eeebc9ca4d0b669a1ef5fac3630601aa4c730
Tim Brooks <tim@uncontended.net>
2019-01-14 15:19:59 -0700

    Work on changes

--- x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/CcrDisabledIT.java
+++ x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/CcrDisabledIT.java
@@ -8,25 +8,44 @@ package org.elasticsearch.xpack.ccr;
 
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.protocol.xpack.XPackInfoRequest;
+import org.elasticsearch.protocol.xpack.XPackInfoResponse;
 import org.elasticsearch.test.ESIntegTestCase;
+import org.elasticsearch.xpack.core.XPackClient;
+import org.elasticsearch.xpack.core.XPackClientPlugin;
 import org.elasticsearch.xpack.core.XPackSettings;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.EnumSet;
 
 public class CcrDisabledIT extends ESIntegTestCase {
 
     public void testClusterCanStartWithCcrInstalledButNotEnabled() throws Exception {
         ensureGreen();
+        XPackClient xPackClient = new XPackClient(client());
+        XPackInfoResponse response = xPackClient.prepareInfo().setCategories(EnumSet.of(XPackInfoRequest.Category.FEATURES)).get();
+        XPackInfoResponse.FeatureSetsInfo featureSetsInfo = response.getFeatureSetsInfo();
     }
 
     @Override
     protected Settings nodeSettings(int nodeOrdinal) {
-        return Settings.builder().put(super.nodeSettings(nodeOrdinal)).put(XPackSettings.CCR_ENABLED_SETTING.getKey(), false).build();
+        return Settings.builder().put(super.nodeSettings(nodeOrdinal)).put(XPackSettings.CCR_ENABLED_SETTING.getKey(), true)
+            .put(XPackSettings.SECURITY_ENABLED.getKey(), false).build();
+    }
+
+    @Override
+    protected Settings transportClientSettings() {
+        return Settings.builder().put(super.transportClientSettings()).put(XPackSettings.SECURITY_ENABLED.getKey(), false).build();
     }
 
     @Override
     protected Collection<Class<? extends Plugin>> nodePlugins() {
         return Arrays.asList(LocalStateCcr.class);
+    }
+
+    @Override
+    protected Collection<Class<? extends Plugin>> transportClientPlugins() {
+        return Arrays.asList(XPackClientPlugin.class);
     }
 }

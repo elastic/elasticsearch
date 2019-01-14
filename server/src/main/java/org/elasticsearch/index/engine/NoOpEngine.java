eeaaf50bc052c4d286a528ffc20da30dfbd71c19
Tanguy Leroux <tlrx.dev@gmail.com>
2019-01-14 11:25:24 +0100

    Relax NoOpEngine constraints

--- server/src/main/java/org/elasticsearch/index/engine/NoOpEngine.java
+++ server/src/main/java/org/elasticsearch/index/engine/NoOpEngine.java
@@ -24,54 +24,20 @@ import org.apache.lucene.index.IndexCommit;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.store.Directory;
-import org.elasticsearch.common.Nullable;
-import org.elasticsearch.core.internal.io.IOUtils;
-import org.elasticsearch.index.translog.Translog;
-import org.elasticsearch.index.translog.TranslogConfig;
-import org.elasticsearch.index.translog.TranslogCorruptedException;
-import org.elasticsearch.index.translog.TranslogDeletionPolicy;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-import java.util.function.LongSupplier;
-import java.util.stream.Stream;
+import java.util.function.Function;
 
 /**
  * NoOpEngine is an engine implementation that does nothing but the bare minimum
  * required in order to have an engine. All attempts to do something (search,
- * index, get), throw {@link UnsupportedOperationException}. This does maintain
- * a translog with a deletion policy so that when flushing, no translog is
- * retained on disk (setting a retention size and age of 0).
- *
- * It's also important to notice that this does list the commits of the Store's
- * Directory so that the last commit's user data can be read for the historyUUID
- * and last committed segment info.
+ * index, get), throw {@link UnsupportedOperationException}.
  */
 public final class NoOpEngine extends ReadOnlyEngine {
 
-    public NoOpEngine(EngineConfig engineConfig) {
-        super(engineConfig, null, null, true, directoryReader -> directoryReader);
-        boolean success = false;
-        try {
-            // The deletion policy for the translog should not keep any translogs around, so the min age/size is set to -1
-            final TranslogDeletionPolicy translogDeletionPolicy = new TranslogDeletionPolicy(-1, -1);
-
-            // The translog is opened and closed to validate that the translog UUID from lucene is the same as the one in the translog
-            try (Translog translog = openTranslog(engineConfig, translogDeletionPolicy, engineConfig.getGlobalCheckpointSupplier())) {
-                final int nbOperations = translog.totalOperations();
-                if (nbOperations != 0) {
-                    throw new IllegalArgumentException("Expected 0 translog operations but there were " + nbOperations);
-                }
-            }
-            success = true;
-        } catch (IOException | TranslogCorruptedException e) {
-            throw new EngineCreationFailureException(shardId, "failed to create engine", e);
-        } finally {
-            if (success == false) {
-                IOUtils.closeWhileHandlingException(this);
-            }
-        }
+    public NoOpEngine(EngineConfig config) {
+        super(config, null, null, true, Function.identity());
     }
 
     @Override
@@ -120,31 +86,5 @@ public final class NoOpEngine extends ReadOnlyEngine {
                 return null;
             }
         };
-    }
-
-    private Translog openTranslog(EngineConfig engineConfig, TranslogDeletionPolicy translogDeletionPolicy,
-                                  LongSupplier globalCheckpointSupplier) throws IOException {
-        final TranslogConfig translogConfig = engineConfig.getTranslogConfig();
-        final String translogUUID = loadTranslogUUIDFromLastCommit();
-        // We expect that this shard already exists, so it must already have an existing translog else something is badly wrong!
-        return new Translog(translogConfig, translogUUID, translogDeletionPolicy, globalCheckpointSupplier,
-                engineConfig.getPrimaryTermSupplier());
-    }
-
-    /**
-     * Reads the current stored translog ID from the last commit data.
-     */
-    @Nullable
-    private String loadTranslogUUIDFromLastCommit() {
-        final Map<String, String> commitUserData = getLastCommittedSegmentInfos().getUserData();
-        if (commitUserData.containsKey(Translog.TRANSLOG_GENERATION_KEY) == false) {
-            throw new IllegalStateException("Commit doesn't contain translog generation id");
-        }
-        return commitUserData.get(Translog.TRANSLOG_UUID_KEY);
-    }
-
-    @Override
-    public boolean ensureTranslogSynced(Stream<Translog.Location> locations) {
-        throw new UnsupportedOperationException("Translog synchronization should never be needed");
     }
 }

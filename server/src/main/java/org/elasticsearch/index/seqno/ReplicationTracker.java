eb90d5484c8abf6428c165485aa5d2c2c410f451
Jason Tedor <jason@tedor.me>
2019-01-15 07:41:59 -0500

    Fix imports

--- server/src/main/java/org/elasticsearch/index/seqno/ReplicationTracker.java
+++ server/src/main/java/org/elasticsearch/index/seqno/ReplicationTracker.java
@@ -42,7 +42,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.OptionalLong;
 import java.util.Set;
 import java.util.function.Consumer;

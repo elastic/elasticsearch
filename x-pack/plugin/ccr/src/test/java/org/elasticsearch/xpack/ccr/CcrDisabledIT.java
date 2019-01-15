e10820e5d29f4625efb4067150a432d267b1be8a
Tim Brooks <tim@uncontended.net>
2019-01-15 12:48:47 -0700

    Fix checkstyle

--- x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/CcrDisabledIT.java
+++ x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/CcrDisabledIT.java
@@ -12,7 +12,6 @@ import org.elasticsearch.test.ESIntegTestCase;
 import org.elasticsearch.xpack.core.XPackClientPlugin;
 import org.elasticsearch.xpack.core.XPackSettings;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 

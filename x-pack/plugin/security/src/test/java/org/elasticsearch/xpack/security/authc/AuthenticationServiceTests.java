23eff70dab9933a3a76efe16d08f99ac173b557e
Yogesh Gaikwad <902768+bizybot@users.noreply.github.com>
2019-01-17 15:39:30 +1100

    Add invalidate API key transport action (#37399)
    This commit adds transport action for invalidating API keys.
    Users can request invalidation of api keys for:-
    - all the keys for a given username
    - all the keys for a given realm name
    - all the keys for a given realm and username
    - for a given api key id
    - for a given api key name
    This is similar to invalidate tokens API.
    Follow up PRs: Rest action and HLRC changes for the invalidate API.

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authc/AuthenticationServiceTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authc/AuthenticationServiceTests.java
@@ -1221,6 +1221,7 @@ public class AuthenticationServiceTests extends ESTestCase {
                 final Map<String, Object> source = new HashMap<>();
                 source.put("doc_type", "api_key");
                 source.put("creation_time", Instant.now().minus(5, ChronoUnit.MINUTES).toEpochMilli());
+                source.put("api_key_invalidated", false);
                 source.put("api_key_hash", new String(Hasher.BCRYPT4.hash(new SecureString(key.toCharArray()))));
                 source.put("role_descriptors", Collections.singletonMap("api key role", Collections.singletonMap("cluster", "all")));
                 source.put("name", "my api key for testApiKeyAuth");
@@ -1259,6 +1260,7 @@ public class AuthenticationServiceTests extends ESTestCase {
                 source.put("doc_type", "api_key");
                 source.put("creation_time", Instant.now().minus(5L, ChronoUnit.HOURS).toEpochMilli());
                 source.put("expiration_time", Instant.now().minus(1L, ChronoUnit.HOURS).toEpochMilli());
+                source.put("api_key_invalidated", false);
                 source.put("api_key_hash", new String(Hasher.BCRYPT4.hash(new SecureString(key.toCharArray()))));
                 source.put("role_descriptors", Collections.singletonList(Collections.singletonMap("name", "a role")));
                 source.put("name", "my api key for testApiKeyAuth");

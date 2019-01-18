ae7185a1f9ce2bcec7da854f6b25c496cb99bdd3
Michael Basnight <mbasnight@gmail.com>
2019-01-18 14:16:49 -0600

    Update verify repository to allow unknown fields
    The subparser in verify repository allows for unknown fields. This
    commit sets the value to true for the parser and modifies the test such
    that it accurately tests it.
    
    Relates #36938

---
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/watcher/VerifyRepositoryResponseTests.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.client.watcher;
+
+import org.elasticsearch.action.admin.cluster.repositories.verify.VerifyRepositoryResponse;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.common.xcontent.json.JsonXContent;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.test.XContentTestUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.elasticsearch.test.AbstractXContentTestCase.xContentTester;
+
+public class VerifyRepositoryResponseTests extends ESTestCase {
+
+    public void testFromXContent() throws IOException {
+        xContentTester(this::createParser,
+            VerifyRepositoryResponseTests::createTestInstance,
+            VerifyRepositoryResponseTests::toXContent,
+            VerifyRepositoryResponse::fromXContent)
+            .supportsUnknownFields(false)
+            .shuffleFieldsExceptions(new String[] {"nodes"}) // do not mix up the order of nodes, it will cause the tests to fail
+            .assertToXContentEquivalence(false)
+            .test();
+    }
+
+    private static VerifyRepositoryResponse createTestInstance() {
+        List<VerifyRepositoryResponse.NodeView> nodes = new ArrayList<>();
+        for (int i = 0; i < randomIntBetween(0, 2); i++) {
+            nodes.add(new VerifyRepositoryResponse.NodeView(randomAlphaOfLength(5), randomAlphaOfLength(5)));
+        }
+
+        return new VerifyRepositoryResponse(nodes);
+    }
+
+    private static XContentBuilder toXContent(VerifyRepositoryResponse.NodeView node, XContentBuilder builder) throws IOException {
+        // Use a temp builder to create the object we want to add random fields to
+        XContentBuilder tempBuilder = JsonXContent.contentBuilder().startObject().field("name", node.getName()).endObject();
+        BytesReference newBytes = XContentTestUtils.insertRandomFields(XContentType.JSON, BytesReference.bytes(tempBuilder),
+            null, random());
+
+        // add the temp object into the actual builder
+        builder.rawField(node.getNodeId(), newBytes.streamInput(), XContentType.JSON);
+
+        return builder;
+    }
+
+    private static XContentBuilder toXContent(VerifyRepositoryResponse response, XContentBuilder builder) throws IOException {
+        builder.startObject();
+        {
+            builder.startObject("nodes");
+            {
+                for (VerifyRepositoryResponse.NodeView node : response.getNodes()) {
+                    toXContent(node, builder);
+                }
+            }
+            builder.endObject();
+        }
+        builder.endObject();
+        return builder;
+    }
+}

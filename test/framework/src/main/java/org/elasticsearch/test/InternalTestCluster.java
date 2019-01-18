9fcf0d58a3d257f7847ca906cacbb03fd547fd70
Yannick Welsch <yannick@welsch.lu>
2019-01-18 10:51:55 +0100

    only set initial_master_nodes if not explicitly set by test

--- test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
+++ test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
@@ -936,7 +936,9 @@ public final class InternalTestCluster extends TestCluster {
             if (minMasterNodes >= 0) {
                 assert DISCOVERY_ZEN_MINIMUM_MASTER_NODES_SETTING.exists(newSettings.build()) == false : "min master nodes is auto managed";
                 newSettings.put(DISCOVERY_ZEN_MINIMUM_MASTER_NODES_SETTING.getKey(), minMasterNodes);
-                newSettings.putList(INITIAL_MASTER_NODES_SETTING.getKey());
+                if (INITIAL_MASTER_NODES_SETTING.exists(callbackSettings) == false) {
+                    newSettings.putList(INITIAL_MASTER_NODES_SETTING.getKey());
+                }
             }
             // delete data folders now, before we start other nodes that may claim it
             clearDataIfNeeded(callback);

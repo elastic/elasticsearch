a50cc5661ee69f4ad28122eca9464a3b8a0c1ac9
jaymode <jay@elastic.co>
2019-01-18 12:40:58 -0700

    Fix ssl configuration in openldap test
    This commit fixes the ssl configuration in the openldap tests to not
    use the deprecated global configuration and instead properly define the
    realm settings so that the session factory can retrieve the correct
    ssl configuration.
    
    See #37591

--- x-pack/qa/openldap-tests/src/test/java/org/elasticsearch/xpack/security/authc/ldap/OpenLdapUserSearchSessionFactoryTests.java
+++ x-pack/qa/openldap-tests/src/test/java/org/elasticsearch/xpack/security/authc/ldap/OpenLdapUserSearchSessionFactoryTests.java
@@ -56,7 +56,8 @@ public class OpenLdapUserSearchSessionFactoryTests extends ESTestCase {
          */
         globalSettings = Settings.builder()
             .put("path.home", createTempDir())
-            .put("xpack.ssl.certificate_authorities", caPath)
+            .put("xpack.security.authc.realms.oldap-test.type", "ldap")
+            .put("xpack.security.authc.realms.oldap-test.ssl.certificate_authorities", caPath)
             .build();
         threadPool = new TestThreadPool("LdapUserSearchSessionFactoryTests");
     }

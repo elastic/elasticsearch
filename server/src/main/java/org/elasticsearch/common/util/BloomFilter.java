2f3a9598749e9d71a4653b788d537eadda4cfb82
Zachary Tong <zach@elastic.co>
2019-01-22 16:04:02 -0500

    Make bloom filter less bad (cleanup legacy cruft)

--- server/src/main/java/org/elasticsearch/common/util/BloomFilter.java
+++ server/src/main/java/org/elasticsearch/common/util/BloomFilter.java
@@ -25,7 +25,6 @@ import org.elasticsearch.common.hash.MurmurHash3;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
-import org.elasticsearch.common.lease.Releasable;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -34,25 +33,26 @@ import java.util.Objects;
 /**
  * A bloom filter. Inspired by Guava bloom filter implementation though with some optimizations.
  */
-public class BloomFilter implements Writeable, Releasable {
+public class BloomFilter implements Writeable {
 
-    // Some numbers:
-    // 10k =0.001: 140.4kb  , 10 Hashes
-    // 10k =0.01 :  93.6kb  ,  6 Hashes
-    // 100k=0.01 : 936.0kb  ,  6 Hashes
-    // 100k=0.03 : 712.7kb  ,  5 Hashes
-    // 500k=0.01 :   4.5mb  ,  6 Hashes
-    // 500k=0.03 :   3.4mb  ,  5 Hashes
-    // 500k=0.05 :   2.9mb  ,  4 Hashes
-    //   1m=0.01 :   9.1mb  ,  6 Hashes
-    //   1m=0.03 :   6.9mb  ,  5 Hashes
-    //   1m=0.05 :   5.9mb  ,  4 Hashes
-    //   5m=0.01 :  45.7mb  ,  6 Hashes
-    //   5m=0.03 :  34.8mb  ,  5 Hashes
-    //   5m=0.05 :  29.7mb  ,  4 Hashes
-    //  50m=0.01 : 457.0mb  ,  6 Hashes
-    //  50m=0.03 : 297.3mb  ,  4 Hashes
-    //  50m=0.10 : 228.5mb  ,  3 Hashes
+    // Some anecdotal sizing numbers:
+    // expected insertions, false positive probability, bloom size, num hashes
+    //  10k, 0.001, 140.4kb, 10 Hashes
+    //  10k,  0.01,  93.6kb,  6 Hashes
+    // 100k,  0.01, 936.0kb,  6 Hashes
+    // 100k,  0.03, 712.7kb,  5 Hashes
+    // 500k,  0.01,   4.5mb,  6 Hashes
+    // 500k,  0.03,   3.4mb,  5 Hashes
+    // 500k,  0.05,   2.9mb,  4 Hashes
+    //   1m,  0.01,   9.1mb,  6 Hashes
+    //   1m,  0.03,   6.9mb,  5 Hashes
+    //   1m,  0.05,   5.9mb,  4 Hashes
+    //   5m,  0.01,  45.7mb,  6 Hashes
+    //   5m,  0.03,  34.8mb,  5 Hashes
+    //   5m,  0.05,  29.7mb,  4 Hashes
+    //  50m,  0.01, 457.0mb,  6 Hashes
+    //  50m,  0.03, 297.3mb,  4 Hashes
+    //  50m,  0.10, 228.5mb,  3 Hashes
 
     /**
      * The bit set of the BloomFilter (not necessarily power of 2!)
@@ -64,10 +64,8 @@ public class BloomFilter implements Writeable, Releasable {
      */
     private final int numHashFunctions;
 
-    private final Hashing hashing = Hashing.V1;
-
     /**
-     * Creates a bloom filter based on the with the expected number
+     * Creates a bloom filter with the expected number
      * of insertions and expected false positive probability.
      *
      * @param expectedInsertions the number of expected insertions to the constructed
@@ -92,7 +90,7 @@ public class BloomFilter implements Writeable, Releasable {
         /*
          * TODO(user): Put a warning in the javadoc about tiny fpp values,
          * since the resulting size is proportional to -log(p), but there is not
-         * much of a point after all, e.g. optimalM(1000, 0.0000000000000001) = 76680
+         * much of a point after all, e.g. optimalNumOfBits(1000, 0.0000000000000001) = 76680
          * which is less that 10kb. Who cares!
          */
         long numBits = optimalNumOfBits(expectedInsertions, fpp);
@@ -111,13 +109,13 @@ public class BloomFilter implements Writeable, Releasable {
     }
 
     public BloomFilter(StreamInput in) throws IOException {
-        int numLongs = in.readVInt();
-        long[] data = new long[numLongs];
-        for (int i = 0; i < numLongs; i++) {
-            data[i] = in.readLong();
-        }
+        this.bits = new BitArray(in);
         this.numHashFunctions = in.readVInt();
-        this.bits = new BitArray(data);
+    }
+
+    public void writeTo(StreamOutput out) throws IOException {
+        bits.writeTo(out);
+        out.writeVInt(numHashFunctions);
     }
 
     public void merge(BloomFilter other) {
@@ -125,11 +123,11 @@ public class BloomFilter implements Writeable, Releasable {
     }
 
     public boolean put(BytesRef value) {
-        return hashing.put(value, numHashFunctions, bits);
+        return Hashing.put(value, numHashFunctions, bits);
     }
 
     public boolean put(byte[] value) {
-        return hashing.put(value, 0, value.length, numHashFunctions, bits);
+        return Hashing.put(value, 0, value.length, numHashFunctions, bits);
     }
 
     public boolean put(long value) {
@@ -137,11 +135,11 @@ public class BloomFilter implements Writeable, Releasable {
     }
 
     public boolean mightContain(BytesRef value) {
-        return hashing.mightContain(value, numHashFunctions, bits);
+        return Hashing.mightContain(value, numHashFunctions, bits);
     }
 
-    public boolean mightContain(byte[] value) {
-        return hashing.mightContain(value, 0, value.length, numHashFunctions, bits);
+    private boolean mightContain(byte[] value) {
+        return Hashing.mightContain(value, 0, value.length, numHashFunctions, bits);
     }
 
     public boolean mightContain(long value) {
@@ -172,22 +170,9 @@ public class BloomFilter implements Writeable, Releasable {
 
         final BloomFilter that = (BloomFilter) other;
         return Objects.equals(this.bits, that.bits)
-            && Objects.equals(this.hashing, that.hashing)
             && Objects.equals(this.numHashFunctions, that.numHashFunctions);
     }
 
-    public void writeTo(StreamOutput out) throws IOException {
-        out.writeVInt(bits.data.length);
-        for (long l : bits.data) {
-            out.writeLong(l);
-        }
-        out.writeVInt(numHashFunctions);
-    }
-
-    @Override
-    public void close() {
-
-    }
 
     /*
      * Cheat sheet:
@@ -234,13 +219,19 @@ public class BloomFilter implements Writeable, Releasable {
     }
 
     // Note: We use this instead of java.util.BitSet because we need access to the long[] data field
-    static final class BitArray {
-        final long[] data;
-        final long bitSize;
-        long bitCount;
+    static final class BitArray implements Writeable {
+        private final long[] data;
+        private final long bitSize;
+        private long bitCount;
 
         BitArray(long bits) {
-            this(new long[size(bits)]);
+            this.data = new long[size(bits)];
+            long bitCount = 0;
+            for (long value : data) {
+                bitCount += Long.bitCount(value);
+            }
+            this.bitCount = bitCount;
+            this.bitSize = data.length * Long.SIZE;
         }
 
         private static int size(long bits) {
@@ -249,15 +240,17 @@ public class BloomFilter implements Writeable, Releasable {
             return Math.toIntExact(remainder == 0 ? quotient : 1 + quotient);
         }
 
-        // Used by serialization
-        BitArray(long[] data) {
-            this.data = data;
-            long bitCount = 0;
-            for (long value : data) {
-                bitCount += Long.bitCount(value);
-            }
-            this.bitCount = bitCount;
-            this.bitSize = data.length * Long.SIZE;
+        BitArray(StreamInput in) throws IOException {
+            this.data = in.readVLongArray();
+            this.bitSize = in.readVLong();
+            this.bitCount = in.readVLong();
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) throws IOException {
+            out.writeVLongArray(data);
+            out.writeVLong(bitSize);
+            out.writeVLong(bitCount);
         }
 
         /**
@@ -290,10 +283,6 @@ public class BloomFilter implements Writeable, Releasable {
             return bitCount;
         }
 
-        BitArray copy() {
-            return new BitArray(data.clone());
-        }
-
         /**
          * Combines the two BitArrays using bitwise OR.
          */
@@ -319,63 +308,48 @@ public class BloomFilter implements Writeable, Releasable {
             return Arrays.hashCode(data);
         }
 
-
         public long ramBytesUsed() {
             return Long.BYTES * data.length + RamUsageEstimator.NUM_BYTES_ARRAY_HEADER + 16;
         }
     }
 
-    enum Hashing {
-        V1() {
-            @Override
-            protected boolean put(byte[] bytes, int offset, int length, int numHashFunctions, BitArray bits) {
-                long bitSize = bits.bitSize();
-                MurmurHash3.Hash128 hash128 = MurmurHash3.hash128(bytes, offset, length, 0, new MurmurHash3.Hash128());
+    private static class Hashing {
 
-                boolean bitsChanged = false;
-                long combinedHash = hash128.h1;
-                for (int i = 0; i < numHashFunctions; i++) {
-                    // Make the combined hash positive and indexable
-                    bitsChanged |= bits.set((combinedHash & Long.MAX_VALUE) % bitSize);
-                    combinedHash += hash128.h2;
-                }
-                return bitsChanged;
-            }
-
-            @Override
-            protected boolean mightContain(byte[] bytes, int offset, int length, int numHashFunctions, BitArray bits) {
-                long bitSize = bits.bitSize();
-                MurmurHash3.Hash128 hash128 = MurmurHash3.hash128(bytes, offset, length, 0, new MurmurHash3.Hash128());
-
-                long combinedHash = hash128.h1;
-                for (int i = 0; i < numHashFunctions; i++) {
-                    // Make the combined hash positive and indexable
-                    if (!bits.get((combinedHash & Long.MAX_VALUE) % bitSize)) {
-                        return false;
-                    }
-                    combinedHash += hash128.h2;
-                }
-                return true;
-            }
-
-            @Override
-            protected int type() {
-                return 1;
-            }
-        };
-
-        protected boolean put(BytesRef value, int numHashFunctions, BitArray bits) {
+        static boolean put(BytesRef value, int numHashFunctions, BitArray bits) {
             return put(value.bytes, value.offset, value.length, numHashFunctions, bits);
         }
 
-        protected abstract boolean put(byte[] bytes, int offset, int length, int numHashFunctions, BitArray bits);
+        static boolean put(byte[] bytes, int offset, int length, int numHashFunctions, BitArray bits) {
+            long bitSize = bits.bitSize();
+            MurmurHash3.Hash128 hash128 = MurmurHash3.hash128(bytes, offset, length, 0, new MurmurHash3.Hash128());
 
-        protected boolean mightContain(BytesRef value, int numHashFunctions, BitArray bits) {
+            boolean bitsChanged = false;
+            long combinedHash = hash128.h1;
+            for (int i = 0; i < numHashFunctions; i++) {
+                // Make the combined hash positive and indexable
+                bitsChanged |= bits.set((combinedHash & Long.MAX_VALUE) % bitSize);
+                combinedHash += hash128.h2;
+            }
+            return bitsChanged;
+        }
+
+        static boolean mightContain(BytesRef value, int numHashFunctions, BitArray bits) {
             return mightContain(value.bytes, value.offset, value.length, numHashFunctions, bits);
         }
 
-        protected abstract boolean mightContain(byte[] bytes, int offset, int length, int numHashFunctions, BitArray bits);
+        static boolean mightContain(byte[] bytes, int offset, int length, int numHashFunctions, BitArray bits) {
+            long bitSize = bits.bitSize();
+            MurmurHash3.Hash128 hash128 = MurmurHash3.hash128(bytes, offset, length, 0, new MurmurHash3.Hash128());
 
-        protected abstract int type();
+            long combinedHash = hash128.h1;
+            for (int i = 0; i < numHashFunctions; i++) {
+                // Make the combined hash positive and indexable
+                if (!bits.get((combinedHash & Long.MAX_VALUE) % bitSize)) {
+                    return false;
+                }
+                combinedHash += hash128.h2;
+            }
+            return true;
+        }
     }
 }

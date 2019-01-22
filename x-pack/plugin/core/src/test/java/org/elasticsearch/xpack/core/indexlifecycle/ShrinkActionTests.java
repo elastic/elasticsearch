37b0dc74eae305576ea94b279153ae2ade92ed41
Tal Levy <jubboy333@gmail.com>
2019-01-22 13:57:56 -0800

    Skip Shrink when numberOfShards not changed
    Previously, ShrinkAction would fail if
    it was executed on an index that had
    the same number of shards as the target
    shrunken number.
    
    This PR introduced a new BranchingStep that
    is used inside of ShrinkAction to branch which
    step to move to next, depending on the
    shard values. So no shrink will occur if the
    shard count is unchanged.

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/indexlifecycle/ShrinkActionTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/indexlifecycle/ShrinkActionTests.java
@@ -5,12 +5,18 @@
  */
 package org.elasticsearch.xpack.core.indexlifecycle;
 
+import org.elasticsearch.Version;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
+import org.elasticsearch.cluster.metadata.MetaData;
 import org.elasticsearch.common.io.stream.Writeable.Reader;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.xpack.core.indexlifecycle.Step.StepKey;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -46,59 +52,134 @@ public class ShrinkActionTests extends AbstractActionTestCase<ShrinkAction> {
         assertThat(e.getMessage(), equalTo("[number_of_shards] must be greater than 0"));
     }
 
+    public void testPerformActionWithSkip() {
+        String lifecycleName = randomAlphaOfLengthBetween(4, 10);
+        int numberOfShards = randomIntBetween(1, 10);
+        ShrinkAction action = new ShrinkAction(numberOfShards);
+        String phase = randomAlphaOfLengthBetween(1, 10);
+        StepKey nextStepKey = new StepKey(randomAlphaOfLengthBetween(1, 10), randomAlphaOfLengthBetween(1, 10),
+            randomAlphaOfLengthBetween(1, 10));
+        List<Step> steps = action.toSteps(null, phase, nextStepKey);
+        BranchingStep step = ((BranchingStep) steps.get(0));
+
+        LifecyclePolicy policy = new LifecyclePolicy(lifecycleName, Collections.singletonMap("warm",
+                new Phase("warm", TimeValue.ZERO, Collections.singletonMap(action.getWriteableName(), action))));
+        LifecyclePolicyMetadata policyMetadata = new LifecyclePolicyMetadata(policy, Collections.emptyMap(),
+            randomNonNegativeLong(), randomNonNegativeLong());
+        String indexName = randomAlphaOfLength(5);
+        ClusterState state = ClusterState.builder(ClusterName.DEFAULT).metaData(MetaData.builder()
+            .putCustom(IndexLifecycleMetadata.TYPE, new IndexLifecycleMetadata(
+                Collections.singletonMap(policyMetadata.getName(), policyMetadata), OperationMode.RUNNING))
+            .put(IndexMetaData.builder(indexName).settings(settings(Version.CURRENT).put(LifecycleSettings.LIFECYCLE_NAME, lifecycleName))
+                .putCustom(LifecycleExecutionState.ILM_CUSTOM_METADATA_KEY,
+                    LifecycleExecutionState.builder()
+                        .setPhase(step.getKey().getPhase())
+                        .setPhaseTime(0L)
+                        .setAction(step.getKey().getAction())
+                        .setActionTime(0L)
+                        .setStep(step.getKey().getName())
+                        .setStepTime(0L)
+                        .build().asMap())
+                .numberOfShards(numberOfShards).numberOfReplicas(0))).build();
+        step.performAction(state.metaData().index(indexName).getIndex(), state);
+        assertThat(step.getNextStepKey(), equalTo(nextStepKey));
+    }
+
+    public void testPerformActionWithoutSkip() {
+        int numShards = 6;
+        int divisor = randomFrom(2, 3, 6);
+        int expectedFinalShards = numShards / divisor;
+        String lifecycleName = randomAlphaOfLengthBetween(4, 10);
+        ShrinkAction action = new ShrinkAction(expectedFinalShards);
+        String phase = randomAlphaOfLengthBetween(1, 10);
+        StepKey nextStepKey = new StepKey(randomAlphaOfLengthBetween(1, 10), randomAlphaOfLengthBetween(1, 10),
+            randomAlphaOfLengthBetween(1, 10));
+        List<Step> steps = action.toSteps(null, phase, nextStepKey);
+        BranchingStep step = ((BranchingStep) steps.get(0));
+
+        LifecyclePolicy policy = new LifecyclePolicy(lifecycleName, Collections.singletonMap("warm",
+            new Phase("warm", TimeValue.ZERO, Collections.singletonMap(action.getWriteableName(), action))));
+        LifecyclePolicyMetadata policyMetadata = new LifecyclePolicyMetadata(policy, Collections.emptyMap(),
+            randomNonNegativeLong(), randomNonNegativeLong());
+        String indexName = randomAlphaOfLength(5);
+        ClusterState state = ClusterState.builder(ClusterName.DEFAULT).metaData(MetaData.builder()
+            .putCustom(IndexLifecycleMetadata.TYPE, new IndexLifecycleMetadata(
+                Collections.singletonMap(policyMetadata.getName(), policyMetadata), OperationMode.RUNNING))
+            .put(IndexMetaData.builder(indexName).settings(settings(Version.CURRENT).put(LifecycleSettings.LIFECYCLE_NAME, lifecycleName))
+                .putCustom(LifecycleExecutionState.ILM_CUSTOM_METADATA_KEY,
+                    LifecycleExecutionState.builder()
+                        .setPhase(step.getKey().getPhase())
+                        .setPhaseTime(0L)
+                        .setAction(step.getKey().getAction())
+                        .setActionTime(0L)
+                        .setStep(step.getKey().getName())
+                        .setStepTime(0L)
+                        .build().asMap())
+                .numberOfShards(numShards).numberOfReplicas(0))).build();
+        ClusterState newState = step.performAction(state.metaData().index(indexName).getIndex(), state);
+        assertThat(step.getNextStepKey(), equalTo(steps.get(1).getKey()));
+    }
+
     public void testToSteps() {
         ShrinkAction action = createTestInstance();
         String phase = randomAlphaOfLengthBetween(1, 10);
         StepKey nextStepKey = new StepKey(randomAlphaOfLengthBetween(1, 10), randomAlphaOfLengthBetween(1, 10),
             randomAlphaOfLengthBetween(1, 10));
         List<Step> steps = action.toSteps(null, phase, nextStepKey);
-        assertThat(steps.size(), equalTo(8));
-        StepKey expectedFirstKey = new StepKey(phase, ShrinkAction.NAME, ReadOnlyAction.NAME);
-        StepKey expectedSecondKey = new StepKey(phase, ShrinkAction.NAME, SetSingleNodeAllocateStep.NAME);
-        StepKey expectedThirdKey = new StepKey(phase, ShrinkAction.NAME, CheckShrinkReadyStep.NAME);
-        StepKey expectedFourthKey = new StepKey(phase, ShrinkAction.NAME, ShrinkStep.NAME);
-        StepKey expectedFifthKey = new StepKey(phase, ShrinkAction.NAME, ShrunkShardsAllocatedStep.NAME);
-        StepKey expectedSixthKey = new StepKey(phase, ShrinkAction.NAME, CopyExecutionStateStep.NAME);
-        StepKey expectedSeventhKey = new StepKey(phase, ShrinkAction.NAME, ShrinkSetAliasStep.NAME);
-        StepKey expectedEighthKey = new StepKey(phase, ShrinkAction.NAME, ShrunkenIndexCheckStep.NAME);
+        assertThat(steps.size(), equalTo(9));
+        StepKey expectedFirstKey = new StepKey(phase, ShrinkAction.NAME, BranchingStep.NAME);
+        StepKey expectedSecondKey = new StepKey(phase, ShrinkAction.NAME, ReadOnlyAction.NAME);
+        StepKey expectedThirdKey = new StepKey(phase, ShrinkAction.NAME, SetSingleNodeAllocateStep.NAME);
+        StepKey expectedFourthKey = new StepKey(phase, ShrinkAction.NAME, CheckShrinkReadyStep.NAME);
+        StepKey expectedFifthKey = new StepKey(phase, ShrinkAction.NAME, ShrinkStep.NAME);
+        StepKey expectedSixthKey = new StepKey(phase, ShrinkAction.NAME, ShrunkShardsAllocatedStep.NAME);
+        StepKey expectedSeventhKey = new StepKey(phase, ShrinkAction.NAME, CopyExecutionStateStep.NAME);
+        StepKey expectedEighthKey = new StepKey(phase, ShrinkAction.NAME, ShrinkSetAliasStep.NAME);
+        StepKey expectedNinthKey = new StepKey(phase, ShrinkAction.NAME, ShrunkenIndexCheckStep.NAME);
 
-        assertTrue(steps.get(0) instanceof UpdateSettingsStep);
+        assertTrue(steps.get(0) instanceof BranchingStep);
         assertThat(steps.get(0).getKey(), equalTo(expectedFirstKey));
-        assertThat(steps.get(0).getNextStepKey(), equalTo(expectedSecondKey));
-        assertTrue(IndexMetaData.INDEX_BLOCKS_WRITE_SETTING.get(((UpdateSettingsStep)steps.get(0)).getSettings()));
+        expectThrows(UnsupportedOperationException.class, () -> steps.get(0).getNextStepKey());
+        assertThat(((BranchingStep) steps.get(0)).getNextStepKeyOnFalse(), equalTo(expectedSecondKey));
+        assertThat(((BranchingStep) steps.get(0)).getNextStepKeyOnTrue(), equalTo(nextStepKey));
 
-        assertTrue(steps.get(1) instanceof SetSingleNodeAllocateStep);
+        assertTrue(steps.get(1) instanceof UpdateSettingsStep);
         assertThat(steps.get(1).getKey(), equalTo(expectedSecondKey));
         assertThat(steps.get(1).getNextStepKey(), equalTo(expectedThirdKey));
+        assertTrue(IndexMetaData.INDEX_BLOCKS_WRITE_SETTING.get(((UpdateSettingsStep)steps.get(1)).getSettings()));
 
-        assertTrue(steps.get(2) instanceof CheckShrinkReadyStep);
+        assertTrue(steps.get(2) instanceof SetSingleNodeAllocateStep);
         assertThat(steps.get(2).getKey(), equalTo(expectedThirdKey));
         assertThat(steps.get(2).getNextStepKey(), equalTo(expectedFourthKey));
 
-        assertTrue(steps.get(3) instanceof ShrinkStep);
+        assertTrue(steps.get(3) instanceof CheckShrinkReadyStep);
         assertThat(steps.get(3).getKey(), equalTo(expectedFourthKey));
         assertThat(steps.get(3).getNextStepKey(), equalTo(expectedFifthKey));
-        assertThat(((ShrinkStep) steps.get(3)).getShrunkIndexPrefix(), equalTo(ShrinkAction.SHRUNKEN_INDEX_PREFIX));
 
-        assertTrue(steps.get(4) instanceof ShrunkShardsAllocatedStep);
+        assertTrue(steps.get(4) instanceof ShrinkStep);
         assertThat(steps.get(4).getKey(), equalTo(expectedFifthKey));
         assertThat(steps.get(4).getNextStepKey(), equalTo(expectedSixthKey));
-        assertThat(((ShrunkShardsAllocatedStep) steps.get(4)).getShrunkIndexPrefix(), equalTo(ShrinkAction.SHRUNKEN_INDEX_PREFIX));
+        assertThat(((ShrinkStep) steps.get(4)).getShrunkIndexPrefix(), equalTo(ShrinkAction.SHRUNKEN_INDEX_PREFIX));
 
-        assertTrue(steps.get(5) instanceof CopyExecutionStateStep);
+        assertTrue(steps.get(5) instanceof ShrunkShardsAllocatedStep);
         assertThat(steps.get(5).getKey(), equalTo(expectedSixthKey));
         assertThat(steps.get(5).getNextStepKey(), equalTo(expectedSeventhKey));
-        assertThat(((CopyExecutionStateStep) steps.get(5)).getShrunkIndexPrefix(), equalTo(ShrinkAction.SHRUNKEN_INDEX_PREFIX));
+        assertThat(((ShrunkShardsAllocatedStep) steps.get(5)).getShrunkIndexPrefix(), equalTo(ShrinkAction.SHRUNKEN_INDEX_PREFIX));
 
-        assertTrue(steps.get(6) instanceof ShrinkSetAliasStep);
+        assertTrue(steps.get(6) instanceof CopyExecutionStateStep);
         assertThat(steps.get(6).getKey(), equalTo(expectedSeventhKey));
         assertThat(steps.get(6).getNextStepKey(), equalTo(expectedEighthKey));
-        assertThat(((ShrinkSetAliasStep) steps.get(6)).getShrunkIndexPrefix(), equalTo(ShrinkAction.SHRUNKEN_INDEX_PREFIX));
+        assertThat(((CopyExecutionStateStep) steps.get(6)).getShrunkIndexPrefix(), equalTo(ShrinkAction.SHRUNKEN_INDEX_PREFIX));
 
-        assertTrue(steps.get(7) instanceof ShrunkenIndexCheckStep);
+        assertTrue(steps.get(7) instanceof ShrinkSetAliasStep);
         assertThat(steps.get(7).getKey(), equalTo(expectedEighthKey));
-        assertThat(steps.get(7).getNextStepKey(), equalTo(nextStepKey));
-        assertThat(((ShrunkenIndexCheckStep) steps.get(7)).getShrunkIndexPrefix(), equalTo(ShrinkAction.SHRUNKEN_INDEX_PREFIX));
+        assertThat(steps.get(7).getNextStepKey(), equalTo(expectedNinthKey));
+        assertThat(((ShrinkSetAliasStep) steps.get(7)).getShrunkIndexPrefix(), equalTo(ShrinkAction.SHRUNKEN_INDEX_PREFIX));
+
+        assertTrue(steps.get(8) instanceof ShrunkenIndexCheckStep);
+        assertThat(steps.get(8).getKey(), equalTo(expectedNinthKey));
+        assertThat(steps.get(8).getNextStepKey(), equalTo(nextStepKey));
+        assertThat(((ShrunkenIndexCheckStep) steps.get(8)).getShrunkIndexPrefix(), equalTo(ShrinkAction.SHRUNKEN_INDEX_PREFIX));
     }
 
     @Override
--- x-pack/plugin/ilm/src/main/java/org/elasticsearch/xpack/indexlifecycle/ExecuteStepsUpdateTask.java
+++ x-pack/plugin/ilm/src/main/java/org/elasticsearch/xpack/indexlifecycle/ExecuteStepsUpdateTask.java
@@ -86,12 +86,11 @@ public class ExecuteStepsUpdateTask extends ClusterStateUpdateTask {
             // either get to a step that isn't a cluster state step or a
             // cluster state wait step returns not completed
             while (currentStep instanceof ClusterStateActionStep || currentStep instanceof ClusterStateWaitStep) {
-                nextStepKey = currentStep.getNextStepKey();
                 if (currentStep instanceof ClusterStateActionStep) {
                     // cluster state action step so do the action and
                     // move the cluster state to the next step
-                    logger.trace("[{}] performing cluster state action ({}) [{}], next: [{}]",
-                        index.getName(), currentStep.getClass().getSimpleName(), currentStep.getKey(), currentStep.getNextStepKey());
+                    logger.trace("[{}] performing cluster state action ({}) [{}]",
+                        index.getName(), currentStep.getClass().getSimpleName(), currentStep.getKey());
                     try {
                         state = ((ClusterStateActionStep) currentStep).performAction(index, state);
                     } catch (Exception exception) {
@@ -102,8 +101,14 @@ public class ExecuteStepsUpdateTask extends ClusterStateUpdateTask {
                     } else {
                         state = IndexLifecycleRunner.moveClusterStateToNextStep(index, state, currentStep.getKey(),
                             currentStep.getNextStepKey(), nowSupplier, false);
+                        // set here to make sure that the clusterProcessed knows to execute the
+                        // correct step if it an async action
+                        nextStepKey = currentStep.getNextStepKey();
                     }
                 } else {
+                    // set here to make sure that the clusterProcessed knows to execute the
+                    // correct step if it an async action
+                    nextStepKey = currentStep.getNextStepKey();
                     // cluster state wait step so evaluate the
                     // condition, if the condition is met move to the
                     // next step, if its not met return the current

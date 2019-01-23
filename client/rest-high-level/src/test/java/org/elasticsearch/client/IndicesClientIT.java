2d3e0802155a7f7baf00cf2c48375a15bdfa6907
Christoph Büscher <cbuescher@posteo.de>
2019-01-23 22:26:07 +0100

    Correct indention

--- client/rest-high-level/src/test/java/org/elasticsearch/client/IndicesClientIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/IndicesClientIT.java
@@ -176,18 +176,15 @@ public class IndicesClientIT extends ESRestHighLevelClientTestCase {
 
     public void testIndicesExistsWithTypes() throws IOException {
         // Index present
-        {
-            String indexName = "test_index_exists_index_present";
-            createIndex(indexName, Settings.EMPTY);
+        String indexName = "test_index_exists_index_present";
+        createIndex(indexName, Settings.EMPTY);
 
-            org.elasticsearch.action.admin.indices.get.GetIndexRequest request =
-                    new org.elasticsearch.action.admin.indices.get.GetIndexRequest();
-            request.indices(indexName);
+        org.elasticsearch.action.admin.indices.get.GetIndexRequest request = new org.elasticsearch.action.admin.indices.get.GetIndexRequest();
+        request.indices(indexName);
 
-            boolean response = execute(request, highLevelClient().indices()::exists, highLevelClient().indices()::existsAsync,
-                    expectWarnings(RestGetIndicesAction.TYPES_DEPRECATION_MESSAGE));
-            assertTrue(response);
-        }
+        boolean response = execute(request, highLevelClient().indices()::exists, highLevelClient().indices()::existsAsync,
+                expectWarnings(RestGetIndicesAction.TYPES_DEPRECATION_MESSAGE));
+        assertTrue(response);
     }
 
     @SuppressWarnings({"unchecked", "rawtypes"})

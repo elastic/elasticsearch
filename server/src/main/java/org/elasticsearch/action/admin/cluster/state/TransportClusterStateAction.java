de6f242a87a18a217d0a07f45d684ef4f7409aec
David Turner <david.turner@elastic.co>
2019-01-24 09:45:52 +0000

    Expose minimum_master_nodes in cluster state
    To safely support rolling upgrades from 6.x to 7.x we need the 7.x nodes to
    have access to the `minimum_master_nodes` setting, but this setting is
    otherwise unnecessary in 7.x and we would like to remove it.  Since a rolling
    upgrade from 6.x to 7.x involves the 7.x nodes joining a 6.x master, we can
    avoid the need for setting `minimum_master_nodes` on the 7.x nodes by copying
    the value set on the 6.x master.
    
    This change exposes the master's node-level value for `minimum_master_nodes`
    via a field in the cluster state.

--- server/src/main/java/org/elasticsearch/action/admin/cluster/state/TransportClusterStateAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/state/TransportClusterStateAction.java
@@ -128,6 +128,8 @@ public class TransportClusterStateAction extends TransportMasterNodeReadAction<C
         ClusterState.Builder builder = ClusterState.builder(currentState.getClusterName());
         builder.version(currentState.version());
         builder.stateUUID(currentState.stateUUID());
+        builder.minimumMasterNodesOnPublishingMaster(currentState.getMinimumMasterNodesOnPublishingMaster());
+
         if (request.nodes()) {
             builder.nodes(currentState.nodes());
         }

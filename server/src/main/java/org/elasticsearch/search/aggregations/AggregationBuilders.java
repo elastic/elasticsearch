778452da824c4c2ffe552cbbe15ccb71fe3362c7
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2019-01-28 13:48:00 -0500

    Add geotile_grid test to ShardReduceIT

--- server/src/main/java/org/elasticsearch/search/aggregations/AggregationBuilders.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/AggregationBuilders.java
@@ -255,7 +255,7 @@ public class AggregationBuilders {
     /**
      * Create a new {@link InternalGeoTileGrid} aggregation with the given name.
      */
-    public static GeoTileGridAggregationBuilder geoTileGrid(String name) {
+    public static GeoTileGridAggregationBuilder geotileGrid(String name) {
         return new GeoTileGridAggregationBuilder(name);
     }
 

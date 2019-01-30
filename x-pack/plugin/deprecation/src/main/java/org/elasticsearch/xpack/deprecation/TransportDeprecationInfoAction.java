58a76c27a9ccc0f4d97517c11b3cb797009c8e4a
David Kyle <david.kyle@elastic.co>
2019-01-30 06:45:21 +0000

    [ML] Datafeed deprecation checks (#37932)
    Deprecation checks for the ML datafeed
    query and aggregations.

--- x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/TransportDeprecationInfoAction.java
+++ x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/TransportDeprecationInfoAction.java
@@ -26,13 +26,20 @@ import org.elasticsearch.license.XPackLicenseState;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 import org.elasticsearch.xpack.core.XPackField;
+import org.elasticsearch.xpack.core.XPackSettings;
 import org.elasticsearch.xpack.core.deprecation.DeprecationInfoAction;
+import org.elasticsearch.xpack.core.ml.action.GetDatafeedsAction;
+import org.elasticsearch.xpack.core.ml.datafeed.DatafeedConfig;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.elasticsearch.xpack.core.ClientHelper.DEPRECATION_ORIGIN;
 import static org.elasticsearch.xpack.core.ClientHelper.executeAsyncWithOrigin;
 import static org.elasticsearch.xpack.deprecation.DeprecationChecks.CLUSTER_SETTINGS_CHECKS;
 import static org.elasticsearch.xpack.deprecation.DeprecationChecks.INDEX_SETTINGS_CHECKS;
 import static org.elasticsearch.xpack.deprecation.DeprecationChecks.NODE_SETTINGS_CHECKS;
+import static org.elasticsearch.xpack.deprecation.DeprecationChecks.ML_SETTINGS_CHECKS;
 
 public class TransportDeprecationInfoAction extends TransportMasterNodeReadAction<DeprecationInfoAction.Request,
         DeprecationInfoAction.Response> {
@@ -40,6 +47,7 @@ public class TransportDeprecationInfoAction extends TransportMasterNodeReadActio
     private final XPackLicenseState licenseState;
     private final NodeClient client;
     private final IndexNameExpressionResolver indexNameExpressionResolver;
+    private final Settings settings;
 
     @Inject
     public TransportDeprecationInfoAction(Settings settings, TransportService transportService,
@@ -52,6 +60,7 @@ public class TransportDeprecationInfoAction extends TransportMasterNodeReadActio
         this.licenseState = licenseState;
         this.client = client;
         this.indexNameExpressionResolver = indexNameExpressionResolver;
+        this.settings = settings;
     }
 
     @Override
@@ -90,17 +99,34 @@ public class TransportDeprecationInfoAction extends TransportMasterNodeReadActio
                                             if (nodesStatsResponse.hasFailures()) {
                                                 throw nodesStatsResponse.failures().get(0);
                                             }
-                                            listener.onResponse(
-                                                    DeprecationInfoAction.Response.from(nodesInfoResponse.getNodes(),
-                                                    nodesStatsResponse.getNodes(), state, indexNameExpressionResolver,
-                                                    request.indices(), request.indicesOptions(),
-                                                    CLUSTER_SETTINGS_CHECKS, NODE_SETTINGS_CHECKS,
-                                                    INDEX_SETTINGS_CHECKS));
+                                            getDatafeedConfigs(ActionListener.wrap(
+                                                    datafeeds -> {
+                                                        listener.onResponse(
+                                                                DeprecationInfoAction.Response.from(nodesInfoResponse.getNodes(),
+                                                                        nodesStatsResponse.getNodes(), state, indexNameExpressionResolver,
+                                                                        request.indices(), request.indicesOptions(), datafeeds,
+                                                                        CLUSTER_SETTINGS_CHECKS, NODE_SETTINGS_CHECKS,
+                                                                        INDEX_SETTINGS_CHECKS, ML_SETTINGS_CHECKS));
+                                                    },
+                                                    listener::onFailure
+                                            ));
                                         }, listener::onFailure),
                                 client.admin().cluster()::nodesStats);
                     }, listener::onFailure), client.admin().cluster()::nodesInfo);
         } else {
             listener.onFailure(LicenseUtils.newComplianceException(XPackField.DEPRECATION));
+        }
+    }
+
+    private void getDatafeedConfigs(ActionListener<List<DatafeedConfig>> listener) {
+        if (XPackSettings.MACHINE_LEARNING_ENABLED.get(settings) == false) {
+            listener.onResponse(Collections.emptyList());
+        } else {
+            executeAsyncWithOrigin(client, DEPRECATION_ORIGIN, GetDatafeedsAction.INSTANCE,
+                    new GetDatafeedsAction.Request(GetDatafeedsAction.ALL), ActionListener.wrap(
+                            datafeedsResponse -> listener.onResponse(datafeedsResponse.getResponse().results()),
+                            listener::onFailure
+                    ));
         }
     }
 }

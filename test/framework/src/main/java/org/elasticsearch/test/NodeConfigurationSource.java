c6f40dfd36c74f5a520fd2732669cf12fc062013
Andrey Ershov <andrey.ershov@elastic.co>
2019-01-31 13:45:14 +0100

    Remove unused import

--- test/framework/src/main/java/org/elasticsearch/test/NodeConfigurationSource.java
+++ test/framework/src/main/java/org/elasticsearch/test/NodeConfigurationSource.java
@@ -24,7 +24,6 @@ import org.elasticsearch.plugins.Plugin;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 public abstract class NodeConfigurationSource {
 

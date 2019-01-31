1d5ec917108ab8d7412a81eaa084123163d926af
Julie Tibshirani <julie.tibshirani@elastic.co>
2019-01-31 11:53:03 -0800

    Update to the latest deprecation message for 6.7.

--- server/src/main/java/org/elasticsearch/rest/action/admin/indices/RestCreateIndexAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/admin/indices/RestCreateIndexAction.java
@@ -43,9 +43,9 @@ public class RestCreateIndexAction extends BaseRestHandler {
         LogManager.getLogger(RestCreateIndexAction.class));
     public static final String TYPES_DEPRECATION_MESSAGE = "[types removal] Using include_type_name in create " +
         "index requests is deprecated. The parameter will be removed in the next major version.";
-    public static final String TYPES_DEPRECATION_MESSAGE_6_7_0 = "[types removal] Specifying types in create " +
-        "index requests is deprecated. The parameter include_type_name should be provided and set to false to be " +
-        "compatible with the next major version.";
+    public static final String TYPES_DEPRECATION_MESSAGE_6_7_0 = "[types removal] Specifying types in create index " +
+        "requests is deprecated. To be compatible with 7.0, the mapping definition should not be nested under " +
+        "the type name, and the parameter include_type_name must be provided and set to false.";
 
     public RestCreateIndexAction(Settings settings, RestController controller) {
         super(settings);

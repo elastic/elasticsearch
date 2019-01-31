7bd68eb3b125f84840b2e9ef2aebbd7d678b0a8d
Henning Andersen <henning.andersen@elastic.co>
2019-01-31 15:19:49 +0100

    Warn about left-behind data or metadata
    Fix checkstyle issue.

--- server/src/test/java/org/elasticsearch/env/NodeEnvironmentTests.java
+++ server/src/test/java/org/elasticsearch/env/NodeEnvironmentTests.java
@@ -495,7 +495,8 @@ public class NodeEnvironmentTests extends ESTestCase {
             @Override
             public void append(LogEvent event) {
                 if (event.getLevel() == Level.WARN
-                    && event.getMessage().getFormattedMessage().endsWith(", this should be cleaned up (will refuse to start in 7.0). Beware of data-loss.")) {
+                    && event.getMessage().getFormattedMessage()
+                    .endsWith(", this should be cleaned up (will refuse to start in 7.0). Beware of data-loss.")) {
                     throw new LoggingException(new IllegalStateException(event.getMessage().getFormattedMessage()));
                 }
             }

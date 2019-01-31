4267b8fa76b8afb1e9314b412179e1791a9343cf
Julie Tibshirani <julie.tibshirani@elastic.co>
2019-01-31 11:50:52 -0800

    Improve the deprecation messages around include_type_name. (#38052)
    This PR standardizes the message across the different indices APIs and makes
    sure to mention that mappings should no longer be nested under the type name.

--- server/src/main/java/org/elasticsearch/rest/action/admin/indices/RestCreateIndexAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/admin/indices/RestCreateIndexAction.java
@@ -42,8 +42,8 @@ public class RestCreateIndexAction extends BaseRestHandler {
     private static final DeprecationLogger deprecationLogger = new DeprecationLogger(
         LogManager.getLogger(RestPutMappingAction.class));
     public static final String TYPES_DEPRECATION_MESSAGE = "[types removal] Specifying types in create index " +
-        "requests is deprecated. The parameter include_type_name should be provided and set to false to be " +
-        "compatible with the next major version.";
+        "requests is deprecated. To be compatible with 7.0, the mapping definition should not be nested under " +
+        "the type name, and the parameter include_type_name must be provided and set to false.";
 
     public RestCreateIndexAction(Settings settings, RestController controller) {
         super(settings);

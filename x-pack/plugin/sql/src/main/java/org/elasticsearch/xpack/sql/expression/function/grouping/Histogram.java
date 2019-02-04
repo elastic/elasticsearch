openeuler-mirror/poissonsearch-oss

SQL: Generate relevant error message when grouping functions are not used in GROUP BY (#38017)
* Add checks for Grouping functions restriction to be placed inside GROUP BY
* Fixed bug where GROUP BY HISTOGRAM (not using alias) wasn't recognized
properly in the Verifier due to functions equality not working correctly.

x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/expression/function/grouping/Histogram.java
@@ -10,12 +10,14 @@ import org.elasticsearch.xpack.sql.expression.Expression;
 import org.elasticsearch.xpack.sql.expression.Expressions;
 import org.elasticsearch.xpack.sql.expression.Expressions.ParamOrdinal;
 import org.elasticsearch.xpack.sql.expression.Literal;
-import org.elasticsearch.xpack.sql.tree.Source;
 import org.elasticsearch.xpack.sql.tree.NodeInfo;
+import org.elasticsearch.xpack.sql.tree.Source;
 import org.elasticsearch.xpack.sql.type.DataType;
 import org.elasticsearch.xpack.sql.type.DataTypes;
 
 import java.time.ZoneId;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 public class Histogram extends GroupingFunction {
@@ -24,8 +26,8 @@ public class Histogram extends GroupingFunction {
     private final ZoneId zoneId;
 
     public Histogram(Source source, Expression field, Expression interval, ZoneId zoneId) {
-        super(source, field);
-        this.interval = (Literal) interval;
+        super(source, field, Collections.singletonList(interval));
+        this.interval = Literal.of(interval);
         this.zoneId = zoneId;
     }
 
@@ -51,10 +53,13 @@ public class Histogram extends GroupingFunction {
 
         return resolution;
     }
-
+    
     @Override
-    protected GroupingFunction replaceChild(Expression newChild) {
-        return new Histogram(source(), newChild, interval, zoneId);
+    public final GroupingFunction replaceChildren(List<Expression> newChildren) {
+        if (newChildren.size() != 2) {
+            throw new IllegalArgumentException("expected [2] children but received [" + newChildren.size() + "]");
+        }
+        return new Histogram(source(), newChildren.get(0), newChildren.get(1), zoneId);
     }
 
     @Override

elastic/elasticsearch

(cherry picked from commit 6968f0925b3304a66516916cf69d4bcb9749ea30)

x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/expression/function/grouping/Histogram.java
@@ -10,12 +10,14 @@ import org.elasticsearch.xpack.sql.expression.Expression;
 import org.elasticsearch.xpack.sql.expression.Expressions;
 import org.elasticsearch.xpack.sql.expression.Expressions.ParamOrdinal;
 import org.elasticsearch.xpack.sql.expression.Literal;
-import org.elasticsearch.xpack.sql.tree.Location;
 import org.elasticsearch.xpack.sql.tree.NodeInfo;
+import org.elasticsearch.xpack.sql.tree.Location;
 import org.elasticsearch.xpack.sql.type.DataType;
 import org.elasticsearch.xpack.sql.type.DataTypes;
 
 import java.time.ZoneId;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 public class Histogram extends GroupingFunction {
@@ -24,8 +26,8 @@ public class Histogram extends GroupingFunction {
     private final ZoneId zoneId;
 
     public Histogram(Location location, Expression field, Expression interval, ZoneId zoneId) {
-        super(location, field);
-        this.interval = (Literal) interval;
+        super(location, field, Collections.singletonList(interval));
+        this.interval = Literal.of(interval);
         this.zoneId = zoneId;
     }
 
@@ -51,10 +53,13 @@ public class Histogram extends GroupingFunction {
 
         return resolution;
     }
-
+    
     @Override
-    protected GroupingFunction replaceChild(Expression newChild) {
-        return new Histogram(location(), newChild, interval, zoneId);
+    public final GroupingFunction replaceChildren(List<Expression> newChildren) {
+        if (newChildren.size() != 2) {
+            throw new IllegalArgumentException("expected [2] children but received [" + newChildren.size() + "]");
+        }
+        return new Histogram(location(), newChildren.get(0), newChildren.get(1), zoneId);
     }
 
     @Override
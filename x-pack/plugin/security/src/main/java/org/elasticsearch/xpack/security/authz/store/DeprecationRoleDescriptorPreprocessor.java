ce25f2f72aa625107ff171cf6ed047f0fa83654a
Albert Zaharovits <albert.zaharovits@elastic.co>
2019-02-04 11:46:04 +0200

    Replace client.getAlias with clusterService.metadata()

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authz/store/DeprecationRoleDescriptorPreprocessor.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authz/store/DeprecationRoleDescriptorPreprocessor.java
@@ -6,80 +6,64 @@
 
 package org.elasticsearch.xpack.security.authz.store;
 
-import com.carrotsearch.hppc.cursors.ObjectObjectCursor;
-
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.action.admin.indices.alias.get.GetAliasesAction;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.cluster.metadata.AliasMetaData;
-import org.elasticsearch.common.collect.ImmutableOpenMap;
+import org.elasticsearch.cluster.metadata.AliasOrIndex;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
+import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.logging.DeprecationLogger;
 import org.elasticsearch.xpack.core.security.authz.RoleDescriptor;
 import org.elasticsearch.xpack.core.security.authz.RoleDescriptor.IndicesPrivileges;
 import org.elasticsearch.xpack.core.security.authz.permission.IndicesPermission;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
 import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 
-import static org.elasticsearch.xpack.core.ClientHelper.SECURITY_ORIGIN;
-import static org.elasticsearch.xpack.core.ClientHelper.executeAsyncWithOrigin;
-
 public final class DeprecationRoleDescriptorPreprocessor implements BiConsumer<Set<RoleDescriptor>, ActionListener<Set<RoleDescriptor>>> {
 
-    private final Logger logger;
     private final DeprecationLogger deprecationLogger;
-    private final Client client;
+    private final ClusterService clusterService;
 
-    public DeprecationRoleDescriptorPreprocessor(Client client, Logger logger) {
-        this.logger = logger;
+    public DeprecationRoleDescriptorPreprocessor(ClusterService clusterService, Logger logger) {
         this.deprecationLogger = new DeprecationLogger(logger);
-        this.client = client;
+        this.clusterService = clusterService;
     }
 
     @Override
     public void accept(Set<RoleDescriptor> effectiveRoleDescriptors, ActionListener<Set<RoleDescriptor>> roleBuilder) {
-        executeAsyncWithOrigin(client, SECURITY_ORIGIN, GetAliasesAction.INSTANCE,
-                client.admin().indices().prepareGetAliases("_all").request(), ActionListener.wrap(getAliasesResponse -> {
-                    final ImmutableOpenMap<String, List<AliasMetaData>> allAliasesMap = getAliasesResponse.getAliases();
-                    // iterate over all effective role descriptors and their indices privileges
-                    for (final RoleDescriptor roleDescriptor : effectiveRoleDescriptors) {
-                        for (final IndicesPrivileges indicesPrivilege : roleDescriptor.getIndicesPrivileges()) {
-                            final Predicate<String> indexNamePatternPredicate = IndicesPermission
-                                    .indexMatcher(Arrays.asList(indicesPrivilege.getIndices()));
-                            // iterate over all aliases
-                            for (final ObjectObjectCursor<String, List<AliasMetaData>> cursor : allAliasesMap) {
-                                final String indexName = cursor.key;
-                                // the privilege does not match this index
-                                if (false == indexNamePatternPredicate.test(indexName)) {
-                                    for (final AliasMetaData aliasMetaData : cursor.value) {
-                                        final String aliasName = aliasMetaData.alias();
-                                        // but the privilege matches the alias pointing to the index
-                                        if (indexNamePatternPredicate.test(aliasName)) {
-                                            deprecationLogger.deprecated(
-                                                    "Role [{}] grants index privileges over the [{}] alias"
-                                                            + " and not over the [{}] index. Granting privileges over an alias"
-                                                            + " and hence granting privileges over all the indices that it points to"
-                                                            + " is deprecated and will be removed in a future version of Elasticsearch."
-                                                            + " Instead define permissions exclusively on indices or index patterns.",
-                                                    roleDescriptor.getName(), aliasName, indexName);
-                                        }
-                                    }
+        final SortedMap<String, AliasOrIndex> aliasOrIndexMap = clusterService.state().metaData().getAliasAndIndexLookup();
+        // iterate on indices privileges of all effective role descriptors
+        for (final RoleDescriptor roleDescriptor : effectiveRoleDescriptors) {
+            for (final IndicesPrivileges indicesPrivilege : roleDescriptor.getIndicesPrivileges()) {
+                final Predicate<String> namePatternPredicate = IndicesPermission.indexMatcher(Arrays.asList(indicesPrivilege.getIndices()));
+                // iterate over all current aliases
+                for (final Map.Entry<String, AliasOrIndex> aliasOrIndex : aliasOrIndexMap.entrySet()) {
+                    if (aliasOrIndex.getValue().isAlias()) {
+                        final AliasOrIndex.Alias alias = ((AliasOrIndex.Alias) aliasOrIndex);
+                        // the privilege name pattern matches the alias name
+                        if (namePatternPredicate.test(alias.getAliasName())) {
+                            for (final IndexMetaData indexMeta : alias.getIndices()) {
+                                // but it does not match the name of an index pointed to by the alias
+                                if (false == namePatternPredicate.test(indexMeta.getIndex().getName())) {
+                                    deprecationLogger.deprecated(
+                                            "Role [{}] grants index privileges over the [{}] alias"
+                                                    + " and not over the [{}] index. Granting privileges over an alias"
+                                                    + " and hence granting privileges over all the indices that it points to"
+                                                    + " is deprecated and will be removed in a future version of Elasticsearch."
+                                                    + " Instead define permissions exclusively on indices or index patterns.",
+                                            roleDescriptor.getName(), alias.getAliasName(), indexMeta.getIndex().getName());
                                 }
                             }
                         }
                     }
-                    // forward the effective role descriptors untouched to the builder
-                    roleBuilder.onResponse(effectiveRoleDescriptors);
-                }, e -> {
-                    // swallow exception: role building is not hindered by an exception in the deprecation logging logic
-                    logger.debug("Swallowed exception encountered while checking Role Descriptors for deprecated features.", e);
-                    // forward the effective role descriptors untouched to the builder
-                    roleBuilder.onResponse(effectiveRoleDescriptors);
-                }));
+                }
+            }
+        }
+        // forward the effective role descriptors untouched to the builder
+        roleBuilder.onResponse(effectiveRoleDescriptors);
     }
-
 }

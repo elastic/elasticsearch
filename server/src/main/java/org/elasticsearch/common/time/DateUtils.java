f6acf7ca5a9773ed61a9d18e9a360695f7a757e8
Alexander Reelsen <alexander@reelsen.net>
2019-02-04 15:12:34 +0100

    split joda time code into separate class

--- server/src/main/java/org/elasticsearch/common/time/DateUtils.java
+++ server/src/main/java/org/elasticsearch/common/time/DateUtils.java
@@ -31,6 +31,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import static org.elasticsearch.common.time.DateUtilsRounding.getMonthOfYear;
+import static org.elasticsearch.common.time.DateUtilsRounding.getTotalMillisByYearMonth;
+import static org.elasticsearch.common.time.DateUtilsRounding.getYear;
+import static org.elasticsearch.common.time.DateUtilsRounding.utcMillisAtStartOfYear;
+
 public class DateUtils {
     public static DateTimeZone zoneIdToDateTimeZone(ZoneId zoneId) {
         if (zoneId == null) {
@@ -165,8 +170,8 @@ public class DateUtils {
      * @return The milliseconds since the epoch rounded down to the quarter of the year
      */
     public static long roundQuarterOfYear(final long utcMillis) {
-        int year = DateUtils.getYear(utcMillis);
-        int month = DateUtils.getMonthOfYear(utcMillis, year);
+        int year = getYear(utcMillis);
+        int month = getMonthOfYear(utcMillis, year);
         int firstMonthOfQuarter = (((month-1) / 3) * 3) + 1;
         return DateUtils.of(year, firstMonthOfQuarter);
     }
@@ -177,8 +182,8 @@ public class DateUtils {
      * @return The milliseconds since the epoch rounded down to the month of the year
      */
     public static long roundMonthOfYear(final long utcMillis) {
-        int year = DateUtils.getYear(utcMillis);
-        int month = DateUtils.getMonthOfYear(utcMillis, year);
+        int year = getYear(utcMillis);
+        int month = getMonthOfYear(utcMillis, year);
         return DateUtils.of(year, month);
     }
 
@@ -211,157 +216,5 @@ public class DateUtils {
         long millis = utcMillisAtStartOfYear(year);
         millis += getTotalMillisByYearMonth(year, month);
         return millis;
-    }
-
-    /*
-     * begin of code that is partially copied from the joda time implementation in order to make calculations about utc rounding much
-     * faster than using java-time and assigning all those objects
-     *
-     */
-    private static final int DAYS_0000_TO_1970 = 719527;
-    private static final int MILLIS_PER_DAY = 86_400_000;
-    private static final long MILLIS_PER_YEAR = 31556952000L;
-
-    /**
-     * calculates the first day of a year in milliseconds since the epoch (assuming UTC)
-     *
-     * @param year the year
-     * @return the milliseconds since the epoch of the first of january at midnight of the specified year
-     */
-    // see org.joda.time.chrono.GregorianChronology.calculateFirstDayOfYearMillis
-    private static long utcMillisAtStartOfYear(final int year) {
-        // Initial value is just temporary.
-        int leapYears = year / 100;
-        if (year < 0) {
-            // Add 3 before shifting right since /4 and >>2 behave differently
-            // on negative numbers. When the expression is written as
-            // (year / 4) - (year / 100) + (year / 400),
-            // it works for both positive and negative values, except this optimization
-            // eliminates two divisions.
-            leapYears = ((year + 3) >> 2) - leapYears + ((leapYears + 3) >> 2) - 1;
-        } else {
-            leapYears = (year >> 2) - leapYears + (leapYears >> 2);
-            if (isLeapYear(year)) {
-                leapYears--;
-            }
-        }
-
-        return (year * 365L + (leapYears - DAYS_0000_TO_1970)) * MILLIS_PER_DAY; // millis per day
-    }
-
-    private static boolean isLeapYear(final int year) {
-        return ((year & 3) == 0) && ((year % 100) != 0 || (year % 400) == 0);
-    }
-
-    private static final long AVERAGE_MILLIS_PER_YEAR_DIVIDED_BY_TWO = MILLIS_PER_YEAR / 2;
-    private static final long APPROX_MILLIS_AT_EPOCH_DIVIDED_BY_TWO = (1970L * MILLIS_PER_YEAR) / 2;
-
-    // see org.joda.time.chrono.BasicChronology
-    private static int getYear(final long utcMillis) {
-        // Get an initial estimate of the year, and the millis value that
-        // represents the start of that year. Then verify estimate and fix if
-        // necessary.
-
-        // Initial estimate uses values divided by two to avoid overflow.
-        long unitMillis = AVERAGE_MILLIS_PER_YEAR_DIVIDED_BY_TWO;
-        long i2 = (utcMillis >> 1) + APPROX_MILLIS_AT_EPOCH_DIVIDED_BY_TWO;
-        if (i2 < 0) {
-            i2 = i2 - unitMillis + 1;
-        }
-        int year = (int) (i2 / unitMillis);
-
-        long yearStart = utcMillisAtStartOfYear(year);
-        long diff = utcMillis - yearStart;
-
-        if (diff < 0) {
-            year--;
-        } else if (diff >= MILLIS_PER_DAY * 365L) {
-            // One year may need to be added to fix estimate.
-            long oneYear;
-            if (isLeapYear(year)) {
-                oneYear = MILLIS_PER_DAY * 366L;
-            } else {
-                oneYear = MILLIS_PER_DAY * 365L;
-            }
-
-            yearStart += oneYear;
-
-            if (yearStart <= utcMillis) {
-                // Didn't go too far, so actually add one year.
-                year++;
-            }
-        }
-
-        return year;
-    }
-
-    // see org.joda.time.chrono.BasicGJChronology
-    private static int getMonthOfYear(final long utcMillis, final int year) {
-        // Perform a binary search to get the month. To make it go even faster,
-        // compare using ints instead of longs. The number of milliseconds per
-        // year exceeds the limit of a 32-bit int's capacity, so divide by
-        // 1024. No precision is lost (except time of day) since the number of
-        // milliseconds per day contains 1024 as a factor. After the division,
-        // the instant isn't measured in milliseconds, but in units of
-        // (128/125)seconds.
-
-        int i = (int)((utcMillis - utcMillisAtStartOfYear(year)) >> 10);
-
-        // There are 86400000 milliseconds per day, but divided by 1024 is
-        // 84375. There are 84375 (128/125)seconds per day.
-
-        return
-            (isLeapYear(year))
-                ? ((i < 182 * 84375)
-                ? ((i < 91 * 84375)
-                ? ((i < 31 * 84375) ? 1 : (i < 60 * 84375) ? 2 : 3)
-                : ((i < 121 * 84375) ? 4 : (i < 152 * 84375) ? 5 : 6))
-                : ((i < 274 * 84375)
-                ? ((i < 213 * 84375) ? 7 : (i < 244 * 84375) ? 8 : 9)
-                : ((i < 305 * 84375) ? 10 : (i < 335 * 84375) ? 11 : 12)))
-                : ((i < 181 * 84375)
-                ? ((i < 90 * 84375)
-                ? ((i < 31 * 84375) ? 1 : (i < 59 * 84375) ? 2 : 3)
-                : ((i < 120 * 84375) ? 4 : (i < 151 * 84375) ? 5 : 6))
-                : ((i < 273 * 84375)
-                ? ((i < 212 * 84375) ? 7 : (i < 243 * 84375) ? 8 : 9)
-                : ((i < 304 * 84375) ? 10 : (i < 334 * 84375) ? 11 : 12)));
-    }
-
-    // see org.joda.time.chrono.BasicGJChronology
-    private static long getTotalMillisByYearMonth(final int year, final int month) {
-        if (isLeapYear(year)) {
-            return MAX_TOTAL_MILLIS_BY_MONTH_ARRAY[month - 1];
-        } else {
-            return MIN_TOTAL_MILLIS_BY_MONTH_ARRAY[month - 1];
-        }
-    }
-
-    private static final long[] MIN_TOTAL_MILLIS_BY_MONTH_ARRAY;
-    private static final long[] MAX_TOTAL_MILLIS_BY_MONTH_ARRAY;
-    private static final int[] MIN_DAYS_PER_MONTH_ARRAY = {
-        31,28,31,30,31,30,31,31,30,31,30,31
-    };
-    private static final int[] MAX_DAYS_PER_MONTH_ARRAY = {
-        31,29,31,30,31,30,31,31,30,31,30,31
-    };
-
-    static {
-        MIN_TOTAL_MILLIS_BY_MONTH_ARRAY = new long[12];
-        MAX_TOTAL_MILLIS_BY_MONTH_ARRAY = new long[12];
-
-        long minSum = 0;
-        long maxSum = 0;
-        for (int i = 0; i < 11; i++) {
-            long millis = MIN_DAYS_PER_MONTH_ARRAY[i]
-                * (long) MILLIS_PER_DAY;
-            minSum += millis;
-            MIN_TOTAL_MILLIS_BY_MONTH_ARRAY[i + 1] = minSum;
-
-            millis = MAX_DAYS_PER_MONTH_ARRAY[i]
-                * (long) MILLIS_PER_DAY;
-            maxSum += millis;
-            MAX_TOTAL_MILLIS_BY_MONTH_ARRAY[i + 1] = maxSum;
-        }
     }
 }

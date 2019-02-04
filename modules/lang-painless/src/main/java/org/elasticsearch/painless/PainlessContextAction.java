1ae7533863c70c18288c68bf747644fff6af4690
Jack Conradson <osjdconrad@gmail.com>
2019-02-04 10:33:07 -0800

    Convert to use HandledRestAction

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/PainlessContextAction.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/PainlessContextAction.java
@@ -20,10 +20,12 @@
 package org.elasticsearch.painless;
 
 import org.elasticsearch.action.Action;
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionRequestValidationException;
 import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.action.support.ActionFilters;
+import org.elasticsearch.action.support.HandledTransportAction;
 import org.elasticsearch.action.support.single.shard.SingleShardRequest;
 import org.elasticsearch.action.support.single.shard.TransportSingleShardAction;
 import org.elasticsearch.client.node.NodeClient;
@@ -32,6 +34,8 @@ import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.routing.ShardsIterator;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.ToXContent;
 import org.elasticsearch.common.xcontent.ToXContentObject;
@@ -44,7 +48,9 @@ import org.elasticsearch.rest.RestController;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.RestResponse;
 import org.elasticsearch.rest.action.RestBuilderListener;
+import org.elasticsearch.rest.action.RestToXContentListener;
 import org.elasticsearch.script.ScriptService;
+import org.elasticsearch.tasks.Task;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
@@ -63,11 +69,16 @@ public class PainlessContextAction extends Action<PainlessContextAction.Response
     }
 
     @Override
-    public PainlessContextAction.Response newResponse() {
-        return new PainlessContextAction.Response();
+    public Response newResponse() {
+        throw new UnsupportedOperationException();
     }
 
-    public static class Request extends SingleShardRequest<PainlessContextAction.Request> {
+    @Override
+    public Writeable.Reader<Response> getResponseReader() {
+        return Response::new;
+    }
+
+    public static class Request extends ActionRequest {
 
         @Override
         public ActionRequestValidationException validate() {
@@ -76,6 +87,14 @@ public class PainlessContextAction extends Action<PainlessContextAction.Response
     }
 
     public static class Response extends ActionResponse implements ToXContentObject {
+
+        public Response(StreamInput input) {
+
+        }
+
+        public Response() {
+
+        }
 
         @Override
         public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
@@ -87,37 +106,19 @@ public class PainlessContextAction extends Action<PainlessContextAction.Response
         }
     }
 
-    public static class TransportAction extends TransportSingleShardAction<PainlessContextAction.Request, PainlessContextAction.Response> {
+    public static class TransportAction extends HandledTransportAction<Request, Response> {
 
         PainlessScriptEngine painlessScriptEngine;
 
         @Inject
-        public TransportAction(ThreadPool threadPool, TransportService transportService,
-                               ActionFilters actionFilters, IndexNameExpressionResolver indexNameExpressionResolver,
-                               ClusterService clusterService, PainlessScriptEngine painlessScriptEngine) {
-            super(NAME, threadPool, clusterService, transportService, actionFilters, indexNameExpressionResolver,
-                    PainlessContextAction.Request::new, ThreadPool.Names.MANAGEMENT);
+        public TransportAction(TransportService transportService, ActionFilters actionFilters, PainlessScriptEngine painlessScriptEngine) {
+            super(NAME, transportService, actionFilters, Request::new);
             this.painlessScriptEngine = painlessScriptEngine;
         }
 
         @Override
-        protected Response shardOperation(Request request, ShardId shardId) throws IOException {
-            return new Response();
-        }
-
-        @Override
-        protected Response newResponse() {
-            return new Response();
-        }
-
-        @Override
-        protected boolean resolveIndex(Request request) {
-            return false;
-        }
-
-        @Override
-        protected ShardsIterator shards(ClusterState state, InternalRequest request) {
-            return null;
+        protected void doExecute(Task task, Request request, ActionListener<Response> listener) {
+            listener.onResponse(new Response());
         }
     }
 
@@ -134,15 +135,8 @@ public class PainlessContextAction extends Action<PainlessContextAction.Response
         }
 
         @Override
-        protected RestChannelConsumer prepareRequest(RestRequest restRequest, NodeClient client) throws IOException {
-            final Request request = new Request();
-            return channel -> client.executeLocally(INSTANCE, request, new RestBuilderListener<PainlessContextAction.Response>(channel) {
-                @Override
-                public RestResponse buildResponse(PainlessContextAction.Response response, XContentBuilder builder) throws Exception {
-                    response.toXContent(builder, ToXContent.EMPTY_PARAMS);
-                    return new BytesRestResponse(OK, builder);
-                }
-            });
+        protected RestChannelConsumer prepareRequest(RestRequest restRequest, NodeClient client) {
+            return channel -> client.executeLocally(INSTANCE, new Request(), new RestToXContentListener<>(channel));
         }
     }
 }

44c3e8abad46ff70f4cbcd7fbbaa723afef8423e
Zachary Tong <polyfractal@elastic.co>
2019-02-05 17:06:28 -0500

    Assert job is not null in FullClusterRestartIT
    `waitForRollUpJob` is an assertBusy that waits for the rollup job
    to appear in the tasks list, and waits for it to be a certain state.
    
    However, there was a null check around the state assertion, which meant
    if the job _was_ null, the assertion would be skipped, and the
    assertBusy would pass withouot an exception.  This could then lead to
    downstream assertions to fail because the job was not actually ready,
    or in the wrong state.
    
    This changes the test to assert the job is not null, so the assertBusy
    operates as intended.
    Backport of #38218

--- x-pack/qa/full-cluster-restart/src/test/java/org/elasticsearch/xpack/restart/FullClusterRestartIT.java
+++ x-pack/qa/full-cluster-restart/src/test/java/org/elasticsearch/xpack/restart/FullClusterRestartIT.java
@@ -631,9 +631,8 @@ public class FullClusterRestartIT extends AbstractFullClusterRestartTestCase {
         final Request getRollupJobRequest = new Request("GET", "_xpack/rollup/job/" + rollupJob);
         Map<String, Object> getRollupJobResponse = entityAsMap(client().performRequest(getRollupJobRequest));
         Map<String, Object> job = getJob(getRollupJobResponse, rollupJob);
-        if (job != null) {
-            assertThat(ObjectPath.eval("status.job_state", job), expectedStates);
-        }
+        assertNotNull(job);
+        assertThat(ObjectPath.eval("status.job_state", job), expectedStates);
 
         // check that the rollup job is started using the Tasks API
         final Request taskRequest = new Request("GET", "_tasks");
@@ -679,9 +678,8 @@ public class FullClusterRestartIT extends AbstractFullClusterRestartTestCase {
             assertThat(getRollupJobResponse.getStatusLine().getStatusCode(), equalTo(RestStatus.OK.getStatus()));
 
             Map<String, Object> job = getJob(getRollupJobResponse, rollupJob);
-            if (job != null) {
-                assertThat(ObjectPath.eval("status.job_state", job), expectedStates);
-            }
+            assertNotNull(job);
+            assertThat(ObjectPath.eval("status.job_state", job), expectedStates);
         }, 30L, TimeUnit.SECONDS);
     }
 

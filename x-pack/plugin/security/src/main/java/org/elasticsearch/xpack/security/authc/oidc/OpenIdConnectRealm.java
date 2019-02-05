9d30bd2d975ad3ddb64433f5b10011167ef77e9d
Ioannis Kakavas <ioannis@elastic.co>
2019-02-05 02:07:13 +0200

    address feedback

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/oidc/OpenIdConnectRealm.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/oidc/OpenIdConnectRealm.java
@@ -98,8 +98,6 @@ public class OpenIdConnectRealm extends Realm implements Releasable {
         this.roleMapper = roleMapper;
         this.rpConfiguration = buildRelyingPartyConfiguration(config);
         this.opConfiguration = buildOpenIdConnectProviderConfiguration(config);
-        this.openIdConnectAuthenticator =
-            new OpenIdConnectAuthenticator(config, opConfiguration, rpConfiguration, sslService, watcherService);
         this.principalAttribute = ClaimParser.forSetting(logger, PRINCIPAL_CLAIM, config, true);
         this.groupsAttribute = ClaimParser.forSetting(logger, GROUPS_CLAIM, config, false);
         this.dnAttribute = ClaimParser.forSetting(logger, DN_CLAIM, config, false);
@@ -110,6 +108,8 @@ public class OpenIdConnectRealm extends Realm implements Releasable {
             throw new IllegalStateException("OpenID Connect Realm requires that the token service be enabled ("
                 + XPackSettings.TOKEN_SERVICE_ENABLED_SETTING.getKey() + ")");
         }
+        this.openIdConnectAuthenticator =
+            new OpenIdConnectAuthenticator(config, opConfiguration, rpConfiguration, sslService, watcherService);
     }
 
     // For testing
@@ -363,12 +363,12 @@ public class OpenIdConnectRealm extends Realm implements Releasable {
                                 values = Collections.emptyList();
                             } else if (claimValueObject instanceof String) {
                                 values = Collections.singletonList((String) claimValueObject);
-                            } else if (claimValueObject instanceof List == false) {
+                            } else if (claimValueObject instanceof List) {
+                                values = (List<String>) claimValueObject;
+                            } else {
                                 throw new SettingsException("Setting [" + RealmSettings.getFullSettingKey(realmConfig, setting.getClaim())
                                     + " expects a claim with String or a String Array value but found a "
                                     + claimValueObject.getClass().getName());
-                            } else {
-                                values = (List<String>) claimValueObject;
                             }
                             return values.stream().map(s -> {
                                 final Matcher matcher = regex.matcher(s);

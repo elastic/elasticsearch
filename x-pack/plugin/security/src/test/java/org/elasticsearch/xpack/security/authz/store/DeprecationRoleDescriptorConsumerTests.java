5a481e258c97de258cc0fa7199a0c52e703859ea
Albert Zaharovits <albert.zaharovits@elastic.co>
2019-02-05 16:06:09 +0200

    Bare bare test

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authz/store/DeprecationRoleDescriptorConsumerTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authz/store/DeprecationRoleDescriptorConsumerTests.java
@@ -6,8 +6,10 @@
 
 package org.elasticsearch.xpack.security.authz.store;
 
+import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.ClusterState;
-import org.elasticsearch.cluster.metadata.AliasOrIndex;
+import org.elasticsearch.cluster.metadata.AliasMetaData;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.cluster.metadata.MetaData;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.logging.DeprecationLogger;
@@ -15,11 +17,12 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.mock.orig.Mockito;
 import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.test.VersionUtils;
 import org.elasticsearch.threadpool.ThreadPool;
+import org.elasticsearch.xpack.core.security.authz.RoleDescriptor;
 import org.junit.Before;
 import org.mockito.stubbing.Answer;
 
-import java.util.SortedMap;
 import java.util.concurrent.ExecutorService;
 
 import static org.mockito.Mockito.mock;
@@ -44,14 +47,40 @@ public final class DeprecationRoleDescriptorConsumerTests extends ESTestCase {
         when(threadPool.getThreadContext()).thenReturn(new ThreadContext(Settings.EMPTY));
     }
 
-    private ClusterService mockClusterService(SortedMap<String, AliasOrIndex> aliasOrIndexMap) {
+    public void testSimpleAliasIndexPair() throws Exception {
+        final MetaData.Builder metaDataBuilder = MetaData.builder();
+        addIndex(metaDataBuilder, "index", "alias");
+        final RoleDescriptor roleDescriptor = new RoleDescriptor("role1", null,
+                new RoleDescriptor.IndicesPrivileges[] { indexPrivileges("read", "alias") }, null);
+        DeprecationRoleDescriptorConsumer deprecation = new DeprecationRoleDescriptorConsumer(mockClusterService(metaDataBuilder.build()),
+                this.threadPool, deprecationLogger);
+    }
+
+    private void addIndex(MetaData.Builder metaDataBuilder, String index, String... aliases) {
+        final IndexMetaData.Builder indexMetaDataBuilder = IndexMetaData.builder(index)
+                .settings(Settings.builder().put("index.version.created", VersionUtils.randomVersion(random())))
+                .numberOfShards(1)
+                .numberOfReplicas(1);
+        for (final String alias : aliases) {
+            indexMetaDataBuilder.putAlias(AliasMetaData.builder(alias).build());
+        }
+        metaDataBuilder.put(indexMetaDataBuilder.build(), false);
+    }
+
+    private ClusterService mockClusterService(MetaData metaData) {
         final ClusterService clusterService = mock(ClusterService.class);
-        final ClusterState clusterState = mock(ClusterState.class);
-        final MetaData metadata = mock(MetaData.class);
-        when(metadata.getAliasAndIndexLookup()).thenReturn(aliasOrIndexMap);
-        when(clusterState.metaData()).thenReturn(metadata);
+        final ClusterState clusterState = ClusterState.builder(ClusterName.DEFAULT).metaData(metaData).build();
         when(clusterService.state()).thenReturn(clusterState);
         return clusterService;
+    }
+
+    private RoleDescriptor.IndicesPrivileges indexPrivileges(String priv, String... indicesOrAliases) {
+        return RoleDescriptor.IndicesPrivileges.builder()
+                .indices(indicesOrAliases)
+                .privileges(priv)
+                .grantedFields(randomArray(0, 2, String[]::new, () -> randomBoolean() ? null : randomAlphaOfLengthBetween(1, 4)))
+                .query(randomFrom(null, "{ }"))
+                .build();
     }
 
 }

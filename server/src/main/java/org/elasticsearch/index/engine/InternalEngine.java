7de228ae3b7d43e8403b15253f9799fc9a7e6326
Nhat Nguyen <nhat.nguyen@elastic.co>
2019-02-09 10:05:27 -0500

    don’t check if fail on tragic

--- server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
+++ server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -2076,7 +2076,7 @@ public class InternalEngine extends Engine {
                 // no need to commit in this case!, we snapshot before we close the shard, so translog and all sync'ed
                 logger.trace("rollback indexWriter");
                 try {
-                    assert hasSnapshottedCommits() == false : "not all snapshotted commits released";
+                    assert failedEngine.get() != null || hasSnapshottedCommits() == false : "not all snapshotted commits released";
                     indexWriter.rollback();
                 } catch (AlreadyClosedException ex) {
                     failOnTragicEvent(ex);

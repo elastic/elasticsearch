32947f4e4b207be4c28f86e4e39c294f6e89a2ed
jimczi <jimczi@apache.org>
2019-02-12 10:00:11 +0100

    Mute RetentionLeastIT#testRetentionLeaseSyncedOnRemove
    Relates #38764

--- server/src/test/java/org/elasticsearch/index/seqno/RetentionLeaseIT.java
+++ server/src/test/java/org/elasticsearch/index/seqno/RetentionLeaseIT.java
@@ -127,6 +127,7 @@ public class RetentionLeaseIT extends ESIntegTestCase  {
         }
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/38764")
     public void testRetentionLeaseSyncedOnRemove() throws Exception {
         final int numberOfReplicas = 2 - scaledRandomIntBetween(0, 2);
         internalCluster().ensureAtLeastNumDataNodes(1 + numberOfReplicas);

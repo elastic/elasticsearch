f3fcff50ae4e272ed111beca14f64fd10b9153b4
Andrei Stefan <andrei@elastic.co>
2019-02-13 15:13:28 +0200

    Improved the tests

--- x-pack/plugin/sql/jdbc/src/test/java/org/elasticsearch/xpack/sql/jdbc/JdbcResultSetMetaDataTests.java
+++ x-pack/plugin/sql/jdbc/src/test/java/org/elasticsearch/xpack/sql/jdbc/JdbcResultSetMetaDataTests.java
@@ -20,7 +20,8 @@ public class JdbcResultSetMetaDataTests extends ESTestCase {
     private final List<JdbcColumnInfo> columns = Arrays.asList(
                 new JdbcColumnInfo("test_keyword", EsType.KEYWORD, EMPTY, EMPTY, EMPTY, EMPTY, 0),
                 new JdbcColumnInfo("test_integer", EsType.INTEGER, EMPTY, EMPTY, EMPTY, EMPTY, 11),
-                new JdbcColumnInfo("test_double", EsType.DOUBLE, EMPTY, EMPTY, EMPTY, EMPTY, 25)
+                new JdbcColumnInfo("test_double", EsType.DOUBLE, EMPTY, EMPTY, EMPTY, EMPTY, 25),
+                new JdbcColumnInfo("test_long", EsType.LONG, "test_table", "test", "schema", "custom_label", 20)
             );
     private final JdbcResultSetMetaData metaData = new JdbcResultSetMetaData(null, columns);
 
@@ -34,15 +35,18 @@ public class JdbcResultSetMetaDataTests extends ESTestCase {
         assertEquals(false, metaData.isSigned(1));
         assertEquals(true, metaData.isSigned(2));
         assertEquals(true, metaData.isSigned(3));
+        assertEquals(true, metaData.isSigned(4));
     }
     
     public void testColumnNamesAndLabels() throws SQLException {
         assertEquals("test_keyword", metaData.getColumnName(1));
         assertEquals("test_integer", metaData.getColumnName(2));
         assertEquals("test_double", metaData.getColumnName(3));
+        assertEquals("test_long", metaData.getColumnName(4));
         
         assertEquals("test_keyword", metaData.getColumnLabel(1));
         assertEquals("test_integer", metaData.getColumnLabel(2));
         assertEquals("test_double", metaData.getColumnLabel(3));
+        assertEquals("custom_label", metaData.getColumnLabel(4));
     }
 }

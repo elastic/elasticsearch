c7d612556f51b084288513dbc2ec04c68df84b91
Luca Cavanna <javanna@users.noreply.github.com>
2019-02-13 17:00:15 +0100

    address comments

--- server/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
+++ server/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
@@ -341,7 +341,7 @@ public class TransportSearchAction extends HandledTransportAction<SearchRequest,
         final DiscoveryNodes nodes = clusterState.nodes();
         BiFunction<String, String, Transport.Connection> connectionLookup = buildConnectionLookup(searchRequest.getLocalClusterAlias(),
             nodes::get, remoteConnections, searchTransportService::getConnection);
-        assert nodeCount > 0 || shardIterators.size() == 0;
+        assert nodeCount > 0 || shardIterators.size() == 0 : "non empty search iterators but node count is 0";
         setMaxConcurrentShardRequests(searchRequest, nodeCount);
         boolean preFilterSearchShards = shouldPreFilterSearchShards(searchRequest, shardIterators);
         searchAsyncAction(task, searchRequest, shardIterators, timeProvider, connectionLookup, clusterState.version(),
@@ -350,11 +350,10 @@ public class TransportSearchAction extends HandledTransportAction<SearchRequest,
 
     static void setMaxConcurrentShardRequests(SearchRequest searchRequest, int nodeCount) {
         if (searchRequest.isMaxConcurrentShardRequestsSet() == false) {
-            // we try to set a default of max concurrent shard requests based on
-            // the node count but upper-bound it by 256 by default to keep it sane. A single
-            // search request that fans out lots of shards should hit a cluster too hard while 256 is already a lot.
-            // we multiply it by the default number of shards such that a single request in a cluster of 1 would hit all shards of a
-            // default index.
+            // we try to set a default of max concurrent shard requests based on the node count but upper-bound it by 256 by default to
+            // keep it sane. A single search request that fans out to lots of shards should hit a cluster too hard while 256 is already
+            // a lot. we multiply it by the default number of shards such that a single request in a cluster of 1 would hit all shards of
+            // a default index. We take into account that we may be in a cluster with no data nodes searching against no shards.
             searchRequest.setMaxConcurrentShardRequests(Math.min(256, Math.max(nodeCount, 1)
                 * IndexMetaData.INDEX_NUMBER_OF_SHARDS_SETTING.getDefault(Settings.EMPTY)));
         }

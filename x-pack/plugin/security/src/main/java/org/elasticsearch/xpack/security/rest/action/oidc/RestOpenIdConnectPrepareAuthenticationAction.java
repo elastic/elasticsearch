0fbe3c79d2757a6026cf1baf0b70702e824eaa37
Ioannis Kakavas <ioannis@elastic.co>
2019-02-13 14:26:13 +0200

    add parser field

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/rest/action/oidc/RestOpenIdConnectPrepareAuthenticationAction.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/rest/action/oidc/RestOpenIdConnectPrepareAuthenticationAction.java
@@ -37,6 +37,7 @@ public class RestOpenIdConnectPrepareAuthenticationAction extends OpenIdConnectB
     static {
         PARSER.declareString(OpenIdConnectPrepareAuthenticationRequest::setRealmName, new ParseField("realm"));
         PARSER.declareString(OpenIdConnectPrepareAuthenticationRequest::setIssuer, new ParseField("iss"));
+        PARSER.declareString(OpenIdConnectPrepareAuthenticationRequest::setLoginHint, new ParseField("login_hint"));
         PARSER.declareString(OpenIdConnectPrepareAuthenticationRequest::setState, new ParseField("state"));
         PARSER.declareString(OpenIdConnectPrepareAuthenticationRequest::setNonce, new ParseField("nonce"));
     }

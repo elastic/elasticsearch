1f680331289082c1b37b51cd76f6c930292dfd89
Adrien Grand <jpountz@gmail.com>
2019-02-18 14:44:42 +0100

    iter

--- server/src/test/java/org/elasticsearch/indices/IndicesServiceCloseTests.java
+++ server/src/test/java/org/elasticsearch/indices/IndicesServiceCloseTests.java
@@ -21,6 +21,8 @@ package org.elasticsearch.indices;
 
 import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.routing.allocation.DiskThresholdSettings;
+import org.elasticsearch.common.network.NetworkModule;
+import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.env.Environment;
@@ -33,43 +35,43 @@ import org.elasticsearch.node.NodeValidationException;
 import org.elasticsearch.script.ScriptService;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.InternalTestCluster;
-import org.elasticsearch.test.MockHttpTransport;
-import org.elasticsearch.transport.nio.MockNioTransportPlugin;
+import org.elasticsearch.test.discovery.TestZenDiscovery;
+import org.elasticsearch.transport.MockTcpTransportPlugin;
 
 import java.nio.file.Path;
 import java.util.Arrays;
 
-import static org.elasticsearch.cluster.coordination.ClusterBootstrapService.INITIAL_MASTER_NODES_SETTING;
 import static org.elasticsearch.cluster.metadata.IndexMetaData.SETTING_NUMBER_OF_REPLICAS;
 import static org.elasticsearch.cluster.metadata.IndexMetaData.SETTING_NUMBER_OF_SHARDS;
-import static org.elasticsearch.discovery.SettingsBasedSeedHostsProvider.DISCOVERY_SEED_HOSTS_SETTING;
+import static org.elasticsearch.discovery.zen.SettingsBasedHostsProvider.DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 
 public class IndicesServiceCloseTests extends ESTestCase {
 
     private Node startNode() throws NodeValidationException {
         final Path tempDir = createTempDir();
-        String nodeName = "node_s_0";
         Settings settings = Settings.builder()
-            .put(ClusterName.CLUSTER_NAME_SETTING.getKey(), InternalTestCluster.clusterName("single-node-cluster", random().nextLong()))
-            .put(Environment.PATH_HOME_SETTING.getKey(), tempDir)
-            .put(Environment.PATH_REPO_SETTING.getKey(), tempDir.resolve("repo"))
-            .put(Environment.PATH_SHARED_DATA_SETTING.getKey(), createTempDir().getParent())
-            .put(Node.NODE_NAME_SETTING.getKey(), nodeName)
-            .put(ScriptService.SCRIPT_MAX_COMPILATIONS_RATE.getKey(), "1000/1m")
-            .put(EsExecutors.PROCESSORS_SETTING.getKey(), 1) // limit the number of threads created
-            .put("transport.type", getTestTransportType())
-            .put(Node.NODE_DATA_SETTING.getKey(), true)
-            .put(NodeEnvironment.NODE_ID_SEED_SETTING.getKey(), random().nextLong())
-            // default the watermarks low values to prevent tests from failing on nodes without enough disk space
-            .put(DiskThresholdSettings.CLUSTER_ROUTING_ALLOCATION_LOW_DISK_WATERMARK_SETTING.getKey(), "1b")
-            .put(DiskThresholdSettings.CLUSTER_ROUTING_ALLOCATION_HIGH_DISK_WATERMARK_SETTING.getKey(), "1b")
-            .put(DiskThresholdSettings.CLUSTER_ROUTING_ALLOCATION_DISK_FLOOD_STAGE_WATERMARK_SETTING.getKey(), "1b")
-            .putList(DISCOVERY_SEED_HOSTS_SETTING.getKey()) // empty list disables a port scan for other nodes
-            .putList(INITIAL_MASTER_NODES_SETTING.getKey(), nodeName)
-            .build();
+                .put(ClusterName.CLUSTER_NAME_SETTING.getKey(), InternalTestCluster.clusterName("single-node-cluster", random().nextLong()))
+                .put(Environment.PATH_HOME_SETTING.getKey(), tempDir)
+                .put(Environment.PATH_REPO_SETTING.getKey(), tempDir.resolve("repo"))
+                // TODO: use a consistent data path for custom paths
+                // This needs to tie into the ESIntegTestCase#indexSettings() method
+                .put(Environment.PATH_SHARED_DATA_SETTING.getKey(), createTempDir().getParent())
+                .put("node.name", "node_s_0")
+                .put(ScriptService.SCRIPT_MAX_COMPILATIONS_RATE.getKey(), "1000/1m")
+                .put(EsExecutors.PROCESSORS_SETTING.getKey(), 1) // limit the number of threads created
+                .put(NetworkModule.HTTP_ENABLED.getKey(), false)
+                .put("transport.type", getTestTransportType())
+                .put(Node.NODE_DATA_SETTING.getKey(), true)
+                .put(NodeEnvironment.NODE_ID_SEED_SETTING.getKey(), random().nextLong())
+                // default the watermarks low values to prevent tests from failing on nodes without enough disk space
+                .put(DiskThresholdSettings.CLUSTER_ROUTING_ALLOCATION_LOW_DISK_WATERMARK_SETTING.getKey(), "1b")
+                .put(DiskThresholdSettings.CLUSTER_ROUTING_ALLOCATION_HIGH_DISK_WATERMARK_SETTING.getKey(), "1b")
+                .put(DiskThresholdSettings.CLUSTER_ROUTING_ALLOCATION_DISK_FLOOD_STAGE_WATERMARK_SETTING.getKey(), "1b")
+                .putList(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey()) // empty list disables a port scan for other nodes
+                .build();
 
-        Node node = new MockNode(settings, Arrays.asList(MockNioTransportPlugin.class, MockHttpTransport.TestPlugin.class), true);
+        Node node = new MockNode(settings, Arrays.asList(MockTcpTransportPlugin.class, TestZenDiscovery.TestPlugin.class), true);
         node.start();
         return node;
     }
@@ -80,6 +82,8 @@ public class IndicesServiceCloseTests extends ESTestCase {
         assertEquals(1, indicesService.indicesRefCount.refCount());
         node.close();
         assertEquals(0, indicesService.indicesRefCount.refCount());
+
+        assertSettingDeprecationsAndWarnings(new Setting[] {NetworkModule.HTTP_ENABLED});
     }
 
     public void testCloseNonEmptyIndicesService() throws Exception {
@@ -94,6 +98,8 @@ public class IndicesServiceCloseTests extends ESTestCase {
 
         node.close();
         assertEquals(0, indicesService.indicesRefCount.refCount());
+
+        assertSettingDeprecationsAndWarnings(new Setting[] {NetworkModule.HTTP_ENABLED});
     }
 
     public void testCloseWhileOngoingRequest() throws Exception {
@@ -115,6 +121,8 @@ public class IndicesServiceCloseTests extends ESTestCase {
 
         shard.store().decRef();
         assertEquals(0, indicesService.indicesRefCount.refCount());
+
+        assertSettingDeprecationsAndWarnings(new Setting[] {NetworkModule.HTTP_ENABLED});
     }
 
 }

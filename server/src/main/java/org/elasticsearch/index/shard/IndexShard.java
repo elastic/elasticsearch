8d9b3918b6046579155e5f24d3a77dc2858a73c5
Nhat Nguyen <nhat.nguyen@elastic.co>
2019-02-20 08:08:27 -0500

    Add retention leases replication tests (#38857)
    This commit introduces the retention leases to ESIndexLevelReplicationTestCase,
    then adds some tests verifying that the retention leases replication works
    correctly in spite of the presence of the primary failover or out of order
    delivery of retention leases sync requests.
    
    Relates #37165

--- server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
@@ -3162,4 +3162,8 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
     public void verifyShardBeforeIndexClosing() throws IllegalStateException {
         getEngine().verifyEngineBeforeIndexClosing();
     }
+
+    RetentionLeaseSyncer getRetentionLeaseSyncer() {
+        return retentionLeaseSyncer;
+    }
 }

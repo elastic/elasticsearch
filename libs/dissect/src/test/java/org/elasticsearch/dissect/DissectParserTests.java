97dd3ca3368c454d00c2be7353c6a536146f2598
Albert Zaharovits <albert.zaharovits@elastic.co>
2019-02-21 20:20:50 +0200

    Checkstyle

--- libs/dissect/src/test/java/org/elasticsearch/dissect/DissectParserTests.java
+++ libs/dissect/src/test/java/org/elasticsearch/dissect/DissectParserTests.java
@@ -28,7 +28,6 @@ import org.mockito.internal.util.collections.Sets;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;

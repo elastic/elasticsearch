90260bd0001ac4019e7e04cfabdb1241d0ebe1ca
Ryan Ernst <ryan@iernst.net>
2019-02-22 11:41:22 -0800

    narrow test for debugging

--- qa/vagrant/src/main/java/org/elasticsearch/packaging/test/PackageTestCase.java
+++ qa/vagrant/src/main/java/org/elasticsearch/packaging/test/PackageTestCase.java
@@ -33,7 +33,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static org.elasticsearch.packaging.util.FileUtils.assertPathsDontExist;
-import static org.elasticsearch.packaging.util.FileUtils.mv;
 import static org.elasticsearch.packaging.util.Packages.SYSTEMD_SERVICE;
 import static org.elasticsearch.packaging.util.Packages.assertInstalled;
 import static org.elasticsearch.packaging.util.Packages.assertRemoved;
@@ -89,14 +88,18 @@ public abstract class PackageTestCase extends PackagingTestCase {
         verifyPackageInstallation(installation, distribution()); // check startup script didn't change permissions
     }
 
-    public void assertRunsWithJavaHome() throws IOException {
+    public void assertRunsWithJavaHome() throws Exception {
         Shell sh = new Shell();
 
         String systemJavaHome = sh.run("echo $SYSTEM_JAVA_HOME").stdout.trim();
+        String javaHome = sh.run("echo $JAVA_HOME").stdout.trim();
+        System.err.println("JAVA HOME = " + javaHome);
         byte[] originalEnvFile = Files.readAllBytes(installation.envFile);
         try {
-            Files.write(installation.envFile, ("JAVA_HOME=" + systemJavaHome).getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+            Files.write(installation.envFile, ("JAVA_HOME=" + systemJavaHome + "\n").getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
             System.err.println("ENV FILE: " + new String(Files.readAllBytes(installation.envFile), StandardCharsets.UTF_8));
+            System.err.println("HOME DIR LISTING: ");
+            Files.list(installation.home).forEach(System.err::println);
             startElasticsearch();
             runElasticsearchTests();
         } finally {
@@ -107,13 +110,13 @@ public abstract class PackageTestCase extends PackagingTestCase {
         assertThat(new String(Files.readAllBytes(log), StandardCharsets.UTF_8), containsString(systemJavaHome));
     }
 
-    public void test41JavaHomeOverride() throws IOException {
+    public void test41JavaHomeOverride() throws Exception {
         assumeThat(installation, is(notNullValue()));
 
         assertRunsWithJavaHome();
     }
 
-    public void test42BundledJdkRemoved() throws IOException {
+    /*public void test42BundledJdkRemoved() throws IOException {
         assumeThat(installation, is(notNullValue()));
 
         Path relocatedJdk = installation.bundledJdk.getParent().resolve("jdk.relocated");
@@ -123,7 +126,7 @@ public abstract class PackageTestCase extends PackagingTestCase {
         } finally {
             mv(relocatedJdk, installation.bundledJdk);
         }
-    }
+    }*/
 
     public void test50Remove() {
         assumeThat(installation, is(notNullValue()));

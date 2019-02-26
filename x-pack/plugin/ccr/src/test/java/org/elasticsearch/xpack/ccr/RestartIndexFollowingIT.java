24e478c58e93438f29cb4309147dcae6a2c0d0a5
Martijn van Groningen <martijn.v.groningen@gmail.com>
2019-02-26 10:39:29 +0100

    Fix test, more than one node may be connected.
    Relates to #37681

--- x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/RestartIndexFollowingIT.java
+++ x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/RestartIndexFollowingIT.java
@@ -23,6 +23,7 @@ import java.util.Locale;
 import static java.util.Collections.singletonMap;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 public class RestartIndexFollowingIT extends CcrIntegTestCase {
 
@@ -97,7 +98,7 @@ public class RestartIndexFollowingIT extends CcrIntegTestCase {
             List<RemoteConnectionInfo> infos =
                 followerClient().execute(RemoteInfoAction.INSTANCE, new RemoteInfoRequest()).get().getInfos();
             assertThat(infos.size(), equalTo(1));
-            assertThat(infos.get(0).getNumNodesConnected(), equalTo(1));
+            assertThat(infos.get(0).getNumNodesConnected(), greaterThanOrEqualTo(1));
         });
     }
 

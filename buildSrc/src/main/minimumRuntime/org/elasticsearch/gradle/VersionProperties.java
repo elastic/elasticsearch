40848ba08ca4f13e8c43c380b9759df90a73e106
Ryan Ernst <ryan@iernst.net>
2019-02-27 20:40:45 -0800

    move jdk version parsing to distribution

--- buildSrc/src/main/minimumRuntime/org/elasticsearch/gradle/VersionProperties.java
+++ buildSrc/src/main/minimumRuntime/org/elasticsearch/gradle/VersionProperties.java
@@ -15,7 +15,6 @@ import java.util.regex.Pattern;
  * Accessor for shared dependency versions used by elasticsearch, namely the elasticsearch and lucene versions.
  */
 public class VersionProperties {
-    private static final Pattern JDK_VERSION = Pattern.compile("(\\d+)\\.(\\d+)\\.(\\d+)\\+(\\d+)");
 
     public static String getElasticsearch() {
         return elasticsearch;
@@ -25,14 +24,7 @@ public class VersionProperties {
         return lucene;
     }
 
-    /**
-     * Return the bundled jdk version, broken into elements as:
-     * [feature, interim, update, build]
-     *
-     * Note the "patch" version is not yet handled here, as it has not yet
-     * been used by java.
-     */
-    public static List<String> getBundledJdk() {
+    public static String getBundledJdk() {
         return bundledJdk;
     }
 
@@ -42,23 +34,14 @@ public class VersionProperties {
 
     private static final String elasticsearch;
     private static final String lucene;
-    private static final List<String> bundledJdk;
+    private static final String bundledJdk;
     private static final Map<String, String> versions = new HashMap<String, String>();
+
     static {
         Properties props = getVersionProperties();
         elasticsearch = props.getProperty("elasticsearch");
         lucene = props.getProperty("lucene");
-
-        String bundledJdkProperty = props.getProperty("bundled_jdk");
-        Matcher jdkVersionMatcher = JDK_VERSION.matcher(bundledJdkProperty);
-        if (jdkVersionMatcher.matches() == false) {
-            throw new IllegalArgumentException("Malformed jdk version [" + bundledJdkProperty + "]");
-        }
-        List<String> bundledJdkList = new ArrayList<>();
-        for (int i = 1; i <= jdkVersionMatcher.groupCount(); ++i) {
-            bundledJdkList.add(jdkVersionMatcher.group(i));
-        }
-        bundledJdk = Collections.unmodifiableList(bundledJdkList);
+        bundledJdk = props.getProperty("bundled_jdk");
 
         for (String property : props.stringPropertyNames()) {
             versions.put(property, props.getProperty(property));

349c8a3b191677eaab68ae7c5534749a7c03a5d9
Yannick Welsch <yannick@welsch.lu>
2019-03-01 09:16:53 +0100

    Do not close bad indices on startup (#39500)
    With #17187, we verified IndexService creation during initial state recovery on the master and if the
    recovery failed the index was imported as closed, not allocating any shards. This was mainly done to
    prevent endless allocation loops and full log files on data-nodes when the indexmetadata contained
    broken settings / analyzers. Zen2 loads the cluster state eagerly, and this check currently runs on all
    nodes (not only the elected master), which can significantly slow down startup on data nodes.
    Furthermore, with replicated closed indices (#33888) on the horizon, importing the index as closed
    will no longer not allocate any shards. Fortunately, the original issue for endless allocation loops is
    no longer a problem due to #18467, where we limit the retries of failed allocations. The solution here
    is therefore to just undo #17187, as it's no longer necessary, and covered by #18467, which will solve
    the issue for Zen2 and replicated closed indices as well.

--- server/src/test/java/org/elasticsearch/gateway/ClusterStateUpdatersTests.java
+++ server/src/test/java/org/elasticsearch/gateway/ClusterStateUpdatersTests.java
@@ -34,10 +34,8 @@ import org.elasticsearch.common.settings.SettingUpgrader;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.set.Sets;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.test.ESTestCase;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
@@ -48,7 +46,6 @@ import java.util.stream.Stream;
 
 import static org.elasticsearch.cluster.metadata.MetaData.CLUSTER_READ_ONLY_BLOCK;
 import static org.elasticsearch.gateway.ClusterStateUpdaters.addStateNotRecoveredBlock;
-import static org.elasticsearch.gateway.ClusterStateUpdaters.closeBadIndices;
 import static org.elasticsearch.gateway.ClusterStateUpdaters.hideStateIfNotRecovered;
 import static org.elasticsearch.gateway.ClusterStateUpdaters.mixCurrentStateAndRecoveredState;
 import static org.elasticsearch.gateway.ClusterStateUpdaters.recoverClusterBlocks;
@@ -60,8 +57,6 @@ import static org.elasticsearch.gateway.GatewayService.STATE_NOT_RECOVERED_BLOCK
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
 
 public class ClusterStateUpdatersTests extends ESTestCase {
 
@@ -200,32 +195,6 @@ public class ClusterStateUpdatersTests extends ESTestCase {
 
         assertMetaDataEquals(initialState, newState);
         assertTrue(newState.blocks().hasGlobalBlock(STATE_NOT_RECOVERED_BLOCK));
-    }
-
-    public void testCloseBadIndices() throws IOException {
-        final IndicesService indicesService = mock(IndicesService.class);
-        final IndexMetaData good = createIndexMetaData("good", Settings.EMPTY);
-        final IndexMetaData bad = createIndexMetaData("bad", Settings.EMPTY);
-        final IndexMetaData ugly = IndexMetaData.builder(createIndexMetaData("ugly", Settings.EMPTY))
-                .state(IndexMetaData.State.CLOSE)
-                .build();
-
-        final ClusterState initialState = ClusterState
-                .builder(ClusterState.EMPTY_STATE)
-                .metaData(MetaData.builder()
-                        .put(good, false)
-                        .put(bad, false)
-                        .put(ugly, false)
-                        .build())
-                .build();
-
-        doThrow(new RuntimeException("test")).when(indicesService).verifyIndexMetadata(bad, bad);
-        doThrow(new AssertionError("ugly index is already closed")).when(indicesService).verifyIndexMetadata(ugly, ugly);
-
-        final ClusterState newState = closeBadIndices(initialState, indicesService);
-        assertThat(newState.metaData().index(good.getIndex()).getState(), equalTo(IndexMetaData.State.OPEN));
-        assertThat(newState.metaData().index(bad.getIndex()).getState(), equalTo(IndexMetaData.State.CLOSE));
-        assertThat(newState.metaData().index(ugly.getIndex()).getState(), equalTo(IndexMetaData.State.CLOSE));
     }
 
     public void testUpdateRoutingTable() {

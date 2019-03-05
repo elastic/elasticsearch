311d6f1c44d5abd3ba6101fcb64561a71495d903
Tim Brooks <tim@uncontended.net>
2019-03-05 11:48:07 -0700

    Changes

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/transport/netty4/DualStackHandler.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/transport/netty4/DualStackHandler.java
@@ -10,16 +10,15 @@ import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
 import io.netty.handler.ssl.SslHandler;
+import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.transport.netty4.Netty4Utils;
 import org.elasticsearch.xpack.core.security.transport.DualStackCoordinator;
-import org.elasticsearch.xpack.core.security.transport.ESMessageHelper;
 
 import javax.net.ssl.SSLEngine;
 
 import static org.elasticsearch.transport.netty4.Netty4Transport.CHANNEL_KEY;
 
-public class DualStackHandler extends ChannelDuplexHandler {
-
+final class DualStackHandler extends ChannelDuplexHandler {
 
     static final String HANDLER_NAME = "dual_tls_stack_handler";
     private final SSLEngine engine;
@@ -36,12 +35,12 @@ public class DualStackHandler extends ChannelDuplexHandler {
             ByteBuf in = (ByteBuf) msg;
             if (in.readableBytes() < 2) {
                 return;
-            } else if (ESMessageHelper.isPlaintextElasticsearchMessage(Netty4Utils.toBytesReference(in)) &&
+            } else if (isPlaintextElasticsearchMessage(Netty4Utils.toBytesReference(in)) &&
                 coordinator.isDualStackEnabled()) {
                 coordinator.registerPlaintextChannel(ctx.channel().attr(CHANNEL_KEY).get());
                 ctx.pipeline().remove(this);
             } else {
-                ctx.pipeline().addAfter(HANDLER_NAME,"sslhandler", new SslHandler(engine));
+                ctx.pipeline().addAfter(HANDLER_NAME, "sslhandler", new SslHandler(engine));
                 ctx.pipeline().remove(this);
             }
 
@@ -59,5 +58,9 @@ public class DualStackHandler extends ChannelDuplexHandler {
     @Override
     public void flush(ChannelHandlerContext ctx) {
         assert false : "Should not flush before receiving read";
+    }
+
+    private static boolean isPlaintextElasticsearchMessage(BytesReference reference) {
+        return reference.get(0) == 'E' && reference.get(1) == 'S';
     }
 }

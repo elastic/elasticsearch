e02fb912663a06b74ce81f7a2fa8e0b4bc35ef3c
Armin Braun <me@obrown.io>
2019-03-05 07:58:36 +0100

    bring back assertion

--- server/src/main/java/org/elasticsearch/action/support/replication/TransportReplicationAction.java
+++ server/src/main/java/org/elasticsearch/action/support/replication/TransportReplicationAction.java
@@ -21,6 +21,7 @@ package org.elasticsearch.action.support.replication;
 
 import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.apache.lucene.store.AlreadyClosedException;
+import org.elasticsearch.Assertions;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ExceptionsHelper;
 import org.elasticsearch.Version;
@@ -1031,6 +1032,13 @@ public abstract class TransportReplicationAction<
 
         @Override
         public void perform(Request request, ActionListener<PrimaryResult<ReplicaRequest, Response>> listener) {
+            if (Assertions.ENABLED) {
+                listener = ActionListener.map(listener, result -> {
+                    assert result.replicaRequest() == null || result.finalFailure == null : "a replica request [" + result.replicaRequest()
+                        + "] with a primary failure [" + result.finalFailure + "]";
+                    return result;
+                });
+            }
             shardOperationOnPrimary(request, indexShard, listener);
         }
 

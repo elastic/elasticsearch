f7e329e47ea2efc2b61c3cfe373399469b3db1f4
Jason Tedor <jason@tedor.me>
2019-03-07 22:18:16 -0500

    Fix Watcher stats class cast exception
    The watcher stats implementation tries to look at all queued watches
    before preparing the result. We want to cast these to a
    WatchExecutionTask to extract the context to prepare the stats for
    queued watches. The problem is that not all tasks on the watcher queue
    were WatchExecutionTask. This is because a manually executed watch was
    not even at all wrapped in a WatchExecutionTask. Moreover, we were using
    ExecutorService#submit(Runnable) which would wrap the Runnable in a
    FutureTask<?>. This commit addresses this by using a WatchExecutionTask,
    and also using ExecutorService#execute(Runnable) so that no wrapping
    occurs. This will let us continue with the assumption that all queued
    tasks are WatchExecutionTasks.

--- x-pack/plugin/watcher/src/main/java/org/elasticsearch/xpack/watcher/execution/ExecutionService.java
+++ x-pack/plugin/watcher/src/main/java/org/elasticsearch/xpack/watcher/execution/ExecutionService.java
@@ -67,6 +67,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
@@ -534,12 +535,12 @@ public class ExecutionService {
     // the watch execution task takes another runnable as parameter
     // the best solution would be to move the whole execute() method, which is handed over as ctor parameter
     // over into this class, this is the quicker way though
-    static final class WatchExecutionTask implements Runnable {
+    public static final class WatchExecutionTask implements Runnable {
 
         private final WatchExecutionContext ctx;
         private final Runnable runnable;
 
-        WatchExecutionTask(WatchExecutionContext ctx, Runnable runnable) {
+        public WatchExecutionTask(WatchExecutionContext ctx, Runnable runnable) {
             this.ctx = ctx;
             this.runnable = runnable;
         }

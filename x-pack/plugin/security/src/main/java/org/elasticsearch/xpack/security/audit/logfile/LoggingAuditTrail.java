ce3248884955b8f15eab665d4e810b34e297ac95
Albert Zaharovits <albert.zaharovits@elastic.co>
2019-03-11 17:35:47 +0200

    LoggingAuditTrail correctly handle ReplicatedWriteRequest (and BulkShardRequests)

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/audit/logfile/LoggingAuditTrail.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/audit/logfile/LoggingAuditTrail.java
@@ -8,6 +8,7 @@ package org.elasticsearch.xpack.security.audit.logfile;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.StringMapMessage;
 import org.elasticsearch.action.IndicesRequest;
+import org.elasticsearch.action.support.replication.ReplicationRequest;
 import org.elasticsearch.cluster.ClusterChangedEvent;
 import org.elasticsearch.cluster.ClusterStateListener;
 import org.elasticsearch.cluster.node.DiscoveryNode;
@@ -806,13 +807,15 @@ public class LoggingAuditTrail implements AuditTrail, ClusterStateListener {
 
 
     private static Optional<String[]> indices(TransportMessage message) {
+        final String[] indices;
         if (message instanceof IndicesRequest) {
-            final String[] indices = ((IndicesRequest) message).indices();
-            if (indices != null) {
-                return Optional.of(((IndicesRequest) message).indices());
-            }
+            indices = ((IndicesRequest) message).indices();
+        } else if (message instanceof ReplicationRequest) {
+            indices = ((ReplicationRequest<?>) message).indices();
+        } else {
+            indices = null;
         }
-        return Optional.empty();
+        return Optional.ofNullable(indices);
     }
 
     private static String effectiveRealmName(Authentication authentication) {

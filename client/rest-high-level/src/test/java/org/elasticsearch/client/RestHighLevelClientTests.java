105cadcc5c07bd1cbbbd78f742f37f0f4d4d6c78
David Kyle <david.kyle@elastic.co>
2019-03-14 10:06:37 +0000

    [ML-Dataframe] Add Data Frame client to the Java HLRC (#39921)
    Adds DataFrameClient to the Java HLRC and implements PUT and 
    DELETE data frame transform.

--- client/rest-high-level/src/test/java/org/elasticsearch/client/RestHighLevelClientTests.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/RestHighLevelClientTests.java
@@ -784,8 +784,9 @@ public class RestHighLevelClientTests extends ESTestCase {
                                 apiName.startsWith("security.") == false &&
                                 apiName.startsWith("index_lifecycle.") == false &&
                                 apiName.startsWith("ccr.") == false &&
+                                apiName.startsWith("data_frame") == false &&
                                 apiName.endsWith("freeze") == false &&
-                                // IndicesClientIT.getIndexTemplate should be renamed "getTemplate" in version 8.0 when we 
+                                // IndicesClientIT.getIndexTemplate should be renamed "getTemplate" in version 8.0 when we
                                 // can get rid of 7.0's deprecated "getTemplate"
                                 apiName.equals("indices.get_index_template") == false) {
                                 apiNotFound.add(apiName);

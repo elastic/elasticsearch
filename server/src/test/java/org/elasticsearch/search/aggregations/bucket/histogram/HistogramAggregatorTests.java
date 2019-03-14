6d986f10459a6344c10baf3c81675ea361186956
Mark Tozzi <mark.tozzi@gmail.com>
2019-03-14 14:13:05 -0400

    very minimal range histo test

--- server/src/test/java/org/elasticsearch/search/aggregations/bucket/histogram/HistogramAggregatorTests.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/bucket/histogram/HistogramAggregatorTests.java
@@ -19,6 +19,7 @@
 
 package org.elasticsearch.search.aggregations.bucket.histogram;
 
+import org.apache.lucene.document.BinaryDocValuesField;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.SortedNumericDocValuesField;
 import org.apache.lucene.index.IndexReader;
@@ -26,11 +27,15 @@ import org.apache.lucene.index.RandomIndexWriter;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.NumericUtils;
 import org.elasticsearch.index.mapper.MappedFieldType;
 import org.elasticsearch.index.mapper.NumberFieldMapper;
+import org.elasticsearch.index.mapper.RangeFieldMapper;
 import org.elasticsearch.search.aggregations.AggregatorTestCase;
 import org.elasticsearch.search.aggregations.support.AggregationInspectionHelper;
+
+import java.util.Collections;
 
 public class HistogramAggregatorTests extends AggregatorTestCase {
 
@@ -93,6 +98,32 @@ public class HistogramAggregatorTests extends AggregatorTestCase {
                 assertEquals(1, histogram.getBuckets().get(3).getDocCount());
                 assertTrue(AggregationInspectionHelper.hasValue(histogram));
             }
+        }
+    }
+
+    public void testDoubleRanges() throws Exception {
+
+        RangeFieldMapper.RangeType rangeType = RangeFieldMapper.RangeType.DOUBLE;
+        try (Directory dir = newDirectory();
+             RandomIndexWriter w = new RandomIndexWriter(random(), dir)) {
+            Document doc = new Document();
+            BytesRef encodedRange =
+                rangeType.encodeRanges(Collections.singleton(new RangeFieldMapper.Range(rangeType, 1.0D, 3.0D, true, true)));
+            doc.add(new BinaryDocValuesField("field", encodedRange));
+            w.addDocument(doc);
+
+            HistogramAggregationBuilder aggBuilder = new HistogramAggregationBuilder("my_agg")
+                .field("field")
+                .interval(5);
+            MappedFieldType fieldType = new RangeFieldMapper.Builder("field", rangeType).fieldType();
+            fieldType.setName("field");
+
+            try (IndexReader reader = w.getReader()) {
+                IndexSearcher searcher = new IndexSearcher(reader);
+                InternalHistogram histogram = search(searcher, new MatchAllDocsQuery(), aggBuilder, fieldType);
+                assertEquals(1, histogram.getBuckets().size());
+            }
+
         }
     }
 

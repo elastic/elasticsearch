e3366da56acd92d95624afa876354fe3be9c07dc
Mayya Sharipova <mayya.sharipova@elastic.co>
2019-03-15 18:31:21 -0400

    Revert back origin

--- server/src/main/java/org/elasticsearch/index/query/DistanceFeatureQueryBuilder.java
+++ server/src/main/java/org/elasticsearch/index/query/DistanceFeatureQueryBuilder.java
@@ -122,7 +122,8 @@ public class DistanceFeatureQueryBuilder extends AbstractQueryBuilder<DistanceFe
         }
         Object originObj = origin.origin();
         if (fieldType instanceof DateFieldType) {
-            long originLong = ((DateFieldType) fieldType).parseToLong(originObj, true, null, null, context);
+            long originLong = (originObj instanceof Long) ? (Long) originObj :
+                ((DateFieldType) fieldType).parseToLong(originObj, true, null, null, context);
             TimeValue pivotVal = TimeValue.parseTimeValue(pivot, TimeValue.timeValueHours(24),
                 DistanceFeatureQueryBuilder.class.getSimpleName() + ".pivot");
             if (((DateFieldType) fieldType).resolution() == DateFieldMapper.Resolution.MILLISECONDS) {

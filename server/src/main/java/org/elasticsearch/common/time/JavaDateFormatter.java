ffcf861b84b2816084e618fce71bb2bc637132f5
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2019-03-18 14:43:07 +0100

    unused import

--- server/src/main/java/org/elasticsearch/common/time/JavaDateFormatter.java
+++ server/src/main/java/org/elasticsearch/common/time/JavaDateFormatter.java
@@ -37,7 +37,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 class JavaDateFormatter implements DateFormatter {
 

c12cb64fbcb529b4160c7ea7b3bf684702cfaa2e
Zachary Tong <zach@elastic.co>
2019-03-19 12:30:00 -0400

    Add version skip, another test

--- server/src/test/java/org/elasticsearch/search/aggregations/pipeline/BucketHelpersTests.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/pipeline/BucketHelpersTests.java
@@ -25,6 +25,7 @@ import org.elasticsearch.search.aggregations.AggregationExecutionException;
 import org.elasticsearch.search.aggregations.Aggregations;
 import org.elasticsearch.search.aggregations.InternalMultiBucketAggregation;
 import org.elasticsearch.search.aggregations.bucket.MultiBucketsAggregation;
+import org.elasticsearch.search.aggregations.metrics.InternalTDigestPercentiles;
 import org.elasticsearch.test.ESTestCase;
 
 import java.io.IOException;
@@ -32,6 +33,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.mock;
 
 public class BucketHelpersTests extends ESTestCase {
 
@@ -106,5 +108,78 @@ public class BucketHelpersTests extends ESTestCase {
 
         assertThat(e.getMessage(), equalTo("buckets_path must reference either a number value or a single value numeric " +
             "metric aggregation, got: [Object[]] at aggregation [foo]"));
+    }
+
+    public void testReturnMultiValueObject() {
+
+        MultiBucketsAggregation agg = new MultiBucketsAggregation() {
+            @Override
+            public List<? extends Bucket> getBuckets() {
+                return null;
+            }
+
+            @Override
+            public String getName() {
+                return "foo";
+            }
+
+            @Override
+            public String getType() {
+                return null;
+            }
+
+            @Override
+            public Map<String, Object> getMetaData() {
+                return null;
+            }
+
+            @Override
+            public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+                return null;
+            }
+        };
+
+        InternalMultiBucketAggregation.InternalBucket bucket = new InternalMultiBucketAggregation.InternalBucket() {
+            @Override
+            public void writeTo(StreamOutput out) throws IOException {
+
+            }
+
+            @Override
+            public Object getKey() {
+                return null;
+            }
+
+            @Override
+            public String getKeyAsString() {
+                return null;
+            }
+
+            @Override
+            public long getDocCount() {
+                return 0;
+            }
+
+            @Override
+            public Aggregations getAggregations() {
+                return null;
+            }
+
+            @Override
+            public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+                return null;
+            }
+
+            @Override
+            public Object getProperty(String containingAggName, List<String> path) {
+                return mock(InternalTDigestPercentiles.class);
+            }
+        };
+
+        AggregationExecutionException e = expectThrows(AggregationExecutionException.class,
+            () -> BucketHelpers.resolveBucketValue(agg, bucket, "foo>bar", BucketHelpers.GapPolicy.SKIP));
+
+        assertThat(e.getMessage(), equalTo("buckets_path must reference either a number value or a single value numeric metric aggregation, " +
+            "but [foo] contains multiple values. Please specify which to use."));
     }
 }

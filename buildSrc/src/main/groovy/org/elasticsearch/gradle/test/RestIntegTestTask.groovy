eded9ca7577bb43525b487f0df623385a5525119
Alpar Torok <torokalpar@gmail.com>
2019-03-27 08:37:04 +0200

    Testclusters: convert plugin repository-s3  (#40399)
    * Add support for setting and keystore settings
    * system properties and env var config
    * use testclusters for repository-s3
    * Some cleanup of the build.gradle file for plugin-s3
    * add runner {} to rest integ test task

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/test/RestIntegTestTask.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/test/RestIntegTestTask.groovy
@@ -62,13 +62,13 @@ public class RestIntegTestTask extends DefaultTask {
             clusterConfig = project.extensions.create("${name}Cluster", ClusterConfiguration.class, project)
         } else {
             project.testClusters {
-                integTestCluster {
+                "$name" {
                     distribution = 'INTEG_TEST'
                     version = project.version
                     javaHome = project.file(project.ext.runtimeJavaHome)
                 }
             }
-            runner.useCluster project.testClusters.integTestCluster
+            runner.useCluster project.testClusters."$name"
         }
 
         // override/add more for rest tests
@@ -81,7 +81,7 @@ public class RestIntegTestTask extends DefaultTask {
                 throw new IllegalArgumentException("tests.rest.cluster and tests.cluster must both be null or non-null")
             }
             if (usesTestclusters == true) {
-                ElasticsearchNode node = project.testClusters.integTestCluster
+                ElasticsearchNode node = project.testClusters."${name}"
                 runner.systemProperty('tests.rest.cluster', {node.allHttpSocketURI.join(",") })
                 runner.systemProperty('tests.config.dir', {node.getConfigDir()})
                 runner.systemProperty('tests.cluster', {node.transportPortURI})
@@ -185,6 +185,10 @@ public class RestIntegTestTask extends DefaultTask {
     @Override
     public Task mustRunAfter(Object... tasks) {
         clusterInit.mustRunAfter(tasks)
+    }
+
+    public void runner(Closure configure) {
+        project.tasks.getByName("${name}Runner").configure(configure)
     }
 
     /** Print out an excerpt of the log from the given node. */

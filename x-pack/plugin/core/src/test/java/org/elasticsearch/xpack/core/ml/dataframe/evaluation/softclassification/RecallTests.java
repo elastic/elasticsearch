1cb3dd1bf924c38cdcf72b3a51aed794ee449096
Dimitris Athanasiou <dimitris@elastic.co>
2019-03-28 17:30:49 +0200

    Add unit tests for evaluate of precision,recall,confusion_matrix

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/ml/dataframe/evaluation/softclassification/RecallTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/ml/dataframe/evaluation/softclassification/RecallTests.java
@@ -5,13 +5,22 @@
  */
 package org.elasticsearch.xpack.core.ml.dataframe.evaluation.softclassification;
 
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.filter.Filter;
 import org.elasticsearch.test.AbstractSerializingTestCase;
+import org.elasticsearch.xpack.core.ml.dataframe.evaluation.EvaluationMetricResult;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class RecallTests extends AbstractSerializingTestCase<Recall> {
 
@@ -37,5 +46,48 @@ public class RecallTests extends AbstractSerializingTestCase<Recall> {
             thresholds.add(randomDouble());
         }
         return new Recall(thresholds);
+    }
+
+    public void testEvaluate() {
+        SoftClassificationMetric.ClassInfo classInfo = mock(SoftClassificationMetric.ClassInfo.class);
+        when(classInfo.getName()).thenReturn("foo");
+
+        Aggregations aggs = new Aggregations(Arrays.asList(
+            createFilterAgg("recall_foo_at_0.25_TP", 1L),
+            createFilterAgg("recall_foo_at_0.25_FN", 4L),
+            createFilterAgg("recall_foo_at_0.5_TP", 3L),
+            createFilterAgg("recall_foo_at_0.5_FN", 1L),
+            createFilterAgg("recall_foo_at_0.75_TP", 5L),
+            createFilterAgg("recall_foo_at_0.75_FN", 0L)
+        ));
+
+        Recall recall = new Recall(Arrays.asList(0.25, 0.5, 0.75));
+        EvaluationMetricResult result = recall.evaluate(classInfo, aggs);
+
+        String expected = "{\"0.25\":0.2,\"0.5\":0.75,\"0.75\":1.0}";
+        assertThat(Strings.toString(result), equalTo(expected));
+    }
+
+    public void testEvaluate_GivenZeroTpAndFp() {
+        SoftClassificationMetric.ClassInfo classInfo = mock(SoftClassificationMetric.ClassInfo.class);
+        when(classInfo.getName()).thenReturn("foo");
+
+        Aggregations aggs = new Aggregations(Arrays.asList(
+            createFilterAgg("recall_foo_at_1.0_TP", 0L),
+            createFilterAgg("recall_foo_at_1.0_FN", 0L)
+        ));
+
+        Recall recall = new Recall(Arrays.asList(1.0));
+        EvaluationMetricResult result = recall.evaluate(classInfo, aggs);
+
+        String expected = "{\"1.0\":0.0}";
+        assertThat(Strings.toString(result), equalTo(expected));
+    }
+
+    private static Filter createFilterAgg(String name, long docCount) {
+        Filter agg = mock(Filter.class);
+        when(agg.getName()).thenReturn(name);
+        when(agg.getDocCount()).thenReturn(docCount);
+        return agg;
     }
 }

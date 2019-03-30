c4b10b6d2f6a70f42fb0025044be53ae83635986
Marios Trivyzas <matriv@users.noreply.github.com>
2019-03-30 16:10:42 +0100

    SQL: Fix displa size for DATE/DATETIME
    A full format for a DATETIME would be:
    `2019-03-30T10:20:30.123456789+10:00` (example when asking for
    `current_timestamp(9)`) which is 35 chars long.
    
    For DATE a full format would be: `2019-03-30T00:00:00.000+10:00`
    which is 29 chars long.

--- x-pack/plugin/sql/qa/src/main/java/org/elasticsearch/xpack/sql/qa/SqlProtocolTestCase.java
+++ x-pack/plugin/sql/qa/src/main/java/org/elasticsearch/xpack/sql/qa/SqlProtocolTestCase.java
@@ -67,11 +67,16 @@ public abstract class SqlProtocolTestCase extends ESRestTestCase {
     
     public void testDateTimes() throws IOException {
         assertQuery("SELECT CAST('2019-01-14T12:29:25.000Z' AS DATETIME)", "CAST('2019-01-14T12:29:25.000Z' AS DATETIME)",
-            "datetime", "2019-01-14T12:29:25.000Z", 24);
+            "datetime", "2019-01-14T12:29:25.000Z", 35);
         assertQuery("SELECT CAST(-26853765751000 AS DATETIME)", "CAST(-26853765751000 AS DATETIME)",
-            "datetime", "1119-01-15T12:37:29.000Z", 24);
+            "datetime", "1119-01-15T12:37:29.000Z", 35);
         assertQuery("SELECT CAST(CAST('-26853765751000' AS BIGINT) AS DATETIME)", "CAST(CAST('-26853765751000' AS BIGINT) AS DATETIME)",
-            "datetime", "1119-01-15T12:37:29.000Z", 24);
+            "datetime", "1119-01-15T12:37:29.000Z", 35);
+
+        assertQuery("SELECT CAST('2019-01-14' AS DATE)", "CAST('2019-01-14' AS DATE)",
+            "date", "2019-01-14T00:00:00.000Z", 29);
+        assertQuery("SELECT CAST(-26853765751000 AS DATE)", "CAST(-26853765751000 AS DATE)",
+            "date", "1119-01-15T00:00:00.000Z", 29);
     }
     
     public void testIPs() throws IOException {

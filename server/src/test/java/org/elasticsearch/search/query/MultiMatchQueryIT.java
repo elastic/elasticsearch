0fe2ac1ad2f2b464f80577537a1085555a3d1617
Julie Tibshirani <julie.tibshirani@elastic.co>
2019-04-02 13:11:17 -0700

    Rename 'json' to 'embedded_json'. (#40712)
    One concern around the name `json` is that because the entire document is JSON,
    new users may see this field and think that they should always use it. We
    thought that a more verbose name like `embedded_json` would help convey that the
    field type has a special, non-obvious purpose.
    
    This commit updates documentation references to `embedded_json`, but leaves the
    `JsonField` naming in the code to avoid very long class names.

--- server/src/test/java/org/elasticsearch/search/query/MultiMatchQueryIT.java
+++ server/src/test/java/org/elasticsearch/search/query/MultiMatchQueryIT.java
@@ -717,7 +717,7 @@ public class MultiMatchQueryIT extends ESIntegTestCase {
             .startObject("_doc")
                 .startObject("properties")
                     .startObject("headers")
-                        .field("type", "json")
+                        .field("type", "embedded_json")
                         .field("split_queries_on_whitespace", true)
                     .endObject()
                 .endObject()

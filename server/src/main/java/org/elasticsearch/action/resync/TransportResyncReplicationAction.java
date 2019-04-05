922a70ce326c8d6b98aedc366559e7e746f46f6f
David Turner <david.turner@elastic.co>
2019-04-05 09:21:23 +0100

    Remove unused import
    Relates #40863

--- server/src/main/java/org/elasticsearch/action/resync/TransportResyncReplicationAction.java
+++ server/src/main/java/org/elasticsearch/action/resync/TransportResyncReplicationAction.java
@@ -19,7 +19,6 @@
 package org.elasticsearch.action.resync;
 
 import org.apache.logging.log4j.message.ParameterizedMessage;
-import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.replication.ReplicationOperation;

9052b52eaaf141424c3ab277abbdec5cdc36a142
Albert Zaharovits <albert.zaharovits@elastic.co>
2019-04-05 13:29:39 +0300

    threadPool.executor(Names.GENERIC).submit(this);

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/ExpiredTokenRemover.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/ExpiredTokenRemover.java
@@ -101,7 +101,7 @@ final class ExpiredTokenRemover extends AbstractRunnable {
 
     void submit(ThreadPool threadPool) {
         if (inProgress.compareAndSet(false, true)) {
-            threadPool.executor(Names.GENERIC).submit(() -> doRun());
+            threadPool.executor(Names.GENERIC).submit(this);
         }
     }
 

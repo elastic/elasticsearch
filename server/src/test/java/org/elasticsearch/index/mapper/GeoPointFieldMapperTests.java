013a0140ff07bccb4c6663f43f7c6769df1e7a0f
Henning Andersen <henning.andersen@elastic.co>
2019-04-08 16:51:58 +0200

    Geo Point parse error fix (#40447)
    Fixed compilation on 6.7

--- server/src/test/java/org/elasticsearch/index/mapper/GeoPointFieldMapperTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/GeoPointFieldMapperTests.java
@@ -524,12 +524,12 @@ public class GeoPointFieldMapperTests extends ESSingleNodeTestCase {
                 .startObject().field("location", "NaN,12").endObject()
             ), XContentType.JSON)).rootDoc().getField("location"), nullValue());
 
-        assertThat(defaultMapper.parse(new SourceToParse("test", "type", "1",
+        assertThat(defaultMapper.parse(SourceToParse.source("test", "type", "1",
             BytesReference.bytes(XContentFactory.jsonBuilder()
                 .startObject().startObject("location").nullField("lat").field("lon", 1).endObject().endObject()
             ), XContentType.JSON)).rootDoc().getField("location"), nullValue());
 
-        assertThat(defaultMapper.parse(new SourceToParse("test", "type", "1",
+        assertThat(defaultMapper.parse(SourceToParse.source("test", "type", "1",
             BytesReference.bytes(XContentFactory.jsonBuilder()
                 .startObject().startObject("location").nullField("lat").nullField("lon").endObject().endObject()
             ), XContentType.JSON)).rootDoc().getField("location"), nullValue());

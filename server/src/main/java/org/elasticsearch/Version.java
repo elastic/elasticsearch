d160f29b908728b73a1558e68ad209a2bfdea43c
Jason Tedor <jason@tedor.me>
2019-04-09 10:46:41 -0400

    Add 6.8 version constant
    This commit adds the 6.8 version constant to the master branch.

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -128,6 +128,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_6_7_1 = new Version(V_6_7_1_ID, org.apache.lucene.util.Version.LUCENE_7_7_0);
     public static final int V_6_7_2_ID = 6070299;
     public static final Version V_6_7_2 = new Version(V_6_7_2_ID, org.apache.lucene.util.Version.LUCENE_7_7_0);
+    public static final int V_6_8_0_ID = 6080099;
+    public static final Version V_6_8_0 = new Version(V_6_8_0_ID, org.apache.lucene.util.Version.LUCENE_7_7_0);
     public static final int V_7_0_0_ID = 7000099;
     public static final Version V_7_0_0 = new Version(V_7_0_0_ID, org.apache.lucene.util.Version.LUCENE_8_0_0);
     public static final int V_7_0_1_ID = 7000199;
@@ -162,6 +164,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
                 return V_7_0_1;
             case V_7_0_0_ID:
                 return V_7_0_0;
+            case V_6_8_0_ID:
+                return V_6_8_0;
             case V_6_7_1_ID:
                 return V_6_7_1;
             case V_6_7_2_ID:

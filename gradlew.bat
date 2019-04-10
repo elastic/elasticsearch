c53c2227828b6433c55d3eff68eacdc623ee42e6
Alpar Torok <torokalpar@gmail.com>
2019-04-10 17:56:11 +0300

    Increase the heap of the Gradle runner
    The Gradle Daemon actually running the build does so  with a 2GB heap.
    This PT changes the heap configuration of the gradle process that talks
    with the daemon to trigger the builds and relies the messages.
    
    Relates to #41061

--- gradlew.bat
+++ gradlew.bat
@@ -30,7 +30,7 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set DEFAULT_JVM_OPTS="-Xmx128m" "-Xms128m"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome

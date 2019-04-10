39b069221e43c1639cebb8a1cc4f4ba56c7d16f6
jimczi <jimczi@apache.org>
2019-04-10 12:30:36 +0200

    fix checkstyle

--- server/src/main/java/org/elasticsearch/index/search/MultiMatchQuery.java
+++ server/src/main/java/org/elasticsearch/index/search/MultiMatchQuery.java
@@ -38,16 +38,12 @@ import org.elasticsearch.index.query.QueryShardContext;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static org.elasticsearch.common.lucene.search.Queries.newLenientFieldQuery;
-import static org.elasticsearch.common.lucene.search.Queries.newUnmappedFieldQuery;
 
 public class MultiMatchQuery extends MatchQuery {
 

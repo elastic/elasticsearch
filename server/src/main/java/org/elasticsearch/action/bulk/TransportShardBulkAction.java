23ea5d5c74963de0ddf974d6ca6712c3f796b70e
Armin Braun <me@obrown.io>
2019-04-12 08:29:08 +0200

    CR: add assertion

--- server/src/main/java/org/elasticsearch/action/bulk/TransportShardBulkAction.java
+++ server/src/main/java/org/elasticsearch/action/bulk/TransportShardBulkAction.java
@@ -398,6 +398,7 @@ public class TransportShardBulkAction extends TransportWriteAction<BulkShardRequ
             final DocWriteResponse writeResponse = response.getResponse();
             final long seqNum = failure == null ? writeResponse.getSeqNo() : failure.getSeqNo();
             if (seqNum == SequenceNumbers.UNASSIGNED_SEQ_NO) {
+                assert failure != null;
                 continue;
             }
             if (failure == null) {

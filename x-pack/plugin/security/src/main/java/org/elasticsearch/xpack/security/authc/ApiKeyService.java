36862020fc509e4fcb1c29f96d8a4bcd118fcd85
Tim Vernum <tim.vernum@elastic.co>
2019-04-15 17:32:18 +1000

    Remove Version.V_6_x_x constants use in security
    This removes some use of the v6 constants in various parts of
    security.
    Mostly this is BWC testing code, which is no longer needed as ES8 will
    not need to maintain compatibility with ES6.
    
    Relates: #41164

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/ApiKeyService.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/ApiKeyService.java
@@ -233,12 +233,6 @@ public class ApiKeyService {
         final Instant expiration = getApiKeyExpiration(created, request);
         final SecureString apiKey = UUIDs.randomBase64UUIDSecureString();
         final Version version = clusterService.state().nodes().getMinNodeVersion();
-        if (version.before(Version.V_6_7_0)) {
-            logger.warn(
-                    "nodes prior to the minimum supported version for api keys {} exist in the cluster;"
-                            + " these nodes will not be able to use api keys",
-                    Version.V_6_7_0);
-        }
 
         final char[] keyHash = hasher.hash(apiKey);
         try (XContentBuilder builder = XContentFactory.jsonBuilder()) {

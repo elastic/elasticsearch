005d49bd110eb82b594f3741c210b4e606475f16
Jake Landis <jake.landis@elastic.co>
2019-04-15 16:15:30 -0500

    fix checkSnapshot

--- qa/full-cluster-restart/src/test/java/org/elasticsearch/upgrades/FullClusterRestartIT.java
+++ qa/full-cluster-restart/src/test/java/org/elasticsearch/upgrades/FullClusterRestartIT.java
@@ -1177,8 +1177,7 @@ public class FullClusterRestartIT extends AbstractFullClusterRestartTestCase {
             expectedTemplate.put("index_patterns", singletonList("evil_*"));
         }
         expectedTemplate.put("settings", singletonMap("index", singletonMap("number_of_shards", "1")));
-        if (isRunningAgainstOldCluster() && getOldClusterVersion().onOrAfter(Version.V_6_7_0) &&
-            getOldClusterVersion().before(Version.V_7_0_0)) {
+        if (tookOnVersion.before(Version.V_7_0_0)) {
             expectedTemplate.put("mappings", singletonMap("doc", singletonMap("_source", singletonMap("enabled", true))));
         } else {
             expectedTemplate.put("mappings", singletonMap("_source", singletonMap("enabled", true)));

27a1401bce0993af28176d83c9ffd95944eeaf02
Costin Leau <costin@users.noreply.github.com>
2019-04-18 22:44:16 +0300

    SQL: Predicate diff takes into account all values (#41346)
    Fix bug in predicate subtraction that caused the evaluation to be
    skipped on the first mismatch instead of evaluating the whole list. In
    some cases this caused not only an incorrect result but one that kept on
    growing causing the engine to bail
    
    Fix #40835
    
    (cherry picked from commit bd2b33d6eaca616a5acd846204e2d12f905854d4)

--- x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/capabilities/UnresolvedException.java
+++ x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/capabilities/UnresolvedException.java
@@ -7,16 +7,12 @@ package org.elasticsearch.xpack.sql.capabilities;
 
 import org.elasticsearch.xpack.sql.ServerSqlException;
 
-import java.util.Locale;
-
-import static java.lang.String.format;
-
 /**
  * Thrown when we accidentally attempt to resolve something on on an unresolved entity. Throwing this
  * is always a bug.
  */
 public class UnresolvedException extends ServerSqlException {
     public UnresolvedException(String action, Object target) {
-        super(format(Locale.ROOT, "Invalid call to %s on an unresolved object %s", action, target));
+        super("Invalid call to {} on an unresolved object {}", action, target);
     }
 }

152c4f8efe60d010bbef21842bd7fb7c2dc6d651
Simon Willnauer <simonw@apache.org>
2019-04-18 19:14:23 +0200

    Mark searcher as accessed in acquireSearcher (#41335)
    This fixes an issue where every N seconds a slow search request is triggered
    since the searcher access time is not set unless the shard is idle. This change
    moves to a more pro-active approach setting the searcher as accessed all the time.

--- server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
+++ server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
@@ -3199,6 +3199,12 @@ public class IndexShardTests extends IndexShardTestCase {
             // now loop until we are fast enough... shouldn't take long
             primary.awaitShardSearchActive(aBoolean -> {});
         } while (primary.isSearchIdle());
+
+        assertBusy(() -> assertTrue(primary.isSearchIdle()));
+        do {
+            // now loop until we are fast enough... shouldn't take long
+            primary.acquireSearcher("test").close();
+        } while (primary.isSearchIdle());
         closeShards(primary);
     }
 

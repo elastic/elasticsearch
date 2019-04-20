1eaf2699001aa91d200fab1dadb6b7871fa1bc50
Jason Tedor <jason@tedor.me>
2019-04-20 08:22:56 -0400

    Remove script engine from X-Pack plugin (#41387)
    The X-Pack plugin implements ScriptEngine yet it does not actually
    implement any of the methods on the interface, effectively making this a
    no-op. This commit removes this interface from the X-Pack plugin.

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/LocalStateCompositeXPackPlugin.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/LocalStateCompositeXPackPlugin.java
@@ -256,7 +256,6 @@ public class LocalStateCompositeXPackPlugin extends XPackPlugin implements Scrip
     @Override
     public List<ScriptContext<?>> getContexts() {
         List<ScriptContext<?>> contexts = new ArrayList<>();
-        contexts.addAll(super.getContexts());
         filterPlugins(ScriptPlugin.class).stream().forEach(p -> contexts.addAll(p.getContexts()));
         return contexts;
     }

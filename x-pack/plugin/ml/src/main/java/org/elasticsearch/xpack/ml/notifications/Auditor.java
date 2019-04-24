f0b7829c8dd1316a75a7abeeb51889531257a961
Benjamin Trent <ben.w.trent@gmail.com>
2019-04-24 06:53:53 -0500

    [ML] refactoring the ML plugin to use the common auditor code (#41419)

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/notifications/Auditor.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/notifications/Auditor.java
@@ -5,71 +5,15 @@
  */
 package org.elasticsearch.xpack.ml.notifications;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.common.xcontent.ToXContent;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.xpack.core.ml.notifications.AuditMessage;
 import org.elasticsearch.xpack.core.ml.notifications.AuditorField;
+import org.elasticsearch.xpack.core.ml.notifications.AuditMessage;
 
-import java.io.IOException;
-import java.util.Objects;
-
-import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.elasticsearch.xpack.core.ClientHelper.ML_ORIGIN;
-import static org.elasticsearch.xpack.core.ClientHelper.executeAsyncWithOrigin;
 
-public class Auditor {
-
-    private static final Logger LOGGER = LogManager.getLogger(Auditor.class);
-
-    private final Client client;
-    private final String nodeName;
+public class Auditor extends org.elasticsearch.xpack.core.common.notifications.Auditor<AuditMessage> {
 
     public Auditor(Client client, String nodeName) {
-        this.client = Objects.requireNonNull(client);
-        this.nodeName = Objects.requireNonNull(nodeName);
-    }
-
-    public void info(String jobId, String message) {
-        indexDoc(AuditMessage.newInfo(jobId, message, nodeName));
-    }
-
-    public void warning(String jobId, String message) {
-        indexDoc(AuditMessage.newWarning(jobId, message, nodeName));
-    }
-
-    public void error(String jobId, String message) {
-        indexDoc(AuditMessage.newError(jobId, message, nodeName));
-    }
-
-    private void indexDoc(ToXContent toXContent) {
-        IndexRequest indexRequest = new IndexRequest(AuditorField.NOTIFICATIONS_INDEX);
-        indexRequest.source(toXContentBuilder(toXContent));
-        indexRequest.timeout(TimeValue.timeValueSeconds(5));
-        executeAsyncWithOrigin(client.threadPool().getThreadContext(), ML_ORIGIN, indexRequest, new ActionListener<IndexResponse>() {
-            @Override
-            public void onResponse(IndexResponse indexResponse) {
-                LOGGER.trace("Successfully persisted audit message");
-            }
-
-            @Override
-            public void onFailure(Exception e) {
-                LOGGER.debug("Error writing audit message", e);
-            }
-        }, client::index);
-    }
-
-    private XContentBuilder toXContentBuilder(ToXContent toXContent) {
-        try (XContentBuilder jsonBuilder = jsonBuilder()) {
-            return toXContent.toXContent(jsonBuilder, ToXContent.EMPTY_PARAMS);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        super(client, nodeName, AuditorField.NOTIFICATIONS_INDEX, ML_ORIGIN, AuditMessage.builder());
     }
 }

f65a86c258de78410365b9fdae873d278c017723
Tim Brooks <tim@uncontended.net>
2019-04-25 14:30:16 -0600

    Remove dedicated SSL network write buffer (#41283)
    This is related to #27260. Currently for the SSLDriver we allocate a
    dedicated network write buffer and encrypt the data into that buffer one
    buffer at a time. This requires constantly switching between encrypting
    and flushing. This commit adds a dedicated outbound buffer for SSL
    operations that will internally allocate new packet sized buffers as
    they are need (for writing encrypted data). This allows us to totally
    encrypt an operation before writing it to the network. Eventually it can
    be hooked up to buffer recycling.

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/transport/nio/SecurityNioHttpServerTransport.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/transport/nio/SecurityNioHttpServerTransport.java
@@ -22,6 +22,7 @@ import org.elasticsearch.nio.ChannelFactory;
 import org.elasticsearch.nio.InboundChannelBuffer;
 import org.elasticsearch.nio.NioSelector;
 import org.elasticsearch.nio.NioSocketChannel;
+import org.elasticsearch.nio.Page;
 import org.elasticsearch.nio.ServerChannelContext;
 import org.elasticsearch.nio.SocketChannelContext;
 import org.elasticsearch.threadpool.ThreadPool;
@@ -92,9 +93,9 @@ public class SecurityNioHttpServerTransport extends NioHttpServerTransport {
         @Override
         public NioHttpChannel createChannel(NioSelector selector, SocketChannel channel) throws IOException {
             NioHttpChannel httpChannel = new NioHttpChannel(channel);
-            Supplier<InboundChannelBuffer.Page> pageSupplier = () -> {
+            Supplier<Page> pageSupplier = () -> {
                 Recycler.V<byte[]> bytes = pageCacheRecycler.bytePage(false);
-                return new InboundChannelBuffer.Page(ByteBuffer.wrap(bytes.v()), bytes::close);
+                return new Page(ByteBuffer.wrap(bytes.v()), bytes::close);
             };
             HttpReadWriteHandler httpHandler = new HttpReadWriteHandler(httpChannel,SecurityNioHttpServerTransport.this,
                 handlingSettings, corsConfig);

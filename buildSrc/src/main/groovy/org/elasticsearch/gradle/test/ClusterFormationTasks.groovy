d81e4195960cccbec7172b85e3b4e19bdf31a983
David Kyle <david.kyle@elastic.co>
2019-04-25 12:04:35 +0100

    Revert "Clean up  clusters between tests (#41187)"
    This reverts commit 78e7f4a94c4a633100c86c02d073f7f6a26444b5.

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/test/ClusterFormationTasks.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/test/ClusterFormationTasks.groovy
@@ -936,8 +936,6 @@ class ClusterFormationTasks {
             }
             doLast {
                 project.delete(node.pidFile)
-                // Large tests can exhaust disk space, clean up on stop, but leave the data dir as some tests reuse it
-                project.delete(project.fileTree(node.baseDir).minus(project.fileTree(node.dataDir)))
             }
         }
     }

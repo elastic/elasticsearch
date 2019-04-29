56c6e4869ac8c10a89de1f31252f90fc3002a030
Yogesh Gaikwad <yogesh.gaikwad@elastic.co>
2019-04-29 13:58:47 +1000

    Revert "Suppress illegal access in plugin install (#41620)"
    This reverts commit f8e5beec9a3a3c932c4360a97ce395d6deb22a64.

--- distribution/src/bin/elasticsearch-plugin.bat
+++ distribution/src/bin/elasticsearch-plugin.bat
@@ -3,7 +3,6 @@
 setlocal enabledelayedexpansion
 setlocal enableextensions
 
-set ES_JAVA_OPTS="--add-opens java.base/sun.security.provider=ALL-UNNAMED %ES_JAVA_OPTS%"
 set ES_MAIN_CLASS=org.elasticsearch.plugins.PluginCli
 set ES_ADDITIONAL_CLASSPATH_DIRECTORIES=lib/tools/plugin-cli
 call "%~dp0elasticsearch-cli.bat" ^

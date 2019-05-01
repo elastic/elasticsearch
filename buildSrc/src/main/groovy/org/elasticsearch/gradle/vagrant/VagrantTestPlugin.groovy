ada1e9de287824a8fef42818275cc4ab5e343a23
Toby McLaughlin <toby@jarpy.net>
2019-05-02 08:53:33 +1000

    Run packaging tests on RHEL 8 (#41662)

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/vagrant/VagrantTestPlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/vagrant/VagrantTestPlugin.groovy
@@ -30,6 +30,7 @@ class VagrantTestPlugin implements Plugin<Project> {
             'oel-6',
             'oel-7',
             'opensuse-42',
+            'rhel-8',
             'sles-12',
             'ubuntu-1404',
             'ubuntu-1604',

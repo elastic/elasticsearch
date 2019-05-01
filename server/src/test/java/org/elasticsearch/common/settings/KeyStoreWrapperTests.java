26c72c96bdb248b595809a3307f4014c3d1b6a98
Jason Tedor <jason@tedor.me>
2019-05-01 07:21:16 -0400

    Fix imports in KeyStoreWrapperTests
    This commit addresses a checkstyle violation in KeyStoreWrapperTests,
    removing a leftover import.

--- server/src/test/java/org/elasticsearch/common/settings/KeyStoreWrapperTests.java
+++ server/src/test/java/org/elasticsearch/common/settings/KeyStoreWrapperTests.java
@@ -55,7 +55,6 @@ import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 

59228034f7bc351b8ea66b4b79064cc3e95ee458
Jay Modi <jaymode@users.noreply.github.com>
2019-05-01 09:22:31 -0600

    Bump 7.0 branch version to 7.0.2 (#41714)

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -132,7 +132,9 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_7_0_0 = new Version(V_7_0_0_ID, org.apache.lucene.util.Version.LUCENE_8_0_0);
     public static final int V_7_0_1_ID = 7000199;
     public static final Version V_7_0_1 = new Version(V_7_0_1_ID, org.apache.lucene.util.Version.LUCENE_8_0_0);
-    public static final Version CURRENT = V_7_0_1;
+    public static final int V_7_0_2_ID = 7000299;
+    public static final Version V_7_0_2 = new Version(V_7_0_2_ID, org.apache.lucene.util.Version.LUCENE_8_0_0);
+    public static final Version CURRENT = V_7_0_2;
 
 
     static {
@@ -146,6 +148,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_7_0_2_ID:
+                return V_7_0_2;
             case V_7_0_1_ID:
                 return V_7_0_1;
             case V_7_0_0_ID:

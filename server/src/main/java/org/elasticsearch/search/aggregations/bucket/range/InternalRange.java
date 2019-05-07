c85f285298eaf307c93ed3c912974cbc7e0af4b8
Luca Cavanna <javanna@users.noreply.github.com>
2019-05-07 10:54:47 +0200

    Move InternalAggregations to Writeable (#41841)
    Relates to #34389

--- server/src/main/java/org/elasticsearch/search/aggregations/bucket/range/InternalRange.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/bucket/range/InternalRange.java
@@ -263,7 +263,7 @@ public class InternalRange<B extends InternalRange.Bucket, R extends InternalRan
                 ? in.readString()
                 : in.readOptionalString();
             ranges.add(getFactory().createBucket(key, in.readDouble(), in.readDouble(), in.readVLong(),
-                    InternalAggregations.readAggregations(in), keyed, format));
+                    new InternalAggregations(in), keyed, format));
         }
         this.ranges = ranges;
     }

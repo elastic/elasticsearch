834fff7ab2df15295dd098723c750cbe60a59a70
David Kyle <david.kyle@elastic.co>
2019-05-07 09:42:18 +0100

    Address review comments

--- x-pack/plugin/data-frame/src/main/java/org/elasticsearch/xpack/dataframe/action/DataFrameNodes.java
+++ x-pack/plugin/data-frame/src/main/java/org/elasticsearch/xpack/dataframe/action/DataFrameNodes.java
@@ -15,7 +15,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class DataFrameNodes {
+public final class DataFrameNodes {
 
     private DataFrameNodes() {
     }

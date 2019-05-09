d48526923b6ee21965038336b599785d49fe69e3
Christian Mesh <christianmesh1@gmail.com>
2019-05-09 14:03:23 -0400

    Remove string.format from augment test

--- modules/lang-painless/src/test/java/org/elasticsearch/painless/AugmentationTests.java
+++ modules/lang-painless/src/test/java/org/elasticsearch/painless/AugmentationTests.java
@@ -235,7 +235,7 @@ public class AugmentationTests extends ScriptTestCase {
             //System.out.println(String.format("Splitting '%s' by '%s' %d times", split.input, split.token, split.count));
             assertArrayEquals(
                 split.input.split(Pattern.quote(split.token), split.count),
-                (String[])exec(String.format("return \"%s\".splitOnToken(\"%s\", %d);", split.input, split.token, split.count))
+                (String[])exec("return \""+split.input+"\".splitOnToken(\""+split.token+"\", "+split.count+");")
             );
         }
     }

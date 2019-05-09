542d1ee66ce409c48e6f20af6bdac85745c6fa2f
Marios Trivyzas <matriv@users.noreply.github.com>
2019-05-09 16:16:03 +0300

    SQL: Fix issue regarding INTERVAL * number
    Interval * integer number is a valid operation which previously was
    only supported for foldables (literals) and not when a field was
    involved. That was because:
    
    1. There was no common type returned for that combination
    2. The `BinaryArithmeticOperation` was permitting the multiplication
    (called by fold()) but the BinaryArithmeticProcessor didn't allow it
    
    Moreover the error message for invalid arithmetic operations was wrong
    because of the issue with the overloading methods of
    `LoggerMessageFormat.format`.
    
    Fixes: #41239

--- x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/analysis/analyzer/VerifierErrorMessagesTests.java
+++ x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/analysis/analyzer/VerifierErrorMessagesTests.java
@@ -241,6 +241,21 @@ public class VerifierErrorMessagesTests extends ESTestCase {
             error("SELECT INTERVAL 1 MONTH - CAST('12:23:56.789' AS TIME)"));
     }
 
+    public void testAddIntervalAndNumberNotAllowed() {
+        assertEquals("1:8: [+] has arguments with incompatible types [INTERVAL_DAY] and [INTEGER]",
+            error("SELECT INTERVAL 1 DAY + 100"));
+    }
+
+    public void testSubtractIntervalAndNumberNotAllowed() {
+        assertEquals("1:8: [-] has arguments with incompatible types [INTERVAL_MINUTE] and [DOUBLE]",
+            error("SELECT INTERVAL 10 MINUTE - 100.0"));
+    }
+
+    public void testMultiplyIntervalWithDecimalNotAllowed() {
+        assertEquals("1:8: [*] has arguments with incompatible types [INTERVAL_MONTH] and [DOUBLE]",
+            error("SELECT INTERVAL 1 MONTH * 1.234"));
+    }
+
     public void testMultipleColumns() {
         assertEquals("1:43: Unknown column [xxx]\nline 1:8: Unknown column [xxx]",
                 error("SELECT xxx FROM test GROUP BY DAY_oF_YEAR(xxx)"));

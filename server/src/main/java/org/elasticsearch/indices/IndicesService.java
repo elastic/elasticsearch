dc444cef493f405883e58aa2a2b61d24c1e0e96d
Armin Braun <me@obrown.io>
2019-05-10 09:29:27 +0200

    Fix Race in Closing IndicesService.CacheCleaner (#42016) (#42052)
    * When close becomes true while the management pool is shut down, we run
    into an unhandled `EsRejectedExecutionException` that fails tests
    * Found this while trying to reproduce #32506
       * Running the IndexStatsIT in a loop is a way of reproducing this

--- server/src/main/java/org/elasticsearch/indices/IndicesService.java
+++ server/src/main/java/org/elasticsearch/indices/IndicesService.java
@@ -65,6 +65,7 @@ import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.util.BigArrays;
 import org.elasticsearch.common.util.concurrent.AbstractRefCounted;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
+import org.elasticsearch.common.util.concurrent.EsRejectedExecutionException;
 import org.elasticsearch.common.util.iterable.Iterables;
 import org.elasticsearch.common.xcontent.LoggingDeprecationHandler;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
@@ -1225,7 +1226,13 @@ public class IndicesService extends AbstractLifecycleComponent
             }
             // Reschedule itself to run again if not closed
             if (closed.get() == false) {
-                threadPool.schedule(this, interval, ThreadPool.Names.SAME);
+                try {
+                    threadPool.schedule(this, interval, ThreadPool.Names.SAME);
+                } catch (EsRejectedExecutionException e) {
+                    if (closed.get() == false) {
+                        throw e;
+                    }
+                }
             }
         }
 

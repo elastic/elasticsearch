871613a33cd5c3d7a1022d033f2e826cb38f8089
Jake Landis <jake.landis@elastic.co>
2019-05-20 11:43:51 -0500

    version bump 7.0.0 -> 7.0.1 (includes 6.8.1 too)

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -128,10 +128,10 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_6_7_1 = new Version(V_6_7_1_ID, org.apache.lucene.util.Version.LUCENE_7_7_0);
     public static final int V_6_7_2_ID = 6070299;
     public static final Version V_6_7_2 = new Version(V_6_7_2_ID, org.apache.lucene.util.Version.LUCENE_7_7_0);
-    public static final int V_6_7_3_ID = 6070399;
-    public static final Version V_6_7_3 = new Version(V_6_7_3_ID, org.apache.lucene.util.Version.LUCENE_7_7_0);
     public static final int V_6_8_0_ID = 6080099;
     public static final Version V_6_8_0 = new Version(V_6_8_0_ID, org.apache.lucene.util.Version.LUCENE_7_7_0);
+    public static final int V_6_8_1_ID = 6080199;
+    public static final Version V_6_8_1 = new Version(V_6_8_1_ID, org.apache.lucene.util.Version.LUCENE_7_7_0);
     public static final int V_7_0_0_ID = 7000099;
     public static final Version V_7_0_0 = new Version(V_7_0_0_ID, org.apache.lucene.util.Version.LUCENE_8_0_0);
     public static final int V_7_0_1_ID = 7000199;
@@ -140,7 +140,9 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_7_0_2 = new Version(V_7_0_2_ID, org.apache.lucene.util.Version.LUCENE_8_0_0);
     public static final int V_7_1_0_ID = 7010099;
     public static final Version V_7_1_0 = new Version(V_7_1_0_ID, org.apache.lucene.util.Version.LUCENE_8_0_0);
-    public static final Version CURRENT = V_7_1_0;
+    public static final int V_7_1_1_ID = 7010199;
+    public static final Version V_7_1_1 = new Version(V_7_1_1_ID, org.apache.lucene.util.Version.LUCENE_8_0_0);
+    public static final Version CURRENT = V_7_1_1;
 
     static {
         assert CURRENT.luceneVersion.equals(org.apache.lucene.util.Version.LATEST) : "Version must be upgraded to ["
@@ -153,6 +155,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
 
     public static Version fromId(int id) {
         switch (id) {
+            case V_7_1_1_ID:
+                return V_7_1_1;
             case V_7_1_0_ID:
                 return V_7_1_0;
             case V_7_0_2_ID:
@@ -161,10 +165,10 @@ public class Version implements Comparable<Version>, ToXContentFragment {
                 return V_7_0_1;
             case V_7_0_0_ID:
                 return V_7_0_0;
+            case V_6_8_1_ID:
+                return V_6_8_1;
             case V_6_8_0_ID:
                 return V_6_8_0;
-            case V_6_7_3_ID:
-                return V_6_7_3;
             case V_6_7_1_ID:
                 return V_6_7_1;
             case V_6_7_2_ID:

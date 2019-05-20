1f1b0fe9a4184f81f65a5d10e2ad76639609e30e
Alexander Reelsen <alexander@reelsen.net>
2019-05-20 10:17:08 -0400

    Update to joda time 2.10.2 (#42199)

--- server/src/test/java/org/elasticsearch/common/time/DateUtilsTests.java
+++ server/src/test/java/org/elasticsearch/common/time/DateUtilsTests.java
@@ -45,7 +45,8 @@ import static org.hamcrest.Matchers.is;
 
 public class DateUtilsTests extends ESTestCase {
     private static final Set<String> IGNORE = new HashSet<>(Arrays.asList(
-        "Eire", "Europe/Dublin" // dublin timezone in joda does not account for DST
+        "Eire", "Europe/Dublin", // dublin timezone in joda does not account for DST
+        "Asia/Qostanay" // this has been added in joda 2.10.2 but is not part of the JDK 12.0.1 tzdata yet
     ));
 
     public void testTimezoneIds() {

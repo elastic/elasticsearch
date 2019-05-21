84df48ccb3b07e8f2a56b95256537ae7b4c29fef
Nhat Nguyen <nhat.nguyen@elastic.co>
2019-05-21 17:20:27 -0400

    Recovery with syncId should verify seqno infos (#41265)
    This change verifies and aborts recovery if source and target have the
    same syncId but different sequenceId. This commit also adds an upgrade
    test to ensure that we always utilize syncId.

--- qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/AbstractRollingTestCase.java
+++ qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/AbstractRollingTestCase.java
@@ -44,6 +44,7 @@ public abstract class AbstractRollingTestCase extends ESRestTestCase {
 
     protected static final ClusterType CLUSTER_TYPE = ClusterType.parse(System.getProperty("tests.rest.suite"));
     protected static final Version UPGRADE_FROM_VERSION = Version.fromString(System.getProperty("tests.upgrade_from_version"));
+    protected static final boolean firstMixedRound = Boolean.parseBoolean(System.getProperty("tests.first_round", "false"));
 
     @Override
     protected final boolean preserveIndicesUponCompletion() {

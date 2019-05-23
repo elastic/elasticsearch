f825d3f9bf9b2aacf8199f95649755af998e3aa9
Armin Braun <me@obrown.io>
2019-05-23 21:48:26 +0200

    s3 third party tests

--- plugins/repository-s3/src/test/java/org/elasticsearch/repositories/s3/S3RepositoryThirdPartyTests.java
+++ plugins/repository-s3/src/test/java/org/elasticsearch/repositories/s3/S3RepositoryThirdPartyTests.java
@@ -16,8 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-package org.elasticsearch.repositories.gcs;
+package org.elasticsearch.repositories.s3;
 
 import org.elasticsearch.action.ActionRunnable;
 import org.elasticsearch.action.admin.cluster.snapshots.create.CreateSnapshotResponse;
@@ -37,7 +36,6 @@ import org.elasticsearch.test.ESSingleNodeTestCase;
 import org.elasticsearch.threadpool.ThreadPool;
 
 import java.io.ByteArrayInputStream;
-import java.util.Base64;
 import java.util.Collection;
 import java.util.concurrent.Executor;
 
@@ -46,21 +44,22 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.not;
 
-public class GoogleCloudStorageThirdPartyTests extends ESSingleNodeTestCase {
+public class S3RepositoryThirdPartyTests extends ESSingleNodeTestCase {
 
     @Override
     protected Collection<Class<? extends Plugin>> getPlugins() {
-        return pluginList(GoogleCloudStoragePlugin.class);
+        return pluginList(S3RepositoryPlugin.class);
     }
 
     @Override
     protected Settings nodeSettings() {
-        assertThat(System.getProperty("test.google.account"), not(blankOrNullString()));
-        assertThat(System.getProperty("test.google.bucket"), not(blankOrNullString()));
+        assertThat(System.getProperty("test.s3.account"), not(blankOrNullString()));
+        assertThat(System.getProperty("test.s3.key"), not(blankOrNullString()));
+        assertThat(System.getProperty("test.s3.bucket"), not(blankOrNullString()));
 
         MockSecureSettings secureSettings = new MockSecureSettings();
-        secureSettings.setFile("gcs.client.default.credentials_file",
-            Base64.getDecoder().decode(System.getProperty("test.google.account")));
+        secureSettings.setString("s3.client.default.access_key", System.getProperty("test.s3.account"));
+        secureSettings.setString("s3.client.default.secret_key", System.getProperty("test.s3.key"));
         return Settings.builder()
             .put(super.nodeSettings())
             .setSecureSettings(secureSettings)
@@ -70,12 +69,16 @@ public class GoogleCloudStorageThirdPartyTests extends ESSingleNodeTestCase {
     public void testCleanup() {
         Client client = client();
 
+        Settings.Builder settings = Settings.builder()
+            .put("bucket", System.getProperty("test.s3.bucket"))
+            .put("base_path", System.getProperty("test.s3.base", "/"));
+        final String endpoint = System.getProperty("test.s3.endpoint");
+        if (endpoint != null) {
+            settings = settings.put("endpoint", endpoint);
+        }
         AcknowledgedResponse putRepositoryResponse = client.admin().cluster().preparePutRepository("test-repo")
-            .setType("gcs")
-            .setSettings(Settings.builder()
-                .put("bucket", System.getProperty("test.google.bucket"))
-                .put("base_path", System.getProperty("test.google.base", "/"))
-            ).get();
+            .setType("s3")
+            .setSettings(settings).get();
         assertThat(putRepositoryResponse.isAcknowledged(), equalTo(true));
 
         createIndex("test-idx-1");
@@ -116,8 +119,8 @@ public class GoogleCloudStorageThirdPartyTests extends ESSingleNodeTestCase {
             equalTo(SnapshotState.SUCCESS));
 
         logger.info("--> creating a dangling index folder");
-        final GoogleCloudStorageRepository repo =
-            (GoogleCloudStorageRepository) getInstanceFromNode(RepositoriesService.class).repository("test-repo");
+        final S3Repository repo =
+            (S3Repository) getInstanceFromNode(RepositoriesService.class).repository("test-repo");
         final PlainActionFuture<Void> future = PlainActionFuture.newFuture();
         final Executor genericExec = repo.threadPool().executor(ThreadPool.Names.GENERIC);
         genericExec.execute(new ActionRunnable<>(future) {

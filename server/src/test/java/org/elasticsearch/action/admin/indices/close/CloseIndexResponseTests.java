271c29fc7ed562d716942fabb4a215fc49410f50
Tanguy Leroux <tlrx.dev@gmail.com>
2019-05-23 20:18:34 +0200

    Improve Close Index Response (#39687)
    This changes the `CloseIndexResponse` so that it reports closing result
    for each index. Shard failures or exception are also reported per index,
    and the global acknowledgment flag is computed from the index results
    only.
    
    The response looks like:
    ```
    {
      "acknowledged" : true,
      "shards_acknowledged" : true,
      "indices" : {
        "docs" : {
          "closed" : true
        }
      }
    }
    ```
    
    The response reports shard failures like:
    ```
    {
      "acknowledged" : false,
      "shards_acknowledged" : false,
      "indices" : {
        "docs-1" : {
          "closed" : true
        },
        "docs-2" : {
          "closed" : false,
          "shards" : {
            "1" : {
              "failures" : [
                {
                  "shard" : 1,
                  "index" : "docs-2",
                  "status" : "BAD_REQUEST",
                  "reason" : {
                    "type" : "index_closed_exception",
                    "reason" : "closed",
                    "index_uuid" : "JFmQwr_aSPiZbkAH_KEF7A",
                    "index" : "docs-2"
                  }
                }
              ]
            }
          }
        },
        "docs-3" : {
          "closed" : true
        }
      }
    }
    ```
    
    Co-authored-by: Tanguy Leroux <tlrx.dev@gmail.com>

--- server/src/test/java/org/elasticsearch/action/admin/indices/close/CloseIndexResponseTests.java
+++ server/src/test/java/org/elasticsearch/action/admin/indices/close/CloseIndexResponseTests.java
@@ -19,14 +19,30 @@
 
 package org.elasticsearch.action.admin.indices.close;
 
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.Version;
+import org.elasticsearch.action.NoShardAvailableActionException;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.index.Index;
+import org.elasticsearch.index.IndexNotFoundException;
+import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.transport.ActionNotFoundTransportException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.elasticsearch.test.VersionUtils.getPreviousVersion;
 import static org.elasticsearch.test.VersionUtils.randomVersionBetween;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 
 public class CloseIndexResponseTests extends ESTestCase {
 
@@ -47,11 +63,12 @@ public class CloseIndexResponseTests extends ESTestCase {
         {
             final CloseIndexResponse response = randomResponse();
             try (BytesStreamOutput out = new BytesStreamOutput()) {
-                out.setVersion(randomVersionBetween(random(), Version.V_7_0_0, Version.V_7_1_0));
+                out.setVersion(randomVersionBetween(random(), Version.V_7_0_0, getPreviousVersion(Version.V_7_2_0)));
                 response.writeTo(out);
 
                 final AcknowledgedResponse deserializedResponse = new AcknowledgedResponse();
                 try (StreamInput in = out.bytes().streamInput()) {
+                    in.setVersion(out.getVersion());
                     deserializedResponse.readFrom(in);
                 }
                 assertThat(deserializedResponse.isAcknowledged(), equalTo(response.isAcknowledged()));
@@ -64,22 +81,136 @@ public class CloseIndexResponseTests extends ESTestCase {
 
                 final CloseIndexResponse deserializedResponse = new CloseIndexResponse();
                 try (StreamInput in = out.bytes().streamInput()) {
-                    in.setVersion(randomVersionBetween(random(), Version.V_7_0_0, Version.V_7_1_0));
+                    in.setVersion(randomVersionBetween(random(), Version.V_7_0_0, getPreviousVersion(Version.V_7_2_0)));
                     deserializedResponse.readFrom(in);
                 }
                 assertThat(deserializedResponse.isAcknowledged(), equalTo(response.isAcknowledged()));
             }
         }
+        {
+            final CloseIndexResponse response = randomResponse();
+            try (BytesStreamOutput out = new BytesStreamOutput()) {
+                Version version = randomVersionBetween(random(), Version.V_7_2_0, Version.CURRENT);
+                out.setVersion(version);
+                response.writeTo(out);
+                final CloseIndexResponse deserializedResponse = new CloseIndexResponse();
+                try (StreamInput in = out.bytes().streamInput()) {
+                    in.setVersion(version);
+                    deserializedResponse.readFrom(in);
+                }
+                assertThat(deserializedResponse.isAcknowledged(), equalTo(response.isAcknowledged()));
+                assertThat(deserializedResponse.isShardsAcknowledged(), equalTo(response.isShardsAcknowledged()));
+                if (version.onOrAfter(Version.V_8_0_0)) {
+                    assertThat(deserializedResponse.getIndices(), hasSize(response.getIndices().size()));
+                } else {
+                    assertThat(deserializedResponse.getIndices(), empty());
+                }
+            }
+        }
     }
 
     private CloseIndexResponse randomResponse() {
-        final boolean acknowledged = randomBoolean();
+        boolean acknowledged = true;
+        final String[] indicesNames = generateRandomStringArray(10, 10, false, true);
+
+        final List<CloseIndexResponse.IndexResult> indexResults = new ArrayList<>();
+        for (String indexName : indicesNames) {
+            final Index index = new Index(indexName, "_na_");
+            if (randomBoolean()) {
+                indexResults.add(new CloseIndexResponse.IndexResult(index));
+            } else {
+                if (randomBoolean()) {
+                    acknowledged = false;
+                    indexResults.add(new CloseIndexResponse.IndexResult(index, randomException(index, 0)));
+                } else {
+                    final int nbShards = randomIntBetween(1, 5);
+                    CloseIndexResponse.ShardResult[] shards = new CloseIndexResponse.ShardResult[nbShards];
+                    for (int i = 0; i < nbShards; i++) {
+                        CloseIndexResponse.ShardResult.Failure[] failures = null;
+                        if (randomBoolean()) {
+                            acknowledged = false;
+                            failures = new CloseIndexResponse.ShardResult.Failure[randomIntBetween(1, 3)];
+                            for (int j = 0; j < failures.length; j++) {
+                                String nodeId = randomAlphaOfLength(5);
+                                failures[j] = new CloseIndexResponse.ShardResult.Failure(indexName, i, randomException(index, i), nodeId);
+                            }
+                        }
+                        shards[i] = new CloseIndexResponse.ShardResult(i, failures);
+                    }
+                    indexResults.add(new CloseIndexResponse.IndexResult(index, shards));
+                }
+            }
+
+        }
+
         final boolean shardsAcknowledged = acknowledged ? randomBoolean() : false;
-        return new CloseIndexResponse(acknowledged, shardsAcknowledged);
+        return new CloseIndexResponse(acknowledged, shardsAcknowledged, indexResults);
+    }
+
+    private static ElasticsearchException randomException(final Index index, final int id) {
+        return randomFrom(
+            new IndexNotFoundException(index),
+            new ActionNotFoundTransportException("test"),
+            new NoShardAvailableActionException(new ShardId(index, id)));
     }
 
     private static void assertCloseIndexResponse(final CloseIndexResponse actual, final CloseIndexResponse expected) {
         assertThat(actual.isAcknowledged(), equalTo(expected.isAcknowledged()));
         assertThat(actual.isShardsAcknowledged(), equalTo(expected.isShardsAcknowledged()));
+
+        for (int i = 0; i < expected.getIndices().size(); i++) {
+            CloseIndexResponse.IndexResult expectedIndexResult = expected.getIndices().get(i);
+            CloseIndexResponse.IndexResult actualIndexResult = actual.getIndices().get(i);
+            assertThat(actualIndexResult.getIndex(), equalTo(expectedIndexResult.getIndex()));
+            assertThat(actualIndexResult.hasFailures(), equalTo(expectedIndexResult.hasFailures()));
+
+            if (expectedIndexResult.hasFailures() == false) {
+                assertThat(actualIndexResult.getException(), nullValue());
+                if (actualIndexResult.getShards() != null) {
+                    assertThat(Arrays.stream(actualIndexResult.getShards())
+                        .allMatch(shardResult -> shardResult.hasFailures() == false), is(true));
+                }
+            }
+
+            if (expectedIndexResult.getException() != null) {
+                assertThat(actualIndexResult.getShards(), nullValue());
+                assertThat(actualIndexResult.getException(), notNullValue());
+                assertThat(actualIndexResult.getException().getMessage(), equalTo(expectedIndexResult.getException().getMessage()));
+                assertThat(actualIndexResult.getException().getClass(), equalTo(expectedIndexResult.getException().getClass()));
+                assertArrayEquals(actualIndexResult.getException().getStackTrace(), expectedIndexResult.getException().getStackTrace());
+            } else {
+                assertThat(actualIndexResult.getException(), nullValue());
+            }
+
+            if (expectedIndexResult.getShards() != null) {
+                assertThat(actualIndexResult.getShards().length, equalTo(expectedIndexResult.getShards().length));
+
+                for (int j = 0; j < expectedIndexResult.getShards().length; j++) {
+                    CloseIndexResponse.ShardResult expectedShardResult = expectedIndexResult.getShards()[j];
+                    CloseIndexResponse.ShardResult actualShardResult = actualIndexResult.getShards()[j];
+                    assertThat(actualShardResult.getId(), equalTo(expectedShardResult.getId()));
+                    assertThat(actualShardResult.hasFailures(), equalTo(expectedShardResult.hasFailures()));
+
+                    if (expectedShardResult.hasFailures()) {
+                        assertThat(actualShardResult.getFailures().length, equalTo(expectedShardResult.getFailures().length));
+
+                        for (int k = 0; k < expectedShardResult.getFailures().length; k++) {
+                            CloseIndexResponse.ShardResult.Failure expectedFailure = expectedShardResult.getFailures()[k];
+                            CloseIndexResponse.ShardResult.Failure actualFailure = actualShardResult.getFailures()[k];
+                            assertThat(actualFailure.getNodeId(), equalTo(expectedFailure.getNodeId()));
+                            assertThat(actualFailure.index(), equalTo(expectedFailure.index()));
+                            assertThat(actualFailure.shardId(), equalTo(expectedFailure.shardId()));
+                            assertThat(actualFailure.getCause().getMessage(), equalTo(expectedFailure.getCause().getMessage()));
+                            assertThat(actualFailure.getCause().getClass(), equalTo(expectedFailure.getCause().getClass()));
+                            assertArrayEquals(actualFailure.getCause().getStackTrace(), expectedFailure.getCause().getStackTrace());
+                        }
+                    } else {
+                        assertThat(actualShardResult.getFailures(), nullValue());
+                    }
+                }
+            } else {
+                assertThat(actualIndexResult.getShards(), nullValue());
+            }
+        }
     }
 }

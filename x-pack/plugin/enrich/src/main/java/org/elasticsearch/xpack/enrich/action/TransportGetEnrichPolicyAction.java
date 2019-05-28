55f80ae4c86b67cfbc88873c928bb34776fb3944
Michael Basnight <mbasnight@gmail.com>
2019-05-28 14:47:50 -0500

    Add enrich policy GET API (#41384)
    This commit wires up the Rest calls and Transport calls for GET enrich
    policy, as well as tests and rest spec additions.

---
+++ x-pack/plugin/enrich/src/main/java/org/elasticsearch/xpack/enrich/action/TransportGetEnrichPolicyAction.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+package org.elasticsearch.xpack.enrich.action;
+
+import org.elasticsearch.ResourceNotFoundException;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.support.ActionFilters;
+import org.elasticsearch.action.support.master.TransportMasterNodeReadAction;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.block.ClusterBlockException;
+import org.elasticsearch.cluster.block.ClusterBlockLevel;
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.threadpool.ThreadPool;
+import org.elasticsearch.transport.TransportService;
+import org.elasticsearch.xpack.core.enrich.EnrichPolicy;
+import org.elasticsearch.xpack.core.enrich.action.GetEnrichPolicyAction;
+import org.elasticsearch.xpack.enrich.EnrichStore;
+
+import java.io.IOException;
+
+public class TransportGetEnrichPolicyAction extends TransportMasterNodeReadAction<GetEnrichPolicyAction.Request,
+    GetEnrichPolicyAction.Response> {
+
+    @Inject
+    public TransportGetEnrichPolicyAction(TransportService transportService,
+                                               ClusterService clusterService,
+                                               ThreadPool threadPool,
+                                               ActionFilters actionFilters,
+                                               IndexNameExpressionResolver indexNameExpressionResolver) {
+        super(GetEnrichPolicyAction.NAME, transportService, clusterService, threadPool, actionFilters,
+            GetEnrichPolicyAction.Request::new, indexNameExpressionResolver);
+    }
+
+    @Override
+    protected String executor() {
+        return ThreadPool.Names.SAME;
+    }
+
+    @Override
+    protected GetEnrichPolicyAction.Response newResponse() {
+        throw new UnsupportedOperationException("usage of Streamable is to be replaced by Writeable");
+    }
+
+    @Override
+    protected GetEnrichPolicyAction.Response read(StreamInput in) throws IOException {
+        return new GetEnrichPolicyAction.Response(in);
+    }
+
+    @Override
+    protected void masterOperation(GetEnrichPolicyAction.Request request,
+                                   ClusterState state,
+                                   ActionListener<GetEnrichPolicyAction.Response> listener) throws Exception {
+        final EnrichPolicy policy = EnrichStore.getPolicy(request.getName(), state);
+        if (policy == null) {
+            throw new ResourceNotFoundException("Policy [{}] was not found", request.getName());
+        }
+        listener.onResponse(new GetEnrichPolicyAction.Response(policy));
+    }
+
+    @Override
+    protected ClusterBlockException checkBlock(GetEnrichPolicyAction.Request request, ClusterState state) {
+        return state.blocks().globalBlockedException(ClusterBlockLevel.METADATA_READ);
+    }
+
+}
---
+++ x-pack/plugin/enrich/src/test/java/org/elasticsearch/xpack/enrich/action/GetEnrichPolicyActionResponseTests.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+package org.elasticsearch.xpack.enrich.action;
+
+import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.test.AbstractSerializingTestCase;
+import org.elasticsearch.xpack.core.enrich.EnrichPolicy;
+import org.elasticsearch.xpack.core.enrich.action.GetEnrichPolicyAction;
+
+import java.io.IOException;
+
+import static org.elasticsearch.xpack.enrich.EnrichPolicyTests.assertEqualPolicies;
+import static org.elasticsearch.xpack.enrich.EnrichPolicyTests.randomEnrichPolicy;
+
+public class GetEnrichPolicyActionResponseTests extends AbstractSerializingTestCase<GetEnrichPolicyAction.Response> {
+
+    @Override
+    protected GetEnrichPolicyAction.Response doParseInstance(XContentParser parser) throws IOException {
+        EnrichPolicy policy = EnrichPolicy.fromXContent(parser);
+        return new GetEnrichPolicyAction.Response(policy);
+    }
+
+    @Override
+    protected GetEnrichPolicyAction.Response createTestInstance() {
+        EnrichPolicy policy = randomEnrichPolicy(XContentType.JSON);
+        return new GetEnrichPolicyAction.Response(policy);
+    }
+
+    @Override
+    protected Writeable.Reader<GetEnrichPolicyAction.Response> instanceReader() {
+        return GetEnrichPolicyAction.Response::new;
+    }
+
+    @Override
+    protected void assertEqualInstances(GetEnrichPolicyAction.Response expectedInstance, GetEnrichPolicyAction.Response newInstance) {
+        assertNotSame(expectedInstance, newInstance);
+        // the tests shuffle around the policy query source xcontent type, so this is needed here
+        assertEqualPolicies(expectedInstance.getPolicy(), newInstance.getPolicy());
+    }
+}

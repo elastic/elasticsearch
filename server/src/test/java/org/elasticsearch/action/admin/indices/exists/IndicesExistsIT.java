df0f0b3d40589c3dca3d0f72a12c7e8458d84295
David Turner <david.turner@elastic.co>
2019-06-03 12:12:07 +0100

    Rename autoMinMasterNodes to autoManageMasterNodes (#42789)
    Renames the `ClusterScope` attribute `autoMinMasterNodes` to reflect its
    broader meaning since 7.0.
    
    Backport of the relevant part of #42700 to `7.x`.

--- server/src/test/java/org/elasticsearch/action/admin/indices/exists/IndicesExistsIT.java
+++ server/src/test/java/org/elasticsearch/action/admin/indices/exists/IndicesExistsIT.java
@@ -31,7 +31,7 @@ import java.io.IOException;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertThrows;
 
 @ClusterScope(scope = ESIntegTestCase.Scope.TEST, numDataNodes = 0, numClientNodes = 0, transportClientRatio = 0.0,
-    autoMinMasterNodes = false)
+    autoManageMasterNodes = false)
 public class IndicesExistsIT extends ESIntegTestCase {
 
     public void testIndexExistsWithBlocksInPlace() throws IOException {

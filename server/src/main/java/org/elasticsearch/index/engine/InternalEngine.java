7d8c8f4d2bde1431ecda6bfa3e5ff1188e372f72
Simon Willnauer <simonw@apache.org>
2019-06-04 20:19:21 +0200

    fix imports

--- server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
+++ server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -95,7 +95,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;

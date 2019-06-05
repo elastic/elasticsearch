de0ea4bbf77235fd25e9a340b648be981b9218f7
Jim Ferenczi <jimczi@apache.org>
2019-06-05 08:47:33 +0200

    Deduplicate alias and concrete fields in query field expansion (#42328)
    The full-text query parsers accept field pattern that are expanded using the mapping.
    Alias field are also detected during the expansion but they are not deduplicated with the
    concrete fields that are found from other patterns (or the same). This change ensures
    that we deduplicate the target fields of the full-text query parsers in order to avoid
    adding the same clause multiple times. Boolean queries are already able to deduplicate
    clauses during rewrite but since we also use DisjunctionMaxQuery it is preferable to detect
     these duplicates early on.

--- server/src/main/java/org/elasticsearch/index/search/QueryParserHelper.java
+++ server/src/main/java/org/elasticsearch/index/search/QueryParserHelper.java
@@ -55,6 +55,10 @@ public final class QueryParserHelper {
             } else {
                 fieldName = field;
             }
+            // handle duplicates
+            if (fieldsAndWeights.containsKey(field)) {
+                boost *= fieldsAndWeights.get(field);
+            }
             fieldsAndWeights.put(fieldName, boost);
         }
         return fieldsAndWeights;
@@ -84,7 +88,13 @@ public final class QueryParserHelper {
             float weight = fieldEntry.getValue() == null ? 1.0f : fieldEntry.getValue();
             Map<String, Float> fieldMap = resolveMappingField(context, fieldEntry.getKey(), weight,
                 !multiField, !allField, fieldSuffix);
-            resolvedFields.putAll(fieldMap);
+            for (Map.Entry<String, Float> field : fieldMap.entrySet()) {
+                float boost = field.getValue();
+                if (resolvedFields.containsKey(field.getKey())) {
+                    boost *= resolvedFields.get(field.getKey());
+                }
+                resolvedFields.put(field.getKey(), boost);
+            }
         }
         checkForTooManyFields(resolvedFields, context);
         return resolvedFields;
@@ -149,7 +159,12 @@ public final class QueryParserHelper {
                     // other exceptions are parsing errors or not indexed fields: keep
                 }
             }
-            fields.put(fieldName, weight);
+            // handle duplicates
+            float w = weight;
+            if (fields.containsKey(fieldType.name())) {
+                w *= fields.get(fieldType.name());
+            }
+            fields.put(fieldType.name(), w);
         }
         checkForTooManyFields(fields, context);
         return fields;

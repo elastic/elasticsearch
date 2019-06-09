7945a587832b331dfe86902fabc60cc0daf71797
Jason Tedor <jason@tedor.me>
2019-06-09 10:39:47 -0400

    Update server/src/main/java/org/elasticsearch/action/admin/indices/alias/TransportIndicesAliasesAction.java

--- server/src/main/java/org/elasticsearch/action/admin/indices/alias/TransportIndicesAliasesAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/alias/TransportIndicesAliasesAction.java
@@ -50,7 +50,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static java.util.Collections.unmodifiableList;
 

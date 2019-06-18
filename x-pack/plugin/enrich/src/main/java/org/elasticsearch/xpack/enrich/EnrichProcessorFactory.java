ee07d87b29bba1c3fb77336b4d35e1424b3fc3e3
James Baiera <james.baiera@gmail.com>
2019-06-18 00:40:30 -0400

    Make EnrichProcessor use search action through a client
    Add client to processor parameters in the ingest service.
    Remove the search provider function from the processor parameters.
    ExactMatchProcessor and Factory converted to use client.
    Remove test cases that are no longer applicable from processor.

--- x-pack/plugin/enrich/src/main/java/org/elasticsearch/xpack/enrich/EnrichProcessorFactory.java
+++ x-pack/plugin/enrich/src/main/java/org/elasticsearch/xpack/enrich/EnrichProcessorFactory.java
@@ -5,10 +5,8 @@
  */
 package org.elasticsearch.xpack.enrich;
 
+import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.ClusterState;
-import org.elasticsearch.cluster.metadata.IndexMetaData;
-import org.elasticsearch.common.collect.Tuple;
-import org.elasticsearch.index.engine.Engine;
 import org.elasticsearch.ingest.ConfigurationUtils;
 import org.elasticsearch.ingest.Processor;
 import org.elasticsearch.xpack.core.enrich.EnrichPolicy;
@@ -16,17 +14,16 @@ import org.elasticsearch.xpack.core.enrich.EnrichPolicy;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 final class EnrichProcessorFactory implements Processor.Factory, Consumer<ClusterState> {
 
     static final String TYPE = "enrich";
-    private final Function<String, Tuple<IndexMetaData, Engine.Searcher>> searchProvider;
+    private final Client client;
     volatile Map<String, EnrichPolicy> policies = Map.of();
 
-    EnrichProcessorFactory(Function<String, Tuple<IndexMetaData, Engine.Searcher>> searchProvider) {
-        this.searchProvider = searchProvider;
+    EnrichProcessorFactory(Client client) {
+        this.client = client;
     }
 
     @Override
@@ -56,7 +53,7 @@ final class EnrichProcessorFactory implements Processor.Factory, Consumer<Cluste
 
         switch (policy.getType()) {
             case EnrichPolicy.EXACT_MATCH_TYPE:
-                return new ExactMatchProcessor(tag, searchProvider, policyName, enrichKey, ignoreMissing, specifications);
+                return new ExactMatchProcessor(tag, client, policyName, enrichKey, ignoreMissing, specifications);
             default:
                 throw new IllegalArgumentException("unsupported policy type [" + policy.getType() + "]");
         }

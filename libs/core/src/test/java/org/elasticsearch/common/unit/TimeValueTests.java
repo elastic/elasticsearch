a193d8c0fe3b01bfbe2df59f2660ad032f3a7127
Lee Hinman <lee@writequit.org>
2019-06-18 12:08:37 -0600

    Enhance TimeValue.toString() to allow specifying fractional values.
    This enhances the `TimeValue` class to allow specifying the number of
    truncated fractional decimals when calling `toString()`. The default
    remains 1, however, more (or less, such as 0) can be specified to change
    the output.
    
    This commit also re-organizes some things in `TimeValue` such as putting
    all the class variables near the top of the class, and moving the
    constructors to the first methods in the class, in order to follow the
    structure of our other code.

--- libs/core/src/test/java/org/elasticsearch/common/unit/TimeValueTests.java
+++ libs/core/src/test/java/org/elasticsearch/common/unit/TimeValueTests.java
@@ -47,6 +47,26 @@ public class TimeValueTests extends ESTestCase {
         assertThat("1.5m", equalTo(new TimeValue(90, TimeUnit.SECONDS).toString()));
         assertThat("1.5h", equalTo(new TimeValue(90, TimeUnit.MINUTES).toString()));
         assertThat("1.5d", equalTo(new TimeValue(36, TimeUnit.HOURS).toString()));
+        assertThat("1d", equalTo(new TimeValue(36, TimeUnit.HOURS).toString(0)));
+        assertThat("1d", equalTo(new TimeValue(36, TimeUnit.HOURS).toString(-4)));
+        assertThat("1.5d", equalTo(new TimeValue(36, TimeUnit.HOURS).toString(2)));
+        assertThat("1.45d", equalTo(new TimeValue(35, TimeUnit.HOURS).toString(2)));
+        assertThat("1.4583333333d", equalTo(new TimeValue(35, TimeUnit.HOURS).toString(10)));
+        assertThat("1d", equalTo(new TimeValue(103723200, TimeUnit.MILLISECONDS).toString(0)));
+        assertThat("1.2d", equalTo(new TimeValue(103723200, TimeUnit.MILLISECONDS).toString(1)));
+        assertThat("1.2d", equalTo(new TimeValue(103723200, TimeUnit.MILLISECONDS).toString(2)));
+        assertThat("1.2d", equalTo(new TimeValue(103723200, TimeUnit.MILLISECONDS).toString(3)));
+        assertThat("1.2005d", equalTo(new TimeValue(103723200, TimeUnit.MILLISECONDS).toString(4)));
+        assertThat("1d", equalTo(new TimeValue(86400077, TimeUnit.MILLISECONDS).toString(0)));
+        assertThat("1d", equalTo(new TimeValue(86400077, TimeUnit.MILLISECONDS).toString(1)));
+        assertThat("1d", equalTo(new TimeValue(86400077, TimeUnit.MILLISECONDS).toString(2)));
+        assertThat("1d", equalTo(new TimeValue(86400077, TimeUnit.MILLISECONDS).toString(3)));
+        assertThat("1d", equalTo(new TimeValue(86400077, TimeUnit.MILLISECONDS).toString(4)));
+        assertThat("1d", equalTo(new TimeValue(86400077, TimeUnit.MILLISECONDS).toString(5)));
+        assertThat("1d", equalTo(new TimeValue(86400077, TimeUnit.MILLISECONDS).toString(6)));
+        assertThat("1.0000008d", equalTo(new TimeValue(86400077, TimeUnit.MILLISECONDS).toString(7)));
+        assertThat("1.00000089d", equalTo(new TimeValue(86400077, TimeUnit.MILLISECONDS).toString(8)));
+        assertThat("1.4583333333d", equalTo(new TimeValue(35, TimeUnit.HOURS).toString(Integer.MAX_VALUE)));
         assertThat("1000d", equalTo(new TimeValue(1000, TimeUnit.DAYS).toString()));
     }
 

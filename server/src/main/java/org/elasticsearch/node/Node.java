7d8ce43bf3d6644aa7e740ea12f531da04c57837
David Turner <david.turner@elastic.co>
2019-06-19 16:59:16 +0100

    Avoid parallel reroutes in DiskThresholdMonitor
    Today the `DiskThresholdMonitor` limits the frequency with which it submits
    reroute tasks, but it might still submit these tasks faster than the master can
    process them if, for instance, each reroute takes over 60 seconds. This causes
    a problem since the reroute task runs with priority `IMMEDIATE` and is always
    scheduled when there is a node over the high watermark, so this can starve any
    other pending tasks on the master.
    
    This change avoids further updates from the monitor while its last task(s) are
    still in progress, and it measures the time of each update from the completion
    time of the reroute task rather than its start time, to allow a larger window
    for other tasks to run.
    
    Fixes #40174

--- server/src/main/java/org/elasticsearch/node/Node.java
+++ server/src/main/java/org/elasticsearch/node/Node.java
@@ -368,7 +368,7 @@ public class Node implements Closeable {
             final IngestService ingestService = new IngestService(clusterService, threadPool, this.environment,
                 scriptModule.getScriptService(), analysisModule.getAnalysisRegistry(), pluginsService.filterPlugins(IngestPlugin.class));
             final DiskThresholdMonitor listener = new DiskThresholdMonitor(settings, clusterService::state,
-                clusterService.getClusterSettings(), client);
+                clusterService.getClusterSettings(), client, threadPool::relativeTimeInMillis);
             final ClusterInfoService clusterInfoService = newClusterInfoService(settings, clusterService, threadPool, client,
                 listener::onNewInfo);
             final UsageService usageService = new UsageService();

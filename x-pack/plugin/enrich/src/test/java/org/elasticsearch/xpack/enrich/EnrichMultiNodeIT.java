af4e49c582635ba187a551e72501d4e091733c60
Martijn van Groningen <martijn.v.groningen@gmail.com>
2019-06-20 09:07:17 +0200

    added more tests

--- x-pack/plugin/enrich/src/test/java/org/elasticsearch/xpack/enrich/EnrichMultiNodeIT.java
+++ x-pack/plugin/enrich/src/test/java/org/elasticsearch/xpack/enrich/EnrichMultiNodeIT.java
@@ -6,11 +6,21 @@
 package org.elasticsearch.xpack.enrich;
 
 import org.apache.lucene.search.TotalHits;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.ingest.PutPipelineRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.common.bytes.BytesArray;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.reindex.ReindexPlugin;
+import org.elasticsearch.node.Node;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.test.ESIntegTestCase;
 import org.elasticsearch.xpack.core.enrich.EnrichPolicy;
@@ -21,15 +31,20 @@ import org.elasticsearch.xpack.core.enrich.action.ListEnrichPolicyAction;
 import org.elasticsearch.xpack.core.enrich.action.PutEnrichPolicyAction;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
-@ESIntegTestCase.ClusterScope(numDataNodes = 0, numClientNodes = 0)
+@ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST, numDataNodes = 0, numClientNodes = 0)
 public class EnrichMultiNodeIT extends ESIntegTestCase {
 
     private static final String POLICY_NAME = "my-policy";
+    private static final String PIPELINE_NAME = "my-pipeline";
     private static final String SOURCE_INDEX_NAME = "users";
     private static final String KEY_FIELD = "email";
     private static final String[] DECORATE_FIELDS = new String[]{"address", "city", "country"};
@@ -42,7 +57,7 @@ public class EnrichMultiNodeIT extends ESIntegTestCase {
     public void testEnrichAPIs() {
         final int numPolicies = 4;
         internalCluster().startNodes(3);
-        createSourceIndex();
+        createSourceIndex(8);
 
         for (int i = 0; i < numPolicies; i++) {
             String policyName = POLICY_NAME + i;
@@ -75,10 +90,62 @@ public class EnrichMultiNodeIT extends ESIntegTestCase {
         assertThat(response.getPolicies().size(), equalTo(0));
     }
 
-    private static void createSourceIndex() {
-        int numDocs = 8;
+    public void testEnrich() {
+        List<String> nodes = internalCluster().startNodes(3);
+        List<String> keys = createSourceIndex(64);
+        createAndExecutePolicy();
+        createPipeline();
+        enrich(keys, randomFrom(nodes));
+    }
+
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/pull/43311")
+    public void testEnrichDedicatedIngestNode() {
+        internalCluster().startNode();
+        Settings settings = Settings.builder()
+            .put(Node.NODE_MASTER_SETTING.getKey(), false)
+            .put(Node.NODE_DATA_SETTING.getKey(), false)
+            .put(Node.NODE_INGEST_SETTING.getKey(), true)
+            .build();
+        String ingestOnlyNode = internalCluster().startNode(settings);
+
+        List<String> keys = createSourceIndex(64);
+        createAndExecutePolicy();
+        createPipeline();
+        enrich(keys, ingestOnlyNode);
+    }
+
+    private static void enrich(List<String> keys, String coordinatingNode) {
+        int numDocs = 256;
+        BulkRequest bulkRequest = new BulkRequest("my-index");
         for (int i = 0; i < numDocs; i++) {
-            String key = randomAlphaOfLength(16);
+            IndexRequest indexRequest = new IndexRequest();
+            indexRequest.id(Integer.toString(i));
+            indexRequest.setPipeline(PIPELINE_NAME);
+            indexRequest.source(Map.of(KEY_FIELD, randomFrom(keys)));
+            bulkRequest.add(indexRequest);
+        }
+        BulkResponse bulkResponse = client(coordinatingNode).bulk(bulkRequest).actionGet();
+        assertThat("Expected no failure, but " + bulkResponse.buildFailureMessage(), bulkResponse.hasFailures(), is(false));
+
+        for (int i = 0; i < numDocs; i++) {
+            GetResponse getResponse = client().get(new GetRequest("my-index", Integer.toString(i))).actionGet();
+            Map<String, Object> source = getResponse.getSourceAsMap();
+            assertThat(source.size(), equalTo(1 + DECORATE_FIELDS.length));
+            for (String field : DECORATE_FIELDS) {
+                assertThat(source.get(field), notNullValue());
+            }
+        }
+        client().admin().indices().delete(new DeleteIndexRequest("my-index")).actionGet();
+    }
+
+    private static List<String> createSourceIndex(int numDocs) {
+        Set<String> keys = new HashSet<>();
+        for (int i = 0; i < numDocs; i++) {
+            String key;
+            do {
+                key = randomAlphaOfLength(16);
+            } while (keys.add(key) == false);
+
             IndexRequest indexRequest = new IndexRequest(SOURCE_INDEX_NAME);
             indexRequest.create(true);
             indexRequest.id(key);
@@ -87,5 +154,24 @@ public class EnrichMultiNodeIT extends ESIntegTestCase {
             client().index(indexRequest).actionGet();
         }
         client().admin().indices().refresh(new RefreshRequest(SOURCE_INDEX_NAME)).actionGet();
+        return List.copyOf(keys);
+    }
+
+    private static void createAndExecutePolicy() {
+        EnrichPolicy enrichPolicy =
+            new EnrichPolicy(EnrichPolicy.EXACT_MATCH_TYPE, null, List.of(SOURCE_INDEX_NAME), KEY_FIELD, List.of(DECORATE_FIELDS));
+        PutEnrichPolicyAction.Request request = new PutEnrichPolicyAction.Request(POLICY_NAME, enrichPolicy);
+        client().execute(PutEnrichPolicyAction.INSTANCE, request).actionGet();
+        client().execute(ExecuteEnrichPolicyAction.INSTANCE, new ExecuteEnrichPolicyAction.Request(POLICY_NAME)).actionGet();
+    }
+
+    private static void createPipeline() {
+        String pipelineBody = "{\"processors\": [{\"enrich\": {\"policy_name\":\"" + POLICY_NAME +
+            "\", \"enrich_values\": [{\"source\": \"" + DECORATE_FIELDS[0] + "\", \"target\": \"" + DECORATE_FIELDS[0] + "\"}," +
+            "{\"source\": \"" + DECORATE_FIELDS[1] + "\", \"target\": \"" + DECORATE_FIELDS[1] + "\"}," +
+            "{\"source\": \"" + DECORATE_FIELDS[2] + "\", \"target\": \"" + DECORATE_FIELDS[2] + "\"}" +
+            "]}}]}";
+        PutPipelineRequest request = new PutPipelineRequest(PIPELINE_NAME, new BytesArray(pipelineBody), XContentType.JSON);
+        client().admin().cluster().putPipeline(request).actionGet();
     }
 }

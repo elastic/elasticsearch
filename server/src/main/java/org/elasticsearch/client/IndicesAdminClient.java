b714c2e1a33a828605246c51de3930100aff61ae
Luca Cavanna <javanna@users.noreply.github.com>
2019-06-21 20:14:11 +0200

    Remove aliases exist action (#43430)
    The aliases exists action was kept around only to not break transport
    client, as RestGetAliasesAction has its own logic that ties into the
    unified HEAD requests handling.
    
    Now that transport client is removed, we can remove also
    TransportAliasesExistAction as well as its corresponding request,
    request builder, response, and action.

--- server/src/main/java/org/elasticsearch/client/IndicesAdminClient.java
+++ server/src/main/java/org/elasticsearch/client/IndicesAdminClient.java
@@ -23,8 +23,6 @@ import org.elasticsearch.action.ActionFuture;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest;
 import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequestBuilder;
-import org.elasticsearch.action.admin.indices.alias.exists.AliasesExistRequestBuilder;
-import org.elasticsearch.action.admin.indices.alias.exists.AliasesExistResponse;
 import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
 import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequestBuilder;
 import org.elasticsearch.action.admin.indices.alias.get.GetAliasesResponse;
@@ -525,26 +523,6 @@ public interface IndicesAdminClient extends ElasticsearchClient {
      * Get specific index aliases that exists in particular indices and / or by name.
      */
     GetAliasesRequestBuilder prepareGetAliases(String... aliases);
-
-    /**
-     * Allows to check to existence of aliases from indices.
-     */
-    AliasesExistRequestBuilder prepareAliasesExist(String... aliases);
-
-    /**
-     * Check to existence of index aliases.
-     *
-     * @param request The result future
-     */
-    ActionFuture<AliasesExistResponse> aliasesExist(GetAliasesRequest request);
-
-    /**
-     * Check the existence of specified index aliases.
-     *
-     * @param request  The index aliases request
-     * @param listener A listener to be notified with a result
-     */
-    void aliasesExist(GetAliasesRequest request, ActionListener<AliasesExistResponse> listener);
 
     /**
      * Get index metadata for particular indices.
--- server/src/test/java/org/elasticsearch/snapshots/SharedClusterSnapshotRestoreIT.java
+++ server/src/test/java/org/elasticsearch/snapshots/SharedClusterSnapshotRestoreIT.java
@@ -129,8 +129,6 @@ import static org.elasticsearch.index.IndexSettings.INDEX_SOFT_DELETES_SETTING;
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 import static org.elasticsearch.index.shard.IndexShardTests.getEngineFromShard;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
-import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAliasesExist;
-import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAliasesMissing;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAllSuccessful;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertHitCount;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertIndexTemplateExists;
@@ -500,7 +498,8 @@ public class SharedClusterSnapshotRestoreIT extends AbstractSnapshotIntegTestCas
                 .addAlias("test-idx-3", "alias-123")
                 .addAlias("test-idx-1", "alias-1")
                 .get());
-        assertAliasesExist(client.admin().indices().prepareAliasesExist("alias-123").get());
+
+        assertFalse(client.admin().indices().prepareGetAliases("alias-123").get().getAliases().isEmpty());
 
         logger.info("--> snapshot");
         assertThat(client.admin().cluster().prepareCreateSnapshot("test-repo", "test-snap")
@@ -509,7 +508,8 @@ public class SharedClusterSnapshotRestoreIT extends AbstractSnapshotIntegTestCas
 
         logger.info("-->  delete all indices");
         cluster().wipeIndices("test-idx-1", "test-idx-2", "test-idx-3");
-        assertAliasesMissing(client.admin().indices().prepareAliasesExist("alias-123", "alias-1").get());
+        assertTrue(client.admin().indices().prepareGetAliases("alias-123").get().getAliases().isEmpty());
+        assertTrue(client.admin().indices().prepareGetAliases("alias-1").get().getAliases().isEmpty());
 
         logger.info("--> restore snapshot with aliases");
         RestoreSnapshotResponse restoreSnapshotResponse = client.admin().cluster().prepareRestoreSnapshot("test-repo", "test-snap")
@@ -519,7 +519,8 @@ public class SharedClusterSnapshotRestoreIT extends AbstractSnapshotIntegTestCas
             equalTo(restoreSnapshotResponse.getRestoreInfo().totalShards())));
 
         logger.info("--> check that aliases are restored");
-        assertAliasesExist(client.admin().indices().prepareAliasesExist("alias-123", "alias-1").get());
+        assertFalse(client.admin().indices().prepareGetAliases("alias-123").get().getAliases().isEmpty());
+        assertFalse(client.admin().indices().prepareGetAliases("alias-1").get().getAliases().isEmpty());
 
         logger.info("-->  update aliases");
         assertAcked(client.admin().indices().prepareAliases().removeAlias("test-idx-3", "alias-123"));
@@ -528,7 +529,8 @@ public class SharedClusterSnapshotRestoreIT extends AbstractSnapshotIntegTestCas
         logger.info("-->  delete and close indices");
         cluster().wipeIndices("test-idx-1", "test-idx-2");
         assertAcked(client.admin().indices().prepareClose("test-idx-3"));
-        assertAliasesMissing(client.admin().indices().prepareAliasesExist("alias-123", "alias-1").get());
+        assertTrue(client.admin().indices().prepareGetAliases("alias-123").get().getAliases().isEmpty());
+        assertTrue(client.admin().indices().prepareGetAliases("alias-1").get().getAliases().isEmpty());
 
         logger.info("--> restore snapshot without aliases");
         restoreSnapshotResponse = client.admin().cluster().prepareRestoreSnapshot("test-repo", "test-snap").setWaitForCompletion(true)
@@ -538,8 +540,9 @@ public class SharedClusterSnapshotRestoreIT extends AbstractSnapshotIntegTestCas
             equalTo(restoreSnapshotResponse.getRestoreInfo().totalShards())));
 
         logger.info("--> check that aliases are not restored and existing aliases still exist");
-        assertAliasesMissing(client.admin().indices().prepareAliasesExist("alias-123", "alias-1").get());
-        assertAliasesExist(client.admin().indices().prepareAliasesExist("alias-3").get());
+        assertTrue(client.admin().indices().prepareGetAliases("alias-123").get().getAliases().isEmpty());
+        assertTrue(client.admin().indices().prepareGetAliases("alias-1").get().getAliases().isEmpty());
+        assertFalse(client.admin().indices().prepareGetAliases("alias-3").get().getAliases().isEmpty());
     }
 
     public void testRestoreTemplates() throws Exception {

d0027fcfffa5966b804e32e2ec127f275f4e524f
James Baiera <james.baiera@gmail.com>
2019-06-24 09:36:54 -0400

    Add SLM Package Docs

---
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/snapshotlifecycle/action/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+/**
+ * Contains the action definitions for SLM. For the transport and rest action implementations, please see the <tt>ilm</tt> module's
+ * <tt>org.elasticsearch.xpack.snapshotlifecycle</tt> package.
+ */
+package org.elasticsearch.xpack.core.snapshotlifecycle.action;

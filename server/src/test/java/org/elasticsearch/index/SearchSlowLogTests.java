4a0acfe56056133f42c6c23873265825a4a52e45
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2019-06-24 07:59:17 +0200

    fix test due to types removal

--- server/src/test/java/org/elasticsearch/index/SearchSlowLogTests.java
+++ server/src/test/java/org/elasticsearch/index/SearchSlowLogTests.java
@@ -164,7 +164,8 @@ public class SearchSlowLogTests extends ESSingleNodeTestCase {
         assertThat(p.getValueFor("took"), equalTo("\"10nanos\""));
         assertThat(p.getValueFor("took_millis"), equalTo("\"0\""));
         assertThat(p.getValueFor("total_hits"), equalTo("\"-1\""));
-        assertThat(p.getValueFor("types"), equalTo("[]"));
+        //TODO add that in 7.x
+//        assertThat(p.getValueFor("types"), equalTo("[]"));
         assertThat(p.getValueFor("stats"), equalTo("[]"));
         assertThat(p.getValueFor("search_type"), equalTo("\"\""));
         assertThat(p.getValueFor("total_shards"), equalTo("\"1\""));

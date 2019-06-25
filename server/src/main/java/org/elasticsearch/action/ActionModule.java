0dc1c12f134bb82e5d68f2ca57a8fa72ec915b67
Tanguy Leroux <tlrx.dev@gmail.com>
2019-06-25 20:02:34 +0200

    Fix indices shown in _cat/indices (#43286)
    After two recent changes (#38824 and #33888), the _cat/indices API
    no longer report information for active recovering indices and
    non-replicated closed indices. It also misreport replicated closed
    indices that are potentially not authorized for the user.
    
    This commit changes how the cat action works by first using the
    Get Settings API in order to resolve authorized indices. It then uses
    the Cluster State, Cluster Health and Indices Stats APIs to retrieve
     information about the indices.
    
    Closes #39933

--- server/src/main/java/org/elasticsearch/action/ActionModule.java
+++ server/src/main/java/org/elasticsearch/action/ActionModule.java
@@ -666,7 +666,7 @@ public class ActionModule extends AbstractModule {
         registerHandler.accept(new RestMasterAction(settings, restController));
         registerHandler.accept(new RestNodesAction(settings, restController));
         registerHandler.accept(new RestTasksAction(settings, restController, nodesInCluster));
-        registerHandler.accept(new RestIndicesAction(settings, restController, indexNameExpressionResolver));
+        registerHandler.accept(new RestIndicesAction(settings, restController));
         registerHandler.accept(new RestSegmentsAction(settings, restController));
         // Fully qualified to prevent interference with rest.action.count.RestCountAction
         registerHandler.accept(new org.elasticsearch.rest.action.cat.RestCountAction(settings, restController));

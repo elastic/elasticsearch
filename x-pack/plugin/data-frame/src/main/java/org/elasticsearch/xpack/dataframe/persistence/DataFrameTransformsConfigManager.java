75cabc537227c65e38c795ce231ec2ebf08f0796
David Kyle <david.kyle@elastic.co>
2019-06-26 16:35:14 +0100

    [Ml Data Frame] Size the GET stats search by number of Ids requested (#43206)
    Set the size of the search request to the number of ids limited by 10,000

--- x-pack/plugin/data-frame/src/main/java/org/elasticsearch/xpack/dataframe/persistence/DataFrameTransformsConfigManager.java
+++ x-pack/plugin/data-frame/src/main/java/org/elasticsearch/xpack/dataframe/persistence/DataFrameTransformsConfigManager.java
@@ -335,6 +335,7 @@ public class DataFrameTransformsConfigManager {
         SearchRequest searchRequest = client.prepareSearch(DataFrameInternalIndex.INDEX_NAME)
                 .addSort(DataFrameField.ID.getPreferredName(), SortOrder.ASC)
                 .setQuery(builder)
+                .setSize(Math.min(transformIds.size(), 10_000))
                 .request();
 
         executeAsyncWithOrigin(client.threadPool().getThreadContext(), DATA_FRAME_ORIGIN, searchRequest,

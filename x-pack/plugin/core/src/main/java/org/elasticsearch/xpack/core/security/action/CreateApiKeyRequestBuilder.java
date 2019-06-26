fa23b683a84216afd6c579a4aa37c82afe5252a3
Yogesh Gaikwad <902768+bizybot@users.noreply.github.com>
2019-06-26 14:31:08 +1000

    Make role descriptors optional when creating API keys (#43481) (#43615)
    This commit changes the `role_descriptors` field from required
    to optional when creating API key. The default behavior in .NET ES
    client is to omit properties with `null` value requiring additional
    workarounds. The behavior for the API does not change.
    Field names (`id`, `name`) in the invalidate api keys API documentation have been
    corrected where they were wrong.
    
    Closes #42053

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/CreateApiKeyRequestBuilder.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/CreateApiKeyRequestBuilder.java
@@ -40,7 +40,7 @@ public final class CreateApiKeyRequestBuilder
 
     static {
         PARSER.declareString(constructorArg(), new ParseField("name"));
-        PARSER.declareNamedObjects(constructorArg(), (p, c, n) -> {
+        PARSER.declareNamedObjects(optionalConstructorArg(), (p, c, n) -> {
             p.nextToken();
             return RoleDescriptor.parse(n, p, false);
         }, new ParseField("role_descriptors"));

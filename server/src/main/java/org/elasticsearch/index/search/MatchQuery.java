090445db929227ef5f56af7f3a125ce51d08a85c
jimczi <jim.ferenczi@elastic.co>
2019-06-27 12:17:10 +0200

    apply feedback

--- server/src/main/java/org/elasticsearch/index/search/MatchQuery.java
+++ server/src/main/java/org/elasticsearch/index/search/MatchQuery.java
@@ -238,7 +238,7 @@ public class MatchQuery {
         Analyzer analyzer = getAnalyzer(fieldType, type == Type.PHRASE || type == Type.PHRASE_PREFIX);
         assert analyzer != null;
 
-        MatchQueryBuilder builder = new MatchQueryBuilder(analyzer, fieldType);
+        MatchQueryBuilder builder = new MatchQueryBuilder(analyzer, fieldType, enablePositionIncrements, autoGenerateSynonymsPhraseQuery);
 
         /*
          * If a keyword analyzer is used, we know that further analysis isn't
@@ -316,15 +316,16 @@ public class MatchQuery {
         /**
          * Creates a new QueryBuilder using the given analyzer.
          */
-        MatchQueryBuilder(Analyzer analyzer, MappedFieldType fieldType) {
+        MatchQueryBuilder(Analyzer analyzer, MappedFieldType fieldType,
+                            boolean enablePositionIncrements, boolean autoGenerateSynonymsPhraseQuery) {
             super(analyzer);
             this.fieldType = fieldType;
+            setEnablePositionIncrements(enablePositionIncrements);
             if (hasPositions(fieldType)) {
-                setAutoGenerateMultiTermSynonymsPhraseQuery(MatchQuery.this.autoGenerateSynonymsPhraseQuery);
+                setAutoGenerateMultiTermSynonymsPhraseQuery(autoGenerateSynonymsPhraseQuery);
             } else {
                 setAutoGenerateMultiTermSynonymsPhraseQuery(false);
             }
-            setEnablePositionIncrements(MatchQuery.this.enablePositionIncrements);
         }
 
         @Override

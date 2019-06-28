ae69685b4e8f95749544087472b86edcadd4cd09
Andrey Ershov <andrey.ershov@elastic.co>
2019-06-28 09:32:52 +0300

    Replace futures with GroupedActionListener

--- distribution/tools/snapshot-cli/src/main/java/org/elasticsearch/snapshots/AbstractRepository.java
+++ distribution/tools/snapshot-cli/src/main/java/org/elasticsearch/snapshots/AbstractRepository.java
@@ -1,21 +1,22 @@
 package org.elasticsearch.snapshots;
 
 import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.action.ActionRunnable;
+import org.elasticsearch.action.support.GroupedActionListener;
+import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.cli.Terminal;
 import org.elasticsearch.common.collect.Tuple;
 import org.elasticsearch.common.util.set.Sets;
 import org.elasticsearch.repositories.IndexId;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 public abstract class AbstractRepository implements Repository {
@@ -51,83 +52,78 @@ public abstract class AbstractRepository implements Repository {
         Date shiftedIndexNTimestamp = new Date(indexNTimestamp.getTime() - safetyGapMillis);
         terminal.println(Terminal.Verbosity.VERBOSE, "Latest index file creation timestamp is " + indexNTimestamp);
         terminal.println(Terminal.Verbosity.VERBOSE, "Shifted by safety gap creation timestamp is " + shiftedIndexNTimestamp);
+
         terminal.println(Terminal.Verbosity.VERBOSE, "Reading latest index file");
         Set<String> referencedIndexIds = getRepositoryData(latestIndexId)
                 .getIndices().values().stream().map(IndexId::getId).collect(Collectors.toSet());
         describeCollection("Set of indices referenced by index file", referencedIndexIds);
+
         terminal.println(Terminal.Verbosity.VERBOSE, "Listing indices/ directory");
         Set<String> allIndexIds = getAllIndexDirectoryNames();
         describeCollection("Set of indices inside indices/ directory", allIndexIds);
+
         Set<String> deletionCandidates = new TreeSet<>(Sets.difference(allIndexIds, referencedIndexIds));
         describeCollection("Set of deletion candidates", deletionCandidates);
         if (deletionCandidates.isEmpty()) {
             terminal.println(Terminal.Verbosity.NORMAL, "Set of deletion candidates is empty. Exiting");
             return;
         }
+
         ExecutorService executor = Executors.newFixedThreadPool(parallelism);
         try {
-            List<Future<String>> futures = new ArrayList<>();
+            PlainActionFuture<Collection<String>> orphanedIndicesFuture = new PlainActionFuture<>();
+            GroupedActionListener<String> groupedOrphanedIndicesListener = new GroupedActionListener<>(orphanedIndicesFuture,
+                    deletionCandidates.size());
             for (String candidate : deletionCandidates) {
-                Future<String> future = executor.submit(() ->
-                {
-                    terminal.println(Terminal.Verbosity.VERBOSE, "Reading index " + candidate + " last modification timestamp");
-                    Date indexTimestamp = getIndexTimestamp(candidate);
-                    if (indexTimestamp != null) {
-                        if (indexTimestamp.before(shiftedIndexNTimestamp)) {
-                            terminal.println(Terminal.Verbosity.VERBOSE,
-                                    "Index " + candidate + " is orphaned because it's modification timestamp " + indexTimestamp + " is " +
-                                            "less than index-N shifted timestamp " + shiftedIndexNTimestamp);
-                            return candidate;
+                executor.submit(new ActionRunnable<>(groupedOrphanedIndicesListener) {
+                    @Override
+                    protected void doRun() {
+                        terminal.println(Terminal.Verbosity.VERBOSE, "Reading index " + candidate + " last modification timestamp");
+                        Date indexTimestamp = getIndexTimestamp(candidate);
+                        if (indexTimestamp != null) {
+                            if (indexTimestamp.before(shiftedIndexNTimestamp)) {
+                                terminal.println(Terminal.Verbosity.VERBOSE,
+                                        "Index " + candidate + " is orphaned because it's modification timestamp " + indexTimestamp + " is " +
+                                                "less than index-N shifted timestamp " + shiftedIndexNTimestamp);
+                                groupedOrphanedIndicesListener.onResponse(candidate);
+                            } else {
+                                terminal.println(Terminal.Verbosity.VERBOSE,
+                                        "Index  " + candidate + " might not be orphaned because " + indexTimestamp +
+                                                " is gte than " + shiftedIndexNTimestamp);
+                                groupedOrphanedIndicesListener.onResponse(null);
+                            }
                         } else {
-                            terminal.println(Terminal.Verbosity.VERBOSE,
-                                    "Index  " + candidate + " might not be orphaned because " + indexTimestamp +
-                                            " is gte than " + shiftedIndexNTimestamp);
+                            groupedOrphanedIndicesListener.onResponse(null);
                         }
                     }
-                    return null;
                 });
-                futures.add(future);
             }
-
-            Set<String> orphanedIndexIds = new TreeSet<>();
-            for (Future<String> future : futures) {
-                try {
-                    String indexId = future.get();
-                    if (indexId != null) {
-                        orphanedIndexIds.add(indexId);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
+            Set<String> orphanedIndexIds =
+                    new TreeSet<>(orphanedIndicesFuture.actionGet().stream().filter(Objects::nonNull).collect(Collectors.toSet()));
             describeCollection("Set of orphaned indices", orphanedIndexIds);
             if (orphanedIndexIds.isEmpty()) {
                 terminal.println(Terminal.Verbosity.NORMAL, "Set of orphaned indices is empty. Exiting");
                 return;
             }
+
             confirm(terminal, "Do you want to remove orphaned indices files? This action is NOT REVERSIBLE");
+
             terminal.println(Terminal.Verbosity.NORMAL, "Removing orphaned indices");
-            List<Future<Tuple<Integer, Long>>> removalFutures = new ArrayList<>();
-            for (String indexId : orphanedIndexIds) {
-                removalFutures.add(executor.submit(() -> {
-                    terminal.println(Terminal.Verbosity.NORMAL, "Removing orphaned index " + indexId);
-                    return deleteIndex(indexId);
-                }));
+            PlainActionFuture<Collection<Tuple<Integer, Long>>> removalFuture = new PlainActionFuture<>();
+            GroupedActionListener<Tuple<Integer, Long>> groupedRemovalListener =
+                    new GroupedActionListener<>(removalFuture, orphanedIndexIds.size());
+            for (final String indexId : orphanedIndexIds) {
+                executor.submit(new ActionRunnable<>(groupedRemovalListener) {
+                    @Override
+                    protected void doRun() {
+                        terminal.println(Terminal.Verbosity.NORMAL,"Removing orphaned index "+indexId);
+                        groupedRemovalListener.onResponse(deleteIndex(indexId));
+                    }
+                });
             }
-
-            int totalFilesRemoved = 0;
-            long totalSpaceFreed = 0;
-            for (Future<Tuple<Integer, Long>> future : removalFutures) {
-                try {
-                    Tuple<Integer, Long> removedFilesCountAndSize = future.get();
-                    totalFilesRemoved += removedFilesCountAndSize.v1();
-                    totalSpaceFreed += removedFilesCountAndSize.v2();
-                } catch (Exception e) {
-                    throw new ElasticsearchException(e);
-                }
-            }
-
+            Collection<Tuple<Integer, Long>> removeResults = removalFuture.actionGet();
+            int totalFilesRemoved = removeResults.stream().map(Tuple::v1).reduce(0, (a, b) -> a + b);
+            long totalSpaceFreed = removeResults.stream().map(Tuple::v2).reduce(0L, (a, b) -> a + b);
             terminal.println(Terminal.Verbosity.NORMAL, "In total removed " + totalFilesRemoved + " files");
             terminal.println(Terminal.Verbosity.NORMAL, "In total space freed " + totalSpaceFreed + " bytes");
             terminal.println(Terminal.Verbosity.NORMAL, "Finished removing orphaned indices");

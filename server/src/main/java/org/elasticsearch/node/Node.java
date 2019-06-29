cdfc98680f0e62aec2b558c883e092ee48140601
Albert Zaharovits <albert.zaharovits@elastic.co>
2019-06-29 15:17:00 +0300

    Consistent Secure Settings (#40416)
    Introduces a new `ConsistentSecureSettingsValidatorService` service that exposes
    a single public method, namely `allSecureSettingsConsistent`. The method returns
    `true` if the local node's secure settings (inside the keystore) are equal to the
    master's, and `false` otherwise. Technically, the local node has to have exactly
    the same secure settings - setting names should not be missing or in surplus
    - for all `SecureSetting` instances that are flagged with the newly introduced
    `Property.Consistent`. It is worth highlighting that the `allSecureSettingsConsistent`
    is not a consensus view across the cluster, but rather the local node's perspective
    in relation to the master.

--- server/src/main/java/org/elasticsearch/node/Node.java
+++ server/src/main/java/org/elasticsearch/node/Node.java
@@ -74,6 +74,7 @@ import org.elasticsearch.common.network.NetworkAddress;
 import org.elasticsearch.common.network.NetworkModule;
 import org.elasticsearch.common.network.NetworkService;
 import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.ConsistentSettingsService;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Setting.Property;
 import org.elasticsearch.common.settings.SettingUpgrader;
@@ -363,6 +364,9 @@ public class Node implements Closeable {
             final ClusterService clusterService = new ClusterService(settings, settingsModule.getClusterSettings(), threadPool);
             clusterService.addStateApplier(scriptModule.getScriptService());
             resourcesToClose.add(clusterService);
+            clusterService.addLocalNodeMasterListener(
+                    new ConsistentSettingsService(settings, clusterService, settingsModule.getConsistentSettings())
+                            .newHashPublisher());
             final IngestService ingestService = new IngestService(clusterService, threadPool, this.environment,
                 scriptModule.getScriptService(), analysisModule.getAnalysisRegistry(), pluginsService.filterPlugins(IngestPlugin.class));
             final DiskThresholdMonitor listener = new DiskThresholdMonitor(settings, clusterService::state,

bca865dd42557c5dcf081f4d3b4a8416569a1bea
Yannick Welsch <yannick@welsch.lu>
2019-07-05 19:35:06 +0200

    Move ConnectionManager to async APIs (#42636)
    This commit converts the ConnectionManager's openConnection and connectToNode methods to
    async-style. This will allow us to not block threads anymore when opening connections. This PR also
    adapts the cluster coordination subsystem to make use of the new async APIs, allowing to remove
    some hacks in the test infrastructure that had to account for the previous synchronous nature of the
    connection APIs.

--- server/src/test/java/org/elasticsearch/snapshots/SnapshotResiliencyTests.java
+++ server/src/test/java/org/elasticsearch/snapshots/SnapshotResiliencyTests.java
@@ -753,7 +753,7 @@ public class SnapshotResiliencyTests extends ESTestCase {
             disconnectedNodes.forEach(nodeName -> {
                 if (testClusterNodes.nodes.containsKey(nodeName)) {
                     final DiscoveryNode node = testClusterNodes.nodes.get(nodeName).node;
-                    testClusterNodes.nodes.values().forEach(n -> n.transportService.getConnectionManager().openConnection(node, null));
+                    testClusterNodes.nodes.values().forEach(n -> n.transportService.openConnection(node, null));
                 }
             });
         }

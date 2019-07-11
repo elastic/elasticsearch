ea89212d93110b5f81120aa7258ee8896220a6e3
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2019-07-11 17:13:01 -0500

    [ML] Add r_squared eval metric to regression

--- client/rest-high-level/src/test/java/org/elasticsearch/client/ml/dataframe/evaluation/regression/RSquaredMetricResultTests.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/ml/dataframe/evaluation/regression/RSquaredMetricResultTests.java
@@ -18,41 +18,27 @@
  */
 package org.elasticsearch.client.ml.dataframe.evaluation.regression;
 
-import org.elasticsearch.client.ml.dataframe.evaluation.EvaluationMetric;
 import org.elasticsearch.client.ml.dataframe.evaluation.MlEvaluationNamedXContentProvider;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.test.AbstractXContentTestCase;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
 
-public class RegressionTests extends AbstractXContentTestCase<Regression> {
+public class RSquaredMetricResultTests extends AbstractXContentTestCase<RSquaredMetric.Result> {
 
-    @Override
-    protected NamedXContentRegistry xContentRegistry() {
-        return new NamedXContentRegistry(new MlEvaluationNamedXContentProvider().getNamedXContentParsers());
+    public static RSquaredMetric.Result randomResult() {
+        return new RSquaredMetric.Result(randomDouble());
     }
 
     @Override
-    protected Regression createTestInstance() {
-        List<EvaluationMetric> metrics = new ArrayList<>();
-        if (randomBoolean()) {
-            metrics.add(new MeanSquaredErrorMetric());
-        }
-        if (randomBoolean()) {
-            metrics.add(new RSquaredMetric());
-        }
-        return randomBoolean() ?
-            new Regression(randomAlphaOfLength(10), randomAlphaOfLength(10)) :
-            new Regression(randomAlphaOfLength(10), randomAlphaOfLength(10), metrics.isEmpty() ? null : metrics);
+    protected RSquaredMetric.Result createTestInstance() {
+        return randomResult();
     }
 
     @Override
-    protected Regression doParseInstance(XContentParser parser) throws IOException {
-        return Regression.fromXContent(parser);
+    protected RSquaredMetric.Result doParseInstance(XContentParser parser) throws IOException {
+        return RSquaredMetric.Result.fromXContent(parser);
     }
 
     @Override
@@ -61,8 +47,7 @@ public class RegressionTests extends AbstractXContentTestCase<Regression> {
     }
 
     @Override
-    protected Predicate<String> getRandomFieldsExcludeFilter() {
-        // allow unknown fields in the root of the object only
-        return field -> !field.isEmpty();
+    protected NamedXContentRegistry xContentRegistry() {
+        return new NamedXContentRegistry(new MlEvaluationNamedXContentProvider().getNamedXContentParsers());
     }
 }

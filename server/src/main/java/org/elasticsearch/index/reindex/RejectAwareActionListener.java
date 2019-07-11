269c21bcac9df7bf6569d4016b8e21b70e285cbb
Henning Andersen <henning.andersen@elastic.co>
2019-07-11 08:48:54 +0200

    Reindex share retry between hit sources
    The client and remote hit sources had each their own retry mechanism,
    which would do the same. Supporting resiliency we would have to expand
    on the retry mechanisms and as a preparation for that, the retry
    mechanism is now shared such that each sub class is only responsible for
    sending requests and converting responses/failures to common format.
    
    Part of #42612

---
+++ server/src/main/java/org/elasticsearch/index/reindex/RejectAwareActionListener.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.index.reindex;
+
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.common.CheckedConsumer;
+
+import java.util.function.Consumer;
+
+// public for testing
+public interface RejectAwareActionListener<T> extends ActionListener<T> {
+    void onRejection(Exception e);
+
+    /**
+     * Return a new listener that delegates failure/reject to original but forwards response to responseHandler
+     */
+    default <X> RejectAwareActionListener<X> withResponseHandler(Consumer<X> responseHandler) {
+        RejectAwareActionListener<?> outer = this;
+        return new RejectAwareActionListener<>() {
+            @Override
+            public void onRejection(Exception e) {
+                outer.onRejection(e);
+            }
+
+            @Override
+            public void onResponse(X t) {
+                responseHandler.accept(t);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                outer.onFailure(e);
+            }
+        };
+    }
+
+    /**
+     * Similar to {@link ActionListener#wrap(CheckedConsumer, Consumer)}, extended to have handler for onRejection.
+     */
+    static <Response> RejectAwareActionListener<Response> wrap(CheckedConsumer<Response, ? extends Exception> onResponse,
+                                                    Consumer<Exception> onFailure, Consumer<Exception> onRejection) {
+        return new RejectAwareActionListener<Response>() {
+            @Override
+            public void onResponse(Response response) {
+                try {
+                    onResponse.accept(response);
+                } catch (Exception e) {
+                    onFailure(e);
+                }
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                onFailure.accept(e);
+            }
+
+            @Override
+            public void onRejection(Exception e) {
+                onRejection.accept(e);
+            }
+        };
+    }
+
+}
+

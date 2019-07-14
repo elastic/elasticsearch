46bedaf37e62d2db8948dac03c60ba520f17516b
streamich <streamich@gmail.com>
2019-07-14 18:54:17 +0200

    feat: 🎸 add GraphQL library

---
+++ server/src/main/java/org/elasticsearch/graphql/GraphqlServer.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.elasticsearch.graphql;
+
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
+
+public class GraphqlServer {
+//    TypeDefinitionRegistry types;
+    RuntimeWiring wiring;
+    GraphQL graphql;
+
+    public GraphqlServer() {
+
+        wiring = newRuntimeWiring()
+            .type("Query",
+                builder -> builder
+                    .dataFetcher("hello", new StaticDataFetcher("world!"))
+            )
+            .build();
+
+//        SchemaGenerator schemaGenerator = new SchemaGenerator();
+//        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(types, wiring);
+
+//        graphql = GraphQL.newGraphQL(graphQLSchema).build();
+    }
+}

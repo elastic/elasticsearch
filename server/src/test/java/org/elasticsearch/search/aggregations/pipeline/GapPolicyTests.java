264e760be3ced3081e53da692915e999c5c12170
Zachary Tong <zach@elastic.co>
2019-07-15 16:57:34 -0400

    Convert BucketScript to static parser
    BucketScript was using the old-style parser and could easily be
    converted over to the newer static parser.
    
    Also adds a test for GapPolicy enum serialization

---
+++ server/src/test/java/org/elasticsearch/search/aggregations/pipeline/GapPolicyTests.java
@@ -0,0 +1,42 @@
+package org.elasticsearch.search.aggregations.pipeline;
+
+import org.elasticsearch.common.ParsingException;
+import org.elasticsearch.common.io.stream.AbstractWriteableEnumTestCase;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class GapPolicyTests extends AbstractWriteableEnumTestCase {
+
+    public GapPolicyTests() {
+        super(BucketHelpers.GapPolicy::readFrom);
+    }
+
+    @Override
+    public void testValidOrdinals() {
+        assertThat(BucketHelpers.GapPolicy.INSERT_ZEROS.ordinal(), equalTo(0));
+        assertThat(BucketHelpers.GapPolicy.SKIP.ordinal(), equalTo(1));
+    }
+
+    @Override
+    public void testFromString() {
+        assertThat(BucketHelpers.GapPolicy.parse("insert_zeros", null), equalTo(BucketHelpers.GapPolicy.INSERT_ZEROS));
+        assertThat(BucketHelpers.GapPolicy.parse("skip", null), equalTo(BucketHelpers.GapPolicy.SKIP));
+        ParsingException e = expectThrows(ParsingException.class, () -> BucketHelpers.GapPolicy.parse("does_not_exist", null));
+        assertThat(e.getMessage(),
+            equalTo("Invalid gap policy: [does_not_exist], accepted values: [insert_zeros, skip]"));
+    }
+
+    @Override
+    public void testReadFrom() throws IOException {
+        assertReadFromStream(0, BucketHelpers.GapPolicy.INSERT_ZEROS);
+        assertReadFromStream(1, BucketHelpers.GapPolicy.SKIP);
+    }
+
+    @Override
+    public void testWriteTo() throws IOException {
+        assertWriteToStream(BucketHelpers.GapPolicy.INSERT_ZEROS, 0);
+        assertWriteToStream(BucketHelpers.GapPolicy.SKIP, 1);
+    }
+}

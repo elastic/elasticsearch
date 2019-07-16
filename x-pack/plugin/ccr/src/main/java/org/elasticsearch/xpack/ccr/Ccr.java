100cb89f3e40ce307bba3070aca447498a24a48d
Jason Tedor <jason@tedor.me>
2019-07-17 07:23:40 +0900

    Avoid stack overflow in auto-follow coordinator (#44421)
    This commit avoids a situation where we might stack overflow in the
    auto-follower coordinator. In the face of repeated failures to get the
    remote cluster state, we would previously be called back on the same
    thread and then recurse to try again. If this failure persists, the
    repeated callbacks on the same thread would lead to a stack
    overflow. The repeated failures can occur, for example, if the connect
    queue is full when we attempt to make a connection to the remote
    cluster. This commit avoids this by truncating the call stack if we are
    called back on the same thread as the initial request was made on.

--- x-pack/plugin/ccr/src/main/java/org/elasticsearch/xpack/ccr/Ccr.java
+++ x-pack/plugin/ccr/src/main/java/org/elasticsearch/xpack/ccr/Ccr.java
@@ -181,16 +181,17 @@ public class Ccr extends Plugin implements ActionPlugin, PersistentTaskPlugin, E
         CcrRestoreSourceService restoreSourceService = new CcrRestoreSourceService(threadPool, ccrSettings);
         this.restoreSourceService.set(restoreSourceService);
         return Arrays.asList(
+            ccrLicenseChecker,
+            restoreSourceService,
+            new CcrRepositoryManager(settings, clusterService, client),
+            new AutoFollowCoordinator(
+                settings,
+                client,
+                clusterService,
                 ccrLicenseChecker,
-                restoreSourceService,
-                new CcrRepositoryManager(settings, clusterService, client),
-                new AutoFollowCoordinator(
-                        settings,
-                        client,
-                        clusterService,
-                        ccrLicenseChecker,
-                        threadPool::relativeTimeInMillis,
-                        threadPool::absoluteTimeInMillis));
+                threadPool::relativeTimeInMillis,
+                threadPool::absoluteTimeInMillis,
+                threadPool.executor(Ccr.CCR_THREAD_POOL_NAME)));
     }
 
     @Override

503ecbe16180a57b4954c8859a0a7f7aff34e01f
Tal Levy <jubboy333@gmail.com>
2019-07-16 10:17:45 -0700

    fix putdataframeanalyticsaction

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/action/PutDataFrameAnalyticsAction.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/action/PutDataFrameAnalyticsAction.java
@@ -29,7 +29,7 @@ public class PutDataFrameAnalyticsAction extends ActionType<PutDataFrameAnalytic
     public static final String NAME = "cluster:admin/xpack/ml/data_frame/analytics/put";
 
     private PutDataFrameAnalyticsAction() {
-        super(NAME);
+        super(NAME, Response::new);
     }
 
     public static class Request extends AcknowledgedRequest<Request> implements ToXContentObject {

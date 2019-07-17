834ca47e4210acda24782918c911b351159c2829
Tal Levy <JubBoy333@gmail.com>
2019-07-17 14:25:03 -0700

    migrate rollup/monitoring/graph/watcher actions to Writeable (#44464)
    this commit migrates leftover actions from a few x-pack plugins
    to the new Writeable.Reader infrastructure.
    
    relates #34389.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/protocol/xpack/graph/GraphExploreRequest.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/protocol/xpack/graph/GraphExploreRequest.java
@@ -121,6 +121,31 @@ public class GraphExploreRequest extends ActionRequest implements IndicesRequest
         return this;
     }
 
+    public GraphExploreRequest(StreamInput in) throws IOException {
+        super(in);
+
+        indices = in.readStringArray();
+        indicesOptions = IndicesOptions.readIndicesOptions(in);
+        types = in.readStringArray();
+        routing = in.readOptionalString();
+        timeout = in.readOptionalTimeValue();
+        sampleSize = in.readInt();
+        sampleDiversityField = in.readOptionalString();
+        maxDocsPerDiversityValue = in.readInt();
+
+        useSignificance = in.readBoolean();
+        returnDetailedInfo = in.readBoolean();
+
+        int numHops = in.readInt();
+        Hop parentHop = null;
+        for (int i = 0; i < numHops; i++) {
+            Hop hop = new Hop(parentHop);
+            hop.readFrom(in);
+            hops.add(hop);
+            parentHop = hop;
+        }
+    }
+
     public String routing() {
         return this.routing;
     }
@@ -164,29 +189,7 @@ public class GraphExploreRequest extends ActionRequest implements IndicesRequest
 
     @Override
     public void readFrom(StreamInput in) throws IOException {
-        super.readFrom(in);
-
-        indices = in.readStringArray();
-        indicesOptions = IndicesOptions.readIndicesOptions(in);
-        types = in.readStringArray();
-        routing = in.readOptionalString();
-        timeout = in.readOptionalTimeValue();
-        sampleSize = in.readInt();
-        sampleDiversityField = in.readOptionalString();
-        maxDocsPerDiversityValue = in.readInt();
-
-        useSignificance = in.readBoolean();
-        returnDetailedInfo = in.readBoolean();
-
-        int numHops = in.readInt();
-        Hop parentHop = null;
-        for (int i = 0; i < numHops; i++) {
-            Hop hop = new Hop(parentHop);
-            hop.readFrom(in);
-            hops.add(hop);
-            parentHop = hop;
-        }
-
+        throw new UnsupportedOperationException("usage of Streamable is to be replaced by Writeable");
     }
 
     @Override

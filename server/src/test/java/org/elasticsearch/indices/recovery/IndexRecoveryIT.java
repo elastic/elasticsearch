39c5f98de71708d756d36bfb303a87030e781bf2
Jason Tedor <jason@tedor.me>
2019-07-18 05:26:01 +0900

    Introduce test issue logging (#44477)
    Today we have an annotation for controlling logging levels in
    tests. This annotation serves two purposes, one is to control the
    logging level used in tests, when such control is needed to impact and
    assert the behavior of loggers in tests. The other use is when a test is
    failing and additional logging is needed. This commit separates these
    two concerns into separate annotations.
    
    The primary motivation for this is that we have a history of leaving
    behind the annotation for the purpose of investigating test failures
    long after the test failure is resolved. The accumulation of these stale
    logging annotations has led to excessive disk consumption. Having
    recently cleaned this up, we would like to avoid falling into this state
    again. To do this, we are adding a link to the test failure under
    investigation to the annotation when used for the purpose of
    investigating test failures. We will add tooling to inspect these
    annotations, in the same way that we have tooling on awaits fix
    annotations. This will enable us to report on the use of these
    annotations, and report when stale uses of the annotation exist.

--- server/src/test/java/org/elasticsearch/indices/recovery/IndexRecoveryIT.java
+++ server/src/test/java/org/elasticsearch/indices/recovery/IndexRecoveryIT.java
@@ -71,7 +71,6 @@ import org.elasticsearch.test.ESIntegTestCase;
 import org.elasticsearch.test.ESIntegTestCase.ClusterScope;
 import org.elasticsearch.test.ESIntegTestCase.Scope;
 import org.elasticsearch.test.InternalTestCluster;
-import org.elasticsearch.test.junit.annotations.TestLogging;
 import org.elasticsearch.test.store.MockFSIndexStore;
 import org.elasticsearch.test.transport.MockTransportService;
 import org.elasticsearch.test.transport.StubbableTransport;
@@ -291,12 +290,6 @@ public class IndexRecoveryIT extends ESIntegTestCase {
         assertHitCount(client().prepareSearch(INDEX_NAME).setSize(0).get(), numOfDocs);
     }
 
-    @TestLogging(
-            "_root:DEBUG,"
-                    + "org.elasticsearch.cluster.service:TRACE,"
-                    + "org.elasticsearch.indices.cluster:TRACE,"
-                    + "org.elasticsearch.indices.recovery:TRACE,"
-                    + "org.elasticsearch.index.shard:TRACE")
     public void testRerouteRecovery() throws Exception {
         logger.info("--> start node A");
         final String nodeA = internalCluster().startNode();
@@ -713,7 +706,6 @@ public class IndexRecoveryIT extends ESIntegTestCase {
      * Tests scenario where recovery target successfully sends recovery request to source but then the channel gets closed while
      * the source is working on the recovery process.
      */
-    @TestLogging("_root:DEBUG,org.elasticsearch.indices.recovery:TRACE")
     public void testDisconnectsDuringRecovery() throws Exception {
         boolean primaryRelocation = randomBoolean();
         final String indexName = "test";
@@ -1015,4 +1007,5 @@ public class IndexRecoveryIT extends ESIntegTestCase {
                 });
         }
     }
+
 }
--- test/framework/src/main/java/org/elasticsearch/test/junit/annotations/TestIssueLogging.java
+++ test/framework/src/main/java/org/elasticsearch/test/junit/annotations/TestIssueLogging.java
@@ -28,15 +28,15 @@ import static java.lang.annotation.ElementType.PACKAGE;
 import static java.lang.annotation.ElementType.TYPE;
 
 /**
- * Annotation used to set a custom log level for controlling logging behavior in tests. Do not use this annotation when
- * investigating test failures; instead, use {@link TestIssueLogging}.
+ * Annotation used to set a custom log level when investigating test failures. Do not use this annotation to explicitly
+ * control the logging level in tests; instead, use {@link TestLogging}.
  *
  * It supports multiple logger:level comma-separated key-value pairs of logger:level (e.g.,
  * org.elasticsearch.cluster.metadata:TRACE). Use the _root keyword to set the root logger level.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({PACKAGE, TYPE, METHOD})
-public @interface TestLogging {
+public @interface TestIssueLogging {
 
     /**
      * A comma-separated list of key-value pairs of logger:level. For each key-value pair of logger:level, the test
@@ -47,10 +47,10 @@ public @interface TestLogging {
     String value();
 
     /**
-     * The reason this annotation is used to control logger behavior during a test.
+     * This property is used to link to the open test issue under investigation.
      *
-     * @return the reason for adding the annotation
+     * @return the issue link
      */
-    String reason();
+    String issueUrl();
 
 }

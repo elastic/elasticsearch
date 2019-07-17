1f4aedbd7347208d4babf1fd7e7ceae7e43448f1
David Turner <david.turner@elastic.co>
2019-07-17 09:08:58 +0100

    Assert consistency of routing nodes

--- server/src/main/java/org/elasticsearch/cluster/routing/allocation/AllocationService.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/allocation/AllocationService.java
@@ -109,6 +109,7 @@ public class AllocationService {
         Collections.sort(startedShards, Comparator.comparing(ShardRouting::primary));
         applyStartedShards(allocation, startedShards);
         gatewayAllocator.applyStartedShards(allocation, startedShards);
+        assert RoutingNodes.assertShardStats(allocation.routingNodes());
         String startedShardsAsString = firstListElementsToCommaDelimitedString(startedShards, s -> s.shardId().toString());
         return buildResultAndLogHealthChange(clusterState, allocation, "shards started [" + startedShardsAsString + "] ...");
     }

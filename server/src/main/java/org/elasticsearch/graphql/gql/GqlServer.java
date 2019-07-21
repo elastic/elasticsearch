11332e010f986700ac069bdd6962d39d9d9778a8
streamich <streamich@gmail.com>
2019-07-21 15:31:01 +0200

    feat: start GQL server

--- server/src/main/java/org/elasticsearch/graphql/gql/GqlServer.java
+++ server/src/main/java/org/elasticsearch/graphql/gql/GqlServer.java
@@ -1,9 +1,49 @@
 package org.elasticsearch.graphql.gql;
 
+import graphql.*;
+import graphql.schema.*;
+
+import java.util.Map;
+
+import static graphql.schema.GraphQLObjectType.newObject;
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
+import static graphql.schema.GraphQLTypeReference.typeRef;
+import static graphql.schema.GraphQLNonNull.nonNull;
+import static graphql.schema.GraphQLList.list;
+import static graphql.schema.GraphQLArgument.newArgument;
+import static graphql.Scalars.*;
+
 public class GqlServer {
     GqlBuilder builder;
+    GraphQLSchema schema;
+    GraphQL graphql;
 
     public GqlServer() {
         builder = new GqlBuilder();
+
+        addPingResolve(builder);
+
+        schema = builder.build();
+        graphql = GraphQL.newGraphQL(schema).build();
+    }
+
+    void addPingResolve(GqlBuilder builder) {
+        builder
+            .queryField(newFieldDefinition()
+                .description("Ping server if it is available.")
+                .name("ping")
+                .type(nonNull(GraphQLString)))
+            .fetcher("Query", "ping", new StaticDataFetcher("pong"));
+    }
+
+    Map<String, Object> executeToSpecification(String query, String operationName, Map<String, Object> variables, Object ctx) {
+        ExecutionResult result = graphql.execute(
+            ExecutionInput.newExecutionInput(query)
+                .operationName(operationName)
+                .variables(variables)
+                .context(ctx)
+                .build()
+        );
+        return result.toSpecification();
     }
 }

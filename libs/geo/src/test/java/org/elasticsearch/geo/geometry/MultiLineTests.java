612e7e5776e5c8d0121b907f95207e8ce4b8d5c8
Igor Motov <igor@motovs.org>
2019-07-22 08:51:03 -0400

    GEO: Switch to using GeoTestUtil to generate random geo shapes (#44635)
    Switches to more robust way of generating random test geometries by
    reusing lucene's GeoTestUtil. Removes duplicate random geometry
    generators by moving them to the test framework.
    
    Closes #37278

--- libs/geo/src/test/java/org/elasticsearch/geo/geometry/MultiLineTests.java
+++ libs/geo/src/test/java/org/elasticsearch/geo/geometry/MultiLineTests.java
@@ -19,6 +19,7 @@
 
 package org.elasticsearch.geo.geometry;
 
+import org.elasticsearch.geo.GeometryTestUtils;
 import org.elasticsearch.geo.utils.GeographyValidator;
 import org.elasticsearch.geo.utils.StandardValidator;
 import org.elasticsearch.geo.utils.WellKnownText;
@@ -36,7 +37,7 @@ public class MultiLineTests extends BaseGeometryTestCase<MultiLine> {
         int size = randomIntBetween(1, 10);
         List<Line> arr = new ArrayList<Line>();
         for (int i = 0; i < size; i++) {
-            arr.add(randomLine(hasAlt));
+            arr.add(GeometryTestUtils.randomLine(hasAlt));
         }
         return new MultiLine(arr);
     }

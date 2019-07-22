82ace70d50aef0aabb09270f70f24126eb73df2c
streamich <streamich@gmail.com>
2019-07-23 00:29:29 +0200

    refactor: use utility to execute any rest handler

--- server/src/main/java/org/elasticsearch/graphql/api/GqlApiUtils.java
+++ server/src/main/java/org/elasticsearch/graphql/api/GqlApiUtils.java
@@ -5,40 +5,45 @@ import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.action.ActionType;
 import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
-import org.elasticsearch.common.xcontent.ToXContent;
-import org.elasticsearch.common.xcontent.ToXContentObject;
-import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.*;
 import org.elasticsearch.common.xcontent.javautil.JavaUtilXContent;
 import org.elasticsearch.common.xcontent.javautil.JavaUtilXContentGenerator;
+import org.elasticsearch.rest.*;
+import org.elasticsearch.rest.action.cat.RestIndicesAction;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class GqlApiUtils {
 
-    static XContentBuilder createJavaUtilBuilder() throws IOException  {
+    static public XContentBuilder createJavaUtilBuilder() throws IOException  {
         BytesStreamOutput bos = new BytesStreamOutput();
         XContentBuilder builder = new XContentBuilder(JavaUtilXContent.javaUtilXContent, bos);
         return builder;
     }
 
-    static Object getJavaUtilBuilderResult(XContentBuilder builder) throws Exception {
+    static public Object getJavaUtilBuilderResult(XContentBuilder builder) throws Exception {
         builder.close();
         JavaUtilXContentGenerator generator = (JavaUtilXContentGenerator) builder.generator();
         return generator.getResult();
     }
 
     @SuppressWarnings("unchecked")
-    static Map<String, Object> toMap(ToXContentObject response) throws Exception {
+    static public Map<String, Object> toMap(ToXContentObject response) throws Exception {
         XContentBuilder builder = createJavaUtilBuilder();
         response.toXContent(builder, ToXContent.EMPTY_PARAMS);
         return (Map) getJavaUtilBuilderResult(builder);
     }
 
-    static <Request extends ActionRequest, Response extends ActionResponse>
-    CompletableFuture<Map<String, Object>> executeAction(NodeClient client, ActionType<Response> action, Request request) {
+    static public <Request extends ActionRequest, Response extends ActionResponse>
+            CompletableFuture<Map<String, Object>> executeAction(NodeClient client,
+                                                                 ActionType<Response> action,
+                                                                 Request request) {
         CompletableFuture<Map<String, Object>> future = new CompletableFuture<Map<String, Object>>();
         client.execute(action, request, new ActionListener<Response>() {
             @Override
@@ -59,5 +64,70 @@ public class GqlApiUtils {
             }
         });
         return future;
+    }
+
+    @SuppressWarnings("unchecked")
+    static public CompletableFuture<List<Object>> executeRestHandler(NodeClient client,
+                                                                     BaseRestHandler handler,
+                                                                     RestRequest.Method method,
+                                                                     String uri) throws Exception {
+        CompletableFuture<List<Object>> promise = new CompletableFuture<>();
+        XContentBuilder builder = GqlApiUtils.createJavaUtilBuilder();
+
+        GqlApiFakeHttpRequest internalHttpRequest = new GqlApiFakeHttpRequest(method, uri, BytesArray.EMPTY, new HashMap<>());
+        GqlApiFakeHttpChannel internalHttpChannel = new GqlApiFakeHttpChannel(null);
+        RestRequest innerRequest = RestRequest.request(NamedXContentRegistry.EMPTY, internalHttpRequest, internalHttpChannel);
+
+        handler.handleRequest(innerRequest, new RestChannel() {
+            BytesStreamOutput bytesStreamOutput = new BytesStreamOutput();
+
+            @Override
+            public XContentBuilder newBuilder() throws IOException {
+                return builder;
+            }
+
+            @Override
+            public XContentBuilder newErrorBuilder() throws IOException {
+                return builder;
+            }
+
+            @Override
+            public XContentBuilder newBuilder(XContentType xContentType, boolean useFiltering) throws IOException {
+                return builder;
+            }
+
+            @Override
+            public XContentBuilder newBuilder(XContentType xContentType, XContentType responseContentType, boolean useFiltering) throws IOException {
+                return builder;
+            }
+
+            @Override
+            public BytesStreamOutput bytesOutput() {
+                return bytesStreamOutput;
+            }
+
+            @Override
+            public RestRequest request() {
+                return innerRequest;
+            }
+
+            @Override
+            public boolean detailedErrorsEnabled() {
+                return false;
+            }
+
+            @Override
+            public void sendResponse(RestResponse response) {
+                try {
+                    List<Object> result = (List) GqlApiUtils.getJavaUtilBuilderResult(builder);
+                    System.out.println(result);
+                    promise.complete(result);
+                } catch (Exception e) {
+                    promise.completeExceptionally(e);
+                }
+            }
+        }, client);
+
+        return promise;
     }
 }
--- server/src/main/java/org/elasticsearch/graphql/api/GqlElasticsearchApi.java
+++ server/src/main/java/org/elasticsearch/graphql/api/GqlElasticsearchApi.java
@@ -3,18 +3,9 @@ package org.elasticsearch.graphql.api;
 import org.elasticsearch.action.main.MainAction;
 import org.elasticsearch.action.main.MainRequest;
 import org.elasticsearch.client.node.NodeClient;
-import org.elasticsearch.common.bytes.BytesArray;
-import org.elasticsearch.common.io.stream.BytesStreamOutput;
-import org.elasticsearch.common.xcontent.NamedXContentRegistry;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.rest.BaseRestHandler;
-import org.elasticsearch.rest.RestChannel;
-import org.elasticsearch.rest.RestRequest;
-import org.elasticsearch.rest.RestResponse;
+import static org.elasticsearch.rest.RestRequest.Method.GET;
 import org.elasticsearch.rest.action.cat.RestIndicesAction;
 
-import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
@@ -33,66 +24,6 @@ public class GqlElasticsearchApi implements GqlApi {
     @Override
     @SuppressWarnings("unchecked")
     public CompletableFuture<List<Object>> getIndices() throws Exception {
-        CompletableFuture<List<Object>> promise = new CompletableFuture<>();
-        XContentBuilder builder = GqlApiUtils.createJavaUtilBuilder();
-
-        GqlApiFakeHttpRequest internalHttpRequest = new GqlApiFakeHttpRequest(RestRequest.Method.GET, "/_cat/indices?format=json", BytesArray.EMPTY, new HashMap<>());
-        GqlApiFakeHttpChannel internalHttpChannel = new GqlApiFakeHttpChannel(null);
-        RestRequest innerRequest = RestRequest.request(NamedXContentRegistry.EMPTY, internalHttpRequest, internalHttpChannel);
-
-        BaseRestHandler.RestChannelConsumer channelConsumer = RestIndicesAction.INSTANCE.doCatRequest(innerRequest, client);
-        channelConsumer.accept(new RestChannel() {
-            BytesStreamOutput bytes1 = new BytesStreamOutput();
-
-            @Override
-            public XContentBuilder newBuilder() throws IOException {
-                return builder;
-            }
-
-            @Override
-            public XContentBuilder newErrorBuilder() throws IOException {
-                return builder;
-            }
-
-            @Override
-            public XContentBuilder newBuilder(XContentType xContentType, boolean useFiltering) throws IOException {
-                return builder;
-            }
-
-            @Override
-            public XContentBuilder newBuilder(XContentType xContentType, XContentType responseContentType, boolean useFiltering) throws IOException {
-                return builder;
-            }
-
-            @Override
-            public BytesStreamOutput bytesOutput() {
-                return bytes1;
-            }
-
-            @Override
-            public RestRequest request() {
-                return innerRequest;
-            }
-
-            @Override
-            public boolean detailedErrorsEnabled() {
-                return false;
-            }
-
-            @Override
-            public void sendResponse(RestResponse response) {
-                System.out.println("SENDIN RESPONSE");
-                System.out.println(response);
-                try {
-                    List<Object> result = (List) GqlApiUtils.getJavaUtilBuilderResult(builder);
-                    System.out.println(result);
-                    promise.complete(result);
-                } catch (Exception e) {
-                    promise.completeExceptionally(e);
-                }
-            }
-        });
-
-        return promise;
+        return GqlApiUtils.executeRestHandler(client, RestIndicesAction.INSTANCE, GET, "/_cat/indices?format=json");
     }
 }

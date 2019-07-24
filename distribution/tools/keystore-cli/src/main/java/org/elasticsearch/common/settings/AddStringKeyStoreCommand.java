54de1eb5a41d397457f93603177804a33a591da2
William Brafford <william.brafford@elastic.co>
2019-07-23 21:08:11 -0400

    Remove some unused imports
    I must have missed these in a recent merge. Oops.

--- distribution/tools/keystore-cli/src/main/java/org/elasticsearch/common/settings/AddStringKeyStoreCommand.java
+++ distribution/tools/keystore-cli/src/main/java/org/elasticsearch/common/settings/AddStringKeyStoreCommand.java
@@ -19,10 +19,7 @@
 
 package org.elasticsearch.common.settings;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import joptsimple.OptionSet;

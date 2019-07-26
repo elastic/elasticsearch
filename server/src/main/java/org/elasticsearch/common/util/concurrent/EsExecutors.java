931503cc04e83990a3f3e98c75b7104cca12b780
Jason Tedor <jason@tedor.me>
2019-07-26 17:10:36 +0900

    Limit processors by available processors
    This commit limits the processors setting to be more than the number of
    available processors.

--- server/src/main/java/org/elasticsearch/common/util/concurrent/EsExecutors.java
+++ server/src/main/java/org/elasticsearch/common/util/concurrent/EsExecutors.java
@@ -48,26 +48,14 @@ import java.util.stream.Collectors;
 
 public class EsExecutors {
 
-    private static final DeprecationLogger deprecationLogger = new DeprecationLogger(LogManager.getLogger(EsExecutors.class));
-
     /**
      * Setting to manually set the number of available processors. This setting is used to adjust thread pool sizes per node.
      */
-    public static final Setting<Integer> PROCESSORS_SETTING = new Setting<>(
+    public static final Setting<Integer> PROCESSORS_SETTING = Setting.intSetting(
         "processors",
-        s -> Integer.toString(Runtime.getRuntime().availableProcessors()),
-        s -> {
-            final int value = Setting.parseInt(s, 1, "processors");
-            final int availableProcessors = Runtime.getRuntime().availableProcessors();
-            if (value > availableProcessors) {
-                deprecationLogger.deprecatedAndMaybeLog(
-                    "processors",
-                    "setting processors to value [{}] which is more than available processors [{}] is deprecated",
-                    value,
-                    availableProcessors);
-            }
-            return value;
-        },
+        Runtime.getRuntime().availableProcessors(),
+        1,
+        Runtime.getRuntime().availableProcessors(),
         Property.NodeScope);
 
     /**

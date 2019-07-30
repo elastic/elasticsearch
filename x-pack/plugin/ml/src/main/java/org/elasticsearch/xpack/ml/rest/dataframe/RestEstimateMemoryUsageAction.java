05839633f9ce62bc661d81f809a5bfafecc9dd54
Przemyslaw Witek <przemyslaw.witek@elastic.co>
2019-07-30 16:25:44 +0200

    Introduce data_frame/analytics/_estimate_memory_usage API endpoint

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/rest/dataframe/RestEstimateMemoryUsageAction.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/rest/dataframe/RestEstimateMemoryUsageAction.java
@@ -6,7 +6,6 @@
 package org.elasticsearch.xpack.ml.rest.dataframe;
 
 import org.elasticsearch.client.node.NodeClient;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.RestController;
 import org.elasticsearch.rest.RestRequest;
@@ -18,8 +17,7 @@ import java.io.IOException;
 
 public class RestEstimateMemoryUsageAction extends BaseRestHandler {
 
-    public RestEstimateMemoryUsageAction(Settings settings, RestController controller) {
-        super(settings);
+    public RestEstimateMemoryUsageAction(RestController controller) {
         controller.registerHandler(
             RestRequest.Method.POST,
             MachineLearning.BASE_PATH + "data_frame/analytics/_estimate_memory_usage", this);

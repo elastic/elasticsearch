85714700ee0085666e16d7ddd3a2abbe655a2536
Nick Knize <nknize@gmail.com>
2019-07-30 21:52:59 -0500

    [SPATIAL] New ShapeFieldMapper for indexing cartesian geometries (#44980)
    This commit adds a new ShapeFieldMapper to the xpack spatial module for
    indexing arbitrary cartesian geometries using a new field type called shape.
    The indexing approach leverages lucene's new XYShape field type which is
    backed by BKD in the same manner as LatLonShape but without the WGS84
    latitude longitude restrictions. The new field mapper builds on and
    extends the refactoring effort in AbstractGeometryFieldMapper and accepts
    shapes in either GeoJSON or WKT format (both of which support non geospatial
    geometries).
    
    Tests are provided in the ShapeFieldMapperTest class in the same manner
    as GeoShapeFieldMapperTests and LegacyGeoShapeFieldMapperTests.
    Documentation for how to use the new field type and what parameters are
    accepted is included. The QueryBuilder for searching indexed shapes is
    provided in a separate commit.

--- x-pack/plugin/spatial/src/main/java/org/elasticsearch/xpack/spatial/SpatialPlugin.java
+++ x-pack/plugin/spatial/src/main/java/org/elasticsearch/xpack/spatial/SpatialPlugin.java
@@ -7,13 +7,18 @@ package org.elasticsearch.xpack.spatial;
 
 import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.mapper.Mapper;
+import org.elasticsearch.plugins.MapperPlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.xpack.core.XPackPlugin;
+import org.elasticsearch.xpack.spatial.index.mapper.ShapeFieldMapper;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class SpatialPlugin extends Plugin {
+public class SpatialPlugin extends Plugin implements MapperPlugin {
 
     public SpatialPlugin(Settings settings) {
     }
@@ -22,5 +27,12 @@ public class SpatialPlugin extends Plugin {
         return Collections.singletonList(b -> {
             XPackPlugin.bindFeatureSet(b, SpatialFeatureSet.class);
         });
+    }
+
+    @Override
+    public Map<String, Mapper.TypeParser> getMappers() {
+        Map<String, Mapper.TypeParser> mappers = new LinkedHashMap<>();
+        mappers.put(ShapeFieldMapper.CONTENT_TYPE, new ShapeFieldMapper.TypeParser());
+        return Collections.unmodifiableMap(mappers);
     }
 }

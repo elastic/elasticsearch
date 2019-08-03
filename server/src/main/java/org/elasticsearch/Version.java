e5805c961e3f220af399713a85ba484966da76ed
Jason Tedor <jason@tedor.me>
2019-08-03 11:24:47 -0400

    Use the full hash in build info (#45163)
    This commit switches to using the full hash to build into the JAR
    manifest, which is used in node startup and the REST main action to
    display the build hash.

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -338,7 +338,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
             Build.CURRENT.getQualifiedVersion(),
                 Build.CURRENT.flavor().displayName(),
                 Build.CURRENT.type().displayName(),
-                Build.CURRENT.shortHash(),
+                Build.CURRENT.hash(),
                 Build.CURRENT.date(),
                 JvmInfo.jvmInfo().version());
         System.out.println(versionOutput);

9f7df372066d29c6d70831e4df3aad3e326d89e5
Tim Vernum <tim@adjective.org>
2019-08-07 18:35:06 +1000

    Add SSL/TLS settings for watcher email
    This change adds a new SSL context
    
       xpack.notification.email.ssl.*
    
    that supports the standard SSL configuration settings (truststore,
    verification_mode, etc). This SSL context is used when configuring
    outbound SMTP properties for watcher email notifications.
    
    Resolves: #30307

--- x-pack/plugin/watcher/src/main/java/org/elasticsearch/xpack/watcher/Watcher.java
+++ x-pack/plugin/watcher/src/main/java/org/elasticsearch/xpack/watcher/Watcher.java
@@ -265,11 +265,12 @@ public class Watcher extends Plugin implements ActionPlugin, ScriptPlugin, Reloa
 
         new WatcherIndexTemplateRegistry(environment.settings(), clusterService, threadPool, client, xContentRegistry);
 
+        final SSLService sslService = getSslService();
         // http client
-        httpClient = new HttpClient(settings, getSslService(), cryptoService, clusterService);
+        httpClient = new HttpClient(settings, sslService, cryptoService, clusterService);
 
         // notification
-        EmailService emailService = new EmailService(settings, cryptoService, clusterService.getClusterSettings());
+        EmailService emailService = new EmailService(settings, cryptoService, sslService, clusterService.getClusterSettings());
         JiraService jiraService = new JiraService(settings, httpClient, clusterService.getClusterSettings());
         SlackService slackService = new SlackService(settings, httpClient, clusterService.getClusterSettings());
         PagerDutyService pagerDutyService = new PagerDutyService(settings, httpClient, clusterService.getClusterSettings());

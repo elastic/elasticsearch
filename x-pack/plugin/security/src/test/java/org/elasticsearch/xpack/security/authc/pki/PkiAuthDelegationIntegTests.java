1d6e31b8304411f4110cfc60b4a1939c9ffef880
Albert Zaharovits <albert.zaharovits@elastic.co>
2019-08-07 11:18:34 +0300

    Cluster privilege for proxied PKI (#44862)
    This adds the delegate_pki cluster privilege that grants
    permissions to run the delegate-pki and invalidate-token
    APIs. The delegate_pki is not part of the manage, manage_security
    or any other cluster privilege apart from all.
    
    It also adds the privilege to the kibana_system role.

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authc/pki/PkiAuthDelegationIntegTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authc/pki/PkiAuthDelegationIntegTests.java
@@ -18,13 +18,16 @@ import org.elasticsearch.client.security.DeleteRoleMappingRequest;
 import org.elasticsearch.client.security.support.expressiondsl.fields.FieldRoleMapperExpression;
 import org.elasticsearch.client.security.DelegatePkiAuthenticationRequest;
 import org.elasticsearch.client.security.DelegatePkiAuthenticationResponse;
+import org.elasticsearch.client.security.InvalidateTokenRequest;
+import org.elasticsearch.client.security.InvalidateTokenResponse;
 import org.elasticsearch.client.security.user.User;
 import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.test.SecurityIntegTestCase;
-import org.elasticsearch.test.SecuritySettingsSource;
 import org.elasticsearch.test.SecuritySettingsSourceField;
 import org.elasticsearch.xpack.core.XPackSettings;
+import org.elasticsearch.xpack.core.security.authc.support.Hasher;
+import org.elasticsearch.test.SecuritySettingsSource;
 
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -53,7 +56,7 @@ public class PkiAuthDelegationIntegTests extends SecurityIntegTestCase {
                 .putList("xpack.security.authc.realms.pki.pki1.certificate_authorities",
                     getDataPath("/org/elasticsearch/xpack/security/action/pki_delegation/testRootCA.crt").toString())
                 .put("xpack.security.authc.realms.pki.pki1.files.role_mapping", getDataPath("role_mapping.yml"))
-                // pki2 allows delegation but has a non-matching username pattern 
+                // pki2 allows delegation but has a non-matching username pattern
                 .put("xpack.security.authc.realms.pki.pki2.order", "2")
                 .putList("xpack.security.authc.realms.pki.pki2.certificate_authorities",
                     getDataPath("/org/elasticsearch/xpack/security/action/pki_delegation/testRootCA.crt").toString())
@@ -70,6 +73,44 @@ public class PkiAuthDelegationIntegTests extends SecurityIntegTestCase {
     }
 
     @Override
+    protected String configUsers() {
+        final String usersPasswdHashed = new String(Hasher.resolve(
+            randomFrom("pbkdf2", "pbkdf2_1000", "bcrypt", "bcrypt9")).hash(SecuritySettingsSourceField.TEST_PASSWORD_SECURE_STRING));
+        return super.configUsers() +
+            "user_manage:" + usersPasswdHashed + "\n" +
+            "user_manage_security:" + usersPasswdHashed + "\n" +
+            "user_delegate_pki:" + usersPasswdHashed + "\n" +
+            "user_all:" + usersPasswdHashed + "\n" +
+            "kibana_system:" + usersPasswdHashed + "\n";
+    }
+
+    @Override
+    protected String configRoles() {
+        return super.configRoles() + "\n" +
+                "role_manage:\n" +
+                "  cluster: [ manage ]\n" +
+                "\n" +
+                "role_manage_security:\n" +
+                "  cluster: [ manage_security ]\n" +
+                "\n" +
+                "role_delegate_pki:\n" +
+                "  cluster: [ delegate_pki ]\n" +
+                "\n" +
+                "role_all:\n" +
+                "  cluster: [ all ]\n";
+    }
+
+    @Override
+    protected String configUsersRoles() {
+        return super.configUsersRoles() + "\n" +
+                "role_manage:user_manage\n" +
+                "role_manage_security:user_manage_security\n" +
+                "role_delegate_pki:user_delegate_pki\n" +
+                "role_all:user_all\n" +
+                "kibana_system:kibana_system\n";
+    }
+
+    @Override
     protected boolean transportSSLEnabled() {
         return true;
     }
@@ -79,26 +120,68 @@ public class PkiAuthDelegationIntegTests extends SecurityIntegTestCase {
         return false; // enable http
     }
 
-    public void testDelegatePki() throws Exception {
-        X509Certificate clientCertificate = readCertForPkiDelegation("testClient.crt");
-        X509Certificate intermediateCA = readCertForPkiDelegation("testIntermediateCA.crt");
-        X509Certificate rootCA = readCertForPkiDelegation("testRootCA.crt");
-        RequestOptions.Builder optionsBuilder;
+    public void testDelegateThenAuthenticate() throws Exception {
+        final X509Certificate clientCertificate = readCertForPkiDelegation("testClient.crt");
+        final X509Certificate intermediateCA = readCertForPkiDelegation("testIntermediateCA.crt");
+        final X509Certificate rootCA = readCertForPkiDelegation("testRootCA.crt");
+        DelegatePkiAuthenticationRequest delegatePkiRequest;
+        // trust root is optional
+        if (randomBoolean()) {
+            delegatePkiRequest = new DelegatePkiAuthenticationRequest(Arrays.asList(clientCertificate, intermediateCA));
+        } else {
+            delegatePkiRequest = new DelegatePkiAuthenticationRequest(Arrays.asList(clientCertificate, intermediateCA, rootCA));
+        }
+
         try (RestHighLevelClient restClient = new TestRestHighLevelClient()) {
-            DelegatePkiAuthenticationRequest delegatePkiRequest;
-            // trust root is optional
-            if (randomBoolean()) {
-                delegatePkiRequest = new DelegatePkiAuthenticationRequest(Arrays.asList(clientCertificate, intermediateCA));
-            } else {
-                delegatePkiRequest = new DelegatePkiAuthenticationRequest(Arrays.asList(clientCertificate, intermediateCA, rootCA));
+            for (String delegateeUsername : Arrays.asList("user_all", "user_delegate_pki", "kibana_system")) {
+                // delegate
+                RequestOptions.Builder optionsBuilder = RequestOptions.DEFAULT.toBuilder();
+                optionsBuilder.addHeader("Authorization",
+                        basicAuthHeaderValue(delegateeUsername, SecuritySettingsSourceField.TEST_PASSWORD_SECURE_STRING));
+                DelegatePkiAuthenticationResponse delegatePkiResponse = restClient.security().delegatePkiAuthentication(delegatePkiRequest,
+                        optionsBuilder.build());
+                String token = delegatePkiResponse.getAccessToken();
+                assertThat(token, is(notNullValue()));
+                // authenticate
+                optionsBuilder = RequestOptions.DEFAULT.toBuilder();
+                optionsBuilder.addHeader("Authorization", "Bearer " + token);
+                AuthenticateResponse resp = restClient.security().authenticate(optionsBuilder.build());
+                User user = resp.getUser();
+                assertThat(user, is(notNullValue()));
+                assertThat(user.getUsername(), is("Elasticsearch Test Client"));
+                RealmInfo authnRealm = resp.getAuthenticationRealm();
+                assertThat(authnRealm, is(notNullValue()));
+                assertThat(authnRealm.getName(), is("pki3"));
+                assertThat(authnRealm.getType(), is("pki"));
             }
-            optionsBuilder = RequestOptions.DEFAULT.toBuilder();
-            optionsBuilder.addHeader("Authorization", basicAuthHeaderValue(SecuritySettingsSource.TEST_USER_NAME,
-                    new SecureString(SecuritySettingsSourceField.TEST_PASSWORD.toCharArray())));
+        }
+    }
+
+    public void testTokenInvalidate() throws Exception {
+        final X509Certificate clientCertificate = readCertForPkiDelegation("testClient.crt");
+        final X509Certificate intermediateCA = readCertForPkiDelegation("testIntermediateCA.crt");
+        final X509Certificate rootCA = readCertForPkiDelegation("testRootCA.crt");
+        DelegatePkiAuthenticationRequest delegatePkiRequest;
+        // trust root is optional
+        if (randomBoolean()) {
+            delegatePkiRequest = new DelegatePkiAuthenticationRequest(Arrays.asList(clientCertificate, intermediateCA));
+        } else {
+            delegatePkiRequest = new DelegatePkiAuthenticationRequest(Arrays.asList(clientCertificate, intermediateCA, rootCA));
+        }
+
+        try (RestHighLevelClient restClient = new TestRestHighLevelClient()) {
+            String delegateeUsername = randomFrom("user_all", "user_delegate_pki", "kibana_system");
+            // delegate
+            RequestOptions.Builder optionsBuilder = RequestOptions.DEFAULT.toBuilder();
+            optionsBuilder.addHeader("Authorization",
+                    basicAuthHeaderValue(delegateeUsername, SecuritySettingsSourceField.TEST_PASSWORD_SECURE_STRING));
             DelegatePkiAuthenticationResponse delegatePkiResponse = restClient.security().delegatePkiAuthentication(delegatePkiRequest,
                     optionsBuilder.build());
+            String token = delegatePkiResponse.getAccessToken();
+            assertThat(token, is(notNullValue()));
+            // authenticate
             optionsBuilder = RequestOptions.DEFAULT.toBuilder();
-            optionsBuilder.addHeader("Authorization", "Bearer " + delegatePkiResponse.getAccessToken());
+            optionsBuilder.addHeader("Authorization", "Bearer " + token);
             AuthenticateResponse resp = restClient.security().authenticate(optionsBuilder.build());
             User user = resp.getUser();
             assertThat(user, is(notNullValue()));
@@ -106,7 +189,7 @@ public class PkiAuthDelegationIntegTests extends SecurityIntegTestCase {
             assertThat(user.getMetadata().get("pki_dn"), is(notNullValue()));
             assertThat(user.getMetadata().get("pki_dn"), is("O=org, OU=Elasticsearch, CN=Elasticsearch Test Client"));
             assertThat(user.getMetadata().get("pki_delegated_by_user"), is(notNullValue()));
-            assertThat(user.getMetadata().get("pki_delegated_by_user"), is("test_user"));
+            assertThat(user.getMetadata().get("pki_delegated_by_user"), is(delegateeUsername));
             assertThat(user.getMetadata().get("pki_delegated_by_realm"), is(notNullValue()));
             assertThat(user.getMetadata().get("pki_delegated_by_realm"), is("file"));
             // no roles because no role mappings
@@ -115,6 +198,43 @@ public class PkiAuthDelegationIntegTests extends SecurityIntegTestCase {
             assertThat(authnRealm, is(notNullValue()));
             assertThat(authnRealm.getName(), is("pki3"));
             assertThat(authnRealm.getType(), is("pki"));
+            // invalidate
+            InvalidateTokenRequest invalidateRequest = new InvalidateTokenRequest(token, null, null, null);
+            optionsBuilder = RequestOptions.DEFAULT.toBuilder();
+            optionsBuilder.addHeader("Authorization",
+                    basicAuthHeaderValue(delegateeUsername, SecuritySettingsSourceField.TEST_PASSWORD_SECURE_STRING));
+            InvalidateTokenResponse invalidateResponse = restClient.security().invalidateToken(invalidateRequest, optionsBuilder.build());
+            assertThat(invalidateResponse.getInvalidatedTokens(), is(1));
+            assertThat(invalidateResponse.getErrorsCount(), is(0));
+            // failed authenticate
+            ElasticsearchStatusException e1 = expectThrows(ElasticsearchStatusException.class, () -> restClient.security()
+                    .authenticate(RequestOptions.DEFAULT.toBuilder().addHeader("Authorization", "Bearer " + token).build()));
+            assertThat(e1.getMessage(), is("Elasticsearch exception [type=security_exception, reason=token expired]"));
+        }
+    }
+
+    public void testDelegateUnauthorized() throws Exception {
+        final X509Certificate clientCertificate = readCertForPkiDelegation("testClient.crt");
+        final X509Certificate intermediateCA = readCertForPkiDelegation("testIntermediateCA.crt");
+        final X509Certificate rootCA = readCertForPkiDelegation("testRootCA.crt");
+        DelegatePkiAuthenticationRequest delegatePkiRequest;
+        // trust root is optional
+        if (randomBoolean()) {
+            delegatePkiRequest = new DelegatePkiAuthenticationRequest(Arrays.asList(clientCertificate, intermediateCA));
+        } else {
+            delegatePkiRequest = new DelegatePkiAuthenticationRequest(Arrays.asList(clientCertificate, intermediateCA, rootCA));
+        }
+        try (RestHighLevelClient restClient = new TestRestHighLevelClient()) {
+            for (String delegateeUsername : Arrays.asList("user_manage", "user_manage_security")) {
+                RequestOptions.Builder optionsBuilder = RequestOptions.DEFAULT.toBuilder();
+                optionsBuilder.addHeader("Authorization",
+                        basicAuthHeaderValue(delegateeUsername, SecuritySettingsSourceField.TEST_PASSWORD_SECURE_STRING));
+                ElasticsearchStatusException e = expectThrows(ElasticsearchStatusException.class, () -> {
+                    restClient.security().delegatePkiAuthentication(delegatePkiRequest, optionsBuilder.build());
+                });
+                assertThat(e.getMessage(), startsWith("Elasticsearch exception [type=security_exception, reason=action"
+                        + " [cluster:admin/xpack/security/delegate_pki] is unauthorized for user"));
+            }
         }
     }
 
@@ -123,6 +243,7 @@ public class PkiAuthDelegationIntegTests extends SecurityIntegTestCase {
         X509Certificate intermediateCA = readCertForPkiDelegation("testIntermediateCA.crt");
         X509Certificate rootCA = readCertForPkiDelegation("testRootCA.crt");
         DelegatePkiAuthenticationRequest delegatePkiRequest;
+        // trust root is optional
         if (randomBoolean()) {
             delegatePkiRequest = new DelegatePkiAuthenticationRequest(Arrays.asList(clientCertificate, intermediateCA));
         } else {
@@ -171,7 +292,7 @@ public class PkiAuthDelegationIntegTests extends SecurityIntegTestCase {
         }
     }
 
-    public void testDelegatePkiFailure() throws Exception {
+    public void testIncorrectCertChain() throws Exception {
         X509Certificate clientCertificate = readCertForPkiDelegation("testClient.crt");
         X509Certificate intermediateCA = readCertForPkiDelegation("testIntermediateCA.crt");
         X509Certificate bogusCertificate = readCertForPkiDelegation("bogus.crt");

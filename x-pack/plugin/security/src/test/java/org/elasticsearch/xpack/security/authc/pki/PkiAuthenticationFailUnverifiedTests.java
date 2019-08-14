fe7adf96059f04b9e8cc96f03d89528bbca9e68b
Albert Zaharovits <albert.zaharovits@elastic.co>
2019-08-15 01:11:04 +0300

    Test

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authc/pki/PkiAuthenticationFailUnverifiedTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authc/pki/PkiAuthenticationFailUnverifiedTests.java
@@ -3,6 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+
 package org.elasticsearch.xpack.security.authc.pki;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -19,10 +20,8 @@ import org.elasticsearch.xpack.core.common.socket.SocketAccess;
 import org.elasticsearch.xpack.core.ssl.CertParsingUtils;
 import org.elasticsearch.xpack.core.ssl.PemUtils;
 import org.elasticsearch.xpack.core.ssl.SSLClientAuth;
+import org.elasticsearch.xpack.core.ssl.VerificationMode;
 
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
 import java.net.InetSocketAddress;
 import java.security.SecureRandom;
 import java.util.Arrays;
@@ -31,14 +30,15 @@ import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+
 import static org.elasticsearch.test.SecuritySettingsSource.addSSLSettingsForNodePEMFiles;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
-/**
- * Test authentication via PKI on both REST and Transport layers
- */
-public class PkiAuthenticationTests extends SecuritySingleNodeTestCase {
+public class PkiAuthenticationFailUnverifiedTests extends SecuritySingleNodeTestCase {
 
     @Override
     protected boolean addMockHttpTransport() {
@@ -48,28 +48,22 @@ public class PkiAuthenticationTests extends SecuritySingleNodeTestCase {
     @Override
     protected Settings nodeSettings() {
         SSLClientAuth sslClientAuth = randomBoolean() ? SSLClientAuth.REQUIRED : SSLClientAuth.OPTIONAL;
-
         Settings.Builder builder = Settings.builder()
             .put(super.nodeSettings());
         addSSLSettingsForNodePEMFiles(builder, "xpack.security.http.", true);
         builder.put("xpack.security.http.ssl.enabled", true)
             .put("xpack.security.http.ssl.client_authentication", sslClientAuth)
+            // verification mode does not validate certificates, hence those should not be used by the PKI realm
+            .put("xpack.security.http.ssl.verification_mode", VerificationMode.NONE)
             .put("xpack.security.authc.realms.file.file.order", "0")
             .put("xpack.security.authc.realms.pki.pki1.order", "1")
             .putList("xpack.security.authc.realms.pki.pki1.certificate_authorities",
                 getDataPath("/org/elasticsearch/xpack/security/transport/ssl/certs/simple/testclient.crt").toString())
-            .put("xpack.security.authc.realms.pki.pki1.files.role_mapping", getDataPath("role_mapping.yml"))
-            // pki1 never authenticates because of the principal pattern
-            .put("xpack.security.authc.realms.pki.pki1.username_pattern", "CN=(MISMATCH.*?)(?:,|$)")
-            .put("xpack.security.authc.realms.pki.pki2.order", "2")
-            .putList("xpack.security.authc.realms.pki.pki2.certificate_authorities",
-                getDataPath("/org/elasticsearch/xpack/security/transport/ssl/certs/simple/testnode.crt").toString(),
-                getDataPath("/org/elasticsearch/xpack/security/transport/ssl/certs/simple/testnode_ec.crt").toString())
-            .put("xpack.security.authc.realms.pki.pki2.files.role_mapping", getDataPath("role_mapping.yml"));
+            .put("xpack.security.authc.realms.pki.pki1.files.role_mapping", getDataPath("role_mapping.yml"));
         return builder.build();
     }
 
-    public void testRestAuthenticationViaPki() throws Exception {
+    public void testAuthenticationFailsWithMissingCredentials() throws Exception {
         SSLContext context = getRestSSLContext("/org/elasticsearch/xpack/security/transport/ssl/certs/simple/testnode.pem",
             "testnode",
             "/org/elasticsearch/xpack/security/transport/ssl/certs/simple/testnode.crt",
@@ -79,24 +73,9 @@ public class PkiAuthenticationTests extends SecuritySingleNodeTestCase {
         try (CloseableHttpClient client = HttpClients.custom().setSSLContext(context).build()) {
             HttpPut put = new HttpPut(getNodeUrl() + "foo");
             try (CloseableHttpResponse response = SocketAccess.doPrivileged(() -> client.execute(put))) {
-                String body = EntityUtils.toString(response.getEntity());
-                assertThat(body, containsString("\"acknowledged\":true"));
-            }
-        }
-    }
-
-    public void testRestAuthenticationFailure() throws Exception {
-        SSLContext context = getRestSSLContext("/org/elasticsearch/xpack/security/transport/ssl/certs/simple/testclient.pem",
-            "testclient", "/org/elasticsearch/xpack/security/transport/ssl/certs/simple/testclient.crt",
-            Arrays.asList("/org/elasticsearch/xpack/security/transport/ssl/certs/simple/testclient.crt",
-                "/org/elasticsearch/xpack/security/transport/ssl/certs/simple/testnode.crt",
-                "/org/elasticsearch/xpack/security/transport/ssl/certs/simple/testnode_ec.crt"));
-        try (CloseableHttpClient client = HttpClients.custom().setSSLContext(context).build()) {
-            HttpPut put = new HttpPut(getNodeUrl() + "foo");
-            try (CloseableHttpResponse response = SocketAccess.doPrivileged(() -> client.execute(put))) {
                 assertThat(response.getStatusLine().getStatusCode(), is(401));
                 String body = EntityUtils.toString(response.getEntity());
-                assertThat(body, containsString("unable to authenticate user [Elasticsearch Test Client]"));
+                assertThat(body, containsString("missing authentication credentials for REST request"));
             }
         }
     }

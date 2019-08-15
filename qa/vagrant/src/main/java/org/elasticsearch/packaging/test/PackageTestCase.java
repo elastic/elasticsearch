c4b494b8fcbc47a96fcf914408230fd7fe70247d
Jason Tedor <jason@tedor.me>
2019-08-15 16:14:57 -0400

    Use bundled JDK in Sys V init (#45593)
    This commit addresses an issue when trying to using Elasticsearch on
    systems with Sys V init and the bundled JDK was not being used. Instead,
    we were still inadvertently trying to fallback on the path. This commit
    removes that fallback as that is against our intentions for 7.x where we
    only support the bundled JDK or an explicit JDK via JAVA_HOME.

--- qa/vagrant/src/main/java/org/elasticsearch/packaging/test/PackageTestCase.java
+++ qa/vagrant/src/main/java/org/elasticsearch/packaging/test/PackageTestCase.java
@@ -136,6 +136,25 @@ public abstract class PackageTestCase extends PackagingTestCase {
         assertRunsWithJavaHome();
     }
 
+    public void test33RunsIfJavaNotOnPath() throws Exception {
+        assumeThat(distribution().hasJdk, is(true));
+
+        final Result readlink = sh.run("readlink /usr/bin/java");
+        boolean unlinked = false;
+        try {
+            sh.run("unlink /usr/bin/java");
+            unlinked = true;
+
+            startElasticsearch(sh);
+            runElasticsearchTests();
+            stopElasticsearch(sh);
+        } finally {
+            if (unlinked) {
+                sh.run("ln -sf " + readlink.stdout.trim() + " /usr/bin/java");
+            }
+        }
+    }
+
     public void test42BundledJdkRemoved() throws Exception {
         assumeThat(installation, is(notNullValue()));
         assumeThat(distribution().hasJdk, is(true));

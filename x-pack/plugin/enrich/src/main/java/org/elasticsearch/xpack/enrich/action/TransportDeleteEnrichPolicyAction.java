ae4bfe99ecd6e298c56236c7abf9a53c6b490ab2
Michael Basnight <mbasnight@gmail.com>
2019-08-15 06:20:16 -0500

    Fix policy removal bug in delete policy (#45573)
    The delete policy had a subtle bug in that it would still delete the
    policy if pipelines were accessing it, after giving the client back an
    error. This commit fixes that and ensures it does not happen by adding
    verification in the test.

--- x-pack/plugin/enrich/src/main/java/org/elasticsearch/xpack/enrich/action/TransportDeleteEnrichPolicyAction.java
+++ x-pack/plugin/enrich/src/main/java/org/elasticsearch/xpack/enrich/action/TransportDeleteEnrichPolicyAction.java
@@ -83,6 +83,7 @@ public class TransportDeleteEnrichPolicyAction extends TransportMasterNodeAction
             listener.onFailure(
                 new ElasticsearchStatusException("Could not delete policy [{}] because a pipeline is referencing it {}",
                     RestStatus.CONFLICT, request.getName(), pipelinesWithProcessors));
+            return;
         }
 
         EnrichStore.deletePolicy(request.getName(), clusterService, e -> {

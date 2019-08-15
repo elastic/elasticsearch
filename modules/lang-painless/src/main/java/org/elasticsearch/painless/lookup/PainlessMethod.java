de58353722d0cc767cc1149948c2ae9e7e816ace
Armin Braun <me@obrown.io>
2019-08-15 19:41:45 +0200

    Lower Painless Static Memory Footprint (#45487) (#45619)
    * Painless generates a ton of duplicate strings and empty `Hashmap` instances wrapped as unmodifiable
    * This change brings down the static footprint of Painless on an idle node by 20MB (after running the PMC benchmark against said node)
       * Since we were looking into ways of optimizing for smaller node sizes I think this is a worthwhile optimization

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/lookup/PainlessMethod.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/lookup/PainlessMethod.java
@@ -22,6 +22,7 @@ package org.elasticsearch.painless.lookup;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -41,7 +42,7 @@ public class PainlessMethod {
         this.javaMethod = javaMethod;
         this.targetClass = targetClass;
         this.returnType = returnType;
-        this.typeParameters = Collections.unmodifiableList(typeParameters);
+        this.typeParameters = typeParameters.isEmpty() ? Collections.emptyList() : Arrays.asList(typeParameters.toArray(new Class<?>[0]));
         this.methodHandle = methodHandle;
         this.methodType = methodType;
     }

2e3772d63c7d8a69730b71b613cdde9939660938
Zachary Tong <zach@elastic.co>
2019-08-16 16:50:59 -0400

    checkstyle

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/datascience/DataScienceFeatureSetUsage.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/datascience/DataScienceFeatureSetUsage.java
@@ -7,7 +7,6 @@
 package org.elasticsearch.xpack.core.datascience;
 
 import org.elasticsearch.common.io.stream.StreamInput;
-import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.xpack.core.XPackFeatureSet;
 import org.elasticsearch.xpack.core.XPackField;
 

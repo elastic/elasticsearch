80dd0a0948e7158da1114587b8ea15470b5373bd
Przemysław Witek <przemyslaw.witek@elastic.co>
2019-08-20 15:49:17 +0200

    Get rid of EstimateMemoryUsageRequest and EstimateMemoryUsageAction.Request. (#45718) (#45725)

--- client/rest-high-level/src/main/java/org/elasticsearch/client/MachineLearningClient.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/MachineLearningClient.java
@@ -34,7 +34,6 @@ import org.elasticsearch.client.ml.DeleteForecastRequest;
 import org.elasticsearch.client.ml.DeleteJobRequest;
 import org.elasticsearch.client.ml.DeleteJobResponse;
 import org.elasticsearch.client.ml.DeleteModelSnapshotRequest;
-import org.elasticsearch.client.ml.EstimateMemoryUsageRequest;
 import org.elasticsearch.client.ml.EstimateMemoryUsageResponse;
 import org.elasticsearch.client.ml.EvaluateDataFrameRequest;
 import org.elasticsearch.client.ml.EvaluateDataFrameResponse;
@@ -2195,14 +2194,15 @@ public final class MachineLearningClient {
      * see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/estimate-memory-usage-dfanalytics.html">
      *     Estimate Memory Usage for Data Frame Analytics documentation</a>
      *
-     * @param request The {@link EstimateMemoryUsageRequest}
+     * @param request The {@link PutDataFrameAnalyticsRequest}
      * @param options Additional request options (e.g. headers), use {@link RequestOptions#DEFAULT} if nothing needs to be customized
      * @return {@link EstimateMemoryUsageResponse} response object
      * @throws IOException when there is a serialization issue sending the request or receiving the response
      */
-    public EstimateMemoryUsageResponse estimateMemoryUsage(EstimateMemoryUsageRequest request,
+    public EstimateMemoryUsageResponse estimateMemoryUsage(PutDataFrameAnalyticsRequest request,
                                                            RequestOptions options) throws IOException {
-        return restHighLevelClient.performRequestAndParseEntity(request,
+        return restHighLevelClient.performRequestAndParseEntity(
+            request,
             MLRequestConverters::estimateMemoryUsage,
             options,
             EstimateMemoryUsageResponse::fromXContent,
@@ -2216,13 +2216,14 @@ public final class MachineLearningClient {
      * see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/estimate-memory-usage-dfanalytics.html">
      *     Estimate Memory Usage for Data Frame Analytics documentation</a>
      *
-     * @param request The {@link EstimateMemoryUsageRequest}
+     * @param request The {@link PutDataFrameAnalyticsRequest}
      * @param options Additional request options (e.g. headers), use {@link RequestOptions#DEFAULT} if nothing needs to be customized
      * @param listener Listener to be notified upon request completion
      */
-    public void estimateMemoryUsageAsync(EstimateMemoryUsageRequest request, RequestOptions options,
+    public void estimateMemoryUsageAsync(PutDataFrameAnalyticsRequest request, RequestOptions options,
                                          ActionListener<EstimateMemoryUsageResponse> listener) {
-        restHighLevelClient.performRequestAsyncAndParseEntity(request,
+        restHighLevelClient.performRequestAsyncAndParseEntity(
+            request,
             MLRequestConverters::estimateMemoryUsage,
             options,
             EstimateMemoryUsageResponse::fromXContent,

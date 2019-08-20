da53ac4423ae73105bd2c93c0bcbd60b12088ff9
William Brafford <william.brafford@elastic.co>
2019-08-20 19:38:51 -0400

    Pass COMPUTERNAME env var to elasticsearch.bat
    When we run bin/elasticsearch with bash, we get a $HOSTNAME builtin that
    contains the hostname of the machine the script is running on. When
    there's no provided nodename, Elasticsearch uses the HOSTNAME to create
    a nodename. On Windows, Powershell provides a $COMPUTERNAME variable for
    the same purpose. CMD.EXE provides the same thing, except it's called
    %COMPUTERNAME%. bin/elasticsearch.bat sets $HOSTNAME to the value of
    $COMPUTERNAME. However, when testclusters invokes bin/elasticsearch.bat,
    the COMPUTERNAME variable doesn't get passed in, leaving HOSTNAME null
    and breaking an integration test on Windows.
    
    This commit sets COMPUTERNAME in the environment so that our tests get
    the value that Elasticsearch would have when bin/elasticsearch.bat is
    invoked from the shell.

--- buildSrc/src/main/java/org/elasticsearch/gradle/testclusters/ElasticsearchNode.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/testclusters/ElasticsearchNode.java
@@ -642,6 +642,8 @@ public class ElasticsearchNode implements TestClusterConfiguration {
         defaultEnv.put("ES_TMPDIR", tmpDir.toString());
         // Windows requires this as it defaults to `c:\windows` despite ES_TMPDIR
         defaultEnv.put("TMP", tmpDir.toString());
+        // Windows needs the COMPUTERNAME variable in order to set HOSTNAME
+        defaultEnv.put("COMPUTERNAME", System.getenv("COMPUTERNAME"));
 
         Set<String> commonKeys = new HashSet<>(environment.keySet());
         commonKeys.retainAll(defaultEnv.keySet());

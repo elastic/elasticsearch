e5e87e5d53be6c48a9e9c9fd13988f24e3f64339
Jason Tedor <jason@tedor.me>
2019-08-23 14:35:14 -0400

    Add deprecation check for processors
    The processors setting is deprecated. This commit adds a deprecation
    check for the use of the processors setting.

---
+++ x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/NodeDeprecationChecks.java
@@ -0,0 +1,32 @@
+package org.elasticsearch.xpack.deprecation;
+
+import org.elasticsearch.action.admin.cluster.node.info.PluginsAndModules;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.concurrent.EsExecutors;
+import org.elasticsearch.xpack.core.deprecation.DeprecationIssue;
+
+import java.util.Locale;
+
+class NodeDeprecationChecks {
+
+    static DeprecationIssue checkProcessors(final Settings settings , final PluginsAndModules pluginsAndModules) {
+        if (EsExecutors.PROCESSORS_SETTING.exists(settings) == false) {
+            return null;
+        }
+        final String message = String.format(
+            Locale.ROOT,
+            "setting [%s] is deprecated in favor of setting [%s]",
+            EsExecutors.PROCESSORS_SETTING.getKey(),
+            EsExecutors.NODE_PROCESSORS_SETTING.getKey());
+        final String url = "https://www.elastic.co/guide/en/elasticsearch/reference/master/breaking-changes-8.0.html#remove-processors";
+        final String details = String.format(
+            Locale.ROOT,
+            "the setting [%s] is currently set to [%d], instead set [%s] to [%d]",
+            EsExecutors.PROCESSORS_SETTING.getKey(),
+            EsExecutors.PROCESSORS_SETTING.get(settings),
+            EsExecutors.NODE_PROCESSORS_SETTING.getKey(),
+            EsExecutors.PROCESSORS_SETTING.get(settings));
+        return new DeprecationIssue(DeprecationIssue.Level.CRITICAL, message, url, details);
+    }
+
+}

a6c768245603ecffafb8d299798940294abdff48
Jason Tedor <jason@tedor.me>
2019-08-25 14:50:46 -0400

    Consider artifact repositories backed by S3 secure
    Since credentials are required to access such a repository, and these
    repositories are accessed over an encrypted protocol (https), this
    commit adds support to consider S3-backed artifact repositories as
    secure. Additionally, we add tests for this functionality.

--- buildSrc/src/test/java/org/elasticsearch/gradle/BuildPluginIT.java
+++ buildSrc/src/test/java/org/elasticsearch/gradle/BuildPluginIT.java
@@ -18,18 +18,28 @@
  */
 package org.elasticsearch.gradle;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.elasticsearch.gradle.test.GradleIntegrationTestCase;
 import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class BuildPluginIT extends GradleIntegrationTestCase {
+
+    @Rule
+    public TemporaryFolder tmpDir = new TemporaryFolder();
 
     public void testPluginCanBeApplied() {
         BuildResult result = getGradleRunner("elasticsearch.build")
@@ -44,6 +54,31 @@ public class BuildPluginIT extends GradleIntegrationTestCase {
             .withArguments("check", "assemble", "-s", "-Dlocal.repo.path=" + getLocalTestRepoPath())
             .build();
         assertTaskSuccessful(result, ":check");
+    }
+
+    public void testInsecure() throws IOException {
+        final File projectDir = getProjectDir("elasticsearch.build");
+        FileUtils.copyDirectory(projectDir, tmpDir.getRoot(), pathname -> pathname.getPath().contains("/build/") == false);
+        final List<String> buildGradleLines =
+            Files.readAllLines(tmpDir.getRoot().toPath().resolve("build.gradle"), StandardCharsets.UTF_8);
+        // add an insecure repository to the build.gradle
+        final String url = "http://s3.amazonaws.com/artifacts.elastic.co/maven";
+        buildGradleLines.addAll(Arrays.asList(
+            "repositories {",
+                "  maven {",
+                "    name \"elastic-maven\"",
+                "    url \"" +  url + "\"\n",
+                "  }",
+                "}"));
+        Files.write(tmpDir.getRoot().toPath().resolve("build.gradle"), buildGradleLines, StandardCharsets.UTF_8);
+        final BuildResult result = GradleRunner.create()
+            .withProjectDir(tmpDir.getRoot())
+            .withArguments("clean", "hello", "-s", "-i", "--warning-mode=all", "--scan")
+            .withPluginClasspath()
+            .buildAndFail();
+        assertOutputContains(
+            result.getOutput(),
+            "repository [elastic-maven] on project with path [:] is not using a secure protocol for artifacts on [" + url + "]");
     }
 
     public void testLicenseAndNotice() throws IOException {

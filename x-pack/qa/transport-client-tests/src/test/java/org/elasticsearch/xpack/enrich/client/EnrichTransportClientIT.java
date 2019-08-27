b0f46b8eec491c79c975e156be499ed6a2573570
Michael Basnight <mbasnight@gmail.com>
2019-08-27 08:25:45 -0500

    Fixing nits

--- x-pack/qa/transport-client-tests/src/test/java/org/elasticsearch/xpack/enrich/client/EnrichTransportClientIT.java
+++ x-pack/qa/transport-client-tests/src/test/java/org/elasticsearch/xpack/enrich/client/EnrichTransportClientIT.java
@@ -15,27 +15,26 @@ import org.elasticsearch.xpack.core.enrich.action.DeleteEnrichPolicyAction;
 import org.elasticsearch.xpack.core.enrich.action.GetEnrichPolicyAction;
 import org.elasticsearch.xpack.core.enrich.action.PutEnrichPolicyAction;
 import org.elasticsearch.xpack.core.enrich.client.EnrichClient;
-import org.hamcrest.Matchers;
 
 import java.io.IOException;
 import java.util.Collections;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.nullValue;
 
 public class EnrichTransportClientIT extends ESXPackSmokeClientTestCase {
 
     private static void assertEqualPolicies(EnrichPolicy expectedInstance, EnrichPolicy newInstance) {
-        assertThat(newInstance.getType(), Matchers.equalTo(expectedInstance.getType()));
+        assertThat(newInstance.getType(), equalTo(expectedInstance.getType()));
         if (newInstance.getQuery() != null) {
             // testFromXContent, always shuffles the xcontent and then byte wise the query is different, so we check the parsed version:
-            assertThat(newInstance.getQuery().getQueryAsMap(), Matchers.equalTo(expectedInstance.getQuery().getQueryAsMap()));
+            assertThat(newInstance.getQuery().getQueryAsMap(), equalTo(expectedInstance.getQuery().getQueryAsMap()));
         } else {
             assertThat(expectedInstance.getQuery(), nullValue());
         }
-        assertThat(newInstance.getIndices(), Matchers.equalTo(expectedInstance.getIndices()));
-        assertThat(newInstance.getMatchField(), Matchers.equalTo(expectedInstance.getMatchField()));
-        assertThat(newInstance.getEnrichFields(), Matchers.equalTo(expectedInstance.getEnrichFields()));
+        assertThat(newInstance.getIndices(), equalTo(expectedInstance.getIndices()));
+        assertThat(newInstance.getMatchField(), equalTo(expectedInstance.getMatchField()));
+        assertThat(newInstance.getEnrichFields(), equalTo(expectedInstance.getEnrichFields()));
     }
 
     public void testEnrichCrud() throws IOException {

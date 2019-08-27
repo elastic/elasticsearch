0d15eed268e0f6d6a8a9df35a7ec09ca44fa83fc
Dimitris Athanasiou <dimitris@elastic.co>
2019-08-27 13:13:42 +0300

    [7.x][ML] Add option to regression to randomize training set (#45969)
    Adds a parameter `training_percent` to regression. The default
    value is `100`. When the parameter is set to a value less than `100`,
    from the rows that can be used for training (ie. those that have a
    value for the dependent variable) we randomly choose whether to actually
    use for training. This enables splitting the data into a training set and
    the rest, usually called testing, validation or holdout set, which allows
    for validating the model on data that have not been used for training.
    
    Technically, the analytics process considers as training the data that
    have a value for the dependent variable. Thus, when we decide a training
    row is not going to be used for training, we simply clear the row's
    dependent variable.

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/ml/dataframe/analyses/RegressionTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/ml/dataframe/analyses/RegressionTests.java
@@ -33,8 +33,9 @@ public class RegressionTests extends AbstractSerializingTestCase<Regression> {
         Integer maximumNumberTrees = randomBoolean() ? null : randomIntBetween(1, 2000);
         Double featureBagFraction = randomBoolean() ? null : randomDoubleBetween(0.0, 1.0, false);
         String predictionFieldName = randomBoolean() ? null : randomAlphaOfLength(10);
+        Double trainingPercent = randomBoolean() ? null : randomDoubleBetween(0.0, 100.0, true);
         return new Regression(randomAlphaOfLength(10), lambda, gamma, eta, maximumNumberTrees, featureBagFraction,
-            predictionFieldName);
+            predictionFieldName, trainingPercent);
     }
 
     @Override
@@ -44,57 +45,83 @@ public class RegressionTests extends AbstractSerializingTestCase<Regression> {
 
     public void testRegression_GivenNegativeLambda() {
         ElasticsearchStatusException e = expectThrows(ElasticsearchStatusException.class,
-            () -> new Regression("foo", -0.00001, 0.0, 0.5, 500, 0.3, "result"));
+            () -> new Regression("foo", -0.00001, 0.0, 0.5, 500, 0.3, "result", 100.0));
 
         assertThat(e.getMessage(), equalTo("[lambda] must be a non-negative double"));
     }
 
     public void testRegression_GivenNegativeGamma() {
         ElasticsearchStatusException e = expectThrows(ElasticsearchStatusException.class,
-            () -> new Regression("foo", 0.0, -0.00001, 0.5, 500, 0.3, "result"));
+            () -> new Regression("foo", 0.0, -0.00001, 0.5, 500, 0.3, "result", 100.0));
 
         assertThat(e.getMessage(), equalTo("[gamma] must be a non-negative double"));
     }
 
     public void testRegression_GivenEtaIsZero() {
         ElasticsearchStatusException e = expectThrows(ElasticsearchStatusException.class,
-            () -> new Regression("foo", 0.0, 0.0, 0.0, 500, 0.3, "result"));
+            () -> new Regression("foo", 0.0, 0.0, 0.0, 500, 0.3, "result", 100.0));
 
         assertThat(e.getMessage(), equalTo("[eta] must be a double in [0.001, 1]"));
     }
 
     public void testRegression_GivenEtaIsGreaterThanOne() {
         ElasticsearchStatusException e = expectThrows(ElasticsearchStatusException.class,
-            () -> new Regression("foo", 0.0, 0.0, 1.00001, 500, 0.3, "result"));
+            () -> new Regression("foo", 0.0, 0.0, 1.00001, 500, 0.3, "result", 100.0));
 
         assertThat(e.getMessage(), equalTo("[eta] must be a double in [0.001, 1]"));
     }
 
     public void testRegression_GivenMaximumNumberTreesIsZero() {
         ElasticsearchStatusException e = expectThrows(ElasticsearchStatusException.class,
-            () -> new Regression("foo", 0.0, 0.0, 0.5, 0, 0.3, "result"));
+            () -> new Regression("foo", 0.0, 0.0, 0.5, 0, 0.3, "result", 100.0));
 
         assertThat(e.getMessage(), equalTo("[maximum_number_trees] must be an integer in [1, 2000]"));
     }
 
     public void testRegression_GivenMaximumNumberTreesIsGreaterThan2k() {
         ElasticsearchStatusException e = expectThrows(ElasticsearchStatusException.class,
-            () -> new Regression("foo", 0.0, 0.0, 0.5, 2001, 0.3, "result"));
+            () -> new Regression("foo", 0.0, 0.0, 0.5, 2001, 0.3, "result", 100.0));
 
         assertThat(e.getMessage(), equalTo("[maximum_number_trees] must be an integer in [1, 2000]"));
     }
 
     public void testRegression_GivenFeatureBagFractionIsLessThanZero() {
         ElasticsearchStatusException e = expectThrows(ElasticsearchStatusException.class,
-            () -> new Regression("foo", 0.0, 0.0, 0.5, 500, -0.00001, "result"));
+            () -> new Regression("foo", 0.0, 0.0, 0.5, 500, -0.00001, "result", 100.0));
 
         assertThat(e.getMessage(), equalTo("[feature_bag_fraction] must be a double in (0, 1]"));
     }
 
     public void testRegression_GivenFeatureBagFractionIsGreaterThanOne() {
         ElasticsearchStatusException e = expectThrows(ElasticsearchStatusException.class,
-            () -> new Regression("foo", 0.0, 0.0, 0.5, 500, 1.00001, "result"));
+            () -> new Regression("foo", 0.0, 0.0, 0.5, 500, 1.00001, "result", 100.0));
 
         assertThat(e.getMessage(), equalTo("[feature_bag_fraction] must be a double in (0, 1]"));
+    }
+
+    public void testRegression_GivenTrainingPercentIsNull() {
+        Regression regression = new Regression("foo", 0.0, 0.0, 0.5, 500, 1.0, "result", null);
+        assertThat(regression.getTrainingPercent(), equalTo(100.0));
+    }
+
+    public void testRegression_GivenTrainingPercentIsBoundary() {
+        Regression regression = new Regression("foo", 0.0, 0.0, 0.5, 500, 1.0, "result", 1.0);
+        assertThat(regression.getTrainingPercent(), equalTo(1.0));
+        regression = new Regression("foo", 0.0, 0.0, 0.5, 500, 1.0, "result", 100.0);
+        assertThat(regression.getTrainingPercent(), equalTo(100.0));
+    }
+
+    public void testRegression_GivenTrainingPercentIsLessThanOne() {
+        ElasticsearchStatusException e = expectThrows(ElasticsearchStatusException.class,
+            () -> new Regression("foo", 0.0, 0.0, 0.5, 500, 1.0, "result", 0.999));
+
+        assertThat(e.getMessage(), equalTo("[training_percent] must be a double in [1, 100]"));
+    }
+
+    public void testRegression_GivenTrainingPercentIsGreaterThan100() {
+        ElasticsearchStatusException e = expectThrows(ElasticsearchStatusException.class,
+            () -> new Regression("foo", 0.0, 0.0, 0.5, 500, 1.0, "result", 100.0001));
+
+        assertThat(e.getMessage(), equalTo("[training_percent] must be a double in [1, 100]"));
     }
 }

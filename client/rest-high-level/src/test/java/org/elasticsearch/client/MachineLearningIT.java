ed638b8f88ff53b4de8530fc83310611ea55bc1c
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2019-08-28 15:38:47 -0500

    Muting org.elasticsearch.client.MachineLearningIT.testEstimateMemoryUsage

--- client/rest-high-level/src/test/java/org/elasticsearch/client/MachineLearningIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/MachineLearningIT.java
@@ -1749,6 +1749,7 @@ public class MachineLearningIT extends ESRestHighLevelClientTestCase {
         highLevelClient().indices().create(new CreateIndexRequest(indexName).mapping(mapping), RequestOptions.DEFAULT);
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/45787")
     public void testEstimateMemoryUsage() throws IOException {
         String indexName = "estimate-test-index";
         createIndex(indexName, mappingForClassification());

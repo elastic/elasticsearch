da86b6147867ed722e1716c82ca8b459b6f07402
Rory Hunter <roryhunter2@gmail.com>
2019-09-03 16:32:28 +0100

    Checkstyle

--- server/src/test/java/org/elasticsearch/cluster/routing/allocation/decider/RestoreInProgressAllocationDeciderTests.java
+++ server/src/test/java/org/elasticsearch/cluster/routing/allocation/decider/RestoreInProgressAllocationDeciderTests.java
@@ -154,8 +154,8 @@ public class RestoreInProgressAllocationDeciderTests extends ESAllocationTestCas
         Decision decision = executeAllocation(clusterState, primary);
         if (shardState == RestoreInProgress.State.FAILURE) {
             assertEquals(Decision.Type.NO, decision.type());
-            assertThat(decision.getExplanation(), startsWith("shard has failed to be restored from the snapshot [_repository:_existing/_uuid] - " +
-                "manually close or delete the index " +
+            assertThat(decision.getExplanation(), startsWith("shard has failed to be restored from the snapshot " +
+                "[_repository:_existing/_uuid] - manually close or delete the index " +
                 "[test] in order to retry to restore the snapshot again or use the reroute API to force the allocation of " +
                 "an empty primary shard. Details: [restore_source[_repository/_existing], failure java.io.IOException: i/o failure"));
         } else {

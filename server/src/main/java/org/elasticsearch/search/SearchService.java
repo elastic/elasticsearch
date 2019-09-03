f7a9725f1b26e5e2777493ce01578f8ad6cdbdbc
Henning Andersen <henning.andersen@elastic.co>
2019-09-03 12:11:03 +0200

    Fix SearchService.createContext exception handling
    An exception from the DefaultSearchContext constructor could leak a
    searcher, causing future issues like shard lock obtained exceptions. The
    underlying cause of the exception in the constructor has been fixed, but
    as a safety precaution we also fix the exception handling in
    createContext.
    
    Closes #45378

--- server/src/main/java/org/elasticsearch/search/SearchService.java
+++ server/src/main/java/org/elasticsearch/search/SearchService.java
@@ -626,11 +626,12 @@ public class SearchService extends AbstractLifecycleComponent implements IndexEv
                 indexShard.shardId(), request.getClusterAlias(), OriginalIndices.NONE);
         Engine.Searcher searcher = indexShard.acquireSearcher(source);
 
-        final DefaultSearchContext searchContext = new DefaultSearchContext(idGenerator.incrementAndGet(), request, shardTarget,
-            searcher, clusterService, indexService, indexShard, bigArrays, threadPool::relativeTimeInMillis, timeout,
-            fetchPhase, clusterService.state().nodes().getMinNodeVersion());
         boolean success = false;
+        DefaultSearchContext searchContext = null;
         try {
+            searchContext = new DefaultSearchContext(idGenerator.incrementAndGet(), request, shardTarget,
+                searcher, clusterService, indexService, indexShard, bigArrays, threadPool::relativeTimeInMillis, timeout,
+                fetchPhase, clusterService.state().nodes().getMinNodeVersion());
             // we clone the query shard context here just for rewriting otherwise we
             // might end up with incorrect state since we are using now() or script services
             // during rewrite and normalized / evaluate templates etc.
@@ -641,6 +642,9 @@ public class SearchService extends AbstractLifecycleComponent implements IndexEv
         } finally {
             if (success == false) {
                 IOUtils.closeWhileHandlingException(searchContext);
+                if (searchContext == null) {
+                    IOUtils.closeWhileHandlingException(searcher);
+                }
             }
         }
         return searchContext;

a66935d7c3e17c132933a8bb6dc1e6877891bfe0
Alpar Torok <torokalpar@gmail.com>
2019-09-04 12:37:12 +0300

    move method to get current version

--- buildSrc/src/minimumRuntime/java/org/elasticsearch/gradle/Version.java
+++ buildSrc/src/minimumRuntime/java/org/elasticsearch/gradle/Version.java
@@ -51,10 +51,6 @@ public final class Version implements Comparable<Version> {
         );
     }
 
-    public static Version current() {
-        return fromString(VersionProperties.getElasticsearch());
-    }
-
     @Override
     public String toString() {
         return String.valueOf(getMajor()) + "." + String.valueOf(getMinor()) + "." + String.valueOf(getRevision());

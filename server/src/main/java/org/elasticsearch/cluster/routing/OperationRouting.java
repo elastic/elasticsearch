435b811deb228b46454ea445923498848e75f848
Jim Ferenczi <jim.ferenczi@elastic.co>
2019-09-06 09:29:27 +0200

    Add a system property to ignore awareness attributes (#46375)
    This is a follow up of #19191 for 7.x.
    This change adds a system property called "es.routing.search_ignore_awareness_attributes" that when set to true will
    effectively ignore allocation awareness attributes when routing search and get requests. This is now the default in 8.x so this
    commit adds a way to opt-in to this new behavior in a minor version of 7.x.
    
    Relates #45735
    # Conflicts:
    #	docs/reference/modules/cluster/allocation_awareness.asciidoc

--- server/src/main/java/org/elasticsearch/cluster/routing/OperationRouting.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/OperationRouting.java
@@ -46,6 +46,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.elasticsearch.common.Booleans.parseBoolean;
+
 public class OperationRouting {
     private static final Logger logger = LogManager.getLogger(OperationRouting.class);
     private static final DeprecationLogger deprecationLogger = new DeprecationLogger(logger);
@@ -58,15 +60,26 @@ public class OperationRouting {
     private boolean useAdaptiveReplicaSelection;
 
     public OperationRouting(Settings settings, ClusterSettings clusterSettings) {
-        this.awarenessAttributes = AwarenessAllocationDecider.CLUSTER_ROUTING_ALLOCATION_AWARENESS_ATTRIBUTE_SETTING.get(settings);
+        // whether to ignore awareness attributes when routing requests
+        boolean ignoreAwarenessAttr = parseBoolean(System.getProperty("es.search.ignore_awareness_attributes"), false);
+        if (ignoreAwarenessAttr == false) {
+            awarenessAttributes = AwarenessAllocationDecider.CLUSTER_ROUTING_ALLOCATION_AWARENESS_ATTRIBUTE_SETTING.get(settings);
+            clusterSettings.addSettingsUpdateConsumer(AwarenessAllocationDecider.CLUSTER_ROUTING_ALLOCATION_AWARENESS_ATTRIBUTE_SETTING,
+                this::setAwarenessAttributes);
+        } else {
+            awarenessAttributes = Collections.emptyList();
+        }
+
         this.useAdaptiveReplicaSelection = USE_ADAPTIVE_REPLICA_SELECTION_SETTING.get(settings);
-        clusterSettings.addSettingsUpdateConsumer(AwarenessAllocationDecider.CLUSTER_ROUTING_ALLOCATION_AWARENESS_ATTRIBUTE_SETTING,
-            this::setAwarenessAttributes);
         clusterSettings.addSettingsUpdateConsumer(USE_ADAPTIVE_REPLICA_SELECTION_SETTING, this::setUseAdaptiveReplicaSelection);
     }
 
     void setUseAdaptiveReplicaSelection(boolean useAdaptiveReplicaSelection) {
         this.useAdaptiveReplicaSelection = useAdaptiveReplicaSelection;
+    }
+
+    List<String> getAwarenessAttributes() {
+        return awarenessAttributes;
     }
 
     private void setAwarenessAttributes(List<String> awarenessAttributes) {

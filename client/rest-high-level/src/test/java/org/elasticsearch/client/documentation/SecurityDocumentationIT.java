503ce9adb3bccfdfc02485b939b5f4bda4e0e991
Larry Gregory <larry.gregory@elastic.co>
2019-09-06 12:57:40 -0400

    update documentation test

--- client/rest-high-level/src/test/java/org/elasticsearch/client/documentation/SecurityDocumentationIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/documentation/SecurityDocumentationIT.java
@@ -681,8 +681,8 @@ public class SecurityDocumentationIT extends ESRestHighLevelClientTestCase {
 
             List<Role> roles = response.getRoles();
             assertNotNull(response);
-            // 27 system roles plus the three we created
-            assertThat(roles.size(), equalTo(30));
+            // 28 system roles plus the three we created
+            assertThat(roles.size(), equalTo(31));
         }
 
         {

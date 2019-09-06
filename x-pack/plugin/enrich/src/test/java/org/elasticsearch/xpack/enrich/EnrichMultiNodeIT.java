155479a28eba39f8cc5fe822f0e5a69bd80ef2ee
Martijn van Groningen <martijn.v.groningen@gmail.com>
2019-09-06 20:43:05 +0200

    Add enrich stats api
    The enrich api returns enrich coordinator stats and
    information about currently executing enrich policies.
    
    The coordinator stats include per ingest node:
    * The current number of search requests in the queue.
    * The total number of outstanding remote requests that
      have been executed since node startup. Each remote
      request is likely to include multiple search requests.
      This depends on how much search requests are in the
      queue at the time when the remote request is performed.
    * The number of current outstanding remote requests.
    * The total number of search requests that `enrich`
      processors have executed since node startup.
    
    The current execution policies stats include:
    * The name of policy that is executing
    * A full blow task info object that is executing the policy.
    
    Relates to #32789

--- x-pack/plugin/enrich/src/test/java/org/elasticsearch/xpack/enrich/EnrichMultiNodeIT.java
+++ x-pack/plugin/enrich/src/test/java/org/elasticsearch/xpack/enrich/EnrichMultiNodeIT.java
@@ -16,6 +16,7 @@ import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.ingest.PutPipelineRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentType;
@@ -25,6 +26,7 @@ import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.test.ESIntegTestCase;
 import org.elasticsearch.xpack.core.enrich.EnrichPolicy;
 import org.elasticsearch.xpack.core.enrich.action.DeleteEnrichPolicyAction;
+import org.elasticsearch.xpack.core.enrich.action.EnrichStatsAction;
 import org.elasticsearch.xpack.core.enrich.action.ExecuteEnrichPolicyAction;
 import org.elasticsearch.xpack.core.enrich.action.GetEnrichPolicyAction;
 import org.elasticsearch.xpack.core.enrich.action.PutEnrichPolicyAction;
@@ -36,6 +38,7 @@ import java.util.Map;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
@@ -142,6 +145,13 @@ public class EnrichMultiNodeIT extends ESIntegTestCase {
                 assertThat(userEntry.get(field), notNullValue());
             }
         }
+
+        EnrichStatsAction.Response statsResponse =
+            client().execute(EnrichStatsAction.INSTANCE, new EnrichStatsAction.Request()).actionGet();
+        assertThat(statsResponse.getCoordinatorStats().size(), equalTo(internalCluster().size()));
+        String nodeId = internalCluster().getInstance(ClusterService.class, coordinatingNode).localNode().getId();
+        assertThat(statsResponse.getCoordinatorStats().get(nodeId).getRemoteRequestsTotal(), greaterThanOrEqualTo(1L));
+        assertThat(statsResponse.getCoordinatorStats().get(nodeId).getExecutedSearchesTotal(), equalTo((long) numDocs));
     }
 
     private static List<String> createSourceIndex(int numDocs) {

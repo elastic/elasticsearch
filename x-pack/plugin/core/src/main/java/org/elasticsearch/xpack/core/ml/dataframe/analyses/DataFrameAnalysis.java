8c79b6fbbb24da39b81f6de2d1b40eac34bd65c2
Przemyslaw Witek <przemyslaw.witek@elastic.co>
2019-09-09 10:58:48 +0200

    Add audit messages for Data Frame Analytics

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/dataframe/analyses/DataFrameAnalysis.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/dataframe/analyses/DataFrameAnalysis.java
@@ -14,6 +14,13 @@ import java.util.Map;
 public interface DataFrameAnalysis extends ToXContentObject, NamedWriteable {
 
     /**
+     * @return Name of this analysis that can be presented to the user
+     */
+    default String getUserFriendlyName() {
+        return getWriteableName().replace('_', ' ');
+    }
+
+    /**
      * @return The analysis parameters as a map
      */
     Map<String, Object> getParams();

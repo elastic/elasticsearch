9b9df1f2ce58e8ca16c2a5e264bba7e6ca48b490
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2019-09-12 17:59:50 +0200

    unused import

--- server/src/main/java/org/elasticsearch/common/time/JavaDateMathParser.java
+++ server/src/main/java/org/elasticsearch/common/time/JavaDateMathParser.java
@@ -34,7 +34,6 @@ import java.time.temporal.TemporalAccessor;
 import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.TemporalQueries;
 import java.util.Objects;
-import java.util.function.Function;
 import java.util.function.LongSupplier;
 
 /**

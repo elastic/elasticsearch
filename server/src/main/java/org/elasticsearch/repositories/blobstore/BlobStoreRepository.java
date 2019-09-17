ff9e8c622427d42a2d87b4ceb298d043ae3c4e6a
Rory Hunter <pugnascotia@users.noreply.github.com>
2019-09-17 18:29:58 +0100

    Remove ExceptionHelper.detailedMessage (#45878)
    Closes #19069. This method tries to summarize an exception in a single
    string, but the entire point of the structured exception work was to get
    real stack traces. This method just hurts, and does not help. Remove this
    method, replacing it with a mix of ` e.getMessage()`,
    `ExceptionsHelper.stackTrace`.
    
    In some cases, I rearranged some message formats so that exceptions are
    reported at the end of the message, instead of in the middle.
    
    Other changes:
    
       * Rewrite DetailedErrorsEnabledIT to parse responses. Instead of relying
         on substring matching, parse the response JSON and check fields
         explicitly. This uses Jackson, which has been added to the build
         dependencies for :qa:smoke-test-http.
       * Add a getter to `VerificationFailure` so that the `cause` field can be
         accessed and added as suppressed exceptions elsewhere.

--- server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
+++ server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
@@ -929,7 +929,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
         final long startTime = threadPool.absoluteTimeInMillis();
         final StepListener<Void> snapshotDoneListener = new StepListener<>();
         snapshotDoneListener.whenComplete(listener::onResponse, e -> {
-            snapshotStatus.moveToFailed(threadPool.absoluteTimeInMillis(), ExceptionsHelper.detailedMessage(e));
+            snapshotStatus.moveToFailed(threadPool.absoluteTimeInMillis(), ExceptionsHelper.stackTrace(e));
             listener.onFailure(e instanceof IndexShardSnapshotFailedException ? (IndexShardSnapshotFailedException) e
                 : new IndexShardSnapshotFailedException(store.shardId(), e));
         });

b0942e9a5bd517865197de1021e35d72ae654024
Tim Brooks <tim@uncontended.net>
2019-09-23 20:27:53 -0600

    WIP

---
+++ server/src/main/java/org/elasticsearch/transport/ProxyConnectionStrategy.java
@@ -0,0 +1,133 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.transport;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.ParameterizedMessage;
+import org.apache.lucene.store.AlreadyClosedException;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.support.ContextPreservingActionListener;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.threadpool.ThreadPool;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ProxyConnectionStrategy implements TransportConnectionListener {
+
+    private static final Logger logger = LogManager.getLogger(ProxyConnectionStrategy.class);
+
+    private static final int MAX_LISTENERS = 100;
+    private final AtomicBoolean closed = new AtomicBoolean(false);
+    private final Object mutex = new Object();
+    private final ConnectionManager connectionManager;
+    private final ThreadPool threadPool;
+    private final int maxNumRemoteConnections;
+    private List<ActionListener<Void>> listeners = new ArrayList<>();
+
+    ProxyConnectionStrategy(ConnectionManager connectionManager, ThreadPool threadPool, int maxNumRemoteConnections) {
+        this.connectionManager = connectionManager;
+        this.threadPool = threadPool;
+        this.maxNumRemoteConnections = maxNumRemoteConnections;
+    }
+
+    /**
+     * Triggers a connect round unless there is one running already. If there is a connect round running, the listener will either
+     * be queued or rejected and failed.
+     */
+    void connect(ActionListener<Void> connectListener) {
+        boolean runConnect = false;
+        final ActionListener<Void> listener =
+            ContextPreservingActionListener.wrapPreservingContext(connectListener, threadPool.getThreadContext());
+        boolean closed;
+        synchronized (mutex) {
+            closed = this.closed.get();
+            if (closed) {
+                assert listeners.isEmpty();
+            } else {
+                if (listeners.size() >= MAX_LISTENERS) {
+                    assert listeners.size() == MAX_LISTENERS;
+                    listener.onFailure(new RejectedExecutionException("connect queue is full"));
+                    return;
+                } else {
+                    listeners.add(listener);
+                }
+                runConnect = listeners.size() == 1;
+            }
+        }
+        if (closed) {
+            connectListener.onFailure(new AlreadyClosedException("connect handler is already closed"));
+            return;
+        }
+        if (runConnect) {
+            ExecutorService executor = threadPool.executor(ThreadPool.Names.MANAGEMENT);
+//            executor.submit(new AbstractRunnable() {
+//                @Override
+//                public void onFailure(Exception e) {
+//                    ActionListener.onFailure(getAndClearListeners(), e);
+//                }
+//
+//                @Override
+//                protected void doRun() {
+//                    collectRemoteNodes(seedNodes.stream().map(Tuple::v2).iterator(),
+//                        new ActionListener<>() {
+//                            @Override
+//                            public void onResponse(Void aVoid) {
+//                                ActionListener.onResponse(getAndClearListeners(), aVoid);
+//                            }
+//
+//                            @Override
+//                            public void onFailure(Exception e) {
+//                                ActionListener.onFailure(getAndClearListeners(), e);
+//                            }
+//                        });
+//                }
+//            });
+        }
+    }
+
+    @Override
+    public void onNodeDisconnected(DiscoveryNode node) {
+        if (connectionManager.size() < maxNumRemoteConnections) {
+            // try to reconnect and fill up the slot of the disconnected node
+            connect(ActionListener.wrap(
+                ignore -> logger.trace("successfully connected after disconnect of {}", node),
+                e -> logger.trace(() -> new ParameterizedMessage("failed to connect after disconnect of {}", node), e)));
+        }
+    }
+
+    private List<ActionListener<Void>> getAndClearListeners() {
+        final List<ActionListener<Void>> result;
+        synchronized (mutex) {
+            if (listeners.isEmpty()) {
+                result = Collections.emptyList();
+            } else {
+                result = listeners;
+                listeners = new ArrayList<>();
+            }
+        }
+        return result;
+    }
+}

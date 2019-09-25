9df6cbef9e4516c8a07f8c559652c51c49605def
Nhat Nguyen <nhat.nguyen@elastic.co>
2019-09-25 08:25:14 -0400

    Remove isRecovering method from Engine (#47039)
    We already prevent flushing in Engine if it's recovering. Hence, we can
    remove the protection in IndexShard.

--- server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
+++ server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -2457,7 +2457,7 @@ public class InternalEngine extends Engine {
         }
     }
 
-    private void ensureCanFlush() {
+    final void ensureCanFlush() {
         // translog recover happens after the engine is fully constructed
         // if we are in this stage we have to prevent flushes from this
         // engine otherwise we might loose documents if the flush succeeds
@@ -2647,11 +2647,6 @@ public class InternalEngine extends Engine {
         } else {
             return translog.acquireRetentionLock();
         }
-    }
-
-    @Override
-    public boolean isRecovering() {
-        return pendingTranslogRecovery.get();
     }
 
     /**

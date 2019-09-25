27520cac3ba8360d5d40a44bcd0a5b4074f7ef53
Andrei Dan <andrei.dan@elastic.co>
2019-09-25 21:44:16 +0100

    ILM: parse origination date from index name (#46755) (#47124)
    * ILM: parse origination date from index name (#46755)
    
    Introduce the `index.lifecycle.parse_origination_date` setting that
    indicates if the origination date should be parsed from the index name.
    If set to true an index which doesn't match the expected format (namely
    `indexName-{dateFormat}-optional_digits` will fail before being created.
    The origination date will be parsed when initialising a lifecycle for an
    index and it will be set as the `index.lifecycle.origination_date` for
    that index.
    
    A user set value for `index.lifecycle.origination_date` will always
    override a possible parsable date from the index name.
    
    (cherry picked from commit c363d27f0210733dad0c307d54fa224a92ddb569)
    Signed-off-by: Andrei Dan <andrei.dan@elastic.co>
    
    * Drop usage of Map.of to be java 8 compliant

--- x-pack/plugin/ilm/src/main/java/org/elasticsearch/xpack/ilm/IndexLifecycle.java
+++ x-pack/plugin/ilm/src/main/java/org/elasticsearch/xpack/ilm/IndexLifecycle.java
@@ -28,6 +28,7 @@ import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.core.internal.io.IOUtils;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.env.NodeEnvironment;
+import org.elasticsearch.index.IndexModule;
 import org.elasticsearch.plugins.ActionPlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.rest.RestController;
@@ -155,6 +156,7 @@ public class IndexLifecycle extends Plugin implements ActionPlugin {
             LifecycleSettings.LIFECYCLE_POLL_INTERVAL_SETTING,
             LifecycleSettings.LIFECYCLE_NAME_SETTING,
             LifecycleSettings.LIFECYCLE_ORIGINATION_DATE_SETTING,
+            LifecycleSettings.LIFECYCLE_PARSE_ORIGINATION_DATE_SETTING,
             LifecycleSettings.LIFECYCLE_INDEXING_COMPLETE_SETTING,
             RolloverAction.LIFECYCLE_ROLLOVER_ALIAS_SETTING,
             LifecycleSettings.SLM_HISTORY_INDEX_ENABLED_SETTING,
@@ -278,6 +280,14 @@ public class IndexLifecycle extends Plugin implements ActionPlugin {
             ));
         }
         return actions;
+    }
+
+    @Override
+    public void onIndexModule(IndexModule indexModule) {
+        if (ilmEnabled) {
+            assert indexLifecycleInitialisationService.get() != null;
+            indexModule.addIndexEventListener(indexLifecycleInitialisationService.get());
+        }
     }
 
     @Override

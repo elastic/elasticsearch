811a14889f90b6cded8a6781f77de21359b50bad
Ioannis Kakavas <ioannis@elastic.co>
2019-09-25 12:07:12 +0300

    Handle RelayState in preparing a SAMLAuthN Request (#46534)
    This change allows for the caller of the `saml/prepare` API to pass
    a `relay_state` parameter that will then be part of the redirect
    URL in the response as the `RelayState` query parameter.
    
    The SAML IdP is required to reflect back the value of that relay
    state when sending a SAML Response. The caller of the APIs can
    then, when receiving the SAML Response, read and consume the value
    as it see fit.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/saml/SamlPrepareAuthenticationRequest.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/saml/SamlPrepareAuthenticationRequest.java
@@ -5,6 +5,7 @@
  */
 package org.elasticsearch.xpack.core.security.action.saml;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionRequestValidationException;
 import org.elasticsearch.common.Nullable;
@@ -24,10 +25,16 @@ public final class SamlPrepareAuthenticationRequest extends ActionRequest {
     @Nullable
     private String assertionConsumerServiceURL;
 
+    @Nullable
+    private String relayState;
+
     public SamlPrepareAuthenticationRequest(StreamInput in) throws IOException {
         super(in);
         realmName = in.readOptionalString();
         assertionConsumerServiceURL = in.readOptionalString();
+        if (in.getVersion().onOrAfter(Version.V_7_5_0)) {
+            relayState = in.readOptionalString();
+        }
     }
 
     public SamlPrepareAuthenticationRequest() {
@@ -54,11 +61,20 @@ public final class SamlPrepareAuthenticationRequest extends ActionRequest {
         this.assertionConsumerServiceURL = assertionConsumerServiceURL;
     }
 
+    public String getRelayState() {
+        return relayState;
+    }
+
+    public void setRelayState(String relayState) {
+        this.relayState = relayState;
+    }
+
     @Override
     public String toString() {
         return getClass().getSimpleName() + "{" +
                 "realmName=" + realmName +
                 ", assertionConsumerServiceURL=" + assertionConsumerServiceURL +
+                ", relayState=" + relayState +
                 '}';
     }
 
@@ -67,5 +83,8 @@ public final class SamlPrepareAuthenticationRequest extends ActionRequest {
         super.writeTo(out);
         out.writeOptionalString(realmName);
         out.writeOptionalString(assertionConsumerServiceURL);
+        if (out.getVersion().onOrAfter(Version.V_7_5_0)) {
+            out.writeOptionalString(relayState);
+        }
     }
 }

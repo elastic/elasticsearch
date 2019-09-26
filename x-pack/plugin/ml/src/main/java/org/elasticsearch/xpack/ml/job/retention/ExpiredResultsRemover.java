81dd9c5ced3927e51ff605f7ed4486d809b562e7
David Roberts <dave.roberts@elastic.co>
2019-09-26 17:18:38 +0100

    [ML] Throttle the delete-by-query of expired results
    Due to #47003 many clusters will have built up a
    large backlog of expired results. On upgrading to
    a version where that bug is fixed users could find
    that the first ML daily maintenance task deletes
    a very large amount of documents.
    
    This change introduces throttling to the
    delete-by-query that the ML daily maintenance uses
    to delete expired results:
    
    - Average 200 documents per second
    - Maximum of 10 million documents per day
    
    (There is no throttling for state/forecast documents
    as these are expected to be lower volume.)
    
    Relates #47103

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/job/retention/ExpiredResultsRemover.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/job/retention/ExpiredResultsRemover.java
@@ -88,6 +88,14 @@ public class ExpiredResultsRemover extends AbstractExpiredJobDataRemover {
         DeleteByQueryRequest request = new DeleteByQueryRequest();
         request.setSlices(AbstractBulkByScrollRequest.AUTO_SLICES);
 
+        // Delete the documents gradually.
+        // With DEFAULT_SCROLL_SIZE = 1000 this implies we spread deletion of 1 million documents over 5000 seconds ~= 83 minutes.
+        // And we delete a maximum of 10000 batches per day (= 10 million documents per day if DEFAULT_SCROLL_SIZE = 1000).
+        // If more documents than this have expired then some will be deleted on a subsequent day.
+        request.setBatchSize(AbstractBulkByScrollRequest.DEFAULT_SCROLL_SIZE);
+        request.setRequestsPerSecond(AbstractBulkByScrollRequest.DEFAULT_SCROLL_SIZE / 5);
+        request.setMaxDocs(10000 * AbstractBulkByScrollRequest.DEFAULT_SCROLL_SIZE);
+
         request.indices(AnomalyDetectorsIndex.jobResultsAliasedName(job.getId()));
         QueryBuilder excludeFilter = QueryBuilders.termsQuery(Result.RESULT_TYPE.getPreferredName(),
                 ModelSizeStats.RESULT_TYPE_VALUE, ForecastRequestStats.RESULT_TYPE_VALUE, Forecast.RESULT_TYPE_VALUE);

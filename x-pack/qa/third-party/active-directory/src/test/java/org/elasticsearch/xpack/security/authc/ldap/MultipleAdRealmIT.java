5cdf867ab3a3cec57ef59fb696375ad6c3e01a60
Ioannis Kakavas <ioannis@elastic.co>
2019-10-01 13:30:30 +0300

    Revert "Fix Active Directory tests (#47266)"
    This reverts commit b917b57dfe45148917232fe13dd2b6c791f34c3f.

--- x-pack/qa/third-party/active-directory/src/test/java/org/elasticsearch/xpack/security/authc/ldap/MultipleAdRealmIT.java
+++ x-pack/qa/third-party/active-directory/src/test/java/org/elasticsearch/xpack/security/authc/ldap/MultipleAdRealmIT.java
@@ -49,7 +49,7 @@ public class MultipleAdRealmIT extends AbstractAdLdapRealmTestCase {
         final Settings secondarySettings = super.buildRealmSettings(secondaryRealmConfig, secondaryRoleMappings,
             getNodeTrustedCertificates());
         secondarySettings.keySet().forEach(name -> {
-            String newName = name.replace(XPACK_SECURITY_AUTHC_REALMS_AD_EXTERNAL, XPACK_SECURITY_AUTHC_REALMS_AD_EXTERNAL + "2");
+            String newName = name.replace(XPACK_SECURITY_AUTHC_REALMS_EXTERNAL, XPACK_SECURITY_AUTHC_REALMS_EXTERNAL + "2");
             builder.copy(newName, name, secondarySettings);
         });
 
@@ -62,6 +62,7 @@ public class MultipleAdRealmIT extends AbstractAdLdapRealmTestCase {
      * Because one realm is using "common name" (cn) for login, and the other uses the "userid" (sAMAccountName) [see
      * {@link #setupSecondaryRealm()}], this is simply a matter of checking that we can authenticate with both identifiers.
      */
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/35738")
     public void testCanAuthenticateAgainstBothRealms() throws IOException {
         assertAccessAllowed("Natasha Romanoff", "avengers");
         assertAccessAllowed("blackwidow", "avengers");

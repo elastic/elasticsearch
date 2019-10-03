c5b39fd33ec03153ffa4570bc40ac0c9f7584fc3
Nhat Nguyen <nhat.nguyen@elastic.co>
2019-10-03 10:34:15 -0400

    Limit number of retaining translog files for peer recovery (#47414)
    Today we control the extra translog (when soft-deletes is disabled) for
    peer recoveries by size and age. If users manually (force) flush many
    times within a short period, we can keep many small (or empty) translog
    files as neither the size or age condition is reached. We can protect
    the cluster from running out of the file descriptors in such a situation
    by limiting the number of retaining translog files.

--- server/src/main/java/org/elasticsearch/index/engine/ReadOnlyEngine.java
+++ server/src/main/java/org/elasticsearch/index/engine/ReadOnlyEngine.java
@@ -227,7 +227,8 @@ public class ReadOnlyEngine extends Engine {
         final TranslogConfig translogConfig = config.getTranslogConfig();
         final TranslogDeletionPolicy translogDeletionPolicy = new TranslogDeletionPolicy(
             config.getIndexSettings().getTranslogRetentionSize().getBytes(),
-            config.getIndexSettings().getTranslogRetentionAge().getMillis()
+            config.getIndexSettings().getTranslogRetentionAge().getMillis(),
+            config.getIndexSettings().getTranslogRetentionTotalFiles()
         );
         translogDeletionPolicy.setTranslogGenerationOfLastCommit(translogGenOfLastCommit);
 

d064b153a02c0446cc6b304793b135e1f22dda1c
Mark Vieira <portugee@gmail.com>
2019-10-03 15:35:05 -0700

    Remove unused import
    Signed-off-by: Mark Vieira <portugee@gmail.com>

--- buildSrc/src/main/java/org/elasticsearch/gradle/tar/SymbolicLinkPreservingUntarTask.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/tar/SymbolicLinkPreservingUntarTask.java
@@ -27,7 +27,6 @@ import org.gradle.api.GradleException;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.model.ObjectFactory;
-import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.OutputDirectory;

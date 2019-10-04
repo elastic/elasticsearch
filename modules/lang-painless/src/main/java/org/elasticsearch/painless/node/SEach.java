2a527dbba4a89633b83a281ecf41ff247cdacaa2
Jack Conradson <osjdconrad@gmail.com>
2019-10-04 08:36:44 -0700

    Add a ScriptRoot to consolidate global data necessary for multiple passes (#47532)
    This PR is to get plumbing in for a ScriptRoot class that will consolidate 
    several pieces of state required by potentially multiple passes including 
    PainlessLookup, CompilerSettings, FunctionTable, the root class node, and a 
    synthetic counter. It's possible more may be added to this as we move 
    forward and slowly make the the nodes have less mutable state.

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/node/SEach.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/node/SEach.java
@@ -28,7 +28,7 @@ import org.elasticsearch.painless.Location;
 import org.elasticsearch.painless.MethodWriter;
 import org.elasticsearch.painless.lookup.PainlessLookupUtility;
 import org.elasticsearch.painless.lookup.def;
-import org.elasticsearch.painless.symbol.FunctionTable;
+import org.elasticsearch.painless.ScriptRoot;
 
 import java.util.Objects;
 import java.util.Set;
@@ -75,12 +75,12 @@ public class SEach extends AStatement {
     }
 
     @Override
-    void analyze(FunctionTable functions, Locals locals) {
-        expression.analyze(functions, locals);
+    void analyze(ScriptRoot scriptRoot, Locals locals) {
+        expression.analyze(scriptRoot, locals);
         expression.expected = expression.actual;
-        expression = expression.cast(functions, locals);
+        expression = expression.cast(scriptRoot, locals);
 
-        Class<?> clazz = locals.getPainlessLookup().canonicalTypeNameToType(this.type);
+        Class<?> clazz = scriptRoot.getPainlessLookup().canonicalTypeNameToType(this.type);
 
         if (clazz == null) {
             throw createError(new IllegalArgumentException("Not a type [" + this.type + "]."));
@@ -98,7 +98,7 @@ public class SEach extends AStatement {
                     "[" + PainlessLookupUtility.typeToCanonicalTypeName(expression.actual) + "]."));
         }
 
-        sub.analyze(functions, locals);
+        sub.analyze(scriptRoot, locals);
 
         if (block == null) {
             throw createError(new IllegalArgumentException("Extraneous for each loop."));
@@ -106,7 +106,7 @@ public class SEach extends AStatement {
 
         block.beginLoop = true;
         block.inLoop = true;
-        block.analyze(functions, locals);
+        block.analyze(scriptRoot, locals);
         block.statementCount = Math.max(1, block.statementCount);
 
         if (block.loopEscape && !block.anyContinue) {

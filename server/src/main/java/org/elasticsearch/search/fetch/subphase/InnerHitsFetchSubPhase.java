0ea1067718fd52ba1b78545c067b5f6ac2a5f823
jimczi <jim.ferenczi@elastic.co>
2019-10-08 21:35:55 +0200

    copy the concrete map too

--- server/src/main/java/org/elasticsearch/search/fetch/subphase/InnerHitsFetchSubPhase.java
+++ server/src/main/java/org/elasticsearch/search/fetch/subphase/InnerHitsFetchSubPhase.java
@@ -74,6 +74,9 @@ public final class InnerHitsFetchSubPhase implements FetchSubPhase {
                 innerHits.docIdsToLoad(docIdsToLoad, 0, docIdsToLoad.length);
                 innerHits.setUid(new Uid(MapperService.SINGLE_MAPPING_NAME, hit.getId()));
                 innerHits.lookup().source().setSource(context.lookup().source().internalSourceRef());
+                if (context.lookup().source().source() != null) {
+                    innerHits.lookup().source().setSource(context.lookup().source().source());
+                }
 
                 fetchPhase.execute(innerHits);
                 FetchSearchResult fetchResult = innerHits.fetchResult();

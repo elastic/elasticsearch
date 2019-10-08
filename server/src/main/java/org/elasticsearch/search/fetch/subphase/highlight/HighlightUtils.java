6912aa52761daff9a3d774e5ed90e9f606b3e05c
jimczi <jim.ferenczi@elastic.co>
2019-10-08 15:43:38 +0200

    Remove the SearchContext from the highlighter context
    Today built-in highlighter and plugins have access to the SearchContext through the
    highlighter context. However most of the information exposed in the SearchContext are not needed and a QueryShardContext
    would be enough to perform highlighting. This change replaces the SearchContext by the informations that are absolutely
    required by highlighter: a QueryShardContext and the SearchContextHighlight. This change allows to reduce the exposure of the
    complex SearchContext and remove the needs to clone it in the percolator sub phase.
    
    Relates #47198
    Relates #46523

--- server/src/main/java/org/elasticsearch/search/fetch/subphase/highlight/HighlightUtils.java
+++ server/src/main/java/org/elasticsearch/search/fetch/subphase/highlight/HighlightUtils.java
@@ -23,8 +23,8 @@ import org.apache.lucene.search.highlight.Encoder;
 import org.apache.lucene.search.highlight.SimpleHTMLEncoder;
 import org.elasticsearch.index.fieldvisitor.CustomFieldsVisitor;
 import org.elasticsearch.index.mapper.MappedFieldType;
+import org.elasticsearch.index.query.QueryShardContext;
 import org.elasticsearch.search.fetch.FetchSubPhase;
-import org.elasticsearch.search.internal.SearchContext;
 import org.elasticsearch.search.lookup.SourceLookup;
 
 import java.io.IOException;
@@ -48,10 +48,10 @@ public final class HighlightUtils {
      */
     public static List<Object> loadFieldValues(SearchContextHighlight.Field field,
                                                MappedFieldType fieldType,
-                                               SearchContext searchContext,
-                                               FetchSubPhase.HitContext hitContext) throws IOException {
+                                               QueryShardContext context,
+                                               FetchSubPhase.HitContext hitContext,
+                                               boolean forceSource) throws IOException {
         //percolator needs to always load from source, thus it sets the global force source to true
-        boolean forceSource = searchContext.highlight().forceSource(field);
         List<Object> textsToHighlight;
         if (!forceSource && fieldType.stored()) {
             CustomFieldsVisitor fieldVisitor = new CustomFieldsVisitor(singleton(fieldType.name()), false);
@@ -62,7 +62,7 @@ public final class HighlightUtils {
                 textsToHighlight = Collections.emptyList();
             }
         } else {
-            SourceLookup sourceLookup = searchContext.lookup().source();
+            SourceLookup sourceLookup = context.lookup().source();
             sourceLookup.setSegmentAndDocument(hitContext.readerContext(), hitContext.docId());
             textsToHighlight = sourceLookup.extractRawValues(fieldType.name());
         }

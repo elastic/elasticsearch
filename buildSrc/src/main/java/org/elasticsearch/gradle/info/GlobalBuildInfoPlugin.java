14b979a7bcf814fca422414050b4f6e79da23b18
Ryan Ernst <ryan@elastic.co>
2019-10-09 12:00:59 -0700

    Make internal project flag overrideable by tests (#47757)
    In order to work with external elasticsearch plugins, some parts of
    build-tools need to know when the current build is part of the
    elasticsearch project or an external build. We identify these "internal"
    builds by a marker in our buildSrc classpath. However, some build-tools
    integ tests need to override this flag to test both the external and
    internal behavior.
    
    This commit moves the storage of the flag indicating whether we are
    running in an internal build to global build info. This will allow
    testkit projects to override the flag.

--- buildSrc/src/main/java/org/elasticsearch/gradle/info/GlobalBuildInfoPlugin.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/info/GlobalBuildInfoPlugin.java
@@ -108,6 +108,7 @@ public class GlobalBuildInfoPlugin implements Plugin<Project> {
             ext.set("buildDate", ZonedDateTime.now(ZoneOffset.UTC));
             ext.set("testSeed", testSeed);
             ext.set("isCi", System.getenv("JENKINS_URL") != null);
+            ext.set("isInternal", GlobalBuildInfoPlugin.class.getResource("/buildSrc.marker") != null);
         });
     }
 

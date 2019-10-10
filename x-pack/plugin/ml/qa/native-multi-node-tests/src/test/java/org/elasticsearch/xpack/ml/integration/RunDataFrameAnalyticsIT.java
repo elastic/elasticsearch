f966fa1188ecd02a9917ca8bf72d33a1f2191207
Igor Motov <igor@motovs.org>
2019-10-10 17:49:58 +0400

    Mute RunDataFrameAnalyticsIT.testOutlierDetectionStopAndRestart
    Tracked by #47612

--- x-pack/plugin/ml/qa/native-multi-node-tests/src/test/java/org/elasticsearch/xpack/ml/integration/RunDataFrameAnalyticsIT.java
+++ x-pack/plugin/ml/qa/native-multi-node-tests/src/test/java/org/elasticsearch/xpack/ml/integration/RunDataFrameAnalyticsIT.java
@@ -27,6 +27,7 @@ import org.elasticsearch.xpack.core.ml.dataframe.DataFrameAnalyticsSource;
 import org.elasticsearch.xpack.core.ml.dataframe.DataFrameAnalyticsState;
 import org.elasticsearch.xpack.core.ml.dataframe.analyses.OutlierDetection;
 import org.junit.After;
+import org.junit.Ignore;
 
 import java.util.Map;
 
@@ -485,6 +486,7 @@ public class RunDataFrameAnalyticsIT extends MlNativeDataFrameAnalyticsIntegTest
             "Estimated memory usage for this analytics to be");
     }
 
+    @Ignore("https://github.com/elastic/elasticsearch/issues/47612")
     public void testOutlierDetectionStopAndRestart() throws Exception {
         String sourceIndex = "test-outlier-detection-stop-and-restart";
 

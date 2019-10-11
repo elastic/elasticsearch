ad2b1de63c8210560a9546e4862cc42cb954bddd
Alan Woodward <romseygeek@apache.org>
2019-10-11 11:29:55 +0100

    Upgrade to lucene 7.7.2

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -147,7 +147,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_6_8_1 = new Version(6080199, org.apache.lucene.util.Version.LUCENE_7_7_0);
     public static final Version V_6_8_2 = new Version(6080299, org.apache.lucene.util.Version.LUCENE_7_7_0);
     public static final Version V_6_8_3 = new Version(6080399, org.apache.lucene.util.Version.LUCENE_7_7_0);
-    public static final Version V_6_8_4 = new Version(6080499, org.apache.lucene.util.Version.LUCENE_7_7_0);
+    public static final Version V_6_8_4 = new Version(6080499, org.apache.lucene.util.Version.LUCENE_7_7_2);
 
     public static final Version CURRENT = V_6_8_4;
 

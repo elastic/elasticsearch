c31c88e0bf659230d6d1aaf3ec2960d5f369c0e0
William Brafford <william.brafford@elastic.co>
2019-10-16 17:20:53 -0400

    Checkstyle - remove unused import

--- qa/os/src/test/java/org/elasticsearch/packaging/util/Archives.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/util/Archives.java
@@ -26,7 +26,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.joining;

bdc269e2cf3b8eb21061303e36d1b76230b67bf6
Tim Brooks <tim@uncontended.net>
2019-10-17 17:34:03 -0600

    Checkstyle

--- server/src/main/java/org/elasticsearch/transport/RemoteClusterService.java
+++ server/src/main/java/org/elasticsearch/transport/RemoteClusterService.java
@@ -21,7 +21,6 @@ package org.elasticsearch.transport;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.OriginalIndices;

f155d885ccb5e722045679dffb08e96667418361
Mark Vieira <portugee@gmail.com>
2019-10-21 09:44:41 -0700

    Add 'javadoc' task to lifecycle check tasks (#48214)

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
@@ -676,6 +676,10 @@ class BuildPlugin implements Plugin<Project> {
              */
             (javadoc.options as CoreJavadocOptions).addBooleanOption('html5', true)
         }
+        // ensure javadoc task is run with 'check'
+        project.pluginManager.withPlugin('lifecycle-base') {
+            project.tasks.getByName(LifecycleBasePlugin.CHECK_TASK_NAME).dependsOn(project.tasks.withType(Javadoc))
+        }
         configureJavadocJar(project)
     }
 

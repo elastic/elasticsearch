fc30e05e9721cbed06133327e33fef107327617c
Tim Brooks <tim@uncontended.net>
2019-10-21 10:37:03 -0600

    Remove option to enable direct buffer pooling (#47956)
    This commit removes the option to change the netty system properties to
    reenable the direct buffer pooling. It also removes the need for us to
    disable the buffer pooling in the system properties file. Instead, we
    programmatically craete an allocator that is used by our networking
    layer.
    
    This commit does introduce an Elasticsearch property which allows the
    user to fallback on the netty default allocator. If they choose this
    option, they can configure the default allocator how they wish using the
    standard netty properties.

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
@@ -889,7 +889,6 @@ class BuildPlugin implements Plugin<Project> {
                 test.systemProperty('io.netty.noUnsafe', 'true')
                 test.systemProperty('io.netty.noKeySetOptimization', 'true')
                 test.systemProperty('io.netty.recycler.maxCapacityPerThread', '0')
-                test.systemProperty('io.netty.allocator.numDirectArenas', '0')
 
                 test.testLogging { TestLoggingContainer logging ->
                     logging.showExceptions = true

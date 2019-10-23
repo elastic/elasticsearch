55b5d048b944b04176a2278dec78c377419078c4
Michael Basnight <mbasnight@gmail.com>
2019-10-23 12:24:23 -0500

    Add slices to delete and update by query in HLRC
    The slices param was missing from both delete by query and update by
    query in the HLRC request converters. This commit fixes the omission.

--- client/rest-high-level/src/main/java/org/elasticsearch/client/RequestConverters.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/RequestConverters.java
@@ -567,7 +567,8 @@ final class RequestConverters {
             .withWaitForActiveShards(deleteByQueryRequest.getWaitForActiveShards())
             .withRequestsPerSecond(deleteByQueryRequest.getRequestsPerSecond())
             .withIndicesOptions(deleteByQueryRequest.indicesOptions())
-            .withWaitForCompletion(waitForCompletion);
+            .withWaitForCompletion(waitForCompletion)
+            .withSlices(deleteByQueryRequest.getSlices());
         if (deleteByQueryRequest.isAbortOnVersionConflict() == false) {
             params.putParam("conflicts", "proceed");
         }
@@ -595,7 +596,8 @@ final class RequestConverters {
             .withTimeout(updateByQueryRequest.getTimeout())
             .withWaitForActiveShards(updateByQueryRequest.getWaitForActiveShards())
             .withRequestsPerSecond(updateByQueryRequest.getRequestsPerSecond())
-            .withIndicesOptions(updateByQueryRequest.indicesOptions());
+            .withIndicesOptions(updateByQueryRequest.indicesOptions())
+            .withSlices(updateByQueryRequest.getSlices());
         if (updateByQueryRequest.isAbortOnVersionConflict() == false) {
             params.putParam("conflicts", "proceed");
         }

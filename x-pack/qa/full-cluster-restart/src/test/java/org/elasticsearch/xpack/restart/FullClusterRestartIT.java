28bfa6e138a2e6d20edf1331613926b659b3dffa
Jake Landis <jake.landis@elastic.co>
2019-10-24 08:50:23 -0500

    Allow more time for restart tests to reach yellow state. (#48434)
    The testWatcher method will on occasion timeout waiting for
    a yellow cluster state. This change increases the timeout
    to 60s.

--- x-pack/qa/full-cluster-restart/src/test/java/org/elasticsearch/xpack/restart/FullClusterRestartIT.java
+++ x-pack/qa/full-cluster-restart/src/test/java/org/elasticsearch/xpack/restart/FullClusterRestartIT.java
@@ -404,7 +404,7 @@ public class FullClusterRestartIT extends AbstractFullClusterRestartTestCase {
     private void waitForYellow(String indexName) throws IOException {
         Request request = new Request("GET", "/_cluster/health/" + indexName);
         request.addParameter("wait_for_status", "yellow");
-        request.addParameter("timeout", "30s");
+        request.addParameter("timeout", "60s");
         request.addParameter("wait_for_no_relocating_shards", "true");
         request.addParameter("wait_for_no_initializing_shards", "true");
         Map<String, Object> response = entityAsMap(client().performRequest(request));

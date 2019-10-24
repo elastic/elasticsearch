42f66ac61bbbf14fbdfca6d568663bb8e68ebe72
Armin Braun <me@obrown.io>
2019-10-24 13:11:33 +0200

    Handle Concurrent Repo Modification to Fix Test (#48433)
    Just like #48329 (and using the changes) in that PR
    we can run into a concurrent repo modification that we
    will throw on and must retry until consistent handling of
    this situation is implemented.
    
    Closes #47834

--- x-pack/plugin/ilm/src/test/java/org/elasticsearch/xpack/slm/SLMSnapshotBlockingIntegTests.java
+++ x-pack/plugin/ilm/src/test/java/org/elasticsearch/xpack/slm/SLMSnapshotBlockingIntegTests.java
@@ -217,6 +217,10 @@ public class SLMSnapshotBlockingIntegTests extends ESIntegTestCase {
                     SnapshotsStatusResponse s =
                         client().admin().cluster().prepareSnapshotStatus(REPO).setSnapshots(completedSnapshotName).get();
                     assertNull("expected no snapshot but one was returned", s.getSnapshots().get(0));
+                } catch (RepositoryException e) {
+                    // Concurrent status calls and write operations may lead to failures in determining the current repository generation
+                    // TODO: Remove this hack once tracking the current repository generation has been made consistent
+                    throw new AssertionError(e);
                 } catch (SnapshotMissingException e) {
                     // Great, we wanted it to be deleted!
                 }

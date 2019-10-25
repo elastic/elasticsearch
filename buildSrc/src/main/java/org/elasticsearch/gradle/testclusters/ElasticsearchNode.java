4b48f7fb51ebeab6afbf2b08b34ac6f683d6981b
Ioannis Kakavas <ioannis@elastic.co>
2019-10-25 12:12:27 +0300

    unused import

--- buildSrc/src/main/java/org/elasticsearch/gradle/testclusters/ElasticsearchNode.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/testclusters/ElasticsearchNode.java
@@ -70,7 +70,6 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;

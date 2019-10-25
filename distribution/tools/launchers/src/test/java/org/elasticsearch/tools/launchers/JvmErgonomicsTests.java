ddf820a3a1cef4eb6136fa85e8e5b00c681240b0
William Brafford <william.brafford@elastic.co>
2019-10-25 13:27:43 -0400

    Warn when MaxDirectMemorySize may be incorrect (Windows/JDK8 only issue) (#48365)
    Our JVM ergonomics extract max heap size from JDK PrintFlagsFinal output.
    On JDK 8, there is a system-dependent bug where memory sizes are cast to
    32-bit integers. On affected systems (namely, Windows), when 1/4 of physical
    memory is more than the maximum integer value, the output of PrintFlagsFinal
    will be inaccurate. In the pathological case, where the max heap size would
    be a multiple of 4g, the test will fail.
    
    The practical effect of this bug, beyond test failures, is that we may set
    MaxDirectMemorySize to an incorrect value on Windows. This commit adds a
    warning about this situation during startup.

--- distribution/tools/launchers/src/test/java/org/elasticsearch/tools/launchers/JvmErgonomicsTests.java
+++ distribution/tools/launchers/src/test/java/org/elasticsearch/tools/launchers/JvmErgonomicsTests.java
@@ -56,8 +56,8 @@ public class JvmErgonomicsTests extends LaunchersTestCase {
     }
 
     public void testExtractValidHeapSizeNoOptionPresent() throws InterruptedException, IOException {
-        // Muting on Windows, awaitsfix: https://github.com/elastic/elasticsearch/issues/47384
-        assumeFalse(System.getProperty("os.name").startsWith("Windows"));
+        // Muted for jdk8/Windows, see: https://github.com/elastic/elasticsearch/issues/47384
+        assumeFalse(System.getProperty("os.name").startsWith("Windows") && JavaVersion.majorVersion(JavaVersion.CURRENT) == 8);
         assertThat(
                 JvmErgonomics.extractHeapSize(JvmErgonomics.finalJvmOptions(Collections.emptyList())),
                 greaterThan(0L));
@@ -140,6 +140,7 @@ public class JvmErgonomicsTests extends LaunchersTestCase {
     }
 
     public void testMaxDirectMemorySizeChoice() throws InterruptedException, IOException {
+        // Muted for jdk8/Windows, see: https://github.com/elastic/elasticsearch/issues/47384
         assumeFalse(System.getProperty("os.name").startsWith("Windows") && JavaVersion.majorVersion(JavaVersion.CURRENT) == 8);
         final Map<String, String> heapMaxDirectMemorySize = new HashMap<>();
         heapMaxDirectMemorySize.put("64M", Long.toString((64L << 20) / 2));

dbf757dcecbcb831a5c1deb8e8d69b95b7d0001c
Armin Braun <me@obrown.io>
2019-10-29 13:30:14 +0100

    Fix SnapshotShardStatus Reporting for Failed Shard (#48556)
    Fixes the shard snapshot status reporting for failed shards
    in the corner case of failing the shard because of an exception
    thrown in `SnapshotShardsService` and not the repository.
    We were missing the update on the `snapshotStatus` instance in
    this case which made the transport APIs using this field report
    back an incorrect status.
    Fixed by moving the failure handling to the `SnapshotShardsService`
    for all cases (which also simplifies the code, the ex. wrapping in
    the repository was pointless as we only used the ex. trace upstream
    anyway).
    Also, added an assertion to another test that explicitly checks this
    failure situation (ex. in the `SnapshotShardsService`) already.
    
    Closes #48526

--- server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
+++ server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
@@ -31,7 +31,6 @@ import org.apache.lucene.store.IndexInput;
 import org.apache.lucene.store.IndexOutput;
 import org.apache.lucene.store.RateLimiter;
 import org.apache.lucene.util.SetOnce;
-import org.elasticsearch.ExceptionsHelper;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.ActionRunnable;
 import org.elasticsearch.action.StepListener;
@@ -1056,10 +1055,6 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
                               ActionListener<String> listener) {
         final ShardId shardId = store.shardId();
         final long startTime = threadPool.absoluteTimeInMillis();
-        final ActionListener<String> snapshotDoneListener = ActionListener.wrap(listener::onResponse, e -> {
-            snapshotStatus.moveToFailed(threadPool.absoluteTimeInMillis(), ExceptionsHelper.stackTrace(e));
-            listener.onFailure(e instanceof IndexShardSnapshotFailedException ? e : new IndexShardSnapshotFailedException(shardId, e));
-        });
         try {
             final String generation = snapshotStatus.generation();
             logger.debug("[{}] [{}] snapshot to [{}] [{}] ...", shardId, snapshotId, metadata.name(), generation);
@@ -1205,8 +1200,8 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
                     }
                 }
                 snapshotStatus.moveToDone(threadPool.absoluteTimeInMillis(), indexGeneration);
-                snapshotDoneListener.onResponse(indexGeneration);
-            }, snapshotDoneListener::onFailure);
+                listener.onResponse(indexGeneration);
+            }, listener::onFailure);
             if (indexIncrementalFileCount == 0) {
                 allFilesUploadedListener.onResponse(Collections.emptyList());
                 return;
@@ -1232,7 +1227,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
                 }));
             }
         } catch (Exception e) {
-            snapshotDoneListener.onFailure(e);
+            listener.onFailure(e);
         }
     }
 

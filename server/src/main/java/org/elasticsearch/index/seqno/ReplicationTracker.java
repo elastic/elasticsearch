784739d874fece0ed12787c73a6c841b54e8c010
Nhat Nguyen <nhat.nguyen@elastic.co>
2019-10-30 14:53:41 -0400

    Handle lower retaining seqno retention lease error (#46420)
    We renew the CCR retention lease at a fixed interval, therefore it's
    possible to have more than one in-flight renewal requests at the same
    time. If requests arrive out of order, then the assertion is violated.

--- server/src/main/java/org/elasticsearch/index/seqno/ReplicationTracker.java
+++ server/src/main/java/org/elasticsearch/index/seqno/ReplicationTracker.java
@@ -384,21 +384,27 @@ public class ReplicationTracker extends AbstractIndexShardComponent implements L
      * @param source                  the source of the retention lease
      * @return the renewed retention lease
      * @throws RetentionLeaseNotFoundException if the specified retention lease does not exist
+     * @throws IllegalArgumentException        if the new retaining sequence number is lower than
+     *                                         the retaining sequence number of the current retention lease.
      */
     public synchronized RetentionLease renewRetentionLease(final String id, final long retainingSequenceNumber, final String source) {
         assert primaryMode;
-        if (retentionLeases.contains(id) == false) {
+        final RetentionLease existingRetentionLease = retentionLeases.get(id);
+        if (existingRetentionLease == null) {
             throw new RetentionLeaseNotFoundException(id);
         }
+        if (retainingSequenceNumber < existingRetentionLease.retainingSequenceNumber()) {
+            assert PEER_RECOVERY_RETENTION_LEASE_SOURCE.equals(source) == false :
+                "renewing peer recovery retention lease [" + existingRetentionLease + "]" +
+                    " with a lower retaining sequence number [" + retainingSequenceNumber + "]";
+            throw new IllegalArgumentException(
+                "the current retention lease with [" + id + "]" +
+                    " is retaining a higher sequence number [" + existingRetentionLease.retainingSequenceNumber() + "]" +
+                    " than the new retaining sequence number [" + retainingSequenceNumber + "] from [" + source + "]"
+            );
+        }
         final RetentionLease retentionLease =
-                new RetentionLease(id, retainingSequenceNumber, currentTimeMillisSupplier.getAsLong(), source);
-        final RetentionLease existingRetentionLease = retentionLeases.get(id);
-        assert existingRetentionLease != null;
-        assert existingRetentionLease.retainingSequenceNumber() <= retentionLease.retainingSequenceNumber() :
-                "retention lease renewal for [" + id + "]"
-                        + " from [" + source + "]"
-                        + " renewed a lower retaining sequence number [" + retentionLease.retainingSequenceNumber() + "]"
-                        + " than the current lease retaining sequence number [" + existingRetentionLease.retainingSequenceNumber() + "]";
+            new RetentionLease(id, retainingSequenceNumber, currentTimeMillisSupplier.getAsLong(), source);
         retentionLeases = new RetentionLeases(
                 operationPrimaryTerm,
                 retentionLeases.version() + 1,

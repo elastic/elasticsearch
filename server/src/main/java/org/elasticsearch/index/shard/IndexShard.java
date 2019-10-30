f8ef402027e01d03e616a3a64403631dbdc79f3a
Nhat Nguyen <nhat.nguyen@elastic.co>
2019-10-30 14:08:35 -0400

    Do not warm up searcher in engine constructor (#48605)
    With this change, we won't warm up searchers until we externally refresh
    an engine. We explicitly refresh before allowing reading from a shard
    (i.e., move to post_recovery state) and during resetting. These
    guarantees that we have warmed up the engine before exposing the
    external searcher.
    
    Another prerequisite for #47186.

--- server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
@@ -2702,6 +2702,7 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
         final Sort indexSort = indexSortSupplier.get();
         final Engine.Warmer warmer = reader -> {
             assert Thread.holdsLock(mutex) == false : "warming engine under mutex";
+            assert reader != null;
             if (this.warmer != null) {
                 this.warmer.warm(reader);
             }
@@ -3413,6 +3414,7 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
                 // TODO: add a dedicate recovery stats for the reset translog
             });
         newEngineReference.get().recoverFromTranslog(translogRunner, globalCheckpoint);
+        newEngineReference.get().refresh("reset_engine");
         synchronized (mutex) {
             verifyNotClosed();
             IOUtils.close(currentEngineReference.getAndSet(newEngineReference.get()));

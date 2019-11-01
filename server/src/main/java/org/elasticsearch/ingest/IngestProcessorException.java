6dffebfecdfa816b30f40bc17fe97924746ab59e
Jason Tedor <jason@tedor.me>
2019-11-01 12:53:59 -0400

    Add serialization

--- server/src/main/java/org/elasticsearch/ingest/IngestProcessorException.java
+++ server/src/main/java/org/elasticsearch/ingest/IngestProcessorException.java
@@ -21,15 +21,22 @@ package org.elasticsearch.ingest;
 
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ElasticsearchWrapperException;
+import org.elasticsearch.common.io.stream.StreamInput;
+
+import java.io.IOException;
 
 /**
  * A dedicated wrapper for exceptions encountered executing an ingest processor. The wrapper is needed as we currently only unwrap causes
  * for instances of {@link ElasticsearchWrapperException}.
  */
-class IngestProcessorException extends ElasticsearchException implements ElasticsearchWrapperException {
+public class IngestProcessorException extends ElasticsearchException implements ElasticsearchWrapperException {
 
     IngestProcessorException(final Exception cause) {
         super(cause);
+    }
+
+    public IngestProcessorException(final StreamInput in) throws IOException {
+        super(in);
     }
 
 }

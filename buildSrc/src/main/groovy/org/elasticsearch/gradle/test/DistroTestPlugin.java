745ec614257f0c295398808efc83080e018d16d6
Rory Hunter <pugnascotia@users.noreply.github.com>
2019-11-07 12:30:39 +0000

    Disable docker packaging tests again (#48883)
    Per elastic/infra#15864, the Elasticsearch CI images are failing due to
    a packer_cache failure. This is because Gradle is trying to resolve
    a `.docker` file through the Ivy repository, which doesn't work. Disable
    the Docker tests again until we figure out the way forward.

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/test/DistroTestPlugin.java
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/test/DistroTestPlugin.java
@@ -353,7 +353,8 @@ public class DistroTestPlugin implements Plugin<Project> {
         List<ElasticsearchDistribution> currentDistros = new ArrayList<>();
         List<ElasticsearchDistribution> upgradeDistros = new ArrayList<>();
 
-        for (Type type : Arrays.asList(Type.DEB, Type.RPM, Type.DOCKER)) {
+        // Disable DOCKER due to https://github.com/elastic/infra/issues/15864
+        for (Type type : Arrays.asList(Type.DEB, Type.RPM /*, Type.DOCKER */)) {
             for (Flavor flavor : Flavor.values()) {
                 for (boolean bundledJdk : Arrays.asList(true, false)) {
                     // All our Docker images include a bundled JDK so it doesn't make sense to test without one

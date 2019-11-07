625c00ddd65f8361d29a8a40a667acf83a3b835f
Tanguy Leroux <tlrx.dev@gmail.com>
2019-11-07 11:17:06 -0500

    Add docker-composed based test fixture for GCS (#48762)
    Similarly to what has be done for Azure in #48636, this commit 
    adds a new :test:fixtures:gcs-fixture project which provides two 
    docker-compose based fixtures that emulate a Google Cloud 
    Storage service.
    
    Some code has been extracted from existing tests and placed 
    into this new project so that it can be easily reused in other 
    projects.

---
+++ test/fixtures/gcs-fixture/src/main/java/fixture/gcs/FakeOAuth2HttpHandler.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package fixture.gcs;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.elasticsearch.common.SuppressForbidden;
+import org.elasticsearch.rest.RestStatus;
+
+import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@SuppressForbidden(reason = "Uses a HttpServer to emulate a fake OAuth2 authentication service")
+public class FakeOAuth2HttpHandler implements HttpHandler {
+
+    @Override
+    public void handle(final HttpExchange exchange) throws IOException {
+        byte[] response = ("{\"access_token\":\"foo\",\"token_type\":\"Bearer\",\"expires_in\":3600}").getBytes(UTF_8);
+        exchange.getResponseHeaders().add("Content-Type", "application/json");
+        exchange.sendResponseHeaders(RestStatus.OK.getStatus(), response.length);
+        exchange.getResponseBody().write(response);
+        exchange.close();
+    }
+}

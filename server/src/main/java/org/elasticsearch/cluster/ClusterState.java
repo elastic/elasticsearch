bfc1103163f4cb1fb6c49d5a3bff06e50a30eef6
Xi Chen <zacharymorn@gmail.com>
2019-11-10 10:06:40 -0800

    Subtitute relevant logic in ClusterState.toXContent with IndexTemplateMetaData.toXContent, and note the difference in serialization. This test may sometimes fail due to JsonNode.equals may be order sensitive for deeply nested json structure

--- server/src/main/java/org/elasticsearch/cluster/ClusterState.java
+++ server/src/main/java/org/elasticsearch/cluster/ClusterState.java
@@ -430,31 +430,7 @@ public class ClusterState implements ToXContentFragment, Diffable<ClusterState> 
 
             builder.startObject("templates");
             for (ObjectCursor<IndexTemplateMetaData> cursor : metaData().templates().values()) {
-                IndexTemplateMetaData templateMetaData = cursor.value;
-                builder.startObject(templateMetaData.name());
-
-                builder.field("index_patterns", templateMetaData.patterns());
-                builder.field("order", templateMetaData.order());
-
-                builder.startObject("settings");
-                Settings settings = templateMetaData.settings();
-                settings.toXContent(builder, params);
-                builder.endObject();
-
-                builder.startObject("mappings");
-                for (ObjectObjectCursor<String, CompressedXContent> cursor1 : templateMetaData.mappings()) {
-                    Map<String, Object> mapping = XContentHelper.convertToMap(new BytesArray(cursor1.value.uncompressed()), false).v2();
-                    if (mapping.size() == 1 && mapping.containsKey(cursor1.key)) {
-                        // the type name is the root value, reduce it
-                        mapping = (Map<String, Object>) mapping.get(cursor1.key);
-                    }
-                    builder.field(cursor1.key);
-                    builder.map(mapping);
-                }
-                builder.endObject();
-
-
-                builder.endObject();
+                IndexTemplateMetaData.Builder.toXContent(cursor.value, builder, params);
             }
             builder.endObject();
 

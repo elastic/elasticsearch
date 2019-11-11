b810048f63523832b87999e6514f5c641a4f486c
Jake Landis <jake.landis@elastic.co>
2019-11-11 09:38:43 -0600

    [7.5] Mute FullClusterRestartTest#testWatcher and 30s timeout… (#48851)
    The timeout was increased to 60s to allow this test more time to reach a
    yellow state. However, the test will still on occasion fail even with the
    60s timeout.
    
    Related: #48381
    Related: #48434
    Related: #47950
    Related: #40178

--- x-pack/qa/full-cluster-restart/src/test/java/org/elasticsearch/xpack/restart/FullClusterRestartIT.java
+++ x-pack/qa/full-cluster-restart/src/test/java/org/elasticsearch/xpack/restart/FullClusterRestartIT.java
@@ -145,6 +145,7 @@ public class FullClusterRestartIT extends AbstractFullClusterRestartTestCase {
     }
 
     @SuppressWarnings("unchecked")
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/48381")
     public void testWatcher() throws Exception {
         if (isRunningAgainstOldCluster()) {
             logger.info("Adding a watch on old cluster {}", getOldClusterVersion());
@@ -602,7 +603,7 @@ public class FullClusterRestartIT extends AbstractFullClusterRestartTestCase {
     private void waitForYellow(String indexName) throws IOException {
         Request request = new Request("GET", "/_cluster/health/" + indexName);
         request.addParameter("wait_for_status", "yellow");
-        request.addParameter("timeout", "60s");
+        request.addParameter("timeout", "30s");
         request.addParameter("wait_for_no_relocating_shards", "true");
         if (getOldClusterVersion().onOrAfter(Version.V_6_2_0)) {
             request.addParameter("wait_for_no_initializing_shards", "true");

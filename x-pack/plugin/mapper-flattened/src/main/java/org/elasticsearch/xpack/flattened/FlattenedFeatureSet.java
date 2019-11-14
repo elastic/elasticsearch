fdc5ef104dcfd9fd682b58329c192648d07d868c
Julie Tibshirani <julie.tibshirani@elastic.co>
2019-11-14 10:27:08 -0500

    Add telemetry for flattened fields. (#48972)
    Currently we just record the number of flattened fields defined in the mappings.

--- x-pack/plugin/mapper-flattened/src/main/java/org/elasticsearch/xpack/flattened/FlattenedFeatureSet.java
+++ x-pack/plugin/mapper-flattened/src/main/java/org/elasticsearch/xpack/flattened/FlattenedFeatureSet.java
@@ -6,6 +6,9 @@
 package org.elasticsearch.xpack.flattened;
 
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
+import org.elasticsearch.cluster.metadata.MappingMetaData;
+import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.license.XPackLicenseState;
@@ -13,6 +16,7 @@ import org.elasticsearch.xpack.core.XPackFeatureSet;
 import org.elasticsearch.xpack.core.XPackField;
 import org.elasticsearch.xpack.core.XPackSettings;
 import org.elasticsearch.xpack.core.flattened.FlattenedFeatureSetUsage;
+import org.elasticsearch.xpack.flattened.mapper.FlatObjectFieldMapper;
 
 import java.util.Map;
 
@@ -20,11 +24,13 @@ public class FlattenedFeatureSet implements XPackFeatureSet {
 
     private final boolean enabled;
     private final XPackLicenseState licenseState;
+    private final ClusterService clusterService;
 
     @Inject
-    public FlattenedFeatureSet(Settings settings, XPackLicenseState licenseState) {
+    public FlattenedFeatureSet(Settings settings, XPackLicenseState licenseState, ClusterService clusterService) {
         this.enabled = XPackSettings.FLATTENED_ENABLED.get(settings);
         this.licenseState = licenseState;
+        this.clusterService = clusterService;
     }
 
     @Override
@@ -49,6 +55,29 @@ public class FlattenedFeatureSet implements XPackFeatureSet {
 
     @Override
     public void usage(ActionListener<Usage> listener) {
-        listener.onResponse(new FlattenedFeatureSetUsage(available(), enabled()));
+        int fieldCount = 0;
+        if (available() && enabled() && clusterService.state() != null) {
+            for (IndexMetaData indexMetaData : clusterService.state().metaData()) {
+                MappingMetaData mappingMetaData = indexMetaData.mapping();
+
+                if (mappingMetaData != null) {
+                    Map<String, Object> mappings = mappingMetaData.getSourceAsMap();
+
+                    if (mappings.containsKey("properties")) {
+                        @SuppressWarnings("unchecked")
+                        Map<String, Map<String, Object>> fieldMappings = (Map<String, Map<String, Object>>) mappings.get("properties");
+
+                        for (Map<String, Object> fieldMapping : fieldMappings.values()) {
+                            String fieldType = (String) fieldMapping.get("type");
+                            if (fieldType != null && fieldType.equals(FlatObjectFieldMapper.CONTENT_TYPE)) {
+                                fieldCount++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        listener.onResponse(new FlattenedFeatureSetUsage(available(), enabled(), fieldCount));
     }
 }

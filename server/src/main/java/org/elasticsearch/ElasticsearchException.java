2bcdcb17cdd19ecd0336c8760a93243e4e6630e5
Jason Tedor <jason@tedor.me>
2019-11-14 09:53:08 -0600

    Introduce dedicated ingest processor exception (#48810)
    Today we wrap exceptions that occur while executing an ingest processor
    in an ElasticsearchException. Today, in ExceptionsHelper#unwrapCause we
    only unwrap causes for exceptions that implement
    ElasticsearchWrapperException, which the top-level
    ElasticsearchException does not. Ultimately, this means that any
    exception that occurs during processor execution does not have its cause
    unwrapped, and so its status is blanket treated as a 500. This means
    that while executing a bulk request with an ingest pipeline,
    document-level failures that occur during a processor will cause the
    status for that document to be treated as 500. Since that does not give
    the client any indication that they made a mistake, it means some
    clients will enter infinite retries, thinking that there is some
    server-side problem that merely needs to clear. This commit addresses
    this by introducing a dedicated ingest processor exception, so that its
    causes can be unwrapped. While we could consider a broader change to
    unwrap causes for more than just ElasticsearchWrapperExceptions, that is
    a broad change with unclear implications. Since the problem of reporting
    500s on client errors is a user-facing bug, we take the conservative
    approach for now, and we can revisit the unwrapping in a future change.

--- server/src/main/java/org/elasticsearch/ElasticsearchException.java
+++ server/src/main/java/org/elasticsearch/ElasticsearchException.java
@@ -1036,7 +1036,12 @@ public class ElasticsearchException extends RuntimeException implements ToXConte
                 org.elasticsearch.index.seqno.RetentionLeaseInvalidRetainingSeqNoException.class,
                 org.elasticsearch.index.seqno.RetentionLeaseInvalidRetainingSeqNoException::new,
                 156,
-                Version.V_7_5_0);
+                Version.V_7_5_0),
+        INGEST_PROCESSOR_EXCEPTION(
+                org.elasticsearch.ingest.IngestProcessorException.class,
+                org.elasticsearch.ingest.IngestProcessorException::new,
+                157,
+                Version.V_7_6_0);
 
         final Class<? extends ElasticsearchException> exceptionClass;
         final CheckedFunction<StreamInput, ? extends ElasticsearchException, IOException> constructor;

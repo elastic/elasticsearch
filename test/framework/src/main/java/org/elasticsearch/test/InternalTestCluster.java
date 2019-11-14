dec7afce7f50835f9924a91e583d0ce4da015ee8
Nhat Nguyen <nhat.nguyen@elastic.co>
2019-11-14 18:03:48 -0500

    fix data node settings

--- test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
+++ test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
@@ -1984,8 +1984,8 @@ public final class InternalTestCluster extends TestCluster {
     public List<String> startDataOnlyNodes(int numNodes, Settings settings) {
         return startNodes(
             numNodes,
-            Settings.builder().put(Settings.EMPTY).put(Node.NODE_MASTER_SETTING.getKey(), false)
-                .put(Node.NODE_DATA_SETTING.getKey(), true).put(settings).build());
+            Settings.builder().put(settings).put(Node.NODE_MASTER_SETTING.getKey(), false)
+                .put(Node.NODE_DATA_SETTING.getKey(), true).build());
     }
 
     private int getMasterNodesCount() {

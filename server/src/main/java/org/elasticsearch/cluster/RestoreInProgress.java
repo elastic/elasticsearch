9bb38125015417fbcb704ef78e04d1437689e8ea
Armin Braun <me@obrown.io>
2019-11-18 11:16:03 +0100

    nicer to string

--- server/src/main/java/org/elasticsearch/cluster/RestoreInProgress.java
+++ server/src/main/java/org/elasticsearch/cluster/RestoreInProgress.java
@@ -23,7 +23,6 @@ import com.carrotsearch.hppc.cursors.ObjectCursor;
 import com.carrotsearch.hppc.cursors.ObjectObjectCursor;
 import org.elasticsearch.Version;
 import org.elasticsearch.cluster.ClusterState.Custom;
-import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.collect.ImmutableOpenMap;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
@@ -76,7 +75,10 @@ public class RestoreInProgress extends AbstractNamedDiffable<Custom> implements 
 
     @Override
     public String toString() {
-        return Strings.toString(this);
+        StringBuilder builder = new StringBuilder("RestoreInProgress[");
+        entries.forEach(entry -> builder.append("{").append(entry.key).append("}{").append(entry.value.snapshot).append("},"));
+        builder.setCharAt(builder.length() - 1, ']');
+        return builder.toString();
     }
 
     public Entry get(String restoreUUID) {

787bf8f5fd3f38f4e5b22f0f3aa5e5996c95e357
kyra.wkh <kyra.wkh@alibaba-inc.com>
2019-11-20 20:58:48 +0800

    fix assemble err

--- server/src/main/java/org/elasticsearch/metrics/CoordinatingIndicesStatsCollector.java
+++ server/src/main/java/org/elasticsearch/metrics/CoordinatingIndicesStatsCollector.java
@@ -13,7 +13,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CoordinatingIndicesStatsCollector {
 
     /**
-     * indexName --> collector
+     * indexName -- collector
      */
     private final Map<String, StatsCollector> collectorMap = new ConcurrentHashMap<>();
 

9468c4954db019c437b2ba7018223d036ae1aaca
Armin Braun <me@obrown.io>
2019-11-24 21:45:27 +0100

    cs

--- test/fixtures/azure-fixture/src/main/java/fixture/azure/AzureHttpHandler.java
+++ test/fixtures/azure-fixture/src/main/java/fixture/azure/AzureHttpHandler.java
@@ -31,7 +31,6 @@ import org.elasticsearch.rest.RestUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;

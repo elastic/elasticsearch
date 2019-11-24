48c7e8d22126a3af96573a11ef998155ad9917e4
Armin Braun <me@obrown.io>
2019-11-24 21:38:46 +0100

    Improve Stability of Mock APIs
    This commit ensures that even for requests that are known to be empty body
    we at least attempt to read one bytes from the request body input stream.
    This is done to work around the behavior in `sun.net.httpserver.ServerImpl.Dispatcher#handleEvent`
    that will close a TCP/HTTP connection that does not have the `eof` flag (see `sun.net.httpserver.LeftOverInputStream#isEOF`)
    set on its input stream. As far as I can tell the only way to set this flag is to do a read when there's no more bytes buffered.
    This fixes the numerous connection closing issues because the `ServerImpl` stops closing connections that it thinks
    weren't fully drained.
    
    Also, I removed a now redundant drain loop in the Azure handler as well as removed the connection closing in the error handler's
    drain action (this shouldn't have an effect but makes things more predictable/easier to reason about IMO).
    
    I would suggest merging this and closing related issue after verifying that this fixes things on CI.
    
    The way to locally reproduce the issues we're seeing in tests is to make the retry timings more aggressive in e.g. the azure tests
    and move them to single digit values. This makes the retries happen quickly enough that they run into the async connecting closing
    of allegedly non-eof connections by `ServerImpl` and produces the exact kinds of failures we're seeing currently.
    
    Relates #49401

--- test/fixtures/azure-fixture/src/main/java/fixture/azure/AzureHttpHandler.java
+++ test/fixtures/azure-fixture/src/main/java/fixture/azure/AzureHttpHandler.java
@@ -63,6 +63,10 @@ public class AzureHttpHandler implements HttpHandler {
     @Override
     public void handle(final HttpExchange exchange) throws IOException {
         final String request = exchange.getRequestMethod() + " " + exchange.getRequestURI().toString();
+        if (request.startsWith("GET") || request.startsWith("HEAD") || request.startsWith("DELETE")) {
+            int read = exchange.getRequestBody().read();
+            assert read == -1 : "Request body should have been empty but saw [" + read + "]";
+        }
         try {
             if (Regex.simpleMatch("PUT /" + container + "/*blockid=*", request)) {
                 // Put Block (https://docs.microsoft.com/en-us/rest/api/storageservices/put-block)
@@ -140,9 +144,6 @@ public class AzureHttpHandler implements HttpHandler {
 
             } else if (Regex.simpleMatch("DELETE /" + container + "/*", request)) {
                 // Delete Blob (https://docs.microsoft.com/en-us/rest/api/storageservices/delete-blob)
-                try (InputStream is = exchange.getRequestBody()) {
-                    while (is.read() >= 0);
-                }
                 blobs.entrySet().removeIf(blob -> blob.getKey().startsWith(exchange.getRequestURI().getPath()));
                 exchange.sendResponseHeaders(RestStatus.ACCEPTED.getStatus(), -1);
 

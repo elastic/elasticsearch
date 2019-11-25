06143154b5a3f205eb8a721560b5ef902a144ca8
Przemko Robakowski <przemko.robakowski@elastic.co>
2019-11-25 23:16:33 +0100

    Documentation + required target_fields parameter

--- modules/ingest-common/src/main/java/org/elasticsearch/ingest/common/CsvProcessor.java
+++ modules/ingest-common/src/main/java/org/elasticsearch/ingest/common/CsvProcessor.java
@@ -30,11 +30,11 @@ import java.util.Map;
  * A processor that breaks line from CSV file into separate fields.
  * If there's more fields requested than there is in the CSV, extra field will not be present in the document after processing.
  * In the same way this processor will skip any field that is empty in CSV.
- *
- *
+ * <p>
+ * <p>
  * By default it uses rules according to <a href="https://tools.ietf.org/html/rfc4180">RCF 4180</a>,
  * but it can be tweaked with following parameters:
- *
+ * <p>
  * quote: set custom quote character (defaults to ")
  * separator: set custom separator (defaults to ,)
  * trim: trim leading whitespaces in each field (allows also whitespaces before quoted fields, defaults to false)
@@ -96,9 +96,12 @@ public final class CsvProcessor extends AbstractProcessor {
             }
             boolean trim = ConfigurationUtils.readBooleanProperty(TYPE, processorTag, config, "trim", false);
             boolean ignoreMissing = ConfigurationUtils.readBooleanProperty(TYPE, processorTag, config, "ignore_missing", false);
-            List<String> targetFields = ConfigurationUtils.readOptionalList(TYPE, processorTag, config, "target_fields");
-            return new CsvProcessor(processorTag, field, targetFields == null ? new String[0] : targetFields.toArray(String[]::new),
-                trim, separator.charAt(0), quote.charAt(0), ignoreMissing);
+            List<String> targetFields = ConfigurationUtils.readList(TYPE, processorTag, config, "target_fields");
+            if (targetFields.isEmpty()) {
+                throw new IllegalArgumentException("target fields list can't be empty");
+            }
+            return new CsvProcessor(processorTag, field, targetFields.toArray(String[]::new), trim, separator.charAt(0), quote.charAt(0),
+                ignoreMissing);
         }
     }
 }

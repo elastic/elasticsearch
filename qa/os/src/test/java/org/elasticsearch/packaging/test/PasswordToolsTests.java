db5a3d92588152b7694fea4ea9cf2d1c8fd9e588
Ryan Ernst <ryan@iernst.net>
2019-11-25 11:22:55 -0800

    add sleep...FML

--- qa/os/src/test/java/org/elasticsearch/packaging/test/PasswordToolsTests.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/test/PasswordToolsTests.java
@@ -69,10 +69,18 @@ public class PasswordToolsTests extends PackagingTestCase {
     }
 
     public void test30AddBootstrapPassword() throws Exception {
+        // HACK: windows asynchronously releases file locks after processes exit. Unfortunately there is no clear way to wait on
+        // those locks being released. We might be able to use `openfiles /query`, but that requires modifying global settings
+        // in our windows images with `openfiles /local on` (which requires a restart, thus needs to be baked into the images).
+        // The following sleep allows time for windows to release the data file locks from Elasticsearch which was stopped in the
+        // previous test.
+        Thread.sleep(3000);
+
         try (Stream<Path> dataFiles = Files.list(installation.data)) {
             // delete each dir under data, not data itself
             dataFiles.forEach(FileUtils::rm);
         }
+
         installation.executables().elasticsearchKeystore.run(sh, "add --stdin bootstrap.password", BOOTSTRAP_PASSWORD);
 
         assertWhileRunning(() -> {

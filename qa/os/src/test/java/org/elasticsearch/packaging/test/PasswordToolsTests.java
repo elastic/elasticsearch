b9f1802c2c14f5c37908a91ecd7ef866a031d792
Ryan Ernst <ryan@iernst.net>
2019-11-27 13:20:37 -0800

    another try at corralling windows

--- qa/os/src/test/java/org/elasticsearch/packaging/test/PasswordToolsTests.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/test/PasswordToolsTests.java
@@ -69,16 +69,40 @@ public class PasswordToolsTests extends PackagingTestCase {
     }
 
     public void test30AddBootstrapPassword() throws Exception {
-        // HACK: windows asynchronously releases file locks after processes exit. Unfortunately there is no clear way to wait on
-        // those locks being released. We might be able to use `openfiles /query`, but that requires modifying global settings
-        // in our windows images with `openfiles /local on` (which requires a restart, thus needs to be baked into the images).
-        // The following sleep allows time for windows to release the data file locks from Elasticsearch which was stopped in the
-        // previous test.
-        Thread.sleep(3000);
 
         try (Stream<Path> dataFiles = Files.list(installation.data)) {
             // delete each dir under data, not data itself
-            dataFiles.forEach(FileUtils::rm);
+            dataFiles.forEach(file -> {
+                if (distribution.platform != Distribution.Platform.WINDOWS) {
+                    FileUtils.rm(file);
+                }
+                // HACK: windows asynchronously releases file locks after processes exit. Unfortunately there is no clear way to wait on
+                // those locks being released. We might be able to use `openfiles /query`, but that requires modifying global settings
+                // in our windows images with `openfiles /local on` (which requires a restart, thus needs to be baked into the images).
+                // The following sleep allows time for windows to release the data file locks from Elasticsearch which was stopped in the
+                // previous test.
+                int retries = 30;
+                Exception failure = null;
+                while (retries-- > 0) {
+                    try {
+                        FileUtils.rm(file);
+                        return;
+                    } catch (Exception e) {
+                        if (failure == null) {
+                            failure = e;
+                        } else {
+                            failure.addSuppressed(e);
+                        }
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException interrupted) {
+                            Thread.currentThread().interrupt();
+                            return;
+                        }
+                    }
+                }
+                throw new RuntimeException("failed to delete " + file, failure);
+            });
         }
 
         installation.executables().elasticsearchKeystore.run(sh, "add --stdin bootstrap.password", BOOTSTRAP_PASSWORD);

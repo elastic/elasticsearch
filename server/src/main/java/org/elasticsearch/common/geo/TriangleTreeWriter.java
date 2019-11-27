5a4b5a4f0779411dd3ce0f2c3fcc4065f587f915
iverase <ivera@apache.org>
2019-11-27 16:55:36 +0100

    put proper headers

--- server/src/main/java/org/elasticsearch/common/geo/TriangleTreeWriter.java
+++ server/src/main/java/org/elasticsearch/common/geo/TriangleTreeWriter.java
@@ -17,11 +17,7 @@
  * under the License.
  */
 package org.elasticsearch.common.geo;
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
+
 import org.apache.lucene.geo.GeoUtils;
 import org.apache.lucene.geo.Tessellator;
 import org.apache.lucene.util.ArrayUtil;

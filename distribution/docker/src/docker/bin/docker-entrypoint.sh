1bc3e69fa31857ba52bbc556e048c8a034469cb1
Rory Hunter <pugnascotia@users.noreply.github.com>
2019-12-04 08:57:58 +0000

    Migrate some of the Docker tests from old repository (#49792)
    Backport of #49079. Reimplement a number of the tests from
    elastic/elasticsearch-docker.
    
    There is also one Docker image fix here, which is that two of the provided
    config files had different file permissions to the rest. I've fixed this
    with another RUN chmod while building the image, and adjusted the
    corresponding packaging test.

--- distribution/docker/src/docker/bin/docker-entrypoint.sh
+++ distribution/docker/src/docker/bin/docker-entrypoint.sh
@@ -85,9 +85,8 @@ declare -a es_opts
 while IFS='=' read -r envvar_key envvar_value
 do
   # Elasticsearch settings need to have at least two dot separated lowercase
-  # words, e.g. `cluster.name`, except for `processors` which we handle
-  # specially
-  if [[ "$envvar_key" =~ ^[a-z0-9_]+\.[a-z0-9_]+ || "$envvar_key" == "processors" ]]; then
+  # words, e.g. `cluster.name`
+  if [[ "$envvar_key" =~ ^[a-z0-9_]+\.[a-z0-9_]+ ]]; then
     if [[ ! -z $envvar_value ]]; then
       es_opt="-E${envvar_key}=${envvar_value}"
       es_opts+=("${es_opt}")

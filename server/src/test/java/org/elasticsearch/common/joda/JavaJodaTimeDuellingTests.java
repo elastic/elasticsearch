69e8c1a4bc1a31be8c5667f200cc25d3dcf55655
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2019-12-09 13:59:21 +0100

    unused import

--- server/src/test/java/org/elasticsearch/common/joda/JavaJodaTimeDuellingTests.java
+++ server/src/test/java/org/elasticsearch/common/joda/JavaJodaTimeDuellingTests.java
@@ -28,7 +28,6 @@ import org.elasticsearch.test.ESTestCase;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.ISODateTimeFormat;
 
 import java.time.ZoneId;
 import java.time.ZoneOffset;

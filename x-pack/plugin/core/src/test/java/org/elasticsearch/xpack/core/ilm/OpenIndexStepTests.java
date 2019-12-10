1f8c96a6a1437613fa7c62c7a5548e38ac299a76
Sivagurunathan Velayutham <sivadeva.93@gmail.com>
2019-12-09 18:47:27 -0800

    Added license and renamed files for tests

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/ilm/OpenIndexStepTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/ilm/OpenIndexStepTests.java
@@ -1,20 +1,7 @@
 /*
- * Licensed to Elasticsearch under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
  */
 
 package org.elasticsearch.xpack.core.ilm;
@@ -22,8 +9,8 @@ package org.elasticsearch.xpack.core.ilm;
 import org.apache.lucene.util.SetOnce;
 import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.action.admin.indices.close.CloseIndexRequest;
-import org.elasticsearch.action.admin.indices.close.CloseIndexResponse;
+import org.elasticsearch.action.admin.indices.open.OpenIndexRequest;
+import org.elasticsearch.action.admin.indices.open.OpenIndexResponse;
 import org.elasticsearch.client.AdminClient;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.IndicesAdminClient;
@@ -32,11 +19,9 @@ import org.junit.Before;
 import org.mockito.Mockito;
 import org.mockito.stubbing.Answer;
 
-import java.util.Collections;
-
 import static org.hamcrest.Matchers.equalTo;
 
-public class CloseIndexStepTest extends AbstractStepTestCase<CloseIndexStep> {
+public class OpenIndexStepTests extends AbstractStepTestCase<OpenIndexStep> {
 
     private Client client;
 
@@ -46,12 +31,12 @@ public class CloseIndexStepTest extends AbstractStepTestCase<CloseIndexStep> {
     }
 
     @Override
-    protected CloseIndexStep createRandomInstance() {
-        return new CloseIndexStep(randomStepKey(), randomStepKey(), client);
+    protected OpenIndexStep createRandomInstance() {
+        return new OpenIndexStep(randomStepKey(), randomStepKey(), client);
     }
 
     @Override
-    protected CloseIndexStep mutateInstance(CloseIndexStep instance) {
+    protected OpenIndexStep mutateInstance(OpenIndexStep instance) {
         Step.StepKey key = instance.getKey();
         Step.StepKey nextKey = instance.getNextStepKey();
 
@@ -66,19 +51,22 @@ public class CloseIndexStepTest extends AbstractStepTestCase<CloseIndexStep> {
                 throw new AssertionError("Illegal randomisation branch");
         }
 
-        return new CloseIndexStep(key, nextKey, client);
+        return new OpenIndexStep(key, nextKey, client);
     }
 
     @Override
-    protected CloseIndexStep copyInstance(CloseIndexStep instance) {
-        return new CloseIndexStep(instance.getKey(), instance.getNextStepKey(), instance.getClient());
+    protected OpenIndexStep copyInstance(OpenIndexStep instance) {
+        return new OpenIndexStep(instance.getKey(), instance.getNextStepKey(), instance.getClient());
     }
 
     public void testPerformAction() {
         IndexMetaData indexMetaData = IndexMetaData.builder(randomAlphaOfLength(10)).settings(settings(Version.CURRENT))
-            .numberOfShards(randomIntBetween(1, 5)).numberOfReplicas(randomIntBetween(0, 5)).build();
+            .numberOfShards(randomIntBetween(1, 5))
+            .numberOfReplicas(randomIntBetween(0, 5))
+            .state(IndexMetaData.State.CLOSE)
+            .build();
 
-        CloseIndexStep step = createRandomInstance();
+        OpenIndexStep step = createRandomInstance();
 
         AdminClient adminClient = Mockito.mock(AdminClient.class);
         IndicesAdminClient indicesClient = Mockito.mock(IndicesAdminClient.class);
@@ -87,14 +75,13 @@ public class CloseIndexStepTest extends AbstractStepTestCase<CloseIndexStep> {
         Mockito.when(adminClient.indices()).thenReturn(indicesClient);
 
         Mockito.doAnswer((Answer<Void>) invocation -> {
-            CloseIndexRequest request = (CloseIndexRequest) invocation.getArguments()[0];
+            OpenIndexRequest request = (OpenIndexRequest) invocation.getArguments()[0];
             @SuppressWarnings("unchecked")
-            ActionListener<CloseIndexResponse> listener = (ActionListener<CloseIndexResponse>) invocation.getArguments()[1];
+            ActionListener<OpenIndexResponse> listener = (ActionListener<OpenIndexResponse>) invocation.getArguments()[1];
             assertThat(request.indices(), equalTo(new String[]{indexMetaData.getIndex().getName()}));
-            listener.onResponse(new CloseIndexResponse(true, true,
-                Collections.singletonList(new CloseIndexResponse.IndexResult(indexMetaData.getIndex()))));
+            listener.onResponse(new OpenIndexResponse(true, true));
             return null;
-        }).when(indicesClient).close(Mockito.any(), Mockito.any());
+        }).when(indicesClient).open(Mockito.any(), Mockito.any());
 
         SetOnce<Boolean> actionCompleted = new SetOnce<>();
 
@@ -114,15 +101,18 @@ public class CloseIndexStepTest extends AbstractStepTestCase<CloseIndexStep> {
         assertEquals(true, actionCompleted.get());
         Mockito.verify(client, Mockito.only()).admin();
         Mockito.verify(adminClient, Mockito.only()).indices();
-        Mockito.verify(indicesClient, Mockito.only()).close(Mockito.any(), Mockito.any());
+        Mockito.verify(indicesClient, Mockito.only()).open(Mockito.any(), Mockito.any());
     }
 
 
     public void testPerformActionFailure() {
         IndexMetaData indexMetaData = IndexMetaData.builder(randomAlphaOfLength(10)).settings(settings(Version.CURRENT))
-            .numberOfShards(randomIntBetween(1, 5)).numberOfReplicas(randomIntBetween(0, 5)).build();
+            .numberOfShards(randomIntBetween(1, 5))
+            .numberOfReplicas(randomIntBetween(0, 5))
+            .state(IndexMetaData.State.CLOSE)
+            .build();
 
-        CloseIndexStep step = createRandomInstance();
+        OpenIndexStep step = createRandomInstance();
         Exception exception = new RuntimeException();
         AdminClient adminClient = Mockito.mock(AdminClient.class);
         IndicesAdminClient indicesClient = Mockito.mock(IndicesAdminClient.class);
@@ -131,13 +121,13 @@ public class CloseIndexStepTest extends AbstractStepTestCase<CloseIndexStep> {
         Mockito.when(adminClient.indices()).thenReturn(indicesClient);
 
         Mockito.doAnswer((Answer<Void>) invocation -> {
-            CloseIndexRequest request = (CloseIndexRequest) invocation.getArguments()[0];
+            OpenIndexRequest request = (OpenIndexRequest) invocation.getArguments()[0];
             @SuppressWarnings("unchecked")
-            ActionListener<CloseIndexResponse> listener = (ActionListener<CloseIndexResponse>) invocation.getArguments()[1];
+            ActionListener<OpenIndexResponse> listener = (ActionListener<OpenIndexResponse>) invocation.getArguments()[1];
             assertThat(request.indices(), equalTo(new String[]{indexMetaData.getIndex().getName()}));
             listener.onFailure(exception);
             return null;
-        }).when(indicesClient).close(Mockito.any(), Mockito.any());
+        }).when(indicesClient).open(Mockito.any(), Mockito.any());
 
         SetOnce<Boolean> exceptionThrown = new SetOnce<>();
 
@@ -158,6 +148,6 @@ public class CloseIndexStepTest extends AbstractStepTestCase<CloseIndexStep> {
         assertEquals(true, exceptionThrown.get());
         Mockito.verify(client, Mockito.only()).admin();
         Mockito.verify(adminClient, Mockito.only()).indices();
-        Mockito.verify(indicesClient, Mockito.only()).close(Mockito.any(), Mockito.any());
+        Mockito.verify(indicesClient, Mockito.only()).open(Mockito.any(), Mockito.any());
     }
 }

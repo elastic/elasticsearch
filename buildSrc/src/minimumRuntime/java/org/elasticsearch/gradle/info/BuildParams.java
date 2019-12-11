31fe7000a9b700c6112da65165a497ed5351d78b
Mark Vieira <portugee@gmail.com>
2019-12-11 11:04:26 -0800

    Fix exception message for boolean BuildParams properties (#50094)

--- buildSrc/src/minimumRuntime/java/org/elasticsearch/gradle/info/BuildParams.java
+++ buildSrc/src/minimumRuntime/java/org/elasticsearch/gradle/info/BuildParams.java
@@ -137,7 +137,7 @@ public class BuildParams {
     }
 
     private static String propertyName(String methodName) {
-        String propertyName = methodName.substring("get".length());
+        String propertyName = methodName.startsWith("is") ? methodName.substring("is".length()) : methodName.substring("get".length());
         return propertyName.substring(0, 1).toLowerCase() + propertyName.substring(1);
     }
 

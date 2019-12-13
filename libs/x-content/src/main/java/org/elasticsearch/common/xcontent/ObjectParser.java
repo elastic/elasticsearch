e6da525a45d08e55ae8ad1d547b7daecaab3bc1f
Zachary Tong <zach@elastic.co>
2019-12-13 13:02:57 -0500

    Checkstyle

--- libs/x-content/src/main/java/org/elasticsearch/common/xcontent/ObjectParser.java
+++ libs/x-content/src/main/java/org/elasticsearch/common/xcontent/ObjectParser.java
@@ -25,7 +25,6 @@ import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Iterator;

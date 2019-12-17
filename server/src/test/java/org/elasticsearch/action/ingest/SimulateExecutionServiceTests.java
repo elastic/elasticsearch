40676bc7be624d2aa9a767faf9ed41731a81f9d9
Martijn van Groningen <martijn.v.groningen@gmail.com>
2019-12-17 09:59:25 +0100

    added timeout

--- server/src/test/java/org/elasticsearch/action/ingest/SimulateExecutionServiceTests.java
+++ server/src/test/java/org/elasticsearch/action/ingest/SimulateExecutionServiceTests.java
@@ -42,6 +42,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 
@@ -378,7 +379,7 @@ public class SimulateExecutionServiceTests extends ESTestCase {
             errorHolder.set(e);
             latch.countDown();
         }));
-        latch.await();
+        latch.await(1, TimeUnit.MINUTES);
         assertThat(errorHolder.get(), nullValue());
         SimulatePipelineResponse response = responseHolder.get();
         assertThat(response, notNullValue());

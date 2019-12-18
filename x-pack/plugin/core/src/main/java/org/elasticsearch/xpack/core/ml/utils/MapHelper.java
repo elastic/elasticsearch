4f580b5ff3d1a192fc9bfc435b600391d4480a6d
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2019-12-18 09:17:54 -0500

    adjusing dig

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/utils/MapHelper.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/utils/MapHelper.java
@@ -9,16 +9,21 @@ import org.elasticsearch.common.Nullable;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Stack;
 
 public final class MapHelper {
 
     private MapHelper() {}
 
     /**
-     * This eagerly digs through the map by tokenizing the provided path on '.'.
+     * This eagerly digs (depth first search, longer keys first) through the map by tokenizing the provided path on '.'.
      *
-     * Only fully nested or fully collapsed fields are searched.
-     * Examples
+     * It is possible for ES _source docs to have "mixed" path formats. So, we should search all potential paths
+     * given the current knowledge of the map.
+     *
+     * Examples:
+     *
+     * The following maps would return `2` given the path "a.b.c.d"
      *
      * {
      *     "a.b.c.d" : 2
@@ -26,25 +31,31 @@ public final class MapHelper {
      * {
      *     "a" :{"b": {"c": {"d" : 2}}}
      * }
-     *
-     * It is possible for ES _source docs to have "mixed" path formats.
-     *
-     * Meaning the following _sources (along with the above) are indexed the same:
      * {
-     *     "a": {"b.c": {"d": 2}}
+     *     "a" :{"b.c": {"d" : 2}}}
      * }
      * {
-     *     "a.b.c": {"d": 2}
+     *     "a" :{"b": {"c": {"d" : 2}}},
+     *     "a.b" :{"c": {"d" : 5}} // we choose the first one found, we go down longer keys first
+     * }
+     * {
+     *     "a" :{"b": {"c": {"NOT_d" : 2, "d": 2}}}
      * }
      *
-     * Exhaustively exploring all the combinations of nesting fields via '.' (field name delimiter) or ':' (nested objects)
-     * would result in 2^n-1 total possible paths, where {@code n = path.split("\\.").length}.
+     * Conceptual "Worse case" 5 potential paths explored for "a.b.c.d" until 2 is finally returned
+     * {
+     *     "a.b.c": {"not_d": 2},
+     *     "a.b": {"c": {"not_d": 2}},
+     *     "a": {"b.c": {"not_d": 2}},
+     *     "a": {"b" :{ "c.not_d": 2}},
+     *     "a" :{"b": {"c": {"not_d" : 2}}},
+     *     "a" :{"b": {"c": {"d" : 2}}},
+     * }
      *
-     * This would result in an exponential runtime algorithm.
+     * We don't exhaustively create all potential paths.
+     * If we did, this would result in 2^n-1 total possible paths, where {@code n = path.split("\\.").length}.
      *
-     * NOTE: The default maximum field depth is 20. Meaning 524288 different ways of
-     * nesting the same fields.
-     *
+     * Instead we lazily create potential paths once we know that they are possibilities.
      *
      * @param path Dot delimited path containing the field desired
      * @param map The {@link Map} map to dig
@@ -60,17 +71,63 @@ public final class MapHelper {
         if (Arrays.stream(fields).anyMatch(String::isEmpty)) {
             throw new IllegalArgumentException("Empty path detected. Invalid field name");
         }
-        return explore(fields, map);
+        Stack<PotentialPath> pathStack = new Stack<>();
+        pathStack.push(new PotentialPath(map, 0));
+        return explore(fields, pathStack);
     }
 
     @SuppressWarnings("unchecked")
-    private static Object explore(String[] path, Map<String, Object> map) {
-        for(int i = 0; i < path.length - 1; i++) {
-            if (map.get(path[i]) instanceof Map<?, ?> == false) {
-                return null;
+    private static Object explore(String[] path, Stack<PotentialPath> pathStack) {
+        while (pathStack.empty() == false) {
+            PotentialPath potentialPath = pathStack.pop();
+            int endPos = potentialPath.pathPosition + 1;
+            int startPos = potentialPath.pathPosition;
+            Map<String, Object> map = potentialPath.map;
+            String candidateKey = null;
+            while(endPos <= path.length) {
+                candidateKey = mergePath(path, startPos, endPos);
+                Object next = map.get(candidateKey);
+                if (endPos == path.length && next != null) { // exit early, we reached the full path and found something
+                    return next;
+                }
+                if (next instanceof Map<?, ?>) { // we found another map, continue exploring down this path
+                    pathStack.push(new PotentialPath((Map<String, Object>)next, endPos));
+                }
+                endPos++;
             }
-            map = (Map<String, Object>) map.get(path[i]);
+            if (candidateKey != null && map.containsKey(candidateKey)) { //exit early
+                return map.get(candidateKey);
+            }
         }
-        return map.get(path[path.length - 1]);
+
+        return null;
+    }
+
+    private static String mergePath(String[] path, int start, int end) {
+        if (start == end + 1) { // early exit, no need to create sb
+            return path[start];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = start; i < end - 1; i++) {
+            sb.append(path[i]);
+            sb.append(".");
+        }
+        sb.append(path[end - 1]);
+        return sb.toString();
+    }
+
+    private static class PotentialPath {
+
+        // Pointer to where to start exploring
+        private final Map<String, Object> map;
+        // Where in the requested path are we
+        private final int pathPosition;
+
+        private PotentialPath(Map<String, Object> map, int pathPosition) {
+            this.map = map;
+            this.pathPosition = pathPosition;
+        }
+
     }
 }

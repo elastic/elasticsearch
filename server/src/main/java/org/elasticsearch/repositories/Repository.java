b1af8ee66fb3035e938455c90623df3054bc2143
Armin Braun <me@obrown.io>
2019-12-18 05:13:12 +0100

    add comment

--- server/src/main/java/org/elasticsearch/repositories/Repository.java
+++ server/src/main/java/org/elasticsearch/repositories/Repository.java
@@ -95,6 +95,7 @@ public interface Repository extends LifecycleComponent {
     /**
      * Returns the index metadata associated with the snapshot.
      *
+     * @param repositoryData current {@link RepositoryData}
      * @param snapshotId the snapshot id to load the index metadata from
      * @param index      the {@link IndexId} to load the metadata from
      * @return the index metadata about the given index for the given snapshot

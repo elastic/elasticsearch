d1b40000f9f8f608d039f0c6bc32721b6b875091
Przemysław Witek <przemyslaw.witek@elastic.co>
2019-12-19 16:07:09 +0100

    Implement `precision` and `recall` metrics for classification evaluation (#49671)

--- client/rest-high-level/src/test/java/org/elasticsearch/client/ml/dataframe/evaluation/classification/RecallMetricTests.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/ml/dataframe/evaluation/classification/RecallMetricTests.java
@@ -18,53 +18,36 @@
  */
 package org.elasticsearch.client.ml.dataframe.evaluation.classification;
 
-import org.elasticsearch.client.ml.dataframe.evaluation.EvaluationMetric;
 import org.elasticsearch.client.ml.dataframe.evaluation.MlEvaluationNamedXContentProvider;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.test.AbstractXContentTestCase;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Predicate;
 
-public class ClassificationTests extends AbstractXContentTestCase<Classification> {
+public class RecallMetricTests extends AbstractXContentTestCase<RecallMetric> {
 
     @Override
     protected NamedXContentRegistry xContentRegistry() {
         return new NamedXContentRegistry(new MlEvaluationNamedXContentProvider().getNamedXContentParsers());
     }
 
-    static Classification createRandom() {
-        List<EvaluationMetric> metrics =
-            randomSubsetOf(
-                Arrays.asList(
-                    AccuracyMetricTests.createRandom(),
-                    PrecisionMetricTests.createRandom(),
-                    RecallMetricTests.createRandom(),
-                    MulticlassConfusionMatrixMetricTests.createRandom()));
-        return new Classification(randomAlphaOfLength(10), randomAlphaOfLength(10), metrics.isEmpty() ? null : metrics);
+    static RecallMetric createRandom() {
+        return new RecallMetric();
     }
 
     @Override
-    protected Classification createTestInstance() {
+    protected RecallMetric createTestInstance() {
         return createRandom();
     }
 
     @Override
-    protected Classification doParseInstance(XContentParser parser) throws IOException {
-        return Classification.fromXContent(parser);
+    protected RecallMetric doParseInstance(XContentParser parser) throws IOException {
+        return RecallMetric.fromXContent(parser);
     }
 
     @Override
     protected boolean supportsUnknownFields() {
         return true;
-    }
-
-    @Override
-    protected Predicate<String> getRandomFieldsExcludeFilter() {
-        // allow unknown fields in the root of the object only
-        return field -> !field.isEmpty();
     }
 }

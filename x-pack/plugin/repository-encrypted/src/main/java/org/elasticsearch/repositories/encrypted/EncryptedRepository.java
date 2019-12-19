bae9a8d5786efe30b7f90b9819e66da5b4a2a730
Albert Zaharovits <albert.zaharovits@elastic.co>
2019-12-19 13:38:34 +0200

    Repository and plugin, WIP

--- x-pack/plugin/repository-encrypted/src/main/java/org/elasticsearch/repositories/encrypted/EncryptedRepository.java
+++ x-pack/plugin/repository-encrypted/src/main/java/org/elasticsearch/repositories/encrypted/EncryptedRepository.java
@@ -6,11 +6,40 @@
 
 package org.elasticsearch.repositories.encrypted;
 
-public class EncryptedRepository {
+import org.elasticsearch.cluster.metadata.RepositoryMetaData;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.blobstore.BlobPath;
+import org.elasticsearch.common.blobstore.BlobStore;
+import org.elasticsearch.common.settings.SecureSetting;
+import org.elasticsearch.common.settings.SecureString;
+import org.elasticsearch.common.settings.Setting;
+import org.elasticsearch.common.xcontent.NamedXContentRegistry;
+import org.elasticsearch.repositories.blobstore.BlobStoreRepository;
+
+import java.util.function.Function;
+
+public class EncryptedRepository extends BlobStoreRepository {
     static final int GCM_TAG_SIZE_IN_BYTES = 16;
     static final int GCM_IV_SIZE_IN_BYTES = 12;
     static final int AES_BLOCK_SIZE_IN_BYTES = 128;
     static final String GCM_ENCRYPTION_SCHEME = "AES/GCM/NoPadding";
     static final long PACKET_START_COUNTER = Long.MIN_VALUE;
     static final int MAX_PACKET_LENGTH_IN_BYTES = 1 << 30;
+
+    private static final String ENCRYPTION_METADATA_PREFIX = "encryption-metadata-";
+
+    private final BlobStoreRepository delegatedRepository;
+    private final char[] masterPassword;
+
+    protected EncryptedRepository(RepositoryMetaData metadata, NamedXContentRegistry namedXContentRegistry, ClusterService clusterService
+            , BlobStoreRepository delegatedRepository, char[] materPassword) {
+        super(metadata, namedXContentRegistry, clusterService, delegatedRepository.basePath());
+        this.delegatedRepository = delegatedRepository;
+        this.masterPassword = materPassword;
+    }
+
+    @Override
+    protected BlobStore createBlobStore() throws Exception {
+        return null;
+    }
 }
--- x-pack/plugin/repository-encrypted/src/main/java/org/elasticsearch/repositories/encrypted/EncryptedRepositoryPlugin.java
+++ x-pack/plugin/repository-encrypted/src/main/java/org/elasticsearch/repositories/encrypted/EncryptedRepositoryPlugin.java
@@ -6,26 +6,86 @@
 
 package org.elasticsearch.repositories.encrypted;
 
+import org.elasticsearch.cluster.metadata.RepositoryMetaData;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.settings.SecureSetting;
+import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.NamedXContentRegistry;
+import org.elasticsearch.env.Environment;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.plugins.ReloadablePlugin;
 import org.elasticsearch.plugins.RepositoryPlugin;
+import org.elasticsearch.repositories.Repository;
+import org.elasticsearch.repositories.blobstore.BlobStoreRepository;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
-public final class EncryptedRepositoryPlugin extends Plugin implements RepositoryPlugin, ReloadablePlugin {
+public final class EncryptedRepositoryPlugin extends Plugin implements RepositoryPlugin {
 
-    public EncryptedRepositoryPlugin(final Settings settings) {
+    static final Setting.AffixSetting<SecureString> ENCRYPTION_PASSWORD_SETTING = Setting.affixKeySetting("repository.encrypted.",
+            "password", key -> SecureSetting.secureString(key, null));
+    static final Setting<String> DELEGATE_TYPE = new Setting<>("delegate_type", "", Function.identity());
+    static final String TYPE = "encrypted";
+
+    private final Map<String, char[]> cachedRepositoryPasswords = new HashMap<>();
+
+    public EncryptedRepositoryPlugin(Settings settings) {
+        // cache the passwords for all encrypted repositories during *plugin* instantiation
+        // the keystore-based secure passwords are not readable on repository instantiation
+        for (String repositoryName : ENCRYPTION_PASSWORD_SETTING.getNamespaces(settings)) {
+            Setting<SecureString> encryptionPasswordSetting = ENCRYPTION_PASSWORD_SETTING
+                    .getConcreteSettingForNamespace(repositoryName);
+            SecureString encryptionPassword = encryptionPasswordSetting.get(settings);
+            cachedRepositoryPasswords.put(repositoryName, encryptionPassword.getChars());
+        }
     }
 
     @Override
     public List<Setting<?>> getSettings() {
-        return List.of();
+        return List.of(ENCRYPTION_PASSWORD_SETTING);
     }
 
     @Override
-    public void reload(Settings settings) {
-        // Secure settings should be readable inside this method.
+    public Map<String, Repository.Factory> getRepositories(final Environment env, final NamedXContentRegistry registry,
+                                                           final ClusterService clusterService) {
+        return Collections.singletonMap(TYPE, new Repository.Factory() {
+
+            @Override
+            public Repository create(RepositoryMetaData metadata) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public Repository create(RepositoryMetaData metaData, Function<String, Repository.Factory> typeLookup) throws Exception {
+                String delegateType = DELEGATE_TYPE.get(metaData.settings());
+                if (Strings.hasLength(delegateType) == false) {
+                    throw new IllegalArgumentException(DELEGATE_TYPE.getKey() + " must be set");
+                }
+                if (TYPE.equals(delegateType)) {
+                    throw new IllegalArgumentException("Cannot encrypt an already encrypted repository. " + DELEGATE_TYPE.getKey() +
+                            " must not be equal to " + TYPE);
+                }
+                if (false == cachedRepositoryPasswords.containsKey(metaData.name())) {
+                    throw new IllegalArgumentException(
+                            ENCRYPTION_PASSWORD_SETTING.getConcreteSettingForNamespace(metaData.name()).getKey() + " must be set");
+                }
+                Repository.Factory factory = typeLookup.apply(delegateType);
+                Repository delegatedRepository = factory.create(new RepositoryMetaData(metaData.name(),
+                        delegateType, metaData.settings()));
+                if (false == (delegatedRepository instanceof BlobStoreRepository) || delegatedRepository instanceof EncryptedRepository) {
+                    throw new IllegalArgumentException("Unsupported type " + DELEGATE_TYPE.getKey());
+                }
+                char[] masterPassword = cachedRepositoryPasswords.get(metaData.name());
+                return new EncryptedRepository(metaData, registry, clusterService, (BlobStoreRepository) delegatedRepository,
+                        masterPassword);
+            }
+        });
     }
 }

9038168520f43e096af9aaadbfd29b82bfbc34c3
Martijn van Groningen <martijn.v.groningen@gmail.com>
2019-12-24 11:45:34 +0100

    Revert "Revert "Add remote info to the HLRC (#49657)""
    This reverts commit b7ac7324d23c54d942b1445e3c277abd986e9735.

--- server/src/main/java/org/elasticsearch/transport/ProxyConnectionStrategy.java
+++ server/src/main/java/org/elasticsearch/transport/ProxyConnectionStrategy.java
@@ -268,13 +268,13 @@ public class ProxyConnectionStrategy extends RemoteConnectionStrategy {
         return new TransportAddress(parseConfiguredAddress(address));
     }
 
-    static class ProxyModeInfo implements RemoteConnectionInfo.ModeInfo {
+    public static class ProxyModeInfo implements RemoteConnectionInfo.ModeInfo {
 
         private final String address;
         private final int maxSocketConnections;
         private final int numSocketsConnected;
 
-        ProxyModeInfo(String address, int maxSocketConnections, int numSocketsConnected) {
+        public ProxyModeInfo(String address, int maxSocketConnections, int numSocketsConnected) {
             this.address = address;
             this.maxSocketConnections = maxSocketConnections;
             this.numSocketsConnected = numSocketsConnected;
@@ -309,6 +309,18 @@ public class ProxyConnectionStrategy extends RemoteConnectionStrategy {
         @Override
         public String modeName() {
             return "proxy";
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public int getMaxSocketConnections() {
+            return maxSocketConnections;
+        }
+
+        public int getNumSocketsConnected() {
+            return numSocketsConnected;
         }
 
         @Override

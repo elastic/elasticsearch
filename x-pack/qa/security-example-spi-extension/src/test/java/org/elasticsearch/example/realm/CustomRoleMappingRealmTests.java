e132cccd523f174be7b0f6718d9e36f0756fd455
Tim Vernum <tim@adjective.org>
2019-12-31 15:34:15 +1100

    Fix unchecked warnings

--- x-pack/qa/security-example-spi-extension/src/test/java/org/elasticsearch/example/realm/CustomRoleMappingRealmTests.java
+++ x-pack/qa/security-example-spi-extension/src/test/java/org/elasticsearch/example/realm/CustomRoleMappingRealmTests.java
@@ -7,26 +7,18 @@ package org.elasticsearch.example.realm;
 
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.PlainActionFuture;
-import org.elasticsearch.common.settings.SecureString;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.env.Environment;
-import org.elasticsearch.env.TestEnvironment;
 import org.elasticsearch.test.ESTestCase;
-import org.elasticsearch.xpack.core.security.authc.AuthenticationResult;
 import org.elasticsearch.xpack.core.security.authc.RealmConfig;
 import org.elasticsearch.xpack.core.security.authc.support.UserRoleMapper;
-import org.elasticsearch.xpack.core.security.authc.support.UsernamePasswordToken;
 import org.elasticsearch.xpack.core.security.user.User;
-import org.hamcrest.Matchers;
-import org.mockito.Mockito;
 
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 
 import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.mockito.Matchers.any;
@@ -45,12 +37,10 @@ public class CustomRoleMappingRealmTests extends ESTestCase {
         CustomRoleMappingRealm realm = new CustomRoleMappingRealm(realmConfig, roleMapper);
 
         final AtomicInteger roleMappingCounter = new AtomicInteger(0);
-        doAnswer(inv -> {
-            ActionListener<Set<String>> listener = (ActionListener<Set<String>>) inv.getArguments()[1];
+        mockRoleMapping(roleMapper, () -> {
             roleMappingCounter.incrementAndGet();
-            listener.onResponse(Set.of("role1", "role2"));
-            return null;
-        }).when(roleMapper).resolveRoles(any(UserRoleMapper.UserData.class), any(ActionListener.class));
+            return Set.of("role1", "role2");
+        });
 
         PlainActionFuture<User> future = new PlainActionFuture<>();
         realm.lookupUser(CustomRoleMappingRealm.USERNAME, future);
@@ -64,6 +54,15 @@ public class CustomRoleMappingRealmTests extends ESTestCase {
         final User user2 = future.get();
         assertThat(user2, sameInstance(user1));
         assertThat(roleMappingCounter.get(), is(1));
+    }
+
+    @SuppressWarnings("unchecked")
+    private void mockRoleMapping(UserRoleMapper roleMapper, Supplier<Set<String>> supplier) {
+        doAnswer(inv -> {
+            ActionListener<Set<String>> listener = (ActionListener<Set<String>>) inv.getArguments()[1];
+            listener.onResponse(supplier.get());
+            return null;
+        }).when(roleMapper).resolveRoles(any(UserRoleMapper.UserData.class), any(ActionListener.class));
     }
 
 }

95da5cb0db5008174298d6ff3980246b2426f209
Nik Everett <nik9000@gmail.com>
2020-01-03 13:08:39 -0500

    use 8.0.0

--- server/src/main/java/org/elasticsearch/search/aggregations/bucket/composite/DateHistogramValuesSourceBuilder.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/bucket/composite/DateHistogramValuesSourceBuilder.java
@@ -91,7 +91,7 @@ public class DateHistogramValuesSourceBuilder
         super(in);
         dateHistogramInterval = new DateIntervalWrapper(in);
         timeZone = in.readOptionalZoneId();
-        if (in.getVersion().onOrAfter(Version.V_7_6_0)) {
+        if (in.getVersion().onOrAfter(Version.V_8_0_0)) {
             offset = in.readLong();
         }
     }
@@ -100,7 +100,7 @@ public class DateHistogramValuesSourceBuilder
     protected void innerWriteTo(StreamOutput out) throws IOException {
         dateHistogramInterval.writeTo(out);
         out.writeOptionalZoneId(timeZone);
-        if (out.getVersion().onOrAfter(Version.V_7_6_0)) {
+        if (out.getVersion().onOrAfter(Version.V_8_0_0)) {
             out.writeLong(offset);
         }
     }

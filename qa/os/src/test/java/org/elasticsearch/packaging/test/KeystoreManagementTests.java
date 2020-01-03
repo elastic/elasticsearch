a433e13fa5aa97432f810e885317eaebfc68a878
William Brafford <william.brafford@elastic.co>
2020-01-03 13:42:56 -0500

    Remove unused import

--- qa/os/src/test/java/org/elasticsearch/packaging/test/KeystoreManagementTests.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/test/KeystoreManagementTests.java
@@ -50,7 +50,6 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assume.assumeFalse;
 import static org.junit.Assume.assumeThat;
 import static org.junit.Assume.assumeTrue;
 

7fd84a03a0fca7f61901bf557ab47c79deba4030
Nik Everett <nik9000@gmail.com>
2020-01-06 16:34:07 -0500

    Drop references to deprecated logger (#50474) (#50681)
    This drops all remaining references to `BaseRestHandler.logger` which
    has been deprecated for something like a year now. I replaced all of the
    references with locally declared loggers which is so much less spooky
    action at a distance to me.

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/rest/action/saml/RestSamlAuthenticateAction.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/rest/action/saml/RestSamlAuthenticateAction.java
@@ -6,6 +6,7 @@
 package org.elasticsearch.xpack.security.rest.action.saml;
 
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.common.ParseField;
 import org.elasticsearch.common.Strings;
@@ -36,9 +37,9 @@ import static org.elasticsearch.rest.RestRequest.Method.POST;
  * A REST handler that attempts to authenticate a user based on the provided SAML response/assertion.
  */
 public class RestSamlAuthenticateAction extends SamlBaseRestHandler implements RestHandler {
+    private static final Logger logger = LogManager.getLogger();
+    private static final DeprecationLogger deprecationLogger = new DeprecationLogger(logger);
 
-    private static final DeprecationLogger deprecationLogger =
-        new DeprecationLogger(LogManager.getLogger(RestSamlAuthenticateAction.class));
     static class Input {
         String content;
         List<String> ids;

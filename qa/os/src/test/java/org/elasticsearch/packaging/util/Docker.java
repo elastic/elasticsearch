77d38a6ac610668c4d561f1532f0598e3bdddd35
William Brafford <william.brafford@elastic.co>
2020-01-10 11:30:13 -0500

    Test password-protected keystore with Docker (#50803)
    This commit adds two tests for the case where we mount a
    password-protected keystore into a Docker container and provide a
    password via a Docker environment variable.
    
    We also fix a logging bug where we were logging the identifier for an
    array of strings rather than the contents of that array.

--- qa/os/src/test/java/org/elasticsearch/packaging/util/Docker.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/util/Docker.java
@@ -502,6 +502,13 @@ public class Docker {
         }
     }
 
+    /**
+     * @return The ID of the container that this class will be operating on.
+     */
+    public static String getContainerId() {
+        return containerId;
+    }
+
     public static JsonNode getJson(String path) throws Exception {
         final String pluginsResponse = makeRequest(Request.Get("http://localhost:9200/" + path));
 

c63d440b4910cf6d224ffb795f7e838124e1694b
Costin Leau <costin@users.noreply.github.com>
2020-01-10 13:43:49 +0200

    Introduce reusable QL plugin for SQL and EQL (#50815)
    Extract reusable functionality from SQL into its own dedicated project QL.
    Implemented as a plugin, it provides common components across SQL and the upcoming EQL.
    
    While this commit is fairly large, for the most part it's just a big file move from sql package to the newly introduced ql.

---
+++ x-pack/plugin/ql/src/main/java/org/elasticsearch/xpack/ql/expression/Foldables.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+package org.elasticsearch.xpack.ql.expression;
+
+import org.elasticsearch.xpack.ql.QlIllegalArgumentException;
+import org.elasticsearch.xpack.ql.type.DataType;
+import org.elasticsearch.xpack.ql.type.DataTypeConversion;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public abstract class Foldables {
+
+    @SuppressWarnings("unchecked")
+    public static <T> T valueOf(Expression e, DataType to) {
+        if (e.foldable()) {
+            return (T) DataTypeConversion.conversionFor(e.dataType(), to).convert(e.fold());
+        }
+        throw new QlIllegalArgumentException("Cannot determine value for {}", e);
+    }
+
+    public static Object valueOf(Expression e) {
+        if (e.foldable()) {
+            return e.fold();
+        }
+        throw new QlIllegalArgumentException("Cannot determine value for {}", e);
+    }
+
+    public static Integer intValueOf(Expression e) {
+        return valueOf(e, DataType.INTEGER);
+    }
+
+    public static double doubleValueOf(Expression e) {
+        return valueOf(e, DataType.DOUBLE);
+    }
+
+    public static <T> List<T> valuesOf(List<Expression> list, DataType to) {
+        return foldTo(list, to, new ArrayList<>(list.size()));
+    }
+
+    public static <T> Set<T> valuesOfNoDuplicates(List<Expression> list, DataType to) {
+        return foldTo(list, to, new LinkedHashSet<>(list.size()));
+    }
+
+    private static <T, C extends Collection<T>> C foldTo(Collection<Expression> expressions, DataType to, C values) {
+        for (Expression e : expressions) {
+            values.add(valueOf(e, to));
+        }
+        return values;
+    }
+
+    public static List<Double> doubleValuesOf(List<Expression> list) {
+        return valuesOf(list, DataType.DOUBLE);
+    }
+}
--- x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/expression/function/aggregate/TopHits.java
+++ x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/expression/function/aggregate/TopHits.java
@@ -5,21 +5,23 @@
  */
 package org.elasticsearch.xpack.sql.expression.function.aggregate;
 
-import org.elasticsearch.xpack.sql.expression.Expression;
-import org.elasticsearch.xpack.sql.expression.TypeResolutions;
-import org.elasticsearch.xpack.sql.tree.Source;
-import org.elasticsearch.xpack.sql.type.DataType;
+import org.elasticsearch.xpack.ql.expression.Expression;
+import org.elasticsearch.xpack.ql.expression.Expressions.ParamOrdinal;
+import org.elasticsearch.xpack.ql.expression.TypeResolutions;
+import org.elasticsearch.xpack.ql.expression.function.OptionalArgument;
+import org.elasticsearch.xpack.ql.expression.function.aggregate.AggregateFunction;
+import org.elasticsearch.xpack.ql.tree.Source;
+import org.elasticsearch.xpack.ql.type.DataType;
 
 import java.util.Collections;
 
-import static org.elasticsearch.xpack.sql.expression.Expressions.ParamOrdinal;
-import static org.elasticsearch.xpack.sql.expression.TypeResolutions.isNotFoldable;
+import static org.elasticsearch.xpack.ql.expression.TypeResolutions.isNotFoldable;
 
 /**
  * Super class of Aggregation functions on field types other than numeric, that need to be
  * translated into an ES {@link org.elasticsearch.search.aggregations.metrics.TopHits} aggregation.
  */
-public abstract class TopHits extends AggregateFunction {
+public abstract class TopHits extends AggregateFunction implements OptionalArgument {
 
     TopHits(Source source, Expression field, Expression sortField) {
         super(source, field, sortField != null ? Collections.singletonList(sortField) : Collections.emptyList());

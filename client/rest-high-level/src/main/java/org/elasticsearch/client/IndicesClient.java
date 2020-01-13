e9e209deb1bf7ee047eadfc23febc91df1a9b6ee
Nhat Nguyen <nhat.nguyen@elastic.co>
2020-01-13 10:09:37 -0500

    Remove in 8.0

--- client/rest-high-level/src/main/java/org/elasticsearch/client/IndicesClient.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/IndicesClient.java
@@ -601,7 +601,7 @@ public final class IndicesClient {
      * @param options the request options (e.g. headers), use {@link RequestOptions#DEFAULT} if nothing needs to be customized
      * @return the response
      * @throws IOException in case there is a problem sending the request or parsing back the response
-     * @deprecated synced flush is deprecated and will be removed in a future version.
+     * @deprecated synced flush is deprecated and will be removed in 8.0.
      * Use {@link #flush(FlushRequest, RequestOptions)} instead.
      */
     @Deprecated
@@ -618,7 +618,7 @@ public final class IndicesClient {
      * @param options the request options (e.g. headers), use {@link RequestOptions#DEFAULT} if nothing needs to be customized
      * @param listener the listener to be notified upon request completion
      * @return cancellable that may be used to cancel the request
-     * @deprecated synced flush is deprecated and will be removed in a future version.
+     * @deprecated synced flush is deprecated and will be removed in 8.0.
      * Use {@link #flushAsync(FlushRequest, RequestOptions, ActionListener)} instead.
      */
     @Deprecated

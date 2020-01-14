927a52bbeb95507c01b1d4ea2fe76eb655ae8c51
James Baiera <james.baiera@gmail.com>
2020-01-13 19:53:52 -0500

    Add a stats tracker for enrich policy executions

---
+++ x-pack/plugin/enrich/src/main/java/org/elasticsearch/xpack/enrich/EnrichPolicyExecutionStatsTracker.java
@@ -0,0 +1,70 @@
+package org.elasticsearch.xpack.enrich;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.elasticsearch.xpack.core.enrich.action.EnrichStatsAction.Response.ExecutionStats;
+
+/**
+ * Tracker object for enrich policy execution stats.
+ *
+ * The execution stats are tracked externally from the {@link EnrichPolicyExecutor} so that they may be shared across transport actions
+ * on the master node. The executor's dependencies make it difficult to share via dependency injection.
+ */
+public class EnrichPolicyExecutionStatsTracker {
+
+    private final AtomicLong totalExecutionCount = new AtomicLong(0L);
+    private final AtomicLong totalExecutionTime = new AtomicLong(0L);
+    private final AtomicLong minExecutionTime = new AtomicLong(Long.MAX_VALUE);
+    private final AtomicLong maxExecutionTime = new AtomicLong(0L);
+    private final AtomicLong totalRepeatExecutionCount = new AtomicLong(0L);
+    private final AtomicLong totalTimeBetweenExecutions = new AtomicLong(0L);
+    private final AtomicLong minTimeBetweenExecutions = new AtomicLong(Long.MAX_VALUE);
+    private final AtomicLong maxTimeBetweenExecutions = new AtomicLong(0L);
+    private final ConcurrentMap<String, Long> previousExecutionCompletionTimes = new ConcurrentHashMap<>();
+
+    void captureCompletedExecution(String policyName, long startTime, long endTime) {
+        long duration = Math.max(0, endTime - startTime);
+        totalExecutionCount.incrementAndGet();
+        totalExecutionTime.addAndGet(duration);
+        minExecutionTime.accumulateAndGet(duration, Math::min);
+        maxExecutionTime.accumulateAndGet(duration, Math::max);
+        Long lastExec = previousExecutionCompletionTimes.get(policyName);
+        if (lastExec != null) {
+            long sinceLastRun = Math.max(0, startTime - lastExec);
+            totalRepeatExecutionCount.incrementAndGet();
+            totalTimeBetweenExecutions.addAndGet(sinceLastRun);
+            minTimeBetweenExecutions.accumulateAndGet(sinceLastRun, Math::min);
+            maxTimeBetweenExecutions.accumulateAndGet(sinceLastRun, Math::max);
+        }
+        previousExecutionCompletionTimes.put(policyName, endTime);
+    }
+
+    public ExecutionStats getStats() {
+        // Snap min times to -1 if they do not have values set.
+        long minExecTime = minExecutionTime.get();
+        if (Long.MAX_VALUE == minExecTime) {
+            minExecTime = -1L;
+        }
+        long minTimeBetweenExecs = minTimeBetweenExecutions.get();
+        if (Long.MAX_VALUE == minTimeBetweenExecs) {
+            minTimeBetweenExecs = -1L;
+        }
+        // Calculate average safely
+        long totalRepeatExecutions = totalRepeatExecutionCount.get();
+        long totalTimeBetweenExecs = totalTimeBetweenExecutions.get();
+        long averageTimeBetweenExecs = totalRepeatExecutions == 0 ? 0 : totalTimeBetweenExecs / totalRepeatExecutions;
+        return new ExecutionStats(
+            totalExecutionCount.get(),
+            totalExecutionTime.get(),
+            minExecTime,
+            maxExecutionTime.get(),
+            totalRepeatExecutions,
+            totalTimeBetweenExecs,
+            averageTimeBetweenExecs,
+            minTimeBetweenExecs,
+            maxTimeBetweenExecutions.get()
+        );
+    }
+}
---
+++ x-pack/plugin/enrich/src/test/java/org/elasticsearch/xpack/enrich/EnrichPolicyExecutionStatsTrackerTest.java
@@ -0,0 +1,96 @@
+package org.elasticsearch.xpack.enrich;
+
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.xpack.core.enrich.action.EnrichStatsAction;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EnrichPolicyExecutionStatsTrackerTest extends ESTestCase {
+
+    public void testEmptyStats() {
+        EnrichPolicyExecutionStatsTracker tracker = new EnrichPolicyExecutionStatsTracker();
+        EnrichStatsAction.Response.ExecutionStats stats = tracker.getStats();
+        assertNotNull(stats);
+        assertEquals(0L, stats.getTotalExecutionCount());
+        assertEquals(0L, stats.getTotalExecutionTime());
+        assertEquals(-1L, stats.getMinExecutionTime());
+        assertEquals(0L, stats.getMaxExecutionTime());
+        assertEquals(0L, stats.getTotalRepeatExecutionCount());
+        assertEquals(0L, stats.getTotalTimeBetweenExecutions());
+        assertEquals(0L, stats.getAvgTimeBetweenExecutions());
+        assertEquals(-1L, stats.getMinTimeBetweenExecutions());
+        assertEquals(0L, stats.getMaxTimeBetweenExecutions());
+    }
+
+    public void testSingleRunStat() {
+        EnrichPolicyExecutionStatsTracker tracker = new EnrichPolicyExecutionStatsTracker();
+        tracker.captureCompletedExecution("test", 1000, 1025);
+        EnrichStatsAction.Response.ExecutionStats stats = tracker.getStats();
+        assertNotNull(stats);
+        assertEquals(1L, stats.getTotalExecutionCount());
+        assertEquals(25L, stats.getTotalExecutionTime());
+        assertEquals(25L, stats.getMinExecutionTime());
+        assertEquals(25L, stats.getMaxExecutionTime());
+        assertEquals(0L, stats.getTotalRepeatExecutionCount());
+        assertEquals(0L, stats.getTotalTimeBetweenExecutions());
+        assertEquals(0L, stats.getAvgTimeBetweenExecutions());
+        assertEquals(-1L, stats.getMinTimeBetweenExecutions());
+        assertEquals(0L, stats.getMaxTimeBetweenExecutions());
+    }
+
+    public void testUniqueRunStats() {
+        EnrichPolicyExecutionStatsTracker tracker = new EnrichPolicyExecutionStatsTracker();
+        tracker.captureCompletedExecution("test1", 1000, 1025);
+        tracker.captureCompletedExecution("test2", 1050, 1100);
+        EnrichStatsAction.Response.ExecutionStats stats = tracker.getStats();
+        assertNotNull(stats);
+        assertEquals(2L, stats.getTotalExecutionCount());
+        assertEquals(75L, stats.getTotalExecutionTime());
+        assertEquals(25L, stats.getMinExecutionTime());
+        assertEquals(50L, stats.getMaxExecutionTime());
+        assertEquals(0L, stats.getTotalRepeatExecutionCount());
+        assertEquals(0L, stats.getTotalTimeBetweenExecutions());
+        assertEquals(0L, stats.getAvgTimeBetweenExecutions());
+        assertEquals(-1L, stats.getMinTimeBetweenExecutions());
+        assertEquals(0L, stats.getMaxTimeBetweenExecutions());
+    }
+
+    public void testMultipleRunStats() {
+        EnrichPolicyExecutionStatsTracker tracker = new EnrichPolicyExecutionStatsTracker();
+        tracker.captureCompletedExecution("test", 1000, 1050);
+        tracker.captureCompletedExecution("test", 1100, 1150);
+        EnrichStatsAction.Response.ExecutionStats stats = tracker.getStats();
+        assertNotNull(stats);
+        assertEquals(2L, stats.getTotalExecutionCount());
+        assertEquals(100L, stats.getTotalExecutionTime());
+        assertEquals(50L, stats.getMinExecutionTime());
+        assertEquals(50L, stats.getMaxExecutionTime());
+        assertEquals(1L, stats.getTotalRepeatExecutionCount());
+        assertEquals(50L, stats.getTotalTimeBetweenExecutions());
+        assertEquals(50L, stats.getAvgTimeBetweenExecutions());
+        assertEquals(50L, stats.getMinTimeBetweenExecutions());
+        assertEquals(50L, stats.getMaxTimeBetweenExecutions());
+    }
+
+    public void testRepeatedRunStats() {
+        EnrichPolicyExecutionStatsTracker tracker = new EnrichPolicyExecutionStatsTracker();
+        tracker.captureCompletedExecution("test", 1000, 1050);
+        tracker.captureCompletedExecution("test", 1075, 1125);
+        tracker.captureCompletedExecution("test", 1200, 1250);
+        tracker.captureCompletedExecution("test", 1300, 1350);
+        tracker.captureCompletedExecution("test", 1400, 1450);
+        EnrichStatsAction.Response.ExecutionStats stats = tracker.getStats();
+        assertNotNull(stats);
+        assertEquals(5L, stats.getTotalExecutionCount());
+        assertEquals(250L, stats.getTotalExecutionTime());
+        assertEquals(25L, stats.getMinExecutionTime());
+        assertEquals(75L, stats.getMaxExecutionTime());
+        assertEquals(4L, stats.getTotalRepeatExecutionCount());
+        assertEquals(400L, stats.getTotalTimeBetweenExecutions());
+        assertEquals(100L, stats.getAvgTimeBetweenExecutions());
+        assertEquals(25L, stats.getMinTimeBetweenExecutions());
+        assertEquals(75L, stats.getMaxTimeBetweenExecutions());
+    }
+
+}

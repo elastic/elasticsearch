9025578d4fb3b94aefb2b37671e699abff1df3f8
djptek <dominic.page@elastic.co>
2020-01-14 15:03:34 +0100

    refactor

--- server/src/test/java/org/elasticsearch/search/geo/GeoPointShapeQueryTests.java
+++ server/src/test/java/org/elasticsearch/search/geo/GeoPointShapeQueryTests.java
@@ -19,16 +19,13 @@
 
 package org.elasticsearch.search.geo;
 
-import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
-
-import java.io.IOException;
 
 public class GeoPointShapeQueryTests extends GeoQueryTests {
 
     @Override
-    protected XContentBuilder createMapping() throws Exception {
+    protected XContentBuilder createDefaultMapping() throws Exception {
         XContentBuilder xcb = XContentFactory.jsonBuilder().startObject()
             .startObject("properties").startObject("location")
             .field("type", "geo_point")
@@ -37,57 +34,8 @@ public class GeoPointShapeQueryTests extends GeoQueryTests {
         return xcb;
     }
 
-    public void testNullShape() throws Exception {
-        super.testNullShape();
-    }
-
     public void testIndexPointsFilterRectangle() throws Exception {
-        //super.testIndexPointsFilterRectangle(Strings.toString(createMapping()));
-    }
-    public void testIndexedShapeReference() throws Exception {
-        //super.testIndexedShapeReference(Strings.toString(createMapping()));
-    }
-
-    public void testShapeFetchingPath() throws Exception {
-        //super.testShapeFetchingPath();
-    }
-
-    public void testQueryRandomGeoCollection() throws Exception {
-        //super.testQueryRandomGeoCollection(Strings.toString(createMapping()));
-    }
-
-    public void testRandomGeoCollectionQuery() throws Exception {
-        //super.testRandomGeoCollectionQuery();
-    }
-
-    public void testShapeFilterWithDefinedGeoCollection() throws Exception {
-        //super.testShapeFilterWithDefinedGeoCollection(Strings.toString(createMapping()));
-    }
-
-    public void testFieldAlias() throws IOException {
-        /* super.testFieldAlias(XContentFactory.jsonBuilder()
-            .startObject()
-            .startObject("type")
-            .startObject("properties")
-            .startObject("location")
-            .field("type", "geo_point")
-            .endObject()
-            .startObject("alias")
-            .field("type", "alias")
-            .field("path", "location")
-            .endObject()
-            .endObject()
-            .endObject()
-            .endObject()); */
-    }
-
-    // Test for issue #34418
-    public void testEnvelopeSpanningDateline() throws Exception {
-        //super.testEnvelopeSpanningDateline();
-    }
-
-    public void testGeometryCollectionRelations() throws Exception {
-        //super.testGeometryCollectionRelations();
+        //super.testIndexPointsFilterRectangle(Strings.toString(createDefaultMapping()));
     }
 
 }

7bd6d9a8c8dd9352398b3b740b965def1327b179
Przemko Robakowski <przemko.robakowski@elastic.co>
2020-01-15 13:33:48 +0100

    Spelling fix

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/AsyncWaitStep.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/AsyncWaitStep.java
@@ -43,7 +43,7 @@ public abstract class AsyncWaitStep extends Step {
 
     public interface Listener {
 
-        void onResponse(boolean conditionMet, ToXContentObject infomationContext);
+        void onResponse(boolean conditionMet, ToXContentObject informationContext);
 
         void onFailure(Exception e);
     }

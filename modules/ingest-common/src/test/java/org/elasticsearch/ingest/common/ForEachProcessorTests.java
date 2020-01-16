bf8fdc201bf3497005cf291da1b40848f96c3a65
Przemko Robakowski <przemko.robakowski@elastic.co>
2020-01-16 21:53:36 +0100

    Unused import removed

--- modules/ingest-common/src/test/java/org/elasticsearch/ingest/common/ForEachProcessorTests.java
+++ modules/ingest-common/src/test/java/org/elasticsearch/ingest/common/ForEachProcessorTests.java
@@ -19,7 +19,6 @@
 
 package org.elasticsearch.ingest.common;
 
-import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.ingest.CompoundProcessor;
 import org.elasticsearch.ingest.IngestDocument;
 import org.elasticsearch.ingest.Processor;

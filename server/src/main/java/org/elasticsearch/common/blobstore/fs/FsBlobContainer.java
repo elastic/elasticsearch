c75a2a00069208c68b4ebb124680b1d126731471
Yannick Welsch <yannick@welsch.lu>
2020-01-16 19:41:03 +0100

    Add ranged readBlob to S3BlobContainer

--- server/src/main/java/org/elasticsearch/common/blobstore/fs/FsBlobContainer.java
+++ server/src/main/java/org/elasticsearch/common/blobstore/fs/FsBlobContainer.java
@@ -154,7 +154,7 @@ public class FsBlobContainer extends AbstractBlobContainer {
 
     @Override
     public InputStream readBlob(String blobName, long position, int length) throws IOException {
-        final InputStream inputStream = readBlob(blobName);
+        final InputStream inputStream = readBlob(blobName); // TODO: limit the consumable size of the input stream here to length
         long skipped = inputStream.skip(position); // NORELEASE
         assert skipped == position;
         return inputStream;

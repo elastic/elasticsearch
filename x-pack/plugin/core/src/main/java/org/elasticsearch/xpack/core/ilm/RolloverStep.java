9d3c67f65d76ebb3231a735f7b716dbf81996f95
Przemko Robakowski <przemko.robakowski@elastic.co>
2020-01-16 22:11:23 +0100

    Unused imports removed

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/RolloverStep.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/RolloverStep.java
@@ -14,8 +14,6 @@ import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.ClusterStateObserver;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.common.Strings;
-import org.elasticsearch.common.settings.ClusterSettings;
-import org.elasticsearch.common.unit.TimeValue;
 
 import java.util.Locale;
 import java.util.Objects;

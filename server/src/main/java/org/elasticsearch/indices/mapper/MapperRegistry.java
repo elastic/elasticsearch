89c13b7bcbebf1470a345659fa2477546aa59c3c
Alan Woodward <romseygeek@apache.org>
2020-01-16 14:42:13 +0000

    Don't add nested field mapper for pre-8 indexes, as this breaks mapping updates due to the _type field being overloaded

--- server/src/main/java/org/elasticsearch/indices/mapper/MapperRegistry.java
+++ server/src/main/java/org/elasticsearch/indices/mapper/MapperRegistry.java
@@ -23,6 +23,7 @@ import org.elasticsearch.Version;
 import org.elasticsearch.index.mapper.AllFieldMapper;
 import org.elasticsearch.index.mapper.Mapper;
 import org.elasticsearch.index.mapper.MetadataFieldMapper;
+import org.elasticsearch.index.mapper.NestedPathFieldMapper;
 import org.elasticsearch.plugins.MapperPlugin;
 
 import java.util.Collections;
@@ -38,6 +39,7 @@ public final class MapperRegistry {
 
     private final Map<String, Mapper.TypeParser> mapperParsers;
     private final Map<String, MetadataFieldMapper.TypeParser> metadataMapperParsers;
+    private final Map<String, MetadataFieldMapper.TypeParser> metadataMapperParsers7x;
     private final Map<String, MetadataFieldMapper.TypeParser> metadataMapperParsers6x;
     private final Function<String, Predicate<String>> fieldFilter;
 
@@ -51,6 +53,9 @@ public final class MapperRegistry {
         metadata6x.put(AllFieldMapper.NAME, new AllFieldMapper.TypeParser());
         metadata6x.putAll(metadataMapperParsers);
         this.metadataMapperParsers6x = Collections.unmodifiableMap(metadata6x);
+        Map<String, MetadataFieldMapper.TypeParser> metadata7x = new LinkedHashMap<>(metadataMapperParsers);
+        metadata7x.remove(NestedPathFieldMapper.NAME);
+        this.metadataMapperParsers7x = metadata7x;
         this.fieldFilter = fieldFilter;
     }
 
@@ -67,7 +72,13 @@ public final class MapperRegistry {
      * returned map uses the name of the field as a key.
      */
     public Map<String, MetadataFieldMapper.TypeParser> getMetadataMapperParsers(Version indexCreatedVersion) {
-        return indexCreatedVersion.onOrAfter(Version.V_7_0_0) ? metadataMapperParsers : metadataMapperParsers6x;
+        if (indexCreatedVersion.onOrAfter(Version.V_8_0_0)) {
+            return metadataMapperParsers;
+        }
+        if (indexCreatedVersion.onOrAfter(Version.V_7_0_0)) {
+            return metadataMapperParsers7x;
+        }
+        return metadataMapperParsers6x;
     }
 
     /**

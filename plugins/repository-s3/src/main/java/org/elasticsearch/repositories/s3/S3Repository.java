f9047d7738f432dea04cd21efb1128d1686fb1be
Armin Braun <me@obrown.io>
2020-01-16 22:53:20 +0100

    test + docs

--- plugins/repository-s3/src/main/java/org/elasticsearch/repositories/s3/S3Repository.java
+++ plugins/repository-s3/src/main/java/org/elasticsearch/repositories/s3/S3Repository.java
@@ -40,6 +40,7 @@ import org.elasticsearch.repositories.blobstore.BlobStoreRepository;
 import org.elasticsearch.snapshots.SnapshotId;
 import org.elasticsearch.snapshots.SnapshotInfo;
 import org.elasticsearch.snapshots.SnapshotShardFailure;
+import org.elasticsearch.snapshots.SnapshotsService;
 import org.elasticsearch.threadpool.Scheduler;
 import org.elasticsearch.threadpool.ThreadPool;
 
@@ -142,9 +143,13 @@ class S3Repository extends BlobStoreRepository {
     /**
      * Artificial delay to introduce after a snapshot finalization or delete has finished so long as the repository is still using the
      * backwards compatible snapshot format from before
-     * {@link org.elasticsearch.snapshots.SnapshotsService#SHARD_GEN_IN_REPO_DATA_VERSION}.
+     * {@link org.elasticsearch.snapshots.SnapshotsService#SHARD_GEN_IN_REPO_DATA_VERSION} ({@link org.elasticsearch.Version#V_7_6_0}).
      * This delay is necessary so that the eventually consistent nature of AWS S3 does not randomly result in repository corruption when
-     * doing repository operations in rapid succession.
+     * doing repository operations in rapid succession on a repository in the old metadata format.
+     * This setting should not be adjusted in production when working with an AWS S3 backed repository. Doing so risks the repository
+     * becoming silently corrupted. To get rid of this waiting period, either create a new S3 repository or remove all snapshots older than
+     * {@link org.elasticsearch.Version#V_7_6_0} from the repository which will trigger an upgrade of the repository metadata to the new
+     * format and disable the cooldown period.
      */
     static final Setting<TimeValue> COOLDOWN_PERIOD = Setting.timeSetting(
         "cooldown_period",
@@ -258,6 +263,7 @@ class S3Repository extends BlobStoreRepository {
         return new ActionListener<>() {
             @Override
             public void onResponse(T response) {
+                logCooldownInfo();
                 final Scheduler.Cancellable existing = finalizationFuture.getAndSet(
                     threadPool.schedule(() -> wrappedListener.onResponse(response), coolDown, ThreadPool.Names.SNAPSHOT));
                 assert existing == null : "Already have an ongoing finalization " + finalizationFuture;
@@ -265,11 +271,21 @@ class S3Repository extends BlobStoreRepository {
 
             @Override
             public void onFailure(Exception e) {
+                logCooldownInfo();
                 final Scheduler.Cancellable existing = finalizationFuture.getAndSet(
                     threadPool.schedule(() -> wrappedListener.onFailure(e), coolDown, ThreadPool.Names.SNAPSHOT));
                 assert existing == null : "Already have an ongoing finalization " + finalizationFuture;
             }
         };
+    }
+
+    private void logCooldownInfo() {
+        logger.info("Sleeping for [{}] after modifying repository [{}] because it contains snapshots older than version [{}]" +
+                " and therefore is using a backwards compatible metadata format that requires this cooldown period to avoid " +
+                "repository corruption. To get rid of this message and move to the new repository metadata format, either remove " +
+                "all snapshots older than version [{}] from the repository or create a new repository at an empty location.",
+            coolDown, metadata.name(), SnapshotsService.SHARD_GEN_IN_REPO_DATA_VERSION,
+            SnapshotsService.SHARD_GEN_IN_REPO_DATA_VERSION);
     }
 
     private static BlobPath buildBasePath(RepositoryMetaData metadata) {

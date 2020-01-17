7a6ac3adbde94b4bb0e894776895ceba3caa906a
jaymode <jay@elastic.co>
2020-01-17 08:25:07 -0700

    restore CloseIndexRequestTests and fix them

--- server/src/test/java/org/elasticsearch/action/admin/indices/close/CloseIndexRequestTests.java
+++ server/src/test/java/org/elasticsearch/action/admin/indices/close/CloseIndexRequestTests.java
@@ -49,23 +49,68 @@ public class CloseIndexRequestTests extends ESTestCase {
     }
 
     public void testBwcSerialization() throws Exception {
-        final CloseIndexRequest request = randomRequest();
-        try (BytesStreamOutput out = new BytesStreamOutput()) {
-            out.setVersion(VersionUtils.randomCompatibleVersion(random(), Version.CURRENT));
-            request.writeTo(out);
+        {
+            final CloseIndexRequest request = randomRequest();
+            try (BytesStreamOutput out = new BytesStreamOutput()) {
+                out.setVersion(VersionUtils.randomCompatibleVersion(random(), Version.CURRENT));
+                request.writeTo(out);
 
-            try (StreamInput in = out.bytes().streamInput()) {
-                in.setVersion(out.getVersion());
-                assertEquals(request.getParentTask(), TaskId.readFromStream(in));
-                assertEquals(request.masterNodeTimeout(), in.readTimeValue());
-                assertEquals(request.timeout(), in.readTimeValue());
-                assertArrayEquals(request.indices(), in.readStringArray());
+                try (StreamInput in = out.bytes().streamInput()) {
+                    in.setVersion(out.getVersion());
+                    assertEquals(request.getParentTask(), TaskId.readFromStream(in));
+                    assertEquals(request.masterNodeTimeout(), in.readTimeValue());
+                    assertEquals(request.timeout(), in.readTimeValue());
+                    assertArrayEquals(request.indices(), in.readStringArray());
+                    final IndicesOptions indicesOptions = IndicesOptions.readIndicesOptions(in);
+                    // indices options are not equivalent when sent to an older version and re-read due
+                    // to the addition of hidden indices as expand to hidden indices is always true when
+                    // read from a prior version
+                    // TODO update version on backport!
+                    if (out.getVersion().onOrAfter(Version.V_8_0_0) || request.indicesOptions().expandWildcardsHidden()) {
+                        assertEquals(request.indicesOptions(), indicesOptions);
+                    }
+                    if (in.getVersion().onOrAfter(Version.V_7_2_0)) {
+                        assertEquals(request.waitForActiveShards(), ActiveShardCount.readFrom(in));
+                    } else {
+                        assertEquals(0, in.available());
+                    }
+                }
+            }
+        }
+        {
+            final CloseIndexRequest sample = randomRequest();
+            final Version version = VersionUtils.randomCompatibleVersion(random(), Version.CURRENT);
+            try (BytesStreamOutput out = new BytesStreamOutput()) {
+                out.setVersion(version);
+                sample.getParentTask().writeTo(out);
+                out.writeTimeValue(sample.masterNodeTimeout());
+                out.writeTimeValue(sample.timeout());
+                out.writeStringArray(sample.indices());
+                sample.indicesOptions().writeIndicesOptions(out);
+                if (out.getVersion().onOrAfter(Version.V_7_2_0)) {
+                    sample.waitForActiveShards().writeTo(out);
+                }
+
+                final CloseIndexRequest deserializedRequest;
+                try (StreamInput in = out.bytes().streamInput()) {
+                    in.setVersion(version);
+                    deserializedRequest = new CloseIndexRequest(in);
+                }
+                assertEquals(sample.getParentTask(), deserializedRequest.getParentTask());
+                assertEquals(sample.masterNodeTimeout(), deserializedRequest.masterNodeTimeout());
+                assertEquals(sample.timeout(), deserializedRequest.timeout());
+                assertArrayEquals(sample.indices(), deserializedRequest.indices());
                 // indices options are not equivalent when sent to an older version and re-read due
                 // to the addition of hidden indices as expand to hidden indices is always true when
                 // read from a prior version
-                // TODO update version on backport!
-                if (out.getVersion().onOrAfter(Version.V_8_0_0) || request.indicesOptions().expandWildcardsHidden()) {
-                    assertEquals(request.indicesOptions(), IndicesOptions.readIndicesOptions(in));
+                // TODO change version on backport
+                if (out.getVersion().onOrAfter(Version.V_8_0_0) || sample.indicesOptions().expandWildcardsHidden()) {
+                    assertEquals(sample.indicesOptions(), deserializedRequest.indicesOptions());
+                }
+                if (out.getVersion().onOrAfter(Version.V_7_2_0)) {
+                    assertEquals(sample.waitForActiveShards(), deserializedRequest.waitForActiveShards());
+                } else {
+                    assertEquals(ActiveShardCount.NONE, deserializedRequest.waitForActiveShards());
                 }
             }
         }

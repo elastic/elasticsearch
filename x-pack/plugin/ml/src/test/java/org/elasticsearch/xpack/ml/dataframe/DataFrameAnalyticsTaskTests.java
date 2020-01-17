72ab0bedd11b38ffafc0a1387e35ccee1371df10
Przemyslaw Witek <przemyslaw.witek@elastic.co>
2020-01-17 11:16:12 +0100

    Fix tests randomly failing on CI

--- x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/dataframe/DataFrameAnalyticsTaskTests.java
+++ x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/dataframe/DataFrameAnalyticsTaskTests.java
@@ -34,6 +34,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Matchers.any;
@@ -116,13 +117,13 @@ public class DataFrameAnalyticsTaskTests extends ESTestCase {
         assertThat(startingState, equalTo(StartingState.FINISHED));
     }
 
-    private void testMarkAsCompleted(SearchHits searchHits, String expectedIndexOrAlias) {
+    private void testPersistProgress(SearchHits searchHits, String expectedIndexOrAlias) {
         Client client = mock(Client.class);
         ThreadPool threadPool = mock(ThreadPool.class);
         when(threadPool.getThreadContext()).thenReturn(new ThreadContext(Settings.EMPTY));
         when(client.threadPool()).thenReturn(threadPool);
 
-        GetDataFrameAnalyticsStatsAction.Response getStatsResponse = GetDataFrameAnalyticsStatsActionResponseTests.randomResponse();
+        GetDataFrameAnalyticsStatsAction.Response getStatsResponse = GetDataFrameAnalyticsStatsActionResponseTests.randomResponse(1);
         doAnswer(withResponse(getStatsResponse)).when(client).execute(eq(GetDataFrameAnalyticsStatsAction.INSTANCE), any(), any());
 
         SearchResponse searchResponse = mock(SearchResponse.class);
@@ -132,27 +133,17 @@ public class DataFrameAnalyticsTaskTests extends ESTestCase {
         IndexResponse indexResponse = mock(IndexResponse.class);
         doAnswer(withResponse(indexResponse)).when(client).execute(eq(IndexAction.INSTANCE), any(), any());
 
-        TaskParams taskParams = new TaskParams("task_id", Version.CURRENT, Collections.emptyList(), false);
-        DataFrameAnalyticsTask task =
-            new DataFrameAnalyticsTask(
-                0,
-                "",
-                "",
-                null,
-                null,
-                client,
-                mock(ClusterService.class),
-                mock(DataFrameAnalyticsManager.class),
-                mock(DataFrameAnalyticsAuditor.class),
-                taskParams);
-        task.markAsCompleted();
+        Runnable runnable = mock(Runnable.class);
+
+        DataFrameAnalyticsTask.persistProgress(client, "task_id", runnable);
 
         ArgumentCaptor<IndexRequest> indexRequestCaptor = ArgumentCaptor.forClass(IndexRequest.class);
 
-        InOrder inOrder = inOrder(client);
+        InOrder inOrder = inOrder(client, runnable);
         inOrder.verify(client).execute(eq(GetDataFrameAnalyticsStatsAction.INSTANCE), any(), any());
         inOrder.verify(client).execute(eq(SearchAction.INSTANCE), any(), any());
         inOrder.verify(client).execute(eq(IndexAction.INSTANCE), indexRequestCaptor.capture(), any());
+        inOrder.verify(runnable).run();
         inOrder.verifyNoMoreInteractions();
 
         IndexRequest indexRequest = indexRequestCaptor.getValue();
@@ -160,12 +151,12 @@ public class DataFrameAnalyticsTaskTests extends ESTestCase {
         assertThat(indexRequest.id(), equalTo("data_frame_analytics-task_id-progress"));
     }
 
-    public void testMarkAsCompleted_ProgressDocumentCreated() {
-        testMarkAsCompleted(SearchHits.empty(), ".ml-state-write");
+    public void testPersistProgress_ProgressDocumentCreated() {
+        testPersistProgress(SearchHits.empty(), ".ml-state-write");
     }
 
-    public void testMarkAsCompleted_ProgressDocumentUpdated() {
-        testMarkAsCompleted(
+    public void testPersistProgress_ProgressDocumentUpdated() {
+        testPersistProgress(
             new SearchHits(new SearchHit[]{ SearchHit.createFromMap(Map.of("_index", ".ml-state-dummy")) }, null, 0.0f),
             ".ml-state-dummy");
     }

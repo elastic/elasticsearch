4f0fbf0dd733e743e726f055eddf2112721a146b
Paul Sanwald <paul.sanwald@elastic.co>
2020-01-21 09:01:55 -0500

    version bump for 7.5

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -127,7 +127,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_7_5_0 = new Version(7050099, org.apache.lucene.util.Version.LUCENE_8_3_0);
     public static final Version V_7_5_1 = new Version(7050199, org.apache.lucene.util.Version.LUCENE_8_3_0);
     public static final Version V_7_5_2 = new Version(7050299, org.apache.lucene.util.Version.LUCENE_8_3_0);
-    public static final Version CURRENT = V_7_5_2;
+    public static final Version V_7_5_3 = new Version(7050399, org.apache.lucene.util.Version.LUCENE_8_3_0);
+    public static final Version CURRENT = V_7_5_3;
 
     private static final ImmutableOpenIntMap<Version> idToVersion;
 

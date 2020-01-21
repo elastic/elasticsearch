820f07e77f57307b839c3cf4356ed56db2d5a796
Rory Hunter <roryhunter2@gmail.com>
2020-01-21 15:51:20 +0000

    Improve NodeDanglingIndicesResponse serialisation

--- server/src/main/java/org/elasticsearch/action/admin/indices/dangling/NodeDanglingIndicesResponse.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/dangling/NodeDanglingIndicesResponse.java
@@ -26,7 +26,6 @@ import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -46,22 +45,12 @@ public class NodeDanglingIndicesResponse extends BaseNodeResponse {
 
     protected NodeDanglingIndicesResponse(StreamInput in) throws IOException {
         super(in);
-
-        final int size = in.readInt();
-        this.indexMetaData = new ArrayList<>(size);
-
-        for (int i = 0; i < size; i++) {
-            this.indexMetaData.add(IndexMetaData.readFrom(in));
-        }
+        this.indexMetaData = in.readList(IndexMetaData::readFrom);
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         super.writeTo(out);
-
-        out.writeInt(this.indexMetaData.size());
-        for (IndexMetaData indexMetaData : this.indexMetaData) {
-            indexMetaData.writeTo(out);
-        }
+        out.writeList(this.indexMetaData);
     }
 }

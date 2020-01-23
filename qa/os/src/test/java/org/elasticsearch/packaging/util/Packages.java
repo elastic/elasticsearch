17a4324c3c2429456c16934a82258e0999300b6e
William Brafford <william.brafford@elastic.co>
2020-01-23 13:31:50 -0500

    Remove unused import

--- qa/os/src/test/java/org/elasticsearch/packaging/util/Packages.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/util/Packages.java
@@ -49,7 +49,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class Packages {
 

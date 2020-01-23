936ba917be88750fef115204406e67dfa28cfe9b
William Brafford <william.brafford@elastic.co>
2020-01-23 09:59:20 -0500

    Adjust clearing the journalctl logs

--- qa/os/src/test/java/org/elasticsearch/packaging/util/Packages.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/util/Packages.java
@@ -286,8 +286,7 @@ public class Packages {
      */
     public static void clearJournal(Shell sh) {
         if (isSystemd()) {
-            sh.run("systemctl stop systemd-journald");
-            sh.run("rm -rf /run/log/journal/* /var/log/journal/*");
+            sh.run("journalctl --flush && rm -rf /var/log/journal");
             final Result result = sh.runIgnoreExitCode("systemctl restart systemd-journald");
 
             // Sometimes the restart fails on Debian 10 with:

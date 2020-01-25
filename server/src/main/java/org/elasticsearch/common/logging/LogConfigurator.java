a156629b4ad3b965dfb32039b9ca5071937b927d
Ryan Ernst <ryan@iernst.net>
2020-01-25 11:13:33 -0800

    Fix backport compilation
    Java 8 doesn't have a PrintStream ctor which takes a Charset object,
    only a charset name. This fixes that.

--- server/src/main/java/org/elasticsearch/common/logging/LogConfigurator.java
+++ server/src/main/java/org/elasticsearch/common/logging/LogConfigurator.java
@@ -247,8 +247,8 @@ public class LogConfigurator {
 
         // Redirect stdout/stderr to log4j. While we ensure Elasticsearch code does not write to those streams,
         // third party libraries may do that
-        System.setOut(new PrintStream(new LoggingOutputStream(LogManager.getLogger("stdout"), Level.INFO), false, StandardCharsets.UTF_8));
-        System.setOut(new PrintStream(new LoggingOutputStream(LogManager.getLogger("stderr"), Level.WARN), false, StandardCharsets.UTF_8));
+        System.setOut(new PrintStream(new LoggingOutputStream(LogManager.getLogger("stdout"), Level.INFO), false, StandardCharsets.UTF_8.name()));
+        System.setOut(new PrintStream(new LoggingOutputStream(LogManager.getLogger("stderr"), Level.WARN), false, StandardCharsets.UTF_8.name()));
     }
 
     private static void configureStatusLogger() {

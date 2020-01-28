b7bfb1e305fc315e1cd8d063bbbcbb504b62416b
Costin Leau <costin.leau@gmail.com>
2020-01-28 20:03:41 +0200

    EQL: Introduce basic execution pipeline
    The main classes that form the 'execution' pipeline are added - most of
    them have no functionality; the purpose of this PR is to add flesh out
    the contract between the various moving parts so that work can start on
    them independently.

---
+++ x-pack/plugin/eql/src/main/java/org/elasticsearch/xpack/eql/analysis/Failure.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+package org.elasticsearch.xpack.eql.analysis;
+
+import org.elasticsearch.xpack.ql.tree.Node;
+
+import java.util.Objects;
+
+import static org.elasticsearch.common.logging.LoggerMessageFormat.format;
+
+class Failure {
+
+    private final Node<?> node;
+    private final String message;
+
+    Failure(Node<?> node, String message) {
+        this.node = node;
+        this.message = message;
+    }
+
+    Node<?> node() {
+        return node;
+    }
+
+    String message() {
+        return message;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(node);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Failure other = (Failure) obj;
+        return Objects.equals(node, other.node);
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+
+    static Failure fail(Node<?> source, String message, Object... args) {
+        return new Failure(source, format(message, args));
+    }
+}
--- x-pack/plugin/eql/src/main/java/org/elasticsearch/xpack/eql/plan/physical/PhysicalPlan.java
+++ x-pack/plugin/eql/src/main/java/org/elasticsearch/xpack/eql/plan/physical/PhysicalPlan.java
@@ -3,29 +3,24 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-package org.elasticsearch.xpack.sql.plan.physical;
+package org.elasticsearch.xpack.eql.plan.physical;
 
+import org.elasticsearch.xpack.eql.session.Executable;
 import org.elasticsearch.xpack.ql.plan.QueryPlan;
 import org.elasticsearch.xpack.ql.tree.Source;
-import org.elasticsearch.xpack.ql.type.Schema;
-import org.elasticsearch.xpack.sql.session.Executable;
-import org.elasticsearch.xpack.sql.session.Rows;
 
 import java.util.List;
 
+/**
+ * A PhysicalPlan is "how" a LogicalPlan (the "what") actually gets translated into one or more queries.
+ *
+ * LogicalPlan = I want to get from DEN to SFO
+ * PhysicalPlan = take Delta, DEN to SJC, then SJC to SFO
+ */
 public abstract class PhysicalPlan extends QueryPlan<PhysicalPlan> implements Executable {
-
-    private Schema lazySchema;
 
     public PhysicalPlan(Source source, List<PhysicalPlan> children) {
         super(source, children);
-    }
-
-    public Schema schema() {
-        if (lazySchema == null) {
-            lazySchema = Rows.schema(output());
-        }
-        return lazySchema;
     }
 
     @Override

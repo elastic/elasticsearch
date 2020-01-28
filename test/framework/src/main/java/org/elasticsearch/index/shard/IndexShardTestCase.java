e4b2d5d54347ff3e0b3a859bddca7b61d4531ced
Armin Braun <me@obrown.io>
2020-01-28 16:46:15 +0100

    Allow Repository Plugins to Filter Metadata on Create (#51472)
    * Allow Repository Plugins to Filter Metadata on Create
    
    Add a hook that allows repository plugins to filter the repository metadata
    before it gets written to the cluster state.

--- test/framework/src/main/java/org/elasticsearch/index/shard/IndexShardTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/index/shard/IndexShardTestCase.java
@@ -840,7 +840,7 @@ public abstract class IndexShardTestCase extends ESTestCase {
         final String shardGen;
         try (Engine.IndexCommitRef indexCommitRef = shard.acquireLastIndexCommit(true)) {
             repository.snapshotShard(shard.store(), shard.mapperService(), snapshot.getSnapshotId(), indexId,
-                indexCommitRef.getIndexCommit(), snapshotStatus, true, future);
+                indexCommitRef.getIndexCommit(), snapshotStatus, true, Collections.emptyMap(), future);
             shardGen = future.actionGet();
         }
 

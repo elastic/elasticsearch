78e3a644ee3e9738a2da57a5a6598f32c1bfa001
Jack Conradson <osjdconrad@gmail.com>
2020-01-29 16:50:35 -0800

    Clean up variable scoping in the user tree (#51584)
    Cleans up variable scoping in the user tree by renaming Locals to Scope, 
    removing variable slotting from the user tree (now part of the ir tree), and 
    removing specialization of parameters which individual user nodes are now 
    responsible for. The new scopes also track variable usage, so in a follow up 
    PR, the extractVariables pass will be removed as with a bit more work it's no 
    longer necessary.

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/node/PSubArrayLength.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/node/PSubArrayLength.java
@@ -19,8 +19,9 @@
 
 package org.elasticsearch.painless.node;
 
-import org.elasticsearch.painless.Locals;
+import org.elasticsearch.painless.Scope;
 import org.elasticsearch.painless.Location;
+import org.elasticsearch.painless.ir.ClassNode;
 import org.elasticsearch.painless.ir.DotSubArrayLengthNode;
 import org.elasticsearch.painless.symbol.ScriptRoot;
 
@@ -48,7 +49,7 @@ final class PSubArrayLength extends AStoreable {
     }
 
     @Override
-    void analyze(ScriptRoot scriptRoot, Locals locals) {
+    void analyze(ScriptRoot scriptRoot, Scope scope) {
         if ("length".equals(value)) {
             if (write) {
                 throw createError(new IllegalArgumentException("Cannot write to read-only field [length] for an array."));
@@ -61,7 +62,7 @@ final class PSubArrayLength extends AStoreable {
     }
 
     @Override
-    DotSubArrayLengthNode write() {
+    DotSubArrayLengthNode write(ClassNode classNode) {
         DotSubArrayLengthNode dotSubArrayLengthNode = new DotSubArrayLengthNode();
 
         dotSubArrayLengthNode.setLocation(location);

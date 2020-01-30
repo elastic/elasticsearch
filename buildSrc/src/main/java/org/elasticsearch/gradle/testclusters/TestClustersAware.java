8d2370bf00874fbe7804cbfeb9bba618bde5673c
Mark Vieira <portugee@gmail.com>
2020-01-30 14:35:43 -0800

    Always use bundled JDK for external cluster nodes when BWC testing (#51505) (#51701)

--- buildSrc/src/main/java/org/elasticsearch/gradle/testclusters/TestClustersAware.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/testclusters/TestClustersAware.java
@@ -1,9 +1,11 @@
 package org.elasticsearch.gradle.testclusters;
 
+import org.elasticsearch.gradle.Jdk;
 import org.gradle.api.Task;
 import org.gradle.api.tasks.Nested;
 
 import java.util.Collection;
+import java.util.concurrent.Callable;
 
 interface TestClustersAware extends Task {
 
@@ -15,7 +17,12 @@ interface TestClustersAware extends Task {
             throw new TestClustersException("Task " + getPath() + " can't use test cluster from" + " another project " + cluster);
         }
 
+        // Add configured distributions as task dependencies so they are built before starting the cluster
         cluster.getNodes().stream().flatMap(node -> node.getDistributions().stream()).forEach(distro -> dependsOn(distro.getExtracted()));
+
+        // Add legacy BWC JDK runtime as a dependency so it's downloaded before starting the cluster if necessary
+        cluster.getNodes().stream().map(node -> (Callable<Jdk>) node::getBwcJdk).forEach(this::dependsOn);
+
         getClusters().add(cluster);
     }
 

c5355428bd8bc1f750bfade9990b637f9403698a
Przemko Robakowski <przemko.robakowski@elastic.co>
2020-01-31 15:37:17 +0100

    removed additional file

--- x-pack/plugin/ilm/qa/multi-node/src/test/java/org/elasticsearch/xpack/ilm/a.js
+++
@@ -1,2 +0,0 @@
-phase_time_millis=1580403396594, action_time_millis=1580403395090, step_time_millis=1580403396594
-last_success     =1580403396386                     1580403396386                   1580403396386

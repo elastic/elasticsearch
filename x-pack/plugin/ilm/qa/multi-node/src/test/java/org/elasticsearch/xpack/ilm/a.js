a5fda6eab80e3207ab56a4287d18683fb6ba4c52
Przemko Robakowski <przemko.robakowski@elastic.co>
2020-01-31 15:34:22 +0100

    Change index.lifecycle.step.master_timeout to indices.lifecycle.step.master_timeout
    This changes setting name from `index.lifecycle.step.master_timeout` to
    `indices.lifecycle.step.master_timeout` to avoid confusion about its scope.
    `index.*` settings are recognized as index level settings, this one is node level.
    
    Reletes to #51698

---
+++ x-pack/plugin/ilm/qa/multi-node/src/test/java/org/elasticsearch/xpack/ilm/a.js
@@ -0,0 +1,2 @@
+phase_time_millis=1580403396594, action_time_millis=1580403395090, step_time_millis=1580403396594
+last_success     =1580403396386                     1580403396386                   1580403396386

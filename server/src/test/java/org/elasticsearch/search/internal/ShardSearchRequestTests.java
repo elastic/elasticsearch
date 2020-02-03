a0581270a25c6a11182a5eae9ec2cdeec39e2a07
jimczi <jim.ferenczi@elastic.co>
2020-02-03 11:52:09 +0100

    fix test

--- server/src/test/java/org/elasticsearch/search/internal/ShardSearchRequestTests.java
+++ server/src/test/java/org/elasticsearch/search/internal/ShardSearchRequestTests.java
@@ -75,8 +75,8 @@ public class ShardSearchRequestTests extends AbstractSearchTestCase {
         assertEquals(deserializedRequest.indexBoost(), shardSearchTransportRequest.indexBoost(), 0.0f);
         assertEquals(deserializedRequest.getClusterAlias(), shardSearchTransportRequest.getClusterAlias());
         assertEquals(shardSearchTransportRequest.allowPartialSearchResults(), deserializedRequest.allowPartialSearchResults());
-        assertEquals(deserializedRequest.isMatchNoDocsReturnNullResponse(),
-            shardSearchTransportRequest.isMatchNoDocsReturnNullResponse());
+        assertEquals(deserializedRequest.canReturnNullResponseIfMatchNoDocs(),
+            shardSearchTransportRequest.canReturnNullResponseIfMatchNoDocs());
     }
 
     private ShardSearchRequest createShardSearchRequest() throws IOException {
@@ -93,7 +93,7 @@ public class ShardSearchRequestTests extends AbstractSearchTestCase {
         ShardSearchRequest req = new ShardSearchRequest(new OriginalIndices(searchRequest), searchRequest, shardId,
             randomIntBetween(1, 100), filteringAliases, randomBoolean() ? 1.0f : randomFloat(),
             Math.abs(randomLong()), randomAlphaOfLengthBetween(3, 10), routings);
-        req.setMatchNoDocsReturnNullResponse(randomBoolean());
+        req.canReturnNullResponseIfMatchNoDocs(randomBoolean());
         return req;
     }
 

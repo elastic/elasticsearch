d293980a09d58dead747656864c61f0bd9d14972
Benjamin Trent <ben.w.trent@gmail.com>
2020-02-03 17:16:33 -0500

    [7.x] [ML] add GET _cat/ml/datafeeds (#51500) (#51829)
    * [ML] add GET _cat/ml/datafeeds (#51500)
    
    This adds GET _cat/ml/datafeeds && _cat/ml/datafeeds/{datafeed_id}
    
    * fixing for java8 compilation

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/MachineLearning.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/MachineLearning.java
@@ -254,6 +254,7 @@ import org.elasticsearch.xpack.ml.rest.calendar.RestGetCalendarsAction;
 import org.elasticsearch.xpack.ml.rest.calendar.RestPostCalendarEventAction;
 import org.elasticsearch.xpack.ml.rest.calendar.RestPutCalendarAction;
 import org.elasticsearch.xpack.ml.rest.calendar.RestPutCalendarJobAction;
+import org.elasticsearch.xpack.ml.rest.cat.RestCatDatafeedsAction;
 import org.elasticsearch.xpack.ml.rest.cat.RestCatJobsAction;
 import org.elasticsearch.xpack.ml.rest.datafeeds.RestDeleteDatafeedAction;
 import org.elasticsearch.xpack.ml.rest.datafeeds.RestGetDatafeedStatsAction;
@@ -784,7 +785,8 @@ public class MachineLearning extends Plugin implements SystemIndexPlugin, Analys
             new RestGetTrainedModelsStatsAction(restController),
             new RestPutTrainedModelAction(restController),
             // CAT Handlers
-            new RestCatJobsAction(restController)
+            new RestCatJobsAction(restController),
+            new RestCatDatafeedsAction(restController)
         );
     }
 

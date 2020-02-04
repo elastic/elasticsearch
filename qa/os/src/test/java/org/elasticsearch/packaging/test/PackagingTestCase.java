a841a6a290632d3be09faf55e9c68996495a1730
Ioannis Kakavas <ioannis@elastic.co>
2020-02-04 12:18:39 +0200

    checkstyle

--- qa/os/src/test/java/org/elasticsearch/packaging/test/PackagingTestCase.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/test/PackagingTestCase.java
@@ -50,7 +50,6 @@ import org.junit.runner.RunWith;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 

7923833367e316d6fb6f94aff97f4eccd729d399
jimczi <jim.ferenczi@elastic.co>
2020-02-06 11:59:19 +0100

    Adapt missing version after backport (bis)
    Relates #51708

--- server/src/main/java/org/elasticsearch/search/internal/ShardSearchRequest.java
+++ server/src/main/java/org/elasticsearch/search/internal/ShardSearchRequest.java
@@ -154,7 +154,7 @@ public class ShardSearchRequest extends TransportRequest implements IndicesReque
         numberOfShards = in.readVInt();
         scroll = in.readOptionalWriteable(Scroll::new);
         source = in.readOptionalWriteable(SearchSourceBuilder::new);
-        if (in.getVersion().before(Version.V_7_7_0)) {
+        if (in.getVersion().before(Version.V_8_0_0)) {
             // types no longer relevant so ignore
             String[] types = in.readStringArray();
             if (types.length > 0) {

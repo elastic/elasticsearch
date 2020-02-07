d893b690a42de87f13ab0b9715e328f2e44c601b
Jason Tedor <jason@tedor.me>
2020-02-06 19:15:17 -0500

    Add autoscaling API skelton (#51564)
    The main purpose of this commit is to add a single autoscaling REST
    endpoint skeleton, for the purpose of starting to build out the build
    and testing infrastructure that will surround it. For example, rather
    than commiting a fully-functioning autoscaling API, we introduce here
    the skeleton so that we can start wiring up the build and testing
    infrastructure, establish security roles/permissions, an so on. This
    way, in a forthcoming PR that introduces actual functionality, that PR
    will be smaller and have less distractions around that sort of
    infrastructure.

--- x-pack/plugin/autoscaling/src/main/java/org/elasticsearch/xpack/autoscaling/Autoscaling.java
+++ x-pack/plugin/autoscaling/src/main/java/org/elasticsearch/xpack/autoscaling/Autoscaling.java
@@ -7,22 +7,43 @@
 package org.elasticsearch.xpack.autoscaling;
 
 import org.elasticsearch.Build;
+import org.elasticsearch.action.ActionRequest;
+import org.elasticsearch.action.ActionResponse;
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.IndexScopedSettings;
 import org.elasticsearch.common.settings.Setting;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsFilter;
+import org.elasticsearch.plugins.ActionPlugin;
 import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestHandler;
+import org.elasticsearch.xpack.autoscaling.action.GetAutoscalingDecisionAction;
+import org.elasticsearch.xpack.autoscaling.action.TransportGetAutoscalingDecisionAction;
+import org.elasticsearch.xpack.autoscaling.rest.RestGetAutoscalingDecisionHandler;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * Container class for autoscaling functionality.
  */
-public class Autoscaling extends Plugin {
+public class Autoscaling extends Plugin implements ActionPlugin {
 
     public static final Setting<Boolean> AUTOSCALING_ENABLED_SETTING = Setting.boolSetting(
         "xpack.autoscaling.enabled",
         false,
         Setting.Property.NodeScope
     );
+
+    private final boolean enabled;
+
+    public Autoscaling(final Settings settings) {
+        this.enabled = AUTOSCALING_ENABLED_SETTING.get(settings);
+    }
 
     /**
      * The settings defined by autoscaling.
@@ -33,6 +54,34 @@ public class Autoscaling extends Plugin {
     public List<Setting<?>> getSettings() {
         if (isSnapshot()) {
             return Collections.singletonList(AUTOSCALING_ENABLED_SETTING);
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public List<ActionHandler<? extends ActionRequest, ? extends ActionResponse>> getActions() {
+        if (enabled) {
+            return Collections.singletonList(
+                new ActionHandler<>(GetAutoscalingDecisionAction.INSTANCE, TransportGetAutoscalingDecisionAction.class)
+            );
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public List<RestHandler> getRestHandlers(
+        final Settings settings,
+        final RestController controller,
+        final ClusterSettings clusterSettings,
+        final IndexScopedSettings indexScopedSettings,
+        final SettingsFilter settingsFilter,
+        final IndexNameExpressionResolver indexNameExpressionResolver,
+        final Supplier<DiscoveryNodes> nodesInCluster
+    ) {
+        if (enabled) {
+            return Collections.singletonList(new RestGetAutoscalingDecisionHandler(controller));
         } else {
             return Collections.emptyList();
         }

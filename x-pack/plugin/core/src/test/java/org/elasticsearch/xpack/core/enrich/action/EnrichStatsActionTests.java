fe9854135c4b7df18ea825a148ba036b387db3df
Yang Wang <yang.wang@elastic.co>
2020-02-07 18:19:59 +1100

    Add tests for changed action privileges

---
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/enrich/action/EnrichStatsActionTests.java
@@ -0,0 +1,21 @@
+/*
+ *
+ *  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ *  * or more contributor license agreements. Licensed under the Elastic License;
+ *  * you may not use this file except in compliance with the Elastic License.
+ *
+ */
+
+package org.elasticsearch.xpack.core.enrich.action;
+
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.xpack.core.security.authz.permission.ClusterPermission;
+import org.elasticsearch.xpack.core.security.authz.privilege.ClusterPrivilegeResolver;
+
+public class EnrichStatsActionTests extends ESTestCase {
+
+    public void testMonitorPrivilegeWillAllowAction() {
+        final ClusterPermission clusterPermission = ClusterPrivilegeResolver.MONITOR.buildPermission(ClusterPermission.builder()).build();
+        assertTrue(clusterPermission.check(EnrichStatsAction.NAME, null, null));
+    }
+}

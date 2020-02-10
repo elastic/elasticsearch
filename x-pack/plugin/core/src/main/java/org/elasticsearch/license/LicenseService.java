9d1164c191b3a5b27484df2ad95cf079c858521e
Yang Wang <yang.wang@elastic.co>
2020-02-10 14:13:56 +1100

    Fix fips license test

--- x-pack/plugin/core/src/main/java/org/elasticsearch/license/LicenseService.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/license/LicenseService.java
@@ -255,7 +255,7 @@ public class LicenseService extends AbstractLifecycleComponent implements Cluste
                     throw new IllegalStateException("Cannot install a [" + newLicense.operationMode() +
                         "] license unless TLS is configured or security is disabled");
                 } else if (XPackSettings.FIPS_MODE_ENABLED.get(settings)
-                    && XPackLicenseState.isFipsAllowedForOperationMode(newLicense.operationMode())) {
+                    && false == XPackLicenseState.isFipsAllowedForOperationMode(newLicense.operationMode())) {
                     throw new IllegalStateException("Cannot install a [" + newLicense.operationMode() +
                         "] license unless FIPS mode is disabled");
                 }

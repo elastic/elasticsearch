6e804bca2ee35297f5937bf2edd9ac9f8d8bbb8c
Zachary Tong <zach@elastic.co>
2020-02-10 11:26:48 -0500

    checkstyle

--- server/src/main/java/org/elasticsearch/search/aggregations/metrics/PercentilesConfig.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/metrics/PercentilesConfig.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.elasticsearch.search.aggregations.metrics;
 
 import org.elasticsearch.common.io.stream.StreamInput;
@@ -52,7 +71,8 @@ public abstract class PercentilesConfig implements ToXContent, Writeable {
 
     abstract Aggregator createPercentilesAggregator(String name, ValuesSource valuesSource, SearchContext context, Aggregator parent,
                                                     double[] values, boolean keyed, DocValueFormat formatter,
-                                                    List<PipelineAggregator> pipelineAggregators, Map<String, Object> metaData) throws IOException;
+                                                    List<PipelineAggregator> pipelineAggregators,
+                                                    Map<String, Object> metaData) throws IOException;
 
     abstract Aggregator createPercentileRanksAggregator(String name, ValuesSource valuesSource, SearchContext context,
                                                         Aggregator parent, double[] values, boolean keyed,

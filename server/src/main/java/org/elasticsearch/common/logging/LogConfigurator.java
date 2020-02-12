82bb8c228cf1f39147d4afc40461355975652923
Ryan Ernst <ryan@iernst.net>
2020-02-12 14:07:38 -0800

    try another

--- server/src/main/java/org/elasticsearch/common/logging/LogConfigurator.java
+++ server/src/main/java/org/elasticsearch/common/logging/LogConfigurator.java
@@ -246,21 +246,10 @@ public class LogConfigurator {
         }
 
         // Redirect stdout/stderr to log4j. While we ensure Elasticsearch code does not write to those streams,
-        // third party libraries may do that
-        closeSysOut();
+        // third party libraries may do that. Note that we do NOT close the streams because other code may have
+        // grabbed a handle to the streams and intend to write to it, eg log4j for writing to the console
         System.setOut(new PrintStream(new LoggingOutputStream(LogManager.getLogger("stdout"), Level.INFO), false, StandardCharsets.UTF_8));
-        closeSysError();
         System.setErr(new PrintStream(new LoggingOutputStream(LogManager.getLogger("stderr"), Level.WARN), false, StandardCharsets.UTF_8));
-    }
-
-    @SuppressForbidden(reason = "System#out")
-    private static void closeSysOut() {
-        System.out.close();
-    }
-
-    @SuppressForbidden(reason = "System#err")
-    private static void closeSysError() {
-        System.err.close();
     }
 
     private static void configureStatusLogger() {

3369668e2f66fb6f269e719468b7c59aa9ac243f
Ross Wolf <31489089+rw-access@users.noreply.github.com>
2020-02-13 09:27:53 -0700

    Add escape tests and formatting changes

--- x-pack/plugin/eql/src/test/java/org/elasticsearch/xpack/eql/parser/ParameterTests.java
+++ x-pack/plugin/eql/src/test/java/org/elasticsearch/xpack/eql/parser/ParameterTests.java
@@ -9,33 +9,12 @@ package org.elasticsearch.xpack.eql.parser;
 
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.xpack.ql.expression.Expression;
-import org.elasticsearch.xpack.ql.expression.Literal;
-import org.elasticsearch.xpack.ql.expression.UnresolvedAttribute;
-import org.elasticsearch.xpack.ql.expression.function.UnresolvedFunction;
-import org.elasticsearch.xpack.ql.expression.predicate.logical.And;
-import org.elasticsearch.xpack.ql.expression.predicate.logical.Or;
-import org.elasticsearch.xpack.ql.expression.predicate.operator.arithmetic.Neg;
-import org.elasticsearch.xpack.ql.expression.predicate.operator.comparison.Equals;
-import org.elasticsearch.xpack.ql.expression.predicate.operator.comparison.GreaterThan;
-import org.elasticsearch.xpack.ql.expression.predicate.operator.comparison.GreaterThanOrEqual;
-import org.elasticsearch.xpack.ql.expression.predicate.operator.comparison.LessThan;
-import org.elasticsearch.xpack.ql.expression.predicate.operator.comparison.LessThanOrEqual;
-import org.elasticsearch.xpack.ql.expression.predicate.operator.comparison.NotEquals;
-import org.elasticsearch.xpack.ql.type.DataTypes;
-import org.elasticsearch.xpack.sql.proto.SqlTypedParamValue;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.elasticsearch.xpack.eql.parser.AbstractBuilder.unquoteString;
-import static org.elasticsearch.xpack.ql.type.DataTypes.KEYWORD;
-import static org.elasticsearch.xpack.ql.type.DataTypes.LONG;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 
 public class ParameterTests extends ESTestCase {
 
@@ -44,6 +23,7 @@ public class ParameterTests extends ESTestCase {
     public Expression expr(String source) {
         return parser.createExpression(source);
     }
+
     public Expression expr(String source, List<Object> params) {
         return parser.createExpression(source, params);
     }
@@ -57,11 +37,18 @@ public class ParameterTests extends ESTestCase {
         assertEquals(expr("a == ?", Collections.singletonList(null)), expr("a == null"));
     }
 
+    public void testEscapes() {
+        assertEquals(expr("a == ?", Collections.singletonList("test \\ \n \r \t ' \"")),
+                     expr("a == 'test \\\\ \\n \\r \\t \\' \\\"'"));
+    }
+
     public void testInvalidParameters() {
         ParsingException ex = expectThrows(ParsingException.class,
-            () -> new EqlParser().createExpression("a + ?", Collections.singletonList(new Object())));
+            () -> expr("a + ?", Collections.singletonList(new Object())));
+
         assertThat(ex.getMessage(), containsString("Invalid parameter"));
     }
+
     public void testMultipleParameters() {
         Expression expression = expr("(? + ? * ?) - ?", Arrays.asList(1, 2, 3, 4));
         assertEquals(expression, expr("(1 + 2 * 3) - 4"));
@@ -69,7 +56,8 @@ public class ParameterTests extends ESTestCase {
 
     public void testNotEnoughParameters() {
         ParsingException ex = expectThrows(ParsingException.class,
-            () -> new EqlParser().createExpression("(? + ? * ?) - ?", Arrays.asList(1, 2, 3)));
+            () -> expr("(? + ? * ?) - ?", Arrays.asList(1, 2, 3)));
+
         assertThat(ex.getMessage(), containsString("Not enough actual parameters"));
     }
 }

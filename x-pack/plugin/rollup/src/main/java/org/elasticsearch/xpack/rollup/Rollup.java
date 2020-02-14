a9afdd7611bc3c3efdb257546933de55d123fe07
Yannick Welsch <yannick@welsch.lu>
2020-02-14 16:20:40 +0100

    Remove fixed_auto_queue_size threadpool type (#52280)
    * Remove fixed_auto_queue_size threadpool type
    
    * Remove less
    
    * compilation fix
    
    * weaken assertion to accomodate tests that mock threadpool

--- x-pack/plugin/rollup/src/main/java/org/elasticsearch/xpack/rollup/Rollup.java
+++ x-pack/plugin/rollup/src/main/java/org/elasticsearch/xpack/rollup/Rollup.java
@@ -158,7 +158,7 @@ public class Rollup extends Plugin implements ActionPlugin, PersistentTaskPlugin
         }
 
         FixedExecutorBuilder indexing = new FixedExecutorBuilder(settings, Rollup.TASK_THREAD_POOL_NAME,
-                4, 4, "xpack.rollup.task_thread_pool");
+                4, 4, "xpack.rollup.task_thread_pool", false);
 
         return Collections.singletonList(indexing);
     }

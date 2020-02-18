540ebec8c57f6bbdab6eda382d16d394acd47c5b
Yang Wang <yang.wang@elastic.co>
2020-02-19 00:53:04 +1100

    yeah checkstyle

--- x-pack/plugin/core/src/main/java/org/elasticsearch/license/LicenseService.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/license/LicenseService.java
@@ -45,7 +45,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;

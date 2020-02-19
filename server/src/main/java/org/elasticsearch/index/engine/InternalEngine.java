f3b5bd951b0426dac0c7e8c6284b4b841832d1f3
Adrien Grand <jpountz@gmail.com>
2020-02-19 16:01:39 +0100

    Move the terms index of `_id` off-heap. (#52405)
    In #42838 we moved the terms index of all fields off-heap except the
    `_id` field because we were worried it might make indexing slower. In
    general, the indexing rate is only affected if explicit IDs are used, as
    otherwise Elasticsearch almost never performs lookups in the terms
    dictionary for the purpose of indexing. So it's quite wasteful to
    require the terms index of `_id` to be loaded on-heap for users who have
    append-only workloads. Furthermore I've been conducting benchmarks when
    indexing with explicit ids on the http_logs dataset that suggest that
    the slowdown is low enough that it's probably not worth forcing the terms
    index to be kept on-heap. Here are some numbers for the median indexing
    rate in docs/s:
    
    | Run | Master  | Patch   |
    | --- | ------- | ------- |
    | 1   | 45851.2 | 46401.4 |
    | 2   | 45192.6 | 44561.0 |
    | 3   | 45635.2 | 44137.0 |
    | 4   | 46435.0 | 44692.8 |
    | 5   | 45829.0 | 44949.0 |
    
    And now heap usage in MB for segments:
    
    | Run | Master  | Patch    |
    | --- | ------- | -------- |
    | 1   | 41.1720 | 0.352083 |
    | 2   | 45.1545 | 0.382534 |
    | 3   | 41.7746 | 0.381285 |
    | 4   | 45.3673 | 0.412737 |
    | 5   | 45.4616 | 0.375063 |
    
    Indexing rate decreased by 1.8% on average, while memory usage decreased
    by more than 100x.
    
    The `http_logs` dataset contains small documents and has a simple
    indexing chain. More complex indexing chains, e.g. with more fields,
    ingest pipelines, etc. would see an even lower decrease of indexing rate.

--- server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
+++ server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -102,6 +102,7 @@ import org.elasticsearch.threadpool.ThreadPool;
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -2074,21 +2075,24 @@ public class InternalEngine extends Engine {
         }
     }
 
-    static Map<String, String> getReaderAttributes(Directory directory) {
+    static Map<String, String> getReaderAttributes(Directory directory, IndexSettings indexSettings) {
         Directory unwrap = FilterDirectory.unwrap(directory);
         boolean defaultOffHeap = FsDirectoryFactory.isHybridFs(unwrap) || unwrap instanceof MMapDirectory;
-        return Map.of(
-            BlockTreeTermsReader.FST_MODE_KEY, // if we are using MMAP for term dics we force all off heap unless it's the ID field
-            defaultOffHeap ? FSTLoadMode.OFF_HEAP.name() : FSTLoadMode.ON_HEAP.name()
-            , BlockTreeTermsReader.FST_MODE_KEY + "." + IdFieldMapper.NAME, // always force ID field on-heap for fast updates
-            FSTLoadMode.ON_HEAP.name());
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put(BlockTreeTermsReader.FST_MODE_KEY, defaultOffHeap ? FSTLoadMode.OFF_HEAP.name() : FSTLoadMode.ON_HEAP.name());
+        if (IndexSettings.ON_HEAP_ID_TERMS_INDEX.exists(indexSettings.getSettings())) {
+            final boolean idOffHeap = IndexSettings.ON_HEAP_ID_TERMS_INDEX.get(indexSettings.getSettings()) == false;
+            attributes.put(BlockTreeTermsReader.FST_MODE_KEY + "." + IdFieldMapper.NAME,
+                    idOffHeap ? FSTLoadMode.OFF_HEAP.name() : FSTLoadMode.ON_HEAP.name());
+        }
+        return Collections.unmodifiableMap(attributes);
     }
 
     private IndexWriterConfig getIndexWriterConfig() {
         final IndexWriterConfig iwc = new IndexWriterConfig(engineConfig.getAnalyzer());
         iwc.setCommitOnClose(false); // we by default don't commit on close
         iwc.setOpenMode(IndexWriterConfig.OpenMode.APPEND);
-        iwc.setReaderAttributes(getReaderAttributes(store.directory()));
+        iwc.setReaderAttributes(getReaderAttributes(store.directory(), engineConfig.getIndexSettings()));
         iwc.setIndexDeletionPolicy(combinedDeletionPolicy);
         // with tests.verbose, lucene sets this up: plumb to align with filesystem stream
         boolean verbose = false;

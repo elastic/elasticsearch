a280ff566b8577c76da03d16ae6abbcbc04b0f6d
przemek <przemek.gomulka@gmail.com>
2020-02-20 18:08:36 +0100

    todos

--- server/src/main/java/org/elasticsearch/index/mapper/RangeFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/RangeFieldMapper.java
@@ -140,7 +140,7 @@ public class RangeFieldMapper extends FieldMapper {
 
                 if (hasPatternChanged || Objects.equals(builder.locale, formatter.locale()) == false) {
                     DateFormatter dateTimeFormatter;
-                    if (Joda.isJodaStyleIndex(context,pattern) ) {
+                    if (Joda.isJodaStyleIndex(context,pattern) ) { //TODO make sure it fails too..g
                         dateTimeFormatter = Joda.forPattern(pattern).withLocale(locale);
                     } else {
                         dateTimeFormatter = DateFormatter.forPattern(pattern).withLocale(locale);

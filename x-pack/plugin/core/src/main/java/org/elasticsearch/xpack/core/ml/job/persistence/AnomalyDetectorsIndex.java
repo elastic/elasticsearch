4b479528b05223d35fa699fc1642b7b32c0f6e36
Przemysław Witek <przemyslaw.witek@elastic.co>
2020-02-20 21:05:55 +0100

    Rename .ml-state index to .ml-state-000001 to support rollover (#52510)

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/job/persistence/AnomalyDetectorsIndex.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/job/persistence/AnomalyDetectorsIndex.java
@@ -22,7 +22,9 @@ import org.elasticsearch.xpack.core.ml.utils.ExceptionsHelper;
 import org.elasticsearch.xpack.core.template.TemplateUtils;
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Comparator;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import static org.elasticsearch.xpack.core.ClientHelper.ML_ORIGIN;
 import static org.elasticsearch.xpack.core.ClientHelper.executeAsyncWithOrigin;
@@ -36,6 +38,29 @@ public final class AnomalyDetectorsIndex {
 
     private static final String RESULTS_MAPPINGS_VERSION_VARIABLE = "xpack.ml.version";
     private static final String RESOURCE_PATH = "/org/elasticsearch/xpack/core/ml/anomalydetection/";
+
+    // Visible for testing
+    static final Comparator<String> STATE_INDEX_NAME_COMPARATOR = new Comparator<>() {
+
+        private final Predicate<String> HAS_SIX_DIGIT_SUFFIX = Pattern.compile("\\d{6}").asMatchPredicate();
+
+        @Override
+        public int compare(String index1, String index2) {
+            String[] index1Parts = index1.split("-");
+            String index1Suffix = index1Parts[index1Parts.length - 1];
+            boolean index1HasSixDigitsSuffix = HAS_SIX_DIGIT_SUFFIX.test(index1Suffix);
+            String[] index2Parts = index2.split("-");
+            String index2Suffix = index2Parts[index2Parts.length - 1];
+            boolean index2HasSixDigitsSuffix = HAS_SIX_DIGIT_SUFFIX.test(index2Suffix);
+            if (index1HasSixDigitsSuffix && index2HasSixDigitsSuffix) {
+                return index1Suffix.compareTo(index2Suffix);
+            } else if (index1HasSixDigitsSuffix != index2HasSixDigitsSuffix) {
+                return Boolean.compare(index1HasSixDigitsSuffix, index2HasSixDigitsSuffix);
+            } else {
+                return index1.compareTo(index2);
+            }
+        }
+    };
 
     private AnomalyDetectorsIndex() {
     }
@@ -90,8 +115,8 @@ public final class AnomalyDetectorsIndex {
     }
 
     /**
-     * Create the .ml-state index (if necessary)
-     * Create the .ml-state-write alias for the .ml-state index (if necessary)
+     * Creates the .ml-state-000001 index (if necessary)
+     * Creates the .ml-state-write alias for the .ml-state-000001 index (if necessary)
      */
     public static void createStateIndexAndAliasIfNecessary(Client client, ClusterState state, final ActionListener<Boolean> finalListener) {
 
@@ -123,12 +148,14 @@ public final class AnomalyDetectorsIndex {
             IndicesOptions.lenientExpandOpen(),
             jobStateIndexPattern());
         if (stateIndices.length > 0) {
-            Arrays.sort(stateIndices, Collections.reverseOrder());
-            createAliasListener.onResponse(stateIndices[0]);
+            String latestStateIndex = Arrays.stream(stateIndices).max(STATE_INDEX_NAME_COMPARATOR).get();
+            createAliasListener.onResponse(latestStateIndex);
         } else {
+            // The initial index name must be suitable for rollover functionality.
+            String initialJobStateIndex = AnomalyDetectorsIndexFields.STATE_INDEX_PREFIX + "-000001";
             CreateIndexRequest createIndexRequest = client.admin()
                 .indices()
-                .prepareCreate(AnomalyDetectorsIndexFields.STATE_INDEX_PREFIX)
+                .prepareCreate(initialJobStateIndex)
                 .addAlias(new Alias(jobStateIndexWriteAlias()))
                 .request();
             executeAsyncWithOrigin(client.threadPool().getThreadContext(),
@@ -141,7 +168,7 @@ public final class AnomalyDetectorsIndex {
                         // Adding an alias that already exists is idempotent. So, no need to double check if the alias exists
                         // as well.
                         if (ExceptionsHelper.unwrapCause(createIndexFailure) instanceof ResourceAlreadyExistsException) {
-                            createAliasListener.onResponse(AnomalyDetectorsIndexFields.STATE_INDEX_PREFIX);
+                            createAliasListener.onResponse(initialJobStateIndex);
                         } else {
                             finalListener.onFailure(createIndexFailure);
                         }

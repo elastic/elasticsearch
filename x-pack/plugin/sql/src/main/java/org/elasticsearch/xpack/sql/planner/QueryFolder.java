7af8f142c4b7fee4b977aa90bb96cd1b18557279
Andrei Stefan <andrei@elastic.co>
2020-02-21 11:51:25 +0200

    Address feedback

--- x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/planner/QueryFolder.java
+++ x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/planner/QueryFolder.java
@@ -90,6 +90,8 @@ import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.elasticsearch.xpack.ql.util.CollectionUtils.combine;
+import static org.elasticsearch.xpack.sql.expression.function.grouping.Histogram.MONTH_INTERVAL;
+import static org.elasticsearch.xpack.sql.expression.function.grouping.Histogram.YEAR_INTERVAL;
 import static org.elasticsearch.xpack.sql.planner.QueryTranslator.toAgg;
 import static org.elasticsearch.xpack.sql.planner.QueryTranslator.toQuery;
 import static org.elasticsearch.xpack.sql.type.SqlDataTypes.DATE;
@@ -320,30 +322,21 @@ class QueryFolder extends RuleExecutor<PhysicalPlan> {
                             // date histogram
                             if (isDateBased(h.dataType())) {
                                 Object value = h.interval().value();
+
                                 // interval of exactly 1 year or 1 month
                                 if (value instanceof IntervalYearMonth &&
                                         (((IntervalYearMonth) value).interval().equals(Period.ofYears(1)) 
                                         || ((IntervalYearMonth) value).interval().equals(Period.ofMonths(1)))) {
                                     Period yearMonth = ((IntervalYearMonth) value).interval();
-                                    
-                                    String calendarInterval = null;
-                                    if (yearMonth.equals(Period.ofYears(1))) {
-                                        calendarInterval = Histogram.YEAR_INTERVAL;
-                                    } else if (yearMonth.equals(Period.ofMonths(1))) {
-                                        calendarInterval = Histogram.MONTH_INTERVAL;
-                                    }
+                                    String calendarInterval = yearMonth.equals(Period.ofYears(1)) ? YEAR_INTERVAL : MONTH_INTERVAL;
 
                                     // When the histogram is `INTERVAL '1' YEAR` or `INTERVAL '1' MONTH`, the interval used in 
                                     // the ES date_histogram will be a calendar_interval with value "1y" or "1M" respectively.
                                     // All other intervals will be fixed_intervals expressed in ms.
-                                    if (calendarInterval != null) {
-                                        if (field instanceof FieldAttribute) {
-                                            key = new GroupByDateHistogram(aggId, QueryTranslator.nameOf(field), calendarInterval,
-                                                    h.zoneId());
-                                        } else if (field instanceof Function) {
-                                            key = new GroupByDateHistogram(aggId, ((Function) field).asScript(), calendarInterval,
-                                                    h.zoneId());
-                                        }
+                                    if (field instanceof FieldAttribute) {
+                                        key = new GroupByDateHistogram(aggId, QueryTranslator.nameOf(field), calendarInterval, h.zoneId());
+                                    } else if (field instanceof Function) {
+                                        key = new GroupByDateHistogram(aggId, ((Function) field).asScript(), calendarInterval, h.zoneId());
                                     }
                                 }
                                 // typical interval

ebe5b63294317616ffc411515095f9a06a2e6efb
Sachin Frayne <32795683+sachinfrayne@users.noreply.github.com>
2020-02-25 18:44:22 +0000

    minor changes to try to keep the error message as precise/short as possible

--- server/src/main/java/org/elasticsearch/index/mapper/TextFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/TextFieldMapper.java
@@ -749,10 +749,10 @@ public class TextFieldMapper extends FieldMapper {
         @Override
         public IndexFieldData.Builder fielddataBuilder(String fullyQualifiedIndexName) {
             if (fielddata == false) {
-                throw new IllegalArgumentException("Text fields are not optimised for aggregations and/or sorting and therefore these "
-                    + "operations are disabled by default. Please use a field with a keyword datatype instead. "
-                        + "Otherwise Set fielddata=true on [" + name() + "] in order to load fielddata in memory "
-                            + "by uninverting the inverted index. Note that this can however use significant memory.");
+            throw new IllegalArgumentException("Text fields are not optimised for operations that require per-document "
+                + "field data like aggregations and sorting, so these operations are disabled by default. Please use a "
+                    + " keyword field instead. Alternatively, set fielddata=true on [" + name() + "] in order to load "
+                        + "field data by uninverting the inverted index. Note that this can use significant memory.");
             }
             return new PagedBytesIndexFieldData.Builder(fielddataMinFrequency, fielddataMaxFrequency, fielddataMinSegmentSize);
         }

68ba571f70abeec030b0b636fdef2401603e772a
Josh Devins <josh.devins@elastic.co>
2020-02-27 16:04:24 +0100

    Adds recall@k metric to rank eval API (#52889)
    This change adds the recall@k metric and refactors precision@k to match
    the new metric.
    
    Recall@k is an important metric to use for learning to rank (LTR)
    use-cases. Candidate generation or first ranking phase ranking functions
    are often optimized for high recall, in order to generate as many
    relevant candidates in the top-k as possible for a second phase of
    ranking. Adding this metric allows tuning that base query for LTR.
    
    See: https://github.com/elastic/elasticsearch/issues/51676
    Backports: https://github.com/elastic/elasticsearch/pull/52577

--- modules/rank-eval/src/main/java/org/elasticsearch/index/rankeval/RecallAtK.java
+++ modules/rank-eval/src/main/java/org/elasticsearch/index/rankeval/RecallAtK.java
@@ -39,44 +39,34 @@ import static org.elasticsearch.common.xcontent.ConstructingObjectParser.optiona
 import static org.elasticsearch.index.rankeval.EvaluationMetric.joinHitsWithRatings;
 
 /**
- * Metric implementing Precision@K
- * (https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Precision).<br>
+ * Metric implementing Recall@K
+ * (https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Recall).<br>
  * By default documents with a rating equal or bigger than 1 are considered to
  * be "relevant" for this calculation. This value can be changed using the
  * `relevant_rating_threshold` parameter.<br>
- * The `ignore_unlabeled` parameter (default to false) controls if unrated
- * documents should be ignored.
  * The `k` parameter (defaults to 10) controls the search window size.
  */
-public class PrecisionAtK implements EvaluationMetric {
+public class RecallAtK implements EvaluationMetric {
 
-    public static final String NAME = "precision";
+    public static final String NAME = "recall";
 
     private static final int DEFAULT_RELEVANT_RATING_THRESHOLD = 1;
-    private static final boolean DEFAULT_IGNORE_UNLABELED = false;
     private static final int DEFAULT_K = 10;
 
     private static final ParseField RELEVANT_RATING_THRESHOLD_FIELD = new ParseField("relevant_rating_threshold");
-    private static final ParseField IGNORE_UNLABELED_FIELD = new ParseField("ignore_unlabeled");
     private static final ParseField K_FIELD = new ParseField("k");
 
     private final int relevantRatingThreshold;
-    private final boolean ignoreUnlabeled;
     private final int k;
 
     /**
-     * Metric implementing Precision@K.
+     * Metric implementing Recall@K.
      * @param relevantRatingThreshold
      *            ratings equal or above this value will be considered relevant.
-     * @param ignoreUnlabeled
-     *            Controls how unlabeled documents in the search hits are treated.
-     *            Set to 'true', unlabeled documents are ignored and neither count
-     *            as true or false positives. Set to 'false', they are treated as
-     *            false positives.
      * @param k
      *            controls the window size for the search results the metric takes into account
      */
-    public PrecisionAtK(int relevantRatingThreshold, boolean ignoreUnlabeled, int k) {
+    public RecallAtK(int relevantRatingThreshold, int k) {
         if (relevantRatingThreshold < 0) {
             throw new IllegalArgumentException("Relevant rating threshold for precision must be positive integer.");
         }
@@ -84,46 +74,37 @@ public class PrecisionAtK implements EvaluationMetric {
             throw new IllegalArgumentException("Window size k must be positive.");
         }
         this.relevantRatingThreshold = relevantRatingThreshold;
-        this.ignoreUnlabeled = ignoreUnlabeled;
         this.k = k;
     }
 
-    public PrecisionAtK(boolean ignoreUnlabeled) {
-        this(DEFAULT_RELEVANT_RATING_THRESHOLD, ignoreUnlabeled, DEFAULT_K);
+    public RecallAtK() {
+        this(DEFAULT_RELEVANT_RATING_THRESHOLD, DEFAULT_K);
     }
 
-    public PrecisionAtK() {
-        this(DEFAULT_RELEVANT_RATING_THRESHOLD, DEFAULT_IGNORE_UNLABELED, DEFAULT_K);
+    RecallAtK(StreamInput in) throws IOException {
+        this(in.readVInt(), in.readVInt());
     }
 
-    PrecisionAtK(StreamInput in) throws IOException {
-        this(in.readVInt(), in.readBoolean(), in.readVInt());
-    }
-
-    private static final ConstructingObjectParser<PrecisionAtK, Void> PARSER = new ConstructingObjectParser<>(NAME, args -> {
+    private static final ConstructingObjectParser<RecallAtK, Void> PARSER = new ConstructingObjectParser<>(NAME, args -> {
         Integer relevantRatingThreshold = (Integer) args[0];
-        Boolean ignoreUnlabeled = (Boolean) args[1];
-        Integer k = (Integer) args[2];
-        return new PrecisionAtK(
+        Integer k = (Integer) args[1];
+        return new RecallAtK(
             relevantRatingThreshold == null ? DEFAULT_RELEVANT_RATING_THRESHOLD : relevantRatingThreshold,
-            ignoreUnlabeled == null ? DEFAULT_IGNORE_UNLABELED : ignoreUnlabeled,
             k == null ? DEFAULT_K : k);
     });
 
     static {
         PARSER.declareInt(optionalConstructorArg(), RELEVANT_RATING_THRESHOLD_FIELD);
-        PARSER.declareBoolean(optionalConstructorArg(), IGNORE_UNLABELED_FIELD);
         PARSER.declareInt(optionalConstructorArg(), K_FIELD);
     }
 
-    public static PrecisionAtK fromXContent(XContentParser parser) {
+    public static RecallAtK fromXContent(XContentParser parser) {
         return PARSER.apply(parser, null);
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         out.writeVInt(getRelevantRatingThreshold());
-        out.writeBoolean(getIgnoreUnlabeled());
         out.writeVInt(getK());
     }
 
@@ -132,7 +113,6 @@ public class PrecisionAtK implements EvaluationMetric {
         builder.startObject();
         builder.startObject(NAME);
         builder.field(RELEVANT_RATING_THRESHOLD_FIELD.getPreferredName(), getRelevantRatingThreshold());
-        builder.field(IGNORE_UNLABELED_FIELD.getPreferredName(), getIgnoreUnlabeled());
         builder.field(K_FIELD.getPreferredName(), getK());
         builder.endObject();
         builder.endObject();
@@ -152,13 +132,6 @@ public class PrecisionAtK implements EvaluationMetric {
         return relevantRatingThreshold;
     }
 
-    /**
-     * Gets the 'ignore_unlabeled' parameter.
-     */
-    public boolean getIgnoreUnlabeled() {
-        return ignoreUnlabeled;
-    }
-
     public int getK() {
         return k;
     }
@@ -176,16 +149,9 @@ public class PrecisionAtK implements EvaluationMetric {
     }
 
     /**
-     * Should we count unlabeled documents? This is the inverse of {@link #getIgnoreUnlabeled()}.
-     */
-    private boolean shouldCountUnlabeled() {
-        return !getIgnoreUnlabeled();
-    }
-
-    /**
-     * Compute precision at k based on provided relevant document IDs.
+     * Compute recall at k based on provided relevant document IDs.
      *
-     * @return precision at k for above {@link SearchResult} list.
+     * @return recall at k for above {@link SearchResult} list.
      **/
     @Override
     public EvalQueryQuality evaluate(String taskId, SearchHit[] hits,
@@ -194,27 +160,27 @@ public class PrecisionAtK implements EvaluationMetric {
         List<RatedSearchHit> ratedSearchHits = joinHitsWithRatings(hits, ratedDocs);
 
         int relevantRetrieved = 0;
-        int retrieved = 0;
-
         for (RatedSearchHit hit : ratedSearchHits) {
             OptionalInt rating = hit.getRating();
-            if (rating.isPresent()) {
-                retrieved++;
-                if (isRelevant(rating.getAsInt())) {
-                    relevantRetrieved++;
-                }
-            } else if (shouldCountUnlabeled()) {
-                retrieved++;
+            if (rating.isPresent() && isRelevant(rating.getAsInt())) {
+                relevantRetrieved++;
             }
         }
 
-        double precision = 0.0;
-        if (retrieved > 0) {
-            precision = (double) relevantRetrieved / retrieved;
+        int relevant = 0;
+        for (RatedDocument rd : ratedDocs) {
+            if(isRelevant(rd.getRating())) {
+                relevant++;
+            }
         }
 
-        EvalQueryQuality evalQueryQuality = new EvalQueryQuality(taskId, precision);
-        evalQueryQuality.setMetricDetails(new PrecisionAtK.Detail(relevantRetrieved, retrieved));
+        double recall = 0.0;
+        if (relevant > 0) {
+            recall = (double) relevantRetrieved / relevant;
+        }
+
+        EvalQueryQuality evalQueryQuality = new EvalQueryQuality(taskId, recall);
+        evalQueryQuality.setMetricDetails(new RecallAtK.Detail(relevantRetrieved, relevant));
         evalQueryQuality.addHitsAndRatings(ratedSearchHits);
         return evalQueryQuality;
     }
@@ -227,32 +193,31 @@ public class PrecisionAtK implements EvaluationMetric {
         if (obj == null || getClass() != obj.getClass()) {
             return false;
         }
-        PrecisionAtK other = (PrecisionAtK) obj;
+        RecallAtK other = (RecallAtK) obj;
         return Objects.equals(relevantRatingThreshold, other.relevantRatingThreshold)
-            && Objects.equals(ignoreUnlabeled, other.ignoreUnlabeled)
             && Objects.equals(k, other.k);
     }
 
     @Override
     public final int hashCode() {
-        return Objects.hash(relevantRatingThreshold, ignoreUnlabeled, k);
+        return Objects.hash(relevantRatingThreshold, k);
     }
 
     public static final class Detail implements MetricDetail {
 
         private static final ParseField RELEVANT_DOCS_RETRIEVED_FIELD = new ParseField("relevant_docs_retrieved");
-        private static final ParseField DOCS_RETRIEVED_FIELD = new ParseField("docs_retrieved");
+        private static final ParseField RELEVANT_DOCS_FIELD = new ParseField("relevant_docs");
+        private long relevantRetrieved;
+        private long relevant;
 
-        private int relevantRetrieved;
-        private int retrieved;
-
-        Detail(int relevantRetrieved, int retrieved) {
+        Detail(long relevantRetrieved, long relevant) {
             this.relevantRetrieved = relevantRetrieved;
-            this.retrieved = retrieved;
+            this.relevant = relevant;
         }
 
         Detail(StreamInput in) throws IOException {
-            this(in.readVInt(), in.readVInt());
+            this.relevantRetrieved = in.readVLong();
+            this.relevant = in.readVLong();
         }
 
         private static final ConstructingObjectParser<Detail, Void> PARSER =
@@ -260,7 +225,7 @@ public class PrecisionAtK implements EvaluationMetric {
 
         static {
             PARSER.declareInt(constructorArg(), RELEVANT_DOCS_RETRIEVED_FIELD);
-            PARSER.declareInt(constructorArg(), DOCS_RETRIEVED_FIELD);
+            PARSER.declareInt(constructorArg(), RELEVANT_DOCS_FIELD);
         }
 
         public static Detail fromXContent(XContentParser parser) {
@@ -270,14 +235,14 @@ public class PrecisionAtK implements EvaluationMetric {
         @Override
         public void writeTo(StreamOutput out) throws IOException {
             out.writeVLong(relevantRetrieved);
-            out.writeVLong(retrieved);
+            out.writeVLong(relevant);
         }
 
         @Override
         public XContentBuilder innerToXContent(XContentBuilder builder, Params params)
             throws IOException {
             builder.field(RELEVANT_DOCS_RETRIEVED_FIELD.getPreferredName(), relevantRetrieved);
-            builder.field(DOCS_RETRIEVED_FIELD.getPreferredName(), retrieved);
+            builder.field(RELEVANT_DOCS_FIELD.getPreferredName(), relevant);
             return builder;
         }
 
@@ -286,12 +251,12 @@ public class PrecisionAtK implements EvaluationMetric {
             return NAME;
         }
 
-        public int getRelevantRetrieved() {
+        public long getRelevantRetrieved() {
             return relevantRetrieved;
         }
 
-        public int getRetrieved() {
-            return retrieved;
+        public long getRelevant() {
+            return relevant;
         }
 
         @Override
@@ -302,14 +267,14 @@ public class PrecisionAtK implements EvaluationMetric {
             if (obj == null || getClass() != obj.getClass()) {
                 return false;
             }
-            PrecisionAtK.Detail other = (PrecisionAtK.Detail) obj;
+            RecallAtK.Detail other = (RecallAtK.Detail) obj;
             return Objects.equals(relevantRetrieved, other.relevantRetrieved)
-                    && Objects.equals(retrieved, other.retrieved);
+                && Objects.equals(relevant, other.relevant);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(relevantRetrieved, retrieved);
+            return Objects.hash(relevantRetrieved, relevant);
         }
     }
 }

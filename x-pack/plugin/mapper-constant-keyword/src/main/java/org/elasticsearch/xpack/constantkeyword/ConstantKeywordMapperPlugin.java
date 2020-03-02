9a11f75deb9a848f7dd8b40c2ced09da751d75db
Adrien Grand <jpountz@gmail.com>
2020-03-02 16:46:46 +0100

    Introduce a `constant_keyword` field. (#49713)
    This field is a specialization of the `keyword` field for the case when all
    documents have the same value. It typically performs more efficiently than
    keywords at query time by figuring out whether all or none of the documents
    match at rewrite time, like `term` queries on `_index`.
    
    The name is up for discussion. I liked including `keyword` in it, so that we
    still have room for a `singleton_numeric` in the future. However I'm unsure
    whether to call it `singleton`, `constant` or something else, any opinions?
    
    For this field there is a choice between
     1. accepting values in `_source` when they are equal to the value configured
        in mappings, but rejecting mapping updates
     2. rejecting values in `_source` but then allowing updates to the value that
        is configured in the mapping
    This commit implements option 1, so that it is possible to reindex from/to an
    index that has the field mapped as a keyword with no changes to the source.
    
    Backport of #49713

---
+++ x-pack/plugin/mapper-constant-keyword/src/main/java/org/elasticsearch/xpack/constantkeyword/ConstantKeywordMapperPlugin.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+package org.elasticsearch.xpack.constantkeyword;
+
+import org.elasticsearch.action.ActionRequest;
+import org.elasticsearch.action.ActionResponse;
+import org.elasticsearch.common.inject.Module;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.mapper.Mapper;
+import org.elasticsearch.plugins.ActionPlugin;
+import org.elasticsearch.plugins.MapperPlugin;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.xpack.constantkeyword.mapper.ConstantKeywordFieldMapper;
+import org.elasticsearch.xpack.core.XPackPlugin;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Collections.singletonMap;
+
+public class ConstantKeywordMapperPlugin extends Plugin implements MapperPlugin, ActionPlugin {
+
+    public ConstantKeywordMapperPlugin(Settings settings) {}
+
+    @Override
+    public Map<String, Mapper.TypeParser> getMappers() {
+        return singletonMap(ConstantKeywordFieldMapper.CONTENT_TYPE, new ConstantKeywordFieldMapper.TypeParser());
+    }
+
+    public Collection<Module> createGuiceModules() {
+        return Collections.singletonList(b -> {
+            XPackPlugin.bindFeatureSet(b, ConstantKeywordFeatureSet.class);
+        });
+    }
+
+}

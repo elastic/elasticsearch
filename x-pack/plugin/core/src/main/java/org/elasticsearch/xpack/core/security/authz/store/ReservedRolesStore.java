a78286d45c96c37a36a4f156be2be513c1e40a4b
Larry Gregory <larry.gregory@elastic.co>
2020-03-03 14:27:23 -0500

    update role deprecation reasons

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStore.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStore.java
@@ -54,7 +54,7 @@ public class ReservedRolesStore implements BiConsumer<Set<String>, ActionListene
                         MetadataUtils.DEFAULT_RESERVED_METADATA))
                 .put("kibana_admin", kibanaAdminUser("kibana_admin", MetadataUtils.DEFAULT_RESERVED_METADATA))
                 .put("kibana_user", kibanaAdminUser("kibana_user",
-                    MetadataUtils.getDeprecatedReservedMetadata("Please use the [kibana_admin] role instead")))
+                    MetadataUtils.getDeprecatedReservedMetadata("Use kibana_admin instead.")))
                 .put("monitoring_user", new RoleDescriptor("monitoring_user",
                         new String[] { "cluster:monitor/main", "cluster:monitor/xpack/info", RemoteInfoAction.NAME },
                         new RoleDescriptor.IndicesPrivileges[] {
@@ -108,7 +108,7 @@ public class ReservedRolesStore implements BiConsumer<Set<String>, ActionListene
                             RoleDescriptor.ApplicationResourcePrivileges.builder()
                             .application("kibana-.kibana").resources("*").privileges("read").build() },
                         null, null,
-                        MetadataUtils.getDeprecatedReservedMetadata("Please use Kibana feature privileges instead"),
+                        MetadataUtils.getDeprecatedReservedMetadata("Configure a custom role to grant access to Dashboard."),
                         null))
                 .put(KibanaUser.ROLE_NAME, new RoleDescriptor(KibanaUser.ROLE_NAME,
                         new String[] {

7b8f9d772eb4cd98195a5d053b9335cc2216e906
William Brafford <william.brafford@elastic.co>
2020-03-04 11:07:51 -0500

    Streamline tests and test defaults
    NodesInfoRequest defaults to specifying all values. We need to test for
    this behavior as we refactor. Also, we can use random testing for the
    various combinations of metrics rather than having individual tests for
    each metric.

--- server/src/test/java/org/elasticsearch/action/admin/cluster/node/info/NodesInfoRequestTests.java
+++ server/src/test/java/org/elasticsearch/action/admin/cluster/node/info/NodesInfoRequestTests.java
@@ -27,176 +27,80 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class NodesInfoRequestTests extends ESTestCase {
 
-    public void testNodesInfoRequestSettings() throws Exception {
-        NodesInfoRequest request = new NodesInfoRequest("node");
-        request.settings(true);
+    public void testMetricsSetters() throws Exception {
+        NodesInfoRequest request = new NodesInfoRequest(randomAlphaOfLength(8));
+        request.settings(randomBoolean());
+        request.os(randomBoolean());
+        request.process(randomBoolean());
+        request.jvm(randomBoolean());
+        request.threadPool(randomBoolean());
+        request.transport(randomBoolean());
+        request.http(randomBoolean());
+        request.plugins(randomBoolean());
+        request.ingest(randomBoolean());
+        request.indices(randomBoolean());
         NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
         assertThat(request.settings(), equalTo(roundTrippedRequest.settings()));
-
-        request.settings(false);
-        roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.settings(), equalTo(roundTrippedRequest.settings()));
     }
 
-    public void testNodesInfoRequestOs() throws Exception {
-        NodesInfoRequest request = new NodesInfoRequest("node");
-        request.os(true);
-        NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.os(), equalTo(roundTrippedRequest.os()));
+    public void testNodesInfoRequestDefaults() {
+        NodesInfoRequest defaultNodesInfoRequest = new NodesInfoRequest(randomAlphaOfLength(8));
+        NodesInfoRequest allMetricsNodesInfoRequest = new NodesInfoRequest(randomAlphaOfLength(8));
 
-        request.os(false);
-        roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.os(), equalTo(roundTrippedRequest.os()));
-    }
-
-    public void testNodesInfoRequestProcess() throws Exception {
-        NodesInfoRequest request = new NodesInfoRequest("node");
-        request.process(true);
-        NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.process(), equalTo(roundTrippedRequest.process()));
-
-        request.process(false);
-        roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.process(), equalTo(roundTrippedRequest.process()));
-    }
-
-    public void testNodesInfoRequestJvm() throws Exception {
-        NodesInfoRequest request = new NodesInfoRequest("node");
-        request.jvm(true);
-        NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.jvm(), equalTo(roundTrippedRequest.jvm()));
-
-        request.jvm(false);
-        roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.jvm(), equalTo(roundTrippedRequest.jvm()));
-    }
-
-    public void testNodesInfoRequestThreadPool() throws Exception {
-        NodesInfoRequest request = new NodesInfoRequest("node");
-        request.threadPool(true);
-        NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.threadPool(), equalTo(roundTrippedRequest.threadPool()));
-
-        request.threadPool(false);
-        roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.threadPool(), equalTo(roundTrippedRequest.threadPool()));
-    }
-
-    public void testNodesInfoRequestTransport() throws Exception {
-        NodesInfoRequest request = new NodesInfoRequest("node");
-        request.transport(true);
-        NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.transport(), equalTo(roundTrippedRequest.transport()));
-
-        request.transport(false);
-        roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.transport(), equalTo(roundTrippedRequest.transport()));
-    }
-
-    public void testNodesInfoRequestHttp() throws Exception {
-        NodesInfoRequest request = new NodesInfoRequest("node");
-        request.http(true);
-        NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.http(), equalTo(roundTrippedRequest.http()));
-
-        request.http(false);
-        roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.http(), equalTo(roundTrippedRequest.http()));
-    }
-
-    public void testNodesInfoRequestPlugins() throws Exception {
-        NodesInfoRequest request = new NodesInfoRequest("node");
-        request.plugins(true);
-        NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.plugins(), equalTo(roundTrippedRequest.plugins()));
-
-        request.plugins(false);
-        roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.plugins(), equalTo(roundTrippedRequest.plugins()));
-    }
-
-    public void testNodesInfoRequestIngest() throws Exception {
-        NodesInfoRequest request = new NodesInfoRequest("node");
-        request.ingest(true);
-        NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.ingest(), equalTo(roundTrippedRequest.ingest()));
-
-        request.ingest(false);
-        roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.ingest(), equalTo(roundTrippedRequest.ingest()));
-    }
-
-    public void testNodesInfoRequestIndices() throws Exception {
-        NodesInfoRequest request = new NodesInfoRequest("node");
-        request.indices(true);
-        NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.indices(), equalTo(roundTrippedRequest.indices()));
-
-        request.indices(false);
-        roundTrippedRequest = roundTripRequest(request);
-        assertThat(request.indices(), equalTo(roundTrippedRequest.indices()));
+        assertRequestsEqual(defaultNodesInfoRequest, allMetricsNodesInfoRequest);
     }
 
     public void testNodesInfoRequestAll() throws Exception {
         NodesInfoRequest request = new NodesInfoRequest("node");
         request.all();
-        NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
 
         assertTrue(request.settings());
-        assertTrue(roundTrippedRequest.settings());
         assertTrue(request.os());
-        assertTrue(roundTrippedRequest.os());
         assertTrue(request.process());
-        assertTrue(roundTrippedRequest.process());
         assertTrue(request.jvm());
-        assertTrue(roundTrippedRequest.jvm());
         assertTrue(request.threadPool());
-        assertTrue(roundTrippedRequest.threadPool());
         assertTrue(request.transport());
-        assertTrue(roundTrippedRequest.transport());
         assertTrue(request.http());
-        assertTrue(roundTrippedRequest.http());
         assertTrue(request.plugins());
-        assertTrue(roundTrippedRequest.plugins());
         assertTrue(request.ingest());
-        assertTrue(roundTrippedRequest.ingest());
         assertTrue(request.indices());
-        assertTrue(roundTrippedRequest.indices());
     }
 
     public void testNodesInfoRequestClear() throws Exception {
         NodesInfoRequest request = new NodesInfoRequest("node");
         request.clear();
-        NodesInfoRequest roundTrippedRequest = roundTripRequest(request);
 
         assertFalse(request.settings());
-        assertFalse(roundTrippedRequest.settings());
         assertFalse(request.os());
-        assertFalse(roundTrippedRequest.os());
         assertFalse(request.process());
-        assertFalse(roundTrippedRequest.process());
         assertFalse(request.jvm());
-        assertFalse(roundTrippedRequest.jvm());
         assertFalse(request.threadPool());
-        assertFalse(roundTrippedRequest.threadPool());
         assertFalse(request.transport());
-        assertFalse(roundTrippedRequest.transport());
         assertFalse(request.http());
-        assertFalse(roundTrippedRequest.http());
         assertFalse(request.plugins());
-        assertFalse(roundTrippedRequest.plugins());
         assertFalse(request.ingest());
-        assertFalse(roundTrippedRequest.ingest());
         assertFalse(request.indices());
-        assertFalse(roundTrippedRequest.indices());
     }
 
-    private NodesInfoRequest roundTripRequest(NodesInfoRequest request) throws Exception {
+    private static NodesInfoRequest roundTripRequest(NodesInfoRequest request) throws Exception {
         try (BytesStreamOutput out = new BytesStreamOutput()) {
             request.writeTo(out);
             try (StreamInput in = out.bytes().streamInput()) {
                 return new NodesInfoRequest(in);
             }
         }
+    }
+
+    private static void assertRequestsEqual(NodesInfoRequest request1, NodesInfoRequest request2) {
+        assertThat(request1.settings(), equalTo(request2.settings()));
+        assertThat(request1.os(), equalTo(request2.os()));
+        assertThat(request1.process(), equalTo(request2.process()));
+        assertThat(request1.jvm(), equalTo(request2.jvm()));
+        assertThat(request1.threadPool(), equalTo(request2.threadPool()));
+        assertThat(request1.transport(), equalTo(request2.transport()));
+        assertThat(request1.http(), equalTo(request2.http()));
+        assertThat(request1.plugins(), equalTo(request2.plugins()));
+        assertThat(request1.ingest(), equalTo(request2.ingest()));
+        assertThat(request1.indices(), equalTo(request2.indices()));
     }
 }

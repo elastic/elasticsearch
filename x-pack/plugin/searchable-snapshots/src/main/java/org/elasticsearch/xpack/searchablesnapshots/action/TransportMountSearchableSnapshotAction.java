2bcab06cc58edf8981945f408dee83b40a8fecd0
Lee Hinman <lee@writequit.org>
2020-03-04 11:34:40 -0700

    Remove extra line

--- x-pack/plugin/searchable-snapshots/src/main/java/org/elasticsearch/xpack/searchablesnapshots/action/TransportMountSearchableSnapshotAction.java
+++ x-pack/plugin/searchable-snapshots/src/main/java/org/elasticsearch/xpack/searchablesnapshots/action/TransportMountSearchableSnapshotAction.java
@@ -76,7 +76,6 @@ public class TransportMountSearchableSnapshotAction
             return blockException;
         }
         return state.blocks().globalBlockedException(ClusterBlockLevel.WRITE);
-
     }
 
     /**

c5738ae312a14906748046cc3a62f9c08868ff30
Jason Tedor <jason@tedor.me>
2020-03-07 13:02:02 -0500

    Notify refresh listeners on the calling thread (#53259)
    Today we notify refresh listeners by forking to the listener thread pool
    and then serially notifying listeners on a thread there. Refreshes are
    expensive though, so the expectation is that we are executing refreshes
    on threads that can afford an expensive operation (e.g., not a network
    thread) and as such, executing listeners that we expect to be cheap aon
    the calling thread is okay. This commit removes the forking of notifying
    refresh listeners to run directly on the calling thread that executed a
    refresh.

--- server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
@@ -3179,7 +3179,6 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
         return new RefreshListeners(
             indexSettings::getMaxRefreshListeners,
             () -> refresh("too_many_listeners"),
-            threadPool.executor(ThreadPool.Names.LISTENER),
             logger, threadPool.getThreadContext(),
             externalRefreshMetric);
     }

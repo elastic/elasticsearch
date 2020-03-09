62c8ac99932e51e900234b71a9df0b013fb4c37e
Bogdan Pintea <bogdan.pintea@elastic.co>
2020-03-09 21:16:57 +0100

    SQL: transfer version compatibility decision to the server (#53082) (#53302)
    This commit adds a new request object field, "version", containing the version of the requesting client. This parameter is now accepted - and for certain clients required - by the server and the request is validated against it. Currently server's and client's versions still need to be equal in order for the request to be accepted. Relaxing this check is going to be part of future work. 
    
    On the clients' side, the only check remaining is to ensure that the peer server is supporting version backwards compatibility (i.e. is on, or newer than a certain release).
    
    (cherry picked from commit a8f413a20fb023bec83af0de1211a2936a7f558c)

--- x-pack/plugin/sql/jdbc/src/test/java/org/elasticsearch/xpack/sql/jdbc/VersionTests.java
+++ x-pack/plugin/sql/jdbc/src/test/java/org/elasticsearch/xpack/sql/jdbc/VersionTests.java
@@ -6,15 +6,14 @@
 package org.elasticsearch.xpack.sql.jdbc;
 
 import org.elasticsearch.test.ESTestCase;
-import org.elasticsearch.xpack.sql.client.Version;
+import org.elasticsearch.xpack.sql.client.ClientVersion;
 
 public class VersionTests extends ESTestCase {
     public void testVersionIsCurrent() {
         /* This test will only work properly in gradle because in gradle we run the tests
          * using the jar. */
-        assertNotNull(Version.CURRENT.hash);
-        assertEquals(org.elasticsearch.Version.CURRENT.major, Version.CURRENT.major);
-        assertEquals(org.elasticsearch.Version.CURRENT.minor, Version.CURRENT.minor);
-        assertEquals(org.elasticsearch.Version.CURRENT.revision, Version.CURRENT.revision);
+        assertEquals(org.elasticsearch.Version.CURRENT.major, ClientVersion.CURRENT.major);
+        assertEquals(org.elasticsearch.Version.CURRENT.minor, ClientVersion.CURRENT.minor);
+        assertEquals(org.elasticsearch.Version.CURRENT.revision, ClientVersion.CURRENT.revision);
     }
 }

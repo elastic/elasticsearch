cebf279027aba1eaf9f8db63212fbbee5e3275ad
djptek <dominic.page@elastic.co>
2020-03-11 13:05:10 +0100

    override doCreateTestQueryBuilder() in GeoShapeQueryBuilderTests

--- server/src/test/java/org/elasticsearch/index/query/GeoShapeQueryBuilderGeoPointTests.java
+++ server/src/test/java/org/elasticsearch/index/query/GeoShapeQueryBuilderGeoPointTests.java
@@ -18,9 +18,9 @@
  */
 package org.elasticsearch.index.query;
 
-import java.io.IOException;
-
-import static org.hamcrest.Matchers.containsString;
+import org.elasticsearch.common.geo.ShapeRelation;
+import org.elasticsearch.common.geo.builders.ShapeBuilder;
+import org.elasticsearch.test.geo.RandomShapeGenerator;
 
 public class GeoShapeQueryBuilderGeoPointTests extends GeoShapeQueryBuilderTests {
 
@@ -29,26 +29,42 @@ public class GeoShapeQueryBuilderGeoPointTests extends GeoShapeQueryBuilderTests
     }
 
     @Override
-    public void testCacheability() throws IOException {
-        GeoShapeQueryBuilder queryBuilder = createTestQueryBuilder();
-        QueryShardContext context = createShardContext();
-        try {
-            rewriteQuery(queryBuilder, new QueryShardContext(context));
-        } catch (
-            QueryShardException e) {
-            assertThat(e.getCause().getMessage(),
-                containsString("Field [mapped_geo_point] does not support point queries"));
-        }
+    protected GeoShapeQueryBuilder doCreateTestQueryBuilder() {
+        return doCreateTestQueryBuilder(randomBoolean());
     }
 
-    @Override
-    public void testToQuery() throws IOException {
-        try {
-            createTestQueryBuilder();
-        } catch (
-            QueryShardException e) {
-            assertThat(e.getCause().getMessage(),
-                containsString("Field [mapped_geo_point] does not support point queries"));
+    protected GeoShapeQueryBuilder doCreateTestQueryBuilder(boolean indexedShape) {
+        RandomShapeGenerator.ShapeType shapeType = RandomShapeGenerator.ShapeType.POLYGON;
+        ShapeBuilder<?, ?, ?> shape = RandomShapeGenerator.createShapeWithin(random(), null, shapeType);
+        GeoShapeQueryBuilder builder;
+        clearShapeFields();
+        if (indexedShape == false) {
+            builder = new GeoShapeQueryBuilder(fieldName(), shape);
+        } else {
+            indexedShapeToReturn = shape;
+            indexedShapeId = randomAlphaOfLengthBetween(3, 20);
+            builder = new GeoShapeQueryBuilder(fieldName(), indexedShapeId);
+            if (randomBoolean()) {
+                indexedShapeIndex = randomAlphaOfLengthBetween(3, 20);
+                builder.indexedShapeIndex(indexedShapeIndex);
+            }
+            if (randomBoolean()) {
+                indexedShapePath = randomAlphaOfLengthBetween(3, 20);
+                builder.indexedShapePath(indexedShapePath);
+            }
+            if (randomBoolean()) {
+                indexedShapeRouting = randomAlphaOfLengthBetween(3, 20);
+                builder.indexedShapeRouting(indexedShapeRouting);
+            }
         }
+        if (randomBoolean()) {
+            builder.relation(randomFrom(ShapeRelation.INTERSECTS));
+        }
+
+        if (randomBoolean()) {
+            builder.ignoreUnmapped(randomBoolean());
+        }
+        return builder;
     }
+
 }

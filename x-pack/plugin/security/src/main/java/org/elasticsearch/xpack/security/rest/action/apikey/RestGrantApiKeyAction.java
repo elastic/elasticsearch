04339c6a52e8f225e68c14ab66039a96dea43a66
Tim Vernum <tim@adjective.org>
2020-03-12 15:47:57 +1100

    Address more feedback

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/rest/action/apikey/RestGrantApiKeyAction.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/rest/action/apikey/RestGrantApiKeyAction.java
@@ -6,8 +6,6 @@
 
 package org.elasticsearch.xpack.security.rest.action.apikey;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.elasticsearch.ElasticsearchSecurityException;
 import org.elasticsearch.ExceptionsHelper;
 import org.elasticsearch.action.ActionListener;
@@ -17,15 +15,12 @@ import org.elasticsearch.common.ParseField;
 import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.ObjectParser;
-import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.license.XPackLicenseState;
-import org.elasticsearch.rest.BytesRestResponse;
-import org.elasticsearch.rest.RestChannel;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.rest.action.RestToXContentListener;
 import org.elasticsearch.xpack.core.security.action.CreateApiKeyRequestBuilder;
-import org.elasticsearch.xpack.core.security.action.CreateApiKeyResponse;
 import org.elasticsearch.xpack.core.security.action.GrantApiKeyAction;
 import org.elasticsearch.xpack.core.security.action.GrantApiKeyRequest;
 
@@ -41,8 +36,6 @@ import static org.elasticsearch.rest.RestRequest.Method.PUT;
  * {@link org.elasticsearch.xpack.security.rest.action.oauth2.RestGetTokenAction} combined with {@link RestCreateApiKeyAction}
  */
 public final class RestGrantApiKeyAction extends ApiKeyBaseRestHandler {
-
-    final Logger logger = LogManager.getLogger();
 
     static final ObjectParser<GrantApiKeyRequest, Void> PARSER = new ObjectParser<>("grant_api_key_request", GrantApiKeyRequest::new);
     static {
@@ -85,44 +78,16 @@ public final class RestGrantApiKeyAction extends ApiKeyBaseRestHandler {
             if (refresh != null) {
                 grantRequest.setRefreshPolicy(WriteRequest.RefreshPolicy.parse(refresh));
             }
-            return channel -> client.execute(GrantApiKeyAction.INSTANCE, grantRequest, new ResponseListener(channel));
+            return channel -> client.execute(GrantApiKeyAction.INSTANCE, grantRequest,
+                ActionListener.delegateResponse(new RestToXContentListener<>(channel), (listener, ex) -> {
+                    RestStatus status = ExceptionsHelper.status(ex);
+                    if (status == RestStatus.UNAUTHORIZED) {
+                        listener.onFailure(
+                            new ElasticsearchSecurityException("Failed to authenticate api key grant", RestStatus.FORBIDDEN, ex));
+                    } else {
+                        listener.onFailure(ex);
+                    }
+                }));
         }
-    }
-
-    private class ResponseListener implements ActionListener<CreateApiKeyResponse> {
-        private final RestChannel channel;
-
-        ResponseListener(RestChannel channel) {
-            this.channel = channel;
-        }
-
-        @Override
-        public void onResponse(CreateApiKeyResponse response) {
-            try (XContentBuilder builder = channel.newBuilder()) {
-                channel.sendResponse(new BytesRestResponse(RestStatus.OK, response.toXContent(builder, channel.request())));
-            } catch (IOException e) {
-                sendFailure(e);
-            }
-        }
-
-        @Override
-        public void onFailure(Exception e) {
-            RestStatus status = ExceptionsHelper.status(e);
-            if (status == RestStatus.UNAUTHORIZED) {
-                sendFailure(new ElasticsearchSecurityException("Failed to authenticate api key grant", RestStatus.FORBIDDEN, e));
-            } else {
-                sendFailure(e);
-            }
-        }
-
-        void sendFailure(Exception e) {
-            try {
-                channel.sendResponse(new BytesRestResponse(channel, e));
-            } catch (Exception inner) {
-                inner.addSuppressed(e);
-                logger.error("failed to send failure response", inner);
-            }
-        }
-
     }
 }

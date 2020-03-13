b17c48a89a1d85d302368031b931341f2da73e9f
William Brafford <william.brafford@elastic.co>
2020-03-13 13:08:41 -0400

    Remove old getters and setters from tests

--- server/src/test/java/org/elasticsearch/action/admin/cluster/node/stats/NodesStatsRequestTests.java
+++ server/src/test/java/org/elasticsearch/action/admin/cluster/node/stats/NodesStatsRequestTests.java
@@ -24,6 +24,9 @@ import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.test.ESTestCase;
 
+import java.util.List;
+import java.util.Set;
+
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -32,24 +35,12 @@ public class NodesStatsRequestTests extends ESTestCase {
     /**
      * Make sure that we can set, serialize, and deserialize arbitrary sets
      * of metrics.
-     *
-     * TODO: Use a looping construct rather than direct API calls
      */
     public void testMetricsSetters() throws Exception {
         NodesStatsRequest request = new NodesStatsRequest(randomAlphaOfLength(8));
         request.indices(randomFrom(CommonStatsFlags.ALL));
-        request.os(randomBoolean());
-        request.process(randomBoolean());
-        request.jvm(randomBoolean());
-        request.threadPool(randomBoolean());
-        request.fs(randomBoolean());
-        request.transport(randomBoolean());
-        request.http(randomBoolean());
-        request.breaker(randomBoolean());
-        request.script(randomBoolean());
-        request.discovery(randomBoolean());
-        request.ingest(randomBoolean());
-        request.adaptiveSelection(randomBoolean());
+        List<String> metrics = randomSubsetOf(NodesStatsRequest.Metric.allMetrics());
+        request.addMetrics(metrics);
         NodesStatsRequest deserializedRequest = roundTripRequest(request);
         assertRequestsEqual(request, deserializedRequest);
     }
@@ -78,19 +69,6 @@ public class NodesStatsRequestTests extends ESTestCase {
         request.all();
 
         assertThat(request.indices().getFlags(), equalTo(CommonStatsFlags.ALL.getFlags()));
-        assertTrue(request.os());
-        assertTrue(request.process());
-        assertTrue(request.jvm());
-        assertTrue(request.threadPool());
-        assertTrue(request.fs());
-        assertTrue(request.transport());
-        assertTrue(request.http());
-        assertTrue(request.breaker());
-        assertTrue(request.script());
-        assertTrue(request.discovery());
-        assertTrue(request.ingest());
-        assertTrue(request.adaptiveSelection());
-
         assertThat(request.requestedMetrics(), equalTo(NodesStatsRequest.Metric.allMetrics()));
     }
 
@@ -105,19 +83,6 @@ public class NodesStatsRequestTests extends ESTestCase {
         request.clear();
 
         assertThat(request.indices().getFlags(), equalTo(CommonStatsFlags.NONE.getFlags()));
-        assertFalse(request.os());
-        assertFalse(request.process());
-        assertFalse(request.jvm());
-        assertFalse(request.threadPool());
-        assertFalse(request.fs());
-        assertFalse(request.transport());
-        assertFalse(request.http());
-        assertFalse(request.breaker());
-        assertFalse(request.script());
-        assertFalse(request.discovery());
-        assertFalse(request.ingest());
-        assertFalse(request.adaptiveSelection());
-
         assertThat(request.requestedMetrics(), empty());
     }
 
@@ -136,22 +101,7 @@ public class NodesStatsRequestTests extends ESTestCase {
     }
 
     private static void assertRequestsEqual(NodesStatsRequest request1, NodesStatsRequest request2) {
-        // TODO: Use a looping construct rather than direct API calls
         assertThat(request1.indices().getFlags(), equalTo(request2.indices().getFlags()));
-
-        assertThat(request1.os(), equalTo(request2.os()));
-        assertThat(request1.process(), equalTo(request2.process()));
-        assertThat(request1.jvm(), equalTo(request2.jvm()));
-        assertThat(request1.threadPool(), equalTo(request2.threadPool()));
-        assertThat(request1.fs(), equalTo(request2.fs()));
-        assertThat(request1.transport(), equalTo(request2.transport()));
-        assertThat(request1.http(), equalTo(request2.http()));
-        assertThat(request1.breaker(), equalTo(request2.breaker()));
-        assertThat(request1.script(), equalTo(request2.script()));
-        assertThat(request1.discovery(), equalTo(request2.discovery()));
-        assertThat(request1.ingest(), equalTo(request2.ingest()));
-        assertThat(request1.adaptiveSelection(), equalTo(request2.adaptiveSelection()));
-
         assertThat(request1.requestedMetrics(), equalTo(request2.requestedMetrics()));
     }
 }

a6e74d1db57031f1f52cefc7ad39a9db2caa2664
Hakan Altindag <hakangoudberg@hotmail.com>
2020-03-13 10:15:51 +0100

    Importing only specific classes

--- client/rest-high-level/src/main/java/org/elasticsearch/client/RestHighLevelClient.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/RestHighLevelClient.java
@@ -197,7 +197,11 @@ import org.elasticsearch.search.suggest.phrase.PhraseSuggestionBuilder;
 import org.elasticsearch.search.suggest.term.TermSuggestion;
 import org.elasticsearch.search.suggest.term.TermSuggestionBuilder;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;

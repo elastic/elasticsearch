99d4240eb9b54ad7afd360db78a30c5c183a81d9
Tal Levy <jubboy333@gmail.com>
2020-03-13 15:43:55 -0700

    add empty unit tests

---
+++ modules/spatial-extras/src/test/java/org/elasticsearch/spatial/SpatialExtrasTests.java
@@ -0,0 +1,10 @@
+package org.elasticsearch.spatial;
+
+import org.elasticsearch.test.ESTestCase;
+
+public class SpatialExtrasTests extends ESTestCase {
+
+    public void testStub() {
+        // the build expects unit tests to exist in a module, so here one is.
+    }
+}

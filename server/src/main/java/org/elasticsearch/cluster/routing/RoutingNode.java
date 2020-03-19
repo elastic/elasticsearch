9b3b08318d32e486ecdb60f4b58f024c00b38dd2
Mark Vieira <portugee@gmail.com>
2020-03-18 21:07:17 -0700

    Remove unused import

--- server/src/main/java/org/elasticsearch/cluster/routing/RoutingNode.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/RoutingNode.java
@@ -34,7 +34,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**

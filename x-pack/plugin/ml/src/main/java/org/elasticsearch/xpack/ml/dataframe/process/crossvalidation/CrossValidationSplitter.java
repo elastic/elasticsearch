33d11b7d35c32a75d28d00ca1a0174a90b1b6250
Dimitris Athanasiou <dimitris@elastic.co>
2020-03-20 17:20:41 +0200

    [ML] Refactor DFA custom processor to cross validation splitter
    While `CustomProcessor` is generic and allows for flexibility, there
    are new requirements that make cross validation a concept it's hard
    to abstract behind custom processor. In particular, we would like to
    add data_counts to the DFA jobs stats. Counting training VS. test
    docs would be a useful statistic. We would also want to add a
    different cross validation strategy for multiclass classification.
    
    This commit renames custom processors to cross validation splitters
    which allows for those enhancements without cryptically doing
    things as a side effect of the abstract custom processing.

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/dataframe/process/crossvalidation/CrossValidationSplitter.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/dataframe/process/crossvalidation/CrossValidationSplitter.java
@@ -3,12 +3,12 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-package org.elasticsearch.xpack.ml.dataframe.process.customprocessing;
+package org.elasticsearch.xpack.ml.dataframe.process.crossvalidation;
 
 /**
- * A processor to manipulate rows before writing them to the process
+ * Processes rows in order to split the dataset in training and test subsets
  */
-public interface CustomProcessor {
+public interface CrossValidationSplitter {
 
     void process(String[] row);
 }

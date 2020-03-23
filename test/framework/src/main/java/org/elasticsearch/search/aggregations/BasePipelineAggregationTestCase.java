c0612cde62f0abc36d21173c710d6d58884ea699
Nik Everett <nik9000@gmail.com>
2020-03-23 16:18:42 -0400

    Oops

--- test/framework/src/main/java/org/elasticsearch/search/aggregations/BasePipelineAggregationTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/search/aggregations/BasePipelineAggregationTestCase.java
@@ -80,7 +80,7 @@ public abstract class BasePipelineAggregationTestCase<AF extends AbstractPipelin
             .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir())
             .build();
         IndicesModule indicesModule = new IndicesModule(Collections.emptyList());
-        SearchModule searchModule = new SearchModule(settings, false, emptyList());
+        SearchModule searchModule = new SearchModule(settings, false, plugins());
         List<NamedWriteableRegistry.Entry> entries = new ArrayList<>();
         entries.addAll(indicesModule.getNamedWriteables());
         entries.addAll(searchModule.getNamedWriteables());

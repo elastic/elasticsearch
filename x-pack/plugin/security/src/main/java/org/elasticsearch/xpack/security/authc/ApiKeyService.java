4d3fe9e55481c585ea3679370624a9365cd3bdef
Tim Vernum <tim@adjective.org>
2020-03-23 13:39:31 +1100

    Create API Key on behalf of other user
    This change adds a "grant API key action"
    
       POST /_security/api_key/grant
    
    that creates a new API key using the privileges of one user ("the
    system user") to execute the action, but creates the API key with
    the roles of the second user ("the end user").
    
    This allows a system (such as Kibana) to create API keys representing
    the identity and access of an authenticated user without requiring
    that user to have permission to create API keys on their own.
    
    This also creates a new QA project for security on trial licenses and runs
    the API key tests there
    
    Backport of: #52886

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/ApiKeyService.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/ApiKeyService.java
@@ -581,7 +581,7 @@ public class ApiKeyService {
         return enabled && licenseState.isApiKeyServiceAllowed();
     }
 
-    private void ensureEnabled() {
+    public void ensureEnabled() {
         if (licenseState.isApiKeyServiceAllowed() == false) {
             throw LicenseUtils.newComplianceException("api keys");
         }

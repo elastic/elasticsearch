ac9e9c3c18c8c39aab98b2ca79bb4d9e71168c28
Andrei Dan <andrei.dan@elastic.co>
2020-03-24 17:19:10 +0000

    Make CleanupSnapshotStep retryable

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/CleanupSnapshotStep.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/CleanupSnapshotStep.java
@@ -29,7 +29,7 @@ public class CleanupSnapshotStep extends AsyncRetryDuringSnapshotActionStep {
 
     @Override
     public boolean isRetryable() {
-        return false;
+        return true;
     }
 
     @Override

3fe6cea443fd8347465ad5b1691051964cc09e12
Yannick Welsch <yannick@welsch.lu>
2020-03-24 14:48:50 +0100

    checkstyle

--- server/src/main/java/org/elasticsearch/cluster/metadata/IndexMetaData.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/IndexMetaData.java
@@ -1189,7 +1189,8 @@ public class IndexMetaData implements Diffable<IndexMetaData>, ToXContentFragmen
         }
 
         public static void toXContent(IndexMetaData indexMetaData, XContentBuilder builder, ToXContent.Params params) throws IOException {
-            MetaData.XContentContext context = MetaData.XContentContext.valueOf(params.param(CONTEXT_MODE_PARAM, MetaData.CONTEXT_MODE_API));
+            MetaData.XContentContext context = MetaData.XContentContext.valueOf(
+                params.param(CONTEXT_MODE_PARAM, MetaData.CONTEXT_MODE_API));
 
             builder.startObject(indexMetaData.getIndex().getName());
 

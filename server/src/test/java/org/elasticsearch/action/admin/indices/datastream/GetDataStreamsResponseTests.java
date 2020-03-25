3418fa330825db449e3e11e92ab7b009bacb5154
Martijn van Groningen <martijn.v.groningen@gmail.com>
2020-03-25 16:42:35 +0100

    Data stream should refer to the backing indices using the org.elasticsearch.index.Index class. (#54189)

--- server/src/test/java/org/elasticsearch/action/admin/indices/datastream/GetDataStreamsResponseTests.java
+++ server/src/test/java/org/elasticsearch/action/admin/indices/datastream/GetDataStreamsResponseTests.java
@@ -30,6 +30,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.elasticsearch.cluster.metadata.DataStreamTests.randomIndexInstances;
+
 public class GetDataStreamsResponseTests extends AbstractSerializingTestCase<Response> {
 
     @Override
@@ -53,8 +55,7 @@ public class GetDataStreamsResponseTests extends AbstractSerializingTestCase<Res
         int numDataStreams = randomIntBetween(0, 8);
         List<DataStream> dataStreams = new ArrayList<>();
         for (int i = 0; i < numDataStreams; i++) {
-            dataStreams.add(new DataStream(randomAlphaOfLength(4), randomAlphaOfLength(4),
-                Arrays.asList(generateRandomStringArray(8, 4, false))));
+            dataStreams.add(new DataStream(randomAlphaOfLength(4), randomAlphaOfLength(4), randomIndexInstances()));
         }
         return new Response(dataStreams);
     }

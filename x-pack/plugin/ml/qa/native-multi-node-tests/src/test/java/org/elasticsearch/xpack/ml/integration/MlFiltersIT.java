elastic/elasticsearch

commit b0d77828670ccfaf121f5a83873c3a1be04ecde4

[ML] Refactor common utils out of ML plugin to XPack.Core (#39976)
* [ML] Refactor common utils out of ML plugin to XPack.Core

* implementing GET filters with abstract transport

* removing added rest param

* adjusting how defaults can be supplied

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/action/GetFiltersAction.java
@@ -6,23 +6,17 @@
 package org.elasticsearch.xpack.core.ml.action;
 
 import org.elasticsearch.action.Action;
-import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionRequestBuilder;
 import org.elasticsearch.action.ActionRequestValidationException;
-import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.client.ElasticsearchClient;
-import org.elasticsearch.common.Strings;
-import org.elasticsearch.common.io.stream.StreamInput;
-import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.xcontent.StatusToXContentObject;
-import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.rest.RestStatus;
-import org.elasticsearch.xpack.core.ml.action.util.PageParams;
-import org.elasticsearch.xpack.core.ml.action.util.QueryPage;
+import org.elasticsearch.xpack.core.action.AbstractGetResourcesRequest;
+import org.elasticsearch.xpack.core.action.AbstractGetResourcesResponse;
+import org.elasticsearch.xpack.core.action.util.PageParams;
+import org.elasticsearch.xpack.core.action.util.QueryPage;
 import org.elasticsearch.xpack.core.ml.job.config.MlFilter;
 
-import java.io.IOException;
-import java.util.Objects;
 
 import static org.elasticsearch.action.ValidateActions.addValidationError;
 
@@ -41,34 +35,25 @@ public class GetFiltersAction extends Action<GetFiltersAction.Response> {
         return new Response();
     }
 
-    public static class Request extends ActionRequest {
-
-        private String filterId;
-        private PageParams pageParams;
+    public static class Request extends AbstractGetResourcesRequest {
 
         public Request() {
+            // Put our own defaults for backwards compatibility
+            super(null, null, true);
         }
 
         public void setFilterId(String filterId) {
-            this.filterId = filterId;
+            setResourceId(filterId);
         }
 
         public String getFilterId() {
-            return filterId;
-        }
-
-        public PageParams getPageParams() {
-            return pageParams;
-        }
-
-        public void setPageParams(PageParams pageParams) {
-            this.pageParams = pageParams;
+            return getResourceId();
         }
 
         @Override
         public ActionRequestValidationException validate() {
             ActionRequestValidationException validationException = null;
-            if (pageParams != null && filterId != null) {
+            if (getPageParams() != null && getResourceId() != null) {
                 validationException = addValidationError("Params [" + PageParams.FROM.getPreferredName() +
                         ", " + PageParams.SIZE.getPreferredName() + "] are incompatible with ["
                         + MlFilter.ID.getPreferredName() + "]", validationException);
@@ -77,32 +62,8 @@ public class GetFiltersAction extends Action<GetFiltersAction.Response> {
         }
 
         @Override
-        public void readFrom(StreamInput in) throws IOException {
-            super.readFrom(in);
-            filterId = in.readOptionalString();
-        }
-
-        @Override
-        public void writeTo(StreamOutput out) throws IOException {
-            super.writeTo(out);
-            out.writeOptionalString(filterId);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(filterId);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            Request other = (Request) obj;
-            return Objects.equals(filterId, other.filterId);
+        public String getResourceIdField() {
+            return MlFilter.ID.getPreferredName();
         }
     }
 
@@ -113,31 +74,17 @@ public class GetFiltersAction extends Action<GetFiltersAction.Response> {
         }
     }
 
-    public static class Response extends ActionResponse implements StatusToXContentObject {
-
-        private QueryPage<MlFilter> filters;
+    public static class Response extends AbstractGetResourcesResponse<MlFilter> implements StatusToXContentObject {
 
         public Response(QueryPage<MlFilter> filters) {
-            this.filters = filters;
+            super(filters);
         }
 
         public Response() {
         }
 
         public QueryPage<MlFilter> getFilters() {
-            return filters;
-        }
-
-        @Override
-        public void readFrom(StreamInput in) throws IOException {
-            super.readFrom(in);
-            filters = new QueryPage<>(in, MlFilter::new);
-        }
-
-        @Override
-        public void writeTo(StreamOutput out) throws IOException {
-            super.writeTo(out);
-            filters.writeTo(out);
+            return getResources();
         }
 
         @Override
@@ -146,33 +93,8 @@ public class GetFiltersAction extends Action<GetFiltersAction.Response> {
         }
 
         @Override
-        public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
-            builder.startObject();
-            filters.doXContentBody(builder, params);
-            builder.endObject();
-            return builder;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(filters);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            Response other = (Response) obj;
-            return Objects.equals(filters, other.filters);
-        }
-
-        @Override
-        public final String toString() {
-            return Strings.toString(this);
+        protected Reader<MlFilter> getReader() {
+            return MlFilter::new;
         }
     }
 

x-pack/plugin/ml/qa/native-multi-node-tests/src/test/java/org/elasticsearch/xpack/ml/integration/MlNativeAutodetectIntegTestCase.java
@@ -44,7 +44,7 @@ import org.elasticsearch.xpack.core.ml.action.RevertModelSnapshotAction;
 import org.elasticsearch.xpack.core.ml.action.StartDatafeedAction;
 import org.elasticsearch.xpack.core.ml.action.StopDatafeedAction;
 import org.elasticsearch.xpack.core.ml.action.UpdateJobAction;
-import org.elasticsearch.xpack.core.ml.action.util.PageParams;
+import org.elasticsearch.xpack.core.action.util.PageParams;
 import org.elasticsearch.xpack.core.ml.calendars.Calendar;
 import org.elasticsearch.xpack.core.ml.calendars.ScheduledEvent;
 import org.elasticsearch.xpack.core.ml.datafeed.DatafeedConfig;

commit d89ce57b49be78c14eb1535900c7f691e6a9d5ea

[ML] Get ML filters size should default to 100
When get filters is called without setting the `size`
paramter only up to 10 filters are returned. However,
100 filters should be returned. This commit fixes this
and adds an integ test to guard it.

It seems this was accidentally broken in #39976.

Closes #54206

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/action/GetFiltersAction.java
@@ -36,7 +36,7 @@ public class GetFiltersAction extends ActionType<GetFiltersAction.Response> {
 
         public Request() {
             // Put our own defaults for backwards compatibility
-            super(null, null, true);
+            super(null, PageParams.defaultParams(), true);
         }
 
         public Request(StreamInput in) throws IOException {

x-pack/plugin/ml/qa/native-multi-node-tests/src/test/java/org/elasticsearch/xpack/ml/integration/MlFiltersIT.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+package org.elasticsearch.xpack.ml.integration;
+
+import org.elasticsearch.xpack.core.ml.action.GetFiltersAction;
+import org.elasticsearch.xpack.core.ml.job.config.MlFilter;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class MlFiltersIT extends MlNativeIntegTestCase {
+
+    @Override
+    protected void cleanUpResources() {
+        // nothing to clean
+    }
+
+    public void testGetFilters_ShouldReturnUpTo100ByDefault() {
+        int filtersCount = randomIntBetween(11, 100);
+        for (int i = 0; i < filtersCount; i++) {
+            putMlFilter(MlFilter.builder("filter-" + i).setItems("item-" + i).build());
+        }
+
+        GetFiltersAction.Response filters = getMlFilters();
+        assertThat((int) filters.getFilters().count(), equalTo(filtersCount));
+        assertThat(filters.getFilters().results().size(), equalTo(filtersCount));
+    }
+}

x-pack/plugin/ml/qa/native-multi-node-tests/src/test/java/org/elasticsearch/xpack/ml/integration/MlNativeAutodetectIntegTestCase.java
@@ -353,10 +353,6 @@ abstract class MlNativeAutodetectIntegTestCase extends MlNativeIntegTestCase {
         return forecasts;
     }
 
-    protected PutFilterAction.Response putMlFilter(MlFilter filter) {
-        return client().execute(PutFilterAction.INSTANCE, new PutFilterAction.Request(filter)).actionGet();
-    }
-
     protected PutCalendarAction.Response putCalendar(String calendarId, List<String> jobIds, String description) {
         PutCalendarAction.Request request = new PutCalendarAction.Request(new Calendar(calendarId, jobIds, description));
         return client().execute(PutCalendarAction.INSTANCE, request).actionGet();

x-pack/plugin/ml/qa/native-multi-node-tests/src/test/java/org/elasticsearch/xpack/ml/integration/MlNativeIntegTestCase.java
@@ -32,12 +32,15 @@ import org.elasticsearch.xpack.core.ml.MachineLearningField;
 import org.elasticsearch.xpack.core.ml.MlMetadata;
 import org.elasticsearch.xpack.core.ml.MlTasks;
 import org.elasticsearch.xpack.core.ml.action.DeleteExpiredDataAction;
+import org.elasticsearch.xpack.core.ml.action.GetFiltersAction;
 import org.elasticsearch.xpack.core.ml.action.OpenJobAction;
+import org.elasticsearch.xpack.core.ml.action.PutFilterAction;
 import org.elasticsearch.xpack.core.ml.action.StartDataFrameAnalyticsAction;
 import org.elasticsearch.xpack.core.ml.action.StartDatafeedAction;
 import org.elasticsearch.xpack.core.ml.datafeed.DatafeedState;
 import org.elasticsearch.xpack.core.ml.dataframe.DataFrameAnalyticsTaskState;
 import org.elasticsearch.xpack.core.ml.job.config.JobTaskState;
+import org.elasticsearch.xpack.core.ml.job.config.MlFilter;
 import org.elasticsearch.xpack.core.security.SecurityField;
 import org.elasticsearch.xpack.core.security.authc.TokenMetaData;
 import org.elasticsearch.xpack.ilm.IndexLifecycle;
@@ -159,6 +162,14 @@ abstract class MlNativeIntegTestCase extends ESIntegTestCase {
         refresh("*");
 
         return response;
+    }
+
+    protected PutFilterAction.Response putMlFilter(MlFilter filter) {
+        return client().execute(PutFilterAction.INSTANCE, new PutFilterAction.Request(filter)).actionGet();
+    }
+
+    protected GetFiltersAction.Response getMlFilters() {
+        return client().execute(GetFiltersAction.INSTANCE, new GetFiltersAction.Request()).actionGet();
     }
 
     @Override
7f3fbdea69b3286e76dfd1db4a589aef2cffa5ce
Nik Everett <nik9000@gmail.com>
2020-03-26 10:16:34 -0400

    Fix compilation in Eclipse
    These mock calls cause Eclipse to think that `Exception` can be thrown
    because `CheckedFunction`'s lower bound is `Exception`. This makes
    Eclipse happy.

--- server/src/test/java/org/elasticsearch/indices/cluster/ClusterStateChanges.java
+++ server/src/test/java/org/elasticsearch/indices/cluster/ClusterStateChanges.java
@@ -175,7 +175,13 @@ public class ClusterStateChanges {
                     //noinspection unchecked
                     return ((CheckedFunction) invocationOnMock.getArguments()[1]).apply(indexService);
                 });
-        } catch (IOException e) {
+        } catch (Exception e) {
+            /*
+             * Catch Exception because Eclipse uses the lower bound for
+             * CheckedFunction's exception type so it thinks the "when" call
+             * can throw Exception. javac seems to be ok inferring something
+             * else.
+             */
             throw new IllegalStateException(e);
         }
 

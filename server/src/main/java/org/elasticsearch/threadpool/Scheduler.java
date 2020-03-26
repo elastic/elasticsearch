0a34b71f3ca7f5ed6e93955f4c0f2f69291a2037
Yannick Welsch <yannick@welsch.lu>
2020-03-26 09:46:36 +0100

    Schedule commands in current thread context (#54187)
    Changes ThreadPool's schedule method to run the schedule task in the context of the thread
    that scheduled the task.
    
    This is the more sensible default for this method, and eliminates a range of bugs where the
    current thread context is mistakenly dropped.
    
    Closes #17143

--- server/src/main/java/org/elasticsearch/threadpool/Scheduler.java
+++ server/src/main/java/org/elasticsearch/threadpool/Scheduler.java
@@ -83,17 +83,7 @@ public interface Scheduler {
     }
 
     /**
-     * Does nothing by default but can be used by subclasses to save the current thread context and wraps the command in a Runnable
-     * that restores that context before running the command.
-     */
-    default Runnable preserveContext(Runnable command) {
-        return command;
-    }
-
-    /**
-     * Schedules a one-shot command to be run after a given delay. The command is not run in the context of the calling thread.
-     * To preserve the context of the calling thread you may call {@link #preserveContext(Runnable)} on the runnable before passing
-     * it to this method.
+     * Schedules a one-shot command to be run after a given delay. The command is run in the context of the calling thread.
      * The command runs on scheduler thread. Do not run blocking calls on the scheduler thread. Subclasses may allow
      * to execute on a different executor, in which case blocking calls are allowed.
      *

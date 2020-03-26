elastic/elasticsearch

commit e8c54c73dbee2877637c8dd076d82fb5a94e1b47

Save a little space in agg tree (#53730)
This drop the "top level" pipeline aggregators from the aggregation
result tree which should save a little memory and a few serialization
bytes. Perhaps more imporantly, this provides a mechanism by which we
can remove *all* pipelines from the aggregation result tree. This will
save quite a bit of space when pipelines are deep in the tree.

Sadly, doing this isn't simple because of backwards compatibility. Nodes
before 7.7.0 *need* those pipelines. We provide them by setting passing
a `Supplier<PipelineTree>` into the root of the aggregation tree that we
only call if we need to serialize to a version before 7.7.0.

This solution works for cross cluster search because we always reduce
the aggregations in each remote cluster and then forward them back to
the coordinating node. Its quite possible that the coordinating node
needs the pipeline (say it is version 7.1.0) and the gateway node in the
remote cluster doesn't (version 7.7.0). In that case the data nodes
won't send the pipeline aggregations back to the gateway node.
Critically, the gateway node *will* send the pipeline aggregations back
to the coordinating node. This is all managed with that
`Supplier<PipelineTree>`, but *how* it is managed is a bit tricky.

server/src/main/java/org/elasticsearch/search/aggregations/InternalAggregation.java
@@ -18,6 +18,7 @@
  */
 package org.elasticsearch.search.aggregations;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.NamedWriteable;
 import org.elasticsearch.common.io.stream.StreamInput;
@@ -37,7 +38,9 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.IntConsumer;
+import java.util.function.Supplier;
 
+import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -62,12 +65,19 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
         private final ScriptService scriptService;
         private final IntConsumer multiBucketConsumer;
         private final PipelineTree pipelineTreeRoot;
+        /**
+         * Supplies the pipelines when the result of the reduce is serialized
+         * to node versions that need pipeline aggregators to be serialized
+         * to them.
+         */
+        private final Supplier<PipelineTree> pipelineTreeForBwcSerialization;
 
         /**
          * Build a {@linkplain ReduceContext} to perform a partial reduction.
          */
-        public static ReduceContext forPartialReduction(BigArrays bigArrays, ScriptService scriptService) {
-            return new ReduceContext(bigArrays, scriptService, (s) -> {}, null);
+        public static ReduceContext forPartialReduction(BigArrays bigArrays, ScriptService scriptService,
+                Supplier<PipelineTree> pipelineTreeForBwcSerialization) {
+            return new ReduceContext(bigArrays, scriptService, (s) -> {}, null, pipelineTreeForBwcSerialization);
         }
 
         /**
@@ -77,15 +87,16 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
         public static ReduceContext forFinalReduction(BigArrays bigArrays, ScriptService scriptService,
                 IntConsumer multiBucketConsumer, PipelineTree pipelineTreeRoot) {
             return new ReduceContext(bigArrays, scriptService, multiBucketConsumer,
-                    requireNonNull(pipelineTreeRoot, "prefer EMPTY to null"));
+                    requireNonNull(pipelineTreeRoot, "prefer EMPTY to null"), () -> pipelineTreeRoot);
         }
 
         private ReduceContext(BigArrays bigArrays, ScriptService scriptService, IntConsumer multiBucketConsumer,
-                PipelineTree pipelineTreeRoot) {
+                PipelineTree pipelineTreeRoot, Supplier<PipelineTree> pipelineTreeForBwcSerialization) {
             this.bigArrays = bigArrays;
             this.scriptService = scriptService;
             this.multiBucketConsumer = multiBucketConsumer;
             this.pipelineTreeRoot = pipelineTreeRoot;
+            this.pipelineTreeForBwcSerialization = pipelineTreeForBwcSerialization;
         }
 
         /**
@@ -113,6 +124,15 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
         }
 
         /**
+         * Supplies the pipelines when the result of the reduce is serialized
+         * to node versions that need pipeline aggregators to be serialized
+         * to them.
+         */
+        public Supplier<PipelineTree> pipelineTreeForBwcSerialization() {
+            return pipelineTreeForBwcSerialization;
+        }
+
+        /**
          * Adds {@code count} buckets to the global count for the request and fails if this number is greater than
          * the maximum number of buckets allowed in a response
          */
@@ -129,9 +149,9 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
     private final List<PipelineAggregator> pipelineAggregators;
 
     /**
-     * Constructs an get with a given name.
+     * Constructs an aggregation result with a given name.
      *
-     * @param name The name of the get.
+     * @param name The name of the aggregation.
      */
     protected InternalAggregation(String name, List<PipelineAggregator> pipelineAggregators, Map<String, Object> metaData) {
         this.name = name;
@@ -145,14 +165,20 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
     protected InternalAggregation(StreamInput in) throws IOException {
         name = in.readString();
         metaData = in.readMap();
-        pipelineAggregators = in.readNamedWriteableList(PipelineAggregator.class);
+        if (in.getVersion().before(Version.V_8_0_0)) {
+            pipelineAggregators = in.readNamedWriteableList(PipelineAggregator.class);
+        } else {
+            pipelineAggregators = emptyList();
+        }
     }
 
     @Override
     public final void writeTo(StreamOutput out) throws IOException {
         out.writeString(name);
         out.writeGenericValue(metaData);
-        out.writeNamedWriteableList(pipelineAggregators);
+        if (out.getVersion().before(Version.V_8_0_0)) {
+            out.writeNamedWriteableList(pipelineAggregators);
+        }
         doWriteTo(out);
     }
 

commit c1734b3b068ccc77ae46a53690dcee11fa36ed34

Fix pipeline agg serialization for ccs
This fixes pipeline aggregations used in cross cluster search from an older
version of Elasticsearch to a newer version of Elasticsearch. I broke
this in #53730 when I was too aggressive in shutting off serialization
of pipeline aggs. In particular, this comes up when the coordinating
node is pre-7.8.0 and the gateway node is on or after 7.8.0.

The fix is another step down the line to remove pipeline aggregators
from the aggregation tree. Sort of. It create a new
`List<PipelineAggregator>` member in `InternalAggregation` *but* it is
only used for bwc serialization and it is fed by the mechanism
established in #53730 to read the pipelines from the

server/src/main/java/org/elasticsearch/search/aggregations/InternalAggregation.java
@@ -36,6 +36,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.IntConsumer;
 import java.util.function.Supplier;
@@ -147,6 +148,7 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
     protected final Map<String, Object> metaData;
 
     private final List<PipelineAggregator> pipelineAggregators;
+    private List<PipelineAggregator> pipelineAggregatorsForBwcSerialization;
 
     /**
      * Constructs an aggregation result with a given name.
@@ -160,15 +162,24 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
     }
 
     /**
+     * Merge a {@linkplain PipelineAggregator.PipelineTree} into this
+     * aggregation result tree before serializing to a node older than
+     * 7.8.0.
+     */
+    public final void mergePipelineTreeForBWCSerialization(PipelineAggregator.PipelineTree pipelineTree) {
+        pipelineAggregatorsForBwcSerialization = pipelineTree.aggregators();
+        forEachBucket(bucketAggs -> bucketAggs.mergePipelineTreeForBWCSerialization(pipelineTree));
+    }
+
+    /**
      * Read from a stream.
      */
     protected InternalAggregation(StreamInput in) throws IOException {
         name = in.readString();
         metaData = in.readMap();
+        pipelineAggregators = emptyList();
         if (in.getVersion().before(Version.V_7_8_0)) {
-            pipelineAggregators = in.readNamedWriteableList(PipelineAggregator.class);
-        } else {
-            pipelineAggregators = emptyList();
+            in.readNamedWriteableList(PipelineAggregator.class);
         }
     }
 
@@ -177,7 +188,9 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
         out.writeString(name);
         out.writeGenericValue(metaData);
         if (out.getVersion().before(Version.V_7_8_0)) {
-            out.writeNamedWriteableList(pipelineAggregators);
+            assert pipelineAggregatorsForBwcSerialization != null :
+                "serializing to pre-7.8.0 versions should have called mergePipelineTreeForBWCSerialization";
+            out.writeNamedWriteableList(pipelineAggregatorsForBwcSerialization);
         }
         doWriteTo(out);
     }
@@ -211,6 +224,11 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
         throw new IllegalStateException(
                 "Aggregation [" + getName() + "] must be a bucket aggregation but was [" + getWriteableName() + "]");
     }
+
+    /**
+     * Run a {@linkplain Consumer} over all buckets in this aggregation.
+     */
+    public void forEachBucket(Consumer<InternalAggregations> consumer) {}
 
     /**
      * Creates the output from all pipeline aggs that this aggregation is associated with.  Should only
@@ -278,8 +296,21 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
         return metaData;
     }
 
+    /**
+     * @deprecated soon to be removed because it is not longer needed
+     */
+    @Deprecated
     public List<PipelineAggregator> pipelineAggregators() {
         return pipelineAggregators;
+    }
+
+    /**
+     * The {@linkplain PipelineAggregator}s sent to older versions of Elasticsearch.
+     * @deprecated only use these for serializing to older Elasticsearch versions
+     */
+    @Deprecated
+    public List<PipelineAggregator> pipelineAggregatorsForBwcSerialization() {
+        return pipelineAggregatorsForBwcSerialization;
     }
 
     @Override

server/src/main/java/org/elasticsearch/search/aggregations/InternalAggregations.java
@@ -100,16 +100,31 @@ public final class InternalAggregations extends Aggregations implements Writeabl
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public void writeTo(StreamOutput out) throws IOException {
-        out.writeNamedWriteableList((List<InternalAggregation>)aggregations);
         if (out.getVersion().before(Version.V_7_8_0)) {
             if (pipelineTreeForBwcSerialization == null) {
+                out.writeNamedWriteableList(getInternalAggregations());
                 out.writeNamedWriteableList(emptyList());
             } else {
-                out.writeNamedWriteableList(pipelineTreeForBwcSerialization.get().aggregators());
+                PipelineAggregator.PipelineTree pipelineTree = pipelineTreeForBwcSerialization.get();
+                mergePipelineTreeForBWCSerialization(pipelineTree);
+                out.writeNamedWriteableList(getInternalAggregations());
+                out.writeNamedWriteableList(pipelineTree.aggregators());
             }
+        } else {
+            out.writeNamedWriteableList(getInternalAggregations());
         }
+    }
+
+    /**
+     * Merge a {@linkplain PipelineAggregator.PipelineTree} into this
+     * aggregation result tree before serializing to a node older than
+     * 7.8.0.
+     */
+    public void mergePipelineTreeForBWCSerialization(PipelineAggregator.PipelineTree pipelineTree) {
+        getInternalAggregations().stream().forEach(agg -> {
+            agg.mergePipelineTreeForBWCSerialization(pipelineTree.subTree(agg.getName()));
+        });
     }
 
     /**
@@ -133,6 +148,14 @@ public final class InternalAggregations extends Aggregations implements Writeabl
         return pipelineTreeForBwcSerialization.get().aggregators().stream()
                 .map(p -> (SiblingPipelineAggregator) p)
                 .collect(toList());
+    }
+
+    /**
+     * Get the transient pipeline tree used to serialize pipeline aggregators to old nodes.
+     */
+    @Deprecated
+    Supplier<PipelineAggregator.PipelineTree> getPipelineTreeForBwcSerialization() {
+        return pipelineTreeForBwcSerialization;
     }
 
     @SuppressWarnings("unchecked")

server/src/main/java/org/elasticsearch/search/aggregations/InternalMultiBucketAggregation.java
@@ -30,6 +30,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public abstract class InternalMultiBucketAggregation<A extends InternalMultiBucketAggregation,
@@ -145,7 +146,7 @@ public abstract class InternalMultiBucketAggregation<A extends InternalMultiBuck
     }
 
     /**
-     * Amulti-bucket agg needs to first reduce the buckets and *their* pipelines
+     * A multi-bucket agg needs to first reduce the buckets and *their* pipelines
      * before allowing sibling pipelines to materialize.
      */
     @Override
@@ -171,6 +172,13 @@ public abstract class InternalMultiBucketAggregation<A extends InternalMultiBuck
             newBuckets.add(newBucket);
         }
         return modified ? create(newBuckets) : this;
+    }
+
+    @Override
+    public void forEachBucket(Consumer<InternalAggregations> consumer) {
+        for (B bucket : getBuckets()) {
+            consumer.accept((InternalAggregations) bucket.getAggregations());
+        }
     }
 
     private List<B> reducePipelineBuckets(ReduceContext reduceContext, PipelineTree pipelineTree) {

server/src/main/java/org/elasticsearch/search/aggregations/bucket/InternalSingleBucketAggregation.java
@@ -34,6 +34,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
@@ -179,6 +180,11 @@ public abstract class InternalSingleBucketAggregation extends InternalAggregatio
             return this;
         }
         return create(rewritten);
+    }
+
+    @Override
+    public void forEachBucket(Consumer<InternalAggregations> consumer) {
+        consumer.accept(aggregations);
     }
 
     @Override

server/src/test/java/org/elasticsearch/search/aggregations/InternalAggregationsTests.java
@@ -88,6 +88,10 @@ public class InternalAggregationsTests extends ESTestCase {
     }
 
     public static InternalAggregations createTestInstance() throws Exception {
+        return createTestInstance(randomPipelineTree());
+    }
+
+    public static InternalAggregations createTestInstance(PipelineAggregator.PipelineTree pipelineTree) throws Exception {
         List<InternalAggregation> aggsList = new ArrayList<>();
         if (randomBoolean()) {
             StringTermsTests stringTermsTests = new StringTermsTests();
@@ -104,7 +108,7 @@ public class InternalAggregationsTests extends ESTestCase {
             InternalSimpleValueTests simpleValueTests = new InternalSimpleValueTests();
             aggsList.add(simpleValueTests.createTestInstance());
         }
-        return new InternalAggregations(aggsList);
+        return new InternalAggregations(aggsList, () -> pipelineTree);
     }
 
     private static PipelineAggregator.PipelineTree randomPipelineTree() {
@@ -129,11 +133,9 @@ public class InternalAggregationsTests extends ESTestCase {
     }
 
     public void testGetTopLevelPipelineAggregators() throws Exception {
-        InternalAggregations orig = createTestInstance();
-        PipelineAggregator.PipelineTree tree = randomPipelineTree();
-        InternalAggregations withPipelines = new InternalAggregations(orig.copyResults(), () -> tree);
-        assertThat(withPipelines.aggregations, equalTo(orig.aggregations));
-        assertThat(withPipelines.getTopLevelPipelineAggregators(), equalTo(tree.aggregators()));
+        PipelineAggregator.PipelineTree pipelineTree = randomPipelineTree();
+        InternalAggregations aggs = createTestInstance(pipelineTree);
+        assertThat(aggs.getTopLevelPipelineAggregators(), equalTo(pipelineTree.aggregators()));
     }
 
     private void writeToAndReadFrom(InternalAggregations aggregations, int iteration) throws IOException {
@@ -146,8 +148,14 @@ public class InternalAggregationsTests extends ESTestCase {
                 InternalAggregations deserialized = new InternalAggregations(in);
                 assertEquals(aggregations.aggregations, deserialized.aggregations);
                 if (iteration < 2) {
-                    //serialize this enough times to make sure that we are able to write again what we read
-                    writeToAndReadFrom(deserialized, iteration + 1);
+                    /*
+                     * Add the pipeline tree for bwc serialization just like we
+                     * do when we merge the aggregation. Without that we can't
+                     * properly serialize to older versions.
+                     */
+                    InternalAggregations asThoughReduced = new InternalAggregations(
+                        deserialized.copyResults(), aggregations.getPipelineTreeForBwcSerialization());
+                    writeToAndReadFrom(asThoughReduced, iteration + 1);
                 }
             }
         }

server/src/test/java/org/elasticsearch/search/aggregations/bucket/significant/SignificanceHeuristicTests.java
@@ -102,6 +102,9 @@ public class SignificanceHeuristicTests extends ESTestCase {
         ByteArrayOutputStream outBuffer = new ByteArrayOutputStream();
         OutputStreamStreamOutput out = new OutputStreamStreamOutput(outBuffer);
         out.setVersion(version);
+        if (version.before(Version.V_7_8_0)) {
+            sigTerms.mergePipelineTreeForBWCSerialization(PipelineAggregator.PipelineTree.EMPTY);
+        }
         out.writeNamedWriteable(sigTerms);
 
         // read

test/framework/src/main/java/org/elasticsearch/test/InternalAggregationTestCase.java
@@ -126,10 +126,12 @@ import org.elasticsearch.search.aggregations.metrics.SumAggregationBuilder;
 import org.elasticsearch.search.aggregations.metrics.TopHitsAggregationBuilder;
 import org.elasticsearch.search.aggregations.metrics.ValueCountAggregationBuilder;
 import org.elasticsearch.search.aggregations.metrics.WeightedAvgAggregationBuilder;
+import org.elasticsearch.search.aggregations.pipeline.AvgBucketPipelineAggregationBuilder;
 import org.elasticsearch.search.aggregations.pipeline.DerivativePipelineAggregationBuilder;
 import org.elasticsearch.search.aggregations.pipeline.ExtendedStatsBucketPipelineAggregationBuilder;
 import org.elasticsearch.search.aggregations.pipeline.InternalBucketMetricValue;
 import org.elasticsearch.search.aggregations.pipeline.InternalSimpleValue;
+import org.elasticsearch.search.aggregations.pipeline.MaxBucketPipelineAggregationBuilder;
 import org.elasticsearch.search.aggregations.pipeline.ParsedBucketMetricValue;
 import org.elasticsearch.search.aggregations.pipeline.ParsedDerivative;
 import org.elasticsearch.search.aggregations.pipeline.ParsedExtendedStatsBucket;
@@ -140,6 +142,7 @@ import org.elasticsearch.search.aggregations.pipeline.PercentilesBucketPipelineA
 import org.elasticsearch.search.aggregations.pipeline.PipelineAggregator;
 import org.elasticsearch.search.aggregations.pipeline.PipelineAggregator.PipelineTree;
 import org.elasticsearch.search.aggregations.pipeline.StatsBucketPipelineAggregationBuilder;
+import org.elasticsearch.search.aggregations.pipeline.SumBucketPipelineAggregationBuilder;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -153,6 +156,7 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonMap;
 import static org.elasticsearch.common.xcontent.XContentHelper.toXContent;
 import static org.elasticsearch.search.aggregations.InternalMultiBucketAggregation.countInnerBucket;
@@ -382,6 +386,52 @@ public abstract class InternalAggregationTestCase<T extends InternalAggregation>
         final T aggregation = createTestInstance();
         final ParsedAggregation parsedAggregation = parseAndAssert(aggregation, randomBoolean(), true);
         assertFromXContent(aggregation, parsedAggregation);
+    }
+
+    public void testMergePipelineTreeForBWCSerialization() {
+        T agg = createTestInstance();
+        PipelineAggregator.PipelineTree pipelineTree = randomPipelineTree(agg);
+        agg.mergePipelineTreeForBWCSerialization(pipelineTree);
+        assertMergedPipelineTreeForBWCSerialization(agg, pipelineTree);
+    }
+
+    public static PipelineAggregator.PipelineTree randomPipelineTree(InternalAggregation aggregation) {
+        Map<String, PipelineTree> subTree = new HashMap<>();
+        aggregation.forEachBucket(bucketAggs -> {
+            for (Aggregation subAgg : bucketAggs) {
+                if (subTree.containsKey(subAgg.getName())) {
+                    continue;
+                }
+                subTree.put(subAgg.getName(), randomPipelineTree((InternalAggregation) subAgg));
+            }
+        });
+        return new PipelineAggregator.PipelineTree(emptyMap(), randomPipelineAggregators());
+    }
+
+    public static List<PipelineAggregator> randomPipelineAggregators() {
+        List<PipelineAggregator> pipelines = new ArrayList<>();
+        if (randomBoolean()) {
+            if (randomBoolean()) {
+                pipelines.add(new MaxBucketPipelineAggregationBuilder("name1", "bucket1").create());
+            }
+            if (randomBoolean()) {
+                pipelines.add(new AvgBucketPipelineAggregationBuilder("name2", "bucket2").create());
+            }
+            if (randomBoolean()) {
+                pipelines.add(new SumBucketPipelineAggregationBuilder("name3", "bucket3").create());
+            }
+        }
+        return pipelines;
+    }
+
+    @SuppressWarnings("deprecation")
+    private void assertMergedPipelineTreeForBWCSerialization(InternalAggregation agg, PipelineAggregator.PipelineTree pipelineTree) {
+        assertThat(agg.pipelineAggregatorsForBwcSerialization(), equalTo(pipelineTree.aggregators()));
+        agg.forEachBucket(bucketAggs -> {
+            for (Aggregation subAgg : bucketAggs) {
+                assertMergedPipelineTreeForBWCSerialization((InternalAggregation) subAgg, pipelineTree.subTree(subAgg.getName()));
+            }
+        });
     }
 
     protected abstract void assertFromXContent(T aggregation, ParsedAggregation parsedAggregation) throws IOException;
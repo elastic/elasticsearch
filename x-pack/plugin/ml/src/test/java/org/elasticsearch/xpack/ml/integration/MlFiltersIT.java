3853aa0ce7fec862e974589913bb6b327d2429b7
Dimitris Athanasiou <dimitris@elastic.co>
2020-03-26 10:31:29 +0200

    Make test single node and further fixes

--- x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/integration/MlFiltersIT.java
+++ x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/integration/MlFiltersIT.java
@@ -7,24 +7,29 @@
 package org.elasticsearch.xpack.ml.integration;
 
 import org.elasticsearch.xpack.core.ml.action.GetFiltersAction;
+import org.elasticsearch.xpack.core.ml.action.PutFilterAction;
 import org.elasticsearch.xpack.core.ml.job.config.MlFilter;
+import org.elasticsearch.xpack.ml.MlSingleNodeTestCase;
+import org.junit.Before;
 
 import static org.hamcrest.Matchers.equalTo;
 
-public class MlFiltersIT extends MlNativeIntegTestCase {
+public class MlFiltersIT extends MlSingleNodeTestCase {
 
-    @Override
-    protected void cleanUpResources() {
-        // nothing to clean
+    @Before
+    public void beforeTests() throws Exception {
+        waitForMlTemplates();
     }
 
     public void testGetFilters_ShouldReturnUpTo100ByDefault() {
         int filtersCount = randomIntBetween(11, 100);
         for (int i = 0; i < filtersCount; i++) {
-            putMlFilter(MlFilter.builder("filter-" + i).setItems("item-" + i).build());
+            PutFilterAction.Request putFilterRequest = new PutFilterAction.Request(
+                MlFilter.builder("filter-" + i).setItems("item-" + i).build());
+            client().execute(PutFilterAction.INSTANCE, putFilterRequest).actionGet();
         }
 
-        GetFiltersAction.Response filters = getMlFilters();
+        GetFiltersAction.Response filters = client().execute(GetFiltersAction.INSTANCE, new GetFiltersAction.Request()).actionGet();
         assertThat((int) filters.getFilters().count(), equalTo(filtersCount));
         assertThat(filters.getFilters().results().size(), equalTo(filtersCount));
     }

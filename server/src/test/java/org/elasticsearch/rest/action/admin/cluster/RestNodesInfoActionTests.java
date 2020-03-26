b11960e3e618f26d0db6249d002f7c9dc305b28b
William Brafford <william.brafford@elastic.co>
2020-03-26 07:28:09 -0400

    Create set-based interface for NodesInfoRequest (#53410) (#54223)
    This commit begins the work of removing the "hard-coded" metric getters
    and setters from the NodesInfoRequest classes. We start by providing new
    flexible getters and setters. We then update the test classes to remove
    the old getters, and then remove those getters.

--- server/src/test/java/org/elasticsearch/rest/action/admin/cluster/RestNodesInfoActionTests.java
+++ server/src/test/java/org/elasticsearch/rest/action/admin/cluster/RestNodesInfoActionTests.java
@@ -20,71 +20,77 @@
 package org.elasticsearch.rest.action.admin.cluster;
 
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequest;
+import org.elasticsearch.common.util.set.Sets;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.rest.FakeRestRequest;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.elasticsearch.rest.action.admin.cluster.RestNodesInfoAction.ALLOWED_METRICS;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
 
 public class RestNodesInfoActionTests extends ESTestCase {
 
     public void testDuplicatedFiltersAreNotRemoved() {
         Map<String, String> params = new HashMap<>();
         params.put("nodeId", "_all,master:false,_all");
-        
+
         RestRequest restRequest = buildRestRequest(params);
         NodesInfoRequest actual = RestNodesInfoAction.prepareRequest(restRequest);
         assertArrayEquals(new String[] { "_all", "master:false", "_all" }, actual.nodesIds());
     }
-    
+
     public void testOnlyMetrics() {
         Map<String, String> params = new HashMap<>();
         int metricsCount = randomIntBetween(1, ALLOWED_METRICS.size());
         List<String> metrics = new ArrayList<>();
-        
+
         for(int i = 0; i < metricsCount; i++) {
             metrics.add(randomFrom(ALLOWED_METRICS));
         }
         params.put("nodeId", String.join(",", metrics));
-        
+
         RestRequest restRequest = buildRestRequest(params);
         NodesInfoRequest actual = RestNodesInfoAction.prepareRequest(restRequest);
         assertArrayEquals(new String[] { "_all" }, actual.nodesIds());
         assertMetrics(metrics, actual);
     }
-    
+
     public void testAllMetricsSelectedWhenNodeAndMetricSpecified() {
         Map<String, String> params = new HashMap<>();
         String nodeId = randomValueOtherThanMany(ALLOWED_METRICS::contains, () -> randomAlphaOfLength(23));
         String metric = randomFrom(ALLOWED_METRICS);
-        
+
         params.put("nodeId", nodeId + "," + metric);
         RestRequest restRequest = buildRestRequest(params);
-        
+
         NodesInfoRequest actual = RestNodesInfoAction.prepareRequest(restRequest);
         assertArrayEquals(new String[] { nodeId, metric }, actual.nodesIds());
         assertAllMetricsTrue(actual);
     }
-    
+
     public void testSeparateNodeIdsAndMetrics() {
         Map<String, String> params = new HashMap<>();
         List<String> nodeIds = new ArrayList<>(5);
         List<String> metrics = new ArrayList<>(5);
-        
+
         for(int i = 0; i < 5; i++) {
             nodeIds.add(randomValueOtherThanMany(ALLOWED_METRICS::contains, () -> randomAlphaOfLength(23)));
             metrics.add(randomFrom(ALLOWED_METRICS));
         }
-        
+
         params.put("nodeId", String.join(",", nodeIds));
         params.put("metrics", String.join(",", metrics));
         RestRequest restRequest = buildRestRequest(params);
-        
+
         NodesInfoRequest actual = RestNodesInfoAction.prepareRequest(restRequest);
         assertArrayEquals(nodeIds.toArray(), actual.nodesIds());
         assertMetrics(metrics, actual);
@@ -97,14 +103,40 @@ public class RestNodesInfoActionTests extends ESTestCase {
         for(int i = 0; i < 5; i++) {
             nodeIds.add(randomValueOtherThanMany(ALLOWED_METRICS::contains, () -> randomAlphaOfLength(23)));
         }
-        
+
         params.put("nodeId", String.join(",", nodeIds));
         params.put("metrics", "_all");
         RestRequest restRequest = buildRestRequest(params);
-        
+
         NodesInfoRequest actual = RestNodesInfoAction.prepareRequest(restRequest);
         assertArrayEquals(nodeIds.toArray(), actual.nodesIds());
         assertAllMetricsTrue(actual);
+    }
+
+    /**
+     * Test that if a user requests a non-existent metric, it's dropped from the
+     * request without an error.
+     */
+    public void testNonexistentMetricsDropped() {
+        Map<String, String> params = new HashMap<>();
+        List<String> nodeIds = new ArrayList<>(5);
+        List<String> metrics = new ArrayList<>(5);
+
+        for(int i = 0; i < 5; i++) {
+            nodeIds.add(randomValueOtherThanMany(ALLOWED_METRICS::contains, () -> randomAlphaOfLength(23)));
+            metrics.add(randomFrom(ALLOWED_METRICS));
+        }
+        String nonAllowedMetric = randomValueOtherThanMany(ALLOWED_METRICS::contains, () -> randomAlphaOfLength(5));
+        metrics.add(nonAllowedMetric);
+
+        params.put("nodeId", String.join(",", nodeIds));
+        params.put("metrics", String.join(",", metrics));
+        RestRequest restRequest = buildRestRequest(params);
+
+        NodesInfoRequest actual = RestNodesInfoAction.prepareRequest(restRequest);
+        assertArrayEquals(nodeIds.toArray(), actual.nodesIds());
+        assertThat(actual.requestedMetrics(), not(hasItem(nonAllowedMetric)));
+        assertMetrics(metrics, actual);
     }
 
     private FakeRestRequest buildRestRequest(Map<String, String> params) {
@@ -114,30 +146,13 @@ public class RestNodesInfoActionTests extends ESTestCase {
                 .withParams(params)
                 .build();
     }
-    
+
     private void assertMetrics(List<String> metrics, NodesInfoRequest nodesInfoRequest) {
-        assertTrue((metrics.contains("http") && nodesInfoRequest.http()) || metrics.contains("http") == false);
-        assertTrue((metrics.contains("ingest") && nodesInfoRequest.ingest()) || metrics.contains("ingest") == false);
-        assertTrue((metrics.contains("indices") && nodesInfoRequest.indices()) || metrics.contains("indices") == false);
-        assertTrue((metrics.contains("jvm") && nodesInfoRequest.jvm()) || metrics.contains("jvm") == false);
-        assertTrue((metrics.contains("os") && nodesInfoRequest.os()) || metrics.contains("os") == false);
-        assertTrue((metrics.contains("plugins") && nodesInfoRequest.plugins()) || metrics.contains("plugins") == false);
-        assertTrue((metrics.contains("process") && nodesInfoRequest.process()) || metrics.contains("process") == false);
-        assertTrue((metrics.contains("settings") && nodesInfoRequest.settings()) || metrics.contains("settings") == false);
-        assertTrue((metrics.contains("thread_pool") && nodesInfoRequest.threadPool()) || metrics.contains("thread_pool") == false);
-        assertTrue((metrics.contains("transport") && nodesInfoRequest.transport()) || metrics.contains("transport") == false);
+        Set<String> validRequestedMetrics = Sets.intersection(new HashSet<>(metrics), ALLOWED_METRICS);
+        assertThat(nodesInfoRequest.requestedMetrics(), equalTo(validRequestedMetrics));
     }
-    
+
     private void assertAllMetricsTrue(NodesInfoRequest nodesInfoRequest) {
-        assertTrue(nodesInfoRequest.http());
-        assertTrue(nodesInfoRequest.ingest());
-        assertTrue(nodesInfoRequest.indices());
-        assertTrue(nodesInfoRequest.jvm());
-        assertTrue(nodesInfoRequest.os());
-        assertTrue(nodesInfoRequest.plugins());
-        assertTrue(nodesInfoRequest.process());
-        assertTrue(nodesInfoRequest.settings());
-        assertTrue(nodesInfoRequest.threadPool());
-        assertTrue(nodesInfoRequest.transport());
+        assertThat(nodesInfoRequest.requestedMetrics(), equalTo(ALLOWED_METRICS));
     }
 }

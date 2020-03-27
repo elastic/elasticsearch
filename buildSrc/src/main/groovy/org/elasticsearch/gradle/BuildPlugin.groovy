9c80ca8cf0268ba9a16428cdb1381464d8e9ff5c
Ryan Ernst <ryan@iernst.net>
2020-03-27 14:49:12 -0700

    apply no-transitive rules to runtimeOnly

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/BuildPlugin.groovy
@@ -284,6 +284,7 @@ class BuildPlugin implements Plugin<Project> {
         project.configurations.getByName(JavaPlugin.COMPILE_CONFIGURATION_NAME).dependencies.all(disableTransitiveDeps)
         project.configurations.getByName(JavaPlugin.TEST_COMPILE_CONFIGURATION_NAME).dependencies.all(disableTransitiveDeps)
         project.configurations.getByName(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME).dependencies.all(disableTransitiveDeps)
+        project.configurations.getByName(JavaPlugin.RUNTIME_ONLY_CONFIGURATION_NAME).dependencies.all(disableTransitiveDeps)
     }
 
     /** Adds repositories used by ES dependencies */

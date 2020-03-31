599aacff467d5e9bacb8d3f0cbc6a4d11d594774
Yannick Welsch <yannick@welsch.lu>
2020-03-31 16:18:23 +0200

    Avoid holding onto bulk items until all completed (#54407)
    Bulk requests currently keep a reference to all bulk item requests until every one of them has
    completed. There is no need to do so, however, and, in case of large bulks, can mean
    unnecessary holding onto memory that might be better used elsewhere. More so as different
    shard-level bulks can complete at different speeds, and one slow shard-level request should
    not require holding onto every other shard-level request.

--- server/src/main/java/org/elasticsearch/action/bulk/TransportBulkAction.java
+++ server/src/main/java/org/elasticsearch/action/bulk/TransportBulkAction.java
@@ -388,7 +388,7 @@ public class TransportBulkAction extends HandledTransportAction<BulkRequest, Bul
      * */
     private final class BulkOperation extends ActionRunnable<BulkResponse> {
         private final Task task;
-        private final BulkRequest bulkRequest;
+        private BulkRequest bulkRequest; // set to null once all requests are sent out
         private final AtomicArray<BulkItemResponse> responses;
         private final long startTimeNanos;
         private final ClusterStateObserver observer;
@@ -407,6 +407,7 @@ public class TransportBulkAction extends HandledTransportAction<BulkRequest, Bul
 
         @Override
         protected void doRun() {
+            assert bulkRequest != null;
             final ClusterState clusterState = observer.setAndGetObservedState();
             if (handleBlockExceptions(clusterState)) {
                 return;
@@ -525,6 +526,7 @@ public class TransportBulkAction extends HandledTransportAction<BulkRequest, Bul
                     }
                 });
             }
+            bulkRequest = null; // allow memory for bulk request items to be reclaimed before all items have been completed
         }
 
         private boolean handleBlockExceptions(ClusterState state) {

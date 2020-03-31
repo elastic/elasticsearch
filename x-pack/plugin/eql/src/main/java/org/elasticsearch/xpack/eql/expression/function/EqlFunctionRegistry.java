977302e46c0be754206f5ae524340e578720fd3d
Andrei Stefan <astefan@users.noreply.github.com>
2020-03-31 18:06:03 +0300

    EQL: startsWith and endsWith functions implementation (#54504)
    * EQL: startsWith function implementation (#54400)
    
    (cherry picked from commit 666719fcfc40f6fc0535609577791369123320ab)
    
    * EQL: endsWith function implementation (#54442)
    
    (cherry picked from commit 554a4c8ef04b67eed107d29b57185e9af25d9d4f)

--- x-pack/plugin/eql/src/main/java/org/elasticsearch/xpack/eql/expression/function/EqlFunctionRegistry.java
+++ x-pack/plugin/eql/src/main/java/org/elasticsearch/xpack/eql/expression/function/EqlFunctionRegistry.java
@@ -6,7 +6,9 @@
 
 package org.elasticsearch.xpack.eql.expression.function;
 
+import org.elasticsearch.xpack.eql.expression.function.scalar.string.EndsWith;
 import org.elasticsearch.xpack.eql.expression.function.scalar.string.Length;
+import org.elasticsearch.xpack.eql.expression.function.scalar.string.StartsWith;
 import org.elasticsearch.xpack.eql.expression.function.scalar.string.Substring;
 import org.elasticsearch.xpack.ql.expression.function.FunctionDefinition;
 import org.elasticsearch.xpack.ql.expression.function.FunctionRegistry;
@@ -24,7 +26,9 @@ public class EqlFunctionRegistry extends FunctionRegistry {
         // Scalar functions
         // String
             new FunctionDefinition[] {
+                def(EndsWith.class, EndsWith::new, "endswith"),
                 def(Length.class, Length::new, "length"),
+                def(StartsWith.class, StartsWith::new, "startswith"),
                 def(Substring.class, Substring::new, "substring")
             }
         };

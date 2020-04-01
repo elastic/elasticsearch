b134de3548ab1323694dcaf09f862d99e81ad287
Mark Vieira <portugee@gmail.com>
2020-04-01 13:45:45 -0700

    Improve total build configuration time
    This commit includes a number of changes to reduce overall build
    configuration time. These optimizations include:
    
     - Removing the usage of the 'nebula.info-scm' plugin. This plugin
       leverages jgit to load read various pieces of VCS information. This
       is mostly overkill and we have our own minimal implementation for
       determining the current commit id.
     - Removing unnecessary build dependencies such as perforce and jgit
       now that we don't need them. This reduces our classpath considerably.
     - Expanding the usage lazy task creation, particularly in our
       distribution projects. The archives and packages projects create
       lots of tasks with very complex configuration. Avoiding the creation
       of these tasks at configuration time gives us a nice boost.

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/plugin/PluginBuildPlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/plugin/PluginBuildPlugin.groovy
@@ -37,6 +37,7 @@ import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.bundling.Zip
 
 import java.util.regex.Matcher
@@ -68,7 +69,7 @@ class PluginBuildPlugin implements Plugin<Project> {
             project.description = extension1.description
             configurePublishing(project, extension1)
 
-            project.tasks.integTest.dependsOn(project.tasks.bundlePlugin)
+            project.tasks.named('integTest').configure { dependsOn(project.tasks.named('bundlePlugin')) }
             if (isModule) {
                 project.testClusters.integTest.module(
                         project.file(project.tasks.bundlePlugin.archiveFile)
@@ -98,7 +99,7 @@ class PluginBuildPlugin implements Plugin<Project> {
             if (extension1.classname == null) {
                 throw new InvalidUserDataException('classname is a required setting for esplugin')
             }
-            Copy buildProperties = project.tasks.getByName('pluginProperties')
+
             Map<String, String> properties = [
                     'name'                : extension1.name,
                     'description'         : extension1.description,
@@ -110,8 +111,10 @@ class PluginBuildPlugin implements Plugin<Project> {
                     'hasNativeController' : extension1.hasNativeController,
                     'requiresKeystore'    : extension1.requiresKeystore
             ]
-            buildProperties.expand(properties)
-            buildProperties.inputs.properties(properties)
+            project.tasks.named('pluginProperties').configure {
+                expand(properties)
+                inputs.properties(properties)
+            }
             if (isModule == false || isXPackModule) {
                 addNoticeGeneration(project, extension1)
             }
@@ -181,7 +184,7 @@ class PluginBuildPlugin implements Plugin<Project> {
         File templateFile = new File(project.buildDir, "templates/plugin-descriptor.properties")
 
         // create tasks to build the properties file for this plugin
-        Task copyPluginPropertiesTemplate = project.tasks.create('copyPluginPropertiesTemplate') {
+        TaskProvider<Task> copyPluginPropertiesTemplate = project.tasks.register('copyPluginPropertiesTemplate') {
             outputs.file(templateFile)
             doLast {
                 InputStream resourceTemplate = PluginBuildPlugin.getResourceAsStream("/${templateFile.name}")
@@ -189,7 +192,7 @@ class PluginBuildPlugin implements Plugin<Project> {
             }
         }
 
-        Copy buildProperties = project.tasks.create('pluginProperties', Copy) {
+        TaskProvider<Copy> buildProperties = project.tasks.register('pluginProperties', Copy) {
             dependsOn(copyPluginPropertiesTemplate)
             from(templateFile)
             into("${project.buildDir}/generated-resources")
@@ -198,11 +201,11 @@ class PluginBuildPlugin implements Plugin<Project> {
         // add the plugin properties and metadata to test resources, so unit tests can
         // know about the plugin (used by test security code to statically initialize the plugin in unit tests)
         SourceSet testSourceSet = project.sourceSets.test
-        testSourceSet.output.dir(buildProperties.destinationDir, builtBy: buildProperties)
+        testSourceSet.output.dir("${project.buildDir}/generated-resources", builtBy: buildProperties)
         testSourceSet.resources.srcDir(pluginMetadata)
 
         // create the actual bundle task, which zips up all the files for the plugin
-        Zip bundle = project.tasks.create(name: 'bundlePlugin', type: Zip) {
+        TaskProvider<Zip> bundle = project.tasks.register('bundlePlugin', Zip) {
             from buildProperties
             from pluginMetadata // metadata (eg custom security policy)
             /*
@@ -250,16 +253,21 @@ class PluginBuildPlugin implements Plugin<Project> {
     protected static void addNoticeGeneration(Project project, PluginPropertiesExtension extension) {
         File licenseFile = extension.licenseFile
         if (licenseFile != null) {
-            project.tasks.bundlePlugin.from(licenseFile.parentFile) {
-                include(licenseFile.name)
-                rename { 'LICENSE.txt' }
+            project.tasks.named('bundlePlugin').configure {
+                from(licenseFile.parentFile) {
+                    include(licenseFile.name)
+                    rename { 'LICENSE.txt' }
+                }
             }
         }
         File noticeFile = extension.noticeFile
         if (noticeFile != null) {
-            NoticeTask generateNotice = project.tasks.create('generateNotice', NoticeTask.class)
-            generateNotice.inputFile = noticeFile
-            project.tasks.bundlePlugin.from(generateNotice)
+            TaskProvider<NoticeTask> generateNotice = project.tasks.register('generateNotice', NoticeTask) {
+                inputFile = noticeFile
+            }
+            project.tasks.named('bundlePlugin').configure {
+                from(generateNotice)
+            }
         }
     }
 }

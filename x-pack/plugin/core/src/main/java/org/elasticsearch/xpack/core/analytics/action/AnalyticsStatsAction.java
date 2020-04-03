77f7b090bf2c61712680bc10272ee8829b53c2c5
Igor Motov <igor@motovs.org>
2020-04-03 15:12:38 -0400

    Update version in serialization of t-test agg usage stats
    Update version in the t-test agg usage stats serialization
    after backport to 7.8.0
    
    Relates to #53692

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/analytics/action/AnalyticsStatsAction.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/analytics/action/AnalyticsStatsAction.java
@@ -146,7 +146,7 @@ public class AnalyticsStatsAction extends ActionType<AnalyticsStatsAction.Respon
                 stringStatsUsage = 0;
                 topMetricsUsage = 0;
             }
-            if (in.getVersion().onOrAfter(Version.V_8_0_0)) { // Will drop to 7.8.0 after backport
+            if (in.getVersion().onOrAfter(Version.V_7_8_0)) {
                 ttestUsage = in.readVLong();
             } else {
                 ttestUsage = 0;
@@ -164,7 +164,7 @@ public class AnalyticsStatsAction extends ActionType<AnalyticsStatsAction.Respon
                 out.writeVLong(stringStatsUsage);
                 out.writeVLong(topMetricsUsage);
             }
-            if (out.getVersion().onOrAfter(Version.V_8_0_0)) { // Will drop to 7.8.0 after backport
+            if (out.getVersion().onOrAfter(Version.V_7_8_0)) {
                 out.writeVLong(ttestUsage);
             }
         }

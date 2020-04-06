e6b7cc93faa3edd1b0dba6a58ccb64f0a624c785
Jack Conradson <osjdconrad@gmail.com>
2020-04-06 09:27:09 -0700

    Move not a statement responsibility to user tree expression nodes (#54443)
    This removes the "statement" field from the user tree expression nodes 
    output. Instead, we delegate responsibility to check whether or not 
    something is a statement to each individual user tree expression node 
    based on whether or not the result is read from using input from the 
    parent. This removes unnecessary state from the output, is more reliable 
    as each node can determine its own correct behavior, and results in better 
    "not a statement" error messages to give the user an idea of what is 
    considered not a statement.
    
    Relates #53702

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/node/PBrace.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/node/PBrace.java
@@ -60,6 +60,10 @@ public class PBrace extends AStoreable {
 
     @Override
     Output analyze(ClassNode classNode, ScriptRoot scriptRoot, Scope scope, AStoreable.Input input) {
+        if (input.read == false && input.write == false) {
+            throw createError(new IllegalArgumentException("not a statement: result of brace operator not used"));
+        }
+
         Output output = new Output();
 
         Input prefixInput = new Input();

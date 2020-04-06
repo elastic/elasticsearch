70a7e57064cf77cdde34df1953161792e46acf3b
Costin Leau <costin.leau@gmail.com>
2020-04-06 13:37:29 +0300

    QL: Introduce infrastructure for substituting functions
    Some functions act as shortcuts for more verbose declarations (sometimes
    with certain constraints). This PR removes the boilerplate around
    declaring such functions as well as a dedicated rule for the optimizer
    to perform the actual substitution.
    
    Fix #54334

--- x-pack/plugin/ql/src/main/java/org/elasticsearch/xpack/ql/optimizer/OptimizerRules.java
+++ x-pack/plugin/ql/src/main/java/org/elasticsearch/xpack/ql/optimizer/OptimizerRules.java
@@ -9,6 +9,7 @@ import org.elasticsearch.xpack.ql.expression.Expression;
 import org.elasticsearch.xpack.ql.expression.Expressions;
 import org.elasticsearch.xpack.ql.expression.Literal;
 import org.elasticsearch.xpack.ql.expression.Order;
+import org.elasticsearch.xpack.ql.expression.function.scalar.SubstituteFunction;
 import org.elasticsearch.xpack.ql.expression.predicate.BinaryOperator;
 import org.elasticsearch.xpack.ql.expression.predicate.BinaryPredicate;
 import org.elasticsearch.xpack.ql.expression.predicate.Negatable;
@@ -999,6 +1000,21 @@ public final class OptimizerRules {
             return false;
         }
 
+    }
+
+    public static class ReplaceSubstituteFunction extends OptimizerExpressionRule {
+
+        public ReplaceSubstituteFunction() {
+            super(TransformDirection.DOWN);
+        }
+
+        @Override
+        protected Expression rule(Expression e) {
+            if (e instanceof SubstituteFunction) {
+                e = ((SubstituteFunction) e).substitute();
+            }
+            return e;
+        }
     }
     
     public static final class PruneFilters extends OptimizerRule<Filter> {

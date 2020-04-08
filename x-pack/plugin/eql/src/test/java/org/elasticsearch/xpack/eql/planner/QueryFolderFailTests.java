d02f774cb6a6aaf7aa456391ac865a9306abcc13
Aleksandr Maus <aleksandr.maus@elastic.co>
2020-04-07 22:07:28 -0400

    EQL: implement cidrMatch function (#54186) (#54928)
    Related to https://github.com/elastic/elasticsearch/issues/54132

--- x-pack/plugin/eql/src/test/java/org/elasticsearch/xpack/eql/planner/QueryFolderFailTests.java
+++ x-pack/plugin/eql/src/test/java/org/elasticsearch/xpack/eql/planner/QueryFolderFailTests.java
@@ -62,6 +62,53 @@ public class QueryFolderFailTests extends AbstractQueryFolderTestCase {
                 error("process where between(process_name, \"s\", \"e\", false, 2)"));
     }
 
+    public void testCIDRMatchNonIPField() {
+        VerificationException e = expectThrows(VerificationException.class,
+                () -> plan("process where cidrMatch(hostname, \"10.0.0.0/8\")"));
+        String msg = e.getMessage();
+        assertEquals("Found 1 problem\n" +
+                "line 1:15: first argument of [cidrMatch(hostname, \"10.0.0.0/8\")] must be [ip], found value [hostname] type [text]", msg);
+    }
+
+    public void testCIDRMatchMissingValue() {
+        ParsingException e = expectThrows(ParsingException.class,
+                () -> plan("process where cidrMatch(source_address)"));
+        String msg = e.getMessage();
+        assertEquals("line 1:16: error building [cidrmatch]: expects at least two arguments", msg);
+    }
+
+    public void testCIDRMatchAgainstField() {
+        VerificationException e = expectThrows(VerificationException.class,
+                () -> plan("process where cidrMatch(source_address, hostname)"));
+        String msg = e.getMessage();
+        assertEquals("Found 1 problem\n" +
+                "line 1:15: second argument of [cidrMatch(source_address, hostname)] must be a constant, received [hostname]", msg);
+    }
+
+    public void testCIDRMatchNonString() {
+        VerificationException e = expectThrows(VerificationException.class,
+                () -> plan("process where cidrMatch(source_address, 12345)"));
+        String msg = e.getMessage();
+        assertEquals("Found 1 problem\n" +
+                "line 1:15: argument of [cidrMatch(source_address, 12345)] must be [string], found value [12345] type [integer]", msg);
+    }
+
+    public void testEndsWithFunctionWithInexact() {
+        VerificationException e = expectThrows(VerificationException.class,
+                () -> plan("process where endsWith(plain_text, \"foo\") == true"));
+        String msg = e.getMessage();
+        assertEquals("Found 1 problem\nline 1:15: [endsWith(plain_text, \"foo\")] cannot operate on first argument field of data type "
+                + "[text]: No keyword/multi-field defined exact matches for [plain_text]; define one or use MATCH/QUERY instead", msg);
+    }
+
+    public void testLengthFunctionWithInexact() {
+        VerificationException e = expectThrows(VerificationException.class,
+                () -> plan("process where length(plain_text) > 0"));
+        String msg = e.getMessage();
+        assertEquals("Found 1 problem\nline 1:15: [length(plain_text)] cannot operate on field of data type [text]: No keyword/multi-field "
+                + "defined exact matches for [plain_text]; define one or use MATCH/QUERY instead", msg);
+    }
+
     public void testPropertyEquationFilterUnsupported() {
         QlIllegalArgumentException e = expectThrows(QlIllegalArgumentException.class,
                 () -> plan("process where (serial_event_id<9 and serial_event_id >= 7) or (opcode == pid)"));
@@ -75,22 +122,6 @@ public class QueryFolderFailTests extends AbstractQueryFolderTestCase {
         String msg = e.getMessage();
         assertEquals("Found 1 problem\nline 1:35: Comparisons against variables are not (currently) supported; " +
                 "offender [parent_process_name] in [process_name in (parent_process_name, \"SYSTEM\")]", msg);
-    }
-
-    public void testLengthFunctionWithInexact() {
-        VerificationException e = expectThrows(VerificationException.class,
-                () -> plan("process where length(plain_text) > 0"));
-        String msg = e.getMessage();
-        assertEquals("Found 1 problem\nline 1:15: [length(plain_text)] cannot operate on field of data type [text]: No keyword/multi-field "
-                + "defined exact matches for [plain_text]; define one or use MATCH/QUERY instead", msg);
-    }
-
-    public void testEndsWithFunctionWithInexact() {
-        VerificationException e = expectThrows(VerificationException.class,
-                () -> plan("process where endsWith(plain_text, \"foo\") == true"));
-        String msg = e.getMessage();
-        assertEquals("Found 1 problem\nline 1:15: [endsWith(plain_text, \"foo\")] cannot operate on first argument field of data type "
-                + "[text]: No keyword/multi-field defined exact matches for [plain_text]; define one or use MATCH/QUERY instead", msg);
     }
 
     public void testStartsWithFunctionWithInexact() {

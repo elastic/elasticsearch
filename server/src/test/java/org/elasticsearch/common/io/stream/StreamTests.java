473988fbd4ffa0a08b8ae75116a50aadee9c926b
Ryan Ernst <ryan@iernst.net>
2020-04-07 23:25:05 -0700

    fix checkstyle

--- server/src/test/java/org/elasticsearch/common/io/stream/StreamTests.java
+++ server/src/test/java/org/elasticsearch/common/io/stream/StreamTests.java
@@ -45,7 +45,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;

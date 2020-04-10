a0cb977f23e71ee8fc93cadd21b8a83435b054a2
Jason Tedor <jason@tedor.me>
2020-04-10 08:38:00 -0400

    Clarify available processors (#54907)
    The use of available processors, the terminology, and the settings
    around it have evolved over time. This commit cleans up some places in
    the codes and in the docs to adjust to the current terminology.

--- server/src/main/java/org/elasticsearch/monitor/os/OsService.java
+++ server/src/main/java/org/elasticsearch/monitor/os/OsService.java
@@ -45,7 +45,7 @@ public class OsService {
     public OsService(Settings settings) throws IOException {
         this.probe = OsProbe.getInstance();
         TimeValue refreshInterval = REFRESH_INTERVAL_SETTING.get(settings);
-        this.info = probe.osInfo(refreshInterval.millis(), EsExecutors.numberOfProcessors(settings));
+        this.info = probe.osInfo(refreshInterval.millis(), EsExecutors.allocatedProcessors(settings));
         this.osStatsCache = new OsStatsCache(refreshInterval, probe.osStats());
         logger.debug("using refresh_interval [{}]", refreshInterval);
     }

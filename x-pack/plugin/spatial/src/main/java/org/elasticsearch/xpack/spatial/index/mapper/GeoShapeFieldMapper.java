2ce6df94a8da943343e50f9de1b2a0e41a0a1004
Tal Levy <jubboy333@gmail.com>
2020-04-13 10:40:14 -0700

    fix extra doc-value-field addition

--- x-pack/plugin/spatial/src/main/java/org/elasticsearch/xpack/spatial/index/mapper/GeoShapeFieldMapper.java
+++ x-pack/plugin/spatial/src/main/java/org/elasticsearch/xpack/spatial/index/mapper/GeoShapeFieldMapper.java
@@ -137,7 +137,6 @@ public class GeoShapeFieldMapper extends AbstractGeometryFieldMapper<Geometry, G
                         } else {
                             docValuesField.add(triangles, calculator);
                         }
-                        fields.add(docValuesField);
                     }
                     return fields;
                 }

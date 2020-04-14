1754e50cbd2dd80aba6f943cd9b1a57d9dd322ed
Igor Motov <igor@motovs.org>
2020-04-14 17:03:14 -0400

    [7.x] Add analytics plugin usage stats to _xpack/usage (#54911) (#55162)
    Adds analytics plugin usage stats to _xpack/usage.
    
    Closes #54847

--- x-pack/plugin/analytics/src/test/java/org/elasticsearch/xpack/analytics/action/TransportAnalyticsStatsActionTests.java
+++ x-pack/plugin/analytics/src/test/java/org/elasticsearch/xpack/analytics/action/TransportAnalyticsStatsActionTests.java
@@ -22,6 +22,7 @@ import org.elasticsearch.test.rest.yaml.ObjectPath;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 import org.elasticsearch.xpack.analytics.AnalyticsUsage;
+import org.elasticsearch.xpack.core.analytics.AnalyticsFeatureSetUsage;
 import org.elasticsearch.xpack.core.analytics.action.AnalyticsStatsAction;
 
 import java.io.IOException;
@@ -54,40 +55,36 @@ public class TransportAnalyticsStatsActionTests extends ESTestCase {
         when(clusterService.state()).thenReturn(clusterState);
 
         return new TransportAnalyticsStatsAction(transportService, clusterService, threadPool,
-                new ActionFilters(Collections.emptySet()), usage);
+            new ActionFilters(Collections.emptySet()), usage);
     }
 
     public void test() throws IOException {
-        for (AnalyticsUsage.Item item : AnalyticsUsage.Item.values()) {
+        for (AnalyticsStatsAction.Item item : AnalyticsStatsAction.Item.values()) {
             AnalyticsUsage realUsage = new AnalyticsUsage();
             AnalyticsUsage emptyUsage = new AnalyticsUsage();
             ContextParser<Void, Void> parser = realUsage.track(item, (p, c) -> c);
             ObjectPath unused = run(realUsage, emptyUsage);
-            assertThat(unused.evaluate("stats.0." + item.name().toLowerCase(Locale.ROOT) + "_usage"), equalTo(0));
-            assertThat(unused.evaluate("stats.1." + item.name().toLowerCase(Locale.ROOT) + "_usage"), equalTo(0));
+            assertThat(unused.evaluate("stats." + item.name().toLowerCase(Locale.ROOT) + "_usage"), equalTo(0));
             int count = between(1, 10000);
             for (int i = 0; i < count; i++) {
                 assertNull(parser.parse(null, null));
             }
             ObjectPath used = run(realUsage, emptyUsage);
-            assertThat(item.name(), used.evaluate("stats.0." + item.name().toLowerCase(Locale.ROOT) + "_usage"), equalTo(count));
-            assertThat(item.name(), used.evaluate("stats.1." + item.name().toLowerCase(Locale.ROOT) + "_usage"), equalTo(0));
+            assertThat(item.name(), used.evaluate("stats." + item.name().toLowerCase(Locale.ROOT) + "_usage"), equalTo(count));
         }
     }
 
     private ObjectPath run(AnalyticsUsage... nodeUsages) throws IOException {
         AnalyticsStatsAction.Request request = new AnalyticsStatsAction.Request();
         List<AnalyticsStatsAction.NodeResponse> nodeResponses = Arrays.stream(nodeUsages)
-                .map(usage -> action(usage).nodeOperation(new AnalyticsStatsAction.NodeRequest(request)))
-                .collect(toList());
+            .map(usage -> action(usage).nodeOperation(new AnalyticsStatsAction.NodeRequest(request)))
+            .collect(toList());
         AnalyticsStatsAction.Response response = new AnalyticsStatsAction.Response(
-                new ClusterName("cluster_name"), nodeResponses, emptyList());
+            new ClusterName("cluster_name"), nodeResponses, emptyList());
 
+        AnalyticsFeatureSetUsage usage = new AnalyticsFeatureSetUsage(true, true, response);
         try (XContentBuilder builder = jsonBuilder()) {
-            builder.startObject();
-            response.toXContent(builder, ToXContent.EMPTY_PARAMS);
-            builder.endObject();
-
+            usage.toXContent(builder, ToXContent.EMPTY_PARAMS);
             return ObjectPath.createFromXContent(JsonXContent.jsonXContent, BytesReference.bytes(builder));
         }
     }

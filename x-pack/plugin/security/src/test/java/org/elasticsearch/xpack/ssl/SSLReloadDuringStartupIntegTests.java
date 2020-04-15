2db3a9a6b724bdf6adf01972a9be8e2504a74698
jaymode <jay@elastic.co>
2020-04-15 13:47:47 -0600

    add comments

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/ssl/SSLReloadDuringStartupIntegTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/ssl/SSLReloadDuringStartupIntegTests.java
@@ -4,25 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/*
- * Licensed to Elasticsearch under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package org.elasticsearch.xpack.ssl;
 
 import org.elasticsearch.common.settings.MockSecureSettings;
@@ -74,7 +55,11 @@ public class SSLReloadDuringStartupIntegTests extends SecurityIntegTestCase {
     public void testReloadDuringStartup() throws Exception {
         final String node = randomFrom(internalCluster().getNodeNames());
         final Environment env = internalCluster().getInstance(Environment.class, node);
+        // this latch is to synchronize the start of the thread that updates the TLS config
+        // and the continuation of the node restart
         final CountDownLatch latch = new CountDownLatch(2);
+        // this latch is used by the test to signal when the updated TLS configuration has been
+        // written so that the test should proceed with checks to ensure the node rejoins the cluster
         final CountDownLatch writtenLatch = new CountDownLatch(1);
 
         // restart the node
@@ -85,7 +70,7 @@ public class SSLReloadDuringStartupIntegTests extends SecurityIntegTestCase {
                 Path keystorePath = env.configFile().resolve("testnode.jks");
                 Path updatedKeystorePath = getDataPath("/org/elasticsearch/xpack/security/transport/ssl/certs/simple/testnode_updated.jks");
                 assertTrue(Files.exists(keystorePath));
-                // replace the keystore
+                // replace the keystore with one that will fail during handshaking
                 copyAndAtomicMoveIfPossible(updatedKeystorePath, keystorePath);
                 new Thread(() -> {
                     latch.countDown();
@@ -96,6 +81,7 @@ public class SSLReloadDuringStartupIntegTests extends SecurityIntegTestCase {
                         Thread.currentThread().interrupt();
                     }
 
+                    // replace the bad keystore with a new copy of the good one
                     try {
                         copyAndAtomicMoveIfPossible(origKeystorePath, keystorePath);
                         writtenLatch.countDown();

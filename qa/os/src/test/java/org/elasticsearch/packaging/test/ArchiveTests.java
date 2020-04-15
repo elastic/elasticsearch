d8ef77e24a5cf4756bf41671f16632f7c94e6abb
Ryan Ernst <ryan@iernst.net>
2020-04-15 12:50:24 -0700

    only run test with bundled jdk

--- qa/os/src/test/java/org/elasticsearch/packaging/test/ArchiveTests.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/test/ArchiveTests.java
@@ -237,6 +237,7 @@ public class ArchiveTests extends PackagingTestCase {
     }
 
     public void test54ForceBundledJdkEmptyJavaHome() throws Exception {
+        assumeThat(distribution().hasJdk, is(true));
         // cleanup from previous test
         rm(installation.config("elasticsearch.keystore"));
 

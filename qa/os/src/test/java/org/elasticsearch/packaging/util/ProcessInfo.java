91557a52967334c3594e31f1852e952d59fe03d8
Rory Hunter <roryhunter2@gmail.com>
2020-04-16 14:26:20 +0100

    Add missing license and javadoc

--- qa/os/src/test/java/org/elasticsearch/packaging/util/ProcessInfo.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/util/ProcessInfo.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.elasticsearch.packaging.util;
 
 import java.util.List;
@@ -6,6 +25,12 @@ import java.util.stream.Collectors;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 
+/**
+ * Encapsulates the fetching of information about a running process.
+ *
+ * This is helpful on stripped-down images where, in order to fetch full information about a process,
+ * we have to consult <code>/proc</code>.
+ */
 public class ProcessInfo {
     public final int pid;
     public final int uid;
@@ -21,6 +46,10 @@ public class ProcessInfo {
         this.group = group;
     }
 
+    /**
+     * Fetches process information about <code>command</code>, using <code>sh</code> to execute commands.
+     * @return a populated <code>ProcessInfo</code> object
+     */
     public static ProcessInfo getProcessInfo(Shell sh, String command) {
         final List<String> processes = sh.run("pgrep " + command).stdout.lines().collect(Collectors.toList());
 

71855fbfe0650793596a423b9db0d26a86faca87
Tanguy Leroux <tlrx.dev@gmail.com>
2020-04-17 10:49:05 +0200

    Mute testSupportedFieldTypes in HDRPreAggregatedPercentile tests (#55369)
    Relates #55360

--- x-pack/plugin/analytics/src/test/java/org/elasticsearch/xpack/analytics/mapper/HDRPreAggregatedPercentilesAggregatorTests.java
+++ x-pack/plugin/analytics/src/test/java/org/elasticsearch/xpack/analytics/mapper/HDRPreAggregatedPercentilesAggregatorTests.java
@@ -158,4 +158,10 @@ public class HDRPreAggregatedPercentilesAggregatorTests extends AggregatorTestCa
             }
         }
     }
+
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/55360")
+    @Override
+    public void testSupportedFieldTypes() throws IOException {
+        super.testSupportedFieldTypes();
+    }
 }

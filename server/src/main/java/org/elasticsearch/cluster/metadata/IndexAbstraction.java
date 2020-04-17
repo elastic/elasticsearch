5db8714a83aa209534123e82aab9f295bcb08a63
Dan Hermann <danhermann@users.noreply.github.com>
2020-04-17 09:02:09 -0500

    Add explicit generation attribute to data streams (#55342)

--- server/src/main/java/org/elasticsearch/cluster/metadata/IndexAbstraction.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/IndexAbstraction.java
@@ -31,6 +31,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import static org.elasticsearch.cluster.metadata.DataStream.getBackingIndexName;
 import static org.elasticsearch.cluster.metadata.IndexMetadata.INDEX_HIDDEN_SETTING;
 import static org.elasticsearch.common.collect.List.copyOf;
 
@@ -266,12 +267,11 @@ public interface IndexAbstraction {
         private final List<IndexMetadata> dataStreamIndices;
         private final IndexMetadata writeIndex;
 
-        public DataStream(org.elasticsearch.cluster.metadata.DataStream dataStream,
-                          List<IndexMetadata> dataStreamIndices, IndexMetadata writeIndex) {
+        public DataStream(org.elasticsearch.cluster.metadata.DataStream dataStream, List<IndexMetadata> dataStreamIndices) {
             this.dataStream = dataStream;
             this.dataStreamIndices = copyOf(dataStreamIndices);
-            this.writeIndex = writeIndex;
-            assert dataStreamIndices.contains(writeIndex);
+            this.writeIndex =  dataStreamIndices.get(dataStreamIndices.size() - 1);
+            assert writeIndex.getIndex().getName().equals(getBackingIndexName(dataStream.getName(), dataStream.getGeneration()));
         }
 
         @Override

419be4c841164edd065f93e7f844a14241d8f0c8
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2020-04-17 11:18:38 -0400

    fixing unused import

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/inference/TrainedModelStatsService.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/inference/TrainedModelStatsService.java
@@ -39,7 +39,6 @@ import org.elasticsearch.xpack.ml.utils.persistence.ResultsPersisterService;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;

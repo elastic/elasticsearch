1768c6d21bd38ec6d3064b5d33ed69937c97fde2
Jake Landis <jake.landis@elastic.co>
2020-04-20 17:31:35 -0500

    hard code schema location

--- buildSrc/src/main/java/org/elasticsearch/gradle/precommit/ValidateRestSpecPlugin.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/precommit/ValidateRestSpecPlugin.java
@@ -22,11 +22,11 @@ package org.elasticsearch.gradle.precommit;
 import org.elasticsearch.gradle.util.Util;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.file.FileTree;
 import org.gradle.api.provider.Provider;
 
+import java.io.File;
+
 public class ValidateRestSpecPlugin implements Plugin<Project> {
-    private static final String SCHEMA_PROJECT = ":rest-api-spec";
     private static final String DOUBLE_STAR = "**"; // checkstyle thinks these are javadocs :(
 
     @Override
@@ -37,21 +37,7 @@ public class ValidateRestSpecPlugin implements Plugin<Project> {
                     filter.include(DOUBLE_STAR + "/rest-api-spec/api/" + DOUBLE_STAR + "/*.json");
                     filter.exclude(DOUBLE_STAR + "/_common.json");
                 }));
-                FileTree jsonSchemas = Util.getJavaMainSourceResources(
-                    project.findProject(SCHEMA_PROJECT),
-                    filter -> filter.include(DOUBLE_STAR + "/schema.json")
-                );
-
-                if (jsonSchemas == null || jsonSchemas.getFiles().size() != 1) {
-                    throw new IllegalStateException(
-                        String.format(
-                            "Could not find the schema file from glob pattern [%s] and project [%s] for JSON spec validation",
-                            DOUBLE_STAR + "/schema.json",
-                            SCHEMA_PROJECT
-                        )
-                    );
-                }
-                task.setJsonSchema(jsonSchemas.iterator().next());
+                task.setJsonSchema(new File(project.getRootDir(), "rest-api-spec/src/main/resources/schema.json"));
             });
         project.getTasks().named("precommit").configure(t -> t.dependsOn(validateRestSpecTask));
     }

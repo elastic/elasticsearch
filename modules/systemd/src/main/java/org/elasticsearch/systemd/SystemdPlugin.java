cfde1ee56f42a059cf7c12119d06de680e8cfb0f
Jason Tedor <jason@tedor.me>
2020-04-20 17:30:45 -0400

    Further consistency

--- modules/systemd/src/main/java/org/elasticsearch/systemd/SystemdPlugin.java
+++ modules/systemd/src/main/java/org/elasticsearch/systemd/SystemdPlugin.java
@@ -97,24 +97,24 @@ public class SystemdPlugin extends Plugin implements ClusterPlugin {
         final Supplier<RepositoriesService> repositoriesServiceSupplier) {
         if (enabled == false) {
             extender.set(null);
-        } else {
-            /*
-             * Since we have set the service type to notify, by default systemd will wait up to sixty seconds for the process to send the
-             * READY=1 status via sd_notify. Since our startup can take longer than that (e.g., if we are upgrading on-disk metadata) then
-             * we need to repeatedly notify systemd that we are still starting up by sending EXTEND_TIMEOUT_USEC with an extension to the
-             * timeout. Therefore, every fifteen seconds we send systemd a message via sd_notify to extend the timeout by thirty seconds.
-             * We will cancel this scheduled task after we successfully notify systemd that we are ready.
-             */
-            extender.set(threadPool.scheduleWithFixedDelay(
-                () -> {
-                    final int rc = sd_notify(0, "EXTEND_TIMEOUT_USEC=30000000");
-                    if (rc < 0) {
-                        logger.warn("extending startup timeout via sd_notify failed with [{}]", rc);
-                    }
-                },
-                TimeValue.timeValueSeconds(15),
-                ThreadPool.Names.SAME));
+            return List.of();
         }
+        /*
+         * Since we have set the service type to notify, by default systemd will wait up to sixty seconds for the process to send the
+         * READY=1 status via sd_notify. Since our startup can take longer than that (e.g., if we are upgrading on-disk metadata) then we
+         * need to repeatedly notify systemd that we are still starting up by sending EXTEND_TIMEOUT_USEC with an extension to the timeout.
+         * Therefore, every fifteen seconds we send systemd a message via sd_notify to extend the timeout by thirty seconds. We will cancel
+         * this scheduled task after we successfully notify systemd that we are ready.
+         */
+        extender.set(threadPool.scheduleWithFixedDelay(
+            () -> {
+                final int rc = sd_notify(0, "EXTEND_TIMEOUT_USEC=30000000");
+                if (rc < 0) {
+                    logger.warn("extending startup timeout via sd_notify failed with [{}]", rc);
+                }
+            },
+            TimeValue.timeValueSeconds(15),
+            ThreadPool.Names.SAME));
         return List.of();
     }
 

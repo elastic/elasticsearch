4ed0dc8703ffdb5c3a3f52cbb3ec0f616080aba1
Tim Brooks <tim@uncontended.net>
2020-04-21 12:17:05 -0600

    Retry failed peer recovery due to transient errors (#55353)
    Currently a failed peer recovery action will fail an recovery. This
    includes when the recovery fails due to potentially short lived
    transient issues such as rejected exceptions or circuit breaking
    errors.
    
    This commit adds the concept of a retryable action. A retryable action
    will be retryed in face of certain errors. The action will be retried
    after an exponentially increasing backoff period. After defined time,
    the action will timeout.
    
    This commit only implements retries for responses that indicate the
    target node has NOT executed the action.

--- server/src/test/java/org/elasticsearch/indices/recovery/PeerRecoverySourceServiceTests.java
+++ server/src/test/java/org/elasticsearch/indices/recovery/PeerRecoverySourceServiceTests.java
@@ -21,6 +21,7 @@ package org.elasticsearch.indices.recovery;
 
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.BigArrays;
 import org.elasticsearch.index.seqno.SequenceNumbers;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.index.shard.IndexShardTestCase;
@@ -39,7 +40,8 @@ public class PeerRecoverySourceServiceTests extends IndexShardTestCase {
         IndexShard primary = newStartedShard(true);
         PeerRecoverySourceService peerRecoverySourceService = new PeerRecoverySourceService(
             mock(TransportService.class), mock(IndicesService.class),
-            new RecoverySettings(Settings.EMPTY, new ClusterSettings(Settings.EMPTY, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS)));
+            new RecoverySettings(Settings.EMPTY, new ClusterSettings(Settings.EMPTY, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS)),
+            BigArrays.NON_RECYCLING_INSTANCE);
         StartRecoveryRequest startRecoveryRequest = new StartRecoveryRequest(primary.shardId(), randomAlphaOfLength(10),
             getFakeDiscoNode("source"), getFakeDiscoNode("target"), Store.MetadataSnapshot.EMPTY, randomBoolean(), randomLong(),
             SequenceNumbers.UNASSIGNED_SEQ_NO);

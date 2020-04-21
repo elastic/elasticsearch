923343862ec075a0eacc535a0aa82bf2008405e1
Yannick Welsch <yannick@welsch.lu>
2020-04-21 13:13:09 +0200

    Use streaming reads for GCS (#55506)
    To read from GCS repositories we're currently using Google SDK's official BlobReadChannel,
    which issues a new request every 2MB (default chunk size for BlobReadChannel) using range
    requests, and fully downloads the chunk before exposing it to the returned InputStream. This
    means that the SDK issues an awfully high number of requests to download large blobs.
    Increasing the chunk size is not an option, as that will mean that an awfully high amount of
    heap memory will be consumed by the download process.
    
    The Google SDK does not provide the right abstractions for a streaming download. This PR
    uses the lower-level primitives of the SDK to implement a streaming download, similar to what
    S3's SDK does.
    
    Also closes #55505

--- plugins/repository-gcs/src/test/java/org/elasticsearch/repositories/gcs/GoogleCloudStorageBlobContainerRetriesTests.java
+++ plugins/repository-gcs/src/test/java/org/elasticsearch/repositories/gcs/GoogleCloudStorageBlobContainerRetriesTests.java
@@ -93,13 +93,6 @@ public class GoogleCloudStorageBlobContainerRetriesTests extends AbstractBlobCon
         return "http://" + InetAddresses.toUriString(address.getAddress()) + ":" + address.getPort();
     }
 
-    // Google's SDK ignores Content-Length header when no bytes are sent, see SizeValidatingInputStream
-    // TODO: fix this in the SDK
-    @Override
-    protected int minIncompleteContentToSend() {
-        return 1;
-    }
-
     @Override
     protected String downloadStorageEndpoint(String blob) {
         return "/download/storage/v1/b/bucket/o/" + blob;
@@ -147,7 +140,7 @@ public class GoogleCloudStorageBlobContainerRetriesTests extends AbstractBlobCon
                     .setRpcTimeoutMultiplier(options.getRetrySettings().getRpcTimeoutMultiplier())
                     .setMaxRpcTimeout(Duration.ofSeconds(1));
                 if (maxRetries != null) {
-                    retrySettingsBuilder.setMaxAttempts(maxRetries);
+                    retrySettingsBuilder.setMaxAttempts(maxRetries + 1);
                 }
                 return options.toBuilder()
                     .setHost(options.getHost())
@@ -185,10 +178,9 @@ public class GoogleCloudStorageBlobContainerRetriesTests extends AbstractBlobCon
         httpServer.createContext("/download/storage/v1/b/bucket/o/large_blob_retries", exchange -> {
             Streams.readFully(exchange.getRequestBody());
             exchange.getResponseHeaders().add("Content-Type", "application/octet-stream");
-            final String[] range = exchange.getRequestHeaders().get("Range").get(0).substring("bytes=".length()).split("-");
-            final int offset = Integer.parseInt(range[0]);
-            final int end = Integer.parseInt(range[1]);
-            final byte[] chunk = Arrays.copyOfRange(bytes, offset, Math.min(end + 1, bytes.length));
+            final Tuple<Long, Long> range = getRange(exchange);
+            final int offset = Math.toIntExact(range.v1());
+            final byte[] chunk = Arrays.copyOfRange(bytes, offset, Math.toIntExact(Math.min(range.v2() + 1, bytes.length)));
             exchange.sendResponseHeaders(RestStatus.OK.getStatus(), chunk.length);
             if (randomBoolean() && countDown.decrementAndGet() >= 0) {
                 exchange.getResponseBody().write(chunk, 0, chunk.length - 1);
@@ -393,11 +385,5 @@ public class GoogleCloudStorageBlobContainerRetriesTests extends AbstractBlobCon
                 exchange.close();
             }
         };
-    }
-
-    @Override
-    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/55505")
-    public void testReadRangeBlobWithRetries() throws Exception {
-
     }
 }

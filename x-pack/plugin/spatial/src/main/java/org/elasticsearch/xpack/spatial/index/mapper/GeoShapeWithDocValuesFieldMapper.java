4431ed72da0d3b23647987cbc865576dbfa5f332
Tal Levy <JubBoy333@gmail.com>
2020-04-22 08:11:56 -0700

    Add geo_bounds aggregation support for geo_shape (#55328)
    This commit adds a new GeoShapeBoundsAggregator to the spatial plugin and registers it with the GeoShapeValuesSourceType. This enables geo_bounds aggregations on geo_shape fields

--- x-pack/plugin/spatial/src/main/java/org/elasticsearch/xpack/spatial/index/mapper/GeoShapeWithDocValuesFieldMapper.java
+++ x-pack/plugin/spatial/src/main/java/org/elasticsearch/xpack/spatial/index/mapper/GeoShapeWithDocValuesFieldMapper.java
@@ -35,6 +35,7 @@ import org.elasticsearch.index.mapper.ParseContext;
 import org.elasticsearch.index.mapper.TypeParsers;
 import org.elasticsearch.index.query.QueryShardContext;
 import org.elasticsearch.index.query.VectorGeoShapeQueryProcessor;
+import org.elasticsearch.search.aggregations.support.ValuesSourceType;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -168,6 +169,11 @@ public class GeoShapeWithDocValuesFieldMapper extends GeoShapeFieldMapper {
             } else {
                 return new TermQuery(new Term(FieldNamesFieldMapper.NAME, name()));
             }
+        }
+
+        @Override
+        public ValuesSourceType getValuesSourceType() {
+            return GeoShapeValuesSourceType.INSTANCE;
         }
 
         @Override

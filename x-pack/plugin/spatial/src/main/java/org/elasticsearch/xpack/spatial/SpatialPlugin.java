0844455505819f23be77291f0e579f13644f7444
Tal Levy <JubBoy333@gmail.com>
2020-04-22 08:12:54 -0700

    Add geo_shape mapper supporting doc-values in Spatial Plugin (#55037) (#55500)
    After #53562, the `geo_shape` field mapper is registered within
    a module. This opens the door for introducing a new `geo_shape`
    field mapper into the Spatial Plugin that has doc-values support.
    
    This is very much an extension of server's GeoShapeFieldMapper,
    but with the addition of the doc values implementation.

--- x-pack/plugin/spatial/src/main/java/org/elasticsearch/xpack/spatial/SpatialPlugin.java
+++ x-pack/plugin/spatial/src/main/java/org/elasticsearch/xpack/spatial/SpatialPlugin.java
@@ -6,14 +6,14 @@
 package org.elasticsearch.xpack.spatial;
 
 import org.elasticsearch.common.inject.Module;
-import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.geo.GeoPlugin;
 import org.elasticsearch.index.mapper.Mapper;
 import org.elasticsearch.ingest.Processor;
 import org.elasticsearch.plugins.IngestPlugin;
 import org.elasticsearch.plugins.MapperPlugin;
-import org.elasticsearch.plugins.Plugin;
-import org.elasticsearch.xpack.core.XPackPlugin;
 import org.elasticsearch.plugins.SearchPlugin;
+import org.elasticsearch.xpack.core.XPackPlugin;
+import org.elasticsearch.xpack.spatial.index.mapper.GeoShapeWithDocValuesFieldMapper;
 import org.elasticsearch.xpack.spatial.index.mapper.PointFieldMapper;
 import org.elasticsearch.xpack.spatial.index.mapper.ShapeFieldMapper;
 import org.elasticsearch.xpack.spatial.index.query.ShapeQueryBuilder;
@@ -21,16 +21,13 @@ import org.elasticsearch.xpack.spatial.ingest.CircleProcessor;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static java.util.Collections.singletonList;
 
-public class SpatialPlugin extends Plugin implements MapperPlugin, SearchPlugin, IngestPlugin {
-
-    public SpatialPlugin(Settings settings) {
-    }
+public class SpatialPlugin extends GeoPlugin implements MapperPlugin, SearchPlugin, IngestPlugin {
 
     public Collection<Module> createGuiceModules() {
         return Collections.singletonList(b -> {
@@ -40,9 +37,10 @@ public class SpatialPlugin extends Plugin implements MapperPlugin, SearchPlugin,
 
     @Override
     public Map<String, Mapper.TypeParser> getMappers() {
-        Map<String, Mapper.TypeParser> mappers = new LinkedHashMap<>();
+        Map<String, Mapper.TypeParser> mappers = new HashMap<>(super.getMappers());
         mappers.put(ShapeFieldMapper.CONTENT_TYPE, new ShapeFieldMapper.TypeParser());
         mappers.put(PointFieldMapper.CONTENT_TYPE, new PointFieldMapper.TypeParser());
+        mappers.put(GeoShapeWithDocValuesFieldMapper.CONTENT_TYPE, new GeoShapeWithDocValuesFieldMapper.TypeParser());
         return Collections.unmodifiableMap(mappers);
     }
 

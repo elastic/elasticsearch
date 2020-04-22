b92f9b9e2628100475139d5a35162341873594e9
Tal Levy <jubboy333@gmail.com>
2020-04-22 16:48:20 -0700

    add docs

--- x-pack/plugin/spatial/src/test/java/org/elasticsearch/xpack/spatial/LocalStateSpatialPlugin.java
+++ x-pack/plugin/spatial/src/test/java/org/elasticsearch/xpack/spatial/LocalStateSpatialPlugin.java
@@ -14,6 +14,12 @@ import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.VersionUtils;
 import org.elasticsearch.xpack.spatial.SpatialPlugin;
 
+/**
+ * This class overrides the {@link SpatialPlugin} in order
+ * to provide the integration test clusters a hook into a real
+ * {@link XPackLicenseState}. In the cases that this is used, the
+ * actual license's operation mode is not important
+ */
 public class LocalStateSpatialPlugin extends SpatialPlugin {
     protected XPackLicenseState getLicenseState() {
         TestUtils.UpdatableLicenseState licenseState = new TestUtils.UpdatableLicenseState();

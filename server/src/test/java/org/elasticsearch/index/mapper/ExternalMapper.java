b0e8a8a4d123e72048685885390491e306cf97fc
Nick Knize <nknize@gmail.com>
2020-04-24 14:05:16 -0500

    [Backport] Refactor Spatial Field Mappers (#55696)
    This commit refactors all spatial Field Mappers to a common
    AbstractGeometryFieldMapper that implements shared parameter functionality
    (e.g., ignore_malformed, ignore_z_value) and provides a common framework for
    overriding type parsing, and building in xpack. Common shape functionality is
    implemented in a new AbstractShapeGeometryFieldMapper that is reused and
    overridden in GeoShapeFieldMapper, GeoShapeFieldMapperWithDocValues,
    LegacyGeoShapeFieldMapper, and ShapeFieldMapper. This abstraction provides a
    reusable foundation for adding new xpack features; such as coordinate reference
    system support.

--- server/src/test/java/org/elasticsearch/index/mapper/ExternalMapper.java
+++ server/src/test/java/org/elasticsearch/index/mapper/ExternalMapper.java
@@ -88,7 +88,7 @@ public class ExternalMapper extends FieldMapper {
             BinaryFieldMapper binMapper = binBuilder.build(context);
             BooleanFieldMapper boolMapper = boolBuilder.build(context);
             GeoPointFieldMapper pointMapper = latLonPointBuilder.build(context);
-            AbstractGeometryFieldMapper shapeMapper = (context.indexCreatedVersion().before(Version.V_6_6_0))
+            AbstractShapeGeometryFieldMapper shapeMapper = (context.indexCreatedVersion().before(Version.V_6_6_0))
                 ? legacyShapeBuilder.build(context)
                 : shapeBuilder.build(context);
             FieldMapper stringMapper = (FieldMapper)stringBuilder.build(context);
@@ -154,13 +154,13 @@ public class ExternalMapper extends FieldMapper {
     private BinaryFieldMapper binMapper;
     private BooleanFieldMapper boolMapper;
     private GeoPointFieldMapper pointMapper;
-    private AbstractGeometryFieldMapper shapeMapper;
+    private AbstractShapeGeometryFieldMapper shapeMapper;
     private FieldMapper stringMapper;
 
     public ExternalMapper(String simpleName, MappedFieldType fieldType,
                           String generatedValue, String mapperName,
                           BinaryFieldMapper binMapper, BooleanFieldMapper boolMapper, GeoPointFieldMapper pointMapper,
-                          AbstractGeometryFieldMapper shapeMapper, FieldMapper stringMapper, Settings indexSettings,
+                          AbstractShapeGeometryFieldMapper shapeMapper, FieldMapper stringMapper, Settings indexSettings,
                           MultiFields multiFields, CopyTo copyTo) {
         super(simpleName, fieldType, new ExternalFieldType(), indexSettings, multiFields, copyTo);
         this.generatedValue = generatedValue;
@@ -218,7 +218,8 @@ public class ExternalMapper extends FieldMapper {
         BinaryFieldMapper binMapperUpdate = (BinaryFieldMapper) binMapper.updateFieldType(fullNameToFieldType);
         BooleanFieldMapper boolMapperUpdate = (BooleanFieldMapper) boolMapper.updateFieldType(fullNameToFieldType);
         GeoPointFieldMapper pointMapperUpdate = (GeoPointFieldMapper) pointMapper.updateFieldType(fullNameToFieldType);
-        AbstractGeometryFieldMapper shapeMapperUpdate = (AbstractGeometryFieldMapper) shapeMapper.updateFieldType(fullNameToFieldType);
+        AbstractShapeGeometryFieldMapper shapeMapperUpdate =
+            (AbstractShapeGeometryFieldMapper) shapeMapper.updateFieldType(fullNameToFieldType);
         TextFieldMapper stringMapperUpdate = (TextFieldMapper) stringMapper.updateFieldType(fullNameToFieldType);
         if (update == this
                 && multiFieldsUpdate == multiFields

297b2906323dc89b41d7e01561cdaa8a6e8b31ae
Xi Chen <zacharymorn@gmail.com>
2020-04-24 19:47:50 -0700

    Add voting config exclusion add and clear API spec and integration test cases

--- client/rest-high-level/src/test/java/org/elasticsearch/client/RestHighLevelClientTests.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/RestHighLevelClientTests.java
@@ -869,6 +869,8 @@ public class RestHighLevelClientTests extends ESTestCase {
             "cluster.reroute",
             "cluster.state",
             "cluster.stats",
+            "cluster.post_voting_config_exclusions",
+            "cluster.delete_voting_config_exclusions",
             "indices.shard_stores",
             "indices.upgrade",
             "indices.recovery",

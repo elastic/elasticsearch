4a63a16a5ee3c17670219c087d157d77761d2cf5
Yang Wang <yang.wang@elastic.co>
2020-04-28 01:56:50 +1000

    Minor

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authz/store/NativeRolesStoreIntegTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authz/store/NativeRolesStoreIntegTests.java
@@ -21,7 +21,6 @@ import org.junit.Before;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
 
 import static java.util.Collections.emptyMap;
 

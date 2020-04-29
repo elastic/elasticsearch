35a029e05a0b237f0de461efc33cb94992739614
Jack Conradson <osjdconrad@gmail.com>
2020-04-29 13:57:49 -0700

    response to pr comments

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/node/EDot.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/node/EDot.java
@@ -173,8 +173,7 @@ public class EDot extends AExpression {
 
                         Output indexOutput;
 
-                        if ((input.read || input.write)
-                                && (input.read == false || getter != null) && (input.write == false || setter != null)) {
+                        if ((input.read == false || getter != null) && (input.write == false || setter != null)) {
                             Input indexInput = new Input();
                             indexInput.expected = setter != null ? setter.typeParameters.get(0) : getter.typeParameters.get(0);
                             indexOutput = index.analyze(classNode, scriptRoot, scope, indexInput);
@@ -217,8 +216,7 @@ public class EDot extends AExpression {
 
                         Output indexOutput;
 
-                        if ((input.read || input.write)
-                                && (input.read == false || getter != null) && (input.write == false || setter != null)) {
+                        if ((input.read == false || getter != null) && (input.write == false || setter != null)) {
                             Input indexInput = new Input();
                             indexInput.expected = int.class;
                             indexOutput = index.analyze(classNode, scriptRoot, scope, indexInput);

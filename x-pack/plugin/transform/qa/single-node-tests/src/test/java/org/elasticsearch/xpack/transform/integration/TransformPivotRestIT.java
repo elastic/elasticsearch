4123f297ec3117543fbff985e08b0a0964e27636
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2020-05-15 07:20:14 -0400

    fixing for java 8

--- x-pack/plugin/transform/qa/single-node-tests/src/test/java/org/elasticsearch/xpack/transform/integration/TransformPivotRestIT.java
+++ x-pack/plugin/transform/qa/single-node-tests/src/test/java/org/elasticsearch/xpack/transform/integration/TransformPivotRestIT.java
@@ -580,22 +580,22 @@ public class TransformPivotRestIT extends TransformRestTestCase {
             searchResult
         )).get(0);
         assertThat(commonUsers, is(not(nullValue())));
-        assertThat(commonUsers, equalTo(new HashMap<>(){{
+        assertThat(commonUsers, equalTo(new HashMap<String, Object>(){{
             put("user_10",
                 Collections.singletonMap(
                     "common_businesses",
-                    new HashMap<>(){{
+                    new HashMap<String, Object>(){{
                         put("business_12", 6);
                         put("business_9", 4);
             }}));
             put("user_0", Collections.singletonMap(
                 "common_businesses",
-                new HashMap<>(){{
+                new HashMap<String, Object>(){{
                     put("business_0", 35);
             }}));
         }}));
         assertThat(rareUsers, is(not(nullValue())));
-        assertThat(rareUsers, equalTo(new HashMap<>(){{
+        assertThat(rareUsers, equalTo(new HashMap<String, Object>(){{
             put("user_5", 1);
             put("user_12", 1);
         }}));

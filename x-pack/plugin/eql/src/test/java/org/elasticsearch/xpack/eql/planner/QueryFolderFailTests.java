4df6805fc87b94a4f5c61c53f9241a35d2d08246
Bogdan Pintea <bogdan.pintea@elastic.co>
2020-05-26 10:15:58 +0200

    Change error message for comp against fields (#57126)
    Change the error message wording for comparisons against fields in
    filtering (s/variables/fields).
    
    (cherry picked from commit d9a1cb50940d0a98fd75b9c0123ca6e1d862f65d)

--- x-pack/plugin/eql/src/test/java/org/elasticsearch/xpack/eql/planner/QueryFolderFailTests.java
+++ x-pack/plugin/eql/src/test/java/org/elasticsearch/xpack/eql/planner/QueryFolderFailTests.java
@@ -13,13 +13,13 @@ public class QueryFolderFailTests extends AbstractQueryFolderTestCase {
         QlIllegalArgumentException e = expectThrows(QlIllegalArgumentException.class,
                 () -> plan("process where (serial_event_id<9 and serial_event_id >= 7) or (opcode == pid)"));
         String msg = e.getMessage();
-        assertEquals("Line 1:74: Comparisons against variables are not (currently) supported; offender [pid] in [==]", msg);
+        assertEquals("Line 1:74: Comparisons against fields are not (currently) supported; offender [pid] in [==]", msg);
     }
 
     public void testPropertyEquationInClauseFilterUnsupported() {
         QlIllegalArgumentException e = expectThrows(QlIllegalArgumentException.class,
                 () -> plan("process where opcode in (1,3) and process_name in (parent_process_name, \"SYSTEM\")"));
         String msg = e.getMessage();
-        assertEquals("Line 1:52: Comparisons against variables are not (currently) supported; offender [parent_process_name] in [==]", msg);
+        assertEquals("Line 1:52: Comparisons against fields are not (currently) supported; offender [parent_process_name] in [==]", msg);
     }
 }

37cc341e7da0292bdfb2f42992b7d59f6f32f51e
jimczi <jim.ferenczi@elastic.co>
2020-05-28 13:54:14 +0200

    Add index filtering in field capabilities API
    This change allows to use an `index_filter` in the
    field capabilities API. Indices are filtered from
    the response if the provided query rewrites to `match_none`
    on every shard:
    
    ````
    GET metrics-*
    {
      "index_filter": {
        "bool": {
          "must": [
            "range": {
              "@timestamp": {
                "gt": "2019"
              }
            }
          }
      }
    }
    ````
    
    The filtering is done on a best-effort basis, it uses the can match phase
    to rewrite queries to `match_none` instead of fully executing the request.
    The first shard that can match the filter is used to create the field
    capabilities response for the entire index.
    
    Closes #56195

--- server/src/main/java/org/elasticsearch/action/fieldcaps/FieldCapabilitiesRequest.java
+++ server/src/main/java/org/elasticsearch/action/fieldcaps/FieldCapabilitiesRequest.java
@@ -30,6 +30,9 @@ import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.xcontent.ObjectParser;
+import org.elasticsearch.common.xcontent.ToXContentObject;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -39,15 +42,18 @@ import java.util.Set;
 
 import static org.elasticsearch.common.xcontent.ObjectParser.fromList;
 
-public final class FieldCapabilitiesRequest extends ActionRequest implements IndicesRequest.Replaceable {
+public final class FieldCapabilitiesRequest extends ActionRequest implements IndicesRequest.Replaceable, ToXContentObject {
     public static final ParseField FIELDS_FIELD = new ParseField("fields");
     public static final String NAME = "field_caps_request";
+
     private String[] indices = Strings.EMPTY_ARRAY;
     private IndicesOptions indicesOptions = IndicesOptions.strictExpandOpen();
     private String[] fields = Strings.EMPTY_ARRAY;
     private boolean includeUnmapped = false;
     // pkg private API mainly for cross cluster search to signal that we do multiple reductions ie. the results should not be merged
     private boolean mergeResults = true;
+    private QueryBuilder indexFilter;
+    private Long nowInMillis;
 
     private static final ObjectParser<FieldCapabilitiesRequest, Void> PARSER =
         new ObjectParser<>(NAME, FieldCapabilitiesRequest::new);
@@ -67,13 +73,16 @@ public final class FieldCapabilitiesRequest extends ActionRequest implements Ind
         } else {
             includeUnmapped = false;
         }
+        indexFilter = in.getVersion().onOrAfter(Version.V_8_0_0) ? in.readOptionalNamedWriteable(QueryBuilder.class) : null;
+        nowInMillis = in.getVersion().onOrAfter(Version.V_8_0_0) ? in.readOptionalLong() : null;
     }
 
-    public FieldCapabilitiesRequest() {}
+    public FieldCapabilitiesRequest() {
+    }
 
     /**
      * Returns <code>true</code> iff the results should be merged.
-     *
+     * <p>
      * Note that when using the high-level REST client, results are always merged (this flag is always considered 'true').
      */
     boolean isMergeResults() {
@@ -83,7 +92,7 @@ public final class FieldCapabilitiesRequest extends ActionRequest implements Ind
     /**
      * If set to <code>true</code> the response will contain only a merged view of the per index field capabilities.
      * Otherwise only unmerged per index field capabilities are returned.
-     *
+     * <p>
      * Note that when using the high-level REST client, results are always merged (this flag is always considered 'true').
      */
     void setMergeResults(boolean mergeResults) {
@@ -100,6 +109,20 @@ public final class FieldCapabilitiesRequest extends ActionRequest implements Ind
         if (out.getVersion().onOrAfter(Version.V_7_2_0)) {
             out.writeBoolean(includeUnmapped);
         }
+        if (out.getVersion().onOrAfter(Version.V_8_0_0)) {
+            out.writeOptionalWriteable(indexFilter);
+            out.writeOptionalLong(nowInMillis);
+        }
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.startObject();
+        if (indexFilter != null) {
+            builder.field("index_filter", indexFilter);
+        }
+        builder.endObject();
+        return builder;
     }
 
     /**
@@ -150,6 +173,26 @@ public final class FieldCapabilitiesRequest extends ActionRequest implements Ind
         return includeUnmapped;
     }
 
+    /**
+     * Allows to filter indices if the provided {@link QueryBuilder} rewrites to `match_none` on every shard.
+     */
+    public FieldCapabilitiesRequest indexFilter(QueryBuilder indexFilter) {
+        this.indexFilter = indexFilter;
+        return this;
+    }
+
+    public QueryBuilder indexFilter() {
+        return indexFilter;
+    }
+
+    Long nowInMillis() {
+        return nowInMillis;
+    }
+
+    void nowInMillis(long nowInMillis) {
+        this.nowInMillis = nowInMillis;
+    }
+
     @Override
     public ActionRequestValidationException validate() {
         ActionRequestValidationException validationException = null;
@@ -163,17 +206,21 @@ public final class FieldCapabilitiesRequest extends ActionRequest implements Ind
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         FieldCapabilitiesRequest that = (FieldCapabilitiesRequest) o;
-        return  Arrays.equals(indices, that.indices) &&
-            Objects.equals(indicesOptions, that.indicesOptions) &&
+        return includeUnmapped == that.includeUnmapped &&
+            mergeResults == that.mergeResults &&
+            Arrays.equals(indices, that.indices) &&
+            indicesOptions.equals(that.indicesOptions) &&
             Arrays.equals(fields, that.fields) &&
-            Objects.equals(mergeResults, that.mergeResults) &&
-            includeUnmapped == that.includeUnmapped;
+            Objects.equals(indexFilter, that.indexFilter) &&
+            Objects.equals(nowInMillis, that.nowInMillis);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(Arrays.hashCode(indices), indicesOptions, Arrays.hashCode(fields), mergeResults, includeUnmapped);
+        int result = Objects.hash(indicesOptions, includeUnmapped, mergeResults, indexFilter, nowInMillis);
+        result = 31 * result + Arrays.hashCode(indices);
+        result = 31 * result + Arrays.hashCode(fields);
+        return result;
     }
 }

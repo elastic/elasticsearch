ebbe657b0e466551943dc78af331bcb60a2f3e33
Mark Vieira <portugee@gmail.com>
2020-06-01 11:37:18 -0700

    Add modifications copyright notice on vendored code

--- server/src/main/java/org/elasticsearch/common/inject/package-info.java
+++ server/src/main/java/org/elasticsearch/common/inject/package-info.java
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Modifications copyright (C) 2020 Elasticsearch B.V.
  */
 
 /**

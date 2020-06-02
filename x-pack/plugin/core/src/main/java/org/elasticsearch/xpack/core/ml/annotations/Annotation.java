f8d8e0b7219f358ff24cc3ec8c2e022b087436ed
Przemysław Witek <przemyslaw.witek@elastic.co>
2020-06-02 09:57:23 +0200

    Make Annotation a result type (#56342)
    * Make Annotation a result type
    
    * Use bulk requests for indexing annotations
    
    * Fix AutodetectProcessManagerTests
    
    * Fix AutodetectResultProcessorTests
    
    * Align comments
    
    * Execute annotation persister bulk request on flush

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/annotations/Annotation.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/annotations/Annotation.java
@@ -54,6 +54,11 @@ public class Annotation implements ToXContentObject, Writeable {
         }
     }
 
+    /**
+     * Result type is needed due to the fact that {@link Annotation} can be returned from C++ as an ML result.
+     */
+    public static final ParseField RESULTS_FIELD = new ParseField("annotation");
+
     public static final ParseField ANNOTATION = new ParseField("annotation");
     public static final ParseField CREATE_TIME = new ParseField("create_time");
     public static final ParseField CREATE_USERNAME = new ParseField("create_username");
@@ -81,7 +86,8 @@ public class Annotation implements ToXContentObject, Writeable {
     /**
      * Strict parser for cases when {@link Annotation} is returned from C++ as an ML result.
      */
-    private static final ObjectParser<Builder, Void> STRICT_PARSER = new ObjectParser<>(ANNOTATION.getPreferredName(), false, Builder::new);
+    private static final ObjectParser<Builder, Void> STRICT_PARSER =
+        new ObjectParser<>(RESULTS_FIELD.getPreferredName(), false, Builder::new);
 
     static {
         STRICT_PARSER.declareString(Builder::setAnnotation, ANNOTATION);

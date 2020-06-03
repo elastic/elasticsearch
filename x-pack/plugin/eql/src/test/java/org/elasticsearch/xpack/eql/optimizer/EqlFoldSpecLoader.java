5a1cdf633efffd5a67d6e130df970941ecfbd67e
Ross Wolf <31489089+rw-access@users.noreply.github.com>
2020-06-03 11:51:30 -0600

    EQL: PR feedback and remove duplicate licenses

--- x-pack/plugin/eql/src/test/java/org/elasticsearch/xpack/eql/optimizer/EqlFoldSpecLoader.java
+++ x-pack/plugin/eql/src/test/java/org/elasticsearch/xpack/eql/optimizer/EqlFoldSpecLoader.java
@@ -4,12 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
-
 package org.elasticsearch.xpack.eql.optimizer;
 
 import io.ous.jtoml.JToml;

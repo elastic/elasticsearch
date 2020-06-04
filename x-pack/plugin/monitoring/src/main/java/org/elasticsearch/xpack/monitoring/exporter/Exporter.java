b2bbd8e1fc33a5509c700f71c09120d6c80863b7
Jake Landis <jake.landis@elastic.co>
2020-06-04 16:47:09 -0500

    Ensure type exists for all monitoring configuration (#57399)
    #47711 and #47246 helped to validate that monitoring settings are
    rejected at time of setting the monitoring settings. Else an invalid
    monitoring setting can find it's way into the cluster state and result
    in an exception thrown [1] on the cluster state application (there by
    causing significant issues). Some additional monitoring settings have
    been identified that can result in invalid cluster state that also
    result in exceptions thrown on cluster state application.
    
    All settings require a type of either http or local to be
    applicable. When a setting is changed, the exporters are automatically
    updated with the new settings. However, if the old or new settings lack
    of a type setting an exception will be thrown (since exporters are
    always of type 'http' or 'local'). Arguably we shouldn't blindly create
    and destroy new exporters on each monitoring setting update, but the
    lifecycle of the exporters is abit out the scope this PR is trying to
    address.
    
    This commit introduces a similar methodology to check for validity as
    #47711 and #47246 but this time for ALL (including non-http) settings.
    Monitoring settings are not useful unless there an exporter with a type
    defined. The type is used as dependent setting, such that it must
    exist to set the value. This ensures that when any monitoring settings
    changes that they can only get added to cluster state if the type
    exists. If the type exists (and the other validations pass) then the
    exporters will get re-built and the cluster state remains valid.
    
    Tests have been included to ensure that all dynamic monitoring settings
    have the type as dependent settings.
    
    [1]
    org.elasticsearch.common.settings.SettingsException: missing exporter type for [found-user-defined] exporter
    at org.elasticsearch.xpack.monitoring.exporter.Exporters.initExporters(Exporters.java:126) ~[?:?]

--- x-pack/plugin/monitoring/src/main/java/org/elasticsearch/xpack/monitoring/exporter/Exporter.java
+++ x-pack/plugin/monitoring/src/main/java/org/elasticsearch/xpack/monitoring/exporter/Exporter.java
@@ -25,9 +25,11 @@ import java.util.function.Function;
 
 public abstract class Exporter implements AutoCloseable {
 
+    public static Setting.AffixSettingDependency TYPE_DEPENDENCY = () -> Exporter.TYPE_SETTING;
+
     private static final Setting.AffixSetting<Boolean> ENABLED_SETTING =
             Setting.affixKeySetting("xpack.monitoring.exporters.","enabled",
-                    key -> Setting.boolSetting(key, true, Property.Dynamic, Property.NodeScope));
+                    key -> Setting.boolSetting(key, true, Property.Dynamic, Property.NodeScope), TYPE_DEPENDENCY);
 
     public static final Setting.AffixSetting<String> TYPE_SETTING = Setting.affixKeySetting(
         "xpack.monitoring.exporters.",
@@ -83,13 +85,13 @@ public abstract class Exporter implements AutoCloseable {
      */
     public static final Setting.AffixSetting<Boolean> USE_INGEST_PIPELINE_SETTING =
             Setting.affixKeySetting("xpack.monitoring.exporters.","use_ingest",
-                    key -> Setting.boolSetting(key, true, Property.Dynamic, Property.NodeScope));
+                    key -> Setting.boolSetting(key, true, Property.Dynamic, Property.NodeScope), TYPE_DEPENDENCY);
     /**
      * Every {@code Exporter} allows users to explicitly disable cluster alerts.
      */
     public static final Setting.AffixSetting<Boolean> CLUSTER_ALERTS_MANAGEMENT_SETTING =
             Setting.affixKeySetting("xpack.monitoring.exporters.", "cluster_alerts.management.enabled",
-                    key -> Setting.boolSetting(key, true, Property.Dynamic, Property.NodeScope));
+                    key -> Setting.boolSetting(key, true, Property.Dynamic, Property.NodeScope), TYPE_DEPENDENCY);
     /**
      * Every {@code Exporter} allows users to explicitly disable specific cluster alerts.
      * <p>
@@ -97,7 +99,8 @@ public abstract class Exporter implements AutoCloseable {
      */
     public static final Setting.AffixSetting<List<String>> CLUSTER_ALERTS_BLACKLIST_SETTING = Setting
                 .affixKeySetting("xpack.monitoring.exporters.", "cluster_alerts.management.blacklist",
-                    key -> Setting.listSetting(key, Collections.emptyList(), Function.identity(), Property.Dynamic, Property.NodeScope));
+                    key -> Setting.listSetting(key, Collections.emptyList(), Function.identity(), Property.Dynamic, Property.NodeScope),
+                    TYPE_DEPENDENCY);
 
     /**
      * Every {@code Exporter} allows users to use a different index time format.
@@ -109,7 +112,7 @@ public abstract class Exporter implements AutoCloseable {
                                 Exporter.INDEX_FORMAT,
                                 DateFormatter::forPattern,
                                 Property.Dynamic,
-                                Property.NodeScope));
+                                Property.NodeScope), TYPE_DEPENDENCY);
 
     private static final String INDEX_FORMAT = "yyyy.MM.dd";
 

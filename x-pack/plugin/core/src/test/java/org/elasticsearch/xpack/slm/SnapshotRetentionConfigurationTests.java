e97739c5223aed3cc7a07ce27940f54f694fe84b
Przemko Robakowski <przemko.robakowski@elastic.co>
2020-06-04 16:34:38 +0200

    fix SLM

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/slm/SnapshotRetentionConfigurationTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/slm/SnapshotRetentionConfigurationTests.java
@@ -249,6 +249,7 @@ public class SnapshotRetentionConfigurationTests extends ESTestCase {
         final int totalShards = between(1,20);
         SnapshotInfo snapInfo = new SnapshotInfo(new SnapshotId("snap-" + randomAlphaOfLength(3), "uuid"),
             Collections.singletonList("foo"),
+            Collections.emptyList(),
             startTime,
             null,
             startTime + between(1, 10000),
@@ -280,6 +281,7 @@ public class SnapshotRetentionConfigurationTests extends ESTestCase {
         assert failureCount == failures.size();
         SnapshotInfo snapInfo = new SnapshotInfo(new SnapshotId("snap-fail-" + randomAlphaOfLength(3), "uuid-fail"),
             Collections.singletonList("foo-fail"),
+            Collections.emptyList(),
             startTime,
             "forced-failure",
             startTime + between(1, 10000),
@@ -303,6 +305,7 @@ public class SnapshotRetentionConfigurationTests extends ESTestCase {
         assert failureCount == failures.size();
         SnapshotInfo snapInfo = new SnapshotInfo(new SnapshotId("snap-fail-" + randomAlphaOfLength(3), "uuid-fail"),
             Collections.singletonList("foo-fail"),
+            Collections.emptyList(),
             startTime,
             null,
             startTime + between(1, 10000),

90fc523170d0601e04a5e5f697492c0ea5ff5495
Priya Raut <rautpriya215@gmail.com>
2020-06-06 19:03:13 +0530

    Add a parser for parsing GetGrokPatternsResponse.

--- client/rest-high-level/src/main/java/org/elasticsearch/client/core/GetGrokPatternsResponse.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/core/GetGrokPatternsResponse.java
@@ -19,12 +19,56 @@
 
 package org.elasticsearch.client.core;
 
+import org.elasticsearch.common.ParseField;
+import org.elasticsearch.common.xcontent.ConstructingObjectParser;
 import org.elasticsearch.common.xcontent.XContentParser;
 
 import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class GetGrokPatternsResponse {
+    private static final String GROK_PATTERN_KEY = "[A-Z_0-9]+";
+    private static final String GROK_PATTERN_VALUE =
+        "%\\{" +
+            "(?<name>" +
+            "(?<pattern>[A-z0-9]+)" +
+            "(?::(?<subname>[[:alnum:]@\\[\\]_:.-]+))?" +
+            ")" +
+            "(?:=(?<definition>" +
+            "(?:[^{}]+|\\.+)+" +
+            ")" +
+            ")?" + "\\}";
+    static final ParseField PATTERNS = new ParseField("patterns");
+    static final ParseField PATTERN_KEY = new ParseField(GROK_PATTERN_KEY);
+    static final ParseField PATTERN_VALUE = new ParseField(GROK_PATTERN_VALUE);
+
+    @SuppressWarnings("unchecked")
+    private static final ConstructingObjectParser<GetGrokPatternsResponse, Void> PARSER =
+        new ConstructingObjectParser<>(
+            "patterns",
+            true,
+            args -> new GetGrokPatternsResponse(new TreeMap<>(
+                ((List<Map.Entry<String, String>>) args[0])
+                    .stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)))));
+
+    private static final ConstructingObjectParser<Map.Entry<String, String>, Void> PATTERNS_PARSER =
+        new ConstructingObjectParser<>(
+            "patterns",
+            true,
+            args -> new AbstractMap.SimpleEntry<>((String) args[0], (String) args[1]));
+
+    static{
+        PATTERNS_PARSER.declareString(ConstructingObjectParser.constructorArg(), PATTERN_KEY);
+        PATTERNS_PARSER.declareLong(ConstructingObjectParser.constructorArg(), PATTERN_VALUE);
+
+        PARSER.declareObjectArray(ConstructingObjectParser.constructorArg(), PATTERNS_PARSER,
+            PATTERNS);
+    }
     private final Map<String, String> grokPatterns;
 
     GetGrokPatternsResponse(Map<String, String> grokPatterns) {
@@ -36,7 +80,7 @@ public class GetGrokPatternsResponse {
     }
 
     public static GetGrokPatternsResponse fromXContent(XContentParser parser) throws IOException {
-        return new GetGrokPatternsResponse(parser.mapStrings());
+        return PARSER.apply(parser, null);
     }
 
     @Override

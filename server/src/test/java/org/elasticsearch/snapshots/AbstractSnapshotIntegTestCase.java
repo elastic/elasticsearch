ac96e880bb86492ce929143dea30602253feb50c
Armin Braun <me@obrown.io>
2020-06-08 12:02:09 +0200

    Fix Bug With RepositoryData Caching (#57785)
    * Fix Bug With RepositoryData Caching
    
    This fixes a really subtle bug with caching `RepositoryData`
    that can corrupt a repository.
    We were caching `RepositoryData` serialized in the newest
    metadata format. This lead to a confusing situation where
    numeric shard generations would be cached in `ShardGenerations`
    that were not written to the repository because the repository
    or cluster did not yet support `ShardGenerations`.
    In the case where shard generations are not actually supported yet,
    these cached numeric generations are not safe and there's multiple
    scenarios where they would be incorrect, leading to the repository
    trying to read shard level metadata from index-N that don't exist.
    This commit makes it so that cached metadata is always in the same
    format as the metadata in the repository.
    
    Relates #57798

--- server/src/test/java/org/elasticsearch/snapshots/AbstractSnapshotIntegTestCase.java
+++ server/src/test/java/org/elasticsearch/snapshots/AbstractSnapshotIntegTestCase.java
@@ -47,6 +47,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.hamcrest.Matchers.equalTo;
 
 public abstract class AbstractSnapshotIntegTestCase extends ESIntegTestCase {
@@ -90,6 +91,10 @@ public abstract class AbstractSnapshotIntegTestCase extends ESIntegTestCase {
     protected void disableRepoConsistencyCheck(String reason) {
         assertNotNull(reason);
         skipRepoConsistencyCheckReason = reason;
+    }
+
+    protected RepositoryData getRepositoryData(String repository) {
+        return getRepositoryData(internalCluster().getMasterNodeInstance(RepositoriesService.class).repository(repository));
     }
 
     protected RepositoryData getRepositoryData(Repository repository) {
@@ -236,5 +241,12 @@ public abstract class AbstractSnapshotIntegTestCase extends ESIntegTestCase {
 
     public static void unblockNode(final String repository, final String node) {
         ((MockRepository)internalCluster().getInstance(RepositoriesService.class, node).repository(repository)).unblock();
+    }
+
+    protected void createRepository(String repoName, String type, Path location) {
+        logger.info("-->  creating repository");
+        assertAcked(client().admin().cluster().preparePutRepository(repoName)
+                .setType(type)
+                .setSettings(Settings.builder().put("location", location)));
     }
 }

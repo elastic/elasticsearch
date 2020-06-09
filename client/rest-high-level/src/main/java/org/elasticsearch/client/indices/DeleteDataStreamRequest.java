06e7b1fe5a905f272cb6523a80cd906782f16579
Przemko Robakowski <przemko.robakowski@elastic.co>
2020-06-09 12:01:26 +0200

    wip

---
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/indices/DeleteDataStreamRequest.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.client.indices;
+
+import org.elasticsearch.client.TimedRequest;
+
+/**
+ * A request to delete an data stream.
+ */
+public class DeleteDataStreamRequest extends TimedRequest {
+
+    private final String dataStream;
+
+    /**
+     * Constructs a new request to delete an data stream with the specified name.
+     */
+    public DeleteDataStreamRequest(String dataStream) {
+        if (dataStream == null) {
+            throw new IllegalArgumentException("The data stream name cannot be null.");
+        }
+        this.dataStream = dataStream;
+    }
+
+    /**
+     * The name of the data stream to delete.
+     */
+    public String dataStream() {
+        return dataStream;
+    }
+}

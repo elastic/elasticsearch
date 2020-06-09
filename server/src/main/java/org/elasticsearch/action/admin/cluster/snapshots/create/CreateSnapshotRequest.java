73015090c478419cc41aafa94e6f63fae7031e87
Gordon Brown <gordon.brown@elastic.co>
2020-06-09 16:03:41 -0600

    Include hidden indices in snapshots by default (#57325)
    Previously, hidden indices were not included in snapshots by default, unless
    specified using one of the usual methods for doing so: naming indices directly,
    using index patterns starting with a ., or specifying expand_wildcards to
    a value that includes hidden (e.g. all or hidden,open).
    
    This commit changes the default expand_wildcards value to include hidden
    indices.
    
    Also includes test changes from #57661, as they are necessary to prevent test
    failures with these changes, due to code sharing with the HLRC and the new 
    defaults using values that are only accepted by >=7.7.0

--- server/src/main/java/org/elasticsearch/action/admin/cluster/snapshots/create/CreateSnapshotRequest.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/snapshots/create/CreateSnapshotRequest.java
@@ -73,7 +73,7 @@ public class CreateSnapshotRequest extends MasterNodeRequest<CreateSnapshotReque
 
     private String[] indices = EMPTY_ARRAY;
 
-    private IndicesOptions indicesOptions = IndicesOptions.strictExpandOpen();
+    private IndicesOptions indicesOptions = IndicesOptions.strictExpandOpenHidden();
 
     private boolean partial = false;
 

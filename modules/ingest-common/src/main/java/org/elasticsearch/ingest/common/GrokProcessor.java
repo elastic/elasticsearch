a767730f5f7e59e67307dfa38a4155428b06c769
Jake Landis <jake.landis@elastic.co>
2020-06-09 17:07:23 -0500

    [7.7] Ensure Joni warning are logged at debug (#57302) (#57895)
    When Joni, the regex engine that powers grok emits a warning it
    does so by default to System.err. System.err logs are all bucketed
    together in the server log at WARN level. When Joni emits a warning,
    it can be extremely verbose, logging a message for each execution
    again that pattern. For ingest node that means for every document
    that is run that Grok filter. Fortunately, Joni provides a call
    back hook to push these warnings to a custom location.
    
    This commit implements Joni's callback hook to push the Joni warning
    to the Elasticsearch server logger (logger.org.elasticsearch.ingest.common.GrokProcessor)
    at debug level. A warning will be emitted on processor creation at WARN
    level. Generally these warning indicate a possible issue with
    the regular expression. Since Grok is an abstraction on top of the regex,
    these warning may not provide much value outside of a troubleshooting
    scenario.
    
    Additionally, the documentation is updated with instructions for how
    to set the logger to debug level.

--- modules/ingest-common/src/main/java/org/elasticsearch/ingest/common/GrokProcessor.java
+++ modules/ingest-common/src/main/java/org/elasticsearch/ingest/common/GrokProcessor.java
@@ -19,6 +19,8 @@
 
 package org.elasticsearch.ingest.common;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.elasticsearch.grok.Grok;
 import org.elasticsearch.grok.MatcherWatchdog;
 import org.elasticsearch.ingest.AbstractProcessor;
@@ -36,6 +38,7 @@ public final class GrokProcessor extends AbstractProcessor {
 
     public static final String TYPE = "grok";
     private static final String PATTERN_MATCH_KEY = "_ingest._grok_match_index";
+    private static final Logger logger = LogManager.getLogger(GrokProcessor.class);
 
     private final String matchField;
     private final List<String> matchPatterns;
@@ -48,9 +51,12 @@ public final class GrokProcessor extends AbstractProcessor {
         super(tag);
         this.matchField = matchField;
         this.matchPatterns = matchPatterns;
-        this.grok = new Grok(patternBank, combinePatterns(matchPatterns, traceMatch), matcherWatchdog);
+        this.grok = new Grok(patternBank, combinePatterns(matchPatterns, traceMatch), matcherWatchdog, logger::debug);
         this.traceMatch = traceMatch;
         this.ignoreMissing = ignoreMissing;
+        // Joni warnings are only emitted on an attempt to match, and the warning emitted for every call to match which is too verbose
+        // so here we emit a warning (if there is one) to the logfile at warn level on construction / processor creation.
+        new Grok(patternBank, combinePatterns(matchPatterns, traceMatch), matcherWatchdog, logger::warn).match("___nomatch___");
     }
 
     @Override

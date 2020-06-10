1fd430c78e070ad9d7cf9e6e08eeee4c3fa8fdfa
Martijn van Groningen <martijn.v.groningen@gmail.com>
2020-06-10 15:07:10 +0200

    adjusted comment

--- modules/reindex/src/main/java/org/elasticsearch/index/reindex/Reindexer.java
+++ modules/reindex/src/main/java/org/elasticsearch/index/reindex/Reindexer.java
@@ -266,8 +266,11 @@ public class Reindexer {
              */
             index.routing(mainRequest.getDestination().routing());
             index.setPipeline(mainRequest.getDestination().getPipeline());
-            // Only copy the op_type if it has been set to CREATE.
-            // (INDEX is the default and UPDATE and DELETE are invalid values)
+
+            // (For ensuring no document exists, the op_type create doesn't need to be copied, since Versions.MATCH_DELETED
+            //  will copied from the 'mainRequest.getDestination().version()' a few lines back)
+            // (In order to be able to index into a data stream, the op_type must be create. So in order to support that
+            //  the op_type must be copied)
             if (mainRequest.getDestination().opType() == DocWriteRequest.OpType.CREATE) {
                 index.opType(mainRequest.getDestination().opType());
             }

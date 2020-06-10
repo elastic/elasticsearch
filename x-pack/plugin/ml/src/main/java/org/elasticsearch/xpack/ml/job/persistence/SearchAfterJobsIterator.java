96a6de22d9003763202e56b7fe324e2aae70f215
David Kyle <david.kyle@elastic.co>
2020-06-10 09:53:22 +0100

    Use Search After job iterators (#57875)
    Search after is a better choice for the delete expired data iterators
    where processing takes a long time as unlike scroll a context does not
    have to be kept alive. Also changes the delete expired data endpoint to
    404 if the job is unknown

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/job/persistence/SearchAfterJobsIterator.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/job/persistence/SearchAfterJobsIterator.java
@@ -3,44 +3,64 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+
 package org.elasticsearch.xpack.ml.job.persistence;
 
 import org.elasticsearch.ElasticsearchParseException;
 import org.elasticsearch.client.OriginSettingClient;
-import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.xcontent.LoggingDeprecationHandler;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.TermQueryBuilder;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.xpack.core.ml.job.config.Job;
-import org.elasticsearch.xpack.ml.utils.persistence.BatchedDocumentsIterator;
+import org.elasticsearch.xpack.core.ml.job.persistence.AnomalyDetectorsIndex;
+import org.elasticsearch.xpack.ml.utils.persistence.SearchAfterDocumentsIterator;
 
 import java.io.IOException;
 import java.io.InputStream;
 
-public class BatchedJobsIterator extends BatchedDocumentsIterator<Job.Builder> {
+public class SearchAfterJobsIterator extends SearchAfterDocumentsIterator<Job.Builder> {
 
-    private final String jobIdExpression;
+    private String lastJobId;
 
-    public BatchedJobsIterator(OriginSettingClient client, String index, String jobIdExpression) {
-        super(client, index);
-        this.jobIdExpression = jobIdExpression;
+    public SearchAfterJobsIterator(OriginSettingClient client) {
+        super(client, AnomalyDetectorsIndex.configIndexName());
     }
 
     @Override
     protected QueryBuilder getQuery() {
-        String [] tokens = Strings.tokenizeToStringArray(jobIdExpression, ",");
-        return JobConfigProvider.buildJobWildcardQuery(tokens, true);
+        return new TermQueryBuilder(Job.JOB_TYPE.getPreferredName(), Job.ANOMALY_DETECTOR_JOB_TYPE);
+    }
+
+    @Override
+    protected FieldSortBuilder sortField() {
+        return new FieldSortBuilder(Job.ID.getPreferredName());
+    }
+
+    @Override
+    protected Object[] searchAfterFields() {
+        if (lastJobId == null) {
+            return null;
+        } else {
+            return new Object[] {lastJobId};
+        }
+    }
+
+    @Override
+    protected void extractSearchAfterFields(SearchHit lastSearchHit) {
+        lastJobId = Job.extractJobIdFromDocumentId(lastSearchHit.getId());
     }
 
     @Override
     protected Job.Builder map(SearchHit hit) {
         try (InputStream stream = hit.getSourceRef().streamInput();
              XContentParser parser = XContentFactory.xContent(XContentType.JSON)
-                     .createParser(NamedXContentRegistry.EMPTY, LoggingDeprecationHandler.INSTANCE, stream)) {
+                 .createParser(NamedXContentRegistry.EMPTY, LoggingDeprecationHandler.INSTANCE, stream)) {
             return Job.LENIENT_PARSER.apply(parser, null);
         } catch (IOException e) {
             throw new ElasticsearchParseException("failed to parse job document [" + hit.getId() + "]", e);

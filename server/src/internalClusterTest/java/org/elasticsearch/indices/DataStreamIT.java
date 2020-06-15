3955ee46c00db519ae3cefc039f0a609cc6daada
Przemko Robakowski <przemko.robakowski@elastic.co>
2020-06-15 09:03:45 +0200

    test fix

--- server/src/internalClusterTest/java/org/elasticsearch/indices/DataStreamIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/indices/DataStreamIT.java
@@ -96,11 +96,11 @@ public class DataStreamIT extends ESIntegTestCase {
     }
 
     public void testBasicScenario() throws Exception {
-        createIndexTemplate("id1", "metrics-foo*", "@timestamp1");
+        createIndexTemplate("id1", "@timestamp1", "metrics-foo*");
         CreateDataStreamAction.Request createDataStreamRequest = new CreateDataStreamAction.Request("metrics-foo");
         client().admin().indices().createDataStream(createDataStreamRequest).get();
 
-        createIndexTemplate("id2", "metrics-bar*", "@timestamp2");
+        createIndexTemplate("id2", "@timestamp2", "metrics-bar*");
         createDataStreamRequest = new CreateDataStreamAction.Request("metrics-bar");
         client().admin().indices().createDataStream(createDataStreamRequest).get();
 
@@ -184,7 +184,7 @@ public class DataStreamIT extends ESIntegTestCase {
     }
 
     public void testOtherWriteOps() throws Exception {
-        createIndexTemplate("id", "metrics-foobar*", "@timestamp1");
+        createIndexTemplate("id", "@timestamp1", "metrics-foobar*");
         String dataStreamName = "metrics-foobar";
         CreateDataStreamAction.Request createDataStreamRequest = new CreateDataStreamAction.Request(dataStreamName);
         client().admin().indices().createDataStream(createDataStreamRequest).get();
@@ -350,7 +350,7 @@ public class DataStreamIT extends ESIntegTestCase {
     }
 
     public void testResolvabilityOfDataStreamsInAPIs() throws Exception {
-        createIndexTemplate("id", "logs-*", "ts");
+        createIndexTemplate("id", "ts", "logs-*");
         String dataStreamName = "logs-foobar";
         CreateDataStreamAction.Request request = new CreateDataStreamAction.Request(dataStreamName);
         client().admin().indices().createDataStream(request).actionGet();

476e454aa91c35bc54b54877ecdfb9eceebc3f2c
Julie Tibshirani <julie.tibshirani@elastic.co>
2020-06-17 10:36:10 -0700

    For the fields fetch phase, avoid reloading stored fields. (#58196)
    This PR updates FetchFieldsPhase to override hitExecute instead of hitsExecute
    (plural). This way, we can make sure that the stored fields (including _source)
    are only loaded once per hit as part of FetchPhase.

--- server/src/main/java/org/elasticsearch/search/fetch/FetchPhase.java
+++ server/src/main/java/org/elasticsearch/search/fetch/FetchPhase.java
@@ -101,7 +101,8 @@ public class FetchPhase implements SearchPhase {
             if (!context.hasScriptFields() && !context.hasFetchSourceContext()) {
                 context.fetchSourceContext(new FetchSourceContext(true));
             }
-            fieldsVisitor = new FieldsVisitor(context.sourceRequested());
+            boolean loadSource = context.sourceRequested() || context.fetchFieldsContext() != null;
+            fieldsVisitor = new FieldsVisitor(loadSource);
         } else if (storedFieldsContext.fetchFields() == false) {
             // disable stored fields entirely
             fieldsVisitor = null;
@@ -130,7 +131,7 @@ public class FetchPhase implements SearchPhase {
                     }
                 }
             }
-            boolean loadSource = context.sourceRequested();
+            boolean loadSource = context.sourceRequested() || context.fetchFieldsContext() != null;
             if (storedToRequestedFields.isEmpty()) {
                 // empty list specified, default to disable _source if no explicit indication
                 fieldsVisitor = new FieldsVisitor(loadSource);

c0983e6235cdd2471b5a73ae9f0eb17f04de763b
Ryan Ernst <ryan@iernst.net>
2020-06-17 12:25:33 -0700

    checkstyle

--- qa/os/src/test/java/org/elasticsearch/packaging/util/FileUtils.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/util/FileUtils.java
@@ -36,7 +36,6 @@ import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.OpenOption;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileOwnerAttributeView;

d535146fd4ce696255bb84f8d66e08f7745e2224
Armin Braun <me@obrown.io>
2020-06-19 19:18:23 +0200

    Deduplicate Empty InternalAggregations
    Working through a heap dump for an unrelated issue I found that we can easily rack up
    tens of MBs of duplicate empty instances in some cases.
    I moved to a static constructor to guard against that in all cases.

--- server/src/test/java/org/elasticsearch/search/aggregations/AggregationsTests.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/AggregationsTests.java
@@ -296,6 +296,6 @@ public class AggregationsTests extends ESTestCase {
             }
             aggs.add(testCase.createTestInstance());
         }
-        return new InternalAggregations(aggs);
+        return InternalAggregations.from(aggs);
     }
 }

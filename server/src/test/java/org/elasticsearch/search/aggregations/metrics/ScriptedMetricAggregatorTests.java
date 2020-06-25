elastic/elasticsearch

commit 2b82551c9369e308b728c4710d34e3fa6c4f3212

Remove deprecated wrapped from scripted_metric (#57627)
This removes the deprecated `asMultiBucketAggregator` wrapper from
`scripted_metric`. Unlike most other such removals, this isn't likely to
save much memory. But it does make the internals of the aggregator
slightly less twisted.

Relates to #56487

server/src/main/java/org/elasticsearch/search/aggregations/metrics/ScriptedMetricAggregator.java
@@ -23,38 +23,66 @@ import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.search.Scorable;
 import org.apache.lucene.search.ScoreMode;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.lease.Releasables;
 import org.elasticsearch.common.util.CollectionUtils;
+import org.elasticsearch.common.util.ObjectArray;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.script.ScriptedMetricAggContexts;
+import org.elasticsearch.script.ScriptedMetricAggContexts.MapScript;
 import org.elasticsearch.search.aggregations.Aggregator;
 import org.elasticsearch.search.aggregations.InternalAggregation;
 import org.elasticsearch.search.aggregations.LeafBucketCollector;
 import org.elasticsearch.search.aggregations.LeafBucketCollectorBase;
 import org.elasticsearch.search.internal.SearchContext;
+import org.elasticsearch.search.lookup.SearchLookup;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 class ScriptedMetricAggregator extends MetricsAggregator {
+    /**
+     * Estimated cost to maintain a bucket. Since this aggregator uses
+     * untracked java collections for its state it is going to both be
+     * much "heavier" than a normal metric aggregator and not going to be
+     * tracked by the circuit breakers properly. This is sad. So we pick a big
+     * number and estimate that each bucket costs that. It could be wildly
+     * inaccurate. We're sort of hoping that the real memory breaker saves
+     * us here. Or that folks just don't use the aggregation.
+     */
+    private static final long BUCKET_COST_ESTIMATE = 1024 * 5;
 
-    private final ScriptedMetricAggContexts.MapScript.LeafFactory mapScript;
-    private final ScriptedMetricAggContexts.CombineScript combineScript;
+    private final SearchLookup lookup;
+    private final Map<String, Object> initialState;
+    private final ScriptedMetricAggContexts.MapScript.Factory mapScriptFactory;
+    private final Map<String, Object> mapScriptParams;
+    private final ScriptedMetricAggContexts.CombineScript.Factory combineScriptFactory;
+    private final Map<String, Object> combineScriptParams;
     private final Script reduceScript;
-    private Map<String, Object> aggState;
+    private ObjectArray<State> states;
 
-    ScriptedMetricAggregator(String name,
-                                ScriptedMetricAggContexts.MapScript.LeafFactory mapScript,
-                                ScriptedMetricAggContexts.CombineScript combineScript,
-                                Script reduceScript,
-                                Map<String, Object> aggState,
-                                SearchContext context,
-                                Aggregator parent,
-                                Map<String, Object> metadata) throws IOException {
+    ScriptedMetricAggregator(
+        String name,
+        SearchLookup lookup,
+        Map<String, Object> initialState,
+        ScriptedMetricAggContexts.MapScript.Factory mapScriptFactory,
+        Map<String, Object> mapScriptParams,
+        ScriptedMetricAggContexts.CombineScript.Factory combineScriptFactory,
+        Map<String, Object> combineScriptParams,
+        Script reduceScript,
+        SearchContext context,
+        Aggregator parent,
+        Map<String, Object> metadata
+    ) throws IOException {
         super(name, context, parent, metadata);
-        this.aggState = aggState;
-        this.mapScript = mapScript;
-        this.combineScript = combineScript;
+        this.lookup = lookup;
+        this.initialState = initialState;
+        this.mapScriptFactory = mapScriptFactory;
+        this.mapScriptParams = mapScriptParams;
+        this.combineScriptFactory = combineScriptFactory;
+        this.combineScriptParams = combineScriptParams;
         this.reduceScript = reduceScript;
+        states = context.bigArrays().newObjectArray(1);
     }
 
     @Override
@@ -63,36 +91,77 @@ class ScriptedMetricAggregator extends MetricsAggregator {
     }
 
     @Override
-    public LeafBucketCollector getLeafCollector(LeafReaderContext ctx,
-            final LeafBucketCollector sub) throws IOException {
-        final ScriptedMetricAggContexts.MapScript leafMapScript = mapScript.newInstance(ctx);
-        return new LeafBucketCollectorBase(sub, leafMapScript) {
+    public LeafBucketCollector getLeafCollector(LeafReaderContext ctx, LeafBucketCollector sub) throws IOException {
+        // Clear any old leaf scripts so we rebuild them on the new leaf when we first see them.
+        for (long i = 0; i < states.size(); i++) {
+            State state = states.get(i);
+            if (state == null) {
+                continue;
+            }
+            state.leafMapScript = null;
+        }
+        return new LeafBucketCollectorBase(sub, null) {
+            private Scorable scorer;
+
             @Override
             public void setScorer(Scorable scorer) throws IOException {
-                leafMapScript.setScorer(scorer);
+                this.scorer = scorer;
             }
 
             @Override
-            public void collect(int doc, long bucket) throws IOException {
-                assert bucket == 0 : bucket;
-
-                leafMapScript.setDocument(doc);
-                leafMapScript.execute();
+            public void collect(int doc, long owningBucketOrd) throws IOException {
+                states = context.bigArrays().grow(states, owningBucketOrd + 1);
+                State state = states.get(owningBucketOrd);
+                if (state == null) {
+                    addRequestCircuitBreakerBytes(BUCKET_COST_ESTIMATE);
+                    state = new State();
+                    states.set(owningBucketOrd, state);
+                }
+                if (state.leafMapScript == null) {
+                    state.leafMapScript = state.mapScript.newInstance(ctx);
+                    state.leafMapScript.setScorer(scorer);
+                }
+                state.leafMapScript.setDocument(doc);
+                state.leafMapScript.execute();
             }
         };
     }
 
     @Override
     public InternalAggregation buildAggregation(long owningBucketOrdinal) {
-        Object aggregation;
-        if (combineScript != null) {
-            aggregation = combineScript.execute();
-            CollectionUtils.ensureNoSelfReferences(aggregation, "Scripted metric aggs combine script");
-        } else {
-            aggregation = aggState;
+        Object result = resultFor(aggStateFor(owningBucketOrdinal));
+        StreamOutput.checkWriteable(result);
+        return new InternalScriptedMetric(name, result, reduceScript, metadata());
+    }
+
+    private Map<String, Object> aggStateFor(long owningBucketOrdinal) {
+        if (owningBucketOrdinal >= states.size()) {
+            return newInitialState();
         }
-        StreamOutput.checkWriteable(aggregation);
-        return new InternalScriptedMetric(name, aggregation, reduceScript, metadata());
+        State state = states.get(owningBucketOrdinal);
+        if (state == null) {
+            return newInitialState();
+        }
+        // The last script that touched the state at this point is the "map" script
+        CollectionUtils.ensureNoSelfReferences(state.aggState, "Scripted metric aggs map script");
+        return state.aggState;
+    }
+
+    private Object resultFor(Map<String, Object> aggState) {
+        if (combineScriptFactory == null) {
+            return aggState;
+        }
+        Object result = combineScriptFactory.newInstance(
+            // Send a deep copy of the params because the script is allowed to mutate it
+            ScriptedMetricAggregatorFactory.deepCopyParams(combineScriptParams, context),
+            aggState
+        ).execute();
+        CollectionUtils.ensureNoSelfReferences(result, "Scripted metric aggs combine script");
+        return result;
+    }
+
+    private Map<String, Object> newInitialState() {
+        return initialState == null ? new HashMap<>() : ScriptedMetricAggregatorFactory.deepCopyParams(initialState, context);
     }
 
     @Override
@@ -101,9 +170,23 @@ class ScriptedMetricAggregator extends MetricsAggregator {
     }
 
     @Override
-    protected void doPostCollection() throws IOException {
-        CollectionUtils.ensureNoSelfReferences(aggState, "Scripted metric aggs map script");
+    public void close() {
+        Releasables.close(states);
+    }
 
-        super.doPostCollection();
+    private class State {
+        private final ScriptedMetricAggContexts.MapScript.LeafFactory mapScript;
+        private final Map<String, Object> aggState;
+        private MapScript leafMapScript;
+
+        State() {
+            aggState = newInitialState();
+            mapScript = mapScriptFactory.newFactory(
+                // Send a deep copy of the params because the script is allowed to mutate it
+                ScriptedMetricAggregatorFactory.deepCopyParams(mapScriptParams, context),
+                aggState,
+                lookup
+            );
+        }
     }
 }

commit 01ff4720768d7f7aab65451e792e081fec01f091

Fix two scripted_metric bugs
Fixes two bugs introduced by #57627:
1. We were not properly letting go of memory from the request breaker
   when the aggregation finished.
2. We no longer supported totally arbitrary stuff produced by the init
   script because we *assumed* that it'd be ok to run the script once
   and clone its results. Sadly, cloning can't clone *anything* that the
   init script can make, like `String` arrays. This runs the init script
   once for every new bucket so we don't need to clone.

server/src/main/java/org/elasticsearch/search/aggregations/metrics/ScriptedMetricAggregationBuilder.java
@@ -232,7 +232,7 @@ public class ScriptedMetricAggregationBuilder extends AbstractAggregationBuilder
             compiledInitScript = queryShardContext.compile(initScript, ScriptedMetricAggContexts.InitScript.CONTEXT);
             initScriptParams = initScript.getParams();
         } else {
-            compiledInitScript = (p, a) -> null;
+            compiledInitScript = null;
             initScriptParams = Collections.emptyMap();
         }
 
@@ -241,12 +241,9 @@ public class ScriptedMetricAggregationBuilder extends AbstractAggregationBuilder
         Map<String, Object> mapScriptParams = mapScript.getParams();
 
 
-        ScriptedMetricAggContexts.CombineScript.Factory compiledCombineScript;
-        Map<String, Object> combineScriptParams;
-
-        compiledCombineScript = queryShardContext.compile(combineScript,
+        ScriptedMetricAggContexts.CombineScript.Factory compiledCombineScript = queryShardContext.compile(combineScript,
             ScriptedMetricAggContexts.CombineScript.CONTEXT);
-        combineScriptParams = combineScript.getParams();
+        Map<String, Object> combineScriptParams = combineScript.getParams();
 
         return new ScriptedMetricAggregatorFactory(name, compiledMapScript, mapScriptParams, compiledInitScript,
                 initScriptParams, compiledCombineScript, combineScriptParams, reduceScript,

server/src/main/java/org/elasticsearch/search/aggregations/metrics/ScriptedMetricAggregator.java
@@ -22,6 +22,7 @@ package org.elasticsearch.search.aggregations.metrics;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.search.Scorable;
 import org.apache.lucene.search.ScoreMode;
+import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.lease.Releasables;
 import org.elasticsearch.common.util.CollectionUtils;
@@ -53,7 +54,8 @@ class ScriptedMetricAggregator extends MetricsAggregator {
     private static final long BUCKET_COST_ESTIMATE = 1024 * 5;
 
     private final SearchLookup lookup;
-    private final Map<String, Object> initialState;
+    private final ScriptedMetricAggContexts.InitScript.Factory initScriptFactory;
+    private final Map<String, Object> initScriptParams;
     private final ScriptedMetricAggContexts.MapScript.Factory mapScriptFactory;
     private final Map<String, Object> mapScriptParams;
     private final ScriptedMetricAggContexts.CombineScript.Factory combineScriptFactory;
@@ -64,7 +66,8 @@ class ScriptedMetricAggregator extends MetricsAggregator {
     ScriptedMetricAggregator(
         String name,
         SearchLookup lookup,
-        Map<String, Object> initialState,
+        @Nullable ScriptedMetricAggContexts.InitScript.Factory initScriptFactory,
+        Map<String, Object> initScriptParams,
         ScriptedMetricAggContexts.MapScript.Factory mapScriptFactory,
         Map<String, Object> mapScriptParams,
         ScriptedMetricAggContexts.CombineScript.Factory combineScriptFactory,
@@ -76,7 +79,8 @@ class ScriptedMetricAggregator extends MetricsAggregator {
     ) throws IOException {
         super(name, context, parent, metadata);
         this.lookup = lookup;
-        this.initialState = initialState;
+        this.initScriptFactory = initScriptFactory;
+        this.initScriptParams = initScriptParams;
         this.mapScriptFactory = mapScriptFactory;
         this.mapScriptParams = mapScriptParams;
         this.combineScriptFactory = combineScriptFactory;
@@ -129,12 +133,12 @@ class ScriptedMetricAggregator extends MetricsAggregator {
 
     @Override
     public InternalAggregation buildAggregation(long owningBucketOrdinal) {
-        Object result = resultFor(aggStateFor(owningBucketOrdinal));
+        Object result = resultFor(aggStateForResult(owningBucketOrdinal));
         StreamOutput.checkWriteable(result);
         return new InternalScriptedMetric(name, result, reduceScript, metadata());
     }
 
-    private Map<String, Object> aggStateFor(long owningBucketOrdinal) {
+    private Map<String, Object> aggStateForResult(long owningBucketOrdinal) {
         if (owningBucketOrdinal >= states.size()) {
             return newInitialState();
         }
@@ -161,7 +165,13 @@ class ScriptedMetricAggregator extends MetricsAggregator {
     }
 
     private Map<String, Object> newInitialState() {
-        return initialState == null ? new HashMap<>() : ScriptedMetricAggregatorFactory.deepCopyParams(initialState, context);
+        if (initScriptFactory == null) {
+            return new HashMap<>();
+        }
+        Map<String, Object> initialState = new HashMap<>();
+        initScriptFactory.newInstance(ScriptedMetricAggregatorFactory.deepCopyParams(initScriptParams, context), initialState).execute();
+        CollectionUtils.ensureNoSelfReferences(initialState, "Scripted metric aggs init script");
+        return initialState;
     }
 
     @Override
@@ -170,7 +180,7 @@ class ScriptedMetricAggregator extends MetricsAggregator {
     }
 
     @Override
-    public void close() {
+    public void doClose() {
         Releasables.close(states);
     }
 

server/src/main/java/org/elasticsearch/search/aggregations/metrics/ScriptedMetricAggregatorFactory.java
@@ -19,7 +19,7 @@
 
 package org.elasticsearch.search.aggregations.metrics;
 
-import org.elasticsearch.common.util.CollectionUtils;
+import org.elasticsearch.common.Nullable;
 import org.elasticsearch.index.query.QueryShardContext;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.script.ScriptedMetricAggContexts;
@@ -45,16 +45,26 @@ class ScriptedMetricAggregatorFactory extends AggregatorFactory {
     private final Script reduceScript;
     private final Map<String, Object> aggParams;
     private final SearchLookup lookup;
+    @Nullable
     private final ScriptedMetricAggContexts.InitScript.Factory initScript;
     private final Map<String, Object> initScriptParams;
 
-    ScriptedMetricAggregatorFactory(String name,
-                                    ScriptedMetricAggContexts.MapScript.Factory mapScript, Map<String, Object> mapScriptParams,
-                                    ScriptedMetricAggContexts.InitScript.Factory initScript, Map<String, Object> initScriptParams,
-                                    ScriptedMetricAggContexts.CombineScript.Factory combineScript,
-                                    Map<String, Object> combineScriptParams, Script reduceScript, Map<String, Object> aggParams,
-                                    SearchLookup lookup, QueryShardContext queryShardContext, AggregatorFactory parent,
-                                    AggregatorFactories.Builder subFactories, Map<String, Object> metadata) throws IOException {
+    ScriptedMetricAggregatorFactory(
+        String name,
+        ScriptedMetricAggContexts.MapScript.Factory mapScript,
+        Map<String, Object> mapScriptParams,
+        @Nullable ScriptedMetricAggContexts.InitScript.Factory initScript,
+        Map<String, Object> initScriptParams,
+        ScriptedMetricAggContexts.CombineScript.Factory combineScript,
+        Map<String, Object> combineScriptParams,
+        Script reduceScript,
+        Map<String, Object> aggParams,
+        SearchLookup lookup,
+        QueryShardContext queryShardContext,
+        AggregatorFactory parent,
+        AggregatorFactories.Builder subFactories,
+        Map<String, Object> metadata
+    ) throws IOException {
         super(name, queryShardContext, parent, subFactories, metadata);
         this.mapScript = mapScript;
         this.mapScriptParams = mapScriptParams;
@@ -73,17 +83,8 @@ class ScriptedMetricAggregatorFactory extends AggregatorFactory {
                                         boolean collectsFromSingleBucket,
                                         Map<String, Object> metadata) throws IOException {
         Map<String, Object> aggParams = this.aggParams == null ? Map.of() : this.aggParams;
-        Map<String, Object> initialState = new HashMap<String, Object>();
 
-        ScriptedMetricAggContexts.InitScript initScript = this.initScript.newInstance(
-            mergeParams(aggParams, initScriptParams),
-            initialState
-        );
-        if (initScript != null) {
-            initScript.execute();
-            CollectionUtils.ensureNoSelfReferences(initialState, "Scripted metric aggs init script");
-        }
-
+        Map<String, Object> initParams = mergeParams(aggParams, initScriptParams);
         Map<String, Object> mapParams = mergeParams(aggParams, mapScriptParams); 
         Map<String, Object> combineParams = mergeParams(aggParams, combineScriptParams); 
         Script reduceScript = deepCopyScript(this.reduceScript, searchContext, aggParams);
@@ -91,7 +92,8 @@ class ScriptedMetricAggregatorFactory extends AggregatorFactory {
         return new ScriptedMetricAggregator(
             name,
             lookup,
-            initialState,
+            initScript,
+            initParams,
             mapScript,
             mapParams,
             combineScript,

server/src/test/java/org/elasticsearch/search/aggregations/metrics/ScriptedMetricAggregatorTests.java
@@ -19,6 +19,7 @@
 
 package org.elasticsearch.search.aggregations.metrics;
 
+import org.apache.lucene.document.Document;
 import org.apache.lucene.document.SortedNumericDocValuesField;
 import org.apache.lucene.document.SortedSetDocValuesField;
 import org.apache.lucene.index.DirectoryReader;
@@ -26,12 +27,17 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.RandomIndexWriter;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.CheckedConsumer;
+import org.elasticsearch.common.breaker.CircuitBreaker;
+import org.elasticsearch.common.breaker.CircuitBreakingException;
+import org.elasticsearch.common.breaker.NoopCircuitBreaker;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.BigArrays;
 import org.elasticsearch.index.IndexSettings;
+import org.elasticsearch.index.mapper.MappedFieldType;
 import org.elasticsearch.index.mapper.MapperService;
 import org.elasticsearch.index.query.QueryShardContext;
 import org.elasticsearch.indices.breaker.CircuitBreakerService;
@@ -42,9 +48,13 @@ import org.elasticsearch.script.ScriptModule;
 import org.elasticsearch.script.ScriptService;
 import org.elasticsearch.script.ScriptType;
 import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.Aggregator;
 import org.elasticsearch.search.aggregations.AggregatorTestCase;
+import org.elasticsearch.search.aggregations.MultiBucketConsumerService.MultiBucketConsumer;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.internal.SearchContext;
+import org.junit.Before;
 import org.junit.BeforeClass;
 
 import java.io.IOException;
@@ -58,6 +68,8 @@ import java.util.function.Function;
 
 import static java.util.Collections.singleton;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ScriptedMetricAggregatorTests extends AggregatorTestCase {
 
@@ -94,6 +106,13 @@ public class ScriptedMetricAggregatorTests extends AggregatorTestCase {
             Collections.emptyMap());
     private static final Script COMBINE_SCRIPT_SELF_REF = new Script(ScriptType.INLINE, MockScriptEngine.NAME, "combineScriptSelfRef",
             Collections.emptyMap());
+
+    private static final Script INIT_SCRIPT_MAKING_ARRAY = new Script(
+        ScriptType.INLINE,
+        MockScriptEngine.NAME,
+        "initScriptMakingArray",
+        Collections.emptyMap()
+    );
 
     private static final Map<String, Function<Map<String, Object>, Object>> SCRIPTS = new HashMap<>();
 
@@ -181,6 +200,46 @@ public class ScriptedMetricAggregatorTests extends AggregatorTestCase {
            state.put("selfRef", state);
            return state;
         });
+        SCRIPTS.put("initScriptMakingArray", params -> {
+            Map<String, Object> state = (Map<String, Object>) params.get("state");
+            state.put("array", new String[] {"foo", "bar"});
+            state.put("collector", new ArrayList<Integer>());
+            return state;
+         });
+    }
+
+    private CircuitBreakerService circuitBreakerService;
+
+    @Before
+    public void mockBreaker() {
+        circuitBreakerService = mock(CircuitBreakerService.class);
+        when(circuitBreakerService.getBreaker(CircuitBreaker.REQUEST)).thenReturn(new NoopCircuitBreaker(CircuitBreaker.REQUEST) {
+            private long total = 0;
+
+            @Override
+            public double addEstimateBytesAndMaybeBreak(long bytes, String label) throws CircuitBreakingException {
+                logger.debug("Used {} grabbing {} for {}", total, bytes, label);
+                total += bytes;
+                return total;
+            }
+
+            @Override
+            public long addWithoutBreaking(long bytes) {
+                logger.debug("Used {} grabbing {}", total, bytes);
+                total += bytes;
+                return total;
+            }
+
+            @Override
+            public long getUsed() {
+                return total;
+            }
+        });
+    }
+
+    @Override
+    protected void afterClose() {
+        assertThat(circuitBreakerService.getBreaker(CircuitBreaker.REQUEST).getUsed(), equalTo(0L));
     }
 
     @Override
@@ -420,8 +479,19 @@ public class ScriptedMetricAggregatorTests extends AggregatorTestCase {
         }
     }
 
+    public void testInitScriptMakesArray() throws IOException {
+        ScriptedMetricAggregationBuilder aggregationBuilder = new ScriptedMetricAggregationBuilder(AGG_NAME);
+        aggregationBuilder.initScript(INIT_SCRIPT_MAKING_ARRAY).mapScript(MAP_SCRIPT)
+            .combineScript(COMBINE_SCRIPT).reduceScript(REDUCE_SCRIPT);
+        testCase(aggregationBuilder, new MatchAllDocsQuery(), iw -> {
+            iw.addDocument(new Document());
+        }, (InternalScriptedMetric r) -> {
+            assertEquals(1, r.aggregation());
+        });
+    }
+
     public void testAsSubAgg() throws IOException {
-        AggregationBuilder aggregationBuilder = new TermsAggregationBuilder("t").field("t")
+        AggregationBuilder aggregationBuilder = new TermsAggregationBuilder("t").field("t").executionHint("map")
             .subAggregation(
                 new ScriptedMetricAggregationBuilder("scripted").initScript(INIT_SCRIPT)
                     .mapScript(MAP_SCRIPT)
@@ -444,6 +514,25 @@ public class ScriptedMetricAggregatorTests extends AggregatorTestCase {
             assertThat(oddMetric.aggregation(), equalTo(49));
         };
         testCase(aggregationBuilder, new MatchAllDocsQuery(), buildIndex, verify, keywordField("t"), longField("number"));
+    }
+
+    protected <A extends Aggregator> A createAggregator(
+        Query query,
+        AggregationBuilder aggregationBuilder,
+        IndexSearcher indexSearcher,
+        IndexSettings indexSettings,
+        MultiBucketConsumer bucketConsumer,
+        MappedFieldType... fieldTypes
+    ) throws IOException {
+        SearchContext searchContext = createSearchContext(
+            indexSearcher,
+            indexSettings,
+            query,
+            bucketConsumer,
+            circuitBreakerService,
+            fieldTypes
+        );
+        return createAggregator(aggregationBuilder, searchContext);
     }
 
     /**

test/framework/src/main/java/org/elasticsearch/search/aggregations/AggregatorTestCase.java
@@ -232,9 +232,13 @@ public abstract class AggregatorTestCase extends ESTestCase {
                                                         MultiBucketConsumer bucketConsumer,
                                                         MappedFieldType... fieldTypes) throws IOException {
         SearchContext searchContext = createSearchContext(indexSearcher, indexSettings, query, bucketConsumer, fieldTypes);
+        return createAggregator(aggregationBuilder, searchContext);
+    }
+
+    protected <A extends Aggregator> A createAggregator(AggregationBuilder aggregationBuilder, SearchContext searchContext)
+        throws IOException {
         @SuppressWarnings("unchecked")
-        A aggregator = (A) aggregationBuilder
-            .rewrite(searchContext.getQueryShardContext())
+        A aggregator = (A) aggregationBuilder.rewrite(searchContext.getQueryShardContext())
             .build(searchContext.getQueryShardContext(), null)
             .create(searchContext, null, true);
         return aggregator;
@@ -873,6 +877,11 @@ public abstract class AggregatorTestCase extends ESTestCase {
         Releasables.close(releasables);
         releasables.clear();
     }
+
+    /**
+     * Hook for checking things after all {@link Aggregator}s have been closed.
+     */
+    protected void afterClose() {}
 
     /**
      * Make a {@linkplain DateFieldMapper.DateFieldType} for a {@code date}.
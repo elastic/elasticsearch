694968b05095326c60c50628db26de89a6bb457c
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2020-06-26 08:45:30 +0200

    Reenable DateUtilsTests.testTimezoneIds (#58551)
    joda upgrade made its timezone db more up to date than joda 
    (causing this test to fail), hence it had to be reverted. 
    Joda and Java's timezone dbs are in sync now.
    revert of joda upgrade https://github.com/elastic/elasticsearch/pull/57137
    closes https://github.com/elastic/elasticsearch/issues/56343

--- server/src/test/java/org/elasticsearch/common/time/DateUtilsTests.java
+++ server/src/test/java/org/elasticsearch/common/time/DateUtilsTests.java
@@ -34,7 +34,7 @@ public class DateUtilsTests extends ESTestCase {
     private static final Set<String> IGNORE = new HashSet<>(Arrays.asList(
         "Eire", "Europe/Dublin" // dublin timezone in joda does not account for DST
     ));
-    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/56343")
+
     public void testTimezoneIds() {
         assertNull(DateUtils.dateTimeZoneToZoneId(null));
         assertNull(DateUtils.zoneIdToDateTimeZone(null));

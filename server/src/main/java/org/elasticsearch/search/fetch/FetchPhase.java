c6f8a8e5a06c46a02692cb2e5cb830770db59c91
boicehuang <boicehuang@tencent.com>
2020-06-28 10:10:00 +0800

    removed unused imports

--- server/src/main/java/org/elasticsearch/search/fetch/FetchPhase.java
+++ server/src/main/java/org/elasticsearch/search/fetch/FetchPhase.java
@@ -60,13 +60,11 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyMap;
 

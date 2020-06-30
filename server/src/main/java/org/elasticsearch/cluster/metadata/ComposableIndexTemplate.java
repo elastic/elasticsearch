344df52bca82c6cfc29bee69bada0a3f4f0f407e
Martijn van Groningen <martijn.v.groningen@gmail.com>
2020-06-30 08:29:23 +0200

    fixed checkstyle violation

--- server/src/main/java/org/elasticsearch/cluster/metadata/ComposableIndexTemplate.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/ComposableIndexTemplate.java
@@ -34,7 +34,6 @@ import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentParser;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;

8c93f4e1544d54364dfb98bd0e1dc1f408104334
Boice Huang <boicehuang@tencent.com>
2020-06-30 15:26:37 +0800

    Sort document by internal doc id in FetchPhase to better use LRU cache (#57273)
    This change sorts the docIdsToLoad once instead of in each sub-phase.

--- server/src/main/java/org/elasticsearch/search/fetch/subphase/FetchDocValuesPhase.java
+++ server/src/main/java/org/elasticsearch/search/fetch/subphase/FetchDocValuesPhase.java
@@ -40,9 +40,7 @@ import org.elasticsearch.search.internal.SearchContext;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
@@ -77,9 +75,6 @@ public final class FetchDocValuesPhase implements FetchSubPhase {
         if (context.docValuesContext() == null) {
             return;
         }
-
-        hits = hits.clone(); // don't modify the incoming hits
-        Arrays.sort(hits, Comparator.comparingInt(SearchHit::docId));
 
         if (context.docValuesContext().fields().stream()
                 .map(f -> f.format)

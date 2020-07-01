3a234d266914d78c83594568d5498e9277b3b83f
David Turner <david.turner@elastic.co>
2020-07-01 10:12:44 +0100

    Account for remaining recovery in disk allocator (#58800)
    Today the disk-based shard allocator accounts for incoming shards by
    subtracting the estimated size of the incoming shard from the free space on the
    node. This is an overly conservative estimate if the incoming shard has almost
    finished its recovery since in that case it is already consuming most of the
    disk space it needs.
    
    This change adds to the shard stats a measure of how much larger each store is
    expected to grow, computed from the ongoing recovery, and uses this to account
    for the disk usage of incoming shards more accurately.
    
    Backport of #58029 to 7.x
    
    * Picky picky
    
    * Missing type

--- server/src/main/java/org/elasticsearch/index/store/StoreStats.java
+++ server/src/main/java/org/elasticsearch/index/store/StoreStats.java
@@ -31,7 +31,16 @@ import java.io.IOException;
 
 public class StoreStats implements Writeable, ToXContentFragment {
 
+    /**
+     * Sentinel value for cases where the shard does not yet know its reserved size so we must fall back to an estimate, for instance
+     * prior to receiving the list of files in a peer recovery.
+     */
+    public static final long UNKNOWN_RESERVED_BYTES = -1L;
+
+    public static final Version RESERVED_BYTES_VERSION = Version.V_7_9_0;
+
     private long sizeInBytes;
+    private long reservedSize;
 
     public StoreStats() {
 
@@ -42,10 +51,21 @@ public class StoreStats implements Writeable, ToXContentFragment {
         if (in.getVersion().before(Version.V_6_0_0_alpha1)) {
             in.readVLong(); // throttleTimeInNanos
         }
+        if (in.getVersion().onOrAfter(RESERVED_BYTES_VERSION)) {
+            reservedSize = in.readZLong();
+        } else {
+            reservedSize = UNKNOWN_RESERVED_BYTES;
+        }
     }
 
-    public StoreStats(long sizeInBytes) {
+    /**
+     * @param sizeInBytes the size of the store in bytes
+     * @param reservedSize a prediction of how much larger the store is expected to grow, or {@link StoreStats#UNKNOWN_RESERVED_BYTES}.
+     */
+    public StoreStats(long sizeInBytes, long reservedSize) {
+        assert reservedSize == UNKNOWN_RESERVED_BYTES || reservedSize >= 0 : reservedSize;
         this.sizeInBytes = sizeInBytes;
+        this.reservedSize = reservedSize;
     }
 
     public void add(StoreStats stats) {
@@ -53,8 +73,12 @@ public class StoreStats implements Writeable, ToXContentFragment {
             return;
         }
         sizeInBytes += stats.sizeInBytes;
+        reservedSize = ignoreIfUnknown(reservedSize) + ignoreIfUnknown(stats.reservedSize);
     }
 
+    private static long ignoreIfUnknown(long reservedSize) {
+        return reservedSize == UNKNOWN_RESERVED_BYTES ? 0L : reservedSize;
+    }
 
     public long sizeInBytes() {
         return sizeInBytes;
@@ -72,11 +96,23 @@ public class StoreStats implements Writeable, ToXContentFragment {
         return size();
     }
 
+    /**
+     * A prediction of how much larger this store will eventually grow. For instance, if we are currently doing a peer recovery or restoring
+     * a snapshot into this store then we can account for the rest of the recovery using this field. A value of {@code -1B} indicates that
+     * the reserved size is unknown.
+     */
+    public ByteSizeValue getReservedSize() {
+        return new ByteSizeValue(reservedSize);
+    }
+
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         out.writeVLong(sizeInBytes);
         if (out.getVersion().before(Version.V_6_0_0_alpha1)) {
             out.writeVLong(0L); // throttleTimeInNanos
+        }
+        if (out.getVersion().onOrAfter(RESERVED_BYTES_VERSION)) {
+            out.writeZLong(reservedSize);
         }
     }
 
@@ -84,6 +120,7 @@ public class StoreStats implements Writeable, ToXContentFragment {
     public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
         builder.startObject(Fields.STORE);
         builder.humanReadableField(Fields.SIZE_IN_BYTES, Fields.SIZE, size());
+        builder.humanReadableField(Fields.RESERVED_IN_BYTES, Fields.RESERVED, getReservedSize());
         builder.endObject();
         return builder;
     }
@@ -92,5 +129,7 @@ public class StoreStats implements Writeable, ToXContentFragment {
         static final String STORE = "store";
         static final String SIZE = "size";
         static final String SIZE_IN_BYTES = "size_in_bytes";
+        static final String RESERVED = "reserved";
+        static final String RESERVED_IN_BYTES = "reserved_in_bytes";
     }
 }

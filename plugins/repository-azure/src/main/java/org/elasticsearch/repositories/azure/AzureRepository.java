15c85b29fd9ad8f3b16495a381e2ff1c41a83bcd
Yannick Welsch <yannick@welsch.lu>
2020-07-01 12:19:29 +0200

    Account for recovery throttling when restoring snapshot (#58658) (#58811)
    Restoring from a snapshot (which is a particular form of recovery) does not currently take recovery throttling into account
    (i.e. the `indices.recovery.max_bytes_per_sec` setting). While restores are subject to their own throttling (repository
    setting `max_restore_bytes_per_sec`), this repository setting does not allow for values to be configured differently on a
    per-node basis. As restores are very similar in nature to peer recoveries (streaming bytes to the node), it makes sense to
    configure throttling in a single place.
    
    The `max_restore_bytes_per_sec` setting is also changed to default to unlimited now, whereas previously it was set to
    `40mb`, which is the current default of `indices.recovery.max_bytes_per_sec`). This means that no behavioral change
    will be observed by clusters where the recovery and restore settings were not adapted.
    
    Relates https://github.com/elastic/elasticsearch/issues/57023
    
    Co-authored-by: James Rodewig <james.rodewig@elastic.co>

--- plugins/repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureRepository.java
+++ plugins/repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureRepository.java
@@ -32,6 +32,7 @@ import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Setting.Property;
 import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
+import org.elasticsearch.indices.recovery.RecoverySettings;
 import org.elasticsearch.repositories.blobstore.BlobStoreRepository;
 
 import java.util.Locale;
@@ -81,8 +82,10 @@ public class AzureRepository extends BlobStoreRepository {
         final RepositoryMetadata metadata,
         final NamedXContentRegistry namedXContentRegistry,
         final AzureStorageService storageService,
-        final ClusterService clusterService) {
-        super(metadata, Repository.COMPRESS_SETTING.get(metadata.settings()), namedXContentRegistry, clusterService);
+        final ClusterService clusterService,
+        final RecoverySettings recoverySettings) {
+        super(metadata, Repository.COMPRESS_SETTING.get(metadata.settings()), namedXContentRegistry, clusterService,
+            recoverySettings);
         this.chunkSize = Repository.CHUNK_SIZE_SETTING.get(metadata.settings());
         this.storageService = storageService;
 

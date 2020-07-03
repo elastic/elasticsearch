f83cf2ac1b3db5133df80939b6ff737635475112
Tim Brooks <tim@uncontended.net>
2020-07-02 18:03:20 -0600

    AF

--- server/src/internalClusterTest/java/org/elasticsearch/action/bulk/WriteMemoryLimitsIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/action/bulk/WriteMemoryLimitsIT.java
@@ -71,8 +71,7 @@ public class WriteMemoryLimitsIT extends ESIntegTestCase {
     protected int numberOfShards() {
         return 1;
     }
-
-    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/58983")
+    
     public void testWriteBytesAreIncremented() throws Exception {
         assertAcked(prepareCreate(INDEX_NAME, Settings.builder()
             .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, 1)

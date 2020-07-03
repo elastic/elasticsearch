827c4edd14b9f394b43931937b3b24adc66b1e1d
Dan Hermann <danhermann@users.noreply.github.com>
2020-07-02 21:56:16 -0500

    Mirror privileges over data streams to their backing indices (#58381)

--- server/src/main/java/org/elasticsearch/action/admin/indices/stats/IndicesStatsRequest.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/stats/IndicesStatsRequest.java
@@ -290,4 +290,9 @@ public class IndicesStatsRequest extends BroadcastRequest<IndicesStatsRequest> {
         super.writeTo(out);
         flags.writeTo(out);
     }
+
+    @Override
+    public boolean includeDataStreams() {
+        return true;
+    }
 }

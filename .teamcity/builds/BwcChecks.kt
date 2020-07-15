80ac72b10dd14b98e849ad1eac2048f9dedbadfc
Mark Vieira <portugee@gmail.com>
2020-07-15 10:47:30 -0700

    Add bwc checks

--- .teamcity/builds/BwcChecks.kt
+++ .teamcity/builds/BwcChecks.kt
@@ -17,40 +17,23 @@
  * under the License.
  */
 
-import builds.BwcChecks
-import builds.OssChecks
-import builds.SanityCheck
-import builds.XpackChecks
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import templates.DefaultTemplate
+package builds
 
-version = "2020.1"
+import dependsOn
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 
-project {
-    vcsRoot(DefaultRoot)
-    template(DefaultTemplate)
+object BwcChecks : BuildType({
+    name = "BWC Checks"
+    description = "Runs backwards compatibility test suite"
 
-    defaultTemplate = DefaultTemplate
+    dependsOn(SanityCheck)
 
-    buildType {
-        id("Passed_Intake")
-        name = "Passed Intake"
-        type = BuildTypeSettings.Type.COMPOSITE
-
-        dependsOn(OssChecks, XpackChecks, BwcChecks) {
-            onDependencyFailure = FailureAction.ADD_PROBLEM
-            onDependencyCancel = FailureAction.ADD_PROBLEM
+    steps {
+        gradle {
+            useGradleWrapper = true
+            gradleParams = "--scan"
+            tasks = "bwcTestSnapshots"
         }
     }
-
-    subProject {
-        id("Checks")
-        name = "Checks"
-
-        buildType(SanityCheck)
-        buildType(OssChecks)
-        buildType(XpackChecks)
-        buildType(BwcChecks)
-    }
-
-}
+})

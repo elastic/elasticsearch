604f22db7974073675be0390c187130dc758c1d4
Tanguy Leroux <tlrx.dev@gmail.com>
2020-07-15 11:45:52 +0200

    Use a dedicated thread pool for searchable snapshot cache prewarming (#59313) (#59590)
    Since #58728 writing operations on searchable snapshot directory cache files
    are executed in an asynchronous manner using a dedicated thread pool. The
    thread pool used is searchable_snapshots which has been created to execute
    prewarming tasks.
    
    Reusing the same thread pool wasn't a good idea as it can lead to deadlock
    situations. One of these situation arose in a test failure where the thread pool
    was full of prewarming tasks, all waiting for a cache file to be accessible, while
    the cache file was being evicted by the cache service. But such an eviction
    can only be processed when all read/write operations on the cache file are
    completed and in this case the deadlock occurred because the cache file was
    actively being read by a concurrent search which also won the privilege to
    write the range of bytes in cache... and this writing operation could never have
     been completed because of the prewarming tasks making no progress and
    filling up the thread pool.
    
    This commit renames the searchable_snapshots thread pool to
    searchable_snapshots_cache_fetch_async. Assertions are added to assert
    that cache writes are executed using this thread pool and to assert that read
    on cached index inputs are executed using a different thread pool to avoid
    potential deadlock situations.
    
    This commit also adds a searchable_snapshots_cache_prewarming that is
    used to execute prewarming tasks. It also converts the existing cache prewarming
    test into a more complte integration test that creates multiple searchable
    snapshot indices concurrently with randomized thread pool sizes, and verifies
    that all files have been correctly prewarmed.

--- x-pack/plugin/searchable-snapshots/src/test/java/org/elasticsearch/index/store/SearchableSnapshotDirectoryTests.java
+++ x-pack/plugin/searchable-snapshots/src/test/java/org/elasticsearch/index/store/SearchableSnapshotDirectoryTests.java
@@ -471,7 +471,7 @@ public class SearchableSnapshotDirectoryTests extends ESTestCase {
                 writer.commit();
             }
 
-            final ThreadPool threadPool = new TestThreadPool(getTestName(), SearchableSnapshots.executorBuilder());
+            final ThreadPool threadPool = new TestThreadPool(getTestName(), SearchableSnapshots.executorBuilders());
             releasables.add(() -> terminate(threadPool));
 
             final Store store = new Store(shardId, indexSettings, directory, new DummyShardLock(shardId));
@@ -639,7 +639,7 @@ public class SearchableSnapshotDirectoryTests extends ESTestCase {
             final ShardId shardId = new ShardId(new Index("_name", "_id"), 0);
 
             final Path cacheDir = createTempDir();
-            final ThreadPool threadPool = new TestThreadPool(getTestName(), SearchableSnapshots.executorBuilder());
+            final ThreadPool threadPool = new TestThreadPool(getTestName(), SearchableSnapshots.executorBuilders());
             try (
                 SearchableSnapshotDirectory directory = new SearchableSnapshotDirectory(
                     () -> blobContainer,

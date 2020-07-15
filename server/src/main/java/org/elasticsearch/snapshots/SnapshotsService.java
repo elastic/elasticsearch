e0aea84efc878f02e731bb079a9ac874920b04a6
Armin Braun <me@obrown.io>
2020-07-15 03:43:38 +0200

    Reenable BwC Tests After #59578
    Reenabling BwC Tests now that #59578 is merged.

--- server/src/main/java/org/elasticsearch/snapshots/SnapshotsService.java
+++ server/src/main/java/org/elasticsearch/snapshots/SnapshotsService.java
@@ -119,7 +119,7 @@ import static org.elasticsearch.cluster.SnapshotsInProgress.completed;
  */
 public class SnapshotsService extends AbstractLifecycleComponent implements ClusterStateApplier {
 
-    public static final Version FULL_CONCURRENCY_VERSION = Version.V_8_0_0;
+    public static final Version FULL_CONCURRENCY_VERSION = Version.V_7_9_0;
 
     public static final Version SHARD_GEN_IN_REPO_DATA_VERSION = Version.V_7_6_0;
 

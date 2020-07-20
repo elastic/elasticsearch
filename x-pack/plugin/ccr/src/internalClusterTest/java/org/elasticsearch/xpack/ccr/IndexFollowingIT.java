0fe4d5df671ba8932fc6dd1e6341c4e1a9f011ae
Nhat Nguyen <nhat.nguyen@elastic.co>
2020-07-20 08:36:12 -0400

    Increase timeout testFollowIndexWithConcurrentMappingChanges
    Fixes #59273

--- x-pack/plugin/ccr/src/internalClusterTest/java/org/elasticsearch/xpack/ccr/IndexFollowingIT.java
+++ x-pack/plugin/ccr/src/internalClusterTest/java/org/elasticsearch/xpack/ccr/IndexFollowingIT.java
@@ -279,7 +279,9 @@ public class IndexFollowingIT extends CcrIntegTestCase {
                         leaderClient().admin().indices().prepareFlush("index1").setForce(true).get();
                     }
                 }
-                leaderClient().admin().indices().prepareFlush("index1").setForce(true).setWaitIfOngoing(true).get();
+                if (between(0, 100) < 20) {
+                    leaderClient().admin().indices().prepareFlush("index1").setForce(false).setWaitIfOngoing(false).get();
+                }
             }
         });
         thread.start();
@@ -290,7 +292,7 @@ public class IndexFollowingIT extends CcrIntegTestCase {
         ensureFollowerGreen("index2");
 
         for (int i = 0; i < firstBatchNumDocs; i++) {
-            assertBusy(assertExpectedDocumentRunnable(i));
+            assertBusy(assertExpectedDocumentRunnable(i), 1, TimeUnit.MINUTES);
         }
 
         final int secondBatchNumDocs = randomIntBetween(2, 64);
@@ -301,7 +303,7 @@ public class IndexFollowingIT extends CcrIntegTestCase {
         }
 
         for (int i = firstBatchNumDocs; i < firstBatchNumDocs + secondBatchNumDocs; i++) {
-            assertBusy(assertExpectedDocumentRunnable(i));
+            assertBusy(assertExpectedDocumentRunnable(i), 1, TimeUnit.MINUTES);
         }
 
         isRunning.set(false);

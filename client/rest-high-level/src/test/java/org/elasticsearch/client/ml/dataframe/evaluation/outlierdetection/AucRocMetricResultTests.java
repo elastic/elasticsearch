aad119f161000e4632faccb4f195a5d0f28a67a2
Przemyslaw Witek <przemyslaw.witek@elastic.co>
2020-07-21 10:49:49 +0200

    Revert spurious whitespace changes

--- client/rest-high-level/src/test/java/org/elasticsearch/client/ml/dataframe/evaluation/outlierdetection/AucRocMetricResultTests.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/ml/dataframe/evaluation/outlierdetection/AucRocMetricResultTests.java
@@ -7,7 +7,7 @@
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -32,7 +32,7 @@ public class AucRocMetricResultTests extends AbstractXContentTestCase<AucRocMetr
         return new AucRocMetric.Result(
             randomDouble(),
             Stream
-                .generate(() -> AucRocMetricAucRocPointTests.randomPoint())
+                .generate(AucRocMetricAucRocPointTests::randomPoint)
                 .limit(randomIntBetween(1, 10))
                 .collect(Collectors.toList()));
     }

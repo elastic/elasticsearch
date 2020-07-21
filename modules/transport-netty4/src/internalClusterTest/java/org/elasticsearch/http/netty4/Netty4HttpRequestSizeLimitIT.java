7dd57c9415a702f0f608dba0a38b54a99c85d405
Jake Landis <jake.landis@elastic.co>
2020-07-21 17:17:17 -0500

    Introduce javaRestTest source set/task and convert modules (#59939)
    Introduce a javaRestTest source set and task to compliment the yamlRestTest.
    javaRestTest differs such that the code is sourced from Java and may have
    different dependencies and setup requirements for the test clusters. This also
    allows the tests to run in parallel in different cluster instances to prevent any
    cross test contamination between the two types of tests.
    
    Included in this PR is all :modules no longer use the integTest task. The tests
    are now driven by test, yamlRestTest, javaRestTest, and internalClusterTest.
    Since only :modules (and :rest-api-spec) have been converted to yamlRestTest
    we can now disable the integTest task if either yamlRestTest or javaRestTest have
    been applied. Once all projects are converted, we can delete the integTest task.
    
    related: #56841
    related: #59444

--- modules/transport-netty4/src/internalClusterTest/java/org/elasticsearch/http/netty4/Netty4HttpRequestSizeLimitIT.java
+++ modules/transport-netty4/src/internalClusterTest/java/org/elasticsearch/http/netty4/Netty4HttpRequestSizeLimitIT.java
@@ -7,7 +7,7 @@
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -20,6 +20,7 @@
 package org.elasticsearch.http.netty4;
 
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.util.ReferenceCounted;
 import org.elasticsearch.ESNetty4IntegTestCase;
 import org.elasticsearch.common.collect.Tuple;
@@ -31,7 +32,6 @@ import org.elasticsearch.http.HttpServerTransport;
 import org.elasticsearch.indices.breaker.HierarchyCircuitBreakerService;
 import org.elasticsearch.test.ESIntegTestCase.ClusterScope;
 import org.elasticsearch.test.ESIntegTestCase.Scope;
-import io.netty.handler.codec.http.HttpResponseStatus;
 
 import java.util.ArrayList;
 import java.util.Collection;

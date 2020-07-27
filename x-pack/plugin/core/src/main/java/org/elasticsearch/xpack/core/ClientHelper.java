aa6ef62349bdb80f7b16784041f6c8d986b82977
David Roberts <dave.roberts@elastic.co>
2020-07-27 10:45:05 +0100

    Don't require separate privilege for internal detail of put pipeline (#60191)
    Putting an ingest pipeline used to require that the user calling
    it had permission to get nodes info as well as permission to
    manage ingest.  This was due to an internal implementaton detail
    that was not visible to the end user.
    
    This change alters the behaviour so that a user with the
    manage_pipeline cluster privilege can put an ingest pipeline
    regardless of whether they have the separate privilege to get
    nodes info.  The internal implementation detail now runs as
    the internal _xpack user when security is enabled.
    
    Backport of #60106

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ClientHelper.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ClientHelper.java
@@ -15,6 +15,7 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.client.OriginSettingClient;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.common.util.set.Sets;
+import org.elasticsearch.persistent.PersistentTasksService;
 import org.elasticsearch.xpack.core.security.authc.AuthenticationField;
 import org.elasticsearch.xpack.core.security.authc.AuthenticationServiceField;
 
@@ -63,7 +64,7 @@ public final class ClientHelper {
     public static final String INDEX_LIFECYCLE_ORIGIN = "index_lifecycle";
     public static final String MONITORING_ORIGIN = "monitoring";
     public static final String DEPRECATION_ORIGIN = "deprecation";
-    public static final String PERSISTENT_TASK_ORIGIN = "persistent_tasks";
+    public static final String PERSISTENT_TASK_ORIGIN = PersistentTasksService.PERSISTENT_TASK_ORIGIN;
     public static final String ROLLUP_ORIGIN = "rollup";
     public static final String ENRICH_ORIGIN = "enrich";
     public static final String TRANSFORM_ORIGIN = "transform";

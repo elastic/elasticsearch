1a3efc43292a23cb697108f294de3dfbe7ed8a0e
Jack Conradson <osjdconrad@gmail.com>
2020-07-27 10:47:58 -0700

    Make the Painless Semantic Phase External (#59797)
    This change switches the semantic phase for processing the user tree to the external visitor pattern. 
    Given the length the semantic phase is, each user tree node ends up with a static default semantic 
    analysis method. This helps keep the external phase visitor from being over 2k lines of code. It's still 
    separate in design, and each of the default methods could be moved somewhere else in the future if 
    necessary.

--- modules/lang-painless/src/test/java/org/elasticsearch/painless/FunctionTests.java
+++ modules/lang-painless/src/test/java/org/elasticsearch/painless/FunctionTests.java
@@ -22,6 +22,7 @@ package org.elasticsearch.painless;
 import static org.hamcrest.Matchers.containsString;
 
 public class FunctionTests extends ScriptTestCase {
+
     public void testBasic() {
         assertEquals(5, exec("int get() {5;} get()"));
     }
@@ -51,7 +52,8 @@ public class FunctionTests extends ScriptTestCase {
         Exception expected = expectScriptThrows(IllegalArgumentException.class, () -> {
             exec("void test(int x) {} test()");
         });
-        assertThat(expected.getMessage(), containsString("Cannot generate an empty function"));
+        assertThat(expected.getMessage(), containsString(
+                "invalid function definition: found no statements for function [test] with [1] parameters"));
     }
 
     public void testReturnsAreUnboxedIfNeeded() {
@@ -95,7 +97,8 @@ public class FunctionTests extends ScriptTestCase {
         Exception expected = expectScriptThrows(IllegalArgumentException.class, () -> {
             exec("int test(StringBuilder b, int i) {b.setLength(i)} test(new StringBuilder(), 1)");
         });
-        assertEquals("not all paths provide a return value for function [test] with [2] parameters", expected.getMessage());
+        assertEquals("invalid function definition: " +
+                "not all paths provide a return value for function [test] with [2] parameters", expected.getMessage());
         expected = expectScriptThrows(ClassCastException.class, () -> {
             exec("int test(StringBuilder b, int i) {return b.setLength(i)} test(new StringBuilder(), 1)");
         });

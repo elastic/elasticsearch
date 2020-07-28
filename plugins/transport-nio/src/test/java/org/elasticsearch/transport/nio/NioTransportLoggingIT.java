c98be4d29d2802ecebc31da5f50c8f0f803b8527
David Turner <david.turner@elastic.co>
2020-07-28 18:12:21 +0100

    Mute tests for #60321

--- plugins/transport-nio/src/test/java/org/elasticsearch/transport/nio/NioTransportLoggingIT.java
+++ plugins/transport-nio/src/test/java/org/elasticsearch/transport/nio/NioTransportLoggingIT.java
@@ -21,6 +21,7 @@ package org.elasticsearch.transport.nio;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
+import org.apache.lucene.util.LuceneTestCase;
 import org.elasticsearch.NioIntegTestCase;
 import org.elasticsearch.action.admin.cluster.node.hotthreads.NodesHotThreadsRequest;
 import org.elasticsearch.common.logging.Loggers;
@@ -34,6 +35,7 @@ import org.elasticsearch.transport.TransportLogger;
 import java.io.IOException;
 
 @ESIntegTestCase.ClusterScope(numDataNodes = 2)
+@LuceneTestCase.AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/60321")
 public class NioTransportLoggingIT extends NioIntegTestCase {
 
     private MockLogAppender appender;

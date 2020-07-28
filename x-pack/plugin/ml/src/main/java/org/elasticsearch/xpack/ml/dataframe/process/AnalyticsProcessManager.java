9528f9c0d2c9c15356e10ee71619eec83dc910a8
Dimitris Athanasiou <dimitris@elastic.co>
2020-07-28 14:46:05 +0300

    [ML] Ensure bulk requests are not over memory limit (#60219)
    Data frame analytics jobs that work with very large datasets
    may produce bulk requests that are over the memory limit
    for indexing. This commit adds a helper class that bundles
    index requests in bulk requests that steer away from the
    memory limit. We then use this class both from the results
    joiner and the inference runner ensuring data frame analytics
    jobs do not generate bulk requests that are too large.
    
    Note the limit was implemented in #58885.

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/dataframe/process/AnalyticsProcessManager.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/dataframe/process/AnalyticsProcessManager.java
@@ -18,6 +18,7 @@ import org.elasticsearch.client.ParentTaskAssigningClient;
 import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
@@ -61,6 +62,7 @@ public class AnalyticsProcessManager {
 
     private static final Logger LOGGER = LogManager.getLogger(AnalyticsProcessManager.class);
 
+    private final Settings settings;
     private final Client client;
     private final ExecutorService executorServiceForJob;
     private final ExecutorService executorServiceForProcess;
@@ -72,7 +74,8 @@ public class AnalyticsProcessManager {
     private final ResultsPersisterService resultsPersisterService;
     private final int numAllocatedProcessors;
 
-    public AnalyticsProcessManager(Client client,
+    public AnalyticsProcessManager(Settings settings,
+                                   Client client,
                                    ThreadPool threadPool,
                                    AnalyticsProcessFactory<AnalyticsResult> analyticsProcessFactory,
                                    DataFrameAnalyticsAuditor auditor,
@@ -81,6 +84,7 @@ public class AnalyticsProcessManager {
                                    ResultsPersisterService resultsPersisterService,
                                    int numAllocatedProcessors) {
         this(
+            settings,
             client,
             threadPool.generic(),
             threadPool.executor(MachineLearning.JOB_COMMS_THREAD_POOL_NAME),
@@ -93,7 +97,8 @@ public class AnalyticsProcessManager {
     }
 
     // Visible for testing
-    public AnalyticsProcessManager(Client client,
+    public AnalyticsProcessManager(Settings settings,
+                                   Client client,
                                    ExecutorService executorServiceForJob,
                                    ExecutorService executorServiceForProcess,
                                    AnalyticsProcessFactory<AnalyticsResult> analyticsProcessFactory,
@@ -102,6 +107,7 @@ public class AnalyticsProcessManager {
                                    ModelLoadingService modelLoadingService,
                                    ResultsPersisterService resultsPersisterService,
                                    int numAllocatedProcessors) {
+        this.settings = Objects.requireNonNull(settings);
         this.client = Objects.requireNonNull(client);
         this.executorServiceForJob = Objects.requireNonNull(executorServiceForJob);
         this.executorServiceForProcess = Objects.requireNonNull(executorServiceForProcess);
@@ -330,7 +336,7 @@ public class AnalyticsProcessManager {
 
         if (processContext.config.getAnalysis().supportsInference()) {
             refreshDest(parentTaskClient, processContext.config);
-            InferenceRunner inferenceRunner = new InferenceRunner(parentTaskClient, modelLoadingService, resultsPersisterService,
+            InferenceRunner inferenceRunner = new InferenceRunner(settings, parentTaskClient, modelLoadingService, resultsPersisterService,
                 task.getParentTaskId(), processContext.config, extractedFields, task.getStatsHolder().getProgressTracker(),
                 task.getStatsHolder().getDataCountsTracker());
             processContext.setInferenceRunner(inferenceRunner);
@@ -489,7 +495,7 @@ public class AnalyticsProcessManager {
         private AnalyticsResultProcessor createResultProcessor(DataFrameAnalyticsTask task,
                                                                DataFrameDataExtractorFactory dataExtractorFactory) {
             DataFrameRowsJoiner dataFrameRowsJoiner =
-                new DataFrameRowsJoiner(config.getId(), task.getParentTaskId(),
+                new DataFrameRowsJoiner(config.getId(), settings, task.getParentTaskId(),
                         dataExtractorFactory.newExtractor(true), resultsPersisterService);
             return new AnalyticsResultProcessor(
                 config, dataFrameRowsJoiner, task.getStatsHolder(), trainedModelProvider, auditor, statsPersister,

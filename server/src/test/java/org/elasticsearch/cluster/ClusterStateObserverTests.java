2a42d09fc9db54b305b4b4494b6f13229764d49a
David Turner <david.turner@elastic.co>
2020-07-28 11:37:07 +0100

    Precommit

--- server/src/test/java/org/elasticsearch/cluster/ClusterStateObserverTests.java
+++ server/src/test/java/org/elasticsearch/cluster/ClusterStateObserverTests.java
@@ -21,7 +21,6 @@ package org.elasticsearch.cluster;
 
 import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.cluster.service.ClusterApplierService;
-import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.util.concurrent.ThreadContext;

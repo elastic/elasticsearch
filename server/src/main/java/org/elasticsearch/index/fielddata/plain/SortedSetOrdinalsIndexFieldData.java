f3403faf1276351dae348e2712a4a36ff1a0914d
Julie Tibshirani <julie.tibshirani@elastic.co>
2020-07-30 13:53:59 -0700

    Remove IndexFieldData#clear since it is unused. (#60475)
    This method was never called. It also seemed tricky that calling a method on
    `IndexFieldData` could clear the contents of a shared cache.

--- server/src/main/java/org/elasticsearch/index/fielddata/plain/SortedSetOrdinalsIndexFieldData.java
+++ server/src/main/java/org/elasticsearch/index/fielddata/plain/SortedSetOrdinalsIndexFieldData.java
@@ -110,11 +110,6 @@ public class SortedSetOrdinalsIndexFieldData implements IndexOrdinalsFieldData {
     }
 
     @Override
-    public final void clear() {
-        // can't do
-    }
-
-    @Override
     public SortField sortField(@Nullable Object missingValue, MultiValueMode sortMode, Nested nested, boolean reverse) {
         XFieldComparatorSource source = new BytesRefFieldComparatorSource(this, missingValue, sortMode, nested);
         /**

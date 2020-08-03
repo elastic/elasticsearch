131d3071315e9c7eaa71d3e1a96c85c7248bdaca
Tim Brooks <tim@uncontended.net>
2020-08-03 17:39:24 -0600

    WIP

--- server/src/main/java/org/elasticsearch/index/shard/IndexShardOperationPermits.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexShardOperationPermits.java
@@ -238,7 +238,7 @@ final class IndexShardOperationPermits implements Closeable {
     }
 
     private void acquire(final ActionListener<Releasable> onAcquired, final String executorOnDelay, final boolean forceExecution,
-                        final Object debugInfo, final StackTraceElement[] stackTrace) {
+                         final Object debugInfo, final StackTraceElement[] stackTrace) {
         if (closed) {
             onAcquired.onFailure(new IndexShardClosedException(shardId));
             return;
@@ -271,7 +271,7 @@ final class IndexShardOperationPermits implements Closeable {
                     } else {
                         wrappedListener = new ContextPreservingActionListener<>(contextSupplier, onAcquired);
                     }
-                    delayedOperations.add(new DelayedOperation(wrappedListener, debugInfo, stackTrace));
+                    delayedOperations.add(new DelayedOperation(wrappedListener, debugInfo, stackTrace, forceExecution));
                     return;
                 } else {
                     releasable = acquire(debugInfo, stackTrace);
@@ -341,9 +341,12 @@ final class IndexShardOperationPermits implements Closeable {
         private final ActionListener<Releasable> listener;
         private final String debugInfo;
         private final StackTraceElement[] stackTrace;
+        private final boolean forceExecution;
 
-        private DelayedOperation(ActionListener<Releasable> listener, Object debugInfo, StackTraceElement[] stackTrace) {
+        private DelayedOperation(ActionListener<Releasable> listener, Object debugInfo, StackTraceElement[] stackTrace,
+                                 boolean forceExecution) {
             this.listener = listener;
+            this.forceExecution = forceExecution;
             if (Assertions.ENABLED) {
                 this.debugInfo = "[delayed] " + debugInfo;
                 this.stackTrace = stackTrace;

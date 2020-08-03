8ff1dece451c868ea080de75cf1d497dba444d2e
Armin Braun <me@obrown.io>
2020-08-03 21:47:21 +0200

    Fix Broken Stream Close in writeRawValue
    Small oversight in #56078 that only showed up
    during backporting.

--- libs/x-content/src/main/java/org/elasticsearch/common/xcontent/json/JsonXContentGenerator.java
+++ libs/x-content/src/main/java/org/elasticsearch/common/xcontent/json/JsonXContentGenerator.java
@@ -364,7 +364,7 @@ public class JsonXContentGenerator implements XContentGenerator {
                 generator.writeRaw(':');
             }
             flush();
-            Streams.copy(stream, os);
+            Streams.copy(stream, os, false);
             writeEndRaw();
         }
     }

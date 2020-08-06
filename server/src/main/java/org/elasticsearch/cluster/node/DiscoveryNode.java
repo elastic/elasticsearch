27974f13be8a140e8404f4484d3aa837937ce945
Lee Hinman <lee@writequit.org>
2020-08-06 12:48:53 -0600

    Add data tiers (hot, warm, cold, frozen) as custom node roles
    This commit adds the `data_hot`, `data_warm`, `data_cold`, and `data_frozen` node roles to the
    x-pack plugin. These roles are intended to be the base for the formalization of data tiers in
    Elasticsearch.
    
    These roles all act as data nodes (meaning shards can be allocated to them). Nodes with the existing
    `data` role acts as though they have all of the roles configured (it is a hot, warm, cold, and
    frozen node).
    
    This also includes a custom `AllocationDecider` that allows the user to configure the following
    settings on a cluster level:
    - `cluster.routing.allocation.require._tier`
    - `cluster.routing.allocation.include._tier`
    - `cluster.routing.allocation.exclude._tier`
    
    And in index settings:
    - `index.routing.allocation.require._tier`
    - `index.routing.allocation.include._tier`
    - `index.routing.allocation.exclude._tier`
    
    Relates to #60848

--- server/src/main/java/org/elasticsearch/cluster/node/DiscoveryNode.java
+++ server/src/main/java/org/elasticsearch/cluster/node/DiscoveryNode.java
@@ -24,6 +24,7 @@ import org.elasticsearch.common.UUIDs;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.util.set.Sets;
@@ -72,8 +73,13 @@ public class DiscoveryNode implements Writeable, ToXContentFragment {
         return hasRole(settings, DiscoveryNodeRole.MASTER_ROLE);
     }
 
+    /**
+     * Note, due to the way that plugins may not be available when settings are being initialized,
+     * do not use this from a static {@link Setting} default value function, as it may not contain
+     * all roles at that point.
+     */
     public static boolean isDataNode(final Settings settings) {
-        return hasRole(settings, DiscoveryNodeRole.DATA_ROLE);
+        return getRolesFromSettings(settings).stream().anyMatch(DiscoveryNodeRole::canContainData);
     }
 
     public static boolean isIngestNode(final Settings settings) {
@@ -328,7 +334,7 @@ public class DiscoveryNode implements Writeable, ToXContentFragment {
      * Should this node hold data (shards) or not.
      */
     public boolean isDataNode() {
-        return roles.contains(DiscoveryNodeRole.DATA_ROLE);
+        return roles.stream().anyMatch(DiscoveryNodeRole::canContainData);
     }
 
     /**

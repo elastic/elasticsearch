e021c4cb5b0fbd9502decc22830fd8d20b1648dc
boicehuang <boicehuang@tencent.com>
2020-08-07 11:13:17 +0800

    Fix Coordination State handleJoin method typo.

--- server/src/main/java/org/elasticsearch/cluster/coordination/CoordinationState.java
+++ server/src/main/java/org/elasticsearch/cluster/coordination/CoordinationState.java
@@ -254,7 +254,7 @@ public class CoordinationState {
         boolean prevElectionWon = electionWon;
         electionWon = isElectionQuorum(joinVotes);
         assert !prevElectionWon || electionWon : // we cannot go from won to not won
-            "locaNode= " + localNode + ", join=" + join + ", joinVotes=" + joinVotes;
+            "localNode= " + localNode + ", join=" + join + ", joinVotes=" + joinVotes;
         logger.debug("handleJoin: added join {} from [{}] for election, electionWon={} lastAcceptedTerm={} lastAcceptedVersion={}", join,
             join.getSourceNode(), electionWon, lastAcceptedTerm, getLastAcceptedVersion());
 

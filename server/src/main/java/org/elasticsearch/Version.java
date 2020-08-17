5f620555384c745f302ef38b156f31cea0d0b2f4
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-08-17 14:27:10 +0300

    Bump current version to 6.8.13

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -156,6 +156,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_6_8_10 = new Version(6081099, org.apache.lucene.util.Version.LUCENE_7_7_3);
     public static final Version V_6_8_11 = new Version(6081199, org.apache.lucene.util.Version.LUCENE_7_7_3);
     public static final Version V_6_8_12 = new Version(6081299, org.apache.lucene.util.Version.LUCENE_7_7_3);
+    public static final Version V_6_8_13 = new Version(6081399, org.apache.lucene.util.Version.LUCENE_7_7_3);
 
     public static final Version CURRENT = V_6_8_12;
 

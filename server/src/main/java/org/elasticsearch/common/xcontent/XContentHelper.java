542d288af95c905b9fb15ceeba4fe3d3a3a1a520
Armin Braun <me@obrown.io>
2020-08-21 14:33:36 +0200

    Remove Potentially Expensive Use of BytesReference.toBytesRef
    This method might materialize all the bytes in a reference into a fresh `byte[]`.
    Using the stream is much safer and only trivially more expensive.

--- server/src/main/java/org/elasticsearch/common/xcontent/XContentHelper.java
+++ server/src/main/java/org/elasticsearch/common/xcontent/XContentHelper.java
@@ -19,7 +19,6 @@
 
 package org.elasticsearch.common.xcontent;
 
-import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.ElasticsearchParseException;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.bytes.BytesArray;
@@ -32,6 +31,7 @@ import org.elasticsearch.common.xcontent.ToXContent.Params;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -380,8 +380,13 @@ public class XContentHelper {
      */
     @Deprecated
     public static XContentType xContentType(BytesReference bytes) {
-        BytesRef br = bytes.toBytesRef();
-        return XContentFactory.xContentType(br.bytes, br.offset, br.length);
+        try {
+            final InputStream inputStream = bytes.streamInput();
+            return XContentFactory.xContentType(inputStream);
+        } catch (IOException e) {
+            assert false : "Should not happen, we're just reading bytes from memory";
+            throw new UncheckedIOException(e);
+        }
     }
 
     /**

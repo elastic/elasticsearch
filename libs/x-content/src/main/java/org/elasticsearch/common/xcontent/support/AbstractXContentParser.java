b34636a2d9fa9fbcaad49b362b15f0d1309620b8
Armin Braun <me@obrown.io>
2020-08-22 21:09:11 +0200

    Speed up XContent Collection Parsing
    1. Get rid of the capturing lambda on the hot path that inlines very badly
    2. Remove as many bounds checks as possible, thereby reducing method size and improving inlining
    3. Return singletons for empty collections instead of eagerly creating potentially redundant empty collections

--- libs/x-content/src/main/java/org/elasticsearch/common/xcontent/support/AbstractXContentParser.java
+++ libs/x-content/src/main/java/org/elasticsearch/common/xcontent/support/AbstractXContentParser.java
@@ -264,12 +264,12 @@ public abstract class AbstractXContentParser implements XContentParser {
 
     @Override
     public Map<String, Object> map() throws IOException {
-        return readMap(this, SIMPLE_MAP_FACTORY);
+        return readMapSafe(this, SIMPLE_MAP_FACTORY);
     }
 
     @Override
     public Map<String, Object> mapOrdered() throws IOException {
-        return readMap(this, ORDERED_MAP_FACTORY);
+        return readMapSafe(this, ORDERED_MAP_FACTORY);
     }
 
     @Override
@@ -280,97 +280,124 @@ public abstract class AbstractXContentParser implements XContentParser {
     @Override
     public <T> Map<String, T> map(
             Supplier<Map<String, T>> mapFactory, CheckedFunction<XContentParser, T, IOException> mapValueParser) throws IOException {
-        XContentParser.Token token = currentToken();
-        if (token == null) {
-            token = nextToken();
-        }
-        if (token == XContentParser.Token.START_OBJECT) {
-            token = nextToken();
-        }
-        if (token == Token.END_OBJECT) {
+        if (findMapStart(this) || nextToken() != XContentParser.Token.FIELD_NAME) {
             return Collections.emptyMap();
         }
         final Map<String, T> map = mapFactory.get();
-        for (; token == XContentParser.Token.FIELD_NAME; token = nextToken()) {
+        do {
             // Must point to field name
             String fieldName = currentName();
             // And then the value...
             nextToken();
             T value = mapValueParser.apply(this);
             map.put(fieldName, value);
-        }
+        } while (nextToken() == XContentParser.Token.FIELD_NAME);
         return map;
     }
 
     @Override
     public List<Object> list() throws IOException {
-        return readList(this, SIMPLE_MAP_FACTORY);
+        skipToListStart(this);
+        return readListUnsafe(this, SIMPLE_MAP_FACTORY);
     }
 
     @Override
     public List<Object> listOrderedMap() throws IOException {
-        return readList(this, ORDERED_MAP_FACTORY);
+        skipToListStart(this);
+        return readListUnsafe(this, ORDERED_MAP_FACTORY);
     }
 
     private static final Supplier<Map<String, Object>> SIMPLE_MAP_FACTORY = HashMap::new;
 
     private static final Supplier<Map<String, Object>> ORDERED_MAP_FACTORY = LinkedHashMap::new;
 
-    private static Map<String, Object> readMap(XContentParser parser, Supplier<Map<String, Object>> mapFactory) throws IOException {
-        XContentParser.Token token = parser.currentToken();
-        if (token == null) {
-            token = parser.nextToken();
-        }
-        if (token == XContentParser.Token.START_OBJECT) {
-            token = parser.nextToken();
-        }
-        if (token == Token.END_OBJECT) {
+    private static Map<String, Object> readMapSafe(XContentParser parser, Supplier<Map<String, Object>> mapFactory) throws IOException {
+        return findMapStart(parser) ? Collections.emptyMap() : readMapUnsafe(parser, mapFactory);
+    }
+
+    // Read a map without bounds checks from a parser that is assumed to be at a map start token
+    private static Map<String, Object> readMapUnsafe(XContentParser parser, Supplier<Map<String, Object>> mapFactory) throws IOException {
+        assert parser.currentToken() == Token.START_OBJECT;
+        if (parser.nextToken() != XContentParser.Token.FIELD_NAME) {
             return Collections.emptyMap();
         }
         final Map<String, Object> map = mapFactory.get();
-        for (; token == XContentParser.Token.FIELD_NAME; token = parser.nextToken()) {
+        do {
             // Must point to field name
             String fieldName = parser.currentName();
             // And then the value...
-            parser.nextToken();
-            Object value = readValue(parser, mapFactory);
+            Object value = readValueUnsafe(parser.nextToken(), parser, mapFactory);
             map.put(fieldName, value);
-        }
+        } while (parser.nextToken() == XContentParser.Token.FIELD_NAME);
         return map;
     }
 
-    static List<Object> readList(XContentParser parser, Supplier<Map<String, Object>> mapFactory) throws IOException {
-        XContentParser.Token token = parser.currentToken();
+    /**
+     * Checks if the next current token in the supplied parser is a map start.
+     * Skips to the next token if the parser does not yet have a current token (i.e. {@link #currentToken()} returns {@code null}) and then
+     * checks it.
+     *
+     * @return true if a map start is found
+     */
+    private static boolean findMapStart(XContentParser parser) throws IOException {
+        Token token = parser.currentToken();
+        if (token == null) {
+            token = parser.nextToken();
+        }
+        return token != XContentParser.Token.START_OBJECT;
+    }
+
+    // Skips the current parser to the next array start. Assumes that the parser is either positioned before an array field's name token or
+    // on the start array token.
+    private static void skipToListStart(XContentParser parser) throws IOException {
+        Token token = parser.currentToken();
         if (token == null) {
             token = parser.nextToken();
         }
         if (token == XContentParser.Token.FIELD_NAME) {
             token = parser.nextToken();
         }
-        if (token == XContentParser.Token.START_ARRAY) {
-            token = parser.nextToken();
-        } else {
+        if (token != XContentParser.Token.START_ARRAY) {
             throw new XContentParseException(parser.getTokenLocation(), "Failed to parse list:  expecting "
                     + XContentParser.Token.START_ARRAY + " but got " + token);
         }
+    }
 
+    // read a list without bounds checks, assuming the the current parser is always on an array start
+    private static List<Object> readListUnsafe(XContentParser parser, Supplier<Map<String, Object>> mapFactory) throws IOException {
+        assert parser.currentToken() == Token.START_ARRAY;
+        Token token = parser.nextToken();
         if (token == XContentParser.Token.END_ARRAY) {
             return Collections.emptyList();
         }
         ArrayList<Object> list = new ArrayList<>();
         for (; token != null && token != XContentParser.Token.END_ARRAY; token = parser.nextToken()) {
-            list.add(readValue(parser, mapFactory));
+            list.add(readValueUnsafe(token, parser, mapFactory));
         }
         return list;
     }
 
     public static Object readValue(XContentParser parser, Supplier<Map<String, Object>> mapFactory) throws IOException {
-        switch (parser.currentToken()) {
+        return readValueUnsafe(parser.currentToken(), parser, mapFactory);
+    }
+
+    /**
+     * Reads next value from the parser that is assumed to be at the given current token without any additional checks.
+     *
+     * @param currentToken current token that the parser is at
+     * @param parser       parser to read from
+     * @param mapFactory   map factory to use for reading objects
+     */
+    private static Object readValueUnsafe(Token currentToken, XContentParser parser,
+                                          Supplier<Map<String, Object>> mapFactory) throws IOException {
+        assert currentToken == parser.currentToken() : "Supplied current token [" + currentToken +
+                "] is different from actual parser current token [" + parser.currentToken() + "]";
+        switch (currentToken) {
             case VALUE_STRING: return parser.text();
             case VALUE_NUMBER: return parser.numberValue();
             case VALUE_BOOLEAN: return parser.booleanValue();
-            case START_OBJECT: return readMap(parser, mapFactory);
-            case START_ARRAY: return readList(parser, mapFactory);
+            case START_OBJECT: return readMapUnsafe(parser, mapFactory);
+            case START_ARRAY: return readListUnsafe(parser, mapFactory);
             case VALUE_EMBEDDED_OBJECT: return parser.binaryValue();
             case VALUE_NULL:
             default: return null;

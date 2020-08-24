3c7fe453e84de221a20075bbf2f7272fc5a94743
Luca Cavanna <javanna@users.noreply.github.com>
2020-08-24 17:26:08 +0200

    lower max field chain limit to 5

--- server/src/test/java/org/elasticsearch/index/query/QueryShardContextTests.java
+++ server/src/test/java/org/elasticsearch/index/query/QueryShardContextTests.java
@@ -273,9 +273,7 @@ public class QueryShardContextTests extends ESTestCase {
                 });
                 return ft;
             }));
-        String expectedMessage = "Field requires resolving too many dependent fields: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> " +
-            "11 -> 12 -> 13 -> 14 -> 15 -> 16 -> 17 -> 18 -> 19 -> 20 -> 21 -> 22 -> 23 -> 24 -> 25 -> 26 -> 27 -> 28 -> " +
-            "29 -> 30 -> 31 -> 32 -> 33";
+        String expectedMessage = "Field requires resolving too many dependent fields: 1 -> 2 -> 3 -> 4 -> 5 -> 6";
         MappedFieldType fieldType = queryShardContext.fieldMapper("1");
         {
             IllegalArgumentException iae = expectThrows(IllegalArgumentException.class, () -> queryShardContext.getForField(fieldType));

e60c74240a1e9e48e125fc8260dd5458587bee62
Ryan Ernst <ryan@elastic.co>
2020-08-26 17:16:28 -0700

    Add base precommit task to all java projects (#61439)
    This commit adds java compilation to the base precommit task, and adds
    that to the java plugin. This further reduces dependence on the build
    plugin.

--- buildSrc/src/main/java/org/elasticsearch/gradle/ElasticsearchJavaPlugin.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/ElasticsearchJavaPlugin.java
@@ -23,6 +23,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar;
 import nebula.plugin.info.InfoBrokerPlugin;
 import org.elasticsearch.gradle.info.BuildParams;
 import org.elasticsearch.gradle.info.GlobalBuildInfoPlugin;
+import org.elasticsearch.gradle.precommit.PrecommitTaskPlugin;
 import org.elasticsearch.gradle.util.Util;
 import org.gradle.api.Action;
 import org.gradle.api.JavaVersion;
@@ -67,6 +68,7 @@ public class ElasticsearchJavaPlugin implements Plugin<Project> {
         project.getPluginManager().apply(RepositoriesSetupPlugin.class);
         project.getPluginManager().apply(JavaLibraryPlugin.class);
         project.getPluginManager().apply(ElasticsearchTestBasePlugin.class);
+        project.getPluginManager().apply(PrecommitTaskPlugin.class);
 
         configureConfigurations(project);
         configureCompile(project);

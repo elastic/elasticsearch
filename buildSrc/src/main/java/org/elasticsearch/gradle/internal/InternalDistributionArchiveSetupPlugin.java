7d3390ec1f7b1b85d62ab784373d71fc8890f03a
Rene Groeschke <rene@elastic.co>
2020-09-02 18:27:41 +0200

    Setup local distributions faster
    - avoid packaging/unpackaging cycle when relying on locally build distributions
    - Move more logic into distribution/archives

--- buildSrc/src/main/java/org/elasticsearch/gradle/internal/InternalDistributionArchiveSetupPlugin.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/internal/InternalDistributionArchiveSetupPlugin.java
@@ -23,34 +23,72 @@ import org.elasticsearch.gradle.EmptyDirTask;
 import org.elasticsearch.gradle.tar.SymbolicLinkPreservingTar;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.ConfigurationPublications;
+import org.gradle.api.artifacts.type.ArtifactTypeDefinition;
+import org.gradle.api.internal.artifacts.ArtifactAttributes;
+import org.gradle.api.internal.artifacts.ConfigurationVariantInternal;
+import org.gradle.api.internal.artifacts.publish.AbstractPublishArtifact;
 import org.gradle.api.plugins.BasePlugin;
+import org.gradle.api.tasks.AbstractCopyTask;
 import org.gradle.api.tasks.bundling.AbstractArchiveTask;
 import org.gradle.api.tasks.bundling.Compression;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.Date;
+
+import static org.gradle.util.GUtil.toCamelCase;
 
 /**
  * Common configurations for elasticsearch distribution archives
- * */
+ */
 public class InternalDistributionArchiveSetupPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
         project.getPlugins().apply(BasePlugin.class);
         registerEmptyDirectoryTasks(project);
-        configureArchiveDefaults(project);
+        registerSubProjectArtifacts(project);
+        configureGeneralDefaults(project);
         configureTarDefaults(project);
     }
 
-    private void configureArchiveDefaults(Project project) {
+    private void registerSubProjectArtifacts(Project project) {
+        // TODO look into avoiding afterEvaluate
+        project.afterEvaluate(project1 -> project1.subprojects(sub -> {
+            String buildArchiveTaskName = "build" + toCamelCase(sub.getName());
+            sub.getArtifacts().add("default", project1.getTasks().named(buildArchiveTaskName));
+            String copyDistributionTaskName = buildArchiveTaskName.substring(0, buildArchiveTaskName.length() - 3);
+
+            // TODO do this in a task avoidance api friendly way
+            Task copyTask = project1.getTasks().findByName(copyDistributionTaskName);
+            // TODO should not be required once completetly configured for each archive
+            if (copyTask != null) {
+                Configuration defaultConfiguration = sub.getConfigurations().getByName("default");
+                ConfigurationPublications publications = defaultConfiguration.getOutgoing();
+                ConfigurationVariantInternal variant = (ConfigurationVariantInternal) publications.getVariants().maybeCreate("directory");
+                variant.getAttributes().attribute(ArtifactAttributes.ARTIFACT_FORMAT, ArtifactTypeDefinition.DIRECTORY_TYPE);
+                variant.artifactsProvider(() -> Collections.singletonList(new DirectoryPublishArtifact(copyTask)));
+            }
+        }));
+    }
+
+    private void configureGeneralDefaults(Project project) {
+        // common config across all copy / archive tasks
+        project.getTasks().withType(AbstractCopyTask.class).configureEach(t -> {
+            t.dependsOn(project.getTasks().withType(EmptyDirTask.class));
+            t.setIncludeEmptyDirs(true);
+            t.setDirMode(0755);
+            t.setFileMode(0644);
+        });
+
         // common config across all archives
         project.getTasks().withType(AbstractArchiveTask.class).configureEach(t -> {
-            t.dependsOn(project.getTasks().withType(EmptyDirTask.class));
             String subdir = buildTaskToSubprojectName(t);
             t.getDestinationDirectory().set(project.file(subdir + "/build/distributions"));
             t.getArchiveBaseName().set(subdir.contains("oss") ? "elasticsearch-oss" : "elasticsearch");
-            t.setDirMode(0755);
-            t.setFileMode(0644);
         });
     }
 
@@ -90,5 +128,49 @@ public class InternalDistributionArchiveSetupPlugin implements Plugin<Project> {
 
     private String buildTaskToSubprojectName(AbstractArchiveTask t) {
         return t.getName().substring("build".length()).replaceAll("[A-Z]", "-$0").toLowerCase().substring(1);
+    }
+
+    private static class DirectoryPublishArtifact extends AbstractPublishArtifact {
+
+        private final Task providerTask;
+
+        DirectoryPublishArtifact(Task providerTask) {
+            this.providerTask = providerTask;
+        }
+
+        @Override
+        public String getName() {
+            return providerTask.getName();
+        }
+
+        @Override
+        public String getExtension() {
+            return "";
+        }
+
+        @Override
+        public String getType() {
+            return ArtifactTypeDefinition.DIRECTORY_TYPE;
+        }
+
+        @Override
+        public String getClassifier() {
+            return null;
+        }
+
+        @Override
+        public File getFile() {
+            return providerTask.getOutputs().getFiles().getSingleFile();
+        }
+
+        @Override
+        public Date getDate() {
+            return null;
+        }
+
+        @Override
+        public boolean shouldBePublished() {
+            return false;
+        }
     }
 }

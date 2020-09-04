8d9334faaa095c3bd3e2caeb2a91e3ed8770b0f9
Ryan Ernst <ryan@iernst.net>
2020-09-04 12:32:21 -0700

    checkstyle

--- buildSrc/src/main/java/org/elasticsearch/gradle/test/rest/RestTestUtil.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/test/rest/RestTestUtil.java
@@ -21,12 +21,10 @@ package org.elasticsearch.gradle.test.rest;
 
 import org.elasticsearch.gradle.VersionProperties;
 import org.elasticsearch.gradle.info.BuildParams;
-import org.elasticsearch.gradle.plugin.PluginPropertiesExtension;
 import org.elasticsearch.gradle.test.RestIntegTestTask;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.tasks.SourceSet;
-import org.gradle.api.tasks.TaskProvider;
 import org.gradle.api.tasks.bundling.Zip;
 
 /**

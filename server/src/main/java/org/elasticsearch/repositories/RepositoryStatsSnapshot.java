1b798288e3947f280e6acb8d352fa4c8cf2974c1
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2020-09-04 11:18:19 +0200

    minor corrections

--- server/src/main/java/org/elasticsearch/repositories/RepositoryStatsSnapshot.java
+++ server/src/main/java/org/elasticsearch/repositories/RepositoryStatsSnapshot.java
@@ -23,13 +23,13 @@ import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
-import org.elasticsearch.common.xcontent.ToXContent;
+import org.elasticsearch.common.xcontent.ToXContentObject;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 
 import java.io.IOException;
 import java.util.Objects;
 
-public final class RepositoryStatsSnapshot implements Writeable, ToXContent {
+public final class RepositoryStatsSnapshot implements Writeable, ToXContentObject {
     public static final long UNKNOWN_CLUSTER_VERSION = -1;
     private final RepositoryInfo repositoryInfo;
     private final RepositoryStats repositoryStats;

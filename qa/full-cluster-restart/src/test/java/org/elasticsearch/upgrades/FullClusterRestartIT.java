555910c7e6fcb27c7c130fe465ca7425234b3bc6
Gordon Brown <gordon.brown@elastic.co>
2020-09-08 14:15:06 -0600

    Remove now unnecessary allow_system_index_access from upgrade test

--- qa/full-cluster-restart/src/test/java/org/elasticsearch/upgrades/FullClusterRestartIT.java
+++ qa/full-cluster-restart/src/test/java/org/elasticsearch/upgrades/FullClusterRestartIT.java
@@ -1475,7 +1475,6 @@ public class FullClusterRestartIT extends AbstractFullClusterRestartTestCase {
                 if (tasksCreatedVersion.before(SYSTEM_INDEX_ENFORCEMENT_VERSION)) {
                     // Verify that the alias survived the upgrade
                     Request getAliasRequest = new Request("GET", "/_alias/test-system-alias");
-                    getAliasRequest.addParameter("allow_system_index_access", "true");
                     Map<String, Object> aliasResponse = entityAsMap(client().performRequest(getAliasRequest));
                     assertThat(aliasResponse, hasKey(".tasks"));
                     assertThat(aliasResponse, hasKey("test_index_reindex"));

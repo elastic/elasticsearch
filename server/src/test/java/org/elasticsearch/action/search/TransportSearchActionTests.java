8d3b239408f6b9beedbd6d65ac179816a6bcb851
Nhat Nguyen <nhat.nguyen@elastic.co>
2020-09-08 11:51:13 -0400

    Fix NPE in testShouldMinimizeRoundtrips
    Relates #62080

--- server/src/test/java/org/elasticsearch/action/search/TransportSearchActionTests.java
+++ server/src/test/java/org/elasticsearch/action/search/TransportSearchActionTests.java
@@ -829,7 +829,9 @@ public class TransportSearchActionTests extends ESTestCase {
             searchRequestTests.setUp();
             SearchRequest searchRequest = searchRequestTests.createSearchRequest();
             searchRequest.scroll((Scroll)null);
-            searchRequest.source().pointInTimeBuilder(null);
+            if (searchRequest.source() != null) {
+                searchRequest.source().pointInTimeBuilder(null);
+            }
             searchRequest.searchType(SearchType.QUERY_THEN_FETCH);
             SearchSourceBuilder source = searchRequest.source();
             if (source != null) {

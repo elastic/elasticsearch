118cd0e6e22816140d418a4555259bffba206435
Nhat Nguyen <nhat.nguyen@elastic.co>
2020-09-09 12:11:14 -0400

    Revert "Fix testInvalidScrollKeepAlive"
    This reverts commit 7af2fec66695b7407f82fe57ca58c8a96fcd077f.

--- server/src/internalClusterTest/java/org/elasticsearch/search/scroll/SearchScrollIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/search/scroll/SearchScrollIT.java
@@ -610,19 +610,16 @@ public class SearchScrollIT extends ESIntegTestCase {
             .setScroll(TimeValue.timeValueMinutes(5))
             .get();
         assertNotNull(searchResponse.getScrollId());
-        try {
-            assertThat(searchResponse.getHits().getTotalHits().value, equalTo(2L));
-            assertThat(searchResponse.getHits().getHits().length, equalTo(1));
+        assertThat(searchResponse.getHits().getTotalHits().value, equalTo(2L));
+        assertThat(searchResponse.getHits().getHits().length, equalTo(1));
 
-            exc = expectThrows(Exception.class,
-                () -> client().prepareSearchScroll(searchResponse.getScrollId()).setScroll(TimeValue.timeValueHours(3)).get());
-            illegalArgumentException =
-                (IllegalArgumentException) ExceptionsHelper.unwrap(exc, IllegalArgumentException.class);
-            assertNotNull(illegalArgumentException);
-            assertThat(illegalArgumentException.getMessage(), containsString("Keep alive for request (3h) is too large"));
-        } finally {
-            client().prepareClearScroll().addScrollId(searchResponse.getScrollId()).get();
-        }
+        exc = expectThrows(Exception.class,
+            () -> client().prepareSearchScroll(searchResponse.getScrollId())
+                    .setScroll(TimeValue.timeValueHours(3)).get());
+        illegalArgumentException =
+            (IllegalArgumentException) ExceptionsHelper.unwrap(exc, IllegalArgumentException.class);
+        assertNotNull(illegalArgumentException);
+        assertThat(illegalArgumentException.getMessage(), containsString("Keep alive for request (3h) is too large"));
     }
 
     /**

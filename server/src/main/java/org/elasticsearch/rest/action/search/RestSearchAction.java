58b498e2a29a92874bb2e801fdeac0dc3a799b8c
Nhat Nguyen <nhat.nguyen@elastic.co>
2020-09-09 20:55:44 -0400

    Support point in time cross cluster search (#61827)
    This commit integrates point in time into cross cluster search.
    
    Relates #61062
    Closes #61790

--- server/src/main/java/org/elasticsearch/rest/action/search/RestSearchAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/search/RestSearchAction.java
@@ -186,12 +186,14 @@ public class RestSearchAction extends BaseRestHandler {
         searchRequest.routing(request.param("routing"));
         searchRequest.preference(request.param("preference"));
         searchRequest.indicesOptions(IndicesOptions.fromRequest(request, searchRequest.indicesOptions()));
-        searchRequest.setCcsMinimizeRoundtrips(request.paramAsBoolean("ccs_minimize_roundtrips", searchRequest.isCcsMinimizeRoundtrips()));
 
         checkRestTotalHits(request, searchRequest);
 
         if (searchRequest.pointInTimeBuilder() != null) {
-            preparePointInTime(searchRequest, namedWriteableRegistry);
+            preparePointInTime(searchRequest, request, namedWriteableRegistry);
+        } else {
+            searchRequest.setCcsMinimizeRoundtrips(
+                request.paramAsBoolean("ccs_minimize_roundtrips", searchRequest.isCcsMinimizeRoundtrips()));
         }
     }
 
@@ -308,7 +310,7 @@ public class RestSearchAction extends BaseRestHandler {
         }
     }
 
-    static void preparePointInTime(SearchRequest request, NamedWriteableRegistry namedWriteableRegistry) {
+    static void preparePointInTime(SearchRequest request, RestRequest restRequest, NamedWriteableRegistry namedWriteableRegistry) {
         assert request.pointInTimeBuilder() != null;
         ActionRequestValidationException validationException = null;
         if (request.indices().length > 0) {
@@ -322,6 +324,11 @@ public class RestSearchAction extends BaseRestHandler {
         }
         if (request.preference() != null) {
             validationException = addValidationError("[preference] cannot be used with point in time", validationException);
+        }
+        if (restRequest.paramAsBoolean("ccs_minimize_roundtrips", false)) {
+            validationException =
+                addValidationError("[ccs_minimize_roundtrips] cannot be used with point in time", validationException);
+            request.setCcsMinimizeRoundtrips(false);
         }
         ExceptionsHelper.reThrowIfNotNull(validationException);
 

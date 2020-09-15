03d2395183e0b4f0d9022ed683f466ac7f9afde7
Costin Leau <costin@users.noreply.github.com>
2020-09-15 15:22:41 +0300

    EQL: Use Point In Time inside sequences (#62276)
    Use the newly introduced PIT API to have a consistent view of the data
    while doing sequence matching, which involves multiple calls, aka
    repeatable reads and thus avoid race conditions or any in-flight updates
    on the data.
    
    (cherry picked from commit daa72fc3c71fd36afb55278021ff6bbc591ef148)

--- x-pack/plugin/eql/src/main/java/org/elasticsearch/xpack/eql/execution/sequence/TumblingWindow.java
+++ x-pack/plugin/eql/src/main/java/org/elasticsearch/xpack/eql/execution/sequence/TumblingWindow.java
@@ -23,6 +23,7 @@ import org.elasticsearch.xpack.eql.session.EmptyPayload;
 import org.elasticsearch.xpack.eql.session.Payload;
 import org.elasticsearch.xpack.eql.session.Payload.Type;
 import org.elasticsearch.xpack.eql.util.ReversedIterator;
+import org.elasticsearch.xpack.ql.util.ActionListeners;
 
 import java.util.Iterator;
 import java.util.List;
@@ -296,14 +297,20 @@ public class TumblingWindow implements Executable {
 
         if (completed.isEmpty()) {
             listener.onResponse(new EmptyPayload(Type.SEQUENCE, timeTook()));
-            matcher.clear();
+            close(listener);
             return;
         }
 
-        client.get(hits(completed), wrap(hits -> {
-            listener.onResponse(new SequencePayload(completed, hits, false, timeTook()));
+        client.get(hits(completed), ActionListeners.map(listener, hits -> {
+            SequencePayload payload = new SequencePayload(completed, hits, false, timeTook());
+            close(listener);
+            return payload;
+        }));
+    }
+
+    private void close(ActionListener<Payload> listener) {
             matcher.clear();
-        }, listener::onFailure));
+        client.close(ActionListener.delegateFailure(listener, (l, r) -> {}));
     }
 
     private TimeValue timeTook() {

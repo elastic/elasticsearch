2fc92dd6d52fcfc530ba137b7c8afdc2d0769379
Martijn van Groningen <martijn.v.groningen@gmail.com>
2020-09-17 11:18:33 +0200

    Prohibit the usage of create index api in namespaces managed by data stream templates.
    This commit adds validation that prohibits the creation of regular indices
    in the namespace of templates with data streams enabled.
    
    It shouldn't be possible to create ordinary indices when the name of the index
    matches with a composable index template that enables data streams. Auto creation
    has logic that creates data streams instead of regular indices. However validation
    logic for the create index api was missing.

--- server/src/main/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexService.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexService.java
@@ -502,6 +502,12 @@ public class MetadataCreateIndexService {
                                                                                     throws Exception {
         logger.debug("applying create index request using composable template [{}]", templateName);
 
+        ComposableIndexTemplate template = currentState.getMetadata().templatesV2().get(templateName);
+        if (request.dataStreamName() == null && template.getDataStreamTemplate() != null) {
+           throw new IllegalArgumentException("cannot create index with name [" + request.index() +
+               "], because it matches with template [" + templateName + "] that creates data streams only");
+        }
+
         final List<Map<String, Object>> mappings =
             collectV2Mappings(request.mappings(), currentState, templateName, xContentRegistry, request.index());
         final Settings aggregatedIndexSettings =

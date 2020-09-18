6849550894da2bc41fcd9300fc459ce2f985de4f
Christoph Büscher <cbuescher@posteo.de>
2020-09-18 16:59:24 +0200

    Removing specialized script doc values functions

--- x-pack/plugin/versionfield/src/test/java/org/elasticsearch/xpack/versionfield/VersionStringFieldMapperTests.java
+++ x-pack/plugin/versionfield/src/test/java/org/elasticsearch/xpack/versionfield/VersionStringFieldMapperTests.java
@@ -124,7 +124,7 @@ public class VersionStringFieldMapperTests extends MapperTestCase {
         );
     }
 
-    public void testFetchSourceValue() {
+    public void testFetchSourceValue() throws IOException {
         Settings settings = Settings.builder().put(IndexMetadata.SETTING_VERSION_CREATED, Version.CURRENT.id).build();
         Mapper.BuilderContext context = new Mapper.BuilderContext(settings, new ContentPath());
 

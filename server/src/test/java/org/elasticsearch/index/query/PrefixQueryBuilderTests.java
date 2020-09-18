38b1bd84d9d79385ce7f1316538cfb421acc8488
markharwood <markharwood@gmail.com>
2020-09-18 20:46:27 +0100

    Version bumps

--- server/src/test/java/org/elasticsearch/index/query/PrefixQueryBuilderTests.java
+++ server/src/test/java/org/elasticsearch/index/query/PrefixQueryBuilderTests.java
@@ -44,13 +44,9 @@ public class PrefixQueryBuilderTests extends AbstractQueryTestCase<PrefixQueryBu
         if (randomBoolean()) {
             query.rewrite(getRandomRewriteMethod());
         }
-        //TODO code below is commented out while we do the Version dance for PR 61596. Steps are
-        // 1) Commit PR 61596 with this code commented out in master
-        // 2) Backport PR 61596 to 7.x, uncommented
-        // 3) New PR on master to uncomment this code now that 7.x has support for case insensitive flag.
-//        if (randomBoolean()) {
-//            query.caseInsensitive(true);
-//        }         
+        if (randomBoolean()) {
+            query.caseInsensitive(true);
+        }         
         return query;
     }
 
@@ -108,13 +104,8 @@ public class PrefixQueryBuilderTests extends AbstractQueryTestCase<PrefixQueryBu
 
     public void testFromJson() throws IOException {
         String json =
-                "{    \"prefix\" : { \"user\" :  { \"value\" : \"ki\", \"boost\" : 2.0 "
-            //TODO code below is commented out while we do the Version dance for PR 61596. Steps are
-            // 1) Commit PR 61596 with this code commented out in master
-            // 2) Backport PR 61596 to 7.x, uncommented
-            // 3) New PR on master to uncomment this code now that 7.x has support for case insensitive flag.                
-//            "      \"case_insensitive\" : true\n" +
-            
+                "{    \"prefix\" : { \"user\" :  { \"value\" : \"ki\", \"boost\" : 2.0, " 
+                + " \"case_insensitive\" : true\n"            
                 + "} }}";
 
         PrefixQueryBuilder parsed = (PrefixQueryBuilder) parseQuery(json);

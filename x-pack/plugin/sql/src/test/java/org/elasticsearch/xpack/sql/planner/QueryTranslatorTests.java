904c31a4f91edf89b6684ffef1b091e32ac12d5c
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-09-18 18:03:52 -0300

    Reordering tests cases

--- x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/planner/QueryTranslatorTests.java
+++ x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/planner/QueryTranslatorTests.java
@@ -1346,6 +1346,18 @@ public class QueryTranslatorTests extends ESTestCase {
             endsWith("\"date_histogram\":{\"field\":\"date\",\"missing_bucket\":true,\"order\":\"asc\","
                     + "\"calendar_interval\":\"1M\",\"time_zone\":\"Z\"}}}]}}}"));
     }
+
+    public void testGroupByMoreMonthsHistogramQueryTranslator() {
+        PhysicalPlan p = optimizeAndPlan("SELECT HISTOGRAM(date, INTERVAL 5 MONTH) AS h FROM test GROUP BY h");
+        assertEquals(EsQueryExec.class, p.getClass());
+        EsQueryExec eqe = (EsQueryExec) p;
+        assertEquals(1, eqe.output().size());
+        assertEquals("h", eqe.output().get(0).qualifiedName());
+        assertEquals(DATETIME, eqe.output().get(0).dataType());
+        assertThat(eqe.queryContainer().aggs().asAggBuilder().toString().replaceAll("\\s+", ""),
+            endsWith("\"date_histogram\":{\"field\":\"date\",\"missing_bucket\":true,\"order\":\"asc\","
+                    + "\"fixed_interval\":\"12960000000ms\",\"time_zone\":\"Z\"}}}]}}}"));
+    }
     
     public void testGroupByOneWeekHistogramQueryTranslator() {
         PhysicalPlan p = optimizeAndPlan("SELECT HISTOGRAM(date, INTERVAL 1 WEEK) AS h FROM test GROUP BY h");
@@ -1371,18 +1383,6 @@ public class QueryTranslatorTests extends ESTestCase {
                     + "\"calendar_interval\":\"1814400000ms\",\"time_zone\":\"Z\"}}}]}}}"));
     }
     
-    public void testGroupByMoreMonthsHistogramQueryTranslator() {
-        PhysicalPlan p = optimizeAndPlan("SELECT HISTOGRAM(date, INTERVAL 5 MONTH) AS h FROM test GROUP BY h");
-        assertEquals(EsQueryExec.class, p.getClass());
-        EsQueryExec eqe = (EsQueryExec) p;
-        assertEquals(1, eqe.output().size());
-        assertEquals("h", eqe.output().get(0).qualifiedName());
-        assertEquals(DATETIME, eqe.output().get(0).dataType());
-        assertThat(eqe.queryContainer().aggs().asAggBuilder().toString().replaceAll("\\s+", ""),
-            endsWith("\"date_histogram\":{\"field\":\"date\",\"missing_bucket\":true,\"order\":\"asc\","
-                    + "\"fixed_interval\":\"12960000000ms\",\"time_zone\":\"Z\"}}}]}}}"));
-    }
-
     public void testGroupByOneDayHistogramQueryTranslator() {
         PhysicalPlan p = optimizeAndPlan("SELECT HISTOGRAM(date, INTERVAL 1 DAY) AS h FROM test GROUP BY h");
         assertEquals(EsQueryExec.class, p.getClass());

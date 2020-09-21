876be3303e61ad8ce1366d23a10132e24812ca1e
Rene Groeschke <rene@elastic.co>
2020-09-21 11:04:35 +0200

    Port more bwc setup into binary plugin

--- buildSrc/src/main/java/org/elasticsearch/gradle/internal/InternalBwcGitPlugin.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/internal/InternalBwcGitPlugin.java
@@ -20,31 +20,42 @@
 package org.elasticsearch.gradle.internal;
 
 import org.elasticsearch.gradle.LoggedExec;
+import org.elasticsearch.gradle.info.GlobalBuildInfoPlugin;
+import org.gradle.api.Action;
 import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.logging.Logger;
 import org.gradle.api.plugins.ExtraPropertiesExtension;
 import org.gradle.api.provider.Provider;
+import org.gradle.api.provider.ProviderFactory;
 import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.tasks.TaskProvider;
+import org.gradle.process.ExecResult;
+import org.gradle.process.ExecSpec;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import static java.util.Arrays.asList;
 
 public class InternalBwcGitPlugin implements Plugin<Project> {
 
-    BwcGitExtension gitExtension;
+    private BwcGitExtension gitExtension;
+    private Project project;
 
     @Override
     public void apply(Project project) {
         gitExtension = new BwcGitExtension(project.getObjects());
+        this.project = project;
         project.getExtensions().add("bwcGitConfig", new BwcGitExtension(project.getObjects()));
+        ProviderFactory providers = project.getProviders();
         Provider<String> remote = project.getProviders().systemProperty("bwc.remote").forUseAtConfigurationTime().orElse("elastic");
 
         RegularFileProperty checkoutDir = gitExtension.checkoutDir;
-
         TaskContainer tasks = project.getTasks();
         TaskProvider<LoggedExec> createCloneTaskProvider = tasks.register("createClone", LoggedExec.class, createClone -> {
             createClone.onlyIf(task -> gitExtension.checkoutDir.get().getAsFile().exists() == false);
@@ -73,7 +84,7 @@ public class InternalBwcGitPlugin implements Plugin<Project> {
             addRemote.setCommandLine(asList("git", "remote", "add", remoteRepo, "https://github.com/" + remoteRepo + "/elasticsearch.git"));
         });
 
-        tasks.register("fetchLatest", LoggedExec.class, fetchLatest -> {
+        TaskProvider<LoggedExec> fetchLatestTaskProvider = tasks.register("fetchLatest", LoggedExec.class, fetchLatest -> {
             var gitFetchLatest = project.getProviders()
                 .systemProperty("tests.bwc.git_fetch_latest")
                 .forUseAtConfigurationTime()
@@ -92,13 +103,98 @@ public class InternalBwcGitPlugin implements Plugin<Project> {
             fetchLatest.setWorkingDir(checkoutDir.get());
             fetchLatest.setCommandLine(asList("git", "fetch", "--all"));
         });
-    }
 
-    private static boolean isRemoteAvailable(Provider<String> remote, ByteArrayOutputStream output) {
-        return new String(output.toByteArray()).lines().anyMatch(l -> l.contains(remote.get() + "\t"));
+        tasks.register("checkoutBwcBranch", checkoutBwcBranch -> {
+            checkoutBwcBranch.dependsOn(fetchLatestTaskProvider);
+            checkoutBwcBranch.doLast(t -> {
+                Logger logger = project.getLogger();
+
+                String bwcBranch = gitExtension.bwcBranch.get();
+                final String refspec = providers.systemProperty("bwc.refspec." + bwcBranch)
+                    .orElse(providers.systemProperty("tests.bwc.refspec." + bwcBranch))
+                    .getOrElse(remote.get() + "/" + bwcBranch);
+
+                String effectiveRefSpec = maybeAlignedRefSpec(project, logger, refspec);
+
+                logger.lifecycle("Performing checkout of {}...", refspec);
+                LoggedExec.exec(project, spec -> {
+                    spec.workingDir(checkoutDir);
+                    spec.commandLine("git", "checkout", effectiveRefSpec);
+                });
+
+                String checkoutHash = GlobalBuildInfoPlugin.gitInfo(checkoutDir.get().getAsFile()).getRevision();
+                logger.lifecycle("Checkout hash for {} is {}", project.getPath(), checkoutHash);
+                writeFile(new File(project.getBuildDir(), refspec), checkoutHash);
+            });
+        });
     }
 
     public BwcGitExtension getGitExtension() {
         return gitExtension;
+    }
+
+    /**
+     * We use a time based approach to make the bwc versions built deterministic and compatible with the current hash.
+     * Most of the time we want to test against latest, but when running delayed exhaustive tests or wanting
+     * reproducible builds we want this to be deterministic by using a hash that was the latest when the current
+     * commit was made.
+     * <p>
+     * This approach doesn't work with merge commits as these can introduce commits in the chronological order
+     * after the fact e.x. a merge done today can add commits dated with yesterday so the result will no longer be
+     * deterministic.
+     * <p>
+     * We don't use merge commits, but for additional safety we check that no such commits exist in the time period
+     * we are interested in.
+     * <p>
+     * Timestamps are at seconds resolution. rev-parse --before and --after are inclusive w.r.t the second
+     * passed as input. This means the results might not be deterministic in the current second, but this
+     * should not matter in practice.
+     */
+    private String maybeAlignedRefSpec(Project project, Logger logger, String defaultRefSpec) {
+        if (project.getProviders().systemProperty("bwc.checkout.align").isPresent() == false) {
+            return defaultRefSpec;
+        }
+
+        String timeOfCurrent = execGit(execSpec -> {
+            execSpec.commandLine(asList("git", "show", "--no-patch", "--no-notes", "--pretty='%cD'"));
+            execSpec.workingDir(project.getRootDir());
+        });
+
+        logger.lifecycle("Commit date of current: {}", timeOfCurrent);
+
+        String mergeCommits = execGit(
+            spec -> spec.commandLine(asList("git", "rev-list", defaultRefSpec, "--after", timeOfCurrent, "--merges"))
+        );
+        if (mergeCommits.isEmpty() == false) {
+            throw new IllegalStateException("Found the following merge commits which prevent determining bwc commits: " + mergeCommits);
+        }
+        return execGit(
+            spec -> spec.commandLine(asList("git", "rev-list", defaultRefSpec, "-n", "1", "--before", timeOfCurrent, "--date-order"))
+        );
+    }
+
+    private void writeFile(File file, String content) {
+        try {
+            FileWriter myWriter = new FileWriter(file, false);
+            myWriter.write(content);
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String execGit(Action<ExecSpec> execSpecConfig) {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ExecResult exec = project.exec(execSpec -> {
+            execSpec.setStandardOutput(os);
+            execSpec.workingDir(gitExtension.checkoutDir.getAsFile());
+            execSpecConfig.execute(execSpec);
+        });
+        exec.assertNormalExitValue();
+        return os.toString().trim();
+    }
+
+    private static boolean isRemoteAvailable(Provider<String> remote, ByteArrayOutputStream output) {
+        return new String(output.toByteArray()).lines().anyMatch(l -> l.contains(remote.get() + "\t"));
     }
 }
--- buildSrc/src/main/java/org/elasticsearch/gradle/internal/InternalDistributionBwcSetupPlugin.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/internal/InternalDistributionBwcSetupPlugin.java
@@ -19,15 +19,38 @@
 
 package org.elasticsearch.gradle.internal;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.tools.ant.taskdefs.condition.Os;
 import org.elasticsearch.gradle.BwcVersions;
+import org.elasticsearch.gradle.LoggedExec;
+import org.elasticsearch.gradle.Version;
 import org.elasticsearch.gradle.info.BuildParams;
 import org.elasticsearch.gradle.info.GlobalBuildInfoPlugin;
+import org.gradle.api.Action;
+import org.gradle.api.GradleException;
+import org.gradle.api.InvalidUserDataException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.logging.LogLevel;
 import org.gradle.api.plugins.ExtraPropertiesExtension;
 import org.gradle.api.provider.Provider;
+import org.gradle.api.tasks.TaskProvider;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.elasticsearch.gradle.util.JavaUtil.getJavaHome;
 
 public class InternalDistributionBwcSetupPlugin implements Plugin<Project> {
 
@@ -47,8 +70,8 @@ public class InternalDistributionBwcSetupPlugin implements Plugin<Project> {
     private void configureProject(Project bwcProject, BwcVersions.UnreleasedVersionInfo unreleasedVersion, Provider<String> remote) {
 
         BwcGitExtension gitExtension = bwcProject.getPlugins().apply(InternalBwcGitPlugin.class).getGitExtension();
-
         String bwcBranch = unreleasedVersion.branch;
+        Version bwcVersion = unreleasedVersion.version;
         File checkoutDir = new File(bwcProject.getBuildDir(), "bwc/checkout-" + bwcBranch);
 
         // using extra properties temporally while porting build script code over to this plugin
@@ -65,5 +88,286 @@ public class InternalDistributionBwcSetupPlugin implements Plugin<Project> {
         bwcProject.getPlugins().apply("distribution");
         // Not published so no need to assemble
         bwcProject.getTasks().named("assemble").configure(t -> t.setEnabled(false));
+
+        TaskProvider<Task> buildBwcTaskProvider = bwcProject.getTasks().register("buildBwc");
+        Map<String, File> artifactFiles = new HashMap<>();
+        List<String> projectDirs = new ArrayList<>();
+        List<String> projects = new ArrayList<>();
+        projects.addAll(Arrays.asList("deb", "rpm"));
+        if (bwcVersion.onOrAfter("7.0.0")) {
+            projects.addAll(Arrays.asList("windows-zip", "darwin-tar", "linux-tar"));
+        } else {
+            projects.add("zip");
+        }
+
+        for (String projectName : projects) {
+            String baseDir = "distribution";
+            String classifier = "";
+            String extension = projectName;
+            if (bwcVersion.onOrAfter("7.0.0") && (projectName.contains("zip") || projectName.contains("tar"))) {
+                int index = projectName.indexOf('-');
+                classifier = "-${projectName.substring(0, index)}-x86_64";
+                extension = projectName.substring(index + 1);
+                if (extension.equals("tar")) {
+                    extension += ".gz";
+                }
+            }
+            if (bwcVersion.onOrAfter("7.0.0") && projectName.contains("deb")) {
+                classifier = "-amd64";
+            }
+            if (bwcVersion.onOrAfter("7.0.0") && projectName.contains("rpm")) {
+                classifier = "-x86_64";
+            }
+            if (bwcVersion.onOrAfter("6.3.0")) {
+                baseDir += projectName.endsWith("zip") || projectName.endsWith("tar") ? "/archives" : "/packages";
+                // add oss variant first
+                projectDirs.add(baseDir + "/oss-" + projectName);
+                String relativeOssPath = baseDir
+                    + "/oss-"
+                    + projectName
+                    + "/build/distributions/elasticsearch-oss-"
+                    + bwcVersion
+                    + "-SNAPSHOT"
+                    + classifier
+                    + "."
+                    + extension;
+
+                File ossProjectArtifact = new File(checkoutDir, relativeOssPath);
+                artifactFiles.put("oss-" + projectName, ossProjectArtifact);
+                String projectPath = baseDir + "/oss-" + projectName;
+                createBuildBwcTask(
+                    bwcProject,
+                    checkoutDir,
+                    bwcVersion,
+                    "oss-" + projectName,
+                    projectPath,
+                    ossProjectArtifact,
+                    buildBwcTaskProvider
+                );
+            }
+            projectDirs.add(baseDir + "/" + projectName);
+
+            String relativePath = baseDir
+                + "/"
+                + projectName
+                + "build/distributions/elasticsearch-"
+                + bwcVersion
+                + "-SNAPSHOT"
+                + classifier
+                + "."
+                + extension;
+            File projectArtifact = new File(checkoutDir, relativePath);
+            artifactFiles.put(projectName, projectArtifact);
+
+            createBuildBwcTask(
+                bwcProject,
+                checkoutDir,
+                bwcVersion,
+                projectName,
+                baseDir + "/" + projectName,
+                projectArtifact,
+                buildBwcTaskProvider
+            );
+        }
+
+        // Create build tasks for the JDBC driver used for compatibility testing
+        String jdbcProjectDir = "x-pack/plugin/sql/jdbc";
+
+        File jdbcProjectArtifact = new File(
+            checkoutDir,
+            jdbcProjectDir + "build/distributions/x-pack-sql-jdbc-" + bwcVersion + "-SNAPSHOT.jar"
+        );
+
+        createBuildBwcTask(bwcProject, checkoutDir, bwcVersion, "jdbc", jdbcProjectDir, jdbcProjectArtifact, buildBwcTaskProvider);
+        TaskProvider<LoggedExec> resolveAllBwcDepsTaskProvider = createRunBwcGradleTask(
+            bwcProject,
+            "resolveAllBwcDependencies",
+            checkoutDir,
+            bwcVersion,
+            t -> t.args("resolveAllDependencies")
+        );
+
+        Version currentVersion = Version.fromString(bwcProject.getVersion().toString());
+        if (currentVersion.getMinor() == 0 && currentVersion.getRevision() == 0) {
+            // We only want to resolve dependencies for live versions of master, without cascading this to older versions
+            bwcProject.getTasks().named("resolveAllDependencies").configure(t -> t.dependsOn(resolveAllBwcDepsTaskProvider));
+        }
+
+        for (Map.Entry<String, File> e : artifactFiles.entrySet()) {
+            String projectName = e.getKey();
+            String buildBwcTask = buildBwcTaskName(projectName);
+            File artifactFile = e.getValue();
+            String artifactFileName = artifactFile.getName();
+            String artifactName = artifactFileName.contains("oss") ? "elasticsearch-oss" : "elasticsearch";
+
+            String suffix = artifactFileName.endsWith("tar.gz") ? "tar.gz" : artifactFileName.substring(artifactFileName.length() - 3);
+            int archIndex = artifactFileName.indexOf("x86_64");
+
+            bwcProject.getConfigurations().create(projectName);
+            bwcProject.getArtifacts().add(projectName, artifactFile, artifact -> {
+                artifact.setName(artifactName);
+                artifact.builtBy(buildBwcTask);
+                artifact.setType(suffix);
+
+                String classifier = "";
+                if (archIndex != -1) {
+                    int osIndex = artifactFileName.lastIndexOf('-', archIndex - 2);
+                    classifier = artifactFileName.substring(osIndex + 1, archIndex - 1) + "-x86_64";
+                }
+                artifact.setClassifier(classifier);
+            });
+        }
+        // make sure no dependencies were added to assemble; we want it to be a no-op
+        bwcProject.getTasks().named("assemble").configure(t -> t.setDependsOn(Collections.emptyList()));
+    }
+
+    private TaskProvider<LoggedExec> createRunBwcGradleTask(
+        Project project,
+        String name,
+        File checkoutDir,
+        Version bwcVersion,
+        Action<LoggedExec> configAction
+    ) {
+        return project.getTasks().register(name, LoggedExec.class, loggedExec -> {
+            // TODO revisit
+            loggedExec.dependsOn("checkoutBwcBranch");
+            loggedExec.setSpoolOutput(true);
+            loggedExec.setWorkingDir(checkoutDir);
+            loggedExec.doFirst(t -> {
+                // Execution time so that the checkouts are available
+                String javaVersionsString = readFromFile(new File(checkoutDir, ".ci/java-versions.properties"));
+                loggedExec.environment(
+                    "JAVA_HOME",
+                    getJavaHome(
+                        Integer.parseInt(
+                            javaVersionsString.lines()
+                                .filter(l -> l.startsWith("ES_BUILD_JAVA="))
+                                .map(l -> l.replace("ES_BUILD_JAVA=java", "").trim())
+                                .map(l -> l.replace("ES_BUILD_JAVA=openjdk", "").trim())
+                                .collect(Collectors.joining("!!"))
+                        )
+                    )
+                );
+                loggedExec.environment(
+                    "RUNTIME_JAVA_HOME",
+                    getJavaHome(
+                        Integer.parseInt(
+                            javaVersionsString.lines()
+                                .filter(l -> l.startsWith("ES_RUNTIME_JAVA=java"))
+                                .map(l -> l.replace("ES_RUNTIME_JAVA=java", "").trim())
+                                .collect(Collectors.joining("!!"))
+                        )
+                    )
+                );
+            });
+
+            if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+                loggedExec.executable("cmd");
+                loggedExec.args("/C", "call", new File(checkoutDir, "gradlew").toString());
+            } else {
+                loggedExec.executable(new File(checkoutDir, "gradlew").toString());
+            }
+            if (project.getGradle().getStartParameter().isOffline()) {
+                loggedExec.args("--offline");
+            }
+            // TODO resolve
+            String buildCacheUrl = System.getProperty("org.elasticsearch.build.cache.url");
+            if (buildCacheUrl != null) {
+                loggedExec.args("-Dorg.elasticsearch.build.cache.url=" + buildCacheUrl);
+            }
+
+            loggedExec.args("-Dbuild.snapshot=true");
+            loggedExec.args("-Dscan.tag.NESTED");
+            final LogLevel logLevel = project.getGradle().getStartParameter().getLogLevel();
+            List<LogLevel> nonDefaultLogLevels = Arrays.asList(LogLevel.QUIET, LogLevel.WARN, LogLevel.INFO, LogLevel.DEBUG);
+            if (nonDefaultLogLevels.contains(logLevel)) {
+                loggedExec.args("--" + logLevel.name().toLowerCase(Locale.ENGLISH));
+            }
+            final String showStacktraceName = project.getGradle().getStartParameter().getShowStacktrace().name();
+            assert Arrays.asList("INTERNAL_EXCEPTIONS", "ALWAYS", "ALWAYS_FULL").contains(showStacktraceName);
+            if (showStacktraceName.equals("ALWAYS")) {
+                loggedExec.args("--stacktrace");
+            } else if (showStacktraceName.equals("ALWAYS_FULL")) {
+                loggedExec.args("--full-stacktrace");
+            }
+            if (project.getGradle().getStartParameter().isParallelProjectExecutionEnabled()) {
+                loggedExec.args("--parallel");
+            }
+            loggedExec.setStandardOutput(new IndentingOutputStream(System.out, bwcVersion));
+            loggedExec.setErrorOutput(new IndentingOutputStream(System.err, bwcVersion));
+            configAction.execute(loggedExec);
+        });
+    }
+
+    private String readFromFile(File file) {
+        try {
+            return FileUtils.readFileToString(file);
+        } catch (IOException ioException) {
+            throw new GradleException("Cannot read java properties file.", ioException);
+        }
+    }
+
+    private String buildBwcTaskName(String projectName) {
+        return "buildBwc"
+            + Arrays.stream(projectName.split("-"))
+                .map(i -> i.substring(0, 1).toUpperCase(Locale.ROOT) + i.substring(1))
+                .collect(Collectors.joining());
+    }
+
+    void createBuildBwcTask(
+        Project project,
+        File checkoutDir,
+        Version bwcVersion,
+        String projectName,
+        String projectPath,
+        File projectArtifact,
+        TaskProvider<Task> bwcTaskProvider
+    ) {
+        String bwcTaskName = buildBwcTaskName(projectName);
+
+        createRunBwcGradleTask(project, bwcTaskName, checkoutDir, bwcVersion, c -> {
+            c.getInputs().file(new File(project.getBuildDir(), "refspec"));
+            c.getOutputs().files(projectArtifact);
+            c.getOutputs().cacheIf("BWC distribution caching is disabled on 'master' branch", task -> {
+                String gitBranch = System.getenv("GIT_BRANCH");
+                return BuildParams.isCi() && (gitBranch == null || gitBranch.endsWith("master") == false);
+            });
+            c.args(projectPath.replace('/', ':'));
+            if (project.getGradle().getStartParameter().isBuildCacheEnabled()) {
+                c.args("--build-cache");
+            }
+            c.doLast(task -> {
+                if (projectArtifact.exists() == false) {
+                    throw new InvalidUserDataException("Building " + bwcVersion + " didn't generate expected file " + projectArtifact);
+                }
+            });
+        });
+        bwcTaskProvider.configure(t -> t.dependsOn(bwcTaskName));
+    }
+
+    private static class IndentingOutputStream extends OutputStream {
+
+        public final byte[] indent;
+        private final OutputStream delegate;
+
+        IndentingOutputStream(OutputStream delegate, Object version) {
+            this.delegate = delegate;
+            indent = (" [" + version + "] ").getBytes(StandardCharsets.UTF_8);
+        }
+
+        @Override
+        public void write(int b) throws IOException {
+            int[] arr = { b };
+            write(arr, 0, 1);
+        }
+
+        public void write(int[] bytes, int offset, int length) throws IOException {
+            for (int i = 0; i < bytes.length; i++) {
+                delegate.write(bytes[i]);
+                if (bytes[i] == '\n') {
+                    delegate.write(indent);
+                }
+            }
+        }
     }
 }

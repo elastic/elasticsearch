4dad3e675362026f7c850e8210c684108c6f9762
Tim Brooks <tim@uncontended.net>
2020-09-21 16:29:30 -0600

    nio and javadoc

--- plugins/transport-nio/src/main/java/org/elasticsearch/http/nio/NioHttpResponseCreator.java
+++ plugins/transport-nio/src/main/java/org/elasticsearch/http/nio/NioHttpResponseCreator.java
@@ -7,7 +7,7 @@
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-package org.elasticsearch.http.netty4;
+package org.elasticsearch.http.nio;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
@@ -28,7 +28,7 @@ import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.DefaultLastHttpContent;
 import io.netty.handler.codec.http.HttpResponse;
 import org.elasticsearch.common.Booleans;
-import org.elasticsearch.transport.NettyAllocator;
+import org.elasticsearch.monitor.jvm.JvmInfo;
 
 import java.util.List;
 
@@ -36,7 +36,7 @@ import java.util.List;
  * Split up large responses to prevent batch compression or other CPU intensive operations down the pipeline.
  */
 @ChannelHandler.Sharable
-class Netty4HttpResponseCreator extends MessageToMessageEncoder<Netty4HttpResponse> {
+public class NioHttpResponseCreator extends MessageToMessageEncoder<NioHttpResponse> {
 
     private static final String DO_NOT_SPLIT = "es.unsafe.do_not_split_http_responses";
 
@@ -46,11 +46,11 @@ class Netty4HttpResponseCreator extends MessageToMessageEncoder<Netty4HttpRespon
     static {
         DO_NOT_SPLIT_HTTP_RESPONSES = Booleans.parseBoolean(System.getProperty(DO_NOT_SPLIT), false);
         // Netty will add some header bytes if it compresses this message. So we downsize slightly.
-        SPLIT_THRESHOLD = (int) (NettyAllocator.suggestedMaxAllocationSize() * 0.99);
+        SPLIT_THRESHOLD = (int) (suggestedMaxAllocationSize() * 0.99);
     }
 
     @Override
-    protected void encode(ChannelHandlerContext ctx, Netty4HttpResponse msg, List<Object> out) {
+    protected void encode(ChannelHandlerContext ctx, NioHttpResponse msg, List<Object> out) {
         if (DO_NOT_SPLIT_HTTP_RESPONSES || msg.content().readableBytes() <= SPLIT_THRESHOLD) {
             out.add(msg.retain());
         } else {
@@ -62,5 +62,9 @@ class Netty4HttpResponseCreator extends MessageToMessageEncoder<Netty4HttpRespon
             }
             out.add(new DefaultLastHttpContent(content.readRetainedSlice(content.readableBytes())));
         }
+    }
+
+    private static long suggestedMaxAllocationSize() {
+        return Math.max(Math.max(JvmInfo.jvmInfo().getG1RegionSize(), 0) >> 2, 256 * 1024);
     }
 }

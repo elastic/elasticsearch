242083a36e02496aae9214dc41b89372022e7076
Jay Modi <jaymode@users.noreply.github.com>
2020-09-22 12:14:45 -0600

    Dedicated threadpool for system index writes (#61655)
    This commit adds a dedicated threadpool for system index write
    operations. The dedicated resources for system index writes serves as
    a means to ensure that user activity does not block important system
    operations from occurring such as the management of users and roles.

--- server/src/main/java/org/elasticsearch/action/bulk/BulkRequest.java
+++ server/src/main/java/org/elasticsearch/action/bulk/BulkRequest.java
@@ -43,6 +43,7 @@ import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
@@ -393,5 +394,9 @@ public class BulkRequest extends ActionRequest implements CompositeIndicesReques
     @Override
     public long ramBytesUsed() {
         return SHALLOW_SIZE + requests.stream().mapToLong(Accountable::ramBytesUsed).sum();
+    }
+
+    public Set<String> getIndices() {
+        return Collections.unmodifiableSet(indices);
     }
 }

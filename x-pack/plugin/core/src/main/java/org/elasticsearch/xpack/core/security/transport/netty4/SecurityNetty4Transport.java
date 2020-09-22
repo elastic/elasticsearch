16d790dfcdd4945a8dc8492cb2eed0ab5bbd4581
Tim Brooks <tim@uncontended.net>
2020-09-22 17:38:46 -0600

    Fix order

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/transport/netty4/SecurityNetty4Transport.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/transport/netty4/SecurityNetty4Transport.java
@@ -121,12 +121,10 @@ public class SecurityNetty4Transport extends Netty4Transport {
 
         @Override
         protected void initChannel(Channel ch) throws Exception {
+            super.initChannel(ch);
             SSLEngine serverEngine = sslService.createSSLEngine(configuration, null, -1);
             serverEngine.setUseClientMode(false);
-            final SslHandler sslHandler = new SslHandler(serverEngine);
-            ch.pipeline().addAfter(copyToHeapHandlerName, "sslhandler", sslHandler);
-            super.initChannel(ch);
-            assert ch.pipeline().first() == sslHandler : "SSL handler must be first handler in pipeline";
+            ch.pipeline().addAfter(copyToHeapHandlerName, "sslhandler", new SslHandler(serverEngine));
         }
     }
 

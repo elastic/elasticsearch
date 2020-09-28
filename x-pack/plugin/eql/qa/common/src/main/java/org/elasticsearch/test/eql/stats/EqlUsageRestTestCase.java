48db3f285aa8976ead5a9f5d071a9c1046d7bd31
Costin Leau <costin@users.noreply.github.com>
2020-09-28 14:04:05 +0300

    EQL: Refactor testing infrastructure (#62928)
    Extract reusable methods inside QL TestUtils
    Rename abstract base classes for clarity
    Clean-up EQL DataLoader

--- x-pack/plugin/eql/qa/common/src/main/java/org/elasticsearch/test/eql/stats/EqlUsageRestTestCase.java
+++ x-pack/plugin/eql/qa/common/src/main/java/org/elasticsearch/test/eql/stats/EqlUsageRestTestCase.java
@@ -6,7 +6,6 @@
 
 package org.elasticsearch.test.eql.stats;
 
-import org.elasticsearch.Build;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentHelper;
@@ -14,7 +13,6 @@ import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.test.eql.DataLoader;
 import org.elasticsearch.test.rest.ESRestTestCase;
 import org.junit.Before;
-import org.junit.BeforeClass;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,22 +22,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static org.elasticsearch.test.eql.DataLoader.testIndexName;
 
 /**
- * Tests a random number of queries that increase various (most of the times, one query will "touch" multiple metrics values) metrics. 
+ * Tests a random number of queries that increase various (most of the times, one query will "touch" multiple metrics values) metrics.
  */
-public abstract class RestEqlUsageTestCase extends ESRestTestCase {
+public abstract class EqlUsageRestTestCase extends ESRestTestCase {
 
     private RestHighLevelClient highLevelClient;
     private Map<String, Integer> baseMetrics = new HashMap<String, Integer>();
     private Integer baseAllTotalQueries = 0;
     private Integer baseAllFailedQueries = 0;
-
-    @BeforeClass
-    public static void checkForSnapshot() {
-        assumeTrue("Only works on snapshot builds for now", Build.CURRENT.isSnapshot());
-    }
 
     /**
      * This method gets the metrics' values before the test runs, in case these values
@@ -54,7 +46,7 @@ public abstract class RestEqlUsageTestCase extends ESRestTestCase {
         List<Map<String, Map<String, Map>>> nodesListStats = (List) baseStats.get("stats");
 
         for (Map perNodeStats : nodesListStats) {
-            Map queriesMetrics = (Map) ((Map) perNodeStats.get("stats")).get("queries"); 
+            Map queriesMetrics = (Map) ((Map) perNodeStats.get("stats")).get("queries");
             Map featuresMetrics = getFeaturesMetrics(perNodeStats);
 
             for (FeatureMetric metric : FeatureMetric.values()) {
@@ -118,10 +110,10 @@ public abstract class RestEqlUsageTestCase extends ESRestTestCase {
 
     public void testEqlRestUsage() throws IOException {
         // create the index and load the data, if the index doesn't exist
-        // it doesn't matter if the index is already there (probably created by another test); _if_ its mapping is the expected one 
+        // it doesn't matter if the index is already there (probably created by another test); _if_ its mapping is the expected one
         // it should be enough
-        if (client().performRequest(new Request("HEAD", "/" + testIndexName)).getStatusLine().getStatusCode() == 404) {
-            DataLoader.loadDatasetIntoEs(highLevelClient(), (t, u) -> createParser(t, u));
+        if (client().performRequest(new Request("HEAD", "/" + DataLoader.TEST_INDEX)).getStatusLine().getStatusCode() == 404) {
+            DataLoader.loadDatasetIntoEs(highLevelClient(), this::createParser);
         }
 
         //
@@ -174,13 +166,13 @@ public abstract class RestEqlUsageTestCase extends ESRestTestCase {
         int randomMaxspanExecutions = randomIntBetween(1, 15);
         allTotalQueries += randomMaxspanExecutions;
         for (int i = 0; i < randomMaxspanExecutions; i++) {
-            runEql("sequence with maxspan=1d" + 
-                "  [process where serial_event_id < 4] by exit_code" + 
-                "  [process where opcode == 1] by user" + 
-                "  [process where opcode == 2] by user" + 
+            runEql("sequence with maxspan=1d" +
+                "  [process where serial_event_id < 4] by exit_code" +
+                "  [process where opcode == 1] by user" +
+                "  [process where opcode == 2] by user" +
                 "  [file where parent_process_name == \\\"file_delete_event\\\"] by exit_code" +
                 " until [process where opcode==1] by ppid" +
-                " | head 4" + 
+                " | head 4" +
                 " | tail 2");
         }
         responseAsMap = getStats();
@@ -196,9 +188,9 @@ public abstract class RestEqlUsageTestCase extends ESRestTestCase {
         int randomThreeQueriesSequences = randomIntBetween(1, 15);
         allTotalQueries += randomThreeQueriesSequences;
         for (int i = 0; i < randomThreeQueriesSequences; i++) {
-            runEql("sequence with maxspan=1d" + 
-                "  [process where serial_event_id < 4] by exit_code" + 
-                "  [process where opcode == 1] by user" + 
+            runEql("sequence with maxspan=1d" +
+                "  [process where serial_event_id < 4] by exit_code" +
+                "  [process where opcode == 1] by user" +
                 "  [process where opcode == 2] by user");
         }
         responseAsMap = getStats();
@@ -213,12 +205,12 @@ public abstract class RestEqlUsageTestCase extends ESRestTestCase {
         int randomFiveQueriesSequences = randomIntBetween(1, 15);
         allTotalQueries += randomFiveQueriesSequences;
         for (int i = 0; i < randomFiveQueriesSequences; i++) {
-            runEql("sequence by user, ppid, exit_code with maxspan=1m" + 
-                "  [process where serial_event_id < 4]" + 
-                "  [process where opcode == 1]" + 
+            runEql("sequence by user, ppid, exit_code with maxspan=1m" +
+                "  [process where serial_event_id < 4]" +
+                "  [process where opcode == 1]" +
                 "  [file where parent_process_name == \\\"file_delete_event\\\"]" +
-                "  [process where serial_event_id < 4]" + 
-                "  [process where opcode == 1]" + 
+                "  [process where serial_event_id < 4]" +
+                "  [process where opcode == 1]" +
                 "| tail 4");
         }
         responseAsMap = getStats();
@@ -233,8 +225,8 @@ public abstract class RestEqlUsageTestCase extends ESRestTestCase {
         int randomFourJoinKeysExecutions = randomIntBetween(1, 15);
         allTotalQueries += randomFourJoinKeysExecutions;
         for (int i = 0; i < randomFourJoinKeysExecutions; i++) {
-            runEql("sequence by exit_code, user, serial_event_id, pid" + 
-                "  [process where serial_event_id < 4]" + 
+            runEql("sequence by exit_code, user, serial_event_id, pid" +
+                "  [process where serial_event_id < 4]" +
                 "  [process where opcode == 1]");
         }
         responseAsMap = getStats();
@@ -249,8 +241,8 @@ public abstract class RestEqlUsageTestCase extends ESRestTestCase {
         int randomFiveJoinKeysExecutions = randomIntBetween(1, 15);
         allTotalQueries += randomFiveJoinKeysExecutions;
         for (int i = 0; i < randomFiveJoinKeysExecutions; i++) {
-            runEql("sequence by exit_code, user, serial_event_id, pid, ppid" + 
-                "  [process where serial_event_id < 4]" + 
+            runEql("sequence by exit_code, user, serial_event_id, pid, ppid" +
+                "  [process where serial_event_id < 4]" +
                 "  [process where opcode == 1]");
         }
         responseAsMap = getStats();
@@ -303,8 +295,12 @@ public abstract class RestEqlUsageTestCase extends ESRestTestCase {
     }
 
     private void runEql(String eql) throws IOException {
-        Request request = new Request("POST", DataLoader.testIndexName + "/_eql/search");
+        Request request = new Request("POST", DataLoader.TEST_INDEX + "/_eql/search");
         request.setJsonEntity("{\"query\":\"" + eql +"\"}");
+        runRequest(request);
+    }
+
+    protected void runRequest(Request request) throws IOException {
         client().performRequest(request);
     }
 

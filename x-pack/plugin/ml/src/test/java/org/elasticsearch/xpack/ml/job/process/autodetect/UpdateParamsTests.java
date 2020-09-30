094eeb22cfbe0e470e72280bf240573d6688ce5f
Dimitris Athanasiou <dimitris@elastic.co>
2020-09-30 15:30:42 +0300

    [7.9][ML] Fix online updates with custom rules referencing filters (#63057) (#63065)
    When an opened anomaly detection job is updated with a detection
    rule that references a filter, apart from updating the c++ process
    with the rule, we also need to update it with the referenced filter.
    
    This commit fixes a bug which led to the job not applying such updates
    on-the-fly.
    
    Fixes #62948
    
    Backport of #63057

--- x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/job/process/autodetect/UpdateParamsTests.java
+++ x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/job/process/autodetect/UpdateParamsTests.java
@@ -8,13 +8,18 @@ package org.elasticsearch.xpack.ml.job.process.autodetect;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.xpack.core.ml.job.config.DetectionRule;
 import org.elasticsearch.xpack.core.ml.job.config.JobUpdate;
+import org.elasticsearch.xpack.core.ml.job.config.MlFilter;
 import org.elasticsearch.xpack.core.ml.job.config.ModelPlotConfig;
 import org.elasticsearch.xpack.core.ml.job.config.Operator;
 import org.elasticsearch.xpack.core.ml.job.config.PerPartitionCategorizationConfig;
 import org.elasticsearch.xpack.core.ml.job.config.RuleCondition;
+import org.elasticsearch.xpack.core.ml.job.config.RuleScope;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
 
 public class UpdateParamsTests extends ESTestCase {
@@ -44,4 +49,22 @@ public class UpdateParamsTests extends ESTestCase {
         assertTrue(params.isJobUpdate());
     }
 
+    public void testExtractReferencedFilters() {
+        JobUpdate.DetectorUpdate detectorUpdate1 = new JobUpdate.DetectorUpdate(0, "",
+            Arrays.asList(
+                new DetectionRule.Builder(RuleScope.builder().include("a", "filter_1")).build(),
+                new DetectionRule.Builder(RuleScope.builder().include("b", "filter_2")).build()
+            )
+        );
+        JobUpdate.DetectorUpdate detectorUpdate2 = new JobUpdate.DetectorUpdate(0, "",
+            Collections.singletonList(new DetectionRule.Builder(RuleScope.builder().include("c", "filter_3")).build())
+        );
+
+        UpdateParams updateParams = new UpdateParams.Builder("test_job")
+            .detectorUpdates(Arrays.asList(detectorUpdate1, detectorUpdate2))
+            .filter(MlFilter.builder("filter_4").build())
+            .build();
+
+        assertThat(updateParams.extractReferencedFilters(), containsInAnyOrder("filter_1", "filter_2", "filter_3", "filter_4"));
+    }
 }

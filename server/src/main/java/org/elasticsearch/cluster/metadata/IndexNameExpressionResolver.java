cae36a5ff97ff98510939b1b58401c2572e275bf
Gordon Brown <gordon.brown@elastic.co>
2020-10-01 16:33:49 -0600

    Remove unnecessary semicolon

--- server/src/main/java/org/elasticsearch/cluster/metadata/IndexNameExpressionResolver.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/IndexNameExpressionResolver.java
@@ -339,7 +339,6 @@ public class IndexNameExpressionResolver {
                         assert false : errorMsg;
                         throw new IllegalArgumentException(errorMsg);
                 }
-                ;
             }
         }
     }

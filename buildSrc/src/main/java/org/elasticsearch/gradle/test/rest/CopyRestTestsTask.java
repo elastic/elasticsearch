f58ebe58ee52799fb42bb5a7099dc4cf582b833a
Rene Groeschke <rene@elastic.co>
2020-10-05 15:52:15 +0200

    Use services for archive and file operations in tasks (#62968) (#63201)
    Referencing a project instance during task execution is discouraged by
    Gradle and should be avoided. E.g. It is incompatible with Gradles
    incubating configuration cache. Instead there are services available to handle
    archive and filesystem operations in task actions.
    
    Brings us one step closer to #57918

--- buildSrc/src/main/java/org/elasticsearch/gradle/test/rest/CopyRestTestsTask.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/test/rest/CopyRestTestsTask.java
@@ -24,7 +24,9 @@ import org.elasticsearch.gradle.util.GradleUtils;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.file.ArchiveOperations;
 import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.FileSystemOperations;
 import org.gradle.api.file.FileTree;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.provider.ListProperty;
@@ -43,6 +45,8 @@ import java.io.File;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static org.elasticsearch.gradle.util.GradleUtils.getProjectPathFromTask;
 
 /**
  * Copies the Rest YAML test to the current projects test resources output directory.
@@ -70,6 +74,16 @@ public class CopyRestTestsTask extends DefaultTask {
 
     @Inject
     protected Factory<PatternSet> getPatternSetFactory() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Inject
+    protected FileSystemOperations getFileSystemOperations() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Inject
+    protected ArchiveOperations getArchiveOperations() {
         throw new UnsupportedOperationException();
     }
 
@@ -127,25 +141,24 @@ public class CopyRestTestsTask extends DefaultTask {
 
     @TaskAction
     void copy() {
-        Project project = getProject();
+        String projectPath = getProjectPathFromTask(getPath());
         // only copy core tests if explicitly instructed
         if (includeCore.get().isEmpty() == false) {
             if (BuildParams.isInternal()) {
-                getLogger().debug("Rest tests for project [{}] will be copied to the test resources.", project.getPath());
-                project.copy(c -> {
+                getLogger().debug("Rest tests for project [{}] will be copied to the test resources.", projectPath);
+                getFileSystemOperations().copy(c -> {
                     c.from(coreConfig.getAsFileTree());
                     c.into(getOutputDir());
                     c.include(corePatternSet.getIncludes());
                 });
-
             } else {
                 getLogger().debug(
                     "Rest tests for project [{}] will be copied to the test resources from the published jar (version: [{}]).",
-                    project.getPath(),
+                    projectPath,
                     VersionProperties.getElasticsearch()
                 );
-                project.copy(c -> {
-                    c.from(project.zipTree(coreConfig.getSingleFile()));
+                getFileSystemOperations().copy(c -> {
+                    c.from(getArchiveOperations().zipTree(coreConfig.getSingleFile()));
                     // this ends up as the same dir as outputDir
                     c.into(Objects.requireNonNull(getSourceSet().orElseThrow().getOutput().getResourcesDir()));
                     c.include(
@@ -156,17 +169,16 @@ public class CopyRestTestsTask extends DefaultTask {
         }
         // only copy x-pack tests if explicitly instructed
         if (includeXpack.get().isEmpty() == false) {
-            getLogger().debug("X-pack rest tests for project [{}] will be copied to the test resources.", project.getPath());
-            project.copy(c -> {
+            getLogger().debug("X-pack rest tests for project [{}] will be copied to the test resources.", projectPath);
+            getFileSystemOperations().copy(c -> {
                 c.from(xpackConfig.getAsFileTree());
                 c.into(getOutputDir());
                 c.include(xpackPatternSet.getIncludes());
             });
         }
-        // TODO: once https://github.com/elastic/elasticsearch/pull/62968 lands ensure that this uses `getFileSystemOperations()`
         // copy any additional config
         if (additionalConfig != null) {
-            project.copy(c -> {
+            getFileSystemOperations().copy(c -> {
                 c.from(additionalConfig.getAsFileTree());
                 c.into(getOutputDir());
             });

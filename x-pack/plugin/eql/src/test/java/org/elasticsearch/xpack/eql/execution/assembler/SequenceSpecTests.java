efd2243886f772bf31ae822497b1d545ff0544c9
Costin Leau <costin@users.noreply.github.com>
2020-10-05 14:15:10 +0300

    EQL: Avoid filtering on tiebreakers (#63215)
    Do not filter by tiebreaker while searching sequence matches as
    it's not monotonic and thus can filter out valid data.
    
    Fix #62781
    
    (cherry picked from commit 4d62198df70f3b70f8b6e7730e888057652c18a8)

--- x-pack/plugin/eql/src/test/java/org/elasticsearch/xpack/eql/execution/assembler/SequenceSpecTests.java
+++ x-pack/plugin/eql/src/test/java/org/elasticsearch/xpack/eql/execution/assembler/SequenceSpecTests.java
@@ -95,7 +95,7 @@ public class SequenceSpecTests extends ESTestCase {
 
         TestCriterion(final int ordinal) {
             super(ordinal,
-                  new BoxedQueryRequest(() -> SearchSourceBuilder.searchSource().query(matchAllQuery()).size(ordinal), "timestamp", null),
+                  new BoxedQueryRequest(() -> SearchSourceBuilder.searchSource().query(matchAllQuery()).size(ordinal), "timestamp"),
                   keyExtractors,
                   tsExtractor, tbExtractor, false);
             this.ordinal = ordinal;

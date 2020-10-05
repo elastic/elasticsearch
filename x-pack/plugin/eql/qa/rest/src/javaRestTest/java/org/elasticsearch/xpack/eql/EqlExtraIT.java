e4611038f4f0bc4000094385ba2a8f08d4dae0c7
Andrei Stefan <astefan@users.noreply.github.com>
2020-10-05 16:53:25 +0300

    Remove case_sensitive request option (#63218)
    Make EQL case sensitive by default and adapt some of the string functions
    Remove the case sensitive option from Between string function
    Add case_insensitive option to term and wildcard queries usage
    
    (cherry picked from commit 7550e0664c8c2f1f13519036c759b1e76345551f)

--- x-pack/plugin/eql/qa/rest/src/javaRestTest/java/org/elasticsearch/xpack/eql/EqlExtraIT.java
+++ x-pack/plugin/eql/qa/rest/src/javaRestTest/java/org/elasticsearch/xpack/eql/EqlExtraIT.java
@@ -12,7 +12,7 @@ import org.elasticsearch.test.junit.annotations.TestLogging;
 @TestLogging(value = "org.elasticsearch.xpack.eql:TRACE", reason = "results logging")
 public class EqlExtraIT extends EqlExtraSpecTestCase {
 
-    public EqlExtraIT(String query, String name, long[] eventIds, boolean caseSensitive) {
-        super(query, name, eventIds, caseSensitive);
+    public EqlExtraIT(String query, String name, long[] eventIds) {
+        super(query, name, eventIds);
     }
 }

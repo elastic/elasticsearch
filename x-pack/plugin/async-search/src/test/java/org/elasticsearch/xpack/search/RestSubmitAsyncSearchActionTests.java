4f420aca65ac4b4471227f508825f93dea7c5e80
Gordon Brown <gordon.brown@elastic.co>
2020-10-05 14:00:04 -0600

    Stash context in RestSubmitAsyncSearchActionTests so the same headers don't get added multiple times

--- x-pack/plugin/async-search/src/test/java/org/elasticsearch/xpack/search/RestSubmitAsyncSearchActionTests.java
+++ x-pack/plugin/async-search/src/test/java/org/elasticsearch/xpack/search/RestSubmitAsyncSearchActionTests.java
@@ -5,6 +5,7 @@
  */
 package org.elasticsearch.xpack.search;
 
+import org.apache.lucene.util.SetOnce;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionResponse;
@@ -23,6 +24,7 @@ import org.elasticsearch.tasks.Task;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.rest.FakeRestChannel;
 import org.elasticsearch.test.rest.FakeRestRequest;
+import org.elasticsearch.test.rest.RestActionTestCase;
 import org.elasticsearch.usage.UsageService;
 import org.elasticsearch.xpack.core.search.action.SubmitAsyncSearchRequest;
 import org.junit.Before;
@@ -33,7 +35,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-public class RestSubmitAsyncSearchActionTests extends ESTestCase {
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+
+public class RestSubmitAsyncSearchActionTests extends RestActionTestCase {
 
     private RestSubmitAsyncSearchAction action;
     private ActionRequest lastCapturedRequest;
@@ -100,15 +105,26 @@ public class RestSubmitAsyncSearchActionTests extends ESTestCase {
 
     private <T> void doTestParameter(String paramName, String paramValue, T expectedValue,
             Function<SubmitAsyncSearchRequest, T> valueAccessor) {
+        SetOnce<Boolean> executeCalled = new SetOnce<>();
+        verifyingClient.setExecuteLocallyVerifier((actionType, request) -> {
+            assertThat(request, instanceOf(SubmitAsyncSearchRequest.class));
+            assertThat(valueAccessor.apply((SubmitAsyncSearchRequest) request), equalTo(expectedValue));
+            executeCalled.set(true);
+            return null;
+        });
         Map<String, String> params = new HashMap<>();
         params.put(paramName, paramValue);
         RestRequest submitAsyncRestRequest = new FakeRestRequest.Builder(xContentRegistry()).withMethod(RestRequest.Method.POST)
-                .withPath("/test_index/_async_search")
-                .withParams(params)
-                .withContent(new BytesArray("{}"), XContentType.JSON).build();
-        dispatchRequest(submitAsyncRestRequest);
-        SubmitAsyncSearchRequest submitRequest = (SubmitAsyncSearchRequest) lastCapturedRequest;
-        assertEquals(expectedValue, valueAccessor.apply(submitRequest));
+            .withPath("/test_index/_async_search")
+            .withParams(params)
+            .withContent(new BytesArray("{}"), XContentType.JSON).build();
+
+        // Get a new context each time, so we don't get exceptions due to trying to add the same header multiple times
+        try (ThreadContext.StoredContext context = verifyingClient.threadPool().getThreadContext().stashContext()) {
+            dispatchRequest(submitAsyncRestRequest);
+        }
+        assertThat(executeCalled.get(), equalTo(true));
+        verifyingClient.reset();
     }
 
     /**

7969fbb4abb9e4f4ff5df4fe4001499ced4256e6
Yang Wang <yang.wang@elastic.co>
2020-10-06 23:49:23 +1100

    Cache API key doc to reduce traffic to the security index (#59376) (#63319)
    Getting the API key document form the security index is the most time consuing part
    of the API Key authentication flow (>60% if index is local and >90% if index is remote).
    This traffic is now avoided by caching added with this PR.
    
    Additionally, we add a cache invalidator registry so that clearing of different caches will
    be managed in a single place (requires follow-up PRs).

---
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/support/CacheInvalidatorRegistry.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+package org.elasticsearch.xpack.security.support;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.elasticsearch.xpack.security.support.SecurityIndexManager.isIndexDeleted;
+import static org.elasticsearch.xpack.security.support.SecurityIndexManager.isMoveFromRedToNonRed;
+
+/**
+ * A registry that provides common cache invalidation services for caches that relies on the security index.
+ */
+public class CacheInvalidatorRegistry {
+
+    private final Map<String, CacheInvalidator> cacheInvalidators = new ConcurrentHashMap<>();
+
+    public CacheInvalidatorRegistry() {
+    }
+
+    public void registerCacheInvalidator(String name, CacheInvalidator cacheInvalidator) {
+        if (cacheInvalidators.containsKey(name)) {
+            throw new IllegalArgumentException("Cache invalidator registry already has an entry with name: [" + name + "]");
+        }
+        cacheInvalidators.put(name, cacheInvalidator);
+    }
+
+    public void onSecurityIndexStageChange(SecurityIndexManager.State previousState, SecurityIndexManager.State currentState) {
+        if (isMoveFromRedToNonRed(previousState, currentState)
+            || isIndexDeleted(previousState, currentState)
+            || previousState.isIndexUpToDate != currentState.isIndexUpToDate) {
+            cacheInvalidators.values().forEach(CacheInvalidator::invalidateAll);
+        }
+    }
+
+    public void invalidateByKey(String cacheName, Collection<String> keys) {
+        final CacheInvalidator cacheInvalidator = cacheInvalidators.get(cacheName);
+        if (cacheInvalidator != null) {
+            cacheInvalidator.invalidate(keys);
+        } else {
+            throw new IllegalArgumentException("No cache named [" + cacheName + "] is found");
+        }
+    }
+
+    public void invalidateCache(String cacheName) {
+        final CacheInvalidator cacheInvalidator = cacheInvalidators.get(cacheName);
+        if (cacheInvalidator != null) {
+            cacheInvalidator.invalidateAll();
+        } else {
+            throw new IllegalArgumentException("No cache named [" + cacheName + "] is found");
+        }
+    }
+
+    public interface CacheInvalidator {
+        void invalidate(Collection<String> keys);
+
+        void invalidateAll();
+    }
+}

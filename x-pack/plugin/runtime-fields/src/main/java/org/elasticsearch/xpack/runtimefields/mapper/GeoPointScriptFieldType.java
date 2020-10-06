f53b662c3014fc168c3e3d4d5b176009ffbd4cc5
iverase <ivera@apache.org>
2020-10-06 10:50:01 +0200

    Move creation of LatLonGeometries to GeoShapeUtils

--- x-pack/plugin/runtime-fields/src/main/java/org/elasticsearch/xpack/runtimefields/mapper/GeoPointScriptFieldType.java
+++ x-pack/plugin/runtime-fields/src/main/java/org/elasticsearch/xpack/runtimefields/mapper/GeoPointScriptFieldType.java
@@ -9,24 +9,15 @@ package org.elasticsearch.xpack.runtimefields.mapper;
 import org.apache.lucene.geo.LatLonGeometry;
 import org.apache.lucene.search.MatchNoDocsQuery;
 import org.apache.lucene.search.Query;
-import org.elasticsearch.common.geo.GeoPolygonDecomposer;
 import org.elasticsearch.common.geo.GeoShapeUtils;
 import org.elasticsearch.common.geo.ShapeRelation;
 import org.elasticsearch.common.time.DateMathParser;
-import org.elasticsearch.geometry.Circle;
 import org.elasticsearch.geometry.Geometry;
-import org.elasticsearch.geometry.GeometryCollection;
-import org.elasticsearch.geometry.GeometryVisitor;
-import org.elasticsearch.geometry.LinearRing;
+import org.elasticsearch.geometry.Line;
 import org.elasticsearch.geometry.MultiLine;
-import org.elasticsearch.geometry.MultiPoint;
-import org.elasticsearch.geometry.MultiPolygon;
-import org.elasticsearch.geometry.Point;
-import org.elasticsearch.geometry.Rectangle;
 import org.elasticsearch.index.mapper.GeoPointFieldMapper;
 import org.elasticsearch.index.mapper.GeoShapeQueryable;
 import org.elasticsearch.index.query.QueryShardContext;
-import org.elasticsearch.index.query.QueryShardException;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.search.lookup.SearchLookup;
 import org.elasticsearch.xpack.runtimefields.fielddata.GeoPointScriptFieldData;
@@ -40,6 +31,12 @@ import java.util.Map;
 import java.util.function.Supplier;
 
 public final class GeoPointScriptFieldType extends AbstractScriptFieldType<GeoPointFieldScript.LeafFactory> implements GeoShapeQueryable {
+
+    private static final List<Class<? extends Geometry>> UNSUPPORTED_GEOMETRIES = new ArrayList<>();
+    static {
+        UNSUPPORTED_GEOMETRIES.add(Line.class);
+        UNSUPPORTED_GEOMETRIES.add(MultiLine.class);
+    }
 
     GeoPointScriptFieldType(String name, Script script, GeoPointFieldScript.Factory scriptFactory, Map<String, String> meta) {
         super(name, script, scriptFactory::newFactory, meta);
@@ -86,89 +83,7 @@ public final class GeoPointScriptFieldType extends AbstractScriptFieldType<GeoPo
         if (shape == null) {
             return new MatchNoDocsQuery();
         }
-        final LatLonGeometry[] geometries = toLuceneGeometry(fieldName, context, shape);
+        final LatLonGeometry[] geometries = GeoShapeUtils.toLuceneGeometry(fieldName, context, shape, UNSUPPORTED_GEOMETRIES);
         return new GeoPointScriptFieldGeoShapeQuery(script, leafFactory(context), fieldName, geometries);
-    }
-
-    private static LatLonGeometry[] toLuceneGeometry(String name, QueryShardContext context, Geometry geometry) {
-        final List<LatLonGeometry> geometries = new ArrayList<>();
-        geometry.visit(new GeometryVisitor<>() {
-            @Override
-            public Void visit(Circle circle) {
-                if (circle.isEmpty() == false) {
-                    geometries.add(GeoShapeUtils.toLuceneCircle(circle));
-                }
-                return null;
-            }
-
-            @Override
-            public Void visit(GeometryCollection<?> collection) {
-                for (Geometry shape : collection) {
-                    shape.visit(this);
-                }
-                return null;
-            }
-
-            @Override
-            public Void visit(org.elasticsearch.geometry.Line line) {
-                throw new QueryShardException(context, "Field [" + name + "] found and unsupported shape Line");
-            }
-
-            @Override
-            public Void visit(LinearRing ring) {
-                throw new QueryShardException(context, "Field [" + name + "] found and unsupported shape LinearRing");
-            }
-
-            @Override
-            public Void visit(MultiLine multiLine) {
-                throw new QueryShardException(context, "Field [" + name + "] found and unsupported shape MultiLine");
-            }
-
-            @Override
-            public Void visit(MultiPoint multiPoint) {
-                for (Point point : multiPoint) {
-                    visit(point);
-                }
-                return null;
-            }
-
-            @Override
-            public Void visit(MultiPolygon multiPolygon) {
-                if (multiPolygon.isEmpty() == false) {
-                    List<org.elasticsearch.geometry.Polygon> collector = new ArrayList<>();
-                    GeoPolygonDecomposer.decomposeMultiPolygon(multiPolygon, true, collector);
-                    collector.forEach((p) -> geometries.add(GeoShapeUtils.toLucenePolygon(p)));
-                }
-                return null;
-            }
-
-            @Override
-            public Void visit(Point point) {
-                if (point.isEmpty() == false) {
-                    geometries.add(GeoShapeUtils.toLucenePoint(point));
-                }
-                return null;
-
-            }
-
-            @Override
-            public Void visit(org.elasticsearch.geometry.Polygon polygon) {
-                if (polygon.isEmpty() == false) {
-                    List<org.elasticsearch.geometry.Polygon> collector = new ArrayList<>();
-                    GeoPolygonDecomposer.decomposePolygon(polygon, true, collector);
-                    collector.forEach((p) -> geometries.add(GeoShapeUtils.toLucenePolygon(p)));
-                }
-                return null;
-            }
-
-            @Override
-            public Void visit(Rectangle r) {
-                if (r.isEmpty() == false) {
-                    geometries.add(GeoShapeUtils.toLuceneRectangle(r));
-                }
-                return null;
-            }
-        });
-        return geometries.toArray(new LatLonGeometry[geometries.size()]);
     }
 }

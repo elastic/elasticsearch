5b85d43cd581796461a4dd42ee7e2aaa80178e59
Jack Conradson <osjdconrad@gmail.com>
2020-10-08 14:02:38 -0700

    Improve WriteScope in ir tree write phase (#63267)
    This change moves loose ASM labels into the WriteScope as opposed to having these as member data 
    on the nodes when they don't make sense for all executed phases. This adds the required scopes such 
    as loop scope and try scope to the WriteScope to account for this.

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/ir/NewObjectNode.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/ir/NewObjectNode.java
@@ -19,7 +19,6 @@
 
 package org.elasticsearch.painless.ir;
 
-import org.elasticsearch.painless.ClassWriter;
 import org.elasticsearch.painless.Location;
 import org.elasticsearch.painless.MethodWriter;
 import org.elasticsearch.painless.lookup.PainlessConstructor;
@@ -72,7 +71,8 @@ public class NewObjectNode extends ArgumentsNode {
     }
 
     @Override
-    protected void write(ClassWriter classWriter, MethodWriter methodWriter, WriteScope writeScope) {
+    protected void write(WriteScope writeScope) {
+        MethodWriter methodWriter = writeScope.getMethodWriter();
         methodWriter.writeDebugInfo(getLocation());
 
         methodWriter.newInstance(MethodWriter.getType(getExpressionType()));
@@ -82,7 +82,7 @@ public class NewObjectNode extends ArgumentsNode {
         }
 
         for (ExpressionNode argumentNode : getArgumentNodes()) {
-            argumentNode.write(classWriter, methodWriter, writeScope);
+            argumentNode.write(writeScope);
         }
 
         methodWriter.invokeConstructor(

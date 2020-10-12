ec93dc40d18b97da476eb1c3d734ba96cd3e6db2
Andrei Stefan <andrei@elastic.co>
2020-10-12 22:04:47 +0300

    Update PreAnalyzer

--- x-pack/plugin/eql/src/main/java/org/elasticsearch/xpack/eql/analysis/PreAnalyzer.java
+++ x-pack/plugin/eql/src/main/java/org/elasticsearch/xpack/eql/analysis/PreAnalyzer.java
@@ -7,9 +7,7 @@
 package org.elasticsearch.xpack.eql.analysis;
 
 import org.elasticsearch.xpack.ql.common.Failure;
-import org.elasticsearch.xpack.ql.index.EsIndex;
 import org.elasticsearch.xpack.ql.index.IndexResolution;
-import org.elasticsearch.xpack.ql.index.MappingException;
 import org.elasticsearch.xpack.ql.plan.logical.EsRelation;
 import org.elasticsearch.xpack.ql.plan.logical.LogicalPlan;
 import org.elasticsearch.xpack.ql.plan.logical.UnresolvedRelation;
@@ -19,15 +17,12 @@ import java.util.Collections;
 public class PreAnalyzer {
 
     public LogicalPlan preAnalyze(LogicalPlan plan, IndexResolution indices) {
-        EsIndex esIndex = null;
         // wrap a potential index_not_found_exception with a VerificationException (expected by client)
-        try {
-            esIndex = indices.get();
-        } catch (MappingException me) {
-            throw new VerificationException(Collections.singletonList(Failure.fail(plan, me.getMessage())));
+        if (indices.isValid() == false) {
+            throw new VerificationException(Collections.singletonList(Failure.fail(plan, indices.toString())));
         }
         if (plan.analyzed() == false) {
-            final EsRelation esRelation = new EsRelation(plan.source(), esIndex, false);
+            final EsRelation esRelation = new EsRelation(plan.source(), indices.get(), false);
             // FIXME: includeFrozen needs to be set already
             plan = plan.transformUp(r -> esRelation, UnresolvedRelation.class);
             plan.forEachUp(LogicalPlan::setPreAnalyzed);

4b3073bfaa9b949fb9e1a24dd73978c320630811
Nik Everett <nik9000@gmail.com>
2020-10-12 10:25:17 -0400

    Hit it with the formatter

--- server/src/main/java/org/elasticsearch/search/aggregations/metrics/PercentilesAggregationBuilder.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/metrics/PercentilesAggregationBuilder.java
@@ -128,10 +128,12 @@ public class PercentilesAggregationBuilder extends AbstractPercentilesAggregatio
     }
 
     @Override
-    protected ValuesSourceAggregatorFactory innerBuild(AggregationContext context,
-                                                                    ValuesSourceConfig config,
-                                                                    AggregatorFactory parent,
-                                                                    AggregatorFactories.Builder subFactoriesBuilder) throws IOException {
+    protected ValuesSourceAggregatorFactory innerBuild(
+        AggregationContext context,
+        ValuesSourceConfig config,
+        AggregatorFactory parent,
+        AggregatorFactories.Builder subFactoriesBuilder
+    ) throws IOException {
         return new PercentilesAggregatorFactory(name, config, values, configOrDefault(), keyed,
             context, parent, subFactoriesBuilder, metadata);
     }

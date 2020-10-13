e25b7b77022a88b551cb8a4336dbe6e3fcac6ec4
David Roberts <dave.roberts@elastic.co>
2020-10-13 16:24:32 +0100

    Mute ML HLRC tests that use native processes

--- client/rest-high-level/src/test/java/org/elasticsearch/client/documentation/MlClientDocumentationIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/documentation/MlClientDocumentationIT.java
@@ -18,6 +18,7 @@
  */
 package org.elasticsearch.client.documentation;
 
+import org.apache.lucene.util.LuceneTestCase;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.LatchedActionListener;
 import org.elasticsearch.action.bulk.BulkRequest;
@@ -244,6 +245,7 @@ import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 
+@LuceneTestCase.AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/pull/63542")
 public class MlClientDocumentationIT extends ESRestHighLevelClientTestCase {
 
     @After

6d9447f13d65b3078098fa9d5fa724282b5890dd
David Roberts <dave.roberts@elastic.co>
2020-10-13 17:12:23 +0100

    [7.x][ML] Mute all ML tests that may create native processes
    This is to allow merging of elastic/ml-cpp#1520 and #63542
    without causing every single CI build to fail.
    
    These changes will be reverted in #63542 after downloadable
    snapshots containing the changes of elastic/ml-cpp#1520 are
    available.
    
    Backport of #63610

--- x-pack/qa/full-cluster-restart/src/test/java/org/elasticsearch/xpack/restart/MlConfigIndexMappingsFullClusterRestartIT.java
+++ x-pack/qa/full-cluster-restart/src/test/java/org/elasticsearch/xpack/restart/MlConfigIndexMappingsFullClusterRestartIT.java
@@ -50,6 +50,7 @@ public class MlConfigIndexMappingsFullClusterRestartIT extends AbstractFullClust
         }
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/pull/63542")
     public void testMlConfigIndexMappingsAfterMigration() throws Exception {
         assumeTrue("This test only makes sense in version 6.6.0 and above", getOldClusterVersion().onOrAfter(Version.V_6_6_0));
         if (isRunningAgainstOldCluster()) {

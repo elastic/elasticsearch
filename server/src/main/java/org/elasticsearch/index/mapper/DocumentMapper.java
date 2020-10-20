f7807973ac0922d1b9455580eb9d7ae2c4c1496b
Luca Cavanna <javanna@users.noreply.github.com>
2020-10-20 12:11:43 +0200

    checkstyle

--- server/src/main/java/org/elasticsearch/index/mapper/DocumentMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/DocumentMapper.java
@@ -42,7 +42,6 @@ import org.elasticsearch.search.internal.SearchContext;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;

6eb168e640b1af65e307a873844f8d65e5a2e8f7
Jack Conradson <osjdconrad@gmail.com>
2020-10-22 11:47:32 -0700

    Add decorations to ir nodes in Painless (#63894)
    This change starts to switch all ir nodes to use decorations instead of specific member data. For this 
    specific change, we add only an expression type decoration to begin with to keep the change smaller. 
    Other members of the ir nodes will be converted to decorations in future changes.
    
    The decoration system has two important advantages:
    - The first is it's consistent with the user nodes as generated data is attached to them as a decoration, 
    so we have a clear, consistent model for how to use both trees.
    - This allows additionally generated data to be attached as necessary for optimization phases making 
    the ir tree extendable which is one of our primary, long-term goals.

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/ir/IRNode.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/ir/IRNode.java
@@ -22,9 +22,82 @@ package org.elasticsearch.painless.ir;
 import org.elasticsearch.painless.Location;
 import org.elasticsearch.painless.phase.IRTreeVisitor;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public abstract class IRNode {
 
-    /* ---- begin node data ---- */
+    /* ---- begin decorations ---- */
+
+    public interface IRDecoration {
+
+    }
+
+    private final Map<Class<? extends IRDecoration>, IRDecoration> decorations = new HashMap<>();
+
+    @SuppressWarnings("unchecked")
+    public <T extends IRDecoration> T attachDecoration(T decoration) {
+        return (T)decorations.put(decoration.getClass(), decoration);
+    }
+
+    public <T extends IRDecoration> T removeDecoration(Class<T> type) {
+        return type.cast(decorations.remove(type));
+    }
+
+    public <T extends IRDecoration> T getDecoration(Class<T> type) {
+        return type.cast(decorations.get(type));
+    }
+
+    public boolean hasDecoration(Class<? extends IRDecoration> type) {
+        return decorations.containsKey(type);
+    }
+
+    public <T extends IRDecoration> boolean copyDecorationFrom(IRNode copyFromIRNode, Class<T> type) {
+        T decoration = copyFromIRNode.getDecoration(type);
+
+
+        if (decoration != null) {
+            attachDecoration(decoration);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /* ---- end decorations, begin conditions ---- */
+
+    public interface IRCondition {
+
+    }
+
+    private final Set<Class<? extends IRCondition>> conditions = new HashSet<>();
+
+    public boolean attachCondition(Class<? extends IRCondition> type) {
+        return conditions.add(type);
+    }
+
+    public boolean removeCondition(Class<? extends IRCondition> type) {
+        return conditions.remove(type);
+    }
+
+    public boolean hasCondition(Class<? extends IRCondition> type) {
+        return conditions.contains(type);
+    }
+
+    public boolean copyConditionFrom(IRNode copyFromIRNode, Class<? extends IRCondition> type) {
+        if (copyFromIRNode.hasCondition(type)) {
+            attachCondition(type);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /* ---- end conditions, begin node data ---- */
 
     private final Location location;
 

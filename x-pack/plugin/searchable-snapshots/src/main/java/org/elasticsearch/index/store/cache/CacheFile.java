eaac6496fc42167b382cc3081d0312f227354313
Armin Braun <me@obrown.io>
2020-10-27 17:59:17 +0100

    Fix #invariant Assertion in CacheFile (#64180)
    Fix #invariant Assertion in CacheFile
    
    closes #64141

--- x-pack/plugin/searchable-snapshots/src/main/java/org/elasticsearch/index/store/cache/CacheFile.java
+++ x-pack/plugin/searchable-snapshots/src/main/java/org/elasticsearch/index/store/cache/CacheFile.java
@@ -51,6 +51,7 @@ public class CacheFile {
     private final AbstractRefCounted refCounter = new AbstractRefCounted("CacheFile") {
         @Override
         protected void closeInternal() {
+            assert evicted.get();
             assert assertNoPendingListeners();
             try {
                 Files.deleteIfExists(file);
@@ -95,7 +96,7 @@ public class CacheFile {
             } catch (IOException e) {
                 throw new UncheckedIOException(e);
             } finally {
-                refCounter.decRef();
+                decrementRefCount();
             }
         }
     }
@@ -147,7 +148,7 @@ public class CacheFile {
                 success = true;
             } finally {
                 if (success == false) {
-                    refCounter.decRef();
+                    decrementRefCount();
                 }
             }
         } else {
@@ -177,7 +178,7 @@ public class CacheFile {
             success = true;
         } finally {
             if (success) {
-                refCounter.decRef();
+                decrementRefCount();
             }
         }
         assert invariant();
@@ -191,6 +192,11 @@ public class CacheFile {
         return true;
     }
 
+    private void decrementRefCount() {
+        final boolean released = refCounter.decRef();
+        assert released == false || Files.notExists(file);
+    }
+
     /**
      * Evicts this file from the cache. Once this method has been called, subsequent use of this class with throw exceptions.
      */
@@ -200,7 +206,7 @@ public class CacheFile {
             synchronized (listeners) {
                 evictionListeners = new HashSet<>(listeners);
             }
-            refCounter.decRef();
+            decrementRefCount();
             evictionListeners.forEach(listener -> listener.onEviction(this));
         }
         assert invariant();
@@ -210,7 +216,6 @@ public class CacheFile {
         synchronized (listeners) {
             if (listeners.isEmpty()) {
                 assert channelRef == null;
-                assert evicted.get() == false || refCounter.refCount() != 0 || Files.notExists(file);
             } else {
                 assert channelRef != null;
                 assert refCounter.refCount() > 0;

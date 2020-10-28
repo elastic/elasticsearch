92c90421e9f7f4b66cb1a39bc3b85de2de25e212
wushuaiwangyin <1003217468@qq.com>
2020-10-28 16:59:23 +0800

    Add QueryCache SKIP_CACHE_FACTOR_SETTING
    Add QueryCache SKIP_CACHE_FACTOR_SETTING

--- server/src/main/java/org/elasticsearch/common/settings/ClusterSettings.java
+++ server/src/main/java/org/elasticsearch/common/settings/ClusterSettings.java
@@ -208,6 +208,7 @@ public final class ClusterSettings extends AbstractScopedSettings {
             IndicesQueryCache.INDICES_CACHE_QUERY_SIZE_SETTING,
             IndicesQueryCache.INDICES_CACHE_QUERY_COUNT_SETTING,
             IndicesQueryCache.INDICES_QUERIES_CACHE_ALL_SEGMENTS_SETTING,
+            IndicesQueryCache.INDICES_QUERIES_CACHE_SKIP_CACHE_FACTOR_SETTING,
             IndicesService.INDICES_ID_FIELD_DATA_ENABLED_SETTING,
             IndicesService.WRITE_DANGLING_INDICES_INFO_SETTING,
             MappingUpdatedAction.INDICES_MAPPING_DYNAMIC_TIMEOUT_SETTING,

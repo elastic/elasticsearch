d4bc30b4b0006ac0b084c7d8d0438ccd5d872259
Mark Vieira <portugee@gmail.com>
2020-10-28 13:21:11 -0700

    Add pull request job

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -21,8 +21,11 @@ import builds.BwcChecks
 import builds.OssChecks
 import builds.SanityCheck
 import builds.XpackChecks
-import jetbrains.buildServer.configs.kotlin.v2019_2.BuildTypeSettings
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildTypeSettings.Type.COMPOSITE
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 import jetbrains.buildServer.configs.kotlin.v2019_2.FailureAction
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
@@ -45,7 +48,7 @@ project {
     buildType {
         id("Intake")
         name = "Intake"
-        type = BuildTypeSettings.Type.COMPOSITE
+        type = COMPOSITE
 
         dependsOn(OssChecks, XpackChecks, BwcChecks) {
             onDependencyFailure = FailureAction.ADD_PROBLEM
@@ -55,6 +58,29 @@ project {
         triggers {
             vcs {
                 perCheckinTriggering = true
+            }
+        }
+    }
+
+    buildType {
+        id("PullRequest")
+        name = "Pull Request"
+        type = COMPOSITE
+
+        dependsOn(OssChecks, XpackChecks, BwcChecks) {
+            onDependencyFailure = FailureAction.ADD_PROBLEM
+            onDependencyCancel = FailureAction.ADD_PROBLEM
+        }
+
+        features {
+            pullRequests {
+                github {
+                    authType = token {
+                        token = "credentialsJSON:c8d7c068-fdda-4800-92f2-106fcebbfca4"
+                    }
+                    filterTargetBranch = "refs/heads/${DslContext.projectName}"
+                    filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+                }
             }
         }
     }

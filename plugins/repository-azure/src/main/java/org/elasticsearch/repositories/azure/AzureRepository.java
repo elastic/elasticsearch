2c7e57bec98fe3315456542f323dcda08a164857
Armin Braun <me@obrown.io>
2020-10-28 12:36:29 +0100

    Use Pooled Byte Arrays in BlobStoreRepository Serialization (#63461) (#64270)
    Many of the metadata blobs we handle in the changed spots can grow
    up in size up to `O(1M)`. Not using recycled bytes when working with
    them causes significant spikes in memory use for larger repositories.

--- plugins/repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureRepository.java
+++ plugins/repository-azure/src/main/java/org/elasticsearch/repositories/azure/AzureRepository.java
@@ -31,6 +31,7 @@ import org.elasticsearch.common.blobstore.BlobStore;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Setting.Property;
 import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.util.BigArrays;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.indices.recovery.RecoverySettings;
 import org.elasticsearch.repositories.blobstore.MeteredBlobStoreRepository;
@@ -84,9 +85,10 @@ public class AzureRepository extends MeteredBlobStoreRepository {
         final NamedXContentRegistry namedXContentRegistry,
         final AzureStorageService storageService,
         final ClusterService clusterService,
+        final BigArrays bigArrays,
         final RecoverySettings recoverySettings) {
         super(metadata, Repository.COMPRESS_SETTING.get(metadata.settings()), namedXContentRegistry, clusterService,
-            recoverySettings, buildLocation(metadata));
+            bigArrays, recoverySettings, buildLocation(metadata));
         this.chunkSize = Repository.CHUNK_SIZE_SETTING.get(metadata.settings());
         this.storageService = storageService;
 

a8e1c5982a09afd60770f9e3186fec5117113abd
Ryan Ernst <ryan@iernst.net>
2020-10-29 14:50:26 -0700

    Mute flaky RarTermsAggregatorTests.testEmbeddedMaxAgg
    see https://github.com/elastic/elasticsearch/issues/64383

--- server/src/test/java/org/elasticsearch/search/aggregations/bucket/terms/RareTermsAggregatorTests.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/bucket/terms/RareTermsAggregatorTests.java
@@ -191,6 +191,7 @@ public class RareTermsAggregatorTests extends AggregatorTestCase {
         );
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/64383")
     public void testEmbeddedMaxAgg() throws IOException {
         Query query = new MatchAllDocsQuery();
 

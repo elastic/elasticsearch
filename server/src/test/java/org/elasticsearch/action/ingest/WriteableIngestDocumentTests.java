110e1751e36a2d3514ddc2a2f886ecdb41258a68
Dan Hermann <dan.hermann@elastic.co>
2020-11-02 15:03:53 -0600

    checkstyle

--- server/src/test/java/org/elasticsearch/action/ingest/WriteableIngestDocumentTests.java
+++ server/src/test/java/org/elasticsearch/action/ingest/WriteableIngestDocumentTests.java
@@ -37,7 +37,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.StringJoiner;
 import java.util.function.Predicate;
 

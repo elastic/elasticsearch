cbf355abde4b7b206fa102ff9a62bd27cd4c05ff
Alan Woodward <romseygeek@apache.org>
2020-11-04 16:01:47 +0000

    Don't expose TextFieldMapper subfields as mappers

--- server/src/test/java/org/elasticsearch/index/mapper/TextFieldTypeTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/TextFieldTypeTests.java
@@ -133,7 +133,7 @@ public class TextFieldTypeTests extends FieldTypeTestCase {
 
     public void testIndexPrefixes() {
         TextFieldType ft = createFieldType();
-        ft.setPrefixFieldType(new TextFieldMapper.PrefixFieldType(ft, "field._index_prefix", 2, 10));
+        ft.setIndexPrefixes(2, 10);
 
         Query q = ft.prefixQuery("goin", CONSTANT_SCORE_REWRITE, false, randomMockShardContext());
         assertEquals(new ConstantScoreQuery(new TermQuery(new Term("field._index_prefix", "goin"))), q);
@@ -168,15 +168,5 @@ public class TextFieldTypeTests extends FieldTypeTestCase {
         assertEquals(List.of("value"), fetchSourceValue(fieldType, "value"));
         assertEquals(List.of("42"), fetchSourceValue(fieldType, 42L));
         assertEquals(List.of("true"), fetchSourceValue(fieldType, true));
-
-        TextFieldMapper.PrefixFieldType prefixFieldType = new TextFieldMapper.PrefixFieldType(fieldType, "field._index_prefix", 2, 10);
-        assertEquals(List.of("value"), fetchSourceValue(prefixFieldType, "value"));
-        assertEquals(List.of("42"), fetchSourceValue(prefixFieldType, 42L));
-        assertEquals(List.of("true"), fetchSourceValue(prefixFieldType, true));
-
-        TextFieldMapper.PhraseFieldType phraseFieldType = new TextFieldMapper.PhraseFieldType(fieldType);
-        assertEquals(List.of("value"), fetchSourceValue(phraseFieldType, "value"));
-        assertEquals(List.of("42"), fetchSourceValue(phraseFieldType, 42L));
-        assertEquals(List.of("true"), fetchSourceValue(phraseFieldType, true));
     }
 }

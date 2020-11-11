2cdd5f98548d040b1aa489b980d2cd79340fe17e
mantuliu <240951888@qq.com>
2020-11-11 23:17:51 +0800

    SQL: Fix the return type in the sign function (#64845)
    Fixes the inconsistency between the type of the object returned by the
    `SIGN()/SIGNUM()` SQL functions and the specified `DataType`.
    
    In the Class Sign, DataType is DataTypes.INTEGER. The source code is as
    follows：
    
    ```
        @Override
        public DataType dataType() {
            return DataTypes.INTEGER;
        }
    ```
    
    But In the Class MathProcessor, the source code of SIGN((Object l),
    Parameter and return value types are the same. Therefore, when using
    double or float parameters to test, there is a little problem, the test
    method is like the following curl :
    
    ```
    curl -XPOST 127.0.0.1:9200/_sql -d "{\"query\":\"select  SIGN(1.0) \"}" \
      -H 'Content-Type: application/json'
    ```
    
    The result is:
    
    ```
    {"columns":[{"name":"SIGN(1.0)","type":"integer"}],"rows":[[1.0]]}
    ```
    
    The result value is `1.0`, but the type is `integer`.
    
    Signed-off-by: mantuliu <240951888@qq.com>
    Co-authored-by: Marios Trivyzas <matriv@gmail.com>

--- x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/expression/function/scalar/math/MathOperationTests.java
+++ x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/expression/function/scalar/math/MathOperationTests.java
@@ -10,6 +10,9 @@ import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.xpack.ql.QlIllegalArgumentException;
 import org.elasticsearch.xpack.sql.expression.function.scalar.math.MathProcessor.MathOperation;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MathOperationTests extends ESTestCase {
 
     public void testAbsLongMax() {
@@ -34,5 +37,29 @@ public class MathOperationTests extends ESTestCase {
         assertEquals((long) 42, MathOperation.ABS.apply((long) -42));
         assertEquals(42f, MathOperation.ABS.apply(-42f));
         assertEquals(42d, MathOperation.ABS.apply(-42d));
+    }
+
+    public void testSignIntegerType() {
+        List<Number> negative = Arrays.asList((byte) -42, (short) -42, -42, Long.valueOf(-42), Float.valueOf(-42.0f), Double.valueOf(-42.0d));
+        List<Number> zero = Arrays.asList((byte) 0, (short) 0, 0, Long.valueOf(0), Float.valueOf(0.0f), Double.valueOf(0.0d));
+        List<Number> positive = Arrays.asList((byte) 42, (short) 42, 42, Long.valueOf(42), Float.valueOf(42.0f), Double.valueOf(42.0d));
+
+        for (Number number : negative) {
+            Number result = MathOperation.SIGN.apply(number);
+            assertEquals(Integer.class, result.getClass());
+            assertEquals(-1, result);
+        }
+
+        for (Number number : zero) {
+            Number result = MathOperation.SIGN.apply(number);
+            assertEquals(Integer.class, result.getClass());
+            assertEquals(0, result);
+        }
+
+        for (Number number : positive) {
+            Number result = MathOperation.SIGN.apply(number);
+            assertEquals(Integer.class, result.getClass());
+            assertEquals(1, result);
+        }
     }
 }

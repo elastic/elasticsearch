9bd3e58dc78d810eeac0715351d30b2da79e088f
Mark Vieira <portugee@gmail.com>
2020-11-23 14:44:19 -0800

    Mute WatcherUtilsTests.testDeserializeSearchRequest

--- x-pack/plugin/watcher/src/test/java/org/elasticsearch/xpack/watcher/support/WatcherUtilsTests.java
+++ x-pack/plugin/watcher/src/test/java/org/elasticsearch/xpack/watcher/support/WatcherUtilsTests.java
@@ -150,6 +150,7 @@ public class WatcherUtilsTests extends ESTestCase {
         }
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/65397")
     public void testDeserializeSearchRequest() throws Exception {
 
         XContentBuilder builder = jsonBuilder().startObject();

f0f0930bd7675660dba1177bf720f2c9bdbbe35f
Christoph Büscher <cbuescher@posteo.de>
2020-11-24 18:58:38 +0100

    Make keyword_marker filter updateable

--- modules/analysis-common/src/main/java/org/elasticsearch/analysis/common/KeywordMarkerTokenFilterFactory.java
+++ modules/analysis-common/src/main/java/org/elasticsearch/analysis/common/KeywordMarkerTokenFilterFactory.java
@@ -28,6 +28,7 @@ import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
 import org.elasticsearch.index.analysis.Analysis;
+import org.elasticsearch.index.analysis.AnalysisMode;
 
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -51,6 +52,7 @@ public class KeywordMarkerTokenFilterFactory extends AbstractTokenFilterFactory 
 
     private final CharArraySet keywordLookup;
     private final Pattern keywordPattern;
+    private final AnalysisMode analysisMode;
 
     KeywordMarkerTokenFilterFactory(IndexSettings indexSettings, Environment env, String name, Settings settings) {
         super(indexSettings, name, settings);
@@ -78,6 +80,13 @@ public class KeywordMarkerTokenFilterFactory extends AbstractTokenFilterFactory 
             keywordLookup = new CharArraySet(rules, ignoreCase);
             keywordPattern = null;
         }
+        boolean updateable = settings.getAsBoolean("updateable", false);
+        this.analysisMode = updateable ? AnalysisMode.SEARCH_TIME : AnalysisMode.ALL;
+    }
+
+    @Override
+    public AnalysisMode getAnalysisMode() {
+        return this.analysisMode;
     }
 
     @Override

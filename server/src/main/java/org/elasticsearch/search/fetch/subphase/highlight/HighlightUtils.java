28d7f65929d01a59b6e40ec84987fd55b2bc5600
Alan Woodward <romseygeek@apache.org>
2020-11-26 14:18:44 +0000

    Add note re DocValuesFetchers not working

--- server/src/main/java/org/elasticsearch/search/fetch/subphase/highlight/HighlightUtils.java
+++ server/src/main/java/org/elasticsearch/search/fetch/subphase/highlight/HighlightUtils.java
@@ -44,6 +44,10 @@ public final class HighlightUtils {
 
     }
 
+    // Note that this doesn't work for DocValueFetchers yet, because it doesn't do
+    // any positioning.  Can we maybe unify access to fetchers etc via SearchLookup,
+    // and let the FetchPhase drive positioning?
+
     /**
      * Load field values for highlighting.
      */

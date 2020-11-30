b7c8cf7616b30d93529065d6be21956da0b6b816
David Kyle <david.kyle@elastic.co>
2020-11-30 22:31:24 +0000

    Add test for datafeed aggs

--- x-pack/plugin/ml/qa/native-multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/ml/integration/DatafeedWithAggsIT.java
+++ x-pack/plugin/ml/qa/native-multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/ml/integration/DatafeedWithAggsIT.java
@@ -5,10 +5,13 @@
  */
 package org.elasticsearch.xpack.ml.integration;
 
+import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.support.WriteRequest;
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.AggregatorFactories;
@@ -22,13 +25,20 @@ import org.elasticsearch.xpack.core.ml.job.config.AnalysisConfig;
 import org.elasticsearch.xpack.core.ml.job.config.DataDescription;
 import org.elasticsearch.xpack.core.ml.job.config.Detector;
 import org.elasticsearch.xpack.core.ml.job.config.Job;
+import org.elasticsearch.xpack.core.ml.job.config.JobState;
+import org.elasticsearch.xpack.core.ml.job.process.autodetect.state.DataCounts;
 import org.elasticsearch.xpack.core.ml.job.results.Bucket;
 import org.junit.After;
 
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import static org.elasticsearch.xpack.ml.support.BaseMlIntegTestCase.getDataCounts;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
@@ -137,5 +147,113 @@ public class DatafeedWithAggsIT extends MlNativeAutodetectIntegTestCase {
                 fail("Bucket [" + bucket.getTimestamp().getTime() + "] has [" + bucket.getEventCount() + "] when 2 were expected");
             }
         }
+    }
+
+    public void testLookbackOnly_WithRuntimeMapping() throws Exception {
+        String indexName = "df-data";
+        client().admin().indices().prepareCreate(indexName)
+            .setMapping("time", "type=date", "metric", "type=double")
+            .get();
+
+        ZonedDateTime startOfDay = ZonedDateTime.now().toLocalDate().atStartOfDay(ZonedDateTime.now().getZone());
+        long endTime = startOfDay.toEpochSecond() * 1000;
+        long startTime = startOfDay.minus(2, ChronoUnit.DAYS).toEpochSecond() * 1000L;
+        long bucketSize = 3600000;
+        long numDocsPerBucket = 2L;
+        indexDocs(indexName, numDocsPerBucket, startTime,  endTime, bucketSize);
+
+        DataDescription.Builder dataDescription = new DataDescription.Builder();
+        dataDescription.setTimeFormat("yyyy-MM-dd HH:mm:ss");
+
+        Detector.Builder d = new Detector.Builder("sum", "metric_sum");
+        AnalysisConfig.Builder analysisConfig = new AnalysisConfig.Builder(Collections.singletonList(d.build()));
+        analysisConfig.setBucketSpan(TimeValue.timeValueHours(1));
+        analysisConfig.setSummaryCountFieldName("doc_count");
+
+        Job.Builder jobBuilder = new Job.Builder();
+        jobBuilder.setId("lookback-job-with-rt-fields-agg");
+        jobBuilder.setAnalysisConfig(analysisConfig);
+        jobBuilder.setDataDescription(dataDescription);
+
+        registerJob(jobBuilder);
+        putJob(jobBuilder);
+        openJob(jobBuilder.getId());
+        assertBusy(() -> assertEquals(getJobStats(jobBuilder.getId()).get(0).getState(), JobState.OPENED));
+
+        DatafeedConfig.Builder dfBuilder = new DatafeedConfig.Builder(jobBuilder.getId() + "-datafeed", jobBuilder.getId());
+        dfBuilder.setIndices(Collections.singletonList(indexName));
+
+        // aggregate on a runtime field
+        AggregatorFactories.Builder aggs = new AggregatorFactories.Builder();
+        aggs.addAggregator(AggregationBuilders.dateHistogram("time").field("time")
+            .fixedInterval(new DateHistogramInterval("1h"))
+            .subAggregation(AggregationBuilders.max("time").field("time"))
+            .subAggregation(AggregationBuilders.sum("metric_sum").field("metric_percent"))
+        );
+
+        dfBuilder.setParsedAggregations(aggs);
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("type", "double");
+        properties.put("script", "emit(doc['metric'].value * 100.0)");
+        Map<String, Object> fields = new HashMap<>();
+        fields.put("metric_percent", properties);
+        dfBuilder.setRuntimeMappings(fields);
+
+        DatafeedConfig datafeedConfig = dfBuilder.build();
+
+        registerDatafeed(datafeedConfig);
+        putDatafeed(datafeedConfig);
+
+        startDatafeed(datafeedConfig.getId(), 0L, endTime);
+        long expectedNumberOfHistoBuckets = ((endTime - startTime) / bucketSize);
+        assertBusy(() -> {
+            GetDatafeedsStatsAction.Request request = new GetDatafeedsStatsAction.Request(datafeedConfig.getId());
+            GetDatafeedsStatsAction.Response response = client().execute(GetDatafeedsStatsAction.INSTANCE, request).actionGet();
+            assertThat(response.getResponse().results().get(0).getDatafeedState(), equalTo(DatafeedState.STOPPED));
+
+            DataCounts dataCounts = getDataCounts(jobBuilder.getId());
+            System.out.println(Strings.toString(dataCounts));
+            assertThat(dataCounts.getProcessedRecordCount(), equalTo(expectedNumberOfHistoBuckets));
+            assertThat(dataCounts.getBucketCount(), equalTo(expectedNumberOfHistoBuckets -1));
+            assertThat(dataCounts.getInputFieldCount(), equalTo(expectedNumberOfHistoBuckets * 2));
+            assertThat(dataCounts.getMissingFieldCount(), equalTo(0L));
+            assertThat(dataCounts.getEmptyBucketCount(), equalTo(0L));
+        }, 60, TimeUnit.SECONDS);
+
+        waitUntilJobIsClosed(jobBuilder.getId());
+    }
+
+    private void indexDocs(String index, long numDocs, long start, long end, long bucketSize) {
+        BulkRequestBuilder bulkRequestBuilder = client().prepareBulk();
+        long numBuckets = (end - start) / bucketSize;
+        for (long i = 0; i < numBuckets; i++) {
+            for (long j = 0; j < numDocs; j++) {
+                IndexRequest indexRequest = new IndexRequest(index);
+                long timestamp = start + randomLongBetween(1, bucketSize - 1);
+                double value = randomDoubleBetween(0.0, 1.0, true);
+                indexRequest.source("time", timestamp, "metric", value).opType(DocWriteRequest.OpType.CREATE);
+                bulkRequestBuilder.add(indexRequest);
+            }
+
+            start += bucketSize;
+        }
+        assertThat(start, equalTo(end));
+
+        BulkResponse bulkResponse = bulkRequestBuilder
+            .setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE)
+            .get();
+
+        if (bulkResponse.hasFailures()) {
+            int failures = 0;
+            for (BulkItemResponse itemResponse : bulkResponse) {
+                if (itemResponse.isFailed()) {
+                    failures++;
+                    logger.error("Item response failure [{}]", itemResponse.getFailureMessage());
+                }
+            }
+            fail("Bulk response contained " + failures + " failures");
+        }
+        logger.info("Indexed [{}] documents", bulkRequestBuilder.numberOfActions());
     }
 }

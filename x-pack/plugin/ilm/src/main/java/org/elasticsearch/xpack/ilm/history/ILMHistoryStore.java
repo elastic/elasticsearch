b0e6f2d37c7ad0a217b791ff2f9aef9b3014bb10
Armin Braun <me@obrown.io>
2020-12-01 00:48:34 +0100

    Fix AbstractClient#execute Listener Leak (#65415)
    This was observed in #65405 due to trying to locally execute a
    task whose parent was already cancelled but is a general issue.
    
    We should not throw from APIs that consume a listener as this may
    like in this case leak the listener in that case.
    Rather than fixing the specific case of #65405 this fixes the
    abstract client overall to avoid other such leaks.
    
    Closes #65405

--- x-pack/plugin/ilm/src/main/java/org/elasticsearch/xpack/ilm/history/ILMHistoryStore.java
+++ x-pack/plugin/ilm/src/main/java/org/elasticsearch/xpack/ilm/history/ILMHistoryStore.java
@@ -42,6 +42,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -110,7 +111,8 @@ public class ILMHistoryStore implements Closeable {
                     if (response.hasFailures()) {
                         Map<String, String> failures = Arrays.stream(response.getItems())
                             .filter(BulkItemResponse::isFailed)
-                            .collect(Collectors.toMap(BulkItemResponse::getId, BulkItemResponse::getFailureMessage));
+                            .collect(Collectors.toMap(BulkItemResponse::getId, BulkItemResponse::getFailureMessage,
+                                    (msg1, msg2) -> Objects.equals(msg1, msg2) ? msg1 : msg1 + "," + msg2));
                         logger.error("failures: [{}]", failures);
                     }
                 }

2ae1bef7a0e39fe51d2d9c5e0cc150f4acb00a1d
Yang Wang <yang.wang@elastic.co>
2020-11-30 22:59:32 +1100

    Fix import

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/operator/FileOperatorUsersStore.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/operator/FileOperatorUsersStore.java
@@ -8,6 +8,7 @@ package org.elasticsearch.xpack.security.operator;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ElasticsearchParseException;
 import org.elasticsearch.common.Nullable;

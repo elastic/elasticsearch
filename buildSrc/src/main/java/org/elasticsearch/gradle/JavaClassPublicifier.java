a27c23b0a73a4a554dd57207d8c0b4129ad4ffa2
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2020-12-02 11:32:57 +0100

    Fix precommit error

--- buildSrc/src/main/java/org/elasticsearch/gradle/JavaClassPublicifier.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/JavaClassPublicifier.java
@@ -33,7 +33,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.objectweb.asm.Opcodes.ACC_PUBLIC;

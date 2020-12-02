0f522ade45e68e2c17ae75335614e692ea7fe7e8
Ryan Ernst <ryan@iernst.net>
2020-12-01 16:16:07 -0800

    Force azure impl to be public
    The azure sdk internally dynamically constructs its client classes.
    However, one of these, for the blob storage is private. This has been
    fixed upstream, but until that is released, the client is unuseable
    without the newProxyInPackage permission. Rather than grant that
    permission, this commit makes the class in question public by patching
    the azure class file when building the azure repository plugin.
    
    relates https://github.com/Azure/azure-sdk-for-java/issues/17368

---
+++ buildSrc/src/main/java/org/elasticsearch/gradle/JavaClassPublicifier.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.gradle;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.TaskAction;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.ClassNode;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
+
+/**
+ * A task to manipulate an existing class file.
+ */
+public class JavaClassPublicifier extends DefaultTask {
+
+    private String classFile;
+    private DirectoryProperty inputDir;
+    private DirectoryProperty outputDir;
+
+    public JavaClassPublicifier() {
+        this.inputDir = getProject().getObjects().directoryProperty();
+        this.outputDir = getProject().getObjects().directoryProperty();
+    }
+
+    @Input
+    public String getClassFile() {
+        return classFile;
+    }
+
+    public void setClassFile(String classFile) {
+        this.classFile = classFile;
+    }
+
+    @InputDirectory
+    public DirectoryProperty getInputDir() {
+        return inputDir;
+    }
+
+    @OutputDirectory
+    public DirectoryProperty getOutputDir() {
+        return outputDir;
+    }
+
+    @TaskAction
+    public void adapt() throws IOException {
+        final ClassNode classNode;
+        try (InputStream is = Files.newInputStream(inputDir.get().file(classFile).getAsFile().toPath())) {
+            ClassReader classReader = new ClassReader(is);
+            classNode = new ClassNode();
+            classReader.accept(classNode, ClassReader.EXPAND_FRAMES);
+        }
+
+        classNode.access |= ACC_PUBLIC;
+
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        classNode.accept(classWriter);
+
+        File outputFile = outputDir.get().file(classFile).getAsFile();
+        outputFile.getParentFile().mkdirs();
+        Files.write(outputFile.toPath(), classWriter.toByteArray());
+    }
+}

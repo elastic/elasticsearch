9f451c1802d4ca63b9827b7aacd5c3c940a0c1ac
Gordon Brown <gordon.brown@elastic.co>
2020-12-03 11:22:19 -0700

    Remove _fleet APIs

--- x-pack/plugin/fleet/src/main/java/org/elasticsearch/xpack/fleet/Fleet.java
+++ x-pack/plugin/fleet/src/main/java/org/elasticsearch/xpack/fleet/Fleet.java
@@ -6,36 +6,13 @@
 
 package org.elasticsearch.xpack.fleet;
 
-import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
-import org.elasticsearch.cluster.node.DiscoveryNodes;
-import org.elasticsearch.common.settings.ClusterSettings;
-import org.elasticsearch.common.settings.IndexScopedSettings;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.settings.SettingsFilter;
-import org.elasticsearch.index.reindex.RestDeleteByQueryAction;
 import org.elasticsearch.indices.SystemIndexDescriptor;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.plugins.SystemIndexPlugin;
-import org.elasticsearch.rest.BaseRestHandler;
-import org.elasticsearch.rest.RestController;
-import org.elasticsearch.rest.RestHandler;
-import org.elasticsearch.rest.action.admin.indices.RestCreateIndexAction;
-import org.elasticsearch.rest.action.admin.indices.RestRefreshAction;
-import org.elasticsearch.rest.action.document.RestBulkAction;
-import org.elasticsearch.rest.action.document.RestDeleteAction;
-import org.elasticsearch.rest.action.document.RestGetAction;
-import org.elasticsearch.rest.action.document.RestIndexAction;
-import org.elasticsearch.rest.action.document.RestIndexAction.AutoIdHandler;
-import org.elasticsearch.rest.action.document.RestIndexAction.CreateHandler;
-import org.elasticsearch.rest.action.document.RestUpdateAction;
-import org.elasticsearch.rest.action.search.RestClearScrollAction;
-import org.elasticsearch.rest.action.search.RestSearchAction;
-import org.elasticsearch.rest.action.search.RestSearchScrollAction;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 /**
  * A plugin to manage and provide access to the system indices used by Fleet.
@@ -52,64 +29,5 @@ public class Fleet extends Plugin implements SystemIndexPlugin {
             new SystemIndexDescriptor(".fleet-agents*", "Agents and agent checkins"),
             new SystemIndexDescriptor(".fleet-actions*", "Fleet actions")
         );
-    }
-
-    @Override
-    public List<RestHandler> getRestHandlers(
-        Settings settings,
-        RestController restController,
-        ClusterSettings clusterSettings,
-        IndexScopedSettings indexScopedSettings,
-        SettingsFilter settingsFilter,
-        IndexNameExpressionResolver indexNameExpressionResolver,
-        Supplier<DiscoveryNodes> nodesInCluster
-    ) {
-        // TODO need to lock down access to just Fleet indices
-        return List.of(
-            new FleetWrappedRestHandler(new RestCreateIndexAction()),
-
-            new FleetWrappedRestHandler(new RestIndexAction()),
-            new FleetWrappedRestHandler(new RestGetAction()),
-            new FleetWrappedRestHandler(new RestUpdateAction()),
-            new FleetWrappedRestHandler(new CreateHandler()),
-            new FleetWrappedRestHandler(new AutoIdHandler(nodesInCluster)),
-
-            new FleetWrappedRestHandler(new RestSearchAction()),
-            new FleetWrappedRestHandler(new RestSearchScrollAction()),
-            new FleetWrappedRestHandler(new RestClearScrollAction()),
-
-            new FleetWrappedRestHandler(new RestBulkAction(settings)),
-            new FleetWrappedRestHandler(new RestDeleteAction()),
-            new FleetWrappedRestHandler(new RestDeleteByQueryAction()),
-            new FleetWrappedRestHandler(new RestRefreshAction())
-        );
-
-    }
-
-    /**
-     * Wraps a generic {@link BaseRestHandler} with a {@code _fleet} prefix.
-     */
-    static class FleetWrappedRestHandler extends BaseRestHandler.Wrapper {
-
-        FleetWrappedRestHandler(BaseRestHandler delegate) {
-            super(delegate);
-        }
-
-        @Override
-        public String getName() {
-            return "fleet_" + super.getName();
-        }
-
-        @Override
-        public boolean allowSystemIndexAccessByDefault() {
-            return true;
-        }
-
-        @Override
-        public List<Route> routes() {
-            return super.routes().stream()
-                .map(route -> new Route(route.getMethod(), "/_fleet" + route.getPath()))
-                .collect(Collectors.toUnmodifiableList());
-        }
     }
 }

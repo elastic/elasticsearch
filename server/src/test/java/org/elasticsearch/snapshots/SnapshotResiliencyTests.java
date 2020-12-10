728116d2f45e41fbc55cb4cc1b1a4fb8293fc7fd
Tanguy Leroux <tlrx.dev@gmail.com>
2020-12-10 12:24:56 +0100

    Introduce a mechanism to notify plugin before an index/shard folder is going to be deleted from disk (#65926)
    This commit introduces a new type of listeners
    IndexStorePlugin.IndexFoldersDeletionListener
    that allows plugins to be notified when an index
    folder (or a shard folder) is about to be deleted
    from disk.
    
    This is useful for some plugins that require to
    take an action before folders are deleted, like
    searchable snapshots which should evict the
    cache files that are contained in the folders.

--- server/src/test/java/org/elasticsearch/snapshots/SnapshotResiliencyTests.java
+++ server/src/test/java/org/elasticsearch/snapshots/SnapshotResiliencyTests.java
@@ -1499,7 +1499,8 @@ public class SnapshotResiliencyTests extends ESTestCase {
                     Collections.emptyList(),
                     emptyMap(),
                     null,
-                    emptyMap()
+                    emptyMap(),
+                    List.of()
                 );
                 final RecoverySettings recoverySettings = new RecoverySettings(settings, clusterSettings);
                 snapshotShardsService =

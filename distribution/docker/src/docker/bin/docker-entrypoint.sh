0906c33bf222d9bb9c12969466ecff07e506fbc8
Rory Hunter <pugnascotia@users.noreply.github.com>
2020-12-10 12:25:47 +0000

    Make it possible to use Stack logging in Docker
    Backport of #65778.
    
    Closes #62758.
    
    Include the Stack log4j config in the Docker image, in order to
    make it possible to write logs in a container environment in the
    same way as for an archive or package deployment. This is useful
    in situations where the user is bind-mounting the logs directory
    and has their own arrangements for log shipping.
    
    To use stack logging, set the environment variable `ES_LOG_STYLE`
    to `file`. It can also be set to `console`, which is the same as
    not specifying it at all.
    
    The Docker logging config is now auto-generated at image build time,
    by running the default config through a transformer program when
    preparing the distribution in an image builder step.
    
    Also, in the docker distribution `build.gradle`, I changed a helper
    closure into a class with a static method in order to fix an
    issue where the Docker image was always being rebuilt, even when
    there were no changes.

--- distribution/docker/src/docker/bin/docker-entrypoint.sh
+++ distribution/docker/src/docker/bin/docker-entrypoint.sh
@@ -80,4 +80,21 @@ if [[ "$(id -u)" == "0" ]]; then
   fi
 fi
 
+if [[ -n "$ES_LOG_STYLE" ]]; then
+  case "$ES_LOG_STYLE" in
+    console)
+      # This is the default. Nothing to do.
+      ;;
+    file)
+      # Overwrite the default config with the stack config
+      mv /usr/share/elasticsearch/config/log4j2.file.properties /usr/share/elasticsearch/config/log4j2.properties
+      ;;
+    *)
+      echo "ERROR: ES_LOG_STYLE set to [$ES_LOG_STYLE]. Expected [console] or [file]" >&2
+      exit 1 ;;
+  esac
+fi
+
+# Signal forwarding and child reaping is handled by `tini`, which is the
+# actual entrypoint of the container
 run_as_other_user_if_needed /usr/share/elasticsearch/bin/elasticsearch <<<"$KEYSTORE_PASSWORD"

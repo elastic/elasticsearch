d4396547121189b4ec62def97e98b7421ed5ca5f
Yang Wang <yang.wang@elastic.co>
2020-12-11 21:32:49 +1100

    Do not restore autoscaling policy from snapshots (#66023) (#66199)
    Autoscaling policies (custom metadata) are marked as non-restorable and hence
    they are never restored from a snapshot.

--- x-pack/plugin/autoscaling/src/main/java/org/elasticsearch/xpack/autoscaling/AutoscalingMetadata.java
+++ x-pack/plugin/autoscaling/src/main/java/org/elasticsearch/xpack/autoscaling/AutoscalingMetadata.java
@@ -32,7 +32,7 @@ import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class AutoscalingMetadata implements XPackPlugin.XPackMetadataCustom {
+public class AutoscalingMetadata implements XPackPlugin.XPackMetadataCustom, Metadata.NonRestorableCustom {
 
     public static final String NAME = "autoscaling";
 

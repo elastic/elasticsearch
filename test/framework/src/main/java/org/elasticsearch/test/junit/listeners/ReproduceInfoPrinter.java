c5d05eea436b313fdae6b3a7abe836689aa7196d
Rene Groeschke <rene@elastic.co>
2020-12-12 19:42:40 +0100

    Show -Dlicense.key value in test repro info (#66179)
    - When a -Dlicense.key sys property is passed to the build we want to consider
    this in the test reproduction info message
    - Absolute Paths tried to be converted to relative paths relative to workspace
    root to allow simply copy & paste
    - Also fixes a inconsistency for checking license existence in x-pack plugin core build

--- test/framework/src/main/java/org/elasticsearch/test/junit/listeners/ReproduceInfoPrinter.java
+++ test/framework/src/main/java/org/elasticsearch/test/junit/listeners/ReproduceInfoPrinter.java
@@ -89,7 +89,6 @@ public class ReproduceInfoPrinter extends RunListener {
             }
         }
         b.append("\"");
-
         GradleMessageBuilder gradleMessageBuilder = new GradleMessageBuilder(b);
         gradleMessageBuilder.appendAllOpts(failure.getDescription());
 
@@ -171,6 +170,7 @@ public class ReproduceInfoPrinter extends RunListener {
             appendOpt("tests.timezone", TimeZone.getDefault().getID());
             appendOpt("tests.distribution", System.getProperty("tests.distribution"));
             appendOpt("runtime.java", Integer.toString(JavaVersion.current().getVersion().get(0)));
+            appendOpt("license.key", System.getProperty("licence.key"));
             appendOpt(ESTestCase.FIPS_SYSPROP, System.getProperty(ESTestCase.FIPS_SYSPROP));
             return this;
         }

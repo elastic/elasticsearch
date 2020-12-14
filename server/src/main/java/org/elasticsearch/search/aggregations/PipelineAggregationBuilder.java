7bc827d681a20642b318a3085a4d7200d868b83e
Abdul Aziz Ali <abdulazizali@acm.org>
2020-12-14 14:29:16 +0800

    Add unit tests for CuSum with Terms parent. Rename existing CuSum Histo tests. Modify parent error messages and parent validation tests (#46225)

--- server/src/main/java/org/elasticsearch/search/aggregations/PipelineAggregationBuilder.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/PipelineAggregationBuilder.java
@@ -137,7 +137,7 @@ public abstract class PipelineAggregationBuilder
             @Override
             public void validateParentAggSequentiallyOrdered(String type, String name) {
                 addValidationError(type + " aggregation [" + name
-                        + "] must have a histogram, date_histogram or auto_date_histogram as parent but doesn't have a parent");
+                        + "] must have a histogram, date_histogram, auto_date_histogram or terms as parent but doesn't have a parent");
             }
         }
 
@@ -184,7 +184,7 @@ public abstract class PipelineAggregationBuilder
                     //TODO: what should be checked?
                 } else {
                     addValidationError(
-                            type + " aggregation [" + name + "] must have a histogram, date_histogram or auto_date_histogram as parent");
+                            type + " aggregation [" + name + "] must have a histogram, date_histogram, auto_date_histogram or terms as parent");
                 }
             }
         }

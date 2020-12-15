242ca00666fb98c1a7d5d10480cbc9a1f8af556f
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2020-12-15 11:50:31 +0100

    Allow blocking calls on futures used in TimestampFieldMapperService
    Some of the clients of TimestampFieldMapperService might be executed
    in a transport thread, we only do a blocking action once the future
    is completed in #getTimestampFieldType, so technically we are not
    blocking the thread. To avoid triggering assertion errors, we mark
    that future as non-blocking.

--- server/src/main/java/org/elasticsearch/indices/TimestampFieldMapperService.java
+++ server/src/main/java/org/elasticsearch/indices/TimestampFieldMapperService.java
@@ -104,7 +104,15 @@ public class TimestampFieldMapperService extends AbstractLifecycleComponent impl
 
             if (hasUsefulTimestampField(indexMetadata) && fieldTypesByIndex.containsKey(index) == false) {
                 logger.trace("computing timestamp mapping for {}", index);
-                final PlainActionFuture<DateFieldMapper.DateFieldType> future = new PlainActionFuture<>();
+                final PlainActionFuture<DateFieldMapper.DateFieldType> future = new PlainActionFuture<>() {
+                    @Override
+                    protected boolean blockingAllowed() {
+                        // We only get the future result once it's completed
+                        // so technically we don't block the calling thread.
+                        // See #getTimestampFieldType()
+                        return true;
+                    }
+                };
                 fieldTypesByIndex.put(index, future);
 
                 final IndexService indexService = indicesService.indexService(index);

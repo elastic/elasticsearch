69870411e933a8bbf008cf5a9f3e9e929871b0f5
Tal Levy <jubboy333@gmail.com>
2020-12-15 13:22:08 -0800

    change rollup threadpool to use one thread and unsized queue

--- x-pack/plugin/rollup/src/main/java/org/elasticsearch/xpack/rollup/Rollup.java
+++ x-pack/plugin/rollup/src/main/java/org/elasticsearch/xpack/rollup/Rollup.java
@@ -159,7 +159,7 @@ public class Rollup extends Plugin implements ActionPlugin, PersistentTaskPlugin
     @Override
     public List<ExecutorBuilder<?>> getExecutorBuilders(Settings settings) {
         FixedExecutorBuilder indexing = new FixedExecutorBuilder(settings, Rollup.TASK_THREAD_POOL_NAME,
-                4, 100, "xpack.rollup.task_thread_pool", false);
+            1, -1, "xpack.rollup.task_thread_pool", false);
 
         return Collections.singletonList(indexing);
     }

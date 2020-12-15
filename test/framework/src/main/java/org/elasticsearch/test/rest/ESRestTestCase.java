98401a515d27649a51561163df460672a6f2fceb
Rory Hunter <pugnascotia@users.noreply.github.com>
2020-12-15 10:26:14 +0000

    Migrate logstash system index to be auto-created (#66190)
    Part of #61656. Auto-create the .logstash index using the system index
    infrastructure.

--- test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
@@ -1337,8 +1337,6 @@ public abstract class ESRestTestCase extends ESTestCase {
         }
         switch (name) {
             case ".watches":
-            case "logstash-index-template":
-            case ".logstash-management":
             case "security_audit_log":
             case ".slm-history":
             case ".async-search":

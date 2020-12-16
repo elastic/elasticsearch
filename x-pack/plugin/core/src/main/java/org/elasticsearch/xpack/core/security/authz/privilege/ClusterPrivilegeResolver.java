78420cdf20f65ece7595593067a339bee8c02577
BigPandaToo <lyudmila.fokina@elastic.co>
2020-12-16 12:18:05 +0100

    Rename read_ingest_pipelines to read_pipeline
    Renaming read_ingest_pipelines to read_pipelines
    
    Related: #48028

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/privilege/ClusterPrivilegeResolver.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/privilege/ClusterPrivilegeResolver.java
@@ -169,7 +169,7 @@ public class ClusterPrivilegeResolver {
         MANAGE_WATCHER,
         MANAGE_IDX_TEMPLATES,
         MANAGE_INGEST_PIPELINES,
-        READ_INGEST_PIPELINES,
+        READ_PIPELINE,
         TRANSPORT_CLIENT,
         MANAGE_SECURITY,
         MANAGE_SAML,

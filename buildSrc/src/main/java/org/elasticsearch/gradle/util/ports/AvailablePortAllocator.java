db779a6dcde3560f1f7621a79f8f0d701e8750a0
Rene Groeschke <rene@elastic.co>
2020-12-17 09:30:25 +0100

    Adjust port ranges for tests and test fixtures (#66470) (#66488)
    * Adjust port ranges for tests and test fixtures
    * Do not use general ephermal port range

--- buildSrc/src/main/java/org/elasticsearch/gradle/util/ports/AvailablePortAllocator.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/util/ports/AvailablePortAllocator.java
@@ -23,67 +23,25 @@ import org.gradle.internal.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-public class AvailablePortAllocator implements PortAllocator {
-    public static final int MIN_PRIVATE_PORT = 49152;
-    public static final int MAX_PRIVATE_PORT = 65535;
-    public static final int DEFAULT_RANGE_SIZE = 100;
+public class AvailablePortAllocator {
+    public static final int MIN_PRIVATE_PORT = 10300;
+    public static final int MAX_PRIVATE_PORT = 13300;
+    public static final int DEFAULT_RANGE_SIZE = 10;
 
     private final List<ReservedPortRange> reservations = new ArrayList<ReservedPortRange>();
 
-    private final Lock lock = new ReentrantLock();
-
     private ReservedPortRangeFactory portRangeFactory = new DefaultReservedPortRangeFactory();
-
-    @Override
-    public int assignPort() {
-        try {
-            lock.lock();
-            return reservePort();
-        } finally {
-            lock.unlock();
-        }
-    }
 
     protected Pair<Integer, Integer> getNextPortRange(int rangeNumber) {
         int startPort = MIN_PRIVATE_PORT + (rangeNumber * DEFAULT_RANGE_SIZE);
         int endPort = startPort + DEFAULT_RANGE_SIZE - 1;
+        if (endPort > MAX_PRIVATE_PORT) {
+            throw new IllegalStateException(
+                "Available port ranges have exceeded Range from " + MIN_PRIVATE_PORT + " to " + MAX_PRIVATE_PORT
+            );
+        }
         return Pair.of(startPort, endPort);
-    }
-
-    @Override
-    public void releasePort(int port) {
-        try {
-            lock.lock();
-            for (int i = 0; i < reservations.size(); i++) {
-                ReservedPortRange range = reservations.get(i);
-                if (range.getAllocated().contains(port)) {
-                    range.deallocate(port);
-                    if (reservations.size() > 1 && range.getAllocated().isEmpty()) {
-                        releaseRange(range);
-                    }
-                }
-            }
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    private int reservePort() {
-        while (true) {
-            for (int i = 0; i < reservations.size(); i++) {
-                ReservedPortRange range = reservations.get(i);
-                int port = range.allocate();
-                if (port > 0) {
-                    return port;
-                }
-            }
-
-            // if we couldn't allocate a port from the existing reserved port ranges, get another range
-            reservePortRange();
-        }
     }
 
     public ReservedPortRange reservePortRange() {
@@ -91,14 +49,5 @@ public class AvailablePortAllocator implements PortAllocator {
         ReservedPortRange range = portRangeFactory.getReservedPortRange(portRange.getLeft(), portRange.getRight());
         reservations.add(range);
         return range;
-    }
-
-    private void releaseRange(ReservedPortRange range) {
-        try {
-            lock.lock();
-            reservations.remove(range);
-        } finally {
-            lock.unlock();
-        }
     }
 }

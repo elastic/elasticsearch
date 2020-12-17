d40937beccd15adf3b468a504d09fa7d1d337547
Benjamin Trent <ben.w.trent@gmail.com>
2020-12-17 10:12:19 -0500

    [7.x] [ML] [Deprecation] add deprecation check for job model snapshots that need upgraded (#66062) (#66475)
    * [ML] [Deprecation] add deprecation check for job model snapshots that need upgraded (#66062)
    
    This adds checks that verify that machine learning anomaly job model snapshots support the required minimal version.
    
    If any are not the required version, directions are given to either delete the model snapshot, or utilize the _upgrade API.
    
    relates: https://github.com/elastic/elasticsearch/issues/64154

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/job/persistence/JobResultsProvider.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/job/persistence/JobResultsProvider.java
@@ -1079,6 +1079,11 @@ public class JobResultsProvider {
 
         FieldSortBuilder sb = new FieldSortBuilder(sortField)
                 .order(sortDescending ? SortOrder.DESC : SortOrder.ASC);
+        // `min_version` might not be present in very early snapshots.
+        // Consequently, we should treat it as being at least from 6.3.0 or before
+        if (sortField.equals(ModelSnapshot.MIN_VERSION.getPreferredName())) {
+            sb.missing(Version.fromString("6.3.0"));
+        }
 
         String indexName = AnomalyDetectorsIndex.jobResultsAliasedName(jobId);
         LOGGER.trace("ES API CALL: search all model snapshots from index {} sort ascending {} with filter after sort from {} size {}",

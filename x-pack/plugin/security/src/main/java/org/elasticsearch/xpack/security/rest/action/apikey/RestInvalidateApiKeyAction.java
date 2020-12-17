b0f91816ae97aedd3afd477177e4cb2b0984fde9
Yang Wang <yang.wang@elastic.co>
2020-12-17 15:37:26 +1100

    Restore location info for deprecated field

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/rest/action/apikey/RestInvalidateApiKeyAction.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/rest/action/apikey/RestInvalidateApiKeyAction.java
@@ -31,7 +31,7 @@ import static org.elasticsearch.rest.RestRequest.Method.DELETE;
  * Rest action to invalidate one or more API keys
  */
 public final class RestInvalidateApiKeyAction extends ApiKeyBaseRestHandler {
-    static final ConstructingObjectParser<InvalidateApiKeyRequest, Void> PARSER = new ConstructingObjectParser<>(null,
+    static final ConstructingObjectParser<InvalidateApiKeyRequest, Void> PARSER = new ConstructingObjectParser<>("invalidate_api_key",
             a -> {
                 return new InvalidateApiKeyRequest((String) a[0], (String) a[1], (String) a[2], (String) a[3],
                     (a[4] == null) ? false : (Boolean) a[4],

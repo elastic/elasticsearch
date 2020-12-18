445c850648437c6b7cb8aad381de2e9bf0ab1126
William Brafford <williamrandolphbrafford@gmail.com>
2020-12-18 17:33:09 -0500

    Add deprecation warning for removed strict_duplicate_detection setting (#63500)
    * Add deprecation warning for removed setting
    
    The setting es.xcontent.strict_duplicate_detection was removed in 7.0
    (see #34588). It was an undocumented setting, but enough users relied
    on it that its absence is causing problems during upgrades. This commit
    adds a message to the deprecation log if this setting is present at
    startup.
    
    * Spotless formatting
    
    * Avoid forbidden API
    
    * Reset between custom config tests
    
    * Spotless apply
    
    * Access system properties consistently
    
    * Revert changes to ConfigurationTests setup
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- server/src/main/java/org/elasticsearch/bootstrap/Bootstrap.java
+++ server/src/main/java/org/elasticsearch/bootstrap/Bootstrap.java
@@ -364,6 +364,14 @@ final class Bootstrap {
                 System.getProperty("java.home"));
             DeprecationLogger.getLogger(Bootstrap.class).deprecate("java_version_11_required", message);
         }
+        if (BootstrapInfo.getSystemProperties().get("es.xcontent.strict_duplicate_detection") != null) {
+            final String message = String.format(
+                Locale.ROOT,
+                "The Java option es.xcontent.strict_duplicate_detection is set to [%s]; " +
+                    "this option is deprecated and non-functional and should be removed from Java configuration.",
+                BootstrapInfo.getSystemProperties().get("es.xcontent.strict_duplicate_detection"));
+            DeprecationLogger.getLogger(Bootstrap.class).deprecate("strict_duplicate_detection_setting_removed", message);
+        }
         if (environment.pidFile() != null) {
             try {
                 PidFile.create(environment.pidFile(), true);

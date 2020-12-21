3a2122df0f426d051ef394b5ec86b06014feada5
Rene Groeschke <rene@elastic.co>
2020-12-21 11:10:45 +0100

    Ensure we verify versioned dir for es bwc builds (#66590) (#66673)
    Update according integ tests

--- buildSrc/src/main/java/org/elasticsearch/gradle/internal/InternalDistributionBwcSetupPlugin.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/internal/InternalDistributionBwcSetupPlugin.java
@@ -215,7 +215,9 @@ public class InternalDistributionBwcSetupPlugin implements InternalPlugin {
         String bwcTaskName = buildBwcTaskName(projectName);
         bwcSetupExtension.bwcTask(bwcTaskName, c -> {
             boolean useNativeExpanded = projectArtifact.expandedDistDir != null;
-            File expectedOutputFile = useNativeExpanded ? projectArtifact.expandedDistDir : projectArtifact.distFile;
+            File expectedOutputFile = useNativeExpanded
+                ? new File(projectArtifact.expandedDistDir, "elasticsearch-" + bwcVersion.get() + "-SNAPSHOT")
+                : projectArtifact.distFile;
             c.getInputs().file(new File(project.getBuildDir(), "refspec"));
             if (useNativeExpanded) {
                 c.getOutputs().dir(expectedOutputFile);
@@ -281,7 +283,7 @@ public class InternalDistributionBwcSetupPlugin implements InternalPlugin {
                         + "."
                         + extension
                 ),
-                expandedDistDirSupport ? new File(checkoutDir, baseDir + "/" + name + "/build/install/") : null
+                expandedDistDirSupport ? new File(checkoutDir, baseDir + "/" + name + "/build/install") : null
             );
         }
 

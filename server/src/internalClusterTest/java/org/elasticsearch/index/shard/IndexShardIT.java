d45bfd85f4ef81b57736f0233c7cc15df5d51a6f
David Turner <david.turner@elastic.co>
2021-01-05 17:58:30 +0000

    Make InternalClusterInfoService async
    This commit reworks the InternalClusterInfoService to run
    asynchronously, using timeouts on the stats requests instead of
    implementing its own blocking timeouts. It also improves the logging of
    failures by identifying the nodes that failed or timed out. Finally it
    ensures that only a single refresh is running at once, enqueueing later
    refresh requests to run immediately after the current refresh is
    finished rather than racing them against each other.
    
    Backport of #66993

--- server/src/internalClusterTest/java/org/elasticsearch/index/shard/IndexShardIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/index/shard/IndexShardIT.java
@@ -32,6 +32,7 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.ActiveShardCount;
 import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.cluster.ClusterInfoService;
+import org.elasticsearch.cluster.ClusterInfoServiceUtils;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.InternalClusterInfoService;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
@@ -272,7 +273,7 @@ public class IndexShardIT extends ESSingleNodeTestCase {
         }
         ensureGreen("test");
         InternalClusterInfoService clusterInfoService = (InternalClusterInfoService) getInstanceFromNode(ClusterInfoService.class);
-        clusterInfoService.refresh();
+        ClusterInfoServiceUtils.refresh(clusterInfoService);
         ClusterState state = getInstanceFromNode(ClusterService.class).state();
         Long test = clusterInfoService.getClusterInfo().getShardSize(state.getRoutingTable().index("test")
             .getShards().get(0).primaryShard());

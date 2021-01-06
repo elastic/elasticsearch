a131f67a8245d05ed49bf8ffae2f67f41ffe1995
Martijn van Groningen <martijn.v.groningen@gmail.com>
2021-01-06 13:45:00 +0100

    removed unused import

--- server/src/main/java/org/elasticsearch/cluster/metadata/IndexAbstraction.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/IndexAbstraction.java
@@ -21,7 +21,6 @@ package org.elasticsearch.cluster.metadata;
 import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.Strings;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;

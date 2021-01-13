ac7cf01292b421f4f0f3b1e30e56359fd0384bb0
Rory Hunter <roryhunter2@gmail.com>
2021-01-13 15:34:30 +0000

    Checkstyle

--- server/src/main/java/org/elasticsearch/common/logging/ESJsonLayout.java
+++ server/src/main/java/org/elasticsearch/common/logging/ESJsonLayout.java
@@ -49,7 +49,8 @@ import java.util.Set;
  * <li>level - INFO, WARN etc</li>
  * <li>component - logger name, most of the times class name</li>
  * <li>elasticsearch.cluster.name - taken from sys:es.logs.cluster_name system property because it is always set</li>
- * <li>elasticsearch.node.name - taken from NodeNamePatternConverter, as it can be set in runtime as hostname when not set in elasticsearch.yml</li>
+ * <li>elasticsearch.node.name - taken from NodeNamePatternConverter, as it can be set in runtime as hostname when not set in
+ * elasticsearch.yml</li>
  * <li>elasticsearch.node_and_cluster_id - in json as node.id and cluster.uuid - taken from NodeIdConverter and present
  * once clusterStateUpdate is first received</li>
  * <li>message - a json escaped message. Multiline messages will be converted to single line with new line explicitly

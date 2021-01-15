5f9f8656b5f54ad6d4f57d73f5d24b4c7bacd09b
Luca Cavanna <javanna@users.noreply.github.com>
2021-01-15 17:08:18 +0100

    checkstyle

--- server/src/main/java/org/elasticsearch/cluster/metadata/MetadataMappingService.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/MetadataMappingService.java
@@ -21,7 +21,6 @@ package org.elasticsearch.cluster.metadata;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.indices.mapping.put.PutMappingClusterStateUpdateRequest;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
@@ -39,7 +38,6 @@ import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.core.internal.io.IOUtils;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.index.IndexService;
 import org.elasticsearch.index.mapper.DocumentMapper;
 import org.elasticsearch.index.mapper.MapperService;
 import org.elasticsearch.index.mapper.MapperService.MergeReason;

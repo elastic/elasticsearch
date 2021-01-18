6cc85a316e69a137c4b3e16d7547ec4777ffd0d1
Armin Braun <me@obrown.io>
2021-01-18 14:10:37 +0100

    Fix Inconsistent RepositoryData on First Repository Mount
    Add mechanism that forces tracking the repository generation in the
    cluster state before it is first used to avoid races between the first
    write of repository data and the next operation (e.g. when doing
    concurrent snapshot creation right after mounting a repository) in
    extreme corner cases.
    
    Closes #66663
    Closes #67199

--- server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
+++ server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
@@ -1370,14 +1370,26 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
         }
     }
 
+    // Listener used to ensure that repository data is only initialized once in the cluster state by #initializeRepoGenerationTracking
     private PlainListenableActionFuture<RepositoryData> repoDataInitialized;
 
+    /**
+     * Method used to set the current repository generation in the cluster state's {@link RepositoryMetadata} to the latest generation that
+     * can be physically found in the repository before passing the latest {@link RepositoryData} to the given listener.
+     * This ensures that operations using {@link #executeConsistentStateUpdate} right after mounting a fresh repository will have a
+     * consistent view of the {@link RepositoryData} before any data has been written to the repository.
+     *
+     * @param listener listener to resolve with new repository data
+     */
     private void initializeRepoGenerationTracking(ActionListener<RepositoryData> listener) {
         synchronized (this) {
             if (repoDataInitialized == null) {
+                logger.trace("[{}] initializing repository generation in cluster state", metadata.name());
                 repoDataInitialized = PlainListenableActionFuture.newListenableFuture();
                 repoDataInitialized.addListener(listener);
-                Consumer<Exception> onFailure = e -> {
+                final Consumer<Exception> onFailure = e -> {
+                    logger.warn(new ParameterizedMessage("[{}] Exception when initializing repository generation in cluster state",
+                            metadata.name()), e);
                     final ActionListener<RepositoryData> existingListener;
                     synchronized (BlobStoreRepository.this) {
                         existingListener = repoDataInitialized;
@@ -1392,8 +1404,13 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
                                     @Override
                                     public ClusterState execute(ClusterState currentState) {
                                         RepositoryMetadata metadata = getRepoMetadata(currentState);
-                                        assert metadata.generation() == RepositoryData.UNKNOWN_REPO_GEN :
-                                                "Found unexpected initialized repo metadata [" + metadata + "]";
+                                        // No update to the repository generation should have occurred concurrently in general except for
+                                        // extreme corner cases like failing over to an older version master node and back to the current
+                                        // node concurrently
+                                        if (metadata.generation() != RepositoryData.UNKNOWN_REPO_GEN) {
+                                            throw new RepositoryException(
+                                                    metadata.name(), "Found unexpected initialized repo metadata [" + metadata + "]");
+                                        }
                                         return ClusterState.builder(currentState)
                                                 .metadata(Metadata.builder(currentState.getMetadata()).putCustom(
                                                         RepositoriesMetadata.TYPE,
@@ -1409,6 +1426,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
 
                                     @Override
                                     public void clusterStateProcessed(String source, ClusterState oldState, ClusterState newState) {
+                                        // Resolve listeners on generic pool since some callbacks for repository data do additional IO
                                         threadPool.generic().execute(() -> {
                                             final ActionListener<RepositoryData> existingListener;
                                             synchronized (BlobStoreRepository.this) {

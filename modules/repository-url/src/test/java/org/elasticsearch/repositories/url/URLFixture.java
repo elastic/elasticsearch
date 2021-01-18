ad238056c00ab2c9a960d276982431304c4cc42f
Rene Groeschke <rene@elastic.co>
2021-01-18 12:18:43 +0100

    Fix repository dir setup and apply review feedback

--- modules/repository-url/src/test/java/org/elasticsearch/repositories/url/URLFixture.java
+++ modules/repository-url/src/test/java/org/elasticsearch/repositories/url/URLFixture.java
@@ -52,11 +52,11 @@ public class URLFixture extends AbstractHttpFixture {
         }
         String workingDirectory = args[1];
         if(Files.exists(dir(workingDirectory)) == false) {
-            throw new IllegalArgumentException("Configured working direcotry " + workingDirectory + " does not exist");
+            throw new IllegalArgumentException("Configured working directory " + workingDirectory + " does not exist");
         }
-        String repositoryDirectory = args[1];
+        String repositoryDirectory = args[2];
         if(Files.exists(dir(repositoryDirectory)) == false) {
-            throw new IllegalArgumentException("Configured repository direcotry " + repositoryDirectory + " does not exist");
+            throw new IllegalArgumentException("Configured repository directory " + repositoryDirectory + " does not exist");
         }
         final URLFixture fixture = new URLFixture(Integer.parseInt(args[0]), workingDirectory, repositoryDirectory);
         fixture.listen(InetAddress.getByName("0.0.0.0"), false);

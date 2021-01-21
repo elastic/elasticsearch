2f2b690025666fdf52d097afd75cc162e70f3da9
Costin Leau <costin@users.noreply.github.com>
2021-01-21 15:02:02 +0200

    QL: Refactor FunctionRegistry to make it pluggable (#67761)
    Break the FunctionRegistry monolith into a common QL base and a SQL
    specific registry that handles aspects such as distinct and extract.
    In the process clean-up the names and signature of internal interfaces.
    Most of the semantics were preserved however the error messages were
    slightly tweaked to make them more readable - this shouldn't be a
    problem as they are being used internally mainly in test assertions.

--- x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/expression/function/SqlFunctionRegistryTests.java
+++ x-pack/plugin/sql/src/test/java/org/elasticsearch/xpack/sql/expression/function/SqlFunctionRegistryTests.java
@@ -11,7 +11,6 @@ import org.elasticsearch.xpack.ql.ParsingException;
 import org.elasticsearch.xpack.ql.expression.Expression;
 import org.elasticsearch.xpack.ql.expression.function.FunctionDefinition;
 import org.elasticsearch.xpack.ql.expression.function.FunctionRegistry;
-import org.elasticsearch.xpack.ql.expression.function.FunctionRegistryTests;
 import org.elasticsearch.xpack.ql.expression.function.FunctionRegistryTests.DummyFunction;
 import org.elasticsearch.xpack.ql.expression.function.UnresolvedFunction;
 import org.elasticsearch.xpack.ql.session.Configuration;
@@ -20,9 +19,11 @@ import org.elasticsearch.xpack.ql.tree.Source;
 import java.time.ZoneId;
 
 import static org.elasticsearch.xpack.ql.TestUtils.randomConfiguration;
-import static org.elasticsearch.xpack.ql.expression.function.FunctionRegistry.def;
+import static org.elasticsearch.xpack.ql.expression.function.FunctionRegistryTests.defineDummyNoArgFunction;
+import static org.elasticsearch.xpack.ql.expression.function.FunctionRegistryTests.defineDummyUnaryFunction;
 import static org.elasticsearch.xpack.ql.expression.function.FunctionRegistryTests.uf;
 import static org.elasticsearch.xpack.ql.expression.function.FunctionResolutionStrategy.DEFAULT;
+import static org.elasticsearch.xpack.sql.expression.function.SqlFunctionRegistry.def;
 import static org.elasticsearch.xpack.sql.expression.function.SqlFunctionResolution.DISTINCT;
 import static org.elasticsearch.xpack.sql.expression.function.SqlFunctionResolution.EXTRACT;
 import static org.hamcrest.Matchers.endsWith;
@@ -32,13 +33,13 @@ public class SqlFunctionRegistryTests extends ESTestCase {
 
     public void testNoArgFunction() {
         UnresolvedFunction ur = uf(DEFAULT);
-        FunctionRegistry r = new FunctionRegistry(def(DummyFunction.class, DummyFunction::new, "DUMMY_FUNCTION"));
+        FunctionRegistry r = new SqlFunctionRegistry(defineDummyNoArgFunction());
         FunctionDefinition def = r.resolveFunction(ur.name());
 
         // Distinct isn't supported
         ParsingException e = expectThrows(ParsingException.class, () ->
             uf(DISTINCT).buildResolved(randomConfiguration(), def));
-        assertThat(e.getMessage(), endsWith("does not support DISTINCT yet it was specified"));
+        assertThat(e.getMessage(), endsWith("Cannot use DISTINCT on non-SQL function DUMMY_FUNCTION()"));
 
         // Any children aren't supported
         e = expectThrows(ParsingException.class, () ->
@@ -48,16 +49,13 @@ public class SqlFunctionRegistryTests extends ESTestCase {
 
     public void testUnaryFunction() {
         UnresolvedFunction ur = uf(DEFAULT, mock(Expression.class));
-        FunctionRegistry r = new FunctionRegistry(def(DummyFunction.class, (Source l, Expression e) -> {
-            assertSame(e, ur.children().get(0));
-            return new DummyFunction(l);
-        }, "DUMMY_FUNCTION"));
+        FunctionRegistry r = new SqlFunctionRegistry(defineDummyUnaryFunction(ur));
         FunctionDefinition def = r.resolveFunction(ur.name());
 
         // Distinct isn't supported
         ParsingException e = expectThrows(ParsingException.class, () ->
             uf(DISTINCT, mock(Expression.class)).buildResolved(randomConfiguration(), def));
-        assertThat(e.getMessage(), endsWith("does not support DISTINCT yet it was specified"));
+        assertThat(e.getMessage(), endsWith("Cannot use DISTINCT on non-SQL function DUMMY_FUNCTION()"));
 
         // No children aren't supported
         e = expectThrows(ParsingException.class, () ->
@@ -73,15 +71,17 @@ public class SqlFunctionRegistryTests extends ESTestCase {
     public void testUnaryDistinctAwareFunction() {
         boolean urIsDistinct = randomBoolean();
         UnresolvedFunction ur = uf(urIsDistinct ? DISTINCT : DEFAULT, mock(Expression.class));
-        FunctionRegistry r = new FunctionRegistry(
-            def(FunctionRegistryTests.DummyFunction.class, (Source l, Expression e, boolean distinct) -> {
-                assertEquals(urIsDistinct, distinct);
-                assertSame(e, ur.children().get(0));
-                return new FunctionRegistryTests.DummyFunction(l);
-            }, "DUMMY_FUNCTION"));
+        FunctionDefinition definition = def(DummyFunction.class, (Source l, Expression e, Boolean distinct) -> {
+            assertEquals(urIsDistinct, distinct);
+            assertSame(e, ur.children().get(0));
+            return new DummyFunction(l);
+        }, "DUMMY_FUNCTION");
+        FunctionRegistry r = new SqlFunctionRegistry(definition);
         FunctionDefinition def = r.resolveFunction(ur.name());
         assertEquals(ur.source(), ur.buildResolved(randomConfiguration(), def).source());
-        assertFalse(def.extractViable());
+
+        assertEquals(SqlFunctionDefinition.class, def.getClass());
+        assertFalse(((SqlFunctionDefinition) def).extractViable());
 
         // No children aren't supported
         ParsingException e = expectThrows(ParsingException.class, () ->
@@ -96,21 +96,23 @@ public class SqlFunctionRegistryTests extends ESTestCase {
 
     public void testDateTimeFunction() {
         boolean urIsExtract = randomBoolean();
-        UnresolvedFunction ur = uf(urIsExtract ? EXTRACT : DEFAULT, mock(Expression.class));
+        Expression exprMock = mock(Expression.class);
+        UnresolvedFunction ur = uf(urIsExtract ? EXTRACT : DEFAULT, exprMock);
         ZoneId providedTimeZone = randomZone().normalized();
         Configuration providedConfiguration = randomConfiguration(providedTimeZone);
-        FunctionRegistry r = new FunctionRegistry(def(FunctionRegistryTests.DummyFunction.class, (Source l, Expression e, ZoneId zi) -> {
+        FunctionRegistry r = new SqlFunctionRegistry(def(DummyFunction.class, (Source l, Expression e, ZoneId zi) -> {
             assertEquals(providedTimeZone, zi);
             assertSame(e, ur.children().get(0));
-            return new FunctionRegistryTests.DummyFunction(l);
+            return new DummyFunction(l);
         }, "DUMMY_FUNCTION"));
         FunctionDefinition def = r.resolveFunction(ur.name());
         assertEquals(ur.source(), ur.buildResolved(providedConfiguration, def).source());
-        assertTrue(def.extractViable());
+        assertEquals(SqlFunctionDefinition.class, def.getClass());
+        assertTrue(((SqlFunctionDefinition) def).extractViable());
 
         // Distinct isn't supported
         ParsingException e = expectThrows(ParsingException.class, () ->
-            uf(DISTINCT, mock(Expression.class)).buildResolved(randomConfiguration(), def));
+            uf(DISTINCT, exprMock).buildResolved(providedConfiguration, def));
         assertThat(e.getMessage(), endsWith("does not support DISTINCT yet it was specified"));
 
         // No children aren't supported
@@ -120,7 +122,7 @@ public class SqlFunctionRegistryTests extends ESTestCase {
 
         // Multiple children aren't supported
         e = expectThrows(ParsingException.class, () ->
-            uf(DEFAULT, mock(Expression.class), mock(Expression.class)).buildResolved(randomConfiguration(), def));
+            uf(DEFAULT, exprMock, exprMock).buildResolved(randomConfiguration(), def));
         assertThat(e.getMessage(), endsWith("expects exactly one argument"));
     }
 }

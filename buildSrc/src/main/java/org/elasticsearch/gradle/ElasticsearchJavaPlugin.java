c3194c0dcb1d0d671046dee274c91d12d89bf81c
Mark Vieira <portugee@gmail.com>
2021-01-26 10:46:27 -0800

    Remove unused import

--- buildSrc/src/main/java/org/elasticsearch/gradle/ElasticsearchJavaPlugin.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/ElasticsearchJavaPlugin.java
@@ -55,7 +55,6 @@ import java.io.File;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 import static org.elasticsearch.gradle.util.Util.toStringable;
 

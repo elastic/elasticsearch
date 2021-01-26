a1ade03056ec68dda75bf795c46f105823c40e3f
Alan Woodward <romseygeek@apache.org>
2021-01-26 11:45:11 +0000

    precommit

--- server/src/main/java/org/elasticsearch/index/search/MatchQuery.java
+++ server/src/main/java/org/elasticsearch/index/search/MatchQuery.java
@@ -61,7 +61,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import java.util.function.Supplier;
 
 import static org.elasticsearch.common.lucene.search.Queries.newLenientFieldQuery;

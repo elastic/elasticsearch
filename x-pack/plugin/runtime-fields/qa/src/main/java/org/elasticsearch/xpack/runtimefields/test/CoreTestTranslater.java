0cd8e5085e6831d28a606aa5e0eed4ccfa426dd8
Luca Cavanna <javanna@users.noreply.github.com>
2021-01-27 14:45:43 +0100

    spotless

--- x-pack/plugin/runtime-fields/qa/src/main/java/org/elasticsearch/xpack/runtimefields/test/CoreTestTranslater.java
+++ x-pack/plugin/runtime-fields/qa/src/main/java/org/elasticsearch/xpack/runtimefields/test/CoreTestTranslater.java
@@ -31,10 +31,8 @@ import org.elasticsearch.test.rest.yaml.section.SetupSection;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;

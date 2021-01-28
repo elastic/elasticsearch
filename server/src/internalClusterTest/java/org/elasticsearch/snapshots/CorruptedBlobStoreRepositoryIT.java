7c2d66654c4cf532221039ea056fa477b9b3a96d
Christoph Büscher <cbuescher@posteo.de>
2021-01-28 11:19:40 +0100

    Mute CorruptedBlobStoreRepositoryIT#testMountCorruptedRepositoryData

--- server/src/internalClusterTest/java/org/elasticsearch/snapshots/CorruptedBlobStoreRepositoryIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/snapshots/CorruptedBlobStoreRepositoryIT.java
@@ -296,6 +296,7 @@ public class CorruptedBlobStoreRepositoryIT extends AbstractSnapshotIntegTestCas
         }
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/67696")
     public void testMountCorruptedRepositoryData() throws Exception {
         disableRepoConsistencyCheck("This test intentionally corrupts the repository contents");
         Client client = client();

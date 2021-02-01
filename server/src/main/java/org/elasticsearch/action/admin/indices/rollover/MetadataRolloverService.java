f933f9cb10a13df5624587f4aea30832796be4e2
Armin Braun <me@obrown.io>
2021-02-01 13:29:56 +0100

    Fix Snapshot + DataStream RollOver Bugs (#68258) (#68277)
    There were a number of issues around data streams rolling over during
    a snapshot that are fixed in this PR:
    
    1. If partial snapshots cause a data stream to not be fully snapshotted
    because an index gets concurrently deleted we must not add it to the
    resulting cluster state (otherwise we trip assertions on either snapshot itself
    or a later restore, depending on whether or not the complete global state is
    snapshotted).
    2. When a non-partial snapshot is running we must not allow a datastream rollover,
    otherwise we cannot finish the datastream snapshot correctly because the newly
    created write index has not become part of the snapshot.
    3. If any part of a datastream fails snapshotting or gets concurrently deleted during
    a partial snapshot we must not add it to `SnapshotInfo` same as we do not add concurrently
    deleted indices to it.

--- server/src/main/java/org/elasticsearch/action/admin/indices/rollover/MetadataRolloverService.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/rollover/MetadataRolloverService.java
@@ -40,6 +40,8 @@ import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.Index;
+import org.elasticsearch.snapshots.SnapshotInProgressException;
+import org.elasticsearch.snapshots.SnapshotsService;
 import org.elasticsearch.threadpool.ThreadPool;
 
 import java.util.Arrays;
@@ -211,6 +213,18 @@ public class MetadataRolloverService {
     private RolloverResult rolloverDataStream(ClusterState currentState, IndexAbstraction.DataStream dataStream, String dataStreamName,
                                               CreateIndexRequest createIndexRequest, List<Condition<?>> metConditions,
                                               boolean silent, boolean onlyValidate) throws Exception {
+
+        if (SnapshotsService.snapshottingDataStreams(currentState, Collections.singleton(dataStream.getName())).isEmpty() == false) {
+            // we can't roll over the snapshot concurrently because the snapshot contains the indices that existed when it was started but
+            // the cluster metadata of when it completes so the new write index would not exist in the snapshot if there was a concurrent
+            // rollover
+            throw new SnapshotInProgressException(
+                    "Cannot roll over data stream that is being snapshotted: "
+                            + dataStream.getName()
+                            + ". Try again after snapshot finishes or cancel the currently running snapshot."
+            );
+        }
+
         lookupTemplateForDataStream(dataStreamName, currentState.metadata());
 
         final Version minNodeVersion = currentState.nodes().getMinNodeVersion();

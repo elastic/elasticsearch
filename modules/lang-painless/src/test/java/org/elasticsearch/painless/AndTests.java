58a0a79ab805fb137efac9de6695b142280369a8
Mark Vieira <portugee@gmail.com>
2021-02-02 14:01:18 -0800

    Update license headers

--- modules/lang-painless/src/test/java/org/elasticsearch/painless/AndTests.java
+++ modules/lang-painless/src/test/java/org/elasticsearch/painless/AndTests.java
@@ -1,27 +1,16 @@
 /*
- * Licensed to Elasticsearch under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 package org.elasticsearch.painless;
 
 /** Tests for and operator across all types */
 public class AndTests extends ScriptTestCase {
-    
+
     public void testBasics() throws Exception {
         assertEquals(5 & 3, exec("return 5 & 3;"));
         assertEquals(5 & 3L, exec("return 5 & 3L;"));
@@ -52,7 +41,7 @@ public class AndTests extends ScriptTestCase {
         assertEquals(5L & -12L, exec("return 5L & -12L;"));
         assertEquals(7L & 15L & 3L, exec("return 7L & 15L & 3L;"));
     }
-    
+
     public void testIllegal() throws Exception {
         expectScriptThrows(ClassCastException.class, () -> {
             exec("float x = (float)4; int y = 1; return x & y");
@@ -61,7 +50,7 @@ public class AndTests extends ScriptTestCase {
             exec("double x = (double)4; int y = 1; return x & y");
         });
     }
-    
+
     public void testDef() {
         expectScriptThrows(ClassCastException.class, () -> {
             exec("def x = (float)4; def y = (byte)1; return x & y");
@@ -110,7 +99,7 @@ public class AndTests extends ScriptTestCase {
         assertEquals(false, exec("def x = false; def y = true; return x & y"));
         assertEquals(false, exec("def x = false; def y = false; return x & y"));
     }
-    
+
     public void testDefTypedLHS() {
         expectScriptThrows(ClassCastException.class, () -> {
             exec("float x = (float)4; def y = (byte)1; return x & y");
@@ -159,7 +148,7 @@ public class AndTests extends ScriptTestCase {
         assertEquals(false, exec("boolean x = false; def y = true; return x & y"));
         assertEquals(false, exec("boolean x = false; def y = false; return x & y"));
     }
-    
+
     public void testDefTypedRHS() {
         expectScriptThrows(ClassCastException.class, () -> {
             exec("def x = (float)4; byte y = (byte)1; return x & y");
@@ -208,7 +197,7 @@ public class AndTests extends ScriptTestCase {
         assertEquals(false, exec("def x = false; boolean y = true; return x & y"));
         assertEquals(false, exec("def x = false; boolean y = false; return x & y"));
     }
-    
+
     public void testCompoundAssignment() {
         // boolean
         assertEquals(true, exec("boolean x = true; x &= true; return x;"));
@@ -231,7 +220,7 @@ public class AndTests extends ScriptTestCase {
         // long
         assertEquals((long) (13 & 14), exec("long x = 13L; x &= 14; return x;"));
     }
-    
+
     public void testBogusCompoundAssignment() {
         expectScriptThrows(ClassCastException.class, () -> {
             exec("float x = 4; int y = 1; x &= y");
@@ -246,7 +235,7 @@ public class AndTests extends ScriptTestCase {
             exec("int x = 4; double y = 1; x &= y");
         });
     }
-    
+
     public void testDefCompoundAssignment() {
         // boolean
         assertEquals(true, exec("def x = true; x &= true; return x;"));
@@ -269,7 +258,7 @@ public class AndTests extends ScriptTestCase {
         // long
         assertEquals((long) (13 & 14), exec("def x = 13L; x &= 14; return x;"));
     }
-    
+
     public void testDefBogusCompoundAssignment() {
         expectScriptThrows(ClassCastException.class, () -> {
             exec("def x = 4F; int y = 1; x &= y");

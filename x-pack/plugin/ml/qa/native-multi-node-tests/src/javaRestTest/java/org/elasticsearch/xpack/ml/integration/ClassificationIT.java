a92a647b9f17d1bddf5c707490a19482c273eda3
Mark Vieira <portugee@gmail.com>
2021-02-02 13:41:39 -0800

    Update sources with new SSPL+Elastic-2.0 license headers
    As per the new licensing change for Elasticsearch and Kibana this commit
    moves existing Apache 2.0 licensed source code to the new dual license
    SSPL+Elastic license 2.0. In addition, existing x-pack code now uses
    the new version 2.0 of the Elastic license. Full changes include:
    
     - Updating LICENSE and NOTICE files throughout the code base, as well
       as those packaged in our published artifacts
     - Update IDE integration to now use the new license header on newly
       created source files
     - Remove references to the "OSS" distribution from our documentation
     - Update build time verification checks to no longer allow Apache 2.0
       license header in Elasticsearch source code
     - Replace all existing Apache 2.0 license headers for non-xpack code
       with updated header (vendored code with Apache 2.0 headers obviously
       remains the same).
     - Replace all Elastic license 1.0 headers with new 2.0 header in xpack.

--- x-pack/plugin/ml/qa/native-multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/ml/integration/ClassificationIT.java
+++ x-pack/plugin/ml/qa/native-multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/ml/integration/ClassificationIT.java
@@ -1,7 +1,8 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
 package org.elasticsearch.xpack.ml.integration;
 
@@ -389,7 +390,7 @@ public class ClassificationIT extends MlNativeDataFrameAnalyticsIntegTestCase {
                 sourceIndex,
                 destIndex,
                 null,
-                new Classification(dependentVariable, BoostedTreeParams.builder().build(), null, null, 
+                new Classification(dependentVariable, BoostedTreeParams.builder().build(), null, null,
                 numTopClasses, 50.0, null, null, null));
         putAnalytics(config);
 

ffe104400c8491e7e0d73f8b1229aa3d89b65577
Tim Vernum <tim.vernum@elastic.co>
2021-02-03 18:57:47 +1100

    Fix identification of action type in error message (#68426)
    Some actions that start with "indices:" are actually handled by
    cluster privileges in ES security (e.g. indices:admin/template/*)
    In #60357 and #66900 we added better context information for the
    error messages that are generated when an action is denied, but the
    generation of that message did not correctly classify actions between
    cluster and index level privileges.
    
    This change does 2 things:
    1. It fixes the code that determines whether an action is handled by a
       cluster privilege or an index privilege
    2. Includes the words "cluster" and "index" in the error message so
       that classification is clear to the reader
    
    The latter change is not directly related to the issue being resolved,
    but in the course of fixing the issue it became evident that the
    message lacked clarity because it did not tell the reader what type of
    privilege would be needed to resolve the access denied issue.
    
    Backport of: #68260

--- x-pack/plugin/ilm/qa/with-security/src/javaRestTest/java/org/elasticsearch/xpack/security/PermissionsIT.java
+++ x-pack/plugin/ilm/qa/with-security/src/javaRestTest/java/org/elasticsearch/xpack/security/PermissionsIT.java
@@ -144,8 +144,10 @@ public class PermissionsIT extends ESRestTestCase {
                 assertThat(indexExplain.get("failed_step"), equalTo("wait-for-shard-history-leases"));
                 Map<String, String> stepInfo = (Map<String, String>) indexExplain.get("step_info");
                 assertThat(stepInfo.get("type"), equalTo("security_exception"));
-                assertThat(stepInfo.get("reason"), equalTo("action [indices:monitor/stats] is unauthorized for user [test_ilm]" +
-                    " on indices [not-ilm], this action is granted by the privileges [monitor,manage,all]"));
+                assertThat(stepInfo.get("reason"), equalTo("action [indices:monitor/stats] is unauthorized" +
+                    " for user [test_ilm]" +
+                    " on indices [not-ilm]," +
+                    " this action is granted by the index privileges [monitor,manage,all]"));
             }
         });
     }

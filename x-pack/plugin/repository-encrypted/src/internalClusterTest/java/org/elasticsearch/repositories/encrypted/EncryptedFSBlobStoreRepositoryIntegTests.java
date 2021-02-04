eab1bda48469b5b828eab84028193d8d2f302e40
Armin Braun <me@obrown.io>
2021-02-04 16:23:52 +0100

    Fix Encrypted Repo Test and RepositoriesService Logging (#68469) (#68527)
    This test creates a strange situation when it restarts the
    two master nodes back to back without strong guarantees about
    whether or not the cluster has cleanly formed again. Since we really just want to
    make sure that the master is after the restart is actually running
    with the changed password, it seemed easiest to simply do a full restart rather than the
    current loop.
    
    Also, this commit fixes the logging around repository creation and deletion which made this
    hard to debug and by logging before a CS update has actually been processed would also
    log incorrectly.
    Lastly, I removed the now unnecessary hacks of using the generic pool on a node to fetch
    repository data. This logic is not needed any longer and the repository will now automatically
    use the correct thread under the hood. The hack effectively just adds another possible failure
    mode around node restarts since the generic pool quietly rejects tasks after shutdown.
    
    Closes #67834

--- x-pack/plugin/repository-encrypted/src/internalClusterTest/java/org/elasticsearch/repositories/encrypted/EncryptedFSBlobStoreRepositoryIntegTests.java
+++ x-pack/plugin/repository-encrypted/src/internalClusterTest/java/org/elasticsearch/repositories/encrypted/EncryptedFSBlobStoreRepositoryIntegTests.java
@@ -6,7 +6,6 @@
  */
 package org.elasticsearch.repositories.encrypted;
 
-import org.elasticsearch.action.ActionRunnable;
 import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.common.settings.MockSecureSettings;
 import org.elasticsearch.common.settings.Settings;
@@ -158,9 +157,7 @@ public final class EncryptedFSBlobStoreRepositoryIntegTests extends ESFsBasedRep
         assertThat(
             expectThrows(
                 RepositoryException.class,
-                () -> PlainActionFuture.<RepositoryData, Exception>get(
-                    f -> blobStoreRepository.threadPool().generic().execute(ActionRunnable.wrap(f, blobStoreRepository::getRepositoryData))
-                )
+                () -> PlainActionFuture.<RepositoryData, Exception>get(blobStoreRepository::getRepositoryData)
             ).getMessage(),
             containsString("the encryption metadata in the repository has been corrupted")
         );

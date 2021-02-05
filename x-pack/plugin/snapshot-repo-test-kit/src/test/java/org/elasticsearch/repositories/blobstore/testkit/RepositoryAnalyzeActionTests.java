d433948f23d2406fe55e02f70617fabec99fed98
David Turner <david.turner@elastic.co>
2021-02-05 18:48:28 +0000

    Better tests, corresponding with docs

--- x-pack/plugin/snapshot-repo-test-kit/src/test/java/org/elasticsearch/repositories/blobstore/testkit/RepositoryAnalyzeActionTests.java
+++ x-pack/plugin/snapshot-repo-test-kit/src/test/java/org/elasticsearch/repositories/blobstore/testkit/RepositoryAnalyzeActionTests.java
@@ -11,16 +11,20 @@ import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.test.ESTestCase;
 
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 import static org.hamcrest.Matchers.equalTo;
 
 public class RepositoryAnalyzeActionTests extends ESTestCase {
 
     public void testGetBlobSizesAssertions() {
+        // Just checking that no assertions are thrown
         getBlobSizesOrException(randomLongBetween(1L, Long.MAX_VALUE), randomLongBetween(1L, Long.MAX_VALUE), between(100, 10000));
     }
 
     public void testGetBlobSizesAssertionsSmall() {
+        // Just checking that no assertions are thrown for an exhaustive set of small examples
         for (long maxBlobSize = 1L; maxBlobSize < 50L; maxBlobSize++) {
             for (long maxTotalDataSize = 1L; maxTotalDataSize < 50L; maxTotalDataSize++) {
                 for (int blobCount = 1; blobCount < 25; blobCount++) {
@@ -30,15 +34,7 @@ public class RepositoryAnalyzeActionTests extends ESTestCase {
         }
     }
 
-    private void getBlobSizesOrException(long maxBlobSize, long maxTotalDataSize, int blobCount) {
-        try {
-            getBlobSizes(maxBlobSize, maxTotalDataSize, blobCount);
-        } catch (IllegalArgumentException e) {
-            // ok
-        }
-    }
-
-    public void testGetBlobSizesExample() {
+    public void testGetBlobSizesWithoutRepeats() {
         final int logBlobSize = 10;
         final long blobSize = 1 << logBlobSize;
         final long totalSize = 2047;
@@ -68,6 +64,40 @@ public class RepositoryAnalyzeActionTests extends ESTestCase {
             getBlobSizes(blobSize, totalSize, logBlobSize + 1).stream().mapToLong(l -> l).sorted().toArray(),
             equalTo(new long[] { 2, 4, 8, 16, 32, 64, 128, 128, 256, 256, 1024 })
         );
+    }
+
+    public void testGetBlobSizesDefaultExample() {
+        // The defaults are 10MB max blob size, 1GB max total size, 100 blobs, which gives 4 blobs of each size (either powers of 2 or 10MB)
+        assertThat(
+            RepositoryAnalyzeAction.getBlobSizes(new RepositoryAnalyzeAction.Request("repo")).stream().mapToLong(l -> l).sorted().toArray(),
+            equalTo(
+                LongStream.range(0, 25)
+                    .flatMap(power -> IntStream.range(0, 4).mapToLong(i -> power == 24 ? ByteSizeValue.ofMb(10).getBytes() : 1L << power))
+                    .toArray()
+            )
+        );
+    }
+
+    public void testGetBlobSizesRealisticExample() {
+        // The docs suggest a realistic example needs 2GB max blob size, 1TB max total size, 2000 blobs, which gives 62 or 63 blobs of each
+        // size (powers of 2 up to 2GB)
+        assertThat(
+            getBlobSizes(ByteSizeValue.ofGb(2).getBytes(), ByteSizeValue.ofTb(1).getBytes(), 2000).stream()
+                .mapToLong(l -> l)
+                .sorted()
+                .toArray(),
+            equalTo(
+                LongStream.range(0, 32).flatMap(power -> IntStream.range(0, power < 16 ? 62 : 63).mapToLong(i -> 1L << power)).toArray()
+            )
+        );
+    }
+
+    private static void getBlobSizesOrException(long maxBlobSize, long maxTotalDataSize, int blobCount) {
+        try {
+            getBlobSizes(maxBlobSize, maxTotalDataSize, blobCount);
+        } catch (IllegalArgumentException e) {
+            // ok
+        }
     }
 
     private static List<Long> getBlobSizes(long maxBlobSize, long maxTotalDataSize, int blobCount) {

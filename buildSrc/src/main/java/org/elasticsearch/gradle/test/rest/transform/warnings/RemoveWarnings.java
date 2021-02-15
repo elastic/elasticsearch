19141dd5c847589609aa81040e3912a025c160d2
Jake Landis <jake.landis@elastic.co>
2021-02-15 09:46:02 -0600

    start remove warning

---
+++ buildSrc/src/main/java/org/elasticsearch/gradle/test/rest/transform/warnings/RemoveWarnings.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.gradle.test.rest.transform.warnings;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.elasticsearch.gradle.test.rest.transform.RestTestTransformByParentObject;
+import org.elasticsearch.gradle.test.rest.transform.feature.FeatureInjector;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+//TODO
+//NOTE: this does not remove the features from the setup and/or teardown. while it would be more technically correct to do so, the effort/complexity does not warrant it.
+public class RemoveWarnings implements RestTestTransformByParentObject {
+
+    private static JsonNodeFactory jsonNodeFactory = JsonNodeFactory.withExactBigDecimals(false);
+
+    private final Set<String> warnings;
+
+    /**
+     * @param warnings The allowed warnings to inject
+     */
+    public RemoveWarnings(Set<String> warnings) {
+        this.warnings = warnings;
+    }
+
+    @Override
+    public void transformTest(ObjectNode doNodeParent) {
+        ObjectNode doNodeValue = (ObjectNode) doNodeParent.get(getKeyToFind());
+        ArrayNode arrayWarnings = (ArrayNode) doNodeValue.get("warnings");
+        if (arrayWarnings == null) {
+            arrayWarnings = new ArrayNode(jsonNodeFactory);
+            doNodeValue.set("warnings", arrayWarnings);
+        }
+        List<String> keepWarnings = new ArrayList<>();
+        arrayWarnings.elements().forEachRemaining(warning -> {
+            String warningValue = warning.textValue();
+            if (warnings.contains(warningValue) == false) {
+                keepWarnings.add(warningValue);
+            }
+
+        });
+        arrayWarnings.removeAll();
+        keepWarnings.forEach(arrayWarnings::add);
+    }
+
+    @Override
+    public String getKeyToFind() {
+        return "do";
+    }
+
+}
---
+++ buildSrc/src/test/java/org/elasticsearch/gradle/test/rest/transform/warnings/RemoveWarningsTests.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.gradle.test.rest.transform.warnings;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.elasticsearch.gradle.test.rest.transform.RestTestTransform;
+import org.elasticsearch.gradle.test.rest.transform.feature.InjectFeatureTests;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RemoveWarningsTests extends InjectFeatureTests {
+
+List<String> addWarnings = List.of("added warning");
+
+    /**
+     * test file does not any headers defined
+     */
+    @Test
+    public void testInjectHeadersNoPreExisting() throws Exception {
+        String testName = "/rest/transform/warnings/without_existing_warnings.yml";
+        List<ObjectNode> tests = getTests(testName);
+        validateSetupDoesNotExist(tests);
+        List<ObjectNode> transformedTests = transformTests(tests);
+        printTest(testName, transformedTests);
+        validateSetupAndTearDown(transformedTests);
+      //  validateBodyHasHeaders(transformedTests, addWarnings);
+    }
+
+    /**
+     * test file has preexisting headers
+     */
+    @Test
+    public void testInjectHeadersWithPreExisting() throws Exception {
+        String testName = "/rest/transform/warnings/with_existing_warnings.yml";
+        List<ObjectNode> tests = getTests(testName);
+        validateSetupDoesNotExist(tests);
+    //    validateBodyHasHeaders(tests, Map.of("foo", "bar"));
+        List<ObjectNode> transformedTests = transformTests(tests);
+        printTest(testName, transformedTests);
+        validateSetupAndTearDown(transformedTests);
+    //    validateBodyHasHeaders(tests, Map.of("foo", "bar"));
+        //validateBodyHasHeaders(transformedTests, addWarnings);
+    }
+
+    @Override
+    protected List<String> getKnownFeatures() {
+        return Collections.singletonList("warnings");
+    }
+
+    @Override
+    protected List<RestTestTransform<?>> getTransformations() {
+        return Collections.singletonList(new InjectWarnings(addWarnings));
+    }
+
+    @Override
+    protected boolean getHumanDebug() {
+        return true;
+    }
+
+//    private void validateBodyHasHeaders(List<ObjectNode> tests, Map<String, String> headers) {
+//        tests.forEach(test -> {
+//            Iterator<Map.Entry<String, JsonNode>> testsIterator = test.fields();
+//            while (testsIterator.hasNext()) {
+//                Map.Entry<String, JsonNode> testObject = testsIterator.next();
+//                assertThat(testObject.getValue(), CoreMatchers.instanceOf(ArrayNode.class));
+//                ArrayNode testBody = (ArrayNode) testObject.getValue();
+//                testBody.forEach(arrayObject -> {
+//                    assertThat(arrayObject, CoreMatchers.instanceOf(ObjectNode.class));
+//                    ObjectNode testSection = (ObjectNode) arrayObject;
+//                    if (testSection.get("do") != null) {
+//                        ObjectNode doSection = (ObjectNode) testSection.get("do");
+//                        assertThat(doSection.get("headers"), CoreMatchers.notNullValue());
+//                        ObjectNode headersNode = (ObjectNode) doSection.get("headers");
+//                        LongAdder assertions = new LongAdder();
+//                        headers.forEach((k, v) -> {
+//                            assertThat(headersNode.get(k), CoreMatchers.notNullValue());
+//                            TextNode textNode = (TextNode) headersNode.get(k);
+//                            assertThat(textNode.asText(), CoreMatchers.equalTo(v));
+//                            assertions.increment();
+//                        });
+//                        assertThat(assertions.intValue(), CoreMatchers.equalTo(headers.size()));
+//                    }
+//                });
+//            }
+//        });
+//    }
+}

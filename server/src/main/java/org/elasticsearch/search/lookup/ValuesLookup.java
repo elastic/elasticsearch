35751c18f362929ba623a3229c81c05d665a5669
Alan Woodward <romseygeek@apache.org>
2021-02-17 15:41:24 +0000

    javadocs

--- server/src/main/java/org/elasticsearch/search/lookup/ValuesLookup.java
+++ server/src/main/java/org/elasticsearch/search/lookup/ValuesLookup.java
@@ -14,14 +14,25 @@ import org.elasticsearch.common.xcontent.XContentType;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * Per-document values, from source or formatted doc-values
+ */
 public interface ValuesLookup {
 
+    /**
+     * Returns a SourceLookup positioned on the current document
+     */
     SourceLookup source();
 
+    /**
+     * Returns a LeafDocLookup positioned on the current document
+     */
     LeafDocLookup doc();
 
+    /**
+     * Override the source exposed by a ValuesLookup
+     */
     static ValuesLookup wrapWithSource(ValuesLookup in, Map<String, Object> source, XContentType xContentType) {
-        System.out.println("Building independent source: " + source);
         SourceLookup sourceLookup = new SourceLookup();
         sourceLookup.setSource(source);
         sourceLookup.setSourceContentType(Objects.requireNonNull(xContentType));
@@ -38,6 +49,9 @@ public interface ValuesLookup {
         };
     }
 
+    /**
+     * Returns a source-only implementation of ValuesLookup
+     */
     static ValuesLookup sourceOnly(BytesReference source) {
         SourceLookup sourceLookup = new SourceLookup();
         sourceLookup.setSource(source);
@@ -54,6 +68,9 @@ public interface ValuesLookup {
         };
     }
 
+    /**
+     * Returns a source-only implementation of ValuesLookup
+     */
     static ValuesLookup sourceOnly(Map<String, Object> source) {
         SourceLookup sourceLookup = new SourceLookup();
         sourceLookup.setSource(source);

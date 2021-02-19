6cc9cfa508483a69e1fd8b05d93a9d0c4e5ea671
Nik Everett <nik9000@gmail.com>
2021-02-19 14:17:32 -0500

    Speed up terms agg when alone
    This speeds up the `terms` agg in a very specific case:
    1. It has no child aggregations
    2. It has no parent aggregations
    3. There are no deleted documents
    4. You are not using document level security
    5. There is no top level query
    6. The field has global ordinals
    7. There are less than one thousand distinct terms
    
    That is a lot of restirctions! But the speed up pretty substantial because
    in those cases we can serve the entire aggregation using metadata that
    lucene precomputes while it builds the index. In a real rally track we
    have we get a 92% speed improvement, but the index isn't *that* big:
    
    ```
    | 90th percentile service time | keyword-terms-low-cardinality |     446.031 |     36.7677 | -409.263 |     ms |
    ```
    
    In a rally track with a larger index I ran some tests by hand and the
    aggregation went from 2200ms to 8ms.
    
    Even though there are 7 restrictions on this, I expect it to come into
    play enough to matter. Restriction 6 just means you are aggregating on
    a `keyword` field. Or an `ip`. And its fairly common for `keyword`s to
    have less than a thousand distinct values. Certainly not everywhere, but
    some places.
    
    I expect "cold tier" indices are very very likely not to have deleted
    documents at all. And the optimization works segment by segment - so
    it'll save some time on each segment without deleted documents. But more
    time if the entire index doesn't have any.
    
    The optimization builds on #68871 which translates `terms` aggregations
    against low cardinality fields with global ordinals into a `filters`
    aggregation. This teaches the `filters` aggregation to recognize when
    it can get its results from the index metadata. Rather, it creates the
    infrastructure to make that fairly simple and applies it in the case of
    the queries generated by the terms aggregation.

---
+++ server/src/main/java/org/elasticsearch/search/aggregations/bucket/filter/QueryToFilterAdapter.java
@@ -0,0 +1,343 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.search.aggregations.bucket.filter;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.BulkScorer;
+import org.apache.lucene.search.ConstantScoreQuery;
+import org.apache.lucene.search.IndexOrDocValuesQuery;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.IndexSortSortedNumericDocValuesRangeQuery;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.MatchNoDocsQuery;
+import org.apache.lucene.search.PointRangeQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreMode;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.Weight;
+import org.apache.lucene.util.Bits;
+import org.elasticsearch.common.CheckedSupplier;
+import org.elasticsearch.common.lucene.Lucene;
+
+import java.io.IOException;
+import java.util.function.BiConsumer;
+import java.util.function.IntPredicate;
+
+/**
+ * Adapts a Lucene {@link Query} to the behaviors used be the
+ * {@link FiltersAggregator}. In general we try to delegate to {@linkplain Query}
+ * when we don't have 
+ */
+public abstract class QueryToFilterAdapter {
+    /**
+     * Build a filter against the provided searcher.
+     * <p>
+     * Note: This method rewrites the query against the {@link IndexSearcher}
+     */
+    public static QueryToFilterAdapter build(IndexSearcher searcher, String key, Query query) throws IOException {
+        query = searcher.rewrite(query);
+        if (query instanceof TermQuery) {
+            return new TermFilter(searcher, key, (TermQuery) query);
+        }
+        if (query instanceof MatchAllDocsQuery) {
+            return new MatchAllQueryToFilterAdapter(searcher, key);
+        }
+        if (query instanceof MatchNoDocsQuery) {
+            return new MatchNoneQueryToFilterAdapter(searcher, key);
+        }
+        return new DefaultQueryToFilterAdapter(searcher, key, query);
+    }
+
+    private final IndexSearcher searcher;
+    private final String key;
+
+    private QueryToFilterAdapter(IndexSearcher searcher, String key) {
+        this.searcher = searcher;
+        this.key = key;
+    }
+
+    public final String key() {
+        return key;
+    }
+
+    protected final IndexSearcher searcher() {
+        return searcher;
+    }
+
+    /**
+     * Is it safe to use index metadata like
+     * {@link IndexReader#docFreq} or {@link IndexReader#maxDoc} to count. the
+     * number of matching documents.
+     */
+    protected final boolean countCanUseMetadata(FiltersAggregator.Counter counter, Bits live) {
+        return live == null && counter.docCount.alwaysOne();
+    }
+
+    /**
+     * Make a filter that matches this filter and the provided query.
+     * <p>
+     * Note: This method rewrites the query against the {@link IndexSearcher}.
+     */
+    abstract QueryToFilterAdapter union(Query extraQuery) throws IOException;
+
+    abstract IntPredicate matchingDocIds(LeafReaderContext ctx) throws IOException;
+
+    abstract long count(LeafReaderContext ctx, FiltersAggregator.Counter counter, Bits live) throws IOException;
+
+    /**
+     * Estimate the cost of calling {@code #count} on this leaf.
+     */
+    abstract long estimateCountCost(LeafReaderContext ctx, CheckedSupplier<Boolean, IOException> canUseMetadata) throws IOException;
+
+    /**
+     * Collect profiling information for this filter.
+     */
+    void collectDebugInfo(BiConsumer<String, Object> add) {}
+
+    /**
+     * Special case when the filter can't match anything.
+     */
+    private static class MatchNoneQueryToFilterAdapter extends QueryToFilterAdapter {
+        private MatchNoneQueryToFilterAdapter(IndexSearcher searcher, String key) {
+            super(searcher, key);
+        }
+
+        @Override
+        QueryToFilterAdapter union(Query extraQuery) throws IOException {
+            return this;
+        }
+
+        @Override
+        IntPredicate matchingDocIds(LeafReaderContext ctx) throws IOException {
+            return l -> false;
+        }
+
+        @Override
+        long count(LeafReaderContext ctx, FiltersAggregator.Counter counter, Bits live) throws IOException {
+            return 0;
+        }
+
+        @Override
+        long estimateCountCost(LeafReaderContext ctx, CheckedSupplier<Boolean, IOException> canUseMetadata) throws IOException {
+            return 0;
+        }
+
+        @Override
+        void collectDebugInfo(BiConsumer<String, Object> add) {
+            super.collectDebugInfo(add);
+            add.accept("type", "match_none");
+        }
+    }
+
+    /**
+     * Filter that matches every document.
+     */
+    private static class MatchAllQueryToFilterAdapter extends QueryToFilterAdapter {
+        private int resultsFromMetadata;
+
+        private MatchAllQueryToFilterAdapter(IndexSearcher searcher, String key) {
+            super(searcher, key);
+        }
+
+        @Override
+        QueryToFilterAdapter union(Query extraQuery) throws IOException {
+            return QueryToFilterAdapter.build(searcher(), key(), extraQuery);
+        }
+
+        @Override
+        IntPredicate matchingDocIds(LeafReaderContext ctx) throws IOException {
+            return l -> true;
+        }
+
+        @Override
+        long count(LeafReaderContext ctx, FiltersAggregator.Counter counter, Bits live) throws IOException {
+            if (countCanUseMetadata(counter, live)) {
+                resultsFromMetadata++;
+                return ctx.reader().maxDoc();
+            }
+            new MatchAllDocsQuery().createWeight(searcher(), ScoreMode.COMPLETE_NO_SCORES, 1.0f).bulkScorer(ctx).score(counter, live);
+            return counter.readAndReset(ctx);
+        }
+
+        @Override
+        long estimateCountCost(LeafReaderContext ctx, CheckedSupplier<Boolean, IOException> canUseMetadata) throws IOException {
+            return canUseMetadata.get() ? 0 : ctx.reader().maxDoc();
+        }
+
+        @Override
+        void collectDebugInfo(BiConsumer<String, Object> add) {
+            super.collectDebugInfo(add);
+            add.accept("type", "match_all");
+            add.accept("results_from_metadata", resultsFromMetadata);
+        }
+    }
+
+    /**
+     * Abstract superclass of filters that delegates everything to the query.
+     */
+    private abstract static class CommonQueryToFilterAdapter<Q extends Query> extends QueryToFilterAdapter {
+        private final Q query;
+        private Weight weight;
+        private BulkScorer[] bulkScorers;
+        private int scorersPreparedWhileEstimatingCost;
+
+        private CommonQueryToFilterAdapter(IndexSearcher searcher, String key, Q query) {
+            super(searcher, key);
+            this.query = query;
+        }
+
+        /**
+         * The query we're adapting.
+         * <p>
+         * Subclasses should use this to fetch the query when making query
+         * specific optimizations.
+         */
+        protected Q query() {
+            return query;
+        }
+
+        @Override
+        public QueryToFilterAdapter union(Query extraQuery) throws IOException {
+            extraQuery = searcher().rewrite(extraQuery);
+            if (extraQuery instanceof MatchAllDocsQuery) {
+                return this;
+            }
+            Query unwrappedQuery = unwrap(query);
+            Query unwrappedExtraQuery = unwrap(extraQuery);
+            if (unwrappedQuery instanceof PointRangeQuery && unwrappedExtraQuery instanceof PointRangeQuery) {
+                Query merged = MergedPointRangeQuery.merge((PointRangeQuery) unwrappedQuery, (PointRangeQuery) unwrappedExtraQuery);
+                if (merged != null) {
+                    // Should we rewrap here?
+                    return new DefaultQueryToFilterAdapter(searcher(), key(), merged);
+                }
+            }
+            BooleanQuery.Builder builder = new BooleanQuery.Builder();
+            builder.add(query, BooleanClause.Occur.MUST);
+            builder.add(extraQuery, BooleanClause.Occur.MUST);
+            return new DefaultQueryToFilterAdapter(searcher(), key(), builder.build());
+        }
+
+        private static Query unwrap(Query query) {
+            while (true) {
+                if (query instanceof ConstantScoreQuery) {
+                    query = ((ConstantScoreQuery) query).getQuery();
+                    continue;
+                }
+                if (query instanceof IndexSortSortedNumericDocValuesRangeQuery) {
+                    query = ((IndexSortSortedNumericDocValuesRangeQuery) query).getFallbackQuery();
+                    continue;
+                }
+                if (query instanceof IndexOrDocValuesQuery) {
+                    query = ((IndexOrDocValuesQuery) query).getIndexQuery();
+                    continue;
+                }
+                return query;
+            }
+        }
+
+        @Override
+        @SuppressWarnings("resource")  // Closing the reader is someone else's problem
+        public IntPredicate matchingDocIds(LeafReaderContext ctx) throws IOException {
+            return Lucene.asSequentialAccessBits(ctx.reader().maxDoc(), weight().scorerSupplier(ctx))::get;
+        }
+
+        @Override
+        long count(LeafReaderContext ctx, FiltersAggregator.Counter counter, Bits live) throws IOException {
+            BulkScorer scorer = bulkScorer(ctx, () -> {});
+            if (scorer == null) {
+                // No hits in this segment.
+                return 0;
+            }
+            scorer.score(counter, live);
+            return counter.readAndReset(ctx);
+        }
+
+        @Override
+        long estimateCountCost(LeafReaderContext ctx, CheckedSupplier<Boolean, IOException> canUseMetadata) throws IOException {
+            BulkScorer scorer = bulkScorer(ctx, () -> scorersPreparedWhileEstimatingCost++);
+            if (scorer == null) {
+                // There aren't any matches for this filter in this leaf
+                return 0;
+            }
+            return scorer.cost();
+        }
+
+        private BulkScorer bulkScorer(LeafReaderContext ctx, Runnable onPrepare) throws IOException {
+            if (bulkScorers == null) {
+                bulkScorers = new BulkScorer[searcher().getIndexReader().leaves().size()];
+            }
+            if (bulkScorers[ctx.ord] == null) {
+                onPrepare.run();
+                return bulkScorers[ctx.ord] = weight().bulkScorer(ctx);
+            }
+            return bulkScorers[ctx.ord];
+        }
+
+        private Weight weight() throws IOException {
+            if (weight == null) {
+                weight = searcher().createWeight(query, ScoreMode.COMPLETE_NO_SCORES, 1.0f);
+            }
+            return weight;
+        }
+
+        @Override
+        void collectDebugInfo(BiConsumer<String, Object> add) {
+            super.collectDebugInfo(add);
+            add.accept("query", query.toString());
+            add.accept("scorers_prepared_while_estimating_cost", scorersPreparedWhileEstimatingCost);
+        }
+    }
+
+    private static class DefaultQueryToFilterAdapter extends CommonQueryToFilterAdapter<Query> {
+        private DefaultQueryToFilterAdapter(IndexSearcher searcher, String key, Query query) {
+            super(searcher, key, query);
+        }
+
+        @Override
+        void collectDebugInfo(BiConsumer<String, Object> add) {
+            super.collectDebugInfo(add);
+            add.accept("type", "query");
+        }
+    }
+
+    private static class TermFilter extends CommonQueryToFilterAdapter<TermQuery> {
+        private int resultsFromMetadata;
+
+        private TermFilter(IndexSearcher searcher, String key, TermQuery query) {
+            super(searcher, key, query);
+        }
+
+        @Override
+        long count(LeafReaderContext ctx, FiltersAggregator.Counter counter, Bits live) throws IOException {
+            if (countCanUseMetadata(counter, live)) {
+                resultsFromMetadata++;
+                return ctx.reader().docFreq(query().getTerm());
+            }
+            return super.count(ctx, counter, live);
+        }
+
+        @Override
+        long estimateCountCost(LeafReaderContext ctx, CheckedSupplier<Boolean, IOException> canUseMetadata) throws IOException {
+            if (canUseMetadata.get()) {
+                return 0;
+            }
+            return super.estimateCountCost(ctx, canUseMetadata);
+        }
+
+        @Override
+        void collectDebugInfo(BiConsumer<String, Object> add) {
+            super.collectDebugInfo(add);
+            add.accept("type", "term");
+            add.accept("results_from_metadata", resultsFromMetadata);
+        }
+    }
+}

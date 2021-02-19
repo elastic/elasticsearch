aae7b8bf7ea65b0ecf23a31d656d5ba85849ac1b
Alan Woodward <romseygeek@apache.org>
2021-02-19 15:15:46 +0000

    More test coverage

--- server/src/test/java/org/elasticsearch/index/mapper/GeoShapeFieldTypeTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/GeoShapeFieldTypeTests.java
@@ -9,7 +9,6 @@
 package org.elasticsearch.index.mapper;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -17,21 +16,33 @@ public class GeoShapeFieldTypeTests extends FieldTypeTestCase {
 
     public void testFetchSourceValue() throws IOException {
         MappedFieldType mapper
-            = new GeoShapeFieldMapper.Builder("field", false, true).build(new ContentPath()).fieldType();
+            = new GeoShapeFieldMapper.Builder("field", true, true).build(new ContentPath()).fieldType();
 
         Map<String, Object> jsonLineString = Map.of("type", "LineString", "coordinates",
             List.of(List.of(42.0, 27.1), List.of(30.0, 50.0)));
         Map<String, Object> jsonPoint = Map.of("type", "Point", "coordinates", List.of(14.0, 15.0));
+        Map<String, Object> jsonMalformed = Map.of("type", "Point", "coordinates", "foo");
         String wktLineString = "LINESTRING (42.0 27.1, 30.0 50.0)";
         String wktPoint = "POINT (14.0 15.0)";
+        String wktMalformed = "POINT foo";
 
         // Test a single shape in geojson format.
         Object sourceValue = jsonLineString;
         assertEquals(List.of(jsonLineString), fetchSourceValue(mapper, sourceValue, null));
         assertEquals(List.of(wktLineString), fetchSourceValue(mapper, sourceValue, "wkt"));
 
+        // Test a malformed single shape in geojson format
+        sourceValue = jsonMalformed;
+        assertEquals(List.of(), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(List.of(), fetchSourceValue(mapper, sourceValue, "wkt"));
+
         // Test a list of shapes in geojson format.
         sourceValue = List.of(jsonLineString, jsonPoint);
+        assertEquals(List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
+
+        // Test a list of shapes including one malformed in geojson format
+        sourceValue = List.of(jsonLineString, jsonMalformed, jsonPoint);
         assertEquals(List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
         assertEquals(List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
 
@@ -40,18 +51,19 @@ public class GeoShapeFieldTypeTests extends FieldTypeTestCase {
         assertEquals(List.of(jsonLineString), fetchSourceValue(mapper, sourceValue, null));
         assertEquals(List.of(wktLineString), fetchSourceValue(mapper, sourceValue, "wkt"));
 
+        // Test a single malformed shape in wkt format
+        sourceValue = wktMalformed;
+        assertEquals(List.of(), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(List.of(), fetchSourceValue(mapper, sourceValue, "wkt"));
+
         // Test a list of shapes in wkt format.
         sourceValue = List.of(wktLineString, wktPoint);
         assertEquals(List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
         assertEquals(List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
-    }
 
-    public void testFetchMalformedValue() throws IOException {
-
-        MappedFieldType mapper
-            = new GeoShapeFieldMapper.Builder("field", true, true).build(new ContentPath()).fieldType();
-
-        Map<String, Object> jsonPoint = Map.of("type", "Point", "coordinates", "foo");
-        assertEquals(Collections.emptyList(), fetchSourceValue(mapper, jsonPoint, null));
+        // Test a list of shapes including one malformed in wkt format
+        sourceValue = List.of(wktLineString, wktMalformed, wktPoint);
+        assertEquals(List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
     }
 }

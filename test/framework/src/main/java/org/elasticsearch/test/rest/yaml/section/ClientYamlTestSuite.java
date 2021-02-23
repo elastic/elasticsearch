02ef8db7541a79b9255682e4bfba7bd6ce0cc4ca
Jake Landis <jake.landis@elastic.co>
2021-02-23 17:14:33 -0600

    initial draft

--- test/framework/src/main/java/org/elasticsearch/test/rest/yaml/section/ClientYamlTestSuite.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/yaml/section/ClientYamlTestSuite.java
@@ -152,6 +152,14 @@ public class ClientYamlTestSuite {
                 "without a corresponding [\"skip\": \"features\": \"warnings\"] so runners that do not support the [warnings] " +
                 "section can skip the test at line [" + section.getLocation().lineNumber + "]");
 
+        errors = sections.stream().filter(section -> section instanceof DoSection)
+            .map(section -> (DoSection) section)
+            .filter(section -> false == section.getExpectedWarningHeaders().isEmpty())
+            .filter(section -> false == hasSkipFeature("warnings_regex", testSection, setupSection, teardownSection))
+            .map(section -> "attempted to add a [do] with a [warnings_regex] section " +
+                "without a corresponding [\"skip\": \"features\": \"warnings_regex\"] so runners that do not support the [warnings_regex] "+
+                "section can skip the test at line [" + section.getLocation().lineNumber + "]");
+
         errors = Stream.concat(errors, sections.stream().filter(section -> section instanceof DoSection)
                 .map(section -> (DoSection) section)
                 .filter(section -> false == section.getAllowedWarningHeaders().isEmpty())
@@ -159,6 +167,14 @@ public class ClientYamlTestSuite {
                 .map(section -> "attempted to add a [do] with a [allowed_warnings] section " +
                     "without a corresponding [\"skip\": \"features\": \"allowed_warnings\"] so runners that do not " +
                     "support the [allowed_warnings] section can skip the test at line [" + section.getLocation().lineNumber + "]"));
+
+        errors = Stream.concat(errors, sections.stream().filter(section -> section instanceof DoSection)
+            .map(section -> (DoSection) section)
+            .filter(section -> false == section.getAllowedWarningHeaders().isEmpty())
+            .filter(section -> false == hasSkipFeature("allowed_warnings_regex", testSection, setupSection, teardownSection))
+            .map(section -> "attempted to add a [do] with a [allowed_warnings_regex] section " +
+                "without a corresponding [\"skip\": \"features\": \"allowed_warnings_regex\"] so runners that do not " +
+                "support the [allowed_warnings_regex] section can skip the test at line [" + section.getLocation().lineNumber + "]"));
 
         errors = Stream.concat(errors, sections.stream().filter(section -> section instanceof DoSection)
             .map(section -> (DoSection) section)

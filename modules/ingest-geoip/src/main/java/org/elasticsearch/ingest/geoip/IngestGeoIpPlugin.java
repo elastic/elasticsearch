2ba3e929e7be16ed100bb9139a4b1ea25d6fd4a1
Przemko Robakowski <przemko.robakowski@elastic.co>
2021-02-23 19:41:18 +0100

    GeoIP database downloader (#68424)
    This change adds component that will download new GeoIP databases from infra service
    New databases are downloaded in chunks and stored in .geoip_databases index
    Downloads are verified against MD5 checksum provided by the server
    Current state of all stored databases is stored in cluster state in persistent task state
    
    Relates to #68920

--- modules/ingest-geoip/src/main/java/org/elasticsearch/ingest/geoip/IngestGeoIpPlugin.java
+++ modules/ingest-geoip/src/main/java/org/elasticsearch/ingest/geoip/IngestGeoIpPlugin.java
@@ -9,17 +9,29 @@
 package org.elasticsearch.ingest.geoip;
 
 import org.apache.lucene.util.SetOnce;
+import org.elasticsearch.Version;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.ParseField;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.settings.Setting;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsModule;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.env.NodeEnvironment;
+import org.elasticsearch.indices.SystemIndexDescriptor;
 import org.elasticsearch.ingest.Processor;
+import org.elasticsearch.persistent.PersistentTaskParams;
+import org.elasticsearch.persistent.PersistentTaskState;
+import org.elasticsearch.persistent.PersistentTasksExecutor;
 import org.elasticsearch.plugins.IngestPlugin;
+import org.elasticsearch.plugins.PersistentTaskPlugin;
 import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.plugins.SystemIndexPlugin;
 import org.elasticsearch.repositories.RepositoriesService;
 import org.elasticsearch.script.ScriptService;
 import org.elasticsearch.threadpool.ThreadPool;
@@ -28,13 +40,21 @@ import org.elasticsearch.watcher.ResourceWatcherService;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
-public class IngestGeoIpPlugin extends Plugin implements IngestPlugin, Closeable {
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import static org.elasticsearch.index.mapper.MapperService.SINGLE_MAPPING_NAME;
+import static org.elasticsearch.ingest.geoip.GeoIpDownloader.DATABASES_INDEX;
+import static org.elasticsearch.ingest.geoip.GeoIpDownloader.GEOIP_DOWNLOADER;
+import static org.elasticsearch.ingest.geoip.GeoIpDownloader.GEOIP_V2_FEATURE_FLAG_ENABLED;
+
+public class IngestGeoIpPlugin extends Plugin implements IngestPlugin, SystemIndexPlugin, Closeable, PersistentTaskPlugin {
     public static final Setting<Long> CACHE_SIZE =
         Setting.longSetting("ingest.geoip.cache_size", 1000, 0, Setting.Property.NodeScope);
 
@@ -44,7 +64,13 @@ public class IngestGeoIpPlugin extends Plugin implements IngestPlugin, Closeable
 
     @Override
     public List<Setting<?>> getSettings() {
-        return List.of(CACHE_SIZE);
+        List<Setting<?>> settings = new ArrayList<>(Arrays.asList(CACHE_SIZE,
+            GeoIpDownloader.ENDPOINT_SETTING,
+            GeoIpDownloader.POLL_INTERVAL_SETTING));
+        if (GEOIP_V2_FEATURE_FLAG_ENABLED) {
+            settings.add(GeoIpDownloaderTaskExecutor.ENABLED_SETTING);
+        }
+        return settings;
     }
 
     @Override
@@ -72,7 +98,7 @@ public class IngestGeoIpPlugin extends Plugin implements IngestPlugin, Closeable
             localDatabases.get().initialize(resourceWatcherService);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
-        }
+                }
         return List.of();
     }
 
@@ -81,4 +107,83 @@ public class IngestGeoIpPlugin extends Plugin implements IngestPlugin, Closeable
         localDatabases.get().close();
     }
 
+    @Override
+    public List<PersistentTasksExecutor<?>> getPersistentTasksExecutor(ClusterService clusterService, ThreadPool threadPool,
+                                                                       Client client, SettingsModule settingsModule,
+                                                                       IndexNameExpressionResolver expressionResolver) {
+        if (GEOIP_V2_FEATURE_FLAG_ENABLED) {
+            Settings settings = settingsModule.getSettings();
+            return List.of(new GeoIpDownloaderTaskExecutor(client, new HttpClient(), clusterService, threadPool, settings));
+        } else {
+            return List.of();
+        }
+    }
+
+    @Override
+    public List<NamedXContentRegistry.Entry> getNamedXContent() {
+        return List.of(new NamedXContentRegistry.Entry(PersistentTaskParams.class, new ParseField(GEOIP_DOWNLOADER),
+                GeoIpTaskParams::fromXContent),
+            new NamedXContentRegistry.Entry(PersistentTaskState.class, new ParseField(GEOIP_DOWNLOADER), GeoIpTaskState::fromXContent));
+    }
+
+    @Override
+    public List<NamedWriteableRegistry.Entry> getNamedWriteables() {
+        return List.of(new NamedWriteableRegistry.Entry(PersistentTaskState.class, GEOIP_DOWNLOADER, GeoIpTaskState::new),
+            new NamedWriteableRegistry.Entry(PersistentTaskParams.class, GEOIP_DOWNLOADER, GeoIpTaskParams::new));
+    }
+
+    @Override
+    public Collection<SystemIndexDescriptor> getSystemIndexDescriptors(Settings settings) {
+        SystemIndexDescriptor geoipDatabasesIndex = SystemIndexDescriptor.builder()
+            .setIndexPattern(DATABASES_INDEX)
+            .setDescription("GeoIP databases")
+            .setMappings(mappings())
+            .setSettings(Settings.builder()
+                .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, 1)
+                .put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 0)
+                .put(IndexMetadata.SETTING_AUTO_EXPAND_REPLICAS, "0-1")
+                .build())
+            .setOrigin("geoip")
+            .setVersionMetaKey("version")
+            .setPrimaryIndex(DATABASES_INDEX)
+            .build();
+        return Collections.singleton(geoipDatabasesIndex);
+    }
+
+    @Override
+    public String getFeatureName() {
+        return "geoip";
+    }
+
+    @Override
+    public String getFeatureDescription() {
+        return "Manages data related to GeoIP database downloader";
+    }
+
+    private static XContentBuilder mappings() {
+        try {
+            return jsonBuilder()
+                .startObject()
+                .startObject(SINGLE_MAPPING_NAME)
+                .startObject("_meta")
+                .field("version", Version.CURRENT)
+                .endObject()
+                .field("dynamic", "strict")
+                .startObject("properties")
+                .startObject("name")
+                .field("type", "keyword")
+                .endObject()
+                .startObject("chunk")
+                .field("type", "integer")
+                .endObject()
+                .startObject("data")
+                .field("type", "binary")
+                .endObject()
+                .endObject()
+                .endObject()
+                .endObject();
+        } catch (IOException e) {
+            throw new UncheckedIOException("Failed to build mappings for " + DATABASES_INDEX, e);
+        }
+    }
 }

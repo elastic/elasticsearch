9d1e21fd722cdd5da6db41081f832bc04523e7ee
Przemko Robakowski <przemko.robakowski@elastic.co>
2021-02-24 10:54:54 +0100

    Add ToS query parameter to GeoIP downloader (#69495)
    This change adds query parameter confirming that we accept ToS of GeoIP database service provided by Infra.
    It also changes integration test to use lower timeout when using local fixture.
    
    Relates to #68920

--- modules/ingest-geoip/src/internalClusterTest/java/org/elasticsearch/ingest/geoip/GeoIpDownloaderIT.java
+++ modules/ingest-geoip/src/internalClusterTest/java/org/elasticsearch/ingest/geoip/GeoIpDownloaderIT.java
@@ -46,12 +46,13 @@ import static java.nio.file.StandardOpenOption.WRITE;
 @ClusterScope(scope = Scope.TEST, maxNumDataNodes = 1)
 public class GeoIpDownloaderIT extends AbstractGeoIpIT {
 
+    private static final String ENDPOINT = System.getProperty("geoip_endpoint");
+
     @Override
     protected Settings nodeSettings(int nodeOrdinal) {
         Settings.Builder settings = Settings.builder().put(super.nodeSettings(nodeOrdinal));
-        String endpoint = System.getProperty("geoip_endpoint");
-        if (endpoint != null) {
-            settings.put(GeoIpDownloader.ENDPOINT_SETTING.getKey(), endpoint);
+        if (ENDPOINT != null) {
+            settings.put(GeoIpDownloader.ENDPOINT_SETTING.getKey(), ENDPOINT);
         }
         return settings.build();
     }
@@ -66,6 +67,8 @@ public class GeoIpDownloaderIT extends AbstractGeoIpIT {
     }
 
     public void testGeoIpDatabasesDownload() throws Exception {
+        // use short wait for local fixture, longer when we hit real service
+        int waitTime = ENDPOINT == null ? 120 : 10;
         ClusterUpdateSettingsResponse settingsResponse = client().admin().cluster()
             .prepareUpdateSettings()
             .setPersistentSettings(Settings.builder().put(GeoIpDownloaderTaskExecutor.ENABLED_SETTING.getKey(), true))
@@ -77,7 +80,7 @@ public class GeoIpDownloaderIT extends AbstractGeoIpIT {
             GeoIpTaskState state = (GeoIpTaskState) task.getState();
             assertNotNull(state);
             assertEquals(Set.of("GeoLite2-ASN.mmdb", "GeoLite2-City.mmdb", "GeoLite2-Country.mmdb"), state.getDatabases().keySet());
-        }, 2, TimeUnit.MINUTES);
+        }, waitTime, TimeUnit.SECONDS);
 
         GeoIpTaskState state = (GeoIpTaskState) getTask().getState();
         for (String id : org.elasticsearch.common.collect.List.of("GeoLite2-ASN.mmdb", "GeoLite2-City.mmdb", "GeoLite2-Country.mmdb")) {

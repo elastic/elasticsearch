bd88d8db0938bbd58a9ea11ca0dd6370b54750c5
David Turner <david.turner@elastic.co>
2021-03-01 12:45:24 +0000

    Don't subclass ElasticsearchException, no need to register

--- server/src/main/java/org/elasticsearch/transport/TransportNotReadyException.java
+++ server/src/main/java/org/elasticsearch/transport/TransportNotReadyException.java
@@ -10,9 +10,9 @@ package org.elasticsearch.transport;
 
 /**
  * Exception indicating that the {@link TransportService} received a request before it was ready to handle it, so the request should be
- * rejected and the connection closed.
+ * rejected and the connection closed. Never goes over the wire, so it's just a {@link RuntimeException}.
  */
-public class TransportNotReadyException extends TransportException {
+public class TransportNotReadyException extends RuntimeException {
     public TransportNotReadyException() {
         super("transport not ready yet to handle incoming requests");
     }

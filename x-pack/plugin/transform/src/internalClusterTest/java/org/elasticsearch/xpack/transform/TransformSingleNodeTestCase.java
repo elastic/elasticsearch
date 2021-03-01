eccb73e13c002204375b380a22060b4c17e99e90
David Roberts <dave.roberts@elastic.co>
2021-03-01 13:36:14 +0000

    [Transform] Fix transform internal index test (#69679)
    Fixes an internal cluster test that was migrated from being
    a REST test by the system indices project.
    
    The REST test framework waits for pending tasks between tests
    whereas the internal cluster test framework does not.  The
    particular test that was migrated requires this functionality,
    as updating a transform triggers an async side effect: writing
    an audit message.
    
    This PR does not add the wait for pending tasks step to all
    internal cluster tests.  Clearly it is not required for most,
    and could slow down tests that don't require it.  The new
    method can be called from elsewhere or moved up the class
    hierarchy in the future if it is needed elsewhere in the
    future.
    
    Backport of #69617

--- x-pack/plugin/transform/src/internalClusterTest/java/org/elasticsearch/xpack/transform/TransformSingleNodeTestCase.java
+++ x-pack/plugin/transform/src/internalClusterTest/java/org/elasticsearch/xpack/transform/TransformSingleNodeTestCase.java
@@ -9,9 +9,13 @@ package org.elasticsearch.xpack.transform;
 
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.LatchedActionListener;
+import org.elasticsearch.action.admin.cluster.node.tasks.list.ListTasksAction;
+import org.elasticsearch.action.admin.cluster.node.tasks.list.ListTasksRequest;
+import org.elasticsearch.action.admin.cluster.node.tasks.list.ListTasksResponse;
 import org.elasticsearch.common.CheckedConsumer;
 import org.elasticsearch.index.reindex.ReindexPlugin;
 import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.tasks.TaskInfo;
 import org.elasticsearch.test.ESSingleNodeTestCase;
 
 import java.util.Collection;
@@ -55,4 +59,27 @@ public abstract class TransformSingleNodeTestCase extends ESSingleNodeTestCase {
         assertTrue("timed out after 20s", latch.await(20, TimeUnit.SECONDS));
     }
 
+    protected void waitForPendingTasks() throws Exception {
+        assertBusy(() -> {
+            try {
+                final ListTasksRequest request = new ListTasksRequest().setDetailed(true);
+                final ListTasksResponse response = client().execute(ListTasksAction.INSTANCE, request).actionGet();
+
+                // Check to see if there are outstanding tasks; we exclude the list task itself.
+                StringBuilder tasksListString = new StringBuilder();
+                int activeTasks = 0;
+                for (TaskInfo task : response.getTasks()) {
+                    if (task.getAction().startsWith(ListTasksAction.NAME)) {
+                        continue;
+                    }
+                    ++activeTasks;
+                    tasksListString.append(task.toString());
+                    tasksListString.append('\n');
+                }
+                assertEquals(activeTasks + " active tasks found:\n" + tasksListString, 0, activeTasks);
+            } catch (final Exception e) {
+                throw new AssertionError("error getting active tasks list", e);
+            }
+        }, 30L, TimeUnit.SECONDS);
+    }
 }

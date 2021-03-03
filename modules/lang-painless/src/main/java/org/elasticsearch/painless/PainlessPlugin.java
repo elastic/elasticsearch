d9edab524e77f15bd0a4686930d644830a927685
Jack Conradson <osjdconrad@gmail.com>
2021-03-03 12:06:10 -0800

    Add stub REST action for Painless suggestions

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/PainlessPlugin.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/PainlessPlugin.java
@@ -27,6 +27,7 @@ import org.elasticsearch.env.Environment;
 import org.elasticsearch.env.NodeEnvironment;
 import org.elasticsearch.painless.action.PainlessContextAction;
 import org.elasticsearch.painless.action.PainlessExecuteAction;
+import org.elasticsearch.painless.action.PainlessSuggestionsAction;
 import org.elasticsearch.painless.spi.PainlessExtension;
 import org.elasticsearch.painless.spi.Whitelist;
 import org.elasticsearch.painless.spi.WhitelistLoader;
@@ -178,6 +179,7 @@ public final class PainlessPlugin extends Plugin implements ScriptPlugin, Extens
         List<ActionHandler<? extends ActionRequest, ? extends ActionResponse>> actions = new ArrayList<>();
         actions.add(new ActionHandler<>(PainlessExecuteAction.INSTANCE, PainlessExecuteAction.TransportAction.class));
         actions.add(new ActionHandler<>(PainlessContextAction.INSTANCE, PainlessContextAction.TransportAction.class));
+        actions.add(new ActionHandler<>(PainlessSuggestionsAction.INSTANCE, PainlessSuggestionsAction.TransportAction.class));
         return actions;
     }
 
@@ -189,6 +191,7 @@ public final class PainlessPlugin extends Plugin implements ScriptPlugin, Extens
         List<RestHandler> handlers = new ArrayList<>();
         handlers.add(new PainlessExecuteAction.RestAction());
         handlers.add(new PainlessContextAction.RestAction());
+        handlers.add(new PainlessSuggestionsAction.RestAction());
         return handlers;
     }
 }

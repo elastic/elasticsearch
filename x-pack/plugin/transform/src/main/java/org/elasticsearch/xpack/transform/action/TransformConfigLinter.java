e272aa5570ae2001f92928388d90ace22301c200
Przemyslaw Witek <przemyslaw.witek@elastic.co>
2021-03-05 08:36:20 +0100

    Add docs for TransformConfigLinter

--- x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/action/TransformConfigLinter.java
+++ x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/action/TransformConfigLinter.java
@@ -16,8 +16,29 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * {@link TransformConfigLinter} analyzes transform config in order to find issues that might be of interest to the transform user.
+ *
+ * These issues are reported as warnings. They do *not* fail the request (like validations do) but instead provide hints that may be then
+ * logged or displayed in the UI.
+ */
 final class TransformConfigLinter {
 
+    private static final String GROUP_BY_FIELDS_ARE_RUNTIME_FIELDS =
+        "all the group-by fields are script-based runtime fields, this transform might run slowly, please check your configuration.";
+    private static final String SYNC_FIELD_IS_RUNTIME_FIELD =
+        "sync time field is a script-based runtime field, this transform might run slowly, please check your configuration.";
+    private static final String NOT_OPTIMIZED =
+        "could not find any optimizations for continuous execution, this transform might run slowly, please check your configuration.";
+
+    /**
+     * Gets the list of warnings for the given config.
+     *
+     * @param function transform function
+     * @param sourceConfig source config
+     * @param syncConfig synchronization config when the transform is continuous or {@code null} otherwise
+     * @return list of warnings
+     */
     static List<String> getWarnings(Function function, SourceConfig sourceConfig, SyncConfig syncConfig) {
         if (syncConfig == null) {
             return Collections.emptyList();
@@ -27,20 +48,14 @@ final class TransformConfigLinter {
         Map<String, Object> scriptBasedRuntimeFieldNames = sourceConfig.getScriptBasedRuntimeMappings();
         List<String> performanceCriticalFields = function.getPerformanceCriticalFields();
         if (performanceCriticalFields.stream().allMatch(scriptBasedRuntimeFieldNames::containsKey)) {
-            String message = "all the group-by fields are script-based runtime fields, "
-                + "this transform might run slowly, please check your configuration.";
-            warnings.add(message);
+            warnings.add(GROUP_BY_FIELDS_ARE_RUNTIME_FIELDS);
         }
         if (scriptBasedRuntimeFieldNames.containsKey(syncConfig.getField())) {
-            String message = "sync time field is a script-based runtime field, "
-                + "this transform might run slowly, please check your configuration.";
-            warnings.add(message);
+            warnings.add(SYNC_FIELD_IS_RUNTIME_FIELD);
         }
         Function.ChangeCollector changeCollector = function.buildChangeCollector(syncConfig.getField());
         if (changeCollector.isOptimized() == false) {
-            String message = "could not find any optimizations for continuous execution, "
-                + "this transform might run slowly, please check your configuration.";
-            warnings.add(message);
+            warnings.add(NOT_OPTIMIZED);
         }
         return warnings;
     }

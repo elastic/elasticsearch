7b01378bca010267ca5f2b3b28f8d5788c3be7a2
Martijn van Groningen <martijn.v.groningen@gmail.com>
2021-03-08 14:50:42 +0100

    Adjust RolloverAction serialization of maxPrimaryShardSize during an upgrade.
    If node doesn't support maxPrimaryShardSize then serialize maxPrimaryShardSize as maxSize.
    This should fix a problematic situation if an older node doesn't support maxPrimaryShardSize
    and this is the only condition specified then the older node ends up with a instance without
    any conditions. This could lead to upgrade failures, new nodes not able to start because
    local cluster state can't be read.
    
    Relates to #69918

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/RolloverAction.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/RolloverAction.java
@@ -96,12 +96,17 @@ public class RolloverAction implements LifecycleAction {
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         boolean hasMaxSize = maxSize != null;
-        out.writeBoolean(hasMaxSize);
+        boolean hasMaxPrimaryShardSize = maxPrimaryShardSize != null;
         if (hasMaxSize) {
+            out.writeBoolean(true);
             maxSize.writeTo(out);
+        } else if (hasMaxPrimaryShardSize && out.getVersion().before(Version.V_7_13_0)) {
+            out.writeBoolean(true);
+            maxPrimaryShardSize.writeTo(out);
+        } else {
+            out.writeBoolean(false);
         }
         if (out.getVersion().onOrAfter(Version.V_7_13_0)) {
-            boolean hasMaxPrimaryShardSize = maxPrimaryShardSize != null;
             out.writeBoolean(hasMaxPrimaryShardSize);
             if (hasMaxPrimaryShardSize) {
                 maxPrimaryShardSize.writeTo(out);

d8ef26ad26234141f09a21465727001b7bbbf5b9
Christoph Büscher <cbuescher@posteo.de>
2021-03-09 17:15:27 +0100

    Add earlier validation for some SearchSourceBuilder settings (#69548)
    Currently we check several search parameters for illegal values in their
    SearchSourceBuilder setters, e.g. negative values throw IAE for: `size`,
    `terminateAfter` and `trackTotalHits`.
    
    The validation in the builder setters are used when parsing the above as rest
    request parameters, however we currently don't check values when parsing them
    from the search request body. This leads to builders with invalid parameters
    that sometimes get caucht later (e.g. a negative size is triggering an
    IllegalArgumentException in TotalHitCountCollector), but we should validate and
    throw errors early.
    
    This PR changes the parsing in SearchSourceBuilder to use the setters, adds
    tests and also adds a deprecation for allowing a size parameter of -1, currently
    meaning an "unset" value.
    
    Closes #54958

--- server/src/main/java/org/elasticsearch/rest/action/search/RestSearchAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/search/RestSearchAction.java
@@ -19,6 +19,8 @@ import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.common.Booleans;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.logging.DeprecationCategory;
+import org.elasticsearch.common.logging.DeprecationLogger;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.rest.BaseRestHandler;
@@ -57,6 +59,8 @@ public class RestSearchAction extends BaseRestHandler {
     public static final String TOTAL_HITS_AS_INT_PARAM = "rest_total_hits_as_int";
     public static final String TYPED_KEYS_PARAM = "typed_keys";
     private static final Set<String> RESPONSE_PARAMS;
+
+    private static final DeprecationLogger deprecationLogger = DeprecationLogger.getLogger(RestSearchAction.class);
 
     static {
         final Set<String> responseParams = new HashSet<>(Arrays.asList(TYPED_KEYS_PARAM, TOTAL_HITS_AS_INT_PARAM));
@@ -190,9 +194,18 @@ public class RestSearchAction extends BaseRestHandler {
         if (request.hasParam("from")) {
             searchSourceBuilder.from(request.paramAsInt("from", 0));
         }
-        int size = request.paramAsInt("size", -1);
-        if (size != -1) {
-            setSize.accept(size);
+        if (request.hasParam("size")) {
+            int size = request.paramAsInt("size", -1);
+            if (size != -1) {
+                setSize.accept(size);
+            } else {
+                deprecationLogger.deprecate(
+                    DeprecationCategory.API,
+                    "search-api-size-1",
+                    "Using search size of -1 is deprecated and will be removed in future versions. Instead, don't use the `size` parameter "
+                        + "if you don't want to set it explicitely."
+                );
+            }
         }
 
         if (request.hasParam("explain")) {
@@ -208,13 +221,8 @@ public class RestSearchAction extends BaseRestHandler {
             searchSourceBuilder.timeout(request.paramAsTime("timeout", null));
         }
         if (request.hasParam("terminate_after")) {
-            int terminateAfter = request.paramAsInt("terminate_after",
-                    SearchContext.DEFAULT_TERMINATE_AFTER);
-            if (terminateAfter < 0) {
-                throw new IllegalArgumentException("terminateAfter must be > 0");
-            } else if (terminateAfter > 0) {
-                searchSourceBuilder.terminateAfter(terminateAfter);
-            }
+            int terminateAfter = request.paramAsInt("terminate_after", SearchContext.DEFAULT_TERMINATE_AFTER);
+            searchSourceBuilder.terminateAfter(terminateAfter);
         }
 
         StoredFieldsContext storedFieldsContext =

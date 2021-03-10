ee50b4f74ae3fd4d407bd373f9c877451040190e
Hendrik Muhs <hendrik.muhs@elastic.co>
2021-03-10 10:04:39 +0100

    split into 2 tests

--- x-pack/plugin/transform/src/internalClusterTest/java/org/elasticsearch/xpack/transform/integration/TransformNoTransformNodeIT.java
+++ x-pack/plugin/transform/src/internalClusterTest/java/org/elasticsearch/xpack/transform/integration/TransformNoTransformNodeIT.java
@@ -19,22 +19,23 @@ public class TransformNoTransformNodeIT extends TransformSingleNodeTestCase {
         return Settings.builder().put(NodeRoleSettings.NODE_ROLES_SETTING.getKey(), "master, data, ingest").build();
     }
 
-    public void testWarning() {
-        if (randomBoolean()) {
-            GetTransformStatsAction.Request getTransformStatsRequest = new GetTransformStatsAction.Request("_all");
-            GetTransformStatsAction.Response getTransformStatsResponse = client().execute(
-                GetTransformStatsAction.INSTANCE,
-                getTransformStatsRequest
-            ).actionGet();
+    public void testWarningForStats() {
+        GetTransformStatsAction.Request getTransformStatsRequest = new GetTransformStatsAction.Request("_all");
+        GetTransformStatsAction.Response getTransformStatsResponse = client().execute(
+            GetTransformStatsAction.INSTANCE,
+            getTransformStatsRequest
+        ).actionGet();
 
-            assertEquals(0, getTransformStatsResponse.getTransformsStats().size());
-        } else {
-            GetTransformAction.Request getTransformRequest = new GetTransformAction.Request("_all");
-            GetTransformAction.Response getTransformResponse = client().execute(GetTransformAction.INSTANCE, getTransformRequest)
-                .actionGet();
-            assertEquals(0, getTransformResponse.getTransformConfigurations().size());
+        assertEquals(0, getTransformStatsResponse.getTransformsStats().size());
 
-        }
+        assertWarnings("Transform requires the transform node role for at least 1 node, found no transform nodes");
+    }
+
+    public void testWarningForGet() {
+        GetTransformAction.Request getTransformRequest = new GetTransformAction.Request("_all");
+        GetTransformAction.Response getTransformResponse = client().execute(GetTransformAction.INSTANCE, getTransformRequest).actionGet();
+        assertEquals(0, getTransformResponse.getTransformConfigurations().size());
+
         assertWarnings("Transform requires the transform node role for at least 1 node, found no transform nodes");
     }
 }

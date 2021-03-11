2ce2dcdce341259811adc2aaa022390d3af0ad65
Jason Tedor <jason@tedor.me>
2021-03-10 22:09:31 -0500

    Fix imports

--- server/src/main/java/org/elasticsearch/bootstrap/Natives.java
+++ server/src/main/java/org/elasticsearch/bootstrap/Natives.java
@@ -10,9 +10,7 @@ package org.elasticsearch.bootstrap;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.env.Environment;
 
-import java.io.IOException;
 import java.nio.file.Path;
 
 /**

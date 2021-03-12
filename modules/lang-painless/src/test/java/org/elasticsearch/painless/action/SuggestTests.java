7a4f4f9cbb6a0e5909e8c1068edd1311988ef29b
Jack Conradson <osjdconrad@gmail.com>
2021-03-12 15:40:00 -0800

    add more tests

--- modules/lang-painless/src/test/java/org/elasticsearch/painless/action/SuggestTests.java
+++ modules/lang-painless/src/test/java/org/elasticsearch/painless/action/SuggestTests.java
@@ -16,6 +16,9 @@ import java.util.List;
 public class SuggestTests extends ScriptTestCase {
 
     private static void compareSuggestions(List<Suggestion> actual, String... values) {
+        assertEquals(values.length % 2, 0);
+        assertEquals(actual.size(), values.length / 2);
+
         for (int i = 0; i < values.length; ++i) {
             assertTrue(actual.contains(new Suggestion(values[i], values[++i])));
         }
@@ -70,19 +73,73 @@ public class SuggestTests extends ScriptTestCase {
         );
 
         compareSuggestions(
-                suggest("List list; list.add")
+                suggest("List list; list.add"),
+                Suggestion.METHOD, "add/1",
+                Suggestion.METHOD, "add/2",
+                Suggestion.METHOD, "addAll/1",
+                Suggestion.METHOD, "addAll/2"
+        );
+
+        compareSuggestions(
+                suggest("Math.ma"),
+                Suggestion.METHOD, "max/2"
+        );
+
+        compareSuggestions(
+                suggest("int value = Math.max(2.0, 2.0), Math.min(va"),
+                Suggestion.VARIABLE, "value"
         );
     }
 
     public void testFields() {
-
+        compareSuggestions(
+                suggest("Math.P"),
+                Suggestion.FIELD, "PI"
+        );
     }
 
     public void testFunctions() {
+        compareSuggestions(
+                suggest("para"),
+                Suggestion.VARIABLE, "params"
+        );
 
-    }
+        compareSuggestions(
+                suggest("int testF() {} int testY(blah) {} test"),
+                Suggestion.USER, "testF/0",
+                Suggestion.USER, "testY/0"
+        );
 
-    public void testParameters() {
+        compareSuggestions(
+                suggest("int testF() {} int testY(blah) {} test"),
+                Suggestion.USER, "testF/0",
+                Suggestion.USER, "testY/0"
+        );
 
+        compareSuggestions(
+                suggest("int testF() {} int testY(blah) {} testY().toS"),
+                Suggestion.METHOD, "toString/0"
+        );
+
+        compareSuggestions(
+                suggest("int testF() {} int testY(List x) {return blah blah} test"),
+                Suggestion.USER, "testF/0",
+                Suggestion.USER, "testY/1"
+        );
+
+        compareSuggestions(
+                suggest("int testF() {} int testY(blah) {trash trash} testY().toS"),
+                Suggestion.METHOD, "toString/0"
+        );
+
+        compareSuggestions(
+                suggest("int testF(int para) {par"),
+                Suggestion.VARIABLE, "para"
+        );
+
+        compareSuggestions(
+                suggest("int testF(int para) {int par = para; return par} par"),
+                Suggestion.VARIABLE, "params"
+        );
     }
 }

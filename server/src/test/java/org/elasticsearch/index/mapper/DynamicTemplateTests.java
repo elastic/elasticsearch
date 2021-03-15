ea394f17b73608e468bd8f48c634246b30ed1228
Nhat Nguyen <nhat.nguyen@elastic.co>
2021-03-14 23:38:08 -0400

    stylecheck

--- server/src/test/java/org/elasticsearch/index/mapper/DynamicTemplateTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/DynamicTemplateTests.java
@@ -22,8 +22,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.hamcrest.Matchers.containsString;
-
 public class DynamicTemplateTests extends ESTestCase {
 
     public void testMappingTypeTypeNotSet() {

203f8947c726c9aa2bf15066f633bb79a8b430b6
Nhat Nguyen <nhat.nguyen@elastic.co>
2021-03-19 20:56:41 -0400

    stylecheck

--- server/src/main/java/org/elasticsearch/search/SearchSortValues.java
+++ server/src/main/java/org/elasticsearch/search/SearchSortValues.java
@@ -8,7 +8,6 @@
 
 package org.elasticsearch.search;
 
-import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.Version;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;

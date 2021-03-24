86d0d686aaa970445982904f7068337e6de8edf4
Tanguy Leroux <tlrx.dev@gmail.com>
2021-03-24 12:07:19 +0100

    Wait for blob cache fills to complete before stopping the service (#70796)
    Today nothing prevents to use the BlobStoreCacheService after the component
    is stopped. It sometimes happens because the shards are one of the very last
    resources to be closed when a node stops, after components are stopped, and
    the closing of shards also releases more resources that are likely to trigger more
    blobs to be cached by the BlobStoreCacheService.
    
    On CI we can notice this behavior by seeing the blob cache index re-created
    while the after-test clean up logic is running (see #69735). This committ changes
    integration tests so that at stop time they now waits for in-flight index operations
    to be completed. It also prevents any new blob to be cached after the service
    has been stopped.
    
    Closes #69735
    
    Backport of #70220 for 7.x

--- x-pack/plugin/searchable-snapshots/src/test/java/org/elasticsearch/index/store/cache/TestUtils.java
+++ x-pack/plugin/searchable-snapshots/src/test/java/org/elasticsearch/index/store/cache/TestUtils.java
@@ -315,7 +315,7 @@ public final class TestUtils {
     public static class NoopBlobStoreCacheService extends BlobStoreCacheService {
 
         public NoopBlobStoreCacheService() {
-            super(null, null, mockClient(), null);
+            super(null, mockClient(), null, () -> 0L);
         }
 
         @Override
@@ -351,13 +351,7 @@ public final class TestUtils {
         private final ConcurrentHashMap<String, CachedBlob> blobs = new ConcurrentHashMap<>();
 
         public SimpleBlobStoreCacheService() {
-            super(null, null, mockClient(), null);
-        }
-
-        private static Client mockClient() {
-            final Client client = mock(Client.class);
-            when(client.settings()).thenReturn(Settings.EMPTY);
-            return client;
+            super(null, mockClient(), null, () -> 0L);
         }
 
         @Override

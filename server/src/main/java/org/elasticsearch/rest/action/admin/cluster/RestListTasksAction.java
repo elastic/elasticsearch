0211dbd8da2911b76b00207eefec9262498239a4
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2021-03-26 07:42:59 -0400

    removing from the rest layer

--- server/src/main/java/org/elasticsearch/rest/action/admin/cluster/RestListTasksAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/admin/cluster/RestListTasksAction.java
@@ -63,19 +63,18 @@ public class RestListTasksAction extends BaseRestHandler {
         boolean detailed = request.paramAsBoolean("detailed", false);
         String[] nodes = Strings.splitStringByCommaToArray(request.param("nodes"));
         String[] actions = Strings.splitStringByCommaToArray(request.param("actions"));
-        String[] descriptions = Strings.splitStringByCommaToArray(request.param("descriptions"));
         TaskId parentTaskId = new TaskId(request.param("parent_task_id"));
         boolean waitForCompletion = request.paramAsBoolean("wait_for_completion", false);
         TimeValue timeout = request.paramAsTime("timeout", null);
 
-        return new ListTasksRequest()
-            .setNodes(nodes)
-            .setDetailed(detailed)
-            .setActions(actions)
-            .setParentTaskId(parentTaskId)
-            .setWaitForCompletion(waitForCompletion)
-            .setTimeout(timeout)
-            .setDescriptions(descriptions);
+        ListTasksRequest listTasksRequest = new ListTasksRequest();
+        listTasksRequest.setNodes(nodes);
+        listTasksRequest.setDetailed(detailed);
+        listTasksRequest.setActions(actions);
+        listTasksRequest.setParentTaskId(parentTaskId);
+        listTasksRequest.setWaitForCompletion(waitForCompletion);
+        listTasksRequest.setTimeout(timeout);
+        return listTasksRequest;
     }
 
     /**

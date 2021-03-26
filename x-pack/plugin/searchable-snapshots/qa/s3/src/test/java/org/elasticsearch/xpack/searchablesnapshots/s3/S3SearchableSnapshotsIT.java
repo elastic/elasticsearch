308cdad05985907e98444f1ba0491ffcb8f67e95
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2021-03-26 17:24:00 +0100

    Add searchable snapshots integration tests for URL repositories (#70917)
    Relates #69521
    Backport of #70709

--- x-pack/plugin/searchable-snapshots/qa/s3/src/test/java/org/elasticsearch/xpack/searchablesnapshots/s3/S3SearchableSnapshotsIT.java
+++ x-pack/plugin/searchable-snapshots/qa/s3/src/test/java/org/elasticsearch/xpack/searchablesnapshots/s3/S3SearchableSnapshotsIT.java
@@ -15,12 +15,12 @@ import static org.hamcrest.Matchers.not;
 public class S3SearchableSnapshotsIT extends AbstractSearchableSnapshotsRestTestCase {
 
     @Override
-    protected String repositoryType() {
+    protected String writeRepositoryType() {
         return "s3";
     }
 
     @Override
-    protected Settings repositorySettings() {
+    protected Settings writeRepositorySettings() {
         final String bucket = System.getProperty("test.s3.bucket");
         assertThat(bucket, not(blankOrNullString()));
 

b9c2e4d0f84979fe0c05c8eca141b163e51e80a7
Nhat Nguyen <nhat.nguyen@elastic.co>
2021-03-28 17:08:34 -0400

    Fix licence header of point in time
    These files have been moved to server.
    
    Relates #70704

--- server/src/main/java/org/elasticsearch/action/search/RestOpenPointInTimeAction.java
+++ server/src/main/java/org/elasticsearch/action/search/RestOpenPointInTimeAction.java
@@ -6,13 +6,6 @@
  * Side Public License, v 1.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
 package org.elasticsearch.action.search;
 
 import org.elasticsearch.action.support.IndicesOptions;

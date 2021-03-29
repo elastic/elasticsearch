19a6d3e3f2e14b202c95b88b9b2dda7ac053a09d
Alan Woodward <romseygeek@gmail.com>
2021-03-29 09:36:33 +0100

    Better typing on LeafStoredFieldsLookup (#70962)
    LeafStoredFieldsLookup always returns a FieldLookup object. This
    commit makes that explicit in the generic parameters, which will
    reduce unnecessary casting when we come to use this in index time
    scripts.

--- server/src/main/java/org/elasticsearch/search/lookup/LeafStoredFieldsLookup.java
+++ server/src/main/java/org/elasticsearch/search/lookup/LeafStoredFieldsLookup.java
@@ -27,7 +27,7 @@ import java.util.function.Function;
 import static java.util.Collections.singletonMap;
 
 @SuppressWarnings({"unchecked", "rawtypes"})
-public class LeafStoredFieldsLookup implements Map<Object, Object> {
+public class LeafStoredFieldsLookup implements Map<Object, FieldLookup> {
 
     private final Function<String, MappedFieldType> fieldTypeLookup;
     private final CheckedBiConsumer<Integer, StoredFieldVisitor, IOException> reader;
@@ -51,7 +51,7 @@ public class LeafStoredFieldsLookup implements Map<Object, Object> {
     }
 
     @Override
-    public Object get(Object key) {
+    public FieldLookup get(Object key) {
         return loadFieldData(key.toString());
     }
 
@@ -91,12 +91,12 @@ public class LeafStoredFieldsLookup implements Map<Object, Object> {
     }
 
     @Override
-    public Object put(Object key, Object value) {
+    public FieldLookup put(Object key, FieldLookup value) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Object remove(Object key) {
+    public FieldLookup remove(Object key) {
         throw new UnsupportedOperationException();
     }
 

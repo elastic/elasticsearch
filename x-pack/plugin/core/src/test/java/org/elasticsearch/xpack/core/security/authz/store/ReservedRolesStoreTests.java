19134db4ab4f6acc5fb40061dd63cd9c93c7134a
Søren Louv-Jansen <sorenlouv@gmail.com>
2021-03-29 11:00:14 +0200

    Update ReservedRolesStoreTests.java

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStoreTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStoreTests.java
@@ -1257,13 +1257,13 @@ public class ReservedRolesStoreTests extends ESTestCase {
 
         assertNoAccessAllowed(role, "foo");
         assertNoAccessAllowed(role, "foo-apm");
-        assertNoAccessAllowed(role, "foo-logs-apm");
-        assertNoAccessAllowed(role, "foo-traces-apm");
-        assertNoAccessAllowed(role, "foo-metrics-apm");
+        assertNoAccessAllowed(role, "foo-logs-apm.bar");
+        assertNoAccessAllowed(role, "foo-traces-apm.bar");
+        assertNoAccessAllowed(role, "foo-metrics-apm.bar");
 
-        assertOnlyReadAllowed(role, "logs-apm" + randomIntBetween(0, 5));
-        assertOnlyReadAllowed(role, "traces-apm" + randomIntBetween(0, 5));
-        assertOnlyReadAllowed(role, "metrics-apm" + randomIntBetween(0, 5));
+        assertOnlyReadAllowed(role, "logs-apm." + randomIntBetween(0, 5));
+        assertOnlyReadAllowed(role, "traces-apm." + randomIntBetween(0, 5));
+        assertOnlyReadAllowed(role, "metrics-apm." + randomIntBetween(0, 5));
         assertOnlyReadAllowed(role, "apm-" + randomIntBetween(0, 5));
         assertOnlyReadAllowed(role, AnomalyDetectorsIndexFields.RESULTS_INDEX_PREFIX + AnomalyDetectorsIndexFields.RESULTS_INDEX_DEFAULT);
 

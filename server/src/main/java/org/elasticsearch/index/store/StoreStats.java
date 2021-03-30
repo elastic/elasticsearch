0ebab4ab31f91c4676142dd28f55d711a1466376
Henning Andersen <33268011+henningandersen@users.noreply.github.com>
2021-03-30 15:23:29 +0200

    Total data set size in stats (#70625)
    With shared cache searchable snapshots we have shards that have a size
    in S3 that differs from the locally occupied disk space. This commit
    introduces `store.total_data_set_size` to node and indices stats, allowing to
    differ between the two.
    
    Relates #69820

--- server/src/main/java/org/elasticsearch/index/store/StoreStats.java
+++ server/src/main/java/org/elasticsearch/index/store/StoreStats.java
@@ -27,8 +27,10 @@ public class StoreStats implements Writeable, ToXContentFragment {
     public static final long UNKNOWN_RESERVED_BYTES = -1L;
 
     public static final Version RESERVED_BYTES_VERSION = Version.V_7_9_0;
+    public static final Version TOTAL_DATA_SET_SIZE_SIZE_VERSION = Version.V_7_13_0;
 
     private long sizeInBytes;
+    private long totalDataSetSizeInBytes;
     private long reservedSize;
 
     public StoreStats() {
@@ -40,6 +42,11 @@ public class StoreStats implements Writeable, ToXContentFragment {
         if (in.getVersion().before(Version.V_6_0_0_alpha1)) {
             in.readVLong(); // throttleTimeInNanos
         }
+        if (in.getVersion().onOrAfter(TOTAL_DATA_SET_SIZE_SIZE_VERSION)) {
+            totalDataSetSizeInBytes = in.readVLong();
+        } else {
+            totalDataSetSizeInBytes = sizeInBytes;
+        }
         if (in.getVersion().onOrAfter(RESERVED_BYTES_VERSION)) {
             reservedSize = in.readZLong();
         } else {
@@ -49,19 +56,22 @@ public class StoreStats implements Writeable, ToXContentFragment {
 
     /**
      * @param sizeInBytes the size of the store in bytes
+     * @param totalDataSetSizeInBytes the size of the total data set in bytes, can differ from sizeInBytes for shards using shared cache
+     *                                storage
      * @param reservedSize a prediction of how much larger the store is expected to grow, or {@link StoreStats#UNKNOWN_RESERVED_BYTES}.
      */
-    public StoreStats(long sizeInBytes, long reservedSize) {
+    public StoreStats(long sizeInBytes, long totalDataSetSizeInBytes, long reservedSize) {
         assert reservedSize == UNKNOWN_RESERVED_BYTES || reservedSize >= 0 : reservedSize;
         this.sizeInBytes = sizeInBytes;
+        this.totalDataSetSizeInBytes = totalDataSetSizeInBytes;
         this.reservedSize = reservedSize;
     }
-
     public void add(StoreStats stats) {
         if (stats == null) {
             return;
         }
         sizeInBytes += stats.sizeInBytes;
+        totalDataSetSizeInBytes += stats.totalDataSetSizeInBytes;
         reservedSize = ignoreIfUnknown(reservedSize) + ignoreIfUnknown(stats.reservedSize);
     }
 
@@ -85,6 +95,14 @@ public class StoreStats implements Writeable, ToXContentFragment {
         return size();
     }
 
+    public ByteSizeValue totalDataSetSize() {
+        return new ByteSizeValue(totalDataSetSizeInBytes);
+    }
+
+    public ByteSizeValue getTotalDataSetSize() {
+        return totalDataSetSize();
+    }
+
     /**
      * A prediction of how much larger this store will eventually grow. For instance, if we are currently doing a peer recovery or restoring
      * a snapshot into this store then we can account for the rest of the recovery using this field. A value of {@code -1B} indicates that
@@ -100,6 +118,9 @@ public class StoreStats implements Writeable, ToXContentFragment {
         if (out.getVersion().before(Version.V_6_0_0_alpha1)) {
             out.writeVLong(0L); // throttleTimeInNanos
         }
+        if (out.getVersion().onOrAfter(TOTAL_DATA_SET_SIZE_SIZE_VERSION)) {
+            out.writeVLong(totalDataSetSizeInBytes);
+        }
         if (out.getVersion().onOrAfter(RESERVED_BYTES_VERSION)) {
             out.writeZLong(reservedSize);
         }
@@ -109,6 +130,7 @@ public class StoreStats implements Writeable, ToXContentFragment {
     public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
         builder.startObject(Fields.STORE);
         builder.humanReadableField(Fields.SIZE_IN_BYTES, Fields.SIZE, size());
+        builder.humanReadableField(Fields.TOTAL_DATA_SET_SIZE_IN_BYTES, Fields.TOTAL_DATA_SET_SIZE, totalDataSetSize());
         builder.humanReadableField(Fields.RESERVED_IN_BYTES, Fields.RESERVED, getReservedSize());
         builder.endObject();
         return builder;
@@ -118,6 +140,8 @@ public class StoreStats implements Writeable, ToXContentFragment {
         static final String STORE = "store";
         static final String SIZE = "size";
         static final String SIZE_IN_BYTES = "size_in_bytes";
+        static final String TOTAL_DATA_SET_SIZE = "total_data_set_size";
+        static final String TOTAL_DATA_SET_SIZE_IN_BYTES = "total_data_set_size_in_bytes";
         static final String RESERVED = "reserved";
         static final String RESERVED_IN_BYTES = "reserved_in_bytes";
     }

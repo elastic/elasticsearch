d2054697f73e860d39293b2d18815d9ec4e3955b
David Roberts <dave.roberts@elastic.co>
2021-03-31 18:54:13 +0100

    [ML] Make ML memory tracker more robust to flipping on/off master (#71067)
    Testing has shown that during cluster formation it is possible for
    a node to flip backwards and forwards between being master/not
    being master a couple of times. The ML memory tracker is not
    completely robust to this.
    
    This change improves the situation in a low-risk way, by aborting
    refreshes if the node ceases to be master during the refresh, and
    not updating the last update time when this happens. This helps
    because then the "is recently refreshed?" question will return
    false if the node briefly became master, then ceased to be master
    before the refresh was complete, then later became master again.
    
    There is still a race condition after this change: it is possible
    that the "is recently refreshed?" question is asked after a
    refresh is complete and while the node is still master, but then
    the node ceases to be master before the memory information is used.
    Fixing this is a much more major change, so too dangerous for a
    patch. Since the refresh is a longer operation than a simple
    boolean accessor, the change in this PR should still help a lot.
    
    Relates #68685

--- x-pack/plugin/ml/src/internalClusterTest/java/org/elasticsearch/xpack/ml/integration/MlDistributedFailureIT.java
+++ x-pack/plugin/ml/src/internalClusterTest/java/org/elasticsearch/xpack/ml/integration/MlDistributedFailureIT.java
@@ -38,7 +38,6 @@ import org.elasticsearch.persistent.PersistentTasksClusterService;
 import org.elasticsearch.persistent.PersistentTasksCustomMetadata;
 import org.elasticsearch.persistent.PersistentTasksCustomMetadata.PersistentTask;
 import org.elasticsearch.persistent.UpdatePersistentTaskStatusAction;
-import org.elasticsearch.test.junit.annotations.TestIssueLogging;
 import org.elasticsearch.xpack.core.action.util.QueryPage;
 import org.elasticsearch.xpack.core.ml.MlTasks;
 import org.elasticsearch.xpack.core.ml.action.CloseJobAction;
@@ -397,10 +396,6 @@ public class MlDistributedFailureIT extends BaseMlIntegTestCase {
         assertTrue(closeJobResponse.isClosed());
     }
 
-    @TestIssueLogging(issueUrl = "https://github.com/elastic/elasticsearch/issues/68685",
-        value = "org.elasticsearch.xpack.ml.process:TRACE,"
-            + "org.elasticsearch.xpack.ml.job:TRACE,"
-            + "org.elasticsearch.persistent.PersistentTasksClusterService:TRACE")
     public void testJobRelocationIsMemoryAware() throws Exception {
         internalCluster().ensureAtLeastNumDataNodes(1);
         ensureStableCluster();

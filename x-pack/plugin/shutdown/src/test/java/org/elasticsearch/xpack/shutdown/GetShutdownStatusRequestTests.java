07ceb0a03a8429fa313d96cec93b975118890102
Gordon Brown <gordon.brown@elastic.co>
2021-04-01 16:13:55 -0600

    Unused import

--- x-pack/plugin/shutdown/src/test/java/org/elasticsearch/xpack/shutdown/GetShutdownStatusRequestTests.java
+++ x-pack/plugin/shutdown/src/test/java/org/elasticsearch/xpack/shutdown/GetShutdownStatusRequestTests.java
@@ -14,7 +14,6 @@ import org.elasticsearch.test.AbstractWireSerializingTestCase;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class GetShutdownStatusRequestTests extends AbstractWireSerializingTestCase<GetShutdownStatusAction.Request> {

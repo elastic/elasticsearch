ff432d80624166c6bee1f001df68f1e0b401c885
David Turner <david.turner@elastic.co>
2021-04-07 11:09:51 +0100

    Precommit

--- server/src/test/java/org/elasticsearch/cluster/node/DiscoveryNodeTests.java
+++ server/src/test/java/org/elasticsearch/cluster/node/DiscoveryNodeTests.java
@@ -38,7 +38,6 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.startsWith;
 

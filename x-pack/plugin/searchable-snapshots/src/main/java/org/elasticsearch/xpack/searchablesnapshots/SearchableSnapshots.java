adaf372ddfef26ce98132b21d00dfdfe970ac7b7
David Turner <david.turner@elastic.co>
2021-04-07 13:56:17 +0100

    Forbid dedicated frozen nodes w/ unfrozen indices (#71395)
    We only want the shards of frozen (i.e. partially-cached searchable
    snapshot backed) indices to be allocated to dedicated frozen nodes but
    today we only weakly achieve this thanks to the data tier allocation
    rules. This commit introduces an allocation decider that directly
    enforces this constraint.

--- x-pack/plugin/searchable-snapshots/src/main/java/org/elasticsearch/xpack/searchablesnapshots/SearchableSnapshots.java
+++ x-pack/plugin/searchable-snapshots/src/main/java/org/elasticsearch/xpack/searchablesnapshots/SearchableSnapshots.java
@@ -12,6 +12,8 @@ import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
+import org.elasticsearch.xpack.searchablesnapshots.allocation.decider.DedicatedFrozenNodeAllocationDecider;
+import org.elasticsearch.xpack.searchablesnapshots.cache.blob.BlobStoreCacheService;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
@@ -529,7 +531,8 @@ public class SearchableSnapshots extends Plugin implements IndexStorePlugin, Eng
         return org.elasticsearch.common.collect.List.of(
             new SearchableSnapshotAllocationDecider(() -> getLicenseState().isAllowed(XPackLicenseState.Feature.SEARCHABLE_SNAPSHOTS)),
             new SearchableSnapshotEnableAllocationDecider(settings, clusterSettings),
-            new HasFrozenCacheAllocationDecider(frozenCacheInfoService)
+            new HasFrozenCacheAllocationDecider(frozenCacheInfoService),
+            new DedicatedFrozenNodeAllocationDecider()
         );
     }
 

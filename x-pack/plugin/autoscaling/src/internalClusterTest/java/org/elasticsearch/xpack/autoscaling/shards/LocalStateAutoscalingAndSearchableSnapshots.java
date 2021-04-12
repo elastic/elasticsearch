1c4c4aee16fd36ad5f60eb117efe323b0ec10cf6
Henning Andersen <henning.andersen@elastic.co>
2021-04-12 19:42:08 +0200

    Add comment on local state combined plugin

--- x-pack/plugin/autoscaling/src/internalClusterTest/java/org/elasticsearch/xpack/autoscaling/shards/LocalStateAutoscalingAndSearchableSnapshots.java
+++ x-pack/plugin/autoscaling/src/internalClusterTest/java/org/elasticsearch/xpack/autoscaling/shards/LocalStateAutoscalingAndSearchableSnapshots.java
@@ -12,6 +12,10 @@ import org.elasticsearch.license.XPackLicenseState;
 import org.elasticsearch.xpack.autoscaling.LocalStateAutoscaling;
 import org.elasticsearch.xpack.searchablesnapshots.SearchableSnapshots;
 
+/**
+ * We need a local state plugin for searchable snapshots too (test sources are not exposed).
+ * The local state plugin is necessary to avoid touching the "static SetOnce" licenseState field in XPackPlugin.
+ */
 public class LocalStateAutoscalingAndSearchableSnapshots extends LocalStateAutoscaling {
 
     public LocalStateAutoscalingAndSearchableSnapshots(final Settings settings) {

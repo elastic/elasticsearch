cb76a362fbd6fa603d91ad91c0ab8ec6938b797f
Nhat Nguyen <nhat.nguyen@elastic.co>
2021-04-13 11:48:10 -0400

    comment

--- server/src/main/java/org/elasticsearch/common/metrics/CounterMetric.java
+++ server/src/main/java/org/elasticsearch/common/metrics/CounterMetric.java
@@ -13,6 +13,12 @@ import org.elasticsearch.Assertions;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.LongAdder;
 
+
+/**
+ * A {@link CounterMetric} is used to track the number of completed and outstanding items, for example, the number of executed refreshes,
+ * the currently used memory by indexing, the current pending search requests. In both cases, the current {@link CounterMetric#count} is
+ * always non-negative.
+ */
 public final class CounterMetric {
     private final LongAdder counter = new LongAdder();
     private final AtomicLong assertingCounter = Assertions.ENABLED ? new AtomicLong() : null;
@@ -42,8 +48,19 @@ public final class CounterMetric {
         assert assertNonNegative(assertingCounter.addAndGet(-n));
     }
 
+    /**
+     * Returns the current count of this metric. The returned value is always non-negative.
+     * <p>
+     * As this metric is implemented using a {@link LongAdder}, the returned value is NOT an atomic snapshot;
+     * invocation in the absence of concurrent updates returns an accurate result, but concurrent updates that
+     * occur while the sum is being calculated might not be incorporated.
+     *
+     * @see LongAdder#sum()
+     */
     public long count() {
-        // Retries up to 5 times as the returned value of LongAdder#sum is NOT an atomic snapshot.
+        // The `counter.sum()` value is expected to always be non-negative. And if it's negative, then some concurrent updates
+        // aren't incorporated yet. In this case, we can immediately return 0L; but here we choose to retry several times
+        // to hopefully have a more accurate value than 0L.
         for (int i = 0; i < 5; i++) {
             final long count = counter.sum();
             if (count >= 0L) {

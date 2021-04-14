c1385fb64c585010156bba49bbac3146a03125a5
Rory Hunter <pugnascotia@users.noreply.github.com>
2021-04-14 20:06:02 +0100

    Change deprecation logs data stream name (#71642)
    Backport of #68737.
    
    More fixes to deprecation log indexing so that the data stream name and document
    contents are more ECS-compatible.

--- modules/transport-netty4/src/javaRestTest/java/org/elasticsearch/rest/Netty4HeadBodyIsEmptyIT.java
+++ modules/transport-netty4/src/javaRestTest/java/org/elasticsearch/rest/Netty4HeadBodyIsEmptyIT.java
@@ -134,7 +134,7 @@ public class Netty4HeadBodyIsEmptyIT extends ESRestTestCase {
                 request.setOptions(expectWarnings(
                     "legacy template [template] has index patterns [*] matching patterns from existing composable templates " +
                     "[.deprecation-indexing-template,.slm-history,.watch-history-13,ilm-history,logs," +
-                    "metrics,synthetics] with patterns (.deprecation-indexing-template => [.logs-deprecation-elasticsearch]," +
+                    "metrics,synthetics] with patterns (.deprecation-indexing-template => [.logs-deprecation.elasticsearch-default]," +
                     ".slm-history => [.slm-history-5*]," +
                     ".watch-history-13 => [.watcher-history-13*],ilm-history => [ilm-history-5*]," +
                     "logs => [logs-*-*],metrics => [metrics-*-*],synthetics => [synthetics-*-*]" +

e6ef3dce3d2331d02e43355cda2770dc07e227d6
bellengao <gbl_long@163.com>
2021-04-15 19:48:57 +0800

    Enhance the validation for alias when creating index template or index (#68807)
    Relates to #61009.
    
    The main changes are:
    
    1.Throw an error if there are some unknown fields such as bool or some tokens such as [] in the alias parameter when creating index template or creating index.
    2.Add some tests for the change.

--- server/src/test/java/org/elasticsearch/action/admin/indices/template/put/PutIndexTemplateRequestTests.java
+++ server/src/test/java/org/elasticsearch/action/admin/indices/template/put/PutIndexTemplateRequestTests.java
@@ -162,6 +162,38 @@ public class PutIndexTemplateRequestTests extends AbstractXContentTestCase<PutIn
         assertThat(actual.aliases(), equalTo(expected.aliases()));
         assertThat(actual.mappings(), equalTo(expected.mappings()));
         assertThat(actual.settings(), equalTo(expected.settings()));
+
+        XContentBuilder aliases1 = XContentFactory.jsonBuilder().startObject()
+            .startObject("aliases")
+                .startObject("filtered-data")
+                    .startObject("bool")
+                        .startObject("filter")
+                            .startObject("term")
+                            .field("a", "b")
+                            .endObject()
+                        .endObject()
+                    .endObject()
+                .endObject()
+            .endObject()
+        .endObject();
+        e = expectThrows(IllegalArgumentException.class, () -> new PutIndexTemplateRequest().source(aliases1));
+        assertThat(e.getCause().getMessage(), containsString("Unknown field [bool] in alias [filtered-data]"));
+
+        XContentBuilder aliases2 = XContentFactory.jsonBuilder().startObject()
+            .startObject("aliases")
+                .startObject("filtered-data")
+                    .startArray("filter")
+                        .startObject()
+                            .startObject("term")
+                            .field("a", "b")
+                            .endObject()
+                        .endObject()
+                    .endArray()
+                .endObject()
+            .endObject()
+        .endObject();
+        e = expectThrows(IllegalArgumentException.class, () -> new PutIndexTemplateRequest().source(aliases2));
+        assertThat(e.getCause().getMessage(), containsString("Unknown token [START_ARRAY] in alias [filtered-data]"));
     }
 
     @Override

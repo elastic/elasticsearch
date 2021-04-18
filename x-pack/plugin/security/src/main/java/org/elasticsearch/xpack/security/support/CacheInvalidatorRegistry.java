5167355a36b6b879cb3b94bbf218e546b5c9eac9
Yang Wang <yang.wang@elastic.co>
2021-04-19 09:55:47 +1000

    Improve reusability of the cache invalidation counter (#66310)
    This PR is a follow-up refactoring for #59376. It replaces
    InvalidationCountingCacheWrapper with a simpler and more resuable
    LockingAtomicCounter.
    
    The change includes extracting the logic of "minimizing chance of caching stale
    results" into a new class, which is now generic and not tied to any cache
    related operations. This in turn allows it to be reused by different caches,
    which often have subtle, important and anonying differences that make it
    impossible (or at least cubersome and painful) to have a generic solution. So
    this change of reducing the size of reusable code is the right move.
    
    As an example of applying this new pattern, the caches for NativePrivilegeStore
    are now migrated and managed by the centrialied CacheInvalidatorRegistry. The
    clear privilege cache rest and transport actions are also updated to use the
    new implementation. The Request/Response classes are intentionally untouched
    and plan to have a separate PR to address them because they would involve user
    visible deprecation changes.
    
    Co-authored-by: Albert Zaharovits <albert.zaharovits@elastic.co>

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/support/CacheInvalidatorRegistry.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/support/CacheInvalidatorRegistry.java
@@ -32,7 +32,7 @@ public class CacheInvalidatorRegistry {
         cacheInvalidators.put(name, cacheInvalidator);
     }
 
-    public void onSecurityIndexStageChange(SecurityIndexManager.State previousState, SecurityIndexManager.State currentState) {
+    public void onSecurityIndexStateChange(SecurityIndexManager.State previousState, SecurityIndexManager.State currentState) {
         if (isMoveFromRedToNonRed(previousState, currentState)
             || isIndexDeleted(previousState, currentState)
             || Objects.equals(previousState.indexUUID, currentState.indexUUID) == false

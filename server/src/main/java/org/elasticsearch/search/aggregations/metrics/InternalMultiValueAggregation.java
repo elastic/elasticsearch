6d82a6dd010e75c6cb57cc4856af7c5484944ad4
Hendrik Muhs <hendrik.muhs@elastic.co>
2021-04-20 11:45:32 +0200

    add some missing methods

--- server/src/main/java/org/elasticsearch/search/aggregations/metrics/InternalMultiValueAggregation.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/metrics/InternalMultiValueAggregation.java
@@ -9,12 +9,20 @@
 package org.elasticsearch.search.aggregations.metrics;
 
 import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.search.DocValueFormat;
 import org.elasticsearch.search.aggregations.InternalAggregation;
+import org.elasticsearch.search.aggregations.support.AggregationPath;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Objects;
 
 public abstract class InternalMultiValueAggregation extends InternalAggregation implements MultiValueAggregation {
+
+    private static final DocValueFormat DEFAULT_FORMAT = DocValueFormat.RAW;
+
+    protected DocValueFormat format = DEFAULT_FORMAT;
 
     protected InternalMultiValueAggregation(String name, Map<String, Object> metadata) {
         super(name, metadata);
@@ -28,7 +36,27 @@ public abstract class InternalMultiValueAggregation extends InternalAggregation 
     }
 
     @Override
+    public final double sortValue(AggregationPath.PathElement head, Iterator<AggregationPath.PathElement> tail) {
+        throw new IllegalArgumentException("Metrics aggregations cannot have sub-aggregations (at [>" + head + "]");
+    }
+
+    @Override
     protected boolean mustReduceOnSingleInternalAgg() {
         return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), format);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        if (super.equals(obj) == false) return false;
+
+        InternalMultiValueAggregation other = (InternalMultiValueAggregation) obj;
+        return Objects.equals(format, other.format);
     }
 }

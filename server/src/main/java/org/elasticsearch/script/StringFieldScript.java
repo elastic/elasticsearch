e5c52adc677e813389c81d057169785b610eeb9a
Luca Cavanna <javanna@users.noreply.github.com>
2021-04-20 14:32:06 +0200

    Rename string_field script context to keyword_field (#71854)
    Up until now, the name of the script contexts that runtime fields use was internal only. They recently got exposed through the painless execute API. This commit fixes the discrepancy between the field type used to define a runtime field of type keyword and the script context needed to simulate its corresponding script: string_field should be keyword_field.

--- server/src/main/java/org/elasticsearch/script/StringFieldScript.java
+++ server/src/main/java/org/elasticsearch/script/StringFieldScript.java
@@ -24,7 +24,7 @@ public abstract class StringFieldScript extends AbstractFieldScript {
      */
     public static final long MAX_CHARS = 1024 * 1024;
 
-    public static final ScriptContext<Factory> CONTEXT = newContext("string_field", Factory.class);
+    public static final ScriptContext<Factory> CONTEXT = newContext("keyword_field", Factory.class);
 
     @SuppressWarnings("unused")
     public static final String[] PARAMETERS = {};

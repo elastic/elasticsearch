2fdcc9df79d579f2fd6d981dad3c8d90fed2b783
Armin Braun <me@obrown.io>
2021-04-21 07:30:29 +0200

    Fix ListenableFuture Resolving Listeners under Mutex (#71943) (#71963)
    We shouldn't loop over the listeners under the mutex in `done` since in most use-cases we used `DirectExecutorService`
    with this class.
    Also, no need to create an `AbstractRunnable` for direct execution. We use this listener on the hot path in authentication
    making this a worthwhile optimization I think.
    Lastly, no need to clear and thus loop over `listeners`, the list is not used again after the `done` call returns anyway
    so no point in retaining it at all (especially when in a number of use cases we add listeners only after the `done` call
    so we can also save the instantiation by making the field non-final).

--- server/src/main/java/org/elasticsearch/cluster/coordination/Coordinator.java
+++ server/src/main/java/org/elasticsearch/cluster/coordination/Coordinator.java
@@ -47,7 +47,6 @@ import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.common.util.concurrent.ListenableFuture;
 import org.elasticsearch.common.xcontent.XContentHelper;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
@@ -1458,7 +1457,7 @@ public class Coordinator extends AbstractLifecycleComponent implements Discovery
                     ackListener.onNodeAck(getLocalNode(), exception); // other nodes have acked, but not the master.
                     publishListener.onFailure(exception);
                 }
-            }, EsExecutors.newDirectExecutorService(), transportService.getThreadPool().getThreadContext());
+            }, null, transportService.getThreadPool().getThreadContext());
         }
 
         private void cancelTimeoutHandlers() {

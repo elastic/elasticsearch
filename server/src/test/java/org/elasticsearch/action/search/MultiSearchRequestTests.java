b053944c74a6788c691aa8aa6322f264d7246cb7
Jim Ferenczi <jim.ferenczi@elastic.co>
2021-04-22 10:33:33 +0200

    Fix MultiSearchRequestTests#testWritingExpandWildcards (#72045)
    Adjust this random test now that default indices options are not
    exposed in rest.
    
    Relates #71688
    Closes #71916

--- server/src/test/java/org/elasticsearch/action/search/MultiSearchRequestTests.java
+++ server/src/test/java/org/elasticsearch/action/search/MultiSearchRequestTests.java
@@ -314,7 +314,6 @@ public class MultiSearchRequestTests extends ESTestCase {
         }
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/71916")
     public void testWritingExpandWildcards() throws IOException {
         assertExpandWildcardsValue(IndicesOptions.fromOptions(randomBoolean(), randomBoolean(), true, true, true, randomBoolean(),
             randomBoolean(), randomBoolean(), randomBoolean()), "all");
@@ -341,8 +340,12 @@ public class MultiSearchRequestTests extends ESTestCase {
             MultiSearchRequest.writeSearchRequestParams(request, builder);
             Map<String, Object> map =
                 XContentHelper.convertToMap(XContentType.JSON.xContent(), BytesReference.bytes(builder).streamInput(), false);
-            final String value = (String) map.get("expand_wildcards");
-            assertEquals(expectedValue, value);
+            if (options.equals(SearchRequest.DEFAULT_INDICES_OPTIONS) == false) {
+                final String value = (String) map.get("expand_wildcards");
+                assertEquals(expectedValue, value);
+            } else {
+                assertNull(map.get("expand_wildcards"));
+            }
         }
     }
 

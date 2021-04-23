cccb84aa5adfe712d9a9b4b1720552b7190fadb6
Luca Cavanna <javanna@users.noreply.github.com>
2021-04-23 14:21:23 +0200

    checkstyle

--- server/src/main/java/org/elasticsearch/index/mapper/Mapping.java
+++ server/src/main/java/org/elasticsearch/index/mapper/Mapping.java
@@ -26,7 +26,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static java.util.Collections.unmodifiableMap;

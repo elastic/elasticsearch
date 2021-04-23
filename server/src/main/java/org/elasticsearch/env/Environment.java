be899f88061f8f340c3af304e9933c2d32fc1318
Ryan Ernst <ryan@iernst.net>
2021-04-23 16:44:01 -0700

    Copy single datapath as single path to new settings

--- server/src/main/java/org/elasticsearch/env/Environment.java
+++ server/src/main/java/org/elasticsearch/env/Environment.java
@@ -158,7 +158,12 @@ public class Environment {
 
         final Settings.Builder finalSettings = Settings.builder().put(settings);
         if (PATH_DATA_SETTING.exists(settings)) {
-            finalSettings.putList(PATH_DATA_SETTING.getKey(), Arrays.stream(dataFiles).map(Path::toString).collect(Collectors.toList()));
+            if (dataFiles.length == 1) {
+                finalSettings.put(PATH_DATA_SETTING.getKey(), dataFiles[0]);
+            } else {
+                finalSettings.putList(PATH_DATA_SETTING.getKey(),
+                    Arrays.stream(dataFiles).map(Path::toString).collect(Collectors.toList()));
+            }
         }
         finalSettings.put(PATH_HOME_SETTING.getKey(), homeFile);
         finalSettings.put(PATH_LOGS_SETTING.getKey(), logsFile.toString());

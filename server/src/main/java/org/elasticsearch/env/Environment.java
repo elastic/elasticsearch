cb3cf6dba25df45249b38cbbae3085f017104c5c
Ryan Ernst <ryan@elastic.co>
2021-04-23 13:14:14 -0700

    Add deprecation for single valued list of data path (#72180)
    Multiple data paths are deprecated, but because path.data is a
    ListSetting, it still allows a single value. This will change to a
    String setting in 8.0. This commit adds detection for the single valued
    list case and adds a deprecation warning to the node log.

--- server/src/main/java/org/elasticsearch/env/Environment.java
+++ server/src/main/java/org/elasticsearch/env/Environment.java
@@ -296,6 +296,15 @@ public class Environment {
         }
     }
 
+    /** Returns true if the data path is a list, false otherwise */
+    public static boolean dataPathUsesList(Settings settings) {
+        if (settings.hasValue(PATH_DATA_SETTING.getKey()) == false) {
+            return false;
+        }
+        String rawDataPath = settings.get(PATH_DATA_SETTING.getKey());
+        return rawDataPath.startsWith("[");
+    }
+
     public static FileStore getFileStore(final Path path) throws IOException {
         return new ESFileStore(Files.getFileStore(path));
     }

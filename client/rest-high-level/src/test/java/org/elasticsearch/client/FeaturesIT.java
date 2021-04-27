fc7c06d8a1d7662c9a72a8ff30067ffc797ebd6e
William Brafford <william.brafford@elastic.co>
2021-04-27 13:47:10 -0400

    Make feature reset API response more informative (#71240)
    Previously, the ResetFeatureStateStatus object captured its status in a
    String, which meant that if we wanted to know if something succeeded or
    failed, we'd have to parse information out of the string. This isn't a
    good way of doing things.
    
    I've introduced a SUCCESS/FAILURE enum for status constants, and added a
    check for failures in the transport action. We return a 207 if some but not all
    reset actions fail, and for every failure, we also return information about the
    exception or error that caused it.
    
    Co-authored-by: Jay Modi <jaymode@users.noreply.github.com>

--- client/rest-high-level/src/test/java/org/elasticsearch/client/FeaturesIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/FeaturesIT.java
@@ -12,9 +12,15 @@ import org.elasticsearch.client.feature.GetFeaturesRequest;
 import org.elasticsearch.client.feature.GetFeaturesResponse;
 import org.elasticsearch.client.feature.ResetFeaturesRequest;
 import org.elasticsearch.client.feature.ResetFeaturesResponse;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.search.SearchModule;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.notNullValue;
 
@@ -34,13 +40,25 @@ public class FeaturesIT extends ESRestHighLevelClientTestCase {
     public void testResetFeatures() throws IOException {
         ResetFeaturesRequest request = new ResetFeaturesRequest();
 
+        // need superuser privileges to execute the reset
+        RestHighLevelClient adminHighLevelClient = new RestHighLevelClient(
+            adminClient(),
+            (client) -> {},
+            new SearchModule(Settings.EMPTY, Collections.emptyList()).getNamedXContents());
         ResetFeaturesResponse response = execute(request,
-            highLevelClient().features()::resetFeatures, highLevelClient().features()::resetFeaturesAsync);
+            adminHighLevelClient.features()::resetFeatures,
+            adminHighLevelClient.features()::resetFeaturesAsync);
 
         assertThat(response, notNullValue());
-        assertThat(response.getFeatures(), notNullValue());
-        assertThat(response.getFeatures().size(), greaterThan(1));
-        assertTrue(response.getFeatures().stream().anyMatch(
+        assertThat(response.getFeatureResetStatuses(), notNullValue());
+        assertThat(response.getFeatureResetStatuses().size(), greaterThan(1));
+        assertTrue(response.getFeatureResetStatuses().stream().anyMatch(
             feature -> "tasks".equals(feature.getFeatureName()) && "SUCCESS".equals(feature.getStatus())));
+
+        Set<String> statuses = response.getFeatureResetStatuses().stream()
+            .map(ResetFeaturesResponse.ResetFeatureStateStatus::getStatus)
+            .collect(Collectors.toSet());
+
+        assertThat(statuses, contains("SUCCESS"));
     }
 }

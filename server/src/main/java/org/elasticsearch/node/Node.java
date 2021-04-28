315d28a23e08ca4e68bfaa8ce0e72d34ef8f9e87
Armin Braun <me@obrown.io>
2021-04-28 22:09:33 +0200

    Don't Set up SystemIndexManager on Non-Master Nodes
    This cluster state listener needs to only be created on master nodes,
    it's a noop on non-master nodes.

--- server/src/main/java/org/elasticsearch/node/Node.java
+++ server/src/main/java/org/elasticsearch/node/Node.java
@@ -494,8 +494,9 @@ public class Node implements Closeable {
                     .flatMap(m -> m.entrySet().stream())
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
-            final SystemIndexManager systemIndexManager = new SystemIndexManager(systemIndices, client);
-            clusterService.addListener(systemIndexManager);
+            if (DiscoveryNode.isMasterNode(settings)) {
+                clusterService.addListener(new SystemIndexManager(systemIndices, client));
+            }
 
             final RerouteService rerouteService
                 = new BatchedRerouteService(clusterService, clusterModule.getAllocationService()::reroute);

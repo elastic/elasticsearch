d38fd493b13bdec81f0bca29a77c14f72da1594d
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2021-04-28 18:16:31 +0200

    Add support for Rest XPackUsage task cancellation
    Backport of #72304

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/rest/action/RestXPackUsageAction.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/rest/action/RestXPackUsageAction.java
@@ -7,13 +7,17 @@
 package org.elasticsearch.xpack.core.rest.action;
 
 import org.elasticsearch.action.support.master.MasterNodeRequest;
+import org.elasticsearch.client.ElasticsearchClient;
+import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.http.HttpChannel;
 import org.elasticsearch.rest.BytesRestResponse;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.RestResponse;
 import org.elasticsearch.rest.action.RestBuilderListener;
 import org.elasticsearch.xpack.core.XPackClient;
+import org.elasticsearch.rest.action.RestCancellableNodeClient;
 import org.elasticsearch.xpack.core.XPackFeatureSet;
 import org.elasticsearch.xpack.core.action.XPackUsageRequestBuilder;
 import org.elasticsearch.xpack.core.action.XPackUsageResponse;
@@ -39,8 +43,10 @@ public class RestXPackUsageAction extends XPackRestHandler {
 
     @Override
     public RestChannelConsumer doPrepareRequest(RestRequest request, XPackClient client) {
+        assert client.es() instanceof NodeClient : "Expected a NodeClient";
         final TimeValue masterTimeout = request.paramAsTime("master_timeout", MasterNodeRequest.DEFAULT_MASTER_NODE_TIMEOUT);
-        return channel -> new XPackUsageRequestBuilder(client.es())
+        final HttpChannel httpChannel = request.getHttpChannel();
+        return channel -> new XPackUsageRequestBuilder(new RestCancellableNodeClient((NodeClient) client.es(), httpChannel))
                 .setMasterNodeTimeout(masterTimeout)
                 .execute(new RestBuilderListener<XPackUsageResponse>(channel) {
                     @Override

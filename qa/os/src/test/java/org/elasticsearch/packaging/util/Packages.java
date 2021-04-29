d303c554ffd3e5becec1fd0a8a90da55bbbc53c3
Mark Vieira <portugee@gmail.com>
2021-04-29 09:52:12 -0700

    Add missing import

--- qa/os/src/test/java/org/elasticsearch/packaging/util/Packages.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/util/Packages.java
@@ -10,6 +10,7 @@ package org.elasticsearch.packaging.util;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.Version;
 import org.elasticsearch.packaging.util.Shell.Result;
 
 import java.io.IOException;

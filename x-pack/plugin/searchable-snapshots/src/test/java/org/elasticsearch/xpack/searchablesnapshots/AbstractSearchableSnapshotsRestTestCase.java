5755c03e23d4d138f46168fc1ade289bf3241cd2
Dan Hermann <dan.hermann@elastic.co>
2021-05-03 15:53:54 -0500

    checkstyle

--- x-pack/plugin/searchable-snapshots/src/test/java/org/elasticsearch/xpack/searchablesnapshots/AbstractSearchableSnapshotsRestTestCase.java
+++ x-pack/plugin/searchable-snapshots/src/test/java/org/elasticsearch/xpack/searchablesnapshots/AbstractSearchableSnapshotsRestTestCase.java
@@ -172,8 +172,12 @@ public abstract class AbstractSearchableSnapshotsRestTestCase extends ESRestTest
     public void testSearchResultsWhenFrozen() throws Exception {
         runSearchableSnapshotsTest((restoredIndexName, numDocs) -> {
             final Request freezeRequest = new Request(HttpPost.METHOD_NAME, restoredIndexName + "/_freeze");
-            freezeRequest.setOptions(expectWarnings("Frozen indices are deprecated because they provide no benefit given improvements in " +
-                "heap memory utilization. They will be removed in a future release."));
+            freezeRequest.setOptions(
+                expectWarnings(
+                    "Frozen indices are deprecated because they provide no benefit given improvements in " +
+                        "heap memory utilization. They will be removed in a future release."
+                )
+            );
             assertOK(client().performRequest(freezeRequest));
             ensureGreen(restoredIndexName);
             for (int i = 0; i < 10; i++) {

0b55eccd35acf15a28856ce3195050ba61bc108a
Tanguy Leroux <tlrx.dev@gmail.com>
2021-05-06 11:24:24 +0200

    [7.x] Apply spotless formatting to :x-pack:plugin:voting-only-node (#72758)

--- x-pack/plugin/voting-only-node/src/test/java/org/elasticsearch/cluster/coordination/VotingOnlyNodeCoordinationStateTests.java
+++ x-pack/plugin/voting-only-node/src/test/java/org/elasticsearch/cluster/coordination/VotingOnlyNodeCoordinationStateTests.java
@@ -19,13 +19,27 @@ import java.util.stream.IntStream;
 public class VotingOnlyNodeCoordinationStateTests extends ESTestCase {
 
     public void testSafety() {
-        new CoordinationStateTestCluster(IntStream.range(0, randomIntBetween(1, 5))
-            .mapToObj(i -> new DiscoveryNode("node_" + i, buildNewFakeTransportAddress(), Collections.emptyMap(),
-                randomBoolean() ? DiscoveryNodeRole.BUILT_IN_ROLES :
-                    Sets.newHashSet(DiscoveryNodeRole.DATA_ROLE, DiscoveryNodeRole.INGEST_ROLE, DiscoveryNodeRole.MASTER_ROLE,
-                    VotingOnlyNodePlugin.VOTING_ONLY_NODE_ROLE), Version.CURRENT))
-            .collect(Collectors.toList()), new VotingOnlyNodePlugin.VotingOnlyNodeElectionStrategy())
-            .runRandomly();
+        new CoordinationStateTestCluster(
+            IntStream.range(0, randomIntBetween(1, 5))
+                .mapToObj(
+                    i -> new DiscoveryNode(
+                        "node_" + i,
+                        buildNewFakeTransportAddress(),
+                        Collections.emptyMap(),
+                        randomBoolean()
+                            ? DiscoveryNodeRole.BUILT_IN_ROLES
+                            : Sets.newHashSet(
+                                DiscoveryNodeRole.DATA_ROLE,
+                                DiscoveryNodeRole.INGEST_ROLE,
+                                DiscoveryNodeRole.MASTER_ROLE,
+                                VotingOnlyNodePlugin.VOTING_ONLY_NODE_ROLE
+                            ),
+                        Version.CURRENT
+                    )
+                )
+                .collect(Collectors.toList()),
+            new VotingOnlyNodePlugin.VotingOnlyNodeElectionStrategy()
+        ).runRandomly();
     }
 
 }

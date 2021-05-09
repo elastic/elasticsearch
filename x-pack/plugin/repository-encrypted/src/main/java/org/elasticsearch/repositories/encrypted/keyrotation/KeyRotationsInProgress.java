e4b24064766d8820ad046d0b9df09f5d4c5b6964
Armin Braun <me@obrown.io>
2021-05-10 00:17:23 +0200

    finished

--- x-pack/plugin/repository-encrypted/src/main/java/org/elasticsearch/repositories/encrypted/keyrotation/KeyRotationsInProgress.java
+++ x-pack/plugin/repository-encrypted/src/main/java/org/elasticsearch/repositories/encrypted/keyrotation/KeyRotationsInProgress.java
@@ -11,6 +11,7 @@ package org.elasticsearch.repositories.encrypted.keyrotation;
 import org.elasticsearch.Version;
 import org.elasticsearch.cluster.AbstractNamedDiffable;
 import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.xcontent.ToXContent;
@@ -29,7 +30,11 @@ public final class KeyRotationsInProgress extends AbstractNamedDiffable<ClusterS
     private final List<Entry> entries;
 
     private KeyRotationsInProgress(List<Entry> entries) {
-        this.entries = entries;
+        this.entries = List.copyOf(entries);
+    }
+
+    public static KeyRotationsInProgress of(List<Entry> entries) {
+        return new KeyRotationsInProgress(entries);
     }
 
     @Override
@@ -49,10 +54,47 @@ public final class KeyRotationsInProgress extends AbstractNamedDiffable<ClusterS
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
+        out.writeList(entries);
+    }
 
+    public static Entry waitingEntry(String repository, long repositoryStateId, String target, String source) {
+        return new Entry(target, repository, repositoryStateId, source);
     }
 
     public static final class Entry implements Writeable, ToXContent, RepositoryOperation {
+
+        private final String target;
+
+        private final String source;
+
+        private final String repo;
+
+        private final long repositoryStateId;
+
+        private final State state;
+
+        private Entry(String target, String repo, long repositoryStateId, String source) {
+            this.target = target;
+            this.repo = repo;
+            this.repositoryStateId = repositoryStateId;
+            this.state = State.WAITING;
+            this.source = source;
+        }
+
+        private Entry(String target, String repo, long repositoryStateId, State state, String source) {
+            this.target = target;
+            this.repo = repo;
+            this.repositoryStateId = repositoryStateId;
+            this.state = state;
+            this.source = source;
+        }
+
+        /**
+         * @return target key identifier to rotate to
+         */
+        public String target() {
+            return target;
+        }
 
         @Override
         public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
@@ -64,14 +106,73 @@ public final class KeyRotationsInProgress extends AbstractNamedDiffable<ClusterS
 
         }
 
+        public Entry asStarted() {
+            if (state != State.WAITING) {
+                assert false : "[" + this + "] is not waiting";
+                throw new IllegalStateException("can't start [" + this + "], is not waiting");
+            }
+            return new Entry(target, repo, repositoryStateId, State.STARTED, source);
+        }
+
+        public Entry asCleanup() {
+            if (state != State.STARTED) {
+                assert false : "[" + this + "] is not started";
+                throw new IllegalStateException("can't cleanup [" + this + "], is not started");
+            }
+            return new Entry(target, repo, repositoryStateId, State.CLEANUP, source);
+        }
+
         @Override
         public String repository() {
-            return null;
+            return repo;
         }
 
         @Override
         public long repositoryStateId() {
-            return 0;
+            return repositoryStateId;
+        }
+    }
+
+    public enum State implements Writeable {
+
+        /**
+         * Rotation is waiting to execute because there are repository operations to complete before this rotation may run.
+         */
+        WAITING((byte) 0),
+
+        /**
+         * Rotation is physically executing on the repository.
+         */
+        STARTED((byte) 1),
+
+        /**
+         * Rotation is removing previous key from the repository.
+         */
+        CLEANUP((byte) 2);
+
+        private final byte value;
+
+        State(byte value) {
+            this.value = value;
+        }
+
+        public static State readFrom(StreamInput in) throws IOException {
+            final byte value = in.readByte();
+            switch (value) {
+                case 0:
+                    return WAITING;
+                case 1:
+                    return STARTED;
+                case 2:
+                    return CLEANUP;
+                default:
+                    throw new IllegalArgumentException("No key rotation state for value [" + value + "]");
+            }
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) throws IOException {
+            out.writeByte(value);
         }
     }
 }
--- x-pack/plugin/repository-encrypted/src/main/java/org/elasticsearch/repositories/encrypted/keyrotation/package-info.java
+++ x-pack/plugin/repository-encrypted/src/main/java/org/elasticsearch/repositories/encrypted/keyrotation/package-info.java
@@ -14,6 +14,39 @@
  * up instead of starting to execute for the repository. The key rotation process will then wait for existing {@code RepositoryOperation}
  * to finish executing to ensure that no new files relevant to any snapshot are written to the repository before it starts.
  *
+ * Once the rotation is ready to execute its target key (TODO: hash/setting/identifier???)
+ * its status is moved to {@link org.elasticsearch.repositories.encrypted.keyrotation.KeyRotationsInProgress.State#STARTED}.
+ *
+ * After the cluster state update that moves the rotations state to {@code STARTED} has been executed the physical rotation to the new
+ * key can be executed.
+ * Once the physical key rotation has finished, a new {@link org.elasticsearch.repositories.RepositoryData} is written to the repository,
+ * encrypted and prepended with the new key identifier (TODO: can't we just put the key identifier used when writing a blob to every blob
+ *                                                            we write to avoid any special path for RepositoryData? This seems like it
+ *                                                            would also help debugging in other situations and the cost is trivial
+ *                                                            relative to any blob we write out?).
+ * The writing of this new repositoryData goes through the standard
+ * {@link org.elasticsearch.repositories.blobstore.BlobStoreRepository#writeIndexGen} path and uses a state update filter that moves the
+ * repository settings to the new password and moves the rotation entry to state {@code CLEANUP} in the same update that points the
+ * safe generation to the new repository data.
+ *
+ * Once the entry has been moved to state {@code CLEANUP} the previous key container referenced by the {@code #source} of the entry can be
+ * deleted from the repository safely.
+ *
+ * After the previous key's container has been deleted from the repository, the key rotation entry is removed from the cluster state, thus
+ * completing the rotation operation.
+ *
+ * Notes on failure modes:
+ * This state machine is completely resilient to master failover by using the same mechanism of action used in snapshot deletes.
+ * Concretely, it behaves as follows in failover scenarios:
+ *
+ * <p>a dangling waiting operation is executed on the new master node once it is ready to execute</p>
+ * <p>a dangling started operation is executed on the new master node, continuing to use the existing identifier (TODO: how idempotent can
+ *                                                                                                                      we get file
+ *                                                                                                                      operations
+ *                                                                                                                      here or do we just
+ *                                                                                                                      want to bail in
+ *                                                                                                                      this case?)</p>
+ * <p>a dangling cleanup state rotation is executed on the new master node</p>
  */
 package org.elasticsearch.repositories.encrypted.keyrotation;
 

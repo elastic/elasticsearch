9b096aad7604bd74b6f6cc5a38cdddf804eb5568
Johannes Westlund <johannes.wirkkala.westlund@gmail.com>
2021-05-13 15:10:40 +0200

    Fix: Change URLs from test.com to example.com
    As requested, the URLs used in the testing of
    the whitelisting of the java.net.URL class
    should be on the form "example.com" instead
    of "test.com".

--- modules/lang-painless/src/test/java/org/elasticsearch/painless/UrlTests.java
+++ modules/lang-painless/src/test/java/org/elasticsearch/painless/UrlTests.java
@@ -21,37 +21,37 @@ public class UrlTests extends ScriptTestCase {
         e = expectScriptThrows(MalformedURLException.class, () -> exec("new URL(null)"));
         assertEquals("Cannot invoke \"String.length()\" because \"spec\" is null", e.getMessage());
 
-        e = expectScriptThrows(MalformedURLException.class, () -> exec("new URL('abc://www.test.com')"));
+        e = expectScriptThrows(MalformedURLException.class, () -> exec("new URL('abc://www.example.com')"));
         assertEquals("unknown protocol: abc", e.getMessage());
     }
 
     public void testGetHost() {
-        assertEquals("www.test.com", exec("URL url = new URL('https://www.test.com:8080/path?query#fragment'); url.getHost()"));
+        assertEquals("www.example.com", exec("URL url = new URL('https://www.example.com:8080/path?query#fragment'); url.getHost()"));
 
-        assertEquals("www.test2.org", exec("URL url = new URL('https://www.test2.org'); url.getHost()"));
+        assertEquals("www.example2.org", exec("URL url = new URL('https://www.example2.org'); url.getHost()"));
     }
 
     public void testGetPath() {
-        assertEquals("/path", exec("URL url = new URL('https://www.test.com:8080/path?query#fragment'); url.getPath()"));
+        assertEquals("/path", exec("URL url = new URL('https://www.example.com:8080/path?query#fragment'); url.getPath()"));
 
-        assertEquals("/docs/example", exec("URL url = new URL('https://www.test.com/docs/example'); url.getPath()"));
+        assertEquals("/docs/example", exec("URL url = new URL('https://www.example.com/docs/example'); url.getPath()"));
     }
 
     public void testGetPort() {
-        assertEquals(8080, exec("URL url = new URL('https://www.test.com:8080/path?query#fragment'); url.getPort()"));
+        assertEquals(8080, exec("URL url = new URL('https://www.example.com:8080/path?query#fragment'); url.getPort()"));
 
-        assertEquals(3000, exec("URL url = new URL('https://www.test.com:3000'); url.getPort()"));
+        assertEquals(3000, exec("URL url = new URL('https://www.example.com:3000'); url.getPort()"));
     }
 
     public void testGetProtocol() {
-        assertEquals("https", exec("URL url = new URL('https://www.test.com:8080/path?query#fragment'); url.getProtocol()"));
+        assertEquals("https", exec("URL url = new URL('https://www.example.com:8080/path?query#fragment'); url.getProtocol()"));
 
-        assertEquals("ftp", exec("URL url = new URL('ftp://www.test.eu'); url.getProtocol()"));
+        assertEquals("ftp", exec("URL url = new URL('ftp://www.example.eu'); url.getProtocol()"));
     }
 
     public void testGetReference() {
-        assertEquals("reference", exec("URL url = new URL('https://www.test.com:8080/path?query#reference'); url.getRef()"));
+        assertEquals("reference", exec("URL url = new URL('https://www.example.com:8080/path?query#reference'); url.getRef()"));
 
-        assertEquals("section1", exec("URL url = new URL('https://www.test.com#section1'); url.getRef()"));
+        assertEquals("section1", exec("URL url = new URL('https://www.example.com#section1'); url.getRef()"));
     }
 }

40734f8272d3efcf2689b59e6ded1045bcb8eb35
David Kyle <david.kyle@elastic.co>
2021-05-25 17:00:55 +0100

    Remove big config file

--- x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/inference/nlp/tokenizers/BertTokenizerTests.java
+++ x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/inference/nlp/tokenizers/BertTokenizerTests.java
@@ -7,19 +7,10 @@
 
 package org.elasticsearch.xpack.ml.inference.nlp.tokenizers;
 
-import org.elasticsearch.ElasticsearchException;
-import org.elasticsearch.common.xcontent.DeprecationHandler;
-import org.elasticsearch.common.xcontent.NamedXContentRegistry;
-import org.elasticsearch.common.xcontent.XContentParser;
-import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.test.ESTestCase;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 import static org.hamcrest.Matchers.contains;
 
@@ -43,16 +34,7 @@ public class BertTokenizerTests extends ESTestCase {
         assertArrayEquals(new int[] {3, 0, 1, 2, 4}, tokenization.getTokenIds());
         assertArrayEquals(new int[] {-1, 0, 0, 1, -1}, tokenization.getTokenMap());
     }
-
-    public void testBertVocab() throws IOException {
-        BertTokenizer tokenizer = BertTokenizer.builder(loadVocab()).setDoLowerCase(false).build();
-
-        BertTokenizer.TokenizationResult tokenization = tokenizer.tokenize("Jim bought 300 shares of Acme Corp. in 2006", true);
-
-        assertArrayEquals(new int[] {101, 3104, 3306, 3127, 6117, 1104, 138, 1665, 3263, 13619, 119, 1107, 1386, 102},
-            tokenization.getTokenIds());
-    }
-
+    
     public void testNeverSplitTokens() {
         final String specialToken = "SP001";
 
@@ -90,28 +72,6 @@ public class BertTokenizerTests extends ESTestCase {
 
             BertTokenizer.TokenizationResult tokenization = tokenizer.tokenize("Elasticsearch fun", false);
             assertThat(tokenization.getTokens(), contains("elastic", "##search", "fun"));
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private List<String> loadVocab() throws IOException {
-
-        String path = "/org/elasticsearch/xpack/core/ml/inference/pipeline_config.json";
-        URL resource = getClass().getResource(path);
-        if (resource == null) {
-            throw new ElasticsearchException("Could not find resource stored at [" + path + "]");
-        }
-        try(XContentParser parser =
-                XContentType.JSON.xContent().createParser(
-                    NamedXContentRegistry.EMPTY,
-                    DeprecationHandler.THROW_UNSUPPORTED_OPERATION,
-                    getClass().getResourceAsStream(path))) {
-
-            Map<String, Object> map = parser.map();
-            assertNotNull(map.get("task_type"));
-            assertNotNull(map.get("vocab"));
-
-            return (List<String>)map.get("vocab");
         }
     }
 }

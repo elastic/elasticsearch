elastic/elasticsearch

commit 6cf4eb72733e0b7dacaed1e504c033a9e6eb7cae

Deprecate multiple path.data entries (#71207)
This commit adds a node level deprecation log message when multiple
data paths are specified.

relates #71205

test/framework/src/main/java/org/elasticsearch/test/AbstractMultiClustersTestCase.java
@@ -36,6 +36,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.elasticsearch.discovery.DiscoveryModule.DISCOVERY_SEED_PROVIDERS_SETTING;
 import static org.elasticsearch.discovery.SettingsBasedSeedHostsProvider.DISCOVERY_SEED_HOSTS_SETTING;
@@ -100,6 +101,13 @@ public abstract class AbstractMultiClustersTestCase extends ESTestCase {
         }
         clusterGroup = new ClusterGroup(clusters);
         configureAndConnectsToRemoteClusters();
+    }
+
+    @Override
+    public List<String> filteredWarnings() {
+        return Stream.concat(super.filteredWarnings().stream(),
+            List.of("Configuring multiple [path.data] paths is deprecated. Use RAID or other system level features for utilizing " +
+            "multiple disks. This feature will be removed in 8.0.").stream()).collect(Collectors.toList());
     }
 
     @After

test/framework/src/main/java/org/elasticsearch/test/ESTestCase.java
@@ -407,12 +407,11 @@ public abstract class ESTestCase extends LuceneTestCase {
         //appropriate test
         try {
             final List<String> warnings = threadContext.getResponseHeaders().get("Warning");
-            if (warnings != null && JvmInfo.jvmInfo().getBundledJdk() == false) {
+            if (warnings != null) {
                 // unit tests do not run with the bundled JDK, if there are warnings we need to filter the no-jdk deprecation warning
                 final List<String> filteredWarnings = warnings
                     .stream()
-                    .filter(k -> k.contains(
-                        "no-jdk distributions that do not bundle a JDK are deprecated and will be removed in a future release") == false)
+                    .filter(k -> filteredWarnings().stream().anyMatch(s -> s.contains(k)))
                     .collect(Collectors.toList());
                 assertThat("unexpected warning headers", filteredWarnings, empty());
             } else {
@@ -420,6 +419,14 @@ public abstract class ESTestCase extends LuceneTestCase {
             }
         } finally {
             resetDeprecationLogger();
+        }
+    }
+
+    protected List<String> filteredWarnings() {
+        if (JvmInfo.jvmInfo().getBundledJdk() == false) {
+            return List.of("no-jdk distributions that do not bundle a JDK are deprecated and will be removed in a future release");
+        } else {
+            return List.of();
         }
     }
 

server/src/main/java/org/elasticsearch/node/Node.java
@@ -306,6 +306,13 @@ public class Node implements Closeable {
                     Build.CURRENT.getQualifiedVersion());
             }
 
+            if (initialEnvironment.dataFiles().length > 1) {
+                // NOTE: we use initialEnvironment here, but assertEquivalent below ensures the data paths do not change
+                deprecationLogger.deprecate(DeprecationCategory.SETTINGS, "multiple-data-paths",
+                    "Configuring multiple [path.data] paths is deprecated. Use RAID or other system level features for utilizing " +
+                        "multiple disks. This feature will be removed in 8.0.");
+            }
+
             if (logger.isDebugEnabled()) {
                 logger.debug("using config [{}], data [{}], logs [{}], plugins [{}]",
                     initialEnvironment.configFile(), Arrays.toString(initialEnvironment.dataFiles()),

test/framework/src/test/java/org/elasticsearch/test/test/InternalTestClusterTests.java
@@ -42,6 +42,7 @@ import java.util.Random;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.elasticsearch.discovery.DiscoveryModule.DISCOVERY_SEED_PROVIDERS_SETTING;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertFileExists;
@@ -58,6 +59,13 @@ public class InternalTestClusterTests extends ESTestCase {
 
     private static Collection<Class<? extends Plugin>> mockPlugins() {
         return Arrays.asList(getTestTransportPlugin(), MockHttpTransport.TestPlugin.class);
+    }
+
+    @Override
+    protected List<String> filteredWarnings() {
+        return Stream.concat(super.filteredWarnings().stream(),
+            List.of("Configuring multiple [path.data] paths is deprecated. Use RAID or other system level features for utilizing " +
+            "multiple disks. This feature will be removed in 8.0.").stream()).collect(Collectors.toList());
     }
 
     public void testInitializiationIsConsistent() {

x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/CcrIntegTestCase.java
@@ -308,6 +308,13 @@ public abstract class CcrIntegTestCase extends ESTestCase {
         };
     }
 
+    @Override
+    public List<String> filteredWarnings() {
+        return Stream.concat(super.filteredWarnings().stream(),
+            List.of("Configuring multiple [path.data] paths is deprecated. Use RAID or other system level features for utilizing " +
+            "multiple disks. This feature will be removed in 8.0.").stream()).collect(Collectors.toList());
+    }
+
     @AfterClass
     public static void stopClusters() throws IOException {
         IOUtils.close(clusterGroup);

commit 3c2adabd7efb5f945d8ab9acb41a49cab100f61f

Fix EnsureNoWarning assertion backport(#73647) (#73685)
EnsureNoWarnings method should assert that there is no other warnings
than the allowed "predefined" warnings in filteredWarnings() method

bug introduced in #71207
backports #73647

server/src/test/java/org/elasticsearch/bootstrap/BootstrapChecksTests.java
@@ -418,8 +418,10 @@ public class BootstrapChecksTests extends AbstractBootstrapCheckTestCase {
 
     public void testSystemCallFilterCheck() throws NodeValidationException {
         final AtomicBoolean isSystemCallFilterInstalled = new AtomicBoolean();
-        BootstrapContext context = randomBoolean() ? createTestContext(Settings.builder().put("bootstrap.system_call_filter", true)
-            .build(), null) : emptyContext;
+        boolean useBootstrapSystemCallFilter = randomBoolean();
+        BootstrapContext context = useBootstrapSystemCallFilter ?
+            createTestContext(Settings.builder().put("bootstrap.system_call_filter", true).build(), null)
+            : emptyContext;
 
         final BootstrapChecks.SystemCallFilterCheck systemCallFilterEnabledCheck = new BootstrapChecks.SystemCallFilterCheck() {
             @Override
@@ -435,9 +437,20 @@ public class BootstrapChecksTests extends AbstractBootstrapCheckTestCase {
             e.getMessage(),
             containsString("system call filters failed to install; " +
                 "check the logs and fix your configuration or disable system call filters at your own risk"));
+        if (useBootstrapSystemCallFilter) {
+            assertWarnings("[bootstrap.system_call_filter] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release!" +
+                " See the breaking changes documentation for the next major version.");
+        }
 
         isSystemCallFilterInstalled.set(true);
         BootstrapChecks.check(context, true, Collections.singletonList(systemCallFilterEnabledCheck));
+        if (useBootstrapSystemCallFilter) {
+            assertWarnings("[bootstrap.system_call_filter] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release!" +
+                " See the breaking changes documentation for the next major version.");
+        }
+
         BootstrapContext context_1 = createTestContext(Settings.builder().put("bootstrap.system_call_filter", false).build(), null);
         final BootstrapChecks.SystemCallFilterCheck systemCallFilterNotEnabledCheck = new BootstrapChecks.SystemCallFilterCheck() {
             @Override
@@ -447,8 +460,12 @@ public class BootstrapChecksTests extends AbstractBootstrapCheckTestCase {
         };
         isSystemCallFilterInstalled.set(false);
         BootstrapChecks.check(context_1, true, Collections.singletonList(systemCallFilterNotEnabledCheck));
+        assertWarnings("[bootstrap.system_call_filter] setting was deprecated in Elasticsearch and will be removed in a future release!" +
+                " See the breaking changes documentation for the next major version.");
         isSystemCallFilterInstalled.set(true);
         BootstrapChecks.check(context_1, true, Collections.singletonList(systemCallFilterNotEnabledCheck));
+        assertWarnings("[bootstrap.system_call_filter] setting was deprecated in Elasticsearch and will be removed in a future release!" +
+                " See the breaking changes documentation for the next major version.");
     }
 
     public void testMightForkCheck() throws NodeValidationException {

test/framework/src/main/java/org/elasticsearch/index/mapper/MapperTestCase.java
@@ -689,6 +689,8 @@ public abstract class MapperTestCase extends MapperServiceTestCase {
         } else {
             expectThrows(MapperParsingException.class, () -> mapper.parse(source(b -> b.nullField("field"))));
         }
+
+        assertWarnings(getParseMinimalWarnings());
     }
 
     protected boolean allowsNullValues() {

test/framework/src/main/java/org/elasticsearch/test/AbstractMultiClustersTestCase.java
@@ -107,8 +107,10 @@ public abstract class AbstractMultiClustersTestCase extends ESTestCase {
     @Override
     public List<String> filteredWarnings() {
         return Stream.concat(super.filteredWarnings().stream(),
-            Stream.of("Configuring multiple [path.data] paths is deprecated. Use RAID or other system level features for utilizing " +
-            "multiple disks. This feature will be removed in 8.0.")).collect(Collectors.toList());
+            Stream.of("setting [path.shared_data] is deprecated and will be removed in a future release",
+            "Configuring [path.data] with a list is deprecated. Instead specify as a string value.",
+            "Configuring multiple [path.data] paths is deprecated. Use RAID or other system level features for utilizing " +
+                "multiple disks. This feature will be removed in 8.0.")).collect(Collectors.toList());
     }
 
     @After

test/framework/src/main/java/org/elasticsearch/test/ESSingleNodeTestCase.java
@@ -54,7 +54,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.elasticsearch.cluster.coordination.ClusterBootstrapService.INITIAL_MASTER_NODES_SETTING;
 import static org.elasticsearch.discovery.SettingsBasedSeedHostsProvider.DISCOVERY_SEED_HOSTS_SETTING;
@@ -187,6 +190,14 @@ public abstract class ESSingleNodeTestCase extends ESTestCase {
     /** True if a dummy http transport should be used, or false if the real http transport should be used. */
     protected boolean addMockHttpTransport() {
         return true;
+    }
+
+    @Override
+    protected List<String> filteredWarnings() {
+        return Stream.concat(super.filteredWarnings().stream(),
+            Stream.of("[index.data_path] setting was deprecated in Elasticsearch and will be removed in a future release! " +
+                    "See the breaking changes documentation for the next major version."))
+            .collect(Collectors.toList());
     }
 
     private Node newNode() {

test/framework/src/main/java/org/elasticsearch/test/ESTestCase.java
@@ -415,7 +415,7 @@ public abstract class ESTestCase extends LuceneTestCase {
                 // unit tests do not run with the bundled JDK, if there are warnings we need to filter the no-jdk deprecation warning
                 final List<String> filteredWarnings = warnings
                     .stream()
-                    .filter(k -> filteredWarnings().stream().anyMatch(s -> s.contains(k)))
+                    .filter(k -> filteredWarnings().stream().noneMatch(s -> k.contains(s)))
                     .collect(Collectors.toList());
                 assertThat("unexpected warning headers", filteredWarnings, empty());
             } else {
@@ -428,13 +428,16 @@ public abstract class ESTestCase extends LuceneTestCase {
 
     protected List<String> filteredWarnings() {
         List<String> filtered = new ArrayList<>();
-        if (enableJodaDeprecationWarningsCheck()) {
+        filtered.add("Configuring [path.data] with a list is deprecated. Instead specify as a string value");
+        filtered.add("setting [path.shared_data] is deprecated and will be removed in a future release");
+        if (enableJodaDeprecationWarningsCheck() == false) {
             filtered.add(JodaDeprecationPatterns.USE_NEW_FORMAT_SPECIFIERS);
         }
         if (JvmInfo.jvmInfo().getBundledJdk() == false) {
             filtered.add("no-jdk distributions that do not bundle a JDK are deprecated and will be removed in a future release");
         }
         return filtered;
+
     }
 
     /**

x-pack/plugin/autoscaling/src/test/java/org/elasticsearch/xpack/autoscaling/storage/ReactiveStorageDeciderDecisionTests.java
@@ -140,6 +140,19 @@ public class ReactiveStorageDeciderDecisionTests extends AutoscalingTestCase {
         this.subjectShards = new HashSet<>(randomSubsetOf(randomIntBetween(1, shardIds.size()), shardIds));
     }
 
+    @Override
+    protected List<String> filteredWarnings() {
+        return Stream.concat(
+            super.filteredWarnings().stream(),
+            Stream.of(
+                "[index.routing.allocation.include._tier] setting was deprecated in Elasticsearch "
+                    + "and will be removed in a future release! See the breaking changes documentation for the next major version.",
+                "[index.routing.allocation.require._tier] setting was deprecated in Elasticsearch "
+                    + "and will be removed in a future release! See the breaking changes documentation for the next major version."
+            )
+        ).collect(Collectors.toList());
+    }
+
     public void testStoragePreventsAllocation() {
         ClusterState lastState = null;
         int maxRounds = state.getRoutingNodes().unassigned().size() + 3; // (allocated + start + detect-same)

x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/cluster/routing/allocation/DataTierAllocationDeciderTests.java
@@ -114,6 +114,8 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
                 containsString("node does not match all cluster setting [cluster.routing.allocation.require._tier] " +
                     "tier filters [data_hot]"));
         }
+        assertWarnings("[cluster.routing.allocation.require._tier] setting was deprecated in Elasticsearch " +
+            "and will be removed in a future release! See the breaking changes documentation for the next major version.");
     }
 
     public void testClusterIncludes() {
@@ -126,6 +128,8 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
             .build());
         Decision d;
         RoutingNode node;
+        assertWarnings("[cluster.routing.allocation.include._tier] setting was deprecated in Elasticsearch " +
+            "and will be removed in a future release! See the breaking changes documentation for the next major version.");
 
         for (DiscoveryNode n : Arrays.asList(WARM_NODE, DATA_NODE, COLD_NODE)) {
             node = new RoutingNode(n.getId(), n, shard);
@@ -161,7 +165,8 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
             .build());
         Decision d;
         RoutingNode node;
-
+        assertWarnings("[cluster.routing.allocation.exclude._tier] setting was deprecated in Elasticsearch " +
+            "and will be removed in a future release! See the breaking changes documentation for the next major version.");
         for (DiscoveryNode n : Arrays.asList(WARM_NODE, DATA_NODE)) {
             node = new RoutingNode(n.getId(), n, shard);
             d = decider.canAllocate(shard, node, allocation);
@@ -216,6 +221,8 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
             assertThat(d.getExplanation(),
                 containsString("node does not match all index setting [index.routing.allocation.require._tier] tier filters [data_hot]"));
         }
+        assertWarnings("[index.routing.allocation.require._tier] setting was deprecated in Elasticsearch " +
+            "and will be removed in a future release! See the breaking changes documentation for the next major version.");
     }
 
     public void testIndexIncludes() {
@@ -250,6 +257,8 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
                 containsString("node does not match any index setting [index.routing.allocation.include._tier] " +
                     "tier filters [data_warm,data_cold]"));
         }
+        assertWarnings("[index.routing.allocation.include._tier] setting was deprecated in Elasticsearch " +
+            "and will be removed in a future release! See the breaking changes documentation for the next major version.");
     }
 
     public void testIndexExcludes() {
@@ -285,6 +294,8 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
             d = decider.canRemain(shard, node, allocation);
             assertThat(n.toString(), d.type(), equalTo(Decision.Type.YES));
         }
+        assertWarnings("[index.routing.allocation.exclude._tier] setting was deprecated in Elasticsearch " +
+            "and will be removed in a future release! See the breaking changes documentation for the next major version.");
     }
 
     public void testIndexPrefer() {
@@ -428,6 +439,9 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
             assertThat(node.toString(), d.getExplanation(),
                 containsString("index has a preference for tiers [data_warm,data_cold] and node has tier [data_warm]"));
         }
+
+        assertWarnings("[index.routing.allocation.include._tier] setting was deprecated in Elasticsearch " +
+            "and will be removed in a future release! See the breaking changes documentation for the next major version.");
     }
 
     public void testIndexPreferWithExclude() {
@@ -452,6 +466,7 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
         allocation.debugDecision(true);
         Decision d;
         RoutingNode node;
+
 
         for (DiscoveryNode n : Arrays.asList(HOT_NODE, COLD_NODE, CONTENT_NODE)) {
             node = new RoutingNode(n.getId(), n, shard);
@@ -490,6 +505,8 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
             assertThat(node.toString(), d.getExplanation(),
                 containsString("node matches any index setting [index.routing.allocation.exclude._tier] tier filters [data_warm]"));
         }
+        assertWarnings("[index.routing.allocation.exclude._tier] setting was deprecated in Elasticsearch " +
+            "and will be removed in a future release! See the breaking changes documentation for the next major version.");
     }
 
     public void testIndexPreferWithRequire() {
@@ -552,6 +569,8 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
             assertThat(node.toString(), d.getExplanation(),
                 containsString("index has a preference for tiers [data_warm,data_cold] and node has tier [data_warm]"));
         }
+        assertWarnings("[index.routing.allocation.require._tier] setting was deprecated in Elasticsearch " +
+            "and will be removed in a future release! See the breaking changes documentation for the next major version.");
     }
 
     public void testClusterAndIndex() {
@@ -601,6 +620,10 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
             d = decider.canRemain(shard, node, allocation);
             assertThat(n.toString(), d.type(), equalTo(Decision.Type.YES));
         }
+        assertWarnings("[cluster.routing.allocation.exclude._tier] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release! See the breaking changes documentation for the next major version.",
+            "[index.routing.allocation.include._tier] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release! See the breaking changes documentation for the next major version.");
     }
 
     public void testTierNodesPresent() {
@@ -670,7 +693,10 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
             EmptySnapshotsInfoService.INSTANCE);
 
         ClusterState clusterState = prepareState(service.reroute(ClusterState.EMPTY_STATE, "initial state"));
-
+        assertWarnings("[cluster.routing.allocation.include._tier] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release! See the breaking changes documentation for the next major version.",
+            "[cluster.routing.allocation.exclude._tier] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release! See the breaking changes documentation for the next major version.");
         RoutingAllocation allocation = new RoutingAllocation(allocationDeciders, clusterState.getRoutingNodes(), clusterState,
             null, null, 0);
         allocation.debugDecision(true);
@@ -727,6 +753,12 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
         Settings settings = builder.build();
         IllegalArgumentException exception = expectThrows(IllegalArgumentException.class, () -> setting.get(settings));
         assertThat(exception.getMessage(), equalTo("[data_frozen] tier can only be used for partial searchable snapshots"));
+        allowedWarnings("[index.routing.allocation.exclude._tier] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release! See the breaking changes documentation for the next major version.",
+            "[index.routing.allocation.include._tier] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release! See the breaking changes documentation for the next major version.",
+            "[index.routing.allocation.require._tier] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release! See the breaking changes documentation for the next major version.");
     }
 
     public void testFrozenLegalForPartialSnapshot() {
@@ -739,6 +771,12 @@ public class DataTierAllocationDeciderTests extends ESAllocationTestCase {
 
         // validate do not throw
         assertThat(setting.get(settings), equalTo(DATA_FROZEN));
+        allowedWarnings("[index.routing.allocation.exclude._tier] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release! See the breaking changes documentation for the next major version.",
+            "[index.routing.allocation.include._tier] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release! See the breaking changes documentation for the next major version.",
+            "[index.routing.allocation.require._tier] setting was deprecated in Elasticsearch " +
+                "and will be removed in a future release! See the breaking changes documentation for the next major version.");
     }
 
     public void testDefaultValueForPreference() {

x-pack/plugin/deprecation/src/test/java/org/elasticsearch/xpack/deprecation/ClusterDeprecationChecksTests.java
@@ -301,6 +301,8 @@ public class ClusterDeprecationChecksTests extends ESTestCase {
         assertThat(issues, hasSize(1));
         assertThat(issues.get(0).getDetails(),
             equalTo("Index templates [multiple-types] define multiple types and so will cause errors when used in index creation"));
+        assertWarnings("Index template multiple-types contains multiple typed mappings;" +
+            " templates in 8x will only support a single mapping");
 
         Metadata goodMetadata = Metadata.builder()
             .templates(ImmutableOpenMap.<String, IndexTemplateMetadata>builder().fPut("single-type", singleType).build())
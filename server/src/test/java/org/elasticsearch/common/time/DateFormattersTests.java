fb2361b74751fa813347deb253ebb810448b941d
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2021-06-07 09:33:18 +0200

    Change year max digits for strict_date_optional_time and date_optional_time backports(#73034) (#73750)
    We changed the default joda behaviour in strict_date_optional_time to
    max 4 digits in a year. Java.time implementation should behave the same way.
    At the same time date_optional_time should have 9digits for year part.
    
    closes #52396
    closes #72191
    backports #73034

--- server/src/test/java/org/elasticsearch/common/time/DateFormattersTests.java
+++ server/src/test/java/org/elasticsearch/common/time/DateFormattersTests.java
@@ -90,8 +90,8 @@ public class DateFormattersTests extends ESTestCase {
     public void testPrintersLongMinMaxValue() {
         for (FormatNames format : FormatNames.values()) {
             DateFormatter formatter = DateFormatters.forPattern(format.getSnakeCaseName());
-            formatter.format(DateFieldMapper.Resolution.MILLISECONDS.toInstant(Long.MIN_VALUE));
-            formatter.format(DateFieldMapper.Resolution.MILLISECONDS.toInstant(Long.MAX_VALUE));
+            formatter.format(DateFieldMapper.Resolution.MILLISECONDS.toInstant(DateUtils.MAX_MILLIS_BEFORE_MINUS_9999));
+            formatter.format(DateFieldMapper.Resolution.MILLISECONDS.toInstant(DateUtils.MAX_MILLIS_BEFORE_9999));
         }
         assertWarnings("Format name \"week_year\" is deprecated and will be removed in a future version. Use \"weekyear\" format instead");
     }

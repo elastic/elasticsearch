61697d0d5e085d757c5ce6c4d58377920b34ff28
Yannick Welsch <yannick@welsch.lu>
2021-06-14 10:36:42 +0200

    docs

--- server/src/main/java/org/elasticsearch/index/search/stats/ShardFieldUsageStats.java
+++ server/src/main/java/org/elasticsearch/index/search/stats/ShardFieldUsageStats.java
@@ -8,23 +8,32 @@
 
 package org.elasticsearch.index.search.stats;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * Records which fields are being accessed by searches, aggregations, etc.
+ */
 public class ShardFieldUsageStats {
 
     private final Map<String, InternalFieldStats> perFieldStats = new ConcurrentHashMap<>();
 
+    /**
+     * Called whenever a field is used in an aggregation context
+     */
     public void onFieldAggregation(String field) {
         perFieldStats.computeIfAbsent(field, f -> new InternalFieldStats()).aggregationCount.incrementAndGet();
     }
 
+    /**
+     * Clears the currently recorded stats
+     */
     public void clear() {
         perFieldStats.clear();
     }
-
 
     static class InternalFieldStats {
         final AtomicLong aggregationCount = new AtomicLong();
@@ -35,6 +44,9 @@ public class ShardFieldUsageStats {
             this.aggregationCount = aggregationCount;
         }
 
+        /**
+         * Returns the number of times the given field was used in an aggregation context
+         */
         public long getAggregationCount() {
             return aggregationCount;
         }
@@ -42,11 +54,14 @@ public class ShardFieldUsageStats {
         private final long aggregationCount;
     }
 
+    /**
+     * Returns an immutable snapshot of the stats
+     */
     public Map<String, FieldStats> getPerFieldStats() {
         final Map<String, FieldStats> stats = new HashMap<>(perFieldStats.size());
         for (Map.Entry<String, InternalFieldStats> entry : perFieldStats.entrySet()) {
             stats.put(entry.getKey(), new FieldStats(entry.getValue().aggregationCount.get()));
         }
-        return stats;
+        return Collections.unmodifiableMap(stats);
     }
 }

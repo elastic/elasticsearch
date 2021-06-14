80593b43f6419e8fdd438608194b9a1e953a9074
Martijn van Groningen <martijn.v.groningen@gmail.com>
2021-06-14 19:15:10 +0200

    Tidy up deprecation code. (#74065)
    Removed unused code and made fields immutable.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/deprecation/DeprecationIssue.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/deprecation/DeprecationIssue.java
@@ -59,15 +59,10 @@ public class DeprecationIssue implements Writeable, ToXContentObject {
         }
     }
 
-    private Level level;
-    private String message;
-    private String url;
-    private String details;
-
-    // pkg-private for tests
-    DeprecationIssue() {
-
-    }
+    private final Level level;
+    private final String message;
+    private final String url;
+    private final String details;
 
     public DeprecationIssue(Level level, String message, String url, @Nullable String details) {
         this.level = level;

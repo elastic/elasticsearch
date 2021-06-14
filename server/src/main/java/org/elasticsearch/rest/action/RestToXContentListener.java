0989f881c05546dd8cf4b8162851a19036129951
David Turner <david.turner@elastic.co>
2021-06-14 06:58:58 +0100

    Log at DEBUG only on disconnect during cancellation (#74042)
    If a `NodeDisconnectedException` happens when sending a ban for a task
    then today we log a message at `INFO` or `WARN` indicating that the ban
    failed, but we don't indicate why. The message also uses a default
    `toString()` for an inner class which is unhelpful.
    
    Ban failures during disconnections are benign and somewhat expected, and
    task cancellation respects disconnections anyway (#65443). There's not
    much the user can do about these messages either, and they can be
    confusing and draw attention away from the real problem.
    
    With this commit we log the failure messages at `DEBUG` on
    disconnections, and include the exception details. We also include the
    exception message for other kinds of failures, and we fix up a few cases
    where a useless default `toString()` implementation was used in log
    messages.
    
    Slightly relates #72968 in that these messages tend to obscure a
    connectivity issue.

--- server/src/main/java/org/elasticsearch/rest/action/RestToXContentListener.java
+++ server/src/main/java/org/elasticsearch/rest/action/RestToXContentListener.java
@@ -34,10 +34,16 @@ public class RestToXContentListener<Response extends ToXContentObject> extends R
     public RestResponse buildResponse(Response response, XContentBuilder builder) throws Exception {
         assert response.isFragment() == false; //would be nice if we could make default methods final
         response.toXContent(builder, channel.request());
+
         return new BytesRestResponse(getStatus(response), builder);
     }
 
     protected RestStatus getStatus(Response response) {
         return RestStatus.OK;
+    }
+
+    boolean assertBuilderClosed(XContentBuilder xContentBuilder) {
+        assert xContentBuilder.generator().isClosed() : "callers should ensure the XContentBuilder is closed themselves";
+        return true;
     }
 }

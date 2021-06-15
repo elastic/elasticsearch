5748a3b20861588ca0e8a60557580a73c52a13f9
Luegg <lukas.wegmann@elastic.co>
2021-06-15 16:44:37 +0200

    rm unused import

--- x-pack/plugin/ql/src/main/java/org/elasticsearch/xpack/ql/tree/Node.java
+++ x-pack/plugin/ql/src/main/java/org/elasticsearch/xpack/ql/tree/Node.java
@@ -17,7 +17,6 @@ import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 import static java.util.Collections.emptyList;
 

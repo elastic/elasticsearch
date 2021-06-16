ff4abb91a987ee950d0855befd7b688a276eae5a
jaymode <jay@elastic.co>
2021-06-16 14:10:43 -0600

    fix warnings

--- server/src/main/java/org/elasticsearch/action/admin/indices/alias/get/TransportGetAliasesAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/alias/get/TransportGetAliasesAction.java
@@ -187,7 +187,7 @@ public class TransportGetAliasesAction extends TransportMasterNodeReadAction<Get
 
         if (systemAliases.isEmpty() == false) {
             deprecationLogger.deprecate(DeprecationCategory.API, "open_system_alias_access",
-                "this request accesses aliases with names reserved for system indices: {}, but in a future major version, direct" +
+                "this request accesses aliases with names reserved for system indices: {}, but in a future major version, direct " +
                     "access to system indices and their aliases will not be allowed", systemAliases);
         }
         if (netNewSystemAliases.isEmpty() == false) {

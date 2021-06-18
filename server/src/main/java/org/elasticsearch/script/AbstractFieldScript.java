23e0470709f30ccfc6c0751bb0d4e35b3ca6c309
Luca Cavanna <javanna@users.noreply.github.com>
2021-06-18 17:09:18 +0200

    remove system out leftover

--- server/src/main/java/org/elasticsearch/script/AbstractFieldScript.java
+++ server/src/main/java/org/elasticsearch/script/AbstractFieldScript.java
@@ -109,9 +109,7 @@ public abstract class AbstractFieldScript {
     protected abstract void emitFromObject(Object v);
 
     protected final void emitFromSource() {
-        System.out.println("emitFromSource");
         for (Object v : extractFromSource(fieldName)) {
-            System.out.println("value " + v + " - field: " + fieldName);
             emitFromObject(v);
         }
     }

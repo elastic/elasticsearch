292ec08cac1efaebf1ff22a2f564ae3b3adf9a32
Eduardo Maksoud <eduardomaksoud@gmail.com>
2021-06-22 16:17:50 -0300

    fixed formatting

--- server/src/test/java/org/elasticsearch/search/aggregations/bucket/composite/CompositeAggregatorTests.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/bucket/composite/CompositeAggregatorTests.java
@@ -2503,7 +2503,6 @@ public class CompositeAggregatorTests  extends AggregatorTestCase {
         }
     }
 
-
     public void testInvalidCompositeValueSourceName() throws IOException {
         String invalidName = randomAlphaOfLengthBetween(0, 5)
             + ILLEGAL_FIELD_NAME_CHARACTERS[randomIntBetween(0, ILLEGAL_FIELD_NAME_CHARACTERS.length - 1)]

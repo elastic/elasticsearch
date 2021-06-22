db0f0eee1c2e577bbf952b4c331102cdf4fe0043
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2021-06-22 16:28:36 -0400

    making the change to use a master node action not bwc

--- server/src/main/java/org/elasticsearch/action/admin/cluster/snapshots/features/ResetFeatureStateRequest.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/snapshots/features/ResetFeatureStateRequest.java
@@ -9,7 +9,6 @@
 package org.elasticsearch.action.admin.cluster.snapshots.features;
 
 import org.elasticsearch.Version;
-import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionRequestValidationException;
 import org.elasticsearch.action.support.master.MasterNodeRequest;
 import org.elasticsearch.common.io.stream.StreamInput;
@@ -20,15 +19,14 @@ import java.io.IOException;
 /** Request for resetting feature state */
 public class ResetFeatureStateRequest extends MasterNodeRequest<ResetFeatureStateRequest> {
 
-    private static final Version FEATURE_RESET_ON_MASTER = Version.V_8_0_0;
-
+    private static final Version FEATURE_RESET_ON_MASTER = Version.V_7_14_0;
     public static ResetFeatureStateRequest fromStream(StreamInput in) throws IOException {
-        if (in.getVersion().onOrAfter(FEATURE_RESET_ON_MASTER)) {
-            return new ResetFeatureStateRequest(in);
-        } else {
-            LegacyResetFeatureStateRequest legacyResetFeatureStateRequest = new LegacyResetFeatureStateRequest(in);
-            return legacyResetFeatureStateRequest.toResetFeatureStateRequest();
+        if (in.getVersion().before(FEATURE_RESET_ON_MASTER)) {
+            throw new IllegalStateException(
+                "feature reset is not available in a cluster that have nodes with version before " + FEATURE_RESET_ON_MASTER
+            );
         }
+        return new ResetFeatureStateRequest(in);
     }
 
     public ResetFeatureStateRequest() {}
@@ -39,11 +37,12 @@ public class ResetFeatureStateRequest extends MasterNodeRequest<ResetFeatureStat
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
-        if (out.getVersion().onOrAfter(FEATURE_RESET_ON_MASTER)) {
-            super.writeTo(out);
-        } else {
-            this.toLegacyStateRequest().writeTo(out);
+        if (out.getVersion().before(FEATURE_RESET_ON_MASTER)) {
+            throw new IllegalStateException(
+                "feature reset is not available in a cluster that have nodes with version before " + FEATURE_RESET_ON_MASTER
+            );
         }
+        super.writeTo(out);
     }
 
     @Override
@@ -51,34 +50,4 @@ public class ResetFeatureStateRequest extends MasterNodeRequest<ResetFeatureStat
         return null;
     }
 
-    private LegacyResetFeatureStateRequest toLegacyStateRequest() {
-        LegacyResetFeatureStateRequest legacyResetFeatureStateRequest = new LegacyResetFeatureStateRequest();
-        legacyResetFeatureStateRequest.setParentTask(this.getParentTask());
-        return legacyResetFeatureStateRequest;
-    }
-
-    private static class LegacyResetFeatureStateRequest extends ActionRequest {
-
-        private LegacyResetFeatureStateRequest() {}
-
-        private LegacyResetFeatureStateRequest(StreamInput in) throws IOException {
-            super(in);
-        }
-
-        @Override
-        public void writeTo(StreamOutput out) throws IOException {
-            super.writeTo(out);
-        }
-
-        @Override
-        public ActionRequestValidationException validate() {
-            return null;
-        }
-
-        ResetFeatureStateRequest toResetFeatureStateRequest() {
-            ResetFeatureStateRequest request = new ResetFeatureStateRequest();
-            request.setParentTask(this.getParentTask());
-            return request;
-        }
-    }
 }

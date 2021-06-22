ed102adb601f2a81885cb85a62ab6db2c48b8240
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2021-06-22 15:07:51 -0400

    Make reset feature request a master node action

--- server/src/main/java/org/elasticsearch/action/admin/cluster/snapshots/features/ResetFeatureStateRequest.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/snapshots/features/ResetFeatureStateRequest.java
@@ -8,29 +8,77 @@
 
 package org.elasticsearch.action.admin.cluster.snapshots.features;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionRequestValidationException;
+import org.elasticsearch.action.support.master.MasterNodeRequest;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 
 import java.io.IOException;
 
 /** Request for resetting feature state */
-public class ResetFeatureStateRequest extends ActionRequest {
+public class ResetFeatureStateRequest extends MasterNodeRequest<ResetFeatureStateRequest> {
+
+    private static final Version FEATURE_RESET_ON_MASTER = Version.V_8_0_0;
+
+    public static ResetFeatureStateRequest fromStream(StreamInput in) throws IOException {
+        if (in.getVersion().onOrAfter(FEATURE_RESET_ON_MASTER)) {
+            return new ResetFeatureStateRequest(in);
+        } else {
+            LegacyResetFeatureStateRequest legacyResetFeatureStateRequest = new LegacyResetFeatureStateRequest(in);
+            return legacyResetFeatureStateRequest.toResetFeatureStateRequest();
+        }
+    }
 
     public ResetFeatureStateRequest() {}
 
-    public ResetFeatureStateRequest(StreamInput in) throws IOException {
+    private ResetFeatureStateRequest(StreamInput in) throws IOException {
         super(in);
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
-        super.writeTo(out);
+        if (out.getVersion().onOrAfter(FEATURE_RESET_ON_MASTER)) {
+            super.writeTo(out);
+        } else {
+            this.toLegacyStateRequest().writeTo(out);
+        }
     }
 
     @Override
     public ActionRequestValidationException validate() {
         return null;
+    }
+
+    private LegacyResetFeatureStateRequest toLegacyStateRequest() {
+        LegacyResetFeatureStateRequest legacyResetFeatureStateRequest = new LegacyResetFeatureStateRequest();
+        legacyResetFeatureStateRequest.setParentTask(this.getParentTask());
+        return legacyResetFeatureStateRequest;
+    }
+
+    private static class LegacyResetFeatureStateRequest extends ActionRequest {
+
+        private LegacyResetFeatureStateRequest() {}
+
+        private LegacyResetFeatureStateRequest(StreamInput in) throws IOException {
+            super(in);
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) throws IOException {
+            super.writeTo(out);
+        }
+
+        @Override
+        public ActionRequestValidationException validate() {
+            return null;
+        }
+
+        ResetFeatureStateRequest toResetFeatureStateRequest() {
+            ResetFeatureStateRequest request = new ResetFeatureStateRequest();
+            request.setParentTask(this.getParentTask());
+            return request;
+        }
     }
 }

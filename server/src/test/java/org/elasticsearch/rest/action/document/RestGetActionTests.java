30b965c05670a0e06da4bc0b1cb51c6fb2b6c53d
pgomulka <przemyslaw.gomulka@elastic.co>
2021-06-23 14:06:41 +0200

    revert changes to testcases

--- server/src/test/java/org/elasticsearch/rest/action/document/RestGetActionTests.java
+++ server/src/test/java/org/elasticsearch/rest/action/document/RestGetActionTests.java
@@ -35,16 +35,19 @@ public class RestGetActionTests extends RestActionTestCase {
         });
     }
 
-    public void testTypeInPath() {
-        testTypeInPath(RestRequest.Method.GET);
-        testTypeInPath(RestRequest.Method.HEAD);
-    }
-
-    private void testTypeInPath(RestRequest.Method head) {
+    public void testTypeInPathWithGet() {
         FakeRestRequest.Builder deprecatedRequest = new FakeRestRequest.Builder(xContentRegistry()).withHeaders(
             Map.of("Content-Type", contentTypeHeader, "Accept", contentTypeHeader)
         ).withPath("/some_index/some_type/some_id");
-        dispatchRequest(deprecatedRequest.withMethod(head).build());
+        dispatchRequest(deprecatedRequest.withMethod(RestRequest.Method.GET).build());
+        assertWarnings(RestGetAction.TYPES_DEPRECATION_MESSAGE);
+    }
+
+    public void testTypeInPathWithHead() {
+        FakeRestRequest.Builder deprecatedRequest = new FakeRestRequest.Builder(xContentRegistry()).withHeaders(
+            Map.of("Content-Type", contentTypeHeader, "Accept", contentTypeHeader)
+        ).withPath("/some_index/some_type/some_id");
+        dispatchRequest(deprecatedRequest.withMethod(RestRequest.Method.HEAD).build());
         assertWarnings(RestGetAction.TYPES_DEPRECATION_MESSAGE);
     }
 }

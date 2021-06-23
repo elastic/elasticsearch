2219182f65ed206e1d1148409a32e1c73a8ded21
Nhat Nguyen <nhat.nguyen@elastic.co>
2021-06-22 22:08:39 -0400

    fix imports

--- server/src/main/java/org/elasticsearch/rest/action/search/RestMultiSearchAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/search/RestMultiSearchAction.java
@@ -16,6 +16,7 @@ import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.common.CheckedBiConsumer;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.core.Tuple;
 import org.elasticsearch.common.logging.DeprecationCategory;
 import org.elasticsearch.common.logging.DeprecationLogger;

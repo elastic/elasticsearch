cfa313a1a77a4bd3bd71a5be40c10260b364681f
iverase <ivera@apache.org>
2021-06-28 19:59:04 +0200

    test that hopefully fails because of backcompat

---
+++ qa/mixed-cluster/src/test/java/org/elasticsearch/backwards/RareTermsIT.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.backwards;
+
+import org.elasticsearch.client.Request;
+import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.test.rest.ESRestTestCase;
+
+public class RareTermsIT extends ESRestTestCase {
+
+    private int indexDocs(int numDocs, int id) throws Exception  {
+        Request request = new Request("POST", "/_bulk");
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < numDocs; ++i) {
+            builder.append("{ \"index\" : { \"_index\" : \"idx\", \"_id\": \"" + (id++) + "\" } }\n");
+            builder.append("{\"str_value\" : \"s" + i + "\"}\n");
+        }
+        request.setJsonEntity(builder.toString());
+        assertOK(client().performRequest(request));
+        return id;
+    }
+
+    public void testSingleValuedString() throws Exception {
+        Settings.Builder settings = Settings.builder()
+            .put(IndexMetadata.INDEX_NUMBER_OF_SHARDS_SETTING.getKey(), 2)
+            .put(IndexMetadata.INDEX_NUMBER_OF_REPLICAS_SETTING.getKey(), 0);
+        final String index = "idx";
+        createIndex(index, settings.build());
+
+        int numDocs = 15000;
+        int id = 1;
+        for (int i = 0; i< 5; i++) {
+            id = indexDocs(numDocs, id);
+            refreshAllIndices();
+        }
+
+        Request request = new Request("POST", "idx/_search");
+        request.setJsonEntity("{\"size\": 0,\"aggs\":{\"rareTerms\":{\"rare_terms\" : {\"field\": \"str_value.keyword\"}}}}");
+        assertOK(client().performRequest(request));
+    }
+}

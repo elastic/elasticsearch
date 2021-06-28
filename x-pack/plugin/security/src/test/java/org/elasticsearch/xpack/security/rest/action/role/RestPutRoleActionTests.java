3cfec42671d7fc287cc6935b45639ca3bab25704
pgomulka <przemyslaw.gomulka@elastic.co>
2021-06-28 10:54:26 +0200

    malformed queries tests

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/rest/action/role/RestPutRoleActionTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/rest/action/role/RestPutRoleActionTests.java
@@ -7,46 +7,28 @@
 
 package org.elasticsearch.xpack.security.rest.action.role;
 
-import junit.framework.TestCase;
-
-import org.elasticsearch.ElasticsearchParseException;
-import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.support.ActionFilters;
-import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.bytes.BytesArray;
-import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.core.RestApiVersion;
-import org.elasticsearch.license.License;
 import org.elasticsearch.license.XPackLicenseState;
 import org.elasticsearch.rest.RestRequest;
-import org.elasticsearch.rest.action.document.RestGetAction;
-import org.elasticsearch.tasks.Task;
+import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.test.rest.FakeRestChannel;
 import org.elasticsearch.test.rest.FakeRestRequest;
 import org.elasticsearch.test.rest.RestActionTestCase;
-import org.elasticsearch.transport.Transport;
-import org.elasticsearch.transport.TransportService;
 import org.elasticsearch.xpack.core.security.action.role.PutRoleRequest;
 import org.elasticsearch.xpack.core.security.action.role.PutRoleResponse;
-import org.elasticsearch.xpack.security.action.role.TransportPutRoleAction;
-import org.elasticsearch.xpack.security.authz.store.NativeRolesStore;
 import org.junit.Before;
 import org.mockito.Mockito;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.nullValue;
-import static org.mockito.Mockito.mock;
 
 public class RestPutRoleActionTests extends RestActionTestCase {
 
@@ -55,7 +37,7 @@ public class RestPutRoleActionTests extends RestActionTestCase {
 
     @Before
     public void setUpAction() {
-        controller().registerHandler(new RestPutRoleAction(Settings.EMPTY, new XPackLicenseState(Settings.EMPTY, ()->0)));
+        controller().registerHandler(new RestPutRoleAction(Settings.EMPTY, new XPackLicenseState(Settings.EMPTY, () -> 0)));
         verifyingClient.setExecuteVerifier((actionType, request) -> {
             assertThat(request, instanceOf(PutRoleRequest.class));
             return Mockito.mock(PutRoleResponse.class);
@@ -65,35 +47,40 @@ public class RestPutRoleActionTests extends RestActionTestCase {
 
     public void testCreationOfRoleWithMalformedQueryJsonFails() {
 
-        PutRoleRequest request = new PutRoleRequest();
-        request.name("test");
-        String[] malformedQueryJson = new String[]{"{ \"match_all\": { \"unknown_field\": \"\" } }",
+        String[] malformedQueries = new String[]{"{ \"match_all\": { \"unknown_field\": \"\" } }",
             "{ malformed JSON }",
             "{ \"unknown\": {\"\"} }",
             "{}"};
-        BytesReference query = new BytesArray(randomFrom(malformedQueryJson));
-        request.addIndex(new String[]{"idx1"}, new String[]{"read"}, null, null, query, randomBoolean());
 
-        FakeRestRequest.Builder deprecatedRequest = new FakeRestRequest.Builder(xContentRegistry())
-            .withHeaders(Map.of("Content-Type", jsonContentType, "Accept", jsonContentType))
-            .withPath("_security/role/rolename")
-            .withMethod(RestRequest.Method.POST)
-            .withContent(query, null);
-        dispatchRequest(deprecatedRequest.build());
+        String queryPattern = "{" +
+            "\"indices\": [\n" +
+            "    {\n" +
+            "      \"names\": \"index\",\n" +
+            "      \"privileges\": [\n" +
+            "        \"all\"\n" +
+            "      ],\n" +
+            "      \"query\": %s" +
+            "      }\n" +
+            "    }\n" +
+            "  ]" +
+            "}";
+        for (String malformedQuery : malformedQueries) {
 
-//        Throwable t = throwableRef.get();
-//        assertThat(t, instanceOf(ElasticsearchParseException.class));
-//        assertThat(t.getMessage(), containsString("failed to parse field 'query' for indices [" +
-//            Strings.arrayToCommaDelimitedString(new String[]{"idx1"}) +
-//            "] at index privilege [0] of role descriptor"));
+            final String query = String.format(Locale.ROOT, queryPattern, malformedQuery);
+            FakeRestRequest.Builder deprecatedRequest = new FakeRestRequest.Builder(xContentRegistry())
+                .withHeaders(Map.of("Content-Type", jsonContentType, "Accept", jsonContentType))
+                .withPath("_security/role/rolename")
+                .withMethod(RestRequest.Method.POST)
+                .withContent(new BytesArray(query), null);
+
+            RestRequest request = deprecatedRequest.build();
+            FakeRestChannel channel = new FakeRestChannel(request, true, 1);
+
+            dispatchRequest(request, channel);
+
+            assertThat(channel.errors().get(), equalTo(1));
+            assertThat(channel.responses().get(), equalTo(0));
+            assertThat(channel.capturedResponse().status(), equalTo(RestStatus.BAD_REQUEST));
+        }
     }
-//    public void testValidation() {
-//
-//        FakeRestRequest.Builder deprecatedRequest = new FakeRestRequest.Builder(xContentRegistry()).withHeaders(
-//            Map.of("Content-Type", contentTypeHeader, "Accept", contentTypeHeader)
-//        ).withPath("/some_index/some_type/some_id");
-//        dispatchRequest(deprecatedRequest.withMethod(method).build());
-//        assertWarnings(RestGetAction.TYPES_DEPRECATION_MESSAGE);
-//    }
-
 }

b3cf960dd2f87c6be7be735a64ead94c1a419585
Yannick Welsch <yannick@welsch.lu>
2021-06-30 10:44:33 +0200

    fixfix

--- server/src/main/java/org/elasticsearch/index/engine/TranslogDirectoryReader.java
+++ server/src/main/java/org/elasticsearch/index/engine/TranslogDirectoryReader.java
@@ -218,7 +218,7 @@ final class TranslogDirectoryReader extends DirectoryReader {
 
         @Override
         public CacheHelper getReaderCacheHelper() {
-            return getDelegate().getReaderCacheHelper();
+            return null;
         }
 
         @Override

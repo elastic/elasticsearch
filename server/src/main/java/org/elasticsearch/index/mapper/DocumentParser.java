af364df462a288b0283d2799dd0479df9657d58b
elasticsearchmachine <58790826+elasticsearchmachine@users.noreply.github.com>
2021-07-05 10:07:44 -0400

    [7.14] Call fixRedundantIncludes on dynamic mapping updates (#74903) (#74918)
    Dynamic mappings updates containing nested mappings were not fixing
    their redundant include settings before being posted back to the put
    mappings service; this meant that a dynamic mapping that had both
    'include_in_parent' and 'include_in_root' set to 'true' could cause an
    error if it was returned from multiple shards, because the first mapping
    would be updated to have 'include_in_root' set to 'false' when merged,
    and merging this with the second mapping with 'include_in_root' set to
    'true' would cause a conflict.
    
    This commit ensures that fixRedundantIncludes is called on dynamic
    mappings before they are posted back to the mapping service.
    
    Fixes #74899

--- server/src/main/java/org/elasticsearch/index/mapper/DocumentParser.java
+++ server/src/main/java/org/elasticsearch/index/mapper/DocumentParser.java
@@ -277,6 +277,7 @@ public final class DocumentParser {
         RootObjectMapper root;
         if (dynamicMappers.isEmpty() == false) {
             root = createDynamicUpdate(mappingLookup, dynamicMappers);
+            root.fixRedundantIncludes();
         } else {
             root = mappingLookup.getMapping().getRoot().copyAndReset();
         }

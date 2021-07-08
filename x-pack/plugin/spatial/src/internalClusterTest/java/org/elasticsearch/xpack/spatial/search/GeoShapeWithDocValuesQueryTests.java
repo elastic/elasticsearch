b54f4b2fe162fb5dbc6dbcb073b00d7bdc6cc3b1
Ignacio Vera <iverase@gmail.com>
2021-07-08 07:19:38 +0200

    Alias field does not work with geo_shape query (#74895)
    Fixes the resolution of the field name in the geo_shape query.

--- x-pack/plugin/spatial/src/internalClusterTest/java/org/elasticsearch/xpack/spatial/search/GeoShapeWithDocValuesQueryTests.java
+++ x-pack/plugin/spatial/src/internalClusterTest/java/org/elasticsearch/xpack/spatial/search/GeoShapeWithDocValuesQueryTests.java
@@ -1,12 +1,19 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
  * 2.0 and the Server Side Public License, v 1; you may not use this file except
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
 
-package org.elasticsearch.search.geo;
+package org.elasticsearch.xpack.spatial.search;
 
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.Strings;
@@ -17,14 +24,24 @@ import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.geo.GeometryTestUtils;
 import org.elasticsearch.geometry.MultiPoint;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.search.geo.GeoShapeQueryTestCase;
+import org.elasticsearch.xpack.spatial.LocalStateSpatialPlugin;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.elasticsearch.action.support.WriteRequest.RefreshPolicy.IMMEDIATE;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.elasticsearch.index.query.QueryBuilders.geoShapeQuery;
 
-public class GeoShapeQueryTests extends GeoShapeQueryTestCase {
+public class GeoShapeWithDocValuesQueryTests extends GeoShapeQueryTestCase {
+
+    @Override
+    protected Collection<Class<? extends Plugin>> getPlugins() {
+        return Collections.singleton(LocalStateSpatialPlugin.class);
+    }
 
     @Override
     protected void createMapping(String indexName, String type, String fieldName, Settings settings) throws Exception {
@@ -49,7 +66,7 @@ public class GeoShapeQueryTests extends GeoShapeQueryTestCase {
             .endObject()
             .endObject());
 
-        client().admin().indices().prepareCreate(defaultIndexName).addMapping(defaultType, mapping, XContentType.JSON).get();
+        client().admin().indices().prepareCreate(defaultIndexName).addMapping("_doc", mapping, XContentType.JSON).get();
         ensureGreen();
 
         MultiPoint multiPoint = GeometryTestUtils.randomMultiPoint(false);

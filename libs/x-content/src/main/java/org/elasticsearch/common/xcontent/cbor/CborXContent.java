62fd7a3769adb2dc45b8e55778d3e67c00a7e646
Armin Braun <me@obrown.io>
2021-07-08 15:19:18 +0200

    Dry Up XContent Parser Construction (#75114) (#75128)
    Cleanup duplication in how we parse byte arrrays directly.

--- libs/x-content/src/main/java/org/elasticsearch/common/xcontent/cbor/CborXContent.java
+++ libs/x-content/src/main/java/org/elasticsearch/common/xcontent/cbor/CborXContent.java
@@ -21,6 +21,7 @@ import org.elasticsearch.common.xcontent.XContentParseException;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.XContentType;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -81,13 +82,14 @@ public class CborXContent implements XContent {
     @Override
     public XContentParser createParser(NamedXContentRegistry xContentRegistry,
             DeprecationHandler deprecationHandler, byte[] data) throws IOException {
-        return new CborXContentParser(xContentRegistry, deprecationHandler, cborFactory.createParser(data));
+        return createParser(xContentRegistry, deprecationHandler, data, 0, data.length);
     }
 
     @Override
     public XContentParser createParser(NamedXContentRegistry xContentRegistry,
             DeprecationHandler deprecationHandler, byte[] data, int offset, int length) throws IOException {
-        return new CborXContentParser(xContentRegistry, deprecationHandler, cborFactory.createParser(data, offset, length));
+        return new CborXContentParser(xContentRegistry, deprecationHandler,
+            cborFactory.createParser(new ByteArrayInputStream(data, offset, length)));
     }
 
     @Override

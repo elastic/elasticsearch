16608a646f6919c79a9c2c2d1c99d762f61b8be2
Luegg <lukas.wegmann@elastic.co>
2021-07-09 08:37:29 +0200

    Revert "introduce marker for eliminated aggregate functions"
    1e308f6cdc31ba1857f44d55a37e1971f305a025

--- x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/optimizer/Optimizer.java
+++ x-pack/plugin/sql/src/main/java/org/elasticsearch/xpack/sql/optimizer/Optimizer.java
@@ -24,7 +24,6 @@ import org.elasticsearch.xpack.ql.expression.UnresolvedAttribute;
 import org.elasticsearch.xpack.ql.expression.function.Function;
 import org.elasticsearch.xpack.ql.expression.function.aggregate.AggregateFunction;
 import org.elasticsearch.xpack.ql.expression.function.aggregate.Count;
-import org.elasticsearch.xpack.ql.expression.function.aggregate.EliminatedAggregateFunction;
 import org.elasticsearch.xpack.ql.expression.function.aggregate.InnerAggregate;
 import org.elasticsearch.xpack.ql.expression.predicate.nulls.IsNotNull;
 import org.elasticsearch.xpack.ql.expression.predicate.nulls.IsNull;
@@ -162,6 +161,7 @@ public class Optimizer extends RuleExecutor<LogicalPlan> {
                 new PruneCast(),
                 // order by alignment of the aggs
                 new SortAggregateOnOrderBy(),
+                // ReplaceAggregationsInLocalRelations, ConstantFolding and PruneFilters must all be applied before this:
                 new PushDownAndCombineFilters()
         );
 
@@ -857,12 +857,10 @@ public class Optimizer extends RuleExecutor<LogicalPlan> {
                     long count = (relation.executable() instanceof EmptyExecutable ? 0L : 1L);
 
                     return plan.transformExpressionsDown(AggregateFunction.class, aggregateFunction -> {
-                        if (aggregateFunction instanceof EliminatedAggregateFunction) {
-                            return aggregateFunction;
-                        } else if (aggregateFunction instanceof Count) {
-                            return new EliminatedAggregateFunction(aggregateFunction.source(), Literal.of(aggregateFunction, count));
+                        if (aggregateFunction instanceof Count) {
+                            return Literal.of(aggregateFunction, count);
                         } else if (count == 0) {
-                            return new EliminatedAggregateFunction(aggregateFunction.source(), Literal.of(aggregateFunction, null));
+                            return Literal.of(aggregateFunction, null);
                         } else {
                             // note, most aggregation functions have already been substituted in ReplaceAggregatesWithLiterals
                             return aggregateFunction;

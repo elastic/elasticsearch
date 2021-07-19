6a6cd1459b2d03297e30d562c386b68577688172
Alan Woodward <romseygeek@gmail.com>
2021-07-19 09:44:48 +0100

    Make NestedObjectMapper its own class (#74410)
    Nested objects are implemented via a Nested class directly on object mappers,
    even though nested and non-nested objects have quite different semantics. In
    addition, most call-sites that need to get an object mapper in fact need a nested
    object mapper. To make it clearer that nested and object mappers are different
    beasts with different implementations and different requirements, we should
    split them into different classes.

--- server/src/main/java/org/elasticsearch/index/query/SearchExecutionContext.java
+++ server/src/main/java/org/elasticsearch/index/query/SearchExecutionContext.java
@@ -42,6 +42,7 @@ import org.elasticsearch.index.mapper.MapperParsingException;
 import org.elasticsearch.index.mapper.MapperService;
 import org.elasticsearch.index.mapper.MappingLookup;
 import org.elasticsearch.index.mapper.MappingParserContext;
+import org.elasticsearch.index.mapper.NestedObjectMapper;
 import org.elasticsearch.index.mapper.ObjectMapper;
 import org.elasticsearch.index.mapper.ParsedDocument;
 import org.elasticsearch.index.mapper.RuntimeField;
@@ -301,7 +302,7 @@ public class SearchExecutionContext extends QueryRewriteContext {
         return mappingLookup.hasMappings();
     }
 
-    public List<ObjectMapper> nestedMappings() {
+    public List<NestedObjectMapper> nestedMappings() {
         return mappingLookup.getNestedMappers();
     }
 

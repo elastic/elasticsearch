cd4446732e53a3d427b36bf765d3be3f62ae8e2d
Rory Hunter <pugnascotia@users.noreply.github.com>
2021-07-21 13:47:50 +0100

    Re-enable compiler warnings in :test:framework (#75449)
    Part of #40366.

--- test/framework/src/main/java/org/elasticsearch/index/replication/ESIndexLevelReplicationTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/index/replication/ESIndexLevelReplicationTestCase.java
@@ -478,6 +478,7 @@ public abstract class ESIndexLevelReplicationTestCase extends IndexShardTestCase
         }
 
         @Override
+        @SuppressWarnings("unchecked")
         public Iterator<IndexShard> iterator() {
             return Iterators.concat(replicas.iterator(), Collections.singleton(primary).iterator());
         }

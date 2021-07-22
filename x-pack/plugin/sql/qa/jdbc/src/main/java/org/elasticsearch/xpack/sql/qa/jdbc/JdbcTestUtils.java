d825310fb05811f3b48016465b9a291d6f0082ad
Bogdan Pintea <bogdan.pintea@elastic.co>
2021-07-22 16:46:15 +0200

    Update target merge version #2
    One more location to update the target merge version in.

--- x-pack/plugin/sql/qa/jdbc/src/main/java/org/elasticsearch/xpack/sql/qa/jdbc/JdbcTestUtils.java
+++ x-pack/plugin/sql/qa/jdbc/src/main/java/org/elasticsearch/xpack/sql/qa/jdbc/JdbcTestUtils.java
@@ -125,8 +125,7 @@ final class JdbcTestUtils {
     }
 
     static boolean versionSupportsArrayTypes() {
-        // TODO: add equality (onOrAfter) only once actually ported to 7.12
-        return JDBC_DRIVER_VERSION.after(Version.V_7_13_0);
-
+        // TODO: add equality (onOrAfter) only once actually ported to 7.15
+        return JDBC_DRIVER_VERSION.after(Version.V_7_15_0);
     }
 }

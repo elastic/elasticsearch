a13f3805fda2c77da401bc77d9f5744149abe5bb
Armin Braun <me@obrown.io>
2021-07-27 18:07:52 +0200

    Enhance Shard Level Metdata check in BlobStoreTestUtil (#75737)
    Adding check that shard level index metadata actually contains the snapshots
    it's supposed to contain. This would have caught a number of recent bugs.

--- server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
+++ server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
@@ -3329,13 +3329,9 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
      * Loads all available snapshots in the repository using the given {@code generation} for a shard. When {@code shardGen}
      * is null it tries to load it using the BwC mode, listing the available index- blobs in the shard container.
      */
-    public BlobStoreIndexShardSnapshots getBlobStoreIndexShardSnapshots(
-        IndexId indexId,
-        ShardId shardId,
-        @Nullable ShardGeneration shardGen
-    ) throws IOException {
-        final int shard = shardId.getId();
-        final BlobContainer shardContainer = shardContainer(indexId, shard);
+    public BlobStoreIndexShardSnapshots getBlobStoreIndexShardSnapshots(IndexId indexId, int shardId, @Nullable ShardGeneration shardGen)
+        throws IOException {
+        final BlobContainer shardContainer = shardContainer(indexId, shardId);
 
         Set<String> blobs = Collections.emptySet();
         if (shardGen == null) {

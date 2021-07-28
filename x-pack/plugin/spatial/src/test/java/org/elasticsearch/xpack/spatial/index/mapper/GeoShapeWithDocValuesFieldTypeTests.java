6e3e3dccd0a6d3dd48d9f26ea303b3acd400d659
iverase <ivera@apache.org>
2021-07-28 10:31:34 +0200

    compile error

--- x-pack/plugin/spatial/src/test/java/org/elasticsearch/xpack/spatial/index/mapper/GeoShapeWithDocValuesFieldTypeTests.java
+++ x-pack/plugin/spatial/src/test/java/org/elasticsearch/xpack/spatial/index/mapper/GeoShapeWithDocValuesFieldTypeTests.java
@@ -29,53 +29,54 @@ public class GeoShapeWithDocValuesFieldTypeTests extends FieldTypeTestCase {
         MappedFieldType mapper
             = new GeoShapeFieldMapper.Builder("field", true, true).build(new ContentPath()).fieldType();
 
-        Map<String, Object> jsonLineString = Map.of("type", "LineString", "coordinates",
-            List.of(List.of(42.0, 27.1), List.of(30.0, 50.0)));
-        Map<String, Object> jsonPoint = Map.of("type", "Point", "coordinates", List.of(14.0, 15.0));
-        Map<String, Object> jsonMalformed = Map.of("type", "Point", "coordinates", "foo");
+        Map<String, Object> jsonLineString = org.elasticsearch.core.Map.of("type", "LineString", "coordinates",
+            org.elasticsearch.core.List.of(org.elasticsearch.core.List.of(42.0, 27.1), org.elasticsearch.core.List.of(30.0, 50.0)));
+        Map<String, Object> jsonPoint = org.elasticsearch.core.Map.of("type", "Point", "coordinates",
+            org.elasticsearch.core.List.of(14.0, 15.0));
+        Map<String, Object> jsonMalformed = org.elasticsearch.core.Map.of("type", "Point", "coordinates", "foo");
         String wktLineString = "LINESTRING (42.0 27.1, 30.0 50.0)";
         String wktPoint = "POINT (14.0 15.0)";
         String wktMalformed = "POINT foo";
 
         // Test a single shape in geojson format.
         Object sourceValue = jsonLineString;
-        assertEquals(List.of(jsonLineString), fetchSourceValue(mapper, sourceValue, null));
-        assertEquals(List.of(wktLineString), fetchSourceValue(mapper, sourceValue, "wkt"));
+        assertEquals(org.elasticsearch.core.List.of(jsonLineString), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(org.elasticsearch.core.List.of(wktLineString), fetchSourceValue(mapper, sourceValue, "wkt"));
 
         // Test a malformed single shape in geojson format
         sourceValue = jsonMalformed;
-        assertEquals(List.of(), fetchSourceValue(mapper, sourceValue, null));
-        assertEquals(List.of(), fetchSourceValue(mapper, sourceValue, "wkt"));
+        assertEquals(org.elasticsearch.core.List.of(), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(org.elasticsearch.core.List.of(), fetchSourceValue(mapper, sourceValue, "wkt"));
 
         // Test a list of shapes in geojson format.
-        sourceValue = List.of(jsonLineString, jsonPoint);
-        assertEquals(List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
-        assertEquals(List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
+        sourceValue = org.elasticsearch.core.List.of(jsonLineString, jsonPoint);
+        assertEquals(org.elasticsearch.core.List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(org.elasticsearch.core.List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
 
         // Test a list of shapes including one malformed in geojson format
-        sourceValue = List.of(jsonLineString, jsonMalformed, jsonPoint);
-        assertEquals(List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
-        assertEquals(List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
+        sourceValue = org.elasticsearch.core.List.of(jsonLineString, jsonMalformed, jsonPoint);
+        assertEquals(org.elasticsearch.core.List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(org.elasticsearch.core.List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
 
         // Test a single shape in wkt format.
         sourceValue = wktLineString;
-        assertEquals(List.of(jsonLineString), fetchSourceValue(mapper, sourceValue, null));
-        assertEquals(List.of(wktLineString), fetchSourceValue(mapper, sourceValue, "wkt"));
+        assertEquals(org.elasticsearch.core.List.of(jsonLineString), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(org.elasticsearch.core.List.of(wktLineString), fetchSourceValue(mapper, sourceValue, "wkt"));
 
         // Test a single malformed shape in wkt format
         sourceValue = wktMalformed;
-        assertEquals(List.of(), fetchSourceValue(mapper, sourceValue, null));
-        assertEquals(List.of(), fetchSourceValue(mapper, sourceValue, "wkt"));
+        assertEquals(org.elasticsearch.core.List.of(), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(org.elasticsearch.core.List.of(), fetchSourceValue(mapper, sourceValue, "wkt"));
 
         // Test a list of shapes in wkt format.
-        sourceValue = List.of(wktLineString, wktPoint);
-        assertEquals(List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
-        assertEquals(List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
+        sourceValue = org.elasticsearch.core.List.of(wktLineString, wktPoint);
+        assertEquals(org.elasticsearch.core.List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(org.elasticsearch.core.List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
 
         // Test a list of shapes including one malformed in wkt format
-        sourceValue = List.of(wktLineString, wktMalformed, wktPoint);
-        assertEquals(List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
-        assertEquals(List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
+        sourceValue = org.elasticsearch.core.List.of(wktLineString, wktMalformed, wktPoint);
+        assertEquals(org.elasticsearch.core.List.of(jsonLineString, jsonPoint), fetchSourceValue(mapper, sourceValue, null));
+        assertEquals(org.elasticsearch.core.List.of(wktLineString, wktPoint), fetchSourceValue(mapper, sourceValue, "wkt"));
     }
 
     public void testFetchVectorTile() throws IOException {

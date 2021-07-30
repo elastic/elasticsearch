ce48fc50a3748ff4b1886a063b15fd6a36c563ff
danielhuang(黄华) <danielhuang@tencent.com>
2021-07-30 09:36:29 +0800

    fix unused import issue

--- server/src/main/java/org/elasticsearch/index/shard/ShardPath.java
+++ server/src/main/java/org/elasticsearch/index/shard/ShardPath.java
@@ -19,7 +19,6 @@ import org.elasticsearch.index.IndexSettings;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
 

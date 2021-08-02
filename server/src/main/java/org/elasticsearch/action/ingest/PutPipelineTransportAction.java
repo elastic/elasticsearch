01cbd6925d9f3b3ee9eb165418190a0cb463fd7d
Keith Massey <keith.massey@elastic.co>
2021-08-02 15:45:52 -0500

    Preventing pipelines with _meta config from being created on clusters with older nodes

--- server/src/main/java/org/elasticsearch/action/ingest/PutPipelineTransportAction.java
+++ server/src/main/java/org/elasticsearch/action/ingest/PutPipelineTransportAction.java
@@ -8,6 +8,9 @@
 
 package org.elasticsearch.action.ingest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequest;
@@ -22,8 +25,10 @@ import org.elasticsearch.cluster.block.ClusterBlockLevel;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.xcontent.XContentHelper;
 import org.elasticsearch.ingest.IngestInfo;
 import org.elasticsearch.ingest.IngestService;
+import org.elasticsearch.ingest.Pipeline;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
@@ -37,6 +42,7 @@ public class PutPipelineTransportAction extends AcknowledgedTransportMasterNodeA
 
     private final IngestService ingestService;
     private final OriginSettingClient client;
+    private static final Logger logger = LogManager.getLogger(PutPipelineTransportAction.class);
 
     @Inject
     public PutPipelineTransportAction(ThreadPool threadPool, TransportService transportService,
@@ -55,6 +61,21 @@ public class PutPipelineTransportAction extends AcknowledgedTransportMasterNodeA
     @Override
     protected void masterOperation(Task task, PutPipelineRequest request, ClusterState state, ActionListener<AcknowledgedResponse> listener)
             throws Exception {
+        if (state.getNodes().getMinNodeVersion().before(Version.V_8_0_0)) {
+            Map<String, Object> pipelineConfig = XContentHelper.convertToMap(request.getSource(), false, request.getXContentType()).v2();
+            if (pipelineConfig.containsKey(Pipeline.META_KEY)) {
+                throw new IllegalArgumentException("All nodes in the cluster must be at " + Version.V_8_0_0 + " or higher in order to" +
+                    "create a pipeline with the " + Pipeline.META_KEY + " field. The minimum version currently in the cluster is " +
+                    state.getNodes().getMinNodeVersion());
+            }
+        } else {
+            Map<String, Object> pipelineConfig = XContentHelper.convertToMap(request.getSource(), false, request.getXContentType()).v2();
+            if (pipelineConfig.containsKey(Pipeline.META_KEY)) {
+                logger.info("Found _meta entry");
+            } else {
+                logger.info("Did not find _meta entry");
+            }
+        }
         NodesInfoRequest nodesInfoRequest = new NodesInfoRequest();
         nodesInfoRequest.clear()
             .addMetric(NodesInfoRequest.Metric.INGEST.metricName());

0b21842b9cc6650861f05c783be357af4e895089
Yang Wang <yang.wang@elastic.co>
2021-08-03 15:21:31 +1000

    A new search API for API keys - core search function (#75335)
    This PR adds a new API for searching API keys. The API supports searching API
    keys with a controlled list of field names and a subset of Query DSL. It also
    provides a translation layer between the field names used in the REST layer and
    those in the index layer. This is to prevent tight coupling between the user
    facing request and index mappings so that they can evolve separately.
    
    Compared to the Get API key API, this new search API automatically applies
    calling user's security context similar to regular searches, e.g. if the user
    has only manage_own_api_key privilege, only keys owned by the user are returned
    in the search response.
    
    Relates: #71023

--- x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
+++ x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
@@ -174,6 +174,7 @@ public class Constants {
         "cluster:admin/xpack/security/api_key/get",
         "cluster:admin/xpack/security/api_key/grant",
         "cluster:admin/xpack/security/api_key/invalidate",
+        "cluster:admin/xpack/security/api_key/query",
         "cluster:admin/xpack/security/cache/clear",
         "cluster:admin/xpack/security/delegate_pki",
         "cluster:admin/xpack/security/enroll/node",

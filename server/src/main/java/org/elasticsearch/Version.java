cc8d12d7b73e6393a6b500afef1213ccda6bd1de
Martijn van Groningen <martijn.v.groningen@gmail.com>
2021-08-03 10:29:00 +0200

    Bump version to 6.8.19

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -162,8 +162,9 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_6_8_16 = new Version(6081699, org.apache.lucene.util.Version.LUCENE_7_7_3);
     public static final Version V_6_8_17 = new Version(6081799, org.apache.lucene.util.Version.LUCENE_7_7_3);
     public static final Version V_6_8_18 = new Version(6081899, org.apache.lucene.util.Version.LUCENE_7_7_3);
+    public static final Version V_6_8_19 = new Version(6081999, org.apache.lucene.util.Version.LUCENE_7_7_3);
 
-    public static final Version CURRENT = V_6_8_18;
+    public static final Version CURRENT = V_6_8_19;
 
     private static final ImmutableOpenIntMap<Version> idToVersion;
 

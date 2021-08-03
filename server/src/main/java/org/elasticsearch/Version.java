e2a7866030e7806885e7c569ec94bcb2194ecdff
Przemko Robakowski <przemko.robakowski@elastic.co>
2021-08-03 10:42:13 +0200

    bump version to 7.14.1

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -152,9 +152,9 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_7_13_2 = new Version(7130299, org.apache.lucene.util.Version.LUCENE_8_8_2);
     public static final Version V_7_13_3 = new Version(7130399, org.apache.lucene.util.Version.LUCENE_8_8_2);
     public static final Version V_7_13_4 = new Version(7130499, org.apache.lucene.util.Version.LUCENE_8_8_2);
-    public static final Version V_7_13_5 = new Version(7130599, org.apache.lucene.util.Version.LUCENE_8_8_2);
     public static final Version V_7_14_0 = new Version(7140099, org.apache.lucene.util.Version.LUCENE_8_9_0);
-    public static final Version CURRENT = V_7_14_0;
+    public static final Version V_7_14_1 = new Version(7140099, org.apache.lucene.util.Version.LUCENE_8_9_0);
+    public static final Version CURRENT = V_7_14_1;
 
     private static final ImmutableOpenIntMap<Version> idToVersion;
     private static final ImmutableOpenMap<String, Version> stringToVersion;

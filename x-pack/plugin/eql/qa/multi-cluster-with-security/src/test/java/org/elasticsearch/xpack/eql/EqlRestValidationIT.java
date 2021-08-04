889d8e67d38fb52ebb127e67d64f69793d030598
Bogdan Pintea <bogdan.pintea@elastic.co>
2021-08-04 14:59:25 +0200

    Adress doc review comments. Update error messages
    Adress doc review comments. Update error messages

--- x-pack/plugin/eql/qa/multi-cluster-with-security/src/test/java/org/elasticsearch/xpack/eql/EqlRestValidationIT.java
+++ x-pack/plugin/eql/qa/multi-cluster-with-security/src/test/java/org/elasticsearch/xpack/eql/EqlRestValidationIT.java
@@ -21,17 +21,15 @@ public class EqlRestValidationIT extends EqlRestValidationTestCase {
     }
 
     protected void assertErrorMessageWhenAllowNoIndicesIsFalse(String reqParameter) throws IOException {
-        assertErrorMessage("inexistent1*", reqParameter, "\"root_cause\":[{\"type\":\"index_not_found_exception\","
-            + "\"reason\":\"no such index [inexistent1*]\"");
-        assertErrorMessage("inexistent1*,inexistent2*", reqParameter, "\"root_cause\":[{\"type\":\"index_not_found_exception\","
-            + "\"reason\":\"no such index [inexistent1*]\"");
-        assertErrorMessage("test_eql,inexistent*", reqParameter, "\"root_cause\":[{\"type\":\"index_not_found_exception\","
-            + "\"reason\":\"no such index [inexistent*]\"");
+        assertErrorMessage("inexistent1*", reqParameter, getInexistentIndexErrorMessage() + "[" + indexPattern("inexistent1*") + "]\"");
+        assertErrorMessage("inexistent1*,inexistent2*", reqParameter, getInexistentIndexErrorMessage() +
+            "[" + indexPattern("inexistent1*") + "]\"");
+        assertErrorMessage("test_eql,inexistent*", reqParameter, getInexistentIndexErrorMessage() +
+            "[" + indexPattern("inexistent*") + "]\"");
         //TODO: revisit the next two tests when https://github.com/elastic/elasticsearch/issues/64190 is closed
-        assertErrorMessage("inexistent", reqParameter, "\"root_cause\":[{\"type\":\"index_not_found_exception\","
-            + "\"reason\":\"no such index [[inexistent]]\"");
-        assertErrorMessage("inexistent1,inexistent2", reqParameter, "\"root_cause\":[{\"type\":\"index_not_found_exception\","
-            + "\"reason\":\"no such index [[inexistent1, inexistent2]]\"");
+        assertErrorMessage("inexistent", reqParameter, getInexistentIndexErrorMessage() + "[" + indexPattern("[inexistent]") + "]\"");
+        assertErrorMessage("inexistent1,inexistent2", reqParameter, getInexistentIndexErrorMessage() +
+            "[[" + indexPattern("inexistent1") + "," + indexPattern("inexistent2") + "]]\"");
     }
 
     @Override

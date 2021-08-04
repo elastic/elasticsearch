544f8b867ff7773b45deaf87e2a995a8b166d973
Ignacio Vera <iverase@gmail.com>
2021-08-04 07:58:28 +0200

    Speed up creation of vector tiles features (#75874)
    *  Simplify the shape when defined in spherical mercator projection instead of when defined in pixel space.

--- x-pack/plugin/vector-tile/src/test/java/org/elasticsearch/xpack/vectortile/feature/FeatureFactoryTests.java
+++ x-pack/plugin/vector-tile/src/test/java/org/elasticsearch/xpack/vectortile/feature/FeatureFactoryTests.java
@@ -20,16 +20,24 @@ import org.elasticsearch.geometry.MultiPolygon;
 import org.elasticsearch.geometry.Point;
 import org.elasticsearch.geometry.Polygon;
 import org.elasticsearch.geometry.Rectangle;
+import org.elasticsearch.geometry.utils.StandardValidator;
+import org.elasticsearch.geometry.utils.WellKnownText;
 import org.elasticsearch.search.aggregations.bucket.geogrid.GeoTileUtils;
 import org.elasticsearch.test.ESTestCase;
 import org.hamcrest.Matchers;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.zip.GZIPInputStream;
 
 public class FeatureFactoryTests extends ESTestCase {
 
@@ -150,5 +158,46 @@ public class FeatureFactoryTests extends ESTestCase {
 
     private GeometryCollection<Geometry> buildGeometryCollection(Rectangle r) {
         return new GeometryCollection<>(org.elasticsearch.core.List.of(buildPolygon(r), buildLine(r)));
+    }
+
+    public void testStackOverflowError() throws IOException, ParseException {
+        // The provided polygon contains 49K points and we have observed that for some tiles and some extent values,
+        // it makes the library we are using to compute features to fail with a StackOverFlowError. This test just makes
+        // sure the fix in place avoids that error.
+        final InputStream is = new GZIPInputStream(getClass().getResourceAsStream("polygon.wkt.gz"));
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+        final Geometry geometry = WellKnownText.fromWKT(StandardValidator.instance(true), true, reader.readLine());
+        for (int i = 0; i < 10; i++) {
+            final int z = randomIntBetween(0, 4);
+            final int x = randomIntBetween(0, (1 << z) - 1);
+            final int y = randomIntBetween(0, (1 << z) - 1);
+            final int extent = randomIntBetween(128, 8012);
+            final FeatureFactory builder = new FeatureFactory(z, x, y, extent);
+            try {
+                builder.getFeatures(geometry);
+            } catch (StackOverflowError error) {
+                fail("stackoverflow error thrown at " + z + "/" + x + "/" + y + "@" + extent);
+            }
+        }
+    }
+
+    public void testTileInsidePolygon() throws Exception {
+        final int z = randomIntBetween(0, 4);
+        final int x = randomIntBetween(0, (1 << z) - 1);
+        final int y = randomIntBetween(0, (1 << z) - 1);
+        final int extent = randomIntBetween(128, 8012);
+        final FeatureFactory builder = new FeatureFactory(z, x, y, extent);
+        final Rectangle rectangle = GeoTileUtils.toBoundingBox(x, y, z);
+        final double minX = Math.max(-180, rectangle.getMinX() - 1);
+        final double maxX = Math.min(180, rectangle.getMaxX() + 1);
+        final double minY = Math.max(-GeoTileUtils.LATITUDE_MASK, rectangle.getMinY() - 1);
+        final double maxY = Math.min(GeoTileUtils.LATITUDE_MASK, rectangle.getMaxY() + 1);
+        Polygon bigPolygon = new Polygon(
+            new LinearRing(new double[] { minX, maxX, maxX, minX, minX }, new double[] { minY, minY, maxY, maxY, minY })
+        );
+        final List<byte[]> bytes = builder.getFeatures(bigPolygon);
+        assertThat(bytes, Matchers.iterableWithSize(1));
+        final VectorTile.Tile.Feature feature = VectorTile.Tile.Feature.parseFrom(bytes.get(0));
+        assertThat(feature.getType(), Matchers.equalTo(VectorTile.Tile.GeomType.POLYGON));
     }
 }

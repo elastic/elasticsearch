4abdb59221c407685ccf260deaaf9ef1b8005999
elasticsearchmachine <58790826+elasticsearchmachine@users.noreply.github.com>
2021-08-05 10:44:17 -0400

    Fix null value usage for index.number_of_routing_shards setting. (#76174)
    Backport #76069 to 7.14 branch.
    
    Ensure that the same number of routing shards is used for new indices
    when `index.number_of_routing_shards` is not specified and `null` as value is specified.
    
    Without this change if no index.number_of_routing_shards has been specified then
    logic inside the MetadataCreateIndexService#getIndexNumberOfRoutingShards(...) kicks and
    calculates a default based on number of primary shards use that as the true number of routing shards.
    If a value is specified (including null) then the value the true number of routing shards is
    determined from the IndexMetadata.INDEX_NUMBER_OF_ROUTING_SHARDS_SETTING setting and
    in the case of when null is specified then the default value is gathered from
    the IndexMetadata.INDEX_NUMBER_OF_SHARDS_SETTING setting.
    
    Closes #75139
    
    Co-authored-by: Martijn van Groningen <martijn.v.groningen@gmail.com>

--- server/src/test/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexServiceTests.java
+++ server/src/test/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexServiceTests.java
@@ -935,6 +935,29 @@ public class MetadataCreateIndexServiceTests extends ESTestCase {
         assertThat(targetRoutingNumberOfShards, is(9));
     }
 
+    public void testGetIndexNumberOfRoutingShardsNullVsNotDefined() {
+        int numberOfPrimaryShards = randomIntBetween(1, 16);
+        Settings indexSettings = settings(Version.CURRENT)
+            .put(INDEX_NUMBER_OF_ROUTING_SHARDS_SETTING.getKey(), (String) null)
+            .put(INDEX_NUMBER_OF_SHARDS_SETTING.getKey(), numberOfPrimaryShards)
+            .build();
+        int targetRoutingNumberOfShardsWithNull = getIndexNumberOfRoutingShards(indexSettings, null);
+        indexSettings = settings(Version.CURRENT)
+            .put(INDEX_NUMBER_OF_SHARDS_SETTING.getKey(), numberOfPrimaryShards)
+            .build();
+        int targetRoutingNumberOfShardsWithNotDefined = getIndexNumberOfRoutingShards(indexSettings, null);
+        assertThat(targetRoutingNumberOfShardsWithNull, is(targetRoutingNumberOfShardsWithNotDefined));
+    }
+
+    public void testGetIndexNumberOfRoutingShardsNull() {
+        Settings indexSettings = settings(Version.CURRENT)
+            .put(INDEX_NUMBER_OF_ROUTING_SHARDS_SETTING.getKey(), (String) null)
+            .put(INDEX_NUMBER_OF_SHARDS_SETTING.getKey(), 2)
+            .build();
+        int targetRoutingNumberOfShardsWithNull = getIndexNumberOfRoutingShards(indexSettings, null);
+        assertThat(targetRoutingNumberOfShardsWithNull, is(1024));
+    }
+
     public void testGetIndexNumberOfRoutingShardsYieldsSourceNumberOfShards() {
         Settings indexSettings = Settings.builder()
             .put(INDEX_NUMBER_OF_SHARDS_SETTING.getKey(), 3)

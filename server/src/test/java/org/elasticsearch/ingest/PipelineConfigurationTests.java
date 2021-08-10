d055640b11df3930805b30e4f80c0500239dfb17
Keith Massey <keith.massey@elastic.co>
2021-08-10 17:23:55 -0500

    removing unused imports

--- server/src/test/java/org/elasticsearch/ingest/PipelineConfigurationTests.java
+++ server/src/test/java/org/elasticsearch/ingest/PipelineConfigurationTests.java
@@ -8,7 +8,6 @@
 
 package org.elasticsearch.ingest;
 
-import org.elasticsearch.Version;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;

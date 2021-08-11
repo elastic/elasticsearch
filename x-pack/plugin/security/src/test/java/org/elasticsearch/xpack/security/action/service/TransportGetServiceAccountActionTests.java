2e9c380316453985cf395cfe2d69fee411e6583c
Ioannis Kakavas <ioannis@elastic.co>
2021-08-11 22:53:11 +0300

    fix more tests

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/action/service/TransportGetServiceAccountActionTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/action/service/TransportGetServiceAccountActionTests.java
@@ -14,11 +14,15 @@ import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.transport.TransportService;
 import org.elasticsearch.xpack.core.security.action.service.GetServiceAccountRequest;
 import org.elasticsearch.xpack.core.security.action.service.GetServiceAccountResponse;
+import org.elasticsearch.xpack.core.security.action.service.ServiceAccountInfo;
 import org.elasticsearch.xpack.security.authc.support.HttpTlsRuntimeCheck;
 import org.junit.Before;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doAnswer;
@@ -43,23 +47,33 @@ public class TransportGetServiceAccountActionTests extends ESTestCase {
     }
 
     public void testDoExecute() {
-        final GetServiceAccountRequest request1 = randomFrom(
-            new GetServiceAccountRequest(null, null),
-            new GetServiceAccountRequest("elastic", null),
-            new GetServiceAccountRequest("elastic", "fleet-server"));
+        final GetServiceAccountRequest request1 = randomFrom(new GetServiceAccountRequest(null, null),
+        new GetServiceAccountRequest("elastic", null));
         final PlainActionFuture<GetServiceAccountResponse> future1 = new PlainActionFuture<>();
         transportGetServiceAccountAction.doExecute(mock(Task.class), request1, future1);
         final GetServiceAccountResponse getServiceAccountResponse1 = future1.actionGet();
-        assertThat(getServiceAccountResponse1.getServiceAccountInfos().length, equalTo(1));
-        assertThat(getServiceAccountResponse1.getServiceAccountInfos()[0].getPrincipal(), equalTo("elastic/fleet-server"));
+        assertThat(getServiceAccountResponse1.getServiceAccountInfos().length, equalTo(2));
+        assertThat(
+            Arrays.stream(getServiceAccountResponse1.getServiceAccountInfos())
+                .map(ServiceAccountInfo::getPrincipal)
+                .collect(Collectors.toList()),
+            containsInAnyOrder("elastic/fleet-server", "elastic/kibana-system")
+        );
 
-        final GetServiceAccountRequest request2 = randomFrom(
-            new GetServiceAccountRequest("foo", null),
-            new GetServiceAccountRequest("elastic", "foo"),
-            new GetServiceAccountRequest("foo", "bar"));
+        final GetServiceAccountRequest request2 = new GetServiceAccountRequest("elastic", "fleet-server");
         final PlainActionFuture<GetServiceAccountResponse> future2 = new PlainActionFuture<>();
         transportGetServiceAccountAction.doExecute(mock(Task.class), request2, future2);
         final GetServiceAccountResponse getServiceAccountResponse2 = future2.actionGet();
-        assertThat(getServiceAccountResponse2.getServiceAccountInfos().length, equalTo(0));
+        assertThat(getServiceAccountResponse2.getServiceAccountInfos().length, equalTo(1));
+        assertThat(getServiceAccountResponse2.getServiceAccountInfos()[0].getPrincipal(), equalTo("elastic/fleet-server"));
+
+        final GetServiceAccountRequest request3 = randomFrom(
+            new GetServiceAccountRequest("foo", null),
+            new GetServiceAccountRequest("elastic", "foo"),
+            new GetServiceAccountRequest("foo", "bar"));
+        final PlainActionFuture<GetServiceAccountResponse> future3 = new PlainActionFuture<>();
+        transportGetServiceAccountAction.doExecute(mock(Task.class), request3, future3);
+        final GetServiceAccountResponse getServiceAccountResponse3 = future3.actionGet();
+        assertThat(getServiceAccountResponse3.getServiceAccountInfos().length, equalTo(0));
     }
 }

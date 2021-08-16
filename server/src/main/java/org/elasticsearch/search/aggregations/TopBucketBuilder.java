181d01456cf03104c96e98c67438ac09aadac941
Nik Everett <nik9000@gmail.com>
2021-08-16 14:28:01 -0400

    Gracefully nandle very large sizes on terms
    Folks often as for the `terms` agg to have *very* large `size`
    parameters in an effort to get everything they can. But they never fill
    that large size. They can't! There isn't really enough heap to return
    two billion buckets.
    
    After #74096 we try to pre-allocate an array of `size + 1` length,
    regardless of how many results are returned. When folks ask for
    `MAX_INT` buckets this helpfully fails off in lucene land with an error
    message that fairly esoterit to someone reading it outside of
    Elasticsearch.
    
    This change handles that `MAX_INT` case by building a
    `TopBucketsBuilder` designed to handle large max sizes that are rarely
    filled. When you ask for 1024 or more buckets you get that one instead
    and we don't preallocate the entire array for the reduction. You get the
    old preallocated one when you have few buckets.
    
    Closes #76492

--- server/src/main/java/org/elasticsearch/search/aggregations/TopBucketBuilder.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/TopBucketBuilder.java
@@ -8,6 +8,7 @@
 
 package org.elasticsearch.search.aggregations;
 
+import org.apache.lucene.util.ArrayUtil;
 import org.apache.lucene.util.PriorityQueue;
 import org.elasticsearch.search.aggregations.bucket.MultiBucketsAggregation.Bucket;
 
@@ -20,26 +21,47 @@ import java.util.function.Consumer;
 /**
  * Merges many buckets into the "top" buckets as sorted by {@link BucketOrder}.
  */
-public class TopBucketBuilder<B extends InternalMultiBucketAggregation.InternalBucket> {
-    private final Consumer<DelayedBucket<B>> nonCompetitive;
-    private final PriorityQueue<DelayedBucket<B>> queue;
+public abstract class TopBucketBuilder<B extends InternalMultiBucketAggregation.InternalBucket> {
+    /**
+     * The number of buckets required before we switch to the
+     * {@link BufferingTopBucketBuilder}. If we need fewer buckets we use
+     * {@link PriorityQueueTopBucketBuilder}.
+     */
+    static final int USE_BUFFERING_BUILDER = 1024;
 
     /**
      * Create a {@link TopBucketBuilder} to build a list of the top buckets.
+     * <p>
+     * If there are few required results we use a {@link PriorityQueueTopBucketBuilder}
+     * which is simpler and when the priority queue is full but allocates {@code size + 1}
+     * slots in an array. If there are many required results we prefer a
+     * {@link BufferingTopBucketBuilder} which doesn't preallocate and is faster for the
+     * first {@code size} results. But it's a little slower when the priority queue is full.
+     * <p>
+     * It's important for this <strong>not</strong> to preallocate a bunch of memory when
+     * {@code size} is very very large because this backs the reduction of the {@code terms}
+     * aggregation and folks often set the {@code size} of that to something quite large.
+     * The choice in the paragraph above handles this case.
+     *
      * @param size the requested size of the list
      * @param order the sort order of the buckets
      * @param nonCompetitive called with non-competitive buckets
      */
-    public TopBucketBuilder(int size, BucketOrder order, Consumer<DelayedBucket<B>> nonCompetitive) {
-        this.nonCompetitive = nonCompetitive;
-        queue = new PriorityQueue<DelayedBucket<B>>(size) {
-            private final Comparator<DelayedBucket<? extends Bucket>> comparator = order.delayedBucketComparator();
+    public static <B extends InternalMultiBucketAggregation.InternalBucket> TopBucketBuilder<B> build(
+        int size,
+        BucketOrder order,
+        Consumer<DelayedBucket<B>> nonCompetitive
+    ) {
+        if (size < USE_BUFFERING_BUILDER) {
+            return new PriorityQueueTopBucketBuilder<>(size, order, nonCompetitive);
+        }
+        return new BufferingTopBucketBuilder<>(size, order, nonCompetitive);
+    }
 
-            @Override
-            protected boolean lessThan(DelayedBucket<B> a, DelayedBucket<B> b) {
-                return comparator.compare(a, b) > 0;
-            }
-        };
+    protected final Consumer<DelayedBucket<B>> nonCompetitive;
+
+    private TopBucketBuilder(Consumer<DelayedBucket<B>> nonCompetitive) {
+        this.nonCompetitive = nonCompetitive;
     }
 
     /**
@@ -53,23 +75,105 @@ public class TopBucketBuilder<B extends InternalMultiBucketAggregation.InternalB
      * directly on the {@linkplain DelayedBucket}s so we only need to
      * merge <strong>exactly</strong> the sub-buckets we need.
      */
-    public void add(DelayedBucket<B> bucket) {
-        DelayedBucket<B> removed = queue.insertWithOverflow(bucket);
-        if (removed != null) {
-            nonCompetitive.accept(removed);
-            removed.nonCompetitive();
-        }
-    }
+    public abstract void add(DelayedBucket<B> bucket);
 
     /**
      * Return the most competitive buckets sorted by the comparator.
      */
-    public List<B> build() {
-        List<B> result = new ArrayList<>(queue.size());
-        for (int i = queue.size() - 1; i >= 0; i--) {
-            result.add(queue.pop().reduced());
+    public abstract List<B> build();
+
+    /**
+     * Collects the "top" buckets by adding them directly to a {@link PriorityQueue}.
+     * This is always going to be faster than {@link BufferingTopBucketBuilder}
+     * but it requires allocating an array of {@code size + 1}.
+     */
+    static class PriorityQueueTopBucketBuilder<B extends InternalMultiBucketAggregation.InternalBucket> extends TopBucketBuilder<B> {
+        private final PriorityQueue<DelayedBucket<B>> queue;
+
+        PriorityQueueTopBucketBuilder(int size, BucketOrder order, Consumer<DelayedBucket<B>> nonCompetitive) {
+            super(nonCompetitive);
+            if (size >= ArrayUtil.MAX_ARRAY_LENGTH) {
+                throw new IllegalArgumentException("can't reduce more than [" + ArrayUtil.MAX_ARRAY_LENGTH + "] buckets");
+            }
+            queue = new PriorityQueue<DelayedBucket<B>>(size) {
+                private final Comparator<DelayedBucket<? extends Bucket>> comparator = order.delayedBucketComparator();
+
+                @Override
+                protected boolean lessThan(DelayedBucket<B> a, DelayedBucket<B> b) {
+                    return comparator.compare(a, b) > 0;
+                }
+            };
         }
-        Collections.reverse(result);
-        return result;
+
+        @Override
+        public void add(DelayedBucket<B> bucket) {
+            DelayedBucket<B> removed = queue.insertWithOverflow(bucket);
+            if (removed != null) {
+                nonCompetitive.accept(removed);
+                removed.nonCompetitive();
+            }
+        }
+
+        @Override
+        public List<B> build() {
+            List<B> result = new ArrayList<>(queue.size());
+            for (int i = queue.size() - 1; i >= 0; i--) {
+                result.add(queue.pop().reduced());
+            }
+            Collections.reverse(result);
+            return result;
+        }
+    }
+
+    /**
+     * Collects the "top" buckets by adding them to a {@link List} that grows
+     * as more buckets arrive and is converting into a
+     * {@link PriorityQueueTopBucketBuilder} when {@code size} buckets arrive.
+     */
+    private static class BufferingTopBucketBuilder<B extends InternalMultiBucketAggregation.InternalBucket> extends TopBucketBuilder<B> {
+        private final int size;
+        private final BucketOrder order;
+
+        private List<DelayedBucket<B>> buffer;
+        private PriorityQueueTopBucketBuilder<B> next;
+
+        BufferingTopBucketBuilder(int size, BucketOrder order, Consumer<DelayedBucket<B>> nonCompetitive) {
+            super(nonCompetitive);
+            this.size = size;
+            this.order = order;
+            buffer = new ArrayList<>();
+        }
+
+        @Override
+        public void add(DelayedBucket<B> bucket) {
+            if (next != null) {
+                assert buffer == null;
+                next.add(bucket);
+                return;
+            }
+            buffer.add(bucket);
+            if (buffer.size() < size) {
+                return;
+            }
+            next = new PriorityQueueTopBucketBuilder<>(size, order, nonCompetitive);
+            for (DelayedBucket<B> b : buffer) {
+                next.queue.add(b);
+            }
+            buffer = null;
+        }
+
+        @Override
+        public List<B> build() {
+            if (next != null) {
+                assert buffer == null;
+                return next.build();
+            }
+            List<B> result = new ArrayList<>(buffer.size());
+            for (DelayedBucket<B> b : buffer) {
+                result.add(b.reduced());
+            }
+            result.sort(order.comparator());
+            return result;
+        }
     }
 }

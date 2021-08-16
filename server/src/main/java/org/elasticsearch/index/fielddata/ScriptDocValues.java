69ec48d48f1c8a5a6270d27957863f9ff91952b4
Stuart Tettemer <stu@elastic.co>
2021-08-16 18:26:25 -0500

    remove unused TimeUnit

--- server/src/main/java/org/elasticsearch/index/fielddata/ScriptDocValues.java
+++ server/src/main/java/org/elasticsearch/index/fielddata/ScriptDocValues.java
@@ -30,7 +30,6 @@ import java.util.AbstractList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.function.UnaryOperator;
 
 /**

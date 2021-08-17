37e3d46e1f383491ceef9a5f7fff90db7ef75479
Ioannis Kakavas <ioannis@elastic.co>
2021-08-17 12:54:54 +0300

    address feeback

--- x-pack/qa/security-tools-tests/src/test/java/org/elasticsearch/xpack/security/enrollment/tool/AutoConfigGenerateElasticPasswordHashTests.java
+++ x-pack/qa/security-tools-tests/src/test/java/org/elasticsearch/xpack/security/enrollment/tool/AutoConfigGenerateElasticPasswordHashTests.java
@@ -20,27 +20,25 @@ import org.elasticsearch.core.internal.io.IOUtils;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.xpack.core.XPackSettings;
 import org.elasticsearch.xpack.core.security.authc.support.Hasher;
-import org.elasticsearch.xpack.security.tool.GenerateElasticPasswordHash;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Map;
 
 import static org.elasticsearch.test.SecurityIntegTestCase.getFastStoredHashAlgoForTests;
+import static org.elasticsearch.xpack.security.authc.esnative.ReservedRealm.AUTOCONFIG_BOOOTSTRAP_ELASTIC_PASSWORD_HASH;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.hasLength;
 import static org.hamcrest.Matchers.is;
 
-public class GenerateElasticPasswordHashTests extends CommandTestCase {
+public class AutoConfigGenerateElasticPasswordHashTests extends CommandTestCase {
 
     private static FileSystem jimfs;
     private static Hasher hasher;
@@ -61,16 +59,12 @@ public class GenerateElasticPasswordHashTests extends CommandTestCase {
         IOUtils.rm(homeDir);
         confDir = homeDir.resolve("config");
         Files.createDirectories(confDir);
-        Files.write(confDir.resolve("users"), List.of(), StandardCharsets.UTF_8);
-        Files.write(confDir.resolve("users_roles"), List.of(), StandardCharsets.UTF_8);
         hasher = getFastStoredHashAlgoForTests();
         settings = Settings.builder()
             .put("path.home", homeDir)
             .put(XPackSettings.PASSWORD_HASHING_ALGORITHM.getKey(), hasher.name())
             .build();
-        env = new Environment(GenerateElasticPasswordHashTests.this.settings, confDir);
-        KeyStoreWrapper keystore = KeyStoreWrapper.create();
-        keystore.save(confDir, new char[0]);
+        env = new Environment(AutoConfigGenerateElasticPasswordHashTests.this.settings, confDir);
     }
 
     @AfterClass
@@ -82,7 +76,7 @@ public class GenerateElasticPasswordHashTests extends CommandTestCase {
     }
 
     @Override protected Command newCommand() {
-        return new GenerateElasticPasswordHash() {
+        return new AutoConfigGenerateElasticPasswordHash() {
             @Override
             protected Environment createEnv(Map<String, String> settings) throws UserException {
                 return env;
@@ -94,18 +88,17 @@ public class GenerateElasticPasswordHashTests extends CommandTestCase {
         execute();
         assertThat(terminal.getOutput(), hasLength(20));
         KeyStoreWrapper keyStoreWrapper = KeyStoreWrapper.load(env.configFile());
+        assertNotNull(keyStoreWrapper);
         keyStoreWrapper.decrypt(new char[0]);
         assertThat(keyStoreWrapper.getSettingNames(),
-            containsInAnyOrder(GenerateElasticPasswordHash.KEYSTORE_SETTING_NAME, "keystore.seed"));
+            containsInAnyOrder(AUTOCONFIG_BOOOTSTRAP_ELASTIC_PASSWORD_HASH.getKey(), "keystore.seed"));
     }
 
-    public void testFailToWriteToKeystore() throws Exception {
-        KeyStoreWrapper keyStoreWrapper = KeyStoreWrapper.load(env.configFile());
-        keyStoreWrapper.decrypt(new char[0]);
-        // set a random password so that we fail to decrypt it in GenerateElasticPasswordHash#execute
+    public void testWithExistingKeystore() throws Exception {
+        KeyStoreWrapper keyStoreWrapper = KeyStoreWrapper.create();
         keyStoreWrapper.save(env.configFile(), randomAlphaOfLength(8).toCharArray());
         execute();
         assertThat(terminal.getOutput(), is(emptyString()));
-        assertThat(terminal.getErrorOutput(), containsString("Provided keystore password was incorrect"));
+        assertThat(terminal.getErrorOutput(), containsString("elasticsearch.keystore already exists"));
     }
 }

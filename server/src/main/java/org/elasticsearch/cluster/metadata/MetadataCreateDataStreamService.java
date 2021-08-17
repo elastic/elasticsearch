067925374a1677d319fa3b9c5d6cd5d192cbfa2a
Dan Hermann <dan.hermann@elastic.co>
2021-08-17 17:58:30 -0500

    fix compilation error

--- server/src/main/java/org/elasticsearch/cluster/metadata/MetadataCreateDataStreamService.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/MetadataCreateDataStreamService.java
@@ -37,6 +37,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;

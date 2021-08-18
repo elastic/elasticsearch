f45614128f6a613d3032af9e7052b6066c8adb71
Gordon Brown <gordon.brown@elastic.co>
2021-08-18 12:35:02 -0600

    Remove Node Shutdown API feature flag (#76588)
    * Remove Node Shutdown API feature flag
    
    This PR removes the Node Shutdown API feature flag.
    
    The Node Shutdown API will now always be available.
    
    * Check if xpack is enabled in cleanup
    
    When I removed the feature flag, I assumed that we would always have the
    Node Shutdown APIs, but that turns out not to be the case if xpack isn't
    enabled. This case was caught by the logic to handle the case where the
    feature flag wasn't enabled by accident.
    
    This commit adds the check we always should have had.
    
    * Also check version before tyring cleanup

--- test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
@@ -763,23 +763,19 @@ public abstract class ESRestTestCase extends ESTestCase {
      */
     @SuppressWarnings("unchecked")
     protected void deleteAllNodeShutdownMetadata() throws IOException {
-        if (minimumNodeVersion().onOrAfter(Version.V_7_15_0)) {
-            Request getShutdownStatus = new Request("GET", "_nodes/shutdown");
-            Map<String, Object> statusResponse = responseAsMap(adminClient().performRequest(getShutdownStatus));
-            if (statusResponse.containsKey("_nodes") && statusResponse.containsKey("cluster_name")) {
-                // If the response contains these two keys, the feature flag isn't enabled on this cluster, so skip out now.
-                // We can't check the system property directly because it only gets set for the cluster under test's JVM, not for the test
-                // runner's JVM.
-                return;
-            }
-            List<Map<String, Object>> nodesArray = (List<Map<String, Object>>) statusResponse.get("nodes");
-            List<String> nodeIds = nodesArray.stream()
-                .map(nodeShutdownMetadata -> (String) nodeShutdownMetadata.get("node_id"))
-                .collect(Collectors.toList());
-            for (String nodeId : nodeIds) {
-                Request deleteRequest = new Request("DELETE", "_nodes/" + nodeId + "/shutdown");
-                assertOK(adminClient().performRequest(deleteRequest));
-            }
+        if (hasXPack() == false || minimumNodeVersion().before(Version.V_7_15_0)) {
+            // Node shutdown APIs are only present in xpack
+            return;
+        }
+        Request getShutdownStatus = new Request("GET", "_nodes/shutdown");
+        Map<String, Object> statusResponse = responseAsMap(adminClient().performRequest(getShutdownStatus));
+        List<Map<String, Object>> nodesArray = (List<Map<String, Object>>) statusResponse.get("nodes");
+        List<String> nodeIds = nodesArray.stream()
+            .map(nodeShutdownMetadata -> (String) nodeShutdownMetadata.get("node_id"))
+            .collect(Collectors.toUnmodifiableList());
+        for (String nodeId : nodeIds) {
+            Request deleteRequest = new Request("DELETE", "_nodes/" + nodeId + "/shutdown");
+            assertOK(adminClient().performRequest(deleteRequest));
         }
     }
 

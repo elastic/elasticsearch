d0353bf6105ffeaefac14e9037d2dca11377288a
Jack Conradson <osjdconrad@gmail.com>
2021-08-18 10:29:53 -0700

    add javadoc for augmented annotation

--- modules/lang-painless/spi/src/main/java/org/elasticsearch/painless/spi/annotation/AugmentedAnnotation.java
+++ modules/lang-painless/spi/src/main/java/org/elasticsearch/painless/spi/annotation/AugmentedAnnotation.java
@@ -8,6 +8,35 @@
 
 package org.elasticsearch.painless.spi.annotation;
 
+/**
+ * Use an augmented annotation to augment a class to have available
+ * a static final field from a different class.
+ *
+ * Example:
+ * {@code
+ * // Whitelist
+ *
+ *...
+ * class java.lang.Integer {
+ *     ...
+ * }
+ *
+ * class Augmented {
+ *     int MAX_VALUE @augmented[augmented_canonical_class_name="java.lang.Integer"]
+ *     int MIN_VALUE @augmented[augmented_canonical_class_name="java.lang.Integer"]
+ * }
+ * ...
+ *
+ * // Script
+ * ...
+ * long value = <some_value>;
+ * int augmented = 0;
+ * if (value < Augmented.MAX_VALUE && value > Augmented.MIN_VALUE) {
+ *     augmented = (int)value;
+ * }
+ * ...
+ * }
+ */
 public class AugmentedAnnotation {
 
     public static final String NAME = "augmented";

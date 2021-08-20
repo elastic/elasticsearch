d4d08abbdcd1ccb1d0e96402cba01f36461eca7d
James Baiera <james.baiera@gmail.com>
2021-08-20 17:33:36 -0400

    upgrading to hadoop 3 and adding over-the-wire encryption tests

--- test/fixtures/hdfs-fixture/src/main/java/hdfs/MiniHDFS.java
+++ test/fixtures/hdfs-fixture/src/main/java/hdfs/MiniHDFS.java
@@ -84,6 +84,7 @@ public class MiniHDFS {
             cfg.set(DFSConfigKeys.DFS_BLOCK_ACCESS_TOKEN_ENABLE_KEY, "true");
             cfg.set(DFSConfigKeys.IGNORE_SECURE_PORTS_FOR_TESTING_KEY, "true");
             cfg.set(DFSConfigKeys.DFS_ENCRYPT_DATA_TRANSFER_KEY, "true");
+            cfg.set(DFSConfigKeys.DFS_ENCRYPT_DATA_TRANSFER_CIPHER_SUITES_KEY, "AES/CTR/NoPadding");
         }
 
         UserGroupInformation.setConfiguration(cfg);

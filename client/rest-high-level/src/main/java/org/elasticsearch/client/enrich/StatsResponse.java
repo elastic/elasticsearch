fdd7d2457af6ce7f3a907735127f79d3600d3d6c
Martijn van Groningen <martijn.v.groningen@gmail.com>
2021-08-23 09:23:11 +0200

    fix hlrc integration

--- client/rest-high-level/src/main/java/org/elasticsearch/client/enrich/StatsResponse.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/enrich/StatsResponse.java
@@ -17,19 +17,21 @@ import java.util.Objects;
 
 public final class StatsResponse {
 
-    private static ParseField EXECUTING_POLICIES_FIELD = new ParseField("executing_policies");
-    private static ParseField COORDINATOR_STATS_FIELD = new ParseField("coordinator_stats");
+    private static final ParseField EXECUTING_POLICIES_FIELD = new ParseField("executing_policies");
+    private static final ParseField COORDINATOR_STATS_FIELD = new ParseField("coordinator_stats");
+    private static final ParseField CACHE_STATS_FIELD = new ParseField("cache_stats");
 
     @SuppressWarnings("unchecked")
     private static final ConstructingObjectParser<StatsResponse, Void> PARSER = new ConstructingObjectParser<>(
         "stats_response",
         true,
-        args -> new StatsResponse((List<ExecutingPolicy>) args[0], (List<CoordinatorStats>) args[1])
+        args -> new StatsResponse((List<ExecutingPolicy>) args[0], (List<CoordinatorStats>) args[1], (List<CacheStats>) args[2])
     );
 
     static {
         PARSER.declareObjectArray(ConstructingObjectParser.constructorArg(), ExecutingPolicy.PARSER::apply, EXECUTING_POLICIES_FIELD);
         PARSER.declareObjectArray(ConstructingObjectParser.constructorArg(), CoordinatorStats.PARSER::apply, COORDINATOR_STATS_FIELD);
+        PARSER.declareObjectArray(ConstructingObjectParser.constructorArg(), CacheStats.PARSER::apply, CACHE_STATS_FIELD);
     }
 
     public static StatsResponse fromXContent(XContentParser parser) {
@@ -38,10 +40,12 @@ public final class StatsResponse {
 
     private final List<ExecutingPolicy> executingPolicies;
     private final List<CoordinatorStats> coordinatorStats;
+    private final List<CacheStats> cacheStats;
 
-    public StatsResponse(List<ExecutingPolicy> executingPolicies, List<CoordinatorStats> coordinatorStats) {
+    public StatsResponse(List<ExecutingPolicy> executingPolicies, List<CoordinatorStats> coordinatorStats, List<CacheStats> cacheStats) {
         this.executingPolicies = executingPolicies;
         this.coordinatorStats = coordinatorStats;
+        this.cacheStats = cacheStats;
     }
 
     public List<ExecutingPolicy> getExecutingPolicies() {
@@ -50,6 +54,10 @@ public final class StatsResponse {
 
     public List<CoordinatorStats> getCoordinatorStats() {
         return coordinatorStats;
+    }
+
+    public List<CacheStats> getCacheStats() {
+        return cacheStats;
     }
 
     public static final class CoordinatorStats {
@@ -177,4 +185,74 @@ public final class StatsResponse {
         }
     }
 
+    public static final class CacheStats {
+
+        static ParseField NODE_ID_FIELD = new ParseField("node_id");
+        static ParseField COUNT_FIELD = new ParseField("count");
+        static ParseField HITS_FIELD = new ParseField("hits");
+        static ParseField MISSES_FIELD = new ParseField("misses");
+        static ParseField EVICTIONS_FIELD = new ParseField("evictions");
+
+        private static final ConstructingObjectParser<CacheStats, Void> PARSER = new ConstructingObjectParser<>(
+            "coordinator_stats_item",
+            true,
+            args -> new CacheStats((String) args[0], (long) args[1], (long) args[2], (long) args[3], (long) args[4])
+        );
+
+        static {
+            PARSER.declareString(ConstructingObjectParser.constructorArg(), NODE_ID_FIELD);
+            PARSER.declareLong(ConstructingObjectParser.constructorArg(), COUNT_FIELD);
+            PARSER.declareLong(ConstructingObjectParser.constructorArg(), HITS_FIELD);
+            PARSER.declareLong(ConstructingObjectParser.constructorArg(), MISSES_FIELD);
+            PARSER.declareLong(ConstructingObjectParser.constructorArg(), EVICTIONS_FIELD);
+        }
+
+        private final String nodeId;
+        private final long count;
+        private final long hits;
+        private final long misses;
+        private final long evictions;
+
+        public CacheStats(String nodeId, long count, long hits, long misses, long evictions) {
+            this.nodeId = nodeId;
+            this.count = count;
+            this.hits = hits;
+            this.misses = misses;
+            this.evictions = evictions;
+        }
+
+        public String getNodeId() {
+            return nodeId;
+        }
+
+        public long getCount() {
+            return count;
+        }
+
+        public long getHits() {
+            return hits;
+        }
+
+        public long getMisses() {
+            return misses;
+        }
+
+        public long getEvictions() {
+            return evictions;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CacheStats that = (CacheStats) o;
+            return count == that.count && hits == that.hits && misses == that.misses &&
+                evictions == that.evictions && nodeId.equals(that.nodeId);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(nodeId, count, hits, misses, evictions);
+        }
+    }
 }

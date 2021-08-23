59e0adac4c6f0a51eacbc3b79fef66cc13902d06
Armin Braun <me@obrown.io>
2021-08-23 19:51:51 +0200

    Make Releasing Aggregation Buffers Safer (#76741)
    Fixing a few possible leaks and making the logic more obviously correct.

--- server/src/main/java/org/elasticsearch/search/query/QuerySearchResult.java
+++ server/src/main/java/org/elasticsearch/search/query/QuerySearchResult.java
@@ -21,6 +21,7 @@ import org.elasticsearch.common.io.stream.DelayableWriteable;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.lucene.search.TopDocsAndMaxScore;
+import org.elasticsearch.core.Releasables;
 import org.elasticsearch.search.DocValueFormat;
 import org.elasticsearch.search.RescoreDocIds;
 import org.elasticsearch.search.SearchPhaseResult;
@@ -212,6 +213,7 @@ public final class QuerySearchResult extends SearchPhaseResult {
     }
 
     public void aggregations(InternalAggregations aggregations) {
+        assert this.aggregations == null : "aggregations already set to [" + this.aggregations + "]";
         this.aggregations = aggregations == null ? null : DelayableWriteable.referencing(aggregations);
         hasAggs = aggregations != null;
     }
@@ -245,10 +247,7 @@ public final class QuerySearchResult extends SearchPhaseResult {
         if (hasConsumedTopDocs() == false) {
             consumeTopDocs();
         }
-        if (aggregations != null) {
-            aggregations.close();
-            aggregations = null;
-        }
+        releaseAggs();
     }
 
     /**
@@ -332,31 +331,41 @@ public final class QuerySearchResult extends SearchPhaseResult {
             }
         }
         setTopDocs(readTopDocs(in));
-        if (in.getVersion().before(Version.V_7_7_0)) {
-            if (hasAggs = in.readBoolean()) {
-                aggregations = DelayableWriteable.referencing(InternalAggregations.readFrom(in));
+        hasAggs = in.readBoolean();
+        boolean success = false;
+        try {
+            if (in.getVersion().before(Version.V_7_7_0)) {
+                if (hasAggs) {
+                    aggregations = DelayableWriteable.referencing(InternalAggregations.readFrom(in));
+                }
+                if (in.getVersion().before(Version.V_7_2_0)) {
+                    // The list of PipelineAggregators is sent by old versions. We don't need it anyway.
+                    in.readNamedWriteableList(PipelineAggregator.class);
+                }
+            } else {
+                if (hasAggs) {
+                    aggregations = DelayableWriteable.delayed(InternalAggregations::readFrom, in);
+                }
             }
-            if (in.getVersion().before(Version.V_7_2_0)) {
-                // The list of PipelineAggregators is sent by old versions. We don't need it anyway.
-                in.readNamedWriteableList(PipelineAggregator.class);
+            if (in.readBoolean()) {
+                suggest = new Suggest(in);
             }
-        } else {
-            if (hasAggs = in.readBoolean()) {
-                aggregations = DelayableWriteable.delayed(InternalAggregations::readFrom, in);
+            searchTimedOut = in.readBoolean();
+            terminatedEarly = in.readOptionalBoolean();
+            profileShardResults = in.readOptionalWriteable(ProfileShardResult::new);
+            hasProfileResults = profileShardResults != null;
+            serviceTimeEWMA = in.readZLong();
+            nodeQueueSize = in.readInt();
+            if (in.getVersion().onOrAfter(Version.V_7_10_0)) {
+                setShardSearchRequest(in.readOptionalWriteable(ShardSearchRequest::new));
+                setRescoreDocIds(new RescoreDocIds(in));
             }
-        }
-        if (in.readBoolean()) {
-            suggest = new Suggest(in);
-        }
-        searchTimedOut = in.readBoolean();
-        terminatedEarly = in.readOptionalBoolean();
-        profileShardResults = in.readOptionalWriteable(ProfileShardResult::new);
-        hasProfileResults = profileShardResults != null;
-        serviceTimeEWMA = in.readZLong();
-        nodeQueueSize = in.readInt();
-        if (in.getVersion().onOrAfter(Version.V_7_10_0)) {
-            setShardSearchRequest(in.readOptionalWriteable(ShardSearchRequest::new));
-            setRescoreDocIds(new RescoreDocIds(in));
+            success = true;
+        } finally {
+            if (success == false) {
+                // in case we were not able to deserialize the full message we must release the aggregation buffer
+                Releasables.close(aggregations);
+            }
         }
     }
 

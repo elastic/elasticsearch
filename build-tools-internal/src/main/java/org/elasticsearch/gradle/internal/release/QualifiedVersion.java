fec4df13eff5e3c7f2f39323920b7af76bc14483
Rory Hunter <pugnascotia@users.noreply.github.com>
2021-09-01 07:25:29 +0100

    Drop version field from changelog YAML (#76985)
    The changelog generation process currently relies on version
    information being present in the changelog YAML descriptors. However,
    this makes them difficult to update in some scenarios. For example,
    if a PR is merged and subsequently labelled for backporting, our
    automation won't update the versions in the changelog YAML.
    
    We can make the process more flexible by removing version data from
    the changelog YAML files, and instead inferring the versions from
    each changelog YAML file's existence in the git tree at each tag
    in the minor series.
    
    This change makes the process more ergonomic for developers, but
    harder to test, since I can't simply concoct YAML data for a range
    of versions. Instead, I've added a number of unit tests, and tried
    to exercise all the relevant parts.
    
    It is now an error to include `versions` the YAML file.

---
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/release/QualifiedVersion.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.gradle.internal.release;
+
+import org.elasticsearch.gradle.Version;
+
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Encapsulates comparison and printing logic for an x.y.z version with optional qualifier. This class is very similar
+ * to {@link Version}, but it dissects the qualifier in such a way that is incompatible
+ * with how {@link Version} is used in the build. It also retains any qualifier (prerelease) information, and uses
+ * that information when comparing instances.
+ */
+public final class QualifiedVersion implements Comparable<QualifiedVersion> {
+    private final int major;
+    private final int minor;
+    private final int revision;
+    private final Qualifier qualifier;
+
+    private static final Pattern pattern = Pattern.compile(
+        "^v? (\\d+) \\. (\\d+) \\. (\\d+) (?: - (alpha\\d+ | beta\\d+ | rc\\d+ | SNAPSHOT ) )? $",
+        Pattern.COMMENTS
+    );
+
+    private QualifiedVersion(int major, int minor, int revision, String qualifier) {
+        this.major = major;
+        this.minor = minor;
+        this.revision = revision;
+        this.qualifier = qualifier == null ? null : Qualifier.of(qualifier);
+    }
+
+    /**
+     * Parses the supplied string into an object.
+     * @param s a version string in strict semver
+     * @return a new instance
+     */
+    public static QualifiedVersion of(final String s) {
+        Objects.requireNonNull(s);
+        Matcher matcher = pattern.matcher(s);
+        if (matcher.matches() == false) {
+            throw new IllegalArgumentException("Invalid version format: '" + s + "'. Should be " + pattern);
+        }
+
+        return new QualifiedVersion(
+            Integer.parseInt(matcher.group(1)),
+            Integer.parseInt(matcher.group(2)),
+            Integer.parseInt(matcher.group(3)),
+            matcher.group(4)
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "%d.%d.%d%s".formatted(major, minor, revision, qualifier == null ? "" : "-" + qualifier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QualifiedVersion version = (QualifiedVersion) o;
+        return major == version.major
+            && minor == version.minor
+            && revision == version.revision
+            && Objects.equals(qualifier, version.qualifier);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(major, minor, revision, qualifier);
+    }
+
+    public int getMajor() {
+        return major;
+    }
+
+    public int getMinor() {
+        return minor;
+    }
+
+    public int getRevision() {
+        return revision;
+    }
+
+    public boolean hasQualifier() {
+        return qualifier != null;
+    }
+
+    public Qualifier getQualifier() {
+        return qualifier;
+    }
+
+    public boolean isSnapshot() {
+        return this.qualifier != null && this.qualifier.level == QualifierLevel.SNAPSHOT;
+    }
+
+    public QualifiedVersion withoutQualifier() {
+        return new QualifiedVersion(major, minor, revision, null);
+    }
+
+    private static final Comparator<QualifiedVersion> COMPARATOR = Comparator.comparing((QualifiedVersion v) -> v.major)
+        .thenComparing(v -> v.minor)
+        .thenComparing(v -> v.revision)
+        .thenComparing((QualifiedVersion v) -> v.qualifier, Comparator.nullsLast(Comparator.naturalOrder()));
+
+    @Override
+    public int compareTo(QualifiedVersion other) {
+        return COMPARATOR.compare(this, other);
+    }
+
+    public boolean isBefore(QualifiedVersion other) {
+        return this.compareTo(other) < 0;
+    }
+
+    private enum QualifierLevel {
+        alpha,
+        beta,
+        rc,
+        SNAPSHOT
+    }
+
+    private static class Qualifier implements Comparable<Qualifier> {
+        private final QualifierLevel level;
+        private final int number;
+
+        private Qualifier(QualifierLevel level, int number) {
+            this.level = level;
+            this.number = number;
+        }
+
+        private static final Comparator<Qualifier> COMPARATOR = Comparator.comparing((Qualifier p) -> p.level).thenComparing(p -> p.number);
+
+        @Override
+        public int compareTo(Qualifier other) {
+            return COMPARATOR.compare(this, other);
+        }
+
+        private static Qualifier of(String qualifier) {
+            if ("SNAPSHOT".equals(qualifier)) {
+                return new Qualifier(QualifierLevel.SNAPSHOT, 0);
+            }
+
+            Pattern pattern = Pattern.compile("^(alpha|beta|rc)(\\d+)$");
+            Matcher matcher = pattern.matcher(qualifier);
+            if (matcher.find()) {
+                String level = matcher.group(1);
+                int number = Integer.parseInt(matcher.group(2));
+                return new Qualifier(QualifierLevel.valueOf(level), number);
+            } else {
+                // This shouldn't happen - we check the format before this is called
+                throw new IllegalArgumentException("Invalid qualifier [" + qualifier + "] passed");
+            }
+        }
+
+        public String toString() {
+            return level == QualifierLevel.SNAPSHOT ? level.name() : level.name() + number;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Qualifier that = (Qualifier) o;
+            return number == that.number && level == that.level;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(level, number);
+        }
+    }
+}

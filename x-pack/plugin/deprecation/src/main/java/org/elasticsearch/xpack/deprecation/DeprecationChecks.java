b95df284052252f53be196aff110aa1486823806
Keith Massey <keith.massey@elastic.co>
2021-09-02 13:51:04 -0500

    Adding deprecation info API check for client transport profile setting (#76999)
    In 8.0, access to the transport client has been removed, so the transport.profiles.*.xpack.security.type
    setting is gone. This commit adds a deprecation info API check for this property.
    Relates #42404 #43236

--- x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/DeprecationChecks.java
+++ x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/DeprecationChecks.java
@@ -97,6 +97,7 @@ public class DeprecationChecks {
                     NodeDeprecationChecks::checkImplicitlyDisabledSecurityOnBasicAndTrial,
                     NodeDeprecationChecks::checkSearchRemoteSettings,
                     NodeDeprecationChecks::checkMonitoringExporterPassword,
+                    NodeDeprecationChecks::checkTransportClientProfilesFilterSetting,
                     NodeDeprecationChecks::checkDelayClusterStateRecoverySettings,
                     NodeDeprecationChecks::checkFixedAutoQueueSizeThreadpool,
                     NodeDeprecationChecks::checkJoinTimeoutSetting,

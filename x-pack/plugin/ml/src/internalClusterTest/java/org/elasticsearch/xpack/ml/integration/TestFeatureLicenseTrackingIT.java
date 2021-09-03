174e226876820e8792ff1b897440b85f690f5733
Benjamin Trent <benjamin.trent@elastic.co>
2021-09-03 09:05:07 -0400

    [ML] Fix bug in inference stats persister for when feature reset is called
    The test failure originates from an unhandled exception being thrown in the ml_utility threadpool.
    
    This exception was thrown by the results persister service, I found a place where the exception thrown was not ultimately handled.
    
    closes #77182

--- x-pack/plugin/ml/src/internalClusterTest/java/org/elasticsearch/xpack/ml/integration/TestFeatureLicenseTrackingIT.java
+++ x-pack/plugin/ml/src/internalClusterTest/java/org/elasticsearch/xpack/ml/integration/TestFeatureLicenseTrackingIT.java
@@ -117,7 +117,6 @@ public class TestFeatureLicenseTrackingIT extends MlSingleNodeTestCase {
         });
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/77182")
     public void testFeatureTrackingInferenceModelPipeline() throws Exception {
         String modelId = "test-load-models-classification-license-tracking";
         Map<String, String> oneHotEncoding = new HashMap<>();

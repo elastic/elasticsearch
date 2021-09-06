6f5f26a8b729b017726ee055a0c642aff423824a
danielhuang(黄华) <danielhuang@tencent.com>
2021-09-07 00:50:05 +0800

    revert base node response

--- server/src/main/java/org/elasticsearch/action/support/nodes/BaseNodeResponse.java
+++ server/src/main/java/org/elasticsearch/action/support/nodes/BaseNodeResponse.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 public abstract class BaseNodeResponse extends TransportResponse {
 
     private DiscoveryNode node;
-    private String nodeId;
 
     protected BaseNodeResponse(StreamInput in) throws IOException {
         super(in);
@@ -33,25 +32,10 @@ public abstract class BaseNodeResponse extends TransportResponse {
         this.node = node;
     }
 
-    public void purgeNodeInfo() {
-        // set nodeId and purge DiscoveryNode and TransportAddr structures
-        this.nodeId = this.getNode().getId();
-        this.node = null;
-        this.remoteAddress(null);
-    }
-
-    public String getNodeId() {
-        if (this.nodeId == null && node != null) {
-            return node.getId();
-        }
-        return this.nodeId;
-    }
-
     /**
      * The node this information relates to.
      */
     public DiscoveryNode getNode() {
-        assert node != null;
         return node;
     }
 

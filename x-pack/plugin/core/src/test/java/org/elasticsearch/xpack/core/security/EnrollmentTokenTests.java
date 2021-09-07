1399fb67846317c078db2bd016477345e29d6231
Ioannis Kakavas <ioannis@elastic.co>
2021-09-07 23:34:16 +0300

    Enroll additional nodes to cluster (#77292)
    This introduces a new CLI tool `elasticsearch-enroll-node`.
    It takes an Enrollment Token as a parameter and using the
    information in it, it attempts to
    
    - Communicate with an existing node of the cluster
    - Receive necessary key/certificate material
    - Persist said material and configuration
    
    This tool needs to be run before the first time the current
    node starts and if it doesn't have any explicit security
    related configuration already defined.

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/EnrollmentTokenTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/EnrollmentTokenTests.java
@@ -5,11 +5,12 @@
  * 2.0.
  */
 
-package org.elasticsearch.xpack.security.enrollment;
+package org.elasticsearch.xpack.core.security;
 
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.xpack.core.security.EnrollmentToken;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
@@ -18,17 +19,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static org.hamcrest.Matchers.equalTo;
+
 public class EnrollmentTokenTests extends ESTestCase {
-    EnrollmentToken createEnrollmentToken() {
-        final String apiKey = randomAlphaOfLength(16);
-        final String fingerprint = randomAlphaOfLength(64);
-        final String version = randomAlphaOfLength(5);
-        final List<String> boundAddresses = Arrays.asList(generateRandomStringArray(4, randomIntBetween(2, 32), false));
-        return new EnrollmentToken(apiKey, fingerprint, version, boundAddresses);
-    }
 
     public void testEnrollmentToken() throws Exception {
-        EnrollmentToken enrollmentToken = createEnrollmentToken();
+        final EnrollmentToken enrollmentToken = createEnrollmentToken();
         final String apiKey = enrollmentToken.getApiKey();
         final String fingerprint = enrollmentToken.getFingerprint();
         final String version = enrollmentToken.getVersion();
@@ -47,5 +43,19 @@ public class EnrollmentTokenTests extends ESTestCase {
         assertEquals(enrollmentMap.get("ver"), version);
         assertEquals(enrollmentMap.get("adr"), "[" + boundAddresses.stream().collect(Collectors.joining(", ")) + "]");
         assertEquals(new String(Base64.getDecoder().decode(encoded), StandardCharsets.UTF_8), jsonString);
+    }
+
+    public void testDeserialization() throws Exception {
+        final EnrollmentToken enrollmentToken = createEnrollmentToken();
+        final EnrollmentToken deserialized = EnrollmentToken.decodeFromString(enrollmentToken.getEncoded());
+        assertThat(enrollmentToken, equalTo(deserialized));
+    }
+
+    private EnrollmentToken createEnrollmentToken() {
+        final String apiKey = randomAlphaOfLength(16);
+        final String fingerprint = randomAlphaOfLength(64);
+        final String version = randomAlphaOfLength(5);
+        final List<String> boundAddresses = Arrays.asList(generateRandomStringArray(4, randomIntBetween(2, 32), false));
+        return new EnrollmentToken(apiKey, fingerprint, version, boundAddresses);
     }
 }

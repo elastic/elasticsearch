25ae1363a217b07b6369c8d463c600c9bd9c7037
Jack Conradson <osjdconrad@gmail.com>
2021-09-15 11:55:28 -0700

    Split up fields into different field types for Painless Fields API (#77732)
    This change refactors the inner fields classes and inner converter classes into field class split by type 
    for the Painless Fields API.
    
    Each field class now contains the following:
    
    * it's own members
    * a converter that converts from other types of fields to its own type of field
    * conversion helper methods to go from its own type of field to other types of fields
    * conversion helper methods to go from its own type to other types

--- server/src/main/java/org/elasticsearch/script/field/LongField.java
+++ server/src/main/java/org/elasticsearch/script/field/LongField.java
@@ -8,7 +8,86 @@
 
 package org.elasticsearch.script.field;
 
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LongField extends Field<Long> {
+
+    /* ---- Conversion Class From Other Fields ----*/
+
+    /**
+     * Convert to a {@link LongField} from Double, String, DateMillis, DateNanos, BigInteger or Boolean Fields.
+     * Double is cast to a Long.
+     * String is parsed as a Long.
+     * DateMillis is milliseconds since epoch.
+     * DateNanos is nanoseconds since epoch.
+     * {@link java.math.BigInteger#longValue()} is used for the BigInteger conversion.
+     * Boolean is {@code 1L} if {@code true}, {@code 0L} if {@code false}.
+     */
+    public static final Converter<Long, LongField> Long;
+
+    static {
+        Long = new Converter<Long, LongField>() {
+            @Override
+            public LongField convert(Field<?> sourceField) {
+                if (sourceField instanceof DoubleField) {
+                    return DoubleField.toLongField((DoubleField) sourceField);
+                }
+
+                if (sourceField instanceof StringField) {
+                    return StringField.toLongField((StringField) sourceField);
+                }
+
+                if (sourceField instanceof DateMillisField) {
+                    return DateMillisField.toLongField((DateMillisField) sourceField);
+                }
+
+                if (sourceField instanceof DateNanosField) {
+                    return DateNanosField.toLongField((DateNanosField) sourceField);
+                }
+
+                if (sourceField instanceof BigIntegerField) {
+                    return BigIntegerField.toLongField((BigIntegerField) sourceField);
+                }
+
+                if (sourceField instanceof BooleanField) {
+                    return BooleanField.toLongField((BooleanField) sourceField);
+                }
+
+                throw new InvalidConversion(sourceField.getClass(), getFieldClass());
+            }
+
+            @Override
+            public Class<LongField> getFieldClass() {
+                return LongField.class;
+            }
+
+            @Override
+            public Class<Long> getTargetClass() {
+                return Long.class;
+            }
+        };
+    }
+
+    /* ---- Conversion Helpers To Other Fields ---- */
+
+    public static BigIntegerField toBigIntegerField(LongField sourceField) {
+        FieldValues<Long> fv = sourceField.getFieldValues();
+        return new BigIntegerField(sourceField.getName(), new DelegatingFieldValues<BigInteger, Long>(fv) {
+            @Override
+            public List<BigInteger> getValues() {
+                return values.getValues().stream().map(BigInteger::valueOf).collect(Collectors.toList());
+            }
+
+            @Override
+            public BigInteger getNonPrimitiveValue() {
+                return BigInteger.valueOf(values.getLongValue());
+            }
+        });
+    }
+
+    /* ---- Long Field Members ---- */
 
     public LongField(String name, FieldValues<Long> values) {
         super(name, values);

1186ca6f19fc74b06ecc4e2af6a65d2bc2d37d82
Artem Prigoda <artem.prigoda@elastic.co>
2021-09-15 17:07:35 +0200

    Revert "Use Maps.toUnmodifiableSortedMap"
    This reverts commit 13e916c65ef07fc0311e54ee8ce3d7461cb20366.

--- client/rest-high-level/src/main/java/org/elasticsearch/client/ccr/GetAutoFollowPatternResponse.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/ccr/GetAutoFollowPatternResponse.java
@@ -8,14 +8,13 @@
 
 package org.elasticsearch.client.ccr;
 
+import org.elasticsearch.common.xcontent.ParseField;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.ByteSizeValue;
-import org.elasticsearch.common.util.Maps;
+import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.common.xcontent.ConstructingObjectParser;
 import org.elasticsearch.common.xcontent.ObjectParser;
-import org.elasticsearch.common.xcontent.ParseField;
 import org.elasticsearch.common.xcontent.XContentParser;
-import org.elasticsearch.core.TimeValue;
 
 import java.util.AbstractMap;
 import java.util.Collections;
@@ -23,6 +22,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.Objects;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public final class GetAutoFollowPatternResponse {
 
@@ -42,8 +43,8 @@ public final class GetAutoFollowPatternResponse {
         "get_auto_follow_pattern_response", true, args -> {
             @SuppressWarnings("unchecked")
             List<Map.Entry<String, Pattern>> entries = (List<Map.Entry<String, Pattern>>) args[0];
-            return new GetAutoFollowPatternResponse(entries.stream()
-                .collect(Maps.toUnmodifiableSortedMap(Map.Entry::getKey, Map.Entry::getValue)));
+            return new GetAutoFollowPatternResponse(new TreeMap<>(entries.stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))));
     });
 
     static {

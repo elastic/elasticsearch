751bbdd341509301daaa4cade581581e3ae418a3
Ignacio Vera <iverase@gmail.com>
2021-09-16 11:37:40 +0200

    Improve testing of geo_shape field in the spatial plugin (#77687) (#77834)
    Adds more integration test to the spatial plugin.

--- x-pack/plugin/spatial/src/internalClusterTest/java/org/elasticsearch/xpack/spatial/search/LegacyGeoShapeWithDocValuesIT.java
+++ x-pack/plugin/spatial/src/internalClusterTest/java/org/elasticsearch/xpack/spatial/search/LegacyGeoShapeWithDocValuesIT.java
@@ -1,19 +1,20 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
 
-package org.elasticsearch.search.geo;
+package org.elasticsearch.xpack.spatial.search;
 
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.xcontent.ToXContent;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.geometry.Circle;
 import org.elasticsearch.plugins.Plugin;
-import org.elasticsearch.test.TestLegacyGeoShapeFieldMapperPlugin;
+import org.elasticsearch.search.geo.GeoShapeIntegTestCase;
+import org.elasticsearch.xpack.spatial.LocalStateSpatialPlugin;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -21,9 +22,10 @@ import java.util.Collections;
 
 import static org.elasticsearch.index.query.QueryBuilders.geoShapeQuery;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
-public class LegacyGeoShapeIT extends GeoShapeIntegTestCase {
+public class LegacyGeoShapeWithDocValuesIT extends GeoShapeIntegTestCase {
 
     @Override
     protected boolean addMockGeoShapeFieldMapper() {
@@ -32,7 +34,7 @@ public class LegacyGeoShapeIT extends GeoShapeIntegTestCase {
 
     @Override
     protected Collection<Class<? extends Plugin>> nodePlugins() {
-        return Collections.singleton(TestLegacyGeoShapeFieldMapperPlugin.class);
+        return Collections.singleton(LocalStateSpatialPlugin.class);
     }
 
     @Override
@@ -44,6 +46,26 @@ public class LegacyGeoShapeIT extends GeoShapeIntegTestCase {
     @Override
     protected boolean allowExpensiveQueries() {
         return false;
+    }
+
+    public void testMappingUpdate() {
+        // create index
+        assertAcked(client().admin().indices().prepareCreate("test")
+            .addMapping("shape", "shape", "type=geo_shape,strategy=recursive").get());
+        ensureGreen();
+
+        String update ="{\n" +
+            "  \"properties\": {\n" +
+            "    \"shape\": {\n" +
+            "      \"type\": \"geo_shape\"" +
+            "    }\n" +
+            "  }\n" +
+            "}";
+
+        IllegalArgumentException e = expectThrows(IllegalArgumentException.class, () -> client().admin().indices()
+            .preparePutMapping("test").setType("shape")
+            .setSource(update, XContentType.JSON).get());
+        assertThat(e.getMessage(), containsString("mapper [shape] of type [geo_shape] cannot change strategy from [recursive] to [BKD]"));
     }
 
     /**

e681a7c47e792c683b26faafb97e160858b0b452
Christos Soulios <1561376+csoulios@users.noreply.github.com>
2021-09-20 21:18:39 +0300

    Rename `dimension` mapping parameter to `time_series_dimension` (#78012)
    This PR renames dimension mapping parameter to time_series_dimension to make it consistent with time_series_metric parameter (#76766)
    
    Relates to #74450 and #74014

--- server/src/test/java/org/elasticsearch/index/mapper/NumberFieldMapperTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/NumberFieldMapperTests.java
@@ -255,9 +255,9 @@ public abstract class NumberFieldMapperTests extends MapperTestCase {
         // dimension = true is not allowed
         Exception e = expectThrows(MapperParsingException.class, () -> createDocumentMapper(fieldMapping(b -> {
             minimalMapping(b);
-            b.field("dimension", true);
+            b.field("time_series_dimension", true);
         })));
-        assertThat(e.getCause().getMessage(), containsString("Parameter [dimension] cannot be set"));
+        assertThat(e.getCause().getMessage(), containsString("Parameter [time_series_dimension] cannot be set"));
     }
 
     public void testMetricType() throws IOException {

38392ff6fa8929544ad772a0ea80fdf790cbd804
Ryan Ernst <ryan@elastic.co>
2021-09-21 11:37:13 -0700

    Fix split package in analysis-smartcn plugin (#78045) (#78127)
    The analysis-smartcn plugin reuses server the server package name for
    analysis. This commit moves the plugin implementation to use a single
    package name, o.e.p.analysis.smartcn
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- plugins/analysis-smartcn/src/main/java/org/elasticsearch/plugin/analysis/smartcn/SmartChineseNoOpTokenFilterFactory.java
+++ plugins/analysis-smartcn/src/main/java/org/elasticsearch/plugin/analysis/smartcn/SmartChineseNoOpTokenFilterFactory.java
@@ -6,22 +6,22 @@
  * Side Public License, v 1.
  */
 
-package org.elasticsearch.index.analysis;
+package org.elasticsearch.plugin.analysis.smartcn;
 
-import org.apache.lucene.analysis.Tokenizer;
-import org.apache.lucene.analysis.cn.smart.HMMChineseTokenizer;
+import org.apache.lucene.analysis.TokenStream;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
+import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
 
-public class SmartChineseTokenizerTokenizerFactory extends AbstractTokenizerFactory {
+public class SmartChineseNoOpTokenFilterFactory extends AbstractTokenFilterFactory {
 
-    public SmartChineseTokenizerTokenizerFactory(IndexSettings indexSettings, Environment environment, String name, Settings settings) {
-        super(indexSettings, settings, name);
+    public SmartChineseNoOpTokenFilterFactory(IndexSettings indexSettings, Environment environment, String name, Settings settings) {
+        super(indexSettings, name, settings);
     }
 
     @Override
-    public Tokenizer create() {
-        return new HMMChineseTokenizer();
+    public TokenStream create(TokenStream tokenStream) {
+        return tokenStream;
     }
 }

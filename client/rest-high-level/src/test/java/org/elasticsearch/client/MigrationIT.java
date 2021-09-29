6c18bf9892bf782fb2006c37e34151f5b6a3188e
William Brafford <william.brafford@elastic.co>
2021-09-29 10:03:01 -0400

    Fix client integrationt test

--- client/rest-high-level/src/test/java/org/elasticsearch/client/MigrationIT.java
+++ client/rest-high-level/src/test/java/org/elasticsearch/client/MigrationIT.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.util.Collections;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
 
 public class MigrationIT extends ESRestHighLevelClientTestCase {
 
@@ -37,16 +38,24 @@ public class MigrationIT extends ESRestHighLevelClientTestCase {
     public void testGetFeatureUpgradeStatus() throws IOException {
         GetFeatureUpgradeStatusRequest request = new GetFeatureUpgradeStatusRequest();
         GetFeatureUpgradeStatusResponse response = highLevelClient().migration().getFeatureUpgradeStatus(request, RequestOptions.DEFAULT);
-        // a test like this cannot test actual deprecations
         assertThat(response.getUpgradeStatus(), equalTo("UPGRADE_NEEDED"));
-        assertThat(response.getFeatureUpgradeStatuses().size(), equalTo(0));
+        assertThat(response.getFeatureUpgradeStatuses().size(), equalTo(1));
+        GetFeatureUpgradeStatusResponse.FeatureUpgradeStatus status = response.getFeatureUpgradeStatuses().get(0);
+        assertThat(status.getUpgradeStatus(), equalTo("UPGRADE_NEEDED"));
+        assertThat(status.getMinimumIndexVersion(), equalTo("7.1.1"));
+        assertThat(status.getFeatureName(), equalTo("security"));
+        assertThat(status.getIndexVersions().size(), equalTo(1));
     }
 
     public void testPostFeatureUpgradeStatus() throws IOException {
         PostFeatureUpgradeRequest request = new PostFeatureUpgradeRequest();
         PostFeatureUpgradeResponse response = highLevelClient().migration().postFeatureUpgrade(request, RequestOptions.DEFAULT);
         // a test like this cannot test actual deprecations
-        assertThat(response.isAccepted(), equalTo(false));
-        assertThat(response.getFeatures().size(), equalTo(0));
+        assertThat(response.isAccepted(), equalTo(true));
+        assertThat(response.getFeatures().size(), equalTo(1));
+        PostFeatureUpgradeResponse.Feature feature = response.getFeatures().get(0);
+        assertThat(feature.getFeatureName(), equalTo("security"));
+        assertThat(response.getReason(), nullValue());
+        assertThat(response.getElasticsearchException(), nullValue());
     }
 }

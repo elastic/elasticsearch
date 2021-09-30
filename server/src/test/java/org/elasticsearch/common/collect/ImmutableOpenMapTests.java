4998241af969adff3f72529edb27918fd9865120
Artem Prigoda <artem.prigoda@elastic.co>
2021-09-30 12:07:10 +0200

    Remove unused import

--- server/src/test/java/org/elasticsearch/common/collect/ImmutableOpenMapTests.java
+++ server/src/test/java/org/elasticsearch/common/collect/ImmutableOpenMapTests.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;

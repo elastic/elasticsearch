b1799df930443edef026eb8fe463f3575e8e3625
Nikola Grcevski <nikola.grcevski@elastic.co>
2021-10-01 19:14:01 -0400

    Fix style.

--- server/src/test/java/org/elasticsearch/gateway/MetadataStateFormatTests.java
+++ server/src/test/java/org/elasticsearch/gateway/MetadataStateFormatTests.java
@@ -42,13 +42,9 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
 
 @LuceneTestCase.SuppressFileSystems("ExtrasFS") // TODO: fix test to work with ExtrasFS
 public class MetadataStateFormatTests extends ESTestCase {

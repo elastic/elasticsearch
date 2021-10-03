f6d80e9b122c2248431f33d32dde4e231a1ab3b7
Ioannis Kakavas <ioannis@elastic.co>
2021-10-04 00:43:28 +0300

    can retries be the solution ?

--- qa/os/src/test/java/org/elasticsearch/packaging/util/Cleanup.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/util/Cleanup.java
@@ -74,7 +74,7 @@ public class Cleanup {
         // when we run es as a role user on windows, add the equivalent here
         // delete files that may still exist
 
-        lsGlob(getRootTempDir(), "elasticsearch*").forEach(FileUtils::rm);
+        lsGlob(getRootTempDir(), "elasticsearch*").forEach(Platforms.WINDOWS ? FileUtils::rmWithRetries : FileUtils::rm);
         final List<String> filesToDelete = Platforms.WINDOWS ? ELASTICSEARCH_FILES_WINDOWS : ELASTICSEARCH_FILES_LINUX;
         // windows needs leniency due to asinine releasing of file locking async from a process exiting
         Consumer<? super Path> rm = Platforms.WINDOWS ? FileUtils::rmWithRetries : FileUtils::rm;

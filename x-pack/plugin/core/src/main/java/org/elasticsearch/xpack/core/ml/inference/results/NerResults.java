e0270013f7da90f54b088bb09b5b863bbe978d4a
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2021-10-04 15:43:48 -0400

    [ML] adding some new test hooks for inference results testing
    New testing scaffolding for inference ingest testing and
    a bug fix for text embedding inference results

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/inference/results/NerResults.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/inference/results/NerResults.java
@@ -83,6 +83,7 @@ public class NerResults implements InferenceResults {
         return entityGroups;
     }
 
+    @Override
     public String getResultsField() {
         return resultsField;
     }

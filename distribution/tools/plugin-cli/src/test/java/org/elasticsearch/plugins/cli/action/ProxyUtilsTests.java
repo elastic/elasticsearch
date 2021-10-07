ddbd26b61e891661bff8fbad27ddd9d6a37f8abb
Rory Hunter <roryhunter2@gmail.com>
2021-10-07 10:33:57 +0100

    Fix tests

--- distribution/tools/plugin-cli/src/test/java/org/elasticsearch/plugins/cli/action/ProxyUtilsTests.java
+++ distribution/tools/plugin-cli/src/test/java/org/elasticsearch/plugins/cli/action/ProxyUtilsTests.java
@@ -8,6 +8,7 @@
 
 package org.elasticsearch.plugins.cli.action;
 
+import org.elasticsearch.cli.UserException;
 import org.elasticsearch.core.SuppressForbidden;
 import org.elasticsearch.test.ESTestCase;
 
@@ -17,6 +18,8 @@ import java.util.stream.Stream;
 import static org.elasticsearch.plugins.cli.action.ProxyMatcher.matchesProxy;
 import static org.elasticsearch.plugins.cli.action.ProxyUtils.buildProxy;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 public class ProxyUtilsTests extends ESTestCase {
     /**
@@ -37,7 +40,7 @@ public class ProxyUtilsTests extends ESTestCase {
      * Check that building a proxy with a null value succeeds, returning a pass-through (direct) proxy.
      */
     public void testBuildProxy_withNullValue() throws Exception {
-        assertThat(buildProxy(null), matchesProxy(Type.DIRECT));
+        assertThat(buildProxy(null), is(nullValue()));
     }
 
     /**
@@ -45,7 +48,7 @@ public class ProxyUtilsTests extends ESTestCase {
      */
     public void testBuildProxy_withInvalidHost() {
         Stream.of("blah_blah:1234", "-host.domain:1234", "host.-domain:1234", "tést:1234", ":1234").forEach(testCase -> {
-            PluginSyncException e = expectThrows(PluginSyncException.class, () -> buildProxy(testCase));
+            UserException e = expectThrows(UserException.class, () -> buildProxy(testCase));
             assertThat(e.getMessage(), equalTo("Malformed [proxy], expected [host:port]"));
         });
     }
@@ -55,71 +58,8 @@ public class ProxyUtilsTests extends ESTestCase {
      */
     public void testBuildProxy_withInvalidPort() {
         Stream.of("host.domain:-1", "host.domain:$PORT", "host.domain:{{port}}", "host.domain").forEach(testCase -> {
-            PluginSyncException e = expectThrows(PluginSyncException.class, () -> buildProxy(testCase));
+            UserException e = expectThrows(UserException.class, () -> buildProxy(testCase));
             assertThat(e.getMessage(), equalTo("Malformed [proxy], expected [host:port]"));
         });
-    }
-
-    /**
-     * Check that building a proxy with a null input but with system {@code http.*} properties set returns the correct proxy.
-     */
-    @SuppressForbidden(reason = "Sets http proxy properties")
-    public void testBuildProxy_withNullValueAndSystemHttpProxy() throws Exception {
-        String prevHost = null;
-        String prevPort = null;
-
-        try {
-            prevHost = System.getProperty("http.proxyHost");
-            prevPort = System.getProperty("http.proxyPort");
-            System.setProperty("http.proxyHost", "host.localhost");
-            System.setProperty("http.proxyPort", "1234");
-
-            assertThat(buildProxy(null), matchesProxy(Type.HTTP, "host.localhost", 1234));
-        } finally {
-            System.setProperty("http.proxyHost", prevHost == null ? "" : prevHost);
-            System.setProperty("http.proxyPort", prevPort == null ? "" : prevPort);
-        }
-    }
-
-    /**
-     * Check that building a proxy with a null input but with system {@code https.*} properties set returns the correct proxy.
-     */
-    @SuppressForbidden(reason = "Sets https proxy properties")
-    public void testBuildProxy_withNullValueAndSystemHttpsProxy() throws Exception {
-        String prevHost = null;
-        String prevPort = null;
-
-        try {
-            prevHost = System.getProperty("https.proxyHost");
-            prevPort = System.getProperty("https.proxyPort");
-            System.setProperty("https.proxyHost", "host.localhost");
-            System.setProperty("https.proxyPort", "1234");
-
-            assertThat(buildProxy(null), matchesProxy(Type.HTTP, "host.localhost", 1234));
-        } finally {
-            System.setProperty("https.proxyHost", prevHost == null ? "" : prevHost);
-            System.setProperty("https.proxyPort", prevPort == null ? "" : prevPort);
-        }
-    }
-
-    /**
-     * Check that building a proxy with a null input but with system {@code socks.*} properties set returns the correct proxy.
-     */
-    @SuppressForbidden(reason = "Sets socks proxy properties")
-    public void testBuildProxy_withNullValueAndSystemSocksProxy() throws Exception {
-        String prevHost = null;
-        String prevPort = null;
-
-        try {
-            prevHost = System.getProperty("socks.proxyHost");
-            prevPort = System.getProperty("socks.proxyPort");
-            System.setProperty("socks.proxyHost", "host.localhost");
-            System.setProperty("socks.proxyPort", "1234");
-
-            assertThat(buildProxy(null), matchesProxy(Type.SOCKS, "host.localhost", 1234));
-        } finally {
-            System.setProperty("socks.proxyHost", prevHost == null ? "" : prevHost);
-            System.setProperty("socks.proxyPort", prevPort == null ? "" : prevPort);
-        }
     }
 }

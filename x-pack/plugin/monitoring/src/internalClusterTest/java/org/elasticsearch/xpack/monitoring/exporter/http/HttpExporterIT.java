4d58cb45a9078cc12cd1db15b61aa4316cdc4faf
pgomulka <przemyslaw.gomulka@elastic.co>
2021-10-12 15:41:36 +0200

    remove overrides in gradle files

--- x-pack/plugin/monitoring/src/internalClusterTest/java/org/elasticsearch/xpack/monitoring/exporter/http/HttpExporterIT.java
+++ x-pack/plugin/monitoring/src/internalClusterTest/java/org/elasticsearch/xpack/monitoring/exporter/http/HttpExporterIT.java
@@ -110,8 +110,7 @@ public class HttpExporterIT extends MonitoringIntegTestCase {
             .put(MonitoringService.INTERVAL.getKey(), MonitoringService.MIN_INTERVAL)
             // we do this by default in core, but for monitoring this isn't needed and only adds noise.
             .put("indices.lifecycle.history_index_enabled", false)
-            .put("index.store.mock.check_index_on_close", false)
-            .put("cluster.deprecation_indexing.enabled", false);
+            .put("index.store.mock.check_index_on_close", false);
         return builder.build();
     }
 
--- x-pack/plugin/monitoring/src/test/java/org/elasticsearch/xpack/monitoring/test/MonitoringIntegTestCase.java
+++ x-pack/plugin/monitoring/src/test/java/org/elasticsearch/xpack/monitoring/test/MonitoringIntegTestCase.java
@@ -54,8 +54,7 @@ public abstract class MonitoringIntegTestCase extends ESIntegTestCase {
 //                .put(XPackSettings.MACHINE_LEARNING_ENABLED.getKey(), false)
                 // we do this by default in core, but for monitoring this isn't needed and only adds noise.
                 .put("indices.lifecycle.history_index_enabled", false)
-                .put("index.store.mock.check_index_on_close", false)
-                .put("cluster.deprecation_indexing.enabled", false);
+                .put("index.store.mock.check_index_on_close", false);
 
         return builder.build();
     }

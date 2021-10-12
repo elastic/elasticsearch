486a0fb10de70979cfa45b74600efa039fca5ee6
jimczi <jimczi@apache.org>
2021-10-12 12:41:46 +0200

    fix compil

--- server/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
+++ server/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
@@ -22,6 +22,7 @@ import org.elasticsearch.cluster.block.ClusterBlockException;
 import org.elasticsearch.cluster.block.ClusterBlockLevel;
 import org.elasticsearch.cluster.metadata.IndexAbstraction;
 import org.elasticsearch.cluster.metadata.IndexAbstraction.DataStream;
+import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.node.DiscoveryNodes;

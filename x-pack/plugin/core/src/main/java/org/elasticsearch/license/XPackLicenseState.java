7542a5fa773d06db712792a7200d0e9149dfe9fa
Ryan Ernst <ryan@iernst.net>
2021-10-14 20:27:59 -0700

    actually remove constant

--- x-pack/plugin/core/src/main/java/org/elasticsearch/license/XPackLicenseState.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/license/XPackLicenseState.java
@@ -48,8 +48,6 @@ public class XPackLicenseState {
         SECURITY_AUTHORIZATION_REALM(OperationMode.PLATINUM, true),
         SECURITY_AUTHORIZATION_ENGINE(OperationMode.PLATINUM, true),
 
-        MONITORING_CLUSTER_ALERTS(OperationMode.STANDARD, true),
-
         CCR(OperationMode.PLATINUM, true),
 
         MACHINE_LEARNING(OperationMode.PLATINUM, true),

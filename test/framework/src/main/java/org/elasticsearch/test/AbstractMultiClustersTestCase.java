c6dd6cfc699ba01551eb0c331ff34225b1c6bc63
Ryan Ernst <ryan@iernst.net>
2021-10-14 17:56:16 -0700

    fix test

--- test/framework/src/main/java/org/elasticsearch/test/AbstractMultiClustersTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/AbstractMultiClustersTestCase.java
@@ -110,8 +110,9 @@ public abstract class AbstractMultiClustersTestCase extends ESTestCase {
     @Override
     public List<String> filteredWarnings() {
         return Stream.concat(super.filteredWarnings().stream(),
-            List.of("Configuring multiple [path.data] paths is deprecated. Use RAID or other system level features for utilizing " +
-            "multiple disks. This feature will be removed in 8.0.").stream()).collect(Collectors.toList());
+            List.of("Configuring [path.data] with a list is deprecated. Instead specify as a string value.",
+                "Configuring multiple [path.data] paths is deprecated. Use RAID or other system level features for utilizing " +
+            "multiple disks. This feature will be removed in a future release.").stream()).collect(Collectors.toList());
     }
 
     @After

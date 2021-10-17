868c1caaabe4cec3f072f74edd88e86e8a321949
Armin Braun <me@obrown.io>
2021-10-17 15:47:12 +0200

    Reenable BwC Tests after #79308 (#79313)
    Reenabling BwC Tests now that #79308 has been merged.

--- server/src/main/java/org/elasticsearch/action/admin/cluster/snapshots/get/GetSnapshotsRequest.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/snapshots/get/GetSnapshotsRequest.java
@@ -40,7 +40,7 @@ public class GetSnapshotsRequest extends MasterNodeRequest<GetSnapshotsRequest> 
     public static final String NO_POLICY_PATTERN = "_none";
     public static final boolean DEFAULT_VERBOSE_MODE = true;
 
-    public static final Version SLM_POLICY_FILTERING_VERSION = Version.V_8_0_0;
+    public static final Version SLM_POLICY_FILTERING_VERSION = Version.V_7_16_0;
 
     public static final Version FROM_SORT_VALUE_VERSION = Version.V_8_0_0;
 

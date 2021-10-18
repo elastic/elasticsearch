d97a18d1f426ac06ee07951bfddd8ba2067b8cb0
Yang Wang <yang.wang@elastic.co>
2021-10-18 12:39:45 +1100

    Better logging and internal user handling for operator privileges
    This PR improves logging situation for operator privilegs. The loggings
    are now more informational and also do not rely on the tracing level.
    Internal user handling is also improved by skipping them more
    consistently when marking and checking for operator users.
    
    The existing code performs unnecessary checks for internal actions such
    as leader/follower checks. It is technically a bug because threadContext
    is prepared differently for internal actions and does not have the
    operatorUser marking. However, the bug currently does not manifest
    itself since internal actions are not protected by operator privileges.

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authz/AuthorizationServiceTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authz/AuthorizationServiceTests.java
@@ -364,7 +364,7 @@ public class AuthorizationServiceTests extends ESTestCase {
         final ElasticsearchSecurityException operatorPrivilegesException =
             new ElasticsearchSecurityException("Operator privileges check failed");
         if (shouldFailOperatorPrivilegesCheck) {
-            when(operatorPrivilegesService.check(action, request, threadContext)).thenAnswer(invocationOnMock -> {
+            when(operatorPrivilegesService.check(authentication, action, request, threadContext)).thenAnswer(invocationOnMock -> {
                 operatorPrivilegesChecked.set(true);
                 return operatorPrivilegesException;
             });
@@ -376,7 +376,7 @@ public class AuthorizationServiceTests extends ESTestCase {
             authorizationInfo.onResponse(threadContext.getTransient(AUTHORIZATION_INFO_KEY));
             indicesPermissions.onResponse(threadContext.getTransient(INDICES_PERMISSIONS_KEY));
 
-            assertNull(verify(operatorPrivilegesService).check(action, request, threadContext));
+            assertNull(verify(operatorPrivilegesService).check(authentication, action, request, threadContext));
 
             if (listenerBody != null) {
                 listenerBody.run();

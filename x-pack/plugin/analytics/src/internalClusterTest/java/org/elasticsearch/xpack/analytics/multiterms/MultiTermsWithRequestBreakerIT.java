cb2e5b37bb9a88271d6b55696ca6d135619d1864
Ignacio Vera <iverase@gmail.com>
2021-10-19 11:09:15 +0200

    mute MultiTermsWithRequestBreakerIT (#79440)

--- x-pack/plugin/analytics/src/internalClusterTest/java/org/elasticsearch/xpack/analytics/multiterms/MultiTermsWithRequestBreakerIT.java
+++ x-pack/plugin/analytics/src/internalClusterTest/java/org/elasticsearch/xpack/analytics/multiterms/MultiTermsWithRequestBreakerIT.java
@@ -41,6 +41,7 @@ public class MultiTermsWithRequestBreakerIT extends ESIntegTestCase {
     /**
      * Test that searches using multiterms aggregations returns all request breaker memory.
      */
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/79437")
     public void testRequestBreaker() throws Exception {
         final String requestBreaker = randomIntBetween(1, 10000) + "kb";
         logger.info("--> Using request breaker setting: {}", requestBreaker);

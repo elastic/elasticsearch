88250c216c118ab56ec6ae5fa5551bec1f7b10a9
Keith Massey <keith.massey@elastic.co>
2021-10-19 07:51:05 -0500

    Exposing the ability to log deprecated settings at non-critical level (#79107)
    A recent change for the deprecation logs provided the capability to emit deprecation's at critical vs. warning levels, #77482.
    However deprecated settings always log at critical level without the ability to express that the setting deprecation is only a
    warning.
    
    This commit exposes the ability to set the deprecation level when deprecating a setting.
    Closes #78781

--- x-pack/plugin/core/src/main/java/org/elasticsearch/license/XPackLicenseState.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/license/XPackLicenseState.java
@@ -7,6 +7,7 @@
 package org.elasticsearch.license;
 
 import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.logging.DeprecationLogger;
 import org.elasticsearch.common.logging.HeaderWarning;
 import org.elasticsearch.common.logging.LoggerMessageFormat;
 import org.elasticsearch.common.settings.Settings;
@@ -437,7 +438,7 @@ public class XPackLicenseState {
     void checkExpiry() {
         String warning = status.expiryWarning;
         if (warning != null) {
-            HeaderWarning.addWarning(warning);
+            HeaderWarning.addWarning(DeprecationLogger.CRITICAL, warning);
         }
     }
 

2709d7f371cdc71294c1b77e38f73e65dcc6126d
Jack Conradson <osjdconrad@gmail.com>
2021-10-20 16:35:34 -0700

    fix import

--- server/src/test/java/org/elasticsearch/search/lookup/LeafDocLookupTests.java
+++ server/src/test/java/org/elasticsearch/search/lookup/LeafDocLookupTests.java
@@ -22,7 +22,6 @@ import java.util.function.Function;
 
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.Matchers.anyObject;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;

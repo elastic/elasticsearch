6ad4093ace85042862fef24be50168fc8c193f8b
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2021-10-20 16:40:42 -0400

    [ML] fail on poor configuration for categorize_text
    This commit fixes a handful of bugs with categorize_text agg
    
    The agg now fails on fields that are not text fields
    
    Limits the number of tokens categorized
    
    Validates the configuration inputs to disallow settings above static maximums

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/aggs/categorization/CategorizeTextAggregationBuilder.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/aggs/categorization/CategorizeTextAggregationBuilder.java
@@ -123,7 +123,7 @@ public class CategorizeTextAggregationBuilder extends AbstractAggregationBuilder
 
     public CategorizeTextAggregationBuilder setMaxUniqueTokens(int maxUniqueTokens) {
         this.maxUniqueTokens = maxUniqueTokens;
-        if (maxUniqueTokens <= 0) {
+        if (maxUniqueTokens <= 0 || maxUniqueTokens > MAX_MAX_UNIQUE_TOKENS) {
             throw ExceptionsHelper.badRequestException(
                 "[{}] must be greater than 0 and less than [{}]. Found [{}] in [{}]",
                 MAX_UNIQUE_TOKENS.getPreferredName(),
@@ -191,7 +191,7 @@ public class CategorizeTextAggregationBuilder extends AbstractAggregationBuilder
 
     public CategorizeTextAggregationBuilder setMaxMatchedTokens(int maxMatchedTokens) {
         this.maxMatchedTokens = maxMatchedTokens;
-        if (maxMatchedTokens <= 0) {
+        if (maxMatchedTokens <= 0 || maxMatchedTokens > MAX_MAX_MATCHED_TOKENS) {
             throw ExceptionsHelper.badRequestException(
                 "[{}] must be greater than 0 and less than [{}]. Found [{}] in [{}]",
                 MAX_MATCHED_TOKENS.getPreferredName(),

072470fbf3434fd96dc3eebd226007a2983e935d
Ioannis Kakavas <ioannis@elastic.co>
2021-10-21 13:28:27 +0300

    adjst test

--- qa/os/src/test/java/org/elasticsearch/packaging/test/EnrollmentProcessTests.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/test/EnrollmentProcessTests.java
@@ -14,9 +14,6 @@ import org.elasticsearch.packaging.util.Distribution;
 import org.elasticsearch.packaging.util.Shell;
 import org.junit.BeforeClass;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static org.elasticsearch.packaging.util.Archives.installArchive;
 import static org.elasticsearch.packaging.util.Archives.verifyArchiveInstallation;
 import static org.elasticsearch.packaging.util.FileUtils.getCurrentVersion;
@@ -31,20 +28,17 @@ public class EnrollmentProcessTests extends PackagingTestCase {
         assumeTrue("only archives", distribution.isArchive());
     }
 
-    private static final Pattern PASSWORD_REGEX = Pattern.compile("Password for the elastic user is: (.+)$", Pattern.MULTILINE);
-
     public void test10AutoFormCluster() throws Exception {
         /* Windows issue awaits fix: https://github.com/elastic/elasticsearch/issues/49340 */
         assumeTrue("expect command isn't on Windows", distribution.platform != Distribution.Platform.WINDOWS);
         installation = installArchive(sh, distribution(), getRootTempDir().resolve("elasticsearch-node1"), getCurrentVersion(), true);
         verifyArchiveInstallation(installation, distribution());
+        setFileSuperuser("test_superuser", "test_superuser_password");
         sh.getEnv().put("ES_JAVA_OPTS", "-Xms1g -Xmx1g");
         Shell.Result startFirstNode = awaitElasticsearchStartupWithResult(
             Archives.startElasticsearchWithTty(installation, sh, null, false)
         );
-        // Capture auto-generated password of the elastic user from the node startup output
-        final String elasticPassword = parseElasticPassword(startFirstNode.stdout);
-        assertNotNull(elasticPassword);
+        assertThat(startFirstNode.isSuccess(), is(true));
         // Verify that the first node was auto-configured for security
         verifySecurityAutoConfigured(installation);
         // Generate a node enrollment token to be subsequently used by the second node
@@ -55,26 +49,13 @@ public class EnrollmentProcessTests extends PackagingTestCase {
         installation = installArchive(sh, distribution(), getRootTempDir().resolve("elasticsearch-node2"), getCurrentVersion(), true);
         // auto-configure security using the enrollment token
         installation.executables().enrollToExistingCluster.run("--enrollment-token " + enrollmentToken);
+        // Verify that the second node was also configured (via enrollment) for security
         verifySecurityAutoConfigured(installation);
         Shell.Result startSecondNode = awaitElasticsearchStartupWithResult(
             Archives.startElasticsearchWithTty(installation, sh, null, false)
         );
-        assertThat(startSecondNode.exitCode, is(0));
-        assertNull(parseElasticPassword(startSecondNode.stdout));
+        assertThat(startSecondNode.isSuccess(), is(true));
         // verify that the two nodes formed a cluster
-        assertThat(
-            makeRequestAsElastic("https://localhost:9200/_cluster/health", elasticPassword),
-            containsString("\"number_of_nodes\":2")
-        );
-    }
-
-    private String parseElasticPassword(String output) {
-        Matcher matcher = PASSWORD_REGEX.matcher(output);
-        assertNotNull(matcher);
-        if (matcher.find()) {
-            return matcher.group(1);
-        } else {
-            return null;
-        }
+        assertThat(makeRequest("https://localhost:9200/_cluster/health"), containsString("\"number_of_nodes\":2"));
     }
 }

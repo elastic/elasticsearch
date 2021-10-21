808b70d2f906892e0b27fe62367723b70a48c8ae
Stuart Tettemer <stu@elastic.co>
2021-10-21 07:57:27 -0500

    Script: Restore the scripting general cache (#79453)
    Deprecate the script context cache in favor of the general cache.
    
    Users should use the following settings:
    `script.max_compilations_rate` to set the max compilation rate
      for user scripts such as filter scripts.  Certain script contexts
      that submit scripts outside of the control of the user are
      exempted from this rate limit.  Examples include runtime fields,
      ingest and watcher.
    
    `script.cache.max_size` to set the max size of the cache.
    
    `script.cache.expire` to set the expiration time for entries in
    the cache.
    
    Whats deprecated?
    `script.max_compilations_rate: use-context`.  This special
    setting value was used to turn on the script context-specific caches.
    
    `script.context.$CONTEXT.cache_max_size`, use `script.cache.max_size`
    instead.
    
    `script.context.$CONTEXT.cache_expire`, use `script.cache.expire`
    instead.
    
    `script.context.$CONTEXT.max_compilations_rate`, use
    `script.max_compilations_rate` instead.
    
    The default cache size was increased from `100` to `3000`, which
    was approximately the max cache size when using context-specific caches.
    
    The default compilation rate limit was increased from `75/5m` to
    `150/5m` to account for increasing uses of scripts.
    
    System script contexts can now opt-out of compilation rate limiting
    using a flag rather than a sentinel rate limit value.
    
    7.16: Script: Deprecate script context cache #79508
    Refs: #62899
    
    7.16: Script: Opt-out system contexts from script compilation rate limit #79459
    Refs: #62899

--- x-pack/plugin/watcher/src/main/java/org/elasticsearch/xpack/watcher/transform/script/WatcherTransformScript.java
+++ x-pack/plugin/watcher/src/main/java/org/elasticsearch/xpack/watcher/transform/script/WatcherTransformScript.java
@@ -7,7 +7,6 @@
 package org.elasticsearch.xpack.watcher.transform.script;
 
 import org.elasticsearch.core.TimeValue;
-import org.elasticsearch.script.ScriptCache;
 import org.elasticsearch.script.ScriptContext;
 import org.elasticsearch.xpack.core.watcher.execution.WatchExecutionContext;
 import org.elasticsearch.xpack.core.watcher.watch.Payload;
@@ -46,5 +45,5 @@ public abstract class WatcherTransformScript {
     }
 
     public static ScriptContext<Factory> CONTEXT = new ScriptContext<>("watcher_transform", Factory.class,
-        200, TimeValue.timeValueMillis(0), ScriptCache.UNLIMITED_COMPILATION_RATE.asTuple(), true);
+        200, TimeValue.timeValueMillis(0), false, true);
 }

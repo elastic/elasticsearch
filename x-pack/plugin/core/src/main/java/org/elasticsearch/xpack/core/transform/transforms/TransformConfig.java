9a3bea368cf3db588bc1a22b5a62875c25debd0d
lcawl <lcawley@elastic.co>
2021-10-21 10:25:18 -0700

    Revert messages changes accomplished in #79601

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/transform/transforms/TransformConfig.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/transform/transforms/TransformConfig.java
@@ -399,7 +399,7 @@ public class TransformConfig extends AbstractDiffable<TransformConfig> implement
                     Level.CRITICAL,
                     "Transform [" + id + "] is too old",
                     TransformDeprecations.BREAKING_CHANGES_BASE_URL,
-                    "Use the upgrade transforms API to fix your transforms.",
+                    "The configuration uses an old format, you can use [_update] or [_upgrade] to update",
                     false,
                     null
                 )

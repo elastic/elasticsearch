0db55c1e284f39ef4c504f6a93f954da30fef001
Ioannis Kakavas <ioannis@elastic.co>
2021-10-21 11:46:54 +0300

    unused import

--- x-pack/plugin/core/src/test/java/org/elasticsearch/license/LicenseTLSTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/license/LicenseTLSTests.java
@@ -19,7 +19,6 @@ import java.net.InetAddress;
 
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.emptySet;
-import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;

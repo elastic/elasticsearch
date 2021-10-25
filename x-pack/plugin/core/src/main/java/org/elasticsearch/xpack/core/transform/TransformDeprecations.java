c35c14f0272de365c50c5e576372e5fb8ea4cc4c
Hendrik Muhs <hendrik.muhs@elastic.co>
2021-10-25 11:01:33 +0200

    [Transform] Improve transform upgrader (#79601)
    This changes fixes some issues with the upgrader. It inlines the index name resolver as wildcard
    deletes are not supported if action.destructive_requires_name is set to true(default). It changes
    response to always return all counters in order to avoid an empty response. After documentation has
    been created, it switches the deprecation URL to the upgrade API docs.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/transform/TransformDeprecations.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/transform/TransformDeprecations.java
@@ -8,12 +8,21 @@
 package org.elasticsearch.xpack.core.transform;
 
 public class TransformDeprecations {
+
+    public static final String UPGRADE_TRANSFORM_URL = "https://ela.st/es-8-upgrade-transforms";
+
+    // breaking changes base url for the _next_ major release
     public static final String BREAKING_CHANGES_BASE_URL =
         "https://www.elastic.co/guide/en/elasticsearch/reference/master/migrating-8.0.html";
 
     public static final String QUERY_BREAKING_CHANGES_URL = BREAKING_CHANGES_BASE_URL + "#breaking_80_search_changes";
 
     public static final String AGGS_BREAKING_CHANGES_URL = BREAKING_CHANGES_BASE_URL + "#breaking_80_aggregations_changes";
+
+    public static final String ACTION_UPGRADE_TRANSFORMS_API = "Use the upgrade transforms API to fix your transforms.";
+
+    public static final String ACTION_MAX_PAGE_SEARCH_SIZE_IS_DEPRECATED =
+        "[max_page_search_size] is deprecated inside pivot. Use settings instead.";
 
     private TransformDeprecations() {}
 }

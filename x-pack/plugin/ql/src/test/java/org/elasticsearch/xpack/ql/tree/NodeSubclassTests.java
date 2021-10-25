8f97d85114be5074d09fe17b3095354aa0ea2949
Ryan Ernst <ryan@iernst.net>
2021-10-25 06:56:40 -0700

    Add jdk internal package for mocking
    When mocking some jdk classes, the jdk.internal package must be
    accessible to mockito. This commit adds the needed permission so classes
    like ZoneId can be mocked.
    
    closes #79704

--- x-pack/plugin/ql/src/test/java/org/elasticsearch/xpack/ql/tree/NodeSubclassTests.java
+++ x-pack/plugin/ql/src/test/java/org/elasticsearch/xpack/ql/tree/NodeSubclassTests.java
@@ -89,7 +89,6 @@ import static org.mockito.Mockito.mock;
  * node of that type is called for.
  * </ul>
  */
-@AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/79704")
 public class NodeSubclassTests<T extends B, B extends Node<B>> extends ESTestCase {
 
     private static final List<Class<?>> CLASSES_WITH_MIN_TWO_CHILDREN = asList(In.class, InPipe.class);

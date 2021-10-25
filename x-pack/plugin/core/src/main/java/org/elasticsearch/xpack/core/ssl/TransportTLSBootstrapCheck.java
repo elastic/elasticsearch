ac77f7daa082d48472d64e0970c27530a8fae4c7
Ioannis Kakavas <ikakavas@protonmail.com>
2021-10-25 09:49:53 +0300

    Assert security is enabled
    Co-authored-by: Tim Vernum <tim@adjective.org>

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ssl/TransportTLSBootstrapCheck.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ssl/TransportTLSBootstrapCheck.java
@@ -16,6 +16,8 @@ import org.elasticsearch.xpack.core.XPackSettings;
 public final class TransportTLSBootstrapCheck implements BootstrapCheck {
     @Override
     public BootstrapCheckResult check(BootstrapContext context) {
+        assert XPackSettings.SECURITY_ENABLED.get(context.settings()) 
+            : "Bootstrap check should not be installed unless security is enabled";
         if (XPackSettings.TRANSPORT_SSL_ENABLED.get(context.settings()) == false) {
             return BootstrapCheckResult.failure(
                 "Transport SSL must be enabled if security is enabled. "

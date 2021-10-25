3fd7c6cc389d8b88ebd410186909ae7dc8b8f837
Rene Groeschke <rene@elastic.co>
2021-10-25 10:42:21 +0200

    Apply notice to some licenses
    - Applies review feedback
    - some polishing on multiple licenses per file

--- server/src/main/java/org/elasticsearch/http/CorsHandler.java
+++ server/src/main/java/org/elasticsearch/http/CorsHandler.java
@@ -1,9 +1,5 @@
 /*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
+ * @notice
  *
  * Copyright 2013 The Netty Project
  *
@@ -18,8 +14,16 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
+ *
+ * =============================================================================
+ * Modifications copyright Elasticsearch B.V.
+ *
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
-
 package org.elasticsearch.http;
 
 import org.elasticsearch.common.Strings;

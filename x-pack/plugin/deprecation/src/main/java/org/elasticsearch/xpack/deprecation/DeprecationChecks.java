9d27c74e99dc9e27e9f6799c6d1eb9224e1e5a95
Joe Gallo <joe.gallo@elastic.co>
2021-10-26 11:09:14 -0400

    Always enforce default tier preference (ENFORCE_DEFAULT_TIER_PREFERENCE is ignored) (#79751)

--- x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/DeprecationChecks.java
+++ x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/DeprecationChecks.java
@@ -83,7 +83,8 @@ public class DeprecationChecks {
         NodeDeprecationChecks::checkScriptContextCache,
         NodeDeprecationChecks::checkScriptContextCompilationsRateLimitSetting,
         NodeDeprecationChecks::checkScriptContextCacheSizeSetting,
-        NodeDeprecationChecks::checkScriptContextCacheExpirationSetting
+        NodeDeprecationChecks::checkScriptContextCacheExpirationSetting,
+        NodeDeprecationChecks::checkEnforceDefaultTierPreferenceSetting
     );
 
     static List<Function<IndexMetadata, DeprecationIssue>> INDEX_SETTINGS_CHECKS = List.of(

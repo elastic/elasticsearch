51fdcdf16009f132a0f0b0cbb53c47bf9846886d
weizijun <weizijun1989@gmail.com>
2021-10-29 16:26:11 +0800

    add benchmark to test xcontent parser filter performance

---
+++ benchmarks/src/main/java/org/elasticsearch/benchmark/xcontent/FilterContentBenchmark.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.benchmark.xcontent;
+
+import org.elasticsearch.common.bytes.BytesArray;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.io.Streams;
+import org.elasticsearch.common.io.stream.BytesStreamOutput;
+import org.elasticsearch.common.util.Maps;
+import org.elasticsearch.common.xcontent.XContentHelper;
+import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xcontent.XContentParser;
+import org.elasticsearch.xcontent.XContentParserConfiguration;
+import org.elasticsearch.xcontent.XContentType;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@Fork(1)
+@Warmup(iterations = 2)
+@Measurement(iterations = 3)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class FilterContentBenchmark {
+
+    @Param({ "cluster_stats", "index_stats", "node_stats" })
+    private String type;
+
+    @Param({ "10_field", "half_field", "all_field", "wildcard_field", "10_wildcard_field" })
+    private String fieldCount;
+
+    @Param({ "true", "false" })
+    private boolean inclusive;
+
+    private BytesReference source;
+    private XContentParserConfiguration parserConfig;
+
+    @Setup
+    public void setup() throws IOException {
+        String sourceFile;
+        switch (type) {
+            case "cluster_stats":
+                sourceFile = "monitor_cluster_stats.json";
+                break;
+            case "index_stats":
+                sourceFile = "monitor_index_stats.json";
+                break;
+            case "node_stats":
+                sourceFile = "monitor_node_stats.json";
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown type [" + type + "]");
+        }
+        source = readSource(sourceFile);
+
+        Map<String, Object> flattenMap = Maps.flatten(XContentHelper.convertToMap(source, true, XContentType.JSON).v2(), false, true);
+        Set<String> keys = flattenMap.keySet();
+        Set<String> filters;
+        switch (fieldCount) {
+            case "10_field": {
+                AtomicInteger count = new AtomicInteger();
+                Set<String> filterKeys = keys.stream()
+                    .filter(key -> count.getAndIncrement() % 5 == 0)
+                    .limit(10)
+                    .collect(Collectors.toSet());
+                filters = filterKeys;
+            }
+                break;
+            case "half_field": {
+                AtomicInteger count = new AtomicInteger();
+                Set<String> halfKeys = keys.stream().filter(key -> count.getAndIncrement() % 2 == 0).collect(Collectors.toSet());
+                filters = halfKeys;
+            }
+                break;
+            case "all_field": {
+                Set<String> allKeys = new HashSet<>(keys);
+                allKeys.remove("cluster_uuid");
+                filters = allKeys;
+            }
+                break;
+            case "wildcard_field": {
+                Set<String> wildcardField = new HashSet<>(Arrays.asList("*stats"));
+                filters = wildcardField;
+            }
+                break;
+            case "10_wildcard_field": {
+                filters = new HashSet<>(
+                    Arrays.asList(
+                        "*stats.nodes*",
+                        "*stats.ind*",
+                        "*sta*.shards",
+                        "*stats*.xpack",
+                        "*stats.*.segments",
+                        "*stat*.*.data*",
+                        inclusive ? "*stats.**.request_cache" : "*stats.*.request_cache",
+                        inclusive ? "*stats.**.stat" : "*stats.*.stat",
+                        inclusive ? "*stats.**.threads" : "*stats.*.threads",
+                        "*source_node.t*"
+                    )
+                );
+            }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown type [" + type + "]");
+        }
+
+        Set<String> includes;
+        Set<String> excludes;
+        if (inclusive) {
+            includes = filters;
+            excludes = null;
+        } else {
+            includes = null;
+            excludes = filters;
+        }
+        parserConfig = XContentParserConfiguration.EMPTY.withFiltering(includes, excludes);
+    }
+
+    @Benchmark
+    public BytesReference benchmark() throws IOException {
+        try (BytesStreamOutput os = new BytesStreamOutput()) {
+            XContentBuilder builder = new XContentBuilder(XContentType.JSON.xContent(), os);
+            try (XContentParser parser = XContentType.JSON.xContent().createParser(parserConfig, source.streamInput())) {
+                builder.copyCurrentStructure(parser);
+                return BytesReference.bytes(builder);
+            } catch (Exception e) {
+                return new BytesArray("");
+            }
+        }
+    }
+
+    private BytesReference readSource(String fileName) throws IOException {
+        return Streams.readFully(FilterContentBenchmark.class.getResourceAsStream(fileName));
+    }
+}

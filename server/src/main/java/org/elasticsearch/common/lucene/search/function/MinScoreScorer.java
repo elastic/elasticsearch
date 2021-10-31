aaedb0d932696bce574813ee9be4bbd64b4946ce
Nhat Nguyen <nhat.nguyen@elastic.co>
2021-10-30 22:16:00 -0400

    Prevent approximation of MinScoreScorer from unwrapping as TwoPhaseIterator

--- server/src/main/java/org/elasticsearch/common/lucene/search/function/MinScoreScorer.java
+++ server/src/main/java/org/elasticsearch/common/lucene/search/function/MinScoreScorer.java
@@ -59,10 +59,45 @@ final class MinScoreScorer extends Scorer {
         return TwoPhaseIterator.asDocIdSetIterator(twoPhaseIterator());
     }
 
+    private static class DocIdSetIteratorWrapper extends DocIdSetIterator {
+        private final DocIdSetIterator disi;
+
+        DocIdSetIteratorWrapper(DocIdSetIterator disi) {
+            this.disi = disi;
+        }
+
+        @Override
+        public int docID() {
+            return disi.docID();
+        }
+
+        @Override
+        public int nextDoc() throws IOException {
+            return disi.nextDoc();
+        }
+
+        @Override
+        public int advance(int target) throws IOException {
+            return disi.advance(target);
+        }
+
+        @Override
+        public long cost() {
+            return disi.cost();
+        }
+    }
+
     @Override
     public TwoPhaseIterator twoPhaseIterator() {
         final TwoPhaseIterator inTwoPhase = this.in.twoPhaseIterator();
-        final DocIdSetIterator approximation = inTwoPhase == null ? in.iterator() : inTwoPhase.approximation();
+        DocIdSetIterator approximation = inTwoPhase != null ? inTwoPhase.approximation() : in.iterator();
+        // If the approximation is a wrapper of TwoPhaseIterator, then ConjunctionScorer can add it the list of TwoPhaseIterator
+        // after the TwoPhaseIterator of MinScoreScorer. Then, the `matches()` method of the approximation can be called twice:
+        // one before `score()` and one after `score()`. This doesn't work well with ToParentBlockJoinQuery with which `matches()`
+        // can't be called after `score()`. Here we wrap the approximation to prevent it from unwrapping as a TwoPhaseIterator.
+        if (inTwoPhase == null && TwoPhaseIterator.unwrap(approximation) != null) {
+            approximation = new DocIdSetIteratorWrapper(approximation);
+        }
         return new TwoPhaseIterator(approximation) {
 
             @Override

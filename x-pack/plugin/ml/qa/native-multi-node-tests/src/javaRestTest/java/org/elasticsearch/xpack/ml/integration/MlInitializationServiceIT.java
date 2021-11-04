5a8cf3303aea50f870ec4782b3c96e35b0893199
Przemysław Witek <przemyslaw.witek@elastic.co>
2021-11-04 08:15:02 +0100

    [ML] Implement test for making ML internal aliases hidden

--- x-pack/plugin/ml/qa/native-multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/ml/integration/MlInitializationServiceIT.java
+++ x-pack/plugin/ml/qa/native-multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/ml/integration/MlInitializationServiceIT.java
@@ -114,6 +114,76 @@ public class MlInitializationServiceIT extends MlNativeAutodetectIntegTestCase {
         }
     }
 
+    public void testThatMlAliasesBecomeHiddenWhenTheNodeBecomesMaster() throws Exception {
+        String[] mlHiddenIndexNames = {
+            ".ml-anomalies-7",
+            ".ml-state-000001",
+            ".ml-stats-000001",
+            ".ml-notifications-000002",
+            ".ml-annotations-6" };
+        String[] otherIndexNames = { "some-index-1", "some-other-index-2" };
+        String[] allIndexNames = Stream.concat(Arrays.stream(mlHiddenIndexNames), Arrays.stream(otherIndexNames)).toArray(String[]::new);
+
+
+
+        for (String indexName : mlHiddenIndexNames) {
+            try {
+                assertAcked(prepareCreate(indexName).setSettings(Collections.singletonMap(SETTING_INDEX_HIDDEN, randomBoolean())));
+            } catch (ResourceAlreadyExistsException e) {
+                logger.info("Index " + indexName + "already exists: {}", e.getDetailedMessage());
+            }
+        }
+        createIndex(otherIndexNames);
+
+        GetSettingsResponse settingsResponse = client().admin()
+            .indices()
+            .prepareGetSettings(allIndexNames)
+            .setIndicesOptions(IndicesOptions.LENIENT_EXPAND_OPEN_CLOSED_HIDDEN)
+            .get();
+        assertThat(settingsResponse, is(notNullValue()));
+        for (String indexName : mlHiddenIndexNames) {
+            Settings settings = settingsResponse.getIndexToSettings().get(indexName);
+            assertThat(settings, is(notNullValue()));
+        }
+        for (String indexName : otherIndexNames) {
+            Settings settings = settingsResponse.getIndexToSettings().get(indexName);
+            assertThat(settings, is(notNullValue()));
+            assertThat(
+                "Index " + indexName + " expected not to be hidden but was",
+                settings.getAsBoolean(SETTING_INDEX_HIDDEN, false),
+                is(equalTo(false))
+            );
+        }
+
+        mlInitializationService.onMaster();
+        assertBusy(() -> assertTrue(mlInitializationService.areMlInternalIndicesHidden()));
+
+        settingsResponse = client().admin()
+            .indices()
+            .prepareGetSettings(allIndexNames)
+            .setIndicesOptions(IndicesOptions.LENIENT_EXPAND_OPEN_CLOSED_HIDDEN)
+            .get();
+        assertThat(settingsResponse, is(notNullValue()));
+        for (String indexName : mlHiddenIndexNames) {
+            Settings settings = settingsResponse.getIndexToSettings().get(indexName);
+            assertThat(settings, is(notNullValue()));
+            assertThat(
+                "Index " + indexName + " expected to be hidden but wasn't, settings = " + settings,
+                settings.getAsBoolean(SETTING_INDEX_HIDDEN, false),
+                is(equalTo(true))
+            );
+        }
+        for (String indexName : otherIndexNames) {
+            Settings settings = settingsResponse.getIndexToSettings().get(indexName);
+            assertThat(settings, is(notNullValue()));
+            assertThat(
+                "Index " + indexName + " expected not to be hidden but was, settings = " + settings,
+                settings.getAsBoolean(SETTING_INDEX_HIDDEN, false),
+                is(equalTo(false))
+            );
+        }
+    }
+
     @Override
     public Settings indexSettings() {
         return Settings.builder().put(super.indexSettings()).put(IndexMetadata.SETTING_DATA_PATH, (String) null).build();

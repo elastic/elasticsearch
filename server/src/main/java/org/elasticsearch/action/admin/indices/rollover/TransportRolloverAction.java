5745b8ea6e7390d7d0b48be59b4a9b77d4a7ca8b
Martijn van Groningen <martijn.v.groningen@gmail.com>
2021-11-05 10:47:42 +0100

    removed unused import

--- server/src/main/java/org/elasticsearch/action/admin/indices/rollover/TransportRolloverAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/rollover/TransportRolloverAction.java
@@ -49,7 +49,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 /**

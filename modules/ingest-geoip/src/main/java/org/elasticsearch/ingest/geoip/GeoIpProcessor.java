e4271b684505f5482ef6caaadf5b66234b284128
Keith Massey <keith.massey@elastic.co>
2021-11-08 11:43:46 -0600

    Changing HeaderWarning to always use 299 as the warning code (#80304) (#80503)
    * Changing HeaderWarning to always use 299 as the warning code (#80304)
    
    This commit changes the deprecation logger so that all messages (critical or warning) are written
    out with "299" as the level at the beginning of the header in order to be compliant with
    https://www.rfc-editor.org/rfc/rfc7234.html#section-5.5.7. In #79107 we mistakenly began logging
    warning-level messages with the 300 code, which is not valid in the RFC.
    
    * fixing compilation errors from merge

--- modules/ingest-geoip/src/main/java/org/elasticsearch/ingest/geoip/GeoIpProcessor.java
+++ modules/ingest-geoip/src/main/java/org/elasticsearch/ingest/geoip/GeoIpProcessor.java
@@ -23,7 +23,6 @@ import org.elasticsearch.ResourceNotFoundException;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.CheckedSupplier;
-import org.elasticsearch.common.logging.DeprecationLogger;
 import org.elasticsearch.common.logging.HeaderWarning;
 import org.elasticsearch.common.network.InetAddresses;
 import org.elasticsearch.common.network.NetworkAddress;
@@ -464,7 +463,6 @@ public final class GeoIpProcessor extends AbstractProcessor {
                 boolean valid = metadata.isValid(currentState.metadata().settings());
                 if (valid && metadata.isCloseToExpiration()) {
                     HeaderWarning.addWarning(
-                        DeprecationLogger.CRITICAL,
                         "database [{}] was not updated for over 25 days, geoip processor"
                             + " will stop working if there is no update for 30 days",
                         databaseFile

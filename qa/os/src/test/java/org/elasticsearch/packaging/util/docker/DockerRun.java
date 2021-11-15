0724109ad26daa642afcac3b7a1dc877b28dde98
Rory Hunter <pugnascotia@users.noreply.github.com>
2021-11-15 18:53:03 +0000

    Introduce declarative plugin management (#77544) (#80734)
    Closes #70219.
    
    Introduce a declarative way for the Elasticsearch server to manage plugins,
    which reads the `elasticsearch-plugins.yml` file and works which out
    plugins need to be added and / or removed to match the configuration. Also
    make it possible to configure a proxy in the config file, instead of
    through the environment.
    
    Most of the work of adding and removing is still done in the
    `InstallPluginAction` and `RemovePluginAction` classes, so the
    behaviour should be the same as with the `install` and `remove`
    commands. However, these commands will now abort if the above config
    file exists. The intent is to make it harder for the configuration
    to drift.
    
    This new method only applies to `docker` distribution types at the
    moment.
    
    Since this syncing mechanism declarative, rather than imperative,
    the Cloud-specific plugin wrapper script is no longer required.
    Instead, an environment variable informs `InstallPluginAction` to
    install plugins from an archive directory instead of downloading
    them, where possible.

--- qa/os/src/test/java/org/elasticsearch/packaging/util/docker/DockerRun.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/util/docker/DockerRun.java
@@ -11,6 +11,7 @@ package org.elasticsearch.packaging.util.docker;
 import org.elasticsearch.packaging.util.Distribution;
 import org.elasticsearch.packaging.util.Platforms;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -54,6 +55,10 @@ public class DockerRun {
     }
 
     public DockerRun volume(Path from, String to) {
+        requireNonNull(from);
+        if (Files.exists(from) == false) {
+            throw new RuntimeException("Path [" + from + "] does not exist");
+        }
         this.volumes.put(requireNonNull(from), Paths.get(requireNonNull(to)));
         return this;
     }

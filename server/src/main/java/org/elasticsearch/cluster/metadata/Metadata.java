812d6f0b0bb02cb85306c7212c3bb8b281ce0a49
Joe Gallo <joegallo@gmail.com>
2021-11-16 15:42:23 -0500

    Rewrite getIndicesLookup slightly
    and move the unmodifiable wrapping into buildIndicesLookup itself

--- server/src/main/java/org/elasticsearch/cluster/metadata/Metadata.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/Metadata.java
@@ -354,11 +354,10 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata>, To
     }
 
     public SortedMap<String, IndexAbstraction> getIndicesLookup() {
-        if (indicesLookup != null) {
-            return indicesLookup;
+        if (indicesLookup == null) {
+            DataStreamMetadata dataStreamMetadata = custom(DataStreamMetadata.TYPE);
+            indicesLookup = Builder.buildIndicesLookup(dataStreamMetadata, indices);
         }
-        DataStreamMetadata dataStreamMetadata = custom(DataStreamMetadata.TYPE);
-        indicesLookup = Collections.unmodifiableSortedMap(Builder.buildIndicesLookup(dataStreamMetadata, indices));
         return indicesLookup;
     }
 
@@ -1632,7 +1631,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata>, To
                 indicesLookup = previousIndicesLookup;
             } else {
                 if (builtIndicesLookupEagerly) {
-                    indicesLookup = Collections.unmodifiableSortedMap(buildIndicesLookup(dataStreamMetadata, indices));
+                    indicesLookup = buildIndicesLookup(dataStreamMetadata, indices);
                 } else {
                     indicesLookup = null;
                 }
@@ -1757,7 +1756,7 @@ public class Metadata implements Iterable<IndexMetadata>, Diffable<Metadata>, To
             }
 
             validateDataStreams(indicesLookup, dataStreamMetadata);
-            return indicesLookup;
+            return Collections.unmodifiableSortedMap(indicesLookup);
         }
 
         static void validateDataStreams(SortedMap<String, IndexAbstraction> indicesLookup, @Nullable DataStreamMetadata dsMetadata) {

f2c6fec58cb9513c8864c590bacff72f43db041d
Nikola Grcevski <nikola.grcevski@elastic.co>
2021-11-18 14:32:02 -0500

    Remove unused import.

--- server/src/main/java/org/elasticsearch/monitor/jvm/OldObjectsSampler.java
+++ server/src/main/java/org/elasticsearch/monitor/jvm/OldObjectsSampler.java
@@ -19,7 +19,6 @@ import org.apache.logging.log4j.Logger;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;

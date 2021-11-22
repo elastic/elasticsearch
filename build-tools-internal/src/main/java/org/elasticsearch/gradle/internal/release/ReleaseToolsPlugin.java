2553be7355915d02bbaa2a5882b063493650a51e
Rory Hunter <roryhunter2@gmail.com>
2021-11-22 16:33:03 +0000

    Rework breaking changes for new structure
    The structure of the breaking changes / migration guide was updated in
    different files. This PR amends the changelog generator code in line
    with this rework.

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/release/ReleaseToolsPlugin.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/release/ReleaseToolsPlugin.java
@@ -84,9 +84,15 @@ public class ReleaseToolsPlugin implements Plugin<Project> {
             task.setReleaseHighlightsFile(projectDirectory.file("docs/reference/release-notes/highlights.asciidoc"));
 
             task.setBreakingChangesTemplate(projectDirectory.file(RESOURCES + "templates/breaking-changes.asciidoc"));
-            task.setBreakingChangesFile(
+            task.setBreakingChangesIndexFile(
                 projectDirectory.file(
                     String.format("docs/reference/migration/migrate_%d_%d.asciidoc", version.getMajor(), version.getMinor())
+                )
+            );
+            task.setBreakingChangesAreaTemplate(projectDirectory.file(RESOURCES + "templates/breaking-changes-area.asciidoc"));
+            task.setBreakingChangesDirectory(
+                projectDirectory.dir(
+                    String.format("docs/reference/migration/migrate_%d_%d", version.getMajor(), version.getMinor())
                 )
             );
 

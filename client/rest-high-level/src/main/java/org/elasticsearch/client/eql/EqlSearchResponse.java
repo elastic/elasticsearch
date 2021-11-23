d0b04643a66bdad61ca504a2a1a8beaa4533d593
Andrei Stefan <andrei@elastic.co>
2021-11-23 11:45:19 +0200

    HLRC documentation for EQL

--- client/rest-high-level/src/main/java/org/elasticsearch/client/eql/EqlSearchResponse.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/eql/EqlSearchResponse.java
@@ -10,6 +10,7 @@ package org.elasticsearch.client.eql;
 
 import org.apache.lucene.search.TotalHits;
 import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.document.DocumentField;
 import org.elasticsearch.common.xcontent.XContentParserUtils;
 import org.elasticsearch.core.Nullable;
 import org.elasticsearch.index.get.GetResult;
@@ -25,6 +26,7 @@ import org.elasticsearch.xcontent.XContentParser;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -144,16 +146,19 @@ public class EqlSearchResponse {
             static final String INDEX = GetResult._INDEX;
             static final String ID = GetResult._ID;
             static final String SOURCE = SourceFieldMapper.NAME;
+            static final String FIELDS = "fields";
         }
 
         private static final ParseField INDEX = new ParseField(Fields.INDEX);
         private static final ParseField ID = new ParseField(Fields.ID);
         private static final ParseField SOURCE = new ParseField(Fields.SOURCE);
+        private static final ParseField FIELDS = new ParseField(Fields.FIELDS);
 
+        @SuppressWarnings("unchecked")
         private static final ConstructingObjectParser<Event, Void> PARSER = new ConstructingObjectParser<>(
             "eql/search_response_event",
             true,
-            args -> new Event((String) args[0], (String) args[1], (BytesReference) args[2])
+            args -> new Event((String) args[0], (String) args[1], (BytesReference) args[2], (Map<String, DocumentField>) args[3])
         );
 
         static {
@@ -165,17 +170,27 @@ public class EqlSearchResponse {
                     return BytesReference.bytes(builder);
                 }
             }, SOURCE);
+            PARSER.declareObject(optionalConstructorArg(), (p, c) -> {
+                Map<String, DocumentField> fields = new HashMap<>();
+                while (p.nextToken() != XContentParser.Token.END_OBJECT) {
+                    DocumentField field = DocumentField.fromXContent(p);
+                    fields.put(field.getName(), field);
+                }
+                return fields;
+            }, FIELDS);
         }
 
         private final String index;
         private final String id;
         private final BytesReference source;
         private Map<String, Object> sourceAsMap;
+        private final Map<String, DocumentField> fetchFields;
 
-        public Event(String index, String id, BytesReference source) {
+        public Event(String index, String id, BytesReference source, Map<String, DocumentField> fetchFields) {
             this.index = index;
             this.id = id;
             this.source = source;
+            this.fetchFields = fetchFields;
         }
 
         public static Event fromXContent(XContentParser parser) throws IOException {
@@ -194,6 +209,10 @@ public class EqlSearchResponse {
             return source;
         }
 
+        public Map<String, DocumentField> fetchFields() {
+            return fetchFields;
+        }
+
         public Map<String, Object> sourceAsMap() {
             if (source == null) {
                 return null;
@@ -208,7 +227,7 @@ public class EqlSearchResponse {
 
         @Override
         public int hashCode() {
-            return Objects.hash(index, id, source);
+            return Objects.hash(index, id, source, fetchFields);
         }
 
         @Override
@@ -222,7 +241,10 @@ public class EqlSearchResponse {
             }
 
             EqlSearchResponse.Event other = (EqlSearchResponse.Event) obj;
-            return Objects.equals(index, other.index) && Objects.equals(id, other.id) && Objects.equals(source, other.source);
+            return Objects.equals(index, other.index)
+                && Objects.equals(id, other.id)
+                && Objects.equals(source, other.source)
+                && Objects.equals(fetchFields, other.fetchFields);
         }
     }
 

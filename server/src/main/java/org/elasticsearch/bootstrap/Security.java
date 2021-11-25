d8e1425bef1f092ddd9ba449eaaf75a4dcc17fd8
ChrisHegarty <christopher.hegarty@elastic.co>
2021-11-25 16:58:54 +0000

    Support codesource URLs for modules
    Fixes issues like this:  uncaught exception in thread [main]
    java.lang.IllegalArgumentException: Unknown codebases [codebase.jna, codebase.lucene-core, codebase.elasticsearch-plugin-classloader, codebase.elasticsearch, codebase.elasticsearch-secure-sm, codebase.lucene-misc] in policy file [jar:file:///Users/chegar/git/es_modules/distribution/archives/darwin-aarch64-tar/build/install/elasticsearch-8.1.0-SNAPSHOT/lib/elasticsearch-8.1.0-SNAPSHOT.jar!/org/elasticsearch/bootstrap/security.policy]

--- server/src/main/java/org/elasticsearch/bootstrap/Security.java
+++ server/src/main/java/org/elasticsearch/bootstrap/Security.java
@@ -24,8 +24,12 @@ import java.io.IOException;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
+import java.lang.module.ModuleReference;
+import java.lang.module.ResolvedModule;
 import java.lang.reflect.Field;
+import java.net.MalformedURLException;
 import java.net.SocketPermission;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.AccessMode;
@@ -40,8 +44,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.lang.invoke.MethodType.methodType;
 import static org.elasticsearch.bootstrap.FilePermissionUtils.addDirectoryPath;
@@ -102,6 +109,26 @@ final class Security {
         System.setSecurityManager(sm);
     }
 
+    private static URL toURL(URI uri) {
+        try {
+            return uri.toURL();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Stream<URL> parseModulePath() {
+        return ModuleLayer.boot()
+            .configuration()
+            .modules()
+            .stream()
+            .map(ResolvedModule::reference)
+            .map(ModuleReference::location)
+            .flatMap(Optional::stream)
+            .map(Security::toURL)
+            .filter(url -> url.getProtocol().equals("file"));
+    }
+
     /**
      * Initializes SecurityManager for the environment
      * Can only happen once!
@@ -111,7 +138,10 @@ final class Security {
     static void configure(Environment environment, boolean filterBadDefaults) throws IOException, NoSuchAlgorithmException {
 
         // enable security policy: union of template and environment-based paths, and possibly plugin permissions
-        Map<String, URL> codebases = PolicyUtil.getCodebaseJarMap(JarHell.parseClassPath());
+        Set<URL> cp = JarHell.parseClassPath();
+        Stream<URL> mp = parseModulePath();
+        Set<URL> all = Stream.concat(cp.stream(), mp).collect(Collectors.toSet());
+        Map<String, URL> codebases = PolicyUtil.getCodebaseJarMap(all);
         Policy.setPolicy(
             new ESPolicy(
                 codebases,

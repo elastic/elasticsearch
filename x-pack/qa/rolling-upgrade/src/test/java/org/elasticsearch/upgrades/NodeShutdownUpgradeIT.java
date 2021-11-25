d3c0e0dc3a57e13ecd75dc010b2314cebaf90539
pgomulka <przemyslaw.gomulka@elastic.co>
2021-11-25 17:24:45 +0100

    rolling test

--- x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/NodeShutdownUpgradeIT.java
+++ x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/NodeShutdownUpgradeIT.java
@@ -1,13 +1,11 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
 
 package org.elasticsearch.upgrades;
-
 
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
@@ -21,13 +19,13 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class NodeShutdownUpgradeIT extends AbstractUpgradeTestCase {
     List<String> namesSorted;
@@ -39,22 +37,23 @@ public class NodeShutdownUpgradeIT extends AbstractUpgradeTestCase {
         final Request getNodesReq = new Request("GET", "_nodes");
         final Response getNodesResp = adminClient().performRequest(getNodesReq);
         final Map<String, Map<String, Object>> nodes = (Map<String, Map<String, Object>>) entityAsMap(getNodesResp).get("nodes");
-        nodeNameToIdMap = nodes.entrySet().stream()
-            .collect(Collectors.toMap(e -> (String) (e.getValue().get("name")), e -> e.getKey()));
-        namesSorted = nodeNameToIdMap.keySet().stream()
-            .sorted()
-            .collect(Collectors.toList());
-        System.out.println(namesSorted);
+        nodeNameToIdMap = nodes.entrySet().stream().collect(Collectors.toMap(e -> (String) (e.getValue().get("name")), e -> e.getKey()));
+        namesSorted = nodeNameToIdMap.keySet().stream().sorted().collect(Collectors.toList());
     }
 
-    @SuppressWarnings("unchecked")
-    public void testShutdown() throws IOException {
+    public void testShutdown() throws Exception {
         String nodeIdToShutdown;
         switch (CLUSTER_TYPE) {
             case OLD:
                 nodeIdToShutdown = nodeIdToShutdown(0);
                 assertOK(client().performRequest(shutdownNode(nodeIdToShutdown)));
+                assertBusy(
+                    () -> assertThat(getShutdownStatus(nodeIdToShutdown), equalTo(SingleNodeShutdownMetadata.Status.COMPLETE)),
+                    30,
+                    TimeUnit.SECONDS
+                );
                 break;
+
             case MIXED:
                 if (FIRST_MIXED_ROUND) {
                     nodeIdToShutdown = nodeIdToShutdown(1);
@@ -62,30 +61,38 @@ public class NodeShutdownUpgradeIT extends AbstractUpgradeTestCase {
                     nodeIdToShutdown = nodeIdToShutdown(2);
                 }
                 assertOK(client().performRequest(shutdownNode(nodeIdToShutdown)));
-                break;
-
-            case UPGRADED:
-                final Request getShutdownsReq = new Request("GET", "_nodes/shutdown");
-                final Response getShutdownsResp = client().performRequest(getShutdownsReq);
-                final List<Map<String, Object>> shutdowns = (List<Map<String, Object>>) entityAsMap(getShutdownsResp).get("nodes");
-                assertThat("there should be exactly one shutdown registered", shutdowns, hasSize(6));
-                final Map<String, Object> shutdown = shutdowns.get(0);
-                assertThat(shutdown.get("node_id"), notNullValue()); // Since we randomly determine the node ID, we can't check it
-                assertThat(shutdown.get("reason"), equalTo(this.getTestName()));
-                assertThat(
-                    (String) shutdown.get("status"),
-                    anyOf(
-                        Arrays.stream(SingleNodeShutdownMetadata.Status.values())
-                            .map(value -> equalToIgnoringCase(value.toString()))
-                            .collect(Collectors.toList())
-                    )
+                assertBusy(
+                    () -> assertThat(getShutdownStatus(nodeIdToShutdown), equalTo(SingleNodeShutdownMetadata.Status.COMPLETE)),
+                    30,
+                    TimeUnit.SECONDS
                 );
                 break;
 
+            case UPGRADED:
+                break;
             default:
                 throw new UnsupportedOperationException("Unknown cluster type [" + CLUSTER_TYPE + "]");
         }
+    }
 
+    @SuppressWarnings("unchecked")
+    private SingleNodeShutdownMetadata.Status getShutdownStatus(String nodeIdToShutdown) throws IOException {
+        final Request getShutdownsReq = new Request("GET", "_nodes/shutdown");
+        final Response getShutdownsResp = client().performRequest(getShutdownsReq);
+        final List<Map<String, Object>> shutdowns = (List<Map<String, Object>>) entityAsMap(getShutdownsResp).get("nodes");
+        assertThat("there should be exactly one shutdown registered", shutdowns, hasSize(1));
+        final Map<String, Object> shutdown = shutdowns.get(0);
+        assertThat(shutdown.get("node_id"), equalTo(nodeIdToShutdown));
+        assertThat(shutdown.get("reason"), equalTo(this.getTestName()));
+        assertThat(
+            (String) shutdown.get("status"),
+            anyOf(
+                Arrays.stream(SingleNodeShutdownMetadata.Status.values())
+                    .map(value -> equalToIgnoringCase(value.toString()))
+                    .collect(Collectors.toList())
+            )
+        );
+        return SingleNodeShutdownMetadata.Status.valueOf((String) shutdown.get("status"));
     }
 
     private Request shutdownNode(String nodeIdToShutdown) throws IOException {

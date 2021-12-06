286b0a6d229d05996002c6d80bfc73e766995ab5
Rene Groeschke <rene@elastic.co>
2021-12-06 11:03:19 +0100

    Pass proper max workers to profiled gradle builds

--- .ci/scripts/install-gradle-profiler.sh
+++ .ci/scripts/install-gradle-profiler.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 # profiler version we wanna install
 PROFILER_VERSION="0.16.0"
 wget https://repo.gradle.org/gradle/ext-releases-local/org/gradle/profiler/gradle-profiler/$PROFILER_VERSION/gradle-profiler-$PROFILER_VERSION.zip -O $WORKSPACE/gradle-profiler-$PROFILER_VERSION.zip

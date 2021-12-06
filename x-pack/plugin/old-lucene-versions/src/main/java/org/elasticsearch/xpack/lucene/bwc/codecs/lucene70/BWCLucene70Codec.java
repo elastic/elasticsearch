bc0d6693adfb7510f66b95d2065ee98b2611b744
Yannick Welsch <yannick@welsch.lu>
2021-12-06 13:41:41 +0100

    lice

--- x-pack/plugin/old-lucene-versions/src/main/java/org/elasticsearch/xpack/lucene/bwc/codecs/lucene70/BWCLucene70Codec.java
+++ x-pack/plugin/old-lucene-versions/src/main/java/org/elasticsearch/xpack/lucene/bwc/codecs/lucene70/BWCLucene70Codec.java
@@ -3,6 +3,8 @@
  * or more contributor license agreements. Licensed under the Elastic License
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
+ *
+ * Modifications copyright (C) 2021 Elasticsearch B.V.
  */
 
 package org.elasticsearch.xpack.lucene.bwc.codecs.lucene70;

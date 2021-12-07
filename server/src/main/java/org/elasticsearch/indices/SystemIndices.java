d6dc264fe8b3f9979b687dea5089e4e8847710d7
Artem Prigoda <arteamon@gmail.com>
2021-12-07 14:37:02 +0100

    Update server/src/main/java/org/elasticsearch/indices/SystemIndices.java
    Co-authored-by: Rory Hunter <pugnascotia@users.noreply.github.com>

--- server/src/main/java/org/elasticsearch/indices/SystemIndices.java
+++ server/src/main/java/org/elasticsearch/indices/SystemIndices.java
@@ -643,9 +643,12 @@ public class SystemIndices {
      */
     public static void validateFeatureName(String name, String plugin) {
         if (SnapshotsService.NO_FEATURE_STATES_VALUE.equalsIgnoreCase(name)) {
-            throw new IllegalArgumentException("""
-                feature name cannot be reserved name ["%s"], but was for plugin [%s]\
-                """.formatted(SnapshotsService.NO_FEATURE_STATES_VALUE, plugin));
+            throw new IllegalArgumentException(
+                "feature name cannot be reserved name [\"%s\"], but was for plugin [%s]".formatted(
+                    SnapshotsService.NO_FEATURE_STATES_VALUE,
+                    plugin
+                )
+            );
         }
     }
 

d4434a873c08c9e8baa496122e909df0cb8d2ab0
Stuart Tettemer <stu@elastic.co>
2021-12-08 15:26:06 -0600

    ScriptCompilationSettingsIT WIP

---
+++ x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/ScriptCompilationSettingsIT.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+package org.elasticsearch.upgrades;
+
+import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.Level;
+import org.elasticsearch.Version;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseException;
+import org.elasticsearch.client.WarningsHandler;
+import org.elasticsearch.common.logging.DeprecationLogger;
+import org.elasticsearch.common.xcontent.XContentHelper;
+import org.elasticsearch.script.ScriptService;
+import org.elasticsearch.xcontent.json.JsonXContent;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+public class ScriptCompilationSettingsIT extends AbstractUpgradeTestCase {
+    private static final Version UPGRADE_FROM_VERSION = Version.fromString(System.getProperty("tests.upgrade_from_version"));
+    private static final String WARNING =
+        "[script.max_compilations_rate] setting was deprecated in Elasticsearch and will be removed in a future release! " +
+            "See the breaking changes documentation for the next major version.";
+    public void testMaxCompilationRate() throws IOException {
+        assumeTrue("default changed in v7.16", UPGRADE_FROM_VERSION.onOrAfter(Version.V_7_15_0) && UPGRADE_FROM_VERSION.before(Version.V_7_16_0));
+        if (CLUSTER_TYPE.equals(ClusterType.OLD)) {
+            Request request = new Request("PUT", "_cluster/settings");
+            request.setJsonEntity("{\"persistent\" : { \"script.max_compilations_rate\": \"use-context\" } }");
+            request.setOptions(expectWarnings(WARNING));
+            Response response = client().performRequest(request);
+            assertEquals("{\"acknowledged\":true,\"persistent\":{\"script\":{\"max_compilations_rate\":\"use-context\"}},\"transient\":{}}", EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8));
+            allowedWarnings(WARNING);
+        } /*else if (CLUSTER_TYPE.equals(ClusterType.MIXED)) {
+            Request request = new Request("GET", "_cluster/settings");
+            Response response = client().performRequest(request);
+            assertEquals("{\"persistent\" : { \"script.max_compilations_rate\": \"use-context\" } }", EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8));
+        } */
+    }
+}

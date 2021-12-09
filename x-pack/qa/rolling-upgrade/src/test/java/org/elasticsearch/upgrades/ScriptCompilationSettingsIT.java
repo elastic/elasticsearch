0bd94648ef5069c79fe016af05aadcf10e8aa402
Stuart Tettemer <stu@elastic.co>
2021-12-09 13:18:38 -0600

    XContent and start project

--- x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/ScriptCompilationSettingsIT.java
+++ x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/ScriptCompilationSettingsIT.java
@@ -10,41 +10,53 @@ import org.apache.http.util.EntityUtils;
 import org.elasticsearch.Version;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xcontent.json.JsonXContent;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class ScriptCompilationSettingsIT extends AbstractUpgradeTestCase {
     private static final String WARNING =
-        "[script.max_compilations_rate] setting was deprecated in Elasticsearch and will be removed in a future release! " +
-            "See the breaking changes documentation for the next major version.";
+        "[script.max_compilations_rate] setting was deprecated in Elasticsearch and will be removed in a future release! "
+            + "See the breaking changes documentation for the next major version.";
+
     public void testMaxCompilationRate() throws IOException {
-        assumeTrue("default changed in v7.16", UPGRADE_FROM_VERSION.onOrAfter(Version.V_7_15_0) && UPGRADE_FROM_VERSION.before(Version.V_7_16_0));
+        assumeTrue(
+            "default changed in v7.16",
+            UPGRADE_FROM_VERSION.onOrAfter(Version.V_7_15_0) && UPGRADE_FROM_VERSION.before(Version.V_7_16_0)
+        );
+
+        String maxCompilationRate = compilationRate("template", "max_compilations_rate", "5000/5m");
+        String expectedSetting = "{\"acknowledged\":true,\"persistent\":" + maxCompilationRate + ",\"transient\":{}}";
+
         if (CLUSTER_TYPE.equals(ClusterType.OLD)) {
             Request request = new Request("PUT", "_cluster/settings");
-            request.setJsonEntity("{\"persistent\" : { \"script.context.template.max_compilations_rate\": \"5000/5m\"" +
-                // ", \"script.max_compilations_rate\": \"use-context\" " +
-                "} }");
+            request.setJsonEntity("{\"persistent\":" + maxCompilationRate + "}");
             request.setOptions(expectWarnings(WARNING));
             Response response = client().performRequest(request);
-            assertEquals("{\"acknowledged\":true," +
-                    "\"persistent\":{" +
-                    "\"script\":{" +
-                        "\"context\":{\"template\":{\"max_compilations_rate\":\"5000/5m\"}}" +
-                        //",\"max_compilations_rate\":\"use-context\"" +
-                    "}}," +
-                    "\"transient\":{}}",
-                EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8));
+            assertEquals(expectedSetting, EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8));
 
         } else if (CLUSTER_TYPE.equals(ClusterType.MIXED)) {
             Request request = new Request("GET", "_cluster/settings");
             Response response = client().performRequest(request);
-            assertEquals("{\"persistent\":{\"script\":{" +
-                    "\"context\":{\"template\":{\"max_compilations_rate\":\"5000/5m\"}}" +
-                    //",\"max_compilations_rate\":\"use-context\"" +
-                    "}},\"transient\":{}}",
-                EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8));
+            assertEquals(expectedSetting, EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8));
         }
+    }
+
+    protected String compilationRate(String context, String field, String value) throws IOException {
+        XContentBuilder builder = JsonXContent.contentBuilder()
+            .startObject()
+            .startObject("script")
+            .startObject("context")
+            .startObject(context)
+            .field(field, value)
+            .endObject()
+            .endObject()
+            .endObject()
+            .endObject();
+        return Strings.toString(builder);
     }
 
     @Override

1e99bc614d251bec0fa9381b95349a29014581be
Yannick Welsch <yannick@welsch.lu>
2021-12-14 11:21:20 +0100

    Make peer recovery work with archive data (#81522)
    Adapts peer recovery so that it properly integrates with the hook to convert old indices.
    
    Relates #81210

--- x-pack/plugin/searchable-snapshots/src/main/java/org/elasticsearch/xpack/searchablesnapshots/allocation/SearchableSnapshotIndexEventListener.java
+++ x-pack/plugin/searchable-snapshots/src/main/java/org/elasticsearch/xpack/searchablesnapshots/allocation/SearchableSnapshotIndexEventListener.java
@@ -9,7 +9,6 @@ package org.elasticsearch.xpack.searchablesnapshots.allocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.ParameterizedMessage;
-import org.apache.lucene.index.SegmentInfos;
 import org.elasticsearch.action.StepListener;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.routing.RecoverySource;
@@ -18,19 +17,14 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.core.Nullable;
 import org.elasticsearch.index.IndexService;
 import org.elasticsearch.index.IndexSettings;
-import org.elasticsearch.index.seqno.SequenceNumbers;
 import org.elasticsearch.index.shard.IndexEventListener;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.index.shard.ShardId;
-import org.elasticsearch.index.translog.Translog;
-import org.elasticsearch.index.translog.TranslogException;
 import org.elasticsearch.indices.cluster.IndicesClusterStateService.AllocatedIndices.IndexRemovalReason;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.xpack.searchablesnapshots.cache.full.CacheService;
 import org.elasticsearch.xpack.searchablesnapshots.cache.shared.FrozenCacheService;
 import org.elasticsearch.xpack.searchablesnapshots.store.SearchableSnapshotDirectory;
-
-import java.nio.file.Path;
 
 import static org.elasticsearch.snapshots.SearchableSnapshotsSettings.isSearchableSnapshotStore;
 import static org.elasticsearch.xpack.searchablesnapshots.SearchableSnapshots.SNAPSHOT_INDEX_NAME_SETTING;
@@ -65,7 +59,6 @@ public class SearchableSnapshotIndexEventListener implements IndexEventListener 
     public void beforeIndexShardRecovery(IndexShard indexShard, IndexSettings indexSettings) {
         assert Thread.currentThread().getName().contains(ThreadPool.Names.GENERIC);
         ensureSnapshotIsLoaded(indexShard);
-        associateNewEmptyTranslogWithIndex(indexShard);
     }
 
     private static void ensureSnapshotIsLoaded(IndexShard indexShard) {
@@ -91,26 +84,6 @@ public class SearchableSnapshotIndexEventListener implements IndexEventListener 
         assert directory.listAll().length > 0 : "expecting directory listing to be non-empty";
         assert success || indexShard.routingEntry().recoverySource().getType() == RecoverySource.Type.PEER
             : "loading snapshot must not be called twice unless we are retrying a peer recovery";
-    }
-
-    private static void associateNewEmptyTranslogWithIndex(IndexShard indexShard) {
-        final ShardId shardId = indexShard.shardId();
-        assert isSearchableSnapshotStore(indexShard.indexSettings().getSettings()) : "Expected a searchable snapshot shard " + shardId;
-        if (indexShard.routingEntry().primary()
-            && indexShard.routingEntry().recoverySource().getType().equals(RecoverySource.Type.SNAPSHOT)) {
-            // translog initialization is done later in the restore step
-            return;
-        }
-        try {
-            final SegmentInfos segmentInfos = indexShard.store().readLastCommittedSegmentsInfo();
-            final long localCheckpoint = Long.parseLong(segmentInfos.userData.get(SequenceNumbers.LOCAL_CHECKPOINT_KEY));
-            final long primaryTerm = indexShard.getPendingPrimaryTerm();
-            final String translogUUID = segmentInfos.userData.get(Translog.TRANSLOG_UUID_KEY);
-            final Path translogLocation = indexShard.shardPath().resolveTranslog();
-            Translog.createEmptyTranslog(translogLocation, shardId, localCheckpoint, primaryTerm, translogUUID, null);
-        } catch (Exception e) {
-            throw new TranslogException(shardId, "failed to associate a new translog", e);
-        }
     }
 
     @Override

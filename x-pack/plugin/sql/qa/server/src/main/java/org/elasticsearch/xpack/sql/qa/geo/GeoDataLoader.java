b3d019f5afa43859dcfd239c1ad250b978a2690b
Artem Prigoda <artem.prigoda@elastic.co>
2021-12-14 18:11:08 +0100

    Add missed import

--- x-pack/plugin/sql/qa/server/src/main/java/org/elasticsearch/xpack/sql/qa/geo/GeoDataLoader.java
+++ x-pack/plugin/sql/qa/server/src/main/java/org/elasticsearch/xpack/sql/qa/geo/GeoDataLoader.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
 
 import static org.elasticsearch.xpack.sql.qa.jdbc.DataLoader.createString;

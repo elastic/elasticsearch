a436785f54949f63b19734e9166fe3e3b11ea6f0
Martijn van Groningen <martijn.v.groningen@gmail.com>
2021-12-15 14:16:23 +0100

    fix npe

--- server/src/main/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexService.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexService.java
@@ -867,7 +867,8 @@ public class MetadataCreateIndexService {
         if (sourceMetadata == null) {
             final Settings.Builder additionalIndexSettings = Settings.builder();
             final Settings templateAndRequestSettings = Settings.builder().put(combinedTemplateSettings).put(request.settings()).build();
-            final boolean newDataStream = currentState.getMetadata().dataStreams().containsKey(request.dataStreamName()) == false;
+            final boolean newDataStream = request.dataStreamName() != null
+                && currentState.getMetadata().dataStreams().containsKey(request.dataStreamName()) == false;
 
             // Loop through all the explicit index setting providers, adding them to the
             // additionalIndexSettings map

1ac814571b23807b78d77d201f5f0233f8706584
Martijn van Groningen <martijn.v.groningen@gmail.com>
2021-12-15 14:42:19 +0100

    iter

--- server/src/main/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexService.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexService.java
@@ -867,7 +867,7 @@ public class MetadataCreateIndexService {
         if (sourceMetadata == null) {
             final Settings.Builder additionalIndexSettings = Settings.builder();
             final Settings templateAndRequestSettings = Settings.builder().put(combinedTemplateSettings).put(request.settings()).build();
-            final boolean newDataStream = request.dataStreamName() != null
+            final boolean newDataStream = isDataStreamIndex
                 && currentState.getMetadata().dataStreams().containsKey(request.dataStreamName()) == false;
 
             // Loop through all the explicit index setting providers, adding them to the

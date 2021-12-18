a46c34b35c303542ea8cf01a11ec49efec1aabdb
Chris Hegarty <62058229+ChrisHegarty@users.noreply.github.com>
2021-12-18 18:10:15 +0000

    [7.16] Tolerate unprivileged log4j getClassLoaders calls (#81903)

--- server/src/main/java/org/elasticsearch/bootstrap/ESPolicy.java
+++ server/src/main/java/org/elasticsearch/bootstrap/ESPolicy.java
@@ -20,8 +20,10 @@ import java.security.PermissionCollection;
 import java.security.Permissions;
 import java.security.Policy;
 import java.security.ProtectionDomain;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 /** custom policy for union of static and dynamic permissions */
@@ -56,6 +58,26 @@ final class ESPolicy extends Policy {
         }
         this.dynamic = dynamic;
         this.plugins = plugins;
+    }
+
+    private static final Predicate<StackTraceElement> JDK_BOOT = f -> f.getClassName().startsWith("java.lang.")
+        || f.getClassName().startsWith("java.security.");
+    private static final Predicate<StackTraceElement> ES_BOOTSTRAP = f -> f.getClassName().startsWith("org.elasticsearch.bootstrap");
+    private static final Predicate<StackTraceElement> IS_LOG4J = f -> "org.apache.logging.log4j.util.LoaderUtil".equals(f.getClassName())
+        && "getClassLoaders".equals(f.getMethodName());
+
+    /**
+     *  Returns true if the top of the call stack has:
+     *   1) Only frames belonging from the JDK's boot loader or org.elasticsearch.bootstrap, followed directly by
+     *   2) org.apache.logging.log4j.util.LoaderUtil.getClassLoaders
+     */
+    private static boolean isLoaderUtilGetClassLoaders() {
+        Optional<StackTraceElement> frame = Arrays.stream(Thread.currentThread().getStackTrace())
+            .filter(JDK_BOOT.or(ES_BOOTSTRAP).negate())
+            .limit(1)
+            .findFirst()
+            .filter(IS_LOG4J);
+        return frame.isPresent();
     }
 
     @Override
@@ -98,6 +120,10 @@ final class ESPolicy extends Policy {
         // The FilePermission to check access to the path.data is the hottest permission check in
         // Elasticsearch, so we check it first.
         if (permission instanceof FilePermission && dataPathPermission.implies(permission)) {
+            return true;
+        }
+
+        if (permission instanceof RuntimePermission && "getClassLoader".equals(permission.getName()) && isLoaderUtilGetClassLoaders()) {
             return true;
         }
 

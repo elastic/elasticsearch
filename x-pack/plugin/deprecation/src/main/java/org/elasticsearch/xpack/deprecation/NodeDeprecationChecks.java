6d1c24f14e65bae8c1ef0604970c0eef9e25ed5a
Fabio Busatto <52658645+bytebilly@users.noreply.github.com>
2021-12-29 19:08:24 +0100

    Fix copying

--- x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/NodeDeprecationChecks.java
+++ x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/NodeDeprecationChecks.java
@@ -208,8 +208,8 @@ class NodeDeprecationChecks {
         if (XPackSettings.SECURITY_ENABLED.exists(settings) == false
             && (licenseState.getOperationMode().equals(License.OperationMode.BASIC)
                 || licenseState.getOperationMode().equals(License.OperationMode.TRIAL))) {
-            String details = "Elasticsearch Security features will no longer be implicitly disabled in 8.0. The [xpack.security.enabled] "
-                + "setting will always default to \"true\", and other required security settings must be explicitly set in the configuration. "
+            String details = "In Elasticsearch 8.0 the [xpack.security.enabled] setting will always default to true. "
+                + "In your environment, the value is not set and you need to set it before upgrading, along with other required security settings. "
                 + "See https://ela.st/es-deprecation-7-security-basic-setup to secure your cluster. "
                 + "To explicitly disable security, set [xpack.security.enabled] to \"false\" (not recommended).";
             return new DeprecationIssue(

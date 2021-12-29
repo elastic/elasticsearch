86f9f9ce13014d5fcd8283acab5fceec987162d0
Igor Motov <igor@motovs.org>
2021-12-29 11:54:32 -1000

    Fix TransformAggregationsTest

--- x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/transforms/pivot/TransformAggregations.java
+++ x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/transforms/pivot/TransformAggregations.java
@@ -79,7 +79,8 @@ public final class TransformAggregations {
         "t_test", // https://github.com/elastic/elasticsearch/issues/54503,
         "variable_width_histogram", // https://github.com/elastic/elasticsearch/issues/58140
         "rate", // https://github.com/elastic/elasticsearch/issues/61351
-        "multi_terms" // https://github.com/elastic/elasticsearch/issues/67609
+        "multi_terms", // https://github.com/elastic/elasticsearch/issues/67609
+        "time_series" // https://github.com/elastic/elasticsearch/issues/74660
     );
 
     private TransformAggregations() {}

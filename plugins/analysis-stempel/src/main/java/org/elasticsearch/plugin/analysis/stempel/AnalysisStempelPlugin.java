f0387153912c9055717ea357b9a3c7f963f7c2c6
Nikola Grcevski <nikola.grcevski@elastic.co>
2021-12-30 18:44:36 -0500

    Revert changes.

--- plugins/analysis-stempel/src/main/java/org/elasticsearch/plugin/analysis/stempel/AnalysisStempelPlugin.java
+++ plugins/analysis-stempel/src/main/java/org/elasticsearch/plugin/analysis/stempel/AnalysisStempelPlugin.java
@@ -9,9 +9,7 @@
 package org.elasticsearch.plugin.analysis.stempel;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.core.UpperCaseFilter;
 import org.elasticsearch.index.analysis.AnalyzerProvider;
-import org.elasticsearch.index.analysis.PreConfiguredTokenFilter;
 import org.elasticsearch.index.analysis.TokenFilterFactory;
 import org.elasticsearch.index.analysis.pl.PolishAnalyzerProvider;
 import org.elasticsearch.index.analysis.pl.PolishStemTokenFilterFactory;
@@ -20,7 +18,6 @@ import org.elasticsearch.indices.analysis.AnalysisModule.AnalysisProvider;
 import org.elasticsearch.plugins.AnalysisPlugin;
 import org.elasticsearch.plugins.Plugin;
 
-import java.util.List;
 import java.util.Map;
 
 import static java.util.Collections.singletonMap;
@@ -28,18 +25,11 @@ import static java.util.Collections.singletonMap;
 public class AnalysisStempelPlugin extends Plugin implements AnalysisPlugin {
     @Override
     public Map<String, AnalysisProvider<TokenFilterFactory>> getTokenFilters() {
-        return Map.of(
-            "polish_stem", PolishStemTokenFilterFactory::new,
-            "polish_stop", PolishStopTokenFilterFactory::new);
+        return Map.of("polish_stem", PolishStemTokenFilterFactory::new, "polish_stop", PolishStopTokenFilterFactory::new);
     }
 
     @Override
     public Map<String, AnalysisProvider<AnalyzerProvider<? extends Analyzer>>> getAnalyzers() {
         return singletonMap("polish", PolishAnalyzerProvider::new);
-    }
-
-    @Override
-    public List<PreConfiguredTokenFilter> getPreConfiguredTokenFilters() {
-        return List.of(PreConfiguredTokenFilter.singleton("uppercase", true, UpperCaseFilter::new));
     }
 }

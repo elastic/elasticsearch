060ab86077dc73676a218a082fe39296f90aa3c9
markharwood <markharwood@gmail.com>
2022-01-04 12:11:55 +0000

    Search - r	eject arrays of values when passed to the Term query. Use error message as an opportunity to advertise the `terms` query
    Closes #82183

--- server/src/main/java/org/elasticsearch/index/query/TermQueryBuilder.java
+++ server/src/main/java/org/elasticsearch/index/query/TermQueryBuilder.java
@@ -120,6 +120,12 @@ public class TermQueryBuilder extends BaseTermQueryBuilder<TermQueryBuilder> {
                         if (TERM_FIELD.match(currentFieldName, parser.getDeprecationHandler())) {
                             value = maybeConvertToBytesRef(parser.objectBytes());
                         } else if (VALUE_FIELD.match(currentFieldName, parser.getDeprecationHandler())) {
+                            if (parser.currentToken() == XContentParser.Token.START_ARRAY) {
+                                throw new ParsingException(
+                                    parser.getTokenLocation(),
+                                    "[term] query does not support arrays for value - use Terms query instead."
+                                );
+                            }
                             value = maybeConvertToBytesRef(parser.objectBytes());
                         } else if (AbstractQueryBuilder.NAME_FIELD.match(currentFieldName, parser.getDeprecationHandler())) {
                             queryName = parser.text();

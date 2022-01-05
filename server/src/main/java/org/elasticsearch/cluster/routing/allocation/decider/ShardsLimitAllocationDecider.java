e8f309c73b13560f54387d1c362219b7dde736d8
Armin Braun <me@obrown.io>
2022-01-05 11:43:01 +0100

    Faster ShardsLimitAllocationDecider
    As in many other cases, cache the setting value on `IndexMetadata`
    and also don't parse the constant setting value from the static
    settings on every decision and store it in a field instead of the
    settings.

--- server/src/main/java/org/elasticsearch/cluster/routing/allocation/decider/ShardsLimitAllocationDecider.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/allocation/decider/ShardsLimitAllocationDecider.java
@@ -46,6 +46,8 @@ public class ShardsLimitAllocationDecider extends AllocationDecider {
 
     private volatile int clusterShardLimit;
 
+    private final int indexShardLimit;
+
     /**
      * Controls the maximum number of shards per index on a single Elasticsearch
      * node. Negative values are interpreted as unlimited.
@@ -70,11 +72,9 @@ public class ShardsLimitAllocationDecider extends AllocationDecider {
         Property.NodeScope
     );
 
-    private final Settings settings;
-
     public ShardsLimitAllocationDecider(Settings settings, ClusterSettings clusterSettings) {
-        this.settings = settings;
         this.clusterShardLimit = CLUSTER_TOTAL_SHARDS_PER_NODE_SETTING.get(settings);
+        this.indexShardLimit = INDEX_TOTAL_SHARDS_PER_NODE_SETTING.get(settings);
         clusterSettings.addSettingsUpdateConsumer(CLUSTER_TOTAL_SHARDS_PER_NODE_SETTING, this::setClusterShardLimit);
     }
 
@@ -100,7 +100,8 @@ public class ShardsLimitAllocationDecider extends AllocationDecider {
         BiPredicate<Integer, Integer> decider
     ) {
         IndexMetadata indexMd = allocation.metadata().getIndexSafe(shardRouting.index());
-        final int indexShardLimit = INDEX_TOTAL_SHARDS_PER_NODE_SETTING.get(indexMd.getSettings(), settings);
+        final Integer indexLevelLimit = indexMd.getShardsPerNodeLimit();
+        final int indexShardLimit = indexLevelLimit == null ? this.indexShardLimit : indexLevelLimit;
         // Capture the limit here in case it changes during this method's
         // execution
         final int clusterShardLimit = this.clusterShardLimit;

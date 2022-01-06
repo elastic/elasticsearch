f927b840275a79f31078c960723d51a7f1e8eecb
Ievgen Degtiarenko <ievgen.degtiarenko@gmail.com>
2022-01-06 12:48:25 +0100

    Update test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
    Co-authored-by: Tanguy Leroux <tlrx.dev@gmail.com>

--- test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
+++ test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
@@ -2456,7 +2456,7 @@ public final class InternalTestCluster extends TestCluster {
                 try {
                     env.shardLock(id, "InternalTestCluster assert after test", TimeUnit.SECONDS.toMillis(5)).close();
                 } catch (ShardLockObtainFailedException ex) {
-                    fail("Shard " + id + " is still locked after 5 sec waiting: " + ex.getMessage());
+throw new AssertionError("Shard " + id + " is still locked after 5 sec waiting", ex);
                 }
             }
         }

36b917a50c67b31be3ea4e37dde5f878ac979d1d
Nikola Grcevski <nikola.grcevski@elastic.co>
2022-01-06 14:12:53 -0500

    Fix test message comparison.

--- server/src/internalClusterTest/java/org/elasticsearch/env/NodeEnvironmentIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/env/NodeEnvironmentIT.java
@@ -126,7 +126,16 @@ public class NodeEnvironmentIT extends ESIntegTestCase {
         );
         assertThat(
             illegalStateException.getMessage(),
-            allOf(startsWith("cannot downgrade a node from version ["), endsWith("] to version [" + Version.CURRENT + "]"))
+            allOf(
+                startsWith("cannot upgrade a node from version ["),
+                endsWith(
+                    "] directly to version ["
+                        + Version.CURRENT
+                        + "], upgrade to version ["
+                        + Version.CURRENT.minimumCompatibilityVersion()
+                        + "] first."
+                )
+            )
         );
     }
 
@@ -136,7 +145,16 @@ public class NodeEnvironmentIT extends ESIntegTestCase {
         );
         assertThat(
             illegalStateException.getMessage(),
-            allOf(startsWith("cannot upgrade a node from version ["), endsWith("] directly to version [" + Version.CURRENT + "]"))
+            allOf(
+                startsWith("cannot upgrade a node from version ["),
+                endsWith(
+                    "] directly to version ["
+                        + Version.CURRENT
+                        + "], upgrade to version ["
+                        + Version.CURRENT.minimumCompatibilityVersion()
+                        + "] first."
+                )
+            )
         );
     }
 

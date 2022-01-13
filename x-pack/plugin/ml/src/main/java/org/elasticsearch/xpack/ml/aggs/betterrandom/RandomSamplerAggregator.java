270e9d47362ce8ed2a7ebfe3cdc166d8c80a3509
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2022-01-13 14:03:27 -0500

    updating agg

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/aggs/betterrandom/RandomSamplerAggregator.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/aggs/betterrandom/RandomSamplerAggregator.java
@@ -7,11 +7,7 @@
 package org.elasticsearch.xpack.ml.aggs.betterrandom;
 
 import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.search.ConjunctionUtils;
-import org.apache.lucene.search.ConstantScoreScorer;
-import org.apache.lucene.search.ConstantScoreWeight;
 import org.apache.lucene.search.DocIdSetIterator;
-import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreMode;
 import org.apache.lucene.search.Scorer;
 import org.apache.lucene.search.Weight;
@@ -25,23 +21,19 @@ import org.elasticsearch.search.aggregations.bucket.BucketsAggregator;
 import org.elasticsearch.search.aggregations.bucket.SingleBucketAggregator;
 import org.elasticsearch.search.aggregations.bucket.sampler.InternalSampler;
 import org.elasticsearch.search.aggregations.support.AggregationContext;
-import org.elasticsearch.xpack.ml.aggs.FastGeometric;
-import org.elasticsearch.xpack.ml.aggs.PCG;
 import org.elasticsearch.xpack.ml.randomsampling.RandomSamplingQuery;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
-import java.util.function.IntSupplier;
 
 /**
  * Aggregate random docs on the shard
  */
 public class RandomSamplerAggregator extends BucketsAggregator implements SingleBucketAggregator {
 
-    private final FastGeometric geometric;
     private final double probability;
-    private final PCG rng;
+    private final int seed;
+    private final int hash;
 
     RandomSamplerAggregator(
         String name,
@@ -54,16 +46,9 @@ public class RandomSamplerAggregator extends BucketsAggregator implements Single
         Map<String, Object> metadata
     ) throws IOException {
         super(name, factories, context, parent, cardinalityUpperBound, metadata);
-        this.geometric = new FastGeometric(new IntSupplier() {
-            private final PCG rng = new PCG(seed, context.shardRandomSeed());
-
-            @Override
-            public int getAsInt() {
-                return rng.nextInt();
-            }
-        }, probability);
         this.probability = probability;
-        this.rng = new PCG(seed, context.shardRandomSeed());
+        this.seed = seed;
+        this.hash = context.shardRandomSeed();
         if (this.subAggregators().length == 0) {
             throw new IllegalArgumentException("must have sub aggs");
         }
@@ -90,34 +75,8 @@ public class RandomSamplerAggregator extends BucketsAggregator implements Single
     @Override
     protected LeafBucketCollector getLeafCollector(LeafReaderContext ctx, LeafBucketCollector sub) throws IOException {
         // TODO Can we optimize this by joining the query results with a random_sample query????
-        Query query = searcher().rewrite(topLevelQuery());
-        Weight innerWeight = searcher().createWeight(searcher().rewrite(topLevelQuery()), ScoreMode.COMPLETE_NO_SCORES, 1f);
-        Weight weight = new ConstantScoreWeight(query, 1f) {
-            @Override
-            public Scorer scorer(LeafReaderContext context) throws IOException {
-                int maxDoc = context.reader().maxDoc();
-                Scorer scorer = new ConstantScoreScorer(
-                    this,
-                    score(),
-                    ScoreMode.COMPLETE_NO_SCORES,
-                    new RandomSamplingQuery.RandomSamplingIterator(maxDoc, probability, rng::nextInt)
-                );
-                Scorer queryScorer = innerWeight.scorer(context);
-                if (queryScorer == null) {
-                    return null;
-                }
-                return new ConstantScoreScorer(
-                    this,
-                    score(),
-                    ScoreMode.COMPLETE_NO_SCORES,
-                    ConjunctionUtils.intersectScorers(List.of(scorer, queryScorer))
-                );
-            }
-            @Override
-            public boolean isCacheable(LeafReaderContext ctx) {
-                return false;
-            }
-        };
+        RandomSamplingQuery query = new RandomSamplingQuery(probability, seed, hash, false, topLevelQuery());
+        Weight weight = searcher().createWeight(searcher().rewrite(query), ScoreMode.COMPLETE_NO_SCORES, 1f);
         Scorer scorer = weight.scorer(ctx);
         if (scorer == null) {
             return LeafBucketCollector.NO_OP_COLLECTOR;
--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/randomsampling/RandomSamplingQuery.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/randomsampling/RandomSamplingQuery.java
@@ -45,7 +45,7 @@ public final class RandomSamplingQuery extends Query {
      *                  can be generated
      * @param cacheable True if the seed is static (provided by the user) so that we can cache the query, false otherwise
      */
-    RandomSamplingQuery(double p, int seed, long hash, boolean cacheable, Query query) {
+    public RandomSamplingQuery(double p, int seed, long hash, boolean cacheable, Query query) {
         if (p <= 0.0 || p >= 1.0) {
             throw new IllegalArgumentException("RandomSampling probability must be between 0.0 and 1.0");
         }

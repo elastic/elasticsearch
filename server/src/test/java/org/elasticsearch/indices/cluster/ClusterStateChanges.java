4e66a97573f60b2f79ea5bb9bdbe987b125fffd9
Ievgen Degtiarenko <ievgen.degtiarenko@elastic.co>
2022-01-17 15:09:39 +0100

    Combine node removal task and listener (#82663)
    Today node removal tasks executed by the master have a separate
    ClusterStateTaskListener to feed back the result to the requester.
    It'd be preferable to use the task itself as the listener.

--- server/src/test/java/org/elasticsearch/indices/cluster/ClusterStateChanges.java
+++ server/src/test/java/org/elasticsearch/indices/cluster/ClusterStateChanges.java
@@ -321,7 +321,7 @@ public class ClusterStateChanges {
             EmptySystemIndices.INSTANCE
         );
 
-        nodeRemovalExecutor = new NodeRemovalClusterStateTaskExecutor(allocationService, logger);
+        nodeRemovalExecutor = new NodeRemovalClusterStateTaskExecutor(allocationService);
         joinTaskExecutor = new JoinTaskExecutor(allocationService, logger, (s, p, r) -> {});
     }
 
@@ -400,7 +400,7 @@ public class ClusterStateChanges {
         return runTasks(
             nodeRemovalExecutor,
             clusterState,
-            nodes.stream().map(n -> new NodeRemovalClusterStateTaskExecutor.Task(n, "dummy reason")).collect(Collectors.toList())
+            nodes.stream().map(n -> new NodeRemovalClusterStateTaskExecutor.Task(n, "dummy reason", () -> {})).collect(Collectors.toList())
         );
     }
 

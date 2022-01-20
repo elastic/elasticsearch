c07efea260ce5c18b954dc1b0d7e6b5c1c45bb4c
Martijn van Groningen <martijn.v.groningen@gmail.com>
2022-01-20 15:00:50 +0100

    removed unused import

--- server/src/main/java/org/elasticsearch/cluster/metadata/ComposableIndexTemplate.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/ComposableIndexTemplate.java
@@ -28,7 +28,6 @@ import org.elasticsearch.xcontent.XContentParser;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 

861dd90c8719742f004a882f09415d8fbab1fc12
Frank Hassanabad <frank.hassanabad@elastic.co>
2022-01-19 21:56:23 -0700

    Fixes permissions within pre-defined "viewer" and "editor" roles for security solutions  (#82835)
    ##Summary
    
    See Kibana issue here from testing that brought up the bugs and details:
    https://github.com/elastic/kibana/issues/123413
    
    Elasticsearch has built-in `editor` and `viewer` roles that grant access to all features in Kibana. This PR touches up and fixes permission issues found from "viewer" and "editor".
    
    * This adds permissions for the `.lists*` and `.items*` indexes when using `editor` role and `kibana_system` role that are missing.
    * This adds the `maintenance` role needed for index refreshes to the alerting indexes.
    * This changes some strings for the same constants used in the file to be consistent with permissions across these indexes.
    * This adds tests
    * This adds some docs to the code.
    
    
    **Manual testing (for curious Kibana devs)**
    * See [testing](https://github.com/elastic/elasticsearch/blob/master/TESTING.asciidoc) and the main [README here](https://github.com/elastic/elasticsearch) to help get setup as you go through the steps below
    * Install gradle and opendjk. I installed them through brew.
    * Run `gradle wrapper` to create the wrapper
    * Start elasticsearch through `./gradlew run -Drun.license_type=trial`
    * Edit your `kibana.dev.yml` and `auditbeat.yml` file(s) to use the `elastic-admin` to first log into the system and start Kibana as well as run auditbeat.
    * Start auditbeat.
    * Add two users. 1 user should have the role of `viewer` and the other `editor`
    * Go to "Security -> Detections" and create a rule that has KQL such as "\*:\*" to generate alerts as the user with the role "editor"
    * Ensure you don't get permission warnings about the user.
    * Log out and log in as the user with "viewer" role.
    * Check to ensure that user can access read-only mode on the detections and that the banner now says it has read permissions and is only missing write permissions.

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStoreTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStoreTests.java
@@ -474,7 +474,9 @@ public class ReservedRolesStoreTests extends ESTestCase {
             ReservedRolesStore.ALERTS_BACKING_INDEX + randomAlphaOfLength(randomIntBetween(0, 13)),
             ReservedRolesStore.ALERTS_INDEX_ALIAS + randomAlphaOfLength(randomIntBetween(0, 13)),
             ReservedRolesStore.PREVIEW_ALERTS_INDEX_ALIAS + randomAlphaOfLength(randomIntBetween(0, 13)),
-            ReservedRolesStore.PREVIEW_ALERTS_BACKING_INDEX_ALIAS + randomAlphaOfLength(randomIntBetween(0, 13))
+            ReservedRolesStore.PREVIEW_ALERTS_BACKING_INDEX_ALIAS + randomAlphaOfLength(randomIntBetween(0, 13)),
+            ReservedRolesStore.LISTS_INDEX + randomAlphaOfLength(randomIntBetween(0, 13)),
+            ReservedRolesStore.LISTS_ITEMS_INDEX + randomAlphaOfLength(randomIntBetween(0, 13))
         ).forEach(index -> assertAllIndicesAccessAllowed(kibanaRole, index));
 
         // read-only index access, including cross cluster
@@ -2278,6 +2280,8 @@ public class ReservedRolesStoreTests extends ESTestCase {
         assertOnlyReadAllowed(role, ".siem-signals-" + randomIntBetween(0, 5));
         assertOnlyReadAllowed(role, ".alerts-" + randomIntBetween(0, 5));
         assertOnlyReadAllowed(role, ".preview.alerts-" + randomIntBetween(0, 5));
+        assertOnlyReadAllowed(role, ".lists-" + randomIntBetween(0, 5));
+        assertOnlyReadAllowed(role, ".items-" + randomIntBetween(0, 5));
         assertOnlyReadAllowed(role, "apm-" + randomIntBetween(0, 5) + "-transaction-" + randomIntBetween(0, 5));
         assertOnlyReadAllowed(role, "logs-" + randomIntBetween(0, 5));
         assertOnlyReadAllowed(role, "auditbeat-" + randomIntBetween(0, 5));
@@ -2340,10 +2344,10 @@ public class ReservedRolesStoreTests extends ESTestCase {
         assertReadWriteDocsAndMaintenanceButNotDeleteIndexAllowed(role, ".lists-" + randomIntBetween(0, 5));
         assertReadWriteDocsAndMaintenanceButNotDeleteIndexAllowed(role, ".items-" + randomIntBetween(0, 5));
         assertReadWriteDocsButNotDeleteIndexAllowed(role, "observability-annotations");
-        assertReadWriteDocsButNotDeleteIndexAllowed(role, ".alerts-" + randomIntBetween(0, 5));
-        assertReadWriteDocsButNotDeleteIndexAllowed(role, ".internal.alerts-" + randomIntBetween(0, 5));
-        assertReadWriteDocsButNotDeleteIndexAllowed(role, ".preview.alerts-" + randomIntBetween(0, 5));
-        assertReadWriteDocsButNotDeleteIndexAllowed(role, ".internal.preview.alerts-" + randomIntBetween(0, 5));
+        assertReadWriteDocsAndMaintenanceButNotDeleteIndexAllowed(role, ".alerts-" + randomIntBetween(0, 5));
+        assertReadWriteDocsAndMaintenanceButNotDeleteIndexAllowed(role, ".internal.alerts-" + randomIntBetween(0, 5));
+        assertReadWriteDocsAndMaintenanceButNotDeleteIndexAllowed(role, ".preview.alerts-" + randomIntBetween(0, 5));
+        assertReadWriteDocsAndMaintenanceButNotDeleteIndexAllowed(role, ".internal.preview.alerts-" + randomIntBetween(0, 5));
 
         assertNoAccessAllowed(role, RestrictedIndicesNames.RESTRICTED_NAMES);
         assertNoAccessAllowed(role, "." + randomAlphaOfLengthBetween(6, 10));

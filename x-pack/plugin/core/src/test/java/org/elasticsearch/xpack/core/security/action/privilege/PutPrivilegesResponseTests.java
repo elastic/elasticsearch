e74d6cafae74e114aa67ca61d042b0d52c93fc0a
Artem Prigoda <arteamon@gmail.com>
2022-01-24 23:33:58 +0100

    Update PutPrivilegesResponseTests.java

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/action/privilege/PutPrivilegesResponseTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/action/privilege/PutPrivilegesResponseTests.java
@@ -10,6 +10,7 @@ package org.elasticsearch.xpack.core.security.action.privilege;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.xcontent.XContentParser;
 import org.elasticsearch.xcontent.json.JsonXContent;
 
 import java.io.IOException;

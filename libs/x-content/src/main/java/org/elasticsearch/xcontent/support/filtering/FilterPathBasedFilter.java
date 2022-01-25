b1aee0d5e2c3e400a0a6deab8005275d29aafc1c
Nik Everett <nik9000@gmail.com>
2022-01-25 11:29:23 -0500

    Handle fields.with.dots in routing_path
    Our document parsing code treats:
    ```
    {"foo.bar": "baz"}
    ```
    the same as:
    ```
    {"foo": {"bar": "baz"}}
    ```
    but the code that extracted routing_path didn't! This is bad because
    routing_path has to identify a subset of dimensions precisely and in the
    same way that our mapping code identifies the dimensions. They have to
    line up or two time series could end up on different shards. Sad times.
    
    This makes `routing_path` interpret dots in field names as though they
    were an object. It creates an option for the includes/excludes filters
    on `XContentParser` to treats dots as objects. So the filter would see
    ```
    {"foo.bar": "baz"}
    ```
    as though it were:
    ```
    {"foo": {"bar": "baz"}}
    ```
    
    So the filter `foo.bar` would match both documents.
    
    This is part of #82511.

--- libs/x-content/src/main/java/org/elasticsearch/xcontent/support/filtering/FilterPathBasedFilter.java
+++ libs/x-content/src/main/java/org/elasticsearch/xcontent/support/filtering/FilterPathBasedFilter.java
@@ -20,7 +20,7 @@ public class FilterPathBasedFilter extends TokenFilter {
      * Marker value that should be used to indicate that a property name
      * or value matches one of the filter paths.
      */
-    private static final TokenFilter MATCHING = new TokenFilter() {
+    private static final FilterPathBasedFilter MATCHING = new FilterPathBasedFilter() {
         @Override
         public String toString() {
             return "MATCHING";
@@ -31,7 +31,7 @@ public class FilterPathBasedFilter extends TokenFilter {
      * Marker value that should be used to indicate that none of the
      * property names/values matches one of the filter paths.
      */
-    private static final TokenFilter NO_MATCHING = new TokenFilter() {
+    private static final FilterPathBasedFilter NO_MATCHING = new FilterPathBasedFilter() {
         @Override
         public String toString() {
             return "NO_MATCHING";
@@ -42,25 +42,36 @@ public class FilterPathBasedFilter extends TokenFilter {
 
     private final boolean inclusive;
 
-    public FilterPathBasedFilter(FilterPath[] filters, boolean inclusive) {
+    private final boolean matchFieldNamesWithDots;
+
+    public FilterPathBasedFilter(FilterPath[] filters, boolean inclusive, boolean matchFieldNamesWithDots) {
         if (filters == null || filters.length == 0) {
             throw new IllegalArgumentException("filters cannot be null or empty");
         }
-        this.inclusive = inclusive;
         this.filters = filters;
+        this.inclusive = inclusive;
+        this.matchFieldNamesWithDots = matchFieldNamesWithDots;
+    }
+
+    private FilterPathBasedFilter() {
+        this.inclusive = true;
+        this.filters = null;
+        this.matchFieldNamesWithDots = false;
     }
 
     public FilterPathBasedFilter(Set<String> filters, boolean inclusive) {
-        this(FilterPath.compile(filters), inclusive);
+        this(FilterPath.compile(filters), inclusive, false);
     }
 
     /**
      * Evaluates if a property name matches one of the given filter paths.
      */
-    private TokenFilter evaluate(String name, FilterPath[] filterPaths) {
-        if (filterPaths != null) {
+    private FilterPathBasedFilter evaluate(String name) {
+        assert matchFieldNamesWithDots == false || name.indexOf('.') < 0;
+
+        if (filters != null) {
             List<FilterPath> nextFilters = new ArrayList<>();
-            for (FilterPath filter : filterPaths) {
+            for (FilterPath filter : filters) {
                 boolean matches = filter.matches(name, nextFilters);
                 if (matches) {
                     return MATCHING;
@@ -68,7 +79,11 @@ public class FilterPathBasedFilter extends TokenFilter {
             }
 
             if (nextFilters.isEmpty() == false) {
-                return new FilterPathBasedFilter(nextFilters.toArray(new FilterPath[nextFilters.size()]), inclusive);
+                return new FilterPathBasedFilter(
+                    nextFilters.toArray(new FilterPath[nextFilters.size()]),
+                    inclusive,
+                    matchFieldNamesWithDots
+                );
             }
         }
         return NO_MATCHING;
@@ -76,7 +91,23 @@ public class FilterPathBasedFilter extends TokenFilter {
 
     @Override
     public TokenFilter includeProperty(String name) {
-        TokenFilter filter = evaluate(name, filters);
+        FilterPathBasedFilter filter = this;
+        if (matchFieldNamesWithDots) {
+            int dot = name.indexOf('.');
+            while (dot > 0) {
+                String first = name.substring(0, dot);
+                filter = filter.evaluate(first);
+                if (filter == MATCHING) {
+                    return inclusive ? TokenFilter.INCLUDE_ALL : null;
+                }
+                if (filter == NO_MATCHING) {
+                    return inclusive ? null : TokenFilter.INCLUDE_ALL;
+                }
+                name = name.substring(dot + 1);
+                dot = name.indexOf('.');
+            }
+        }
+        filter = filter.evaluate(name);
         if (filter == MATCHING) {
             return inclusive ? TokenFilter.INCLUDE_ALL : null;
         }

cb3235689aabf50bdd86c7e0b824c9b02447068e
David Turner <david.turner@elastic.co>
2022-01-26 19:22:31 +0000

    Correct context for ClusterConnManager listener (#83035)
    Today `ClusterConnectionManager#connectToNode` completes its listeners
    in the thread context in which the connection completes, which may not
    be the correct context if there are multiple concurrent connection
    attempts. With this commit we make sure to complete each listener in the
    context in which it was passed to the corresponding call to
    `connectToNode`.
    
    Co-authored-by: ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>

--- test/framework/src/main/java/org/elasticsearch/test/transport/MockTransportService.java
+++ test/framework/src/main/java/org/elasticsearch/test/transport/MockTransportService.java
@@ -223,7 +223,7 @@ public final class MockTransportService extends TransportService {
             localNodeFactory,
             clusterSettings,
             taskHeaders,
-            new StubbableConnectionManager(new ClusterConnectionManager(settings, transport))
+            new StubbableConnectionManager(new ClusterConnectionManager(settings, transport, threadPool.getThreadContext()))
         );
         this.original = transport.getDelegate();
     }
--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/action/saml/TransportSamlLogoutActionTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/action/saml/TransportSamlLogoutActionTests.java
@@ -241,7 +241,7 @@ public class TransportSamlLogoutActionTests extends SamlTestCase {
         final TransportService transportService = new TransportService(
             Settings.EMPTY,
             mock(Transport.class),
-            null,
+            threadPool,
             TransportService.NOOP_TRANSPORT_INTERCEPTOR,
             x -> null,
             null,

3ae69479182ab7188a8dc1e0dcf33acc3f4ab248
James Baiera <james.baiera@gmail.com>
2022-01-31 15:59:47 -0500

    Preserve thread context when putting followers

--- x-pack/plugin/ccr/src/main/java/org/elasticsearch/xpack/ccr/action/TransportPutFollowAction.java
+++ x-pack/plugin/ccr/src/main/java/org/elasticsearch/xpack/ccr/action/TransportPutFollowAction.java
@@ -16,6 +16,7 @@ import org.elasticsearch.action.admin.cluster.snapshots.restore.RestoreSnapshotR
 import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.ActiveShardCount;
 import org.elasticsearch.action.support.ActiveShardsObserver;
+import org.elasticsearch.action.support.ContextPreservingActionListener;
 import org.elasticsearch.action.support.master.TransportMasterNodeAction;
 import org.elasticsearch.client.internal.Client;
 import org.elasticsearch.cluster.ClusterState;
@@ -250,7 +251,7 @@ public final class TransportPutFollowAction extends TransportMasterNodeAction<Pu
                 }
             };
         } else {
-            listener = originalListener;
+            listener = new ContextPreservingActionListener<>(threadPool.getThreadContext().newRestorableContext(true), originalListener);
         }
 
         RestoreClusterStateListener.createAndRegisterListener(

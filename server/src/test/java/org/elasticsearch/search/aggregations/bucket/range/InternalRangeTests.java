8cfbb49d5410bbe802d58becced47a225bdabc5f
Salvatore Campagna <93581129+salvatore-campagna@users.noreply.github.com>
2022-02-01 08:59:09 +0100

    [8.0] refactor: use the original from/to values when creating the bucket (#83262) (#83337)
    The range bucket values should always be set to the original `from`
    and `to` values. These values are the ones used by the serialisation
    logic and are the values the client expects in the response.
    
    Also, the key for a Range is made optional, avoiding unnecessary
    serialisation and deserialisation and the actual key generation
    happens only if required (calling  'getKey' or 'getKeyAsString'
    directly or at key serialisation time).
    A Range does not actually need a key to accomplish its purpose ('from'
    and 'to' are the only required parameters), other than propagating a
    user-specified key for the bucket collecting documents for that range.
    
    (cherry picked from commit a03cd922079f55961b28afa8c8a3766924890850)

--- server/src/test/java/org/elasticsearch/search/aggregations/bucket/range/InternalRangeTests.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/bucket/range/InternalRangeTests.java
@@ -76,7 +76,7 @@ public class InternalRangeTests extends InternalRangeTestCase<InternalRange<Inte
             int docCount = randomIntBetween(0, 1000);
             double from = range.v1();
             double to = range.v2();
-            buckets.add(new InternalRange.Bucket("range_" + i, from, from, to, to, docCount, aggregations, keyed, format));
+            buckets.add(new InternalRange.Bucket("range_" + i, from, to, docCount, aggregations, keyed, format));
         }
         return new InternalRange<>(name, buckets, format, keyed, metadata);
     }
@@ -111,17 +111,7 @@ public class InternalRangeTests extends InternalRangeTestCase<InternalRange<Inte
                 double from = randomDouble();
                 double to = from + randomDouble();
                 buckets.add(
-                    new InternalRange.Bucket(
-                        "range_a",
-                        from,
-                        from,
-                        to,
-                        to,
-                        randomNonNegativeLong(),
-                        InternalAggregations.EMPTY,
-                        false,
-                        format
-                    )
+                    new InternalRange.Bucket("range_a", from, to, randomNonNegativeLong(), InternalAggregations.EMPTY, false, format)
                 );
             }
             case 3 -> {

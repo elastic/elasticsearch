16b459fce28fa5177690a4979893d35e8fea2a0c
Przemko Robakowski <przemko.robakowski@elastic.co>
2022-02-01 02:13:53 +0100

    import

--- server/src/main/java/org/elasticsearch/action/admin/indices/rollover/TransportRolloverAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/rollover/TransportRolloverAction.java
@@ -50,7 +50,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**

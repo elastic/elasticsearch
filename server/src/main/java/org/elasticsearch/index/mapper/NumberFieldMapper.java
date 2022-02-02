65368034dabbf5e23ed61070f298864a75350780
Salvatore Campagna <93581129+salvatore-campagna@users.noreply.github.com>
2022-02-02 14:59:13 +0100

    fix: reduce float and half-float values to their stored precision (#83213)
    The bug is a result of reducing precision of float values. If the filed the range aggregation is applied to is a long field, we try to reduce the precision of the rage from and/or to values to long. This results in an exception at parsing time happening when parsing a long with decimal positions.
    
    Here we parse FLOAT and HALF-FLOAT values actually reducing their precision. For other types, including long we do nothing, or better, we do not do any "precision-reduction" action in method reduceToStoredPrecision.
    
    (cherry picked from commit 2d701b99670246724399f37df2fd63299af7d33b)
    
    # Conflicts:
    #	server/src/main/java/org/elasticsearch/index/mapper/NumberFieldMapper.java

--- server/src/main/java/org/elasticsearch/index/mapper/NumberFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/NumberFieldMapper.java
@@ -224,6 +224,11 @@ public class NumberFieldMapper extends FieldMapper {
                 return HalfFloatPoint.sortableShortToHalfFloat(HalfFloatPoint.halfFloatToSortableShort(result));
             }
 
+            @Override
+            public double reduceToStoredPrecision(double value) {
+                return parse(value, false).doubleValue();
+            }
+
             /**
              * Parse a query parameter or {@code _source} value to a float,
              * keeping float precision. Used by queries which need more
@@ -347,6 +352,11 @@ public class NumberFieldMapper extends FieldMapper {
                 }
                 validateParsed(result);
                 return result;
+            }
+
+            @Override
+            public double reduceToStoredPrecision(double value) {
+                return parse(value, false).doubleValue();
             }
 
             @Override
@@ -1053,6 +1063,20 @@ public class NumberFieldMapper extends FieldMapper {
             }
             return builder.apply(l, u);
         }
+
+        public abstract IndexFieldData.Builder getFieldDataBuilder(String name);
+
+        /**
+         * Adjusts a value to the value it would have been had it been parsed by that mapper
+         * and then cast up to a double. This is meant to be an entry point to manipulate values
+         * before the actual value is parsed.
+         *
+         * @param value the value to reduce to the field stored value
+         * @return the double value
+         */
+        public double reduceToStoredPrecision(double value) {
+            return ((Number) value).doubleValue();
+        }
     }
 
     public static class NumberFieldType extends SimpleMappedFieldType {
@@ -1123,7 +1147,7 @@ public class NumberFieldMapper extends FieldMapper {
                 // Trying to parse infinite values into ints/longs throws. Understandably.
                 return value;
             }
-            return type.parse(value, false).doubleValue();
+            return type.reduceToStoredPrecision(value);
         }
 
         public NumericType numericType() {

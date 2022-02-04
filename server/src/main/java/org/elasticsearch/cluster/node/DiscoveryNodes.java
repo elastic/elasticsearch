9af11d6a9c52da90ea98b95cbddfde9d272f2d39
Artem Prigoda <artem.prigoda@elastic.co>
2022-02-04 11:33:15 +0100

    Remove unused import

--- server/src/main/java/org/elasticsearch/cluster/node/DiscoveryNodes.java
+++ server/src/main/java/org/elasticsearch/cluster/node/DiscoveryNodes.java
@@ -33,7 +33,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.Spliterator;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;

c51edfb0d1132e09e6202743559be87e55427a10
Martijn van Groningen <martijn.v.groningen@gmail.com>
2022-02-08 18:12:18 +0100

    it is ok if no data streams module is registered

--- test/framework/src/main/java/org/elasticsearch/test/ESSingleNodeTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/ESSingleNodeTestCase.java
@@ -132,7 +132,15 @@ public abstract class ESSingleNodeTestCase extends ESTestCase {
         super.tearDown();
         var deleteDataStreamsRequest = new DeleteDataStreamAction.Request("*");
         deleteDataStreamsRequest.indicesOptions(IndicesOptions.LENIENT_EXPAND_OPEN_CLOSED_HIDDEN);
-        assertAcked(client().execute(DeleteDataStreamAction.INSTANCE, deleteDataStreamsRequest).actionGet());
+        try {
+            assertAcked(client().execute(DeleteDataStreamAction.INSTANCE, deleteDataStreamsRequest).actionGet());
+        } catch (IllegalStateException e) {
+            // Ignore if action isn't registered, because data streams is a module and
+            // if the delete action isn't registered then there no data streams to delete.
+            if (e.getMessage().startsWith("failed to find action") == false) {
+                throw e;
+            }
+        }
         var deleteComposableIndexTemplateRequest = new DeleteComposableIndexTemplateAction.Request("*");
         assertAcked(client().execute(DeleteComposableIndexTemplateAction.INSTANCE, deleteComposableIndexTemplateRequest).actionGet());
         var deleteComponentTemplateRequest = new DeleteComponentTemplateAction.Request("*");

299882912ecc5dc8336fada10fc1f632a5b22b9b
Armin Braun <me@obrown.io>
2022-02-09 14:12:52 +0100

    Speed up ContentPath
    This class shows up as 2%+ on some write threads in many-shards benchmarks
    during indexing. We can at laeast make it a little faster by short-circuiting
    the empty path array case, reusing the string builder as thread-local and
    removing the redundant `offset` field that's always `0` (the last one probably
    won't do much for performance though).

--- server/src/main/java/org/elasticsearch/index/mapper/ContentPath.java
+++ server/src/main/java/org/elasticsearch/index/mapper/ContentPath.java
@@ -8,46 +8,34 @@
 
 package org.elasticsearch.index.mapper;
 
+import org.elasticsearch.common.Strings;
+
 public final class ContentPath {
 
     private static final char DELIMITER = '.';
 
-    private final StringBuilder sb;
+    private static final ThreadLocal<StringBuilder> stringBuilder = ThreadLocal.withInitial(StringBuilder::new);
 
-    private final int offset;
+    private int index;
 
-    private int index = 0;
-
-    private String[] path = new String[10];
+    private String[] path = Strings.EMPTY_ARRAY;
 
     public ContentPath() {
-        this(0);
-    }
-
-    /**
-     * Constructs a json path with an offset. The offset will result an {@code offset}
-     * number of path elements to not be included in {@link #pathAsText(String)}.
-     */
-    public ContentPath(int offset) {
-        this.sb = new StringBuilder();
-        this.offset = offset;
         this.index = 0;
     }
 
     public ContentPath(String path) {
-        this.sb = new StringBuilder();
-        this.offset = 0;
         this.index = 0;
         add(path);
     }
 
     public void add(String name) {
-        path[index++] = name;
         if (index == path.length) { // expand if needed
             String[] newPath = new String[path.length + 10];
             System.arraycopy(path, 0, newPath, 0, path.length);
             path = newPath;
         }
+        path[index++] = name;
     }
 
     public void remove() {
@@ -55,8 +43,12 @@ public final class ContentPath {
     }
 
     public String pathAsText(String name) {
+        if (index == 0) {
+            return name;
+        }
+        final StringBuilder sb = stringBuilder.get();
         sb.setLength(0);
-        for (int i = offset; i < index; i++) {
+        for (int i = 0; i < index; i++) {
             sb.append(path[i]).append(DELIMITER);
         }
         sb.append(name);

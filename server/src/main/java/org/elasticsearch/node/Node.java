15c0254a8fa0ce91c9e66a7b0e138372026be4f2
Artem Prigoda <arteamon@gmail.com>
2022-02-10 17:12:28 +0100

    Revert "Add "Has eligible master" Indicator for Health API (#82874)"
    This reverts commit 7ce47613c3aae57b55ea824cc9a49b18747b6b3b.

--- server/src/main/java/org/elasticsearch/node/Node.java
+++ server/src/main/java/org/elasticsearch/node/Node.java
@@ -41,7 +41,6 @@ import org.elasticsearch.cluster.NodeConnectionsService;
 import org.elasticsearch.cluster.action.index.MappingUpdatedAction;
 import org.elasticsearch.cluster.coordination.Coordinator;
 import org.elasticsearch.cluster.coordination.InstanceHasMasterHealthIndicatorService;
-import org.elasticsearch.cluster.coordination.indicators.HasEligibleMasterNodeIndicator;
 import org.elasticsearch.cluster.desirednodes.DesiredNodesSettingsValidator;
 import org.elasticsearch.cluster.metadata.IndexMetadataVerifier;
 import org.elasticsearch.cluster.metadata.IndexTemplateMetadata;
@@ -902,8 +901,7 @@ public class Node implements Closeable {
             );
 
             List<HealthIndicatorService> serverHealthIndicatorServices = List.of(
-                new InstanceHasMasterHealthIndicatorService(clusterService),
-                new HasEligibleMasterNodeIndicator(clusterService)
+                new InstanceHasMasterHealthIndicatorService(clusterService)
             );
             List<HealthIndicatorService> pluginHealthIndicatorServices = pluginsService.filterPlugins(HealthPlugin.class)
                 .stream()

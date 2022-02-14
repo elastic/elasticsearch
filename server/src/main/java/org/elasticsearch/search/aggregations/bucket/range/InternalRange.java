8f994c25bcc49438646d1ce2c52300bfecafc7cf
Salvatore Campagna <93581129+salvatore-campagna@users.noreply.github.com>
2022-02-14 17:59:05 +0100

    fix: add a note explaining bucket key requirements

--- server/src/main/java/org/elasticsearch/search/aggregations/bucket/range/InternalRange.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/bucket/range/InternalRange.java
@@ -267,7 +267,9 @@ public class InternalRange<B extends InternalRange.Bucket, R extends InternalRan
         int size = in.readVInt();
         List<B> ranges = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
-            String key = in.getVersion().equals(Version.V_8_0_0) ? in.readString()
+            // NOTE: the key is required in version == 8.0.0 and version <= 7.17.0,
+            // while it is optional in for all subsequent versions.
+            final String key = in.getVersion().equals(Version.V_8_0_0) ? in.readString()
                 : in.getVersion().onOrAfter(Version.V_7_17_1) ? in.readOptionalString()
                 : in.readString();
             double from = in.readDouble();

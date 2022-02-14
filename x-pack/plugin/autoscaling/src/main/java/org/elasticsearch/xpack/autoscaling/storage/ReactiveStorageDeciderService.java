656111d31026ebc161995343e48fbf1aea2d8554
Martijn van Groningen <martijn.v.groningen@gmail.com>
2022-02-14 13:33:08 +0100

    no need to take index_mode into account in auto scaling

--- x-pack/plugin/autoscaling/src/main/java/org/elasticsearch/xpack/autoscaling/storage/ReactiveStorageDeciderService.java
+++ x-pack/plugin/autoscaling/src/main/java/org/elasticsearch/xpack/autoscaling/storage/ReactiveStorageDeciderService.java
@@ -15,7 +15,6 @@ import org.elasticsearch.cluster.metadata.DataStreamMetadata;
 import org.elasticsearch.cluster.metadata.IndexAbstraction;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.Metadata;
-import org.elasticsearch.cluster.metadata.MetadataIndexTemplateService;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.node.DiscoveryNodeFilters;
 import org.elasticsearch.cluster.node.DiscoveryNodeRole;
@@ -44,7 +43,6 @@ import org.elasticsearch.common.unit.ByteSizeUnit;
 import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.core.Tuple;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.index.IndexMode;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.snapshots.SnapshotShardSizeInfo;
 import org.elasticsearch.xcontent.XContentBuilder;
@@ -554,9 +552,6 @@ public class ReactiveStorageDeciderService implements AutoscalingDeciderService 
             }
 
             IndexMetadata writeIndex = metadata.index(stream.getWriteIndex());
-            var templateV2Name = MetadataIndexTemplateService.findV2Template(state.metadata(), stream.getName(), false);
-            var templateV2 = metadata.templatesV2().get(templateV2Name);
-            IndexMode indexModeFromTemplate = templateV2.getDataStreamTemplate().getIndexMode();
 
             Map<IndexMetadata, Long> newIndices = new HashMap<>();
             DataStream dataStream = stream.getDataStream();
@@ -566,7 +561,7 @@ public class ReactiveStorageDeciderService implements AutoscalingDeciderService 
                 dataStream = dataStream.unsafeRollover(
                     new Index(rolledDataStreamInfo.v1(), uuid),
                     rolledDataStreamInfo.v2(),
-                    indexModeFromTemplate
+                    null
                 );
 
                 // this unintentionally copies the in-sync allocation ids too. This has the fortunate effect of these indices

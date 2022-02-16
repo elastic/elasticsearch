dbcf4e69f939cf31d58fed82f5c94eefa69f0881
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2022-02-16 15:44:05 +0100

    Unused import

--- server/src/main/java/org/elasticsearch/cluster/metadata/DesiredNodes.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/DesiredNodes.java
@@ -10,7 +10,6 @@ package org.elasticsearch.cluster.metadata;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.Version;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.common.io.stream.StreamInput;

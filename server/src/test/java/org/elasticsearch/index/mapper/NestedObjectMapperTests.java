c9cf46dd47eecccf853a32965bf2128885f61d90
weizijun <weizijun1989@gmail.com>
2022-02-17 01:06:24 +0800

    fixup

--- server/src/test/java/org/elasticsearch/index/mapper/NestedObjectMapperTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/NestedObjectMapperTests.java
@@ -1390,7 +1390,7 @@ public class NestedObjectMapperTests extends MapperServiceTestCase {
                     b.startObject("properties");
                     {
                         b.startObject("foo")
-                            .field("type", randomFrom(List.of("keyword", "ip", "long", "double", "unsigned_long")))
+                            .field("type", randomFrom(List.of("keyword", "ip", "long", "short", "integer", "byte")))
                             .field("time_series_dimension", true)
                             .endObject();
                     }
@@ -1414,7 +1414,7 @@ public class NestedObjectMapperTests extends MapperServiceTestCase {
                             b.startObject("properties");
                             {
                                 b.startObject("foo")
-                                    .field("type", randomFrom(List.of("keyword", "ip", "long", "short", "integer", "byte", "unsigned_long")))
+                                    .field("type", randomFrom(List.of("keyword", "ip", "long", "short", "integer", "byte")))
                                     .field("time_series_dimension", true)
                                     .endObject();
                             }

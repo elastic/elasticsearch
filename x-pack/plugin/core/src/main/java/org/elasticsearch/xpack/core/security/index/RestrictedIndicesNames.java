1861e8a19eabf6ab5d67657c36fba7b7698a97e0
Tim Vernum <tim@adjective.org>
2022-02-22 15:54:11 +1100

    Use same system index pattern in restricted names (#84180)
    The index pattern that was used for the ".security" system index was
    not identical to the pattern used in RestrictedIndices. The
    consequence was that it would be possible for a user without
    restricted indices access to create an index that would get caught by
    the system indices pattern, which could lead to confusion.
    
    In 8.0 all system indices are automatically restricted, using the
    index name pattern from the system index descriptor, so in 7.17 we are
    changing the restricted index name to cover the same set of names as
    the system index descriptor

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/index/RestrictedIndicesNames.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/index/RestrictedIndicesNames.java
@@ -7,13 +7,17 @@
 
 package org.elasticsearch.xpack.core.security.index;
 
+import org.apache.lucene.util.automaton.Automata;
 import org.apache.lucene.util.automaton.Automaton;
+import org.apache.lucene.util.automaton.Operations;
 import org.elasticsearch.common.util.set.Sets;
+import org.elasticsearch.core.List;
 import org.elasticsearch.xpack.core.security.support.Automatons;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
+import java.util.function.Predicate;
 
 public final class RestrictedIndicesNames {
     public static final String INTERNAL_SECURITY_MAIN_INDEX_6 = ".security-6";
@@ -22,6 +26,17 @@ public final class RestrictedIndicesNames {
 
     public static final String INTERNAL_SECURITY_TOKENS_INDEX_7 = ".security-tokens-7";
     public static final String SECURITY_TOKENS_ALIAS = ".security-tokens";
+
+    // See o.e.x.security.Security#getSecurityMainIndexDescriptor, o.e.x.security.Security#getSecurityTokensIndexDescriptor
+    private static final Automaton SECURITY_INDEX_AUTOMATON = Operations.concatenate(
+        List.of(
+            Operations.union(Automata.makeString(SECURITY_MAIN_ALIAS), Automata.makeString(SECURITY_TOKENS_ALIAS)),
+            Automata.makeChar('-'),
+            Automata.makeCharRange('0', '9'),
+            Automata.makeAnyString()
+        )
+    );
+    private static final Predicate<String> SECURITY_INDEX_PREDICATE = Automatons.predicate(SECURITY_INDEX_AUTOMATON);
 
     // public for tests
     public static final String ASYNC_SEARCH_PREFIX = ".async-search";
@@ -39,11 +54,13 @@ public final class RestrictedIndicesNames {
     );
 
     public static boolean isRestricted(String concreteIndexName) {
-        return RESTRICTED_NAMES.contains(concreteIndexName) || concreteIndexName.startsWith(ASYNC_SEARCH_PREFIX);
+        return RESTRICTED_NAMES.contains(concreteIndexName)
+            || concreteIndexName.startsWith(ASYNC_SEARCH_PREFIX)
+            || SECURITY_INDEX_PREDICATE.test(concreteIndexName);
     }
 
     public static final Automaton NAMES_AUTOMATON = Automatons.unionAndMinimize(
-        Arrays.asList(Automatons.patterns(RESTRICTED_NAMES), ASYNC_SEARCH_AUTOMATON)
+        Arrays.asList(Automatons.patterns(RESTRICTED_NAMES), ASYNC_SEARCH_AUTOMATON, SECURITY_INDEX_AUTOMATON)
     );
 
     private RestrictedIndicesNames() {}

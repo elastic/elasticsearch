11423661d4ca8eb7372c89f79214af28c008b2d9
David Turner <david.turner@elastic.co>
2022-02-27 09:20:44 +0000

    Migrate implementation in ClusterStateTaskExecutorTests

--- server/src/test/java/org/elasticsearch/cluster/ClusterStateTaskExecutorTests.java
+++ server/src/test/java/org/elasticsearch/cluster/ClusterStateTaskExecutorTests.java
@@ -36,7 +36,7 @@ public class ClusterStateTaskExecutorTests extends ESTestCase {
     public void testDescribeTasks() {
         final ClusterStateTaskExecutor<TestTask> executor = new ClusterStateTaskExecutor<>() {
             @Override
-            public ClusterTasksResult<TestTask> execute(ClusterState currentState, List<TestTask> tasks) {
+            public ClusterState executeInContext(ClusterState currentState, List<TaskContext<TestTask>> taskContexts) {
                 throw new AssertionError("should not be called");
             }
         };

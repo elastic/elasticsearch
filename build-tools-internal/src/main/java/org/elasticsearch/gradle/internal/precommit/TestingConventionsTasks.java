6b9dc1334f50c3d795136d2df6771613fbd069c9
Rene Groeschke <rene@elastic.co>
2022-03-03 18:16:29 +0100

    Minor polish of TestingConventionsTasks and plugin (#84320) (#84611)
    We can build a named object container within a task using the objectfactory.
    This makes constructing the task way simpler and does not require us to
    inject the container from the constructing plugin

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/precommit/TestingConventionsTasks.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/precommit/TestingConventionsTasks.java
@@ -15,6 +15,7 @@ import org.gradle.api.Task;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.FileTree;
 import org.gradle.api.file.ProjectLayout;
+import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.tasks.Classpath;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.OutputFile;
@@ -62,8 +63,9 @@ public class TestingConventionsTasks extends DefaultTask {
     private TaskCollection<Test> testTasks;
 
     @Inject
-    public TestingConventionsTasks(ProjectLayout projectLayout) {
+    public TestingConventionsTasks(ProjectLayout projectLayout, ObjectFactory objectFactory) {
         this.projectLayout = projectLayout;
+        this.naming = objectFactory.domainObjectContainer(TestingConventionRule.class);
         setDescription("Tests various testing conventions");
     }
 
@@ -416,9 +418,5 @@ public class TestingConventionsTasks extends DefaultTask {
 
     public void setSourceSets(SourceSetContainer sourceSets) {
         this.sourceSets = sourceSets;
-    }
-
-    public void setNaming(NamedDomainObjectContainer<TestingConventionRule> naming) {
-        this.naming = naming;
     }
 }

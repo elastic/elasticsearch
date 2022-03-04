9fbc6c7f13f5f0621274d7aa11b1eb5e3a00448e
Ryan Ernst <ryan@iernst.net>
2022-03-04 07:23:38 -0800

    checkstyle

--- server/src/main/java/org/elasticsearch/common/collect/ImmutableOpenMap.java
+++ server/src/main/java/org/elasticsearch/common/collect/ImmutableOpenMap.java
@@ -31,7 +31,6 @@ import java.util.Set;
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 

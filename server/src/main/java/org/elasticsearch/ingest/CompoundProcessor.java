7d6977a1acfa7149826feab83cb7bb519116a416
Dan Hermann <dan.hermann@elastic.co>
2022-03-04 16:19:17 -0600

    checkstyle

--- server/src/main/java/org/elasticsearch/ingest/CompoundProcessor.java
+++ server/src/main/java/org/elasticsearch/ingest/CompoundProcessor.java
@@ -16,7 +16,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.function.LongSupplier;
 import java.util.stream.Collectors;

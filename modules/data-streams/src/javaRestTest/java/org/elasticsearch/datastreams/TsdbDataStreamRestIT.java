75126f7c18f10c994f478e42fbea4339f419ee2e
Martijn van Groningen <martijn.v.groningen@gmail.com>
2022-03-07 13:28:19 +0100

    TSDB: Truncate timestamps for routing purposes only
    Truncate timestamp to second precision for resolving data stream to backing index only.
    Also add support for parsing timestamps in date nanos format.
    
    Closes #83517

--- modules/data-streams/src/javaRestTest/java/org/elasticsearch/datastreams/TsdbDataStreamRestIT.java
+++ modules/data-streams/src/javaRestTest/java/org/elasticsearch/datastreams/TsdbDataStreamRestIT.java
@@ -243,6 +243,92 @@ public class TsdbDataStreamRestIT extends ESRestTestCase {
         assertThat(entityAsMap(response).get("_index"), equalTo(secondBackingIndex));
     }
 
+    public void testTsdbDataStreamsNanos() throws Exception {
+        // Create a template
+        var putComposableIndexTemplateRequest = new Request("POST", "/_index_template/1");
+        putComposableIndexTemplateRequest.setJsonEntity(TEMPLATE.replace("date", "date_nanos"));
+        assertOK(client().performRequest(putComposableIndexTemplateRequest));
+
+        var bulkRequest = new Request("POST", "/k8s/_bulk");
+        bulkRequest.setJsonEntity(
+            """
+                {"create": {}}
+                {"@timestamp": "$now", "metricset": "pod", "k8s": {"pod": {"name": "cat", "uid":"947e4ced-1786-4e53-9e0c-5c447e959507", "ip": "10.10.55.1", "network": {"tx": 2001818691, "rx": 802133794}}}}
+                {"create": {}}
+                {"@timestamp": "$now", "metricset": "pod", "k8s": {"pod": {"name": "cat", "uid":"947e4ced-1786-4e53-9e0c-5c447e959507", "ip": "10.10.55.1", "network": {"tx": 2005177954, "rx": 801479970}}}}
+                {"create": {}}
+                {"@timestamp": "$now", "metricset": "pod", "k8s": {"pod": {"name": "cat", "uid":"947e4ced-1786-4e53-9e0c-5c447e959507", "ip": "10.10.55.1", "network": {"tx": 2006223737, "rx": 802337279}}}}
+                {"create": {}}
+                {"@timestamp": "$now", "metricset": "pod", "k8s": {"pod": {"name": "cat", "uid":"947e4ced-1786-4e53-9e0c-5c447e959507", "ip": "10.10.55.2", "network": {"tx": 2012916202, "rx": 803685721}}}}
+                {"create": {}}
+                {"@timestamp": "$now", "metricset": "pod", "k8s": {"pod": {"name": "dog", "uid":"df3145b3-0563-4d3b-a0f7-897eb2876ea9", "ip": "10.10.55.3", "network": {"tx": 1434521831, "rx": 530575198}}}}
+                {"create": {}}
+                {"@timestamp": "$now", "metricset": "pod", "k8s": {"pod": {"name": "dog", "uid":"df3145b3-0563-4d3b-a0f7-897eb2876ea9", "ip": "10.10.55.3", "network": {"tx": 1434577921, "rx": 530600088}}}}
+                {"create": {}}
+                {"@timestamp": "$now", "metricset": "pod", "k8s": {"pod": {"name": "dog", "uid":"df3145b3-0563-4d3b-a0f7-897eb2876ea9", "ip": "10.10.55.3", "network": {"tx": 1434587694, "rx": 530604797}}}}
+                {"create": {}}
+                {"@timestamp": "$now", "metricset": "pod", "k8s": {"pod": {"name": "dog", "uid":"df3145b3-0563-4d3b-a0f7-897eb2876ea9", "ip": "10.10.55.3", "network": {"tx": 1434595272, "rx": 530605511}}}}
+                """
+                .replace("$now", formatInstantNanos(Instant.now()))
+        );
+        bulkRequest.addParameter("refresh", "true");
+        assertOK(client().performRequest(bulkRequest));
+
+        var getDataStreamsRequest = new Request("GET", "/_data_stream");
+        var response = client().performRequest(getDataStreamsRequest);
+        assertOK(response);
+        var dataStreams = entityAsMap(response);
+        assertThat(ObjectPath.evaluate(dataStreams, "data_streams"), hasSize(1));
+        assertThat(ObjectPath.evaluate(dataStreams, "data_streams.0.name"), equalTo("k8s"));
+        assertThat(ObjectPath.evaluate(dataStreams, "data_streams.0.generation"), equalTo(1));
+        assertThat(ObjectPath.evaluate(dataStreams, "data_streams.0.template"), equalTo("1"));
+        assertThat(ObjectPath.evaluate(dataStreams, "data_streams.0.indices"), hasSize(1));
+        String firstBackingIndex = ObjectPath.evaluate(dataStreams, "data_streams.0.indices.0.index_name");
+        assertThat(firstBackingIndex, backingIndexEqualTo("k8s", 1));
+
+        var indices = getIndex(firstBackingIndex);
+        var escapedBackingIndex = firstBackingIndex.replace(".", "\\.");
+        assertThat(ObjectPath.evaluate(indices, escapedBackingIndex + ".data_stream"), equalTo("k8s"));
+        assertThat(ObjectPath.evaluate(indices, escapedBackingIndex + ".settings.index.mode"), equalTo("time_series"));
+        String startTimeFirstBackingIndex = ObjectPath.evaluate(indices, escapedBackingIndex + ".settings.index.time_series.start_time");
+        assertThat(startTimeFirstBackingIndex, notNullValue());
+        String endTimeFirstBackingIndex = ObjectPath.evaluate(indices, escapedBackingIndex + ".settings.index.time_series.end_time");
+        assertThat(endTimeFirstBackingIndex, notNullValue());
+
+        var rolloverRequest = new Request("POST", "/k8s/_rollover");
+        assertOK(client().performRequest(rolloverRequest));
+
+        response = client().performRequest(getDataStreamsRequest);
+        assertOK(response);
+        dataStreams = entityAsMap(response);
+        assertThat(ObjectPath.evaluate(dataStreams, "data_streams.0.name"), equalTo("k8s"));
+        assertThat(ObjectPath.evaluate(dataStreams, "data_streams.0.generation"), equalTo(2));
+        String secondBackingIndex = ObjectPath.evaluate(dataStreams, "data_streams.0.indices.1.index_name");
+        assertThat(secondBackingIndex, backingIndexEqualTo("k8s", 2));
+
+        indices = getIndex(secondBackingIndex);
+        escapedBackingIndex = secondBackingIndex.replace(".", "\\.");
+        assertThat(ObjectPath.evaluate(indices, escapedBackingIndex + ".data_stream"), equalTo("k8s"));
+        String startTimeSecondBackingIndex = ObjectPath.evaluate(indices, escapedBackingIndex + ".settings.index.time_series.start_time");
+        assertThat(startTimeSecondBackingIndex, equalTo(endTimeFirstBackingIndex));
+        String endTimeSecondBackingIndex = ObjectPath.evaluate(indices, escapedBackingIndex + ".settings.index.time_series.end_time");
+        assertThat(endTimeSecondBackingIndex, notNullValue());
+
+        var indexRequest = new Request("POST", "/k8s/_doc");
+        Instant time = parseInstant(startTimeFirstBackingIndex);
+        indexRequest.setJsonEntity(DOC.replace("$time", formatInstantNanos(time)));
+        response = client().performRequest(indexRequest);
+        assertOK(response);
+        assertThat(entityAsMap(response).get("_index"), equalTo(firstBackingIndex));
+
+        indexRequest = new Request("POST", "/k8s/_doc");
+        time = parseInstant(endTimeSecondBackingIndex).minusMillis(1);
+        indexRequest.setJsonEntity(DOC.replace("$time", formatInstantNanos(time)));
+        response = client().performRequest(indexRequest);
+        assertOK(response);
+        assertThat(entityAsMap(response).get("_index"), equalTo(secondBackingIndex));
+    }
+
     public void testSimulateTsdbDataStreamTemplate() throws Exception {
         var putComposableIndexTemplateRequest = new Request("POST", "/_index_template/1");
         putComposableIndexTemplateRequest.setJsonEntity(TEMPLATE);
@@ -418,6 +504,10 @@ public class TsdbDataStreamRestIT extends ESRestTestCase {
 
     static String formatInstant(Instant instant) {
         return DateFormatter.forPattern(FormatNames.STRICT_DATE_OPTIONAL_TIME.getName()).format(instant);
+    }
+
+    static String formatInstantNanos(Instant instant) {
+        return DateFormatter.forPattern(FormatNames.STRICT_DATE_OPTIONAL_TIME_NANOS.getName()).format(instant);
     }
 
     static Instant parseInstant(String input) {

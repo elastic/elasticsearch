55b5663d8aa17ffea5111aa2a2245b75498e2406
Nhat Nguyen <nhat.nguyen@elastic.co>
2022-03-08 16:22:46 -0500

    Relax error handling in transport disk usage API (#84774)
    Relax the failure handling to cover other exceptions such as
    BroadcastShardOperationFailedException.

--- server/src/main/java/org/elasticsearch/action/admin/indices/diskusage/TransportAnalyzeIndexDiskUsageAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/diskusage/TransportAnalyzeIndexDiskUsageAction.java
@@ -8,6 +8,7 @@
 
 package org.elasticsearch.action.admin.indices.diskusage;
 
+import org.elasticsearch.ExceptionsHelper;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.NoShardAvailableActionException;
 import org.elasticsearch.action.support.ActionFilters;
@@ -112,6 +113,8 @@ public class TransportAnalyzeIndexDiskUsageAction extends TransportBroadcastActi
                 combined.compute(resp.getIndex(), (k, v) -> v == null ? resp.stats : v.add(resp.stats));
             } else if (r instanceof DefaultShardOperationFailedException) {
                 shardFailures.add((DefaultShardOperationFailedException) r);
+            } else if (r instanceof Exception) {
+                shardFailures.add(new DefaultShardOperationFailedException(ExceptionsHelper.convertToElastic((Exception) r)));
             } else {
                 assert false : "unknown response [" + r + "]";
                 throw new IllegalStateException("unknown response [" + r + "]");

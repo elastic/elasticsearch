443be217ea100092866160ffe2725220cd43c2ee
Ryan Ernst <ryan@iernst.net>
2022-03-09 06:09:41 -0800

    spotless

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/watcher/common/stats/Counters.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/watcher/common/stats/Counters.java
@@ -6,8 +6,6 @@
  */
 package org.elasticsearch.xpack.core.watcher.common.stats;
 
-import com.carrotsearch.hppc.cursors.ObjectLongCursor;
-
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;

ec0d85636d4c2d72bf5474b27ccfb1eae4d0c450
Nhat Nguyen <nhat.nguyen@elastic.co>
2022-03-09 09:17:14 -0500

    Feedback

--- server/src/test/java/org/elasticsearch/action/fieldcaps/FieldCapabilitiesIndexResponseTests.java
+++ server/src/test/java/org/elasticsearch/action/fieldcaps/FieldCapabilitiesIndexResponseTests.java
@@ -10,14 +10,15 @@ package org.elasticsearch.action.fieldcaps;
 
 import org.elasticsearch.Version;
 import org.elasticsearch.common.Randomness;
-import org.elasticsearch.common.UUIDs;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.util.CollectionUtils;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.VersionUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -50,114 +51,180 @@ public class FieldCapabilitiesIndexResponseTests extends ESTestCase {
         return fieldCaps;
     }
 
-    public void testShareResponsesUsingMappingHash() throws Exception {
-        final List<FieldCapabilitiesIndexResponse> inList = new ArrayList<>();
-        int numGroups = randomIntBetween(0, 20);
+    private Map<String, List<String>> randomMappingHashToIndices() {
         Map<String, List<String>> mappingHashToIndices = new HashMap<>();
-        for (int i = 0; i < numGroups; i++) {
-            String groupName = "group_" + i;
-            String hashing = UUIDs.randomBase64UUID();
-            List<String> indices = IntStream.range(0, randomIntBetween(1, 5)).mapToObj(n -> groupName + "_" + n).toList();
-            mappingHashToIndices.put(hashing, indices);
+        int numGroups = between(0, 10);
+        for (int g = 0; g < numGroups; g++) {
+            String mappingHash = "mapping_hash_" + g;
+            String group = "group_" + g;
+            List<String> indices = IntStream.range(0, between(1, 10)).mapToObj(n -> group + "_index_" + n).toList();
+            mappingHashToIndices.put(mappingHash, indices);
+        }
+        return mappingHashToIndices;
+    }
+
+    private List<FieldCapabilitiesIndexResponse> randomIndexResponses(Map<String, List<String>> mappingHashToIndices) {
+        final List<FieldCapabilitiesIndexResponse> responses = new ArrayList<>();
+        for (Map.Entry<String, List<String>> e : mappingHashToIndices.entrySet()) {
             Map<String, IndexFieldCapabilities> fieldCaps = randomFieldCaps();
-            for (String index : indices) {
-                inList.add(new FieldCapabilitiesIndexResponse(index, hashing, fieldCaps, true));
+            String mappingHash = e.getKey();
+            for (String index : e.getValue()) {
+                responses.add(new FieldCapabilitiesIndexResponse(index, mappingHash, fieldCaps, true));
             }
         }
-        int numUngroups = randomIntBetween(0, 5);
-        for (int i = 0; i < numUngroups; i++) {
-            String index = "ungrouped_" + i;
-            final String hashing;
-            final boolean canMatch;
-            Map<String, IndexFieldCapabilities> fieldCaps = Map.of();
-            if (randomBoolean()) {
-                canMatch = false;
-                hashing = UUIDs.randomBase64UUID();
-            } else {
-                canMatch = randomBoolean();
-                hashing = null;
-                if (canMatch) {
-                    fieldCaps = randomFieldCaps();
-                }
-            }
-            inList.add(new FieldCapabilitiesIndexResponse(index, hashing, fieldCaps, canMatch));
+        return responses;
+    }
+
+    private List<FieldCapabilitiesIndexResponse> randomIndexResponsesWithoutMappingHash() {
+        final List<FieldCapabilitiesIndexResponse> responses = new ArrayList<>();
+        int numIndices = between(0, 10);
+        for (int i = 0; i < numIndices; i++) {
+            String index = "index_without_mapping_hash_" + i;
+            responses.add(new FieldCapabilitiesIndexResponse(index, null, randomFieldCaps(), randomBoolean()));
         }
-        Randomness.shuffle(inList);
-        final List<FieldCapabilitiesIndexResponse> serializedList;
-        final Version newVersion = VersionUtils.randomVersionBetween(random(), Version.V_8_2_0, Version.CURRENT);
-        try (BytesStreamOutput output = new BytesStreamOutput()) {
-            output.setVersion(newVersion);
-            FieldCapabilitiesIndexResponse.writeList(output, inList);
-            try (StreamInput in = output.bytes().streamInput()) {
-                in.setVersion(newVersion);
-                serializedList = FieldCapabilitiesIndexResponse.readList(in);
-            }
-        }
-        assertThat(
-            serializedList.stream().sorted(Comparator.comparing(FieldCapabilitiesIndexResponse::getIndexName)).toList(),
-            equalTo(inList.stream().sorted(Comparator.comparing(FieldCapabilitiesIndexResponse::getIndexName)).toList())
+        return responses;
+    }
+
+    public void testWriteResponsesBetweenNewNodes() throws Exception {
+        Map<String, List<String>> mappingHashToIndices = randomMappingHashToIndices();
+        List<FieldCapabilitiesIndexResponse> responseList = CollectionUtils.concatLists(
+            randomIndexResponses(mappingHashToIndices),
+            randomIndexResponsesWithoutMappingHash()
         );
-        Map<String, List<FieldCapabilitiesIndexResponse>> groupedResponses = serializedList.stream()
-            .filter(r -> r.canMatch() && r.getIndexMappingHash() != null)
-            .collect(Collectors.groupingBy(FieldCapabilitiesIndexResponse::getIndexMappingHash));
-        assertThat(groupedResponses.keySet(), equalTo(mappingHashToIndices.keySet()));
-        for (Map.Entry<String, List<FieldCapabilitiesIndexResponse>> e : groupedResponses.entrySet()) {
-            List<String> indices = mappingHashToIndices.get(e.getKey());
-            List<FieldCapabilitiesIndexResponse> rs = e.getValue();
-            assertThat(rs.stream().map(FieldCapabilitiesIndexResponse::getIndexName).sorted().toList(), equalTo(indices));
-            for (FieldCapabilitiesIndexResponse r : rs) {
-                assertTrue(r.canMatch());
-                assertSame(r.get(), rs.get(0).get());
-            }
+        assertSerializeOnNewNodes(responseList, mappingHashToIndices);
+        final Version minCompactVersion = Version.CURRENT.minimumCompatibilityVersion();
+        if (minCompactVersion.before(Version.V_8_2_0)) {
+            assertSerializeOnOldNodes(responseList, minCompactVersion);
         }
     }
 
-    public void testWriteResponsesOldNodes() throws IOException {
+    public void testWriteResponsesBetweenOldNodes() throws IOException {
         final Version minCompactVersion = Version.CURRENT.minimumCompatibilityVersion();
         assumeTrue("Write list with mapping hash is introduced in 8.2", minCompactVersion.before(Version.V_8_2_0));
-        final List<FieldCapabilitiesIndexResponse> inList = new ArrayList<>();
-        final int numResponses = randomIntBetween(0, 20);
-        final Map<String, Map<String, IndexFieldCapabilities>> mappingHashToFieldResponses = new HashMap<>();
-        for (int i = 0; i < numResponses; i++) {
-            final String index = "index_" + i;
-            final String hashing;
-            final Map<String, IndexFieldCapabilities> fieldCaps;
-            final boolean canMatch;
-            if (randomBoolean()) {
-                hashing = Integer.toString(between(1, 10));
-                canMatch = true;
-                fieldCaps = mappingHashToFieldResponses.computeIfAbsent(hashing, k -> randomFieldCaps());
-            } else {
-                hashing = null;
-                canMatch = randomBoolean();
-                fieldCaps = randomFieldCaps();
-            }
-            inList.add(new FieldCapabilitiesIndexResponse(index, hashing, fieldCaps, canMatch));
-        }
-        Randomness.shuffle(inList);
-
-        final Version oldVersion = VersionUtils.randomVersionBetween(
-            random(),
-            minCompactVersion,
-            VersionUtils.getPreviousVersion(Version.V_8_2_0)
+        Map<String, List<String>> mappingHashToIndices = randomMappingHashToIndices();
+        List<FieldCapabilitiesIndexResponse> responseList = CollectionUtils.concatLists(
+            randomIndexResponses(mappingHashToIndices),
+            randomIndexResponsesWithoutMappingHash()
         );
-        final List<FieldCapabilitiesIndexResponse> serializedList;
-        try (BytesStreamOutput output = new BytesStreamOutput()) {
-            output.setVersion(oldVersion);
-            FieldCapabilitiesIndexResponse.writeList(output, inList);
-            try (StreamInput in = output.bytes().streamInput()) {
-                in.setVersion(oldVersion);
-                serializedList = FieldCapabilitiesIndexResponse.readList(in);
-            }
+        assertSerializeOnOldNodes(responseList, minCompactVersion);
+    }
+
+    public void testReadResponsesFromOldNode() throws Exception {
+        final Version minCompactVersion = Version.CURRENT.minimumCompatibilityVersion();
+        assumeTrue("Write list with mapping hash is introduced in 8.2", minCompactVersion.before(Version.V_8_2_0));
+        String base64FromES80 =
+            "AwhpbmRleF8wMQIJcmVkX2ZpZWxkCXJlZF9maWVsZAR0ZXh0AAEAAAAACmJsdWVfZmllbGQJcmVkX2ZpZWxkBGxvbmcAAQEAAAABCGluZGV4XzAyAAAIaW5kZXhfMDMCB19zZXFfbm8HX3NlcV9ubwRsb25nAQEBAAAADHllbGxvd19maWVsZAx5ZWxsb3dfZmllbGQHa2V5d29yZAABAQAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAA==";
+        StreamInput in = StreamInput.wrap(Base64.getDecoder().decode(base64FromES80));
+        in.setVersion(VersionUtils.randomVersionBetween(random(), Version.V_8_0_0, VersionUtils.getPreviousVersion(Version.V_8_2_0)));
+        List<FieldCapabilitiesIndexResponse> responses = FieldCapabilitiesIndexResponse.readList(in);
+        List<FieldCapabilitiesIndexResponse> expectedResponses = List.of(
+            new FieldCapabilitiesIndexResponse(
+                "index_01",
+                null,
+                Map.of(
+                    "red_field",
+                    new IndexFieldCapabilities("red_field", "text", false, true, false, false, null, Map.of()),
+                    "blue_field",
+                    new IndexFieldCapabilities("red_field", "long", false, true, true, false, null, Map.of())
+                ),
+                true
+            ),
+            new FieldCapabilitiesIndexResponse("index_02", null, Map.of(), false),
+            new FieldCapabilitiesIndexResponse(
+                "index_03",
+                null,
+                Map.of(
+                    "yellow_field",
+                    new IndexFieldCapabilities("yellow_field", "keyword", false, true, true, false, null, Map.of()),
+                    "_seq_no",
+                    new IndexFieldCapabilities("_seq_no", "long", true, true, true, false, null, Map.of())
+                ),
+                true
+            )
+        );
+        assertThat(responses, equalTo(expectedResponses));
+        assertSerializeOnNewNodes(new ArrayList<>(responses), Map.of());
+        assertSerializeOnOldNodes(new ArrayList<>(responses), minCompactVersion);
+    }
+
+    /**
+     * Asserts the serialization of a list of index responses between two nodes on 8.2 or later. The serialized list must contain
+     * the same elements of the input list but can be in any order. Importantly, the elements of the serialized list must be shared
+     * between indices that have the same mapping hashes specified by {@code mappingHashToIndices}.
+     */
+    private void assertSerializeOnNewNodes(List<FieldCapabilitiesIndexResponse> inList, Map<String, List<String>> mappingHashToIndices)
+        throws IOException {
+        final Version[] versions = new Version[between(2, 5)];
+        for (int i = 0; i < versions.length; i++) {
+            versions[i] = VersionUtils.randomVersionBetween(random(), Version.V_8_2_0, Version.CURRENT);
         }
-        assertThat(serializedList, hasSize(inList.size()));
-        for (int i = 0; i < inList.size(); i++) {
-            FieldCapabilitiesIndexResponse serialized = serializedList.get(i);
-            assertThat(serialized.getIndexMappingHash(), nullValue());
-            FieldCapabilitiesIndexResponse in = inList.get(i);
-            assertThat(serialized.get(), equalTo(in.get()));
-            assertThat(serialized.getIndexName(), equalTo(in.getIndexName()));
-            assertThat(serialized.canMatch(), equalTo(in.canMatch()));
+        for (Version version : versions) {
+            Randomness.shuffle(inList);
+            final List<FieldCapabilitiesIndexResponse> outList;
+            try (BytesStreamOutput output = new BytesStreamOutput()) {
+                output.setVersion(version);
+                FieldCapabilitiesIndexResponse.writeList(output, inList);
+                try (StreamInput in = output.bytes().streamInput()) {
+                    in.setVersion(version);
+                    outList = FieldCapabilitiesIndexResponse.readList(in);
+                }
+            }
+            assertThat(
+                outList.stream().sorted(Comparator.comparing(FieldCapabilitiesIndexResponse::getIndexName)).toList(),
+                equalTo(inList.stream().sorted(Comparator.comparing(FieldCapabilitiesIndexResponse::getIndexName)).toList())
+            );
+            Map<String, List<FieldCapabilitiesIndexResponse>> groupedResponses = outList.stream()
+                .filter(r -> r.canMatch() && r.getIndexMappingHash() != null)
+                .collect(Collectors.groupingBy(FieldCapabilitiesIndexResponse::getIndexMappingHash));
+            assertThat(groupedResponses.keySet(), equalTo(mappingHashToIndices.keySet()));
+
+            // Asserts responses of indices with the same mapping hash must be shared.
+            for (Map.Entry<String, List<FieldCapabilitiesIndexResponse>> e : groupedResponses.entrySet()) {
+                List<String> indices = mappingHashToIndices.get(e.getKey());
+                List<FieldCapabilitiesIndexResponse> rs = e.getValue();
+                assertThat(rs.stream().map(FieldCapabilitiesIndexResponse::getIndexName).sorted().toList(), equalTo(indices));
+                for (FieldCapabilitiesIndexResponse r : rs) {
+                    assertTrue(r.canMatch());
+                    assertSame(r.get(), rs.get(0).get());
+                }
+            }
+            inList = new ArrayList<>(outList);
+        }
+    }
+
+    /**
+     * Asserts the serialization of a list of index responses between two nodes before 8.2. The serialized list must contain the same
+     * elements of the input list except that the elements of the serialized list don't have mapping hash. After the first iteration,
+     * this method can perform the serialization between nodes of any version.
+     */
+    private void assertSerializeOnOldNodes(List<FieldCapabilitiesIndexResponse> inList, Version minCompactVersion) throws IOException {
+        final Version[] wireVersions = new Version[between(2, 5)];
+        // old version first, then any version
+        wireVersions[0] = VersionUtils.randomVersionBetween(random(), minCompactVersion, VersionUtils.getPreviousVersion(Version.V_8_2_0));
+        for (int i = 1; i < wireVersions.length; i++) {
+            wireVersions[i] = VersionUtils.randomVersionBetween(random(), minCompactVersion, Version.CURRENT);
+        }
+        for (Version wireVersion : wireVersions) {
+            Randomness.shuffle(inList);
+            final List<FieldCapabilitiesIndexResponse> outList;
+            try (BytesStreamOutput output = new BytesStreamOutput()) {
+                output.setVersion(wireVersion);
+                FieldCapabilitiesIndexResponse.writeList(output, inList);
+                try (StreamInput in = output.bytes().streamInput()) {
+                    in.setVersion(wireVersion);
+                    outList = FieldCapabilitiesIndexResponse.readList(in);
+                }
+            }
+            assertThat(outList, hasSize(inList.size()));
+            for (int i = 0; i < inList.size(); i++) {
+                FieldCapabilitiesIndexResponse resp = outList.get(i);
+                assertThat("Responses between old nodes don't have mapping hash", resp.getIndexMappingHash(), nullValue());
+                FieldCapabilitiesIndexResponse in = inList.get(i);
+                assertThat(resp.get(), equalTo(in.get()));
+                assertThat(resp.getIndexName(), equalTo(in.getIndexName()));
+                assertThat(resp.canMatch(), equalTo(in.canMatch()));
+            }
+            inList = new ArrayList<>(outList);
         }
     }
 }

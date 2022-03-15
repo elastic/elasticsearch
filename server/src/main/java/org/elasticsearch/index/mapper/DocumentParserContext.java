afc1cdffa009c0d9c4e6e69b059992607bcbb8e1
Luca Cavanna <javanna@users.noreply.github.com>
2022-03-15 15:55:41 +0100

    Remove MappingLookup#isShadowed

--- server/src/main/java/org/elasticsearch/index/mapper/DocumentParserContext.java
+++ server/src/main/java/org/elasticsearch/index/mapper/DocumentParserContext.java
@@ -225,10 +225,6 @@ public abstract class DocumentParserContext {
         return dynamicMappers;
     }
 
-    public final boolean isShadowed(String field) {
-        return mappingLookup.isShadowed(field);
-    }
-
     public final ObjectMapper getDynamicObjectMapper(String name) {
         return dynamicObjectMappers.get(name);
     }

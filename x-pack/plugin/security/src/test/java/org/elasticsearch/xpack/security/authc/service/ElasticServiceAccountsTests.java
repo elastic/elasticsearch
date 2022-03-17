fd8d87a4e3be29bb0d100a2ea2eb049b3fcecfc1
Tim Vernum <tim@adjective.org>
2022-03-17 16:39:49 +1100

    Refactor security classes for restricted indices
    This commit introduces a `RestrictedIndices` class that is a thin
    wrapper around the existing `Automaton` that is used to represent the
    set of restricted indices.
    This new class is used in all places where security needs to be aware
    of, or test for, restricted indices.
    
    The RestrictedIndicesNames class has been removed, with some of its
    members moved to SecuritySystemIndices and others moved to
    TestRestrictedIndices

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authc/service/ElasticServiceAccountsTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authc/service/ElasticServiceAccountsTests.java
@@ -117,7 +117,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import static org.elasticsearch.xpack.core.security.test.TestRestrictedIndices.RESTRICTED_INDICES_AUTOMATON;
+import static org.elasticsearch.xpack.core.security.test.TestRestrictedIndices.RESTRICTED_INDICES;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -147,7 +147,7 @@ public class ElasticServiceAccountsTests extends ESTestCase {
         final Role role = Role.builder(
             ElasticServiceAccounts.ACCOUNTS.get("elastic/fleet-server").roleDescriptor(),
             null,
-            RESTRICTED_INDICES_AUTOMATON
+            RESTRICTED_INDICES
         ).build();
         final Authentication authentication = mock(Authentication.class);
         assertThat(
@@ -304,7 +304,7 @@ public class ElasticServiceAccountsTests extends ESTestCase {
         final Role role = Role.builder(
             ElasticServiceAccounts.ACCOUNTS.get("elastic/enterprise-search-server").roleDescriptor(),
             null,
-            RESTRICTED_INDICES_AUTOMATON
+            RESTRICTED_INDICES
         ).build();
 
         final Authentication authentication = mock(Authentication.class);

e3154a19b7bbd4f3f22cca37e45a7df2580e8b96
David Roberts <dave.roberts@elastic.co>
2022-03-24 11:28:04 +0000

    [Transform] Revert surfacing script deprecations warnings in deprecation info API (#85317)
    This largely reverts the change made in #84040. (A few bits of
    infrastructure like the extra option on OriginSettingClient are
    not reverted.)
    
    The reason is that the transform validation action that was used
    to validate the search syntax (including Painless scripts) actually
    runs the search. This can take a long time for transforms with big
    source indices, and leads to an unacceptably long response time for
    the deprecation info API. For example, if as a result of validating
    transform searches the deprecation info API takes longer than 30
    seconds to respond then the Kibana upgrade assistant will time out
    and think it's never going to respond.
    
    Removing this functionality that was added in 7.17.1 leaves us no
    worse off than we were in 7.17.0 in terms of surfacing deprecated
    search syntax used in transforms, so we should do this for 7.17.2
    to avoid the worse problem of the upgrade assistant hanging. Then
    in a later release we should try to find a way to surface
    deprecations in the Painless scripts embedded in transform searches
    without running the entire search.

--- x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/TransformDeprecationChecker.java
+++ x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/TransformDeprecationChecker.java
@@ -7,36 +7,20 @@
 
 package org.elasticsearch.xpack.deprecation;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.cluster.metadata.Metadata;
-import org.elasticsearch.common.logging.HeaderWarning;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.util.concurrent.CountDown;
-import org.elasticsearch.core.TimeValue;
-import org.elasticsearch.xpack.core.ClientHelper;
 import org.elasticsearch.xpack.core.action.util.PageParams;
 import org.elasticsearch.xpack.core.deprecation.DeprecationIssue;
-import org.elasticsearch.xpack.core.transform.TransformDeprecations;
-import org.elasticsearch.xpack.core.transform.TransformField;
 import org.elasticsearch.xpack.core.transform.action.GetTransformAction;
-import org.elasticsearch.xpack.core.transform.action.ValidateTransformAction;
 import org.elasticsearch.xpack.core.transform.transforms.TransformConfig;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-import static java.util.stream.Collectors.toList;
 
 public class TransformDeprecationChecker implements DeprecationChecker {
 
     public static final String TRANSFORM_DEPRECATION_KEY = "transform_settings";
-
-    private static final Logger logger = LogManager.getLogger(TransformDeprecationChecker.class);
 
     @Override
     public boolean enabled(Settings settings) {
@@ -48,7 +32,7 @@ public class TransformDeprecationChecker implements DeprecationChecker {
     public void check(Components components, ActionListener<CheckResult> deprecationIssueListener) {
 
         PageParams startPage = new PageParams(0, PageParams.DEFAULT_SIZE);
-        Collection<DeprecationIssue> issues = new ConcurrentLinkedQueue<>();
+        List<DeprecationIssue> issues = new ArrayList<>();
         recursiveGetTransformsAndCollectDeprecations(
             components,
             issues,
@@ -67,7 +51,7 @@ public class TransformDeprecationChecker implements DeprecationChecker {
 
     private void recursiveGetTransformsAndCollectDeprecations(
         Components components,
-        Collection<DeprecationIssue> issues,
+        List<DeprecationIssue> issues,
         PageParams page,
         ActionListener<List<DeprecationIssue>> listener
     ) {
@@ -75,74 +59,17 @@ public class TransformDeprecationChecker implements DeprecationChecker {
         request.setPageParams(page);
         request.setAllowNoResources(true);
 
-        ClientHelper.executeAsyncWithOrigin(
-            components.client(),
-            ClientHelper.DEPRECATION_ORIGIN,
-            GetTransformAction.INSTANCE,
-            request,
-            ActionListener.wrap(getTransformResponse -> {
-                final int numberOfTransforms = getTransformResponse.getTransformConfigurations().size();
-                Runnable processNextPage = () -> {
-                    if (getTransformResponse.getCount() >= (page.getFrom() + page.getSize())) {
-                        PageParams nextPage = new PageParams(page.getFrom() + page.getSize(), PageParams.DEFAULT_SIZE);
-                        recursiveGetTransformsAndCollectDeprecations(components, issues, nextPage, listener);
-                    } else {
-                        listener.onResponse(new ArrayList<>(issues));
-                    }
-                };
-                if (numberOfTransforms == 0) {
-                    processNextPage.run();
-                    return;
-                }
-                final CountDown numberOfResponsesToProcess = new CountDown(numberOfTransforms);
-                for (TransformConfig config : getTransformResponse.getTransformConfigurations()) {
-                    issues.addAll(config.checkForDeprecations(components.xContentRegistry()));
+        components.client().execute(GetTransformAction.INSTANCE, request, ActionListener.wrap(getTransformResponse -> {
+            for (TransformConfig config : getTransformResponse.getTransformConfigurations()) {
+                issues.addAll(config.checkForDeprecations(components.xContentRegistry()));
+            }
+            if (getTransformResponse.getCount() >= (page.getFrom() + page.getSize())) {
+                PageParams nextPage = new PageParams(page.getFrom() + page.getSize(), PageParams.DEFAULT_SIZE);
+                recursiveGetTransformsAndCollectDeprecations(components, issues, nextPage, listener);
+            } else {
+                listener.onResponse(issues);
+            }
 
-                    ValidateTransformAction.Request validateTransformRequest = new ValidateTransformAction.Request(
-                        config,
-                        false,
-                        TimeValue.timeValueSeconds(30)
-                    );
-                    ActionListener<ValidateTransformAction.Response> validateTransformListener = ActionListener.wrap(
-                        validateTransformResponse -> {
-                            List<String> warningHeaders = components.client()
-                                .threadPool()
-                                .getThreadContext()
-                                .getResponseHeaders()
-                                .get("Warning");
-                            if (warningHeaders != null) {
-                                issues.addAll(
-                                    warningHeaders.stream()
-                                        .map(warningHeader -> createDeprecationIssue(config.getId(), warningHeader))
-                                        .collect(toList())
-                                );
-                            }
-                            if (numberOfResponsesToProcess.countDown()) {
-                                processNextPage.run();
-                            }
-                        },
-                        e -> {
-                            logger.warn("An exception occurred while gathering deprecation warnings for transform", e);
-                            if (numberOfResponsesToProcess.countDown()) {
-                                processNextPage.run();
-                            }
-                        }
-                    );
-
-                    components.client().execute(ValidateTransformAction.INSTANCE, validateTransformRequest, validateTransformListener);
-                }
-            }, listener::onFailure)
-        );
-    }
-
-    private static DeprecationIssue createDeprecationIssue(String transformId, String warningHeader) {
-        return new DeprecationIssue(
-            DeprecationIssue.Level.WARNING,
-            "Transform [" + transformId + "]: " + HeaderWarning.extractWarningValueFromWarningHeader(warningHeader, true),
-            TransformDeprecations.PAINLESS_BREAKING_CHANGES_URL,
-            null,
-            false,
-            Collections.singletonMap(TransformField.TRANSFORM_ID, transformId)
-        );
+        }, listener::onFailure));
     }
 }

53e5290b546bf8b39f590fad38db6552ddfe4ee4
Ryan Ernst <ryan@iernst.net>
2022-03-25 15:47:03 -0700

    checkstyle

--- server/src/test/java/org/elasticsearch/bootstrap/BootstrapChecksTests.java
+++ server/src/test/java/org/elasticsearch/bootstrap/BootstrapChecksTests.java
@@ -28,7 +28,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;

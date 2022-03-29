elastic/elasticsearch

commit 08141cf875bb5e5e1a5c52bdbac9a482d77b17a1

fix: nested top metrics sort on keyword field (#85058)
Using a double as a return value works only if the field we are
sorting on is a number. If the field is not a value we can convert
to a double, like a non-numeric keyword, converting it to a number
returns `NaN`. Without this patch, sorting takes place on the bucket
key, if the order field points to a non-numeric value. The additional
bucket key comparator is implicitly added as a tie breaker to avoid
non-deterministic sorting of buckets.

With this change we support sorting using any subclass of SortValue.
This means the bucket key will be used just in case of equal values
on the order field.

Issue: #78506

server/src/main/java/org/elasticsearch/search/sort/SortValue.java
@@ -25,6 +25,8 @@ import java.util.List;
  * A {@link Comparable}, {@link DocValueFormat} aware wrapper around a sort value.
  */
 public abstract class SortValue implements NamedWriteable, Comparable<SortValue> {
+    private static final SortValue EMPTY_SORT_VALUE = new EmptySortValue();
+
     /**
      * Get a {@linkplain SortValue} for a double.
      */
@@ -48,13 +50,21 @@ public abstract class SortValue implements NamedWriteable, Comparable<SortValue>
     }
 
     /**
+     * Get a {@linkplain SortValue} for data which cannot be sorted.
+     */
+    public static SortValue empty() {
+        return EMPTY_SORT_VALUE;
+    }
+
+    /**
      * Get the list of {@linkplain NamedWriteable}s that this class needs.
      */
     public static List<NamedWriteableRegistry.Entry> namedWriteables() {
         return Arrays.asList(
             new NamedWriteableRegistry.Entry(SortValue.class, DoubleSortValue.NAME, DoubleSortValue::new),
             new NamedWriteableRegistry.Entry(SortValue.class, LongSortValue.NAME, LongSortValue::new),
-            new NamedWriteableRegistry.Entry(SortValue.class, BytesSortValue.NAME, BytesSortValue::new)
+            new NamedWriteableRegistry.Entry(SortValue.class, BytesSortValue.NAME, BytesSortValue::new),
+            new NamedWriteableRegistry.Entry(SortValue.class, EmptySortValue.NAME, EmptySortValue::new)
         );
     }
 
@@ -336,6 +346,64 @@ public abstract class SortValue implements NamedWriteable, Comparable<SortValue>
         @Override
         public Number numberValue() {
             return Double.NaN;
+        }
+    }
+
+    private static class EmptySortValue extends SortValue {
+
+        public static final String NAME = "empty";
+        private static final String EMPTY_STRING = "";
+
+        private EmptySortValue() {}
+
+        private EmptySortValue(StreamInput ignoredIn) {}
+
+        @Override
+        public String getWriteableName() {
+            return NAME;
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) throws IOException {}
+
+        @Override
+        public Object getKey() {
+            return EMPTY_STRING;
+        }
+
+        @Override
+        public String format(DocValueFormat format) {
+            return EMPTY_STRING;
+        }
+
+        @Override
+        protected XContentBuilder rawToXContent(XContentBuilder builder) throws IOException {
+            return builder;
+        }
+
+        @Override
+        protected int compareToSameType(SortValue obj) {
+            return 0;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj != null && false != getClass().equals(obj.getClass());
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+
+        @Override
+        public String toString() {
+            return EMPTY_STRING;
+        }
+
+        @Override
+        public Number numberValue() {
+            return null;
         }
     }
 }

x-pack/plugin/analytics/src/main/java/org/elasticsearch/xpack/analytics/topmetrics/InternalTopMetrics.java
@@ -171,23 +171,21 @@ public class InternalTopMetrics extends InternalMultiValueAggregation {
     }
 
     @Override
-    public final double sortValue(String key) {
+    public final SortValue sortValue(String key) {
         int index = metricNames.indexOf(key);
         if (index < 0) {
             throw new IllegalArgumentException("unknown metric [" + key + "]");
         }
         if (topMetrics.isEmpty()) {
-            return Double.NaN;
+            return SortValue.empty();
         }
 
         MetricValue value = topMetrics.get(0).metricValues.get(index);
         if (value == null) {
-            return Double.NaN;
+            return SortValue.empty();
         }
 
-        // TODO it'd probably be nicer to have "compareTo" instead of assuming a double.
-        // non-numeric fields always return NaN
-        return value.numberValue().doubleValue();
+        return value.getValue();
     }
 
     @Override

server/src/test/java/org/elasticsearch/search/sort/SortValueTests.java
@@ -48,10 +48,11 @@ public class SortValueTests extends AbstractNamedWriteableTestCase<SortValue> {
 
     @Override
     protected SortValue createTestInstance() {
-        return switch (between(0, 2)) {
+        return switch (between(0, 3)) {
             case 0 -> SortValue.from(randomDouble());
             case 1 -> SortValue.from(randomLong());
             case 2 -> SortValue.from(new BytesRef(randomAlphaOfLength(5)));
+            case 3 -> SortValue.empty();
             default -> throw new AssertionError();
         };
     }
@@ -70,6 +71,10 @@ public class SortValueTests extends AbstractNamedWriteableTestCase<SortValue> {
     public void testFormatLong() {
         assertThat(SortValue.from(1).format(DocValueFormat.RAW), equalTo("1"));
         assertThat(SortValue.from(1).format(STRICT_DATE_TIME), equalTo("1970-01-01T00:00:00.001Z"));
+    }
+
+    public void testFormatEmpty() {
+        assertThat(SortValue.empty().format(DocValueFormat.RAW), equalTo(""));
     }
 
     public void testToXContentDouble() {
@@ -91,6 +96,10 @@ public class SortValueTests extends AbstractNamedWriteableTestCase<SortValue> {
         );
     }
 
+    public void testToXContentEmpty() {
+        assertThat(toXContent(SortValue.empty(), DocValueFormat.RAW), equalTo("{\"test\"}"));
+    }
+
     public void testCompareDifferentTypes() {
         assertThat(SortValue.from(1.0), lessThan(SortValue.from(1)));
         assertThat(SortValue.from(Double.MAX_VALUE), lessThan(SortValue.from(Long.MIN_VALUE)));
@@ -100,6 +109,20 @@ public class SortValueTests extends AbstractNamedWriteableTestCase<SortValue> {
         assertThat(SortValue.from(1), greaterThan(SortValue.from(new BytesRef("cat"))));
         assertThat(SortValue.from(new BytesRef("cat")), lessThan(SortValue.from(1.0)));
         assertThat(SortValue.from(1.0), greaterThan(SortValue.from(new BytesRef("cat"))));
+    }
+
+    /**
+     * When comparing different types ordering takes place according to the writable name.
+     * This is the reason why "long" is greater than "empty" and "double" is less than "empty".
+     * See {@link org.elasticsearch.search.sort.SortValue#compareTo}.
+     */
+    public void testCompareToEmpty() {
+        assertThat(SortValue.from(1.0), lessThan(SortValue.empty()));
+        assertThat(SortValue.from(Double.MAX_VALUE), lessThan(SortValue.empty()));
+        assertThat(SortValue.from(Double.NaN), lessThan(SortValue.empty()));
+        assertThat(SortValue.from(1), greaterThan(SortValue.empty()));
+        assertThat(SortValue.from(Long.MIN_VALUE), greaterThan(SortValue.empty()));
+        assertThat(SortValue.from(new BytesRef("cat")), lessThan(SortValue.empty()));
     }
 
     public void testCompareDoubles() {
@@ -114,6 +137,10 @@ public class SortValueTests extends AbstractNamedWriteableTestCase<SortValue> {
         assertThat(SortValue.from(r), equalTo(SortValue.from(r)));
         assertThat(SortValue.from(r), lessThan(SortValue.from(r + 1)));
         assertThat(SortValue.from(r), greaterThan(SortValue.from(r - 1)));
+    }
+
+    public void testCompareEmpty() {
+        assertThat(SortValue.empty(), equalTo(SortValue.empty()));
     }
 
     public void testBytes() {

server/src/main/java/org/elasticsearch/search/aggregations/InternalAggregation.java
@@ -16,6 +16,7 @@ import org.elasticsearch.search.aggregations.pipeline.PipelineAggregator;
 import org.elasticsearch.search.aggregations.pipeline.PipelineAggregator.PipelineTree;
 import org.elasticsearch.search.aggregations.support.AggregationPath;
 import org.elasticsearch.search.aggregations.support.SamplingContext;
+import org.elasticsearch.search.sort.SortValue;
 import org.elasticsearch.xcontent.XContentBuilder;
 
 import java.io.IOException;
@@ -231,7 +232,7 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
     /**
      * Get value to use when sorting by this aggregation.
      */
-    public double sortValue(String key) {
+    public SortValue sortValue(String key) {
         // subclasses will override this with a real implementation if they can be sorted
         throw new IllegalArgumentException("Can't sort a [" + getType() + "] aggregation [" + getName() + "]");
     }
@@ -239,7 +240,7 @@ public abstract class InternalAggregation implements Aggregation, NamedWriteable
     /**
      * Get value to use when sorting by a descendant of this aggregation.
      */
-    public double sortValue(AggregationPath.PathElement head, Iterator<AggregationPath.PathElement> tail) {
+    public SortValue sortValue(AggregationPath.PathElement head, Iterator<AggregationPath.PathElement> tail) {
         // subclasses will override this with a real implementation if you can sort on a descendant
         throw new IllegalArgumentException("Can't sort by a descendant of a [" + getType() + "] aggregation [" + head + "]");
     }

server/src/main/java/org/elasticsearch/search/aggregations/InternalAggregations.java
@@ -14,6 +14,7 @@ import org.elasticsearch.search.aggregations.pipeline.PipelineAggregator;
 import org.elasticsearch.search.aggregations.pipeline.SiblingPipelineAggregator;
 import org.elasticsearch.search.aggregations.support.AggregationPath;
 import org.elasticsearch.search.aggregations.support.SamplingContext;
+import org.elasticsearch.search.sort.SortValue;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -80,7 +81,7 @@ public final class InternalAggregations extends Aggregations implements Writeabl
     /**
      * Get value to use when sorting by a descendant of the aggregation containing this.
      */
-    public double sortValue(AggregationPath.PathElement head, Iterator<AggregationPath.PathElement> tail) {
+    public SortValue sortValue(AggregationPath.PathElement head, Iterator<AggregationPath.PathElement> tail) {
         InternalAggregation aggregation = get(head.name());
         if (aggregation == null) {
             throw new IllegalArgumentException("Cannot find aggregation named [" + head.name() + "]");

server/src/main/java/org/elasticsearch/search/aggregations/InternalOrder.java
@@ -11,12 +11,12 @@ import org.elasticsearch.common.ParsingException;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.logging.DeprecationLogger;
-import org.elasticsearch.common.util.Comparators;
 import org.elasticsearch.core.RestApiVersion;
 import org.elasticsearch.search.aggregations.Aggregator.BucketComparator;
 import org.elasticsearch.search.aggregations.bucket.MultiBucketsAggregation.Bucket;
 import org.elasticsearch.search.aggregations.support.AggregationPath;
 import org.elasticsearch.search.sort.SortOrder;
+import org.elasticsearch.search.sort.SortValue;
 import org.elasticsearch.xcontent.XContent;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentParser;
@@ -74,9 +74,10 @@ public abstract class InternalOrder extends BucketOrder {
         @Override
         public Comparator<Bucket> comparator() {
             return (lhs, rhs) -> {
-                double l = path.resolveValue(((InternalAggregations) lhs.getAggregations()));
-                double r = path.resolveValue(((InternalAggregations) rhs.getAggregations()));
-                return Comparators.compareDiscardNaN(l, r, order == SortOrder.ASC);
+                final SortValue l = path.resolveValue(((InternalAggregations) lhs.getAggregations()));
+                final SortValue r = path.resolveValue(((InternalAggregations) rhs.getAggregations()));
+                int compareResult = l.compareTo(r);
+                return order == SortOrder.ASC ? compareResult : -compareResult;
             };
         }
 

server/src/main/java/org/elasticsearch/search/aggregations/bucket/InternalSingleBucketAggregation.java
@@ -15,6 +15,7 @@ import org.elasticsearch.search.aggregations.InternalAggregation;
 import org.elasticsearch.search.aggregations.InternalAggregations;
 import org.elasticsearch.search.aggregations.pipeline.PipelineAggregator.PipelineTree;
 import org.elasticsearch.search.aggregations.support.AggregationPath;
+import org.elasticsearch.search.sort.SortValue;
 import org.elasticsearch.xcontent.XContentBuilder;
 
 import java.io.IOException;
@@ -154,7 +155,7 @@ public abstract class InternalSingleBucketAggregation extends InternalAggregatio
     }
 
     @Override
-    public final double sortValue(String key) {
+    public final SortValue sortValue(String key) {
         if (key != null && false == key.equals("doc_count")) {
             throw new IllegalArgumentException(
                 "Unknown value key ["
@@ -164,11 +165,11 @@ public abstract class InternalSingleBucketAggregation extends InternalAggregatio
                     + "]. Either use [doc_count] as key or drop the key all together."
             );
         }
-        return docCount;
+        return SortValue.from(docCount);
     }
 
     @Override
-    public final double sortValue(AggregationPath.PathElement head, Iterator<AggregationPath.PathElement> tail) {
+    public final SortValue sortValue(AggregationPath.PathElement head, Iterator<AggregationPath.PathElement> tail) {
         return aggregations.sortValue(head, tail);
     }
 

server/src/main/java/org/elasticsearch/search/aggregations/metrics/InternalMultiValueAggregation.java
@@ -11,6 +11,7 @@ package org.elasticsearch.search.aggregations.metrics;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.search.aggregations.InternalAggregation;
 import org.elasticsearch.search.aggregations.support.AggregationPath;
+import org.elasticsearch.search.sort.SortValue;
 
 import java.io.IOException;
 import java.util.Iterator;
@@ -30,7 +31,7 @@ public abstract class InternalMultiValueAggregation extends InternalAggregation 
     }
 
     @Override
-    public final double sortValue(AggregationPath.PathElement head, Iterator<AggregationPath.PathElement> tail) {
+    public final SortValue sortValue(AggregationPath.PathElement head, Iterator<AggregationPath.PathElement> tail) {
         throw new IllegalArgumentException("Metrics aggregations cannot have sub-aggregations (at [>" + head + "]");
     }
 }

commit 6f59efaa0d3aa3dfcebc70e4178c0f38ec55d10e

fix: make ValuelessSortValue instances sort after everything else
When comparing instances of SortValue we make sure that instances
of subclass ValuelessSortValue sort after evrything else. This is
also to be consistent with the way Double.NaN values are sorted
with respect to Double values.

The name `valueless` is chosen so that instances of ValuelessSortValue
sorts after `bytes`, `double` and `long`, the names of the other three
existing subclasses.

Additionally we make sure to create an instance of ValuelessSortValue
if creating an instance from a Double.NaN value.

This PR is intented to fix a potential issue introduced by #85058.

server/src/main/java/org/elasticsearch/search/sort/SortValue.java
@@ -25,12 +25,15 @@ import java.util.List;
  * A {@link Comparable}, {@link DocValueFormat} aware wrapper around a sort value.
  */
 public abstract class SortValue implements NamedWriteable, Comparable<SortValue> {
-    private static final SortValue EMPTY_SORT_VALUE = new EmptySortValue();
+    private static final SortValue VALUELESS_SORT_VALUE = new ValuelessSortValue();
 
     /**
      * Get a {@linkplain SortValue} for a double.
      */
     public static SortValue from(double d) {
+        if (Double.isNaN(d)) {
+            return new ValuelessSortValue();
+        }
         return new DoubleSortValue(d);
     }
 
@@ -52,8 +55,8 @@ public abstract class SortValue implements NamedWriteable, Comparable<SortValue>
     /**
      * Get a {@linkplain SortValue} for data which cannot be sorted.
      */
-    public static SortValue empty() {
-        return EMPTY_SORT_VALUE;
+    public static SortValue valueless() {
+        return VALUELESS_SORT_VALUE;
     }
 
     /**
@@ -64,7 +67,7 @@ public abstract class SortValue implements NamedWriteable, Comparable<SortValue>
             new NamedWriteableRegistry.Entry(SortValue.class, DoubleSortValue.NAME, DoubleSortValue::new),
             new NamedWriteableRegistry.Entry(SortValue.class, LongSortValue.NAME, LongSortValue::new),
             new NamedWriteableRegistry.Entry(SortValue.class, BytesSortValue.NAME, BytesSortValue::new),
-            new NamedWriteableRegistry.Entry(SortValue.class, EmptySortValue.NAME, EmptySortValue::new)
+            new NamedWriteableRegistry.Entry(SortValue.class, ValuelessSortValue.NAME, ValuelessSortValue::new)
         );
     }
 
@@ -349,14 +352,26 @@ public abstract class SortValue implements NamedWriteable, Comparable<SortValue>
         }
     }
 
-    private static class EmptySortValue extends SortValue {
+    private static class ValuelessSortValue extends SortValue {
 
-        public static final String NAME = "empty";
+        /**
+         * The name `valueless` is chosen carefully to make sure
+         * that sorting different `SortValue`s results in objects
+         * of type `ValuelessSortValue` to be sorted after everything
+         * else. This is to be consistent with how `Double.NaN` is
+         * sorted when sorting double values. This is necessary
+         * as a result of comparison being done using the `NAME`
+         * returned by {@ValuelessSortValue#getWritableName}.
+         * See {@link SortValue#compareTo(SortValue)} and
+         * {@link SortValue#compareToSameType(SortValue)} for
+         * more details.
+         */
+        public static final String NAME = "valueless";
         private static final String EMPTY_STRING = "";
 
-        private EmptySortValue() {}
+        private ValuelessSortValue() {}
 
-        private EmptySortValue(StreamInput ignoredIn) {}
+        private ValuelessSortValue(StreamInput ignoredIn) {}
 
         @Override
         public String getWriteableName() {

x-pack/plugin/analytics/src/main/java/org/elasticsearch/xpack/analytics/topmetrics/InternalTopMetrics.java
@@ -177,12 +177,12 @@ public class InternalTopMetrics extends InternalMultiValueAggregation {
             throw new IllegalArgumentException("unknown metric [" + key + "]");
         }
         if (topMetrics.isEmpty()) {
-            return SortValue.empty();
+            return SortValue.valueless();
         }
 
         MetricValue value = topMetrics.get(0).metricValues.get(index);
         if (value == null) {
-            return SortValue.empty();
+            return SortValue.valueless();
         }
 
         return value.getValue();

server/src/test/java/org/elasticsearch/search/sort/SortValueTests.java
@@ -24,6 +24,9 @@ import org.elasticsearch.xcontent.XContentBuilder;
 
 import java.io.IOException;
 import java.time.ZoneId;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
@@ -52,7 +55,7 @@ public class SortValueTests extends AbstractNamedWriteableTestCase<SortValue> {
             case 0 -> SortValue.from(randomDouble());
             case 1 -> SortValue.from(randomLong());
             case 2 -> SortValue.from(new BytesRef(randomAlphaOfLength(5)));
-            case 3 -> SortValue.empty();
+            case 3 -> SortValue.valueless();
             default -> throw new AssertionError();
         };
     }
@@ -74,7 +77,7 @@ public class SortValueTests extends AbstractNamedWriteableTestCase<SortValue> {
     }
 
     public void testFormatEmpty() {
-        assertThat(SortValue.empty().format(DocValueFormat.RAW), equalTo(""));
+        assertThat(SortValue.valueless().format(DocValueFormat.RAW), equalTo(""));
     }
 
     public void testToXContentDouble() {
@@ -97,7 +100,7 @@ public class SortValueTests extends AbstractNamedWriteableTestCase<SortValue> {
     }
 
     public void testToXContentEmpty() {
-        assertThat(toXContent(SortValue.empty(), DocValueFormat.RAW), equalTo("{\"test\"}"));
+        assertThat(toXContent(SortValue.valueless(), DocValueFormat.RAW), equalTo("{\"test\"}"));
     }
 
     public void testCompareDifferentTypes() {
@@ -117,12 +120,12 @@ public class SortValueTests extends AbstractNamedWriteableTestCase<SortValue> {
      * See {@link org.elasticsearch.search.sort.SortValue#compareTo}.
      */
     public void testCompareToEmpty() {
-        assertThat(SortValue.from(1.0), lessThan(SortValue.empty()));
-        assertThat(SortValue.from(Double.MAX_VALUE), lessThan(SortValue.empty()));
-        assertThat(SortValue.from(Double.NaN), lessThan(SortValue.empty()));
-        assertThat(SortValue.from(1), greaterThan(SortValue.empty()));
-        assertThat(SortValue.from(Long.MIN_VALUE), greaterThan(SortValue.empty()));
-        assertThat(SortValue.from(new BytesRef("cat")), lessThan(SortValue.empty()));
+        assertThat(SortValue.from(1.0), lessThan(SortValue.valueless()));
+        assertThat(SortValue.from(Double.MAX_VALUE), lessThan(SortValue.valueless()));
+        assertThat(SortValue.from(Double.NaN), equalTo(SortValue.valueless()));
+        assertThat(SortValue.from(1), lessThan(SortValue.valueless()));
+        assertThat(SortValue.from(Long.MIN_VALUE), lessThan(SortValue.valueless()));
+        assertThat(SortValue.from(new BytesRef("cat")), lessThan(SortValue.valueless()));
     }
 
     public void testCompareDoubles() {
@@ -140,7 +143,50 @@ public class SortValueTests extends AbstractNamedWriteableTestCase<SortValue> {
     }
 
     public void testCompareEmpty() {
-        assertThat(SortValue.empty(), equalTo(SortValue.empty()));
+        assertThat(SortValue.valueless(), equalTo(SortValue.valueless()));
+    }
+
+    public void testSortValueOrdering() {
+        final SortValue maxLong = SortValue.from(Long.MAX_VALUE);
+        final SortValue minLong = SortValue.from(Long.MIN_VALUE);
+        final SortValue negativeLong = SortValue.from(-12L);
+        final SortValue zeroLong = SortValue.from(0L);
+        final SortValue positiveLong = SortValue.from(110L);
+        final SortValue negativeNan = SortValue.from(-Double.NaN);
+        final SortValue positiveNan = SortValue.from(Double.NaN);
+        final SortValue maxDouble = SortValue.from(Double.MAX_VALUE);
+        final SortValue minDouble = SortValue.from(Double.MIN_VALUE);
+        final SortValue negativeDouble = SortValue.from(-30.5D);
+        final SortValue zeroDouble = SortValue.from(0.0D);
+        final SortValue positiveDouble = SortValue.from(18.97D);
+        final SortValue emptyBytesRef = SortValue.from(new BytesRef(""));
+        final SortValue fooBytesRef = SortValue.from(new BytesRef("Foo"));
+        final SortValue barBytesRef = SortValue.from(new BytesRef("bar"));
+        final SortValue valueless = SortValue.valueless();
+        final List<SortValue> values = List.of(
+            maxLong, minLong, negativeLong, zeroLong, positiveLong, //Long
+            negativeNan, positiveNan, maxDouble, minDouble, negativeDouble, zeroDouble, positiveDouble, //Double
+            emptyBytesRef, fooBytesRef, barBytesRef, //BytesRef
+            valueless //Valueless
+        );
+
+        final List<SortValue> sortedValues = values.stream()
+            .sorted(Comparator.naturalOrder())
+            .collect(Collectors.toList());
+
+        /**
+         * `negativeNan` and `positiveNan` are instances of
+         * {@link org.elasticsearch.search.sort.SortValue.ValuelessSortValue}
+         * the same of {@link SortValue#valueless()}.
+         */
+        assertThat(sortedValues, equalTo(
+            List.of(
+                emptyBytesRef, fooBytesRef, barBytesRef, //BytesRef
+                negativeDouble, zeroDouble, minDouble, positiveDouble, maxDouble, minLong, //Double
+                negativeLong, zeroLong, positiveLong, maxLong, //Long
+                negativeNan, positiveNan, valueless //Valueless
+            )
+        ));
     }
 
     public void testBytes() {
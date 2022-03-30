705561d3d163e9a2e574bb5cbc7457719dcc4107
Ievgen Degtiarenko <ievgen.degtiarenko@elastic.co>
2022-03-30 09:25:50 +0200

    [8.1] Ensure the cleanup in testAutoFollowPatterns is always happening (#85481)
    * Assert actual follow tasks instead of count (#85151)
    
    Currently, the test is flaky and sometimes fails with expected 1 but was
     2 failure. Updating the code to assert against collection content to
     collect additional information on the cause of the failure.
    
    (cherry picked from commit e31be1b570091ca8b29f924bf1fbae99937e44db)
    
    * Ensure the cleanup in testAutoFollowPatterns is always happening (#85401)
    
    In case of failure testAutoFollowPatterns will leave auto-following
    pattern running that would case assertion failure in other test cases
    
    (cherry picked from commit 92c45385f128ccdd872b93122af77c3bd6847d03)

--- x-pack/plugin/ccr/qa/src/main/java/org/elasticsearch/xpack/ccr/ESCCRRestTestCase.java
+++ x-pack/plugin/ccr/qa/src/main/java/org/elasticsearch/xpack/ccr/ESCCRRestTestCase.java
@@ -26,9 +26,11 @@ import org.elasticsearch.xcontent.json.JsonXContent;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 import static org.elasticsearch.rest.action.search.RestSearchAction.TOTAL_HITS_AS_INT_PARAM;
 import static org.elasticsearch.xcontent.XContentFactory.jsonBuilder;
@@ -271,7 +273,7 @@ public class ESCCRRestTestCase extends ESRestTestCase {
         });
     }
 
-    protected int countCcrNodeTasks() throws IOException {
+    protected Set<CcrNodeTask> getCcrNodeTasks() throws IOException {
         final Request request = new Request("GET", "/_tasks");
         request.addParameter("detailed", "true");
         Map<String, Object> rsp1 = toMap(adminClient().performRequest(request));
@@ -279,16 +281,26 @@ public class ESCCRRestTestCase extends ESRestTestCase {
         assertThat(nodes.size(), equalTo(1));
         Map<?, ?> node = (Map<?, ?>) nodes.values().iterator().next();
         Map<?, ?> nodeTasks = (Map<?, ?>) node.get("tasks");
-        int numNodeTasks = 0;
+        var ccrNodeTasks = new HashSet<CcrNodeTask>();
         for (Map.Entry<?, ?> entry : nodeTasks.entrySet()) {
             Map<?, ?> nodeTask = (Map<?, ?>) entry.getValue();
             String action = (String) nodeTask.get("action");
             if (action.startsWith("xpack/ccr/shard_follow_task")) {
-                numNodeTasks++;
+                var status = (Map<?, ?>) nodeTask.get("status");
+                ccrNodeTasks.add(
+                    new CcrNodeTask(
+                        (String) status.get("remote_cluster"),
+                        (String) status.get("leader_index"),
+                        (String) status.get("follower_index"),
+                        (Integer) status.get("shard_id")
+                    )
+                );
             }
         }
-        return numNodeTasks;
+        return ccrNodeTasks;
     }
+
+    protected record CcrNodeTask(String remoteCluster, String leaderIndex, String followerIndex, int shardId) {}
 
     protected static void createIndex(String name, Settings settings) throws IOException {
         createIndex(name, settings, "");

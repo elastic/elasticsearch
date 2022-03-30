5b764404f4595de7732e484a286194428d7ebdf9
Nhat Nguyen <nhat.nguyen@elastic.co>
2022-03-30 16:00:41 -0400

    Fix skip caching factor with indices.queries.cache.all_segments

--- server/src/main/java/org/elasticsearch/indices/IndicesQueryCache.java
+++ server/src/main/java/org/elasticsearch/indices/IndicesQueryCache.java
@@ -77,7 +77,7 @@ public class IndicesQueryCache implements QueryCache, Closeable {
         final int count = INDICES_CACHE_QUERY_COUNT_SETTING.get(settings);
         logger.debug("using [node] query cache with size [{}] max filter count [{}]", size, count);
         if (INDICES_QUERIES_CACHE_ALL_SEGMENTS_SETTING.get(settings)) {
-            cache = new ElasticsearchLRUQueryCache(count, size.getBytes(), context -> true, 1f);
+            cache = new ElasticsearchLRUQueryCache(count, size.getBytes(), context -> true, 10f);
         } else {
             cache = new ElasticsearchLRUQueryCache(count, size.getBytes());
         }

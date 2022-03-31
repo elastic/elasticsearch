89eca33e6467e27d9b2d087a79e389523e06dda4
ChrisHegarty <christopher.hegarty@elastic.co>
2022-03-31 08:55:43 +0100

    more painless module exports

--- modules/lang-painless/src/main/java/module-info.java
+++ modules/lang-painless/src/main/java/module-info.java
@@ -22,6 +22,20 @@ module org.elasticsearch.painless {
     exports org.elasticsearch.painless;
     exports org.elasticsearch.painless.api;
 
+    // java.lang.IllegalAccessError:
+    // superclass access check failed: class org.elasticsearch.painless.PainlessScript$Script (in unnamed module @0x4a108434)
+    // cannot access class org.elasticsearch.painless.action.PainlessExecuteAction$PainlessTestScript (in module org.elasticsearch.painless)
+    // because module org.elasticsearch.painless does not export org.elasticsearch.painless.action to unnamed module @0x4a108434
+    //   at java.base/java.lang.ClassLoader.defineClass1(Native Method)
+    //   at java.base/java.lang.ClassLoader.defineClass(ClassLoader.java:1012)
+    //   at java.base/java.security.SecureClassLoader.defineClass(SecureClassLoader.java:150)
+    //   at org.elasticsearch.painless@8.2.0-SNAPSHOT/org.elasticsearch.painless.Compiler$Loader.defineScript(Compiler.java:115)
+    //   at org.elasticsearch.painless@8.2.0-SNAPSHOT/org.elasticsearch.painless.Compiler.compile(Compiler.java:225)
+    //   at org.elasticsearch.painless@8.2.0-SNAPSHOT/org.elasticsearch.painless.PainlessScriptEngine$2.run(PainlessScriptEngine.java:401)
+    //   at org.elasticsearch.painless@8.2.0-SNAPSHOT/org.elasticsearch.painless.PainlessScriptEngine$2.run(PainlessScriptEngine.java:397)
+    //   ...
+    exports org.elasticsearch.painless.action;
+
     opens org.elasticsearch.painless to org.elasticsearch.painless.spi;  // whitelist access
     opens org.elasticsearch.painless.action to org.elasticsearch.server; // guice
 }

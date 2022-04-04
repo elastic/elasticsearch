d086ad46d103821ce6190b4474ec6e3af1e46f6e
David Turner <david.turner@elastic.co>
2022-04-04 11:47:15 +0100

    Use expectThrows instead of ThrowableAssertions

--- server/src/test/java/org/elasticsearch/repositories/RepositoriesServiceTests.java
+++ server/src/test/java/org/elasticsearch/repositories/RepositoriesServiceTests.java
@@ -52,8 +52,7 @@ import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
-import static org.elasticsearch.test.hamcrest.ThrowableAssertions.assertThatException;
-import static org.elasticsearch.test.hamcrest.ThrowableAssertions.assertThatThrows;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.isA;
 import static org.mockito.Mockito.mock;
@@ -188,9 +187,8 @@ public class RepositoriesServiceTests extends ESTestCase {
         repositoriesService.applyClusterState(new ClusterChangedEvent("starting", clusterState, emptyState()));
         repositoriesService.applyClusterState(new ClusterChangedEvent("removing repo", emptyState(), clusterState));
 
-        assertThatThrows(
-            () -> repositoriesService.repository(repoName),
-            RepositoryMissingException.class,
+        assertThat(
+            expectThrows(RepositoryMissingException.class, () -> repositoriesService.repository(repoName)).getMessage(),
             equalTo("[" + repoName + "] missing")
         );
     }
@@ -207,7 +205,8 @@ public class RepositoriesServiceTests extends ESTestCase {
 
             @Override
             public void onFailure(Exception e) {
-                assertThatException(e, RepositoryException.class, equalTo("[" + repoName + "] repository type [unknown] does not exist"));
+                assertThat(e, isA(RepositoryException.class));
+                assertThat(e.getMessage(), equalTo("[" + repoName + "] repository type [unknown] does not exist"));
             }
         });
     }

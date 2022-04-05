c52ce29c67f01f0e80c802530cf8859bf7d4e9dd
mashhur <mashhur@elastic.co>
2022-04-04 20:41:10 -0700

    Wildcard support in Logstash pipelines search API.

--- x-pack/plugin/logstash/src/main/java/org/elasticsearch/xpack/logstash/action/TransportGetPipelineAction.java
+++ x-pack/plugin/logstash/src/main/java/org/elasticsearch/xpack/logstash/action/TransportGetPipelineAction.java
@@ -35,7 +35,10 @@ import org.elasticsearch.xpack.logstash.Logstash;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static org.elasticsearch.xpack.core.ClientHelper.LOGSTASH_MANAGEMENT_ORIGIN;
@@ -43,6 +46,13 @@ import static org.elasticsearch.xpack.core.ClientHelper.LOGSTASH_MANAGEMENT_ORIG
 public class TransportGetPipelineAction extends HandledTransportAction<GetPipelineRequest, GetPipelineResponse> {
 
     private static final Logger logger = LogManager.getLogger(TransportGetPipelineAction.class);
+
+    private static final Integer SIZE = 10000;
+
+    private static final String WILDCARD = "*";
+
+    private static final Pattern WILDCARD_PATTERN = Pattern.compile("[^*]+|(\\*)");
+
     private final Client client;
 
     @Inject
@@ -53,7 +63,74 @@ public class TransportGetPipelineAction extends HandledTransportAction<GetPipeli
 
     @Override
     protected void doExecute(Task task, GetPipelineRequest request, ActionListener<GetPipelineResponse> listener) {
-        if (request.ids().isEmpty()) {
+        final Set<String> explicitPipelineIds = request.ids()
+            .stream()
+            .filter(pipeline -> pipeline.contains(WILDCARD) == false)
+            .collect(Collectors.toSet());
+
+        final Set<String> wildcardPipelineIdPatterns = request.ids()
+            .stream()
+            .filter(pipeline -> pipeline.contains(WILDCARD))
+            .map(this::toWildcardPipelineIdPattern)
+            .collect(Collectors.toSet());
+
+        if (explicitPipelineIds.size() > 0 && wildcardPipelineIdPatterns.size() == 0) {
+            getPipelinesByIds(explicitPipelineIds, listener);
+            return;
+        }
+
+        client.prepareSearch(Logstash.LOGSTASH_CONCRETE_INDEX_NAME)
+            .setSource(
+                SearchSourceBuilder.searchSource().fetchSource(true).query(QueryBuilders.matchAllQuery()).size(SIZE).trackTotalHits(true)
+            )
+            .setScroll(TimeValue.timeValueMinutes(1L))
+            .execute(ActionListener.wrap(searchResponse -> {
+                final int numHits = Math.toIntExact(searchResponse.getHits().getTotalHits().value);
+                final Map<String, BytesReference> pipelineSources = Maps.newMapWithExpectedSize(numHits);
+                final Consumer<SearchResponse> clearScroll = (response) -> {
+                    if (response != null && response.getScrollId() != null) {
+                        ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+                        clearScrollRequest.addScrollId(response.getScrollId());
+                        client.clearScroll(
+                            clearScrollRequest,
+                            ActionListener.wrap(
+                                (r) -> {},
+                                e -> logger.warn(
+                                    new ParameterizedMessage("clear scroll failed for scroll id [{}]", response.getScrollId()),
+                                    e
+                                )
+                            )
+                        );
+                    }
+                };
+                final int maxNumHitsToFetch = 0;
+                handleFilteringSearchResponse(
+                    searchResponse,
+                    pipelineSources,
+                    explicitPipelineIds,
+                    wildcardPipelineIdPatterns,
+                    maxNumHitsToFetch,
+                    clearScroll,
+                    listener
+                );
+            }, e -> handleFailure(e, listener)));
+    }
+
+    private void getPipelinesByIds(Set<String> ids, ActionListener<GetPipelineResponse> listener) {
+        client.prepareMultiGet()
+            .addIds(Logstash.LOGSTASH_CONCRETE_INDEX_NAME, ids)
+            .execute(ActionListener.wrap(mGetResponse -> {
+                logFailures(mGetResponse);
+                listener.onResponse(
+                    new GetPipelineResponse(
+                        Arrays.stream(mGetResponse.getResponses())
+                            .filter(itemResponse -> itemResponse.isFailed() == false)
+                            .filter(itemResponse -> itemResponse.getResponse().isExists())
+                            .map(MultiGetItemResponse::getResponse)
+                            .collect(Collectors.toMap(GetResponse::getId, GetResponse::getSourceAsBytesRef))
+                    ));
+            }, e -> handleFailure(e, listener)));
+        /*if (request.ids().isEmpty()) {
             client.prepareSearch(Logstash.LOGSTASH_CONCRETE_INDEX_NAME)
                 .setSource(
                     SearchSourceBuilder.searchSource()
@@ -106,7 +183,7 @@ public class TransportGetPipelineAction extends HandledTransportAction<GetPipeli
                         )
                     );
                 }, e -> handleFailure(e, listener)));
-        }
+        }*/
     }
 
     private void handleFailure(Exception e, ActionListener<GetPipelineResponse> listener) {
@@ -118,37 +195,66 @@ public class TransportGetPipelineAction extends HandledTransportAction<GetPipeli
         }
     }
 
-    private void handleSearchResponse(
+    private void handleFilteringSearchResponse(
         SearchResponse searchResponse,
         Map<String, BytesReference> pipelineSources,
+        Set<String> explicitPipelineIds,
+        Set<String> wildcardPipelineIdPatterns,
+        int maxNumHitsToFetch,
         Consumer<SearchResponse> clearScroll,
         ActionListener<GetPipelineResponse> listener
     ) {
+        maxNumHitsToFetch += searchResponse.getHits().getHits().length;
         for (SearchHit hit : searchResponse.getHits().getHits()) {
-            pipelineSources.put(hit.getId(), hit.getSourceRef());
+            if (explicitPipelineIds.isEmpty() && wildcardPipelineIdPatterns.isEmpty()) {
+                pipelineSources.put(hit.getId(), hit.getSourceRef());
+                continue;
+            }
+
+            // take if ID is in request IDs set
+            if (explicitPipelineIds.contains(hit.getId())) {
+                pipelineSources.put(hit.getId(), hit.getSourceRef());
+                continue;
+            }
+            // take if id matches request wildcard pattern
+            for (String wildcardPipelineIdPattern : wildcardPipelineIdPatterns) {
+                if (Pattern.matches(wildcardPipelineIdPattern, hit.getId())) {
+                    pipelineSources.put(hit.getId(), hit.getSourceRef());
+                    break;
+                }
+            }
         }
 
-        if (pipelineSources.size() > searchResponse.getHits().getTotalHits().value) {
+        if (maxNumHitsToFetch > searchResponse.getHits().getTotalHits().value) {
             clearScroll.accept(searchResponse);
             listener.onFailure(
                 new IllegalStateException(
                     "scrolling returned more hits ["
-                        + pipelineSources.size()
+                        + maxNumHitsToFetch
                         + "] than expected ["
                         + searchResponse.getHits().getTotalHits().value
                         + "] so bailing out to prevent unbounded "
                         + "memory consumption."
                 )
             );
-        } else if (pipelineSources.size() == searchResponse.getHits().getTotalHits().value) {
+        } else if (maxNumHitsToFetch == searchResponse.getHits().getTotalHits().value) {
             clearScroll.accept(searchResponse);
             listener.onResponse(new GetPipelineResponse(pipelineSources));
         } else {
+            int finalNumHitsToFetch = maxNumHitsToFetch;
             client.prepareSearchScroll(searchResponse.getScrollId())
                 .setScroll(TimeValue.timeValueMinutes(1L))
                 .execute(
                     ActionListener.wrap(
-                        searchResponse1 -> handleSearchResponse(searchResponse1, pipelineSources, clearScroll, listener),
+                        searchResponse1 -> handleFilteringSearchResponse(
+                            searchResponse1,
+                            pipelineSources,
+                            explicitPipelineIds,
+                            wildcardPipelineIdPatterns,
+                            finalNumHitsToFetch,
+                            clearScroll,
+                            listener
+                        ),
                         listener::onFailure
                     )
                 );
@@ -164,5 +270,19 @@ public class TransportGetPipelineAction extends HandledTransportAction<GetPipeli
         if (ids.isEmpty() == false) {
             logger.info("Could not retrieve logstash pipelines with ids: {}", ids);
         }
+    }
+
+    private String toWildcardPipelineIdPattern(String wildcardPipelineId) {
+        Matcher matcher = WILDCARD_PATTERN.matcher(wildcardPipelineId);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (matcher.find()) {
+            if (matcher.group(1) != null) {
+                matcher.appendReplacement(stringBuilder, ".*");
+            } else {
+                matcher.appendReplacement(stringBuilder, "\\\\Q" + matcher.group(0) + "\\\\E");
+            }
+        }
+        matcher.appendTail(stringBuilder);
+        return stringBuilder.toString();
     }
 }

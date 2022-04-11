b361ff2335ba06004eebc2a32360138c59d8c085
Ryan Ernst <ryan@iernst.net>
2022-04-11 14:52:01 -0700

    Use multi-release-jar for java version checker (#85767)
    The java version checker tests whether the current jdk is a sufficient
    version to run Elasticsearch. Since it is compatible with java 7, it
    must parse Java's version sysprop.
    
    This commit reworks the checker to no longer need to parse the version.
    Instead, it uses a multi-release-jar, so that there are two versions of
    the main class. The Java 7 compatible version simply exits with an error
    message. The Java 17 version simple exits with no error.
    
    relates #85758

--- distribution/tools/java-version-checker/src/unsupportedJdkVersionEntrypoint/java/org/elasticsearch/tools/java_version_checker/JavaVersionChecker.java
+++ distribution/tools/java-version-checker/src/unsupportedJdkVersionEntrypoint/java/org/elasticsearch/tools/java_version_checker/JavaVersionChecker.java
@@ -9,9 +9,10 @@
 package org.elasticsearch.tools.java_version_checker;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 /**
- * Java 17 compatible main which just exits without error.
+ * Java 7 compatible main which exits with an error.
  */
 final class JavaVersionChecker {
 
@@ -22,5 +23,13 @@ final class JavaVersionChecker {
         if (args.length != 0) {
             throw new IllegalArgumentException("expected zero arguments but was " + Arrays.toString(args));
         }
+        final String message = String.format(
+            Locale.ROOT,
+            "The minimum required Java version is 17; your Java version %s from [%s] does not meet that requirement.",
+            System.getProperty("java.specification.version"),
+            System.getProperty("java.home")
+        );
+        System.err.println(message);
+        System.exit(1);
     }
 }

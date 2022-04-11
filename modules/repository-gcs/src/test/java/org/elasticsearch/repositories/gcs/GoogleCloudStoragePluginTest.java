6fd4f2d0a3b7d41336ba2eb64a6f3ecceff845f8
Artem Prigoda <artem.prigoda@elastic.co>
2022-04-11 17:33:16 +0200

    Expose proxy settings for GCS repositories
    They were added in #82737, but not exposed via the GCSPlugin to end users.

---
+++ modules/repository-gcs/src/test/java/org/elasticsearch/repositories/gcs/GoogleCloudStoragePluginTest.java
@@ -0,0 +1,32 @@
+package org.elasticsearch.repositories.gcs;
+
+import org.elasticsearch.common.settings.Setting;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.test.ESTestCase;
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GoogleCloudStoragePluginTest extends ESTestCase {
+
+    public void testGetSettings() {
+        List<Setting<?>> settings = new GoogleCloudStoragePlugin(Settings.EMPTY).getSettings();
+
+        Assert.assertEquals(
+            List.of(
+                "gcs.client.*.credentials_file",
+                "gcs.client.*.endpoint",
+                "gcs.client.*.project_id",
+                "gcs.client.*.connect_timeout",
+                "gcs.client.*.read_timeout",
+                "gcs.client.*.application_name",
+                "gcs.client.*.token_uri",
+                "gcs.client.*.proxy.type",
+                "gcs.client.*.proxy.host",
+                "gcs.client.*.proxy.port"
+            ),
+            settings.stream().map(Setting::getKey).collect(Collectors.toList())
+        );
+    }
+}

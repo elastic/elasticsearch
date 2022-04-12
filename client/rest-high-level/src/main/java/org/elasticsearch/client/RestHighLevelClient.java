e7d77aaa8400e05b8ea59753d0fefa829f9a6fec
Joe Gallo <joe.gallo@elastic.co>
2022-04-12 14:15:02 -0400

    Drop some unused methods

--- client/rest-high-level/src/main/java/org/elasticsearch/client/RestHighLevelClient.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/RestHighLevelClient.java
@@ -64,7 +64,6 @@ import org.elasticsearch.client.core.MultiTermVectorsRequest;
 import org.elasticsearch.client.core.MultiTermVectorsResponse;
 import org.elasticsearch.client.core.TermVectorsRequest;
 import org.elasticsearch.client.core.TermVectorsResponse;
-import org.elasticsearch.client.tasks.TaskSubmissionResponse;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.util.concurrent.FutureUtils;
 import org.elasticsearch.common.util.concurrent.ListenableFuture;
@@ -431,23 +430,6 @@ public class RestHighLevelClient implements Closeable {
     }
 
     /**
-     * Submits a reindex task.
-     * See <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-reindex.html">Reindex API on elastic.co</a>
-     * @param reindexRequest the request
-     * @param options the request options (e.g. headers), use {@link RequestOptions#DEFAULT} if nothing needs to be customized
-     * @return the submission response
-     */
-    public final TaskSubmissionResponse submitReindexTask(ReindexRequest reindexRequest, RequestOptions options) throws IOException {
-        return performRequestAndParseEntity(
-            reindexRequest,
-            RequestConverters::submitReindex,
-            options,
-            TaskSubmissionResponse::fromXContent,
-            emptySet()
-        );
-    }
-
-    /**
      * Asynchronously executes a reindex request.
      * See <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-reindex.html">Reindex API on elastic.co</a>
      * @param reindexRequest the request
@@ -485,25 +467,6 @@ public class RestHighLevelClient implements Closeable {
             options,
             BulkByScrollResponse::fromXContent,
             singleton(409)
-        );
-    }
-
-    /**
-     * Submits a update by query task.
-     * See <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-update-by-query.html">
-     *     Update By Query API on elastic.co</a>
-     * @param updateByQueryRequest the request
-     * @param options the request options (e.g. headers), use {@link RequestOptions#DEFAULT} if nothing needs to be customized
-     * @return the submission response
-     */
-    public final TaskSubmissionResponse submitUpdateByQueryTask(UpdateByQueryRequest updateByQueryRequest, RequestOptions options)
-        throws IOException {
-        return performRequestAndParseEntity(
-            updateByQueryRequest,
-            RequestConverters::submitUpdateByQuery,
-            options,
-            TaskSubmissionResponse::fromXContent,
-            emptySet()
         );
     }
 
@@ -546,25 +509,6 @@ public class RestHighLevelClient implements Closeable {
             options,
             BulkByScrollResponse::fromXContent,
             singleton(409)
-        );
-    }
-
-    /**
-     * Submits a delete by query task
-     * See <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-delete-by-query.html">
-     *      Delete By Query API on elastic.co</a>
-     * @param deleteByQueryRequest the request
-     * @param options the request options (e.g. headers), use {@link RequestOptions#DEFAULT} if nothing needs to be customized
-     * @return the submission response
-     */
-    public final TaskSubmissionResponse submitDeleteByQueryTask(DeleteByQueryRequest deleteByQueryRequest, RequestOptions options)
-        throws IOException {
-        return performRequestAndParseEntity(
-            deleteByQueryRequest,
-            RequestConverters::submitDeleteByQuery,
-            options,
-            TaskSubmissionResponse::fromXContent,
-            emptySet()
         );
     }
 

fdbe843e7be38412d3936b3d6fcf00a9ab0e73c5
Rory Hunter <roryhunter2@gmail.com>
2022-04-13 15:35:27 +0100

    Fixes for using latest agent version

--- run.sh
+++ run.sh
@@ -2,17 +2,20 @@
 
 set -eo pipefail
 
+ES_VERSION=$(awk '/^elasticsearch/ { print $3 }' build-tools-internal/version.properties)
 AGENT_VERSION=$(awk '/apm_agent/ { print $3 }' build-tools-internal/version.properties)
 
 # This is the path that `./gradlew localDistro` prints out at the end
 cd build/distribution/local/elasticsearch-8.3.0-SNAPSHOT
 
+sed -i.bak -e "s|<ES_HOME>|$PWD|" modules/apm-integration/plugin-security.policy
+
 # URL and token for sending traces
-SERVER_URL="https://apm-testing.apm.us-west2.gcp.elastic-cloud.com"
-SECRET_TOKEN="bNJCAZxDjQwIFKxdk2"
+SERVER_URL=""
+SECRET_TOKEN=""
 
 # Optional - override the agent jar
-# OVERRIDE_AGENT_JAR="$HOME/.m2/repository/co/elastic/apm/elastic-apm-agent/1.30.1-SNAPSHOT/elastic-apm-agent-1.30.1-SNAPSHOT.jar"
+# OVERRIDE_AGENT_JAR="$HOME/.m2/repository/co/elastic/apm/elastic-apm-agent/1.30.2-SNAPSHOT/elastic-apm-agent-1.30.2-SNAPSHOT.jar"
 
 # Clear this so that ES doesn't repeatedly complain about ignoring it
 export JAVA_HOME=''
@@ -35,9 +38,9 @@ AGENT_OPTS="$AGENT_OPTS -Delastic.apm.service_name=elasticsearch"
 AGENT_OPTS="$AGENT_OPTS -Delastic.apm.instrument=false"
 AGENT_OPTS="$AGENT_OPTS -Delastic.apm.server_url=$SERVER_URL"
 AGENT_OPTS="$AGENT_OPTS -Delastic.apm.secret_token=$SECRET_TOKEN"
-AGENT_OPTS="$AGENT_OPTS -Delastic.apm.service_version=8.2.0-SNAPSHOT"
+AGENT_OPTS="$AGENT_OPTS -Delastic.apm.service_version=$ES_VERSION-SNAPSHOT"
 AGENT_OPTS="$AGENT_OPTS -Delastic.apm.environment=dev"
-AGENT_OPTS="$AGENT_OPTS -Delastic.apm.log_level=trace"
+AGENT_OPTS="$AGENT_OPTS -Delastic.apm.log_level=debug"
 AGENT_OPTS="$AGENT_OPTS -Delastic.apm.log_file=$PWD/apm.log"
 AGENT_OPTS="$AGENT_OPTS -Delastic.apm.enable_experimental_instrumentations=true"
 

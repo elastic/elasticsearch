319bb1dde7bb80221f60c45510c4b202c3a9d60d
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2022-04-13 10:34:33 -0400

    updating bwc

--- x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/MLModelDeploymentsUpgradeIT.java
+++ x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/MLModelDeploymentsUpgradeIT.java
@@ -21,7 +21,6 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
@@ -72,13 +71,9 @@ public class MLModelDeploymentsUpgradeIT extends AbstractUpgradeTestCase {
                     request.addParameter("wait_for_status", "yellow");
                     request.addParameter("timeout", "70s");
                 }));
-                if (UPGRADE_FROM_VERSION.onOrAfter(Version.V_8_3_0)) {
-                    waitForDeploymentStarted(modelId);
-                    assertInfer(modelId);
-                    assertInfer(modelId);
-                } else {
-                    ensureDeploymentExistsAndIsStartedOrFailed(modelId);
-                }
+                waitForDeploymentStarted(modelId);
+                assertInfer(modelId);
+                assertInfer(modelId);
             }
             case UPGRADED -> {
                 ensureHealth(".ml-inference-*,.ml-config*", (request -> {
@@ -86,12 +81,8 @@ public class MLModelDeploymentsUpgradeIT extends AbstractUpgradeTestCase {
                     request.addParameter("timeout", "70s");
                 }));
 
-                if (UPGRADE_FROM_VERSION.onOrAfter(Version.V_8_3_0)) {
-                    waitForDeploymentStarted(modelId);
-                    assertInfer(modelId);
-                } else {
-                    ensureDeploymentExistsAndIsStartedOrFailed(modelId);
-                }
+                waitForDeploymentStarted(modelId);
+                assertInfer(modelId);
                 stopDeployment(modelId);
             }
             default -> throw new UnsupportedOperationException("Unknown cluster type [" + CLUSTER_TYPE + "]");
@@ -148,27 +139,6 @@ public class MLModelDeploymentsUpgradeIT extends AbstractUpgradeTestCase {
                 equalTo("fully_allocated")
             );
             assertThat(stat.toString(), XContentMapValues.extractValue("deployment_stats.state", stat), equalTo("started"));
-        }, 30, TimeUnit.SECONDS);
-    }
-
-    @SuppressWarnings("unchecked")
-    private void ensureDeploymentExistsAndIsStartedOrFailed(String modelId) throws Exception {
-        assertBusy(() -> {
-            var response = getTrainedModelStats(modelId);
-            Map<String, Object> map = entityAsMap(response);
-            List<Map<String, Object>> stats = (List<Map<String, Object>>) map.get("trained_model_stats");
-            assertThat(stats, hasSize(1));
-            var stat = stats.get(0);
-            assertThat(
-                stat.toString(),
-                XContentMapValues.extractValue("deployment_stats.allocation_status.state", stat),
-                anyOf(equalTo("fully_allocated"), equalTo("started"))
-            );
-            assertThat(
-                stat.toString(),
-                XContentMapValues.extractValue("deployment_stats.state", stat),
-                anyOf(equalTo("started"), equalTo("failed"))
-            );
         }, 30, TimeUnit.SECONDS);
     }
 

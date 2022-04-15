cbc4c6bc179441b5337d1b4ac09cbff03c00a1e8
Ryan Ernst <ryan@iernst.net>
2022-04-15 08:10:15 -0700

    cleanup tests and add some comments

--- server/src/test/java/org/elasticsearch/common/cli/EnvironmentAwareCommandTests.java
+++ server/src/test/java/org/elasticsearch/common/cli/EnvironmentAwareCommandTests.java
@@ -65,7 +65,8 @@ public class EnvironmentAwareCommandTests extends CommandTestCase {
         };
     }
 
-    public void testNonDockerEnvVarsIgnored() throws Exception {
+    // Check that for non-Docker, environment variables are not translated into settings
+    public void testNonDockerEnvVarSettingsIgnored() throws Exception {
         mockEnvVars.put("ES_SETTING_FOO_BAR", "baz");
         mockEnvVars.put("some.setting", "1");
         callback = env -> {
@@ -76,29 +77,59 @@ public class EnvironmentAwareCommandTests extends CommandTestCase {
         execute();
     }
 
-    public void testDockerEnvSettings() throws Exception {
-        buildType = Build.Type.DOCKER;
-        mockEnvVars.put("ES_SETTING_SIMPLE_SETTING", "baz");
-        mockEnvVars.put("ES_SETTING_UNDERSCORE__HERE", "baz2");
-        mockEnvVars.put("ES_SETTING_UNDERSCORE__DOT_BAZ", "buzz");
-        mockEnvVars.put("ES_SETTING_DOUBLE____UNDERSCORE", "buzz");
-        mockEnvVars.put("ES_SETTING_TRIPLE___BAZ", "buzz");
-        mockEnvVars.put("lowercase.setting", "baz");
-        mockEnvVars.put("setting.Ignored", "baz");
+    // Check that for Docker, environment variables that do not match the criteria for translation to settings are ignored.
+    public void testDockerEnvVarSettingsIgnored() throws Exception {
+        // No ES_SETTING_ prefix
+        mockEnvVars.put("XPACK_SECURITY_FIPS__MODE_ENABLED", "false");
+        // Incomplete prefix
+        mockEnvVars.put("ES_XPACK_SECURITY_FIPS__MODE_ENABLED", "false");
+        // Not underscore-separated
+        mockEnvVars.put("ES.SETTING.XPACK.SECURITY.FIPS_MODE.ENABLED", "false");
+        // Not uppercase
+        mockEnvVars.put("es_setting_xpack_security_fips__mode_enabled", "false");
+        // single word is not translated, it must contain a dot
+        mockEnvVars.put("singleword", "value");
+        // any uppercase letters cause the var to be ignored
+        mockEnvVars.put("setting.Ignored", "value");
         callback = env -> {
             Settings settings = env.settings();
-            assertThat(settings.hasValue("simple.setting"), is(true));
-            assertThat(settings.hasValue("underscore_here"), is(true));
-            assertThat(settings.hasValue("underscore_dot.baz"), is(true));
-            assertThat(settings.hasValue("triple_.baz"), is(true));
-            assertThat(settings.hasValue("double__underscore"), is(true));
-            assertThat(settings.hasValue("lowercase.setting"), is(true));
+            assertThat(settings.hasValue("xpack.security.fips_mode.enabled"), is(false));
+            assertThat(settings.hasValue("singleword"), is(false));
             assertThat(settings.hasValue("setting.Ignored"), is(false));
+            assertThat(settings.hasValue("setting.ignored"), is(false));
         };
         execute();
     }
 
-    public void testDockerEnvSettingsOverride() throws Exception {
+    // Check that for Docker builds, various env vars are translated correctly to settings
+    public void testDockerEnvVarSettingsTranslated() throws Exception {
+        buildType = Build.Type.DOCKER;
+        // normal setting with a dot
+        mockEnvVars.put("ES_SETTING_SIMPLE_SETTING", "value");
+        // double underscore is translated to literal underscore
+        mockEnvVars.put("ES_SETTING_UNDERSCORE__HERE", "value");
+        // literal underscore and a dot
+        mockEnvVars.put("ES_SETTING_UNDERSCORE__DOT_BAZ", "value");
+        // two literal underscores
+        mockEnvVars.put("ES_SETTING_DOUBLE____UNDERSCORE", "value");
+        // literal underscore followed by a dot (not valid setting, but translated nonetheless
+        mockEnvVars.put("ES_SETTING_TRIPLE___BAZ", "value");
+        // lowercase
+        mockEnvVars.put("lowercase.setting", "value");
+        callback = env -> {
+            Settings settings = env.settings();
+            assertThat(settings.get("simple.setting"), equalTo("value"));
+            assertThat(settings.get("underscore_here"), equalTo("value"));
+            assertThat(settings.get("underscore_dot.baz"), equalTo("value"));
+            assertThat(settings.get("triple_.baz"), equalTo("value"));
+            assertThat(settings.get("double__underscore"), equalTo("value"));
+            assertThat(settings.get("lowercase.setting"), equalTo("value"));
+        };
+        execute();
+    }
+
+    // Check that for Docker builds, env vars takes precedence over settings on the command line.
+    public void testDockerEnvVarSettingsOverrideCommandLine() throws Exception {
         // docker env takes precedence over settings on the command line
         buildType = Build.Type.DOCKER;
         mockEnvVars.put("ES_SETTING_SIMPLE_SETTING", "override");

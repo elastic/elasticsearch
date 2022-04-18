9f46aae6150e241ffe170167076709cebfc06196
Ryan Ernst <ryan@iernst.net>
2022-04-18 08:22:50 -0700

    Consolidating logging initialization in cli launcher (#85920)
    Serveral mechanisms exist for intializing logging in cli tools. Some
    base Command classes exist which initialize logging. But they do this
    late, when they are constructed, which may be after static init has
    occured for classes grabbing a Logger. Other CLIs like node tool
    explicitly initialize logging to avoid that problem.
    
    This commit removes all the of the LoggingAware classes, and
    unifies logging configuration to occur at the very beginning of the cli
    launcher.
    
    relates #85758

--- libs/cli/src/main/java/org/elasticsearch/cli/Command.java
+++ libs/cli/src/main/java/org/elasticsearch/cli/Command.java
@@ -33,8 +33,6 @@ public abstract class Command implements Closeable {
     /** A description of the command, used in the help output. */
     protected final String description;
 
-    private final Runnable beforeMain;
-
     // these are the system properties and env vars from the environment,
     // but they can be overriden by tests. Really though Command should be stateless,
     // so the signature of main should take them in, which can happen once the entrypoint
@@ -52,13 +50,11 @@ public abstract class Command implements Closeable {
 
     /**
      * Construct the command with the specified command description and runnable to execute before main is invoked.
+     *  @param description the command description
      *
-     * @param description the command description
-     * @param beforeMain the before-main runnable
      */
-    public Command(final String description, final Runnable beforeMain) {
+    public Command(final String description) {
         this.description = description;
-        this.beforeMain = beforeMain;
         this.sysprops = captureSystemProperties();
         this.envVars = captureEnvironmentVariables();
     }
@@ -85,8 +81,6 @@ public abstract class Command implements Closeable {
             });
             Runtime.getRuntime().addShutdownHook(shutdownHookThread);
         }
-
-        beforeMain.run();
 
         try {
             mainWithoutErrorHandling(args, terminal);

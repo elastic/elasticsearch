0497825bf339c79c2ec82772849048e2d00459b8
Ryan Ernst <ryan@iernst.net>
2022-04-21 07:27:41 -0700

    Remove hppc from nested aggs
    The nested and reverse nested aggs use hppc maps and lists for mapping buckets to
    ordinals. This commit converts these to use HashMaps and ArrayLists.
    
    relates #84735

--- server/src/main/java/org/elasticsearch/search/aggregations/bucket/nested/NestedAggregator.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/bucket/nested/NestedAggregator.java
@@ -7,8 +7,6 @@
  */
 package org.elasticsearch.search.aggregations.bucket.nested;
 
-import com.carrotsearch.hppc.LongArrayList;
-
 import org.apache.lucene.index.IndexReaderContext;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.ReaderUtil;
@@ -35,6 +33,9 @@ import org.elasticsearch.search.aggregations.support.AggregationContext;
 import org.elasticsearch.xcontent.ParseField;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class NestedAggregator extends BucketsAggregator implements SingleBucketAggregator {
@@ -141,7 +142,7 @@ public class NestedAggregator extends BucketsAggregator implements SingleBucketA
         final BitSet parentDocs;
         final LeafBucketCollector sub;
         final DocIdSetIterator childDocs;
-        final LongArrayList bucketBuffer = new LongArrayList();
+        final List<Long> bucketBuffer = new ArrayList<>();
 
         Scorable scorer;
         int currentParentDoc = -1;
@@ -193,11 +194,14 @@ public class NestedAggregator extends BucketsAggregator implements SingleBucketA
 
             for (; childDocId < currentParentDoc; childDocId = childDocs.nextDoc()) {
                 cachedScorer.doc = childDocId;
-                final long[] buffer = bucketBuffer.buffer;
-                final int size = bucketBuffer.size();
-                for (int i = 0; i < size; i++) {
-                    collectBucket(sub, childDocId, buffer[i]);
-                }
+                final int docId = childDocId;
+                bucketBuffer.stream().mapToLong(Long::longValue).forEach(bucket -> {
+                    try {
+                        collectBucket(sub, docId, bucket);
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                });
             }
             bucketBuffer.clear();
         }

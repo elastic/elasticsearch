6d0a183232ee5d9f39ad095f29e84d96474606d6
Yannick Welsch <yannick@welsch.lu>
2022-04-26 16:59:03 +0200

    Fix _terms_enum on unconfigured constant_keyword

--- x-pack/plugin/mapper-constant-keyword/src/main/java/org/elasticsearch/xpack/constantkeyword/mapper/ConstantKeywordFieldMapper.java
+++ x-pack/plugin/mapper-constant-keyword/src/main/java/org/elasticsearch/xpack/constantkeyword/mapper/ConstantKeywordFieldMapper.java
@@ -151,11 +151,14 @@ public class ConstantKeywordFieldMapper extends FieldMapper {
 
         @Override
         public TermsEnum getTerms(boolean caseInsensitive, String string, SearchExecutionContext queryShardContext, String searchAfter) {
+            if (value == null) {
+                return TermsEnum.EMPTY;
+            }
             boolean matches = caseInsensitive
                 ? value.toLowerCase(Locale.ROOT).startsWith(string.toLowerCase(Locale.ROOT))
                 : value.startsWith(string);
             if (matches == false) {
-                return null;
+                return TermsEnum.EMPTY;
             }
             if (searchAfter != null) {
                 if (searchAfter.compareTo(value) >= 0) {

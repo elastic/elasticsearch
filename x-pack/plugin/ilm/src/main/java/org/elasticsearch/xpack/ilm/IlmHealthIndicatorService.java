f5684037822a7f71b2c4b2d544bac9c7e75ba6ab
Keith Massey <keith.massey@elastic.co>
2022-05-03 15:19:44 -0500

    checkstyle

--- x-pack/plugin/ilm/src/main/java/org/elasticsearch/xpack/ilm/IlmHealthIndicatorService.java
+++ x-pack/plugin/ilm/src/main/java/org/elasticsearch/xpack/ilm/IlmHealthIndicatorService.java
@@ -68,7 +68,8 @@ public class IlmHealthIndicatorService implements HealthIndicatorService {
             List<HealthIndicatorImpact> impacts = Collections.singletonList(
                 new HealthIndicatorImpact(
                     3,
-                    "Automatic index lifecycle and data retention management is disabled. The performance and stability of your system could be impacted.",
+                    "Automatic index lifecycle and data retention management is disabled. The performance and stability of your system " +
+                        "could be impacted.",
                     List.of(ImpactArea.DEPLOYMENT_MANAGEMENT)
                 )
             );

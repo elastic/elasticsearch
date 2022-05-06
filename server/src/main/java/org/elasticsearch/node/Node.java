c97ff8dc93d671cb479fd71d8b5abd87b486cf54
Keith Massey <keith.massey@elastic.co>
2022-05-06 11:03:03 -0500

    reverting accidental change

--- server/src/main/java/org/elasticsearch/node/Node.java
+++ server/src/main/java/org/elasticsearch/node/Node.java
@@ -1030,7 +1030,7 @@ public class Node implements Closeable {
             this.namedXContentRegistry = xContentRegistry;
 
             logger.debug("initializing HTTP handlers ...");
-            actionModule.initRestHandlers(() -> clusterService.state().nodes());
+            actionModule.initRestHandlers(() -> clusterService.state().nodesIfRecovered());
             logger.info("initialized");
 
             success = true;

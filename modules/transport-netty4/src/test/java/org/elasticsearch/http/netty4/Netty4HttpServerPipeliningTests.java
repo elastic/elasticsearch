337c0446a07584ff8d7fcf20aceb9d470ecb4bf5
Armin Braun <me@obrown.io>
2022-05-13 11:04:35 +0200

    Flatten Netty4 http pipeline handlers into a single handler (#86133)
    This sets up the implementation of chunked REST response encoding
    that will be able to send large responses without serializing them
    in full on heap upfront.
    Having all the steps of sending a response in one place together with
    the pipelining logic allows adding another response type that serializes
    on the transport threads alongside `Netty4HttpResponse` in a follow-up
    that will only require changes to `Netty4PipeliningHandler`.
    Also, this change in isolation saves loads of indirection and now (that the NIO
    transport is gone) redundant abstractions, making the path of sending a REST
    response all around easier to follow.

--- modules/transport-netty4/src/test/java/org/elasticsearch/http/netty4/Netty4HttpServerPipeliningTests.java
+++ modules/transport-netty4/src/test/java/org/elasticsearch/http/netty4/Netty4HttpServerPipeliningTests.java
@@ -10,14 +10,10 @@ package org.elasticsearch.http.netty4;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelPromise;
-import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.util.ReferenceCounted;
 
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.network.NetworkService;
 import org.elasticsearch.common.settings.ClusterSettings;
@@ -25,7 +21,8 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.util.MockBigArrays;
 import org.elasticsearch.common.util.MockPageCacheRecycler;
-import org.elasticsearch.http.HttpPipelinedRequest;
+import org.elasticsearch.http.HttpChannel;
+import org.elasticsearch.http.HttpRequest;
 import org.elasticsearch.http.HttpResponse;
 import org.elasticsearch.http.HttpServerTransport;
 import org.elasticsearch.http.NullDispatcher;
@@ -116,97 +113,43 @@ public class Netty4HttpServerPipeliningTests extends ESTestCase {
         }
 
         @Override
-        public ChannelHandler configureServerChannelHandler() {
-            return new CustomHttpChannelHandler(this, executorService);
-        }
-
-        @Override
         protected void doClose() {
             executorService.shutdown();
             super.doClose();
         }
 
-    }
-
-    private class CustomHttpChannelHandler extends Netty4HttpServerTransport.HttpChannelHandler {
-
-        private final ExecutorService executorService;
-
-        CustomHttpChannelHandler(Netty4HttpServerTransport transport, ExecutorService executorService) {
-            super(transport, transport.handlingSettings);
-            this.executorService = executorService;
-        }
-
         @Override
-        protected void initChannel(Channel ch) throws Exception {
-            super.initChannel(ch);
-            ch.pipeline().replace("handler", "handler", new PossiblySlowUpstreamHandler(executorService));
-        }
+        public void incomingRequest(HttpRequest httpRequest, HttpChannel httpChannel) {
+            executorService.submit(() -> {
+                final Netty4HttpRequest pipelinedRequest = (Netty4HttpRequest) httpRequest;
+                try {
+                    final String uri = pipelinedRequest.uri();
 
-    }
+                    final ByteBuf buffer = Unpooled.copiedBuffer(uri, StandardCharsets.UTF_8);
 
-    class PossiblySlowUpstreamHandler extends SimpleChannelInboundHandler<HttpPipelinedRequest> {
+                    HttpResponse response = pipelinedRequest.createResponse(
+                        RestStatus.OK,
+                        new BytesArray(uri.getBytes(StandardCharsets.UTF_8))
+                    );
+                    response.addHeader("content-length", Integer.toString(buffer.readableBytes()));
 
-        private final ExecutorService executorService;
-
-        PossiblySlowUpstreamHandler(ExecutorService executorService) {
-            this.executorService = executorService;
-        }
-
-        @Override
-        protected void channelRead0(ChannelHandlerContext ctx, HttpPipelinedRequest msg) throws Exception {
-            executorService.submit(new PossiblySlowRunnable(ctx, msg));
-        }
-
-        @Override
-        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-            logger.info("Caught exception", cause);
-            ctx.channel().close().sync();
-        }
-
-    }
-
-    class PossiblySlowRunnable implements Runnable {
-
-        private ChannelHandlerContext ctx;
-        private HttpPipelinedRequest pipelinedRequest;
-
-        PossiblySlowRunnable(ChannelHandlerContext ctx, HttpPipelinedRequest msg) {
-            this.ctx = ctx;
-            this.pipelinedRequest = msg;
-        }
-
-        @Override
-        public void run() {
-            try {
-                final String uri = pipelinedRequest.uri();
-
-                final ByteBuf buffer = Unpooled.copiedBuffer(uri, StandardCharsets.UTF_8);
-
-                HttpResponse response = pipelinedRequest.createResponse(
-                    RestStatus.OK,
-                    new BytesArray(uri.getBytes(StandardCharsets.UTF_8))
-                );
-                response.addHeader("content-length", Integer.toString(buffer.readableBytes()));
-
-                final boolean slow = uri.matches("/slow/\\d+");
-                if (slow) {
-                    try {
-                        Thread.sleep(scaledRandomIntBetween(500, 1000));
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
+                    final boolean slow = uri.matches("/slow/\\d+");
+                    if (slow) {
+                        try {
+                            Thread.sleep(scaledRandomIntBetween(500, 1000));
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else {
+                        assert uri.matches("/\\d+");
                     }
-                } else {
-                    assert uri.matches("/\\d+");
+
+                    httpChannel.sendResponse(response, ActionListener.noop());
+                } finally {
+                    pipelinedRequest.release();
                 }
-
-                final ChannelPromise promise = ctx.newPromise();
-                ctx.writeAndFlush(response, promise);
-            } finally {
-                pipelinedRequest.release();
-            }
+            });
         }
-
     }
 
 }

83bf1d0f4883a6ebfe163afdfc7855b81d4bc93a
ChrisHegarty <christopher.hegarty@elastic.co>
2022-05-13 15:24:00 +0100

    Revert "cleanup"
    This reverts commit fb3c35f82556c34e180dca78b4588a808302bd31.

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/ElasticsearchJavaPlugin.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/ElasticsearchJavaPlugin.java
@@ -43,6 +43,7 @@ public class ElasticsearchJavaPlugin implements Plugin<Project> {
     public void apply(Project project) {
         project.getPluginManager().apply(ElasticsearchJavaBasePlugin.class);
         project.getPluginManager().apply(JavaLibraryPlugin.class);
+        project.getPluginManager().apply(ElasticsearchJavaModulePathPlugin.class);
 
         // configureConfigurations(project);
         configureJars(project);

elastic/elasticsearch

commit 8f9d2fa924bb50a0eb604c599da41ed5810d0bfa

Complete all transport handlers at shutdown (#85131)
Transport handlers may hold resources that need to be released, so we
must complete them all to avoid leaks. Also, the completion of a handler
may fork onto a different threadpool. Today we fork using a plain
`Runnable` in various places, but this means that the completion may be
rejected if the threadpool queue is full or the executor is shutting
down. This commit changes the behaviour to force execution even if the
queue is full, and to complete the handler directly if the target
executor is shutting down.

Closes #84948

server/src/main/java/org/elasticsearch/transport/TransportService.java
@@ -31,6 +31,7 @@ import org.elasticsearch.common.transport.BoundTransportAddress;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.util.concurrent.AbstractRunnable;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
+import org.elasticsearch.core.AbstractRefCounted;
 import org.elasticsearch.core.Nullable;
 import org.elasticsearch.core.Releasable;
 import org.elasticsearch.core.TimeValue;
@@ -52,6 +53,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -80,6 +83,8 @@ public class TransportService extends AbstractLifecycleComponent
     private final boolean remoteClusterClient;
     private final Transport.ResponseHandlers responseHandlers;
     private final TransportInterceptor interceptor;
+
+    private final PendingDirectHandlers pendingDirectHandlers = new PendingDirectHandlers();
 
     // An LRU (don't really care about concurrency here) that holds the latest timed out requests so if they
     // do show up, we can print more descriptive information about them
@@ -285,27 +290,42 @@ public class TransportService extends AbstractLifecycleComponent
     @Override
     protected void doStop() {
         try {
-            IOUtils.close(connectionManager, remoteClusterService, transport::stop);
+            IOUtils.close(connectionManager, remoteClusterService, transport::stop, pendingDirectHandlers::stop);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         } finally {
-            // in case the transport is not connected to our local node (thus cleaned on node disconnect)
-            // make sure to clean any leftover on going handles
+            // The underlying transport has stopped which closed all the connections to remote nodes and hence completed all their handlers,
+            // but there may still be pending handlers for node-local requests since this connection is not closed. We complete them here:
             for (final Transport.ResponseContext<?> holderToNotify : responseHandlers.prune(h -> true)) {
                 try {
-                    holderToNotify.handler()
-                        .handleException(
-                            new SendRequestTransportException(
-                                holderToNotify.connection().getNode(),
-                                holderToNotify.action(),
-                                new NodeClosedException(localNode)
-                            )
-                        );
+                    final TransportResponseHandler<?> handler = holderToNotify.handler();
+                    final var targetNode = holderToNotify.connection().getNode();
+
+                    // Assertion only holds for TcpTransport only because other transports (used in tests) may not implement the proper
+                    // close-connection behaviour. TODO fix this.
+                    assert transport instanceof TcpTransport == false || targetNode.equals(localNode) : targetNode + " vs " + localNode;
+
+                    final var exception = new SendRequestTransportException(
+                        targetNode,
+                        holderToNotify.action(),
+                        new NodeClosedException(localNode)
+                    );
+                    final var executor = handler.executor();
+                    if (executor.equals(ThreadPool.Names.SAME)) {
+                        handler.handleException(exception);
+                    } else {
+                        threadPool.executor(executor).execute(new ForkingResponseHandlerRunnable(handler, exception) {
+                            @Override
+                            protected void doRun() {
+                                handler.handleException(exception);
+                            }
+                        });
+                    }
                 } catch (Exception e) {
                     assert false : e;
                     logger.warn(
                         () -> new ParameterizedMessage(
-                            "failed to notify response handler on exception, action: {}",
+                            "failed to notify response handler on shutdown, action: {}",
                             holderToNotify.action()
                         ),
                         e
@@ -1389,16 +1409,23 @@ public class TransportService extends AbstractLifecycleComponent
         @Override
         public void sendResponse(TransportResponse response) throws IOException {
             service.onResponseSent(requestId, action, response);
-            final TransportResponseHandler<?> handler = service.responseHandlers.onResponseReceived(requestId, service);
-            // ignore if its null, the service logs it
-            if (handler != null) {
+            try (var shutdownBlock = service.pendingDirectHandlers.withRef()) {
+                if (shutdownBlock == null) {
+                    // already shutting down, the handler will be completed by sendRequestInternal or doStop
+                    return;
+                }
+                final TransportResponseHandler<?> handler = service.responseHandlers.onResponseReceived(requestId, service);
+                if (handler == null) {
+                    // handler already completed, likely by a timeout which is logged elsewhere
+                    return;
+                }
                 final String executor = handler.executor();
                 if (ThreadPool.Names.SAME.equals(executor)) {
                     processResponse(handler, response);
                 } else {
-                    threadPool.executor(executor).execute(new Runnable() {
+                    threadPool.executor(executor).execute(new ForkingResponseHandlerRunnable(handler, null) {
                         @Override
-                        public void run() {
+                        protected void doRun() {
                             processResponse(handler, response);
                         }
 
@@ -1423,17 +1450,24 @@ public class TransportService extends AbstractLifecycleComponent
         @Override
         public void sendResponse(Exception exception) throws IOException {
             service.onResponseSent(requestId, action, exception);
-            final TransportResponseHandler<?> handler = service.responseHandlers.onResponseReceived(requestId, service);
-            // ignore if its null, the service logs it
-            if (handler != null) {
+            try (var shutdownBlock = service.pendingDirectHandlers.withRef()) {
+                if (shutdownBlock == null) {
+                    // already shutting down, the handler will be completed by sendRequestInternal or doStop
+                    return;
+                }
+                final TransportResponseHandler<?> handler = service.responseHandlers.onResponseReceived(requestId, service);
+                if (handler == null) {
+                    // handler already completed, likely by a timeout which is logged elsewhere
+                    return;
+                }
                 final RemoteTransportException rtx = wrapInRemote(exception);
                 final String executor = handler.executor();
                 if (ThreadPool.Names.SAME.equals(executor)) {
                     processException(handler, rtx);
                 } else {
-                    threadPool.executor(handler.executor()).execute(new Runnable() {
+                    threadPool.executor(executor).execute(new ForkingResponseHandlerRunnable(handler, rtx) {
                         @Override
-                        public void run() {
+                        protected void doRun() {
                             processException(handler, rtx);
                         }
 
@@ -1447,10 +1481,9 @@ public class TransportService extends AbstractLifecycleComponent
         }
 
         protected RemoteTransportException wrapInRemote(Exception e) {
-            if (e instanceof RemoteTransportException) {
-                return (RemoteTransportException) e;
-            }
-            return new RemoteTransportException(localNode.getName(), localNode.getAddress(), action, e);
+            return e instanceof RemoteTransportException remoteTransportException
+                ? remoteTransportException
+                : new RemoteTransportException(localNode.getName(), localNode.getAddress(), action, e);
         }
 
         protected void processException(final TransportResponseHandler<?> handler, final RemoteTransportException rtx) {
@@ -1529,6 +1562,45 @@ public class TransportService extends AbstractLifecycleComponent
         public void onResponseReceived(long requestId, Transport.ResponseContext holder) {
             for (TransportMessageListener listener : listeners) {
                 listener.onResponseReceived(requestId, holder);
+            }
+        }
+    }
+
+    private static class PendingDirectHandlers extends AbstractRefCounted {
+
+        // To handle a response we (i) remove the handler from responseHandlers and then (ii) enqueue an action to complete the handler on
+        // the target executor. Once step (i) succeeds then the handler won't be completed by any other mechanism, but if the target
+        // executor is stopped then step (ii) will fail with an EsRejectedExecutionException which means the handler leaks.
+        //
+        // We wait for all transport threads to finish before stopping any executors, so a transport thread will never fail at step (ii).
+        // Remote responses are always delivered on transport threads so there's no problem there, but direct responses may be delivered on
+        // a non-transport thread which runs concurrently to the stopping of the transport service. This means we need this explicit
+        // mechanism to block the shutdown of the transport service while there are direct handlers in between steps (i) and (ii).
+
+        private final CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        @Override
+        protected void closeInternal() {
+            countDownLatch.countDown();
+        }
+
+        void stop() {
+            decRef();
+            try {
+                final boolean completed = countDownLatch.await(30, TimeUnit.SECONDS);
+                assert completed : "timed out waiting for all direct handlers to be enqueued";
+            } catch (InterruptedException e) {
+                assert false : e;
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        @Nullable
+        Releasable withRef() {
+            if (tryIncRef()) {
+                return this::decRef;
+            } else {
+                return null;
             }
         }
     }

server/src/main/java/org/elasticsearch/transport/ForkingResponseHandlerRunnable.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.transport;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.ParameterizedMessage;
+import org.elasticsearch.common.util.concurrent.AbstractRunnable;
+import org.elasticsearch.core.Nullable;
+import org.elasticsearch.threadpool.ThreadPool;
+
+/**
+ * {@link AbstractRunnable} implementation for completing a forking response handler, overriding any threadpool queue length limit and
+ * handling shutdown-related rejections by completing the handler without forking.
+ */
+abstract class ForkingResponseHandlerRunnable extends AbstractRunnable {
+
+    private static final Logger logger = LogManager.getLogger(ForkingResponseHandlerRunnable.class);
+
+    private final TransportResponseHandler<?> handler;
+
+    @Nullable
+    private final TransportException transportException;
+
+    ForkingResponseHandlerRunnable(TransportResponseHandler<?> handler, @Nullable TransportException transportException) {
+        assert handler.executor().equals(ThreadPool.Names.SAME) == false : "forking handler required, but got " + handler;
+        this.handler = handler;
+        this.transportException = transportException;
+    }
+
+    @Override
+    protected abstract void doRun(); // no 'throws Exception' here
+
+    @Override
+    public boolean isForceExecution() {
+        // we must complete every pending listener
+        return true;
+    }
+
+    @Override
+    public void onRejection(Exception e) {
+        // force-executed tasks are only rejected on shutdown, but we should have enqueued the completion of every handler before shutting
+        // down any thread pools so this indicates a bug
+        assert false : e;
+
+        // we must complete every pending listener, and we can't fork to the target threadpool because we're shutting down, so just complete
+        // it on this thread.
+        final TransportException exceptionToDeliver;
+        if (transportException == null) {
+            exceptionToDeliver = new RemoteTransportException(e.getMessage(), e);
+        } else {
+            exceptionToDeliver = transportException;
+            exceptionToDeliver.addSuppressed(e);
+        }
+        try {
+            handler.handleException(exceptionToDeliver);
+        } catch (Exception e2) {
+            exceptionToDeliver.addSuppressed(e2);
+            logger.error(
+                () -> new ParameterizedMessage(
+                    "{} [{}]",
+                    transportException == null ? "failed to handle rejection of response" : "failed to handle rejection of error response",
+                    handler
+                ),
+                exceptionToDeliver
+            );
+        }
+    }
+
+    @Override
+    public void onFailure(Exception e) {
+        assert false : e; // delivering the response shouldn't throw anything
+        logger.error(
+            () -> new ParameterizedMessage(
+                "{} [{}]",
+                transportException == null ? "failed to handle rejection of response" : "failed to handle rejection of error response",
+                handler
+            ),
+            e
+        );
+    }
+}

server/src/main/java/org/elasticsearch/transport/InboundHandler.java
@@ -338,7 +338,7 @@ public class InboundHandler {
             response = handler.read(stream);
             response.remoteAddress(remoteAddress);
         } catch (Exception e) {
-            final Exception serializationException = new TransportSerializationException(
+            final TransportException serializationException = new TransportSerializationException(
                 "Failed to deserialize response from handler [" + handler + "]",
                 e
             );
@@ -351,15 +351,12 @@ public class InboundHandler {
         if (ThreadPool.Names.SAME.equals(executor)) {
             doHandleResponse(handler, response);
         } else {
-            boolean success = false;
-            try {
-                threadPool.executor(executor).execute(() -> doHandleResponse(handler, response));
-                success = true;
-            } finally {
-                if (success == false) {
-                    response.decRef();
+            threadPool.executor(executor).execute(new ForkingResponseHandlerRunnable(handler, null) {
+                @Override
+                protected void doRun() {
+                    doHandleResponse(handler, response);
                 }
-            }
+            });
         }
     }
 
@@ -367,7 +364,7 @@ public class InboundHandler {
         try {
             handler.handleResponse(response);
         } catch (Exception e) {
-            handleException(handler, new ResponseHandlerFailureTransportException(e));
+            doHandleException(handler, new ResponseHandlerFailureTransportException(e));
         } finally {
             response.decRef();
         }
@@ -384,21 +381,33 @@ public class InboundHandler {
             );
             assert ignoreDeserializationErrors : error;
         }
-        handleException(handler, error);
+        handleException(
+            handler,
+            error instanceof RemoteTransportException rtx ? rtx : new RemoteTransportException(error.getMessage(), error)
+        );
     }
 
-    private void handleException(final TransportResponseHandler<?> handler, Throwable error) {
-        if ((error instanceof RemoteTransportException) == false) {
-            error = new RemoteTransportException(error.getMessage(), error);
+    private void handleException(final TransportResponseHandler<?> handler, TransportException transportException) {
+        final var executor = handler.executor();
+        if (ThreadPool.Names.SAME.equals(executor)) {
+            doHandleException(handler, transportException);
+        } else {
+            threadPool.executor(executor).execute(new ForkingResponseHandlerRunnable(handler, transportException) {
+                @Override
+                protected void doRun() {
+                    doHandleException(handler, transportException);
+                }
+            });
         }
-        final RemoteTransportException rtx = (RemoteTransportException) error;
-        threadPool.executor(handler.executor()).execute(() -> {
-            try {
-                handler.handleException(rtx);
-            } catch (Exception e) {
-                logger.error(() -> new ParameterizedMessage("failed to handle exception response [{}]", handler), e);
-            }
-        });
+    }
+
+    private void doHandleException(final TransportResponseHandler<?> handler, TransportException transportException) {
+        try {
+            handler.handleException(transportException);
+        } catch (Exception e) {
+            transportException.addSuppressed(e);
+            logger.error(() -> new ParameterizedMessage("failed to handle exception response [{}]", handler), transportException);
+        }
     }
 
     private StreamInput namedWriteableStream(StreamInput delegate) {

server/src/test/java/org/elasticsearch/transport/TransportServiceLifecycleTests.java
@@ -1,0 +1,181 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.transport;
+
+import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.Version;
+import org.elasticsearch.action.support.PlainActionFuture;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.core.Releasable;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.test.transport.MockTransportService;
+import org.elasticsearch.threadpool.TestThreadPool;
+import org.elasticsearch.threadpool.ThreadPool;
+
+import java.util.Random;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.emptySet;
+
+public class TransportServiceLifecycleTests extends ESTestCase {
+
+    public void testHandlersCompleteAtShutdown() throws Exception {
+        try (var nodeA = new TestNode("node-A")) {
+
+            final var threads = new Thread[between(1, 10)];
+            final var keepGoing = new AtomicBoolean(true);
+            final var requestPermits = new Semaphore(Integer.MAX_VALUE);
+
+            try (var nodeB = new TestNode("node-B")) {
+
+                final var connectFuture = new PlainActionFuture<Releasable>();
+                nodeB.transportService.connectToNode(nodeA.transportService.getLocalNode(), connectFuture);
+                connectFuture.get(10, TimeUnit.SECONDS);
+
+                final var startBarrier = new CyclicBarrier(threads.length + 1);
+
+                for (int i = 0; i < threads.length; i++) {
+                    final var seed = randomLong();
+                    threads[i] = new Thread(() -> {
+                        try {
+                            startBarrier.await(10, TimeUnit.SECONDS);
+                        } catch (Exception e) {
+                            throw new AssertionError(e);
+                        }
+                        final var random = new Random(seed);
+                        while (keepGoing.get() && requestPermits.tryAcquire()) {
+                            nodeB.transportService.sendRequest(
+                                randomFrom(random, nodeA, nodeB).transportService.getLocalNode(),
+                                TestNode.ACTION_NAME_PREFIX + randomFrom(random, TestNode.EXECUTOR_NAMES),
+                                TransportRequest.Empty.INSTANCE,
+                                new TransportResponseHandler<TransportResponse.Empty>() {
+
+                                    final AtomicBoolean completed = new AtomicBoolean();
+
+                                    final String executor = randomFrom(random, TestNode.EXECUTOR_NAMES);
+
+                                    @Override
+                                    public void handleResponse(TransportResponse.Empty response) {
+                                        assertTrue(completed.compareAndSet(false, true));
+                                        requestPermits.release();
+                                    }
+
+                                    @Override
+                                    public void handleException(TransportException exp) {
+                                        assertTrue(completed.compareAndSet(false, true));
+                                        requestPermits.release();
+                                    }
+
+                                    @Override
+                                    public TransportResponse.Empty read(StreamInput in) {
+                                        return TransportResponse.Empty.INSTANCE;
+                                    }
+
+                                    @Override
+                                    public String executor() {
+                                        return executor;
+                                    }
+                                }
+                            );
+                        }
+                    }, "test-thread-" + i);
+                    threads[i].start();
+                }
+
+                startBarrier.await(10, TimeUnit.SECONDS);
+
+            }
+
+            // exiting the try-with-resources block stops node B while the background threads continue to submit requests, to verify that
+            // every handler is completed even if the request or response are being handled concurrently with shutdown
+
+            keepGoing.set(false);
+            assertTrue(requestPermits.tryAcquire(Integer.MAX_VALUE, 10, TimeUnit.SECONDS));
+            for (final var thread : threads) {
+                thread.join();
+            }
+        }
+    }
+
+    private static class TestNode implements Releasable {
+
+        static final String ACTION_NAME_PREFIX = "internal:test/";
+        static final String[] EXECUTOR_NAMES = new String[] {
+            ThreadPool.Names.SAME,
+            ThreadPool.Names.GENERIC,
+            ThreadPool.Names.CLUSTER_COORDINATION,
+            ThreadPool.Names.SEARCH };
+
+        final ThreadPool threadPool;
+        final TransportService transportService;
+
+        TestNode(String nodeName) {
+            threadPool = new TestThreadPool(
+                nodeName,
+                // tiny search thread pool & queue to trigger non-shutdown-related rejections
+                Settings.builder().put("thread_pool.search.size", 2).put("thread_pool.search.queue_size", 5).build()
+            ) {
+                @Override
+                public ExecutorService executor(String name) {
+                    // yielding here is enough to expose the race that #85131 fixes with reasonable probability
+                    Thread.yield();
+                    return super.executor(name);
+                }
+            };
+            final var tcpTransport = MockTransportService.newMockTransport(Settings.EMPTY, Version.CURRENT, threadPool);
+            transportService = new TransportService(
+                Settings.EMPTY,
+                tcpTransport,
+                threadPool,
+                TransportService.NOOP_TRANSPORT_INTERCEPTOR,
+                boundTransportAddress -> new DiscoveryNode(
+                    nodeName,
+                    nodeName,
+                    tcpTransport.boundAddress().publishAddress(),
+                    emptyMap(),
+                    emptySet(),
+                    Version.CURRENT
+                ),
+                null,
+                emptySet()
+            );
+            for (final var executor : EXECUTOR_NAMES) {
+                transportService.registerRequestHandler(
+                    ACTION_NAME_PREFIX + executor,
+                    executor,
+                    TransportRequest.Empty::new,
+                    (request, channel, task) -> {
+                        if (randomBoolean()) {
+                            channel.sendResponse(TransportResponse.Empty.INSTANCE);
+                        } else {
+                            channel.sendResponse(new ElasticsearchException("simulated"));
+                        }
+                    }
+                );
+            }
+            transportService.start();
+            transportService.acceptIncomingRequests();
+        }
+
+        @Override
+        public void close() {
+            transportService.stop();
+            transportService.close();
+            ThreadPool.terminate(threadPool, 30, TimeUnit.SECONDS);
+        }
+    }
+
+}

commit 3737666a1419abffee013fc7f99566440f22e1f9

Drop faulty assertion in TransportService
We introduced an assertion in #85131 to check the assumption that there
are no pending non-local response handlers when the `TransportService`
shuts down. We later discovered that this assertion rarely tripped,
which we fixed in #86315, but that fix did not go into 8.2 so there are
still rare failures on this branch. This commit drops the faulty
assertion in 8.2.

Relates #86293

server/src/main/java/org/elasticsearch/transport/TransportService.java
@@ -300,10 +300,6 @@ public class TransportService extends AbstractLifecycleComponent
                     final TransportResponseHandler<?> handler = holderToNotify.handler();
                     final var targetNode = holderToNotify.connection().getNode();
 
-                    // Assertion only holds for TcpTransport only because other transports (used in tests) may not implement the proper
-                    // close-connection behaviour. TODO fix this.
-                    assert transport instanceof TcpTransport == false || targetNode.equals(localNode) : targetNode + " vs " + localNode;
-
                     final var exception = new SendRequestTransportException(
                         targetNode,
                         holderToNotify.action(),
14a4265bb5a7dd635bd6dfdc5c8eb66cc25f5481
Ryan Ernst <ryan@iernst.net>
2022-05-31 11:36:53 -0700

    Convert cluster state "customs" to Map
    The pluggable portions of in memory cluster state are held within an
    ImmutableOpenMap. This commit converts that to a Map. Note that the diff
    format is the same for immutableopenmap and jdk map, so no bwc logic is
    necessary.
    
    relates #86239

--- server/src/main/java/org/elasticsearch/common/io/stream/VersionedNamedWriteable.java
+++ server/src/main/java/org/elasticsearch/common/io/stream/VersionedNamedWriteable.java
@@ -12,6 +12,7 @@ import org.elasticsearch.Version;
 import org.elasticsearch.common.collect.ImmutableOpenMap;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * A {@link NamedWriteable} that has a minimum version associated with it.
@@ -49,7 +50,7 @@ public interface VersionedNamedWriteable extends NamedWriteable {
      * @param customs map of customs
      * @param <T>     type of customs in map
      */
-    static <T extends VersionedNamedWriteable> void writeVersionedWritables(StreamOutput out, ImmutableOpenMap<String, T> customs)
+    static <T extends VersionedNamedWriteable> void writeVersionedWritables(StreamOutput out, Map<String, T> customs)
         throws IOException {
         // filter out custom states not supported by the other node
         int numberOfCustoms = 0;

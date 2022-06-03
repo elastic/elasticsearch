43bbb8cec46d2aa76829ab38c165c4070c5dc2a3
Ryan Ernst <ryan@iernst.net>
2022-06-03 15:01:04 -0700

    spotless

--- server/src/main/java/org/elasticsearch/index/engine/SegmentsStats.java
+++ server/src/main/java/org/elasticsearch/index/engine/SegmentsStats.java
@@ -23,8 +23,6 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class SegmentsStats implements Writeable, ToXContentFragment {
 

23c41e45d9a20f43220d7a50d00dd373ab774394
Yang Wang <yang.wang@elastic.co>
2022-06-04 22:31:32 +1000

    Remove unused import from NumberFieldMapper
    Fixes checkstyle error of #85688

--- server/src/main/java/org/elasticsearch/index/mapper/NumberFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/NumberFieldMapper.java
@@ -62,7 +62,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.BiFunction;

d892fdef6a7fc3a585491f59a39fd3484efece6e
Juan Camilo Bages <jcbages95@gmail.com>
2022-06-05 15:23:12 -0500

    add a new noop step which does nothing but enables backwards compatibility whenever we change the functionality of a step

---
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/NoopStep.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+package org.elasticsearch.xpack.core.ilm;
+
+/**
+ * Signals that an error was encountered during the execution of a policy on an index.
+ */
+public class NoopStep extends Step {
+
+    public ClusterStateWaitStep(StepKey key, StepKey nextStepKey) {
+        super(key, nextStepKey);
+    }
+
+    @Override
+    public boolean isRetryable() {
+        // this is noop step we don't want to get stuck in, so we want it to be retryable
+        return true;
+    }
+}

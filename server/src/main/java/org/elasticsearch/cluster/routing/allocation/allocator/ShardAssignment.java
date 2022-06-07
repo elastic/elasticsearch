593942de9ca0596e5b994f6def77f9db0d67f6b9
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2022-06-07 12:45:44 +0200

    Whitelist throttled allocation state. This could happen if calculation has not converged.

--- server/src/main/java/org/elasticsearch/cluster/routing/allocation/allocator/ShardAssignment.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/allocation/allocator/ShardAssignment.java
@@ -24,16 +24,4 @@ public record ShardAssignment(Set<String> nodeIds, int unassigned, int ignored) 
     public static ShardAssignment of(List<ShardRouting> routings) {
         return new ShardAssignment(routings.stream().map(ShardRouting::currentNodeId).collect(toUnmodifiableSet()), 0, 0);
     }
-
-    public static ShardAssignment merge(ShardAssignment a, ShardAssignment b) {
-        return new ShardAssignment(merge(a.nodeIds, b.nodeIds), a.unassigned + b.unassigned, a.ignored + b.ignored);
-    }
-
-    private static Set<String> merge(Set<String> a, Set<String> b) {
-        assert (a.size() > 0 && b.size() > 0) == false : "Only one assignment is expected to have assigned node ids";
-        return a.size() > 0 ? a : b;
-    }
-
-    public static ShardAssignment UNASSIGNED = new ShardAssignment(Set.of(), 1, 0);
-    public static ShardAssignment IGNORED = new ShardAssignment(Set.of(), 1, 1);
 }

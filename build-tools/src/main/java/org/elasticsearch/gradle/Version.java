d764a01edce88fba3e821f3fb60ac72373415df4
Mark Vieira <portugee@gmail.com>
2022-06-07 16:13:05 -0700

    [7.17] Skip ML tests on later glibc for incompatible BWC versions (#87487)
    * Skip ML tests on later glibc for incompatible BWC versions
    
    * More flexible version parsing
    
    * Use relaxed mode for parsing glibc version
    
    * Missed a spot
    
    * spotless

--- build-tools/src/main/java/org/elasticsearch/gradle/Version.java
+++ build-tools/src/main/java/org/elasticsearch/gradle/Version.java
@@ -40,7 +40,7 @@ public final class Version implements Comparable<Version> {
     private static final Pattern pattern = Pattern.compile("(\\d+)\\.(\\d+)\\.(\\d+)(?:-(alpha\\d+|beta\\d+|rc\\d+|SNAPSHOT))?");
 
     private static final Pattern relaxedPattern = Pattern.compile(
-        "v?(\\d+)\\.(\\d+)\\.(\\d+)(?:[\\-+]+([a-zA-Z0-9_]+(?:-[a-zA-Z0-9]+)*))?"
+        "v?(\\d+)\\.(\\d+)(?:\\.(\\d+))?(?:[\\-+]+([a-zA-Z0-9_]+(?:-[a-zA-Z0-9]+)*))?"
     );
 
     public Version(int major, int minor, int revision) {
@@ -72,14 +72,12 @@ public final class Version implements Comparable<Version> {
             throw new IllegalArgumentException("Invalid version format: '" + s + "'. Should be " + expected);
         }
 
+        String major = matcher.group(1);
+        String minor = matcher.group(2);
+        String revision = matcher.group(3);
         String qualifier = matcher.group(4);
 
-        return new Version(
-            Integer.parseInt(matcher.group(1)),
-            Integer.parseInt(matcher.group(2)),
-            Integer.parseInt(matcher.group(3)),
-            qualifier
-        );
+        return new Version(Integer.parseInt(major), Integer.parseInt(minor), revision == null ? 0 : Integer.parseInt(revision), qualifier);
     }
 
     @Override

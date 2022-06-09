8250527b7e658f4c604109e625a42dda738ee1e3
Juan Camilo Bages <jcbages95@gmail.com>
2022-06-09 10:14:09 -0500

    make noopstep a clusterstatewaitstep

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/NoopStep.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/NoopStep.java
@@ -6,10 +6,13 @@
  */
 package org.elasticsearch.xpack.core.ilm;
 
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.index.Index;
+
 /**
  * Signals that an error was encountered during the execution of a policy on an index.
  */
-public class NoopStep extends Step {
+public class NoopStep extends ClusterStateWaitStep {
     public static final String NAME = "NOOP";
 
     public NoopStep(StepKey key, StepKey nextStepKey) {
@@ -20,5 +23,11 @@ public class NoopStep extends Step {
     public boolean isRetryable() {
         // this is noop step we don't want to get stuck in, so we want it to be retryable
         return true;
+    }
+
+    @Override
+    public Result isConditionMet(Index index, ClusterState clusterState) {
+        // we always want to move forward with this step so this should always be true
+        return new Result(true, null);
     }
 }

3f051a6511b14394cf0c7e298a0e9bb9a737119b
Armin Braun <me@obrown.io>
2022-06-18 19:06:21 +0200

    Cleanup dead code in org.elasticsearch.common
    Just some obvious static cleanup of this package. Should be trivial
    to review since it's deletes only and still compiles/tests fine.

--- server/src/main/java/org/elasticsearch/common/io/stream/RecyclerBytesStreamOutput.java
+++ server/src/main/java/org/elasticsearch/common/io/stream/RecyclerBytesStreamOutput.java
@@ -157,7 +157,6 @@ public class RecyclerBytesStreamOutput extends BytesStream implements Releasable
         // nothing to do
     }
 
-    @Override
     public void seek(long position) {
         ensureCapacityFromPosition(position);
         this.pageIndex = (int) position / pageSize;

63ce5eb488ee1f1f835699f26e4da1051cad45fb
Stuart Tettemer <stu@elastic.co>
2022-06-21 18:08:02 -0500

    Add getter tests

--- server/src/test/java/org/elasticsearch/script/field/IngestSourceAndMetadataTests.java
+++ server/src/test/java/org/elasticsearch/script/field/IngestSourceAndMetadataTests.java
@@ -10,27 +10,85 @@ package org.elasticsearch.script.field;
 
 import org.elasticsearch.test.ESTestCase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IngestSourceAndMetadataTests extends ESTestCase {
 
     IngestSourceAndMetadata map;
-    static final String DELETE = "_delete";
-    static final String EVEN = "even";
 
     @Override
     public void setUp() throws Exception {
+        super.setUp();
     }
 
-    public void testEntrySet() {}
+    public void testSettersAndGetters() {
+        Map<String, Object> metadata = new HashMap<>();
+        metadata.put("_index", "myIndex");
+        metadata.put("_id", "myId");
+        metadata.put("_routing", "myRouting");
+        metadata.put("_version", 20);
+        metadata.put("_if_seq_no", 500);
+        metadata.put("_if_primary_term", 10000);
+        metadata.put("_version_type", "internal");
+        metadata.put("_dynamic_templates", Map.of("foo", "bar"));
+        map = new IngestSourceAndMetadata(new HashMap<>(), metadata, null);
+        assertEquals("myIndex", map.getIndex());
+        map.setIndex("myIndex2");
+        assertEquals("myIndex2", map.getIndex());
 
-    public void testPut() {}
+        assertEquals("myId", map.getId());
+        map.setId("myId2");
+        assertEquals("myId2", map.getId());
 
-    public void testRemove() {}
+        assertEquals("myRouting", map.getRouting());
+        map.setRouting("myRouting2");
+        assertEquals("myRouting2", map.getRouting());
 
-    public void testClear() {}
+        assertEquals(20, map.getVersion());
+        map.setVersion(10);
+        assertEquals(10, map.getVersion());
 
-    public void testGet() {}
+        assertEquals("internal", map.getVersionType());
+        map.setVersionType("external_gte");
+        assertEquals("external_gte", map.getVersionType());
 
-    public void testGetString() {}
+        assertEquals(Map.of("foo", "bar"), map.getDynamicTemplates());
 
-    public void testGetNumber() {}
+        assertEquals(500, map.getIfSeqNo());
+        assertEquals(10000, map.getIfPrimaryTerm());
+    }
+
+    public void testGetString() {
+        Map<String, Object> metadata = new HashMap<>();
+        metadata.put("_routing", "myRouting");
+        Map<String, Object> source = new HashMap<>();
+        source.put("str", "myStr");
+        source.put("toStr", new Object() {
+            @Override
+            public String toString() {
+                return "myToString()";
+            }
+        });
+        source.put("missing", null);
+        map = new IngestSourceAndMetadata(source, metadata, null, IngestSourceAndMetadata.VALIDATORS);
+        assertNull(map.getNumber("missing"));
+        assertNull(map.getNumber("no key"));
+        assertEquals("myToString()", map.getString("toStr"));
+        assertEquals("myStr", map.getString("str"));
+    }
+
+    public void testGetNumber() {
+        Map<String, Object> metadata = new HashMap<>();
+        metadata.put("_version", Long.MAX_VALUE);
+        Map<String, Object> source = new HashMap<>();
+        source.put("number", "NaN");
+        source.put("missing", null);
+        map = new IngestSourceAndMetadata(source, metadata, null, IngestSourceAndMetadata.VALIDATORS);
+        assertEquals(Long.MAX_VALUE, map.getNumber("_version"));
+        IllegalArgumentException err = expectThrows(IllegalArgumentException.class, () -> map.getNumber("number"));
+        assertEquals("unexpected type for [number] with value [NaN], expected Number, got [java.lang.String]", err.getMessage());
+        assertNull(map.getNumber("missing"));
+        assertNull(map.getNumber("no key"));
+    }
 }

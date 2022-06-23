36d5fb57596a09c467d708f15f1755f7c5d24174
Nhat Nguyen <nhat.nguyen@elastic.co>
2022-06-22 20:38:07 -0400

    implement docValueCount

--- x-pack/plugin/old-lucene-versions/src/main/java/org/elasticsearch/xpack/lucene/bwc/codecs/index/LegacySortedSetDocValuesWrapper.java
+++ x-pack/plugin/old-lucene-versions/src/main/java/org/elasticsearch/xpack/lucene/bwc/codecs/index/LegacySortedSetDocValuesWrapper.java
@@ -100,7 +100,7 @@ public final class LegacySortedSetDocValuesWrapper extends SortedSetDocValues {
 
     @Override
     public int docValueCount() {
-        throw new UnsupportedOperationException();
+        return values.docValueCount();
     }
 
     @Override

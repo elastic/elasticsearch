3afec10c9994949ad632eb55bd3ae67ce60b564b
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2022-06-24 12:25:24 +0200

    Remove import

--- x-pack/plugin/indices-write-load-tracker/src/main/java/org/elasticsearch/xpack/indiceswriteloadtracker/IndicesWriteLoadStore.java
+++ x-pack/plugin/indices-write-load-tracker/src/main/java/org/elasticsearch/xpack/indiceswriteloadtracker/IndicesWriteLoadStore.java
@@ -39,7 +39,6 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;

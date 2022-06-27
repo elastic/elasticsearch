077fbbbb808560c8d60be444ebaef91cb5b56a6e
Nik Everett <nik9000@gmail.com>
2022-06-27 08:11:22 -0400

    words are hard

--- server/src/main/java/org/elasticsearch/index/mapper/NumberFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/NumberFieldMapper.java
@@ -1689,7 +1689,7 @@ public class NumberFieldMapper extends FieldMapper {
         /**
          * Load all values for all docs up front. This should be much more
          * disk and cpu-friendly than {@link ImmediateLeaf} because it resolves
-         * the values all at once, keeping the disk .
+         * the values all at once, always scanning forwards on the disk.
          */
         private Leaf singletonLeaf(NumericDocValues singleton, int[] docIdsInLeaf) throws IOException {
             long[] values = new long[docIdsInLeaf.length];

699e78245e9b923f9bb1db0e8dfeb8e64f9f5aa0
Stuart Tettemer <stu@elastic.co>
2022-06-28 13:06:44 -0500

    Fix spotless in TODO

--- server/src/test/java/org/elasticsearch/action/ingest/WriteableIngestDocumentTests.java
+++ server/src/test/java/org/elasticsearch/action/ingest/WriteableIngestDocumentTests.java
@@ -162,7 +162,7 @@ public class WriteableIngestDocumentTests extends AbstractXContentTestCase<Write
         sourceAndMetadata.putAll(toXContentSource);
         sourceAndMetadata.putAll(metadataMap);
         IngestDocument serializedIngestDocument = new IngestDocument(sourceAndMetadata, toXContentIngestMetadata);
-        // TODO(stu): is this test correct?  Comparing against ingestDocument fails due to incorrectly failed byte array comparisons
+        // TODO(stu): is this test correct? Comparing against ingestDocument fails due to incorrectly failed byte array comparisons
         assertThat(serializedIngestDocument, equalTo(serializedIngestDocument));
     }
 

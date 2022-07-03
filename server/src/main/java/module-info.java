3e706778fe5d95032394a1a00325dbf462f93eed
ChrisHegarty <christopher.hegarty@elastic.co>
2022-07-03 14:55:12 +0100

    Upgrade to log4J 2.18.0

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -358,5 +358,4 @@ module org.elasticsearch.server {
         with
             org.elasticsearch.cluster.coordination.NodeToolCliProvider,
             org.elasticsearch.index.shard.ShardToolCliProvider;
-    provides org.apache.logging.log4j.util.PropertySource with org.elasticsearch.common.logging.ESSystemPropertiesPropertySource;
 }

elastic/elasticsearch

commit 5a0b3323032a1fdf3774ce8a7e1bce5d8e7becd8

Use desired nodes during data tier allocation decisions  (#87735)
Today when the DataTierAllocationDecider checks if a shard can be allocated
in a node or not it uses the current cluster topology. This works fine in most cases
when the data flows from hotter to colder data nodes as it was designed. But
in certain situations users might want to remove a tier (i.e. the cold tier) that
allows having shards in hotter tiers, but with the current implementation once
a shard is allocated into the highest preference tier and there are nodes of that
tier, the shard won't move. This commit introduces a change that allows to use
the desired nodes (when available) to compute the preferred tier based on the
cluster topology provided by the desired nodes (only taking into account desired
nodes that are in ACTUALIZED status).

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/cluster/routing/allocation/DataTierAllocationDecider.java
@@ -7,6 +7,8 @@
 
 package org.elasticsearch.xpack.cluster.routing.allocation;
 
+import org.elasticsearch.cluster.metadata.DesiredNode;
+import org.elasticsearch.cluster.metadata.DesiredNodes;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.node.DiscoveryNodeRole;
@@ -19,6 +21,7 @@ import org.elasticsearch.cluster.routing.allocation.decider.AllocationDecider;
 import org.elasticsearch.cluster.routing.allocation.decider.Decision;
 import org.elasticsearch.common.Strings;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -65,7 +68,7 @@ public final class DataTierAllocationDecider extends AllocationDecider {
     }
 
     public interface PreferredTierFunction {
-        Optional<String> apply(List<String> tierPreference, DiscoveryNodes nodes);
+        Optional<String> apply(List<String> tierPreference, DiscoveryNodes nodes, DesiredNodes desiredNodes);
     }
 
     private static final Decision YES_PASSES = Decision.single(Decision.YES.type(), NAME, "node passes tier preference filters");
@@ -80,7 +83,7 @@ public final class DataTierAllocationDecider extends AllocationDecider {
         if (tierPreference.isEmpty() != false) {
             return YES_PASSES;
         }
-        Optional<String> tier = preferredTierFunction.apply(tierPreference, allocation.nodes());
+        Optional<String> tier = preferredTierFunction.apply(tierPreference, allocation.nodes(), allocation.desiredNodes());
         if (tier.isPresent()) {
             String tierName = tier.get();
             if (allocationAllowed(tierName, roles)) {
@@ -133,9 +136,74 @@ public final class DataTierAllocationDecider extends AllocationDecider {
      * Given a string of comma-separated prioritized tiers (highest priority
      * first) and an allocation, find the highest priority tier for which nodes
      * exist. If no nodes for any of the tiers are available, returns an empty
-     * {@code Optional<String>}.
+     * {@code Optional<String>}. This method takes into account the desired nodes
+     * in order to know if there are planned topology changes in the cluster
+     * that can remove a tier that's part of the cluster now.
      */
-    public static Optional<String> preferredAvailableTier(List<String> prioritizedTiers, DiscoveryNodes nodes) {
+    public static Optional<String> preferredAvailableTier(List<String> prioritizedTiers, DiscoveryNodes nodes, DesiredNodes desiredNodes) {
+        final var desiredNodesPreferredTier = getPreferredTierFromDesiredNodes(prioritizedTiers, nodes, desiredNodes);
+
+        if (desiredNodesPreferredTier.isPresent()) {
+            return desiredNodesPreferredTier;
+        }
+
+        return getPreferredAvailableTierFromClusterMembers(prioritizedTiers, nodes);
+    }
+
+    /**
+     * Given a list of tiers in descending order, return the tier that's present
+     * in the desired nodes with the highest priority, if none is present returns an
+     * {@code Optional.empty()}.
+     */
+    public static Optional<String> getPreferredTierFromDesiredNodes(
+        List<String> prioritizedTiers,
+        DiscoveryNodes discoveryNodes,
+        DesiredNodes desiredNodes
+    ) {
+        if (desiredNodes == null) {
+            return Optional.empty();
+        }
+
+        for (int tierIndex = 0; tierIndex < prioritizedTiers.size(); tierIndex++) {
+            final var tier = prioritizedTiers.get(tierIndex);
+            if (tierNodesPresent(tier, desiredNodes.actualized())
+                || isDesiredNodeWithinTierJoining(tier, discoveryNodes, desiredNodes)
+                || nextTierIsGrowingAndCurrentTierCanHoldTheIndex(prioritizedTiers, tierIndex, discoveryNodes, desiredNodes)) {
+                return Optional.of(tier);
+            }
+        }
+        return Optional.empty();
+    }
+
+    private static boolean nextTierIsGrowingAndCurrentTierCanHoldTheIndex(
+        List<String> prioritizedTiers,
+        int tierIndex,
+        DiscoveryNodes discoveryNodes,
+        DesiredNodes desiredNodes
+    ) {
+        final var tier = prioritizedTiers.get(tierIndex);
+        assert tierNodesPresent(tier, desiredNodes.actualized()) == false;
+        // If there's a plan to grow the next preferred tier, and it hasn't materialized yet,
+        // wait until all the nodes in the next tier have joined. This would avoid overwhelming
+        // the next tier if within the same plan one tier is removed and the next preferred tier
+        // grows.
+        boolean nextPreferredTierIsGrowing = false;
+        for (int i = tierIndex + 1; i < prioritizedTiers.size(); i++) {
+            final var nextTier = prioritizedTiers.get(i);
+            nextPreferredTierIsGrowing |= tierNodesPresent(nextTier, desiredNodes.pending());
+        }
+        return tierNodesPresent(tier, discoveryNodes) && nextPreferredTierIsGrowing;
+    }
+
+    private static boolean isDesiredNodeWithinTierJoining(String tier, DiscoveryNodes discoveryNodes, DesiredNodes desiredNodes) {
+        assert tierNodesPresent(tier, desiredNodes.actualized()) == false;
+        // Take into account the case when the desired nodes have been updated and the node in the tier would be replaced by
+        // a new one. In that case the desired node in the tier won't be actualized as it has to join, but we still need to ensure
+        // that at least one cluster member has the requested tier as we would prefer to minimize the shard movements in these cases.
+        return tierNodesPresent(tier, desiredNodes.pending()) && tierNodesPresent(tier, discoveryNodes);
+    }
+
+    private static Optional<String> getPreferredAvailableTierFromClusterMembers(List<String> prioritizedTiers, DiscoveryNodes nodes) {
         for (String tier : prioritizedTiers) {
             if (tierNodesPresent(tier, nodes)) {
                 return Optional.of(tier);
@@ -144,15 +212,16 @@ public final class DataTierAllocationDecider extends AllocationDecider {
         return Optional.empty();
     }
 
+    static boolean tierNodesPresent(String singleTier, Collection<DesiredNode> nodes) {
+        assert singleTier.equals(DiscoveryNodeRole.DATA_ROLE.roleName()) || DataTier.validTierName(singleTier)
+            : "tier " + singleTier + " is an invalid tier name";
+        return nodes.stream().anyMatch(node -> allocationAllowed(singleTier, node.getRoles()));
+    }
+
     static boolean tierNodesPresent(String singleTier, DiscoveryNodes nodes) {
         assert singleTier.equals(DiscoveryNodeRole.DATA_ROLE.roleName()) || DataTier.validTierName(singleTier)
             : "tier " + singleTier + " is an invalid tier name";
-        for (DiscoveryNode node : nodes) {
-            if (allocationAllowed(singleTier, node.getRoles())) {
-                return true;
-            }
-        }
-        return false;
+        return nodes.stream().anyMatch(node -> allocationAllowed(singleTier, node.getRoles()));
     }
 
     private static boolean allocationAllowed(String tierName, Set<DiscoveryNodeRole> roles) {

server/src/main/java/org/elasticsearch/action/admin/cluster/desirednodes/TransportUpdateDesiredNodesAction.java
@@ -25,6 +25,7 @@ import org.elasticsearch.cluster.metadata.DesiredNodes;
 import org.elasticsearch.cluster.metadata.DesiredNodesMetadata;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.routing.RerouteService;
+import org.elasticsearch.cluster.routing.allocation.AllocationService;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.Priority;
 import org.elasticsearch.common.inject.Inject;
@@ -50,7 +51,8 @@ public class TransportUpdateDesiredNodesAction extends TransportMasterNodeAction
         ThreadPool threadPool,
         ActionFilters actionFilters,
         IndexNameExpressionResolver indexNameExpressionResolver,
-        DesiredNodesSettingsValidator settingsValidator
+        DesiredNodesSettingsValidator settingsValidator,
+        AllocationService allocationService
     ) {
         super(
             UpdateDesiredNodesAction.NAME,
@@ -65,7 +67,7 @@ public class TransportUpdateDesiredNodesAction extends TransportMasterNodeAction
             ThreadPool.Names.SAME
         );
         this.settingsValidator = settingsValidator;
-        this.taskExecutor = new UpdateDesiredNodesExecutor(clusterService.getRerouteService());
+        this.taskExecutor = new UpdateDesiredNodesExecutor(clusterService.getRerouteService(), allocationService);
     }
 
     @Override
@@ -167,9 +169,11 @@ public class TransportUpdateDesiredNodesAction extends TransportMasterNodeAction
         );
 
         private final RerouteService rerouteService;
+        private final AllocationService allocationService;
 
-        UpdateDesiredNodesExecutor(RerouteService rerouteService) {
+        UpdateDesiredNodesExecutor(RerouteService rerouteService, AllocationService allocationService) {
             this.rerouteService = rerouteService;
+            this.allocationService = allocationService;
         }
 
         @Override
@@ -194,7 +198,12 @@ public class TransportUpdateDesiredNodesAction extends TransportMasterNodeAction
 
             desiredNodes = DesiredNodes.updateDesiredNodesStatusIfNeeded(currentState.nodes(), desiredNodes);
 
-            return desiredNodes == initialDesiredNodes ? currentState : replaceDesiredNodes(currentState, desiredNodes);
+            if (desiredNodes == initialDesiredNodes) {
+                return currentState;
+            } else {
+                final ClusterState withUpdatedDesiredNodes = replaceDesiredNodes(currentState, desiredNodes);
+                return allocationService.adaptAutoExpandReplicas(withUpdatedDesiredNodes);
+            }
         }
 
         @Override

server/src/main/java/org/elasticsearch/cluster/routing/allocation/RoutingAllocation.java
@@ -11,6 +11,7 @@ package org.elasticsearch.cluster.routing.allocation;
 import org.elasticsearch.cluster.ClusterInfo;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.RestoreInProgress;
+import org.elasticsearch.cluster.metadata.DesiredNodes;
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.metadata.SingleNodeShutdownMetadata;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
@@ -87,6 +88,8 @@ public class RoutingAllocation {
      * @param deciders {@link AllocationDeciders} to used to make decisions for routing allocations
      * @param routingNodes Routing nodes in the current cluster or {@code null} if using those in the given cluster state
      * @param clusterState cluster state before rerouting
+     * @param clusterInfo information about node disk usage and shard disk usage
+     * @param shardSizeInfo information about snapshot shard sizes
      * @param currentNanoTime the nano time to use for all delay allocation calculation (typically {@link System#nanoTime()})
      */
     public RoutingAllocation(
@@ -166,6 +169,11 @@ public class RoutingAllocation {
 
     public SnapshotShardSizeInfo snapshotShardSizeInfo() {
         return shardSizeInfo;
+    }
+
+    @Nullable
+    public DesiredNodes desiredNodes() {
+        return DesiredNodes.latestFromClusterState(clusterState);
     }
 
     /**

server/src/test/java/org/elasticsearch/action/admin/cluster/desirednodes/TransportUpdateDesiredNodesActionTests.java
@@ -24,6 +24,7 @@ import org.elasticsearch.cluster.metadata.DesiredNodesMetadata;
 import org.elasticsearch.cluster.metadata.DesiredNodesTestCase;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.metadata.Metadata;
+import org.elasticsearch.cluster.routing.allocation.AllocationService;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.threadpool.ThreadPool;
@@ -59,7 +60,8 @@ public class TransportUpdateDesiredNodesActionTests extends DesiredNodesTestCase
             mock(ThreadPool.class),
             mock(ActionFilters.class),
             mock(IndexNameExpressionResolver.class),
-            NO_OP_SETTINGS_VALIDATOR
+            NO_OP_SETTINGS_VALIDATOR,
+            mock(AllocationService.class)
         );
 
         final ClusterBlocks blocks = ClusterBlocks.builder()
@@ -83,7 +85,8 @@ public class TransportUpdateDesiredNodesActionTests extends DesiredNodesTestCase
             mock(ThreadPool.class),
             mock(ActionFilters.class),
             mock(IndexNameExpressionResolver.class),
-            NO_OP_SETTINGS_VALIDATOR
+            NO_OP_SETTINGS_VALIDATOR,
+            mock(AllocationService.class)
         );
 
         final ClusterBlocks blocks = ClusterBlocks.builder().build();
@@ -106,7 +109,8 @@ public class TransportUpdateDesiredNodesActionTests extends DesiredNodesTestCase
             mock(ThreadPool.class),
             mock(ActionFilters.class),
             mock(IndexNameExpressionResolver.class),
-            validator
+            validator,
+            mock(AllocationService.class)
         );
 
         final ClusterState state = ClusterState.builder(new ClusterName(randomAlphaOfLength(10))).build();

server/src/test/java/org/elasticsearch/cluster/metadata/DesiredNodeSerializationTests.java
@@ -16,8 +16,6 @@ import org.elasticsearch.xcontent.XContentParser;
 
 import java.io.IOException;
 
-import static org.elasticsearch.cluster.metadata.DesiredNodesTestCase.randomProcessorRange;
-
 public class DesiredNodeSerializationTests extends AbstractSerializingTestCase<DesiredNode> {
     @Override
     protected DesiredNode doParseInstance(XContentParser parser) throws IOException {
@@ -52,15 +50,14 @@ public class DesiredNodeSerializationTests extends AbstractSerializingTestCase<D
             );
             case 1 -> new DesiredNode(
                 instance.settings(),
-                randomFloat() + randomIntBetween(1, 128),
+                randomValueOtherThan(instance.processors(), () -> randomFloat() + randomIntBetween(1, 128)),
                 instance.memory(),
                 instance.storage(),
                 instance.version()
             );
-
             case 2 -> new DesiredNode(
                 instance.settings(),
-                randomProcessorRange(),
+                randomValueOtherThan(instance.processorsRange(), DesiredNodesTestCase::randomProcessorRange),
                 instance.memory(),
                 instance.storage(),
                 instance.version()
@@ -69,7 +66,7 @@ public class DesiredNodeSerializationTests extends AbstractSerializingTestCase<D
                 instance.settings(),
                 instance.processors(),
                 instance.processorsRange(),
-                ByteSizeValue.ofGb(randomIntBetween(1, 128)),
+                ByteSizeValue.ofGb(randomValueOtherThan(instance.memory().getGb(), () -> (long) randomIntBetween(1, 128))),
                 instance.storage(),
                 instance.version()
             );
@@ -78,7 +75,7 @@ public class DesiredNodeSerializationTests extends AbstractSerializingTestCase<D
                 instance.processors(),
                 instance.processorsRange(),
                 instance.memory(),
-                ByteSizeValue.ofGb(randomIntBetween(1, 128)),
+                ByteSizeValue.ofGb(randomValueOtherThan(instance.storage().getGb(), () -> (long) randomIntBetween(1, 128))),
                 instance.version()
             );
             case 5 -> new DesiredNode(

x-pack/plugin/autoscaling/src/main/java/org/elasticsearch/xpack/autoscaling/storage/ReactiveStorageDeciderService.java
@@ -13,6 +13,7 @@ import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.DiskUsage;
 import org.elasticsearch.cluster.metadata.DataStream;
 import org.elasticsearch.cluster.metadata.DataStreamMetadata;
+import org.elasticsearch.cluster.metadata.DesiredNodes;
 import org.elasticsearch.cluster.metadata.IndexAbstraction;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.Metadata;
@@ -401,7 +402,7 @@ public class ReactiveStorageDeciderService implements AutoscalingDeciderService 
             return allocation.metadata().getIndexSafe(shard.index());
         }
 
-        private Optional<String> highestPreferenceTier(List<String> preferredTiers, DiscoveryNodes unused) {
+        private Optional<String> highestPreferenceTier(List<String> preferredTiers, DiscoveryNodes unused, DesiredNodes desiredNodes) {
             assert preferredTiers.isEmpty() == false;
             return Optional.of(preferredTiers.get(0));
         }

commit 15a2980241325ae7ba336b83209dfd7f334c8df2

Fix performance regression in DataTierAllocationDecider
In #87735 we moved to using streams here. This results in a visible
regression in the many shards benchmarks that gets bigger the larger
the cluster is.
Using streams here adds a lot of alloction and indirection but does not
come with any advantages since `DiscoveryNodes` does not implement
`stream` so it's just an indirection on top of the existing iterator
that gets instantiated for every shard and then runs a compartively
short loop over the nodes.

-> move this back to iterator

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/cluster/routing/allocation/DataTierAllocationDecider.java
@@ -215,13 +215,23 @@ public final class DataTierAllocationDecider extends AllocationDecider {
     static boolean tierNodesPresent(String singleTier, Collection<DesiredNode> nodes) {
         assert singleTier.equals(DiscoveryNodeRole.DATA_ROLE.roleName()) || DataTier.validTierName(singleTier)
             : "tier " + singleTier + " is an invalid tier name";
-        return nodes.stream().anyMatch(node -> allocationAllowed(singleTier, node.getRoles()));
+        for (DesiredNode node : nodes) {
+            if (allocationAllowed(singleTier, node.getRoles())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     static boolean tierNodesPresent(String singleTier, DiscoveryNodes nodes) {
         assert singleTier.equals(DiscoveryNodeRole.DATA_ROLE.roleName()) || DataTier.validTierName(singleTier)
             : "tier " + singleTier + " is an invalid tier name";
-        return nodes.stream().anyMatch(node -> allocationAllowed(singleTier, node.getRoles()));
+        for (DiscoveryNode node : nodes) {
+            if (allocationAllowed(singleTier, node.getRoles())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static boolean allocationAllowed(String tierName, Set<DiscoveryNodeRole> roles) {
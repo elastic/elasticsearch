95111b963682a65b7879f85a2b3f3317701683b6
gmarouli <mary.gouseti@elastic.co>
2022-07-07 17:26:33 +0300

    Remove unnecessary equals & hash code

--- server/src/main/java/org/elasticsearch/common/unit/RelativeByteSizeValue.java
+++ server/src/main/java/org/elasticsearch/common/unit/RelativeByteSizeValue.java
@@ -10,8 +10,6 @@ package org.elasticsearch.common.unit;
 
 import org.elasticsearch.ElasticsearchParseException;
 
-import java.util.Objects;
-
 /**
  * A byte size value that allows specification using either of:
  * 1. Absolute value (200GB for instance)
@@ -96,18 +94,5 @@ public class RelativeByteSizeValue {
         } else {
             return absolute.getStringRep();
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RelativeByteSizeValue that = (RelativeByteSizeValue) o;
-        return Objects.equals(absolute, that.absolute) && Objects.equals(ratio, that.ratio);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(absolute, ratio);
     }
 }

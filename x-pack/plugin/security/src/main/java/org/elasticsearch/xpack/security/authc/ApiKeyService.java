477ab8d5973645b27abdbc469a9f048c424b7bb0
Yang Wang <ywangd@gmail.com>
2022-07-11 15:42:40 +1000

    Ensure CreateApiKey always creates a new document (#88413)
    The OpType of the indexRequest used for creating new API keys does not
    have its OpType configured. This means it defaults to OpType.INDEX which
    allows it to replace an existing document. This PR fixes it by explicity
    set OpType to CREATE so that it always create a new document (or throw
    error if ID conflict does happen).
    
    Since API key ID is time-based random base64 UUID, it is unlikely for
    this to happen in practice and we are not aware of any related bug
    report.

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/ApiKeyService.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/ApiKeyService.java
@@ -304,6 +304,7 @@ public class ApiKeyService {
                 final IndexRequest indexRequest = client.prepareIndex(SECURITY_MAIN_ALIAS)
                     .setSource(builder)
                     .setId(request.getId())
+                    .setOpType(DocWriteRequest.OpType.CREATE)
                     .setRefreshPolicy(request.getRefreshPolicy())
                     .request();
                 final BulkRequest bulkRequest = toSingleItemBulkRequest(indexRequest);
@@ -317,6 +318,7 @@ public class ApiKeyService {
                         bulkRequest,
                         TransportSingleItemBulkWriteAction.<IndexResponse>wrapBulkResponse(ActionListener.wrap(indexResponse -> {
                             assert request.getId().equals(indexResponse.getId());
+                            assert indexResponse.getResult() == DocWriteResponse.Result.CREATED;
                             final ListenableFuture<CachedApiKeyHashResult> listenableFuture = new ListenableFuture<>();
                             listenableFuture.onResponse(new CachedApiKeyHashResult(true, apiKey));
                             apiKeyAuthCache.put(request.getId(), listenableFuture);

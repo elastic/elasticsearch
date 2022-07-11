946b367ac2dffdba5628579491d0afad27e5166b
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2022-07-11 13:55:14 +0200

    Simplify map copying
    This commit introduces a method that simplifies creating a map deep copy

--- server/src/main/java/org/elasticsearch/cluster/routing/RoutingNode.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/RoutingNode.java
@@ -69,10 +69,7 @@ public class RoutingNode implements Iterable<ShardRouting> {
         this.shards = new LinkedHashMap<>(original.shards);
         this.relocatingShards = new LinkedHashSet<>(original.relocatingShards);
         this.initializingShards = new LinkedHashSet<>(original.initializingShards);
-        this.shardsByIndex = Maps.newMapWithExpectedSize(original.shardsByIndex.size());
-        for (Map.Entry<Index, Set<ShardRouting>> entry : original.shardsByIndex.entrySet()) {
-            shardsByIndex.put(entry.getKey(), new HashSet<>(entry.getValue()));
-        }
+        this.shardsByIndex = Maps.copyOf(original.shardsByIndex, HashSet::new);
         assert invariant();
     }
 

adc7eff22935bc9d3fcf7cd810ff71cb5ec8d232
Ryan Ernst <ryan@iernst.net>
2022-07-20 14:51:35 -0700

    spotless

--- server/src/test/java/org/elasticsearch/plugins/PluginDescriptorTests.java
+++ server/src/test/java/org/elasticsearch/plugins/PluginDescriptorTests.java
@@ -18,13 +18,10 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;

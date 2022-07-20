4732780dd3ff32f33670b10c42138e8649d9d0da
Justin Cranford <justin.cranford@elastic.co>
2022-07-19 20:05:32 -0400

    Moved expire() logs inside the if check.

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/jwt/JwtRealm.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/jwt/JwtRealm.java
@@ -374,11 +374,11 @@ public class JwtRealm extends Realm implements CachingRealm, Releasable {
     @Override
     public void expire(final String username) {
         this.ensureInitialized();
-        LOGGER.trace("Expiring JWT cache entries for realm [" + super.name() + "] principal=[" + username + "]");
         if (this.jwtCacheHelper != null) {
+            LOGGER.trace("Expiring JWT cache entries for realm [" + super.name() + "] principal=[" + username + "]");
             this.jwtCacheHelper.removeValuesIf(expiringUser -> expiringUser.user.principal().equals(username));
+            LOGGER.trace("Expired JWT cache entries for realm [" + super.name() + "] principal=[" + username + "]");
         }
-        LOGGER.trace("Expired JWT cache entries for realm [" + super.name() + "] principal=[" + username + "]");
     }
 
     @Override

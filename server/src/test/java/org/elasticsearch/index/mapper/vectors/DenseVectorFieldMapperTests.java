daff73da0eaabb29f4b041d7480939991c5ed0e9
iverase <ivera@apache.org>
2022-07-22 09:36:44 +0200

    s/93/92

--- server/src/test/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapperTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapperTests.java
@@ -455,7 +455,7 @@ public class DenseVectorFieldMapperTests extends MapperTestCase {
         assertThat(codec, instanceOf(PerFieldMapperCodec.class));
         KnnVectorsFormat knnVectorsFormat = ((PerFieldMapperCodec) codec).getKnnVectorsFormatForField("field");
         assertThat(knnVectorsFormat, instanceOf(Lucene92HnswVectorsFormat.class));
-        String expectedString = "Lucene92HnswVectorsFormat(name=Lucene93HnswVectorsFormat, maxConn="
+        String expectedString = "Lucene92HnswVectorsFormat(name=Lucene92HnswVectorsFormat, maxConn="
             + m
             + ", beamWidth="
             + efConstruction

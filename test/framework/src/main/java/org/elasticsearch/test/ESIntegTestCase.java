8e6f84ee19a2c44a13d03fb1f2b9002039c40801
Nikola Grcevski <nikola.grcevski@elastic.co>
2022-07-22 22:12:24 -0400

    Spotless

--- test/framework/src/main/java/org/elasticsearch/test/ESIntegTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/ESIntegTestCase.java
@@ -909,7 +909,7 @@ public abstract class ESIntegTestCase extends ESTestCase {
     public ClusterHealthStatus ensureYellow(String... indices) {
         return ensureColor(ClusterHealthStatus.YELLOW, TimeValue.timeValueSeconds(30), false, indices);
     }
-    
+
     /**
      * Ensures the cluster has a yellow state via the cluster health API and ensures the that cluster has no initializing shards
      * for the given indices

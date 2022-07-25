04e5dc97d29c278907d92116b435216e6f275145
Slobodan Adamovic <slobodan.adamovic@elastic.co>
2022-07-25 15:22:54 +0200

    Remove added assert.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/permission/IndicesPermission.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/permission/IndicesPermission.java
@@ -367,9 +367,6 @@ public final class IndicesPermission {
         Map<String, IndexAbstraction> lookup,
         FieldPermissionsCache fieldPermissionsCache
     ) {
-        assert requestedIndicesOrAliases.isEmpty() == false
-            : "every indices action request must have its indices set, hence the requested indices must not be empty";
-
         // Short circuit if the indicesPermission allows all access to every index
         if (Arrays.stream(groups).anyMatch(Group::isTotal)) {
             return IndicesAccessControl.allowAll();

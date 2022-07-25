bfb4a0ac1d337578037c22a839c9d0ee81817c08
Christos Soulios <csoulios@gmail.com>
2022-07-25 11:18:16 +0300

    checkstyle

--- server/src/main/java/org/elasticsearch/action/fieldcaps/FieldCapabilitiesFetcher.java
+++ server/src/main/java/org/elasticsearch/action/fieldcaps/FieldCapabilitiesFetcher.java
@@ -118,7 +118,7 @@ class FieldCapabilitiesFetcher {
                 IndexFieldCapabilities fieldCap = new IndexFieldCapabilities(
                     field,
                     // This is a nasty hack so that we expose aggregate_metric_double field,
-                    // when the index  is a time series index and the field is marked as metric.
+                    // when the index is a time series index and the field is marked as metric.
                     // This code should be reverted once PR https://github.com/elastic/elasticsearch/pull/87849
                     // is merged.
                     isTimeSeriesIndex && ft.getMetricType() != null ? ft.typeName() : ft.familyTypeName(),

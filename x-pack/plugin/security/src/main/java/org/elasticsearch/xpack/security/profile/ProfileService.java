4172a8dba476f0866aff15a25807d174cd8deec7
Yang Wang <yang.wang@elastic.co>
2022-07-26 14:01:26 +1000

    tweak

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/profile/ProfileService.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/profile/ProfileService.java
@@ -490,8 +490,7 @@ public class ProfileService {
 
     private static final String INVALID_USERNAME_MESSAGE = "Security domain [%s] is configured to use literal username. "
         + "As a result, creating new user profile requires the username to be at least 1 and no more than 256 characters. "
-        + "The username must begin with an alphanumeric character (a-z, A-Z, 0-9) and followed by any alphanumeric, "
-        + "underscore (_) or dash (-) characters.";
+        + "The username must contain only alphanumeric characters (a-z, A-Z, 0-9).";
 
     private void validateUsername(Subject subject) {
         final RealmDomain realmDomain = subject.getRealm().getDomain();

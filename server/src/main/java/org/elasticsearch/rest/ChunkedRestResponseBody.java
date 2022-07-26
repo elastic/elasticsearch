fbb56fde924edf6af96d0ad376c59f602156c3e2
Armin Braun <me@obrown.io>
2022-07-26 11:45:50 +0200

    moar api

--- server/src/main/java/org/elasticsearch/rest/ChunkedRestResponseBody.java
+++ server/src/main/java/org/elasticsearch/rest/ChunkedRestResponseBody.java
@@ -9,7 +9,13 @@ package org.elasticsearch.rest;
 
 import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.bytes.ReleasableBytesReference;
+import org.elasticsearch.common.io.stream.RecyclerBytesStreamOutput;
 import org.elasticsearch.common.recycler.Recycler;
+import org.elasticsearch.common.xcontent.ChunkedToXContent;
+import org.elasticsearch.core.IOUtils;
+import org.elasticsearch.core.Streams;
+import org.elasticsearch.xcontent.ToXContent;
+import org.elasticsearch.xcontent.XContentBuilder;
 
 import java.io.IOException;
 
@@ -18,4 +24,43 @@ public interface ChunkedRestResponseBody {
     boolean isDone();
 
     ReleasableBytesReference encodeChunk(int sizeHint, Recycler<BytesRef> recycler) throws IOException;
+
+    static ChunkedRestResponseBody fromXContent(ChunkedToXContent chunkedToXContent, ToXContent.Params params, RestChannel channel) {
+        return new ChunkedRestResponseBody() {
+
+            final RedirectOutputStream out = new RedirectOutputStream();
+            private ChunkedToXContent.ChunkedXContentSerialization serialization;
+
+            private boolean done = false;
+
+            @Override
+            public boolean isDone() {
+                return done;
+            }
+
+            @Override
+            public ReleasableBytesReference encodeChunk(int sizeHint, Recycler<BytesRef> recycler) throws IOException {
+                final RecyclerBytesStreamOutput chunkStream = new RecyclerBytesStreamOutput(recycler);
+                out.newTarget(chunkStream);
+                if (serialization == null) {
+                    serialization = chunkedToXContent.toXContentChunked(
+                        channel.newBuilder(channel.request().getXContentType(), null, true, Streams.noCloseStream(out)),
+                        params
+                    );
+                }
+                XContentBuilder b;
+                while ((b = serialization.encodeChunk()) == null) {
+                    if (chunkStream.size() > sizeHint) {
+                        break;
+                    }
+                }
+                if (b != null) {
+                    done = true;
+                    b.close();
+                }
+                out.clearTarget();
+                return new ReleasableBytesReference(chunkStream.bytes(), () -> IOUtils.closeWhileHandlingException(chunkStream));
+            }
+        };
+    }
 }

daf3dca9a79180de858fc8ee23fecbd61efc6391
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2022-07-26 10:49:43 +0200

    Review comment

--- x-pack/plugin/autoscaling/src/internalClusterTest/java/org/elasticsearch/xpack/autoscaling/storage/ReactiveStorageIT.java
+++ x-pack/plugin/autoscaling/src/internalClusterTest/java/org/elasticsearch/xpack/autoscaling/storage/ReactiveStorageIT.java
@@ -407,17 +407,20 @@ public class ReactiveStorageIT extends AutoscalingStorageIntegTestCase {
         // It might take a while until the autoscaling polls the node information of dataNode2 and
         // provides a complete autoscaling capacity response
         assertBusy(() -> {
-            // validate initial state looks good
             GetAutoscalingCapacityAction.Response response = capacity();
             assertThat(response.results().keySet(), equalTo(Set.of(policyName)));
-            assertThat(response.results().get(policyName).currentCapacity().total().storage().getBytes(), equalTo(enoughSpace * 2));
             assertThat(response.results().get(policyName).requiredCapacity(), is(notNullValue()));
-            assertThat(response.results().get(policyName).requiredCapacity().total().storage().getBytes(), equalTo(enoughSpace * 2));
-            assertThat(
-                response.results().get(policyName).requiredCapacity().node().storage().getBytes(),
-                equalTo(used + LOW_WATERMARK_BYTES + ReactiveStorageDeciderService.NODE_DISK_OVERHEAD)
-            );
         });
+
+        // validate initial state looks good
+        GetAutoscalingCapacityAction.Response response = capacity();
+        assertThat(response.results().keySet(), equalTo(Set.of(policyName)));
+        assertThat(response.results().get(policyName).currentCapacity().total().storage().getBytes(), equalTo(enoughSpace * 2));
+        assertThat(response.results().get(policyName).requiredCapacity().total().storage().getBytes(), equalTo(enoughSpace * 2));
+        assertThat(
+            response.results().get(policyName).requiredCapacity().node().storage().getBytes(),
+            equalTo(used + LOW_WATERMARK_BYTES + ReactiveStorageDeciderService.NODE_DISK_OVERHEAD)
+        );
 
         assertAcked(
             client().admin()
@@ -447,7 +450,7 @@ public class ReactiveStorageIT extends AutoscalingStorageIntegTestCase {
         // * 2 since worst case is no hard links, see DiskThresholdDecider.getExpectedShardSize.
         long requiredSpaceForClone = used * 2 + LOW_WATERMARK_BYTES;
 
-        GetAutoscalingCapacityAction.Response response = capacity();
+        response = capacity();
         assertThat(response.results().keySet(), equalTo(Set.of(policyName)));
         assertThat(response.results().get(policyName).currentCapacity().total().storage().getBytes(), equalTo(enoughSpace * 2));
         // test that even when the shard cannot allocate due to disk space, we do not request a "total" scale up, only a node-level.

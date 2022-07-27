c9b2b479c2624e476416956d9d147b5f20069d41
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2022-07-27 16:43:43 +0200

    Hack order

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/action/apikey/BulkUpdateApiKeyResponseTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/action/apikey/BulkUpdateApiKeyResponseTests.java
@@ -19,18 +19,23 @@ import org.elasticsearch.xcontent.XContentFactory;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import static org.hamcrest.Matchers.equalTo;
 
 public class BulkUpdateApiKeyResponseTests extends ESTestCase {
 
     public void testToXContent() throws IOException {
-        final var response = new BulkUpdateApiKeyResponse(
-            List.of("api-key-id-1"),
-            List.of("api-key-id-2", "api-key-id-3"),
-            Map.of("failed-api-key-id-1", new IllegalArgumentException("msg - 1"))
-        );
+        // Force ordered key set for deterministic comparison with raw JSON string below
+        final SortedMap<String, Exception> errorDetails = new TreeMap<>();
+        errorDetails.put("failed-api-key-id-1", new IllegalArgumentException("msg - 1"));
+        errorDetails.put("failed-api-key-id-2", new ResourceNotFoundException("potato"));
+        errorDetails.put("failed-api-key-id-3", new ElasticsearchException("error1", new IllegalArgumentException("msg - 1")));
+        errorDetails.put("failed-api-key-id-4", new ElasticsearchException("error2", new IllegalArgumentException("msg - 2")));
+        final var response = new BulkUpdateApiKeyResponse(List.of("api-key-id-1"), List.of("api-key-id-2", "api-key-id-3"), errorDetails);
         final XContentBuilder builder = XContentFactory.jsonBuilder();
+
         response.toXContent(builder, ToXContent.EMPTY_PARAMS);
 
         assertThat(Strings.toString(builder), equalTo(XContentHelper.stripWhitespace("""
@@ -43,11 +48,31 @@ public class BulkUpdateApiKeyResponseTests extends ESTestCase {
                  "api-key-id-3"
                ],
                "errors": {
-                 "count": 1,
+                 "count": 4,
                  "details": {
                    "failed-api-key-id-1": {
                      "type": "illegal_argument_exception",
                      "reason": "msg - 1"
+                   },
+                   "failed-api-key-id-2": {
+                     "type": "resource_not_found_exception",
+                     "reason": "potato"
+                   },
+                   "failed-api-key-id-3": {
+                     "type": "exception",
+                     "reason": "error1",
+                     "caused_by": {
+                       "type": "illegal_argument_exception",
+                       "reason": "msg - 1"
+                     }
+                   },
+                   "failed-api-key-id-4": {
+                     "type": "exception",
+                     "reason": "error2",
+                     "caused_by": {
+                       "type": "illegal_argument_exception",
+                       "reason": "msg - 2"
+                     }
                    }
                  }
                }

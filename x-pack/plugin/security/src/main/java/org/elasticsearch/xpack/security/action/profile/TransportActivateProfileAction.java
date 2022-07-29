9106bf3487b420d548a510a927cc927f0011fb16
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2022-07-29 15:56:21 +0200

    Refactor authentication handling for grant actions

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/action/profile/TransportActivateProfileAction.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/action/profile/TransportActivateProfileAction.java
@@ -16,6 +16,7 @@ import org.elasticsearch.transport.TransportService;
 import org.elasticsearch.xpack.core.security.action.profile.ActivateProfileAction;
 import org.elasticsearch.xpack.core.security.action.profile.ActivateProfileRequest;
 import org.elasticsearch.xpack.core.security.action.profile.ActivateProfileResponse;
+import org.elasticsearch.xpack.core.security.authc.Authentication;
 import org.elasticsearch.xpack.security.action.TransportGrantAction;
 import org.elasticsearch.xpack.security.authc.AuthenticationService;
 import org.elasticsearch.xpack.security.authz.AuthorizationService;
@@ -47,12 +48,11 @@ public class TransportActivateProfileAction extends TransportGrantAction<Activat
     }
 
     @Override
-    protected void doExecute(Task task, ActivateProfileRequest request, ActionListener<ActivateProfileResponse> listener) {
-        executeWithGrantAuthentication(
-            request,
-            listener.delegateFailure(
-                (l, authentication) -> profileService.activateProfile(authentication, l.map(ActivateProfileResponse::new))
-            )
-        );
+    protected void executeOnSuccessfulGrant(
+        Authentication authentication,
+        ActivateProfileRequest request,
+        ActionListener<ActivateProfileResponse> listener
+    ) {
+        profileService.activateProfile(authentication, listener.map(ActivateProfileResponse::new));
     }
 }

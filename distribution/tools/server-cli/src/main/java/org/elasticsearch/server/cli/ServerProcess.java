6378130e321d346ca01220e65582ac5c60dff8b4
ChrisHegarty <christopher.hegarty@elastic.co>
2022-07-29 10:39:55 +0100

    Initial commit

--- distribution/tools/server-cli/src/main/java/org/elasticsearch/server/cli/ServerProcess.java
+++ distribution/tools/server-cli/src/main/java/org/elasticsearch/server/cli/ServerProcess.java
@@ -216,6 +216,7 @@ public class ServerProcess {
         command.addAll(jvmOptions);
         command.add("--module-path");
         command.add(esHome.resolve("lib").toString());
+        command.add("--add-modules=jdk.net");
         command.add("-m");
         command.add("org.elasticsearch.server/org.elasticsearch.bootstrap.Elasticsearch");
 

3868a314d2c3f1581491d5e560ce1cc4fb69f512
Rory Hunter <roryhunter2@gmail.com>
2022-08-03 13:12:45 +0100

    Formatting

--- server/src/main/java/org/elasticsearch/bootstrap/ServerArgs.java
+++ server/src/main/java/org/elasticsearch/bootstrap/ServerArgs.java
@@ -11,7 +11,6 @@ package org.elasticsearch.bootstrap;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
-import org.elasticsearch.common.settings.KeyStoreWrapper;
 import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.core.PathUtils;

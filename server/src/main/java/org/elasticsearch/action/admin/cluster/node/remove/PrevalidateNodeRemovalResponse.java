73b65bf8eb9df727bd6299cd429ef8e48bb84d67
Pooya Salehi <pooya.salehi@elastic.co>
2022-08-10 14:48:59 +0200

    Some cleanup

--- server/src/main/java/org/elasticsearch/action/admin/cluster/node/remove/PrevalidateNodeRemovalResponse.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/node/remove/PrevalidateNodeRemovalResponse.java
@@ -42,19 +42,4 @@ public class PrevalidateNodeRemovalResponse extends ActionResponse implements To
     public void writeTo(StreamOutput out) throws IOException {
         throw new AssertionError("PrevalidateNodeRemovalAction should not be sent over the wire.");
     }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
 }

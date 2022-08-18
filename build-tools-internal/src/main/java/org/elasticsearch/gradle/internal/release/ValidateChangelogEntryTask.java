52f6edf1ab0eac133542eeb4a6cfe164565a6da7
Rory Hunter <pugnascotia@users.noreply.github.com>
2022-08-18 19:44:35 +0100

    Updates to changelog processing after docs redesign (#89463)

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/release/ValidateChangelogEntryTask.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/release/ValidateChangelogEntryTask.java
@@ -8,6 +8,8 @@
 
 package org.elasticsearch.gradle.internal.release;
 
+import com.google.common.annotations.VisibleForTesting;
+
 import org.gradle.api.DefaultTask;
 import org.gradle.api.GradleException;
 import org.gradle.api.file.ConfigurableFileCollection;
@@ -30,6 +32,21 @@ public class ValidateChangelogEntryTask extends DefaultTask {
     private final ConfigurableFileCollection changelogs;
     private final ProjectLayout projectLayout;
 
+    public static final String TRIPLE_BACKTICK = "```";
+    private static final String CODE_BLOCK_ERROR = """
+        [%s] uses a triple-backtick in the [%s] section, but it must be
+        formatted as a Asciidoc code block. For example:
+
+            [source,yaml]
+            ----
+            {
+              "metrics.time" : 10,
+              "metrics.time.min" : 1,
+              "metrics.time.max" : 500
+            }
+            ----
+        """;
+
     @Inject
     public ValidateChangelogEntryTask(ObjectFactory objectFactory, ProjectLayout projectLayout) {
         this.changelogs = objectFactory.fileCollection();
@@ -43,37 +60,60 @@ public class ValidateChangelogEntryTask extends DefaultTask {
             .stream()
             .collect(Collectors.toMap(file -> rootDir.relativize(file.toURI()).toString(), ChangelogEntry::parse));
 
+        changelogs.forEach(ValidateChangelogEntryTask::validate);
+    }
+
+    @VisibleForTesting
+    static void validate(String path, ChangelogEntry entry) {
         // We don't try to find all such errors, because we expect them to be rare e.g. only
         // when a new file is added.
-        changelogs.forEach((path, entry) -> {
-            final String type = entry.getType();
+        final String type = entry.getType();
 
-            if (type.equals("known-issue") == false && type.equals("security") == false) {
-                if (entry.getPr() == null) {
-                    throw new GradleException(
-                        "[" + path + "] must provide a [pr] number (only 'known-issue' and " + "'security' entries can omit this"
-                    );
-                }
-
-                if (entry.getArea() == null) {
-                    throw new GradleException(
-                        "[" + path + "] must provide an [area] (only 'known-issue' and " + "'security' entries can omit this"
-                    );
-                }
+        if (type.equals("known-issue") == false && type.equals("security") == false) {
+            if (entry.getPr() == null) {
+                throw new GradleException(
+                    "[" + path + "] must provide a [pr] number (only 'known-issue' and 'security' entries can omit this"
+                );
             }
 
-            if ((type.equals("breaking") || type.equals("breaking-java")) && entry.getBreaking() == null) {
+            if (entry.getArea() == null) {
+                throw new GradleException("[" + path + "] must provide an [area] (only 'known-issue' and 'security' entries can omit this");
+            }
+        }
+
+        if (type.equals("breaking") || type.equals("breaking-java")) {
+            if (entry.getBreaking() == null) {
                 throw new GradleException(
                     "[" + path + "] has type [" + type + "] and must supply a [breaking] section with further information"
                 );
             }
 
-            if (type.equals("deprecation") && entry.getDeprecation() == null) {
+            if (entry.getBreaking().getDetails().contains(TRIPLE_BACKTICK)) {
+                throw new GradleException(CODE_BLOCK_ERROR.formatted(path, "breaking.details"));
+            }
+            if (entry.getBreaking().getImpact().contains(TRIPLE_BACKTICK)) {
+                throw new GradleException(CODE_BLOCK_ERROR.formatted(path, "breaking.impact"));
+            }
+        }
+
+        if (type.equals("deprecation")) {
+            if (entry.getDeprecation() == null) {
                 throw new GradleException(
                     "[" + path + "] has type [deprecation] and must supply a [deprecation] section with further information"
                 );
             }
-        });
+
+            if (entry.getDeprecation().getDetails().contains(TRIPLE_BACKTICK)) {
+                throw new GradleException(CODE_BLOCK_ERROR.formatted(path, "deprecation.details"));
+            }
+            if (entry.getDeprecation().getImpact().contains(TRIPLE_BACKTICK)) {
+                throw new GradleException(CODE_BLOCK_ERROR.formatted(path, "deprecation.impact"));
+            }
+        }
+
+        if (entry.getHighlight() != null && entry.getHighlight().getBody().contains(TRIPLE_BACKTICK)) {
+            throw new GradleException(CODE_BLOCK_ERROR.formatted(path, "highlight.body"));
+        }
     }
 
     @InputFiles

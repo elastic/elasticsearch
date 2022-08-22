d5aa7aa84c2e3c81e97560282dfab62f6973c7b8
gmarouli <mary.gouseti@elastic.co>
2022-08-22 15:19:50 +0300

    Use write/read enum

--- server/src/main/java/org/elasticsearch/health/node/DiskHealthInfo.java
+++ server/src/main/java/org/elasticsearch/health/node/DiskHealthInfo.java
@@ -14,10 +14,6 @@ import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.health.HealthStatus;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * The health status of the disk space of this node along with the cause.
@@ -28,23 +24,20 @@ public record DiskHealthInfo(HealthStatus healthStatus, Cause cause) implements 
     }
 
     public DiskHealthInfo(StreamInput in) throws IOException {
-        this(HealthStatus.from(in), in.readOptionalWriteable(Cause::from));
+        this(in.readEnum(HealthStatus.class), in.readOptionalEnum(Cause.class));
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         healthStatus.writeTo(out);
-        out.writeOptionalWriteable(cause);
+        out.writeOptionalEnum(cause);
     }
 
-    public enum Cause implements Writeable {
+    public enum Cause {
         NODE_OVER_HIGH_THRESHOLD((byte) 0),
         NODE_OVER_THE_FLOOD_STAGE_THRESHOLD((byte) 1),
         FROZEN_NODE_OVER_FLOOD_STAGE_THRESHOLD((byte) 2),
         NODE_HAS_NO_DISK_STATS((byte) 3);
-
-        private static final Map<Byte, Cause> values = Arrays.stream(Cause.values())
-            .collect(Collectors.toMap(Cause::value, Function.identity()));
 
         private final byte value;
 
@@ -52,22 +45,8 @@ public record DiskHealthInfo(HealthStatus healthStatus, Cause cause) implements 
             this.value = value;
         }
 
-        @Override
-        public void writeTo(StreamOutput out) throws IOException {
-            out.writeByte(value);
-        }
-
         public byte value() {
             return value;
-        }
-
-        public static DiskHealthInfo.Cause from(StreamInput in) throws IOException {
-            byte b = in.readByte();
-            DiskHealthInfo.Cause cause = values.get(b);
-            if (cause != null) {
-                return cause;
-            }
-            throw new IllegalArgumentException("Cannot deserialize disk health indo cause with id " + b);
         }
     }
 }

25c0fa1c3b0869c8d541c3ccca1a258295479716
Nikola Grcevski <nikola.grcevski@elastic.co>
2022-08-29 16:32:26 -0400

    Make some methods public for testing

--- server/src/main/java/org/elasticsearch/reservedstate/service/FileSettingsService.java
+++ server/src/main/java/org/elasticsearch/reservedstate/service/FileSettingsService.java
@@ -82,13 +82,11 @@ public class FileSettingsService extends AbstractLifecycleComponent implements C
         this.operatorSettingsDir = environment.configFile().toAbsolutePath().resolve(OPERATOR_DIRECTORY);
     }
 
-    // package private for testing
-    Path operatorSettingsDir() {
+    public Path operatorSettingsDir() {
         return operatorSettingsDir;
     }
 
-    // package private for testing
-    Path operatorSettingsFile() {
+    public Path operatorSettingsFile() {
         return operatorSettingsDir().resolve(SETTINGS_FILE_NAME);
     }
 
@@ -202,8 +200,7 @@ public class FileSettingsService extends AbstractLifecycleComponent implements C
         }
     }
 
-    // package private for testing
-    boolean watching() {
+    public boolean watching() {
         return this.watchService != null;
     }
 

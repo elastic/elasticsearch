3493b22b4c1eef5a30343f7c10663746468458ff
Pooya Salehi <pooya.salehi@elastic.co>
2022-09-12 16:32:46 +0200

    Extend TransportMasterNodeReadAction

--- server/src/main/java/org/elasticsearch/action/admin/cluster/node/shutdown/PrevalidateNodeRemovalRequest.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/node/shutdown/PrevalidateNodeRemovalRequest.java
@@ -8,22 +8,29 @@
 
 package org.elasticsearch.action.admin.cluster.node.shutdown;
 
-import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionRequestValidationException;
+import org.elasticsearch.action.support.master.MasterNodeReadRequest;
+import org.elasticsearch.common.io.stream.StreamInput;
 
+import java.io.IOException;
 import java.util.List;
 
-public class PrevalidateNodeRemovalRequest extends ActionRequest {
+public class PrevalidateNodeRemovalRequest extends MasterNodeReadRequest<PrevalidateNodeRemovalRequest> {
     private final String[] nodeIds;
+
+    public PrevalidateNodeRemovalRequest(String... nodeIds) {
+        this.nodeIds = nodeIds;
+    }
+
+    public PrevalidateNodeRemovalRequest(final StreamInput in) throws IOException {
+        super(in);
+        nodeIds = in.readStringArray();
+    }
 
     @Override
     public ActionRequestValidationException validate() {
         // TODO: make sure all provided node IDs are in the cluster
         return null;
-    }
-
-    public PrevalidateNodeRemovalRequest(String... nodesIds) {
-        this.nodeIds = nodesIds;
     }
 
     public List<String> getNodeIds() {

dd8b69f9fafeb4925507da08bba3dee85810ba1a
Pooya Salehi <pooya.salehi@elastic.co>
2022-09-14 17:24:57 +0200

    consider removal with red SS indices safe

--- server/src/main/java/org/elasticsearch/action/admin/cluster/node/shutdown/TransportPrevalidateNodeRemovalAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/node/shutdown/TransportPrevalidateNodeRemovalAction.java
@@ -15,12 +15,15 @@ import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.master.TransportMasterNodeReadAction;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.block.ClusterBlockException;
+import org.elasticsearch.cluster.health.ClusterHealthStatus;
 import org.elasticsearch.cluster.health.ClusterStateHealth;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.core.Tuple;
 import org.elasticsearch.logging.LogManager;
 import org.elasticsearch.logging.Logger;
 import org.elasticsearch.rest.RestStatus;
@@ -31,6 +34,7 @@ import org.elasticsearch.transport.TransportService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -78,7 +82,7 @@ public class TransportPrevalidateNodeRemovalAction extends TransportMasterNodeRe
         try {
             List<DiscoveryNode> concreteNodes = resolveNodes(nodes, state.nodes());
             request.setConcreteNodes(concreteNodes.toArray(new DiscoveryNode[0]));
-            doPrevalidation(request, new ClusterStateHealth(state), listener);
+            doPrevalidation(request, state, listener);
         } catch (IllegalArgumentException e) {
             listener.onFailure(new ElasticsearchStatusException(e.getMessage(), RestStatus.BAD_REQUEST));
         } catch (ResourceNotFoundException e) {
@@ -112,9 +116,11 @@ public class TransportPrevalidateNodeRemovalAction extends TransportMasterNodeRe
 
     private void doPrevalidation(
         PrevalidateNodeRemovalRequest prevalidationRequest,
-        ClusterStateHealth clusterStateHealth,
+        ClusterState clusterState,
         ActionListener<PrevalidateNodeRemovalResponse> listener
     ) {
+        ClusterStateHealth clusterStateHealth = new ClusterStateHealth(clusterState);
+        Metadata metadata = clusterState.metadata();
         switch (clusterStateHealth.getStatus()) {
             case GREEN, YELLOW -> {
                 Result overall = new Result(IsSafe.YES, "");
@@ -124,12 +130,32 @@ public class TransportPrevalidateNodeRemovalAction extends TransportMasterNodeRe
                 listener.onResponse(new PrevalidateNodeRemovalResponse(new NodesRemovalPrevalidation(overall, nodeResults)));
             }
             case RED -> {
-                // TODO: search for RED indices which are searchable snapshot based.
-                Result overall = new Result(IsSafe.UNKNOWN, "cluster health is RED");
-                Map<String, Result> nodeResults = prevalidationRequest.getNodeIds()
+                Set<String> redIndices = clusterStateHealth.getIndices()
+                    .entrySet()
                     .stream()
-                    .collect(Collectors.toMap(Function.identity(), id -> new Result(IsSafe.UNKNOWN, "")));
-                listener.onResponse(new PrevalidateNodeRemovalResponse(new NodesRemovalPrevalidation(overall, nodeResults)));
+                    .filter(entry -> entry.getValue().getStatus() == ClusterHealthStatus.RED)
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toSet());
+                assert redIndices.isEmpty() == false;
+                // If all red indices are searchable snapshot indices, it is safe to remove any node.
+                Set<String> redNonSSIndices = redIndices.stream()
+                    .map(index -> Tuple.tuple(index, metadata.index(index)))
+                    .filter(tuple -> tuple.v2().isSearchableSnapshot() || tuple.v2().isPartialSearchableSnapshot())
+                    .map(Tuple::v1)
+                    .collect(Collectors.toSet());
+                if (redNonSSIndices.isEmpty()) {
+                    Result overall = new Result(IsSafe.YES, "");
+                    Map<String, Result> nodeResults = prevalidationRequest.getNodeIds()
+                        .stream()
+                        .collect(Collectors.toMap(Function.identity(), id -> new Result(IsSafe.YES, "")));
+                    listener.onResponse(new PrevalidateNodeRemovalResponse(new NodesRemovalPrevalidation(overall, nodeResults)));
+                } else {
+                    Result overall = new Result(IsSafe.UNKNOWN, "cluster health is RED");
+                    Map<String, Result> nodeResults = prevalidationRequest.getNodeIds()
+                        .stream()
+                        .collect(Collectors.toMap(Function.identity(), id -> new Result(IsSafe.UNKNOWN, "")));
+                    listener.onResponse(new PrevalidateNodeRemovalResponse(new NodesRemovalPrevalidation(overall, nodeResults)));
+                }
             }
         }
     }

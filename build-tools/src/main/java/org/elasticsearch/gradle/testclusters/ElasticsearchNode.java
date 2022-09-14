7296debe5f588af535b255327e4b957806bfc5c2
Jake Landis <jake.landis@elastic.co>
2022-09-13 22:27:24 -0500

    add tls support to gradle run/run-ccs

--- build-tools/src/main/java/org/elasticsearch/gradle/testclusters/ElasticsearchNode.java
+++ build-tools/src/main/java/org/elasticsearch/gradle/testclusters/ElasticsearchNode.java
@@ -1675,7 +1675,8 @@ public class ElasticsearchNode implements TestClusterConfiguration {
         if (settings.containsKey("xpack.security.http.ssl.certificate")) {
             wait.setCertificateAuthorities(getConfigDir().resolve(settings.get("xpack.security.http.ssl.certificate").toString()).toFile());
         }
-        if (settings.containsKey("xpack.security.http.ssl.keystore.path")) {
+        if (settings.containsKey("xpack.security.http.ssl.keystore.path")
+            && settings.containsKey("xpack.security.http.ssl.certificate_authorities") == false) { //Can not set both trust stores and CA
             wait.setTrustStoreFile(getConfigDir().resolve(settings.get("xpack.security.http.ssl.keystore.path").toString()).toFile());
         }
         if (keystoreSettings.containsKey("xpack.security.http.ssl.keystore.secure_password")) {

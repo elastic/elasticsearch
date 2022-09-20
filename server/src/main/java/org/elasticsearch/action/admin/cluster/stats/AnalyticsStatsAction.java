68d299d51271bf20d02a8b7e20f7b617991fbcb4
Martijn van Groningen <martijn.v.groningen@gmail.com>
2022-09-20 09:18:53 +0200

    fixed server module violations

--- server/src/main/java/org/elasticsearch/action/admin/cluster/stats/AnalyticsStatsAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/stats/AnalyticsStatsAction.java
@@ -1,8 +1,9 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 package org.elasticsearch.action.admin.cluster.stats;
 
@@ -17,10 +18,10 @@ import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.common.util.EnumCounters;
 import org.elasticsearch.transport.TransportRequest;
 import org.elasticsearch.xcontent.ToXContentObject;
 import org.elasticsearch.xcontent.XContentBuilder;
-import org.elasticsearch.common.util.EnumCounters;
 
 import java.io.IOException;
 import java.util.List;

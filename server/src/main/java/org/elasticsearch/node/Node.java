adc4178c4ceb8677e82cfbf9828eccedf79a9fc9
Andrei Dan <andrei.dan@elastic.co>
2022-09-20 22:38:58 +0100

    Spotless

--- server/src/main/java/org/elasticsearch/node/Node.java
+++ server/src/main/java/org/elasticsearch/node/Node.java
@@ -231,6 +231,7 @@ import java.util.function.LongSupplier;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 import javax.net.ssl.SNIHostName;
 
 import static java.util.stream.Collectors.toList;

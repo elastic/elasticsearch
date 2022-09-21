2f4e29173773b90c42522dab2b2b3b930c1aa50d
Pooya Salehi <pooya.salehi@elastic.co>
2022-09-21 16:47:17 +0200

    Update test

--- server/src/internalClusterTest/java/org/elasticsearch/cluster/PrevalidateNodeRemovalIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/cluster/PrevalidateNodeRemovalIT.java
@@ -12,17 +12,26 @@ import org.elasticsearch.action.admin.cluster.node.shutdown.NodesRemovalPrevalid
 import org.elasticsearch.action.admin.cluster.node.shutdown.PrevalidateNodeRemovalAction;
 import org.elasticsearch.action.admin.cluster.node.shutdown.PrevalidateNodeRemovalRequest;
 import org.elasticsearch.action.admin.cluster.node.shutdown.PrevalidateNodeRemovalResponse;
+import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.test.ESIntegTestCase;
 
 import static org.hamcrest.Matchers.equalTo;
 
-@ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST)
+@ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST, numDataNodes = 0)
 public class PrevalidateNodeRemovalIT extends ESIntegTestCase {
 
-    public void testNodeRemovalFromGreenClusterIsSafe() throws Exception {
-        createIndex("test");
+    public void testNodeRemovalFromNonRedClusterIsSafe() throws Exception {
+        internalCluster().startMasterOnlyNode();
+        String node1 = internalCluster().startDataOnlyNode();
+        String node2 = internalCluster().startDataOnlyNode();
+        String indexName = "test-idx";
+        createIndex(
+            indexName,
+            Settings.builder().put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, 1).put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 1).build()
+        );
         ensureGreen();
-        String nodeName = randomFrom(internalCluster().getNodeNames());
+        String nodeName = randomFrom(node1, node2);
         PrevalidateNodeRemovalRequest req = new PrevalidateNodeRemovalRequest(nodeName);
         PrevalidateNodeRemovalResponse resp = client().execute(PrevalidateNodeRemovalAction.INSTANCE, req).get();
 
@@ -32,5 +41,17 @@ public class PrevalidateNodeRemovalIT extends ESIntegTestCase {
         assertNotNull(nodeResult);
         assertThat(nodeResult.name(), equalTo(nodeName));
         assertThat(nodeResult.result().isSafe(), equalTo(NodesRemovalPrevalidation.IsSafe.YES));
+
+        assertTrue(internalCluster().stopNode(node1));
+        ensureYellow();
+        PrevalidateNodeRemovalRequest req2 = new PrevalidateNodeRemovalRequest(node2);
+        PrevalidateNodeRemovalResponse resp2 = client().execute(PrevalidateNodeRemovalAction.INSTANCE, req2).get();
+
+        assertThat(resp2.getPrevalidation().getResult().isSafe(), equalTo(NodesRemovalPrevalidation.IsSafe.YES));
+        assertThat(resp2.getPrevalidation().getNodes().size(), equalTo(1));
+        NodesRemovalPrevalidation.NodeResult nodeResult2 = resp2.getPrevalidation().getNodes().get(0);
+        assertNotNull(nodeResult2);
+        assertThat(nodeResult2.name(), equalTo(node2));
+        assertThat(nodeResult2.result().isSafe(), equalTo(NodesRemovalPrevalidation.IsSafe.YES));
     }
 }

f9287cb1f513b63287c3bad5a10ad22588cd6717
Ievgen Degtiarenko <ievgen.degtiarenko@gmail.com>
2022-09-22 14:19:58 +0200

    Update server/src/main/java/org/elasticsearch/cluster/routing/ShardRouting.java
    Co-authored-by: Iraklis Psaroudakis <kingherc@gmail.com>

--- server/src/main/java/org/elasticsearch/cluster/routing/ShardRouting.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/ShardRouting.java
@@ -91,7 +91,7 @@ public final class ShardRouting implements Writeable, ToXContentObject {
         assert (currentNodeId == null) == (state == ShardRoutingState.UNASSIGNED)
             : "unassigned shard must not be assigned to a node " + this;
         assert relocatingNodeId == null || state != ShardRoutingState.STARTED
-            : "reallocating shard must node be started " + this;
+            : "reallocating shard must not be started " + this;
     }
 
     @Nullable

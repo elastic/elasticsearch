e174f4e005823bd92eb71715979f0fee74fbc828
Pooya Salehi <pooya.salehi@elastic.co>
2022-09-22 11:59:22 +0200

    Add integration tests

--- server/src/internalClusterTest/java/org/elasticsearch/cluster/PrevalidateNodeRemovalIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/cluster/PrevalidateNodeRemovalIT.java
@@ -8,11 +8,14 @@
 
 package org.elasticsearch.cluster;
 
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.action.admin.cluster.node.shutdown.NodesRemovalPrevalidation;
 import org.elasticsearch.action.admin.cluster.node.shutdown.PrevalidateNodeRemovalAction;
 import org.elasticsearch.action.admin.cluster.node.shutdown.PrevalidateNodeRemovalRequest;
 import org.elasticsearch.action.admin.cluster.node.shutdown.PrevalidateNodeRemovalResponse;
+import org.elasticsearch.cluster.health.ClusterHealthStatus;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.common.Priority;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.test.ESIntegTestCase;
 
@@ -21,7 +24,7 @@ import static org.hamcrest.Matchers.equalTo;
 @ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST, numDataNodes = 0)
 public class PrevalidateNodeRemovalIT extends ESIntegTestCase {
 
-    public void testNodeRemovalFromNonRedClusterIsSafe() throws Exception {
+    public void testNodeRemovalFromNonRedCluster() throws Exception {
         internalCluster().startMasterOnlyNode();
         String node1 = internalCluster().startDataOnlyNode();
         String node2 = internalCluster().startDataOnlyNode();
@@ -42,7 +45,8 @@ public class PrevalidateNodeRemovalIT extends ESIntegTestCase {
         assertThat(nodeResult.name(), equalTo(nodeName));
         assertThat(nodeResult.result().isSafe(), equalTo(NodesRemovalPrevalidation.IsSafe.YES));
 
-        assertTrue(internalCluster().stopNode(node1));
+        // Enforce a replica to get unassigned
+        updateIndexSettings(indexName, Settings.builder().put("index.routing.allocation.require._name", node1));
         ensureYellow();
         PrevalidateNodeRemovalRequest req2 = new PrevalidateNodeRemovalRequest(node2);
         PrevalidateNodeRemovalResponse resp2 = client().execute(PrevalidateNodeRemovalAction.INSTANCE, req2).get();
@@ -53,5 +57,38 @@ public class PrevalidateNodeRemovalIT extends ESIntegTestCase {
         assertNotNull(nodeResult2);
         assertThat(nodeResult2.name(), equalTo(node2));
         assertThat(nodeResult2.result().isSafe(), equalTo(NodesRemovalPrevalidation.IsSafe.YES));
+    }
+
+    public void testNodeRemovalFromRedCluster() throws Exception {
+        internalCluster().startMasterOnlyNode();
+        String node1 = internalCluster().startDataOnlyNode();
+        String node2 = internalCluster().startDataOnlyNode();
+        // Create an index pinned to one node, and then stop that node so the index is RED.
+        String indexName = "test-idx";
+        createIndex(
+            indexName,
+            Settings.builder().put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, 1).put("index.routing.allocation.require._name", node1).build()
+        );
+        ensureYellow(indexName);
+        internalCluster().stopNode(node1);
+        assertBusy(() -> {
+            ClusterHealthResponse healthResponse = client().admin()
+                .cluster()
+                .prepareHealth(indexName)
+                .setWaitForStatus(ClusterHealthStatus.RED)
+                .setWaitForEvents(Priority.LANGUID)
+                .execute()
+                .actionGet();
+            assertThat(healthResponse.getStatus(), equalTo(ClusterHealthStatus.RED));
+        });
+        // With a RED non-searchable-snapshot index, node removal safety is unknown
+        // since that node might have the last copy of the unassigned index.
+        PrevalidateNodeRemovalRequest req = new PrevalidateNodeRemovalRequest(node2);
+        PrevalidateNodeRemovalResponse resp = client().execute(PrevalidateNodeRemovalAction.INSTANCE, req).get();
+        assertThat(resp.getPrevalidation().getResult().isSafe(), equalTo(NodesRemovalPrevalidation.IsSafe.UNKNOWN));
+        assertThat(resp.getPrevalidation().getNodes().size(), equalTo(1));
+        NodesRemovalPrevalidation.NodeResult nodeResult = resp.getPrevalidation().getNodes().get(0);
+        assertThat(nodeResult.name(), equalTo(node2));
+        assertThat(nodeResult.result().isSafe(), equalTo(NodesRemovalPrevalidation.IsSafe.UNKNOWN));
     }
 }

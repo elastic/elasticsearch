2cc8fddfce1d95d8c04d2842ffc5c8c6c9bcd6c8
Jack Conradson <osjdconrad@gmail.com>
2022-09-22 15:57:16 -0700

    add tests and split out dfs phase

--- server/src/main/java/org/elasticsearch/search/dfs/DfsPhase.java
+++ server/src/main/java/org/elasticsearch/search/dfs/DfsPhase.java
@@ -45,148 +45,150 @@ public class DfsPhase {
 
     public void execute(SearchContext context) {
         try {
-            final boolean[] profile = new boolean[2];
-            profile[0] = context.getProfilers() != null;
-            final Timer rewriteTimer = profile[0] ? new Timer() : null;
-            final Timer createWeightTimer = profile[0] ? new Timer() : null;
-
-            Map<String, CollectionStatistics> fieldStatistics = new HashMap<>();
-            Map<Term, TermStatistics> stats = new HashMap<>();
-            IndexSearcher searcher = new IndexSearcher(context.searcher().getIndexReader()) {
-                @Override
-                public TermStatistics termStatistics(Term term, int docFreq, long totalTermFreq) throws IOException {
-                    if (context.isCancelled()) {
-                        throw new TaskCancelledException("cancelled");
-                    }
-                    TermStatistics ts = super.termStatistics(term, docFreq, totalTermFreq);
-                    if (ts != null) {
-                        stats.put(term, ts);
-                    }
-                    return ts;
-                }
-
-                @Override
-                public CollectionStatistics collectionStatistics(String field) throws IOException {
-                    if (context.isCancelled()) {
-                        throw new TaskCancelledException("cancelled");
-                    }
-                    CollectionStatistics cs = super.collectionStatistics(field);
-                    if (cs != null) {
-                        fieldStatistics.put(field, cs);
-                    }
-                    return cs;
-                }
-
-                @Override
-                public Query rewrite(Query original) throws IOException {
-                    if (profile[1]) {
-                        rewriteTimer.start();
-                    }
-
-                    try {
-                        return super.rewrite(original);
-                    } finally {
-                        if (profile[1]) {
-                            rewriteTimer.stop();
-                        }
-                    }
-                }
-
-                @Override
-                public Weight createWeight(Query query, ScoreMode scoreMode, float boost) throws IOException {
-                    if (profile[1]) {
-                        createWeightTimer.start();
-                    }
-
-                    try {
-                        return super.createWeight(query, scoreMode, boost);
-                    } finally {
-                        if (profile[1]) {
-                            createWeightTimer.stop();
-                        }
-                    }
-                }
-            };
-
-            searcher.createWeight(context.rewrittenQuery(), ScoreMode.COMPLETE, 1);
-            for (RescoreContext rescoreContext : context.rescore()) {
-                for (Query query : rescoreContext.getQueries()) {
-                    searcher.createWeight(context.searcher().rewrite(query), ScoreMode.COMPLETE, 1);
-                }
-            }
-
-            Term[] terms = stats.keySet().toArray(new Term[0]);
-            TermStatistics[] termStatistics = new TermStatistics[terms.length];
-            for (int i = 0; i < terms.length; i++) {
-                termStatistics[i] = stats.get(terms[i]);
-            }
-
-            context.dfsResult()
-                .termsStatistics(terms, termStatistics)
-                .fieldStatistics(fieldStatistics)
-                .maxDoc(context.searcher().getIndexReader().maxDoc());
-
-            // If kNN search is requested, perform kNN query and gather top docs
-            SearchSourceBuilder source = context.request().source();
-            if (source != null && source.knnSearch() != null) {
-                profile[1] = profile[0];
-                long knnStartTime = 0L;
-
-                if (profile[1]) {
-                    knnStartTime = System.nanoTime();
-                }
-
-                SearchExecutionContext searchExecutionContext = context.getSearchExecutionContext();
-                KnnSearchBuilder knnSearch = source.knnSearch();
-
-                KnnVectorQueryBuilder knnVectorQueryBuilder = knnSearch.toQueryBuilder();
-                if (context.request().getAliasFilter().getQueryBuilder() != null) {
-                    knnVectorQueryBuilder.addFilterQuery(context.request().getAliasFilter().getQueryBuilder());
-                }
-                ParsedQuery query = searchExecutionContext.toQuery(knnVectorQueryBuilder);
-
-                if (profile[1]) {
-                    TopScoreDocCollector tsdc = TopScoreDocCollector.create(knnSearch.k(), Integer.MAX_VALUE);
-                    InternalProfileCollector ipc = new InternalProfileCollector(tsdc, "KnnVectorQuery", List.of());
-
-                    try {
-                        searcher.search(query.query(), ipc);
-                        DfsKnnResults knnResults = new DfsKnnResults(tsdc.topDocs().scoreDocs);
-                        context.dfsResult().knnResults(knnResults);
-                    } finally {
-                        long knnTotalTime = System.nanoTime() - knnStartTime;
-                        context.dfsResult()
-                            .profileResult(
-                                new ProfileResult(
-                                    "KnnVectorQuery",
-                                    query.query().toString(),
-                                    Map.of(
-                                        "create_weight_count",
-                                        createWeightTimer.getCount(),
-                                        "create_weight",
-                                        createWeightTimer.getApproximateTiming(),
-                                        "rewrite_count",
-                                        rewriteTimer.getCount(),
-                                        "rewrite",
-                                        rewriteTimer.getApproximateTiming(),
-                                        "collector",
-                                        ipc.getTime()
-                                    ),
-                                    Map.of(),
-                                    knnTotalTime,
-                                    null
-                                )
-                            );
-                    }
-                } else {
-                    TopDocs topDocs = searcher.search(query.query(), knnSearch.k());
-                    DfsKnnResults knnResults = new DfsKnnResults(topDocs.scoreDocs);
-                    context.dfsResult().knnResults(knnResults);
-                }
-            }
+            collectStatistics(context);
+            executeKnnVectorQuery(context);
         } catch (Exception e) {
             throw new DfsPhaseExecutionException(context.shardTarget(), "Exception during dfs phase", e);
         }
     }
 
+    private void collectStatistics(SearchContext context) throws IOException {
+        Map<String, CollectionStatistics> fieldStatistics = new HashMap<>();
+        Map<Term, TermStatistics> stats = new HashMap<>();
+        IndexSearcher searcher = new IndexSearcher(context.searcher().getIndexReader()) {
+            @Override
+            public TermStatistics termStatistics(Term term, int docFreq, long totalTermFreq) throws IOException {
+                if (context.isCancelled()) {
+                    throw new TaskCancelledException("cancelled");
+                }
+                TermStatistics ts = super.termStatistics(term, docFreq, totalTermFreq);
+                if (ts != null) {
+                    stats.put(term, ts);
+                }
+                return ts;
+            }
+
+            @Override
+            public CollectionStatistics collectionStatistics(String field) throws IOException {
+                if (context.isCancelled()) {
+                    throw new TaskCancelledException("cancelled");
+                }
+                CollectionStatistics cs = super.collectionStatistics(field);
+                if (cs != null) {
+                    fieldStatistics.put(field, cs);
+                }
+                return cs;
+            }
+        };
+
+        searcher.createWeight(context.rewrittenQuery(), ScoreMode.COMPLETE, 1);
+        for (RescoreContext rescoreContext : context.rescore()) {
+            for (Query query : rescoreContext.getQueries()) {
+                searcher.createWeight(context.searcher().rewrite(query), ScoreMode.COMPLETE, 1);
+            }
+        }
+
+        Term[] terms = stats.keySet().toArray(new Term[0]);
+        TermStatistics[] termStatistics = new TermStatistics[terms.length];
+        for (int i = 0; i < terms.length; i++) {
+            termStatistics[i] = stats.get(terms[i]);
+        }
+
+        context.dfsResult()
+            .termsStatistics(terms, termStatistics)
+            .fieldStatistics(fieldStatistics)
+            .maxDoc(context.searcher().getIndexReader().maxDoc());
+    }
+
+    private void executeKnnVectorQuery(SearchContext context) throws IOException {
+        SearchSourceBuilder source = context.request().source();
+        if (source == null || source.knnSearch() == null) {
+            return;
+        }
+
+        boolean profile = context.getProfilers() != null;
+        long knnStartTime = profile ? System.nanoTime() : 0L;
+        final Timer rewriteTimer = profile ? new Timer() : null;
+        final Timer createWeightTimer = profile ? new Timer() : null;
+
+        IndexSearcher searcher = new IndexSearcher(context.searcher().getIndexReader()) {
+            @Override
+            public Query rewrite(Query original) throws IOException {
+                if (rewriteTimer != null) {
+                    rewriteTimer.start();
+                }
+
+                try {
+                    return super.rewrite(original);
+                } finally {
+                    if (rewriteTimer != null) {
+                        rewriteTimer.stop();
+                    }
+                }
+            }
+
+            @Override
+            public Weight createWeight(Query query, ScoreMode scoreMode, float boost) throws IOException {
+                if (createWeightTimer != null) {
+                    createWeightTimer.start();
+                }
+
+                try {
+                    return super.createWeight(query, scoreMode, boost);
+                } finally {
+                    if (createWeightTimer != null) {
+                        createWeightTimer.stop();
+                    }
+                }
+            }
+        };
+
+        SearchExecutionContext searchExecutionContext = context.getSearchExecutionContext();
+        KnnSearchBuilder knnSearch = source.knnSearch();
+
+        KnnVectorQueryBuilder knnVectorQueryBuilder = knnSearch.toQueryBuilder();
+        if (context.request().getAliasFilter().getQueryBuilder() != null) {
+            knnVectorQueryBuilder.addFilterQuery(context.request().getAliasFilter().getQueryBuilder());
+        }
+        ParsedQuery query = searchExecutionContext.toQuery(knnVectorQueryBuilder);
+
+        if (profile) {
+            TopScoreDocCollector tsdc = TopScoreDocCollector.create(knnSearch.k(), Integer.MAX_VALUE);
+            InternalProfileCollector ipc = new InternalProfileCollector(tsdc, "KnnVectorQuery", List.of());
+
+            try {
+                searcher.search(query.query(), ipc);
+                DfsKnnResults knnResults = new DfsKnnResults(tsdc.topDocs().scoreDocs);
+                context.dfsResult().knnResults(knnResults);
+            } finally {
+                long knnTotalTime = System.nanoTime() - knnStartTime;
+                context.dfsResult()
+                    .profileResult(
+                        new ProfileResult(
+                            "KnnVectorQuery",
+                            query.query().toString(),
+                            Map.of(
+                                "create_weight_count",
+                                createWeightTimer.getCount(),
+                                "create_weight",
+                                createWeightTimer.getApproximateTiming(),
+                                "rewrite_count",
+                                rewriteTimer.getCount(),
+                                "rewrite",
+                                rewriteTimer.getApproximateTiming(),
+                                "collector",
+                                ipc.getTime()
+                            ),
+                            Map.of(),
+                            knnTotalTime,
+                            null
+                        )
+                    );
+            }
+        } else {
+            TopDocs topDocs = searcher.search(query.query(), knnSearch.k());
+            DfsKnnResults knnResults = new DfsKnnResults(topDocs.scoreDocs);
+            context.dfsResult().knnResults(knnResults);
+        }
+    }
 }

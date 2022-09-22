b83a934f4550333fb1a619f44bf8f795f1789612
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2022-09-22 16:06:47 +0200

    problems with runtime jackson

--- build-tools/src/main/java/org/elasticsearch/gradle/stableplugin/StableGenerateNamedComponentsTask.java
+++ build-tools/src/main/java/org/elasticsearch/gradle/stableplugin/StableGenerateNamedComponentsTask.java
@@ -8,9 +8,12 @@
 
 package org.elasticsearch.gradle.stableplugin;
 
+import org.elasticsearch.gradle.stableplugin.scanner.NamedComponentScanner;
+import org.elasticsearch.plugin.scanner.impl.ClassReaders;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.FileSystemLocation;
 import org.gradle.api.file.ProjectLayout;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.logging.Logger;
@@ -22,12 +25,19 @@ import org.gradle.api.tasks.CompileClasspath;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.internal.impldep.com.fasterxml.jackson.core.JsonProcessingException;
+import org.gradle.internal.impldep.com.fasterxml.jackson.databind.ObjectMapper;
 import org.gradle.workers.WorkAction;
 import org.gradle.workers.WorkParameters;
 import org.gradle.workers.WorkerExecutor;
+import org.objectweb.asm.ClassReader;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 import javax.inject.Inject;
 
 //asm here?
@@ -50,6 +60,7 @@ public abstract class StableGenerateNamedComponentsTask extends DefaultTask {
         workerExecutor.noIsolation().submit(GenerateNamedComponentsAction.class, params -> {
             params.getClasspath().from(classpath);
             params.getPluginClasses().setFrom(pluginClasses);
+            params.getOutputFile().set(getOutputFile());
         });
     }
 
@@ -78,21 +89,42 @@ public abstract class StableGenerateNamedComponentsTask extends DefaultTask {
     public abstract static class GenerateNamedComponentsAction implements WorkAction<Parameters> {
         @Override
         public void execute() {
-            final Parameters parameters = getParameters();
             Set<File> files = getParameters().getClasspath().getFiles();
             Set<File> pluginFiles = getParameters().getPluginClasses().getFiles();
             LOGGER.info(files.toString());
             LOGGER.info(pluginFiles.toString());
+
+            Stream<Path> pluginJars = Stream.concat(files.stream(), pluginFiles.stream())
+                .map(File::toPath);
+            Stream<ClassReader> classReaderStream = ClassReaders.ofPaths(pluginJars);
+
+            NamedComponentScanner namedComponentScanner = new NamedComponentScanner();
+            Map<String, Map<String, String>> namedComponentsMap
+                = namedComponentScanner.scanForNamedClasses(classReaderStream);
+            writeToFile(namedComponentsMap);
+        }
+
+        private void writeToFile(Map<String, Map<String, String>> namedComponentsMap) {
+
+            try {
+                String json = new ObjectMapper().writeValueAsString(namedComponentsMap);
+                File file = getParameters().getOutputFile().getAsFile().get();
+                Path of = Path.of(file.getAbsolutePath());
+                Files.writeString(of, json);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
     interface Parameters extends WorkParameters {
-        Property<String> getProjectPath();
 
         ConfigurableFileCollection getClasspath();
 
-        SetProperty<File> getSrcDirs();
 
         ConfigurableFileCollection getPluginClasses();
+
+        RegularFileProperty getOutputFile();
     }
 }

e03ffa7e37ee284a84a7bba92ef6a010e9023795
Justin Cranford <justin.cranford@elastic.co>
2022-09-23 14:27:39 -0400

    checkStyle

--- server/src/main/java/org/elasticsearch/transport/SniffConnectionStrategy.java
+++ server/src/main/java/org/elasticsearch/transport/SniffConnectionStrategy.java
@@ -23,7 +23,6 @@ import org.elasticsearch.common.UUIDs;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
-import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;

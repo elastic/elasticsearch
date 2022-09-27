9f6c7f6637d80a1c9d9c68ff57a079d185127f4d
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2022-09-27 14:14:09 +0200

    partial tests

---
+++ build-tools/src/main/java/org/elasticsearch/gradle/plugin/scanner/ClassReaders.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.gradle.plugin.scanner;
+
+import org.objectweb.asm.ClassReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.jar.JarFile;
+import java.util.stream.Stream;
+import java.util.zip.ZipFile;
+
+/**
+ * A utility class containing methods to create streams of ASM's ClassReader
+ *
+ * @see ClassReader
+ */
+public class ClassReaders {
+    // TODO javadoc on closing a stream
+
+    public static Stream<ClassReader> ofPaths(Stream<Path> list) {
+        return list.filter(Files::exists).flatMap(p -> {
+            if (p.toString().endsWith(".jar")) {
+                return classesInJar(p);
+            } else {
+                return classesInPath(p);
+            }
+        });
+    }
+
+    private static final String MODULE_INFO = "module-info.class";
+
+    private static Stream<ClassReader> classesInJar(Path jar) {
+        // try {
+        // FileSystem jarFs = FileSystems.newFileSystem(jar);
+        // Path root = jarFs.getPath("/");
+
+        try {
+            JarFile jf = new JarFile(jar.toFile(), true, ZipFile.OPEN_READ, Runtime.version());
+
+            Stream<ClassReader> classReaderStream = jf.versionedStream()
+                .filter(e -> e.getName().endsWith(".class") && e.getName().equals(MODULE_INFO) == false)
+                .map(e -> {
+                    try (InputStream is = jf.getInputStream(e)) {
+                        byte[] classBytes = is.readAllBytes();
+                        return new ClassReader(classBytes);
+                    } catch (IOException ex) {
+                        throw new UncheckedIOException(ex);
+                    }
+                });
+            return classReaderStream;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Stream.empty();
+    }
+
+    private static Stream<ClassReader> classesInPath(Path root) {
+        try { // todo what to do on exception
+            Stream<Path> stream = Files.walk(root);
+            return stream.filter(p -> p.toString().endsWith(".class"))
+                .filter(p -> p.toString().endsWith("module-info.class") == false)
+                .map(p -> {
+                    try (InputStream is = Files.newInputStream(p)) {
+                        byte[] classBytes = is.readAllBytes();
+                        return new ClassReader(classBytes);
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                });
+        } catch (IOException e) {
+            // e.printStackTrace();
+        }
+        return Stream.empty();
+    }
+
+    // Duplication from PluginsService::uncheckedToURI
+    public static final URI uncheckedToURI(URL url) {
+        try {
+            return url.toURI();
+        } catch (URISyntaxException e) {
+            throw new AssertionError(new IOException(e));
+        }
+    }
+}
---
+++ build-tools/src/test/groovy/org/elasticsearch/gradle/plugin/scanner/ClassScannerTests.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.gradle.plugin.scanner;
+
+import junit.framework.TestCase;
+
+
+import org.elasticsearch.gradle.plugin.scanner.test_classes.TestExtensible;
+import org.hamcrest.Matchers;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Type;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ClassScannerTests extends TestCase {
+
+    public void testExtensibleInHierarchy() throws IOException {
+        ClassScanner reader = new ClassScanner(Type.getDescriptor(TestExtensible.class), (classname, map) -> {
+            map.put(classname, classname);
+            return null;
+        });
+        Stream<ClassReader> classReaderStream = ofClassPath();
+        reader.visit(classReaderStream);
+        Map<String, String> extensibleClasses = reader.getFoundClasses();
+
+        assertThat(
+            extensibleClasses,
+            Matchers.allOf(
+                Matchers.hasEntry(
+                    "org/elasticsearch/gradle/plugin/scanner/test_classes/ExtensibleClass",
+                    "org/elasticsearch/gradle/plugin/scanner/test_classes/ExtensibleClass"
+                ),
+                Matchers.hasEntry(
+                    "org/elasticsearch/gradle/plugin/scanner/test_classes/ImplementingExtensible",
+                    "org/elasticsearch/gradle/plugin/scanner/test_classes/ExtensibleInterface"
+                ),
+                Matchers.hasEntry(
+                    "org/elasticsearch/gradle/plugin/scanner/test_classes/SubClass",
+                    "org/elasticsearch/gradle/plugin/scanner/test_classes/ExtensibleClass"
+                )
+            )
+        );
+    }
+
+    public static Stream<ClassReader> ofClassPath() throws IOException {
+        String classpath = System.getProperty("java.class.path");
+        return ofClassPath(classpath);
+    }
+    static Stream<ClassReader> ofClassPath(String classpath) {
+        if (classpath != null && classpath.equals("") == false) {// todo when do we set cp to "" ?
+            String[] pathelements = classpath.split(":");
+            return ClassReaders.ofPaths(Arrays.stream(pathelements).map(Paths::get));
+        }
+        return Stream.empty();
+    }
+
+}

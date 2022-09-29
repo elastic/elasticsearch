cec444ab1f5ccaefa6fda89084c79595e9de6c5f
Simon Cooper <simon.cooper@elastic.co>
2022-09-29 16:56:12 +0100

    Update RestResponseTests

--- server/src/test/java/org/elasticsearch/rest/RestResponseTests.java
+++ server/src/test/java/org/elasticsearch/rest/RestResponseTests.java
@@ -66,7 +66,7 @@ public class RestResponseTests extends ESTestCase {
         Exception t = new ElasticsearchException("an error occurred reading data", new FileNotFoundException("/foo/bar"));
         RestResponse response = new RestResponse(channel, t);
         String text = response.content().utf8ToString();
-        assertThat(text, containsString("ElasticsearchException[an error occurred reading data]"));
+        assertThat(text, containsString("an error occurred reading data"));
         assertThat(text, not(containsString("FileNotFoundException")));
         assertThat(text, not(containsString("/foo/bar")));
         assertThat(text, not(containsString("error_trace")));
@@ -83,19 +83,6 @@ public class RestResponseTests extends ESTestCase {
             {"type":"exception","reason":"an error occurred reading data"}"""));
         assertThat(text, containsString("""
             {"type":"file_not_found_exception","reason":"/foo/bar"}"""));
-    }
-
-    public void testNonElasticsearchExceptionIsNotShownAsSimpleMessage() throws Exception {
-        RestRequest request = new FakeRestRequest();
-        RestChannel channel = new SimpleExceptionRestChannel(request);
-
-        Exception t = new UnknownException("an error occurred reading data", new FileNotFoundException("/foo/bar"));
-        RestResponse response = new RestResponse(channel, t);
-        String text = response.content().utf8ToString();
-        assertThat(text, not(containsString("UnknownException[an error occurred reading data]")));
-        assertThat(text, not(containsString("FileNotFoundException[/foo/bar]")));
-        assertThat(text, not(containsString("error_trace")));
-        assertThat(text, containsString("\"error\":\"No ElasticsearchException found\""));
     }
 
     public void testErrorTrace() throws Exception {
@@ -138,7 +125,8 @@ public class RestResponseTests extends ESTestCase {
 
         RestResponse response = new RestResponse(channel, null);
         String text = response.content().utf8ToString();
-        assertThat(text, containsString("\"error\":\"unknown\""));
+        assertThat(text, containsString("\"type\":\"unknown\""));
+        assertThat(text, containsString("\"reason\":\"unknown\""));
         assertThat(text, not(containsString("error_trace")));
     }
 
@@ -229,32 +217,26 @@ public class RestResponseTests extends ESTestCase {
                 original = new ElasticsearchException("ElasticsearchException without cause");
                 if (detailed) {
                     addHeadersOrMetadata = randomBoolean();
-                    reason = "ElasticsearchException without cause";
-                } else {
-                    reason = "ElasticsearchException[ElasticsearchException without cause]";
                 }
+                reason = "ElasticsearchException without cause";
             }
             case 1 -> {
                 original = new ElasticsearchException("ElasticsearchException with a cause", new FileNotFoundException("missing"));
                 if (detailed) {
                     addHeadersOrMetadata = randomBoolean();
-                    type = "exception";
-                    reason = "ElasticsearchException with a cause";
                     cause = new ElasticsearchException("Elasticsearch exception [type=file_not_found_exception, reason=missing]");
-                } else {
-                    reason = "ElasticsearchException[ElasticsearchException with a cause]";
                 }
+                type = "exception";
+                reason = "ElasticsearchException with a cause";
             }
             case 2 -> {
                 original = new ResourceNotFoundException("ElasticsearchException with custom status");
                 status = RestStatus.NOT_FOUND;
                 if (detailed) {
                     addHeadersOrMetadata = randomBoolean();
-                    type = "resource_not_found_exception";
-                    reason = "ElasticsearchException with custom status";
-                } else {
-                    reason = "ResourceNotFoundException[ElasticsearchException with custom status]";
                 }
+                type = "resource_not_found_exception";
+                reason = "ElasticsearchException with custom status";
             }
             case 3 -> {
                 TransportAddress address = buildNewFakeTransportAddress();
@@ -265,12 +247,8 @@ public class RestResponseTests extends ESTestCase {
                         new ResourceAlreadyExistsException("ElasticsearchWrapperException with a cause that has a custom status")
                 );
                 status = RestStatus.BAD_REQUEST;
-                if (detailed) {
-                    type = "resource_already_exists_exception";
-                    reason = "ElasticsearchWrapperException with a cause that has a custom status";
-                } else {
-                    reason = "RemoteTransportException[[remote][" + address.toString() + "][action]]";
-                }
+                type = "resource_already_exists_exception";
+                reason = "ElasticsearchWrapperException with a cause that has a custom status";
             }
             case 4 -> {
                 original = new RemoteTransportException(
@@ -278,23 +256,17 @@ public class RestResponseTests extends ESTestCase {
                         new IllegalArgumentException("wrong")
                 );
                 status = RestStatus.BAD_REQUEST;
-                if (detailed) {
-                    type = "illegal_argument_exception";
-                    reason = "wrong";
-                } else {
-                    reason = "RemoteTransportException[[ElasticsearchWrapperException with a cause that has a special treatment]]";
-                }
+                type = "illegal_argument_exception";
+                reason = "wrong";
             }
             case 5 -> {
                 status = randomFrom(RestStatus.values());
                 original = new ElasticsearchStatusException("ElasticsearchStatusException with random status", status);
                 if (detailed) {
                     addHeadersOrMetadata = randomBoolean();
-                    type = "status_exception";
-                    reason = "ElasticsearchStatusException with random status";
-                } else {
-                    reason = "ElasticsearchStatusException[ElasticsearchStatusException with random status]";
                 }
+                type = "status_exception";
+                reason = "ElasticsearchStatusException with random status";
             }
             default -> throw new UnsupportedOperationException("Failed to generate random exception");
         }

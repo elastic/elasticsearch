cf0224c081583143bb252d8c64222367120f77c0
Aurelien FOUCRET <aurelien.foucret@elastic.co>
2022-09-30 12:07:56 +0200

    Create plugin to handle sarch engine abstraction.

---
+++ x-pack/plugin/search-engines/src/main/java/org/elasticsearch/searchengines/SearchEnginesPlugin.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+package org.elasticsearch.searchengines;
+
+import org.elasticsearch.action.ActionRequest;
+import org.elasticsearch.action.ActionResponse;
+import org.elasticsearch.client.internal.Client;
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.cluster.routing.allocation.decider.AllocationDeciders;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.IndexScopedSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsFilter;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.env.NodeEnvironment;
+import org.elasticsearch.plugins.ActionPlugin;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.repositories.RepositoriesService;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestHandler;
+import org.elasticsearch.script.ScriptService;
+import org.elasticsearch.searchengines.action.CreateSearchEngineAction;
+import org.elasticsearch.searchengines.action.CreateSearchEngineTransportAction;
+import org.elasticsearch.searchengines.action.GetSearchEngineAction;
+import org.elasticsearch.searchengines.action.GetSearchEngineTransportAction;
+import org.elasticsearch.searchengines.rest.RestCreateSearchEngineAction;
+import org.elasticsearch.searchengines.rest.RestGetSearchEngineAction;
+import org.elasticsearch.threadpool.ThreadPool;
+import org.elasticsearch.tracing.Tracer;
+import org.elasticsearch.watcher.ResourceWatcherService;
+import org.elasticsearch.xcontent.NamedXContentRegistry;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SearchEnginesPlugin extends Plugin implements ActionPlugin {
+
+    public static final String REST_BASE_PATH = "/_search_engine";
+
+    @Override
+    public Collection<Object> createComponents(
+        Client client,
+        ClusterService clusterService,
+        ThreadPool threadPool,
+        ResourceWatcherService resourceWatcherService,
+        ScriptService scriptService,
+        NamedXContentRegistry xContentRegistry,
+        Environment environment,
+        NodeEnvironment nodeEnvironment,
+        NamedWriteableRegistry namedWriteableRegistry,
+        IndexNameExpressionResolver indexNameExpressionResolver,
+        Supplier<RepositoriesService> repositoriesServiceSupplier,
+        Tracer tracer,
+        AllocationDeciders allocationDeciders
+    ) {
+        var searchEngineMetadataService = new SearchEngineMetadataService(clusterService);
+        return List.of(searchEngineMetadataService);
+    }
+
+    @Override
+    public List<ActionPlugin.ActionHandler<? extends ActionRequest, ? extends ActionResponse>> getActions() {
+        return List.of(
+            new ActionPlugin.ActionHandler<>(CreateSearchEngineAction.INSTANCE, CreateSearchEngineTransportAction.class),
+            new ActionPlugin.ActionHandler<>(GetSearchEngineAction.INSTANCE, GetSearchEngineTransportAction.class)
+        );
+    }
+
+    @Override
+    public List<RestHandler> getRestHandlers(
+        Settings settings,
+        RestController restController,
+        ClusterSettings clusterSettings,
+        IndexScopedSettings indexScopedSettings,
+        SettingsFilter settingsFilter,
+        IndexNameExpressionResolver indexNameExpressionResolver,
+        Supplier<DiscoveryNodes> nodesInCluster
+    ) {
+        return List.of(
+            new RestCreateSearchEngineAction(),
+            new RestGetSearchEngineAction()
+        );
+    }
+}

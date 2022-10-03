a1670c7e20fa63ede0b715d722ff005ab8d704ac
Kathleen DeRusso <kathleen.derusso@elastic.co>
2022-10-03 09:26:59 -0400

    Remove extra comments

--- x-pack/plugin/relevance-search/src/main/java/org/elasticsearch/xpack/relevancesearch/relevance/QueryConfiguration.java
+++ x-pack/plugin/relevance-search/src/main/java/org/elasticsearch/xpack/relevancesearch/relevance/QueryConfiguration.java
@@ -13,12 +13,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
 public class QueryConfiguration {
 
     private Map<String, Float> fieldsAndBoosts;
--- x-pack/plugin/relevance-search/src/main/java/org/elasticsearch/xpack/relevancesearch/relevance/RelevanceSettings.java
+++ x-pack/plugin/relevance-search/src/main/java/org/elasticsearch/xpack/relevancesearch/relevance/RelevanceSettings.java
@@ -7,19 +7,10 @@
 
 package org.elasticsearch.xpack.relevancesearch.relevance;
 
-import java.util.Map;
-
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
 public class RelevanceSettings {
 
     private final String TYPE = "relevance_settings";
     private final String QUERY_TYPE = "combined_fields";
-
     private String name;
 
     private QueryConfiguration queryConfiguration;

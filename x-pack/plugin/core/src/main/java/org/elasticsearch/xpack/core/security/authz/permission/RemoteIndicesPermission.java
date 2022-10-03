8aa5e780a38db9d08b1d1417fa9077c31cb677bb
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2022-10-03 17:31:51 +0200

    TODOs

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/permission/RemoteIndicesPermission.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/permission/RemoteIndicesPermission.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Set;
 
 public record RemoteIndicesPermission(
+    // TODO this is redundant
     RestrictedIndices restrictedIndices,
     List<IndicesPermissionWithRemoteClusterAliases> indicesPermissions
 ) {

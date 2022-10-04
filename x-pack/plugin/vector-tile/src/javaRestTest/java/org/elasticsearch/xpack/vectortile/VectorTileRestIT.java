2850104a0e42ebbf5fbc280b2764da4a068d81ae
Rene Groeschke <rene@elastic.co>
2022-10-04 18:55:02 +0200

    More forbidden api fixes

--- x-pack/plugin/vector-tile/src/javaRestTest/java/org/elasticsearch/xpack/vectortile/VectorTileRestIT.java
+++ x-pack/plugin/vector-tile/src/javaRestTest/java/org/elasticsearch/xpack/vectortile/VectorTileRestIT.java
@@ -35,6 +35,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Rest test for _mvt end point. The tests only check that the structure of the vector tiles is sound in
@@ -91,10 +92,10 @@ public class VectorTileRestIT extends ESRestTestCase {
         for (int i = 0; i < 30; i += 10) {
             for (int j = 0; j <= i; j++) {
                 final Request putRequest = new Request(HttpPost.METHOD_NAME, INDEX_POINTS + "/_doc/");
-                putRequest.setJsonEntity("""
+                putRequest.setJsonEntity(String.format(Locale.ROOT, """
                     {
                       "location": "POINT(%s %s)", "name": "point%s", "value1": %s, "value2": %s
-                    }""".formatted(x, y, i, i, i + 1));
+                    }""", x, y, i, i, i + 1));
                 response = client().performRequest(putRequest);
                 assertThat(response.getStatusLine().getStatusCode(), Matchers.equalTo(HttpStatus.SC_CREATED));
             }
@@ -134,10 +135,10 @@ public class VectorTileRestIT extends ESRestTestCase {
         assertThat(response.getStatusLine().getStatusCode(), Matchers.equalTo(HttpStatus.SC_OK));
 
         final Request putRequest = new Request(HttpPost.METHOD_NAME, indexName + "/_doc/" + id);
-        putRequest.setJsonEntity("""
+        putRequest.setJsonEntity(String.format(Locale.ROOT, """
             {
               "location": "%s", "name": "geometry", "value1": %s, "value2": %s, "nullField" : null, "ignore_value" : ""
-            }""".formatted(WellKnownText.toWKT(geometry), 1, 2));
+            }""", WellKnownText.toWKT(geometry), 1, 2));
         response = client().performRequest(putRequest);
         assertThat(response.getStatusLine().getStatusCode(), Matchers.equalTo(HttpStatus.SC_CREATED));
 
@@ -191,10 +192,10 @@ public class VectorTileRestIT extends ESRestTestCase {
             + " "
             + y
             + "))";
-        putRequest.setJsonEntity("""
+        putRequest.setJsonEntity(String.format(Locale.ROOT, """
             {
               "location": "%s", "name": "collection", "value1": %s, "value2": %s
-            }""".formatted(collection, 1, 2));
+            }""", collection, 1, 2));
         response = client().performRequest(putRequest);
         assertThat(response.getStatusLine().getStatusCode(), Matchers.equalTo(HttpStatus.SC_CREATED));
 

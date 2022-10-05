261bba1a61ebe767a1adfa967063575440666608
Luca Cavanna <javanna@users.noreply.github.com>
2022-10-05 10:00:59 +0200

    spotless

--- test/framework/src/main/java/org/elasticsearch/test/AbstractQueryTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/AbstractQueryTestCase.java
@@ -43,7 +43,6 @@ import org.elasticsearch.xcontent.XContentParserConfiguration;
 import org.elasticsearch.xcontent.XContentType;
 import org.elasticsearch.xcontent.json.JsonStringEncoder;
 import org.elasticsearch.xcontent.json.JsonXContent;
-import org.hamcrest.Matchers;
 
 import java.io.IOException;
 import java.time.Instant;
@@ -63,7 +62,6 @@ import static org.elasticsearch.index.query.AbstractQueryBuilder.parseTopLevelQu
 import static org.elasticsearch.search.SearchModule.INDICES_MAX_NESTED_DEPTH_SETTING;
 import static org.elasticsearch.test.EqualsHashCodeTestUtils.checkEqualsAndHashCode;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.either;
 import static org.hamcrest.Matchers.greaterThan;
@@ -126,10 +124,7 @@ public abstract class AbstractQueryTestCase<QB extends AbstractQueryBuilder<QB>>
                 + "]";
             QB q = query;
             // one more level causes an exception
-            Exception exception = expectThrows(
-                Exception.class,
-                () -> parseQuery(Strings.toString(createQueryWithInnerQuery(q)))
-            );
+            Exception exception = expectThrows(Exception.class, () -> parseQuery(Strings.toString(createQueryWithInnerQuery(q))));
             // there may be nested XContentParseExceptions coming from ObjectParser, we just extract the root cause
             while (exception.getCause() != null) {
                 assertThat(exception.getCause(), either(instanceOf(IllegalArgumentException.class)).or(instanceOf(ParsingException.class)));

7b4f2d2e5773caaa49f95b8d4a74ed7e7222ec20
Andrei Dan <andrei.dan@elastic.co>
2022-10-05 15:12:38 +0100

    [8.5] [HealthAPI] Diagnosis: report typed affected resources (#90653) (#90678)
    The health API reports the affected resources in case of an unhealthy
    deployment. Until now all indicators reported one type of resource per
    diagnosis (index, ILM policy, snapshot repository)
    
    With the introduction of the disk indicator we now have an indicator
    that reports multiple types of resources under the same diagnosis (ie.
    nodes and indices).
    
    This changes the structure of the `affected_resources` field to
    accommodate multiple types of resources:
    ```
    "affected_resources": {
      "nodes": [
        {
          "id": "e1af6F5rTcmgpExkdOMzCg",
          "name": "hot"
        },
        {
          "id": "u_wBVl4ZRne4uZq_ziLsuw",
          "name": "warm"
        }
      ],
      "indices": [
        ".geoip_databases",
        "test_index"
      ]
    }
    ```

--- server/src/main/java/org/elasticsearch/health/Diagnosis.java
+++ server/src/main/java/org/elasticsearch/health/Diagnosis.java
@@ -8,12 +8,16 @@
 
 package org.elasticsearch.health;
 
+import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.core.Nullable;
+import org.elasticsearch.xcontent.ToXContentFragment;
 import org.elasticsearch.xcontent.ToXContentObject;
 import org.elasticsearch.xcontent.XContentBuilder;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 import static org.elasticsearch.health.HealthService.HEALTH_API_ID_PREFIX;
 
@@ -23,7 +27,102 @@ import static org.elasticsearch.health.HealthService.HEALTH_API_ID_PREFIX;
  * @param definition The definition of the diagnosis (e.g. message, helpURL)
  * @param affectedResources Optional list of "things" that are affected by this condition (e.g. shards, indices, or policies).
  */
-public record Diagnosis(Definition definition, @Nullable List<String> affectedResources) implements ToXContentObject {
+public record Diagnosis(Definition definition, @Nullable List<Resource> affectedResources) implements ToXContentObject {
+
+    /**
+     * Represents a type of affected resource, together with the resources/abstractions that
+     * are affected.
+     */
+    public static class Resource implements ToXContentFragment {
+
+        public static final String ID_FIELD = "id";
+        public static final String NAME_FIELD = "name";
+
+        public enum Type {
+            INDEX("indices"),
+            NODE("nodes"),
+            SLM_POLICY("slm_policy"),
+            SNAPSHOT_REPOSITORY("snapshot_repository");
+
+            private final String displayValue;
+
+            Type(String displayValue) {
+                this.displayValue = displayValue;
+            }
+        }
+
+        private final Type type;
+
+        @Nullable
+        private Collection<String> values;
+        @Nullable
+        private Collection<DiscoveryNode> nodes;
+
+        public Resource(Type type, Collection<String> values) {
+            if (type == Type.NODE) {
+                throw new IllegalArgumentException("Nodes should be modelled using the dedicated constructor");
+            }
+
+            this.type = type;
+            this.values = values;
+        }
+
+        public Resource(Collection<DiscoveryNode> nodes) {
+            this.type = Type.NODE;
+            this.nodes = nodes;
+        }
+
+        @Override
+        public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+            if (nodes != null) {
+                // we report both node ids and names so we need a bit of structure
+                builder.startArray(type.displayValue);
+                for (DiscoveryNode node : nodes) {
+                    builder.startObject();
+                    builder.field(ID_FIELD, node.getId());
+                    if (node.getName() != null) {
+                        builder.field(NAME_FIELD, node.getName());
+                    }
+                    builder.endObject();
+                }
+                builder.endArray();
+            } else {
+                builder.field(type.displayValue, values);
+            }
+            return builder;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Resource resource = (Resource) o;
+            return type == resource.type && Objects.equals(values, resource.values) && Objects.equals(nodes, resource.nodes);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(type, values, nodes);
+        }
+
+        public Type getType() {
+            return type;
+        }
+
+        @Nullable
+        public Collection<String> getValues() {
+            return values;
+        }
+
+        @Nullable
+        public Collection<DiscoveryNode> getNodes() {
+            return nodes;
+        }
+    }
 
     /**
      * Details a diagnosis - cause and a potential action that a user could take to clear an issue identified by a {@link HealthService}.
@@ -44,7 +143,11 @@ public record Diagnosis(Definition definition, @Nullable List<String> affectedRe
         builder.field("action", definition.action);
 
         if (affectedResources != null && affectedResources.size() > 0) {
-            builder.field("affected_resources", affectedResources);
+            builder.startObject("affected_resources");
+            for (Resource affectedResource : affectedResources) {
+                affectedResource.toXContent(builder, params);
+            }
+            builder.endObject();
         }
 
         builder.field("help_url", definition.helpURL);

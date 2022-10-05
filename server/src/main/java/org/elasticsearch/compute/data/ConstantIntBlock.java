a4eda61fc6a895800ace220ec4a2579bfffec051
Chris Hegarty <62058229+ChrisHegarty@users.noreply.github.com>
2022-10-05 14:11:19 +0100

    Add Page and Block data abstractions to support compute. (ESQL-261)
    Add Page and Block data abstractions to support compute.
    
    A page is a column-oriented data abstraction that allows data to be
    passed between operators in batches. A page is further composed of a
    number of blocks. A Block is a columnar data representation that has a
    position (row) count, and various data retrieval methods for accessing
    the underlying data that is stored at a given position. Pages and Blocks
    are immutable and can be passed between threads.
    
    The data abstractions are added to the org.elasticsearch.compute.data
    package of server, since they are fundamental to how data moves through
    the compute engine.

---
+++ server/src/main/java/org/elasticsearch/compute/data/ConstantIntBlock.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.compute.data;
+
+/**
+ * Block implementation that stores a constant integer value.
+ */
+public final class ConstantIntBlock extends Block {
+
+    private final int value;
+
+    public ConstantIntBlock(int value, int positionCount) {
+        super(positionCount);
+        this.value = value;
+    }
+
+    @Override
+    public int getInt(int position) {
+        assert assertPosition(position);
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "ConstantIntBlock{positions=" + getPositionCount() + ", value=" + value + '}';
+    }
+}
---
+++ server/src/main/java/org/elasticsearch/compute/data/ConstantLongBlock.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.compute.data;
+
+/**
+ * Block implementation that stores a constant long value.
+ */
+public final class ConstantLongBlock extends Block {
+
+    private final long value;
+
+    public ConstantLongBlock(long value, int positionCount) {
+        super(positionCount);
+        this.value = value;
+    }
+
+    @Override
+    public long getLong(int position) {
+        assert assertPosition(position);
+        return value;
+    }
+
+    @Override
+    public double getDouble(int position) {
+        assert assertPosition(position);
+        return value;  // Widening primitive conversions, no loss of precision
+    }
+
+    @Override
+    public String toString() {
+        return "ConstantLongBlock{positions=" + getPositionCount() + ", value=" + value + '}';
+    }
+}

e958cadf38ed68009e93983944293d77c91c8339
cdelgado <carlos.delgado@elastic.co>
2022-10-06 18:25:07 +0200

    Add curations integration testing

--- x-pack/plugin/relevance-search/src/test/java/org/elasticsearch/xpack/relevancesearch/query/RelevanceMatchQueryIntTests.java
+++ x-pack/plugin/relevance-search/src/test/java/org/elasticsearch/xpack/relevancesearch/query/RelevanceMatchQueryIntTests.java
@@ -106,29 +106,40 @@ public class RelevanceMatchQueryIntTests extends ESSingleNodeTestCase {
 
     public void testCuration() {
         final String curationId = "test-curation";
+        final String query = "hit";
+
         indexDocument(
             CurationsService.ENT_SEARCH_INDEX,
             CurationsService.CURATIONS_SETTINGS_PREFIX + curationId,
-            Map.of("fields", List.of("textField"))
+            Map.of(
+                "conditions",
+                List.of(Map.of("context", "query", "value", query)),
+                "pinned_document_ids",
+                List.of(Map.of("_id", "3", "_index", DOCUMENTS_INDEX)),
+                "excluded_document_ids",
+                List.of(Map.of("_id", "1", "_index", DOCUMENTS_INDEX))
+            )
         );
 
         indexDocument(
             DOCUMENTS_INDEX,
             "1",
-            Map.of("textField", "text example", "intField", 12, "doubleField", 13.45, "anotherTextField", "should match")
+            Map.of("textField", "text example hit")
         );
         indexDocument(
             DOCUMENTS_INDEX,
             "2",
-            Map.of("textField", "other document", "intField", 12, "doubleField", 13.45, "anotherTextField", "text example")
+            Map.of("textField", "text another example hit")
+        );
+        indexDocument(DOCUMENTS_INDEX, "3", Map.of("textField", "this should not be found")
         );
 
-        relevanceMatchQueryBuilder.setQuery("text example");
-        relevanceMatchQueryBuilder.setRelevanceSettingsId(curationId);
+        relevanceMatchQueryBuilder.setQuery(query);
+        relevanceMatchQueryBuilder.setCurationsSettingsId(curationId);
         SearchResponse response = client().prepareSearch(DOCUMENTS_INDEX).setQuery(relevanceMatchQueryBuilder).get();
 
-        assertHitCount(response, 1);
-        assertSearchHits(response, "1");
+        assertHitCount(response, 2);
+        assertSearchHits(response, "3", "2");
     }
 
     public void testFieldSettingsWithBoosts() {

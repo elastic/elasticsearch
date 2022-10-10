11fc6ce40c96642752ddbc1d6f320385aa330dfb
Ioana Tagirta <ioana.tagirta@elastic.co>
2022-10-10 13:45:27 +0200

    Make .ent-search hidden

--- x-pack/plugin/relevance-search/src/main/java/org/elasticsearch/xpack/relevancesearch/relevance/RelevanceSettingsService.java
+++ x-pack/plugin/relevance-search/src/main/java/org/elasticsearch/xpack/relevancesearch/relevance/RelevanceSettingsService.java
@@ -123,6 +123,7 @@ public class RelevanceSettingsService {
             .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, 1)
             .put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 0)
             .put(IndexMetadata.SETTING_AUTO_EXPAND_REPLICAS, "0-1")
+            .put(IndexMetadata.SETTING_INDEX_HIDDEN, true)
             .build();
     }
 

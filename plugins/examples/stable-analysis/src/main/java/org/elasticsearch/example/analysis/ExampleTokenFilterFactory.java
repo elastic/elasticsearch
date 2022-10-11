1576a3de533623eb6a229c399b1ae1386a2cefd4
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2022-10-11 14:18:27 +0200

    draft example analysis plugin

---
+++ plugins/examples/stable-analysis/src/main/java/org/elasticsearch/example/analysis/ExampleTokenFilterFactory.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.example.analysis;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.util.SuppressForbidden;
+import org.elasticsearch.example.analysis.lucene.AppendTokenFilter;
+import org.elasticsearch.example.analysis.lucene.Skip1TokenFilter;
+import org.elasticsearch.plugin.analysis.api.AnalysisMode;
+import org.elasticsearch.plugin.api.NamedComponent;
+
+@NamedComponent(name = "stableTokenFilterFactory")
+public class ExampleTokenFilterFactory implements org.elasticsearch.plugin.analysis.api.TokenFilterFactory {
+
+    @SuppressForbidden(reason = "need a public constructor")
+    public ExampleTokenFilterFactory() {
+    }
+
+    @Override
+    public TokenStream create(TokenStream tokenStream) {
+        return new Skip1TokenFilter(tokenStream);
+    }
+
+    @Override
+    public TokenStream normalize(TokenStream tokenStream) {
+        return new AppendTokenFilter(tokenStream, "1");
+    }
+
+    @Override
+    public AnalysisMode getAnalysisMode() {
+        return org.elasticsearch.plugin.analysis.api.TokenFilterFactory.super.getAnalysisMode();
+    }
+
+}
+

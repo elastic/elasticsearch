4afe8834facbe68c85e0d5819715970d468dea1f
Kathleen DeRusso <kathleen.derusso@elastic.co>
2022-10-13 17:03:53 -0400

    Hook in query rewriter

--- x-pack/plugin/relevance-search/src/main/java/org/elasticsearch/xpack/relevancesearch/xsearch/action/XSearchSearchTransportAction.java
+++ x-pack/plugin/relevance-search/src/main/java/org/elasticsearch/xpack/relevancesearch/xsearch/action/XSearchSearchTransportAction.java
@@ -15,30 +15,40 @@ import org.elasticsearch.action.support.HandledTransportAction;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.transport.TransportService;
+import org.elasticsearch.xpack.relevancesearch.query.RelevanceMatchQueryBuilder;
+import org.elasticsearch.xpack.relevancesearch.query.RelevanceMatchQueryRewriter;
 
 public class XSearchSearchTransportAction extends HandledTransportAction<XSearchSearchAction.Request, XSearchSearchAction.Response> {
 
     private static final Logger LOGGER = LogManager.getLogger(XSearchSearchTransportAction.class);
 
-    // private final RelevanceSettingsService relevanceSettingsService;
-    // private final RelevanceMatchQueryRewriter relevanceMatchQueryRewriter;
+    private final RelevanceMatchQueryRewriter relevanceMatchQueryRewriter;
 
     @Inject
     public XSearchSearchTransportAction(
         TransportService transportService,
         ActionFilters actionFilters,
-        String executor
-        // RelevanceSettingsService relevanceSettingsService,
-        // RelevanceMatchQueryRewriter relevanceMatchQueryRewriter
+        String executor,
+        RelevanceMatchQueryRewriter relevanceMatchQueryRewriter
     ) {
         super(XSearchSearchAction.NAME, false, transportService, actionFilters, XSearchSearchAction.Request::new, executor);
-        // this.relevanceSettingsService = relevanceSettingsService;
-        // this.relevanceMatchQueryRewriter = relevanceMatchQueryRewriter;
+        this.relevanceMatchQueryRewriter = relevanceMatchQueryRewriter;
     }
 
     @Override
     protected void doExecute(Task task, XSearchSearchAction.Request request, ActionListener<XSearchSearchAction.Response> listener) {
-        listener.onResponse(new XSearchSearchAction.Response("hello world"));
+
+        String relevanceSettingsId = "123";
+        String curationsSettingsId = "456";
+
+        RelevanceMatchQueryBuilder relevanceMatchQueryBuilder = new RelevanceMatchQueryBuilder(
+            relevanceMatchQueryRewriter,
+            request.getQuery(),
+            relevanceSettingsId,
+            curationsSettingsId
+        );
+
+        listener.onResponse(new XSearchSearchAction.Response(request.getQuery()));
         listener.onFailure(new UnsupportedOperationException("oh no!"));
     }
 }

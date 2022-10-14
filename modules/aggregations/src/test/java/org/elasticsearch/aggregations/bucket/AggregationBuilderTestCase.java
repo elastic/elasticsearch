fcca353882d3527759b87ea229ed8d8e59abf721
Martijn van Groningen <martijn.v.groningen@gmail.com>
2022-10-14 21:27:06 +0200

    Add test base classes for tests in aggregation module
    Otherwise, many tests that will be moved need to overwrite getPlugin(...) or nodePlugin(...)
    method and supply the AggregationsPlugin.class as plugin class.
    
    Added the following base classes:
    * AggregationIntegTestCase for aggregation java integ tests.
    * AggregationBuilderTestCase for testing aggregation builders.
    * AggregationMultiBucketAggregationTestCase for testing multi bucket response classes.
      This also simplifies supplying `ParsedAggregation` parsers a little bit.
    * AggregationTestCase for testing aggregators in this module.
    
    Relates to #90283

---
+++ modules/aggregations/src/test/java/org/elasticsearch/aggregations/bucket/AggregationBuilderTestCase.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.aggregations.bucket;
+
+import org.elasticsearch.aggregations.AggregationsPlugin;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.search.aggregations.AbstractAggregationBuilder;
+import org.elasticsearch.search.aggregations.BaseAggregationTestCase;
+
+import java.util.Collection;
+import java.util.List;
+
+public abstract class AggregationBuilderTestCase<AB extends AbstractAggregationBuilder<AB>> extends BaseAggregationTestCase<AB> {
+
+    @Override
+    protected Collection<Class<? extends Plugin>> getPlugins() {
+        return List.of(AggregationsPlugin.class);
+    }
+
+}

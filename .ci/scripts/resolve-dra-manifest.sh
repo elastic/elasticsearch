9328e4fa0f19bb6a2a9a6a6884832207f4b0a478
Rene Groeschke <rene@elastic.co>
2022-10-19 15:48:00 +0200

    Cleanup buildIds resolution script

--- .ci/scripts/resolve-dra-manifest.sh
+++ .ci/scripts/resolve-dra-manifest.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-#!/bin/bash
-
 set -e
 WORKSPACE="${WORKSPACE:-$PWD}"
 ARTIFACT="${ARTIFACT:-$1}"
 BRANCH='master'
 RELEASE_VERSION="8.6.0-SNAPSHOT"
 
-echo $WORKSPACE
 LOCAL_MANIFEST_FILE="$WORKSPACE/release-manifest-${ARTIFACT}.json"
-curl https://artifacts-snapshot.elastic.co/$ARTIFACT/latest/$BRANCH.json \
+curl -sS https://artifacts-snapshot.elastic.co/$ARTIFACT/latest/$BRANCH.json \
   | grep build_id \
   | sed "s/build_id/$ARTIFACT/g" \
   | sed 's/\"//g' \
   | sed 's/,//' \
   | sed 's/ : /=/' \
-  | xargs >> buildids.properties
+  | xargs

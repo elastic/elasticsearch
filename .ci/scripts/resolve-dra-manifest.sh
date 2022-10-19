743984a9eb68c4b018624524943ac416cf671424
Rene Groeschke <rene@elastic.co>
2022-10-19 15:05:46 +0200

    Simplify resolve dra buildid script

--- .ci/scripts/resolve-dra-manifest.sh
+++ .ci/scripts/resolve-dra-manifest.sh
@@ -9,6 +9,10 @@ RELEASE_VERSION="8.6.0-SNAPSHOT"
 
 echo $WORKSPACE
 LOCAL_MANIFEST_FILE="$WORKSPACE/release-manifest-${ARTIFACT}.json"
-wget https://artifacts-snapshot.elastic.co/$ARTIFACT/latest/$BRANCH.json -O $LOCAL_MANIFEST_FILE
-
-cat $LOCAL_MANIFEST_FILE | grep build_id | sed "s/build_id/$ARTIFACT/g" | sed 's/\"//g' | sed 's/,//'| sed 's/ : /=/' | xargs >> deps.properties
+curl https://artifacts-snapshot.elastic.co/$ARTIFACT/latest/$BRANCH.json \
+  | grep build_id \
+  | sed "s/build_id/$ARTIFACT/g" \
+  | sed 's/\"//g' \
+  | sed 's/,//' \
+  | sed 's/ : /=/' \
+  | xargs >> buildids.properties

fa396cbac68d31df24c34df0c3390f37d0db7c72
Nikola Grcevski <nikola.grcevski@elastic.co>
2022-10-20 16:44:54 -0400

    Fix APM configuration file delete
    When we launch Elasticsearch with the APM monitoring
    agent, we create a temporary configuration file to
    securely pass the API key or secret. This temporary
    file is cleaned up on Elasticsearch Node creation.
    
    After we renamed the APM module, the delete logic
    didn't get updated, which means we never delete the file
    anymore.
    
    This commit:
     - fixes the APM module pattern match when we delete
     - adds additional delete safety net on failed node start
     - adds tests for ensuring the naming dependency isn't
       broken again.

---
+++ distribution/tools/server-cli/src/test/java/org/elasticsearch/server/cli/APMJvmOptionsTests.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.server.cli;
+
+import org.elasticsearch.cli.UserException;
+import org.elasticsearch.monitor.jvm.JvmInfo;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.test.ESTestCase;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+@ESTestCase.WithoutSecurityManager
+public class APMJvmOptionsTests extends ESTestCase {
+
+    public void testFindJar() throws IOException, UserException {
+        Path installDir = makeFakeAgentJar();
+        var agentPath = APMJvmOptions.findAgentJar(installDir.toAbsolutePath().toString());
+        assertNotNull(agentPath);
+
+        Path anotherPath = Files.createDirectories(installDir.resolve("another"));
+        Path apmPathDir = anotherPath.resolve("modules").resolve("apm");
+        Files.createDirectories(apmPathDir);
+
+        assertTrue(
+            expectThrows(UserException.class, () -> APMJvmOptions.findAgentJar(anotherPath.toAbsolutePath().toString())).getMessage()
+                .contains("Installation is corrupt")
+        );
+    }
+
+    public void testFileDeleteWorks() throws IOException, UserException {
+        var agentPath = APMJvmOptions.findAgentJar(makeFakeAgentJar().toAbsolutePath().toString());
+        var tempFile = APMJvmOptions.createTemporaryPropertiesFile(agentPath.getParent());
+        var commandLineOption = APMJvmOptions.agentCommandLineOption(agentPath, tempFile);
+        var jvmInfo = mock(JvmInfo.class);
+        doReturn(new String[] { commandLineOption }).when(jvmInfo).getInputArguments();
+        assertTrue(Files.exists(tempFile));
+        Node.deleteTemporaryApmConfig(jvmInfo, (e, p) -> fail("Shouldn't hit an exception"));
+        assertFalse(Files.exists(tempFile));
+    }
+
+    private Path makeFakeAgentJar() throws IOException {
+        Path tempFile = createTempFile();
+        Path apmPathDir = tempFile.getParent().resolve("modules").resolve("apm");
+        Files.createDirectories(apmPathDir);
+        Path apmAgentFile = apmPathDir.resolve("elastic-apm-agent-0.0.0.jar");
+        Files.move(tempFile, apmAgentFile);
+
+        return tempFile.getParent();
+    }
+}

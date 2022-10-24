95cfcd005555dffca307f599a1fe05bff0cb2f18
Jake Landis <jake.landis@elastic.co>
2022-10-24 15:40:34 -0500

    spotless

--- test/framework/src/main/java/org/elasticsearch/test/ESTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/ESTestCase.java
@@ -1697,7 +1697,7 @@ public abstract class ESTestCase extends LuceneTestCase {
     }
 
     public static boolean inFipsJvm() {
-       // return Boolean.parseBoolean(System.getProperty(FIPS_SYSPROP));
+        // return Boolean.parseBoolean(System.getProperty(FIPS_SYSPROP));
         return true;// TODO: DO NOT COMMIT THIS !!
     }
 

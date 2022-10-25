e968e52c6a1cffdcbbdde29c6a6c4e075a184058
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2022-10-25 17:29:02 +0200

    Disable sec to fix test

--- qa/os/src/test/java/org/elasticsearch/packaging/test/PackageUpgradeTests.java
+++ qa/os/src/test/java/org/elasticsearch/packaging/test/PackageUpgradeTests.java
@@ -106,6 +106,9 @@ public class PackageUpgradeTests extends PackagingTestCase {
         assertInstalled(distribution);
         verifyPackageInstallation(installation, distribution, sh);
         verifySecurityNotAutoConfigured(installation);
+        
+        // Upgrade overwrites the configuration file because we run with --force-confnew so we need to disable security again
+        possiblyRemoveSecurityConfiguration(installation);
     }
 
     public void test21CheckUpgradedVersion() throws Exception {

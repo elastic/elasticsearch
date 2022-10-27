2333cdc2288f35ebac7f6551ca258e1cb4db4e74
Hendrik Muhs <hendrik.muhs@elastic.co>
2022-10-27 14:08:35 +0200

    remove outdated test

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/transform/transforms/TransformTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/transform/transforms/TransformTests.java
@@ -8,16 +8,12 @@
 package org.elasticsearch.xpack.core.transform.transforms;
 
 import org.elasticsearch.Version;
-import org.elasticsearch.common.io.stream.BytesStreamOutput;
-import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.Writeable.Reader;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.xcontent.XContentParser;
 import org.elasticsearch.xpack.core.transform.AbstractSerializingTransformTestCase;
 
 import java.io.IOException;
-
-import static org.hamcrest.Matchers.equalTo;
 
 public class TransformTests extends AbstractSerializingTransformTestCase<TransformTaskParams> {
 
@@ -39,22 +35,5 @@ public class TransformTests extends AbstractSerializingTransformTestCase<Transfo
     @Override
     protected Reader<TransformTaskParams> instanceReader() {
         return TransformTaskParams::new;
-    }
-
-    public void testBackwardsSerialization() throws IOException {
-        for (int i = 0; i < NUMBER_OF_TEST_RUNS; i++) {
-            TransformTaskParams transformTask = createTestInstance();
-            try (BytesStreamOutput output = new BytesStreamOutput()) {
-                output.setVersion(Version.V_7_2_0);
-                transformTask.writeTo(output);
-                try (StreamInput in = output.bytes().streamInput()) {
-                    in.setVersion(Version.V_7_2_0);
-                    // Since the old version does not have the version serialized, the version NOW is 7.2.0
-                    TransformTaskParams streamedTask = new TransformTaskParams(in);
-                    assertThat(streamedTask.getVersion(), equalTo(Version.V_7_2_0));
-                    assertThat(streamedTask.getId(), equalTo(transformTask.getId()));
-                }
-            }
-        }
     }
 }

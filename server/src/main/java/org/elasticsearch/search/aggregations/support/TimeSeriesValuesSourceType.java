34a0093928765bb78a520b18b364550fde48e768
Alan Woodward <romseygeek@apache.org>
2022-10-31 20:42:56 +0000

    Add a new values source type returned by metric counters (#90680)
    This commit adds a new values source type to identify counter fields in
    TSDB indices. Because aggregate values over this field only make sense
    in specific time series contexts, this should prevent users trying to issue
    nonsensical aggregations (eg, sum or avg) over counters.

---
+++ server/src/main/java/org/elasticsearch/search/aggregations/support/TimeSeriesValuesSourceType.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.search.aggregations.support;
+
+import org.elasticsearch.index.fielddata.IndexNumericFieldData;
+import org.elasticsearch.script.AggregationScript;
+import org.elasticsearch.search.DocValueFormat;
+
+import java.util.Locale;
+
+/**
+ * Holds {@link ValuesSourceType} implementations for time series fields
+ */
+public enum TimeSeriesValuesSourceType implements ValuesSourceType {
+
+    COUNTER {
+        @Override
+        public ValuesSource getEmpty() {
+            throw new IllegalArgumentException("Cannot use unmapped counter field");
+        }
+
+        @Override
+        public ValuesSource getScript(AggregationScript.LeafFactory script, ValueType scriptValueType) {
+            throw new IllegalArgumentException("Cannot use scripts for time-series counters");
+        }
+
+        @Override
+        public ValuesSource getField(FieldContext fieldContext, AggregationScript.LeafFactory script, AggregationContext context) {
+            if (script != null) {
+                throw new IllegalArgumentException("Cannot use scripts for time-series counters");
+            }
+            if (fieldContext.indexFieldData()instanceof IndexNumericFieldData fieldData) {
+                return new ValuesSource.Numeric.FieldData(fieldData);
+            }
+            throw new IllegalArgumentException(
+                "Expected numeric type on field [" + fieldContext.field() + "], but got [" + fieldContext.fieldType().typeName() + "]"
+            );
+        }
+
+        @Override
+        public ValuesSource replaceMissing(
+            ValuesSource valuesSource,
+            Object rawMissing,
+            DocValueFormat docValueFormat,
+            AggregationContext context
+        ) {
+            throw new IllegalArgumentException("Cannot replace missing values for time-series counters");
+        }
+    };
+
+    public static ValuesSourceType fromString(String name) {
+        return valueOf(name.trim().toUpperCase(Locale.ROOT));
+    }
+
+    public String value() {
+        return name().toLowerCase(Locale.ROOT);
+    }
+
+    @Override
+    public String typeName() {
+        return value();
+    }
+}

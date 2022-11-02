5602f5c2061106f763f7cd921e68850dc963c975
David Turner <david.turner@elastic.co>
2022-11-02 13:24:41 +0000

    s/a/this/

--- server/src/main/java/org/elasticsearch/env/NodeEnvironment.java
+++ server/src/main/java/org/elasticsearch/env/NodeEnvironment.java
@@ -519,7 +519,7 @@ public final class NodeEnvironment implements Closeable {
             throw new IllegalStateException(
                 "Cannot start this node because it holds metadata for indices created with version ["
                     + metadata.oldestIndexVersion()
-                    + "] with which a node of version ["
+                    + "] with which this node of version ["
                     + Version.CURRENT
                     + "] is incompatible. Revert this node to version ["
                     + Version.max(Version.CURRENT.minimumCompatibilityVersion(), metadata.previousNodeVersion())

9b6b6273ee6e1db23bd987aefcabfe18713687bc
Rene Groeschke <rene@elastic.co>
2022-11-02 23:54:16 +0100

    apply review feedback

--- .ci/scripts/resolve-dra-manifest.sh
+++ .ci/scripts/resolve-dra-manifest.sh
@@ -4,11 +4,7 @@ ARTIFACT="${ARTIFACT:-$1}"
 BRANCH="${BRANCH:-$2}"
 
 curl -sS https://artifacts-snapshot.elastic.co/$ARTIFACT/latest/$BRANCH.json \
-  | grep build_id \
-  | sed "s/build_id/$ARTIFACT/g" \
-  | sed 's/\"//g' \
-  | sed 's/,//' \
-  | sed 's/ : /=/' \
+  | jq -r '.build_id' \
   | xargs
 
 # https://artifacts-snapshot.elastic.co/beats/0.1.0-283a66b2/manifest-0.1.0-SNAPSHOT.json

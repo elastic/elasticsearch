1f7492c412227f79f39701627efc430de9b9c908
Nhat Nguyen <nhat.nguyen@elastic.co>
2022-11-07 01:56:21 -0800

    Remove old compute actions (ESQL-359)
    This PR removes old endpoints and planner as we no longer use them. I
    removed the old planner's tests. I think we should write new tests for
    the new planner.
    
    Closes ESQL-290

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/EsqlPlugin.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/EsqlPlugin.java
@@ -33,11 +33,7 @@ import org.elasticsearch.watcher.ResourceWatcherService;
 import org.elasticsearch.xcontent.NamedXContentRegistry;
 import org.elasticsearch.xpack.esql.action.EsqlQueryAction;
 import org.elasticsearch.xpack.esql.action.RestEsqlQueryAction;
-import org.elasticsearch.xpack.esql.compute.transport.ComputeAction;
-import org.elasticsearch.xpack.esql.compute.transport.RestComputeAction;
-import org.elasticsearch.xpack.esql.compute.transport.TransportComputeAction;
 import org.elasticsearch.xpack.esql.execution.PlanExecutor;
-import org.elasticsearch.xpack.esql.plan.physical.old.PlanNode;
 import org.elasticsearch.xpack.ql.index.IndexResolver;
 import org.elasticsearch.xpack.ql.type.DefaultDataTypeRegistry;
 
@@ -87,10 +83,7 @@ public class EsqlPlugin extends Plugin implements ActionPlugin {
 
     @Override
     public List<ActionHandler<? extends ActionRequest, ? extends ActionResponse>> getActions() {
-        return Arrays.asList(
-            new ActionHandler<>(EsqlQueryAction.INSTANCE, TransportEsqlQueryAction.class),
-            new ActionHandler<>(ComputeAction.INSTANCE, TransportComputeAction.class)
-        );
+        return List.of(new ActionHandler<>(EsqlQueryAction.INSTANCE, TransportEsqlQueryAction.class));
     }
 
     @Override
@@ -103,11 +96,6 @@ public class EsqlPlugin extends Plugin implements ActionPlugin {
         IndexNameExpressionResolver indexNameExpressionResolver,
         Supplier<DiscoveryNodes> nodesInCluster
     ) {
-        return List.of(new RestEsqlQueryAction(), new RestComputeAction());
-    }
-
-    @Override
-    public List<NamedXContentRegistry.Entry> getNamedXContent() {
-        return PlanNode.getNamedXContentParsers();
+        return List.of(new RestEsqlQueryAction());
     }
 }

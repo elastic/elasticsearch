834b7952b2510a3b7c8addad6e482a11817a34a5
Iraklis Psaroudakis <kingherc@gmail.com>
2022-11-07 14:08:41 +0200

    Add comment

--- server/src/test/java/org/elasticsearch/cluster/coordination/CoordinatorTests.java
+++ server/src/test/java/org/elasticsearch/cluster/coordination/CoordinatorTests.java
@@ -1917,6 +1917,7 @@ public class CoordinatorTests extends AbstractCoordinatorTestCase {
             .put(DiscoveryModule.DISCOVERY_TYPE_SETTING.getKey(), DiscoveryModule.SINGLE_NODE_DISCOVERY_TYPE);
 
         // If the delay variability is high, set election duration accordingly, to avoid the possible endless repetition of voting rounds.
+        // Note that elections could take even longer than the delay variability, but this seems to be long enough to avoid bad collisions.
         if (ElectionSchedulerFactory.ELECTION_DURATION_SETTING.getDefault(Settings.EMPTY).getMillis() < delayVariabilityMillis) {
             settings = settings.put(
                 ElectionSchedulerFactory.ELECTION_DURATION_SETTING.getKey(),

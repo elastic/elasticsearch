8e3cfb8d4706a2e05e92a6b5776d287a1030c76e
Ievgen Degtiarenko <ievgen.degtiarenko@elastic.co>
2022-11-17 10:31:40 +0100

    New balancer unit tests (#91612)
    (cherry picked from commit 7c39880365f518a7d231a9856c71b5f31e91a79b)

--- server/src/test/java/org/elasticsearch/cluster/routing/allocation/allocator/BalancedShardsAllocatorTests.java
+++ server/src/test/java/org/elasticsearch/cluster/routing/allocation/allocator/BalancedShardsAllocatorTests.java
@@ -15,6 +15,7 @@ import org.elasticsearch.cluster.ClusterInfo;
 import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.ESAllocationTestCase;
+import org.elasticsearch.cluster.EmptyClusterInfoService;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.node.DiscoveryNode;
@@ -22,6 +23,7 @@ import org.elasticsearch.cluster.node.DiscoveryNodeRole;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.cluster.routing.AllocationId;
 import org.elasticsearch.cluster.routing.IndexRoutingTable;
+import org.elasticsearch.cluster.routing.RoutingNode;
 import org.elasticsearch.cluster.routing.RoutingNodes;
 import org.elasticsearch.cluster.routing.RoutingNodesHelper;
 import org.elasticsearch.cluster.routing.RoutingTable;
@@ -30,10 +32,14 @@ import org.elasticsearch.cluster.routing.ShardRoutingState;
 import org.elasticsearch.cluster.routing.TestShardRouting;
 import org.elasticsearch.cluster.routing.allocation.AllocateUnassignedDecision;
 import org.elasticsearch.cluster.routing.allocation.RoutingAllocation;
+import org.elasticsearch.cluster.routing.allocation.WriteLoadForecaster;
 import org.elasticsearch.cluster.routing.allocation.decider.AllocationDeciders;
 import org.elasticsearch.cluster.routing.allocation.decider.ThrottlingAllocationDecider;
 import org.elasticsearch.common.UUIDs;
+import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.core.SuppressForbidden;
 import org.elasticsearch.core.Tuple;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.snapshots.SnapshotShardSizeInfo;
@@ -43,10 +49,16 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.OptionalDouble;
 import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.elasticsearch.cluster.routing.ShardRoutingState.RELOCATING;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 public class BalancedShardsAllocatorTests extends ESAllocationTestCase {
 
@@ -64,14 +76,7 @@ public class BalancedShardsAllocatorTests extends ESAllocationTestCase {
         clusterState = ClusterState.builder(clusterState).metadata(metadata).routingTable(initialRoutingTable).build();
 
         ShardRouting shard = clusterState.routingTable().index("idx_new").shard(0).primaryShard();
-        RoutingAllocation allocation = new RoutingAllocation(
-            new AllocationDeciders(Collections.emptyList()),
-            RoutingNodes.mutable(clusterState.routingTable(), clusterState.nodes()),
-            clusterState,
-            ClusterInfo.EMPTY,
-            SnapshotShardSizeInfo.EMPTY,
-            System.nanoTime()
-        );
+        RoutingAllocation allocation = createRoutingAllocation(clusterState);
 
         allocation.debugDecision(false);
         AllocateUnassignedDecision allocateDecision = allocator.decideShardAllocation(shard, allocation).getAllocateDecision();
@@ -85,6 +90,99 @@ public class BalancedShardsAllocatorTests extends ESAllocationTestCase {
         assertEquals(1, assignedShards.size());
         // the allocation result be consistent with allocation decision
         assertNotNull(allocateDecision.getTargetNode().getId(), assignedShards.get(0).currentNodeId());
+    }
+
+    public void testBalanceByShardLoad() {
+
+        var smallIndices = IntStream.range(1, 5)
+            .mapToObj(i -> IndexMetadata.builder("small-index-" + i))
+            .map(builder -> builder.settings(settings(Version.CURRENT)).numberOfShards(1).numberOfReplicas(0))
+            .map(builder -> builder.indexWriteLoadForecast(randomIngestLoad(1.5)));
+
+        var heavyIndex = IndexMetadata.builder("heavy-index")
+            .settings(settings(Version.CURRENT))
+            .numberOfShards(1)
+            .numberOfReplicas(0)
+            .indexWriteLoadForecast(8.0);
+
+        var clusterState = stateWithStartedIndices(Stream.concat(smallIndices, Stream.of(heavyIndex)).toList());
+
+        var testWriteLoadForecaster = new WriteLoadForecaster() {
+            @Override
+            public Metadata.Builder withWriteLoadForecastForWriteIndex(String dataStreamName, Metadata.Builder metadata) {
+                throw new UnsupportedOperationException("Not required for test");
+            }
+
+            @Override
+            @SuppressForbidden(reason = "This is required to test balancing by ingest load")
+            public OptionalDouble getForecastedWriteLoad(IndexMetadata indexMetadata) {
+                return indexMetadata.getForecastedWriteLoad();
+            }
+        };
+
+        var settings = Settings.EMPTY;
+        var allocator = new BalancedShardsAllocator(
+            settings,
+            new ClusterSettings(settings, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS),
+            testWriteLoadForecaster,
+            EmptyClusterInfoService.INSTANCE
+        );
+        var allocation = createRoutingAllocation(clusterState);
+        allocator.allocate(allocation);
+
+        assertThat(allocation.metadata().getTotalNumberOfShards(), allOf(greaterThanOrEqualTo(3), lessThanOrEqualTo(5)));
+        for (RoutingNode routingNode : allocation.routingNodes()) {
+            var nodeIngestLoad = 0.0;
+            for (ShardRouting shardRouting : routingNode) {
+                if (shardRouting.started() || shardRouting.initializing()) { // count load from the target node when relocating
+                    var indexMetadata = clusterState.metadata().index(shardRouting.index());
+                    nodeIngestLoad += testWriteLoadForecaster.getForecastedWriteLoad(indexMetadata).orElse(0.0);
+                }
+            }
+            assertThat(nodeIngestLoad, lessThanOrEqualTo(8.0 + 1.5));
+        }
+    }
+
+    private Double randomIngestLoad(double max) {
+        return switch (randomInt(3)) {
+            case 0 -> null;
+            case 1 -> 0.0;
+            default -> randomDoubleBetween(1.0, max, true);
+        };
+    }
+
+    public void testBalanceByDiskUsage() {
+
+        var smallIndices = IntStream.range(1, 5)
+            .mapToObj(i -> IndexMetadata.builder("small-index-" + i))
+            .map(builder -> builder.settings(settings(Version.CURRENT)).numberOfShards(1).numberOfReplicas(0))
+            .map(builder -> builder.shardSizeInBytesForecast(ByteSizeValue.ofGb(1).getBytes()));
+
+        var heavyIndex = IndexMetadata.builder("heavy-index")
+            .settings(settings(Version.CURRENT))
+            .numberOfShards(1)
+            .numberOfReplicas(0)
+            .shardSizeInBytesForecast(ByteSizeValue.ofGb(8).getBytes());
+
+        var clusterState = stateWithStartedIndices(Stream.concat(smallIndices, Stream.of(heavyIndex)).toList());
+
+        var allocator = new BalancedShardsAllocator(
+            Settings.builder().put("cluster.routing.allocation.balance.disk_usage", "1e-9").build()
+        );
+        var allocation = createRoutingAllocation(clusterState);
+        allocator.allocate(allocation);
+
+        assertThat(allocation.metadata().getTotalNumberOfShards(), allOf(greaterThanOrEqualTo(3), lessThanOrEqualTo(5)));
+        for (RoutingNode routingNode : allocation.routingNodes()) {
+            var nodeDiskUsage = 0L;
+            for (ShardRouting shardRouting : routingNode) {
+                if (shardRouting.started() || shardRouting.initializing()) { // count load from the target node when relocating
+                    var indexMetadata = clusterState.metadata().index(shardRouting.index());
+                    nodeDiskUsage += indexMetadata.getForecastedShardSizeInBytes().orElse(0L);
+                }
+            }
+            assertThat(nodeDiskUsage, lessThanOrEqualTo(ByteSizeValue.ofGb(8 + 1).getBytes()));
+        }
     }
 
     /**
@@ -149,7 +247,47 @@ public class BalancedShardsAllocatorTests extends ESAllocationTestCase {
         return counts;
     }
 
-    private DiscoveryNode createNode(String nodeId) {
+    private RoutingAllocation createRoutingAllocation(ClusterState clusterState) {
+        return new RoutingAllocation(
+            new AllocationDeciders(List.of()),
+            RoutingNodes.mutable(clusterState.routingTable(), clusterState.nodes()),
+            clusterState,
+            ClusterInfo.EMPTY,
+            SnapshotShardSizeInfo.EMPTY,
+            System.nanoTime()
+        );
+    }
+
+    private static ClusterState stateWithStartedIndices(List<IndexMetadata.Builder> indices) {
+        var metadataBuilder = Metadata.builder();
+        var routingTableBuilder = RoutingTable.builder();
+        for (var index : indices) {
+            var inSyncId = UUIDs.randomBase64UUID(random());
+            var build = index.putInSyncAllocationIds(0, Set.of(inSyncId)).build();
+            metadataBuilder.put(build, false);
+            routingTableBuilder.add(
+                IndexRoutingTable.builder(build.getIndex())
+                    .addShard(
+                        TestShardRouting.newShardRouting(
+                            new ShardId(build.getIndex(), 0),
+                            randomFrom("node-1", "node-2"),
+                            null,
+                            true,
+                            ShardRoutingState.STARTED,
+                            AllocationId.newInitializing(inSyncId)
+                        )
+                    )
+            );
+        }
+
+        return ClusterState.builder(ClusterName.DEFAULT)
+            .nodes(DiscoveryNodes.builder().add(createNode("node-1")).add(createNode("node-2")))
+            .metadata(metadataBuilder)
+            .routingTable(routingTableBuilder)
+            .build();
+    }
+
+    private static DiscoveryNode createNode(String nodeId) {
         return new DiscoveryNode(
             nodeId,
             nodeId,

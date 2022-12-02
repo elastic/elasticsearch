4c0ffa8b0a104bb8b0697b8baa0173e5415d2203
Armin Braun <me@obrown.io>
2022-12-02 12:17:18 +0100

    Fork Reponse Handler on non-master node in TransportMasterNodeAction
    This makes it so we fork to hte executor specified for the transport action
    in transport master node action. This has two advantages:
    1. Responses that might be large will be deserialized off the transport threads (mostly relevant for cluster state requests as far as I can see)
    2. Transport master node action handling happens on the same thread, regardless of whether or not the executing node is the current master
    or if the action was actually forwarded via a transport message.

--- server/src/main/java/org/elasticsearch/action/support/master/TransportMasterNodeAction.java
+++ server/src/main/java/org/elasticsearch/action/support/master/TransportMasterNodeAction.java
@@ -245,7 +245,7 @@ public abstract class TransportMasterNodeAction<Request extends MasterNodeReques
                             masterNode,
                             actionName,
                             request,
-                            new ActionListenerResponseHandler<Response>(listener, responseReader) {
+                            new ActionListenerResponseHandler<>(listener, responseReader) {
                                 @Override
                                 public void handleException(final TransportException exp) {
                                     Throwable cause = exp.unwrapCause();
@@ -267,6 +267,11 @@ public abstract class TransportMasterNodeAction<Request extends MasterNodeReques
                                         );
                                         listener.onFailure(exp);
                                     }
+                                }
+
+                                @Override
+                                public String executor() {
+                                    return executor;
                                 }
                             }
                         );

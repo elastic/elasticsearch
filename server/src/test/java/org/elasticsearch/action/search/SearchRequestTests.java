f52928068c2103e94de9b13ba630df0ac6d5294a
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2022-12-06 07:41:54 -0500

    formatting fix

--- server/src/test/java/org/elasticsearch/action/search/SearchRequestTests.java
+++ server/src/test/java/org/elasticsearch/action/search/SearchRequestTests.java
@@ -14,7 +14,6 @@ import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.common.recycler.Recycler;
 import org.elasticsearch.common.util.ArrayUtils;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -31,7 +30,6 @@ import org.elasticsearch.test.VersionUtils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import static java.util.Collections.emptyMap;
 import static org.elasticsearch.test.EqualsHashCodeTestUtils.checkEqualsAndHashCode;
@@ -131,9 +129,7 @@ public class SearchRequestTests extends AbstractSearchTestCase {
             // Versions before 8.4.0 don't support force_synthetic_source
             searchRequest.setForceSyntheticSource(false);
         }
-        if (version.before(Version.V_8_7_0)
-            && searchRequest.hasKnnSearch()
-            && searchRequest.source().knnSearch().size() > 1) {
+        if (version.before(Version.V_8_7_0) && searchRequest.hasKnnSearch() && searchRequest.source().knnSearch().size() > 1) {
             // Versions before 8.7.0 don't support more than one KNN clause
             searchRequest.source().knnSearch(List.of(searchRequest.source().knnSearch().get(0)));
         }

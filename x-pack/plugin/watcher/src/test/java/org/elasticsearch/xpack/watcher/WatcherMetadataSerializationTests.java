59fcccfd961936e150c911193af682090b834094
Armin Braun <me@obrown.io>
2022-12-07 10:20:23 +0100

    Chunked XContent encoding for Metadata (#92169)
    Mostly straight-forward chunked encoding implementation for Metadata.

--- x-pack/plugin/watcher/src/test/java/org/elasticsearch/xpack/watcher/WatcherMetadataSerializationTests.java
+++ x-pack/plugin/watcher/src/test/java/org/elasticsearch/xpack/watcher/WatcherMetadataSerializationTests.java
@@ -64,7 +64,7 @@ public class WatcherMetadataSerializationTests extends ESTestCase {
             Collections.singletonMap(Metadata.CONTEXT_MODE_PARAM, Metadata.CONTEXT_MODE_GATEWAY)
         );
         builder.startObject();
-        builder = metadataBuilder.build().toXContent(builder, params);
+        builder = ChunkedToXContent.wrapAsXContentObject(metadataBuilder.build()).toXContent(builder, params);
         builder.endObject();
         // deserialize metadata again
         Metadata metadata = Metadata.Builder.fromXContent(createParser(builder));

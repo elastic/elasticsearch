eb2803ba149526bf34a8c7b2cd7bfc3979290145
Artem Prigoda <artem.prigoda@elastic.co>
2022-12-09 01:12:48 +0100

    Use Apache HTTP client for testing

--- modules/repository-gcs/src/test/java/org/elasticsearch/repositories/gcs/MockHttpProxyServerTest.java
+++ modules/repository-gcs/src/test/java/org/elasticsearch/repositories/gcs/MockHttpProxyServerTest.java
@@ -1,39 +1,36 @@
 package org.elasticsearch.repositories.gcs;
 
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.test.ESTestCase;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 public class MockHttpProxyServerTest extends ESTestCase {
 
     public void testProxyServerWorks() throws Exception {
         String httpBody = randomAlphaOfLength(32);
         var proxyServer = new MockHttpProxyServer((reader, writer) -> {
-            assertEquals("GET http://googleapis.com HTTP/1.1", reader.readLine());
+            assertEquals("GET http://googleapis.com/ HTTP/1.1", reader.readLine());
             writer.write(formatted("""
                 HTTP/1.1 200 OK\r
                 Content-Length: %s\r
                 \r
                 %s""", httpBody.length(), httpBody));
         }).await();
-        try (proxyServer) {
-            var urlConnection = SocketAccess.doPrivilegedIOException(() -> {
-                var connection = new URL("http://googleapis.com").openConnection(
-                    new Proxy(Proxy.Type.HTTP, new InetSocketAddress(InetAddress.getLoopbackAddress(), proxyServer.getPort()))
-                );
-                connection.connect();
-                return connection;
-            });
-            assertEquals(httpBody.length(), urlConnection.getContentLengthLong());
-            try (var bodyReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8))) {
-                assertEquals(httpBody, bodyReader.readLine());
-            }
+        var httpClient = HttpClients.custom()
+            .setRoutePlanner(new DefaultProxyRoutePlanner(new HttpHost(InetAddress.getLoopbackAddress(), proxyServer.getPort())))
+            .build();
+        try (
+            proxyServer;
+            httpClient;
+            var httpResponse = SocketAccess.doPrivilegedIOException(() -> httpClient.execute(new HttpGet("http://googleapis.com/")))
+        ) {
+            assertEquals(httpBody.length(), httpResponse.getEntity().getContentLength());
+            assertEquals(httpBody, EntityUtils.toString(httpResponse.getEntity()));
         }
     }
 }

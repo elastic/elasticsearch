2b86a906a0cd7c0fbe1c8b4f6d1f7bd5d4a7928a
Salvatore Campagna <93581129+salvatore-campagna@users.noreply.github.com>
2022-12-12 14:51:23 +0100

    fix: move test to existing class

--- server/src/test/java/org/elasticsearch/index/codec/tsdb/DocValuesForUtilTests.java
+++ server/src/test/java/org/elasticsearch/index/codec/tsdb/DocValuesForUtilTests.java
@@ -10,7 +10,11 @@ package org.elasticsearch.index.codec.tsdb;
 
 import com.carrotsearch.randomizedtesting.generators.RandomNumbers;
 
+import org.apache.lucene.store.ByteArrayDataInput;
+import org.apache.lucene.store.ByteArrayDataOutput;
 import org.apache.lucene.store.ByteBuffersDirectory;
+import org.apache.lucene.store.DataInput;
+import org.apache.lucene.store.DataOutput;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.IOContext;
 import org.apache.lucene.store.IndexInput;
@@ -22,9 +26,11 @@ import org.apache.lucene.util.packed.PackedInts;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Random;
 
 public class DocValuesForUtilTests extends LuceneTestCase {
     private final ES87TSDBDocValuesEncoder encoder = new ES87TSDBDocValuesEncoder();
+    private static final ForUtil forUtil = new ForUtil();
 
     public void testEncodeDecode() throws IOException {
         final int iterations = RandomNumbers.randomIntBetween(random(), 50, 1000);
@@ -80,5 +86,40 @@ public class DocValuesForUtilTests extends LuceneTestCase {
         }
 
         d.close();
+    }
+
+    public void testEncodeDecodeBitsPerValue() throws IOException {
+        int size = 128;
+        byte[] dataOutputBuffer = new byte[16 * 1024];
+        byte[] dataInputBuffer = new byte[16 * 1024];
+        long[] decodeBuffer = new long[size];
+
+        for (int bitsPerValue = 1; bitsPerValue < 32; bitsPerValue++) {
+            long[] values = randomValues(bitsPerValue, size);
+
+            // Encode
+            DataOutput dataOutput = new ByteArrayDataOutput(dataOutputBuffer);
+            long[] encodeBuffer = Arrays.copyOf(values, values.length);
+            forUtil.encode(encodeBuffer, bitsPerValue, dataOutput);
+
+            // Prepare for decoding
+            DataInput dataInput = new ByteArrayDataInput(dataInputBuffer);
+            System.arraycopy(dataOutputBuffer, 0, dataInputBuffer, 0, dataOutputBuffer.length);
+
+            // Decode
+            forUtil.decode(bitsPerValue, dataInput, decodeBuffer);
+
+            assertArrayEquals(decodeBuffer, values);
+        }
+    }
+
+    private long[] randomValues(int bitsPerValue, int size) {
+        final Random random = new Random(0);
+        long[] values = new long[size];
+        long upperBound = 1L << bitsPerValue;
+        for (int i = 0; i < size; i++) {
+            values[i] = random.nextLong(upperBound);
+        }
+        return values;
     }
 }

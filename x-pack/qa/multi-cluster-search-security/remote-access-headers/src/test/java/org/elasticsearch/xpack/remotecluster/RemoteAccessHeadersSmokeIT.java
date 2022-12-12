ff96c1bfc521c1efa00a4ec8c26800df18b589da
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2022-12-12 12:42:44 +0100

    WIP IT

--- x-pack/qa/multi-cluster-search-security/remote-access-headers/src/test/java/org/elasticsearch/xpack/remotecluster/RemoteAccessHeadersSmokeIT.java
+++ x-pack/qa/multi-cluster-search-security/remote-access-headers/src/test/java/org/elasticsearch/xpack/remotecluster/RemoteAccessHeadersSmokeIT.java
@@ -10,15 +10,38 @@ package org.elasticsearch.xpack.remotecluster;
 import org.apache.http.HttpEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.nio.entity.NStringEntity;
+import org.apache.lucene.search.TotalHits;
+import org.elasticsearch.Version;
+import org.elasticsearch.action.admin.cluster.shards.ClusterSearchShardsAction;
+import org.elasticsearch.action.admin.cluster.shards.ClusterSearchShardsGroup;
+import org.elasticsearch.action.admin.cluster.shards.ClusterSearchShardsRequest;
+import org.elasticsearch.action.admin.cluster.shards.ClusterSearchShardsResponse;
+import org.elasticsearch.action.admin.cluster.state.ClusterStateAction;
+import org.elasticsearch.action.admin.cluster.state.ClusterStateRequest;
+import org.elasticsearch.action.admin.cluster.state.ClusterStateResponse;
+import org.elasticsearch.action.search.SearchAction;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.ShardSearchFailure;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.ResponseException;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.InternalAggregations;
+import org.elasticsearch.search.internal.InternalSearchResponse;
 import org.elasticsearch.test.rest.ESRestTestCase;
-import org.elasticsearch.test.rest.ObjectPath;
+import org.elasticsearch.test.transport.MockTransportService;
+import org.elasticsearch.threadpool.TestThreadPool;
+import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TcpTransport;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.json.JsonXContent;
@@ -26,20 +49,24 @@ import org.junit.BeforeClass;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 
 /**
  * This test suite will be run twice: Once against the fulfilling cluster, then again against the querying cluster.
  */
 public class RemoteAccessHeadersSmokeIT extends ESRestTestCase {
-
     @BeforeClass
     public static void checkFeatureFlag() {
         assumeTrue("untrusted remote cluster feature flag must be enabled", TcpTransport.isUntrustedRemoteClusterEnabled());
     }
 
+    private final ThreadPool threadPool = new TestThreadPool(getClass().getName());
     private static final String USER = "test_user";
     private static final SecureString PASS = new SecureString("x-pack-test-password".toCharArray());
 
@@ -59,8 +86,10 @@ public class RemoteAccessHeadersSmokeIT extends ESRestTestCase {
         return Settings.builder().put(ThreadContext.PREFIX + ".Authorization", token).build();
     }
 
-    private boolean isFulfillingCluster() {
-        return "fulfilling_cluster".equals(System.getProperty("tests.rest.suite"));
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+        ThreadPool.terminate(threadPool, 10, TimeUnit.SECONDS);
     }
 
     /**
@@ -74,17 +103,28 @@ public class RemoteAccessHeadersSmokeIT extends ESRestTestCase {
             final Response response = client().performRequest(indexDocRequest);
             assertOK(response);
         } else {
-            updateRemoteClusterSettings(
-                Collections.singletonMap("authorization", "ZmU0SzdZUUJkRUZzTC1jMlZPalE6M2wxbG9KZWFRVXlkT3RPUzJaU0tsdw==")
-            );
-            final Request searchRequest = new Request("GET", "/my_remote_cluster:test_idx/_search");
-            final ResponseException e = expectThrows(ResponseException.class, () -> client().performRequest(searchRequest));
-            // 401 since fulfilling cluster is not yet set up to process remote access headers, and we do not send the authentication header
-            // anymore
-            assertEquals(401, e.getResponse().getStatusLine().getStatusCode());
+            try (
+                MockTransportService remoteTransport = startTransport("node0", new CopyOnWriteArrayList<>(), Version.CURRENT, threadPool)
+            ) {
+                final DiscoveryNode remoteNode = remoteTransport.getLocalDiscoNode();
+                updateRemoteClusterSettings(
+                    Map.of(
+                        "authorization",
+                        "ZmU0SzdZUUJkRUZzTC1jMlZPalE6M2wxbG9KZWFRVXlkT3RPUzJaU0tsdw==",
+                        "seeds",
+                        remoteNode.getAddress().toString()
+                    )
+                );
+                final Request searchRequest = new Request("GET", "/my_remote_cluster:test_idx/_search");
+                final ResponseException e = expectThrows(ResponseException.class, () -> client().performRequest(searchRequest));
+                // 401 since fulfilling cluster is not yet set up to process remote access headers, and we do not send the authentication
+                // header anymore
+                assertEquals(401, e.getResponse().getStatusLine().getStatusCode());
+            }
         }
     }
 
+    // TODO
     private static void updateRemoteClusterSettings(Map<String, Object> settings) throws IOException {
         Request request = new Request("PUT", "/_cluster/settings");
         request.setEntity(buildUpdateSettingsRequestBody(settings));
@@ -114,4 +154,89 @@ public class RemoteAccessHeadersSmokeIT extends ESRestTestCase {
         }
         return new NStringEntity(requestBody, ContentType.APPLICATION_JSON);
     }
+
+    private boolean isFulfillingCluster() {
+        return "fulfilling_cluster".equals(System.getProperty("tests.rest.suite"));
+    }
+
+    private static MockTransportService startTransport(
+        final String id,
+        final List<DiscoveryNode> knownNodes,
+        final Version version,
+        final ThreadPool threadPool
+    ) {
+        boolean success = false;
+        final Settings s = Settings.builder().put("node.name", id).build();
+        final ClusterName clusterName = ClusterName.CLUSTER_NAME_SETTING.get(s);
+        final MockTransportService newService = MockTransportService.createNewService(s, version, threadPool, null);
+        try {
+            newService.registerRequestHandler(
+                ClusterSearchShardsAction.NAME,
+                ThreadPool.Names.SAME,
+                ClusterSearchShardsRequest::new,
+                (request, channel, task) -> {
+                    expectRemoteAccessHeaders(threadPool);
+                    channel.sendResponse(
+                        new ClusterSearchShardsResponse(
+                            new ClusterSearchShardsGroup[0],
+                            knownNodes.toArray(new DiscoveryNode[0]),
+                            Collections.emptyMap()
+                        )
+                    );
+                }
+            );
+            newService.registerRequestHandler(SearchAction.NAME, ThreadPool.Names.SAME, SearchRequest::new, (request, channel, task) -> {
+                expectRemoteAccessHeaders(threadPool);
+                channel.sendResponse(
+                    new SearchResponse(
+                        new InternalSearchResponse(
+                            new SearchHits(new SearchHit[0], new TotalHits(0, TotalHits.Relation.EQUAL_TO), Float.NaN),
+                            InternalAggregations.EMPTY,
+                            null,
+                            null,
+                            false,
+                            null,
+                            1
+                        ),
+                        null,
+                        1,
+                        1,
+                        0,
+                        100,
+                        ShardSearchFailure.EMPTY_ARRAY,
+                        SearchResponse.Clusters.EMPTY
+                    )
+                );
+            });
+            newService.registerRequestHandler(
+                ClusterStateAction.NAME,
+                ThreadPool.Names.SAME,
+                ClusterStateRequest::new,
+                (request, channel, task) -> {
+                    assertThat(threadPool.getThreadContext().getHeader("_xpack_security_authentication"), notNullValue());
+                    DiscoveryNodes.Builder builder = DiscoveryNodes.builder();
+                    for (DiscoveryNode node : knownNodes) {
+                        builder.add(node);
+                    }
+                    ClusterState build = ClusterState.builder(clusterName).nodes(builder.build()).build();
+                    channel.sendResponse(new ClusterStateResponse(clusterName, build, false));
+                }
+            );
+            newService.start();
+            newService.acceptIncomingRequests();
+            success = true;
+            return newService;
+        } finally {
+            if (success == false) {
+                newService.close();
+            }
+        }
+    }
+
+    private static void expectRemoteAccessHeaders(ThreadPool threadPool) {
+        assertThat(threadPool.getThreadContext().getHeader("_xpack_security_authentication"), nullValue());
+        assertThat(threadPool.getThreadContext().getHeader("_remote_access_authentication"), notNullValue());
+        assertThat(threadPool.getThreadContext().getHeader("_remote_access_cluster_credential"), notNullValue());
+    }
+
 }

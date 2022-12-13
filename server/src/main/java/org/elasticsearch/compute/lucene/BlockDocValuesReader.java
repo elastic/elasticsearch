6a3e675bc46574dbf42d33d2ad8c076059b51820
Nhat Nguyen <nhat.nguyen@elastic.co>
2022-12-13 08:49:15 -0800

    Separate readers from ValuesSourceReaderOperator (ESQL-456)
    Move ValuesSource readers to a separate class from
    ValuesSourceReaderOperator, so we reuse them in HashAggregator.

---
+++ server/src/main/java/org/elasticsearch/compute/lucene/BlockDocValuesReader.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.compute.lucene;
+
+import org.apache.lucene.index.DocValues;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.index.NumericDocValues;
+import org.apache.lucene.index.SortedNumericDocValues;
+import org.elasticsearch.compute.data.Block;
+import org.elasticsearch.compute.data.BytesRefArrayBlock;
+import org.elasticsearch.compute.data.DoubleArrayBlock;
+import org.elasticsearch.compute.data.LongArrayBlock;
+import org.elasticsearch.index.fielddata.FieldData;
+import org.elasticsearch.index.fielddata.NumericDoubleValues;
+import org.elasticsearch.index.fielddata.SortedBinaryDocValues;
+import org.elasticsearch.index.fielddata.SortedNumericDoubleValues;
+import org.elasticsearch.search.aggregations.support.CoreValuesSourceType;
+import org.elasticsearch.search.aggregations.support.ValuesSource;
+import org.elasticsearch.search.aggregations.support.ValuesSourceType;
+
+import java.io.IOException;
+
+/**
+ * A reader that supports reading doc-values from a Lucene segment in Block fashion.
+ */
+public abstract class BlockDocValuesReader {
+
+    protected final Thread creationThread;
+
+    public BlockDocValuesReader() {
+        this.creationThread = Thread.currentThread();
+    }
+
+    /**
+     * Returns the current doc that this reader is on.
+     */
+    public abstract int docID();
+
+    /**
+     * Reads the values of the given documents specified in the input block
+     */
+    public abstract Block readValues(Block docs) throws IOException;
+
+    /**
+     * Checks if the reader can be used to read a range documents starting with the given docID by the current thread.
+     */
+    public static boolean canReuse(BlockDocValuesReader reader, int startingDocID) {
+        return reader != null && reader.docID() <= startingDocID && reader.creationThread == Thread.currentThread();
+    }
+
+    public static BlockDocValuesReader createBlockReader(
+        ValuesSource valuesSource,
+        ValuesSourceType valuesSourceType,
+        LeafReaderContext leafReaderContext
+    ) throws IOException {
+        if (CoreValuesSourceType.NUMERIC.equals(valuesSourceType) || CoreValuesSourceType.DATE.equals(valuesSourceType)) {
+            ValuesSource.Numeric numericVS = (ValuesSource.Numeric) valuesSource;
+            if (numericVS.isFloatingPoint()) {
+                final SortedNumericDoubleValues doubleValues = numericVS.doubleValues(leafReaderContext);
+                return new DoubleValuesReader(doubleValues);
+            } else {
+                final SortedNumericDocValues longValues = numericVS.longValues(leafReaderContext);
+                return new LongValuesReader(longValues);
+            }
+        }
+        if (CoreValuesSourceType.KEYWORD.equals(valuesSourceType)) {
+            final ValuesSource.Bytes bytesVS = (ValuesSource.Bytes) valuesSource;
+            final SortedBinaryDocValues bytesValues = bytesVS.bytesValues(leafReaderContext);
+            return new BytesValuesReader(bytesValues);
+        }
+        throw new IllegalArgumentException("Field type [" + valuesSourceType.typeName() + "] is not supported");
+    }
+
+    private static class LongValuesReader extends BlockDocValuesReader {
+        private final NumericDocValues numericDocValues;
+
+        LongValuesReader(SortedNumericDocValues numericDocValues) {
+            this.numericDocValues = DocValues.unwrapSingleton(numericDocValues);
+        }
+
+        @Override
+        public Block readValues(Block docs) throws IOException {
+            final int positionCount = docs.getPositionCount();
+            final long[] values = new long[positionCount];
+            int lastDoc = -1;
+            for (int i = 0; i < positionCount; i++) {
+                int doc = docs.getInt(i);
+                // docs within same block must be in order
+                if (lastDoc >= doc) {
+                    throw new IllegalStateException("docs within same block must be in order");
+                }
+                if (numericDocValues.advanceExact(doc) == false) {
+                    throw new IllegalStateException("sparse fields not supported for now, could not read doc [" + doc + "]");
+                }
+                values[i] = numericDocValues.longValue();
+                lastDoc = doc;
+            }
+            return new LongArrayBlock(values, positionCount);
+        }
+
+        @Override
+        public int docID() {
+            return numericDocValues.docID();
+        }
+    }
+
+    private static class DoubleValuesReader extends BlockDocValuesReader {
+        private final NumericDoubleValues numericDocValues;
+        private int docID = -1;
+
+        DoubleValuesReader(SortedNumericDoubleValues numericDocValues) {
+            this.numericDocValues = FieldData.unwrapSingleton(numericDocValues);
+        }
+
+        @Override
+        public Block readValues(Block docs) throws IOException {
+            final int positionCount = docs.getPositionCount();
+            final double[] values = new double[positionCount];
+            int lastDoc = -1;
+            for (int i = 0; i < positionCount; i++) {
+                int doc = docs.getInt(i);
+                // docs within same block must be in order
+                if (lastDoc >= doc) {
+                    throw new IllegalStateException("docs within same block must be in order");
+                }
+                if (numericDocValues.advanceExact(doc) == false) {
+                    throw new IllegalStateException("sparse fields not supported for now, could not read doc [" + doc + "]");
+                }
+                values[i] = numericDocValues.doubleValue();
+                lastDoc = doc;
+                this.docID = doc;
+            }
+            return new DoubleArrayBlock(values, positionCount);
+        }
+
+        @Override
+        public int docID() {
+            return docID;
+        }
+    }
+
+    private static class BytesValuesReader extends BlockDocValuesReader {
+        private int docID = -1;
+        private final SortedBinaryDocValues binaryDV;
+
+        BytesValuesReader(SortedBinaryDocValues binaryDV) {
+            this.binaryDV = binaryDV;
+        }
+
+        @Override
+        public Block readValues(Block docs) throws IOException {
+            final int positionCount = docs.getPositionCount();
+            BytesRefArrayBlock.Builder builder = BytesRefArrayBlock.builder(positionCount);
+            int lastDoc = -1;
+            for (int i = 0; i < docs.getPositionCount(); i++) {
+                int doc = docs.getInt(i);
+                // docs within same block must be in order
+                if (lastDoc >= doc) {
+                    throw new IllegalStateException("docs within same block must be in order");
+                }
+                if (binaryDV.advanceExact(doc) == false) {
+                    throw new IllegalStateException("sparse fields not supported for now, could not read doc [" + doc + "]");
+                }
+                if (binaryDV.docValueCount() != 1) {
+                    throw new IllegalStateException("multi-values not supported for now, could not read doc [" + doc + "]");
+                }
+                builder.append(binaryDV.nextValue());
+                lastDoc = doc;
+                this.docID = doc;
+            }
+            return builder.build();
+        }
+
+        @Override
+        public int docID() {
+            return docID;
+        }
+    }
+}
--- server/src/main/java/org/elasticsearch/compute/lucene/ValuesSourceReaderOperator.java
+++ server/src/main/java/org/elasticsearch/compute/lucene/ValuesSourceReaderOperator.java
@@ -8,24 +8,13 @@
 
 package org.elasticsearch.compute.lucene;
 
-import org.apache.lucene.index.DocValues;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.index.NumericDocValues;
-import org.apache.lucene.index.SortedNumericDocValues;
 import org.elasticsearch.compute.Experimental;
 import org.elasticsearch.compute.data.Block;
-import org.elasticsearch.compute.data.BytesRefArrayBlock;
 import org.elasticsearch.compute.data.ConstantIntBlock;
-import org.elasticsearch.compute.data.DoubleArrayBlock;
-import org.elasticsearch.compute.data.LongArrayBlock;
 import org.elasticsearch.compute.data.Page;
 import org.elasticsearch.compute.operator.Operator;
-import org.elasticsearch.index.fielddata.FieldData;
-import org.elasticsearch.index.fielddata.NumericDoubleValues;
-import org.elasticsearch.index.fielddata.SortedBinaryDocValues;
-import org.elasticsearch.index.fielddata.SortedNumericDoubleValues;
-import org.elasticsearch.search.aggregations.support.CoreValuesSourceType;
 import org.elasticsearch.search.aggregations.support.ValuesSource;
 import org.elasticsearch.search.aggregations.support.ValuesSourceType;
 
@@ -50,12 +39,9 @@ public class ValuesSourceReaderOperator implements Operator {
     private final int shardChannel;
     private final String field;
 
-    private LeafReaderContext lastLeafReaderContext;
-    private DocValuesCollector docValuesCollector;
-    private ValuesSource lastValuesSource;
-    private ValuesSourceType lastValuesSourceType;
-    private Thread lastThread;
+    private BlockDocValuesReader lastReader;
     private int lastShard = -1;
+    private int lastSegment = -1;
 
     private Page lastPage;
 
@@ -152,17 +138,19 @@ public class ValuesSourceReaderOperator implements Operator {
         ConstantIntBlock shardOrd = (ConstantIntBlock) page.getBlock(shardChannel);
 
         if (docs.getPositionCount() > 0) {
-            int ord = leafOrd.getInt(0);
+            int segment = leafOrd.getInt(0);
             int shard = shardOrd.getInt(0);
-            initState(ord, shard);
             int firstDoc = docs.getInt(0);
-            // reset iterator when blocks arrive out-of-order
-            if (firstDoc <= docValuesCollector.docID()) {
-                resetDocValues();
-            }
-
             try {
-                Block block = docValuesCollector.createBlock(docs);
+                if (lastShard != shard || lastSegment != segment || BlockDocValuesReader.canReuse(lastReader, firstDoc) == false) {
+                    ValuesSource vs = valuesSources.get(shard);
+                    ValuesSourceType vt = valuesSourceTypes.get(shard);
+                    LeafReaderContext leafReaderContext = indexReaders.get(shard).leaves().get(segment);
+                    lastReader = BlockDocValuesReader.createBlockReader(vs, vt, leafReaderContext);
+                    lastShard = shard;
+                    lastSegment = segment;
+                }
+                Block block = lastReader.readValues(docs);
                 lastPage = page.appendBlock(block);
             } catch (IOException e) {
                 throw new UncheckedIOException(e);
@@ -170,187 +158,8 @@ public class ValuesSourceReaderOperator implements Operator {
         }
     }
 
-    private void initState(int ord, int shard) {
-        boolean resetDV = false;
-        if (lastShard != shard) {
-            lastLeafReaderContext = null;
-            lastShard = shard;
-        }
-        if (lastLeafReaderContext != null && lastLeafReaderContext.ord != ord) {
-            lastLeafReaderContext = null;
-            lastValuesSource = null;
-            lastValuesSourceType = null;
-        }
-        if (lastLeafReaderContext == null || lastValuesSource == null) {
-            lastLeafReaderContext = indexReaders.get(shard).getContext().leaves().get(ord);
-            lastValuesSource = valuesSources.get(shard);
-            lastValuesSourceType = valuesSourceTypes.get(shard);
-            resetDV = true;
-        }
-        if (lastLeafReaderContext.ord != ord) {
-            throw new IllegalStateException("wrong ord id");
-        }
-        if (Thread.currentThread() != lastThread) {
-            // reset iterator when executing thread changes
-            resetDV = true;
-        }
-        if (resetDV) {
-            resetDocValues();
-        }
-    }
-
-    private void resetDocValues() {
-        try {
-            if (CoreValuesSourceType.NUMERIC.equals(lastValuesSourceType) || CoreValuesSourceType.DATE.equals(lastValuesSourceType)) {
-                resetNumericField((ValuesSource.Numeric) lastValuesSource);
-            } else if (CoreValuesSourceType.KEYWORD.equals(lastValuesSourceType)) {
-                resetKeywordField((ValuesSource.Bytes) lastValuesSource);
-            } else {
-                throw new IllegalArgumentException("Field type [" + lastValuesSourceType.typeName() + "] is not supported");
-            }
-            lastThread = Thread.currentThread();
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
-
-    private void resetNumericField(ValuesSource.Numeric numericVS) throws IOException {
-        if (numericVS.isFloatingPoint()) {
-            // Extract double values
-            SortedNumericDoubleValues sortedNumericDocValues = numericVS.doubleValues(lastLeafReaderContext);
-            final NumericDoubleValues numericDocValues = FieldData.unwrapSingleton(sortedNumericDocValues);
-            this.docValuesCollector = new DocValuesCollector() {
-                /**
-                 * Store docID internally because class {@link NumericDoubleValues} does not support
-                 * a docID() method.
-                 */
-                private int docID = -1;
-
-                @Override
-                public int docID() {
-                    return docID;
-                }
-
-                @Override
-                public Block createBlock(Block docs) throws IOException {
-                    final int positionCount = docs.getPositionCount();
-                    final double[] values = new double[positionCount];
-                    int lastDoc = -1;
-                    for (int i = 0; i < positionCount; i++) {
-                        int doc = docs.getInt(i);
-                        // docs within same block must be in order
-                        if (lastDoc >= doc) {
-                            throw new IllegalStateException("docs within same block must be in order");
-                        }
-                        if (numericDocValues.advanceExact(doc) == false) {
-                            throw new IllegalStateException("sparse fields not supported for now, could not read doc [" + doc + "]");
-                        }
-                        values[i] = numericDocValues.doubleValue();
-                        lastDoc = doc;
-                        docID = doc;
-                    }
-                    return new DoubleArrayBlock(values, positionCount);
-                }
-            };
-        } else {
-            // Extract long values
-            SortedNumericDocValues sortedNumericDocValues = numericVS.longValues(lastLeafReaderContext);
-            final NumericDocValues numericDocValues = DocValues.unwrapSingleton(sortedNumericDocValues);
-            this.docValuesCollector = new DocValuesCollector() {
-
-                @Override
-                public int docID() {
-                    return numericDocValues.docID();
-                }
-
-                @Override
-                public Block createBlock(Block docs) throws IOException {
-                    final int positionCount = docs.getPositionCount();
-                    final long[] values = new long[positionCount];
-                    int lastDoc = -1;
-                    for (int i = 0; i < positionCount; i++) {
-                        int doc = docs.getInt(i);
-                        // docs within same block must be in order
-                        if (lastDoc >= doc) {
-                            throw new IllegalStateException("docs within same block must be in order");
-                        }
-                        if (numericDocValues.advanceExact(doc) == false) {
-                            throw new IllegalStateException("sparse fields not supported for now, could not read doc [" + doc + "]");
-                        }
-                        values[i] = numericDocValues.longValue();
-                        lastDoc = doc;
-                    }
-                    return new LongArrayBlock(values, positionCount);
-                }
-            };
-        }
-    }
-
-    private void resetKeywordField(ValuesSource.Bytes bytesVS) throws IOException {
-        final SortedBinaryDocValues binaryDV = bytesVS.bytesValues(lastLeafReaderContext);
-        this.docValuesCollector = new DocValuesCollector() {
-            private int docID = -1;
-
-            @Override
-            public int docID() {
-                return docID;
-            }
-
-            @Override
-            public Block createBlock(Block docs) throws IOException {
-                final int positionCount = docs.getPositionCount();
-                BytesRefArrayBlock.Builder builder = BytesRefArrayBlock.builder(positionCount);
-                int lastDoc = -1;
-                for (int i = 0; i < docs.getPositionCount(); i++) {
-                    int doc = docs.getInt(i);
-                    // docs within same block must be in order
-                    if (lastDoc >= doc) {
-                        throw new IllegalStateException("docs within same block must be in order");
-                    }
-                    if (binaryDV.advanceExact(doc) == false) {
-                        throw new IllegalStateException("sparse fields not supported for now, could not read doc [" + doc + "]");
-                    }
-                    if (binaryDV.docValueCount() != 1) {
-                        throw new IllegalStateException("multi-values not supported for now, could not read doc [" + doc + "]");
-                    }
-                    builder.append(binaryDV.nextValue());
-                    lastDoc = doc;
-                    docID = doc;
-                }
-                return builder.build();
-            }
-        };
-    }
-
     @Override
     public void close() {
-        lastLeafReaderContext = null;
-        lastValuesSource = null;
-        docValuesCollector = null;
-        lastThread = null;
-    }
 
-    /**
-     * Interface that collects documents, extracts its doc_value data and creates a
-     * {@link Block} with all extracted values.
-     */
-    interface DocValuesCollector {
-
-        /**
-         * This method iterates over a block containing document ids and create a block
-         * containing all extracted values for the collected documents.
-         *
-         * @param docs a block containing the documents ids for the documents to read
-         * @return a {@link Block} with all extracted values
-         */
-        Block createBlock(Block docs) throws IOException;
-
-        /**
-         * @return  the following:
-         * -1 if nextDoc() or advance(int) were not called yet.
-         * NO_MORE_DOCS if the iterator has exhausted.
-         * Otherwise, it should return the doc ID it is currently on.
-         */
-        int docID();
     }
 }

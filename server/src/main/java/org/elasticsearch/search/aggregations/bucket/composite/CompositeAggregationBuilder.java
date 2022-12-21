3770c29d5797f62386b8fd67ea18848975ee8c42
Salvatore Campagna <93581129+salvatore-campagna@users.noreply.github.com>
2022-12-21 09:48:03 +0100

    fix: prevent usage of time series aggregations with some parent aggregations
    We walk the sub-aggregations searching for the first time series aggregation.
    It would have been better to have this check done in the TimSeriesAggregationBuilder,
    but it is located in a different module and in a different package. As a result,
    due to some dependency issues between modules, we are not able to reference other
    aggregations available in the server module. Ideally, by the time migration of all
    aggregations to the aggregations module is complete, the time series aggregation
    would be fixed to work also with aggregations that are not currently supported.

--- server/src/main/java/org/elasticsearch/search/aggregations/bucket/composite/CompositeAggregationBuilder.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/bucket/composite/CompositeAggregationBuilder.java
@@ -27,8 +27,10 @@ import org.elasticsearch.xcontent.XContentBuilder;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -183,6 +185,23 @@ public class CompositeAggregationBuilder extends AbstractAggregationBuilder<Comp
             }
     }
 
+    private static String validateSubAggregations(Collection<AggregationBuilder> builders) {
+        if (builders == null || builders.isEmpty()) {
+            return null;
+        }
+
+        for (final AggregationBuilder builder : builders) {
+            if ("time_series".equals(builder.getType().toLowerCase(Locale.ROOT))) {
+                return builder.getType();
+            }
+            final String subAggregations = validateSubAggregations(builder.getSubAggregations());
+            if (subAggregations != null) {
+                return subAggregations;
+            }
+        }
+        return null;
+    }
+
     private static void validateSources(List<CompositeValuesSourceBuilder<?>> sources) {
         if (sources == null || sources.isEmpty()) {
             throw new IllegalArgumentException("Composite [" + SOURCES_FIELD_NAME.getPreferredName() + "] cannot be null or empty");
@@ -219,6 +238,14 @@ public class CompositeAggregationBuilder extends AbstractAggregationBuilder<Comp
                     + invalid.getClass().getSimpleName()
                     + "]"
             );
+        }
+        if (context.isInSortOrderExecutionRequired()) {
+            final String invalidSubAggregation = validateSubAggregations(subfactoriesBuilder.getAggregatorFactories());
+            if (invalidSubAggregation != null) {
+                throw new IllegalArgumentException(
+                    "[composite] aggregation cannot be used with a child aggregation of" + " type: [" + invalidSubAggregation + "]"
+                );
+            }
         }
         CompositeValuesSourceConfig[] configs = new CompositeValuesSourceConfig[sources.size()];
         for (int i = 0; i < configs.length; i++) {

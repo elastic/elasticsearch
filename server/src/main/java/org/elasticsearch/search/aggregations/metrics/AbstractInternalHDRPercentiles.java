9f524539db56afc4d10ce8167fd17a8b9c7498c6
Martijn van Groningen <martijn.v.groningen@gmail.com>
2022-12-23 16:14:05 +0100

    fixed npe in mixed cluster

--- server/src/main/java/org/elasticsearch/search/aggregations/metrics/AbstractInternalHDRPercentiles.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/metrics/AbstractInternalHDRPercentiles.java
@@ -84,13 +84,14 @@ abstract class AbstractInternalHDRPercentiles extends InternalNumericMetricsAggr
         out.writeNamedWriteable(format);
         out.writeDoubleArray(keys);
         if (out.getVersion().onOrAfter(Version.V_8_7_0)) {
-            if (state != null) {
+            if (this.state != null) {
                 out.writeBoolean(true);
-                encode(state, out);
+                encode(this.state, out);
             } else {
                 out.writeBoolean(false);
             }
         } else {
+            DoubleHistogram state = this.state != null ? this.state : EMPTY_HISTOGRAM;
             encode(state, out);
         }
         out.writeBoolean(keyed);

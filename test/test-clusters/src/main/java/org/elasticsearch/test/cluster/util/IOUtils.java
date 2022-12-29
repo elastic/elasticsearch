0f28d1fcd44a9649ffc3a13f2938467efea369c5
Mark Vieira <portugee@gmail.com>
2022-12-29 12:02:07 -0800

    [7.17] Add support for addition configuration files to test clusters framework (#92579) (#92603)
    * Add support for addition configuration files to test clusters framework (#92579)
    
    This adds the ability to supply arbitrary files to the config directory
    of cluster nodes. Typically, this is used for security use cases, such
    as providing for SSL certificates and trust stores.
    
    This commit adds a few other features to enable more testing ues cases
    as well, such as the ability to restart a cluster, as well as explicit
    ordering of test cases withing a test class. This is needed for test
    suites that need to execute some tests, restart the cluster, then
    execute more in a particular order.
    # Conflicts:
    #	test/test-clusters/src/main/java/org/elasticsearch/test/cluster/local/LocalClusterHandle.java
    #	x-pack/plugin/security/qa/basic-enable-security/build.gradle
    #	x-pack/plugin/security/qa/basic-enable-security/src/javaRestTest/java/org/elasticsearch/xpack/security/EnableSecurityOnBasicLicenseIT.java
    #	x-pack/qa/multi-node/src/javaRestTest/java/org/elasticsearch/multi_node/GlobalCheckpointSyncActionIT.java
    
    * Fix static initialization of random value
    
    * Remove unused imports
    
    * Spotless

--- test/test-clusters/src/main/java/org/elasticsearch/test/cluster/util/IOUtils.java
+++ test/test-clusters/src/main/java/org/elasticsearch/test/cluster/util/IOUtils.java
@@ -10,9 +10,7 @@ package org.elasticsearch.test.cluster.util;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UncheckedIOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
@@ -87,22 +85,6 @@ public final class IOUtils {
                 throw new UncheckedIOException("Failed to copy " + s + " to " + d, e);
             }
         });
-    }
-
-    public static String readStringFromStream(InputStream in) {
-        try {
-            return new String(in.readAllBytes(), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    throw new UncheckedIOException(e);
-                }
-            }
-        }
     }
 
     private static void sync(Path sourceRoot, Path destinationRoot, BiConsumer<Path, Path> syncMethod) {

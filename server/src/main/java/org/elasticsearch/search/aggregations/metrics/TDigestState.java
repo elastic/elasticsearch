68cfd58c32e61f5fb6e2722bacc4fe8a3101d9d1
Salvatore Campagna <93581129+salvatore-campagna@users.noreply.github.com>
2023-01-03 11:25:35 +0100

    fix: use MergingTDigest instead of AvlTDigest
    MergingTDigest is more efficient since it allocates memory upfront
    not incurring memory allocation cost during histogram calculations.
    
    Alos fix a potential serialization issue where a long is written
    instead of a int when the number of centroids is serialized.

--- server/src/main/java/org/elasticsearch/search/aggregations/metrics/TDigestState.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/metrics/TDigestState.java
@@ -7,44 +7,136 @@
  */
 package org.elasticsearch.search.aggregations.metrics;
 
-import com.tdunning.math.stats.AVLTreeDigest;
 import com.tdunning.math.stats.Centroid;
+import com.tdunning.math.stats.TDigest;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Extension of {@link com.tdunning.math.stats.TDigest} with custom serialization.
  */
-public class TDigestState extends AVLTreeDigest {
+public class TDigestState extends TDigest {
+    private final TDigest delegate;
 
-    private final double compression;
+    private static VariantStage withCompression(double compression) {
+        return new Builder(compression);
+    }
 
     public TDigestState(double compression) {
-        super(compression);
-        this.compression = compression;
+        this.delegate = TDigestState.withCompression(compression).defaultTDigest();
+    }
+
+    private TDigestState(final TDigest delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void add(double x, int w) {
+        this.delegate.add(x, w);
+    }
+
+    @Override
+    public void add(List<? extends TDigest> others) {
+        this.delegate.add(others);
+    }
+
+    @Override
+    public void compress() {
+        this.delegate.compress();
+    }
+
+    @Override
+    public long size() {
+        return this.delegate.size();
+    }
+
+    @Override
+    public double cdf(double x) {
+        return this.delegate.cdf(x);
+    }
+
+    @Override
+    public double quantile(double q) {
+        return this.delegate.quantile(q);
+    }
+
+    @Override
+    public Collection<Centroid> centroids() {
+        return this.delegate.centroids();
     }
 
     @Override
     public double compression() {
-        return compression;
+        return this.delegate.compression();
+    }
+
+    @Override
+    public int byteSize() {
+        return this.delegate.byteSize();
+    }
+
+    @Override
+    public int smallByteSize() {
+        return this.delegate.smallByteSize();
+    }
+
+    @Override
+    public void asBytes(ByteBuffer buf) {
+        this.delegate.asBytes(buf);
+    }
+
+    @Override
+    public void asSmallBytes(ByteBuffer buf) {
+        this.delegate.asSmallBytes(buf);
+    }
+
+    @Override
+    public TDigest recordAllData() {
+        return this.delegate.recordAllData();
+    }
+
+    @Override
+    public boolean isRecording() {
+        return this.delegate.isRecording();
+    }
+
+    @Override
+    public void add(double x) {
+        this.delegate.add(x);
+    }
+
+    @Override
+    public void add(TDigest other) {
+        this.delegate.add(other);
+    }
+
+    @Override
+    public int centroidCount() {
+        return this.delegate.centroidCount();
     }
 
     public static void write(TDigestState state, StreamOutput out) throws IOException {
-        out.writeDouble(state.compression);
+        out.writeDouble(state.compression());
         out.writeVInt(state.centroidCount());
         for (Centroid centroid : state.centroids()) {
             out.writeDouble(centroid.mean());
-            out.writeVLong(centroid.count());
+            out.writeVInt(centroid.count());
         }
     }
 
     public static TDigestState read(StreamInput in) throws IOException {
         double compression = in.readDouble();
-        TDigestState state = new TDigestState(compression);
+        TDigestState state = in.getVersion().onOrAfter(Version.V_8_7_0)
+            ? TDigestState.withCompression(compression).mergingTDigest()
+            : TDigestState.withCompression(compression).defaultTDigest();
         int n = in.readVInt();
         for (int i = 0; i < n; i++) {
             state.add(in.readDouble(), in.readVInt());
@@ -58,7 +150,7 @@ public class TDigestState extends AVLTreeDigest {
             return false;
         }
         TDigestState that = (TDigestState) obj;
-        if (compression != that.compression) {
+        if (this.compression() != that.compression()) {
             return false;
         }
         Iterator<? extends Centroid> thisCentroids = centroids().iterator();
@@ -79,11 +171,43 @@ public class TDigestState extends AVLTreeDigest {
     @Override
     public int hashCode() {
         int h = getClass().hashCode();
-        h = 31 * h + Double.hashCode(compression);
+        h = 31 * h + Double.hashCode(compression());
         for (Centroid centroid : centroids()) {
             h = 31 * h + Double.hashCode(centroid.mean());
             h = 31 * h + centroid.count();
         }
         return h;
+    }
+
+    public interface VariantStage {
+        TDigestState defaultTDigest();
+
+        TDigestState avlTDigest();
+
+        TDigestState mergingTDigest();
+    }
+
+    public static class Builder implements VariantStage {
+
+        private final double compression;
+
+        public Builder(double compression) {
+            this.compression = compression;
+        }
+
+        @Override
+        public TDigestState defaultTDigest() {
+            return new TDigestState(TDigest.createAvlTreeDigest(this.compression));
+        }
+
+        @Override
+        public TDigestState avlTDigest() {
+            return new TDigestState(TDigest.createAvlTreeDigest(this.compression));
+        }
+
+        @Override
+        public TDigestState mergingTDigest() {
+            return new TDigestState(TDigest.createMergingDigest(this.compression));
+        }
     }
 }

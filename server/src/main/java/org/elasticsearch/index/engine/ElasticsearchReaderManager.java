8d630e0d8e761a66143100bde07a320ee1b9590a
Tim Brooks <tim@timbrooks.org>
2023-01-08 18:25:15 -0700

    Make engine components public for plugins
    Some plugins require certain engine components (fetching the engine and
    creating completion stats) public for extending index shard and the
    underlying engine. This commit makes these components public.

--- server/src/main/java/org/elasticsearch/index/engine/ElasticsearchReaderManager.java
+++ server/src/main/java/org/elasticsearch/index/engine/ElasticsearchReaderManager.java
@@ -25,6 +25,7 @@ import java.io.IOException;
  *
  */
 @SuppressForbidden(reason = "reference counting is required here")
+public
 class ElasticsearchReaderManager extends ReferenceManager<ElasticsearchDirectoryReader> {
 
     /**
@@ -34,7 +35,7 @@ class ElasticsearchReaderManager extends ReferenceManager<ElasticsearchDirectory
      *
      * @param reader            the directoryReader to use for future reopens
      */
-    ElasticsearchReaderManager(ElasticsearchDirectoryReader reader) {
+    public ElasticsearchReaderManager(ElasticsearchDirectoryReader reader) {
         this.current = reader;
     }
 

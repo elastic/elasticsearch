6200f2b4a28587363e8ab01588ee104be1ee00df
Artem Prigoda <artem.prigoda@elastic.co>
2023-01-11 15:41:32 +0100

    Run S3RegisterCASLinearizabilityTests only on CI (#47)
    Add a new Gradle task `statelessS3ThirdPartyTests` just to run `S3RegisterCASLinearizabilityTests` separately. Do it against a real AWS bucket with the credentials from Vault. Run it for every pull request, move to nightly later when nightly jobs are supported for Stateless.
    
    We also exclude `S3RegisterCASLinearizabilityTests` from the set of normal integration tests that are on a development machine. Nevertheless, `S3RegisterCASLinearizabilityTests` still can be run individually on a development machine configured with an AWS development account.
    
    The Jenkins job reuses the credentials and the bucket from elasticsearch's `statelessS3ThirdPartyTests` job. (Note: Does it make sense to create a new bucket for stateless? )
    
    Fixes

--- x-pack/plugin/stateless/build.gradle.kts
+++ x-pack/plugin/stateless/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.elasticsearch.gradle.internal.test.InternalClusterTestPlugin
+
 plugins {
     id("elasticsearch.internal-cluster-test")
 }
@@ -32,4 +34,22 @@ dependencies {
         add("testImplementation", "org.elasticsearch.plugin:repository-gcs")
         add("testImplementation", "org.elasticsearch.plugin:repository-azure")
     }
+}
+
+tasks {
+    internalClusterTest {
+        exclude ("**/S3RegisterCASLinearizabilityTests.class")
+    }
+}
+
+tasks.register<Test>("statelessS3ThirdPartyTests") {
+  val internalTestSourceSet = sourceSets.getByName(InternalClusterTestPlugin.SOURCE_SET_NAME)
+  setTestClassesDirs(internalTestSourceSet.getOutput().getClassesDirs())
+  setClasspath(internalTestSourceSet.getRuntimeClasspath())
+  include ("**/S3RegisterCASLinearizabilityTests.class")
+  systemProperty ("test.s3.access_key", System.getenv("stateless_aws_s3_access_key"))
+  systemProperty ("test.s3.secret_key", System.getenv("stateless_aws_s3_secret_key"))
+  systemProperty ("test.s3.bucket", System.getenv("stateless_aws_s3_bucket"))
+  systemProperty ("test.s3.region", System.getenv("stateless_aws_s3_region"))
+  systemProperty ("test.s3.base_path", System.getenv("stateless_aws_s3_base_path"))
 }

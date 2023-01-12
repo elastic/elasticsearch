1cdd59d9f99d2054e009879516a92a94f9272c00
Martijn van Groningen <martijn.v.groningen@gmail.com>
2023-01-12 09:40:30 +0100

    iter

--- server/src/main/java/org/elasticsearch/common/util/ReleasableByteArray.java
+++ server/src/main/java/org/elasticsearch/common/util/ReleasableByteArray.java
@@ -81,7 +81,11 @@ public class ReleasableByteArray implements ByteArray {
 
     @Override
     public byte[] array() {
-        return ref.array();
+        // This assumption of this method is that the return array has valid entries starting from 0 and
+        // this isn't case when just returning the array from ReleasableBytesReference#array().
+        // The interface this class implements should have something like an arrayOffset() method,
+        // so that callers from what array offset the first elements starts.
+        throw new UnsupportedOperationException();
     }
 
     @Override

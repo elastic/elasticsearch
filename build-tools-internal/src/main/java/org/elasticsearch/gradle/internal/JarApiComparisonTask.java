64a08d90e39dd26bc1d367e4445ce5d6b6bd5996
William Brafford <william.brafford@elastic.co>
2023-01-13 09:56:01 -0500

    A little bit of gradle cleanup and simplification

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/JarApiComparisonTask.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/JarApiComparisonTask.java
@@ -12,11 +12,12 @@ import org.gradle.api.DefaultTask;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.provider.Property;
-import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.TaskAction;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -49,6 +50,14 @@ public abstract class JarApiComparisonTask extends DefaultTask {
 
     @TaskAction
     public void compare() {
+        FileCollection fileCollection = getOldJar().get();
+        File newJarFile = getNewJar().get().getAsFile();
+
+        Set<String> oldJarNames = fileCollection.getFiles().stream().map(File::getName).collect(Collectors.toSet());
+        if (oldJarNames.contains(newJarFile.getName())) {
+            throw new IllegalStateException("what? " + oldJarNames);
+        }
+
         JarScanner oldJS = new JarScanner(getOldJar().get().getSingleFile().toString());
         JarScanner newJS = new JarScanner(getNewJar().get().toString());
         try {
@@ -58,7 +67,7 @@ public abstract class JarApiComparisonTask extends DefaultTask {
         }
     }
 
-    @Input
+    @InputFiles
     public abstract Property<FileCollection> getOldJar();
 
     @InputFile

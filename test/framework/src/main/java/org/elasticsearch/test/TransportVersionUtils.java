c64e813844ae362231f449dd0060da3ec3ae4a67
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2023-01-16 13:01:27 +0100

    test cleanup

--- test/framework/src/main/java/org/elasticsearch/test/TransportVersionUtils.java
+++ test/framework/src/main/java/org/elasticsearch/test/TransportVersionUtils.java
@@ -9,39 +9,34 @@
 package org.elasticsearch.test;
 
 import org.elasticsearch.TransportVersion;
-import org.elasticsearch.Version;
-import org.elasticsearch.core.Tuple;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TransportVersionUtils {
     private static final List<TransportVersion> ALL_VERSIONS;
 
     static {
-//        Tuple<List<Version>, List<Version>> versions = resolveReleasedVersions(Version.CURRENT, Version.class);
-//        RELEASED_VERSIONS = versions.v1();
-//        UNRELEASED_VERSIONS = versions.v2();
-//        List<Version> allVersions = new ArrayList<>(RELEASED_VERSIONS.size() + UNRELEASED_VERSIONS.size());
-//        allVersions.addAll(RELEASED_VERSIONS);
-//        allVersions.addAll(UNRELEASED_VERSIONS);
-//        Collections.sort(allVersions);
-        ALL_VERSIONS = getDeclaredVersions();//Collections.emptyList();//Collections.unmodifiableList(allVersions);
+        ALL_VERSIONS = getDeclaredVersions();
     }
 
     /** Returns a random {@link TransportVersion} from all available versions. */
-    public static TransportVersion randomVersion(Random random) {
-        return ALL_VERSIONS.get(random.nextInt(ALL_VERSIONS.size()));
+    public static TransportVersion randomVersion() {
+        return ESTestCase.randomFrom(ALL_VERSIONS);
+    }
+
+    /** Returns a random {@link TransportVersion} from all available versions without the ignore set */
+    public static TransportVersion randomVersion(Set<TransportVersion> ignore) {
+        return ESTestCase.randomFrom(ALL_VERSIONS.stream().filter(v -> ignore.contains(v) == false).collect(Collectors.toList()));
     }
 
     /**
-     * Extracts a sorted list of declared version constants from a class.
-     * The argument would normally be Version.class but is exposed for
-     * testing with other classes-containing-version-constants.
+     * Extracts a sorted list of declared transport version constants from a class.
      */
     public static List<TransportVersion> getDeclaredVersions() {
         final Field[] fields = TransportVersion.class.getFields();
@@ -59,7 +54,8 @@ public class TransportVersionUtils {
                 case "V_EMPTY":
                     continue;
             }
-            assert field.getName().matches("V(_\\d+){3}?") || field.getName().equals("ZERO") || field.getName().equals("MINIMUM_COMPATIBLE") : field.getName();
+            assert field.getName().matches("V(_\\d+){3}?") || field.getName().equals("ZERO") || field.getName().equals("MINIMUM_COMPATIBLE")
+                : field.getName();
             try {
                 versions.add(((TransportVersion) field.get(null)));
             } catch (final IllegalAccessException e) {
@@ -77,7 +73,7 @@ public class TransportVersionUtils {
     }
 
     public static TransportVersion getPreviousVersion(TransportVersion version) {
-        for (int i = ALL_VERSIONS.size() - 1; i >= 0; i--) {//shoudl be RELEASED_VERSIONS..
+        for (int i = ALL_VERSIONS.size() - 1; i >= 0; i--) {// TODO should be RELEASED_VERSIONS
             TransportVersion v = ALL_VERSIONS.get(i);
             if (v.before(version)) {
                 return v;

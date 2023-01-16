1f5f433a9dbf7c4fdcccd06e0d4818d6ab61d4ca
Tim Brooks <tim@timbrooks.org>
2023-01-15 22:48:03 -0700

    Add configurable operation listener for translog
    This commit adds a listener that will be called when new operations are
    written to the translog writer.

--- server/src/main/java/org/elasticsearch/index/translog/TranslogConfig.java
+++ server/src/main/java/org/elasticsearch/index/translog/TranslogConfig.java
@@ -32,6 +32,7 @@ public final class TranslogConfig {
     private final ShardId shardId;
     private final Path translogPath;
     private final ByteSizeValue bufferSize;
+    private final OperationListener operationListener;
 
     /**
      * Creates a new TranslogConfig instance
@@ -44,6 +45,16 @@ public final class TranslogConfig {
         this(shardId, translogPath, indexSettings, bigArrays, DEFAULT_BUFFER_SIZE, DiskIoBufferPool.INSTANCE);
     }
 
+    public TranslogConfig(
+        ShardId shardId,
+        Path translogPath,
+        IndexSettings indexSettings,
+        BigArrays bigArrays,
+        OperationListener operationListener
+    ) {
+        this(shardId, translogPath, indexSettings, bigArrays, DEFAULT_BUFFER_SIZE, DiskIoBufferPool.INSTANCE, operationListener);
+    }
+
     TranslogConfig(
         ShardId shardId,
         Path translogPath,
@@ -52,12 +63,25 @@ public final class TranslogConfig {
         ByteSizeValue bufferSize,
         DiskIoBufferPool diskIoBufferPool
     ) {
+        this(shardId, translogPath, indexSettings, bigArrays, bufferSize, diskIoBufferPool, (d, s, l) -> {});
+    }
+
+    TranslogConfig(
+        ShardId shardId,
+        Path translogPath,
+        IndexSettings indexSettings,
+        BigArrays bigArrays,
+        ByteSizeValue bufferSize,
+        DiskIoBufferPool diskIoBufferPool,
+        OperationListener operationListener
+    ) {
         this.bufferSize = bufferSize;
         this.indexSettings = indexSettings;
         this.shardId = shardId;
         this.translogPath = translogPath;
         this.bigArrays = bigArrays;
         this.diskIoBufferPool = diskIoBufferPool;
+        this.operationListener = operationListener;
     }
 
     /**
@@ -101,5 +125,9 @@ public final class TranslogConfig {
      */
     public DiskIoBufferPool getDiskIoBufferPool() {
         return diskIoBufferPool;
+    }
+
+    public OperationListener getOperationListener() {
+        return operationListener;
     }
 }

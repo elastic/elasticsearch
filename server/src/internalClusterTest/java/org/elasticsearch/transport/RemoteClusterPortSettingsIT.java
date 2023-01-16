f218af162013fe6bea2f9c31dd0e9642f802cf26
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2023-01-16 16:21:26 +0100

    Spotless

--- server/src/internalClusterTest/java/org/elasticsearch/transport/RemoteClusterPortSettingsIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/transport/RemoteClusterPortSettingsIT.java
@@ -20,7 +20,7 @@ public class RemoteClusterPortSettingsIT extends ESIntegTestCase {
 
     public void testDirectlyConfiguringTransportProfileForRemoteClusterWillFailToStartTheNode() {
         assumeTrue("untrusted remote cluster feature flag must be enabled", TcpTransport.isUntrustedRemoteClusterEnabled());
-        
+
         internalCluster().setBootstrapMasterNodeIndex(0);
 
         final Settings.Builder builder = Settings.builder()

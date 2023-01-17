a907d3d8ff4fc9cd11fc6f170de0cee26de4150c
Pooya Salehi <pxsalehi@users.noreply.github.com>
2023-01-17 14:15:30 +0100

    Allow listeners for the segment generation (#92818)
    This change allows adding generation listeners which are needed for
    Stateless Refresh. The changes include:
    
    - Aligning Engine#refresh and Engine#maybeRefresh to always return a
      RefreshResult.
    - Add addSegmentGenerationListener to Engine which by default throw
      UnsupportedOperationException.
    - Adapt TransportShardRefreshAction to work with Stateless.
    
    Relates: ES-4862

--- server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
@@ -1217,12 +1217,12 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
     /**
      * Writes all indexing changes to disk and opens a new searcher reflecting all changes.  This can throw {@link AlreadyClosedException}.
      */
-    public void refresh(String source) {
+    public Engine.RefreshResult refresh(String source) {
         verifyNotClosed();
         if (logger.isTraceEnabled()) {
             logger.trace("refresh with source [{}]", source);
         }
-        getEngine().refresh(source);
+        return getEngine().refresh(source);
     }
 
     /**
@@ -3803,7 +3803,7 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
                 if (logger.isTraceEnabled()) {
                     logger.trace("refresh with source [schedule]");
                 }
-                return getEngine().maybeRefresh("schedule");
+                return getEngine().maybeRefresh("schedule").refreshed();
             }
         }
         final Engine engine = getEngine();
@@ -4119,5 +4119,9 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
     @Override
     public String toString() {
         return "IndexShard(shardRouting=" + shardRouting + ")";
+    }
+
+    public void waitForSegmentGeneration(long segmentGeneration, ActionListener<Long> listener) {
+        getEngine().addSegmentGenerationListener(segmentGeneration, listener::onResponse);
     }
 }

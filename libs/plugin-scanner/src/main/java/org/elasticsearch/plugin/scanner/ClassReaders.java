7d424b67ee1af5f3ec46f2a6b193ed23d686a6e1
ChrisHegarty <christopher.hegarty@elastic.co>
2023-01-18 10:55:48 +0000

    spotless

--- libs/plugin-scanner/src/main/java/org/elasticsearch/plugin/scanner/ClassReaders.java
+++ libs/plugin-scanner/src/main/java/org/elasticsearch/plugin/scanner/ClassReaders.java
@@ -45,7 +45,7 @@ public class ClassReaders {
             return Collections.emptyList();
         }
         Path dir = Paths.get(path);
-        try (var stream = Files.list(dir)){
+        try (var stream = Files.list(dir)) {
             return ofPaths(stream);
         } catch (IOException e) {
             throw new UncheckedIOException(e);

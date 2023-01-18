1673dd8306e51f8cd51c10492d2cc75b6f852d89
Nikola Grcevski <nikola.grcevski@elastic.co>
2023-01-18 11:51:22 -0500

    Unmute test.

--- server/src/internalClusterTest/java/org/elasticsearch/readiness/ReadinessClusterIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/readiness/ReadinessClusterIT.java
@@ -242,7 +242,6 @@ public class ReadinessClusterIT extends ESIntegTestCase implements ReadinessClie
         logger.info("--> New file settings: [{}]", Strings.format(json, version));
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/93036")
     public void testNotReadyOnBadFileSettings() throws Exception {
         internalCluster().setBootstrapMasterNodeIndex(0);
         logger.info("--> start data node / non master node");

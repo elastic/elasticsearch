a2caf316fc634714f097afaaeea1c05c272ccf9a
Ievgen Degtiarenko <ievgen.degtiarenko@elastic.co>
2023-01-19 09:03:23 +0100

    Simplify watching for a settings changes (#92152)

--- server/src/main/java/org/elasticsearch/cluster/ClusterModule.java
+++ server/src/main/java/org/elasticsearch/cluster/ClusterModule.java
@@ -382,13 +382,12 @@ public class ClusterModule extends AbstractModule {
         WriteLoadForecaster writeLoadForecaster
     ) {
         Map<String, Supplier<ShardsAllocator>> allocators = new HashMap<>();
-        allocators.put(BALANCED_ALLOCATOR, () -> new BalancedShardsAllocator(settings, clusterSettings, writeLoadForecaster));
+        allocators.put(BALANCED_ALLOCATOR, () -> new BalancedShardsAllocator(clusterSettings, writeLoadForecaster));
         allocators.put(
             DESIRED_BALANCE_ALLOCATOR,
             () -> new DesiredBalanceShardsAllocator(
-                settings,
                 clusterSettings,
-                new BalancedShardsAllocator(settings, clusterSettings, writeLoadForecaster),
+                new BalancedShardsAllocator(clusterSettings, writeLoadForecaster),
                 threadPool,
                 clusterService,
                 reconciler

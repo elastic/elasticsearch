5a4ba64054b0d44e9417161303bf1e25876538a6
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2023-01-19 11:14:54 +0100

    Address feedback

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/transport/SecurityServerTransportInterceptor.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/transport/SecurityServerTransportInterceptor.java
@@ -306,7 +306,7 @@ public class SecurityServerTransportInterceptor implements TransportInterceptor 
 
                 final Authentication authentication = securityContext.getAuthentication();
                 assert authentication != null : "authentication must be present in security context";
-                final Subject effectiveSubject = securityContext.getAuthentication().getEffectiveSubject();
+                final Subject effectiveSubject = authentication.getEffectiveSubject();
                 if (false == effectiveSubject.getType().equals(Subject.Type.USER)) {
                     logger.trace(
                         "Effective subject of request to remote cluster [{}] has an unsupported type [{}]",
@@ -350,11 +350,11 @@ public class SecurityServerTransportInterceptor implements TransportInterceptor 
 
                 final ThreadContext threadContext = securityContext.getThreadContext();
                 final var contextRestoreHandler = new ContextRestoreResponseHandler<>(threadContext.newRestorableContext(true), handler);
-                if (User.isInternal(authentication.getEffectiveSubject().getUser())) {
-                    assertInternalUserExpectedInCrossClusterRequest(authentication.getEffectiveSubject().getUser());
-                    try (ThreadContext.StoredContext ignore = threadContext.stashContext()) {
+                final User user = authentication.getEffectiveSubject().getUser();
+                if (SystemUser.is(user)) {
+                    try (ThreadContext.StoredContext ignored = threadContext.stashContext()) {
                         remoteAccessCredentials.writeToContext(threadContext);
-                        // Access control is handled differently for internal users. Privileges are defined by the fulfilling cluster,
+                        // Access control is handled differently for the system user. Privileges are defined by the fulfilling cluster,
                         // so we pass an empty role descriptors intersection here and let the receiver resolve privileges based on the
                         // authentication instance
                         new RemoteAccessAuthentication(authentication, RoleDescriptorsIntersection.EMPTY).writeToContext(threadContext);
@@ -362,6 +362,10 @@ public class SecurityServerTransportInterceptor implements TransportInterceptor 
                     } catch (IOException e) {
                         contextRestoreHandler.handleException(new SendRequestTransportException(connection.getNode(), action, e));
                     }
+                } else if (User.isInternal(user)) {
+                    final String message = "internal user [" + user.principal() + "] should not be used for cross cluster requests";
+                    assert false : message;
+                    throw new IllegalStateException(message);
                 } else {
                     authzService.retrieveRemoteAccessRoleDescriptorsIntersection(
                         remoteClusterAlias,
@@ -374,14 +378,6 @@ public class SecurityServerTransportInterceptor implements TransportInterceptor 
                             }
                         }, e -> contextRestoreHandler.handleException(new SendRequestTransportException(connection.getNode(), action, e)))
                     );
-                }
-            }
-
-            private void assertInternalUserExpectedInCrossClusterRequest(final User user) {
-                if (false == (SystemUser.is(user))) {
-                    final String message = "internal user [" + user.principal() + "] should not be used for cross cluster requests";
-                    assert false : message;
-                    throw new IllegalStateException(message);
                 }
             }
 

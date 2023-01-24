ca32150615709c1ec40e3e26d2834e2833e3e47b
David Turner <david.turner@elastic.co>
2023-01-24 11:02:33 +0000

    Extract unit test harness for ObjectStoreService (#91)
    Creating an `ObjectStoreService` needs rather a lot of other things too.
    This commit extracts a test harness to make it a little easier to write
    more unit tests for it.

--- x-pack/plugin/stateless/src/test/java/co/elastic/elasticsearch/stateless/ObjectStoreServiceTests.java
+++ x-pack/plugin/stateless/src/test/java/co/elastic/elasticsearch/stateless/ObjectStoreServiceTests.java
@@ -25,11 +25,14 @@ import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexFileNames;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.tests.mockfile.ExtrasFS;
 import org.elasticsearch.Version;
 import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.blobstore.BlobContainer;
 import org.elasticsearch.common.blobstore.BlobPath;
 import org.elasticsearch.common.blobstore.BlobStore;
@@ -40,6 +43,8 @@ import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.BigArrays;
 import org.elasticsearch.core.CheckedConsumer;
+import org.elasticsearch.core.IOUtils;
+import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.engine.Engine;
 import org.elasticsearch.index.shard.ShardId;
@@ -57,10 +62,14 @@ import org.elasticsearch.threadpool.TestThreadPool;
 import org.elasticsearch.transport.TransportService;
 import org.hamcrest.Matchers;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -124,160 +133,241 @@ public class ObjectStoreServiceTests extends ESTestCase {
     }
 
     public void testBlobUploadFailureBlocksSegmentsUpload() throws IOException {
-        final var node = new DiscoveryNode("node", buildNewFakeTransportAddress(), Version.CURRENT);
-        final var repoPath = createTempDir();
-        final var nodeSettings = Settings.builder()
-            .put(PATH_REPO_SETTING.getKey(), repoPath)
-            .put(BUCKET_SETTING.getKey(), repoPath)
-            .build();
-        final var clusterSettings = new ClusterSettings(nodeSettings, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS);
-        final var environment = newEnvironment(nodeSettings);
-
-        final var indexMetadata = IndexMetadata.builder("index")
-            .settings(
-                Settings.builder()
-                    .put(IndexMetadata.INDEX_NUMBER_OF_SHARDS_SETTING.getKey(), 1)
-                    .put(IndexMetadata.INDEX_NUMBER_OF_REPLICAS_SETTING.getKey(), 1)
-                    .put(IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey(), Version.CURRENT)
-            )
-            .build();
-        final var shardId = new ShardId(indexMetadata.getIndex(), 0);
-        final var indexSettings = new IndexSettings(indexMetadata, nodeSettings);
-        final var shardPath = new ShardPath(
-            false,
-            createTempDir().resolve(shardId.getIndex().getUUID()).resolve("0"),
-            createTempDir().resolve(shardId.getIndex().getUUID()).resolve("0"),
-            shardId
-        );
-
         final var indexWriterConfig = new IndexWriterConfig(new KeywordAnalyzer());
-        indexWriterConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-        indexWriterConfig.setCommitOnClose(true);
+        final var permits = new Semaphore(0);
+        try (var testHarness = new TestHarness() {
+            @Override
+            public BlobContainer wrapBlobContainer(BlobPath path, BlobContainer innerContainer) {
 
-        final var threadPool = new TestThreadPool("test");
-        try (
-            var transport = new MockTransport();
-            var clusterService = ClusterServiceUtils.createClusterService(threadPool);
-            var client = new NodeClient(nodeSettings, threadPool);
-            var directory = new FsDirectoryFactory().newDirectory(indexSettings, shardPath);
-            var store = new Store(shardId, indexSettings, directory, new DummyShardLock(shardId));
-            var indexWriter = new IndexWriter(store.directory(), indexWriterConfig)
-        ) {
-
-            final var transportService = transport.createTransportService(
-                nodeSettings,
-                threadPool,
-                TransportService.NOOP_TRANSPORT_INTERCEPTOR,
-                ignored -> node,
-                null,
-                Set.of()
-            );
-
-            final var permits = new Semaphore(0);
-
-            class WrappedBlobContainer extends FilterBlobContainer {
-                WrappedBlobContainer(BlobContainer delegate) {
-                    super(delegate);
-                }
-
-                @Override
-                protected BlobContainer wrapChild(BlobContainer child) {
-                    return new WrappedBlobContainer(child);
-                }
-
-                @Override
-                public void writeBlob(String blobName, InputStream inputStream, long blobSize, boolean failIfAlreadyExists)
-                    throws IOException {
-                    assertFalse(blobName, blobName.startsWith(IndexFileNames.SEGMENTS));
-                    if (permits.tryAcquire()) {
-                        super.writeBlob(blobName, inputStream, blobSize, failIfAlreadyExists);
-                    } else {
-                        throw new IOException("simulated upload failure");
+                class WrappedBlobContainer extends FilterBlobContainer {
+                    WrappedBlobContainer(BlobContainer delegate) {
+                        super(delegate);
                     }
-                }
 
-                @Override
-                public void writeMetadataBlob(
-                    String blobName,
-                    boolean failIfAlreadyExists,
-                    boolean atomic,
-                    CheckedConsumer<OutputStream, IOException> writer
-                ) {
-                    throw new AssertionError("should not be called");
-                }
+                    @Override
+                    protected BlobContainer wrapChild(BlobContainer child) {
+                        return new WrappedBlobContainer(child);
+                    }
 
-                @Override
-                public void writeBlobAtomic(String blobName, BytesReference bytes, boolean failIfAlreadyExists) {
-                    throw new AssertionError("should not be called");
-                }
-            }
-
-            final var repoService = new RepositoriesService(
-                nodeSettings,
-                clusterService,
-                transportService,
-                Map.of(
-                    FsRepository.TYPE,
-                    metadata -> new FsRepository(
-                        metadata,
-                        environment,
-                        xContentRegistry(),
-                        clusterService,
-                        BigArrays.NON_RECYCLING_INSTANCE,
-                        new RecoverySettings(nodeSettings, clusterSettings)
-                    ) {
-                        @Override
-                        protected BlobStore createBlobStore() throws Exception {
-                            final String location = REPOSITORIES_LOCATION_SETTING.get(getMetadata().settings());
-                            final Path locationFile = environment.resolveRepoFile(location);
-                            return new FsBlobStore(bufferSize, locationFile, isReadOnly()) {
-                                @Override
-                                public BlobContainer blobContainer(BlobPath path) {
-                                    return new WrappedBlobContainer(super.blobContainer(path));
-                                }
-                            };
+                    @Override
+                    public void writeBlob(String blobName, InputStream inputStream, long blobSize, boolean failIfAlreadyExists)
+                        throws IOException {
+                        assertFalse(blobName, blobName.startsWith(IndexFileNames.SEGMENTS));
+                        if (permits.tryAcquire()) {
+                            super.writeBlob(blobName, inputStream, blobSize, failIfAlreadyExists);
+                        } else {
+                            throw new IOException("simulated upload failure");
                         }
                     }
-                ),
-                Map.of(),
-                threadPool,
-                List.of()
-            );
 
-            transportService.start();
-            transportService.acceptIncomingRequests();
+                    @Override
+                    public void writeMetadataBlob(
+                        String blobName,
+                        boolean failIfAlreadyExists,
+                        boolean atomic,
+                        CheckedConsumer<OutputStream, IOException> writer
+                    ) {
+                        throw new AssertionError("should not be called");
+                    }
 
-            try (var objectStoreService = new ObjectStoreService(nodeSettings, () -> repoService, threadPool, client, clusterService)) {
-                objectStoreService.start();
+                    @Override
+                    public void writeBlobAtomic(String blobName, BytesReference bytes, boolean failIfAlreadyExists) {
+                        throw new AssertionError("should not be called");
+                    }
+                }
 
+                return new WrappedBlobContainer(innerContainer);
+            }
+        }; var indexWriter = new IndexWriter(testHarness.indexingStore.directory(), indexWriterConfig)) {
+
+            var commits = between(1, 3);
+            for (int i = 0; i < commits; i++) {
                 indexWriter.addDocument(List.of());
                 indexWriter.commit();
-
-                try (var indexReader = DirectoryReader.open(indexWriter)) {
-                    final var indexCommit = indexReader.getIndexCommit();
-                    final var commitFiles = store.getMetadata(indexCommit).fileMetadataMap();
-
-                    permits.release(between(0, commitFiles.size() - 2));
-
-                    PlainActionFuture.<Void, IOException>get(
-                        fut -> objectStoreService.onCommitCreation(
-                            new StatelessCommitRef(
-                                shardId,
-                                new Engine.IndexCommitRef(indexCommit, () -> fut.onResponse(null)),
-                                commitFiles,
-                                commitFiles.keySet(),
-                                1
-                            )
-                        ),
-                        10,
-                        TimeUnit.SECONDS
-                    );
-                }
             }
 
-        } finally {
-            TestThreadPool.terminate(threadPool, 10, TimeUnit.SECONDS);
+            var primaryTerm = randomNonNegativeLong();
+
+            try (var indexReader = DirectoryReader.open(indexWriter)) {
+
+                final var indexCommit = indexReader.getIndexCommit();
+                final var commitFiles = testHarness.indexingStore.getMetadata(indexCommit).fileMetadataMap();
+
+                final var permittedBlobs = between(0, commitFiles.size() - 2);
+                permits.release(permittedBlobs);
+
+                PlainActionFuture.<Void, IOException>get(
+                    future -> testHarness.objectStoreService.onCommitCreation(
+                        new StatelessCommitRef(
+                            testHarness.shardId,
+                            new Engine.IndexCommitRef(indexCommit, () -> future.onResponse(null)),
+                            commitFiles,
+                            commitFiles.keySet(),
+                            primaryTerm
+                        )
+                    ),
+                    10,
+                    TimeUnit.SECONDS
+                );
+
+                final var blobs = new HashSet<>(
+                    testHarness.objectStoreService.getBlobContainer(testHarness.shardId, primaryTerm).listBlobs().keySet()
+                );
+                blobs.removeIf(ExtrasFS::isExtra);
+                assertEquals(permittedBlobs + " vs " + blobs, permittedBlobs, blobs.size());
+                for (String blobName : blobs) {
+                    assertFalse(blobName, blobName.startsWith(IndexFileNames.SEGMENTS));
+                }
+            }
         }
     }
 
+    private class TestHarness implements Closeable {
+
+        final DiscoveryNode node;
+        final Path repoPath;
+        final Settings nodeSettings;
+        final ClusterSettings clusterSettings;
+        final Environment environment;
+        final IndexMetadata indexMetadata;
+        final ShardId shardId;
+        final IndexSettings indexSettings;
+        final MockTransport transport;
+        final ClusterService clusterService;
+        final NodeClient client;
+        final ShardPath indexingShardPath;
+        final Directory indexingDirectory;
+        final Store indexingStore;
+        final TransportService transportService;
+        final RepositoriesService repoService;
+        final ObjectStoreService objectStoreService;
+
+        private final Closeable closeables;
+
+        TestHarness() throws IOException {
+            node = new DiscoveryNode("node", buildNewFakeTransportAddress(), Version.CURRENT);
+            repoPath = createTempDir();
+            nodeSettings = Settings.builder().put(PATH_REPO_SETTING.getKey(), repoPath).put(BUCKET_SETTING.getKey(), repoPath).build();
+            clusterSettings = new ClusterSettings(nodeSettings, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS);
+            environment = newEnvironment(nodeSettings);
+
+            indexMetadata = IndexMetadata.builder("index")
+                .settings(
+                    Settings.builder()
+                        .put(IndexMetadata.INDEX_NUMBER_OF_SHARDS_SETTING.getKey(), 1)
+                        .put(IndexMetadata.INDEX_NUMBER_OF_REPLICAS_SETTING.getKey(), 1)
+                        .put(IndexMetadata.SETTING_INDEX_VERSION_CREATED.getKey(), Version.CURRENT)
+                )
+                .build();
+            shardId = new ShardId(indexMetadata.getIndex(), 0);
+            indexSettings = new IndexSettings(indexMetadata, nodeSettings);
+            indexingShardPath = new ShardPath(
+                false,
+                createTempDir().resolve(shardId.getIndex().getUUID()).resolve("0"),
+                createTempDir().resolve(shardId.getIndex().getUUID()).resolve("0"),
+                shardId
+            );
+
+            try (var localCloseables = new TransferableCloseables()) {
+
+                final var threadPool = new TestThreadPool("test");
+                localCloseables.add(() -> TestThreadPool.terminate(threadPool, 10, TimeUnit.SECONDS));
+
+                transport = localCloseables.add(new MockTransport());
+                clusterService = localCloseables.add(ClusterServiceUtils.createClusterService(threadPool));
+                client = localCloseables.add(new NodeClient(nodeSettings, threadPool));
+                indexingDirectory = localCloseables.add(new FsDirectoryFactory().newDirectory(indexSettings, indexingShardPath));
+                indexingStore = localCloseables.add(new Store(shardId, indexSettings, indexingDirectory, new DummyShardLock(shardId)));
+
+                transportService = transport.createTransportService(
+                    nodeSettings,
+                    threadPool,
+                    TransportService.NOOP_TRANSPORT_INTERCEPTOR,
+                    ignored -> node,
+                    null,
+                    Set.of()
+                );
+
+                repoService = new RepositoriesService(
+                    nodeSettings,
+                    clusterService,
+                    transportService,
+                    Map.of(
+                        FsRepository.TYPE,
+                        metadata -> new FsRepository(
+                            metadata,
+                            environment,
+                            xContentRegistry(),
+                            clusterService,
+                            BigArrays.NON_RECYCLING_INSTANCE,
+                            new RecoverySettings(nodeSettings, clusterSettings)
+                        ) {
+                            @Override
+                            protected BlobStore createBlobStore() throws Exception {
+                                final String location = REPOSITORIES_LOCATION_SETTING.get(getMetadata().settings());
+                                final Path locationFile = environment.resolveRepoFile(location);
+                                return new FsBlobStore(bufferSize, locationFile, isReadOnly()) {
+                                    @Override
+                                    public BlobContainer blobContainer(BlobPath path) {
+                                        return wrapBlobContainer(path, super.blobContainer(path));
+                                    }
+                                };
+                            }
+                        }
+                    ),
+                    Map.of(),
+                    threadPool,
+                    List.of()
+                );
+
+                transportService.start();
+                transportService.acceptIncomingRequests();
+                localCloseables.add(transportService::stop);
+
+                objectStoreService = new ObjectStoreService(nodeSettings, () -> repoService, threadPool, client, clusterService);
+                objectStoreService.start();
+
+                closeables = localCloseables.transfer();
+            }
+        }
+
+        public BlobContainer wrapBlobContainer(BlobPath path, BlobContainer innerContainer) {
+            return innerContainer;
+        }
+
+        @Override
+        public void close() throws IOException {
+            IOUtils.close(closeables);
+        }
+    }
+
+    /**
+     * Encapsulates a common pattern of trying to open a bunch of resources and then transferring ownership elsewhere on success,
+     * but closing them on failure.
+     */
+    private static class TransferableCloseables implements Closeable {
+
+        private boolean transferred = false;
+        private final List<Closeable> closeables = new ArrayList<>();
+
+        <T extends Closeable> T add(T releasable) {
+            assert transferred == false : "already transferred";
+            closeables.add(releasable);
+            return releasable;
+        }
+
+        Closeable transfer() {
+            assert transferred == false : "already transferred";
+            transferred = true;
+            Collections.reverse(closeables);
+            return () -> IOUtils.close(closeables);
+        }
+
+        @Override
+        public void close() throws IOException {
+            if (transferred == false) {
+                IOUtils.close(closeables);
+            }
+        }
+    }
 }

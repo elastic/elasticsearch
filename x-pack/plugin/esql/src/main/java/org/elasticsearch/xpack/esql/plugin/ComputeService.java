776ab398355000c7f2e544170e4d963d2641d021
Nhat Nguyen <nhat.nguyen@elastic.co>
2023-01-25 09:45:22 -0800

    Integrate ESQL with security (ESQL-602)
    We must translate ESQL requests to IndicesRequests before acquiring
    search contexts to have security context applied.
    
    Closes ESQL-412

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/ComputeService.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/ComputeService.java
@@ -8,8 +8,14 @@
 package org.elasticsearch.xpack.esql.plugin;
 
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.ActionListenerResponseHandler;
+import org.elasticsearch.action.IndicesRequest;
+import org.elasticsearch.action.support.ChannelActionListener;
+import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.util.BigArrays;
 import org.elasticsearch.common.util.concurrent.CountDown;
 import org.elasticsearch.compute.data.Page;
@@ -29,6 +35,13 @@ import org.elasticsearch.search.internal.ShardSearchRequest;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.tasks.TaskId;
 import org.elasticsearch.threadpool.ThreadPool;
+import org.elasticsearch.transport.TransportChannel;
+import org.elasticsearch.transport.TransportRequest;
+import org.elasticsearch.transport.TransportRequestHandler;
+import org.elasticsearch.transport.TransportRequestOptions;
+import org.elasticsearch.transport.TransportResponse;
+import org.elasticsearch.transport.TransportService;
+import org.elasticsearch.xpack.esql.action.EsqlQueryAction;
 import org.elasticsearch.xpack.esql.plan.physical.EsQueryExec;
 import org.elasticsearch.xpack.esql.plan.physical.OutputExec;
 import org.elasticsearch.xpack.esql.plan.physical.PhysicalPlan;
@@ -51,74 +64,41 @@ public class ComputeService {
     private final NodeClient client;
     private final ThreadPool threadPool;
     private final BigArrays bigArrays;
+    private final TransportService transportService;
 
     public ComputeService(
         SearchService searchService,
         ClusterService clusterService,
+        TransportService transportService,
         NodeClient client,
         ThreadPool threadPool,
         BigArrays bigArrays
     ) {
         this.searchService = searchService;
         this.clusterService = clusterService;
+        this.transportService = transportService;
         this.client = client;
         this.threadPool = threadPool;
         this.bigArrays = bigArrays.withCircuitBreaking();
+        transportService.registerRequestHandler(
+            NODE_ACTION,
+            ThreadPool.Names.SEARCH,
+            AcquireSearchContextsRequest::new,
+            new AcquireSearchContextHandler()
+        );
     }
 
-    private void acquireSearchContexts(String[] indexNames, ActionListener<List<SearchContext>> listener) {
-        try {
-            Index[] indices = Arrays.stream(indexNames)
-                .map(x -> clusterService.state().metadata().index(x).getIndex())
-                .toArray(Index[]::new);
-            List<IndexShard> targetShards = new ArrayList<>();
-            for (Index index : indices) {
-                IndexService indexService = searchService.getIndicesService().indexServiceSafe(index);
-                for (IndexShard indexShard : indexService) {
-                    targetShards.add(indexShard);
-                }
-            }
-            if (targetShards.isEmpty()) {
-                listener.onResponse(List.of());
-                return;
-            }
-            CountDown countDown = new CountDown(targetShards.size());
-            for (IndexShard targetShard : targetShards) {
-                targetShard.awaitShardSearchActive(ignored -> {
-                    if (countDown.countDown()) {
-                        ActionListener.completeWith(listener, () -> {
-                            final List<SearchContext> searchContexts = new ArrayList<>();
-                            boolean success = false;
-                            try {
-                                for (IndexShard shard : targetShards) {
-                                    ShardSearchRequest shardSearchLocalRequest = new ShardSearchRequest(
-                                        shard.shardId(),
-                                        0,
-                                        AliasFilter.EMPTY
-                                    );
-                                    SearchContext context = searchService.createSearchContext(
-                                        shardSearchLocalRequest,
-                                        SearchService.NO_TIMEOUT
-                                    );
-                                    searchContexts.add(context);
-                                }
-                                for (SearchContext searchContext : searchContexts) {
-                                    searchContext.preProcess();
-                                }
-                                success = true;
-                                return searchContexts;
-                            } finally {
-                                if (success == false) {
-                                    IOUtils.close(searchContexts);
-                                }
-                            }
-                        });
-                    }
-                });
-            }
-        } catch (Exception e) {
-            listener.onFailure(e);
-        }
+    private void acquireSearchContexts(Task task, String[] indices, ActionListener<List<SearchContext>> listener) {
+        // We need to wrap ESQL request as IndicesRequest to integrate with security before performing the computation
+        // TODO: Remove this wrap once we support multi-node clusters
+        transportService.sendChildRequest(
+            clusterService.localNode(),
+            NODE_ACTION,
+            new AcquireSearchContextsRequest(indices),
+            task,
+            TransportRequestOptions.EMPTY,
+            new ActionListenerResponseHandler<>(listener.map(r -> r.searchContexts), AcquireSearchContextsResponse::new)
+        );
     }
 
     public void runCompute(Task rootTask, PhysicalPlan physicalPlan, EsqlConfiguration configuration, ActionListener<List<Page>> listener) {
@@ -129,7 +109,7 @@ public class ComputeService {
             .distinct()
             .toArray(String[]::new);
 
-        acquireSearchContexts(indexNames, ActionListener.wrap(searchContexts -> {
+        acquireSearchContexts(rootTask, indexNames, ActionListener.wrap(searchContexts -> {
             boolean success = false;
             List<Driver> drivers = new ArrayList<>();
             Runnable release = () -> Releasables.close(() -> Releasables.close(searchContexts), () -> Releasables.close(drivers));
@@ -184,5 +164,113 @@ public class ComputeService {
                 }
             }
         }, listener::onFailure));
+    }
+
+    private static class AcquireSearchContextsRequest extends TransportRequest implements IndicesRequest {
+        private final String[] indices;
+
+        AcquireSearchContextsRequest(StreamInput in) {
+            throw new UnsupportedOperationException("AcquireSearchContextsRequest should never leave the current node");
+        }
+
+        AcquireSearchContextsRequest(String[] indices) {
+            this.indices = indices;
+        }
+
+        @Override
+        public String[] indices() {
+            return indices;
+        }
+
+        @Override
+        public IndicesOptions indicesOptions() {
+            return IndicesOptions.strictSingleIndexNoExpandForbidClosed();
+        }
+    }
+
+    private static class AcquireSearchContextsResponse extends TransportResponse {
+        private final List<SearchContext> searchContexts;
+
+        AcquireSearchContextsResponse(List<SearchContext> searchContexts) {
+            this.searchContexts = searchContexts;
+        }
+
+        AcquireSearchContextsResponse(StreamInput in) {
+            throw new UnsupportedOperationException("AcquireSearchContextsResponse should never leave the current node");
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) {
+            throw new UnsupportedOperationException("AcquireSearchContextsResponse should never leave the current node");
+        }
+    }
+
+    private static final String NODE_ACTION = EsqlQueryAction.NAME + "[n]";
+
+    private class AcquireSearchContextHandler implements TransportRequestHandler<AcquireSearchContextsRequest> {
+        @Override
+        public void messageReceived(AcquireSearchContextsRequest request, TransportChannel channel, Task task) {
+            ChannelActionListener<AcquireSearchContextsResponse, AcquireSearchContextsRequest> listener = new ChannelActionListener<>(
+                channel,
+                NODE_ACTION,
+                request
+            );
+            doAcquireSearchContexts(request.indices, listener.map(AcquireSearchContextsResponse::new));
+        }
+
+        private void doAcquireSearchContexts(String[] indexNames, ActionListener<List<SearchContext>> listener) {
+            try {
+                Index[] indices = Arrays.stream(indexNames)
+                    .map(x -> clusterService.state().metadata().index(x).getIndex())
+                    .toArray(Index[]::new);
+                List<IndexShard> targetShards = new ArrayList<>();
+                for (Index index : indices) {
+                    IndexService indexService = searchService.getIndicesService().indexServiceSafe(index);
+                    for (IndexShard indexShard : indexService) {
+                        targetShards.add(indexShard);
+                    }
+                }
+                if (targetShards.isEmpty()) {
+                    listener.onResponse(List.of());
+                    return;
+                }
+                CountDown countDown = new CountDown(targetShards.size());
+                for (IndexShard targetShard : targetShards) {
+                    targetShard.awaitShardSearchActive(ignored -> {
+                        if (countDown.countDown()) {
+                            ActionListener.completeWith(listener, () -> {
+                                final List<SearchContext> searchContexts = new ArrayList<>();
+                                boolean success = false;
+                                try {
+                                    for (IndexShard shard : targetShards) {
+                                        ShardSearchRequest shardSearchLocalRequest = new ShardSearchRequest(
+                                            shard.shardId(),
+                                            0,
+                                            AliasFilter.EMPTY
+                                        );
+                                        SearchContext context = searchService.createSearchContext(
+                                            shardSearchLocalRequest,
+                                            SearchService.NO_TIMEOUT
+                                        );
+                                        searchContexts.add(context);
+                                    }
+                                    for (SearchContext searchContext : searchContexts) {
+                                        searchContext.preProcess();
+                                    }
+                                    success = true;
+                                    return searchContexts;
+                                } finally {
+                                    if (success == false) {
+                                        IOUtils.close(searchContexts);
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                listener.onFailure(e);
+            }
+        }
     }
 }

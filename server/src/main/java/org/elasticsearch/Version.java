387f5432d9462708e1f1d7906e6dce9db3213b54
Mark Vieira <portugee@gmail.com>
2023-01-26 11:46:59 -0800

    Bump versions after 8.6.1 release

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -130,7 +130,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_8_5_3 = new Version(8_05_03_99, org.apache.lucene.util.Version.LUCENE_9_4_2);
     public static final Version V_8_6_0 = new Version(8_06_00_99, org.apache.lucene.util.Version.LUCENE_9_4_2);
     public static final Version V_8_6_1 = new Version(8_06_01_99, org.apache.lucene.util.Version.LUCENE_9_4_2);
-    public static final Version CURRENT = V_8_6_1;
+    public static final Version V_8_6_2 = new Version(8_06_02_99, org.apache.lucene.util.Version.LUCENE_9_4_2);
+    public static final Version CURRENT = V_8_6_2;
 
     private static final Map<Integer, Version> idToVersion;
     private static final Map<String, Version> stringToVersion;

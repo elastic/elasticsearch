de4a0f5ead392cd8beab9d3135bb02171a579903
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2023-01-30 15:49:39 +0100

    Fix assertion

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authc/Subject.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authc/Subject.java
@@ -68,7 +68,7 @@ public class Subject {
             assert ServiceAccountSettings.REALM_NAME.equals(realm.getName()) : "service account realm name mismatch";
             this.type = Type.SERVICE_ACCOUNT;
         } else if (AuthenticationField.REMOTE_ACCESS_REALM_TYPE.equals(realm.getType())) {
-            assert AuthenticationField.REMOTE_ACCESS_REALM_TYPE.equals(realm.getName()) : "remote access realm name mismatch";
+            assert AuthenticationField.REMOTE_ACCESS_REALM_NAME.equals(realm.getName()) : "remote access realm name mismatch";
             this.type = Type.REMOTE_ACCESS;
         } else {
             this.type = Type.USER;

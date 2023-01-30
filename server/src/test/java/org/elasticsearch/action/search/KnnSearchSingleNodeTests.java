7306d43fd7eb273f2834f3d8030d366ca3212dc5
Jack Conradson <osjdconrad@gmail.com>
2023-01-30 08:00:13 -0800

    revert KnnSearchSingleNodeTests

--- server/src/test/java/org/elasticsearch/action/search/KnnSearchSingleNodeTests.java
+++ server/src/test/java/org/elasticsearch/action/search/KnnSearchSingleNodeTests.java
@@ -61,7 +61,6 @@ public class KnnSearchSingleNodeTests extends ESSingleNodeTestCase {
         float[] queryVector = randomVector();
         KnnSearchBuilder knnSearch = new KnnSearchBuilder("vector", queryVector, 5, 50).boost(5.0f);
         SearchResponse response = client().prepareSearch("index")
-            //.setTrackTotalHits(false)
             .setKnnSearch(List.of(knnSearch))
             .setQuery(QueryBuilders.matchQuery("text", "goodnight"))
             .addFetchField("*")

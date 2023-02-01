f1c338714d65f7ab46ab75915a940214f0dc7dad
Tanguy Leroux <tlrx.dev@gmail.com>
2023-02-01 15:19:54 +0100

    Do not refresh all indices in TransportBulkAction
    Relates #93160

--- server/src/main/java/org/elasticsearch/action/bulk/TransportBulkAction.java
+++ server/src/main/java/org/elasticsearch/action/bulk/TransportBulkAction.java
@@ -48,6 +48,7 @@ import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.routing.IndexRouting;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.util.concurrent.AtomicArray;
 import org.elasticsearch.core.Releasable;
@@ -197,15 +198,19 @@ public class TransportBulkAction extends HandledTransportAction<BulkRequest, Bul
         // TODO: Replace with a less hacky approach.
         ActionListener<BulkResponse> listener = outerListener;
         if (DiscoveryNode.isStateless(clusterService.getSettings()) && bulkRequest.getRefreshPolicy() != WriteRequest.RefreshPolicy.NONE) {
-            listener = outerListener.delegateFailure((l, r) -> client.admin().indices().prepareRefresh().execute(l.map(ignored -> {
+            listener = outerListener.delegateFailure((l, r) -> {
+                final Set<String> indices = new HashSet<>();
                 for (BulkItemResponse response : r.getItems()) {
+                    if (response.isFailed() == false) {
+                        indices.add(response.getIndex());
+                    }
                     DocWriteResponse docWriteResponse = response.getResponse();
                     if (docWriteResponse != null) {
                         docWriteResponse.setForcedRefresh(true);
                     }
                 }
-                return r;
-            })));
+                client.admin().indices().prepareRefresh().setIndices(indices.toArray(Strings.EMPTY_ARRAY)).execute(l.map(ignored -> r));
+            });
             bulkRequest.setRefreshPolicy(WriteRequest.RefreshPolicy.NONE);
         }
         /*

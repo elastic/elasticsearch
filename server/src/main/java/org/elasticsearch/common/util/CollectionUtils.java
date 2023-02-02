a0317bb94941e31d6464921a3feff5a632e53d5c
Joe Gallo <joe.gallo@elastic.co>
2023-02-02 11:00:59 -0500

    Add a warning to future generations
    This place is not a place of honor

--- server/src/main/java/org/elasticsearch/common/util/CollectionUtils.java
+++ server/src/main/java/org/elasticsearch/common/util/CollectionUtils.java
@@ -103,6 +103,10 @@ public class CollectionUtils {
     }
 
     private static void ensureNoSelfReferences(final Object value, final Set<Object> ancestors, final String messageHint) {
+        // these instanceof checks are a bit on the ugly side, but it's important for performance that we have
+        // a separate dispatch point for Maps versus for Iterables. a polymorphic version of this code would
+        // be prettier, but it would also likely be quite a bit slower. this is a hot path for ingest pipelines,
+        // and performance here is important.
         if (value == null || value instanceof String || value instanceof Number || value instanceof Boolean) {
             // noop
         } else if (value instanceof Map<?, ?> m && m.isEmpty() == false) {

0aa1a9f733db887326bd7f2c39ac66b61b443533
Adrien Grand <jpountz@gmail.com>
2023-02-02 09:37:43 +0100

    Remove 32-bits MurmurHash implementation.

--- server/src/main/java/org/elasticsearch/index/codec/bloomfilter/ES87BloomFilterPostingsFormat.java
+++ server/src/main/java/org/elasticsearch/index/codec/bloomfilter/ES87BloomFilterPostingsFormat.java
@@ -560,61 +560,6 @@ public class ES87BloomFilterPostingsFormat extends PostingsFormat {
         return outputs;
     }
 
-    /**
-     * Forked from Lucene's StringHelper#murmurhash3_x86_32 so that changes to the Lucene implementation
-     * do not break the compatibility of this format.
-     */
-    @SuppressWarnings("fallthrough")
-    private static int murmurhash3_x86_32(byte[] data, int offset, int len, int seed) {
-        final int c1 = 0xcc9e2d51;
-        final int c2 = 0x1b873593;
-
-        int h1 = seed;
-        int roundedEnd = offset + (len & 0xfffffffc); // round down to 4 byte block
-
-        for (int i = offset; i < roundedEnd; i += 4) {
-            // little endian load order
-            int k1 = (int) BitUtil.VH_LE_INT.get(data, i);
-            k1 *= c1;
-            k1 = Integer.rotateLeft(k1, 15);
-            k1 *= c2;
-
-            h1 ^= k1;
-            h1 = Integer.rotateLeft(h1, 13);
-            h1 = h1 * 5 + 0xe6546b64;
-        }
-
-        // tail
-        int k1 = 0;
-
-        switch (len & 0x03) {
-            case 3:
-                k1 = (data[roundedEnd + 2] & 0xff) << 16;
-                // fallthrough
-            case 2:
-                k1 |= (data[roundedEnd + 1] & 0xff) << 8;
-                // fallthrough
-            case 1:
-                k1 |= (data[roundedEnd] & 0xff);
-                k1 *= c1;
-                k1 = Integer.rotateLeft(k1, 15);
-                k1 *= c2;
-                h1 ^= k1;
-        }
-
-        // finalization
-        h1 ^= len;
-
-        // fmix(h1);
-        h1 ^= h1 >>> 16;
-        h1 *= 0x85ebca6b;
-        h1 ^= h1 >>> 13;
-        h1 *= 0xc2b2ae35;
-        h1 ^= h1 >>> 16;
-
-        return h1;
-    }
-
     // A self-contained implementation of CityHash that is used for the Bloom
     // filter.
     /*

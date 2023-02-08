ddaa5e4135298e954f390180f21811981f0397a5
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2023-02-08 10:37:53 -0500

    adding invariant tests

--- server/src/test/java/org/elasticsearch/search/vectors/VectorSimilarityQueryTests.java
+++ server/src/test/java/org/elasticsearch/search/vectors/VectorSimilarityQueryTests.java
@@ -19,17 +19,20 @@ import org.apache.lucene.search.Explanation;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.KnnFloatVectorQuery;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.ScoreMode;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.Weight;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.util.VectorUtil;
 import org.elasticsearch.common.lucene.LuceneTests;
 import org.elasticsearch.test.ESTestCase;
 
 import java.io.IOException;
+import java.util.function.Supplier;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 public class VectorSimilarityQueryTests extends ESTestCase {
 
@@ -66,6 +69,41 @@ public class VectorSimilarityQueryTests extends ESTestCase {
                     5
                 );
                 assertThat(docs.totalHits.value, equalTo(4L));
+            }
+        }
+    }
+
+    public void testEuclideanInvariant() throws Exception {
+        int dim = 4;
+        int L = 1;
+        int n = 100;
+        String fieldName = "vector";
+        Supplier<float[]> vectorValue = () -> new float[] { randomFloat(), randomFloat(), randomFloat(), randomFloat() };
+        try (Directory d = newDirectory()) {
+            try (IndexWriter w = new IndexWriter(d, new IndexWriterConfig())) {
+                KnnFloatVectorField vectorField = new KnnFloatVectorField(fieldName, vectorValue.get());
+                Document document = new Document();
+                document.add(vectorField);
+                for (int i = 0; i < n; i++) {
+                    w.addDocument(document);
+                    vectorField.setVectorValue(vectorValue.get());
+                }
+                w.commit();
+            }
+            try (IndexReader reader = DirectoryReader.open(d)) {
+                float radius = 0.25f * (float) Math.sqrt(dim) * L;
+                float score = 1f / (1f + radius);
+                IndexSearcher searcher = LuceneTests.newSearcher(reader);
+                for (int i = 0; i < 10; i++) {
+                    TopDocs docs = searcher.search(
+                        new VectorSimilarityQuery(new KnnFloatVectorQuery(fieldName, vectorValue.get(), n), radius, score),
+                        n
+                    );
+                    for (ScoreDoc scoreDoc : docs.scoreDocs) {
+                        float dist = (1 / scoreDoc.score) - 1;
+                        assertThat(dist, lessThanOrEqualTo(radius));
+                    }
+                }
             }
         }
     }
@@ -111,6 +149,44 @@ public class VectorSimilarityQueryTests extends ESTestCase {
         }
     }
 
+    public void testCosineInvariant() throws Exception {
+        int dim = 4;
+        int L = 1;
+        int n = 100;
+        String fieldName = "vector";
+        Supplier<float[]> vectorValue = () -> new float[] { randomFloat(), randomFloat(), randomFloat(), randomFloat() };
+        try (Directory d = newDirectory()) {
+            try (IndexWriter w = new IndexWriter(d, new IndexWriterConfig())) {
+                KnnFloatVectorField vectorField = new KnnFloatVectorField(fieldName, vectorValue.get(), VectorSimilarityFunction.COSINE);
+                Document document = new Document();
+                document.add(vectorField);
+                for (int i = 0; i < n; i++) {
+                    w.addDocument(document);
+                    vectorField.setVectorValue(vectorValue.get());
+                }
+                w.commit();
+            }
+            try (IndexReader reader = DirectoryReader.open(d)) {
+                float radius = 0.25f * (float) Math.sqrt(dim) * L;
+                float cos = (float) Math.cos(radius);
+                float score = (1 + radius) / 2f;
+                IndexSearcher searcher = LuceneTests.newSearcher(reader);
+                for (int i = 0; i < 10; i++) {
+                    TopDocs docs = searcher.search(
+                        new VectorSimilarityQuery(new KnnFloatVectorQuery(fieldName, vectorValue.get(), n), radius, score),
+                        n
+                    );
+                    for (ScoreDoc scoreDoc : docs.scoreDocs) {
+                        float dist = (2 * scoreDoc.score) - 1;
+                        float distCos = (float) Math.cos(dist);
+                        assertThat(scoreDoc.score, greaterThanOrEqualTo(score));
+                        assertThat(distCos, lessThanOrEqualTo(cos));
+                    }
+                }
+            }
+        }
+    }
+
     public void testExplain() throws IOException {
         try (Directory d = newDirectory()) {
             try (IndexWriter w = new IndexWriter(d, new IndexWriterConfig())) {
@@ -140,12 +216,6 @@ public class VectorSimilarityQueryTests extends ESTestCase {
                 assertFalse(ex.isMatch());
             }
         }
-    }
-
-    public void testDumb() {
-        float v = VectorSimilarityFunction.EUCLIDEAN.compare(new byte[] { 127, -128, 0, 10, -1 }, new byte[] { 127, -128, 0, 1, -1 });
-        float v2 = VectorUtil.squareDistance(new byte[] { 127, -128, 0, 10, -1 }, new byte[] { 127, -128, 0, 1, -1 });
-        assertTrue(false);
     }
 
 }

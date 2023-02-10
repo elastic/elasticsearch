9370fa5910e8dd6011b6fbb4470ff7d27380b67e
David Turner <david.turner@elastic.co>
2023-02-10 07:19:54 +0000

    Add cluster stats re. snapshot activity
    Shows how many ongoing snapshots/clones/deletions/etc. there are, and
    summarises the shard-level status too for progress tracking.

--- test/framework/src/main/java/org/elasticsearch/test/ESTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/ESTestCase.java
@@ -815,6 +815,10 @@ public abstract class ESTestCase extends LuceneTestCase {
         return random().ints(streamSize);
     }
 
+    public static int randomNonNegativeInt() {
+        return randomInt() & Integer.MAX_VALUE;
+    }
+
     /**
      * @return a <code>long</code> between <code>0</code> and <code>Long.MAX_VALUE</code> (inclusive) chosen uniformly at random.
      */

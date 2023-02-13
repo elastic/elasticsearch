5ac93c14d51f86242d180fed9f704ccd48816348
Simon Cooper <simon.cooper@elastic.co>
2023-02-13 12:20:29 +0000

    Some fixes

--- server/src/test/java/org/elasticsearch/transport/InboundHandlerTests.java
+++ server/src/test/java/org/elasticsearch/transport/InboundHandlerTests.java
@@ -32,6 +32,7 @@ import org.elasticsearch.tasks.TaskId;
 import org.elasticsearch.tasks.TaskManager;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.MockLogAppender;
+import org.elasticsearch.test.TransportVersionUtils;
 import org.elasticsearch.threadpool.TestThreadPool;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.tracing.Tracer;

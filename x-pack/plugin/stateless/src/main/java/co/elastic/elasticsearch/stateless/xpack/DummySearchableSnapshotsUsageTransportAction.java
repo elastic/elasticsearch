7619c3aab5f7b4ca2f6dbca25f2f439947dfe8d9
Ryan Ernst <ryan@iernst.net>
2023-02-15 09:19:34 -0800

    Fix missing searchable snapshots xpack info and usage (#148)
    With the recent removal of the searchable snapshots module from
    serverless, the xpack info and usage requests error because it expects
    actions to exist with the searchable snapshots name for info and usage.
    This commit temporarily works around the issue by creating dummy
    versions of these actions which fullfill the requirements.

---
+++ x-pack/plugin/stateless/src/main/java/co/elastic/elasticsearch/stateless/xpack/DummySearchableSnapshotsUsageTransportAction.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package co.elastic.elasticsearch.stateless.xpack;
+
+import org.elasticsearch.TransportVersion;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.support.ActionFilters;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.protocol.xpack.XPackUsageRequest;
+import org.elasticsearch.tasks.Task;
+import org.elasticsearch.threadpool.ThreadPool;
+import org.elasticsearch.transport.TransportService;
+import org.elasticsearch.xpack.core.XPackFeatureSet;
+import org.elasticsearch.xpack.core.action.XPackUsageFeatureAction;
+import org.elasticsearch.xpack.core.action.XPackUsageFeatureResponse;
+import org.elasticsearch.xpack.core.action.XPackUsageFeatureTransportAction;
+
+public class DummySearchableSnapshotsUsageTransportAction extends XPackUsageFeatureTransportAction {
+
+    @Inject
+    public DummySearchableSnapshotsUsageTransportAction(
+        TransportService transportService,
+        ClusterService clusterService,
+        ThreadPool threadPool,
+        ActionFilters actionFilters,
+        IndexNameExpressionResolver indexNameExpressionResolver
+    ) {
+        super(
+            XPackUsageFeatureAction.SEARCHABLE_SNAPSHOTS.name(),
+            transportService,
+            clusterService,
+            threadPool,
+            actionFilters,
+            indexNameExpressionResolver
+        );
+    }
+
+    @Override
+    protected void masterOperation(
+        Task task,
+        XPackUsageRequest request,
+        ClusterState state,
+        ActionListener<XPackUsageFeatureResponse> listener
+    ) throws Exception {
+        listener.onResponse(new XPackUsageFeatureResponse(new Usage()));
+    }
+
+    private static class Usage extends XPackFeatureSet.Usage {
+
+        private Usage() {
+            super(XPackUsageFeatureAction.SEARCHABLE_SNAPSHOTS.name(), false, false);
+        }
+
+        @Override
+        public TransportVersion getMinimalSupportedVersion() {
+            return TransportVersion.MINIMUM_COMPATIBLE;
+        }
+    }
+}

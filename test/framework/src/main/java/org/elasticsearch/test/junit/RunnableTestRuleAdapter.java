c24eb54b1771ab1f019c41b6526f94876df68de2
Tim Vernum <tim@adjective.org>
2023-02-16 16:06:51 +1100

    Add javadoc

--- test/framework/src/main/java/org/elasticsearch/test/junit/RunnableTestRuleAdapter.java
+++ test/framework/src/main/java/org/elasticsearch/test/junit/RunnableTestRuleAdapter.java
@@ -13,6 +13,19 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+/**
+ * Adapts a {@link CheckedRunnable} to a JUnit {@link TestRule}.
+ * The runnable is executed ({@link CheckedRunnable#run()}) when the {@link Statement} is evaluated, before the test is executed.
+ * That is, the supplied runnable represents a set-up step for the test.
+ * This is useful when a class needs some set-up code to be executed as part of a TestRule rather than a {@link org.junit.Before}
+ * or {@link org.junit.BeforeClass} annotated method. For example, if a test must run some set-up code <i>before</i> launching an
+ * {@code ElasticsearchCluster} that set-up code must be contained within a {@link TestRule} and chained to the cluster via a
+ * {@link org.junit.rules.RuleChain}. By using this adapter, the setup code can be expressed as a method reference as in the example below:
+ * <pre>{@code
+ * @ClassRule
+ * public static TestRule ruleChain = RuleChain.outerRule(new RunnableTestRuleAdapter(MyTests::generateTestFiles)).around(cluster);
+ * }</pre>
+ */
 public class RunnableTestRuleAdapter implements TestRule {
 
     private CheckedRunnable<Exception> runnable;

fb033b9e829e892d9cdb0dc9e01e95e782121bf2
Andrei Dan <andrei.dan@elastic.co>
2023-02-17 08:55:40 +0000

    Move SchedulerEngine and TimeValueSchedule to server/common/scheduler (#93862)
    We built quite a bit of infrastructure to have one polling job
    running via the `SchedulerEngine` and `ActiveSchedule`. This moves this
    infrastructure outside x-pack to server so elasticsearch/modules can use
    it and avoid re-implementing it using `threadPool.schedule`.

--- server/src/test/java/org/elasticsearch/common/scheduler/SchedulerEngineTests.java
+++ server/src/test/java/org/elasticsearch/common/scheduler/SchedulerEngineTests.java
@@ -1,19 +1,20 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
-package org.elasticsearch.xpack.core.scheduler;
+package org.elasticsearch.common.scheduler;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.util.Supplier;
+import org.elasticsearch.common.scheduler.SchedulerEngine.ActiveSchedule;
+import org.elasticsearch.common.scheduler.SchedulerEngine.Job;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.core.Tuple;
 import org.elasticsearch.test.ESTestCase;
-import org.elasticsearch.xpack.core.scheduler.SchedulerEngine.ActiveSchedule;
-import org.elasticsearch.xpack.core.scheduler.SchedulerEngine.Job;
 import org.mockito.ArgumentCaptor;
 
 import java.time.Clock;

da3fa76eb57b9da30eae3dffa477d5981d3d9f55
Pooya Salehi <pooya.salehi@elastic.co>
2023-02-22 15:01:46 +0100

    clean up

--- server/src/main/java/org/elasticsearch/action/get/TransportGetAction.java
+++ server/src/main/java/org/elasticsearch/action/get/TransportGetAction.java
@@ -141,19 +141,22 @@ public class TransportGetAction extends TransportSingleShardAction<GetRequest, G
     }
 
     @Override
-    protected boolean ignoreShardCopyResponse(ShardRouting lastShardTried, GetResponse lastResponse) {
-        // TODO: double-check that request is real-time?
-        if (lastShardTried.isPromotableToPrimary()) {
-            return false;
+    protected boolean acceptResponse(ShardRouting shardRouting, GetResponse response) {
+        assert response != null;
+        if (shardRouting.isPromotableToPrimary()) {
+            return true;
         }
-        return lastResponse.isExists() == false;
+        return response.isExists();
     }
 
     @Override
     protected boolean skipShardCopy(ShardRouting shardRouting, GetResponse lastResponse, Exception lastFailure) {
-        return lastFailure == null
-            && lastResponse != null
-            && lastResponse.isExists() == false
-            && shardRouting.isPromotableToPrimary() == false;
+        // Never skip if the last shard copy returned failure or the one to try is promotable
+        if (shardRouting.isPromotableToPrimary() || lastFailure != null) {
+            return false;
+        }
+        assert shardRouting.isPromotableToPrimary() == false;
+        // If the last response is "not found", skip until the next shard copy to try is promotable.
+        return lastResponse != null && lastResponse.isExists() == false;
     }
 }

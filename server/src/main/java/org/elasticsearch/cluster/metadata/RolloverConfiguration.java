d0f7d3f0b7b1208da9586ed0adea36513abe85e6
gmarouli <mary.gouseti@elastic.co>
2023-02-22 15:02:19 +0200

    New RolloverConfiguration abstracts the RolloverAction configuration

---
+++ server/src/main/java/org/elasticsearch/cluster/metadata/RolloverConfiguration.java
@@ -0,0 +1,315 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+package org.elasticsearch.cluster.metadata;
+
+import org.elasticsearch.TransportVersion;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.core.Nullable;
+import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.xcontent.ConstructingObjectParser;
+import org.elasticsearch.xcontent.ObjectParser.ValueType;
+import org.elasticsearch.xcontent.ParseField;
+import org.elasticsearch.xcontent.ToXContent;
+import org.elasticsearch.xcontent.ToXContentObject;
+import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xcontent.XContentParser;
+
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * Contains the conditions that determine if an index can be rolled over or not. Mainly used in
+ * Index Lifecycle Management and Data Lifecycle Management.
+ */
+public class RolloverConfiguration implements Writeable, ToXContentObject {
+    public static final String NAME = "rollover";
+    public static final ParseField MAX_SIZE_FIELD = new ParseField("max_size");
+    public static final ParseField MAX_PRIMARY_SHARD_SIZE_FIELD = new ParseField("max_primary_shard_size");
+    public static final ParseField MAX_DOCS_FIELD = new ParseField("max_docs");
+    public static final ParseField MAX_AGE_FIELD = new ParseField("max_age");
+    public static final ParseField MAX_PRIMARY_SHARD_DOCS_FIELD = new ParseField("max_primary_shard_docs");
+    public static final ParseField MIN_SIZE_FIELD = new ParseField("min_size");
+    public static final ParseField MIN_PRIMARY_SHARD_SIZE_FIELD = new ParseField("min_primary_shard_size");
+    public static final ParseField MIN_DOCS_FIELD = new ParseField("min_docs");
+    public static final ParseField MIN_AGE_FIELD = new ParseField("min_age");
+    public static final ParseField MIN_PRIMARY_SHARD_DOCS_FIELD = new ParseField("min_primary_shard_docs");
+
+    private static final ConstructingObjectParser<RolloverConfiguration, Void> PARSER = new ConstructingObjectParser<>(
+        NAME,
+        a -> new RolloverConfiguration(
+            (ByteSizeValue) a[0],
+            (ByteSizeValue) a[1],
+            (TimeValue) a[2],
+            (Long) a[3],
+            (Long) a[4],
+            (ByteSizeValue) a[5],
+            (ByteSizeValue) a[6],
+            (TimeValue) a[7],
+            (Long) a[8],
+            (Long) a[9]
+        )
+    );
+
+    static {
+        PARSER.declareField(
+            ConstructingObjectParser.optionalConstructorArg(),
+            (p, c) -> ByteSizeValue.parseBytesSizeValue(p.text(), MAX_SIZE_FIELD.getPreferredName()),
+            MAX_SIZE_FIELD,
+            ValueType.VALUE
+        );
+        PARSER.declareField(
+            ConstructingObjectParser.optionalConstructorArg(),
+            (p, c) -> ByteSizeValue.parseBytesSizeValue(p.text(), MAX_PRIMARY_SHARD_SIZE_FIELD.getPreferredName()),
+            MAX_PRIMARY_SHARD_SIZE_FIELD,
+            ValueType.VALUE
+        );
+        PARSER.declareField(
+            ConstructingObjectParser.optionalConstructorArg(),
+            (p, c) -> TimeValue.parseTimeValue(p.text(), MAX_AGE_FIELD.getPreferredName()),
+            MAX_AGE_FIELD,
+            ValueType.VALUE
+        );
+        PARSER.declareLong(ConstructingObjectParser.optionalConstructorArg(), MAX_DOCS_FIELD);
+        PARSER.declareLong(ConstructingObjectParser.optionalConstructorArg(), MAX_PRIMARY_SHARD_DOCS_FIELD);
+        PARSER.declareField(
+            ConstructingObjectParser.optionalConstructorArg(),
+            (p, c) -> ByteSizeValue.parseBytesSizeValue(p.text(), MIN_SIZE_FIELD.getPreferredName()),
+            MIN_SIZE_FIELD,
+            ValueType.VALUE
+        );
+        PARSER.declareField(
+            ConstructingObjectParser.optionalConstructorArg(),
+            (p, c) -> ByteSizeValue.parseBytesSizeValue(p.text(), MIN_PRIMARY_SHARD_SIZE_FIELD.getPreferredName()),
+            MIN_PRIMARY_SHARD_SIZE_FIELD,
+            ValueType.VALUE
+        );
+        PARSER.declareField(
+            ConstructingObjectParser.optionalConstructorArg(),
+            (p, c) -> TimeValue.parseTimeValue(p.text(), MIN_AGE_FIELD.getPreferredName()),
+            MIN_AGE_FIELD,
+            ValueType.VALUE
+        );
+        PARSER.declareLong(ConstructingObjectParser.optionalConstructorArg(), MIN_DOCS_FIELD);
+        PARSER.declareLong(ConstructingObjectParser.optionalConstructorArg(), MIN_PRIMARY_SHARD_DOCS_FIELD);
+    }
+
+    private final ByteSizeValue maxSize;
+    private final ByteSizeValue maxPrimaryShardSize;
+    private final Long maxDocs;
+    private final TimeValue maxAge;
+    private final Long maxPrimaryShardDocs;
+    private final ByteSizeValue minSize;
+    private final ByteSizeValue minPrimaryShardSize;
+    private final Long minDocs;
+    private final TimeValue minAge;
+    private final Long minPrimaryShardDocs;
+
+    public static RolloverConfiguration parse(XContentParser parser) {
+        return PARSER.apply(parser, null);
+    }
+
+    public RolloverConfiguration(
+        @Nullable ByteSizeValue maxSize,
+        @Nullable ByteSizeValue maxPrimaryShardSize,
+        @Nullable TimeValue maxAge,
+        @Nullable Long maxDocs,
+        @Nullable Long maxPrimaryShardDocs,
+        @Nullable ByteSizeValue minSize,
+        @Nullable ByteSizeValue minPrimaryShardSize,
+        @Nullable TimeValue minAge,
+        @Nullable Long minDocs,
+        @Nullable Long minPrimaryShardDocs
+    ) {
+        if (maxSize == null && maxPrimaryShardSize == null && maxAge == null && maxDocs == null && maxPrimaryShardDocs == null) {
+            throw new IllegalArgumentException("At least one max_* rollover condition must be set.");
+        }
+
+        this.maxSize = maxSize;
+        this.maxPrimaryShardSize = maxPrimaryShardSize;
+        this.maxAge = maxAge;
+        this.maxDocs = maxDocs;
+        this.maxPrimaryShardDocs = maxPrimaryShardDocs;
+
+        this.minSize = minSize;
+        this.minPrimaryShardSize = minPrimaryShardSize;
+        this.minAge = minAge;
+        this.minDocs = minDocs;
+        this.minPrimaryShardDocs = minPrimaryShardDocs;
+    }
+
+    public RolloverConfiguration(StreamInput in) throws IOException {
+        maxSize = in.readOptionalWriteable(ByteSizeValue::readFrom);
+        maxPrimaryShardSize = in.readOptionalWriteable(ByteSizeValue::readFrom);
+        maxAge = in.readOptionalTimeValue();
+        maxDocs = in.readOptionalVLong();
+        if (in.getTransportVersion().onOrAfter(TransportVersion.V_8_2_0)) {
+            maxPrimaryShardDocs = in.readOptionalVLong();
+        } else {
+            maxPrimaryShardDocs = null;
+        }
+        if (in.getTransportVersion().onOrAfter(TransportVersion.V_8_4_0)) {
+            minSize = in.readOptionalWriteable(ByteSizeValue::readFrom);
+            minPrimaryShardSize = in.readOptionalWriteable(ByteSizeValue::readFrom);
+            minAge = in.readOptionalTimeValue();
+            minDocs = in.readOptionalVLong();
+            minPrimaryShardDocs = in.readOptionalVLong();
+        } else {
+            minSize = null;
+            minPrimaryShardSize = null;
+            minAge = null;
+            minDocs = null;
+            minPrimaryShardDocs = null;
+        }
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        out.writeOptionalWriteable(maxSize);
+        out.writeOptionalWriteable(maxPrimaryShardSize);
+        out.writeOptionalTimeValue(maxAge);
+        out.writeOptionalVLong(maxDocs);
+        if (out.getTransportVersion().onOrAfter(TransportVersion.V_8_2_0)) {
+            out.writeOptionalVLong(maxPrimaryShardDocs);
+        }
+        if (out.getTransportVersion().onOrAfter(TransportVersion.V_8_4_0)) {
+            out.writeOptionalWriteable(minSize);
+            out.writeOptionalWriteable(minPrimaryShardSize);
+            out.writeOptionalTimeValue(minAge);
+            out.writeOptionalVLong(minDocs);
+            out.writeOptionalVLong(minPrimaryShardDocs);
+        }
+    }
+
+    public ByteSizeValue getMaxSize() {
+        return maxSize;
+    }
+
+    public ByteSizeValue getMaxPrimaryShardSize() {
+        return maxPrimaryShardSize;
+    }
+
+    public TimeValue getMaxAge() {
+        return maxAge;
+    }
+
+    public Long getMaxDocs() {
+        return maxDocs;
+    }
+
+    public Long getMaxPrimaryShardDocs() {
+        return maxPrimaryShardDocs;
+    }
+
+    public ByteSizeValue getMinSize() {
+        return minSize;
+    }
+
+    public ByteSizeValue getMinPrimaryShardSize() {
+        return minPrimaryShardSize;
+    }
+
+    public TimeValue getMinAge() {
+        return minAge;
+    }
+
+    public Long getMinDocs() {
+        return minDocs;
+    }
+
+    public Long getMinPrimaryShardDocs() {
+        return minPrimaryShardDocs;
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, ToXContent.Params params) throws IOException {
+        builder.startObject();
+        if (maxSize != null) {
+            builder.field(MAX_SIZE_FIELD.getPreferredName(), maxSize.getStringRep());
+        }
+        if (maxPrimaryShardSize != null) {
+            builder.field(MAX_PRIMARY_SHARD_SIZE_FIELD.getPreferredName(), maxPrimaryShardSize.getStringRep());
+        }
+        if (maxAge != null) {
+            builder.field(MAX_AGE_FIELD.getPreferredName(), maxAge.getStringRep());
+        }
+        if (maxDocs != null) {
+            builder.field(MAX_DOCS_FIELD.getPreferredName(), maxDocs);
+        }
+        if (maxPrimaryShardDocs != null) {
+            builder.field(MAX_PRIMARY_SHARD_DOCS_FIELD.getPreferredName(), maxPrimaryShardDocs);
+        }
+        if (minSize != null) {
+            builder.field(MIN_SIZE_FIELD.getPreferredName(), minSize.getStringRep());
+        }
+        if (minPrimaryShardSize != null) {
+            builder.field(MIN_PRIMARY_SHARD_SIZE_FIELD.getPreferredName(), minPrimaryShardSize.getStringRep());
+        }
+        if (minAge != null) {
+            builder.field(MIN_AGE_FIELD.getPreferredName(), minAge.getStringRep());
+        }
+        if (minDocs != null) {
+            builder.field(MIN_DOCS_FIELD.getPreferredName(), minDocs);
+        }
+        if (minPrimaryShardDocs != null) {
+            builder.field(MIN_PRIMARY_SHARD_DOCS_FIELD.getPreferredName(), minPrimaryShardDocs);
+        }
+        builder.endObject();
+        return builder;
+    }
+
+    public static RolloverConfiguration fromXContent(XContentParser parser) throws IOException {
+        return PARSER.parse(parser, null);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+            maxSize,
+            maxPrimaryShardSize,
+            maxAge,
+            maxDocs,
+            maxPrimaryShardDocs,
+            minSize,
+            minPrimaryShardSize,
+            minAge,
+            minDocs,
+            minPrimaryShardDocs
+        );
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        RolloverConfiguration other = (RolloverConfiguration) obj;
+        return Objects.equals(maxSize, other.maxSize)
+            && Objects.equals(maxPrimaryShardSize, other.maxPrimaryShardSize)
+            && Objects.equals(maxAge, other.maxAge)
+            && Objects.equals(maxDocs, other.maxDocs)
+            && Objects.equals(maxPrimaryShardDocs, other.maxPrimaryShardDocs)
+            && Objects.equals(minSize, other.minSize)
+            && Objects.equals(minPrimaryShardSize, other.minPrimaryShardSize)
+            && Objects.equals(minAge, other.minAge)
+            && Objects.equals(minDocs, other.minDocs)
+            && Objects.equals(minPrimaryShardDocs, other.minPrimaryShardDocs);
+    }
+
+    @Override
+    public String toString() {
+        return Strings.toString(this);
+    }
+
+}
---
+++ server/src/test/java/org/elasticsearch/cluster/metadata/RolloverConfigurationTests.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.cluster.metadata;
+
+import org.elasticsearch.TransportVersion;
+import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.test.AbstractXContentSerializingTestCase;
+import org.elasticsearch.xcontent.XContentParser;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+
+public class RolloverConfigurationTests extends AbstractXContentSerializingTestCase<RolloverConfiguration> {
+
+    @Override
+    protected Writeable.Reader<RolloverConfiguration> instanceReader() {
+        return RolloverConfiguration::new;
+    }
+
+    @Override
+    protected RolloverConfiguration createTestInstance() {
+        return randomInstance();
+    }
+
+    public static RolloverConfiguration randomInstance() {
+        ByteSizeUnit maxSizeUnit = randomFrom(ByteSizeUnit.values());
+        ByteSizeValue maxSize = randomBoolean() ? null : new ByteSizeValue(randomNonNegativeLong() / maxSizeUnit.toBytes(1), maxSizeUnit);
+        ByteSizeUnit maxPrimaryShardSizeUnit = randomFrom(ByteSizeUnit.values());
+        ByteSizeValue maxPrimaryShardSize = randomBoolean()
+            ? null
+            : new ByteSizeValue(randomNonNegativeLong() / maxPrimaryShardSizeUnit.toBytes(1), maxPrimaryShardSizeUnit);
+        Long maxDocs = randomBoolean() ? null : randomNonNegativeLong();
+        TimeValue maxAge = (maxDocs == null && maxSize == null || randomBoolean())
+            ? TimeValue.parseTimeValue(randomPositiveTimeValue(), "rollover_action_test")
+            : null;
+        Long maxPrimaryShardDocs = (maxSize == null && maxPrimaryShardSize == null && maxAge == null && maxDocs == null || randomBoolean())
+            ? randomNonNegativeLong()
+            : null;
+        ByteSizeUnit minSizeUnit = randomFrom(ByteSizeUnit.values());
+        ByteSizeValue minSize = randomBoolean() ? null : new ByteSizeValue(randomNonNegativeLong() / minSizeUnit.toBytes(1), minSizeUnit);
+        ByteSizeUnit minPrimaryShardSizeUnit = randomFrom(ByteSizeUnit.values());
+        ByteSizeValue minPrimaryShardSize = randomBoolean()
+            ? null
+            : new ByteSizeValue(randomNonNegativeLong() / minPrimaryShardSizeUnit.toBytes(1), minPrimaryShardSizeUnit);
+        Long minDocs = randomBoolean() ? null : randomNonNegativeLong();
+        TimeValue minAge = (minDocs == null || randomBoolean())
+            ? TimeValue.parseTimeValue(randomPositiveTimeValue(), "rollover_action_test")
+            : null;
+        Long minPrimaryShardDocs = (minSize == null && minPrimaryShardSize == null && minAge == null && minDocs == null || randomBoolean())
+            ? randomNonNegativeLong()
+            : null;
+        return
+            new RolloverConfiguration(
+                maxSize,
+                maxPrimaryShardSize,
+                maxAge,
+                maxDocs,
+                maxPrimaryShardDocs,
+                minSize,
+                minPrimaryShardSize,
+                minAge,
+                minDocs,
+                minPrimaryShardDocs
+        );
+    }
+
+    @Override
+    protected RolloverConfiguration mutateInstance(RolloverConfiguration instance) {
+        ByteSizeValue maxSize = instance.getMaxSize();
+        ByteSizeValue maxPrimaryShardSize = instance.getMaxPrimaryShardSize();
+        TimeValue maxAge = instance.getMaxAge();
+        Long maxDocs = instance.getMaxDocs();
+        Long maxPrimaryShardDocs = instance.getMaxPrimaryShardDocs();
+        ByteSizeValue minSize = instance.getMinSize();
+        ByteSizeValue minPrimaryShardSize = instance.getMinPrimaryShardSize();
+        TimeValue minAge = instance.getMinAge();
+        Long minDocs = instance.getMinDocs();
+        Long minPrimaryShardDocs = instance.getMinPrimaryShardDocs();
+        switch (between(0, 9)) {
+            case 0 -> maxSize = randomValueOtherThan(maxSize, () -> {
+                ByteSizeUnit maxSizeUnit = randomFrom(ByteSizeUnit.values());
+                return new ByteSizeValue(randomNonNegativeLong() / maxSizeUnit.toBytes(1), maxSizeUnit);
+            });
+            case 1 -> maxPrimaryShardSize = randomValueOtherThan(maxPrimaryShardSize, () -> {
+                ByteSizeUnit maxPrimaryShardSizeUnit = randomFrom(ByteSizeUnit.values());
+                return new ByteSizeValue(randomNonNegativeLong() / maxPrimaryShardSizeUnit.toBytes(1), maxPrimaryShardSizeUnit);
+            });
+            case 2 -> maxAge = randomValueOtherThan(
+                maxAge,
+                () -> TimeValue.parseTimeValue(randomPositiveTimeValue(), "rollover_action_test")
+            );
+            case 3 -> maxDocs = maxDocs == null ? randomNonNegativeLong() : maxDocs + 1;
+            case 4 -> maxPrimaryShardDocs = maxPrimaryShardDocs == null ? randomNonNegativeLong() : maxPrimaryShardDocs + 1;
+            case 5 -> minSize = randomValueOtherThan(minSize, () -> {
+                ByteSizeUnit minSizeUnit = randomFrom(ByteSizeUnit.values());
+                return new ByteSizeValue(randomNonNegativeLong() / minSizeUnit.toBytes(1), minSizeUnit);
+            });
+            case 6 -> minPrimaryShardSize = randomValueOtherThan(minPrimaryShardSize, () -> {
+                ByteSizeUnit minPrimaryShardSizeUnit = randomFrom(ByteSizeUnit.values());
+                return new ByteSizeValue(randomNonNegativeLong() / minPrimaryShardSizeUnit.toBytes(1), minPrimaryShardSizeUnit);
+            });
+            case 7 -> minAge = randomValueOtherThan(
+                minAge,
+                () -> TimeValue.parseTimeValue(randomPositiveTimeValue(), "rollover_action_test")
+            );
+            case 8 -> minDocs = minDocs == null ? randomNonNegativeLong() : minDocs + 1;
+            case 9 -> minPrimaryShardDocs = minPrimaryShardDocs == null ? randomNonNegativeLong() : minPrimaryShardDocs + 1;
+            default -> throw new AssertionError("Illegal randomisation branch");
+        }
+        return
+            new RolloverConfiguration(
+                maxSize,
+                maxPrimaryShardSize,
+                maxAge,
+                maxDocs,
+                maxPrimaryShardDocs,
+                minSize,
+                minPrimaryShardSize,
+                minAge,
+                minDocs,
+                minPrimaryShardDocs
+        );
+    }
+
+    public void testNoConditions() {
+        IllegalArgumentException exception = expectThrows(
+            IllegalArgumentException.class,
+            () -> new RolloverConfiguration(null, null, null, null, null, null, null, null, null, null)
+        );
+        assertEquals("At least one max_* rollover condition must be set.", exception.getMessage());
+    }
+
+    public void testBwcSerializationWithMaxPrimaryShardDocs() throws Exception {
+        // In case of serializing to node with older version, replace maxPrimaryShardDocs with maxDocs.
+        RolloverConfiguration instance = new RolloverConfiguration(null, null, null, null, 1L, null, null, null, null, null);
+        RolloverConfiguration deserializedInstance = copyInstance(instance, TransportVersion.V_8_1_0);
+        assertThat(deserializedInstance.getMaxPrimaryShardDocs(), nullValue());
+
+        // But not if maxDocs is also specified:
+        instance = new RolloverConfiguration(null, null, null, 2L, 1L, null, null, null, null, null);
+        deserializedInstance = copyInstance(instance, TransportVersion.V_8_1_0);
+        assertThat(deserializedInstance.getMaxPrimaryShardDocs(), nullValue());
+        assertThat(deserializedInstance.getMaxDocs(), equalTo(instance.getMaxDocs()));
+    }
+
+    @Override
+    protected RolloverConfiguration doParseInstance(XContentParser parser) throws IOException {
+        return RolloverConfiguration.fromXContent(parser);
+    }
+}

db2958ade47b45f2aa9a2f52ccda5f3525316081
Slobodan Adamovic <slobodan.adamovic@elastic.co>
2023-02-24 16:05:36 +0100

    Test using ApiKey for remote access authentication against FC

--- x-pack/plugin/security/qa/multi-cluster/src/javaRestTest/java/org/elasticsearch/xpack/remotecluster/RemoteClusterSecurityRestIT.java
+++ x-pack/plugin/security/qa/multi-cluster/src/javaRestTest/java/org/elasticsearch/xpack/remotecluster/RemoteClusterSecurityRestIT.java
@@ -17,6 +17,7 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.core.Strings;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.test.cluster.ElasticsearchCluster;
+import org.elasticsearch.test.rest.ObjectPath;
 import org.junit.ClassRule;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
@@ -32,6 +33,7 @@ import java.util.stream.Collectors;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class RemoteClusterSecurityRestIT extends AbstractRemoteClusterSecurityTestCase {
 
@@ -95,6 +97,7 @@ public class RemoteClusterSecurityRestIT extends AbstractRemoteClusterSecurityTe
             final var putRoleRequest = new Request("PUT", "/_security/role/" + REMOTE_SEARCH_ROLE);
             putRoleRequest.setJsonEntity("""
                 {
+                  "cluster": ["manage_own_api_key"],
                   "indices": [
                     {
                       "names": ["local_index"],
@@ -118,6 +121,43 @@ public class RemoteClusterSecurityRestIT extends AbstractRemoteClusterSecurityTe
                 }""");
             assertOK(adminClient().performRequest(putUserRequest));
 
+            // Create API key (with REMOTE_SEARCH_USER as owner) which can be used for remote cluster search
+            final var createApiKeyRequest = new Request("PUT", "/_security/api_key");
+            // Note: index2 is added to remote_indices for the API key,
+            // but it should be ignored when intersected since owner user does not have access to it.
+            createApiKeyRequest.setJsonEntity(randomBoolean() ? """
+                {
+                  "name": "qc_api_key_with_remote_access",
+                  "role_descriptors": {
+                    "my_remote_access_role": {
+                      "indices": [
+                        {
+                          "names": ["local_index"],
+                          "privileges": ["read"]
+                        }
+                      ],
+                      "remote_indices": [
+                        {
+                          "names": ["index1", "not_found_index", "prefixed_index", "index2"],
+                          "privileges": ["read", "read_cross_cluster"],
+                          "clusters": ["my_remote_*", "non_existing_remote_cluster"]
+                        }
+                      ]
+                    }
+                  }
+                }""" : """
+                {
+                  "name": "qc_api_key_with_remote_access",
+                  "role_descriptors": {}
+                }""");
+            final var createApiKeyResponse = performRequestWithRemoteAccessUser(createApiKeyRequest);
+            assertOK(createApiKeyResponse);
+            var createApiKeyResponsePath = ObjectPath.createFromResponse(createApiKeyResponse);
+            final String apiKeyEncoded = createApiKeyResponsePath.evaluate("encoded");
+            final String apiKeyId = createApiKeyResponsePath.evaluate("id");
+            assertThat(apiKeyEncoded, notNullValue());
+            assertThat(apiKeyId, notNullValue());
+
             // Check that we can search the fulfilling cluster from the querying cluster
             final boolean alsoSearchLocally = randomBoolean();
             final var searchRequest = new Request(
@@ -131,7 +171,10 @@ public class RemoteClusterSecurityRestIT extends AbstractRemoteClusterSecurityTe
                     randomBoolean()
                 )
             );
-            final Response response = performRequestWithRemoteAccessUser(searchRequest);
+            final String sendRequestWith = randomFrom("user", "apikey");
+            final Response response = sendRequestWith.equals("user")
+                ? performRequestWithRemoteAccessUser(searchRequest)
+                : performRequestWithApiKey(searchRequest, apiKeyEncoded);
             assertOK(response);
             final SearchResponse searchResponse = SearchResponse.fromXContent(responseAsParser(response));
             final List<String> actualIndices = Arrays.stream(searchResponse.getHits().getHits())
@@ -143,42 +186,69 @@ public class RemoteClusterSecurityRestIT extends AbstractRemoteClusterSecurityTe
                 assertThat(actualIndices, containsInAnyOrder("index1"));
             }
 
-            // Check that access is denied because of user privileges
+            // Check that access is denied because of user/API key privileges
+            final Request index2SearchRequest = new Request("GET", "/my_remote_cluster:index2/_search");
             final ResponseException exception = expectThrows(
                 ResponseException.class,
-                () -> performRequestWithRemoteAccessUser(new Request("GET", "/my_remote_cluster:index2/_search"))
+                sendRequestWith.equals("user")
+                    ? () -> performRequestWithRemoteAccessUser(index2SearchRequest)
+                    : () -> performRequestWithApiKey(index2SearchRequest, apiKeyEncoded)
             );
             assertThat(exception.getResponse().getStatusLine().getStatusCode(), equalTo(403));
             assertThat(
                 exception.getMessage(),
-                containsString(
-                    "action [indices:data/read/search] towards remote cluster is unauthorized for user [remote_search_user] "
-                        + "with assigned roles [remote_search] authenticated by API key id ["
-                        + remoteAccessApiKeyId
-                        + "] of user [test_user] on indices [index2]"
-                )
+                sendRequestWith.equals("user")
+                    ? containsString(
+                        "action [indices:data/read/search] towards remote cluster is unauthorized for user [remote_search_user] "
+                            + "with assigned roles [remote_search] authenticated by API key id ["
+                            + remoteAccessApiKeyId
+                            + "] of user [test_user] on indices [index2]"
+                    )
+                    : containsString(
+                        "action [indices:data/read/search] towards remote cluster is unauthorized for API key id ["
+                            + apiKeyId
+                            + "] of user ["
+                            + REMOTE_SEARCH_USER
+                            + "] authenticated by API key id ["
+                            + remoteAccessApiKeyId
+                            + "] of user [test_user] on indices [index2]"
+                    )
             );
 
             // Check that access is denied because of API key privileges
+            final Request prefixedIndexSearchRequest = new Request("GET", "/my_remote_cluster:prefixed_index/_search");
             final ResponseException exception2 = expectThrows(
                 ResponseException.class,
-                () -> performRequestWithRemoteAccessUser(new Request("GET", "/my_remote_cluster:prefixed_index/_search"))
+                sendRequestWith.equals("user")
+                    ? () -> performRequestWithRemoteAccessUser(prefixedIndexSearchRequest)
+                    : () -> performRequestWithApiKey(prefixedIndexSearchRequest, apiKeyEncoded)
             );
             assertThat(exception2.getResponse().getStatusLine().getStatusCode(), equalTo(403));
             assertThat(
                 exception2.getMessage(),
-                containsString(
-                    "action [indices:data/read/search] towards remote cluster is unauthorized for user [remote_search_user] "
-                        + "with assigned roles [remote_search] authenticated by API key id ["
-                        + remoteAccessApiKeyId
-                        + "] of user [test_user] on indices [prefixed_index]"
-                )
+                sendRequestWith.equals("user")
+                    ? containsString(
+                        "action [indices:data/read/search] towards remote cluster is unauthorized for user [remote_search_user] "
+                            + "with assigned roles [remote_search] authenticated by API key id ["
+                            + remoteAccessApiKeyId
+                            + "] of user [test_user] on indices [prefixed_index]"
+                    )
+                    : containsString(
+                        "action [indices:data/read/search] towards remote cluster is unauthorized for API key id ["
+                            + apiKeyId
+                            + "] of user ["
+                            + REMOTE_SEARCH_USER
+                            + "] authenticated by API key id ["
+                            + remoteAccessApiKeyId
+                            + "] of user [test_user] on indices [prefixed_index]"
+                    )
             );
 
             // Check access is denied when user has no remote indices privileges
             final var putLocalSearchRoleRequest = new Request("PUT", "/_security/role/local_search");
             putLocalSearchRoleRequest.setJsonEntity(Strings.format("""
                 {
+                  "cluster": ["manage_own_api_key"],
                   "indices": [
                     {
                       "names": ["local_index"],
@@ -202,31 +272,89 @@ public class RemoteClusterSecurityRestIT extends AbstractRemoteClusterSecurityTe
                   "roles" : ["local_search"]
                 }""");
             assertOK(adminClient().performRequest(putlocalSearchUserRequest));
+            final var createLocalApiKeyRequest = new Request("PUT", "/_security/api_key");
+            String localApiKeyRoleDescriptors = Strings.format("""
+                  "my_local_access_role": {
+                      "indices": [
+                        {
+                          "names": ["local_index"],
+                          "privileges": ["read"]
+                        }
+                      ]%s
+                  }
+                """, randomBoolean() ? "" : """
+                ,
+                "remote_indices": [
+                   {
+                     "names": ["*"],
+                     "privileges": ["read", "read_cross_cluster"],
+                     "clusters": ["other_remote_*"]
+                   }
+                 ]""");
+            createLocalApiKeyRequest.setJsonEntity(Strings.format("""
+                {
+                  "name": "qc_api_key_with_remote_access",
+                  "role_descriptors": { %s }
+                }""", randomBoolean() ? localApiKeyRoleDescriptors : ""));
+            final var createLocalApiKeyResponse = performRequestWithLocalSearchUser(createLocalApiKeyRequest);
+            assertOK(createApiKeyResponse);
+            var createLocalApiKeyResponsePath = ObjectPath.createFromResponse(createLocalApiKeyResponse);
+            final String localApiKeyEncoded = createLocalApiKeyResponsePath.evaluate("encoded");
+            final String localApiKeyId = createLocalApiKeyResponsePath.evaluate("id");
+            assertThat(localApiKeyEncoded, notNullValue());
+            assertThat(localApiKeyId, notNullValue());
+
+            final Request randomRemoteSearch = new Request(
+                "GET",
+                "/" + randomFrom("my_remote_cluster:*", "*:*", "*,*:*", "my_*:*,local_index") + "/_search"
+            );
             final ResponseException exception3 = expectThrows(
                 ResponseException.class,
-                () -> performRequestWithLocalSearchUser(
-                    new Request("GET", "/" + randomFrom("my_remote_cluster:*", "*:*", "*,*:*", "my_*:*,local_index") + "/_search")
-                )
+                sendRequestWith.equals("user")
+                    ? () -> performRequestWithLocalSearchUser(randomRemoteSearch)
+                    : () -> performRequestWithApiKey(randomRemoteSearch, localApiKeyEncoded)
             );
             assertThat(exception3.getResponse().getStatusLine().getStatusCode(), equalTo(403));
             assertThat(
                 exception3.getMessage(),
-                containsString(
-                    "action [indices:data/read/search] towards remote cluster [my_remote_cluster]"
-                        + " is unauthorized for user [local_search_user] with effective roles [local_search]"
-                        + " because no remote indices privileges apply for the target cluster"
-                )
+                sendRequestWith.equals("user")
+                    ? containsString(
+                        "action [indices:data/read/search] towards remote cluster [my_remote_cluster]"
+                            + " is unauthorized for user [local_search_user] with effective roles [local_search]"
+                            + " because no remote indices privileges apply for the target cluster"
+                    )
+                    : containsString(
+                        "action [indices:data/read/search] towards remote cluster [my_remote_cluster] "
+                            + "is unauthorized for API key id ["
+                            + localApiKeyId
+                            + "] of user [local_search_user] because no remote indices privileges apply for the target cluster"
+                    )
             );
 
             // Check that authentication fails if we use a non-existent API key
-            updateClusterSettings(Settings.builder().put("cluster.remote.my_remote_cluster.authorization", randomEncodedApiKey()).build());
+            updateClusterSettings(
+                Settings.builder()
+                    .put(
+                        "cluster.remote.my_remote_cluster.authorization",
+                        randomValueOtherThan(remoteAccessApiKeyId, () -> randomEncodedApiKey())
+                    )
+                    .build()
+            );
             final ResponseException exception4 = expectThrows(
                 ResponseException.class,
-                () -> performRequestWithRemoteAccessUser(new Request("GET", "/my_remote_cluster:index1/_search"))
+                sendRequestWith.equals("user")
+                    ? () -> performRequestWithRemoteAccessUser(new Request("GET", "/my_remote_cluster:index1/_search"))
+                    : () -> performRequestWithApiKey(new Request("GET", "/my_remote_cluster:index1/_search"), localApiKeyEncoded)
             );
-            assertThat(exception4.getResponse().getStatusLine().getStatusCode(), equalTo(401));
-            assertThat(exception4.getMessage(), containsString("unable to authenticate user"));
-            assertThat(exception4.getMessage(), containsString("unable to find apikey"));
+            if (sendRequestWith.equals("user")) {
+                assertThat(exception4.getResponse().getStatusLine().getStatusCode(), equalTo(401));
+                assertThat(exception4.getMessage(), containsString("unable to authenticate user"));
+                assertThat(exception4.getMessage(), containsString("unable to find apikey"));
+            } else {
+                assertThat(exception4.getResponse().getStatusLine().getStatusCode(), equalTo(403));
+                assertThat(exception4.getMessage(), containsString("unauthorized for API key id [" + localApiKeyId + "]"));
+                assertThat(exception4.getMessage(), containsString("no remote indices privileges apply for the target cluster"));
+            }
         }
     }
 
@@ -237,6 +365,11 @@ public class RemoteClusterSecurityRestIT extends AbstractRemoteClusterSecurityTe
 
     private Response performRequestWithLocalSearchUser(final Request request) throws IOException {
         request.setOptions(RequestOptions.DEFAULT.toBuilder().addHeader("Authorization", basicAuthHeaderValue("local_search_user", PASS)));
+        return client().performRequest(request);
+    }
+
+    private Response performRequestWithApiKey(final Request request, final String encoded) throws IOException {
+        request.setOptions(RequestOptions.DEFAULT.toBuilder().addHeader("Authorization", "ApiKey " + encoded));
         return client().performRequest(request);
     }
 

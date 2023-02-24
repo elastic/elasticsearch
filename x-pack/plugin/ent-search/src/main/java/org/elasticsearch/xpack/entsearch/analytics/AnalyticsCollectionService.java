d138e51651db8cee0e056339a250096fb5ae5a01
Aurelien FOUCRET <aurelien.foucret@elastic.co>
2023-02-24 17:39:37 +0100

    Boostraping a service to manage analytics collection.

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/entsearch/analytics/AnalyticsCollectionService.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/entsearch/analytics/AnalyticsCollectionService.java
@@ -10,7 +10,9 @@ package org.elasticsearch.xpack.entsearch.analytics;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.client.internal.Client;
 import org.elasticsearch.client.internal.OriginSettingClient;
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.inject.Inject;
 
 import static org.elasticsearch.xpack.core.ClientHelper.ENT_SEARCH_ORIGIN;
 
@@ -20,24 +22,46 @@ public class AnalyticsCollectionService {
 
     private final ClusterService clusterService;
 
-    public AnalyticsCollectionService(Client client, ClusterService clusterService) {
+    private final IndexNameExpressionResolver indexNameExpressionResolver;
+
+    @Inject
+    public AnalyticsCollectionService(
+        Client client,
+        ClusterService clusterService,
+        IndexNameExpressionResolver indexNameExpressionResolver
+    ) {
         this.clientWithOrigin = new OriginSettingClient(client, ENT_SEARCH_ORIGIN);
         this.clusterService = clusterService;
+        this.indexNameExpressionResolver = indexNameExpressionResolver;
     }
 
+    /**
+     * Retrieve an analytics collection by name {@AnalyticsCollection}
+     *
+     * @param collectionName {@AnalyticsCollection} name.
+     * @param listener The action listener to invoke on response/failure.
+     */
     public void getAnalyticsCollection(String collectionName, ActionListener<AnalyticsCollection> listener) {
 
     }
 
-    public void putAnalyticsCollection(AnalyticsCollection analyticsCollection, ActionListener<AnalyticsCollection> listener) {
+    /**
+     * Create a new {@AnalyticsCollection}
+     *
+     * @param analyticsCollection {@AnalyticsCollection} to be created.
+     * @param listener The action listener to invoke on response/failure.
+     */
+    public void createAnalyticsCollection(AnalyticsCollection analyticsCollection, ActionListener<AnalyticsCollection> listener) {
 
     }
 
-    public void deleteAnalyticsCollection(ActionListener<AnalyticsCollection> listener) {
-
-    }
-
-    public void listAnalyticsCollection(ActionListener<AnalyticsCollection> listener) {
+    /**
+     * Delete an analytics collection by name {@AnalyticsCollection}
+     *
+     * @param collectionName {@AnalyticsCollection} name.
+     * @param listener The action listener to invoke on response/failure.
+     */
+    public void deleteAnalyticsCollection(String collectionName, ActionListener<AnalyticsCollection> listener) {
 
     }
 }

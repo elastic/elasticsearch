19c741c380ff109a232d37d8f9ae7332cb6c4343
Albert Zaharovits <albert.zaharovits@elastic.co>
2023-02-24 15:58:34 +0200

    x-pack/plugin/core/src/main/java/module-info.java

--- x-pack/plugin/core/src/main/java/module-info.java
+++ x-pack/plugin/core/src/main/java/module-info.java
@@ -154,7 +154,6 @@ module org.elasticsearch.xcore {
     exports org.elasticsearch.xpack.core.security.authz.store;
     exports org.elasticsearch.xpack.core.security.authz.support;
     exports org.elasticsearch.xpack.core.security.authz;
-    exports org.elasticsearch.xpack.core.security.index;
     exports org.elasticsearch.xpack.core.security.support;
     exports org.elasticsearch.xpack.core.security.user;
     exports org.elasticsearch.xpack.core.security.xcontent;

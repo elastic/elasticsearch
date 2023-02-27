196590f19599484109f495d994603f3a4c3a493f
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2023-02-27 09:52:43 +0100

    Missing assert

--- server/src/main/java/org/elasticsearch/cluster/coordination/CoordinationState.java
+++ server/src/main/java/org/elasticsearch/cluster/coordination/CoordinationState.java
@@ -345,8 +345,8 @@ public class CoordinationState {
             throw new CoordinationStateRejectedException("only allow reconfiguration if joinVotes have quorum for new config");
         }
 
-        // assert clusterState.getLastCommittedConfiguration().equals(getLastCommittedConfiguration())
-        // : "last committed configuration should not change";
+        assert clusterState.getLastCommittedConfiguration().equals(getLastCommittedConfiguration()) || REGISTER_COORDINATION_MODE_ENABLED
+            : "last committed configuration should not change";
 
         lastPublishedVersion = clusterState.version();
         lastPublishedConfiguration = clusterState.getLastAcceptedConfiguration();

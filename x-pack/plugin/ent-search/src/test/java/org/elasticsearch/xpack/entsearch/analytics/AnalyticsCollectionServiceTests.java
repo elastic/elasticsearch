e42282e9fa1c34c8db0d85c6e1548013d761c80b
Aurelien FOUCRET <aurelien.foucret@elastic.co>
2023-02-28 17:28:47 +0100

    Finishing tests implementation

--- x-pack/plugin/ent-search/src/test/java/org/elasticsearch/xpack/entsearch/analytics/AnalyticsCollectionServiceTests.java
+++ x-pack/plugin/ent-search/src/test/java/org/elasticsearch/xpack/entsearch/analytics/AnalyticsCollectionServiceTests.java
@@ -7,10 +7,14 @@
 
 package org.elasticsearch.xpack.entsearch.analytics;
 
+import org.elasticsearch.ResourceAlreadyExistsException;
+import org.elasticsearch.ResourceNotFoundException;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.datastreams.CreateDataStreamAction;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.junit.Before;
 
+import java.util.Locale;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -18,20 +22,79 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 
 public class AnalyticsCollectionServiceTests extends AnalyticsTestCase {
     private static final int NUM_COLLECTIONS = 10;
 
     @Before
-    public void setup() throws Exception {
+    public void setupDataStreams() throws Exception {
         for (int i = 0; i < NUM_COLLECTIONS; i++) {
             createDataStream(new AnalyticsCollection("collection_" + i).getEventDataStream());
         }
     }
 
     public void testGetExistingAnalyticsCollection() throws Exception {
-        AnalyticsCollection analyticsCollection = awaitGetAnalyticsCollection("collection_1");
-        assertThat(analyticsCollection.getName(), equalTo("collection_1"));
+        String collectionName = "collection_" + random().nextInt(NUM_COLLECTIONS);
+        AnalyticsCollection analyticsCollection = awaitGetAnalyticsCollection(collectionName);
+        assertThat(analyticsCollection.getName(), equalTo(collectionName));
+    }
+
+    public void testGetMissingAnalyticsCollection() throws Exception {
+        ResourceNotFoundException e = expectThrows(
+            ResourceNotFoundException.class,
+            () -> { awaitGetAnalyticsCollection("not-a-collection-name"); }
+        );
+
+        assertThat(e.getMessage(), equalTo("not-a-collection-name"));
+    }
+
+    public void testCreateAnalyticsCollection() throws Exception {
+        String collectionName = randomAlphaOfLengthBetween(1, 10).toLowerCase(Locale.ROOT);
+
+        AnalyticsCollection analyticsCollection = awaitCreateAnalyticsCollection(new AnalyticsCollection(collectionName));
+        assertThat(analyticsCollection.getName(), equalTo(collectionName));
+
+        // Checking a data stream has been created for the analytics collection.
+        assertThat(clusterService().state().metadata().dataStreams(), hasKey(analyticsCollection.getEventDataStream()));
+
+        // Checking we can get the collection we have just created.
+        assertThat(awaitGetAnalyticsCollection(collectionName).getName(), equalTo(collectionName));
+    }
+
+    public void testCreateAlreadyExistingAnalyticsCollection() throws Exception {
+        String collectionName = "collection_" + random().nextInt(NUM_COLLECTIONS);
+
+        ResourceAlreadyExistsException e = expectThrows(
+            ResourceAlreadyExistsException.class,
+            () -> { awaitCreateAnalyticsCollection(new AnalyticsCollection(collectionName)); }
+        );
+
+        assertThat(e.getMessage(), equalTo(collectionName));
+    }
+
+    public void testDeleteAnalyticsCollection() throws Exception {
+        String collectionName = "collection_" + random().nextInt(NUM_COLLECTIONS);
+        String dataStreamName = awaitGetAnalyticsCollection(collectionName).getEventDataStream();
+
+        AcknowledgedResponse response = awaitDeleteAnalyticsCollection(collectionName);
+        assertThat(response.isAcknowledged(), equalTo(true));
+
+        // Checking that the underlying data stream has been deleted too.
+        assertThat(clusterService().state().metadata().dataStreams(), not(hasKey(dataStreamName)));
+
+        // Checking that the analytics collection is not accessible anymore.
+        expectThrows(ResourceNotFoundException.class, () -> awaitGetAnalyticsCollection(collectionName));
+    }
+
+    public void testDeleteMissingAnalyticsCollection() throws Exception {
+        ResourceNotFoundException e = expectThrows(
+            ResourceNotFoundException.class,
+            () -> { awaitDeleteAnalyticsCollection("not-a-collection-name"); }
+        );
+
+        assertThat(e.getMessage(), equalTo("not-a-collection-name"));
     }
 
     private void createDataStream(String dataStreamName) throws ExecutionException, InterruptedException {
@@ -39,17 +102,30 @@ public class AnalyticsCollectionServiceTests extends AnalyticsTestCase {
     }
 
     private AnalyticsCollection awaitGetAnalyticsCollection(String collectionName) throws Exception {
-        return new ResponseAwaiter<String, AnalyticsCollection>(analyticsCollectionService()::getAnalyticsCollection).get(collectionName);
+        Executor<String, AnalyticsCollection> executor = new Executor<>(analyticsCollectionService()::getAnalyticsCollection);
+        return executor.execute(collectionName);
     }
 
-    private static class ResponseAwaiter<T, R> {
+    private AnalyticsCollection awaitCreateAnalyticsCollection(AnalyticsCollection collection) throws Exception {
+        Executor<AnalyticsCollection, AnalyticsCollection> executor = new Executor<>(
+            analyticsCollectionService()::createAnalyticsCollection
+        );
+        return executor.execute(collection);
+    }
+
+    private AcknowledgedResponse awaitDeleteAnalyticsCollection(String collectionName) throws Exception {
+        Executor<String, AcknowledgedResponse> executor = new Executor<>(analyticsCollectionService()::deleteAnalyticsCollection);
+        return executor.execute(collectionName);
+    }
+
+    private static class Executor<T, R> {
         BiConsumer<T, ActionListener<R>> f;
 
-        public ResponseAwaiter(BiConsumer<T, ActionListener<R>> f) {
+        Executor(BiConsumer<T, ActionListener<R>> f) {
             this.f = f;
         }
 
-        public R get(T param) throws Exception {
+        public R execute(T param) throws Exception {
             CountDownLatch latch = new CountDownLatch(1);
             final AtomicReference<R> resp = new AtomicReference<>(null);
             final AtomicReference<Exception> exc = new AtomicReference<>(null);
--- x-pack/plugin/ent-search/src/test/java/org/elasticsearch/xpack/entsearch/analytics/AnalyticsTestCase.java
+++ x-pack/plugin/ent-search/src/test/java/org/elasticsearch/xpack/entsearch/analytics/AnalyticsTestCase.java
@@ -8,6 +8,9 @@
 package org.elasticsearch.xpack.entsearch.analytics;
 
 import org.elasticsearch.client.internal.Client;
+import org.elasticsearch.cluster.ClusterChangedEvent;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.ClusterStateListener;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.routing.allocation.AllocationService;
 import org.elasticsearch.cluster.service.ClusterService;
@@ -16,7 +19,6 @@ import org.elasticsearch.datastreams.DataStreamsPlugin;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.env.NodeEnvironment;
 import org.elasticsearch.plugins.Plugin;
-import org.elasticsearch.plugins.SystemIndexPlugin;
 import org.elasticsearch.repositories.RepositoriesService;
 import org.elasticsearch.script.ScriptService;
 import org.elasticsearch.test.ESSingleNodeTestCase;
@@ -26,19 +28,40 @@ import org.elasticsearch.watcher.ResourceWatcherService;
 import org.elasticsearch.xcontent.NamedXContentRegistry;
 import org.elasticsearch.xpack.core.XPackPlugin;
 import org.elasticsearch.xpack.ilm.IndexLifecycle;
-import org.junit.After;
+import org.junit.Before;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.CountDownLatch;
 import java.util.function.Supplier;
 
-public class AnalyticsTestCase extends ESSingleNodeTestCase {
+public abstract class AnalyticsTestCase extends ESSingleNodeTestCase {
+
+    @Before
+    public void setupTemplateRegistry() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        if (isTemplateRegistrySetup(clusterService().state())) {
+            latch.countDown();
+        } else {
+            clusterService().addListener(new ClusterStateListener() {
+                @Override
+                public void clusterChanged(ClusterChangedEvent event) {
+                    if (isTemplateRegistrySetup(event.state())) {
+                        latch.countDown();
+                    }
+                }
+            });
+        }
+
+        latch.await();
+    }
 
     @Override
-    @After
     public void tearDown() throws Exception {
         clusterService().removeListener(analyticsTemplateRegistry());
         super.tearDown();
+        clusterService().addListener(analyticsTemplateRegistry());
     }
 
     protected Collection<Class<? extends Plugin>> getPlugins() {
@@ -57,10 +80,14 @@ public class AnalyticsTestCase extends ESSingleNodeTestCase {
         return getInstanceFromNode(AnalyticsCollectionService.class);
     }
 
+    private boolean isTemplateRegistrySetup(ClusterState state) {
+        return state.metadata().templatesV2().containsKey(AnalyticsTemplateRegistry.EVENT_DATA_STREAM_TEMPLATE_NAME);
+    }
+
     /**
      * Mock plugin used to instantiate analytics tests requirement.
      */
-    public static class TestPlugin extends Plugin implements SystemIndexPlugin {
+    public static class TestPlugin extends Plugin {
         @Override
         public Collection<Object> createComponents(
             Client client,
@@ -91,16 +118,6 @@ public class AnalyticsTestCase extends ESSingleNodeTestCase {
                 indexNameExpressionResolver
             );
             return Arrays.asList(analyticsTemplateRegistry, analyticsCollectionService);
-        }
-
-        @Override
-        public String getFeatureName() {
-            return this.getClass().getSimpleName();
-        }
-
-        @Override
-        public String getFeatureDescription() {
-            return this.getClass().getCanonicalName();
         }
     }
 }

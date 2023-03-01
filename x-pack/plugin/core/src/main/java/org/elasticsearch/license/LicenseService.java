44e5917796eee5ff6a16a4158e79cd2e9ff497cb
Jake Landis <jake.landis@elastic.co>
2023-03-01 15:38:27 -0600

    javadoc

--- x-pack/plugin/core/src/main/java/org/elasticsearch/license/LicenseService.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/license/LicenseService.java
@@ -22,16 +22,24 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * ReadOnly service to get the current license. Consumers should generally not need to read the license directly and should instead
- * prefer {@link XPackLicenseState} to check if a feature is allowed by the of license. This interface is not intended to be implemented
- * and alternative implementations
+ * Interface to read the current license. Consumers should generally not need to read the license directly and should instead
+ * prefer {@link XPackLicenseState} to check if a feature is allowed by the license. This interface is not intended to be implemented
+ * by alternative implementations and exists for internal use only.
  */
 public interface LicenseService extends LifecycleComponent {
 
-    // should prefer getXPackLicenseState
+    /**
+     * Get the current license.
+     * @return the current license, null or {@link LicensesMetadata#LICENSE_TOMBSTONE} if no license is available.
+     */
     License getLicense();
 
-    // Read the license from a provided Metadata. Implementations not backed by {@link ClusterState} should not implement.
+    /**
+     * Get the current license from the provided metadata. Implementations not backed by {@link org.elasticsearch.cluster.ClusterState}
+     * should not implement this interface.
+     * @param metaData the {@link Metadata} to read the license information from.
+     * @return the current license, null or {@link LicensesMetadata#LICENSE_TOMBSTONE} if no license is available.
+     */
     default License getLicense(Metadata metaData) {
         return getLicense();
     }
@@ -90,21 +98,42 @@ public interface LicenseService extends LifecycleComponent {
         }
     }
 
+    /**
+     * Interface to update the current license.
+     */
     interface MutableLicense extends LicenseService, LifecycleComponent {
 
+        /**
+         * Updates the {@link XPackLicenseState}. The {@link XPackLicenseState} is the preferred way to make decisions based on the license.
+         * The {@link XPackLicenseState} is derived from the current {@link License} and must be updated for any changes to the license.
+         */
         void updateXPackLicenseState(License license);
 
+        /**
+         * Creates or updates the current license as defined by the request.
+         */
         void registerLicense(PutLicenseRequest request, ActionListener<PutLicenseResponse> listener);
 
+        /**
+         * Removes the current license. Implementations should remove the current license and ensure that attempts to read returns
+         * {@link LicensesMetadata#LICENSE_TOMBSTONE} if a license was removed. Additionally the {@link XPackLicenseState} must be updated.
+         */
         void removeLicense(ActionListener<? extends AcknowledgedResponse> listener);
 
         /**
-         * @return true if the license was found to be expired, false otherwise. If license is null - return false.
+         * Check and maybe expire the license.
+         * @return true if the license was found to be expired, false otherwise (a null license should return false).
          */
         boolean maybeExpireLicense(License license);
 
+        /**
+         * Installs a basic license.
+         */
         void startBasicLicense(PostStartBasicRequest request, ActionListener<PostStartBasicResponse> listener);
 
+        /**
+         * Installs a trial license.
+         */
         void startTrialLicense(PostStartTrialRequest request, ActionListener<PostStartTrialResponse> listener);
 
     }

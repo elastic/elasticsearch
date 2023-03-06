3091c5daddb04950528c4442868685b8ce8e8a85
Kathleen DeRusso <kathleen.derusso@elastic.co>
2023-03-06 11:28:55 -0500

    Fix build

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/entsearch/EnterpriseSearch.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/entsearch/EnterpriseSearch.java
@@ -44,6 +44,7 @@ import org.elasticsearch.xpack.entsearch.analytics.AnalyticsTemplateRegistry;
 import org.elasticsearch.xpack.entsearch.engine.EngineIndexService;
 import org.elasticsearch.xpack.entsearch.engine.action.DeleteEngineAction;
 import org.elasticsearch.xpack.entsearch.engine.action.GetEngineAction;
+import org.elasticsearch.xpack.entsearch.engine.action.ListEnginesAction;
 import org.elasticsearch.xpack.entsearch.engine.action.PutEngineAction;
 import org.elasticsearch.xpack.entsearch.engine.action.RestDeleteEngineAction;
 import org.elasticsearch.xpack.entsearch.engine.action.RestGetEngineAction;
@@ -51,6 +52,7 @@ import org.elasticsearch.xpack.entsearch.engine.action.RestListEnginesAction;
 import org.elasticsearch.xpack.entsearch.engine.action.RestPutEngineAction;
 import org.elasticsearch.xpack.entsearch.engine.action.TransportDeleteEngineAction;
 import org.elasticsearch.xpack.entsearch.engine.action.TransportGetEngineAction;
+import org.elasticsearch.xpack.entsearch.engine.action.TransportListEnginesAction;
 import org.elasticsearch.xpack.entsearch.engine.action.TransportPutEngineAction;
 
 import java.util.Arrays;
@@ -82,9 +84,10 @@ public class EnterpriseSearch extends Plugin implements ActionPlugin, SystemInde
             return Collections.emptyList();
         }
         return List.of(
+            new ActionHandler<>(DeleteEngineAction.INSTANCE, TransportDeleteEngineAction.class),
             new ActionHandler<>(GetEngineAction.INSTANCE, TransportGetEngineAction.class),
-            new ActionHandler<>(PutEngineAction.INSTANCE, TransportPutEngineAction.class),
-            new ActionHandler<>(DeleteEngineAction.INSTANCE, TransportDeleteEngineAction.class)
+            new ActionHandler<>(ListEnginesAction.INSTANCE, TransportListEnginesAction.class),
+            new ActionHandler<>(PutEngineAction.INSTANCE, TransportPutEngineAction.class)
         );
     }
 

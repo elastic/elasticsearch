55b4aeda529c0bb2c0231a79cb34d992ed688a32
Joseph McElroy <joseph.mcelroy@elastic.co>
2023-03-07 10:13:02 +0000

    fix missing API_ENDPOINT

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/entsearch/EnterpriseSearch.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/entsearch/EnterpriseSearch.java
@@ -72,6 +72,7 @@ import java.util.function.Supplier;
 
 public class EnterpriseSearch extends Plugin implements ActionPlugin, SystemIndexPlugin {
     public static final String ENGINE_API_ENDPOINT = "_engine";
+    public static final String BEHAVIORAL_ANALYTICS_API_ENDPOINT = "_behavioral_analytics";
 
     private static final Logger logger = LogManager.getLogger(EnterpriseSearch.class);
 

e9df2373c566902ce7f8b0dda180135e04678b89
gmarouli <mary.gouseti@elastic.co>
2023-03-07 17:47:51 +0200

    Move DataStreamsActionUtil to server

--- server/src/main/java/org/elasticsearch/action/datastreams/DataStreamsActionUtil.java
+++ server/src/main/java/org/elasticsearch/action/datastreams/DataStreamsActionUtil.java
@@ -5,7 +5,8 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-package org.elasticsearch.datastreams.action;
+
+package org.elasticsearch.action.datastreams;
 
 import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.cluster.ClusterState;

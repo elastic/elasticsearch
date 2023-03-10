8cf2a65a19b78f05b2829d11ba0eedadb7f0b8db
Yang Wang <yang.wang@elastic.co>
2023-03-10 15:30:03 +1100

    tweak

--- server/src/main/java/org/elasticsearch/action/admin/cluster/node/info/NodesInfoRequest.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/node/info/NodesInfoRequest.java
@@ -11,6 +11,7 @@ package org.elasticsearch.action.admin.cluster.node.info;
 import org.elasticsearch.action.support.nodes.BaseNodesRequest;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.transport.TcpTransport;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -169,7 +170,10 @@ public class NodesInfoRequest extends BaseNodesRequest<NodesInfoRequest> {
         }
 
         public static Set<String> allMetrics() {
-            return Arrays.stream(values()).map(Metric::metricName).collect(Collectors.toSet());
+            return Arrays.stream(values())
+                .filter(metric -> TcpTransport.isUntrustedRemoteClusterEnabled() || metric != REMOTE_CLUSTER_SERVER)
+                .map(Metric::metricName)
+                .collect(Collectors.toSet());
         }
     }
 }

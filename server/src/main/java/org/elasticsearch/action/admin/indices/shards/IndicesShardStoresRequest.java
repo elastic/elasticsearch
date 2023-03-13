54ed3324450646665bd7cb2115701369143eb380
David Turner <david.turner@elastic.co>
2023-03-13 08:41:22 -0400

    Simplify `IndicesShardStoresAction`
    - No need to use an `AsyncShardFetch` here, there is no caching
    - Response may be very large, introduce chunking
    - Fan-out may be very large, introduce throttling
    - Processing time may be nontrivial, introduce cancellability
    - Eliminate many unnecessary intermediate data structures
    - Do shard-level response processing more eagerly
    - Determine allocation from `RoutingTable` not `RoutingNodes`
    - Add tests
    
    Relates #81081

--- server/src/main/java/org/elasticsearch/action/admin/indices/shards/IndicesShardStoresRequest.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/shards/IndicesShardStoresRequest.java
@@ -15,9 +15,13 @@ import org.elasticsearch.cluster.health.ClusterHealthStatus;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.tasks.CancellableTask;
+import org.elasticsearch.tasks.Task;
+import org.elasticsearch.tasks.TaskId;
 
 import java.io.IOException;
 import java.util.EnumSet;
+import java.util.Map;
 
 /**
  * Request for {@link IndicesShardStoresAction}
@@ -117,5 +121,10 @@ public class IndicesShardStoresRequest extends MasterNodeReadRequest<IndicesShar
     @Override
     public ActionRequestValidationException validate() {
         return null;
+    }
+
+    @Override
+    public Task createTask(long id, String type, String action, TaskId parentTaskId, Map<String, String> headers) {
+        return new CancellableTask(id, type, action, "", parentTaskId, headers);
     }
 }

1d17dd3a8f8df4a62365fecd119e4a8105e6ed7b
Aurélien FOUCRET <aurelien.foucret@gmail.com>
2023-03-21 13:56:51 +0100

    Add license checks to behavioral analytics actions. (#94578)

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/analytics/action/TransportGetAnalyticsCollectionAction.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/analytics/action/TransportGetAnalyticsCollectionAction.java
@@ -15,16 +15,20 @@ import org.elasticsearch.cluster.block.ClusterBlockException;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.license.XPackLicenseState;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 import org.elasticsearch.xpack.application.analytics.AnalyticsCollectionService;
+import org.elasticsearch.xpack.application.utils.LicenseUtils;
 
 public class TransportGetAnalyticsCollectionAction extends TransportMasterNodeReadAction<
     GetAnalyticsCollectionAction.Request,
     GetAnalyticsCollectionAction.Response> {
 
     private final AnalyticsCollectionService analyticsCollectionService;
+
+    private final XPackLicenseState licenseState;
 
     @Inject
     public TransportGetAnalyticsCollectionAction(
@@ -33,7 +37,8 @@ public class TransportGetAnalyticsCollectionAction extends TransportMasterNodeRe
         ThreadPool threadPool,
         ActionFilters actionFilters,
         IndexNameExpressionResolver indexNameExpressionResolver,
-        AnalyticsCollectionService analyticsCollectionService
+        AnalyticsCollectionService analyticsCollectionService,
+        XPackLicenseState licenseState
     ) {
         super(
             GetAnalyticsCollectionAction.NAME,
@@ -47,6 +52,7 @@ public class TransportGetAnalyticsCollectionAction extends TransportMasterNodeRe
             ThreadPool.Names.SAME
         );
         this.analyticsCollectionService = analyticsCollectionService;
+        this.licenseState = licenseState;
     }
 
     @Override
@@ -56,7 +62,11 @@ public class TransportGetAnalyticsCollectionAction extends TransportMasterNodeRe
         ClusterState state,
         ActionListener<GetAnalyticsCollectionAction.Response> listener
     ) {
-        analyticsCollectionService.getAnalyticsCollection(state, request, listener);
+        LicenseUtils.runIfSupportedLicense(
+            licenseState,
+            () -> analyticsCollectionService.getAnalyticsCollection(state, request, listener),
+            listener::onFailure
+        );
     }
 
     @Override

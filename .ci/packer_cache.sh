3b5fcca89a756206bf21020fde2eb8b0026663ae
Mark Vieira <portugee@gmail.com>
2023-03-23 14:10:08 -0700

    Improve efficiency of packer cache script (#94701) (#94705)
    # Conflicts:
    #	.ci/packer_cache.sh
    #	distribution/bwc/build.gradle
    #	distribution/docker/build.gradle

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -4,11 +4,11 @@ SCRIPT="$0"
 
 # SCRIPT might be an arbitrarily deep series of symbolic links; loop until we
 # have the concrete path
-while [ -h "$SCRIPT" ] ; do
+while [ -h "$SCRIPT" ]; do
   ls=$(ls -ld "$SCRIPT")
   # Drop everything prior to ->
   link=$(expr "$ls" : '.*-> \(.*\)$')
-  if expr "$link" : '/.*' > /dev/null; then
+  if expr "$link" : '/.*' >/dev/null; then
     SCRIPT="$link"
   else
     SCRIPT=$(dirname "$SCRIPT")/"$link"

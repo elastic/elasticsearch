9dc3d739082dc41a03e4e5a9c3279f516656b38f
Ryan Ernst <ryan@iernst.net>
2023-03-24 11:55:31 -0700

    more requires

--- x-pack/plugin/searchable-snapshots/src/main/java/module-info.java
+++ x-pack/plugin/searchable-snapshots/src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module org.elasticsearch.searchablesnapshots {
     requires org.elasticsearch.base;
     requires org.elasticsearch.server;
+    requires org.elasticsearch.xcore;
+    requires org.elasticsearch.xcontent;
+
     requires org.elasticsearch.blobcache;
 
     requires org.apache.logging.log4j;
     requires org.apache.lucene.core;
+    requires org.apache.lucene.analysis.common;
 }

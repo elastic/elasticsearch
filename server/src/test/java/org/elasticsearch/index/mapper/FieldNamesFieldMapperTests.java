4f44e2a50636b007c9bb18cd611fef659d522bb0
Simon Cooper <simon.cooper@elastic.co>
2023-03-28 15:09:30 +0100

    splotless

--- server/src/test/java/org/elasticsearch/index/mapper/FieldNamesFieldMapperTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/FieldNamesFieldMapperTests.java
@@ -16,11 +16,8 @@ import org.elasticsearch.xcontent.XContentFactory;
 import org.elasticsearch.xcontent.XContentType;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
 

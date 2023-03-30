95bec32d4698bd13e7f664a95b6f5e0ecea67c80
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2023-03-30 13:49:40 +0200

    Abstract base class

--- x-pack/plugin/security/qa/multi-cluster/src/javaRestTest/java/org/elasticsearch/xpack/remotecluster/AbstractRemoteClusterSecurityCcsWithMultipleRemotesRestIT.java
+++ x-pack/plugin/security/qa/multi-cluster/src/javaRestTest/java/org/elasticsearch/xpack/remotecluster/AbstractRemoteClusterSecurityCcsWithMultipleRemotesRestIT.java
@@ -19,78 +19,34 @@ import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.test.cluster.ElasticsearchCluster;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 
-public class RemoteClusterSecurityRestWithMultipleRemotesIT extends AbstractRemoteClusterSecurityTestCase {
+public abstract class AbstractRemoteClusterSecurityCcsWithMultipleRemotesRestIT extends AbstractRemoteClusterSecurityTestCase {
 
-    private static final AtomicReference<Map<String, Object>> API_KEY_MAP_REF = new AtomicReference<>();
-    private static final ElasticsearchCluster secondFulfillingCluster;
-    private static RestClient secondFulfillingClusterClient;
-
-    static {
-        fulfillingCluster = ElasticsearchCluster.local()
-            .name("fulfilling-cluster-1")
-            .apply(commonClusterConfig)
-            .setting("remote_cluster_server.enabled", "true")
-            .setting("remote_cluster.port", "0")
-            .setting("xpack.security.remote_cluster_server.ssl.enabled", "true")
-            .setting("xpack.security.remote_cluster_server.ssl.key", "remote-cluster.key")
-            .setting("xpack.security.remote_cluster_server.ssl.certificate", "remote-cluster.crt")
-            .keystore("xpack.security.remote_cluster_server.ssl.secure_key_passphrase", "remote-cluster-password")
-            .build();
-
-        secondFulfillingCluster = ElasticsearchCluster.local().name("fulfilling-cluster-2").apply(commonClusterConfig).build();
-
-        queryCluster = ElasticsearchCluster.local()
-            .name("query-cluster")
-            .apply(commonClusterConfig)
-            .setting("xpack.security.remote_cluster_client.ssl.enabled", "true")
-            .setting("xpack.security.remote_cluster_client.ssl.certificate_authorities", "remote-cluster-ca.crt")
-            .keystore("cluster.remote.my_remote_cluster.credentials", () -> {
-                if (API_KEY_MAP_REF.get() == null) {
-                    final Map<String, Object> apiKeyMap = createCrossClusterAccessApiKey("""
-                        [
-                          {
-                             "names": ["cluster*_index*"],
-                             "privileges": ["read", "read_cross_cluster"]
-                          }
-                        ]""");
-                    API_KEY_MAP_REF.set(apiKeyMap);
-                }
-                return (String) API_KEY_MAP_REF.get().get("encoded");
-            })
-            .build();
-    }
-
-    @ClassRule
-    // Use a RuleChain to ensure that fulfilling cluster is started before query cluster
-    public static TestRule clusterRule = RuleChain.outerRule(fulfillingCluster).around(secondFulfillingCluster).around(queryCluster);
+    protected static ElasticsearchCluster otherFulfillingCluster;
+    protected static RestClient otherFulfillingClusterClient;
 
     @BeforeClass
     public static void initSecondFulfillingClusterClient() {
-        secondFulfillingClusterClient = buildRestClient(secondFulfillingCluster);
+        otherFulfillingClusterClient = buildRestClient(otherFulfillingCluster);
     }
 
     @AfterClass
     public static void closeSecondFulfillingClusterClient() throws IOException {
-        IOUtils.close(secondFulfillingClusterClient);
+        IOUtils.close(otherFulfillingClusterClient);
     }
 
     public void testCrossClusterSearch() throws Exception {
         configureRemoteClusters();
+        setupRolesOnClusters();
 
         // Fulfilling cluster
         {
@@ -98,70 +54,30 @@ public class RemoteClusterSecurityRestWithMultipleRemotesIT extends AbstractRemo
             final Request bulkRequest = new Request("POST", "/_bulk?refresh=true");
             bulkRequest.setJsonEntity(Strings.format("""
                 { "index": { "_index": "cluster1_index1" } }
-                { "cluster1_doc1": "doc_value" }
+                { "name": "doc1" }
                 { "index": { "_index": "cluster1_index2" } }
-                { "cluster1_doc2": "doc_value" }\n"""));
+                { "name": "doc2" }\n"""));
             assertOK(performRequestAgainstFulfillingCluster(bulkRequest));
         }
 
-        // Second fulfilling cluster
+        // Other fulfilling cluster
         {
-            // In the basic model, we need to set up the role on the FC
-            final var putRoleRequest = new Request("PUT", "/_security/role/" + REMOTE_SEARCH_ROLE);
-            putRoleRequest.setJsonEntity("""
-                {
-                  "indices": [
-                    {
-                      "names": ["cluster*_index1"],
-                      "privileges": ["read", "read_cross_cluster"]
-                    }
-                  ]
-                }""");
-            assertOK(performRequestWithDefaultUser(secondFulfillingClusterClient, putRoleRequest));
-
             // Index some documents, so we can attempt to search them from the querying cluster
             final Request bulkRequest = new Request("POST", "/_bulk?refresh=true");
             bulkRequest.setJsonEntity(Strings.format("""
                 { "index": { "_index": "cluster2_index1" } }
-                { "cluster2_doc1": "doc_value" }
+                { "name": "doc1" }
                 { "index": { "_index": "cluster2_index2" } }
-                { "cluster2_doc2": "doc_value" }\n"""));
-            assertOK(performRequestWithDefaultUser(secondFulfillingClusterClient, bulkRequest));
+                { "name": "doc2" }\n"""));
+            assertOK(performRequestAgainstOtherFulfillingCluster(bulkRequest));
         }
 
         // Query cluster
         {
             // Index some documents, to use them in a mixed-cluster search
             final var indexDocRequest = new Request("POST", "/local_index/_doc?refresh=true");
-            indexDocRequest.setJsonEntity("{\"local_doc1\": \"doc_value\"}");
+            indexDocRequest.setJsonEntity("{\"name\": \"doc1\"}");
             assertOK(client().performRequest(indexDocRequest));
-
-            // Create user role with privileges for remote and local indices
-            final var putRoleRequest = new Request("PUT", "/_security/role/" + REMOTE_SEARCH_ROLE);
-            putRoleRequest.setJsonEntity("""
-                {
-                  "indices": [
-                    {
-                      "names": ["local_index"],
-                      "privileges": ["read"]
-                    }
-                  ],
-                  "remote_indices": [
-                    {
-                      "names": ["cluster*_index1"],
-                      "privileges": ["read", "read_cross_cluster"],
-                      "clusters": ["my_remote_cluster"]
-                    }
-                  ]
-                }""");
-            assertOK(adminClient().performRequest(putRoleRequest));
-            final var putUserRequest = new Request("PUT", "/_security/user/" + REMOTE_SEARCH_USER);
-            putUserRequest.setJsonEntity("""
-                {
-                  "password": "x-pack-test-password",
-                  "roles" : ["remote_search"]
-                }""");
-            assertOK(adminClient().performRequest(putUserRequest));
 
             // Can search across local cluster and both remotes
             searchAndAssertIndicesFound(
@@ -234,11 +150,12 @@ public class RemoteClusterSecurityRestWithMultipleRemotesIT extends AbstractRemo
                 )
             );
             assertThat(exception.getResponse().getStatusLine().getStatusCode(), equalTo(403));
-
         }
     }
 
-    private void searchAndAssertIndicesFound(String searchPath, String... expectedIndices) throws IOException {
+    protected abstract void setupRolesOnClusters() throws IOException;
+
+    static void searchAndAssertIndicesFound(String searchPath, String... expectedIndices) throws IOException {
         final Response response = performRequestWithRemoteSearchUser(new Request("GET", searchPath));
         assertOK(response);
         final SearchResponse searchResponse = SearchResponse.fromXContent(responseAsParser(response));
@@ -248,14 +165,12 @@ public class RemoteClusterSecurityRestWithMultipleRemotesIT extends AbstractRemo
         assertThat(actualIndices, containsInAnyOrder(expectedIndices));
     }
 
-    @Override
-    protected void configureRemoteClusters() throws Exception {
-        super.configureRemoteClusters();
-        configureRemoteCluster("my_remote_cluster_2", secondFulfillingCluster, true, randomBoolean());
-    }
-
-    private Response performRequestWithRemoteSearchUser(final Request request) throws IOException {
+    static Response performRequestWithRemoteSearchUser(final Request request) throws IOException {
         request.setOptions(RequestOptions.DEFAULT.toBuilder().addHeader("Authorization", basicAuthHeaderValue(REMOTE_SEARCH_USER, PASS)));
         return client().performRequest(request);
+    }
+
+    static Response performRequestAgainstOtherFulfillingCluster(Request putRoleRequest) throws IOException {
+        return performRequestWithDefaultUser(otherFulfillingClusterClient, putRoleRequest);
     }
 }

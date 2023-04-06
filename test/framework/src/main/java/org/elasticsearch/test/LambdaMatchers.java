a41752ef8d92381475f64b6c0e3679984128da38
Simon Cooper <simon.cooper@elastic.co>
2023-04-06 11:49:20 +0100

    Add transform matcher

--- test/framework/src/main/java/org/elasticsearch/test/LambdaMatchers.java
+++ test/framework/src/main/java/org/elasticsearch/test/LambdaMatchers.java
@@ -8,15 +8,17 @@
 
 package org.elasticsearch.test;
 
+import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeDiagnosingMatcher;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-public class LambdaMatcher {
+public class LambdaMatchers {
 
     private static class PredicateMatcher<T> extends TypeSafeDiagnosingMatcher<T> {
         private final Predicate<? super T> predicate;
@@ -65,7 +67,7 @@ public class LambdaMatcher {
 
         @Override
         public void describeTo(Description description) {
-            description.appendText("every item is ").appendDescriptionOf(itemMatcher);
+            description.appendText("every item ").appendDescriptionOf(itemMatcher);
         }
 
         @Override
@@ -91,5 +93,50 @@ public class LambdaMatcher {
 
     public static <T> Matcher<T[]> everyItemMatches(Predicate<? super T> predicate, Supplier<String> description) {
         return new EveryArrayItem<>(matches(predicate, description));
+    }
+
+    private static class TransformMatcher<T, U> extends BaseMatcher<T> {
+        private final Matcher<U> matcher;
+        private final Function<T, U> transform;
+
+        private TransformMatcher(Matcher<U> matcher, Function<T, U> transform) {
+            this.matcher = matcher;
+            this.transform = transform;
+        }
+
+        @Override
+        public boolean matches(Object actual) {
+            U u;
+            try {
+                u = transform.apply((T) actual);
+            } catch (ClassCastException e) {
+                return false;
+            }
+
+            return matcher.matches(u);
+        }
+
+        @Override
+        public void describeMismatch(Object item, Description description) {
+            U u;
+            try {
+                u = transform.apply((T) item);
+            } catch (ClassCastException e) {
+                description.appendValue(item).appendText(" is not of the correct type (").appendText(e.getMessage()).appendText(")");
+                return;
+            }
+
+            description.appendText("transformed value ");
+            matcher.describeMismatch(u, description);
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("transformed to match ").appendDescriptionOf(matcher);
+        }
+    }
+
+    public static <T, U> Matcher<T> transformed(Function<T, U> function, Matcher<U> matcher) {
+        return new TransformMatcher<>(matcher, function);
     }
 }
--- test/framework/src/test/java/org/elasticsearch/test/LambdaMatchersTests.java
+++ test/framework/src/test/java/org/elasticsearch/test/LambdaMatchersTests.java
@@ -15,14 +15,16 @@ import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
 
-import static org.elasticsearch.test.LambdaMatcher.matches;
-import static org.elasticsearch.test.LambdaMatcher.everyItemMatches;
+import static org.elasticsearch.test.LambdaMatchers.everyItemMatches;
+import static org.elasticsearch.test.LambdaMatchers.matches;
+import static org.elasticsearch.test.LambdaMatchers.transformed;
+import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.startsWith;
 
-public class LambdaMatcherTests extends ESTestCase {
+public class LambdaMatchersTests extends ESTestCase {
 
     private static class A {
         final String str;
@@ -75,7 +77,8 @@ public class LambdaMatcherTests extends ESTestCase {
         assertMismatch(new A("x"), matches(a -> a.str.isEmpty(), "MyPredicate"), startsWith("<A[x]> did not match predicate MyPredicate"));
     }
 
-    public void testDescription() {
+    public void testPredicateDescription() {
+        assertDescribeTo(matches(o -> true, "MyPredicate"), equalTo("matches predicate MyPredicate"));
         assertDescribeTo(matches(new ToStringPredicate<>(o -> true, "MyPredicate")), equalTo("matches predicate <MyPredicate>"));
     }
 
@@ -88,6 +91,27 @@ public class LambdaMatcherTests extends ESTestCase {
     public void testArrayPredicate() {
         A[] as = new A[] { new A("1"), new A("2"), new A("3") };
         assertThat(as, everyItemMatches(a -> a.str.isEmpty() == false));
+
+        assertMismatch(
+            as,
+            everyItemMatches(a -> a.str.isEmpty(), "empty string"),
+            equalTo("an item <A[1]> did not match predicate empty string")
+        );
+    }
+
+    public void testArrayDescription() {
+        assertDescribeTo(everyItemMatches((A a) -> a.str.isEmpty(), "empty string"), equalTo("every item matches predicate empty string"));
+    }
+
+    public void testTransformMatcher() {
+        assertThat(new A("1"), transformed(a -> a.str, equalTo("1")));
+        assertThat(new B("1"), transformed((A a) -> a.str, equalTo("1")));
+
+        assertMismatch(new A("1"), transformed(a -> a.str, emptyString()), equalTo("transformed value was \"1\""));
+    }
+
+    public void testTransformDescription() {
+        assertDescribeTo(transformed((A a) -> a.str, emptyString()), equalTo("transformed to match an empty string"));
     }
 
     static <T> void assertMismatch(T v, Matcher<? super T> matcher, Matcher<String> mismatchDescriptionMatcher) {

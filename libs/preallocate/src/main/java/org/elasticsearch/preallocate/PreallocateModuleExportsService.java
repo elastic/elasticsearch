6d98e4b8370d2c4c28a7bf1b0564cde7b7b2fa11
Ryan Ernst <ryan@iernst.net>
2023-04-10 06:56:02 -0700

    fix license headers

--- libs/preallocate/src/main/java/org/elasticsearch/preallocate/PreallocateModuleExportsService.java
+++ libs/preallocate/src/main/java/org/elasticsearch/preallocate/PreallocateModuleExportsService.java
@@ -5,7 +5,6 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-
 package org.elasticsearch.preallocate;
 
 import org.elasticsearch.jdk.ModuleQualifiedExportsService;

40ff97037d80cd5269c8e4bd2163d1bd21e8bdba
Andrei Dan <andrei.dan@elastic.co>
2023-04-12 14:25:00 +0100

    GET _lifecycle returns data streams without lifecycle
    This changes the `GET _data_stream/ds_name/_lifecycle` endpoint to
    return the data stream name even if it doesn't have a lifecycle
    configured.
    
    e.g.
    ```
    {
      "data_streams": [
        {
          "name": "logs-nginx"
        }
      ]
    }
    ```

--- modules/dlm/src/main/java/org/elasticsearch/dlm/action/GetDataLifecycleAction.java
+++ modules/dlm/src/main/java/org/elasticsearch/dlm/action/GetDataLifecycleAction.java
@@ -139,7 +139,7 @@ public class GetDataLifecycleAction extends ActionType<GetDataLifecycleAction.Re
     public static class Response extends ActionResponse implements ChunkedToXContentObject {
         public static final ParseField DATA_STREAMS_FIELD = new ParseField("data_streams");
 
-        public record DataStreamLifecycle(String dataStreamName, DataLifecycle lifecycle) implements Writeable, ToXContentObject {
+        public record DataStreamLifecycle(String dataStreamName, @Nullable DataLifecycle lifecycle) implements Writeable, ToXContentObject {
 
             public static final ParseField NAME_FIELD = new ParseField("name");
             public static final ParseField LIFECYCLE_FIELD = new ParseField("lifecycle");
@@ -167,7 +167,9 @@ public class GetDataLifecycleAction extends ActionType<GetDataLifecycleAction.Re
                 builder.startObject();
                 builder.field(NAME_FIELD.getPreferredName(), dataStreamName);
                 builder.field(LIFECYCLE_FIELD.getPreferredName());
-                lifecycle.toXContent(builder, params, rolloverConfiguration);
+                if (lifecycle != null) {
+                    lifecycle.toXContent(builder, params, rolloverConfiguration);
+                }
                 builder.endObject();
                 return builder;
             }

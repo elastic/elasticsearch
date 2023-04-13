66021785b4f9d25cef96732d9f103e527086103a
Ryan Ernst <ryan@iernst.net>
2023-04-13 16:22:55 -0700

    remove dead code

--- x-pack/plugin/core/src/main/java/module-info.java
+++ x-pack/plugin/core/src/main/java/module-info.java
@@ -212,7 +212,6 @@ module org.elasticsearch.xcore {
     exports org.elasticsearch.xpack.core.watcher.trigger;
     exports org.elasticsearch.xpack.core.watcher.watch;
     exports org.elasticsearch.xpack.core.watcher;
-
-    // opens org.elasticsearch.license.internal to org.elasticsearch.server; // spi
+    
     provides org.elasticsearch.jdk.ModuleQualifiedExportsService with org.elasticsearch.xpack.core.XPackCoreModuleExportsService;
 }

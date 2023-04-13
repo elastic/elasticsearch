c49e70b53eb0937d353ad4a44e71a6ea700b2573
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2023-04-13 11:40:21 +0200

    Use regex for sanitization

--- modules/ingest-common/src/main/java/org/elasticsearch/ingest/common/RerouteProcessor.java
+++ modules/ingest-common/src/main/java/org/elasticsearch/ingest/common/RerouteProcessor.java
@@ -19,6 +19,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 
 import static org.elasticsearch.core.Strings.format;
 import static org.elasticsearch.ingest.ConfigurationUtils.newConfigurationException;
@@ -190,9 +191,9 @@ public final class RerouteProcessor extends AbstractProcessor {
     static final class DataStreamValueSource {
 
         private static final int MAX_LENGTH = 100;
-        private static final char REPLACEMENT_CHAR = '_';
-        private static final char[] DISALLOWED_IN_DATASET = new char[] { '\\', '/', '*', '?', '"', '<', '>', '|', ' ', ',', '#', ':', '-' };
-        private static final char[] DISALLOWED_IN_NAMESPACE = new char[] { '\\', '/', '*', '?', '"', '<', '>', '|', ' ', ',', '#', ':' };
+        private static final String REPLACEMENT = "_";
+        private static final Pattern DISALLOWED_IN_DATASET = Pattern.compile("[\\\\/*?\"<>| ,#:-]");
+        private static final Pattern DISALLOWED_IN_NAMESPACE = Pattern.compile("[\\\\/*?\"<>| ,#:]");
         static final DataStreamValueSource DATASET_VALUE_SOURCE = dataset("{{" + DATA_STREAM_DATASET + "}}");
         static final DataStreamValueSource NAMESPACE_VALUE_SOURCE = namespace("{{" + DATA_STREAM_NAMESPACE + "}}");
 
@@ -208,16 +209,13 @@ public final class RerouteProcessor extends AbstractProcessor {
             return new DataStreamValueSource(namespace, nsp -> sanitizeDataStreamField(nsp, DISALLOWED_IN_NAMESPACE));
         }
 
-        private static String sanitizeDataStreamField(String s, char[] disallowedInDataset) {
+        private static String sanitizeDataStreamField(String s, Pattern disallowedInDataset) {
             if (s == null) {
                 return null;
             }
             s = s.toLowerCase(Locale.ROOT);
             s = s.substring(0, Math.min(s.length(), MAX_LENGTH));
-            for (char c : disallowedInDataset) {
-                s = s.replace(c, REPLACEMENT_CHAR);
-            }
-            return s;
+            return disallowedInDataset.matcher(s).replaceAll(REPLACEMENT);
         }
 
         private DataStreamValueSource(String value, Function<String, String> sanitizer) {

d5b56020723a6d596fad460e244dbbbc965b8621
Martijn van Groningen <martijn.v.groningen@gmail.com>
2023-04-18 11:45:57 +0200

    iter

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -356,7 +356,6 @@ module org.elasticsearch.server {
     exports org.elasticsearch.snapshots;
     exports org.elasticsearch.tasks;
     exports org.elasticsearch.threadpool;
-    exports org.elasticsearch.timeseries.support;
     exports org.elasticsearch.tracing;
     exports org.elasticsearch.transport;
     exports org.elasticsearch.upgrades;

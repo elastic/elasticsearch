cb8aa6dd37dd696297860b794fe2e68c762fcbcf
iverase <ivera@apache.org>
2023-04-18 16:33:21 +0200

    rever Aggregation phase to use wrapped singleton

--- server/src/main/java/org/elasticsearch/search/aggregations/AggregationPhase.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/AggregationPhase.java
@@ -7,7 +7,6 @@
  */
 package org.elasticsearch.search.aggregations;
 
-import org.apache.lucene.search.Collector;
 import org.elasticsearch.action.search.SearchShardTask;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.search.SearchService;
@@ -26,6 +25,7 @@ import java.util.List;
  * Aggregation phase of a search request, used to collect aggregations
  */
 public class AggregationPhase {
+
     @Inject
     public AggregationPhase() {}
 
@@ -33,20 +33,25 @@ public class AggregationPhase {
         if (context.aggregations() == null) {
             return;
         }
-        AggregationCollectorManager collectorManager = new AggregationCollectorManager(context);
+        BucketCollector bucketCollector;
+        try {
+            context.aggregations().aggregators(context.aggregations().factories().createTopLevelAggregators());
+            bucketCollector = MultiBucketCollector.wrap(true, List.of(context.aggregations().aggregators()));
+            bucketCollector.preCollection();
+        } catch (IOException e) {
+            throw new AggregationInitializationException("Could not initialize aggregators", e);
+        }
         if (context.aggregations().factories().context() != null
             && context.aggregations().factories().context().isInSortOrderExecutionRequired()) {
             TimeSeriesIndexSearcher searcher = new TimeSeriesIndexSearcher(context.searcher(), getCancellationChecks(context));
             try {
-                // Execute here the time_series aggregations. The collecting top docs for the main query is done in QueryPhase
-                Collector collector = collectorManager.newCollector();
-                searcher.search(context.rewrittenQuery(), collectorManager.bucketCollector);
-                collectorManager.reduce(List.of(collector));
+                searcher.search(context.rewrittenQuery(), bucketCollector);
             } catch (IOException e) {
                 throw new AggregationExecutionException("Could not perform time series aggregation", e);
             }
             context.registerAggsCollectorManager(SingleThreadCollectorManager.wrap(BucketCollector.NO_OP_COLLECTOR));
         } else {
+            SingleThreadCollectorManager collectorManager = SingleThreadCollectorManager.wrap(bucketCollector.asCollector());
             if (context.getProfilers() != null) {
                 context.registerAggsCollectorManager(
                     new InternalProfileCollectorManager(collectorManager, CollectorResult.REASON_AGGREGATION, List.of())
@@ -87,51 +92,36 @@ public class AggregationPhase {
         return cancellationChecks;
     }
 
-    private static class AggregationCollectorManager extends SingleThreadCollectorManager {
-        private final SearchContext context;
-        Aggregator[] aggregators;
-        BucketCollector bucketCollector;
-
-        AggregationCollectorManager(SearchContext context) {
-            this.context = context;
+    public static void execute(SearchContext context) {
+        if (context.aggregations() == null) {
+            context.queryResult().aggregations(null);
+            return;
         }
 
-        @Override
-        protected Collector getNewCollector() {
+        if (context.queryResult().hasAggs()) {
+            // no need to compute the aggs twice, they should be computed on a per context basis
+            return;
+        }
+
+        Aggregator[] aggregators = context.aggregations().aggregators();
+
+        List<InternalAggregation> aggregations = new ArrayList<>(aggregators.length);
+        if (context.aggregations().factories().context() != null) {
+            // Rollup can end up here with a null context but not null factories.....
+            context.aggregations().factories().context().multiBucketConsumer().reset();
+        }
+        for (Aggregator aggregator : context.aggregations().aggregators()) {
             try {
-                aggregators = context.aggregations().factories().createTopLevelAggregators();
-                bucketCollector = MultiBucketCollector.wrap(true, List.of(aggregators));
-                bucketCollector.preCollection();
-                return bucketCollector.asCollector();
+                aggregator.postCollection();
+                aggregations.add(aggregator.buildTopLevel());
             } catch (IOException e) {
-                throw new AggregationInitializationException("Could not initialize aggregators", e);
+                throw new AggregationExecutionException("Failed to build aggregation [" + aggregator.name() + "]", e);
             }
         }
+        context.queryResult().aggregations(InternalAggregations.from(aggregations));
 
-        @Override
-        protected void reduce(Collector collector) throws IOException {
-            if (context.queryResult().hasAggs()) { // TODO: why would this happen?
-                // no need to compute the aggs twice, they should be computed on a per context basis
-                return;
-            }
-            List<InternalAggregation> aggregations = new ArrayList<>(aggregators.length);
-            if (context.aggregations().factories().context() != null) { // TODO: not sure why we need this
-                // Rollup can end up here with a null context but not null factories.....
-                context.aggregations().factories().context().multiBucketConsumer().reset();
-            }
-            for (Aggregator aggregator : aggregators) {
-                try {
-                    aggregator.postCollection();
-                    aggregations.add(aggregator.buildTopLevel());
-                } catch (IOException e) {
-                    throw new AggregationExecutionException("Failed to build aggregation [" + aggregator.name() + "]", e);
-                }
-            }
-            // register the internal aggregations
-            context.queryResult().aggregations(InternalAggregations.from(aggregations));
-            // disable aggregations so that they don't run on next pages in case of scrolling
-            context.aggregations(null);
-            context.registerAggsCollectorManager(null);
-        }
+        // disable aggregations so that they don't run on next pages in case of scrolling
+        context.aggregations(null);
+        context.registerAggsCollectorManager(null);
     }
 }
--- server/src/main/java/org/elasticsearch/search/query/QueryPhase.java
+++ server/src/main/java/org/elasticsearch/search/query/QueryPhase.java
@@ -87,6 +87,7 @@ public class QueryPhase {
 
         RescorePhase.execute(searchContext);
         SuggestPhase.execute(searchContext);
+        AggregationPhase.execute(searchContext);
 
         if (searchContext.getProfilers() != null) {
             searchContext.queryResult().profileResults(searchContext.getProfilers().buildQueryPhaseResults());

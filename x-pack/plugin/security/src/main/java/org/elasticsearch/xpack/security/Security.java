57b65854e65b806b761195a3f029541c9d884389
Albert Zaharovits <albert.zaharovits@elastic.co>
2023-04-24 16:29:06 +0300

    Spotless

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/Security.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/Security.java
@@ -7,6 +7,7 @@
 package org.elasticsearch.xpack.security;
 
 import io.netty.channel.Channel;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.util.SetOnce;

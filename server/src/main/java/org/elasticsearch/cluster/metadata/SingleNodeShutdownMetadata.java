1a3e190ba05dc5f37f690fcf56af73f2883e2506
Stuart Tettemer <stu@elastic.co>
2023-04-26 08:47:09 -0500

    Update to TransportVersion.V_8_9_0

--- server/src/main/java/org/elasticsearch/cluster/metadata/SingleNodeShutdownMetadata.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/SingleNodeShutdownMetadata.java
@@ -34,7 +34,7 @@ import static org.elasticsearch.core.Strings.format;
 public class SingleNodeShutdownMetadata implements SimpleDiffable<SingleNodeShutdownMetadata>, ToXContentObject {
 
     public static final TransportVersion REPLACE_SHUTDOWN_TYPE_ADDED_VERSION = TransportVersion.V_7_16_0;
-    public static final TransportVersion SIGTERM_ADDED_VERSION = TransportVersion.V_8_8_0; // TODO(stu): wait until after v8.8.0 freeze
+    public static final TransportVersion SIGTERM_ADDED_VERSION = TransportVersion.V_8_9_0;
 
     public static final ParseField NODE_ID_FIELD = new ParseField("node_id");
     public static final ParseField TYPE_FIELD = new ParseField("type");

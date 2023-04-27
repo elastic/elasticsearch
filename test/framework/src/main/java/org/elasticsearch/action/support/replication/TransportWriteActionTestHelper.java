ab0a6fa53e752ca7be9f2f7d5d9ffcf40a766ea1
Artem Prigoda <artem.prigoda@elastic.co>
2023-04-27 17:38:26 +0200

    Remove unused imports

--- test/framework/src/main/java/org/elasticsearch/action/support/replication/TransportWriteActionTestHelper.java
+++ test/framework/src/main/java/org/elasticsearch/action/support/replication/TransportWriteActionTestHelper.java
@@ -8,7 +8,6 @@
 package org.elasticsearch.action.support.replication;
 
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.core.Nullable;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.index.translog.Translog;

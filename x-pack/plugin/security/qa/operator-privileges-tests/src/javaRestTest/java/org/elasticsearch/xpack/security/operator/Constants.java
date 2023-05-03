65b06de629ece4640bfb53e707c132badf47c14e
cdelgado <carlos.delgado@elastic.co>
2023-05-03 10:41:49 +0200

    Fix duplicate security

--- x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
+++ x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
@@ -102,7 +102,6 @@ public class Constants {
         "cluster:admin/xpack/application/search_application/get",
         "cluster:admin/xpack/application/search_application/list",
         "cluster:admin/xpack/application/search_application/put",
-        "cluster:admin/xpack/application/search_application/search",
         "cluster:admin/xpack/application/search_application/render_query",
         "cluster:admin/xpack/application/search_application/search",
         "cluster:admin/xpack/ccr/auto_follow_pattern/activate",

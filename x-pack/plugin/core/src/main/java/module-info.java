474cfd207c127e2b958477408b289ef47153b6a5
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2023-05-03 10:12:34 +0200

    Module info

--- x-pack/plugin/core/src/main/java/module-info.java
+++ x-pack/plugin/core/src/main/java/module-info.java
@@ -214,6 +214,4 @@ module org.elasticsearch.xcore {
     exports org.elasticsearch.xpack.core.watcher.watch;
     exports org.elasticsearch.xpack.core.watcher;
     exports org.elasticsearch.xpack.core.security.action.dlm;
-
-    opens org.elasticsearch.license.internal;
 }

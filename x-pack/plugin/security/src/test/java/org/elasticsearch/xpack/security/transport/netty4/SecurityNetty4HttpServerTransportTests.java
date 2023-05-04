d143282dc08b85db5375d6baf1dbdb050687e1cd
Albert Zaharovits <albert.zaharovits@elastic.co>
2023-05-04 14:42:37 +0300

    testAuthnContextWrapping

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/transport/netty4/SecurityNetty4HttpServerTransportTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/transport/netty4/SecurityNetty4HttpServerTransportTests.java
@@ -22,14 +22,20 @@ import org.elasticsearch.common.network.NetworkService;
 import org.elasticsearch.common.settings.MockSecureSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.ssl.SslClientAuthenticationMode;
+import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.env.TestEnvironment;
 import org.elasticsearch.http.AbstractHttpServerTransportTestCase;
+import org.elasticsearch.http.HttpRequest;
+import org.elasticsearch.http.HttpServerTransport;
 import org.elasticsearch.http.NullDispatcher;
 import org.elasticsearch.http.netty4.Netty4HttpResponse;
 import org.elasticsearch.http.netty4.Netty4HttpServerTransport;
 import org.elasticsearch.http.netty4.internal.HttpHeadersAuthenticatorUtils;
 import org.elasticsearch.http.netty4.internal.HttpHeadersWithAuthenticationContext;
+import org.elasticsearch.rest.RestChannel;
+import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.test.rest.FakeRestRequest;
 import org.elasticsearch.threadpool.TestThreadPool;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.tracing.Tracer;
@@ -44,6 +50,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 import javax.net.ssl.SSLEngine;
 
@@ -55,6 +62,8 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.Mockito.mock;
 
 public class SecurityNetty4HttpServerTransportTests extends AbstractHttpServerTransportTestCase {
@@ -259,7 +268,85 @@ public class SecurityNetty4HttpServerTransportTests extends AbstractHttpServerTr
         assertNotNull(transport.configureServerChannelHandler());
     }
 
-    public void testHttpHeaderAuthenticationErrors() throws Exception {
+    public void testAuthnContextWrapping() throws Exception {
+        final Settings settings = Settings.builder().put(env.settings()).build();
+        final AtomicReference<HttpRequest> dispatchedHttpRequestReference = new AtomicReference<>();
+        final String header = "TEST-" + randomAlphaOfLength(8);
+        final String headerValue = "TEST-" + randomAlphaOfLength(8);
+        final String transientHeader = "TEST-" + randomAlphaOfLength(8);
+        final String transientHeaderValue = "TEST-" + randomAlphaOfLength(8);
+        final HttpServerTransport.Dispatcher dispatcher = new HttpServerTransport.Dispatcher() {
+            @Override
+            public void dispatchRequest(final RestRequest request, final RestChannel channel, final ThreadContext threadContext) {
+                // STEP 2: store the dispatched request, which should be wrapping the context
+                dispatchedHttpRequestReference.set(request.getHttpRequest());
+            }
+
+            @Override
+            public void dispatchBadRequest(final RestChannel channel, final ThreadContext threadContext, final Throwable cause) {
+                logger.error(() -> "--> Unexpected bad request [" + FakeRestRequest.requestToString(channel.request()) + "]", cause);
+                throw new AssertionError("Unexpected bad request");
+            }
+
+        };
+        final ThreadPool testThreadPool = new TestThreadPool(TEST_MOCK_TRANSPORT_THREAD_PREFIX);
+        try (
+            Netty4HttpServerTransport transport = Security.getHttpServerTransportWithHeadersValidator(
+                settings,
+                new NetworkService(List.of()),
+                testThreadPool,
+                xContentRegistry(),
+                dispatcher,
+                randomClusterSettings(),
+                new SharedGroupFactory(settings),
+                Tracer.NOOP,
+                TLSConfig.noTLS(),
+                null,
+                (httpPreRequest, channel, listener) -> {
+                    // STEP 1: amend the thread context during authentication
+                    testThreadPool.getThreadContext().putHeader(header, headerValue);
+                    testThreadPool.getThreadContext().putTransient(transientHeader, transientHeaderValue);
+                    listener.onResponse(null);
+                }
+            )
+        ) {
+            final ChannelHandler handler = transport.configureServerChannelHandler();
+            final EmbeddedChannel ch = new EmbeddedChannel(handler);
+            // remove these pipeline handlers as they interfere in the test scenario
+            for (String pipelineHandlerName : ch.pipeline().names()) {
+                if (pipelineHandlerName.equals("decoder")
+                    || pipelineHandlerName.equals("encoder")
+                    || pipelineHandlerName.equals("encoder_compress")
+                    || pipelineHandlerName.equals("chunked_writer")) {
+                    ch.pipeline().remove(pipelineHandlerName);
+                }
+            }
+            var writeFuture = testThreadPool.generic().submit(() -> {
+                ch.writeInbound(
+                    HttpHeadersAuthenticatorUtils.wrapAsMessageWithAuthenticationContext(
+                        new DefaultHttpRequest(HTTP_1_1, HttpMethod.GET, "/wrapped_request")
+                    )
+                );
+                ch.writeInbound(new DefaultLastHttpContent());
+                ch.flushInbound();
+            });
+            writeFuture.get();
+            // STEP 3: assert the wrapped context
+            var storedAuthnContext = HttpHeadersAuthenticatorUtils.extractAuthenticationContext(dispatchedHttpRequestReference.get());
+            assertThat(storedAuthnContext, notNullValue());
+            try (var ignored = testThreadPool.getThreadContext().stashContext()) {
+                assertThat(testThreadPool.getThreadContext().getHeader(header), nullValue());
+                assertThat(testThreadPool.getThreadContext().getTransientHeaders().get(transientHeader), nullValue());
+                storedAuthnContext.restore();
+                assertThat(testThreadPool.getThreadContext().getHeader(header), is(headerValue));
+                assertThat(testThreadPool.getThreadContext().getTransientHeaders().get(transientHeader), is(transientHeaderValue));
+            }
+        } finally {
+            testThreadPool.shutdownNow();
+        }
+    }
+
+    public void testHttpHeaderAuthnWithoutHeaderValidator() throws Exception {
         final Settings settings = Settings.builder().put(env.settings()).build();
         final ThreadPool testThreadPool = new TestThreadPool(TEST_MOCK_TRANSPORT_THREAD_PREFIX);
         try (
@@ -282,7 +369,7 @@ public class SecurityNetty4HttpServerTransportTests extends AbstractHttpServerTr
             // remove these pipeline handlers as they interfere in the test scenario
             for (String pipelineHandlerName : ch.pipeline().names()) {
                 if (pipelineHandlerName.equals("decoder")
-                    || pipelineHandlerName.equals("header_validator")
+                    || pipelineHandlerName.equals("header_validator") // ALSO REMOVE VALIDATOR so requests are not "validatable"
                     || pipelineHandlerName.equals("encoder")
                     || pipelineHandlerName.equals("encoder_compress")
                     || pipelineHandlerName.equals("chunked_writer")) {

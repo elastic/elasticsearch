8e40ffb186d08a62c79e33d83afd761d87d13f05
Simon Cooper <simon.cooper@elastic.co>
2023-05-10 10:13:13 +0100

    Add a builder to TestDiscoveryNode

--- test/framework/src/main/java/org/elasticsearch/cluster/node/TestDiscoveryNode.java
+++ test/framework/src/main/java/org/elasticsearch/cluster/node/TestDiscoveryNode.java
@@ -13,8 +13,8 @@ import org.elasticsearch.common.UUIDs;
 import org.elasticsearch.common.transport.TransportAddress;
 
 import java.security.AccessControlException;
-import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
@@ -26,69 +26,40 @@ import static org.elasticsearch.test.ESTestCase.buildNewFakeTransportAddress;
  */
 public class TestDiscoveryNode {
 
-    private static String newEphemeralId() {
-        try {
-            return UUIDs.randomBase64UUID(random());
-        } catch (AccessControlException e) {
-            // don't have SM permissions to access thread group context - probably part of a static init
-            return UUID.randomUUID().toString();
-        }
-    }
-
     public static DiscoveryNode create(String id) {
-        return create(id, buildNewFakeTransportAddress());
+        return builder(id).build();
     }
 
     public static DiscoveryNode create(String name, String id) {
-        return create(name, id, buildNewFakeTransportAddress());
+        return builder(id).name(name).build();
     }
 
     public static DiscoveryNode create(String id, TransportAddress address) {
-        return create(null, id, address);
+        return builder(id).address(address).build();
     }
 
     public static DiscoveryNode create(String name, String id, TransportAddress address) {
-        return new DiscoveryNode(
-            name,
-            id,
-            newEphemeralId(),
-            address.address().getHostString(),
-            address.getAddress(),
-            address,
-            Collections.emptyMap(),
-            DiscoveryNodeRole.roles(),
-            null
-        );
+        return builder(id).name(name).address(address).build();
     }
 
     public static DiscoveryNode create(String id, Version version) {
-        return create(id, buildNewFakeTransportAddress(), version);
+        return builder(id).version(version).build();
     }
 
     public static DiscoveryNode create(String id, TransportAddress address, Version version) {
-        return new DiscoveryNode(
-            null,
-            id,
-            newEphemeralId(),
-            address.address().getHostString(),
-            address.getAddress(),
-            address,
-            Collections.emptyMap(),
-            DiscoveryNodeRole.roles(),
-            version
-        );
+        return builder(id).address(address).version(version).build();
     }
 
     public static DiscoveryNode create(String id, Map<String, String> attributes, Set<DiscoveryNodeRole> roles) {
-        return create(id, buildNewFakeTransportAddress(), attributes, roles, null);
+        return builder(id).attributes(attributes).roles(roles).build();
     }
 
     public static DiscoveryNode create(String id, TransportAddress address, Map<String, String> attributes, Set<DiscoveryNodeRole> roles) {
-        return create(id, address, attributes, roles, null);
+        return builder(id).address(address).attributes(attributes).roles(roles).build();
     }
 
     public static DiscoveryNode create(String id, Map<String, String> attributes, Set<DiscoveryNodeRole> roles, Version version) {
-        return create(id, buildNewFakeTransportAddress(), attributes, roles, version);
+        return builder(id).attributes(attributes).roles(roles).version(version).build();
     }
 
     public static DiscoveryNode create(
@@ -98,21 +69,11 @@ public class TestDiscoveryNode {
         Set<DiscoveryNodeRole> roles,
         Version version
     ) {
-        return new DiscoveryNode(
-            null,
-            id,
-            newEphemeralId(),
-            address.address().getHostString(),
-            address.getAddress(),
-            address,
-            attributes,
-            roles,
-            version
-        );
+        return builder(id).address(address).attributes(attributes).roles(roles).version(version).build();
     }
 
     public static DiscoveryNode create(String nodeName, String nodeId, Map<String, String> attributes, Set<DiscoveryNodeRole> roles) {
-        return create(nodeName, nodeId, buildNewFakeTransportAddress(), attributes, roles);
+        return builder(nodeId).name(nodeName).attributes(attributes).roles(roles).build();
     }
 
     public static DiscoveryNode create(
@@ -122,17 +83,7 @@ public class TestDiscoveryNode {
         Map<String, String> attributes,
         Set<DiscoveryNodeRole> roles
     ) {
-        return new DiscoveryNode(
-            nodeName,
-            nodeId,
-            newEphemeralId(),
-            address.address().getHostString(),
-            address.getAddress(),
-            address,
-            attributes,
-            roles,
-            null
-        );
+        return builder(nodeId).name(nodeName).address(address).attributes(attributes).roles(roles).build();
     }
 
     public static DiscoveryNode create(
@@ -143,17 +94,91 @@ public class TestDiscoveryNode {
         Set<DiscoveryNodeRole> roles,
         String externalId
     ) {
-        return new DiscoveryNode(
-            nodeName,
-            nodeId,
-            newEphemeralId(),
-            address.address().getHostString(),
-            address.getAddress(),
-            address,
-            attributes,
-            roles,
-            null,
-            externalId
-        );
+        return builder(nodeId).name(nodeName).address(address).attributes(attributes).roles(roles).externalId(externalId).build();
+    }
+
+    private static String newEphemeralId() {
+        try {
+            return UUIDs.randomBase64UUID(random());
+        } catch (AccessControlException e) {
+            // don't have SM permissions to access thread group context - probably part of a static init
+            return UUID.randomUUID().toString();
+        }
+    }
+
+    public static Builder builder(String name) {
+        return new Builder(name);
+    }
+
+    public static class Builder {
+        private final String id;
+        private String name;
+        private String ephemeralId = newEphemeralId();
+        private String hostName;
+        private String hostAddress;
+        private TransportAddress address;
+        private Map<String, String> attributes = Map.of();
+        private Set<DiscoveryNodeRole> roles = DiscoveryNodeRole.roles();
+        private Version version;
+        private String externalId;
+
+        private Builder(String id) {
+            this.id = Objects.requireNonNull(id);
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder ephemeralId(String ephemeralId) {
+            this.ephemeralId = Objects.requireNonNull(ephemeralId);
+            return this;
+        }
+
+        public Builder address(TransportAddress address) {
+            return address(null, null, address);
+        }
+
+        public Builder address(String hostName, String hostAddress, TransportAddress address) {
+            this.hostName = hostName;
+            this.hostAddress = hostAddress;
+            this.address = Objects.requireNonNull(address);
+            return this;
+        }
+
+        public Builder attributes(Map<String, String> attributes) {
+            this.attributes = Objects.requireNonNull(attributes);
+            return this;
+        }
+
+        public Builder roles(Set<DiscoveryNodeRole> roles) {
+            this.roles = Objects.requireNonNull(roles);
+            return this;
+        }
+
+        public Builder version(Version version) {
+            this.version = version;
+            return this;
+        }
+
+        public Builder externalId(String externalId) {
+            this.externalId = externalId;
+            return this;
+        }
+
+        public DiscoveryNode build() {
+            if (address == null) {
+                address = buildNewFakeTransportAddress();
+            }
+            if (hostName == null) {
+                hostName = address.address().getHostString();
+            }
+            if (hostAddress == null) {
+                hostAddress = address.getAddress();
+            }
+
+            return new DiscoveryNode(name, id, ephemeralId, hostName, hostAddress, address, attributes, roles, version, externalId);
+        }
     }
 }

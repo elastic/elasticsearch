603285855bcefece73ba8681fa97b63caf4e265e
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2023-05-11 13:18:31 +0200

    Fix RebalanceOnlyWhenActiveAllocationDecider
    Currently above decider has 2 issues:
    * it does not allow rebalance is relocation target is initializing
    * it does allow rebalancing when one of the shards is relocating and another is
    unassigned
    
    This commit fixes above 2 issues

--- server/src/main/java/org/elasticsearch/cluster/routing/RoutingNodes.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/RoutingNodes.java
@@ -377,15 +377,17 @@ public class RoutingNodes implements Iterable<RoutingNode> {
      */
     public boolean allReplicasActive(ShardId shardId, Metadata metadata) {
         final List<ShardRouting> shards = assignedShards(shardId);
-        if (shards.isEmpty() || shards.size() < metadata.getIndexSafe(shardId.getIndex()).getNumberOfReplicas() + 1) {
+        final int shardCopies = metadata.getIndexSafe(shardId.getIndex()).getNumberOfReplicas() + 1;
+        if (shards.size() < shardCopies) {
             return false; // if we are empty nothing is active if we have less than total at least one is unassigned
         }
+        int active = 0;
         for (ShardRouting shard : shards) {
-            if (shard.active() == false) {
-                return false;
+            if (shard.active() && shard.isRelocationTarget() == false) {
+                active++;
             }
         }
-        return true;
+        return active == shardCopies;
     }
 
     @Override

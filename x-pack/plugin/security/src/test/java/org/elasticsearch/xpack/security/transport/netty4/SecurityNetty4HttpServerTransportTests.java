5ef6530e292bf228f22b2665aebd6f3c0b9027a7
Albert Zaharovits <albert.zaharovits@elastic.co>
2023-05-12 17:47:57 +0300

    testNoAuthnForResourceOptionsMethod

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/transport/netty4/SecurityNetty4HttpServerTransportTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/transport/netty4/SecurityNetty4HttpServerTransportTests.java
@@ -524,6 +524,10 @@ public class SecurityNetty4HttpServerTransportTests extends AbstractHttpServerTr
         }
     }
 
+    public void testUnauthenticatedOptionsMethod() throws Exception {
+
+    }
+
     public void testMalformedRequestDispatchedNoAuthn() throws Exception {
         final AtomicReference<Throwable> dispatchThrowableReference = new AtomicReference<>();
         final AtomicInteger authnInvocationCount = new AtomicInteger();

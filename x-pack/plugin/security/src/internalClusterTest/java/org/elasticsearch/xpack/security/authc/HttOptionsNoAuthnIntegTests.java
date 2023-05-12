4a288f29315e8a47be49bd728b2a6deb33729452
Albert Zaharovits <albert.zaharovits@elastic.co>
2023-05-12 18:42:04 +0300

    HttOptionsNoAuthnIntegTests testNoAuthnForPreFlightRequest

--- x-pack/plugin/security/src/internalClusterTest/java/org/elasticsearch/xpack/security/authc/HttOptionsNoAuthnIntegTests.java
+++ x-pack/plugin/security/src/internalClusterTest/java/org/elasticsearch/xpack/security/authc/HttOptionsNoAuthnIntegTests.java
@@ -11,9 +11,14 @@ import org.elasticsearch.client.Request;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.common.settings.SecureString;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.http.CorsHandler;
+import org.elasticsearch.http.HttpTransportSettings;
 import org.elasticsearch.test.SecurityIntegTestCase;
 import org.elasticsearch.test.SecuritySettingsSource;
 import org.elasticsearch.xpack.core.security.authc.support.UsernamePasswordToken;
+
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -25,28 +30,70 @@ public class HttOptionsNoAuthnIntegTests extends SecurityIntegTestCase {
         return false; // need real http
     }
 
+    @Override
+    protected Settings nodeSettings(int nodeOrdinal, Settings otherSettings) {
+        final Settings.Builder builder = Settings.builder().put(super.nodeSettings(nodeOrdinal, otherSettings));
+        // needed to test preflight requests
+        builder.put(HttpTransportSettings.SETTING_CORS_ENABLED.getKey(), "true")
+            .put(HttpTransportSettings.SETTING_CORS_ALLOW_ORIGIN.getKey(), "*");
+        return builder.build();
+    }
+
     public void testNoAuthnForResourceOptionsMethod() throws Exception {
-        Request request = new Request("OPTIONS", randomFrom("/", "/_cluster/stats", "/some-index", "/index/_stats", "/_stats/flush"));
+        Request requestNoCredentials = new Request(
+            "OPTIONS",
+            randomFrom("/", "/_cluster/stats", "/some-index", "/index/_stats", "/_stats/flush")
+        );
         // no "Authorization" request header -> request is unauthenticated
-        assertThat(request.getOptions().getHeaders().isEmpty(), is(true));
-        Response response = getRestClient().performRequest(request);
-        assertThat(response.getStatusLine().getStatusCode(), is(200));
-        assertThat(response.getHeader("Allow"), notNullValue());
-        assertThat(response.getHeader("X-elastic-product"), is("Elasticsearch"));
-        assertThat(response.getHeader("content-length"), is("0"));
+        assertThat(requestNoCredentials.getOptions().getHeaders().isEmpty(), is(true));
         // WRONG "Authorization" request header
-        request = new Request("OPTIONS", randomFrom("/", "/_cluster/stats", "/some-index", "/index/_stats", "/_stats/flush"));
-        RequestOptions.Builder options = request.getOptions().toBuilder();
+        Request requestWrongCredentials = new Request(
+            "OPTIONS",
+            randomFrom("/", "/_cluster/stats", "/some-index", "/index/_stats", "/_stats/flush")
+        );
+        RequestOptions.Builder options = requestWrongCredentials.getOptions().toBuilder();
         options.addHeader(
             "Authorization",
             UsernamePasswordToken.basicAuthHeaderValue(SecuritySettingsSource.TEST_USER_NAME, new SecureString("WRONG"))
         );
-        request.setOptions(options);
-        response = getRestClient().performRequest(request);
-        assertThat(response.getStatusLine().getStatusCode(), is(200));
-        assertThat(response.getHeader("Allow"), notNullValue());
-        assertThat(response.getHeader("X-elastic-product"), is("Elasticsearch"));
-        assertThat(response.getHeader("content-length"), is("0"));
+        requestWrongCredentials.setOptions(options);
+        for (Request request : List.of(requestNoCredentials, requestWrongCredentials)) {
+            Response response = getRestClient().performRequest(request);
+            assertThat(response.getStatusLine().getStatusCode(), is(200));
+            assertThat(response.getHeader("Allow"), notNullValue());
+            assertThat(response.getHeader("X-elastic-product"), is("Elasticsearch"));
+            assertThat(response.getHeader("content-length"), is("0"));
+        }
+    }
+
+    public void testNoAuthnForPreFlightRequest() throws Exception {
+        Request requestNoCredentials = new Request(
+            "OPTIONS",
+            randomFrom("/", "/_cluster/stats", "/some-index", "/index/_stats", "/_stats/flush")
+        );
+        RequestOptions.Builder options = requestNoCredentials.getOptions().toBuilder();
+        options.addHeader(CorsHandler.ORIGIN, "google.com");
+        options.addHeader(CorsHandler.ACCESS_CONTROL_REQUEST_METHOD, "GET");
+        requestNoCredentials.setOptions(options);
+        // no "Authorization" request header -> request is unauthenticated
+        Request requestWrongCredentials = new Request(
+            "OPTIONS",
+            randomFrom("/", "/_cluster/stats", "/some-index", "/index/_stats", "/_stats/flush")
+        );
+        options = requestWrongCredentials.getOptions().toBuilder();
+        // WRONG "Authorization" request header
+        options.addHeader(
+            "Authorization",
+            UsernamePasswordToken.basicAuthHeaderValue(SecuritySettingsSource.TEST_USER_NAME, new SecureString("WRONG"))
+        );
+        options.addHeader(CorsHandler.ORIGIN, "google.com");
+        options.addHeader(CorsHandler.ACCESS_CONTROL_REQUEST_METHOD, "GET");
+        requestWrongCredentials.setOptions(options);
+        for (Request request : List.of(requestWrongCredentials)) {
+            Response response = getRestClient().performRequest(request);
+            assertThat(response.getStatusLine().getStatusCode(), is(200));
+            assertThat(response.getHeader("content-length"), is("0"));
+        }
     }
 
 }

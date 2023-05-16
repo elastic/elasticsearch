8cdee3ed655ccf85ec9301218aae59f3a48aa605
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2023-05-16 12:36:31 +0200

    Balance priorities during reconciliation (#95454)
    When reconciling a balance with a lot of shards on undesired nodes there is a
    possibility of causing node hot spots due to usage of
    nodeInterleavedShardIterator. This iterator orders shards based on nodes they
    are located and order nodes based hash map iteration. This means it tends
    to pick shards returned first by the iterator. This change uses OrderedShardsIterator
    that applies custom shards order based on allocation recency.
    
    (cherry picked from commit 6ecd74d29fdedb9973465b7f1ed13c9cb001a48e)

--- server/src/main/java/org/elasticsearch/cluster/routing/RoutingNodes.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/RoutingNodes.java
@@ -249,6 +249,10 @@ public class RoutingNodes extends AbstractCollection<RoutingNode> {
         return primary;
     }
 
+    public Set<String> getAllNodeIds() {
+        return Collections.unmodifiableSet(nodesToShards.keySet());
+    }
+
     @Override
     public Iterator<RoutingNode> iterator() {
         return Collections.unmodifiableCollection(nodesToShards.values()).iterator();

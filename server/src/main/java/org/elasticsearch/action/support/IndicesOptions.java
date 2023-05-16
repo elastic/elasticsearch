6e4cec57ca2d3a2e5f77c725c3c56f920f0f8e30
Jim Ferenczi <jim.ferenczi@elastic.co>
2023-05-16 11:24:10 +0100

    Simplify privilege requirement for QuerySearchApplicationAction
    This change allows the usage of the QuerySearchApplicationAction to roles that have the read privilege on
    the underlying alias. The `manage_search_application` is no longer needed.

--- server/src/main/java/org/elasticsearch/action/support/IndicesOptions.java
+++ server/src/main/java/org/elasticsearch/action/support/IndicesOptions.java
@@ -145,6 +145,11 @@ public record IndicesOptions(EnumSet<Option> options, EnumSet<WildcardStates> ex
         EnumSet.noneOf(WildcardStates.class)
     );
 
+    public static final IndicesOptions STRICT_NO_EXPAND_FORBID_CLOSED = new IndicesOptions(
+        EnumSet.of(Option.FORBID_CLOSED_INDICES),
+        EnumSet.noneOf(WildcardStates.class)
+    );
+
     /**
      * @return Whether specified concrete indices should be ignored when unavailable (missing or closed)
      */
@@ -577,6 +582,13 @@ public record IndicesOptions(EnumSet<Option> options, EnumSet<WildcardStates> ex
      */
     public static IndicesOptions strictExpandHidden() {
         return STRICT_EXPAND_OPEN_CLOSED_HIDDEN;
+    }
+
+    /**
+     * @return indices option that requires each specified index or alias to exist, doesn't expand wildcards.
+     */
+    public static IndicesOptions strictNoExpandForbidClosed() {
+        return STRICT_NO_EXPAND_FORBID_CLOSED;
     }
 
     /**

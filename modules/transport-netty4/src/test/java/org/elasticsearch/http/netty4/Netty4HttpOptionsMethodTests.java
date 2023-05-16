55a9ebd9d3b900e863749ba859f0997feb6914f0
Albert Zaharovits <albert.zaharovits@elastic.co>
2023-05-16 18:15:22 +0300

    testRequestVariableLengthChunkedTransferEncoding with follow-up request

--- modules/transport-netty4/src/test/java/org/elasticsearch/http/netty4/Netty4HttpOptionsMethodTests.java
+++ modules/transport-netty4/src/test/java/org/elasticsearch/http/netty4/Netty4HttpOptionsMethodTests.java
@@ -15,7 +15,6 @@ import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.http.HttpConstants;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.util.AsciiString;
-
 import org.elasticsearch.common.network.NetworkService;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Settings;
@@ -146,12 +145,10 @@ public class Netty4HttpOptionsMethodTests extends AbstractHttpServerTransportTes
                     assertThat(dispatchedRequest.content().length(), is(0));
                     // netty adds a content length of "0" when there's no content...
                     assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_LENGTH.toString()), is("0"));
-                    assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_ENCODING.toString()), nullValue());
                     assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_TYPE.toString()), nullValue());
                 } else {
                     assertThat(dispatchedRequest.content().utf8ToString(), is(content));
                     assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_LENGTH.toString()), is(Integer.toString(content.length())));
-                    assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_ENCODING.toString()), nullValue());
                     if (hasContentTypeHeader) {
                         assertThat(
                             dispatchedRequest.header(HttpHeaderNames.CONTENT_TYPE.toString()),
@@ -159,6 +156,7 @@ public class Netty4HttpOptionsMethodTests extends AbstractHttpServerTransportTes
                         );
                     }
                 }
+                assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_ENCODING.toString()), nullValue());
                 assertThat(dispatchedRequest.uri(), is("/url/whatever/fixed-length-single-chunk"));
                 if (hasHostHeader) {
                     assertThat(dispatchedRequest.header(HttpHeaderNames.HOST.toString()), is("localhost"));
@@ -206,7 +204,7 @@ public class Netty4HttpOptionsMethodTests extends AbstractHttpServerTransportTes
                 }
                 String[] contentParts = randomArray(1, 5, String[]::new, () -> randomAlphaOfLengthBetween(2, 20));
                 // having a "Content-Length" request header is what makes it "fixed length"
-                final int totalContentLength = Arrays.stream(contentParts).map(String::length).reduce(Integer::sum).get();
+                final int totalContentLength = Arrays.stream(contentParts).map(String::length).reduce(0, Integer::sum);
                 ByteBufUtil.copy(AsciiString.of("Content-Length: " + totalContentLength), buf);
                 buf.writeByte(HttpConstants.LF);
                 // end of headers
@@ -231,7 +229,6 @@ public class Netty4HttpOptionsMethodTests extends AbstractHttpServerTransportTes
                     assertThat(dispatchedRequest.content().length(), is(0));
                     // netty adds a content length of "0" when there's no content...
                     assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_LENGTH.toString()), is("0"));
-                    assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_ENCODING.toString()), nullValue());
                     assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_TYPE.toString()), nullValue());
                 } else {
                     assertThat(dispatchedRequest.content().length(), is(totalContentLength));
@@ -240,7 +237,6 @@ public class Netty4HttpOptionsMethodTests extends AbstractHttpServerTransportTes
                         dispatchedRequest.header(HttpHeaderNames.CONTENT_LENGTH.toString()),
                         is(Integer.toString(totalContentLength))
                     );
-                    assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_ENCODING.toString()), nullValue());
                     if (hasContentTypeHeader) {
                         assertThat(
                             dispatchedRequest.header(HttpHeaderNames.CONTENT_TYPE.toString()),
@@ -248,6 +244,7 @@ public class Netty4HttpOptionsMethodTests extends AbstractHttpServerTransportTes
                         );
                     }
                 }
+                assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_ENCODING.toString()), nullValue());
                 assertThat(dispatchedRequest.uri(), is("/url/whatever/fixed-length?multiple-chunks=true"));
                 if (hasHostHeader) {
                     assertThat(dispatchedRequest.header(HttpHeaderNames.HOST.toString()), is("localhost"));
@@ -265,81 +262,109 @@ public class Netty4HttpOptionsMethodTests extends AbstractHttpServerTransportTes
         try (Netty4HttpServerTransport transport = getHttpServerTransport(dispatchedRequestReference)) {
             final ChannelHandler handler = transport.configureServerChannelHandler();
             final EmbeddedChannel ch = new EmbeddedChannel(handler);
-            ByteBuf buf = ch.alloc().buffer();
-            ByteBufUtil.copy(AsciiString.of("OPTIONS /url/whatever/chunked-transfer?encoding HTTP/1.1"), buf);
-            buf.writeByte(HttpConstants.LF);
-            boolean hasHostHeader = randomBoolean();
-            if (hasHostHeader) {
-                ByteBufUtil.copy(AsciiString.of("Host: localhost"), buf);
+            // also test that requests following an OPTIONS one are not affected
+            for (String httpMethod : List.of("OPTIONS", randomFrom("PUT", "POST", "GET", "DELETE"))) {
+                ByteBuf buf = ch.alloc().buffer();
+                ByteBufUtil.copy(AsciiString.of(httpMethod + " /url/whatever/chunked-transfer?encoding HTTP/1.1"), buf);
                 buf.writeByte(HttpConstants.LF);
-            }
-            boolean hasAcceptHeader = randomBoolean();
-            if (hasAcceptHeader) {
-                ByteBufUtil.copy(AsciiString.of("Accept: */*"), buf);
+                boolean hasHostHeader = randomBoolean();
+                if (hasHostHeader) {
+                    ByteBufUtil.copy(AsciiString.of("Host: localhost"), buf);
+                    buf.writeByte(HttpConstants.LF);
+                }
+                boolean hasAcceptHeader = randomBoolean();
+                if (hasAcceptHeader) {
+                    ByteBufUtil.copy(AsciiString.of("Accept: */*"), buf);
+                    buf.writeByte(HttpConstants.LF);
+                }
+                // content-encoding should be ignored for OPTIONS but it trips the test scenario for others
+                if (randomBoolean() && httpMethod.equals("OPTIONS")) {
+                    ByteBufUtil.copy(AsciiString.of("Content-Encoding: gzip"), buf);
+                    buf.writeByte(HttpConstants.LF);
+                }
+                boolean hasContentTypeHeader = randomBoolean();
+                if (hasContentTypeHeader) {
+                    ByteBufUtil.copy(
+                            AsciiString.of("Content-Type: " + randomFrom("text/plain; charset=utf-8", "application/json; charset=utf-8")),
+                            buf
+                    );
+                    buf.writeByte(HttpConstants.LF);
+                }
+                // do not write a "Content-Length" header to make the request "variable length"
+                // transfer-encoding besides "chunked" should be ignored for OPTIONS but it trips the test scenario for others
+                if (randomBoolean() && httpMethod.equals("OPTIONS")) {
+                    ByteBufUtil.copy(AsciiString.of("Transfer-Encoding: " + randomFrom("chunked", "gzip, chunked")), buf);
+                } else {
+                    ByteBufUtil.copy(AsciiString.of("Transfer-Encoding: chunked"), buf);
+                }
                 buf.writeByte(HttpConstants.LF);
-            }
-            if (randomBoolean()) {
-                ByteBufUtil.copy(AsciiString.of("Content-Encoding: gzip"), buf);
                 buf.writeByte(HttpConstants.LF);
-            }
-            if (randomBoolean()) {
-                ByteBufUtil.copy(AsciiString.of("Content-Type: text/plain; charset=UTF-8"), buf);
-                buf.writeByte(HttpConstants.LF);
-            }
-            // do not write a "Content-Length" header to make the request "variable length"
-            ByteBufUtil.copy(AsciiString.of("Transfer-Encoding: " + randomFrom("chunked", "gzip, chunked")), buf);
-            buf.writeByte(HttpConstants.LF);
-            buf.writeByte(HttpConstants.LF);
-            // maybe append some chunks as well
-            String[] contentParts = randomArray(0, 4, String[]::new, () -> randomAlphaOfLengthBetween(1, 64));
-            for (String content : contentParts) {
-                ByteBufUtil.copy(AsciiString.of(Integer.toHexString(content.length())), buf);
-                buf.writeByte(HttpConstants.CR);
-                buf.writeByte(HttpConstants.LF);
-                ByteBufUtil.copy(AsciiString.of(content), buf);
-                buf.writeByte(HttpConstants.CR);
-                buf.writeByte(HttpConstants.LF);
-            }
-            threadPool.generic().submit(() -> {
-                ch.writeInbound(buf);
-                ch.flushInbound();
-            }).get();
-            // append some more chunks as well
-            ByteBuf buf2 = ch.alloc().buffer();
-            contentParts = randomArray(1, 4, String[]::new, () -> randomAlphaOfLengthBetween(1, 64));
-            for (String content : contentParts) {
-                ByteBufUtil.copy(AsciiString.of(Integer.toHexString(content.length())), buf2);
+                // maybe append some chunks as well
+                String[] contentParts = randomArray(0, 4, String[]::new, () -> randomAlphaOfLengthBetween(1, 64));
+                for (String content : contentParts) {
+                    ByteBufUtil.copy(AsciiString.of(Integer.toHexString(content.length())), buf);
+                    buf.writeByte(HttpConstants.CR);
+                    buf.writeByte(HttpConstants.LF);
+                    ByteBufUtil.copy(AsciiString.of(content), buf);
+                    buf.writeByte(HttpConstants.CR);
+                    buf.writeByte(HttpConstants.LF);
+                }
+                threadPool.generic().submit(() -> {
+                    ch.writeInbound(buf);
+                    ch.flushInbound();
+                }).get();
+                // append some more chunks as well
+                ByteBuf buf2 = ch.alloc().buffer();
+                int totalContentLength = Arrays.stream(contentParts).map(String::length).reduce(0, Integer::sum);
+                contentParts = randomArray(1, 4, String[]::new, () -> randomAlphaOfLengthBetween(1, 64));
+                for (String content : contentParts) {
+                    ByteBufUtil.copy(AsciiString.of(Integer.toHexString(content.length())), buf2);
+                    buf2.writeByte(HttpConstants.CR);
+                    buf2.writeByte(HttpConstants.LF);
+                    ByteBufUtil.copy(AsciiString.of(content), buf2);
+                    buf2.writeByte(HttpConstants.CR);
+                    buf2.writeByte(HttpConstants.LF);
+                }
+                totalContentLength += Arrays.stream(contentParts).map(String::length).reduce(0, Integer::sum);
+                // finish chunked request
+                ByteBufUtil.copy(AsciiString.of("0"), buf2);
                 buf2.writeByte(HttpConstants.CR);
                 buf2.writeByte(HttpConstants.LF);
-                ByteBufUtil.copy(AsciiString.of(content), buf2);
                 buf2.writeByte(HttpConstants.CR);
                 buf2.writeByte(HttpConstants.LF);
+                threadPool.generic().submit(() -> {
+                    ch.writeInbound(buf2);
+                    ch.flushInbound();
+                }).get();
+                ch.runPendingTasks();
+                RestRequest dispatchedRequest = dispatchedRequestReference.get();
+                if ("OPTIONS".equals(httpMethod)) {
+                    assertThat(dispatchedRequest.content().length(), is(0));
+                    // netty adds a content length of "0" when there's no content...
+                    assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_LENGTH.toString()), is("0"));
+                    assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_TYPE.toString()), nullValue());
+                } else {
+                    assertThat(dispatchedRequest.content().length(), is(totalContentLength));
+                    // netty adds a content length of "0" when there's no content...
+                    assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_LENGTH.toString()), is(Integer.toString(totalContentLength)));
+                    if (hasContentTypeHeader) {
+                        assertThat(
+                                dispatchedRequest.header(HttpHeaderNames.CONTENT_TYPE.toString()),
+                                anyOf(is("text/plain; charset=utf-8"), is("application/json; charset=utf-8"))
+                        );
+                    }
+                }
+                assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_ENCODING.toString()), nullValue());
+                assertThat(dispatchedRequest.header(HttpHeaderNames.TRANSFER_ENCODING.toString()), nullValue());
+                assertThat(dispatchedRequest.uri(), is("/url/whatever/chunked-transfer?encoding"));
+                if (hasHostHeader) {
+                    assertThat(dispatchedRequest.header(HttpHeaderNames.HOST.toString()), is("localhost"));
+                }
+                if (hasAcceptHeader) {
+                    assertThat(dispatchedRequest.header(HttpHeaderNames.ACCEPT.toString()), is("*/*"));
+                }
+                dispatchedRequest.getHttpRequest().release();
             }
-            // finish chunked request
-            ByteBufUtil.copy(AsciiString.of("0"), buf2);
-            buf2.writeByte(HttpConstants.CR);
-            buf2.writeByte(HttpConstants.LF);
-            buf2.writeByte(HttpConstants.CR);
-            buf2.writeByte(HttpConstants.LF);
-            threadPool.generic().submit(() -> {
-                ch.writeInbound(buf2);
-                ch.flushInbound();
-            }).get();
-            RestRequest dispatchedRequest = dispatchedRequestReference.get();
-            assertThat(dispatchedRequest.content().length(), is(0));
-            // netty adds a content length of "0" when there's no content...
-            assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_LENGTH.toString()), is("0"));
-            assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_ENCODING.toString()), nullValue());
-            assertThat(dispatchedRequest.header(HttpHeaderNames.CONTENT_TYPE.toString()), nullValue());
-            assertThat(dispatchedRequest.header(HttpHeaderNames.TRANSFER_ENCODING.toString()), nullValue());
-            assertThat(dispatchedRequest.uri(), is("/url/whatever/chunked-transfer?encoding"));
-            if (hasHostHeader) {
-                assertThat(dispatchedRequest.header(HttpHeaderNames.HOST.toString()), is("localhost"));
-            }
-            if (hasAcceptHeader) {
-                assertThat(dispatchedRequest.header(HttpHeaderNames.ACCEPT.toString()), is("*/*"));
-            }
-            dispatchedRequest.getHttpRequest().release();
         }
     }
 }

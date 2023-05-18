2cc77c966e644ef0562ff84e56f9671599ec905f
Joseph Crail <joseph.crail@elastic.co>
2023-05-18 12:05:46 -0700

    Fix constructor errors in tests

--- x-pack/plugin/profiler/src/test/java/org/elasticsearch/xpack/profiler/StackFrameTests.java
+++ x-pack/plugin/profiler/src/test/java/org/elasticsearch/xpack/profiler/StackFrameTests.java
@@ -66,18 +66,17 @@ public class StackFrameTests extends ESTestCase {
             .array("function_name", "helloWorld")
             .array("function_offset", 31733)
             .array("line_number", 22)
-            .array("source_type", 3)
             .endObject();
 
         XContentBuilder actualRequest = XContentFactory.contentBuilder(contentType);
-        StackFrame stackTrace = new StackFrame("Main.java", "helloWorld", 31733, 22, 3);
+        StackFrame stackTrace = new StackFrame("Main.java", "helloWorld", 31733, 22);
         stackTrace.toXContent(actualRequest, ToXContent.EMPTY_PARAMS);
 
         assertToXContentEquivalent(BytesReference.bytes(expectedRequest), BytesReference.bytes(actualRequest), contentType);
     }
 
     public void testEquality() {
-        StackFrame frame = new StackFrame("Main.java", "helloWorld", 31733, 22, 3);
+        StackFrame frame = new StackFrame("Main.java", "helloWorld", 31733, 22);
         EqualsHashCodeTestUtils.checkEqualsAndHashCode(
             frame,
             (o -> new StackFrame(o.fileName, o.functionName, o.functionOffset, o.lineNumber))

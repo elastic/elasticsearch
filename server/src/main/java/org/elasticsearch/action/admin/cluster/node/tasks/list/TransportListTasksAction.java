ee3065e9c3114de3a4a3fbb91035685cc6252695
David Turner <david.turner@elastic.co>
2023-05-22 08:39:07 +0100

    TransportTasksAction#processTasks returns tasks
    Today `TransportTasksAction#processTasks` accepts a consumer which
    always just appends the matching tasks to a list. This indirection isn't
    necessary, we can just return the list of the matching tasks directly.
    
    This commit adjusts the sync overload only. The async overload will be
    adjusted similarly in a follow-up.

--- server/src/main/java/org/elasticsearch/action/admin/cluster/node/tasks/list/TransportListTasksAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/node/tasks/list/TransportListTasksAction.java
@@ -106,14 +106,9 @@ public class TransportListTasksAction extends TransportTasksAction<Task, ListTas
                 () -> taskManager.unregisterRemovedTaskListener(removedTaskListener)
             );
             try {
-                processTasks(request, task -> {
-                    if (task.getAction().startsWith(ListTasksAction.NAME) == false) {
-                        // It doesn't make sense to wait for List Tasks and it can cause an infinite loop of the task waiting
-                        // for itself or one of its child tasks
-                        matchedTasks.add(task);
-                    }
+                for (final var task : processTasks(request)) {
                     operation.accept(task);
-                });
+                }
             } catch (Exception e) {
                 allMatchedTasksRemovedListener.onFailure(e);
                 return;

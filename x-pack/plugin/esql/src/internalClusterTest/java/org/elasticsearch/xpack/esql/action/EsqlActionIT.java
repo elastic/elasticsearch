95ab9a7c863967d23c7138a871f821eb107931d9
Nik Everett <nik9000@gmail.com>
2023-05-24 11:21:43 -0400

    Fix broken test
    Like ships passing in the fog, I believe two PRs were merged that
    increased the number of functions. That broke the build because we count
    them....

--- x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/EsqlActionIT.java
+++ x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/EsqlActionIT.java
@@ -878,7 +878,7 @@ public class EsqlActionIT extends AbstractEsqlIntegTestCase {
     public void testShowFunctions() {
         EsqlQueryResponse results = run("show functions");
         assertThat(results.columns(), equalTo(List.of(new ColumnInfo("name", "keyword"), new ColumnInfo("synopsis", "keyword"))));
-        assertThat(results.values().size(), equalTo(31));
+        assertThat(results.values().size(), equalTo(32));
     }
 
     public void testInWithNullValue() {

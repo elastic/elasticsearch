5352c96f656b9cdc63f0f673c983150bea35c7ed
Kostas Krikellas <kostas.krikellas@elastic.co>
2023-05-24 15:23:56 +0300

    Remove explicit compression calls.
    This was added to prevent concurrency tests from failing, but it leads
    to reduces precision. Submit this to see if the concurrency tests are
    still failing.

--- server/src/main/java/org/elasticsearch/search/aggregations/metrics/AbstractInternalTDigestPercentiles.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/metrics/AbstractInternalTDigestPercentiles.java
@@ -45,10 +45,6 @@ abstract class AbstractInternalTDigestPercentiles extends InternalNumericMetrics
         this.keys = keys;
         this.state = state;
         this.keyed = keyed;
-
-        if (state != null) {
-            state.compress();
-        }
     }
 
     /**
@@ -60,7 +56,6 @@ abstract class AbstractInternalTDigestPercentiles extends InternalNumericMetrics
         if (in.getTransportVersion().onOrAfter(TransportVersion.V_8_8_0)) {
             if (in.readBoolean()) {
                 state = TDigestState.read(in);
-                state.compress();
             } else {
                 state = null;
             }
--- x-pack/plugin/analytics/src/main/java/org/elasticsearch/xpack/analytics/boxplot/InternalBoxplot.java
+++ x-pack/plugin/analytics/src/main/java/org/elasticsearch/xpack/analytics/boxplot/InternalBoxplot.java
@@ -176,7 +176,6 @@ public class InternalBoxplot extends InternalNumericMetricsAggregation.MultiValu
     InternalBoxplot(String name, TDigestState state, DocValueFormat formatter, Map<String, Object> metadata) {
         super(name, formatter, metadata);
         this.state = state;
-        this.state.compress();
     }
 
     /**
@@ -185,7 +184,6 @@ public class InternalBoxplot extends InternalNumericMetricsAggregation.MultiValu
     public InternalBoxplot(StreamInput in) throws IOException {
         super(in);
         state = TDigestState.read(in);
-        state.compress();
     }
 
     @Override

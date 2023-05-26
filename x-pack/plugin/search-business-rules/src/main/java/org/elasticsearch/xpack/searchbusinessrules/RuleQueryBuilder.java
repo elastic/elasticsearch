6835ee1f88d6fa43394e4624c9760b08ea258e2c
Kathleen DeRusso <kathleen.derusso@elastic.co>
2023-05-26 10:50:17 -0400

    IntelliJ gremlins

--- x-pack/plugin/search-business-rules/src/main/java/org/elasticsearch/xpack/searchbusinessrules/RuleQueryBuilder.java
+++ x-pack/plugin/search-business-rules/src/main/java/org/elasticsearch/xpack/searchbusinessrules/RuleQueryBuilder.java
@@ -4,14 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-package org.elasticsearch.xpack.searchbusinessrules;/*
-                                                    * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-                                                    * or more contributor license agreements. Licensed under the Elastic License
-                                                    * 2.0 and the Server Side Public License, v 1; you may not use this file except
-                                                    * in compliance with, at your election, the Elastic License 2.0 or the Server
-                                                    * Side Public License, v 1.
-                                                    */
+package org.elasticsearch.xpack.searchbusinessrules;
 
 import org.apache.lucene.search.Query;
 import org.apache.lucene.util.SetOnce;
@@ -237,6 +230,14 @@ public class RuleQueryBuilder extends AbstractQueryBuilder<RuleQueryBuilder> {
             boolQueryBuilder.should(boolSubQueryBuilder);
         }
         return boolQueryBuilder;
+    }
+
+    private void foo() {
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+
+        boolQueryBuilder.filter()
+
+        boolQueryBuilder.filter(QueryBuilders.termsQuery("ruleset_id", rulesetIds));
     }
 
     @Override

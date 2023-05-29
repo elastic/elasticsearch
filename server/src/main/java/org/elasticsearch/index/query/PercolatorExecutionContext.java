1068b4b9b8e303014fa735d3673f33b39b48f591
Salvatore Campagna <salvatore.campagna@elastic.co>
2023-05-29 15:34:15 +0200

    fix: make member variables private

--- server/src/main/java/org/elasticsearch/index/query/PercolatorExecutionContext.java
+++ server/src/main/java/org/elasticsearch/index/query/PercolatorExecutionContext.java
@@ -15,11 +15,15 @@ import org.elasticsearch.index.query.support.NestedScope;
 
 public class PercolatorExecutionContext extends SearchExecutionContext {
 
-    protected boolean allowUnmappedFields;
-    protected boolean mapUnmappedFieldAsString;
+    private boolean allowUnmappedFields;
+    private final boolean mapUnmappedFieldAsString;
 
-    public PercolatorExecutionContext(final SearchExecutionContext source, boolean allowUnmappedFields, boolean mapUnmappedFieldAsString) {
-        super(source);
+    public PercolatorExecutionContext(
+        final SearchExecutionContext searchExecutionContext,
+        boolean allowUnmappedFields,
+        boolean mapUnmappedFieldAsString
+    ) {
+        super(searchExecutionContext);
         this.allowUnmappedFields = allowUnmappedFields;
         this.mapUnmappedFieldAsString = mapUnmappedFieldAsString;
     }

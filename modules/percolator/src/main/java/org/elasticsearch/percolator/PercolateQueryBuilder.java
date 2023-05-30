7df4b1330358c93f8bb68592beb58057f52e71a6
Salvatore Campagna <salvatore.campagna@elastic.co>
2023-05-30 17:42:26 +0200

    Revert "fix: first create the context and then wrap it"
    This reverts commit 65dda6bc46e0e152eb663ccfad0128eee382fe3f.

--- modules/percolator/src/main/java/org/elasticsearch/percolator/PercolateQueryBuilder.java
+++ modules/percolator/src/main/java/org/elasticsearch/percolator/PercolateQueryBuilder.java
@@ -530,17 +530,11 @@ public class PercolateQueryBuilder extends AbstractQueryBuilder<PercolateQueryBu
 
         PercolatorFieldMapper.PercolatorFieldType pft = (PercolatorFieldMapper.PercolatorFieldType) fieldType;
         String queryName = this.name != null ? this.name : pft.name();
-        SearchExecutionContext percolateShardContext = wrap(PercolatorFieldMapper.configureContext(context, pft.mapUnmappedFieldsAsText));
-        PercolateQuery.QueryStore queryStore = createStore(pft.queryBuilderField, percolateShardContext);
+        SearchExecutionContext percolateShardContext = wrap(context);
+        SearchExecutionContext pec = PercolatorFieldMapper.configureContext(percolateShardContext, pft.mapUnmappedFieldsAsText);
+        PercolateQuery.QueryStore queryStore = createStore(pft.queryBuilderField, pec);
 
-        return pft.percolateQuery(
-            queryName,
-            queryStore,
-            documents,
-            docSearcher,
-            excludeNestedDocuments,
-            percolateShardContext.indexVersionCreated()
-        );
+        return pft.percolateQuery(queryName, queryStore, documents, docSearcher, excludeNestedDocuments, pec.indexVersionCreated());
     }
 
     public String getField() {

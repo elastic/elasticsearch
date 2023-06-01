8b1cd47455314ade5c09751620868860f42c423e
Michael Peterson <michael.peterson@elastic.co>
2023-06-01 10:34:16 -0400

    Support CCS minimize round trips in async search (#96012)
    * Support CCS minimize round trips in async search
    
    This commit makes the smallest set of changes to allow async-search based cross-cluster search
    to work with the CCS minimize_round_trips feature without changing the internals/architecture of
    the search action.
    
    When ccsMinimizeRoundtrips is set to true on SubmitAsyncSearchRequest, the AsyncSearchTask on the
    primary CCS coordinator sends a synchronous SearchRequest to all to clusters for a remote coordinator
    to orchestrate and return the entire result set to the CCS coordinator as a single response.
    
    This is the same functionality provided by synchronous CCS search using minimize_roundtrips.
    Since this is an async search, it means that the async search coordinator has no visibility
    into search progress on the remote clusters while they are running the search, thus losing one of
    the key features of async search. However, this is a good first approach for improving overall search
    latency for cross cluster searches that query a large number of shards on remote clusters, since
    Kibana does not currently expose incremental progress of an async search to users.
    
    Relates #73971

--- server/src/test/java/org/elasticsearch/action/search/TransportSearchActionTests.java
+++ server/src/test/java/org/elasticsearch/action/search/TransportSearchActionTests.java
@@ -63,7 +63,6 @@ import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.SearchService;
 import org.elasticsearch.search.SearchShardTarget;
-import org.elasticsearch.search.aggregations.AggregationReduceContext;
 import org.elasticsearch.search.aggregations.InternalAggregations;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.collapse.CollapseBuilder;
@@ -501,7 +500,6 @@ public class TransportSearchActionTests extends ESTestCase {
         boolean local = randomBoolean();
         OriginalIndices localIndices = local ? new OriginalIndices(new String[] { "index" }, SearchRequest.DEFAULT_INDICES_OPTIONS) : null;
         TransportSearchAction.SearchTimeProvider timeProvider = new TransportSearchAction.SearchTimeProvider(0, 0, () -> 0);
-        Function<Boolean, AggregationReduceContext> reduceContext = finalReduce -> null;
         try (
             MockTransportService service = MockTransportService.createNewService(
                 settings,

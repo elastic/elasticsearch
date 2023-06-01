c1a2a7bba69af47a013f0337db0cd46cfa6df26a
Tim Brooks <tim@timbrooks.org>
2023-06-01 17:16:18 -0600

    Increment transport version for plugin development
    Incrementing the transport version as a internal plugin has changed
    action serialization.

--- server/src/main/java/org/elasticsearch/TransportVersion.java
+++ server/src/main/java/org/elasticsearch/TransportVersion.java
@@ -127,12 +127,13 @@ public record TransportVersion(int id) implements Comparable<TransportVersion> {
     public static final TransportVersion V_8_500_004 = registerTransportVersion(8_500_004, "6a00db6a-fd66-42a9-97ea-f6cc53169110");
     public static final TransportVersion V_8_500_005 = registerTransportVersion(8_500_005, "65370d2a-d936-4383-a2e0-8403f708129b");
     public static final TransportVersion V_8_500_006 = registerTransportVersion(8_500_006, "7BB5621A-80AC-425F-BA88-75543C442F23");
+    public static final TransportVersion V_8_500_007 = registerTransportVersion(8_500_007, "8884ab9d-94cd-4bac-aff8-01f2c394f47c");
 
     /**
      * Reference to the most recent transport version.
      * This should be the transport version with the highest id.
      */
-    public static final TransportVersion CURRENT = V_8_500_006;
+    public static final TransportVersion CURRENT = V_8_500_007;
 
     /**
      * Reference to the earliest compatible transport version to this version of the codebase.

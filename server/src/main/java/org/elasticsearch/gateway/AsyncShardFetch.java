0b7943158739bca7452da52e35f3499b2cbec3e4
David Turner <david.turner@elastic.co>
2023-06-05 10:00:24 +0100

    Add one more consistency check in AsyncShardFetch (#96553)
    Relates #93632

--- server/src/main/java/org/elasticsearch/gateway/AsyncShardFetch.java
+++ server/src/main/java/org/elasticsearch/gateway/AsyncShardFetch.java
@@ -127,6 +127,8 @@ public abstract class AsyncShardFetch<T extends BaseNodeResponse> implements Rel
             asyncFetch(discoNodesToFetch, fetchingRound);
         }
 
+        assert assertFetchingCountConsistent();
+
         // if we are still fetching, return null to indicate it
         if (hasAnyNodeFetching()) {
             return new FetchResult<>(shardId, null, emptySet());

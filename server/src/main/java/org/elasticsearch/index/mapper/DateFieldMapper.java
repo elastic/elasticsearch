40140cc3ad31106f4b760f5bb56dd13cbf995c31
Salvatore Campagna <salvatore.campagna@elastic.co>
2023-06-05 11:12:49 +0200

    fix: not using null searcher anymore

--- server/src/main/java/org/elasticsearch/index/mapper/DateFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/DateFieldMapper.java
@@ -692,10 +692,6 @@ public final class DateFieldMapper extends FieldMapper {
             DateMathParser dateParser,
             QueryRewriteContext context
         ) throws IOException {
-            if (reader == null) {
-                // NOTE: in case we rewrite a query before we have a reader available
-                return Relation.INTERSECTS;
-            }
             if (isIndexed() == false && pointsMetadataAvailable == false && hasDocValues()) {
                 // we don't have a quick way to run this check on doc values, so fall back to default assuming we are within bounds
                 return Relation.INTERSECTS;

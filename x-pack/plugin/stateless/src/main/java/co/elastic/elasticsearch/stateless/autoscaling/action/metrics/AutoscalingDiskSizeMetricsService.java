ba84c4433fdf1b845832a2c17c69437c5f0adc6f
Mark Vieira <portugee@gmail.com>
2023-06-12 11:51:59 -0700

    Re-enabled license header check and fix existing license headers (#413)

--- x-pack/plugin/stateless/src/main/java/co/elastic/elasticsearch/stateless/autoscaling/action/metrics/AutoscalingDiskSizeMetricsService.java
+++ x-pack/plugin/stateless/src/main/java/co/elastic/elasticsearch/stateless/autoscaling/action/metrics/AutoscalingDiskSizeMetricsService.java
@@ -1,4 +1,21 @@
 /*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ * Copyright Elasticsearch B.V. All rights reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch B.V. and its suppliers, if any.
+ * The intellectual and technical concepts contained herein
+ * are proprietary to Elasticsearch B.V. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright
+ * law.  Dissemination of this information or reproduction of
+ * this material is strictly forbidden unless prior written
+ * permission is obtained from Elasticsearch B.V.
+ */
+
+/*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
  * 2.0 and the Server Side Public License, v 1; you may not use this file except
--- x-pack/plugin/stateless/src/main/java/co/elastic/elasticsearch/stateless/commits/BlobLocation.java
+++ x-pack/plugin/stateless/src/main/java/co/elastic/elasticsearch/stateless/commits/BlobLocation.java
@@ -1,9 +1,18 @@
 /*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ * Copyright Elasticsearch B.V. All rights reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch B.V. and its suppliers, if any.
+ * The intellectual and technical concepts contained herein
+ * are proprietary to Elasticsearch B.V. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright
+ * law.  Dissemination of this information or reproduction of
+ * this material is strictly forbidden unless prior written
+ * permission is obtained from Elasticsearch B.V.
  */
 
 package co.elastic.elasticsearch.stateless.commits;

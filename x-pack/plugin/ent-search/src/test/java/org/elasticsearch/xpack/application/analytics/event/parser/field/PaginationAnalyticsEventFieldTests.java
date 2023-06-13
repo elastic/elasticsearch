e6a104dead0071f4aca9fa5827d6dc2ef5915105
Simon Cooper <simon.cooper@elastic.co>
2023-06-13 12:56:22 +0100

    Missed one

--- x-pack/plugin/ent-search/src/test/java/org/elasticsearch/xpack/application/analytics/event/parser/field/PaginationAnalyticsEventFieldTests.java
+++ x-pack/plugin/ent-search/src/test/java/org/elasticsearch/xpack/application/analytics/event/parser/field/PaginationAnalyticsEventFieldTests.java
@@ -14,6 +14,7 @@ import org.elasticsearch.xcontent.XContentParser;
 import org.elasticsearch.xpack.application.analytics.event.AnalyticsEvent;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -57,7 +58,7 @@ public class PaginationAnalyticsEventFieldTests extends AnalyticsEventFieldParse
 
     @Override
     protected Map<String, Integer> createTestInstance() {
-        return randomEventSearchPaginationField();
+        return new HashMap<>(randomEventSearchPaginationField());
     }
 
     @Override

150b6cfa8ec36cfd3af9e69575cf3c6e1056c1ce
Brian Seeders <brian.seeders@elastic.co>
2023-06-14 13:47:47 -0400

    Add missing windows env vars

--- .buildkite/hooks/pre-command.bat
+++ .buildkite/hooks/pre-command.bat
@@ -6,4 +6,11 @@ SET JAVA_HOME="%USERPROFILE%\\.java\\%ES_BUILD_JAVA%"
 SET JAVA11_HOME="%USERPROFILE%\\.java\\java11"
 SET JAVA16_HOME="%USERPROFILE%\\.java\\openjdk16"
 
+SET GRADLEW_BAT=./gradlew.bat --parallel --scan --build-cache --no-watch-fs -Dorg.elasticsearch.build.cache.url=https://gradle-enterprise.elastic.co/cache/
+
+set WORKSPACE="%cd%"
+set BUILD_NUMBER="%BUILDKITE_BUILD_NUMBER%"
+set COMPOSE_HTTP_TIMEOUT="120"
+set JOB_BRANCH="%BUILDKITE_BRANCH%"
+
 set GRADLE_BUILD_CACHE_USERNAME=vault read -field=username secret/ci/elastic-elasticsearch/migrated/gradle-build-cache

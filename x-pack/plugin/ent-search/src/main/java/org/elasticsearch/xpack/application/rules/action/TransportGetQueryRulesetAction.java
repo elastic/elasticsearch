528b9ab7849e58a8671adfb349f78cfd8b816d3e
Kathleen DeRusso <kathleen.derusso@elastic.co>
2023-06-14 10:04:12 -0400

    Add Request classes

---
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/rules/action/TransportGetQueryRulesetAction.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.application.rules.action;
+
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.support.ActionFilters;
+import org.elasticsearch.action.support.HandledTransportAction;
+import org.elasticsearch.client.internal.Client;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.util.BigArrays;
+import org.elasticsearch.tasks.Task;
+import org.elasticsearch.transport.TransportService;
+import org.elasticsearch.xpack.application.rules.QueryRulesIndexService;
+import org.elasticsearch.xpack.application.search.SearchApplicationIndexService;
+import org.elasticsearch.xpack.application.search.action.GetSearchApplicationAction;
+
+public class TransportGetQueryRulesetAction extends HandledTransportAction<
+    GetQueryRulesetAction.Request,
+    GetQueryRulesetAction.Response> {
+
+    protected final QueryRulesIndexService systemIndexService;
+
+    @Inject
+    public TransportGetQueryRulesetAction(
+        TransportService transportService,
+        ActionFilters actionFilters,
+        Client client
+    ) {
+        super(GetQueryRulesetAction.NAME, transportService, actionFilters, GetQueryRulesetAction.Request::new);
+        this.systemIndexService = new QueryRulesIndexService(client);
+    }
+
+    @Override
+    protected void doExecute(
+        Task task,
+        GetQueryRulesetAction.Request request,
+        ActionListener<GetQueryRulesetAction.Response> listener
+    ) {
+        systemIndexService.getQueryRuleset(request.rulesetId(), listener.map(GetQueryRulesetAction.Response::new));
+    }
+}

3e4a196d3e005764964f06d23d4d560ecb3bbca4
Brian Seeders <brian.seeders@elastic.co>
2023-06-15 11:20:46 -0400

    Back to linux for now

--- .buildkite/generated-scripts/ca6b2b9c7738eac3d32fd7cd4ac2faa8.bat
+++
@@ -1,5 +0,0 @@
-del /f /s /q %USERPROFILE%\.gradle\init.d\*.*
-mkdir %USERPROFILE%\.gradle\init.d
-copy .ci\init.gradle %USERPROFILE%\.gradle\init.d\
-powershell.exe .\.ci\scripts\packaging-test.ps1 -GradleTasks destructiveDistroTest.%PACKAGING_TASK%   || exit /b 1
-

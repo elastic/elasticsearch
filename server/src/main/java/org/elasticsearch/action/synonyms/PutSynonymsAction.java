fe4726997c7765d59a61c73c17ee649ae98d1c4e
carlosdelest <carlos.delgado@elastic.co>
2023-06-20 17:58:47 +0200

    Remove PutSynonymsAction.Response

--- server/src/main/java/org/elasticsearch/action/synonyms/PutSynonymsAction.java
+++ server/src/main/java/org/elasticsearch/action/synonyms/PutSynonymsAction.java
@@ -10,31 +10,23 @@ package org.elasticsearch.action.synonyms;
 
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionRequestValidationException;
-import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.action.ActionType;
 import org.elasticsearch.action.ValidateActions;
-import org.elasticsearch.action.admin.indices.analyze.ReloadAnalyzersResponse;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.common.xcontent.StatusToXContentObject;
 import org.elasticsearch.common.xcontent.XContentHelper;
-import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.synonyms.SynonymRule;
 import org.elasticsearch.synonyms.SynonymsManagementAPIService;
-import org.elasticsearch.synonyms.SynonymsManagementAPIService.UpdateSynonymsResultStatus;
 import org.elasticsearch.xcontent.ConstructingObjectParser;
 import org.elasticsearch.xcontent.ParseField;
-import org.elasticsearch.xcontent.ToXContent;
-import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentParserConfiguration;
 import org.elasticsearch.xcontent.XContentType;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.Objects;
 
 public class PutSynonymsAction extends ActionType<SynonymUpdateResponse> {
@@ -118,63 +110,6 @@ public class PutSynonymsAction extends ActionType<SynonymUpdateResponse> {
         @Override
         public int hashCode() {
             return Objects.hash(synonymsSetId, Arrays.hashCode(synonymRules));
-        }
-    }
-
-    public static class Response extends ActionResponse implements StatusToXContentObject {
-
-        private final UpdateSynonymsResultStatus updateStatus;
-        private final ReloadAnalyzersResponse reloadAnalyzersResponse;
-
-        public Response(StreamInput in) throws IOException {
-            super(in);
-            this.updateStatus = in.readEnum(UpdateSynonymsResultStatus.class);
-            this.reloadAnalyzersResponse = new ReloadAnalyzersResponse(in);
-        }
-
-        public Response(UpdateSynonymsResultStatus updateStatus, ReloadAnalyzersResponse reloadAnalyzersResponse) {
-            super();
-            Objects.requireNonNull(updateStatus, "Update status must not be null");
-            Objects.requireNonNull(updateStatus, "Reload analyzers response must not be null");
-            this.updateStatus = updateStatus;
-            this.reloadAnalyzersResponse = reloadAnalyzersResponse;
-        }
-
-        @Override
-        public XContentBuilder toXContent(XContentBuilder builder, ToXContent.Params params) throws IOException {
-            builder.startObject();
-            builder.field("result", updateStatus.name().toLowerCase(Locale.ENGLISH));
-            builder.field("reload_analyzers_details");
-            reloadAnalyzersResponse.toXContent(builder, params);
-            builder.endObject();
-            return builder;
-        }
-
-        @Override
-        public void writeTo(StreamOutput out) throws IOException {
-            out.writeEnum(updateStatus);
-            reloadAnalyzersResponse.writeTo(out);
-        }
-
-        @Override
-        public RestStatus status() {
-            return switch (updateStatus) {
-                case CREATED -> RestStatus.CREATED;
-                default -> RestStatus.OK;
-            };
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Response response = (Response) o;
-            return updateStatus == response.updateStatus && Objects.equals(reloadAnalyzersResponse, response.reloadAnalyzersResponse);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(updateStatus, reloadAnalyzersResponse);
         }
     }
 }
--- server/src/test/java/org/elasticsearch/action/synonyms/SynonymUpdateResponseSerializingTests.java
+++ server/src/test/java/org/elasticsearch/action/synonyms/SynonymUpdateResponseSerializingTests.java
@@ -13,7 +13,7 @@ import org.elasticsearch.action.admin.indices.analyze.ReloadAnalyzersResponseTes
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.xcontent.XContentHelper;
-import org.elasticsearch.synonyms.SynonymsManagementAPIService.UpdateSynonymsResultStatus;
+import org.elasticsearch.synonyms.SynonymsManagementAPIService.SynonymsReloadResult;
 import org.elasticsearch.test.AbstractWireSerializingTestCase;
 
 import java.io.IOException;
@@ -23,23 +23,23 @@ import java.util.Map;
 import static org.elasticsearch.synonyms.SynonymsManagementAPIService.UpdateSynonymsResultStatus.CREATED;
 import static org.elasticsearch.synonyms.SynonymsManagementAPIService.UpdateSynonymsResultStatus.UPDATED;
 
-public class SynonymUpdateResponseSerializingTests extends AbstractWireSerializingTestCase<PutSynonymsAction.Response> {
+public class SynonymUpdateResponseSerializingTests extends AbstractWireSerializingTestCase<SynonymUpdateResponse> {
 
     @Override
-    protected Writeable.Reader<PutSynonymsAction.Response> instanceReader() {
-        return PutSynonymsAction.Response::new;
+    protected Writeable.Reader<SynonymUpdateResponse> instanceReader() {
+        return SynonymUpdateResponse::new;
     }
 
     @Override
-    protected PutSynonymsAction.Response createTestInstance() {
-        UpdateSynonymsResultStatus updateStatus = randomFrom(CREATED, UPDATED);
+    protected SynonymUpdateResponse createTestInstance() {
         Map<String, ReloadAnalyzersResponse.ReloadDetails> reloadedIndicesDetails = ReloadAnalyzersResponseTests
             .createRandomReloadDetails();
-        return new PutSynonymsAction.Response(updateStatus, new ReloadAnalyzersResponse(10, 10, 0, null, reloadedIndicesDetails));
+        ReloadAnalyzersResponse reloadAnalyzersResponse = new ReloadAnalyzersResponse(10, 10, 0, null, reloadedIndicesDetails);
+        return new SynonymUpdateResponse(new SynonymsReloadResult<>(randomFrom(CREATED, UPDATED), reloadAnalyzersResponse));
     }
 
     @Override
-    protected PutSynonymsAction.Response mutateInstance(PutSynonymsAction.Response instance) throws IOException {
+    protected SynonymUpdateResponse mutateInstance(SynonymUpdateResponse instance) throws IOException {
         return randomValueOtherThan(instance, this::createTestInstance);
     }
 
@@ -49,7 +49,7 @@ public class SynonymUpdateResponseSerializingTests extends AbstractWireSerializi
             new ReloadAnalyzersResponse.ReloadDetails("index", Collections.singleton("nodeId"), Collections.singleton("my_analyzer"))
         );
         ReloadAnalyzersResponse reloadAnalyzersResponse = new ReloadAnalyzersResponse(10, 5, 0, null, reloadedIndicesNodes);
-        PutSynonymsAction.Response response = new PutSynonymsAction.Response(CREATED, reloadAnalyzersResponse);
+        SynonymUpdateResponse response = new SynonymUpdateResponse(new SynonymsReloadResult<>(CREATED, reloadAnalyzersResponse));
 
         String output = Strings.toString(response);
         assertEquals(XContentHelper.stripWhitespace("""

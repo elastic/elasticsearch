c662442805b38d2813172e35a5adebc01f88ce95
Kostas Krikellas <kostas.krikellas@elastic.co>
2023-06-20 23:18:56 +0300

    Restore V_8_500_014 executionHint serialization.
    This was replaced by optional serialization in version
    V_8_500_018 (#96943), breaking backwards compatibility for serverless.
    
    Related to #95903

--- x-pack/plugin/analytics/src/main/java/org/elasticsearch/xpack/analytics/boxplot/BoxplotAggregationBuilder.java
+++ x-pack/plugin/analytics/src/main/java/org/elasticsearch/xpack/analytics/boxplot/BoxplotAggregationBuilder.java
@@ -84,6 +84,8 @@ public class BoxplotAggregationBuilder extends ValuesSourceAggregationBuilder.Me
         compression = in.readDouble();
         if (in.getTransportVersion().onOrAfter(TransportVersion.V_8_500_018)) {
             executionHint = in.readOptionalWriteable(TDigestExecutionHint::readFrom);
+        } else if (in.getTransportVersion().onOrAfter(TransportVersion.V_8_500_014)) {
+            executionHint = TDigestExecutionHint.readFrom(in);
         } else {
             executionHint = TDigestExecutionHint.HIGH_ACCURACY;
         }
@@ -99,6 +101,8 @@ public class BoxplotAggregationBuilder extends ValuesSourceAggregationBuilder.Me
         out.writeDouble(compression);
         if (out.getTransportVersion().onOrAfter(TransportVersion.V_8_500_018)) {
             out.writeOptionalWriteable(executionHint);
+        } else if (out.getTransportVersion().onOrAfter(TransportVersion.V_8_500_014)) {
+            (executionHint == null ? TDigestExecutionHint.DEFAULT : executionHint).writeTo(out);
         }
     }
 

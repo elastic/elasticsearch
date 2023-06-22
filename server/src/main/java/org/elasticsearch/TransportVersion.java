d803b502ab5947515f40010ecc0b87e02e820d7b
Craig Taverner <craig@amanzi.com>
2023-06-22 14:02:27 +0200

    Fix time-series geo_line to include reduce phase in MergedGeoLines (#96953) (#97005)
    Recently we added support for line-simplification in geo-lines when in
    time-series aggregations:
    https://github.com/elastic/elasticsearch/pull/94954. This work did not,
    however, cover the case of `MergedGeoLines`, which occurs when the same
    geo_line covers multiple shards on data nodes, and needs to be merged on
    the coordinating node. The original work would use line-simplification
    and time-ordering on the data node, but then revert to re-sorting
    (unnecessary) and truncation (incorrect) on the coordinating node.
    
    This PR rectifies that, and adds two fields to the InternalGeoLine: *
    nonOverlapping   * re-sorting is *NOT* required because the sort value
    ranges do not overlap, so we only sort the set of incoming geo_lines
    (easy/fast) instead of every single point (complex/slow). * simplified  
    * The approach to memory limiting is by line-simplification instead of
    truncation.   * If the data nodes are doing line-simplification, we want
    the coordinating node to do that as well
    
    For time-series, both of the above are true, and at this point all other
    cases will have both false. The reason for two booleans is: * They are
    not necessarily related concepts * We consider supporting
    line-simplification as a user-requested option in the non-time-series
    geo_line in future (much nicer than truncation)
    
    Fixes #96983

--- server/src/main/java/org/elasticsearch/TransportVersion.java
+++ server/src/main/java/org/elasticsearch/TransportVersion.java
@@ -145,8 +145,10 @@ public record TransportVersion(int id) implements Comparable<TransportVersion> {
     public static final TransportVersion V_8_500_018 = registerTransportVersion(8_500_018, "827C32CE-33D9-4AC3-A773-8FB768F59EAF");
     public static final TransportVersion V_8_500_019 = registerTransportVersion(8_500_019, "09bae57f-cab8-423c-aab3-c9778509ffe3");
 
+    public static final TransportVersion V_8_500_020 = registerTransportVersion(8_500_020, "ECB42C26-B258-42E5-A835-E31AF84A76DE");
+
     private static class CurrentHolder {
-        private static final TransportVersion CURRENT = findCurrent(V_8_500_019);
+        private static final TransportVersion CURRENT = findCurrent(V_8_500_020);
 
         // finds the pluggable current version, or uses the given fallback
         private static TransportVersion findCurrent(TransportVersion fallback) {

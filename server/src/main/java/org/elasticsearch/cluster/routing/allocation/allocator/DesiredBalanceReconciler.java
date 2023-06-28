9453d693932a126bf555f036ad1bf169df974ca7
David Turner <david.turner@elastic.co>
2023-06-28 14:08:10 +0100

    Stateless primary relocations (#97162)
    Adds support for proper online relocations of stateless primaries.

--- server/src/main/java/org/elasticsearch/cluster/routing/allocation/allocator/DesiredBalanceReconciler.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/allocation/allocator/DesiredBalanceReconciler.java
@@ -378,7 +378,7 @@ public class DesiredBalanceReconciler {
                 final var moveTarget = findRelocationTarget(shardRouting, assignment.nodeIds());
                 if (moveTarget != null) {
                     logger.debug("Moving shard {} from {} to {}", shardRouting.shardId(), shardRouting.currentNodeId(), moveTarget.getId());
-                    routingNodes.relocateOrReinitializeShard(
+                    routingNodes.relocateShard(
                         shardRouting,
                         moveTarget.getId(),
                         allocation.clusterInfo().getShardSize(shardRouting, ShardRouting.UNAVAILABLE_EXPECTED_SHARD_SIZE),
@@ -443,7 +443,7 @@ public class DesiredBalanceReconciler {
                         rebalanceTarget.getId()
                     );
 
-                    routingNodes.relocateOrReinitializeShard(
+                    routingNodes.relocateShard(
                         shardRouting,
                         rebalanceTarget.getId(),
                         allocation.clusterInfo().getShardSize(shardRouting, ShardRouting.UNAVAILABLE_EXPECTED_SHARD_SIZE),

ad3d42c6b5f4f776cb4775abdbba2f2cd1212473
Salvatore Campagna <salvatore.campagna@elastic.co>
2023-06-28 12:03:56 +0200

    test: named writable registry not necessary

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/rollup/action/RollupShardStatusSerializingTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/rollup/action/RollupShardStatusSerializingTests.java
@@ -7,7 +7,6 @@
 
 package org.elasticsearch.xpack.core.rollup.action;
 
-import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.io.stream.Writeable.Reader;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.rest.RestStatus;
@@ -15,19 +14,8 @@ import org.elasticsearch.test.AbstractXContentSerializingTestCase;
 import org.elasticsearch.xcontent.XContentParser;
 
 import java.io.IOException;
-import java.util.List;
 
 public class RollupShardStatusSerializingTests extends AbstractXContentSerializingTestCase<RollupShardStatus> {
-
-    @Override
-    protected NamedWriteableRegistry getNamedWriteableRegistry() {
-        return new NamedWriteableRegistry(
-            List.of(
-                new NamedWriteableRegistry.Entry(RollupBeforeBulkInfo.class, RollupBeforeBulkInfo.NAME, RollupBeforeBulkInfo::new),
-                new NamedWriteableRegistry.Entry(RollupAfterBulkInfo.class, RollupAfterBulkInfo.NAME, RollupAfterBulkInfo::new)
-            )
-        );
-    }
 
     @Override
     protected RollupShardStatus doParseInstance(XContentParser parser) throws IOException {

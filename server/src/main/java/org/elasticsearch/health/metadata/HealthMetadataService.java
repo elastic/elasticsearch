dafecdd247bd6561dcdc810ec5fddb8a866862b6
Pablo Alcantar Morales <pablo.alcantar.morales@gmail.com>
2023-06-28 10:11:12 +0200

    Update server/src/main/java/org/elasticsearch/health/metadata/HealthMetadataService.java
    Co-authored-by: Mary Gouseti <mgouseti@gmail.com>

--- server/src/main/java/org/elasticsearch/health/metadata/HealthMetadataService.java
+++ server/src/main/java/org/elasticsearch/health/metadata/HealthMetadataService.java
@@ -61,7 +61,7 @@ public class HealthMetadataService {
     private volatile boolean isMaster = false;
     // we hold an in-memory representation of the healthMetadata which will be updated by the settings updaters. This way, we can
     // get the initial values (reading default values from `Settings`) and then, potentially, user changed values (they'll be received
-    // through the settings updater). Later, we always post the latest in-memory HealthMetadata to the ClusterState to maintain an
+    // through the settings updater). Later and if this node is the master, we will always post the latest in-memory HealthMetadata to the ClusterState to maintain an
     // up-to-date version of it across the cluster.
     private volatile HealthMetadata localHealthMetadata;
 

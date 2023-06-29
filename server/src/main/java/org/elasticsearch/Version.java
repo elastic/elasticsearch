e3fe4cfa087696c3cc7ea9dbe4b9869ca0e538c2
Mark Vieira <portugee@gmail.com>
2023-06-29 12:27:49 -0700

    Bump versions after 8.8.2 release

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -142,6 +142,7 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_8_8_0 = new Version(8_08_00_99, IndexVersion.V_8_8_0);
     public static final Version V_8_8_1 = new Version(8_08_01_99, IndexVersion.V_8_8_1);
     public static final Version V_8_8_2 = new Version(8_08_02_99, IndexVersion.V_8_8_2);
+    public static final Version V_8_8_3 = new Version(8_08_03_99, IndexVersion.V_8_8_3);
     public static final Version V_8_9_0 = new Version(8_09_00_99, IndexVersion.V_8_9_0);
     public static final Version V_8_10_0 = new Version(8_10_00_99, IndexVersion.V_8_10_0);
     public static final Version CURRENT = V_8_10_0;

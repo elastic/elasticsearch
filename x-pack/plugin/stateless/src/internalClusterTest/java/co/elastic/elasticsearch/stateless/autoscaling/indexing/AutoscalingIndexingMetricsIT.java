40dc5732d71bca0870ac5c2eabf437556aeda727
Pooya Salehi <pxsalehi@users.noreply.github.com>
2023-06-30 11:56:42 +0200

    Add IT for index autoscaling with queueing (#483)
    Relates #453

--- x-pack/plugin/stateless/src/internalClusterTest/java/co/elastic/elasticsearch/stateless/autoscaling/indexing/AutoscalingIndexingMetricsIT.java
+++ x-pack/plugin/stateless/src/internalClusterTest/java/co/elastic/elasticsearch/stateless/autoscaling/indexing/AutoscalingIndexingMetricsIT.java
@@ -21,17 +21,26 @@ import co.elastic.elasticsearch.stateless.AbstractStatelessIntegTestCase;
 import co.elastic.elasticsearch.stateless.autoscaling.MetricQuality;
 
 import org.elasticsearch.action.admin.cluster.settings.ClusterGetSettingsAction;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.concurrent.TaskExecutionTimeTrackingEsThreadPoolExecutor;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.test.transport.MockTransportService;
+import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 public class AutoscalingIndexingMetricsIT extends AbstractStatelessIntegTestCase {
     public void testIndexingMetricsArePublishedEventually() throws Exception {
@@ -90,5 +99,77 @@ public class AutoscalingIndexingMetricsIT extends AbstractStatelessIntegTestCase
             getSettingsResponse.settings().get(IngestLoadProbe.MAX_TIME_TO_CLEAR_QUEUE.getKey()),
             equalTo(TimeValue.timeValueSeconds(1).getStringRep())
         );
+    }
+
+    public void testAutoscalingWithQueueSize() throws Exception {
+        startMasterOnlyNode();
+        // Reduce the time between publications, so we can expect at least one publication per second.
+        var indexNodeName = startIndexNode(
+            Settings.builder()
+                .put(IngestLoadSampler.MAX_TIME_BETWEEN_METRIC_PUBLICATIONS_SETTING.getKey(), TimeValue.timeValueSeconds(1))
+                .put(IngestLoadProbe.MAX_TIME_TO_CLEAR_QUEUE.getKey(), TimeValue.timeValueSeconds(1))
+                .build()
+        );
+
+        final AtomicInteger ingestLoadPublishSent = new AtomicInteger(0);
+        for (var transportService : internalCluster().getInstances(TransportService.class)) {
+            MockTransportService mockTransportService = (MockTransportService) transportService;
+            mockTransportService.addSendBehavior((connection, requestId, action, request, options) -> {
+                if (action.equals(PublishNodeIngestLoadAction.NAME)) {
+                    ingestLoadPublishSent.incrementAndGet();
+                }
+                connection.sendRequest(requestId, action, request, options);
+            });
+        }
+
+        assertBusy(() -> assertThat(ingestLoadPublishSent.get(), equalTo(1)));
+        var metrics = internalCluster().getCurrentMasterNodeInstance(IngestMetricsService.class).getIndexTierMetrics();
+        assertThat(metrics.toString(), metrics.nodesLoad().size(), equalTo(1));
+        assertThat(metrics.toString(), metrics.nodesLoad().get(0).metricQuality(), equalTo(MetricQuality.EXACT));
+        assertThat(metrics.toString(), metrics.nodesLoad().get(0).load(), equalTo(0.0));
+
+        final String indexName = randomAlphaOfLength(10).toLowerCase(Locale.ROOT);
+        createIndex(indexName, indexSettings(1, 0).build());
+        ensureGreen(indexName);
+
+        // some write so that the WRITE EWMA is not zero
+        indexDocs(indexName, randomIntBetween(5000, 10000));
+        assertBusy(() -> {
+            var metricsAfter = internalCluster().getCurrentMasterNodeInstance(IngestMetricsService.class).getIndexTierMetrics();
+            assertThat(metricsAfter.toString(), metricsAfter.nodesLoad().size(), equalTo(1));
+            assertThat(metricsAfter.toString(), metricsAfter.nodesLoad().get(0).metricQuality(), equalTo(MetricQuality.EXACT));
+            assertThat(metricsAfter.toString(), metricsAfter.nodesLoad().get(0).load(), allOf(greaterThan(0.0), lessThanOrEqualTo(1.0)));
+        });
+        // Block the executor workers to pile up writes
+        var threadpool = internalCluster().getInstance(ThreadPool.class, indexNodeName);
+        var executor = (TaskExecutionTimeTrackingEsThreadPoolExecutor) threadpool.executor(ThreadPool.Names.WRITE);
+        final var executorThreads = threadpool.info(ThreadPool.Names.WRITE).getMax();
+        var barrier = new CyclicBarrier(executorThreads + 1);
+        for (int i = 0; i < executorThreads; i++) {
+            executor.execute(() -> {
+                try {
+                    barrier.await(30, TimeUnit.SECONDS);
+                } catch (InterruptedException | BrokenBarrierException | TimeoutException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        // TODO: after https://elasticco.atlassian.net/browse/ES-6391 much smaller queues would also lead to higher ingestion load
+        // since the busy WRITE threads would be sampled as they are running.
+        var writeRequests = randomIntBetween(1500, 3000);
+        for (int i = 0; i < writeRequests; i++) {
+            client().prepareBulk().add(new IndexRequest(indexName).source("field", i)).execute();
+        }
+        // Wait for at least one more publish
+        assertBusy(() -> assertThat(ingestLoadPublishSent.get(), greaterThan(1)));
+        // We'd need an assertBusy since the second publish might still miss the recent load.
+        // Eventually just because of queueing, the load will go above the current available threads
+        assertBusy(() -> {
+            var metricsAfter = internalCluster().getCurrentMasterNodeInstance(IngestMetricsService.class).getIndexTierMetrics();
+            assertThat(metricsAfter.toString(), metricsAfter.nodesLoad().size(), equalTo(1));
+            assertThat(metricsAfter.toString(), metricsAfter.nodesLoad().get(0).metricQuality(), equalTo(MetricQuality.EXACT));
+            assertThat(metricsAfter.toString(), metricsAfter.nodesLoad().get(0).load(), greaterThan((double) executorThreads));
+        });
+        barrier.await(30, TimeUnit.SECONDS);
     }
 }

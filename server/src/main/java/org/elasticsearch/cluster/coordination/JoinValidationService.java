8be19bab699dd5e5245726778093918ec9f29da2
David Turner <david.turner@elastic.co>
2023-07-01 11:22:55 +0100

    Avoid getStateForMasterService where possible
    `Coordinator#getStateForMasterService` will rebuild the state to add a
    no-master block, which can be rather expensive if there are lots of
    index blocks to process too. This commit replaces most of its usages
    with something cheaper.
    
    Relates #97295

--- server/src/main/java/org/elasticsearch/cluster/coordination/JoinValidationService.java
+++ server/src/main/java/org/elasticsearch/cluster/coordination/JoinValidationService.java
@@ -156,7 +156,7 @@ public class JoinValidationService {
             transportService.sendRequest(
                 discoveryNode,
                 JOIN_VALIDATE_ACTION_NAME,
-                new ValidateJoinRequest(clusterStateSupplier.get()),
+                new ValidateJoinRequest(getClusterState()),
                 REQUEST_OPTIONS,
                 new ActionListenerResponseHandler<>(listener.delegateResponse((l, e) -> {
                     logger.warn(() -> "failed to validate incoming join request from node [" + discoveryNode + "]", e);
@@ -353,7 +353,7 @@ public class JoinValidationService {
         final var bytesStream = transportService.newNetworkBytesStream();
         var success = false;
         try {
-            final var clusterState = clusterStateSupplier.get();
+            final var clusterState = getClusterState();
             try (
                 var stream = new OutputStreamStreamOutput(
                     CompressorFactory.COMPRESSOR.threadLocalOutputStream(Streams.flushOnCloseStream(bytesStream))
@@ -381,5 +381,15 @@ public class JoinValidationService {
                 assert false;
             }
         }
+    }
+
+    private ClusterState getClusterState() {
+        final var clusterState = clusterStateSupplier.get();
+        if (clusterState == null) {
+            final String message = "join validation before cluster state set";
+            assert false : message;
+            throw new CoordinationStateRejectedException(message);
+        }
+        return clusterState;
     }
 }

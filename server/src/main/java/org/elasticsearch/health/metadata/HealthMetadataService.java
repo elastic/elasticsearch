68710207177cfc6179d08af6e36cd2412f31fd1f
Pablo Alcantar Morales <pablo.alcantar@elastic.co>
2023-07-03 14:37:56 +0200

    spotless, love to hate you

--- server/src/main/java/org/elasticsearch/health/metadata/HealthMetadataService.java
+++ server/src/main/java/org/elasticsearch/health/metadata/HealthMetadataService.java
@@ -146,10 +146,10 @@ public class HealthMetadataService {
             return;
         }
 
-     final boolean prevIsMaster = this.isMaster;
-     if (prevIsMaster != event.localNodeMaster()) {
-        this.isMaster = event.localNodeMaster(); 
-      }
+        final boolean prevIsMaster = this.isMaster;
+        if (prevIsMaster != event.localNodeMaster()) {
+            this.isMaster = event.localNodeMaster();
+        }
         if (canPostClusterStateUpdates(event.state())) {
             if (this.localHealthMetadata.equals(HealthMetadata.getFromClusterState(event.state())) == false) {
                 taskQueue.submitTask("store-local-health-metadata", () -> this.localHealthMetadata, null);

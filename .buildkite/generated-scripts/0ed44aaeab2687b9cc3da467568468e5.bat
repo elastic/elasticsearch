0fac8332be537a1aedff323c0bccc495af6ed7aa
Brian Seeders <brian.seeders@elastic.co>
2023-07-03 17:06:32 -0400

    Test a few things

--- .buildkite/generated-scripts/0ed44aaeab2687b9cc3da467568468e5.bat
+++ .buildkite/generated-scripts/0ed44aaeab2687b9cc3da467568468e5.bat
@@ -1,5 +1,5 @@
 del /f /s /q %USERPROFILE%\.gradle\init.d\*.*
 mkdir %USERPROFILE%\.gradle\init.d
 copy .ci\init.gradle %USERPROFILE%\.gradle\init.d\
-call %GRADLEW_BAT% --max-workers=4 -Dbwc.checkout.align=true %GRADLE_TASK% || exit /b 1
+call %GRADLEW_BAT% --max-workers=4 -Dbwc.checkout.align=true %GRADLE_TASK%
 

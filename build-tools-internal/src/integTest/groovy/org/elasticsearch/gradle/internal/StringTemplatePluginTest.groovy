a750ce125a3e12d32a58ecd89b45d2c1e5f7a1f5
ChrisHegarty <christopher.hegarty@elastic.co>
2023-07-05 13:55:59 +0100

    more tests

--- build-tools-internal/src/integTest/groovy/org/elasticsearch/gradle/internal/StringTemplatePluginTest.groovy
+++ build-tools-internal/src/integTest/groovy/org/elasticsearch/gradle/internal/StringTemplatePluginTest.groovy
@@ -13,7 +13,7 @@ import org.gradle.testkit.runner.TaskOutcome;
 
 class StringTemplatePluginTest extends AbstractGradleFuncTest {
 
-    def "test simple substitution"() {
+    def "test substitution"() {
         given:
         internalBuild()
         file('src/main/p/X-Box.java.st') << """
@@ -23,7 +23,7 @@ class StringTemplatePluginTest extends AbstractGradleFuncTest {
               this.value = value;
             }
           }
-        """
+        """.stripIndent().stripTrailing()
 
         buildFile << """
           apply plugin: 'elasticsearch.build'
@@ -50,6 +50,93 @@ class StringTemplatePluginTest extends AbstractGradleFuncTest {
         result.task(":stringTemplates").outcome == TaskOutcome.SUCCESS
         file("src/main/generated-src/p/IntBox.java").exists()
         file("src/main/generated-src/p/LongBox.java").exists()
-        //TODO: assert output
+
+        //assert output
+        normalized(file("src/main/generated-src/p/IntBox.java").text) == """
+            public class IntBox {
+              final int value;
+              public Code(int value) {
+                this.value = value;
+              }
+            }
+          """.stripIndent().stripTrailing()
+        normalized(file("src/main/generated-src/p/LongBox.java").text) == """
+            public class LongBox {
+              final long value;
+              public Code(long value) {
+                this.value = value;
+              }
+            }
+          """.stripIndent().stripTrailing()
+    }
+
+    def "test basic conditional"() {
+        given:
+        internalBuild()
+        file('src/main/Color.txt.st') << """
+          \$if(Red)\$1 Red\$endif\$
+          \$if(Blue)\$2 Blue\$endif\$
+          \$if(Green)\$3 Green\$endif\$
+        """.stripIndent().stripTrailing()
+
+        buildFile << """
+          apply plugin: 'elasticsearch.build'
+          apply plugin: 'elasticsearch.string-templates'
+
+          tasks.named("stringTemplates").configure {
+            template {
+              it.properties = ["Red" : "true", "Blue" : "", "Green" : "true"]
+              it.inputFile = new File("${projectDir}/src/main/Color.txt.st")
+              it.outputFile = "Color.txt"
+            }
+          }
+        """
+
+        when:
+        def result = gradleRunner("stringTemplates", '-g', gradleUserHome).build()
+
+        then:
+        result.task(":stringTemplates").outcome == TaskOutcome.SUCCESS
+        file("src/main/generated-src/Color.txt").exists()
+        //assert output
+        normalized(file("src/main/generated-src/Color.txt").text) == """
+            1 Red
+            3 Green
+          """.stripIndent().stripTrailing()
+    }
+
+    def "test if then else"() {
+        given:
+        internalBuild()
+        file('src/main/Token.txt.st') << """
+          \$if(Foo)\$1 Foo
+          \$elseif(Bar)\$2 Bar
+          \$else\$3 Baz
+          \$endif\$
+        """.stripIndent().stripTrailing()
+
+        buildFile << """
+          apply plugin: 'elasticsearch.build'
+          apply plugin: 'elasticsearch.string-templates'
+
+          tasks.named("stringTemplates").configure {
+            template {
+              it.properties = [:] // no properties
+              it.inputFile = new File("${projectDir}/src/main/Token.txt.st")
+              it.outputFile = "Token.txt"
+            }
+          }
+        """
+
+        when:
+        def result = gradleRunner("stringTemplates", '-g', gradleUserHome).build()
+
+        then:
+        result.task(":stringTemplates").outcome == TaskOutcome.SUCCESS
+        file("src/main/generated-src/Token.txt").exists()
+        //assert output
+        normalized(file("src/main/generated-src/Token.txt").text) == """
+            3 Baz
+          """.stripIndent().stripTrailing()
     }
 }

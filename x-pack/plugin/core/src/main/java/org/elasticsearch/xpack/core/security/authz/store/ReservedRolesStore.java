2de067300e88ee8bded68219e8602e9fecf74b4f
Søren Louv-Jansen <sorenlouv@gmail.com>
2023-07-05 09:40:25 +0200

    Formatting

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStore.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStore.java
@@ -374,7 +374,9 @@ public class ReservedRolesStore implements BiConsumer<Set<String>, ActionListene
                             .build() },
                     null,
                     null,
-                    MetadataUtils.getDeprecatedReservedMetadata("This role will be removed in a future major release. Please use editor and viewer roles instead"),
+                    MetadataUtils.getDeprecatedReservedMetadata(
+                        "This role will be removed in a future major release. Please use editor and viewer roles instead"
+                    ),
                     null
                 )
             ),

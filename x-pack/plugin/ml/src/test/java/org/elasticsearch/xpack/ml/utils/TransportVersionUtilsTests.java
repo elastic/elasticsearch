e863be051e1c4413918e16c912e7c0de458eedb0
Ed Savage <ed.savage@elastic.co>
2023-07-05 07:31:51 +0100

    Make unit test more deterministic

--- x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/utils/TransportVersionUtilsTests.java
+++ x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/utils/TransportVersionUtilsTests.java
@@ -13,23 +13,24 @@ import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.xpack.core.ml.utils.TransportVersionUtils;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.hamcrest.Matchers.equalTo;
 
 public class TransportVersionUtilsTests extends ESTestCase {
 
-    private static Map<String, TransportVersion> transportVersions;
-    static {
-        transportVersions = new HashMap<>();
-        transportVersions.put("Alfredo", TransportVersion.V_7_0_0);
-        transportVersions.put("Bertram", TransportVersion.V_7_0_1);
-        transportVersions.put("Charles", TransportVersion.V_8_500_003);
-        transportVersions.put("Dominic", TransportVersion.V_8_0_0);
-    }
+    private static final Map<String, TransportVersion> transportVersions = Map.of(
+        "Alfredo",
+        TransportVersion.V_7_0_0,
+        "Bertram",
+        TransportVersion.V_7_0_1,
+        "Charles",
+        TransportVersion.V_8_500_003,
+        "Dominic",
+        TransportVersion.V_8_0_0
+    );
 
-    private static ClusterState state = new ClusterState(
+    private static final ClusterState state = new ClusterState(
         new ClusterName("fred"),
         0L,
         "EC7C0637-1644-43AB-AEAB-D8B7970CAECA",
@@ -72,23 +73,31 @@ public class TransportVersionUtilsTests extends ESTestCase {
     public void testAreAllTransformVersionsTheSame() {
         assertThat(TransportVersionUtils.areAllTransformVersionsTheSame(state), equalTo(false));
 
-        // mutate the versions so that they are al the same
-        transportVersions.replaceAll((k, v) -> v = TransportVersion.V_7_0_0);
+        Map<String, TransportVersion> transportVersions1 = Map.of(
+            "Alfredo",
+            TransportVersion.V_7_0_0,
+            "Bertram",
+            TransportVersion.V_7_0_0,
+            "Charles",
+            TransportVersion.V_7_0_0,
+            "Dominic",
+            TransportVersion.V_7_0_0
+        );
 
-        state = new ClusterState(
+        ClusterState state1 = new ClusterState(
             new ClusterName("harry"),
             0L,
             "20F833F2-7C48-4522-BA78-6821C9DCD5D8",
             null,
             null,
             null,
-            transportVersions,
+            transportVersions1,
             null,
             null,
             false,
             null
         );
 
-        assertThat(TransportVersionUtils.areAllTransformVersionsTheSame(state), equalTo(true));
+        assertThat(TransportVersionUtils.areAllTransformVersionsTheSame(state1), equalTo(true));
     }
 }

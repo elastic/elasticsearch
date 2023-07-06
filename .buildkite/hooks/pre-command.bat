d97d41916bbf60171906f34f77b2433e811b7cb9
Brian Seeders <brian.seeders@elastic.co>
2023-07-06 17:50:01 -0400

    Try some bash

--- .buildkite/hooks/pre-command.bat
+++ .buildkite/hooks/pre-command.bat
@@ -1,26 +1,26 @@
-@ECHO OFF
+@REM @ECHO OFF
 
-FOR /F "tokens=* eol=#" %%i in ('type .ci\java-versions.properties') do set %%i
+@REM FOR /F "tokens=* eol=#" %%i in ('type .ci\java-versions.properties') do set %%i
 
-SET JAVA_HOME=%USERPROFILE%\.java\%ES_BUILD_JAVA%
-SET JAVA11_HOME=%USERPROFILE%\.java\java11
-SET JAVA16_HOME=%USERPROFILE%\.java\openjdk16
+@REM SET JAVA_HOME=%USERPROFILE%\.java\%ES_BUILD_JAVA%
+@REM SET JAVA11_HOME=%USERPROFILE%\.java\java11
+@REM SET JAVA16_HOME=%USERPROFILE%\.java\openjdk16
 
-SET GRADLEW_BAT=./gradlew.bat --parallel --build-cache --no-watch-fs -Dorg.elasticsearch.build.cache.url=https://gradle-enterprise.elastic.co/cache/
+@REM SET GRADLEW_BAT=./gradlew.bat --parallel --build-cache --no-watch-fs -Dorg.elasticsearch.build.cache.url=https://gradle-enterprise.elastic.co/cache/
 
-set WORKSPACE=%cd%
-set BUILD_NUMBER=%BUILDKITE_BUILD_NUMBER%
-set COMPOSE_HTTP_TIMEOUT=120
-set JOB_BRANCH=%BUILDKITE_BRANCH%
+@REM set WORKSPACE=%cd%
+@REM set BUILD_NUMBER=%BUILDKITE_BUILD_NUMBER%
+@REM set COMPOSE_HTTP_TIMEOUT=120
+@REM set JOB_BRANCH=%BUILDKITE_BRANCH%
 
-set GRADLE_BUILD_CACHE_USERNAME=vault read -field=username secret/ci/elastic-elasticsearch/migrated/gradle-build-cache
-set GRADLE_BUILD_CACHE_PASSWORD=vault read -field=password secret/ci/elastic-elasticsearch/migrated/gradle-build-cache
+@REM set GRADLE_BUILD_CACHE_USERNAME=vault read -field=username secret/ci/elastic-elasticsearch/migrated/gradle-build-cache
+@REM set GRADLE_BUILD_CACHE_PASSWORD=vault read -field=password secret/ci/elastic-elasticsearch/migrated/gradle-build-cache
 
-powershell -Command "Invoke-WebRequest https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_windows-x64_bin.zip -OutFile openjdk-17.0.2_windows-x64_bin.zip"
-powershell -Command "Expand-Archive openjdk-17.0.2_windows-x64_bin.zip"
+@REM powershell -Command "Invoke-WebRequest https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_windows-x64_bin.zip -OutFile openjdk-17.0.2_windows-x64_bin.zip"
+@REM powershell -Command "Expand-Archive openjdk-17.0.2_windows-x64_bin.zip"
 
-md "%JAVA_HOME%"
-rem move openjdk-17.0.2_windows-x64_bin\jdk-17.0.2 "%JAVA_HOME%"
-robocopy openjdk-17.0.2_windows-x64_bin\jdk-17.0.2\ %JAVA_HOME% /E /MOV /NFL /NDL
+@REM md "%JAVA_HOME%"
+@REM rem move openjdk-17.0.2_windows-x64_bin\jdk-17.0.2 "%JAVA_HOME%"
+@REM robocopy openjdk-17.0.2_windows-x64_bin\jdk-17.0.2\ %JAVA_HOME% /E /MOV /NFL /NDL
 
-exit /b 0
+@REM exit /b 0
--- .ci/scripts/run-gradle.sh
+++ .ci/scripts/run-gradle.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
-# drop page cache and kernel slab objects on linux
-[[ -x /usr/local/sbin/drop-caches ]] && sudo /usr/local/sbin/drop-caches
+
+if [[ -z "${WORKSPACE:-}" && "$BUILDKITE" ]]; then
+  source .buildkite/hooks/pre-command
+fi
 
 GRADLE_USER_HOME="$HOME/.gradle"
 export GRADLE_USER_HOME
 
 rm -Rfv ~/.gradle/init.d
 mkdir -p ~/.gradle/init.d && cp -v $WORKSPACE/.ci/init.gradle ~/.gradle/init.d
-if [ "$(uname -m)" = "arm64" ] || [ "$(uname -m)" = "aarch64" ]; then
-   MAX_WORKERS=16
-elif [ -f /proc/cpuinfo ]; then
-   MAX_WORKERS=`grep '^cpu\scores' /proc/cpuinfo  | uniq | sed 's/\s\+//g' |  cut -d':' -f 2`
-else
-   if [[ "$OSTYPE" == "darwin"* ]]; then
-      MAX_WORKERS=`sysctl -n hw.physicalcpu | sed 's/\s\+//g'`
-   else
-      echo "Unsupported OS Type: $OSTYPE"
-      exit 1
-   fi
-fi
-if pwd | grep -v -q ^/dev/shm ; then
-   echo "Not running on a ramdisk, reducing number of workers"
-   MAX_WORKERS=$(($MAX_WORKERS*2/3))
-fi
 
-# Export glibc version as environment variable since some BWC tests are incompatible with later versions
-export GLIBC_VERSION=$(ldd --version | grep '^ldd' | sed 's/.* \([1-9]\.[0-9]*\).*/\1/')
+MAX_WORKERS=4
+
+# Don't run this stuff on Windows
+if ! uname -a | grep -q MING; then
+  # drop page cache and kernel slab objects on linux
+  [[ -x /usr/local/sbin/drop-caches ]] && sudo /usr/local/sbin/drop-caches
+
+  if [ "$(uname -m)" = "arm64" ] || [ "$(uname -m)" = "aarch64" ]; then
+    MAX_WORKERS=16
+  elif [ -f /proc/cpuinfo ]; then
+    MAX_WORKERS=`grep '^cpu\scores' /proc/cpuinfo  | uniq | sed 's/\s\+//g' |  cut -d':' -f 2`
+  else
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        MAX_WORKERS=`sysctl -n hw.physicalcpu | sed 's/\s\+//g'`
+    else
+        echo "Unsupported OS Type: $OSTYPE"
+        exit 1
+    fi
+  fi
+  if pwd | grep -v -q ^/dev/shm ; then
+    echo "Not running on a ramdisk, reducing number of workers"
+    MAX_WORKERS=$(($MAX_WORKERS*2/3))
+  fi
+
+  # Export glibc version as environment variable since some BWC tests are incompatible with later versions
+  export GLIBC_VERSION=$(ldd --version | grep '^ldd' | sed 's/.* \([1-9]\.[0-9]*\).*/\1/')
+fi
 
 set -e
 $GRADLEW -S --max-workers=$MAX_WORKERS $@

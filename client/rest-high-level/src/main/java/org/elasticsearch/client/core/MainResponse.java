388b3f5cd7126e60a07a72b84c1e9f9b37b6eab7
Simon Cooper <simon.cooper@elastic.co>
2023-07-07 16:21:48 +0100

    Undo mainresponse changes

--- client/rest-high-level/src/main/java/org/elasticsearch/client/core/MainResponse.java
+++ client/rest-high-level/src/main/java/org/elasticsearch/client/core/MainResponse.java
@@ -12,7 +12,9 @@ import org.elasticsearch.xcontent.ConstructingObjectParser;
 import org.elasticsearch.xcontent.ParseField;
 import org.elasticsearch.xcontent.XContentParser;
 
-public record MainResponse(String nodeName, Version version, String clusterName, String clusterUuid, String tagline) {
+import java.util.Objects;
+
+public class MainResponse {
 
     private static final ConstructingObjectParser<MainResponse, Void> PARSER = new ConstructingObjectParser<>(
         MainResponse.class.getName(),
@@ -28,6 +30,21 @@ public record MainResponse(String nodeName, Version version, String clusterName,
         PARSER.declareString(ConstructingObjectParser.constructorArg(), new ParseField("cluster_name"));
         PARSER.declareString(ConstructingObjectParser.constructorArg(), new ParseField("cluster_uuid"));
         PARSER.declareString(ConstructingObjectParser.constructorArg(), new ParseField("tagline"));
+
+    }
+
+    private final String nodeName;
+    private final Version version;
+    private final String clusterName;
+    private final String clusterUuid;
+    private final String tagline;
+
+    public MainResponse(String nodeName, Version version, String clusterName, String clusterUuid, String tagline) {
+        this.nodeName = nodeName;
+        this.version = version;
+        this.clusterName = clusterName;
+        this.clusterUuid = clusterUuid;
+        this.tagline = tagline;
     }
 
     public String getNodeName() {
@@ -54,36 +71,40 @@ public record MainResponse(String nodeName, Version version, String clusterName,
         return PARSER.apply(parser, null);
     }
 
-    public record Version(
-        String number,
-        String buildFlavor,
-        String buildType,
-        String buildHash,
-        String buildDate,
-        boolean isSnapshot,
-        String luceneVersion,
-        String indexVersion,
-        String minimumWireCompatibilityVersion,
-        String minimumIndexCompatibilityVersion,
-        String transportVersion
-    ) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MainResponse that = (MainResponse) o;
+        return nodeName.equals(that.nodeName)
+            && version.equals(that.version)
+            && clusterName.equals(that.clusterName)
+            && clusterUuid.equals(that.clusterUuid)
+            && tagline.equals(that.tagline);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(nodeName, version, clusterName, clusterUuid, tagline);
+    }
+
+    public static class Version {
         private static final ConstructingObjectParser<Version, Void> PARSER = new ConstructingObjectParser<>(
             Version.class.getName(),
             true,
-            args -> new Version(
-                (String) args[0],
-                (String) args[1],
-                (String) args[2],
-                (String) args[3],
-                (String) args[4],
-                (Boolean) args[5],
-                (String) args[6],
-                (String) args[7],
-                (String) args[8],
-                (String) args[9],
-                (String) args[10]
-            )
+            args -> {
+                return new Version(
+                    (String) args[0],
+                    (String) args[1],
+                    (String) args[2],
+                    (String) args[3],
+                    (String) args[4],
+                    (Boolean) args[5],
+                    (String) args[6],
+                    (String) args[7],
+                    (String) args[8]
+                );
+            }
         );
 
         static {
@@ -94,10 +115,39 @@ public record MainResponse(String nodeName, Version version, String clusterName,
             PARSER.declareString(ConstructingObjectParser.constructorArg(), new ParseField("build_date"));
             PARSER.declareBoolean(ConstructingObjectParser.constructorArg(), new ParseField("build_snapshot"));
             PARSER.declareString(ConstructingObjectParser.constructorArg(), new ParseField("lucene_version"));
-            PARSER.declareString(ConstructingObjectParser.optionalConstructorArg(), new ParseField("index_version"));
             PARSER.declareString(ConstructingObjectParser.constructorArg(), new ParseField("minimum_wire_compatibility_version"));
             PARSER.declareString(ConstructingObjectParser.constructorArg(), new ParseField("minimum_index_compatibility_version"));
-            PARSER.declareString(ConstructingObjectParser.optionalConstructorArg(), new ParseField("transport_version"));
+        }
+        private final String number;
+        private final String buildFlavor;
+        private final String buildType;
+        private final String buildHash;
+        private final String buildDate;
+        private final boolean isSnapshot;
+        private final String luceneVersion;
+        private final String minimumWireCompatibilityVersion;
+        private final String minimumIndexCompatibilityVersion;
+
+        public Version(
+            String number,
+            String buildFlavor,
+            String buildType,
+            String buildHash,
+            String buildDate,
+            boolean isSnapshot,
+            String luceneVersion,
+            String minimumWireCompatibilityVersion,
+            String minimumIndexCompatibilityVersion
+        ) {
+            this.number = number;
+            this.buildFlavor = buildFlavor;
+            this.buildType = buildType;
+            this.buildHash = buildHash;
+            this.buildDate = buildDate;
+            this.isSnapshot = isSnapshot;
+            this.luceneVersion = luceneVersion;
+            this.minimumWireCompatibilityVersion = minimumWireCompatibilityVersion;
+            this.minimumIndexCompatibilityVersion = minimumIndexCompatibilityVersion;
         }
 
         public String getNumber() {
@@ -134,6 +184,37 @@ public record MainResponse(String nodeName, Version version, String clusterName,
 
         public String getMinimumIndexCompatibilityVersion() {
             return minimumIndexCompatibilityVersion;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Version version = (Version) o;
+            return isSnapshot == version.isSnapshot
+                && number.equals(version.number)
+                && Objects.equals(buildFlavor, version.buildFlavor)
+                && Objects.equals(buildType, version.buildType)
+                && buildHash.equals(version.buildHash)
+                && buildDate.equals(version.buildDate)
+                && luceneVersion.equals(version.luceneVersion)
+                && minimumWireCompatibilityVersion.equals(version.minimumWireCompatibilityVersion)
+                && minimumIndexCompatibilityVersion.equals(version.minimumIndexCompatibilityVersion);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(
+                number,
+                buildFlavor,
+                buildType,
+                buildHash,
+                buildDate,
+                isSnapshot,
+                luceneVersion,
+                minimumWireCompatibilityVersion,
+                minimumIndexCompatibilityVersion
+            );
         }
     }
 }

920a203193793d1cf1dbe7008d71bfb147928b6d
Rene Groeschke <rene@elastic.co>
2023-07-11 12:12:57 +0200

    fix imports

--- build-tools/src/main/java/org/elasticsearch/gradle/Jdk.java
+++ build-tools/src/main/java/org/elasticsearch/gradle/Jdk.java
@@ -14,6 +14,7 @@ import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.TaskDependency;
 
+import java.io.IOException;
 import java.io.File;
 import java.util.Iterator;
 import java.util.List;

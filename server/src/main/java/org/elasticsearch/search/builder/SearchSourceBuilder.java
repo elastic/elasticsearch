33c94397f464b1083246f2f44daec9066df0f626
Jack Conradson <osjdconrad@gmail.com>
2023-07-11 09:47:55 -0700

    fix bwc bug

--- server/src/main/java/org/elasticsearch/search/builder/SearchSourceBuilder.java
+++ server/src/main/java/org/elasticsearch/search/builder/SearchSourceBuilder.java
@@ -292,7 +292,7 @@ public final class SearchSourceBuilder implements Writeable, ToXContentObject, R
         out.writeOptionalNamedWriteable(postQueryBuilder);
         if (out.getTransportVersion().onOrAfter(TransportVersion.V_8_500_013)) {
             out.writeList(subSearchSourceBuilders);
-        } else if (out.getTransportVersion().before(TransportVersion.V_8_8_0) && subSearchSourceBuilders.size() >= 2) {
+        } else if (out.getTransportVersion().before(TransportVersion.V_8_4_0) && subSearchSourceBuilders.size() >= 2) {
             throw new IllegalArgumentException("cannot serialize [sub_searches] to version [" + out.getTransportVersion() + "]");
         } else {
             out.writeOptionalNamedWriteable(query());

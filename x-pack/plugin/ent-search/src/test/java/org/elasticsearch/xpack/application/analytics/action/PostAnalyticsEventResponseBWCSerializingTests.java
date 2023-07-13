36cd5a8b4dca41a70497364773d58e8c544401b9
saarikabhasi <nairsaarika.bhasi@elastic.co>
2023-07-13 15:08:53 -0400

    POST Analytics Event Response

--- x-pack/plugin/ent-search/src/test/java/org/elasticsearch/xpack/application/analytics/action/PostAnalyticsEventResponseBWCSerializingTests.java
+++ x-pack/plugin/ent-search/src/test/java/org/elasticsearch/xpack/application/analytics/action/PostAnalyticsEventResponseBWCSerializingTests.java
@@ -7,12 +7,14 @@
 
 package org.elasticsearch.xpack.application.analytics.action;
 
+import org.elasticsearch.TransportVersion;
 import org.elasticsearch.common.io.stream.Writeable;
-import org.elasticsearch.test.AbstractWireSerializingTestCase;
+import org.elasticsearch.xcontent.XContentParser;
+import org.elasticsearch.xpack.core.ml.AbstractBWCSerializationTestCase;
 
 import java.io.IOException;
 
-public class PostAnalyticsEventResponseSerializingTests extends AbstractWireSerializingTestCase<PostAnalyticsEventAction.Response> {
+public class PostAnalyticsEventResponseBWCSerializingTests extends AbstractBWCSerializationTestCase<PostAnalyticsEventAction.Response> {
     @Override
     protected Writeable.Reader<PostAnalyticsEventAction.Response> instanceReader() {
         return PostAnalyticsEventAction.Response::readFromStreamInput;
@@ -26,5 +28,16 @@ public class PostAnalyticsEventResponseSerializingTests extends AbstractWireSeri
     @Override
     protected PostAnalyticsEventAction.Response mutateInstance(PostAnalyticsEventAction.Response instance) throws IOException {
         return randomValueOtherThan(instance, this::createTestInstance);
+    }
+
+    @Override
+    protected PostAnalyticsEventAction.Response doParseInstance(XContentParser parser) throws IOException {
+        return PostAnalyticsEventAction.Response.parse(parser);
+    }
+
+    @Override
+    protected PostAnalyticsEventAction.Response mutateInstanceForVersion(
+            PostAnalyticsEventAction.Response instance, TransportVersion version) {
+        return new PostAnalyticsEventAction.Response(instance.isAccepted());
     }
 }

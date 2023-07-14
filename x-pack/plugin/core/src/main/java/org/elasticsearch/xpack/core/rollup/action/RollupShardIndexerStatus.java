d6f8e727e919179ac38e62a57af0140418fe229a
Salvatore Campagna <salvatore.campagna@elastic.co>
2023-07-14 13:55:46 +0200

    fix: serielize enum as a string for bwc

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/rollup/action/RollupShardIndexerStatus.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/rollup/action/RollupShardIndexerStatus.java
@@ -12,46 +12,23 @@ import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 /**
  * Status of the rollup indexer task
  */
 public enum RollupShardIndexerStatus implements Writeable {
-    INITIALIZED(0),
-    STARTED(1),
-    FAILED(2),
-    COMPLETED(3),
-    CANCELLED(4);
-
-    private final byte ordinal;
-
-    RollupShardIndexerStatus(int ordinal) {
-        this.ordinal = (byte) ordinal;
-    }
+    INITIALIZED,
+    STARTED,
+    FAILED,
+    COMPLETED,
+    CANCELLED;
 
     public static RollupShardIndexerStatus readFromStream(final StreamInput in) throws IOException {
-        int ordinal = in.readByte();
-        return switch (ordinal) {
-            case 0 -> RollupShardIndexerStatus.INITIALIZED;
-            case 1 -> RollupShardIndexerStatus.STARTED;
-            case 2 -> RollupShardIndexerStatus.FAILED;
-            case 3 -> RollupShardIndexerStatus.COMPLETED;
-            case 4 -> RollupShardIndexerStatus.CANCELLED;
-            default -> throw new IllegalArgumentException(
-                "Unknown ["
-                    + RollupShardIndexerStatus.class.getSimpleName()
-                    + "] value ["
-                    + ordinal
-                    + "]. Expected values ["
-                    + Arrays.toString(RollupShardIndexerStatus.values())
-                    + "]"
-            );
-        };
+        return RollupShardIndexerStatus.valueOf(in.readString());
     }
 
     @Override
     public void writeTo(final StreamOutput out) throws IOException {
-        out.writeByte(this.ordinal);
+        out.writeEnum(this);
     }
 }

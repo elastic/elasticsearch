d13cde4ed1c75b3e145219a642692c0969f0b952
Brian Seeders <brian.seeders@elastic.co>
2023-07-14 11:33:42 -0400

    Add script to terminate job

---
+++ .buildkite/scripts/windows-end-job.ps1
@@ -0,0 +1,4 @@
+$ProcessID = (Get-Process | Where {$_.ProcessName -Like "buildkite-agent*"} | Select -first 1).id
+$encodedCommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes("Add-Type -Names 'w' -Name 'k' -M '[DllImport(""kernel32.dll"")]public static extern bool FreeConsole();[DllImport(""kernel32.dll"")]public static extern bool AttachConsole(uint p);[DllImport(""kernel32.dll"")]public static extern bool SetConsoleCtrlHandler(uint h, bool a);[DllImport(""kernel32.dll"")]public static extern bool GenerateConsoleCtrlEvent(uint e, uint p);public static void SendCtrlC(uint p){FreeConsole();AttachConsole(p);GenerateConsoleCtrlEvent(0, 0);}';[w.k]::SendCtrlC($ProcessID)"))
+start-process powershell.exe -argument "-nologo -noprofile -executionpolicy bypass -EncodedCommand $encodedCommand"
+start-process powershell.exe -argument "-nologo -noprofile -executionpolicy bypass -EncodedCommand $encodedCommand"

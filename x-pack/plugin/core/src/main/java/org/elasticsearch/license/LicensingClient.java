8bacd20d449e6ba798a27686a63c3c955c8f99a7
Armin Braun <me@obrown.io>
2023-07-16 14:50:29 +0200

    cs

--- x-pack/plugin/core/src/main/java/org/elasticsearch/license/LicensingClient.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/license/LicensingClient.java
@@ -6,12 +6,7 @@
  */
 package org.elasticsearch.license;
 
-import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.internal.ElasticsearchClient;
-import org.elasticsearch.protocol.xpack.license.DeleteLicenseRequest;
-import org.elasticsearch.protocol.xpack.license.GetLicenseRequest;
-import org.elasticsearch.protocol.xpack.license.PutLicenseResponse;
 
 public class LicensingClient {
 

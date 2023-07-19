6a53e6dc3fd610c74a2c613edc1c7b74f6260828
Volodymyr Krasnikov <volodymyr.krasnikov@elastic.co>
2023-07-18 23:04:45 -0700

    Fix test

--- server/src/test/java/org/elasticsearch/cluster/metadata/IndexNameExpressionResolverTests.java
+++ server/src/test/java/org/elasticsearch/cluster/metadata/IndexNameExpressionResolverTests.java
@@ -3124,20 +3124,17 @@ public class IndexNameExpressionResolverTests extends ESTestCase {
                 options,
                 SystemIndexAccessLevel.NONE
             );
-            IllegalArgumentException iae = expectThrows(
-                IllegalArgumentException.class,
+            IndexNotFoundException iae = expectThrows(
+                IndexNotFoundException.class,
                 () -> indexNameExpressionResolver.concreteIndexNames(context, "cluster:index", "local")
             );
-            assertEquals(
-                "Cross-cluster calls are not supported in this context but remote indices were requested: [cluster:index]",
-                iae.getMessage()
-            );
+            assertThat(iae.getMessage(), containsString("no such index [local]"));
             // but datemath with colon doesn't trip cross-cluster check
             IndexNotFoundException e = expectThrows(
                 IndexNotFoundException.class,
                 () -> indexNameExpressionResolver.concreteIndexNames(context, "<datemath-{2001-01-01-13||+1h/h{yyyy-MM-dd-HH|-07:00}}>")
             );
-            assertThat(e.getMessage(), containsString("no such index [datemath-2001-01-01-14"));
+            assertThat(e.getMessage(), containsString("no such index [datemath-2001-01-01-14]"));
         }
         {
             IndicesOptions options = IndicesOptions.fromOptions(true, true, randomBoolean(), randomBoolean(), randomBoolean());

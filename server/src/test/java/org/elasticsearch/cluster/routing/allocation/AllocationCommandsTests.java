251bdadb79e221c20fef212474f7939e3f813bf4
Simon Cooper <simon.cooper@elastic.co>
2023-07-20 17:13:48 +0100

    Fix compile

--- server/src/test/java/org/elasticsearch/cluster/routing/allocation/AllocationCommandsTests.java
+++ server/src/test/java/org/elasticsearch/cluster/routing/allocation/AllocationCommandsTests.java
@@ -10,6 +10,7 @@ package org.elasticsearch.cluster.routing.allocation;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.cluster.ClusterInfo;
 import org.elasticsearch.cluster.ClusterName;

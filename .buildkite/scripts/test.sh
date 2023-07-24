4c451abb3ae111601c6c0d76496575bb7ecaccf9
Brian Seeders <brian.seeders@elastic.co>
2023-07-24 17:00:52 -0400

    Trap signals

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-bash -c 'sleep 2; echo 2; exit 0' &
-bash -c 'sleep 3; echo 3; exit 0' &
-bash -c 'sleep 4; echo 4; exit 0' &
-bash -c 'sleep 5; echo 5; exit 0' &
-bash -c 'sleep 6; echo 6; exit 0' &
-bash -c 'sleep 7; echo 7; exit 0' &
-bash -c 'sleep 8; echo 8; exit 0' &
-bash -c 'sleep 9; echo 9; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 2; echo 2; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 3; echo 3; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 4; echo 4; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 5; echo 5; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 6; echo 6; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 7; echo 7; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 8; echo 8; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 9; echo 9; exit 0' &
 
 exit 1

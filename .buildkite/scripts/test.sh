c7763e7baf38d4ebe03cae47a8f1c06662bcecd5
Brian Seeders <brian.seeders@elastic.co>
2023-07-24 16:55:22 -0400

    exit 1

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -2,11 +2,13 @@
 
 set -euo pipefail
 
-bash -c 'sleep 2; echo 2;' &
-bash -c 'sleep 3; echo 3;' &
-bash -c 'sleep 4; echo 4;' &
-bash -c 'sleep 5; echo 5;' &
-bash -c 'sleep 6; echo 6;' &
-bash -c 'sleep 7; echo 7;' &
-bash -c 'sleep 8; echo 8;' &
-bash -c 'sleep 9; echo 9;' &
+bash -c 'sleep 2; echo 2; exit 0' &
+bash -c 'sleep 3; echo 3; exit 0' &
+bash -c 'sleep 4; echo 4; exit 0' &
+bash -c 'sleep 5; echo 5; exit 0' &
+bash -c 'sleep 6; echo 6; exit 0' &
+bash -c 'sleep 7; echo 7; exit 0' &
+bash -c 'sleep 8; echo 8; exit 0' &
+bash -c 'sleep 9; echo 9; exit 0' &
+
+exit 1

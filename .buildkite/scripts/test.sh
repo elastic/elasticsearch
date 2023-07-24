9ed4afdd44bf5f8113739aebd5254a55e246840f
Brian Seeders <brian.seeders@elastic.co>
2023-07-24 17:18:15 -0400

    Add some cats

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 2; echo 2; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 3; echo 3; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 4; echo 4; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 5; echo 5; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 6; echo 6; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 7; echo 7; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 8; echo 8; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 9; echo 9; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 2; echo 2; cat; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 3; echo 3; cat; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 4; echo 4; cat; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 5; echo 5; cat; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 6; echo 6; cat; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 7; echo 7; cat; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 8; echo 8; cat; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 9; echo 9; cat; exit 0' &
 
 exit 1

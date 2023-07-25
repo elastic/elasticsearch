6bb24d7467002b7c035a394723509e98aed552a6
Brian Seeders <brian.seeders@elastic.co>
2023-07-25 11:41:26 -0400

    Simple test script

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -2,13 +2,6 @@
 
 set -euo pipefail
 
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 2; echo 2; cat; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 3; echo 3; cat; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 4; echo 4; cat; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 5; echo 5; cat; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 6; echo 6; cat; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 7; echo 7; cat; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 8; echo 8; cat; exit 0' &
-bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 9; echo 9; cat; exit 0' &
+bash -c 'trap "echo trap" 0 1 2 3 13 15; sleep 600; echo 2;' &
 
 exit 1

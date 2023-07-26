b9f1608ba00ade599fc14bb784f228313b006766
danielhuang(黄华) <danielhuang@tencent.com>
2023-07-26 10:05:03 +0800

    Set response as null after failure.

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/async/AsyncSearchIndexServiceTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/async/AsyncSearchIndexServiceTests.java
@@ -110,7 +110,7 @@ public class AsyncSearchIndexServiceTests extends ESSingleNodeTestCase {
 
         @Override
         public TestAsyncResponse convertToFailure(Exception exc) {
-            return new TestAsyncResponse(test, expirationTimeMillis, exc.getMessage());
+            return new TestAsyncResponse("", expirationTimeMillis, exc.getMessage());
         }
     }
 

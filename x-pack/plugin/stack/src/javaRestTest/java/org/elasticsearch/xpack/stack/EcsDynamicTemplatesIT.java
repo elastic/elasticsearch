2290ec8f8e886f6310fcf55e53399644776e705f
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2023-07-27 09:16:18 +0300

    Enforcing formatting

--- x-pack/plugin/stack/src/javaRestTest/java/org/elasticsearch/xpack/stack/EcsDynamicTemplatesIT.java
+++ x-pack/plugin/stack/src/javaRestTest/java/org/elasticsearch/xpack/stack/EcsDynamicTemplatesIT.java
@@ -43,7 +43,6 @@ import static org.hamcrest.Matchers.instanceOf;
 
 @SuppressWarnings("unchecked")
 public class EcsDynamicTemplatesIT extends ESRestTestCase {
-
     @ClassRule
     public static ElasticsearchCluster cluster = ElasticsearchCluster.local().module("mapper-extras").module("wildcard").build();
 

f02f192e6297f85551fd879a3b4dfd532c605ff2
Simon Cooper <simon.cooper@elastic.co>
2023-07-27 12:30:54 +0100

    Refactor non-testcase uses of setting index created version with a release version

--- test/framework/src/main/java/org/elasticsearch/test/IndexSettingsModule.java
+++ test/framework/src/main/java/org/elasticsearch/test/IndexSettingsModule.java
@@ -7,7 +7,6 @@
  */
 package org.elasticsearch.test;
 
-import org.elasticsearch.Version;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.common.inject.AbstractModule;
 import org.elasticsearch.common.settings.IndexScopedSettings;
@@ -15,6 +14,7 @@ import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.Index;
 import org.elasticsearch.index.IndexSettings;
+import org.elasticsearch.index.IndexVersion;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -50,7 +50,7 @@ public class IndexSettingsModule extends AbstractModule {
 
     public static IndexSettings newIndexSettings(Index index, Settings indexSetting, Settings nodeSettings, Setting<?>... setting) {
         Settings build = Settings.builder()
-            .put(IndexMetadata.SETTING_VERSION_CREATED, Version.CURRENT)
+            .put(IndexMetadata.SETTING_VERSION_CREATED, IndexVersion.current())
             .put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 1)
             .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, 1)
             .put(indexSetting)
@@ -68,7 +68,7 @@ public class IndexSettingsModule extends AbstractModule {
 
     public static IndexSettings newIndexSettings(Index index, Settings settings, IndexScopedSettings indexScopedSettings) {
         Settings build = Settings.builder()
-            .put(IndexMetadata.SETTING_VERSION_CREATED, Version.CURRENT)
+            .put(IndexMetadata.SETTING_VERSION_CREATED, IndexVersion.current())
             .put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 1)
             .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, 1)
             .put(settings)

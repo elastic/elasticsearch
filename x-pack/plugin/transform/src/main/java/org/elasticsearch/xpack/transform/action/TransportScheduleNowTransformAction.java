d88c9a1db9e776520adf9d3507296d68a5845cee
xingrufei <qhdxssm@qq.com>
2023-07-31 21:58:15 +0800

    Fix the issue that PersistentTask.getState() cause NullPointerException

--- x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/action/TransportScheduleNowTransformAction.java
+++ x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/action/TransportScheduleNowTransformAction.java
@@ -91,6 +91,7 @@ public class TransportScheduleNowTransformAction extends TransportTasksAction<Tr
             // - transform is not failed (stopped transforms do not have a task)
             if (transformTask != null
                 && transformTask.isAssigned()
+                && transformTask.getState() != null
                 && transformTask.getState() instanceof TransformState
                 && ((TransformState) transformTask.getState()).getTaskState() != TransformTaskState.FAILED) {
 

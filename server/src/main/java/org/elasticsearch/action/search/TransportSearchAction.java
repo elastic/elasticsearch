3bee598b3802ff5c1b1d73aeef63aba728bbd489
David Turner <david.turner@elastic.co>
2023-08-02 08:46:19 +0100

    Fork CCS remote-cluster responses
    Today during CCS we process responses from remote clusters on the
    transport worker thread, but that's not the right place for this work.
    This commit moves it to the `SEARCH_COORDINATION` pool.
    
    Closes #97997

--- server/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
+++ server/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
@@ -467,8 +467,7 @@ public class TransportSearchAction extends HandledTransportAction<SearchRequest,
         ActionListener<SearchResponse> listener,
         BiConsumer<SearchRequest, ActionListener<SearchResponse>> localSearchConsumer
     ) {
-        // TODO pick a more appropriate executor for this work - see https://github.com/elastic/elasticsearch/issues/97997
-        final var remoteClientResponseExecutor = EsExecutors.DIRECT_EXECUTOR_SERVICE;
+        final var remoteClientResponseExecutor = threadPool.executor(ThreadPool.Names.SEARCH_COORDINATION);
         if (localIndices == null && remoteIndices.size() == 1) {
             // if we are searching against a single remote cluster, we simply forward the original search request to such cluster
             // and we directly perform final reduction in the remote cluster

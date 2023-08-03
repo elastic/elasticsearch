2e614e52f461573ed795b978d45f9897b647da55
Iraklis Psaroudakis <kingherc@gmail.com>
2023-08-03 16:02:19 +0300

    Refresh throttling for all non-fastrefresh indices (#638)
    With a separate node budget for system vs regular indices.
    
    Fixes ES-6155

--- x-pack/plugin/stateless/src/internalClusterTest/java/co/elastic/elasticsearch/stateless/engine/StatelessRefreshThrottlingIT.java
+++ x-pack/plugin/stateless/src/internalClusterTest/java/co/elastic/elasticsearch/stateless/engine/StatelessRefreshThrottlingIT.java
@@ -20,6 +20,7 @@ package co.elastic.elasticsearch.stateless.engine;
 import co.elastic.elasticsearch.stateless.AbstractStatelessIntegTestCase;
 
 import org.elasticsearch.index.Index;
+import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.indices.IndicesService;
 
 import java.util.ArrayList;
@@ -34,6 +35,11 @@ public class StatelessRefreshThrottlingIT extends AbstractStatelessIntegTestCase
     private RefreshNodeCreditManager getRegularIndicesCreditManager(String indexingNode) {
         var refreshThrottlingService = internalCluster().getInstance(RefreshThrottlingService.class, indexingNode);
         return refreshThrottlingService.getRegularIndicesCreditManager();
+    }
+
+    private RefreshNodeCreditManager getSystemIndicesCreditManager(String indexingNode) {
+        var refreshThrottlingService = internalCluster().getInstance(RefreshThrottlingService.class, indexingNode);
+        return refreshThrottlingService.getSystemIndicesCreditManager();
     }
 
     private RefreshThrottler getRefreshThrottler(String indexingNode, String indexName, int shard) {
@@ -64,8 +70,13 @@ public class StatelessRefreshThrottlingIT extends AbstractStatelessIntegTestCase
 
     public void testShardRefreshThrottling() throws Exception {
         String indexNode = startMasterAndIndexNode();
-        final String indexName = randomAlphaOfLength(10).toLowerCase(Locale.ROOT);
-        createIndex(indexName, indexSettings(1, 0).build());
+        boolean useSystemIndex = randomBoolean();
+        final String indexName = useSystemIndex ? SYSTEM_INDEX_NAME : randomAlphaOfLength(10).toLowerCase(Locale.ROOT);
+        if (useSystemIndex) {
+            createSystemIndex(indexSettings(1, 0).build());
+        } else {
+            createIndex(indexName, indexSettings(1, 0).build());
+        }
         ensureGreen(indexName);
 
         RefreshBurstableThrottler refreshThrottler = getRefreshBurstableThrottler(indexNode, indexName, 0);
@@ -89,7 +100,7 @@ public class StatelessRefreshThrottlingIT extends AbstractStatelessIntegTestCase
         });
     }
 
-    public void testNodeRefreshThrottling() throws Exception {
+    public void testNodeRegularIndicesRefreshThrottling() throws Exception {
         String indexNode = startMasterAndIndexNode();
 
         int indices = randomIntBetween(1, 3);
@@ -105,10 +116,26 @@ public class StatelessRefreshThrottlingIT extends AbstractStatelessIntegTestCase
             refresh(indicesNames.get(i));
         }
 
-        RefreshNodeCreditManager nodeCreditManager = getRegularIndicesCreditManager(indexNode);
-        // Trigger throttler's credit to number of indices - 1
-        nodeCreditManager.setCredit(indices - 1);
+        // System index refreshes should be unaffected in this test
+        createSystemIndex(indexSettings(1, 0).build());
+        indexDocs(SYSTEM_INDEX_NAME, randomIntBetween(1, 5));
+        refresh(SYSTEM_INDEX_NAME);
+        RefreshBurstableThrottler systemIndexThrottler = getRefreshBurstableThrottler(indexNode, SYSTEM_INDEX_NAME, 0);
 
+        RefreshNodeCreditManager nodeRegularIndicesCreditManager = getRegularIndicesCreditManager(indexNode);
+        RefreshNodeCreditManager nodeSystemIndicesCreditManager = getSystemIndicesCreditManager(indexNode);
+        // Trigger throttler's credit to number of indices - 1
+        nodeRegularIndicesCreditManager.setCredit(indices - 1);
+
+        // System index refresh should go on unhindered and should not affect later regular index refreshes
+        indexDocs(SYSTEM_INDEX_NAME, randomIntBetween(1, 5));
+        refresh(SYSTEM_INDEX_NAME);
+        assertBusy(() -> {
+            assertThat(getAcceptedRefreshes(systemIndexThrottler), equalTo(2L));
+            assertThat(getThrottledRefreshes(systemIndexThrottler), equalTo(0L));
+        });
+
+        // Last regular index's refresh should be throttled
         for (int i = 0; i < indices; i++) {
             indexDocs(indicesNames.get(i), randomIntBetween(1, 5));
             refresh(indicesNames.get(i));
@@ -125,9 +152,62 @@ public class StatelessRefreshThrottlingIT extends AbstractStatelessIntegTestCase
         }
     }
 
-    public void testSystemIndexIsNotThrottled() throws Exception {
+    public void testNodeSystemIndicesRefreshThrottling() throws Exception {
         String indexNode = startMasterAndIndexNode();
-        createSystemIndex(indexSettings(1, 0).build());
+
+        int indices = randomIntBetween(1, 3);
+        List<String> indicesNames = new ArrayList<>(indices);
+        List<RefreshBurstableThrottler> shardThrottlers = new ArrayList<>(indices);
+        for (int i = 0; i < indices; i++) {
+            final String indexName = SYSTEM_INDEX_NAME + i;
+            createIndex(indexName, indexSettings(1, 0).build());
+            ensureGreen(indexName);
+            indicesNames.add(indexName);
+            shardThrottlers.add(getRefreshBurstableThrottler(indexNode, indexName, 0));
+            indexDocs(indicesNames.get(i), randomIntBetween(1, 5));
+            refresh(indicesNames.get(i));
+        }
+
+        // Regular index refreshes should be unaffected in this test
+        String regularIndexName = randomAlphaOfLength(10).toLowerCase(Locale.ROOT);
+        createIndex(regularIndexName, indexSettings(1, 0).build());
+        indexDocs(regularIndexName, randomIntBetween(1, 5));
+        refresh(regularIndexName);
+        RefreshBurstableThrottler regularIndexThrottler = getRefreshBurstableThrottler(indexNode, regularIndexName, 0);
+
+        RefreshNodeCreditManager nodeRegularIndicesCreditManager = getRegularIndicesCreditManager(indexNode);
+        RefreshNodeCreditManager nodeSystemIndicesCreditManager = getSystemIndicesCreditManager(indexNode);
+        // Trigger throttler's credit to number of indices - 1
+        nodeSystemIndicesCreditManager.setCredit(indices - 1);
+
+        // Regular index refresh should go on unhindered and should not affect later system index refreshes
+        indexDocs(regularIndexName, randomIntBetween(1, 5));
+        refresh(regularIndexName);
+        assertBusy(() -> {
+            assertThat(getAcceptedRefreshes(regularIndexThrottler), equalTo(2L));
+            assertThat(getThrottledRefreshes(regularIndexThrottler), equalTo(0L));
+        });
+
+        // Last system index's refresh should be throttled
+        for (int i = 0; i < indices; i++) {
+            indexDocs(indicesNames.get(i), randomIntBetween(1, 5));
+            refresh(indicesNames.get(i));
+            final int final_i = i;
+            assertBusy(() -> {
+                if (final_i < indices - 1) {
+                    assertThat(getAcceptedRefreshes(shardThrottlers.get(final_i)), equalTo(2L));
+                    assertThat(getThrottledRefreshes(shardThrottlers.get(final_i)), equalTo(0L));
+                } else {
+                    assertThat(getAcceptedRefreshes(shardThrottlers.get(final_i)), equalTo(1L));
+                    assertThat(getThrottledRefreshes(shardThrottlers.get(final_i)), equalTo(1L));
+                }
+            });
+        }
+    }
+
+    public void testSystemIndexWithFastRefreshIsNotThrottled() throws Exception {
+        String indexNode = startMasterAndIndexNode();
+        createSystemIndex(indexSettings(1, 0).put(IndexSettings.INDEX_FAST_REFRESH_SETTING.getKey(), true).build());
         indexDoc(SYSTEM_INDEX_NAME, "1", "field", "value");
         RefreshThrottler shardThrottler = getRefreshThrottler(indexNode, SYSTEM_INDEX_NAME, 0);
         assertThat(shardThrottler, instanceOf(RefreshThrottler.Noop.class));

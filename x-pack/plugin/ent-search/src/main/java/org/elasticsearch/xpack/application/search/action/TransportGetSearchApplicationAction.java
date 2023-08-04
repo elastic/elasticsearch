2471ec27ff5900185b41a192b74ebf5c48a1bbdc
Kathleen DeRusso <kathleen.derusso@elastic.co>
2023-08-04 10:52:52 -0400

    Remove if conditional per PR feedback

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/search/action/TransportGetSearchApplicationAction.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/search/action/TransportGetSearchApplicationAction.java
@@ -55,15 +55,15 @@ public class TransportGetSearchApplicationAction extends HandledTransportAction<
                     }
                     if (searchApplication.hasStoredTemplate() == false) {
                         HeaderWarning.addWarning(SearchApplication.NO_TEMPLATE_STORED_WARNING);
-                        return new GetSearchApplicationAction.Response(
-                            searchApplication.name(),
-                            searchApplication.indices(),
-                            searchApplication.analyticsCollectionName(),
-                            searchApplication.updatedAtMillis(),
-                            searchApplication.searchApplicationTemplateOrDefault()
-                        );
                     }
-                    return new GetSearchApplicationAction.Response(searchApplication);
+                    // Construct a new object to ensure we backfill the stored application with the default template
+                    return new GetSearchApplicationAction.Response(
+                        searchApplication.name(),
+                        searchApplication.indices(),
+                        searchApplication.analyticsCollectionName(),
+                        searchApplication.updatedAtMillis(),
+                        searchApplication.searchApplicationTemplateOrDefault()
+                    );
                 }))
             )
         );

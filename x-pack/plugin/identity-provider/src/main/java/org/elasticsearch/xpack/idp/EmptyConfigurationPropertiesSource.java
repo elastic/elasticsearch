41527f4c1554ec81b7fcd02499ee4785613e2b00
Albert Zaharovits <albert.zaharovits@elastic.co>
2023-08-04 18:44:35 +0300

    idp empty configuration properties

---
+++ x-pack/plugin/identity-provider/src/main/java/org/elasticsearch/xpack/idp/EmptyConfigurationPropertiesSource.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.idp;
+
+import org.opensaml.core.config.ConfigurationPropertiesSource;
+
+import java.util.Properties;
+
+public class EmptyConfigurationPropertiesSource implements ConfigurationPropertiesSource {
+    private final Properties empty = new Properties();
+
+    @Override
+    public Properties getProperties() {
+        return empty;
+    }
+}

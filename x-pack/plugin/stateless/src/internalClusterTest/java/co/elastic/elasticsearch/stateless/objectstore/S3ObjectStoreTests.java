1856a19f4aed46809949842dfb82d38bd31cc29f
Yang Wang <yang.wang@elastic.co>
2023-08-07 16:13:18 +1000

    [Test] Unmute for S3ObjectStoreTests.testBlobStoreStats (#658)
    This reverts commit c1eca73 because the fix was merged in #643
    
    Resolves: #656

--- x-pack/plugin/stateless/src/internalClusterTest/java/co/elastic/elasticsearch/stateless/objectstore/S3ObjectStoreTests.java
+++ x-pack/plugin/stateless/src/internalClusterTest/java/co/elastic/elasticsearch/stateless/objectstore/S3ObjectStoreTests.java
@@ -28,7 +28,6 @@ import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.repositories.RepositoryStats;
 import org.elasticsearch.repositories.s3.S3RepositoryPlugin;
 
-import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -61,12 +60,6 @@ public class S3ObjectStoreTests extends AbstractMockObjectStoreIntegTestCase {
             .put(ObjectStoreService.BUCKET_SETTING.getKey(), "bucket")
             .put(ObjectStoreService.CLIENT_SETTING.getKey(), "test")
             .setSecureSettings(mockSecureSettings);
-    }
-
-    @Override
-    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch-serverless/issues/656")
-    public void testBlobStoreStats() throws IOException {
-        super.testBlobStoreStats();
     }
 
     @Override

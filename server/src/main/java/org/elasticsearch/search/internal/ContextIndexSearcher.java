0b2b2433c7a47f09f9ccef75d1fd72674b32f216
Luca Cavanna <javanna@apache.org>
2023-08-08 07:23:33 +0200

    spotless

--- server/src/main/java/org/elasticsearch/search/internal/ContextIndexSearcher.java
+++ server/src/main/java/org/elasticsearch/search/internal/ContextIndexSearcher.java
@@ -345,7 +345,7 @@ public class ContextIndexSearcher extends IndexSearcher implements Releasable {
         if (getExecutor() == null) {
             search(leafContexts, weight, firstCollector);
             return collectorManager.reduce(Collections.singletonList(firstCollector));
-        } else if (leafSlices.length == 0){
+        } else if (leafSlices.length == 0) {
             assert leafContexts.isEmpty();
             return collectorManager.reduce(Collections.singletonList(firstCollector));
         } else {

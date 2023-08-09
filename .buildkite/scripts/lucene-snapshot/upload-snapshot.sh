a1cb802ab02b0d2a86cf1cda53f79d6eab3390ce
Brian Seeders <brian.seeders@elastic.co>
2023-08-09 16:18:09 -0400

    WIP moving lucene snapshot build to separate pipeline

---
+++ .buildkite/scripts/lucene-snapshot/upload-snapshot.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -euo pipefail
+
+LUCENE_BUILD_ID=${LUCENE_BUILD_ID:-}
+
+if [[ -z "$LUCENE_BUILD_ID" ]]; then
+  LUCENE_BUILD_ID=$(buildkite-agent step get --step lucene-build triggered_build | jq -r .id)
+fi
+
+export LUCENE_BUILD_ID
+
+LUCENE_SHA=$(buildkite-agent meta-data get --build "$LUCENE_BUILD_ID" lucene-snapshot-sha)
+export LUCENE_SHA
+
+LUCENE_SNAPSHOT_VERSION=$(buildkite-agent meta-data get --build "$LUCENE_BUILD_ID" lucene-snapshot-version)
+export LUCENE_SNAPSHOT_VERSION
+
+echo --- Downloading lucene snapshot
+
+mkdir lucene-snapshot
+cd lucene-snapshot
+buildkite-agent artifact download --build "$LUCENE_BUILD_ID" lucene-snapshot.tar.gz .
+tar -xvf lucene-snapshot.tar.gz
+cd -
+
+echo --- Upload lucene snapshot to S3
+
+if ! which aws; then
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+  rm -rf awscliv2.zip aws
+fi
+
+aws s3 sync lucene-snapshot/ "s3://download.elasticsearch.org/lucenesnapshots/$LUCENE_SHA/" --acl public-read
+
+cd "$WORKSPACE"
+
+if [[ "${UPDATE_ES_LUCENE_SNAPSHOT:-}" ]]; then
+  .buildkite/scripts/lucene-snapshot/update-es-snapshot.sh
+fi

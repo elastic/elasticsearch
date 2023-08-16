7ba3c9bc3fb4e54e0b022512f22817dcb954315e
Alexander Spies <alexander.spies@elastic.co>
2023-08-16 14:50:12 +0200

    Exception not error on unsupported neg mapping
    Mapping a Neg to an evaluator for unsupported types like unsigned long
    should not result in  an AssertionError. Use
    UnsupportedOperationException instead.

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/planner/NegMapper.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/planner/NegMapper.java
@@ -57,6 +57,6 @@ abstract class NegMapper extends EvalMapper.ExpressionMapper<Neg> {
                 return () -> doubles.apply(childEvaluator);
             }
         }
-        throw new AssertionError("arithmetic negation operator with unsupported data type [" + type.typeName() + "]");
+        throw new UnsupportedOperationException("arithmetic negation operator with unsupported data type [" + type.typeName() + "]");
     }
 }

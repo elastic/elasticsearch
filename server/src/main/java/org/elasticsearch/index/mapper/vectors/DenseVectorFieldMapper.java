6d4b1432cc14cfeba786b6e6f19d363b99771448
Kathleen DeRusso <kathleen.derusso@elastic.co>
2023-08-18 11:00:39 -0400

    Go back to non testing value

--- server/src/main/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapper.java
@@ -79,7 +79,7 @@ public class DenseVectorFieldMapper extends FieldMapper {
     public static final String CONTENT_TYPE = "dense_vector";
     public static short MAX_DIMS_COUNT = 2048; // maximum allowed number of dimensions
 
-    public static short MIN_DIMS_FOR_DYNAMIC_FLOAT_MAPPING = 3; // minimum number of dims for floats to be dynamically mapped to vector
+    public static short MIN_DIMS_FOR_DYNAMIC_FLOAT_MAPPING = 128; // minimum number of dims for floats to be dynamically mapped to vector
     public static final int MAGNITUDE_BYTES = 4;
 
     private static DenseVectorFieldMapper toType(FieldMapper in) {

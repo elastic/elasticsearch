a8c4559492ef80cd00f9f84512d2704f2f7c3da6
David Turner <david.turner@elastic.co>
2023-08-22 18:35:20 +0100

    Revert "Accept empty chunked responses (#98733)"
    This reverts commit ae52a06a51064fff8c1b202e5624668197993afd.

--- modules/transport-netty4/src/main/java/org/elasticsearch/http/netty4/Netty4HttpRequest.java
+++ modules/transport-netty4/src/main/java/org/elasticsearch/http/netty4/Netty4HttpRequest.java
@@ -19,7 +19,6 @@ import io.netty.handler.codec.http.cookie.Cookie;
 import io.netty.handler.codec.http.cookie.ServerCookieDecoder;
 import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
 
-import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.http.HttpRequest;
 import org.elasticsearch.http.HttpResponse;
@@ -178,11 +177,7 @@ public class Netty4HttpRequest implements HttpRequest {
 
     @Override
     public HttpResponse createResponse(RestStatus status, ChunkedRestResponseBody content) {
-        if (content.isDone()) {
-            return new Netty4HttpResponse(sequence, request.protocolVersion(), status, BytesArray.EMPTY);
-        } else {
-            return new Netty4ChunkedHttpResponse(sequence, request.protocolVersion(), status, content);
-        }
+        return new Netty4ChunkedHttpResponse(sequence, request.protocolVersion(), status, content);
     }
 
     @Override

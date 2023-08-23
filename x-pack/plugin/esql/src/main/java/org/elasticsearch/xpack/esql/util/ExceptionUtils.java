8b4cea3e3e3e321983853c13fc82a0774364f416
Alexander Spies <alexander.spies@elastic.co>
2023-08-23 12:37:35 +0200

    Add utils class for exception helper methods

---
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/util/ExceptionUtils.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.esql.util;
+
+import org.elasticsearch.xpack.esql.EsqlIllegalArgumentException;
+import org.elasticsearch.xpack.esql.EsqlUnsupportedOperationException;
+import org.elasticsearch.xpack.ql.type.DataType;
+
+/**
+ * Contains commonly used helper methods for dealing with exceptions.
+ */
+public class ExceptionUtils {
+    /**
+     * @see ExceptionUtils#illegalDataType(String)
+     *
+     * @param dataType the illegal data type that was encountered
+     */
+    public static EsqlIllegalArgumentException illegalDataType(DataType dataType) {
+        return EsqlIllegalArgumentException.illegalDataType(dataType.typeName());
+    }
+
+    /**
+     * Create a {@link EsqlIllegalArgumentException} whose cause is an illegal data type.
+     *
+     * @param dataTypeName the name of the  illegal data type that was encountered
+     */
+    public static EsqlIllegalArgumentException illegalDataType(String dataTypeName) {
+        return new EsqlIllegalArgumentException("illegal data type [" + dataTypeName + "]");
+    }
+
+    /**
+     * Create a {@link EsqlUnsupportedOperationException} whose cause is a missing method implementation.
+     */
+    public static EsqlUnsupportedOperationException methodNotImplemented() {
+        return new EsqlUnsupportedOperationException("method not implemented");
+    }
+
+    /**
+     * @see ExceptionUtils#unsupportedDataType(String)
+     *
+     * @param dataType the unsupported data type that was encountered
+     */
+    public static EsqlUnsupportedOperationException unsupportedDataType(DataType dataType) {
+        return EsqlUnsupportedOperationException.unsupportedDataType(dataType.typeName());
+    }
+
+    /**
+     * Create a {@link EsqlUnsupportedOperationException} whose cause is an unsupported data type.
+     * In contrast to an {@link EsqlIllegalArgumentException}, this is appropriate when an unsupported data type was not encountered in the
+     * argument of a method, but e.g. a class' method is only implemented for a subset of data types used in the class' attributes (like an
+     * expression evaluator that requires the child expressions to resolve to certain data types).
+     *
+     * @param dataTypeName the unsupported data type that was encountered
+     */
+    public static EsqlUnsupportedOperationException unsupportedDataType(String dataTypeName) {
+        return new EsqlUnsupportedOperationException("unsupported data type [" + dataTypeName + "]");
+    }
+}

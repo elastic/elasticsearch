318bee65d29a615180f6646f2e4ee41d981a1d15
Henning Andersen <33268011+henningandersen@users.noreply.github.com>
2023-08-24 16:25:19 +0200

    Search index input clone copy cache file (#775)
    Search index input clone now copies the cache file object ensuring that the
    fast path optimizations work. There is also unconfirmed speculation that
    this could fix a corruption during read.

--- x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/lucene/SearchIndexInputTests.java
+++ x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/lucene/SearchIndexInputTests.java
@@ -37,21 +37,15 @@ import java.util.concurrent.TimeUnit;
 import static org.elasticsearch.xpack.searchablesnapshots.AbstractSearchableSnapshotsTestCase.randomChecksumBytes;
 import static org.elasticsearch.xpack.searchablesnapshots.AbstractSearchableSnapshotsTestCase.randomIOContext;
 import static org.elasticsearch.xpack.searchablesnapshots.cache.common.TestUtils.pageAligned;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 
 public class SearchIndexInputTests extends ESIndexInputTestCase {
 
     public void testRandomReads() throws IOException {
         final ThreadPool threadPool = new TestThreadPool("testRandomReads");
         final ByteSizeValue cacheSize = ByteSizeValue.ofBytes(randomLongBetween(0, 10_000_000));
-        final var settings = Settings.builder()
-            .put(SharedBlobCacheService.SHARED_CACHE_SIZE_SETTING.getKey(), cacheSize)
-            .put(
-                SharedBlobCacheService.SHARED_CACHE_REGION_SIZE_SETTING.getKey(),
-                pageAligned(new ByteSizeValue(randomIntBetween(4, 1024), ByteSizeUnit.KB))
-            )
-            .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir())
-            .putList(Environment.PATH_DATA_SETTING.getKey(), createTempDir().toAbsolutePath().toString())
-            .build();
+        final var settings = sharedCacheSettings(cacheSize);
         try (
             NodeEnvironment nodeEnvironment = new NodeEnvironment(settings, TestEnvironment.newEnvironment(settings));
             SharedBlobCacheService<FileCacheKey> sharedBlobCacheService = new SharedBlobCacheService<>(
@@ -82,5 +76,55 @@ public class SearchIndexInputTests extends ESIndexInputTestCase {
         } finally {
             assertTrue(ThreadPool.terminate(threadPool, 10L, TimeUnit.SECONDS));
         }
+    }
+
+    /**
+     * Test that clone copies the underlying cachefile object. Reads on cloned instances are checked by #testRandomReads
+     * @throws IOException
+     */
+    public void testClone() throws IOException {
+        final ThreadPool threadPool = new TestThreadPool("testRandomReads");
+        final var settings = sharedCacheSettings(ByteSizeValue.ofBytes(randomLongBetween(0, 10_000_000)));
+        try (
+            NodeEnvironment nodeEnvironment = new NodeEnvironment(settings, TestEnvironment.newEnvironment(settings));
+            SharedBlobCacheService<FileCacheKey> sharedBlobCacheService = new SharedBlobCacheService<>(
+                nodeEnvironment,
+                settings,
+                threadPool,
+                ThreadPool.Names.GENERIC
+            )
+        ) {
+            final ShardId shardId = new ShardId(new Index("_index_name", "_index_id"), 0);
+            final byte[] input = randomByteArrayOfLength(randomIntBetween(1, 100_000));
+            final String fileName = randomAlphaOfLength(5) + randomFileExtension();
+            final SearchIndexInput indexInput = new SearchIndexInput(
+                fileName,
+                sharedBlobCacheService.getCacheFile(new FileCacheKey(shardId, fileName), input.length),
+                randomIOContext(),
+                TestUtils.singleBlobContainer(fileName, input),
+                sharedBlobCacheService,
+                input.length,
+                0
+            );
+
+            indexInput.seek(randomLongBetween(0, input.length - 1));
+            SearchIndexInput clone = indexInput.clone();
+            assertThat(clone.cacheFile(), not(equalTo(indexInput.cacheFile())));
+            assertThat(clone.getFilePointer(), equalTo(indexInput.getFilePointer()));
+        } finally {
+            assertTrue(ThreadPool.terminate(threadPool, 10L, TimeUnit.SECONDS));
+        }
+    }
+
+    private static Settings sharedCacheSettings(ByteSizeValue cacheSize) {
+        return Settings.builder()
+            .put(SharedBlobCacheService.SHARED_CACHE_SIZE_SETTING.getKey(), cacheSize)
+            .put(
+                SharedBlobCacheService.SHARED_CACHE_REGION_SIZE_SETTING.getKey(),
+                pageAligned(new ByteSizeValue(randomIntBetween(4, 1024), ByteSizeUnit.KB))
+            )
+            .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir())
+            .putList(Environment.PATH_DATA_SETTING.getKey(), createTempDir().toAbsolutePath().toString())
+            .build();
     }
 }

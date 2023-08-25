e78136d066c08afbfe5dcae8569e67d58ed3e1ee
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2023-08-25 09:32:32 +0200

    [Profiling] Abort index creation on outdated index
    Profiling-related indices that have been created in versions before
    8.9.1 are incompatible with later versions. Therefore, the Universal
    Profiling upgrade documentation states that indices must be deleted on
    upgrade. However, if the user forgets to do this, the cluster is in an
    inconsistent state as some indices can be created in the new format but
    others cannot because they conflict with existing indices in the old
    format. This leads to a situation where the upgrade appears to have
    worked but ultimately writes will fail for non-obvious reasons.
    
    With this commit we introduce the following changes:
    
    1. We detect that an outdated index is present and then stop creating
       new indices.
    2. We tighten the status check for the `resources.created` flag and not
       only include index templates and ILM policies but also indices and
    data streams. Consequently, this flag remains `false` if not all indices
    / data streams have been created, thus allowing the UI to detect this
    state.
    3. We introduce a new flag `resources.too_old` in the status API that is
       true iff Elasticsearch has detected that there is at least one
    outdated index or data stream present. This allows the UI to flag that
    specific condition and instruct the user to delete their
    profiling-related indices and data streams as required by the upgrade
    instructions.

--- x-pack/plugin/profiling/src/main/java/org/elasticsearch/xpack/profiling/GetStatusAction.java
+++ x-pack/plugin/profiling/src/main/java/org/elasticsearch/xpack/profiling/GetStatusAction.java
@@ -33,18 +33,21 @@ public class GetStatusAction extends ActionType<GetStatusAction.Response> {
         private boolean profilingEnabled;
         private boolean resourceManagementEnabled;
         private boolean resourcesCreated;
+        private boolean anyResourceTooOld;
 
         public Response(StreamInput in) throws IOException {
             super(in);
             profilingEnabled = in.readBoolean();
             resourceManagementEnabled = in.readBoolean();
             resourcesCreated = in.readBoolean();
+            anyResourceTooOld = in.readBoolean();
         }
 
-        public Response(boolean profilingEnabled, boolean resourceManagementEnabled, boolean resourcesCreated) {
+        public Response(boolean profilingEnabled, boolean resourceManagementEnabled, boolean resourcesCreated, boolean anyResourceTooOld) {
             this.profilingEnabled = profilingEnabled;
             this.resourceManagementEnabled = resourceManagementEnabled;
             this.resourcesCreated = resourcesCreated;
+            this.anyResourceTooOld = anyResourceTooOld;
         }
 
         @Override
@@ -52,7 +55,7 @@ public class GetStatusAction extends ActionType<GetStatusAction.Response> {
             builder.startObject();
             builder.startObject("profiling").field("enabled", profilingEnabled).endObject();
             builder.startObject("resource_management").field("enabled", resourceManagementEnabled).endObject();
-            builder.startObject("resources").field("created", resourcesCreated).endObject();
+            builder.startObject("resources").field("created", resourcesCreated).field("too_old", anyResourceTooOld).endObject();
             builder.endObject();
             return builder;
         }
@@ -62,6 +65,7 @@ public class GetStatusAction extends ActionType<GetStatusAction.Response> {
             out.writeBoolean(profilingEnabled);
             out.writeBoolean(resourceManagementEnabled);
             out.writeBoolean(resourcesCreated);
+            out.writeBoolean(anyResourceTooOld);
         }
 
         @Override
@@ -71,12 +75,13 @@ public class GetStatusAction extends ActionType<GetStatusAction.Response> {
             Response response = (Response) o;
             return profilingEnabled == response.profilingEnabled
                 && resourceManagementEnabled == response.resourceManagementEnabled
-                && resourcesCreated == response.resourcesCreated;
+                && resourcesCreated == response.resourcesCreated
+                && anyResourceTooOld == response.anyResourceTooOld;
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(profilingEnabled, resourceManagementEnabled, resourcesCreated);
+            return Objects.hash(profilingEnabled, resourceManagementEnabled, resourcesCreated, anyResourceTooOld);
         }
 
         @Override

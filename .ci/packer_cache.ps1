606b6d793f3f33d53fc96b6faf1c89c993095882
Brian Seeders <brian.seeders@elastic.co>
2023-08-25 12:54:24 -0400

    Try to fix SSL error

--- .ci/packer_cache.ps1
+++ .ci/packer_cache.ps1
@@ -3,11 +3,12 @@ $RUNBLD = Join-Path $RUNBLD_DIR 'runbld'
 
 # Check if 7.0.3 doesn't already exist
 if (-not (Test-Path $RUNBLD)) {
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
   New-Item -Path $RUNBLD_DIR -ItemType Directory -Force
   Invoke-WebRequest -Uri 'https://packages.elasticsearch.org.s3.amazonaws.com/infra/runbld-7.0.3' -OutFile $RUNBLD
 
   $RUNBLD_HARDLINK_DIR = 'C:\Program Files\infra\bin'
-  $RUNBLD_HARDLINK = Join-Path $RUNBLD_HARDLINK_DIR 'runbld'
+  $RUNBLD_HARDLINK = Join-Path $RUNBLD_HARDLINK_DIR 'runbld-test'
 
   Remove-Item -Path $RUNBLD_HARDLINK -Force
   New-Item -Path $RUNBLD_HARDLINK_DIR -ItemType Directory -Force

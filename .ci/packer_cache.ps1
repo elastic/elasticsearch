a935d5bfaeee7997cdbf3e259cea22ba77610427
Brian Seeders <brian.seeders@elastic.co>
2023-08-25 13:06:57 -0400

    Try this instead

--- .ci/packer_cache.ps1
+++ .ci/packer_cache.ps1
@@ -3,12 +3,12 @@ $RUNBLD = Join-Path $RUNBLD_DIR 'runbld'
 
 # Check if 7.0.3 doesn't already exist
 if (-not (Test-Path $RUNBLD)) {
-  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+  [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
   New-Item -Path $RUNBLD_DIR -ItemType Directory -Force
   Invoke-WebRequest -Uri 'https://packages.elasticsearch.org.s3.amazonaws.com/infra/runbld-7.0.3' -OutFile $RUNBLD
 
   $RUNBLD_HARDLINK_DIR = 'C:\Program Files\infra\bin'
-  $RUNBLD_HARDLINK = Join-Path $RUNBLD_HARDLINK_DIR 'runbld-test'
+  $RUNBLD_HARDLINK = Join-Path $RUNBLD_HARDLINK_DIR 'runbld-test' # TODO remove -test suffix
 
   Remove-Item -Path $RUNBLD_HARDLINK -Force
   New-Item -Path $RUNBLD_HARDLINK_DIR -ItemType Directory -Force

3da43b4ed0931559f2690cac9b3184748dde3d49
Albert Zaharovits <albert.zaharovits@elastic.co>
2023-08-28 14:06:13 +0300

    test that mixed channel works though

--- server/src/test/java/org/elasticsearch/transport/InboundDecoderTests.java
+++ server/src/test/java/org/elasticsearch/transport/InboundDecoderTests.java
@@ -18,6 +18,7 @@ import org.elasticsearch.common.util.MockPageCacheRecycler;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.TransportVersionUtils;
+import org.elasticsearch.transport.InboundDecoder.ChannelType;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -225,25 +226,42 @@ public class InboundDecoderTests extends ESTestCase {
         if (randomBoolean()) {
             final String headerKey = randomAlphaOfLength(10);
             final String headerValue = randomAlphaOfLength(20);
-            threadContext.putHeader(headerKey, headerValue);
+            if (randomBoolean()) {
+                threadContext.putHeader(headerKey, headerValue);
+            } else {
+                threadContext.addResponseHeader(headerKey, headerValue);
+            }
         }
-        Compression.Scheme scheme = randomFrom(Compression.Scheme.DEFLATE, Compression.Scheme.LZ4, null);
+        // a request
         OutboundMessage message = new OutboundMessage.Request(
             threadContext,
             new TestRequest(randomAlphaOfLength(100)),
-            TransportVersion.current(),
+            TransportHandshaker.REQUEST_HANDSHAKE_VERSION,
             action,
             requestId,
             randomBoolean(),
-            scheme
+            randomFrom(Compression.Scheme.DEFLATE, Compression.Scheme.LZ4, null)
         );
 
         try (RecyclerBytesStreamOutput os = new RecyclerBytesStreamOutput(recycler)) {
             final BytesReference bytes = message.serialize(os);
-            try (InboundDecoder decoder = new InboundDecoder(recycler, InboundDecoder.ChannelType.CLIENT)) {
-                final ReleasableBytesReference releasable1 = ReleasableBytesReference.wrap(bytes);
-                IllegalArgumentException e = expectThrows(IllegalArgumentException.class, () -> decoder.decode(releasable1, ignored -> {}));
+            try (InboundDecoder clientDecoder = new InboundDecoder(recycler, ChannelType.CLIENT)) {
+                IllegalArgumentException e = expectThrows(
+                    IllegalArgumentException.class,
+                    () -> clientDecoder.decode(ReleasableBytesReference.wrap(bytes), ignored -> {})
+                );
                 assertThat(e.getMessage(), containsString("client channels do not accept inbound requests, only responses"));
+            }
+            // the same message will be decoded by a server or mixed decoder
+            try (InboundDecoder decoder = new InboundDecoder(recycler, randomFrom(ChannelType.SERVER, ChannelType.MIX))) {
+                final ArrayList<Object> fragments = new ArrayList<>();
+                int bytesConsumed = decoder.decode(ReleasableBytesReference.wrap(bytes), fragments::add);
+                int totalHeaderSize = TcpHeader.headerSize(TransportVersion.current()) + bytes.getInt(
+                    TcpHeader.VARIABLE_HEADER_SIZE_POSITION
+                );
+                assertEquals(totalHeaderSize, bytesConsumed);
+                final Header header = (Header) fragments.get(0);
+                assertEquals(requestId, header.getRequestId());
             }
         }
     }
@@ -253,24 +271,39 @@ public class InboundDecoderTests extends ESTestCase {
         if (randomBoolean()) {
             final String headerKey = randomAlphaOfLength(10);
             final String headerValue = randomAlphaOfLength(20);
-            threadContext.putHeader(headerKey, headerValue);
+            if (randomBoolean()) {
+                threadContext.putHeader(headerKey, headerValue);
+            } else {
+                threadContext.addResponseHeader(headerKey, headerValue);
+            }
         }
-        Compression.Scheme scheme = randomFrom(Compression.Scheme.DEFLATE, Compression.Scheme.LZ4, null);
+        // a response
         OutboundMessage message = new OutboundMessage.Response(
             threadContext,
             new TestResponse(randomAlphaOfLength(100)),
-            TransportVersion.current(),
+            TransportHandshaker.REQUEST_HANDSHAKE_VERSION,
             requestId,
             randomBoolean(),
-            scheme
+            randomFrom(Compression.Scheme.DEFLATE, Compression.Scheme.LZ4, null)
         );
 
         try (RecyclerBytesStreamOutput os = new RecyclerBytesStreamOutput(recycler)) {
             final BytesReference bytes = message.serialize(os);
-            try (InboundDecoder decoder = new InboundDecoder(recycler, InboundDecoder.ChannelType.SERVER)) {
+            try (InboundDecoder decoder = new InboundDecoder(recycler, ChannelType.SERVER)) {
                 final ReleasableBytesReference releasable1 = ReleasableBytesReference.wrap(bytes);
                 IllegalArgumentException e = expectThrows(IllegalArgumentException.class, () -> decoder.decode(releasable1, ignored -> {}));
                 assertThat(e.getMessage(), containsString("server channels do not accept inbound responses, only requests"));
+            }
+            // the same message will be decoded by a client or mixed decoder
+            try (InboundDecoder decoder = new InboundDecoder(recycler, randomFrom(ChannelType.CLIENT, ChannelType.MIX))) {
+                final ArrayList<Object> fragments = new ArrayList<>();
+                int bytesConsumed = decoder.decode(ReleasableBytesReference.wrap(bytes), fragments::add);
+                int totalHeaderSize = TcpHeader.headerSize(TransportVersion.current()) + bytes.getInt(
+                    TcpHeader.VARIABLE_HEADER_SIZE_POSITION
+                );
+                assertEquals(totalHeaderSize, bytesConsumed);
+                final Header header = (Header) fragments.get(0);
+                assertEquals(requestId, header.getRequestId());
             }
         }
     }

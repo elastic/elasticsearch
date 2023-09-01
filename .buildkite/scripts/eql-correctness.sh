1c8ffd931600fb5850de04c8f0c0d9a810db87ba
Brian Seeders <brian.seeders@elastic.co>
2023-09-01 11:59:45 -0400

    Cleanup

--- .buildkite/scripts/eql-correctness.sh
+++ .buildkite/scripts/eql-correctness.sh
@@ -2,12 +2,8 @@
 
 set -euo pipefail
 
-set +x
-#VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id=$VAULT_ROLE_ID secret_id=$VAULT_SECRET_ID)
-export VAULT_TOKEN
-export eql_test_credentials_file="$(pwd)/x-pack/plugin/eql/qa/correctness/credentials.gcs.json"
-vault read -field=credentials.gcs.json secret/ci/elastic-elasticsearch/migrated/eql_test_credentials > ${eql_test_credentials_file}
-#unset VAULT_TOKEN
-set -x
+eql_test_credentials_file="$(pwd)/x-pack/plugin/eql/qa/correctness/credentials.gcs.json"
+export eql_test_credentials_file
+vault read -field=credentials.gcs.json secret/ci/elastic-elasticsearch/migrated/eql_test_credentials > "${eql_test_credentials_file}"
 
 .ci/scripts/run-gradle.sh -Dignore.tests.seed :x-pack:plugin:eql:qa:correctness:check

0676742c5efa4acd00301b82b5faff1fc950491b
Saikat Sarkar <132922331+saikatsarkar056@users.noreply.github.com>
2023-09-01 10:36:16 -0600

    Update x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/search/SearchApplication.java
    Co-authored-by: Carlos Delgado <6339205+carlosdelest@users.noreply.github.com>

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/search/SearchApplication.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/search/SearchApplication.java
@@ -98,7 +98,11 @@ public class SearchApplication implements Writeable, ToXContentObject {
 
     public SearchApplication(StreamInput in, String[] indices) throws IOException {
         this.name = in.readString();
-        this.indices = indices;
+if (in.getTransportVersion().onOrAfter(INDICES_REMOVED_TRANSPORT_VERSION) {
+        this.indices = indices; // Uses the provided indices, as they are no longer serialized
+} else {
+        this.indices = in.readStringArray(); // old behaviour, read it from input as it was serialized
+}
         this.analyticsCollectionName = in.readOptionalString();
         this.updatedAtMillis = in.readLong();
         this.searchApplicationTemplate = in.readOptionalWriteable(SearchApplicationTemplate::new);

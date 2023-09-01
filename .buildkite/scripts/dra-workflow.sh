0f2b0d618e6e1ee2289a45b2ebb4c3b420c3d750
Brian Seeders <brian.seeders@elastic.co>
2023-09-01 14:07:51 -0400

    Prepare for PR

--- .buildkite/scripts/dra-workflow.sh
+++ .buildkite/scripts/dra-workflow.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 WORKFLOW="${DRA_WORKFLOW:-snapshot}"
 BRANCH="${BUILDKITE_BRANCH:-}"
-BRANCH=main # TODO remove
 
 # Don't publish main branch to staging
 if [[ "$BRANCH" == "main" && "$WORKFLOW" == "staging" ]]; then
@@ -74,12 +73,11 @@ find "$WORKSPACE" -type d -path "*/build/distributions" -exec chmod a+w {} \;
 echo --- Running release-manager
 
 # Artifacts should be generated
-# TODO remove echo, fix DRA_VAULT vars
-echo docker run --rm \
+docker run --rm \
   --name release-manager \
   -e VAULT_ADDR="$DRA_VAULT_ADDR" \
-  -e VAULT_ROLE_ID="DRA_VAULT_ROLE_ID_SECRET" \
-  -e VAULT_SECRET_ID="DRA_VAULT_SECRET_ID_SECRET" \
+  -e VAULT_ROLE_ID="$DRA_VAULT_ROLE_ID_SECRET" \
+  -e VAULT_SECRET_ID="$DRA_VAULT_SECRET_ID_SECRET" \
   --mount type=bind,readonly=false,src="$PWD",target=/artifacts \
   docker.elastic.co/infra/release-manager:latest \
   cli collect \

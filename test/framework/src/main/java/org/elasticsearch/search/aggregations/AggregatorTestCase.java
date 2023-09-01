432b62e7fdea8bc4f1630ecf13f3459fa3c5cb55
Salvatore Campagna <93581129+salvatore-campagna@users.noreply.github.com>
2023-09-01 17:42:01 +0200

    Concurrent thread access to shared doc values (#99007)
    The doc values in the `GlobalOrdCardinalityAggregator` are shared
    among multiple search threads, `search` and `search_worker`.
    The search thread also runs the aggregation phase. When an
    executor is used the 'search' thread is running `postCollection`, which
    uses doc values, while other methods are executed by the `search_worker`
    thread, using doc values too. As a result, doc values are concurrently
    accessed by different threads. Using doc values concurrently from multiple
    threads is not correct since multiple threads end up updating the doc values
    state. This breaks access to doc values resulting in different issue depending
    on how threads end up being scheduled (prematurely exhausting doc values,
    accessing incorrect documents as a result of trying to access docIds not
    in the thread owned leaf/segment,...).
    
    The solution here is to:
    1. make sure we executed `postCollection in the same thread as other
    methods, which is `search` or `search_worker`.
    2. make sure we do not call `postCollection` in case the `TimeSeriesIndexSearcher`
    is used. In that case `postCollection` is called by `TimeSeriesIndexSearcher`.

--- test/framework/src/main/java/org/elasticsearch/search/aggregations/AggregatorTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/search/aggregations/AggregatorTestCase.java
@@ -543,8 +543,8 @@ public abstract class AggregatorTestCase extends ESTestCase {
                     } else {
                         Weight weight = subSearcher.createWeight(rewritten, ScoreMode.COMPLETE, 1f);
                         subSearcher.search(weight, a.asCollector());
+                        a.postCollection();
                     }
-                    a.postCollection();
                     assertEquals(shouldBeCached, context.isCacheable());
                     internalAggs.add(a.buildTopLevel());
                 } finally {

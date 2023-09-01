7bc7946e3eb524dc675b1990700d4ae2daa30b6f
William Brafford <william.brafford@elastic.co>
2023-08-31 23:54:09 -0400

    Fix test

--- x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/utils/TransportVersionUtilsTests.java
+++ x-pack/plugin/ml/src/test/java/org/elasticsearch/xpack/ml/utils/TransportVersionUtilsTests.java
@@ -10,6 +10,7 @@ import org.elasticsearch.TransportVersion;
 import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.block.ClusterBlocks;
+import org.elasticsearch.cluster.version.VersionsWrapper;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.xpack.core.ml.utils.TransportVersionUtils;
 
@@ -19,15 +20,15 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class TransportVersionUtilsTests extends ESTestCase {
 
-    private static final Map<String, TransportVersion> transportVersions = Map.of(
+    private static final Map<String, VersionsWrapper> transportVersions = Map.of(
         "Alfredo",
-        TransportVersion.V_7_0_0,
+        new VersionsWrapper(TransportVersion.V_7_0_0),
         "Bertram",
-        TransportVersion.V_7_0_1,
+        new VersionsWrapper(TransportVersion.V_7_0_1),
         "Charles",
-        TransportVersion.V_8_500_010,
+        new VersionsWrapper(TransportVersion.V_8_500_010),
         "Dominic",
-        TransportVersion.V_8_0_0
+        new VersionsWrapper(TransportVersion.V_8_0_0)
     );
 
     private static final ClusterState state = new ClusterState(
@@ -51,7 +52,7 @@ public class TransportVersionUtilsTests extends ESTestCase {
     public void testIsMinTransformVersionSameAsCurrent() {
         assertThat(TransportVersionUtils.isMinTransportVersionSameAsCurrent(state), equalTo(false));
 
-        Map<String, TransportVersion> transportVersions1 = Map.of("Eugene", TransportVersion.current());
+        Map<String, VersionsWrapper> transportVersions1 = Map.of("Eugene", new VersionsWrapper(TransportVersion.current()));
 
         ClusterState state1 = new ClusterState(
             new ClusterName("harry"),

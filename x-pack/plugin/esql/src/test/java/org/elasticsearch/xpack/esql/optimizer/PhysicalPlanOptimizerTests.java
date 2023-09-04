7ab7dbe6bea5a5682976da38b51fd038ad15521c
Andrei Stefan <andrei@elastic.co>
2023-09-05 02:45:09 +0300

    Skip syntactic attributes when planning the physical fragment

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/optimizer/PhysicalPlanOptimizerTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/optimizer/PhysicalPlanOptimizerTests.java
@@ -67,6 +67,7 @@ import org.elasticsearch.xpack.esql.type.EsqlDataTypes;
 import org.elasticsearch.xpack.ql.expression.Attribute;
 import org.elasticsearch.xpack.ql.expression.Expressions;
 import org.elasticsearch.xpack.ql.expression.FieldAttribute;
+import org.elasticsearch.xpack.ql.expression.Literal;
 import org.elasticsearch.xpack.ql.expression.MetadataAttribute;
 import org.elasticsearch.xpack.ql.expression.NamedExpression;
 import org.elasticsearch.xpack.ql.expression.Order;
@@ -1782,6 +1783,37 @@ public class PhysicalPlanOptimizerTests extends ESTestCase {
         eval = as(project.child(), EvalExec.class);
         assertThat(Expressions.names(eval.fields()), contains(nullField));
         var source = source(eval.child());
+    }
+
+    public void testAvgSurrogateFunctionAfterRenameAndLimit() {
+        var plan = optimizedPlan(physicalPlan("""
+            from test
+            | limit 10
+            | rename first_name as FN
+            | stats average_salary = avg(salary)
+            """));
+
+        var project = as(plan, ProjectExec.class);
+        var eval = as(project.child(), EvalExec.class);
+        var limit = as(eval.child(), LimitExec.class);
+        assertThat(limit.limit(), instanceOf(Literal.class));
+        assertThat(limit.limit().fold(), equalTo(10000));
+        var aggFinal = as(limit.child(), AggregateExec.class);
+        assertThat(aggFinal.getMode(), equalTo(AggregateExec.Mode.FINAL));
+        var aggPartial = as(aggFinal.child(), AggregateExec.class);
+        assertThat(aggPartial.getMode(), equalTo(AggregateExec.Mode.PARTIAL));
+        limit = as(aggPartial.child(), LimitExec.class);
+        assertThat(limit.limit(), instanceOf(Literal.class));
+        assertThat(limit.limit().fold(), equalTo(10));
+
+        var exchange = as(limit.child(), ExchangeExec.class);
+        project = as(exchange.child(), ProjectExec.class);
+        var expectedFields = List.of("salary");
+        assertThat(Expressions.names(project.projections()), is(expectedFields));
+        var fieldExtract = as(project.child(), FieldExtractExec.class);
+        assertThat(Expressions.names(fieldExtract.attributesToExtract()), is(expectedFields));
+        var source = source(fieldExtract.child());
+        assertThat(source.limit().fold(), equalTo(10));
     }
 
     private static EsQueryExec source(PhysicalPlan plan) {

e3e053e4006d76582b0277904a845942c0279658
Armin Braun <me@obrown.io>
2023-09-04 12:03:28 +0200

    Remove redundant StreamOutput.writeMapOfLists (#99144)
    This shortcut is useless these days. We have a short-cut for writing maps
    with string keys so using this method is actually more verbose than just doing
    a normal map write in all cases. Also, this allows removing a bunch of redundant
    test code.

--- server/src/main/java/org/elasticsearch/common/io/stream/StreamOutput.java
+++ server/src/main/java/org/elasticsearch/common/io/stream/StreamOutput.java
@@ -576,26 +576,6 @@ public abstract class StreamOutput extends OutputStream {
     }
 
     /**
-     * Write a {@link Map} of {@code K}-type keys to {@code V}-type {@link List}s.
-     * <pre><code>
-     * Map&lt;String, List&lt;String&gt;&gt; map = ...;
-     * out.writeMapOfLists(map, StreamOutput::writeString, StreamOutput::writeString);
-     * </code></pre>
-     *
-     * @param keyWriter The key writer
-     * @param valueWriter The value writer
-     */
-    public final <K, V> void writeMapOfLists(final Map<K, List<V>> map, final Writer<K> keyWriter, final Writer<V> valueWriter)
-        throws IOException {
-        writeMap(map, keyWriter, (stream, list) -> {
-            writeVInt(list.size());
-            for (final V value : list) {
-                valueWriter.write(this, value);
-            }
-        });
-    }
-
-    /**
      * Write a {@link Map} of {@code K}-type keys to {@code V}-type.
      */
     public final <K extends Writeable, V extends Writeable> void writeMap(final Map<K, V> map) throws IOException {

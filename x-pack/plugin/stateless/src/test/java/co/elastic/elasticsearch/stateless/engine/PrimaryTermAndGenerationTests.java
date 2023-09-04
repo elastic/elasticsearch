297816cffbf6713df60981260d33cf8ef5f165e8
Pooya Salehi <pxsalehi@users.noreply.github.com>
2023-09-04 11:51:21 +0200

    Make PrimaryTermAndGeneration comparable (#843)

--- x-pack/plugin/stateless/src/test/java/co/elastic/elasticsearch/stateless/engine/PrimaryTermAndGenerationTests.java
+++ x-pack/plugin/stateless/src/test/java/co/elastic/elasticsearch/stateless/engine/PrimaryTermAndGenerationTests.java
@@ -19,10 +19,15 @@ package co.elastic.elasticsearch.stateless.engine;
 
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.test.AbstractWireSerializingTestCase;
+import org.elasticsearch.test.ESTestCase;
 
 import java.io.IOException;
 
-public class PrimaryTermAndGenerationSerializationTests extends AbstractWireSerializingTestCase<PrimaryTermAndGeneration> {
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+
+public class PrimaryTermAndGenerationTests extends AbstractWireSerializingTestCase<PrimaryTermAndGeneration> {
     @Override
     protected Writeable.Reader<PrimaryTermAndGeneration> instanceReader() {
         return PrimaryTermAndGeneration::new;
@@ -39,24 +44,36 @@ public class PrimaryTermAndGenerationSerializationTests extends AbstractWireSeri
     }
 
     public static PrimaryTermAndGeneration randomPrimaryTermAndGeneration() {
-        return new PrimaryTermAndGeneration(randomPositiveLong(), randomPositiveLong());
+        return new PrimaryTermAndGeneration(randomNonNegativeLong(), randomNonNegativeLong());
     }
 
     public static PrimaryTermAndGeneration mutatePrimaryTermAndGeneration(PrimaryTermAndGeneration instance) {
         return switch (randomInt(1)) {
             case 0 -> new PrimaryTermAndGeneration(
-                randomValueOtherThan(instance.primaryTerm(), PrimaryTermAndGenerationSerializationTests::randomPositiveLong),
+                randomValueOtherThan(instance.primaryTerm(), ESTestCase::randomNonNegativeLong),
                 instance.generation()
             );
             case 1 -> new PrimaryTermAndGeneration(
                 instance.primaryTerm(),
-                randomValueOtherThan(instance.generation(), PrimaryTermAndGenerationSerializationTests::randomPositiveLong)
+                randomValueOtherThan(instance.generation(), ESTestCase::randomNonNegativeLong)
             );
             default -> throw new IllegalArgumentException("Unexpected branch");
         };
     }
 
-    private static long randomPositiveLong() {
-        return randomLongBetween(0, Long.MAX_VALUE);
+    public void testCompareTo() {
+        var p1 = new PrimaryTermAndGeneration(randomNonNegativeLong(), randomNonNegativeLong());
+        var p2 = new PrimaryTermAndGeneration(p1.primaryTerm(), p1.generation());
+        assertThat("p1=" + p1 + ", p2=" + p2, p1.compareTo(p2), equalTo(0));
+
+        p1 = new PrimaryTermAndGeneration(randomNonNegativeLong(), randomNonNegativeInt());
+        p2 = new PrimaryTermAndGeneration(p1.primaryTerm(), p1.generation() + randomLongBetween(1, Byte.MAX_VALUE));
+        assertThat("p1=" + p1 + ", p2=" + p2, p1.compareTo(p2), lessThan(0));
+        assertThat("p1=" + p1 + ", p2=" + p2, p2.compareTo(p1), greaterThan(0));
+
+        p1 = new PrimaryTermAndGeneration(randomNonNegativeInt(), randomNonNegativeLong());
+        p2 = new PrimaryTermAndGeneration(p1.primaryTerm() + randomLongBetween(1, Byte.MAX_VALUE), randomNonNegativeLong());
+        assertThat("p1=" + p1 + ", p2=" + p2, p1.compareTo(p2), lessThan(0));
+        assertThat("p1=" + p1 + ", p2=" + p2, p2.compareTo(p1), greaterThan(0));
     }
 }

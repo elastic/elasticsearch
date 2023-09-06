49ed0c2c212d1275cb2ec0cb4cf487e9e75a4c63
carlosdelest <carlos.delgado@elastic.co>
2023-09-06 20:48:57 +0200

    Update TransportVersion

--- x-pack/plugin/search-business-rules/src/main/java/org/elasticsearch/xpack/searchbusinessrules/PinnedQueryBuilder.java
+++ x-pack/plugin/search-business-rules/src/main/java/org/elasticsearch/xpack/searchbusinessrules/PinnedQueryBuilder.java
@@ -56,7 +56,7 @@ public class PinnedQueryBuilder extends AbstractQueryBuilder<PinnedQueryBuilder>
     public static final ParseField DOCS_FIELD = new ParseField("docs");
     public static final ParseField ORGANIC_QUERY_FIELD = new ParseField("organic");
 
-    private static final TransportVersion OPTIONAL_INDEX_IN_DOCS_VERSION = TransportVersion.V_8_500_070;
+    private static final TransportVersion OPTIONAL_INDEX_IN_DOCS_VERSION = TransportVersion.V_8_500_071;
 
     private final List<String> ids;
     private final List<Item> docs;

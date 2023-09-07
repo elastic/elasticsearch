4606fd500d405e16826f81f9c332aafbdad4c422
Luca Cavanna <javanna@apache.org>
2023-09-07 17:35:17 +0200

    import

--- test/framework/src/main/java/org/elasticsearch/test/ESTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/ESTestCase.java
@@ -156,7 +156,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BooleanSupplier;

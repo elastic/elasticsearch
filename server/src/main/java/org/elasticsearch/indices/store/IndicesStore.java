f69c5b146006aad99564809f7871c7637551ff49
Dianna Hohensee <dianna.hohensee@elastic.co>
2023-09-08 17:12:40 -0400

    Call TransportService#registerRequestHandler with Executor (#99385)
    Remove temporary TransportService#registerRequestHandler functions
    that take String instead of Executor and update the callers. This
    is part of a larger change to pass Executor into request handlers,
    rather than creating and stashing Strings that are only used to
    eventually fetch appropriate Executors anyway.
    
    Extensive testing changes were required to make TransportService and
    ThreadPool mocks capable of looking up Executors from Strings for
    TransportAction constructors. Added a new MockUtils class to
    modularize the additional test setup.
    
    Part of the changes for #97879

--- server/src/main/java/org/elasticsearch/indices/store/IndicesStore.java
+++ server/src/main/java/org/elasticsearch/indices/store/IndicesStore.java
@@ -31,6 +31,7 @@ import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Setting.Property;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.common.util.concurrent.EsRejectedExecutionException;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.core.Tuple;
@@ -102,7 +103,7 @@ public class IndicesStore implements ClusterStateListener, Closeable {
         this.threadPool = threadPool;
         transportService.registerRequestHandler(
             ACTION_SHARD_EXISTS,
-            ThreadPool.Names.SAME,
+            EsExecutors.DIRECT_EXECUTOR_SERVICE,
             ShardActiveRequest::new,
             new ShardActiveRequestHandler()
         );

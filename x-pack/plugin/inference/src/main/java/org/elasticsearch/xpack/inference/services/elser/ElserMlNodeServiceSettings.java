b6abd562d96f6c720c20ae56257a6c4e34afa9d8
David Kyle <david.kyle@elastic.co>
2023-09-11 10:56:00 +0100

    Update x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/elser/ElserMlNodeServiceSettings.java
    Co-authored-by: David Roberts <dave.roberts@elastic.co>

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/elser/ElserMlNodeServiceSettings.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/elser/ElserMlNodeServiceSettings.java
@@ -89,7 +89,7 @@ public class ElserMlNodeServiceSettings implements ServiceSettings {
     @Override
     public int hashCode() {
         // TODO Class has no members all instances are equivalent
-        // Return the hash of NAME to make the serialization tests poss
+        // Return the hash of NAME to make the serialization tests pass
         return Objects.hashCode(NAME);
     }
 

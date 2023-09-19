48ee3a7f36604a2f0037df2d7a0e26870cb7dfa3
Max Hniebergall <max.hniebergall@elastic.co>
2023-09-19 12:46:31 -0400

    some small code fixes

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/inference/deployment/MLPlatformArchitecturesUtil.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/inference/deployment/MLPlatformArchitecturesUtil.java
@@ -10,8 +10,8 @@ package org.elasticsearch.xpack.ml.inference.deployment;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequestBuilder;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
-import org.elasticsearch.action.admin.cluster.node.info.PluginsAndModules;
 import org.elasticsearch.client.internal.Client;
+import org.elasticsearch.cluster.node.DiscoveryNodeRole;
 import org.elasticsearch.monitor.os.OsInfo;
 import org.elasticsearch.plugins.Platforms;
 import org.elasticsearch.threadpool.ThreadPool;
@@ -20,27 +20,7 @@ import org.elasticsearch.xpack.ml.MachineLearning;
 import java.util.List;
 import java.util.Set;
 
-public enum SupportedPlatformArchitectures {
-    PLATFORM_INDEPENDENT(""),
-    LINUX_ORIGINAL("linux-x86_64"),
-    LINUX_ARM("linux-aarch64"),
-    DARWIN_ORIGINAL("darwin-x86_64"),
-    DARWIN_ARM("darwin-aarch64"),
-    WINDOWS_ORIGINAL("windows-x86_64");
-
-    private final String platformArchitecture;
-
-    SupportedPlatformArchitectures(String platformArchitecture) {
-        this.platformArchitecture = platformArchitecture;
-    }
-
-    SupportedPlatformArchitectures(String osName, String osArch) {
-        platformArchitecture = Platforms.platformName(osName, osArch);
-    }
-
-    public String toString() {
-        return platformArchitecture;
-    }
+public class MLPlatformArchitecturesUtil {
 
     public static void getNodesOsArchitectures(ThreadPool threadPool, Client client, ActionListener<Set<String>> architecturesListener) {
 

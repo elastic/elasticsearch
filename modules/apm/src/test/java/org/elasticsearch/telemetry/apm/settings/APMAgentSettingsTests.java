4c53941e85c71dc765d17b9eeb50f7934172e6e9
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2023-09-20 15:12:52 +0200

    internal -> settings

--- modules/apm/src/test/java/org/elasticsearch/telemetry/apm/settings/APMAgentSettingsTests.java
+++ modules/apm/src/test/java/org/elasticsearch/telemetry/apm/settings/APMAgentSettingsTests.java
@@ -6,10 +6,9 @@
  * Side Public License, v 1.
  */
 
-package org.elasticsearch.telemetry.apm.internal;
+package org.elasticsearch.telemetry.apm.settings;
 
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.telemetry.apm.settings.APMAgentSettings;
 import org.elasticsearch.test.ESTestCase;
 
 import static org.mockito.Mockito.spy;

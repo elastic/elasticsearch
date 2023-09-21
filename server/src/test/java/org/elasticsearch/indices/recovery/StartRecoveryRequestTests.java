b4c8e7d0dc5812912948acef5f21757eb8c5232f
David Turner <david.turner@elastic.co>
2023-09-21 06:55:15 +0100

    Rename version constant

--- server/src/test/java/org/elasticsearch/indices/recovery/StartRecoveryRequestTests.java
+++ server/src/test/java/org/elasticsearch/indices/recovery/StartRecoveryRequestTests.java
@@ -68,7 +68,7 @@ public class StartRecoveryRequestTests extends ESTestCase {
         assertThat(outRequest.recoveryId(), equalTo(inRequest.recoveryId()));
         assertThat(outRequest.startingSeqNo(), equalTo(inRequest.startingSeqNo()));
 
-        if (serializationVersion.onOrAfter(TransportVersions.WAIT_FOR_RECOVERY_ADDED)) {
+        if (serializationVersion.onOrAfter(TransportVersions.WAIT_FOR_CLUSTER_STATE_IN_RECOVERY_ADDED)) {
             assertEquals(outRequest.clusterStateVersion(), inRequest.clusterStateVersion());
         } else {
             assertEquals(0L, inRequest.clusterStateVersion());

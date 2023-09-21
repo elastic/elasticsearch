18332293977cab515747dc86366e66ab23e67299
Max Hniebergall <max.hniebergall@elastic.co>
2023-09-21 13:18:28 -0400

    Revert accidental change to CI script

--- .buildkite/scripts/setup-monitoring.sh
+++ .buildkite/scripts/setup-monitoring.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#
-# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-# or more contributor license agreements. Licensed under the Elastic License
-# 2.0 and the Server Side Public License, v 1; you may not use this file except
-# in compliance with, at your election, the Elastic License 2.0 or the Server
-# Side Public License, v 1.
-#
-
 set -euo pipefail
 
 ELASTIC_AGENT_URL=$(vault read -field=url secret/ci/elastic-elasticsearch/elastic-agent-token)

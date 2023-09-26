bc2d3834df44be2a4a96e167a8c37d231fb1cb1d
Brian Seeders <brian.seeders@elastic.co>
2023-09-26 14:48:45 -0400

    Cleanup

--- .buildkite/scripts/pull-request/pipeline.generate.ts
+++ .buildkite/scripts/pull-request/pipeline.generate.ts
@@ -1,12 +1,6 @@
 import { stringify } from "yaml";
 import { execSync } from "child_process";
 
-// TODO remove these before PR
-process.env["GITHUB_PR_LABELS"] = process.env["GITHUB_PR_LABELS"] || "release_note:skip,:Delivery/Packaging,v8.11.0";
-process.env["GITHUB_PR_TARGET_BRANCH"] = process.env["GITHUB_PR_TARGET_BRANCH"] || "main";
-// process.env["GITHUB_PR_TRIGGER_COMMENT"] =
-//   "hey run elasticsearch-ci/build-benchmarks please and run elasticsearch-ci/part-2";
-
 import { generatePipelines } from "./pipeline";
 
 const pipelines = generatePipelines();

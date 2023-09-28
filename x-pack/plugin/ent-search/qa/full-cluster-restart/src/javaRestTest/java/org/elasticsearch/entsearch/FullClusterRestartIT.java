128e9d3456aa8c56e2420db0fcc972a1dd1a3f5b
Kathleen DeRusso <kathleen.derusso@elastic.co>
2023-09-28 16:57:05 -0400

    Fiddling with tests

--- x-pack/plugin/ent-search/qa/full-cluster-restart/src/javaRestTest/java/org/elasticsearch/entsearch/FullClusterRestartIT.java
+++ x-pack/plugin/ent-search/qa/full-cluster-restart/src/javaRestTest/java/org/elasticsearch/entsearch/FullClusterRestartIT.java
@@ -17,7 +17,7 @@ package org.elasticsearch.entsearch;
 import com.carrotsearch.randomizedtesting.annotations.Name;
 
 import org.elasticsearch.client.Request;
-import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.Response;
 import org.elasticsearch.test.cluster.ElasticsearchCluster;
 import org.elasticsearch.test.cluster.local.distribution.DistributionType;
 import org.elasticsearch.test.rest.ObjectPath;
@@ -55,36 +55,35 @@ public class FullClusterRestartIT extends ParameterizedFullClusterRestartTestCas
         String legacyAnalyticsCollectionName = "oldstuff";
         String newAnalyticsCollectionName = "newstuff";
 
-        try (RestClient restClient = client()) {
-            if (isRunningAgainstOldCluster()) {
-                // Create an analytics collection
-                Request legacyPutRequest = new Request("PUT", "_application/analytics/" + legacyAnalyticsCollectionName);
-                assertOK(restClient.performRequest(legacyPutRequest));
+        if (isRunningAgainstOldCluster()) {
+            // Create an analytics collection
+            Request legacyPutRequest = new Request("PUT", "_application/analytics/" + legacyAnalyticsCollectionName);
+            assertOK(client().performRequest(legacyPutRequest));
 
-                // Validate that ILM lifecycle is in place
-                assertBusy(() -> testLegacyDataRetentionPolicy(legacyAnalyticsCollectionName));
-            } else {
+            // Validate that ILM lifecycle is in place
+            assertBusy(() -> testLegacyDataRetentionPolicy(legacyAnalyticsCollectionName));
+        } else {
+            // Create a new analytics collection
+            Request putRequest = new Request("PUT", "_application/analytics/" + newAnalyticsCollectionName);
+            assertOK(client().performRequest(putRequest));
 
-                // Validate that the existing analytics collection is still using ILM
-                assertBusy(() -> testLegacyDataRetentionPolicy(legacyAnalyticsCollectionName));
+            // Validate that NO ILM lifecycle is in place and we are using DLM instead.
+            assertBusy(() -> testDlmDataRetentionPolicy(newAnalyticsCollectionName));
 
-                // Create a new analytics collection
-                Request putRequest = new Request("PUT", "_application/analytics/" + newAnalyticsCollectionName);
-                assertOK(restClient.performRequest(putRequest));
-
-                // Validate that NO ILM lifecycle is in place and we are using DLM instead.
-                assertBusy(() -> testDlmDataRetentionPolicy(newAnalyticsCollectionName));
-            }
+            // Validate that the existing analytics collection created with an older version is still using ILM
+            assertBusy(() -> testLegacyDataRetentionPolicy(legacyAnalyticsCollectionName));
         }
     }
 
     private void testLegacyDataRetentionPolicy(String analyticsCollectionName) throws IOException {
         String dataStreamName = "behavioral_analytics-events-" + analyticsCollectionName;
         Request getDataStreamRequest = new Request("GET", "_data_stream/" + dataStreamName);
-        ObjectPath dataStream = ObjectPath.createFromResponse(client().performRequest(getDataStreamRequest));
+        Response response = client().performRequest(getDataStreamRequest);
+        assertOK(response);
+        ObjectPath dataStream = ObjectPath.createFromResponse(response);
         String pathToIlmPolicy = "data_streams.0.ilm_policy";
         assertNotNull(dataStream.evaluate(pathToIlmPolicy));
-        assertEquals("behavioral_analytics-events-default_policy", dataStream.evaluate(pathToIlmPolicy));
+        assertEquals(EVENT_DATA_STREAM_LEGACY_ILM_POLICY_NAME, dataStream.evaluate(pathToIlmPolicy));
 
         Request policyRequest = new Request("GET", "_ilm/policy/" + EVENT_DATA_STREAM_LEGACY_ILM_POLICY_NAME);
         ObjectPath policy = ObjectPath.createFromResponse(client().performRequest(policyRequest));
@@ -94,7 +93,9 @@ public class FullClusterRestartIT extends ParameterizedFullClusterRestartTestCas
     private void testDlmDataRetentionPolicy(String analyticsCollectionName) throws IOException {
         String dataStreamName = "behavioral_analytics-events-" + analyticsCollectionName;
         Request getDataStreamRequest = new Request("GET", "_data_stream/" + dataStreamName);
-        ObjectPath dataStream = ObjectPath.createFromResponse(client().performRequest(getDataStreamRequest));
+        Response response = client().performRequest(getDataStreamRequest);
+        assertOK(response);
+        ObjectPath dataStream = ObjectPath.createFromResponse(response);
         assertNull(dataStream.evaluate("data_streams.0.ilm_policy"));
         assertEquals(true, dataStream.evaluate("data_streams.0.lifecycle.enabled"));
         assertEquals("180d", dataStream.evaluate("data_streams.0.lifecycle.retention"));

df52701d1d60ce9127cdafc7d7100a05ae9caf4f
Jaime Pan <33685703+NEUpanning@users.noreply.github.com>
2023-10-02 20:02:38 +0800

    Prune unnecessary information from TransportNodesInfoAction.NodeInfoRequest (#99938)
    There's no need to include the whole top-level `NodesInfoRequest` in the
    requests for info from individual nodes, and this can add substantial
    overhead if there are lots of nodes in the cluster. With this commit we
    drop the wrapper in favour of just the parts of the top-level request
    needed for the node-level processing.
    
    Relates #99744

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -150,6 +150,7 @@ public class TransportVersions {
     public static final TransportVersion ML_TRAINED_MODEL_CONFIG_PLATFORM_ADDED = def(8_507_00_0);
     public static final TransportVersion LONG_COUNT_IN_HISTOGRAM_ADDED = def(8_508_00_0);
     public static final TransportVersion INFERENCE_MODEL_SECRETS_ADDED = def(8_509_00_0);
+    public static final TransportVersion NODE_INFO_REQUEST_SIMPLIFIED = def(8_510_00_0);
     /*
      * STOP! READ THIS FIRST! No, really,
      *        ____ _____ ___  ____  _        ____  _____    _    ____    _____ _   _ ___ ____    _____ ___ ____  ____ _____ _

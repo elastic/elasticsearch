e9d662870396731b80b1ca2df78db3dee7fbab71
Nik Everett <nik9000@gmail.com>
2023-10-04 10:47:45 -0400

    Oh

---
+++ x-pack/plugin/esql/compute/src/main/generated/org/elasticsearch/compute/aggregation/MinIntAggregatorFunctionSupplier.java
@@ -0,0 +1,42 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License
+// 2.0; you may not use this file except in compliance with the Elastic License
+// 2.0.
+package org.elasticsearch.compute.aggregation;
+
+import java.lang.Integer;
+import java.lang.Override;
+import java.lang.String;
+import java.util.List;
+import org.elasticsearch.common.util.BigArrays;
+import org.elasticsearch.compute.operator.DriverContext;
+
+/**
+ * {@link AggregatorFunctionSupplier} implementation for {@link MinIntAggregator}.
+ * This class is generated. Do not edit it.
+ */
+public final class MinIntAggregatorFunctionSupplier implements AggregatorFunctionSupplier {
+  private final BigArrays bigArrays;
+
+  private final List<Integer> channels;
+
+  public MinIntAggregatorFunctionSupplier(BigArrays bigArrays, List<Integer> channels) {
+    this.bigArrays = bigArrays;
+    this.channels = channels;
+  }
+
+  @Override
+  public MinIntAggregatorFunction aggregator(DriverContext driverContext) {
+    return MinIntAggregatorFunction.create(driverContext, channels);
+  }
+
+  @Override
+  public MinIntGroupingAggregatorFunction groupingAggregator(DriverContext driverContext) {
+    return MinIntGroupingAggregatorFunction.create(channels, driverContext);
+  }
+
+  @Override
+  public String describe() {
+    return "min of ints";
+  }
+}
---
+++ x-pack/plugin/esql/compute/src/main/generated/org/elasticsearch/compute/aggregation/PercentileLongAggregatorFunctionSupplier.java
@@ -0,0 +1,46 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License
+// 2.0; you may not use this file except in compliance with the Elastic License
+// 2.0.
+package org.elasticsearch.compute.aggregation;
+
+import java.lang.Integer;
+import java.lang.Override;
+import java.lang.String;
+import java.util.List;
+import org.elasticsearch.common.util.BigArrays;
+import org.elasticsearch.compute.operator.DriverContext;
+
+/**
+ * {@link AggregatorFunctionSupplier} implementation for {@link PercentileLongAggregator}.
+ * This class is generated. Do not edit it.
+ */
+public final class PercentileLongAggregatorFunctionSupplier implements AggregatorFunctionSupplier {
+  private final BigArrays bigArrays;
+
+  private final List<Integer> channels;
+
+  private final double percentile;
+
+  public PercentileLongAggregatorFunctionSupplier(BigArrays bigArrays, List<Integer> channels,
+      double percentile) {
+    this.bigArrays = bigArrays;
+    this.channels = channels;
+    this.percentile = percentile;
+  }
+
+  @Override
+  public PercentileLongAggregatorFunction aggregator(DriverContext driverContext) {
+    return PercentileLongAggregatorFunction.create(driverContext, channels, percentile);
+  }
+
+  @Override
+  public PercentileLongGroupingAggregatorFunction groupingAggregator(DriverContext driverContext) {
+    return PercentileLongGroupingAggregatorFunction.create(channels, driverContext, bigArrays, percentile);
+  }
+
+  @Override
+  public String describe() {
+    return "percentile of longs";
+  }
+}

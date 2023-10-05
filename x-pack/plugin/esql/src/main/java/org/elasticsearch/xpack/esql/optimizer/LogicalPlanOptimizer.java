ad0a26687caf2299748a0b4362c330ac915a7b7e
Costin Leau <costin@users.noreply.github.com>
2023-10-05 19:48:39 +0300

    ESQL: Remove aliasing inside Eval (#100238)
    Evals that introduce aliased can be simplified by extracting them into a
     project (and thus signaling there's no underlying processing).
    
    The following eval:
      eval x = a + 1, y = x, z = y + 1, y = z, w = y + 1
    can be converted into:
      eval x = a + 1, z = a + 1 + 1, w = a + 1 + 1 | project x, z, z as y, w
    
    Fix #100174
    Fix #100050

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/LogicalPlanOptimizer.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/LogicalPlanOptimizer.java
@@ -87,8 +87,13 @@ public class LogicalPlanOptimizer extends RuleExecutor<LogicalPlan> {
     }
 
     protected static List<Batch<LogicalPlan>> rules() {
-        var substitutions = new Batch<>("Substitutions", Limiter.ONCE, new SubstituteSurrogates(), new ReplaceRegexMatch()
-        // new ReplaceTextFieldAttributesWithTheKeywordSubfield()
+        var substitutions = new Batch<>(
+            "Substitutions",
+            Limiter.ONCE,
+            new SubstituteSurrogates(),
+            new ReplaceRegexMatch(),
+            new ReplaceAliasingEvalWithProject()
+            // new ReplaceTextFieldAttributesWithTheKeywordSubfield()
         );
 
         var operators = new Batch<>(
@@ -850,5 +855,86 @@ public class LogicalPlanOptimizer extends RuleExecutor<LogicalPlan> {
         protected Expression regexToEquals(RegexMatch<?> regexMatch, Literal literal) {
             return new Equals(regexMatch.source(), regexMatch.field(), literal);
         }
+    }
+
+    /**
+     * Replace aliasing evals (eval x=a) with a projection which can be further combined / simplified.
+     * The rule gets applied only if there's another project (Project/Stats) above it.
+     *
+     * Needs to take into account shadowing of potentially intermediate fields:
+     * eval x = a + 1, y = x, z = y + 1, y = z, w = y + 1
+     * The output should be
+     * eval x = a + 1, z = a + 1 + 1, w = a + 1 + 1
+     * project x, z, z as y, w
+     */
+    static class ReplaceAliasingEvalWithProject extends Rule<LogicalPlan, LogicalPlan> {
+
+        @Override
+        public LogicalPlan apply(LogicalPlan logicalPlan) {
+            Holder<Boolean> enabled = new Holder<>(false);
+
+            return logicalPlan.transformDown(p -> {
+                // found projection, turn enable flag on
+                if (p instanceof Aggregate || p instanceof Project) {
+                    enabled.set(true);
+                } else if (enabled.get() && p instanceof Eval eval) {
+                    p = rule(eval);
+                }
+
+                return p;
+            });
+        }
+
+        private LogicalPlan rule(Eval eval) {
+            LogicalPlan plan = eval;
+
+            // holds simple aliases such as b = a, c = b, d = c
+            AttributeMap<Expression> basicAliases = new AttributeMap<>();
+            // same as above but keeps the original expression
+            AttributeMap<NamedExpression> basicAliasSources = new AttributeMap<>();
+
+            List<Alias> keptFields = new ArrayList<>();
+
+            var fields = eval.fields();
+            for (int i = 0, size = fields.size(); i < size; i++) {
+                Alias field = fields.get(i);
+                Expression child = field.child();
+                var attribute = field.toAttribute();
+                // put the aliases in a separate map to separate the underlying resolve from other aliases
+                if (child instanceof Attribute) {
+                    basicAliases.put(attribute, child);
+                    basicAliasSources.put(attribute, field);
+                } else {
+                    // be lazy and start replacing name aliases only if needed
+                    if (basicAliases.size() > 0) {
+                        // update the child through the field
+                        field = (Alias) field.transformUp(e -> basicAliases.resolve(e, e));
+                    }
+                    keptFields.add(field);
+                }
+            }
+
+            // at least one alias encountered, move it into a project
+            if (basicAliases.size() > 0) {
+                // preserve the eval output (takes care of shadowing and order) but replace the basic aliases
+                List<NamedExpression> projections = new ArrayList<>(eval.output());
+                // replace the removed aliases with their initial definition - however use the output to preserve the shadowing
+                for (int i = projections.size() - 1; i >= 0; i--) {
+                    NamedExpression project = projections.get(i);
+                    projections.set(i, basicAliasSources.getOrDefault(project, project));
+                }
+
+                LogicalPlan child = eval.child();
+                if (keptFields.size() > 0) {
+                    // replace the eval with just the kept fields
+                    child = new Eval(eval.source(), eval.child(), keptFields);
+                }
+                // put the projection in place
+                plan = new Project(eval.source(), child, projections);
+            }
+
+            return plan;
+        }
+
     }
 }

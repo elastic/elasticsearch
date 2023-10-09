bd1c09f3605dfee5473ca48f127a81b47855cbe0
Nhat Nguyen <nhat.nguyen@elastic.co>
2023-10-09 11:34:31 -0700

    Create new Block on filter (#100465)
    Today, we do not deduplicate data for filtered Block/Vector but rather 
    share the underlying data with the existing one. This choice might
    improve performance since it avoids copying data. However, we have
    encountered issues due to the lack of clear ownership. We will likely
    introduce reference counting for Block/Vector. Until that is
    implemented, this PR addresses the duplication of data for filtered
    Block/Vector.

--- x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/data/AbstractBlock.java
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/data/AbstractBlock.java
@@ -45,7 +45,7 @@ abstract class AbstractBlock implements Block {
         this.blockFactory = blockFactory;
         this.firstValueIndexes = firstValueIndexes;
         this.nullsMask = nullsMask == null || nullsMask.isEmpty() ? null : nullsMask;
-        assert (firstValueIndexes == null && this.nullsMask == null) == false;
+        assert nullsMask != null || firstValueIndexes != null : "Create VectorBlock instead";
     }
 
     @Override

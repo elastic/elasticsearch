261fa5e97b32a8ce25b14405f21aef295735798b
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2023-10-10 17:59:50 +0200

    more test

--- modules/apm/src/test/java/org/elasticsearch/telemetry/apm/internal/metrics/GaugeAdapterTests.java
+++ modules/apm/src/test/java/org/elasticsearch/telemetry/apm/internal/metrics/GaugeAdapterTests.java
@@ -12,6 +12,7 @@ import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.DoubleGaugeBuilder;
 import io.opentelemetry.api.metrics.LongGaugeBuilder;
 import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.metrics.ObservableDoubleMeasurement;
 import io.opentelemetry.api.metrics.ObservableLongMeasurement;
 
 import org.elasticsearch.test.ESTestCase;
@@ -27,41 +28,78 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class LongGaugeAdapterTests extends ESTestCase {
+public class GaugeAdapterTests extends ESTestCase {
     Meter testMeter = Mockito.mock(Meter.class);
     LongGaugeBuilder longGaugeBuilder = Mockito.mock(LongGaugeBuilder.class);
+    DoubleGaugeBuilder mockDoubleGaugeBuilder = Mockito.mock(DoubleGaugeBuilder.class);
 
     @Before
     public void init() {
         when(longGaugeBuilder.setDescription(Mockito.anyString())).thenReturn(longGaugeBuilder);
         when(longGaugeBuilder.setUnit(Mockito.anyString())).thenReturn(longGaugeBuilder);
 
-        DoubleGaugeBuilder mockDoubleGaugeBuilder = Mockito.mock(DoubleGaugeBuilder.class);
 
         when(mockDoubleGaugeBuilder.ofLongs()).thenReturn(longGaugeBuilder);
+        when(mockDoubleGaugeBuilder.setUnit(Mockito.anyString())).thenReturn(mockDoubleGaugeBuilder);
+        when(mockDoubleGaugeBuilder.setDescription(Mockito.anyString())).thenReturn(mockDoubleGaugeBuilder);
         when(testMeter.gaugeBuilder(anyString())).thenReturn(mockDoubleGaugeBuilder);
     }
 
-
-    //testing that a value reported is then used in a callback
+    // testing that a value reported is then used in a callback
     public void testLongGaugeRecord() {
         LongGaugeAdapter longGaugeAdapter = new LongGaugeAdapter(testMeter, "name", "desc", "unit");
 
-        //recording a value
+        // recording a value
         longGaugeAdapter.record(1L, Map.of("k", 1L));
 
-        //upon metric export, the consumer will be called
+        // upon metric export, the consumer will be called
         ArgumentCaptor<Consumer<ObservableLongMeasurement>> captor = ArgumentCaptor.forClass(Consumer.class);
         verify(longGaugeBuilder).buildWithCallback(captor.capture());
 
-
         Consumer<ObservableLongMeasurement> value = captor.getValue();
-        //making sure that a consumer will fetch the value passed down upon recording of a value
+        // making sure that a consumer will fetch the value passed down upon recording of a value
         TestLongMeasurement testLongMeasurement = new TestLongMeasurement();
         value.accept(testLongMeasurement);
 
         assertThat(testLongMeasurement.value, Matchers.equalTo(1L));
         assertThat(testLongMeasurement.attributes, Matchers.equalTo(Attributes.builder().put("k", 1).build()));
+    }
+
+    // testing that a value reported is then used in a callback
+    public void testDoubleGaugeRecord() {
+        DoubleGaugeAdapter doubleGaugeAdapter = new DoubleGaugeAdapter(testMeter, "name", "desc", "unit");
+
+        // recording a value
+        doubleGaugeAdapter.record(1.0, Map.of("k", 1.0));
+
+        // upon metric export, the consumer will be called
+        ArgumentCaptor<Consumer<ObservableDoubleMeasurement>> captor = ArgumentCaptor.forClass(Consumer.class);
+        verify(mockDoubleGaugeBuilder).buildWithCallback(captor.capture());
+
+        Consumer<ObservableDoubleMeasurement> value = captor.getValue();
+        // making sure that a consumer will fetch the value passed down upon recording of a value
+        TestDoubleMeasurement testLongMeasurement = new TestDoubleMeasurement();
+        value.accept(testLongMeasurement);
+
+        assertThat(testLongMeasurement.value, Matchers.equalTo(1.0));
+        assertThat(testLongMeasurement.attributes, Matchers.equalTo(Attributes.builder().put("k", 1.0).build()));
+    }
+
+    private static class TestDoubleMeasurement implements ObservableDoubleMeasurement {
+        double value;
+        Attributes attributes;
+
+        @Override
+        public void record(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public void record(double value, Attributes attributes) {
+            this.value = value;
+            this.attributes = attributes;
+
+        }
     }
 
     private static class TestLongMeasurement implements ObservableLongMeasurement {

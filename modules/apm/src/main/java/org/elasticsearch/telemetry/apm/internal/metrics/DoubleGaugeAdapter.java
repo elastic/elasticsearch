c9e704d5d90a78b5a9cad083c7b17cb855e39bea
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2023-10-10 16:37:26 +0200

    Fix metric gauge creation model
    OTEL gaguges should follow the callback model otherwise they will not be sent by
    apm java agent. (or use BatchCallback)
    This commit changes the gagues creation model to return Observable*Gauge
    and uses AtomicLong/Double to store current value which will be polled when
    metrics are exported (and callback is called)

--- modules/apm/src/main/java/org/elasticsearch/telemetry/apm/internal/metrics/DoubleGaugeAdapter.java
+++ modules/apm/src/main/java/org/elasticsearch/telemetry/apm/internal/metrics/DoubleGaugeAdapter.java
@@ -10,33 +10,42 @@ package org.elasticsearch.telemetry.apm.internal.metrics;
 
 import io.opentelemetry.api.metrics.Meter;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * DoubleGaugeAdapter wraps an otel ObservableDoubleMeasurement
  */
-public class DoubleGaugeAdapter extends AbstractInstrument<io.opentelemetry.api.metrics.ObservableDoubleMeasurement>
+public class DoubleGaugeAdapter extends AbstractInstrument<io.opentelemetry.api.metrics.ObservableDoubleGauge>
     implements
         org.elasticsearch.telemetry.metric.DoubleGauge {
+
+    private final AtomicReference<Double> value = new AtomicReference<>(0.0);
+    private final AtomicReference<Map<String, Object>> attributes = new AtomicReference<>(Collections.emptyMap());
 
     public DoubleGaugeAdapter(Meter meter, String name, String description, String unit) {
         super(meter, name, description, unit);
     }
 
     @Override
-    io.opentelemetry.api.metrics.ObservableDoubleMeasurement buildInstrument(Meter meter) {
+    io.opentelemetry.api.metrics.ObservableDoubleGauge buildInstrument(Meter meter) {
         var builder = Objects.requireNonNull(meter).gaugeBuilder(getName());
-        return builder.setDescription(getDescription()).setUnit(getUnit()).buildObserver();
+        return builder.setDescription(getDescription())
+            .setUnit(getUnit())
+            .buildWithCallback(measurment -> measurment.record(value.get(), OtelHelper.fromMap(attributes.get())));
     }
 
     @Override
     public void record(double value) {
-        getInstrument().record(value);
+        this.value.set(value);
+        this.attributes.set(Collections.emptyMap());
     }
 
     @Override
     public void record(double value, Map<String, Object> attributes) {
-        getInstrument().record(value, OtelHelper.fromMap(attributes));
+        this.value.set(value);
+        this.attributes.set(attributes);
     }
 }
--- modules/apm/src/main/java/org/elasticsearch/telemetry/apm/internal/metrics/LongGaugeAdapter.java
+++ modules/apm/src/main/java/org/elasticsearch/telemetry/apm/internal/metrics/LongGaugeAdapter.java
@@ -10,37 +10,44 @@ package org.elasticsearch.telemetry.apm.internal.metrics;
 
 import io.opentelemetry.api.metrics.Meter;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * LongGaugeAdapter wraps an otel ObservableLongMeasurement
  */
-public class LongGaugeAdapter extends AbstractInstrument<io.opentelemetry.api.metrics.ObservableLongMeasurement>
+public class LongGaugeAdapter extends AbstractInstrument<io.opentelemetry.api.metrics.ObservableLongGauge>
     implements
         org.elasticsearch.telemetry.metric.LongGauge {
+    private final AtomicLong value = new AtomicLong(0);
+    private final AtomicReference<Map<String, Object>> attributes = new AtomicReference<>(Collections.emptyMap());
 
     public LongGaugeAdapter(Meter meter, String name, String description, String unit) {
         super(meter, name, description, unit);
     }
 
     @Override
-    io.opentelemetry.api.metrics.ObservableLongMeasurement buildInstrument(Meter meter) {
+    io.opentelemetry.api.metrics.ObservableLongGauge buildInstrument(Meter meter) {
         return Objects.requireNonNull(meter)
             .gaugeBuilder(getName())
             .ofLongs()
             .setDescription(getDescription())
             .setUnit(getUnit())
-            .buildObserver();
+            .buildWithCallback(measurment -> measurment.record(value.get(), OtelHelper.fromMap(attributes.get())));
     }
 
     @Override
     public void record(long value) {
-        getInstrument().record(value);
+        this.value.set(value);
+        this.attributes.set(Collections.emptyMap());
     }
 
     @Override
     public void record(long value, Map<String, Object> attributes) {
-        getInstrument().record(value, OtelHelper.fromMap(attributes));
+        this.value.set(value);
+        this.attributes.set(attributes);
     }
 }

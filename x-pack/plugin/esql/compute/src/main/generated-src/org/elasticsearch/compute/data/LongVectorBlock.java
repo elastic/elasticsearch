b5e2b51423bec109feb7fc829ea4918a64ff840a
Nhat Nguyen <nhat.nguyen@elastic.co>
2023-10-11 10:23:00 -0700

    Allow release vector without release its block (#100688) (#100706)
    Today, a vector must be closed via its block. This PR removes this restriction 
    as there are cases where we only interact with the vector.

--- x-pack/plugin/esql/compute/src/main/generated-src/org/elasticsearch/compute/data/LongVectorBlock.java
+++ x-pack/plugin/esql/compute/src/main/generated-src/org/elasticsearch/compute/data/LongVectorBlock.java
@@ -71,8 +71,13 @@ public final class LongVectorBlock extends AbstractVectorBlock implements LongBl
     }
 
     @Override
+    public boolean isReleased() {
+        return released || vector.isReleased();
+    }
+
+    @Override
     public void close() {
-        if (released) {
+        if (released || vector.isReleased()) {
             throw new IllegalStateException("can't release already released block [" + this + "]");
         }
         released = true;

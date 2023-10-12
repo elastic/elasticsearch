81f6ae5cdb8575bd2037ef49e6d103f6a54050c1
Kathleen DeRusso <kathleen.derusso@elastic.co>
2023-10-12 08:09:28 -0400

    Fix intellij refactoring fail

--- x-pack/plugin/ent-search/qa/full-cluster-restart/javaRestTest/java/org/elasticsearch/xpack.application/FullClusterRestartIT.java
+++ x-pack/plugin/ent-search/qa/full-cluster-restart/javaRestTest/java/org/elasticsearch/xpack.application/FullClusterRestartIT.java
@@ -12,7 +12,7 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-package org.elasticsearch.entsearch;
+package org.elasticsearch.xpack.application;
 
 import com.carrotsearch.randomizedtesting.annotations.Name;
 

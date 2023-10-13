cdbfbf5bca78586de6b5fafd02c594947a9784fd
Jonathan Buttner <jonathan.buttner@elastic.co>
2023-10-13 08:55:20 -0400

    Revert "Adding timeout and rejection logic"
    This reverts commit acc8ba0c0b2ece48a7416a87cf9724fdb1408fd4.

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/http/sender/RequestTask.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/http/sender/RequestTask.java
@@ -13,115 +13,46 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.ActionListener;
-import org.elasticsearch.core.Nullable;
-import org.elasticsearch.core.TimeValue;
-import org.elasticsearch.threadpool.Scheduler;
-import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.xpack.inference.external.http.HttpClient;
 import org.elasticsearch.xpack.inference.external.http.HttpResult;
 
 import java.io.IOException;
 import java.util.Objects;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.elasticsearch.core.Strings.format;
-import static org.elasticsearch.xpack.inference.InferencePlugin.UTILITY_THREAD_POOL_NAME;
 
 class RequestTask extends HttpTask {
     private static final Logger logger = LogManager.getLogger(RequestTask.class);
-    private static final Scheduler.Cancellable NOOP_TIMEOUT_HANDLER = createDefaultHandler();
 
     private final HttpUriRequest request;
     private final ActionListener<HttpResult> listener;
     private final HttpClient httpClient;
     private final HttpClientContext context;
-    private final Scheduler.Cancellable timeoutHandler;
-    private final AtomicBoolean notified = new AtomicBoolean();
-    private final TimeValue timeout;
 
-    RequestTask(
-        HttpUriRequest request,
-        HttpClient httpClient,
-        HttpClientContext context,
-        @Nullable TimeValue timeout,
-        ThreadPool threadPool,
-        ActionListener<HttpResult> listener
-    ) {
+    RequestTask(HttpUriRequest request, HttpClient httpClient, HttpClientContext context, ActionListener<HttpResult> listener) {
         this.request = Objects.requireNonNull(request);
         this.httpClient = Objects.requireNonNull(httpClient);
         this.listener = Objects.requireNonNull(listener);
         this.context = Objects.requireNonNull(context);
-        this.timeout = timeout;
-        this.timeoutHandler = startTimer(threadPool, timeout);
-    }
-
-    private Scheduler.Cancellable startTimer(ThreadPool threadPool, TimeValue timeout) {
-        Objects.requireNonNull(threadPool);
-
-        if (timeout == null) {
-            return NOOP_TIMEOUT_HANDLER;
-        }
-
-        return threadPool.schedule(this::onTimeout, timeout, threadPool.executor(UTILITY_THREAD_POOL_NAME));
-    }
-
-    private void onTimeout() {
-        assert timeout != null : "timeout must be defined to use a timeout handler";
-        logger.debug(() -> format("Request [%s] timed out after [%s] while waiting to be executed", request.getRequestLine(), timeout));
-        notifyOfResult(
-            () -> listener.onFailure(new TimeoutException(format("Request timed out waiting to be executed after [%s]", timeout)))
-        );
-    }
-
-    private void notifyOfResult(Runnable runnable) {
-        if (notified.compareAndSet(false, true)) {
-            runnable.run();
-            return;
-        }
-
-        logger.debug(() -> format("Attempting to notify of result after already doing so for request [%s]", request.getRequestLine()));
     }
 
     @Override
     public void onFailure(Exception e) {
-        timeoutHandler.cancel();
-        notifyOfResult(() -> listener.onFailure(e));
+        listener.onFailure(e);
     }
 
     @Override
     protected void doRun() throws Exception {
-        ActionListener<HttpResult> notificationListener = ActionListener.wrap(this::onSuccess, this::onFailure);
-
         try {
-            httpClient.send(request, context, notificationListener);
+            httpClient.send(request, context, listener);
         } catch (IOException e) {
             logger.error(format("Failed to send request [%s] via the http client", request.getRequestLine()), e);
             listener.onFailure(new ElasticsearchException(format("Failed to send request [%s]", request.getRequestLine()), e));
         }
     }
 
-    private void onSuccess(HttpResult result) {
-        timeoutHandler.cancel();
-        notifyOfResult(() -> listener.onResponse(result));
-    }
-
     @Override
     public String toString() {
         return request.getRequestLine().toString();
-    }
-
-    private static Scheduler.Cancellable createDefaultHandler() {
-        return new Scheduler.Cancellable() {
-            @Override
-            public boolean cancel() {
-                return true;
-            }
-
-            @Override
-            public boolean isCancelled() {
-                return true;
-            }
-        };
     }
 }

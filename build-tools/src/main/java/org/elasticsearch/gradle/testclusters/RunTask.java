5c4600191fcd2b9731a0a54ff107029ff36eff47
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2023-10-13 16:47:43 +0200

    spotless

--- build-tools/src/main/java/org/elasticsearch/gradle/testclusters/RunTask.java
+++ build-tools/src/main/java/org/elasticsearch/gradle/testclusters/RunTask.java
@@ -193,7 +193,7 @@ public abstract class RunTask extends DefaultTestClustersTask {
                 if (apmServerEnabled) {
                     ApmServerBuildService apmServerBuildService = getMockApmServer().get();
                     node.setting("telemetry.metrics.enabled", "true");
-                    node.setting("tracing.apm.agent.server_url", "http://127.0.0.1:"+apmServerBuildService.getPort());
+                    node.setting("tracing.apm.agent.server_url", "http://127.0.0.1:" + apmServerBuildService.getPort());
                 }
 
             }

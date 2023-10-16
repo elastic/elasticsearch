eda6103a233f5697d2ed976c6298d446fb7b4029
Ryan Ernst <ryan@iernst.net>
2023-10-16 13:20:28 -0700

    fix test

--- modules/lang-painless/src/test/java/org/elasticsearch/painless/FunctionTests.java
+++ modules/lang-painless/src/test/java/org/elasticsearch/painless/FunctionTests.java
@@ -9,6 +9,7 @@
 package org.elasticsearch.painless;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 
 public class FunctionTests extends ScriptTestCase {
 
@@ -74,9 +75,10 @@ public class FunctionTests extends ScriptTestCase {
     }
 
     public void testInfiniteLoop() {
-        Error expected = expectScriptThrows(PainlessError.class, () -> { exec("void test() {boolean x = true; while (x) {}} test()"); });
+        var e = expectScriptThrows(ErrorCauseWrapper.class, () -> { exec("void test() {boolean x = true; while (x) {}} test()"); });
+        assertThat(e.realCause.getClass(), equalTo(PainlessError.class));
         assertThat(
-            expected.getMessage(),
+            e.getMessage(),
             containsString("The maximum number of statements that can be executed in a loop has been reached.")
         );
     }

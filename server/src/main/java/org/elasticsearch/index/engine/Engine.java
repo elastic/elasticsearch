e849d26e76e700e66d1513a242c15ea4188a5956
Artem Prigoda <artem.prigoda@elastic.co>
2023-10-16 19:22:46 +0200

    Adjust comment line length

--- server/src/main/java/org/elasticsearch/index/engine/Engine.java
+++ server/src/main/java/org/elasticsearch/index/engine/Engine.java
@@ -1998,9 +1998,9 @@ public abstract class Engine implements Closeable {
             // This is a (temporary) adapter between the older synchronous (blocking) code and the newer (async) API. Callers expect
             // exceptions to be thrown directly, but Future#get adds an ExecutionException wrapper which we must remove to preserve the
             // expected exception semantics.
-            if (e.getCause() instanceof IOException ioException) {
+            if (e.getCause()instanceof IOException ioException) {
                 throw ioException;
-            } else if (e.getCause() instanceof RuntimeException runtimeException) {
+            } else if (e.getCause()instanceof RuntimeException runtimeException) {
                 throw runtimeException;
             } else {
                 // the old code was "throws IOException" so we shouldn't see any other exception types here
@@ -2124,7 +2124,8 @@ public abstract class Engine implements Closeable {
     }
 
     /**
-     * Allows registering a listener for when the index shard is on a primary term >= minPrimaryTerm and a segment generation >= minGeneration.
+     * Allows registering a listener for when the index shard is on a primary term >= minPrimaryTerm
+     * and a segment generation >= minGeneration.
      */
     public void addPrimaryTermAndGenerationListener(long minPrimaryTerm, long minGeneration, ActionListener<Long> listener) {
         throw new UnsupportedOperationException();

d9ca42bf7d4348b90c9844181a6e1074eea18ede
Rene Groeschke <rene@elastic.co>
2023-10-19 23:18:21 +0200

    Use custom task implementation for use generate manifest (#101165)
    Follow up on #101161 to make this behave better when using gradle
    configuration cache

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/GenerateProviderManifest.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/GenerateProviderManifest.java
@@ -11,28 +11,37 @@ package org.elasticsearch.gradle.internal;
 import org.elasticsearch.gradle.util.FileUtils;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ConfigurableFileCollection;
-import org.gradle.api.provider.Provider;
+import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Classpath;
+import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFiles;
-import org.gradle.api.tasks.OutputFile;
+import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.TaskAction;
 
 import java.io.File;
 import java.util.stream.Collectors;
 
+import javax.inject.Inject;
+
 abstract class GenerateProviderManifest extends DefaultTask {
+
+    @Inject
+    public GenerateProviderManifest() {}
 
     @Classpath
     @InputFiles
     abstract public ConfigurableFileCollection getProviderImplClasspath();
 
-    @OutputFile
-    abstract public Provider<File> getManifestFile();
+    @Input
+    abstract public Property<String> getManifestName();
+
+    @OutputDirectory
+    abstract DirectoryProperty getOutputDir();
 
     @TaskAction
     void generateManifest() {
-        File manifestFile = getManifestFile().get();
-        manifestFile.getParentFile().mkdirs();
+        File manifestFile = getOutputDir().file(getManifestName().get()).get().getAsFile();
         FileUtils.write(manifestFile, generateManifestContent(), "UTF-8");
     }
 

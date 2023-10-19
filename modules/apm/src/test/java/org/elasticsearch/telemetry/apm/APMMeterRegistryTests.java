4760bbabed45f07b28c1ea537a3e17980b4de7fc
Stuart Tettemer <stu@elastic.co>
2023-10-19 10:12:02 -0500

    Move tests up from internal to apm level

--- modules/apm/src/test/java/org/elasticsearch/telemetry/apm/APMMeterRegistryTests.java
+++ modules/apm/src/test/java/org/elasticsearch/telemetry/apm/APMMeterRegistryTests.java
@@ -6,61 +6,62 @@
  * Side Public License, v 1.
  */
 
-package org.elasticsearch.telemetry.apm.internal.metrics;
+package org.elasticsearch.telemetry.apm;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.metrics.Meter;
 
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.telemetry.apm.internal.APMAgentSettings;
+import org.elasticsearch.telemetry.apm.internal.metrics.APMMeterService;
 import org.elasticsearch.telemetry.metric.DoubleCounter;
 import org.elasticsearch.test.ESTestCase;
 
 import static org.hamcrest.Matchers.sameInstance;
 
-public class APMMeterTests extends ESTestCase {
+public class APMMeterRegistryTests extends ESTestCase {
     Meter testOtel = OpenTelemetry.noop().getMeter("test");
 
     Meter noopOtel = OpenTelemetry.noop().getMeter("noop");
 
     public void testMeterIsSetUponConstruction() {
         // test default
-        APMMeter apmMeter = new APMMeter(Settings.EMPTY, () -> testOtel, () -> noopOtel);
+        APMMeterService apmMeter = new APMMeterService(Settings.EMPTY, () -> testOtel, () -> noopOtel);
 
-        Meter meter = apmMeter.getInstruments().getMeter();
+        Meter meter = apmMeter.getMeterRegistry().getMeter();
         assertThat(meter, sameInstance(noopOtel));
 
         // test explicitly enabled
         var settings = Settings.builder().put(APMAgentSettings.TELEMETRY_METRICS_ENABLED_SETTING.getKey(), true).build();
-        apmMeter = new APMMeter(settings, () -> testOtel, () -> noopOtel);
+        apmMeter = new APMMeterService(settings, () -> testOtel, () -> noopOtel);
 
-        meter = apmMeter.getInstruments().getMeter();
+        meter = apmMeter.getMeterRegistry().getMeter();
         assertThat(meter, sameInstance(testOtel));
 
         // test explicitly disabled
         settings = Settings.builder().put(APMAgentSettings.TELEMETRY_METRICS_ENABLED_SETTING.getKey(), true).build();
-        apmMeter = new APMMeter(settings, () -> testOtel, () -> noopOtel);
+        apmMeter = new APMMeterService(settings, () -> testOtel, () -> noopOtel);
 
-        meter = apmMeter.getInstruments().getMeter();
+        meter = apmMeter.getMeterRegistry().getMeter();
         assertThat(meter, sameInstance(noopOtel));
     }
 
     public void testMeterIsOverridden() {
-        APMMeter apmMeter = new APMMeter(Settings.EMPTY, () -> testOtel, () -> noopOtel);
+        APMMeterService apmMeter = new APMMeterService(Settings.EMPTY, () -> testOtel, () -> noopOtel);
 
-        Meter meter = apmMeter.getInstruments().getMeter();
+        Meter meter = apmMeter.getMeterRegistry().getMeter();
         assertThat(meter, sameInstance(noopOtel));
 
         apmMeter.setEnabled(true);
 
-        meter = apmMeter.getInstruments().getMeter();
+        meter = apmMeter.getMeterRegistry().getMeter();
         assertThat(meter, sameInstance(testOtel));
     }
 
     public void testLookupByName() {
         var settings = Settings.builder().put(APMAgentSettings.TELEMETRY_METRICS_ENABLED_SETTING.getKey(), true).build();
 
-        var apmMeter = new APMMeter(settings, () -> testOtel, () -> noopOtel);
+        var apmMeter = new APMMeterService(settings, () -> testOtel, () -> noopOtel).getMeterRegistry();
 
         DoubleCounter registeredCounter = apmMeter.registerDoubleCounter("name", "desc", "unit");
         DoubleCounter lookedUpCounter = apmMeter.getDoubleCounter("name");
@@ -70,15 +71,15 @@ public class APMMeterTests extends ESTestCase {
 
     public void testNoopIsSetOnStop() {
         var settings = Settings.builder().put(APMAgentSettings.TELEMETRY_METRICS_ENABLED_SETTING.getKey(), true).build();
-        APMMeter apmMeter = new APMMeter(settings, () -> testOtel, () -> noopOtel);
+        APMMeterService apmMeter = new APMMeterService(settings, () -> testOtel, () -> noopOtel);
         apmMeter.start();
 
-        Meter meter = apmMeter.getInstruments().getMeter();
+        Meter meter = apmMeter.getMeterRegistry().getMeter();
         assertThat(meter, sameInstance(testOtel));
 
         apmMeter.stop();
 
-        meter = apmMeter.getInstruments().getMeter();
+        meter = apmMeter.getMeterRegistry().getMeter();
         assertThat(meter, sameInstance(noopOtel));
     }
 

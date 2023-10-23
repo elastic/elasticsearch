92a5728a441359c46b1ae9fe3cc730b0ed1929df
Rene Groeschke <rene@elastic.co>
2023-10-23 22:31:10 +0200

    WIP

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/ElasticsearchBuildFinishedPlugin.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/ElasticsearchBuildFinishedPlugin.java
@@ -10,10 +10,12 @@ package org.elasticsearch.gradle.internal;
 
 import org.apache.tools.tar.TarOutputStream;
 import org.elasticsearch.gradle.util.GradleUtils;
+import org.gradle.api.Action;
 import org.gradle.api.AntBuilder;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.file.ArchiveOperations;
+import org.gradle.api.file.ConfigurableFileTree;
 import org.gradle.api.file.FileSystemOperations;
 import org.gradle.api.flow.FlowAction;
 import org.gradle.api.flow.FlowParameters;
@@ -42,12 +44,36 @@ public abstract class ElasticsearchBuildFinishedPlugin implements Plugin<Project
         String buildNumber = System.getenv("BUILD_NUMBER") != null ? System.getenv("BUILD_NUMBER") : System.getenv("BUILDKITE_BUILD_NUMBER");
         String performanceTest = System.getenv("BUILD_PERFORMANCE_TEST");
 
+
+
         if (buildNumber != null && performanceTest == null && GradleUtils.isIncludedBuild(target) == false) {
 
-            getFlowScope().always(BuildFinishedFlowAction.class, spec -> {
-                spec.getParameters().getUploadFile().set(
-                    getFlowProviders().getBuildWorkResult().map(result -> target.file("build/build-finished.txt"));
+            var fileset = target.fileTree(target.getProjectDir(), files -> {
+                files.include("**/*.hprof");
+                files.include("**/build/test-results/**/*.xml");
+                files.include("**/build/testclusters/**");
+                files.include("**/build/testrun/*/temp/**");
+                files.include("**/build/**/hs_err_pid*.log");
+                files.exclude("**/build/testclusters/**/data/**");
+                files.exclude("**/build/testclusters/**/distro/**");
+                files.exclude("**/build/testclusters/**/repo/**");
+                files.exclude("**/build/testclusters/**/extract/**");
+                files.exclude("**/build/testclusters/**/tmp/**");
+                files.exclude("**/build/testrun/*/temp/**/data/**");
+                files.exclude("**/build/testrun/*/temp/**/distro/**");
+                files.exclude("**/build/testrun/*/temp/**/repo/**");
+                files.exclude("**/build/testrun/*/temp/**/extract/**");
+                files.exclude("**/build/testrun/*/temp/**/tmp/**");
+            });
 
+
+
+            getFlowScope().always(BuildFinishedFlowAction.class, spec -> {
+
+                spec.getParameters().getUploadFile().set(
+                    getFlowProviders().getBuildWorkResult().map(result -> target.file("build/build-finished.txt")));
+                spec.getParameters().getProjectDir().set(target.getProjectDir());
+                spec.getParameters().getGradleHome().set(target.getGradle().getGradleHomeDir());
             });
         }
     }
@@ -56,6 +82,11 @@ public abstract class ElasticsearchBuildFinishedPlugin implements Plugin<Project
         interface Parameters extends FlowParameters {
             @Input
             Property<File> getUploadFile();
+
+            Property<File> getProjectDir();
+
+            Property<File> getGradleHome();
+
         }
 
         @Inject
@@ -69,8 +100,8 @@ public abstract class ElasticsearchBuildFinishedPlugin implements Plugin<Project
                 getFileSystemOperations().delete(spec -> spec.delete(uploadFile));
             }
             TarOutputStream outputStream = new TarOutputStream(new FileOutputStream(uploadFile));
-getFileOperations().fileTree("projectDir")
-            outputStream.putNextEntry("build-finished.txt", TarOutputStream.LONGFILE_GNU);
+            getFileOperations().fileTree("projectDir");
+            //outputStream.putNextEntry("build-finished.txt", TarOutputStream.LONGFILE_GNU);
         }
     }
 }

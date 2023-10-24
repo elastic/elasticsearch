f8c253a36fbdacd0aec70f4b53bcdd795e497d1d
Yang Wang <yang.wang@elastic.co>
2023-10-24 19:37:05 +1100

    S3 CAS operation should respect abortMutipartUpload failure (#101253)
    We inadvertently made s3 CAS operation to ignore abortMutipartUpload
    failures in #98664. This PR fixes it.

--- modules/repository-s3/src/main/java/org/elasticsearch/repositories/s3/S3BlobContainer.java
+++ modules/repository-s3/src/main/java/org/elasticsearch/repositories/s3/S3BlobContainer.java
@@ -741,7 +741,9 @@ class S3BlobContainer extends AbstractBlobContainer {
                                 final var currentUploadId = currentUpload.getUploadId();
                                 if (uploadId.equals(currentUploadId) == false) {
                                     blobStore.getSnapshotExecutor()
-                                        .execute(ActionRunnable.run(listeners.acquire(), () -> safeAbortMultipartUpload(currentUploadId)));
+                                        .execute(
+                                            ActionRunnable.run(listeners.acquire(), () -> abortMultipartUploadIfExists(currentUploadId))
+                                        );
                                 }
                             }
                         } finally {

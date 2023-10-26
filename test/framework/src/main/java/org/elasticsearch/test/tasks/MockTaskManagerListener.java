fb225d0f068b6262993c1f5244693d29257c7999
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2023-10-26 17:43:54 +0200

    remove unused listener method

--- test/framework/src/main/java/org/elasticsearch/test/tasks/MockTaskManagerListener.java
+++ test/framework/src/main/java/org/elasticsearch/test/tasks/MockTaskManagerListener.java
@@ -8,7 +8,6 @@
 
 package org.elasticsearch.test.tasks;
 
-import org.elasticsearch.tasks.RemovedTaskListener;
 import org.elasticsearch.tasks.Task;
 
 /**
@@ -18,6 +17,4 @@ public interface MockTaskManagerListener {
     default void onTaskRegistered(Task task) {};
 
     default void onTaskUnregistered(Task task) {};
-
-    default void subscribeForRemovedTasks(RemovedTaskListener removedTaskListener) {}
 }

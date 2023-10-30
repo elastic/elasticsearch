8fc1e853e88413c998399873be1df729953c70f8
Keith Massey <keith.massey@elastic.co>
2023-10-30 11:02:34 -0500

    fixing merge error

--- server/src/test/java/org/elasticsearch/action/bulk/TransportSimulateBulkActionTests.java
+++ server/src/test/java/org/elasticsearch/action/bulk/TransportSimulateBulkActionTests.java
@@ -23,7 +23,7 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentHelper;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.IndexNotFoundException;
-import org.elasticsearch.index.IndexVersion;
+import org.elasticsearch.index.IndexVersions;
 import org.elasticsearch.index.IndexingPressure;
 import org.elasticsearch.indices.EmptySystemIndices;
 import org.elasticsearch.tasks.Task;
@@ -100,7 +100,7 @@ public class TransportSimulateBulkActionTests extends ESTestCase {
         DiscoveryNode discoveryNode = DiscoveryNodeUtils.builder("node")
             .version(
                 VersionUtils.randomCompatibleVersion(random(), Version.CURRENT),
-                IndexVersion.MINIMUM_COMPATIBLE,
+                IndexVersions.MINIMUM_COMPATIBLE,
                 IndexVersionUtils.randomCompatibleVersion(random())
             )
             .build();

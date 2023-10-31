c7ddfef002686a8b2d3023567be96a3983899c21
Tim Vernum <tim@adjective.org>
2023-10-31 16:42:20 +1100

    Fix createComponents signature in APM-Data plugin (#101578)
    Relates: #97546, #101305

--- x-pack/plugin/apm-data/src/main/java/org/elasticsearch/xpack/apmdata/APMPlugin.java
+++ x-pack/plugin/apm-data/src/main/java/org/elasticsearch/xpack/apmdata/APMPlugin.java
@@ -10,56 +10,28 @@ package org.elasticsearch.xpack.apmdata;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.util.SetOnce;
-import org.elasticsearch.client.internal.Client;
-import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
-import org.elasticsearch.cluster.routing.allocation.AllocationService;
-import org.elasticsearch.cluster.service.ClusterService;
-import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.env.Environment;
-import org.elasticsearch.env.NodeEnvironment;
-import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.plugins.ActionPlugin;
 import org.elasticsearch.plugins.Plugin;
-import org.elasticsearch.repositories.RepositoriesService;
-import org.elasticsearch.script.ScriptService;
-import org.elasticsearch.telemetry.TelemetryProvider;
-import org.elasticsearch.threadpool.ThreadPool;
-import org.elasticsearch.watcher.ResourceWatcherService;
-import org.elasticsearch.xcontent.NamedXContentRegistry;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Supplier;
 
 public class APMPlugin extends Plugin implements ActionPlugin {
     private static final Logger logger = LogManager.getLogger(APMPlugin.class);
-    private final Settings settings;
 
     private final SetOnce<APMIndexTemplateRegistry> registry = new SetOnce<>();
 
-    public APMPlugin(Settings settings) {
-        this.settings = settings;
-    }
-
     @Override
-    public Collection<Object> createComponents(
-        Client client,
-        ClusterService clusterService,
-        ThreadPool threadPool,
-        ResourceWatcherService resourceWatcherService,
-        ScriptService scriptService,
-        NamedXContentRegistry xContentRegistry,
-        Environment environment,
-        NodeEnvironment nodeEnvironment,
-        NamedWriteableRegistry namedWriteableRegistry,
-        IndexNameExpressionResolver indexNameExpressionResolver,
-        Supplier<RepositoriesService> repositoriesServiceSupplier,
-        TelemetryProvider telemetryProvider,
-        AllocationService allocationService,
-        IndicesService indicesService
-    ) {
-        registry.set(new APMIndexTemplateRegistry(settings, clusterService, threadPool, client, xContentRegistry));
+    public Collection<?> createComponents(PluginServices services) {
+        registry.set(
+            new APMIndexTemplateRegistry(
+                services.environment().settings(),
+                services.clusterService(),
+                services.threadPool(),
+                services.client(),
+                services.xContentRegistry()
+            )
+        );
         APMIndexTemplateRegistry registryInstance = registry.get();
         logger.info("APM is {}", registryInstance.isEnabled() ? "enabled" : "disabled");
         registryInstance.initialize();

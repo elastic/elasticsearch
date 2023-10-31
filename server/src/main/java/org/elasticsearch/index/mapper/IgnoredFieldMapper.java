aa9cdec061614555f5e05220638409b1f8960364
Eyal Koren <41850454+eyalkoren@users.noreply.github.com>
2023-11-01 06:42:20 +0700

    Improve error message for unsupported aggs

--- server/src/main/java/org/elasticsearch/index/mapper/IgnoredFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/IgnoredFieldMapper.java
@@ -83,7 +83,8 @@ public final class IgnoredFieldMapper extends MetadataFieldMapper {
         public IndexFieldData.Builder fielddataBuilder(FieldDataContext fieldDataContext) {
             if (hasDocValues() == false) {
                 throw new IllegalArgumentException(
-                    "aggregations for the '" + typeName() + "' field are supported from version + " + AGGS_SUPPORT_VERSION
+                    "aggregations on the '" + typeName() + "' field are supported for indices created by stack version "
+                        + AGGS_SUPPORT_VERSION + " or higher"
                 );
             }
             return new SortedSetOrdinalsIndexFieldData.Builder(

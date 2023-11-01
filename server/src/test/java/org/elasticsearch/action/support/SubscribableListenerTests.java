bfa3fe966e9ed167b3b0fabc91ea72957b89b04c
Simon Cooper <simon.cooper@elastic.co>
2023-11-01 15:23:57 +0000

    Mute tests

--- server/src/test/java/org/elasticsearch/action/support/SubscribableListenerTests.java
+++ server/src/test/java/org/elasticsearch/action/support/SubscribableListenerTests.java
@@ -8,6 +8,7 @@
 
 package org.elasticsearch.action.support;
 
+import org.apache.lucene.tests.util.LuceneTestCase;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ElasticsearchTimeoutException;
 import org.elasticsearch.action.ActionListener;
@@ -32,6 +33,7 @@ import java.util.function.IntFunction;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
 
+@LuceneTestCase.AwaitsFix(bugUrl = "testing")
 public class SubscribableListenerTests extends ESTestCase {
 
     private static class OrderAssertingRunnable implements Runnable {

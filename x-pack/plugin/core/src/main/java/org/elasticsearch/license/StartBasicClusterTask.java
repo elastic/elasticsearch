900f10948629763844a13442230cdc5d3169a20f
Athena Brown <athena.brown@elastic.co>
2023-11-02 16:55:32 -0600

    Decouple trial license resets from product version (#100169)
    Prior to this commit, a new trial license was allowed for a cluster for
    each major version. This was a reasonable strategy when we were
    releasing major versions on a highly regular cadence, but since those
    releases have slowed, we may wish to offer new trial licenses that are
    not tied to a major version release.
    
    This commit introduces the concept of a Trial Licence Era, which is
    independent from the Elasticsearch version. In order to avoid
    complications with serialization, the TrialLicenseEra class is
    serialized in a way that is wire-compatible with the general Version
    class, and maintains the existing behavior for trials started in
    released versions. Thorough unit tests are included to verify that
    TrialLicenseEra and Version are compatible in both streamed and XContent
    representations.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/license/StartBasicClusterTask.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/license/StartBasicClusterTask.java
@@ -7,7 +7,6 @@
 package org.elasticsearch.license;
 
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.ClusterStateTaskExecutor;
@@ -15,6 +14,7 @@ import org.elasticsearch.cluster.ClusterStateTaskListener;
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.core.Nullable;
+import org.elasticsearch.license.internal.TrialLicenseVersion;
 import org.elasticsearch.xpack.core.XPackPlugin;
 
 import java.time.Clock;
@@ -78,7 +78,7 @@ public class StartBasicClusterTask implements ClusterStateTaskListener {
                     return currentLicensesMetadata;
                 }
             }
-            Version trialVersion = currentLicensesMetadata != null ? currentLicensesMetadata.getMostRecentTrialVersion() : null;
+            TrialLicenseVersion trialVersion = currentLicensesMetadata != null ? currentLicensesMetadata.getMostRecentTrialVersion() : null;
             updatedLicensesMetadata = new LicensesMetadata(selfGeneratedLicense, trialVersion);
         } else {
             updatedLicensesMetadata = currentLicensesMetadata;

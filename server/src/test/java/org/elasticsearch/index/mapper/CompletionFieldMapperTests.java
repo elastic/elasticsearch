a1035ac5386239fa5a2593f1452d38f0fca0e1ee
Luca Cavanna <javanna@apache.org>
2023-11-08 11:07:46 +0100

    iter

--- server/src/test/java/org/elasticsearch/index/mapper/CompletionFieldMapperTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/CompletionFieldMapperTests.java
@@ -15,7 +15,6 @@ import org.apache.lucene.index.DocValuesType;
 import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.suggest.document.Completion90PostingsFormat;
 import org.apache.lucene.search.suggest.document.Completion99PostingsFormat;
 import org.apache.lucene.search.suggest.document.CompletionAnalyzer;
 import org.apache.lucene.search.suggest.document.ContextSuggestField;

4b0c3c62ec6c00e9fc34cd085686dc47b67c04ad
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2023-11-08 13:26:27 +0100

    accept error messages from multiple versions

--- x-pack/qa/rolling-upgrade-multi-cluster/src/test/java/org/elasticsearch/upgrades/CcrRollingUpgradeIT.java
+++ x-pack/qa/rolling-upgrade-multi-cluster/src/test/java/org/elasticsearch/upgrades/CcrRollingUpgradeIT.java
@@ -21,6 +21,8 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.rest.action.search.RestSearchAction.TOTAL_HITS_AS_INT_PARAM;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -224,8 +226,20 @@ public class CcrRollingUpgradeIT extends AbstractMultiClusterUpgradeTestCase {
                 ResponseException.class,
                 () -> followIndex(leaderClient(), "follower", "not_supported", "not_supported")
             );
-            assertThat(e.getMessage(), containsString("the snapshot was created with index version ["));
-            assertThat(e.getMessage(), containsString("] which is higher than the version used by this node ["));
+
+            assertThat(
+                e.getMessage(),
+                allOf(
+                    anyOf(
+                        containsString("the snapshot was created with Elasticsearch version ["),
+                        containsString("the snapshot was created with index version [")
+                    ),
+                    anyOf(
+                        containsString("] which is higher than the version used by this node ["),
+                        containsString("] which is higher than the version of this node [")
+                    )
+                )
+            );
         } else if (clusterName == ClusterName.LEADER) {
             // At this point all nodes in both clusters have been updated and
             // the leader cluster can now follow not_supported index in the follower cluster:

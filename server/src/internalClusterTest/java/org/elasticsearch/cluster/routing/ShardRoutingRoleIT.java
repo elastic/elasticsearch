d9ec234f9fa1a100bb1af983abdfe15f5bd4d13a
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2023-11-08 16:05:08 +0100

    upd

--- server/src/internalClusterTest/java/org/elasticsearch/cluster/routing/ShardRoutingRoleIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/cluster/routing/ShardRoutingRoleIT.java
@@ -377,7 +377,6 @@ public class ShardRoutingRoleIT extends ESIntegTestCase {
         }
     }
 
-    // @AwaitsFix(bugUrl = "ES-4677")
     public void testRelocation() {
         var routingTableWatcher = new RoutingTableWatcher();
 

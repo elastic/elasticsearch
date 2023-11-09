956f0dafc9f5eaa8d362fc4a99644a78a9a69f02
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2023-11-09 13:49:35 -0500

    Fixing test failure due to inferred index version

--- server/src/test/java/org/elasticsearch/action/search/SearchShardsResponseTests.java
+++ server/src/test/java/org/elasticsearch/action/search/SearchShardsResponseTests.java
@@ -109,16 +109,14 @@ public class SearchShardsResponseTests extends AbstractWireSerializingTestCase<S
     }
 
     public void testLegacyResponse() {
-        DiscoveryNode node1 = DiscoveryNodeUtils.create(
-            "node-1",
-            new TransportAddress(TransportAddress.META_ADDRESS, randomInt(0xFFFF)),
-            VersionUtils.randomVersion(random())
-        );
-        DiscoveryNode node2 = DiscoveryNodeUtils.create(
-            "node-2",
-            new TransportAddress(TransportAddress.META_ADDRESS, randomInt(0xFFFF)),
-            VersionUtils.randomVersion(random())
-        );
+        DiscoveryNode node1 = DiscoveryNodeUtils.builder("node-1")
+            .address(new TransportAddress(TransportAddress.META_ADDRESS, randomInt(0xFFFF)))
+            .version(randomCompatibleVersion(random(), Version.CURRENT), IndexVersions.MINIMUM_COMPATIBLE, IndexVersion.current())
+            .build();
+        DiscoveryNode node2 = DiscoveryNodeUtils.builder("node-2")
+            .address(new TransportAddress(TransportAddress.META_ADDRESS, randomInt(0xFFFF)))
+            .version(randomCompatibleVersion(random(), Version.CURRENT), IndexVersions.MINIMUM_COMPATIBLE, IndexVersion.current())
+            .build();
         final ClusterSearchShardsGroup[] groups = new ClusterSearchShardsGroup[2];
         {
             ShardId shardId = new ShardId("index-1", "uuid-1", 0);

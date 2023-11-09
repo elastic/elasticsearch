f0556ceca660109eab9a0a997252e0c871d2e572
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2023-11-09 09:55:12 +0100

    spotless

--- build-tools/src/main/java/org/elasticsearch/gradle/testclusters/RunTask.java
+++ build-tools/src/main/java/org/elasticsearch/gradle/testclusters/RunTask.java
@@ -195,7 +195,7 @@ public abstract class RunTask extends DefaultTestClustersTask {
                         logger.warn("Unable to start APM server", e);
                     }
                 } else {
-                    //metrics are enabled by default, if the --with-apm-server was not used we should disable it
+                    // metrics are enabled by default, if the --with-apm-server was not used we should disable it
                     node.setting("telemetry.metrics.enabled", "false");
                 }
 

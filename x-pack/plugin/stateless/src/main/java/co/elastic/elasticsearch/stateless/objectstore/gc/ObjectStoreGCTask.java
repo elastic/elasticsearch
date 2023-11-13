cbbc11d6bc8aaf12932ea58ea61b15c706fd08ff
Iraklis Psaroudakis <kingherc@gmail.com>
2023-11-13 15:14:40 +0200

    Translog GC for leftover containers of left nodes (#1121)
    Extend indices GC persistent task with logic that goes around the
    translog containers, comparing them with the known nodes in the
    consistent cluster state, and deletes any unknown node containers
    if the cluster health is yellow/green.
    
    Closes

--- x-pack/plugin/stateless/src/main/java/co/elastic/elasticsearch/stateless/objectstore/gc/ObjectStoreGCTask.java
+++ x-pack/plugin/stateless/src/main/java/co/elastic/elasticsearch/stateless/objectstore/gc/ObjectStoreGCTask.java
@@ -18,6 +18,7 @@
 package co.elastic.elasticsearch.stateless.objectstore.gc;
 
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.support.RefCountingListener;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.core.TimeValue;
@@ -34,6 +35,18 @@ public class ObjectStoreGCTask extends AllocatedPersistentTask {
         Setting.Property.NodeScope
     );
 
+    public static final Setting<Boolean> STALE_TRANSLOGS_GC_ENABLED_SETTING = Setting.boolSetting(
+        "serverless.object_store.gc.stale_translogs.enabled",
+        true,
+        Setting.Property.NodeScope
+    );
+
+    public static final Setting<Integer> STALE_TRANSLOGS_GC_FILES_LIMIT_SETTING = Setting.intSetting(
+        "serverless.object_store.gc.stale_translogs.files_limit",
+        100000,
+        Setting.Property.NodeScope
+    );
+
     public static final Setting<TimeValue> GC_INTERVAL_SETTING = Setting.timeSetting(
         "serverless.object_store.gc.interval",
         TimeValue.timeValueHours(8),
@@ -43,8 +56,10 @@ public class ObjectStoreGCTask extends AllocatedPersistentTask {
     public static final String TASK_NAME = "object_store_gc";
 
     private final StaleIndicesGCService staleIndicesGCService;
+    private final StaleTranslogsGCService staleTranslogsGCService;
     private final ThreadPool threadPool;
     private final boolean staleIndicesGCEnabled;
+    private final boolean staleTranslogsGCEnabled;
     private final TimeValue interval;
 
     public ObjectStoreGCTask(
@@ -55,13 +70,16 @@ public class ObjectStoreGCTask extends AllocatedPersistentTask {
         TaskId parentTask,
         Map<String, String> headers,
         StaleIndicesGCService staleIndicesGCService,
+        StaleTranslogsGCService staleTranslogsGCService,
         ThreadPool threadPool,
         Settings settings
     ) {
         super(id, type, action, description, parentTask, headers);
         this.staleIndicesGCService = staleIndicesGCService;
+        this.staleTranslogsGCService = staleTranslogsGCService;
         this.threadPool = threadPool;
         this.staleIndicesGCEnabled = STALE_INDICES_GC_ENABLED_SETTING.get(settings);
+        this.staleTranslogsGCEnabled = STALE_TRANSLOGS_GC_ENABLED_SETTING.get(settings);
         this.interval = GC_INTERVAL_SETTING.get(settings);
     }
 
@@ -70,7 +88,10 @@ public class ObjectStoreGCTask extends AllocatedPersistentTask {
             return;
         }
 
-        cleanStaleIndices(ActionListener.running(this::rescheduleGC));
+        try (var listeners = new RefCountingListener(ActionListener.running(this::rescheduleGC))) {
+            cleanStaleIndices(listeners.acquire());
+            cleanStaleTranslogs(listeners.acquire());
+        }
     }
 
     private void cleanStaleIndices(ActionListener<Void> listener) {
@@ -80,6 +101,25 @@ public class ObjectStoreGCTask extends AllocatedPersistentTask {
         }
 
         staleIndicesGCService.cleanStaleIndices(listener);
+    }
+
+    private void cleanStaleTranslogs(ActionListener<Void> listener) {
+        if (staleTranslogsGCEnabled == false || isCancelled()) {
+            listener.onResponse(null);
+            return;
+        }
+
+        staleTranslogsGCService.cleanStaleTranslogs(listener, ActionListener.noop());
+    }
+
+    // package private for testing
+    void cleanStaleTranslogs(ActionListener<Void> listener, ActionListener<Void> listenerOnConsistentClusterState) {
+        if (staleTranslogsGCEnabled == false || isCancelled()) {
+            listener.onResponse(null);
+            return;
+        }
+
+        staleTranslogsGCService.cleanStaleTranslogs(listener, listenerOnConsistentClusterState);
     }
 
     private void rescheduleGC() {

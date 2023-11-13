37e503de0b0f24f971bf5ead620814d43b2240bf
Matteo Piergiovanni <134913285+piergm@users.noreply.github.com>
2023-11-13 10:41:33 +0100

    spotless

--- x-pack/plugin/watcher/src/internalClusterTest/java/org/elasticsearch/xpack/watcher/actions/email/EmailAttachmentTests.java
+++ x-pack/plugin/watcher/src/internalClusterTest/java/org/elasticsearch/xpack/watcher/actions/email/EmailAttachmentTests.java
@@ -41,6 +41,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+
 import javax.mail.BodyPart;
 import javax.mail.Multipart;
 import javax.mail.Part;

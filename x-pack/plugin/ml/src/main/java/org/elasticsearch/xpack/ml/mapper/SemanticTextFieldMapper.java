a9c5dbf0a6d3bd939f057a519f1d9729330073b4
carlosdelest <carlos.delgado@elastic.co>
2023-11-14 15:58:42 +0100

    First version for SemanticTextFieldMapper

---
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/mapper/SemanticTextFieldMapper.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.ml.mapper;
+
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.search.Query;
+import org.elasticsearch.index.mapper.DocumentParserContext;
+import org.elasticsearch.index.mapper.FieldMapper;
+import org.elasticsearch.index.mapper.MappedFieldType;
+import org.elasticsearch.index.mapper.MapperBuilderContext;
+import org.elasticsearch.index.mapper.SimpleMappedFieldType;
+import org.elasticsearch.index.mapper.SourceValueFetcher;
+import org.elasticsearch.index.mapper.TextSearchInfo;
+import org.elasticsearch.index.mapper.ValueFetcher;
+import org.elasticsearch.index.mapper.vectors.SparseVectorFieldMapper;
+import org.elasticsearch.index.mapper.vectors.SparseVectorFieldMapper.SparseVectorFieldType;
+import org.elasticsearch.index.query.SearchExecutionContext;
+import org.elasticsearch.xcontent.XContentParser;
+
+import java.io.IOException;
+import java.util.Map;
+
+/** A {@link FieldMapper} for full-text fields. */
+public class SemanticTextFieldMapper extends FieldMapper {
+
+    public static final String CONTENT_TYPE = "semantic_text";
+
+    public static final String TEXT_SUBFIELD_NAME = "text";
+    public static final String SPARSE_VECTOR_SUBFIELD_NAME = "inference";
+
+    private static SemanticTextFieldMapper toType(FieldMapper in) {
+        return (SemanticTextFieldMapper) in;
+    }
+
+    public static class Builder extends FieldMapper.Builder {
+
+        final Parameter<String> modelId = Parameter.stringParam("model_id", false, m -> toType(m).modelId, null).addValidator(value -> {
+            if (value == null) {
+                // TODO check the model exists
+                throw new IllegalArgumentException("field [model_id] must be specified");
+            }
+        });
+
+        private final Parameter<Map<String, String>> meta = Parameter.metaParam();
+
+        public Builder(String name) {
+            super(name);
+        }
+
+        public Builder modelId(String modelId) {
+            this.modelId.setValue(modelId);
+            return this;
+        }
+
+        @Override
+        protected Parameter<?>[] getParameters() {
+            return new Parameter<?>[] { modelId, meta };
+        }
+
+        private SemanticTextFieldType buildFieldType(MapperBuilderContext context) {
+            return new SemanticTextFieldType(context.buildFullName(name), modelId.getValue(), meta.getValue());
+        }
+
+        @Override
+        public SemanticTextFieldMapper build(MapperBuilderContext context) {
+            String fullName = context.buildFullName(name);
+            String subfieldName = fullName + "." + SPARSE_VECTOR_SUBFIELD_NAME;
+            SparseVectorFieldMapper sparseVectorFieldMapper = new SparseVectorFieldMapper.Builder(subfieldName).build(context);
+            return new SemanticTextFieldMapper(
+                name(),
+                new SemanticTextFieldType(name(), modelId.getValue(), meta.getValue()),
+                modelId.getValue(),
+                sparseVectorFieldMapper,
+                copyTo,
+                this
+            );
+        }
+    }
+
+    public static final TypeParser PARSER = new TypeParser((n, c) -> new Builder(n), notInMultiFields(CONTENT_TYPE));
+
+    public static class SemanticTextFieldType extends SimpleMappedFieldType {
+
+        private final SparseVectorFieldType sparseVectorFieldType;
+
+        private final String modelId;
+
+        public SemanticTextFieldType(String name, String modelId, Map<String, String> meta) {
+            super(name, true, false, false, TextSearchInfo.NONE, meta);
+            this.sparseVectorFieldType = new SparseVectorFieldType(name + "." + SPARSE_VECTOR_SUBFIELD_NAME, meta);
+            this.modelId = modelId;
+        }
+
+        public String modelId() {
+            return modelId;
+        }
+
+        public SparseVectorFieldType getSparseVectorFieldType() {
+            return this.sparseVectorFieldType;
+        }
+
+        @Override
+        public String typeName() {
+            return CONTENT_TYPE;
+        }
+
+        public String getInferenceModel() {
+            return modelId;
+        }
+
+        @Override
+        public ValueFetcher valueFetcher(SearchExecutionContext context, String format) {
+            return SourceValueFetcher.identity(name(), context, format);
+        }
+
+        @Override
+        public Query termQuery(Object value, SearchExecutionContext context) {
+            return sparseVectorFieldType.termQuery(value, context);
+        }
+
+        @Override
+        public Query existsQuery(SearchExecutionContext context) {
+            return sparseVectorFieldType.existsQuery(context);
+        }
+    }
+
+    private final String modelId;
+    private final SparseVectorFieldMapper sparseVectorFieldMapper;
+
+    private SemanticTextFieldMapper(
+        String simpleName,
+        MappedFieldType mappedFieldType,
+        String modelId,
+        SparseVectorFieldMapper sparseVectorFieldMapper,
+        CopyTo copyTo,
+        Builder builder
+    ) {
+        super(simpleName, mappedFieldType, MultiFields.empty(), copyTo);
+        this.modelId = modelId;
+        this.sparseVectorFieldMapper = sparseVectorFieldMapper;
+    }
+
+    @Override
+    public FieldMapper.Builder getMergeBuilder() {
+        return new Builder(simpleName()).init(this);
+    }
+
+    @Override
+    public void parse(DocumentParserContext context) throws IOException {
+
+        if (context.parser().currentToken() != XContentParser.Token.START_OBJECT) {
+            throw new IllegalArgumentException(
+                "[semantic_text] fields must be a json object, expected a START_OBJECT but got: " + context.parser().currentToken()
+            );
+        }
+
+        boolean textFound = false;
+        boolean inferenceFound = false;
+        for (XContentParser.Token token = context.parser().nextToken(); token != XContentParser.Token.END_OBJECT; token = context.parser()
+            .nextToken()) {
+            if (token != XContentParser.Token.FIELD_NAME) {
+                throw new IllegalArgumentException("[semantic_text] fields expect an object with field names, found " + token);
+            }
+
+            String fieldName = context.parser().currentName();
+            XContentParser.Token valueToken = context.parser().nextToken();
+            switch (fieldName) {
+                case TEXT_SUBFIELD_NAME:
+                    context.doc().add(new StringField(name() + TEXT_SUBFIELD_NAME, context.parser().textOrNull(), Field.Store.NO));
+                    textFound = true;
+                    break;
+                case SPARSE_VECTOR_SUBFIELD_NAME:
+                    sparseVectorFieldMapper.parse(context);
+                    inferenceFound = true;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unexpected subfield value: " + fieldName);
+            }
+        }
+
+        if (textFound == false) {
+            throw new IllegalArgumentException("[semantic_text] value does not contain [" + TEXT_SUBFIELD_NAME + "] subfield");
+        }
+        if (inferenceFound == false) {
+            throw new IllegalArgumentException("[semantic_text] value does not contain [" + SPARSE_VECTOR_SUBFIELD_NAME + "] subfield");
+        }
+    }
+
+    @Override
+    protected void parseCreateField(DocumentParserContext context) {
+        throw new AssertionError("parse is implemented directly");
+    }
+
+    @Override
+    protected String contentType() {
+        return CONTENT_TYPE;
+    }
+
+    @Override
+    public SemanticTextFieldType fieldType() {
+        return (SemanticTextFieldType) super.fieldType();
+    }
+}
+

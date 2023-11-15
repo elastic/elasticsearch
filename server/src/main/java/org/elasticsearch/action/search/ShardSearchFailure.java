24163f263770450b3812a8102f98482d269f59ff
Jan Kuipers <jan.kuipers@elastic.co>
2023-11-15 10:00:08 +0100

    Use Exception::getMessage instead of a stack trace as reason of a ShardSearchFailure.

--- server/src/main/java/org/elasticsearch/action/search/ShardSearchFailure.java
+++ server/src/main/java/org/elasticsearch/action/search/ShardSearchFailure.java
@@ -61,7 +61,7 @@ public class ShardSearchFailure extends ShardOperationFailedException {
         super(
             shardTarget == null ? null : shardTarget.getFullyQualifiedIndexName(),
             shardTarget == null ? -1 : shardTarget.getShardId().getId(),
-            ExceptionsHelper.stackTrace(e),
+            e.getMessage(),
             ExceptionsHelper.status(ExceptionsHelper.unwrapCause(e)),
             ExceptionsHelper.unwrapCause(e)
         );

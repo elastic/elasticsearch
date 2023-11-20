7bf7d0699d006c8760a3711e652b350f91b26930
Tim Rühsen <tim.ruhsen@elastic.co>
2023-11-20 12:15:58 +0100

    Add unit tests for CO2 Calculator

---
+++ x-pack/plugin/profiling/src/test/java/org/elasticsearch/xpack/profiling/CO2CalculatorTests.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.profiling;
+
+import org.elasticsearch.test.ESTestCase;
+
+import java.util.AbstractMap;
+import java.util.Map;
+
+public class CO2CalculatorTests extends ESTestCase {
+    private static final String hostIDA = "1110256254710195391";
+    private static final String hostIDB = "2220256254710195392";
+    private static final String hostIDC = "3330256254710195393";
+    private static final String hostIDD = "4440256254710195394";
+
+    public void testCreateFromRegularSource() {
+        CostsService costsService = new CostsService();
+        costsService.load();
+
+        // tag::noformat
+        Map<String, HostMetadata> hostsTable = Map.ofEntries(
+            new AbstractMap.SimpleEntry<>(hostIDA,
+                // known datacenter
+                new HostMetadata(hostIDA,
+                    new DatacenterInstance(
+                        "aws",
+                        "eu-west-1",
+                        "" // Doesn't matter for CO2 calculations.
+                    ),
+                    "" // Doesn't matter if datacenter is known.
+                )
+            ),
+            new AbstractMap.SimpleEntry<>(hostIDB,
+                new HostMetadata(hostIDB,
+                    // unknown datacenter, known provider and region, x86_64
+                    new DatacenterInstance(
+                        "gcp",
+                        "europe-west1",
+                        "" // Doesn't matter for CO2 calculations.
+                    ),
+                    "x86_64"
+                )
+            ),
+            new AbstractMap.SimpleEntry<>(hostIDC,
+                new HostMetadata(hostIDC,
+                    // unknown datacenter, known provider and region, aarch64
+                    new DatacenterInstance(
+                        "azure",
+                        "North Central US",
+                        "" // Doesn't matter for CO2 calculations.
+                    ),
+                    "aarch64"
+                )
+            ),
+            new AbstractMap.SimpleEntry<>(hostIDD,
+                new HostMetadata(hostIDD,
+                    // unknown datacenter, unknown provider and region, aarch64
+                    new DatacenterInstance(
+                        "on-prem-provider",
+                        "on-prem-region",
+                        "" // Doesn't matter for CO2 calculations.
+                    ),
+                    "aarch64"
+                )
+            )
+        );
+        // end::noformat
+
+        double duration = 1_800d; // 30 minutes
+        long samples = 100_000L; // 100k samples
+        double annualCoreHours = CostCalculator.annualCoreHours(duration, samples, 20d);
+        CO2Calculator co2Calculator = new CO2Calculator(costsService, hostsTable, duration);
+
+        checkCO2Calculation(co2Calculator.getAnnualCO2Tons(hostIDA, samples), annualCoreHours, 1.135d, 0.0002786d, 7d);
+        checkCO2Calculation(co2Calculator.getAnnualCO2Tons(hostIDB, samples), annualCoreHours, 1.1d, 0.00004452d, 7d);
+        checkCO2Calculation(co2Calculator.getAnnualCO2Tons(hostIDC, samples), annualCoreHours, 1.185d, 0.000410608d, 2.8d);
+        checkCO2Calculation(co2Calculator.getAnnualCO2Tons(hostIDD, samples), annualCoreHours, 1.7d, 0.000379069d, 2.8d);
+    }
+
+    private void checkCO2Calculation(
+        double calculatedAnnualCO2Tons,
+        double annualCoreHours,
+        double datacenterPUE,
+        double co2TonsPerKWH,
+        double wattsPerCore
+    ) {
+        double kiloWattsPerCore = wattsPerCore / 1000d;
+        double expectedAnnualCO2Tons = annualCoreHours * datacenterPUE * co2TonsPerKWH * kiloWattsPerCore;
+        assertEquals(expectedAnnualCO2Tons, calculatedAnnualCO2Tons, 0.000000000001d);
+    }
+}

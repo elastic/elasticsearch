5a3409b7c5df193899341ce8b4b6d1660078a5d2
Mary Gouseti <mary.gouseti@elastic.co>
2023-11-20 10:38:41 +0200

    ES-6566: [DSL] Introduce new endpoint to expose data stream lifecycle stats (#101845)

--- x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
+++ x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
@@ -294,6 +294,7 @@ public class Constants {
         "cluster:monitor/ccr/follow_info",
         "cluster:monitor/ccr/follow_stats",
         "cluster:monitor/ccr/stats",
+        "cluster:monitor/data_stream/lifecycle/stats",
         "cluster:monitor/eql/async/status",
         "cluster:monitor/fetch/health/info",
         "cluster:monitor/health",

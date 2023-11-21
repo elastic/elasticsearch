a4a12a668e52b1f7c43749b7c33097933dac6df7
Nik Everett <nik9000@gmail.com>
2023-11-21 16:34:20 -0500

    ESQL: Support the `_source` metadata field (#102391)
    This adds support for load the `_source` field using the syntax:
    
    ```
    FROM test [METADATA _source]
    ```
    
    The `_source` field is loaded as a special type - `_source` which no
    functions support (1). We just render it on the output. Which looks
    like:
    
    ```
    $ curl -XDELETE -uelastic:password localhost:9200/test
    $ curl -XPOST -HContent-Type:application/json -uelastic:password localhost:9200/test/_doc/1?refresh -d'{
      "words": "words",
      "other stuff": [
        "wow",
        "such",
        "suff"
      ]
    }'
    $ curl -XPOST -HContent-Type:application/json -uelastic:password localhost:9200/_query?pretty -d'{
      "query": "FROM test [METADATA _source] | KEEP _source | LIMIT 1"
    }'
    
    {
      "columns" : [
        {
          "name" : "_source",
          "type" : "_source"
        }
      ],
      "values" : [
        [
          {
            "words" : "words",
            "other stuff" : [
              "wow",
              "such",
              "suff"
            ]
          }
        ]
      ]
    }
    ```
    
    The `_source` is just a json object. We use the same infrastructure to
    convert it to json as the `_search` response.
    
    This works for both stored `_source` and synthetic `_source`, but it
    runs row-by-row every time. This *perfect* for stored `_source` but it's
    less nice to synthetic `_source`. We'd be better of rebuilding synthetic
    `_source` from blocks but that'd require a lot of new infrastructure.
    And synthetic `_source` isn't going to be fast anyway.
    
    (1): `IS NULL` and `IS NOT NULL` support `_source` because we get that
    for free.

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/action/EsqlQueryResponseTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/action/EsqlQueryResponseTests.java
@@ -11,6 +11,7 @@ import org.apache.lucene.document.InetAddressPoint;
 import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.breaker.CircuitBreaker;
+import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.unit.ByteSizeValue;
@@ -32,6 +33,7 @@ import org.elasticsearch.core.Releasables;
 import org.elasticsearch.test.AbstractChunkedSerializingTestCase;
 import org.elasticsearch.xcontent.XContentParser;
 import org.elasticsearch.xcontent.XContentType;
+import org.elasticsearch.xcontent.json.JsonXContent;
 import org.elasticsearch.xpack.esql.planner.LocalExecutionPlanner;
 import org.elasticsearch.xpack.esql.type.EsqlDataTypes;
 import org.elasticsearch.xpack.ql.type.DataType;
@@ -40,6 +42,8 @@ import org.elasticsearch.xpack.versionfield.Version;
 import org.junit.After;
 import org.junit.Before;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -112,6 +116,20 @@ public class EsqlQueryResponseTests extends AbstractChunkedSerializingTestCase<E
                 );
                 case "version" -> ((BytesRefBlock.Builder) builder).appendBytesRef(new Version(randomIdentifier()).toBytesRef());
                 case "null" -> builder.appendNull();
+                case "_source" -> {
+                    try {
+                        ((BytesRefBlock.Builder) builder).appendBytesRef(
+                            BytesReference.bytes(
+                                JsonXContent.contentBuilder()
+                                    .startObject()
+                                    .field(randomAlphaOfLength(3), randomAlphaOfLength(10))
+                                    .endObject()
+                            ).toBytesRef()
+                        );
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                }
                 default -> throw new UnsupportedOperationException("unsupported data type [" + c + "]");
             }
             return builder.build();

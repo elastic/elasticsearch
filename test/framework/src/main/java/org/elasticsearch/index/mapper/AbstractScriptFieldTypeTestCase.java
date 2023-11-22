bb8edcf7353d820e5c3906554d208b62069fd5ed
Nik Everett <nik9000@gmail.com>
2023-11-22 10:13:10 -0500

    ESQL: Load text field from parent keyword field
    This adds support for loading a text field from a parent keyword field.
    The mapping for that looks like:
    ```
    "properties": {
      "foo": {
        "type": "keyword",
        "fields": {
          "text": { "type": "text" }
        }
      }
    }
    ```
    
    In this case it's safe to load the `text` subfield from the doc values
    for the `keyword` field above.

--- test/framework/src/main/java/org/elasticsearch/index/mapper/AbstractScriptFieldTypeTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/index/mapper/AbstractScriptFieldTypeTestCase.java
@@ -430,6 +430,11 @@ public abstract class AbstractScriptFieldTypeTestCase extends MapperServiceTestC
             public Set<String> sourcePaths(String name) {
                 throw new UnsupportedOperationException();
             }
+
+            @Override
+            public String parentField(String field) {
+                throw new UnsupportedOperationException();
+            }
         };
     }
 

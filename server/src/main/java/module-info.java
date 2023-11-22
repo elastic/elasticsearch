2c44a5fbe57cd9acf04a1a24084d4ae6d458cf4c
gmarouli <mary.gouseti@elastic.co>
2023-11-22 15:10:08 +0200

    Fix format in module-info.java

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -422,5 +422,9 @@ module org.elasticsearch.server {
             org.elasticsearch.index.codec.bloomfilter.ES87BloomFilterPostingsFormat;
     provides org.apache.lucene.codecs.DocValuesFormat with ES87TSDBDocValuesFormat;
 
-    exports org.elasticsearch.cluster.routing.allocation.shards to org.elasticsearch.shardhealth, org.elasticsearch.serverless.shardhealth, org.elasticsearch.serverless.apifiltering;
+    exports org.elasticsearch.cluster.routing.allocation.shards
+        to
+            org.elasticsearch.shardhealth,
+            org.elasticsearch.serverless.shardhealth,
+            org.elasticsearch.serverless.apifiltering;
 }

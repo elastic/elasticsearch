8a5bbadfac118bf06e9aeccf8ec90e0b1a432c39
Lorenzo Dematte <lorenzo.dematte@elastic.co>
2023-11-23 17:23:54 +0100

    spotless

--- test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
@@ -111,6 +111,7 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
 import javax.net.ssl.SSLContext;
 
 import static java.util.Collections.sort;

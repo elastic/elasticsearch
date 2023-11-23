fe580af29786c503cca4b989c873f850282417d9
Lorenzo Dematte <lorenzo.dematte@elastic.co>
2023-11-23 11:47:56 +0100

    Use Map.ofEntries

--- test/framework/src/main/java/org/elasticsearch/test/rest/RestTestLegacyFeatures.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/RestTestLegacyFeatures.java
@@ -14,6 +14,8 @@ import org.elasticsearch.features.NodeFeature;
 
 import java.util.Map;
 
+import static java.util.Map.entry;
+
 /**
  * This class groups historical features that have been removed from the production codebase, but are still used by the test
  * framework to support BwC tests. Rather than leaving them in the main src we group them here, so it's clear they are not used in
@@ -30,17 +32,12 @@ public class RestTestLegacyFeatures implements FeatureSpecification {
 
     @Override
     public Map<NodeFeature, Version> getHistoricalFeatures() {
-        return Map.of(
-            FEATURE_STATE_RESET_SUPPORTED,
-            Version.V_7_13_0,
-            SYSTEM_INDICES_REST_ACCESS_ENFORCED,
-            Version.V_8_0_0,
-            HIDDEN_INDICES_SUPPORTED,
-            Version.V_7_7_0,
-            COMPONENT_TEMPLATE_SUPPORTED,
-            Version.V_7_8_0,
-            DELETE_TEMPLATE_MULTIPLE_NAMES_SUPPORTED,
-            Version.V_7_13_0
+        return Map.ofEntries(
+            entry(FEATURE_STATE_RESET_SUPPORTED, Version.V_7_13_0),
+            entry(SYSTEM_INDICES_REST_ACCESS_ENFORCED, Version.V_8_0_0),
+            entry(HIDDEN_INDICES_SUPPORTED, Version.V_7_7_0),
+            entry(COMPONENT_TEMPLATE_SUPPORTED, Version.V_7_8_0),
+            entry(DELETE_TEMPLATE_MULTIPLE_NAMES_SUPPORTED, Version.V_7_13_0)
         );
     }
 }

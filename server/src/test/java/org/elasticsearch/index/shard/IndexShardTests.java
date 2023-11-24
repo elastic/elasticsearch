dab917b6dea88d37b191a916e7ab9819b38b0b34
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2023-11-24 11:07:12 +0100

    Add extra logging

--- server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
+++ server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
@@ -4178,6 +4178,7 @@ public class IndexShardTests extends IndexShardTestCase {
 
         int numberOfFlushes = randomIntBetween(5, 10);
         for (int i = 0; i < numberOfFlushes; i++) {
+            logger.info("--> Writing doc number {}", i);
             indexDoc(shard, "_doc", Integer.toString(i));
             shard.afterWriteOperation();
             safeAwait(flushExecutedBarrier);

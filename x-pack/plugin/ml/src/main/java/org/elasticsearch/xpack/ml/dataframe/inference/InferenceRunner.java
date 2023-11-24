bce728326cf5e009cc372e77cd3326e3904f65b4
Max Hniebergall <max.hniebergall@elastic.co>
2023-11-24 14:16:23 -0500

    InferenceRunner - Throw statusException if a statusException is caught

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/dataframe/inference/InferenceRunner.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/dataframe/inference/InferenceRunner.java
@@ -10,6 +10,7 @@ package org.elasticsearch.xpack.ml.dataframe.inference;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.ElasticsearchStatusException;
 import org.elasticsearch.action.DocWriteRequest;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.index.IndexRequest;
@@ -117,6 +118,17 @@ public class InferenceRunner {
             LOGGER.error(() -> format("[%s] Error running inference on model [%s]", config.getId(), modelId), e);
 
             if (e instanceof ElasticsearchException) {
+                if (e instanceof ElasticsearchStatusException) {
+                    Throwable rootCause = ((ElasticsearchStatusException) e).getRootCause();
+                    throw new ElasticsearchStatusException(
+                        "[{}] failed running inference on model [{}]; cause was [{}]",
+                        ((ElasticsearchStatusException) e).status(),
+                        rootCause,
+                        config.getId(),
+                        modelId,
+                        rootCause.getMessage()
+                    );
+                }
                 Throwable rootCause = ((ElasticsearchException) e).getRootCause();
                 throw new ElasticsearchException(
                     "[{}] failed running inference on model [{}]; cause was [{}]",

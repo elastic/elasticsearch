606b739a839f88026e6b82e499768cb46d30ec98
Yang Wang <yang.wang@elastic.co>
2023-11-24 19:49:30 +1100

    address feedback and add test

--- modules/repository-s3/src/main/java/org/elasticsearch/repositories/s3/S3BlobStore.java
+++ modules/repository-s3/src/main/java/org/elasticsearch/repositories/s3/S3BlobStore.java
@@ -45,6 +45,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicReference;
@@ -54,8 +55,11 @@ import java.util.stream.Collectors;
 import static org.elasticsearch.core.Strings.format;
 import static org.elasticsearch.repositories.RepositoriesModule.METRIC_EXCEPTIONS_COUNT;
 import static org.elasticsearch.repositories.RepositoriesModule.METRIC_EXCEPTIONS_HISTOGRAM;
+import static org.elasticsearch.repositories.RepositoriesModule.METRIC_OPERATIONS_COUNT;
 import static org.elasticsearch.repositories.RepositoriesModule.METRIC_REQUESTS_COUNT;
 import static org.elasticsearch.repositories.RepositoriesModule.METRIC_THROTTLES_COUNT;
+import static org.elasticsearch.repositories.RepositoriesModule.METRIC_THROTTLES_HISTOGRAM;
+import static org.elasticsearch.repositories.RepositoriesModule.METRIC_UNSUCCESSFUL_OPERATIONS_COUNT;
 
 class S3BlobStore implements BlobStore {
 
@@ -89,6 +93,8 @@ class S3BlobStore implements BlobStore {
     private final LongCounter requestCounter;
     private final LongCounter exceptionCounter;
     private final LongCounter throttleCounter;
+    private final LongCounter operationCounter;
+    private final LongCounter unsuccessfulOperationCounter;
     private final LongHistogram exceptionHistogram;
     private final LongHistogram throttleHistogram;
 
@@ -124,8 +130,10 @@ class S3BlobStore implements BlobStore {
         this.requestCounter = this.meterRegistry.getLongCounter(METRIC_REQUESTS_COUNT);
         this.exceptionCounter = this.meterRegistry.getLongCounter(METRIC_EXCEPTIONS_COUNT);
         this.throttleCounter = this.meterRegistry.getLongCounter(METRIC_THROTTLES_COUNT);
+        this.operationCounter = this.meterRegistry.getLongCounter(METRIC_OPERATIONS_COUNT);
+        this.unsuccessfulOperationCounter = this.meterRegistry.getLongCounter(METRIC_UNSUCCESSFUL_OPERATIONS_COUNT);
         this.exceptionHistogram = this.meterRegistry.getLongHistogram(METRIC_EXCEPTIONS_HISTOGRAM);
-        this.throttleHistogram = this.meterRegistry.getLongHistogram(METRIC_EXCEPTIONS_HISTOGRAM);
+        this.throttleHistogram = this.meterRegistry.getLongHistogram(METRIC_THROTTLES_HISTOGRAM);
         s3RequestRetryStats = new S3RequestRetryStats(getMaxRetries());
         threadPool.scheduleWithFixedDelay(() -> {
             var priorRetryStats = s3RequestRetryStats;
@@ -181,18 +189,39 @@ class S3BlobStore implements BlobStore {
 
         @Override
         public final void collectMetrics(Request<?> request, Response<?> response) {
-            if (response != null) {
-                assert assertConsistencyBetweenHttpRequestAndOperation(request, operation);
-                final TimingInfo timingInfo = request.getAWSRequestMetrics().getTimingInfo();
-                final long requestCount = getCountForMetric(timingInfo, AWSRequestMetrics.Field.RequestCount);
-                final long exceptionCount = getCountForMetric(timingInfo, AWSRequestMetrics.Field.Exception);
-                final long throttleCount = getCountForMetric(timingInfo, AWSRequestMetrics.Field.ThrottleException);
+            assert assertConsistencyBetweenHttpRequestAndOperation(request, operation);
+            final AWSRequestMetrics awsRequestMetrics = request.getAWSRequestMetrics();
+            final TimingInfo timingInfo = awsRequestMetrics.getTimingInfo();
+            final long requestCount = getCountForMetric(timingInfo, AWSRequestMetrics.Field.RequestCount);
+            final long exceptionCount = getCountForMetric(timingInfo, AWSRequestMetrics.Field.Exception);
+            final long throttleCount = getCountForMetric(timingInfo, AWSRequestMetrics.Field.ThrottleException);
 
+            // For stats reported by API, do not collect stats for null response for BWC.
+            // See https://github.com/elastic/elasticsearch/pull/71406
+            // TODO Is this BWC really necessary?
+            if (response != null) {
                 counter.add(requestCount);
-                requestCounter.incrementBy(requestCount, attributes);
+            }
+
+            // We collect all metrics regardless whether response is null
+            // There are many situations other than network where a null response can be returned.
+            // In addition, we are interested in the stats when there is a network outage.
+            final int numberOfAwsErrors = Optional.ofNullable(awsRequestMetrics.getProperty(AWSRequestMetrics.Field.AWSErrorCode))
+                .map(List::size)
+                .orElse(0);
+
+            operationCounter.incrementBy(1, attributes);
+            if (numberOfAwsErrors == requestCount) {
+                unsuccessfulOperationCounter.incrementBy(1, attributes);
+            }
+
+            requestCounter.incrementBy(requestCount, attributes);
+            if (exceptionCount > 0) {
                 exceptionCounter.incrementBy(exceptionCount, attributes);
-                throttleCounter.incrementBy(throttleCount, attributes);
                 exceptionHistogram.record(exceptionCount, attributes);
+            }
+            if (throttleCount > 0) {
+                throttleCounter.incrementBy(throttleCount, attributes);
                 throttleHistogram.record(throttleCount, attributes);
             }
         }
@@ -223,7 +252,9 @@ class S3BlobStore implements BlobStore {
         var count = info.getCounter(field.name());
         if (count == null) {
             if (field == AWSRequestMetrics.Field.RequestCount) {
-                logger.warn("Expected request count to be tracked but found not count.");
+                final String message = "Expected request count to be tracked but found not count.";
+                assert false : message;
+                logger.warn(message);
             }
             return 0L;
         } else {
--- server/src/main/java/org/elasticsearch/repositories/RepositoriesModule.java
+++ server/src/main/java/org/elasticsearch/repositories/RepositoriesModule.java
@@ -38,6 +38,8 @@ public final class RepositoriesModule {
     public static final String METRIC_REQUESTS_COUNT = "es.repositories.requests.count";
     public static final String METRIC_EXCEPTIONS_COUNT = "es.repositories.exceptions.count";
     public static final String METRIC_THROTTLES_COUNT = "es.repositories.throttles.count";
+    public static final String METRIC_OPERATIONS_COUNT = "es.repositories.operations.count";
+    public static final String METRIC_UNSUCCESSFUL_OPERATIONS_COUNT = "es.repositories.operations.unsuccessful.count";
     public static final String METRIC_EXCEPTIONS_HISTOGRAM = "es.repositories.exceptions.histogram";
     public static final String METRIC_THROTTLES_HISTOGRAM = "es.repositories.throttles.histogram";
 
@@ -55,7 +57,11 @@ public final class RepositoriesModule {
     ) {
         telemetryProvider.getMeterRegistry().registerLongCounter(METRIC_REQUESTS_COUNT, "repository request counter", "unit");
         telemetryProvider.getMeterRegistry().registerLongCounter(METRIC_EXCEPTIONS_COUNT, "repository request exception counter", "unit");
-        telemetryProvider.getMeterRegistry().registerLongCounter(METRIC_THROTTLES_COUNT, "repository request throttle counter", "unit");
+        telemetryProvider.getMeterRegistry().registerLongCounter(METRIC_THROTTLES_COUNT, "repository operation counter", "unit");
+        telemetryProvider.getMeterRegistry()
+            .registerLongCounter(METRIC_OPERATIONS_COUNT, "repository unsuccessful operation counter", "unit");
+        telemetryProvider.getMeterRegistry()
+            .registerLongCounter(METRIC_UNSUCCESSFUL_OPERATIONS_COUNT, "repository request throttle counter", "unit");
         telemetryProvider.getMeterRegistry()
             .registerLongHistogram(METRIC_EXCEPTIONS_HISTOGRAM, "repository request exception histogram", "unit");
         telemetryProvider.getMeterRegistry()

0dff470b49fc13ddb83abff8dd20b5d27492fb42
Salvatore Campagna <salvatore.campagna@elastic.co>
2023-11-24 16:46:51 +0100

    fix: no way to get tsid from dimensions anymore

--- modules/aggregations/src/internalClusterTest/java/org/elasticsearch/aggregations/bucket/TimeSeriesAggregationsIT.java
+++ modules/aggregations/src/internalClusterTest/java/org/elasticsearch/aggregations/bucket/TimeSeriesAggregationsIT.java
@@ -15,55 +15,28 @@ import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.aggregations.AggregationIntegTestCase;
-import org.elasticsearch.aggregations.bucket.timeseries.InternalTimeSeries;
 import org.elasticsearch.aggregations.bucket.timeseries.TimeSeriesAggregationBuilder;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.mapper.DateFieldMapper;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.RangeQueryBuilder;
-import org.elasticsearch.search.aggregations.Aggregations;
-import org.elasticsearch.search.aggregations.Aggregator;
-import org.elasticsearch.search.aggregations.PipelineAggregatorBuilders;
-import org.elasticsearch.search.aggregations.bucket.MultiBucketsAggregation;
-import org.elasticsearch.search.aggregations.bucket.global.Global;
-import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramInterval;
-import org.elasticsearch.search.aggregations.bucket.histogram.Histogram;
-import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.metrics.CompensatedSum;
-import org.elasticsearch.search.aggregations.metrics.Stats;
-import org.elasticsearch.search.aggregations.metrics.Sum;
-import org.elasticsearch.search.aggregations.pipeline.SimpleValue;
 import org.elasticsearch.search.sort.SortOrder;
 import org.elasticsearch.test.ESIntegTestCase;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentFactory;
 
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static org.elasticsearch.search.aggregations.AggregationBuilders.dateHistogram;
-import static org.elasticsearch.search.aggregations.AggregationBuilders.global;
-import static org.elasticsearch.search.aggregations.AggregationBuilders.stats;
-import static org.elasticsearch.search.aggregations.AggregationBuilders.sum;
-import static org.elasticsearch.search.aggregations.AggregationBuilders.terms;
 import static org.elasticsearch.search.aggregations.AggregationBuilders.topHits;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertNoFailures;
-import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertNoFailuresAndResponse;
-import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.lessThan;
-import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 @ESIntegTestCase.SuiteScopeTestCase
 public class TimeSeriesAggregationsIT extends AggregationIntegTestCase {
@@ -173,206 +146,6 @@ public class TimeSeriesAggregationsIT extends AggregationIntegTestCase {
             docs.add(prepareIndex("index" + findIndex(timestamp)).setOpType(DocWriteRequest.OpType.CREATE).setSource(docSource));
         }
         indexRandom(true, false, docs);
-    }
-
-    public void testStandAloneTimeSeriesAgg() {
-        assertNoFailuresAndResponse(prepareSearch("index").setSize(0).addAggregation(timeSeries("by_ts")), response -> {
-            Aggregations aggregations = response.getAggregations();
-            assertNotNull(aggregations);
-            InternalTimeSeries timeSeries = aggregations.get("by_ts");
-            assertThat(
-                timeSeries.getBuckets().stream().map(MultiBucketsAggregation.Bucket::getKey).collect(Collectors.toSet()),
-                equalTo(data.keySet())
-            );
-            for (InternalTimeSeries.Bucket bucket : timeSeries.getBuckets()) {
-                @SuppressWarnings("unchecked")
-                Map<String, String> key = (Map<String, String>) bucket.getKey();
-                assertThat((long) data.get(key).size(), equalTo(bucket.getDocCount()));
-            }
-        });
-    }
-
-    public void testTimeSeriesGroupedByADimension() {
-        String groupBy = "dim_" + randomIntBetween(0, numberOfDimensions - 1);
-        assertNoFailuresAndResponse(
-            prepareSearch("index").setSize(0)
-                .addAggregation(
-                    terms("by_dim").field(groupBy)
-                        .size(data.size())
-                        .collectMode(randomFrom(Aggregator.SubAggCollectionMode.values()))
-                        .subAggregation(timeSeries("by_ts"))
-                ),
-            response -> {
-                Aggregations aggregations = response.getAggregations();
-                assertNotNull(aggregations);
-                Terms terms = aggregations.get("by_dim");
-                Set<Map<String, String>> keys = new HashSet<>();
-                for (Terms.Bucket term : terms.getBuckets()) {
-                    InternalTimeSeries timeSeries = term.getAggregations().get("by_ts");
-                    for (InternalTimeSeries.Bucket bucket : timeSeries.getBuckets()) {
-                        @SuppressWarnings("unchecked")
-                        Map<String, String> key = (Map<String, String>) bucket.getKey();
-                        assertThat((long) data.get(key).size(), equalTo(bucket.getDocCount()));
-                        assertTrue("key is not unique", keys.add(key));
-                        assertThat(
-                            "time series doesn't contain dimensions we grouped by",
-                            key.get(groupBy),
-                            equalTo(term.getKeyAsString())
-                        );
-                    }
-                }
-                assertThat(keys, equalTo(data.keySet()));
-            }
-        );
-    }
-
-    public void testTimeSeriesGroupedByDateHistogram() {
-        DateHistogramInterval fixedInterval = DateHistogramInterval.days(randomIntBetween(10, 100));
-        assertNoFailuresAndResponse(
-            prepareSearch("index").setSize(0)
-                .addAggregation(
-                    dateHistogram("by_time").field("@timestamp")
-                        .fixedInterval(fixedInterval)
-                        .subAggregation(timeSeries("by_ts").subAggregation(stats("timestamp").field("@timestamp")))
-                ),
-            response -> {
-                Aggregations aggregations = response.getAggregations();
-                assertNotNull(aggregations);
-                Histogram histogram = aggregations.get("by_time");
-                Map<Map<String, String>, Long> keys = new HashMap<>();
-                for (Histogram.Bucket interval : histogram.getBuckets()) {
-                    long intervalStart = ((ZonedDateTime) interval.getKey()).toEpochSecond() * 1000;
-                    long intervalEnd = intervalStart + fixedInterval.estimateMillis();
-                    InternalTimeSeries timeSeries = interval.getAggregations().get("by_ts");
-                    for (InternalTimeSeries.Bucket bucket : timeSeries.getBuckets()) {
-                        @SuppressWarnings("unchecked")
-                        Map<String, String> key = (Map<String, String>) bucket.getKey();
-                        keys.compute(key, (k, v) -> (v == null ? 0 : v) + bucket.getDocCount());
-                        assertThat(bucket.getDocCount(), lessThanOrEqualTo((long) data.get(key).size()));
-                        Stats stats = bucket.getAggregations().get("timestamp");
-                        long minTimestamp = DateFieldMapper.DEFAULT_DATE_TIME_FORMATTER.parseMillis(stats.getMinAsString());
-                        long maxTimestamp = DateFieldMapper.DEFAULT_DATE_TIME_FORMATTER.parseMillis(stats.getMaxAsString());
-                        assertThat(minTimestamp, greaterThanOrEqualTo(intervalStart));
-                        assertThat(maxTimestamp, lessThan(intervalEnd));
-                    }
-                }
-                assertThat(keys.keySet(), equalTo(data.keySet()));
-                for (Map.Entry<Map<String, String>, Long> entry : keys.entrySet()) {
-                    assertThat(entry.getValue(), equalTo((long) data.get(entry.getKey()).size()));
-                }
-            }
-        );
-    }
-
-    public void testStandAloneTimeSeriesAggWithDimFilter() {
-        boolean include = randomBoolean();
-        int dim = randomIntBetween(0, numberOfDimensions - 1);
-        String val = randomFrom(dimensions[dim]);
-        QueryBuilder queryBuilder = QueryBuilders.termQuery("dim_" + dim, val);
-        if (include == false) {
-            queryBuilder = QueryBuilders.boolQuery().mustNot(queryBuilder);
-        }
-        assertNoFailuresAndResponse(
-            prepareSearch("index").setQuery(queryBuilder).setSize(0).addAggregation(timeSeries("by_ts")),
-            response -> {
-                Aggregations aggregations = response.getAggregations();
-                assertNotNull(aggregations);
-                InternalTimeSeries timeSeries = aggregations.get("by_ts");
-                Map<Map<String, String>, Map<Long, Map<String, Double>>> filteredData = dataFilteredByDimension("dim_" + dim, val, include);
-                assertThat(
-                    timeSeries.getBuckets().stream().map(MultiBucketsAggregation.Bucket::getKey).collect(Collectors.toSet()),
-                    equalTo(filteredData.keySet())
-                );
-                for (InternalTimeSeries.Bucket bucket : timeSeries.getBuckets()) {
-                    @SuppressWarnings("unchecked")
-                    Map<String, String> key = (Map<String, String>) bucket.getKey();
-                    assertThat(bucket.getDocCount(), equalTo((long) filteredData.get(key).size()));
-                }
-            }
-        );
-    }
-
-    public void testStandAloneTimeSeriesAggWithGlobalAggregation() {
-        boolean include = randomBoolean();
-        int dim = randomIntBetween(0, numberOfDimensions - 1);
-        int metric = randomIntBetween(0, numberOfMetrics - 1);
-        String val = randomFrom(dimensions[dim]);
-        QueryBuilder queryBuilder = QueryBuilders.termQuery("dim_" + dim, val);
-        if (include == false) {
-            queryBuilder = QueryBuilders.boolQuery().mustNot(queryBuilder);
-        }
-        assertNoFailuresAndResponse(
-            prepareSearch("index").setQuery(queryBuilder)
-                .setSize(0)
-                .addAggregation(timeSeries("by_ts").subAggregation(sum("filter_sum").field("metric_" + metric)))
-                .addAggregation(global("everything").subAggregation(sum("all_sum").field("metric_" + metric)))
-                .addAggregation(PipelineAggregatorBuilders.sumBucket("total_filter_sum", "by_ts>filter_sum")),
-            response -> {
-                Aggregations aggregations = response.getAggregations();
-                assertNotNull(aggregations);
-                InternalTimeSeries timeSeries = aggregations.get("by_ts");
-                Map<Map<String, String>, Map<Long, Map<String, Double>>> filteredData = dataFilteredByDimension("dim_" + dim, val, include);
-                assertThat(
-                    timeSeries.getBuckets().stream().map(MultiBucketsAggregation.Bucket::getKey).collect(Collectors.toSet()),
-                    equalTo(filteredData.keySet())
-                );
-                for (InternalTimeSeries.Bucket bucket : timeSeries.getBuckets()) {
-                    @SuppressWarnings("unchecked")
-                    Map<String, String> key = (Map<String, String>) bucket.getKey();
-                    assertThat(bucket.getDocCount(), equalTo((long) filteredData.get(key).size()));
-                }
-                SimpleValue obj = aggregations.get("total_filter_sum");
-                assertThat(obj.value(), closeTo(sumByMetric(filteredData, "metric_" + metric), obj.value() * 0.0001));
-
-                Global global = aggregations.get("everything");
-                Sum allSum = global.getAggregations().get("all_sum");
-                assertThat(allSum.value(), closeTo(sumByMetric(data, "metric_" + metric), allSum.value() * 0.0001));
-            }
-        );
-
-        ElasticsearchException e = expectThrows(
-            ElasticsearchException.class,
-            () -> prepareSearch("index").setQuery(QueryBuilders.termQuery("dim_" + dim, val))
-                .setSize(0)
-                .addAggregation(global("everything").subAggregation(timeSeries("by_ts")))
-                .get()
-        );
-        assertThat(e.getRootCause().getMessage(), containsString("Time series aggregations cannot be used inside global aggregation."));
-    }
-
-    public void testStandAloneTimeSeriesAggWithMetricFilter() {
-        boolean above = randomBoolean();
-        int metric = randomIntBetween(0, numberOfMetrics - 1);
-        double val = randomDoubleBetween(0, 100000, true);
-        RangeQueryBuilder queryBuilder = QueryBuilders.rangeQuery("metric_" + metric);
-        if (above) {
-            queryBuilder.gt(val);
-        } else {
-            queryBuilder.lte(val);
-        }
-        assertNoFailuresAndResponse(
-            prepareSearch("index").setQuery(queryBuilder).setSize(0).addAggregation(timeSeries("by_ts")),
-            response -> {
-                Aggregations aggregations = response.getAggregations();
-                assertNotNull(aggregations);
-                InternalTimeSeries timeSeries = aggregations.get("by_ts");
-                Map<Map<String, String>, Map<Long, Map<String, Double>>> filteredData = dataFilteredByMetric(
-                    data,
-                    "metric_" + metric,
-                    val,
-                    above
-                );
-                assertThat(
-                    timeSeries.getBuckets().stream().map(MultiBucketsAggregation.Bucket::getKey).collect(Collectors.toSet()),
-                    equalTo(filteredData.keySet())
-                );
-                for (InternalTimeSeries.Bucket bucket : timeSeries.getBuckets()) {
-                    @SuppressWarnings("unchecked")
-                    Map<String, String> key = (Map<String, String>) bucket.getKey();
-                    assertThat(bucket.getDocCount(), equalTo((long) filteredData.get(key).size()));
-                }
-            }
-        );
     }
 
     public void testRetrievingHits() {

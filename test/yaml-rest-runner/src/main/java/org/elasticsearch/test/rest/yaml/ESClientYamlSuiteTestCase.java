a18ee6db4bd4fb7d7765cb4b84a77e277d82c8b3
Jan Kuipers <jan.kuipers@elastic.co>
2023-11-27 17:17:28 +0100

    Fix bug ID in test muting

--- test/yaml-rest-runner/src/main/java/org/elasticsearch/test/rest/yaml/ESClientYamlSuiteTestCase.java
+++ test/yaml-rest-runner/src/main/java/org/elasticsearch/test/rest/yaml/ESClientYamlSuiteTestCase.java
@@ -442,7 +442,7 @@ public abstract class ESClientYamlSuiteTestCase extends ESRestTestCase {
         return RequestOptions.DEFAULT;
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/99153")
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/102596")
     public void test() throws IOException {
         // skip test if it matches one of the blacklist globs
         for (BlacklistedPathPatternMatcher blacklistedPathMatcher : blacklistPathMatchers) {

0c31b4563519c65d847f94f131ec95ff44c26c65
Albert Zaharovits <albert.zaharovits@elastic.co>
2023-11-29 10:28:45 +0200

    Nit

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/jwt/JwtStringClaimValidator.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/jwt/JwtStringClaimValidator.java
@@ -33,7 +33,7 @@ import java.util.function.Predicate;
 public class JwtStringClaimValidator implements JwtFieldValidator {
 
     // Allows any non-null value for the sub claim
-    public static JwtStringClaimValidator ALLOW_ALL_SUBJECTS = new JwtStringClaimValidator("sub", true, List.of(), List.of("*"));
+    public static final JwtStringClaimValidator ALLOW_ALL_SUBJECTS = new JwtStringClaimValidator("sub", true, List.of(), List.of("*"));
 
     private final String claimName;
     // Whether the claim should be a single string

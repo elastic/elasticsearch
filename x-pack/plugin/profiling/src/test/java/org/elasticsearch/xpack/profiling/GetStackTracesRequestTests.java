ba966fef62451f72e2149152a19d50574e4b61a6
Tim Rühsen <tim.ruhsen@elastic.co>
2023-11-29 14:54:31 +0100

    Improve request serialization test

--- x-pack/plugin/profiling/src/test/java/org/elasticsearch/xpack/profiling/GetStackTracesRequestTests.java
+++ x-pack/plugin/profiling/src/test/java/org/elasticsearch/xpack/profiling/GetStackTracesRequestTests.java
@@ -32,7 +32,11 @@ public class GetStackTracesRequestTests extends ESTestCase {
         Integer sampleSize = randomIntBetween(1, Integer.MAX_VALUE);
         Double requestedDuration = randomBoolean() ? randomDoubleBetween(0.001d, Double.MAX_VALUE, true) : null;
         Double awsCostFactor = randomBoolean() ? randomDoubleBetween(0.1d, 5.0d, true) : null;
+        Double customCO2PerKWH = randomBoolean() ? randomDoubleBetween(0.000001d, 0.001d, true) : null;
+        Double datacenterPUE = randomBoolean() ? randomDoubleBetween(1.0d, 3.0d, true) : null;
+        Double perCoreWattX86 = randomBoolean() ? randomDoubleBetween(0.01d, 20.0d, true) : null;
         Double perCoreWattARM64 = randomBoolean() ? randomDoubleBetween(0.01d, 20.0d, true) : null;
+        Double customCostPerCoreHour = randomBoolean() ? randomDoubleBetween(0.001d, 1000.0d, true) : null;
         QueryBuilder query = randomBoolean() ? new BoolQueryBuilder() : null;
 
         GetStackTracesRequest request = new GetStackTracesRequest(
@@ -42,11 +46,11 @@ public class GetStackTracesRequestTests extends ESTestCase {
             query,
             null,
             null,
-            null,
-            null,
-            null,
+            customCO2PerKWH,
+            datacenterPUE,
+            perCoreWattX86,
             perCoreWattARM64,
-            null
+            customCostPerCoreHour
         );
         try (BytesStreamOutput out = new BytesStreamOutput()) {
             request.writeTo(out);

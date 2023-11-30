e69fa080d6d85ad9e924fdff613fcb16be522cf8
Artem Prigoda <artem.prigoda@elastic.co>
2023-11-30 16:33:10 +0100

    Add MissedIndicesUpdateException

---
+++ server/src/main/java/org/elasticsearch/indices/MissedIndicesUpdateException.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+package org.elasticsearch.indices;
+
+import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.common.io.stream.StreamInput;
+
+import java.io.IOException;
+
+public class MissedIndicesUpdateException extends ElasticsearchException {
+
+    public MissedIndicesUpdateException(String message) {
+        super(message);
+    }
+
+    public MissedIndicesUpdateException(StreamInput in) throws IOException {
+        super(in);
+    }
+}

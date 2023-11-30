08a7d005fcb2c864f1037d306059fd14d23ebf78
Lorenzo Dematte <lorenzo.dematte@elastic.co>
2023-11-30 12:14:25 +0100

    Another type change

--- x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/ApiKeyBackwardsCompatibilityIT.java
+++ x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/ApiKeyBackwardsCompatibilityIT.java
@@ -11,6 +11,7 @@ import org.apache.http.HttpHost;
 import org.apache.http.client.methods.HttpGet;
 import org.elasticsearch.Build;
 import org.elasticsearch.TransportVersion;
+import org.elasticsearch.TransportVersions;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.Response;
@@ -309,10 +310,10 @@ public class ApiKeyBackwardsCompatibilityIT extends AbstractUpgradeTestCase {
         TransportVersion transportVersion = getTransportVersionWithFallback(
             nodeVersionString,
             nodeDetails.get("transport_version"),
-            () -> null
+            () -> TransportVersions.ZERO
         );
 
-        if (transportVersion == null) {
+        if (transportVersion.equals(TransportVersions.ZERO)) {
             // In cases where we were not able to find a TransportVersion, a pre-8.8.0 node answered about a newer (upgraded) node.
             // In that case, the node will be current (upgraded), and remote indices are supported for sure.
             var nodeIsCurrent = nodeVersionString.equals(Build.current().version());

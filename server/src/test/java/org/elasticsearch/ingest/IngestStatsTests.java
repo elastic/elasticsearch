87877bd458fcf40e72b193c4f6f2234273a5463a
Joe Gallo <joegallo@gmail.com>
2023-11-30 13:22:27 -0500

    Add a test for mixed stats

--- server/src/test/java/org/elasticsearch/ingest/IngestStatsTests.java
+++ server/src/test/java/org/elasticsearch/ingest/IngestStatsTests.java
@@ -136,6 +136,23 @@ public class IngestStatsTests extends ESTestCase {
         );
     }
 
+    public void testProcessorStatsMergeHeterogeneous() {
+        // if a pipeline has heterogeneous *non-zero* stats, then we defer to the one with a smaller total ingest count
+
+        var first = Map.of(
+            "pipeline-1",
+            List.of(
+                new IngestStats.ProcessorStat("name-1", "type-1", new IngestStats.Stats(randomLongBetween(1, 100), 0, 0, 0)),
+                new IngestStats.ProcessorStat("name-2", "type-2", new IngestStats.Stats(randomLongBetween(1, 100), 0, 0, 0))
+            )
+        );
+        var expected = List.of(new IngestStats.ProcessorStat("name-1", "type-1", new IngestStats.Stats(1, 0, 0, 0)));
+        var second = Map.of("pipeline-1", expected);
+
+        assertEquals(second, IngestStats.merge(first, second));
+        assertSame(expected, IngestStats.merge(second, first).get("pipeline-1"));
+    }
+
     private static List<IngestStats.ProcessorStat> expectedPipelineProcessorStats(
         List<IngestStats.ProcessorStat> first,
         List<IngestStats.ProcessorStat> second

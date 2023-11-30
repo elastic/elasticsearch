4253403dc912055669583780b391e25a65eefded
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2023-11-30 12:08:58 -0500

    Fixing failing test case

--- server/src/main/java/org/elasticsearch/action/search/SearchPhaseController.java
+++ server/src/main/java/org/elasticsearch/action/search/SearchPhaseController.java
@@ -241,8 +241,7 @@ public final class SearchPhaseController {
             final TopFieldGroups[] shardTopDocs = results.toArray(new TopFieldGroups[numShards]);
             mergedTopDocs = TopFieldGroups.merge(sort, from, topN, shardTopDocs, false);
         } else if (topDocs instanceof TopFieldDocs firstTopDocs) {
-            checkSameSortTypes(results);
-            final Sort sort = new Sort(firstTopDocs.fields);
+            final Sort sort = checkSameSortTypes(results, firstTopDocs.fields);
             final TopFieldDocs[] shardTopDocs = results.toArray(new TopFieldDocs[numShards]);
             mergedTopDocs = TopDocs.merge(sort, from, topN, shardTopDocs);
         } else {
@@ -252,8 +251,9 @@ public final class SearchPhaseController {
         return mergedTopDocs;
     }
 
-    private static void checkSameSortTypes(Collection<TopDocs> results) {
-        if (results.size() < 2) return;
+    private static Sort checkSameSortTypes(Collection<TopDocs> results, SortField[] firstSortFields) {
+        Sort sort = new Sort(firstSortFields);
+        if (results.size() < 2) return sort;
 
         SortField.Type[] firstTypes = null;
         boolean isFirstResult = true;
@@ -264,12 +264,13 @@ public final class SearchPhaseController {
             }
             SortField[] curSortFields = ((TopFieldDocs) topDocs).fields;
             if (isFirstResult) {
+                sort = new Sort(curSortFields);
                 firstTypes = new SortField.Type[curSortFields.length];
                 for (int i = 0; i < curSortFields.length; i++) {
                     firstTypes[i] = getType(curSortFields[i]);
                     if (firstTypes[i] == SortField.Type.CUSTOM) {
                         // for custom types that we can't resolve, we can't do the check
-                        return;
+                        return sort;
                     }
                 }
                 isFirstResult = false;
@@ -279,7 +280,7 @@ public final class SearchPhaseController {
                     if (curType != firstTypes[i]) {
                         if (curType == SortField.Type.CUSTOM) {
                             // for custom types that we can't resolve, we can't do the check
-                            return;
+                            return sort;
                         }
                         throw new IllegalArgumentException(
                             "Can't sort on field ["
@@ -294,6 +295,7 @@ public final class SearchPhaseController {
                 }
             }
         }
+        return sort;
     }
 
     private static SortField.Type getType(SortField sortField) {

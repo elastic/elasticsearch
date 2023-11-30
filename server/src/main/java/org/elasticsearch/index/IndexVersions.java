83eae8906a7986552654a100313c81874ee40615
ChrisHegarty <chegar999@gmail.com>
2023-11-30 08:35:38 +0000

    Add IndexVersion constant for Lucene 9.10

--- server/src/main/java/org/elasticsearch/index/IndexVersions.java
+++ server/src/main/java/org/elasticsearch/index/IndexVersions.java
@@ -92,6 +92,7 @@ public class IndexVersions {
     public static final IndexVersion ES_VERSION_8_12 = def(8_500_004, Version.LUCENE_9_8_0);
 
     public static final IndexVersion UPGRADE_TO_LUCENE_9_9 = def(8_500_010, Version.LUCENE_9_9_0);
+    public static final IndexVersion UPGRADE_TO_LUCENE_9_10 = def(8_500_099, Version.LUCENE_9_10_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

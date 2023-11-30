12906cd8f33dae7c8d1169b3aa6d55f3f4bfe065
ChrisHegarty <chegar999@gmail.com>
2023-11-30 09:21:53 +0000

    Fix IndexVersion to use Lucene 9.9

--- server/src/main/java/org/elasticsearch/index/IndexVersions.java
+++ server/src/main/java/org/elasticsearch/index/IndexVersions.java
@@ -89,9 +89,7 @@ public class IndexVersions {
     public static final IndexVersion NEW_SPARSE_VECTOR = def(8_500_001, Version.LUCENE_9_7_0);
     public static final IndexVersion SPARSE_VECTOR_IN_FIELD_NAMES_SUPPORT = def(8_500_002, Version.LUCENE_9_7_0);
     public static final IndexVersion UPGRADE_LUCENE_9_8 = def(8_500_003, Version.LUCENE_9_8_0);
-    public static final IndexVersion ES_VERSION_8_12 = def(8_500_004, Version.LUCENE_9_8_0);
-
-    public static final IndexVersion UPGRADE_TO_LUCENE_9_9 = def(8_500_010, Version.LUCENE_9_9_0);
+    public static final IndexVersion UPGRADE_LUCENE_9_9 = def(8_500_004, Version.LUCENE_9_9_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

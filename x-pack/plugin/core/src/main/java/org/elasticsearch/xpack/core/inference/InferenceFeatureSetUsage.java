c963c22d6c4166e8846e58875f2a7586e9917379
Jan Kuipers <jan.kuipers@elastic.co>
2023-12-01 15:47:16 +0100

    Real inference model stats

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/inference/InferenceFeatureSetUsage.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/inference/InferenceFeatureSetUsage.java
@@ -11,41 +11,67 @@ import org.elasticsearch.TransportVersion;
 import org.elasticsearch.TransportVersions;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.inference.TaskType;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xpack.core.XPackFeatureSet;
 import org.elasticsearch.xpack.core.XPackField;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class InferenceFeatureSetUsage extends XPackFeatureSet.Usage {
 
-    public static final String MODELS_FIELD = "models";
+    public static class ModelStats {
 
-    private final Map<String, Object> stats;
+        private final String service;
+        private final TaskType taskType;
+        private long count;
 
-    public InferenceFeatureSetUsage(boolean available, boolean enabled, Map<String, Object> stats) {
+        public ModelStats(String service, TaskType taskType) {
+            this.service = service;
+            this.taskType = taskType;
+            this.count = 0;
+        }
+
+        public void add() {
+            count++;
+        }
+
+        Map<String, Object> asMap() {
+            return Map.of(
+                "service", service,
+                "task_type", taskType.name(),
+                "count", count
+            );
+        }
+    }
+
+    private final Map<String, Object> modelStats;
+
+    public InferenceFeatureSetUsage(boolean available, boolean enabled, Collection<ModelStats> modelStats) {
         super(XPackField.INFERENCE, available, enabled);
-        this.stats = stats;
+        this.modelStats = Map.of("models", modelStats.stream().map(ModelStats::asMap).collect(Collectors.toList()));
     }
 
     public InferenceFeatureSetUsage(StreamInput input) throws IOException {
         super(input);
-        this.stats = input.readMap();
+        this.modelStats = input.readMap();
     }
 
     @Override
     protected void innerXContent(XContentBuilder builder, Params params) throws IOException {
         super.innerXContent(builder, params);
         if (enabled) {
-            builder.field(MODELS_FIELD, stats);
+            builder.mapContents(modelStats);
         }
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         super.writeTo(out);
-        out.writeGenericMap(stats);
+        out.writeGenericMap(modelStats);
     }
 
     @Override
--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/action/TransportInferenceUsageAction.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/action/TransportInferenceUsageAction.java
@@ -9,10 +9,13 @@ package org.elasticsearch.xpack.inference.action;
 
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.ActionFilters;
+import org.elasticsearch.client.internal.Client;
+import org.elasticsearch.client.internal.OriginSettingClient;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.inference.ModelConfigurations;
 import org.elasticsearch.inference.TaskType;
 import org.elasticsearch.protocol.xpack.XPackUsageRequest;
 import org.elasticsearch.tasks.Task;
@@ -22,10 +25,16 @@ import org.elasticsearch.xpack.core.action.XPackUsageFeatureAction;
 import org.elasticsearch.xpack.core.action.XPackUsageFeatureResponse;
 import org.elasticsearch.xpack.core.action.XPackUsageFeatureTransportAction;
 import org.elasticsearch.xpack.core.inference.InferenceFeatureSetUsage;
+import org.elasticsearch.xpack.core.inference.action.GetInferenceModelAction;
 
 import java.util.Map;
+import java.util.TreeMap;
+
+import static org.elasticsearch.xpack.core.ClientHelper.ML_ORIGIN;
 
 public class TransportInferenceUsageAction extends XPackUsageFeatureTransportAction {
+
+    private final Client client;
 
     @Inject
     public TransportInferenceUsageAction(
@@ -33,17 +42,30 @@ public class TransportInferenceUsageAction extends XPackUsageFeatureTransportAct
         ClusterService clusterService,
         ThreadPool threadPool,
         ActionFilters actionFilters,
-        IndexNameExpressionResolver indexNameExpressionResolver
+        IndexNameExpressionResolver indexNameExpressionResolver,
+        Client client
     ) {
         super(XPackUsageFeatureAction.INFERENCE.name(), transportService, clusterService, threadPool, actionFilters, indexNameExpressionResolver);
+        this.client = new OriginSettingClient(client, ML_ORIGIN);
     }
 
     @Override
     protected void masterOperation(
         Task task, XPackUsageRequest request, ClusterState state, ActionListener<XPackUsageFeatureResponse> listener
     ) throws Exception {
-        Map<String, Object> stats = Map.of("my-model-id", Map.of(TaskType.SPARSE_EMBEDDING.name(), 25L));
-        InferenceFeatureSetUsage usage = new InferenceFeatureSetUsage(true, true, stats);
-        listener.onResponse(new XPackUsageFeatureResponse(usage));
+        GetInferenceModelAction.Request getInferenceModelAction = new GetInferenceModelAction.Request("_all", TaskType.ANY);
+        client.execute(GetInferenceModelAction.INSTANCE,  getInferenceModelAction, ActionListener.wrap(
+            response -> {
+                Map<String, InferenceFeatureSetUsage.ModelStats> stats = new TreeMap<>();
+                for (ModelConfigurations model : response.getModels()) {
+                    String statKey = model.getService() + ":" + model.getTaskType().name();
+                    InferenceFeatureSetUsage.ModelStats stat = stats.computeIfAbsent(statKey,
+                        key -> new InferenceFeatureSetUsage.ModelStats(model.getService(), model.getTaskType()));
+                    stat.add();
+                }
+                InferenceFeatureSetUsage usage = new InferenceFeatureSetUsage(true, true, stats.values());
+                listener.onResponse(new XPackUsageFeatureResponse(usage));
+            },
+            listener::onFailure));
     }
 }

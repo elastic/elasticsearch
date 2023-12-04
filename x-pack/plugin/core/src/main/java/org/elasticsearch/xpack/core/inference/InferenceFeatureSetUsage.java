afd16b64b741b85fc09a84f8ef4385a3af16b975
Jan Kuipers <jan.kuipers@elastic.co>
2023-12-04 12:00:30 +0100

    Replace map by ToXContentObject/Writeable

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/inference/InferenceFeatureSetUsage.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/inference/InferenceFeatureSetUsage.java
@@ -11,19 +11,19 @@ import org.elasticsearch.TransportVersion;
 import org.elasticsearch.TransportVersions;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.inference.TaskType;
+import org.elasticsearch.xcontent.ToXContentObject;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xpack.core.XPackFeatureSet;
 import org.elasticsearch.xpack.core.XPackField;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class InferenceFeatureSetUsage extends XPackFeatureSet.Usage {
 
-    public static class ModelStats {
+    public static class ModelStats implements ToXContentObject, Writeable {
 
         private final String service;
         private final TaskType taskType;
@@ -35,39 +35,56 @@ public class InferenceFeatureSetUsage extends XPackFeatureSet.Usage {
             this.count = 0;
         }
 
+        public ModelStats(StreamInput in) throws IOException {
+            this.service = in.readString();
+            this.taskType = in.readEnum(TaskType.class);
+            this.count = in.readLong();
+        }
+
         public void add() {
             count++;
         }
 
-        Map<String, Object> asMap() {
-            return Map.of("service", service, "task_type", taskType.name(), "count", count);
+        @Override
+        public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+            builder.startObject();
+            builder.field("service", service);
+            builder.field("task_type", taskType.name());
+            builder.field("count", count);
+            builder.endObject();
+            return builder;
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) throws IOException {
+            out.writeString(service);
+            out.writeEnum(taskType);
+            out.writeLong(count);
         }
     }
 
-    private final Map<String, Object> modelStats;
+    private final Collection<ModelStats> modelStats;
 
     public InferenceFeatureSetUsage(Collection<ModelStats> modelStats) {
         super(XPackField.INFERENCE, true, true);
-        this.modelStats = Map.of("models", modelStats.stream().map(ModelStats::asMap).collect(Collectors.toList()));
+        this.modelStats = modelStats;
     }
 
-    public InferenceFeatureSetUsage(StreamInput input) throws IOException {
-        super(input);
-        this.modelStats = input.readMap();
+    public InferenceFeatureSetUsage(StreamInput in) throws IOException {
+        super(in);
+        this.modelStats = in.readCollectionAsList(ModelStats::new);
     }
 
     @Override
     protected void innerXContent(XContentBuilder builder, Params params) throws IOException {
         super.innerXContent(builder, params);
-        if (enabled) {
-            builder.mapContents(modelStats);
-        }
+        builder.array("models", modelStats.toArray());
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         super.writeTo(out);
-        out.writeGenericMap(modelStats);
+        out.writeCollection(modelStats);
     }
 
     @Override

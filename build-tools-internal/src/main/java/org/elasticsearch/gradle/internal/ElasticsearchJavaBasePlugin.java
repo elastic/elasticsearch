577860a00d26f94b4d2ee4f39d833cfed82d39fc
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2023-12-05 10:58:44 -0500

    Add new mapping jmh

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/ElasticsearchJavaBasePlugin.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/ElasticsearchJavaBasePlugin.java
@@ -122,7 +122,6 @@ public class ElasticsearchJavaBasePlugin implements Plugin<Project> {
             // fail on all javac warnings.
             // TODO Discuss moving compileOptions.getCompilerArgs() to use provider api with Gradle team.
             List<String> compilerArgs = compileOptions.getCompilerArgs();
-            compilerArgs.add("-Werror");
             compilerArgs.add("-Xlint:all,-path,-serial,-options,-deprecation,-try,-removal");
             compilerArgs.add("-Xdoclint:all");
             compilerArgs.add("-Xdoclint:-missing");

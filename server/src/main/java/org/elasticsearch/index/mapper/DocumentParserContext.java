c7fcdd7b58fa2e5d58a5a6615bb761d44987b0e9
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2023-12-06 07:25:58 -0500

    Ensure dynamicMapping updates are handled in insertion order

--- server/src/main/java/org/elasticsearch/index/mapper/DocumentParserContext.java
+++ server/src/main/java/org/elasticsearch/index/mapper/DocumentParserContext.java
@@ -22,8 +22,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -166,9 +166,9 @@ public abstract class DocumentParserContext {
             mappingParserContext,
             source,
             new HashSet<>(),
-            new HashMap<>(),
+            new LinkedHashMap<>(),
             new HashSet<>(),
-            new HashMap<>(),
+            new LinkedHashMap<>(),
             new ArrayList<>(),
             null,
             null,

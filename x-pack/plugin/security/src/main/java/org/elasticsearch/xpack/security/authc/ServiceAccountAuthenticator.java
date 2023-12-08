b5153768a1813f5383f3d0b2d30658261aa0f5bf
Slobodan Adamovic <slobodan.adamovic@elastic.co>
2023-12-08 14:52:07 +0100

    This commit adds metrics for recording successful and failed authentications for service accounts. Exposed metrics are:
    - `es.security.authc.service_account.success.count`
    - `es.security.authc.service_account.failures.count`
    
    Each of the metric is exposed at node level and includes
    additional service account information with these attributes:
    
    - `es.security.service_account_id` - unique service account identifier (principal)
    - `es.security.service_account_token_name`
    - `es.security.service_account_token_source` - `index` or `file`
    - `es.security.service_account_authc_failure_reason`
    
    Note: This change builds on top of abstractions introduced in #103178.
    
    Relates: ES-7469

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/ServiceAccountAuthenticator.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/ServiceAccountAuthenticator.java
@@ -11,19 +11,36 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.common.settings.SecureString;
+import org.elasticsearch.telemetry.metric.MeterRegistry;
 import org.elasticsearch.xpack.core.security.authc.Authentication;
 import org.elasticsearch.xpack.core.security.authc.AuthenticationResult;
 import org.elasticsearch.xpack.core.security.authc.AuthenticationToken;
+import org.elasticsearch.xpack.core.security.authc.service.ServiceAccountSettings;
 import org.elasticsearch.xpack.security.authc.service.ServiceAccountService;
 import org.elasticsearch.xpack.security.authc.service.ServiceAccountToken;
 
-class ServiceAccountAuthenticator implements Authenticator {
+import java.util.HashMap;
+import java.util.Map;
+
+class ServiceAccountAuthenticator extends MetricsRecordingAuthenticator implements Authenticator {
+
+    public static final String METRIC_SUCCESS_COUNT = "es.security.authc.service_account.success.count";
+    public static final String METRIC_FAILURES_COUNT = "es.security.authc.service_account.failures.count";
+
+    public static final String ATTRIBUTE_SERVICE_ACCOUNT_ID = "es.security.service_account_id";
+    public static final String ATTRIBUTE_SERVICE_ACCOUNT_TOKEN_NAME = "es.security.service_account_token_name";
+    public static final String ATTRIBUTE_SERVICE_ACCOUNT_TOKEN_SOURCE = "es.security.service_account_token_source";
+    public static final String ATTRIBUTE_AUTHC_FAILURE_REASON = "es.security.service_account_authc_failure_reason";
 
     private static final Logger logger = LogManager.getLogger(ServiceAccountAuthenticator.class);
     private final ServiceAccountService serviceAccountService;
     private final String nodeName;
 
-    ServiceAccountAuthenticator(ServiceAccountService serviceAccountService, String nodeName) {
+    ServiceAccountAuthenticator(ServiceAccountService serviceAccountService, String nodeName, MeterRegistry meterRegistry) {
+        super(
+            meterRegistry.registerLongCounter(METRIC_SUCCESS_COUNT, "Number of successful service account authentications.", "count"),
+            meterRegistry.registerLongCounter(METRIC_FAILURES_COUNT, "Number of failed service account authentications.", "count")
+        );
         this.serviceAccountService = serviceAccountService;
         this.nodeName = nodeName;
     }
@@ -52,10 +69,33 @@ class ServiceAccountAuthenticator implements Authenticator {
         final ServiceAccountToken serviceAccountToken = (ServiceAccountToken) authenticationToken;
         serviceAccountService.authenticateToken(serviceAccountToken, nodeName, ActionListener.wrap(authentication -> {
             assert authentication != null : "service account authenticate should return either authentication or call onFailure";
+            recordSuccessfulAuthentication(buildSuccessAuthMetricAttributes(serviceAccountToken, authentication));
             listener.onResponse(AuthenticationResult.success(authentication));
         }, e -> {
             logger.debug(() -> "Failed to validate service account token for request [" + context.getRequest() + "]", e);
+            recordFailedAuthentication(buildFailedAuthMetricAttributes(serviceAccountToken, e.getMessage()));
             listener.onFailure(context.getRequest().exceptionProcessingRequest(e, serviceAccountToken));
         }));
+    }
+
+    private Map<String, Object> buildSuccessAuthMetricAttributes(ServiceAccountToken serviceAccountToken, Authentication authentication) {
+        return Map.ofEntries(
+            Map.entry(ATTRIBUTE_SERVICE_ACCOUNT_ID, serviceAccountToken.getAccountId().asPrincipal()),
+            Map.entry(ATTRIBUTE_SERVICE_ACCOUNT_TOKEN_NAME, serviceAccountToken.getTokenName()),
+            Map.entry(
+                ATTRIBUTE_SERVICE_ACCOUNT_TOKEN_SOURCE,
+                authentication.getEffectiveSubject().getMetadata().get(ServiceAccountSettings.TOKEN_SOURCE_FIELD)
+            )
+        );
+    }
+
+    private Map<String, Object> buildFailedAuthMetricAttributes(ServiceAccountToken serviceAccountToken, String failureReason) {
+        final Map<String, Object> attributes = new HashMap<>(3);
+        attributes.put(ATTRIBUTE_SERVICE_ACCOUNT_ID, serviceAccountToken.getAccountId().asPrincipal());
+        attributes.put(ATTRIBUTE_SERVICE_ACCOUNT_TOKEN_NAME, serviceAccountToken.getTokenName());
+        if (failureReason != null) {
+            attributes.put(ATTRIBUTE_AUTHC_FAILURE_REASON, failureReason);
+        }
+        return attributes;
     }
 }

efeec01f3ba8602bbad1b9adec442e27858db83f
Lorenzo Dematté <lorenzo.dematte@elastic.co>
2023-12-11 18:34:44 +0100

    Passing down cluster features to YAML test framework (#102921)
    - Cluster features passed down as a predicate via context
    - Removing the not-needed (unused) master node version

--- test/yaml-rest-runner/src/main/java/org/elasticsearch/test/rest/yaml/section/DoSection.java
+++ test/yaml-rest-runner/src/main/java/org/elasticsearch/test/rest/yaml/section/DoSection.java
@@ -19,7 +19,9 @@ import org.elasticsearch.common.ParsingException;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.logging.HeaderWarning;
 import org.elasticsearch.core.Tuple;
+import org.elasticsearch.core.UpdateForV9;
 import org.elasticsearch.rest.action.admin.indices.RestPutIndexTemplateAction;
+import org.elasticsearch.test.rest.RestTestLegacyFeatures;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestExecutionContext;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestResponse;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestResponseException;
@@ -364,10 +366,15 @@ public class DoSection implements ExecutableSection {
                 ? executionContext.getClientYamlTestCandidate().getTestPath()
                 : null;
 
-            if (executionContext.esVersion().onOrAfter(Version.V_8_0_1)
-                || (executionContext.esVersion().major == Version.V_7_17_0.major
-                    && executionContext.esVersion().onOrAfter(Version.V_7_17_2))) {
-                // #84038 and #84089 mean that this assertion fails when running against some 8.0.x and 7.17.x released versions
+            // #84038 and #84089 mean that this assertion fails when running against < 7.17.2 and 8.0.0 released versions
+            // This is really difficult to express just with features, so I will break it down into 2 parts: version check for v7,
+            // and feature check for v8. This way the version check can be removed once we move to v9
+            @UpdateForV9
+            var fixedInV7 = executionContext.esVersion().major == Version.V_7_17_0.major
+                && executionContext.esVersion().onOrAfter(Version.V_7_17_2);
+            var fixedProductionHeader = fixedInV7
+                || executionContext.clusterHasFeature(RestTestLegacyFeatures.REST_ELASTIC_PRODUCT_HEADER_PRESENT.id());
+            if (fixedProductionHeader) {
                 checkElasticProductHeader(response.getHeaders("X-elastic-product"));
             }
             checkWarningHeaders(response.getWarningHeaders(), testPath);

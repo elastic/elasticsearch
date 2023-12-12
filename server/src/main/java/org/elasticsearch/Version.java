0cbe52bdcddda836dbdb85d9ba4bf7cc5b22abe6
Brian Seeders <brian.seeders@elastic.co>
2023-12-12 10:53:17 -0500

    Bump versions after 8.11.3 release

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -158,7 +158,8 @@ public class Version implements VersionId<Version>, ToXContentFragment {
     public static final Version V_8_11_1 = new Version(8_11_01_99);
     public static final Version V_8_11_2 = new Version(8_11_02_99);
     public static final Version V_8_11_3 = new Version(8_11_03_99);
-    public static final Version CURRENT = V_8_11_3;
+    public static final Version V_8_11_4 = new Version(8_11_04_99);
+    public static final Version CURRENT = V_8_11_4;
 
     private static final NavigableMap<Integer, Version> VERSION_IDS;
     private static final Map<String, Version> VERSION_STRINGS;

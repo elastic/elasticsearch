b849b108a0e3780f2d884f7e09c69396670bf023
Rene Groeschke <rene@elastic.co>
2023-12-12 18:22:35 +0100

    Revert "More cleanup"
    This reverts commit 9e9534b51f18a50e50f951b4253b19206dcb3c16.

--- test/test-clusters/src/main/java/org/elasticsearch/test/cluster/local/LocalSpecBuilder.java
+++ test/test-clusters/src/main/java/org/elasticsearch/test/cluster/local/LocalSpecBuilder.java
@@ -113,7 +113,7 @@ interface LocalSpecBuilder<T extends LocalSpecBuilder<?>> {
     T configFile(String fileName, Resource configFile);
 
     /**
-     * Adds a file to the node config directory computed by the given supplier
+     * Adds a file to the node config directory
      */
     T configFile(String fileName, Supplier<Resource> configFile);
 

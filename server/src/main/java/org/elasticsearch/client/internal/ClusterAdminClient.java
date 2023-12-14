40c11ce9639055298fd33946bfcba0754a320dc6
David Turner <david.turner@elastic.co>
2023-12-14 14:25:48 +0000

    Clean up dangling index actions
    Removes the unnecessary `ActionType` subclasses and the helper methods
    in `ClusterAdminClient` that are only used in a single test suite.

--- server/src/main/java/org/elasticsearch/client/internal/ClusterAdminClient.java
+++ server/src/main/java/org/elasticsearch/client/internal/ClusterAdminClient.java
@@ -91,10 +91,6 @@ import org.elasticsearch.action.admin.cluster.storedscripts.PutStoredScriptReque
 import org.elasticsearch.action.admin.cluster.tasks.PendingClusterTasksRequest;
 import org.elasticsearch.action.admin.cluster.tasks.PendingClusterTasksRequestBuilder;
 import org.elasticsearch.action.admin.cluster.tasks.PendingClusterTasksResponse;
-import org.elasticsearch.action.admin.indices.dangling.delete.DeleteDanglingIndexRequest;
-import org.elasticsearch.action.admin.indices.dangling.import_index.ImportDanglingIndexRequest;
-import org.elasticsearch.action.admin.indices.dangling.list.ListDanglingIndicesRequest;
-import org.elasticsearch.action.admin.indices.dangling.list.ListDanglingIndicesResponse;
 import org.elasticsearch.action.ingest.DeletePipelineRequest;
 import org.elasticsearch.action.ingest.DeletePipelineRequestBuilder;
 import org.elasticsearch.action.ingest.GetPipelineRequest;
@@ -582,34 +578,4 @@ public interface ClusterAdminClient extends ElasticsearchClient {
      * Get a script from the cluster state
      */
     void getStoredScript(GetStoredScriptRequest request, ActionListener<GetStoredScriptResponse> listener);
-
-    /**
-     * List dangling indices on all nodes.
-     */
-    void listDanglingIndices(ListDanglingIndicesRequest request, ActionListener<ListDanglingIndicesResponse> listener);
-
-    /**
-     * List dangling indices on all nodes.
-     */
-    ActionFuture<ListDanglingIndicesResponse> listDanglingIndices(ListDanglingIndicesRequest request);
-
-    /**
-     * Restore specified dangling indices.
-     */
-    void importDanglingIndex(ImportDanglingIndexRequest request, ActionListener<AcknowledgedResponse> listener);
-
-    /**
-     * Restore specified dangling indices.
-     */
-    ActionFuture<AcknowledgedResponse> importDanglingIndex(ImportDanglingIndexRequest request);
-
-    /**
-     * Delete specified dangling indices.
-     */
-    void deleteDanglingIndex(DeleteDanglingIndexRequest request, ActionListener<AcknowledgedResponse> listener);
-
-    /**
-     * Delete specified dangling indices.
-     */
-    ActionFuture<AcknowledgedResponse> deleteDanglingIndex(DeleteDanglingIndexRequest request);
 }

ceba9c3702a0b3dfca9fc8d1b9b3c66dde8a21b6
Andrei Dan <andrei.dan@elastic.co>
2023-12-19 14:32:19 +0000

    Fix multi-cluster tests

--- test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
@@ -1950,6 +1950,7 @@ public abstract class ESRestTestCase extends ESTestCase {
         switch (name) {
             case ".watches":
             case "security_audit_log":
+            case ".slm-history":
             case ".slm-history-7":
             case ".async-search":
             case ".profiling-ilm-lock": // TODO: Remove after switch to K/V indices
@@ -1965,6 +1966,7 @@ public abstract class ESRestTestCase extends ESTestCase {
             case "synthetics-settings":
             case "synthetics-mappings":
             case ".snapshot-blob-cache":
+            case "ilm-history":
             case "ilm-history-7":
             case "logstash-index-template":
             case "security-index-template":

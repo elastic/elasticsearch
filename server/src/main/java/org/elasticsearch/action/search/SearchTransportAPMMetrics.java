cf519571a1118f3e341e0e8524e3a81d03e2e321
John Verwolf <osjdverwolf@gmail.com>
2023-12-19 07:17:25 -0800

    Remove todos

--- server/src/main/java/org/elasticsearch/action/search/SearchTransportAPMMetrics.java
+++ server/src/main/java/org/elasticsearch/action/search/SearchTransportAPMMetrics.java
@@ -15,17 +15,17 @@ public class SearchTransportAPMMetrics {
     public static final String SEARCH_ACTION_LATENCY_BASE_METRIC = "es.search.nodes.transport_actions.latency.histogram";
     public static final String ACTION_ATTRIBUTE_NAME = "action";
 
-    public static final String QUERY_CAN_MATCH_NODE_METRIC = "shards_can_match"; // TODO: Not sure how to test.
+    public static final String QUERY_CAN_MATCH_NODE_METRIC = "shards_can_match";
     public static final String DFS_ACTION_METRIC = "dfs_query_then_fetch/shard_dfs_phase";
     public static final String QUERY_ID_ACTION_METRIC = "dfs_query_then_fetch/shard_query_phase";
     public static final String QUERY_ACTION_METRIC = "query_then_fetch/shard_query_phase";
-    public static final String FREE_CONTEXT_ACTION_METRIC = "shard_release_context"; // TODO: Not sure how to test.
+    public static final String FREE_CONTEXT_ACTION_METRIC = "shard_release_context";
     public static final String FETCH_ID_ACTION_METRIC = "shard_fetch_phase";
     public static final String QUERY_SCROLL_ACTION_METRIC = "scroll/shard_query_phase";
     public static final String FETCH_ID_SCROLL_ACTION_METRIC = "scroll/shard_fetch_phase";
-    public static final String QUERY_FETCH_SCROLL_ACTION_METRIC = "scroll/shard_query_and_fetch_phase"; // TODO: Not sure how to test.
+    public static final String QUERY_FETCH_SCROLL_ACTION_METRIC = "scroll/shard_query_and_fetch_phase";
     public static final String FREE_CONTEXT_SCROLL_ACTION_METRIC = "scroll/shard_release_context";
-    public static final String CLEAR_SCROLL_CONTEXTS_ACTION_METRIC = "scroll/shard_release_contexts"; // TODO: Not sure how to test.
+    public static final String CLEAR_SCROLL_CONTEXTS_ACTION_METRIC = "scroll/shard_release_contexts";
 
     private final LongHistogram actionLatencies;
 

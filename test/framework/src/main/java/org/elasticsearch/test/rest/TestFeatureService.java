6330a0826021c252d9168cb7b93bd0353d5dec02
Lorenzo Dematte <lorenzo.dematte@elastic.co>
2023-12-20 16:47:18 +0100

    Expose nodes features (nodeHasFeature)

--- test/framework/src/main/java/org/elasticsearch/test/rest/TestFeatureService.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/TestFeatureService.java
@@ -23,9 +23,10 @@ import java.util.function.Predicate;
 public class TestFeatureService {
     private final Predicate<String> historicalFeaturesPredicate;
     private final Set<String> clusterStateFeatures;
-    private final Map<String, Version> nodeVersions;
+    private final Map<String, Version> nodesVersions;
     private final Map<String, Set<String>> nodesFeatures;
     private final NavigableMap<Version, Set<String>> historicalFeatures;
+    private final String errorMessage;
 
     TestFeatureService(
         boolean hasHistoricalFeaturesInformation,
@@ -33,44 +34,61 @@ public class TestFeatureService {
         Map<String, Version> nodesVersion,
         Map<String, Set<String>> nodesFeatures
     ) {
-        this.nodeVersions = nodesVersion;
+        this.nodesVersions = nodesVersion;
         this.nodesFeatures = nodesFeatures;
 
         var minNodeVersion = nodesVersion.values().stream().min(Version::compareTo);
         var featureData = FeatureData.createFromSpecifications(specs);
         this.historicalFeatures = featureData.getHistoricalFeatures();
-        var allHistoricalFeatures = getAllHistoricalFeatures();
 
-        var errorMessage = hasHistoricalFeaturesInformation
+        this.errorMessage = hasHistoricalFeaturesInformation
             ? "Check the feature has been added to the correct FeatureSpecification in the relevant module or, if this is a "
                 + "legacy feature used only in tests, to a test-only FeatureSpecification"
             : "This test is running on the legacy test framework; historical features from production code will not be available."
                 + " You need to port the test to the new test plugins in order to use historical features from production code."
                 + " If this is a legacy feature used only in tests, you can add it to a test-only FeatureSpecification";
-        this.historicalFeaturesPredicate = minNodeVersion.<Predicate<String>>map(v -> featureId -> {
-            assert allHistoricalFeatures.contains(featureId) : Strings.format("Unknown historical feature %s: %s", featureId, errorMessage);
-            return hasHistoricalFeature(historicalFeatures, v, featureId);
-        }).orElse(featureId -> {
-            // We can safely assume that new non-semantic versions (serverless) support all historical features
-            assert allHistoricalFeatures.contains(featureId) : Strings.format("Unknown historical feature %s: %s", featureId, errorMessage);
-            return true;
-        });
+        this.historicalFeaturesPredicate = minNodeVersion.<Predicate<String>>map(
+            v -> featureId -> hasHistoricalFeatureWithVersion(v, featureId)
+        ).orElse(this::hasHistoricalFeatureWithoutVersion);
         this.clusterStateFeatures = ClusterFeatures.calculateAllNodeFeatures(nodesFeatures.values());
+    }
+
+    private boolean hasHistoricalFeatureWithVersion(Version v, String featureId) {
+        assert getAllHistoricalFeatures().contains(featureId)
+            : Strings.format("Unknown historical feature %s: %s", featureId, errorMessage);
+        var features = historicalFeatures.floorEntry(v);
+        return features != null && features.getValue().contains(featureId);
+    }
+
+    private boolean hasHistoricalFeatureWithoutVersion(String featureId) {
+        // We can safely assume that new non-semantic versions (serverless) support all historical features
+        assert getAllHistoricalFeatures().contains(featureId)
+            : Strings.format("Unknown historical feature %s: %s", featureId, errorMessage);
+        return true;
     }
 
     private Set<String> getAllHistoricalFeatures() {
         return historicalFeatures.lastEntry() == null ? Set.of() : historicalFeatures.lastEntry().getValue();
     }
 
-    private static boolean hasHistoricalFeature(NavigableMap<Version, Set<String>> historicalFeatures, Version version, String featureId) {
-        var features = historicalFeatures.floorEntry(version);
-        return features != null && features.getValue().contains(featureId);
-    }
-
-    boolean clusterHasFeature(String featureId) {
+    public boolean clusterHasFeature(String featureId) {
         if (clusterStateFeatures.contains(featureId)) {
             return true;
         }
         return historicalFeaturesPredicate.test(featureId);
+    }
+
+    public boolean nodeHasFeature(String nodeId, String featureId) {
+        var nodeFeatures = nodesFeatures.get(nodeId);
+        if (nodeFeatures != null && nodeFeatures.contains(featureId)) {
+            return true;
+        }
+
+        if (nodesVersions.isEmpty()) {
+            return hasHistoricalFeatureWithoutVersion(featureId);
+        }
+
+        var nodeVersion = nodesVersions.get(nodeId);
+        return nodeVersion != null && hasHistoricalFeatureWithVersion(nodeVersion, featureId);
     }
 }

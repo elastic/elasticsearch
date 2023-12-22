4802b2398d7d6e9b2db0c1342602f4d0df574bf4
carlosdelest <carlos.delgado@elastic.co>
2023-12-22 18:39:40 +0100

    License headers

--- server/src/main/java/org/elasticsearch/action/inference/results/SparseEmbeddingResults.java
+++ server/src/main/java/org/elasticsearch/action/inference/results/SparseEmbeddingResults.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
 public record SparseEmbeddingResults(List<Embedding> embeddings) implements InferenceServiceResults {
 
     public static final String NAME = "sparse_embedding_results";

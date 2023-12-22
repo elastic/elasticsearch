b29e26af0588c1c8521b44d0350aba9ebd7c7ea0
gmarouli <mary.gouseti@elastic.co>
2023-12-22 13:52:18 +0200

    Captured the failure in a test

--- modules/data-streams/src/javaRestTest/java/org/elasticsearch/datastreams/LazyRolloverDataStreamIT.java
+++ modules/data-streams/src/javaRestTest/java/org/elasticsearch/datastreams/LazyRolloverDataStreamIT.java
@@ -10,10 +10,12 @@ package org.elasticsearch.datastreams;
 
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseException;
 
 import java.util.List;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -71,5 +73,30 @@ public class LazyRolloverDataStreamIT extends DisabledSecurityDataStreamTestCase
             assertThat(dataStream.get("rollover_on_write"), is(false));
             assertThat(((List<Object>) dataStream.get("indices")).size(), is(2));
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testLazyRolloverWithConditions() throws Exception {
+        Request putComposableIndexTemplateRequest = new Request("POST", "/_index_template/lazy-ds-template");
+        putComposableIndexTemplateRequest.setJsonEntity("""
+            {
+              "index_patterns": ["lazy-ds*"],
+              "data_stream": {}
+            }
+            """);
+        assertOK(client().performRequest(putComposableIndexTemplateRequest));
+
+        String dataStreamName = "lazy-ds";
+
+        Request createDocRequest = new Request("POST", "/" + dataStreamName + "/_doc?refresh=true");
+        createDocRequest.setJsonEntity("{ \"@timestamp\": \"2020-10-22\", \"a\": 1 }");
+
+        assertOK(client().performRequest(createDocRequest));
+
+        Request rolloverRequest = new Request("POST", "/" + dataStreamName + "/_rollover?lazy");
+        rolloverRequest.setJsonEntity("{\"conditions\": {\"max_docs\": 1}}");
+        ResponseException responseError = expectThrows(ResponseException.class, () -> client().performRequest(rolloverRequest));
+        assertThat(responseError.getResponse().getStatusLine().getStatusCode(), is(400));
+        assertThat(responseError.getMessage(), containsString("only without any conditions"));
     }
 }

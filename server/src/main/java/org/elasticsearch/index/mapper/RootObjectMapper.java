84ab9ae3b3badb02db1d8765a921eab0bb79417d
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2024-01-02 09:06:22 +0100

    Consider runtime fields for root mapper size

--- server/src/main/java/org/elasticsearch/index/mapper/RootObjectMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/RootObjectMapper.java
@@ -525,7 +525,7 @@ public class RootObjectMapper extends ObjectMapper {
 
     @Override
     public int mapperSize() {
-        int size = 0;
+        int size = runtimeFields().size();
         for (Mapper mapper : this) {
             size += mapper.mapperSize();
         }

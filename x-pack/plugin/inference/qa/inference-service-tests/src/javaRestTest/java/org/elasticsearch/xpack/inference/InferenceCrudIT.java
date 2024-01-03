2d4d0c69cd773283115e74032d7bf21665a2f216
Max Hniebergall <max.hniebergall@elastic.co>
2024-01-03 14:29:35 -0500

    Revert "broken deployment in integration test"
    This reverts commit 27a7f512b34a3f6f64bb8f6341627ef6272d0b1d.

--- x-pack/plugin/inference/qa/inference-service-tests/src/javaRestTest/java/org/elasticsearch/xpack/inference/InferenceCrudIT.java
+++ x-pack/plugin/inference/qa/inference-service-tests/src/javaRestTest/java/org/elasticsearch/xpack/inference/InferenceCrudIT.java
@@ -3,16 +3,12 @@
  * or more contributor license agreements. Licensed under the Elastic License
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
- *
- * This file was contributed to by a generative AI
  */
 
 package org.elasticsearch.xpack.inference;
 
-import org.elasticsearch.client.Request;
 import org.elasticsearch.client.ResponseException;
 import org.elasticsearch.inference.TaskType;
-import org.junit.After;
 
 import java.io.IOException;
 import java.util.List;
@@ -22,17 +18,6 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasSize;
 
 public class InferenceCrudIT extends InferenceBaseRestTest {
-
-    @After
-    public void cleanup() throws Exception {
-        waitForPendingTasks(client());
-    }
-
-    public void testPutAndDeleteModel() throws IOException {
-        String modelId = "a_model_for_happy_case";
-        createBasicModel(modelId);
-        deleteModel(modelId, true);
-    }
 
     @SuppressWarnings("unchecked")
     public void testGet() throws IOException {
@@ -64,8 +49,8 @@ public class InferenceCrudIT extends InferenceBaseRestTest {
     }
 
     public void testGetModelWithWrongTaskType() throws IOException {
-        putModel("sparse_embedding_model_one", mockServiceModelConfig(), TaskType.SPARSE_EMBEDDING);
-        var e = expectThrows(ResponseException.class, () -> getModels("sparse_embedding_model_one", TaskType.TEXT_EMBEDDING));
+        putModel("sparse_embedding_model", mockServiceModelConfig(), TaskType.SPARSE_EMBEDDING);
+        var e = expectThrows(ResponseException.class, () -> getModels("sparse_embedding_model", TaskType.TEXT_EMBEDDING));
         assertThat(
             e.getMessage(),
             containsString("Requested task type [text_embedding] does not match the model's task type [sparse_embedding]")
@@ -74,26 +59,11 @@ public class InferenceCrudIT extends InferenceBaseRestTest {
 
     @SuppressWarnings("unchecked")
     public void testGetModelWithAnyTaskType() throws IOException {
-        String modelId = "sparse_embedding_model_two";
+        String modelId = "sparse_embedding_model";
         putModel(modelId, mockServiceModelConfig(), TaskType.SPARSE_EMBEDDING);
         var singleModel = (List<Map<String, Object>>) getModels(modelId, TaskType.ANY).get("models");
         System.out.println("MODEL" + singleModel);
         assertEquals(modelId, singleModel.get(0).get("model_id"));
         assertEquals(TaskType.SPARSE_EMBEDDING.toString(), singleModel.get(0).get("task_type"));
     }
-
-    public void testPutModelFailsWhenTrainedModelWithIdAlreadyExists() throws Exception {
-        String modelId = "duplicate_model_id";
-        createBasicModel(modelId);
-        Request request = new Request("GET", "_ml/trained_models/_all");
-        System.out.println("99598" + client().performRequest(request));
-        startDeployment(modelId);
-
-        var e = expectThrows(ResponseException.class, () -> putModel(modelId, mockServiceModelConfig(), TaskType.SPARSE_EMBEDDING));
-        assertEquals("Trained machine learning model [duplicate_model_id] already exists", e.getMessage());
-
-        // clean up
-        deleteModel(modelId, true);
-    }
-
 }

4d9eed2b414f2f8481727e6482be39e81a2db294
Craig Taverner <craig@amanzi.com>
2024-01-03 17:25:43 +0100

    Refactor WKBTopNEncoder to use length prefix encoding
    The previous approach of using WKT was expensive and wasteful. Since the types are unsortable, we can just encode them as is.

--- x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/operator/topn/WKBTopNEncoder.java
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/operator/topn/WKBTopNEncoder.java
@@ -8,44 +8,29 @@
 package org.elasticsearch.compute.operator.topn;
 
 import org.apache.lucene.util.BytesRef;
-import org.elasticsearch.common.lucene.BytesRefs;
 import org.elasticsearch.compute.operator.BreakingBytesRefBuilder;
-import org.elasticsearch.geometry.Geometry;
-import org.elasticsearch.geometry.utils.GeometryValidator;
-import org.elasticsearch.geometry.utils.WellKnownBinary;
-import org.elasticsearch.geometry.utils.WellKnownText;
-
-import java.nio.ByteOrder;
 
 /**
- * Encodes WKB bytes[].
- * Since WKB can contain bytes with zero value, which are used as terminator bytes, we need to encode differently.
- * Our initial implementation is to re-write to WKT and encode with UTF8TopNEncoder.
- * This is likely very inefficient.
- * We cannot use the UTF8TopNEncoder as is, because it removes the continuation byte, which could be a valid value in WKB.
+ * Encodes WKB bytes[]. Spatial types are unsortable by definition.
  */
-final class WKBTopNEncoder extends SortableTopNEncoder {
+final class WKBTopNEncoder extends UnsortableTopNEncoder {
     @Override
     public int encodeBytesRef(BytesRef value, BreakingBytesRefBuilder bytesRefBuilder) {
-        // TODO: Consider more compact/efficient encoding. Perhaps base64?
-        Geometry geometry = WellKnownBinary.fromWKB(GeometryValidator.NOOP, false, value.bytes, value.offset, value.length);
-        String wkt = WellKnownText.toWKT(geometry);
-        return UTF8.encodeBytesRef(BytesRefs.toBytesRef(wkt), bytesRefBuilder);
+        final int offset = bytesRefBuilder.length();
+        encodeVInt(value.length, bytesRefBuilder);
+        bytesRefBuilder.append(value);
+        return bytesRefBuilder.length() - offset;
     }
 
     @Override
     public BytesRef decodeBytesRef(BytesRef bytes, BytesRef scratch) {
-        String wkt = BytesRefs.toString(UTF8.decodeBytesRef(bytes, scratch));
-        try {
-            Geometry geometry = WellKnownText.fromWKT(GeometryValidator.NOOP, false, wkt);
-            byte[] wkb = WellKnownBinary.toWKB(geometry, ByteOrder.LITTLE_ENDIAN);
-            scratch.bytes = wkb;
-            scratch.offset = 0;
-            scratch.length = wkb.length;
-            return scratch;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        final int len = decodeVInt(bytes);
+        scratch.bytes = bytes.bytes;
+        scratch.offset = bytes.offset;
+        scratch.length = len;
+        bytes.offset += len;
+        bytes.length -= len;
+        return scratch;
     }
 
     @Override
@@ -55,7 +40,7 @@ final class WKBTopNEncoder extends SortableTopNEncoder {
 
     @Override
     public TopNEncoder toSortable() {
-        return this;
+        throw new UnsupportedOperationException("Cannot sort spatial types");
     }
 
     @Override

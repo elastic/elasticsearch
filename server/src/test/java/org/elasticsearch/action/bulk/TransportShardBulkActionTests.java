511c15405dc6c5ffa2a6e72e97032130c7aad954
Keith Massey <keith.massey@elastic.co>
2024-01-03 10:13:25 -0600

    fixing TransportShardBulkActionTests

--- server/src/test/java/org/elasticsearch/action/bulk/TransportShardBulkActionTests.java
+++ server/src/test/java/org/elasticsearch/action/bulk/TransportShardBulkActionTests.java
@@ -1246,6 +1246,7 @@ public class TransportShardBulkActionTests extends IndexShardTestCase {
 
         latch.await();
         verify(mapperService, times(2)).merge(any(), any(CompressedXContent.class), any());
+        bulkShardRequest.decRef();
     }
 
     private IndexShard mockShard() {

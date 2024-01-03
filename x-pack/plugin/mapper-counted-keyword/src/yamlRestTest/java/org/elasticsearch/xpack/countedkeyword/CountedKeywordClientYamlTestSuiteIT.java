68b15cdeb99c385a881912bf1786d6e26171b48e
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2024-01-03 10:49:18 +0100

    Support sampling in counted_terms aggregation
    With this commit we support using the `counted_terms` aggregation in a
    sampling context (e.g. as a sub-aggregation in a `random_sampler`
    aggregation).

---
+++ x-pack/plugin/mapper-counted-keyword/src/yamlRestTest/java/org/elasticsearch/xpack/countedkeyword/CountedKeywordClientYamlTestSuiteIT.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.countedkeyword;
+
+import com.carrotsearch.randomizedtesting.annotations.Name;
+import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
+
+import org.elasticsearch.test.cluster.ElasticsearchCluster;
+import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
+import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
+import org.junit.ClassRule;
+
+public class CountedKeywordClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
+
+    @ClassRule
+    public static ElasticsearchCluster cluster = ElasticsearchCluster.local().plugin("counted-keyword").build();
+
+    public CountedKeywordClientYamlTestSuiteIT(@Name("yaml") ClientYamlTestCandidate testCandidate) {
+        super(testCandidate);
+    }
+
+    @ParametersFactory
+    public static Iterable<Object[]> parameters() throws Exception {
+        return createParameters();
+    }
+
+    @Override
+    protected String getTestRestCluster() {
+        return cluster.getHttpAddresses();
+    }
+}

a128c585bdbcc10329b258eb8cf77952a26f4a38
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2024-01-04 08:13:46 -0500

    Mute test for issue #103907

--- server/src/test/java/org/elasticsearch/cluster/routing/allocation/allocator/DesiredBalanceComputerTests.java
+++ server/src/test/java/org/elasticsearch/cluster/routing/allocation/allocator/DesiredBalanceComputerTests.java
@@ -861,6 +861,7 @@ public class DesiredBalanceComputerTests extends ESAllocationTestCase {
         assertThat(resultDiskUsage, allOf(aMapWithSize(2), hasEntry("node-0", 950L), hasEntry("node-1", 850L)));
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/103907")
     public void testAccountForSizeOfMisplacedShardsDuringNewComputation() {
 
         var snapshot = new Snapshot("repository", new SnapshotId("snapshot", randomUUID()));

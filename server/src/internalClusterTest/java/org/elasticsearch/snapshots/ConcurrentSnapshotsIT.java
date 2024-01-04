e4280a7f58309f1ef87b99ff599ddd48a8c1e66e
David Turner <david.turner@elastic.co>
2024-01-04 16:21:36 +0000

    Comment about why we recreate the index

--- server/src/internalClusterTest/java/org/elasticsearch/snapshots/ConcurrentSnapshotsIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/snapshots/ConcurrentSnapshotsIT.java
@@ -2148,6 +2148,8 @@ public class ConcurrentSnapshotsIT extends AbstractSnapshotIntegTestCase {
             })).indices(new Index[] { internalCluster().clusterService().state().metadata().index(indexToDelete).getIndex() })
                 .ackTimeout(TimeValue.timeValueSeconds(10))
                 .masterNodeTimeout(TimeValue.timeValueSeconds(10))))
+            // ultimately create the index again so that taking a full snapshot will pick up any missing shard gen blob, and deleting that
+            // full snapshot will clean up all dangling shard-level blobs
             .andThen((l, ignored) -> prepareCreate(indexToDelete, indexSettingsNoReplicas(1)).execute(l.map(r -> {
                 assertTrue(r.isAcknowledged());
                 return null;

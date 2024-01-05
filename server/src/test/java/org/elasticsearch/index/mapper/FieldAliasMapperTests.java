2518e33f300e9e8e7b3a5ef21b1f080a83541435
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2024-01-05 08:47:33 +0100

    Remove unused import

--- server/src/test/java/org/elasticsearch/index/mapper/FieldAliasMapperTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/FieldAliasMapperTests.java
@@ -9,7 +9,6 @@
 package org.elasticsearch.index.mapper;
 
 import org.elasticsearch.common.Strings;
-import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentFactory;
 
 import java.io.IOException;

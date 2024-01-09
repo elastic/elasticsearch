d0adc40844e3cdcc7e96ec5de2854ffd7a5396fc
Nik Everett <nik9000@gmail.com>
2024-01-09 07:43:21 -0500

    Revert mistake

--- x-pack/plugin/esql/qa/server/multi-clusters/src/javaRestTest/java/org/elasticsearch/xpack/esql/ccq/Clusters.java
+++ x-pack/plugin/esql/qa/server/multi-clusters/src/javaRestTest/java/org/elasticsearch/xpack/esql/ccq/Clusters.java
@@ -1,7 +1,4 @@
 /*
-Viewed
-File renamed without changes.
-
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
  * 2.0; you may not use this file except in compliance with the Elastic License

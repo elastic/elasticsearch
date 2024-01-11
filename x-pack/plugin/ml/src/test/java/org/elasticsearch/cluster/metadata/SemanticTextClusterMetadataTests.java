7c657e0a290d765dafa7a039799b7681446c0d0e
Mike Pellegrini <mike.pellegrini@elastic.co>
2024-01-11 15:18:26 -0500

    Renamed test class

--- x-pack/plugin/ml/src/test/java/org/elasticsearch/cluster/metadata/SemanticTextClusterMetadataTests.java
+++ x-pack/plugin/ml/src/test/java/org/elasticsearch/cluster/metadata/SemanticTextClusterMetadataTests.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class SemanticTextMetadataMappingServiceTests extends MlSingleNodeTestCase {
+public class SemanticTextClusterMetadataTests extends MlSingleNodeTestCase {
     public void testCreateIndexWithSemanticTextField() {
         final IndexService indexService = createIndex(
             "test",

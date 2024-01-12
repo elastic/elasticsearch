7cf7ee62097382c7516afb11ff439775ee7841df
Lorenzo Dematte <lorenzo.dematte@elastic.co>
2024-01-12 11:54:58 +0100

    More versions to features

--- test/framework/src/main/java/org/elasticsearch/test/rest/RestTestLegacyFeatures.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/RestTestLegacyFeatures.java
@@ -76,6 +76,27 @@ public class RestTestLegacyFeatures implements FeatureSpecification {
     @UpdateForV9
     public static final NodeFeature ML_ANALYTICS_MAPPINGS = new NodeFeature("ml.analytics_mappings");
 
+    public static final NodeFeature TSDB_NEW_INDEX_FORMAT = new NodeFeature("indices.tsdb_new_format");
+    public static final NodeFeature TSDB_GENERALLY_AVAILABLE = new NodeFeature("indices.tsdb_supported");
+
+    /*
+     * A composable index template with no template defined in the body is mistakenly always assumed to not be a time series template.
+     * Fixed in #98840
+     */
+    public static final NodeFeature TSDB_EMPTY_TEMPLATE_FIXED = new NodeFeature("indices.tsdb_empty_composable_template_fixed");
+    public static final NodeFeature SYNTHETIC_SOURCE_SUPPORTED = new NodeFeature("indices.synthetic_source");
+
+    public static final NodeFeature DESIRED_BALANCED_ALLOCATOR_SUPPORTED = new NodeFeature("allocator.desired_balance");
+
+    /*
+     * Cancel shard allocation command is broken for initial desired balance versions
+     * and might allocate shard on the node where it is not supposed to be. This
+     * is fixed by https://github.com/elastic/elasticsearch/pull/93635.
+     */
+    public static final NodeFeature DESIRED_BALANCED_ALLOCATOR_FIXED = new NodeFeature("allocator.desired_balance_fixed");
+    public static final NodeFeature INDEXING_SLOWLOG_LEVEL_SETTING_REMOVED = new NodeFeature("settings.indexing_slowlog_level_removed");
+    public static final NodeFeature DEPRECATION_WARNINGS_LEAK_FIXED = new NodeFeature("deprecation_warnings_leak_fixed");
+
     // YAML
     public static final NodeFeature REST_ELASTIC_PRODUCT_HEADER_PRESENT = new NodeFeature("action.rest.product_header_present");
 
@@ -104,7 +125,15 @@ public class RestTestLegacyFeatures implements FeatureSpecification {
             entry(ML_INDICES_HIDDEN, Version.V_7_7_0),
             entry(ML_ANALYTICS_MAPPINGS, Version.V_7_3_0),
             entry(REST_ELASTIC_PRODUCT_HEADER_PRESENT, Version.V_8_0_1),
-            entry(DESIRED_NODE_API_SUPPORTED, Version.V_8_1_0)
+            entry(DESIRED_NODE_API_SUPPORTED, Version.V_8_1_0),
+            entry(TSDB_NEW_INDEX_FORMAT, Version.V_8_2_0),
+            entry(SYNTHETIC_SOURCE_SUPPORTED, Version.V_8_4_0),
+            entry(DESIRED_BALANCED_ALLOCATOR_SUPPORTED, Version.V_8_6_0),
+            entry(DESIRED_BALANCED_ALLOCATOR_FIXED, Version.V_8_7_1),
+            entry(TSDB_GENERALLY_AVAILABLE, Version.V_8_7_0),
+            entry(TSDB_EMPTY_TEMPLATE_FIXED, Version.V_8_11_0),
+            entry(INDEXING_SLOWLOG_LEVEL_SETTING_REMOVED, Version.V_8_0_0),
+            entry(DEPRECATION_WARNINGS_LEAK_FIXED, Version.V_7_17_9)
         );
     }
 }

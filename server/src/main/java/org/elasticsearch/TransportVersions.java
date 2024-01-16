cf525b1fd516d9970edf88e6548e49d2dbcf92fd
Kostas Krikellas <131142368+kkrik-es@users.noreply.github.com>
2024-01-16 16:21:14 +0200

    [ 8.12] Downsampling supports date_histogram with tz  (#104390)
    * Port pr-103511 to branch 8.12
    
    * mark changed transport version as patch

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -191,6 +191,7 @@ public class TransportVersions {
     public static final TransportVersion MISSED_INDICES_UPDATE_EXCEPTION_ADDED = def(8_558_00_0);
     public static final TransportVersion INFERENCE_SERVICE_EMBEDDING_SIZE_ADDED = def(8_559_00_0);
     public static final TransportVersion ENRICH_ELASTICSEARCH_VERSION_REMOVED = def(8_560_00_0);
+    public static final TransportVersion DATE_HISTOGRAM_SUPPORT_DOWNSAMPLED_TZ = def(8_560_00_1);
 
     /*
      * STOP! READ THIS FIRST! No, really,

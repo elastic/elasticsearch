80218ffe61f17b6712041ba6e62e05dade661e16
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2024-01-17 19:09:58 +0100

    Some test fixes

--- test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/ESRestTestCase.java
@@ -740,6 +740,7 @@ public abstract class ESRestTestCase extends ESTestCase {
             "logs@lifecycle",
             "metrics",
             "metrics@lifecycle",
+            "traces@lifecycle",
             "profiling-60-days",
             "profiling-60-days@lifecycle",
             "synthetics",

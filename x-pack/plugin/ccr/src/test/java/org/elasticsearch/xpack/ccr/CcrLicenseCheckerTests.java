1581b4f0c2c2dfc2864a68410a5b0de2e97c1fc9
David Turner <david.turner@elastic.co>
2024-01-18 17:09:03 +0000

    Mucks

--- x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/CcrLicenseCheckerTests.java
+++ x-pack/plugin/ccr/src/test/java/org/elasticsearch/xpack/ccr/CcrLicenseCheckerTests.java
@@ -10,6 +10,7 @@ package org.elasticsearch.xpack.ccr;
 import org.elasticsearch.client.internal.Client;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.xpack.core.security.user.User;
 
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -18,6 +19,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasToString;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CcrLicenseCheckerTests extends ESTestCase {
 
@@ -32,7 +34,9 @@ public class CcrLicenseCheckerTests extends ESTestCase {
 
         };
         final AtomicBoolean invoked = new AtomicBoolean();
-        checker.hasPrivilegesToFollowIndices(mock(Client.class), new String[] { randomAlphaOfLength(8) }, e -> {
+        final var client = mock(Client.class);
+        when(client.threadPool()).thenReturn(mock(ThreadPool.class));
+        checker.hasPrivilegesToFollowIndices(client, new String[] { randomAlphaOfLength(8) }, e -> {
             invoked.set(true);
             assertThat(e, instanceOf(IllegalStateException.class));
             assertThat(e, hasToString(containsString("missing or unable to read authentication info on request")));

18cb954a5a15b5925cd08a13284d69f81aa78655
Joe Gallo <joegallo@gmail.com>
2024-01-19 18:29:41 -0500

    Tighten up the failure side, too
    It doesn't invoke new ShardId(...) which is where the exception was,
    but it's still nice to report this information if we have it (which we
    do!).

--- server/src/main/java/org/elasticsearch/action/bulk/TransportBulkAction.java
+++ server/src/main/java/org/elasticsearch/action/bulk/TransportBulkAction.java
@@ -1131,14 +1131,15 @@ public class TransportBulkAction extends HandledTransportAction<BulkRequest, Bul
         }
 
         synchronized void markItemAsFailed(int slot, Exception e) {
-            IndexRequest indexRequest = getIndexWriteRequest(bulkRequest.requests().get(slot));
+            final DocWriteRequest<?> docWriteRequest = bulkRequest.requests().get(slot);
+            final String id = Objects.requireNonNullElse(docWriteRequest.id(), DROPPED_ITEM_WITH_AUTO_GENERATED_ID);
             // We hit a error during preprocessing a request, so we:
             // 1) Remember the request item slot from the bulk, so that when we're done processing all requests we know what failed
             // 2) Add a bulk item failure for this request
             // 3) Continue with the next request in the bulk.
             failedSlots.set(slot);
-            BulkItemResponse.Failure failure = new BulkItemResponse.Failure(indexRequest.index(), indexRequest.id(), e);
-            itemResponses.add(BulkItemResponse.failure(slot, indexRequest.opType(), failure));
+            BulkItemResponse.Failure failure = new BulkItemResponse.Failure(docWriteRequest.index(), id, e);
+            itemResponses.add(BulkItemResponse.failure(slot, docWriteRequest.opType(), failure));
         }
     }
 }

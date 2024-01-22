d30da6f29da436e1084c63ee659a4357796fcc5d
Jack Conradson <osjdconrad@gmail.com>
2024-01-22 10:17:08 -0800

    remove test comment

--- server/src/main/java/org/elasticsearch/search/retriever/RetrieverBuilder.java
+++ server/src/main/java/org/elasticsearch/search/retriever/RetrieverBuilder.java
@@ -31,8 +31,6 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.Objects;
 
-// test change
-
 public abstract class RetrieverBuilder<RB extends RetrieverBuilder<RB>>
     implements
         VersionedNamedWriteable,

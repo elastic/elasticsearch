23bb7612cabaa19cd69b810f15e21979c960683e
Brian Seeders <brian.seeders@elastic.co>
2024-01-23 12:28:19 -0500

    Bump versions after 7.17.17 release

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -176,7 +176,9 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_7_17_15 = new Version(7_17_15_99, org.apache.lucene.util.Version.LUCENE_8_11_1);
     public static final Version V_7_17_16 = new Version(7_17_16_99, org.apache.lucene.util.Version.LUCENE_8_11_1);
     public static final Version V_7_17_17 = new Version(7_17_17_99, org.apache.lucene.util.Version.LUCENE_8_11_1);
-    public static final Version CURRENT = V_7_17_17;
+    public static final Version V_7_17_18 = new Version(7_17_18_99, org.apache.lucene.util.Version.LUCENE_8_11_1);
+
+    public static final Version CURRENT = V_7_17_18;
 
     private static final ImmutableOpenIntMap<Version> idToVersion;
     private static final ImmutableOpenMap<String, Version> stringToVersion;

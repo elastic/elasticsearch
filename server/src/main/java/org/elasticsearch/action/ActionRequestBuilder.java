5beaaf4e50ae69b77dc86c67d5c73379e52fb962
Keith Massey <keith.massey@elastic.co>
2024-01-23 15:43:14 -0600

    reverting change to ActionRequestBuilder

--- server/src/main/java/org/elasticsearch/action/ActionRequestBuilder.java
+++ server/src/main/java/org/elasticsearch/action/ActionRequestBuilder.java
@@ -8,14 +8,10 @@
 
 package org.elasticsearch.action;
 
-import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.client.internal.ElasticsearchClient;
-import org.elasticsearch.core.RefCounted;
 import org.elasticsearch.core.TimeValue;
 
 import java.util.Objects;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public abstract class ActionRequestBuilder<Request extends ActionRequest, Response extends ActionResponse> {
 
@@ -35,11 +31,7 @@ public abstract class ActionRequestBuilder<Request extends ActionRequest, Respon
     }
 
     public ActionFuture<Response> execute() {
-        Request request = request();
-        RefCountedFuture<Response> future = new RefCountedFuture<>();
-        client.execute(action, request, ActionListener.runAfter(future, request::decRef));
-        // TODO: previously we were using RefCountedFuture for the response -- do we need that here?
-        return future;
+        return client.execute(action, request);
     }
 
     /**
@@ -57,31 +49,6 @@ public abstract class ActionRequestBuilder<Request extends ActionRequest, Respon
     }
 
     public void execute(ActionListener<Response> listener) {
-        Request request = request();
-        client.execute(action, request, ActionListener.runAfter(listener, request::decRef));
-    }
-
-    private static class RefCountedFuture<R extends RefCounted> extends PlainActionFuture<R> {
-
-        @Override
-        public final void onResponse(R result) {
-            result.mustIncRef();
-            if (set(result) == false) {
-                result.decRef();
-            }
-        }
-
-        private final AtomicBoolean getCalled = new AtomicBoolean(false);
-
-        @Override
-        public R get() throws InterruptedException, ExecutionException {
-            final boolean firstCall = getCalled.compareAndSet(false, true);
-            if (firstCall == false) {
-                final IllegalStateException ise = new IllegalStateException("must only call .get() once per instance to avoid leaks");
-                assert false : ise;
-                throw ise;
-            }
-            return super.get();
-        }
+        client.execute(action, request, listener);
     }
 }

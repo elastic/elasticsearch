1f70c52cafbe79746a5b3d1f9e3845d62ede8972
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2024-01-24 13:26:57 +0100

    Apply spotless suggestions

--- server/src/test/java/org/elasticsearch/index/mapper/ObjectMapperTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/ObjectMapperTests.java
@@ -534,8 +534,8 @@ public class ObjectMapperTests extends MapperServiceTestCase {
         ObjectMapper.Builder mapperBuilder = new ObjectMapper.Builder("parent_size_1", Explicit.IMPLICIT_TRUE).add(
             new ObjectMapper.Builder("child_size_2", Explicit.IMPLICIT_TRUE).add(
                 new TextFieldMapper.Builder("grand_child_size_3", createDefaultIndexAnalyzers()).addMultiField(
-                        new KeywordFieldMapper.Builder("multi_field_size_4", IndexVersion.current())
-                    )
+                    new KeywordFieldMapper.Builder("multi_field_size_4", IndexVersion.current())
+                )
                     .addMultiField(
                         new TextFieldMapper.Builder("grand_child_size_5", createDefaultIndexAnalyzers()).addMultiField(
                             new KeywordFieldMapper.Builder("multi_field_of_multi_field_size_6", IndexVersion.current())

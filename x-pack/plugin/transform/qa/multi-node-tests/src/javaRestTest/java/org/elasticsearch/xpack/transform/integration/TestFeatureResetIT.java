0cc1e2267fcae65eb4f0bddb34f38b4d06ca2707
Przemyslaw Witek <przemyslaw.witek@elastic.co>
2024-01-25 15:20:37 +0100

    [Transform] Unmute TestFeatureResetIT and include more debug information

--- x-pack/plugin/transform/qa/multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/transform/integration/TestFeatureResetIT.java
+++ x-pack/plugin/transform/qa/multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/transform/integration/TestFeatureResetIT.java
@@ -90,7 +90,8 @@ public class TestFeatureResetIT extends TransformRestTestCase {
 
         putTransform(continuousTransformId, Strings.toString(config), RequestOptions.DEFAULT);
         startTransform(continuousTransformId, RequestOptions.DEFAULT);
-        client().performRequest(new Request(HttpPost.METHOD_NAME, "/_features/_reset"));
+
+        assertOK(client().performRequest(new Request(HttpPost.METHOD_NAME, "/_features/_reset")));
 
         Response response = adminClient().performRequest(new Request("GET", "/_cluster/state?metric=metadata"));
         Map<String, Object> metadata = (Map<String, Object>) ESRestTestCase.entityAsMap(response).get("metadata");

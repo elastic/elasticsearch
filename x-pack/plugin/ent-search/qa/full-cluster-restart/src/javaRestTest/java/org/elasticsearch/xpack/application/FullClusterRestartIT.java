76eeb423442326c77aef2e34ef2fd74a593813c1
Adam Demjen <demjened@gmail.com>
2024-01-26 11:52:54 -0500

    Use @AwaitsFix annotation

--- x-pack/plugin/ent-search/qa/full-cluster-restart/src/javaRestTest/java/org/elasticsearch/xpack/application/FullClusterRestartIT.java
+++ x-pack/plugin/ent-search/qa/full-cluster-restart/src/javaRestTest/java/org/elasticsearch/xpack/application/FullClusterRestartIT.java
@@ -17,7 +17,6 @@ import org.elasticsearch.test.rest.ObjectPath;
 import org.elasticsearch.upgrades.FullClusterRestartUpgradeStatus;
 import org.elasticsearch.upgrades.ParameterizedFullClusterRestartTestCase;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 
 import java.io.IOException;
 import java.util.List;
@@ -49,7 +48,7 @@ public class FullClusterRestartIT extends ParameterizedFullClusterRestartTestCas
         return cluster;
     }
 
-    @Ignore("https://github.com/elastic/elasticsearch/issues/104470")
+    @AwaitsFix(bugUrl="https://github.com/elastic/elasticsearch/issues/104470")
     public void testBehavioralAnalyticsDataRetention() throws Exception {
         assumeTrue(
             "Data retention changed by default to DSL in " + DSL_DEFAULT_RETENTION_VERSION,

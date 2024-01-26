10ad51a8f1870325d2c834434a584ba568c47424
Niels Bauman <nielsbauman@gmail.com>
2024-01-26 11:27:36 +0100

    Add human-readable dates in ILM explain API response

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/PhaseExecutionInfo.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/PhaseExecutionInfo.java
@@ -29,6 +29,7 @@ public class PhaseExecutionInfo implements ToXContentObject, Writeable {
     private static final ParseField PHASE_DEFINITION_FIELD = new ParseField("phase_definition");
     private static final ParseField VERSION_FIELD = new ParseField("version");
     private static final ParseField MODIFIED_DATE_IN_MILLIS_FIELD = new ParseField("modified_date_in_millis");
+    private static final ParseField MODIFIED_DATE_FIELD = new ParseField("modified_date");
 
     private static final ConstructingObjectParser<PhaseExecutionInfo, String> PARSER = new ConstructingObjectParser<>(
         "phase_execution_info",
@@ -40,6 +41,7 @@ public class PhaseExecutionInfo implements ToXContentObject, Writeable {
         PARSER.declareObject(ConstructingObjectParser.optionalConstructorArg(), Phase::parse, PHASE_DEFINITION_FIELD);
         PARSER.declareLong(ConstructingObjectParser.constructorArg(), VERSION_FIELD);
         PARSER.declareLong(ConstructingObjectParser.constructorArg(), MODIFIED_DATE_IN_MILLIS_FIELD);
+        PARSER.declareString(ConstructingObjectParser.optionalConstructorArg(), MODIFIED_DATE_FIELD);
     }
 
     public static PhaseExecutionInfo parse(XContentParser parser, String name) {
@@ -130,7 +132,8 @@ public class PhaseExecutionInfo implements ToXContentObject, Writeable {
             builder.field(PHASE_DEFINITION_FIELD.getPreferredName(), phase);
         }
         builder.field(VERSION_FIELD.getPreferredName(), version);
-        builder.timeField(MODIFIED_DATE_IN_MILLIS_FIELD.getPreferredName(), "modified_date", modifiedDate);
+        builder.field(MODIFIED_DATE_IN_MILLIS_FIELD.getPreferredName(), modifiedDate);
+        builder.timeField(MODIFIED_DATE_FIELD.getPreferredName(), modifiedDate);
         builder.endObject();
         return builder;
     }

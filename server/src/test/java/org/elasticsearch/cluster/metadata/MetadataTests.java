15e30d2b5c39e4e13c96b3a29b3d0bf9501cd73b
Matt Culbreth <matt.culbreth@elastic.co>
2024-01-29 18:27:07 -0500

    Change to `containsInAnyOrder` in assertions for lists

--- server/src/test/java/org/elasticsearch/cluster/metadata/MetadataTests.java
+++ server/src/test/java/org/elasticsearch/cluster/metadata/MetadataTests.java
@@ -197,6 +197,7 @@ public class MetadataTests extends ESTestCase {
             Map<String, List<DataStreamAlias>> aliases = metadata.findDataStreamAliases(request.aliases(), new String[] { "index", "d2" });
             assertEquals(1, aliases.size());
             List<DataStreamAlias> found = aliases.get("d2");
+            assertThat(found, transformedItemsMatch(DataStreamAlias::getAlias, containsInAnyOrder("alias2", "alias2-part2")));
         }
 
         // test exclusion
@@ -206,10 +207,12 @@ public class MetadataTests extends ESTestCase {
                 request.aliases(),
                 new String[] { "index", "d1", "d2", "d3", "d4" }
             );
+            assertThat(aliases.get("d2"), transformedItemsMatch(DataStreamAlias::getAlias, containsInAnyOrder("alias2", "alias2-part2")));
             assertThat(aliases.get("d1"), transformedItemsMatch(DataStreamAlias::getAlias, contains("alias1")));
 
             request.aliases("*", "-alias1");
             aliases = metadata.findDataStreamAliases(request.aliases(), new String[] { "index", "d1", "d2", "d3", "d4" });
+            assertThat(aliases.get("d2"), transformedItemsMatch(DataStreamAlias::getAlias, containsInAnyOrder("alias2", "alias2-part2")));
             assertNull(aliases.get("d1"));
         }
     }
@@ -237,6 +240,8 @@ public class MetadataTests extends ESTestCase {
 
         assertEquals(1, aliases.get("d1").size());
         assertEquals(2, aliases.get("d2").size());
+
+        assertThat(aliases.get("d2"), transformedItemsMatch(DataStreamAlias::getAlias, containsInAnyOrder("alias2", "alias2-part2")));
     }
 
     public void testFindAliasWithExclusion() {

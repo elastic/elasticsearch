7d1f80019bf221712352409a6175b403d5561830
Chenhui Wang <wangch079@outlook.com>
2024-01-29 18:57:47 +0800

    Add job type support for List connector sync jobs API

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/connector/syncjob/action/ListConnectorSyncJobsAction.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/connector/syncjob/action/ListConnectorSyncJobsAction.java
@@ -21,10 +21,12 @@ import org.elasticsearch.xcontent.XContentParser;
 import org.elasticsearch.xpack.application.connector.ConnectorSyncStatus;
 import org.elasticsearch.xpack.application.connector.syncjob.ConnectorSyncJob;
 import org.elasticsearch.xpack.application.connector.syncjob.ConnectorSyncJobSearchResult;
+import org.elasticsearch.xpack.application.connector.syncjob.ConnectorSyncJobType;
 import org.elasticsearch.xpack.core.action.util.PageParams;
 import org.elasticsearch.xpack.core.action.util.QueryPage;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -44,18 +46,26 @@ public class ListConnectorSyncJobsAction {
         private final PageParams pageParams;
         private final String connectorId;
         private final ConnectorSyncStatus connectorSyncStatus;
+        private final List<ConnectorSyncJobType> connectorSyncJobTypeList;
 
         public Request(StreamInput in) throws IOException {
             super(in);
             this.pageParams = new PageParams(in);
             this.connectorId = in.readOptionalString();
             this.connectorSyncStatus = in.readOptionalEnum(ConnectorSyncStatus.class);
+            this.connectorSyncJobTypeList = stringToEnumList(in.readOptionalStringCollectionAsList());
         }
 
-        public Request(PageParams pageParams, String connectorId, ConnectorSyncStatus connectorSyncStatus) {
+        public Request(
+            PageParams pageParams,
+            String connectorId,
+            ConnectorSyncStatus connectorSyncStatus,
+            List<ConnectorSyncJobType> connectorSyncJobTypeList
+        ) {
             this.pageParams = pageParams;
             this.connectorId = connectorId;
             this.connectorSyncStatus = connectorSyncStatus;
+            this.connectorSyncJobTypeList = connectorSyncJobTypeList;
         }
 
         public PageParams getPageParams() {
@@ -70,6 +80,10 @@ public class ListConnectorSyncJobsAction {
             return connectorSyncStatus;
         }
 
+        public List<ConnectorSyncJobType> getConnectorSyncJobTypeList() {
+            return connectorSyncJobTypeList;
+        }
+
         @Override
         public ActionRequestValidationException validate() {
             // Pagination validation is done as part of PageParams constructor
@@ -82,6 +96,7 @@ public class ListConnectorSyncJobsAction {
             pageParams.writeTo(out);
             out.writeOptionalString(connectorId);
             out.writeOptionalEnum(connectorSyncStatus);
+            out.writeOptionalStringCollection(enumToStringList(connectorSyncJobTypeList));
         }
 
         @Override
@@ -91,12 +106,13 @@ public class ListConnectorSyncJobsAction {
             Request request = (Request) o;
             return Objects.equals(pageParams, request.pageParams)
                 && Objects.equals(connectorId, request.connectorId)
-                && connectorSyncStatus == request.connectorSyncStatus;
+                && connectorSyncStatus == request.connectorSyncStatus
+                && connectorSyncJobTypeList == request.connectorSyncJobTypeList;
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(pageParams, connectorId, connectorSyncStatus);
+            return Objects.hash(pageParams, connectorId, connectorSyncStatus, connectorSyncJobTypeList);
         }
 
         private static final ConstructingObjectParser<ListConnectorSyncJobsAction.Request, String> PARSER = new ConstructingObjectParser<>(
@@ -104,7 +120,8 @@ public class ListConnectorSyncJobsAction {
             p -> new ListConnectorSyncJobsAction.Request(
                 (PageParams) p[0],
                 (String) p[1],
-                p[2] != null ? ConnectorSyncStatus.fromString((String) p[2]) : null
+                p[2] != null ? ConnectorSyncStatus.fromString((String) p[2]) : null,
+                p[3] != null ? Arrays.stream(((String) p[3]).split(",")).map(ConnectorSyncJobType::fromString).toList() : null
             )
         );
 
@@ -112,6 +129,7 @@ public class ListConnectorSyncJobsAction {
             PARSER.declareObject(constructorArg(), (p, c) -> PageParams.fromXContent(p), PAGE_PARAMS_FIELD);
             PARSER.declareString(optionalConstructorArg(), CONNECTOR_ID_FIELD);
             PARSER.declareString(optionalConstructorArg(), ConnectorSyncJob.STATUS_FIELD);
+            PARSER.declareString(optionalConstructorArg(), ConnectorSyncJob.JOB_TYPE_FIELD);
         }
 
         public static ListConnectorSyncJobsAction.Request parse(XContentParser parser) {
@@ -125,9 +143,24 @@ public class ListConnectorSyncJobsAction {
                 builder.field(PAGE_PARAMS_FIELD.getPreferredName(), pageParams);
                 builder.field(CONNECTOR_ID_FIELD.getPreferredName(), connectorId);
                 builder.field(ConnectorSyncJob.STATUS_FIELD.getPreferredName(), connectorSyncStatus);
+                builder.field(ConnectorSyncJob.JOB_TYPE_FIELD.getPreferredName(), connectorSyncJobTypeList);
             }
             builder.endObject();
             return builder;
+        }
+
+        private List<ConnectorSyncJobType> stringToEnumList(List<String> jobTypeList) {
+            if (jobTypeList == null) {
+                return null;
+            }
+            return jobTypeList.stream().map(ConnectorSyncJobType::fromString).toList();
+        }
+
+        private List<String> enumToStringList(List<ConnectorSyncJobType> jobTypeList) {
+            if (jobTypeList == null) {
+                return null;
+            }
+            return jobTypeList.stream().map(ConnectorSyncJobType::name).toList();
         }
     }
 

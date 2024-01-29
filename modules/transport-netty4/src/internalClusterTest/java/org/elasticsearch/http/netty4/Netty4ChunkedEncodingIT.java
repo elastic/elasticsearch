baf58cc50a7d3e863cbcb6695e7ebee13698f29d
David Turner <david.turner@elastic.co>
2024-01-29 08:37:06 +0000

    Add test to verify the case where all chunks are empty

--- modules/transport-netty4/src/internalClusterTest/java/org/elasticsearch/http/netty4/Netty4ChunkedEncodingIT.java
+++ modules/transport-netty4/src/internalClusterTest/java/org/elasticsearch/http/netty4/Netty4ChunkedEncodingIT.java
@@ -10,12 +10,11 @@ package org.elasticsearch.http.netty4;
 
 import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.ESNetty4IntegTestCase;
-import org.elasticsearch.action.ActionRequest;
-import org.elasticsearch.action.ActionRequestValidationException;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.bytes.ReleasableBytesReference;
@@ -32,6 +31,7 @@ import org.elasticsearch.plugins.ActionPlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.ChunkedRestResponseBody;
+import org.elasticsearch.rest.RestChannel;
 import org.elasticsearch.rest.RestController;
 import org.elasticsearch.rest.RestHandler;
 import org.elasticsearch.rest.RestRequest;
@@ -62,17 +62,27 @@ public class Netty4ChunkedEncodingIT extends ESNetty4IntegTestCase {
         return false; // enable http
     }
 
-    private static final String expectedBody = """
+    private static final String EXPECTED_NONEMPTY_BODY = """
         chunk-0
         chunk-1
         chunk-2
         """;
 
-    public void testBasic() throws IOException {
-        final var response = getRestClient().performRequest(new Request("GET", YieldsChunksPlugin.ROUTE));
+    public void testNonemptyResponse() throws IOException {
+        getAndCheckBodyContents(YieldsChunksPlugin.CHUNKS_ROUTE, EXPECTED_NONEMPTY_BODY);
+    }
+
+    public void testEmptyResponse() throws IOException {
+        getAndCheckBodyContents(YieldsChunksPlugin.EMPTY_ROUTE, "");
+    }
+
+    private static void getAndCheckBodyContents(String chunksRoute, String expectedBody) throws IOException {
+        final var response = getRestClient().performRequest(new Request("GET", chunksRoute));
         assertEquals(200, response.getStatusLine().getStatusCode());
         assertThat(response.getEntity().getContentType().toString(), containsString(TEXT_CONTENT_TYPE));
-        assertTrue(response.getEntity().isChunked());
+        if (Strings.hasLength(expectedBody)) {
+            assertTrue(response.getEntity().isChunked());
+        } // else we might have no chunks to send which doesn't need chunked-encoding
         final String body;
         try (var reader = new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8)) {
             body = Streams.copyToString(reader);
@@ -81,14 +91,8 @@ public class Netty4ChunkedEncodingIT extends ESNetty4IntegTestCase {
     }
 
     public static class YieldsChunksPlugin extends Plugin implements ActionPlugin {
-        static final String ROUTE = "/_test/yields_chunks";
-
-        public static class Request extends ActionRequest {
-            @Override
-            public ActionRequestValidationException validate() {
-                return null;
-            }
-        }
+        static final String CHUNKS_ROUTE = "/_test/yields_chunks";
+        static final String EMPTY_ROUTE = "/_test/yields_only_empty_chunks";
 
         private static Iterator<BytesReference> emptyChunks() {
             return Iterators.forRange(0, between(0, 2), i -> BytesArray.EMPTY);
@@ -108,44 +112,63 @@ public class Netty4ChunkedEncodingIT extends ESNetty4IntegTestCase {
             return List.of(new BaseRestHandler() {
                 @Override
                 public String getName() {
-                    return ROUTE;
+                    return CHUNKS_ROUTE;
                 }
 
                 @Override
                 public List<Route> routes() {
-                    return List.of(new Route(GET, ROUTE));
+                    return List.of(new Route(GET, CHUNKS_ROUTE));
                 }
 
                 @Override
                 protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) {
-                    return channel -> channel.sendResponse(RestResponse.chunked(RestStatus.OK, new ChunkedRestResponseBody() {
-
-                        final Iterator<BytesReference> chunkIterator = Iterators.concat(
+                    return channel -> sendChunksResponse(
+                        channel,
+                        Iterators.concat(
                             emptyChunks(),
                             Iterators.flatMap(
                                 Iterators.forRange(0, 3, i -> "chunk-" + i + '\n'),
                                 chunk -> Iterators.concat(Iterators.single(new BytesArray(chunk)), emptyChunks())
                             )
-                        );
+                        )
+                    );
+                }
+            }, new BaseRestHandler() {
+                @Override
+                public String getName() {
+                    return EMPTY_ROUTE;
+                }
 
-                        @Override
-                        public boolean isDone() {
-                            return chunkIterator.hasNext() == false;
-                        }
+                @Override
+                public List<Route> routes() {
+                    return List.of(new Route(GET, EMPTY_ROUTE));
+                }
 
-                        @Override
-                        public ReleasableBytesReference encodeChunk(int sizeHint, Recycler<BytesRef> recycler) {
-                            final var page = recycler.obtain(); // just to ensure nothing is leaked
-                            return new ReleasableBytesReference(chunkIterator.next(), page);
-                        }
-
-                        @Override
-                        public String getResponseContentTypeString() {
-                            return TEXT_CONTENT_TYPE;
-                        }
-                    }, null));
+                @Override
+                protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) {
+                    return channel -> sendChunksResponse(channel, emptyChunks());
                 }
             });
+        }
+
+        private static void sendChunksResponse(RestChannel channel, Iterator<BytesReference> chunkIterator) {
+            channel.sendResponse(RestResponse.chunked(RestStatus.OK, new ChunkedRestResponseBody() {
+                @Override
+                public boolean isDone() {
+                    return chunkIterator.hasNext() == false;
+                }
+
+                @Override
+                public ReleasableBytesReference encodeChunk(int sizeHint, Recycler<BytesRef> recycler) {
+                    final var page = recycler.obtain(); // just to ensure nothing is leaked
+                    return new ReleasableBytesReference(chunkIterator.next(), page);
+                }
+
+                @Override
+                public String getResponseContentTypeString() {
+                    return TEXT_CONTENT_TYPE;
+                }
+            }, null));
         }
     }
 }

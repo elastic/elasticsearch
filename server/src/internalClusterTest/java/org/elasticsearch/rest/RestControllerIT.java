9abbcc5b1b676bd9bafcc69a80728ee68b2c2693
David Turner <david.turner@elastic.co>
2024-01-29 08:26:51 +0000

    Fix compile

--- server/src/internalClusterTest/java/org/elasticsearch/rest/RestControllerIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/rest/RestControllerIT.java
@@ -13,7 +13,6 @@ import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.common.collect.Iterators;
-import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.IndexScopedSettings;
 import org.elasticsearch.common.settings.Settings;
@@ -53,9 +52,8 @@ public class RestControllerIT extends ESIntegTestCase {
         static final String HEADER_VALUE = "test-header-value";
 
         @Override
-        public Collection<RestHandler> getRestHandlers(
+        public List<RestHandler> getRestHandlers(
             Settings settings,
-            NamedWriteableRegistry namedWriteableRegistry,
             RestController restController,
             ClusterSettings clusterSettings,
             IndexScopedSettings indexScopedSettings,
@@ -82,9 +80,9 @@ public class RestControllerIT extends ESIntegTestCase {
                             ChunkedRestResponseBody.fromXContent(
                                 params -> Iterators.single((b, p) -> b.startObject().endObject()),
                                 request,
-                                channel
-                            ),
-                            null
+                                channel,
+                                null
+                            )
                         );
                         response.addHeader(HEADER_NAME, HEADER_VALUE);
                         channel.sendResponse(response);

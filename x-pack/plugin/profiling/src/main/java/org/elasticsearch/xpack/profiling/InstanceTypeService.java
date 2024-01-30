f51ae81a28a1303d50ed167c5fc48a5cf6d75870
Tim Rühsen <tim.ruhsen@elastic.co>
2024-01-30 13:10:10 +0100

    [Profiling] Read Azure costs data

--- x-pack/plugin/profiling/src/main/java/org/elasticsearch/xpack/profiling/InstanceTypeService.java
+++ x-pack/plugin/profiling/src/main/java/org/elasticsearch/xpack/profiling/InstanceTypeService.java
@@ -27,33 +27,37 @@ public final class InstanceTypeService {
         private static final Map<InstanceType, CostEntry> costsPerDatacenter;
 
         static {
-            final Map<Object, Object> objects = new HashMap<>();
+            final Map<Object, Object> objects = new HashMap<>(1024);
             final Function<String, String> dedupString = s -> (String) objects.computeIfAbsent(s, Function.identity());
-            final Map<InstanceType, CostEntry> tmp = new HashMap<>();
-            try (
-                GZIPInputStream in = new GZIPInputStream(
-                    InstanceTypeService.class.getClassLoader().getResourceAsStream("profiling-costs.json.gz")
-                );
-                XContentParser parser = XContentType.JSON.xContent().createParser(XContentParserConfiguration.EMPTY, in)
-            ) {
-                if (parser.currentToken() == null) {
-                    parser.nextToken();
+            final Map<InstanceType, CostEntry> tmp = new HashMap<>(16 * 1024);
+
+            // All files ar expected to exist.
+            for (String provider : List.of("aws", "azure")) {
+                String name = "profiling-costs-" + provider + ".json.gz";
+                try (
+                    GZIPInputStream in = new GZIPInputStream(InstanceTypeService.class.getClassLoader().getResourceAsStream(name));
+                    XContentParser parser = XContentType.JSON.xContent().createParser(XContentParserConfiguration.EMPTY, in)
+                ) {
+                    if (parser.currentToken() == null) {
+                        parser.nextToken();
+                    }
+                    List<Map<String, Object>> rawData = XContentParserUtils.parseList(parser, XContentParser::map);
+                    for (Map<String, Object> entry : rawData) {
+                        tmp.put(
+                            new InstanceType(
+                                dedupString.apply((String) entry.get("provider")),
+                                dedupString.apply((String) entry.get("region")),
+                                dedupString.apply((String) entry.get("instance_type"))
+                            ),
+                            (CostEntry) objects.computeIfAbsent(CostEntry.fromSource(entry), Function.identity())
+                        );
+                    }
+                } catch (IOException e) {
+                    throw new ExceptionInInitializerError(e);
                 }
-                List<Map<String, Object>> rawData = XContentParserUtils.parseList(parser, XContentParser::map);
-                for (Map<String, Object> entry : rawData) {
-                    tmp.put(
-                        new InstanceType(
-                            dedupString.apply((String) entry.get("provider")),
-                            dedupString.apply((String) entry.get("region")),
-                            dedupString.apply((String) entry.get("instance_type"))
-                        ),
-                        (CostEntry) objects.computeIfAbsent(CostEntry.fromSource(entry), Function.identity())
-                    );
-                }
-                costsPerDatacenter = Map.copyOf(tmp);
-            } catch (IOException e) {
-                throw new ExceptionInInitializerError(e);
             }
+
+            costsPerDatacenter = Map.copyOf(tmp);
         }
     }
 

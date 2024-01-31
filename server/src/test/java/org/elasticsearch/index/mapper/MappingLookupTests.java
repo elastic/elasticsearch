43dad29c7ce39c8e88bf9ec3b64dddc2b73023a6
Matteo Piergiovanni <134913285+piergm@users.noreply.github.com>
2024-01-31 12:19:11 +0100

    refactor fake

--- server/src/test/java/org/elasticsearch/index/mapper/MappingLookupTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/MappingLookupTests.java
@@ -255,13 +255,16 @@ public class MappingLookupTests extends ESTestCase {
         final String indexedValue;
 
         FakeFieldMapper(FakeFieldType fieldType, String indexedValue) {
-            super(fieldType.name(), fieldType, MultiFields.empty(), CopyTo.empty());
+            super(fieldType.concreteFieldName(), fieldType, MultiFields.empty(), CopyTo.empty());
             this.indexedValue = indexedValue;
         }
 
         @Override
         public Map<String, NamedAnalyzer> indexAnalyzers() {
-            return Map.of(mappedFieldType.name(), new NamedAnalyzer("fake", AnalyzerScope.INDEX, new FakeAnalyzer(indexedValue)));
+            return Map.of(
+                mappedFieldType.concreteFieldName(),
+                new NamedAnalyzer("fake", AnalyzerScope.INDEX, new FakeAnalyzer(indexedValue))
+            );
         }
 
         @Override

421ae2145198cf7ba397a1d7a8d7f3bd673eedbf
Ryan Ernst <ryan@iernst.net>
2024-02-01 12:49:27 -0800

    load linux libc without native mapping

--- libs/native/jna/src/main/java/org/elasticsearch/nativeaccess/jna/JnaLinuxCLibrary.java
+++ libs/native/jna/src/main/java/org/elasticsearch/nativeaccess/jna/JnaLinuxCLibrary.java
@@ -8,13 +8,90 @@
 
 package org.elasticsearch.nativeaccess.jna;
 
+import com.sun.jna.Library;
+import com.sun.jna.Memory;
+import com.sun.jna.Native;
 import com.sun.jna.NativeLong;
+import com.sun.jna.Pointer;
+import com.sun.jna.Structure;
 
-import org.elasticsearch.nativeaccess.jna.JnaStaticLinuxCLibrary.JnaSockFProg;
-import org.elasticsearch.nativeaccess.jna.JnaStaticLinuxCLibrary.JnaStatx;
 import org.elasticsearch.nativeaccess.lib.LinuxCLibrary;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+import java.util.List;
+
 class JnaLinuxCLibrary implements LinuxCLibrary {
+
+    /** corresponds to struct statx */
+    @Structure.FieldOrder({ "_ignore1", "stx_blocks", "_ignore2" })
+    public static final class JnaStatx extends Structure implements Structure.ByReference, statx {
+        // ignoring bytes up to stx_blocks, including 2 bytes padding after stx_mode for alignment
+        public byte[] _ignore1 = new byte[48];
+        public long stx_blocks = 0;
+        // ignoring bytes after stx_blocks. statx_timestamp is 16 bytes each for alignment
+        public byte[] _ignore2 = new byte[104];
+
+        @Override
+        public long stx_blocks() {
+            return stx_blocks;
+        }
+    }
+
+    static final class JnaSockFProg extends Structure implements Structure.ByReference, SockFProg {
+        public short len;           // number of filters
+        public Pointer filter;        // filters
+
+        JnaSockFProg(SockFilter filters[]) {
+            len = (short) filters.length;
+            // serialize struct sock_filter * explicitly, its less confusing than the JNA magic we would need
+            Memory filter = new Memory(len * 8);
+            ByteBuffer bbuf = filter.getByteBuffer(0, len * 8);
+            bbuf.order(ByteOrder.nativeOrder()); // little endian
+            for (SockFilter f : filters) {
+                bbuf.putShort(f.code());
+                bbuf.put(f.jt());
+                bbuf.put(f.jf());
+                bbuf.putInt(f.k());
+            }
+            this.filter = filter;
+        }
+
+        @Override
+        protected List<String> getFieldOrder() {
+            return Arrays.asList("len", "filter");
+        }
+
+        @Override
+        public long address() {
+            return Pointer.nativeValue(getPointer());
+        }
+    }
+
+    private interface NativeFunctions extends Library {
+        int statx(int dirfd, String pathname, int flags, int mask, Pointer statxbuf);
+
+        /**
+         * maps to prctl(2)
+         */
+        int prctl(int option, NativeLong arg2, NativeLong arg3, NativeLong arg4, NativeLong arg5);
+
+        /**
+         * used to call seccomp(2), its too new...
+         * this is the only way, DON'T use it on some other architecture unless you know wtf you are doing
+         */
+        NativeLong syscall(NativeLong number, Object... args);
+
+        int fallocate(int fd, int mode, long offset, long length);
+    }
+
+    private final NativeFunctions functions;
+
+    JnaLinuxCLibrary() {
+        this.functions = Native.load("c", NativeFunctions.class);
+    }
+
     @Override
     public statx newStatx() {
         return new JnaStatx();
@@ -24,7 +101,7 @@ class JnaLinuxCLibrary implements LinuxCLibrary {
     public int statx(int dirfd, String pathname, int flags, int mask, statx statxbuf) {
         assert statxbuf instanceof JnaStatx;
         var jnaStatxbuf = (JnaStatx) statxbuf;
-        return JnaStaticLinuxCLibrary.statx(dirfd, pathname, flags, mask, jnaStatxbuf);
+        return functions.statx(dirfd, pathname, flags, mask, jnaStatxbuf.getPointer());
     }
 
     @Override
@@ -36,16 +113,16 @@ class JnaLinuxCLibrary implements LinuxCLibrary {
 
     @Override
     public int prctl(int option, long arg2, long arg3, long arg4, long arg5) {
-        return JnaStaticLinuxCLibrary.prctl(option, new NativeLong(arg2), new NativeLong(arg3), new NativeLong(arg4), new NativeLong(arg5));
+        return functions.prctl(option, new NativeLong(arg2), new NativeLong(arg3), new NativeLong(arg4), new NativeLong(arg5));
     }
 
     @Override
     public long syscall(long number, int operation, int flags, long address) {
-        return JnaStaticLinuxCLibrary.syscall(new NativeLong(number), operation, flags, address).longValue();
+        return functions.syscall(new NativeLong(number), operation, flags, address).longValue();
     }
 
     @Override
     public int fallocate(int fd, int mode, long offset, long length) {
-        return JnaStaticLinuxCLibrary.fallocate(fd, mode, offset, length);
+        return functions.fallocate(fd, mode, offset, length);
     }
 }

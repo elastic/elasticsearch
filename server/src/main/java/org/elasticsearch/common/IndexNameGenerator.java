6bf1fedf68a77b0a33066503b84aa73c6871d9f9
Dmitry Cherniachenko <2sabio@gmail.com>
2024-02-05 11:56:04 +0100

    Remove a copy of IndexNameGenerator.generateValidIndexName()

--- server/src/main/java/org/elasticsearch/common/IndexNameGenerator.java
+++ server/src/main/java/org/elasticsearch/common/IndexNameGenerator.java
@@ -16,13 +16,14 @@ import org.elasticsearch.indices.InvalidIndexNameException;
 
 import java.util.Locale;
 import java.util.function.Supplier;
+import java.util.regex.Pattern;
 
 /**
  * Generates valid Elasticsearch index names.
  */
 public final class IndexNameGenerator {
 
-    public static final String ILLEGAL_INDEXNAME_CHARS_REGEX = "[/:\"*?<>|# ,\\\\]+";
+    public static final Pattern ILLEGAL_INDEXNAME_CHARS_REGEX = Pattern.compile("[/:\"*?<>|# ,\\\\]+");
     public static final int MAX_GENERATED_UUID_LENGTH = 4;
 
     private IndexNameGenerator() {}
@@ -37,14 +38,15 @@ public final class IndexNameGenerator {
     }
 
     /**
-     *
-     * @param randomGenerator
-     * @return
+     * Generates a valid index suffix, removing any illegal characters.
+     * @param randomGenerator supplier of the initial (random) suffix
+     * @return a valid index suffix without any illegal characters
+     * @throws IllegalArgumentException if removing illegal characters resulted in an empty string
      */
     public static String generateValidIndexSuffix(Supplier<String> randomGenerator) {
         String randomSuffix = randomGenerator.get().toLowerCase(Locale.ROOT);
-        randomSuffix = randomSuffix.replaceAll(ILLEGAL_INDEXNAME_CHARS_REGEX, "");
-        if (randomSuffix.length() == 0) {
+        randomSuffix = ILLEGAL_INDEXNAME_CHARS_REGEX.matcher(randomSuffix).replaceAll("");
+        if (randomSuffix.isEmpty()) {
             throw new IllegalArgumentException("unable to generate random index name suffix");
         }
 

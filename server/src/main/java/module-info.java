43362d5de5feead1e925a8bb2cb532fb0eec3521
Benjamin Trent <ben.w.trent@gmail.com>
2024-02-05 12:56:13 -0500

    Add new int8_flat and flat vector index types (#104872)
    This adds two new vector index types:  - flat   - int8_flat
    
    Both store the vectors in a flat space and search is brute-force over
    the vectors in the index.   For the regular `flat` index, this can be
    considered syntactic sugar that allows `knn` queries without having to
    put indices within HNSW. 
    
    For `int8_flat`, this allows float vectors to be stored in a flat
    manner, but also automatically quantized.

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -424,6 +424,10 @@ module org.elasticsearch.server {
             org.elasticsearch.index.codec.bloomfilter.ES87BloomFilterPostingsFormat,
             org.elasticsearch.index.codec.postings.ES812PostingsFormat;
     provides org.apache.lucene.codecs.DocValuesFormat with ES87TSDBDocValuesFormat;
+    provides org.apache.lucene.codecs.KnnVectorsFormat
+        with
+            org.elasticsearch.index.codec.vectors.ES813FlatVectorFormat,
+            org.elasticsearch.index.codec.vectors.ES813Int8FlatVectorFormat;
 
     exports org.elasticsearch.cluster.routing.allocation.shards
         to

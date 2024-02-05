2c55cf580459f0150720e2157191a010e6ef2557
Jedr Blaszyk <jedrazb@gmail.com>
2024-02-05 15:17:29 +0100

    [Connectors API] Fix bug with crawler configuration parsing and sync_now flag (#105024) (#105139)

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/connector/Connector.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/connector/Connector.java
@@ -682,8 +682,8 @@ public class Connector implements NamedWriteable, ToXContentObject {
             return this;
         }
 
-        public Builder setSyncNow(boolean syncNow) {
-            this.syncNow = syncNow;
+        public Builder setSyncNow(Boolean syncNow) {
+            this.syncNow = Objects.requireNonNullElse(syncNow, false);
             return this;
         }
 

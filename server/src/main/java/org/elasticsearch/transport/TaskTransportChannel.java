c7bc371271621c918e2bc450ebbde87dfc57cad3
Nhat Nguyen <nhat.nguyen@elastic.co>
2024-02-07 11:32:26 -0800

    Revert "Relax release once assertion in TaskTransportChannel"
    This reverts commit 71a7dd167fdacde5cad3914216f644961c294999.

--- server/src/main/java/org/elasticsearch/transport/TaskTransportChannel.java
+++ server/src/main/java/org/elasticsearch/transport/TaskTransportChannel.java
@@ -10,22 +10,20 @@ package org.elasticsearch.transport;
 
 import org.elasticsearch.TransportVersion;
 import org.elasticsearch.common.Strings;
-import org.elasticsearch.core.Assertions;
 import org.elasticsearch.core.Releasable;
 
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TaskTransportChannel implements TransportChannel {
 
     private final long taskId;
     private final TransportChannel channel;
-    private final ReleaseAfter onTaskFinished;
+    private final Releasable onTaskFinished;
 
     TaskTransportChannel(long taskId, TransportChannel channel, Releasable onTaskFinished) {
         this.taskId = taskId;
         this.channel = channel;
-        this.onTaskFinished = wrapAsReleaseAfter(onTaskFinished);
+        this.onTaskFinished = onTaskFinished;
     }
 
     @Override
@@ -38,7 +36,7 @@ public class TaskTransportChannel implements TransportChannel {
         try {
             channel.sendResponse(response);
         } finally {
-            onTaskFinished.release(false);
+            onTaskFinished.close();
         }
     }
 
@@ -47,7 +45,7 @@ public class TaskTransportChannel implements TransportChannel {
         try {
             channel.sendResponse(exception);
         } finally {
-            onTaskFinished.release(true);
+            onTaskFinished.close();
         }
     }
 
@@ -63,45 +61,5 @@ public class TaskTransportChannel implements TransportChannel {
     @Override
     public String toString() {
         return Strings.format("TaskTransportChannel{task=%d}{%s}", taskId, channel);
-    }
-
-    private ReleaseAfter wrapAsReleaseAfter(Releasable releasable) {
-        if (Assertions.ENABLED) {
-            return new AssertingReleaseAfter(releasable);
-        } else {
-            return ignored -> releasable.close();
-        }
-    }
-
-    /**
-     * Allowing the release method to be called only once, or twice with the isExceptionResponse parameter is false, followed by true.
-     */
-    private interface ReleaseAfter {
-        void release(boolean isExceptionResponse);
-    }
-
-    private static class AssertingReleaseAfter implements ReleaseAfter {
-        private volatile Exception releaseByOnFailure;
-        private volatile Exception releaseByOnResponse;
-        private final Releasable releasable;
-        private final AtomicBoolean released = new AtomicBoolean();
-
-        AssertingReleaseAfter(Releasable releasable) {
-            this.releasable = releasable;
-        }
-
-        @Override
-        public void release(boolean isExceptionResponse) {
-            assert releaseByOnFailure == null : new AssertionError("already released on failure", releaseByOnFailure);
-            if (isExceptionResponse) {
-                releaseByOnFailure = new Exception();
-            } else {
-                assert releaseByOnResponse == null : new AssertionError("already released on response", releaseByOnResponse);
-                releaseByOnResponse = new Exception();
-            }
-            if (released.compareAndSet(false, true)) {
-                releasable.close();
-            }
-        }
     }
 }

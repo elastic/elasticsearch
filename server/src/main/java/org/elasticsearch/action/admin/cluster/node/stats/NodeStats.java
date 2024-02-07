a392cdf580173b195cc99196656a4a2da594d33e
David Turner <david.turner@elastic.co>
2024-02-07 07:11:35 +0000

    Neaterer

--- server/src/main/java/org/elasticsearch/action/admin/cluster/node/stats/NodeStats.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/node/stats/NodeStats.java
@@ -304,7 +304,7 @@ public class NodeStats extends BaseNodeResponse implements ChunkedToXContent {
 
         return Iterators.concat(
 
-            Iterators.single((builder, params) -> {
+            singleChunk((builder, params) -> {
                 builder.field("name", getNode().getName());
                 builder.field("transport_address", getNode().getAddress().toString());
                 builder.field("host", getNode().getHostName());
@@ -343,8 +343,7 @@ public class NodeStats extends BaseNodeResponse implements ChunkedToXContent {
             ifPresent(getIngestStats()).toXContentChunked(outerParams),
             singleChunk(ifPresent(getAdaptiveSelectionStats())),
             ifPresent(getScriptCacheStats()).toXContentChunked(outerParams),
-            singleChunk(ifPresent(getIndexingPressureStats())),
-            singleChunk(ifPresent(getRepositoriesStats()))
+            singleChunk((builder, p) -> builder.value(ifPresent(getIndexingPressureStats()), p).value(ifPresent(getRepositoriesStats()), p))
         );
     }
 

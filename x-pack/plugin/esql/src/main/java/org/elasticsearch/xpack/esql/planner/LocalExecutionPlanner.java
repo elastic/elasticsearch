e866fe4c633b76e807c34ac93d72fd861b88242c
Dmitry Cherniachenko <2sabio@gmail.com>
2024-02-08 22:29:15 +0100

    Reuse ChannelSet in other projections with the same input

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/planner/LocalExecutionPlanner.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/planner/LocalExecutionPlanner.java
@@ -524,6 +524,7 @@ public class LocalExecutionPlanner {
                 channelSet = new Layout.ChannelSet(new HashSet<>(), input.type());
                 channelSet.nameIds().add(ne.id());
                 layout.append(channelSet);
+                inputChannelToOutputIds.put(input.channel(), channelSet);
             } else {
                 channelSet.nameIds().add(ne.id());
             }

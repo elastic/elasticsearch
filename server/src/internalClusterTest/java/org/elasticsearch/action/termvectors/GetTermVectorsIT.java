d52e5e42b6ba5a1440a0452b3fc1e787d80d8e2d
Salvatore Campagna <salvatore.campagna@elastic.co>
2024-02-09 16:31:03 +0100

    fix: _ignored field does not have terms vector anymore since it is not stored

--- server/src/internalClusterTest/java/org/elasticsearch/action/termvectors/GetTermVectorsIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/action/termvectors/GetTermVectorsIT.java
@@ -45,7 +45,6 @@ import java.util.concurrent.ExecutionException;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.elasticsearch.xcontent.XContentFactory.jsonBuilder;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
@@ -1048,9 +1047,9 @@ public class GetTermVectorsIT extends AbstractTermVectorsTestCase {
         // add a doc with a bad long field
         indexRandom(true, prepareIndex("index").setId("1").setSource("{\"field\":\"foo\"}", XContentType.JSON));
 
-        // do a tv request for all fields, _ignored should be returned
+        // do a tv request for all fields, _ignored should not be returned as a result of not being stored but just having doc values
         TermVectorsResponse resp = client().prepareTermVectors("index", "1").setSelectedFields("*").get();
-        assertThat(resp.getFields().terms("_ignored").size(), greaterThan(0L));
+        assertNull(resp.getFields().terms("_ignored"));
     }
 
     public void testWithKeywordAndNormalizer() throws IOException, ExecutionException, InterruptedException {

c3bbe3b682d45f948d76a389cec3029acb0b11e4
Martijn van Groningen <martijn.v.groningen@gmail.com>
2024-02-09 16:37:46 +0100

    iter

--- server/src/main/java/org/elasticsearch/index/mapper/RootObjectMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/RootObjectMapper.java
@@ -114,7 +114,11 @@ public class RootObjectMapper extends ObjectMapper {
             Map<String, Mapper> mappers = buildMappers(context);
 
             Map<String, Mapper> aliasMappers = new HashMap<>();
-            getAliasMappers(mappers, aliasMappers, context, 0);
+            Map<String, ObjectMapper.Builder> objectIntermediates = new HashMap<>(1);
+            getAliasMappers(mappers, aliasMappers, objectIntermediates, context, 0);
+            for (var entry : objectIntermediates.entrySet()) {
+                aliasMappers.put(entry.getKey(), entry.getValue().build(context));
+            }
             mappers.putAll(aliasMappers);
 
             return new RootObjectMapper(
@@ -131,7 +135,13 @@ public class RootObjectMapper extends ObjectMapper {
             );
         }
 
-        void getAliasMappers(Map<String, Mapper> mappers, Map<String, Mapper> aliasMappers, MapperBuilderContext context, int level) {
+        void getAliasMappers(
+            Map<String, Mapper> mappers,
+            Map<String, Mapper> aliasMappers,
+            Map<String, ObjectMapper.Builder> objectIntermediates,
+            MapperBuilderContext context,
+            int level
+        ) {
             if (level >= MAX_NESTING_LEVEL_FOR_PASS_THROUGH_OBJECTS) {
                 logger.warn("Exceeded maximum nesting level for searching for pass-through object fields within object fields.");
                 return;
@@ -139,7 +149,6 @@ public class RootObjectMapper extends ObjectMapper {
             for (Mapper mapper : mappers.values()) {
                 // Create aliases for all fields in child passthrough mappers and place them under the root object.
                 if (mapper instanceof PassThroughObjectMapper passthroughMapper) {
-                    Map<String, ObjectMapper.Builder> intermediates = new HashMap<>(1);
                     for (Mapper internalMapper : passthroughMapper.mappers.values()) {
                         if (internalMapper instanceof FieldMapper fieldMapper) {
                             // If there's a conflicting alias with the same name at the root level, we don't want to throw an error
@@ -175,7 +184,7 @@ public class RootObjectMapper extends ObjectMapper {
                                     );
                                     for (int i = fieldNameParts.length - 2; i >= 0; --i) {
                                         String intermediateObjectName = fieldNameParts[i];
-                                        ObjectMapper.Builder intermediate = intermediates.computeIfAbsent(
+                                        ObjectMapper.Builder intermediate = objectIntermediates.computeIfAbsent(
                                             intermediateObjectName,
                                             s -> new ObjectMapper.Builder(intermediateObjectName, ObjectMapper.Defaults.SUBOBJECTS)
                                         );
@@ -186,12 +195,9 @@ public class RootObjectMapper extends ObjectMapper {
                             }
                         }
                     }
-                    for (var entry : intermediates.entrySet()) {
-                        aliasMappers.put(entry.getKey(), entry.getValue().build(context));
-                    }
                 } else if (mapper instanceof ObjectMapper objectMapper) {
                     // Call recursively to check child fields. The level guards against long recursive call sequences.
-                    getAliasMappers(objectMapper.mappers, aliasMappers, context, level + 1);
+                    getAliasMappers(objectMapper.mappers, aliasMappers, objectIntermediates, context, level + 1);
                 }
             }
         }

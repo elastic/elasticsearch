f7d7ab2979b92ffadd40c791e2e6f54919302d24
Keith Massey <keith.massey@elastic.co>
2024-02-09 08:09:50 -0600

    Adding some tests for many nested pipeline processors (#105291)

---
+++ modules/ingest-common/src/internalClusterTest/java/org/elasticsearch/ingest/common/ManyNestedPipelinesIT.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.ingest.common;
+
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.ingest.SimulateDocumentBaseResult;
+import org.elasticsearch.action.ingest.SimulateDocumentResult;
+import org.elasticsearch.action.ingest.SimulateDocumentVerboseResult;
+import org.elasticsearch.action.ingest.SimulatePipelineResponse;
+import org.elasticsearch.action.ingest.SimulateProcessorResult;
+import org.elasticsearch.common.bytes.BytesArray;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.core.Strings;
+import org.elasticsearch.ingest.IngestDocument;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.test.ESIntegTestCase;
+import org.elasticsearch.xcontent.XContentType;
+import org.junit.Before;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import static org.elasticsearch.xcontent.XContentFactory.jsonBuilder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+
+@ESIntegTestCase.ClusterScope(numDataNodes = 0, numClientNodes = 0, scope = ESIntegTestCase.Scope.TEST)
+/*
+ * This test is meant to make sure that we can handle ingesting a document with a reasonably large number of nested pipeline processors.
+ */
+public class ManyNestedPipelinesIT extends ESIntegTestCase {
+    private final int manyPipelinesCount = randomIntBetween(2, 20);
+
+    @Override
+    protected Collection<Class<? extends Plugin>> nodePlugins() {
+        return Arrays.asList(IngestCommonPlugin.class);
+    }
+
+    @Before
+    public void loadManyPipelines() {
+        internalCluster().ensureAtLeastNumDataNodes(1);
+        internalCluster().startMasterOnlyNode();
+        createChainedPipelines(manyPipelinesCount);
+    }
+
+    public void testIngestManyPipelines() {
+        String index = "index";
+        DocWriteResponse response = prepareIndex(index).setSource(Map.of("foo", "bar")).setPipeline("pipeline_0").get();
+        assertThat(response.getResult(), equalTo(DocWriteResponse.Result.CREATED));
+        GetResponse getREsponse = client().prepareGet(index, response.getId()).get();
+        assertThat(getREsponse.getSource().get("foo"), equalTo("baz"));
+    }
+
+    public void testSimulateManyPipelines() throws IOException {
+        List<SimulateDocumentResult> results = executeSimulate(false);
+        assertThat(results.size(), equalTo(1));
+        assertThat(results.get(0), instanceOf(SimulateDocumentBaseResult.class));
+        SimulateDocumentBaseResult result = (SimulateDocumentBaseResult) results.get(0);
+        assertNull(result.getFailure());
+        IngestDocument resultDoc = result.getIngestDocument();
+        assertThat(resultDoc.getFieldValue("foo", String.class), equalTo("baz"));
+    }
+
+    public void testSimulateVerboseManyPipelines() throws IOException {
+        List<SimulateDocumentResult> results = executeSimulate(true);
+        assertThat(results.size(), equalTo(1));
+        assertThat(results.get(0), instanceOf(SimulateDocumentVerboseResult.class));
+        SimulateDocumentVerboseResult result = (SimulateDocumentVerboseResult) results.get(0);
+        assertThat(result.getProcessorResults().size(), equalTo(manyPipelinesCount));
+        List<SimulateProcessorResult> simulateProcessorResults = result.getProcessorResults();
+        SimulateProcessorResult lastResult = simulateProcessorResults.get(simulateProcessorResults.size() - 1);
+        IngestDocument resultDoc = lastResult.getIngestDocument();
+        assertThat(resultDoc.getFieldValue("foo", String.class), equalTo("baz"));
+    }
+
+    private List<SimulateDocumentResult> executeSimulate(boolean verbose) throws IOException {
+        BytesReference simulateRequestBytes = BytesReference.bytes(
+            jsonBuilder().startObject()
+                .startArray("docs")
+                .startObject()
+                .field("_index", "foo")
+                .field("_id", "id")
+                .startObject("_source")
+                .field("foo", "bar")
+                .endObject()
+                .endObject()
+                .endArray()
+                .endObject()
+        );
+        SimulatePipelineResponse simulatePipelineResponse = clusterAdmin().prepareSimulatePipeline(simulateRequestBytes, XContentType.JSON)
+            .setId("pipeline_0")
+            .setVerbose(verbose)
+            .get();
+        return simulatePipelineResponse.getResults();
+    }
+
+    private void createChainedPipelines(int count) {
+        for (int i = 0; i < count - 1; i++) {
+            createChainedPipeline(i);
+        }
+        createLastPipeline(count - 1);
+    }
+
+    private void createChainedPipeline(int number) {
+        String pipelineId = "pipeline_" + number;
+        String nextPipelineId = "pipeline_" + (number + 1);
+        String pipelineTemplate = """
+            {
+                "processors": [
+                    {
+                        "pipeline": {
+                            "name": "%s"
+                        }
+                    }
+                ]
+            }
+            """;
+        String pipeline = Strings.format(pipelineTemplate, nextPipelineId);
+        clusterAdmin().preparePutPipeline(pipelineId, new BytesArray(pipeline), XContentType.JSON).get();
+    }
+
+    private void createLastPipeline(int number) {
+        String pipelineId = "pipeline_" + number;
+        String pipeline = """
+            {
+                "processors": [
+                    {
+                       "set": {
+                          "field": "foo",
+                          "value": "baz"
+                       }
+                    }
+                ]
+            }
+            """;
+        clusterAdmin().preparePutPipeline(pipelineId, new BytesArray(pipeline), XContentType.JSON).get();
+    }
+}

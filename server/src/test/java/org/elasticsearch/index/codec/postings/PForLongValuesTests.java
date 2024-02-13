49e85690ec1ac720b48965ef6bffe27071bc66b2
Ignacio Vera <ivera@apache.org>
2024-02-13 11:33:21 +0100

    iter

--- server/src/test/java/org/elasticsearch/index/codec/postings/PForLongValuesTests.java
+++ server/src/test/java/org/elasticsearch/index/codec/postings/PForLongValuesTests.java
@@ -5,26 +5,6 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-
-/*
- * @notice
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Modifications copyright (C) 2022 Elasticsearch B.V.
- */
 package org.elasticsearch.index.codec.postings;
 
 import com.carrotsearch.randomizedtesting.generators.RandomNumbers;

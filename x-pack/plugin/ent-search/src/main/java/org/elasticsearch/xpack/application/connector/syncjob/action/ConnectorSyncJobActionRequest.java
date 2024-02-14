66ee37eb50870df14fd3b9fba965ff7f43df2687
Tim Grein <tim.grein@elastic.co>
2024-02-14 11:04:48 +0100

    Fix typo

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/connector/syncjob/action/ConnectorSyncJobActionRequest.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/connector/syncjob/action/ConnectorSyncJobActionRequest.java
@@ -16,7 +16,7 @@ import org.elasticsearch.xpack.application.connector.ConnectorTemplateRegistry;
 import java.io.IOException;
 
 /**
- * Abstract base class for action requests targeting the connector syn job index. Implements {@link org.elasticsearch.action.IndicesRequest}
+ * Abstract base class for action requests targeting the connector sync job index. Implements {@link org.elasticsearch.action.IndicesRequest}
  * to ensure index-level privilege support. This class defines the connectors sync job index as the target for all derived action requests.
  */
 public abstract class ConnectorSyncJobActionRequest extends ActionRequest implements IndicesRequest {

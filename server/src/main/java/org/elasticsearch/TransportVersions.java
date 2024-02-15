b03ba86ea9bdfb8491a770a51ab7e2dda21236b7
Simon Cooper <simon.cooper@elastic.co>
2024-02-15 10:02:32 +0000

    Update constant

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -197,7 +197,7 @@ public class TransportVersions {
      * Reference to the minimum transport version that can be used with CCS.
      * This should be the transport version used by the previous minor release.
      */
-    public static final TransportVersion MINIMUM_CCS_VERSION = ENRICH_ELASTICSEARCH_VERSION_REMOVED;
+    public static final TransportVersion MINIMUM_CCS_VERSION = V_8_12_0;
 
     static final NavigableMap<Integer, TransportVersion> VERSION_IDS = getAllVersionIds(TransportVersions.class);
 

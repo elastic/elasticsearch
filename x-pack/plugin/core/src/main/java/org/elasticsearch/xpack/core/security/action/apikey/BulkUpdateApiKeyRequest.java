ba66633e50156a289c42eb8908f303042db5d785
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2024-02-20 21:02:56 +0100

    WIP bulk update API

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/apikey/BulkUpdateApiKeyRequest.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/apikey/BulkUpdateApiKeyRequest.java
@@ -10,12 +10,21 @@ package org.elasticsearch.xpack.core.security.action.apikey;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.core.Nullable;
 import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.xcontent.ConstructingObjectParser;
+import org.elasticsearch.xcontent.ParseField;
+import org.elasticsearch.xcontent.XContentParser;
 import org.elasticsearch.xpack.core.security.authz.RoleDescriptor;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
+
+import static org.elasticsearch.xcontent.ConstructingObjectParser.constructorArg;
+import static org.elasticsearch.xcontent.ConstructingObjectParser.optionalConstructorArg;
 
 public final class BulkUpdateApiKeyRequest extends BaseBulkUpdateApiKeyRequest {
 
@@ -48,5 +57,49 @@ public final class BulkUpdateApiKeyRequest extends BaseBulkUpdateApiKeyRequest {
     @Override
     public ApiKey.Type getType() {
         return ApiKey.Type.REST;
+    }
+
+    public interface RequestTranslator {
+        BulkUpdateApiKeyRequest translate(RestRequest request) throws IOException;
+
+        class Default implements RequestTranslator {
+            private static final ConstructingObjectParser<BulkUpdateApiKeyRequest, Void> PARSER = initParser((n, p) -> {
+                try {
+                    return RoleDescriptor.parse(n, p, false);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            });
+
+            @SuppressWarnings("unchecked")
+            protected static ConstructingObjectParser<BulkUpdateApiKeyRequest, Void> initParser(
+                BiFunction<String, XContentParser, RoleDescriptor> roleDescriptorParser
+            ) {
+                final ConstructingObjectParser<BulkUpdateApiKeyRequest, Void> parser = new ConstructingObjectParser<>(
+                    "bulk_update_api_key_request",
+                    a -> new BulkUpdateApiKeyRequest(
+                        (List<String>) a[0],
+                        (List<RoleDescriptor>) a[1],
+                        (Map<String, Object>) a[2],
+                        TimeValue.parseTimeValue((String) a[3], null, "expiration")
+                    )
+                );
+                parser.declareStringArray(constructorArg(), new ParseField("ids"));
+                parser.declareNamedObjects(optionalConstructorArg(), (p, c, n) -> {
+                    p.nextToken();
+                    return roleDescriptorParser.apply(n, p);
+                }, new ParseField("role_descriptors"));
+                parser.declareObject(optionalConstructorArg(), (p, c) -> p.map(), new ParseField("metadata"));
+                parser.declareString(optionalConstructorArg(), new ParseField("expiration"));
+                return parser;
+            }
+
+            @Override
+            public BulkUpdateApiKeyRequest translate(RestRequest request) throws IOException {
+                try (XContentParser parser = request.contentParser()) {
+                    return PARSER.parse(parser, null);
+                }
+            }
+        }
     }
 }

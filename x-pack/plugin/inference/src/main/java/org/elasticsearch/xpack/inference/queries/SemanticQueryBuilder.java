31f9b71890f4fbe52e8754ccdebb4bf81738bcb3
Mike Pellegrini <mike.pellegrini@elastic.co>
2024-02-21 13:40:24 -0500

    Updated SemanticQueryBuilder to implement doRewrite

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/queries/SemanticQueryBuilder.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/queries/SemanticQueryBuilder.java
@@ -8,17 +8,30 @@
 package org.elasticsearch.xpack.inference.queries;
 
 import org.apache.lucene.search.Query;
+import org.apache.lucene.util.SetOnce;
 import org.elasticsearch.TransportVersion;
 import org.elasticsearch.TransportVersions;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.index.query.AbstractQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryRewriteContext;
 import org.elasticsearch.index.query.SearchExecutionContext;
+import org.elasticsearch.inference.InferenceServiceResults;
+import org.elasticsearch.inference.InputType;
+import org.elasticsearch.inference.TaskType;
 import org.elasticsearch.xcontent.ParseField;
 import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xpack.core.inference.action.InferenceAction;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+
+import static org.elasticsearch.xpack.core.ClientHelper.ML_ORIGIN;
+import static org.elasticsearch.xpack.core.ClientHelper.executeAsyncWithOrigin;
 
 public class SemanticQueryBuilder extends AbstractQueryBuilder<SemanticQueryBuilder> {
     public static final String NAME = "semantic_query";
@@ -27,6 +40,8 @@ public class SemanticQueryBuilder extends AbstractQueryBuilder<SemanticQueryBuil
 
     private final String fieldName;
     private final String query;
+
+    private SetOnce<InferenceServiceResults> inferenceResultsSupplier;
 
     public SemanticQueryBuilder(String fieldName, String query) {
         if (fieldName == null) {
@@ -43,6 +58,12 @@ public class SemanticQueryBuilder extends AbstractQueryBuilder<SemanticQueryBuil
         super(in);
         this.fieldName = in.readString();
         this.query = in.readString();
+    }
+
+    private SemanticQueryBuilder(SemanticQueryBuilder other, SetOnce<InferenceServiceResults> inferenceResultsSupplier) {
+        this.fieldName = other.fieldName;
+        this.query = other.query;
+        this.inferenceResultsSupplier = inferenceResultsSupplier;
     }
 
     @Override
@@ -69,6 +90,46 @@ public class SemanticQueryBuilder extends AbstractQueryBuilder<SemanticQueryBuil
         boostAndQueryNameToXContent(builder);
         builder.endObject();
         builder.endObject();
+    }
+
+    @Override
+    protected QueryBuilder doRewrite(QueryRewriteContext queryRewriteContext) {
+        if (inferenceResultsSupplier != null) {
+            return this;
+        }
+
+        Set<String> modelsForField = queryRewriteContext.getModelsForField(fieldName);
+        if (modelsForField.isEmpty()) {
+            throw new IllegalArgumentException("field [" + fieldName + "] is not a semantic_text field type");
+        }
+
+        if (modelsForField.size() > 1) {
+            // TODO: Handle multi-index semantic queries
+            throw new IllegalArgumentException("field [" + fieldName + "] has multiple models associated with it");
+        }
+
+        // TODO: How to determine task type?
+        InferenceAction.Request inferenceRequest = new InferenceAction.Request(
+            TaskType.SPARSE_EMBEDDING,
+            modelsForField.iterator().next(),
+            List.of(query),
+            Map.of(),
+            InputType.SEARCH
+        );
+
+        SetOnce<InferenceServiceResults> inferenceResultsSupplier = new SetOnce<>();
+        queryRewriteContext.registerAsyncAction((client, listener) -> executeAsyncWithOrigin(
+            client,
+            ML_ORIGIN,
+            InferenceAction.INSTANCE,
+            inferenceRequest,
+            listener.delegateFailureAndWrap((l, inferenceResponse) -> {
+                inferenceResultsSupplier.set(inferenceResponse.getResults());
+                l.onResponse(null);
+            })
+        ));
+
+        return new SemanticQueryBuilder(this, inferenceResultsSupplier);
     }
 
     @Override

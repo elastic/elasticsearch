d181a377f2c7acd93db02ec725d384a249a10bf1
Ryan Ernst <ryan@iernst.net>
2024-02-26 17:09:24 +0100

    Add reference docs links when jna fails to load (#105812)
    closes #105147

--- server/src/main/java/org/elasticsearch/common/ReferenceDocs.java
+++ server/src/main/java/org/elasticsearch/common/ReferenceDocs.java
@@ -70,6 +70,7 @@ public enum ReferenceDocs {
     BOOTSTRAP_CHECK_TOKEN_SSL,
     BOOTSTRAP_CHECK_SECURITY_MINIMAL_SETUP,
     CONTACT_SUPPORT,
+    EXECUTABLE_JNA_TMPDIR,
     // this comment keeps the ';' on the next line so every entry above has a trailing ',' which makes the diff for adding new links cleaner
     ;
 

02a23654806f391f23e51a2dc263ca4e489cd4e3
Ryan Ernst <ryan@iernst.net>
2024-02-26 16:11:42 +0100

    format

--- server/src/main/java/org/elasticsearch/bootstrap/Natives.java
+++ server/src/main/java/org/elasticsearch/bootstrap/Natives.java
@@ -13,6 +13,7 @@ import org.apache.logging.log4j.Logger;
 import org.elasticsearch.common.ReferenceDocs;
 
 import java.nio.file.Path;
+import java.util.Locale;
 
 /**
  * The Natives class is a wrapper class that checks if the classes necessary for calling native methods are available on
@@ -38,8 +39,11 @@ final class Natives {
             logger.warn("JNA not found. native methods will be disabled.", e);
         } catch (UnsatisfiedLinkError e) {
             logger.warn(
-                "unable to load JNA native support library, native methods will be disabled. See {}",
-                ReferenceDocs.EXECUTABLE_JNA_TMPDIR,
+                String.format(
+                    Locale.ROOT,
+                    "unable to load JNA native support library, native methods will be disabled. See %s",
+                    ReferenceDocs.EXECUTABLE_JNA_TMPDIR
+                ),
                 e
             );
         }

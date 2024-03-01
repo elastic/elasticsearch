d1b6891329b54828d0d0de97ed491ca91c70546b
Moritz Mack <mmack@apache.org>
2024-03-01 15:15:36 +0100

    Ignore Europe/Kyiv in timezone tests (closes #105841)

--- server/src/test/java/org/elasticsearch/common/time/DateUtilsTests.java
+++ server/src/test/java/org/elasticsearch/common/time/DateUtilsTests.java
@@ -45,7 +45,8 @@ public class DateUtilsTests extends ESTestCase {
             "Asia/Qostanay", // part of tzdata2018h
             "America/Godthab", // part of tzdata2020a (maps to America/Nuuk)
             "America/Nuuk", // part of tzdata2020a
-            "America/Ciudad_Juarez" // part of tzdata2022g
+            "America/Ciudad_Juarez", // part of tzdata2022g
+            "Europe/Kyiv" // part of tzdata2022c
         )
     );
 

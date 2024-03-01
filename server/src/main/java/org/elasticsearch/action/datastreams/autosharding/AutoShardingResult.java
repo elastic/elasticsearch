5617a63b8d8d1564aefb440e3f772c2325dd240c
Andrei Dan <andrei.dan@elastic.co>
2024-03-01 10:34:56 +0100

    Update server/src/main/java/org/elasticsearch/action/datastreams/autosharding/AutoShardingResult.java
    Co-authored-by: Lee Hinman <dakrone@users.noreply.github.com>

--- server/src/main/java/org/elasticsearch/action/datastreams/autosharding/AutoShardingResult.java
+++ server/src/main/java/org/elasticsearch/action/datastreams/autosharding/AutoShardingResult.java
@@ -16,7 +16,7 @@ import org.elasticsearch.core.TimeValue;
  * period that needs to lapse before the current recommendation should be applied.
  * <p>
  * If auto sharding is not applicable for a data stream (e.g. due to
- * {@link DataStreamAutoShardingService#DATA_STREAMS_AUTO_SHARDING_EXCLUDES_SETTING}) the target number of shards will be 0 and cool down
+ * {@link DataStreamAutoShardingService#DATA_STREAMS_AUTO_SHARDING_EXCLUDES_SETTING}) the target number of shards will be -1 and cool down
  * remaining {@link TimeValue#MAX_VALUE}.
  */
 public record AutoShardingResult(

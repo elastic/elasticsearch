15a95de1b9bd1098e28616e2e9a475c8362350d2
Kathleen DeRusso <kathleen.derusso@elastic.co>
2024-03-06 16:45:12 -0500

    Skeleton for restart IT test

---
+++ x-pack/plugin/ml/qa/native-multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/ml/integration/TextExpansionFullClusterRestartIT.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.ml.integration;
+
+import com.carrotsearch.randomizedtesting.annotations.Name;
+
+import org.elasticsearch.Version;
+import org.junit.ClassRule;
+
+import static org.apache.lucene.tests.util.LuceneTestCase.assumeTrue;
+import static org.elasticsearch.Version.V_8_13_0;
+
+public class TextExpansionFullClusterRestartIT extends ParameterizedFullClusterRestartTestCase {
+
+    private static final Version TOKEN_PRUNING_ADDED_VERSION = V_8_13_0;
+
+    @ClassRule
+    public static final ElasticsearchCluster cluster = ElasticsearchCluster.local()
+        .distribution(DistributionType.DEFAULT)
+        .version(getOldClusterTestVersion())
+        .nodes(2)
+        .setting("xpack.security.enabled", "false")
+        .module("x-pack-ml")
+        .build();
+
+    public FullClusterRestartIT(@Name("cluster") FullClusterRestartUpgradeStatus upgradeStatus) {
+        super(upgradeStatus);
+    }
+
+    @Override
+    protected ElasticsearchCluster getUpgradeCluster() {
+        return cluster;
+    }
+
+    public void testTextExpansionSearch() throws Exception {
+        assumeTrue(
+            "Data retention changed by default to DSL in " + TOKEN_PRUNING_ADDED_VERSION,
+            getOldClusterTestVersion().before(TOKEN_PRUNING_ADDED_VERSION.toString())
+        );
+
+        if (isRunningAgainstOldCluster()) {
+            // TODO
+        } else {
+            // TODO
+        }
+    }
+}

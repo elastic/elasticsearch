312c733e4f25993c98045c1fd0aa4a8d1abb8b4c
David Turner <david.turner@elastic.co>
2024-03-12 10:55:35 +0000

    Comparator constants in TransportGetSnapshotsAction (#106224)
    Today we use a constant `Comparator<SnapshotInfo>` for ascending sorts,
    but create a new instance for descending sorts each time. We should use
    constants for both cases.

--- server/src/main/java/org/elasticsearch/action/admin/cluster/snapshots/get/TransportGetSnapshotsAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/snapshots/get/TransportGetSnapshotsAction.java
@@ -54,7 +54,6 @@ import org.elasticsearch.transport.TransportService;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -529,7 +528,7 @@ public class TransportGetSnapshotsAction extends TransportMasterNodeAction<GetSn
 
         private SnapshotsInRepo sortSnapshots(Stream<SnapshotInfo> snapshotInfoStream, int totalCount, int offset, int size) {
             final var resultsStream = snapshotInfoStream.filter(sortBy.getAfterPredicate(after, order))
-                .sorted(buildComparator())
+                .sorted(sortBy.getSnapshotInfoComparator(order))
                 .skip(offset);
             if (size == GetSnapshotsRequest.NO_LIMIT) {
                 return new SnapshotsInRepo(resultsStream.toList(), totalCount, 0);
@@ -547,11 +546,6 @@ public class TransportGetSnapshotsAction extends TransportMasterNodeAction<GetSn
                 }
                 return new SnapshotsInRepo(results, totalCount, remaining);
             }
-        }
-
-        private Comparator<SnapshotInfo> buildComparator() {
-            final var comparator = sortBy.getSnapshotInfoComparator();
-            return order == SortOrder.DESC ? comparator.reversed() : comparator;
         }
     }
 

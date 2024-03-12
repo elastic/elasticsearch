866e4a940d0159b39d552fa1321aeccaff9ff3a6
Kostas Krikellas <kostas.krikellas@elastic.co>
2024-03-12 18:40:50 +0200

    revert change in SnapshotsService

--- server/src/main/java/org/elasticsearch/snapshots/SnapshotsService.java
+++ server/src/main/java/org/elasticsearch/snapshots/SnapshotsService.java
@@ -3837,7 +3837,7 @@ public final class SnapshotsService extends AbstractLifecycleComponent implement
         @Override
         public void onFailure(Exception e) {
             final var logLevel = snapshotFailureLogLevel(e);
-            if (logLevel == Level.INFO) {
+            if (logLevel == Level.INFO && logger.isDebugEnabled() == false) {
                 // suppress stack trace at INFO unless extra verbosity is configured
                 logger.info(
                     format(

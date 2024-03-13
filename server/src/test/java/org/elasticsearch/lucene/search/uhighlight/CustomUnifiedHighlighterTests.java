98a03f59c4962f2de9c3419f4685eb8b6a4d3c4f
yashdamani <yashdamani1997@gmail.com>
2024-03-13 21:07:59 +0530

    added fix for inconsistent text trimming in Unified Highlighter #101803

--- server/src/test/java/org/elasticsearch/lucene/search/uhighlight/CustomUnifiedHighlighterTests.java
+++ server/src/test/java/org/elasticsearch/lucene/search/uhighlight/CustomUnifiedHighlighterTests.java
@@ -145,7 +145,7 @@ public class CustomUnifiedHighlighterTests extends ESTestCase {
                 UnifiedHighlighter.Builder builder = UnifiedHighlighter.builder(searcher, analyzer);
                 builder.withBreakIterator(() -> breakIterator);
                 builder.withFieldMatcher(name -> "text".equals(name));
-                builder.withFormatter(new CustomPassageFormatter("<b>", "</b>", new DefaultEncoder()));
+                builder.withFormatter(new CustomPassageFormatter("<b>", "</b>", new DefaultEncoder(), 0));
                 CustomUnifiedHighlighter highlighter = new CustomUnifiedHighlighter(
                     builder,
                     offsetSource,

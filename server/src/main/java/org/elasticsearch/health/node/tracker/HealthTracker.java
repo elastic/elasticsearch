20cf6328007e370e4e16a96032f73e136c4c54d7
Niels Bauman <nielsbauman@gmail.com>
2024-03-13 18:55:36 +0100

    Refactor (simplify) LocalHealthMonitor

--- server/src/main/java/org/elasticsearch/health/node/tracker/HealthTracker.java
+++ server/src/main/java/org/elasticsearch/health/node/tracker/HealthTracker.java
@@ -8,31 +8,28 @@
 
 package org.elasticsearch.health.node.tracker;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.elasticsearch.health.node.LocalHealthMonitor;
 import org.elasticsearch.health.node.UpdateHealthInfoCacheAction;
 
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicReference;
-
 /**
  * Base class for health trackers that will be executed by the {@link LocalHealthMonitor}. It keeps track of the last
- * reported value and can retrieve the current health status when requested.
+ * reported health and can retrieve the current health status when requested.
  *
  * @param <T> the type of the health check result they track
  */
 public abstract class HealthTracker<T> {
-    private static final Logger logger = LogManager.getLogger(HealthTracker.class);
 
-    private final AtomicReference<T> lastReportedValue = new AtomicReference<>();
+    /**
+     * We can "simply" use a volatile field here, as we've ensured only one monitoring instance/thread at a time can update this value.
+     */
+    private volatile T lastReportedHealth;
 
     /**
      * Determine the health info for this health check.
      *
      * @return the health info.
      */
-    public abstract T checkCurrentHealth();
+    protected abstract T determineCurrentHealth();
 
     /**
      * Add the health info to the request builder.
@@ -40,28 +37,28 @@ public abstract class HealthTracker<T> {
      * @param builder the builder to add the health info to.
      * @param healthInfo the health info to add.
      */
-    public abstract void addToRequestBuilder(UpdateHealthInfoCacheAction.Request.Builder builder, T healthInfo);
+    protected abstract void addToRequestBuilder(UpdateHealthInfoCacheAction.Request.Builder builder, T healthInfo);
 
     /**
-     * Create a new {@link HealthProgress} instance by getting the current last reported value and determining the health info at this time.
-     *
-     * @return the new {@link HealthProgress} instance.
+     * Add the last reported health to the request builder.
      */
-    public HealthProgress<T> trackHealth() {
-        return new HealthProgress<>(this, lastReportedValue.get(), checkCurrentHealth());
+    public void addToRequestBuilder(UpdateHealthInfoCacheAction.Request.Builder builder) {
+        addToRequestBuilder(builder, lastReportedHealth);
     }
 
     /**
-     * Update the last reported health info to <code>current</code>, but only when the value inside <code>lastReportedValue</code>
-     * is equal to <code>previous</code>.
+     * Determine the current health info for this tracker and check if it has changed from the last reported value. When the health has
+     * changed, we'll store the new health as the last reported value.
      *
-     * @param previous the previous value that should be in <code>lastReportedValue</code> at the time of execution.
-     * @param current the value that should be stored in <code>lastReportedValue</code>.
+     * @return whether the health has changed.
      */
-    public void updateLastReportedHealth(T previous, T current) {
-        if (lastReportedValue.compareAndSet(previous, current)) {
-            logger.debug("Health info [{}] successfully sent, last reported value: {}.", current, previous);
+    public boolean trackHealth() {
+        var health = determineCurrentHealth();
+        if (health.equals(lastReportedHealth)) {
+            return false;
         }
+        lastReportedHealth = health;
+        return true;
     }
 
     /**
@@ -69,37 +66,10 @@ public abstract class HealthTracker<T> {
      * Should be used when, for example, the master or health node has changed.
      */
     public void reset() {
-        lastReportedValue.set(null);
+        lastReportedHealth = null;
     }
 
-    public T getLastReportedValue() {
-        return lastReportedValue.get();
-    }
-
-    /**
-     * A record for storing the previous and current value of a health check. This allows us to be sure no concurrent processes have
-     * updated the health check's reference value.
-     *
-     * @param <T> the type that the health tracker returns
-     */
-    public record HealthProgress<T>(HealthTracker<T> healthTracker, T previousHealth, T currentHealth) {
-        public boolean hasChanged() {
-            return Objects.equals(previousHealth, currentHealth) == false;
-        }
-
-        /**
-         * See {@link HealthTracker#addToRequestBuilder}.
-         */
-        public void updateRequestBuilder(UpdateHealthInfoCacheAction.Request.Builder builder) {
-            healthTracker.addToRequestBuilder(builder, currentHealth);
-        }
-
-        /**
-         * Update the reference value of the health tracker with the current health info.
-         * See {@link HealthTracker#updateLastReportedHealth} for more info.
-         */
-        public void recordProgressIfRelevant() {
-            healthTracker.updateLastReportedHealth(previousHealth, currentHealth);
-        }
+    public T getLastReportedHealth() {
+        return lastReportedHealth;
     }
 }

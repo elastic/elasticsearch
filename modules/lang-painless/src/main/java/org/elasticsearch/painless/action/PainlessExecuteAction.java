d2b7c40ec8247ce37b40e4229ad595feef9f5e2d
Kostas Krikellas <kostas.krikellas@elastic.co>
2024-03-13 16:35:22 +0200

    bwc in PainlessExecuteAction

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/action/PainlessExecuteAction.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/action/PainlessExecuteAction.java
@@ -56,6 +56,7 @@ import org.elasticsearch.geometry.Point;
 import org.elasticsearch.index.Index;
 import org.elasticsearch.index.IndexMode;
 import org.elasticsearch.index.IndexService;
+import org.elasticsearch.index.IndexVersions;
 import org.elasticsearch.index.mapper.DateFieldMapper;
 import org.elasticsearch.index.mapper.DocumentMapper;
 import org.elasticsearch.index.mapper.OnScriptError;
@@ -811,7 +812,14 @@ public class PainlessExecuteAction {
                     XContentType xContentType = request.contextSetup.xContentType;
 
                     SourceToParse sourceToParse = (indexService.getIndexSettings().getMode() == IndexMode.TIME_SERIES)
-                        ? new SourceToParse(null, document, xContentType, TimeSeriesRoutingHashFieldMapper.DUMMY_ENCODED_VALUE)
+                        ? new SourceToParse(
+                            null,
+                            document,
+                            xContentType,
+                            indexService.getIndexSettings().getIndexVersionCreated().onOrAfter(IndexVersions.TIME_SERIES_ROUTING_HASH_IN_ID)
+                                ? TimeSeriesRoutingHashFieldMapper.DUMMY_ENCODED_VALUE
+                                : null
+                        )
                         : new SourceToParse("_id", document, xContentType);
 
                     DocumentMapper documentMapper = indexService.mapperService().documentMapper();

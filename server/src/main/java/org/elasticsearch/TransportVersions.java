75e67f07642e7091ba71f542e43b1b636a569190
Jake Landis <jake.landis@elastic.co>
2024-03-14 12:55:18 -0500

    cross check livedocs for terms aggs when index access control list is non-null (#105714)

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -146,6 +146,7 @@ public class TransportVersions {
     public static final TransportVersion KNN_EXPLICIT_BYTE_QUERY_VECTOR_PARSING = def(8_606_00_0);
     public static final TransportVersion ESQL_EXTENDED_ENRICH_INPUT_TYPE = def(8_607_00_0);
     public static final TransportVersion ESQL_SERIALIZE_BIG_VECTOR = def(8_608_00_0);
+    public static final TransportVersion AGGS_EXCLUDED_DELETED_DOCS = def(8_609_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

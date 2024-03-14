b6f876f32a270e7240117bdc6cd46d01c563f549
Nhat Nguyen <nhat.nguyen@elastic.co>
2024-03-14 12:50:53 -0700

    Serialize big array blocks (#106373)
    Similar to ArrayBlocks, this change serializes the underlying
    structure of BigArrayBlocks.

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -147,6 +147,7 @@ public class TransportVersions {
     public static final TransportVersion ESQL_EXTENDED_ENRICH_INPUT_TYPE = def(8_607_00_0);
     public static final TransportVersion ESQL_SERIALIZE_BIG_VECTOR = def(8_608_00_0);
     public static final TransportVersion AGGS_EXCLUDED_DELETED_DOCS = def(8_609_00_0);
+    public static final TransportVersion ESQL_SERIALIZE_BIG_ARRAY = def(8_610_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

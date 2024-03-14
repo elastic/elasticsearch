bf6ca879b037454c9ef39e50100abf06774e81fc
Craig Taverner <craig@amanzi.com>
2024-03-14 15:21:56 +0100

    Enhance ENRICH LookupRequest to include input data type (#106344)
    This is needed for refined support of type checking when doing range ENRICH queries

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -144,6 +144,7 @@ public class TransportVersions {
     public static final TransportVersion ALLOCATION_STATS = def(8_604_00_0);
     public static final TransportVersion ESQL_EXTENDED_ENRICH_TYPES = def(8_605_00_0);
     public static final TransportVersion KNN_EXPLICIT_BYTE_QUERY_VECTOR_PARSING = def(8_606_00_0);
+    public static final TransportVersion ESQL_EXTENDED_ENRICH_INPUT_TYPE = def(8_607_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

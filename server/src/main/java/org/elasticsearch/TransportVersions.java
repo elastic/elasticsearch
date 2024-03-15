387eb38bfd83d23a2c6592d7253a302f9e37fa81
Andrei Dan <andrei.dan@elastic.co>
2024-03-15 13:28:28 +0000

    Autoshard data streams on rollover (#106076)
    This enhances our rollover logic to use the data stream autosharding
    service to retrieve an autosharding recommendation. If the
    recommendation is an INCREASE_SHARDS or an
    COOLDOWN_PREVENTED_INCREASE_SHARDS we'll create a rollover condition
    that'll capture this recommendation, such that rollover will be
    triggered in ourder to increase the number of shards even if other
    "regular" conditions are not met (or in the case where cooldown
    prevented rollover, display the information as to why in the rollover
    response).
    
    All other recommednations are passed to the `MetadataRolloverService`
    that'll do the needful to ensure the new write index of the data stream
    receives the correct number of shards.
    
    Note that a DECREASE_SHARDS recommendation will reduce the number of
    shards for a data stream when one of the other "regular" rollover
    conditions match. It will not trigger a rollover itself, only the
    INCREASE_SHARDS recommendation will.
    
    Some notes on the `NOT_APPLICABLE` recommendation: N/A results are
    switching back a data stream to the sharding configured in the index
    template. A data stream can be using auto sharding and later be excluded
    from the functionality using the `data_streams.auto_sharding.excludes`
    setting. After a data stream is excluded it needs to start using the
    number of shards configured in the backing index template.
    
    The new autosharding_condition will look like this in the rollover
    response:
    
    ```
      "acknowledged": true,
      "shards_acknowledged": true,
      "old_index": ".ds-logs-nginx-2024.03.13-000003",
      "new_index": ".ds-logs-nginx-2024.03.13-000004",
      "rolled_over": true,
      "dry_run": false,
      "lazy": false,
      "conditions": {
        "[optimal_shard_count : 3]": true
      }
    ```
    
    and like so in the `met_conditions` field, part of rollover info in the
    cluster state :
    
    ```
    "rollover_info" : {
      "logs-nginx" : {
         "met_conditions" : {
            "max_docs" : 20000000,
            "optimal_shard_count" : 3
          },
          "time" : 1710421491138
        }
     },
    ```

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -148,6 +148,7 @@ public class TransportVersions {
     public static final TransportVersion ESQL_SERIALIZE_BIG_VECTOR = def(8_608_00_0);
     public static final TransportVersion AGGS_EXCLUDED_DELETED_DOCS = def(8_609_00_0);
     public static final TransportVersion ESQL_SERIALIZE_BIG_ARRAY = def(8_610_00_0);
+    public static final TransportVersion AUTO_SHARDING_ROLLOVER_CONDITION = def(8_611_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

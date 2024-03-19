f95180df45e3b73b5cf7559fb43142affb35f409
gmarouli <mary.gouseti@elastic.co>
2024-03-19 14:40:13 +0200

    Remove seeds

--- server/src/test/java/org/elasticsearch/cluster/metadata/ComposableIndexTemplateTests.java
+++ server/src/test/java/org/elasticsearch/cluster/metadata/ComposableIndexTemplateTests.java
@@ -8,8 +8,6 @@
 
 package org.elasticsearch.cluster.metadata;
 
-import com.carrotsearch.randomizedtesting.annotations.Seed;
-
 import org.elasticsearch.action.admin.indices.rollover.RolloverConfiguration;
 import org.elasticsearch.action.admin.indices.rollover.RolloverConfigurationTests;
 import org.elasticsearch.cluster.Diff;
@@ -211,7 +209,6 @@ public class ComposableIndexTemplateTests extends SimpleDiffableSerializationTes
         assertThat(ComposableIndexTemplate.componentTemplatesEquals(List.of(), List.of(randomAlphaOfLength(5))), equalTo(false));
     }
 
-    @Seed("1F8A73D5B8BD4DFC")
     public void testXContentSerializationWithRolloverAndGlobalRetention() throws IOException {
         Settings settings = null;
         CompressedXContent mappings = null;

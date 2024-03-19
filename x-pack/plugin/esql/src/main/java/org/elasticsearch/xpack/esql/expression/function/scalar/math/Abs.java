693b2530ab65e657a9887892351a75d242e1b5bc
Nik Everett <nik9000@gmail.com>
2024-03-19 15:47:50 -0400

    ESQL: Use generated docs for abs and acos

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/math/Abs.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/math/Abs.java
@@ -10,6 +10,7 @@ package org.elasticsearch.xpack.esql.expression.function.scalar.math;
 import org.elasticsearch.compute.ann.Evaluator;
 import org.elasticsearch.compute.operator.EvalOperator.ExpressionEvaluator;
 import org.elasticsearch.xpack.esql.EsqlIllegalArgumentException;
+import org.elasticsearch.xpack.esql.expression.function.Example;
 import org.elasticsearch.xpack.esql.expression.function.FunctionInfo;
 import org.elasticsearch.xpack.esql.expression.function.Param;
 import org.elasticsearch.xpack.esql.expression.function.scalar.UnaryScalarFunction;
@@ -22,8 +23,19 @@ import java.util.List;
 import java.util.function.Function;
 
 public class Abs extends UnaryScalarFunction {
-    @FunctionInfo(returnType = { "double", "integer", "long", "unsigned_long" }, description = "Returns the absolute value.")
-    public Abs(Source source, @Param(name = "n", type = { "double", "integer", "long", "unsigned_long" }) Expression n) {
+    @FunctionInfo(
+        returnType = { "double", "integer", "long", "unsigned_long" },
+        description = "Returns the absolute value.",
+        examples = { @Example(file = "math", tag = "abs"), @Example(file = "math", tag = "abs-employees") }
+    )
+    public Abs(
+        Source source,
+        @Param(
+            name = "n",
+            type = { "double", "integer", "long", "unsigned_long" },
+            description = "Numeric expression. If `null`, the function returns `null`."
+        ) Expression n
+    ) {
         super(source, n);
     }
 
--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/math/Acos.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/math/Acos.java
@@ -9,6 +9,7 @@ package org.elasticsearch.xpack.esql.expression.function.scalar.math;
 
 import org.elasticsearch.compute.ann.Evaluator;
 import org.elasticsearch.compute.operator.EvalOperator;
+import org.elasticsearch.xpack.esql.expression.function.Example;
 import org.elasticsearch.xpack.esql.expression.function.FunctionInfo;
 import org.elasticsearch.xpack.esql.expression.function.Param;
 import org.elasticsearch.xpack.ql.expression.Expression;
@@ -21,10 +22,18 @@ import java.util.List;
  * Inverse cosine trigonometric function.
  */
 public class Acos extends AbstractTrigonometricFunction {
-    @FunctionInfo(returnType = "double", description = "The arccosine of an angle, expressed in radians.")
+    @FunctionInfo(
+        returnType = "double",
+        description = "Returns the {wikipedia}/Inverse_trigonometric_functions[arccosine] of `n` as an angle, expressed in radians.",
+        examples = { @Example(file = "floats", tag = "acos") }
+    )
     public Acos(
         Source source,
-        @Param(name = "n", type = { "double", "integer", "long", "unsigned_long" }, description = "Number between -1 and 1") Expression n
+        @Param(
+            name = "n",
+            type = { "double", "integer", "long", "unsigned_long" },
+            description = "Number between -1 and 1. If `null`, the function returns `null`."
+        ) Expression n
     ) {
         super(source, n);
     }

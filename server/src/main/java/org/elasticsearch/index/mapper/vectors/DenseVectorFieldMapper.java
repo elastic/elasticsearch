608498bf310ff5b948c6090f0e350da8552dbcd7
Tommaso Teofili <tommaso.teofili@elastic.co>
2024-03-21 19:49:42 +0100

    formatting fixed

--- server/src/main/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapper.java
@@ -161,11 +161,14 @@ public class DenseVectorFieldMapper extends FieldMapper {
             XContentBuilder::field,
             Objects::toString
         ).setSerializerCheck((id, ic, v) -> v != null)
-            .setMergeValidator((previous, current, c) ->
-                    previous == null || Objects.equals(previous, current) || previous.type.equals(VectorIndexType.FLAT.name) ||
-                    previous.type.equals(VectorIndexType.HNSW.name) && current.type.equals(VectorIndexType.INT8_HNSW.name) ||
-                    previous.type.equals(VectorIndexType.INT8_FLAT.name) && current.type.equals(VectorIndexType.HNSW.name) ||
-                    previous.type.equals(VectorIndexType.INT8_FLAT.name) && current.type.equals(VectorIndexType.INT8_HNSW.name));
+            .setMergeValidator(
+                (previous, current, c) -> previous == null
+                    || Objects.equals(previous, current)
+                    || previous.type.equals(VectorIndexType.FLAT.name)
+                    || previous.type.equals(VectorIndexType.HNSW.name) && current.type.equals(VectorIndexType.INT8_HNSW.name)
+                    || previous.type.equals(VectorIndexType.INT8_FLAT.name) && current.type.equals(VectorIndexType.HNSW.name)
+                    || previous.type.equals(VectorIndexType.INT8_FLAT.name) && current.type.equals(VectorIndexType.INT8_HNSW.name)
+            );
         private final Parameter<Boolean> indexed;
         private final Parameter<Map<String, String>> meta = Parameter.metaParam();
 

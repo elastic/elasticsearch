408355105d35142e44ef8a61e7eb9c07337f92e3
Simon Cooper <simon.cooper@elastic.co>
2024-03-21 15:36:08 +0000

    Update index settings test

--- server/src/test/java/org/elasticsearch/cluster/metadata/HumanReadableIndexSettingsTests.java
+++ server/src/test/java/org/elasticsearch/cluster/metadata/HumanReadableIndexSettingsTests.java
@@ -22,16 +22,19 @@ import static org.hamcrest.Matchers.equalTo;
 public class HumanReadableIndexSettingsTests extends ESTestCase {
     public void testHumanReadableSettings() {
         IndexVersion versionCreated = randomVersion(random());
+        IndexVersion versionUpdated = randomVersion(random());
         long created = System.currentTimeMillis();
         Settings testSettings = Settings.builder()
             .put(IndexMetadata.SETTING_VERSION_CREATED, versionCreated)
+            .put(IndexMetadata.SETTING_VERSION_UPDATED, versionUpdated)
             .put(IndexMetadata.SETTING_CREATION_DATE, created)
             .build();
 
         Settings humanSettings = IndexMetadata.addHumanReadableSettings(testSettings);
-        assertThat(humanSettings.size(), equalTo(4));
-        assertEquals(versionCreated.toString(), humanSettings.get(IndexMetadata.SETTING_VERSION_CREATED_STRING, null));
+        assertThat(humanSettings.size(), equalTo(6));
+        assertThat(humanSettings.get(IndexMetadata.SETTING_VERSION_CREATED_STRING, null), equalTo(versionCreated.toString()));
+        assertThat(humanSettings.get(IndexMetadata.SETTING_VERSION_UPDATED_STRING, null), equalTo(versionUpdated.toString()));
         ZonedDateTime creationDate = ZonedDateTime.ofInstant(Instant.ofEpochMilli(created), ZoneOffset.UTC);
-        assertEquals(creationDate.toString(), humanSettings.get(IndexMetadata.SETTING_CREATION_DATE_STRING, null));
+        assertThat(humanSettings.get(IndexMetadata.SETTING_CREATION_DATE_STRING, null), equalTo(creationDate.toString()));
     }
 }

5bb16c1e31db8f9ec9c8cd549bf0ebfaa1e24c0c
Simon Cooper <simon.cooper@elastic.co>
2024-03-26 11:21:14 +0000

    Add record of the index version when mappings were last updated

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -152,6 +152,7 @@ public class TransportVersions {
     public static final TransportVersion KNN_QUERY_VECTOR_BUILDER = def(8_612_00_0);
     public static final TransportVersion USE_DATA_STREAM_GLOBAL_RETENTION = def(8_613_00_0);
     public static final TransportVersion ML_COMPLETION_INFERENCE_SERVICE_ADDED = def(8_614_00_0);
+    public static final TransportVersion INDEX_METADATA_MAPPINGS_UPDATED_VERSION = def(8_615_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

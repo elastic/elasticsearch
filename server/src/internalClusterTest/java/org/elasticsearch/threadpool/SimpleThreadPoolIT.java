f039d2ff39475ce904b03a2be631f724a48a0f6d
Moritz Mack <mmack@apache.org>
2024-03-26 08:44:50 +0100

    make sure there's no active threads when collecting final pool stats

--- server/src/internalClusterTest/java/org/elasticsearch/threadpool/SimpleThreadPoolIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/threadpool/SimpleThreadPoolIT.java
@@ -30,6 +30,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 
@@ -148,15 +149,17 @@ public class SimpleThreadPoolIT extends ESIntegTestCase {
             assertNoFailures(prepareSearch("idx").setQuery(QueryBuilders.termQuery("str_value", "s" + i)));
             assertNoFailures(prepareSearch("idx").setQuery(QueryBuilders.termQuery("l_value", i)));
         }
+
         final var tp = internalCluster().getInstance(ThreadPool.class, dataNodeName);
+        final var tps = new AtomicReference<ThreadPoolStats>();
         // wait for all threads to complete so that we get deterministic results
-        waitUntil(() -> tp.stats().stats().stream().allMatch(s -> s.active() == 0));
-        ThreadPoolStats tps = tp.stats();
+        waitUntil(() -> tps.updateAndGet(s -> tp.stats()).stats().stream().allMatch(s -> s.active() == 0));
+
         plugin.collect();
         ArrayList<String> registeredMetrics = plugin.getRegisteredMetrics(InstrumentType.LONG_GAUGE);
         registeredMetrics.addAll(plugin.getRegisteredMetrics(InstrumentType.LONG_ASYNC_COUNTER));
 
-        tps.forEach(stats -> {
+        tps.get().forEach(stats -> {
             Map<String, Long> threadPoolStats = List.of(
                 Map.entry(ThreadPool.THREAD_POOL_METRIC_NAME_COMPLETED, stats.completed()),
                 Map.entry(ThreadPool.THREAD_POOL_METRIC_NAME_ACTIVE, (long) stats.active()),

77195c1400ef002b2c76cdee6e0f00e6c7ad4270
Artem Prigoda <artem.prigoda@elastic.co>
2024-03-26 15:49:00 +0100

    Remove empty import

--- server/src/test/java/org/elasticsearch/index/shard/StoreRecoveryTests.java
+++ server/src/test/java/org/elasticsearch/index/shard/StoreRecoveryTests.java
@@ -49,7 +49,6 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Predicate;
 

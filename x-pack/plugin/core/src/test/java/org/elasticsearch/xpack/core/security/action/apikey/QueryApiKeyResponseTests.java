c092e6cad8ca01fd45d29e226e24e8f4c6e7f45a
Albert Zaharovits <albert.zaharovits@elastic.co>
2024-03-26 15:10:52 +0200

    testMismatchApiKeyInfoAndSortValues

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/action/apikey/QueryApiKeyResponseTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/action/apikey/QueryApiKeyResponseTests.java
@@ -50,4 +50,38 @@ public class QueryApiKeyResponseTests extends ESTestCase {
             assertThat(iae.getMessage(), containsString("Each api key info must be associated to a (nullable) owner profile uid"));
         }
     }
+
+    public void testMismatchApiKeyInfoAndSortValues() {
+        List<ApiKey> apiKeys = randomList(
+            0,
+            3,
+            () -> new ApiKey(
+                randomAlphaOfLength(4),
+                randomAlphaOfLength(4),
+                randomFrom(ApiKey.Type.values()),
+                Instant.now(),
+                Instant.now(),
+                randomBoolean(),
+                null,
+                randomAlphaOfLength(4),
+                randomAlphaOfLength(4),
+                null,
+                null,
+                null,
+                null
+            )
+        );
+        List<String> profileUids = new ArrayList<>(apiKeys.size());
+        for (int i = 0; i < apiKeys.size(); i++) {
+            profileUids.add(randomFrom(randomAlphaOfLength(8), null));
+        }
+        List<Object[]> sortValues = randomList(0, 6, () -> new String[] { "dummy sort value" });
+        if (apiKeys.size() != sortValues.size()) {
+            IllegalArgumentException iae = expectThrows(
+                IllegalArgumentException.class,
+                () -> new QueryApiKeyResponse(100, apiKeys, sortValues, profileUids, null)
+            );
+            assertThat(iae.getMessage(), containsString("Each api key info must be associated to a (nullable) sort value"));
+        }
+    }
 }

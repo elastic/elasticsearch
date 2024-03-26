b39b3731a7923f38eddbe815e4f2c777261cd7d1
Rene Groeschke <rene@elastic.co>
2024-03-26 08:39:39 +0100

    Port krb5kdc to test container and rework hdfs handling (#106228)
    This ports our krb5kdc test fixture to test container and reworks hdfs handling to also be based on test containers.
    The yaml rest tests that are using hdfs required introducing variable substitution in yamlresttestparser handling.

--- plugins/repository-hdfs/src/yamlRestTest/java/org/elasticsearch/repositories/hdfs/SecureRepositoryHdfsClientYamlTestSuiteIT.java
+++ plugins/repository-hdfs/src/yamlRestTest/java/org/elasticsearch/repositories/hdfs/SecureRepositoryHdfsClientYamlTestSuiteIT.java
@@ -14,8 +14,10 @@ import com.carrotsearch.randomizedtesting.annotations.ThreadLeakFilters;
 
 import org.elasticsearch.test.cluster.ElasticsearchCluster;
 import org.elasticsearch.test.cluster.local.distribution.DistributionType;
+import org.elasticsearch.test.cluster.util.resource.Resource;
 import org.elasticsearch.test.fixtures.hdfs.HdfsClientThreadLeakFilter;
 import org.elasticsearch.test.fixtures.hdfs.HdfsFixture;
+import org.elasticsearch.test.fixtures.krb5kdc.Krb5kDcContainer;
 import org.elasticsearch.test.fixtures.testcontainers.TestContainersThreadFilter;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
 import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
@@ -26,21 +28,25 @@ import org.junit.rules.TestRule;
 import java.util.Map;
 
 @ThreadLeakFilters(filters = { HdfsClientThreadLeakFilter.class, TestContainersThreadFilter.class })
-public class RepositoryHdfsClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
+public class SecureRepositoryHdfsClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
+    public static Krb5kDcContainer krb5Fixture = new Krb5kDcContainer();
 
-    public static HdfsFixture hdfsFixture = new HdfsFixture();
+    public static HdfsFixture hdfsFixture = new HdfsFixture().withKerberos(() -> krb5Fixture.getPrincipal(), () -> krb5Fixture.getKeytab());
 
     public static ElasticsearchCluster cluster = ElasticsearchCluster.local()
         .distribution(DistributionType.DEFAULT)
         .plugin("repository-hdfs")
         .setting("xpack.license.self_generated.type", "trial")
         .setting("xpack.security.enabled", "false")
+        .systemProperty("java.security.krb5.conf", () -> krb5Fixture.getConfPath().toString())
+        .configFile("repository-hdfs/krb5.conf", Resource.fromString(() -> krb5Fixture.getConf()))
+        .configFile("repository-hdfs/krb5.keytab", Resource.fromFile(() -> krb5Fixture.getEsKeytab()))
         .build();
 
     @ClassRule
-    public static TestRule ruleChain = RuleChain.outerRule(hdfsFixture).around(cluster);
+    public static TestRule ruleChain = RuleChain.outerRule(krb5Fixture).around(hdfsFixture).around(cluster);
 
-    public RepositoryHdfsClientYamlTestSuiteIT(@Name("yaml") ClientYamlTestCandidate testCandidate) {
+    public SecureRepositoryHdfsClientYamlTestSuiteIT(@Name("yaml") ClientYamlTestCandidate testCandidate) {
         super(testCandidate);
     }
 
@@ -51,6 +57,6 @@ public class RepositoryHdfsClientYamlTestSuiteIT extends ESClientYamlSuiteTestCa
 
     @ParametersFactory
     public static Iterable<Object[]> parameters() throws Exception {
-        return createParameters(new String[] { "hdfs_repository" }, Map.of("hdfs_port", hdfsFixture.getPort()));
+        return createParameters(new String[] { "secure_hdfs_repository" }, Map.of("secure_hdfs_port", hdfsFixture.getPort()));
     }
 }

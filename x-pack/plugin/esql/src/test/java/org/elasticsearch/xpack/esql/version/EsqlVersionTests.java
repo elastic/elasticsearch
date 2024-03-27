da6c0e183eaa509d896c48a293c131df808c5298
Alexander Spies <alexander.spies@elastic.co>
2024-03-27 16:39:23 +0100

    Make checkstyle happy

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/version/EsqlVersionTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/version/EsqlVersionTests.java
@@ -11,19 +11,18 @@ import org.elasticsearch.test.ESTestCase;
 
 import java.util.Arrays;
 
-import static org.elasticsearch.xpack.esql.version.EsqlVersion.*;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class EsqlVersionTests extends ESTestCase {
     public void testVersionString() {
-        assertThat(NIGHTLY.toString(), equalTo("nightly.\uD83D\uDE34"));
-        assertThat(PARTY_POPPER.toString(), equalTo("2024.04.\uD83C\uDF89"));
+        assertThat(EsqlVersion.NIGHTLY.toString(), equalTo("nightly.\uD83D\uDE34"));
+        assertThat(EsqlVersion.PARTY_POPPER.toString(), equalTo("2024.04.\uD83C\uDF89"));
     }
 
     public void testVersionId() {
-        assertThat(NIGHTLY.id(), equalTo(Integer.MAX_VALUE));
-        assertThat(PARTY_POPPER.id(), equalTo(202404));
+        assertThat(EsqlVersion.NIGHTLY.id(), equalTo(Integer.MAX_VALUE));
+        assertThat(EsqlVersion.PARTY_POPPER.id(), equalTo(202404));
     }
 
     public void testVersionStringNoEmoji() {

1fbb357c93a7f4a68e4f4f314111e414dc66dd62
Mike Pellegrini <mike.pellegrini@elastic.co>
2024-03-27 16:15:02 -0400

    Fix tests

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/queries/SemanticQueryBuilderTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/queries/SemanticQueryBuilderTests.java
@@ -74,7 +74,7 @@ public class SemanticQueryBuilderTests extends AbstractQueryTestCase<SemanticQue
                         "model_settings": {
                             "task_type": "SPARSE_EMBEDDING"
                         },
-                        "results": [
+                        "chunks": [
                             {
                                 "inference": {
                                     "feature_0": 1.0
@@ -98,7 +98,7 @@ public class SemanticQueryBuilderTests extends AbstractQueryTestCase<SemanticQue
                             "dimensions": %d,
                             "similarity": "cosine"
                         },
-                        "results": [
+                        "chunks": [
                             {
                                 "inference": [ 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 ],
                                 "text": "feature_0"

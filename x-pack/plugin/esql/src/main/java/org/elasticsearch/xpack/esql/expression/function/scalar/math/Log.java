267afe20f262c4a6006ea74ba102a8d561694e7d
Nik Everett <nik9000@gmail.com>
2024-04-02 08:50:58 -0400

    Fix

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/math/Log.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/math/Log.java
@@ -36,7 +36,7 @@ public class Log extends EsqlScalarFunction implements OptionalArgument {
     @FunctionInfo(
         returnType = "double",
         description = "Returns the logarithm of a value to a base. The input can be any numeric value, "
-            + "lthe return value is always a double.\n"
+            + "the return value is always a double.\n"
             + "\n"
             + "Logs of zero, negative numbers, infinities and base of one return `null` as well as a warning.",
         examples = { @Example(file = "math", tag = "log"), @Example(file = "math", tag = "logUnary") }

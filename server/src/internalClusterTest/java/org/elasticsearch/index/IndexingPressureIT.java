60a9c1157213af4e17324ca1c1d94c70c082e976
Volodymyr Krasnikov <volodymyr.krasnikov@elastic.co>
2024-04-03 17:17:39 -0700

    Update test

--- server/src/internalClusterTest/java/org/elasticsearch/index/IndexingPressureIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/index/IndexingPressureIT.java
@@ -301,7 +301,8 @@ public class IndexingPressureIT extends ESIntegTestCase {
     public void testWriteCanBeRejectedAtPrimaryLevel() throws Exception {
         final BulkRequest bulkRequest = new BulkRequest();
         long totalRequestSize = 0;
-        for (int i = 0; i < 80; ++i) {
+        int numberOfIndexRequests = randomIntBetween(50, 100);
+        for (int i = 0; i < numberOfIndexRequests; ++i) {
             IndexRequest request = new IndexRequest(INDEX_NAME).id(UUIDs.base64UUID())
                 .source(Collections.singletonMap("key", randomAlphaOfLength(50)));
             totalRequestSize += request.ramBytesUsed();
@@ -346,6 +347,7 @@ public class IndexingPressureIT extends ESIntegTestCase {
 
             successFuture.actionGet();
 
+            assertEquals(numberOfIndexRequests, primaryWriteLimits.stats().getPrimaryRejections());
             assertEquals(0, primaryWriteLimits.stats().getCurrentCombinedCoordinatingAndPrimaryBytes());
             assertEquals(0, primaryWriteLimits.stats().getCurrentReplicaBytes());
             assertEquals(0, replicaWriteLimits.stats().getCurrentCombinedCoordinatingAndPrimaryBytes());

145271bce720b6fb871194c36f6c3ee7a4bd08a9
Armin Braun <me@obrown.io>
2024-04-04 15:00:32 +0200

    Remove unused methods from SearchContext
    A couple methods can be removed here, they're only used on sub-classes.
    That also cleans up some unsupport operation exception code that is never hit.

--- server/src/main/java/org/elasticsearch/search/internal/SubSearchContext.java
+++ server/src/main/java/org/elasticsearch/search/internal/SubSearchContext.java
@@ -9,7 +9,6 @@ package org.elasticsearch.search.internal;
 
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TotalHits;
-import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.query.ParsedQuery;
 import org.elasticsearch.search.aggregations.SearchContextAggregations;
 import org.elasticsearch.search.collapse.CollapseContext;
@@ -22,9 +21,6 @@ import org.elasticsearch.search.fetch.subphase.ScriptFieldsContext;
 import org.elasticsearch.search.fetch.subphase.highlight.SearchHighlightContext;
 import org.elasticsearch.search.query.QuerySearchResult;
 import org.elasticsearch.search.sort.SortAndFormats;
-import org.elasticsearch.search.suggest.SuggestionSearchContext;
-
-import java.util.List;
 
 public class SubSearchContext extends FilteredSearchContext {
 
@@ -86,11 +82,6 @@ public class SubSearchContext extends FilteredSearchContext {
     }
 
     @Override
-    public void suggest(SuggestionSearchContext suggest) {
-        throw new UnsupportedOperationException("Not supported");
-    }
-
-    @Override
     public boolean hasScriptFields() {
         return scriptFields != null && scriptFields.fields().isEmpty() == false;
     }
@@ -139,11 +130,6 @@ public class SubSearchContext extends FilteredSearchContext {
     public SubSearchContext fetchFieldsContext(FetchFieldsContext fetchFieldsContext) {
         this.fetchFieldsContext = fetchFieldsContext;
         return this;
-    }
-
-    @Override
-    public void timeout(TimeValue timeout) {
-        throw new UnsupportedOperationException("Not supported");
     }
 
     @Override
@@ -248,11 +234,6 @@ public class SubSearchContext extends FilteredSearchContext {
     @Override
     public void explain(boolean explain) {
         this.explain = explain;
-    }
-
-    @Override
-    public void groupStats(List<String> groupStats) {
-        throw new UnsupportedOperationException("Not supported");
     }
 
     @Override

4290eac9f05310c14ebb6a6dfdf022bc125237e6
Salvatore Campagna <salvatore.campagna@elastic.co>
2024-04-05 10:19:01 +0200

    fix: fetch fields and stored fields

--- server/src/main/java/org/elasticsearch/search/fetch/subphase/FetchFieldsPhase.java
+++ server/src/main/java/org/elasticsearch/search/fetch/subphase/FetchFieldsPhase.java
@@ -10,9 +10,14 @@ package org.elasticsearch.search.fetch.subphase;
 
 import org.apache.lucene.index.LeafReaderContext;
 import org.elasticsearch.common.document.DocumentField;
+import org.elasticsearch.index.mapper.IdFieldMapper;
 import org.elasticsearch.index.mapper.IgnoredFieldMapper;
 import org.elasticsearch.index.mapper.LegacyTypeFieldMapper;
+import org.elasticsearch.index.mapper.MappedFieldType;
 import org.elasticsearch.index.mapper.RoutingFieldMapper;
+import org.elasticsearch.index.mapper.SourceFieldMapper;
+import org.elasticsearch.index.mapper.ValueFetcher;
+import org.elasticsearch.index.query.SearchExecutionContext;
 import org.elasticsearch.search.fetch.FetchContext;
 import org.elasticsearch.search.fetch.FetchSubPhase;
 import org.elasticsearch.search.fetch.FetchSubPhaseProcessor;
@@ -21,8 +26,11 @@ import org.elasticsearch.search.fetch.StoredFieldsSpec;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * A fetch sub-phase for high-level field retrieval. Given a list of fields, it
@@ -42,28 +50,48 @@ public final class FetchFieldsPhase implements FetchSubPhase {
         final FetchFieldsContext fetchFieldsContext = fetchContext.fetchFieldsContext();
         final StoredFieldsContext storedFieldsContext = fetchContext.storedFieldsContext();
 
-        // with _stored: _none_ we don't fetch metadata fields either (regardless of whether they are stored or not, for bwc reasons)
         boolean fetchStoredFields = storedFieldsContext != null && storedFieldsContext.fetchFields();
         if (fetchFieldsContext == null && fetchStoredFields == false) {
             return null;
         }
 
-        final List<FieldAndFormat> fetchFields = fetchFieldsContext == null ? Collections.emptyList()
-            : fetchFieldsContext.fields() == null ? Collections.emptyList()
-            : fetchFieldsContext.fields();
+        final SearchExecutionContext searchExecutionContext = fetchContext.getSearchExecutionContext();
+        final FieldFetcher fieldFetcher = fetchFieldsContext == null ? null
+            : fetchFieldsContext.fields() == null ? null
+            : fetchFieldsContext.fields().isEmpty() ? null
+            : FieldFetcher.create(searchExecutionContext, fetchFieldsContext.fields());
 
-        final FieldFetcher fieldFetcher = fetchFields.isEmpty()
-            ? FieldFetcher.create(fetchContext.getSearchExecutionContext(), fetchFields)
-            : null;
-        final FieldFetcher metadataFieldFetcher = FieldFetcher.create(fetchContext.getSearchExecutionContext(), DEFAULT_METADATA_FIELDS);
-
+        final FieldFetcher metadataFieldFetcher;
+        if (storedFieldsContext != null
+            && storedFieldsContext.fieldNames() != null
+            && storedFieldsContext.fieldNames().isEmpty() == false) {
+            final Set<FieldAndFormat> metadataFields = new TreeSet<>(Comparator.comparing(fieldAndFormat -> fieldAndFormat.field));
+            metadataFields.addAll(DEFAULT_METADATA_FIELDS);
+            for (final String storedField : storedFieldsContext.fieldNames()) {
+                final Set<String> matchingFieldNames = searchExecutionContext.getMatchingFieldNames(storedField);
+                for (final String matchingFieldName : matchingFieldNames) {
+                    final MappedFieldType fieldType = searchExecutionContext.getFieldType(matchingFieldName);
+                    if (SourceFieldMapper.NAME.equals(matchingFieldName) == false
+                        && IdFieldMapper.NAME.equals(matchingFieldName) == false
+                        && searchExecutionContext.isMetadataField(matchingFieldName)
+                        && fieldType.isStored()) {
+                        metadataFields.add(new FieldAndFormat(matchingFieldName, null));
+                    }
+                }
+            }
+            metadataFieldFetcher = FieldFetcher.create(searchExecutionContext, metadataFields);
+        } else {
+            metadataFieldFetcher = null;
+        }
         return new FetchSubPhaseProcessor() {
             @Override
             public void setNextReader(LeafReaderContext readerContext) {
                 if (fieldFetcher != null) {
                     fieldFetcher.setNextReader(readerContext);
                 }
-                metadataFieldFetcher.setNextReader(readerContext);
+                if (metadataFieldFetcher != null) {
+                    metadataFieldFetcher.setNextReader(readerContext);
+                }
             }
 
             @Override
@@ -76,11 +104,13 @@ public final class FetchFieldsPhase implements FetchSubPhase {
 
             @Override
             public void process(HitContext hitContext) throws IOException {
-                Map<String, DocumentField> docFields = fieldFetcher == null
-                    ? Collections.emptyMap()
-                    : fieldFetcher.fetch(hitContext.source(), hitContext.docId());
-                Map<String, DocumentField> metaFields = metadataFieldFetcher.fetch(hitContext.source(), hitContext.docId());
-                hitContext.hit().addDocumentFields(docFields, metaFields);
+                final Map<String, DocumentField> fields = fieldFetcher != null
+                    ? fieldFetcher.fetch(hitContext.source(), hitContext.docId())
+                    : Collections.emptyMap();
+                final Map<String, DocumentField> metadataFields = metadataFieldFetcher != null
+                    ? metadataFieldFetcher.fetch(hitContext.source(), hitContext.docId())
+                    : Collections.emptyMap();
+                hitContext.hit().addDocumentFields(fields, metadataFields);
             }
         };
     }

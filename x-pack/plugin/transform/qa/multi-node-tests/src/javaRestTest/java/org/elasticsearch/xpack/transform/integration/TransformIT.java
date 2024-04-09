4099b8599b89677366bfc447f2a2a84e4673384f
Przemyslaw Witek <przemyslaw.witek@elastic.co>
2024-04-09 17:24:56 +0200

    [Transform] Introduce _transform/_node_stats API

--- x-pack/plugin/transform/qa/multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/transform/integration/TransformIT.java
+++ x-pack/plugin/transform/qa/multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/transform/integration/TransformIT.java
@@ -245,16 +245,19 @@ public class TransformIT extends TransformRestTestCase {
                 putTransform(transformId, config, RequestOptions.DEFAULT);
                 assertThat(getTransformTasks(), is(empty()));
                 assertThat(getTransformTasksFromClusterState(transformId), is(empty()));
+                assertThat(getTotalRegisteredTransformCount(), is(equalTo(0)));
 
                 startTransform(transformId, RequestOptions.DEFAULT);
                 // There is 1 transform task after start.
                 assertThat(getTransformTasks(), hasSize(1));
                 assertThat(getTransformTasksFromClusterState(transformId), hasSize(1));
+                assertThat(getTotalRegisteredTransformCount(), is(equalTo(1)));
 
                 Thread.sleep(sleepAfterStartMillis);
                 // There should still be 1 transform task as the transform is continuous.
                 assertThat(getTransformTasks(), hasSize(1));
                 assertThat(getTransformTasksFromClusterState(transformId), hasSize(1));
+                assertThat(getTotalRegisteredTransformCount(), is(equalTo(1)));
 
                 // Stop the transform with force set randomly.
                 stopTransform(transformId, true, null, false, force);
@@ -268,6 +271,7 @@ public class TransformIT extends TransformRestTestCase {
                 }
                 // After the transform is stopped, there should be no transform task left in the cluster state.
                 assertThat(getTransformTasksFromClusterState(transformId), is(empty()));
+                assertThat(getTotalRegisteredTransformCount(), is(equalTo(0)));
 
                 // Delete the transform
                 deleteTransform(transformId);

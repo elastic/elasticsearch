85efb6cea3b02a8f6809d619bded916711ba7bc3
David Turner <david.turner@elastic.co>
2024-04-09 07:59:05 +0100

    Spotless

--- server/src/test/java/org/elasticsearch/action/bulk/TransportBulkActionIngestTests.java
+++ server/src/test/java/org/elasticsearch/action/bulk/TransportBulkActionIngestTests.java
@@ -63,7 +63,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;

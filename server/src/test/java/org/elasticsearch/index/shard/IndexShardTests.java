0ba474c1cee21e571d738a9e96e3e1fdd3d8e036
Artem Prigoda <artem.prigoda@elastic.co>
2024-04-10 17:08:03 +0200

    Run spotless

--- server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
+++ server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
@@ -3995,11 +3995,7 @@ public class IndexShardTests extends IndexShardTestCase {
         safeAwait(flushesLatch);
 
         FlushStats flushStats = shard.flushStats();
-        assertThat(
-            "Flush time excluding waiting should be captured",
-            flushStats.getTotalTimeExcludingWaitingMillis(),
-            greaterThan(0L)
-        );
+        assertThat("Flush time excluding waiting should be captured", flushStats.getTotalTimeExcludingWaitingMillis(), greaterThan(0L));
         assertThat(
             "Flush time excluding waiting should less than flush time with waiting",
             flushStats.getTotalTimeExcludingWaitingMillis(),

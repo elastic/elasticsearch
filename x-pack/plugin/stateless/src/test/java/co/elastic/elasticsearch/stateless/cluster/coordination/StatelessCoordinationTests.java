228f939c751a68d231f1916647e6776e58b47b15
Dianna Hohensee <dianna.hohensee@elastic.co>
2024-04-10 10:40:28 -0400

    Log skipped elections due to shutdown marker (#1563)

--- x-pack/plugin/stateless/src/test/java/co/elastic/elasticsearch/stateless/cluster/coordination/StatelessCoordinationTests.java
+++ x-pack/plugin/stateless/src/test/java/co/elastic/elasticsearch/stateless/cluster/coordination/StatelessCoordinationTests.java
@@ -19,10 +19,13 @@ package co.elastic.elasticsearch.stateless.cluster.coordination;
 
 import co.elastic.elasticsearch.stateless.test.FakeStatelessNode;
 
+import org.apache.logging.log4j.Level;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.coordination.AbstractCoordinatorTestCase;
 import org.elasticsearch.cluster.coordination.AtomicRegisterCoordinatorTests;
 import org.elasticsearch.cluster.coordination.CoordinationState;
+import org.elasticsearch.cluster.coordination.Coordinator;
 import org.elasticsearch.cluster.coordination.ElectionStrategy;
 import org.elasticsearch.cluster.coordination.LeaderHeartbeatService;
 import org.elasticsearch.cluster.coordination.PreVoteCollector;
@@ -34,6 +37,8 @@ import org.elasticsearch.cluster.coordination.stateless.HeartbeatStore;
 import org.elasticsearch.cluster.coordination.stateless.SingleNodeReconfigurator;
 import org.elasticsearch.cluster.coordination.stateless.StoreHeartbeatService;
 import org.elasticsearch.cluster.metadata.Metadata;
+import org.elasticsearch.cluster.metadata.NodesShutdownMetadata;
+import org.elasticsearch.cluster.metadata.SingleNodeShutdownMetadata;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.version.CompatibilityVersionsUtils;
 import org.elasticsearch.common.blobstore.BlobContainer;
@@ -47,20 +52,31 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.core.IOUtils;
 import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.logging.LogManager;
+import org.elasticsearch.logging.Logger;
+import org.elasticsearch.test.MockLogAppender;
 import org.elasticsearch.test.junit.annotations.TestLogging;
 import org.elasticsearch.threadpool.ThreadPool;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.concurrent.Executor;
 import java.util.function.BooleanSupplier;
 import java.util.function.Function;
 import java.util.function.LongSupplier;
 
+import static org.elasticsearch.cluster.coordination.AbstractCoordinatorTestCase.DEFAULT_CLUSTER_STATE_UPDATE_DELAY;
+import static org.elasticsearch.cluster.coordination.AbstractCoordinatorTestCase.DEFAULT_ELECTION_DELAY;
 import static org.elasticsearch.cluster.coordination.stateless.StoreHeartbeatService.HEARTBEAT_FREQUENCY;
 import static org.elasticsearch.cluster.coordination.stateless.StoreHeartbeatService.MAX_MISSED_HEARTBEATS;
+import static org.elasticsearch.test.ESTestCase.randomIntBetween;
+import static org.elasticsearch.test.ESTestCase.randomNonNegativeLong;
+import static org.elasticsearch.test.MockLogAppender.assertThatLogger;
 
 @TestLogging(reason = "these tests do a lot of log-worthy things but we usually don't care", value = "org.elasticsearch:FATAL")
 public class StatelessCoordinationTests extends AtomicRegisterCoordinatorTests {
+    private static final Logger logger = LogManager.getLogger(StatelessCoordinationTests.class);
+
     @Override
     protected CoordinatorStrategy createCoordinatorStrategy() {
         final var inMemoryHeartBeatStore = new InMemoryHeartBeatStore();
@@ -79,6 +95,69 @@ public class StatelessCoordinationTests extends AtomicRegisterCoordinatorTests {
     @Override
     public void testWarnLoggingOnRegisterFailures() {
         // This test injects failures using some features specific to the AtomicRegisterCoordinatorStrategy so it doesn't make sense here
+    }
+
+    /**
+     * A node that is in the process of shutting down can try to run for election, but should fail locally with an error message due to shut
+     * down in progress. This test sets a node as shutting down (in the cluster state), forces it to run for election, and then verifies
+     * that an error message gets logged and the node refuses to run for election.
+     */
+    @TestLogging(reason = "test uses a Coordinator log msg", value = "org.elasticsearch.cluster.coordination.Coordinator:TRACE")
+    public void testShutdownLogMessageDuringElectionAttempt() {
+        // There are two nodes so that a non-leader can be set to shutdown and then made to try to run for election.
+        try (AbstractCoordinatorTestCase.Cluster cluster = new AbstractCoordinatorTestCase.Cluster(randomIntBetween(2, 5))) {
+            logger.info("---> Running a test cluster of [" + cluster.size() + "] nodes");
+            cluster.runRandomly();
+            cluster.stabilise();
+
+            final AbstractCoordinatorTestCase.Cluster.ClusterNode leader = cluster.getAnyLeader();
+            final AbstractCoordinatorTestCase.Cluster.ClusterNode nonLeader = cluster.getAnyNodeExcept(leader);
+            ClusterState originalClusterState = cluster.getAnyLeader().getLastAppliedClusterState();
+
+            // Update the cluster state to say that nonLeader is shutting down. We do not need to worry about shutdown actually taking
+            // place because the base test class' ClusterNode implementation doesn't include shutdown logic.
+            {
+                SingleNodeShutdownMetadata.Type type = SingleNodeShutdownMetadata.Type.RESTART;
+                NodesShutdownMetadata nodesShutdownMetadata = new NodesShutdownMetadata(
+                    Collections.singletonMap(
+                        nonLeader.getLocalNode().getId(),
+                        SingleNodeShutdownMetadata.builder()
+                            .setNodeId(nonLeader.getLocalNode().getId())
+                            .setReason("shutdown for a unit test")
+                            .setType(type)
+                            .setStartedAtMillis(randomNonNegativeLong())
+                            .setGracePeriod(null)
+                            .build()
+                    )
+                );
+                final ClusterState shutdownClusterState = ClusterState.builder(originalClusterState)
+                    .metadata(
+                        Metadata.builder(originalClusterState.metadata())
+                            .putCustom(NodesShutdownMetadata.TYPE, nodesShutdownMetadata)
+                            .build()
+                    )
+                    .version(originalClusterState.version() + 1)
+                    .build();
+
+                leader.submitUpdateTask("set node shutting down", (currentClusterState) -> shutdownClusterState, (e) -> {});
+                cluster.runFor(DEFAULT_CLUSTER_STATE_UPDATE_DELAY, "committing node shutdown update");
+            }
+
+            assertThatLogger(() -> {
+                // Force the nonLeader node to become a candidate for master election. This will trigger the election code and the log
+                // message saying that the node is ineligible for election during shutdown
+                AbstractCoordinatorTestCase.Cluster.becomeCandidate(nonLeader, "forcedForTesting");
+                cluster.runFor(DEFAULT_ELECTION_DELAY, "waiting for node to try to run for election");
+            },
+                Coordinator.class,
+                new MockLogAppender.SeenEventExpectation(
+                    "log emitted by Coordinator when shut down is in progress",
+                    Coordinator.class.getCanonicalName(),
+                    Level.TRACE,
+                    "skip prevoting as local node may not win election (node is ineligible for election during shutdown)*"
+                )
+            );
+        }
     }
 
     class StatelessCoordinatorStrategy implements CoordinatorStrategy {

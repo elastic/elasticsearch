c57dd98ef483e3e87fb01d3ca6542f86a30298ad
Carlos Delgado <6339205+carlosdelest@users.noreply.github.com>
2024-04-10 16:44:46 +0200

    semantic_text: Add index metadata information for inference field mappers (#107147)
    Co-authored-by: @jimczi 
    Co-authored-by: @Mikep86

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -166,6 +166,7 @@ public class TransportVersions {
     public static final TransportVersion INDEXING_PRESSURE_DOCUMENT_REJECTIONS_COUNT = def(8_625_00_0);
     public static final TransportVersion ALIAS_ACTION_RESULTS = def(8_626_00_0);
     public static final TransportVersion HISTOGRAM_AGGS_KEY_SORTED = def(8_627_00_0);
+    public static final TransportVersion INFERENCE_FIELDS_METADATA = def(8_628_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

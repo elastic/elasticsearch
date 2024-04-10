45e47753a67be9a29d3076530370ccf5b57ee4e2
Max Hniebergall <137079448+maxhniebergall@users.noreply.github.com>
2024-04-10 18:47:31 -0400

    [ML] Rerank response format change (#107288)
    * Change rerank result types, add named writables, new transportVersion
    
    * Add description for namedWriteables
    
    * fix RankedDocsResults toString
    
    ---------
    
    Co-authored-by: David Kyle <david.kyle@elastic.co>

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -169,6 +169,7 @@ public class TransportVersions {
     public static final TransportVersion INFERENCE_FIELDS_METADATA = def(8_628_00_0);
     public static final TransportVersion ML_INFERENCE_TIMEOUT_ADDED = def(8_629_00_0);
     public static final TransportVersion MODIFY_DATA_STREAM_FAILURE_STORES = def(8_630_00_0);
+    public static final TransportVersion ML_INFERENCE_RERANK_NEW_RESPONSE_FORMAT = def(8_631_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

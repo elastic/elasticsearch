141dd1b9bc764efbb606074ac5c96e7ad9b1ebd4
Albert Zaharovits <albert.zaharovits@elastic.co>
2024-04-12 13:40:55 +0300

    WIP

---
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/RoleMappingMetadata.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.core.security.authz;
+
+import org.elasticsearch.TransportVersion;
+import org.elasticsearch.TransportVersions;
+import org.elasticsearch.cluster.AbstractNamedDiffable;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.NamedDiff;
+import org.elasticsearch.cluster.metadata.Metadata;
+import org.elasticsearch.common.collect.Iterators;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.xcontent.ChunkedToXContentHelper;
+import org.elasticsearch.xcontent.ToXContent;
+import org.elasticsearch.xpack.core.security.authc.support.mapper.ExpressionRoleMapping;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
+
+public final class RoleMappingMetadata extends AbstractNamedDiffable<ClusterState.Custom> implements ClusterState.Custom {
+
+    public static final String TYPE = "role_mappings";
+
+    private static final RoleMappingMetadata EMPTY = new RoleMappingMetadata(Set.of());
+
+    public static RoleMappingMetadata getFromClusterState(ClusterState clusterState) {
+        return clusterState.custom(RoleMappingMetadata.TYPE, RoleMappingMetadata.EMPTY);
+    }
+
+    private final Set<ExpressionRoleMapping> roleMappings;
+
+    public RoleMappingMetadata(Set<ExpressionRoleMapping> roleMappings) {
+        this.roleMappings = roleMappings;
+    }
+
+    public RoleMappingMetadata(StreamInput input) throws IOException {
+        this.roleMappings = input.readCollectionAsSet(ExpressionRoleMapping::new);
+    }
+
+    public Set<ExpressionRoleMapping> getRoleMappings() {
+        return this.roleMappings;
+    }
+
+    public boolean isEmpty() {
+        return roleMappings.isEmpty();
+    }
+
+    public ClusterState updateClusterState(ClusterState clusterState) {
+        if (isEmpty()) {
+            // prefer no role mapping custom metadata to the empty role mapping metadata
+            return clusterState.copyAndUpdate(b -> b.removeCustom(RoleMappingMetadata.TYPE));
+        } else {
+            return clusterState.copyAndUpdate(b -> b.putCustom(RoleMappingMetadata.TYPE, this));
+        }
+    }
+
+    public static NamedDiff<Metadata.Custom> readDiffFrom(StreamInput streamInput) throws IOException {
+        return readDiffFrom(Metadata.Custom.class, TYPE, streamInput);
+    }
+
+    @Override
+    public Iterator<? extends ToXContent> toXContentChunked(ToXContent.Params params) {
+        return Iterators.concat(ChunkedToXContentHelper.startArray(TYPE), roleMappings.iterator(), ChunkedToXContentHelper.endArray());
+    }
+
+    @Override
+    public String getWriteableName() {
+        return TYPE;
+    }
+
+    @Override
+    public TransportVersion getMinimalSupportedVersion() {
+        return TransportVersions.SECURITY_ROLE_MAPPINGS_IN_CLUSTER_STATE;
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        out.writeCollection(roleMappings);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final var other = (RoleMappingMetadata) o;
+        return Objects.equals(roleMappings, other.roleMappings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleMappings);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("RoleMapping[entries=[");
+        final Iterator<ExpressionRoleMapping> entryList = roleMappings.iterator();
+        boolean firstEntry = true;
+        while (entryList.hasNext()) {
+            if (firstEntry == false) {
+                builder.append(",");
+            }
+            builder.append(entryList.next().toString());
+            firstEntry = false;
+        }
+        return builder.append("]]").toString();
+    }
+}
---
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/support/mapper/ClusterStateRoleMapper.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.security.authc.support.mapper;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.cluster.ClusterChangedEvent;
+import org.elasticsearch.cluster.ClusterStateListener;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.script.ScriptService;
+import org.elasticsearch.xpack.core.security.authc.support.CachingRealm;
+import org.elasticsearch.xpack.core.security.authc.support.UserRoleMapper;
+import org.elasticsearch.xpack.core.security.authc.support.mapper.ExpressionRoleMapping;
+import org.elasticsearch.xpack.core.security.authc.support.mapper.expressiondsl.ExpressionModel;
+import org.elasticsearch.xpack.core.security.authz.RoleMappingMetadata;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+
+public final class ClusterStateRoleMapper implements UserRoleMapper, ClusterStateListener {
+
+    private static final Logger logger = LogManager.getLogger(ClusterStateRoleMapper.class);
+
+    private final ScriptService scriptService;
+    private final ClusterService clusterService;
+    private final CopyOnWriteArrayList<Runnable> clearCacheListeners = new CopyOnWriteArrayList<>();
+
+    public ClusterStateRoleMapper(ScriptService scriptService, ClusterService clusterService) {
+        this.scriptService = scriptService;
+        this.clusterService = clusterService;
+        clusterService.addListener(this);
+    }
+
+    @Override
+    public void resolveRoles(UserData user, ActionListener<Set<String>> listener) {
+        ExpressionModel model = user.asModel();
+        Set<String> roles = getMappings().stream()
+            .filter(ExpressionRoleMapping::isEnabled)
+            .filter(m -> m.getExpression().match(model))
+            .flatMap(m -> {
+                Set<String> roleNames = m.getRoleNames(scriptService, model);
+                logger.trace("Applying role-mapping [{}] to user-model [{}] produced role-names [{}]", m.getName(), model, roleNames);
+                return roleNames.stream();
+            })
+            .collect(Collectors.toSet());
+        logger.debug("Mapping user [{}] to roles [{}]", user, roles);
+        listener.onResponse(roles);
+    }
+
+    @Override
+    public void refreshRealmOnChange(CachingRealm realm) {
+        clearCacheListeners.add(realm::expireAll);
+    }
+
+    private Set<ExpressionRoleMapping> getMappings() {
+        return RoleMappingMetadata.getFromClusterState(clusterService.state()).getRoleMappings();
+    }
+
+    private void notifyClearCache() {
+        clearCacheListeners.forEach(Runnable::run);
+    }
+
+    @Override
+    public void clusterChanged(ClusterChangedEvent event) {
+        if (false == Objects.equals(
+            RoleMappingMetadata.getFromClusterState(event.previousState()),
+            RoleMappingMetadata.getFromClusterState(event.state())
+        )) {
+            notifyClearCache();
+        }
+    }
+}

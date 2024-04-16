66beeb4ed4e9ecc849a194bbaf434936b2a0fa76
ghostspiders <15834128411@126.com>
2024-04-16 22:22:52 +0800

    buildkite test

--- server/src/test/java/org/elasticsearch/search/vectors/AbstractKnnVectorQueryBuilderTestCase.java
+++ server/src/test/java/org/elasticsearch/search/vectors/AbstractKnnVectorQueryBuilderTestCase.java
@@ -150,15 +150,18 @@ abstract class AbstractKnnVectorQueryBuilderTestCase extends AbstractQueryTestCa
         assertThat(e.getMessage(), containsString("the query vector has a different dimension [2] than the index vectors [3]"));
     }
 
-    public void testNonexistentField() throws IOException {
+    public void testNonexistentField(){
+        SearchExecutionContext context = createSearchExecutionContext();
+        KnnVectorQueryBuilder query = new KnnVectorQueryBuilder("nonexistent", new float[] { 1.0f, 1.0f, 1.0f }, 10, null);
+        context.setAllowUnmappedFields(false);
+        QueryShardException e = expectThrows(QueryShardException.class, () -> query.doToQuery(context));
+        assertThat(e.getMessage(), containsString("No field mapping can be found for the field with name [nonexistent]"));
+    }
+    public void testNonexistentFieldReturnEmpty() throws IOException {
         SearchExecutionContext context = createSearchExecutionContext();
         KnnVectorQueryBuilder query = new KnnVectorQueryBuilder("nonexistent", new float[] { 1.0f, 1.0f, 1.0f }, 10, null);
         Query queryNone = query.doToQuery(context);
         assertThat(queryNone, instanceOf(MatchNoDocsQuery.class));
-
-        context.setAllowUnmappedFields(false);
-        QueryShardException e = expectThrows(QueryShardException.class, () -> query.doToQuery(context));
-        assertThat(e.getMessage(), containsString("No field mapping can be found for the field with name [nonexistent]"));
     }
 
     public void testWrongFieldType() {

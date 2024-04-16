cb1bb4f6ce4a8de4332a448c0a36bc4fab64025f
Alexander Spies <alexander.spies@elastic.co>
2024-04-16 11:39:42 +0200

    Add esql version in multi-cluster-search-security

--- x-pack/qa/multi-cluster-search-security/legacy-with-basic-license/src/test/java/org/elasticsearch/xpack/security/MultiClusterSearchWithSecurityYamlTestSuiteIT.java
+++ x-pack/qa/multi-cluster-search-security/legacy-with-basic-license/src/test/java/org/elasticsearch/xpack/security/MultiClusterSearchWithSecurityYamlTestSuiteIT.java
@@ -14,8 +14,18 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
 import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
+import org.elasticsearch.test.rest.yaml.section.ApiCallSection;
+import org.elasticsearch.test.rest.yaml.section.ClientYamlTestSection;
+import org.elasticsearch.test.rest.yaml.section.DoSection;
+import org.elasticsearch.test.rest.yaml.section.ExecutableSection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class MultiClusterSearchWithSecurityYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
+    private static final String ESQL_VERSION = "2024.04.01";
 
     private static final String USER = "test_user";
     private static final String PASS = "x-pack-test-password";
@@ -41,12 +51,57 @@ public class MultiClusterSearchWithSecurityYamlTestSuiteIT extends ESClientYamlS
 
     @ParametersFactory
     public static Iterable<Object[]> parameters() throws Exception {
-        return ESClientYamlSuiteTestCase.createParameters();
+        return updateEsqlQueryDoSections(createParameters(), MultiClusterSearchWithSecurityYamlTestSuiteIT::setEsqlVersion);
     }
 
     @Override
     protected Settings restClientSettings() {
         String token = basicAuthHeaderValue(USER, new SecureString(PASS.toCharArray()));
         return Settings.builder().put(ThreadContext.PREFIX + ".Authorization", token).build();
+    }
+
+    // TODO: refactor, copied from ESQL's single-node AbstractEsqlClientYamlIt
+    public static Iterable<Object[]> updateEsqlQueryDoSections(Iterable<Object[]> parameters, Function<DoSection, ExecutableSection> modify)
+        throws Exception {
+        List<Object[]> result = new ArrayList<>();
+        for (Object[] orig : parameters) {
+            assert orig.length == 1;
+            ClientYamlTestCandidate candidate = (ClientYamlTestCandidate) orig[0];
+            try {
+                ClientYamlTestSection modified = new ClientYamlTestSection(
+                    candidate.getTestSection().getLocation(),
+                    candidate.getTestSection().getName(),
+                    candidate.getTestSection().getPrerequisiteSection(),
+                    candidate.getTestSection().getExecutableSections().stream().map(e -> modifyExecutableSection(e, modify)).toList()
+                );
+                result.add(new Object[] { new ClientYamlTestCandidate(candidate.getRestTestSuite(), modified) });
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("error modifying " + candidate + ": " + e.getMessage(), e);
+            }
+        }
+        return result;
+    }
+
+    // TODO: refactor, copied from ESQL's single-node AbstractEsqlClientYamlIt
+    private static ExecutableSection modifyExecutableSection(ExecutableSection e, Function<DoSection, ExecutableSection> modify) {
+        if (false == (e instanceof DoSection)) {
+            return e;
+        }
+        DoSection doSection = (DoSection) e;
+        String api = doSection.getApiCallSection().getApi();
+        return switch (api) {
+            case "esql.query" -> modify.apply(doSection);
+            default -> e;
+        };
+    }
+
+    // TODO: refactor, copied from ESQL's single-node AbstractEsqlClientYamlIt
+    static DoSection setEsqlVersion(DoSection doSection) {
+        ApiCallSection copy = doSection.getApiCallSection().copyWithNewApi(doSection.getApiCallSection().getApi());
+        for (Map<String, Object> body : copy.getBodies()) {
+            body.putIfAbsent("version", ESQL_VERSION);
+        }
+        doSection.setApiCallSection(copy);
+        return doSection;
     }
 }

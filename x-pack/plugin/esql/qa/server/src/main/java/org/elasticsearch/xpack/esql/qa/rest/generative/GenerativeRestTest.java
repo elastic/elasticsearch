f7c28129e173a687cdee5a6d323ed6aaff7ad1c7
Alexander Spies <alexander.spies@elastic.co>
2024-04-16 12:37:48 +0200

    Add version to RestEnrichTestCase

--- x-pack/plugin/esql/qa/server/src/main/java/org/elasticsearch/xpack/esql/qa/rest/generative/GenerativeRestTest.java
+++ x-pack/plugin/esql/qa/server/src/main/java/org/elasticsearch/xpack/esql/qa/rest/generative/GenerativeRestTest.java
@@ -11,6 +11,7 @@ import org.elasticsearch.client.Request;
 import org.elasticsearch.client.ResponseException;
 import org.elasticsearch.test.rest.ESRestTestCase;
 import org.elasticsearch.xpack.esql.CsvTestsDataLoader;
+import org.elasticsearch.xpack.esql.EsqlTestUtils;
 import org.elasticsearch.xpack.esql.qa.rest.RestEsqlTestCase;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -96,7 +97,9 @@ public abstract class GenerativeRestTest extends ESRestTestCase {
 
     private EsqlQueryGenerator.QueryExecuted execute(String command, int depth) {
         try {
-            Map<String, Object> a = RestEsqlTestCase.runEsqlSync(new RestEsqlTestCase.RequestObjectBuilder().query(command).build());
+            Map<String, Object> a = RestEsqlTestCase.runEsqlSync(
+                new RestEsqlTestCase.RequestObjectBuilder().query(command).version(EsqlTestUtils.latestEsqlVersionOrSnapshot()).build()
+            );
             List<EsqlQueryGenerator.Column> outputSchema = outputSchema(a);
             return new EsqlQueryGenerator.QueryExecuted(command, depth, outputSchema, null);
         } catch (Exception e) {

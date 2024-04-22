ea320e56e9447cd48841066d5eac970e5c4eb1ce
Yang Wang <yang.wang@elastic.co>
2024-04-22 22:08:33 +1000

    Remove test only simulate method from StatelessCommitService (#1747)
    The simulate method can now be replaced with existing methods now that
    we have assertions guarded by the stateless.upload.delayed setting.
    
    Relates: #1641

--- x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/test/FakeStatelessNode.java
+++ x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/test/FakeStatelessNode.java
@@ -433,6 +433,10 @@ public class FakeStatelessNode implements Closeable {
         return innerContainer;
     }
 
+    public BlobContainer getShardContainer() {
+        return objectStoreService.getBlobContainer(shardId, primaryTerm);
+    }
+
     @Override
     public void close() throws IOException {
         IOUtils.close(closeables);

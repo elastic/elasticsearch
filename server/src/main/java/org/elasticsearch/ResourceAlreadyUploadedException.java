25cb991e129c175e2de913af2b16024ff4738b21
Iraklis Psaroudakis <kingherc@gmail.com>
2024-04-22 16:20:34 +0300

    Add ResourceAlreadyUploadedException
    Plus assertFailures variation with Set of accepted rest statuses.
    
    Relates ES-8156

---
+++ server/src/main/java/org/elasticsearch/ResourceAlreadyUploadedException.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+package org.elasticsearch;
+
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.rest.RestStatus;
+
+import java.io.IOException;
+
+public class ResourceAlreadyUploadedException extends ElasticsearchException {
+
+    public ResourceAlreadyUploadedException(String msg, Object... args) {
+        super(msg, args);
+    }
+
+    public ResourceAlreadyUploadedException(StreamInput in) throws IOException {
+        super(in);
+    }
+
+    @Override
+    public final RestStatus status() {
+        return RestStatus.NOT_FOUND;
+    }
+}

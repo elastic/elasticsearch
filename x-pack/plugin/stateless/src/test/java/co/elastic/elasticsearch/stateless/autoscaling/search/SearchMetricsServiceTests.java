fd35da3eea39514cff8cd2d0b42aeb924fa3f89d
Christoph Büscher <christophbuescher@posteo.de>
2024-04-24 11:22:16 +0200

    Configure replicas within boost window for search power>=100 and <250 (#1587)
    For search power (SP) values between 100 (inclusive) and 250 (non inclusive) we
    want to increase the replication for some indices to 2 (below 100, replication
    is always 1, above 250 it is 2). We only want to do this for indices in the
    current boost window. This should be all regular and system indices as well as
    backing indices of data streams that are in the current boost window. Those
    should have a positive "interactive" size for at least one shard.
    
    We rank indices according to the following order:
    
    * system indices
    * regular indices
    * data streams (backing indices)
    
    Within system and regular indices we rank larger indices first (interactive
    size, which in this case should be equal to total size), tie breaking on index
    recency (creation time, newer indices first).
    
    Withing data streams, we want to rank all write indices highest, followed by the
    most recent ones (creation time), tie breaking by interactive size.
    
    The search power setting determines which of these ranked indices should get 2
    replicas. The threshold calculation is:
    
    {total_interactive_size} * (search power - 100) / (250 - 100)
    
    where total_interactive_size is the sum of all interactive sizes of all indices.
    We calculate the "cumulative" size over the ranked indices as the sum of all
    "interactive" sizes of all indices with higher rank, including the index itself.
    Indices with "cumulative" size larger than the threshold should get 2 replicas.
    This means that e.g. for SP=100, no index gets 2 replicas. For SP=175, we try to
    give as many indices 2 replicas so that their summed interactive size doesn't
    exceed half of the total interactive size of all indices (threshold =
    total_interactive_size * (175-100)/150)
    
    This change adds the ranking logic, threshold calculation and index selection to
    the already existing SearchMetricsService#getNumberOfReplicaChanges method and
    adds tests.
    
    Relates to ES-7924

--- x-pack/plugin/stateless/src/test/java/co/elastic/elasticsearch/stateless/autoscaling/search/SearchMetricsServiceTests.java
+++ x-pack/plugin/stateless/src/test/java/co/elastic/elasticsearch/stateless/autoscaling/search/SearchMetricsServiceTests.java
@@ -34,6 +34,7 @@ import org.elasticsearch.cluster.ClusterChangedEvent;
 import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.block.ClusterBlocks;
+import org.elasticsearch.cluster.metadata.DataStream;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.node.DiscoveryNodeRole;
@@ -51,6 +52,7 @@ import org.elasticsearch.common.util.set.Sets;
 import org.elasticsearch.core.Strings;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.gateway.GatewayService;
+import org.elasticsearch.index.IndexVersion;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.MockLogAppender;
@@ -73,6 +75,7 @@ import java.util.stream.IntStream;
 
 import static co.elastic.elasticsearch.stateless.autoscaling.search.SearchMetricsService.ACCURATE_METRICS_WINDOW_SETTING;
 import static co.elastic.elasticsearch.stateless.autoscaling.search.SearchMetricsService.STALE_METRICS_CHECK_INTERVAL_SETTING;
+import static org.elasticsearch.cluster.metadata.DataStreamTestHelper.newInstance;
 import static org.elasticsearch.cluster.routing.TestShardRouting.newShardRouting;
 import static org.hamcrest.Matchers.aMapWithSize;
 import static org.hamcrest.Matchers.allOf;
@@ -806,6 +809,163 @@ public class SearchMetricsServiceTests extends ESTestCase {
         );
     }
 
+    record SearchPowerSteps(int sp, IndexMetadata expectedAdditionalIndex) {}
+
+    /**
+     * Test that increases search power in steps and asserts that indices that
+     * replica calculation for indices that originally have 1 replica is increased
+     * to 2
+     */
+    public void testGetNumberOfReplicaChangesSP100To250() {
+        int initialReplicas = 1;
+        Metadata.Builder clusterMetadata = Metadata.builder();
+        var index1 = createIndex(1, initialReplicas, clusterMetadata);
+        var index2 = createIndex(1, initialReplicas, clusterMetadata);
+        var systemIndex = createSystemIndex(1, initialReplicas, clusterMetadata);
+
+        String dataStream1Name = "ds1";
+        IndexMetadata ds1BackingIndex1 = createBackingIndex(dataStream1Name, 1, initialReplicas, 1714000000000L).build();
+        clusterMetadata.put(ds1BackingIndex1, false);
+        IndexMetadata ds1BackingIndex2 = createBackingIndex(dataStream1Name, 2, initialReplicas, 1715000000000L).build();
+        clusterMetadata.put(ds1BackingIndex2, false);
+        DataStream ds1 = newInstance(dataStream1Name, List.of(ds1BackingIndex1.getIndex(), ds1BackingIndex2.getIndex()));
+        clusterMetadata.put(ds1);
+
+        String dataStream2Name = "ds2";
+        IndexMetadata ds2BackingIndex1 = createBackingIndex(dataStream2Name, 1, initialReplicas, 1713000000000L).build();
+        clusterMetadata.put(ds2BackingIndex1, false);
+        IndexMetadata ds2BackingIndex2 = createBackingIndex(dataStream2Name, 2, initialReplicas, 1714000000000L).build();
+        clusterMetadata.put(ds2BackingIndex2, false);
+        DataStream ds2 = newInstance(dataStream2Name, List.of(ds2BackingIndex1.getIndex(), ds2BackingIndex2.getIndex()));
+        clusterMetadata.put(ds2);
+
+        var state1 = ClusterState.builder(ClusterState.EMPTY_STATE).nodes(createNodes(1)).metadata(clusterMetadata).build();
+        service.clusterChanged(new ClusterChangedEvent("test", state1, ClusterState.EMPTY_STATE));
+
+        Map<ShardId, ShardSize> shards = new HashMap<>();
+        addShard(shards, index1, 0, 2000, 0);
+        addShard(shards, index2, 0, 1000, 0);
+        addShard(shards, systemIndex, 0, 1000, 0);
+        addShard(shards, ds1BackingIndex1, 0, 500, 500);
+        addShard(shards, ds1BackingIndex2, 0, 750, 500);
+        addShard(shards, ds2BackingIndex1, 0, 500, 500);
+        addShard(shards, ds2BackingIndex2, 0, 250, 500);
+        service.processShardSizesRequest(new PublishShardSizesRequest("search_node_1", shards));
+
+        Map<String, Integer> expected = new HashMap<>();
+        service.updateSearchPower(100);
+
+        assertEquals(expected, service.getNumberOfReplicaChanges());
+
+        List<SearchPowerSteps> steps = List.of(
+            new SearchPowerSteps(125, systemIndex),
+            new SearchPowerSteps(175, index1),
+            new SearchPowerSteps(200, index2),
+            new SearchPowerSteps(220, ds1BackingIndex2),
+            new SearchPowerSteps(225, ds2BackingIndex2),
+            new SearchPowerSteps(249, ds1BackingIndex1),
+            new SearchPowerSteps(250, ds2BackingIndex1)
+        );
+
+        steps.forEach(step -> {
+            service.updateSearchPower(step.sp);
+            expected.put(step.expectedAdditionalIndex.getIndex().getName(), 2);
+            assertEquals("Searchpower " + step.sp, expected, service.getNumberOfReplicaChanges());
+        });
+
+        // check that size changes affect the ranking.
+        // We'll swap interactive size for index1 and index2
+        // also swap ds1BackingIndex2 and ds2BackingIndex2 since they should use size as tie-breaker, their
+        // recency is "now" because they are both write indices
+        shards = new HashMap<>();
+        addShard(shards, index1, 0, 1000, 0);
+        addShard(shards, index2, 0, 2000, 0);
+        addShard(shards, ds1BackingIndex2, 0, 250, 500);
+        addShard(shards, ds2BackingIndex2, 0, 750, 500);
+        service.processShardSizesRequest(new PublishShardSizesRequest("search_node_1", shards));
+
+        steps = List.of(
+            new SearchPowerSteps(125, systemIndex),
+            new SearchPowerSteps(175, index2),
+            new SearchPowerSteps(200, index1),
+            new SearchPowerSteps(220, ds2BackingIndex2),
+            new SearchPowerSteps(225, ds1BackingIndex2),
+            new SearchPowerSteps(249, ds1BackingIndex1),
+            new SearchPowerSteps(250, ds2BackingIndex1)
+        );
+
+        expected.clear();
+        steps.forEach(step -> {
+            service.updateSearchPower(step.sp);
+            expected.put(step.expectedAdditionalIndex.getIndex().getName(), 2);
+            assertEquals("Searchpower " + step.sp, expected, service.getNumberOfReplicaChanges());
+        });
+
+    }
+
+    /**
+     * Test that decreases search power in steps and asserts that indices that
+     * replica calculation for indices that originally have 2 replicas is decreased
+     * to 1
+     */
+    public void testGetNumberOfReplicaChangesSP250To100() {
+        int initialReplicas = 2;
+        Metadata.Builder clusterMetadata = Metadata.builder();
+        var index1 = createIndex(1, initialReplicas, clusterMetadata);
+        var index2 = createIndex(1, initialReplicas, clusterMetadata);
+        var systemIndex = createSystemIndex(1, initialReplicas, clusterMetadata);
+
+        String dataStream1Name = "ds1";
+        IndexMetadata ds1BackingIndex1 = createBackingIndex(dataStream1Name, 1, initialReplicas, 1714000000000L).build();
+        clusterMetadata.put(ds1BackingIndex1, false);
+        IndexMetadata ds1BackingIndex2 = createBackingIndex(dataStream1Name, 2, initialReplicas, 1715000000000L).build();
+        clusterMetadata.put(ds1BackingIndex2, false);
+        DataStream ds1 = newInstance(dataStream1Name, List.of(ds1BackingIndex1.getIndex(), ds1BackingIndex2.getIndex()));
+        clusterMetadata.put(ds1);
+
+        String dataStream2Name = "ds2";
+        IndexMetadata ds2BackingIndex1 = createBackingIndex(dataStream2Name, 1, initialReplicas, 1713000000000L).build();
+        clusterMetadata.put(ds2BackingIndex1, false);
+        IndexMetadata ds2BackingIndex2 = createBackingIndex(dataStream2Name, 2, initialReplicas, 1714000000000L).build();
+        clusterMetadata.put(ds2BackingIndex2, false);
+        DataStream ds2 = newInstance(dataStream2Name, List.of(ds2BackingIndex1.getIndex(), ds2BackingIndex2.getIndex()));
+        clusterMetadata.put(ds2);
+
+        var state1 = ClusterState.builder(ClusterState.EMPTY_STATE).nodes(createNodes(1)).metadata(clusterMetadata).build();
+
+        Map<ShardId, ShardSize> shards = new HashMap<>();
+        addShard(shards, index1, 0, 2000, 0);
+        addShard(shards, index2, 0, 1000, 0);
+        addShard(shards, systemIndex, 0, 1000, 0);
+        addShard(shards, ds1BackingIndex1, 0, 500, 500);
+        addShard(shards, ds1BackingIndex2, 0, 750, 500);
+        addShard(shards, ds2BackingIndex1, 0, 500, 500);
+        addShard(shards, ds2BackingIndex2, 0, 250, 500);
+        service.processShardSizesRequest(new PublishShardSizesRequest("search_node_1", shards));
+
+        List<SearchPowerSteps> steps = List.of(
+            new SearchPowerSteps(249, ds2BackingIndex1),
+            new SearchPowerSteps(225, ds1BackingIndex1),
+            new SearchPowerSteps(220, ds2BackingIndex2),
+            new SearchPowerSteps(200, ds1BackingIndex2),
+            new SearchPowerSteps(175, index2),
+            new SearchPowerSteps(150, index1),
+            new SearchPowerSteps(100, systemIndex)
+        );
+
+        Map<String, Integer> expected = new HashMap<>();
+
+        service.updateSearchPower(250);
+        service.clusterChanged(new ClusterChangedEvent("test", state1, ClusterState.EMPTY_STATE));
+        assertEquals(expected, service.getNumberOfReplicaChanges());
+
+        steps.forEach(step -> {
+            service.updateSearchPower(step.sp);
+            expected.put(step.expectedAdditionalIndex.getIndex().getName(), 1);
+            assertEquals("Searchpower " + step.sp, expected, service.getNumberOfReplicaChanges());
+        });
+    }
+
     public void testGetNumberOfReplicaChangesOnSearchPowerUpdate() {
         IndexMetadata outsideBoostWindowMetadata = createIndex(1, 1);
         IndexMetadata withinBoostWindowMetadata = createIndex(3, 1);
@@ -1188,10 +1348,48 @@ public class SearchMetricsServiceTests extends ESTestCase {
         return builder.build();
     }
 
+    private static IndexMetadata createIndex(int shards, int replicas, Metadata.Builder clusterMetadataBuilder) {
+        return createIndex(shards, replicas, false, clusterMetadataBuilder);
+    }
+
     private static IndexMetadata createIndex(int shards, int replicas) {
-        return IndexMetadata.builder(randomIdentifier())
+        return createIndex(shards, replicas, false, null);
+    }
+
+    private static IndexMetadata createSystemIndex(int shards, int replicas, Metadata.Builder clusterMetadataBuilder) {
+        return createIndex(shards, replicas, true, clusterMetadataBuilder);
+    }
+
+    private static IndexMetadata createIndex(int shards, int replicas, boolean system, Metadata.Builder clusterMetadataBuilder) {
+        IndexMetadata indexMetadata = IndexMetadata.builder(randomIdentifier())
             .settings(indexSettings(shards, replicas).put("index.version.created", Version.CURRENT))
+            .system(system)
             .build();
+        if (clusterMetadataBuilder != null) {
+            clusterMetadataBuilder.put(indexMetadata, false);
+        }
+        return indexMetadata;
+    }
+
+    public static IndexMetadata.Builder createBackingIndex(String dataStreamName, int generation, int numberOfReplicas, long epochMillis) {
+        return IndexMetadata.builder(DataStream.getDefaultBackingIndexName(dataStreamName, generation, epochMillis))
+            .settings(ESTestCase.settings(IndexVersion.current()).put("index.hidden", true))
+            .numberOfShards(1)
+            .numberOfReplicas(numberOfReplicas)
+            .creationDate(epochMillis);
+    }
+
+    private static void addShard(
+        Map<ShardId, ShardSize> shardSizes,
+        IndexMetadata index,
+        int shardId,
+        long interactiveSize,
+        long nonInteractiveSize
+    ) {
+        shardSizes.put(
+            new ShardId(index.getIndex(), shardId),
+            new ShardSize(interactiveSize, nonInteractiveSize, new PrimaryTermAndGeneration(1, 1))
+        );
     }
 
     private static void sendInRandomOrder(SearchMetricsService service, PublishShardSizesRequest... requests) {

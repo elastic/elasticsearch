a747a9aa5fbf336f948cb53520c5cb9aa25d2e18
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2024-04-26 11:43:48 +0200

    upd

--- x-pack/plugin/blob-cache/src/main/java/org/elasticsearch/blobcache/shared/SharedBlobCacheService.java
+++ x-pack/plugin/blob-cache/src/main/java/org/elasticsearch/blobcache/shared/SharedBlobCacheService.java
@@ -1156,8 +1156,8 @@ public class SharedBlobCacheService<KeyType> implements Releasable {
          * A caller should also only read up to length, further bytes will be offered by another call to this method
          *
          * @param channel is the cache region to read from
-         * @param channelPos a position in the channel to read from
-         * @param relativePos
+         * @param channelPos a position in the channel (cache file) to read from
+         * @param relativePos a position in the target buffer to store bytes and pass to the caller
          * @param length of the blob
          * @return number of bytes read
          * @throws IOException on failure
@@ -1171,8 +1171,8 @@ public class SharedBlobCacheService<KeyType> implements Releasable {
          * Callback method used to fetch data (usually from a remote storage) and write it in the cache.
          *
          * @param channel is the cache region to write to
-         * @param channelPos a position in the channel to write to
-         * @param relativePos the relative position in remote storate to read from
+         * @param channelPos a position in the channel (cache file) to write to
+         * @param relativePos the relative position in the remote storage to read from
          * @param length of data to fetch
          * @param progressUpdater consumer to invoke with the number of copied bytes as they are written in cache.
          *                        This is used to notify waiting readers that data become available in cache.

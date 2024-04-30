ee4ec776131eec57171d6ce85454ed0e155e10bb
Tom Veasey <tveasey@elastic.co>
2024-04-30 09:55:17 +0100

    Simplify and estimate average duration rather than rate

--- x-pack/dev-tools/average_inference_duration.py
+++ x-pack/dev-tools/average_inference_duration.py
@@ -11,30 +11,29 @@ import numpy as np
 VAR_R = 1.0
 
 
-H = np.array([1.0, 0.0, 0.0])
-x_0 = np.array([0.0, 0.0, 0.0])
-P_0 = np.eye(3) * 25.0 * VAR_R
+H = np.array([1.0, 0.0])
+x_0 = np.array([0.0, 0.0])
+P_0 = np.eye(2) * 64.0 * VAR_R
 
 
 def compute_Q(var_t: float, var_a: float) -> np.ndarray:
-    # We assume mean zero var_t noise on the time derivative of the average inference
-    # duration and mean zero var_a noise on the constant of proportionality between the
-    # allocation count and the average inference duration.
+    # We assume mean zero var_t noise on the average inference duration and mean zero
+    # var_a noise on the constant of proportionality between the allocation count and
+    # the average inference duration.
     var_t *= VAR_R
     var_a *= VAR_R
-    return np.array([[0.0, 0.0, 0.0], [0.0, var_t, 0.0], [0.0, 0.0, var_a]])
+    return np.array([[var_t, 0.0], [0.0, var_a]])
 
 
-def compute_F(dt: float, a: float) -> np.ndarray:
+def compute_F(a: float) -> np.ndarray:
     # Assume that the average inference duration dynmaics are:
     #
-    #  x_k = x_{k-1} + dt * d_{k-1} + c * a_{k-1}
+    #  x_k = x_{k-1} + c * a_{k-1}
     #
-    # Here, d_{k-1} is the rate of change of inference time with time (and is used to
-    # account for load on the node as a function of time) and c is the constant of
-    # proportionality between the allocation count and the inference time. It is used
-    # to account for the ratio physical and virtual cores the node is using for inference.
-    return np.array([[1.0, dt, a], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+    # Here, c is the constant of proportionality between the allocation count and the
+    # inference time. It is used to account for the ratio physical and virtual cores
+    # the node is using for inference.
+    return np.array([[1.0, a], [0.0, 1.0]])
 
 
 def compute_x_k_km1(F: np.ndarray, x_km1_km1: np.ndarray) -> np.ndarray:
@@ -82,7 +81,7 @@ def get_avg_inference_time(num_allocations):
 
 
 def get_inference_time(num_allocations):
-    return np.random.uniform(0.5, 2.0) * get_avg_inference_time(num_allocations)
+    return np.random.uniform(0.5, 1.5) * get_avg_inference_time(num_allocations)
 
 
 # Simulation...
@@ -104,8 +103,44 @@ ALLOCATION_SCHEDULE = np.concatenate(
 )
 
 
+class DurationEstimator:
+    def __init__(
+        self,
+        var_t: float = 1e-10,
+        var_a: float = 1e-12,
+        adjust_interval: float = 100.0,
+    ):
+        self.noise_var_t = var_t
+        self.noise_var_a = var_a
+        self.x_k = x_0
+        self.P_k = P_0
+        self.values_since_last_change = 0
+        self.last_allocations = 0
+        self.adjust_interval = adjust_interval
+
+    def add(self, duration: float, allocations: float) -> None:
+        scale = 1 + (1e4 - 1) * math.exp(
+            -self.values_since_last_change / self.adjust_interval
+        )
+        if allocations != self.last_allocations:
+            self.last_allocations = allocations
+            self.values_since_last_change = 0
+        self.values_since_last_change += 1
+        F_k = compute_F(allocations)
+        x_k_km1 = compute_x_k_km1(F_k, self.x_k)
+        P_k_km1 = compute_P_k_km1(
+            F_k, self.P_k, compute_Q(self.noise_var_t, scale * self.noise_var_a)
+        )
+        K_k = compute_K_k(P_k_km1)
+        self.x_k = compute_x_k_k(x_k_km1, K_k, duration)
+        self.P_k = compute_P_k_k(K_k, P_k_km1)
+
+    def estimate(self) -> float:
+        return self.x_k[0] + self.x_k[1] * self.last_allocations
+
+
 def estimate_average_inference_duration(
-    sigma_t: float = 1e-10, sigma_a: float = 1e-12, manouevre_time: float = 100.0
+    var_t: float = 1e-10, var_a: float = 1e-12, adjust_interval: float = 100.0
 ) -> tuple[list[float], list[float], list[float]]:
     """
     return: ("inference duration", "true average inference duration", "estimated average inference duration")
@@ -113,24 +148,11 @@ def estimate_average_inference_duration(
     rr = []
     zz = []
     zze = []
-    x_k = x_0
-    P_k = P_0
-    last_a = 0
-    last_change = 0
-    for i, a in enumerate(ALLOCATION_SCHEDULE):
-        z = 1.0 / get_inference_time(a)
-        F_k = compute_F(np.random.uniform(1.0, 5.0), a)
-        x_k_km1 = compute_x_k_km1(F_k, x_k)
-        if a != last_a:
-            last_a = a
-            last_change = i
-        scale = 1 + (1e3 - 1) * math.exp(-(i - last_change) / manouevre_time)
-        P_k_km1 = compute_P_k_km1(F_k, P_k, compute_Q(sigma_t, scale * sigma_a))
-        K_k = compute_K_k(P_k_km1)
-        x_k = compute_x_k_k(x_k_km1, K_k, z)
-        P_k = compute_P_k_k(K_k, P_k_km1)
+    estimator = DurationEstimator(var_t, var_a, adjust_interval)
+    for a in ALLOCATION_SCHEDULE:
+        z = get_inference_time(a)
+        estimator.add(z, a)
         rr.append(z)
-        # The math.log(4) / 1.5 comes from the integral of the inference time noise distribution.
-        zz.append(math.log(4) / 1.5 * 1.0 / get_avg_inference_time(a))
-        zze.append(x_k[0] + x_k[2] * a)
+        zz.append(get_avg_inference_time(a))
+        zze.append(estimator.estimate())
     return rr, zz, zze

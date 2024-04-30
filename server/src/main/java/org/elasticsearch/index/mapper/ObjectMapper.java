62632a7d262be8b823dcabb5e343c6780f57b3c7
Kostas Krikellas <kostas.krikellas@elastic.co>
2024-04-30 09:44:47 +0300

    minor refactor

--- server/src/main/java/org/elasticsearch/index/mapper/ObjectMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/ObjectMapper.java
@@ -788,20 +788,20 @@ public class ObjectMapper extends Mapper {
             if (hasValue == false) {
                 return;
             }
-            if (isEnabled() == false && isRoot()) {
-                // If the root object mapper is disabled, it is expected to contain
-                // the source encapsulated within a single ignored source value.
-                assert ignoredValues.size() == 1 : ignoredValues.size();
-                XContentDataHelper.decodeAndWrite(b, ignoredValues.get(0).value());
-                ignoredValues = null;
-                return;
-            }
             if (isRoot()) {
+                if (isEnabled() == false) {
+                    // If the root object mapper is disabled, it is expected to contain
+                    // the source encapsulated within a single ignored source value.
+                    assert ignoredValues.size() == 1 : ignoredValues.size();
+                    XContentDataHelper.decodeAndWrite(b, ignoredValues.get(0).value());
+                    ignoredValues = null;
+                    return;
+                }
                 b.startObject();
             } else {
                 b.startObject(simpleName());
             }
-            ;
+
             for (SourceLoader.SyntheticFieldLoader field : fields) {
                 if (field.hasValue()) {
                     field.write(b);

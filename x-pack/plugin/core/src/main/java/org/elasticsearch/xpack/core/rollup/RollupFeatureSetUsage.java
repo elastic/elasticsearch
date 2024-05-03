b792d7c80bec876ea44a07353b69d6d8fcb8fd60
Martijn van Groningen <martijn.v.groningen@gmail.com>
2024-05-03 12:01:17 +0200

    Add more rollup usage stats
    This change adds `number_of_rollup_jobs` and `number_of_rollup_indices` to  `rollup` usage. The former indicates the number of active rollup jobs running and the latter indicated the number of rollup indices (which could be the result of previous rollup jobs).

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/rollup/RollupFeatureSetUsage.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/rollup/RollupFeatureSetUsage.java
@@ -9,19 +9,56 @@ package org.elasticsearch.xpack.core.rollup;
 import org.elasticsearch.TransportVersion;
 import org.elasticsearch.TransportVersions;
 import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xpack.core.XPackFeatureSet;
 import org.elasticsearch.xpack.core.XPackField;
 
 import java.io.IOException;
 
+import static org.elasticsearch.TransportVersions.ROLLUP_USAGE;
+
 public class RollupFeatureSetUsage extends XPackFeatureSet.Usage {
+
+    private final int numberOfRollupJobs;
+    private final int numberOfRollupIndices;
 
     public RollupFeatureSetUsage(StreamInput input) throws IOException {
         super(input);
+        this.numberOfRollupJobs = input.getTransportVersion().onOrAfter(ROLLUP_USAGE) ? input.readVInt() : 0;
+        this.numberOfRollupIndices = input.getTransportVersion().onOrAfter(ROLLUP_USAGE) ? input.readVInt() : 0;
     }
 
-    public RollupFeatureSetUsage() {
+    public RollupFeatureSetUsage(int numberOfRollupIndices, int numberOfRollupJobs) {
         super(XPackField.ROLLUP, true, true);
+        this.numberOfRollupJobs = numberOfRollupJobs;
+        this.numberOfRollupIndices = numberOfRollupIndices;
+    }
+
+    public int getNumberOfRollupJobs() {
+        return numberOfRollupJobs;
+    }
+
+    public int getNumberOfRollupIndices() {
+        return numberOfRollupIndices;
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        super.writeTo(out);
+        if (out.getTransportVersion().onOrAfter(ROLLUP_USAGE)) {
+            out.writeVInt(numberOfRollupJobs);
+        }
+        if (out.getTransportVersion().onOrAfter(ROLLUP_USAGE)) {
+            out.writeVInt(numberOfRollupIndices);
+        }
+    }
+
+    @Override
+    protected void innerXContent(XContentBuilder builder, Params params) throws IOException {
+        super.innerXContent(builder, params);
+        builder.field("number_of_rollup_jobs", numberOfRollupJobs);
+        builder.field("number_of_rollup_indices", numberOfRollupIndices);
     }
 
     @Override
--- x-pack/plugin/rollup/src/main/java/org/elasticsearch/xpack/rollup/RollupUsageTransportAction.java
+++ x-pack/plugin/rollup/src/main/java/org/elasticsearch/xpack/rollup/RollupUsageTransportAction.java
@@ -12,16 +12,33 @@ import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.xcontent.XContentHelper;
+import org.elasticsearch.core.Predicates;
+import org.elasticsearch.persistent.PersistentTasksCustomMetadata;
 import org.elasticsearch.protocol.xpack.XPackUsageRequest;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
+import org.elasticsearch.xcontent.XContentParser;
+import org.elasticsearch.xcontent.XContentParserConfiguration;
 import org.elasticsearch.xpack.core.action.XPackUsageFeatureAction;
 import org.elasticsearch.xpack.core.action.XPackUsageFeatureResponse;
 import org.elasticsearch.xpack.core.action.XPackUsageFeatureTransportAction;
 import org.elasticsearch.xpack.core.rollup.RollupFeatureSetUsage;
+import org.elasticsearch.xpack.core.rollup.job.RollupJob;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.elasticsearch.common.xcontent.XContentParserUtils.ensureExpectedToken;
 
 public class RollupUsageTransportAction extends XPackUsageFeatureTransportAction {
+
+    private static final XContentParserConfiguration PARSER_CONFIGURATION = XContentParserConfiguration.EMPTY.withFiltering(
+        Set.of("_meta._rollup"),
+        null,
+        false
+    );
 
     @Inject
     public RollupUsageTransportAction(
@@ -48,8 +65,28 @@ public class RollupUsageTransportAction extends XPackUsageFeatureTransportAction
         ClusterState state,
         ActionListener<XPackUsageFeatureResponse> listener
     ) {
-        // TODO expose the currently running rollup tasks on this node? Unclear the best way to do that
-        RollupFeatureSetUsage usage = new RollupFeatureSetUsage();
+        int numberOfRollupIndices = 0;
+        for (var imd : state.metadata()) {
+            try (var parser = XContentHelper.createParser(PARSER_CONFIGURATION, imd.mapping().source().compressedReference())) {
+                ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.nextToken(), parser);
+                ensureExpectedToken(XContentParser.Token.FIELD_NAME, parser.nextToken(), parser);
+
+                var rollupConfig = parser.map();
+                if (rollupConfig != null) {
+                    numberOfRollupIndices++;
+                }
+            } catch (IOException e) {
+                listener.onFailure(e);
+                return;
+            }
+        }
+
+        int numberOfRollupJobs = 0;
+        PersistentTasksCustomMetadata persistentTasks = state.metadata().custom(PersistentTasksCustomMetadata.TYPE);
+        if (persistentTasks != null) {
+            numberOfRollupJobs = persistentTasks.findTasks(RollupJob.NAME, Predicates.always()).size();
+        }
+        RollupFeatureSetUsage usage = new RollupFeatureSetUsage(numberOfRollupIndices, numberOfRollupJobs);
         listener.onResponse(new XPackUsageFeatureResponse(usage));
     }
 }

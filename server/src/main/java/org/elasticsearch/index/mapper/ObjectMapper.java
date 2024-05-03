1d01c27ff2250350b395bc9ea623f3396724ecba
Martijn van Groningen <martijn.v.groningen@gmail.com>
2024-05-03 14:43:12 +0200

    iter

--- server/src/main/java/org/elasticsearch/index/mapper/ObjectMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/ObjectMapper.java
@@ -25,7 +25,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;

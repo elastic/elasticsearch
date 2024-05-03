df30fd5033b72fea748b1448aaf57ec7b5a8339a
ChrisHegarty <chegar999@gmail.com>
2024-05-03 10:38:44 +0100

    Update native vector provider to use int7 values only

--- libs/vec/src/main21/java/org/elasticsearch/vec/VectorScorerFactoryImpl.java
+++ libs/vec/src/main21/java/org/elasticsearch/vec/VectorScorerFactoryImpl.java
@@ -10,10 +10,10 @@ package org.elasticsearch.vec;
 
 import org.apache.lucene.store.IndexInput;
 import org.elasticsearch.nativeaccess.NativeAccess;
-import org.elasticsearch.vec.internal.DotProduct;
-import org.elasticsearch.vec.internal.Euclidean;
 import org.elasticsearch.vec.internal.IndexInputUtils;
-import org.elasticsearch.vec.internal.MaximumInnerProduct;
+import org.elasticsearch.vec.internal.Int7DotProduct;
+import org.elasticsearch.vec.internal.Int7Euclidean;
+import org.elasticsearch.vec.internal.Int7MaximumInnerProduct;
 
 import java.util.Optional;
 
@@ -28,7 +28,7 @@ class VectorScorerFactoryImpl implements VectorScorerFactory {
     }
 
     @Override
-    public Optional<VectorScorer> getScalarQuantizedVectorScorer(
+    public Optional<VectorScorer> getInt7ScalarQuantizedVectorScorer(
         int dims,
         int maxOrd,
         float scoreCorrectionConstant,
@@ -40,9 +40,9 @@ class VectorScorerFactoryImpl implements VectorScorerFactory {
             return Optional.empty(); // the input type is not MemorySegment based
         }
         return Optional.of(switch (similarityType) {
-            case COSINE, DOT_PRODUCT -> new DotProduct(dims, maxOrd, scoreCorrectionConstant, input);
-            case EUCLIDEAN -> new Euclidean(dims, maxOrd, scoreCorrectionConstant, input);
-            case MAXIMUM_INNER_PRODUCT -> new MaximumInnerProduct(dims, maxOrd, scoreCorrectionConstant, input);
+            case COSINE, DOT_PRODUCT -> new Int7DotProduct(dims, maxOrd, scoreCorrectionConstant, input);
+            case EUCLIDEAN -> new Int7Euclidean(dims, maxOrd, scoreCorrectionConstant, input);
+            case MAXIMUM_INNER_PRODUCT -> new Int7MaximumInnerProduct(dims, maxOrd, scoreCorrectionConstant, input);
         });
     }
 }

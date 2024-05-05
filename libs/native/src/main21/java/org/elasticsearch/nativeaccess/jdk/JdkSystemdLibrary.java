e6adbaee19b697dd769c4e5b6e9b1755358f9c7c
Andrew Wilkins <axw@elastic.co>
2024-05-05 17:27:02 +0800

    Revert to findLibSystemd, load in static block

--- libs/native/src/main21/java/org/elasticsearch/nativeaccess/jdk/JdkSystemdLibrary.java
+++ libs/native/src/main21/java/org/elasticsearch/nativeaccess/jdk/JdkSystemdLibrary.java
@@ -17,7 +17,10 @@ import java.lang.foreign.FunctionDescriptor;
 import java.lang.foreign.MemorySegment;
 import java.lang.invoke.MethodHandle;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 import static java.lang.foreign.ValueLayout.ADDRESS;
 import static java.lang.foreign.ValueLayout.JAVA_INT;
@@ -26,48 +29,45 @@ import static org.elasticsearch.nativeaccess.jdk.LinkerHelper.downcallHandle;
 class JdkSystemdLibrary implements SystemdLibrary {
 
     static {
-        loadLibSystemd();
+        // Find and load libsystemd. We attempt all instances of
+        // libsystemd in case of multiarch systems, and stop when
+        // one is successfully loaded. If none can be loaded,
+        // UnsatisfiedLinkError will be thrown.
+        List<String> paths = findLibSystemd();
+        if (paths.isEmpty()) {
+            String libpath = System.getProperty("java.library.path");
+            throw new UnsatisfiedLinkError("Could not find libsystemd in java.library.path: " + libpath);
+        }
+        UnsatisfiedLinkError last = null;
+        for (String path : paths) {
+            try {
+                System.load(path);
+                last = null;
+                break;
+            } catch (UnsatisfiedLinkError e) {
+                last = e;
+            }
+        }
+        if (last != null) {
+            throw last;
+        }
     }
 
-    // loadLibSystemd looks through java.library.path for one or more instances
-    // of libsystemd, returning the first one that can be loaded. If none
-    // can be found, or loaded, UnsatisfiedLinkError is thrown.
-    static String loadLibSystemd() {
+    // findLibSystemd returns a list of paths to instances of libsystemd
+    // found within java.library.path.
+    static List<String> findLibSystemd() {
         // Note: on some systems libsystemd does not have a non-versioned symlink.
         // System.loadLibrary only knows how to find non-versioned library files,
         // so we must manually check the library path to find what we need.
-        final String libsystemd = "libsystemd.so.0";
-        String libpath = System.getProperty("java.library.path");
-        for (String basepathStr : libpath.split(":")) {
-            var basepath = Paths.get(basepathStr);
-            if (Files.exists(basepath) == false) {
-                continue;
-            }
-            try (var stream = Files.walk(basepath)) {
-                var foundpaths = stream.filter(Files::isDirectory)
-                    .map(p -> p.resolve(libsystemd))
-                    .filter(Files::exists)
-                    .map(p -> p.toAbsolutePath().toString())
-                    .toList();
-
-                UnsatisfiedLinkError last = null;
-                for (String path : foundpaths) {
-                    try {
-                        System.load(path);
-                        return path;
-                    } catch (UnsatisfiedLinkError e) {
-                        last = e;
-                    }
-                }
-                if (last != null) {
-                    throw last;
-                }
+        final Path libsystemd = Paths.get("libsystemd.so.0");
+        final String libpath = System.getProperty("java.library.path");
+        return Arrays.stream(libpath.split(":")).map(Paths::get).filter(Files::exists).flatMap(p -> {
+            try {
+                return Files.find(p, Integer.MAX_VALUE, (fp, attrs) -> (!attrs.isDirectory() && fp.getFileName().equals(libsystemd)));
             } catch (IOException e) {
                 throw new UncheckedIOException(e);
             }
-
-        }
-        throw new UnsatisfiedLinkError("Could not find " + libsystemd + " in java.library.path: " + libpath);
+        }).map(p -> p.toAbsolutePath().toString()).toList();
     }
 
     private static final MethodHandle sd_notify$mh = downcallHandle("sd_notify", FunctionDescriptor.of(JAVA_INT, JAVA_INT, ADDRESS));

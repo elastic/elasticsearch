568d6daf905a92e3a48dbe23cd7573a031e7e930
Jonathan Buttner <56361221+jonathan-buttner@users.noreply.github.com>
2024-05-07 15:06:52 -0400

    Revert "Adding manage_ml (#108262)" (#108371)
    Reverts elastic/elasticsearch#108262
    
    Accidentally merged the above PR without security's approval. They asked
    us to revert for now and we'll continue discussing on a new PR.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/store/KibanaOwnedReservedRoleDescriptors.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/store/KibanaOwnedReservedRoleDescriptors.java
@@ -69,7 +69,6 @@ class KibanaOwnedReservedRoleDescriptors {
                 // For Fleet package upgrade
                 "manage_pipeline",
                 "manage_ilm",
-                "manage_inference",
                 // For the endpoint package that ships a transform
                 "manage_transform",
                 InvalidateApiKeyAction.NAME,

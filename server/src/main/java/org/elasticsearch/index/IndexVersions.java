705fb60ae23a189d5fd5b74d9963b5cd7a73bc95
Kostas Krikellas <kostas.krikellas@elastic.co>
2024-05-09 16:19:32 +0300

    Version-guard checking for lossy params in _source

--- server/src/main/java/org/elasticsearch/index/IndexVersions.java
+++ server/src/main/java/org/elasticsearch/index/IndexVersions.java
@@ -105,6 +105,7 @@ public class IndexVersions {
     public static final IndexVersion TIME_SERIES_ROUTING_HASH_IN_ID = def(8_504_00_0, Version.LUCENE_9_10_0);
     public static final IndexVersion DEFAULT_DENSE_VECTOR_TO_INT8_HNSW = def(8_505_00_0, Version.LUCENE_9_10_0);
     public static final IndexVersion DOC_VALUES_FOR_IGNORED_META_FIELD = def(8_505_00_1, Version.LUCENE_9_10_0);
+    public static final IndexVersion SOURCE_MAPPER_LOSSY_PARAMS_CHECK = def(8_506_00_0, Version.LUCENE_9_10_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

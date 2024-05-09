2700abffba868eb97280e7d43495c008e81f55e9
Keith Massey <keith.massey@elastic.co>
2024-05-09 16:49:53 -0500

    removing accidental change

--- server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
+++ server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -1210,7 +1210,7 @@ public class InternalEngine extends Engine {
                     }
 
                     assert index.seqNo() >= 0 : "ops should have an assigned seq no.; origin: " + index.origin();
-                    if ((plan.indexIntoLucene || plan.addStaleOpToLucene)) {
+                    if (plan.indexIntoLucene || plan.addStaleOpToLucene) {
                         indexResult = indexIntoLucene(index, plan);
                     } else {
                         indexResult = new IndexResult(

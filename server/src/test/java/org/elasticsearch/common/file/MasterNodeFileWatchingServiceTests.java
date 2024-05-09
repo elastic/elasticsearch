cb9eeaf8fcd4032fa46fd175a936d67d7d107d61
Ryan Ernst <ryan@iernst.net>
2024-05-09 11:39:56 -0700

    Handled blocked cluster state in file settings
    When file settings is first loaded on a master node starting up, the
    cluster state will likely be in a blocked state before it recovers. In
    that case the file settings will not be processable since the metadata
    will be missing in cluster state.
    
    This commit makes watching for file settings not start until the cluster
    state is in a recovered state. It also updates the the reserved state
    update task to handle a similar case where a task may be queued and then
    run at time when the node is no longer master, but before the watcher is
    stopped.

---
+++ server/src/test/java/org/elasticsearch/common/file/MasterNodeFileWatchingServiceTests.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.common.file;
+
+import org.elasticsearch.cluster.ClusterChangedEvent;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.block.ClusterBlocks;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.cluster.node.DiscoveryNodeRole;
+import org.elasticsearch.cluster.node.DiscoveryNodeUtils;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.gateway.GatewayService;
+import org.elasticsearch.node.NodeRoleSettings;
+import org.elasticsearch.test.ESTestCase;
+import org.junit.After;
+import org.junit.Before;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.concurrent.ExecutionException;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MasterNodeFileWatchingServiceTests extends ESTestCase {
+
+    MasterNodeFileWatchingService testService;
+    Path watchedFile;
+    Runnable fileChangedCallback;
+
+    @Before
+    public void setupTestService() throws IOException {
+        watchedFile = createTempFile();
+        ClusterService clusterService = mock(ClusterService.class);
+        Settings settings = Settings.builder()
+            .put(NodeRoleSettings.NODE_ROLES_SETTING.getKey(), DiscoveryNodeRole.MASTER_ROLE.roleName())
+            .build();
+        when(clusterService.getSettings()).thenReturn(settings);
+        fileChangedCallback = () -> {};
+        testService = new MasterNodeFileWatchingService(clusterService, watchedFile) {
+
+            @Override
+            protected void processFileChanges() throws InterruptedException, ExecutionException, IOException {
+                fileChangedCallback.run();
+            }
+
+            @Override
+            protected void processInitialFileMissing() throws InterruptedException, ExecutionException, IOException {
+                // file always exists, but we don't care about the missing case for master node behavior
+            }
+        };
+        testService.start();
+    }
+
+    @After
+    public void stopTestService() {
+        testService.stop();
+    }
+
+    public void testBecomingMasterNodeStartsWatcher() {
+        final DiscoveryNode localNode = DiscoveryNodeUtils.create("node");
+        ClusterState notRecoveredClusterState = ClusterState.builder(ClusterName.DEFAULT)
+            .nodes(DiscoveryNodes.builder().add(localNode).localNodeId(localNode.getId()).masterNodeId(localNode.getId()))
+            .blocks(ClusterBlocks.builder().addGlobalBlock(GatewayService.STATE_NOT_RECOVERED_BLOCK))
+            .build();
+        testService.clusterChanged(new ClusterChangedEvent("test", notRecoveredClusterState, ClusterState.EMPTY_STATE));
+        // just a master node isn't sufficient, cluster state also must be recovered
+        assertThat(testService.watching(), is(false));
+
+        ClusterState recoveredClusterState = ClusterState.builder(notRecoveredClusterState)
+            .blocks(ClusterBlocks.EMPTY_CLUSTER_BLOCK)
+            .build();
+        testService.clusterChanged(new ClusterChangedEvent("test", recoveredClusterState, notRecoveredClusterState));
+        // just a master node isn't sufficient, cluster state also must be recovered
+        assertThat(testService.watching(), is(true));
+
+    }
+}

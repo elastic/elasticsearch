1217103fe087580889fe69e00307670dd4d56239
Henning Andersen <henning.andersen@elastic.co>
2024-05-10 17:08:50 +0200

    add ordering note.

--- server/src/main/java/org/elasticsearch/index/shard/IndexEventListener.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexEventListener.java
@@ -57,6 +57,8 @@ public interface IndexEventListener {
 
     /**
      * Called after the index shard has been marked closed. It could still be waiting for the async close of the engine.
+     * The ordering between this and the subsequent state notifications (closed, deleted, store closed) is
+     * not guaranteed.
      */
     default void afterIndexShardClosing(ShardId shardId, @Nullable IndexShard indexShard, Settings indexSettings) {}
 

9d4c75f1de1215132cb6996750cd75e8d4ceeef4
Michael Peterson <michael.peterson@elastic.co>
2024-05-13 16:43:50 -0400

    Added min transport version check to RestoreService

--- server/src/main/java/org/elasticsearch/snapshots/RestoreService.java
+++ server/src/main/java/org/elasticsearch/snapshots/RestoreService.java
@@ -10,6 +10,7 @@ package org.elasticsearch.snapshots;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.TransportVersions;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.cluster.snapshots.restore.RestoreSnapshotRequest;
 import org.elasticsearch.action.support.IndicesOptions;
@@ -1304,6 +1305,9 @@ public final class RestoreService implements ClusterStateApplier {
                         indexMdBuilder.removeAllAliases();
                     } else {
                         ensureNoAliasNameConflicts(snapshotIndexMetadata);
+                    }
+                    if (currentState.getMinTransportVersion().before(TransportVersions.EVENT_INGESTED_RANGE_IN_CLUSTER_STATE)) {
+                        indexMdBuilder.eventIngestedRange(IndexLongFieldRange.UNKNOWN);
                     }
                     updatedIndexMetadata = indexMdBuilder.build();
                     if (partial) {

d406cb43ec9c797b053ce75839b6980afbbab1ec
Dianna Hohensee <dianna.hohensee@elastic.co>
2024-05-13 16:56:55 -0400

    Add setting to override default number of shards (#1906)
    Adds a new serverless.indices.regular.default_number_of_shards serverless
    setting. Defaults to 0 and is unused. When set >0, the setting value will be
    used for any new create index request that does not specify the number
    of shards. The new setting, when >0, will also override the ProjectType
    default number of shards normally used for create index requests with
    no number of shards specified.
    
    Closes

--- x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/allocation/StatelessIndexSettingProvider.java
+++ x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/allocation/StatelessIndexSettingProvider.java
@@ -24,9 +24,12 @@ import org.apache.lucene.util.automaton.CharacterRunAutomaton;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.routing.allocation.ExistingShardsAllocator;
+import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.compress.CompressedXContent;
+import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.core.Nullable;
 import org.elasticsearch.index.IndexSettingProvider;
 import org.elasticsearch.logging.LogManager;
 import org.elasticsearch.logging.Logger;
@@ -36,10 +39,29 @@ import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
 
+import static java.lang.Integer.parseInt;
 import static org.elasticsearch.cluster.metadata.IndexMetadata.INDEX_NUMBER_OF_SHARDS_SETTING;
+import static org.elasticsearch.cluster.metadata.IndexMetadata.PER_INDEX_MAX_NUMBER_OF_SHARDS;
 
 public class StatelessIndexSettingProvider implements IndexSettingProvider {
     private static final Logger logger = LogManager.getLogger(StatelessIndexSettingProvider.class);
+
+    /**
+     * Sets the number of shards that a new index will have, absent the create index request specifying the number of shards.
+     * This setting is not applied to indices backing data streams, nor internal system indices.
+     * Values > 0 will be used to override the {@code ServerlessSharedSettings.ProjectType} shard number default.
+     * The default of 0 will indicate that this setting is inactive.
+     * NB that setting 'null' is equivalent to 0.
+     */
+    public static final Setting<Integer> DEFAULT_NUMBER_OF_SHARDS_FOR_REGULAR_INDICES_SETTING = Setting.intSetting(
+        "serverless.indices.regular.default_number_of_shards",
+        0,
+        0,
+        parseInt(PER_INDEX_MAX_NUMBER_OF_SHARDS),
+        Setting.Property.NodeScope,
+        Setting.Property.OperatorDynamic
+    );
+    private volatile int defaultNumberOfShardsForRegularIndices = 0;
 
     private final ProjectType projectType;
     private Predicate<String> systemNamePredicate;
@@ -48,7 +70,12 @@ public class StatelessIndexSettingProvider implements IndexSettingProvider {
         this.projectType = projectType;
     }
 
-    public void initialize(IndexNameExpressionResolver indexNameExpressionResolver) {
+    public void initialize(ClusterService clusterService, IndexNameExpressionResolver indexNameExpressionResolver) {
+        clusterService.getClusterSettings()
+            .initializeAndWatch(
+                DEFAULT_NUMBER_OF_SHARDS_FOR_REGULAR_INDICES_SETTING,
+                updatedValue -> defaultNumberOfShardsForRegularIndices = updatedValue
+            );
         final var systemNameRunAutomaton = new CharacterRunAutomaton(indexNameExpressionResolver.getSystemNameAutomaton());
         systemNamePredicate = systemNameRunAutomaton::run;
     }
@@ -56,11 +83,11 @@ public class StatelessIndexSettingProvider implements IndexSettingProvider {
     @Override
     public Settings getAdditionalIndexSettings(
         String indexName,
-        String dataStreamName,
-        boolean timeSeries,
+        @Nullable String dataStreamName,
+        boolean isTimeSeries,
         Metadata metadata,
         Instant resolvedAt,
-        Settings allSettings,
+        Settings indexTemplateAndCreateRequestSettings,
         List<CompressedXContent> combinedTemplateMappings
     ) {
         assert systemNamePredicate != null : "object is not initialized properly";
@@ -71,17 +98,25 @@ public class StatelessIndexSettingProvider implements IndexSettingProvider {
         }
 
         if (dataStreamName == null
-            && false == INDEX_NUMBER_OF_SHARDS_SETTING.exists(allSettings)
+            && false == INDEX_NUMBER_OF_SHARDS_SETTING.exists(indexTemplateAndCreateRequestSettings)
             && false == systemNamePredicate.test(indexName)) {
+            int defaultShards = defaultNumberOfShardsForRegularIndices > 0
+                ? defaultNumberOfShardsForRegularIndices
+                : projectType.getNumberOfShards();
             logger.debug(
                 () -> Strings.format(
-                    "apply stateless number_of_shards default of [%d] for project type [%s] to index [%s]",
-                    projectType.getNumberOfShards(),
+                    "Stateless default number of shards is [%d], applying to index [%s]. Project type [%s], project default shards [%d], "
+                        + "%s override (with value [%d]) %s active",
+                    defaultShards,
+                    indexName,
                     projectType,
-                    indexName
+                    projectType.getNumberOfShards(),
+                    DEFAULT_NUMBER_OF_SHARDS_FOR_REGULAR_INDICES_SETTING.getKey(),
+                    defaultNumberOfShardsForRegularIndices,
+                    (defaultNumberOfShardsForRegularIndices > 0 ? "is" : "is not")
                 )
             );
-            settings.put(INDEX_NUMBER_OF_SHARDS_SETTING.getKey(), projectType.getNumberOfShards());
+            settings.put(INDEX_NUMBER_OF_SHARDS_SETTING.getKey(), defaultShards);
         }
         return settings.build();
     }

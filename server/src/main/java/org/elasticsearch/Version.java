a3bbfce4291c51bdd4bae16ef567f5d84fa02974
Max Hniebergall <max.hniebergall@elastic.co>
2024-05-13 12:52:39 -0400

    Revert manual testing configs of "temp for testing"
    This reverts parts of commit fca46fd2b6253accc010a2e2a8bf05edfff5ea9b.

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -176,8 +176,7 @@ public class Version implements VersionId<Version>, ToXContentFragment {
     public static final Version V_8_13_4 = new Version(8_13_04_99);
     public static final Version V_8_14_0 = new Version(8_14_00_99);
     public static final Version V_8_15_0 = new Version(8_15_00_99);
-    public static final Version V_8_16_0 = new Version(8_16_00_99);
-    public static final Version CURRENT = V_8_16_0;
+    public static final Version CURRENT = V_8_15_0;
 
     private static final NavigableMap<Integer, Version> VERSION_IDS;
     private static final Map<String, Version> VERSION_STRINGS;

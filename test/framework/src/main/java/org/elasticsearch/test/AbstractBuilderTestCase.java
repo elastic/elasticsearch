8b5c678035446b68f905ecdca8913f664d0f9e34
Mike Pellegrini <mike.pellegrini@elastic.co>
2024-05-14 14:10:10 -0400

    Don't register plugin named writeables

--- test/framework/src/main/java/org/elasticsearch/test/AbstractBuilderTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/AbstractBuilderTestCase.java
@@ -449,7 +449,6 @@ public abstract class AbstractBuilderTestCase extends ESTestCase {
             List<NamedWriteableRegistry.Entry> entries = new ArrayList<>();
             entries.addAll(IndicesModule.getNamedWriteables());
             entries.addAll(searchModule.getNamedWriteables());
-            pluginsService.forEach(plugin -> entries.addAll(plugin.getNamedWriteables()));
             namedWriteableRegistry = new NamedWriteableRegistry(entries);
             parserConfiguration = XContentParserConfiguration.EMPTY.withRegistry(
                 new NamedXContentRegistry(

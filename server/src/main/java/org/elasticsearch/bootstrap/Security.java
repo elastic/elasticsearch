59d2bdad30541db9701d9c86771d6e8410abc11b
Simon Cooper <simon.cooper@elastic.co>
2024-05-14 17:06:44 +0100

    splotless

--- server/src/main/java/org/elasticsearch/bootstrap/Security.java
+++ server/src/main/java/org/elasticsearch/bootstrap/Security.java
@@ -44,7 +44,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Stream;

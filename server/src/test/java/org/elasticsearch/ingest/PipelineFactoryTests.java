532ec576d9092d67fece75cd4ea51837f837e63e
Parker Timmins <parker.timmins@elastic.co>
2024-05-15 11:56:25 -0500

    remove unneeded import

--- server/src/test/java/org/elasticsearch/ingest/PipelineFactoryTests.java
+++ server/src/test/java/org/elasticsearch/ingest/PipelineFactoryTests.java
@@ -17,7 +17,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;

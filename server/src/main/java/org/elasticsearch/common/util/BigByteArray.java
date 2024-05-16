94a3b86ce50e4d65dcf8689c7e5bd23a126d1c55
Armin Braun <me@obrown.io>
2024-05-16 21:14:52 +0200

    Dry up resize method across BigArray implementations
    This method is the exact same across all implementations.

--- server/src/main/java/org/elasticsearch/common/util/BigByteArray.java
+++ server/src/main/java/org/elasticsearch/common/util/BigByteArray.java
@@ -8,10 +8,8 @@
 
 package org.elasticsearch.common.util;
 
-import org.apache.lucene.util.ArrayUtil;
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.BytesRefIterator;
-import org.apache.lucene.util.RamUsageEstimator;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 
@@ -165,23 +163,6 @@ final class BigByteArray extends AbstractBigByteArray implements ByteArray {
     @Override
     protected int numBytesPerElement() {
         return 1;
-    }
-
-    /** Change the size of this array. Content between indexes <code>0</code> and <code>min(size(), newSize)</code> will be preserved. */
-    @Override
-    public void resize(long newSize) {
-        final int numPages = numPages(newSize);
-        if (numPages > pages.length) {
-            pages = Arrays.copyOf(pages, ArrayUtil.oversize(numPages, RamUsageEstimator.NUM_BYTES_OBJECT_REF));
-        }
-        for (int i = numPages - 1; i >= 0 && pages[i] == null; --i) {
-            pages[i] = newBytePage(i);
-        }
-        for (int i = numPages; i < pages.length && pages[i] != null; ++i) {
-            pages[i] = null;
-            releasePage(i);
-        }
-        this.size = newSize;
     }
 
     /** Estimates the number of bytes that would be consumed by an array of the given size. */

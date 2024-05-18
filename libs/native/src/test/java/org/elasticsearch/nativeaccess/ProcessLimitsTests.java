910f02f3b4ca83dd156be52442dbe202783fd23e
Ryan Ernst <ryan@iernst.net>
2024-05-18 10:49:37 -0700

    Move rlimit calls into NativeAccess
    This commit moves getting max threads, max virtual memory size, and max
    file size into NativeAccess.
    
    relates https://github.com/elastic/elasticsearch/pull/104876

--- libs/native/src/test/java/org/elasticsearch/nativeaccess/ProcessLimitsTests.java
+++ libs/native/src/test/java/org/elasticsearch/nativeaccess/ProcessLimitsTests.java
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-package org.elasticsearch.bootstrap;
+package org.elasticsearch.nativeaccess;
 
 import org.apache.lucene.util.Constants;
 import org.elasticsearch.core.PathUtils;
@@ -16,11 +16,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
 
-import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
-public class EvilJNANativesTests extends ESTestCase {
+public class ProcessLimitsTests extends ESTestCase {
+
+    private static final NativeAccess nativeAccess = NativeAccess.instance();
 
     public void testSetMaximumNumberOfThreads() throws IOException {
         if (Constants.LINUX) {
@@ -28,14 +29,14 @@ public class EvilJNANativesTests extends ESTestCase {
             for (final String line : lines) {
                 if (line != null && line.startsWith("Max processes")) {
                     final String[] fields = line.split("\\s+");
-                    final long limit = "unlimited".equals(fields[2]) ? JNACLibrary.RLIM_INFINITY : Long.parseLong(fields[2]);
-                    assertThat(JNANatives.MAX_NUMBER_OF_THREADS, equalTo(limit));
+                    final long limit = "unlimited".equals(fields[2]) ? ProcessLimits.UNLIMITED : Long.parseLong(fields[2]);
+                    assertThat(nativeAccess.getProcessLimits().maxThreads(), equalTo(limit));
                     return;
                 }
             }
             fail("should have read max processes from /proc/self/limits");
         } else {
-            assertThat(JNANatives.MAX_NUMBER_OF_THREADS, equalTo(-1L));
+            assertThat(nativeAccess.getProcessLimits().maxThreads(), equalTo(-1L));
         }
     }
 
@@ -45,16 +46,16 @@ public class EvilJNANativesTests extends ESTestCase {
             for (final String line : lines) {
                 if (line != null && line.startsWith("Max address space")) {
                     final String[] fields = line.split("\\s+");
-                    final String limit = fields[3];
-                    assertThat(JNANatives.rlimitToString(JNANatives.MAX_SIZE_VIRTUAL_MEMORY), equalTo(limit));
+                    final long limit = "unlimited".equals(fields[3]) ? ProcessLimits.UNLIMITED : Long.parseLong(fields[3]);
+                    assertThat(nativeAccess.getProcessLimits().maxVirtualMemorySize(), equalTo(limit));
                     return;
                 }
             }
             fail("should have read max size virtual memory from /proc/self/limits");
         } else if (Constants.MAC_OS_X) {
-            assertThat(JNANatives.MAX_SIZE_VIRTUAL_MEMORY, anyOf(equalTo(Long.MIN_VALUE), greaterThanOrEqualTo(0L)));
+            assertThat(nativeAccess.getProcessLimits().maxVirtualMemorySize(), greaterThanOrEqualTo(0L));
         } else {
-            assertThat(JNANatives.MAX_SIZE_VIRTUAL_MEMORY, equalTo(Long.MIN_VALUE));
+            assertThat(nativeAccess.getProcessLimits().maxVirtualMemorySize(), equalTo(ProcessLimits.UNKNOWN));
         }
     }
 
@@ -64,16 +65,16 @@ public class EvilJNANativesTests extends ESTestCase {
             for (final String line : lines) {
                 if (line != null && line.startsWith("Max file size")) {
                     final String[] fields = line.split("\\s+");
-                    final String limit = fields[3];
-                    assertThat(JNANatives.rlimitToString(JNANatives.MAX_FILE_SIZE), equalTo(limit));
+                    final long limit = "unlimited".equals(fields[3]) ? ProcessLimits.UNLIMITED : Long.parseLong(fields[3]);
+                    assertThat(nativeAccess.getProcessLimits().maxFileSize(), equalTo(limit));
                     return;
                 }
             }
             fail("should have read max file size from /proc/self/limits");
         } else if (Constants.MAC_OS_X) {
-            assertThat(JNANatives.MAX_FILE_SIZE, anyOf(equalTo(Long.MIN_VALUE), greaterThanOrEqualTo(0L)));
+            assertThat(nativeAccess.getProcessLimits().maxFileSize(), greaterThanOrEqualTo(0L));
         } else {
-            assertThat(JNANatives.MAX_FILE_SIZE, equalTo(Long.MIN_VALUE));
+            assertThat(nativeAccess.getProcessLimits().maxFileSize(), equalTo(ProcessLimits.UNKNOWN));
         }
     }
 

c852775c75107ccc2849d6a422c1386a2358fdb7
Ryan Ernst <ryan@iernst.net>
2024-05-18 12:34:40 -0700

    fix rlim access

--- libs/native/src/main21/java/org/elasticsearch/nativeaccess/jdk/JdkPosixCLibrary.java
+++ libs/native/src/main21/java/org/elasticsearch/nativeaccess/jdk/JdkPosixCLibrary.java
@@ -103,12 +103,12 @@ class JdkPosixCLibrary implements PosixCLibrary {
 
         @Override
         public long rlim_cur() {
-            return (long) rlim_cur$vh.get(segment);
+            return (long) rlim_cur$vh.get(segment, 0);
         }
 
         @Override
         public long rlim_max() {
-            return (long) rlim_max$vh.get(segment);
+            return (long) rlim_max$vh.get(segment, 0);
         }
 
         @Override

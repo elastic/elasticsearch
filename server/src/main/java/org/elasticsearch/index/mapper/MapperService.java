4cbcb68de8debf1a536045cb02daf7821e13edd5
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2024-05-27 10:23:11 +0300

    Fixing comment

--- server/src/main/java/org/elasticsearch/index/mapper/MapperService.java
+++ server/src/main/java/org/elasticsearch/index/mapper/MapperService.java
@@ -482,7 +482,7 @@ public class MapperService extends AbstractIndexComponent implements Closeable {
                         }
                         // Recursively merge these two field mappings.
                         // Since "key" is an arbitrary field name, for which we only need plain mapping subtrees merge, no need to pass it
-                        // to the recursion as it shouldn't affect the merge logic. Specifically, passing "null" as parent avoids merge
+                        // to the recursion as it shouldn't affect the merge logic. Specifically, passing a parent may cause merge
                         // failures of fields named "properties". See https://github.com/elastic/elasticsearch/issues/108866
                         XContentHelper.merge(mergedMappings, mapToMerge, INSTANCE);
                         return mergedMappings;

afb3610146ce2bc6ed3160dab6e7a3bbe7f5ee92
Iván Cea Fontenla <ivan.cea@elastic.co>
2024-05-30 14:19:02 +0200

    Discard test supplier modifiers, moved to another branch

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/scalar/ip/IpPrefixTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/scalar/ip/IpPrefixTests.java
@@ -106,7 +106,7 @@ public class IpPrefixTests extends AbstractFunctionTestCase {
             })
         );
 
-        return parameterSuppliersFromTypedData(errorsForCasesWithoutExamples(anyNullIsNull(true, randomizeBytesRefsOffset(suppliers))));
+        return parameterSuppliersFromTypedData(errorsForCasesWithoutExamples(anyNullIsNull(true, suppliers)));
     }
 
     @Override

6b7a9f8e962471807ed822d5f3f9b93090000765
Panagiotis Bailis <pmpailis@gmail.com>
2024-06-07 11:52:45 +0300

    adding docs to AbstractRerankerIT

--- test/framework/src/main/java/org/elasticsearch/search/rank/rerank/AbstractRerankerIT.java
+++ test/framework/src/main/java/org/elasticsearch/search/rank/rerank/AbstractRerankerIT.java
@@ -31,6 +31,14 @@ import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.hasId;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.hasRank;
 import static org.hamcrest.Matchers.equalTo;
 
+/**
+ * this base class acts as a wrapper for testing different rerankers, and their behavior when exceptions are thrown
+ * the main idea is that we:
+ * 1. index some documents, with a rank feature field and a search field
+ * 2. have a random initial scoring
+ * 3. rerank the results based on the rank feature field (converting String -> Float)
+ * 4. assert that the results are correctly reranked & that we properly close all resources
+ */
 @ESIntegTestCase.ClusterScope(minNumDataNodes = 3)
 public abstract class AbstractRerankerIT extends ESIntegTestCase {
 

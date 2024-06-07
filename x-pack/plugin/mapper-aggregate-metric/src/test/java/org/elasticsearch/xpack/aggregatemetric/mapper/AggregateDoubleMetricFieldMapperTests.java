09994aaf85d22482c6a5084510f86f5135b3d704
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2024-06-07 10:07:49 -0700

    Address feedback

--- x-pack/plugin/mapper-aggregate-metric/src/test/java/org/elasticsearch/xpack/aggregatemetric/mapper/AggregateDoubleMetricFieldMapperTests.java
+++ x-pack/plugin/mapper-aggregate-metric/src/test/java/org/elasticsearch/xpack/aggregatemetric/mapper/AggregateDoubleMetricFieldMapperTests.java
@@ -147,38 +147,53 @@ public class AggregateDoubleMetricFieldMapperTests extends MapperTestCase {
 
     @Override
     protected List<ExampleMalformedValue> exampleMalformedValues() {
+        var min = randomDoubleBetween(-100, 100, false);
+        var max = randomDoubleBetween(min, 150, false);
+        var valueCount = randomIntBetween(1, Integer.MAX_VALUE);
+
+        var randomString = randomAlphaOfLengthBetween(1, 10);
+        var randomLong = randomLong();
+        var randomDouble = randomDouble();
+        var randomBoolean = randomBoolean();
+
         return List.of(
             // wrong input structure
-            exampleMalformedValue(b -> b.value("hello")).errorMatches("Failed to parse object"),
-            exampleMalformedValue(b -> b.value(120)).errorMatches("Failed to parse object"),
-            exampleMalformedValue(b -> b.value(120.5)).errorMatches("Failed to parse object"),
-            exampleMalformedValue(b -> b.value(false)).errorMatches("Failed to parse object"),
+            exampleMalformedValue(b -> b.value(randomString)).errorMatches("Failed to parse object"),
+            exampleMalformedValue(b -> b.value(randomLong)).errorMatches("Failed to parse object"),
+            exampleMalformedValue(b -> b.value(randomDouble)).errorMatches("Failed to parse object"),
+            exampleMalformedValue(b -> b.value(randomBoolean)).errorMatches("Failed to parse object"),
             // no metrics
             exampleMalformedValue(b -> b.startObject().endObject()).errorMatches(
                 "Aggregate metric field [field] must contain all metrics [min, max, value_count]"
             ),
             // unmapped metric
             exampleMalformedValue(
-                b -> b.startObject().field("min", -10.1).field("max", 50.0).field("value_count", 14).field("sum", 55).endObject()
+                b -> b.startObject()
+                    .field("min", min)
+                    .field("max", max)
+                    .field("value_count", valueCount)
+                    .field("sum", randomLong)
+                    .endObject()
             ).errorMatches("Aggregate metric [sum] does not exist in the mapping of field [field]"),
             // missing metric
-            exampleMalformedValue(b -> b.startObject().field("min", -10.1).field("max", 50.0).endObject()).errorMatches(
+            exampleMalformedValue(b -> b.startObject().field("min", min).field("max", max).endObject()).errorMatches(
                 "Aggregate metric field [field] must contain all metrics [min, max, value_count]"
             ),
             // invalid metric value
-            exampleMalformedValue(b -> b.startObject().field("min", "10.0").field("max", 50.0).field("value_count", 14).endObject())
+            exampleMalformedValue(b -> b.startObject().field("min", "10.0").field("max", max).field("value_count", valueCount).endObject())
                 .errorMatches("Failed to parse object: expecting token of type [VALUE_NUMBER] but found [VALUE_STRING]"),
-            // invalid metric value with additional data
+            // Invalid metric value with additional data.
+            // `min` field triggers the error and all additional data should be preserved in synthetic source.
             exampleMalformedValue(
                 b -> b.startObject()
+                    .field("max", max)
+                    .field("value_count", valueCount)
                     .field("min", "10.0")
-                    .field("max", 50.0)
-                    .field("value_count", 14)
-                    .field("hello", "world")
+                    .field("hello", randomString)
                     .startObject("object")
-                    .field("hello", "world")
+                    .field("hello", randomLong)
                     .endObject()
-                    .array("list", "hello", "world")
+                    .array("list", randomString, randomString)
                     .endObject()
             ).errorMatches("Failed to parse object: expecting token of type [VALUE_NUMBER] but found [VALUE_STRING]"),
             // metric is an object
@@ -187,19 +202,20 @@ public class AggregateDoubleMetricFieldMapperTests extends MapperTestCase {
                     .startObject("min")
                     .field("hello", "world")
                     .endObject()
-                    .field("max", 50.0)
-                    .field("value_count", 14)
+                    .field("max", max)
+                    .field("value_count", valueCount)
                     .endObject()
             ).errorMatches("Failed to parse object: expecting token of type [VALUE_NUMBER] but found [START_OBJECT]"),
             // metric is an array
             exampleMalformedValue(
-                b -> b.startObject().array("min", "hello", "world").field("max", 50.0).field("value_count", 14).endObject()
+                b -> b.startObject().array("min", "hello", "world").field("max", max).field("value_count", valueCount).endObject()
             ).errorMatches("Failed to parse object: expecting token of type [VALUE_NUMBER] but found [START_ARRAY]"),
             // negative value count
-            exampleMalformedValue(b -> b.startObject().field("min", 10.0).field("max", 50.0).field("value_count", -14).endObject())
-                .errorMatches("Aggregate metric [value_count] of field [field] cannot be a negative number"),
+            exampleMalformedValue(
+                b -> b.startObject().field("min", min).field("max", max).field("value_count", -1 * valueCount).endObject()
+            ).errorMatches("Aggregate metric [value_count] of field [field] cannot be a negative number"),
             // value count with decimal digits (whole numbers formatted as doubles are permitted, but non-whole numbers are not)
-            exampleMalformedValue(b -> b.startObject().field("min", 10.0).field("max", 50.0).field("value_count", 77.33).endObject())
+            exampleMalformedValue(b -> b.startObject().field("min", min).field("max", max).field("value_count", 77.33).endObject())
                 .errorMatches("failed to parse [value_count] sub field: 77.33 cannot be converted to Integer without data loss")
         );
     }

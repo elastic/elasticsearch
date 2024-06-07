962cfa1c4f95255dea7067eaf3471ceb766ba546
Kostas Krikellas <kostas.krikellas@elastic.co>
2024-06-07 13:48:18 +0300

    log id encoding

--- server/src/main/java/org/elasticsearch/index/codec/PerFieldFormatSupplier.java
+++ server/src/main/java/org/elasticsearch/index/codec/PerFieldFormatSupplier.java
@@ -19,9 +19,12 @@ import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.codec.bloomfilter.ES87BloomFilterPostingsFormat;
 import org.elasticsearch.index.codec.postings.ES812PostingsFormat;
 import org.elasticsearch.index.codec.tsdb.ES87TSDBDocValuesFormat;
+import org.elasticsearch.index.mapper.DimensionRoutingHashFieldMapper;
 import org.elasticsearch.index.mapper.IdFieldMapper;
+import org.elasticsearch.index.mapper.LogsIdFieldMapper;
 import org.elasticsearch.index.mapper.Mapper;
 import org.elasticsearch.index.mapper.MapperService;
+import org.elasticsearch.index.mapper.TimeSeriesIdFieldMapper;
 import org.elasticsearch.index.mapper.vectors.DenseVectorFieldMapper;
 
 import java.util.Objects;
@@ -115,7 +118,10 @@ public class PerFieldFormatSupplier {
     private boolean excludeFields(String fieldName) {
         // Avoid using tsdb codec for fields like _seq_no, _primary_term.
         // But _tsid and _ts_routing_hash should always use the tsdb codec.
-        return fieldName.startsWith("_") && fieldName.equals("_tsid") == false && fieldName.equals("_ts_routing_hash") == false;
+        return fieldName.startsWith("_")
+            && fieldName.equals(TimeSeriesIdFieldMapper.NAME) == false
+            && fieldName.equals(LogsIdFieldMapper.NAME) == false
+            && fieldName.equals(DimensionRoutingHashFieldMapper.NAME) == false;
     }
 
     private boolean isTimeSeriesModeIndex() {

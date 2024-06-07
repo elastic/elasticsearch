39e0e475126679668a914ec4da0d7fa014cac6e0
Ryan Ernst <ryan@iernst.net>
2024-06-07 15:22:26 -0600

    Guard file settings readiness on file settings support
    Consistency of file settings is an important invariant. However, when
    upgrading from Elasticsearch versions before file settings existed,
    cluster state will not yet have the file settings metadata. If the first
    node upgraded is not the master node, new nodes will never become
    ready while they wait for file settings metadata to exist.
    
    This commit adds a node feature for file settings to guard waiting on
    file settings for readiness. Although file settings has existed since
    8.4, the feature is not a historical feature because historical features
    are not applied to cluster state that readiness checks. In this case it
    is not needed since upgrading from 8.4+ will already contain file
    settings metadata.

--- server/src/main/java/org/elasticsearch/readiness/ReadinessService.java
+++ server/src/main/java/org/elasticsearch/readiness/ReadinessService.java
@@ -22,6 +22,7 @@ import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.transport.BoundTransportAddress;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.env.Environment;
+import org.elasticsearch.reservedstate.service.FileSettingsFeatures;
 import org.elasticsearch.reservedstate.service.FileSettingsService;
 import org.elasticsearch.shutdown.PluginShutdownService;
 import org.elasticsearch.transport.BindTransportException;
@@ -277,7 +278,15 @@ public class ReadinessService extends AbstractLifecycleComponent implements Clus
     // protected to allow mock service to override
     protected boolean areFileSettingsApplied(ClusterState clusterState) {
         ReservedStateMetadata fileSettingsMetadata = clusterState.metadata().reservedStateMetadata().get(FileSettingsService.NAMESPACE);
-        return fileSettingsMetadata != null && fileSettingsMetadata.version().equals(ReservedStateMetadata.NO_VERSION) == false;
+        if (fileSettingsMetadata == null) {
+            // In order to block readiness on file settings being applied, we need to know that the master node has written an initial
+            // version, or a marker that file settings don't exist. When upgrading from a version that did not have file settings, the
+            // current master node may not be the first node upgraded. To be safe, we wait to consider file settings application for
+            // readiness until the whole cluster supports file settings.
+            return clusterState.clusterFeatures().clusterHasFeature(FileSettingsFeatures.FILE_SETTINGS_SUPPORTED) == false;
+        } else {
+            return fileSettingsMetadata.version().equals(ReservedStateMetadata.NO_VERSION) == false;
+        }
     }
 
     private void setReady(boolean ready) {

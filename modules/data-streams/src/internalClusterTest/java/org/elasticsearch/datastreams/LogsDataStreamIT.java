06d4ff7cef6c78a2df142a79efdb5cf58c4b15d6
Salvatore Campagna <salvatore.campagna@elastic.co>
2024-06-10 16:25:53 +0200

    checkstyle: line too long

--- modules/data-streams/src/internalClusterTest/java/org/elasticsearch/datastreams/LogsDataStreamIT.java
+++ modules/data-streams/src/internalClusterTest/java/org/elasticsearch/datastreams/LogsDataStreamIT.java
@@ -269,8 +269,9 @@ public class LogsDataStreamIT extends ESSingleNodeTestCase {
         assertThat(
             exception.getCause().getCause().getMessage(),
             Matchers.equalTo(
-                "All fields that match routing_path must be configured with [time_series_dimension: true] or flattened fields with "
-                    + "a list of dimensions in [time_series_dimensions] and without the [script] parameter. [host.name] was not a dimension."
+                "All fields that match routing_path must be configured with [time_series_dimension: true] or flattened fields "
+                    + " with a list of dimensions in [time_series_dimensions] and without the [script] parameter. [host.name] was not a"
+                    + "a dimension."
             )
         );
     }

4c3411f38743fddbdc9c1ba1217f19c8d29f3443
Max Hniebergall <max.hniebergall@elastic.co>
2024-06-11 12:11:24 -0400

    Check for body and query parameters, verify they are same if set

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/rest/inference/RestStartTrainedModelDeploymentAction.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/rest/inference/RestStartTrainedModelDeploymentAction.java
@@ -7,12 +7,14 @@
 
 package org.elasticsearch.xpack.ml.rest.inference;
 
+import org.elasticsearch.ElasticsearchStatusException;
 import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.core.RestApiVersion;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.rest.Scope;
 import org.elasticsearch.rest.ServerlessScope;
 import org.elasticsearch.rest.action.RestToXContentListener;
@@ -24,6 +26,7 @@ import org.elasticsearch.xpack.ml.rest.RestCompatibilityChecker;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import static org.elasticsearch.rest.RestRequest.Method.POST;
 import static org.elasticsearch.xpack.core.ml.action.StartTrainedModelDeploymentAction.Request.CACHE_SIZE;
@@ -72,7 +75,7 @@ public class RestStartTrainedModelDeploymentAction extends BaseRestHandler {
         String deploymentId = restRequest.param(StartTrainedModelDeploymentAction.Request.DEPLOYMENT_ID.getPreferredName(), modelId);
         StartTrainedModelDeploymentAction.Request request;
 
-        if (restRequest.hasContentOrSourceParam()) {
+        if (restRequest.hasContentOrSourceParam()) { // request has body
             request = StartTrainedModelDeploymentAction.Request.parseRequest(
                 modelId,
                 deploymentId,
@@ -83,19 +86,26 @@ public class RestStartTrainedModelDeploymentAction extends BaseRestHandler {
         }
 
         if (restRequest.hasParam(TIMEOUT.getPreferredName())) {
-            TimeValue openTimeout = restRequest.paramAsTime(TIMEOUT.getPreferredName(), StartTrainedModelDeploymentAction.DEFAULT_TIMEOUT);
+            TimeValue openTimeout = (TimeValue) sameParamInQueryAndBody(
+                request.getTimeout(),
+                restRequest.paramAsTime(TIMEOUT.getPreferredName(), null)
+            ); // hasParam, so never default
             request.setTimeout(openTimeout);
         }
 
         request.setWaitForState(
-            AllocationStatus.State.fromString(restRequest.param(WAIT_FOR.getPreferredName(), AllocationStatus.State.STARTED.toString()))
+            (AllocationStatus.State) sameParamInQueryAndBody(
+                request.getWaitForState(),
+                AllocationStatus.State.fromString(restRequest.param(WAIT_FOR.getPreferredName(), AllocationStatus.State.STARTED.toString()))
+            )
         );
+
         RestCompatibilityChecker.checkAndSetDeprecatedParam(
             NUMBER_OF_ALLOCATIONS.getDeprecatedNames()[0],
             NUMBER_OF_ALLOCATIONS.getPreferredName(),
             RestApiVersion.V_8,
             restRequest,
-            (r, s) -> r.paramAsInt(s, request.getNumberOfAllocations()),
+            (r, s) -> (Integer) sameParamInQueryAndBody(request.getNumberOfAllocations(), negativeIntToNull(r.paramAsInt(s, -1))),
             request::setNumberOfAllocations
         );
         RestCompatibilityChecker.checkAndSetDeprecatedParam(
@@ -103,24 +113,57 @@ public class RestStartTrainedModelDeploymentAction extends BaseRestHandler {
             THREADS_PER_ALLOCATION.getPreferredName(),
             RestApiVersion.V_8,
             restRequest,
-            (r, s) -> r.paramAsInt(s, request.getThreadsPerAllocation()),
+            (r, s) -> (Integer) sameParamInQueryAndBody(request.getThreadsPerAllocation(), negativeIntToNull(r.paramAsInt(s, -1))),
             request::setThreadsPerAllocation
         );
-        request.setQueueCapacity(restRequest.paramAsInt(QUEUE_CAPACITY.getPreferredName(), request.getQueueCapacity()));
+        request.setQueueCapacity(
+            (Integer) sameParamInQueryAndBody(
+                request.getQueueCapacity(),
+                negativeIntToNull(restRequest.paramAsInt(QUEUE_CAPACITY.getPreferredName(), -1))
+            )
+        );
 
         if (restRequest.hasParam(CACHE_SIZE.getPreferredName())) {
             request.setCacheSize(
-                ByteSizeValue.parseBytesSizeValue(restRequest.param(CACHE_SIZE.getPreferredName()), CACHE_SIZE.getPreferredName())
+                (ByteSizeValue) sameParamInQueryAndBody(
+                    request.getCacheSize(),
+                    ByteSizeValue.parseBytesSizeValue(restRequest.param(CACHE_SIZE.getPreferredName()), CACHE_SIZE.getPreferredName())
+                )
             );
         } else if (defaultCacheSize != null) {
             request.setCacheSize(defaultCacheSize);
         }
 
-        request.setQueueCapacity(restRequest.paramAsInt(QUEUE_CAPACITY.getPreferredName(), request.getQueueCapacity()));
         request.setPriority(
-            restRequest.param(StartTrainedModelDeploymentAction.TaskParams.PRIORITY.getPreferredName(), request.getPriority().toString())
+            (String) sameParamInQueryAndBody(
+                request.getPriority().toString(),
+                restRequest.param(StartTrainedModelDeploymentAction.TaskParams.PRIORITY.getPreferredName(), null)
+            )
         );
 
         return channel -> client.execute(StartTrainedModelDeploymentAction.INSTANCE, request, new RestToXContentListener<>(channel));
+    }
+
+    private Integer negativeIntToNull(int maybeNegativeInt) {
+        if (maybeNegativeInt < 0) {
+            return null;
+        } else {
+            return maybeNegativeInt;
+        }
+    }
+
+    private Object sameParamInQueryAndBody(Object bodyParam, Object queryParam) {
+        if (Objects.equals(bodyParam, queryParam)) {
+            return bodyParam;
+        } else if (bodyParam == null) {
+            return queryParam;
+        } else if (queryParam == null) {
+            return bodyParam;
+        } else {
+            throw new ElasticsearchStatusException(
+                "The parameter " + bodyParam + " in the body is different from the parameter " + queryParam + " in the query",
+                RestStatus.BAD_REQUEST
+            );
+        }
     }
 }

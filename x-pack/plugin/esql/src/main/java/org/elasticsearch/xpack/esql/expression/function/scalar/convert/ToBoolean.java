113087ea74fbd1fb00c281fc0693c10d86fc3cd5
Nik Everett <nik9000@gmail.com>
2024-06-11 17:06:32 -0400

    ESQL: Move serialization for unary functions (#109436)
    This moves the serialization for unary functions from `PlanNamedTypes`
    to the classes themselves which lines up more with how the rest of
    Elasticsearch works. There's a little more ceremony as the subclasses
    themselves declare serialization constructors. *EVERY* subclass does.
    And has to declare `getWriteableName`. Now, those serialization ctors
    are just `super(in);` so you can't screw it up, but they are there and
    that's more than before. But, again, it's what folks coming from the
    rest of Elasticsearch will expect.
    
    There is some silver lining here - it's much easier to make wire changes
    with these serialization ctors. And, maybe better, it's very easy to
    *know* that this class goes over the wire - it has a serialization ctor.
    It's right there, staring at you.
    
    There are other ways to do this, but I chose to put the serialization
    ctors right in the class for the three reasons above.

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/convert/ToBoolean.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/convert/ToBoolean.java
@@ -8,6 +8,8 @@
 package org.elasticsearch.xpack.esql.expression.function.scalar.convert;
 
 import org.apache.lucene.util.BytesRef;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.compute.ann.ConvertEvaluator;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
 import org.elasticsearch.xpack.esql.core.tree.NodeInfo;
@@ -17,6 +19,7 @@ import org.elasticsearch.xpack.esql.expression.function.Example;
 import org.elasticsearch.xpack.esql.expression.function.FunctionInfo;
 import org.elasticsearch.xpack.esql.expression.function.Param;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -31,6 +34,11 @@ import static org.elasticsearch.xpack.esql.type.EsqlDataTypeConverter.stringToBo
 import static org.elasticsearch.xpack.esql.type.EsqlDataTypeConverter.unsignedLongToBoolean;
 
 public class ToBoolean extends AbstractConvertFunction {
+    public static final NamedWriteableRegistry.Entry ENTRY = new NamedWriteableRegistry.Entry(
+        Expression.class,
+        "ToBoolean",
+        ToBoolean::new
+    );
 
     private static final Map<DataType, BuildFactory> EVALUATORS = Map.ofEntries(
         Map.entry(BOOLEAN, (field, source) -> field),
@@ -60,6 +68,15 @@ public class ToBoolean extends AbstractConvertFunction {
         ) Expression field
     ) {
         super(source, field);
+    }
+
+    private ToBoolean(StreamInput in) throws IOException {
+        super(in);
+    }
+
+    @Override
+    public String getWriteableName() {
+        return ENTRY.name;
     }
 
     @Override

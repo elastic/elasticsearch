60a34f2a900042e68cfcf47b9229cc91052cb687
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2024-06-12 14:15:32 -0700

    Do not use nested arrays as malformed values in scaled_float and unsigned_long synthetic source tests (#109650)
    They don't provide any additional value because arrays are parsed at the
    level above and tests already cover arrays.
    
    Fixes #109649.

--- x-pack/plugin/mapper-unsigned-long/src/test/java/org/elasticsearch/xpack/unsignedlong/UnsignedLongFieldMapperTests.java
+++ x-pack/plugin/mapper-unsigned-long/src/test/java/org/elasticsearch/xpack/unsignedlong/UnsignedLongFieldMapperTests.java
@@ -461,8 +461,7 @@ public class UnsignedLongFieldMapperTests extends WholeNumberFieldMapperTests {
             if (ignoreMalformedEnabled && randomBoolean()) {
                 List<Supplier<Object>> choices = List.of(
                     () -> randomAlphaOfLengthBetween(1, 10),
-                    () -> Map.of(randomAlphaOfLengthBetween(1, 10), randomAlphaOfLengthBetween(1, 10)),
-                    () -> List.of(randomAlphaOfLengthBetween(1, 10), randomAlphaOfLengthBetween(1, 10))
+                    () -> Map.of(randomAlphaOfLengthBetween(1, 10), randomAlphaOfLengthBetween(1, 10))
                 );
                 var malformedInput = randomFrom(choices).get();
                 return new Value(malformedInput, null, malformedInput);

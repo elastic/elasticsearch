d58da2c1056733df335a43beb4f0ca1078b808eb
Iván Cea Fontenla <ivancea96@outlook.com>
2024-06-13 11:01:42 +0200

    ESQL: Added BytesRef offset randomization to function tests (#109207)
    * Added BytesRef offset randomization to ESQL function tests
    
    * Added extra array length and randomized paddings

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/scalar/ip/IpPrefixTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/scalar/ip/IpPrefixTests.java
@@ -106,7 +106,7 @@ public class IpPrefixTests extends AbstractFunctionTestCase {
             })
         );
 
-        return parameterSuppliersFromTypedData(errorsForCasesWithoutExamples(anyNullIsNull(true, suppliers)));
+        return parameterSuppliersFromTypedData(errorsForCasesWithoutExamples(anyNullIsNull(true, randomizeBytesRefsOffset(suppliers))));
     }
 
     @Override

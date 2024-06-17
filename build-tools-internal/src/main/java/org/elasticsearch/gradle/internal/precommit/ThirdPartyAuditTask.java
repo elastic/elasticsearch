29e155564cb2e579c430660a9c1cac8d187bceb7
Rene Groeschke <rene@elastic.co>
2024-06-17 20:08:08 +0200

    Fix annotations in ThirdPartyAuditTask

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/precommit/ThirdPartyAuditTask.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/precommit/ThirdPartyAuditTask.java
@@ -28,6 +28,7 @@ import org.gradle.api.tasks.Classpath;
 import org.gradle.api.tasks.CompileClasspath;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputFile;
@@ -119,7 +120,7 @@ public abstract class ThirdPartyAuditTask extends DefaultTask {
     }
 
     @Classpath
-    @PathSensitive(PathSensitivity.NAME_ONLY)
+    @InputFiles
     public abstract ConfigurableFileCollection getForbiddenAPIsClasspath();
 
     @InputFile

435f37f47baf1d06a919cbcd13c6bcc03623d9da
ChrisHegarty <chegar999@gmail.com>
2024-06-18 12:21:52 +0100

    review comments

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/predicate/operator/fulltext/MultiMatchQuerySerializationTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/predicate/operator/fulltext/MultiMatchQuerySerializationTests.java
@@ -17,8 +17,6 @@ import java.util.stream.Collectors;
 
 public class MultiMatchQuerySerializationTests extends AbstractFulltextSerializationTests<MultiMatchQueryPredicate> {
 
-    private static final String COMMA = ",";
-
     @Override
     protected final MultiMatchQueryPredicate createTestInstance() {
         return new MultiMatchQueryPredicate(
@@ -34,10 +32,10 @@ public class MultiMatchQuerySerializationTests extends AbstractFulltextSerializa
         var fieldString = instance.fieldString();
         var query = instance.query();
         var options = instance.options();
-        switch (randomFrom(List.of(1, 2, 3))) {
-            case 1 -> fieldString = randomValueOtherThan(fieldString, this::randomFieldString);
-            case 2 -> query = randomValueOtherThan(query, () -> randomAlphaOfLength(randomIntBetween(1, 16)));
-            case 3 -> options = randomValueOtherThan(options, this::randomOptionOrNull);
+        switch (between(0, 2)) {
+            case 0 -> fieldString = randomValueOtherThan(fieldString, this::randomFieldString);
+            case 1 -> query = randomValueOtherThan(query, () -> randomAlphaOfLength(randomIntBetween(1, 16)));
+            case 2 -> options = randomValueOtherThan(options, this::randomOptionOrNull);
         }
         return new MultiMatchQueryPredicate(instance.source(), fieldString, query, options);
     }
@@ -52,7 +50,7 @@ public class MultiMatchQuerySerializationTests extends AbstractFulltextSerializa
             var opt = randomIndividualField();
             fields.computeIfAbsent(opt.getKey(), k -> opt.getValue()); // no duplicate fields
         }
-        return fields.entrySet().stream().map(e -> e.getKey() + "^" + e.getValue()).collect(Collectors.joining(COMMA));
+        return fields.entrySet().stream().map(e -> e.getKey() + "^" + e.getValue()).collect(Collectors.joining(","));
     }
 
     Map.Entry<String, Float> randomIndividualField() {

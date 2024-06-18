4c6d17bd2585db8c0b83644495890718d1d373a7
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2024-06-18 09:00:07 -0400

    removing unnecessary setting

--- modules/analysis-common/src/main/java/org/elasticsearch/analysis/common/UniqueTokenFilterFactory.java
+++ modules/analysis-common/src/main/java/org/elasticsearch/analysis/common/UniqueTokenFilterFactory.java
@@ -9,8 +9,6 @@
 package org.elasticsearch.analysis.common;
 
 import org.apache.lucene.analysis.TokenStream;
-import org.elasticsearch.common.logging.DeprecationCategory;
-import org.elasticsearch.common.logging.DeprecationLogger;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
@@ -18,27 +16,15 @@ import org.elasticsearch.index.IndexVersions;
 import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
 
 public class UniqueTokenFilterFactory extends AbstractTokenFilterFactory {
-    private static final DeprecationLogger deprecationLogger = DeprecationLogger.getLogger(UniqueTokenFilterFactory.class);
 
     static final String ONLY_ON_SAME_POSITION = "only_on_same_position";
-    static final String CORRECT_POSITION_INCREMENT = "correct_position_increment";
     private final boolean onlyOnSamePosition;
     private final boolean correctPositionIncrement;
 
     UniqueTokenFilterFactory(IndexSettings indexSettings, Environment environment, String name, Settings settings) {
         super(name, settings);
         this.onlyOnSamePosition = settings.getAsBoolean(ONLY_ON_SAME_POSITION, false);
-        this.correctPositionIncrement = settings.getAsBoolean(
-            CORRECT_POSITION_INCREMENT,
-            indexSettings.getIndexVersionCreated().onOrAfter(IndexVersions.UNIQUE_TOKEN_FILTER_POS_FIX)
-        );
-        if (settings.hasValue(CORRECT_POSITION_INCREMENT)) {
-            deprecationLogger.warn(
-                DeprecationCategory.ANALYSIS,
-                "unique_token_filter_position_fix",
-                "The setting [correct_position_increment] is deprecated and enabled by default. It will be removed in a future version."
-            );
-        }
+        this.correctPositionIncrement = indexSettings.getIndexVersionCreated().onOrAfter(IndexVersions.UNIQUE_TOKEN_FILTER_POS_FIX);
     }
 
     @Override

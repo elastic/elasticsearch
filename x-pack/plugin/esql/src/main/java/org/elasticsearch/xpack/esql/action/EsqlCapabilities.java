50f4ca785371cd6591309e3f614230e77e2d887d
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2024-06-19 18:23:27 +0200

    Back to Javadoc

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -27,37 +27,59 @@ public class EsqlCapabilities {
         // Support for function {@code CBRT}. Done in #108574.
         FN_CBRT(false),
 
-        // Support for {@code MV_APPEND} function. #107001
+        /**
+         * Support for {@code MV_APPEND} function. #107001
+         */
         FN_MV_APPEND(false),
 
-        // Support for function {@code IP_PREFIX}.
+        /**
+         * Support for function {@code IP_PREFIX}.
+         */
         FN_IP_PREFIX(false),
 
-        // Fix on function {@code SUBSTRING} that makes it not return null on empty strings.
+        /**
+         * Fix on function {@code SUBSTRING} that makes it not return null on empty strings.
+         */
         FN_SUBSTRING_EMPTY_NULL(false),
 
-        // Support for aggregation function {@code TOP_LIST}.
+        /**
+         * Support for aggregation function {@code TOP_LIST}.
+         */
         AGG_TOP_LIST(false),
 
-        // Optimization for ST_CENTROID changed some results in cartesian data. #108713
+        /**
+         * Optimization for ST_CENTROID changed some results in cartesian data. #108713
+         */
         ST_CENTROID_AGG_OPTIMIZED(false),
 
-        // Support for requesting the "_ignored" metadata field.
+        /**
+         * Support for requesting the "_ignored" metadata field.
+         */
         METADATA_IGNORED_FIELD(false),
 
-        // Support for the "LOOKUP" command.
+        /**
+         * Support for the "LOOKUP" command.
+         */
         LOOKUP_COMMAND(true),
 
-        // Support for the syntax {@code "tables": {"type": [<values>]}}.
+        /**
+         * Support for the syntax {@code "tables": {"type": [<values>]}}.
+         */
         TABLES_TYPES(true),
 
-        // Support for requesting the "REPEAT" command.
+        /**
+         * Support for requesting the "REPEAT" command.
+         */
         REPEAT(false),
 
-        // Cast string literals to datetime in addition and subtraction when the other side is a date or time interval.
+        /**
+         * Cast string literals to datetime in addition and subtraction when the other side is a date or time interval.
+         */
         STRING_LITERAL_AUTO_CASTING_TO_DATETIME_ADD_SUB(false),
 
-        // Support for named or positional parameters in EsqlQueryRequest.
+        /**
+         * Support for named or positional parameters in EsqlQueryRequest.
+         */
         NAMED_POSITIONAL_PARAMETER(false),
 
         /**

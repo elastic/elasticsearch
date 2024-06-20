7f1e72a94ec8e233a4e3689c0a9ab8f00aeeb83c
Nick Tindall <nick.tindall@elastic.co>
2024-06-20 12:18:58 +1000

    Add flag to background sync action, add test for it

--- server/src/main/java/org/elasticsearch/index/seqno/RetentionLeaseBackgroundSyncAction.java
+++ server/src/main/java/org/elasticsearch/index/seqno/RetentionLeaseBackgroundSyncAction.java
@@ -81,7 +81,8 @@ public class RetentionLeaseBackgroundSyncAction extends TransportReplicationActi
             actionFilters,
             Request::new,
             Request::new,
-            threadPool.executor(ThreadPool.Names.MANAGEMENT)
+            threadPool.executor(ThreadPool.Names.MANAGEMENT),
+            ActionFlags.BypassCircuitBreakerOnReplica
         );
     }
 

360320640d02cd1c9606efa635ae8b4937ab1a75
Tim Grein <grein.tim97@gmail.com>
2024-06-24 17:51:38 +0200

    Move embedding batch size comment to correct constant

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/googlevertexai/GoogleVertexAiServiceFields.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/googlevertexai/GoogleVertexAiServiceFields.java
@@ -9,17 +9,16 @@ package org.elasticsearch.xpack.inference.services.googlevertexai;
 
 public class GoogleVertexAiServiceFields {
 
+    public static final String LOCATION = "location";
+
+    public static final String PROJECT_ID = "project_id";
+
     /**
      * In `us-central-1` the max input size is `250`, but in every other region it's `5` according
      * to these docs: https://cloud.google.com/vertex-ai/generative-ai/docs/embeddings/get-text-embeddings.
      *
      * Therefore, being conservative and setting it to `5`.
      */
-
-    public static final String LOCATION = "location";
-
-    public static final String PROJECT_ID = "project_id";
-
     static final int EMBEDDING_MAX_BATCH_SIZE = 5;
 
 }

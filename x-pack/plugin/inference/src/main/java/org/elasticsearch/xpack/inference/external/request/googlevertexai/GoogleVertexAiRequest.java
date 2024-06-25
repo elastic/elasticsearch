adf84b7971a8bdf812829655bd381a016c6a5b2a
Tim Grein <grein.tim97@gmail.com>
2024-06-25 09:56:46 +0200

    Use AccessController and add special permission check

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/request/googlevertexai/GoogleVertexAiRequest.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/request/googlevertexai/GoogleVertexAiRequest.java
@@ -11,14 +11,16 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 
 import org.apache.http.client.methods.HttpPost;
+import org.elasticsearch.SpecialPermission;
 import org.elasticsearch.common.ValidationException;
 import org.elasticsearch.common.settings.SecureString;
-import org.elasticsearch.xpack.core.common.socket.SocketAccess;
 import org.elasticsearch.xpack.inference.external.request.Request;
 import org.elasticsearch.xpack.inference.services.googlevertexai.GoogleVertexAiSecretSettings;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
+import java.security.AccessController;
+import java.security.PrivilegedExceptionAction;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,13 +30,16 @@ public interface GoogleVertexAiRequest extends Request {
     List<String> AUTH_SCOPE = Collections.singletonList("https://www.googleapis.com/auth/cloud-platform");
 
     static void decorateWithBearerToken(HttpPost httpPost, GoogleVertexAiSecretSettings secretSettings) {
+        SpecialPermission.check();
         try {
-            SocketAccess.doPrivileged(() -> {
+            AccessController.doPrivileged((PrivilegedExceptionAction<Void>) () -> {
                 GoogleCredentials credentials = ServiceAccountCredentials.fromStream(
                     new ByteArrayInputStream(secretSettings.serviceAccountJson().toString().getBytes(StandardCharsets.UTF_8))
                 ).createScoped(AUTH_SCOPE);
                 credentials.refreshIfExpired();
                 httpPost.setHeader(createAuthBearerHeader(new SecureString(credentials.getAccessToken().getTokenValue().toCharArray())));
+
+                return null;
             });
         } catch (Exception e) {
             ValidationException validationException = new ValidationException(e);
@@ -42,5 +47,4 @@ public interface GoogleVertexAiRequest extends Request {
             throw validationException;
         }
     }
-
 }

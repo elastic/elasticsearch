784bd41645ffcdb76aa7d73bb2e029e59cacfe14
Adam Demjen <demjened@gmail.com>
2024-06-26 10:33:53 -0400

    Fix leak in tests

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/rank/textsimilarity/TextSimilarityRankTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/rank/textsimilarity/TextSimilarityRankTests.java
@@ -11,8 +11,6 @@ import org.apache.lucene.util.SetOnce;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionResponse;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.ActionFilter;
 import org.elasticsearch.action.support.ActionFilterChain;
 import org.elasticsearch.client.internal.Client;
@@ -22,6 +20,7 @@ import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.test.ESSingleNodeTestCase;
+import org.elasticsearch.test.hamcrest.ElasticsearchAssertions;
 import org.elasticsearch.xpack.core.inference.action.InferenceAction;
 import org.elasticsearch.xpack.core.inference.results.RankedDocsResults;
 import org.elasticsearch.xpack.inference.InferencePlugin;
@@ -111,38 +110,40 @@ public class TextSimilarityRankTests extends ESSingleNodeTestCase {
         client.admin().indices().prepareRefresh("my-index").get();
     }
 
-    public void testRerank() throws Exception {
-        // Execute search with text similarity reranking
-        SearchRequest request = client.prepareSearch()
-            .setRankBuilder(new TextSimilarityRankBuilder("text", "my-rerank-model", "my query", 100, 0.0f))
-            .setQuery(QueryBuilders.matchAllQuery())
-            .request();
-
-        // Verify order, rank and score of results
-        SearchResponse response = client.search(request).get();
-        SearchHit[] hits = response.getHits().getHits();
-        assertEquals(5, hits.length);
-        assertHitHasRankScoreAndText(hits[0], 1, 0.9f, "text 3");
-        assertHitHasRankScoreAndText(hits[1], 2, 0.8f, "text 0");
-        assertHitHasRankScoreAndText(hits[2], 3, 0.7f, "text 2");
-        assertHitHasRankScoreAndText(hits[3], 4, 0.6f, "text 1");
-        assertHitHasRankScoreAndText(hits[4], 5, 0.5f, "text 4");
+    public void testRerank() {
+        ElasticsearchAssertions.assertNoFailuresAndResponse(
+            // Execute search with text similarity reranking
+            client.prepareSearch()
+                .setRankBuilder(new TextSimilarityRankBuilder("text", "my-rerank-model", "my query", 100, 0.0f))
+                .setQuery(QueryBuilders.matchAllQuery()),
+            response -> {
+                // Verify order, rank and score of results
+                SearchHit[] hits = response.getHits().getHits();
+                assertEquals(5, hits.length);
+                assertHitHasRankScoreAndText(hits[0], 1, 0.9f, "text 3");
+                assertHitHasRankScoreAndText(hits[1], 2, 0.8f, "text 0");
+                assertHitHasRankScoreAndText(hits[2], 3, 0.7f, "text 2");
+                assertHitHasRankScoreAndText(hits[3], 4, 0.6f, "text 1");
+                assertHitHasRankScoreAndText(hits[4], 5, 0.5f, "text 4");
+            }
+        );
     }
 
-    public void testRerankWithMinScore() throws Exception {
-        // Execute search with text similarity reranking
-        SearchRequest request = client.prepareSearch()
-            .setRankBuilder(new TextSimilarityRankBuilder("text", "my-rerank-model", "my query", 100, 0.7f))
-            .setQuery(QueryBuilders.matchAllQuery())
-            .request();
-
-        // Verify order, rank and score of results
-        SearchResponse response = client.search(request).get();
-        SearchHit[] hits = response.getHits().getHits();
-        assertEquals(3, hits.length);
-        assertHitHasRankScoreAndText(hits[0], 1, 0.9f, "text 3");
-        assertHitHasRankScoreAndText(hits[1], 2, 0.8f, "text 0");
-        assertHitHasRankScoreAndText(hits[2], 3, 0.7f, "text 2");
+    public void testRerankWithMinScore() {
+        ElasticsearchAssertions.assertNoFailuresAndResponse(
+            // Execute search with text similarity reranking
+            client.prepareSearch()
+                .setRankBuilder(new TextSimilarityRankBuilder("text", "my-rerank-model", "my query", 100, 0.7f))
+                .setQuery(QueryBuilders.matchAllQuery()),
+            response -> {
+                // Verify order, rank and score of results
+                SearchHit[] hits = response.getHits().getHits();
+                assertEquals(3, hits.length);
+                assertHitHasRankScoreAndText(hits[0], 1, 0.9f, "text 3");
+                assertHitHasRankScoreAndText(hits[1], 2, 0.8f, "text 0");
+                assertHitHasRankScoreAndText(hits[2], 3, 0.7f, "text 2");
+            }
+        );
     }
 
     private static void assertHitHasRankScoreAndText(SearchHit hit, int expectedRank, float expectedScore, String expectedText) {

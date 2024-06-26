98246495d5aae51657ed57f5380e69c4ac31b14b
Mikhail Berezovskiy <mikhail@elastic.co>
2024-06-25 17:10:25 -0700

    add integ test

---
+++ x-pack/plugin/security/src/internalClusterTest/java/org/elasticsearch/xpack/security/transport/netty4/SecurityNetty4TransportCloseNotifyIT.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.security.transport.netty4;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.handler.codec.http.HttpResponseDecoder;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.core.SuppressForbidden;
+import org.elasticsearch.http.HttpServerTransport;
+import org.elasticsearch.test.ESIntegTestCase.ClusterScope;
+import org.elasticsearch.test.ESIntegTestCase.Scope;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.test.SecurityIntegTestCase;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+import static org.elasticsearch.test.rest.ESRestTestCase.basicAuthHeaderValue;
+
+@ClusterScope(numDataNodes = 0, scope = Scope.TEST)
+@ESTestCase.WithoutSecurityManager
+@SuppressForbidden(reason = "requires java.io.File for netty self-signed certificate")
+public class SecurityNetty4TransportCloseNotifyIT extends SecurityIntegTestCase {
+
+    @Override
+    protected boolean addMockHttpTransport() {
+        return false;
+    }
+
+    @Override
+    protected Settings nodeSettings(int nodeOrdinal, Settings otherSettings) {
+        try {
+            var ssc = new SelfSignedCertificate();
+            return Settings.builder()
+                .put(super.nodeSettings(nodeOrdinal, otherSettings))
+                .put("xpack.security.http.ssl.enabled", true)
+                .put("xpack.security.http.ssl.key", ssc.privateKey().getPath())
+                .put("xpack.security.http.ssl.certificate", ssc.certificate().getPath())
+                .build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    Bootstrap setupNettyClient(String node, Consumer<FullHttpResponse> responseHandler) throws Exception {
+        var sslCtx = SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+        var httpServer = internalCluster().getInstance(HttpServerTransport.class, node);
+        var remoteAddr = randomFrom(httpServer.boundAddress().boundAddresses());
+        return new Bootstrap().group(new NioEventLoopGroup(1))
+            .channel(NioSocketChannel.class)
+            .remoteAddress(remoteAddr.getAddress(), remoteAddr.getPort())
+            .handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) {
+                    var p = ch.pipeline();
+                    p.addLast(sslCtx.newHandler(ch.alloc()));
+                    p.addLast(new HttpRequestEncoder());
+                    p.addLast(new HttpResponseDecoder());
+                    p.addLast(new HttpObjectAggregator(4096));
+                    p.addLast(new SimpleChannelInboundHandler<FullHttpResponse>() {
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext ctx, FullHttpResponse msg) {
+                            responseHandler.accept(msg);
+                        }
+                    });
+                }
+            });
+    }
+
+    public void testSendCloseNotifyAfterHttpGetRequests() throws Exception {
+        var node = internalCluster().startNode();
+        var serverRespQueue = new ArrayBlockingQueue<FullHttpResponse>(10);
+        var client = setupNettyClient(node, serverRespQueue::add);
+        try {
+            var channel = client.connect().sync().channel();
+
+            // send some HTTP GET requests before closing a channel
+            var nReq = randomIntBetween(1, 10); // nothing particular about number 10
+            for (int i = 0; i < nReq; i++) {
+                var req = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/");
+                req.headers().add(HttpHeaderNames.AUTHORIZATION, basicAuthHeaderValue(nodeClientUsername(), nodeClientPassword()));
+                channel.writeAndFlush(req).get(5, TimeUnit.SECONDS);
+            }
+            assertBusy(() -> assertEquals(nReq, serverRespQueue.size()));
+            assertTrue(serverRespQueue.stream().allMatch(resp -> resp.status().code() == 200));
+
+            // send close_notify alert and wait for channel closure
+            var sslHandler = channel.pipeline().get(SslHandler.class);
+            sslHandler.closeOutbound();
+            try {
+                assertTrue(channel.closeFuture().await(5000));
+            } finally {
+                channel.close().sync();
+            }
+        } finally {
+            client.config().group().shutdownGracefully().sync();
+        }
+    }
+
+}

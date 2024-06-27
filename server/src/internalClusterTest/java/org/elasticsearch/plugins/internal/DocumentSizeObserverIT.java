b778927187c355a713d51219ebef5d25e949a6bd
Przemyslaw Gomulka <przemyslaw.gomulka@elastic.co>
2024-06-27 13:59:58 +0200

    spotless

--- server/src/internalClusterTest/java/org/elasticsearch/plugins/internal/DocumentSizeObserverIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/plugins/internal/DocumentSizeObserverIT.java
@@ -26,7 +26,6 @@ import org.elasticsearch.xcontent.XContentType;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 

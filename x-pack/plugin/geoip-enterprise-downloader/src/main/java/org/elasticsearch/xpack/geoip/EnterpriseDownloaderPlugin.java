de507a7f8fa5fbffd8ba45b10b8354110f73d0e2
Joe Gallo <joegallo@gmail.com>
2024-06-27 14:37:33 -0400

    Simplify slightly, treeshaking

--- x-pack/plugin/geoip-enterprise-downloader/src/main/java/org/elasticsearch/xpack/geoip/EnterpriseDownloaderPlugin.java
+++ x-pack/plugin/geoip-enterprise-downloader/src/main/java/org/elasticsearch/xpack/geoip/EnterpriseDownloaderPlugin.java
@@ -6,23 +6,15 @@
  */
 package org.elasticsearch.xpack.geoip;
 
-import org.elasticsearch.client.internal.Client;
-import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
-import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.settings.SettingsModule;
 import org.elasticsearch.license.XPackLicenseState;
-import org.elasticsearch.persistent.PersistentTasksExecutor;
-import org.elasticsearch.plugins.IngestPlugin;
-import org.elasticsearch.plugins.PersistentTaskPlugin;
 import org.elasticsearch.plugins.Plugin;
-import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.xpack.core.XPackPlugin;
 
 import java.util.Collection;
 import java.util.List;
 
-public class EnterpriseDownloaderPlugin extends Plugin implements IngestPlugin, PersistentTaskPlugin {
+public class EnterpriseDownloaderPlugin extends Plugin {
 
     private final Settings settings;
     private EnterpriseGeoIpDownloaderLicenseListener enterpriseGeoIpDownloaderTaskExecutor;
@@ -45,16 +37,5 @@ public class EnterpriseDownloaderPlugin extends Plugin implements IngestPlugin, 
         );
         enterpriseGeoIpDownloaderTaskExecutor.init();
         return List.of(enterpriseGeoIpDownloaderTaskExecutor);
-    }
-
-    @Override
-    public List<PersistentTasksExecutor<?>> getPersistentTasksExecutor(
-        ClusterService clusterService,
-        ThreadPool threadPool,
-        Client client,
-        SettingsModule settingsModule,
-        IndexNameExpressionResolver expressionResolver
-    ) {
-        return List.of();
     }
 }

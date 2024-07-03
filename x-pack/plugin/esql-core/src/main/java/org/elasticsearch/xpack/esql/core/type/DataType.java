0d31beeb4c7b718364362cb745d983dd975594b4
Nik Everett <nik9000@gmail.com>
2024-07-03 08:20:34 -0400

    ESQL: Add javadocs for some of DataType (#110396)
    This adds some javadoc to a few of the methods on `DataType`. That's
    important because `DataType` is pretty central to how ESQL works and is
    referenced in tons of code. The method `isInteger` especially wants an
    explanation - it's true for all whole numbers.

--- x-pack/plugin/esql-core/src/main/java/org/elasticsearch/xpack/esql/core/type/DataType.java
+++ x-pack/plugin/esql-core/src/main/java/org/elasticsearch/xpack/esql/core/type/DataType.java
@@ -254,18 +254,31 @@ public enum DataType {
         return esType;
     }
 
+    /**
+     * The name we give to types on the response.
+     */
     public String outputType() {
         return esType == null ? "unsupported" : esType;
     }
 
+    /**
+     * Does this data type represent whole numbers? As in, numbers without a decimal point.
+     * Like {@code int} or {@code long}. See {@link #isRational} for numbers with a decimal point.
+     */
     public boolean isInteger() {
         return isInteger;
     }
 
+    /**
+     * Does this data type represent rational numbers (like floating point)?
+     */
     public boolean isRational() {
         return isRational;
     }
 
+    /**
+     * Does this data type represent <strong>any</strong> number?
+     */
     public boolean isNumeric() {
         return isInteger || isRational;
     }

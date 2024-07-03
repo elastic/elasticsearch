06cd6471973d1e36f8ed6d80ce2ed1f9ff7c720f
Alexander Spies <alexander.spies@elastic.co>
2024-07-03 13:31:25 +0200

    Fix withGeneratedNames for Eval

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/GeneratingPlan.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/GeneratingPlan.java
@@ -9,7 +9,9 @@ package org.elasticsearch.xpack.esql.plan;
 
 import org.elasticsearch.xpack.esql.core.expression.Alias;
 import org.elasticsearch.xpack.esql.core.expression.Attribute;
+import org.elasticsearch.xpack.esql.core.expression.AttributeMap;
 import org.elasticsearch.xpack.esql.core.expression.NameId;
+import org.elasticsearch.xpack.esql.core.expression.ReferenceAttribute;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,10 +24,11 @@ public interface GeneratingPlan<PlanType extends GeneratingPlan<PlanType>> {
     static List<Alias> renameAliases(List<Alias> originalAliases, List<String> newNames) {
         if (newNames.size() != originalAliases.size()) {
             throw new IllegalArgumentException(
-                "Number of new names is [" + newNames.size() + "] but there are [" + originalAliases.size() + "] names."
+                "Number of new names is [" + newNames.size() + "] but there are [" + originalAliases.size() + "] existing names."
             );
         }
 
+        AttributeMap.Builder<Attribute> aliasReplacedByBuilder = AttributeMap.builder();
         List<Alias> newFields = new ArrayList<>(originalAliases.size());
         for (int i = 0; i < originalAliases.size(); i++) {
             Alias field = originalAliases.get(i);
@@ -33,10 +36,26 @@ public interface GeneratingPlan<PlanType extends GeneratingPlan<PlanType>> {
             if (field.name().equals(newName)) {
                 newFields.add(field);
             } else {
-                newFields.add(new Alias(field.source(), newName, field.qualifier(), field.child(), new NameId(), field.synthetic()));
+                Alias newField = new Alias(field.source(), newName, field.qualifier(), field.child(), new NameId(), field.synthetic());
+                newFields.add(newField);
+                aliasReplacedByBuilder.put(field.toAttribute(), newField.toAttribute());
             }
         }
+        AttributeMap<Attribute> aliasReplacedBy = aliasReplacedByBuilder.build();
 
-        return newFields;
+        // We need to also update any references to the old attributes in the new attributes; e.g.
+        // EVAL x = 1, y = x + 1
+        // renaming x, y to x1, y1
+        // so far became
+        // EVAL x1 = 1, y1 = x + 1
+        // - but x doesn't exist anymore, so replace it by x1 to obtain
+        // EVAL x1 = 1, y1 = x1 + 1
+
+        List<Alias> newFieldsWithUpdatedRefs = new ArrayList<>(originalAliases.size());
+        for (Alias newField : newFields) {
+            newFieldsWithUpdatedRefs.add((Alias) newField.transformUp(ReferenceAttribute.class, r -> aliasReplacedBy.resolve(r, r)));
+        }
+
+        return newFieldsWithUpdatedRefs;
     }
 }

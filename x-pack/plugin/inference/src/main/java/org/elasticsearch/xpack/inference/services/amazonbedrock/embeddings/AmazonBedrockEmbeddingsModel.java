9b68190704fd2e3b85c9067932c4af17adf914f8
Mark J. Hoy <mark.hoy@elastic.co>
2024-07-05 13:01:29 -0400

    [Inference API] Add Amazon Bedrock support to Inference API (#110248)
    * Initial commit; setup Gradle; start service
    
    * initial commit
    
    * minor cleanups, builds green; needs tests
    
    * bug fixes; tested working embeddings & completion
    
    * use custom json builder for embeddings request
    
    * Ensure auto-close; fix forbidden API
    
    * start of adding unit tests; abstraction layers
    
    * adding additional tests; cleanups
    
    * add requests unit tests
    
    * all tests created
    
    * fix cohere embeddings response
    
    * fix cohere embeddings response
    
    * fix lint
    
    * better test coverage for secrets; inference client
    
    * update thread-safe syncs; make dims/tokens + int
    
    * add tests for dims and max tokens positive integer
    
    * use requireNonNull;override settings type;cleanups
    
    * use r/w lock for client cache
    
    * remove client reference counting
    
    * update locking in cache; client errors; noop doc
    
    * remove extra block in internalGetOrCreateClient
    
    * remove duplicate dependencies; cleanup
    
    * add fxn to get default embeddings similarity
    
    * use async calls to Amazon Bedrock; cleanups
    
    * use Clock in cache; simplify locking; cleanups
    
    * cleanups around executor; remove some instanceof
    
    * cleanups; use EmbeddingRequestChunker
    
    * move max chunk size to constants
    
    * oof - swapped transport vers w/ master node req
    
    * use XContent instead of Jackson JsonFactory
    
    * remove gradle versions; do not allow dimensions
    
    (cherry picked from commit 52e591d6a61c5093535e683bdec1880ef671584f)

---
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/amazonbedrock/embeddings/AmazonBedrockEmbeddingsModel.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.inference.services.amazonbedrock.embeddings;
+
+import org.elasticsearch.common.ValidationException;
+import org.elasticsearch.inference.EmptyTaskSettings;
+import org.elasticsearch.inference.Model;
+import org.elasticsearch.inference.ModelConfigurations;
+import org.elasticsearch.inference.ModelSecrets;
+import org.elasticsearch.inference.ServiceSettings;
+import org.elasticsearch.inference.TaskSettings;
+import org.elasticsearch.inference.TaskType;
+import org.elasticsearch.xpack.inference.external.action.ExecutableAction;
+import org.elasticsearch.xpack.inference.external.action.amazonbedrock.AmazonBedrockActionVisitor;
+import org.elasticsearch.xpack.inference.services.ConfigurationParseContext;
+import org.elasticsearch.xpack.inference.services.amazonbedrock.AmazonBedrockModel;
+import org.elasticsearch.xpack.inference.services.amazonbedrock.AmazonBedrockSecretSettings;
+
+import java.util.Map;
+
+public class AmazonBedrockEmbeddingsModel extends AmazonBedrockModel {
+
+    public static AmazonBedrockEmbeddingsModel of(AmazonBedrockEmbeddingsModel embeddingsModel, Map<String, Object> taskSettings) {
+        if (taskSettings != null && taskSettings.isEmpty() == false) {
+            // no task settings allowed
+            var validationException = new ValidationException();
+            validationException.addValidationError("Amazon Bedrock embeddings model cannot have task settings");
+            throw validationException;
+        }
+
+        return embeddingsModel;
+    }
+
+    public AmazonBedrockEmbeddingsModel(
+        String inferenceEntityId,
+        TaskType taskType,
+        String service,
+        Map<String, Object> serviceSettings,
+        Map<String, Object> taskSettings,
+        Map<String, Object> secretSettings,
+        ConfigurationParseContext context
+    ) {
+        this(
+            inferenceEntityId,
+            taskType,
+            service,
+            AmazonBedrockEmbeddingsServiceSettings.fromMap(serviceSettings, context),
+            new EmptyTaskSettings(),
+            AmazonBedrockSecretSettings.fromMap(secretSettings)
+        );
+    }
+
+    public AmazonBedrockEmbeddingsModel(
+        String inferenceEntityId,
+        TaskType taskType,
+        String service,
+        AmazonBedrockEmbeddingsServiceSettings serviceSettings,
+        TaskSettings taskSettings,
+        AmazonBedrockSecretSettings secrets
+    ) {
+        super(
+            new ModelConfigurations(inferenceEntityId, taskType, service, serviceSettings, new EmptyTaskSettings()),
+            new ModelSecrets(secrets)
+        );
+    }
+
+    public AmazonBedrockEmbeddingsModel(Model model, ServiceSettings serviceSettings) {
+        super(model, serviceSettings);
+    }
+
+    @Override
+    public ExecutableAction accept(AmazonBedrockActionVisitor creator, Map<String, Object> taskSettings) {
+        return creator.create(this, taskSettings);
+    }
+
+    @Override
+    public AmazonBedrockEmbeddingsServiceSettings getServiceSettings() {
+        return (AmazonBedrockEmbeddingsServiceSettings) super.getServiceSettings();
+    }
+}

ed3efade079e2ac4296ec0558297bc81777dc000
Pat Whelan <pat.whelan@elastic.co>
2024-07-12 14:49:40 -0400

    [ML] Consolidate ExecutableActions (#110806)
    - Created two new ExecutableActions, SenderExecutableAction and
      SingleInputSenderExecutableAction
    - Most chat completions are migrated over the
      SingleInputSenderExecutableAction
    - Every other Action are migrated over to SenderExecutableAction
    - RequestManagers and Error Message construction are migrated into the
      ActionCreator classes.
    
    Relate #110805

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/action/SenderExecutableAction.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/action/SenderExecutableAction.java
@@ -5,44 +5,38 @@
  * 2.0.
  */
 
-package org.elasticsearch.xpack.inference.external.action.amazonbedrock;
+package org.elasticsearch.xpack.inference.external.action;
 
-import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.inference.InferenceServiceResults;
-import org.elasticsearch.xpack.inference.external.action.ExecutableAction;
 import org.elasticsearch.xpack.inference.external.http.sender.InferenceInputs;
 import org.elasticsearch.xpack.inference.external.http.sender.RequestManager;
 import org.elasticsearch.xpack.inference.external.http.sender.Sender;
 
 import java.util.Objects;
 
-import static org.elasticsearch.xpack.inference.external.action.ActionUtils.createInternalServerError;
 import static org.elasticsearch.xpack.inference.external.action.ActionUtils.wrapFailuresInElasticsearchException;
 
-public class AmazonBedrockEmbeddingsAction implements ExecutableAction {
+public class SenderExecutableAction implements ExecutableAction {
 
     private final Sender sender;
     private final RequestManager requestManager;
-    private final String errorMessage;
+    private final String failedToSendRequestErrorMessage;
 
-    public AmazonBedrockEmbeddingsAction(Sender sender, RequestManager requestManager, String errorMessage) {
+    public SenderExecutableAction(Sender sender, RequestManager requestManager, String failedToSendRequestErrorMessage) {
         this.sender = Objects.requireNonNull(sender);
         this.requestManager = Objects.requireNonNull(requestManager);
-        this.errorMessage = Objects.requireNonNull(errorMessage);
+        this.failedToSendRequestErrorMessage = Objects.requireNonNull(failedToSendRequestErrorMessage);
     }
 
     @Override
     public void execute(InferenceInputs inferenceInputs, TimeValue timeout, ActionListener<InferenceServiceResults> listener) {
+        var wrappedListener = wrapFailuresInElasticsearchException(failedToSendRequestErrorMessage, listener);
         try {
-            ActionListener<InferenceServiceResults> wrappedListener = wrapFailuresInElasticsearchException(errorMessage, listener);
-
             sender.send(requestManager, inferenceInputs, timeout, wrappedListener);
-        } catch (ElasticsearchException e) {
-            listener.onFailure(e);
         } catch (Exception e) {
-            listener.onFailure(createInternalServerError(e, errorMessage));
+            wrappedListener.onFailure(e);
         }
     }
 }

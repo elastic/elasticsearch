0424472d8988f72d7062d04adc32d0282b79b0ef
Mike Pellegrini <mike.pellegrini@elastic.co>
2024-07-15 17:03:16 -0400

    Test analysis with synonym analyzer

--- modules/analysis-common/src/internalClusterTest/java/org/elasticsearch/analysis/common/SynonymsApiIT.java
+++ modules/analysis-common/src/internalClusterTest/java/org/elasticsearch/analysis/common/SynonymsApiIT.java
@@ -9,6 +9,8 @@
 package org.elasticsearch.analysis.common;
 
 import org.elasticsearch.action.ActionFuture;
+import org.elasticsearch.action.admin.indices.analyze.AnalyzeAction;
+import org.elasticsearch.action.admin.indices.analyze.AnalyzeRequestBuilder;
 import org.elasticsearch.action.admin.indices.close.CloseIndexRequest;
 import org.elasticsearch.action.admin.indices.open.OpenIndexRequest;
 import org.elasticsearch.action.synonyms.PutSynonymsAction;
@@ -34,11 +36,14 @@ import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.action.synonyms.PutSynonymsAction.Request.SYNONYMS_SET_FIELD;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
 @ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST)
 public class SynonymsApiIT extends ESIntegTestCase {
     private static final TimeValue DEFAULT_TIMEOUT = new TimeValue(1, TimeUnit.SECONDS);
+    private static final String INDEX_ANALYZER_NAME = "standard_analyzer";
+    private static final String SEARCH_ANALYZER_NAME = "synonym_analyzer";
 
     @Override
     protected Collection<Class<? extends Plugin>> nodePlugins() {
@@ -54,11 +59,11 @@ public class SynonymsApiIT extends ESIntegTestCase {
         assertCreateSynonymsSet(createPutSynonymsRequest(synonymsSetId, List.of(new SynonymRule(null, "foo => bar"))));
         assertCreateIndexWithSynonyms(indexName, fieldName, synonymsSetId, stopwords);
         ensureGreen(DEFAULT_TIMEOUT, indexName);
-        // TODO: Exercise synonym with analyze API
+        assertAnalysis(DEFAULT_TIMEOUT, indicesAdmin().prepareAnalyze(indexName, "foo").setAnalyzer(SEARCH_ANALYZER_NAME), List.of("bar"));
 
         assertUpdateSynonymsSet(createPutSynonymsRequest(synonymsSetId, List.of(new SynonymRule(null, "foo => baz"))));
         ensureGreen(DEFAULT_TIMEOUT, indexName);
-        // TODO: Exercise synonym with analyze API
+        assertAnalysis(DEFAULT_TIMEOUT, indicesAdmin().prepareAnalyze(indexName, "foo").setAnalyzer(SEARCH_ANALYZER_NAME), List.of("foo"));
 
         reloadIndex(DEFAULT_TIMEOUT, indexName);
         ensureGreen(DEFAULT_TIMEOUT, indexName);
@@ -67,23 +72,23 @@ public class SynonymsApiIT extends ESIntegTestCase {
     private void assertCreateIndexWithSynonyms(String indexName, String fieldName, String synonymsSetId, List<String> stopwords) {
         Settings.Builder builder = Settings.builder()
             .put(indexSettings())
-            .put("index.analysis.analyzer.standard_analyzer.type", "standard")
-            .put("index.analysis.analyzer.synonym_analyzer.tokenizer", "standard")
-            .putList("index.analysis.analyzer.synonym_analyzer.filter", "lowercase", "synonym")
+            .put("index.analysis.analyzer." + INDEX_ANALYZER_NAME + ".type", "standard")
+            .put("index.analysis.analyzer." + SEARCH_ANALYZER_NAME + ".tokenizer", "standard")
+            .putList("index.analysis.analyzer." + SEARCH_ANALYZER_NAME + ".filter", "lowercase", "synonym")
             .put("index.analysis.filter.synonym.type", "synonym_graph")
             .put("index.analysis.filter.synonym.synonyms_set", synonymsSetId)
             .put("index.analysis.filter.synonym.updateable", true);
 
         if (stopwords != null && stopwords.isEmpty() == false) {
-            builder.putList("index.analysis.analyzer.standard_analyzer.stopwords", stopwords)
+            builder.putList("index.analysis.analyzer." + INDEX_ANALYZER_NAME + ".stopwords", stopwords)
                 .put("index.analysis.filter.stopwords.type", "stop")
                 .putList("index.analysis.filter.stopwords.stopwords", stopwords)
-                .putList("index.analysis.analyzer.synonym_analyzer.filter", "lowercase", "stopwords", "synonym");
+                .putList("index.analysis.analyzer." + SEARCH_ANALYZER_NAME + ".filter", "lowercase", "stopwords", "synonym");
         }
 
         assertAcked(
             prepareCreate(indexName).setSettings(builder)
-                .setMapping(fieldName, "type=text,analyzer=standard_analyzer,search_analyzer=synonym_analyzer")
+                .setMapping(fieldName, "type=text,analyzer=" + INDEX_ANALYZER_NAME + ",search_analyzer=" + SEARCH_ANALYZER_NAME)
         );
     }
 
@@ -113,6 +118,11 @@ public class SynonymsApiIT extends ESIntegTestCase {
     private static void assertUpdateSynonymsSet(PutSynonymsAction.Request request) {
         ActionFuture<SynonymUpdateResponse> response = client().execute(PutSynonymsAction.INSTANCE, request);
         assertThat(response.actionGet().status(), is(RestStatus.OK));
+    }
+
+    private static void assertAnalysis(TimeValue timeout, AnalyzeRequestBuilder analyzeRequest, List<String> expectedTokens) {
+        AnalyzeAction.Response response = analyzeRequest.get(timeout);
+        assertThat(expectedTokens, equalTo(response.getTokens().stream().map(AnalyzeAction.AnalyzeToken::getTerm).toList()));
     }
 
     private static void reloadIndex(TimeValue timeout, String indexName) {

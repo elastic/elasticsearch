7dee7738eb1d00dc63d9611d46c5db20df8b066f
Aurelien FOUCRET <aurelien.foucret@elastic.co>
2024-07-17 13:08:29 +0200

    Fixing failing tests.

--- x-pack/plugin/spatial/src/test/java/org/elasticsearch/xpack/spatial/index/mapper/GeoShapeScriptFieldTypeTests.java
+++ x-pack/plugin/spatial/src/test/java/org/elasticsearch/xpack/spatial/index/mapper/GeoShapeScriptFieldTypeTests.java
@@ -175,6 +175,11 @@ public class GeoShapeScriptFieldTypeTests extends AbstractNonTextScriptFieldType
                     }
 
                     @Override
+                    public boolean needs_termStatistics() {
+                        return false;
+                    }
+
+                    @Override
                     public ScoreScript newInstance(DocReader docReader) {
                         return new ScoreScript(Map.of(), searchContext.lookup(), docReader) {
                             @Override

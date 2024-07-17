c7ece9b400aa9abb20e47631f8fc4f1cda00a072
Alexander Spies <alexander.spies@elastic.co>
2024-07-17 17:15:11 +0200

    ESQL: Correctly compute Rename's output (#110968) (#110976)
    Calling Rename.output() previously returned wrong results.
    
    Since #110488, instead it throws an IllegalStateException. That leads to test failures in the EsqlNodeSubclassTests because e.g. MvExpandExec and FieldExtractExec eagerly calls .output() on its child when it's being constructed, and the child can be a fragment containing a Rename.
    
    (cherry picked from commit 7df1b06525d574dcc250736bb506fbb058fd8c9f)
    
    # Conflicts:
    #	x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/logical/Rename.java

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/logical/Rename.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/logical/Rename.java
@@ -7,8 +7,11 @@
 
 package org.elasticsearch.xpack.esql.plan.logical;
 
+import org.elasticsearch.xpack.esql.analysis.Analyzer.ResolveRefs;
 import org.elasticsearch.xpack.esql.core.expression.Alias;
 import org.elasticsearch.xpack.esql.core.expression.Attribute;
+import org.elasticsearch.xpack.esql.core.expression.Expressions;
+import org.elasticsearch.xpack.esql.core.expression.NamedExpression;
 import org.elasticsearch.xpack.esql.core.plan.logical.LogicalPlan;
 import org.elasticsearch.xpack.esql.core.plan.logical.UnaryPlan;
 import org.elasticsearch.xpack.esql.core.tree.NodeInfo;
@@ -33,8 +36,10 @@ public class Rename extends UnaryPlan {
 
     @Override
     public List<Attribute> output() {
-        // Rename is mapped to a Project during analysis; we do not compute the output here.
-        throw new IllegalStateException("Should never reach here.");
+        // Normally shouldn't reach here, as Rename only exists before resolution.
+        List<NamedExpression> projectionsAfterResolution = ResolveRefs.projectionsForRename(this, this.child().output(), null);
+
+        return Expressions.asAttributes(projectionsAfterResolution);
     }
 
     @Override

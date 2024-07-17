85794868c4ed479cfe5ea23ebc66edd24dfce39e
Alexander Spies <alexander.spies@elastic.co>
2024-07-17 13:28:09 +0200

    Redirect non-static method to static method

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/Analyzer.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/Analyzer.java
@@ -576,10 +576,7 @@ public class Analyzer extends ParameterizedRuleExecutor<LogicalPlan, AnalyzerCon
         }
 
         private Attribute maybeResolveAttribute(UnresolvedAttribute ua, List<Attribute> childrenOutput) {
-            if (ua.customMessage()) {
-                return ua;
-            }
-            return resolveAttribute(ua, childrenOutput);
+            maybeResolveAttribute(ua, childrenOutput, log);
         }
 
         private static Attribute maybeResolveAttribute(UnresolvedAttribute ua, List<Attribute> childrenOutput, Logger logger) {

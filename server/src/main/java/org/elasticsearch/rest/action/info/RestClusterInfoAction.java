9caa217967fbc8e031a1be1303885442303a6072
Mikhail Berezovskiy <mikhail@elastic.co>
2024-07-19 17:10:34 -0700

    Use NodesStatsRequestParameters.Metric enums more over string (#110810)
    Use EnumSet for NodesStatsRequestParameters.Metric
    
    ---------
    
    Co-authored-by: David Turner <david.turner@elastic.co>

--- server/src/main/java/org/elasticsearch/rest/action/info/RestClusterInfoAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/info/RestClusterInfoAction.java
@@ -10,6 +10,7 @@ package org.elasticsearch.rest.action.info;
 
 import org.elasticsearch.action.admin.cluster.node.stats.NodeStats;
 import org.elasticsearch.action.admin.cluster.node.stats.NodesStatsRequest;
+import org.elasticsearch.action.admin.cluster.node.stats.NodesStatsRequestParameters.Metric;
 import org.elasticsearch.action.admin.cluster.node.stats.NodesStatsResponse;
 import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.common.Strings;
@@ -40,6 +41,7 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toUnmodifiableSet;
 import static org.elasticsearch.action.admin.cluster.node.stats.NodesStatsRequestParameters.Metric.HTTP;
 import static org.elasticsearch.action.admin.cluster.node.stats.NodesStatsRequestParameters.Metric.INGEST;
 import static org.elasticsearch.action.admin.cluster.node.stats.NodesStatsRequestParameters.Metric.SCRIPT;
@@ -49,29 +51,30 @@ import static org.elasticsearch.xcontent.ToXContent.EMPTY_PARAMS;
 @ServerlessScope(Scope.PUBLIC)
 public class RestClusterInfoAction extends BaseRestHandler {
 
-    static final Map<String, Function<NodesStatsResponse, ChunkedToXContent>> RESPONSE_MAPPER = Map.of(
-        HTTP.metricName(),
+    static final Map<Metric, Function<NodesStatsResponse, ChunkedToXContent>> RESPONSE_MAPPER = Map.of(
+        HTTP,
         nodesStatsResponse -> nodesStatsResponse.getNodes().stream().map(NodeStats::getHttp).reduce(HttpStats.IDENTITY, HttpStats::merge),
         //
-        INGEST.metricName(),
+        INGEST,
         nodesStatsResponse -> nodesStatsResponse.getNodes()
             .stream()
             .map(NodeStats::getIngestStats)
             .reduce(IngestStats.IDENTITY, IngestStats::merge),
         //
-        THREAD_POOL.metricName(),
+        THREAD_POOL,
         nodesStatsResponse -> nodesStatsResponse.getNodes()
             .stream()
             .map(NodeStats::getThreadPool)
             .reduce(ThreadPoolStats.IDENTITY, ThreadPoolStats::merge),
         //
-        SCRIPT.metricName(),
+        SCRIPT,
         nodesStatsResponse -> nodesStatsResponse.getNodes()
             .stream()
             .map(NodeStats::getScriptStats)
             .reduce(ScriptStats.IDENTITY, ScriptStats::merge)
     );
-    static final Set<String> AVAILABLE_TARGETS = RESPONSE_MAPPER.keySet();
+    static final Set<Metric> AVAILABLE_TARGETS = RESPONSE_MAPPER.keySet();
+    static final Set<String> AVAILABLE_TARGET_NAMES = AVAILABLE_TARGETS.stream().map(Metric::metricName).collect(toUnmodifiableSet());
 
     @Override
     public String getName() {
@@ -93,7 +96,7 @@ public class RestClusterInfoAction extends BaseRestHandler {
             targets.clear();
             AVAILABLE_TARGETS.forEach(m -> {
                 nodesStatsRequest.addMetric(m);
-                targets.add(m);
+                targets.add(m.metricName());
             });
 
         } else if (targets.contains("_all")) {
@@ -103,14 +106,14 @@ public class RestClusterInfoAction extends BaseRestHandler {
 
         } else {
             var invalidTargetParams = targets.stream()
-                .filter(Predicate.not(AVAILABLE_TARGETS::contains))
+                .filter(Predicate.not(AVAILABLE_TARGET_NAMES::contains))
                 .collect(Collectors.toCollection(TreeSet::new));
 
             if (invalidTargetParams.isEmpty() == false) {
-                throw new IllegalArgumentException(unrecognized(request, invalidTargetParams, AVAILABLE_TARGETS, "target"));
+                throw new IllegalArgumentException(unrecognized(request, invalidTargetParams, AVAILABLE_TARGET_NAMES, "target"));
             }
 
-            targets.forEach(nodesStatsRequest::addMetric);
+            targets.forEach(metricName -> nodesStatsRequest.addMetric(Metric.get(metricName)));
         }
 
         return channel -> new RestCancellableNodeClient(client, request.getHttpChannel()).admin()
@@ -118,7 +121,11 @@ public class RestClusterInfoAction extends BaseRestHandler {
             .nodesStats(nodesStatsRequest, new RestResponseListener<>(channel) {
                 @Override
                 public RestResponse buildResponse(NodesStatsResponse response) throws Exception {
-                    var chunkedResponses = targets.stream().map(RESPONSE_MAPPER::get).map(mapper -> mapper.apply(response)).iterator();
+                    var chunkedResponses = targets.stream()
+                        .map(Metric::get)
+                        .map(RESPONSE_MAPPER::get)
+                        .map(mapper -> mapper.apply(response))
+                        .iterator();
 
                     return RestResponse.chunked(
                         RestStatus.OK,

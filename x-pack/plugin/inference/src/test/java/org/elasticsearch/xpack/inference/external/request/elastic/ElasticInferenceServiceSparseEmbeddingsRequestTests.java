ebb16d9c0113463975ca5f7c58ec771d9cc0f3d8
Tim Grein <grein.tim97@gmail.com>
2024-07-22 14:27:52 +0200

    Add ElasticInferenceServiceSparseEmbeddingsRequestTests TODOs

---
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/external/request/elastic/ElasticInferenceServiceSparseEmbeddingsRequestTests.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.inference.external.request.elastic;
+
+import org.elasticsearch.test.ESTestCase;
+
+public class ElasticInferenceServiceSparseEmbeddingsRequestTests extends ESTestCase {
+
+    // See AzureOpenAiEmbeddingsRequestTests as reference
+
+    //TODO: test create request: with model defined
+
+    //TODO: test truncate: reduces input text size by half
+
+    //TODO: test is truncated: returns true
+
+}

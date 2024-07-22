499112543ec40155f5e342318b205895b3f591cb
Tim Grein <grein.tim97@gmail.com>
2024-07-22 14:20:28 +0200

    Add ElasticInferenceServiceActionCreatorTests TODOs

---
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/external/action/elastic/ElasticInferenceServiceActionCreatorTests.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.inference.external.action.elastic;
+
+import org.elasticsearch.test.ESTestCase;
+
+public class ElasticInferenceServiceActionCreatorTests extends ESTestCase {
+
+    // See HuggingFaceActionCreatorTests for reference
+
+    //TODO: init mock web server
+
+    //TODO: shutdown mock web server
+
+    //TODO: test execute: returns successful response for elser embeddings action
+
+    //TODO: test execute: fails from invalid response format for elser embeddings action
+
+    //TODO: test execute: returns successful response after truncating
+
+    //TODO: test execute: truncates input before sending
+
+}

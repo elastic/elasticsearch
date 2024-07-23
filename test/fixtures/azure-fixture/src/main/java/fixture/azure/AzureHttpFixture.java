09633762409e76f0e3a485632c3c5e4ac6b4bc32
David Turner <david.turner@elastic.co>
2024-07-23 12:35:00 +0100

    Copy resources to tmpdir

--- test/fixtures/azure-fixture/src/main/java/fixture/azure/AzureHttpFixture.java
+++ test/fixtures/azure-fixture/src/main/java/fixture/azure/AzureHttpFixture.java
@@ -15,13 +15,17 @@ import org.elasticsearch.common.ssl.PemUtils;
 import org.elasticsearch.test.ESTestCase;
 import org.junit.rules.ExternalResource;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.SecureRandom;
 import java.security.cert.Certificate;
 import java.util.List;
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.SSLContext;
+import java.util.Objects;
 
 import static org.elasticsearch.test.ESTestCase.assertThat;
 import static org.hamcrest.Matchers.hasSize;
@@ -47,24 +51,41 @@ public class AzureHttpFixture extends ExternalResource {
     protected void before() throws Exception {
         if (enabled) {
             this.server = HttpsServer.create(new InetSocketAddress(InetAddress.getLoopbackAddress(), 0), 0);
-            final var certificates = PemUtils.readCertificates(
-                List.of(ESTestCase.getResourceDataPath(getClass(), "azure-http-fixture.pem"))
-            );
-            assertThat(certificates, hasSize(1));
-            final SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(
-                new KeyManager[] {
-                    KeyStoreUtil.createKeyManager(
-                        new Certificate[] { certificates.get(0) },
-                        PemUtils.readPrivateKey(ESTestCase.getResourceDataPath(getClass(), "azure-http-fixture.key"), () -> null),
-                        null
-                    ) },
-                null,
-                new SecureRandom()
-            );
-            server.setHttpsConfigurator(new HttpsConfigurator(sslContext));
+
+            try {
+                final var tmpdir = ESTestCase.createTempDir();
+                final var certificates = PemUtils.readCertificates(List.of(copyResource(tmpdir, "azure-http-fixture.pem")));
+                assertThat(certificates, hasSize(1));
+                final SSLContext sslContext = SSLContext.getInstance("TLS");
+                sslContext.init(
+                    new KeyManager[] {
+                        KeyStoreUtil.createKeyManager(
+                            new Certificate[] { certificates.get(0) },
+                            PemUtils.readPrivateKey(copyResource(tmpdir, "azure-http-fixture.key"), () -> null),
+                            null
+                        ) },
+                    null,
+                    new SecureRandom()
+                );
+                server.setHttpsConfigurator(new HttpsConfigurator(sslContext));
+            } catch (Exception e) {
+                throw new AssertionError("unexpected", e);
+            }
             server.createContext("/" + account, new AzureHttpHandler(account, container));
             server.start();
+        }
+    }
+
+    private Path copyResource(Path tmpdir, String name) throws IOException {
+        try (
+            var stream = Objects.requireNonNullElseGet(
+                getClass().getResourceAsStream(name),
+                () -> ESTestCase.fail(null, "resource [%s] not found", name)
+            )
+        ) {
+            final var path = tmpdir.resolve(name);
+            Files.write(path, stream.readAllBytes());
+            return path;
         }
     }
 

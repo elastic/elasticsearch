aa0163991ebe7e5e5c10fcebca04ce219c6d7ac4
Lorenzo Dematté <lorenzo.dematte@elastic.co>
2024-07-24 14:23:17 +0200

    Abstract codec lookup by name, to make CodecService extensible (#111007)

--- server/src/main/java/org/elasticsearch/index/engine/EngineConfig.java
+++ server/src/main/java/org/elasticsearch/index/engine/EngineConfig.java
@@ -23,7 +23,7 @@ import org.elasticsearch.common.unit.MemorySizeValue;
 import org.elasticsearch.core.Nullable;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.IndexSettings;
-import org.elasticsearch.index.codec.CodecService;
+import org.elasticsearch.index.codec.CodecProvider;
 import org.elasticsearch.index.mapper.MapperService;
 import org.elasticsearch.index.seqno.RetentionLeases;
 import org.elasticsearch.index.shard.ShardId;
@@ -60,7 +60,7 @@ public final class EngineConfig {
     private final MergePolicy mergePolicy;
     private final Analyzer analyzer;
     private final Similarity similarity;
-    private final CodecService codecService;
+    private final CodecProvider codecProvider;
     private final Engine.EventListener eventListener;
     private final QueryCache queryCache;
     private final QueryCachingPolicy queryCachingPolicy;
@@ -148,7 +148,7 @@ public final class EngineConfig {
         MergePolicy mergePolicy,
         Analyzer analyzer,
         Similarity similarity,
-        CodecService codecService,
+        CodecProvider codecProvider,
         Engine.EventListener eventListener,
         QueryCache queryCache,
         QueryCachingPolicy queryCachingPolicy,
@@ -176,7 +176,7 @@ public final class EngineConfig {
         this.mergePolicy = mergePolicy;
         this.analyzer = analyzer;
         this.similarity = similarity;
-        this.codecService = codecService;
+        this.codecProvider = codecProvider;
         this.eventListener = eventListener;
         codecName = indexSettings.getValue(INDEX_CODEC_SETTING);
         this.mapperService = mapperService;
@@ -252,14 +252,22 @@ public final class EngineConfig {
      * </p>
      */
     public Codec getCodec() {
-        return codecService.codec(codecName);
+        return codecProvider.codec(codecName);
     }
 
     /**
-     * @return the {@link CodecService}
+     * @return the {@link CodecProvider}
      */
-    public CodecService getCodecService() {
-        return codecService;
+    public CodecProvider getCodecProvider() {
+        return codecProvider;
+    }
+
+    /**
+     * @return the {@link CodecProvider}
+     */
+    @Deprecated // to avoid breaking serverless, just temporary
+    public CodecProvider getCodecService() {
+        return codecProvider;
     }
 
     /**

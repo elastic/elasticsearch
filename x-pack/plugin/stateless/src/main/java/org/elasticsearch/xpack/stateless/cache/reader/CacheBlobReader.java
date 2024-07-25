d9d255afa99ad7537e88348e14064a6a2376a144
Artem Prigoda <artem.prigoda@elastic.co>
2024-07-25 21:37:03 +0200

    Fetch VBCC chunks asynchronously from the indexing node (#2325)
    Support asynchronously fetching data in CacheBlobReader#getRangeInputStream. IndexingShardCacheBlobReader is async by fetching data using VBCC chunks, so make sure that the callback is propagated to async RangeMissingHandlers in SharedBlobCacheWarmingService and SearchIndexInput. Support for async RangeMissingHandler is implemented in elastic/elasticsearch#110587. ObjectStoreCacheBlobReader is still sync since we can't read blobs asynchronously.
    
    Depends on elastic/elasticsearch#110587 and #2404
    
    Resolves

--- x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/cache/reader/CacheBlobReader.java
+++ x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/cache/reader/CacheBlobReader.java
@@ -19,9 +19,9 @@
 
 package co.elastic.elasticsearch.stateless.cache.reader;
 
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.blobcache.common.ByteRange;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -55,8 +55,7 @@ public interface CacheBlobReader {
      *
      * @param position the position of the blob to fetch data from
      * @param length the length to read from the blob starting from position
-     * @return the input stream to fetch the data from
+     * @param listener listener for the input stream to fetch the data from
      */
-    InputStream getRangeInputStream(long position, int length) throws IOException;
-
+    void getRangeInputStream(long position, int length, ActionListener<InputStream> listener);
 }

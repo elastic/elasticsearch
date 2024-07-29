9687fb793a1cc3192a49abb7463af9f8f9db34c7
Adam Demjen <demjened@gmail.com>
2024-07-29 16:19:23 -0400

    Use constructor that exists

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/rank/textsimilarity/TextSimilarityTestPlugin.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/rank/textsimilarity/TextSimilarityTestPlugin.java
@@ -45,6 +45,7 @@ import org.elasticsearch.xpack.core.inference.action.GetInferenceModelAction;
 import org.elasticsearch.xpack.core.inference.action.InferenceAction;
 import org.elasticsearch.xpack.core.inference.results.RankedDocsResults;
 import org.elasticsearch.xpack.inference.services.cohere.CohereService;
+import org.elasticsearch.xpack.inference.services.cohere.CohereServiceSettings;
 import org.elasticsearch.xpack.inference.services.cohere.rerank.CohereRerankServiceSettings;
 import org.elasticsearch.xpack.inference.services.cohere.rerank.CohereRerankTaskSettings;
 
@@ -145,7 +146,7 @@ public class TextSimilarityTestPlugin extends Plugin implements ActionPlugin {
                         request.getInferenceEntityId(),
                         request.getTaskType(),
                         CohereService.NAME,
-                        new CohereRerankServiceSettings("uri", "model", null),
+                        new CohereRerankServiceSettings(new CohereServiceSettings()),
                         topN == null ? new EmptyTaskSettings() : new CohereRerankTaskSettings(topN, null, null)
                     )
                 )

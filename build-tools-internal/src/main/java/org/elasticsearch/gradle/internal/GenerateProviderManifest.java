a038a72df0191f25e91fb8b103dd154f08ee0e76
Rene Groeschke <rene@elastic.co>
2024-08-05 15:52:13 +0200

    Fix potential bug in EmbeddedProviderPlugin
    Ocassionally we see a LISTING.TXT not included in the jar

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/GenerateProviderManifest.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/GenerateProviderManifest.java
@@ -39,6 +39,7 @@ abstract class GenerateProviderManifest extends DefaultTask {
         File manifestFile = getManifestFile().get().getAsFile();
         manifestFile.getParentFile().mkdirs();
         FileUtils.write(manifestFile, generateManifestContent(), "UTF-8");
+        System.out.println("manifestFile = " + manifestFile + " exists = " + manifestFile.exists());
     }
 
     private String generateManifestContent() {

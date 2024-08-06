efb682fea8760ed6fd79fe8b3a2905ba558b8744
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2024-08-06 11:49:09 +0200

    remove dead code

--- x-pack/plugin/ml/qa/native-multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/ml/integration/DetectionRulesIT.java
+++ x-pack/plugin/ml/qa/native-multi-node-tests/src/javaRestTest/java/org/elasticsearch/xpack/ml/integration/DetectionRulesIT.java
@@ -326,7 +326,6 @@ public class DetectionRulesIT extends MlNativeAutodetectIntegTestCase {
         Job.Builder job = new Job.Builder("detection-rules-it-test-force-time-shift");
         job.setAnalysisConfig(analysisConfig);
         job.setDataDescription(dataDescription);
-        // job.setCustomSettings(Collections.singletonMap("keep_job_data", "true"));
 
         putJob(job);
         openJob(job.getId());

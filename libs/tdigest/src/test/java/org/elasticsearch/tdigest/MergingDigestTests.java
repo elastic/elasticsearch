555b7fd6cceef242fdb15026a2d7dcf71ecb0de0
Kostas Krikellas <kostas.krikellas@elastic.co>
2024-08-06 19:33:36 +0300

    spotless

--- libs/tdigest/src/test/java/org/elasticsearch/tdigest/MergingDigestTests.java
+++ libs/tdigest/src/test/java/org/elasticsearch/tdigest/MergingDigestTests.java
@@ -154,7 +154,7 @@ public class MergingDigestTests extends TDigestTests {
 
     public void testLargeInputSmallCompression() {
         TDigest td = TDigest.createMergingDigest(10);
-        for(int i = 0; i < 10_000_000; i++) {
+        for (int i = 0; i < 10_000_000; i++) {
             td.add(between(0, 3_600_000));
         }
     }

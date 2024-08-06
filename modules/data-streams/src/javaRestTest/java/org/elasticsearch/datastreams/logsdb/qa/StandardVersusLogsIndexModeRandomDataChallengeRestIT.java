aa1d2bc63b187305dbabe884c51578effe8c49de
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2024-08-06 13:48:54 -0700

    LogsDB qa tests - add specific matcher for source (#111568)

--- modules/data-streams/src/javaRestTest/java/org/elasticsearch/datastreams/logsdb/qa/StandardVersusLogsIndexModeRandomDataChallengeRestIT.java
+++ modules/data-streams/src/javaRestTest/java/org/elasticsearch/datastreams/logsdb/qa/StandardVersusLogsIndexModeRandomDataChallengeRestIT.java
@@ -23,7 +23,6 @@ import org.elasticsearch.xcontent.XContentFactory;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
-import java.util.function.Function;
 
 /**
  * Challenge test (see {@link StandardVersusLogsIndexModeChallengeRestIT}) that uses randomly generated
@@ -42,26 +41,7 @@ public class StandardVersusLogsIndexModeRandomDataChallengeRestIT extends Standa
             DataGeneratorSpecification.builder()
                 // Nested fields don't work with subobjects: false.
                 .withNestedFieldsLimit(0)
-                // TODO increase depth of objects
-                // Currently matching fails because in synthetic source all fields are flat (given that we have subobjects: false)
-                // but stored source is identical to original document which has nested structure.
-                .withMaxObjectDepth(0)
                 .withDataSourceHandlers(List.of(new DataSourceHandler() {
-                    // TODO enable null values
-                    // Matcher does not handle nulls currently
-                    @Override
-                    public DataSourceResponse.NullWrapper handle(DataSourceRequest.NullWrapper request) {
-                        return new DataSourceResponse.NullWrapper(Function.identity());
-                    }
-
-                    // TODO enable arrays
-                    // List matcher currently does not apply matching logic recursively
-                    // and equality check fails because arrays are sorted in synthetic source.
-                    @Override
-                    public DataSourceResponse.ArrayWrapper handle(DataSourceRequest.ArrayWrapper request) {
-                        return new DataSourceResponse.ArrayWrapper(Function.identity());
-                    }
-
                     // TODO enable scaled_float fields
                     // There a difference in synthetic source (precision loss)
                     // specific to this fields which matcher can't handle.

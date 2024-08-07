e9017ab57503868575e2ba0007096e62ec794e60
Nhat Nguyen <nhat.nguyen@elastic.co>
2024-08-07 16:08:57 -0700

    Fix transport version race (#111696)
    Two new transport versions have been introduced simultaneously.

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -186,8 +186,7 @@ public class TransportVersions {
     public static final TransportVersion REGISTER_SLM_STATS = def(8_716_00_0);
     public static final TransportVersion ESQL_NESTED_UNSUPPORTED = def(8_717_00_0);
     public static final TransportVersion ESQL_SINGLE_VALUE_QUERY_SOURCE = def(8_718_00_0);
-
-    public static final TransportVersion ESQL_ORIGINAL_INDICES = def(8_718_00_0);
+    public static final TransportVersion ESQL_ORIGINAL_INDICES = def(8_719_00_0);
     /*
      * STOP! READ THIS FIRST! No, really,
      *        ____ _____ ___  ____  _        ____  _____    _    ____    _____ _   _ ___ ____    _____ ___ ____  ____ _____ _

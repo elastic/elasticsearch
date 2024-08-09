7a7a794434fee1b47096dfca69184aa7a341450f
Tim Vernum <tim@adjective.org>
2024-08-09 19:05:47 +1000

    Implement Writeable on ProjectId (#1602)
    We need to write `ProjectId` instances to streams. This should be done
    by treating it as a `Writeable` object rather than making consumers deal
    with the internal fields.

--- server/src/main/java/org/elasticsearch/cluster/metadata/ProjectId.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/ProjectId.java
@@ -8,6 +8,22 @@
 
 package org.elasticsearch.cluster.metadata;
 
-public record ProjectId(String id) {
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.io.stream.Writeable;
 
+import java.io.IOException;
+
+public record ProjectId(String id) implements Writeable {
+
+    public static final Reader<ProjectId> READER = ProjectId::new;
+
+    public ProjectId(StreamInput in) throws IOException {
+        this(in.readString());
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        out.writeString(id);
+    }
 }

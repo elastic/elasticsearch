ccad9e9cd761b2778388d622e6a5f43086cb578d
Patrick Doyle <810052+prdoyle@users.noreply.github.com>
2024-08-12 12:54:15 -0400

    Import Inject annotation from new location (#2542)
    * Import Inject annotation from new location
    
    * Bump elasticsearch

--- x-pack/plugin/stateless/src/main/java/co/elastic/elasticsearch/stateless/lucene/stats/GetShardSizeAction.java
+++ x-pack/plugin/stateless/src/main/java/co/elastic/elasticsearch/stateless/lucene/stats/GetShardSizeAction.java
@@ -25,13 +25,13 @@ import org.elasticsearch.action.ActionType;
 import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.TransportAction;
 import org.elasticsearch.cluster.service.ClusterService;
-import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.core.Nullable;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.indices.IndicesService;
+import org.elasticsearch.injection.guice.Inject;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.transport.TransportService;
 

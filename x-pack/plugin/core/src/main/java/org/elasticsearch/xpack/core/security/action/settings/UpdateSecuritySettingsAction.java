93e052e98d0579912782a5aa21439f0001982d11
Athena Brown <athena.brown@elastic.co>
2024-08-12 15:03:35 -0600

    Add tier preference to security index settings allowlist
    This commit allows tier preference for the security system indices to be set using the Security Settings API.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/settings/UpdateSecuritySettingsAction.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/action/settings/UpdateSecuritySettingsAction.java
@@ -14,6 +14,7 @@ import org.elasticsearch.action.ValidateActions;
 import org.elasticsearch.action.support.master.AcknowledgedRequest;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.cluster.routing.allocation.DataTier;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.util.set.Sets;
@@ -44,7 +45,8 @@ public class UpdateSecuritySettingsAction {
 
     public static final Set<String> ALLOWED_SETTING_KEYS = Set.of(
         IndexMetadata.SETTING_NUMBER_OF_REPLICAS,
-        IndexMetadata.SETTING_AUTO_EXPAND_REPLICAS
+        IndexMetadata.SETTING_AUTO_EXPAND_REPLICAS,
+        DataTier.TIER_PREFERENCE
     );
 
     private UpdateSecuritySettingsAction() {/* no instances */}

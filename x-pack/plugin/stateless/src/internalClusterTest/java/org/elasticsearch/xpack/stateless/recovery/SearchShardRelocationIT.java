5d7d76ac33a9426b891f3c37cd4354dda1abcbf8
Yang Wang <yang.wang@elastic.co>
2024-08-12 16:01:45 +1000

    [Test] Faster master election to ensure index recovery in time (#2480)
    By default, master election waits for 30 seconds for heartbeat from
    previous master to expire. This runs very close to the 30 seconds
    timeout of ensureGreen. This PR reduce the heartbeat frequency so that
    master election is guaranteed to complete in time for index recovery.
    
    Resolves: #2479

--- x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/recovery/SearchShardRelocationIT.java
+++ x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/recovery/SearchShardRelocationIT.java
@@ -19,15 +19,26 @@ package co.elastic.elasticsearch.stateless.recovery;
 
 import co.elastic.elasticsearch.stateless.AbstractStatelessIntegTestCase;
 
+import org.elasticsearch.cluster.coordination.stateless.StoreHeartbeatService;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.core.TimeValue;
+
+import static org.elasticsearch.cluster.coordination.stateless.StoreHeartbeatService.HEARTBEAT_FREQUENCY;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.not;
 
 public class SearchShardRelocationIT extends AbstractStatelessIntegTestCase {
 
     public void testSearchShardRelocationWhenHostingNodeTerminates() throws Exception {
-        var indexNode = startMasterAndIndexNode();
-        var searchNodeA = startSearchNode();
-        var searchNodeB = startSearchNode();
+        // MAX_MISSED_HEARTBEATS x HEARTBEAT_FREQUENCY is how long it takes for the last master heartbeat to expire.
+        // Speed up the time to master election after master restart.
+        final Settings nodeSettings = Settings.builder()
+            .put(HEARTBEAT_FREQUENCY.getKey(), TimeValue.timeValueSeconds(1))
+            .put(StoreHeartbeatService.MAX_MISSED_HEARTBEATS.getKey(), 2)
+            .build();
+        var indexNode = startMasterAndIndexNode(nodeSettings);
+        var searchNodeA = startSearchNode(nodeSettings);
+        var searchNodeB = startSearchNode(nodeSettings);
 
         var indexName = randomIdentifier();
         createIndex(indexName, indexSettings(1, 1).build());

d3758e88c247830206dace018ab884d547ca6f20
Ankita Kumar <ankita.kumar@elastic.co>
2024-08-13 13:26:54 -0400

    addComment

---
+++ modules/reindex/src/main/java/org/elasticsearch/reindex/ReindexMetrics.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.reindex;
+
+import org.elasticsearch.telemetry.metric.LongHistogram;
+import org.elasticsearch.telemetry.metric.MeterRegistry;
+
+public class ReindexMetrics {
+
+    public static final String TOOK_TIME_HISTOGRAM = "es.reindex.took_time.histogram";
+
+    private final LongHistogram reindexTimeSecsHistogram;
+
+    private ReindexMetrics(LongHistogram reindexTimeSecsHistogram) {
+        this.reindexTimeSecsHistogram = reindexTimeSecsHistogram;
+    }
+
+    public ReindexMetrics(MeterRegistry meterRegistry) {
+        this(meterRegistry.registerLongHistogram(TOOK_TIME_HISTOGRAM, "Time to reindex by search", "seconds"));
+    }
+
+    public long recordTookTime(long tookTime) {
+        reindexTimeSecsHistogram.record(tookTime);
+        return tookTime;
+    }
+}

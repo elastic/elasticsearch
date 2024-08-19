ff8b72d57695d3f50b289b0c2c805f534f5264cc
Alexander Spies <alexander.spies@elastic.co>
2024-08-19 15:16:30 +0200

    Spotless

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/PhysicalPlanOptimizer.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/PhysicalPlanOptimizer.java
@@ -124,7 +124,9 @@ public class PhysicalPlanOptimizer extends ParameterizedRuleExecutor<PhysicalPla
                 } else {
                     AttributeSet childOutput = currentPlanNode.inputSet();
                     AttributeSet addedAttributes = currentPlanNode.outputSet().subtract(childOutput);
-                    requiredAttributes.set(requiredAttributes.get().subtract(addedAttributes).combine(currentPlanNode.childrenReferences()));
+                    requiredAttributes.set(
+                        requiredAttributes.get().subtract(addedAttributes).combine(currentPlanNode.childrenReferences())
+                    );
                 }
                 return currentPlanNode;
             });

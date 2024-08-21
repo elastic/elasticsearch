470f15e651f675382a9154bd94610d4744188c88
gmarouli <mary.gouseti@elastic.co>
2024-08-21 09:10:34 +0300

    Enhance DataStreamGlobalRetentionIT

--- modules/data-streams/src/javaRestTest/java/org/elasticsearch/datastreams/lifecycle/DataStreamGlobalRetentionIT.java
+++ modules/data-streams/src/javaRestTest/java/org/elasticsearch/datastreams/lifecycle/DataStreamGlobalRetentionIT.java
@@ -9,6 +9,7 @@ package org.elasticsearch.datastreams.lifecycle;
 
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
+import org.elasticsearch.client.WarningFailureException;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.datastreams.DisabledSecurityDataStreamTestCase;
 import org.junit.After;
@@ -19,6 +20,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 
@@ -60,6 +62,50 @@ public class DataStreamGlobalRetentionIT extends DisabledSecurityDataStreamTestC
     }
 
     @SuppressWarnings("unchecked")
+    public void testDataStreamRetention() throws Exception {
+        // Set global retention and add retention to the data stream
+        {
+            updateClusterSettings(
+                Settings.builder()
+                    .put("data_streams.lifecycle.retention.default", "7d")
+                    .put("data_streams.lifecycle.retention.default", "90d")
+                    .build()
+            );
+            Request request = new Request("PUT", "_data_stream/my-data-stream/_lifecycle");
+            request.setJsonEntity("""
+                {
+                  "data_retention": "10s"
+                }""");
+            assertAcknowledged(client().performRequest(request));
+        }
+
+        // Verify that the effective retention matches the default retention
+        {
+            Request request = new Request("GET", "/_data_stream/my-data-stream");
+            Response response = client().performRequest(request);
+            List<Object> dataStreams = (List<Object>) entityAsMap(response).get("data_streams");
+            assertThat(dataStreams.size(), is(1));
+            Map<String, Object> dataStream = (Map<String, Object>) dataStreams.get(0);
+            assertThat(dataStream.get("name"), is("my-data-stream"));
+            Map<String, Object> lifecycle = (Map<String, Object>) dataStream.get("lifecycle");
+            assertThat(lifecycle.get("effective_retention"), is("10s"));
+            assertThat(lifecycle.get("retention_determined_by"), is("data_stream_configuration"));
+            assertThat(lifecycle.get("data_retention"), is("10s"));
+        }
+
+        // Verify that the first generation index was removed
+        assertBusy(() -> {
+            Response response = client().performRequest(new Request("GET", "/_data_stream/my-data-stream"));
+            Map<String, Object> dataStream = ((List<Map<String, Object>>) entityAsMap(response).get("data_streams")).get(0);
+            assertThat(dataStream.get("name"), is("my-data-stream"));
+            List<Object> backingIndices = (List<Object>) dataStream.get("indices");
+            assertThat(backingIndices.size(), is(1));
+            // 2 backing indices created + 1 for the deleted index
+            assertThat(dataStream.get("generation"), is(3));
+        }, 20, TimeUnit.SECONDS);
+    }
+
+    @SuppressWarnings("unchecked")
     public void testDefaultRetention() throws Exception {
         // Set default global retention
         updateClusterSettings(Settings.builder().put("data_streams.lifecycle.retention.default", "10s").build());
@@ -96,12 +142,20 @@ public class DataStreamGlobalRetentionIT extends DisabledSecurityDataStreamTestC
         updateClusterSettings(Settings.builder().put("data_streams.lifecycle.retention.max", "10s").build());
         boolean withDataStreamLevelRetention = randomBoolean();
         if (withDataStreamLevelRetention) {
-            Request request = new Request("PUT", "_data_stream/my-data-stream/_lifecycle");
-            request.setJsonEntity("""
-                {
-                  "data_retention": "30d"
-                }""");
-            assertAcknowledged(client().performRequest(request));
+            try {
+                Request request = new Request("PUT", "_data_stream/my-data-stream/_lifecycle");
+                request.setJsonEntity("""
+                    {
+                      "data_retention": "30d"
+                    }""");
+                assertAcknowledged(client().performRequest(request));
+                fail("Should have returned a warning about data retention exceeding the max retention");
+            } catch (WarningFailureException warningFailureException) {
+                assertThat(
+                    warningFailureException.getMessage(),
+                    containsString("The retention provided [30d] is exceeding the max allowed data retention of this project [10s]")
+                );
+            }
         }
 
         // Verify that the effective retention matches the max retention

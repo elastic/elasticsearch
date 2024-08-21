c524aa4d85f099125fa001045e95e997d2032856
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2024-08-21 08:46:40 -0700

    Fix calculation of parent offset for ignored source in some cases (#112046)

--- server/src/main/java/org/elasticsearch/index/mapper/DocumentParser.java
+++ server/src/main/java/org/elasticsearch/index/mapper/DocumentParser.java
@@ -268,7 +268,7 @@ public final class DocumentParser {
                 context.addIgnoredField(
                     new IgnoredSourceFieldMapper.NameValue(
                         context.parent().fullPath(),
-                        context.parent().fullPath().indexOf(currentFieldName),
+                        context.parent().fullPath().lastIndexOf(currentFieldName),
                         XContentDataHelper.encodeToken(parser),
                         context.doc()
                     )
@@ -295,7 +295,7 @@ public final class DocumentParser {
                 context.addIgnoredField(
                     new IgnoredSourceFieldMapper.NameValue(
                         context.parent().fullPath(),
-                        context.parent().fullPath().indexOf(context.parent().leafName()),
+                        context.parent().fullPath().lastIndexOf(context.parent().leafName()),
                         XContentDataHelper.encodeXContentBuilder(tuple.v2()),
                         context.doc()
                     )

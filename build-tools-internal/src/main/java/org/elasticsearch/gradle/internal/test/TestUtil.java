7528bd2f902bda6f73691d60ad7ac989c551618d
Ryan Ernst <ryan@iernst.net>
2024-08-22 13:13:31 -0700

    only in ide

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/test/TestUtil.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/test/TestUtil.java
@@ -21,6 +21,6 @@ public class TestUtil {
         String platform = String.format(Locale.ROOT, "%s-%s", ElasticsearchDistribution.CURRENT_PLATFORM, arch);
         String existingLibraryPath = System.getProperty("java.library.path");
 
-        return String.format(Locale.ROOT, "\"%s/%s%c%s\"", nativeLibsDir, platform, File.pathSeparatorChar, existingLibraryPath);
+        return String.format(Locale.ROOT, "%s/%s%c%s", nativeLibsDir, platform, File.pathSeparatorChar, existingLibraryPath);
     }
 }

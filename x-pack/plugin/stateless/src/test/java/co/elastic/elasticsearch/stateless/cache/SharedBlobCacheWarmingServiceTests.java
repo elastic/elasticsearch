dcde8091adace49a4900689c6ac545825f2f6e32
Yang Wang <yang.wang@elastic.co>
2024-08-23 10:17:46 +1000

    Improve test for warming with minimized range (#2565)
    Refactor the test to Largely reduce the mock usage and verbosity.
    
    See also
    https://github.com/elastic/elasticsearch-serverless/pull/2474#discussion_r1721379763

--- x-pack/plugin/stateless/src/test/java/co/elastic/elasticsearch/stateless/cache/SharedBlobCacheWarmingServiceTests.java
+++ x-pack/plugin/stateless/src/test/java/co/elastic/elasticsearch/stateless/cache/SharedBlobCacheWarmingServiceTests.java
@@ -26,34 +26,40 @@ import co.elastic.elasticsearch.stateless.lucene.FileCacheKey;
 import co.elastic.elasticsearch.stateless.lucene.StatelessCommitRef;
 import co.elastic.elasticsearch.stateless.test.FakeStatelessNode;
 
-import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.blobcache.common.ByteRange;
 import org.elasticsearch.blobcache.shared.SharedBlobCacheService;
 import org.elasticsearch.cluster.routing.ShardRoutingState;
 import org.elasticsearch.cluster.routing.TestShardRouting;
+import org.elasticsearch.common.blobstore.BlobContainer;
+import org.elasticsearch.common.blobstore.BlobPath;
+import org.elasticsearch.common.blobstore.OperationPurpose;
+import org.elasticsearch.common.blobstore.support.FilterBlobContainer;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.env.NodeEnvironment;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.index.shard.IndexShardState;
-import org.elasticsearch.telemetry.TelemetryProvider;
 import org.elasticsearch.test.ESTestCase;
-import org.mockito.ArgumentCaptor;
+import org.elasticsearch.threadpool.ThreadPool;
 import org.mockito.Mockito;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.Map;
 import java.util.Set;
 
+import static org.elasticsearch.test.ActionListenerUtils.anyActionListener;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -155,12 +161,123 @@ public class SharedBlobCacheWarmingServiceTests extends ESTestCase {
         }
     }
 
-    @SuppressWarnings("unchecked")
-    public void testMinimizeRange() throws Exception {
+    public void testMinimizedRange() throws Exception {
         final long rangeSize = ByteSizeValue.ofMb(between(1, 16)).getBytes();
         final long stepSize = rangeSize / 4;
+        try (var node = createFakeNodeForMinimisingRange(rangeSize, stepSize)) {
+            final IndexShard indexShard = mockIndexShard(node);
+            final var termAndGen = new PrimaryTermAndGeneration(randomNonNegativeLong(), randomLongBetween(3, 42));
+            // Test file located on each quarter
+            for (int i = 1; i <= 4; i++) {
+                Mockito.clearInvocations(node.sharedCacheService);
+                node.sharedCacheService.forceEvict(ignore -> true);
+                final int region = between(1, 10);
+                final long fileLength = randomLongBetween(1, 100);
+                final long rangeStart = region * rangeSize;
+                final long offset = randomLongBetween(stepSize * (i - 1), stepSize * i - fileLength) + rangeStart;
+                final long minimizedEnd = rangeStart + stepSize * i;
+                final var blobLocation = new BlobLocation(
+                    new BlobFile(StatelessCompoundCommit.blobNameFromGeneration(termAndGen.generation()), termAndGen),
+                    offset,
+                    fileLength
+                );
+                final var commit = new StatelessCompoundCommit(
+                    node.shardId,
+                    termAndGen.generation(),
+                    termAndGen.primaryTerm(),
+                    node.node.getEphemeralId(),
+                    Map.of("file", blobLocation),
+                    0,
+                    Set.of()
+                );
 
-        try (var node = new FakeStatelessNode(this::newEnvironment, this::newNodeEnvironment, xContentRegistry()) {
+                // Warm the cache and verify the range is fetched with minimization as expected
+                final PlainActionFuture<Void> future = new PlainActionFuture<>();
+                node.warmingService.warmCache(
+                    randomAlphaOfLength(10),
+                    indexShard,
+                    commit,
+                    node.indexingDirectory.getBlobStoreCacheDirectory(),
+                    future
+                );
+                safeGet(future);
+                verify(node.sharedCacheService).maybeFetchRange(
+                    any(),
+                    eq(region),
+                    eq(ByteRange.of(rangeStart, minimizedEnd)),
+                    anyLong(),
+                    any(),
+                    any(),
+                    anyActionListener()
+                );
+
+                // Data should be available in the cache
+                final var cacheFile = node.sharedCacheService.getCacheFile(
+                    new FileCacheKey(node.shardId, blobLocation.primaryTerm(), blobLocation.blobName()),
+                    minimizedEnd
+                );
+                assertTrue(cacheFile.tryRead(ByteBuffer.allocate(Math.toIntExact(minimizedEnd - rangeStart)), rangeStart));
+            }
+        }
+    }
+
+    public void testWarmFirstRegionInFullWithMinimizedRange() throws Exception {
+        final long rangeSize = ByteSizeValue.ofMb(between(1, 16)).getBytes();
+        final long stepSize = rangeSize / 4;
+        try (var node = createFakeNodeForMinimisingRange(rangeSize, stepSize)) {
+            final IndexShard indexShard = mockIndexShard(node);
+            final var termAndGen = new PrimaryTermAndGeneration(randomNonNegativeLong(), randomLongBetween(3, 42));
+
+            // Always fully warm up region 0
+            final int region = 0;
+            final long fileLength = randomLongBetween(1, 100);
+            final long offset = randomLongBetween(0, rangeSize - fileLength);
+            final var blobLocation = new BlobLocation(
+                new BlobFile(StatelessCompoundCommit.blobNameFromGeneration(termAndGen.generation()), termAndGen),
+                offset,
+                fileLength
+            );
+            final var commit = new StatelessCompoundCommit(
+                node.shardId,
+                termAndGen.generation(),
+                termAndGen.primaryTerm(),
+                node.node.getEphemeralId(),
+                Map.of("file", blobLocation),
+                0,
+                Set.of()
+            );
+
+            // Warm the cache and verify the range is fetched with minimization as expected
+            final PlainActionFuture<Void> future = new PlainActionFuture<>();
+            node.warmingService.warmCache(
+                randomAlphaOfLength(10),
+                indexShard,
+                commit,
+                node.indexingDirectory.getBlobStoreCacheDirectory(),
+                future
+            );
+            safeGet(future);
+            verify(node.sharedCacheService).maybeFetchRange(
+                any(),
+                eq(region),
+                eq(ByteRange.of(0, rangeSize)),
+                anyLong(),
+                any(),
+                any(),
+                anyActionListener()
+            );
+
+            // Data should be available in the cache
+            final var cacheFile = node.sharedCacheService.getCacheFile(
+                new FileCacheKey(node.shardId, blobLocation.primaryTerm(), blobLocation.blobName()),
+                rangeSize
+            );
+            assertTrue(cacheFile.tryRead(ByteBuffer.allocate(Math.toIntExact(rangeSize)), 0));
+        }
+    }
+
+    private FakeStatelessNode createFakeNodeForMinimisingRange(long rangeSize, long stepSize) throws IOException {
+        return new FakeStatelessNode(this::newEnvironment, this::newNodeEnvironment, xContentRegistry()) {
             @Override
             protected Settings nodeSettings() {
                 Settings settings = super.nodeSettings();
@@ -172,121 +289,54 @@ public class SharedBlobCacheWarmingServiceTests extends ESTestCase {
                     .put(SharedBlobCacheWarmingService.PREWARMING_RANGE_MINIMIZATION_STEP.getKey(), ByteSizeValue.ofBytes(stepSize))
                     .build();
             }
-        }) {
-            final var cacheService = mock(StatelessSharedBlobCacheService.class);
-            when(cacheService.getRangeSize()).thenReturn(Math.toIntExact(rangeSize));
-            when(cacheService.getRegionSize()).thenReturn(Math.toIntExact(rangeSize));
-            final IndexShard indexShard = mock(IndexShard.class);
-            when(indexShard.shardId()).thenReturn(node.shardId);
-            when(indexShard.store()).thenReturn(node.indexingStore);
-            when(indexShard.state()).thenReturn(IndexShardState.RECOVERING);
-            when(indexShard.routingEntry()).thenReturn(
-                TestShardRouting.newShardRouting(node.shardId, node.node.getId(), true, ShardRoutingState.INITIALIZING)
-            );
 
-            final var warmingService = new SharedBlobCacheWarmingService(
-                cacheService,
-                node.threadPool,
-                TelemetryProvider.NOOP,
-                Settings.builder()
-                    .put(SharedBlobCacheService.SHARED_CACHE_RANGE_SIZE_SETTING.getKey(), ByteSizeValue.ofBytes(rangeSize))
-                    .put(SharedBlobCacheService.SHARED_CACHE_REGION_SIZE_SETTING.getKey(), ByteSizeValue.ofBytes(rangeSize))
-                    .put(SharedBlobCacheWarmingService.PREWARMING_RANGE_MINIMIZATION_STEP.getKey(), ByteSizeValue.ofBytes(stepSize))
-                    .build()
-            );
-
-            final var termAndGen = new PrimaryTermAndGeneration(randomNonNegativeLong(), randomLongBetween(3, 42));
-            // Test file located on each quarter
-            for (int i = 1; i <= 4; i++) {
-                Mockito.clearInvocations(cacheService);
-                final int region = between(1, 10);
-                final long fileLength = randomLongBetween(1, 100);
-                final long rangeStart = region * rangeSize;
-                final long offset = randomLongBetween(stepSize * (i - 1), stepSize * i - fileLength) + rangeStart;
-                final long minimizedEnd = rangeStart + stepSize * i;
-                final var commit = new StatelessCompoundCommit(
-                    node.shardId,
-                    termAndGen.generation(),
-                    termAndGen.primaryTerm(),
-                    node.node.getEphemeralId(),
-                    Map.of(
-                        "file",
-                        new BlobLocation(
-                            new BlobFile(StatelessCompoundCommit.blobNameFromGeneration(termAndGen.generation()), termAndGen),
-                            offset,
-                            fileLength
-                        )
-                    ),
-                    0,
-                    Set.of()
-                );
-
-                warmingService.warmCacheForShardRecovery(
-                    randomAlphaOfLength(10),
-                    indexShard,
-                    commit,
-                    node.indexingDirectory.getBlobStoreCacheDirectory()
-                );
-                final ArgumentCaptor<ActionListener<Boolean>> listener = ArgumentCaptor.forClass(ActionListener.class);
-
-                assertBusy(() -> {
-                    verify(cacheService).maybeFetchRange(
-                        any(),
-                        eq(region),
-                        eq(ByteRange.of(rangeStart, minimizedEnd)),
-                        anyLong(),
-                        any(),
-                        any(),
-                        listener.capture()
-                    );
-                    listener.getValue().onResponse(true);
-                });
+            @Override
+            protected StatelessSharedBlobCacheService createCacheService(
+                NodeEnvironment nodeEnvironment,
+                Settings settings,
+                ThreadPool threadPool
+            ) {
+                return spy(super.createCacheService(nodeEnvironment, settings, threadPool));
             }
 
-            {
-                // Always fully warm up region 0
-                Mockito.clearInvocations(cacheService);
-                final int region = 0;
-                final long fileLength = randomLongBetween(1, 100);
-                final long offset = randomLongBetween(0, rangeSize - fileLength);
-                final var commit = new StatelessCompoundCommit(
-                    node.shardId,
-                    termAndGen.generation(),
-                    termAndGen.primaryTerm(),
-                    node.node.getEphemeralId(),
-                    Map.of(
-                        "file",
-                        new BlobLocation(
-                            new BlobFile(StatelessCompoundCommit.blobNameFromGeneration(termAndGen.generation()), termAndGen),
-                            offset,
-                            fileLength
-                        )
-                    ),
-                    0,
-                    Set.of()
-                );
-                warmingService.warmCacheForShardRecovery(
-                    randomAlphaOfLength(10),
-                    indexShard,
-                    commit,
-                    node.indexingDirectory.getBlobStoreCacheDirectory()
-                );
-                final ArgumentCaptor<ActionListener<Boolean>> listener = ArgumentCaptor.forClass(ActionListener.class);
+            @Override
+            public BlobContainer wrapBlobContainer(BlobPath path, BlobContainer innerContainer) {
+                return new FilterBlobContainer(innerContainer) {
+                    @Override
+                    protected BlobContainer wrapChild(BlobContainer child) {
+                        return child;
+                    }
 
-                assertBusy(() -> {
-                    verify(cacheService).maybeFetchRange(
-                        any(),
-                        eq(region),
-                        eq(ByteRange.of(0, rangeSize)),
-                        anyLong(),
-                        any(),
-                        any(),
-                        listener.capture()
-                    );
-                    listener.getValue().onResponse(true);
-                });
+                    @Override
+                    public InputStream readBlob(OperationPurpose purpose, String blobName, long position, long length) throws IOException {
+                        return new InputStream() {
+                            private long remaining = length;
+
+                            @Override
+                            public int read() throws IOException {
+                                if (remaining == 0) {
+                                    return -1;
+                                } else {
+                                    remaining -= 1;
+                                    return 1;
+                                }
+                            }
+                        };
+                    }
+                };
             }
-        }
+        };
+    }
+
+    private static IndexShard mockIndexShard(FakeStatelessNode node) {
+        final IndexShard indexShard = mock(IndexShard.class);
+        when(indexShard.shardId()).thenReturn(node.shardId);
+        when(indexShard.store()).thenReturn(node.indexingStore);
+        when(indexShard.state()).thenReturn(IndexShardState.RECOVERING);
+        when(indexShard.routingEntry()).thenReturn(
+            TestShardRouting.newShardRouting(node.shardId, node.node.getId(), true, ShardRoutingState.INITIALIZING)
+        );
+        return indexShard;
     }
 
     private FakeStatelessNode createFakeNode(long primaryTerm) throws IOException {

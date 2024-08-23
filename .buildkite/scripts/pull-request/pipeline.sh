1b577b8ea80b99b869204443884399917129d46d
Brian Seeders <brian.seeders@elastic.co>
2024-08-23 16:39:07 -0400

    Skip PR pipeline

--- .buildkite/scripts/pull-request/pipeline.sh
+++ .buildkite/scripts/pull-request/pipeline.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 echo --- Installing bun
 npm install -g bun@1.0.4
 
-echo --- Generating pipeline
-bun .buildkite/scripts/pull-request/pipeline.generate.ts
+# echo --- Generating pipeline
+# bun .buildkite/scripts/pull-request/pipeline.generate.ts

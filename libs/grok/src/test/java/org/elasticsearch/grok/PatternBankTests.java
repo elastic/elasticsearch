1b3b4aa3739263ecc81f4bfc6fb456fd2a19590b
Keith Massey <keith.massey@elastic.co>
2024-08-23 17:23:32 -0500

    Adding random tests

--- libs/grok/src/test/java/org/elasticsearch/grok/PatternBankTests.java
+++ libs/grok/src/test/java/org/elasticsearch/grok/PatternBankTests.java
@@ -11,10 +11,13 @@ package org.elasticsearch.grok;
 import org.elasticsearch.test.ESTestCase;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 import static org.elasticsearch.test.ESTestCase.randomBoolean;
+import static org.hamcrest.Matchers.containsString;
 
 public class PatternBankTests extends ESTestCase {
 
@@ -191,5 +194,71 @@ public class PatternBankTests extends ESTestCase {
         }
         patternBankMap.put("FOO" + (nodeCount - 1), "foo");
         new PatternBank(patternBankMap);
+    }
+
+    public void testRandomBanksWithoutCycles() {
+        /*
+         * This creates a large number of pattens, each of which refers to a large number of patterns. But there are no cycles in any of
+         * these since each pattern only references patterns with a higher ID. We don't expect any exceptions here.
+         */
+        Map<String, String> patternBankMap = randomBoolean() ? new HashMap<>() : new LinkedHashMap<>();
+        final int nodeCount = 500;
+        for (int i = 0; i < nodeCount - 1; i++) {
+            StringBuilder patternBuilder = new StringBuilder();
+            for (int j = 0; j < randomIntBetween(0, 20); j++) {
+                patternBuilder.append("%{FOO-" + randomIntBetween(i + 1, nodeCount - 1) + "}");
+            }
+            patternBankMap.put("FOO-" + i, patternBuilder.toString());
+        }
+        patternBankMap.put("FOO-" + (nodeCount - 1), "foo");
+        new PatternBank(patternBankMap);
+    }
+
+    public void testRandomBanksWithCycles() {
+        /*
+         * This creates a large number of pattens, each of which refers to a large number of patterns. We have at least one cycle because
+         * we pick a node at random, and make sure that a node that it links (or one of its descendants) to links back. If no descendant
+         * links back to it, we create an artificial cycle at the end.
+         */
+        Map<String, String> patternBankMap = new LinkedHashMap<>();
+        final int nodeCount = 500;
+        int nodeToHaveCycle = randomIntBetween(0, nodeCount);
+        int nodeToPotentiallyCreateCycle = -1;
+        boolean haveCreatedCycle = false;
+        for (int i = 0; i < nodeCount - 1; i++) {
+            StringBuilder patternBuilder = new StringBuilder();
+            int numberOfLinkedPatterns = randomIntBetween(1, 20);
+            int nodeToLinkBackIndex = randomIntBetween(0, numberOfLinkedPatterns);
+            Set<Integer> childNodes = new HashSet<>();
+            for (int j = 0; j < numberOfLinkedPatterns; j++) {
+                int childNode = randomIntBetween(i + 1, nodeCount - 1);
+                childNodes.add(childNode);
+                patternBuilder.append("%{FOO-" + childNode + "}");
+                if (i == nodeToHaveCycle) {
+                    if (nodeToLinkBackIndex == j) {
+                        nodeToPotentiallyCreateCycle = childNode;
+                    }
+                }
+            }
+            if (i == nodeToPotentiallyCreateCycle) {
+                // We either create the cycle here, or randomly pick a child node to maybe create the cycle
+                if (randomBoolean()) {
+                    patternBuilder.append("%{FOO-" + nodeToHaveCycle + "}");
+                    haveCreatedCycle = true;
+                } else {
+                    nodeToPotentiallyCreateCycle = randomFrom(childNodes);
+                }
+            }
+            patternBankMap.put("FOO-" + i, patternBuilder.toString());
+        }
+        if (haveCreatedCycle) {
+            patternBankMap.put("FOO-" + (nodeCount - 1), "foo");
+        } else {
+            // We didn't randomly create a cycle, so just force one in this last pattern
+            nodeToHaveCycle = nodeCount - 1;
+            patternBankMap.put("FOO-" + nodeToHaveCycle, "%{FOO-" + nodeToHaveCycle + "}");
+        }
+        IllegalArgumentException e = expectThrows(IllegalArgumentException.class, () -> new PatternBank(patternBankMap));
+        assertThat(e.getMessage(), containsString("FOO-" + nodeToHaveCycle));
     }
 }

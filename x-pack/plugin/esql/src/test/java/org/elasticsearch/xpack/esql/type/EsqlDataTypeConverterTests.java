f3cefb8076d9085842694dcd69518652efb76e0b
Fang Xing <fang.xing@elastic.co>
2024-08-26 14:45:40 -0400

    combine 3 commonTypes into one

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/type/EsqlDataTypeConverterTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/type/EsqlDataTypeConverterTests.java
@@ -9,6 +9,19 @@ package org.elasticsearch.xpack.esql.type;
 
 import org.elasticsearch.test.ESTestCase;
 
+import static org.elasticsearch.xpack.esql.core.type.DataType.BOOLEAN;
+import static org.elasticsearch.xpack.esql.core.type.DataType.BYTE;
+import static org.elasticsearch.xpack.esql.core.type.DataType.DOUBLE;
+import static org.elasticsearch.xpack.esql.core.type.DataType.FLOAT;
+import static org.elasticsearch.xpack.esql.core.type.DataType.INTEGER;
+import static org.elasticsearch.xpack.esql.core.type.DataType.KEYWORD;
+import static org.elasticsearch.xpack.esql.core.type.DataType.LONG;
+import static org.elasticsearch.xpack.esql.core.type.DataType.NULL;
+import static org.elasticsearch.xpack.esql.core.type.DataType.SHORT;
+import static org.elasticsearch.xpack.esql.core.type.DataType.TEXT;
+import static org.elasticsearch.xpack.esql.core.type.DataType.UNSIGNED_LONG;
+import static org.elasticsearch.xpack.esql.type.EsqlDataTypeConverter.commonType;
+
 public class EsqlDataTypeConverterTests extends ESTestCase {
 
     public void testNanoTimeToString() {
@@ -16,4 +29,24 @@ public class EsqlDataTypeConverterTests extends ESTestCase {
         long actual = EsqlDataTypeConverter.dateNanosToLong(EsqlDataTypeConverter.nanoTimeToString(expected));
         assertEquals(expected, actual);
     }
+
+    public void testCommonType() {
+        assertEquals(BOOLEAN, commonType(BOOLEAN, NULL));
+        assertEquals(BOOLEAN, commonType(NULL, BOOLEAN));
+        assertEquals(BOOLEAN, commonType(BOOLEAN, BOOLEAN));
+        assertEquals(NULL, commonType(NULL, NULL));
+        assertEquals(INTEGER, commonType(INTEGER, KEYWORD));
+        assertEquals(LONG, commonType(TEXT, LONG));
+        assertEquals(SHORT, commonType(SHORT, BYTE));
+        assertEquals(FLOAT, commonType(BYTE, FLOAT));
+        assertEquals(FLOAT, commonType(FLOAT, INTEGER));
+        assertEquals(UNSIGNED_LONG, commonType(UNSIGNED_LONG, LONG));
+        assertEquals(DOUBLE, commonType(DOUBLE, FLOAT));
+        assertEquals(FLOAT, commonType(FLOAT, UNSIGNED_LONG));
+
+        // strings
+        assertEquals(TEXT, commonType(TEXT, KEYWORD));
+        assertEquals(TEXT, commonType(KEYWORD, TEXT));
+    }
+
 }

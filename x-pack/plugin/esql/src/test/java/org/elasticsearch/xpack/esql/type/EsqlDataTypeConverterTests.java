0b0ac507b40c56785d22a4bb17efe01891fde867
Fang Xing <fang.xing@elastic.co>
2024-08-26 19:12:46 -0400

    clean up

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/type/EsqlDataTypeConverterTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/type/EsqlDataTypeConverterTests.java
@@ -35,8 +35,8 @@ public class EsqlDataTypeConverterTests extends ESTestCase {
         assertEquals(BOOLEAN, commonType(NULL, BOOLEAN));
         assertEquals(BOOLEAN, commonType(BOOLEAN, BOOLEAN));
         assertEquals(NULL, commonType(NULL, NULL));
-        assertEquals(null, commonType(INTEGER, KEYWORD));
-        assertEquals(null, commonType(TEXT, LONG));
+        assertNull(commonType(INTEGER, KEYWORD));
+        assertNull(commonType(TEXT, LONG));
         assertEquals(SHORT, commonType(SHORT, BYTE));
         assertEquals(FLOAT, commonType(BYTE, FLOAT));
         assertEquals(FLOAT, commonType(FLOAT, INTEGER));

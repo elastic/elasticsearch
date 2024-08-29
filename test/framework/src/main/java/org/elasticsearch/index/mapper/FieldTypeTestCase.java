dea12a01c5e0def42558d414523c25863c212e73
Luca Cavanna <javanna@apache.org>
2024-08-29 18:08:35 +0200

    Fix FieldTypeTestCase compile error around FieldInfo creation

--- test/framework/src/main/java/org/elasticsearch/index/mapper/FieldTypeTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/index/mapper/FieldTypeTestCase.java
@@ -138,6 +138,7 @@ public abstract class FieldTypeTestCase extends ESTestCase {
             randomBoolean(),
             IndexOptions.NONE,
             DocValuesType.NONE,
+            randomBoolean(),
             -1,
             new HashMap<>(),
             1,

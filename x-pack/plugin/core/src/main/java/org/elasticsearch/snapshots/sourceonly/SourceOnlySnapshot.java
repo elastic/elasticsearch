78bfea4d61fc67ceb9b42a7ef8c5e9f3f3a7e6d0
Luca Cavanna <javanna@apache.org>
2024-08-29 17:56:25 +0200

    fix compile error in SourceOnlySnapshot around FieldInfo creation

--- x-pack/plugin/core/src/main/java/org/elasticsearch/snapshots/sourceonly/SourceOnlySnapshot.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/snapshots/sourceonly/SourceOnlySnapshot.java
@@ -254,6 +254,7 @@ public class SourceOnlySnapshot {
                             false,
                             IndexOptions.NONE,
                             DocValuesType.NONE,
+                            fieldInfo.hasDocValuesSkipIndex(),
                             -1,
                             fieldInfo.attributes(),
                             0,

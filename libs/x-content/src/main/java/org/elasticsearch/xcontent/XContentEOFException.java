6c6e6144c93906321c0a6faa507d1e136973b27d
Christoph Büscher <christophbuescher@posteo.de>
2024-09-04 21:54:57 +0200

    moa checkstyle

--- libs/x-content/src/main/java/org/elasticsearch/xcontent/XContentEOFException.java
+++ libs/x-content/src/main/java/org/elasticsearch/xcontent/XContentEOFException.java
@@ -8,8 +8,6 @@
 
 package org.elasticsearch.xcontent;
 
-import java.io.IOException;
-
 public class XContentEOFException extends XContentParseException {
 
     public XContentEOFException(XContentLocation location, String message, Exception cause) {

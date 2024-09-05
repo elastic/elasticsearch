2651324fb6bae264f8d102b3f58109176f4b4f66
Saikat Sarkar <saikat.sarkar@elastic.co>
2024-09-05 12:35:49 -0600

    Fix the issue with long line

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/ibmwatsonx/embeddings/IbmWatsonxEmbeddingsServiceSettings.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/ibmwatsonx/embeddings/IbmWatsonxEmbeddingsServiceSettings.java
@@ -50,7 +50,8 @@ public class IbmWatsonxEmbeddingsServiceSettings extends FilteredXContentObject
     public static final String NAME = "ibmwatsonx_embeddings_service_settings";
 
     /**
-     * Rate limits are defined at <a href="https://www.ibm.com/docs/en/watsonx/saas?topic=learning-watson-machine-plans">Watson Machine Learning plans and compute usage</a>.
+     * Rate limits are defined at
+     * <a href="https://www.ibm.com/docs/en/watsonx/saas?topic=learning-watson-machine-plans">Watson Machine Learning plans</a>.
      * For Lite plan, you've 120 requests per minute.
      */
     private static final RateLimitSettings DEFAULT_RATE_LIMIT_SETTINGS = new RateLimitSettings(120);

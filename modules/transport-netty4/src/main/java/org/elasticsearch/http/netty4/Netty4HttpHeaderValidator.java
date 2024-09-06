c0767ee2d1223871ed2563773ae26ffddc61480a
Tim Brooks <tim@timbrooks.org>
2024-09-06 11:36:45 -0600

    Even more downstream respect

--- modules/transport-netty4/src/main/java/org/elasticsearch/http/netty4/Netty4HttpHeaderValidator.java
+++ modules/transport-netty4/src/main/java/org/elasticsearch/http/netty4/Netty4HttpHeaderValidator.java
@@ -60,7 +60,7 @@ public class Netty4HttpHeaderValidator extends ChannelInboundHandlerAdapter {
                 pending.add(ReferenceCountUtil.retain(httpObject));
                 requestStart(ctx);
                 assert state == QUEUEING_DATA;
-                ctx.channel().config().setAutoRead(false);
+                assert ctx.channel().config().isAutoRead() == false;
                 break;
             case QUEUEING_DATA:
                 pending.add(ReferenceCountUtil.retain(httpObject));
@@ -105,6 +105,7 @@ public class Netty4HttpHeaderValidator extends ChannelInboundHandlerAdapter {
         }
 
         state = QUEUEING_DATA;
+        ctx.channel().config().setAutoRead(false);
 
         if (httpRequest == null) {
             // this looks like a malformed request and will forward without validation
@@ -150,6 +151,7 @@ public class Netty4HttpHeaderValidator extends ChannelInboundHandlerAdapter {
         assert ctx.channel().config().isAutoRead() == false;
         assert state == QUEUEING_DATA;
 
+        ctx.channel().config().setAutoRead(true);
         boolean fullRequestForwarded = forwardData(ctx, pending);
 
         assert fullRequestForwarded || pending.isEmpty();
@@ -161,7 +163,6 @@ public class Netty4HttpHeaderValidator extends ChannelInboundHandlerAdapter {
         }
 
         assert state == WAITING_TO_START || state == QUEUEING_DATA || state == FORWARDING_DATA_UNTIL_NEXT_REQUEST;
-        ctx.channel().config().setAutoRead(state != QUEUEING_DATA);
     }
 
     private void forwardRequestWithDecoderExceptionAndNoContent(ChannelHandlerContext ctx, Exception e) {
@@ -177,6 +178,8 @@ public class Netty4HttpHeaderValidator extends ChannelInboundHandlerAdapter {
             messageToForward = toReplace.replace(Unpooled.EMPTY_BUFFER);
         }
         messageToForward.setDecoderResult(DecoderResult.failure(e));
+
+        ctx.channel().config().setAutoRead(true);
         ctx.fireChannelRead(messageToForward);
 
         assert fullRequestDropped || pending.isEmpty();
@@ -188,7 +191,6 @@ public class Netty4HttpHeaderValidator extends ChannelInboundHandlerAdapter {
         }
 
         assert state == WAITING_TO_START || state == QUEUEING_DATA || state == DROPPING_DATA_UNTIL_NEXT_REQUEST;
-        ctx.channel().config().setAutoRead(state != QUEUEING_DATA);
     }
 
     @Override

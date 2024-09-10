09b68e2689108016bd9967b0aefe75ee084d664a
Christoph Büscher <christophbuescher@posteo.de>
2024-09-10 18:37:39 +0200

    Mute CompositeRolesStoreTests.testXPackUserCanAccessNonRestrictedIndices

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authz/store/CompositeRolesStoreTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authz/store/CompositeRolesStoreTests.java
@@ -2736,6 +2736,7 @@ public class CompositeRolesStoreTests extends ESTestCase {
         assertThat(CompositeRolesStore.tryGetRoleDescriptorForInternalUser(subject).get().getClusterPrivileges(), emptyArray());
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/112453")
     public void testXPackUserCanAccessNonRestrictedIndices() {
         for (String action : Arrays.asList(
             TransportGetAction.TYPE.name(),

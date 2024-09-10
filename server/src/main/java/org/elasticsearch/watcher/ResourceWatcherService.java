e65160c0e03367d7f389907f21708d29543953b8
Athena Brown <athena.brown@elastic.co>
2024-09-10 15:01:37 -0600

    spotless

--- server/src/main/java/org/elasticsearch/watcher/ResourceWatcherService.java
+++ server/src/main/java/org/elasticsearch/watcher/ResourceWatcherService.java
@@ -99,8 +99,8 @@ public class ResourceWatcherService implements Closeable {
             executor = new Scheduler.SafeScheduledThreadPoolExecutor(1, (runnable, exec) -> {
                 if (exec.isShutdown() == false) {
                     logger.error(
-                        "failed to schedule monitoring file on executor [{}], updates to settings files on disk may require " +
-                            "restarting this process to be applied",
+                        "failed to schedule monitoring file on executor [{}], updates to settings files on disk may require "
+                            + "restarting this process to be applied",
                         exec.toString()
                     );
                     assert false;

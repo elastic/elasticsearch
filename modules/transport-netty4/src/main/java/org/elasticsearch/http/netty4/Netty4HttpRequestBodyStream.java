3bb476010a02cb896316cadd4c9dd13ca532a732
Mikhail Berezovskiy <mikhail@elastic.co>
2024-09-09 20:40:18 -0700

    replace stream queue with composite buffer (#112685)

--- modules/transport-netty4/src/main/java/org/elasticsearch/http/netty4/Netty4HttpRequestBodyStream.java
+++ modules/transport-netty4/src/main/java/org/elasticsearch/http/netty4/Netty4HttpRequestBodyStream.java
@@ -9,6 +9,8 @@
 
 package org.elasticsearch.http.netty4;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.CompositeByteBuf;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.LastHttpContent;
@@ -17,25 +19,21 @@ import io.netty.util.concurrent.FutureListener;
 import org.elasticsearch.http.HttpBody;
 import org.elasticsearch.transport.netty4.Netty4Utils;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 /**
  * Netty based implementation of {@link HttpBody.Stream}.
  * This implementation utilize {@link io.netty.channel.ChannelConfig#setAutoRead(boolean)}
  * to prevent entire payload buffering. But sometimes upstream can send few chunks of data despite
- * autoRead=off. In this case chunks will be queued until downstream calls {@link Stream#next()}
+ * autoRead=off. In this case chunks will be buffered until downstream calls {@link Stream#next()}
  */
 public class Netty4HttpRequestBodyStream implements HttpBody.Stream {
 
     private final Channel channel;
-    private final Queue<HttpContent> chunkQueue = new ArrayDeque<>();
-    private boolean requested = false;
+    private final FutureListener<Void> closeListener = future -> doClose();
+    private ByteBuf buf;
     private boolean hasLast = false;
+    private boolean requested = false;
     private boolean closing = false;
     private HttpBody.ChunkHandler handler;
-
-    private final FutureListener<Void> closeListener = future -> doClose();
 
     public Netty4HttpRequestBodyStream(Channel channel) {
         this.channel = channel;
@@ -53,38 +51,49 @@ public class Netty4HttpRequestBodyStream implements HttpBody.Stream {
         this.handler = chunkHandler;
     }
 
-    private void sendQueuedOrRead() {
-        assert channel.eventLoop().inEventLoop();
-        requested = true;
-        var chunk = chunkQueue.poll();
-        if (chunk == null) {
-            channel.read();
-        } else {
-            sendChunk(chunk);
-        }
-    }
-
     @Override
     public void next() {
+        assert closing == false : "cannot request next chunk on closing stream";
         assert handler != null : "handler must be set before requesting next chunk";
-        if (channel.eventLoop().inEventLoop()) {
-            sendQueuedOrRead();
-        } else {
-            channel.eventLoop().submit(this::sendQueuedOrRead);
-        }
+        channel.eventLoop().submit(() -> {
+            requested = true;
+            if (buf == null) {
+                channel.read();
+            } else {
+                send();
+            }
+        });
     }
 
     public void handleNettyContent(HttpContent httpContent) {
+        assert hasLast == false : "receive http content on completed stream";
         hasLast = httpContent instanceof LastHttpContent;
         if (closing) {
             httpContent.release();
-            return;
-        }
-        assert handler != null : "handler must be set before processing http content";
-        if (requested && chunkQueue.isEmpty()) {
-            sendChunk(httpContent);
         } else {
-            chunkQueue.add(httpContent);
+            addChunk(httpContent.content());
+            if (requested) {
+                send();
+            }
+        }
+        if (hasLast) {
+            channel.config().setAutoRead(true);
+            channel.closeFuture().removeListener(closeListener);
+        }
+    }
+
+    // adds chunk to current buffer, will allocate composite buffer when need to hold more than 1 chunk
+    private void addChunk(ByteBuf chunk) {
+        assert chunk != null;
+        if (buf == null) {
+            buf = chunk;
+        } else if (buf instanceof CompositeByteBuf comp) {
+            comp.addComponent(true, chunk);
+        } else {
+            var comp = channel.alloc().compositeBuffer();
+            comp.addComponent(true, buf);
+            comp.addComponent(true, chunk);
+            buf = comp;
         }
     }
 
@@ -94,8 +103,8 @@ public class Netty4HttpRequestBodyStream implements HttpBody.Stream {
     }
 
     // visible for test
-    Queue<HttpContent> chunkQueue() {
-        return chunkQueue;
+    ByteBuf buf() {
+        return buf;
     }
 
     // visible for test
@@ -103,22 +112,13 @@ public class Netty4HttpRequestBodyStream implements HttpBody.Stream {
         return hasLast;
     }
 
-    private void sendChunk(HttpContent httpContent) {
+    private void send() {
         assert requested;
+        assert handler != null : "must set handler before receiving next chunk";
+        var bytesRef = Netty4Utils.toReleasableBytesReference(buf);
         requested = false;
-        var bytesRef = Netty4Utils.toReleasableBytesReference(httpContent.content());
-        var isLast = httpContent instanceof LastHttpContent;
-        handler.onNext(bytesRef, isLast);
-        if (isLast) {
-            channel.config().setAutoRead(true);
-            channel.closeFuture().removeListener(closeListener);
-        }
-    }
-
-    private void releaseQueuedChunks() {
-        while (chunkQueue.isEmpty() == false) {
-            chunkQueue.poll().release();
-        }
+        buf = null;
+        handler.onNext(bytesRef, hasLast);
     }
 
     @Override
@@ -132,7 +132,10 @@ public class Netty4HttpRequestBodyStream implements HttpBody.Stream {
 
     private void doClose() {
         closing = true;
-        releaseQueuedChunks();
+        if (buf != null) {
+            buf.release();
+            buf = null;
+        }
         channel.config().setAutoRead(true);
     }
 }

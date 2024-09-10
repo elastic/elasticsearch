99964189066bbd0b06e9fddb087c27420145ce14
Nick Tindall <nick.tindall@elastic.co>
2024-09-10 14:00:43 +1000

    Improve message

--- x-pack/plugin/searchable-snapshots/src/internalClusterTest/java/org/elasticsearch/xpack/searchablesnapshots/cache/shared/NodesCachesStatsIntegTests.java
+++ x-pack/plugin/searchable-snapshots/src/internalClusterTest/java/org/elasticsearch/xpack/searchablesnapshots/cache/shared/NodesCachesStatsIntegTests.java
@@ -136,8 +136,8 @@ public class NodesCachesStatsIntegTests extends BaseFrozenSearchableSnapshotsInt
             .collect(toSet())
             .toArray(String[]::new);
 
-        // We've seen `getWrites` inexplicably return zero. `assertBusy` to eliminate the possibility of it being due
-        // to contention on the `LongAdder` in SharedBlobCacheService.
+        // We've seen `getWrites` inexplicably return zero. `assertBusy` to test the theory of it being due
+        // to contention on the `LongAdder` at `SharedBlobCacheService#writeCount`.
         assertBusy(() -> {
             final NodesCachesStatsResponse response = client().execute(
                 TransportSearchableSnapshotsNodeCachesStatsAction.TYPE,

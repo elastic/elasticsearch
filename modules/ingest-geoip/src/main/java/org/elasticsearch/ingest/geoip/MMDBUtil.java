564495289f4fa46afa25b580add9387e6d2dcdfa
Joe Gallo <joegallo@gmail.com>
2024-09-13 16:12:17 -0400

    Extract a constant for the buffer size

--- modules/ingest-geoip/src/main/java/org/elasticsearch/ingest/geoip/MMDBUtil.java
+++ modules/ingest-geoip/src/main/java/org/elasticsearch/ingest/geoip/MMDBUtil.java
@@ -22,6 +22,10 @@ public final class MMDBUtil {
 
     private static final byte[] DATABASE_TYPE_MARKER = "database_type".getBytes(StandardCharsets.UTF_8);
 
+    // note: technically the metadata can be up to 128k long, but we only handle it correctly as long as it's less than
+    // or equal to this buffer size.
+    private static final int BUFFER_SIZE = 512;
+
     /**
      * Read the database type from the database. We do this manually instead of relying on the built-in mechanism to avoid reading the
      * entire database into memory merely to read the type. This is especially important to maintain on master nodes where pipelines are
@@ -33,24 +37,24 @@ public final class MMDBUtil {
      */
     public static String getDatabaseType(Path databasePath) throws IOException {
         final long fileSize = Files.size(databasePath);
-        if (fileSize <= 512) {
+        if (fileSize <= BUFFER_SIZE) {
             throw new IOException("unexpected file length [" + fileSize + "] for [" + databasePath + "]");
         }
         try (InputStream in = Files.newInputStream(databasePath)) {
-            // read the last 512 bytes
-            final long skipped = in.skip(fileSize - 512);
-            if (skipped != fileSize - 512) {
-                throw new IOException("failed to skip [" + (fileSize - 512) + "] bytes while reading [" + databasePath + "]");
+            // read the last BUFFER_SIZE bytes
+            final long skipped = in.skip(fileSize - BUFFER_SIZE);
+            if (skipped != fileSize - BUFFER_SIZE) {
+                throw new IOException("failed to skip [" + (fileSize - BUFFER_SIZE) + "] bytes while reading [" + databasePath + "]");
             }
-            final byte[] tail = new byte[512];
+            final byte[] tail = new byte[BUFFER_SIZE];
             int read = 0;
             do {
-                final int actualBytesRead = in.read(tail, read, 512 - read);
+                final int actualBytesRead = in.read(tail, read, BUFFER_SIZE - read);
                 if (actualBytesRead == -1) {
                     throw new IOException("unexpected end of stream [" + databasePath + "] after reading [" + read + "] bytes");
                 }
                 read += actualBytesRead;
-            } while (read != 512);
+            } while (read != BUFFER_SIZE);
 
             // find the database_type header
             int metadataOffset = -1;

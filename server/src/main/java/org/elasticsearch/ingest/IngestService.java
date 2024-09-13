c7dd111d13e7383d71609eae0e0073abf1cae2f6
Keith Massey <keith.massey@elastic.co>
2024-09-13 09:17:11 -0500

    fixing behavior when component template overrides remove pipeline config

--- server/src/main/java/org/elasticsearch/ingest/IngestService.java
+++ server/src/main/java/org/elasticsearch/ingest/IngestService.java
@@ -287,29 +287,32 @@ public class IngestService implements ClusterStateApplier, ReportingService<Inge
         final IndexRequest indexRequest,
         final Metadata metadata,
         final long epochMillis,
-        final Map<String, ComponentTemplate> templateSubstitutions
+        final Map<String, ComponentTemplate> componentTemplateSubstitutions
     ) {
         if (indexRequest.isPipelineResolved()) {
             return;
         }
 
-        String requestPipeline = indexRequest.getPipeline();
-
-        Optional<Pipelines> pipelinesFromMetadata = resolvePipelinesFromMetadata(originalRequest, indexRequest, metadata, epochMillis);
-        Optional<Pipelines> pipelinesFromTemplates;
-        if (templateSubstitutions.isEmpty() == false || pipelinesFromMetadata.isEmpty()) {
-            pipelinesFromTemplates = resolvePipelinesFromIndexTemplates(indexRequest, metadata, templateSubstitutions);
+        /*
+         * Here we look for the pipelines associated with the index if the index exists. If the index does not exist we fall back to using
+         * templates to find the pipelines. But if a user has passed in component template substitutions, they want the settings from those
+         * used in place of the settings used to create any previous indices. So in that case we use the templates to find the pipelines --
+         * we don't fall back to the existing index if we don't find any because it is possible the user has intentionally removed the
+         * pipeline.
+         */
+        final Pipelines pipelines;
+        if (componentTemplateSubstitutions.isEmpty()) {
+            pipelines = resolvePipelinesFromMetadata(originalRequest, indexRequest, metadata, epochMillis) //
+                .or(() -> resolvePipelinesFromIndexTemplates(indexRequest, metadata, Map.of()))
+                .orElse(Pipelines.NO_PIPELINES_DEFINED);
         } else {
-            pipelinesFromTemplates = Optional.empty();
-        }
-        Pipelines pipelines;
-        if (templateSubstitutions.isEmpty() == false) {
-            pipelines = pipelinesFromTemplates.or(() -> pipelinesFromMetadata).orElse(Pipelines.NO_PIPELINES_DEFINED);
-        } else {
-            pipelines = pipelinesFromMetadata.or(() -> pipelinesFromTemplates).orElse(Pipelines.NO_PIPELINES_DEFINED);
+            pipelines = resolvePipelinesFromIndexTemplates(indexRequest, metadata, componentTemplateSubstitutions).orElse(
+                Pipelines.NO_PIPELINES_DEFINED
+            );
         }
 
         // The pipeline coming as part of the request always has priority over the resolved one from metadata or templates
+        String requestPipeline = indexRequest.getPipeline();
         if (requestPipeline != null) {
             indexRequest.setPipeline(requestPipeline);
         } else {

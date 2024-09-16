933db90fb125e9c37d4bfdb47b52b127a9bce4a1
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2024-09-16 10:41:29 +0200

    Fork Get Privileges

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/action/privilege/TransportGetPrivilegesAction.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/action/privilege/TransportGetPrivilegesAction.java
@@ -7,6 +7,7 @@
 package org.elasticsearch.xpack.security.action.privilege;
 
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.ActionRunnable;
 import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.HandledTransportAction;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
@@ -23,6 +24,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.Executor;
 
 import static org.elasticsearch.common.Strings.isNullOrEmpty;
 
@@ -32,6 +34,7 @@ import static org.elasticsearch.common.Strings.isNullOrEmpty;
 public class TransportGetPrivilegesAction extends HandledTransportAction<GetPrivilegesRequest, GetPrivilegesResponse> {
 
     private final NativePrivilegeStore privilegeStore;
+    private final Executor genericExecutor;
 
     @Inject
     public TransportGetPrivilegesAction(
@@ -39,12 +42,19 @@ public class TransportGetPrivilegesAction extends HandledTransportAction<GetPriv
         NativePrivilegeStore privilegeStore,
         TransportService transportService
     ) {
+        // TODO replace DIRECT_EXECUTOR_SERVICE when removing workaround for https://github.com/elastic/elasticsearch/issues/97916
         super(GetPrivilegesAction.NAME, transportService, actionFilters, GetPrivilegesRequest::new, EsExecutors.DIRECT_EXECUTOR_SERVICE);
         this.privilegeStore = privilegeStore;
+        this.genericExecutor = transportService.getThreadPool().generic();
     }
 
     @Override
     protected void doExecute(Task task, final GetPrivilegesRequest request, final ActionListener<GetPrivilegesResponse> listener) {
+        // workaround for https://github.com/elastic/elasticsearch/issues/97916 - TODO remove this when we can
+        genericExecutor.execute(ActionRunnable.wrap(listener, l -> doExecuteForked(task, request, l)));
+    }
+
+    private void doExecuteForked(Task task, final GetPrivilegesRequest request, final ActionListener<GetPrivilegesResponse> listener) {
         final Set<String> names;
         if (request.privileges() == null || request.privileges().length == 0) {
             names = null;

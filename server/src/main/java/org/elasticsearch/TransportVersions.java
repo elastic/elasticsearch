f6ec7060b61e035886dfd87a0df78f1082ef5088
Dan Rubinstein <daniel.rubinstein@elastic.co>
2024-09-17 09:44:42 -0400

    Adding skip_results, skip_model_update, and force_time_shift to ML calendar ScheduledEvent (#112837)
    * Adding skip_results, skip_model_update, and force_time_shift to ML calendar ScheduledEvent
    
    * Updating skip_results to skip_result in ScheduledEvent
    
    * Increasing MLMetaIndex version and updating typing of new ScheduledEvent values in index

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -214,6 +214,7 @@ public class TransportVersions {
     public static final TransportVersion ESQL_AGGREGATE_EXEC_TRACKS_INTERMEDIATE_ATTRS = def(8_738_00_0);
     public static final TransportVersion CCS_TELEMETRY_STATS = def(8_739_00_0);
     public static final TransportVersion GLOBAL_RETENTION_TELEMETRY = def(8_740_00_0);
+    public static final TransportVersion ML_SCHEDULED_EVENT_TIME_SHIFT_CONFIGURATION = def(8_742_00_0);
     public static final TransportVersion SIMULATE_COMPONENT_TEMPLATES_SUBSTITUTIONS = def(8_743_00_0);
 
     /*

1bb7c4d7bf986f601f32de5c0dfeefc11deaacd8
Jim Ferenczi <jim.ferenczi@elastic.co>
2024-09-17 17:46:38 +0100

    fix license

--- server/src/main/java/org/elasticsearch/index/mapper/PatchSourceUtils.java
+++ server/src/main/java/org/elasticsearch/index/mapper/PatchSourceUtils.java
@@ -1,9 +1,10 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
 package org.elasticsearch.index.mapper;
@@ -69,14 +70,14 @@ public class PatchSourceUtils {
             case START_ARRAY -> {
                 destination.writeStartArray();
                 while (parser.nextToken() != XContentParser.Token.END_ARRAY) {
-                    parseAndPatchSource(destination, parser, fullPath, patchFields, patchApply);
+                    parseAndPatchSource(destination, parser, fullPath, patchFullPaths, patchApply);
                 }
                 destination.writeEndArray();
             }
             case START_OBJECT -> {
                 destination.writeStartObject();
                 while (parser.nextToken() != XContentParser.Token.END_OBJECT) {
-                    parseAndPatchSource(destination, parser, fullPath, patchFields, patchApply);
+                    parseAndPatchSource(destination, parser, fullPath, patchFullPaths, patchApply);
                 }
                 destination.writeEndObject();
             }

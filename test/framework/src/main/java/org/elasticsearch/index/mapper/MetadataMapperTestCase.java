013ac60b55b12e9bc73de9a92c3459fa6493244a
Luca Cavanna <javanna@apache.org>
2024-09-18 10:22:10 +0200

    iter

--- test/framework/src/main/java/org/elasticsearch/index/mapper/MetadataMapperTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/index/mapper/MetadataMapperTestCase.java
@@ -11,7 +11,6 @@ package org.elasticsearch.index.mapper;
 
 import org.elasticsearch.common.compress.CompressedXContent;
 import org.elasticsearch.core.CheckedConsumer;
-import org.elasticsearch.core.UpdateForV9;
 import org.elasticsearch.index.IndexVersion;
 import org.elasticsearch.index.IndexVersions;
 import org.elasticsearch.index.mapper.MapperService.MergeReason;

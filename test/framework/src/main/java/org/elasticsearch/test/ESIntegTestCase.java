f286d97d0744adc278b66dca8d7d2d0f1c261528
David Turner <david.turner@elastic.co>
2024-09-19 10:13:56 +0100

    Avoid deleted docs in DiskThresholdDeciderIT

--- test/framework/src/main/java/org/elasticsearch/test/ESIntegTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/ESIntegTestCase.java
@@ -1600,7 +1600,7 @@ public abstract class ESIntegTestCase extends ESTestCase {
      */
     protected BroadcastResponse forceMerge() {
         waitForRelocation();
-        BroadcastResponse actionGet = indicesAdmin().prepareForceMerge().setMaxNumSegments(1).get();
+        BroadcastResponse actionGet = indicesAdmin().prepareForceMerge().setMaxNumSegments(1).setFlush(true).get();
         assertNoFailures(actionGet);
         return actionGet;
     }

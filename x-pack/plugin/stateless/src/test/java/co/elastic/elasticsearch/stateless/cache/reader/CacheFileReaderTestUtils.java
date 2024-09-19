1fc0cf78d9a2104021aca4c2285ae2b6de6e0506
Tanguy Leroux <tlrx.dev@gmail.com>
2024-09-19 09:44:29 +0200

    Add CacheFileReader as a layer between BlobCacheIndexInput and CacheFile/CacheBlobReader (#2798)
    This change introduces a new CacheFileReader as a thin layer 
    between BlobCacheIndexInput and cache accesses. This will 
    serve later for redirecting read operations on headers/footers 
    of file to a different cache file blob region. For now we just want 
    to ensure that everything works fine with this small change.
    
    Relates 
    Relates #2777

---
+++ x-pack/plugin/stateless/src/test/java/co/elastic/elasticsearch/stateless/cache/reader/CacheFileReaderTestUtils.java
@@ -0,0 +1,31 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ * Copyright Elasticsearch B.V. All rights reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch B.V. and its suppliers, if any.
+ * The intellectual and technical concepts contained herein
+ * are proprietary to Elasticsearch B.V. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright
+ * law.  Dissemination of this information or reproduction of
+ * this material is strictly forbidden unless prior written
+ * permission is obtained from Elasticsearch B.V.
+ */
+
+package co.elastic.elasticsearch.stateless.cache.reader;
+
+import co.elastic.elasticsearch.stateless.lucene.FileCacheKey;
+
+import org.elasticsearch.blobcache.shared.SharedBlobCacheService;
+
+public final class CacheFileReaderTestUtils {
+
+    private CacheFileReaderTestUtils() {}
+
+    public static SharedBlobCacheService<FileCacheKey>.CacheFile getCacheFile(CacheFileReader reader) {
+        return reader.getCacheFile();
+    }
+}

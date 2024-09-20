c0b6794f0ffe50534be763f06613045b18980214
ChrisHegarty <chegar999@gmail.com>
2024-09-20 17:04:18 +0100

    spotless

--- server/src/main/java/org/elasticsearch/index/store/Store.java
+++ server/src/main/java/org/elasticsearch/index/store/Store.java
@@ -28,7 +28,6 @@ import org.apache.lucene.store.BufferedChecksum;
 import org.apache.lucene.store.ByteArrayDataInput;
 import org.apache.lucene.store.ChecksumIndexInput;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FilterDirectory;
 import org.apache.lucene.store.IOContext;
 import org.apache.lucene.store.IndexInput;
 import org.apache.lucene.store.IndexOutput;

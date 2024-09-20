0155456193e4d24c7d1fe8e7ec22a111f00eff1d
Mary Gouseti <mary.gouseti@elastic.co>
2024-09-20 12:42:22 +0300

    Add failure store status in index response of data streams (#112816) (#113245)
    The failure store status is a flag that indicates how the failure store was used or could be used if enabled. The user can be informed about the usage of the failure store in the following way:
    
    When relevant we add the optional field `failure_store` . The field will be omitted when the use of the failure store is not relevant. For example, if a document was successfully indexed in a data stream, if a failure concerns an index or if the opType is not index or create. In more detail:
    - when we have a “success” create/index response, the field `failure_store` will not be present if the documented was indexed in a backing index. Otherwise, if it got stored in the failure store it will have the value `used`.
    - when we have a “rejected“ create/index response, meaning the document was not persisted in elasticsearch, we return the field `failure_store` which is either `not_enabled`, if the document could have ended up in the failure store if it was enabled, or `failed` if something went wrong and the document was not persisted in the failure store, for example, the cluster is out of space and in read-only mode.
    
    We chose to make it an optional field to reduce the impact of this field on a bulk response. The value will exist in the java object but it will not be returned to the user. The only values that will be displayed are:
    
    - `used`: meaning this document was indexed in the failure store
    - `not_enabled`: meaning this document was rejected but could have been stored in the failure store if it was applicable.
    - `failed`: meaning this failed document, failed to be stored in the failure store.
    
    Example:
    ```
    "errors": true,
      "took": 202,
      "items": [
        {
          "create": {
            "_index": ".fs-my-ds-2024.09.04-000002",
            "_id": "iRDDvJEB_J3Inuia2zgH",
            "_version": 1,
            "result": "created",
            "_shards": {
              "total": 2,
              "successful": 1,
              "failed": 0
            },
            "_seq_no": 6,
            "_primary_term": 1,
            "status": 201,
            "failure_store": "used"
          }
        },
        {
          "create": {
            "_index": "ds-no-fs",
            "_id": "hxDDvJEB_J3Inuia2jj3",
            "status": 400,
            "error": {
              "type": "document_parsing_exception",
              "reason": "[1:153] failed to parse field [count] of type [long] in document with id 'hxDDvJEB_J3Inuia2jj3'. Preview of field's value: 'bla'",
              "caused_by": {
                "type": "illegal_argument_exception",
                "reason": "For input string: \"bla\""
              }
            }
          },
          "failure_store": "not_enabled"
        },
        {
          "create": {
            "_index": ".ds-my-ds-2024.09.04-000001",
            "_id": "iBDDvJEB_J3Inuia2jj3",
            "_version": 1,
            "result": "created",
            "_shards": {
              "total": 2,
              "successful": 1,
              "failed": 0
            },
            "_seq_no": 7,
            "_primary_term": 1,
            "status": 201
          }
        }
      ]
    ```
    
    (cherry picked from commit f4f075a2cc9c25f3280a9f3d1a0eea81ce967105)

--- modules/data-streams/src/internalClusterTest/java/org/elasticsearch/datastreams/DataStreamIT.java
+++ modules/data-streams/src/internalClusterTest/java/org/elasticsearch/datastreams/DataStreamIT.java
@@ -1415,7 +1415,7 @@ public class DataStreamIT extends ESIntegTestCase {
 
         IndexRequest indexRequest = new IndexRequest(dataStreamName).opType("create").source("{}", XContentType.JSON);
         Exception e = expectThrows(Exception.class, client().index(indexRequest));
-        assertThat(e.getCause().getMessage(), equalTo("data stream timestamp field [@timestamp] is missing"));
+        assertThat(e.getCause().getCause().getMessage(), equalTo("data stream timestamp field [@timestamp] is missing"));
     }
 
     public void testMultipleTimestampValuesInDocument() throws Exception {
@@ -1431,7 +1431,7 @@ public class DataStreamIT extends ESIntegTestCase {
         IndexRequest indexRequest = new IndexRequest(dataStreamName).opType("create")
             .source("{\"@timestamp\": [\"2020-12-12\",\"2022-12-12\"]}", XContentType.JSON);
         Exception e = expectThrows(Exception.class, client().index(indexRequest));
-        assertThat(e.getCause().getMessage(), equalTo("data stream timestamp field [@timestamp] encountered multiple values"));
+        assertThat(e.getCause().getCause().getMessage(), equalTo("data stream timestamp field [@timestamp] encountered multiple values"));
     }
 
     public void testMixedAutoCreate() throws Exception {

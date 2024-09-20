776c8ee3b3547d345eed2ce8457cd28db3e90554
Sam Xiao <sam.xiao@elastic.co>
2024-09-19 20:15:44 -0400

    Update x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/MountSnapshotStep.java
    Co-authored-by: Lee Hinman <dakrone@users.noreply.github.com>

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/MountSnapshotStep.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/MountSnapshotStep.java
@@ -54,7 +54,7 @@ public class MountSnapshotStep extends AsyncRetryDuringSnapshotActionStep {
         this.restoredIndexPrefix = restoredIndexPrefix;
         this.storageType = Objects.requireNonNull(storageType, "a storage type must be specified");
         if (totalShardsPerNode != null && totalShardsPerNode < -1) {
-            throw new IllegalArgumentException("[totalShardsPerNode] must be >= -1");
+            throw new IllegalArgumentException("[totalShardsPerNode] must be >= 1");
         }
         this.totalShardsPerNode = totalShardsPerNode;
     }

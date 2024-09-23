78fdd5ae4ec7faff3df7fe3d5d42595279bfce06
Pat Whelan <pat.whelan@elastic.co>
2024-09-23 18:19:40 -0400

    [ML] Stream Inference API (#113158)
    Create `POST _inference/<task>/<id>/_stream` and
    `POST _inference/<id>/_stream` API.
    
    REST Streaming API will reuse InferenceAction.
    For now, all services and task types will return an
    HTTP 405 status code and error message.
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/rest/RestStreamInferenceAction.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/rest/RestStreamInferenceAction.java
@@ -11,29 +11,33 @@ import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.rest.RestChannel;
 import org.elasticsearch.rest.Scope;
 import org.elasticsearch.rest.ServerlessScope;
-import org.elasticsearch.rest.action.RestChunkedToXContentListener;
 import org.elasticsearch.xpack.core.inference.action.InferenceAction;
 
 import java.util.List;
 
 import static org.elasticsearch.rest.RestRequest.Method.POST;
-import static org.elasticsearch.xpack.inference.rest.Paths.INFERENCE_ID_PATH;
-import static org.elasticsearch.xpack.inference.rest.Paths.TASK_TYPE_INFERENCE_ID_PATH;
+import static org.elasticsearch.xpack.inference.rest.Paths.STREAM_INFERENCE_ID_PATH;
+import static org.elasticsearch.xpack.inference.rest.Paths.STREAM_TASK_TYPE_INFERENCE_ID_PATH;
 
 @ServerlessScope(Scope.PUBLIC)
-public class RestInferenceAction extends BaseInferenceAction {
+public class RestStreamInferenceAction extends BaseInferenceAction {
     @Override
     public String getName() {
-        return "inference_action";
+        return "stream_inference_action";
     }
 
     @Override
     public List<Route> routes() {
-        return List.of(new Route(POST, INFERENCE_ID_PATH), new Route(POST, TASK_TYPE_INFERENCE_ID_PATH));
+        return List.of(new Route(POST, STREAM_INFERENCE_ID_PATH), new Route(POST, STREAM_TASK_TYPE_INFERENCE_ID_PATH));
+    }
+
+    @Override
+    protected InferenceAction.Request prepareInferenceRequest(InferenceAction.Request.Builder builder) {
+        return builder.setStream(true).build();
     }
 
     @Override
     protected ActionListener<InferenceAction.Response> listener(RestChannel channel) {
-        return new RestChunkedToXContentListener<>(channel);
+        return new ServerSentEventsRestActionListener(channel);
     }
 }

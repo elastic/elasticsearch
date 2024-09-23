89ead0768715e6836896a6e9497655fd5200affc
Christoph Büscher <christophbuescher@posteo.de>
2024-09-23 17:00:20 +0200

    Conditional stemming for 'persian' analyzer
    The 'persian' analyzer for Lucene 10 comes with PersianStemFilter as the last
    token filter by default. In order to maintain compatibility for old indices, we
    only use the new analyzer for newly created indices but configure a legacy
    analyzer with the old behaviour for older index versions.

--- server/src/main/java/org/elasticsearch/index/analysis/AnalyzerProvider.java
+++ server/src/main/java/org/elasticsearch/index/analysis/AnalyzerProvider.java
@@ -12,12 +12,12 @@ package org.elasticsearch.index.analysis;
 import org.apache.lucene.analysis.Analyzer;
 import org.elasticsearch.injection.guice.Provider;
 
-public interface AnalyzerProvider<T extends Analyzer> extends Provider<T> {
+public interface AnalyzerProvider<T extends Analyzer> extends Provider<Analyzer> {
 
     String name();
 
     AnalyzerScope scope();
 
     @Override
-    T get();
+    Analyzer get();
 }

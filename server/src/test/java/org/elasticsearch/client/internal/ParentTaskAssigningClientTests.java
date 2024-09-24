60290581eeb28165510f429288c198a78cc16190
David Turner <david.turner@elastic.co>
2024-09-24 16:08:15 +0100

    Return dummy (null) connection

--- server/src/test/java/org/elasticsearch/client/internal/ParentTaskAssigningClientTests.java
+++ server/src/test/java/org/elasticsearch/client/internal/ParentTaskAssigningClientTests.java
@@ -100,7 +100,7 @@ public class ParentTaskAssigningClientTests extends ESTestCase {
                             Request request,
                             ActionListener<Transport.Connection> listener
                         ) {
-                            fail("should not be called");
+                            listener.onResponse(null);
                         }
                     };
                 }

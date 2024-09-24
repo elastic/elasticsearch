98ec7c2c5b75cc205dc893d5ab96e0d0d0f208f5
Mark Tozzi <mark.tozzi@gmail.com>
2024-09-24 16:12:49 -0400

    add note and regenerate docs

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/convert/ToDateNanos.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/convert/ToDateNanos.java
@@ -61,6 +61,8 @@ public class ToDateNanos extends AbstractConvertFunction {
     @FunctionInfo(
         returnType = "date_nanos",
         description = "Converts an input to a nanosecond-resolution date value (aka date_nanos).",
+        note = "The range for date nanos is 1970-01-01T00:00:00.000000000Z to 2262-04-11T23:47:16.854775807Z.  Additionally, integers "
+        + "cannot be converted into date nanos, as the range of integer nanoseconds only covers about 2 seconds after epoch.",
         preview = true
     )
     public ToDateNanos(

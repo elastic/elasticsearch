52cff884dced6f621f42f3d252d53c4cb618f735
Salvatore Campagna <salvatore.campagna@elastic.co>
2024-09-25 16:43:20 +0200

    fix: put CLUSTER_LOGSDB_ENABLED setting in a common place

--- x-pack/plugin/stack/src/main/java/org/elasticsearch/xpack/stack/StackPlugin.java
+++ x-pack/plugin/stack/src/main/java/org/elasticsearch/xpack/stack/StackPlugin.java
@@ -14,7 +14,7 @@ import org.elasticsearch.plugins.Plugin;
 import java.util.Collection;
 import java.util.List;
 
-import static org.elasticsearch.datastreams.logsdb.LogsdbIndexModeSettingsProvider.CLUSTER_LOGSDB_ENABLED;
+import static org.elasticsearch.xpack.core.settings.IndexSettings.CLUSTER_LOGSDB_ENABLED;
 
 public class StackPlugin extends Plugin implements ActionPlugin {
     private final Settings settings;

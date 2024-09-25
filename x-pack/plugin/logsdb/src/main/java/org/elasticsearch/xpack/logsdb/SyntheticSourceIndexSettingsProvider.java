aaec9f8b787e646ab509fa3e1048893f51de49ad
Martijn van Groningen <martijn.v.groningen@gmail.com>
2024-09-25 13:35:46 +0200

    update comment

--- x-pack/plugin/logsdb/src/main/java/org/elasticsearch/xpack/logsdb/SyntheticSourceIndexSettingsProvider.java
+++ x-pack/plugin/logsdb/src/main/java/org/elasticsearch/xpack/logsdb/SyntheticSourceIndexSettingsProvider.java
@@ -101,8 +101,8 @@ final class SyntheticSourceIndexSettingsProvider implements IndexSettingProvider
         // Create MapperService just to extract keyword dimension fields:
         try (var mapperService = mapperServiceFactory.apply(tmpIndexMetadata.build())) {
             // combinedTemplateMappings can be null when creating system indices
+            // combinedTemplateMappings can be empty when creating a normal index that doesn't match any template and without mapping.
             if (combinedTemplateMappings == null || combinedTemplateMappings.isEmpty()) {
-                // this can happen when creating a normal index that doesn't match any template and without mapping.
                 combinedTemplateMappings = List.of(new CompressedXContent("{}"));
             }
             mapperService.merge(MapperService.SINGLE_MAPPING_NAME, combinedTemplateMappings, MapperService.MergeReason.INDEX_TEMPLATE);

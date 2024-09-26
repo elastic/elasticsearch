spongepower11/Search

Added transportversion for more generic EsQueryExec sort type

server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -210,6 +210,7 @@ public class TransportVersions {
     public static final TransportVersion CCS_TELEMETRY_STATS = def(8_739_00_0);
     public static final TransportVersion GLOBAL_RETENTION_TELEMETRY = def(8_740_00_0);
     public static final TransportVersion ROUTING_TABLE_VERSION_REMOVED = def(8_741_00_0);
+    public static final TransportVersion ESQL_QUERYEXEC_SORT_GENERALIZATION = def(8_742_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

elastic/elasticsearch

server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -225,6 +225,7 @@ public class TransportVersions {
     public static final TransportVersion ILM_ADD_SEARCHABLE_SNAPSHOT_TOTAL_SHARDS_PER_NODE = def(8_749_00_0);
     public static final TransportVersion SEMANTIC_TEXT_SEARCH_INFERENCE_ID = def(8_750_00_0);
     public static final TransportVersion ML_INFERENCE_CHUNKING_SETTINGS = def(8_751_00_0);
+    public static final TransportVersion ESQL_QUERYEXEC_SORT_GENERALIZATION = def(8_752_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,
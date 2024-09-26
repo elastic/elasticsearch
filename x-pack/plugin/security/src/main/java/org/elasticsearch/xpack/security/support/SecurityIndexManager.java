elastic/elasticsearch

commit 1c573efe20bf7e38d4bd0025c9da187496a4ff46

Wait for available security index on application privilege reads (#112813)
Application privilege reads (via Get Privileges or Has Privileges calls)
can return 503s while the security index is initializing, or otherwise
unavailable.

This PR adds a conditional wait for the security index on privilege
reads to avoid excessive 5xx errors. The wait has a timeout configurable
via a system property and is off by default. 

The implementation watches cluster state to determine when the security
index becomes available. 

In a follow-up, I will work on narrowing down the wait condition from
generic unavailable shard exceptions to when the index is in the process
of initiating.

x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/support/SecurityIndexManager.java
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ElasticsearchStatusException;
+import org.elasticsearch.ElasticsearchTimeoutException;
 import org.elasticsearch.ExceptionsHelper;
 import org.elasticsearch.ResourceAlreadyExistsException;
 import org.elasticsearch.action.ActionListener;
@@ -32,6 +33,7 @@ import org.elasticsearch.cluster.metadata.MappingMetadata;
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.routing.IndexRoutingTable;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.core.Tuple;
 import org.elasticsearch.features.FeatureService;
 import org.elasticsearch.features.NodeFeature;
@@ -42,6 +44,7 @@ import org.elasticsearch.index.IndexVersion;
 import org.elasticsearch.indices.IndexClosedException;
 import org.elasticsearch.indices.SystemIndexDescriptor;
 import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.threadpool.Scheduler;
 import org.elasticsearch.xcontent.XContentType;
 import org.elasticsearch.xpack.security.SecurityFeatures;
 
@@ -351,6 +354,52 @@ public class SecurityIndexManager implements ClusterStateListener {
             }
         };
         stateChangeListeners.add(stateChangeListener);
+    }
+
+    /**
+     * Waits up to {@code timeout} for the security index to become available for search, based on cluster state updates.
+     * Notifies {@code listener} once the security index is available, or calls {@code onFailure} on {@code timeout}.
+     */
+    public void onIndexAvailableForSearch(ActionListener<Void> listener, TimeValue timeout) {
+        logger.info("Will wait for security index [{}] to become available for search", getConcreteIndexName());
+
+        final ActionListener<Void> notifyOnceListener = ActionListener.notifyOnce(listener);
+
+        final var indexAvailableForSearchListener = new StateConsumerWithCancellable() {
+            @Override
+            public void accept(SecurityIndexManager.State previousState, SecurityIndexManager.State nextState) {
+                if (nextState.indexAvailableForSearch) {
+                    assert cancellable != null;
+                    // cancel and removeStateListener are idempotent
+                    cancellable.cancel();
+                    removeStateListener(this);
+                    notifyOnceListener.onResponse(null);
+                }
+            }
+        };
+        // schedule failure handling on timeout -- keep reference to cancellable so a successful completion can cancel the timeout
+        indexAvailableForSearchListener.cancellable = client.threadPool().schedule(() -> {
+            removeStateListener(indexAvailableForSearchListener);
+            notifyOnceListener.onFailure(
+                new ElasticsearchTimeoutException(
+                    "timed out waiting for security index [" + getConcreteIndexName() + "] to become available for search"
+                )
+            );
+        }, timeout, client.threadPool().generic());
+
+        // in case the state has meanwhile changed to available, return immediately
+        if (state.indexAvailableForSearch) {
+            indexAvailableForSearchListener.cancellable.cancel();
+            notifyOnceListener.onResponse(null);
+        } else {
+            addStateListener(indexAvailableForSearchListener);
+        }
+    }
+
+    private abstract static class StateConsumerWithCancellable
+        implements
+            BiConsumer<SecurityIndexManager.State, SecurityIndexManager.State> {
+        volatile Scheduler.ScheduledCancellable cancellable;
     }
 
     private Tuple<Boolean, Boolean> checkIndexAvailable(ClusterState state) {
@@ -717,6 +766,42 @@ public class SecurityIndexManager implements ClusterStateListener {
                 indexHealth,
                 securityFeatures
             );
+        }
+
+        @Override
+        public String toString() {
+            return "State{"
+                + "creationTime="
+                + creationTime
+                + ", isIndexUpToDate="
+                + isIndexUpToDate
+                + ", indexAvailableForSearch="
+                + indexAvailableForSearch
+                + ", indexAvailableForWrite="
+                + indexAvailableForWrite
+                + ", mappingUpToDate="
+                + mappingUpToDate
+                + ", createdOnLatestVersion="
+                + createdOnLatestVersion
+                + ", migrationsVersion="
+                + migrationsVersion
+                + ", minClusterMappingVersion="
+                + minClusterMappingVersion
+                + ", indexMappingVersion="
+                + indexMappingVersion
+                + ", concreteIndexName='"
+                + concreteIndexName
+                + '\''
+                + ", indexHealth="
+                + indexHealth
+                + ", indexState="
+                + indexState
+                + ", indexUUID='"
+                + indexUUID
+                + '\''
+                + ", securityFeatures="
+                + securityFeatures
+                + '}';
         }
     }
 }

x-pack/plugin/security/src/internalClusterTest/java/org/elasticsearch/xpack/security/support/SecurityIndexManagerIntegTests.java
@@ -6,20 +6,25 @@
  */
 package org.elasticsearch.xpack.security.support;
 
+import org.elasticsearch.ElasticsearchTimeoutException;
 import org.elasticsearch.action.ActionFuture;
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
 import org.elasticsearch.action.admin.indices.get.GetIndexResponse;
 import org.elasticsearch.action.admin.indices.settings.put.UpdateSettingsRequest;
 import org.elasticsearch.action.admin.indices.template.put.TransportPutComposableIndexTemplateAction;
 import org.elasticsearch.action.support.IndicesOptions;
+import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.cluster.metadata.ComposableIndexTemplate;
 import org.elasticsearch.cluster.metadata.Template;
 import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.AbstractRunnable;
+import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.test.SecurityIntegTestCase;
 import org.elasticsearch.xpack.core.security.action.user.PutUserRequestBuilder;
 import org.elasticsearch.xpack.core.security.action.user.PutUserResponse;
+import org.elasticsearch.xpack.security.authz.store.NativePrivilegeStore;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 
@@ -94,6 +99,74 @@ public class SecurityIndexManagerIntegTests extends SecurityIntegTestCase {
             // index is created. So we don't need to assert the value.
             future.actionGet().created();
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testOnIndexAvailableForSearchIndexCompletesWithinTimeout() throws Exception {
+        final SecurityIndexManager securityIndexManager = internalCluster().getInstances(NativePrivilegeStore.class)
+            .iterator()
+            .next()
+            .getSecurityIndexManager();
+        final ActionFuture<Void> future = new PlainActionFuture<>();
+        // pick longer wait than in the assertBusy that waits for below to ensure index has had enough time to initialize
+        securityIndexManager.onIndexAvailableForSearch((ActionListener<Void>) future, TimeValue.timeValueSeconds(40));
+
+        createSecurityIndex();
+
+        assertBusy(
+            () -> assertThat(securityIndexManager.isAvailable(SecurityIndexManager.Availability.SEARCH_SHARDS), is(true)),
+            30,
+            TimeUnit.SECONDS
+        );
+
+        // security index creation is complete and index is available for search; therefore whenIndexAvailableForSearch should report
+        // success in time
+        future.actionGet();
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testOnIndexAvailableForSearchIndexAlreadyAvailable() throws Exception {
+        createSecurityIndex();
+
+        final SecurityIndexManager securityIndexManager = internalCluster().getInstances(NativePrivilegeStore.class)
+            .iterator()
+            .next()
+            .getSecurityIndexManager();
+
+        // ensure security index manager state is fully in the expected precondition state for this test (ready for search)
+        assertBusy(
+            () -> assertThat(securityIndexManager.isAvailable(SecurityIndexManager.Availability.SEARCH_SHARDS), is(true)),
+            30,
+            TimeUnit.SECONDS
+        );
+
+        // With 0 timeout
+        {
+            final ActionFuture<Void> future = new PlainActionFuture<>();
+            securityIndexManager.onIndexAvailableForSearch((ActionListener<Void>) future, TimeValue.timeValueSeconds(0));
+            future.actionGet();
+        }
+
+        // With non-0 timeout
+        {
+            final ActionFuture<Void> future = new PlainActionFuture<>();
+            securityIndexManager.onIndexAvailableForSearch((ActionListener<Void>) future, TimeValue.timeValueSeconds(10));
+            future.actionGet();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testOnIndexAvailableForSearchIndexWaitTimeOut() {
+        // security index does not exist and nothing is triggering creation, so whenIndexAvailableForSearch will time out
+
+        final SecurityIndexManager securityIndexManager = internalCluster().getInstances(NativePrivilegeStore.class)
+            .iterator()
+            .next()
+            .getSecurityIndexManager();
+
+        final ActionFuture<Void> future = new PlainActionFuture<>();
+        securityIndexManager.onIndexAvailableForSearch((ActionListener<Void>) future, TimeValue.timeValueMillis(100));
+        expectThrows(ElasticsearchTimeoutException.class, future::actionGet);
     }
 
     public void testSecurityIndexSettingsCannotBeChanged() throws Exception {

x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authz/store/CompositeRolesStore.java
@@ -532,6 +532,7 @@ public class CompositeRolesStore {
             privilegeStore.getPrivileges(
                 applicationNames,
                 applicationPrivilegeNames,
+                false, // TODO revisit if we should also wait for an available security index here
                 listener.delegateFailureAndWrap((delegate, appPrivileges) -> {
                     applicationPrivilegesMap.forEach(
                         (key, names) -> ApplicationPrivilege.get(key.v1(), names, appPrivileges)

commit 11c0bf8d6ee4bbd6e982a04211cc3994c5dd48e0

Fix cancellation race condition in `onIndexAvailableForSearch`  (#113386)
This PR fixes following race conditions in `onIndexAvailableForSearch`
introduced in https://github.com/elastic/elasticsearch/pull/112813:

1. If the method is called when the index is already available, cancellation is still scheduled and may execute before successful completion (manifested in test failures https://github.com/elastic/elasticsearch/issues/113336)
2. If the cancel task runs _before_ `addStateListener`, it may fail to remove the listener (noticed while fixing the first issue)

These race conditions only manifest for small timeout windows, and are
completely bypassed for 0 timeout windows based on other checks in prod
code, so the practical impact is fortunately limited.

Resolves: https://github.com/elastic/elasticsearch/issues/113336

x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/support/SecurityIndexManager.java
@@ -54,6 +54,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -364,45 +365,80 @@ public class SecurityIndexManager implements ClusterStateListener {
      * Notifies {@code listener} once the security index is available, or calls {@code onFailure} on {@code timeout}.
      */
     public void onIndexAvailableForSearch(ActionListener<Void> listener, TimeValue timeout) {
-        logger.info("Will wait for security index [{}] to become available for search", getConcreteIndexName());
+        logger.info("Will wait for security index [{}] for [{}] to become available for search", getConcreteIndexName(), timeout);
 
-        final ActionListener<Void> notifyOnceListener = ActionListener.notifyOnce(listener);
+        if (state.indexAvailableForSearch) {
+            logger.debug("Security index [{}] is already available", getConcreteIndexName());
+            listener.onResponse(null);
+            return;
+        }
 
+        final AtomicBoolean isDone = new AtomicBoolean(false);
         final var indexAvailableForSearchListener = new StateConsumerWithCancellable() {
             @Override
             public void accept(SecurityIndexManager.State previousState, SecurityIndexManager.State nextState) {
                 if (nextState.indexAvailableForSearch) {
-                    assert cancellable != null;
-                    // cancel and removeStateListener are idempotent
-                    cancellable.cancel();
-                    removeStateListener(this);
-                    notifyOnceListener.onResponse(null);
+                    if (isDone.compareAndSet(false, true)) {
+                        cancel();
+                        removeStateListener(this);
+                        listener.onResponse(null);
+                    }
                 }
             }
         };
-        // schedule failure handling on timeout -- keep reference to cancellable so a successful completion can cancel the timeout
-        indexAvailableForSearchListener.cancellable = client.threadPool().schedule(() -> {
-            removeStateListener(indexAvailableForSearchListener);
-            notifyOnceListener.onFailure(
-                new ElasticsearchTimeoutException(
-                    "timed out waiting for security index [" + getConcreteIndexName() + "] to become available for search"
-                )
-            );
-        }, timeout, client.threadPool().generic());
+        // add listener _before_ registering timeout -- this way we are guaranteed it gets removed (either by timeout below, or successful
+        // completion above)
+        addStateListener(indexAvailableForSearchListener);
 
-        // in case the state has meanwhile changed to available, return immediately
-        if (state.indexAvailableForSearch) {
-            indexAvailableForSearchListener.cancellable.cancel();
-            notifyOnceListener.onResponse(null);
-        } else {
-            addStateListener(indexAvailableForSearchListener);
-        }
+        // schedule failure handling on timeout -- keep reference to cancellable so a successful completion can cancel the timeout
+        indexAvailableForSearchListener.setCancellable(client.threadPool().schedule(() -> {
+            if (isDone.compareAndSet(false, true)) {
+                removeStateListener(indexAvailableForSearchListener);
+                listener.onFailure(
+                    new ElasticsearchTimeoutException(
+                        "timed out waiting for security index [" + getConcreteIndexName() + "] to become available for search"
+                    )
+                );
+            }
+        }, timeout, client.threadPool().generic()));
     }
 
-    private abstract static class StateConsumerWithCancellable
+    // pkg-private for testing
+    List<BiConsumer<State, State>> getStateChangeListeners() {
+        return stateChangeListeners;
+    }
+
+    /**
+     * This class ensures that if cancel() is called _before_ setCancellable(), the passed-in cancellable is still correctly cancelled on
+     * a subsequent setCancellable() call.
+     */
+    // pkg-private for testing
+    abstract static class StateConsumerWithCancellable
         implements
-            BiConsumer<SecurityIndexManager.State, SecurityIndexManager.State> {
-        volatile Scheduler.ScheduledCancellable cancellable;
+            BiConsumer<SecurityIndexManager.State, SecurityIndexManager.State>,
+            Scheduler.Cancellable {
+        private volatile Scheduler.ScheduledCancellable cancellable;
+        private volatile boolean cancelled = false;
+
+        void setCancellable(Scheduler.ScheduledCancellable cancellable) {
+            this.cancellable = cancellable;
+            if (cancelled) {
+                cancel();
+            }
+        }
+
+        public boolean cancel() {
+            cancelled = true;
+            if (cancellable != null) {
+                // cancellable is idempotent, so it's fine to potentially call it multiple times
+                return cancellable.cancel();
+            }
+            return isCancelled();
+        }
+
+        public boolean isCancelled() {
+            return cancelled;
+        }
     }
 
     private Tuple<Boolean, Boolean> checkIndexAvailable(ClusterState state) {

muted-tests.yml
@@ -251,9 +251,6 @@ tests:
 - class: org.elasticsearch.xpack.test.rest.XPackRestIT
   method: test {p0=transform/transforms_force_delete/Test force deleting a running transform}
   issue: https://github.com/elastic/elasticsearch/issues/113327
-- class: org.elasticsearch.xpack.security.support.SecurityIndexManagerIntegTests
-  method: testOnIndexAvailableForSearchIndexAlreadyAvailable
-  issue: https://github.com/elastic/elasticsearch/issues/113336
 - class: org.elasticsearch.xpack.test.rest.XPackRestIT
   method: test {p0=analytics/top_metrics/sort by scaled float field}
   issue: https://github.com/elastic/elasticsearch/issues/113340

x-pack/plugin/security/src/internalClusterTest/java/org/elasticsearch/xpack/security/support/SecurityIndexManagerIntegTests.java
@@ -26,24 +26,38 @@ import org.elasticsearch.xpack.core.security.action.user.PutUserRequestBuilder;
 import org.elasticsearch.xpack.core.security.action.user.PutUserResponse;
 import org.elasticsearch.xpack.security.authz.store.NativePrivilegeStore;
 import org.hamcrest.Matchers;
+import org.junit.After;
 import org.junit.Before;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.elasticsearch.xpack.security.support.SecuritySystemIndices.SECURITY_MAIN_ALIAS;
 import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
 public class SecurityIndexManagerIntegTests extends SecurityIntegTestCase {
+
+    private final int concurrentCallsToOnAvailable = 6;
+    private final ExecutorService executor = Executors.newFixedThreadPool(concurrentCallsToOnAvailable);
+
+    @After
+    public void shutdownExecutor() {
+        executor.shutdown();
+    }
 
     public void testConcurrentOperationsTryingToCreateSecurityIndexAndAlias() throws Exception {
         final int processors = Runtime.getRuntime().availableProcessors();
@@ -110,6 +124,12 @@ public class SecurityIndexManagerIntegTests extends SecurityIntegTestCase {
         // pick longer wait than in the assertBusy that waits for below to ensure index has had enough time to initialize
         securityIndexManager.onIndexAvailableForSearch((ActionListener<Void>) future, TimeValue.timeValueSeconds(40));
 
+        // check listener added
+        assertThat(
+            securityIndexManager.getStateChangeListeners(),
+            hasItem(instanceOf(SecurityIndexManager.StateConsumerWithCancellable.class))
+        );
+
         createSecurityIndexWithWaitForActiveShards();
 
         assertBusy(
@@ -121,6 +141,12 @@ public class SecurityIndexManagerIntegTests extends SecurityIntegTestCase {
         // security index creation is complete and index is available for search; therefore whenIndexAvailableForSearch should report
         // success in time
         future.actionGet();
+
+        // check no remaining listeners
+        assertThat(
+            securityIndexManager.getStateChangeListeners(),
+            not(hasItem(instanceOf(SecurityIndexManager.StateConsumerWithCancellable.class)))
+        );
     }
 
     @SuppressWarnings("unchecked")
@@ -152,6 +178,69 @@ public class SecurityIndexManagerIntegTests extends SecurityIntegTestCase {
             securityIndexManager.onIndexAvailableForSearch((ActionListener<Void>) future, TimeValue.timeValueSeconds(10));
             future.actionGet();
         }
+
+        // check no remaining listeners
+        assertThat(
+            securityIndexManager.getStateChangeListeners(),
+            not(hasItem(instanceOf(SecurityIndexManager.StateConsumerWithCancellable.class)))
+        );
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testOnIndexAvailableForSearchIndexUnderConcurrentLoad() throws Exception {
+        final SecurityIndexManager securityIndexManager = internalCluster().getInstances(NativePrivilegeStore.class)
+            .iterator()
+            .next()
+            .getSecurityIndexManager();
+        // Long time out calls should all succeed
+        final List<Future<Void>> futures = new ArrayList<>();
+        for (int i = 0; i < concurrentCallsToOnAvailable / 2; i++) {
+            final Future<Void> future = executor.submit(() -> {
+                try {
+                    final ActionFuture<Void> f = new PlainActionFuture<>();
+                    securityIndexManager.onIndexAvailableForSearch((ActionListener<Void>) f, TimeValue.timeValueSeconds(40));
+                    f.actionGet();
+                } catch (Exception ex) {
+                    fail(ex, "should not have encountered exception");
+                }
+                return null;
+            });
+            futures.add(future);
+        }
+
+        // short time-out tasks should all time out
+        for (int i = 0; i < concurrentCallsToOnAvailable / 2; i++) {
+            final Future<Void> future = executor.submit(() -> {
+                expectThrows(ElasticsearchTimeoutException.class, () -> {
+                    final ActionFuture<Void> f = new PlainActionFuture<>();
+                    securityIndexManager.onIndexAvailableForSearch((ActionListener<Void>) f, TimeValue.timeValueMillis(10));
+                    f.actionGet();
+                });
+                return null;
+            });
+            futures.add(future);
+        }
+
+        // Sleep a second for short-running calls to timeout
+        Thread.sleep(1000);
+
+        createSecurityIndexWithWaitForActiveShards();
+        // ensure security index manager state is fully in the expected precondition state for this test (ready for search)
+        assertBusy(
+            () -> assertThat(securityIndexManager.isAvailable(SecurityIndexManager.Availability.SEARCH_SHARDS), is(true)),
+            30,
+            TimeUnit.SECONDS
+        );
+
+        for (var future : futures) {
+            future.get(10, TimeUnit.SECONDS);
+        }
+
+        // check no remaining listeners
+        assertThat(
+            securityIndexManager.getStateChangeListeners(),
+            not(hasItem(instanceOf(SecurityIndexManager.StateConsumerWithCancellable.class)))
+        );
     }
 
     @SuppressWarnings("unchecked")
@@ -163,9 +252,24 @@ public class SecurityIndexManagerIntegTests extends SecurityIntegTestCase {
             .next()
             .getSecurityIndexManager();
 
-        final ActionFuture<Void> future = new PlainActionFuture<>();
-        securityIndexManager.onIndexAvailableForSearch((ActionListener<Void>) future, TimeValue.timeValueMillis(100));
-        expectThrows(ElasticsearchTimeoutException.class, future::actionGet);
+        {
+            final ActionFuture<Void> future = new PlainActionFuture<>();
+            securityIndexManager.onIndexAvailableForSearch((ActionListener<Void>) future, TimeValue.timeValueMillis(100));
+            expectThrows(ElasticsearchTimeoutException.class, future::actionGet);
+        }
+
+        // Also works with 0 timeout
+        {
+            final ActionFuture<Void> future = new PlainActionFuture<>();
+            securityIndexManager.onIndexAvailableForSearch((ActionListener<Void>) future, TimeValue.timeValueMillis(0));
+            expectThrows(ElasticsearchTimeoutException.class, future::actionGet);
+        }
+
+        // check no remaining listeners
+        assertThat(
+            securityIndexManager.getStateChangeListeners(),
+            not(hasItem(instanceOf(SecurityIndexManager.StateConsumerWithCancellable.class)))
+        );
     }
 
     public void testSecurityIndexSettingsCannotBeChanged() throws Exception {
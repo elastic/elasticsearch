762ae81300b191b356cb0aaae36eb1d72b14f14e
David Turner <david.turner@elastic.co>
2024-10-02 21:25:52 +0100

    Fix

--- server/src/main/java/org/elasticsearch/rest/action/admin/cluster/RestClusterStatsAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/admin/cluster/RestClusterStatsAction.java
@@ -32,9 +32,7 @@ import static org.elasticsearch.rest.RestUtils.getTimeout;
 public class RestClusterStatsAction extends BaseRestHandler {
 
     private static final Set<String> SUPPORTED_CAPABILITIES = Set.of("human-readable-total-docs-size");
-    private static final Set<String> SUPPORTED_CAPABILITIES_CCS_STATS = Sets.copyOf(
-        Sets.union(SUPPORTED_CAPABILITIES, Set.of("ccs-stats"))
-    );
+    private static final Set<String> SUPPORTED_CAPABILITIES_CCS_STATS = Set.copyOf(Sets.union(SUPPORTED_CAPABILITIES, Set.of("ccs-stats")));
     public static final FeatureFlag CCS_TELEMETRY_FEATURE_FLAG = new FeatureFlag("ccs_telemetry");
     private static final Set<String> SUPPORTED_QUERY_PARAMETERS = Set.of("include_remotes", "nodeId", REST_TIMEOUT_PARAM);
 

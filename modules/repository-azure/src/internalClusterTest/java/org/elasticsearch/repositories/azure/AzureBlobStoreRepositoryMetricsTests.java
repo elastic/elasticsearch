02b5139b83acd99af5e656face6f10810bf342db
Nick Tindall <nick.tindall@elastic.co>
2024-10-02 14:50:17 +1000

    Tidy tests

--- modules/repository-azure/src/internalClusterTest/java/org/elasticsearch/repositories/azure/AzureBlobStoreRepositoryMetricsTests.java
+++ modules/repository-azure/src/internalClusterTest/java/org/elasticsearch/repositories/azure/AzureBlobStoreRepositoryMetricsTests.java
@@ -18,12 +18,14 @@ import org.elasticsearch.common.blobstore.BlobPath;
 import org.elasticsearch.common.blobstore.OperationPurpose;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.core.SuppressForbidden;
+import org.elasticsearch.plugins.PluginsService;
 import org.elasticsearch.repositories.RepositoriesMetrics;
 import org.elasticsearch.repositories.RepositoriesService;
 import org.elasticsearch.repositories.blobstore.BlobStoreRepository;
 import org.elasticsearch.repositories.blobstore.RequestedRangeNotSatisfiedException;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.telemetry.Measurement;
+import org.elasticsearch.telemetry.TestTelemetryPlugin;
 import org.junit.After;
 
 import java.io.IOException;
@@ -33,11 +35,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.elasticsearch.repositories.azure.AbstractAzureServerTestCase.randomBlobContent;
+import static org.hamcrest.Matchers.greaterThan;
 
 @SuppressForbidden(reason = "we use a HttpServer to emulate Azure")
 public class AzureBlobStoreRepositoryMetricsTests extends AzureBlobStoreRepositoryTests {
@@ -79,7 +84,7 @@ public class AzureBlobStoreRepositoryMetricsTests extends AzureBlobStoreReposito
         return blobStoreRepository.blobStore().blobContainer(BlobPath.EMPTY.add(randomIdentifier()));
     }
 
-    public void testRetriesAreCountedInMetrics() throws IOException {
+    public void testThrottleResponsesAreCountedInMetrics() throws IOException {
         final String repository = createRepository(randomRepositoryName());
         final String dataNodeName = internalCluster().getNodeNameThat(DiscoveryNode::canContainData);
         final BlobContainer blobContainer = getBlobContainer(dataNodeName, repository);
@@ -88,6 +93,7 @@ public class AzureBlobStoreRepositoryMetricsTests extends AzureBlobStoreReposito
         final String blobName = "index-" + randomIdentifier();
         final OperationPurpose purpose = randomFrom(OperationPurpose.values());
         blobContainer.writeBlob(purpose, blobName, BytesReference.fromByteBuffer(ByteBuffer.wrap(randomBlobContent())), false);
+        clearMetrics(dataNodeName);
 
         // Queue up some throttle responses
         final int numThrottles = randomIntBetween(1, MAX_RETRIES);
@@ -96,36 +102,12 @@ public class AzureBlobStoreRepositoryMetricsTests extends AzureBlobStoreReposito
         // Check that the blob exists
         blobContainer.blobExists(purpose, blobName);
 
-        // These are recorded as throttles
-        assertCounterMetricRecorded(
-            dataNodeName,
-            RepositoriesMetrics.METRIC_THROTTLES_TOTAL,
-            purpose,
-            AzureBlobStore.Operation.GET_BLOB_PROPERTIES,
-            numThrottles
-        );
-        assertHistogramMetricRecorded(
-            dataNodeName,
-            RepositoriesMetrics.METRIC_THROTTLES_HISTOGRAM,
-            purpose,
-            AzureBlobStore.Operation.GET_BLOB_PROPERTIES,
-            numThrottles
-        );
-
-        // And exceptions
-        assertCounterMetricRecorded(
-            dataNodeName,
-            RepositoriesMetrics.METRIC_EXCEPTIONS_TOTAL,
-            purpose,
-            AzureBlobStore.Operation.GET_BLOB_PROPERTIES,
-            numThrottles
-        );
-        assertHistogramMetricRecorded(
-            dataNodeName,
-            RepositoriesMetrics.METRIC_EXCEPTIONS_HISTOGRAM,
-            purpose,
-            AzureBlobStore.Operation.GET_BLOB_PROPERTIES,
-            numThrottles
+        // Correct metrics are recorded
+        metricsAsserter(dataNodeName, purpose, AzureBlobStore.Operation.GET_BLOB_PROPERTIES, repository).assertMetricsRecorded(
+            numThrottles + 1,
+            numThrottles,
+            numThrottles,
+            MetricsAsserter.Result.Success
         );
     }
 
@@ -138,6 +120,7 @@ public class AzureBlobStoreRepositoryMetricsTests extends AzureBlobStoreReposito
         final String blobName = "index-" + randomIdentifier();
         final OperationPurpose purpose = randomFrom(OperationPurpose.values());
         blobContainer.writeBlob(purpose, blobName, BytesReference.fromByteBuffer(ByteBuffer.wrap(randomBlobContent())), false);
+        clearMetrics(dataNodeName);
 
         // Queue up a range-not-satisfied error
         errorQueue.offer(new ErrorResponse(RestStatus.REQUESTED_RANGE_NOT_SATISFIED, null, GET_BLOB_REQUEST_PREDICATE));
@@ -145,28 +128,12 @@ public class AzureBlobStoreRepositoryMetricsTests extends AzureBlobStoreReposito
         // Attempt to read the blob
         assertThrows(RequestedRangeNotSatisfiedException.class, () -> blobContainer.readBlob(purpose, blobName));
 
-        assertCounterMetricRecorded(
-            dataNodeName,
-            RepositoriesMetrics.METRIC_EXCEPTIONS_REQUEST_RANGE_NOT_SATISFIED_TOTAL,
-            purpose,
-            AzureBlobStore.Operation.GET_BLOB,
-            1
-        );
-
-        // Also tracked as exceptions
-        assertCounterMetricRecorded(
-            dataNodeName,
-            RepositoriesMetrics.METRIC_EXCEPTIONS_TOTAL,
-            purpose,
-            AzureBlobStore.Operation.GET_BLOB,
-            1
-        );
-        assertHistogramMetricRecorded(
-            dataNodeName,
-            RepositoriesMetrics.METRIC_EXCEPTIONS_HISTOGRAM,
-            purpose,
-            AzureBlobStore.Operation.GET_BLOB,
-            1
+        // Correct metrics are recorded
+        metricsAsserter(dataNodeName, purpose, AzureBlobStore.Operation.GET_BLOB, repository).assertMetricsRecorded(
+            1,
+            0,
+            1,
+            MetricsAsserter.Result.RangeNotSatisfied
         );
     }
 
@@ -182,87 +149,130 @@ public class AzureBlobStoreRepositoryMetricsTests extends AzureBlobStoreReposito
 
         // Queue some retryable error responses
         final int numErrors = randomIntBetween(1, MAX_RETRIES);
-        IntStream.range(0, numErrors)
-            .forEach(
-                i -> errorQueue.offer(
-                    new ErrorResponse(
-                        randomFrom(RestStatus.INTERNAL_SERVER_ERROR, RestStatus.TOO_MANY_REQUESTS, RestStatus.SERVICE_UNAVAILABLE)
-                    )
-                )
-            );
+        final AtomicInteger throttles = new AtomicInteger();
+        IntStream.range(0, numErrors).forEach(i -> {
+            RestStatus status = randomFrom(RestStatus.INTERNAL_SERVER_ERROR, RestStatus.TOO_MANY_REQUESTS, RestStatus.SERVICE_UNAVAILABLE);
+            if (status == RestStatus.TOO_MANY_REQUESTS) {
+                throttles.incrementAndGet();
+            }
+            errorQueue.offer(new ErrorResponse(status));
+        });
+        clearMetrics(dataNodeName);
 
         // Check that the blob exists
         blobContainer.blobExists(purpose, blobName);
 
-        assertCounterMetricRecorded(
-            dataNodeName,
-            RepositoriesMetrics.METRIC_EXCEPTIONS_TOTAL,
-            purpose,
-            AzureBlobStore.Operation.GET_BLOB_PROPERTIES,
-            numErrors
-        );
-        assertHistogramMetricRecorded(
-            dataNodeName,
-            RepositoriesMetrics.METRIC_EXCEPTIONS_HISTOGRAM,
-            purpose,
-            AzureBlobStore.Operation.GET_BLOB_PROPERTIES,
-            numErrors
+        // Correct metrics are recorded
+        metricsAsserter(dataNodeName, purpose, AzureBlobStore.Operation.GET_BLOB_PROPERTIES, repository).assertMetricsRecorded(
+            numErrors + 1,
+            throttles.get(),
+            numErrors,
+            MetricsAsserter.Result.Success
         );
     }
 
-    private void assertCounterMetricRecorded(
+    private void clearMetrics(String discoveryNode) {
+        internalCluster().getInstance(PluginsService.class, discoveryNode)
+            .filterPlugins(TestTelemetryPlugin.class)
+            .forEach(TestTelemetryPlugin::resetMeter);
+    }
+
+    private MetricsAsserter metricsAsserter(
         String dataNodeName,
-        String metricName,
-        OperationPurpose purpose,
-        AzureBlobStore.Operation operation,
-        int expectedValue
-    ) {
-        assertIntValueMetricRecorded(
-            getTelemetryPlugin(dataNodeName).getLongCounterMeasurement(metricName),
-            purpose,
-            operation,
-            expectedValue
-        );
-    }
-
-    private void assertHistogramMetricRecorded(
-        String dataNodeName,
-        String metricName,
-        OperationPurpose purpose,
-        AzureBlobStore.Operation operation,
-        int expectedValue
-    ) {
-        assertIntValueMetricRecorded(
-            getTelemetryPlugin(dataNodeName).getLongHistogramMeasurement(metricName),
-            purpose,
-            operation,
-            expectedValue
-        );
-    }
-
-    private void assertIntValueMetricRecorded(
-        List<Measurement> measurements,
         OperationPurpose operationPurpose,
         AzureBlobStore.Operation operation,
-        int expectedValue
+        String repository
     ) {
-        Measurement measurement = measurements.stream()
-            .filter(
-                m -> m.attributes().get("operation").equals(operation.getKey())
-                    && m.attributes().get("purpose").equals(operationPurpose.getKey())
-            )
-            .findFirst()
-            .orElseThrow(
-                () -> new IllegalStateException(
-                    "No metric found with operation="
-                        + operation.getKey()
-                        + " and purpose="
-                        + operationPurpose.getKey()
-                        + " in "
-                        + measurements
-                )
+        return new MetricsAsserter(dataNodeName, operationPurpose, operation, repository);
+    }
+
+    private class MetricsAsserter {
+        private final String dataNodeName;
+        private final OperationPurpose purpose;
+        private final AzureBlobStore.Operation operation;
+        private final String repository;
+
+        enum Result {
+            Success,
+            Failure,
+            RangeNotSatisfied,
+        }
+
+        private MetricsAsserter(String dataNodeName, OperationPurpose purpose, AzureBlobStore.Operation operation, String repository) {
+            this.dataNodeName = dataNodeName;
+            this.purpose = purpose;
+            this.operation = operation;
+            this.repository = repository;
+        }
+
+        void assertMetricsRecorded(int expectedRequests, int expectedThrottles, int expectedExceptions, Result result) {
+            assertCounterMetricRecorded(RepositoriesMetrics.METRIC_OPERATIONS_TOTAL, 1);
+            assertCounterMetricRecorded(RepositoriesMetrics.METRIC_REQUESTS_TOTAL, expectedRequests);
+
+            if (expectedThrottles > 0) {
+                assertCounterMetricRecorded(RepositoriesMetrics.METRIC_THROTTLES_TOTAL, expectedThrottles);
+                assertHistogramMetricRecorded(RepositoriesMetrics.METRIC_THROTTLES_HISTOGRAM, expectedThrottles);
+            }
+
+            if (expectedExceptions > 0) {
+                assertCounterMetricRecorded(RepositoriesMetrics.METRIC_EXCEPTIONS_TOTAL, expectedExceptions);
+                assertHistogramMetricRecorded(RepositoriesMetrics.METRIC_EXCEPTIONS_HISTOGRAM, expectedExceptions);
+            }
+
+            if (result == Result.RangeNotSatisfied || result == Result.Failure) {
+                assertCounterMetricRecorded(RepositoriesMetrics.METRIC_UNSUCCESSFUL_OPERATIONS_TOTAL, 1);
+            }
+
+            if (result == Result.RangeNotSatisfied) {
+                assertCounterMetricRecorded(RepositoriesMetrics.METRIC_EXCEPTIONS_REQUEST_RANGE_NOT_SATISFIED_TOTAL, 1);
+            }
+
+            assertTimeToResponseMetricRecorded();
+        }
+
+        void assertCounterMetricRecorded(String metricName, int expectedValue) {
+            assertIntValueMetricRecorded(getTelemetryPlugin(dataNodeName).getLongCounterMeasurement(metricName), expectedValue);
+        }
+
+        void assertHistogramMetricRecorded(String metricName, int expectedValue) {
+            assertIntValueMetricRecorded(getTelemetryPlugin(dataNodeName).getLongHistogramMeasurement(metricName), expectedValue);
+        }
+
+        void assertTimeToResponseMetricRecorded() {
+            assertMatchingMetricRecorded(
+                getTelemetryPlugin(dataNodeName).getLongHistogramMeasurement(RepositoriesMetrics.HTTP_REQUEST_TIME_IN_MILLIS_HISTOGRAM),
+                m -> assertThat(m.getLong(), greaterThan(0L))
             );
-        assertEquals(measurement.value().intValue(), expectedValue);
+        }
+
+        private void assertIntValueMetricRecorded(List<Measurement> measurements, int expectedValue) {
+            assertMatchingMetricRecorded(measurements, m -> assertEquals(expectedValue, m.getLong()));
+        }
+
+        private void assertMatchingMetricRecorded(List<Measurement> measurements, Consumer<Measurement> assertion) {
+            Measurement measurement = measurements.stream()
+                .filter(
+                    m -> m.attributes().get("operation").equals(operation.getKey())
+                        && m.attributes().get("purpose").equals(purpose.getKey())
+                        && m.attributes().get("repo_name").equals(repository)
+                        && m.attributes().get("repo_type").equals("azure")
+                )
+                .findFirst()
+                .orElseThrow(
+                    () -> new IllegalStateException(
+                        "No metric found with operation="
+                            + operation.getKey()
+                            + " and purpose="
+                            + purpose.getKey()
+                            + " and repo_name="
+                            + repository
+                            + " in "
+                            + measurements
+                    )
+                );
+
+            assertion.accept(measurement);
+        }
     }
 
     @SuppressForbidden(reason = "we use a HttpServer to emulate Azure")

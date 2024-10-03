eadc462bbc1691da5fa8bd7737575a5967b82a91
Simon Cooper <simon.cooper@elastic.co>
2024-10-03 14:24:07 +0100

    Use params provided in lambda

--- server/src/main/java/org/elasticsearch/common/xcontent/ChunkedToXContentBuilder.java
+++ server/src/main/java/org/elasticsearch/common/xcontent/ChunkedToXContentBuilder.java
@@ -347,7 +347,7 @@ public class ChunkedToXContentBuilder implements Iterator<ToXContent> {
     }
 
     public ChunkedToXContentBuilder field(String name, ToXContent value) {
-        addChunk((b, p) -> b.field(name, value, params));
+        addChunk((b, p) -> b.field(name, value, p));
         return this;
     }
 

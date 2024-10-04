74905eabe73fa8f0a33ae0a8467a75420a8a79e9
matthewabbott <ttobbatam@gmail.com>
2024-10-04 13:38:26 -0700

    Added max_retry docs explanation and linked to the docs in allocation explanation.

--- server/src/main/java/org/elasticsearch/cluster/routing/allocation/decider/MaxRetryAllocationDecider.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/allocation/decider/MaxRetryAllocationDecider.java
@@ -14,6 +14,7 @@ import org.elasticsearch.cluster.routing.RoutingNode;
 import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.cluster.routing.UnassignedInfo;
 import org.elasticsearch.cluster.routing.allocation.RoutingAllocation;
+import org.elasticsearch.common.ReferenceDocs;
 import org.elasticsearch.common.settings.Setting;
 
 /**
@@ -72,7 +73,9 @@ public class MaxRetryAllocationDecider extends AllocationDecider {
             return Decision.single(
                 Decision.Type.NO,
                 NAME,
-                "shard has exceeded the maximum number of retries [%d] on failed allocation attempts - manually call [%s] to retry, [%s]",
+                "shard has exceeded the maximum number of retries [%d] on failed allocation attempts - " +
+                    "manually call [%s] to retry, and for more information, see [%s] [%s]",
+                ReferenceDocs.ALLOCATION_EXPLAIN_MAX_RETRY,
                 maxRetries,
                 RETRY_FAILED_API,
                 info.toString()

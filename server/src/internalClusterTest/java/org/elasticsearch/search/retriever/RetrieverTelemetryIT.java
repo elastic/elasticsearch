88b93842c8508a6686a18a54f8d854040624d09f
Panagiotis Bailis <pmpailis@gmail.com>
2024-10-04 13:27:16 +0300

    iter

--- server/src/internalClusterTest/java/org/elasticsearch/search/retriever/RetrieverTelemetryIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/search/retriever/RetrieverTelemetryIT.java
@@ -1,62 +1,43 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-package org.elasticsearch.xpack.rank.rrf;
+package org.elasticsearch.search.retriever;
 
 import org.elasticsearch.action.admin.cluster.node.capabilities.NodesCapabilitiesRequest;
 import org.elasticsearch.action.admin.cluster.node.capabilities.NodesCapabilitiesResponse;
 import org.elasticsearch.action.admin.cluster.stats.SearchUsageStats;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.common.Strings;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.search.retriever.CompoundRetrieverBuilder;
-import org.elasticsearch.search.retriever.KnnRetrieverBuilder;
-import org.elasticsearch.search.retriever.StandardRetrieverBuilder;
 import org.elasticsearch.search.vectors.KnnSearchBuilder;
 import org.elasticsearch.search.vectors.KnnVectorQueryBuilder;
 import org.elasticsearch.test.ESIntegTestCase;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentFactory;
-import org.elasticsearch.xpack.core.XPackPlugin;
 import org.junit.Before;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.hamcrest.Matchers.equalTo;
 
 @ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST, numDataNodes = 0)
-public class RRFRetrieverTelemetryIT extends ESIntegTestCase {
+public class RetrieverTelemetryIT extends ESIntegTestCase {
 
     private static final String INDEX_NAME = "test_index";
 
     @Override
     protected boolean addMockHttpTransport() {
         return false; // enable http
-    }
-
-    @Override
-    protected Collection<Class<? extends Plugin>> nodePlugins() {
-        return List.of(RRFRankPlugin.class, XPackPlugin.class);
-    }
-
-    @Override
-    protected Settings nodeSettings(int nodeOrdinal, Settings otherSettings) {
-        return Settings.builder()
-            .put(super.nodeSettings(nodeOrdinal, otherSettings))
-            .put("xpack.license.self_generated.type", "trial")
-            .build();
     }
 
     @Before
@@ -134,32 +115,7 @@ public class RRFRetrieverTelemetryIT extends ESIntegTestCase {
             getRestClient().performRequest(request);
         }
 
-        // search#5 - this will record 1 entry for "retriever" in `sections`, and 1 for "rrf" under `retrievers`, as well as
-        // 1 for "knn" and 1 for "standard" under `retrievers`, and eventually 1 for "match" under `queries`
-        {
-            Request request = new Request("GET", INDEX_NAME + "/_search");
-            SearchSourceBuilder source = new SearchSourceBuilder();
-            source.retriever(
-                new RRFRetrieverBuilder(
-                    Arrays.asList(
-                        new CompoundRetrieverBuilder.RetrieverSource(
-                            new KnnRetrieverBuilder("vector", new float[]{1.0f}, null, 10, 15, null),
-                            null
-                        ),
-                        new CompoundRetrieverBuilder.RetrieverSource(
-                            new StandardRetrieverBuilder(QueryBuilders.matchQuery("text", "foo")),
-                            null
-                        )
-                    ),
-                    10,
-                    10
-                )
-            );
-            request.setJsonEntity(Strings.toString(source));
-            getRestClient().performRequest(request);
-        }
-
-        // search#6 - this will record 1 entry for "knn" in `sections`
+        // search#5 - this will record 1 entry for "knn" in `sections`
         {
             Request request = new Request("GET", INDEX_NAME + "/_search");
             SearchSourceBuilder source = new SearchSourceBuilder();
@@ -168,7 +124,7 @@ public class RRFRetrieverTelemetryIT extends ESIntegTestCase {
             getRestClient().performRequest(request);
         }
 
-        // search#7 - this will record 1 entry for "query" in `sections`, and 1 for "match_all" under `queries`
+        // search#6 - this will record 1 entry for "query" in `sections`, and 1 for "match_all" under `queries`
         {
             Request request = new Request("GET", INDEX_NAME + "/_search");
             SearchSourceBuilder source = new SearchSourceBuilder();
@@ -180,22 +136,20 @@ public class RRFRetrieverTelemetryIT extends ESIntegTestCase {
         // cluster stats
         {
             SearchUsageStats stats = clusterAdmin().prepareClusterStats().get().getIndicesStats().getSearchUsageStats();
-            assertEquals(7, stats.getTotalSearchCount());
+            assertEquals(6, stats.getTotalSearchCount());
 
             assertThat(stats.getSectionsUsage().size(), equalTo(3));
-            assertThat(stats.getSectionsUsage().get("retriever"), equalTo(5L));
+            assertThat(stats.getSectionsUsage().get("retriever"), equalTo(4L));
             assertThat(stats.getSectionsUsage().get("query"), equalTo(1L));
             assertThat(stats.getSectionsUsage().get("knn"), equalTo(1L));
 
             assertThat(stats.getRetrieversUsage().size(), equalTo(3));
-            assertThat(stats.getRetrieversUsage().get("standard"), equalTo(4L));
-            assertThat(stats.getRetrieversUsage().get("knn"), equalTo(2L));
-            assertThat(stats.getRetrieversUsage().get("rrf"), equalTo(1L));
+            assertThat(stats.getRetrieversUsage().get("standard"), equalTo(3L));
+            assertThat(stats.getRetrieversUsage().get("knn"), equalTo(1L));
 
             assertThat(stats.getQueryUsage().size(), equalTo(5));
             assertThat(stats.getQueryUsage().get("range"), equalTo(1L));
             assertThat(stats.getQueryUsage().get("term"), equalTo(1L));
-            assertThat(stats.getQueryUsage().get("match"), equalTo(1L));
             assertThat(stats.getQueryUsage().get("match_all"), equalTo(1L));
             assertThat(stats.getQueryUsage().get("knn"), equalTo(1L));
         }

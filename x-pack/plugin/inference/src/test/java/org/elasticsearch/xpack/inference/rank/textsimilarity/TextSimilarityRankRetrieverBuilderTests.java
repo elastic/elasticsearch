4ecc5bd53e473174523fdb9c38655e326408e313
Panagiotis Bailis <pmpailis@gmail.com>
2024-10-08 10:15:10 +0300

    Text_similarity_reranker retriever rework to be evaluated during rewrite phase (#114085)

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/rank/textsimilarity/TextSimilarityRankRetrieverBuilderTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/rank/textsimilarity/TextSimilarityRankRetrieverBuilderTests.java
@@ -9,17 +9,9 @@ package org.elasticsearch.xpack.inference.rank.textsimilarity;
 
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.common.Strings;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.MatchAllQueryBuilder;
-import org.elasticsearch.index.query.MatchNoneQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryRewriteContext;
-import org.elasticsearch.index.query.RandomQueryBuilder;
-import org.elasticsearch.index.query.RangeQueryBuilder;
-import org.elasticsearch.index.query.Rewriteable;
 import org.elasticsearch.index.query.TermQueryBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.search.builder.SubSearchSourceBuilder;
 import org.elasticsearch.search.retriever.RetrieverBuilder;
 import org.elasticsearch.search.retriever.RetrieverParserContext;
 import org.elasticsearch.search.retriever.TestRetrieverBuilder;
@@ -38,10 +30,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.elasticsearch.search.rank.RankBuilder.DEFAULT_RANK_WINDOW_SIZE;
-import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
-import static org.mockito.Mockito.mock;
 
 public class TextSimilarityRankRetrieverBuilderTests extends AbstractXContentTestCase<TextSimilarityRankRetrieverBuilder> {
 
@@ -82,6 +72,7 @@ public class TextSimilarityRankRetrieverBuilderTests extends AbstractXContentTes
                 new SearchUsage(),
                 nf -> nf == RetrieverBuilder.RETRIEVERS_SUPPORTED
                     || nf == TextSimilarityRankRetrieverBuilder.TEXT_SIMILARITY_RERANKER_RETRIEVER_SUPPORTED
+                    || nf == TextSimilarityRankRetrieverBuilder.TEXT_SIMILARITY_RERANKER_COMPOSITION_SUPPORTED
             )
         );
     }
@@ -131,86 +122,6 @@ public class TextSimilarityRankRetrieverBuilderTests extends AbstractXContentTes
         }
     }
 
-    public void testRewriteInnerRetriever() throws IOException {
-        final boolean[] rewritten = { false };
-        List<QueryBuilder> preFilterQueryBuilders = new ArrayList<>();
-        if (randomBoolean()) {
-            for (int i = 0; i < randomIntBetween(1, 5); i++) {
-                preFilterQueryBuilders.add(RandomQueryBuilder.createQuery(random()));
-            }
-        }
-        RetrieverBuilder innerRetriever = new TestRetrieverBuilder("top-level-retriever") {
-            @Override
-            public RetrieverBuilder rewrite(QueryRewriteContext ctx) throws IOException {
-                if (randomBoolean()) {
-                    return this;
-                }
-                rewritten[0] = true;
-                return new TestRetrieverBuilder("nested-rewritten-retriever") {
-                    @Override
-                    public void extractToSearchSourceBuilder(SearchSourceBuilder searchSourceBuilder, boolean compoundUsed) {
-                        if (preFilterQueryBuilders.isEmpty() == false) {
-                            BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder();
-
-                            for (QueryBuilder preFilterQueryBuilder : preFilterQueryBuilders) {
-                                boolQueryBuilder.filter(preFilterQueryBuilder);
-                            }
-                            boolQueryBuilder.must(new RangeQueryBuilder("some_field"));
-                            searchSourceBuilder.subSearches().add(new SubSearchSourceBuilder(boolQueryBuilder));
-                        } else {
-                            searchSourceBuilder.subSearches().add(new SubSearchSourceBuilder(new RangeQueryBuilder("some_field")));
-                        }
-                    }
-                };
-            }
-
-            @Override
-            public void extractToSearchSourceBuilder(SearchSourceBuilder searchSourceBuilder, boolean compoundUsed) {
-                if (preFilterQueryBuilders.isEmpty() == false) {
-                    BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder();
-
-                    for (QueryBuilder preFilterQueryBuilder : preFilterQueryBuilders) {
-                        boolQueryBuilder.filter(preFilterQueryBuilder);
-                    }
-                    boolQueryBuilder.must(new TermQueryBuilder("field", "value"));
-                    searchSourceBuilder.subSearches().add(new SubSearchSourceBuilder(boolQueryBuilder));
-                } else {
-                    searchSourceBuilder.subSearches().add(new SubSearchSourceBuilder(new TermQueryBuilder("field", "value")));
-                }
-            }
-        };
-        TextSimilarityRankRetrieverBuilder textSimilarityRankRetrieverBuilder = createRandomTextSimilarityRankRetrieverBuilder(
-            innerRetriever
-        );
-        textSimilarityRankRetrieverBuilder.getPreFilterQueryBuilders().addAll(preFilterQueryBuilders);
-        SearchSourceBuilder source = new SearchSourceBuilder().retriever(textSimilarityRankRetrieverBuilder);
-        QueryRewriteContext queryRewriteContext = mock(QueryRewriteContext.class);
-        source = Rewriteable.rewrite(source, queryRewriteContext);
-        assertNull(source.retriever());
-        if (false == preFilterQueryBuilders.isEmpty()) {
-            if (source.query() instanceof MatchAllQueryBuilder == false && source.query() instanceof MatchNoneQueryBuilder == false) {
-                assertThat(source.query(), instanceOf(BoolQueryBuilder.class));
-                BoolQueryBuilder bq = (BoolQueryBuilder) source.query();
-                assertFalse(bq.must().isEmpty());
-                assertThat(bq.must().size(), equalTo(1));
-                if (rewritten[0]) {
-                    assertThat(bq.must().get(0), instanceOf(RangeQueryBuilder.class));
-                } else {
-                    assertThat(bq.must().get(0), instanceOf(TermQueryBuilder.class));
-                }
-                for (int j = 0; j < bq.filter().size(); j++) {
-                    assertEqualQueryOrMatchAllNone(bq.filter().get(j), preFilterQueryBuilders.get(j));
-                }
-            }
-        } else {
-            if (rewritten[0]) {
-                assertThat(source.query(), instanceOf(RangeQueryBuilder.class));
-            } else {
-                assertThat(source.query(), instanceOf(TermQueryBuilder.class));
-            }
-        }
-    }
-
     public void testTextSimilarityRetrieverParsing() throws IOException {
         String restContent = "{"
             + "  \"retriever\": {"
@@ -250,29 +161,6 @@ public class TextSimilarityRankRetrieverBuilderTests extends AbstractXContentTes
         }
     }
 
-    public void testIsCompound() {
-        RetrieverBuilder compoundInnerRetriever = new TestRetrieverBuilder(ESTestCase.randomAlphaOfLengthBetween(5, 10)) {
-            @Override
-            public boolean isCompound() {
-                return true;
-            }
-        };
-        RetrieverBuilder nonCompoundInnerRetriever = new TestRetrieverBuilder(ESTestCase.randomAlphaOfLengthBetween(5, 10)) {
-            @Override
-            public boolean isCompound() {
-                return false;
-            }
-        };
-        TextSimilarityRankRetrieverBuilder compoundTextSimilarityRankRetrieverBuilder = createRandomTextSimilarityRankRetrieverBuilder(
-            compoundInnerRetriever
-        );
-        assertTrue(compoundTextSimilarityRankRetrieverBuilder.isCompound());
-        TextSimilarityRankRetrieverBuilder nonCompoundTextSimilarityRankRetrieverBuilder = createRandomTextSimilarityRankRetrieverBuilder(
-            nonCompoundInnerRetriever
-        );
-        assertFalse(nonCompoundTextSimilarityRankRetrieverBuilder.isCompound());
-    }
-
     public void testTopDocsQuery() {
         RetrieverBuilder innerRetriever = new TestRetrieverBuilder(ESTestCase.randomAlphaOfLengthBetween(5, 10)) {
             @Override
@@ -281,11 +169,6 @@ public class TextSimilarityRankRetrieverBuilderTests extends AbstractXContentTes
             }
         };
         TextSimilarityRankRetrieverBuilder retriever = createRandomTextSimilarityRankRetrieverBuilder(innerRetriever);
-        assertThat(retriever.topDocsQuery(), instanceOf(TermQueryBuilder.class));
+        expectThrows(IllegalStateException.class, "Should not be called, missing a rewrite?", retriever::topDocsQuery);
     }
-
-    private static void assertEqualQueryOrMatchAllNone(QueryBuilder actual, QueryBuilder expected) {
-        assertThat(actual, anyOf(instanceOf(MatchAllQueryBuilder.class), instanceOf(MatchNoneQueryBuilder.class), equalTo(expected)));
-    }
-
 }

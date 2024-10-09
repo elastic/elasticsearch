d93212d3e0aa64c8cbd52e56e0d7296fe1c51e9c
Yang Wang <yang.wang@elastic.co>
2024-10-09 14:10:22 +1100

    npe

--- server/src/main/java/org/elasticsearch/action/support/PlainActionFuture.java
+++ server/src/main/java/org/elasticsearch/action/support/PlainActionFuture.java
@@ -39,7 +39,7 @@ public class PlainActionFuture<T> implements ActionFuture<T>, ActionListener<T> 
 
     @Override
     public void onFailure(Exception e) {
-        assert EsExecutors.executorName(Thread.currentThread()).equals(ThreadPool.Names.GENERIC) || assertCompleteAllowed();
+        assert ThreadPool.Names.GENERIC.equals(EsExecutors.executorName(Thread.currentThread())) || assertCompleteAllowed();
         if (sync.setException(Objects.requireNonNull(e))) {
             done(false);
         }

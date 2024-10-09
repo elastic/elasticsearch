d4ca8cd0833e45448f1d3639af61ceb94676d164
Pat Whelan <pat.whelan@elastic.co>
2024-10-09 15:53:35 -0400

    [ML] Stream Bedrock Completion
    Notes:
    - Adds a new API to the chatCompletionRequest to invoke the Bedrock
      Stream API
    - Create a StreamingChatProcessor that subscribes to streaming results
      from bedrock and handles the parsing on another thread.
    - There was no good way (that I could see) to extend the Provider-based
      CompletionRequestEntity, so they have been flattened into one
      RequestEntity that can be shared between ConverseRequest and
      ConverseStreamRequest.

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/request/amazonbedrock/completion/AmazonBedrockConverseUtils.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/request/amazonbedrock/completion/AmazonBedrockConverseUtils.java
@@ -8,9 +8,14 @@
 package org.elasticsearch.xpack.inference.external.request.amazonbedrock.completion;
 
 import software.amazon.awssdk.services.bedrockruntime.model.ContentBlock;
+import software.amazon.awssdk.services.bedrockruntime.model.InferenceConfiguration;
 import software.amazon.awssdk.services.bedrockruntime.model.Message;
 
+import org.elasticsearch.core.Nullable;
+import org.elasticsearch.core.Strings;
+
 import java.util.List;
+import java.util.Optional;
 
 import static org.elasticsearch.xpack.inference.external.request.amazonbedrock.completion.AmazonBedrockChatCompletionRequest.USER_ROLE;
 
@@ -21,5 +26,33 @@ public final class AmazonBedrockConverseUtils {
             .map(text -> ContentBlock.builder().text(text).build())
             .map(content -> Message.builder().role(USER_ROLE).content(content).build())
             .toList();
+    }
+
+    public static Optional<InferenceConfiguration> inferenceConfig(AmazonBedrockConverseRequestEntity request) {
+        if (request.temperature() != null || request.topP() != null || request.maxTokenCount() != null) {
+            var builder = InferenceConfiguration.builder();
+            if (request.temperature() != null) {
+                builder.temperature(request.temperature().floatValue());
+            }
+
+            if (request.topP() != null) {
+                builder.topP(request.topP().floatValue());
+            }
+
+            if (request.maxTokenCount() != null) {
+                builder.maxTokens(request.maxTokenCount());
+            }
+            return Optional.of(builder.build());
+        }
+        return Optional.empty();
+    }
+
+    @Nullable
+    public static List<String> additionalTopK(@Nullable Double topK) {
+        if (topK == null) {
+            return null;
+        }
+
+        return List.of(Strings.format("{\"top_k\":%f}", topK.floatValue()));
     }
 }

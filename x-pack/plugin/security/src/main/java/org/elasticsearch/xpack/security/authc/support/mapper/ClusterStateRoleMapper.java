elastic/elasticsearch

commit 768a001543ce4ef102f9a4b34e1da395b99bd27a

Cluster-state based Security role mapper (#107410)
This implements a new UserRoleMapper that sources the role mapping rules from the cluster state.
The role mapping rules are stored under a new custom cluster state that is persisted (both disk and snapshots).
The role mapper refreshes realm caches when role mapping changes are published.
The role mapper is disabled by default, and it can only be enabled from code, by other plugins.
When enabled, the cluster state role mappings rules, if any, are additive to the rules from
the index-based native role mapping store and the file-based DN one.

x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/support/mapper/ClusterStateRoleMapper.java
@@ -1,0 +1,91 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.security.authc.support.mapper;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.cluster.ClusterChangedEvent;
+import org.elasticsearch.cluster.ClusterStateListener;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.script.ScriptService;
+import org.elasticsearch.xpack.core.security.authc.support.UserRoleMapper;
+import org.elasticsearch.xpack.core.security.authc.support.mapper.ExpressionRoleMapping;
+import org.elasticsearch.xpack.core.security.authz.RoleMappingMetadata;
+
+import java.util.Objects;
+import java.util.Set;
+
+import static org.elasticsearch.xpack.core.security.SecurityExtension.SecurityComponents;
+
+/**
+ * A role mapper the reads the role mapping rules (i.e. {@link ExpressionRoleMapping}s) from the cluster state
+ * (i.e. {@link RoleMappingMetadata}). This is not enabled by default.
+ */
+public final class ClusterStateRoleMapper extends AbstractRoleMapperClearRealmCache implements ClusterStateListener {
+
+    /**
+     * This setting is never registered by the xpack security plugin - in order to enable the
+     * cluster-state based role mapper another plugin must register it as a boolean setting
+     * and set it to `true`.
+     * If this setting is set to <code>true</code> then:
+     * <ul>
+     *     <li>Realms that make use role mappings (all realms but file and native) will,
+     *          in addition, observe the role mappings set in the cluster state.</li>
+     *     <li>Similarly, xpack security's {@link SecurityComponents} extensions will,
+     *          additionally, observe the cluster state role mappings too.</li>
+     *     <li>{@link UserRoleMapper} class will be guice-bound to a {@link CompositeRoleMapper}
+     *          of the {@link NativeRoleMappingStore} and this mapper.</li>
+     * </ul>
+     */
+    public static final String CLUSTER_STATE_ROLE_MAPPINGS_ENABLED = "xpack.security.authc.cluster_state_role_mappings.enabled";
+    private static final Logger logger = LogManager.getLogger(ClusterStateRoleMapper.class);
+
+    private final ScriptService scriptService;
+    private final ClusterService clusterService;
+    private final boolean enabled;
+
+    public ClusterStateRoleMapper(Settings settings, ScriptService scriptService, ClusterService clusterService) {
+        this.scriptService = scriptService;
+        this.clusterService = clusterService;
+        // this role mapper is disabled by default and only code in other plugins can enable it
+        this.enabled = settings.getAsBoolean(CLUSTER_STATE_ROLE_MAPPINGS_ENABLED, false);
+        if (this.enabled) {
+            clusterService.addListener(this);
+        }
+    }
+
+    @Override
+    public void resolveRoles(UserData user, ActionListener<Set<String>> listener) {
+        listener.onResponse(ExpressionRoleMapping.resolveRoles(user, getMappings(), scriptService, logger));
+    }
+
+    @Override
+    public void clusterChanged(ClusterChangedEvent event) {
+        // The cluster state (which contains the new role mappings) is already applied when this listener is called,
+        // such that {@link #resolveRoles} will be returning the new role mappings when called after this is called
+        if (enabled
+            && false == Objects.equals(
+                RoleMappingMetadata.getFromClusterState(event.previousState()),
+                RoleMappingMetadata.getFromClusterState(event.state())
+            )) {
+            // trigger realm cache clear, even if only disabled role mappings have changed
+            // ideally disabled role mappings should not be published in the cluster state
+            clearRealmCachesOnLocalNode();
+        }
+    }
+
+    private Set<ExpressionRoleMapping> getMappings() {
+        if (enabled == false) {
+            return Set.of();
+        } else {
+            return RoleMappingMetadata.getFromClusterState(clusterService.state()).getRoleMappings();
+        }
+    }
+}

x-pack/plugin/security/src/test/java/org/elasticsearch/test/SecuritySettingsSource.java
@@ -14,6 +14,7 @@ import org.elasticsearch.common.network.NetworkModule;
 import org.elasticsearch.common.settings.MockSecureSettings;
 import org.elasticsearch.common.settings.SecureSettings;
 import org.elasticsearch.common.settings.SecureString;
+import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.mapper.extras.MapperExtrasPlugin;
@@ -190,7 +191,8 @@ public class SecuritySettingsSource extends NodeConfigurationSource {
             InternalSettingsPlugin.class,
             MapperExtrasPlugin.class,
             MainRestPlugin.class,
-            Wildcard.class
+            Wildcard.class,
+            UnregisteredSecuritySettingsPlugin.class
         );
     }
 
@@ -389,5 +391,32 @@ public class SecuritySettingsSource extends NodeConfigurationSource {
 
     public boolean isSslEnabled() {
         return sslEnabled;
+    }
+
+    // This plugin registers various normally unregistered settings such that dependent code can be tested.
+    public static class UnregisteredSecuritySettingsPlugin extends Plugin {
+
+        public static final Setting<Boolean> NATIVE_ROLE_MAPPINGS_SETTING = Setting.boolSetting(
+            "xpack.security.authc.native_role_mappings.enabled",
+            true,
+            Setting.Property.NodeScope
+        );
+        public static final Setting<Boolean> CLUSTER_STATE_ROLE_MAPPINGS_ENABLED = Setting.boolSetting(
+            "xpack.security.authc.cluster_state_role_mappings.enabled",
+            false,
+            Setting.Property.NodeScope
+        );
+        public static final Setting<Boolean> NATIVE_ROLES_ENABLED = Setting.boolSetting(
+            "xpack.security.authc.native_roles.enabled",
+            true,
+            Setting.Property.NodeScope
+        );
+
+        public UnregisteredSecuritySettingsPlugin() {}
+
+        @Override
+        public List<Setting<?>> getSettings() {
+            return List.of(NATIVE_ROLE_MAPPINGS_SETTING, CLUSTER_STATE_ROLE_MAPPINGS_ENABLED, NATIVE_ROLES_ENABLED);
+        }
     }
 }

x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authc/support/mapper/ClusterStateRoleMapperTests.java
@@ -1,0 +1,166 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.security.authc.support.mapper;
+
+import org.elasticsearch.action.support.PlainActionFuture;
+import org.elasticsearch.cluster.ClusterChangedEvent;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.script.ScriptModule;
+import org.elasticsearch.script.ScriptService;
+import org.elasticsearch.script.mustache.MustacheScriptEngine;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.xpack.core.security.authc.support.CachingRealm;
+import org.elasticsearch.xpack.core.security.authc.support.UserRoleMapper;
+import org.elasticsearch.xpack.core.security.authc.support.mapper.ExpressionRoleMapping;
+import org.elasticsearch.xpack.core.security.authc.support.mapper.expressiondsl.ExpressionModel;
+import org.elasticsearch.xpack.core.security.authc.support.mapper.expressiondsl.RoleMapperExpression;
+import org.elasticsearch.xpack.core.security.authz.RoleMappingMetadata;
+import org.junit.Before;
+
+import java.util.Collections;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+public class ClusterStateRoleMapperTests extends ESTestCase {
+
+    private ScriptService scriptService;
+    private ClusterService clusterService;
+    private Settings enabledSettings;
+    private Settings disabledSettings;
+
+    @Before
+    public void setup() {
+        scriptService = new ScriptService(
+            Settings.EMPTY,
+            Collections.singletonMap(MustacheScriptEngine.NAME, new MustacheScriptEngine()),
+            ScriptModule.CORE_CONTEXTS,
+            () -> 1L
+        );
+        clusterService = mock(ClusterService.class);
+        enabledSettings = Settings.builder().put("xpack.security.authc.cluster_state_role_mappings.enabled", true).build();
+        if (randomBoolean()) {
+            disabledSettings = Settings.builder().put("xpack.security.authc.cluster_state_role_mappings.enabled", false).build();
+        } else {
+            // the cluster state role mapper is disabled by default
+            disabledSettings = Settings.EMPTY;
+        }
+    }
+
+    public void testRegisterForClusterChangesIfEnabled() {
+        ClusterStateRoleMapper roleMapper = new ClusterStateRoleMapper(enabledSettings, scriptService, clusterService);
+        verify(clusterService, times(1)).addListener(same(roleMapper));
+    }
+
+    public void testNoRegisterForClusterChangesIfNotEnabled() {
+        new ClusterStateRoleMapper(disabledSettings, scriptService, clusterService);
+        verifyNoInteractions(clusterService);
+    }
+
+    public void testRoleResolving() throws Exception {
+        UserRoleMapper.UserData userData = mock(UserRoleMapper.UserData.class);
+        ExpressionModel expressionModel = mock(ExpressionModel.class);
+        when(userData.asModel()).thenReturn(expressionModel);
+        ExpressionRoleMapping mapping1 = mockExpressionRoleMapping(false, Set.of("role1"), expressionModel);
+        ExpressionRoleMapping mapping2 = mockExpressionRoleMapping(true, Set.of("role2"));
+        ExpressionRoleMapping mapping3 = mockExpressionRoleMapping(true, Set.of("role3"), expressionModel);
+        RoleMappingMetadata roleMappingMetadata = new RoleMappingMetadata(Set.of(mapping1, mapping2, mapping3));
+        ClusterState state = roleMappingMetadata.updateClusterState(ClusterState.builder(new ClusterName("elasticsearch")).build());
+        when(clusterService.state()).thenReturn(state);
+        {
+            // the role mapper is enabled
+            ClusterStateRoleMapper roleMapper = new ClusterStateRoleMapper(enabledSettings, scriptService, clusterService);
+            PlainActionFuture<Set<String>> future = new PlainActionFuture<>();
+            roleMapper.resolveRoles(userData, future);
+            Set<String> roleNames = future.get();
+            assertThat(roleNames, contains("role3"));
+            verify(mapping1).isEnabled();
+            verify(mapping2).isEnabled();
+            verify(mapping3).isEnabled();
+            verify(mapping2).getExpression();
+            verify(mapping3).getExpression();
+            verify(mapping3).getRoleNames(same(scriptService), same(expressionModel));
+            verifyNoMoreInteractions(mapping1, mapping2, mapping3);
+        }
+        {
+            // but if the role mapper is disabled, NO roles are resolved
+            ClusterStateRoleMapper roleMapper = new ClusterStateRoleMapper(disabledSettings, scriptService, clusterService);
+            PlainActionFuture<Set<String>> future = new PlainActionFuture<>();
+            roleMapper.resolveRoles(userData, future);
+            Set<String> roleNames = future.get();
+            assertThat(roleNames, empty());
+            verifyNoMoreInteractions(mapping1, mapping2, mapping3);
+        }
+    }
+
+    public void testRoleMappingChangesTriggerRealmCacheClear() {
+        CachingRealm mockRealm = mock(CachingRealm.class);
+        ClusterStateRoleMapper roleMapper = new ClusterStateRoleMapper(enabledSettings, scriptService, clusterService);
+        roleMapper.clearRealmCacheOnChange(mockRealm);
+        ExpressionRoleMapping mapping1 = mockExpressionRoleMapping(true, Set.of("role"), mock(ExpressionModel.class));
+        ExpressionModel model2 = mock(ExpressionModel.class);
+        ExpressionRoleMapping mapping2 = mockExpressionRoleMapping(true, Set.of("role"), model2);
+        ExpressionRoleMapping mapping3 = mockExpressionRoleMapping(true, Set.of("role3"), model2);
+        ClusterState emptyState = ClusterState.builder(new ClusterName("elasticsearch")).build();
+        RoleMappingMetadata roleMappingMetadata1 = new RoleMappingMetadata(Set.of(mapping1));
+        ClusterState state1 = roleMappingMetadata1.updateClusterState(emptyState);
+        roleMapper.clusterChanged(new ClusterChangedEvent("test", emptyState, state1));
+        verify(mockRealm, times(1)).expireAll();
+        RoleMappingMetadata roleMappingMetadata2 = new RoleMappingMetadata(Set.of(mapping2));
+        ClusterState state2 = roleMappingMetadata2.updateClusterState(state1);
+        roleMapper.clusterChanged(new ClusterChangedEvent("test", state1, state2));
+        verify(mockRealm, times(2)).expireAll();
+        RoleMappingMetadata roleMappingMetadata3 = new RoleMappingMetadata(Set.of(mapping3));
+        ClusterState state3 = roleMappingMetadata3.updateClusterState(state2);
+        roleMapper.clusterChanged(new ClusterChangedEvent("test", state2, state3));
+        verify(mockRealm, times(3)).expireAll();
+        RoleMappingMetadata roleMappingMetadata4 = new RoleMappingMetadata(Set.of(mapping2, mapping3));
+        ClusterState state4 = roleMappingMetadata4.updateClusterState(state3);
+        roleMapper.clusterChanged(new ClusterChangedEvent("test", state3, state4));
+        verify(mockRealm, times(4)).expireAll();
+    }
+
+    private ExpressionRoleMapping mockExpressionRoleMapping(boolean enabled, Set<String> roleNames, ExpressionModel... matchingModels) {
+        ExpressionRoleMapping mapping = mock(ExpressionRoleMapping.class);
+        when(mapping.isEnabled()).thenReturn(enabled);
+        RoleMapperExpression roleMapperExpression = mock(RoleMapperExpression.class);
+        when(mapping.getExpression()).thenReturn(roleMapperExpression);
+        doAnswer(invocation -> {
+            ExpressionModel expressionModel = (ExpressionModel) invocation.getArguments()[0];
+            for (ExpressionModel matchingModel : matchingModels) {
+                if (expressionModel.equals(matchingModel)) {
+                    return true;
+                }
+            }
+            return false;
+        }).when(roleMapperExpression).match(any(ExpressionModel.class));
+        doAnswer(invocation -> {
+            ExpressionModel expressionModel = (ExpressionModel) invocation.getArguments()[1];
+            for (ExpressionModel matchingModel : matchingModels) {
+                if (expressionModel.equals(matchingModel)) {
+                    return roleNames;
+                }
+            }
+            return Set.of();
+        }).when(mapping).getRoleNames(same(scriptService), any(ExpressionModel.class));
+        return mapping;
+    }
+}

server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -185,6 +185,7 @@ public class TransportVersions {
     public static final TransportVersion ESQL_MV_ORDERING_SORTED_ASCENDING = def(8_644_00_0);
     public static final TransportVersion ESQL_PAGE_MAPPING_TO_ITERATOR = def(8_645_00_0);
     public static final TransportVersion BINARY_PIT_ID = def(8_646_00_0);
+    public static final TransportVersion SECURITY_ROLE_MAPPINGS_IN_CLUSTER_STATE = def(8_647_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/XPackClientPlugin.java
@@ -81,6 +81,7 @@ import org.elasticsearch.xpack.core.security.authc.support.mapper.expressiondsl.
 import org.elasticsearch.xpack.core.security.authc.support.mapper.expressiondsl.ExceptExpression;
 import org.elasticsearch.xpack.core.security.authc.support.mapper.expressiondsl.FieldExpression;
 import org.elasticsearch.xpack.core.security.authc.support.mapper.expressiondsl.RoleMapperExpression;
+import org.elasticsearch.xpack.core.security.authz.RoleMappingMetadata;
 import org.elasticsearch.xpack.core.security.authz.privilege.ConfigurableClusterPrivilege;
 import org.elasticsearch.xpack.core.security.authz.privilege.ConfigurableClusterPrivileges;
 import org.elasticsearch.xpack.core.slm.SLMFeatureSetUsage;
@@ -154,6 +155,8 @@ public class XPackClientPlugin extends Plugin implements ActionPlugin, NetworkPl
                 ConfigurableClusterPrivileges.WriteProfileDataPrivileges::createFrom
             ),
             // security : role-mappings
+            new NamedWriteableRegistry.Entry(Metadata.Custom.class, RoleMappingMetadata.TYPE, RoleMappingMetadata::new),
+            new NamedWriteableRegistry.Entry(NamedDiff.class, RoleMappingMetadata.TYPE, RoleMappingMetadata::readDiffFrom),
             new NamedWriteableRegistry.Entry(RoleMapperExpression.class, AllExpression.NAME, AllExpression::new),
             new NamedWriteableRegistry.Entry(RoleMapperExpression.class, AnyExpression.NAME, AnyExpression::new),
             new NamedWriteableRegistry.Entry(RoleMapperExpression.class, FieldExpression.NAME, FieldExpression::new),

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/XPackPlugin.java
@@ -106,6 +106,7 @@ import org.elasticsearch.xpack.core.ml.MlMetadata;
 import org.elasticsearch.xpack.core.rest.action.RestXPackInfoAction;
 import org.elasticsearch.xpack.core.rest.action.RestXPackUsageAction;
 import org.elasticsearch.xpack.core.security.authc.TokenMetadata;
+import org.elasticsearch.xpack.core.security.authz.RoleMappingMetadata;
 import org.elasticsearch.xpack.core.ssl.SSLConfigurationReloader;
 import org.elasticsearch.xpack.core.ssl.SSLService;
 import org.elasticsearch.xpack.core.termsenum.action.TermsEnumAction;
@@ -297,6 +298,7 @@ public class XPackPlugin extends XPackClientPlugin
         return metadata.custom(LicensesMetadata.TYPE) != null
             || metadata.custom(MlMetadata.TYPE) != null
             || metadata.custom(WatcherMetadata.TYPE) != null
+            || RoleMappingMetadata.getFromClusterState(clusterState).isEmpty() == false
             || clusterState.custom(TokenMetadata.TYPE) != null
             || metadata.custom(TransformMetadata.TYPE) != null;
     }

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authc/support/mapper/ExpressionRoleMapping.java
@@ -6,6 +6,7 @@
  */
 package org.elasticsearch.xpack.core.security.authc.support.mapper;
 
+import org.apache.logging.log4j.Logger;
 import org.elasticsearch.TransportVersions;
 import org.elasticsearch.common.ParsingException;
 import org.elasticsearch.common.Strings;
@@ -23,6 +24,7 @@ import org.elasticsearch.xcontent.ToXContentObject;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentParser;
 import org.elasticsearch.xcontent.XContentType;
+import org.elasticsearch.xpack.core.security.authc.support.UserRoleMapper;
 import org.elasticsearch.xpack.core.security.authc.support.mapper.expressiondsl.ExpressionModel;
 import org.elasticsearch.xpack.core.security.authc.support.mapper.expressiondsl.ExpressionParser;
 import org.elasticsearch.xpack.core.security.authc.support.mapper.expressiondsl.RoleMapperExpression;
@@ -30,6 +32,7 @@ import org.elasticsearch.xpack.core.security.authz.RoleDescriptor;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -38,6 +41,8 @@ import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static org.elasticsearch.common.Strings.format;
 
 /**
  * A representation of a single role-mapping for use in NativeRoleMappingStore.
@@ -67,6 +72,32 @@ public class ExpressionRoleMapping implements ToXContentObject, Writeable {
         // skip the doc_type and type fields in case we're parsing directly from the index
         PARSER.declareString(ignored, new ParseField(NativeRoleMappingStoreField.DOC_TYPE_FIELD));
         PARSER.declareString(ignored, new ParseField(UPGRADE_API_TYPE_FIELD));
+    }
+
+    /**
+     * Given the user information (in the form of {@link UserRoleMapper.UserData}) and a collection of {@link ExpressionRoleMapping}s,
+     * this returns the set of role names that should be mapped to the user, according to the provided role mapping rules.
+     */
+    public static Set<String> resolveRoles(
+        UserRoleMapper.UserData user,
+        Collection<ExpressionRoleMapping> mappings,
+        ScriptService scriptService,
+        Logger logger
+    ) {
+        ExpressionModel model = user.asModel();
+        Set<String> roles = mappings.stream()
+            .filter(ExpressionRoleMapping::isEnabled)
+            .filter(m -> m.getExpression().match(model))
+            .flatMap(m -> {
+                Set<String> roleNames = m.getRoleNames(scriptService, model);
+                logger.trace(
+                    () -> format("Applying role-mapping [{}] to user-model [{}] produced role-names [{}]", m.getName(), model, roleNames)
+                );
+                return roleNames.stream();
+            })
+            .collect(Collectors.toSet());
+        logger.debug(() -> format("Mapping user [{}] to roles [{}]", user, roles));
+        return roles;
     }
 
     private final String name;

commit 3e06625fd79edc30fb1fb3a49e56d88297d14249

Default enable cluster state role mapper (#114337)
This PR default-enables cluster-state role mappings as the first part of the mitigation for a regression in ECK introduced by https://github.com/elastic/elasticsearch/pull/107410. 

Prior to this PR, cluster-state role mappings were written to cluster-state, but not read from it. 

With this PR, cluster-state role mappings will be read and used to assign roles to users, i.e. in user role resolution. 

However, they will not be included in the output of the [Get role mappings API](https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-get-role-mapping.html) yet. Exposing them via API is a target for a follow-up fix.

Relates: ES-9628
Supersedes: https://github.com/elastic/elasticsearch/pull/113900

x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/support/mapper/ClusterStateRoleMapper.java
@@ -31,9 +31,9 @@ import static org.elasticsearch.xpack.core.security.SecurityExtension.SecurityCo
 public final class ClusterStateRoleMapper extends AbstractRoleMapperClearRealmCache implements ClusterStateListener {
 
     /**
-     * This setting is never registered by the xpack security plugin - in order to enable the
+     * This setting is never registered by the xpack security plugin - in order to disable the
      * cluster-state based role mapper another plugin must register it as a boolean setting
-     * and set it to `true`.
+     * and set it to `false`.
      * If this setting is set to <code>true</code> then:
      * <ul>
      *     <li>Realms that make use role mappings (all realms but file and native) will,
@@ -54,8 +54,8 @@ public final class ClusterStateRoleMapper extends AbstractRoleMapperClearRealmCa
     public ClusterStateRoleMapper(Settings settings, ScriptService scriptService, ClusterService clusterService) {
         this.scriptService = scriptService;
         this.clusterService = clusterService;
-        // this role mapper is disabled by default and only code in other plugins can enable it
-        this.enabled = settings.getAsBoolean(CLUSTER_STATE_ROLE_MAPPINGS_ENABLED, false);
+        // this role mapper is enabled by default and only code in other plugins can disable it
+        this.enabled = settings.getAsBoolean(CLUSTER_STATE_ROLE_MAPPINGS_ENABLED, true);
         if (this.enabled) {
             clusterService.addListener(this);
         }

x-pack/plugin/security/src/internalClusterTest/java/org/elasticsearch/integration/RoleMappingFileSettingsIT.java
@@ -148,15 +148,6 @@ public class RoleMappingFileSettingsIT extends NativeRealmIntegTestCase {
              }
         }""";
 
-    @Override
-    protected Settings nodeSettings(int nodeOrdinal, Settings otherSettings) {
-        Settings.Builder builder = Settings.builder()
-            .put(super.nodeSettings(nodeOrdinal, otherSettings))
-            // some tests make use of cluster-state based role mappings
-            .put("xpack.security.authc.cluster_state_role_mappings.enabled", true);
-        return builder.build();
-    }
-
     @After
     public void cleanUp() {
         updateClusterSettings(Settings.builder().putNull("indices.recovery.max_bytes_per_sec"));

x-pack/plugin/security/src/internalClusterTest/java/org/elasticsearch/xpack/security/authc/jwt/JwtRoleMappingsIntegTests.java
@@ -78,8 +78,6 @@ public final class JwtRoleMappingsIntegTests extends SecurityIntegTestCase {
     protected Settings nodeSettings(int nodeOrdinal, Settings otherSettings) {
         Settings.Builder builder = Settings.builder()
             .put(super.nodeSettings(nodeOrdinal, otherSettings))
-            // some tests make use of cluster-state based role mappings
-            .put("xpack.security.authc.cluster_state_role_mappings.enabled", true)
             .put(XPackSettings.TOKEN_SERVICE_ENABLED_SETTING.getKey(), randomBoolean())
             // 1st JWT realm
             .put("xpack.security.authc.realms.jwt.jwt0.order", 10)

x-pack/plugin/security/src/test/java/org/elasticsearch/test/SecuritySettingsSource.java
@@ -403,7 +403,7 @@ public class SecuritySettingsSource extends NodeConfigurationSource {
         );
         public static final Setting<Boolean> CLUSTER_STATE_ROLE_MAPPINGS_ENABLED = Setting.boolSetting(
             "xpack.security.authc.cluster_state_role_mappings.enabled",
-            false,
+            true,
             Setting.Property.NodeScope
         );
         public static final Setting<Boolean> NATIVE_ROLES_ENABLED = Setting.boolSetting(

x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authc/support/mapper/ClusterStateRoleMapperTests.java
@@ -56,12 +56,12 @@ public class ClusterStateRoleMapperTests extends ESTestCase {
             () -> 1L
         );
         clusterService = mock(ClusterService.class);
-        enabledSettings = Settings.builder().put("xpack.security.authc.cluster_state_role_mappings.enabled", true).build();
+        disabledSettings = Settings.builder().put("xpack.security.authc.cluster_state_role_mappings.enabled", false).build();
         if (randomBoolean()) {
-            disabledSettings = Settings.builder().put("xpack.security.authc.cluster_state_role_mappings.enabled", false).build();
+            enabledSettings = Settings.builder().put("xpack.security.authc.cluster_state_role_mappings.enabled", true).build();
         } else {
-            // the cluster state role mapper is disabled by default
-            disabledSettings = Settings.EMPTY;
+            // the cluster state role mapper is enabled by default
+            enabledSettings = Settings.EMPTY;
         }
     }
 
c7473ad8cc8f945ffad51ec9a2339a7dde60e05a
Nik Everett <nik9000@gmail.com>
2024-10-09 19:13:44 -0400

    ESQL: Delay construction of warnings (#114368) (#114459)
    Delay construction of `Warnings` until they are needed to save memory
    when evaluating many many many expressions. Most expressions won't use
    warnings at all and there isn't any need to make registering warnings
    super duper fast. So let's make the construction lazy to save a little
    memory. It's like 200 bytes per expression which isn't much, but it's
    possible to have thousands of expressions in a single query. Abusive,
    but possible.
    
    This also consolidates all `Warnings` usages to a single `Warnings`
    class. We had two. We don't need two.

--- x-pack/plugin/esql/src/main/generated-src/org/elasticsearch/xpack/esql/expression/predicate/operator/comparison/InBytesRefEvaluator.java
+++ x-pack/plugin/esql/src/main/generated-src/org/elasticsearch/xpack/esql/expression/predicate/operator/comparison/InBytesRefEvaluator.java
@@ -15,20 +15,20 @@ import org.elasticsearch.compute.data.BytesRefVector;
 import org.elasticsearch.compute.data.Page;
 import org.elasticsearch.compute.operator.DriverContext;
 import org.elasticsearch.compute.operator.EvalOperator;
+import org.elasticsearch.compute.operator.Warnings;
 import org.elasticsearch.core.Releasable;
 import org.elasticsearch.core.Releasables;
 import org.elasticsearch.xpack.esql.core.tree.Source;
-import org.elasticsearch.xpack.esql.expression.function.Warnings;
 
 import java.util.Arrays;
 import java.util.BitSet;
 
 /**
  * {@link EvalOperator.ExpressionEvaluator} implementation for {@link In}.
- * This class is generated. Edit {@code InEvaluator.java.st} instead.
+ * This class is generated. Edit {@code X-InEvaluator.java.st} instead.
  */
 public class InBytesRefEvaluator implements EvalOperator.ExpressionEvaluator {
-    private final Warnings warnings;
+    private final Source source;
 
     private final EvalOperator.ExpressionEvaluator lhs;
 
@@ -36,16 +36,18 @@ public class InBytesRefEvaluator implements EvalOperator.ExpressionEvaluator {
 
     private final DriverContext driverContext;
 
+    private Warnings warnings;
+
     public InBytesRefEvaluator(
         Source source,
         EvalOperator.ExpressionEvaluator lhs,
         EvalOperator.ExpressionEvaluator[] rhs,
         DriverContext driverContext
     ) {
+        this.source = source;
         this.lhs = lhs;
         this.rhs = rhs;
         this.driverContext = driverContext;
-        this.warnings = Warnings.createWarnings(driverContext.warningsMode(), source);
     }
 
     @Override
@@ -90,7 +92,7 @@ public class InBytesRefEvaluator implements EvalOperator.ExpressionEvaluator {
                 }
                 if (lhsBlock.getValueCount(p) != 1) {
                     if (lhsBlock.getValueCount(p) > 1) {
-                        warnings.registerException(new IllegalArgumentException("single-value function encountered multi-value"));
+                        warnings().registerException(new IllegalArgumentException("single-value function encountered multi-value"));
                     }
                     result.appendNull();
                     continue;
@@ -105,7 +107,7 @@ public class InBytesRefEvaluator implements EvalOperator.ExpressionEvaluator {
                     }
                     if (rhsBlocks[i].getValueCount(p) > 1) {
                         mvs.set(i);
-                        warnings.registerException(new IllegalArgumentException("single-value function encountered multi-value"));
+                        warnings().registerException(new IllegalArgumentException("single-value function encountered multi-value"));
                         continue;
                     }
                     int o = rhsBlocks[i].getFirstValueIndex(p);
@@ -157,6 +159,18 @@ public class InBytesRefEvaluator implements EvalOperator.ExpressionEvaluator {
     @Override
     public void close() {
         Releasables.closeExpectNoException(lhs, () -> Releasables.close(rhs));
+    }
+
+    private Warnings warnings() {
+        if (warnings == null) {
+            this.warnings = Warnings.createWarnings(
+                driverContext.warningsMode(),
+                source.source().getLineNumber(),
+                source.source().getColumnNumber(),
+                source.text()
+            );
+        }
+        return warnings;
     }
 
     static class Factory implements EvalOperator.ExpressionEvaluator.Factory {

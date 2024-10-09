28ec176e506cad5e3f3ac3ef5c84b87537ef368a
Kostas Krikellas <kostas.krikellas@elastic.co>
2024-10-09 12:19:14 +0300

    fix tests

--- test/framework/src/main/java/org/elasticsearch/index/mapper/MapperTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/index/mapper/MapperTestCase.java
@@ -1598,7 +1598,7 @@ public abstract class MapperTestCase extends MapperServiceTestCase {
         String expected = Strings.toString(builder);
         String actual = syntheticSource(mapperAll, buildInput);
         // Check for single-element array, the array source is not stored in this case.
-        if (expected.replace("[", "").replace("]", "").equals(actual) == false) {
+        if (expected.contains("[") == false) {
             assertThat(actual, equalTo(expected));
         }
     }

7f0d5de5dc60d0abcb690ccc511ceecc9e8b41f0
Iván Cea Fontenla <ivan.cea@elastic.co>
2024-10-10 16:00:05 +0200

    Use success variable instead of try-catch

--- server/src/main/java/org/elasticsearch/search/aggregations/metrics/TDigestState.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/metrics/TDigestState.java
@@ -196,21 +196,24 @@ public class TDigestState implements Releasable, Accountable {
 
     public static TDigestState read(CircuitBreaker breaker, StreamInput in) throws IOException {
         double compression = in.readDouble();
-        TDigestState state;
+        TDigestState state = null;
         long size = 0;
-        breaker.addEstimateBytesAndMaybeBreak(SHALLOW_SIZE, "tdigest-state-read");
+        boolean success = false;
         try {
-            if (in.getTransportVersion().onOrAfter(TransportVersions.V_8_9_X)) {
-                state = new TDigestState(breaker, Type.valueOf(in.readString()), compression);
-                size = in.readVLong();
-            } else {
-                state = new TDigestState(breaker, Type.valueForHighAccuracy(), compression);
+            breaker.addEstimateBytesAndMaybeBreak(SHALLOW_SIZE, "tdigest-state-read");
+            try {
+                if (in.getTransportVersion().onOrAfter(TransportVersions.V_8_9_X)) {
+                    state = new TDigestState(breaker, Type.valueOf(in.readString()), compression);
+                    size = in.readVLong();
+                } else {
+                    state = new TDigestState(breaker, Type.valueForHighAccuracy(), compression);
+                }
+            } finally {
+                if (state == null) {
+                    breaker.addWithoutBreaking(-SHALLOW_SIZE);
+                }
             }
-        } catch (Exception e) {
-            breaker.addWithoutBreaking(-SHALLOW_SIZE);
-            throw e;
-        }
-        try {
+
             int n = in.readVInt();
             if (size > 0) {
                 state.tdigest.reserve(size);
@@ -218,10 +221,12 @@ public class TDigestState implements Releasable, Accountable {
             for (int i = 0; i < n; i++) {
                 state.add(in.readDouble(), in.readVLong());
             }
+            success = true;
             return state;
-        } catch (Exception e) {
-            Releasables.close(state);
-            throw e;
+        } finally {
+            if (success == false) {
+                Releasables.close(state);
+            }
         }
     }
 

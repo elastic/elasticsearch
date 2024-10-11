657054fb539331b8cca6d8e989c6403bd9aae15b
David Kyle <david.kyle@elastic.co>
2024-10-11 09:01:42 +0100

    better test

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/chunking/SentenceBoundaryChunker.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/chunking/SentenceBoundaryChunker.java
@@ -34,6 +34,7 @@ public class SentenceBoundaryChunker implements Chunker {
     public SentenceBoundaryChunker() {
         sentenceIterator = BreakIterator.getSentenceInstance(Locale.ROOT);
         wordIterator = BreakIterator.getWordInstance(Locale.ROOT);
+
     }
 
     /**
@@ -75,33 +76,46 @@ public class SentenceBoundaryChunker implements Chunker {
         int sentenceStart = 0;
         int chunkWordCount = 0;
 
-        int countWordsInPreceedingSentence = 0;
+        int wordsInPrecedingSentenceCount = 0;
         int previousSentenceStart = 0;
 
         int boundary = sentenceIterator.next();
 
         while (boundary != BreakIterator.DONE) {
             int sentenceEnd = sentenceIterator.current();
-            int countWordsInSentence = countWords(sentenceStart, sentenceEnd);
+            int wordsInSentenceCount = countWords(sentenceStart, sentenceEnd);
 
-            if (chunkWordCount + countWordsInSentence > maxNumberWordsPerChunk) {
+            if (chunkWordCount + wordsInSentenceCount > maxNumberWordsPerChunk) {
                 // over the max chunk size, roll back to the last sentence
 
+                int nextChunkWordCount = wordsInSentenceCount;
                 if (chunkWordCount > 0) {
                     // add a new chunk containing all the input up to this sentence
                     chunks.add(input.substring(chunkStart, chunkEnd));
 
                     if (includePrecedingSentence) {
-                        chunkStart = previousSentenceStart;
-                        chunkWordCount = countWordsInPreceedingSentence + countWordsInSentence;
+                        if (wordsInPrecedingSentenceCount + wordsInSentenceCount > maxNumberWordsPerChunk) {
+                            // cut the last sentence
+                            int numWordsToSkip = numWordsToSkipInPreviousSentence(wordsInPrecedingSentenceCount, maxNumberWordsPerChunk);
+
+                            chunkStart = skipWords(input, previousSentenceStart, numWordsToSkip);
+                            chunkWordCount = (wordsInPrecedingSentenceCount - numWordsToSkip) + wordsInSentenceCount;
+                        } else {
+                            chunkWordCount = wordsInPrecedingSentenceCount + wordsInSentenceCount;
+                            chunkStart = previousSentenceStart;
+                        }
+
+                        nextChunkWordCount = chunkWordCount;
                     } else {
                         chunkStart = chunkEnd;
-                        chunkWordCount = countWordsInSentence; // the next chunk will contain this sentence
+                        chunkWordCount = wordsInSentenceCount; // the next chunk will contain this sentence
                     }
                 }
 
-                if (countWordsInSentence > maxNumberWordsPerChunk) {
-                    // This sentence is bigger than the max chunk size.
+                // Is the next chunk larger than max chunk size?
+                // If so split it
+                if (nextChunkWordCount > maxNumberWordsPerChunk) {
+                    // This sentence (and optional overlap) is bigger than the max chunk size.
                     // Split the sentence on the word boundary
                     var sentenceSplits = splitLongSentence(
                         input.substring(chunkStart, sentenceEnd),
@@ -122,17 +136,16 @@ public class SentenceBoundaryChunker implements Chunker {
                     chunkWordCount = sentenceSplits.get(i).wordCount();
                 }
             } else {
-                chunkWordCount += countWordsInSentence;
+                chunkWordCount += wordsInSentenceCount;
             }
 
-            previousSentenceStart = sentenceStart;
+            if (includePrecedingSentence) {
+                previousSentenceStart = sentenceStart;
+                wordsInPrecedingSentenceCount = wordsInSentenceCount;
+            }
 
             sentenceStart = sentenceEnd;
             chunkEnd = sentenceEnd;
-
-            if (includePrecedingSentence) {
-                countWordsInPreceedingSentence = countWordsInSentence;
-            }
 
             boundary = sentenceIterator.next();
         }
@@ -146,6 +159,45 @@ public class SentenceBoundaryChunker implements Chunker {
 
     static List<WordBoundaryChunker.ChunkPosition> splitLongSentence(String text, int maxNumberOfWords, int overlap) {
         return new WordBoundaryChunker().chunkPositions(text, maxNumberOfWords, overlap);
+    }
+
+    static int numWordsToSkipInPreviousSentence(int wordsInPrecedingSentenceCount, int maxNumberWordsPerChunk) {
+        var maxWordsInOverlap = maxWordsInOverlap(maxNumberWordsPerChunk);
+        if (wordsInPrecedingSentenceCount > maxWordsInOverlap) {
+            return wordsInPrecedingSentenceCount - maxWordsInOverlap;
+        } else {
+            return 0;
+        }
+    }
+
+    static int maxWordsInOverlap(int maxNumberWordsPerChunk) {
+        return Math.min(maxNumberWordsPerChunk / 2, 20);
+    }
+
+    private int skipWords(String input, int start, int numWords) {
+        var itr = BreakIterator.getWordInstance(Locale.ROOT);
+        itr.setText(input);
+        return skipWords(start, numWords, itr);
+    }
+
+    static int skipWords(int start, int numWords, BreakIterator wordIterator) {
+        wordIterator.preceding(start); // start of the current word
+
+        int boundary = wordIterator.current();
+        int wordCount = 0;
+        while (boundary != BreakIterator.DONE && wordCount < numWords) {
+            int wordStatus = wordIterator.getRuleStatus();
+            if (wordStatus != BreakIterator.WORD_NONE) {
+                wordCount++;
+            }
+            boundary = wordIterator.next();
+        }
+
+        if (boundary == BreakIterator.DONE) {
+            return wordIterator.last();
+        } else {
+            return boundary;
+        }
     }
 
     private int countWords(int start, int end) {
@@ -172,6 +224,6 @@ public class SentenceBoundaryChunker implements Chunker {
     }
 
     private static int overlapForChunkSize(int chunkSize) {
-        return (chunkSize - 1) / 2;
+        return Math.min(20, (chunkSize - 1) / 2);
     }
 }

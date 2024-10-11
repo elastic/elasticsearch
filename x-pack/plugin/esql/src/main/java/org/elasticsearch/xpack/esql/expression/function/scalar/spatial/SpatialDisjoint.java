c7c764d7ca862dc6c503dc5627f8f46e04f1ce67
Craig Taverner <craig@amanzi.com>
2024-10-11 14:13:36 +0200

    Enable pushing Sort/Filter by ReferenceAttribute down to Lucene, and thereby optimize Sort by ST_DISTANCE (#112938)
    The ST_DISTANCE function added in #108764 was optimized for lucene pushdown in a series of followup PRs, but this did not include sorting by distance. Now this is resolved, for two key scenarios, both known to be valued by users:
    
    * Sorting by distance:
        `FROM index | EVAL distance=ST_DISTANCE(field, literal) | SORT distance`
    * Sorting and filtering by distance:
        `FROM index | EVAL distance=ST_DISTANCE(field, literal) | WHERE distance < literal | SORT distance`
    
    The key changes required to make this work:
    * Add to the EsQueryExec the appropriate sort->_geo_distance sort type
    * Enhance PushTopNToSource to understand how to pushdown the sort even when there is an EVAL in between the FROM and the SORT (between the TopNExec and the EsQueryExec in the physical plan).
    * Enhance PushFiltersToSource to understand how to pushdown the filter even when there is an EVAL in between the FROM and the WHERE (between the Filter and the EsQueryExec in the physical plan).
    
    A useful bonus feature of this additional EVAL intelligence is that other, non-spatial cases are now also pushed down. In particular EVALs that are simple aliases are considered and pushed down, for both filtering and sorting.
    
    Local benchmark results, very approximate, but show massive improvements for distanceSort and distanceFilterSort, which relate to the two cases listed above.
    
    Benchmark	Query DSL	ESQL before this PR	ESQL after this PR	Comments
    distanceFilter	10	5	5	Optimized in #109972
    distanceEvalFilter	10	10000	1500	Still slow due to unnecessary EVAL
    distanceSort	150	12000	160	
    distanceFilterSort	20	10000	24	
    
    NOTE: This enables pushing down sorting by any ReferenceAttribute that either refers to a sortable FieldAttribute, or to an StDistance function that itself refers to a suitable FieldAttribute of geo_point type.
    
    ---------
    
    Co-authored-by: Alexander Spies <alexander.spies@elastic.co>

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/spatial/SpatialDisjoint.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/spatial/SpatialDisjoint.java
@@ -23,7 +23,6 @@ import org.elasticsearch.lucene.spatial.CartesianShapeIndexer;
 import org.elasticsearch.lucene.spatial.CoordinateEncoder;
 import org.elasticsearch.lucene.spatial.GeometryDocValueReader;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
-import org.elasticsearch.xpack.esql.core.expression.FieldAttribute;
 import org.elasticsearch.xpack.esql.core.tree.NodeInfo;
 import org.elasticsearch.xpack.esql.core.tree.Source;
 import org.elasticsearch.xpack.esql.core.type.DataType;
@@ -35,7 +34,6 @@ import org.elasticsearch.xpack.esql.expression.function.Param;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import static org.elasticsearch.xpack.esql.core.type.DataType.CARTESIAN_POINT;
 import static org.elasticsearch.xpack.esql.core.type.DataType.CARTESIAN_SHAPE;
@@ -113,10 +111,10 @@ public class SpatialDisjoint extends SpatialRelatesFunction {
     }
 
     @Override
-    public SpatialDisjoint withDocValues(Set<FieldAttribute> attributes) {
+    public SpatialDisjoint withDocValues(boolean foundLeft, boolean foundRight) {
         // Only update the docValues flags if the field is found in the attributes
-        boolean leftDV = leftDocValues || foundField(left(), attributes);
-        boolean rightDV = rightDocValues || foundField(right(), attributes);
+        boolean leftDV = leftDocValues || foundLeft;
+        boolean rightDV = rightDocValues || foundRight;
         return new SpatialDisjoint(source(), left(), right(), leftDV, rightDV);
     }
 

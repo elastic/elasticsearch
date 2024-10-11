e3ff8c26cc735ab16b0d47ead302403af866a66c
Benjamin Trent <ben.w.trent@gmail.com>
2024-10-11 08:35:54 -0400

    Fix dim validation for bit element_type (#114533)
    A silly bug has reared its ugly head. Apparently, our dimension
    validations are predicated on JSON parsing order, that is not good.
    
    So, this commit adjusts the dim validations so that it is an actual
    validation, instead of something that occurs during parsing.
    
    Additionally, I found that our custom formats were not overriding
    `getMaxDimensions` correctly. Typically, and in production, this isn't
    that big of a deal, but I have found it useful to do this for other
    testing purposes (so that we don't have to rely on the perfield codec
    for more direct and advanced testing).
    
    (cherry picked from commit 7942f3eab080b053f97abbf6abef3619d515f32f)

--- server/src/main/java/org/elasticsearch/index/codec/vectors/ES815BitFlatVectorFormat.java
+++ server/src/main/java/org/elasticsearch/index/codec/vectors/ES815BitFlatVectorFormat.java
@@ -17,6 +17,8 @@ import org.apache.lucene.index.SegmentWriteState;
 
 import java.io.IOException;
 
+import static org.elasticsearch.index.mapper.vectors.DenseVectorFieldMapper.MAX_DIMS_COUNT;
+
 public class ES815BitFlatVectorFormat extends KnnVectorsFormat {
 
     static final String NAME = "ES815BitFlatVectorFormat";
@@ -43,5 +45,10 @@ public class ES815BitFlatVectorFormat extends KnnVectorsFormat {
     @Override
     public String toString() {
         return NAME;
+    }
+
+    @Override
+    public int getMaxDimensions(String fieldName) {
+        return MAX_DIMS_COUNT;
     }
 }

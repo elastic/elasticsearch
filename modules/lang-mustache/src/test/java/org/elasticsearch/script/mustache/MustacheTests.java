e90020a62e8cc1f7b8ae8595ed02e7414953b1bf
Simon Cooper <simon.cooper@elastic.co>
2024-10-14 14:36:00 +0100

    Fix license headers

--- modules/lang-mustache/src/test/java/org/elasticsearch/script/mustache/MustacheTests.java
+++ modules/lang-mustache/src/test/java/org/elasticsearch/script/mustache/MustacheTests.java
@@ -7,8 +7,8 @@
  */
 package org.elasticsearch.script.mustache;
 
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentHelper;
 import org.elasticsearch.script.ScriptEngine;
 import org.elasticsearch.script.ScriptException;

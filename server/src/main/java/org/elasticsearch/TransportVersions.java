69b4a9f8ff634db52b7768bde25352463f34359e
Kathleen DeRusso <kathleen.derusso@elastic.co>
2024-10-14 12:55:11 -0400

    Add a query rules tester API call (#114168)
    * Add a query rules tester API call
    
    * Update docs/changelog/114168.yaml
    
    * Wrap client call in async with origin
    
    * Remove unused param
    
    * PR feedback
    
    * Remove redundant test
    
    * CI workaround - add ent-search as ml dependency so it can find node features

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -242,6 +242,7 @@ public class TransportVersions {
     public static final TransportVersion ESQL_CACHED_STRING_SERIALIZATION = def(8_766_00_0);
     public static final TransportVersion CHUNK_SENTENCE_OVERLAP_SETTING_ADDED = def(8_767_00_0);
     public static final TransportVersion OPT_IN_ESQL_CCS_EXECUTION_INFO = def(8_768_00_0);
+    public static final TransportVersion QUERY_RULE_TEST_API = def(8_769_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

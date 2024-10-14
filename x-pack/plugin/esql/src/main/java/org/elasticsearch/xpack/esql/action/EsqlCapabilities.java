14c1c3c1cc8a113ce4cc72ecb9bf1fbab2f729ef
Carlos Delgado <6339205+carlosdelest@users.noreply.github.com>
2024-10-14 17:14:43 +0200

    [8.x] Add ESQL match function (#113374) (#114695)

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -347,6 +347,11 @@ public class EsqlCapabilities {
         QSTR_FUNCTION(true),
 
         /**
+         * MATCH function
+         */
+        MATCH_FUNCTION(true),
+
+        /**
          * Don't optimize CASE IS NOT NULL function by not requiring the fields to be not null as well.
          * https://github.com/elastic/elasticsearch/issues/112704
          */

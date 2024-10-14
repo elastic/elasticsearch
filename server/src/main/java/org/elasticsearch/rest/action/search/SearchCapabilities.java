df898ba4da2c0ca49e6af8b08613013ab5408b48
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2024-10-14 15:52:20 -0400

    fixing backport of search capabilities

---
+++ server/src/main/java/org/elasticsearch/rest/action/search/SearchCapabilities.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+package org.elasticsearch.rest.action.search;
+
+import java.util.Set;
+
+/**
+ * A {@link Set} of "capabilities" supported by the {@link RestSearchAction}.
+ */
+public final class SearchCapabilities {
+
+    private SearchCapabilities() {}
+
+    /** Support synthetic source with `bit` type in `dense_vector` field when `index` is set to `false`. */
+    private static final String BIT_DENSE_VECTOR_SYNTHETIC_SOURCE_CAPABILITY = "bit_dense_vector_synthetic_source";
+
+    public static final Set<String> CAPABILITIES = Set.of(
+        BIT_DENSE_VECTOR_SYNTHETIC_SOURCE_CAPABILITY
+    );
+}

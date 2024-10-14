9207d3237c28d8a0f5997b40904678bddf30acb7
Jim Ferenczi <jim.ferenczi@elastic.co>
2024-10-14 14:16:21 +0100

    spotless

--- server/src/test/java/org/elasticsearch/search/retriever/rankdoc/RankDocsQueryBuilderTests.java
+++ server/src/test/java/org/elasticsearch/search/retriever/rankdoc/RankDocsQueryBuilderTests.java
@@ -199,20 +199,17 @@ public class RankDocsQueryBuilderTests extends AbstractQueryTestCase<RankDocsQue
                 {
                     // A single rank doc in the last segment
                     RankDoc[] singleRankDoc = new RankDoc[1];
-                    singleRankDoc[0] = rankDocs[rankDocs.length-1];
+                    singleRankDoc[0] = rankDocs[rankDocs.length - 1];
                     RankDocsQuery q = new RankDocsQuery(
-                            reader,
-                            singleRankDoc,
-                            new Query[] { NumericDocValuesField.newSlowExactQuery("active", 1) },
-                            new String[1],
-                            false
+                        reader,
+                        singleRankDoc,
+                        new Query[] { NumericDocValuesField.newSlowExactQuery("active", 1) },
+                        new String[1],
+                        false
                     );
                     var topDocsManager = new TopScoreDocCollectorManager(1, null, 0);
                     var col = searcher.search(q, topDocsManager);
-                    assertThat(
-                            col.totalHits.value,
-                            lessThanOrEqualTo((long) (2 + rankDocs.length))
-                    );
+                    assertThat(col.totalHits.value, lessThanOrEqualTo((long) (2 + rankDocs.length)));
                     assertEqualTopDocs(col.scoreDocs, singleRankDoc);
                 }
             }

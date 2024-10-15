581894a0353bd979de59c3eea7e206ef92742388
Carlos Delgado <6339205+carlosdelest@users.noreply.github.com>
2024-10-15 10:22:43 +0200

    Remove snapshot build restriction for match and qstr functions (#114482) (#114793)

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/EsqlFunctionRegistry.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/EsqlFunctionRegistry.java
@@ -386,18 +386,17 @@ public class EsqlFunctionRegistry {
                 def(MvSlice.class, MvSlice::new, "mv_slice"),
                 def(MvZip.class, MvZip::new, "mv_zip"),
                 def(MvSum.class, MvSum::new, "mv_sum"),
-                def(Split.class, Split::new, "split") } };
+                def(Split.class, Split::new, "split") },
+            // fulltext functions
+            new FunctionDefinition[] { def(Match.class, Match::new, "match"), def(QueryString.class, QueryString::new, "qstr") } };
 
     }
 
     private static FunctionDefinition[][] snapshotFunctions() {
         return new FunctionDefinition[][] {
             new FunctionDefinition[] {
-                def(Rate.class, Rate::withUnresolvedTimestamp, "rate"),
                 def(Categorize.class, Categorize::new, "categorize"),
-                // Full text functions
-                def(QueryString.class, QueryString::new, "qstr"),
-                def(Match.class, Match::new, "match") } };
+                def(Rate.class, Rate::withUnresolvedTimestamp, "rate") } };
     }
 
     public EsqlFunctionRegistry snapshotRegistry() {

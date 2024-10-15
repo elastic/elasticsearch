f273fdc49d5f1494ca0034e01e2a6ef8b014abd7
Pat Whelan <pat.whelan@elastic.co>
2024-10-14 21:37:00 -0400

    [8.x] [ML] Stream Bedrock Completion (#114732) (#114781)
    * [ML] Stream Bedrock Completion (#114732)
    
    Notes:
    - Adds a new API to the chatCompletionRequest to invoke the Bedrock
      Stream API
    - Create a StreamingChatProcessor that subscribes to streaming results
      from bedrock and handles the parsing on another thread.
    - There was no good way (that I could see) to extend the Provider-based
      CompletionRequestEntity, so they have been flattened into one
      RequestEntity that can be shared between ConverseRequest and
      ConverseStreamRequest.
    
    * Use jdk17 API

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/amazonbedrock/AmazonBedrockService.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/amazonbedrock/AmazonBedrockService.java
@@ -43,6 +43,7 @@ import org.elasticsearch.xpack.inference.services.amazonbedrock.embeddings.Amazo
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.elasticsearch.TransportVersions.ML_INFERENCE_AMAZON_BEDROCK_ADDED;
 import static org.elasticsearch.xpack.inference.services.ServiceUtils.createInvalidModelException;
@@ -258,6 +259,11 @@ public class AmazonBedrockService extends SenderService {
     @Override
     public TransportVersion getMinimalSupportedVersion() {
         return ML_INFERENCE_AMAZON_BEDROCK_ADDED;
+    }
+
+    @Override
+    public Set<TaskType> supportedStreamingTasks() {
+        return COMPLETION_ONLY;
     }
 
     /**

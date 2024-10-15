da6ae806627e3fcbae03ce2ec8f4d4f2e0be4a00
Mike Pellegrini <mike.pellegrini@elastic.co>
2024-10-15 13:56:23 -0400

    Updated insertValue implementation

--- server/src/main/java/org/elasticsearch/common/xcontent/support/XContentMapValues.java
+++ server/src/main/java/org/elasticsearch/common/xcontent/support/XContentMapValues.java
@@ -251,12 +251,42 @@ public class XContentMapValues {
     }
 
     /**
-     * Insert or replace the path's value with the provided new value. The map will be modified in-place.
-     * If the path does not exist in the map, it will be added as a flat top-level entry with the provided new value.
+     * <p>
+     * Insert or replace the path's value in the map with the provided new value. The map will be modified in-place.
+     * If the complete path does not exist in the map, it will be added to the deepest (sub-)map possible.
+     * </p>
+     * <p>
+     * For example, given the map:
+     * </p>
+     * <pre>
+     * {
+     *   "path1": {
+     *     "path2": {
+     *       "key1": "value1"
+     *     }
+     *   }
+     * }
+     * </pre>
+     * <p>
+     * And the caller wanted to insert {@code "path1.path2.path3.key2": "value2"}, the method would emit the modified map:
+     * </p>
+     * <pre>
+     * {
+     *   "path1": {
+     *     "path2": {
+     *       "key1": "value1",
+     *       "path3.key2": "value2"
+     *     }
+     *   }
+     * }
+     * </pre>
      *
      * @param path the value's path in the map.
      * @param map the map to search and modify in-place.
      * @param newValue the new value to assign to the path.
+     *
+     * @throws IllegalArgumentException If either the path cannot be fully traversed or there is ambiguity about where to insert the new
+     *                                  value.
      */
     public static void insertValue(String path, Map<?, ?> map, Object newValue) {
         String[] pathElements = path.split("\\.");
@@ -264,45 +294,45 @@ public class XContentMapValues {
             return;
         }
 
-        List<SuffixMap> buffer = new ArrayList<>();
-        extractSuffixMaps(pathElements, 0, map, buffer);
+        List<SuffixMap> suffixMaps = extractSuffixMaps(pathElements, 0, map);
 
-        if (buffer.isEmpty()) {
-            // No suffix maps were extracted, meaning the path was not found in the map. Add the new value using a flat path to handle if
-            // subobjects: false in downstream parsing.
-            @SuppressWarnings("unchecked")
-            Map<String, Object> castedMap = (Map<String, Object>) map;
-            castedMap.put(path, newValue);
-        } else if (buffer.size() == 1) {
-            SuffixMap suffixMap = buffer.getFirst();
+        if (suffixMaps.isEmpty()) {
+            // This should never happen. Throw in case it does for some reason.
+            throw new IllegalStateException("extractSuffixMaps returned an empty suffix map list");
+        } else if (suffixMaps.size() == 1) {
+            SuffixMap suffixMap = suffixMaps.getFirst();
             suffixMap.map().put(suffixMap.suffix(), newValue);
         } else {
             throw new IllegalArgumentException(
-                "Path [" + path + "] matches " + buffer.size() + " values, it is ambiguous which value to replace"
+                "Path [" + path + "] could be inserted in " + suffixMaps.size() + " distinct ways, it is ambiguous which one to use"
             );
         }
     }
 
     private record SuffixMap(String suffix, Map<String, Object> map) {}
 
-    private static boolean extractSuffixMaps(String[] pathElements, int index, Object currentValue, List<SuffixMap> buffer) {
-        if (index == pathElements.length) {
-            return true;
-        } else if (currentValue instanceof List<?> valueList) {
+    private static List<SuffixMap> extractSuffixMaps(String[] pathElements, int index, Object currentValue) {
+        if (currentValue instanceof List<?> valueList) {
+            List<SuffixMap> suffixMaps = new ArrayList<>(valueList.size());
             for (Object o : valueList) {
-                extractSuffixMaps(pathElements, index, o, buffer);
+                suffixMaps.addAll(extractSuffixMaps(pathElements, index, o));
             }
 
-            return false;
+            return suffixMaps;
         } else if (currentValue instanceof Map<?, ?>) {
             @SuppressWarnings("unchecked")
             Map<String, Object> map = (Map<String, Object>) currentValue;
+            List<SuffixMap> suffixMaps = new ArrayList<>(map.size());
 
             String key = pathElements[index];
             while (index < pathElements.length) {
                 if (map.containsKey(key)) {
-                    if (extractSuffixMaps(pathElements, index + 1, map.get(key), buffer)) {
-                        buffer.add(new SuffixMap(key, map));
+                    if (index + 1 == pathElements.length) {
+                        // We found the complete path
+                        suffixMaps.add(new SuffixMap(key, map));
+                    } else {
+                        // We've matched that path partially, keep traversing to try to match it fully
+                        suffixMaps.addAll(extractSuffixMaps(pathElements, index + 1, map.get(key)));
                     }
                 }
 
@@ -311,7 +341,13 @@ public class XContentMapValues {
                 }
             }
 
-            return false;
+            if (suffixMaps.isEmpty()) {
+                // We checked for all remaining elements in the path, and they do not exist. This means we found a leaf map that we should
+                // add the value to.
+                suffixMaps.add(new SuffixMap(key, map));
+            }
+
+            return suffixMaps;
         } else {
             throw new IllegalArgumentException(
                 "Path ["

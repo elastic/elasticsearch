43d85c8ac3c468d8f19063640e25f662786077af
Aurelien FOUCRET <aurelien.foucret@elastic.co>
2024-10-17 16:04:39 +0200

    Adding some grammar tests.

--- x-pack/plugin/kql/src/test/java/org/elasticsearch/xpack/kql/parser/KqlParserTests.java
+++ x-pack/plugin/kql/src/test/java/org/elasticsearch/xpack/kql/parser/KqlParserTests.java
@@ -7,11 +7,21 @@
 
 package org.elasticsearch.xpack.kql.parser;
 
+import org.elasticsearch.core.SuppressForbidden;
 import org.elasticsearch.index.query.MatchAllQueryBuilder;
 import org.elasticsearch.index.query.SearchExecutionContext;
 import org.elasticsearch.test.AbstractBuilderTestCase;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.isA;
@@ -22,6 +32,32 @@ public class KqlParserTests extends AbstractBuilderTestCase {
         KqlParser parser = new KqlParser();
         SearchExecutionContext searchExecutionContext = createSearchExecutionContext();
         assertThat(parser.parseKqlQuery("", searchExecutionContext), isA(MatchAllQueryBuilder.class));
+    }
+
+    public void testSupportedQueries() throws Exception {
+        KqlParser parser = new KqlParser();
+        SearchExecutionContext searchExecutionContext = createSearchExecutionContext();
+
+        for (String query : readQueries("/supported-queries")) {
+            try {
+                parser.parseKqlQuery(query, searchExecutionContext);
+            } catch (Throwable e) {
+                throw new AssertionError("Unexpected error during query parsing [ " + query + "]", e);
+            }
+        }
+    }
+
+    public void testUnsupportedQueries() throws Exception {
+        KqlParser parser = new KqlParser();
+        SearchExecutionContext searchExecutionContext = createSearchExecutionContext();
+
+        for (String query : readQueries("/unsupported-queries")) {
+            assertThrows(
+                "Was expecting a KqlParsingException exception to be thrown while parsing query [" + query + "]",
+                KqlParsingException.class,
+                () -> parser.parseKqlQuery(query, searchExecutionContext)
+            );
+        }
     }
 
     public void testSyntaxErrorsHandling() throws IOException {
@@ -48,5 +84,33 @@ public class KqlParserTests extends AbstractBuilderTestCase {
             assertThat(e.getColumnNumber(), equalTo(15));
             assertThat(e.getMessage(), equalTo("line 1:15: missing ')' at 'AND'"));
         }
+    }
+
+    private static List<String> readQueries(String source) throws Exception {
+        URL url = KqlParserTests.class.getResource(source);
+        Objects.requireNonNull(source, "Cannot find resource " + url);
+
+        List<String> queries = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(readFromJarUrl(url), StandardCharsets.UTF_8))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String query = line.trim();
+                // ignore comments
+                if (query.isEmpty() == false && query.startsWith("//") == false) {
+                    queries.add(line.trim());
+                }
+            }
+        }
+        return queries;
+    }
+
+    @SuppressForbidden(reason = "test reads from jar")
+    private static InputStream readFromJarUrl(URL source) throws IOException {
+        URLConnection con = source.openConnection();
+        // do not to cache files (to avoid keeping file handles around)
+        con.setUseCaches(false);
+        return con.getInputStream();
     }
 }

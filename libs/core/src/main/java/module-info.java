4edd6eb53a67bc58259da4728f39a00d12e67aa6
Patrick Doyle <patrick.doyle@elastic.co>
2024-10-18 10:30:36 -0400

    Try unsuccessfully to get things working

--- libs/core/src/main/java/module-info.java
+++ libs/core/src/main/java/module-info.java
@@ -7,19 +7,13 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import org.elasticsearch.jdk.ModuleQualifiedExportsService;
-
 module org.elasticsearch.base {
     requires static jsr305;
     requires org.elasticsearch.logging;
 
     exports org.elasticsearch.core;
     exports org.elasticsearch.jdk;
-    exports org.elasticsearch.core.internal.provider
-        to
-            org.elasticsearch.xcontent,
-            org.elasticsearch.nativeaccess,
-            org.elasticsearch.entitlement.agent;
+    exports org.elasticsearch.core.internal.provider;
 
-    uses ModuleQualifiedExportsService;
+    uses org.elasticsearch.jdk.ModuleQualifiedExportsService;
 }

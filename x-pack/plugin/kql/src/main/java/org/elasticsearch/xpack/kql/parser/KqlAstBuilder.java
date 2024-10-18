65e533c9edf069abd17e8b824dadc28fc7eacde2
Aurelien FOUCRET <aurelien.foucret@elastic.co>
2024-10-18 10:26:26 +0200

    Implement empty query building + tests.

--- x-pack/plugin/kql/src/main/java/org/elasticsearch/xpack/kql/parser/KqlAstBuilder.java
+++ x-pack/plugin/kql/src/main/java/org/elasticsearch/xpack/kql/parser/KqlAstBuilder.java
@@ -9,6 +9,7 @@ package org.elasticsearch.xpack.kql.parser;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.elasticsearch.index.query.MatchAllQueryBuilder;
+import org.elasticsearch.index.query.MatchNoneQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.SearchExecutionContext;
 
@@ -21,9 +22,28 @@ class KqlAstBuilder extends KqlBaseBaseVisitor<QueryBuilder> {
 
     public QueryBuilder toQueryBuilder(ParserRuleContext ctx) {
         if (ctx instanceof KqlBaseParser.TopLevelQueryContext topLeveQueryContext) {
+            if (topLeveQueryContext.query() != null) {
+                ParserUtils.typedParsing(this, topLeveQueryContext.query(), QueryBuilder.class);
+            }
+
             return new MatchAllQueryBuilder();
         }
 
         throw new IllegalArgumentException("context should be of type TopLevelQueryContext");
+    }
+
+    @Override
+    public QueryBuilder visitBooleanQuery(KqlBaseParser.BooleanQueryContext ctx) {
+        return new MatchNoneQueryBuilder();
+    }
+
+    @Override
+    public QueryBuilder visitNotQuery(KqlBaseParser.NotQueryContext ctx) {
+        return new MatchNoneQueryBuilder();
+    }
+
+    @Override
+    public QueryBuilder visitSimpleQuery(KqlBaseParser.SimpleQueryContext ctx) {
+        return new MatchNoneQueryBuilder();
     }
 }

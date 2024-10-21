d8278493dff1d3bc1418eebeb203046a8b9708da
Jake Landis <jake.landis@elastic.co>
2024-10-21 16:30:09 -0500

    prepare for merge

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -247,7 +247,8 @@ public class TransportVersions {
     public static final TransportVersion ML_INFERENCE_ATTACH_TO_EXISTSING_DEPLOYMENT = def(8_771_00_0);
     public static final TransportVersion CONVERT_FAILURE_STORE_OPTIONS_TO_SELECTOR_OPTIONS_INTERNALLY = def(8_772_00_0);
     public static final TransportVersion REMOVE_MIN_COMPATIBLE_SHARD_NODE = def(8_773_00_0);
-    public static final TransportVersion ROLE_MONITOR_STATS = def(8_774_00_0);
+    
+    public static final TransportVersion ROLE_MONITOR_STATS = def(8_777_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

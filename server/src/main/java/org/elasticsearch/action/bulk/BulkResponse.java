719ce1080db653c5af863d70a3eeebe3e3fa6088
Simon Cooper <simon.cooper@elastic.co>
2024-10-21 13:10:54 +0100

    Minimise number of chunks created

--- server/src/main/java/org/elasticsearch/action/bulk/BulkResponse.java
+++ server/src/main/java/org/elasticsearch/action/bulk/BulkResponse.java
@@ -14,6 +14,7 @@ import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.common.collect.Iterators;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.xcontent.ChunkedToXContent;
 import org.elasticsearch.common.xcontent.ChunkedToXContentObject;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.xcontent.ToXContent;
@@ -157,14 +158,13 @@ public class BulkResponse extends ActionResponse implements Iterable<BulkItemRes
 
     @Override
     public Iterator<? extends ToXContent> toXContentChunked(ToXContent.Params params) {
-        return Iterators.concat(Iterators.single((builder, p) -> {
-            builder.startObject();
-            builder.field(ERRORS, hasFailures());
-            builder.field(TOOK, tookInMillis);
+        return ChunkedToXContent.builder(params).object(ob -> ob.append((b, p) -> {
+            b.field(ERRORS, hasFailures());
+            b.field(TOOK, tookInMillis);
             if (ingestTookInMillis != BulkResponse.NO_INGEST_TOOK) {
-                builder.field(INGEST_TOOK, ingestTookInMillis);
+                b.field(INGEST_TOOK, ingestTookInMillis);
             }
-            return builder.startArray(ITEMS);
-        }), Iterators.forArray(responses), Iterators.<ToXContent>single((builder, p) -> builder.endArray().endObject()));
+            return b;
+        }).array(ITEMS, Iterators.forArray(responses)));
     }
 }

f38be7912f4756b0bc4d5c6261d4acc599c99276
Kathleen DeRusso <kathleen.derusso@elastic.co>
2024-10-22 14:06:58 -0400

    Add specific QueryRuleRankDoc for better explain support

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/rules/retriever/QueryRuleRetrieverBuilder.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/rules/retriever/QueryRuleRetrieverBuilder.java
@@ -160,10 +160,10 @@ public final class QueryRuleRetrieverBuilder extends CompoundRetrieverBuilder<Qu
     protected RankDoc[] combineInnerRetrieverResults(List<ScoreDoc[]> rankResults) {
         assert rankResults.size() == 1;
         ScoreDoc[] scoreDocs = rankResults.getFirst();
-        RankDoc[] rankDocs = new RankDoc[scoreDocs.length];
+        RankDoc[] rankDocs = new QueryRuleRankDoc[scoreDocs.length];
         for (int i = 0; i < scoreDocs.length; i++) {
             ScoreDoc scoreDoc = scoreDocs[i];
-            rankDocs[i] = new RankDoc(scoreDoc.doc, scoreDoc.score, scoreDoc.shardIndex);
+            rankDocs[i] = new QueryRuleRankDoc(scoreDoc.doc, scoreDoc.score, scoreDoc.shardIndex, rulesetIds, matchCriteria);
             rankDocs[i].rank = i + 1;
         }
         return rankDocs;

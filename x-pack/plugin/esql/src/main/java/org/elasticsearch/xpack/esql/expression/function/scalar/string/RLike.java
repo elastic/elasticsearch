72292ed20c95a60b487d0eaeeb289b0cde07cefa
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2024-10-22 15:01:19 +0200

    ES|QL: improve docs about escaping for GROK, DISSECT, LIKE, RLIKE

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/RLike.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/RLike.java
@@ -29,11 +29,34 @@ import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.isStr
 public class RLike extends org.elasticsearch.xpack.esql.core.expression.predicate.regex.RLike implements EvaluatorMapper {
     public static final NamedWriteableRegistry.Entry ENTRY = new NamedWriteableRegistry.Entry(Expression.class, "RLike", RLike::new);
 
-    @FunctionInfo(returnType = "boolean", description = """
-        Use `RLIKE` to filter data based on string patterns using using
-        <<regexp-syntax,regular expressions>>. `RLIKE` usually acts on a field placed on
-        the left-hand side of the operator, but it can also act on a constant (literal)
-        expression. The right-hand side of the operator represents the pattern.""", examples = @Example(file = "docs", tag = "rlike"))
+    @FunctionInfo(
+        returnType = "boolean",
+        description = """
+            Use `RLIKE` to filter data based on string patterns using using
+            <<regexp-syntax,regular expressions>>. `RLIKE` usually acts on a field placed on
+            the left-hand side of the operator, but it can also act on a constant (literal)
+            expression. The right-hand side of the operator represents the pattern, as a <<string-literals,string literal>>.""",
+        detailedDescription = """
+            Matching special characters (eg. `.`, `*`, `(`...) will require escaping.
+            The escape character is backslash `\\`. Since also backslash is a special character in string literals,
+            it will require further escaping.
+
+            [source.merge.styled,esql]
+            ----
+            include::{esql-specs}/string.csv-spec[tag=rlikeEscapingSingleQuotes]
+            ----
+            ====
+
+            To reduce the overhead of escaping, we suggest using triple quotes strings `\"\"\"`
+
+            [source.merge.styled,esql]
+            ----
+            include::{esql-specs}/string.csv-spec[tag=rlikeEscapingTripleQuotes]
+            ----
+            ====
+            """,
+        examples = @Example(file = "docs", tag = "rlike")
+    )
     public RLike(
         Source source,
         @Param(name = "str", type = { "keyword", "text" }, description = "A literal value.") Expression value,
--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/WildcardLike.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/WildcardLike.java
@@ -38,12 +38,30 @@ public class WildcardLike extends org.elasticsearch.xpack.esql.core.expression.p
         Use `LIKE` to filter data based on string patterns using wildcards. `LIKE`
         usually acts on a field placed on the left-hand side of the operator, but it can
         also act on a constant (literal) expression. The right-hand side of the operator
-        represents the pattern.
+        represents the pattern, as a <<string-literals,string literal>>.
 
         The following wildcard characters are supported:
 
         * `*` matches zero or more characters.
-        * `?` matches one character.""", examples = @Example(file = "docs", tag = "like"))
+        * `?` matches one character.""", detailedDescription = """
+        Matching the exact characters `*` and `.` will require escaping.
+        The escape character is backslash `\\`. Since also backslash is a special character in string literals,
+        it will require further escaping.
+
+        [source.merge.styled,esql]
+        ----
+        include::{esql-specs}/string.csv-spec[tag=likeEscapingSingleQuotes]
+        ----
+        ====
+
+        To reduce the overhead of escaping, we suggest using triple quotes strings `\"\"\"`
+
+        [source.merge.styled,esql]
+        ----
+        include::{esql-specs}/string.csv-spec[tag=likeEscapingTripleQuotes]
+        ----
+        ====
+        """, examples = @Example(file = "docs", tag = "like"))
     public WildcardLike(
         Source source,
         @Param(name = "str", type = { "keyword", "text" }, description = "A literal expression.") Expression left,

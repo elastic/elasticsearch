807fb74e1d119e9a8d90a66f1dfb3e0f6cc36e5b
Aurelien FOUCRET <aurelien.foucret@elastic.co>
2024-10-23 17:49:41 +0200

    Test improvements again.

--- x-pack/plugin/kql/src/test/java/org/elasticsearch/xpack/kql/parser/KqlParserTests.java
+++ x-pack/plugin/kql/src/test/java/org/elasticsearch/xpack/kql/parser/KqlParserTests.java
@@ -8,7 +8,7 @@
 package org.elasticsearch.xpack.kql.parser;
 
 import org.elasticsearch.index.query.MatchAllQueryBuilder;
-import org.elasticsearch.index.query.SearchExecutionContext;
+import org.elasticsearch.index.query.QueryBuilder;
 
 import java.io.IOException;
 
@@ -18,56 +18,41 @@ import static org.hamcrest.Matchers.isA;
 public class KqlParserTests extends AbstractKqlParserTestCase {
 
     public void testEmptyQueryParsing() {
-        KqlParser parser = new KqlParser();
-        SearchExecutionContext searchExecutionContext = createSearchExecutionContext();
-
         {
             // In Kql, an empty query is a match_all query.
-            assertThat(parser.parseKqlQuery("", searchExecutionContext), isA(MatchAllQueryBuilder.class));
+            assertThat(parseKqlQuery(""), isA(MatchAllQueryBuilder.class));
         }
 
         for (int runs = 0; runs < 100; runs++) {
             // Also testing that a query that is composed only of whitespace chars returns a match_all query.
             String kqlQuery = randomWhitespaces();
-            assertThat(parser.parseKqlQuery(kqlQuery, searchExecutionContext), isA(MatchAllQueryBuilder.class));
+            assertThat(parseKqlQuery(kqlQuery), isA(MatchAllQueryBuilder.class));
         }
     }
 
     public void testMatchAllQuery() {
-        KqlParser parser = new KqlParser();
-        SearchExecutionContext searchExecutionContext = createSearchExecutionContext();
-
-        assertThat(parser.parseKqlQuery("*", searchExecutionContext), isA(MatchAllQueryBuilder.class));
-        assertThat(parser.parseKqlQuery(wrapWithRandomWhitespaces("*"), searchExecutionContext), isA(MatchAllQueryBuilder.class));
-        assertThat(parser.parseKqlQuery("*:*", searchExecutionContext), isA(MatchAllQueryBuilder.class));
-        assertThat(
-            parser.parseKqlQuery(String.join(wrapWithRandomWhitespaces(":"), "*", "*"), searchExecutionContext),
-            isA(MatchAllQueryBuilder.class)
-        );
+        assertThat(parseKqlQuery("*"), isA(MatchAllQueryBuilder.class));
+        assertThat(parseKqlQuery(wrapWithRandomWhitespaces("*")), isA(MatchAllQueryBuilder.class));
+        assertThat(parseKqlQuery("*:*"), isA(MatchAllQueryBuilder.class));
+        assertThat(parseKqlQuery(String.join(wrapWithRandomWhitespaces(":"), "*", "*")), isA(MatchAllQueryBuilder.class));
     }
 
     public void testParenthesizedQuery() throws IOException {
-        KqlParser parser = new KqlParser();
-        SearchExecutionContext searchExecutionContext = createSearchExecutionContext();
-
         for (String baseQuuery : readQueries(SUPPORTED_QUERY_FILE_PATH)) {
             // For each supported query, wrap it into parentheses and check query remains the same.
             // Adding random whitespaces as well and test they are ignored.
             String parenthesizedQuery = wrapWithRandomWhitespaces("(") + baseQuuery + wrapWithRandomWhitespaces(")");
-            assertThat(
-                parser.parseKqlQuery(parenthesizedQuery, searchExecutionContext),
-                equalTo(parser.parseKqlQuery(baseQuuery, searchExecutionContext))
-            );
+            assertThat(parseKqlQuery(parenthesizedQuery), equalTo(parseKqlQuery(baseQuuery)));
         }
     }
 
     public void testSupportedQueries() throws IOException {
-        KqlParser parser = new KqlParser();
-        SearchExecutionContext searchExecutionContext = createSearchExecutionContext();
-
         for (String query : readQueries(SUPPORTED_QUERY_FILE_PATH)) {
             try {
-                parser.parseKqlQuery(query, searchExecutionContext);
+                QueryBuilder parsedQuery = parseKqlQuery(query);
+
+                // leading and trailing whitespaces does not change the query builing result:
+                assertThat(parseKqlQuery(wrapWithRandomWhitespaces(query)), equalTo(parsedQuery));
             } catch (Throwable e) {
                 throw new AssertionError("Unexpected error during query parsing [ " + query + "]", e);
             }
@@ -75,37 +60,25 @@ public class KqlParserTests extends AbstractKqlParserTestCase {
     }
 
     public void testUnsupportedQueries() throws IOException {
-        KqlParser parser = new KqlParser();
-        SearchExecutionContext searchExecutionContext = createSearchExecutionContext();
-
         for (String query : readQueries(UNSUPPORTED_QUERY_FILE_PATH)) {
             assertThrows(
                 "Was expecting a KqlParsingException exception to be thrown while parsing query [" + query + "]",
                 KqlParsingException.class,
-                () -> parser.parseKqlQuery(query, searchExecutionContext)
+                () -> parseKqlQuery(query)
             );
         }
     }
 
     public void testSyntaxErrorsHandling() {
-        KqlParser parser = new KqlParser();
-        SearchExecutionContext searchExecutionContext = createSearchExecutionContext();
-
         {
-            KqlParsingException e = assertThrows(
-                KqlParsingException.class,
-                () -> parser.parseKqlQuery("foo: \"bar", searchExecutionContext)
-            );
+            KqlParsingException e = assertThrows(KqlParsingException.class, () -> parseKqlQuery("foo: \"bar"));
             assertThat(e.getLineNumber(), equalTo(1));
             assertThat(e.getColumnNumber(), equalTo(6));
             assertThat(e.getMessage(), equalTo("line 1:6: token recognition error at: '\"bar'"));
         }
 
         {
-            KqlParsingException e = assertThrows(
-                KqlParsingException.class,
-                () -> parser.parseKqlQuery("foo: (bar baz AND qux", searchExecutionContext)
-            );
+            KqlParsingException e = assertThrows(KqlParsingException.class, () -> parseKqlQuery("foo: (bar baz AND qux"));
             assertThat(e.getLineNumber(), equalTo(1));
             assertThat(e.getColumnNumber(), equalTo(15));
             assertThat(e.getMessage(), equalTo("line 1:15: missing ')' at 'AND'"));

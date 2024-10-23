6ac098967d9b8c2cfb33762707db176929053305
Ankita Kumar <ankita.kumar@elastic.co>
2024-10-23 10:01:39 -0400

    Address Review comments

--- modules/reindex/src/internalClusterTest/java/org/elasticsearch/index/reindex/ReindexNodeShutdownIT.java
+++ modules/reindex/src/internalClusterTest/java/org/elasticsearch/index/reindex/ReindexNodeShutdownIT.java
@@ -35,7 +35,7 @@ import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertHitC
 /**
  * Test that a wait added during shutdown is necessary for a large reindexing task to complete.
  * The test works as follows:
- * 1. Start a large reindexing request on the coordinator-only node.
+ * 1. Start a large (reasonably long running) reindexing request on the coordinator-only node.
  * 2. Check that the reindexing task appears on the coordinating node
  * 3. With a 5s timeout value for MAXIMUM_REINDEXING_TIMEOUT_SETTING,
  *    wait for the reindexing task to complete before closing the node
@@ -56,6 +56,23 @@ public class ReindexNodeShutdownIT extends ESIntegTestCase {
         return new ReindexRequestBuilder(internalCluster().client(nodeName));
     }
 
+    public void testReindexWithShutdown() throws Exception {
+        final String masterNodeName = internalCluster().startMasterOnlyNode();
+        final String dataNodeName = internalCluster().startDataOnlyNode();
+
+        final Settings COORD_SETTINGS = Settings.builder()
+            .put(MAXIMUM_REINDEXING_TIMEOUT_SETTING.getKey(), TimeValue.timeValueSeconds(10))
+            .build();
+        final String coordNodeName = internalCluster().startCoordinatingOnlyNode(Settings.EMPTY);
+
+        ensureStableCluster(3);
+
+        int numDocs = 20000;
+        createIndex(numDocs);
+        createReindexTaskAndShutdown(coordNodeName);
+        checkDestinationIndex(dataNodeName, numDocs);
+    }
+
     private void createIndex(int numDocs) {
         // INDEX will be created on the dataNode
         createIndex(INDEX);
@@ -74,36 +91,7 @@ public class ReindexNodeShutdownIT extends ESIntegTestCase {
         assertHitCount(prepareSearch(INDEX).setSize(0).setTrackTotalHits(true), numDocs);
     }
 
-    private void checkDestinationIndex(String dataNodeName, int numDocs) {
-        assertTrue(indexExists(DEST_INDEX));
-        flushAndRefresh(DEST_INDEX);
-        assertTrue("Number of documents in source and dest indexes does not match", waitUntil(() -> {
-            final TotalHits totalHits = SearchResponseUtils.getTotalHits(
-                client(dataNodeName).prepareSearch(DEST_INDEX).setSize(0).setTrackTotalHits(true)
-            );
-            return totalHits.relation() == TotalHits.Relation.EQUAL_TO && totalHits.value() == numDocs;
-        }, 10, TimeUnit.SECONDS));
-    }
-
-    public void testReindexWithShutdown() throws Exception {
-        final String masterNodeName = internalCluster().startMasterOnlyNode();
-        final String dataNodeName = internalCluster().startDataOnlyNode();
-
-        final Settings COORD_SETTINGS = Settings.builder()
-            .put(MAXIMUM_REINDEXING_TIMEOUT_SETTING.getKey(), TimeValue.timeValueSeconds(10))
-            .build();
-        final String coordNodeName = internalCluster().startCoordinatingOnlyNode(Settings.EMPTY);
-
-        ensureStableCluster(3);
-
-        int numDocs = 20000;
-        createIndex(numDocs);
-        createReindexTaskAndShutdown(coordNodeName);
-        checkDestinationIndex(dataNodeName, numDocs);
-    }
-
     private void createReindexTaskAndShutdown(final String coordNodeName) throws Exception {
-
         AbstractBulkByScrollRequestBuilder<?, ?> builder = reindex(coordNodeName).source(INDEX).destination(DEST_INDEX);
         AbstractBulkByScrollRequest<?> reindexRequest = builder.request();
         ShutdownPrepareService shutdownPrepareService = internalCluster().getInstance(ShutdownPrepareService.class, coordNodeName);
@@ -121,29 +109,37 @@ public class ReindexNodeShutdownIT extends ESIntegTestCase {
             @Override
             public void onFailure(Exception e) {
                 logger.debug("Encounterd " + e.toString());
+                fail(e, "Encounterd " + e.toString());
             }
         };
         internalCluster().client(coordNodeName).execute(ReindexAction.INSTANCE, reindexRequest, reindexListener);
 
         // Check for reindex task to appear in the tasks list and Immediately stop coordinating node
-        TaskInfo mainTask = findTask(ReindexAction.INSTANCE.name(), coordNodeName);
+        // TaskInfo mainTask = findTask(ReindexAction.INSTANCE.name(), coordNodeName);
+        findTask(ReindexAction.INSTANCE.name(), coordNodeName);
         shutdownPrepareService.prepareForShutdown(taskManager);
         internalCluster().stopNode(coordNodeName);
     }
 
-    private static TaskInfo findTask(String actionName, String nodeName) {
-        ListTasksResponse tasks;
-        long start = System.nanoTime();
-        do {
-            tasks = clusterAdmin().prepareListTasks(nodeName).setActions(actionName).setDetailed(true).get();
+    private void checkDestinationIndex(String dataNodeName, int numDocs) {
+        assertTrue(indexExists(DEST_INDEX));
+        flushAndRefresh(DEST_INDEX);
+        assertTrue("Number of documents in source and dest indexes does not match", waitUntil(() -> {
+            final TotalHits totalHits = SearchResponseUtils.getTotalHits(
+                client(dataNodeName).prepareSearch(DEST_INDEX).setSize(0).setTrackTotalHits(true)
+            );
+            return totalHits.relation() == TotalHits.Relation.EQUAL_TO && totalHits.value() == numDocs;
+        }, 10, TimeUnit.SECONDS));
+    }
+
+    private static void findTask(String actionName, String nodeName) throws Exception {
+        assertBusy(() -> {
+            ListTasksResponse tasks = clusterAdmin().prepareListTasks(nodeName).setActions(actionName).setDetailed(true).get();
             tasks.rethrowFailures("Find my task");
             for (TaskInfo taskInfo : tasks.getTasks()) {
                 // Skip tasks with a parent because those are children of the task we want
-                if (false == taskInfo.parentTaskId().isSet()) {
-                    return taskInfo;
-                }
+                assertFalse(taskInfo.parentTaskId().isSet());
             }
-        } while (System.nanoTime() - start < TimeUnit.SECONDS.toNanos(10));
-        throw new AssertionError("Couldn't find task after waiting tasks=" + tasks.getTasks());
+        }, 10, TimeUnit.NANOSECONDS);
     }
 }

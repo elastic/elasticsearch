2b3cbb1644ff13d45f546344f8533fc56f54f715
Matteo Piergiovanni <134913285+piergm@users.noreply.github.com>
2024-10-24 13:50:31 +0200

    iter

--- x-pack/qa/repository-old-versions/src/test/java/org/elasticsearch/oldrepos/OldRepositoryAccessIT.java
+++ x-pack/qa/repository-old-versions/src/test/java/org/elasticsearch/oldrepos/OldRepositoryAccessIT.java
@@ -484,7 +484,7 @@ public class OldRepositoryAccessIT extends ESRestTestCase {
                 logger.info(searchResponse);
                 assertEquals(0, searchResponse.getHits().getTotalHits().value());
                 assertEquals(numberOfShards, searchResponse.getSuccessfulShards());
-                assertEquals(0, searchResponse.getSkippedShards());
+                assertEquals(numberOfShards, searchResponse.getSkippedShards());
             } finally {
                 searchResponse.decRef();
             }

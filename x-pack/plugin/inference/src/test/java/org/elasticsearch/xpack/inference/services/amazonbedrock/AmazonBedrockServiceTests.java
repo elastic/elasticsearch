ba058da5e8fb1a4ec088122c88a21353720c9a85
Pat Whelan <pat.whelan@elastic.co>
2024-10-28 14:54:03 -0400

    [ML] Fix stream support for TaskType.ANY (#115656)
    If we support one, then we support any.

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/amazonbedrock/AmazonBedrockServiceTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/amazonbedrock/AmazonBedrockServiceTests.java
@@ -1206,6 +1206,13 @@ public class AmazonBedrockServiceTests extends ESTestCase {
         }
     }
 
+    public void testSupportsStreaming() throws IOException {
+        try (var service = new AmazonBedrockService(mock(), mock(), createWithEmptySettings(mock()))) {
+            assertTrue(service.canStream(TaskType.COMPLETION));
+            assertTrue(service.canStream(TaskType.ANY));
+        }
+    }
+
     public void testChunkedInfer_ChunkingSettingsSet() throws IOException {
         var model = AmazonBedrockEmbeddingsModelTests.createModel(
             "id",

06b94a66838e5a8ae6aa354fc5db118c31c6a75a
Johannes Freden Jansson <johannes.freden@elastic.co>
2024-10-29 11:19:01 +0100

    fixup! Cannot verify mappings pre migration anymore

--- x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/SecurityIndexRoleMappingCleanupIT.java
+++ x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/SecurityIndexRoleMappingCleanupIT.java
@@ -41,8 +41,6 @@ public class SecurityIndexRoleMappingCleanupIT extends AbstractUpgradeTestCase {
             // Since the old cluster has role mappings in cluster state, but doesn't check duplicates, create duplicates
             createNativeRoleMapping("operator_role_mapping_1", Map.of("meta", "test"), true);
             createNativeRoleMapping("operator_role_mapping_2", Map.of("meta", "test"), true);
-            // API will return only native role mappings
-            assertAllRoleMappings(client(), "operator_role_mapping_1", "operator_role_mapping_2");
         } else if (CLUSTER_TYPE == ClusterType.MIXED) {
             // Create a native role mapping that doesn't conflict with anything before the migration run
             createNativeRoleMapping("no_name_conflict", Map.of("meta", "test"));

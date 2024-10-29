c43db993f39d1bfa40b5081c0c5bb0a966857a03
Kostas Krikellas <131142368+kkrik-es@users.noreply.github.com>
2024-10-29 21:12:43 +0200

    Use flattened names in ignored source (#115822)
    * Use flattened names in ignored source
    
    * spotless
    
    * fix rest compat
    
    * fix unittests
    
    * expand dots
    
    (cherry picked from commit 06eb0727c22db5559cc9cc9da46ba423bd3663c5)
    
    # Conflicts:
    #	rest-api-spec/build.gradle

--- server/src/main/java/org/elasticsearch/index/mapper/DocumentParserContext.java
+++ server/src/main/java/org/elasticsearch/index/mapper/DocumentParserContext.java
@@ -518,11 +518,7 @@ public abstract class DocumentParserContext {
                     if (canAddIgnoredField()) {
                         try {
                             addIgnoredField(
-                                IgnoredSourceFieldMapper.NameValue.fromContext(
-                                    this,
-                                    mapper.fullPath(),
-                                    XContentDataHelper.encodeToken(parser())
-                                )
+                                IgnoredSourceFieldMapper.NameValue.fromContext(this, mapper.fullPath(), encodeFlattenedToken())
                             );
                         } catch (IOException e) {
                             throw new IllegalArgumentException("failed to parse field [" + mapper.fullPath() + " ]", e);

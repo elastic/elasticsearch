7580087855ffe85a0fecd601757590c9fcc7e917
Keith Massey <keith.massey@elastic.co>
2024-10-30 09:19:44 -0500

    Preparing for the removal of ESClientYamlSuiteTestCase::getGlobalTemplateSettings (#3046)

--- x-pack/plugin/stateless/src/yamlRestTest/java/co/elasticsearch/stateless/rest/ServerlessClientYamlTestSuiteIT.java
+++ x-pack/plugin/stateless/src/yamlRestTest/java/co/elasticsearch/stateless/rest/ServerlessClientYamlTestSuiteIT.java
@@ -22,7 +22,6 @@ import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 import com.carrotsearch.randomizedtesting.annotations.TimeoutSuite;
 
 import org.apache.lucene.tests.util.TimeUnits;
-import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
@@ -31,8 +30,6 @@ import org.elasticsearch.test.cluster.serverless.ServerlessElasticsearchCluster;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
 import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
 import org.junit.ClassRule;
-
-import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 @TimeoutSuite(millis = 40 * TimeUnits.MINUTE)
 public class ServerlessClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
@@ -56,20 +53,6 @@ public class ServerlessClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
     @Override
     protected String getTestRestCluster() {
         return cluster.getHttpAddresses();
-    }
-
-    @Override
-    protected Settings getGlobalTemplateSettings(boolean defaultShardsFeature) {
-        final Settings defaultSettings = super.getGlobalTemplateSettings(defaultShardsFeature);
-        assertThat(IndexMetadata.INDEX_NUMBER_OF_REPLICAS_SETTING.get(defaultSettings), lessThanOrEqualTo(1));
-        if (defaultShardsFeature) {
-            return defaultSettings;
-        }
-        return Settings.builder()
-            .put(defaultSettings)
-            .put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 1)
-            .put(IndexMetadata.SETTING_WAIT_FOR_ACTIVE_SHARDS.getKey(), "all")
-            .build();
     }
 
     @Override

16dab2bf6dbd20c900067da7ec68eb9e9ccbc50a
Craig Taverner <craig@amanzi.com>
2024-10-30 14:36:31 +0100

    Also consider isAggregatable for non-indexed fields like ScriptField

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/rules/physical/local/LucenePushDownUtils.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/rules/physical/local/LucenePushDownUtils.java
@@ -19,12 +19,20 @@ class LucenePushDownUtils {
         return stats.hasIdenticalDelegate(attr.name());
     }
 
+    /**
+     * We see fields as pushable if either they are aggregatable or they are indexed.
+     * This covers non-indexed cases like <code>AbstractScriptFieldType</code> which hard-coded <code>isAggregatable</code> to true,
+     * as well as normal <code>FieldAttribute</code>'s which can only be pushed down if they are indexed.
+     * The reason we don't just rely entirely on <code>isAggregatable</code> is because this is often false for normal fields, and could
+     * also differ from node to node, and we can physically plan each node separately, allowing Lucene pushdown on the nodes that
+     * support it, and relying on the compute engine for the nodes that do not.
+     */
     public static boolean isPushableFieldAttribute(
         Expression exp,
         Predicate<FieldAttribute> hasIdenticalDelegate,
         Predicate<FieldAttribute> isIndexed
     ) {
-        if (exp instanceof FieldAttribute fa && fa.getExactInfo().hasExact() && isIndexed.test(fa)) {
+        if (exp instanceof FieldAttribute fa && fa.getExactInfo().hasExact() && (fa.field().isAggregatable() || isIndexed.test(fa))) {
             return (fa.dataType() != DataType.TEXT && fa.dataType() != DataType.SEMANTIC_TEXT) || hasIdenticalDelegate.test(fa);
         }
         return false;

07f131e835cfa8f7114d36b6670b8f7b16d623f3
Lorenzo Dematte <lorenzo.dematte@elastic.co>
2024-10-31 12:43:14 +0100

    Making it modular

---
+++ libs/entitlement/tools/common/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module elasticsearch.libs.entitlement.tools.common {
+    exports org.elasticsearch.entitlement.tools;
+}
---
+++ libs/entitlement/tools/securitymanager-scanner/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module elasticsearch.libs.entitlement.tools.securitymanager.scanner {
+    requires elasticsearch.libs.entitlement.tools.common;
+    requires org.objectweb.asm.util;
+
+    requires jdk.dynalink;
+    requires java.sql;
+    requires java.sql.rowset;
+    requires java.base;
+    requires java.net.http;
+    requires jdk.unsupported;
+}

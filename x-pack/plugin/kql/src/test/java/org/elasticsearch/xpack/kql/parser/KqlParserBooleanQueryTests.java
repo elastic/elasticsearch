9124f568cf99152d01e8daf67983fc2b90e0c28d
Aurelien FOUCRET <aurelien.foucret@elastic.co>
2024-11-01 10:28:34 +0100

    Improve boolean queries tests.

--- x-pack/plugin/kql/src/test/java/org/elasticsearch/xpack/kql/parser/KqlParserBooleanQueryTests.java
+++ x-pack/plugin/kql/src/test/java/org/elasticsearch/xpack/kql/parser/KqlParserBooleanQueryTests.java
@@ -11,11 +11,11 @@ import org.elasticsearch.core.Strings;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.SearchExecutionContext;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.empty;
@@ -26,7 +26,7 @@ import static org.hamcrest.Matchers.hasSize;
 public class KqlParserBooleanQueryTests extends AbstractKqlParserTestCase {
 
     public void testParseNotQuery() throws IOException {
-        for (String baseQuery : readQueries(SUPPORTED_QUERY_FILE_PATH, Predicate.not(BOOLEAN_QUERY_FILTER))) {
+        for (String baseQuery : readQueries(SUPPORTED_QUERY_FILE_PATH)) {
             if (BOOLEAN_QUERY_FILTER.test(baseQuery)) {
                 baseQuery = wrapWithRandomWhitespaces("(") + baseQuery + wrapWithRandomWhitespaces(")");
             }
@@ -48,7 +48,7 @@ public class KqlParserBooleanQueryTests extends AbstractKqlParserTestCase {
     }
 
     public void testParseOrQuery() throws IOException {
-        List<String> supportedQueries = readQueries(SUPPORTED_QUERY_FILE_PATH, Predicate.not(BOOLEAN_QUERY_FILTER));
+        List<String> supportedQueries = readQueries(SUPPORTED_QUERY_FILE_PATH);
 
         for (int runs = 0; runs < 100; runs++) {
             String queryA = randomFrom(supportedQueries);
@@ -57,12 +57,19 @@ public class KqlParserBooleanQueryTests extends AbstractKqlParserTestCase {
 
             BoolQueryBuilder parsedQuery = asInstanceOf(BoolQueryBuilder.class, parseKqlQuery(orQuery));
 
-            assertThat(parsedQuery.filter(), empty());
-            assertThat(parsedQuery.must(), empty());
-            assertThat(parsedQuery.mustNot(), empty());
-            assertThat(parsedQuery.should(), hasSize(2));
-            assertThat(parsedQuery.minimumShouldMatch(), equalTo("1"));
-            assertThat(parsedQuery.should(), allOf(hasItem(equalTo((parseKqlQuery(queryA)))), hasItem(equalTo((parseKqlQuery(queryB))))));
+            if (Stream.of(queryA, queryB).noneMatch(BOOLEAN_QUERY_FILTER)) {
+                // If one of the subquery is a boolean query, it is impossible to test the content of the generated query because
+                // operator precedence rules are applied. There are specific tests for it in testOperatorPrecedence.
+                assertThat(parsedQuery.filter(), empty());
+                assertThat(parsedQuery.must(), empty());
+                assertThat(parsedQuery.mustNot(), empty());
+                assertThat(parsedQuery.should(), hasSize(2));
+                assertThat(parsedQuery.minimumShouldMatch(), equalTo("1"));
+                assertThat(
+                    parsedQuery.should(),
+                    allOf(hasItem(equalTo((parseKqlQuery(queryA)))), hasItem(equalTo((parseKqlQuery(queryB)))))
+                );
+            }
         }
 
         for (int runs = 0; runs < 100; runs++) {
@@ -72,20 +79,25 @@ public class KqlParserBooleanQueryTests extends AbstractKqlParserTestCase {
             String orQuery = Strings.format("%s OR %s OR %s", queryA, queryB, queryC);
 
             BoolQueryBuilder parsedQuery = asInstanceOf(BoolQueryBuilder.class, parseKqlQuery(orQuery));
-            assertThat(parsedQuery.should(), hasSize(3));
-            assertThat(
-                parsedQuery.should(),
-                allOf(
-                    hasItem(equalTo((parseKqlQuery(queryA)))),
-                    hasItem(equalTo((parseKqlQuery(queryB)))),
-                    hasItem(equalTo((parseKqlQuery(queryC))))
-                )
-            );
+
+            if (Stream.of(queryA, queryB, queryC).noneMatch(BOOLEAN_QUERY_FILTER)) {
+                // If one of the subquery is a boolean query, it is impossible to test the content of the generated query because
+                // operator precedence rules are applied. There are specific tests for it in testOperatorPrecedence.
+                assertThat(parsedQuery.should(), hasSize(3));
+                assertThat(
+                    parsedQuery.should(),
+                    allOf(
+                        hasItem(equalTo((parseKqlQuery(queryA)))),
+                        hasItem(equalTo((parseKqlQuery(queryB)))),
+                        hasItem(equalTo((parseKqlQuery(queryC))))
+                    )
+                );
+            }
         }
     }
 
     public void testParseAndQuery() throws IOException {
-        List<String> supportedQueries = readQueries(SUPPORTED_QUERY_FILE_PATH, Predicate.not(BOOLEAN_QUERY_FILTER));
+        List<String> supportedQueries = readQueries(SUPPORTED_QUERY_FILE_PATH);
 
         for (int runs = 0; runs < 100; runs++) {
             String queryA = randomFrom(supportedQueries);
@@ -94,11 +106,15 @@ public class KqlParserBooleanQueryTests extends AbstractKqlParserTestCase {
 
             BoolQueryBuilder parsedQuery = asInstanceOf(BoolQueryBuilder.class, parseKqlQuery(andQuery));
 
-            assertThat(parsedQuery.filter(), empty());
-            assertThat(parsedQuery.should(), empty());
-            assertThat(parsedQuery.mustNot(), empty());
-            assertThat(parsedQuery.must(), hasSize(2));
-            assertThat(parsedQuery.must(), allOf(hasItem(equalTo((parseKqlQuery(queryA)))), hasItem(equalTo((parseKqlQuery(queryB))))));
+            if (Stream.of(queryA, queryB).noneMatch(BOOLEAN_QUERY_FILTER)) {
+                // If one of the subquery is a boolean query, it is impossible to test the content of the generated query because
+                // operator precedence rules are applied. There are specific tests for it in testOperatorPrecedence.
+                assertThat(parsedQuery.filter(), empty());
+                assertThat(parsedQuery.should(), empty());
+                assertThat(parsedQuery.mustNot(), empty());
+                assertThat(parsedQuery.must(), hasSize(2));
+                assertThat(parsedQuery.must(), allOf(hasItem(equalTo((parseKqlQuery(queryA)))), hasItem(equalTo((parseKqlQuery(queryB))))));
+            }
         }
 
         for (int runs = 0; runs < 100; runs++) {
@@ -107,22 +123,24 @@ public class KqlParserBooleanQueryTests extends AbstractKqlParserTestCase {
             String queryC = randomFrom(supportedQueries);
             String andQuery = Strings.format("%s AND %s AND %s", queryA, queryB, queryC);
 
-            BoolQueryBuilder parsedQuery = asInstanceOf(BoolQueryBuilder.class, parseKqlQuery(andQuery));
-            assertThat(parsedQuery.must(), hasSize(3));
-            assertThat(
-                parsedQuery.must(),
-                allOf(
-                    hasItem(equalTo((parseKqlQuery(queryA)))),
-                    hasItem(equalTo((parseKqlQuery(queryB)))),
-                    hasItem(equalTo((parseKqlQuery(queryC))))
-                )
-            );
+            if (Stream.of(queryA, queryB, queryC).noneMatch(BOOLEAN_QUERY_FILTER)) {
+                // If one of the subquery is a boolean query, it is impossible to test the content of the generated query because
+                // operator precedence rules are applied. There are specific tests for it in testOperatorPrecedence.
+                BoolQueryBuilder parsedQuery = asInstanceOf(BoolQueryBuilder.class, parseKqlQuery(andQuery));
+                assertThat(parsedQuery.must(), hasSize(3));
+                assertThat(
+                    parsedQuery.must(),
+                    allOf(
+                        hasItem(equalTo((parseKqlQuery(queryA)))),
+                        hasItem(equalTo((parseKqlQuery(queryB)))),
+                        hasItem(equalTo((parseKqlQuery(queryC))))
+                    )
+                );
+            }
         }
     }
 
     public void testOperatorPrecedence() throws IOException {
-        KqlParser parser = new KqlParser();
-        SearchExecutionContext searchExecutionContext = createSearchExecutionContext();
         List<String> supportedQueries = readQueries(SUPPORTED_QUERY_FILE_PATH, Predicate.not(BOOLEAN_QUERY_FILTER));
 
         for (int runs = 0; runs < 100; runs++) {

bd52d90fa7b545d05900e493437deab403e8b3e7
Aurelien FOUCRET <aurelien.foucret@elastic.co>
2024-11-01 14:57:17 +0100

    Add the KQL query to snapshot build.

--- x-pack/plugin/kql/src/main/java/org/elasticsearch/xpack/kql/KqlPlugin.java
+++ x-pack/plugin/kql/src/main/java/org/elasticsearch/xpack/kql/KqlPlugin.java
@@ -7,10 +7,36 @@
 
 package org.elasticsearch.xpack.kql;
 
+import org.apache.lucene.util.SetOnce;
+import org.elasticsearch.features.FeatureService;
 import org.elasticsearch.plugins.ExtensiblePlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.plugins.SearchPlugin;
+import org.elasticsearch.xpack.kql.query.KqlQueryBuilder;
+
+import java.util.Collection;
+import java.util.List;
 
 public class KqlPlugin extends Plugin implements SearchPlugin, ExtensiblePlugin {
 
+    private SetOnce<FeatureService> kqlFeatureService;
+
+    @Override
+    public Collection<?> createComponents(PluginServices services) {
+        kqlFeatureService.set(new FeatureService(List.of(new KqlFeatures())));
+        return super.createComponents(services);
+    }
+
+    @Override
+    public List<QuerySpec<?>> getQueries() {
+        if (hasKqlQueryFeature()) {
+            return List.of(new SearchPlugin.QuerySpec<>(KqlQueryBuilder.NAME, KqlQueryBuilder::new, KqlQueryBuilder::fromXContent));
+        }
+
+        return List.of();
+    }
+
+    private boolean hasKqlQueryFeature() {
+        return kqlFeatureService.get().getNodeFeatures().containsKey(KqlQueryBuilder.KQL_QUERY_SUPPORTED.id());
+    }
 }
---
+++ x-pack/plugin/kql/src/main/java/org/elasticsearch/xpack/kql/query/KqlQueryBuilder.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.kql.query;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.lucene.search.Query;
+import org.elasticsearch.TransportVersion;
+import org.elasticsearch.common.ParsingException;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.features.NodeFeature;
+import org.elasticsearch.index.query.AbstractQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.SearchExecutionContext;
+import org.elasticsearch.xcontent.ConstructingObjectParser;
+import org.elasticsearch.xcontent.ParseField;
+import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xcontent.XContentParser;
+import org.elasticsearch.xpack.kql.parser.KqlParser;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import static org.elasticsearch.xcontent.ConstructingObjectParser.constructorArg;
+
+public class KqlQueryBuilder extends AbstractQueryBuilder<KqlQueryBuilder> {
+    public static final NodeFeature KQL_QUERY_SUPPORTED = new NodeFeature("kql_query_supported");
+    public static final String NAME = "kql";
+    public static final ParseField QUERY_FIELD = new ParseField("query");
+    private static final Logger log = LogManager.getLogger(KqlQueryBuilder.class);
+    private static final ConstructingObjectParser<KqlQueryBuilder, Void> PARSER = new ConstructingObjectParser<>(
+        NAME,
+        a -> new KqlQueryBuilder((String) a[0])
+    );
+
+    static {
+        PARSER.declareString(constructorArg(), QUERY_FIELD);
+        declareStandardFields(PARSER);
+    }
+
+    private final String query;
+
+    public KqlQueryBuilder(String query) {
+        this.query = Objects.requireNonNull(query, "query can not be null");
+    }
+
+    public KqlQueryBuilder(StreamInput in) throws IOException {
+        super(in);
+        query = in.readString();
+    }
+
+    public static KqlQueryBuilder fromXContent(XContentParser parser) {
+        try {
+            return PARSER.apply(parser, null);
+        } catch (IllegalArgumentException e) {
+            throw new ParsingException(parser.getTokenLocation(), e.getMessage(), e);
+        }
+    }
+
+    @Override
+    protected void doXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.startObject(NAME);
+        builder.field(QUERY_FIELD.getPreferredName(), query);
+        boostAndQueryNameToXContent(builder);
+        builder.endObject();
+    }
+
+    @Override
+
+    protected Query doToQuery(SearchExecutionContext context) throws IOException {
+        QueryBuilder queryBuilder = new KqlParser().parseKqlQuery(query, context);
+
+        if (log.isTraceEnabled()) {
+            log.trace("KQL query {} translated to Query DSL: {}", query, Strings.toString(queryBuilder));
+        }
+
+        return queryBuilder.toQuery(context);
+    }
+
+    protected void doWriteTo(StreamOutput out) throws IOException {
+        out.writeString(query);
+    }
+
+    @Override
+
+    public String getWriteableName() {
+        return NAME;
+    }
+
+    @Override
+
+    protected int doHashCode() {
+        return Objects.hash(query);
+    }
+
+    @Override
+
+    protected boolean doEquals(KqlQueryBuilder other) {
+        return Objects.equals(query, other.query);
+    }
+
+    @Override
+
+    public TransportVersion getMinimalSupportedVersion() {
+        // TODO: Create a transport versions.
+        return TransportVersion.current();
+    }
+}

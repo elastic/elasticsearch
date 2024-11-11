2bb583ecb849e839f22e18361cb18ff216e0689b
Yang Wang <yang.wang@elastic.co>
2024-11-11 11:16:41 +1100

    separate test class

--- qa/full-cluster-restart/src/javaRestTest/java/org/elasticsearch/upgrades/FullClusterRestartTestOrdering.java
+++ qa/full-cluster-restart/src/javaRestTest/java/org/elasticsearch/upgrades/FullClusterRestartTestOrdering.java
@@ -11,27 +11,15 @@ package org.elasticsearch.upgrades;
 
 import com.carrotsearch.randomizedtesting.TestMethodAndParams;
 
-import org.elasticsearch.test.AnnotationTestOrdering;
-
 import java.util.Comparator;
 
 public class FullClusterRestartTestOrdering implements Comparator<TestMethodAndParams> {
     @Override
     public int compare(TestMethodAndParams o1, TestMethodAndParams o2) {
-        final int result = Integer.compare(getOrdinal(o1), getOrdinal(o2));
-        if (result == 0) {
-            return Integer.compare(getOrderValue(o1), getOrderValue(o2));
-        }
-        return result;
+        return Integer.compare(getOrdinal(o1), getOrdinal(o2));
     }
 
     private int getOrdinal(TestMethodAndParams t) {
         return ((FullClusterRestartUpgradeStatus) t.getInstanceArguments().get(0)).ordinal();
-    }
-
-    private int getOrderValue(TestMethodAndParams t) {
-        return t.getTestMethod().isAnnotationPresent(AnnotationTestOrdering.Order.class)
-            ? t.getTestMethod().getAnnotation(AnnotationTestOrdering.Order.class).value()
-            : Integer.MAX_VALUE;
     }
 }

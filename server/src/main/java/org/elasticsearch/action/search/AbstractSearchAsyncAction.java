7074e9e1633dc517cfffbf4a054581b1694dcc50
Armin Braun <me@obrown.io>
2024-11-12 10:23:32 +0100

    Remove some dead code from SearchPhase and friends
    The separate `onFailure` is unnecessary, just fail the phase like we do
    elsewhere. Also make utility method static.

--- server/src/main/java/org/elasticsearch/action/search/AbstractSearchAsyncAction.java
+++ server/src/main/java/org/elasticsearch/action/search/AbstractSearchAsyncAction.java
@@ -731,7 +731,7 @@ abstract class AbstractSearchAsyncAction<Result extends SearchPhaseResult> exten
      * @see #onShardFailure(int, SearchShardTarget, Exception)
      * @see #onShardResult(SearchPhaseResult, SearchShardIterator)
      */
-    final void onPhaseDone() {  // as a tribute to @kimchy aka. finishHim()
+    private void onPhaseDone() {  // as a tribute to @kimchy aka. finishHim()
         executeNextPhase(this, this::getNextPhase);
     }
 
@@ -752,13 +752,6 @@ abstract class AbstractSearchAsyncAction<Result extends SearchPhaseResult> exten
 
     public final void execute(Runnable command) {
         executor.execute(command);
-    }
-
-    /**
-      * Notifies the top-level listener of the provided exception
-    */
-    public void onFailure(Exception e) {
-        listener.onFailure(e);
     }
 
     /**

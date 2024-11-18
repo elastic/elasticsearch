6d45172d32d515502879a890dd2febddddaba1f0
David Kyle <david.kyle@elastic.co>
2024-11-18 13:41:33 +0000

    [ML] Enable built-in Inference Endpoints and default for Semantic Text (#116931)
    Adds built-in inference endpoints for the ELSER (.elser-2-elasticsearch)
    and multilingual-e5-small models (.multilingual-e5-small-elasticsearch).
    The semantic text inference Id field now defaults to elser-2-elasticsearch
    # Conflicts:
    #	test/test-clusters/src/main/java/org/elasticsearch/test/cluster/FeatureFlag.java
    #	x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/InferenceFeatures.java

--- x-pack/plugin/inference/qa/inference-service-tests/src/javaRestTest/java/org/elasticsearch/xpack/inference/DefaultEndPointsIT.java
+++ x-pack/plugin/inference/qa/inference-service-tests/src/javaRestTest/java/org/elasticsearch/xpack/inference/DefaultEndPointsIT.java
@@ -39,7 +39,6 @@ public class DefaultEndPointsIT extends InferenceBaseRestTest {
 
     @SuppressWarnings("unchecked")
     public void testInferDeploysDefaultElser() throws IOException {
-        assumeTrue("Default config requires a feature flag", DefaultElserFeatureFlag.isEnabled());
         var model = getModel(ElasticsearchInternalService.DEFAULT_ELSER_ID);
         assertDefaultElserConfig(model);
 
@@ -70,7 +69,6 @@ public class DefaultEndPointsIT extends InferenceBaseRestTest {
 
     @SuppressWarnings("unchecked")
     public void testInferDeploysDefaultE5() throws IOException {
-        assumeTrue("Default config requires a feature flag", DefaultElserFeatureFlag.isEnabled());
         var model = getModel(ElasticsearchInternalService.DEFAULT_E5_ID);
         assertDefaultE5Config(model);
 

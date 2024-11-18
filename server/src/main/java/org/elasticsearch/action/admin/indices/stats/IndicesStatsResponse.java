037b18e9127147d6cf484167fed86b8e6cf385a7
David Turner <david.turner@elastic.co>
2024-11-18 15:32:40 +0000

    Revert "Index stats enhancement: creation date and tier_preference (#116339)"
    This reverts commit e0af1238fcdf44ebe838b1da188c8fb46a915ea1.

--- server/src/main/java/org/elasticsearch/action/admin/indices/stats/IndicesStatsResponse.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/stats/IndicesStatsResponse.java
@@ -47,10 +47,6 @@ public class IndicesStatsResponse extends ChunkedBroadcastResponse {
 
     private final Map<String, IndexMetadata.State> indexStateMap;
 
-    private final Map<String, List<String>> indexTierPreferenceMap;
-
-    private final Map<String, Long> indexCreationDateMap;
-
     private final ShardStats[] shards;
 
     private Map<ShardRouting, ShardStats> shardStatsMap;
@@ -58,23 +54,22 @@ public class IndicesStatsResponse extends ChunkedBroadcastResponse {
     IndicesStatsResponse(StreamInput in) throws IOException {
         super(in);
         shards = in.readArray(ShardStats::new, ShardStats[]::new);
-        if (in.getTransportVersion().onOrAfter(TransportVersions.INDEX_STATS_ADDITIONAL_FIELDS)) {
+        if (in.getTransportVersion().onOrAfter(TransportVersions.INDEX_STATS_ADDITIONAL_FIELDS_REVERT)) {
             indexHealthMap = in.readMap(ClusterHealthStatus::readFrom);
             indexStateMap = in.readMap(IndexMetadata.State::readFrom);
-            indexTierPreferenceMap = in.readMap(StreamInput::readStringCollectionAsList);
-            indexCreationDateMap = in.readMap(StreamInput::readLong);
+        } else if (in.getTransportVersion().onOrAfter(TransportVersions.INDEX_STATS_ADDITIONAL_FIELDS)) {
+            indexHealthMap = in.readMap(ClusterHealthStatus::readFrom);
+            indexStateMap = in.readMap(IndexMetadata.State::readFrom);
+            in.readMap(StreamInput::readStringCollectionAsList); // unused, reverted
+            in.readMap(StreamInput::readLong); // unused, reverted
         } else if (in.getTransportVersion().onOrAfter(TransportVersions.V_8_1_0)) {
             // Between 8.1 and INDEX_STATS_ADDITIONAL_FIELDS, we had a different format for the response
             // where we only had health and state available.
             indexHealthMap = in.readMap(ClusterHealthStatus::readFrom);
             indexStateMap = in.readMap(IndexMetadata.State::readFrom);
-            indexTierPreferenceMap = Map.of();
-            indexCreationDateMap = Map.of();
         } else {
             indexHealthMap = Map.of();
             indexStateMap = Map.of();
-            indexTierPreferenceMap = Map.of();
-            indexCreationDateMap = Map.of();
         }
     }
 
@@ -94,8 +89,6 @@ public class IndicesStatsResponse extends ChunkedBroadcastResponse {
         Objects.requireNonNull(shards);
         Map<String, ClusterHealthStatus> indexHealthModifiableMap = new HashMap<>();
         Map<String, IndexMetadata.State> indexStateModifiableMap = new HashMap<>();
-        Map<String, List<String>> indexTierPreferenceModifiableMap = new HashMap<>();
-        Map<String, Long> indexCreationDateModifiableMap = new HashMap<>();
         for (ShardStats shard : shards) {
             Index index = shard.getShardRouting().index();
             IndexMetadata indexMetadata = metadata.index(index);
@@ -105,14 +98,10 @@ public class IndicesStatsResponse extends ChunkedBroadcastResponse {
                     ignored -> new ClusterIndexHealth(indexMetadata, routingTable.index(index)).getStatus()
                 );
                 indexStateModifiableMap.computeIfAbsent(index.getName(), ignored -> indexMetadata.getState());
-                indexTierPreferenceModifiableMap.computeIfAbsent(index.getName(), ignored -> indexMetadata.getTierPreference());
-                indexCreationDateModifiableMap.computeIfAbsent(index.getName(), ignored -> indexMetadata.getCreationDate());
             }
         }
         indexHealthMap = unmodifiableMap(indexHealthModifiableMap);
         indexStateMap = unmodifiableMap(indexStateModifiableMap);
-        indexTierPreferenceMap = unmodifiableMap(indexTierPreferenceModifiableMap);
-        indexCreationDateMap = unmodifiableMap(indexCreationDateModifiableMap);
     }
 
     public Map<ShardRouting, ShardStats> asMap() {
@@ -150,14 +139,7 @@ public class IndicesStatsResponse extends ChunkedBroadcastResponse {
             Index index = shard.getShardRouting().index();
             IndexStatsBuilder indexStatsBuilder = indexToIndexStatsBuilder.computeIfAbsent(
                 index.getName(),
-                k -> new IndexStatsBuilder(
-                    k,
-                    index.getUUID(),
-                    indexHealthMap.get(index.getName()),
-                    indexStateMap.get(index.getName()),
-                    indexTierPreferenceMap.get(index.getName()),
-                    indexCreationDateMap.get(index.getName())
-                )
+                k -> new IndexStatsBuilder(k, index.getUUID(), indexHealthMap.get(index.getName()), indexStateMap.get(index.getName()))
             );
             indexStatsBuilder.add(shard);
         }
@@ -202,12 +184,14 @@ public class IndicesStatsResponse extends ChunkedBroadcastResponse {
     public void writeTo(StreamOutput out) throws IOException {
         super.writeTo(out);
         out.writeArray(shards);
-        if (out.getTransportVersion().onOrAfter(TransportVersions.INDEX_STATS_ADDITIONAL_FIELDS)) {
+        if (out.getTransportVersion().onOrAfter(TransportVersions.INDEX_STATS_ADDITIONAL_FIELDS_REVERT)) {
             out.writeMap(indexHealthMap, StreamOutput::writeWriteable);
             out.writeMap(indexStateMap, StreamOutput::writeWriteable);
-            out.writeMap(indexTierPreferenceMap, StreamOutput::writeStringCollection);
-            out.writeMap(indexCreationDateMap, StreamOutput::writeLong);
-
+        } else if (out.getTransportVersion().onOrAfter(TransportVersions.INDEX_STATS_ADDITIONAL_FIELDS)) {
+            out.writeMap(indexHealthMap, StreamOutput::writeWriteable);
+            out.writeMap(indexStateMap, StreamOutput::writeWriteable);
+            out.writeMap(Map.of(), StreamOutput::writeStringCollection);
+            out.writeMap(Map.of(), StreamOutput::writeLong);
         } else if (out.getTransportVersion().onOrAfter(TransportVersions.V_8_1_0)) {
             out.writeMap(indexHealthMap, StreamOutput::writeWriteable);
             out.writeMap(indexStateMap, StreamOutput::writeWriteable);
@@ -236,12 +220,6 @@ public class IndicesStatsResponse extends ChunkedBroadcastResponse {
                             }
                             if (indexStats.getState() != null) {
                                 builder.field("status", indexStats.getState().toString().toLowerCase(Locale.ROOT));
-                            }
-                            if (indexStats.getTierPreference() != null) {
-                                builder.field("tier_preference", indexStats.getTierPreference());
-                            }
-                            if (indexStats.getCreationDate() != null) {
-                                builder.field("creation_date", indexStats.getCreationDate());
                             }
                             builder.startObject("primaries");
                             indexStats.getPrimaries().toXContent(builder, p);

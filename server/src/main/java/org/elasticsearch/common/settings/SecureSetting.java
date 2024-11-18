bb329efb796a31a81cf1a05a7d5c842befc0d5f0
David Turner <david.turner@elastic.co>
2024-11-18 08:58:23 +0000

    Improve message about insecure S3 settings
    Clarifies that insecure settings are stored in plaintext and must not be
    used. Also removes the mention of the (wrong) system property from the
    error message if insecure settings are not permitted.

--- server/src/main/java/org/elasticsearch/common/settings/SecureSetting.java
+++ server/src/main/java/org/elasticsearch/common/settings/SecureSetting.java
@@ -185,9 +185,7 @@ public abstract class SecureSetting<T> extends Setting<T> {
         @Override
         public SecureString get(Settings settings) {
             if (ALLOW_INSECURE_SETTINGS == false && exists(settings)) {
-                throw new IllegalArgumentException(
-                    "Setting [" + name + "] is insecure, " + "but property [allow_insecure_settings] is not set"
-                );
+                throw new IllegalArgumentException("Setting [" + name + "] is insecure and no longer supported");
             }
             return super.get(settings);
         }

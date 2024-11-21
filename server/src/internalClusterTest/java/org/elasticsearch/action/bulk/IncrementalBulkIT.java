45d59900d0a5a199a288606097fa9e9548bab742
Ankita Kumar <ankita.kumar@elastic.co>
2024-11-21 17:52:45 -0500

    Metrics for incremental bulk splits (#116765) (#117280)
    Backport of PR #116765. Add metrics to track incremental bulk request splits due to indexing pressure. Resolves ES-9612

--- server/src/internalClusterTest/java/org/elasticsearch/action/bulk/IncrementalBulkIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/action/bulk/IncrementalBulkIT.java
@@ -66,7 +66,7 @@ public class IncrementalBulkIT extends ESIntegTestCase {
             .put(super.nodeSettings(nodeOrdinal, otherSettings))
             .put(IndexingPressure.SPLIT_BULK_LOW_WATERMARK.getKey(), "512B")
             .put(IndexingPressure.SPLIT_BULK_LOW_WATERMARK_SIZE.getKey(), "2048B")
-            .put(IndexingPressure.SPLIT_BULK_HIGH_WATERMARK.getKey(), "2KB")
+            .put(IndexingPressure.SPLIT_BULK_HIGH_WATERMARK.getKey(), "4KB")
             .put(IndexingPressure.SPLIT_BULK_HIGH_WATERMARK_SIZE.getKey(), "1024B")
             .build();
     }
@@ -162,6 +162,8 @@ public class IncrementalBulkIT extends ESIntegTestCase {
 
         IndexRequest indexRequest = indexRequest(index);
         long total = indexRequest.ramBytesUsed();
+        long lowWaterMarkSplits = indexingPressure.stats().getLowWaterMarkSplits();
+        long highWaterMarkSplits = indexingPressure.stats().getHighWaterMarkSplits();
         while (total < 2048) {
             refCounted.incRef();
             handler.addItems(List.of(indexRequest), refCounted::decRef, () -> nextPage.set(true));
@@ -176,6 +178,8 @@ public class IncrementalBulkIT extends ESIntegTestCase {
         handler.addItems(List.of(indexRequest(index)), refCounted::decRef, () -> nextPage.set(true));
 
         assertBusy(() -> assertThat(indexingPressure.stats().getCurrentCombinedCoordinatingAndPrimaryBytes(), equalTo(0L)));
+        assertBusy(() -> assertThat(indexingPressure.stats().getLowWaterMarkSplits(), equalTo(lowWaterMarkSplits + 1)));
+        assertThat(indexingPressure.stats().getHighWaterMarkSplits(), equalTo(highWaterMarkSplits));
 
         PlainActionFuture<BulkResponse> future = new PlainActionFuture<>();
         handler.lastItems(List.of(indexRequest), refCounted::decRef, future);
@@ -193,6 +197,8 @@ public class IncrementalBulkIT extends ESIntegTestCase {
         IncrementalBulkService incrementalBulkService = internalCluster().getInstance(IncrementalBulkService.class, nodeName);
         IndexingPressure indexingPressure = internalCluster().getInstance(IndexingPressure.class, nodeName);
         ThreadPool threadPool = internalCluster().getInstance(ThreadPool.class, nodeName);
+        long lowWaterMarkSplits = indexingPressure.stats().getLowWaterMarkSplits();
+        long highWaterMarkSplits = indexingPressure.stats().getHighWaterMarkSplits();
 
         AbstractRefCounted refCounted = AbstractRefCounted.of(() -> {});
         AtomicBoolean nextPage = new AtomicBoolean(false);
@@ -218,6 +224,8 @@ public class IncrementalBulkIT extends ESIntegTestCase {
         handlerNoThrottle.addItems(requestsNoThrottle, refCounted::decRef, () -> nextPage.set(true));
         assertTrue(nextPage.get());
         nextPage.set(false);
+        assertThat(indexingPressure.stats().getHighWaterMarkSplits(), equalTo(highWaterMarkSplits));
+        assertThat(indexingPressure.stats().getLowWaterMarkSplits(), equalTo(lowWaterMarkSplits));
 
         ArrayList<DocWriteRequest<?>> requestsThrottle = new ArrayList<>();
         // Test that a request larger than SPLIT_BULK_HIGH_WATERMARK_SIZE (1KB) is throttled
@@ -236,6 +244,8 @@ public class IncrementalBulkIT extends ESIntegTestCase {
 
         // Wait until we are ready for the next page
         assertBusy(() -> assertTrue(nextPage.get()));
+        assertBusy(() -> assertThat(indexingPressure.stats().getHighWaterMarkSplits(), equalTo(highWaterMarkSplits + 1)));
+        assertThat(indexingPressure.stats().getLowWaterMarkSplits(), equalTo(lowWaterMarkSplits));
 
         for (IncrementalBulkService.Handler h : handlers) {
             refCounted.incRef();

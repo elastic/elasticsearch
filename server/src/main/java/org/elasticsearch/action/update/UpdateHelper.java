2fefe88dcb8b0056e7715e4c0839a789e5f811d6
Mike Pellegrini <mike.pellegrini@elastic.co>
2024-11-22 11:34:53 -0500

    spotless

--- server/src/main/java/org/elasticsearch/action/update/UpdateHelper.java
+++ server/src/main/java/org/elasticsearch/action/update/UpdateHelper.java
@@ -44,7 +44,6 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.LongSupplier;
 

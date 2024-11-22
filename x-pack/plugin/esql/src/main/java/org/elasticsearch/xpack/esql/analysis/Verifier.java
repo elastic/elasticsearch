a83473422fff46f2862ef207a28a51b95490ddf8
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2024-11-22 10:47:23 +0100

    ES|QL: fix validation of SORT by aggregate functions

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/Verifier.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/Verifier.java
@@ -114,6 +114,14 @@ public class Verifier {
 
             if (p instanceof Unresolvable u) {
                 failures.add(fail(p, u.unresolvedMessage()));
+            } else if (p instanceof OrderBy ob) {
+                ob.order().forEach(o -> {
+                    o.forEachDown(Function.class, f -> {
+                        if (f instanceof AggregateFunction) {
+                            failures.add(fail(f, "Aggregate functions are not allowed in SORT [{}]", f.functionName()));
+                        }
+                    });
+                });
             }
             // p is resolved, skip
             else if (p.resolved()) {

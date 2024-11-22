edfc6d145728fc681ddde8ff265b576e7783b630
Yang Wang <yang.wang@elastic.co>
2024-11-22 18:37:10 +1100

    Mark Project APIs as internal (MP-1798)
    Mark these APIs as internal otherwise they are not accessible in
    stateless.

--- modules/multi-project/src/main/java/org/elasticsearch/multiproject/action/RestDeleteProjectAction.java
+++ modules/multi-project/src/main/java/org/elasticsearch/multiproject/action/RestDeleteProjectAction.java
@@ -13,6 +13,8 @@ import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.cluster.metadata.ProjectId;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.rest.Scope;
+import org.elasticsearch.rest.ServerlessScope;
 import org.elasticsearch.rest.action.RestToXContentListener;
 
 import java.io.IOException;
@@ -21,6 +23,7 @@ import java.util.List;
 import static org.elasticsearch.rest.RestUtils.getAckTimeout;
 import static org.elasticsearch.rest.RestUtils.getMasterNodeTimeout;
 
+@ServerlessScope(Scope.INTERNAL)
 public class RestDeleteProjectAction extends BaseRestHandler {
 
     @Override

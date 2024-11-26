635e82e20bd7e48f020d38bbfdac6c3d1a90b542
David Turner <david.turner@elastic.co>
2024-11-26 09:06:02 +0000

    Support dynamic credentials in `S3HttpFixture`
    Rephrase the authorization check in `S3HttpFixture` in terms of a
    predicate provided by the caller so that there's no need for a separate
    subclass that handles session tokens, and so that it can support
    auto-generated credentials more naturally.
    
    Also adapts `Ec2ImdsHttpFixture` to dynamically generate credentials
    this way.
    
    Also extracts the STS fixture in `S3HttpFixtureWithSTS` into a separate
    service, similarly to #117324, and adapts this new fixture to
    dynamically generate credentials too.
    
    Relates ES-9984
    
    Backport of #117458 to 8.x

--- test/fixtures/ec2-imds-fixture/src/main/java/fixture/aws/imds/Ec2ImdsHttpHandler.java
+++ test/fixtures/ec2-imds-fixture/src/main/java/fixture/aws/imds/Ec2ImdsHttpHandler.java
@@ -25,6 +25,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.BiConsumer;
 
 import static org.elasticsearch.test.ESTestCase.randomIdentifier;
 import static org.elasticsearch.test.ESTestCase.randomSecretKey;
@@ -37,13 +38,11 @@ public class Ec2ImdsHttpHandler implements HttpHandler {
 
     private static final String IMDS_SECURITY_CREDENTIALS_PATH = "/latest/meta-data/iam/security-credentials/";
 
-    private final String accessKey;
-    private final String sessionToken;
+    private final BiConsumer<String, String> newCredentialsConsumer;
     private final Set<String> validCredentialsEndpoints = ConcurrentCollections.newConcurrentSet();
 
-    public Ec2ImdsHttpHandler(String accessKey, String sessionToken, Collection<String> alternativeCredentialsEndpoints) {
-        this.accessKey = Objects.requireNonNull(accessKey);
-        this.sessionToken = Objects.requireNonNull(sessionToken);
+    public Ec2ImdsHttpHandler(BiConsumer<String, String> newCredentialsConsumer, Collection<String> alternativeCredentialsEndpoints) {
+        this.newCredentialsConsumer = Objects.requireNonNull(newCredentialsConsumer);
         this.validCredentialsEndpoints.addAll(alternativeCredentialsEndpoints);
     }
 
@@ -71,6 +70,9 @@ public class Ec2ImdsHttpHandler implements HttpHandler {
                     exchange.getResponseBody().write(response);
                     return;
                 } else if (validCredentialsEndpoints.contains(path)) {
+                    final String accessKey = randomIdentifier();
+                    final String sessionToken = randomIdentifier();
+                    newCredentialsConsumer.accept(accessKey, sessionToken);
                     final byte[] response = Strings.format(
                         """
                             {

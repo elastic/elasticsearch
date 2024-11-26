4649c0c3dd274597db4668028842d508e9931062
Brian Seeders <brian.seeders@elastic.co>
2024-11-26 14:20:49 -0500

    Don't run for pipeline upload, and also add missing script

---
+++ .buildkite/scripts/get-latest-test-mutes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+testMuteBranch="${BUILDKITE_PULL_REQUEST_BASE_BRANCH:-main}"
+testMuteFile="$(mktemp)"
+
+# If this PR contains changes to muted-tests.yml, we disable this functionality
+# Otherwise, we wouldn't be able to test unmutes
+if [[ ! $(gh pr diff --name-only | grep 'muted-tests.yml') ]]; then
+  gh api -H 'Accept: application/vnd.github.v3.raw' "repos/elastic/elasticsearch/contents/muted-tests.yml?ref=$testMuteBranch" > "$testMuteFile"
+
+  if [[ -s "$testMuteFile" ]]; then
+    export GRADLE_OPTS="${GRADLE_OPTS:-} -Dorg.gradle.project.org.elasticsearch.additional.muted.tests=$testMuteFile"
+  fi
+fi

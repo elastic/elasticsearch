b13e0d25c0ef52bf6236a981bee4823b12934a57
David Turner <david.turner@elastic.co>
2024-11-26 09:06:02 +0000

    Support dynamic credentials in `S3HttpFixture` (#117458)
    Rephrase the authorization check in `S3HttpFixture` in terms of a
    predicate provided by the caller so that there's no need for a separate
    subclass that handles session tokens, and so that it can support
    auto-generated credentials more naturally.
    
    Also adapts `Ec2ImdsHttpFixture` to dynamically generate credentials
    this way.
    
    Also extracts the STS fixture in `S3HttpFixtureWithSTS` into a separate
    service, similarly to #117324, and adapts this new fixture to
    dynamically generate credentials too.
    
    Relates ES-9984

--- test/fixtures/aws-sts-fixture/src/test/java/fixture/aws/sts/AwsStsHttpHandlerTests.java
+++ test/fixtures/aws-sts-fixture/src/test/java/fixture/aws/sts/AwsStsHttpHandlerTests.java
@@ -7,69 +7,142 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-package fixture.aws.imds;
+package fixture.aws.sts;
 
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpPrincipal;
 
-import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
-import org.elasticsearch.common.xcontent.XContentHelper;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.test.ESTestCase;
-import org.elasticsearch.xcontent.XContentType;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.aMapWithSize;
+import static org.hamcrest.Matchers.containsString;
 
-public class Ec2ImdsHttpHandlerTests extends ESTestCase {
+public class AwsStsHttpHandlerTests extends ESTestCase {
 
-    public void testImdsV1() throws IOException {
+    public void testGenerateCredentials() {
         final Map<String, String> generatedCredentials = new HashMap<>();
 
-        final var handler = new Ec2ImdsHttpHandler(generatedCredentials::put, Set.of());
+        final var webIdentityToken = randomUnicodeOfLength(10);
+        final var handler = new AwsStsHttpHandler(generatedCredentials::put, webIdentityToken);
 
-        final var roleResponse = handleRequest(handler, "GET", "/latest/meta-data/iam/security-credentials/");
-        assertEquals(RestStatus.OK, roleResponse.status());
-        final var profileName = roleResponse.body().utf8ToString();
-        assertTrue(Strings.hasText(profileName));
-
-        final var credentialsResponse = handleRequest(handler, "GET", "/latest/meta-data/iam/security-credentials/" + profileName);
-        assertEquals(RestStatus.OK, credentialsResponse.status());
+        final var response = handleRequest(
+            handler,
+            Map.of(
+                "Action",
+                "AssumeRoleWithWebIdentity",
+                "RoleSessionName",
+                AwsStsHttpHandler.ROLE_NAME,
+                "RoleArn",
+                AwsStsHttpHandler.ROLE_ARN,
+                "WebIdentityToken",
+                webIdentityToken
+            )
+        );
+        assertEquals(RestStatus.OK, response.status());
 
         assertThat(generatedCredentials, aMapWithSize(1));
         final var accessKey = generatedCredentials.keySet().iterator().next();
         final var sessionToken = generatedCredentials.values().iterator().next();
 
-        final var responseMap = XContentHelper.convertToMap(XContentType.JSON.xContent(), credentialsResponse.body().streamInput(), false);
-        assertEquals(Set.of("AccessKeyId", "Expiration", "RoleArn", "SecretAccessKey", "Token"), responseMap.keySet());
-        assertEquals(accessKey, responseMap.get("AccessKeyId"));
-        assertEquals(sessionToken, responseMap.get("Token"));
+        final var responseBody = response.body().utf8ToString();
+        assertThat(responseBody, containsString("<AccessKeyId>" + accessKey + "</AccessKeyId>"));
+        assertThat(responseBody, containsString("<SessionToken>" + sessionToken + "</SessionToken>"));
     }
 
-    public void testImdsV2Disabled() {
-        assertEquals(
-            RestStatus.METHOD_NOT_ALLOWED,
-            handleRequest(new Ec2ImdsHttpHandler((accessKey, sessionToken) -> fail(), Set.of()), "PUT", "/latest/api/token").status()
+    public void testInvalidAction() {
+        final var handler = new AwsStsHttpHandler((key, token) -> fail(), randomUnicodeOfLength(10));
+        final var response = handleRequest(handler, Map.of("Action", "Unsupported"));
+        assertEquals(RestStatus.BAD_REQUEST, response.status());
+    }
+
+    public void testInvalidRole() {
+        final var webIdentityToken = randomUnicodeOfLength(10);
+        final var handler = new AwsStsHttpHandler((key, token) -> fail(), webIdentityToken);
+        final var response = handleRequest(
+            handler,
+            Map.of(
+                "Action",
+                "AssumeRoleWithWebIdentity",
+                "RoleSessionName",
+                randomValueOtherThan(AwsStsHttpHandler.ROLE_NAME, ESTestCase::randomIdentifier),
+                "RoleArn",
+                AwsStsHttpHandler.ROLE_ARN,
+                "WebIdentityToken",
+                webIdentityToken
+            )
         );
+        assertEquals(RestStatus.UNAUTHORIZED, response.status());
+    }
+
+    public void testInvalidToken() {
+        final var webIdentityToken = randomUnicodeOfLength(10);
+        final var handler = new AwsStsHttpHandler((key, token) -> fail(), webIdentityToken);
+        final var response = handleRequest(
+            handler,
+            Map.of(
+                "Action",
+                "AssumeRoleWithWebIdentity",
+                "RoleSessionName",
+                AwsStsHttpHandler.ROLE_NAME,
+                "RoleArn",
+                AwsStsHttpHandler.ROLE_ARN,
+                "WebIdentityToken",
+                randomValueOtherThan(webIdentityToken, () -> randomUnicodeOfLength(10))
+            )
+        );
+        assertEquals(RestStatus.UNAUTHORIZED, response.status());
+    }
+
+    public void testInvalidARN() {
+        final var webIdentityToken = randomUnicodeOfLength(10);
+        final var handler = new AwsStsHttpHandler((key, token) -> fail(), webIdentityToken);
+        final var response = handleRequest(
+            handler,
+            Map.of(
+                "Action",
+                "AssumeRoleWithWebIdentity",
+                "RoleSessionName",
+                AwsStsHttpHandler.ROLE_NAME,
+                "RoleArn",
+                randomValueOtherThan(AwsStsHttpHandler.ROLE_ARN, ESTestCase::randomIdentifier),
+                "WebIdentityToken",
+                webIdentityToken
+            )
+        );
+        assertEquals(RestStatus.UNAUTHORIZED, response.status());
     }
 
     private record TestHttpResponse(RestStatus status, BytesReference body) {}
 
-    private static TestHttpResponse handleRequest(Ec2ImdsHttpHandler handler, String method, String uri) {
-        final var httpExchange = new TestHttpExchange(method, uri, BytesArray.EMPTY, TestHttpExchange.EMPTY_HEADERS);
+    private static TestHttpResponse handleRequest(AwsStsHttpHandler handler, Map<String, String> body) {
+        final var httpExchange = new TestHttpExchange(
+            "POST",
+            "/assume-role-with-web-identity/",
+            new BytesArray(
+                body.entrySet()
+                    .stream()
+                    .map(e -> e.getKey() + "=" + URLEncoder.encode(e.getValue(), StandardCharsets.UTF_8))
+                    .collect(Collectors.joining("&"))
+            ),
+            TestHttpExchange.EMPTY_HEADERS
+        );
         try {
             handler.handle(httpExchange);
         } catch (IOException e) {

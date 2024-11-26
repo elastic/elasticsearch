1bfb2bf16b24abab0798a0ca0578c6a3bf3d4667
Brian Seeders <brian.seeders@elastic.co>
2024-11-26 17:08:15 -0500

    Cleanup for PR

--- .buildkite/scripts/get-latest-test-mutes.sh
+++ .buildkite/scripts/get-latest-test-mutes.sh
@@ -9,16 +9,12 @@ testMuteFile="$(mktemp)"
 
 # If this PR contains changes to muted-tests.yml, we disable this functionality
 # Otherwise, we wouldn't be able to test unmutes
-# if [[ ! $(gh pr diff "$BUILDKITE_PULL_REQUEST" --name-only | grep 'muted-tests.yml') ]]; then # TODO add back before PR review/merge
-  echo "Getting latest muted tests..."
+if [[ ! $(gh pr diff "$BUILDKITE_PULL_REQUEST" --name-only | grep 'muted-tests.yml') ]]; then
   gh api -H 'Accept: application/vnd.github.v3.raw' "repos/elastic/elasticsearch/contents/muted-tests.yml?ref=$testMuteBranch" > "$testMuteFile"
-  echo "Saved to file: $testMuteFile"
-  cat "$testMuteFile"
 
   if [[ -s "$testMuteFile" ]]; then
     mkdir -p ~/.gradle
     # This is using gradle.properties instead of an env var so that it's easily compatible with the Windows pre-command hook
     echo "org.gradle.project.org.elasticsearch.additional.muted.tests=$testMuteFile" >> ~/.gradle/gradle.properties
-    cat ~/.gradle/gradle.properties
   fi
-# fi
+fi

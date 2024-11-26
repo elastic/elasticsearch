3177e9313d20e90194a65e4c3b49a841b14948c2
Brian Seeders <brian.seeders@elastic.co>
2024-11-26 15:43:42 -0500

    Temporary test

--- .buildkite/scripts/get-latest-test-mutes.sh
+++ .buildkite/scripts/get-latest-test-mutes.sh
@@ -5,10 +5,10 @@ testMuteFile="$(mktemp)"
 
 # If this PR contains changes to muted-tests.yml, we disable this functionality
 # Otherwise, we wouldn't be able to test unmutes
-if [[ ! $(gh pr diff "$BUILDKITE_PULL_REQUEST" --name-only | grep 'muted-tests.yml') ]]; then
+# if [[ ! $(gh pr diff "$BUILDKITE_PULL_REQUEST" --name-only | grep 'muted-tests.yml') ]]; then # TODO add back before PR review/merge
   gh api -H 'Accept: application/vnd.github.v3.raw' "repos/elastic/elasticsearch/contents/muted-tests.yml?ref=$testMuteBranch" > "$testMuteFile"
 
   if [[ -s "$testMuteFile" ]]; then
     export GRADLE_OPTS="${GRADLE_OPTS:-} -Dorg.gradle.project.org.elasticsearch.additional.muted.tests=$testMuteFile"
   fi
-fi
+# fi

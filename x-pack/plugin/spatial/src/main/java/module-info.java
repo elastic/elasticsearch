bdcdb306c27d3e9b043ff7fbe0571e3d2ab197e2
Lorenzo Dematte <lorenzo.dematte@elastic.co>
2024-11-27 09:35:29 +0100

    Modular spatial plugin

---
+++ x-pack/plugin/spatial/src/main/java/module-info.java
@@ -0,0 +1,34 @@
+module org.elasticsearch.spatial {
+    requires org.apache.lucene.spatial3d;
+    requires org.elasticsearch.h3;
+    requires org.elasticsearch.legacy.geo;
+    requires org.elasticsearch.painless.spi;
+    requires org.elasticsearch.xcore;
+    requires org.elasticsearch.server;
+    requires org.elasticsearch.xcontent;
+    requires org.elasticsearch.base;
+    requires org.elasticsearch.geo;
+    requires org.apache.lucene.core;
+
+    exports org.elasticsearch.xpack.spatial.action to org.elasticsearch.server;
+    exports org.elasticsearch.xpack.spatial.common; // to vector-tile
+    exports org.elasticsearch.xpack.spatial; // to vector-tile
+    exports org.elasticsearch.xpack.spatial.search.aggregations.bucket.geogrid; // to vector-tile
+
+    exports org.elasticsearch.xpack.spatial.index.query;
+    exports org.elasticsearch.xpack.spatial.index.fielddata;
+    exports org.elasticsearch.xpack.spatial.index.fielddata.plain;
+    exports org.elasticsearch.xpack.spatial.index.mapper;
+
+    exports org.elasticsearch.xpack.spatial.ingest;
+    exports org.elasticsearch.xpack.spatial.script.field;
+
+    exports org.elasticsearch.xpack.spatial.search.aggregations;
+    exports org.elasticsearch.xpack.spatial.search.aggregations.metrics;
+    exports org.elasticsearch.xpack.spatial.search.aggregations.support;
+    exports org.elasticsearch.xpack.spatial.search.runtime;
+
+    opens org.elasticsearch.xpack.spatial to org.elasticsearch.painless.spi; // whitelist resource access
+
+    provides org.elasticsearch.painless.spi.PainlessExtension with org.elasticsearch.xpack.spatial.SpatialPainlessExtension;
+}

e056a3fe2eb01845e22cf531b34191ce87816859
Yang Wang <yang.wang@elastic.co>
2024-11-27 16:04:35 +1100

    Remove put-project from constants of operator action check (#1820)

--- x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
+++ x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
@@ -635,7 +635,6 @@ public class Constants {
         "internal:admin/indices/prevalidate_shard_path",
         "internal:index/metadata/migration_version/update",
         "internal:admin/repository/verify",
-        "internal:admin/repository/verify/coordinate",
-        "cluster:admin/projects/put"
+        "internal:admin/repository/verify/coordinate"
     ).filter(Objects::nonNull).collect(Collectors.toUnmodifiableSet());
 }

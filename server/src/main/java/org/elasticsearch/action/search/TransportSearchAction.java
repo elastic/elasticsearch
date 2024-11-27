5dbc019a4fc7222dafdcd3851938986cf7327e6d
Pawan Kartik <pawankartik.chitrapu@elastic.co>
2024-11-27 19:40:42 +0000

    Add removed code comment

--- server/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
+++ server/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
@@ -413,6 +413,7 @@ public class TransportSearchAction extends HandledTransportAction<SearchRequest,
                     );
                     task.getProgressListener().notifySearchStart(clusters);
                     if (resolvedIndices.getLocalIndices() == null) {
+                        // Notify the progress listener that a CCS with minimize_roundtrips is happening remote-only (no local shards)
                         task.getProgressListener()
                             .notifyListShards(Collections.emptyList(), Collections.emptyList(), clusters, false, timeProvider);
                     }

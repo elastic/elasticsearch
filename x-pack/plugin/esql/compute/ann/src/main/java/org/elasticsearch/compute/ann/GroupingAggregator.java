8a2930be3efc7bffff627ed6a150a6067fe78202
Gal Lalouche <gal.lalouche@elastic.co>
2024-11-28 16:25:43 +0200

    Undo a bunch of temporary stuff

--- x-pack/plugin/esql/compute/ann/src/main/java/org/elasticsearch/compute/ann/GroupingAggregator.java
+++ x-pack/plugin/esql/compute/ann/src/main/java/org/elasticsearch/compute/ann/GroupingAggregator.java
@@ -8,7 +8,6 @@
 package org.elasticsearch.compute.ann;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;

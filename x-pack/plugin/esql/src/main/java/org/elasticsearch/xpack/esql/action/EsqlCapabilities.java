455bc845cd44e4848fe97ddf5838b7f5ba8408b7
Alexander Spies <alexander.spies@elastic.co>
2024-11-29 17:34:05 +0100

    Compute output of LookupJoinExec dynamically (#117763)
    LookupJoinExec should not assume its output but instead compute it from
    - Its input fields from the left - The fields added from the lookup
    index
    
    Currently, LookupJoinExec's output is determined when the logical plan
    is mapped to a physical one, and thereafter the output cannot be changed
    anymore. This makes it impossible to have late materialization of fields
    from the left hand side via field extractions, because we are forced to
    extract *all* fields before the LookupJoinExec, otherwise we do not
    achieve the prescribed output.
    
    Avoid that by tracking only which fields the LookupJoinExec will add
    from the lookup index instead of tracking the whole output (that was
    only correct for the logical plan).
    
    **Note:** While this PR is a refactoring for the current functionality,
    it should unblock @craigtaverner 's ongoing work related to field
    extractions and getting multiple LOOKUP JOIN queries to work correctly
    without adding hacks.
    
    (cherry picked from commit 64107e0a0b032c0ee1ed319f0d6bfefce23def9a)
    
    # Conflicts:
    #	x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/planner/LocalExecutionPlanner.java

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -514,7 +514,7 @@ public class EsqlCapabilities {
         /**
          * LOOKUP JOIN
          */
-        JOIN_LOOKUP_V2(Build.current().isSnapshot()),
+        JOIN_LOOKUP_V3(Build.current().isSnapshot()),
 
         /**
          * Fix for https://github.com/elastic/elasticsearch/issues/117054

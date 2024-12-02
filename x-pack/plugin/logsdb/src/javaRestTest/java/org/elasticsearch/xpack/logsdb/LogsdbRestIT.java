050d4b23f29e13d6da94e4dc3911aa2c1ff4714a
Martijn van Groningen <martijn.v.groningen@gmail.com>
2024-12-02 16:07:39 +0100

    spotless

--- x-pack/plugin/logsdb/src/javaRestTest/java/org/elasticsearch/xpack/logsdb/LogsdbRestIT.java
+++ x-pack/plugin/logsdb/src/javaRestTest/java/org/elasticsearch/xpack/logsdb/LogsdbRestIT.java
@@ -190,8 +190,8 @@ public class LogsdbRestIT extends ESRestTestCase {
         var forceMergeResponse = client().performRequest(forceMergeRequest);
         assertOK(forceMergeResponse);
 
-        String query = "FROM test-foo | STATS count(*), min(@timestamp), max(@timestamp), min(message_length), max(message_length)" +
-            " ,sum(message_length), avg(message_length), min(log.offset), max(log.offset) | LIMIT 1";
+        String query = "FROM test-foo | STATS count(*), min(@timestamp), max(@timestamp), min(message_length), max(message_length)"
+            + " ,sum(message_length), avg(message_length), min(log.offset), max(log.offset) | LIMIT 1";
         final Request esqlRequest = new Request("POST", "/_query");
         esqlRequest.setJsonEntity("""
             {

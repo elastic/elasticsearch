429d0f6c9f2b13d4fc55abd45b8466cb467c40f2
Fang Xing <fang.xing@elastic.co>
2024-12-03 15:07:34 -0500

    fix serialization test failure

--- x-pack/plugin/esql-core/src/main/java/org/elasticsearch/xpack/esql/core/expression/ExpressionCoreWritables.java
+++ x-pack/plugin/esql-core/src/main/java/org/elasticsearch/xpack/esql/core/expression/ExpressionCoreWritables.java
@@ -29,6 +29,7 @@ public class ExpressionCoreWritables {
             entries.add(new NamedWriteableRegistry.Entry(Expression.class, e.name, in -> (Expression) e.reader.read(in)));
         }
         entries.add(Literal.ENTRY);
+        entries.add(NamedLiterals.ENTRY);
         return entries;
     }
 

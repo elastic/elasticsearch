7c09832d98ed6b59f4aeb51821bf00235f454242
Ignacio Vera <ignacio.vera@elastic.co>
2024-12-03 13:18:36 +0100

    iter

--- server/src/main/java/org/elasticsearch/common/breaker/CircuitBreaker.java
+++ server/src/main/java/org/elasticsearch/common/breaker/CircuitBreaker.java
@@ -87,7 +87,8 @@ public interface CircuitBreaker {
     /**
      * Check the real memory circuit breaker. It is a NOOP if
      * {@link org.elasticsearch.indices.breaker.HierarchyCircuitBreakerService#USE_REAL_MEMORY_USAGE_SETTING} is set to false,
-     * otherwise it trips if the current memory usage cannot be lowered below the limit.
+     * otherwise it trips if the current memory usage cannot be lowered below the limit. This method is a temporary solution
+     * and should not be used, use a counter based breaker instead.
      *
      * @param label thing requesting to check the rela memory circuit breaker that is included in
      *              the exception if the breaker is tripped

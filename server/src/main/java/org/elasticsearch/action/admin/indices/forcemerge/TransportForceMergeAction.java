958c0b1c25168cacf8c2a649068f3e078a09237f
Niels Bauman <33722607+nielsbauman@users.noreply.github.com>
2024-12-04 08:47:11 +0100

    Make force merge project aware (#1839)
    Updates the force merge transport action to support multiple projects.

--- server/src/main/java/org/elasticsearch/action/admin/indices/forcemerge/TransportForceMergeAction.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/forcemerge/TransportForceMergeAction.java
@@ -18,6 +18,7 @@ import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.block.ClusterBlockException;
 import org.elasticsearch.cluster.block.ClusterBlockLevel;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.project.ProjectResolver;
 import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.cluster.routing.ShardsIterator;
 import org.elasticsearch.cluster.service.ClusterService;
@@ -42,6 +43,7 @@ public class TransportForceMergeAction extends TransportBroadcastByNodeAction<
 
     private final IndicesService indicesService;
     private final ThreadPool threadPool;
+    private final ProjectResolver projectResolver;
 
     @Inject
     public TransportForceMergeAction(
@@ -49,6 +51,7 @@ public class TransportForceMergeAction extends TransportBroadcastByNodeAction<
         TransportService transportService,
         IndicesService indicesService,
         ActionFilters actionFilters,
+        ProjectResolver projectResolver,
         IndexNameExpressionResolver indexNameExpressionResolver
     ) {
         super(
@@ -62,6 +65,7 @@ public class TransportForceMergeAction extends TransportBroadcastByNodeAction<
         );
         this.indicesService = indicesService;
         this.threadPool = transportService.getThreadPool();
+        this.projectResolver = projectResolver;
     }
 
     @Override
@@ -105,7 +109,7 @@ public class TransportForceMergeAction extends TransportBroadcastByNodeAction<
      */
     @Override
     protected ShardsIterator shards(ClusterState clusterState, ForceMergeRequest request, String[] concreteIndices) {
-        return clusterState.routingTable().allShards(concreteIndices);
+        return clusterState.routingTable(projectResolver.getProjectId()).allShards(concreteIndices);
     }
 
     @Override

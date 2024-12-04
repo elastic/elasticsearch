3a423ecdb2ac4793d972820fa31cac897b5e9345
Dmitrii Burlutskii <dmitrii.burlutckii@elastic.co>
2024-12-04 18:20:52 +0100

    Refactor feature flag

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/InferencePlugin.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/InferencePlugin.java
@@ -232,11 +232,7 @@ public class InferencePlugin extends Plugin implements ActionPlugin, ExtensibleP
         var inferenceServices = new ArrayList<>(inferenceServiceExtensions);
         inferenceServices.add(this::getInferenceServiceFactories);
 
-        ElasticInferenceServiceSettings inferenceServiceSettings = new ElasticInferenceServiceSettings(settings);
-
-        String elasticInferenceUrl = this.getElasticInferenceServiceUrl(inferenceServiceSettings);
-
-        if (elasticInferenceUrl != null) {
+        if (isElasticInferenceServiceEnabled()) {
             // Create a new HTTPClientManager with its own configuration, including the connection pool.
             // Set the sslStrategy to ensure an encrypted connection, as Elastic Inference Service requires it.
             var sslStrategy = HttpClientManager.getSSLStrategy(ELASTIC_INFERENCE_SERVICE_SSL_CONFIGURATION_PREFIX);
@@ -255,6 +251,8 @@ public class InferencePlugin extends Plugin implements ActionPlugin, ExtensibleP
             );
             elasicInferenceServiceFactory.set(elasticInferenceServiceRequestSenderFactory);
 
+            ElasticInferenceServiceSettings inferenceServiceSettings = new ElasticInferenceServiceSettings(settings);
+            String elasticInferenceUrl = this.getElasticInferenceServiceUrl(inferenceServiceSettings);
             elasticInferenceServiceComponents.set(new ElasticInferenceServiceComponents(elasticInferenceUrl));
 
             inferenceServices.add(
@@ -393,8 +391,7 @@ public class InferencePlugin extends Plugin implements ActionPlugin, ExtensibleP
         settings.add(SKIP_VALIDATE_AND_START);
 
         // Do not register Elastic Inference Service settings if the feature is disabled.
-        ElasticInferenceServiceSettings inferenceServiceSettings = new ElasticInferenceServiceSettings(this.settings);
-        if (getElasticInferenceServiceUrl(inferenceServiceSettings) != null ) {
+        if (isElasticInferenceServiceEnabled()) {
             settings.addAll(ElasticInferenceServiceSettings.getSettingsDefinitions());
         }
 
@@ -469,5 +466,9 @@ public class InferencePlugin extends Plugin implements ActionPlugin, ExtensibleP
         }
 
         return elasticInferenceUrl;
+    }
+
+    protected Boolean isElasticInferenceServiceEnabled() {
+        return (ELASTIC_INFERENCE_SERVICE_FEATURE_FLAG.isEnabled() || DEPRECATED_ELASTIC_INFERENCE_SERVICE_FEATURE_FLAG.isEnabled());
     }
 }

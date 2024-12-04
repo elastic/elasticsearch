644874f41e466eacd208e878c4c21b94db986798
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2024-12-04 14:34:08 +0100

    introduce capability

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -425,6 +425,11 @@ public class EsqlCapabilities {
         KQL_FUNCTION(Build.current().isSnapshot()),
 
         /**
+         * Hash function
+         */
+        HASH_FUNCTION(Build.current().isSnapshot()),
+
+        /**
          * Don't optimize CASE IS NOT NULL function by not requiring the fields to be not null as well.
          * https://github.com/elastic/elasticsearch/issues/112704
          */

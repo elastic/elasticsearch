694e11ccba903c8883b9891b323cfd827f3f8dd7
Slobodan Adamovic <slobodan.adamovic@elastic.co>
2024-12-06 07:56:39 +0100

    include _primary_term

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/permission/BuiltInMetadataFieldsAutomatonProvider.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/permission/BuiltInMetadataFieldsAutomatonProvider.java
@@ -10,16 +10,23 @@ package org.elasticsearch.xpack.core.security.authz.permission;
 import org.apache.lucene.util.automaton.Automaton;
 import org.elasticsearch.xpack.core.security.support.Automatons;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 public class BuiltInMetadataFieldsAutomatonProvider {
+
+    private static final Set<String> ADDITIONAL_METADATA_FIELDS = Set.of("_primary_term");
 
     private final Automaton allMetadataFieldsAutomaton;
     private final Set<String> allMetadataFields;
 
     public BuiltInMetadataFieldsAutomatonProvider(Set<String> allMetadataFields) {
-        this.allMetadataFieldsAutomaton = Automatons.patterns(allMetadataFields);
-        this.allMetadataFields = allMetadataFields;
+        Set<String> all = new HashSet<>();
+        all.addAll(allMetadataFields);
+        all.addAll(ADDITIONAL_METADATA_FIELDS);
+        this.allMetadataFieldsAutomaton = Automatons.patterns(all);
+        this.allMetadataFields = Collections.unmodifiableSet(all);
     }
 
     public boolean isMetadataField(String field) {

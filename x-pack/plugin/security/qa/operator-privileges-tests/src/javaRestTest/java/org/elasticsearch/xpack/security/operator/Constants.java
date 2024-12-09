65b5e3f7fdb1e1c93851758ad632030c3e1f2ce2
Keith Massey <keith.massey@elastic.co>
2024-12-09 07:50:04 -0600

    Adding get migration reindex status

--- x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
+++ x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
@@ -495,6 +495,7 @@ public class Constants {
         "indices:admin/cache/clear",
         "indices:admin/data_stream/lazy_rollover",
         "indices:admin/data_stream/reindex",
+        "indices:admin/data_stream/reindex_status",
         "indices:internal/admin/ccr/restore/file_chunk/get",
         "indices:internal/admin/ccr/restore/session/clear",
         "indices:internal/admin/ccr/restore/session/put",

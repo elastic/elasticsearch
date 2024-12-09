99e8c6721f1bf9c659719b57d89ead1e08f2abd2
Bogdan Pintea <bogdan.pintea@elastic.co>
2024-12-09 20:35:49 +0100

    rewview comments

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/physical/AggregateExec.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/physical/AggregateExec.java
@@ -24,7 +24,7 @@ import org.elasticsearch.xpack.esql.plan.logical.Aggregate;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 
@@ -190,18 +190,17 @@ public class AggregateExec extends UnaryExec implements EstimatesRowSize {
     /** Returns the attributes that can be loaded from ordinals -- no explicit extraction is needed */
     public List<Attribute> ordinalAttributes() {
         List<Attribute> orginalAttributs = new ArrayList<>(groupings.size());
-        if (groupings().size() == 1) {
-            // CATEGORIZE requires the standard hash aggregator as well.
-            if (groupings.get(0).anyMatch(e -> e instanceof Categorize) == false) {
-                var leaves = new LinkedList<>();
-                // TODO: this seems out of place
-                aggregates.stream().filter(a -> groupings.contains(a) == false).forEach(a -> leaves.addAll(a.collectLeaves()));
-                groupings.forEach(g -> {
-                    if (leaves.contains(g) == false) {
-                        orginalAttributs.add((Attribute) g);
-                    }
-                });
-            }
+        // Ordinals can be leveraged just for a single grouping. If there are multiple groupings, fields need to be laoded for the
+        // hash aggregator.
+        // CATEGORIZE requires the standard hash aggregator as well.
+        if (groupings().size() == 1 && groupings.get(0).anyMatch(e -> e instanceof Categorize) == false) {
+            var leaves = new HashSet<>();
+            aggregates.stream().filter(a -> groupings.contains(a) == false).forEach(a -> leaves.addAll(a.collectLeaves()));
+            groupings.forEach(g -> {
+                if (leaves.contains(g) == false) {
+                    orginalAttributs.add((Attribute) g);
+                }
+            });
         }
         return orginalAttributs;
     }

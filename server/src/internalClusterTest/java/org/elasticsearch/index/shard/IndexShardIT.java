d213efd272f6ed5cece5d88fc09975f624303363
Jim Ferenczi <jim.ferenczi@elastic.co>
2024-12-10 23:05:27 +0000

    Add a new index setting to skip recovery source when synthetic source is enabled (#114618)
    This change adds a new undocumented index settings that allows to use synthetic source for recovery and CCR without storing a recovery source.

--- server/src/internalClusterTest/java/org/elasticsearch/index/shard/IndexShardIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/index/shard/IndexShardIT.java
@@ -715,7 +715,15 @@ public class IndexShardIT extends ESSingleNodeTestCase {
         }
         IndexShard shard = indexService.getShard(0);
         try (
-            Translog.Snapshot luceneSnapshot = shard.newChangesSnapshot("test", 0, numOps - 1, true, randomBoolean(), randomBoolean());
+            Translog.Snapshot luceneSnapshot = shard.newChangesSnapshot(
+                "test",
+                0,
+                numOps - 1,
+                true,
+                randomBoolean(),
+                randomBoolean(),
+                randomLongBetween(1, ByteSizeValue.ofMb(32).getBytes())
+            );
             Translog.Snapshot translogSnapshot = getTranslog(shard).newSnapshot()
         ) {
             List<Translog.Operation> opsFromLucene = TestTranslog.drainSnapshot(luceneSnapshot, true);

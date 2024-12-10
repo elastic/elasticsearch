a71c547ce5da9e992936d660d209c4def7b77152
Lorenzo Dematte <lorenzo.dematte@elastic.co>
2024-12-10 10:21:01 +0100

    Parametrized test

--- libs/entitlement/qa/src/javaRestTest/java/org/elasticsearch/test/entitlements/EntitlementsAllowedIT.java
+++ libs/entitlement/qa/src/javaRestTest/java/org/elasticsearch/test/entitlements/EntitlementsAllowedIT.java
@@ -9,13 +9,18 @@
 
 package org.elasticsearch.test.entitlements;
 
+import com.carrotsearch.randomizedtesting.annotations.Name;
+import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
+
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.test.cluster.ElasticsearchCluster;
 import org.elasticsearch.test.rest.ESRestTestCase;
+import org.elasticsearch.entitlement.qa.common.RestEntitlementsCheckAction;
 import org.junit.ClassRule;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.equalTo;
 
@@ -24,31 +29,36 @@ public class EntitlementsAllowedIT extends ESRestTestCase {
     @ClassRule
     public static ElasticsearchCluster cluster = ElasticsearchCluster.local()
         .plugin("entitlement-allowed")
+        .plugin("entitlement-allowed-nonmodular")
         .systemProperty("es.entitlements.enabled", "true")
         .setting("xpack.security.enabled", "false")
         .build();
 
-    // private final String actionName;
-    //
-    // public EntitlementsAllowedIT(@Name("actionName") String actionName) {
-    // this.actionName = actionName;
-    // }
-    //
-    // @ParametersFactory
-    // public static Iterable<Object[]> data() {
-    // return RestEntitlementsCheckAction.getServerAndPluginsCheckActions().stream()
-    // .map(s -> new Object[] { s })
-    // .toList();
-    // }
+    private final String pathPrefix;
+    private final String actionName;
+
+    public EntitlementsAllowedIT(@Name("pathPrefix") String pathPrefix, @Name("actionName") String actionName) {
+        this.pathPrefix = pathPrefix;
+        this.actionName = actionName;
+    }
+
+    @ParametersFactory
+    public static Iterable<Object[]> data() {
+     return Stream.of("allowed", "allowed_nonmodular")
+         .flatMap(path -> RestEntitlementsCheckAction.getServerAndPluginsCheckActions().stream()
+             .map(action -> new Object[] { path, action }))
+         .toList();
+    }
 
     @Override
     protected String getTestRestCluster() {
         return cluster.getHttpAddresses();
     }
 
-    public void testCheckCreateURLClassLoaderWithPolicyPass() throws IOException {
-        var request = new Request("GET", "/_entitlement/allowed/_check");
-        request.addParameter("action", "create_classloader");
+    public void testCheckActionWithPolicyPass() throws IOException {
+        logger.info("Executing Entitlement test [{}] for [{}]", pathPrefix, actionName);
+        var request = new Request("GET", "/_entitlement/" + pathPrefix + "/_check");
+        request.addParameter("action", actionName);
         Response result = client().performRequest(request);
         assertThat(result.getStatusLine().getStatusCode(), equalTo(200));
     }

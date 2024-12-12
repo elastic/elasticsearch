25ffd387553223b81d8ce17e78b0dd026f695836
gmarouli <mary.gouseti@elastic.co>
2024-12-12 13:47:30 +0200

    Reject value 'index-setting' for 'wait_for_active_shards' in 9.0

--- server/src/main/java/org/elasticsearch/rest/action/admin/indices/RestCloseIndexAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/admin/indices/RestCloseIndexAction.java
@@ -17,7 +17,8 @@ import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.logging.DeprecationCategory;
 import org.elasticsearch.common.logging.DeprecationLogger;
-import org.elasticsearch.core.UpdateForV9;
+import org.elasticsearch.core.RestApiVersion;
+import org.elasticsearch.core.UpdateForV10;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.Scope;
@@ -47,30 +48,43 @@ public class RestCloseIndexAction extends BaseRestHandler {
     }
 
     @Override
-    @UpdateForV9(owner = UpdateForV9.Owner.DATA_MANAGEMENT)
     public RestChannelConsumer prepareRequest(final RestRequest request, final NodeClient client) throws IOException {
         CloseIndexRequest closeIndexRequest = new CloseIndexRequest(Strings.splitStringByCommaToArray(request.param("index")));
         closeIndexRequest.masterNodeTimeout(getMasterNodeTimeout(request));
         closeIndexRequest.ackTimeout(getAckTimeout(request));
         closeIndexRequest.indicesOptions(IndicesOptions.fromRequest(request, closeIndexRequest.indicesOptions()));
-        String waitForActiveShards = request.param("wait_for_active_shards");
-        if ("index-setting".equalsIgnoreCase(waitForActiveShards)) {
-            deprecationLogger.warn(
-                DeprecationCategory.SETTINGS,
-                "close-index-wait_for_active_shards-index-setting",
-                "?wait_for_active_shards=index-setting is now the default behaviour; the 'index-setting' value for this parameter "
-                    + "should no longer be used since it will become unsupported in version "
-                    + (Version.V_8_0_0.major + 1)
-            );
-            // TODO in v9:
-            // - throw an IllegalArgumentException here
-            // - record the removal of support for this value as a breaking change
-            // TODO in v10:
-            // - remove the IllegalArgumentException here
-        } else if (waitForActiveShards != null) {
-            closeIndexRequest.waitForActiveShards(ActiveShardCount.parseString(waitForActiveShards));
-        }
+        handleIndexSettingWaitForActiveShards(request, closeIndexRequest);
         return channel -> client.admin().indices().close(closeIndexRequest, new RestToXContentListener<>(channel));
     }
 
+    /**
+     * Special handling of the query param 'wait_for_active_shards' because of the deprecated param value 'index-setting'.
+     * - In 10.x this code should be simplified to simply parsing the active shard count with {@link ActiveShardCount#parseString(String)}
+     * - In 9.x we throw an error that informs the user about the parameter value removal; unless we are running in rest-compatibility mode
+     * with 8.x when we log a deprecation warning and handle the value as a noop.
+     */
+    @UpdateForV10(owner = UpdateForV10.Owner.DATA_MANAGEMENT)
+    private void handleIndexSettingWaitForActiveShards(final RestRequest request, final CloseIndexRequest closeIndexRequest) {
+        String waitForActiveShards = request.param("wait_for_active_shards");
+        if (waitForActiveShards == null) {
+            return;
+        }
+        if ("index-setting".equalsIgnoreCase(waitForActiveShards)) {
+            String deprecationMessage =
+                "?wait_for_active_shards=index-setting is now the default behaviour; the 'index-setting' value for this parameter "
+                    + "should no longer be used since it is not supported in version "
+                    + Version.V_9_0_0.major;
+            if (request.getRestApiVersion() == RestApiVersion.V_8) {
+                deprecationLogger.warn(
+                    DeprecationCategory.SETTINGS,
+                    "close-index-wait_for_active_shards-index-setting",
+                    deprecationMessage
+                );
+            } else {
+                throw new IllegalArgumentException(deprecationMessage);
+            }
+        } else {
+            closeIndexRequest.waitForActiveShards(ActiveShardCount.parseString(waitForActiveShards));
+        }
+    }
 }

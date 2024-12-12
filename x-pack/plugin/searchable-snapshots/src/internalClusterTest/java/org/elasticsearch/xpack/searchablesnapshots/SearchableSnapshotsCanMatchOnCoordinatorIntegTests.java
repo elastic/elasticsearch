fd937ebf77c209bf32d9f17b5772bc2ac163994e
Luca Cavanna <javanna@apache.org>
2024-12-12 16:18:41 +0100

    [8.x] Handle all exceptions in data nodes can match (#117469) (#118533) (#118570)
    * Handle all exceptions in data nodes can match (#117469)
    
    During the can match phase, prior to the query phase, we may have exceptions
    that are returned back to the coordinating node, handled gracefully as if the
    shard returned canMatch=true.
    
    During the query phase, we perform an additional rewrite and can match phase
    to eventually shortcut the query phase for the shard. That needs to handle
    exceptions as well. Currently, an exception there causes shard failures, while
    we should rather go ahead and execute the query on the shard.
    
    Instead of adding another try catch on consumers code, this commit adds exception handling to the method itself so that it can no longer throw exceptions and similar mistakes can no longer be made in the future.
    
    At the same time, this commit makes the can match method more easily testable without requiring a full-blown SearchService instance.
    
    Closes #104994
    
    * fix compile

--- x-pack/plugin/searchable-snapshots/src/internalClusterTest/java/org/elasticsearch/xpack/searchablesnapshots/SearchableSnapshotsCanMatchOnCoordinatorIntegTests.java
+++ x-pack/plugin/searchable-snapshots/src/internalClusterTest/java/org/elasticsearch/xpack/searchablesnapshots/SearchableSnapshotsCanMatchOnCoordinatorIntegTests.java
@@ -7,7 +7,6 @@
 
 package org.elasticsearch.xpack.searchablesnapshots;
 
-import org.elasticsearch.action.admin.indices.recovery.RecoveryResponse;
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.search.SearchPhaseExecutionException;
 import org.elasticsearch.action.search.SearchRequest;
@@ -36,7 +35,6 @@ import org.elasticsearch.index.query.TermsQueryBuilder;
 import org.elasticsearch.index.shard.IndexLongFieldRange;
 import org.elasticsearch.indices.DateFieldRangeInfo;
 import org.elasticsearch.indices.IndicesService;
-import org.elasticsearch.indices.recovery.RecoveryState;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.snapshots.SnapshotId;
@@ -1332,18 +1330,6 @@ public class SearchableSnapshotsCanMatchOnCoordinatorIntegTests extends BaseFroz
             .getState()
             .metadata()
             .index(indexName);
-    }
-
-    private static void waitUntilRecoveryIsDone(String index) throws Exception {
-        assertBusy(() -> {
-            RecoveryResponse recoveryResponse = indicesAdmin().prepareRecoveries(index).get();
-            assertThat(recoveryResponse.hasRecoveries(), equalTo(true));
-            for (List<RecoveryState> value : recoveryResponse.shardRecoveryStates().values()) {
-                for (RecoveryState recoveryState : value) {
-                    assertThat(recoveryState.getStage(), equalTo(RecoveryState.Stage.DONE));
-                }
-            }
-        });
     }
 
     private void waitUntilAllShardsAreUnassigned(Index index) throws Exception {

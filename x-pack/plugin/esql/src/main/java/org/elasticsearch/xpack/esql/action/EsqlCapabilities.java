04c89aeba0214a58a3a15687541452d557c2284e
Craig Taverner <craig@amanzi.com>
2024-12-16 21:26:45 +0100

    Support multi-index LOOKUP JOIN and various bug fixes (#118429)
    While working on supporting multi-index LOOKUP JOIN; various bugs were
    fixed:
    
    * Previously we just used '*' for lookup-join indices, because the fieldnames were sometimes not being correctly determined. The problem was with KEEP referencing fields from the right that had previously been defined on the left as aliases, including using the ROW command. We normally don't want to ask for aliases, but if they could be shadowed by a lookup join, we need to keep them.
    * With both single and multi-index LOOKUP JOIN we need to mark each index as potentially wildcard fields, if the KEEP commands occur before the LOOKUP JOIN.

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -537,7 +537,7 @@ public class EsqlCapabilities {
         /**
          * LOOKUP JOIN
          */
-        JOIN_LOOKUP_V5(Build.current().isSnapshot()),
+        JOIN_LOOKUP_V6(Build.current().isSnapshot()),
 
         /**
          * Fix for https://github.com/elastic/elasticsearch/issues/117054

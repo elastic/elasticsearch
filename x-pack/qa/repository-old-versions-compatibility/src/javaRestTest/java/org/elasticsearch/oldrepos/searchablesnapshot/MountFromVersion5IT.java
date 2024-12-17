a2fad250e2c1b73425978e16a34735980c92aa96
Dimitris Rempapis <dimitris.rempapis@elastic.co>
2024-12-17 11:50:05 +0200

    update after review

---
+++ x-pack/qa/repository-old-versions-compatibility/src/javaRestTest/java/org/elasticsearch/oldrepos/searchablesnapshot/MountFromVersion5IT.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.oldrepos.searchablesnapshot;
+
+import org.elasticsearch.oldrepos.archiveindex.ArchiveIndexTestCase;
+import org.elasticsearch.test.cluster.util.Version;
+
+public class MountFromVersion5IT extends ArchiveIndexTestCase {
+
+    public MountFromVersion5IT(Version version) {
+        super(version);
+    }
+
+    public void testSearchableSnapshot() throws Exception {
+        verifyCompatibility("5");
+    }
+}

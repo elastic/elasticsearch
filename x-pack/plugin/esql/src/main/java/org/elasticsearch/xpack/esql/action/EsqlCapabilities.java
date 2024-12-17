d330a3f573636b8669c4961ebf694446cb869da7
Iván Cea Fontenla <ivan.cea@elastic.co>
2024-12-17 16:17:35 +0100

    Post merge capability fix

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -552,7 +552,7 @@ public class EsqlCapabilities {
         /**
          * LOOKUP JOIN with the same index as the FROM
          */
-        JOIN_LOOKUP_REPEATED_INDEX_FROM(JOIN_LOOKUP_V6.isEnabled()),
+        JOIN_LOOKUP_REPEATED_INDEX_FROM(JOIN_LOOKUP_V7.isEnabled()),
 
         /**
          * Fix for https://github.com/elastic/elasticsearch/issues/117054

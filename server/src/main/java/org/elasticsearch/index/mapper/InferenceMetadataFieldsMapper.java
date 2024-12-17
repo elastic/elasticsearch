a718a7ee1db7d68530fdab34deb42fc04dbedd42
Mike Pellegrini <mike.pellegrini@elastic.co>
2024-12-17 12:45:55 -0500

    Use a system property to enable inference metadata fields

--- server/src/main/java/org/elasticsearch/index/mapper/InferenceMetadataFieldsMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/InferenceMetadataFieldsMapper.java
@@ -9,10 +9,12 @@
 
 package org.elasticsearch.index.mapper;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.join.BitSetProducer;
-import org.elasticsearch.common.util.FeatureFlag;
+import org.elasticsearch.core.Booleans;
 import org.elasticsearch.index.IndexVersion;
 import org.elasticsearch.index.IndexVersions;
 import org.elasticsearch.index.query.SearchExecutionContext;
@@ -26,7 +28,37 @@ import java.util.function.Function;
  * the field name for removal from _source.
  */
 public abstract class InferenceMetadataFieldsMapper extends MetadataFieldMapper {
-    public static final FeatureFlag INFERENCE_METADATA_FIELDS_FEATURE_FLAG = new FeatureFlag("inference_metadata_fields");
+    public static class SystemProperty {
+        private static final Logger logger = LogManager.getLogger(SystemProperty.class);
+
+        private final boolean enabled;
+
+        private SystemProperty(String name) {
+            String propertyName = "es." + name;
+            this.enabled = parseSystemProperty(propertyName, false);
+            if (this.enabled) {
+                logger.info("Feature " + name + " (via system property '" + propertyName + "') is enabled");
+            } else {
+                logger.debug("Feature " + name + " (via system property '" + propertyName + "') is disabled");
+            }
+        }
+
+        private boolean parseSystemProperty(String propertyName, boolean defaultValue) {
+            final String propertyValue = System.getProperty(propertyName);
+            logger.trace("System property [{}] is set to [{}]", propertyName, propertyValue);
+            try {
+                return Booleans.parseBoolean(propertyValue, defaultValue);
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("Invalid value [" + propertyValue + "] for system property [" + propertyName + "]", e);
+            }
+        }
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+    }
+
+    public static final SystemProperty INFERENCE_METADATA_FIELDS_SYSTEM_PROPERTY = new SystemProperty("inference_metadata_fields");
 
     public static final String NAME = "_inference_fields";
     public static final String CONTENT_TYPE = "_inference_fields";
@@ -61,6 +93,6 @@ public abstract class InferenceMetadataFieldsMapper extends MetadataFieldMapper 
     }
 
     public static boolean isEnabled(IndexVersion indexVersion) {
-        return indexVersion.onOrAfter(IndexVersions.INFERENCE_METADATA_FIELDS) && INFERENCE_METADATA_FIELDS_FEATURE_FLAG.isEnabled();
+        return indexVersion.onOrAfter(IndexVersions.INFERENCE_METADATA_FIELDS) && INFERENCE_METADATA_FIELDS_SYSTEM_PROPERTY.isEnabled();
     }
 }

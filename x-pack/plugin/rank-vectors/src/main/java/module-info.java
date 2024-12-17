023f11ff4d4040288536df939f260d2e024fb178
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2024-12-17 11:28:30 -0500

    giving whitelist resource access to painless spi

--- x-pack/plugin/rank-vectors/src/main/java/module-info.java
+++ x-pack/plugin/rank-vectors/src/main/java/module-info.java
@@ -16,6 +16,9 @@ module org.elasticsearch.rank.vectors {
     exports org.elasticsearch.xpack.rank.vectors.mapper;
     exports org.elasticsearch.xpack.rank.vectors.script;
 
+    // whitelist resource access
+    opens  org.elasticsearch.xpack.rank.vectors.script to org.elasticsearch.painless.spi;
+
     provides org.elasticsearch.painless.spi.PainlessExtension with org.elasticsearch.xpack.rank.vectors.script.RankVectorsPainlessExtension;
     provides org.elasticsearch.features.FeatureSpecification with org.elasticsearch.xpack.rank.vectors.RankVectorsFeatures;
 

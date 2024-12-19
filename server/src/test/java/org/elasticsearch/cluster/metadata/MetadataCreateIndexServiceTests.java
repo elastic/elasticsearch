1af76851571eb9cc5e48325e2130c1e85825559e
Niels Bauman <nielsbauman@gmail.com>
2024-12-19 11:46:14 +0100

    Fix MetadataCreateIndexServiceTests tests

--- server/src/test/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexServiceTests.java
+++ server/src/test/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexServiceTests.java
@@ -1624,7 +1624,9 @@ public class MetadataCreateIndexServiceTests extends ESTestCase {
 
     public void testClusterStateCreateIndexWithClusterBlockTransformer() {
         {
-            var emptyClusterState = ClusterState.builder(ClusterState.EMPTY_STATE).build();
+            var emptyClusterState = ClusterState.builder(ClusterState.EMPTY_STATE)
+                .putProjectMetadata(ProjectMetadata.builder(projectId))
+                .build();
             var updatedClusterState = clusterStateCreateIndex(
                 emptyClusterState,
                 projectId,
@@ -1644,6 +1646,7 @@ public class MetadataCreateIndexServiceTests extends ESTestCase {
         {
             var minTransportVersion = TransportVersionUtils.randomCompatibleVersion(random());
             var emptyClusterState = ClusterState.builder(ClusterState.EMPTY_STATE)
+                .putProjectMetadata(ProjectMetadata.builder(projectId))
                 .nodes(DiscoveryNodes.builder().add(DiscoveryNodeUtils.create("_node_id")).build())
                 .putCompatibilityVersions("_node_id", new CompatibilityVersions(minTransportVersion, Map.of()))
                 .build();

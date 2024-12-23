3bd5aa2ffeade6e441924c463cfba332d6a6a656
JackPan <826647196@qq.com>
2024-12-23 10:24:21 +0800

    Support to random number for filed of another side

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/scalar/multivalue/MvAppendSerializationTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/scalar/multivalue/MvAppendSerializationTests.java
@@ -12,31 +12,41 @@ import org.elasticsearch.xpack.esql.core.tree.Source;
 import org.elasticsearch.xpack.esql.expression.AbstractExpressionSerializationTests;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MvAppendSerializationTests extends AbstractExpressionSerializationTests<MvAppend> {
+
+    private static final int randomFieldNumber = randomIntBetween(2, 10);
+
     @Override
     protected MvAppend createTestInstance() {
         Source source = randomSource();
         Expression field1 = randomChild();
-        Expression field2 = randomChild();
-        Expression field3 = randomChild();
-        return new MvAppend(source, field1, Arrays.asList(field2, field3));
+
+        List<Expression> filedList = new ArrayList<>();
+        for (int i = 0; i < randomFieldNumber; i++) {
+            filedList.add(randomChild());
+        }
+        return new MvAppend(source, field1, filedList);
     }
 
     @Override
     protected MvAppend mutateInstance(MvAppend instance) throws IOException {
         Source source = randomSource();
         Expression field1 = randomChild();
-        Expression field2 = randomChild();
-        Expression field3 = randomChild();
+        List<Expression> filedList = new ArrayList<>();
+        for (int i = 0; i < randomFieldNumber; i++) {
+            filedList.add(randomChild());
+        }
         if (randomBoolean()) {
             field1 = randomValueOtherThan(field1, AbstractExpressionSerializationTests::randomChild);
         } else {
-            field2 = randomValueOtherThan(field2, AbstractExpressionSerializationTests::randomChild);
-            field3 = randomValueOtherThan(field3, AbstractExpressionSerializationTests::randomChild);
+            for (int i = 0; i < randomFieldNumber; i++) {
+                filedList.set(i, randomValueOtherThan(filedList.get(i), AbstractExpressionSerializationTests::randomChild));
+            }
         }
-        return new MvAppend(source, field1, Arrays.asList(field2, field3));
+        return new MvAppend(source, field1, filedList);
     }
 
     @Override

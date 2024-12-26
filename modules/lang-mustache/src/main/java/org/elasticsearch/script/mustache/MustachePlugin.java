82bbe589c62a4f282de6a6d689a7841a629bcd27
Ryan Ernst <ryan@iernst.net>
2024-12-26 09:07:41 -0800

    Register mustache size limit setting
    In #114002 the maximum size of a mustache script output was made
    configurable. However, the setting was not registered. This commit
    registers the setting so that it can be set in elasticsearch.yml.

--- modules/lang-mustache/src/main/java/org/elasticsearch/script/mustache/MustachePlugin.java
+++ modules/lang-mustache/src/main/java/org/elasticsearch/script/mustache/MustachePlugin.java
@@ -17,6 +17,7 @@ import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.IndexScopedSettings;
+import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.settings.SettingsFilter;
 import org.elasticsearch.features.NodeFeature;
@@ -31,6 +32,7 @@ import org.elasticsearch.script.ScriptEngine;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -72,5 +74,10 @@ public class MustachePlugin extends Plugin implements ScriptPlugin, ActionPlugin
             new RestMultiSearchTemplateAction(settings),
             new RestRenderSearchTemplateAction()
         );
+    }
+
+    @Override
+    public List<Setting<?>> getSettings() {
+        return List.of(MustacheScriptEngine.MUSTACHE_RESULT_SIZE_LIMIT);
     }
 }

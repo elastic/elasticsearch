72ab7c1ff039cef7e2517c30ce918bd316265ded
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2024-12-30 11:20:17 +0100

    Licenses

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/support/MinimizationOperations.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/support/MinimizationOperations.java
@@ -1,34 +1,8 @@
 /*
- * @notice
- *
- * dk.brics.automaton
- *
- * Copyright (c) 2001-2009 Anders Moeller
- * All rights reserved.
- *
- * Modifications copyright (C) 2024 Elasticsearch B.V.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
 
 package org.elasticsearch.xpack.core.security.support;
@@ -58,6 +32,8 @@ public final class MinimizationOperations {
      *     allow more complex queries and lower to prevent memory exhaustion. Use {@link
      *     Operations#DEFAULT_DETERMINIZE_WORK_LIMIT} as a decent default if you don't otherwise know
      *     what to specify.
+     *
+     * Copied of Lucene's AutomatonTestUtil
      */
     public static Automaton minimize(Automaton a, int determinizeWorkLimit) {
 
--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/support/MinimizationOperationsTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/support/MinimizationOperationsTests.java
@@ -1,22 +1,8 @@
 /*
- * @notice
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Modifications copyright (C) 2024 Elasticsearch B.V.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
 
 package org.elasticsearch.xpack.core.security.support;
@@ -28,6 +14,7 @@ import org.elasticsearch.test.ESTestCase;
 
 import static org.elasticsearch.xpack.core.security.support.Automatons.subsetOf;
 
+// partially copied from Lucene's AutomatonTestUtil
 public class MinimizationOperationsTests extends ESTestCase {
 
     /** the minimal and non-minimal are compared to ensure they are the same. */

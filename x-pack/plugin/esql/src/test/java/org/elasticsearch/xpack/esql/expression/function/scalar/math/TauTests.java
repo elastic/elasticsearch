0d43a3e1fc7d26c512c44a51dfb11bbae7d51363
Iván Cea Fontenla <ivan.cea@elastic.co>
2025-01-02 15:57:26 +0100

    Fixed Tau

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/scalar/math/TauTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/scalar/math/TauTests.java
@@ -29,7 +29,7 @@ public class TauTests extends AbstractScalarFunctionTestCase {
 
     @ParametersFactory
     public static Iterable<Object[]> parameters() {
-        return parameterSuppliersFromTypedDataWithDefaultChecks(true, List.of(new TestCaseSupplier("Tau Test", List.of(DataType.INTEGER), () ->
+        return parameterSuppliersFromTypedDataWithDefaultChecks(true, List.of(new TestCaseSupplier("Tau Test", List.of(), () ->
             new TestCaseSupplier.TestCase(
                 List.of(),
                 "LiteralsEvaluator[lit=6.283185307179586]",

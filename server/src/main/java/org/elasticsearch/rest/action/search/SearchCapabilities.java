0c499d455f44db00bec2b5f85d8d59842aa0a043
Aurelien FOUCRET <aurelien.foucret@elastic.co>
2025-01-08 09:38:18 +0100

    Enabling KQL query on non-snapshot build.

--- server/src/main/java/org/elasticsearch/rest/action/search/SearchCapabilities.java
+++ server/src/main/java/org/elasticsearch/rest/action/search/SearchCapabilities.java
@@ -9,8 +9,6 @@
 
 package org.elasticsearch.rest.action.search;
 
-import org.elasticsearch.Build;
-
 import java.util.HashSet;
 import java.util.Set;
 
@@ -55,9 +53,8 @@ public final class SearchCapabilities {
         capabilities.add(OPTIMIZED_SCALAR_QUANTIZATION_BBQ);
         capabilities.add(KNN_QUANTIZED_VECTOR_RESCORE);
         capabilities.add(MOVING_FN_RIGHT_MATH);
-        if (Build.current().isSnapshot()) {
-            capabilities.add(KQL_QUERY_SUPPORTED);
-        }
+        capabilities.add(KQL_QUERY_SUPPORTED);
+
         CAPABILITIES = Set.copyOf(capabilities);
     }
 }

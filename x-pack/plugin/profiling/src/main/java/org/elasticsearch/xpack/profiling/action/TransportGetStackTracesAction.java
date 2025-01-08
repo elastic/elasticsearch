e5148741984090f11a3c0f9eaffa186ff350a622
Tim Rühsen <tim.ruhsen@elastic.co>
2025-01-08 08:47:33 +0100

    Rename executable name to process name

--- x-pack/plugin/profiling/src/main/java/org/elasticsearch/xpack/profiling/action/TransportGetStackTracesAction.java
+++ x-pack/plugin/profiling/src/main/java/org/elasticsearch/xpack/profiling/action/TransportGetStackTracesAction.java
@@ -355,6 +355,7 @@ public class TransportGetStackTracesAction extends TransportAction<GetStackTrace
                 new TermsAggregationBuilder("group_by")
                     // 'size' specifies the max number of host ID we support per request.
                     .size(MAX_TRACE_EVENTS_RESULT_SIZE)
+                    // 'process.executable.name' is the process name (OTEL semconv), not the executable name.
                     .field("process.executable.name")
                     // missing("") is used to include documents where the field is missing.
                     .missing("")
@@ -375,11 +376,11 @@ public class TransportGetStackTracesAction extends TransportAction<GetStackTrace
                 long totalFinalCount = 0;
                 Map<TraceEventID, TraceEvent> stackTraceEvents = new HashMap<>(MAX_TRACE_EVENTS_RESULT_SIZE);
 
-                Terms executableNames = searchResponse.getAggregations().get("group_by");
-                for (Terms.Bucket executableBucket : executableNames.getBuckets()) {
-                    String executableName = executableBucket.getKeyAsString();
+                Terms processNames = searchResponse.getAggregations().get("group_by");
+                for (Terms.Bucket processBucket : processNames.getBuckets()) {
+                    String processName = processBucket.getKeyAsString();
 
-                    Terms threads = executableBucket.getAggregations().get("group_by");
+                    Terms threads = processBucket.getAggregations().get("group_by");
                     for (Terms.Bucket threadBucket : threads.getBuckets()) {
                         String threadName = threadBucket.getKeyAsString();
 
@@ -398,7 +399,7 @@ public class TransportGetStackTracesAction extends TransportAction<GetStackTrace
 
                                 String stackTraceID = stacktraceBucket.getKeyAsString();
 
-                                TraceEventID eventID = new TraceEventID(executableName, threadName, hostID, stackTraceID);
+                                TraceEventID eventID = new TraceEventID(processName, threadName, hostID, stackTraceID);
                                 TraceEvent event = stackTraceEvents.computeIfAbsent(eventID, k -> new TraceEvent());
                                 event.count += finalCount;
                                 subGroups.collectResults(stacktraceBucket, event);

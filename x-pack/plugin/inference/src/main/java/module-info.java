daf5d2507438bacf2051935c3e5587e4736b1462
Tim Grein <grein.tim97@gmail.com>
2025-01-09 14:06:17 +0100

    Export inference.telemetry to mitigate warning in BaseTransportInferenceAction

--- x-pack/plugin/inference/src/main/java/module-info.java
+++ x-pack/plugin/inference/src/main/java/module-info.java
@@ -41,6 +41,7 @@ module org.elasticsearch.inference {
     exports org.elasticsearch.xpack.inference.services;
     exports org.elasticsearch.xpack.inference;
     exports org.elasticsearch.xpack.inference.action.task;
+    exports org.elasticsearch.xpack.inference.telemetry;
 
     provides org.elasticsearch.features.FeatureSpecification with org.elasticsearch.xpack.inference.InferenceFeatures;
 }

9fbe9b89ee7df195767ea1e57d681f7ea7da7d6c
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2025-01-09 17:23:21 +0100

    Log

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authc/Authentication.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authc/Authentication.java
@@ -199,9 +199,14 @@ public final class Authentication implements ToXContentObject {
     }
 
     private User copyUserWithRolesRemovedForLegacyApiKeys(TransportVersion version, User innerUser) {
-        // API keys prior to 7.8 had synthetic role names. Strip these out to make sure we maintain
-        // the invariant that API keys don't have role names
-        if (type == AuthenticationType.API_KEY && version.onOrBefore(TransportVersions.V_7_8_0)) {
+        // API keys prior to 7.8 had synthetic role names. Strip these out to maintain the invariant that API keys don't have role names
+        if (type == AuthenticationType.API_KEY && version.onOrBefore(TransportVersions.V_7_8_0) && innerUser.roles().length > 0) {
+            logger.debug(
+                "Stripping [{}] roles from API key user [{}] for legacy version [{}]",
+                innerUser.roles().length,
+                innerUser.principal(),
+                version
+            );
             return new User(
                 innerUser.principal(),
                 EMPTY_ARRAY,

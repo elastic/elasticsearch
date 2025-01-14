a497dafe9251708c29bfac4b8f8354b9f0be9174
Artem Prigoda <artem.prigoda@elastic.co>
2025-01-14 11:20:14 +0100

    Add support for relocating shards while holding primary permits
    For hollow shards, we `HollowIndexEngine` holds primary permits and we can relocate the shard
    with the permits provided by `HollowIndexEngine`

--- server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
@@ -764,6 +764,15 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
 
     private final AtomicBoolean primaryReplicaResyncInProgress = new AtomicBoolean();
 
+    public void relocated(
+        final String targetNodeId,
+        final String targetAllocationId,
+        final BiConsumer<ReplicationTracker.PrimaryContext, ActionListener<Void>> consumer,
+        final ActionListener<Void> listener
+    ) throws IllegalIndexShardStateException, IllegalStateException {
+        relocated(targetNodeId, targetAllocationId, consumer, listener, null);
+    }
+
     /**
      * Completes the relocation. Operations are blocked and current operations are drained before changing state to relocated. The provided
      * {@link BiConsumer} is executed after all operations are successfully blocked.
@@ -771,6 +780,7 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
      * @param consumer a {@link BiConsumer} that is executed after operations are blocked and that consumes the primary context as well as
      *                 a listener to resolve once it finished
      * @param listener listener to resolve once this method actions including executing {@code consumer} in the non-failure case complete
+     * @param acquiredPrimaryPermits the primary permits held by the caller
      * @throws IllegalIndexShardStateException if the shard is not relocating due to concurrent cancellation
      * @throws IllegalStateException           if the relocation target is no longer part of the replication group
      */
@@ -778,11 +788,12 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
         final String targetNodeId,
         final String targetAllocationId,
         final BiConsumer<ReplicationTracker.PrimaryContext, ActionListener<Void>> consumer,
-        final ActionListener<Void> listener
+        final ActionListener<Void> listener,
+        final Releasable acquiredPrimaryPermits
     ) throws IllegalIndexShardStateException, IllegalStateException {
         assert shardRouting.primary() : "only primaries can be marked as relocated: " + shardRouting;
         try (Releasable forceRefreshes = refreshListeners.forceRefreshes()) {
-            indexShardOperationPermits.blockOperations(new ActionListener<>() {
+            ActionListener<Releasable> onAcquired = new ActionListener<>() {
                 @Override
                 public void onResponse(Releasable releasable) {
                     boolean success = false;
@@ -860,8 +871,13 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
                         listener.onFailure(e);
                     }
                 }
-            }, 30L, TimeUnit.MINUTES, EsExecutors.DIRECT_EXECUTOR_SERVICE); // Wait on current thread because this execution is wrapped by
-                                                                            // CancellableThreads and we want to be able to interrupt it
+            };
+            if (acquiredPrimaryPermits == null) {
+                // Wait on current thread because this execution is wrapped by CancellableThreads and we want to be able to interrupt it
+                indexShardOperationPermits.blockOperations(onAcquired, 30L, TimeUnit.MINUTES, EsExecutors.DIRECT_EXECUTOR_SERVICE);
+            } else {
+                ActionListener.completeWith(onAcquired, () -> acquiredPrimaryPermits);
+            }
         }
     }
 

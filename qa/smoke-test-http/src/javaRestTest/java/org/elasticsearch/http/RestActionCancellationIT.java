4ccd377d27f34f8e9b1c0618e10083af0460b5d0
Niels Bauman <33722607+nielsbauman@users.noreply.github.com>
2025-01-14 13:45:58 +1000

    Run `TransportClusterGetSettingsAction` on local node (#119831)
    This action solely needs the cluster state, it can run on any node.
    Additionally, it needs to be cancellable to avoid doing unnecessary
    work after a client failure or timeout. The `?local` parameter
    becomes a no-op and is marked as deprecated.

--- qa/smoke-test-http/src/javaRestTest/java/org/elasticsearch/http/RestActionCancellationIT.java
+++ qa/smoke-test-http/src/javaRestTest/java/org/elasticsearch/http/RestActionCancellationIT.java
@@ -12,6 +12,7 @@ package org.elasticsearch.http;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.elasticsearch.action.admin.cluster.health.TransportClusterHealthAction;
+import org.elasticsearch.action.admin.cluster.settings.ClusterGetSettingsAction;
 import org.elasticsearch.action.admin.cluster.state.ClusterStateAction;
 import org.elasticsearch.action.admin.indices.alias.get.GetAliasesAction;
 import org.elasticsearch.action.admin.indices.recovery.RecoveryAction;
@@ -96,6 +97,10 @@ public class RestActionCancellationIT extends HttpSmokeTestCase {
             new Request(HttpPost.METHOD_NAME, "/_index_template/_simulate_index/test"),
             SimulateIndexTemplateAction.NAME
         );
+    }
+
+    public void testClusterGetSettingsCancellation() {
+        runRestActionCancellationTest(new Request(HttpGet.METHOD_NAME, "/_cluster/settings"), ClusterGetSettingsAction.NAME);
     }
 
     private void runRestActionCancellationTest(Request request, String actionName) {

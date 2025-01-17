32ecab04a87088483817bcada2596ba43e7fe461
Iván Cea Fontenla <ivan.cea@elastic.co>
2025-01-17 12:42:40 +0100

    Always require the _index field, and add test for lookup on eval case

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
@@ -543,6 +543,10 @@ public class EsqlSession {
         }
     }
 
+    /**
+     * Get the field names from the parsed plan combined with the ENRICH match fields from the ENRICH policy
+     * and the JOIN ON keys.
+     */
     static PreAnalysisResult fieldNames(LogicalPlan parsed, Set<String> enrichPolicyMatchFields, PreAnalysisResult result) {
         if (false == parsed.anyMatch(plan -> plan instanceof Aggregate || plan instanceof Project)) {
             // no explicit columns selection, for example "from employees"
@@ -635,18 +639,14 @@ public class EsqlSession {
         // remove valid metadata attributes because they will be filtered out by the IndexResolver anyway
         // otherwise, in some edge cases, we will fail to ask for "*" (all fields) instead
         references.removeIf(a -> a instanceof MetadataAttribute || MetadataAttribute.isSupported(a.name()));
-        Set<String> fieldNames = references.names();
 
-        if (fieldNames.isEmpty() && enrichPolicyMatchFields.isEmpty()) {
-            // there cannot be an empty list of fields, we'll ask the simplest and lightest one instead: _index
-            return result.withFieldNames(IndexResolver.INDEX_METADATA_FIELD);
-        } else {
+        Set<String> fieldNames = references.names();
             fieldNames.addAll(subfields(fieldNames));
             fieldNames.addAll(enrichPolicyMatchFields);
             fieldNames.addAll(subfields(enrichPolicyMatchFields));
+        fieldNames.addAll(IndexResolver.INDEX_METADATA_FIELD);
             return result.withFieldNames(fieldNames);
         }
-    }
 
     private static boolean matchByName(Attribute attr, String other, boolean skipIfPattern) {
         boolean isPattern = Regex.isSimpleMatchPattern(attr.name());

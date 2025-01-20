b8759671625b5df69909cff7b40258f37c4ae245
Armin Braun <me@obrown.io>
2025-01-20 17:29:11 +0100

    fail phase on any DN merge exception

--- server/src/main/java/org/elasticsearch/action/search/SearchQueryThenFetchAsyncAction.java
+++ server/src/main/java/org/elasticsearch/action/search/SearchQueryThenFetchAsyncAction.java
@@ -683,7 +683,7 @@ public class SearchQueryThenFetchAsyncAction implements AsyncSearchContext {
 
                     @Override
                     public void handleException(TransportException e) {
-                        onNodeQueryFailure((Exception) ExceptionsHelper.unwrapCause(e), request, nodeId);
+                        onPhaseFailure(NAME, "", ExceptionsHelper.unwrapCause(e));
                     }
                 });
         });

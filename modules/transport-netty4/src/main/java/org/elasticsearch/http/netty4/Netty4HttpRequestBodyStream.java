86145b942eb98c72ea48305d0cb382e0e9881e6e
Mikhail Berezovskiy <mikhail@elastic.co>
2025-01-20 15:24:44 -0800

    feedback

--- modules/transport-netty4/src/main/java/org/elasticsearch/http/netty4/Netty4HttpRequestBodyStream.java
+++ modules/transport-netty4/src/main/java/org/elasticsearch/http/netty4/Netty4HttpRequestBodyStream.java
@@ -78,12 +78,12 @@ public class Netty4HttpRequestBodyStream implements HttpBody.Stream {
         assert handler != null : "handler must be set before requesting next chunk";
         requestContext = threadContext.newStoredContext();
         channel.eventLoop().submit(() -> {
-            if (closing) {
-                return;
-            }
             activityTracker.startActivity();
             requested = true;
             try {
+                if (closing) {
+                    return;
+                }
                 if (buf == null) {
                     channel.read();
                 } else {

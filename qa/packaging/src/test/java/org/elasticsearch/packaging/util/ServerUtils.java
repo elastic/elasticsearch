6cd242550c6df57110df32c7a8659aed3ab482ed
Slobodan Adamovic <slobodan.adamovic@elastic.co>
2025-01-21 22:09:47 +0100

    increase docker timeout for waiting on security index

--- qa/packaging/src/test/java/org/elasticsearch/packaging/util/ServerUtils.java
+++ qa/packaging/src/test/java/org/elasticsearch/packaging/util/ServerUtils.java
@@ -66,7 +66,7 @@ public class ServerUtils {
     private static final long waitTime = TimeUnit.MINUTES.toMillis(3);
     private static final long timeoutLength = TimeUnit.SECONDS.toMillis(30);
     private static final long requestInterval = TimeUnit.SECONDS.toMillis(5);
-    private static final long dockerWaitForSecurityIndex = TimeUnit.SECONDS.toMillis(25);
+    private static final long dockerWaitForSecurityIndex = TimeUnit.SECONDS.toMillis(60);
 
     public static void waitForElasticsearch(Installation installation) throws Exception {
         final boolean securityEnabled;

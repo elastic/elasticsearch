716565b39b6e11614dba6eb366ca5dbb82e8c982
carlosdelest <carlos.delgado@elastic.co>
2025-01-21 17:35:50 +0100

    Add guards for match options based on capability

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/Match.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/Match.java
@@ -14,6 +14,7 @@ import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.xpack.esql.action.EsqlCapabilities;
 import org.elasticsearch.xpack.esql.capabilities.PostOptimizationVerificationAware;
 import org.elasticsearch.xpack.esql.common.Failure;
 import org.elasticsearch.xpack.esql.common.Failures;
@@ -226,7 +227,7 @@ public class Match extends FullTextFunction implements PostOptimizationVerificat
         }
 
         // TODO Extract common logic for validating options
-        if (options() != null) {
+        if (options() != null && EsqlCapabilities.Cap.MATCH_FUNCTION_OPTIONS.isEnabled()) {
             // MapExpression does not have a DataType associated with it
             resolution = isMapExpression(options(), sourceText(), SECOND);
             if (resolution.unresolved()) {
@@ -247,7 +248,7 @@ public class Match extends FullTextFunction implements PostOptimizationVerificat
         // Match is lenient by default to avoid failing on incompatible types
         matchOptions.put(LENIENT_FIELD.getPreferredName(), true);
 
-        if (options() == null) {
+        if (options() == null || EsqlCapabilities.Cap.MATCH_FUNCTION_OPTIONS.isEnabled() == false) {
             return matchOptions;
         }
 

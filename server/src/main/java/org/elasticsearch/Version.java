917caf57637fb9ecd77208f3425bf85cc652b181
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-01-21 16:22:51 +0000

    Bump versions after 8.16.3 release

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -195,7 +195,8 @@ public class Version implements VersionId<Version>, ToXContentFragment {
     public static final Version V_8_16_1 = new Version(8_16_01_99);
     public static final Version V_8_16_2 = new Version(8_16_02_99);
     public static final Version V_8_16_3 = new Version(8_16_03_99);
-    public static final Version CURRENT = V_8_16_3;
+    public static final Version V_8_16_4 = new Version(8_16_04_99);
+    public static final Version CURRENT = V_8_16_4;
 
     private static final NavigableMap<Integer, Version> VERSION_IDS;
     private static final Map<String, Version> VERSION_STRINGS;

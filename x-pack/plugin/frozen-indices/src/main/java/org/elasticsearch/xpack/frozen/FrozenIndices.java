50c3b25d7265bac47fac863db884fbc831f1d34e
Pete Gillin <pete.gillin@elastic.co>
2025-01-22 11:21:48 +0000

    Remove the ability to read frozen indices (#120108)
    This won't be supported in 9.0. In 8.18+ we report a `CRITICAL`
    deprecation warning telling users to unfreeze any frozen indices (see
    https://github.com/elastic/elasticsearch/pull/119879).
    
    ES-9736 #comment Removed frozen engine registration from the plugin in https://github.com/elastic/elasticsearch/pull/120108

--- x-pack/plugin/frozen-indices/src/main/java/org/elasticsearch/xpack/frozen/FrozenIndices.java
+++ x-pack/plugin/frozen-indices/src/main/java/org/elasticsearch/xpack/frozen/FrozenIndices.java
@@ -9,11 +9,8 @@ package org.elasticsearch.xpack.frozen;
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.common.settings.Setting;
-import org.elasticsearch.index.IndexSettings;
-import org.elasticsearch.index.engine.EngineFactory;
 import org.elasticsearch.index.engine.frozen.FrozenEngine;
 import org.elasticsearch.plugins.ActionPlugin;
-import org.elasticsearch.plugins.EnginePlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.xpack.core.action.XPackUsageFeatureAction;
 import org.elasticsearch.xpack.core.frozen.action.FreezeIndexAction;
@@ -22,18 +19,8 @@ import org.elasticsearch.xpack.frozen.action.TransportFreezeIndexAction;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
-public class FrozenIndices extends Plugin implements ActionPlugin, EnginePlugin {
-
-    @Override
-    public Optional<EngineFactory> getEngineFactory(IndexSettings indexSettings) {
-        if (indexSettings.getValue(FrozenEngine.INDEX_FROZEN) && indexSettings.getIndexMetadata().isSearchableSnapshot() == false) {
-            return Optional.of(config -> new FrozenEngine(config, true, false));
-        } else {
-            return Optional.empty();
-        }
-    }
+public class FrozenIndices extends Plugin implements ActionPlugin {
 
     @Override
     public List<Setting<?>> getSettings() {

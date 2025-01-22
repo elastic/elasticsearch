8ced46f0c26c8a46bd04b31ac95dcba5d56b8468
David Kyle <david.kyle@elastic.co>
2025-01-22 15:03:56 +0000

    Revert "Test rollover in upgrade"
    This reverts commit 97995d424cfacddf9d5b7497a6390fa48b74dcb7.

--- x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/MlIndexRollover.java
+++ x-pack/plugin/ml/src/main/java/org/elasticsearch/xpack/ml/MlIndexRollover.java
@@ -136,9 +136,8 @@ public class MlIndexRollover implements MlAutoUpdateService.UpdateAction {
         }
 
         String latestIndex = MlIndexAndAlias.latestIndex(concreteIndices);
-        boolean isCompatibleIndexVersion = false;
-        //isCompatibleIndexVersion(clusterState.metadata().index(latestIndex).getCreationVersion());
-        boolean hasAlias = false; //clusterState.getMetadata().hasAlias(alias);
+        boolean isCompatibleIndexVersion = isCompatibleIndexVersion(clusterState.metadata().index(latestIndex).getCreationVersion());
+        boolean hasAlias = clusterState.getMetadata().hasAlias(alias);
 
         if (isCompatibleIndexVersion && hasAlias) {
             // v8 index with alias, no action required
@@ -146,16 +145,13 @@ public class MlIndexRollover implements MlAutoUpdateService.UpdateAction {
             return;
         }
 
-
         SubscribableListener.<Boolean>newForked(l -> {
-            assertThread("ml_utility");
             if (hasAlias == false) {
                 MlIndexAndAlias.updateWriteAlias(client, alias, null, latestIndex, l);
             } else {
                 l.onResponse(Boolean.TRUE);
             }
         }).<Boolean>andThen((l, success) -> {
-            assertThread("ml_utility");
             if (isCompatibleIndexVersion == false) {
                 logger.info("rolling over legacy index [{}] with alias [{}]", latestIndex, alias);
                 rollover(alias, l);
@@ -176,12 +172,5 @@ public class MlIndexRollover implements MlAutoUpdateService.UpdateAction {
      */
     static boolean isCompatibleIndexVersion(IndexVersion version) {
         return version.onOrAfter(IndexVersions.MINIMUM_COMPATIBLE);
-    }
-
-
-    public static boolean assertThread(String expectedName) {
-//        final Thread t = Thread.currentThread();
-//        assert t.getName().contains(expectedName) : "Expected current thread [" + t.getName() + "] to be a [" + expectedName + "] thread";
-        return true;
     }
 }

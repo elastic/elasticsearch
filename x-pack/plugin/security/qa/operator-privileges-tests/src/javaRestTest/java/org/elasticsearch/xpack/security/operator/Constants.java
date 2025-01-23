57b2cd6156f623a043118182623edff83d2e3b9a
Stanislav Malyshev <smalyshev@users.noreply.github.com>
2025-01-23 10:21:52 -0700

    ES|QL async queries: Partial result on demand (#118122)
    Add capability to stop async query on demand
    The theory:
    
    - User initiates async search request
    - User sends the stop request (POST _query/async/<ID>/stop)
    - If the async is finished by that time, it's like regular async get
    - If it's not finished, the sinks are closed and the request is forcefully finished
    
    (cherry picked from commit f27f74666f1486dd2802f0d3ceaf3b208659f261)
    
    # Conflicts:
    #	x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlQueryResponse.java
    #	x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/action/EsqlQueryResponseTests.java
    #	x-pack/plugin/security/qa/multi-cluster/src/javaRestTest/java/org/elasticsearch/xpack/remotecluster/CrossClusterEsqlRCS1UnavailableRemotesIT.java
    #	x-pack/plugin/security/qa/multi-cluster/src/javaRestTest/java/org/elasticsearch/xpack/remotecluster/CrossClusterEsqlRCS2UnavailableRemotesIT.java

--- x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
+++ x-pack/plugin/security/qa/operator-privileges-tests/src/javaRestTest/java/org/elasticsearch/xpack/security/operator/Constants.java
@@ -568,6 +568,7 @@ public class Constants {
         "indices:data/read/eql/async/get",
         "indices:data/read/esql",
         "indices:data/read/esql/async/get",
+        "indices:data/read/esql/async/stop",
         "indices:data/read/esql/resolve_fields",
         "indices:data/read/esql/search_shards",
         "indices:data/read/explain",

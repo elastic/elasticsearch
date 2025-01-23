e87639829e79e5dabddb1206081e371463d88d93
Luke Whiting <luke.whiting@elastic.co>
2025-01-23 11:03:21 +0000

    PR Changes - Tests for re-wording

--- x-pack/plugin/deprecation/src/test/java/org/elasticsearch/xpack/deprecation/IndexDeprecationChecksTests.java
+++ x-pack/plugin/deprecation/src/test/java/org/elasticsearch/xpack/deprecation/IndexDeprecationChecksTests.java
@@ -144,8 +144,9 @@ public class IndexDeprecationChecksTests extends ESTestCase {
             DeprecationIssue.Level.WARNING,
             "Old index with a compatibility version < 9.0 Has Been Ignored",
             "https://www.elastic.co/guide/en/elasticsearch/reference/master/breaking-changes-9.0.html",
-            "This index has version: " + createdWith.toReleaseVersion() + " and has been marked as OK to become read-only after upgrade",
-            false,
+            "This read-only index has version: "
+                + createdWith.toReleaseVersion()
+                + " and will be supported as read-only in 9.0",            false,
             singletonMap("reindex_required", true)
         );
         List<DeprecationIssue> issues = DeprecationChecks.filterChecks(INDEX_SETTINGS_CHECKS, c -> c.apply(indexMetadata, clusterState));

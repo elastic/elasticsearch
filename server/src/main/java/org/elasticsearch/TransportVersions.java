fce11b3307b96ab45a20871ce223aeb7ba93876c
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-01-23 09:52:28 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -162,7 +162,6 @@ public class TransportVersions {
     public static final TransportVersion ADD_INDEX_BLOCK_TWO_PHASE = def(8_828_00_0);
     public static final TransportVersion RESOLVE_CLUSTER_NO_INDEX_EXPRESSION = def(8_829_00_0);
     public static final TransportVersion ML_ROLLOVER_LEGACY_INDICES = def(8_830_00_0);
-  
 
     /*
      * STOP! READ THIS FIRST! No, really,

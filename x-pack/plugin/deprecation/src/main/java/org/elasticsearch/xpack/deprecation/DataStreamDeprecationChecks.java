b3e64ac68eface6ba8f5a42a9548c35479c7b53d
Luke Whiting <luke.whiting@elastic.co>
2025-01-23 10:16:37 +0000

    PR Changes - Reword messaged and methods for clarity

--- x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/DataStreamDeprecationChecks.java
+++ x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/DataStreamDeprecationChecks.java
@@ -24,7 +24,7 @@ public class DataStreamDeprecationChecks {
     static DeprecationIssue oldIndicesCheck(DataStream dataStream, ClusterState clusterState) {
         List<Index> backingIndices = dataStream.getIndices();
 
-        Set<String> indicesNeedingUpgrade = getReIndexRequiredIndices(backingIndices, clusterState, false);
+        Set<String> indicesNeedingUpgrade = getReindexRequiredIndices(backingIndices, clusterState, false);
 
         if (indicesNeedingUpgrade.isEmpty() == false) {
             return new DeprecationIssue(
@@ -48,7 +48,7 @@ public class DataStreamDeprecationChecks {
     static DeprecationIssue ignoredOldIndicesCheck(DataStream dataStream, ClusterState clusterState) {
         List<Index> backingIndices = dataStream.getIndices();
 
-        Set<String> ignoredIndices = getReIndexRequiredIndices(backingIndices, clusterState, true);
+        Set<String> ignoredIndices = getReindexRequiredIndices(backingIndices, clusterState, true);
 
         if (ignoredIndices.isEmpty() == false) {
             return new DeprecationIssue(
@@ -56,7 +56,7 @@ public class DataStreamDeprecationChecks {
                 "Old data stream with a compatibility version < 9.0 Have Been Ignored",
                 "https://www.elastic.co/guide/en/elasticsearch/reference/master/breaking-changes-9.0.html",
                 "This data stream has backing indices that were created before Elasticsearch 9.0.0 and have been marked as OK to"
-                    + "become read-only after upgrade",
+                    + "remain read-only after upgrade",
                 false,
                 ofEntries(
                     entry("reindex_required", true),
@@ -70,7 +70,7 @@ public class DataStreamDeprecationChecks {
         return null;
     }
 
-    private static Set<String> getReIndexRequiredIndices(
+    private static Set<String> getReindexRequiredIndices(
         List<Index> backingIndices,
         ClusterState clusterState,
         boolean filterToBlockedStatus

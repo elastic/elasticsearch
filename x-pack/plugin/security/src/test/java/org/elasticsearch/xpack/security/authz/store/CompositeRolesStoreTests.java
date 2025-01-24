9893ccfbe74788bb29fba1995f392f47c26032f8
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-01-24 23:31:46 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authz/store/CompositeRolesStoreTests.java
+++ x-pack/plugin/security/src/test/java/org/elasticsearch/xpack/security/authz/store/CompositeRolesStoreTests.java
@@ -492,7 +492,7 @@ public class CompositeRolesStoreTests extends ESTestCase {
         getRoleForRoleNames(compositeRolesStore, Set.of("_mock_role"), future);
 
         final ElasticsearchSecurityException e = expectThrows(ElasticsearchSecurityException.class, future::actionGet);
-        //TODO: [Jake] should we figure out how to avoid exposing this error to the user?
+        // TODO: [Jake] should we figure out how to avoid exposing this error to the user?
         assertThat(
             e.getMessage(),
             containsString("The set of patterns [/(~(([.]|ilm-history-).*)&~(~(([.]|ilm-history-).*::failures)/] is invalid")

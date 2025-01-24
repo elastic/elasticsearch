elastic/elasticsearch

commit f61f1396537ff81c09c339e50bd229b2a4049e09

Match, Like and RLike operators improved docs (#120504)

x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/AbstractMatchFullTextFunction.java
@@ -1,0 +1,219 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.esql.expression.function.fulltext;
+
+import org.apache.lucene.util.BytesRef;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.xpack.esql.capabilities.PostOptimizationVerificationAware;
+import org.elasticsearch.xpack.esql.common.Failure;
+import org.elasticsearch.xpack.esql.common.Failures;
+import org.elasticsearch.xpack.esql.core.expression.Expression;
+import org.elasticsearch.xpack.esql.core.expression.FieldAttribute;
+import org.elasticsearch.xpack.esql.core.expression.FoldContext;
+import org.elasticsearch.xpack.esql.core.querydsl.query.Query;
+import org.elasticsearch.xpack.esql.core.tree.Source;
+import org.elasticsearch.xpack.esql.core.type.DataType;
+import org.elasticsearch.xpack.esql.core.type.MultiTypeEsField;
+import org.elasticsearch.xpack.esql.core.util.NumericUtils;
+import org.elasticsearch.xpack.esql.expression.function.scalar.convert.AbstractConvertFunction;
+import org.elasticsearch.xpack.esql.planner.TranslatorHandler;
+import org.elasticsearch.xpack.esql.querydsl.query.MatchQuery;
+import org.elasticsearch.xpack.esql.type.EsqlDataTypeConverter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.ParamOrdinal;
+import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.ParamOrdinal.FIRST;
+import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.ParamOrdinal.SECOND;
+import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.isNotNull;
+import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.isNotNullAndFoldable;
+import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.isType;
+import static org.elasticsearch.xpack.esql.core.type.DataType.BOOLEAN;
+import static org.elasticsearch.xpack.esql.core.type.DataType.DATETIME;
+import static org.elasticsearch.xpack.esql.core.type.DataType.DATE_NANOS;
+import static org.elasticsearch.xpack.esql.core.type.DataType.DOUBLE;
+import static org.elasticsearch.xpack.esql.core.type.DataType.INTEGER;
+import static org.elasticsearch.xpack.esql.core.type.DataType.IP;
+import static org.elasticsearch.xpack.esql.core.type.DataType.KEYWORD;
+import static org.elasticsearch.xpack.esql.core.type.DataType.LONG;
+import static org.elasticsearch.xpack.esql.core.type.DataType.SEMANTIC_TEXT;
+import static org.elasticsearch.xpack.esql.core.type.DataType.TEXT;
+import static org.elasticsearch.xpack.esql.core.type.DataType.UNSIGNED_LONG;
+import static org.elasticsearch.xpack.esql.core.type.DataType.VERSION;
+import static org.elasticsearch.xpack.esql.expression.predicate.operator.comparison.EsqlBinaryComparison.formatIncompatibleTypesMessage;
+
+/**
+ * This class contains the common functionalities between the match function ({@link Match}) and match operator ({@link MatchOperator}),
+ * so the two subclasses just contains the different code
+ */
+public abstract class AbstractMatchFullTextFunction extends FullTextFunction implements PostOptimizationVerificationAware {
+    public static final Set<DataType> FIELD_DATA_TYPES = Set.of(
+        KEYWORD,
+        TEXT,
+        SEMANTIC_TEXT,
+        BOOLEAN,
+        DATETIME,
+        DATE_NANOS,
+        DOUBLE,
+        INTEGER,
+        IP,
+        LONG,
+        UNSIGNED_LONG,
+        VERSION
+    );
+    public static final Set<DataType> QUERY_DATA_TYPES = Set.of(
+        KEYWORD,
+        BOOLEAN,
+        DATETIME,
+        DATE_NANOS,
+        DOUBLE,
+        INTEGER,
+        IP,
+        LONG,
+        UNSIGNED_LONG,
+        VERSION
+    );
+    protected final Expression field;
+
+    protected AbstractMatchFullTextFunction(
+        Source source,
+        Expression query,
+        List<Expression> children,
+        QueryBuilder queryBuilder,
+        Expression field
+    ) {
+        super(source, query, children, queryBuilder);
+        this.field = field;
+    }
+
+    public Expression field() {
+        return field;
+    }
+
+    @Override
+    protected TypeResolution resolveNonQueryParamTypes() {
+        return isNotNull(field, sourceText(), FIRST).and(
+            isType(
+                field,
+                FIELD_DATA_TYPES::contains,
+                sourceText(),
+                FIRST,
+                "keyword, text, boolean, date, date_nanos, double, integer, ip, long, unsigned_long, version"
+            )
+        );
+    }
+
+    @Override
+    protected TypeResolution resolveQueryParamType() {
+        return isType(
+            query(),
+            QUERY_DATA_TYPES::contains,
+            sourceText(),
+            queryParamOrdinal(),
+            "keyword, boolean, date, date_nanos, double, integer, ip, long, unsigned_long, version"
+        ).and(isNotNullAndFoldable(query(), sourceText(), queryParamOrdinal()));
+    }
+
+    @Override
+    protected TypeResolution checkParamCompatibility() {
+        DataType fieldType = field().dataType();
+        DataType queryType = query().dataType();
+
+        // Field and query types should match. If the query is a string, then it can match any field type.
+        if ((fieldType == queryType) || (queryType == KEYWORD)) {
+            return TypeResolution.TYPE_RESOLVED;
+        }
+
+        if (fieldType.isNumeric() && queryType.isNumeric()) {
+            // When doing an unsigned long query, field must be an unsigned long
+            if ((queryType == UNSIGNED_LONG && fieldType != UNSIGNED_LONG) == false) {
+                return TypeResolution.TYPE_RESOLVED;
+            }
+        }
+
+        return new TypeResolution(formatIncompatibleTypesMessage(fieldType, queryType, sourceText()));
+    }
+
+    @Override
+    public void postOptimizationVerification(Failures failures) {
+        Expression fieldExpression = field();
+        // Field may be converted to other data type (field_name :: data_type), so we need to check the original field
+        if (fieldExpression instanceof AbstractConvertFunction convertFunction) {
+            fieldExpression = convertFunction.field();
+        }
+        if (fieldExpression instanceof FieldAttribute == false) {
+            failures.add(
+                Failure.fail(
+                    field,
+                    "[{}] {} cannot operate on [{}], which is not a field from an index mapping",
+                    functionName(),
+                    functionType(),
+                    field.sourceText()
+                )
+            );
+        }
+    }
+
+    @Override
+    public Object queryAsObject() {
+        Object queryAsObject = query().fold(FoldContext.small() /* TODO remove me */);
+
+        // Convert BytesRef to string for string-based values
+        if (queryAsObject instanceof BytesRef bytesRef) {
+            return switch (query().dataType()) {
+                case IP -> EsqlDataTypeConverter.ipToString(bytesRef);
+                case VERSION -> EsqlDataTypeConverter.versionToString(bytesRef);
+                default -> bytesRef.utf8ToString();
+            };
+        }
+
+        // Converts specific types to the correct type for the query
+        if (query().dataType() == DataType.UNSIGNED_LONG) {
+            return NumericUtils.unsignedLongAsBigInteger((Long) queryAsObject);
+        } else if (query().dataType() == DataType.DATETIME && queryAsObject instanceof Long) {
+            // When casting to date and datetime, we get a long back. But Match query needs a date string
+            return EsqlDataTypeConverter.dateTimeToString((Long) queryAsObject);
+        } else if (query().dataType() == DATE_NANOS && queryAsObject instanceof Long) {
+            return EsqlDataTypeConverter.nanoTimeToString((Long) queryAsObject);
+        }
+
+        return queryAsObject;
+    }
+
+    @Override
+    protected Query translate(TranslatorHandler handler) {
+        Expression fieldExpression = field;
+        // Field may be converted to other data type (field_name :: data_type), so we need to check the original field
+        if (fieldExpression instanceof AbstractConvertFunction convertFunction) {
+            fieldExpression = convertFunction.field();
+        }
+        if (fieldExpression instanceof FieldAttribute fieldAttribute) {
+            String fieldName = fieldAttribute.name();
+            if (fieldAttribute.field() instanceof MultiTypeEsField multiTypeEsField) {
+                // If we have multiple field types, we allow the query to be done, but getting the underlying field name
+                fieldName = multiTypeEsField.getName();
+            }
+            // Make query lenient so mixed field types can be queried when a field type is incompatible with the value provided
+            return new MatchQuery(source(), fieldName, queryAsObject(), Map.of("lenient", "true"));
+        }
+
+        throw new IllegalArgumentException("Match must have a field attribute as the first argument");
+    }
+
+    @Override
+    public Expression replaceQueryBuilder(QueryBuilder queryBuilder) {
+        return new Match(source(), field, query(), queryBuilder);
+    }
+
+    protected ParamOrdinal queryParamOrdinal() {
+        return SECOND;
+    }
+
+}

x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/Match.java
@@ -7,101 +7,33 @@
 
 package org.elasticsearch.xpack.esql.expression.function.fulltext;
 
-import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.TransportVersions;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.xpack.esql.capabilities.PostOptimizationVerificationAware;
-import org.elasticsearch.xpack.esql.common.Failure;
-import org.elasticsearch.xpack.esql.common.Failures;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
-import org.elasticsearch.xpack.esql.core.expression.FieldAttribute;
-import org.elasticsearch.xpack.esql.core.expression.FoldContext;
-import org.elasticsearch.xpack.esql.core.expression.TypeResolutions;
-import org.elasticsearch.xpack.esql.core.querydsl.query.Query;
-import org.elasticsearch.xpack.esql.core.querydsl.query.QueryStringQuery;
 import org.elasticsearch.xpack.esql.core.tree.NodeInfo;
 import org.elasticsearch.xpack.esql.core.tree.Source;
-import org.elasticsearch.xpack.esql.core.type.DataType;
-import org.elasticsearch.xpack.esql.core.type.MultiTypeEsField;
-import org.elasticsearch.xpack.esql.core.util.NumericUtils;
 import org.elasticsearch.xpack.esql.expression.function.Example;
 import org.elasticsearch.xpack.esql.expression.function.FunctionInfo;
 import org.elasticsearch.xpack.esql.expression.function.Param;
-import org.elasticsearch.xpack.esql.expression.function.scalar.convert.AbstractConvertFunction;
 import org.elasticsearch.xpack.esql.io.stream.PlanStreamInput;
-import org.elasticsearch.xpack.esql.planner.TranslatorHandler;
-import org.elasticsearch.xpack.esql.querydsl.query.MatchQuery;
-import org.elasticsearch.xpack.esql.type.EsqlDataTypeConverter;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-
-import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.ParamOrdinal.FIRST;
-import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.ParamOrdinal.SECOND;
-import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.isNotNull;
-import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.isNotNullAndFoldable;
-import static org.elasticsearch.xpack.esql.core.expression.TypeResolutions.isType;
-import static org.elasticsearch.xpack.esql.core.type.DataType.BOOLEAN;
-import static org.elasticsearch.xpack.esql.core.type.DataType.DATETIME;
-import static org.elasticsearch.xpack.esql.core.type.DataType.DATE_NANOS;
-import static org.elasticsearch.xpack.esql.core.type.DataType.DOUBLE;
-import static org.elasticsearch.xpack.esql.core.type.DataType.INTEGER;
-import static org.elasticsearch.xpack.esql.core.type.DataType.IP;
-import static org.elasticsearch.xpack.esql.core.type.DataType.KEYWORD;
-import static org.elasticsearch.xpack.esql.core.type.DataType.LONG;
-import static org.elasticsearch.xpack.esql.core.type.DataType.SEMANTIC_TEXT;
-import static org.elasticsearch.xpack.esql.core.type.DataType.TEXT;
-import static org.elasticsearch.xpack.esql.core.type.DataType.UNSIGNED_LONG;
-import static org.elasticsearch.xpack.esql.core.type.DataType.VERSION;
-import static org.elasticsearch.xpack.esql.expression.predicate.operator.comparison.EsqlBinaryComparison.formatIncompatibleTypesMessage;
 
 /**
- * Full text function that performs a {@link QueryStringQuery} .
+ * Full text function that performs a {@link org.elasticsearch.xpack.esql.querydsl.query.MatchQuery} .
  */
-public class Match extends FullTextFunction implements PostOptimizationVerificationAware {
+public class Match extends AbstractMatchFullTextFunction {
 
     public static final NamedWriteableRegistry.Entry ENTRY = new NamedWriteableRegistry.Entry(Expression.class, "Match", Match::readFrom);
 
-    private final Expression field;
-
     private transient Boolean isOperator;
-
-    public static final Set<DataType> FIELD_DATA_TYPES = Set.of(
-        KEYWORD,
-        TEXT,
-        SEMANTIC_TEXT,
-        BOOLEAN,
-        DATETIME,
-        DATE_NANOS,
-        DOUBLE,
-        INTEGER,
-        IP,
-        LONG,
-        UNSIGNED_LONG,
-        VERSION
-    );
-    public static final Set<DataType> QUERY_DATA_TYPES = Set.of(
-        KEYWORD,
-        BOOLEAN,
-        DATETIME,
-        DATE_NANOS,
-        DOUBLE,
-        INTEGER,
-        IP,
-        LONG,
-        UNSIGNED_LONG,
-        VERSION
-    );
 
     @FunctionInfo(
         returnType = "boolean",
-        operator = ":",
         preview = true,
         description = """
             Use `MATCH` to perform a <<query-dsl-match-query,match query>> on the specified field.
@@ -132,8 +64,7 @@ public class Match extends FullTextFunction implements PostOptimizationVerificat
     }
 
     public Match(Source source, Expression field, Expression matchQuery, QueryBuilder queryBuilder) {
-        super(source, matchQuery, List.of(field, matchQuery), queryBuilder);
-        this.field = field;
+        super(source, matchQuery, List.of(field, matchQuery), queryBuilder, field);
     }
 
     private static Match readFrom(StreamInput in) throws IOException {
@@ -163,152 +94,12 @@ public class Match extends FullTextFunction implements PostOptimizationVerificat
     }
 
     @Override
-    protected TypeResolution resolveNonQueryParamTypes() {
-        return isNotNull(field, sourceText(), FIRST).and(
-            isType(
-                field,
-                FIELD_DATA_TYPES::contains,
-                sourceText(),
-                FIRST,
-                "keyword, text, boolean, date, date_nanos, double, integer, ip, long, unsigned_long, version"
-            )
-        );
-    }
-
-    @Override
-    protected TypeResolution resolveQueryParamType() {
-        return isType(
-            query(),
-            QUERY_DATA_TYPES::contains,
-            sourceText(),
-            queryParamOrdinal(),
-            "keyword, boolean, date, date_nanos, double, integer, ip, long, unsigned_long, version"
-        ).and(isNotNullAndFoldable(query(), sourceText(), queryParamOrdinal()));
-    }
-
-    @Override
-    protected TypeResolution checkParamCompatibility() {
-        DataType fieldType = field().dataType();
-        DataType queryType = query().dataType();
-
-        // Field and query types should match. If the query is a string, then it can match any field type.
-        if ((fieldType == queryType) || (queryType == KEYWORD)) {
-            return TypeResolution.TYPE_RESOLVED;
-        }
-
-        if (fieldType.isNumeric() && queryType.isNumeric()) {
-            // When doing an unsigned long query, field must be an unsigned long
-            if ((queryType == UNSIGNED_LONG && fieldType != UNSIGNED_LONG) == false) {
-                return TypeResolution.TYPE_RESOLVED;
-            }
-        }
-
-        return new TypeResolution(formatIncompatibleTypesMessage(fieldType, queryType, sourceText()));
-    }
-
-    @Override
-    public void postOptimizationVerification(Failures failures) {
-        Expression fieldExpression = field();
-        // Field may be converted to other data type (field_name :: data_type), so we need to check the original field
-        if (fieldExpression instanceof AbstractConvertFunction convertFunction) {
-            fieldExpression = convertFunction.field();
-        }
-        if (fieldExpression instanceof FieldAttribute == false) {
-            failures.add(
-                Failure.fail(
-                    field,
-                    "[{}] {} cannot operate on [{}], which is not a field from an index mapping",
-                    functionName(),
-                    functionType(),
-                    field.sourceText()
-                )
-            );
-        }
-    }
-
-    @Override
-    public Object queryAsObject() {
-        Object queryAsObject = query().fold(FoldContext.small() /* TODO remove me */);
-
-        // Convert BytesRef to string for string-based values
-        if (queryAsObject instanceof BytesRef bytesRef) {
-            return switch (query().dataType()) {
-                case IP -> EsqlDataTypeConverter.ipToString(bytesRef);
-                case VERSION -> EsqlDataTypeConverter.versionToString(bytesRef);
-                default -> bytesRef.utf8ToString();
-            };
-        }
-
-        // Converts specific types to the correct type for the query
-        if (query().dataType() == DataType.UNSIGNED_LONG) {
-            return NumericUtils.unsignedLongAsBigInteger((Long) queryAsObject);
-        } else if (query().dataType() == DataType.DATETIME && queryAsObject instanceof Long) {
-            // When casting to date and datetime, we get a long back. But Match query needs a date string
-            return EsqlDataTypeConverter.dateTimeToString((Long) queryAsObject);
-        } else if (query().dataType() == DATE_NANOS && queryAsObject instanceof Long) {
-            return EsqlDataTypeConverter.nanoTimeToString((Long) queryAsObject);
-        }
-
-        return queryAsObject;
-    }
-
-    @Override
     public Expression replaceChildren(List<Expression> newChildren) {
         return new Match(source(), newChildren.get(0), newChildren.get(1), queryBuilder());
     }
 
     @Override
     protected NodeInfo<? extends Expression> info() {
-        return NodeInfo.create(this, Match::new, field, query(), queryBuilder());
-    }
-
-    protected TypeResolutions.ParamOrdinal queryParamOrdinal() {
-        return SECOND;
-    }
-
-    public Expression field() {
-        return field;
-    }
-
-    @Override
-    public String functionType() {
-        return isOperator() ? "operator" : super.functionType();
-    }
-
-    @Override
-    protected Query translate(TranslatorHandler handler) {
-        Expression fieldExpression = field;
-        // Field may be converted to other data type (field_name :: data_type), so we need to check the original field
-        if (fieldExpression instanceof AbstractConvertFunction convertFunction) {
-            fieldExpression = convertFunction.field();
-        }
-        if (fieldExpression instanceof FieldAttribute fieldAttribute) {
-            String fieldName = fieldAttribute.name();
-            if (fieldAttribute.field() instanceof MultiTypeEsField multiTypeEsField) {
-                // If we have multiple field types, we allow the query to be done, but getting the underlying field name
-                fieldName = multiTypeEsField.getName();
-            }
-            // Make query lenient so mixed field types can be queried when a field type is incompatible with the value provided
-            return new MatchQuery(source(), fieldName, queryAsObject(), Map.of("lenient", "true"));
-        }
-
-        throw new IllegalArgumentException("Match must have a field attribute as the first argument");
-    }
-
-    @Override
-    public Expression replaceQueryBuilder(QueryBuilder queryBuilder) {
-        return new Match(source(), field, query(), queryBuilder);
-    }
-
-    @Override
-    public String functionName() {
-        return isOperator() ? ":" : super.functionName();
-    }
-
-    private boolean isOperator() {
-        if (isOperator == null) {
-            isOperator = source().text().toUpperCase(Locale.ROOT).matches("^" + super.functionName() + "\\s*\\(.*\\)") == false;
-        }
-        return isOperator;
+        return NodeInfo.create(this, Match::new, field(), query(), queryBuilder());
     }
 }

commit 838599363ca7365715780ee0a50974fca55e8a2e

Fix serialization bug introduced in #120504

server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -165,6 +165,7 @@ public class TransportVersions {
     public static final TransportVersion ADD_INCLUDE_FAILURE_INDICES_OPTION = def(8_831_00_0);
     public static final TransportVersion ESQL_RESPONSE_PARTIAL = def(8_832_00_0);
     public static final TransportVersion RANK_DOC_OPTIONAL_METADATA_FOR_EXPLAIN = def(8_833_00_0);
+    public static final TransportVersion ESQL_FIX_SERIALIZATION_MATCH_OPERATOR = def(8_834_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/AbstractMatchFullTextFunction.java
@@ -8,8 +8,6 @@
 package org.elasticsearch.xpack.esql.expression.function.fulltext;
 
 import org.apache.lucene.util.BytesRef;
-import org.elasticsearch.TransportVersions;
-import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.xpack.esql.capabilities.PostOptimizationVerificationAware;
 import org.elasticsearch.xpack.esql.common.Failure;
@@ -28,7 +26,6 @@ import org.elasticsearch.xpack.esql.planner.TranslatorHandler;
 import org.elasticsearch.xpack.esql.querydsl.query.MatchQuery;
 import org.elasticsearch.xpack.esql.type.EsqlDataTypeConverter;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -99,16 +96,6 @@ public abstract class AbstractMatchFullTextFunction extends FullTextFunction imp
 
     public Expression field() {
         return field;
-    }
-
-    @Override
-    public void writeTo(StreamOutput out) throws IOException {
-        source().writeTo(out);
-        out.writeNamedWriteable(field());
-        out.writeNamedWriteable(query());
-        if (out.getTransportVersion().onOrAfter(TransportVersions.ESQL_QUERY_BUILDER_IN_SEARCH_FUNCTIONS)) {
-            out.writeOptionalNamedWriteable(queryBuilder());
-        }
     }
 
     @Override

x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/Match.java
@@ -11,6 +11,7 @@ import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.TransportVersions;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.xpack.esql.core.InvalidArgumentException;
@@ -218,6 +219,16 @@ public class Match extends AbstractMatchFullTextFunction implements OptionalArgu
             queryBuilder = in.readOptionalNamedWriteable(QueryBuilder.class);
         }
         return new Match(source, field, query, null, queryBuilder);
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        source().writeTo(out);
+        out.writeNamedWriteable(field());
+        out.writeNamedWriteable(query());
+        if (out.getTransportVersion().onOrAfter(TransportVersions.ESQL_QUERY_BUILDER_IN_SEARCH_FUNCTIONS)) {
+            out.writeOptionalNamedWriteable(queryBuilder());
+        }
     }
 
     @Override

x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/MatchOperator.java
@@ -10,6 +10,7 @@ package org.elasticsearch.xpack.esql.expression.function.fulltext;
 import org.elasticsearch.TransportVersions;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
 import org.elasticsearch.xpack.esql.core.tree.NodeInfo;
@@ -74,10 +75,25 @@ public class MatchOperator extends AbstractMatchFullTextFunction {
         Expression field = in.readNamedWriteable(Expression.class);
         Expression query = in.readNamedWriteable(Expression.class);
         QueryBuilder queryBuilder = null;
-        if (in.getTransportVersion().onOrAfter(TransportVersions.ESQL_QUERY_BUILDER_IN_SEARCH_FUNCTIONS)) {
+        if (in.getTransportVersion().onOrAfter(TransportVersions.ESQL_FIX_SERIALIZATION_MATCH_OPERATOR)) {
             queryBuilder = in.readOptionalNamedWriteable(QueryBuilder.class);
         }
         return new MatchOperator(source, field, query, queryBuilder);
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        source().writeTo(out);
+        out.writeNamedWriteable(field());
+        out.writeNamedWriteable(query());
+        if (out.getTransportVersion().onOrAfter(TransportVersions.ESQL_FIX_SERIALIZATION_MATCH_OPERATOR)) {
+            out.writeOptionalNamedWriteable(queryBuilder());
+        }
+    }
+
+    @Override
+    public String getWriteableName() {
+        return ENTRY.name;
     }
 
     @Override
@@ -88,11 +104,6 @@ public class MatchOperator extends AbstractMatchFullTextFunction {
     @Override
     public String functionName() {
         return ":";
-    }
-
-    @Override
-    public String getWriteableName() {
-        return ENTRY.name;
     }
 
     @Override
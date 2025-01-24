b874f0e586e8b3b26a3e85836ad7e529b8d9784f
Slobodan Adamovic <slobodan.adamovic@elastic.co>
2025-01-24 21:15:28 +0100

    adjust deprecation message

--- server/src/main/java/org/elasticsearch/transport/RemoteConnectionManager.java
+++ server/src/main/java/org/elasticsearch/transport/RemoteConnectionManager.java
@@ -115,9 +115,9 @@ public class RemoteConnectionManager implements ConnectionManager {
 
     private InternalRemoteConnection maybeLogDeprecationWarning(InternalRemoteConnection connection) {
         if (connection.getClusterCredentials() == null) {
-            deprecationLogger.critical(
+            deprecationLogger.warn(
                 DeprecationCategory.SECURITY,
-                "remote_cluster_access_without_credentials-" + connection.getClusterAlias(),
+                "remote_cluster_certificate_access-" + connection.getClusterAlias(),
                 "The remote cluster connection to [{}] is using deprecated certificated based security model. "
                     + "The certificate based security model is deprecated and will be removed in a future major version. "
                     + "Migrate remote cluster from certificate to API key based security model.",

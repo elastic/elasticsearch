a8d6463f950bc47ab0547171986d080900c02fe1
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-01-27 23:14:43 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/index/mapper/IgnoredSourceFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/IgnoredSourceFieldMapper.java
@@ -25,8 +25,6 @@ import org.elasticsearch.xcontent.XContentType;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;

44fbcede15bc3689b6682c2cf77353b44f1e1496
Moritz Mack <mmack@apache.org>
2025-01-28 09:28:51 +0100

    Explicitly disable entitlements in tests for now

--- .buildkite/scripts/windows-run-gradle.sh
+++ .buildkite/scripts/windows-run-gradle.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-.ci/scripts/run-gradle.sh -Dbwc.checkout.align=true $GRADLE_TASK
+.ci/scripts/run-gradle.sh -Dbwc.checkout.align=true -Dtests.jvm.argline="-Des.entitlements.enabled=false" $GRADLE_TASK

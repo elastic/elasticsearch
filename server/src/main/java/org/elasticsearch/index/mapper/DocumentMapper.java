e6a7ed6d8252c95843d91009791dd505ad9aa790
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2025-01-28 10:29:25 -0800

    Fix check for sort fields being in nested objects

--- server/src/main/java/org/elasticsearch/index/mapper/DocumentMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/DocumentMapper.java
@@ -164,12 +164,11 @@ public class DocumentMapper {
                 throw new IllegalArgumentException("cannot have nested fields when index sort is activated");
             }
             for (String field : settings.getValue(IndexSortConfig.INDEX_SORT_FIELD_SETTING)) {
-                for (NestedObjectMapper nestedObjectMapper : mappers().nestedLookup().getNestedMappers().values()) {
-                    if (field.startsWith(nestedObjectMapper.fullPath())) {
-                        throw new IllegalArgumentException(
-                            "cannot apply index sort to field [" + field + "] under nested object [" + nestedObjectMapper.fullPath() + "]"
-                        );
-                    }
+                String nestedParent = mappers().nestedLookup().getNestedParent(field);
+                if (nestedParent != null) {
+                    throw new IllegalArgumentException(
+                        "cannot apply index sort to field [" + field + "] under nested object [" + nestedParent + "]"
+                    );
                 }
             }
         }

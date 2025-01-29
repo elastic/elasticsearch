2134d8317faadda97fc740e9b3c4fddb3da0b1b7
Iván Cea Fontenla <ivan.cea@elastic.co>
2025-01-29 15:40:35 +0100

    First warnings draft, randomly failing on CSV tests with missing warnings

--- x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/operator/AsyncOperator.java
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/operator/AsyncOperator.java
@@ -16,7 +16,9 @@ import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.logging.HeaderWarning;
 import org.elasticsearch.common.util.concurrent.ConcurrentCollections;
+import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.compute.data.Page;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.seqno.LocalCheckpointTracker;
@@ -26,6 +28,7 @@ import org.elasticsearch.xcontent.XContentBuilder;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.atomic.LongAdder;
 
 /**
@@ -41,6 +44,7 @@ public abstract class AsyncOperator<Fetched> implements Operator {
     private volatile SubscribableListener<Void> blockedFuture;
 
     private final Map<Long, Fetched> buffers = ConcurrentCollections.newConcurrentMap();
+    private final Set<String> storedWarningHeaders = ConcurrentCollections.newConcurrentSet();
     private final FailureCollector failureCollector = new FailureCollector();
     private final DriverContext driverContext;
 
@@ -89,9 +93,11 @@ public abstract class AsyncOperator<Fetched> implements Operator {
         try {
             final ActionListener<Fetched> listener = ActionListener.wrap(output -> {
                 buffers.put(seqNo, output);
+                storeWarnings();
                 onSeqNoCompleted(seqNo);
             }, e -> {
                 releasePageOnAnyThread(input);
+                storeWarnings();
                 failureCollector.unwrapAndCollect(e);
                 onSeqNoCompleted(seqNo);
             });
@@ -197,6 +203,7 @@ public abstract class AsyncOperator<Fetched> implements Operator {
     public final Fetched fetchFromBuffer() {
         checkFailure();
         long persistedCheckpoint = checkpoint.getPersistedCheckpoint();
+        restoreWarnings();
         if (persistedCheckpoint < checkpoint.getProcessedCheckpoint()) {
             persistedCheckpoint++;
             Fetched result = buffers.remove(persistedCheckpoint);
@@ -205,6 +212,18 @@ public abstract class AsyncOperator<Fetched> implements Operator {
         } else {
             return null;
         }
+    }
+
+    private void storeWarnings() {
+        Set<String> warnings = HeaderWarning.getWarnings();
+        storedWarningHeaders.addAll(warnings);
+    }
+
+    private void restoreWarnings() {
+        for (String warning : storedWarningHeaders) {
+            HeaderWarning.addRawWarning(warning);
+        }
+        // TODO: Remove warnings?
     }
 
     @Override

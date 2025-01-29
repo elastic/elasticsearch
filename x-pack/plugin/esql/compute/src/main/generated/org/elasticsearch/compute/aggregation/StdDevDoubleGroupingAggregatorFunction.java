de59c51b6dd80938f472186b0c85ce7047ac9185
Ievgen Degtiarenko <ievgen.degtiarenko@elastic.co>
2025-01-29 15:03:09 +0100

    Link generated classes to sources (#121038) (#121167)
    (cherry picked from commit 348a0bc04efded1770df5ec968ab0e1acf58d14c)

--- x-pack/plugin/esql/compute/src/main/generated/org/elasticsearch/compute/aggregation/StdDevDoubleGroupingAggregatorFunction.java
+++ x-pack/plugin/esql/compute/src/main/generated/org/elasticsearch/compute/aggregation/StdDevDoubleGroupingAggregatorFunction.java
@@ -22,7 +22,7 @@ import org.elasticsearch.compute.operator.DriverContext;
 
 /**
  * {@link GroupingAggregatorFunction} implementation for {@link StdDevDoubleAggregator}.
- * This class is generated. Do not edit it.
+ * This class is generated. Edit {@code GroupingAggregatorImplementer} instead.
  */
 public final class StdDevDoubleGroupingAggregatorFunction implements GroupingAggregatorFunction {
   private static final List<IntermediateStateDesc> INTERMEDIATE_STATE_DESC = List.of(

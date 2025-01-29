elastic/elasticsearch

commit a0f00f23b49c65d0a39edb28c65efaf29554fb32

Relax translog assertion for inference fields (#121092)
The inference fields are synthesized from doc_values, regardless of 
whether the synthetic source is enabled. This change relaxes the
translog assertions, allowing inference fields in the same way as we did
with the synthetic source.

Relates #120045

Closes #120979
Closes #121007
Closes #120980
Closes #120975

server/src/main/java/org/elasticsearch/index/engine/TranslogDirectoryReader.java
@@ -97,7 +97,7 @@ final class TranslogDirectoryReader extends DirectoryReader {
             // When using synthetic source, the translog operation must always be reindexed into an in-memory Lucene to ensure consistent
             // output for realtime-get operations. However, this can degrade the performance of realtime-get and update operations.
             // If slight inconsistencies in realtime-get operations are acceptable, the translog operation can be reindexed lazily.
-            if (mappingLookup.isSourceSynthetic()) {
+            if (mappingLookup.isSourceSynthetic() || mappingLookup.inferenceFields().isEmpty() == false) {
                 onSegmentCreated.run();
                 leafReader = createInMemoryReader(shardId, engineConfig, directory, documentParser, mappingLookup, false, operation);
             } else {

server/src/main/java/org/elasticsearch/index/engine/TranslogOperationAsserter.java
@@ -38,7 +38,8 @@ public abstract class TranslogOperationAsserter {
                 if (super.assertSameIndexOperation(o1, o2)) {
                     return true;
                 }
-                if (engineConfig.getIndexSettings().isRecoverySourceSyntheticEnabled()) {
+                if (engineConfig.getIndexSettings().isRecoverySourceSyntheticEnabled()
+                    || engineConfig.getMapperService().mappingLookup().inferenceFields().isEmpty() == false) {
                     return super.assertSameIndexOperation(synthesizeSource(engineConfig, o1), o2)
                         || super.assertSameIndexOperation(o1, synthesizeSource(engineConfig, o2));
                 }
@@ -60,23 +61,39 @@ public abstract class TranslogOperationAsserter {
                 TrivialQueryCachingPolicy.NEVER,
                 () -> {}
             );
-            try (
-                LuceneSyntheticSourceChangesSnapshot snapshot = new LuceneSyntheticSourceChangesSnapshot(
-                    engineConfig.getMapperService(),
-                    searcher,
-                    LuceneSyntheticSourceChangesSnapshot.DEFAULT_BATCH_SIZE,
-                    Integer.MAX_VALUE,
-                    op.seqNo(),
-                    op.seqNo(),
-                    true,
-                    false,
-                    engineConfig.getIndexSettings().getIndexVersionCreated()
-                )
-            ) {
+            try (var snapshot = newSnapshot(engineConfig, op, searcher);) {
                 final Translog.Operation normalized = snapshot.next();
                 assert normalized != null : "expected one operation; got zero";
                 return (Translog.Index) normalized;
             }
+        }
+    }
+
+    static Translog.Snapshot newSnapshot(EngineConfig engineConfig, Translog.Index op, Engine.Searcher searcher) throws IOException {
+        if (engineConfig.getIndexSettings().isRecoverySourceSyntheticEnabled()) {
+            return new LuceneSyntheticSourceChangesSnapshot(
+                engineConfig.getMapperService(),
+                searcher,
+                LuceneSyntheticSourceChangesSnapshot.DEFAULT_BATCH_SIZE,
+                Integer.MAX_VALUE,
+                op.seqNo(),
+                op.seqNo(),
+                true,
+                false,
+                engineConfig.getIndexSettings().getIndexVersionCreated()
+            );
+        } else {
+            return new LuceneChangesSnapshot(
+                engineConfig.getMapperService(),
+                searcher,
+                LuceneSyntheticSourceChangesSnapshot.DEFAULT_BATCH_SIZE,
+                op.seqNo(),
+                op.seqNo(),
+                true,
+                false,
+                false,
+                engineConfig.getIndexSettings().getIndexVersionCreated()
+            );
         }
     }
 

muted-tests.yml
@@ -255,11 +255,6 @@ tests:
 - class: org.elasticsearch.packaging.test.DockerTests
   method: test070BindMountCustomPathConfAndJvmOptions
   issue: https://github.com/elastic/elasticsearch/issues/120910
-- class: org.elasticsearch.xpack.esql.qa.multi_node.RestEsqlIT
-  issue: https://github.com/elastic/elasticsearch/issues/120948
-- class: org.elasticsearch.backwards.MixedClusterClientYamlTestSuiteIT
-  method: test {p0=nodes.stats/11_indices_metrics/indices mappings exact count test for indices level}
-  issue: https://github.com/elastic/elasticsearch/issues/120950
 - class: org.elasticsearch.packaging.test.DockerTests
   method: test071BindMountCustomPathWithDifferentUID
   issue: https://github.com/elastic/elasticsearch/issues/120918
@@ -283,9 +278,6 @@ tests:
 - class: org.elasticsearch.xpack.security.profile.ProfileIntegTests
   method: testSuggestProfilesWithName
   issue: https://github.com/elastic/elasticsearch/issues/121022
-- class: org.elasticsearch.xpack.inference.action.filter.ShardBulkInferenceActionFilterIT
-  method: testBulkOperations {p0=true}
-  issue: https://github.com/elastic/elasticsearch/issues/120969
 - class: org.elasticsearch.xpack.security.profile.ProfileIntegTests
   method: testProfileAPIsWhenIndexNotCreated
   issue: https://github.com/elastic/elasticsearch/issues/121096

commit ff7b091917c1f63982d056d18e9bc80d389b6638

Isolate Real-Time Get from Translog Assertion Logic
This commit refactors the logic introduced in #121092, ensuring that translog assertion logic related to inference fields resides exclusively within `TranslogOperationAsserter`.

For mappings that contain inference fields, we regenerate the metadata field's content during peer recovery.
This can create discrepancies between the original and regenerated sources, which is expected since indexed values may differ from the original.
This change ensures that we compare the "synthetic" version of index operations in the same way we do for mappings with synthetic source enabled.
In contrast, real-time get (`realtime-get`) simply filters inference fields from the original source, eliminating the need to synthesize them as we do for synthetic sources.

server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -825,7 +825,8 @@ public class InternalEngine extends Engine {
             mappingLookup,
             documentParser,
             config(),
-            translogInMemorySegmentsCount::incrementAndGet
+            translogInMemorySegmentsCount::incrementAndGet,
+            false
         );
         final Searcher searcher = new Searcher(
             "realtime_get",

server/src/main/java/org/elasticsearch/index/engine/TranslogDirectoryReader.java
@@ -88,7 +88,8 @@ final class TranslogDirectoryReader extends DirectoryReader {
         MappingLookup mappingLookup,
         DocumentParser documentParser,
         EngineConfig engineConfig,
-        Runnable onSegmentCreated
+        Runnable onSegmentCreated,
+        boolean forceSynthetic
     ) throws IOException {
         final Directory directory = new ByteBuffersDirectory();
         boolean success = false;
@@ -97,7 +98,7 @@ final class TranslogDirectoryReader extends DirectoryReader {
             // When using synthetic source, the translog operation must always be reindexed into an in-memory Lucene to ensure consistent
             // output for realtime-get operations. However, this can degrade the performance of realtime-get and update operations.
             // If slight inconsistencies in realtime-get operations are acceptable, the translog operation can be reindexed lazily.
-            if (mappingLookup.isSourceSynthetic() || mappingLookup.inferenceFields().isEmpty() == false) {
+            if (mappingLookup.isSourceSynthetic() || forceSynthetic) {
                 onSegmentCreated.run();
                 leafReader = createInMemoryReader(shardId, engineConfig, directory, documentParser, mappingLookup, false, operation);
             } else {

server/src/main/java/org/elasticsearch/index/engine/TranslogOperationAsserter.java
@@ -39,7 +39,7 @@ public abstract class TranslogOperationAsserter {
                     return true;
                 }
                 if (engineConfig.getIndexSettings().isRecoverySourceSyntheticEnabled()
-                    || engineConfig.getMapperService().mappingLookup().inferenceFields().isEmpty() == false) {
+                    || engineConfig.getMapperService().mappingLookup().inferenceFields().isEmpty()) {
                     return super.assertSameIndexOperation(synthesizeSource(engineConfig, o1), o2)
                         || super.assertSameIndexOperation(o1, synthesizeSource(engineConfig, o2));
                 }
@@ -52,7 +52,7 @@ public abstract class TranslogOperationAsserter {
         final ShardId shardId = engineConfig.getShardId();
         final MappingLookup mappingLookup = engineConfig.getMapperService().mappingLookup();
         final DocumentParser documentParser = engineConfig.getMapperService().documentParser();
-        try (var reader = TranslogDirectoryReader.create(shardId, op, mappingLookup, documentParser, engineConfig, () -> {})) {
+        try (var reader = TranslogDirectoryReader.create(shardId, op, mappingLookup, documentParser, engineConfig, () -> {}, true)) {
             final Engine.Searcher searcher = new Engine.Searcher(
                 "assert_translog",
                 reader,

x-pack/plugin/inference/src/internalClusterTest/java/org/elasticsearch/xpack/inference/action/filter/ShardBulkInferenceActionFilterIT.java
@@ -20,8 +20,9 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequestBuilder;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.index.IndexVersion;
+import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.mapper.InferenceMetadataFieldsMapper;
+import org.elasticsearch.index.mapper.SourceFieldMapper;
 import org.elasticsearch.index.mapper.vectors.DenseVectorFieldMapper;
 import org.elasticsearch.inference.SimilarityMeasure;
 import org.elasticsearch.plugins.Plugin;
@@ -49,14 +50,21 @@ public class ShardBulkInferenceActionFilterIT extends ESIntegTestCase {
     public static final String INDEX_NAME = "test-index";
 
     private final boolean useLegacyFormat;
+    private final boolean useSyntheticSource;
 
-    public ShardBulkInferenceActionFilterIT(boolean useLegacyFormat) {
+    public ShardBulkInferenceActionFilterIT(boolean useLegacyFormat, boolean useSyntheticSource) {
         this.useLegacyFormat = useLegacyFormat;
+        this.useSyntheticSource = useSyntheticSource;
     }
 
     @ParametersFactory
     public static Iterable<Object[]> parameters() throws Exception {
-        return List.of(new Object[] { true }, new Object[] { false });
+        return List.of(
+            new Object[] { true, false },
+            new Object[] { true, true },
+            new Object[] { false, false },
+            new Object[] { false, true }
+        );
     }
 
     @Before
@@ -79,37 +87,38 @@ public class ShardBulkInferenceActionFilterIT extends ESIntegTestCase {
 
     @Override
     public Settings indexSettings() {
-        return Settings.builder()
-            .put(IndexMetadata.SETTING_VERSION_CREATED, IndexVersion.current())
+        var builder = Settings.builder()
             .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, randomIntBetween(1, 10))
-            .put(InferenceMetadataFieldsMapper.USE_LEGACY_SEMANTIC_TEXT_FORMAT.getKey(), useLegacyFormat)
-            .build();
+            .put(InferenceMetadataFieldsMapper.USE_LEGACY_SEMANTIC_TEXT_FORMAT.getKey(), useLegacyFormat);
+        if (useSyntheticSource) {
+            builder.put(IndexSettings.RECOVERY_USE_SYNTHETIC_SOURCE_SETTING.getKey(), true);
+            builder.put(IndexSettings.INDEX_MAPPER_SOURCE_MODE_SETTING.getKey(), SourceFieldMapper.Mode.SYNTHETIC.name());
+        }
+        return builder.build();
     }
 
     public void testBulkOperations() throws Exception {
-        indicesAdmin().prepareCreate(INDEX_NAME)
-            .setMapping(
-                String.format(
-                    Locale.ROOT,
-                    """
-                        {
-                            "properties": {
-                                "sparse_field": {
-                                    "type": "semantic_text",
-                                    "inference_id": "%s"
-                                },
-                                "dense_field": {
-                                    "type": "semantic_text",
-                                    "inference_id": "%s"
-                                }
+        prepareCreate(INDEX_NAME).setMapping(
+            String.format(
+                Locale.ROOT,
+                """
+                    {
+                        "properties": {
+                            "sparse_field": {
+                                "type": "semantic_text",
+                                "inference_id": "%s"
+                            },
+                            "dense_field": {
+                                "type": "semantic_text",
+                                "inference_id": "%s"
                             }
                         }
-                        """,
-                    TestSparseInferenceServiceExtension.TestInferenceService.NAME,
-                    TestDenseInferenceServiceExtension.TestInferenceService.NAME
-                )
+                    }
+                    """,
+                TestSparseInferenceServiceExtension.TestInferenceService.NAME,
+                TestDenseInferenceServiceExtension.TestInferenceService.NAME
             )
-            .get();
+        ).get();
 
         int totalBulkReqs = randomIntBetween(2, 100);
         long totalDocs = 0;
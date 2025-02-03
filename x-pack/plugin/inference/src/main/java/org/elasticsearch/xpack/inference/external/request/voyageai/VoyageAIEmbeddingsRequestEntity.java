feb1ebd6c383fe10a747064ab45fb1261800920b
fzowl <zoltan@voyageai.com>
2025-02-03 22:27:09 +0100

    VoyageAI embeddings and rerank:  - embeddings works, tested  - rerank works, tested (https://www.elastic.co/search-labs/blog/elasticsearch-cohere-rerank)
    What's missing:
     - unit and integration tests

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/request/voyageai/VoyageAIEmbeddingsRequestEntity.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/request/voyageai/VoyageAIEmbeddingsRequestEntity.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
-import static org.elasticsearch.xpack.inference.services.cohere.embeddings.CohereEmbeddingsTaskSettings.invalidInputTypeMessage;
+import static org.elasticsearch.xpack.inference.services.voyageai.embeddings.VoyageAIEmbeddingsTaskSettings.invalidInputTypeMessage;
 
 public record VoyageAIEmbeddingsRequestEntity(
     List<String> input,

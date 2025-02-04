050d5b2ed071b00e5cfeb6d3443bb36c072e939b
fzowl <zoltan@voyageai.com>
2025-02-04 18:54:12 +0100

    Adding initial tests Moving dimensions to ServiceSettings

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/voyageai/embeddings/VoyageAIEmbeddingsTaskSettings.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/voyageai/embeddings/VoyageAIEmbeddingsTaskSettings.java
@@ -41,7 +41,7 @@ import static org.elasticsearch.xpack.inference.services.voyageai.VoyageAIServic
 public class VoyageAIEmbeddingsTaskSettings implements TaskSettings {
 
     public static final String NAME = "voyageai_embeddings_task_settings";
-    public static final VoyageAIEmbeddingsTaskSettings EMPTY_SETTINGS = new VoyageAIEmbeddingsTaskSettings(null, null, null);
+    public static final VoyageAIEmbeddingsTaskSettings EMPTY_SETTINGS = new VoyageAIEmbeddingsTaskSettings(null, null);
     static final String INPUT_TYPE = "input_type";
     static final EnumSet<InputType> VALID_REQUEST_VALUES = EnumSet.of(
         InputType.INGEST,
@@ -79,7 +79,7 @@ public class VoyageAIEmbeddingsTaskSettings implements TaskSettings {
             throw validationException;
         }
 
-        return new VoyageAIEmbeddingsTaskSettings(inputType, truncation, outputDimension);
+        return new VoyageAIEmbeddingsTaskSettings(inputType, truncation);
     }
 
     /**
@@ -101,9 +101,8 @@ public class VoyageAIEmbeddingsTaskSettings implements TaskSettings {
     ) {
         var inputTypeToUse = getValidInputType(originalSettings, requestTaskSettings, requestInputType);
         var truncationToUse = getValidTruncation(originalSettings, requestTaskSettings);
-        var outputDimension = getValidOutputDimension(originalSettings, requestTaskSettings);
 
-        return new VoyageAIEmbeddingsTaskSettings(inputTypeToUse, truncationToUse, outputDimension);
+        return new VoyageAIEmbeddingsTaskSettings(inputTypeToUse, truncationToUse);
     }
 
     private static InputType getValidInputType(
@@ -129,36 +128,23 @@ public class VoyageAIEmbeddingsTaskSettings implements TaskSettings {
         return requestTaskSettings.getTruncation() == null ? originalSettings.truncation : requestTaskSettings.getTruncation();
     }
 
-    private static Integer getValidOutputDimension(
-        VoyageAIEmbeddingsTaskSettings originalSettings,
-        VoyageAIEmbeddingsTaskSettings requestTaskSettings
-    ) {
-        return requestTaskSettings.getOutputDimension() == null
-            ? originalSettings.outputDimension
-            : requestTaskSettings.getOutputDimension();
-    }
-
     private final InputType inputType;
     private final Boolean truncation;
-    private final Integer outputDimension;
 
     public VoyageAIEmbeddingsTaskSettings(StreamInput in) throws IOException {
         this(
             in.readOptionalEnum(InputType.class),
-            in.readOptionalBoolean(),
-            in.readOptionalInt()
+            in.readOptionalBoolean()
         );
     }
 
     public VoyageAIEmbeddingsTaskSettings(
         @Nullable InputType inputType,
-        @Nullable Boolean truncation,
-        @Nullable Integer outputDimension
+        @Nullable Boolean truncation
     ) {
         validateInputType(inputType);
         this.inputType = inputType;
         this.truncation = truncation;
-        this.outputDimension = outputDimension;
     }
 
     private static void validateInputType(InputType inputType) {
@@ -171,7 +157,7 @@ public class VoyageAIEmbeddingsTaskSettings implements TaskSettings {
 
     @Override
     public boolean isEmpty() {
-        return inputType == null && truncation == null && outputDimension == null;
+        return inputType == null && truncation == null;
     }
 
     @Override
@@ -185,10 +171,6 @@ public class VoyageAIEmbeddingsTaskSettings implements TaskSettings {
             builder.field(TRUNCATION, truncation);
         }
 
-        if (outputDimension != null) {
-            builder.field(OUTPUT_DIMENSION, outputDimension);
-        }
-
         builder.endObject();
         return builder;
     }
@@ -199,10 +181,6 @@ public class VoyageAIEmbeddingsTaskSettings implements TaskSettings {
 
     public Boolean getTruncation() {
         return truncation;
-    }
-
-    public Integer getOutputDimension() {
-        return outputDimension;
     }
 
     @Override
@@ -219,7 +197,6 @@ public class VoyageAIEmbeddingsTaskSettings implements TaskSettings {
     public void writeTo(StreamOutput out) throws IOException {
         out.writeOptionalEnum(inputType);
         out.writeOptionalBoolean(truncation);
-        out.writeOptionalInt(outputDimension);
     }
 
     @Override
@@ -228,13 +205,12 @@ public class VoyageAIEmbeddingsTaskSettings implements TaskSettings {
         if (o == null || getClass() != o.getClass()) return false;
         VoyageAIEmbeddingsTaskSettings that = (VoyageAIEmbeddingsTaskSettings) o;
         return Objects.equals(inputType, that.inputType) &&
-            Objects.equals(truncation, that.truncation) &&
-            Objects.equals(outputDimension, that.outputDimension);
+            Objects.equals(truncation, that.truncation);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(inputType, truncation, outputDimension);
+        return Objects.hash(inputType, truncation);
     }
 
     public static String invalidInputTypeMessage(InputType inputType) {

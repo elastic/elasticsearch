e9339a6b66eb097494d7fa5483d3a26a05c5528d
Ying <ying.mao@elastic.co>
2025-02-05 16:42:30 -0500

    wip

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/mapper/SemanticTextFieldMapper.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/mapper/SemanticTextFieldMapper.java
@@ -709,8 +709,6 @@ public class SemanticTextFieldMapper extends FieldMapper implements InferenceFie
                         MlTextEmbeddingResults textEmbeddingResults = (MlTextEmbeddingResults) inferenceResults;
                         float[] inference = textEmbeddingResults.getInferenceAsFloat();
                         var inferenceLength = modelSettings.elementType() == DenseVectorFieldMapper.ElementType.BIT ? inference.length * Byte.SIZE : inference.length;
-                        System.out.println("AAA inference.length " + inference.length);
-                        System.out.println("AAA inferenceLength " + inferenceLength);
                         if (inferenceLength != modelSettings.dimensions()) {
                             throw new IllegalArgumentException(
                                 generateDimensionCountMismatchMessage(inference.length, modelSettings.dimensions())

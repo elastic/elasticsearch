9ca756a98eaf4777dbbeb0f85a16834ce7ed4949
carlosdelest <carlos.delgado@elastic.co>
2025-02-05 17:29:03 +0100

    Add a ExpressionScoreMapper and a ScoreMapper interface to retrieve scorers for expressions

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/score/ScoreMapper.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/score/ScoreMapper.java
@@ -7,24 +7,47 @@
 
 package org.elasticsearch.xpack.esql.score;
 
-import org.elasticsearch.compute.operator.EvalOperator.ExpressionEvaluator;
-import org.elasticsearch.compute.operator.ScoreOperator.ExpressionScorer;
+import org.elasticsearch.compute.data.DoubleBlock;
+import org.elasticsearch.compute.data.Page;
+import org.elasticsearch.compute.operator.DriverContext;
+import org.elasticsearch.compute.operator.ScoreOperator;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
-import org.elasticsearch.xpack.esql.planner.EsPhysicalOperationProviders;
+import org.elasticsearch.xpack.esql.planner.EsPhysicalOperationProviders.ShardContext;
 
 import java.util.List;
 
-/**
- * Expressions that have a mapping to an {@link ExpressionEvaluator}.
- */
-public interface ScoreMapper {
-    interface ToScorer {
-        ExpressionScorer.Factory apply(Expression expression);
+public class ScoreMapper {
 
-        default List<EsPhysicalOperationProviders.ShardContext> shardContexts() {
-            throw new UnsupportedOperationException("Shard contexts should only be needed for scoring operations");
+    public static ScoreOperator.ExpressionScorer.Factory toScorer(Expression expression, List<ShardContext> shardContexts) {
+        if (expression instanceof ExpressionScoreMapper mapper) {
+            return mapper.toScorer(new ExpressionScoreMapper.ToScorer() {
+                @Override
+                public ScoreOperator.ExpressionScorer.Factory toScorer(Expression expression) {
+                    return ScoreMapper.toScorer(expression, shardContexts);
+                }
+
+                @Override
+                public List<ShardContext> shardContexts() {
+                    return shardContexts;
+                }
+            });
         }
+
+        return page -> new DefaultScoreMapper().get(page);
     }
 
-    ExpressionScorer.Factory toScorer(ToScorer toScorer);
+    public static class DefaultScoreMapper implements ScoreOperator.ExpressionScorer.Factory {
+        @Override
+        public ScoreOperator.ExpressionScorer get(DriverContext driverContext) {
+            return new ScoreOperator.ExpressionScorer() {
+                @Override
+                public DoubleBlock score(Page page) {
+                    return driverContext.blockFactory().newConstantDoubleBlockWith(0.0, page.getPositionCount());
+                }
+
+                @Override
+                public void close() {}
+            };
+        }
+    }
 }

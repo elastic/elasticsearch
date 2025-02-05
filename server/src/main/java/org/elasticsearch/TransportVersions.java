c28b54de7b7269ddea32fa36b997f9484602f411
Artem Prigoda <artem.prigoda@elastic.co>
2025-02-05 18:59:51 +0100

    [9.0] Don't return or accept `node_version` in the Desired Nodes API  (#119049) (#121775)
    Backports #119049 to 9.0
    
    >  Re-submission of #114580
    >  node_version was deprecated in #104209 (8.13) and shouldn't be set or returned in 9.0

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -175,6 +175,7 @@ public class TransportVersions {
     public static final TransportVersion ELASTICSEARCH_9_0 = def(9_000_0_00);
     public static final TransportVersion REMOVE_SNAPSHOT_FAILURES = def(9_002_0_00);
     public static final TransportVersion TRANSPORT_STATS_HANDLING_TIME_REQUIRED = def(9_003_0_00);
+    public static final TransportVersion REMOVE_DESIRED_NODE_VERSION = def(9_004_0_00);
 
     /*
      * STOP! READ THIS FIRST! No, really,

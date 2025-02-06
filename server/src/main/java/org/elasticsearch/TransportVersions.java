8bca2d0a25550c093f662a6e285b1e4d7a0d5493
Ryan Ernst <ryan@iernst.net>
2025-02-06 15:40:57 -0800

    Add 9.0 patch transport version constants #121985
    Transport version changes must be unique per branch. Some transport
    version changes meant for 9.0 are missing unique backport constants.
    This is a backport of #121985, adding unique transport version patch
    numbers for each change intended for 9.0.

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -173,10 +173,11 @@ public class TransportVersions {
     public static final TransportVersion INFERENCE_REQUEST_ADAPTIVE_RATE_LIMITING = def(8_839_0_00);
     public static final TransportVersion ML_INFERENCE_IBM_WATSONX_RERANK_ADDED = def(8_840_0_00);
     public static final TransportVersion ELASTICSEARCH_9_0 = def(9_000_0_00);
-    public static final TransportVersion REMOVE_SNAPSHOT_FAILURES = def(9_002_0_00);
-    public static final TransportVersion TRANSPORT_STATS_HANDLING_TIME_REQUIRED = def(9_003_0_00);
-    public static final TransportVersion REMOVE_DESIRED_NODE_VERSION = def(9_004_0_00);
-    public static final TransportVersion ESQL_DRIVER_TASK_DESCRIPTION = def(9_005_0_00);
+    public static final TransportVersion REMOVE_SNAPSHOT_FAILURES = def(9_000_0_01);
+    public static final TransportVersion TRANSPORT_STATS_HANDLING_TIME_REQUIRED = def(9_000_0_02);
+    public static final TransportVersion REMOVE_DESIRED_NODE_VERSION = def(9_000_0_03);
+    public static final TransportVersion ESQL_DRIVER_TASK_DESCRIPTION = def(9_000_0_04);
+
     /*
      * STOP! READ THIS FIRST! No, really,
      *        ____ _____ ___  ____  _        ____  _____    _    ____    _____ _   _ ___ ____    _____ ___ ____  ____ _____ _

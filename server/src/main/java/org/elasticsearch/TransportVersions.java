c2115833bc01a77f14026cc5ad4d0218813b903f
fzowl <zoltan@voyageai.com>
2025-02-09 15:17:57 +0100

    Adding changelog and correcting TransportVersions

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -186,7 +186,7 @@ public class TransportVersions {
     public static final TransportVersion ESQL_RETRY_ON_SHARD_LEVEL_FAILURE = def(9_006_0_00);
     public static final TransportVersion ESQL_PROFILE_ASYNC_NANOS = def(9_007_00_0);
 
-    public static final TransportVersion VOYAGE_AI_INTEGRATION_ADDED = def(9_005_0_00);
+    public static final TransportVersion VOYAGE_AI_INTEGRATION_ADDED = def(9_008_0_00);
     /*
      * STOP! READ THIS FIRST! No, really,
      *        ____ _____ ___  ____  _        ____  _____    _    ____    _____ _   _ ___ ____    _____ ___ ____  ____ _____ _

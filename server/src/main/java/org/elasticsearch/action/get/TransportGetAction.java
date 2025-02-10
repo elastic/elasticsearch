ff0c1915b71b2fd5335296ace4035bc0150275ba
Artem Prigoda <artem.prigoda@elastic.co>
2025-02-10 15:21:28 +0100

    Correctly check index service in case index is not a different node

--- server/src/main/java/org/elasticsearch/action/get/TransportGetAction.java
+++ server/src/main/java/org/elasticsearch/action/get/TransportGetAction.java
@@ -166,7 +166,9 @@ public class TransportGetAction extends TransportSingleShardAction<GetRequest, G
         final ClusterState clusterState = clusterService.state();
         if (clusterState.metadata().getIndexSafe(shardId.getIndex()).isSystem()) {
             return threadPool.executor(executorSelector.executorForGet(shardId.getIndexName()));
-        } else if (indicesService.indexServiceSafe(shardId.getIndex()).getIndexSettings().isSearchThrottled()) {
+        }
+        var indexService = indicesService.indexService(shardId.getIndex());
+        if (indexService != null && indexService.getIndexSettings().isSearchThrottled()) {
             return threadPool.executor(ThreadPool.Names.SEARCH_THROTTLED);
         } else {
             return super.getExecutor(request, shardId);
--- server/src/main/java/org/elasticsearch/action/get/TransportShardMultiGetAction.java
+++ server/src/main/java/org/elasticsearch/action/get/TransportShardMultiGetAction.java
@@ -155,7 +155,9 @@ public class TransportShardMultiGetAction extends TransportSingleShardAction<Mul
         final ClusterState clusterState = clusterService.state();
         if (clusterState.metadata().index(shardId.getIndex()).isSystem()) {
             return threadPool.executor(executorSelector.executorForGet(shardId.getIndexName()));
-        } else if (indicesService.indexServiceSafe(shardId.getIndex()).getIndexSettings().isSearchThrottled()) {
+        }
+        var indexService = indicesService.indexService(shardId.getIndex());
+        if (indexService != null && indexService.getIndexSettings().isSearchThrottled()) {
             return threadPool.executor(ThreadPool.Names.SEARCH_THROTTLED);
         } else {
             return super.getExecutor(request, shardId);

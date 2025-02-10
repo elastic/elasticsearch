ed8c90206a9b1e57a5b65f861577350fbb5b4c45
Artem Prigoda <artem.prigoda@elastic.co>
2025-02-10 22:07:35 +0100

    Revert "Make TransportGetFromTranslogAction and TransportShardMultiGetFomTranslogAction extendable"
    This reverts commit 3237dfa2

--- server/src/main/java/org/elasticsearch/action/get/MultiGetShardRequest.java
+++ server/src/main/java/org/elasticsearch/action/get/MultiGetShardRequest.java
@@ -174,10 +174,6 @@ public class MultiGetShardRequest extends SingleShardRequest<MultiGetShardReques
         this.items.add(item);
     }
 
-    public List<Integer> locations() {
-        return locations;
-    }
-
     @Override
     public String[] indices() {
         String[] indices = new String[items.size()];

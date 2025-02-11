d4777d54f74f3a4091cdf075effc92ed7eaf7ef5
Martijn van Groningen <martijn.v.groningen@gmail.com>
2025-02-11 12:58:15 +0100

    disable assertion jvm arg work around

--- x-pack/plugin/downsample/qa/mixed-cluster/src/yamlRestTest/java/org/elasticsearch/xpack/downsample/MixedClusterDownsampleRestIT.java
+++ x-pack/plugin/downsample/qa/mixed-cluster/src/yamlRestTest/java/org/elasticsearch/xpack/downsample/MixedClusterDownsampleRestIT.java
@@ -25,6 +25,9 @@ public class MixedClusterDownsampleRestIT extends ESClientYamlSuiteTestCase {
         .withNode(node -> node.version(Version.CURRENT))
         .setting("xpack.security.enabled", "false")
         .setting("xpack.license.self_generated.type", "trial")
+        // TODO: conditionally add jvmarg based on old version
+        // Avoid tripping assertion on old nodes:
+        .jvmArg("-da:org.elasticsearch.index.mapper.DocumentMapper")
         .build();
 
     static Version getOldVersion() {
--- x-pack/plugin/esql/qa/server/mixed-cluster/src/javaRestTest/java/org/elasticsearch/xpack/esql/qa/mixed/Clusters.java
+++ x-pack/plugin/esql/qa/server/mixed-cluster/src/javaRestTest/java/org/elasticsearch/xpack/esql/qa/mixed/Clusters.java
@@ -23,6 +23,9 @@ public class Clusters {
             .setting("xpack.security.enabled", "false")
             .setting("xpack.license.self_generated.type", "trial")
             .setting("cluster.routing.rebalance.enable", "none") // disable relocation until we have retry in ESQL
+            // TODO: conditionally add jvmarg based on old version
+            // Avoid tripping assertion on old nodes:
+            .jvmArg("-da:org.elasticsearch.index.mapper.DocumentMapper")
             .build();
     }
 }

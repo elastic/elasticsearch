8c2272d975280794416f088600f458815803ed0d
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-02-12 10:22:54 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/ingest/WriteReportingSet.java
+++ server/src/main/java/org/elasticsearch/ingest/WriteReportingSet.java
@@ -11,7 +11,6 @@ package org.elasticsearch.ingest;
 
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 import java.util.Spliterator;
 import java.util.function.Consumer;

0a171edc91c44b9878aa8757795bceeede4dd2b3
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-02-13 20:08:46 +0000

    [CI] Auto commit changes from spotless

--- x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/DataStreamsUpgradeIT.java
+++ x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/DataStreamsUpgradeIT.java
@@ -217,12 +217,12 @@ public class DataStreamsUpgradeIT extends AbstractUpgradeTestCase {
                 Map<String, Object> upgradedIndexMetadata = upgradedIndexEntry.getValue();
                 compareSettings(oldIndexMetadata, upgradedIndexMetadata);
                 assertThat("Mappings did not match", upgradedIndexMetadata.get("mappings"), equalTo(oldIndexMetadata.get("mappings")));
-                 assertThat("ILM states did not match", upgradedIndexMetadata.get("ilm"), equalTo(oldIndexMetadata.get("ilm")));
-                 assertThat(
-                 "Rollover info did not match",
-                 upgradedIndexMetadata.get("rollover_info"),
-                 equalTo(oldIndexMetadata.get("rollover_info"))
-                 );
+                assertThat("ILM states did not match", upgradedIndexMetadata.get("ilm"), equalTo(oldIndexMetadata.get("ilm")));
+                assertThat(
+                    "Rollover info did not match",
+                    upgradedIndexMetadata.get("rollover_info"),
+                    equalTo(oldIndexMetadata.get("rollover_info"))
+                );
                 assertThat(upgradedIndexMetadata.get("system"), equalTo(oldIndexMetadata.get("system")));
             }
         }

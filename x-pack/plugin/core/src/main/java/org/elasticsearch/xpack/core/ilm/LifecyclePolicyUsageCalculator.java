4cd9bfbd5e77e924b0f9d0a56bf5d82a2430052b
Niels Bauman <nielsbauman@gmail.com>
2025-02-15 22:11:19 +1000

    Small refactorings

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/LifecyclePolicyUsageCalculator.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ilm/LifecyclePolicyUsageCalculator.java
@@ -17,10 +17,10 @@ import org.elasticsearch.cluster.metadata.ItemUsage;
 import org.elasticsearch.cluster.metadata.MetadataIndexTemplateService;
 import org.elasticsearch.common.regex.Regex;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.Maps;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,6 +29,9 @@ import java.util.Map;
  * use a bit more memory but speeds up the usage calculation significantly.
  */
 public class LifecyclePolicyUsageCalculator {
+
+    /** A 'not found' sentinel value for use in getOrDefault calls in order to avoid containsKey-and-then-get. */
+    private static final String NOT_FOUND = "";
 
     private final ClusterState state;
     /** Whether {@link #calculateUsage} will be called multiple times or not. */
@@ -46,26 +49,29 @@ public class LifecyclePolicyUsageCalculator {
         this.state = state;
         this.willIterate = names.size() > 1 || Regex.isSimpleMatchPattern(names.get(0));
 
-        var allDataStreams = indexNameExpressionResolver.dataStreamNames(
+        final List<String> allDataStreams = indexNameExpressionResolver.dataStreamNames(
             state,
             IndicesOptions.LENIENT_EXPAND_OPEN_CLOSED_HIDDEN_NO_SELECTOR
         );
         // Sort all templates by descending priority. That way, findV2Template can exit on the first found template.
-        var indexTemplates = new ArrayList<>(state.metadata().templatesV2().entrySet());
+        final var indexTemplates = new ArrayList<>(state.metadata().templatesV2().entrySet());
         CollectionUtil.timSort(indexTemplates, Comparator.comparing(entry -> entry.getValue().priorityOrZero(), Comparator.reverseOrder()));
 
         // Build the maps that will be used for the usage calculation later on.
-        IndexLifecycleMetadata metadata = state.metadata().custom(IndexLifecycleMetadata.TYPE);
-        policyToDataStream = new HashMap<>(Regex.isSimpleMatchPattern(names.get(0)) ? metadata.getPolicyMetadatas().size() : names.size());
-        templateToPolicy = new HashMap<>(indexTemplates.size());
+        final IndexLifecycleMetadata metadata = state.metadata().custom(IndexLifecycleMetadata.TYPE);
+        policyToDataStream = Maps.newHashMapWithExpectedSize(
+            Regex.isSimpleMatchPattern(names.get(0)) ? metadata.getPolicyMetadatas().size() : names.size()
+        );
+        templateToPolicy = Maps.newHashMapWithExpectedSize(indexTemplates.size());
         for (String dataStream : allDataStreams) {
             String indexTemplate = MetadataIndexTemplateService.findV2Template(state.metadata(), indexTemplates, dataStream, false, true);
             if (indexTemplate == null) {
+                // Every data stream should ordinarily have an index template, so this branch should not fire under normal circumstances.
                 continue;
             }
-            Settings settings = MetadataIndexTemplateService.resolveSettings(state.metadata(), indexTemplate);
-            var policyName = LifecycleSettings.LIFECYCLE_NAME_SETTING.get(settings);
-            if (names.stream().noneMatch(name -> Regex.simpleMatch(name, policyName))) {
+            final Settings settings = MetadataIndexTemplateService.resolveSettings(state.metadata(), indexTemplate);
+            final var policyName = LifecycleSettings.LIFECYCLE_NAME_SETTING.get(settings);
+            if (doesPolicyMatchAnyName(policyName, names) == false) {
                 // If a template's policy doesn't match any of the supplied names, we can skip it later on.
                 templateToPolicy.put(indexTemplate, null);
                 continue;
@@ -79,18 +85,18 @@ public class LifecyclePolicyUsageCalculator {
      * Calculate the indices, data streams, and composable templates that use the given policy.
      */
     public ItemUsage calculateUsage(String policyName) {
-        List<String> indices = new ArrayList<>();
+        final List<String> indices = new ArrayList<>();
         for (IndexMetadata indexMetadata : state.metadata().indices().values()) {
             if (policyName.equals(indexMetadata.getLifecyclePolicyName())) {
                 indices.add(indexMetadata.getIndex().getName());
             }
         }
 
-        List<String> composableTemplates = new ArrayList<>();
+        final List<String> composableTemplates = new ArrayList<>();
         for (Map.Entry<String, ComposableIndexTemplate> entry : state.metadata().templatesV2().entrySet()) {
-            var foundPolicy = templateToPolicy.get(entry.getKey());
-            // Extra `containsKey` check to account for templates not using any policy.
-            if (foundPolicy == null && templateToPolicy.containsKey(entry.getKey()) == false) {
+            var foundPolicy = templateToPolicy.getOrDefault(entry.getKey(), NOT_FOUND);
+            // Intentionally use strict equals here as we're using the sentinel value.
+            if (foundPolicy == NOT_FOUND) {
                 Settings settings = MetadataIndexTemplateService.resolveSettings(entry.getValue(), state.metadata().componentTemplates());
                 foundPolicy = LifecycleSettings.LIFECYCLE_NAME_SETTING.get(settings);
                 // If this method will only be called once, we don't need to keep building the map.
@@ -104,5 +110,14 @@ public class LifecyclePolicyUsageCalculator {
         }
 
         return new ItemUsage(indices, policyToDataStream.getOrDefault(policyName, List.of()), composableTemplates);
+    }
+
+    private boolean doesPolicyMatchAnyName(String policyName, List<String> names) {
+        for (var name : names) {
+            if (Regex.simpleMatch(name, policyName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }

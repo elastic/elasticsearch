1bc218a651635e3893b4b7176e904f2c4f1c5ac5
Artem Prigoda <artem.prigoda@elastic.co>
2025-02-16 17:51:34 +0100

    Fail primary term and generation listeners on a closed shard
    If a shard has been closed, we should quickly bail out and fail
    all waiting primary term and generation listeners. Otherwise,
    the engine implementation may try to successfully to complete the
    provided listeners and perform operations on an already closed
    shard and cause some unexpected errors.

--- server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
@@ -4494,11 +4494,13 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
      * Registers a listener for an event when the shard advances to the provided primary term and segment generation
      */
     public void waitForPrimaryTermAndGeneration(long primaryTerm, long segmentGeneration, ActionListener<Long> listener) {
-        waitForEngineOrClosedShard(
-            listener.delegateFailureAndWrap(
-                (l, ignored) -> getEngine().addPrimaryTermAndGenerationListener(primaryTerm, segmentGeneration, l)
-            )
-        );
+        waitForEngineOrClosedShard(listener.delegateFailureAndWrap((l, ignored) -> {
+            if (state == IndexShardState.CLOSED) {
+                l.onFailure(new IndexShardClosedException(shardId));
+            } else {
+                getEngine().addPrimaryTermAndGenerationListener(primaryTerm, segmentGeneration, l);
+            }
+        }));
     }
 
     /**

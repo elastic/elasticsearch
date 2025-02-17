c337e2808b46702512d2faa80aa55772cd8468c5
Artem Prigoda <artem.prigoda@elastic.co>
2025-02-17 21:05:32 +0100

    Update javadoc for IndexShard#waitForPrimaryTermAndGeneration with IndexShardClosedException

--- server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
@@ -4491,7 +4491,8 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
     }
 
     /**
-     * Registers a listener for an event when the shard advances to the provided primary term and segment generation
+     * Registers a listener for an event when the shard advances to the provided primary term and segment generation.
+     * Completes the listener with a {@link IndexShardClosedException} if the shard is closed.
      */
     public void waitForPrimaryTermAndGeneration(long primaryTerm, long segmentGeneration, ActionListener<Long> listener) {
         waitForEngineOrClosedShard(listener.delegateFailureAndWrap((l, ignored) -> {

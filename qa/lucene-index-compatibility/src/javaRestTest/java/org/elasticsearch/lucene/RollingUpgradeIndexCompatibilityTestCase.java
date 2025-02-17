ad9cdeb2fed4ab276dd288fb71783c31797f8b8a
Christoph Büscher <christophbuescher@posteo.de>
2025-02-17 19:39:37 +0100

    Lift some common test code to shared parent class§

--- qa/lucene-index-compatibility/src/javaRestTest/java/org/elasticsearch/lucene/RollingUpgradeIndexCompatibilityTestCase.java
+++ qa/lucene-index-compatibility/src/javaRestTest/java/org/elasticsearch/lucene/RollingUpgradeIndexCompatibilityTestCase.java
@@ -14,15 +14,28 @@ import com.carrotsearch.randomizedtesting.annotations.Name;
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 import com.carrotsearch.randomizedtesting.annotations.TestCaseOrdering;
 
+import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.test.cluster.util.Version;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.elasticsearch.cluster.metadata.IndexMetadata.INDEX_READ_ONLY_BLOCK;
+import static org.elasticsearch.cluster.metadata.IndexMetadata.INDEX_WRITE_BLOCK;
+import static org.elasticsearch.cluster.metadata.MetadataIndexStateService.INDEX_CLOSED_BLOCK;
+import static org.elasticsearch.cluster.metadata.MetadataIndexStateService.VERIFIED_BEFORE_CLOSE_SETTING;
+import static org.elasticsearch.cluster.metadata.MetadataIndexStateService.VERIFIED_READ_ONLY_SETTING;
 import static org.elasticsearch.test.cluster.util.Version.CURRENT;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.either;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 
 /**
@@ -84,6 +97,101 @@ public abstract class RollingUpgradeIndexCompatibilityTestCase extends AbstractI
             currentNodeVersion = nodesVersions().get(nodeName);
             assertThat(currentNodeVersion, equalTo(expectedNodeVersion));
         }
+    }
+
+    protected void addAndAssertIndexBlocks(String index, boolean maybeClose) throws Exception {
+        final var randomBlocks = randomFrom(
+            List.of(IndexMetadata.APIBlock.WRITE, IndexMetadata.APIBlock.READ_ONLY),
+            List.of(IndexMetadata.APIBlock.READ_ONLY),
+            List.of(IndexMetadata.APIBlock.WRITE)
+        );
+        for (var randomBlock : randomBlocks) {
+            addIndexBlock(index, randomBlock);
+            assertThat(indexBlocks(index), hasItem(randomBlock.getBlock()));
+        }
+
+        assertThat(indexBlocks(index), maybeClose ? hasItem(INDEX_CLOSED_BLOCK) : not(hasItem(INDEX_CLOSED_BLOCK)));
+        assertIndexSetting(index, VERIFIED_BEFORE_CLOSE_SETTING, is(maybeClose));
+        assertIndexSetting(index, VERIFIED_READ_ONLY_SETTING, is(true));
+        return;
+    }
+
+    /**
+     * assert that index has either a read-only block or write block, if index is closed also a cloded-block.
+     * In case both blocks are present, randomly modify one of them.
+     */
+    protected void assertAndModifyIndexBlocks(String index, boolean isClosed) throws Exception {
+        logger.debug("--> upgraded index [{}] is now in [{}] state", index, isClosed ? "closed" : "open");
+        assertThat(
+            indexBlocks(index),
+            allOf(
+                either(hasItem(INDEX_READ_ONLY_BLOCK)).or(hasItem(INDEX_WRITE_BLOCK)),
+                isClosed ? hasItem(INDEX_CLOSED_BLOCK) : not(hasItem(INDEX_CLOSED_BLOCK))
+            )
+        );
+        assertIndexSetting(index, VERIFIED_BEFORE_CLOSE_SETTING, is(isClosed));
+        assertIndexSetting(index, VERIFIED_READ_ONLY_SETTING, is(true));
+
+        var blocks = indexBlocks(index).stream().filter(c -> c.equals(INDEX_WRITE_BLOCK) || c.equals(INDEX_READ_ONLY_BLOCK)).toList();
+        if (blocks.size() == 2) {
+            switch (randomInt(2)) {
+                case 0:
+                    updateIndexSettings(
+                        index,
+                        Settings.builder()
+                            .putNull(IndexMetadata.APIBlock.WRITE.settingName())
+                            .put(IndexMetadata.APIBlock.READ_ONLY.settingName(), true)
+                    );
+                    assertThat(
+                        indexBlocks(index),
+                        isClosed ? contains(INDEX_CLOSED_BLOCK, INDEX_READ_ONLY_BLOCK) : contains(INDEX_READ_ONLY_BLOCK)
+                    );
+                    break;
+                case 1:
+                    updateIndexSettings(
+                        index,
+                        Settings.builder()
+                            .putNull(IndexMetadata.APIBlock.READ_ONLY.settingName())
+                            .put(IndexMetadata.APIBlock.WRITE.settingName(), true)
+                    );
+                    assertThat(
+                        indexBlocks(index),
+                        isClosed ? contains(INDEX_CLOSED_BLOCK, INDEX_WRITE_BLOCK) : contains(INDEX_WRITE_BLOCK)
+                    );
+                    break;
+                case 2:
+                    updateIndexSettings(index, Settings.builder().put(IndexMetadata.APIBlock.READ_ONLY.settingName(), false));
+                    assertThat(
+                        indexBlocks(index),
+                        isClosed ? contains(INDEX_CLOSED_BLOCK, INDEX_WRITE_BLOCK) : contains(INDEX_WRITE_BLOCK)
+                    );
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        }
+    }
+
+    /**
+     * ensure we have a write-block. If this is currently not the case, set it.
+     */
+    protected void ensureWriteBlock(String index, boolean isClosed) throws Exception {
+        List<org.elasticsearch.cluster.block.ClusterBlock> blocks = indexBlocks(index).stream()
+            .filter(c -> c.equals(INDEX_WRITE_BLOCK) || c.equals(INDEX_READ_ONLY_BLOCK))
+            .toList();
+        if (blocks.contains(INDEX_READ_ONLY_BLOCK)) {
+            logger.debug("--> read_only API block can be replaced by a write block (required for the remaining tests)");
+            updateIndexSettings(
+                index,
+                Settings.builder()
+                    .putNull(IndexMetadata.APIBlock.READ_ONLY.settingName())
+                    .put(IndexMetadata.APIBlock.WRITE.settingName(), true)
+            );
+        }
+
+        assertIndexSetting(index, VERIFIED_READ_ONLY_SETTING, is(true));
+        assertIndexSetting(index, VERIFIED_BEFORE_CLOSE_SETTING, is(isClosed));
+        assertThat(indexBlocks(index), isClosed ? contains(INDEX_CLOSED_BLOCK, INDEX_WRITE_BLOCK) : contains(INDEX_WRITE_BLOCK));
     }
 
     /**

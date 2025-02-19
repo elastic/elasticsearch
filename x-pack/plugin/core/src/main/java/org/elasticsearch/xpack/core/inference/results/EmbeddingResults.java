8a828fc2d1c64eec5057753ea916431c04b8913b
Jan Kuipers <jan.kuipers@elastic.co>
2025-02-19 10:44:45 +0100

    javadoc

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/inference/results/EmbeddingResults.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/inference/results/EmbeddingResults.java
@@ -14,10 +14,18 @@ import org.elasticsearch.xcontent.XContent;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * The results of a call to the inference service that contains embeddings (sparse or dense).
+ * A call to the inference service may contain multiple input texts, so this results may
+ * contain multiple results.
+ */
 public interface EmbeddingResults<C extends EmbeddingResults.Chunk, R extends EmbeddingResults.Embedding<C>>
     extends
         InferenceServiceResults {
 
+    /**
+     * A resulting embedding together with its input text.
+     */
     interface Chunk {
         ChunkedInference.Chunk toChunk(XContent xcontent) throws IOException;
 
@@ -26,9 +34,18 @@ public interface EmbeddingResults<C extends EmbeddingResults.Chunk, R extends Em
         ChunkedInference.TextOffset offset();
     }
 
+    /**
+     * A resulting embedding for one of the input texts to the inference service.
+     */
     interface Embedding<C extends Chunk> {
+        /**
+         * Combines the resulting embedding with the input into a chunk.
+         */
         C toChunk(String text, ChunkedInference.TextOffset offset);
     }
 
+    /**
+     * The resulting list of embeddings for the input texts to the inference service.
+     */
     List<R> embeddings();
 }

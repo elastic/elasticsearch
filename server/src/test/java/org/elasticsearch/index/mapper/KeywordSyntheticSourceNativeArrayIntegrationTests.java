ef6dc44353e2fa714625141aa13e4c9997d61384
Martijn van Groningen <martijn.v.groningen@gmail.com>
2025-02-20 13:14:57 +0100

    fix all release test failures

--- server/src/test/java/org/elasticsearch/index/mapper/KeywordSyntheticSourceNativeArrayIntegrationTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/KeywordSyntheticSourceNativeArrayIntegrationTests.java
@@ -185,7 +185,14 @@ public class KeywordSyntheticSourceNativeArrayIntegrationTests extends ESSingleN
                 var document = reader.storedFields().document(i);
                 // Verify that there is no ignored source:
                 Set<String> storedFieldNames = new LinkedHashSet<>(document.getFields().stream().map(IndexableField::name).toList());
-                assertThat(storedFieldNames, contains(expectedStoredFields));
+                if (IndexSettings.RECOVERY_USE_SYNTHETIC_SOURCE.isEnabled()) {
+                    assertThat(storedFieldNames, contains(expectedStoredFields));
+                } else {
+                    String[] copyExpectedStoredFields = new String[expectedStoredFields.length + 1];
+                    System.arraycopy(expectedStoredFields, 0, copyExpectedStoredFields, 0, expectedStoredFields.length);
+                    copyExpectedStoredFields[copyExpectedStoredFields.length - 1] = "_recovery_source";
+                    assertThat(storedFieldNames, containsInAnyOrder(copyExpectedStoredFields));
+                }
             }
             var fieldInfo = FieldInfos.getMergedFieldInfos(reader).fieldInfo("field.offsets");
             assertThat(fieldInfo.getDocValuesType(), equalTo(DocValuesType.SORTED));
@@ -250,7 +257,11 @@ public class KeywordSyntheticSourceNativeArrayIntegrationTests extends ESSingleN
                 var document = reader.storedFields().document(i);
                 // Verify that there is ignored source because of leaf array being wrapped by object array:
                 List<String> storedFieldNames = document.getFields().stream().map(IndexableField::name).toList();
-                assertThat(storedFieldNames, contains("_id", "_ignored_source"));
+                if (IndexSettings.RECOVERY_USE_SYNTHETIC_SOURCE.isEnabled()) {
+                    assertThat(storedFieldNames, contains("_id", "_ignored_source"));
+                } else {
+                    assertThat(storedFieldNames, containsInAnyOrder("_id", "_ignored_source", "_recovery_source"));
+                }
 
                 // Verify that there is no offset field:
                 LeafReader leafReader = reader.leaves().get(0).reader();

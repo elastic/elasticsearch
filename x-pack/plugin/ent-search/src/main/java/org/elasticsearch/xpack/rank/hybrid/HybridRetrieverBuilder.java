1e083fd2fac272f905645b23c6497394e9ba8a26
Mike Pellegrini <mike.pellegrini@elastic.co>
2025-02-28 14:44:30 -0500

    Implemented doToXContent

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/rank/hybrid/HybridRetrieverBuilder.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/rank/hybrid/HybridRetrieverBuilder.java
@@ -13,14 +13,18 @@ import org.elasticsearch.search.retriever.RetrieverBuilder;
 import org.elasticsearch.search.retriever.RetrieverBuilderWrapper;
 import org.elasticsearch.search.retriever.StandardRetrieverBuilder;
 import org.elasticsearch.xcontent.ParseField;
+import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xpack.rank.linear.LinearRetrieverBuilder;
 import org.elasticsearch.xpack.rank.linear.MinMaxScoreNormalizer;
 import org.elasticsearch.xpack.rank.linear.ScoreNormalizer;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+
+import static org.elasticsearch.search.retriever.CompoundRetrieverBuilder.RANK_WINDOW_SIZE_FIELD;
 
 public class HybridRetrieverBuilder extends RetrieverBuilderWrapper<HybridRetrieverBuilder> {
     public static final String NAME = "hybrid";
@@ -29,9 +33,13 @@ public class HybridRetrieverBuilder extends RetrieverBuilderWrapper<HybridRetrie
 
     private final List<String> fields;
     private final String query;
+    private final int rankWindowSize;
 
     public HybridRetrieverBuilder(List<String> fields, String query, int rankWindowSize) {
-        super(
+        this(
+            fields == null ? List.of() : List.copyOf(fields),
+            query,
+            rankWindowSize,
             new LinearRetrieverBuilder(
                 generateInnerRetrievers(fields, query),
                 rankWindowSize,
@@ -39,20 +47,18 @@ public class HybridRetrieverBuilder extends RetrieverBuilderWrapper<HybridRetrie
                 generateScoreNormalizers(fields)
             )
         );
-        this.fields = fields == null ? List.of() : List.copyOf(fields);
-        this.query = query;
     }
 
-    private HybridRetrieverBuilder(List<String> fields, String query, RetrieverBuilder retrieverBuilder) {
+    private HybridRetrieverBuilder(List<String> fields, String query, int rankWindowSize, RetrieverBuilder retrieverBuilder) {
         super(retrieverBuilder);
         this.fields = fields;
         this.query = query;
+        this.rankWindowSize = rankWindowSize;
     }
 
     @Override
     protected HybridRetrieverBuilder clone(RetrieverBuilder sub) {
-        assert sub instanceof LinearRetrieverBuilder;
-        return new HybridRetrieverBuilder(fields, query, sub);
+        return new HybridRetrieverBuilder(fields, query, rankWindowSize, sub);
     }
 
     @Override
@@ -61,7 +67,15 @@ public class HybridRetrieverBuilder extends RetrieverBuilderWrapper<HybridRetrie
     }
 
     @Override
+    protected void doToXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.field(FIELDS_FIELD.getPreferredName(), fields);
+        builder.field(QUERY_FIELD.getPreferredName(), query);
+        builder.field(RANK_WINDOW_SIZE_FIELD.getPreferredName(), rankWindowSize);
+    }
+
+    @Override
     protected boolean doEquals(Object o) {
+        // TODO: Check rankWindowSize? It should be checked by the wrapped retriever.
         HybridRetrieverBuilder that = (HybridRetrieverBuilder) o;
         return Objects.equals(fields, that.fields) && Objects.equals(query, that.query) && super.doEquals(o);
     }

2d9c0cd51c0bee3cf7b9f2a332865728e2551e38
Mike Pellegrini <mike.pellegrini@elastic.co>
2025-02-28 14:12:40 -0500

    Updated HybridRetrieverBuilder to be a wrapper

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/rank/hybrid/HybridRetrieverBuilder.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/rank/hybrid/HybridRetrieverBuilder.java
@@ -7,35 +7,52 @@
 
 package org.elasticsearch.xpack.rank.hybrid;
 
-import org.apache.lucene.search.ScoreDoc;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.search.rank.RankDoc;
+import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.search.retriever.CompoundRetrieverBuilder;
+import org.elasticsearch.search.retriever.RetrieverBuilder;
+import org.elasticsearch.search.retriever.RetrieverBuilderWrapper;
+import org.elasticsearch.search.retriever.StandardRetrieverBuilder;
 import org.elasticsearch.xcontent.ParseField;
-import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xpack.rank.linear.LinearRetrieverBuilder;
+import org.elasticsearch.xpack.rank.linear.MinMaxScoreNormalizer;
+import org.elasticsearch.xpack.rank.linear.ScoreNormalizer;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
-public class HybridRetrieverBuilder extends CompoundRetrieverBuilder<HybridRetrieverBuilder> {
+public class HybridRetrieverBuilder extends RetrieverBuilderWrapper<HybridRetrieverBuilder> {
     public static final String NAME = "hybrid";
     public static final ParseField FIELDS_FIELD = new ParseField("fields");
+    public static final ParseField QUERY_FIELD = new ParseField("query");
 
     private final List<String> fields;
+    private final String query;
 
-    public HybridRetrieverBuilder(List<String> fields, int rankWindowSize) {
-        super(List.of(), rankWindowSize);
-        this.fields = List.copyOf(fields);
+    public HybridRetrieverBuilder(List<String> fields, String query, int rankWindowSize) {
+        super(
+            new LinearRetrieverBuilder(
+                generateInnerRetrievers(fields, query),
+                rankWindowSize,
+                generateWeights(fields),
+                generateScoreNormalizers(fields)
+            )
+        );
+        this.fields = fields == null ? List.of() : List.copyOf(fields);
+        this.query = query;
+    }
+
+    private HybridRetrieverBuilder(List<String> fields, String query, RetrieverBuilder retrieverBuilder) {
+        super(retrieverBuilder);
+        this.fields = fields;
+        this.query = query;
     }
 
     @Override
-    protected HybridRetrieverBuilder clone(List<RetrieverSource> newChildRetrievers, List<QueryBuilder> newPreFilterQueryBuilders) {
-        return null;
-    }
-
-    @Override
-    protected RankDoc[] combineInnerRetrieverResults(List<ScoreDoc[]> rankResults, boolean explain) {
-        return new RankDoc[0];
+    protected HybridRetrieverBuilder clone(RetrieverBuilder sub) {
+        assert sub instanceof LinearRetrieverBuilder;
+        return new HybridRetrieverBuilder(fields, query, sub);
     }
 
     @Override
@@ -44,7 +61,48 @@ public class HybridRetrieverBuilder extends CompoundRetrieverBuilder<HybridRetri
     }
 
     @Override
-    protected void doToXContent(XContentBuilder builder, Params params) throws IOException {
+    protected boolean doEquals(Object o) {
+        HybridRetrieverBuilder that = (HybridRetrieverBuilder) o;
+        return Objects.equals(fields, that.fields) && Objects.equals(query, that.query) && super.doEquals(o);
+    }
 
+    @Override
+    protected int doHashCode() {
+        return Objects.hash(fields, query, super.doHashCode());
+    }
+
+    private static List<CompoundRetrieverBuilder.RetrieverSource> generateInnerRetrievers(List<String> fields, String query) {
+        if (fields == null) {
+            return List.of();
+        }
+
+        List<CompoundRetrieverBuilder.RetrieverSource> innerRetrievers = new ArrayList<>(fields.size());
+        for (String field : fields) {
+            MatchQueryBuilder matchQueryBuilder = new MatchQueryBuilder(field, query);
+            innerRetrievers.add(new CompoundRetrieverBuilder.RetrieverSource(new StandardRetrieverBuilder(matchQueryBuilder), null));
+        }
+
+        return innerRetrievers;
+    }
+
+    private static float[] generateWeights(List<String> fields) {
+        if (fields == null) {
+            return new float[0];
+        }
+
+        // TODO: Parse field strings for weights
+        float[] weights = new float[fields.size()];
+        Arrays.fill(weights, 1.0f);
+        return weights;
+    }
+
+    private static ScoreNormalizer[] generateScoreNormalizers(List<String> fields) {
+        if (fields == null) {
+            return new ScoreNormalizer[0];
+        }
+
+        ScoreNormalizer[] scoreNormalizers = new ScoreNormalizer[fields.size()];
+        Arrays.fill(scoreNormalizers, MinMaxScoreNormalizer.INSTANCE);
+        return scoreNormalizers;
     }
 }

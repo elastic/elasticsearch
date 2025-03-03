a8a5efaa93ae15e0aa8380b60cc0d7f5c2cb24ef
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-03-03 08:36:36 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/node/NodeConstruction.java
+++ server/src/main/java/org/elasticsearch/node/NodeConstruction.java
@@ -241,8 +241,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;

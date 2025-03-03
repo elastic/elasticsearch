f723d6c58b0db3a80131697661eb2473f580106a
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2025-03-03 13:09:06 +0100

    Rename driver description (#123848)
    (cherry picked from commit 2fcb23a87ed4f94e33d6bebb3294064cd1c877d2)

--- x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/operator/Driver.java
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/operator/Driver.java
@@ -53,11 +53,10 @@ public class Driver implements Releasable, Describable {
     private final String sessionId;
 
     /**
-     * Description of the task this driver is running. This description should be
-     * short and meaningful as a grouping identifier. We use the phase of the
-     * query right now: "data", "node_reduce", "final".
+     * Description of the driver. This description should be short and meaningful as a grouping identifier.
+     * We use the phase of the query right now: "data", "node_reduce", "final".
      */
-    private final String taskDescription;
+    private final String shortDescription;
 
     /**
      * The wall clock time when this driver was created in milliseconds since epoch.
@@ -103,10 +102,8 @@ public class Driver implements Releasable, Describable {
     /**
      * Creates a new driver with a chain of operators.
      * @param sessionId session Id
-     * @param taskDescription Description of the task this driver is running. This
-     *                        description should be short and meaningful as a grouping
-     *                        identifier. We use the phase of the query right now:
-     *                        "data", "node_reduce", "final".
+     * @param shortDescription Description of the driver. This description should be short and meaningful as a grouping identifier.
+     *                         We use the phase of the query right now: "data", "node_reduce", "final".
      * @param driverContext the driver context
      * @param source source operator
      * @param intermediateOperators  the chain of operators to execute
@@ -116,7 +113,7 @@ public class Driver implements Releasable, Describable {
      */
     public Driver(
         String sessionId,
-        String taskDescription,
+        String shortDescription,
         long startTime,
         long startNanos,
         DriverContext driverContext,
@@ -128,7 +125,7 @@ public class Driver implements Releasable, Describable {
         Releasable releasable
     ) {
         this.sessionId = sessionId;
-        this.taskDescription = taskDescription;
+        this.shortDescription = shortDescription;
         this.startTime = startTime;
         this.startNanos = startNanos;
         this.driverContext = driverContext;
@@ -142,7 +139,7 @@ public class Driver implements Releasable, Describable {
         this.status = new AtomicReference<>(
             new DriverStatus(
                 sessionId,
-                taskDescription,
+                shortDescription,
                 startTime,
                 System.currentTimeMillis(),
                 0,
@@ -481,7 +478,7 @@ public class Driver implements Releasable, Describable {
             throw new IllegalStateException("can only get profile from finished driver");
         }
         return new DriverProfile(
-            status.taskDescription(),
+            status.description(),
             status.started(),
             status.lastUpdated(),
             finishNanos - startNanos,
@@ -528,7 +525,7 @@ public class Driver implements Releasable, Describable {
 
             return new DriverStatus(
                 sessionId,
-                taskDescription,
+                shortDescription,
                 startTime,
                 now,
                 prev.cpuNanos() + extraCpuNanos,

a8debe301e331f3f17833ab8754fb2f0ee4cb90b
Dimitris Rempapis <dimitris.rempapis@elastic.co>
2025-03-05 16:03:03 +0200

    update

--- server/src/main/java/org/elasticsearch/common/util/concurrent/TaskExecutionTimeTrackingPerIndexEsThreadPoolExecutor.java
+++ server/src/main/java/org/elasticsearch/common/util/concurrent/TaskExecutionTimeTrackingPerIndexEsThreadPoolExecutor.java
@@ -112,7 +112,7 @@ public class TaskExecutionTimeTrackingPerIndexEsThreadPoolExecutor extends TaskE
                 );
                 t.v1().add(taskTime);
                 t.v2().addValue(taskTime);
-                logger.debug("Task execution time for index [{}] is [{}]", indexName, taskTime);
+                logger.info("Task execution time for index [{}] is [{}]", indexName, taskTime);
             }
             super.trackExecutionTime(r, taskTime);
         } finally {

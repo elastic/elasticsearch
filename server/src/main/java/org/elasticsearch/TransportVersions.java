335a1d4ff27c2d3167577c87f13f8b5327cf99d0
Parker Timmins <parker.timmins@elastic.co>
2025-03-06 16:43:14 -0600

    [8.18] Retry ILM async action after reindexing data stream (#124149) (#124270)
    * Retry ILM async action after reindexing data stream (#124149)
    
    When reindexing a data stream, the ILM metadata is copied from the index metadata of the source index to the destination index. But the ILM state of the new index can be stuck if the source index was in an AsyncAction at the time of reindexing. To un-stick the new index, we call TransportRetryAction to retry the AsyncAction. In the past this action would only run if the index were in the error phase. This change includes an update to TransportRetryAction, which allows it to be run when the index is not in an error phase, if the parameter requireError is set to false.
    
    (cherry picked from commit 10a8dcf0fb4e5c3757e26dfc777dc84dfc25fae3)
    
    # Conflicts:
    #	server/src/main/java/org/elasticsearch/TransportVersions.java
    #	x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/user/InternalUsers.java
    #	x-pack/plugin/ilm/src/main/java/org/elasticsearch/xpack/ilm/action/TransportRetryAction.java
    #	x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/DataStreamsUpgradeIT.java
    
    * index mode cannot be set on v7 indices

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -183,6 +183,7 @@ public class TransportVersions {
     public static final TransportVersion INFERENCE_REQUEST_ADAPTIVE_RATE_LIMITING = def(8_839_0_00);
     public static final TransportVersion ML_INFERENCE_IBM_WATSONX_RERANK_ADDED = def(8_840_00_0);
     public static final TransportVersion REMOVE_ALL_APPLICABLE_SELECTOR_BACKPORT_8_18 = def(8_840_00_1);
+    public static final TransportVersion RETRY_ILM_ASYNC_ACTION_REQUIRE_ERROR_8_18 = def(8_840_0_02);
 
     /*
      * STOP! READ THIS FIRST! No, really,

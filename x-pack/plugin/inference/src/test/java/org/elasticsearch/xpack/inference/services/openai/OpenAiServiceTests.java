6108381af87323f14344496eb66aaa1626412854
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-03-07 18:55:48 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/openai/OpenAiServiceTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/openai/OpenAiServiceTests.java
@@ -63,7 +63,6 @@ import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 

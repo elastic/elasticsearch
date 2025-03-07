c3fa2898a5ee342297cf5a46551503601e26c88a
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-03-07 19:30:48 +0000

    [CI] Auto commit changes from spotless

--- libs/entitlement/src/main/java/org/elasticsearch/entitlement/runtime/policy/FileAccessTree.java
+++ libs/entitlement/src/main/java/org/elasticsearch/entitlement/runtime/policy/FileAccessTree.java
@@ -23,7 +23,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;

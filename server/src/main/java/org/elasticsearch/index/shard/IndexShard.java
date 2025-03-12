e5300c015e4de98150c35bba7eb574752ac47fa6
Tim Brooks <tim@timbrooks.org>
2025-03-11 22:40:11 -0600

    Changes

--- server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
@@ -608,6 +608,31 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
                             try {
                                 replicationTracker.activatePrimaryMode(getLocalCheckpoint());
                                 ensurePeerRecoveryRetentionLeasesExist();
+                                /*
+                                 * If this shard was serving as a replica shard when another shard was promoted to primary then
+                                 * its Lucene index was reset during the primary term transition. In particular, the Lucene index
+                                 * on this shard was reset to the global checkpoint and the operations above the local checkpoint
+                                 * were reverted. If the other shard that was promoted to primary subsequently fails before the
+                                 * primary/replica re-sync completes successfully and we are now being promoted, we have to restore
+                                 * the reverted operations on this shard by replaying the translog to avoid losing acknowledged writes.
+                                 */
+                                final Engine engine = getEngine();
+                                engine.restoreLocalHistoryFromTranslog(
+                                    (resettingEngine, snapshot) -> runTranslogRecovery(
+                                        resettingEngine,
+                                        snapshot,
+                                        Engine.Operation.Origin.LOCAL_RESET,
+                                        () -> {}
+                                    )
+                                );
+                                /* Rolling the translog generation is not strictly needed here (as we will never have collisions between
+                                 * sequence numbers in a translog generation in a new primary as it takes the last known sequence number
+                                 * as a starting point), but it simplifies reasoning about the relationship between primary terms and
+                                 * translog generations.
+                                 */
+                                engine.rollTranslogGeneration();
+                                engine.fillSeqNoGaps(newPrimaryTerm);
+                                replicationTracker.updateLocalCheckpoint(currentRouting.allocationId().getId(), getLocalCheckpoint());
                             } catch (final AlreadyClosedException e) {
                                 // okay, the index was deleted
                             }

71edec2c99cae11b135d97f7a3b3d7cb6cc2baae
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-03-13 18:37:45 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/cluster/metadata/InferenceFieldMetadata.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/InferenceFieldMetadata.java
@@ -22,8 +22,6 @@ import org.elasticsearch.xcontent.XContentParser;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
--- x-pack/plugin/inference/qa/test-service-plugin/src/main/java/org/elasticsearch/xpack/inference/mock/AbstractTestInferenceService.java
+++ x-pack/plugin/inference/qa/test-service-plugin/src/main/java/org/elasticsearch/xpack/inference/mock/AbstractTestInferenceService.java
@@ -29,7 +29,6 @@ import org.elasticsearch.xpack.inference.chunking.WordBoundaryChunkingSettings;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;

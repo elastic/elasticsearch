17127ce53cc84c78f2b7c9c7634e4d927abe8f8d
Luke Whiting <luke.whiting@elastic.co>
2025-03-17 11:03:10 +0000

    ES-9508 Datafeed deprecated aggregation options notice

--- x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/MlDeprecationChecker.java
+++ x-pack/plugin/deprecation/src/main/java/org/elasticsearch/xpack/deprecation/MlDeprecationChecker.java
@@ -57,8 +57,7 @@ public class MlDeprecationChecker implements DeprecationChecker {
                 new DeprecationIssue(
                     DeprecationIssue.Level.WARNING,
                     "Datafeed [" + datafeedConfig.getId() + "] uses deprecated aggregation options",
-                    "https://www.elastic.co/guide/en/elasticsearch/reference/master/breaking-changes-7.0.html"
-                        + "#breaking_70_aggregations_changes",
+                    "https://ela.st/es-deprecation-7-data-feed-aggregation",
                     deprecations.toString(),
                     false,
                     null

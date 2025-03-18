dc6e4a9b78e90857cbaf12ed97aa981b8a36e848
Svilen Mihaylov <svilen.mihaylov@elastic.co>
2025-03-18 13:41:16 -0400

    getFirst() -> get(0)

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/fulltext/QueryStringErrorTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/fulltext/QueryStringErrorTests.java
@@ -39,7 +39,7 @@ public class QueryStringErrorTests extends ErrorsForCasesWithoutExamplesTestCase
 
     @Override
     protected Expression build(Source source, List<Expression> args) {
-        return new QueryString(source, args.getFirst(), args.size() > 1 ? args.get(1) : null);
+        return new QueryString(source, args.get(0), args.size() > 1 ? args.get(1) : null);
     }
 
     @Override

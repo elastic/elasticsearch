f9263c8169612ff917422fbb21ba9aacfac202b8
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2025-03-19 11:04:12 -0700

    [8.x] Use FallbackSyntheticSourceBlockLoader for shape and geo_shape (#124927) (#125164)
    * Use FallbackSyntheticSourceBlockLoader for shape and geo_shape (#124927)
    
    (cherry picked from commit 033d28e7927dd823a1d45e172cc149e620867dc4)
    
    # Conflicts:
    #	server/src/test/java/org/elasticsearch/index/mapper/blockloader/BooleanFieldBlockLoaderTests.java
    #	server/src/test/java/org/elasticsearch/index/mapper/blockloader/DateFieldBlockLoaderTests.java
    #	server/src/test/java/org/elasticsearch/index/mapper/blockloader/KeywordFieldBlockLoaderTests.java
    #	test/framework/src/main/java/org/elasticsearch/index/mapper/BlockLoaderTestCase.java
    #	test/framework/src/main/java/org/elasticsearch/index/mapper/NumberFieldBlockLoaderTestCase.java
    
    * fix merge
    
    * fix
    
    * fix more
    
    * fix more
    
    * properly sync BlockLoaderTestCase
    
    * iter
    
    * iter
    
    * [CI] Auto commit changes from spotless
    
    * f
    
    * f
    
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

--- x-pack/plugin/mapper-unsigned-long/src/main/java/org/elasticsearch/xpack/unsignedlong/UnsignedLongFieldMapper.java
+++ x-pack/plugin/mapper-unsigned-long/src/main/java/org/elasticsearch/xpack/unsignedlong/UnsignedLongFieldMapper.java
@@ -364,7 +364,7 @@ public class UnsignedLongFieldMapper extends FieldMapper {
             var nullValueEncoded = nullValueFormatted != null
                 ? (Number) unsignedToSortableSignedLong(parseUnsignedLong(nullValueFormatted))
                 : null;
-            return new FallbackSyntheticSourceBlockLoader.ReaderWithNullValueSupport<Number>(nullValueFormatted) {
+            return new FallbackSyntheticSourceBlockLoader.SingleValueReader<Number>(nullValueFormatted) {
                 @Override
                 public void convertValue(Object value, List<Number> accumulator) {
                     if (value.equals("")) {

117e7fdfb46760c9535b6b7199bf7f1624bcceb2
gmarouli <mary.gouseti@elastic.co>
2025-03-20 14:50:50 +0200

    Remove assertion that was copied by mistake

--- server/src/main/java/org/elasticsearch/cluster/metadata/Template.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/Template.java
@@ -139,7 +139,6 @@ public class Template implements SimpleDiffable<Template>, ToXContentObject {
         this.mappings = mappings;
         this.aliases = aliases;
         this.lifecycle = lifecycle;
-        assert dataStreamOptions != null : "Template does not accept null values, please use Resettable.undefined()";
         this.dataStreamOptions = ResettableValue.create(dataStreamOptions);
     }
 

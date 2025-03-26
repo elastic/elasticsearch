009a86a0e32231eab5b911f9364202639bb6cc47
Benjamin Trent <ben.w.trent@gmail.com>
2025-03-26 15:56:51 -0400

    Allow zero for rescore_vector.oversample to indicate by-passing oversample and rescoring (#125599)
    This allows a `rescore_vector: {oversample: 0}` to indicate bypassing
    oversampling and rescoring. 
    
    This is useful for:
    
     - Updating a quantized mapping to turn off automatic rescoring
     - Bypassing oversampling at query time in an ad-hoc manner if its on by default in the mapping
    
    closes: https://github.com/elastic/elasticsearch/issues/125157

--- server/src/main/java/org/elasticsearch/index/mapper/MapperFeatures.java
+++ server/src/main/java/org/elasticsearch/index/mapper/MapperFeatures.java
@@ -15,6 +15,7 @@ import org.elasticsearch.features.NodeFeature;
 import java.util.Set;
 
 import static org.elasticsearch.index.mapper.vectors.DenseVectorFieldMapper.RESCORE_VECTOR_QUANTIZED_VECTOR_MAPPING;
+import static org.elasticsearch.index.mapper.vectors.DenseVectorFieldMapper.RESCORE_ZERO_VECTOR_QUANTIZED_VECTOR_MAPPING;
 
 /**
  * Spec for mapper-related features.
@@ -62,7 +63,8 @@ public class MapperFeatures implements FeatureSpecification {
             UKNOWN_FIELD_MAPPING_UPDATE_ERROR_MESSAGE,
             DOC_VALUES_SKIPPER,
             RESCORE_VECTOR_QUANTIZED_VECTOR_MAPPING,
-            DateFieldMapper.INVALID_DATE_FIX
+            DateFieldMapper.INVALID_DATE_FIX,
+            RESCORE_ZERO_VECTOR_QUANTIZED_VECTOR_MAPPING
         );
     }
 }

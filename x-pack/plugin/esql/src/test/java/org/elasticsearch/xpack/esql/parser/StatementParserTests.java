bbfe6f155fa4f2303862f7a906da9b057876fd4c
Fang Xing <fang.xing@elastic.co>
2025-03-26 12:29:53 -0400

    take double parameter markers out of snapshot

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/parser/StatementParserTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/parser/StatementParserTests.java
@@ -3357,10 +3357,7 @@ public class StatementParserTests extends AbstractStatementParserTests {
     }
 
     public void testDoubleParamsForIdentifier() {
-        assumeTrue(
-            "double parameters markers for identifiers requires snapshot build",
-            EsqlCapabilities.Cap.DOUBLE_PARAMETER_MARKERS_FOR_IDENTIFIERS.isEnabled()
-        );
+        assumeTrue("double parameters markers for identifiers", EsqlCapabilities.Cap.DOUBLE_PARAMETER_MARKERS_FOR_IDENTIFIERS.isEnabled());
         // There are three variations of double parameters - named, positional or anonymous, e.g. ??n, ??1 or ??, covered.
         // Each query is executed three times with the three variations.
 
@@ -3829,10 +3826,7 @@ public class StatementParserTests extends AbstractStatementParserTests {
     }
 
     public void testMixedSingleDoubleParams() {
-        assumeTrue(
-            "double parameters markers for identifiers requires snapshot build",
-            EsqlCapabilities.Cap.DOUBLE_PARAMETER_MARKERS_FOR_IDENTIFIERS.isEnabled()
-        );
+        assumeTrue("double parameters markers for identifiers", EsqlCapabilities.Cap.DOUBLE_PARAMETER_MARKERS_FOR_IDENTIFIERS.isEnabled());
         // This is a subset of testDoubleParamsForIdentifier, with single and double parameter markers mixed in the queries
         // Single parameter markers represent a constant value or pattern
         // double parameter markers represent identifiers - field or function names
@@ -4015,10 +4009,7 @@ public class StatementParserTests extends AbstractStatementParserTests {
     }
 
     public void testInvalidDoubleParamsNames() {
-        assumeTrue(
-            "double parameters markers for identifiers requires snapshot build",
-            EsqlCapabilities.Cap.DOUBLE_PARAMETER_MARKERS_FOR_IDENTIFIERS.isEnabled()
-        );
+        assumeTrue("double parameters markers for identifiers", EsqlCapabilities.Cap.DOUBLE_PARAMETER_MARKERS_FOR_IDENTIFIERS.isEnabled());
         expectError(
             "from test | where x < ??n1 | eval y = ??n2",
             List.of(paramAsConstant("n1", "f1"), paramAsConstant("n3", "f2")),
@@ -4035,10 +4026,7 @@ public class StatementParserTests extends AbstractStatementParserTests {
     }
 
     public void testInvalidDoubleParamsPositions() {
-        assumeTrue(
-            "double parameters markers for identifiers requires snapshot build",
-            EsqlCapabilities.Cap.DOUBLE_PARAMETER_MARKERS_FOR_IDENTIFIERS.isEnabled()
-        );
+        assumeTrue("double parameters markers for identifiers", EsqlCapabilities.Cap.DOUBLE_PARAMETER_MARKERS_FOR_IDENTIFIERS.isEnabled());
         expectError(
             "from test | where x < ??0",
             List.of(paramAsConstant(null, "f1")),
@@ -4066,10 +4054,7 @@ public class StatementParserTests extends AbstractStatementParserTests {
     }
 
     public void testInvalidDoubleParamsType() {
-        assumeTrue(
-            "double parameters markers for identifiers requires snapshot build",
-            EsqlCapabilities.Cap.DOUBLE_PARAMETER_MARKERS_FOR_IDENTIFIERS.isEnabled()
-        );
+        assumeTrue("double parameters markers for identifiers", EsqlCapabilities.Cap.DOUBLE_PARAMETER_MARKERS_FOR_IDENTIFIERS.isEnabled());
         // double parameter markers cannot be declared as identifier patterns
         String error = "Query parameter [??f1][f1] declared as a pattern, cannot be used as an identifier";
         List<String> commandWithDoubleParams = List.of(

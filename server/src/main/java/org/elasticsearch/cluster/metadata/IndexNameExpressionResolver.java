cacf00e948ab4a6268346af9d96626542eefd57c
Slobodan Adamović <slobodanadamovic@users.noreply.github.com>
2025-03-28 09:20:51 +0100

    [Failure Store] Prevent usage of :: selectors with cross-cluster expressions (#125252)
    The CCS is currently not supported for failure store backing indices.
    This PR adjusts the selector parsing (introduced in #118614) to prevent
    using `::failures` and `::data` selectors with cross-cluster expressions.
    
    For example, `GET my_remote_cluster:logs-*::failures/_search` request
    will fail early, during expression parsing.
    To test manually, run `./gradlew run-ccs` and execute the example request.
    
    (cherry picked from commit 1d6c6a59c86de232c65a29579a12e0fdb0eb97ad)
    
    # Conflicts:
    #	server/src/main/java/org/elasticsearch/cluster/metadata/IndexNameExpressionResolver.java
    #	x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/parser/StatementParserTests.java

--- server/src/main/java/org/elasticsearch/cluster/metadata/IndexNameExpressionResolver.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/IndexNameExpressionResolver.java
@@ -2126,19 +2126,29 @@ public class IndexNameExpressionResolver {
             int lastDoubleColon = expression.lastIndexOf(SELECTOR_SEPARATOR);
             if (lastDoubleColon >= 0) {
                 String suffix = expression.substring(lastDoubleColon + SELECTOR_SEPARATOR.length());
-                IndexComponentSelector selector = IndexComponentSelector.getByKey(suffix);
-                if (selector == null) {
-                    throw new InvalidIndexNameException(
-                        expression,
-                        "invalid usage of :: separator, [" + suffix + "] is not a recognized selector"
-                    );
-                }
+                IndexComponentSelector selector = resolveAndValidateSelectorString(() -> expression, suffix);
                 String expressionBase = expression.substring(0, lastDoubleColon);
                 ensureNoMoreSelectorSeparators(expressionBase, expression);
+                ensureNotMixingRemoteClusterExpressionWithSelectorSeparator(expressionBase, selector, expression);
                 return bindFunction.apply(expressionBase, suffix);
             }
             // Otherwise accept the default
             return bindFunction.apply(expression, null);
+        }
+
+        public static void validateIndexSelectorString(String indexName, String suffix) {
+            resolveAndValidateSelectorString(() -> indexName + SELECTOR_SEPARATOR + suffix, suffix);
+        }
+
+        private static IndexComponentSelector resolveAndValidateSelectorString(Supplier<String> expression, String suffix) {
+            IndexComponentSelector selector = IndexComponentSelector.getByKey(suffix);
+            if (selector == null) {
+                throw new InvalidIndexNameException(
+                    expression.get(),
+                    "invalid usage of :: separator, [" + suffix + "] is not a recognized selector"
+                );
+            }
+            return selector;
         }
 
         /**
@@ -2167,6 +2177,22 @@ public class IndexNameExpressionResolver {
                     originalExpression,
                     "Invalid usage of :: separator, only one :: separator is allowed per expression"
                 );
+            }
+        }
+
+        /**
+         * Checks the expression for remote cluster pattern and throws an exception if it is combined with :: selectors.
+         * @throws InvalidIndexNameException if remote cluster pattern is detected after parsing the selector expression
+         */
+        private static void ensureNotMixingRemoteClusterExpressionWithSelectorSeparator(
+            String expressionWithoutSelector,
+            IndexComponentSelector selector,
+            String originalExpression
+        ) {
+            if (selector != null) {
+                if (RemoteClusterAware.isRemoteIndexName(expressionWithoutSelector)) {
+                    throw new InvalidIndexNameException(originalExpression, "Selectors are not yet supported on remote cluster patterns");
+                }
             }
         }
     }

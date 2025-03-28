b0ef9bc79c78b55ec6aeff70f99bafd94d4154c7
Mark Vieira <portugee@gmail.com>
2025-03-28 11:52:42 -0700

    [8.18] Convert enterprise search module to new testing framework (#125807) (#125879)
    * Convert enterprise search module to new testing framework (#125807)
    
    (cherry picked from commit 3ac0c136aab10f1ade293c5bb142ef31d6d5c4fb)
    
    # Conflicts:
    #	x-pack/plugin/ent-search/qa/rest/build.gradle
    
    * Fix task name

--- x-pack/plugin/ent-search/src/yamlRestTest/java/org/elasticsearch/xpack/entsearch/EnterpriseSearchRestIT.java
+++ x-pack/plugin/ent-search/src/yamlRestTest/java/org/elasticsearch/xpack/entsearch/EnterpriseSearchRestIT.java
@@ -12,10 +12,27 @@ import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
+import org.elasticsearch.test.cluster.ElasticsearchCluster;
+import org.elasticsearch.test.cluster.local.distribution.DistributionType;
+import org.elasticsearch.test.cluster.util.resource.Resource;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
 import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
+import org.junit.ClassRule;
 
 public class EnterpriseSearchRestIT extends ESClientYamlSuiteTestCase {
+
+    @ClassRule
+    public static ElasticsearchCluster cluster = ElasticsearchCluster.local()
+        .distribution(DistributionType.DEFAULT)
+        .setting("xpack.security.enabled", "true")
+        .setting("xpack.security.autoconfiguration.enabled", "false")
+        .setting("xpack.license.self_generated.type", "trial")
+        .rolesFile(Resource.fromClasspath("roles.yml"))
+        .user("entsearch-superuser", "entsearch-superuser-password", "superuser", false)
+        .user("entsearch-admin", "entsearch-admin-password", "admin", false)
+        .user("entsearch-user", "entsearch-user-password", "user", false)
+        .user("entsearch-unprivileged", "entsearch-unprivileged-password", "unprivileged", false)
+        .build();
 
     public EnterpriseSearchRestIT(final ClientYamlTestCandidate testCandidate) {
         super(testCandidate);
@@ -36,5 +53,10 @@ public class EnterpriseSearchRestIT extends ESClientYamlSuiteTestCase {
     protected Settings restClientSettings() {
         final String value = basicAuthHeaderValue("entsearch-admin", new SecureString("entsearch-admin-password".toCharArray()));
         return Settings.builder().put(ThreadContext.PREFIX + ".Authorization", value).build();
+    }
+
+    @Override
+    protected String getTestRestCluster() {
+        return cluster.getHttpAddresses();
     }
 }

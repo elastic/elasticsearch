d160fe641882db716e7836b641d7c9b8fb390306
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2025-03-30 10:06:55 +0300

    revert constants usage
    Co-authored-by: Lee Hinman <dakrone@users.noreply.github.com>

--- modules/ingest-ecs/src/main/java/org/elasticsearch/ingest/ecs/EcsNamespacingProcessor.java
+++ modules/ingest-ecs/src/main/java/org/elasticsearch/ingest/ecs/EcsNamespacingProcessor.java
@@ -195,12 +195,12 @@ public class EcsNamespacingProcessor extends AbstractProcessor {
     }
 
     private boolean shouldMoveToResourceAttributes(String key) {
-        return key.startsWith(AGENT_PREFIX)
-            || key.equals("agent")
-            || key.startsWith(CLOUD_PREFIX)
-            || key.equals("cloud")
-            || key.startsWith(HOST_PREFIX)
-            || key.equals("host");
+        return key.startsWith(AGENT_PREFIX +".")
+            || key.equals(AGENT_PREFIX)
+            || key.startsWith(CLOUD_PREFIX + ".")
+            || key.equals(CLOUD_PREFIX)
+            || key.startsWith(HOST_PREFIX +".")
+            || key.equals(HOST_PREFIX);
     }
 
     public static final class Factory implements Processor.Factory {

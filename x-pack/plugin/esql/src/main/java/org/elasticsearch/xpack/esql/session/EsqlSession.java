def9a9813369b46929a09d0bd558ce7fd61be644
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-04-01 00:42:00 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
@@ -83,7 +83,6 @@ import org.elasticsearch.xpack.esql.telemetry.PlanTelemetry;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;

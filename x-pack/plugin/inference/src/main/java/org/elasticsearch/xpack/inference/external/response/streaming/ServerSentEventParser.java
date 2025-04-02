50dc134683514d46df76bb8ff614ba5b4cae606e
Pat Whelan <pat.whelan@elastic.co>
2025-04-02 12:40:51 -0400

    Using String.lines()

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/response/streaming/ServerSentEventParser.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/response/streaming/ServerSentEventParser.java
@@ -12,7 +12,6 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Locale;
 import java.util.Optional;
-import java.util.regex.Pattern;
 
 /**
  * https://html.spec.whatwg.org/multipage/server-sent-events.html#event-stream-interpretation
@@ -25,7 +24,6 @@ import java.util.regex.Pattern;
  * and have no plans to use them.
  */
 public class ServerSentEventParser {
-    private static final Pattern END_OF_LINE_REGEX = Pattern.compile("\\r\\n|\\n|\\r");
     private static final String BOM = "\uFEFF";
     private static final String TYPE_FIELD = "event";
     private static final String DATA_FIELD = "data";
@@ -38,13 +36,13 @@ public class ServerSentEventParser {
         }
 
         var body = previousTokens + new String(bytes, StandardCharsets.UTF_8);
-        var lines = END_OF_LINE_REGEX.split(body, -1); // -1 because we actually want trailing empty strings
+        var lines = body.lines();
 
-        var collector = new ArrayDeque<ServerSentEvent>(lines.length);
-        for (var i = 0; i < lines.length - 1; i++) {
-            var line = lines[i].replace(BOM, "");
+        var collector = new ArrayDeque<ServerSentEvent>();
+        lines.reduce((previousLine, nextLine) -> {
+            var line = previousLine.replace(BOM, "");
 
-            if (line.isBlank()) {
+            if (line.isEmpty()) {
                 eventBuffer.dispatch().ifPresent(collector::offer);
             } else if (line.startsWith(":") == false) {
                 if (line.contains(":")) {
@@ -53,12 +51,19 @@ public class ServerSentEventParser {
                     eventBuffer.data("");
                 }
             }
-        }
-
-        // we can sometimes get bytes for incomplete messages, so we save them for the next onNext invocation
-        // if we get an onComplete before we clear this cache, we follow the spec to treat it as an incomplete event and discard it since
-        // it was not followed by a blank line
-        previousTokens = lines[lines.length - 1];
+            return nextLine;
+        }).ifPresent(lastLine -> {
+            if (lastLine.isEmpty()) {
+                // if the last line is an empty line, then we dispatch the event and clear the previousToken cache
+                eventBuffer.dispatch().ifPresent(collector::offer);
+                previousTokens = "";
+            } else {
+                // we can sometimes get bytes for incomplete messages, so we save them for the next onNext invocation
+                // if we get an onComplete before we clear this cache, we follow the spec to treat it as an incomplete event and discard it
+                // since it was not followed by a blank line
+                previousTokens = lastLine;
+            }
+        });
         return collector;
     }
 
@@ -67,6 +72,7 @@ public class ServerSentEventParser {
         var fieldStr = lineWithColon.substring(0, firstColon).toLowerCase(Locale.ROOT);
 
         var value = lineWithColon.substring(firstColon + 1);
+        // "If value starts with a U+0020 SPACE character, remove it from value."
         var trimmedValue = value.length() > 0 && value.charAt(0) == ' ' ? value.substring(1) : value;
 
         if (DATA_FIELD.equals(fieldStr)) {
@@ -88,25 +94,30 @@ public class ServerSentEventParser {
         }
 
         private void data(String data) {
+            // "Append the field value to the data buffer, then append a single U+000A LINE FEED (LF) character to the data buffer."
+            // But then we're told "If the data buffer's last character is a U+000A LINE FEED (LF) character,
+            // then remove the last character from the data buffer."
+            // Rather than add + remove for single-line data fields, we only append a LINE FEED on subsequent data lines.
             if (appendLineFeed) {
                 this.data.append(LINE_FEED);
             } else {
-                // the next time we add data, append line feed
                 appendLineFeed = true;
             }
             this.data.append(data);
         }
 
         private Optional<ServerSentEvent> dispatch() {
-            var dataValue = data.toString();
-
-            // if the data buffer is empty, reset without dispatching
-            if (dataValue.isEmpty()) {
+            // "If the data buffer is an empty string, set the data buffer and the event type buffer to the empty string and return."
+            // We don't process empty events anywhere, so we just drop the events.
+            if (data.isEmpty()) {
                 reset();
                 return Optional.empty();
             }
+            var dataValue = data.toString();
 
-            // if the type buffer is not empty, set that as the type, else default to message
+            // "Initialize event's type attribute to "message""
+            // "If the event type buffer has a value other than the empty string,
+            // change the type of the newly created event to equal the value of the event type buffer."
             var typeValue = type.toString();
             typeValue = typeValue.isBlank() ? MESSAGE : typeValue;
 

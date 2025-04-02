a97d1192fd19d9ac57d3a205a49a248771f6a76f
Armin Braun <me@obrown.io>
2025-04-02 15:19:22 +0200

    Stop retaining transport responses past serialization (#125163) (#126078)
    Remove the `OutboundMessage` class that needlessly holds on to the the response instances after they are not needed any longer. Inlining the logic should save considerably heap under pressure and enabled further optimisations.
    
    backport of #125163

--- server/src/test/java/org/elasticsearch/transport/InboundHandlerTests.java
+++ server/src/test/java/org/elasticsearch/transport/InboundHandlerTests.java
@@ -171,18 +171,19 @@ public class InboundHandlerTests extends ESTestCase {
         );
         requestHandlers.registerHandler(registry);
         String requestValue = randomAlphaOfLength(10);
-        OutboundMessage.Request request = new OutboundMessage.Request(
-            threadPool.getThreadContext(),
-            new TestRequest(requestValue),
-            TransportVersion.current(),
+        BytesRefRecycler recycler = new BytesRefRecycler(PageCacheRecycler.NON_RECYCLING_INSTANCE);
+        BytesReference fullRequestBytes = OutboundHandler.serialize(
             action,
             requestId,
             false,
-            null
+            TransportVersion.current(),
+            false,
+            null,
+            new TestRequest(requestValue),
+            threadPool.getThreadContext(),
+            new RecyclerBytesStreamOutput(recycler)
         );
 
-        BytesRefRecycler recycler = new BytesRefRecycler(PageCacheRecycler.NON_RECYCLING_INSTANCE);
-        BytesReference fullRequestBytes = request.serialize(new RecyclerBytesStreamOutput(recycler));
         BytesReference requestContent = fullRequestBytes.slice(headerSize, fullRequestBytes.length() - headerSize);
         Header requestHeader = new Header(
             fullRequestBytes.length() - 6,

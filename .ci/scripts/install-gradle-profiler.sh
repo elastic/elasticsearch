b250fad883a2fa7c59db349f589bf47ca5c940ab
Rene Groeschke <rene@elastic.co>
2025-04-03 16:53:01 +0200

    Update gredle profiler

--- .ci/scripts/install-gradle-profiler.sh
+++ .ci/scripts/install-gradle-profiler.sh
@@ -2,7 +2,7 @@
 
 set -e
 # profiler version we wanna install
-PROFILER_VERSION="0.16.0"
+PROFILER_VERSION="0.22.0"
 wget https://repo.gradle.org/gradle/ext-releases-local/org/gradle/profiler/gradle-profiler/$PROFILER_VERSION/gradle-profiler-$PROFILER_VERSION.zip -O $WORKSPACE/gradle-profiler-$PROFILER_VERSION.zip
 unzip $WORKSPACE/gradle-profiler-$PROFILER_VERSION.zip
 mv $WORKSPACE/gradle-profiler-$PROFILER_VERSION $WORKSPACE/gradle-profiler

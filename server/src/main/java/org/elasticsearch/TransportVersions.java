b6c6db9861fb21b16f5d47cd435af97ba963a5cd
Sam Xiao <sam.xiao@elastic.co>
2025-04-03 16:26:22 -0400

    Add multi-project support for health indicator data_stream_lifecycle (#126056)

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -212,6 +212,7 @@ public class TransportVersions {
     public static final TransportVersion BATCHED_QUERY_PHASE_VERSION = def(9_043_0_00);
     public static final TransportVersion REMOTE_EXCEPTION = def(9_044_0_00);
     public static final TransportVersion ESQL_REMOVE_AGGREGATE_TYPE = def(9_045_0_00);
+    public static final TransportVersion ADD_PROJECT_ID_TO_DSL_ERROR_INFO = def(9_046_0_00);
 
     /*
      * STOP! READ THIS FIRST! No, really,

3c0a75646cb6fc22c0c7f916e6816846ad95a780
gmarouli <mary.gouseti@elastic.co>
2025-04-03 19:23:45 +0300

    Move PutDataStreamOptionsAction to server to be accessible by other modules

--- server/src/main/java/org/elasticsearch/action/datastreams/PutDataStreamOptionsAction.java
+++ server/src/main/java/org/elasticsearch/action/datastreams/PutDataStreamOptionsAction.java
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-package org.elasticsearch.datastreams.options.action;
+package org.elasticsearch.action.datastreams;
 
 import org.elasticsearch.action.ActionRequestValidationException;
 import org.elasticsearch.action.ActionType;

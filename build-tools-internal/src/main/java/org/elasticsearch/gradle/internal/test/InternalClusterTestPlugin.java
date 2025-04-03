b5c9283a301752929dd0d500ee4e99287f48fe98
Rene Groeschke <rene@elastic.co>
2025-04-04 00:00:10 +0200

    Rework internalClusterTest to support test suites

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/test/InternalClusterTestPlugin.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/test/InternalClusterTestPlugin.java
@@ -11,14 +11,13 @@ package org.elasticsearch.gradle.internal.test;
 
 import org.elasticsearch.gradle.internal.info.GlobalBuildInfoPlugin;
 import org.elasticsearch.gradle.util.GradleUtils;
-import org.gradle.api.JavaVersion;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.plugins.JvmTestSuitePlugin;
+import org.gradle.api.plugins.jvm.JvmTestSuite;
 import org.gradle.api.tasks.SourceSet;
-import org.gradle.api.tasks.TaskProvider;
-import org.gradle.api.tasks.testing.Test;
-
-import static org.elasticsearch.gradle.internal.util.ParamsUtils.loadBuildParams;
+import org.gradle.testing.base.TestingExtension;
 
 public class InternalClusterTestPlugin implements Plugin<Project> {
 
@@ -27,19 +26,18 @@ public class InternalClusterTestPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         project.getRootProject().getPlugins().apply(GlobalBuildInfoPlugin.class);
-        var buildParams = loadBuildParams(project).get();
-
-        TaskProvider<Test> internalClusterTest = GradleUtils.addTestSourceSet(project, SOURCE_SET_NAME);
-        internalClusterTest.configure(task -> {
-            // Set GC options to mirror defaults in jvm.options
-            if (buildParams.getRuntimeJavaVersion().get().compareTo(JavaVersion.VERSION_14) < 0) {
-                task.jvmArgs("-XX:+UseConcMarkSweepGC", "-XX:CMSInitiatingOccupancyFraction=75", "-XX:+UseCMSInitiatingOccupancyOnly");
-            } else {
-                task.jvmArgs("-XX:+UseG1GC");
-            }
+        project.getPluginManager().apply(JavaPlugin.class);
+        project.getPluginManager().apply(JvmTestSuitePlugin.class);
+        TestingExtension testing = project.getExtensions().getByType(TestingExtension.class);
+        testing.getSuites().register(SOURCE_SET_NAME, JvmTestSuite.class, suite -> {
+            suite.useJUnit();
+            suite.getDependencies().getImplementation().add(suite.getDependencies().project());
+            suite.getTargets().all(target -> { target.getTestTask().configure(test -> { test.jvmArgs("-XX:+UseG1GC"); }); });
         });
 
-        // TODO: fix usages of IT tests depending on Tests methods so this extension is not necessary
-        GradleUtils.extendSourceSet(project, SourceSet.TEST_SOURCE_SET_NAME, SOURCE_SET_NAME);
+        project.getPluginManager().withPlugin("java", plugin -> {
+            // TODO: fix usages of IT tests depending on Tests methods so this extension is not necessary
+            GradleUtils.extendSourceSet(project, SourceSet.TEST_SOURCE_SET_NAME, SOURCE_SET_NAME);
+        });
     }
 }

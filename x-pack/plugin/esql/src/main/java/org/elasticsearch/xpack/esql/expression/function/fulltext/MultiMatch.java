282f8a25712ba6fe99df31af999d167e77655af8
Svilen Mihaylov <svilen.mihaylov@elastic.co>
2025-04-04 08:54:55 -0400

    Update x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/MultiMatch.java
    Co-authored-by: Carlos Delgado <6339205+carlosdelest@users.noreply.github.com>

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/MultiMatch.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/MultiMatch.java
@@ -141,9 +141,7 @@ public class MultiMatch extends FullTextFunction implements OptionalArgument, Po
         description = """
             Use `MULTI_MATCH` to perform a <<query-dsl-multi-match-query,multi-match query>> on the specified field.
             The multi_match query builds on the match query to allow multi-field queries.
-
-            TODO.
-            TODO.""",
+            """,
         examples = {
             @Example(file = "multi-match-function", tag = "multi-match-with-field"),
             @Example(file = "multi-match-function", tag = "multi-match-with-named-function-params") },

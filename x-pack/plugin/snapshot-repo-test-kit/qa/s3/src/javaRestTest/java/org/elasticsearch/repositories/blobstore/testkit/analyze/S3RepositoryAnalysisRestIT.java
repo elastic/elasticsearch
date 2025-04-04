953bf7abbe63ea9b24b421a5844909d7b4165b9b
David Turner <david.turner@elastic.co>
2025-04-04 16:43:45 +0100

    Validate more regions

--- x-pack/plugin/snapshot-repo-test-kit/qa/s3/src/javaRestTest/java/org/elasticsearch/repositories/blobstore/testkit/analyze/S3RepositoryAnalysisRestIT.java
+++ x-pack/plugin/snapshot-repo-test-kit/qa/s3/src/javaRestTest/java/org/elasticsearch/repositories/blobstore/testkit/analyze/S3RepositoryAnalysisRestIT.java
@@ -6,6 +6,7 @@
  */
 package org.elasticsearch.repositories.blobstore.testkit.analyze;
 
+import fixture.aws.DynamicRegionSupplier;
 import fixture.s3.S3HttpFixture;
 
 import org.elasticsearch.common.settings.Settings;
@@ -16,6 +17,8 @@ import org.junit.ClassRule;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 
+import java.util.function.Supplier;
+
 import static fixture.aws.AwsCredentialsUtils.fixedAccessKey;
 import static org.hamcrest.Matchers.blankOrNullString;
 import static org.hamcrest.Matchers.not;
@@ -24,11 +27,12 @@ public class S3RepositoryAnalysisRestIT extends AbstractRepositoryAnalysisRestTe
 
     static final boolean USE_FIXTURE = Boolean.parseBoolean(System.getProperty("tests.use.fixture", "true"));
 
+    private static final Supplier<String> regionSupplier = new DynamicRegionSupplier();
     public static final S3HttpFixture s3Fixture = new S3HttpFixture(
         USE_FIXTURE,
         "bucket",
         "base_path_integration_tests",
-        fixedAccessKey("s3_test_access_key", "es-test-region", "s3")
+        fixedAccessKey("s3_test_access_key", regionSupplier, "s3")
     );
 
     public static ElasticsearchCluster cluster = ElasticsearchCluster.local()
@@ -37,7 +41,7 @@ public class S3RepositoryAnalysisRestIT extends AbstractRepositoryAnalysisRestTe
         .keystore("s3.client.repo_test_kit.secret_key", System.getProperty("s3SecretKey"))
         .setting("s3.client.repo_test_kit.protocol", () -> "http", (n) -> USE_FIXTURE)
         .setting("s3.client.repo_test_kit.endpoint", s3Fixture::getAddress, (n) -> USE_FIXTURE)
-        .setting("s3.client.repo_test_kit.region", () -> "es-test-region", (n) -> USE_FIXTURE)
+        .setting("s3.client.repo_test_kit.region", regionSupplier, (n) -> USE_FIXTURE)
         .setting("xpack.security.enabled", "false")
         .build();
 

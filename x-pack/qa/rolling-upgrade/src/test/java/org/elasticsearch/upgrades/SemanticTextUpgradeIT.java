296d240dc008b584f49b617a14426e98a82ea3c7
Mike Pellegrini <mike.pellegrini@elastic.co>
2025-04-09 11:30:25 -0400

    Add semantic query tests

--- x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/SemanticTextUpgradeIT.java
+++ x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/SemanticTextUpgradeIT.java
@@ -9,22 +9,29 @@ package org.elasticsearch.upgrades;
 
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 
+import org.apache.lucene.search.join.ScoreMode;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.client.Request;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.mapper.InferenceMetadataFieldsMapper;
+import org.elasticsearch.index.query.NestedQueryBuilder;
 import org.elasticsearch.inference.Model;
 import org.elasticsearch.inference.TaskType;
+import org.elasticsearch.test.rest.ObjectPath;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentFactory;
 import org.elasticsearch.xcontent.XContentType;
+import org.elasticsearch.xpack.core.ml.search.SparseVectorQueryBuilder;
+import org.elasticsearch.xpack.core.ml.search.WeightedToken;
 import org.elasticsearch.xpack.inference.mapper.SemanticTextField;
 import org.elasticsearch.xpack.inference.model.TestModel;
 import org.junit.BeforeClass;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.elasticsearch.xpack.inference.mapper.SemanticTextFieldMapperTests.addSemanticTextInferenceResults;
@@ -56,6 +63,8 @@ public class SemanticTextUpgradeIT extends AbstractUpgradeTestCase {
     public void testSemanticTextOperations() throws Exception {
         switch (CLUSTER_TYPE) {
             case OLD -> createAndPopulateIndex();
+            case MIXED, UPGRADED -> performIndexQueryHighlightOps();
+            default -> throw new UnsupportedOperationException("Unknown cluster type [" + CLUSTER_TYPE + "]");
         }
     }
 
@@ -79,7 +88,13 @@ public class SemanticTextUpgradeIT extends AbstractUpgradeTestCase {
         );
         assertThat(response.isAcknowledged(), equalTo(true));
 
-        indexDoc("doc_1", List.of("a test value"));
+        indexDoc("doc_1", List.of("a test value", "with multiple test values"));
+    }
+
+    private void performIndexQueryHighlightOps() throws IOException {
+        indexDoc("doc_2", List.of("another test value"));
+        ObjectPath queryObjectPath = semanticQuery("test value");
+        assertThat(queryObjectPath.evaluate("hits.total.value"), equalTo(2));
     }
 
     private String getIndexName() {
@@ -105,10 +120,42 @@ public class SemanticTextUpgradeIT extends AbstractUpgradeTestCase {
         addSemanticTextInferenceResults(useLegacyFormat, builder, List.of(semanticTextField));
         builder.endObject();
 
+        RequestOptions requestOptions = RequestOptions.DEFAULT.toBuilder().addParameter("refresh", "true").build();
         Request request = new Request("POST", indexName + "/_doc/" + id);
         request.setJsonEntity(Strings.toString(builder));
+        request.setOptions(requestOptions);
 
         Response response = client().performRequest(request);
         assertOK(response);
+    }
+
+    private ObjectPath semanticQuery(String query) throws IOException {
+        // We can't perform a real semantic query because that requires performing inference, so instead we perform an equivalent nested
+        // query
+        List<WeightedToken> weightedTokens = Arrays.stream(query.split("\\s")).map(t -> new WeightedToken(t, 1.0f)).toList();
+        SparseVectorQueryBuilder sparseVectorQueryBuilder = new SparseVectorQueryBuilder(
+            SemanticTextField.getEmbeddingsFieldName(SEMANTIC_TEXT_FIELD),
+            weightedTokens,
+            null,
+            null,
+            null,
+            null
+        );
+        NestedQueryBuilder nestedQueryBuilder = new NestedQueryBuilder(
+            SemanticTextField.getChunksFieldName(SEMANTIC_TEXT_FIELD),
+            sparseVectorQueryBuilder,
+            ScoreMode.Max
+        );
+
+        XContentBuilder builder = XContentFactory.jsonBuilder();
+        builder.startObject();
+        builder.field("query", nestedQueryBuilder);
+        builder.endObject();
+
+        Request request = new Request("GET", getIndexName() + "/_search");
+        request.setJsonEntity(Strings.toString(builder));
+
+        Response response = client().performRequest(request);
+        return assertOKAndCreateObjectPath(response);
     }
 }

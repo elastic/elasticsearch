3e3d45994de12f42405d506f5d3beeee13ca1b7b
Martijn van Groningen <martijn.v.groningen@gmail.com>
2025-04-10 21:18:36 +0200

    removed unused import

--- modules/data-streams/src/test/java/org/elasticsearch/datastreams/UpdateTimeSeriesRangeServiceTests.java
+++ modules/data-streams/src/test/java/org/elasticsearch/datastreams/UpdateTimeSeriesRangeServiceTests.java
@@ -8,7 +8,6 @@
  */
 package org.elasticsearch.datastreams;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LogEvent;

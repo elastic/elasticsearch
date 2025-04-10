6770b793ede760588c50774e30da842462dbca18
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2025-04-10 14:32:28 -0700

    Add block loader from stored field and source for ip field

--- server/src/test/java/org/elasticsearch/index/mapper/blockloader/IpFieldBlockLoaderTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/blockloader/IpFieldBlockLoaderTests.java
@@ -9,17 +9,64 @@
 
 package org.elasticsearch.index.mapper.blockloader;
 
+import org.apache.lucene.document.InetAddressPoint;
+import org.apache.lucene.util.BytesRef;
+import org.elasticsearch.common.network.InetAddresses;
 import org.elasticsearch.index.mapper.BlockLoaderTestCase;
+import org.elasticsearch.logsdb.datageneration.FieldType;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class IpFieldBlockLoaderTests extends BlockLoaderTestCase {
-    protected IpFieldBlockLoaderTests(Params params) {
-        super(null, params);
+    public IpFieldBlockLoaderTests(Params params) {
+        super(FieldType.IP.toString(), params);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     protected Object expected(Map<String, Object> fieldMapping, Object value, TestContext testContext) {
+        var rawNullValue = (String) fieldMapping.get("null_value");
+        BytesRef nullValue = convert(rawNullValue, null);
+
+        if (value == null) {
+            return convert(null, nullValue);
+        }
+        if (value instanceof String s) {
+            return convert(s, nullValue);
+        }
+
+        if (hasDocValues(fieldMapping, true)) {
+            var resultList = ((List<String>) value).stream()
+                .map(v -> convert(v, nullValue))
+                .filter(Objects::nonNull)
+                .distinct()
+                .sorted()
+                .toList();
+            return maybeFoldList(resultList);
+        }
+
+        // field is stored or using source
+        var resultList = ((List<String>) value).stream().map(v -> convert(v, nullValue)).filter(Objects::nonNull).toList();
+        return maybeFoldList(resultList);
+    }
+
+    private static BytesRef convert(Object value, BytesRef nullValue) {
+        if (value == null) {
+            return nullValue;
+        }
+
+        if (value instanceof String s) {
+            try {
+                var address = InetAddresses.forString(s);
+                return new BytesRef(InetAddressPoint.encode(address));
+            } catch (Exception ex) {
+                // malformed
+                return null;
+            }
+        }
+
         return null;
     }
 }

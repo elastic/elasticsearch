daa528e054d119687d58ff46dfe549c8a3e5f1ae
Mridula <mridula.s@elastic.co>
2025-04-11 11:39:37 +0100

    Resolved no source yaml error

---
+++ x-pack/plugin/search-business-rules/src/yamlRestTest/java/org/elasticsearch/xpack/searchbusinessrules/SearchBusinessRulesClientYamlTestSuiteIT.java
@@ -0,0 +1,18 @@
+package org.elasticsearch.xpack.searchbusinessrules;
+
+import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
+
+import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
+import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
+
+public class SearchBusinessRulesClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
+
+    public SearchBusinessRulesClientYamlTestSuiteIT(final ClientYamlTestCandidate testCandidate) {
+        super(testCandidate);
+    }
+
+    @ParametersFactory
+    public static Iterable<Object[]> parameters() throws Exception {
+        return ESClientYamlSuiteTestCase.createParameters();
+    }
+} 

3ecb1bfc40ef97c7bb00693219b601d8329d2ad5
gmarouli <mary.gouseti@elastic.co>
2025-04-11 14:46:00 +0300

    Test fix: align timeouts in `testDataStreamLifecycleDownsampleRollingRestart` (#123769)

--- x-pack/plugin/downsample/src/internalClusterTest/java/org/elasticsearch/xpack/downsample/DataStreamLifecycleDownsampleDisruptionIT.java
+++ x-pack/plugin/downsample/src/internalClusterTest/java/org/elasticsearch/xpack/downsample/DataStreamLifecycleDownsampleDisruptionIT.java
@@ -115,7 +115,7 @@ public class DataStreamLifecycleDownsampleDisruptionIT extends ESIntegTestCase {
                 return true;
             }
             return false;
-        });
+        }, timeout);
         safeAwait(listener, timeout.millis(), TimeUnit.MILLISECONDS);
     }
 }

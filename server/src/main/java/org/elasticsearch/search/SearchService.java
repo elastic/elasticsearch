74bdf182edcc4c6900cd01b5d27a35e2d70c6d40
Armin Braun <me@obrown.io>
2025-04-14 22:30:39 +0200

    Avoid walking the complete list of search contexts on shard creation (#123855) (#126798)
    This I found in the many-shards benchmark during some manual testing.
    Creating indices slows down measurably when there's concurrent searches
    going on. Interestingly enough, the bulk of the cost is coming from this
    hook. This makes sense to some extend because the map can quickly grow
    to a massive size as it scales as O(shards_searched_on_average *
    concurrent_searches) and a CHM generally is anything but cheap to
    iterate over.

--- server/src/main/java/org/elasticsearch/search/SearchService.java
+++ server/src/main/java/org/elasticsearch/search/SearchService.java
@@ -30,6 +30,7 @@ import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.support.TransportActions;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver.ResolvedExpression;
+import org.elasticsearch.cluster.routing.RecoverySource;
 import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.CheckedSupplier;
@@ -511,7 +512,7 @@ public class SearchService extends AbstractLifecycleComponent implements IndexEv
         // to stop searches to restore full availability as fast as possible. A known scenario here is that we lost connection to master
         // or master(s) were restarted.
         assert routing.initializing();
-        if (routing.isRelocationTarget() == false) {
+        if (routing.isRelocationTarget() == false && routing.recoverySource() != RecoverySource.EmptyStoreRecoverySource.INSTANCE) {
             freeAllContextsForShard(routing.shardId());
         }
     }

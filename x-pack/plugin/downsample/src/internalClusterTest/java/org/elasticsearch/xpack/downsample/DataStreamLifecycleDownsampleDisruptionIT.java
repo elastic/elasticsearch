edf7cd485b7436de9d1b564239be60e96ad8a6fa
Mary Gouseti <mary.gouseti@elastic.co>
2025-04-14 19:53:59 +0300

    Test fix: align timeouts in `testDataStreamLifecycleDownsampleRollingRestart` (#123769) (#126682)
    Recently we changed the implementation of
    `testDataStreamLifecycleDownsampleRollingRestart` to use a temporary
    state listener. We missed that the listener also had a timeout that was
    quite shorter than the `safeGet` timeout we were configuring. In this PR
    we align these two timeouts.
    
    Fixes: #123769
    (cherry picked from commit e461717627ea7f73b8e1c278b75f08d0e13e3ea3)
    
    # Conflicts:
    #	muted-tests.yml
    #	x-pack/plugin/downsample/src/internalClusterTest/java/org/elasticsearch/xpack/downsample/DataStreamLifecycleDownsampleDisruptionIT.java

--- x-pack/plugin/downsample/src/internalClusterTest/java/org/elasticsearch/xpack/downsample/DataStreamLifecycleDownsampleDisruptionIT.java
+++ x-pack/plugin/downsample/src/internalClusterTest/java/org/elasticsearch/xpack/downsample/DataStreamLifecycleDownsampleDisruptionIT.java
@@ -117,7 +117,7 @@ public class DataStreamLifecycleDownsampleDisruptionIT extends ESIntegTestCase {
                 return true;
             }
             return false;
-        });
+        }, timeout);
         safeAwait(listener, timeout.millis(), TimeUnit.MILLISECONDS);
     }
 }

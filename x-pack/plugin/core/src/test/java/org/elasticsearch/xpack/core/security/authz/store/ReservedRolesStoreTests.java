db1878859e13b49ba133066da2d3697729aaf27a
Paul Tavares <paul.tavares@elastic.co>
2025-04-14 17:12:38 -0400

    Added MS Defender index privileges

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStoreTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStoreTests.java
@@ -1032,6 +1032,8 @@ public class ReservedRolesStoreTests extends ESTestCase {
         Arrays.asList(
             "logs-sentinel_one." + randomAlphaOfLength(randomIntBetween(0, 13)),
             "logs-crowdstrike." + randomAlphaOfLength(randomIntBetween(0, 13))
+            "logs-microsoft_defender_endpoint." + randomAlphaOfLength(randomIntBetween(0, 13))
+            "logs-m365_defender." + randomAlphaOfLength(randomIntBetween(0, 13))
         ).forEach((index) -> {
             final IndexAbstraction indexAbstraction = mockIndexAbstraction(index);
             assertThat(kibanaRole.indices().allowedIndicesMatcher("indices:foo").test(indexAbstraction), is(false));

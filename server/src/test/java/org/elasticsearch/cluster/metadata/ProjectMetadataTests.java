bec18b31cb4c8643699fb4dab841dbc224f2fc55
Keith Massey <keith.massey@elastic.co>
2025-04-16 14:44:17 -0500

    fixing ProjectMetadataTests

--- server/src/test/java/org/elasticsearch/cluster/metadata/ProjectMetadataTests.java
+++ server/src/test/java/org/elasticsearch/cluster/metadata/ProjectMetadataTests.java
@@ -317,6 +317,7 @@ public class ProjectMetadataTests extends ESTestCase {
                             "replicated": false,
                             "system": false,
                             "allow_custom_routing": false,
+                            "settings" : { },
                             "failure_rollover_on_write": false,
                             "rollover_on_write": false
                           }

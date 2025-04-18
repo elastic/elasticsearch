85749d606c85457905d3b35ed0c964d882876e2c
Nik Everett <nik9000@gmail.com>
2025-04-18 13:09:38 -0400

    Add benchmark script (#126596)
    Adds a simple script to run benchmarks for ESQL and collect their
    results. The script has a `--test` mode which takes about ten minutes.
    Running without `--test` takes a four hours fifteen minutes.
    
    To speed up `--test` I reworked the "self test" that each benchmark runs
    to be optional and disabled in `--test` mode.

--- benchmarks/src/test/java/org/elasticsearch/benchmark/compute/operator/AggregatorBenchmarkTests.java
+++ benchmarks/src/test/java/org/elasticsearch/benchmark/compute/operator/AggregatorBenchmarkTests.java
@@ -11,8 +11,8 @@ package org.elasticsearch.benchmark.compute.operator;
 
 import org.elasticsearch.test.ESTestCase;
 
-public class EvalBenchmarkTests extends ESTestCase {
+public class AggregatorBenchmarkTests extends ESTestCase {
     public void test() {
-        EvalBenchmark.selfTest();
+        AggregatorBenchmark.selfTest();
     }
 }

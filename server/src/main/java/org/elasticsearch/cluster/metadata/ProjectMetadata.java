20732ae6bfa5fc7442b4b71a684aa709e4e33d05
Alexey Ivanov <alexey.ivanov@elastic.co>
2025-04-23 17:35:34 +0100

    Fix tests

--- server/src/main/java/org/elasticsearch/cluster/metadata/ProjectMetadata.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/ProjectMetadata.java
@@ -2182,10 +2182,16 @@ public class ProjectMetadata implements Iterable<IndexMetadata>, Diffable<Projec
             ),
             indices,
             customs,
+            multiProject ?
+                Iterators.single((builder, params) -> {
+                    builder.startObject("settings");
+                    settings.toXContent(builder, new ToXContent.MapParams(Collections.singletonMap("flat_settings", "true")));
+                    return builder.endObject();
+                })
+                : Collections.emptyIterator(),
             multiProject
                 ? ChunkedToXContentHelper.object("reserved_state", reservedStateMetadata().values().iterator())
-                : Collections.emptyIterator(),
-            multiProject ? ChunkedToXContentHelper.object("settings", Iterators.single(settings)) : Collections.emptyIterator()
+                : Collections.emptyIterator()
         );
     }
 

a0634d4a688736a12097971d5102ab6c180362d0
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-04-23 09:44:51 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapper.java
@@ -2199,9 +2199,11 @@ public class DenseVectorFieldMapper extends FieldMapper {
             BitSetProducer parentFilter
         ) {
             elementType.checkDimensions(dims, queryVector.length);
-            Query knnQuery = PatienceKnnVectorQuery.fromByteQuery(parentFilter != null
-                ? new ESDiversifyingChildrenByteKnnVectorQuery(name(), queryVector, filter, k, numCands, parentFilter)
-                : new ESKnnByteVectorQuery(name(), queryVector, k, numCands, filter));
+            Query knnQuery = PatienceKnnVectorQuery.fromByteQuery(
+                parentFilter != null
+                    ? new ESDiversifyingChildrenByteKnnVectorQuery(name(), queryVector, filter, k, numCands, parentFilter)
+                    : new ESKnnByteVectorQuery(name(), queryVector, k, numCands, filter)
+            );
             if (similarityThreshold != null) {
                 knnQuery = new VectorSimilarityQuery(
                     knnQuery,
@@ -2226,9 +2228,11 @@ public class DenseVectorFieldMapper extends FieldMapper {
                 float squaredMagnitude = VectorUtil.dotProduct(queryVector, queryVector);
                 elementType.checkVectorMagnitude(similarity, ElementType.errorByteElementsAppender(queryVector), squaredMagnitude);
             }
-            Query knnQuery = PatienceKnnVectorQuery.fromByteQuery(parentFilter != null
-                ? new ESDiversifyingChildrenByteKnnVectorQuery(name(), queryVector, filter, k, numCands, parentFilter)
-                : new ESKnnByteVectorQuery(name(), queryVector, k, numCands, filter));
+            Query knnQuery = PatienceKnnVectorQuery.fromByteQuery(
+                parentFilter != null
+                    ? new ESDiversifyingChildrenByteKnnVectorQuery(name(), queryVector, filter, k, numCands, parentFilter)
+                    : new ESKnnByteVectorQuery(name(), queryVector, k, numCands, filter)
+            );
             if (similarityThreshold != null) {
                 knnQuery = new VectorSimilarityQuery(
                     knnQuery,
@@ -2279,9 +2283,11 @@ public class DenseVectorFieldMapper extends FieldMapper {
                 adjustedK = Math.min((int) Math.ceil(k * oversample), OVERSAMPLE_LIMIT);
                 numCands = Math.max(adjustedK, numCands);
             }
-            Query knnQuery = PatienceKnnVectorQuery.fromFloatQuery(parentFilter != null
-                ? new ESDiversifyingChildrenFloatKnnVectorQuery(name(), queryVector, filter, adjustedK, numCands, parentFilter)
-                : new ESKnnFloatVectorQuery(name(), queryVector, adjustedK, numCands, filter));
+            Query knnQuery = PatienceKnnVectorQuery.fromFloatQuery(
+                parentFilter != null
+                    ? new ESDiversifyingChildrenFloatKnnVectorQuery(name(), queryVector, filter, adjustedK, numCands, parentFilter)
+                    : new ESKnnFloatVectorQuery(name(), queryVector, adjustedK, numCands, filter)
+            );
             if (rescore) {
                 knnQuery = new RescoreKnnVectorQuery(
                     name(),

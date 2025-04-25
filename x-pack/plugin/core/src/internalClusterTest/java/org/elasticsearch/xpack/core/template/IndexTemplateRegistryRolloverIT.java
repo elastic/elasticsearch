c72d00fd3976bb163f8354dbf0714555e5c76d6c
Niels Bauman <33722607+nielsbauman@users.noreply.github.com>
2025-04-25 10:07:52 +0200

    Don't start a new node in `InternalTestCluster#getClient` (#127318)
    This method would default to starting a new node when the cluster was
    empty. This is pretty trappy as `getClient()` (or things like
    `getMaster()` that depend on `getClient()`) don't look at all like
    something that would start a new node.
    
    In any case, the intention of tests is much clearer when they explicitly
    define a cluster configuration.

--- x-pack/plugin/core/src/internalClusterTest/java/org/elasticsearch/xpack/core/template/IndexTemplateRegistryRolloverIT.java
+++ x-pack/plugin/core/src/internalClusterTest/java/org/elasticsearch/xpack/core/template/IndexTemplateRegistryRolloverIT.java
@@ -37,7 +37,6 @@ import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 
-@ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST, numDataNodes = 0, numClientNodes = 0)
 public class IndexTemplateRegistryRolloverIT extends ESIntegTestCase {
 
     private ClusterService clusterService;

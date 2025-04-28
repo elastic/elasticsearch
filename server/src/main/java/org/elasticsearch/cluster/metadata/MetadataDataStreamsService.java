912de5907d80d0573f3fcbb3d87a5b7e33dd23b0
Keith Massey <keith.massey@elastic.co>
2025-04-28 08:40:56 -0500

    Project-centric code review feedback

--- server/src/main/java/org/elasticsearch/cluster/metadata/MetadataDataStreamsService.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/MetadataDataStreamsService.java
@@ -21,7 +21,6 @@ import org.elasticsearch.cluster.ClusterStateTaskExecutor;
 import org.elasticsearch.cluster.ClusterStateUpdateTask;
 import org.elasticsearch.cluster.ProjectState;
 import org.elasticsearch.cluster.SimpleBatchedAckListenerTaskExecutor;
-import org.elasticsearch.cluster.project.ProjectResolver;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.cluster.service.MasterServiceTaskQueue;
 import org.elasticsearch.common.Priority;
@@ -146,7 +145,7 @@ public class MetadataDataStreamsService {
                 ClusterState clusterState
             ) throws Exception {
 
-                ProjectMetadata projectMetadata = clusterState.projectState(updateSettingsTask.projectId).metadata();
+                ProjectMetadata projectMetadata = clusterState.metadata().getProject(updateSettingsTask.projectId);
                 ProjectMetadata.Builder projectMetadataBuilder = ProjectMetadata.builder(projectMetadata);
                 Map<String, DataStream> dataStreamMap = projectMetadata.dataStreams();
                 DataStream dataStream = dataStreamMap.get(updateSettingsTask.dataStreamName);
@@ -408,7 +407,7 @@ public class MetadataDataStreamsService {
     }
 
     public void updateSettings(
-        ProjectResolver projectResolver,
+        ProjectId projectId,
         TimeValue masterNodeTimeout,
         TimeValue ackTimeout,
         String dataStreamName,
@@ -417,7 +416,7 @@ public class MetadataDataStreamsService {
     ) {
         updateSettingsTaskQueue.submitTask(
             "updating settings on data stream",
-            new UpdateSettingsTask(projectResolver.getProjectId(), dataStreamName, settingsOverrides, ackTimeout, listener),
+            new UpdateSettingsTask(projectId, dataStreamName, settingsOverrides, ackTimeout, listener),
             masterNodeTimeout
         );
     }

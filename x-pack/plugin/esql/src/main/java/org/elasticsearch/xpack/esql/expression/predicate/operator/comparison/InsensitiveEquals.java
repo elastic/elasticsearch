55e144be8ff0010f7f0b6ed263b56b6171b327aa
Nik Everett <nik9000@gmail.com>
2025-04-29 16:35:42 -0400

    ESQL: Fix case insensitive comparisons to ""
    This fixes the compute engine side of case insensitive ==. You can
    trigger it by writing:
    ```
    FROM foo
    | WHERE TO_LOWER(field) == ""
    ```
    
    But *only* when we can't push the comparison to lucene - like if `field`
    is not indexed or is a `text` field.
    
    Closes #127431

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/predicate/operator/comparison/InsensitiveEquals.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/predicate/operator/comparison/InsensitiveEquals.java
@@ -7,8 +7,10 @@
 package org.elasticsearch.xpack.esql.expression.predicate.operator.comparison;
 
 import org.apache.lucene.util.BytesRef;
+import org.apache.lucene.util.automaton.Automata;
 import org.apache.lucene.util.automaton.Automaton;
 import org.apache.lucene.util.automaton.ByteRunAutomaton;
+import org.apache.lucene.util.automaton.Operations;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.lucene.BytesRefs;
@@ -83,6 +85,10 @@ public class InsensitiveEquals extends InsensitiveBinaryComparison {
     }
 
     public static Automaton automaton(BytesRef val) {
+        if (val.length == 0) {
+            // toCaseInsensitiveString doesn't match empty strings properly so let's do it ourselves
+            return Automata.makeEmptyString();
+        }
         return AutomatonQueries.toCaseInsensitiveString(val.utf8ToString());
     }
 

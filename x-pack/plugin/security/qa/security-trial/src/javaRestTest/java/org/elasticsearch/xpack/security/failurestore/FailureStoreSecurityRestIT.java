2d5392036bf63384be0e9da3255ee51b17ccfb7a
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-04-29 06:29:33 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/security/qa/security-trial/src/javaRestTest/java/org/elasticsearch/xpack/security/failurestore/FailureStoreSecurityRestIT.java
+++ x-pack/plugin/security/qa/security-trial/src/javaRestTest/java/org/elasticsearch/xpack/security/failurestore/FailureStoreSecurityRestIT.java
@@ -1974,11 +1974,7 @@ public class FailureStoreSecurityRestIT extends ESRestTestCase {
             }
             """, aliasName));
         expectSearch(username, new Search(aliasName + "::failures"), failuresDocId, otherFailuresDocId);
-        expectSearchThrows(
-            username,
-            new Search(randomFrom(aliasName + "::data", aliasName, dataIndexName, failureIndexName)),
-            403
-        );
+        expectSearchThrows(username, new Search(randomFrom(aliasName + "::data", aliasName, dataIndexName, failureIndexName)), 403);
 
         createOrUpdateRoleAndApiKey(username, roleName, Strings.format("""
             {

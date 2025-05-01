084542a69097154217ab0f55172e1f6c1cee0938
Ankita Kumar <ankita.kumar@elastic.co>
2025-05-01 16:56:14 -0400

    Account for time taken to write index buffers in IndexingMemoryController (#126786)
    This PR adds to the indexing write load, the time taken to flush write indexing buffers using the indexing threads (this is done here to push back on indexing)
    
    This changes the semantics of InternalIndexingStats#recentIndexMetric and InternalIndexingStats#peakIndexMetric  to more accurately account for load on the indexing thread. Address ES-11356.

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -234,6 +234,7 @@ public class TransportVersions {
     public static final TransportVersion AGGREGATE_METRIC_DOUBLE_BLOCK = def(9_067_00_0);
     public static final TransportVersion PINNED_RETRIEVER = def(9_068_0_00);
     public static final TransportVersion ML_INFERENCE_SAGEMAKER = def(9_069_0_00);
+    public static final TransportVersion WRITE_LOAD_INCLUDES_BUFFER_WRITES = def(9_070_00_0);
 
     /*
      * STOP! READ THIS FIRST! No, really,

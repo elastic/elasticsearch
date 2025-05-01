4c012cc65efe6a1a59d7eea71f7423b21f933b6a
Albert Zaharovits <albert.zaharovits@elastic.co>
2025-05-01 21:51:54 +0300

    Fix TPMST

--- server/src/test/java/org/elasticsearch/index/engine/ThreadPoolMergeSchedulerTests.java
+++ server/src/test/java/org/elasticsearch/index/engine/ThreadPoolMergeSchedulerTests.java
@@ -460,7 +460,7 @@ public class ThreadPoolMergeSchedulerTests extends ESTestCase {
                         // also check the same for the thread-pool executor
                         assertThat(threadPoolMergeExecutorService.getRunningMergeTasks().size(), is(mergeSchedulerMaxThreadCount));
                         // queued merge tasks do not include backlogged merges
-                        assertThat(threadPoolMergeExecutorService.getMergeTasksQueueLength().size(), is(0));
+                        assertThat(threadPoolMergeExecutorService.getMergeTasksQueueLength(), is(0));
                         // also check thread-pool stats for the same
                         // there are active thread-pool threads waiting for the backlogged merge tasks to be re-enqueued
                         int activeMergeThreads = Math.min(mergeCount - finalCompletedMergesCount, mergeExecutorThreadCount);
@@ -481,7 +481,7 @@ public class ThreadPoolMergeSchedulerTests extends ESTestCase {
                         // also check thread-pool executor for the same
                         assertThat(threadPoolMergeExecutorService.getRunningMergeTasks().size(), is(finalRemainingMergesCount));
                         // no more backlogged merges
-                        assertThat(threadPoolMergeExecutorService.getMergeTasksQueueLength().size(), is(0));
+                        assertThat(threadPoolMergeExecutorService.getMergeTasksQueueLength(), is(0));
                         // also check thread-pool stats for the same
                         assertThat(threadPoolExecutor.getActiveCount(), is(finalRemainingMergesCount));
                         assertThat(threadPoolExecutor.getQueue().size(), is(0));

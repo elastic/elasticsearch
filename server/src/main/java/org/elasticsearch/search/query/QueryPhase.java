99a094d30e4702dfa6fed2e19e4a7d6d5c5cc4eb
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-05-01 08:36:50 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/search/query/QueryPhase.java
+++ server/src/main/java/org/elasticsearch/search/query/QueryPhase.java
@@ -13,7 +13,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.index.MultiReader;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Collector;

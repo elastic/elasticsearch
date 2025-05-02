944c5cc7b28a175ada8d930ebe073930ee47ac2b
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2025-05-02 15:37:16 -0400

    Fixing test failure #127657

--- server/src/test/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapperTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapperTests.java
@@ -904,14 +904,17 @@ public class DenseVectorFieldMapperTests extends MapperTestCase {
     }
 
     public void testRescoreVectorOldIndexVersion() {
-        IndexVersion incompatibleVersion = IndexVersionUtils.randomVersionBetween(
-            random(),
+        IndexVersion incompatibleVersion = randomFrom(
             IndexVersionUtils.randomVersionBetween(
                 random(),
                 IndexVersionUtils.getLowestReadCompatibleVersion(),
                 IndexVersionUtils.getPreviousVersion(IndexVersions.ADD_RESCORE_PARAMS_TO_QUANTIZED_VECTORS_BACKPORT_8_X)
             ),
-            IndexVersionUtils.getPreviousVersion(IndexVersions.ADD_RESCORE_PARAMS_TO_QUANTIZED_VECTORS)
+            IndexVersionUtils.randomVersionBetween(
+                random(),
+                IndexVersions.UPGRADE_TO_LUCENE_10_0_0,
+                IndexVersionUtils.getPreviousVersion(IndexVersions.ADD_RESCORE_PARAMS_TO_QUANTIZED_VECTORS)
+            )
         );
         for (String indexType : List.of("int8_hnsw", "int8_flat", "int4_hnsw", "int4_flat", "bbq_hnsw", "bbq_flat")) {
             expectThrows(

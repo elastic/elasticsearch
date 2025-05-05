2af86c8b6c340f6c24cdd7120bd3d2f812500a52
Niels Bauman <nielsbauman@gmail.com>
2025-05-05 12:19:30 +0200

    Don't include MP module

--- x-pack/qa/multi-project/xpack-rest-tests-with-multiple-projects/src/yamlRestTest/java/org/elasticsearch/multiproject/test/XpackWithMultipleProjectsClientYamlTestSuiteIT.java
+++ x-pack/qa/multi-project/xpack-rest-tests-with-multiple-projects/src/yamlRestTest/java/org/elasticsearch/multiproject/test/XpackWithMultipleProjectsClientYamlTestSuiteIT.java
@@ -26,7 +26,6 @@ public class XpackWithMultipleProjectsClientYamlTestSuiteIT extends MultipleProj
     public static ElasticsearchCluster cluster = ElasticsearchCluster.local()
         .distribution(DistributionType.DEFAULT)
         .name("yamlRestTest")
-        .module("test-multi-project")
         .setting("test.multi_project.enabled", "true")
         .setting("xpack.ml.enabled", "true")
         .setting("xpack.security.enabled", "true")

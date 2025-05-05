bb538a6782e4297a6edc08bae69c4697df513cf8
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2025-05-05 17:45:24 +0300

    Suppress warning for forbidden usage of System.out in tests

--- modules/ingest-otel/src/test/java/org/elasticsearch/ingest/otel/ResourceAttributesTests.java
+++ modules/ingest-otel/src/test/java/org/elasticsearch/ingest/otel/ResourceAttributesTests.java
@@ -9,6 +9,7 @@
 
 package org.elasticsearch.ingest.otel;
 
+import org.elasticsearch.core.SuppressForbidden;
 import org.elasticsearch.test.ESTestCase;
 
 import java.util.HashSet;
@@ -17,6 +18,7 @@ import java.util.Set;
 
 public class ResourceAttributesTests extends ESTestCase {
 
+    @SuppressForbidden(reason = "Used specifically for the output. Only meant to be run manually, not through CI.")
     public void testResourceAttributesWebCrawler() {
         Set<String> resourceAttributes = OTelSemConvWebCrawler.collectOTelSemConvResourceAttributes();
         System.out.println("Resource Attributes: " + resourceAttributes.size());
@@ -25,6 +27,7 @@ public class ResourceAttributesTests extends ESTestCase {
         }
     }
 
+    @SuppressForbidden(reason = "Used specifically for the output. Only meant to be run manually, not through CI.")
     public void testEcsToOTelAttributeNames() {
         Map<String, String> attributes = EcsFieldsDiscoverer.getInstance().getEcsToOTelAttributeNames();
         System.out.println("ECS to OTel attribute mappings: " + attributes.size());
@@ -43,38 +46,42 @@ public class ResourceAttributesTests extends ESTestCase {
             }
         });
         latestEcsOTelResourceAttributes.addAll(EcsFieldsDiscoverer.getInstance().getEcsResourceFields());
-        if (latestEcsOTelResourceAttributes.equals(EcsOTelResourceAttributes.LATEST)) {
-            System.out.println("ECS to OTel resource attributes are up to date.");
-        } else {
-            System.out.println("ECS to OTel resource attributes are not up to date.");
-            // find and print the diff
-            Set<String> addedAttributes = new HashSet<>(latestEcsOTelResourceAttributes);
-            addedAttributes.removeAll(EcsOTelResourceAttributes.LATEST);
-            if (addedAttributes.isEmpty() == false) {
-                System.out.println();
-                System.out.println("The current resource attributes set doesn't contain the following attributes:");
-                System.out.println("-----------------------------------------------------------------------------");
-                for (String attribute : addedAttributes) {
-                    System.out.println(attribute);
-                }
-                System.out.println("-----------------------------------------------------------------------------");
-                System.out.println();
-            }
-            Set<String> removedAttributes = new HashSet<>(EcsOTelResourceAttributes.LATEST);
-            removedAttributes.removeAll(latestEcsOTelResourceAttributes);
-            if (removedAttributes.isEmpty() == false) {
-                System.out.println();
-                System.out.println("The following attributes are no longer considered resource attributes:");
-                System.out.println("----------------------------------------------------------------------");
-                for (String attribute : removedAttributes) {
-                    System.out.println(attribute);
-                }
-                System.out.println("----------------------------------------------------------------------");
-                System.out.println();
-            }
-            System.out.println("Consider updating EcsOTelResourceAttributes accordingly");
-            System.out.println();
-            fail("ECS to OTel resource attributes are not up to date");
+        boolean upToDate = latestEcsOTelResourceAttributes.equals(EcsOTelResourceAttributes.LATEST);
+        if (upToDate == false) {
+            printComparisonResults(latestEcsOTelResourceAttributes);
         }
+        assertTrue("ECS-to-OTel resource attributes set is not up to date.", upToDate);
+    }
+
+    @SuppressForbidden(reason = "Output is used for updating the resource attributes set, not running in the normal PR CI build pipeline")
+    private static void printComparisonResults(Set<String> latestEcsOTelResourceAttributes) {
+        // find and print the diff
+        Set<String> addedAttributes = new HashSet<>(latestEcsOTelResourceAttributes);
+        addedAttributes.removeAll(EcsOTelResourceAttributes.LATEST);
+        if (addedAttributes.isEmpty() == false) {
+            System.out.println();
+            System.out.println("The current resource attributes set doesn't contain the following attributes:");
+            System.out.println("-----------------------------------------------------------------------------");
+            for (String attribute : addedAttributes) {
+                System.out.println(attribute);
+            }
+            System.out.println("-----------------------------------------------------------------------------");
+            System.out.println();
+        }
+        Set<String> removedAttributes = new HashSet<>(EcsOTelResourceAttributes.LATEST);
+        removedAttributes.removeAll(latestEcsOTelResourceAttributes);
+        if (removedAttributes.isEmpty() == false) {
+            System.out.println();
+            System.out.println("The following attributes are no longer considered resource attributes:");
+            System.out.println("----------------------------------------------------------------------");
+            for (String attribute : removedAttributes) {
+                System.out.println(attribute);
+            }
+            System.out.println("----------------------------------------------------------------------");
+            System.out.println();
+        }
+        System.out.println("Consider updating EcsOTelResourceAttributes accordingly");
+        System.out.println();
+        fail("ECS to OTel resource attributes are not up to date");
     }
 }

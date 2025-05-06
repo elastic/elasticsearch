774941359f25c816c9e1c84f9dca3c7875598859
Ryan Ernst <ryan@iernst.net>
2025-05-06 13:51:53 -0700

    fix forbidden

--- server/src/main/java/org/elasticsearch/bootstrap/ElasticsearchUncaughtExceptionHandler.java
+++ server/src/main/java/org/elasticsearch/bootstrap/ElasticsearchUncaughtExceptionHandler.java
@@ -11,6 +11,7 @@ package org.elasticsearch.bootstrap;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.core.SuppressForbidden;
 
 import java.io.IOError;
 
@@ -58,6 +59,7 @@ class ElasticsearchUncaughtExceptionHandler implements Thread.UncaughtExceptionH
         logger.error(message, t);
     }
 
+    @SuppressForbidden(reason = "intentionally halting")
     void halt(int status) {
         // we halt to prevent shutdown hooks from running
         Runtime.getRuntime().halt(status);

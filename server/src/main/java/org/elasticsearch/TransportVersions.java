cc4e6d3de37747353f9e7433fccc249a072db85b
Nik Everett <nik9000@gmail.com>
2025-05-07 17:43:26 -0400

    ESQL: Claim transport version for partitioning (#127775)
    Claims a transport version to backport #125739.

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -173,6 +173,7 @@ public class TransportVersions {
     public static final TransportVersion INTRODUCE_FAILURES_DEFAULT_RETENTION_BACKPORT_8_19 = def(8_841_0_26);
     public static final TransportVersion RESCORE_VECTOR_ALLOW_ZERO_BACKPORT_8_19 = def(8_841_0_27);
     public static final TransportVersion INFERENCE_ADD_TIMEOUT_PUT_ENDPOINT_8_19 = def(8_841_0_28);
+    public static final TransportVersion ESQL_REPORT_SHARD_PARTITIONING_8_19 = def(8_841_0_29);
     public static final TransportVersion V_9_0_0 = def(9_000_0_09);
     public static final TransportVersion INITIAL_ELASTICSEARCH_9_0_1 = def(9_000_0_10);
     public static final TransportVersion INITIAL_ELASTICSEARCH_9_0_2 = def(9_000_0_11);

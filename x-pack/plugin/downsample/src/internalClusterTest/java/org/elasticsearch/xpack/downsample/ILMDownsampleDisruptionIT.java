d7c769e5a5beec61a55043fc48a242d0a46d0240
Mary Gouseti <mary.gouseti@elastic.co>
2025-05-08 09:04:41 +0300

    Change the handling of passthrough dimenensions (#127752)
    When downsampling an index that has a mapping with passthrough dimensions the downsampling process identifies the wrapper object as a dimension and it fails when it tried to retrieve the type.
    
    We did some prework to establish a shared framework in the internalClusterTest. For now it only includes setting up time series data stream helpers and a limited assertion helper for dimensions and metrics. This allows us to setup an internalClusterTest that captures this issue during downsampling in #125156.
    
    To fix this we refine the check that determines if a field is dimension, to skip wrapper field.
    
    Fixes #125156.

--- x-pack/plugin/downsample/src/internalClusterTest/java/org/elasticsearch/xpack/downsample/ILMDownsampleDisruptionIT.java
+++ x-pack/plugin/downsample/src/internalClusterTest/java/org/elasticsearch/xpack/downsample/ILMDownsampleDisruptionIT.java
@@ -9,24 +9,16 @@ package org.elasticsearch.xpack.downsample;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.action.DocWriteRequest;
 import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
 import org.elasticsearch.action.admin.indices.get.GetIndexResponse;
 import org.elasticsearch.action.admin.indices.settings.get.GetSettingsRequest;
 import org.elasticsearch.action.admin.indices.settings.put.UpdateSettingsRequest;
-import org.elasticsearch.action.bulk.BulkItemResponse;
-import org.elasticsearch.action.bulk.BulkRequestBuilder;
-import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.downsample.DownsampleConfig;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.time.DateFormatter;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.IndexMode;
 import org.elasticsearch.index.IndexSettings;
-import org.elasticsearch.index.engine.VersionConflictEngineException;
 import org.elasticsearch.index.mapper.DateFieldMapper;
 import org.elasticsearch.index.query.MatchAllQueryBuilder;
 import org.elasticsearch.plugins.Plugin;
@@ -56,6 +48,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 import static org.elasticsearch.core.Strings.format;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
@@ -65,13 +58,8 @@ import static org.elasticsearch.xpack.core.rollup.ConfigTestHelpers.randomInterv
 import static org.hamcrest.Matchers.equalTo;
 
 @ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST, numDataNodes = 0, numClientNodes = 4)
-public class ILMDownsampleDisruptionIT extends ESIntegTestCase {
+public class ILMDownsampleDisruptionIT extends DownsamplingIntegTestCase {
     private static final Logger logger = LogManager.getLogger(ILMDownsampleDisruptionIT.class);
-    private static final DateFormatter DATE_FORMATTER = DateFormatter.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-    private static final String FIELD_TIMESTAMP = "@timestamp";
-    private static final String FIELD_DIMENSION_1 = "dimension_kw";
-    private static final String FIELD_DIMENSION_2 = "dimension_long";
-    private static final String FIELD_METRIC_COUNTER = "counter";
     private static final String POLICY_NAME = "mypolicy";
     public static final int DOC_COUNT = 10_000;
 
@@ -99,7 +87,7 @@ public class ILMDownsampleDisruptionIT extends ESIntegTestCase {
 
     public void setup(final String sourceIndex, int numOfShards, int numOfReplicas, long startTime) throws IOException {
         final Settings.Builder settings = indexSettings(numOfShards, numOfReplicas).put(IndexSettings.MODE.getKey(), IndexMode.TIME_SERIES)
-            .putList(IndexMetadata.INDEX_ROUTING_PATH.getKey(), List.of(FIELD_DIMENSION_1))
+            .putList(IndexMetadata.INDEX_ROUTING_PATH.getKey(), List.of(FIELD_DIMENSION_KEYWORD))
             .put(
                 IndexSettings.TIME_SERIES_START_TIME.getKey(),
                 DateFieldMapper.DEFAULT_DATE_TIME_FORMATTER.formatMillis(Instant.ofEpochMilli(startTime).toEpochMilli())
@@ -113,10 +101,10 @@ public class ILMDownsampleDisruptionIT extends ESIntegTestCase {
         final XContentBuilder mapping = jsonBuilder().startObject().startObject("_doc").startObject("properties");
         mapping.startObject(FIELD_TIMESTAMP).field("type", "date").endObject();
 
-        mapping.startObject(FIELD_DIMENSION_1).field("type", "keyword").field("time_series_dimension", true).endObject();
-        mapping.startObject(FIELD_DIMENSION_2).field("type", "long").field("time_series_dimension", true).endObject();
+        mapping.startObject(FIELD_DIMENSION_KEYWORD).field("type", "keyword").field("time_series_dimension", true).endObject();
+        mapping.startObject(FIELD_DIMENSION_LONG).field("type", "long").field("time_series_dimension", true).endObject();
 
-        mapping.startObject(FIELD_METRIC_COUNTER)
+        mapping.startObject(FIELD_METRIC_COUNTER_DOUBLE)
             .field("type", "double") /* numeric label indexed as a metric */
             .field("time_series_metric", "counter")
             .endObject();
@@ -149,20 +137,24 @@ public class ILMDownsampleDisruptionIT extends ESIntegTestCase {
         long startTime = LocalDateTime.parse("1993-09-09T18:00:00").atZone(ZoneId.of("UTC")).toInstant().toEpochMilli();
         setup(sourceIndex, 1, 0, startTime);
         final DownsampleConfig config = new DownsampleConfig(randomInterval());
-        final SourceSupplier sourceSupplier = () -> {
+        final Supplier<XContentBuilder> sourceSupplier = () -> {
             final String ts = randomDateForInterval(config.getInterval(), startTime);
             double counterValue = DATE_FORMATTER.parseMillis(ts);
             final List<String> dimensionValues = new ArrayList<>(5);
             for (int j = 0; j < randomIntBetween(1, 5); j++) {
                 dimensionValues.add(randomAlphaOfLength(6));
             }
-            return XContentFactory.jsonBuilder()
-                .startObject()
-                .field(FIELD_TIMESTAMP, ts)
-                .field(FIELD_DIMENSION_1, randomFrom(dimensionValues))
-                .field(FIELD_DIMENSION_2, randomIntBetween(1, 10))
-                .field(FIELD_METRIC_COUNTER, counterValue)
-                .endObject();
+            try {
+                return XContentFactory.jsonBuilder()
+                    .startObject()
+                    .field(FIELD_TIMESTAMP, ts)
+                    .field(FIELD_DIMENSION_KEYWORD, randomFrom(dimensionValues))
+                    .field(FIELD_DIMENSION_LONG, randomIntBetween(1, 10))
+                    .field(FIELD_METRIC_COUNTER_DOUBLE, counterValue)
+                    .endObject();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         };
         int indexedDocs = bulkIndex(sourceIndex, sourceSupplier, DOC_COUNT);
 
@@ -216,47 +208,5 @@ public class ILMDownsampleDisruptionIT extends ESIntegTestCase {
                 assertTrue(targetIndexSearch.getHits().getHits().length > 0);
             }
         );
-    }
-
-    private int bulkIndex(final String indexName, final SourceSupplier sourceSupplier, int docCount) throws IOException {
-        BulkRequestBuilder bulkRequestBuilder = internalCluster().client().prepareBulk();
-        bulkRequestBuilder.setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE);
-        for (int i = 0; i < docCount; i++) {
-            IndexRequest indexRequest = new IndexRequest(indexName).opType(DocWriteRequest.OpType.CREATE);
-            XContentBuilder source = sourceSupplier.get();
-            indexRequest.source(source);
-            bulkRequestBuilder.add(indexRequest);
-        }
-        BulkResponse bulkResponse = bulkRequestBuilder.get();
-        int duplicates = 0;
-        for (BulkItemResponse response : bulkResponse.getItems()) {
-            if (response.isFailed()) {
-                if (response.getFailure().getCause() instanceof VersionConflictEngineException) {
-                    // A duplicate event was created by random generator. We should not fail for this
-                    // reason.
-                    logger.debug("We tried to insert a duplicate: [{}]", response.getFailureMessage());
-                    duplicates++;
-                } else {
-                    fail("Failed to index data: " + bulkResponse.buildFailureMessage());
-                }
-            }
-        }
-        int docsIndexed = docCount - duplicates;
-        logger.info("Indexed [{}] documents. Dropped [{}] duplicates.", docsIndexed, duplicates);
-        return docsIndexed;
-    }
-
-    private String randomDateForInterval(final DateHistogramInterval interval, final long startTime) {
-        long endTime = startTime + 10 * interval.estimateMillis();
-        return randomDateForRange(startTime, endTime);
-    }
-
-    private String randomDateForRange(long start, long end) {
-        return DATE_FORMATTER.formatMillis(randomLongBetween(start, end));
-    }
-
-    @FunctionalInterface
-    public interface SourceSupplier {
-        XContentBuilder get() throws IOException;
     }
 }

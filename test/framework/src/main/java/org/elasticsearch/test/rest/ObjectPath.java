e0fab440f6e5580b1d230dbed960635c520e57a1
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-05-19 17:21:44 +0000

    [CI] Auto commit changes from spotless

--- test/framework/src/main/java/org/elasticsearch/test/rest/ObjectPath.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/ObjectPath.java
@@ -21,7 +21,6 @@ import org.elasticsearch.xcontent.XContentType;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;

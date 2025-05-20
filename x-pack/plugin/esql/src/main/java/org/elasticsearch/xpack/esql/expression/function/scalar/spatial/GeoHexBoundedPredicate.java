44eca39c85cb9a757fc144f44d8487188e4c2bf9
Craig Taverner <craig@amanzi.com>
2025-05-20 18:38:23 +0200

    Check if the issue is related to crossing the dateline

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/spatial/GeoHexBoundedPredicate.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/spatial/GeoHexBoundedPredicate.java
@@ -21,6 +21,8 @@ public class GeoHexBoundedPredicate {
 
     GeoHexBoundedPredicate(GeoBoundingBox bbox) {
         this.crossesDateline = bbox.right() < bbox.left();
+        // TODO remove this once we get serverless flaky tests to pass
+        assert this.crossesDateline == false;
         this.bbox = bbox;
         scratch = new GeoBoundingBox(new org.elasticsearch.common.geo.GeoPoint(), new org.elasticsearch.common.geo.GeoPoint());
     }

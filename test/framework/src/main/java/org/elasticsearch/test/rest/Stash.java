a95a3d290433f7f6821e293cbbb29afa3551425f
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-05-20 12:48:12 +0000

    [CI] Auto commit changes from spotless

--- test/framework/src/main/java/org/elasticsearch/test/rest/Stash.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/Stash.java
@@ -11,7 +11,6 @@ package org.elasticsearch.test.rest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.common.Strings;
 import org.elasticsearch.xcontent.ToXContentFragment;
 import org.elasticsearch.xcontent.XContentBuilder;
 

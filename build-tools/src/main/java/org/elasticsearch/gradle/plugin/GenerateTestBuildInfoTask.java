b8e068ef6f328b171e7b535f147c7f1eb208d898
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-05-22 15:52:33 +0000

    [CI] Auto commit changes from spotless

--- build-tools/src/main/java/org/elasticsearch/gradle/plugin/GenerateTestBuildInfoTask.java
+++ build-tools/src/main/java/org/elasticsearch/gradle/plugin/GenerateTestBuildInfoTask.java
@@ -41,7 +41,6 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.security.CodeSource;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;

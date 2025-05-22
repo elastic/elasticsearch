04628b834c0df2693cfcf83ddfc1a3b18fd9bff3
Lorenzo Dematté <lorenzo.dematte@elastic.co>
2025-05-22 13:48:26 +0200

    [Entitlements] Test ScopeResolver based on TestBuildInfo (parser + resolver) (#127719)
    This PR introduces a test-specific ScopeResolver to use with PolicyManager for checking entitlements within test code running in a test runner (unit tests and integ tests, where code is running withing the same JVM).
    The information for resolving component and module names is derived from the file created in #127486

---
+++ test/framework/src/main/java/org/elasticsearch/bootstrap/TestBuildInfo.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+package org.elasticsearch.bootstrap;
+
+import java.util.List;
+
+record TestBuildInfo(String component, List<TestBuildInfoLocation> locations) {}

2323d0005e3262a608070c713bed5a2cb90ab5d7
Jonathan Buttner <jonathan.buttner@elastic.co>
2025-05-22 12:47:18 -0400

    Using correct origin for inference action

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/inference/InferenceRunner.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/inference/InferenceRunner.java
@@ -21,6 +21,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.elasticsearch.xpack.core.ClientHelper.INFERENCE_ORIGIN;
+import static org.elasticsearch.xpack.core.ClientHelper.executeAsyncWithOrigin;
+
 public class InferenceRunner {
 
     private final Client client;
@@ -73,6 +76,12 @@ public class InferenceRunner {
     }
 
     public void doInference(InferenceAction.Request request, ActionListener<InferenceAction.Response> listener) {
-        client.execute(InferenceAction.INSTANCE, request, listener);
+        executeAsyncWithOrigin(
+            client,
+            INFERENCE_ORIGIN,
+            InferenceAction.INSTANCE,
+            request,
+            listener
+        );
     }
 }

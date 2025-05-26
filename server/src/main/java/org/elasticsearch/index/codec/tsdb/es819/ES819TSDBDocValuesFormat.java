539717aa24259740a1dd5c3c1a1af2726ca77e2a
Martijn van Groningen <martijn.v.groningen@gmail.com>
2025-05-26 09:20:04 +0200

    Remove tsdb_doc_values_optimized_merge feature flag.
    All the development work has been completed in #126111
    and no regressions have been observer in nightly benchmarks.

--- server/src/main/java/org/elasticsearch/index/codec/tsdb/es819/ES819TSDBDocValuesFormat.java
+++ server/src/main/java/org/elasticsearch/index/codec/tsdb/es819/ES819TSDBDocValuesFormat.java
@@ -91,13 +91,11 @@ public class ES819TSDBDocValuesFormat extends org.apache.lucene.codecs.DocValues
 
     // Default for escape hatch:
     static final boolean OPTIMIZED_MERGE_ENABLE_DEFAULT;
-    static final FeatureFlag TSDB_DOC_VALUES_OPTIMIZED_MERGE = new FeatureFlag("tsdb_doc_values_optimized_merge");
     static final String OPTIMIZED_MERGE_ENABLED_NAME = ES819TSDBDocValuesConsumer.class.getName() + ".enableOptimizedMerge";
 
     static {
-        boolean optimizedMergeDefault = TSDB_DOC_VALUES_OPTIMIZED_MERGE.isEnabled();
         OPTIMIZED_MERGE_ENABLE_DEFAULT = Boolean.parseBoolean(
-            System.getProperty(OPTIMIZED_MERGE_ENABLED_NAME, Boolean.toString(optimizedMergeDefault))
+            System.getProperty(OPTIMIZED_MERGE_ENABLED_NAME, Boolean.TRUE.toString())
         );
     }
 

344d86320d9e006712d88fd935223e5a26c81680
Jack Conradson <osjdconrad@gmail.com>
2025-05-28 14:14:05 -0700

    start of parsing for test build info

--- test/framework/src/main/java/org/elasticsearch/bootstrap/BootstrapForTesting.java
+++ test/framework/src/main/java/org/elasticsearch/bootstrap/BootstrapForTesting.java
@@ -16,10 +16,21 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.core.Booleans;
 import org.elasticsearch.core.PathUtils;
 import org.elasticsearch.jdk.JarHell;
+import org.elasticsearch.xcontent.ConstructingObjectParser;
+import org.elasticsearch.xcontent.ParseField;
+import org.elasticsearch.xcontent.XContentParser;
+import org.elasticsearch.xcontent.XContentParserConfiguration;
+import org.elasticsearch.xcontent.json.JsonXContent;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -35,7 +46,18 @@ public class BootstrapForTesting {
     // TODO: can we share more code with the non-test side here
     // without making things complex???
 
+    private static final ConstructingObjectParser<TestBuildInfoLocation, Void> TEST_BUILD_INFO_LOCATION_PARSER =
+        new ConstructingObjectParser<>("test_build_info_location", values -> new TestBuildInfoLocation((String)values[0], (String)values[1]));
+    @SuppressWarnings("unchecked")
+    private static final ConstructingObjectParser<TestBuildInfo, Void> TEST_BUILD_INFO_PARSER =
+        new ConstructingObjectParser<>("test_build_info", values -> new TestBuildInfo((String)values[0], (List<TestBuildInfoLocation>)values[1]));
+
     static {
+        TEST_BUILD_INFO_LOCATION_PARSER.declareString(ConstructingObjectParser.constructorArg(), new ParseField("module"));
+        TEST_BUILD_INFO_LOCATION_PARSER.declareString(ConstructingObjectParser.constructorArg(), new ParseField("representative_class"));
+
+        TEST_BUILD_INFO_PARSER.declareString(ConstructingObjectParser.constructorArg(), new ParseField("component"));
+        TEST_BUILD_INFO_PARSER.declareObjectArray(ConstructingObjectParser.constructorArg(), TEST_BUILD_INFO_LOCATION_PARSER::apply, new ParseField("locations"));
 
         // make sure java.io.tmpdir exists always (in case code uses it in a static initializer)
         Path javaTmpDir = PathUtils.get(
@@ -71,6 +93,24 @@ public class BootstrapForTesting {
 
         // Log ifconfig output before SecurityManager is installed
         IfConfig.logIfNecessary();
+
+        try {
+            Enumeration<URL> urls = BootstrapForTesting.class.getClassLoader().getResources("META-INF/plugin-test-build-info.json");
+            System.out.println("HERE0: " + urls.hasMoreElements());
+            while (urls.hasMoreElements()) {
+                try (XContentParser parser = JsonXContent.jsonXContent.createParser(XContentParserConfiguration.EMPTY, urls.nextElement().openStream())) {
+                    TestBuildInfo testBuildInfo = TEST_BUILD_INFO_PARSER.parse(parser, null);
+                    System.out.println("HERE1: " + testBuildInfo);
+                }
+            }
+            System.out.println(
+                "HERE2: " + BootstrapForTesting.class.getClassLoader().getResource("META-INF/plugin-test-build-info.json"));
+            System.out.println(
+                "HERE3: " + BootstrapForTesting.class.getResource("/META-INF/plugin-test-build-info.json"));
+            System.out.println("HERE4: " + BootstrapForTesting.class.getProtectionDomain().getCodeSource().getLocation());
+        } catch (IOException ioe) {
+            throw new UncheckedIOException(ioe);
+        }
     }
 
     // does nothing, just easy way to make sure the class is loaded.

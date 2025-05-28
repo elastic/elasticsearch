7cf6370eaac23e622e4f75710d4f58a92b7a0d4a
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2025-05-28 11:48:58 +0200

    use esbench for now

--- .buildkite/scripts/index-micro-benchmark-results.sh
+++ .buildkite/scripts/index-micro-benchmark-results.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-METRICS_HOST=$(vault read -field=es_host /secret/performance/employees/cloud/nightly-rally-metrics)
+METRICS_HOST=$(vault read -field=es_host /secret/performance/employees/cloud/esbench-metrics)
 METRICS_INDEX_NAME="dummy-micro-benchmarks"
-METRICS_USERNAME=$(vault read -field=es_username /secret/performance/employees/cloud/nightly-rally-metrics)
-METRICS_PASSWORD=$(vault read -field=es_password /secret/performance/employees/cloud/nightly-rally-metrics)
+METRICS_USERNAME=$(vault read -field=es_username /secret/performance/employees/cloud/esbench-metrics)
+METRICS_PASSWORD=$(vault read -field=es_password /secret/performance/employees/cloud/esbench-metrics)
 
 jq -c '.[]' "benchmarks/build/result.json" | while read -r doc; do
   curl -s -X POST "https://$METRICS_HOST/$METRICS_INDEX_NAME/_doc" \

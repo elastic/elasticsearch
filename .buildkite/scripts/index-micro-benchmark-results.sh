1498dd6266895016e6c2152add88f26cbe4009e4
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2025-05-28 11:30:04 +0200

    Script for indexing micro benchmark results

---
+++ .buildkite/scripts/index-micro-benchmark-results.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+METRICS_HOST=$(vault read -field=es_host /secret/performance/employees/cloud/nightly-rally-metrics)
+METRICS_INDEX_NAME="dummy-micro-benchmarks"
+METRICS_USERNAME=$(vault read -field=es_username /secret/performance/employees/cloud/nightly-rally-metrics)
+METRICS_PASSWORD=$(vault read -field=es_password /secret/performance/employees/cloud/nightly-rally-metrics)
+
+jq -c '.[]' "benchmarks/build/result.json" | while read -r doc; do
+  curl -s -X POST "https://$METRICS_HOST/$METRICS_INDEX_NAME/_doc" \
+    -u "$METRICS_USERNAME:$METRICS_PASSWORD" \
+    -H 'Content-Type: application/json' \
+    -d "$doc"
+done

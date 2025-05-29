4ebdc7c9e28f138b4e87676160018ebdd60522aa
Pat Whelan <pat.whelan@elastic.co>
2025-05-29 12:39:34 -0400

    [ML] Write Chat Completion JSON (#128592)
    Most providers write the UnifiedCompletionRequest JSON as we received
    it, with some exception:
    - the modelId can be null and/or overwritten from various locations
    - `max_completion_tokens` repalced `max_tokens`, but some providers
      still use the deprecated field name
    We will handle the variations using Params, otherwise all of the
    XContent building code has moved into UnifiedCompletionRequest so it can
    be reused across providers.

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/unified/UnifiedChatCompletionRequestEntity.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/external/unified/UnifiedChatCompletionRequestEntity.java
@@ -17,27 +17,8 @@ import java.util.Objects;
 
 public class UnifiedChatCompletionRequestEntity implements ToXContentFragment {
 
-    public static final String NAME_FIELD = "name";
-    public static final String TOOL_CALL_ID_FIELD = "tool_call_id";
-    public static final String TOOL_CALLS_FIELD = "tool_calls";
-    public static final String ID_FIELD = "id";
-    public static final String FUNCTION_FIELD = "function";
-    public static final String ARGUMENTS_FIELD = "arguments";
-    public static final String DESCRIPTION_FIELD = "description";
-    public static final String PARAMETERS_FIELD = "parameters";
-    public static final String STRICT_FIELD = "strict";
-    public static final String TOP_P_FIELD = "top_p";
     public static final String STREAM_FIELD = "stream";
     private static final String NUMBER_OF_RETURNED_CHOICES_FIELD = "n";
-    public static final String MESSAGES_FIELD = "messages";
-    private static final String ROLE_FIELD = "role";
-    private static final String CONTENT_FIELD = "content";
-    private static final String STOP_FIELD = "stop";
-    private static final String TEMPERATURE_FIELD = "temperature";
-    private static final String TOOL_CHOICE_FIELD = "tool_choice";
-    private static final String TOOL_FIELD = "tools";
-    private static final String TEXT_FIELD = "text";
-    private static final String TYPE_FIELD = "type";
     private static final String STREAM_OPTIONS_FIELD = "stream_options";
     private static final String INCLUDE_USAGE_FIELD = "include_usage";
 
@@ -53,108 +34,10 @@ public class UnifiedChatCompletionRequestEntity implements ToXContentFragment {
 
     @Override
     public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
-        builder.startArray(MESSAGES_FIELD);
-        {
-            for (UnifiedCompletionRequest.Message message : unifiedRequest.messages()) {
-                builder.startObject();
-                {
-                    if (message.content() == null) {
-                        // content is optional
-                    } else if (message.content() instanceof UnifiedCompletionRequest.ContentString contentString) {
-                        builder.field(CONTENT_FIELD, contentString.content());
-                    } else if (message.content() instanceof UnifiedCompletionRequest.ContentObjects contentObjects) {
-                        builder.startArray(CONTENT_FIELD);
-                        for (UnifiedCompletionRequest.ContentObject contentObject : contentObjects.contentObjects()) {
-                            builder.startObject();
-                            builder.field(TEXT_FIELD, contentObject.text());
-                            builder.field(TYPE_FIELD, contentObject.type());
-                            builder.endObject();
-                        }
-                        builder.endArray();
-                    }
-
-                    builder.field(ROLE_FIELD, message.role());
-                    if (message.toolCallId() != null) {
-                        builder.field(TOOL_CALL_ID_FIELD, message.toolCallId());
-                    }
-                    if (message.toolCalls() != null) {
-                        builder.startArray(TOOL_CALLS_FIELD);
-                        for (UnifiedCompletionRequest.ToolCall toolCall : message.toolCalls()) {
-                            builder.startObject();
-                            {
-                                builder.field(ID_FIELD, toolCall.id());
-                                builder.startObject(FUNCTION_FIELD);
-                                {
-                                    builder.field(ARGUMENTS_FIELD, toolCall.function().arguments());
-                                    builder.field(NAME_FIELD, toolCall.function().name());
-                                }
-                                builder.endObject();
-                                builder.field(TYPE_FIELD, toolCall.type());
-                            }
-                            builder.endObject();
-                        }
-                        builder.endArray();
-                    }
-                }
-                builder.endObject();
-            }
-        }
-        builder.endArray();
+        unifiedRequest.toXContent(builder, params);
 
         // Underlying providers expect OpenAI to only return 1 possible choice.
         builder.field(NUMBER_OF_RETURNED_CHOICES_FIELD, 1);
-
-        if (unifiedRequest.stop() != null && unifiedRequest.stop().isEmpty() == false) {
-            builder.field(STOP_FIELD, unifiedRequest.stop());
-        }
-        if (unifiedRequest.temperature() != null) {
-            builder.field(TEMPERATURE_FIELD, unifiedRequest.temperature());
-        }
-        if (unifiedRequest.toolChoice() != null) {
-            if (unifiedRequest.toolChoice() instanceof UnifiedCompletionRequest.ToolChoiceString) {
-                builder.field(TOOL_CHOICE_FIELD, ((UnifiedCompletionRequest.ToolChoiceString) unifiedRequest.toolChoice()).value());
-            } else if (unifiedRequest.toolChoice() instanceof UnifiedCompletionRequest.ToolChoiceObject) {
-                builder.startObject(TOOL_CHOICE_FIELD);
-                {
-                    builder.field(TYPE_FIELD, ((UnifiedCompletionRequest.ToolChoiceObject) unifiedRequest.toolChoice()).type());
-                    builder.startObject(FUNCTION_FIELD);
-                    {
-                        builder.field(
-                            NAME_FIELD,
-                            ((UnifiedCompletionRequest.ToolChoiceObject) unifiedRequest.toolChoice()).function().name()
-                        );
-                    }
-                    builder.endObject();
-                }
-                builder.endObject();
-            }
-        }
-        boolean usesTools = unifiedRequest.tools() != null && unifiedRequest.tools().isEmpty() == false;
-
-        if (usesTools) {
-            builder.startArray(TOOL_FIELD);
-            for (UnifiedCompletionRequest.Tool tool : unifiedRequest.tools()) {
-                builder.startObject();
-                {
-                    builder.field(TYPE_FIELD, tool.type());
-                    builder.startObject(FUNCTION_FIELD);
-                    {
-                        builder.field(DESCRIPTION_FIELD, tool.function().description());
-                        builder.field(NAME_FIELD, tool.function().name());
-                        builder.field(PARAMETERS_FIELD, tool.function().parameters());
-                        if (tool.function().strict() != null) {
-                            builder.field(STRICT_FIELD, tool.function().strict());
-                        }
-                    }
-                    builder.endObject();
-                }
-                builder.endObject();
-            }
-            builder.endArray();
-        }
-        if (unifiedRequest.topP() != null) {
-            builder.field(TOP_P_FIELD, unifiedRequest.topP());
-        }
 
         builder.field(STREAM_FIELD, stream);
         if (stream) {

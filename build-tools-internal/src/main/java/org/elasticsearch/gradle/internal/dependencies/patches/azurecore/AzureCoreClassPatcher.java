668c441fc552bd2bcfcb4e7900f899c1cdf3e462
Richard Dennehy <richard.dennehy@elastic.co>
2025-05-29 10:50:57 +0100

    move manifest patching into Utils.patchJar

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/dependencies/patches/azurecore/AzureCoreClassPatcher.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/dependencies/patches/azurecore/AzureCoreClassPatcher.java
@@ -21,13 +21,7 @@ import org.gradle.api.tasks.Classpath;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.util.Enumeration;
 import java.util.List;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.jar.JarOutputStream;
 
 import static org.elasticsearch.gradle.internal.dependencies.patches.PatcherInfo.classPatcher;
 
@@ -54,38 +48,8 @@ public abstract class AzureCoreClassPatcher implements TransformAction<Transform
         // TODO could use a regex here so it matches regardless of JAR version
         if (inputFile.getName().equals(JAR_FILE_TO_PATCH)) {
             System.out.println("Patching " + inputFile.getName());
-            // This would be cleaner if gradle artifact transformers supported temp files https://github.com/gradle/gradle/issues/30440.
-            // We need to patch (or delete) the Manifest file as it contains signatures for the class files - which obviously get
-            // invalidated when we patch the bytecode - but the existing patch tools only work on class files, so we unfortunately need to
-            // duplicate the loop here. We _also_ need to create a "temporary" jar to write (or not) the patched manifest and the rest of
-            // the jar contents.
-            // TODO we could possibly update `Utils.patchJar` to enable patching manifests etc and most of this would be significantly
-            // simplified
-            File firstOutputFile = outputs.file(inputFile.getName().replace(".jar", "-half-patched"));
-
-            try (
-                JarFile jarFile = new JarFile(inputFile);
-                JarOutputStream jos = new JarOutputStream(new FileOutputStream(firstOutputFile))
-            ) {
-                Enumeration<JarEntry> entries = jarFile.entries();
-                while (entries.hasMoreElements()) {
-                    JarEntry entry = entries.nextElement();
-                    String entryName = entry.getName();
-
-                    if (entryName.endsWith("MANIFEST.MF") == false) {
-                        jos.putNextEntry(new JarEntry(entryName));
-                        try (InputStream is = jarFile.getInputStream(entry)) {
-                            is.transferTo(jos);
-                        }
-                        jos.closeEntry();
-                    }
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-
-            File secondOutputFile = outputs.file(inputFile.getName().replace(".jar", "-patched.jar"));
-            Utils.patchJar(firstOutputFile, secondOutputFile, CLASS_PATCHERS);
+            File outputFile = outputs.file(inputFile.getName().replace(".jar", "-patched.jar"));
+            Utils.patchJar(inputFile, outputFile, CLASS_PATCHERS, false);
         } else {
             System.out.println("Skipping " + inputFile.getName());
             outputs.file(getInputArtifact());

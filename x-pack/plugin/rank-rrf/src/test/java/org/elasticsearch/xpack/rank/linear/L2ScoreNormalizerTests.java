a0ab7e305be38b2bfc1cc7ddfa8ecf8ca2226229
Mridula <mridula.s@elastic.co>
2025-05-30 14:55:12 +0100

    Fixed the forbidden test

--- x-pack/plugin/rank-rrf/src/test/java/org/elasticsearch/xpack/rank/linear/L2ScoreNormalizerTests.java
+++ x-pack/plugin/rank-rrf/src/test/java/org/elasticsearch/xpack/rank/linear/L2ScoreNormalizerTests.java
@@ -9,54 +9,46 @@ package org.elasticsearch.xpack.rank.linear;
 
 import org.apache.lucene.search.ScoreDoc;
 import org.elasticsearch.test.ESTestCase;
-import org.junit.Assert;
-import org.junit.Test;
 
 public class L2ScoreNormalizerTests extends ESTestCase {
 
-    @Test
     public void testNormalizeTypicalVector() {
         ScoreDoc[] docs = { new ScoreDoc(1, 3.0f, 0), new ScoreDoc(2, 4.0f, 0) };
         ScoreDoc[] normalized = L2ScoreNormalizer.INSTANCE.normalizeScores(docs);
-        Assert.assertEquals(0.6f, normalized[0].score, 1e-5);
-        Assert.assertEquals(0.8f, normalized[1].score, 1e-5);
+        assertEquals(0.6f, normalized[0].score, 1e-5);
+        assertEquals(0.8f, normalized[1].score, 1e-5);
     }
 
-    @Test
     public void testAllZeros() {
         ScoreDoc[] docs = { new ScoreDoc(1, 0.0f, 0), new ScoreDoc(2, 0.0f, 0) };
         ScoreDoc[] normalized = L2ScoreNormalizer.INSTANCE.normalizeScores(docs);
-        Assert.assertEquals(0.0f, normalized[0].score, 0.0f);
-        Assert.assertEquals(0.0f, normalized[1].score, 0.0f);
+        assertEquals(0.0f, normalized[0].score, 0.0f);
+        assertEquals(0.0f, normalized[1].score, 0.0f);
     }
 
-    @Test
     public void testAllNaN() {
         ScoreDoc[] docs = { new ScoreDoc(1, Float.NaN, 0), new ScoreDoc(2, Float.NaN, 0) };
         ScoreDoc[] normalized = L2ScoreNormalizer.INSTANCE.normalizeScores(docs);
-        Assert.assertTrue(Float.isNaN(normalized[0].score));
-        Assert.assertTrue(Float.isNaN(normalized[1].score));
+        assertTrue(Float.isNaN(normalized[0].score));
+        assertTrue(Float.isNaN(normalized[1].score));
     }
 
-    @Test
     public void testMixedZeroAndNaN() {
         ScoreDoc[] docs = { new ScoreDoc(1, 0.0f, 0), new ScoreDoc(2, Float.NaN, 0) };
         ScoreDoc[] normalized = L2ScoreNormalizer.INSTANCE.normalizeScores(docs);
-        Assert.assertEquals(0.0f, normalized[0].score, 0.0f);
-        Assert.assertTrue(Float.isNaN(normalized[1].score));
+        assertEquals(0.0f, normalized[0].score, 0.0f);
+        assertTrue(Float.isNaN(normalized[1].score));
     }
 
-    @Test
     public void testSingleElement() {
         ScoreDoc[] docs = { new ScoreDoc(1, 42.0f, 0) };
         ScoreDoc[] normalized = L2ScoreNormalizer.INSTANCE.normalizeScores(docs);
-        Assert.assertEquals(1.0f, normalized[0].score, 1e-5);
+        assertEquals(1.0f, normalized[0].score, 1e-5);
     }
 
-    @Test
     public void testEmptyArray() {
         ScoreDoc[] docs = {};
         ScoreDoc[] normalized = L2ScoreNormalizer.INSTANCE.normalizeScores(docs);
-        Assert.assertEquals(0, normalized.length);
+        assertEquals(0, normalized.length);
     }
 }

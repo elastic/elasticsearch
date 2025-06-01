4824f12c79d250bca5e127865506dcedbed077d0
Jan Kazlouski <jan.kazlouski@elastic.co>
2025-06-02 02:49:53 +0300

    Enhance MistralErrorResponse documentation with detailed error examples

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/mistral/response/MistralErrorResponse.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/mistral/response/MistralErrorResponse.java
@@ -16,6 +16,54 @@ import java.nio.charset.StandardCharsets;
  * Represents an error response entity for Mistral inference services.
  * This class extends ErrorResponse and provides a method to create an instance
  * from an HttpResult, attempting to read the body as a UTF-8 string.
+ * An example error response for Not Found error would look like:
+ * <pre><code>
+ * {
+ *     "detail": "Not Found"
+ * }
+ * </code></pre>
+ * An example error response for Bad Request error would look like:
+ * <pre><code>
+ * {
+ *     "object": "error",
+ *     "message": "Invalid model: wrong-model-name",
+ *     "type": "invalid_model",
+ *     "param": null,
+ *     "code": "1500"
+ * }
+ * </code></pre>
+ * An example error response for Unauthorized error would look like:
+ * <pre><code>
+ * {
+ *     "message": "Unauthorized",
+ *     "request_id": "ad95a2165083f20b490f8f78a14bb104"
+ * }
+ * </code></pre>
+ * An example error response for Unprocessable Entity error would look like:
+ * <pre><code>
+ * {
+ *     "object": "error",
+ *     "message": {
+ *         "detail": [
+ *             {
+ *                 "type": "greater_than_equal",
+ *                 "loc": [
+ *                     "body",
+ *                     "max_tokens"
+ *                 ],
+ *                 "msg": "Input should be greater than or equal to 0",
+ *                 "input": -10,
+ *                 "ctx": {
+ *                     "ge": 0
+ *                 }
+ *             }
+ *         ]
+ *     },
+ *     "type": "invalid_request_error",
+ *     "param": null,
+ *     "code": null
+ * }
+ * </code></pre>
  */
 public class MistralErrorResponse extends ErrorResponse {
 

614ec172d55e7707ade19b3be784a5710aa194d4
Jordan Powers <jordan.powers@elastic.co>
2025-06-02 09:36:13 -0700

    Reapply changes to Text class
    This reverts commit 7690f4667eb1c96ad21910205c2d0e04339cc95a.

--- libs/x-content/src/main/java/org/elasticsearch/xcontent/Text.java
+++ libs/x-content/src/main/java/org/elasticsearch/xcontent/Text.java
@@ -6,22 +6,17 @@
  * your election, the "Elastic License 2.0", the "GNU Affero General Public
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
-package org.elasticsearch.common.text;
-
-import org.apache.lucene.util.BytesRef;
-import org.elasticsearch.common.bytes.BytesArray;
-import org.elasticsearch.common.bytes.BytesReference;
-import org.elasticsearch.xcontent.ToXContentFragment;
-import org.elasticsearch.xcontent.XContentBuilder;
+package org.elasticsearch.xcontent;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 /**
- * Both {@link String} and {@link BytesReference} representation of the text. Starts with one of those, and if
+ * Both {@link String} and {@link ByteBuffer} representation of the text. Starts with one of those, and if
  * the other is requests, caches the other one in a local reference so no additional conversion will be needed.
  */
-public final class Text implements Comparable<Text>, ToXContentFragment {
+public final class Text implements XContentString, Comparable<Text>, ToXContentFragment {
 
     public static final Text[] EMPTY_ARRAY = new Text[0];
 
@@ -36,12 +31,26 @@ public final class Text implements Comparable<Text>, ToXContentFragment {
         return texts;
     }
 
-    private BytesReference bytes;
+    private ByteBuffer bytes;
     private String text;
     private int hash;
+    private int stringLength = -1;
 
-    public Text(BytesReference bytes) {
+    /**
+     * Construct a Text from a UTF-8 encoded ByteBuffer. Since no string length is specified, {@link #stringLength()}
+     * will perform a string conversion to measure the string length.
+     */
+    public Text(ByteBuffer bytes) {
         this.bytes = bytes;
+    }
+
+    /**
+     * Construct a Text from a UTF-8 encoded ByteBuffer and an explicit string length. Used to avoid string conversion
+     * in {@link #stringLength()}.
+     */
+    public Text(ByteBuffer bytes, int stringLength) {
+        this.bytes = bytes;
+        this.stringLength = stringLength;
     }
 
     public Text(String text) {
@@ -49,18 +58,16 @@ public final class Text implements Comparable<Text>, ToXContentFragment {
     }
 
     /**
-     * Whether a {@link BytesReference} view of the data is already materialized.
+     * Whether a {@link ByteBuffer} view of the data is already materialized.
      */
     public boolean hasBytes() {
         return bytes != null;
     }
 
-    /**
-     * Returns a {@link BytesReference} view of the data.
-     */
-    public BytesReference bytes() {
+    @Override
+    public ByteBuffer bytes() {
         if (bytes == null) {
-            bytes = new BytesArray(text.getBytes(StandardCharsets.UTF_8));
+            bytes = StandardCharsets.UTF_8.encode(text);
         }
         return bytes;
     }
@@ -72,11 +79,20 @@ public final class Text implements Comparable<Text>, ToXContentFragment {
         return text != null;
     }
 
-    /**
-     * Returns a {@link String} view of the data.
-     */
+    @Override
     public String string() {
-        return text == null ? bytes.utf8ToString() : text;
+        if (text == null) {
+            text = StandardCharsets.UTF_8.decode(bytes).toString();
+        }
+        return text;
+    }
+
+    @Override
+    public int stringLength() {
+        if (stringLength < 0) {
+            stringLength = string().length();
+        }
+        return stringLength;
     }
 
     @Override
@@ -115,8 +131,8 @@ public final class Text implements Comparable<Text>, ToXContentFragment {
         } else {
             // TODO: TextBytesOptimization we can use a buffer here to convert it? maybe add a
             // request to jackson to support InputStream as well?
-            BytesRef br = this.bytes().toBytesRef();
-            return builder.utf8Value(br.bytes, br.offset, br.length);
+            assert bytes.hasArray();
+            return builder.utf8Value(bytes.array(), bytes.arrayOffset() + bytes.position(), bytes.remaining());
         }
     }
 }

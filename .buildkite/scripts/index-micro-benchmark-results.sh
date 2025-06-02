3d3e67b907f145467323a865e4125d3d09a7b3e1
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2025-06-02 14:11:05 +0200

    fix field appending

--- .buildkite/scripts/index-micro-benchmark-results.sh
+++ .buildkite/scripts/index-micro-benchmark-results.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 jq -c '.[]' "benchmarks/build/result.json" | while read -r doc; do
+  doc=$(echo "$doc" | jq --arg timestamp "$(date +%s000)" '. + {"@timestamp": $timestamp}')
   echo "Indexing $(echo "$doc" | jq -r '.benchmark')"
-  doc = $doc | jq --arg timestamp "$(date +%s000)" '. + {"@timestamp": $timestamp}'
   curl -s -X POST "https://$PERF_METRICS_HOST/$PERF_METRICS_INDEX/_doc" \
     -u "$PERF_METRICS_USERNAME:$PERF_METRICS_PASSWORD" \
     -H 'Content-Type: application/json' \

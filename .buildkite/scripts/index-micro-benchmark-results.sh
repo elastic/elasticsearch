a3e652375519b40f3309ed1b7345542246fd4dad
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2025-06-02 14:46:08 +0200

    remove timestamp quotes

--- .buildkite/scripts/index-micro-benchmark-results.sh
+++ .buildkite/scripts/index-micro-benchmark-results.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 jq -c '.[]' "benchmarks/build/result.json" | while read -r doc; do
-  doc=$(echo "$doc" | jq --arg timestamp "$(date +%s000)" '. + {"@timestamp": $timestamp}')
+  doc=$(echo "$doc" | jq --argjson timestamp "$(date +%s000)" '. + {"@timestamp": $timestamp}')
   echo "Indexing $(echo "$doc" | jq -r '.benchmark')"
   curl -s -X POST "https://$PERF_METRICS_HOST/$PERF_METRICS_INDEX/_doc" \
     -u "$PERF_METRICS_USERNAME:$PERF_METRICS_PASSWORD" \

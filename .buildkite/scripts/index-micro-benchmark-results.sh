0697c91eb8814615fcad427466939aa5b73fe6dc
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2025-06-02 13:47:00 +0200

    append timestamp field

--- .buildkite/scripts/index-micro-benchmark-results.sh
+++ .buildkite/scripts/index-micro-benchmark-results.sh
@@ -2,6 +2,7 @@
 
 jq -c '.[]' "benchmarks/build/result.json" | while read -r doc; do
   echo "Indexing $(echo "$doc" | jq -r '.benchmark')"
+  doc = $doc | jq --arg timestamp "$(date +%s000)" '. + {"@timestamp": $timestamp}'
   curl -s -X POST "https://$PERF_METRICS_HOST/$PERF_METRICS_INDEX/_doc" \
     -u "$PERF_METRICS_USERNAME:$PERF_METRICS_PASSWORD" \
     -H 'Content-Type: application/json' \

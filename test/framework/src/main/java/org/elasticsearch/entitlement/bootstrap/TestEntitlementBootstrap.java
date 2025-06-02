21ce09c11e5a4d7cd59b260cbe3d159a3e4a6bd8
Patrick Doyle <patrick.doyle@elastic.co>
2025-06-02 13:20:06 -0400

    Empty TestPathLookup

--- test/framework/src/main/java/org/elasticsearch/entitlement/bootstrap/TestEntitlementBootstrap.java
+++ test/framework/src/main/java/org/elasticsearch/entitlement/bootstrap/TestEntitlementBootstrap.java
@@ -33,21 +33,17 @@ public class TestEntitlementBootstrap {
     private record TestPathLookup() implements PathLookup {
         @Override
         public Path pidFile() {
-            throw notYetImplemented();
+            return null;
         }
 
         @Override
         public Stream<Path> getBaseDirPaths(BaseDir baseDir) {
-            throw notYetImplemented();
+            return Stream.empty();
         }
 
         @Override
         public Stream<Path> resolveSettingPaths(BaseDir baseDir, String settingName) {
-            throw notYetImplemented();
-        }
-
-        private static IllegalStateException notYetImplemented() {
-            return new IllegalStateException("not yet implemented");
+            return Stream.empty();
         }
 
     }

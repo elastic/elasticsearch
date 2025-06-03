f4d9db51bdb3a6784002e89a2348f9b8a7770b25
Nick Tindall <nick.tindall@elastic.co>
2025-06-03 16:18:08 +1000

    Test that InternalClusterInfoService polls for heap usage

--- server/src/test/java/org/elasticsearch/cluster/InternalClusterInfoServiceSchedulingTests.java
+++ server/src/test/java/org/elasticsearch/cluster/InternalClusterInfoServiceSchedulingTests.java
@@ -34,12 +34,17 @@ import org.elasticsearch.test.ClusterServiceUtils;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.client.NoOpClient;
 import org.elasticsearch.threadpool.ThreadPool;
+import org.mockito.Mockito;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.elasticsearch.cluster.InternalClusterInfoService.INTERNAL_CLUSTER_INFO_UPDATE_INTERVAL_SETTING;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 public class InternalClusterInfoServiceSchedulingTests extends ESTestCase {
 
@@ -72,6 +77,8 @@ public class InternalClusterInfoServiceSchedulingTests extends ESTestCase {
 
         final FakeClusterInfoServiceClient client = new FakeClusterInfoServiceClient(threadPool);
         final InternalClusterInfoService clusterInfoService = new InternalClusterInfoService(settings, clusterService, threadPool, client);
+        final HeapUsageSupplier mockHeapUsageSupplier = spy(new StubHeapUsageSupplier());
+        clusterInfoService.setHeapUsageSupplier(mockHeapUsageSupplier);
         clusterService.addListener(clusterInfoService);
         clusterInfoService.addListener(ignored -> {});
 
@@ -107,11 +114,13 @@ public class InternalClusterInfoServiceSchedulingTests extends ESTestCase {
         deterministicTaskQueue.runAllRunnableTasks();
 
         for (int i = 0; i < 3; i++) {
+            Mockito.clearInvocations(mockHeapUsageSupplier);
             final int initialRequestCount = client.requestCount;
             final long duration = INTERNAL_CLUSTER_INFO_UPDATE_INTERVAL_SETTING.get(settings).millis();
             runFor(deterministicTaskQueue, duration);
             deterministicTaskQueue.runAllRunnableTasks();
             assertThat(client.requestCount, equalTo(initialRequestCount + 2)); // should have run two client requests per interval
+            verify(mockHeapUsageSupplier).getClusterHeapUsage(any()); // Should poll for heap usage once per interval
         }
 
         final AtomicBoolean failMaster2 = new AtomicBoolean();
@@ -126,6 +135,14 @@ public class InternalClusterInfoServiceSchedulingTests extends ESTestCase {
         deterministicTaskQueue.runAllRunnableTasks();
         assertFalse(deterministicTaskQueue.hasRunnableTasks());
         assertFalse(deterministicTaskQueue.hasDeferredTasks());
+    }
+
+    private static class StubHeapUsageSupplier implements HeapUsageSupplier {
+
+        @Override
+        public void getClusterHeapUsage(ActionListener<Map<String, HeapUsage>> listener) {
+            listener.onResponse(Map.of());
+        }
     }
 
     private static void runFor(DeterministicTaskQueue deterministicTaskQueue, long duration) {

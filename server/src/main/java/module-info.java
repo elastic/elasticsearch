6e73c2e67f9395bdcab6a53b9fef6fddfb185a17
Dimitris Rempapis <dimitris.rempapis@elastic.co>
2025-06-04 11:29:26 +0300

    remove stale package from .info file

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -138,7 +138,6 @@ module org.elasticsearch.server {
     exports org.elasticsearch.action.ingest;
     exports org.elasticsearch.action.resync;
     exports org.elasticsearch.action.search;
-    exports org.elasticsearch.action.search.load;
     exports org.elasticsearch.action.support;
     exports org.elasticsearch.action.support.broadcast;
     exports org.elasticsearch.action.support.broadcast.node;

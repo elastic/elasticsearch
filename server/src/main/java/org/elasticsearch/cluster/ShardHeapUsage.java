0cacdc7bac571e252c7625d7444b59d6a8f8bdaa
Nick Tindall <nick.tindall@elastic.co>
2025-06-05 09:21:31 +1000

    Remove unused serialization interface/methods

--- server/src/main/java/org/elasticsearch/cluster/ShardHeapUsage.java
+++ server/src/main/java/org/elasticsearch/cluster/ShardHeapUsage.java
@@ -12,19 +12,13 @@ package org.elasticsearch.cluster;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.Writeable;
-import org.elasticsearch.common.unit.ByteSizeValue;
-import org.elasticsearch.xcontent.ToXContentFragment;
-import org.elasticsearch.xcontent.XContentBuilder;
 
 import java.io.IOException;
 
 /**
  * Record representing an estimate of the heap used by allocated shards and ongoing merges on a particular node
  */
-public record ShardHeapUsage(String nodeId, String nodeName, long totalBytes, long estimatedUsageBytes)
-    implements
-        ToXContentFragment,
-        Writeable {
+public record ShardHeapUsage(String nodeId, String nodeName, long totalBytes, long estimatedUsageBytes) implements Writeable {
 
     public ShardHeapUsage {
         assert estimatedUsageBytes <= totalBytes;
@@ -42,36 +36,7 @@ public record ShardHeapUsage(String nodeId, String nodeName, long totalBytes, lo
         out.writeVLong(this.estimatedUsageBytes);
     }
 
-    public XContentBuilder toShortXContent(XContentBuilder builder) throws IOException {
-        builder.field("node_name", this.nodeName);
-        builder.humanReadableField("total_heap_bytes", "total", ByteSizeValue.ofBytes(this.totalBytes));
-        builder.humanReadableField("estimated_usage_bytes", "used", ByteSizeValue.ofBytes(this.estimatedUsageBytes));
-        builder.humanReadableField("estimated_free_bytes", "free", ByteSizeValue.ofBytes(this.estimatedFreeBytes()));
-        builder.field("estimated_free_percent", truncatePercent(this.freeHeapAsPercentage()));
-        builder.field("estimated_usage_percent", truncatePercent(this.estimatedUsageAsPercentage()));
-        return builder;
-    }
-
-    @Override
-    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
-        builder.field("node_id", this.nodeId);
-        toShortXContent(builder);
-        return builder;
-    }
-
-    public double freeHeapAsPercentage() {
-        return 100.0 - estimatedUsageAsPercentage();
-    }
-
-    public double estimatedUsageAsPercentage() {
-        return 100.0 * estimatedUsageBytes / (double) totalBytes;
-    }
-
     public long estimatedFreeBytes() {
         return totalBytes - estimatedUsageBytes;
-    }
-
-    private static double truncatePercent(double pct) {
-        return Math.round(pct * 10.0) / 10.0;
     }
 }

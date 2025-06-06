402f480a88c38309e296fcda462af04e3fd79f72
carlosdelest <carlos.delgado@elastic.co>
2025-06-06 13:36:51 +0200

    Some renaming and refactoring

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -463,7 +463,8 @@ module org.elasticsearch.server {
             org.elasticsearch.index.codec.Elasticsearch814Codec,
             org.elasticsearch.index.codec.Elasticsearch816Codec,
             org.elasticsearch.index.codec.Elasticsearch900Codec,
-            org.elasticsearch.index.codec.Elasticsearch900Lucene101Codec;
+            org.elasticsearch.index.codec.Elasticsearch900Lucene101Codec,
+            org.elasticsearch.index.codec.Elasticsearch910Lucene102Codec;
 
     provides org.apache.logging.log4j.core.util.ContextDataProvider with org.elasticsearch.common.logging.DynamicContextDataProvider;
 

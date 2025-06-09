beee66e263244a04de9c17ecdf33b17b9bc175cd
Larisa Motova <larisa.motova@elastic.co>
2025-06-09 08:50:08 -1000

    tests

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/aggregate/StdDevSampleTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/aggregate/StdDevSampleTests.java
@@ -26,8 +26,8 @@ import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.equalTo;
 
-public class StdDevTests extends AbstractAggregationTestCase {
-    public StdDevTests(@Name("TestCase") Supplier<TestCaseSupplier.TestCase> testCaseSupplier) {
+public class StdDevSampleTests extends AbstractAggregationTestCase {
+    public StdDevSampleTests(@Name("TestCase") Supplier<TestCaseSupplier.TestCase> testCaseSupplier) {
         this.testCase = testCaseSupplier.get();
     }
 
@@ -39,14 +39,14 @@ public class StdDevTests extends AbstractAggregationTestCase {
             MultiRowTestCaseSupplier.intCases(1, 1000, Integer.MIN_VALUE, Integer.MAX_VALUE, true),
             MultiRowTestCaseSupplier.longCases(1, 1000, Long.MIN_VALUE, Long.MAX_VALUE, true),
             MultiRowTestCaseSupplier.doubleCases(1, 1000, -Double.MAX_VALUE, Double.MAX_VALUE, true)
-        ).flatMap(List::stream).map(StdDevTests::makeSupplier).collect(Collectors.toCollection(() -> suppliers));
+        ).flatMap(List::stream).map(StdDevSampleTests::makeSupplier).collect(Collectors.toCollection(() -> suppliers));
 
         return parameterSuppliersFromTypedDataWithDefaultChecksNoErrors(suppliers, true);
     }
 
     @Override
     protected Expression build(Source source, List<Expression> args) {
-        return new StdDev(source, args.get(0));
+        return new StdDevSample(source, args.get(0));
     }
 
     private static TestCaseSupplier makeSupplier(TestCaseSupplier.TypedDataSupplier fieldSupplier) {
@@ -60,14 +60,15 @@ public class StdDevTests extends AbstractAggregationTestCase {
                 var value = ((Number) fieldValue).doubleValue();
                 welfordAlgorithm.add(value);
             }
-            var result = welfordAlgorithm.evaluatePopulation();
+            var result = welfordAlgorithm.evaluateSample();
             var expected = Double.isInfinite(result) ? null : result;
             return new TestCaseSupplier.TestCase(
                 List.of(fieldTypedData),
-                "StdDev[field=Attribute[channel=0]]",
+                "StdDevSample[field=Attribute[channel=0]]",
                 DataType.DOUBLE,
                 equalTo(expected)
             );
         });
     }
+
 }

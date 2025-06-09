70267c68cd08093d5220b2e8353cbdd0354e95d6
Larisa Motova <larisa.motova@elastic.co>
2025-06-09 08:45:55 -1000

    generated files minus std_dev

---
+++ x-pack/plugin/esql/compute/src/main/generated/org/elasticsearch/compute/aggregation/StdDevSampleFloatAggregatorFunctionSupplier.java
@@ -0,0 +1,47 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License
+// 2.0; you may not use this file except in compliance with the Elastic License
+// 2.0.
+package org.elasticsearch.compute.aggregation;
+
+import java.lang.Integer;
+import java.lang.Override;
+import java.lang.String;
+import java.util.List;
+import org.elasticsearch.compute.operator.DriverContext;
+
+/**
+ * {@link AggregatorFunctionSupplier} implementation for {@link StdDevSampleFloatAggregator}.
+ * This class is generated. Edit {@code AggregatorFunctionSupplierImplementer} instead.
+ */
+public final class StdDevSampleFloatAggregatorFunctionSupplier implements AggregatorFunctionSupplier {
+  public StdDevSampleFloatAggregatorFunctionSupplier() {
+  }
+
+  @Override
+  public List<IntermediateStateDesc> nonGroupingIntermediateStateDesc() {
+    return StdDevSampleFloatAggregatorFunction.intermediateStateDesc();
+  }
+
+  @Override
+  public List<IntermediateStateDesc> groupingIntermediateStateDesc() {
+    return StdDevSampleFloatGroupingAggregatorFunction.intermediateStateDesc();
+  }
+
+  @Override
+  public StdDevSampleFloatAggregatorFunction aggregator(DriverContext driverContext,
+      List<Integer> channels) {
+    return StdDevSampleFloatAggregatorFunction.create(driverContext, channels);
+  }
+
+  @Override
+  public StdDevSampleFloatGroupingAggregatorFunction groupingAggregator(DriverContext driverContext,
+      List<Integer> channels) {
+    return StdDevSampleFloatGroupingAggregatorFunction.create(channels, driverContext);
+  }
+
+  @Override
+  public String describe() {
+    return "std_dev_sample of floats";
+  }
+}

d451d6b94b24d731d19eb129346c951665fd0765
Tommaso Teofili <tommaso.teofili@elastic.co>
2025-06-10 17:33:14 +0200

    spotless

--- server/src/main/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/vectors/DenseVectorFieldMapper.java
@@ -103,7 +103,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.Vector;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;

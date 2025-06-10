74b431d18014c88854b84d654997617f5a7326e4
Carlos Delgado <6339205+carlosdelest@users.noreply.github.com>
2025-06-10 11:11:42 +0200

    ES|QL - kNN function initial support (#127322)

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/fulltext/KnnTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/fulltext/KnnTests.java
@@ -11,16 +11,19 @@ import com.carrotsearch.randomizedtesting.annotations.Name;
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.xpack.esql.action.EsqlCapabilities;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
 import org.elasticsearch.xpack.esql.core.expression.FieldAttribute;
 import org.elasticsearch.xpack.esql.core.expression.Literal;
 import org.elasticsearch.xpack.esql.core.expression.MapExpression;
 import org.elasticsearch.xpack.esql.core.tree.Source;
 import org.elasticsearch.xpack.esql.core.type.DataType;
-import org.elasticsearch.xpack.esql.expression.function.FunctionName;
+import org.elasticsearch.xpack.esql.expression.function.AbstractFunctionTestCase;
 import org.elasticsearch.xpack.esql.expression.function.TestCaseSupplier;
+import org.elasticsearch.xpack.esql.expression.function.vector.Knn;
 import org.elasticsearch.xpack.esql.io.stream.PlanStreamOutput;
 import org.elasticsearch.xpack.esql.optimizer.rules.physical.local.LucenePushdownPredicates;
+import org.junit.Before;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,15 +31,15 @@ import java.util.function.Supplier;
 
 import static org.elasticsearch.xpack.esql.SerializationTestUtils.serializeDeserialize;
 import static org.elasticsearch.xpack.esql.core.type.DataType.BOOLEAN;
+import static org.elasticsearch.xpack.esql.core.type.DataType.DENSE_VECTOR;
 import static org.elasticsearch.xpack.esql.core.type.DataType.KEYWORD;
 import static org.elasticsearch.xpack.esql.core.type.DataType.UNSUPPORTED;
 import static org.elasticsearch.xpack.esql.planner.TranslatorHandler.TRANSLATOR_HANDLER;
 import static org.hamcrest.Matchers.equalTo;
 
-@FunctionName("match")
-public class MatchTests extends AbstractMatchFullTextFunctionTests {
+public class KnnTests extends AbstractFunctionTestCase {
 
-    public MatchTests(@Name("TestCase") Supplier<TestCaseSupplier.TestCase> testCaseSupplier) {
+    public KnnTests(@Name("TestCase") Supplier<TestCaseSupplier.TestCase> testCaseSupplier) {
         this.testCase = testCaseSupplier.get();
     }
 
@@ -45,10 +48,41 @@ public class MatchTests extends AbstractMatchFullTextFunctionTests {
         return parameterSuppliersFromTypedData(addFunctionNamedParams(testCaseSuppliers()));
     }
 
+    @Before
+    public void checkCapability() {
+        assumeTrue("KNN is not enabled", EsqlCapabilities.Cap.KNN_FUNCTION.isEnabled());
+    }
+
+    private static List<TestCaseSupplier> testCaseSuppliers() {
+        List<TestCaseSupplier> suppliers = new ArrayList<>();
+
+        suppliers.add(
+            TestCaseSupplier.testCaseSupplier(
+                new TestCaseSupplier.TypedDataSupplier("dense_vector field", KnnTests::randomDenseVector, DENSE_VECTOR),
+                new TestCaseSupplier.TypedDataSupplier("query", KnnTests::randomDenseVector, DENSE_VECTOR, true),
+                (d1, d2) -> equalTo("string"),
+                BOOLEAN,
+                (o1, o2) -> true
+            )
+        );
+
+        return suppliers;
+    }
+
+    private static List<Float> randomDenseVector() {
+        int dimensions = randomIntBetween(64, 128);
+        List<Float> vector = new ArrayList<>();
+        for (int i = 0; i < dimensions; i++) {
+            vector.add(randomFloat());
+        }
+        return vector;
+    }
+
     /**
      * Adds function named parameters to all the test case suppliers provided
      */
     private static List<TestCaseSupplier> addFunctionNamedParams(List<TestCaseSupplier> suppliers) {
+        // TODO get to a common class with MatchTests
         List<TestCaseSupplier> result = new ArrayList<>();
         for (TestCaseSupplier supplier : suppliers) {
             List<DataType> dataTypes = new ArrayList<>(supplier.types());
@@ -57,19 +91,13 @@ public class MatchTests extends AbstractMatchFullTextFunctionTests {
                 List<TestCaseSupplier.TypedData> values = new ArrayList<>(supplier.get().getData());
                 values.add(
                     new TestCaseSupplier.TypedData(
-                        new MapExpression(
-                            Source.EMPTY,
-                            List.of(
-                                new Literal(Source.EMPTY, "fuzziness", KEYWORD),
-                                new Literal(Source.EMPTY, randomAlphaOfLength(10), KEYWORD)
-                            )
-                        ),
+                        new MapExpression(Source.EMPTY, List.of(new Literal(Source.EMPTY, randomAlphaOfLength(10), KEYWORD))),
                         UNSUPPORTED,
                         "options"
                     ).forceLiteral()
                 );
 
-                return new TestCaseSupplier.TestCase(values, equalTo("MatchEvaluator"), BOOLEAN, equalTo(true));
+                return new TestCaseSupplier.TestCase(values, equalTo("KnnEvaluator"), BOOLEAN, equalTo(true));
             }));
         }
         return result;
@@ -77,14 +105,14 @@ public class MatchTests extends AbstractMatchFullTextFunctionTests {
 
     @Override
     protected Expression build(Source source, List<Expression> args) {
-        Match match = new Match(source, args.get(0), args.get(1), args.size() > 2 ? args.get(2) : null);
+        Knn knn = new Knn(source, args.get(0), args.get(1), args.size() > 2 ? args.get(2) : null);
         // We need to add the QueryBuilder to the match expression, as it is used to implement equals() and hashCode() and
         // thus test the serialization methods. But we can only do this if the parameters make sense .
         if (args.get(0) instanceof FieldAttribute && args.get(1).foldable()) {
-            QueryBuilder queryBuilder = TRANSLATOR_HANDLER.asQuery(LucenePushdownPredicates.DEFAULT, match).toQueryBuilder();
-            match = (Match) match.replaceQueryBuilder(queryBuilder);
+            QueryBuilder queryBuilder = TRANSLATOR_HANDLER.asQuery(LucenePushdownPredicates.DEFAULT, knn).toQueryBuilder();
+            knn = (Knn) knn.replaceQueryBuilder(queryBuilder);
         }
-        return match;
+        return knn;
     }
 
     /**

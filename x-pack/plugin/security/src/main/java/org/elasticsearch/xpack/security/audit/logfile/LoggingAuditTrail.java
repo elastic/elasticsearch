999ac5c573c6ce2a0ea37980d9fbaf369861aa03
Slobodan Adamovic <slobodan.adamovic@elastic.co>
2025-06-10 22:05:39 +0200

    support audit logging (pending tests)

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/audit/logfile/LoggingAuditTrail.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/audit/logfile/LoggingAuditTrail.java
@@ -1635,7 +1635,7 @@ public class LoggingAuditTrail implements AuditTrail, ClusterStateListener {
         static void addAuthenticationFieldsToLogEntry(StringMapMessage logEntry, Authentication authentication) {
             logEntry.with(PRINCIPAL_FIELD_NAME, authentication.getEffectiveSubject().getUser().principal());
             logEntry.with(AUTHENTICATION_TYPE_FIELD_NAME, authentication.getAuthenticationType().toString());
-            if (authentication.isApiKey() || authentication.isCrossClusterAccess()) {
+            if (authentication.isApiKey() || authentication.isCrossClusterAccess() || authentication.isCloudApiKey()) {
                 logEntry.with(
                     API_KEY_ID_FIELD_NAME,
                     (String) authentication.getAuthenticatingSubject().getMetadata().get(AuthenticationField.API_KEY_ID_KEY)

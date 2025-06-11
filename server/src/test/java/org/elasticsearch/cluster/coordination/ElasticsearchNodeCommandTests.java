f0d9177d8ef6293a55918320bedc26671b1cbe08
Yang Wang <yang.wang@elastic.co>
2025-06-11 16:25:21 +1000

    Update tests for ElasticsearchNodeCommand
    The PR updates ElasticsearchNodeCommandTests so that they test the
    right behaviours after multi-project related changes:
    1. Persisted cluster state is always written with multi-project enabled
    2. The CLI tools are expected to run on the same node version, i.e. no BWC
    3. Unknown (unregistered) customs can be read and write by the CLI tools
       without modification
    
    Resolves: ES-11328

--- server/src/test/java/org/elasticsearch/cluster/coordination/ElasticsearchNodeCommandTests.java
+++ server/src/test/java/org/elasticsearch/cluster/coordination/ElasticsearchNodeCommandTests.java
@@ -9,23 +9,30 @@
 
 package org.elasticsearch.cluster.coordination;
 
+import org.elasticsearch.TransportVersion;
 import org.elasticsearch.cluster.ClusterModule;
+import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.IndexGraveyard;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.common.UUIDs;
-import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.core.Tuple;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.env.TestEnvironment;
+import org.elasticsearch.gateway.PersistedClusterStateService;
 import org.elasticsearch.index.Index;
 import org.elasticsearch.indices.IndicesModule;
 import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.test.TestClusterCustomMetadata;
+import org.elasticsearch.test.TestProjectCustomMetadata;
 import org.elasticsearch.xcontent.NamedXContentRegistry;
-import org.elasticsearch.xcontent.XContentBuilder;
-import org.elasticsearch.xcontent.XContentParser;
-import org.elasticsearch.xcontent.XContentParserConfiguration;
-import org.elasticsearch.xcontent.json.JsonXContent;
+import org.elasticsearch.xcontent.ParseField;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -34,65 +41,100 @@ import static org.elasticsearch.cluster.metadata.DataStreamTestHelper.createFirs
 import static org.elasticsearch.cluster.metadata.DataStreamTestHelper.newInstance;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.not;
 
 public class ElasticsearchNodeCommandTests extends ESTestCase {
 
-    public void testLoadStateWithoutMissingCustoms() throws IOException {
-        runLoadStateTest(false, false);
-    }
-
     public void testLoadStateWithoutMissingCustomsButPreserved() throws IOException {
-        runLoadStateTest(false, true);
+        runLoadStateTest(false);
     }
 
     public void testLoadStateWithMissingCustomsButPreserved() throws IOException {
-        runLoadStateTest(true, true);
+        runLoadStateTest(true);
     }
 
-    public void testLoadStateWithMissingCustomsAndNotPreserved() throws IOException {
-        runLoadStateTest(true, false);
+    @Override
+    public Settings buildEnvSettings(Settings settings) {
+        // we control the data path in the tests, so we don't need to set it here
+        return settings;
     }
 
-    private void runLoadStateTest(boolean hasMissingCustoms, boolean preserveUnknownCustoms) throws IOException {
-        final Metadata latestMetadata = randomMeta();
-        final XContentBuilder builder = JsonXContent.contentBuilder();
-        builder.startObject();
-        Metadata.FORMAT.toXContent(builder, latestMetadata);
-        builder.endObject();
+    private void runLoadStateTest(boolean hasMissingCustoms) throws IOException {
+        final var dataPath = createTempDir();
+        final Settings settings = Settings.builder()
+            .putList(Environment.PATH_DATA_SETTING.getKey(), List.of(dataPath.toString()))
+            .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir().toAbsolutePath())
+            .build();
 
-        XContentParserConfiguration parserConfig = hasMissingCustoms
-            ? parserConfig().withRegistry(ElasticsearchNodeCommand.namedXContentRegistry)
-            : parserConfig();
-        Metadata loadedMetadata;
-        try (XContentParser parser = createParser(parserConfig, JsonXContent.jsonXContent, BytesReference.bytes(builder))) {
-            loadedMetadata = Metadata.fromXContent(parser);
-        }
-        assertThat(loadedMetadata.clusterUUID(), not(equalTo("_na_")));
-        assertThat(loadedMetadata.clusterUUID(), equalTo(latestMetadata.clusterUUID()));
-        assertThat(loadedMetadata.getProject().dataStreams(), equalTo(latestMetadata.getProject().dataStreams()));
+        try (var nodeEnvironment = newNodeEnvironment(settings)) {
+            final var persistedClusterStateServiceWithFullRegistry = new PersistedClusterStateService(
+                nodeEnvironment,
+                xContentRegistry(),
+                new ClusterSettings(Settings.EMPTY, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS),
+                () -> 0L,
+                () -> false
+            );
 
-        // make sure the index tombstones are the same too
-        if (hasMissingCustoms) {
+            // 1. Simulating persisting cluster state by a running node
+            final long initialTerm = randomNonNegativeLong();
+            final Metadata initialMetadata = randomMeta(hasMissingCustoms);
+            final ClusterState initialState = ClusterState.builder(ClusterState.EMPTY_STATE).metadata(initialMetadata).build();
+            try (var writer = persistedClusterStateServiceWithFullRegistry.createWriter()) {
+                writer.writeFullStateAndCommit(initialTerm, initialState);
+            }
+
+            // 2. Simulating loading the persisted cluster state by the CLI
+            final var persistedClusterStateServiceForNodeCommand = ElasticsearchNodeCommand.createPersistedClusterStateService(
+                Settings.EMPTY,
+                new Path[] { dataPath }
+            );
+            final Tuple<Long, ClusterState> loadedTermAndClusterState = ElasticsearchNodeCommand.loadTermAndClusterState(
+                persistedClusterStateServiceForNodeCommand,
+                TestEnvironment.newEnvironment(settings)
+            );
+
+            assertThat(loadedTermAndClusterState.v1(), equalTo(initialTerm));
+            final var loadedMetadata = loadedTermAndClusterState.v2().metadata();
             assertNotNull(loadedMetadata.getProject().custom(IndexGraveyard.TYPE));
             assertThat(
                 loadedMetadata.getProject().custom(IndexGraveyard.TYPE),
                 instanceOf(ElasticsearchNodeCommand.UnknownProjectCustom.class)
             );
-
-            if (preserveUnknownCustoms) {
-                // check that we reserialize unknown metadata correctly again
-                final Path tempdir = createTempDir();
-                Metadata.FORMAT.write(loadedMetadata, tempdir);
-                final Metadata reloadedMetadata = Metadata.FORMAT.loadLatestState(logger, xContentRegistry(), tempdir);
-                assertThat(reloadedMetadata.getProject().indexGraveyard(), equalTo(latestMetadata.getProject().indexGraveyard()));
+            if (hasMissingCustoms) {
+                assertThat(
+                    loadedMetadata.getProject().custom(MissingProjectCustomMetadata.TYPE),
+                    instanceOf(ElasticsearchNodeCommand.UnknownProjectCustom.class)
+                );
+                assertThat(
+                    loadedMetadata.custom(MissingClusterCustomMetadata.TYPE),
+                    instanceOf(ElasticsearchNodeCommand.UnknownClusterCustom.class)
+                );
+            } else {
+                assertNull(loadedMetadata.getProject().custom(MissingProjectCustomMetadata.TYPE));
+                assertNull(loadedMetadata.custom(MissingClusterCustomMetadata.TYPE));
             }
-        } else {
-            assertThat(loadedMetadata.getProject().indexGraveyard(), equalTo(latestMetadata.getProject().indexGraveyard()));
+
+            final long newTerm = initialTerm + 1;
+            try (var writer = persistedClusterStateServiceForNodeCommand.createWriter()) {
+                writer.writeFullStateAndCommit(newTerm, ClusterState.builder(ClusterState.EMPTY_STATE).metadata(loadedMetadata).build());
+            }
+
+            // 3. Simulate node restart after updating on-disk state with the CLI tool
+            final var bestOnDiskState = persistedClusterStateServiceWithFullRegistry.loadBestOnDiskState();
+            assertThat(bestOnDiskState.currentTerm, equalTo(newTerm));
+            final Metadata reloadedMetadata = bestOnDiskState.metadata;
+            assertThat(reloadedMetadata.getProject().indexGraveyard(), equalTo(initialMetadata.getProject().indexGraveyard()));
+            if (hasMissingCustoms) {
+                assertThat(
+                    reloadedMetadata.getProject().custom(MissingProjectCustomMetadata.TYPE),
+                    equalTo(initialMetadata.getProject().custom(MissingProjectCustomMetadata.TYPE))
+                );
+            } else {
+                assertNull(reloadedMetadata.getProject().custom(MissingProjectCustomMetadata.TYPE));
+            }
         }
     }
 
-    private Metadata randomMeta() {
+    private Metadata randomMeta(boolean hasMissingCustoms) {
         Metadata.Builder mdBuilder = Metadata.builder();
         mdBuilder.generateClusterUuidIfNeeded();
         int numDelIndices = randomIntBetween(0, 5);
@@ -109,15 +151,86 @@ public class ElasticsearchNodeCommandTests extends ESTestCase {
             }
         }
         mdBuilder.indexGraveyard(graveyard.build());
+        if (hasMissingCustoms) {
+            mdBuilder.putCustom(
+                MissingProjectCustomMetadata.TYPE,
+                new MissingProjectCustomMetadata("test missing project custom metadata")
+            );
+            mdBuilder.putCustom(
+                MissingClusterCustomMetadata.TYPE,
+                new MissingClusterCustomMetadata("test missing cluster custom metadata")
+            );
+        }
         return mdBuilder.build();
     }
 
     @Override
     protected NamedXContentRegistry xContentRegistry() {
         return new NamedXContentRegistry(
-            Stream.of(ClusterModule.getNamedXWriteables().stream(), IndicesModule.getNamedXContents().stream())
-                .flatMap(Function.identity())
-                .toList()
+            Stream.of(
+                ClusterModule.getNamedXWriteables().stream(),
+                IndicesModule.getNamedXContents().stream(),
+                Stream.of(
+                    new NamedXContentRegistry.Entry(
+                        Metadata.ProjectCustom.class,
+                        new ParseField(MissingProjectCustomMetadata.TYPE),
+                        parser -> MissingProjectCustomMetadata.fromXContent(MissingProjectCustomMetadata::new, parser)
+                    ),
+                    new NamedXContentRegistry.Entry(
+                        Metadata.ClusterCustom.class,
+                        new ParseField(MissingClusterCustomMetadata.TYPE),
+                        parser -> MissingClusterCustomMetadata.fromXContent(MissingClusterCustomMetadata::new, parser)
+                    )
+                )
+            ).flatMap(Function.identity()).toList()
         );
+    }
+
+    private static class MissingProjectCustomMetadata extends TestProjectCustomMetadata {
+
+        static final String TYPE = "missing_project_custom_metadata";
+
+        MissingProjectCustomMetadata(String data) {
+            super(data);
+        }
+
+        @Override
+        public String getWriteableName() {
+            return TYPE;
+        }
+
+        @Override
+        public TransportVersion getMinimalSupportedVersion() {
+            return TransportVersion.current();
+        }
+
+        @Override
+        public EnumSet<Metadata.XContentContext> context() {
+            return EnumSet.of(Metadata.XContentContext.GATEWAY);
+        }
+    }
+
+    private static class MissingClusterCustomMetadata extends TestClusterCustomMetadata {
+
+        static final String TYPE = "missing_cluster_custom_metadata";
+
+        MissingClusterCustomMetadata(String data) {
+            super(data);
+        }
+
+        @Override
+        public String getWriteableName() {
+            return TYPE;
+        }
+
+        @Override
+        public TransportVersion getMinimalSupportedVersion() {
+            return TransportVersion.current();
+        }
+
+        @Override
+        public EnumSet<Metadata.XContentContext> context() {
+            return EnumSet.of(Metadata.XContentContext.GATEWAY);
+        }
     }
 }

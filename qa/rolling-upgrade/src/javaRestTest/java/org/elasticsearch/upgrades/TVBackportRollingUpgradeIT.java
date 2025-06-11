c388d63001449bc6866a0cd6a64d44e56db423bc
John Verwolf <osjdverwolf@gmail.com>
2025-06-11 14:57:55 -0700

    add skeleton rolling upgrade test

---
+++ qa/rolling-upgrade/src/javaRestTest/java/org/elasticsearch/upgrades/TVBackportRollingUpgradeIT.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+package org.elasticsearch.upgrades;
+
+import com.carrotsearch.randomizedtesting.annotations.Name;
+
+public class TVBackportRollingUpgradeIT extends AbstractRollingUpgradeTestCase {
+
+    public TVBackportRollingUpgradeIT(@Name("upgradedNodes") int upgradedNodes) {
+        super(upgradedNodes);
+    }
+
+    public void testTVBackport() {
+        if (isOldCluster()) {
+            // TODO: Implement the test for old cluster
+        } else if (isMixedCluster()) {
+            // TODO: Test mixed cluster behavior
+        } else if (isUpgradedCluster()) {
+            // TODO: Test upgraded cluster behavior
+        } else {
+            fail("Unknown cluster state");
+        }
+    }
+}

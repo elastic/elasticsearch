5a1dab92db714663dcbf8d0f9f9099a2e6e08aaf
Samiul Monir <samiul.monir@elastic.co>
2025-06-13 02:49:01 -0400

    Adding yaml test for match,sparse, and knn

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/InferenceFeatures.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/InferenceFeatures.java
@@ -35,6 +35,7 @@ public class InferenceFeatures implements FeatureSpecification {
         "test_rule_retriever.with_indices_that_dont_return_rank_docs"
     );
     private static final NodeFeature SEMANTIC_TEXT_MATCH_ALL_HIGHLIGHTER = new NodeFeature("semantic_text.match_all_highlighter");
+    private static final NodeFeature SEMANTIC_TEXT_QUERY_REWRITE_BOOST_AND_QUERY_NAME_FIX = new NodeFeature("semantic_text.query_rewrite.boost_and_query_name_fix");
 
     @Override
     public Set<NodeFeature> getTestFeatures() {
@@ -59,7 +60,8 @@ public class InferenceFeatures implements FeatureSpecification {
             SemanticTextFieldMapper.SEMANTIC_TEXT_HANDLE_EMPTY_INPUT,
             TEST_RULE_RETRIEVER_WITH_INDICES_THAT_DONT_RETURN_RANK_DOCS,
             SEMANTIC_TEXT_SUPPORT_CHUNKING_CONFIG,
-            SEMANTIC_TEXT_MATCH_ALL_HIGHLIGHTER
+            SEMANTIC_TEXT_MATCH_ALL_HIGHLIGHTER,
+            SEMANTIC_TEXT_QUERY_REWRITE_BOOST_AND_QUERY_NAME_FIX
         );
     }
 }

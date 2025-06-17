7fb130c53b8174f9c1379e1cc32414e86b427fa8
Julian Kiryakov <julian.kiryakov@elastic.co>
2025-06-16 21:49:44 -0400

    Add Support for LIKE (LIST) (#129170)
    Adds support for LIKE function alternative syntax with a list of patterns.
    Examples:
    
    FROM foo
    | WHERE bar LIKE ("A*","B*", "C?")
    The new syntax is documented as part of the existing LIKE function documentation. We will use the existing WildcardLike java implementation for existing cases using the old syntax and one list argument case to improve mixed cluster compatibility.

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/regex/WildcardLikeList.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/regex/WildcardLikeList.java
@@ -12,13 +12,11 @@ import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
 import org.elasticsearch.xpack.esql.core.expression.FieldAttribute;
-import org.elasticsearch.xpack.esql.core.expression.predicate.regex.WildcardPattern;
+import org.elasticsearch.xpack.esql.core.expression.predicate.regex.WildcardPatternList;
 import org.elasticsearch.xpack.esql.core.querydsl.query.Query;
 import org.elasticsearch.xpack.esql.core.querydsl.query.WildcardQuery;
 import org.elasticsearch.xpack.esql.core.tree.NodeInfo;
 import org.elasticsearch.xpack.esql.core.tree.Source;
-import org.elasticsearch.xpack.esql.expression.function.Example;
-import org.elasticsearch.xpack.esql.expression.function.FunctionInfo;
 import org.elasticsearch.xpack.esql.expression.function.Param;
 import org.elasticsearch.xpack.esql.io.stream.PlanStreamInput;
 import org.elasticsearch.xpack.esql.optimizer.rules.physical.local.LucenePushdownPredicates;
@@ -26,54 +24,33 @@ import org.elasticsearch.xpack.esql.planner.TranslatorHandler;
 
 import java.io.IOException;
 
-public class WildcardLike extends RegexMatch<WildcardPattern> {
+public class WildcardLikeList extends RegexMatch<WildcardPatternList> {
     public static final NamedWriteableRegistry.Entry ENTRY = new NamedWriteableRegistry.Entry(
         Expression.class,
-        "WildcardLike",
-        WildcardLike::new
+        "WildcardLikeList",
+        WildcardLikeList::new
     );
-    public static final String NAME = "LIKE";
 
-    @FunctionInfo(returnType = "boolean", description = """
-        Use `LIKE` to filter data based on string patterns using wildcards. `LIKE`
-        usually acts on a field placed on the left-hand side of the operator, but it can
-        also act on a constant (literal) expression. The right-hand side of the operator
-        represents the pattern or a list of patterns. If a list of patterns is provided,
-        the expression will return true if any of the patterns match.
-
-        The following wildcard characters are supported:
-
-        * `*` matches zero or more characters.
-        * `?` matches one character.""", detailedDescription = """
-        Matching the exact characters `*` and `.` will require escaping.
-        The escape character is backslash `\\`. Since also backslash is a special character in string literals,
-        it will require further escaping.
-
-        <<load-esql-example, file=string tag=likeEscapingSingleQuotes>>
-
-        <<load-esql-example, file=where-like tag=likeListDocExample>>
-
-        To reduce the overhead of escaping, we suggest using triple quotes strings `\"\"\"`
-
-        <<load-esql-example, file=string tag=likeEscapingTripleQuotes>>
-        """, operator = NAME, examples = @Example(file = "docs", tag = "like"))
-    public WildcardLike(
+    /**
+     * The documentation for this function is in WildcardLike, and shown to the users `LIKE` in the docs.
+     */
+    public WildcardLikeList(
         Source source,
         @Param(name = "str", type = { "keyword", "text" }, description = "A literal expression.") Expression left,
-        @Param(name = "pattern", type = { "keyword", "text" }, description = "Pattern.") WildcardPattern pattern
+        @Param(name = "pattern", type = { "keyword", "text" }, description = "Pattern.") WildcardPatternList patterns
     ) {
-        this(source, left, pattern, false);
+        this(source, left, patterns, false);
     }
 
-    public WildcardLike(Source source, Expression left, WildcardPattern pattern, boolean caseInsensitive) {
-        super(source, left, pattern, caseInsensitive);
+    public WildcardLikeList(Source source, Expression left, WildcardPatternList patterns, boolean caseInsensitive) {
+        super(source, left, patterns, caseInsensitive);
     }
 
-    private WildcardLike(StreamInput in) throws IOException {
+    public WildcardLikeList(StreamInput in) throws IOException {
         this(
             Source.readFrom((PlanStreamInput) in),
             in.readNamedWriteable(Expression.class),
-            new WildcardPattern(in.readString()),
+            new WildcardPatternList(in),
             deserializeCaseInsensitivity(in)
         );
     }
@@ -82,13 +59,13 @@ public class WildcardLike extends RegexMatch<WildcardPattern> {
     public void writeTo(StreamOutput out) throws IOException {
         source().writeTo(out);
         out.writeNamedWriteable(field());
-        out.writeString(pattern().pattern());
+        pattern().writeTo(out);
         serializeCaseInsensitivity(out);
     }
 
     @Override
     public String name() {
-        return NAME;
+        return ENTRY.name;
     }
 
     @Override
@@ -97,20 +74,33 @@ public class WildcardLike extends RegexMatch<WildcardPattern> {
     }
 
     @Override
-    protected NodeInfo<WildcardLike> info() {
-        return NodeInfo.create(this, WildcardLike::new, field(), pattern(), caseInsensitive());
+    protected NodeInfo<WildcardLikeList> info() {
+        return NodeInfo.create(this, WildcardLikeList::new, field(), pattern(), caseInsensitive());
     }
 
     @Override
-    protected WildcardLike replaceChild(Expression newLeft) {
-        return new WildcardLike(source(), newLeft, pattern(), caseInsensitive());
+    protected WildcardLikeList replaceChild(Expression newLeft) {
+        return new WildcardLikeList(source(), newLeft, pattern(), caseInsensitive());
     }
 
+    /**
+     * Returns {@link Translatable#YES} if the field is pushable, otherwise {@link Translatable#NO}.
+     * For now, we only support a single pattern in the list for pushdown.
+     */
     @Override
     public Translatable translatable(LucenePushdownPredicates pushdownPredicates) {
+        if (pattern().patternList().size() != 1) {
+            // we only support a single pattern in the list for pushdown for now
+            return Translatable.NO;
+        }
         return pushdownPredicates.isPushableAttribute(field()) ? Translatable.YES : Translatable.NO;
+
     }
 
+    /**
+     * Returns a {@link Query} that matches the field against the provided patterns.
+     * For now, we only support a single pattern in the list for pushdown.
+     */
     @Override
     public Query asQuery(LucenePushdownPredicates pushdownPredicates, TranslatorHandler handler) {
         var field = field();
@@ -118,8 +108,14 @@ public class WildcardLike extends RegexMatch<WildcardPattern> {
         return translateField(handler.nameOf(field instanceof FieldAttribute fa ? fa.exactAttribute() : field));
     }
 
-    // TODO: see whether escaping is needed
+    /**
+     * Translates the field to a {@link WildcardQuery} using the first pattern in the list.
+     * Throws an {@link IllegalArgumentException} if the pattern list contains more than one pattern.
+     */
     private Query translateField(String targetFieldName) {
-        return new WildcardQuery(source(), targetFieldName, pattern().asLuceneWildcard(), caseInsensitive());
+        if (pattern().patternList().size() != 1) {
+            throw new IllegalArgumentException("WildcardLikeList can only be translated when it has a single pattern");
+        }
+        return new WildcardQuery(source(), targetFieldName, pattern().patternList().getFirst().asLuceneWildcard(), caseInsensitive());
     }
 }

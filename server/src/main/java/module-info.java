e831dfeb91e3d2d081c6007ac96308530af17ecf
ChrisHegarty <chegar999@gmail.com>
2025-06-17 17:30:48 +0100

    itr

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -479,5 +479,4 @@ module org.elasticsearch.server {
     exports org.elasticsearch.monitor.metrics;
     exports org.elasticsearch.plugins.internal.rewriter to org.elasticsearch.inference;
     exports org.elasticsearch.index.codec.perfield;
-    exports org.elasticsearch.lucene.search;
 }

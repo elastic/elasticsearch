2e59ec0ddb1c68bffc09440b6fd378db8071d6e3
Yang Wang <yang.wang@elastic.co>
2025-06-17 13:28:15 +1000

    Add a new heap usage threshold allocation decider (#3935)
    * Add a new heap usage threshold allocation decider
    
    The PR adds a heap usage threshold decider that reject shard allocation
    when the node heap usage is above a configurable threshold.
    
    Relates: 
    
    * tests and renames
    
    * Update after merge
    
    * index nodes
    
    * IT and handle null nodeId for unassigned shards
    
    * double for setting
    
    * remove debugging code
    
    * tweak
    
    * submodule
    
    * improve test
    
    * type
    
    * latch over assertBusy
    
    * revert to ratio setting
    
    * submodule
    
    * submodule
    
    * ratio value
    
    * separate test
    
    * Merge upstream and update elasticsearch submodule to adf4d1005f70d4df3ba9c70f8a1b597473fcecae
    
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

--- x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/Stateless.java
+++ x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/Stateless.java
@@ -24,6 +24,7 @@ import co.elastic.elasticsearch.serverless.constants.ServerlessSharedSettings;
 import co.elastic.elasticsearch.stateless.action.TransportFetchShardCommitsInUseAction;
 import co.elastic.elasticsearch.stateless.action.TransportGetVirtualBatchedCompoundCommitChunkAction;
 import co.elastic.elasticsearch.stateless.action.TransportNewCommitNotificationAction;
+import co.elastic.elasticsearch.stateless.allocation.ShardHeapAllocationDecider;
 import co.elastic.elasticsearch.stateless.allocation.StatelessAllocationDecider;
 import co.elastic.elasticsearch.stateless.allocation.StatelessBalancingWeightsFactory;
 import co.elastic.elasticsearch.stateless.allocation.StatelessExistingShardsAllocator;
@@ -1155,7 +1156,8 @@ public class Stateless extends Plugin
             StatelessBalancingWeightsFactory.SEARCH_TIER_WRITE_LOAD_BALANCE_FACTOR_SETTING,
             TransportStatelessUnpromotableRelocationAction.START_HANDOFF_CLUSTER_STATE_CONVERGENCE_TIMEOUT_SETTING,
             TransportStatelessUnpromotableRelocationAction.START_HANDOFF_REQUEST_TIMEOUT_SETTING,
-            StatelessOnlinePrewarmingService.STATELESS_ONLINE_PREWARMING_ENABLED
+            StatelessOnlinePrewarmingService.STATELESS_ONLINE_PREWARMING_ENABLED,
+            ShardHeapAllocationDecider.CLUSTER_ROUTING_ALLOCATION_SHARD_HEAP_LOW_WATERMARK
         );
     }
 
@@ -1598,7 +1600,7 @@ public class Stateless extends Plugin
 
     @Override
     public Collection<AllocationDecider> createAllocationDeciders(Settings settings, ClusterSettings clusterSettings) {
-        return List.of(new StatelessAllocationDecider());
+        return List.of(new StatelessAllocationDecider(), new ShardHeapAllocationDecider(clusterSettings));
     }
 
     @Override
---
+++ x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/allocation/ShardHeapAllocationDeciderTests.java
@@ -0,0 +1,332 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ * Copyright Elasticsearch B.V. All rights reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch B.V. and its suppliers, if any.
+ * The intellectual and technical concepts contained herein
+ * are proprietary to Elasticsearch B.V. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright
+ * law.  Dissemination of this information or reproduction of
+ * this material is strictly forbidden unless prior written
+ * permission is obtained from Elasticsearch B.V.
+ */
+
+package co.elastic.elasticsearch.stateless.allocation;
+
+import org.elasticsearch.cluster.ClusterInfo;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.ESAllocationTestCase;
+import org.elasticsearch.cluster.InternalClusterInfoService;
+import org.elasticsearch.cluster.ShardHeapUsage;
+import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.cluster.metadata.ProjectId;
+import org.elasticsearch.cluster.metadata.ProjectMetadata;
+import org.elasticsearch.cluster.node.DiscoveryNodeRole;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.cluster.routing.RecoverySource;
+import org.elasticsearch.cluster.routing.RoutingTable;
+import org.elasticsearch.cluster.routing.ShardRouting;
+import org.elasticsearch.cluster.routing.TestShardRouting;
+import org.elasticsearch.cluster.routing.allocation.AllocationService;
+import org.elasticsearch.cluster.routing.allocation.NodeAllocationResult;
+import org.elasticsearch.cluster.routing.allocation.RoutingAllocation;
+import org.elasticsearch.cluster.routing.allocation.allocator.BalancedShardsAllocator;
+import org.elasticsearch.cluster.routing.allocation.decider.AllocationDecider;
+import org.elasticsearch.cluster.routing.allocation.decider.AllocationDeciders;
+import org.elasticsearch.cluster.routing.allocation.decider.Decision;
+import org.elasticsearch.cluster.routing.allocation.decider.ReplicaAfterPrimaryActiveAllocationDecider;
+import org.elasticsearch.cluster.routing.allocation.decider.SameShardAllocationDecider;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.index.IndexVersion;
+import org.elasticsearch.index.shard.ShardId;
+import org.elasticsearch.snapshots.EmptySnapshotsInfoService;
+import org.elasticsearch.snapshots.SnapshotShardSizeInfo;
+import org.elasticsearch.test.gateway.TestGatewayAllocator;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ShardHeapAllocationDeciderTests extends ESAllocationTestCase {
+
+    public void testYesDecisionWhenDisabled() {
+        final var decider = createShardHeapAllocationDecider(false, between(0, 100));
+
+        final String nodeId = randomIdentifier();
+        final ShardRouting shardRouting = createShardRouting();
+        final RoutingAllocation routingAllocation = createRoutingAllocation(
+            decider,
+            nodeId,
+            shardRouting,
+            createClusterInfo(Map.of(nodeId, randomLongBetween(0, 100)))
+        );
+        final Decision decision = decider.canAllocate(shardRouting, routingAllocation.routingNodes().node(nodeId), routingAllocation);
+        assertThat(decision.type(), equalTo(Decision.Type.YES));
+        assertThat(decision.getExplanation(), equalTo("shard heap allocation decider is disabled"));
+    }
+
+    public void testYesDecisionWhenNodeIsNotIndexNode() {
+        final var decider = createShardHeapAllocationDecider(true, between(0, 100));
+
+        final String nodeId = randomIdentifier();
+        final ShardRouting shardRouting = createShardRouting();
+        final RoutingAllocation routingAllocation = createRoutingAllocation(
+            decider,
+            nodeId,
+            shardRouting,
+            createClusterInfo(Map.of(nodeId, randomLongBetween(0, 100)))
+        );
+        final Decision decision = decider.canAllocate(
+            shardRouting,
+            routingAllocation.routingNodes().node("search-node"),
+            routingAllocation
+        );
+        assertThat(decision.type(), equalTo(Decision.Type.YES));
+        assertThat(decision.getExplanation(), equalTo("shard heap allocation decider is applicable only to index nodes"));
+    }
+
+    public void testYesDecisionWhenUsageMetricIsMissing() {
+        final var decider = createShardHeapAllocationDecider(true, between(0, 100));
+        final String nodeId = randomIdentifier();
+        final ShardRouting shardRouting = createShardRouting();
+        final RoutingAllocation routingAllocation = createRoutingAllocation(
+            decider,
+            nodeId,
+            shardRouting,
+            createClusterInfo(Map.of(nodeId, randomLongBetween(0, 100)))
+        );
+        final Decision decision = decider.canAllocate(
+            shardRouting,
+            routingAllocation.routingNodes().node("not-" + nodeId),
+            routingAllocation
+        );
+        assertThat(decision.type(), equalTo(Decision.Type.YES));
+        assertThat(decision.getExplanation(), containsString("no shard heap estimation available for node [not-" + nodeId + "]"));
+    }
+
+    public void testYesDecisionWhenUsageBelowLowWatermark() {
+        final var decider = createShardHeapAllocationDecider(true, 85);
+
+        final String nodeId = randomIdentifier();
+        final ShardRouting shardRouting = createShardRouting();
+        final RoutingAllocation routingAllocation = createRoutingAllocation(
+            decider,
+            nodeId,
+            shardRouting,
+            createClusterInfo(Map.of(nodeId, randomLongBetween(0, 85)))
+        );
+        final Decision decision = decider.canAllocate(shardRouting, routingAllocation.routingNodes().node(nodeId), routingAllocation);
+        assertThat(decision.type(), equalTo(Decision.Type.YES));
+        assertThat(decision.getExplanation(), containsString("sufficient shard heap available on node [" + nodeId + "]"));
+    }
+
+    public void testNoDecisionWhenUsageAboveLowWatermark() {
+        final var decider = createShardHeapAllocationDecider(true, 85);
+
+        final String nodeId = randomIdentifier();
+        final ShardRouting shardRouting = createShardRouting();
+        final RoutingAllocation routingAllocation = createRoutingAllocation(
+            decider,
+            nodeId,
+            shardRouting,
+            createClusterInfo(Map.of(nodeId, randomLongBetween(86, 100)))
+        );
+        final Decision decision = decider.canAllocate(shardRouting, routingAllocation.routingNodes().node(nodeId), routingAllocation);
+        assertThat(decision.type(), equalTo(Decision.Type.NO));
+        assertThat(decision.getExplanation(), containsString("insufficient shard heap available on node [" + nodeId + "]"));
+    }
+
+    public void testAllocationExplain() {
+        final var decider = createShardHeapAllocationDecider(true, 85);
+
+        final String nodeId = randomIdentifier();
+        final ShardRouting shardRouting = createShardRouting();
+
+        final ClusterInfo clusterInfo = createClusterInfo(Map.of(nodeId, randomLongBetween(0, 85), "not-" + nodeId, 100L));
+        final var routingAllocation = createRoutingAllocation(decider, nodeId, shardRouting, clusterInfo);
+        final var allocationService = createAllocationService(decider, clusterInfo);
+
+        final var shardAllocationDecision = allocationService.explainShardAllocation(shardRouting, routingAllocation);
+        final var nodeDecisions = shardAllocationDecision.getAllocateDecision().getNodeDecisions();
+        assertNotNull(nodeDecisions);
+        assertThat(nodeDecisions.size(), equalTo(3));
+
+        final String explanation = Strings.collectionToDelimitedString(nodeDecisions.stream().map(Strings::toString).toList(), "\n");
+        assertExplanationResult(explanation, nodeDecisions, Decision.Type.YES, "sufficient shard heap available on node [" + nodeId + "]");
+        assertExplanationResult(
+            explanation,
+            nodeDecisions,
+            Decision.Type.NO,
+            "insufficient shard heap available on node [not-" + nodeId + "]"
+        );
+        assertExplanationResult(
+            explanation,
+            nodeDecisions,
+            Decision.Type.YES,
+            "shard heap allocation decider is applicable only to index nodes"
+        );
+    }
+
+    private static void assertExplanationResult(
+        String explanation,
+        List<NodeAllocationResult> nodeDecisions,
+        Decision.Type yes,
+        String message
+    ) {
+        assertTrue(
+            explanation,
+            nodeDecisions.stream()
+                .anyMatch(
+                    nodeDecision -> nodeDecision.getCanAllocateDecision()
+                        .getDecisions()
+                        .stream()
+                        .anyMatch(decision -> decision.type().equals(yes) && decision.getExplanation().startsWith(message))
+                )
+        );
+    }
+
+    public void testAllocationBasedOnShardHeapMemoryUsage() {
+        final String nodeId = randomIdentifier();
+        final ShardRouting shardRouting = createShardRouting();
+
+        final var decider = createShardHeapAllocationDecider(true, 85);
+        final ClusterState initialState = createClusterState(nodeId, shardRouting);
+        final var allocationService = createAllocationService(
+            decider,
+            createClusterInfo(Map.of(nodeId, randomLongBetween(0, 85), "not-" + nodeId, 100L))
+        );
+
+        final var newState = applyStartedShardsUntilNoChange(initialState, allocationService);
+
+        assertFalse(newState.getRoutingNodes().toString(), newState.getRoutingNodes().hasUnassignedShards());
+        final var indexRoutingTable = newState.routingTable(ProjectId.DEFAULT).index(shardRouting.index());
+        indexRoutingTable.allShards().forEach(indexShardRoutingTable -> {
+            final var primaryShard = indexShardRoutingTable.primaryShard();
+            assertNotNull(primaryShard);
+            assertThat(primaryShard.currentNodeId(), equalTo(nodeId));
+        });
+    }
+
+    private static ShardHeapAllocationDecider createShardHeapAllocationDecider(boolean enabled, int percent) {
+        final var clusterSettings = new ClusterSettings(
+            Settings.builder()
+                .put(InternalClusterInfoService.CLUSTER_ROUTING_ALLOCATION_SHARD_HEAP_THRESHOLD_DECIDER_ENABLED.getKey(), enabled)
+                .put(ShardHeapAllocationDecider.CLUSTER_ROUTING_ALLOCATION_SHARD_HEAP_LOW_WATERMARK.getKey(), percent + "%")
+                .build(),
+            Set.of(
+                InternalClusterInfoService.CLUSTER_ROUTING_ALLOCATION_SHARD_HEAP_THRESHOLD_DECIDER_ENABLED,
+                ShardHeapAllocationDecider.CLUSTER_ROUTING_ALLOCATION_SHARD_HEAP_LOW_WATERMARK
+            )
+        );
+        return new ShardHeapAllocationDecider(clusterSettings);
+    }
+
+    private static ShardRouting createShardRouting() {
+        return ShardRouting.newUnassigned(
+            new ShardId(randomIdentifier(), IndexMetadata.INDEX_UUID_NA_VALUE, between(0, 2)),
+            true,
+            RecoverySource.EmptyStoreRecoverySource.INSTANCE,
+            TestShardRouting.buildUnassignedInfo("auto generated for test"),
+            ShardRouting.Role.INDEX_ONLY
+        );
+    }
+
+    private RoutingAllocation createRoutingAllocation(
+        AllocationDecider decider,
+        String nodeId,
+        ShardRouting shardRouting,
+        ClusterInfo clusterInfo
+    ) {
+        final var routingAllocation = new RoutingAllocation(
+            createAllocationDeciders(decider),
+            createClusterState(nodeId, shardRouting),
+            clusterInfo,
+            SnapshotShardSizeInfo.EMPTY,
+            System.nanoTime()
+        );
+        routingAllocation.debugDecision(true);
+        return routingAllocation;
+    }
+
+    private AllocationService createAllocationService(AllocationDecider decider, ClusterInfo clusterInfo) {
+
+        return new AllocationService(
+            createAllocationDeciders(decider),
+            new TestGatewayAllocator(),
+            new BalancedShardsAllocator(Settings.EMPTY),
+            () -> clusterInfo,
+            EmptySnapshotsInfoService.INSTANCE,
+            new StatelessShardRoutingRoleStrategy()
+        );
+    }
+
+    private ClusterInfo createClusterInfo(Map<String, Long> nodeShardHeapUsagePercent) {
+        final var clusterInfo = mock(ClusterInfo.class);
+        when(clusterInfo.getShardHeapUsages()).thenReturn(
+            nodeShardHeapUsagePercent.entrySet()
+                .stream()
+                .collect(
+                    Collectors.toUnmodifiableMap(entry -> entry.getKey(), entry -> createNodeHeapUsage(entry.getKey(), entry.getValue()))
+                )
+        );
+        return clusterInfo;
+    }
+
+    private static AllocationDeciders createAllocationDeciders(AllocationDecider decider) {
+        return new AllocationDeciders(
+            Set.of(
+                decider,
+                new ReplicaAfterPrimaryActiveAllocationDecider(),
+                new SameShardAllocationDecider(new ClusterSettings(Settings.EMPTY, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS)),
+                new StatelessAllocationDecider()
+            )
+        );
+    }
+
+    private static ClusterState createClusterState(String nodeId, ShardRouting shardRouting) {
+        var discoveryNodesBuilder = DiscoveryNodes.builder();
+        discoveryNodesBuilder.add(newNode(nodeId, Set.of(DiscoveryNodeRole.INDEX_ROLE)))
+            .add(newNode("not-" + nodeId, Set.of(DiscoveryNodeRole.INDEX_ROLE)))
+            .add(newNode("search-node", Set.of(DiscoveryNodeRole.SEARCH_ROLE)));
+
+        final var projectMetadata = ProjectMetadata.builder(ProjectId.DEFAULT)
+            .put(
+                IndexMetadata.builder(shardRouting.getIndexName())
+                    .settings(settings(IndexVersion.current()))
+                    .numberOfShards(between(shardRouting.id() + 1, shardRouting.id() + 3))
+                    .numberOfReplicas(0)
+            )
+            .build();
+
+        return ClusterState.builder(ClusterName.DEFAULT)
+            .nodes(discoveryNodesBuilder)
+            .putProjectMetadata(projectMetadata)
+            .putRoutingTable(
+                ProjectId.DEFAULT,
+                RoutingTable.builder(new StatelessShardRoutingRoleStrategy())
+                    .addAsNew(projectMetadata.index(shardRouting.getIndexName()))
+                    .build()
+            )
+            .build();
+    }
+
+    private ShardHeapUsage createNodeHeapUsage(String nodeId, long usagePercent) {
+        final var totalInBytes = ByteSizeValue.ofGb(between(1, 32)).getBytes();
+        final var usedInBytes = (long) Math.floor(totalInBytes * usagePercent / 100.0d);
+        return new ShardHeapUsage(nodeId, totalInBytes, usedInBytes);
+    }
+}

eed87b33230a8ccbf4359fbfd53154c8ca52c14d
Jonathan Buttner <56361221+jonathan-buttner@users.noreply.github.com>
2025-06-18 14:46:07 -0400

    [ML] Custom service adding support for the semantic text field (#129558)
    * Adding chunking tests
    
    * adjusting default batch size
    
    (cherry picked from commit daf4fca123b9a7ef575f3540c2dc8762601515b2)
    
    # Conflicts:
    #	server/src/main/java/org/elasticsearch/TransportVersions.java
    #	x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/custom/CustomServiceSettings.java

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -243,6 +243,7 @@ public class TransportVersions {
     public static final TransportVersion IDP_CUSTOM_SAML_ATTRIBUTES_ALLOW_LIST_8_19 = def(8_841_0_50);
     public static final TransportVersion SETTINGS_IN_DATA_STREAMS_8_19 = def(8_841_0_51);
     public static final TransportVersion ML_INFERENCE_CUSTOM_SERVICE_REMOVE_ERROR_PARSING_8_19 = def(8_841_0_52);
+    public static final TransportVersion ML_INFERENCE_CUSTOM_SERVICE_EMBEDDING_BATCH_SIZE_8_19 = def(8_841_0_53);
 
     /*
      * STOP! READ THIS FIRST! No, really,

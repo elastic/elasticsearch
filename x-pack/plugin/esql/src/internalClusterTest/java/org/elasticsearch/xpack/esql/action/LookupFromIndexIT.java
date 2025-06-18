58b243cb0cbb7e32f753919627c17c993beb0304
Alexander Spies <alexander.spies@elastic.co>
2025-06-18 13:18:26 +0200

    ESQL: Fix some more usages of field attribute names in LOOKUP JOIN (#129355) (#129578)
    * Add a test that demonstrates that LOOKUP JOIN works with union types.
    * Fix usage of FieldAttribute#name where we should use FieldAttribute#fieldName in LOOKUP JOIN.
    * Refactor LookupJoinTypesIT
    
    This doesn't fix any bugs per se, but will avoid bwc problems once we propagate union type field attributes into Join LogicalPlan nodes.
    
    (cherry picked from commit 65cbccc1fdb73b64c2678294095ec62bf404fb8c)
    
    # Conflicts:
    #	x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/LookupJoinTypesIT.java

--- x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/LookupFromIndexIT.java
+++ x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/LookupFromIndexIT.java
@@ -48,6 +48,7 @@ import org.elasticsearch.tasks.TaskId;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.xpack.core.async.AsyncExecutionId;
 import org.elasticsearch.xpack.esql.core.expression.Alias;
+import org.elasticsearch.xpack.esql.core.expression.FieldAttribute;
 import org.elasticsearch.xpack.esql.core.expression.ReferenceAttribute;
 import org.elasticsearch.xpack.esql.core.tree.Source;
 import org.elasticsearch.xpack.esql.core.type.DataType;
@@ -222,7 +223,7 @@ public class LookupFromIndexIT extends AbstractEsqlIntegTestCase {
                 keyType,
                 "lookup",
                 "lookup",
-                "key",
+                new FieldAttribute.FieldName("key"),
                 List.of(new Alias(Source.EMPTY, "l", new ReferenceAttribute(Source.EMPTY, "l", DataType.LONG))),
                 Source.EMPTY
             );

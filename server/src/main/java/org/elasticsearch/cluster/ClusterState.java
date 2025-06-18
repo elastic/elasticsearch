20e92964d705ae29829be82a8010f59c9d06d5c7
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-06-18 19:08:10 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/cluster/ClusterState.java
+++ server/src/main/java/org/elasticsearch/cluster/ClusterState.java
@@ -47,7 +47,6 @@ import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.io.stream.VersionedNamedWriteable;
 import org.elasticsearch.common.io.stream.Writeable;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.Maps;
 import org.elasticsearch.common.xcontent.ChunkedToXContent;
 import org.elasticsearch.common.xcontent.ChunkedToXContentHelper;

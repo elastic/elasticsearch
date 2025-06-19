aebb9e8d04bdd1d409df9a8a70de302b952e5a41
Iraklis Psaroudakis <kingherc@gmail.com>
2025-06-19 17:54:27 +0300

    Revert "getMaxSeqNo withEngine"
    This reverts commit a8960ea0cd7198566db860a9f53d1b4a2069d863.

--- server/src/main/java/org/elasticsearch/index/shard/LocalShardSnapshot.java
+++ server/src/main/java/org/elasticsearch/index/shard/LocalShardSnapshot.java
@@ -40,7 +40,7 @@ final class LocalShardSnapshot implements Closeable {
     }
 
     long maxSeqNo() {
-        return shard.withEngine(engine -> engine.getSeqNoStats(-1)).getMaxSeqNo();
+        return shard.getEngine().getSeqNoStats(-1).getMaxSeqNo();
     }
 
     long maxUnsafeAutoIdTimestamp() {

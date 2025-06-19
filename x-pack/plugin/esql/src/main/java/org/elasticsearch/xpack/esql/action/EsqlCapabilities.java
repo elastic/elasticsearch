2db9127320ae03403e97b6c6cc767afe6fc738bc
Jan Kuipers <148754765+jan-elastic@users.noreply.github.com>
2025-06-19 13:11:25 +0200

    [8.19] Backport ES|QL sample agg function (#129627)
    * ES|QL SAMPLE aggregation function (#127629)
    
    * ES|QL SAMPLE aggregation function
    
    * [CI] Auto commit changes from spotless
    
    * ThreadLocalRandom -> SplittableRandom
    
    * Update docs/changelog/127629.yaml
    
    * fix yaml test
    
    * Add SampleTests
    
    * docs + example
    
    * polish code
    
    * mark generated imports
    
    * comment with algorith description
    
    * use Randomness.get()
    
    * close properly
    
    * type checks
    
    * reuse hash
    
    * regen some files
    
    * [CI] Auto commit changes from spotless
    
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
    
    * Fix + unmute SampleTests (#127959)
    
    * Fix memory tracking of ES|QL sample agg (#128467)
    
    * Fix memory tracking of ES|QL sample agg
    
    * [CI] Auto commit changes from spotless
    
    * polish code
    
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
    
    * ESQL: Unclean generated imports (#127723)
    
    This removes a ton of the tricky juggling we do for generated java files
    to keep the imports in order. Instead, we just live with them being out
    of order a little. It's not great, but it's so so so much easier than
    the terrible juggling we had been doing.
    
    * ESQL: Disable format checks on generated imports (#127648)
    
    This builds the infrastructure to disable spotless and some checkstyle
    rules on generated imports. This works around the most frustrating part
    of ESQL's string template generated files - the imports. It allows
    unused and out of order imports. This can let us remove a lot of
    cumbersome, tricky, and fairly useless `$if$` blocks from the templates.
    
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
    Co-authored-by: Nik Everett <nik9000@gmail.com>

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -971,6 +971,7 @@ public class EsqlCapabilities {
          * Support for LIKE operator with a list of patterns
          */
         LIKE_WITH_LIST_OF_PATTERNS,
+
         /**
          * Support for the SAMPLE command
          */
@@ -979,7 +980,12 @@ public class EsqlCapabilities {
         /**
          * Support parameters for SAMPLE command.
          */
-        PARAMETER_FOR_SAMPLE;
+        PARAMETER_FOR_SAMPLE,
+
+        /**
+         * Support for the SAMPLE aggregation function
+         */
+        AGG_SAMPLE;
 
         private final boolean enabled;
 

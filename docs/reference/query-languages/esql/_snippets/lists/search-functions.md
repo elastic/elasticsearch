415c031b77a421d737a8f2124fa5cd017e1fc4b4
Liam Thompson <leemthompo@gmail.com>
2025-06-23 20:51:44 +0200

    render docs, tweak some applies_to metadata in docs gen code
    - **rendered docs (md):**
      - kql: removed serverless preview, added ga 9.1.0
      - match: removed serverless preview, added ga 9.1.0
      - match_phrase: changed from preview 9.1.0 to unavailable 9.0 + ga 9.1.0
      - qstr: removed serverless preview, added ga 9.1.0
      - search functions list: removed bullet point before term function
    
    - **docs generation code (java):**
      - match_phrase: updated function info annotations to unavailable 9.0 + ga 9.1.0
      - query_string: uncommented ga 9.1.0 annotation

--- docs/reference/query-languages/esql/_snippets/lists/search-functions.md
+++ docs/reference/query-languages/esql/_snippets/lists/search-functions.md
@@ -2,4 +2,4 @@
 * [`MATCH`](../../functions-operators/search-functions.md#esql-match)
 * [`MATCH_PHRASE`](../../functions-operators/search-functions.md#esql-match_phrase)
 * [`QSTR`](../../functions-operators/search-functions.md#esql-qstr)
-* % * [preview] [`TERM`](../../functions-operators/search-functions.md#esql-term)
+% * [preview] [`TERM`](../../functions-operators/search-functions.md#esql-term)
--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/QueryString.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/fulltext/QueryString.java
@@ -104,8 +104,9 @@ public class QueryString extends FullTextFunction implements OptionalArgument {
 
     @FunctionInfo(
         returnType = "boolean",
-        appliesTo = { @FunctionAppliesTo(lifeCycle = FunctionAppliesToLifecycle.PREVIEW, version = "9.0.0") },
-        // @FunctionAppliesTo(lifeCycle = FunctionAppliesToLifecycle.GA, version = "9.1.0") },
+        appliesTo = {
+            @FunctionAppliesTo(lifeCycle = FunctionAppliesToLifecycle.PREVIEW, version = "9.0.0"),
+            @FunctionAppliesTo(lifeCycle = FunctionAppliesToLifecycle.GA, version = "9.1.0") },
         description = "Performs a <<query-dsl-query-string-query,query string query>>. "
             + "Returns true if the provided query string matches the row.",
         examples = {

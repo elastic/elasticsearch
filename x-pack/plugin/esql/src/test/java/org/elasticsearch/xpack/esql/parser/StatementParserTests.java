2667a2d4ba5bf6ce0a05ed0e8b19e006ef3a56b7
Pawan Kartik <pawankartik.chitrapu@elastic.co>
2025-06-26 16:11:41 +0100

    Fix: prevent duplication of "invalid index name" string in the final exception error message (#130027)
    * Use `throwInvalidIndexNameException()` to throw invalid ex after
    dropping asterisk in `IdentifierBuilder#resolveAndValidateIndex()`
    
    * Assert the message in test
    
    * Refactor
    
    * drop invalid chars from assertion string due to randomisation issue
    
    * Re-assert invalid chars
    
    * Update docs/changelog/130027.yaml

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/parser/StatementParserTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/parser/StatementParserTests.java
@@ -3230,9 +3230,15 @@ public class StatementParserTests extends AbstractStatementParserTests {
             var randomInvalidChar = randomFrom(invalidChars);
 
             // Construct the new invalid index pattern.
-            var remoteIndexWithInvalidChar = quote(randomIdentifier() + ":" + "foo" + randomInvalidChar + "bar");
+            var invalidIndexName = "foo" + randomInvalidChar + "bar";
+            var remoteIndexWithInvalidChar = quote(randomIdentifier() + ":" + invalidIndexName);
             var query = "FROM " + randomIndex + "," + remoteIndexWithInvalidChar;
-            expectError(query, "must not contain the following characters [' ','\"',',','/','<','>','?','\\','|']");
+            expectError(
+                query,
+                "Invalid index name ["
+                    + invalidIndexName
+                    + "], must not contain the following characters [' ','\"',',','/','<','>','?','\\','|']"
+            );
         }
 
         // Colon outside a quoted string should result in an ANTLR error: a comma is expected.

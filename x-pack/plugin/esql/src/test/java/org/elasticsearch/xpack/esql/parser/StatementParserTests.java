338628416b3a54af3154be6418dce8bebea84aae
Fang Xing <155562079+fang-xing-esql@users.noreply.github.com>
2025-07-02 14:43:15 -0400

    ES|QL fix StatementParserTests to take into consideration SNAPSHOT (#130098) (#130458)
    (cherry picked from commit 78eeeccb48fe33fa319e1a2f1122b557a509bf66)
    
    Co-authored-by: Luigi Dell'Aquila <luigi.dellaquila@gmail.com>

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/parser/StatementParserTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/parser/StatementParserTests.java
@@ -3176,7 +3176,7 @@ public class StatementParserTests extends AbstractStatementParserTests {
     }
 
     public void testInvalidFromPatterns() {
-        var sourceCommands = new String[] { "FROM", "TS" };
+        var sourceCommands = Build.current().isSnapshot() ? new String[] { "FROM", "TS" } : new String[] { "FROM" };
         var indexIsBlank = "Blank index specified in index pattern";
         var remoteIsEmpty = "remote part is empty";
         var invalidDoubleColonUsage = "invalid usage of :: separator";

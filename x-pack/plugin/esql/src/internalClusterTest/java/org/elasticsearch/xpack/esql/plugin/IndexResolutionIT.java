d31fa8f643ac42d852275b9853c27875b261ce0e
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2025-07-02 16:41:34 +0200

    implement todos

--- x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/plugin/IndexResolutionIT.java
+++ x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/plugin/IndexResolutionIT.java
@@ -7,10 +7,20 @@
 
 package org.elasticsearch.xpack.esql.plugin;
 
+import org.elasticsearch.action.admin.indices.template.put.TransportPutComposableIndexTemplateAction;
+import org.elasticsearch.action.datastreams.CreateDataStreamAction;
 import org.elasticsearch.cluster.block.ClusterBlockException;
+import org.elasticsearch.cluster.metadata.ComposableIndexTemplate;
+import org.elasticsearch.common.util.CollectionUtils;
+import org.elasticsearch.datastreams.DataStreamsPlugin;
+import org.elasticsearch.index.IndexNotFoundException;
+import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.xpack.esql.VerificationException;
 import org.elasticsearch.xpack.esql.action.AbstractEsqlIntegTestCase;
 import org.elasticsearch.xpack.esql.action.EsqlQueryResponse;
+
+import java.util.Collection;
+import java.util.List;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.elasticsearch.xpack.esql.action.EsqlQueryRequest.syncEsqlQueryRequest;
@@ -18,6 +28,11 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
 public class IndexResolutionIT extends AbstractEsqlIntegTestCase {
+
+    @Override
+    protected Collection<Class<? extends Plugin>> nodePlugins() {
+        return CollectionUtils.appendToCopy(super.nodePlugins(), DataStreamsPlugin.class);
+    }
 
     public void testResolvesConcreteIndex() {
         assertAcked(client().admin().indices().prepareCreate("index-1"));
@@ -39,7 +54,27 @@ public class IndexResolutionIT extends AbstractEsqlIntegTestCase {
     }
 
     public void testResolvesDataStream() {
-        // TODO
+        assertAcked(
+            client().execute(
+                TransportPutComposableIndexTemplateAction.TYPE,
+                new TransportPutComposableIndexTemplateAction.Request("data-stream-1-template").indexTemplate(
+                    ComposableIndexTemplate.builder()
+                        .indexPatterns(List.of("data-stream-1*"))
+                        .dataStreamTemplate(new ComposableIndexTemplate.DataStreamTemplate())
+                        .build()
+                )
+            )
+        );
+        assertAcked(
+            client().execute(
+                CreateDataStreamAction.INSTANCE,
+                new CreateDataStreamAction.Request(TEST_REQUEST_TIMEOUT, TEST_REQUEST_TIMEOUT, "data-stream-1")
+            )
+        );
+
+        try (var response = run(syncEsqlQueryRequest().query("FROM data-stream-1"))) {
+            assertOk(response);
+        }
     }
 
     public void testResolvesPattern() {
@@ -82,7 +117,18 @@ public class IndexResolutionIT extends AbstractEsqlIntegTestCase {
     }
 
     public void testPartialResolution() {
-        // TODO
+        assertAcked(client().admin().indices().prepareCreate("index-1"));
+        assertAcked(client().admin().indices().prepareCreate("index-2"));
+        indexRandom(true, "index-2", 10);
+
+        try (var response = run(syncEsqlQueryRequest().query("FROM index-1,nonexisting"))) {
+            assertOk(response);
+        }
+        expectThrows(
+            IndexNotFoundException.class,
+            equalTo("no such index [nonexisting]"),
+            () -> run(syncEsqlQueryRequest().query("FROM index-2,nonexisting"))
+        );
     }
 
     private static void assertOk(EsqlQueryResponse response) {

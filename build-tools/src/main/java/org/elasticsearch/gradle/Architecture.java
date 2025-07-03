bc3480641a4349bc85f948f189d4d8b295e5461a
Rene Groeschke <rene@elastic.co>
2025-07-03 15:42:59 +0200

    Fix rpm references for architecture

--- build-tools/src/main/java/org/elasticsearch/gradle/Architecture.java
+++ build-tools/src/main/java/org/elasticsearch/gradle/Architecture.java
@@ -9,20 +9,25 @@
 
 package org.elasticsearch.gradle;
 
+/**
+ * TODO We need to clean that up when other (binary) components have aligned on architecture namings
+ * */
 public enum Architecture {
 
-    AMD64("amd64", "x64", "x86_64", "amd64", "linux/amd64"),
-    AARCH64("aarch64", "aarch64", "aarch64", "arm64", "linux/arm64");
+    AMD64("amd64", "x64", "X86_64", "x86_64", "amd64", "linux/amd64"),
+    AARCH64("aarch64", "aarch64", "aarch64", "aarch64", "arm64", "linux/arm64");
 
     public final String classifier;
     public final String dockerPlatform;
     public final String jdkClassifier;
     public final String mlClassifier;
     public final String debianClassifier;
+    public final String rpmClassifier;
 
-    Architecture(String classifier, String jdkClassifier, String mlClassifier, String debianClassifier, String dockerPlatform) {
+    Architecture(String classifier, String jdkClassifier, String rpmClassifier, String mlClassifier, String debianClassifier, String dockerPlatform) {
         this.classifier = classifier;
         this.jdkClassifier = jdkClassifier;
+        this.rpmClassifier = rpmClassifier;
         this.mlClassifier = mlClassifier;
         this.debianClassifier = debianClassifier;
         this.dockerPlatform = dockerPlatform;

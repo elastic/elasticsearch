5ea45d463bcb27493352501c413c2408a5b4b3ad
Jonathan Buttner <56361221+jonathan-buttner@users.noreply.github.com>
2025-07-07 16:19:37 -0400

    [ML] Removing custom service from service api (#130739)
    * Removing custom service from service api
    
    * Fixing tests
    
    (cherry picked from commit 02b2f5eb66853773f9db61b56efb4c583505e4dc)
    
    # Conflicts:
    #	x-pack/plugin/inference/qa/inference-service-tests/src/javaRestTest/java/org/elasticsearch/xpack/inference/InferenceGetServicesIT.java

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/custom/CustomService.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/custom/CustomService.java
@@ -355,6 +355,12 @@ public class CustomService extends SenderService {
         return TransportVersions.INFERENCE_CUSTOM_SERVICE_ADDED_8_19;
     }
 
+    @Override
+    public boolean hideFromConfigurationApi() {
+        // The Custom service is very configurable so we're going to hide it from being exposed in the service API.
+        return true;
+    }
+
     public static class Configuration {
         public static InferenceServiceConfiguration get() {
             return configuration.getOrCompute();

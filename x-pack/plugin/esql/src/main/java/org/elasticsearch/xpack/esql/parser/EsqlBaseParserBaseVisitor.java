024228dba8186f9f0660f530b83cbcdc477ab89b
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2025-07-09 18:58:47 +0200

    ES|QL: Disallow brackets in unquoted index patterns (#130427) (#130938)

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/parser/EsqlBaseParserBaseVisitor.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/parser/EsqlBaseParserBaseVisitor.java
@@ -383,6 +383,13 @@ public class EsqlBaseParserBaseVisitor<T> extends AbstractParseTreeVisitor<T> im
    * <p>The default implementation returns the result of calling
    * {@link #visitChildren} on {@code ctx}.</p>
    */
+  @Override public T visitEnrichPolicyName(EsqlBaseParser.EnrichPolicyNameContext ctx) { return visitChildren(ctx); }
+  /**
+   * {@inheritDoc}
+   *
+   * <p>The default implementation returns the result of calling
+   * {@link #visitChildren} on {@code ctx}.</p>
+   */
   @Override public T visitEnrichWithClause(EsqlBaseParser.EnrichWithClauseContext ctx) { return visitChildren(ctx); }
   /**
    * {@inheritDoc}

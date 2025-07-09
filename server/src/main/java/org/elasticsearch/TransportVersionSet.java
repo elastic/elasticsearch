c0b7219760185bd7a7c0c459d9956d9ddb0c769f
John Verwolf <osjdverwolf@gmail.com>
2025-07-09 14:57:50 -0700

    wip

--- server/src/main/java/org/elasticsearch/TransportVersionSet.java
+++ server/src/main/java/org/elasticsearch/TransportVersionSet.java
@@ -11,12 +11,22 @@ package org.elasticsearch;
 
 import org.elasticsearch.xcontent.ConstructingObjectParser;
 import org.elasticsearch.xcontent.ParseField;
+import org.elasticsearch.xcontent.XContentParser;
+import org.elasticsearch.xcontent.XContentParserConfiguration;
+import org.elasticsearch.xcontent.json.JsonXContent;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TransportVersionSet {
 
@@ -64,12 +74,17 @@ public class TransportVersionSet {
         return transportVersionSet;
     }
 
+
     private final String name;
     private final List<TransportVersion> versions;
 
     private TransportVersionSet(String name, List<TransportVersion> versions) {
         this.name = name;
         this.versions = versions;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public boolean isCompatible(TransportVersion version) {
@@ -79,4 +94,90 @@ public class TransportVersionSet {
         }
         return compatible;
     }
+
+    public static TransportVersionSet fromXContent(XContentParser parser) throws IOException {
+        return PARSER.apply(parser, null);
+    }
+
+    // TODO: Should this be in this class?
+    private static TransportVersionSet fromJSONInputStream(InputStream inputStream) throws IOException {
+        XContentParser parser = JsonXContent.jsonXContent.createParser(XContentParserConfiguration.EMPTY, inputStream);
+        return TransportVersionSet.fromXContent(parser);
+    }
+
+
+    private static class VersionsHolder {
+        private static final String MANIFEST_LOCATION = "META-INF/transport-versions-files-manifest.txt";
+        private static final String METADATA_LOCATION = "org/elasticsearch/transport/";
+        private static final String LATEST_SUFFIX = "-LATEST";
+
+
+        private static final List<TransportVersion> ALL_VERSIONS_SORTED;
+        private static final Map<Integer, TransportVersion> ALL_VERSIONS_MAP;
+        private static final Map<String, TransportVersionSet> ALL_VERSIONS_SETS_MAP;
+        private static final TransportVersionSet LATEST;
+
+        static {
+            var locallyDefinedVersionSets = loadTransportVersionSets();
+            var extendedVersionSets = new ArrayList<TransportVersionSet>(); // TODO add SPI for serverless
+
+            var allVersionSets = Stream.concat(
+                locallyDefinedVersionSets.stream(),
+                extendedVersionSets.stream()
+            ).toList();
+
+            ALL_VERSIONS_SORTED = allVersionSets.stream().flatMap(tvSet -> tvSet.versions.stream()).sorted().toList();
+
+            ALL_VERSIONS_MAP = ALL_VERSIONS_SORTED.stream()
+                .collect(Collectors.toUnmodifiableMap(TransportVersion::id, Function.identity()));
+
+            ALL_VERSIONS_SETS_MAP = allVersionSets.stream()
+                .collect(Collectors.toUnmodifiableMap(TransportVersionSet::getName, Function.identity()));
+
+            LATEST = getLatestTVSet();
+        }
+
+        private static TransportVersionSet getLatestTVSet() {
+
+            var major = Version.CURRENT.major;
+            var minor = Version.CURRENT.minor;
+            var fileName =  major + "." + minor + "-" + LATEST_SUFFIX;
+
+            var path = METADATA_LOCATION + fileName;
+            return loadTransportVersionSet(path); // todo, use a different format?
+        }
+
+        private static List<TransportVersionSet> loadTransportVersionSets() {
+            var transportVersionSets = new ArrayList<TransportVersionSet>();
+            for (var fileName : loadTransportVersionSetFileNames()) {
+                var path = METADATA_LOCATION + fileName;
+                transportVersionSets.add(loadTransportVersionSet(path));
+            }
+            return transportVersionSets;
+        }
+
+        // TODO will getResourceAsStream work for IntelliJ?
+        private static List<String> loadTransportVersionSetFileNames() {
+            try (var manifestIS = VersionsHolder.class.getResourceAsStream(MANIFEST_LOCATION)) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(manifestIS));
+                return reader.lines().filter(line -> line.isBlank() == false).toList();
+            } catch (IOException e) {
+                throw new RuntimeException("Transport version metadata manifest file not found at " + MANIFEST_LOCATION, e);
+            }
+        }
+
+        private static TransportVersionSet loadTransportVersionSet(String path) {
+            try (var fileStream = VersionsHolder.class.getResourceAsStream(path)) {
+                if (fileStream != null) {
+                    return TransportVersionSet.fromJSONInputStream(fileStream);
+                } else {
+                    throw new RuntimeException("Input stream is null");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to load TransportVersionSet at path: " + path +
+                    " specified in the manifest file: " + MANIFEST_LOCATION, e);
+            }
+        }
+    }
+
 }

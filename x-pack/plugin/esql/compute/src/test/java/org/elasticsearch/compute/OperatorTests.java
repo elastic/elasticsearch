f2e3537ee8040cf675a92f6887f86d666677747d
Przemyslaw Witek <przemyslaw.witek@elastic.co>
2025-07-10 17:32:05 +0200

    Fix test

--- x-pack/plugin/esql/compute/src/test/java/org/elasticsearch/compute/OperatorTests.java
+++ x-pack/plugin/esql/compute/src/test/java/org/elasticsearch/compute/OperatorTests.java
@@ -313,11 +313,13 @@ public class OperatorTests extends MapperServiceTestCase {
                         driverContext
                     )
                 );
+                List<BlockHash.GroupSpec> groups = List.of(new BlockHash.GroupSpec(0, ElementType.BYTES_REF));
                 operators.add(
                     new HashAggregationOperator(
+                        groups,
                         List.of(new ValuesLongAggregatorFunctionSupplier().groupingAggregatorFactory(FINAL, List.of(1))),
                         () -> BlockHash.build(
-                            List.of(new BlockHash.GroupSpec(0, ElementType.BYTES_REF)),
+                            groups,
                             driverContext.blockFactory(),
                             randomPageSize(),
                             false

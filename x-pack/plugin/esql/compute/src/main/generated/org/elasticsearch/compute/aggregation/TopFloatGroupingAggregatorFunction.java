255412025135ebc2988dd76dd0f8c926cfc8aaf9
Nhat Nguyen <nhat.nguyen@elastic.co>
2025-07-11 09:53:27 -0700

    Rename prepareProcessPage to prepareProcessRawInputPage (#130487) (#131110)
    This PR proposes renaming `prepareProcessPage` to 
    `prepareProcessRawInputPage` for two reasons:
    
    1. The name `prepareProcessPage` can be confusing, as it suggests 
    handling all types of input, but it only processes raw input, not
    intermediate input.
    
    2. I plan to add `prepareProcessIntermediateInputPage`, which can enable 
    optimizations for intermediate pages, such as leveraging ordinals from
    intermediate results.

--- x-pack/plugin/esql/compute/src/main/generated/org/elasticsearch/compute/aggregation/TopFloatGroupingAggregatorFunction.java
+++ x-pack/plugin/esql/compute/src/main/generated/org/elasticsearch/compute/aggregation/TopFloatGroupingAggregatorFunction.java
@@ -62,7 +62,7 @@ public final class TopFloatGroupingAggregatorFunction implements GroupingAggrega
   }
 
   @Override
-  public GroupingAggregatorFunction.AddInput prepareProcessPage(SeenGroupIds seenGroupIds,
+  public GroupingAggregatorFunction.AddInput prepareProcessRawInputPage(SeenGroupIds seenGroupIds,
       Page page) {
     FloatBlock valuesBlock = page.getBlock(channels.get(0));
     FloatVector valuesVector = valuesBlock.asVector();

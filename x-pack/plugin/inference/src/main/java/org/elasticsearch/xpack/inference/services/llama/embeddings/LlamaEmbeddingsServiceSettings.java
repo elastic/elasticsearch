06c7bd16bb470c66b088798992321bafef15dfd0
Jan Kazlouski <jan.kazlouski@elastic.co>
2025-07-15 15:04:46 +0300

    Add support for version checks in Llama settings and define new transport version

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/llama/embeddings/LlamaEmbeddingsServiceSettings.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/llama/embeddings/LlamaEmbeddingsServiceSettings.java
@@ -154,7 +154,14 @@ public class LlamaEmbeddingsServiceSettings extends FilteredXContentObject imple
 
     @Override
     public TransportVersion getMinimalSupportedVersion() {
+        assert false : "should never be called when supportsVersion is used";
         return TransportVersions.ML_INFERENCE_LLAMA_ADDED;
+    }
+
+    @Override
+    public boolean supportsVersion(TransportVersion version) {
+        return version.onOrAfter(TransportVersions.ML_INFERENCE_LLAMA_ADDED)
+            || version.isPatchFrom(TransportVersions.ML_INFERENCE_LLAMA_ADDED_8_19);
     }
 
     @Override

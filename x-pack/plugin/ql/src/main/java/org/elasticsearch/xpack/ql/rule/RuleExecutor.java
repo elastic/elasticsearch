6f34981212a4ef2b9f95949c4653e9ba48937af8
Julian Kiryakov <julian.kiryakov@elastic.co>
2025-07-14 20:21:35 -0400

    Remove some debugging code

--- x-pack/plugin/ql/src/main/java/org/elasticsearch/xpack/ql/rule/RuleExecutor.java
+++ x-pack/plugin/ql/src/main/java/org/elasticsearch/xpack/ql/rule/RuleExecutor.java
@@ -8,7 +8,6 @@ package org.elasticsearch.xpack.ql.rule;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.xpack.ql.tree.Node;
 import org.elasticsearch.xpack.ql.tree.NodeUtils;
@@ -138,7 +137,6 @@ public abstract class RuleExecutor<TreeType extends Node<TreeType>> {
     }
 
     protected final ExecutionInfo executeWithInfo(TreeType plan) {
-        Configurator.setLevel(log.getName(), org.apache.logging.log4j.Level.TRACE);
         TreeType currentPlan = plan;
 
         long totalDuration = 0;

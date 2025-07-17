571ec0a52c9512d2e775cb6c790a4a12b171b096
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-07-17 21:20:34 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/node/NodeConstruction.java
+++ server/src/main/java/org/elasticsearch/node/NodeConstruction.java
@@ -60,7 +60,6 @@ import org.elasticsearch.cluster.routing.BatchedRerouteService;
 import org.elasticsearch.cluster.routing.RerouteService;
 import org.elasticsearch.cluster.routing.allocation.AllocationService;
 import org.elasticsearch.cluster.routing.allocation.DiskThresholdMonitor;
-import org.elasticsearch.cluster.routing.allocation.NodeUsageStatsForThreadPoolsMonitor;
 import org.elasticsearch.cluster.routing.allocation.WriteLoadForecaster;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.cluster.version.CompatibilityVersions;

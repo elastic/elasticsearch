b4346d304fd4b2b0946ac251f49577d32c6842e6
Mashhur <mashhur.sattorov@elastic.co>
2025-07-18 09:49:32 -0700

    Export x-pack spatial module packages to make accessible to ES server module.

--- x-pack/plugin/spatial/src/main/java/module-info.java
+++ x-pack/plugin/spatial/src/main/java/module-info.java
@@ -19,8 +19,12 @@ module org.elasticsearch.xpack.spatial {
     requires org.apache.lucene.spatial3d;
 
     exports org.elasticsearch.xpack.spatial;
+    exports org.elasticsearch.xpack.spatial.action;
     exports org.elasticsearch.xpack.spatial.common;
     exports org.elasticsearch.xpack.spatial.search.aggregations.bucket.geogrid;
+    exports org.elasticsearch.xpack.spatial.index.fielddata;
+    exports org.elasticsearch.xpack.spatial.index.fielddata.plain;
+    exports org.elasticsearch.xpack.spatial.index.mapper;
 
     opens org.elasticsearch.xpack.spatial to org.elasticsearch.painless.spi;
 

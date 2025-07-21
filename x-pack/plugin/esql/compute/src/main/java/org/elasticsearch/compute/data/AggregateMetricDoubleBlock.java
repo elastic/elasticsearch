c0fe07194a24de58fed0d935524bdadc63942e68
Larisa Motova <larisa.motova@elastic.co>
2025-07-21 13:13:50 -1000

    [ES|QL] Fix aggregate_metric_double sorting and mv_expand issues
    This commit fixes a bug when sorting on multiple indices, where one
    index contains an aggregate_metric_double field, but at least one other
    index does not contain that field. The root cause being that the
    aggregate_metric_double field, when not present in a doc, will be
    encoded as a NullBlock (1 value), but later on decoded as an
    AggregateMetricDouble block (which expects to decode 4 values, even if
    they're all null).
    
    It also adds an implementation for MV_EXPAND so it just returns the
    existing block instead of erroring out, as multi-values are not
    supported for AggregateMetricDoubleBlock.

--- x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/data/AggregateMetricDoubleBlock.java
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/data/AggregateMetricDoubleBlock.java
@@ -208,8 +208,8 @@ public final class AggregateMetricDoubleBlock extends AbstractNonThreadSafeRefCo
 
     @Override
     public Block expand() {
-        // TODO: support
-        throw new UnsupportedOperationException("AggregateMetricDoubleBlock");
+        this.incRef();
+        return this;
     }
 
     @Override

f13e72345052841989ace60e941b7a986c082b51
Jack Conradson <osjdconrad@gmail.com>
2025-07-21 13:08:29 -0700

    spotless

--- server/src/main/java/org/elasticsearch/TransportVersion.java
+++ server/src/main/java/org/elasticsearch/TransportVersion.java
@@ -32,7 +32,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.ServiceLoader;
 import java.util.function.Function;
 import java.util.function.IntFunction;

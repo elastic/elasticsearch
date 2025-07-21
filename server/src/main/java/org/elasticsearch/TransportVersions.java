9d82c486d6298add6f391609d59882d9a5bf668e
Iván Cea Fontenla <ivancea96@outlook.com>
2025-07-21 16:46:41 +0200

    [9.1] ESQL: Added Sample operator NamedWritable to plugin (#131541) (#131621)
    Manual 9.1 backport of https://github.com/elastic/elasticsearch/pull/131541
    
    There's a TransportVersion change here

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -330,6 +330,7 @@ public class TransportVersions {
     public static final TransportVersion MAPPINGS_IN_DATA_STREAMS = def(9_112_0_00);
     public static final TransportVersion ESQL_SPLIT_ON_BIG_VALUES_9_1 = def(9_112_0_01);
     public static final TransportVersion ESQL_FIXED_INDEX_LIKE_9_1 = def(9_112_0_02);
+    public static final TransportVersion ESQL_SAMPLE_OPERATOR_STATUS_9_1 = def(9_112_0_03);
 
     /*
      * STOP! READ THIS FIRST! No, really,

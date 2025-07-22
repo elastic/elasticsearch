29aa355be36519a1992be9b2f3af4cd0e1b97ea0
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-07-22 18:39:00 +0000

    Bump versions after 8.18.4 release

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -195,6 +195,7 @@ public class TransportVersions {
     public static final TransportVersion INITIAL_ELASTICSEARCH_8_18_2 = def(8_840_0_04);
     public static final TransportVersion INITIAL_ELASTICSEARCH_8_18_3 = def(8_840_0_05);
     public static final TransportVersion INITIAL_ELASTICSEARCH_8_18_4 = def(8_840_0_06);
+    public static final TransportVersion INITIAL_ELASTICSEARCH_8_18_5 = def(8_840_0_07);
 
     /*
      * STOP! READ THIS FIRST! No, really,

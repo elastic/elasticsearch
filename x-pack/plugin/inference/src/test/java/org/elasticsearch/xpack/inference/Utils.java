62daced0991133f1d2ea1f4fa8e90da887ef1a98
Samiul Monir <samiul.monir@elastic.co>
2025-07-23 12:04:16 -0400

    removed duplicate setting

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/Utils.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/Utils.java
@@ -81,8 +81,6 @@ public final class Utils {
             ElasticInferenceServiceSettings.getSettingsDefinitions()
         ).flatMap(Collection::stream).collect(Collectors.toSet());
 
-        registeredSettings.add(InferencePlugin.INFERENCE_QUERY_TIMEOUT);
-
         var cSettings = new ClusterSettings(settings, registeredSettings);
         when(clusterService.getClusterSettings()).thenReturn(cSettings);
 

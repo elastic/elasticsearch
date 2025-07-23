1b06799e3ef249e22026f5c58e5186e4f20c5ce4
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-07-23 09:05:23 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/enrich/src/javaRestTest/java/org/elasticsearch/test/enrich/EnrichAdvancedSecurityIT.java
+++ x-pack/plugin/enrich/src/javaRestTest/java/org/elasticsearch/test/enrich/EnrichAdvancedSecurityIT.java
@@ -7,9 +7,7 @@
 package org.elasticsearch.test.enrich;
 
 import org.elasticsearch.client.Request;
-import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.test.cluster.ElasticsearchCluster;
 import org.elasticsearch.test.cluster.util.resource.Resource;
 import org.junit.ClassRule;

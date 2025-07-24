1c8b60fc738921d8f04f68c7a24748b27a1ed62f
Joshua Adams <joshua.adams@elastic.co>
2025-07-24 11:38:55 +0100

    Remove unused import

--- test/framework/src/test/java/org/elasticsearch/test/test/ESTestCaseTests.java
+++ test/framework/src/test/java/org/elasticsearch/test/test/ESTestCaseTests.java
@@ -33,7 +33,6 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;

ed7d4e0ab9922f7fbc5fb56246f190844f315f93
Jan Kazlouski <jan.kazlouski@elastic.co>
2025-07-29 23:20:20 +0300

    Update tests

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/llama/embeddings/LlamaEmbeddingsModelTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/llama/embeddings/LlamaEmbeddingsModelTests.java
@@ -14,7 +14,12 @@ import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.xpack.inference.services.openai.embeddings.OpenAiEmbeddingsTaskSettings;
 import org.elasticsearch.xpack.inference.services.settings.DefaultSecretSettings;
 
+import java.util.Map;
+
 import static org.elasticsearch.xpack.inference.chunking.ChunkingSettingsTests.createRandomChunkingSettings;
+import static org.elasticsearch.xpack.inference.services.openai.embeddings.OpenAiEmbeddingsRequestTaskSettingsTests.createRequestTaskSettingsMap;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
 
 public class LlamaEmbeddingsModelTests extends ESTestCase {
     public static LlamaEmbeddingsModel createEmbeddingsModel(
@@ -58,5 +63,30 @@ public class LlamaEmbeddingsModelTests extends ESTestCase {
             null,
             EmptySecretSettings.INSTANCE
         );
+    }
+
+    public void testOverrideWith_OverridesUser() {
+        var model = createEmbeddingsModel("model_name", "url", "api_key", "user", null, false);
+        var requestTaskSettingsMap = createRequestTaskSettingsMap("user_override");
+
+        var overriddenModel = LlamaEmbeddingsModel.of(model, requestTaskSettingsMap);
+
+        assertThat(overriddenModel, is(createEmbeddingsModel("model_name", "url", "api_key", "user_override", null, false)));
+    }
+
+    public void testOverrideWith_EmptyMap() {
+        var model = createEmbeddingsModel("model_name", "url", "api_key", "user", null, false);
+
+        var requestTaskSettingsMap = Map.<String, Object>of();
+
+        var overriddenModel = LlamaEmbeddingsModel.of(model, requestTaskSettingsMap);
+        assertThat(overriddenModel, sameInstance(model));
+    }
+
+    public void testOverrideWith_NullMap() {
+        var model = createEmbeddingsModel("model_name", "url", "api_key", "user", null, false);
+
+        var overriddenModel = LlamaEmbeddingsModel.of(model, null);
+        assertThat(overriddenModel, sameInstance(model));
     }
 }

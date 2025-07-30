249946f2d5ac1d7f1c84e512f622d7f3a4510b25
Yang Wang <yang.wang@elastic.co>
2025-07-31 09:14:21 +1000

    [Test] Actually invoke the listener on failure case
    The listener is expected to be called on failure case. This PR does
    that.
    
    Resolves: #131979

--- test/external-modules/multi-project/src/test/java/org/elasticsearch/multiproject/action/DeleteProjectActionTests.java
+++ test/external-modules/multi-project/src/test/java/org/elasticsearch/multiproject/action/DeleteProjectActionTests.java
@@ -59,12 +59,12 @@ public class DeleteProjectActionTests extends ESTestCase {
         var projects = randomList(1, 5, ESTestCase::randomUniqueProjectId);
         var deletedProjects = randomSubsetOf(projects);
         var state = buildState(projects);
-        var listener = ActionListener.assertAtLeastOnce(
-            ActionTestUtils.<AcknowledgedResponse>assertNoSuccessListener(e -> assertTrue(e instanceof IllegalArgumentException))
-        );
+        var listener = ActionListener.assertAtLeastOnce(ActionTestUtils.<AcknowledgedResponse>assertNoSuccessListener(e -> {
+            assertTrue(e instanceof IllegalArgumentException);
+        }));
         var nonExistingTask = createTask(randomUniqueProjectId(), listener);
         var tasks = Stream.concat(Stream.of(nonExistingTask), deletedProjects.stream().map(this::createTask)).toList();
-        var result = ClusterStateTaskExecutorUtils.executeIgnoringFailures(state, executor, tasks);
+        var result = ClusterStateTaskExecutorUtils.executeHandlingResults(state, executor, tasks, t -> {}, DeleteProjectTask::onFailure);
         for (ProjectId deletedProject : deletedProjects) {
             assertNull(result.metadata().projects().get(deletedProject));
             assertNull(result.globalRoutingTable().routingTables().get(deletedProject));

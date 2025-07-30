94876b2599e642e1d67e99db39089e82f62c2d93
Ben Chaplin <benchaplin@protonmail.com>
2025-07-30 09:46:14 -0400

    Add tests for searching index refresh blocks (#4276)
    Adding tests here to fill the gaps left by removing tests in the linked PR.
    
    These follow a change (https://github.com/elastic/elasticsearch/pull/129132) where indices are skipped in searches when an index-level block INDEX_REFRESH_BLOCK is in the cluster state. These tests prove that several search APIs (_search, _msearch, _search_shards, _pit) are affected by the change.

--- x-pack/plugin/stateless/src/internalClusterTest/java/co/elastic/elasticsearch/stateless/StatelessSearchSkipBlockIT.java
+++ x-pack/plugin/stateless/src/internalClusterTest/java/co/elastic/elasticsearch/stateless/StatelessSearchSkipBlockIT.java
@@ -25,25 +25,32 @@ import org.elasticsearch.action.search.ClosePointInTimeRequest;
 import org.elasticsearch.action.search.OpenPointInTimeRequest;
 import org.elasticsearch.action.search.SearchPhaseExecutionException;
 import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchShardsRequest;
 import org.elasticsearch.action.search.TransportClosePointInTimeAction;
 import org.elasticsearch.action.search.TransportOpenPointInTimeAction;
+import org.elasticsearch.action.search.TransportSearchShardsAction;
+import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.MetadataCreateIndexService;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.index.query.MatchAllQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.builder.PointInTimeBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import java.util.Locale;
+import java.util.Objects;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertFailures;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertHitCount;
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertResponse;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 
 public class StatelessSearchSkipBlockIT extends AbstractStatelessIntegTestCase {
@@ -52,9 +59,10 @@ public class StatelessSearchSkipBlockIT extends AbstractStatelessIntegTestCase {
     private final int numReplicas = randomIntBetween(1, 2);
 
     public void testSearchWhenIndexSearchShardsAreNotUp() throws Exception {
-        // If a new index does not have search shards ready when a search request comes in,
-        // depending on USE_INDEX_REFRESH_BLOCK_SETTING_NAME, we should either fail with
-        // a 503 here, or skip the shards and succeed.
+        // If a new index does not have search shards ready when a search request comes in:
+        // - if USE_INDEX_REFRESH_BLOCK_SETTING_NAME is true, the response should have 0 hits as the index was skipped
+        // - if USE_INDEX_REFRESH_BLOCK_SETTING_NAME is false, the response should be a 503
+        // Once at least one search shard is ready, a subsequent request should succeed with the correct number of hits
         boolean useBlock = randomBoolean();
         startMasterOnlyNode(Settings.builder().put(MetadataCreateIndexService.USE_INDEX_REFRESH_BLOCK_SETTING_NAME, useBlock).build());
         startIndexNodes(numShards);
@@ -76,15 +84,105 @@ public class StatelessSearchSkipBlockIT extends AbstractStatelessIntegTestCase {
             assertFailures(searchRequest, RestStatus.SERVICE_UNAVAILABLE, containsString("NoShardAvailableActionException"));
         }
 
-        startSearchNodes(numShards * numReplicas);
-        ensureGreen(indexName);
+        startSearchNodes(randomIntBetween(1, numShards * numReplicas));
         assertBusy(() -> assertHitCount(searchRequest, numDocs));
     }
 
+    public void testMultiSearchWhenIndexSearchShardsAreNotUp() throws Exception {
+        // If a new index does not have search shards ready when a multisearch request comes in:
+        // - if USE_INDEX_REFRESH_BLOCK_SETTING_NAME is true, the response should have 0 hits as the index was skipped
+        // - if USE_INDEX_REFRESH_BLOCK_SETTING_NAME is false, the responses should be null
+        // Once at least one search shard is ready, a subsequent request should succeed with the correct number of hits
+        boolean useBlock = randomBoolean();
+        startMasterOnlyNode(Settings.builder().put(MetadataCreateIndexService.USE_INDEX_REFRESH_BLOCK_SETTING_NAME, useBlock).build());
+        startIndexNodes(numShards);
+
+        final String indexName = randomAlphaOfLength(10).toLowerCase(Locale.ROOT);
+        var indexSettings = Settings.builder()
+            .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, numShards)
+            .put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, numReplicas);
+        // Wait for 0 shards, else this will hang until search shards are added
+        assertAcked(prepareCreate(indexName, indexSettings).setWaitForActiveShards(0));
+
+        int numDocs = randomIntBetween(1, 10);
+        indexDocuments(indexName, numDocs);
+
+        var multiSearchRequest = client().prepareMultiSearch()
+            .add(prepareSearch(indexName).setQuery(QueryBuilders.matchAllQuery()))
+            .add(prepareSearch().setQuery(QueryBuilders.termQuery("field", "blah")));
+        assertResponse(multiSearchRequest, response -> {
+            if (useBlock) {
+                assertHitCount(Objects.requireNonNull(response.getResponses()[0].getResponse()), 0);
+                assertHitCount(Objects.requireNonNull(response.getResponses()[1].getResponse()), 0);
+            } else {
+                assertNull(response.getResponses()[0].getResponse());
+                assertNull(response.getResponses()[1].getResponse());
+            }
+        });
+
+        startSearchNodes(randomIntBetween(1, numShards * numReplicas));
+        assertBusy(() -> assertResponse(multiSearchRequest, response -> {
+            try {
+                assertHitCount(Objects.requireNonNull(response.getResponses()[0].getResponse()), numDocs);
+                assertHitCount(Objects.requireNonNull(response.getResponses()[1].getResponse()), 0);
+            } catch (NullPointerException npe) {
+                throw new AssertionError(npe);
+            }
+        }));
+    }
+
+    public void testSearchShardsWhenIndexSearchShardsAreNotUp() throws Exception {
+        // If a new index does not have search shards ready when a search shards request comes in:
+        // - if USE_INDEX_REFRESH_BLOCK_SETTING_NAME is true, there should be no shards in the response
+        // - if USE_INDEX_REFRESH_BLOCK_SETTING_NAME is false, there should be the correct number of shards in the response
+        // Once at least one search shard is ready, a subsequent request should succeed with the correct number of shards in the response
+        boolean useBlock = randomBoolean();
+        startMasterOnlyNode(Settings.builder().put(MetadataCreateIndexService.USE_INDEX_REFRESH_BLOCK_SETTING_NAME, useBlock).build());
+        startIndexNodes(numShards);
+
+        final String indexName = randomAlphaOfLength(10).toLowerCase(Locale.ROOT);
+        var indexSettings = Settings.builder()
+            .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, numShards)
+            .put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, numReplicas);
+        // Wait for 0 shards, else this will hang until search shards are added
+        assertAcked(prepareCreate(indexName, indexSettings).setWaitForActiveShards(0));
+
+        int numDocs = randomIntBetween(1, 10);
+        indexDocuments(indexName, numDocs);
+
+        var response = client().execute(
+            TransportSearchShardsAction.TYPE,
+            new SearchShardsRequest(new String[] { indexName }, IndicesOptions.DEFAULT, new MatchAllQueryBuilder(), null, null, true, null)
+        ).actionGet();
+        if (useBlock) {
+            assertThat(response.getGroups().size(), equalTo(0));
+        } else {
+            assertThat(response.getGroups().size(), equalTo(numShards));
+        }
+
+        startSearchNodes(randomIntBetween(1, numShards * numReplicas));
+        assertBusy(() -> {
+            var subseqResponse = client().execute(
+                TransportSearchShardsAction.TYPE,
+                new SearchShardsRequest(
+                    new String[] { indexName },
+                    IndicesOptions.DEFAULT,
+                    new MatchAllQueryBuilder(),
+                    null,
+                    null,
+                    true,
+                    null
+                )
+            ).actionGet();
+            assertThat(subseqResponse.getGroups().size(), equalTo(numShards));
+        });
+    }
+
     public void testOpenPITWhenIndexSearchShardsAreNotUp() {
-        // If a new index does not have search shards ready when an open PIT request comes in,
-        // depending on USE_INDEX_REFRESH_BLOCK_SETTING_NAME, we should either fail with
-        // a 503 here, or skip the shards and succeed.
+        // If a new index does not have search shards ready when an open PIT request comes in:
+        // - if USE_INDEX_REFRESH_BLOCK_SETTING_NAME is true, the response should have 0 hits as the index was skipped
+        // Once at least one search shard is ready, a subsequent get PIT request should still have 0 hits, even if docs were indexed
+        // - if USE_INDEX_REFRESH_BLOCK_SETTING_NAME is false, the response should be a 503
         boolean useBlock = randomBoolean();
         startMasterOnlyNode(Settings.builder().put(MetadataCreateIndexService.USE_INDEX_REFRESH_BLOCK_SETTING_NAME, useBlock).build());
         startIndexNodes(numShards);
@@ -108,7 +206,7 @@ public class StatelessSearchSkipBlockIT extends AbstractStatelessIntegTestCase {
                 );
                 assertHitCount(client().search(searchRequest), 0);
 
-                startSearchNodes(numShards * numReplicas);
+                startSearchNodes(randomIntBetween(1, numShards * numReplicas));
                 int numDocs = randomIntBetween(1, 10);
                 indexDocuments(indexName, numDocs).actionGet();
 

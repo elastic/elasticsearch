77a350d5a2ec5a47d0195aa77517c6f28527b4b3
Jan Kuipers <jan.kuipers@elastic.co>
2025-07-31 16:28:41 +0200

    CSV tests

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/CsvTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/CsvTests.java
@@ -695,7 +695,7 @@ public class CsvTests extends ESTestCase {
         PlanTimeProfile planTimeProfile = configuration.profile() ? new PlanTimeProfile() : null;
         session.preOptimizedPlan(analyzed, logicalPlanPreOptimizer, planTimeProfile, listener.delegateFailureAndWrap((l, preOptimized) -> {
             session.executeOptimizedPlan(
-                new EsqlQueryRequest(),
+                new EsqlQueryRequest().approximate(testCase.approximate),
                 esqlExecutionInfo,
                 planRunner(bigArrays, physicalOperationProviders),
                 session.optimizedPlan(preOptimized, logicalPlanOptimizer, planTimeProfile),

3ab190511d3617cd0f08dcb41fa5d46390f03532
Jordan Powers <jordan.powers@elastic.co>
2025-08-04 18:10:38 -0700

    Fix BlockSourceReaderTests

--- server/src/test/java/org/elasticsearch/index/mapper/BlockSourceReaderTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/BlockSourceReaderTests.java
@@ -54,10 +54,12 @@ public class BlockSourceReaderTests extends MapperServiceTestCase {
         BlockLoader loader = new BlockSourceReader.BytesRefsBlockLoader(valueFetcher, lookup);
         assertThat(loader.columnAtATimeReader(ctx), nullValue());
         BlockLoader.RowStrideReader reader = loader.rowStrideReader(ctx);
-        assertThat(loader.rowStrideFieldSpec(), equalTo(StoredFieldsSpec.NEEDS_SOURCE));
+        assertThat(loader.rowStrideFieldSpec(), equalTo(new BlockLoader.FieldsSpec(StoredFieldsSpec.NEEDS_SOURCE, IgnoredFieldsSpec.NONE)));
         BlockLoaderStoredFieldsFromLeafLoader storedFields = new BlockLoaderStoredFieldsFromLeafLoader(
             StoredFieldLoader.fromSpec(loader.rowStrideFieldSpec(), false).getLoader(ctx, null),
-            loader.rowStrideFieldSpec().requiresSource() ? SourceLoader.FROM_STORED_SOURCE.leaf(ctx.reader(), null) : null
+            loader.rowStrideFieldSpec().storedFieldsSpec().requiresSource()
+                ? SourceLoader.FROM_STORED_SOURCE.leaf(ctx.reader(), null)
+                : null
         );
         BlockLoader.Builder builder = loader.builder(TestBlock.factory(), 1);
         storedFields.advanceTo(0);

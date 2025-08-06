da496f3f293e873d4d3862273e28ab3347fc574d
Nick Tindall <nick.tindall@elastic.co>
2025-08-06 16:29:59 +1000

    Fix BWC issue with thread pool types

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -355,6 +355,7 @@ public class TransportVersions {
     public static final TransportVersion TO_CHILD_BLOCK_JOIN_QUERY = def(9_133_0_00);
     public static final TransportVersion ML_INFERENCE_AI21_COMPLETION_ADDED = def(9_134_0_00);
     public static final TransportVersion TRANSPORT_NODE_USAGE_STATS_FOR_THREAD_POOLS_ACTION = def(9_135_0_00);
+    public static final TransportVersion VIRTUAL_THREADS = def(9_136_0_00);
 
     /*
      * STOP! READ THIS FIRST! No, really,

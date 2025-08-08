9868170bc028fc0d8b9aef3bb6ad08a07743db2b
Nick Tindall <nick.tindall@elastic.co>
2025-08-08 10:16:17 +1000

    Reduce dead code

--- server/src/main/java/org/elasticsearch/threadpool/DefaultBuiltInExecutorBuilders.java
+++ server/src/main/java/org/elasticsearch/threadpool/DefaultBuiltInExecutorBuilders.java
@@ -30,14 +30,9 @@ public class DefaultBuiltInExecutorBuilders implements BuiltInExecutorBuilders {
         final int halfProc = ThreadPool.halfAllocatedProcessors(allocatedProcessors);
         final int halfProcMaxAt5 = ThreadPool.halfAllocatedProcessorsMaxFive(allocatedProcessors);
         final int halfProcMaxAt10 = ThreadPool.halfAllocatedProcessorsMaxTen(allocatedProcessors);
-        final int genericThreadPoolMax = ThreadPool.boundedBy(4 * allocatedProcessors, 128, 512);
         final double indexAutoscalingEWMA = WRITE_THREAD_POOLS_EWMA_ALPHA_SETTING.get(settings);
 
         Map<String, ExecutorBuilder> result = new HashMap<>();
-        // result.put(
-        // ThreadPool.Names.GENERIC,
-        // new ScalingExecutorBuilder(ThreadPool.Names.GENERIC, 4, genericThreadPoolMax, TimeValue.timeValueSeconds(30), false)
-        // );
         result.put(ThreadPool.Names.GENERIC, new VirtualThreadsExecutorBuilder(ThreadPool.Names.GENERIC, false));
         result.put(ThreadPool.Names.WRITE_COORDINATION, new VirtualThreadsExecutorBuilder(ThreadPool.Names.WRITE_COORDINATION, true));
         result.put(ThreadPool.Names.WRITE, new VirtualThreadsExecutorBuilder(ThreadPool.Names.WRITE, true));

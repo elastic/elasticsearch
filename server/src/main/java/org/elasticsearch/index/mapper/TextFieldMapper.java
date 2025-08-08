c4a52cd3c59bf70061fab1e49820ce1ca9f9271a
Dmitry Kubikov <dmitry.kubikov@elastic.co>
2025-08-08 16:56:07 -0700

    Fixed incorrect synthetic source field name for text fields

--- server/src/main/java/org/elasticsearch/index/mapper/TextFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/TextFieldMapper.java
@@ -1588,6 +1588,10 @@ public final class TextFieldMapper extends TextFamilyFieldMapper {
             });
         }
 
+        if (fieldType.indexOptions() != IndexOptions.NONE) {
+            return super.syntheticSourceSupport();
+        }
+
         return new SyntheticSourceSupport.Native(() -> syntheticFieldLoader(fullPath(), leafName()));
     }
 

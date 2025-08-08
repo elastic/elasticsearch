819e31c13cc6dcffc602a631e8bd2a35a06a2644
Jonas Kunz <jonas.kunz@elastic.co>
2025-08-08 08:55:38 +0200

    Implement memory accounting and circuit breaker

--- libs/exponential-histogram/src/test/java/org/elasticsearch/exponentialhistogram/ExponentialScaleUtilsTests.java
+++ libs/exponential-histogram/src/test/java/org/elasticsearch/exponentialhistogram/ExponentialScaleUtilsTests.java
@@ -23,8 +23,6 @@ package org.elasticsearch.exponentialhistogram;
 
 import ch.obermuhlner.math.big.BigDecimalMath;
 
-import org.elasticsearch.test.ESTestCase;
-
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
@@ -49,7 +47,7 @@ import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
-public class ExponentialScaleUtilsTests extends ESTestCase {
+public class ExponentialScaleUtilsTests extends ExponentialHistogramTestCase {
 
     public void testMaxIndex() {
         assertThat(getMaximumScaleIncrease(MAX_INDEX), equalTo(0));

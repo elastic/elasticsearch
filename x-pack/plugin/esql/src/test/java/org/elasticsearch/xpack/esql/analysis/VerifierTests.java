0a80914383840d6dd0146f665911c9736e5ef16d
Svilen Mihaylov <svilen.mihaylov@elastic.co>
2025-08-12 14:44:49 -0400

    Add verifier test

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/analysis/VerifierTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/analysis/VerifierTests.java
@@ -2484,24 +2484,27 @@ public class VerifierTests extends ESTestCase {
 
     public void testVectorSimilarityFunctionsNullArgs() throws Exception {
         if (EsqlCapabilities.Cap.COSINE_VECTOR_SIMILARITY_FUNCTION.isEnabled()) {
-            checkVectorSimilarityFunctionsNullArgs("v_cosine(null, vector)", "first");
-            checkVectorSimilarityFunctionsNullArgs("v_cosine(vector, null)", "second");
+            checkVectorFunctionsNullArgs("v_cosine(null, vector)", "first");
+            checkVectorFunctionsNullArgs("v_cosine(vector, null)", "second");
         }
         if (EsqlCapabilities.Cap.DOT_PRODUCT_VECTOR_SIMILARITY_FUNCTION.isEnabled()) {
-            checkVectorSimilarityFunctionsNullArgs("v_dot_product(null, vector)", "first");
-            checkVectorSimilarityFunctionsNullArgs("v_dot_product(vector, null)", "second");
+            checkVectorFunctionsNullArgs("v_dot_product(null, vector)", "first");
+            checkVectorFunctionsNullArgs("v_dot_product(vector, null)", "second");
         }
         if (EsqlCapabilities.Cap.L1_NORM_VECTOR_SIMILARITY_FUNCTION.isEnabled()) {
-            checkVectorSimilarityFunctionsNullArgs("v_l1_norm(null, vector)", "first");
-            checkVectorSimilarityFunctionsNullArgs("v_l1_norm(vector, null)", "second");
+            checkVectorFunctionsNullArgs("v_l1_norm(null, vector)", "first");
+            checkVectorFunctionsNullArgs("v_l1_norm(vector, null)", "second");
         }
         if (EsqlCapabilities.Cap.L2_NORM_VECTOR_SIMILARITY_FUNCTION.isEnabled()) {
-            checkVectorSimilarityFunctionsNullArgs("v_l2_norm(null, vector)", "first");
-            checkVectorSimilarityFunctionsNullArgs("v_l2_norm(vector, null)", "second");
+            checkVectorFunctionsNullArgs("v_l2_norm(null, vector)", "first");
+            checkVectorFunctionsNullArgs("v_l2_norm(vector, null)", "second");
+        }
+        if (EsqlCapabilities.Cap.MAGNITUDE_SCALAR_VECTOR_FUNCTION.isEnabled()) {
+            checkVectorFunctionsNullArgs("v_magnitude(null)", "first");
         }
     }
 
-    private void checkVectorSimilarityFunctionsNullArgs(String functionInvocation, String argOrdinal) throws Exception {
+    private void checkVectorFunctionsNullArgs(String functionInvocation, String argOrdinal) throws Exception {
         assertThat(
             error("from test | eval similarity = " + functionInvocation, fullTextAnalyzer),
             containsString(argOrdinal + " argument of [" + functionInvocation + "] cannot be null, received [null]")

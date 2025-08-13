55cac38f38f9af51a6c3017e055ecd6146bf34e3
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2025-08-13 07:05:47 +0300

    Supporting two new formats

--- x-pack/plugin/text-structure/src/test/java/org/elasticsearch/xpack/textstructure/structurefinder/TimestampFormatFinderTests.java
+++ x-pack/plugin/text-structure/src/test/java/org/elasticsearch/xpack/textstructure/structurefinder/TimestampFormatFinderTests.java
@@ -1335,6 +1335,38 @@ public class TimestampFormatFinderTests extends TextStructureTestCase {
 
     }
 
+    public void testFindFormatGivenYyyyMmDdWithSlashes() {
+
+        Consumer<Boolean> testFindFormatGivenYyyyMmDdWithSlashesAndEcsCompatibility = (ecsCompatibility) -> {
+            validateTimestampMatch(
+                "2018/05/15 17:14:56",
+                "TIMESTAMP_YMD",
+                "\\b\\d{4}[./-]\\d{2}[./-]\\d{2} \\d{2}:\\d{2}:\\d{2}\\b",
+                List.of("yyyy/MM/dd HH:mm:ss", "yyyy-MM-dd HH:mm:ss", "yyyy.MM.dd HH:mm:ss"),
+                1526400896000L,
+                ecsCompatibility
+            );
+        };
+
+        ecsCompatibilityModes.forEach(testFindFormatGivenYyyyMmDdWithSlashesAndEcsCompatibility);
+    }
+
+    public void testFindFormatGivenMmmDCommaYyyy() {
+
+        Consumer<Boolean> testFindFormatGivenMmmDCommaYyyyAndEcsCompatibility = (ecsCompatibility) -> {
+            validateTimestampMatch(
+                "May 15, 2018",
+                "CUSTOM_TIMESTAMP",
+                "\\b[A-Z][a-z]{2} \\d{1,2}, \\d{4}\\b",
+                "MMM d, yyyy",
+                1526338800000L,
+                ecsCompatibility
+            );
+        };
+
+        ecsCompatibilityModes.forEach(testFindFormatGivenMmmDCommaYyyyAndEcsCompatibility);
+    }
+
     public void testCustomOverrideMatchingBuiltInFormat() {
 
         String overrideFormat = "yyyy-MM-dd HH:mm:ss,SSS";

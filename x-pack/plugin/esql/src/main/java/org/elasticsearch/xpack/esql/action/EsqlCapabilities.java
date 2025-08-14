8a278ed856be7b12a5ac62315937db97ad3f30c3
Carlos Delgado <6339205+carlosdelest@users.noreply.github.com>
2025-08-14 18:02:01 +0200

    ESQL - Allow null values in vector similarity functions (#132919)
    * Allow null values and return null as result
    
    * Fix indentation
    
    * [CI] Auto commit changes from spotless
    
    * Remove unnecessary return
    
    * Fix verifier tests
    
    * Fix bwc tests introducing capability
    
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1359,7 +1359,12 @@ public class EsqlCapabilities {
         /**
          * Byte elements dense vector field type support.
          */
-        DENSE_VECTOR_FIELD_TYPE_BYTE_ELEMENTS(EsqlCorePlugin.DENSE_VECTOR_FEATURE_FLAG);
+        DENSE_VECTOR_FIELD_TYPE_BYTE_ELEMENTS(EsqlCorePlugin.DENSE_VECTOR_FEATURE_FLAG),
+
+        /**
+         * Support null elements on vector similarity functions
+         */
+        VECTOR_SIMILARITY_FUNCTIONS_SUPPORT_NULL;
 
         private final boolean enabled;
 

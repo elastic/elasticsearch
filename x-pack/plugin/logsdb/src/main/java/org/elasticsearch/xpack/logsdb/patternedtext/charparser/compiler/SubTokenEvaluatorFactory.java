9bc623305a0a87c3f9585ded248ba4b901702e5b
eyalkoren <41850454+eyalkoren@users.noreply.github.com>
2025-08-14 16:08:48 +0300

    Using modern switch instanceof in SubTokenEvaluatorFactory

--- x-pack/plugin/logsdb/src/main/java/org/elasticsearch/xpack/logsdb/patternedtext/charparser/compiler/SubTokenEvaluatorFactory.java
+++ x-pack/plugin/logsdb/src/main/java/org/elasticsearch/xpack/logsdb/patternedtext/charparser/compiler/SubTokenEvaluatorFactory.java
@@ -23,37 +23,27 @@ import org.elasticsearch.xpack.logsdb.patternedtext.charparser.schema.constraint
 
 public class SubTokenEvaluatorFactory {
     public static SubTokenEvaluator<SubstringView> from(int bitmask, StringConstraint stringConstraint) {
-        if (stringConstraint instanceof EqualsStringConstraint) {
-            EqualsStringConstraint esc = (EqualsStringConstraint) stringConstraint;
-            return new SubTokenEvaluator<>(
+        return switch (stringConstraint) {
+            case EqualsStringConstraint esc -> new SubTokenEvaluator<>(
                 bitmask,
                 (input) -> input != null && input.equals(new SubstringView(esc.targetValue())) ? 1 : -1
             );
-        } else if (stringConstraint instanceof NotEqualsStringConstraint) {
-            NotEqualsStringConstraint nesc = (NotEqualsStringConstraint) stringConstraint;
-            return new SubTokenEvaluator<>(
+            case NotEqualsStringConstraint nesc -> new SubTokenEvaluator<>(
                 bitmask,
                 (input) -> input != null && input.equals(new SubstringView(nesc.targetValue())) == false ? 1 : -1
             );
-        } else if (stringConstraint instanceof LengthStringConstraint) {
-            LengthStringConstraint lsc = (LengthStringConstraint) stringConstraint;
-            return new SubTokenEvaluator<>(bitmask, (input) -> input != null && input.length() == lsc.requiredLength() ? 1 : -1);
-        } else if (stringConstraint instanceof StringSetConstraint) {
-            StringSetConstraint ssc = (StringSetConstraint) stringConstraint;
-            return new SubTokenEvaluator<>(bitmask, new StringToIntSet(ssc.keys()));
-        } else if (stringConstraint instanceof StringToIntMapConstraint) {
-            StringToIntMapConstraint stmc = (StringToIntMapConstraint) stringConstraint;
-            return new SubTokenEvaluator<>(bitmask, new StringToIntMap(stmc.map()));
-        } else if (stringConstraint instanceof AndStringConstraint) {
-            AndStringConstraint asc = (AndStringConstraint) stringConstraint;
-            return from(bitmask, asc.first()).and(from(bitmask, asc.second()));
-        } else if (stringConstraint instanceof OrStringConstraint) {
-            OrStringConstraint osc = (OrStringConstraint) stringConstraint;
-            return from(bitmask, osc.first()).or(from(bitmask, osc.second()));
-        } else if (stringConstraint instanceof AnyString) {
-            return new SubTokenEvaluator<>(bitmask, (input) -> input != null ? 1 : -1);
-        } else {
-            throw new IllegalArgumentException("Unsupported StringConstraint type: " + stringConstraint.getClass().getSimpleName());
-        }
+            case LengthStringConstraint lsc -> new SubTokenEvaluator<>(
+                bitmask,
+                (input) -> input != null && input.length() == lsc.requiredLength() ? 1 : -1
+            );
+            case StringSetConstraint ssc -> new SubTokenEvaluator<>(bitmask, new StringToIntSet(ssc.keys()));
+            case StringToIntMapConstraint stmc -> new SubTokenEvaluator<>(bitmask, new StringToIntMap(stmc.map()));
+            case AndStringConstraint asc -> from(bitmask, asc.first()).and(from(bitmask, asc.second()));
+            case OrStringConstraint osc -> from(bitmask, osc.first()).or(from(bitmask, osc.second()));
+            case AnyString anyString -> new SubTokenEvaluator<>(bitmask, (input) -> input != null ? 1 : -1);
+            default -> throw new IllegalArgumentException(
+                "Unsupported StringConstraint type: " + stringConstraint.getClass().getSimpleName()
+            );
+        };
     }
 }

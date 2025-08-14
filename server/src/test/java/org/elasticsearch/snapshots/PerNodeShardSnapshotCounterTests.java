dfb035f4ad40c7b0fea941b36ccfc905827820bb
Yang Wang <yang.wang@elastic.co>
2025-08-14 16:54:50 +1000

    augument tests for PerNodeShardSnapshotCounter

--- server/src/test/java/org/elasticsearch/snapshots/PerNodeShardSnapshotCounterTests.java
+++ server/src/test/java/org/elasticsearch/snapshots/PerNodeShardSnapshotCounterTests.java
@@ -15,9 +15,21 @@ import org.elasticsearch.cluster.metadata.ProjectId;
 import org.elasticsearch.cluster.node.DiscoveryNodeRole;
 import org.elasticsearch.cluster.node.DiscoveryNodeUtils;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.index.Index;
+import org.elasticsearch.index.IndexVersion;
+import org.elasticsearch.index.shard.ShardId;
+import org.elasticsearch.repositories.IndexId;
+import org.elasticsearch.repositories.RepositoryShardId;
+import org.elasticsearch.repositories.ShardGeneration;
+import org.elasticsearch.repositories.ShardSnapshotResult;
 import org.elasticsearch.test.ESTestCase;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.elasticsearch.snapshots.SnapshotsInProgressSerializationTests.randomSnapshot;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
@@ -37,45 +49,185 @@ public class PerNodeShardSnapshotCounterTests extends ESTestCase {
         assertTrue(perNodeShardSnapshotCounter.completeShardSnapshotOnNode(randomIdentifier()));
     }
 
-    public void testNodesCapacities() {
+    public void testNodesCapacitiesWithKnownStates() {
+        final var snapshotNodeId = randomAlphaOfLength(16);
+        final boolean isStateless = randomBoolean();
+        final DiscoveryNodes discoveryNodes = randomDiscoveryNodes(List.of(snapshotNodeId), List.of(randomAlphaOfLength(18)), isStateless);
+        final int perNodeLimit = 1; // Capacity for only a single shard snapshot
+
+        SnapshotsInProgress snapshotsInProgress = SnapshotsInProgress.EMPTY;
+        {
+            final var perNodeShardSnapshotCounter = new PerNodeShardSnapshotCounter(
+                perNodeLimit,
+                snapshotsInProgress,
+                discoveryNodes,
+                isStateless
+            );
+            // Available capacity for only a single shard snapshot
+            assertTrue(perNodeShardSnapshotCounter.hasCapacityOnAnyNode());
+            assertTrue(perNodeShardSnapshotCounter.tryStartShardSnapshotOnNode(snapshotNodeId));
+            assertFalse(perNodeShardSnapshotCounter.tryStartShardSnapshotOnNode(snapshotNodeId));
+        }
+
+        // Clone does not count towards the limit
+        {
+            final IndexId indexId = new IndexId(randomIdentifier(), randomUUID());
+            final SnapshotsInProgress.Entry cloneEntry = SnapshotsInProgress.startClone(
+                new Snapshot(ProjectId.DEFAULT, randomIdentifier(), new SnapshotId(randomIdentifier(), randomUUID())),
+                new SnapshotId(randomIdentifier(), randomUUID()),
+                Map.of(indexId.getName(), indexId),
+                randomNonNegativeLong(),
+                randomNonNegativeLong(),
+                IndexVersion.current()
+            );
+            snapshotsInProgress.withAddedEntry(
+                cloneEntry.withClones(
+                    Map.of(new RepositoryShardId(indexId, 0), new ShardSnapshotStatus(snapshotNodeId, new ShardGeneration(1L)))
+                )
+            );
+            final var perNodeShardSnapshotCounter = new PerNodeShardSnapshotCounter(
+                perNodeLimit,
+                snapshotsInProgress,
+                discoveryNodes,
+                isStateless
+            );
+            assertTrue(perNodeShardSnapshotCounter.hasCapacityOnAnyNode());
+        }
+
+        // Enumerate states that do not count towards the limit
+        {
+            final Map<ShardId, ShardSnapshotStatus> shards = new HashMap<>();
+            // Add shards that do not count towards limit
+            shards.put(randomShardId(), ShardSnapshotStatus.UNASSIGNED_QUEUED);
+            shards.put(randomShardId(), ShardSnapshotStatus.MISSING);
+            shards.put(
+                randomShardId(),
+                ShardSnapshotStatus.success(snapshotNodeId, new ShardSnapshotResult(new ShardGeneration(1L), ByteSizeValue.ofBytes(1L), 1))
+            );
+            shards.put(
+                randomShardId(),
+                new ShardSnapshotStatus(snapshotNodeId, SnapshotsInProgress.ShardState.WAITING, new ShardGeneration(1L))
+            );
+            shards.put(
+                randomShardId(),
+                new ShardSnapshotStatus(snapshotNodeId, SnapshotsInProgress.ShardState.PAUSED_FOR_NODE_REMOVAL, new ShardGeneration(1L))
+            );
+            shards.put(
+                randomShardId(),
+                new ShardSnapshotStatus(
+                    snapshotNodeId,
+                    SnapshotsInProgress.ShardState.FAILED,
+                    new ShardGeneration(1L),
+                    randomAlphaOfLengthBetween(10, 20)
+                )
+            );
+            // aborted assigned-queued shard
+            shards.put(
+                randomShardId(),
+                new ShardSnapshotStatus(
+                    snapshotNodeId,
+                    SnapshotsInProgress.ShardState.ABORTED,
+                    new ShardGeneration(1L),
+                    "assigned-queued aborted"
+                )
+            );
+            // assigned-queued shard
+            shards.put(randomShardId(), ShardSnapshotStatus.assignedQueued(snapshotNodeId, randomFrom(new ShardGeneration(1L), null)));
+
+            final Map<String, IndexId> indexIds = shards.keySet()
+                .stream()
+                .collect(Collectors.toUnmodifiableMap(ShardId::getIndexName, shardId -> new IndexId(shardId.getIndexName(), randomUUID())));
+
+            snapshotsInProgress = snapshotsInProgress.withAddedEntry(
+                SnapshotsInProgress.Entry.snapshot(
+                    new Snapshot(ProjectId.DEFAULT, randomIdentifier(), new SnapshotId(randomIdentifier(), randomUUID())),
+                    randomBoolean(),
+                    randomBoolean(),
+                    SnapshotsInProgress.State.STARTED,
+                    indexIds,
+                    List.of(),
+                    List.of(),
+                    randomNonNegativeLong(),
+                    randomNonNegativeLong(),
+                    shards,
+                    null,
+                    Map.of(),
+                    IndexVersion.current()
+                )
+            );
+
+            final var perNodeShardSnapshotCounter = new PerNodeShardSnapshotCounter(
+                perNodeLimit,
+                snapshotsInProgress,
+                discoveryNodes,
+                isStateless
+            );
+            assertTrue(perNodeShardSnapshotCounter.hasCapacityOnAnyNode());
+        }
+
+        // Shard snapshots with states counting towards
+        {
+            // INIT shard
+            final Map<ShardId, ShardSnapshotStatus> shards = new HashMap<>();
+            final ShardId shardId = randomShardId();
+            shards.put(shardId, new ShardSnapshotStatus(snapshotNodeId, new ShardGeneration(1L)));
+            var entry = SnapshotsInProgress.Entry.snapshot(
+                new Snapshot(ProjectId.DEFAULT, randomIdentifier(), new SnapshotId(randomIdentifier(), randomUUID())),
+                randomBoolean(),
+                randomBoolean(),
+                SnapshotsInProgress.State.STARTED,
+                Map.of(shardId.getIndexName(), new IndexId(shardId.getIndexName(), randomUUID())),
+                List.of(),
+                List.of(),
+                randomNonNegativeLong(),
+                randomNonNegativeLong(),
+                shards,
+                null,
+                Map.of(),
+                IndexVersion.current()
+            );
+            snapshotsInProgress = snapshotsInProgress.withAddedEntry(entry);
+            var perNodeShardSnapshotCounter = new PerNodeShardSnapshotCounter(
+                perNodeLimit,
+                snapshotsInProgress,
+                discoveryNodes,
+                isStateless
+            );
+            assertFalse(perNodeShardSnapshotCounter.hasCapacityOnAnyNode()); // no capacity left due to the INIT shard
+
+            // Aborted shard snapshot that was previously in INIT state still count towards the limit
+            entry = entry.abort(snapshotNodeId, (ignore0, ignore1) -> {});
+            assertNotNull(entry);
+            snapshotsInProgress = snapshotsInProgress.createCopyWithUpdatedEntriesForRepo(
+                ProjectId.DEFAULT,
+                entry.repository(),
+                List.of(entry)
+            );
+            perNodeShardSnapshotCounter = new PerNodeShardSnapshotCounter(perNodeLimit, snapshotsInProgress, discoveryNodes, isStateless);
+            assertFalse(perNodeShardSnapshotCounter.hasCapacityOnAnyNode());
+
+            // Complete the entry by fail the shard snapshot and the node capacity is released
+            entry = entry.withShardStates(
+                Map.of(
+                    shardId,
+                    new ShardSnapshotStatus(snapshotNodeId, SnapshotsInProgress.ShardState.FAILED, new ShardGeneration(1L), "failed")
+                )
+            );
+            snapshotsInProgress = snapshotsInProgress.createCopyWithUpdatedEntriesForRepo(
+                ProjectId.DEFAULT,
+                entry.repository(),
+                List.of(entry)
+            );
+            perNodeShardSnapshotCounter = new PerNodeShardSnapshotCounter(perNodeLimit, snapshotsInProgress, discoveryNodes, isStateless);
+            assertTrue(perNodeShardSnapshotCounter.hasCapacityOnAnyNode());
+        }
+    }
+
+    public void testNodesCapacitiesWithRandomSnapshots() {
         final var snapshotNodeIds = randomList(1, 5, () -> randomAlphaOfLength(16));
         final var nonSnapshotNodeIds = randomList(0, 5, () -> randomAlphaOfLength(18));
-
         final boolean isStateless = randomBoolean();
-        final Set<DiscoveryNodeRole> snapshotNodeRole;
-        final Set<DiscoveryNodeRole> nonSnapshotNodeRole;
-        if (isStateless) {
-            snapshotNodeRole = Set.of(DiscoveryNodeRole.INDEX_ROLE);
-            nonSnapshotNodeRole = Set.of(randomFrom(DiscoveryNodeRole.SEARCH_ROLE, DiscoveryNodeRole.ML_ROLE));
-        } else {
-            snapshotNodeRole = Set.copyOf(
-                randomNonEmptySubsetOf(
-                    Set.of(
-                        DiscoveryNodeRole.DATA_ROLE,
-                        DiscoveryNodeRole.DATA_HOT_NODE_ROLE,
-                        DiscoveryNodeRole.DATA_WARM_NODE_ROLE,
-                        DiscoveryNodeRole.DATA_COLD_NODE_ROLE,
-                        DiscoveryNodeRole.DATA_FROZEN_NODE_ROLE,
-                        DiscoveryNodeRole.DATA_CONTENT_NODE_ROLE
-                    )
-                )
-            );
-            nonSnapshotNodeRole = Set.copyOf(
-                randomNonEmptySubsetOf(
-                    Set.of(DiscoveryNodeRole.MASTER_ROLE, DiscoveryNodeRole.ML_ROLE, DiscoveryNodeRole.VOTING_ONLY_NODE_ROLE)
-                )
-            );
-        }
-
-        final var nodesBuilder = DiscoveryNodes.builder();
-        for (var snapshotNodeId : snapshotNodeIds) {
-            nodesBuilder.add(DiscoveryNodeUtils.builder(snapshotNodeId).name(snapshotNodeId).roles(snapshotNodeRole).build());
-        }
-        for (var nonSnapshotNodeId : nonSnapshotNodeIds) {
-            nodesBuilder.add(DiscoveryNodeUtils.builder(nonSnapshotNodeId).name(nonSnapshotNodeId).roles(nonSnapshotNodeRole).build());
-        }
-        final DiscoveryNodes discoveryNodes = nodesBuilder.build();
-
+        final DiscoveryNodes discoveryNodes = randomDiscoveryNodes(snapshotNodeIds, nonSnapshotNodeIds, isStateless);
         final int perNodeLimit = between(1, 10);
 
         // Basic test when there is no ongoing snapshots
@@ -169,6 +321,43 @@ public class PerNodeShardSnapshotCounterTests extends ESTestCase {
         }
     }
 
+    private static DiscoveryNodes randomDiscoveryNodes(List<String> snapshotNodeIds, List<String> nonSnapshotNodeIds, boolean isStateless) {
+        final Set<DiscoveryNodeRole> snapshotNodeRole;
+        final Set<DiscoveryNodeRole> nonSnapshotNodeRole;
+        if (isStateless) {
+            snapshotNodeRole = Set.of(DiscoveryNodeRole.INDEX_ROLE);
+            nonSnapshotNodeRole = Set.of(randomFrom(DiscoveryNodeRole.SEARCH_ROLE, DiscoveryNodeRole.ML_ROLE));
+        } else {
+            snapshotNodeRole = Set.copyOf(
+                randomNonEmptySubsetOf(
+                    Set.of(
+                        DiscoveryNodeRole.DATA_ROLE,
+                        DiscoveryNodeRole.DATA_HOT_NODE_ROLE,
+                        DiscoveryNodeRole.DATA_WARM_NODE_ROLE,
+                        DiscoveryNodeRole.DATA_COLD_NODE_ROLE,
+                        DiscoveryNodeRole.DATA_FROZEN_NODE_ROLE,
+                        DiscoveryNodeRole.DATA_CONTENT_NODE_ROLE
+                    )
+                )
+            );
+            nonSnapshotNodeRole = Set.copyOf(
+                randomNonEmptySubsetOf(
+                    Set.of(DiscoveryNodeRole.MASTER_ROLE, DiscoveryNodeRole.ML_ROLE, DiscoveryNodeRole.VOTING_ONLY_NODE_ROLE)
+                )
+            );
+        }
+
+        final var nodesBuilder = DiscoveryNodes.builder();
+        for (var snapshotNodeId : snapshotNodeIds) {
+            nodesBuilder.add(DiscoveryNodeUtils.builder(snapshotNodeId).name(snapshotNodeId).roles(snapshotNodeRole).build());
+        }
+        for (var nonSnapshotNodeId : nonSnapshotNodeIds) {
+            nodesBuilder.add(DiscoveryNodeUtils.builder(nonSnapshotNodeId).name(nonSnapshotNodeId).roles(nonSnapshotNodeRole).build());
+        }
+        final DiscoveryNodes discoveryNodes = nodesBuilder.build();
+        return discoveryNodes;
+    }
+
     private int runningShardSnapshotsForNode(SnapshotsInProgress snapshotsInProgress, String nodeId) {
         int result = 0;
         final var allEntries = snapshotsInProgress.asStream().toList();
@@ -187,5 +376,9 @@ public class PerNodeShardSnapshotCounterTests extends ESTestCase {
             }
         }
         return result;
+    }
+
+    private ShardId randomShardId() {
+        return new ShardId(new Index(randomAlphaOfLength(12), randomUUID()), between(0, 5));
     }
 }

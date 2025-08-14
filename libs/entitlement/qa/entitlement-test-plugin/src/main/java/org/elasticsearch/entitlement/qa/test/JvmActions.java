2a6becabba49171757d417f9e428393e8df89930
Moritz Mack <mmack@apache.org>
2025-08-14 17:30:52 +0200

    add test

--- libs/entitlement/qa/entitlement-test-plugin/src/main/java/org/elasticsearch/entitlement/qa/test/JvmActions.java
+++ libs/entitlement/qa/entitlement-test-plugin/src/main/java/org/elasticsearch/entitlement/qa/test/JvmActions.java
@@ -70,7 +70,14 @@ class JvmActions {
     }
 
     @EntitlementTest(expectedAccess = PLUGINS)
-    static void createClassLoaderNewInstance() throws IOException {
+    static void createClassLoaderNewInstance1() throws IOException {
+        try (var classLoader = URLClassLoader.newInstance(new URL[0])) {
+            // intentionally empty, just let the loader close
+        }
+    }
+
+    @EntitlementTest(expectedAccess = PLUGINS)
+    static void createClassLoaderNewInstance2() throws IOException {
         try (var classLoader = URLClassLoader.newInstance(new URL[0], RestEntitlementsCheckAction.class.getClassLoader())) {
             // intentionally empty, just let the loader close
         }

6d6b9f083729ea8dde782e1c98da1650ac6296c6
Jordan Powers <jordan.powers@elastic.co>
2025-08-14 11:28:07 -0400

    Fix release tests

--- server/src/main/java/org/elasticsearch/index/mapper/MapperFeatures.java
+++ server/src/main/java/org/elasticsearch/index/mapper/MapperFeatures.java
@@ -12,6 +12,7 @@ package org.elasticsearch.index.mapper;
 import org.elasticsearch.features.FeatureSpecification;
 import org.elasticsearch.features.NodeFeature;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import static org.elasticsearch.index.mapper.vectors.DenseVectorFieldMapper.RESCORE_VECTOR_QUANTIZED_VECTOR_MAPPING;
@@ -51,7 +52,7 @@ public class MapperFeatures implements FeatureSpecification {
 
     @Override
     public Set<NodeFeature> getTestFeatures() {
-        return Set.of(
+        Set<NodeFeature> features = Set.of(
             RangeFieldMapper.DATE_RANGE_INDEXING_FIX,
             IgnoredSourceFieldMapper.DONT_EXPAND_DOTS_IN_IGNORED_SOURCE,
             SourceFieldMapper.REMOVE_SYNTHETIC_SOURCE_ONLY_VALIDATION,
@@ -81,8 +82,14 @@ public class MapperFeatures implements FeatureSpecification {
             BBQ_DISK_SUPPORT,
             SEARCH_LOAD_PER_SHARD,
             SPARSE_VECTOR_INDEX_OPTIONS_FEATURE,
-            PATTERNED_TEXT,
-            IGNORED_SOURCE_FIELDS_PER_ENTRY
+            PATTERNED_TEXT
         );
+
+        if (IgnoredSourceFieldMapper.IGNORED_SOURCE_FIELDS_PER_ENTRY_FF.isEnabled()) {
+            features = new HashSet<>(features);
+            features.add(IGNORED_SOURCE_FIELDS_PER_ENTRY);
+        }
+
+        return features;
     }
 }

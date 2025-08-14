1583fa4887bbb8654526a0fc21bfc1c7e73f3d16
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2025-08-14 10:28:56 -0400

    two fixes

--- server/src/main/java/org/elasticsearch/search/vectors/AbstractIVFKnnVectorQuery.java
+++ server/src/main/java/org/elasticsearch/search/vectors/AbstractIVFKnnVectorQuery.java
@@ -120,7 +120,7 @@ abstract class AbstractIVFKnnVectorQuery extends Query implements QueryProfilerP
         // we need to ensure we are getting at least 2*k results to ensure we cover overspill duplicates
         // TODO move the logic for automatically adjusting percentages to the query, so we can only pass
         // 2k to the collector.
-        KnnCollectorManager knnCollectorManager = getKnnCollectorManager(Math.max(Math.round(2f * k), numCands), indexSearcher);
+        KnnCollectorManager knnCollectorManager = getKnnCollectorManager(Math.round(2f * k), indexSearcher);
         TaskExecutor taskExecutor = indexSearcher.getTaskExecutor();
         List<LeafReaderContext> leafReaderContexts = reader.leaves();
 
@@ -136,7 +136,9 @@ abstract class AbstractIVFKnnVectorQuery extends Query implements QueryProfilerP
         final float visitRatio;
         if (providedVisitRatio == 0.0f) {
             // dynamically set the percentage
-            float expected = (float) Math.round(Math.log10(totalVectors) * Math.log10(totalVectors) * (numCands));
+            float expected = (float) Math.round(
+                Math.log10(totalVectors) * Math.log10(totalVectors) * (Math.min(10_000, Math.max(numCands, 5 * k)))
+            );
             visitRatio = expected / totalVectors;
         } else {
             visitRatio = providedVisitRatio;

c191fbd7e8e05c6018d116ea8a0174cff2d63302
Ryan Ernst <ryan@iernst.net>
2025-08-18 12:48:09 -0700

    Rename initial to unreferenced in transport versions (#133082)
    The concept of "initial" transport version is broader than just the
    initial ids created for releases. There are several cases where ids were
    skipped for various reasons. In order to ensure validation can still
    work (eg density of base ids), this commit broadens the concept to a
    place we can define any unreferenced transport version.

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/transport/ValidateTransportVersionResourcesTask.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/transport/ValidateTransportVersionResourcesTask.java
@@ -110,9 +110,14 @@ public abstract class ValidateTransportVersionResourcesTask extends DefaultTask 
         // now load all definitions, do some validation and record them by various keys for later quick lookup
         // NOTE: this must run after loading referenced names and existing definitions
         // NOTE: this is sorted so that the order of cross validation is deterministic
-        try (var definitionsStream = Files.list(definitionsDir).sorted()) {
-            for (var definitionFile : definitionsStream.toList()) {
-                recordAndValidateDefinition(readDefinitionFile(definitionFile));
+        for (String subDirName : List.of("unreferenced", "named")) {
+            Path subDir = definitionsDir.resolve(subDirName);
+            if (Files.isDirectory(subDir)) {
+                try (var definitionsStream = Files.list(subDir).sorted()) {
+                    for (var definitionFile : definitionsStream.toList()) {
+                        recordAndValidateDefinition(readDefinitionFile(definitionFile));
+                    }
+                }
             }
         }
 

a8fd79535a543aead38fe24a42807ce68bab068e
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-08-18 16:37:38 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/mapper/SemanticTextFieldMapper.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/mapper/SemanticTextFieldMapper.java
@@ -131,10 +131,12 @@ import static org.elasticsearch.xpack.inference.services.elasticsearch.Elasticse
 public class SemanticTextFieldMapper extends FieldMapper implements InferenceFieldMapper {
     private static final Logger logger = LogManager.getLogger(SemanticTextFieldMapper.class);
     // TODO: rewrite the warning and error messages below, just placeholders for now
-    static final String WARNING_MESSAGE_8X = "The [dense_vector] field type created by `semantic_text` uses default index settings which may not be optimal. " +
-        "Consider creating a new index ....";
-    private static final String ERROR_MESSAGE_UNSUPPORTED_SPARSE_VECTOR = "The [sparse_vector] field type created by `semantic_text` is not supported on indices created on versions 8.0 to 8.10." +
-        "Try using a `dense_vector` model or upgrade your index to 8.11 or later. ...";
+    static final String WARNING_MESSAGE_8X =
+        "The [dense_vector] field type created by `semantic_text` uses default index settings which may not be optimal. "
+            + "Consider creating a new index ....";
+    private static final String ERROR_MESSAGE_UNSUPPORTED_SPARSE_VECTOR =
+        "The [sparse_vector] field type created by `semantic_text` is not supported on indices created on versions 8.0 to 8.10."
+            + "Try using a `dense_vector` model or upgrade your index to 8.11 or later. ...";
     public static final NodeFeature SEMANTIC_TEXT_IN_OBJECT_FIELD_FIX = new NodeFeature("semantic_text.in_object_field_fix");
     public static final NodeFeature SEMANTIC_TEXT_SINGLE_FIELD_UPDATE_FIX = new NodeFeature("semantic_text.single_field_update_fix");
     public static final NodeFeature SEMANTIC_TEXT_DELETE_FIX = new NodeFeature("semantic_text.delete_fix");
@@ -1308,7 +1310,7 @@ public class SemanticTextFieldMapper extends FieldMapper implements InferenceFie
         MinimalServiceSettings modelSettings,
         SemanticTextIndexOptions indexOptions
     ) {
-        if (indexVersionCreated.onOrAfter(IndexVersions.V_8_0_0)){
+        if (indexVersionCreated.onOrAfter(IndexVersions.V_8_0_0)) {
             if (indexVersionCreated.before(INDEXED_BY_DEFAULT_INDEX_VERSION)) {
                 deprecationLogger.warn(DeprecationCategory.MAPPINGS, "semantic_text", WARNING_MESSAGE_8X);
             }

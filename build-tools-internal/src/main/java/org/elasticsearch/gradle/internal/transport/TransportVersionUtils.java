5e670cca86a0f5c27fb239d8fb7549b9067af244
Ryan Ernst <ryan@iernst.net>
2025-08-18 20:12:09 -0700

    use resource root for manifest output too

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/transport/TransportVersionUtils.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/transport/TransportVersionUtils.java
@@ -50,11 +50,16 @@ class TransportVersionUtils {
         if (projectName == null) {
             projectName = ":server";
         }
+        var resourceRoot = getResourceRoot(project);
+        Directory projectDir = project.project(projectName.toString()).getLayout().getProjectDirectory();
+        return projectDir.dir("src/main/resources/" + resourceRoot);
+    }
+
+    static String getResourceRoot(Project project) {
         var resourceRoot = project.findProperty("org.elasticsearch.transport.resourceRoot");
         if (resourceRoot == null) {
             resourceRoot = "transport";
         }
-        Directory projectDir = project.project(projectName.toString()).getLayout().getProjectDirectory();
-        return projectDir.dir("src/main/resources/" + resourceRoot);
+        return resourceRoot.toString();
     }
 }

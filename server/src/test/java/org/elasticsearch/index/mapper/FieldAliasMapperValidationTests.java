18b5613b344f488afd61f7af0d5898fded95687d
Dmitry Kubikov <dmitry.kubikov@elastic.co>
2025-08-19 16:27:04 -0700

    Reverted MapperBuilderContext changes to make PR smaller

--- server/src/test/java/org/elasticsearch/index/mapper/FieldAliasMapperValidationTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/FieldAliasMapperValidationTests.java
@@ -161,7 +161,15 @@ public class FieldAliasMapperValidationTests extends ESTestCase {
 
     private static FieldMapper createFieldMapper(String parent, String name) {
         return new BooleanFieldMapper.Builder(name, ScriptCompiler.NONE, false, IndexVersion.current(), null).build(
-            MapperBuilderContext.builder().path(parent).isSourceSynthetic(false).build()
+            new MapperBuilderContext(
+                parent,
+                false,
+                false,
+                false,
+                ObjectMapper.Defaults.DYNAMIC,
+                MapperService.MergeReason.MAPPING_UPDATE,
+                false
+            )
         );
     }
 

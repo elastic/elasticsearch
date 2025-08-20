118705fa0122b5e6a0d68f785550e8313ec8c5d8
ankitsethi <ankit.sethi@elastic.co>
2025-08-20 15:23:25 -0500

    correct javadoc

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/PluggableOAuth2TokenAuthenticator.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/PluggableOAuth2TokenAuthenticator.java
@@ -1,8 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
 package org.elasticsearch.xpack.security.authc;
 
 import org.elasticsearch.xpack.core.security.authc.AuthenticationToken;
 import org.elasticsearch.xpack.core.security.authc.apikey.CustomTokenAuthenticator;
 
+/**
+ * An adapter for {@link CustomTokenAuthenticator} that implements the {@link Authenticator} interface, so the custom API key authenticator
+ * can be plugged into the authenticator chain. Module dependencies prevent us from introducing a direct extension point for
+ * an {@link Authenticator}.
+ */
 public class PluggableOAuth2TokenAuthenticator extends AbstractPluggableAuthenticator {
 
     private CustomTokenAuthenticator customOAuth2TokenAuthenticator;

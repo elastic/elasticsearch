b5663a05f94845fa0efb7ce635f8fc98e2b54df0
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-08-20 14:14:44 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/common/hash/MurmurHash3.java
+++ server/src/main/java/org/elasticsearch/common/hash/MurmurHash3.java
@@ -28,8 +28,7 @@ public enum MurmurHash3 {
         /** higher 64 bits part **/
         public long h2;
 
-        public Hash128() {
-        }
+        public Hash128() {}
 
         public Hash128(long h1, long h2) {
             this.h1 = h1;

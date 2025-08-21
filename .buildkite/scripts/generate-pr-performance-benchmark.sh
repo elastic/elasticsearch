5c1bb75ca501075ff67a080cdec9755a0e09d3ef
Grzegorz Banasiak <grzegorz.banasiak@elastic.co>
2025-08-21 14:48:04 +0200

    Switch to heredoc

--- .buildkite/scripts/generate-pr-performance-benchmark.sh
+++ .buildkite/scripts/generate-pr-performance-benchmark.sh
@@ -13,26 +13,28 @@ buildkite-agent meta-data set pr_comment:custom-baseline:head \
 buildkite-agent meta-data set pr_comment:custom-contender:head \
   "* Contender: ${GITHUB_PR_TRIGGERED_SHA} (env ID ${env_id_contender})"
 
-echo "steps:"
-echo "  - label: Trigger baseline benchmark"
-echo "    trigger: elasticsearch-performance-esbench-pr"
-echo "    build:"
-echo "      message: Baseline benchmark for PR${GITHUB_PR_NUMBER}"
-echo "      branch: master"
-echo "      env:"
-echo "        CONFIGURATION_NAME: ${GITHUB_PR_COMMENT_VAR_BENCHMARK}"
-echo "        ENV_ID: ${env_id_baseline}"
-echo "        REVISION: ${merge_base}"
-echo "  - label: Trigger contender benchmark"
-echo "    trigger: elasticsearch-performance-esbench-pr"
-echo "    build:"
-echo "      message: Contender benchmark for PR${GITHUB_PR_NUMBER}"
-echo "      branch: master"
-echo "      env:"
-echo "        CONFIGURATION_NAME: ${GITHUB_PR_COMMENT_VAR_BENCHMARK}"
-echo "        ENV_ID: ${env_id_contender}"
-echo "        ES_REPO_URL: https://github.com/${GITHUB_PR_OWNER}/${GITHUB_PR_REPO}.git"
-echo "        REVISION: ${GITHUB_PR_TRIGGERED_SHA}"
-echo "  - wait: ~"
-echo "  - label: Modify PR comment"
-echo "    command: buildkite-agent meta-data set pr_comment:custom-comparison:head \"* [Benchmark results](<https://esbench-metrics.kb.us-east-2.aws.elastic-cloud.com:9243/app/dashboards#/view/d9079962-5866-49ef-b9f5-145f2141cd31?_a=(query:(language:kuery,query:'user-tags.env-id:${env_id_baseline} or user-tags.env-id:${env_id_contender}'))>)\""
+cat << _EOF_
+steps:
+  - label: Trigger baseline benchmark
+    trigger: elasticsearch-performance-esbench-pr
+    build:
+      message: Baseline benchmark for PR${GITHUB_PR_NUMBER}
+      branch: master
+      env:
+        CONFIGURATION_NAME: ${GITHUB_PR_COMMENT_VAR_BENCHMARK}
+        ENV_ID: ${env_id_baseline}
+        REVISION: ${merge_base}
+  - label: Trigger contender benchmark
+    trigger: elasticsearch-performance-esbench-pr
+    build:
+      message: Contender benchmark for PR${GITHUB_PR_NUMBER}
+      branch: master
+      env:
+        CONFIGURATION_NAME: ${GITHUB_PR_COMMENT_VAR_BENCHMARK}
+        ENV_ID: ${env_id_contender}
+        ES_REPO_URL: https://github.com/${GITHUB_PR_OWNER}/${GITHUB_PR_REPO}.git
+        REVISION: ${GITHUB_PR_TRIGGERED_SHA}
+  - wait: ~
+  - label: Modify PR comment
+    command: buildkite-agent meta-data set pr_comment:custom-comparison:head "* [Benchmark results](<https://esbench-metrics.kb.us-east-2.aws.elastic-cloud.com:9243/app/dashboards#/view/d9079962-5866-49ef-b9f5-145f2141cd31?_a=(query:(language:kuery,query:'user-tags.env-id:${env_id_baseline} or user-tags.env-id:${env_id_contender}'))>)"
+_EOF_

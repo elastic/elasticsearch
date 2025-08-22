73dcff462a2bee49dbaa7edf85a61e4398abd9dd
Stanislav Malyshev <stas.malyshev@elastic.co>
2025-08-22 17:23:17 -0600

    Use safe double range to test long mapper
    Until https://github.com/elastic/elasticsearch/issues/132893 is fixed this sounds like
    the best way to handle it.

--- server/src/test/java/org/elasticsearch/index/mapper/LongFieldMapperTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/LongFieldMapperTests.java
@@ -103,6 +103,9 @@ public class LongFieldMapperTests extends WholeNumberFieldMapperTests {
         assertThat(doc.rootDoc().getFields("field"), hasSize(1));
     }
 
+    // This is the biggest long that double can represent exactly
+    public static final long MAX_SAFE_LONG_FOR_DOUBLE = 1L << 53;
+
     @Override
     protected Number randomNumber() {
         if (randomBoolean()) {
@@ -111,7 +114,7 @@ public class LongFieldMapperTests extends WholeNumberFieldMapperTests {
         if (randomBoolean()) {
             return randomDouble();
         }
-        return randomDoubleBetween(Long.MIN_VALUE, Long.MAX_VALUE, true);
+        return randomDoubleBetween(-MAX_SAFE_LONG_FOR_DOUBLE, MAX_SAFE_LONG_FOR_DOUBLE, true);
     }
 
     public void testFetchCoerced() throws IOException {

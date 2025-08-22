2bf459b4a39c865ebab9fa5f853c06ab54fb6208
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2025-08-22 12:18:19 +0200

    Apply spotless suggestions

--- x-pack/plugin/logsdb/src/test/java/org/elasticsearch/xpack/logsdb/LogsdbIndexSettingsProviderLegacyLicenseTests.java
+++ x-pack/plugin/logsdb/src/test/java/org/elasticsearch/xpack/logsdb/LogsdbIndexSettingsProviderLegacyLicenseTests.java
@@ -104,17 +104,7 @@ public class LogsdbIndexSettingsProviderLegacyLicenseTests extends ESTestCase {
         String dataStreamName = "metrics-my-app";
         String indexName = DataStream.getDefaultBackingIndexName(dataStreamName, 0);
         Settings.Builder builder = Settings.builder();
-        provider.onCreateIndex(
-            indexName,
-            dataStreamName,
-            IndexMode.TIME_SERIES,
-            null,
-            null,
-            settings,
-            List.of(),
-            builder,
-            (k, v) -> {}
-        );
+        provider.onCreateIndex(indexName, dataStreamName, IndexMode.TIME_SERIES, null, null, settings, List.of(), builder, (k, v) -> {});
         var result = builder.build();
         assertThat(result.size(), equalTo(0));
     }
@@ -147,17 +137,7 @@ public class LogsdbIndexSettingsProviderLegacyLicenseTests extends ESTestCase {
         String dataStreamName = "metrics-my-app";
         String indexName = DataStream.getDefaultBackingIndexName(dataStreamName, 0);
         Settings.Builder builder = Settings.builder();
-        provider.onCreateIndex(
-            indexName,
-            dataStreamName,
-            IndexMode.TIME_SERIES,
-            null,
-            null,
-            settings,
-            List.of(),
-            builder,
-            (k, v) -> {}
-        );
+        provider.onCreateIndex(indexName, dataStreamName, IndexMode.TIME_SERIES, null, null, settings, List.of(), builder, (k, v) -> {});
         var result = builder.build();
         assertThat(result.size(), equalTo(1));
         assertThat(result.get(IndexSettings.INDEX_MAPPER_SOURCE_MODE_SETTING.getKey()), equalTo("STORED"));

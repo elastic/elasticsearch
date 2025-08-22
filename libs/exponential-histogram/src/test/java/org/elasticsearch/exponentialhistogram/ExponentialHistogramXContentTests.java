6d181cb59203b7770afa97b942ac9e635b2d59f7
Jonas Kunz <jonas.kunz@elastic.co>
2025-08-22 09:44:11 +0200

    Remove unnecessary empty line

--- libs/exponential-histogram/src/test/java/org/elasticsearch/exponentialhistogram/ExponentialHistogramXContentTests.java
+++ libs/exponential-histogram/src/test/java/org/elasticsearch/exponentialhistogram/ExponentialHistogramXContentTests.java
@@ -55,7 +55,6 @@ public class ExponentialHistogramXContentTests extends ExponentialHistogramTestC
                     + "}"
             )
         );
-
     }
 
     public void testOnlyZeroThreshold() {

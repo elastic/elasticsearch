fb4675196c9829f79eab2e9450770c698ffca7cc
mjmbischoff <mjmbischoff@controplex.com>
2025-08-25 02:41:41 +0200

    Remove trailing whitespace

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/Contains.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/Contains.java
@@ -47,7 +47,7 @@ public class Contains extends EsqlScalarFunction implements OptionalArgument {
         returnType = "boolean",
         description = """
             Returns a boolean that indicates whether a keyword substring is within another string.
-            Returns `null` if either parameter is null. """,
+            Returns `null` if either parameter is null.""",
         examples = @Example(file = "string", tag = "contains"),
         appliesTo = { @FunctionAppliesTo(lifeCycle = FunctionAppliesToLifecycle.GA, version = "9.2.0") }
     )

8bbddc48709c0599c992e56118d7fa31bfcb324c
Ryan Ernst <ryan@iernst.net>
2025-08-25 16:44:22 -0700

    Rename transport version terminology (#133492)
    This commit further tweaks terminology for the new file-based transport
    version system. Specially it renames:
    named -> referable
    unreferenced -> unreferable
    latest -> upper_bound

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/transport/TransportVersionUpperBound.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/transport/TransportVersionUpperBound.java
@@ -11,17 +11,22 @@ package org.elasticsearch.gradle.internal.transport;
 
 import java.nio.file.Path;
 
-record TransportVersionLatest(String branch, String name, TransportVersionId id) {
-    public static TransportVersionLatest fromString(Path file, String contents) {
+/**
+ * An object to represent the loaded version of a transport version upper bound.
+ *
+ * An upper bound is the maximum transport version id that should be loaded for a given release branch.
+ */
+record TransportVersionUpperBound(String branch, String name, TransportVersionId id) {
+    public static TransportVersionUpperBound fromString(Path file, String contents) {
         String filename = file.getFileName().toString();
         assert filename.endsWith(".csv");
         String branch = filename.substring(0, filename.length() - 4);
 
         String[] parts = contents.split(",");
         if (parts.length != 2) {
-            throw new IllegalStateException("Invalid transport version latest file [" + file + "]: " + contents);
+            throw new IllegalStateException("Invalid transport version upper bound file [" + file + "]: " + contents);
         }
 
-        return new TransportVersionLatest(branch, parts[0], TransportVersionId.fromString(parts[1]));
+        return new TransportVersionUpperBound(branch, parts[0], TransportVersionId.fromString(parts[1]));
     }
 }

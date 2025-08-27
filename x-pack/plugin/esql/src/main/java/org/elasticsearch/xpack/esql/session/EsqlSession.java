3a93ec9aac82ff4324c2a4f1a1383af28963ae79
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-08-27 10:15:43 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
@@ -215,6 +215,7 @@ public class EsqlSession {
             plan = explain.query();
             parsedPlanString = plan.toString();
         }
+<<<<<<< HEAD
         analyzedPlan(
             plan,
             configuration,

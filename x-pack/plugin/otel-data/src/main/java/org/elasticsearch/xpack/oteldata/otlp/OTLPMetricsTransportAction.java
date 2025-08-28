1ab3d89c4cba9cc5b52e158c1a0e45104abbfbfd
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2025-08-28 10:31:46 +0200

    OTLP: add support for counters and gauges

--- x-pack/plugin/otel-data/src/main/java/org/elasticsearch/xpack/oteldata/otlp/OTLPMetricsTransportAction.java
+++ x-pack/plugin/otel-data/src/main/java/org/elasticsearch/xpack/oteldata/otlp/OTLPMetricsTransportAction.java
@@ -7,6 +7,10 @@
 
 package org.elasticsearch.xpack.oteldata.otlp;
 
+import com.google.protobuf.MessageLite;
+import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsPartialSuccess;
+import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceRequest;
+import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.ActionListener;
@@ -15,11 +19,17 @@ import org.elasticsearch.action.ActionRequestValidationException;
 import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.action.ActionType;
 import org.elasticsearch.action.CompositeIndicesRequest;
+import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.HandledTransportAction;
 import org.elasticsearch.client.internal.Client;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.injection.guice.Inject;
@@ -27,8 +37,14 @@ import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
+import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xcontent.XContentFactory;
+import org.elasticsearch.xpack.oteldata.otlp.datapoint.DataPointGroupingContext;
+import org.elasticsearch.xpack.oteldata.otlp.docbuilder.MetricDocumentBuilder;
+import org.elasticsearch.xpack.oteldata.otlp.proto.BufferedByteStringAccessor;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Transport action for handling OpenTelemetry Protocol (OTLP) Metrics requests.
@@ -62,7 +78,110 @@ public class OTLPMetricsTransportAction extends HandledTransportAction<
 
     @Override
     protected void doExecute(Task task, MetricsRequest request, ActionListener<MetricsResponse> listener) {
-        listener.onResponse(new MetricsResponse(RestStatus.NOT_IMPLEMENTED, new BytesArray(new byte[0])));
+        BufferedByteStringAccessor byteStringAccessor = new BufferedByteStringAccessor();
+        DataPointGroupingContext context = new DataPointGroupingContext(byteStringAccessor);
+        try {
+            var metricsServiceRequest = ExportMetricsServiceRequest.parseFrom(request.exportMetricsServiceRequest.streamInput());
+            context.groupDataPoints(metricsServiceRequest);
+            BulkRequestBuilder bulkRequestBuilder = client.prepareBulk();
+            MetricDocumentBuilder metricDocumentBuilder = new MetricDocumentBuilder(byteStringAccessor);
+            context.consume(dataPointGroup -> addIndexRequest(bulkRequestBuilder, metricDocumentBuilder, dataPointGroup));
+            if (bulkRequestBuilder.numberOfActions() == 0) {
+                if (context.totalDataPoints() == 0) {
+                    // If the server receives an empty request
+                    // (a request that does not carry any telemetry data)
+                    // the server SHOULD respond with success.
+                    // https://opentelemetry.io/docs/specs/otlp/#full-success-1
+                    listener.onResponse(new MetricsResponse(RestStatus.OK, ExportMetricsServiceResponse.newBuilder().build()));
+                } else {
+                    // If the processing of the request fails because the request contains data that cannot be decoded
+                    // or is otherwise invalid and such failure is permanent,
+                    // then the server MUST respond with HTTP 400 Bad Request.
+                    // https://opentelemetry.io/docs/specs/otlp/#bad-data
+                    listener.onResponse(
+                        new MetricsResponse(
+                            RestStatus.BAD_REQUEST,
+                            responseWithRejectedDataPoints(context.totalDataPoints(), context.getIgnoredDataPointsMessage())
+                        )
+                    );
+                }
+                return;
+            }
+
+            bulkRequestBuilder.execute(new ActionListener<>() {
+                @Override
+                public void onResponse(BulkResponse bulkItemResponses) {
+                    MessageLite response;
+                    // If the request is only partially accepted
+                    // (i.e. when the server accepts only parts of the data and rejects the rest),
+                    // the server MUST respond with HTTP 200 OK.
+                    // https://opentelemetry.io/docs/specs/otlp/#partial-success-1
+                    RestStatus status = RestStatus.OK;
+                    if (bulkItemResponses.hasFailures() || context.getIgnoredDataPoints() > 0) {
+                        int failures = (int) Arrays.stream(bulkItemResponses.getItems()).filter(BulkItemResponse::isFailed).count();
+                        if (failures == bulkItemResponses.getItems().length) {
+                            // If the processing of the request fails,
+                            // the server MUST respond with appropriate HTTP 4xx or HTTP 5xx status code.
+                            // https://opentelemetry.io/docs/specs/otlp/#failures-1
+                            status = RestStatus.INTERNAL_SERVER_ERROR;
+                        }
+                        response = responseWithRejectedDataPoints(
+                            failures + context.getIgnoredDataPoints(),
+                            bulkItemResponses.buildFailureMessage() + context.getIgnoredDataPointsMessage()
+                        );
+                    } else {
+                        response = ExportMetricsServiceResponse.newBuilder().build();
+                    }
+                    listener.onResponse(new MetricsResponse(status, response));
+                }
+
+                @Override
+                public void onFailure(Exception e) {
+                    listener.onResponse(
+                        // https://opentelemetry.io/docs/specs/otlp/#failures-1
+                        // If the processing of the request fails,
+                        // the server MUST respond with appropriate HTTP 4xx or HTTP 5xx status code.
+                        new MetricsResponse(
+                            RestStatus.INTERNAL_SERVER_ERROR,
+                            responseWithRejectedDataPoints(context.totalDataPoints(), e.getMessage())
+                        )
+                    );
+                }
+            });
+
+        } catch (Exception e) {
+            logger.error("failed to execute otlp metrics request", e);
+            listener.onResponse(new MetricsResponse(
+                    RestStatus.INTERNAL_SERVER_ERROR,
+                    responseWithRejectedDataPoints(context.totalDataPoints(), e.getMessage())
+            ));
+        }
+    }
+
+    private static ExportMetricsPartialSuccess responseWithRejectedDataPoints(int rejectedDataPoints, String message) {
+        return ExportMetricsServiceResponse.newBuilder()
+            .getPartialSuccessBuilder()
+            .setRejectedDataPoints(rejectedDataPoints)
+            .setErrorMessage(message)
+            .build();
+    }
+
+    private void addIndexRequest(
+        BulkRequestBuilder bulkRequestBuilder,
+        MetricDocumentBuilder metricDocumentBuilder,
+        DataPointGroupingContext.DataPointGroup dataPointGroup
+    ) throws IOException {
+        try (XContentBuilder xContentBuilder = XContentFactory.cborBuilder(new BytesStreamOutput())) {
+            var dynamicTemplates = metricDocumentBuilder.buildMetricDocument(xContentBuilder, dataPointGroup);
+            bulkRequestBuilder.add(
+                new IndexRequest(dataPointGroup.targetIndex()).opType(DocWriteRequest.OpType.CREATE)
+                    .setRequireDataStream(true)
+                    .source(xContentBuilder)
+                    // TODO explicitly set _tsid once https://github.com/elastic/elasticsearch/pull/132566 is merged
+                    //.tsid(DataPointGroupTsidFunnel.forDataPointGroup(dataPointGroup).buildTsid())
+                    .setDynamicTemplates(dynamicTemplates)
+            );
+        }
     }
 
     public static class MetricsRequest extends ActionRequest implements CompositeIndicesRequest {
@@ -86,6 +205,10 @@ public class OTLPMetricsTransportAction extends HandledTransportAction<
     public static class MetricsResponse extends ActionResponse {
         private final BytesReference response;
         private final RestStatus status;
+
+        public MetricsResponse(RestStatus status, MessageLite response) {
+            this(status, new BytesArray(response.toByteArray()));
+        }
 
         public MetricsResponse(RestStatus status, BytesReference response) {
             this.response = response;
---
+++ x-pack/plugin/otel-data/src/main/java/org/elasticsearch/xpack/oteldata/otlp/datapoint/DataPointGroupingContext.java
@@ -0,0 +1,316 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.oteldata.otlp.datapoint;
+
+import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceRequest;
+import io.opentelemetry.proto.common.v1.InstrumentationScope;
+import io.opentelemetry.proto.common.v1.KeyValue;
+import io.opentelemetry.proto.metrics.v1.Metric;
+import io.opentelemetry.proto.metrics.v1.ResourceMetrics;
+import io.opentelemetry.proto.metrics.v1.ScopeMetrics;
+import io.opentelemetry.proto.resource.v1.Resource;
+
+import com.google.protobuf.ByteString;
+
+import org.elasticsearch.cluster.routing.TsidBuilder;
+import org.elasticsearch.common.hash.BufferedMurmur3Hasher;
+import org.elasticsearch.common.hash.MurmurHash3.Hash128;
+import org.elasticsearch.core.CheckedConsumer;
+import org.elasticsearch.xpack.oteldata.otlp.proto.BufferedByteStringAccessor;
+import org.elasticsearch.xpack.oteldata.otlp.tsid.DataPointTsidFunnel;
+import org.elasticsearch.xpack.oteldata.otlp.tsid.ResourceTsidFunnel;
+import org.elasticsearch.xpack.oteldata.otlp.tsid.ScopeTsidFunnel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiFunction;
+
+public class DataPointGroupingContext {
+
+    private final BufferedByteStringAccessor byteStringAccessor;
+    private final Map<Hash128, ResourceGroup> resourceGroups = new HashMap<>();
+    private final Set<String> ignoredDataPointMessages = new HashSet<>();
+
+    private int totalDataPoints = 0;
+    private int ignoredDataPoints = 0;
+
+    public DataPointGroupingContext(BufferedByteStringAccessor byteStringAccessor) {
+        this.byteStringAccessor = byteStringAccessor;
+    }
+
+    public void groupDataPoints(ExportMetricsServiceRequest exportMetricsServiceRequest) {
+        List<ResourceMetrics> resourceMetricsList = exportMetricsServiceRequest.getResourceMetricsList();
+        for (int i = 0; i < resourceMetricsList.size(); i++) {
+            ResourceMetrics resourceMetrics = resourceMetricsList.get(i);
+            ResourceGroup resourceGroup = getOrCreateResourceGroup(resourceMetrics);
+            List<ScopeMetrics> scopeMetricsList = resourceMetrics.getScopeMetricsList();
+            for (int j = 0; j < scopeMetricsList.size(); j++) {
+                ScopeMetrics scopeMetrics = scopeMetricsList.get(j);
+                ScopeGroup scopeGroup = resourceGroup.getOrCreateScope(scopeMetrics);
+                List<Metric> metricsList = scopeMetrics.getMetricsList();
+                for (int k = 0; k < metricsList.size(); k++) {
+                    var metric = metricsList.get(k);
+                    switch (metric.getDataCase()) {
+                        case SUM:
+                            scopeGroup.addDataPoints(metric, metric.getSum().getDataPointsList(), DataPoint.Number::new);
+                            break;
+                        case GAUGE:
+                            scopeGroup.addDataPoints(metric, metric.getGauge().getDataPointsList(), DataPoint.Number::new);
+                            break;
+                        case EXPONENTIAL_HISTOGRAM:
+                            ignoredDataPoints += metric.getExponentialHistogram().getDataPointsCount();
+                            ignoredDataPointMessages.add("Exponential histogram is not supported yet. Dropping " + metric.getName());
+                            break;
+                        case HISTOGRAM:
+                            ignoredDataPoints += metric.getHistogram().getDataPointsCount();
+                            ignoredDataPointMessages.add("Histogram is not supported yet. Dropping " + metric.getName());
+                            break;
+                        case SUMMARY:
+                            ignoredDataPoints += metric.getSummary().getDataPointsList().size();
+                            ignoredDataPointMessages.add("Summary is not supported yet. Dropping " + metric.getName());
+                            break;
+                        default:
+                            ignoredDataPoints++;
+                            ignoredDataPointMessages.add("unsupported metric type " + metric.getDataCase());
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Consumes all data point groups in the context, removing them from the context.
+     *
+     * @param consumer the consumer to process each {@link DataPointGroup}
+     * @param <E>      the type of exception that can be thrown by the consumer
+     * @throws E if the consumer throws an exception
+     */
+    public <E extends Exception> void consume(CheckedConsumer<DataPointGroup, E> consumer) throws E {
+        for (Iterator<ResourceGroup> iterator = resourceGroups.values().iterator(); iterator.hasNext();) {
+            ResourceGroup resourceGroup = iterator.next();
+            // Remove the resource group from the map can help to significantly reduce GC overhead.
+            // This avoids that the resource groups are promoted to survivor space when the context is kept alive for a while,
+            // for example, when referenced in the bulk response listener.
+            iterator.remove();
+            resourceGroup.forEach(consumer);
+        }
+    }
+
+    public int totalDataPoints() {
+        return totalDataPoints;
+    }
+
+    public int getIgnoredDataPoints() {
+        return ignoredDataPoints;
+    }
+
+    public String getIgnoredDataPointsMessage() {
+        return ignoredDataPointMessages.isEmpty() ? "" : String.join("\n", ignoredDataPointMessages);
+    }
+
+    private ResourceGroup getOrCreateResourceGroup(ResourceMetrics resourceMetrics) {
+        TsidBuilder resourceTsidBuilder = ResourceTsidFunnel.forResource(byteStringAccessor, resourceMetrics);
+        Hash128 resourceHash = resourceTsidBuilder.hash();
+        ResourceGroup resourceGroup = resourceGroups.get(resourceHash);
+        if (resourceGroup == null) {
+            resourceGroup = new ResourceGroup(resourceMetrics.getResource(), resourceMetrics.getSchemaUrlBytes());
+            resourceGroups.put(resourceHash, resourceGroup);
+        }
+        return resourceGroup;
+    }
+
+    class ResourceGroup {
+        private final Resource resource;
+        private final ByteString resourceSchemaUrl;
+        private final Map<Hash128, ScopeGroup> scopes;
+
+        ResourceGroup(Resource resource, ByteString resourceSchemaUrl) {
+            this.resource = resource;
+            this.resourceSchemaUrl = resourceSchemaUrl;
+            this.scopes = new HashMap<>();
+        }
+
+        public ScopeGroup getOrCreateScope(ScopeMetrics scopeMetrics) {
+            TsidBuilder scopeTsidBuilder = ScopeTsidFunnel.forScope(byteStringAccessor, scopeMetrics);
+            Hash128 scopeHash = scopeTsidBuilder.hash();
+            ScopeGroup scopeGroup = scopes.get(scopeHash);
+            if (scopeGroup == null) {
+                scopeGroup = new ScopeGroup(this, scopeMetrics.getScope(), scopeMetrics.getSchemaUrlBytes());
+                scopes.put(scopeHash, scopeGroup);
+            }
+            return scopeGroup;
+        }
+
+        public <E extends Exception> void forEach(CheckedConsumer<DataPointGroup, E> consumer) throws E {
+            for (ScopeGroup scopeGroup : scopes.values()) {
+                scopeGroup.forEach(consumer);
+            }
+        }
+    }
+
+    class ScopeGroup {
+        private final ResourceGroup resourceGroup;
+        private final InstrumentationScope scope;
+        private final ByteString scopeSchemaUrl;
+        // index -> timiestamp -> dataPointGroupHash -> DataPointGroup
+        private final Map<String, Map<Hash128, Map<Hash128, DataPointGroup>>> dataPointGroupsByIndexAndTimestamp;
+
+        ScopeGroup(ResourceGroup resourceGroup, InstrumentationScope scope, ByteString scopeSchemaUrl) {
+            this.resourceGroup = resourceGroup;
+            this.scope = scope;
+            this.scopeSchemaUrl = scopeSchemaUrl;
+            this.dataPointGroupsByIndexAndTimestamp = new HashMap<>();
+        }
+
+        public <T> void addDataPoints(Metric metric, List<T> dataPoints, BiFunction<T, Metric, DataPoint> createDataPoint) {
+            for (int i = 0; i < dataPoints.size(); i++) {
+                T dataPoint = dataPoints.get(i);
+                addDataPoint(createDataPoint.apply(dataPoint, metric));
+            }
+        }
+
+        public void addDataPoint(DataPoint dataPoint) {
+            totalDataPoints++;
+            if (dataPoint.isValid(ignoredDataPointMessages) == false) {
+                ignoredDataPoints++;
+                return;
+            }
+            getOrCreateDataPointGroup(dataPoint).addDataPoint(dataPoint);
+        }
+
+        private DataPointGroup getOrCreateDataPointGroup(DataPoint dataPoint) {
+            TsidBuilder dataPointGroupTsidBuilder = DataPointTsidFunnel.forDataPoint(byteStringAccessor, dataPoint);
+            Hash128 dataPointGroupHash = dataPointGroupTsidBuilder.hash();
+            // in addition to the fields that go into the _tsid, we also need to group by timestamp and start timestamp
+            Hash128 timestamp = new Hash128(dataPoint.getTimestampUnixNano(), dataPoint.getStartTimestampUnixNano());
+            // TODO determine based on attributes and scope name
+            String targetIndex = "metrics-generic.otel-default";
+            var dataPointGroupsByTimestamp = dataPointGroupsByIndexAndTimestamp.computeIfAbsent(targetIndex, k -> new HashMap<>());
+            var dataPointGroups = dataPointGroupsByTimestamp.computeIfAbsent(timestamp, k -> new HashMap<>());
+            DataPointGroup dataPointGroup = dataPointGroups.get(dataPointGroupHash);
+            if (dataPointGroup == null) {
+                dataPointGroup = new DataPointGroup(
+                    resourceGroup.resource,
+                    resourceGroup.resourceSchemaUrl,
+                    scope,
+                    scopeSchemaUrl,
+                    dataPoint.getAttributes(),
+                    dataPoint.getUnit(),
+                    new ArrayList<>(),
+                    targetIndex
+                );
+                dataPointGroups.put(dataPointGroupHash, dataPointGroup);
+            }
+            return dataPointGroup;
+        }
+
+        public <E extends Exception> void forEach(CheckedConsumer<DataPointGroup, E> consumer) throws E {
+            for (var dataPointGroupsByTime : dataPointGroupsByIndexAndTimestamp.values()) {
+                for (var dataPointGroups : dataPointGroupsByTime.values()) {
+                    for (DataPointGroup dataPointGroup : dataPointGroups.values()) {
+                        consumer.accept(dataPointGroup);
+                    }
+                }
+            }
+        }
+    }
+
+    public static final class DataPointGroup {
+        private final Resource resource;
+        private final ByteString resourceSchemaUrl;
+        private final InstrumentationScope scope;
+        private final ByteString scopeSchemaUrl;
+        private final List<KeyValue> dataPointAttributes;
+        private final String unit;
+        private final List<DataPoint> dataPoints;
+        private final String targetIndex;
+        private String metricNamesHash;
+
+        public DataPointGroup(
+            Resource resource,
+            ByteString resourceSchemaUrl,
+            InstrumentationScope scope,
+            ByteString scopeSchemaUrl,
+            List<KeyValue> dataPointAttributes,
+            String unit,
+            List<DataPoint> dataPoints,
+            String targetIndex
+        ) {
+            this.resource = resource;
+            this.resourceSchemaUrl = resourceSchemaUrl;
+            this.scope = scope;
+            this.scopeSchemaUrl = scopeSchemaUrl;
+            this.dataPointAttributes = dataPointAttributes;
+            this.unit = unit;
+            this.dataPoints = dataPoints;
+            this.targetIndex = targetIndex;
+        }
+
+        public long getTimestampUnixNano() {
+            return dataPoints.getFirst().getTimestampUnixNano();
+        }
+
+        public long getStartTimestampUnixNano() {
+            return dataPoints.getFirst().getStartTimestampUnixNano();
+        }
+
+        public String getMetricNamesHash() {
+            if (metricNamesHash == null) {
+                BufferedMurmur3Hasher hasher = new BufferedMurmur3Hasher(0);
+                for (int i = 0; i < dataPoints.size(); i++) {
+                    hasher.addString(dataPoints.get(i).getMetricName());
+                }
+                metricNamesHash = Integer.toHexString(hasher.digestHash().hashCode());
+            }
+            return metricNamesHash;
+        }
+
+        public void addDataPoint(DataPoint dataPoint) {
+            metricNamesHash = null; // reset the hash when adding a new data point
+            dataPoints.add(dataPoint);
+        }
+
+        public Resource resource() {
+            return resource;
+        }
+
+        public ByteString resourceSchemaUrl() {
+            return resourceSchemaUrl;
+        }
+
+        public InstrumentationScope scope() {
+            return scope;
+        }
+
+        public ByteString scopeSchemaUrl() {
+            return scopeSchemaUrl;
+        }
+
+        public List<KeyValue> dataPointAttributes() {
+            return dataPointAttributes;
+        }
+
+        public String unit() {
+            return unit;
+        }
+
+        public List<DataPoint> dataPoints() {
+            return dataPoints;
+        }
+
+        public String targetIndex() {
+            return targetIndex;
+        }
+    }
+}

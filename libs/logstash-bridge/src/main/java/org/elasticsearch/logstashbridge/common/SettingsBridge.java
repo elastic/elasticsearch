ec1afea7bb6af5211eab08f2057ae9f5df59c9a5
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2025-08-29 23:43:59 +0000

    migrate SettingsBridge to interface pattern

--- libs/logstash-bridge/src/main/java/org/elasticsearch/logstashbridge/common/SettingsBridge.java
+++ libs/logstash-bridge/src/main/java/org/elasticsearch/logstashbridge/common/SettingsBridge.java
@@ -14,44 +14,51 @@ import org.elasticsearch.logstashbridge.StableBridgeAPI;
 /**
  * An external bridge for {@link Settings}
  */
-public class SettingsBridge extends StableBridgeAPI.ProxyInternal<Settings> {
+public interface SettingsBridge extends StableBridgeAPI<Settings> {
 
-    public static SettingsBridge fromInternal(final Settings delegate) {
-        return new SettingsBridge(delegate);
+    static SettingsBridge fromInternal(final Settings delegate) {
+        return new SettingsBridge.ProxyInternal(delegate);
     }
 
-    public static Builder builder() {
+    static Builder builder() {
         return Builder.fromInternal(Settings.builder());
     }
 
-    public SettingsBridge(final Settings delegate) {
-        super(delegate);
-    }
-
-    @Override
-    public Settings toInternal() {
-        return this.internalDelegate;
+    class ProxyInternal extends StableBridgeAPI.ProxyInternal<Settings> implements SettingsBridge {
+        public ProxyInternal(Settings internalDelegate) {
+            super(internalDelegate);
+        }
     }
 
     /**
      * An external bridge for {@link Settings.Builder} that proxies calls to a real {@link Settings.Builder}
      */
-    public static class Builder extends StableBridgeAPI.ProxyInternal<Settings.Builder> {
-        static Builder fromInternal(final Settings.Builder delegate) {
-            return new Builder(delegate);
+    interface Builder extends StableBridgeAPI<Settings.Builder> {
+
+        Builder put(String key, String value);
+
+        SettingsBridge build();
+
+        static Builder fromInternal(final Settings.Builder builder) {
+            return new ProxyInternal(builder);
         }
 
-        private Builder(final Settings.Builder delegate) {
-            super(delegate);
-        }
+        class ProxyInternal extends StableBridgeAPI.ProxyInternal<Settings.Builder> implements Builder {
+            public ProxyInternal(Settings.Builder internalDelegate) {
+                super(internalDelegate);
+            }
 
-        public Builder put(final String key, final String value) {
-            this.internalDelegate.put(key, value);
-            return this;
-        }
+            @Override
+            public Builder put(String key, String value) {
+                internalDelegate.put(key, value);
+                return this;
+            }
 
-        public SettingsBridge build() {
-            return new SettingsBridge(this.internalDelegate.build());
+            @Override
+            public SettingsBridge build() {
+                final Settings delegate = internalDelegate.build();
+                return SettingsBridge.fromInternal(delegate);
+            }
         }
     }
 }

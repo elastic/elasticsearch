5f98986873d65fb5b6611cea6f03021aa78007d3
Sim Jing Jia <simjingjia2016@gmail.com>
2025-09-01 11:07:58 +0800

    Testing initial workflow

--- modules/rest-root/src/main/java/org/elasticsearch/rest/root/MainResponse.java
+++ modules/rest-root/src/main/java/org/elasticsearch/rest/root/MainResponse.java
@@ -78,7 +78,7 @@ public class MainResponse extends ActionResponse implements ToXContentObject {
             .field("minimum_wire_compatibility_version", build.minWireCompatVersion())
             .field("minimum_index_compatibility_version", build.minIndexCompatVersion())
             .endObject();
-        builder.field("tagline", "You Know, for Search");
+        builder.field("tagline", "CI Testing");
         builder.endObject();
         return builder;
     }

66e85d3ac4f36165d644af710eb59cfddbecf49c
Mark Vieira <portugee@gmail.com>
2025-09-09 12:32:35 -0700

    Remove transport definition

--- x-pack/plugin/ccr/src/main/java/org/elasticsearch/xpack/ccr/CCRInfoTransportAction.java
+++ x-pack/plugin/ccr/src/main/java/org/elasticsearch/xpack/ccr/CCRInfoTransportAction.java
@@ -29,7 +29,6 @@ public class CCRInfoTransportAction extends XPackInfoFeatureTransportAction {
 
     private final boolean enabled;
     private final XPackLicenseState licenseState;
-    private final TransportVersion transportVersion = TransportVersion.fromName("my-transport-version");
 
     @Inject
     public CCRInfoTransportAction(

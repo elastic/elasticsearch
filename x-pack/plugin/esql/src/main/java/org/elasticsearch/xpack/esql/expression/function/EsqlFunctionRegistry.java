40883ee105926b0153ecc8cbc028ce7cde0542b2
Jan Kuipers <jan.kuipers@elastic.co>
2025-09-09 14:00:53 +0200

    ES|QL confidence_interval

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/EsqlFunctionRegistry.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/EsqlFunctionRegistry.java
@@ -154,6 +154,7 @@ import org.elasticsearch.xpack.esql.expression.function.scalar.math.Sqrt;
 import org.elasticsearch.xpack.esql.expression.function.scalar.math.Tan;
 import org.elasticsearch.xpack.esql.expression.function.scalar.math.Tanh;
 import org.elasticsearch.xpack.esql.expression.function.scalar.math.Tau;
+import org.elasticsearch.xpack.esql.expression.function.scalar.multivalue.ConfidenceInterval;
 import org.elasticsearch.xpack.esql.expression.function.scalar.multivalue.MvAppend;
 import org.elasticsearch.xpack.esql.expression.function.scalar.multivalue.MvAvg;
 import org.elasticsearch.xpack.esql.expression.function.scalar.multivalue.MvConcat;
@@ -540,7 +541,8 @@ public class EsqlFunctionRegistry {
                 def(MvUnion.class, MvUnion::new, "mv_union"),
                 def(MvZip.class, MvZip::new, "mv_zip"),
                 def(MvSum.class, MvSum::new, "mv_sum"),
-                def(Split.class, Split::new, "split") },
+                def(Split.class, Split::new, "split"),
+                def(ConfidenceInterval.class, ConfidenceInterval::new, "confidence_interval") },
             // search functions
             new FunctionDefinition[] {
                 def(Decay.class, quad(Decay::new), "decay"),

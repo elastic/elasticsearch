9824ffdc47ada97370a062c1cc252565d2f87da9
Jack Conradson <osjdconrad@gmail.com>
2025-09-10 14:45:20 -0700

    [8.19] Migrate transport version 9119000 (#134473) (#134496)
    * Migrate transport version 9119000 (#134473)
    
    Migrates the combined versions 9119000, 9112002, 8841064
    
    ES-12334
    
    * add missing upper bounds files

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -241,7 +241,6 @@ public class TransportVersions {
     public static final TransportVersion ESQL_DOCUMENTS_FOUND_AND_VALUES_LOADED_8_19 = def(8_841_0_61);
     public static final TransportVersion ESQL_PROFILE_INCLUDE_PLAN_8_19 = def(8_841_0_62);
     public static final TransportVersion ESQL_SPLIT_ON_BIG_VALUES_8_19 = def(8_841_0_63);
-    public static final TransportVersion ESQL_FIXED_INDEX_LIKE_8_19 = def(8_841_0_64);
     public static final TransportVersion INITIAL_ELASTICSEARCH_8_19_4 = def(8_841_0_68);
 
     /*

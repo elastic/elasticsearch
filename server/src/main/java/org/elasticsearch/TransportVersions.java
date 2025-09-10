9a0779b4aa8f9e85c1bc8edd5086da3a3095305e
Jack Conradson <osjdconrad@gmail.com>
2025-09-10 09:39:09 -0700

    remove comment

--- server/src/main/java/org/elasticsearch/TransportVersions.java
+++ server/src/main/java/org/elasticsearch/TransportVersions.java
@@ -324,7 +324,6 @@ public class TransportVersions {
     public static final TransportVersion ESQL_PROFILE_INCLUDE_PLAN = def(9_111_0_00);
     public static final TransportVersion MAPPINGS_IN_DATA_STREAMS = def(9_112_0_00);
     public static final TransportVersion ESQL_SPLIT_ON_BIG_VALUES_9_1 = def(9_112_0_01);
-    // If you need to backport to 9.1 the next patch version is 9_112_0_08
     public static final TransportVersion PROJECT_STATE_REGISTRY_RECORDS_DELETIONS = def(9_113_0_00);
     public static final TransportVersion ESQL_SERIALIZE_TIMESERIES_FIELD_TYPE = def(9_114_0_00);
     public static final TransportVersion ML_INFERENCE_IBM_WATSONX_COMPLETION_ADDED = def(9_115_0_00);

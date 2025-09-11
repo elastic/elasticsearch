af68b543a033f445bb5a6ce729cfbf12f5907393
Stanislav Malyshev <stas.malyshev@elastic.co>
2025-09-11 15:54:52 -0600

    Add inlinestats stop test

--- x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/CrossClusterAsyncQueryStopIT.java
+++ x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/CrossClusterAsyncQueryStopIT.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.elasticsearch.xpack.esql.action.AbstractEsqlIntegTestCase.randomIncludeCCSMetadata;
@@ -267,4 +268,88 @@ public class CrossClusterAsyncQueryStopIT extends AbstractCrossClusterTestCase {
             assertAcked(deleteAsyncId(client(), asyncExecutionId));
         }
     }
+
+    // What happens here is when we run stop on subquery, the main query will still finish.
+    // TODO: should we have a way to stop the main query as well?
+    public void testStopQueryInlinestats() throws Exception {
+        assumeTrue("Pragma does not work in release builds", Build.current().isSnapshot());
+        Map<String, Object> testClusterInfo = setupClusters(3);
+        int localNumShards = (Integer) testClusterInfo.get("local.num_shards");
+        int remote1NumShards = (Integer) testClusterInfo.get("remote1.num_shards");
+        // Create large index so we could be sure we're stopping before the end
+        populateRuntimeIndex(REMOTE_CLUSTER_2, "pause_count", INDEX_WITH_BLOCKING_MAPPING);
+
+        Tuple<Boolean, Boolean> includeCCSMetadata = randomIncludeCCSMetadata();
+        boolean responseExpectMeta = includeCCSMetadata.v2();
+
+        final String asyncExecutionId = startAsyncQueryWithPragmas(
+            client(),
+            "FROM logs-*,remote-b:blocking | INLINESTATS total=sum(coalesce(const,v)) | KEEP total,foo | LIMIT 100",
+            includeCCSMetadata.v1(),
+            Map.of("page_size", 1, "data_partitioning", "shard", "task_concurrency", 1)
+        );
+        try {
+            // wait until we know that the query against 'remote-b:blocking' has started
+            CountingPauseFieldPlugin.startEmitting.await(30, TimeUnit.SECONDS);
+
+            /* at this point:
+             *  the query against remote-b should be running (blocked on the PauseFieldPlugin.allowEmitting CountDown)
+             *  the query against the local cluster should be running because it has a STATS clause that needs to wait on remote-b
+             */
+
+            // run the stop query
+            AsyncStopRequest stopRequest = new AsyncStopRequest(asyncExecutionId);
+            ActionFuture<EsqlQueryResponse> stopAction = client().execute(EsqlAsyncStopAction.INSTANCE, stopRequest);
+            assertBusy(() -> {
+                List<TaskInfo> tasks = getDriverTasks(client(REMOTE_CLUSTER_2));
+                List<TaskInfo> reduceTasks = tasks.stream()
+                    .filter(t -> t.description().contains("_LuceneSourceOperator") == false)
+                    .toList();
+                assertThat(reduceTasks, empty());
+            });
+            // allow remoteB query to proceed
+            CountingPauseFieldPlugin.allowEmitting.countDown();
+
+            // We're not getting any proper result here since we stopped the query before it could complete
+            try (EsqlQueryResponse asyncResponse = stopAction.actionGet(30, TimeUnit.SECONDS)) {
+                // We will have all rows here but total will be null since we stopped the inline stats before it could complete
+                assertThat(asyncResponse.isRunning(), is(false));
+                assertThat(asyncResponse.columns().size(), equalTo(2));
+                AtomicInteger i = new AtomicInteger(0);
+                asyncResponse.values().forEachRemaining(row -> {
+                    assertThat(row.next(), equalTo(null));
+                    var v = row.next();
+                    if (v != null) {
+                        assertThat((long) v, lessThanOrEqualTo(10L));
+                    }
+                    assertFalse(row.hasNext());
+                    i.getAndIncrement();
+                });
+                assertThat(i.get(), equalTo(20));
+
+                EsqlExecutionInfo executionInfo = asyncResponse.getExecutionInfo();
+                assertNotNull(executionInfo);
+                assertThat(executionInfo.isCrossClusterSearch(), is(true));
+                long overallTookMillis = executionInfo.overallTook().millis();
+                assertThat(overallTookMillis, greaterThanOrEqualTo(0L));
+                assertThat(executionInfo.clusterAliases(), equalTo(Set.of(LOCAL_CLUSTER, REMOTE_CLUSTER_2)));
+                assertThat(executionInfo.isPartial(), equalTo(true));
+
+                EsqlExecutionInfo.Cluster remote2Cluster = executionInfo.getCluster(REMOTE_CLUSTER_2);
+                assertThat(remote2Cluster.getIndexExpression(), equalTo("blocking"));
+                assertThat(remote2Cluster.getStatus(), equalTo(EsqlExecutionInfo.Cluster.Status.PARTIAL));
+
+                EsqlExecutionInfo.Cluster localCluster = executionInfo.getCluster(LOCAL_CLUSTER);
+                assertThat(localCluster.getIndexExpression(), equalTo("logs-*"));
+                assertThat(localCluster.getStatus(), equalTo(EsqlExecutionInfo.Cluster.Status.PARTIAL));
+
+                assertClusterMetadataInResponse(asyncResponse, responseExpectMeta, 2);
+            }
+        } finally {
+            // Ensure proper cleanup if the test fails
+            CountingPauseFieldPlugin.allowEmitting.countDown();
+            assertAcked(deleteAsyncId(client(), asyncExecutionId));
+        }
+    }
+
 }

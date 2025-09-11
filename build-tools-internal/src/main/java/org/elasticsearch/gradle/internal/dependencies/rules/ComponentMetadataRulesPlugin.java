07b823fd92d2331c83931d869727024b3369410a
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-09-11 15:37:42 +0000

    [CI] Auto commit changes from spotless

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/dependencies/rules/ComponentMetadataRulesPlugin.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/dependencies/rules/ComponentMetadataRulesPlugin.java
@@ -284,9 +284,7 @@ public class ComponentMetadataRulesPlugin implements Plugin<Settings> {
         components.withModule("org.apache.directory.server:apacheds-interceptors-authn", ExcludeAllTransitivesRule.class);
 
         // org.apache.tika:tika-core brings in org.slf4j:slf4j-api:1.17.36. We use 2.0.6
-        components.withModule("org.apache.ftpserver:ftpserver-core", ExcludeByGroup.class, rule ->
-            rule.params(List.of("org.slf4j"))
-        );
+        components.withModule("org.apache.ftpserver:ftpserver-core", ExcludeByGroup.class, rule -> rule.params(List.of("org.slf4j")));
 
         // Hadoop dependencies
         components.withModule("org.apache.hadoop:hadoop-client-api", ExcludeAllTransitivesRule.class);

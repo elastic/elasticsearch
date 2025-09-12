30bad49a464eb7dcd426c587ed23098b207ccc37
Mike Pellegrini <mike.pellegrini@elastic.co>
2025-09-12 16:34:07 -0400

    Implemented testInterceptAndRewrite for InterceptedInferenceMatchQueryBuilderTests

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/queries/InterceptedInferenceMatchQueryBuilderTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/queries/InterceptedInferenceMatchQueryBuilderTests.java
@@ -14,6 +14,8 @@ import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryRewriteContext;
 import org.elasticsearch.plugins.internal.rewriter.QueryRewriteInterceptor;
 
+import java.util.Map;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.notNullValue;
@@ -64,5 +66,73 @@ public class InterceptedInferenceMatchQueryBuilderTests extends AbstractIntercep
     protected void assertCoordinatorNodeRewriteOnNonInferenceField(QueryBuilder original, QueryBuilder rewritten) {
         assertThat(original, instanceOf(MatchQueryBuilder.class));
         assertThat(rewritten, equalTo(original));
+    }
+
+    public void testInterceptAndRewrite() throws Exception {
+        final String field = "test_field";
+        final String queryText = "foo";
+        final TestIndex testIndex1 = new TestIndex("test-index-1", Map.of(field, DENSE_INFERENCE_ID), Map.of());
+        final TestIndex testIndex2 = new TestIndex("test-index-2", Map.of(field, SPARSE_INFERENCE_ID), Map.of());
+        final TestIndex testIndex3 = new TestIndex("test-index-3", Map.of(), Map.of(field, Map.of("type", "text")));
+        final MatchQueryBuilder matchQuery = new MatchQueryBuilder(field, queryText).boost(3.0f).queryName("bar");
+
+        // Perform coordinator node rewrite
+        final QueryRewriteContext queryRewriteContext = createQueryRewriteContext(
+            Map.of(
+                testIndex1.name(),
+                testIndex1.semanticTextFields(),
+                testIndex2.name(),
+                testIndex2.semanticTextFields(),
+                testIndex3.name(),
+                testIndex3.semanticTextFields()
+            ),
+            Map.of(),
+            TransportVersion.current()
+        );
+        QueryBuilder coordinatorRewritten = rewriteAndFetch(matchQuery, queryRewriteContext);
+
+        // Use a serialization cycle to strip InterceptedQueryBuilderWrapper
+        coordinatorRewritten = copyNamedWriteable(coordinatorRewritten, writableRegistry(), QueryBuilder.class);
+        assertThat(coordinatorRewritten, instanceOf(InterceptedInferenceMatchQueryBuilder.class));
+        InterceptedInferenceMatchQueryBuilder coordinatorIntercepted = (InterceptedInferenceMatchQueryBuilder) coordinatorRewritten;
+        assertThat(coordinatorIntercepted.originalQuery, equalTo(matchQuery));
+        assertThat(coordinatorIntercepted.inferenceResultsMap, notNullValue());
+        assertThat(coordinatorIntercepted.inferenceResultsMap.size(), equalTo(2));
+        assertTrue(coordinatorIntercepted.inferenceResultsMap.containsKey(DENSE_INFERENCE_ID));
+        assertTrue(coordinatorIntercepted.inferenceResultsMap.containsKey(SPARSE_INFERENCE_ID));
+
+        final SemanticQueryBuilder expectedSemanticQuery = new SemanticQueryBuilder(
+            field,
+            queryText,
+            null,
+            coordinatorIntercepted.inferenceResultsMap
+        ).boost(matchQuery.boost()).queryName(matchQuery.queryName());
+
+        // Perform data node rewrite on test index 1
+        final QueryRewriteContext indexMetadataContextTestIndex1 = createIndexMetadataContext(
+            testIndex1.name(),
+            testIndex1.semanticTextFields(),
+            testIndex1.nonInferenceFields()
+        );
+        QueryBuilder dataRewrittenTestIndex1 = rewriteAndFetch(coordinatorIntercepted, indexMetadataContextTestIndex1);
+        assertThat(dataRewrittenTestIndex1, equalTo(expectedSemanticQuery));
+
+        // Perform data node rewrite on test index 2
+        final QueryRewriteContext indexMetadataContextTestIndex2 = createIndexMetadataContext(
+            testIndex2.name(),
+            testIndex2.semanticTextFields(),
+            testIndex2.nonInferenceFields()
+        );
+        QueryBuilder dataRewrittenTestIndex2 = rewriteAndFetch(coordinatorIntercepted, indexMetadataContextTestIndex2);
+        assertThat(dataRewrittenTestIndex2, equalTo(expectedSemanticQuery));
+
+        // Perform data node rewrite on test index 3
+        final QueryRewriteContext indexMetadataContextTestIndex3 = createIndexMetadataContext(
+            testIndex3.name(),
+            testIndex3.semanticTextFields(),
+            testIndex3.nonInferenceFields()
+        );
+        QueryBuilder dataRewrittenTestIndex3 = rewriteAndFetch(coordinatorIntercepted, indexMetadataContextTestIndex3);
+        assertThat(dataRewrittenTestIndex3, equalTo(matchQuery));
     }
 }

bfa437430d15ce192567cbd10e96e2ff42b421c1
mjmbischoff <mjmbischoff@controplex.com>
2025-09-12 13:09:29 +0200

    - Added a Time-based expiration to match features of the old cache implementation. - Added housekeeping to remove in iterators - Perf optimizations: lowered visibility guarantees a bit in favor of performance, Let the sieve remove. Release other threads from sieving. Added conditionals to allow branch prediction

--- server/src/main/java/org/elasticsearch/common/cache/Cache.java
+++ server/src/main/java/org/elasticsearch/common/cache/Cache.java
@@ -8,6 +8,7 @@
  */
 package org.elasticsearch.common.cache;
 
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Objects;
@@ -17,7 +18,6 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -31,7 +31,7 @@ import static org.elasticsearch.common.cache.RemovalNotification.RemovalReason.R
  * Implementation of <a href="https://junchengyang.com/publication/nsdi24-SIEVE.pdf">SIEVE</a>
  * <p>
  * SIEVE, a simple, efficient, fast, and scalable cache eviction algorithm for web caches that leverages “lazy
- * promotion” and “quick demotion”. The high efficiency in SIEVE comes from gradually sifting out the unpopular objects.
+ * promotion” and “quick demotion.” The high efficiency in SIEVE comes from gradually sifting out the unpopular objects.
  * </p>
  * maxSize and maxWeight are soft limits and we might surge over.
  *
@@ -44,42 +44,69 @@ public class Cache<Key, Value> {
         public final Key key;
         public final Value value;
         public final AtomicBoolean visited = new AtomicBoolean(false);
+        public final long writeTime;
+        public volatile long accessTime;
 
-        EntryHolder(Key key, Value value) {
+        EntryHolder(Key key, Value value, long writeTime) {
             this.key = key;
             this.value = value;
+            this.writeTime = this.accessTime = writeTime;
         }
     }
 
     private final ConcurrentMap<Key, EntryHolder<Key, Value>> cache = new ConcurrentHashMap<>();
     private final ConcurrentLinkedDeque<EntryHolder<Key, Value>> queue = new ConcurrentLinkedDeque<>();
-    private final AtomicLong weight = new AtomicLong();
+    private final LongAdder size = new LongAdder();
+    private final LongAdder weight = new LongAdder();
     private final LongAdder hits = new LongAdder();
     private final LongAdder misses = new LongAdder();
     private final LongAdder evictions = new LongAdder();
-    private final Object sieveLock = new Object();
-    private volatile Iterator<EntryHolder<Key, Value>> sieve;
+    private final AtomicBoolean sieving = new AtomicBoolean();
     private final Long maxCapacity;
     private final Long maxWeight;
     private final ToLongBiFunction<Key, Value> weigher;
     private final RemovalListener<Key, Value> removalListener;
 
+    private volatile Iterator<EntryHolder<Key, Value>> sieve;
+    // positive if entries have an expiration
+    private final long expireAfterAccessNanos;
+    // true if entries can expire after access
+    private final boolean entriesExpireAfterAccess;
+    // positive if entries have an expiration after write
+    private final long expireAfterWriteNanos;
+    // true if entries can expire after initial insertion
+    private final boolean entriesExpireAfterWrite;
+
     public Cache() {
-        this(null,null,null,null);
+        this(null,null,null,null, -1, -1);
     }
 
     public Cache(Long maxCapacity, Long maxWeight, RemovalListener<Key, Value> removalListener, ToLongBiFunction<Key, Value> weigher) {
+        this(maxCapacity, maxWeight, removalListener, weigher, -1, -1);
+    }
+
+    public Cache(
+        Long maxCapacity,
+        Long maxWeight,
+        RemovalListener<Key, Value> removalListener,
+        ToLongBiFunction<Key, Value> weigher,
+        long expireAfterAccessNanos,
+        long expireAfterWriteNanos
+    ) {
         this.maxCapacity = maxCapacity;
         this.maxWeight = maxWeight;
         this.removalListener = removalListener != null ? removalListener : (notification) -> {} ;
-        this.weigher = weigher != null ? weigher : (key, value) -> 0;
+        this.weigher = weigher != null ? weigher : (key, value) -> 1;
+        this.expireAfterAccessNanos = expireAfterAccessNanos;
+        this.entriesExpireAfterAccess = expireAfterAccessNanos > 0;
+        this.expireAfterWriteNanos = expireAfterWriteNanos;
+        this.entriesExpireAfterWrite = expireAfterWriteNanos > 0;
     }
 
     public Value get(Key key) {
         EntryHolder<Key, Value> entry = cache.get(key);
         if(entry != null) {
-            hits.increment();
-            entry.visited.set(true);
+            markHit(entry);
             return entry.value;
         }
         misses.increment();
@@ -87,12 +114,14 @@ public class Cache<Key, Value> {
     }
 
     public void put(Key key, Value value) {
-        EntryHolder<Key, Value> newHead = new EntryHolder<>(key, value);
+        EntryHolder<Key, Value> newHead = new EntryHolder<>(key, value, now());
         EntryHolder<Key, Value> oldValue = cache.put(key, newHead);
-        weight.getAndAdd(weigher.applyAsLong(key, value));
+        size.increment();
+        weight.add(weigher.applyAsLong(key, value));
         appendToHead(newHead);
         if(oldValue!=null) {
-            weight.getAndAdd(-weigher.applyAsLong(oldValue.key, oldValue.value));
+            size.decrement();
+            weight.add(-weigher.applyAsLong(oldValue.key, oldValue.value));
             removeFromQueue(oldValue, REPLACED);
         }
         sieveUntilSpace();
@@ -100,18 +129,35 @@ public class Cache<Key, Value> {
 
     public Value computeIfAbsent(Key key, CacheLoader<Key, Value> loader) throws ExecutionException {
         Objects.requireNonNull(loader);
+        var created = new AtomicBoolean(false);
         try {
-            EntryHolder<Key, Value> entry = cache.computeIfAbsent(key, (loadKey) -> {
+            EntryHolder<Key, Value> result = cache.computeIfAbsent(key, (loadKey) -> {
                 try {
-                    return new EntryHolder<>(loadKey, loader.load(loadKey));
+                    var loadedValue = loader.load(loadKey);
+                    if(loadedValue == null) {
+                        return null;
+                    }
+                    created.set(true);
+                    var entry = new EntryHolder<>(loadKey, loadedValue, now());
+                    size.increment();
+                    weight.add(weigher.applyAsLong(entry.key, entry.value));
+                    return entry;
                 } catch (Exception e) {
                     throw new CacheLoaderException(e);
                 }
             });
-            weight.getAndAdd(weigher.applyAsLong(entry.key, entry.value));
-            appendToHead(entry);
-            sieveUntilSpace();
-            return entry.value;
+            if(created.get()) {
+                appendToHead(result);
+                sieveUntilSpace();
+                assert result != null;
+                return result.value;
+            } else {
+                if(result == null) {
+                    return null;
+                }
+                markHit(result);
+                return result.value;
+            }
         } catch (CacheLoaderException e) {
             throw new ExecutionException(e.getCause());
         }
@@ -120,16 +166,18 @@ public class Cache<Key, Value> {
     public void invalidate(Key key) {
         EntryHolder<Key, Value> removedEntry = cache.remove(key);
         if(removedEntry != null) {
-            weight.getAndAdd(-weigher.applyAsLong(removedEntry.key, removedEntry.value));
+            size.decrement();
+            weight.add(-weigher.applyAsLong(removedEntry.key, removedEntry.value));
             removeFromQueue(removedEntry, INVALIDATED);
         }
     }
 
     public void invalidate(Key key, Value value) {
         EntryHolder<Key, Value> entry = cache.get(key);
-        if(entry != null && entry.value.equals(value)) {
+        if(entry != null && Objects.equals(entry.value, value)) {
             if(cache.remove(key, entry)) {
-                weight.getAndAdd(-weigher.applyAsLong(entry.key, entry.value));
+                size.decrement();
+                weight.add(-weigher.applyAsLong(entry.key, entry.value));
                 removeFromQueue(entry, INVALIDATED);
             } else {
                 // Value already replaced before we could remove it. Invalidating is no longer necessary
@@ -144,7 +192,8 @@ public class Cache<Key, Value> {
                 break;
             }
             if(cache.remove(entry.key, entry)) {
-                weight.getAndAdd(-weigher.applyAsLong(entry.key, entry.value));
+                size.decrement();
+                weight.add(-weigher.applyAsLong(entry.key, entry.value));
                 removalListener.onRemoval(new RemovalNotification<>(entry.key, entry.value, INVALIDATED));
             }
         }
@@ -155,27 +204,27 @@ public class Cache<Key, Value> {
     }
 
     public int count() {
-        return cache.size();
+        return size.intValue();
     }
 
     public long weight() {
-        return weight.get();
+        return weight.sum();
     }
 
     public Iterable<Key> keys() {
-        return cache.keySet();
+        return new BookkeepingIterable<Key>(cache.values()) {
+            @Override
+            protected Key map(EntryHolder<Key, Value> holder) {
+                return holder.key;
+            }
+        };
     }
 
     public Iterable<Value> values() {
-        return new Iterable<>() {
+        return new BookkeepingIterable<Value>(cache.values()) {
             @Override
-            public Iterator<Value> iterator() {
-                return new UnwrappingIterator(cache.values().iterator());
-            }
-
-            @Override
-            public Spliterator<Value> spliterator() {
-                return new UnwrappingSpliterator(cache.values().spliterator());
+            protected Value map(EntryHolder<Key, Value> holder) {
+                return holder.value;
             }
         };
     }
@@ -185,17 +234,19 @@ public class Cache<Key, Value> {
     }
 
     public void forEach(BiConsumer<Key, Value> consumer) {
-        cache.forEach((key, entry) -> {
-            consumer.accept(key, entry.value);
-        });
+        cache.forEach((key, entry) -> consumer.accept(key, entry.value));
     }
 
     private void sieveUntilSpace() {
         if(hasSpace()) {
             return;
         }
-        synchronized (sieveLock) {
-            while (hasSpace()==false) {
+        if (sieving.compareAndSet(false, true) == false) {
+            // Another thread is already sieving; avoid contention.
+            return;
+        }
+        try {
+            while (hasSpace() == false) {
                 if (sieve == null || sieve.hasNext() == false) {
                     if(queue.isEmpty()) {
                         return; // protect against queue.clear() etc.
@@ -203,16 +254,20 @@ public class Cache<Key, Value> {
                     sieve = queue.descendingIterator();
                 }
                 EntryHolder<Key, Value> entry = sieve.next();
-                if(entry.visited.get() == false) {
+                if(isExpired(entry, now()) || entry.visited.getAndSet(false) == false) {
                     if(cache.remove(entry.key, entry)) {
-                        weight.getAndAdd(-weigher.applyAsLong(entry.key, entry.value));
+                        size.decrement();
+                        weight.add(-weigher.applyAsLong(entry.key, entry.value));
+                        removalListener.onRemoval(new RemovalNotification<>(entry.key, entry.value, EVICTED));
+                        evictions.increment();
                     }
                     sieve.remove();
-                    removalListener.onRemoval(new RemovalNotification<>(entry.key, entry.value, EVICTED));
-                    evictions.increment();
                 }
             }
+        } finally {
+            sieving.set(false);
         }
+
     }
 
     private boolean hasSpace() {
@@ -223,13 +278,36 @@ public class Cache<Key, Value> {
         queue.addFirst(newHead);
     }
 
-    /**
-     * We should hold that regardless of which entry, we should be able to transverse to the head.
-     * @param entry currently linked entry
-     */
     private void removeFromQueue(EntryHolder<Key, Value> entry, RemovalNotification.RemovalReason reason) {
-        queue.remove(entry);
+        //queue.remove(entry); we're setting the flag to false instead and let the sieve remove it at O(1)
+        entry.visited.lazySet(false);
         removalListener.onRemoval(new RemovalNotification<>(entry.key, entry.value, reason));
+    }
+
+    private void markHit(EntryHolder<Key, Value> result) {
+        hits.increment();
+        if (result.visited.get() == false) {
+            result.visited.lazySet(true);
+        }
+        if(entriesExpireAfterAccess) {
+            result.accessTime = now();
+        }
+    }
+
+    private boolean isExpired(EntryHolder<Key, Value> entry, long now) {
+        return (entriesExpireAfterAccess && now - entry.accessTime > expireAfterAccessNanos)
+            || (entriesExpireAfterWrite && now - entry.writeTime > expireAfterWriteNanos);
+    }
+
+    /**
+     * The relative time used to track time-based evictions.
+     *
+     * @return the current relative time
+     */
+    protected long now() {
+        // System.nanoTime takes non-negligible time, so we only use it if we need it
+        // use System.nanoTime because we want relative time, not absolute time
+        return entriesExpireAfterAccess || entriesExpireAfterWrite ? System.nanoTime() : 0;
     }
 
     private static class CacheLoaderException extends RuntimeException {
@@ -238,25 +316,66 @@ public class Cache<Key, Value> {
         }
     }
 
-    private class UnwrappingSpliterator implements Spliterator<Value> {
+    private abstract class BookkeepingIterable<Type> implements Iterable<Type> {
+        private final Collection<EntryHolder<Key, Value>> entries;
+
+        private BookkeepingIterable(Collection<EntryHolder<Key, Value>> entries) {
+            this.entries = entries;
+        }
+
+        @Override
+        public BookkeepingIterator<Type> iterator() {
+            return new BookkeepingIterator<Type>(entries.iterator()) {
+                @Override
+                protected Type map(EntryHolder<Key, Value> holder) {
+                    return BookkeepingIterable.this.map(holder);
+                }
+            };
+        }
+
+        @Override
+        public Spliterator<Type> spliterator() {
+            return new BookkeepingSpliterator<Type>(entries.spliterator()) {
+                @Override
+                protected Type map(EntryHolder<Key, Value> holder) {
+                    return BookkeepingIterable.this.map(holder);
+                }
+            };
+        }
+
+        protected abstract Type map(EntryHolder<Key, Value> holder);
+    }
+
+    private abstract class BookkeepingSpliterator<Type> implements Spliterator<Type> {
         private final Spliterator<EntryHolder<Key, Value>> spliterator;
 
-        UnwrappingSpliterator(Spliterator<EntryHolder<Key, Value>> spliterator) {
+        BookkeepingSpliterator(Spliterator<EntryHolder<Key, Value>> spliterator) {
             Objects.requireNonNull(spliterator);
             if(spliterator.hasCharacteristics(ORDERED) || spliterator.hasCharacteristics(SORTED)){
-                throw new UnsupportedOperationException("Because we erase context(key), #getComparator() can't be implemented in any efficient way.");
+                throw new UnsupportedOperationException(
+                    "Because we erase context(key), #getComparator() can't be implemented in any efficient way."
+                );
             }
             this.spliterator = spliterator;
         }
 
         @Override
-        public boolean tryAdvance(Consumer<? super Value> action) {
-            return spliterator.tryAdvance(entry -> action.accept(entry.value));
+        public boolean tryAdvance(Consumer<? super Type> action) {
+            return spliterator.tryAdvance(entry -> action.accept(map(entry)));
         }
 
         @Override
-        public Spliterator<Value> trySplit() {
-            return new UnwrappingSpliterator(spliterator.trySplit());
+        public Spliterator<Type> trySplit() {
+            var split = spliterator.trySplit();
+            if(split == null) {
+                return null;
+            }
+            return new BookkeepingSpliterator<Type>(split) {
+                @Override
+                protected Type map(EntryHolder<Key, Value> entry) {
+                    return BookkeepingSpliterator.this.map(entry);
+                }
+            };
         }
 
         @Override
@@ -270,12 +389,14 @@ public class Cache<Key, Value> {
         }
 
         @Override
-        public void forEachRemaining(Consumer<? super Value> action) {
-            spliterator.forEachRemaining((entry) -> action.accept(entry.value));
+        public void forEachRemaining(Consumer<? super Type> action) {
+            spliterator.forEachRemaining((entry) -> action.accept(map(entry)));
         }
 
+        protected abstract Type map(EntryHolder<Key, Value> entry);
+
         @Override
-        public Comparator<? super Value> getComparator() {
+        public Comparator<? super Type> getComparator() {
             throw new IllegalStateException();
         }
 
@@ -290,12 +411,15 @@ public class Cache<Key, Value> {
         }
     }
 
-    private class UnwrappingIterator implements Iterator<Value> {
+    private abstract class BookkeepingIterator<Type> implements Iterator<Type> {
         private final Iterator<EntryHolder<Key, Value>> iterator;
+        private EntryHolder<Key, Value> last;
 
-        UnwrappingIterator(Iterator<EntryHolder<Key, Value>> iterator) {
+        BookkeepingIterator(Iterator<EntryHolder<Key, Value>> iterator) {
             this.iterator = iterator;
         }
+
+        protected abstract Type map(EntryHolder<Key, Value> holder);
 
         @Override
         public boolean hasNext() {
@@ -303,22 +427,33 @@ public class Cache<Key, Value> {
         }
 
         @Override
-        public Value next() {
-            EntryHolder<Key, Value> entry = iterator.next();
-            if(entry == null) {
-                return null;
-            }
-            return entry.value;
+        public Type next() {
+            last = iterator.next();
+            return map(last);
         }
 
         @Override
         public void remove() {
-            iterator.remove();
+            if (last == null) {
+                throw new IllegalStateException();
+            }
+            if (cache.remove(last.key, last)) {
+                size.decrement();
+                weight.add(-weigher.applyAsLong(last.key, last.value));
+                queue.remove(last);
+                removalListener.onRemoval(new RemovalNotification<>(last.key, last.value, INVALIDATED));
+            } else {
+                queue.remove(last);
+            }
+            last = null;
         }
 
         @Override
-        public void forEachRemaining(Consumer<? super Value> action) {
-            iterator.forEachRemaining((entry) -> action.accept(entry.value));
+        public void forEachRemaining(Consumer<? super Type> action) {
+            iterator.forEachRemaining(holder -> {
+                last = holder;
+                action.accept(map(holder));
+            });
         }
     }
 

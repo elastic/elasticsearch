6961893c304064d7cebf55e1ed44dfc0854a01d5
Pablo <pabloem@apache.org>
2025-09-12 20:57:30 -0700

    fixup

--- x-pack/plugin/esql/compute/src/main/generated-src/org/elasticsearch/compute/aggregation/RateDoubleGroupingAggregatorFunction.java
+++ x-pack/plugin/esql/compute/src/main/generated-src/org/elasticsearch/compute/aggregation/RateDoubleGroupingAggregatorFunction.java
@@ -705,10 +705,9 @@ public final class RateDoubleGroupingAggregatorFunction implements GroupingAggre
             lastValue = lastValue + endGap * slope;
         }
         if (isRateOverTime) {
-            // when this is rate_over_time, we want to divide by the total range, not the sample range
-            return lastValue - firstValue;
-        } else {
             return (lastValue - firstValue) * 1000.0 / (rangeEnd - rangeStart);
+        } else {
+            return lastValue - firstValue;
         }
     }
 

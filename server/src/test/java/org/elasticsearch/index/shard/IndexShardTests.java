27ec57cd23b5efd121637ce59b8a6e64dd04a424
Martijn van Groningen <martijn.v.groningen@gmail.com>
2025-09-17 16:00:44 +0200

    fix test by moving to its suite and overwrite node settings

--- server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
+++ server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
@@ -1981,66 +1981,6 @@ public class IndexShardTests extends IndexShardTestCase {
         closeShards(shard);
     }
 
-    public void testShardFieldStatsWithDeletes() throws IOException {
-        Settings settings = Settings.builder()
-            .put(MergePolicyConfig.INDEX_MERGE_ENABLED, false)
-            .put(IndexSettings.INDEX_REFRESH_INTERVAL_SETTING.getKey(), TimeValue.MINUS_ONE)
-            .build();
-        IndexShard shard = newShard(true, settings);
-        assertNull(shard.getShardFieldStats());
-        recoverShardFromStore(shard);
-
-        // index some documents
-        int numDocs = 10;
-        for (int i = 0; i < numDocs; i++) {
-            indexDoc(shard, "_doc", "first_" + i, """
-                {
-                    "f1": "foo",
-                    "f2": "bar"
-                }
-                """);
-        }
-        shard.refresh("test");
-        var stats = shard.getShardFieldStats();
-        assertThat(stats.numSegments(), equalTo(1));
-        assertThat(stats.liveDocsBytes(), equalTo(0L));
-
-        // delete a doc
-        deleteDoc(shard, "first_0");
-
-        // Refresh and fetch new stats:
-        shard.refresh("test");
-        stats = shard.getShardFieldStats();
-        // More segments because delete operation is stored in the new segment for replication purposes.
-        assertThat(stats.numSegments(), equalTo(2));
-        long expectedLiveDocsSize = 0;
-        // Delete op is stored in new segment, but marked as deleted. All segements have live docs:
-        expectedLiveDocsSize += new FixedBitSet(numDocs).ramBytesUsed();
-        // Second segment the delete operation that is marked as deleted:
-        expectedLiveDocsSize += new FixedBitSet(1).ramBytesUsed();
-        assertThat(stats.liveDocsBytes(), equalTo(expectedLiveDocsSize));
-
-        // delete another doc:
-        deleteDoc(shard, "first_1");
-        shard.getMinRetainedSeqNo();
-
-        // Refresh and fetch new stats:
-        shard.refresh("test");
-        stats = shard.getShardFieldStats();
-        // More segments because delete operation is stored in the new segment for replication purposes.
-        assertThat(stats.numSegments(), equalTo(3));
-        expectedLiveDocsSize = 0;
-        // Delete op is stored in new segment, but marked as deleted. All segements have live docs:
-        // First segment with deletes
-        expectedLiveDocsSize += new FixedBitSet(numDocs).ramBytesUsed();
-        // Second and third segments the delete operation that is marked as deleted:
-        expectedLiveDocsSize += new FixedBitSet(1).ramBytesUsed();
-        expectedLiveDocsSize += new FixedBitSet(1).ramBytesUsed();
-        assertThat(stats.liveDocsBytes(), equalTo(expectedLiveDocsSize));
-
-        closeShards(shard);
-    }
-
     public void testIndexingOperationsListeners() throws IOException {
         IndexShard shard = newStartedShard(true);
         indexDoc(shard, "_doc", "0", "{\"foo\" : \"bar\"}");

3b75e6ca138426ece05cb19ff908dbcc75e952b3
Dmitry Kubikov <dmitry.kubikov@elastic.co>
2025-09-18 15:26:57 -0700

    Fixes

--- x-pack/plugin/logsdb/src/main/java/org/elasticsearch/xpack/logsdb/patterntext/PatternTextFieldType.java
+++ x-pack/plugin/logsdb/src/main/java/org/elasticsearch/xpack/logsdb/patterntext/PatternTextFieldType.java
@@ -75,7 +75,7 @@ public class PatternTextFieldType extends TextFamilyFieldType {
         boolean isSyntheticSource,
         boolean isWithinMultiField
     ) {
-        // Though this type is based on doc_values, hasDocValues is set to false as the patterned_text type is not aggregatable.
+        // Though this type is based on doc_values, hasDocValues is set to false as the pattern_text type is not aggregatable.
         // This does not stop its child .template type from being aggregatable.
         super(name, true, false, false, tsi, meta, isSyntheticSource, isWithinMultiField);
         this.indexAnalyzer = Objects.requireNonNull(indexAnalyzer);

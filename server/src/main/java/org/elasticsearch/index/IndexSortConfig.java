78ec913e63652105e1f3ed0e90e125f16a623710
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-09-18 03:27:37 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/index/IndexSortConfig.java
+++ server/src/main/java/org/elasticsearch/index/IndexSortConfig.java
@@ -27,7 +27,6 @@ import org.elasticsearch.search.lookup.SearchLookup;
 import org.elasticsearch.search.sort.SortOrder;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Objects;

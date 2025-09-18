a78a2c8dd274703f0e87477ba4e12497b06bf319
Stanislav Malyshev <stas.malyshev@elastic.co>
2025-09-18 14:46:40 -0600

    fix multiple topNs

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/rules/logical/HoistRemoteEnrichTopN.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/rules/logical/HoistRemoteEnrichTopN.java
@@ -40,34 +40,40 @@ public final class HoistRemoteEnrichTopN extends OptimizerRules.ParameterizedOpt
         if (en.mode() == Enrich.Mode.REMOTE) {
             LogicalPlan plan = en.child();
             while (true) {
-                if (plan instanceof TopN top && top.isLocal() == false) {
-                    // Create a fake OrderBy and "push" Enrich through it to generate aliases
-                    Enrich topWithEnrich = (Enrich) en.replaceChild(new OrderBy(top.source(), en.child(), top.order()));
-                    LogicalPlan pushPlan = PushDownUtils.pushGeneratingPlanPastProjectAndOrderBy(topWithEnrich);
-                    // If we needed to alias any names, the result would look like this:
-                    // Project[[host{f}#14, timestamp{f}#16, user{f}#15, ip{r}#19, os{r}#20]]
-                    // \_OrderBy[[Order[timestamp{f}#16,ASC,LAST], Order[user{f}#15,ASC,LAST], Order[$$ip$temp_name$21{r$}#22,ASC,LAST]]]
-                    // \_Enrich[REMOTE,hosts[KEYWORD],ip{r}#3,{"match":{"indices":[],"match_field":"ip","enrich_fields":["ip","os"]}},{},
-                    // [ip{r}#19,os{r}#20]]
-                    // \_Eval[[ip{r}#3 AS $$ip$temp_name$21#22]]
-                    if (pushPlan instanceof Project proj) {
-                        // We needed renaming - deconstruct the plan from above and extract the relevant parts
-                        assert proj.child() instanceof OrderBy o && o.child() instanceof Enrich e && e.child() instanceof Eval
-                            : "Unexpected pushed plan structure: " + pushPlan;
-                        OrderBy order = (OrderBy) proj.child();
-                        Enrich enrich = (Enrich) order.child();
-                        Eval eval = (Eval) enrich.child();
-                        // We insert the evals above the original TopN, so that the copy TopN works on the renamed fields
-                        LogicalPlan replacementTop = eval.replaceChild(top.withLocal(true));
-                        LogicalPlan transformedEnrich = en.transformDown(TopN.class, t -> t == top ? replacementTop : t);
-                        // Create the copied topN on top of the Enrich
-                        var copyTop = new TopN(top.source(), transformedEnrich, order.order(), top.limit(), false);
-                        // And use the project to remove the fields that we don't need anymore
-                        return proj.replaceChild(copyTop);
+                if (plan instanceof TopN top) {
+                    if (top.isLocal()) {
+                        plan = top.child();
+                        continue;
                     } else {
-                        // No need for aliasing - then it's simple, just copy the TopN on top and mark the original as local
-                        LogicalPlan transformedEnrich = en.transformDown(TopN.class, t -> t == top ? top.withLocal(true) : t);
-                        return new TopN(top.source(), transformedEnrich, top.order(), top.limit());
+                        // Create a fake OrderBy and "push" Enrich through it to generate aliases
+                        Enrich topWithEnrich = (Enrich) en.replaceChild(new OrderBy(top.source(), en.child(), top.order()));
+                        LogicalPlan pushPlan = PushDownUtils.pushGeneratingPlanPastProjectAndOrderBy(topWithEnrich);
+                        // If we needed to alias any names, the result would look like this:
+                        // Project[[host{f}#14, timestamp{f}#16, user{f}#15, ip{r}#19, os{r}#20]]
+                        // \_OrderBy[[Order[timestamp{f}#16,ASC,LAST], Order[user{f}#15,ASC,LAST],
+                        // Order[$$ip$temp_name$21{r$}#22,ASC,LAST]]]
+                        // \_Enrich[REMOTE,hosts[KEYWORD],ip{r}#3,{"match":{"indices":[],"match_field":"ip",
+                        // "enrich_fields":["ip","os"]}},{},[ip{r}#19,os{r}#20]]
+                        // \_Eval[[ip{r}#3 AS $$ip$temp_name$21#22]]
+                        if (pushPlan instanceof Project proj) {
+                            // We needed renaming - deconstruct the plan from above and extract the relevant parts
+                            assert proj.child() instanceof OrderBy o && o.child() instanceof Enrich e && e.child() instanceof Eval
+                                : "Unexpected pushed plan structure: " + pushPlan;
+                            OrderBy order = (OrderBy) proj.child();
+                            Enrich enrich = (Enrich) order.child();
+                            Eval eval = (Eval) enrich.child();
+                            // We insert the evals above the original TopN, so that the copy TopN works on the renamed fields
+                            LogicalPlan replacementTop = eval.replaceChild(top.withLocal(true));
+                            LogicalPlan transformedEnrich = en.transformDown(TopN.class, t -> t == top ? replacementTop : t);
+                            // Create the copied topN on top of the Enrich
+                            var copyTop = new TopN(top.source(), transformedEnrich, order.order(), top.limit(), false);
+                            // And use the project to remove the fields that we don't need anymore
+                            return proj.replaceChild(copyTop);
+                        } else {
+                            // No need for aliasing - then it's simple, just copy the TopN on top and mark the original as local
+                            LogicalPlan transformedEnrich = en.transformDown(TopN.class, t -> t == top ? top.withLocal(true) : t);
+                            return new TopN(top.source(), transformedEnrich, top.order(), top.limit());
+                        }
                     }
                 }
                 if ((plan instanceof ExecutesOn ex && ex.executesOn() == ExecutesOn.ExecuteLocation.COORDINATOR)

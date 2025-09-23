4d57882339fe30fbea2c426458dcbb8d7d4aadb1
ChrisHegarty <chegar999@gmail.com>
2025-09-23 11:47:48 +0100

    Optimize dotCount in expanding dot parser

--- server/src/test/java/org/elasticsearch/index/mapper/FieldTypeLookupTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/FieldTypeLookupTests.java
@@ -522,6 +522,31 @@ public class FieldTypeLookupTests extends ESTestCase {
         assertEquals(foo.fieldType(), lookup.get("foo"));
     }
 
+    public void testDotCount() {
+        assertEquals(0, FieldTypeLookup.dotCount(""));
+        assertEquals(1, FieldTypeLookup.dotCount("."));
+        assertEquals(2, FieldTypeLookup.dotCount(".."));
+        assertEquals(3, FieldTypeLookup.dotCount("..."));
+        assertEquals(4, FieldTypeLookup.dotCount("...."));
+        assertEquals(0, FieldTypeLookup.dotCount("foo"));
+        assertEquals(1, FieldTypeLookup.dotCount("foo.bar"));
+        assertEquals(2, FieldTypeLookup.dotCount("foo.bar.baz"));
+        assertEquals(3, FieldTypeLookup.dotCount("foo.bar.baz.bob"));
+        assertEquals(4, FieldTypeLookup.dotCount("foo.bar.baz.bob."));
+        assertEquals(4, FieldTypeLookup.dotCount("foo..bar.baz.bob"));
+        assertEquals(5, FieldTypeLookup.dotCount("foo..bar..baz.bob"));
+        assertEquals(6, FieldTypeLookup.dotCount("foo..bar..baz.bob."));
+
+        int times = atLeast(50);
+        for (int i = 0; i < times; i++) {
+            byte[] bytes = new byte[randomInt(1024)];
+            random().nextBytes(bytes);
+            String s = new String(bytes);
+            int expected = s.chars().map(c -> c == '.' ? 1 : 0).sum();
+            assertEquals(expected, FieldTypeLookup.dotCount(s));
+        }
+    }
+
     @SafeVarargs
     @SuppressWarnings("varargs")
     static <T> List<T> randomizedList(T... values) {

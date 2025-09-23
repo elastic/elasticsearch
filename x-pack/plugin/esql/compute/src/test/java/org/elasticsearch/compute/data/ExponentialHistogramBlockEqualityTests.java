283da4ddeaf3bac8d1def2b6855ae1d30551444b
Jonas Kunz <jonas.kunz@elastic.co>
2025-09-23 10:30:03 +0200

    Add equality test with random blocks.

--- x-pack/plugin/esql/compute/src/test/java/org/elasticsearch/compute/data/ExponentialHistogramBlockEqualityTests.java
+++ x-pack/plugin/esql/compute/src/test/java/org/elasticsearch/compute/data/ExponentialHistogramBlockEqualityTests.java
@@ -8,6 +8,7 @@
 package org.elasticsearch.compute.data;
 
 import org.elasticsearch.compute.test.ComputeTestCase;
+import org.elasticsearch.compute.test.RandomBlock;
 import org.elasticsearch.core.Releasables;
 import org.elasticsearch.exponentialhistogram.ExponentialHistogram;
 import org.elasticsearch.exponentialhistogram.ExponentialHistogramCircuitBreaker;
@@ -49,10 +50,9 @@ public class ExponentialHistogramBlockEqualityTests extends ComputeTestCase {
         Releasables.close(blocks);
     }
 
-    public void testPopulatedBlockEquality() {
+    public void testFilteredBlockEquality() {
         ExponentialHistogram histo1 = ExponentialHistogram.create(2, ExponentialHistogramCircuitBreaker.noop(), 1, 2, 3, 4);
         ExponentialHistogram histo2 = ExponentialHistogram.empty();
-
         Block block1 = blockFactory().newExponentialHistogramBlockBuilder(0).append(histo1).append(histo1).append(histo2).build();
 
         Block block2 = blockFactory().newExponentialHistogramBlockBuilder(0).append(histo1).append(histo2).append(histo2).build();
@@ -71,6 +71,18 @@ public class ExponentialHistogramBlockEqualityTests extends ComputeTestCase {
         assertThat(block1Filtered.hashCode(), equalTo(block2Filtered.hashCode()));
 
         Releasables.close(block1, block2, block1Filtered, block2Filtered);
+    }
+
+    public void testRandomBlockEquality() {
+        int positionCount = randomIntBetween(0, 10_000);
+        Block expHistoBlock = RandomBlock.randomBlock(blockFactory(), ElementType.EXPONENTIAL_HISTOGRAM, positionCount, true, 1, 10, 0, 10)
+            .block();
+        Block copy = BlockUtils.deepCopyOf(expHistoBlock, blockFactory());
+
+        assertThat(expHistoBlock, equalTo(copy));
+        assertThat(expHistoBlock.hashCode(), equalTo(copy.hashCode()));
+
+        Releasables.close(expHistoBlock, copy);
     }
 
     private static Block filterAndRelease(Block toFilterAndRelease) {

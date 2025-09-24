ec179e14cd428c99c5c5327a0fcf9c236055e7f0
Rene Groeschke <rene@elastic.co>
2025-09-24 22:21:32 +0200

    [Build] update eclipse formatter used by spotless
    We cannot directly declare the ecl and jdt core dependency but we can tweak the used
    dependency by spotless in our precommit plugin

--- build-conventions/src/main/java/org/elasticsearch/gradle/internal/conventions/precommit/FormattingPrecommitPlugin.java
+++ build-conventions/src/main/java/org/elasticsearch/gradle/internal/conventions/precommit/FormattingPrecommitPlugin.java
@@ -15,6 +15,7 @@ import com.diffplug.gradle.spotless.SpotlessPlugin;
 import org.elasticsearch.gradle.internal.conventions.util.Util;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.internal.artifacts.dependencies.DefaultDependencyConstraint;
 
 import java.io.File;
 
@@ -52,6 +53,13 @@ public class FormattingPrecommitPlugin implements Plugin<Project> {
             // Spotless resolves required dependencies from project repositories, so we need maven central
             project.getRepositories().mavenCentral();
 
+            // we cannot update to a newer spotless plugin version yet but we want to use the latest eclipse formatter to be compatible
+            // with latest java versions
+            project.getConfigurations().matching(it -> it.getName().startsWith("spotless")).configureEach(conf -> {
+                conf.setVisible(false);
+                conf.getDependencyConstraints().add(new DefaultDependencyConstraint("org.eclipse.jdt", "org.eclipse.jdt.core", "3.42.0"));
+                conf.getDependencyConstraints().add(new DefaultDependencyConstraint("org.eclipse.jdt", "ecj", "3.42.0"));
+            });
             project.getExtensions().getByType(SpotlessExtension.class).java(java -> {
                 File elasticsearchWorkspace = Util.locateElasticsearchWorkspace(project.getGradle());
                 String importOrderPath = "build-conventions/elastic.importorder";

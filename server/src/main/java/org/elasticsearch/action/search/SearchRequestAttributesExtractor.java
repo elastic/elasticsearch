408d7aed0679d93f9f9467d20f472e7b1c034bc2
Chris Parrinello <chris.parrinello@elastic.co>
2025-09-24 14:36:23 -0500

    backout instanceof check for CI tests

--- server/src/main/java/org/elasticsearch/action/search/SearchRequestAttributesExtractor.java
+++ server/src/main/java/org/elasticsearch/action/search/SearchRequestAttributesExtractor.java
@@ -62,12 +62,7 @@ public final class SearchRequestAttributesExtractor {
             shardSearchRequest.shardId().getIndexName()
         );
 
-        boolean isSystem;
-        if (Thread.currentThread() instanceof EsExecutors.EsThread) {
-            isSystem = ((EsExecutors.EsThread) Thread.currentThread()).isSystem();
-        } else {
-            isSystem = false;
-        }
+        boolean isSystem = ((EsExecutors.EsThread) Thread.currentThread()).isSystem();
 
         attributes.put(SYSTEM_THREAD_ATTRIBUTE_NAME, isSystem);
         return attributes;

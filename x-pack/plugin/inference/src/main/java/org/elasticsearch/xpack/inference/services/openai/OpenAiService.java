1db5f0d512db4cabe8c71bc72ebbac36449eb80d
Donal Evans <donal.evans@elastic.co>
2025-09-25 13:17:31 -0700

    [9.1] [ML] Restore delayed string copying (#135242) (#135464)
    This commit restores the behaviour introduced in #125837 which was
    inadvertently undone by changes in #121041, specifically, delaying
    copying Strings as part of calling Request.chunkText() until the request
    is being executed.
    
    In addition to the above change, refactor doChunkedInfer() and its
    implementations to take a List<ChunkInferenceInput> rather than
    EmbeddingsInput, since the EmbeddingsInput passed into doChunkedInfer()
    was immediately discarded after extracting the ChunkInferenceInput list
    from it. This change allowed the EmbeddingsInput class to be refactored
    to not know about ChunkInferenceInput, simplifying it significantly.
    
    This commit also simplifies EmbeddingRequestChunker.Request to take only
    the input String rather than the entire list of all inputs, since only
    one input is actually needed. This change prevents Requests from
    retaining a reference to the input list, potentially allowing it to be
    GC'd faster.
    
    (cherry picked from commit f3447d3d45d5caa96f2e3b3f6a124019b96178e6)

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/openai/OpenAiService.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/openai/OpenAiService.java
@@ -15,6 +15,7 @@ import org.elasticsearch.common.ValidationException;
 import org.elasticsearch.common.util.LazyInitializable;
 import org.elasticsearch.core.Nullable;
 import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.inference.ChunkInferenceInput;
 import org.elasticsearch.inference.ChunkedInference;
 import org.elasticsearch.inference.ChunkingSettings;
 import org.elasticsearch.inference.InferenceServiceConfiguration;
@@ -325,7 +326,7 @@ public class OpenAiService extends SenderService {
     @Override
     protected void doChunkedInfer(
         Model model,
-        EmbeddingsInput inputs,
+        List<ChunkInferenceInput> inputs,
         Map<String, Object> taskSettings,
         InputType inputType,
         TimeValue timeout,
@@ -340,14 +341,14 @@ public class OpenAiService extends SenderService {
         var actionCreator = new OpenAiActionCreator(getSender(), getServiceComponents());
 
         List<EmbeddingRequestChunker.BatchRequestAndListener> batchedRequests = new EmbeddingRequestChunker<>(
-            inputs.getInputs(),
+            inputs,
             EMBEDDING_MAX_BATCH_SIZE,
             openAiModel.getConfigurations().getChunkingSettings()
         ).batchRequestsWithListeners(listener);
 
         for (var request : batchedRequests) {
             var action = openAiModel.accept(actionCreator, taskSettings);
-            action.execute(EmbeddingsInput.fromStrings(request.batch().inputs().get(), inputType), timeout, request.listener());
+            action.execute(new EmbeddingsInput(request.batch().inputs(), inputType), timeout, request.listener());
         }
     }
 

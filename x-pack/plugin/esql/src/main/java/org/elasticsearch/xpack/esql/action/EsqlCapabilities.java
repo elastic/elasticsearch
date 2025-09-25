0025f2c9623941d58874015bd186beb9f7acc7cb
Ioana Tagirta <ioana.tagirta@elastic.co>
2025-09-25 21:41:48 +0200

    Bump ES|QL capability

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1368,7 +1368,7 @@ public class EsqlCapabilities {
         /**
          * FUSE command
          */
-        FUSE_V5(Build.current().isSnapshot()),
+        FUSE_V6(Build.current().isSnapshot()),
 
         /**
          * Support improved behavior for LIKE operator when used with index fields.

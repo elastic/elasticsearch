78a85db985c40b74d15cd7edd034b9fc2668ca5a
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-09-25 16:03:23 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/esql/qa/server/src/main/java/org/elasticsearch/xpack/esql/qa/rest/AllSupportedFieldsTestCase.java
+++ x-pack/plugin/esql/qa/server/src/main/java/org/elasticsearch/xpack/esql/qa/rest/AllSupportedFieldsTestCase.java
@@ -31,7 +31,6 @@ import org.junit.Rule;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;

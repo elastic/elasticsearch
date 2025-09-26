0fe6504edc6101857c23ada90ae98e612b6ab344
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2025-09-26 10:17:02 +0200

    Module info

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -55,8 +55,7 @@ module org.elasticsearch.server {
     requires org.apache.lucene.queryparser;
     requires org.apache.lucene.sandbox;
     requires org.apache.lucene.suggest;
-    requires org.jruby.jcodings;
-
+    
     exports org.elasticsearch;
     exports org.elasticsearch.action;
     exports org.elasticsearch.action.admin.cluster.allocation;

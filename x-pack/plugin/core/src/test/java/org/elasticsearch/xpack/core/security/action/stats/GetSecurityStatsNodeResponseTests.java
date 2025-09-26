8459878f30ef99463d24444ba8b50739fb81e39d
Szymon Bialkowski <szybia@tuta.io>
2025-09-26 11:17:35 +0100

    changelog and no birthday problems (no one wants those)

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/action/stats/GetSecurityStatsNodeResponseTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/action/stats/GetSecurityStatsNodeResponseTests.java
@@ -13,6 +13,7 @@ import org.elasticsearch.test.AbstractWireSerializingTestCase;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
 
 public class GetSecurityStatsNodeResponseTests extends AbstractWireSerializingTestCase<GetSecurityStatsNodeResponse> {
 
@@ -24,16 +25,24 @@ public class GetSecurityStatsNodeResponseTests extends AbstractWireSerializingTe
     @Override
     protected GetSecurityStatsNodeResponse createTestInstance() {
         return new GetSecurityStatsNodeResponse(
-            DiscoveryNodeUtils.create(randomUUID()),
-            randomBoolean() ? null : Map.of("key", randomUUID())
+            DiscoveryNodeUtils.builder(randomAlphaOfLength(10)).ephemeralId(randomAlphanumericOfLength(10)).build(),
+            randomBoolean() ? null : Map.of("key", randomAlphaOfLength(5))
         );
     }
 
     @Override
     protected GetSecurityStatsNodeResponse mutateInstance(GetSecurityStatsNodeResponse instance) throws IOException {
+        final var node = instance.getDiscoveryNode();
+        final var value = Objects.requireNonNullElse(instance.getRolesStoreStats(), Map.of()).get("key");
         return switch (randomIntBetween(0, 1)) {
-            case 0 -> new GetSecurityStatsNodeResponse(DiscoveryNodeUtils.create(randomUUID()), instance.getRolesStoreStats());
-            case 1 -> new GetSecurityStatsNodeResponse(instance.getDiscoveryNode(), Map.of("key", randomUUID()));
+            case 0 -> new GetSecurityStatsNodeResponse(
+                DiscoveryNodeUtils.builder(randomValueOtherThan(node.getId(), () -> randomAlphaOfLength(10)))
+                    // DiscoverNode#hashCode only tests ephemeralId, so make sure to change it too
+                    .ephemeralId(randomValueOtherThan(node.getEphemeralId(), () -> randomAlphanumericOfLength(10)))
+                    .build(),
+                instance.getRolesStoreStats()
+            );
+            case 1 -> new GetSecurityStatsNodeResponse(node, Map.of("key", randomValueOtherThan(value, () -> randomAlphaOfLength(5))));
             default -> throw new IllegalStateException("Unexpected value");
         };
     }

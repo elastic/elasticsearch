dcd3169c6762fb782725115b0be649c506a7d6db
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-09-26 08:27:10 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -55,6 +55,7 @@ module org.elasticsearch.server {
     requires org.apache.lucene.queryparser;
     requires org.apache.lucene.sandbox;
     requires org.apache.lucene.suggest;
+
     exports org.elasticsearch;
     exports org.elasticsearch.action;
     exports org.elasticsearch.action.admin.cluster.allocation;

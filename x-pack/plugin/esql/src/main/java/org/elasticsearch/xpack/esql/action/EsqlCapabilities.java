d694176a06ac405b0f11a7b150a4672b2be3a89c
Nik Everett <nik9000@gmail.com>
2025-09-26 09:05:35 -0400

    spotless

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1554,7 +1554,7 @@ public class EsqlCapabilities {
         INLINE_STATS_FIX_OPTIMIZED_AS_LOCAL_RELATION(INLINESTATS_V11.enabled),
 
         DENSE_VECTOR_AGG_METRIC_DOUBLE_IF_FNS
-        
+
         ;
 
         private final boolean enabled;

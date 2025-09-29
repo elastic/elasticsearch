fa82411ade2eeeed2af8819101ba70786ea195f1
Joshua Adams <joshua.adams@elastic.co>
2025-09-29 12:21:36 +0100

    Extend skip and seek tests

--- server/src/test/java/org/elasticsearch/common/io/stream/BytesStreamOutputTests.java
+++ server/src/test/java/org/elasticsearch/common/io/stream/BytesStreamOutputTests.java
@@ -80,6 +80,7 @@ public class BytesStreamOutputTests extends ESTestCase {
     public void testResetShrinksAndResetsCount() {
         int byteArrayLength = randomIntBetween(0, 100);
         stream.writeBytes(randomByteArrayOfLength(byteArrayLength), 0, byteArrayLength);
+        assertEquals(byteArrayLength, stream.size());
         stream.reset();
         assertEquals(0, stream.size());
         // After reset, should be able to write again
@@ -87,6 +88,72 @@ public class BytesStreamOutputTests extends ESTestCase {
         stream.writeByte(b);
         assertEquals(1, stream.size());
         assertEquals(b, stream.bytes().get(0));
+    }
+
+    public void testSeek() {
+        // Do some writing
+        BytesStreamOutput s = new BytesStreamOutput(0, BigArrays.NON_RECYCLING_INSTANCE);
+        int byteArrayLength = randomIntBetween(0, 100);
+        byte[] byteArray = randomByteArrayOfLength(byteArrayLength);
+        s.writeBytes(byteArray, 0, byteArrayLength);
+
+        // Seek either over what we've already written, or what we have yet to write
+        int seekPosition = randomIntBetween(0, 200);
+        s.seek(seekPosition);
+        assertEquals(seekPosition, s.size());
+    }
+
+    public void testSeekAtBoundaries() {
+        // Do some arbitrary writing
+        BytesStreamOutput s = new BytesStreamOutput(0, BigArrays.NON_RECYCLING_INSTANCE);
+        int byteArrayLength = randomIntBetween(0, 100);
+        byte[] byteArray = randomByteArrayOfLength(byteArrayLength);
+        s.writeBytes(byteArray, 0, byteArrayLength);
+
+        s.seek(0);
+        assertEquals(0, s.size());
+        s.seek(Integer.MAX_VALUE);
+        assertEquals(Integer.MAX_VALUE, s.size());
+    }
+
+    public void testSeekWithOverflow() {
+        // Do some arbitrary writing and then overflow
+        BytesStreamOutput s = new BytesStreamOutput(0, BigArrays.NON_RECYCLING_INSTANCE);
+        int byteArrayLength = randomIntBetween(0, 100);
+        byte[] byteArray = randomByteArrayOfLength(byteArrayLength);
+        s.writeBytes(byteArray, 0, byteArrayLength);
+        assertThrows(IllegalArgumentException.class, () -> s.seek(randomLongBetween((long) Integer.MAX_VALUE + 1, (long) Integer.MAX_VALUE + 100)));
+    }
+
+    public void testSkip() {
+        // Do some arbitrary writing
+        BytesStreamOutput s = new BytesStreamOutput(0, BigArrays.NON_RECYCLING_INSTANCE);
+        int byteArrayLength = randomIntBetween(0, 100);
+        byte[] byteArray = randomByteArrayOfLength(byteArrayLength);
+        s.writeBytes(byteArray, 0, byteArrayLength);
+
+        int skipValue = randomIntBetween(0, 100);
+        s.skip(skipValue);
+        assertEquals(byteArrayLength + skipValue, s.size());
+    }
+
+    public void testSkipAtBoundaries() {
+        BytesStreamOutput s = new BytesStreamOutput(0, BigArrays.NON_RECYCLING_INSTANCE);
+        s.skip(0);
+        assertEquals(0, s.size());
+        s.skip(Integer.MAX_VALUE);
+        assertEquals(Integer.MAX_VALUE, s.size());
+    }
+
+    public void testSkipWithOverflow() {
+        // Do some arbitrary writing and then overflow
+        BytesStreamOutput s = new BytesStreamOutput(0, BigArrays.NON_RECYCLING_INSTANCE);
+        int byteArrayLength = randomIntBetween(1, 100);
+        byte[] byteArray = randomByteArrayOfLength(byteArrayLength);
+        s.writeBytes(byteArray, 0, byteArrayLength);
+
+        int skipPosition = randomIntBetween(Integer.MAX_VALUE - byteArrayLength + 1, Integer.MAX_VALUE);
+        assertThrows(IllegalArgumentException.class, () -> s.skip(skipPosition));
     }
 
     public void testSeekAndSkip() {

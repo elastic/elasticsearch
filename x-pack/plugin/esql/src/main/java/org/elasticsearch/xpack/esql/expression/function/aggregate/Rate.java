fb1c4c00debb2f31c62f61955b445098238c2507
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-09-29 13:51:59 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/aggregate/Rate.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/aggregate/Rate.java
@@ -44,12 +44,12 @@ public class Rate extends TimeSeriesAggregateFunction implements OptionalArgumen
     @FunctionInfo(
         type = FunctionType.TIME_SERIES_AGGREGATE,
         returnType = { "double" },
-        description = "Calculates the per-second average rate of increase of a" +
-            " [counter](docs-content://manage-data/data-store/data-streams/time-series-data-stream-tsds.md#time-series-metric). " +
-            "Rate calculations account for changes in monotonicity, such as counter resets when a service restarts, and use extrapolation" +
-            " for each time bucketing interval.Rate is the most appropriate aggregate function for counters. It is only allowed in a " +
-            "[STATS](/reference/query-languages/esql/commands/stats-by.md) command under a " +
-            "[`TS`](/reference/query-languages/esql/commands/ts.md) source command, to be properly applied per time series.",
+        description = "Calculates the per-second average rate of increase of a"
+            + " [counter](docs-content://manage-data/data-store/data-streams/time-series-data-stream-tsds.md#time-series-metric). "
+            + "Rate calculations account for changes in monotonicity, such as counter resets when a service restarts, and use extrapolation"
+            + " for each time bucketing interval.Rate is the most appropriate aggregate function for counters. It is only allowed in a "
+            + "[STATS](/reference/query-languages/esql/commands/stats-by.md) command under a "
+            + "[`TS`](/reference/query-languages/esql/commands/ts.md) source command, to be properly applied per time series.",
         appliesTo = { @FunctionAppliesTo(lifeCycle = FunctionAppliesToLifecycle.PREVIEW, version = "9.2.0") },
         examples = { @Example(file = "k8s-timeseries", tag = "rate") }
     )

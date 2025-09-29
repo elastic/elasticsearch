27f6ad38b793e2a475ee7e0dd8b324ee98478fcd
Ankit Sethi <ankit.sethi@elastic.co>
2025-09-29 10:30:59 -0500

    working commit

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/Security.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/Security.java
@@ -863,7 +863,8 @@ public class Security extends Plugin
             clusterService,
             resourceWatcherService,
             userRoleMapper,
-            projectResolver
+            projectResolver,
+            telemetryProvider
         );
         Map<String, Realm.Factory> realmFactories = new HashMap<>(
             InternalRealms.getFactories(

c4fd2334ea934362cbeae0284d752edb6fc734aa
Kostas Krikellas <kostas.krikellas@elastic.co>
2025-09-29 13:23:57 +0300

    Extend rate documentation

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/aggregate/Rate.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/aggregate/Rate.java
@@ -44,10 +44,16 @@ public class Rate extends TimeSeriesAggregateFunction implements OptionalArgumen
     @FunctionInfo(
         type = FunctionType.TIME_SERIES_AGGREGATE,
         returnType = { "double" },
-        description = "Calculates the rate of a counter field.",
+        description = "Calculates the per-second average rate of increase of a" +
+            " [counter](docs-content://manage-data/data-store/data-streams/time-series-data-stream-tsds.md#time-series-metric). " +
+            "Rate calculations account for changes in monotonicity, such as counter resets when a service restarts, and use extrapolation" +
+            " for each time bucketing interval.\n\nRate is the most appropriate aggregate function for counters. It is only allowed in a " +
+            "[STATS](/reference/query-languages/esql/commands/stats-by.md) command under a " +
+            "[`TS`](/reference/query-languages/esql/commands/ts.md) source command, to be properly applied per time series.",
         appliesTo = { @FunctionAppliesTo(lifeCycle = FunctionAppliesToLifecycle.PREVIEW, version = "9.2.0") },
         examples = { @Example(file = "k8s-timeseries", tag = "rate") }
     )
+
     public Rate(Source source, @Param(name = "field", type = { "counter_long", "counter_integer", "counter_double" }) Expression field) {
         this(source, field, new UnresolvedAttribute(source, "@timestamp"));
     }

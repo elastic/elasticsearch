3016a652e0f657ec53cb7019306068e4dc311d7e
zhubotang-wq <zhubo.tang@elastic.co>
2025-09-30 15:46:44 -0700

    Allocation: Include index shard counts as a criteria
    In a balanced allocation, for an index with n shards on a cluster
    of m nodes, each node should host not significantly more than n / m
    shards. This decider enforces this principle.

--- server/src/internalClusterTest/java/org/elasticsearch/cluster/routing/allocation/decider/IndexShardCountAllocationDeciderIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/cluster/routing/allocation/decider/IndexShardCountAllocationDeciderIT.java
@@ -9,22 +9,81 @@
 
 package org.elasticsearch.cluster.routing.allocation.decider;
 
+import org.elasticsearch.cluster.metadata.ProjectId;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.cluster.routing.RoutingNodes;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.Index;
+import org.elasticsearch.test.ClusterServiceUtils;
 import org.elasticsearch.test.ESIntegTestCase;
+import org.elasticsearch.transport.TransportService;
+
+import static org.elasticsearch.cluster.metadata.IndexMetadata.SETTING_NUMBER_OF_REPLICAS;
+import static org.elasticsearch.cluster.metadata.IndexMetadata.SETTING_NUMBER_OF_SHARDS;
 
 @ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST, numDataNodes = 0)
 public class IndexShardCountAllocationDeciderIT extends ESIntegTestCase {
 
 
+
+    /*
+        Happy path test scenario
+
+        3 ingestion node
+
+        5 shards index ,
+                A       1
+                B       2
+                C       2
+
+              Propose Allocation to move shard from B to C  -> Not preferred
+
+        random number of shards
+
+
+     */
+
+
     /**
      * Happy path -
+     *
+     *
+     *
+     *
+     *
      */
     public void testIndexShardCountExceedsAverageAllocation() {
 
-        Settings settings = Settings.builder().build();
 
+
+
+    }
+
+
+    private boolean checkShardAssignment(
+        RoutingNodes routingNodes,
+        Index index,
+        String firstDataNodeId,
+        String secondDataNodeId,
+        String thirdDataNodeId,
+        int shards
+    ) {
+
+        int firstDataNodeRealNumberOfShards = routingNodes.node(firstDataNodeId).numberOfOwningShardsForIndex(index);
+        int secondDataNodeRealNumberOfShards = routingNodes.node(secondDataNodeId).numberOfOwningShardsForIndex(index);
+        int thirdDataNodeRealNumberOfShards = routingNodes.node(thirdDataNodeId).numberOfOwningShardsForIndex(index);
+
+        return firstDataNodeRealNumberOfShards + secondDataNodeRealNumberOfShards + thirdDataNodeRealNumberOfShards == shards;
+    }
+
+
+    private TestHarness setUpIndex() {
+
+        Settings settings = Settings.builder().build();
         internalCluster().startMasterOnlyNode(settings);
+
         final var dataNodes = internalCluster().startDataOnlyNodes(3, settings);
+
         final String firstDataNodeName = dataNodes.get(0);
         final String secondDataNodeName = dataNodes.get(1);
         final String thirdDataNodeName = dataNodes.get(2);
@@ -33,7 +92,54 @@ public class IndexShardCountAllocationDeciderIT extends ESIntegTestCase {
         final String thirdDataNodeId = getNodeId(thirdDataNodeName);
         ensureStableCluster(4);
 
+        final DiscoveryNode firstDiscoveryNode =  internalCluster().getInstance(TransportService.class, firstDataNodeName).getLocalNode();
+        final DiscoveryNode secondDiscoveryNode = internalCluster().getInstance(TransportService.class, secondDataNodeName).getLocalNode();
+        final DiscoveryNode thirdDiscoveryNode = internalCluster().getInstance(TransportService.class, thirdDataNodeName).getLocalNode();
+        int randomNumberOfShards = randomIntBetween(10, 20);
+
+        String indexName = "test1";
+        int numberOfShards = 4;
+
+        logger.info(
+            "---> first node name "
+                + firstDataNodeName
+                + " and ID "
+                + firstDataNodeId
+                + "; second node name "
+                + secondDataNodeName
+                + " and ID "
+                + secondDataNodeId
+                + "; third node name "
+                + thirdDataNodeName
+                + " and ID "
+                + thirdDataNodeId
+        );
+
+        var verifyShardAllocationListener = ClusterServiceUtils.addMasterTemporaryStateListener(clusterState -> {
+            var indexRoutingTable = clusterState.routingTable(ProjectId.DEFAULT).index(indexName);
+            if (indexRoutingTable == null) {
+                return false;
+            }
+            return checkShardAssignment(clusterState.getRoutingNodes(), indexRoutingTable.getIndex(),
+                firstDataNodeId, secondDataNodeId, thirdDataNodeId, numberOfShards);
+
+        });
+
+        createIndex(
+            indexName,
+            Settings.builder().put(SETTING_NUMBER_OF_SHARDS, randomNumberOfShards).put(SETTING_NUMBER_OF_REPLICAS, 0).build()
+        );
+        ensureGreen(indexName);
+        logger.info("---> wait for  [" + randomNumberOfShards + "] shards to be assigned to node ");
+
+        safeAwait(verifyShardAllocationListener);
+        return new TestHarness(indexName);
     }
+
+
+    record TestHarness(
+        String indexName
+    ) {};
 
 
 

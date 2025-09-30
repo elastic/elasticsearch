423d12927d55980dc1d86fc6d3929e0b2e9bc9a1
Ryan Ernst <ryan@iernst.net>
2025-09-30 14:43:23 -0700

    fix oops

--- server/src/main/java/org/elasticsearch/index/query/AbstractGeometryQueryBuilder.java
+++ server/src/main/java/org/elasticsearch/index/query/AbstractGeometryQueryBuilder.java
@@ -151,8 +151,6 @@ public abstract class AbstractGeometryQueryBuilder<QB extends AbstractGeometryQu
         } else {
             shape = null;
             indexedShapeId = in.readOptionalString();
-            String type = in.readOptionalString();
-            assert MapperService.SINGLE_MAPPING_NAME.equals(type) : "Expected type [_doc], got [" + type + "]";
             indexedShapeIndex = in.readOptionalString();
             indexedShapePath = in.readOptionalString();
             indexedShapeRouting = in.readOptionalString();
@@ -174,7 +172,6 @@ public abstract class AbstractGeometryQueryBuilder<QB extends AbstractGeometryQu
             GeometryIO.writeGeometry(out, shape);
         } else {
             out.writeOptionalString(indexedShapeId);
-            out.writeOptionalString(MapperService.SINGLE_MAPPING_NAME);
             out.writeOptionalString(indexedShapeIndex);
             out.writeOptionalString(indexedShapePath);
             out.writeOptionalString(indexedShapeRouting);

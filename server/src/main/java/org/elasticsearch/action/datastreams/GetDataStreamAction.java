fa296fa5a4ac57046a94fd041744cccd3547c5c0
Jack Conradson <osjdconrad@gmail.com>
2025-10-03 14:28:30 -0700

    use patch version

--- server/src/main/java/org/elasticsearch/action/datastreams/GetDataStreamAction.java
+++ server/src/main/java/org/elasticsearch/action/datastreams/GetDataStreamAction.java
@@ -211,9 +211,9 @@ public class GetDataStreamAction extends ActionType<GetDataStreamAction.Response
 
         public static final ParseField DATA_STREAMS_FIELD = new ParseField("data_streams");
 
-        private static final TransportVersion INTRODUCE_FAILURES_DEFAULT_RETENTION = TransportVersion.fromName(
+        private static final TransportVersion INTRODUCE_FAILURES_DEFAULT_RETENTION_PATCH = TransportVersion.fromName(
             "introduce_failures_default_retention"
-        );
+        ).nextPatchVersion();
         private static final TransportVersion INCLUDE_INDEX_MODE_IN_GET_DATA_STREAM = TransportVersion.fromName(
             "include_index_mode_in_get_data_stream"
         );
@@ -669,7 +669,7 @@ public class GetDataStreamAction extends ActionType<GetDataStreamAction.Response
                 out.writeOptionalWriteable(rolloverConfiguration);
             }
             // A version 9.x cluster will never read this, so we only need to include the patch version here.
-            if (out.getTransportVersion().supports(INTRODUCE_FAILURES_DEFAULT_RETENTION)) {
+            if (out.getTransportVersion().supports(INTRODUCE_FAILURES_DEFAULT_RETENTION_PATCH)) {
                 out.writeOptionalTimeValue(dataGlobalRetention == null ? null : dataGlobalRetention.defaultRetention());
                 out.writeOptionalTimeValue(dataGlobalRetention == null ? null : dataGlobalRetention.maxRetention());
                 out.writeOptionalTimeValue(failuresGlobalRetention == null ? null : failuresGlobalRetention.defaultRetention());

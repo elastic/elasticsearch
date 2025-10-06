76f39cd51738cd093a048a32d5a71f01724e010a
Jan Kuipers <jan.kuipers@elastic.co>
2025-10-06 13:09:47 +0200

    move ConfidenceInterval class

--- x-pack/plugin/esql/src/main/generated/org/elasticsearch/xpack/esql/expression/function/scalar/math/ConfidenceIntervalDoubleEvaluator.java
+++ x-pack/plugin/esql/src/main/generated/org/elasticsearch/xpack/esql/expression/function/scalar/math/ConfidenceIntervalDoubleEvaluator.java
@@ -2,7 +2,7 @@
 // or more contributor license agreements. Licensed under the Elastic License
 // 2.0; you may not use this file except in compliance with the Elastic License
 // 2.0.
-package org.elasticsearch.xpack.esql.expression.function.scalar.multivalue;
+package org.elasticsearch.xpack.esql.expression.function.scalar.math;
 
 import java.lang.Override;
 import java.lang.String;

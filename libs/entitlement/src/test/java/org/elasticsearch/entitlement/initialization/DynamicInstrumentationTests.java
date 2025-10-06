520ad4c0be181922f390918ad0c42451527b1893
Moritz Mack <mmack@apache.org>
2025-10-06 15:11:15 +0200

    Extend public-callers-finder to check if methods are instrumented with entitlements.
    If the system property `es.entitlements.dump` is provided, instrumented methods will
    be dumped to that file and checked against the output of the public callers finder.
    
    Relates to ES-11757

---
+++ libs/entitlement/src/test/java/org/elasticsearch/entitlement/initialization/DynamicInstrumentationTests.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+package org.elasticsearch.entitlement.initialization;
+
+import org.elasticsearch.entitlement.bridge.EntitlementChecker;
+import org.elasticsearch.entitlement.instrumentation.CheckMethod;
+import org.elasticsearch.entitlement.instrumentation.MethodKey;
+import org.elasticsearch.test.ESTestCase;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.elasticsearch.test.LambdaMatchers.transformedMatch;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.startsWith;
+
+public class DynamicInstrumentationTests extends ESTestCase {
+
+    public void testAndDumpMethodsToInstrument() throws Exception {
+        Map<MethodKey, CheckMethod> methodsToInstrument = DynamicInstrumentation.getMethodsToInstrument(
+            EntitlementCheckerUtils.getVersionSpecificCheckerClass(EntitlementChecker.class, Runtime.version().feature())
+        );
+
+        List<Descriptor> descriptors = methodsToInstrument.keySet().stream().map(DynamicInstrumentationTests::lookupDescriptor).toList();
+
+        if (System.getProperty("es.entitlements.dump") != null) {
+            Path path = Path.of(System.getProperty("es.entitlements.dump"));
+            assert path.isAbsolute() : "absolute path required for es.entitlements.dump";
+            Files.write(
+                path,
+                () -> descriptors.stream().filter(d -> d.methodDescriptor != null).map(Descriptor::toLine).iterator(),
+                StandardCharsets.UTF_8
+            );
+        } else {
+            throw new RuntimeException();
+        }
+
+        assertThat(
+            descriptors,
+            everyItem(
+                anyOf(
+                    Descriptor.isKnown(),
+                    // not visible
+                    transformedMatch(Descriptor::className, startsWith("jdk/vm/ci/services/")),
+                    // removed in JDK 24
+                    is(
+                        new Descriptor(
+                            "sun/net/www/protocol/http/HttpURLConnection",
+                            "openConnectionCheckRedirects",
+                            List.of("java/net/URLConnection"),
+                            null
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+    private static Descriptor lookupDescriptor(MethodKey key) {
+        final String[] foundDescriptor = { null };
+        try {
+            ClassReader reader = new ClassReader(key.className());
+            reader.accept(new ClassVisitor(Opcodes.ASM9) {
+                @Override
+                public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+                    if (name.equals(key.methodName()) == false) {
+                        return null;
+                    }
+                    List<String> argTypes = Stream.of(Type.getArgumentTypes(descriptor)).map(Type::getInternalName).toList();
+                    if (argTypes.equals(key.parameterTypes())) {
+                        foundDescriptor[0] = descriptor;
+                    }
+                    return null;
+                }
+            }, 0);
+        } catch (IOException e) {
+            // nothing to do
+        }
+        return new Descriptor(key.className(), key.methodName(), key.parameterTypes(), foundDescriptor[0]);
+
+    }
+
+    record Descriptor(String className, String methodName, List<String> parameterTypes, String methodDescriptor) {
+
+        private static final String SEPARATOR = "\t";
+
+        CharSequence toLine() {
+            return String.join(SEPARATOR, className, methodName, methodDescriptor);
+        }
+
+        static Matcher<Descriptor> isKnown() {
+            return new TypeSafeMatcher<>() {
+                @Override
+                protected boolean matchesSafely(Descriptor item) {
+                    return item.methodDescriptor != null;
+                }
+
+                @Override
+                public void describeTo(Description description) {
+                    description.appendText("valid method descriptor");
+                }
+
+                @Override
+                protected void describeMismatchSafely(Descriptor item, Description mismatchDescription) {
+                    mismatchDescription.appendText("was ").appendValue(item.toString());
+                }
+            };
+        }
+
+    }
+
+    static <T> Matcher<T> never() {
+        return new BaseMatcher<>() {
+            @Override
+            public boolean matches(Object actual) {
+                return false;
+            }
+
+            @Override
+            public void describeTo(Description description) {}
+        };
+    }
+
+}

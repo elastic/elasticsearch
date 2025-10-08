0aea1ddbca96ca98b257e664a3e17d5fdd31a334
Alan Woodward <romseygeek@apache.org>
2025-10-08 16:30:16 +0100

    Doesn't work for ordinals :-(

--- x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/querydsl/query/SingleValueMatchQuery.java
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/querydsl/query/SingleValueMatchQuery.java
@@ -153,10 +153,24 @@ public final class SingleValueMatchQuery extends Query {
                 SortedSetDocValues sortedSetDocValues,
                 float boost,
                 ScoreMode scoreMode
-            ) {
+            ) throws IOException {
                 final int maxDoc = context.reader().maxDoc();
                 if (DocValues.unwrapSingleton(sortedSetDocValues) != null) {
-                    return new DocIdSetIteratorScorerSupplier(boost, scoreMode, sortedSetDocValues);
+                    // check for dense field
+                    // TODO: check doc values skippers
+                    // TODO: can we make Ordinals implement nextDoc() and always use DocIdSetIteratorScorerSupplier
+                    final Terms terms = context.reader().terms(fieldData.getFieldName());
+                    if (terms != null && terms.getDocCount() == maxDoc) {
+                        return new DocIdSetIteratorScorerSupplier(boost, scoreMode, DocIdSetIterator.all(maxDoc));
+                    } else {
+                        return new PredicateScorerSupplier(
+                            boost,
+                            scoreMode,
+                            maxDoc,
+                            MULTI_VALUE_MATCH_COST,
+                            sortedSetDocValues::advanceExact
+                        );
+                    }
                 }
                 final CheckedIntPredicate predicate = doc -> {
                     if (false == sortedSetDocValues.advanceExact(doc)) {

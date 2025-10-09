69ab385b0a22d33ab5bc088acd00a322396edb1c
Shiqin <u7760364@anu.edu.au>
2025-10-10 02:39:12 +1100

    ESQL: delegate ambiguity handling to Analyzer (no throw in AnalyzerRules.maybeResolveAgainstList)
    - Stop throwing VerificationException in AnalyzerRules when multiple non-pattern matches are found.
    - Let Analyzer (resolveKeep/Drop & checkAmbiguousUnqualifiedName) decide real ambiguity.

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/AnalyzerRules.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/AnalyzerRules.java
@@ -7,7 +7,6 @@
 
 package org.elasticsearch.xpack.esql.analysis;
 
-import org.elasticsearch.xpack.esql.VerificationException;
 import org.elasticsearch.xpack.esql.core.expression.Attribute;
 import org.elasticsearch.xpack.esql.core.expression.UnresolvedAttribute;
 import org.elasticsearch.xpack.esql.plan.logical.LogicalPlan;
@@ -111,12 +110,10 @@ public final class AnalyzerRules {
             return matches;
         }
 
-        // report ambiguity
-        List<String> refs = matches.stream().sorted((a, b) -> {
-            int lineDiff = a.sourceLocation().getLineNumber() - b.sourceLocation().getLineNumber();
-            int colDiff = a.sourceLocation().getColumnNumber() - b.sourceLocation().getColumnNumber();
-            return lineDiff != 0 ? lineDiff : (colDiff != 0 ? colDiff : a.name().compareTo(b.name()));
-        }).map(a -> "line " + a.sourceLocation().toString().substring(1) + " [" + a.name() + "]").toList();
-        throw new VerificationException("Found ambiguous reference to [" + ua.name() + "]; " + "matches any of " + refs);
+        // Multiple matches (non-pattern). Do NOT throw here.
+        // Let Analyzer decide whether this is a real ambiguity (e.g., qualifiers exist but name is unqualified).
+        matches.replaceAll(fieldInspector::apply);
+        return matches;
+
     }
 }

a50937811b474d5f199ae77d35e5a020d1765f24
Jack Conradson <osjdconrad@gmail.com>
2025-10-09 15:54:22 -0700

    Remove transport version V_8_16_1 (#136335)
    ES-12334

--- server/src/main/java/org/elasticsearch/search/builder/SearchSourceBuilder.java
+++ server/src/main/java/org/elasticsearch/search/builder/SearchSourceBuilder.java
@@ -286,11 +286,7 @@ public final class SearchSourceBuilder implements Writeable, ToXContentObject, R
         if (in.getTransportVersion().onOrAfter(TransportVersions.V_8_8_0)) {
             rankBuilder = in.readOptionalNamedWriteable(RankBuilder.class);
         }
-        if (in.getTransportVersion().onOrAfter(TransportVersions.V_8_16_1)) {
-            skipInnerHits = in.readBoolean();
-        } else {
-            skipInnerHits = false;
-        }
+        skipInnerHits = in.readBoolean();
     }
 
     @Override
@@ -370,9 +366,7 @@ public final class SearchSourceBuilder implements Writeable, ToXContentObject, R
         } else if (rankBuilder != null) {
             throw new IllegalArgumentException("cannot serialize [rank] to version [" + out.getTransportVersion().toReleaseVersion() + "]");
         }
-        if (out.getTransportVersion().onOrAfter(TransportVersions.V_8_16_1)) {
-            out.writeBoolean(skipInnerHits);
-        }
+        out.writeBoolean(skipInnerHits);
     }
 
     /**

a62266195d9607e05669fe243143cf72531641ca
Joshua Adams <joshua.adams@elastic.co>
2025-10-10 13:48:12 +0100

    Remove import

--- server/src/test/java/org/elasticsearch/gateway/GatewayMetaStatePersistedStateTests.java
+++ server/src/test/java/org/elasticsearch/gateway/GatewayMetaStatePersistedStateTests.java
@@ -42,7 +42,6 @@ import org.elasticsearch.env.TestEnvironment;
 import org.elasticsearch.index.IndexVersion;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.test.ESTestCase;
-import org.elasticsearch.test.junit.annotations.TestIssueLogging;
 import org.elasticsearch.threadpool.TestThreadPool;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;

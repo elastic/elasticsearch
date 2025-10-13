381cbcad56f1991ac4c3e0f0c8d4577edb27b8fb
David Turner <david.turner@elastic.co>
2025-10-13 12:59:12 +0100

    Minor test revert

--- server/src/test/java/org/elasticsearch/rest/action/cat/RestIndicesActionTests.java
+++ server/src/test/java/org/elasticsearch/rest/action/cat/RestIndicesActionTests.java
@@ -153,9 +153,7 @@ public class RestIndicesActionTests extends ESTestCase {
 
             IndexRoutingTable indexRoutingTable = clusterState.routingTable(project.id()).index(indexName);
             if (indexRoutingTable != null) {
-                // Has routing table, use the health status from ClusterIndexHealth
-                final ClusterHealthStatus indexHealthStatus = new ClusterIndexHealth(indexMetadata, indexRoutingTable).getStatus();
-                assertThat(row.get(0).value, equalTo(indexHealthStatus.toString().toLowerCase(Locale.ROOT)));
+                assertThat(row.get(0).value, equalTo(indexHealth.getStatus().toString().toLowerCase(Locale.ROOT)));
             } else if (indexStats != null) {
                 assertThat(row.get(0).value, equalTo("red*"));
             } else {

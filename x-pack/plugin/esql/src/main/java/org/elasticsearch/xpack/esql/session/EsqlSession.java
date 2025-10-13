2fac6695c714f06931dd847c3f222e16352f5cee
Jan Kuipers <jan.kuipers@elastic.co>
2025-10-13 11:17:58 +0200

    fixes

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
@@ -210,7 +210,6 @@ public class EsqlSession {
             plan = explain.query();
             parsedPlanString = plan.toString();
         }
-<<<<<<< HEAD
         analyzedPlan(
             plan,
             configuration,
@@ -238,7 +237,7 @@ public class EsqlSession {
                     SubscribableListener.<LogicalPlan>newForked(l -> preOptimizedPlan(plan, logicalPlanPreOptimizer, l))
                         .<LogicalPlan>andThen((l, p) -> {
                             if (request.approximate()) {
-                                new Approximate(p);  // to verify whether the pre-optimized plan is suitable for approximation
+                                Approximate.verifyPlan(p);
                             }
                             l.onResponse(p);
                         })
@@ -305,7 +304,17 @@ public class EsqlSession {
             // TODO: this could be snuck into the underlying listener
             EsqlCCSUtils.updateExecutionInfoAtEndOfPlanning(executionInfo);
             // execute any potential subplans
-            executeSubPlans(optimizedPlan, configuration, foldContext, planRunner, executionInfo, request, logicalPlanOptimizer, physicalPlanOptimizer, listener);
+            executeSubPlans(
+                optimizedPlan,
+                configuration,
+                foldContext,
+                planRunner,
+                executionInfo,
+                request,
+                logicalPlanOptimizer,
+                physicalPlanOptimizer,
+                listener
+            );
         }
     }
 
@@ -342,7 +351,12 @@ public class EsqlSession {
             );
         } else if (request.approximate()) {
             new Approximate(optimizedPlan).approximate(
-                (p, l) -> runner.run(logicalPlanToPhysicalPlan(optimizedPlan(p, logicalPlanOptimizer), request, physicalPlanOptimizer), configuration, foldContext, l),
+                (p, l) -> runner.run(
+                    logicalPlanToPhysicalPlan(optimizedPlan(p, logicalPlanOptimizer), request, physicalPlanOptimizer),
+                    configuration,
+                    foldContext,
+                    l
+                ),
                 listener
             );
         } else {

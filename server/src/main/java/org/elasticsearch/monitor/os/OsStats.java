30b8b9db23f3d80d1a6ce23038da420d9c118c89
Jack Conradson <osjdconrad@gmail.com>
2025-10-13 10:29:16 -0700

    Revert "Remove transport version V_8_17_0 (#136311)"
    This reverts commit 792636c22e30b3e300dcad7202776de0464143d3.

--- server/src/main/java/org/elasticsearch/monitor/os/OsStats.java
+++ server/src/main/java/org/elasticsearch/monitor/os/OsStats.java
@@ -498,7 +498,11 @@ public class OsStats implements Writeable, ToXContentFragment {
 
         Cgroup(final StreamInput in) throws IOException {
             cpuAcctControlGroup = in.readString();
-            cpuAcctUsageNanos = in.readBigInteger();
+            if (in.getTransportVersion().onOrAfter(TransportVersions.V_8_17_0)) {
+                cpuAcctUsageNanos = in.readBigInteger();
+            } else {
+                cpuAcctUsageNanos = BigInteger.valueOf(in.readLong());
+            }
             cpuControlGroup = in.readString();
             cpuCfsPeriodMicros = in.readLong();
             cpuCfsQuotaMicros = in.readLong();
@@ -511,7 +515,11 @@ public class OsStats implements Writeable, ToXContentFragment {
         @Override
         public void writeTo(final StreamOutput out) throws IOException {
             out.writeString(cpuAcctControlGroup);
-            out.writeBigInteger(cpuAcctUsageNanos);
+            if (out.getTransportVersion().onOrAfter(TransportVersions.V_8_17_0)) {
+                out.writeBigInteger(cpuAcctUsageNanos);
+            } else {
+                out.writeLong(cpuAcctUsageNanos.longValue());
+            }
             out.writeString(cpuControlGroup);
             out.writeLong(cpuCfsPeriodMicros);
             out.writeLong(cpuCfsQuotaMicros);
@@ -607,16 +615,28 @@ public class OsStats implements Writeable, ToXContentFragment {
             }
 
             CpuStat(final StreamInput in) throws IOException {
-                numberOfElapsedPeriods = in.readBigInteger();
-                numberOfTimesThrottled = in.readBigInteger();
-                timeThrottledNanos = in.readBigInteger();
+                if (in.getTransportVersion().onOrAfter(TransportVersions.V_8_17_0)) {
+                    numberOfElapsedPeriods = in.readBigInteger();
+                    numberOfTimesThrottled = in.readBigInteger();
+                    timeThrottledNanos = in.readBigInteger();
+                } else {
+                    numberOfElapsedPeriods = BigInteger.valueOf(in.readLong());
+                    numberOfTimesThrottled = BigInteger.valueOf(in.readLong());
+                    timeThrottledNanos = BigInteger.valueOf(in.readLong());
+                }
             }
 
             @Override
             public void writeTo(final StreamOutput out) throws IOException {
-                out.writeBigInteger(numberOfElapsedPeriods);
-                out.writeBigInteger(numberOfTimesThrottled);
-                out.writeBigInteger(timeThrottledNanos);
+                if (out.getTransportVersion().onOrAfter(TransportVersions.V_8_17_0)) {
+                    out.writeBigInteger(numberOfElapsedPeriods);
+                    out.writeBigInteger(numberOfTimesThrottled);
+                    out.writeBigInteger(timeThrottledNanos);
+                } else {
+                    out.writeLong(numberOfElapsedPeriods.longValue());
+                    out.writeLong(numberOfTimesThrottled.longValue());
+                    out.writeLong(timeThrottledNanos.longValue());
+                }
             }
 
             @Override

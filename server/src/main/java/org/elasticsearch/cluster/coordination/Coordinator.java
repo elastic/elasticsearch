a89c6e95a2aa58861220b288e7785365c8a07f46
Joshua Adams <joshua.adams@elastic.co>
2025-10-15 11:52:10 +0100

    unmute tests

--- server/src/main/java/org/elasticsearch/cluster/coordination/Coordinator.java
+++ server/src/main/java/org/elasticsearch/cluster/coordination/Coordinator.java
@@ -685,12 +685,16 @@ public class Coordinator extends AbstractLifecycleComponent implements ClusterSt
                             // eventual outcome, and we need to wait for that before we can release (our ref to) the connection and complete
                             // the listener.
                             logger.debug(
-                                "the cluster state update adding {} to the cluster was not committed to all nodes, " +
-                                    "and {} will be stepping down as master. However, the next master may have already committed the " +
-                                    "cluster state we've just published and will therefore include {} in its future states too. " +
-                                    "We will keep the connection to {} open until the next published cluster state update confirms " +
-                                    "whether {} has been added to the cluster",
-                                joiningNodeName, masterNode.getName(), joiningNodeName, joiningNodeName, joiningNodeName
+                                "the cluster state update adding {} to the cluster was not committed to all nodes, "
+                                    + "and {} will be stepping down as master. However, the next master may have already committed the "
+                                    + "cluster state we've just published and will therefore include {} in its future states too. "
+                                    + "We will keep the connection to {} open until the next published cluster state update confirms "
+                                    + "whether {} has been added to the cluster",
+                                joiningNodeName,
+                                masterNode.getName(),
+                                joiningNodeName,
+                                joiningNodeName,
+                                joiningNodeName
                             );
 
                             // NB we are on the master update thread here at the end of processing the failed cluster state update, so this
@@ -1262,11 +1266,11 @@ public class Coordinator extends AbstractLifecycleComponent implements ClusterSt
                 assert becomingMaster
                     || activePublication
                     || coordinationState.get()
-                    .getLastAcceptedConfiguration()
-                    .equals(coordinationState.get().getLastCommittedConfiguration())
+                        .getLastAcceptedConfiguration()
+                        .equals(coordinationState.get().getLastCommittedConfiguration())
                     : coordinationState.get().getLastAcceptedConfiguration()
-                    + " != "
-                    + coordinationState.get().getLastCommittedConfiguration();
+                        + " != "
+                        + coordinationState.get().getLastCommittedConfiguration();
             } else if (mode == Mode.FOLLOWER) {
                 assert coordinationState.get().electionWon() == false : getLocalNode() + " is FOLLOWER so electionWon() should be false";
                 assert lastKnownLeader.isPresent() && (lastKnownLeader.get().equals(getLocalNode()) == false);
@@ -1279,7 +1283,7 @@ public class Coordinator extends AbstractLifecycleComponent implements ClusterSt
                 assert followersChecker.getKnownFollowers().isEmpty();
                 assert lastKnownLeader.get().equals(applierState.nodes().getMasterNode())
                     || (applierState.nodes().getMasterNodeId() == null
-                    && (applierState.term() < getCurrentTerm() || applierState.version() < getLastAcceptedState().version()));
+                        && (applierState.term() < getCurrentTerm() || applierState.version() < getLastAcceptedState().version()));
                 assert currentPublication.map(Publication::isCommitted).orElse(true);
                 assert preVoteCollector.getLeader().equals(lastKnownLeader.get()) : preVoteCollector;
                 assert clusterFormationFailureHelper.isRunning() == false;
@@ -1368,9 +1372,9 @@ public class Coordinator extends AbstractLifecycleComponent implements ClusterSt
             var nodeEligibility = localNodeMayWinElection(getLastAcceptedState(), electionStrategy);
             assert nodeEligibility.mayWin()
                 : "initial state does not allow local node to win election, reason: "
-                + nodeEligibility.reason()
-                + " , metadata: "
-                + getLastAcceptedState().coordinationMetadata();
+                    + nodeEligibility.reason()
+                    + " , metadata: "
+                    + getLastAcceptedState().coordinationMetadata();
             preVoteCollector.update(getPreVoteResponse(), null); // pick up the change to last-accepted version
             startElectionScheduler();
             return true;
@@ -1392,7 +1396,7 @@ public class Coordinator extends AbstractLifecycleComponent implements ClusterSt
             .filter(
                 n -> n.isMasterNode() == false
                     && (clusterState.getLastAcceptedConfiguration().getNodeIds().contains(n.getId())
-                    || clusterState.getLastCommittedConfiguration().getNodeIds().contains(n.getId()))
+                        || clusterState.getLastCommittedConfiguration().getNodeIds().contains(n.getId()))
             )
             .map(DiscoveryNode::getId);
 

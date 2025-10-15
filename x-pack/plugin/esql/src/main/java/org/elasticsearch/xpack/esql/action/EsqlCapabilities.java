5f5cc00d6b5f0262f15559075e82768808fabf03
Nhat Nguyen <nhat.nguyen@elastic.co>
2025-10-15 08:14:48 -0700

    Create new block when filter OrdinalBytesRefBlock (#136444)
    Currently, when filtering OrdinalBytesRefBlock and 
    OrdinalBytesRefVector, we reuse the existing dictionary and filter only
    the ordinals. Although this is semantically correct, it can break
    HashAggregationOperator when ordinal optimizations are enabled in
    BlockHash.
    
    Example: given an incoming OrdinalBytesRefBlock after filtering:
    
    dict: [apple, banana, orange]
    ordinals: [0, 0, 0, 0, 1, 1]
    
    Here, orange is not referenced. During grouping and hashing, all 
    dictionary entries [apple, banana, orange] are hashed with group IDs [1,
    2, 3] (0 reserved for null), and the block produces groupings [1, 1, 1,
    1, 2, 2]. The output is correct, but when evaluating partial/final
    results, we cannot exclude orange (groupId=3) unless we enter slow mode
    and track every seen group. This can cause an IndexOutOfBoundsException
    if orange is the largest group and the over-allocation is not enough to
    cover it, or orange may be included in the result even though it was
    excluded previously.
    
    This change enforces the creation of a new Block/Vector when filtering 
    OrdinalBytesRefBlock and OrdinalBytesRefVector.
    
    Closes #136423

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1608,7 +1608,12 @@ public class EsqlCapabilities {
         /**
          * Pack dimension values in TS command
          */
-        PACK_DIMENSIONS_IN_TS
+        PACK_DIMENSIONS_IN_TS,
+
+        /**
+         * Create new block when filtering OrdinalBytesRefBlock
+         */
+        FIX_FILTER_ORDINALS,
 
         ;
 

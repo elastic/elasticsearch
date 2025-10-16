ceff6277a1bd8585d8bef565c5bc46330358cff7
Iván Cea Fontenla <ivan.cea@elastic.co>
2025-10-16 14:17:16 +0200

    Add zoneId to "now" again

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/Configuration.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/Configuration.java
@@ -80,7 +80,7 @@ public class Configuration implements Writeable {
         int resultTruncationDefaultSizeTimeseries
     ) {
         this.zoneId = zi.normalized();
-        this.now = ZonedDateTime.now(Clock.tick(Clock.systemUTC(), Duration.ofNanos(1)));
+        this.now = ZonedDateTime.now(Clock.tick(Clock.system(zoneId), Duration.ofNanos(1)));
         this.username = username;
         this.clusterName = clusterName;
         this.locale = locale;
@@ -333,6 +333,8 @@ public class Configuration implements Writeable {
             + ",timeseries="
             + resultTruncationDefaultSize(true)
             + "]"
+            + ", zoneId="
+            + zoneId
             + ", locale="
             + locale
             + ", query='"

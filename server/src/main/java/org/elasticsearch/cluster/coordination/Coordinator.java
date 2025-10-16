d553217c9cea98b36e8f025a5ec0fdc00ec8f375
Joshua Adams <joshua.adams@elastic.co>
2025-10-16 10:42:15 +0100

    Weaken wording of preceding log

--- server/src/main/java/org/elasticsearch/cluster/coordination/Coordinator.java
+++ server/src/main/java/org/elasticsearch/cluster/coordination/Coordinator.java
@@ -1812,7 +1812,7 @@ public class Coordinator extends AbstractLifecycleComponent implements ClusterSt
 
                     if (foundQuorum) {
                         if (electionScheduler == null) {
-                            logger.debug("starting election scheduler, expecting votes [{}]", expectedVotes);
+                            logger.debug("preparing election scheduler, expecting votes [{}]", expectedVotes);
                             startElectionScheduler();
                         }
                     } else {
@@ -1837,6 +1837,7 @@ public class Coordinator extends AbstractLifecycleComponent implements ClusterSt
             logger.debug("local node is not the master, skipping election scheduler");
             return;
         }
+        logger.debug("starting election scheduler");
 
         final TimeValue gracePeriod = TimeValue.ZERO;
         electionScheduler = electionSchedulerFactory.startElectionScheduler(gracePeriod, new Runnable() {

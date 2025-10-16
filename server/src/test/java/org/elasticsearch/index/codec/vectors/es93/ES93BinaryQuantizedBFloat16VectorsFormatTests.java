cf58886c59efe8eecf38afbccb38b182844a2179
Simon Cooper <simon.cooper@elastic.co>
2025-10-16 10:38:45 +0100

    Create bloat16 copies of Lucene vector tests

--- server/src/test/java/org/elasticsearch/index/codec/vectors/es93/ES93BinaryQuantizedBFloat16VectorsFormatTests.java
+++ server/src/test/java/org/elasticsearch/index/codec/vectors/es93/ES93BinaryQuantizedBFloat16VectorsFormatTests.java
@@ -11,11 +11,6 @@ package org.elasticsearch.index.codec.vectors.es93;
 
 import org.apache.lucene.index.VectorEncoding;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static org.hamcrest.Matchers.closeTo;
-
 public class ES93BinaryQuantizedBFloat16VectorsFormatTests extends ES93BinaryQuantizedVectorsFormatTests {
     @Override
     boolean useBFloat16() {
@@ -59,39 +54,25 @@ public class ES93BinaryQuantizedBFloat16VectorsFormatTests extends ES93BinaryQua
 
     @Override
     public void testWriterRamEstimate() throws Exception {
-        // estimate is different due to bfloat16
+        LuceneBFloat16Tests.testWriterRamEstimate();
     }
 
     @Override
     public void testRandom() throws Exception {
-        AssertionError err = expectThrows(AssertionError.class, super::testRandom);
-        assertFloatsWithinBounds(err);
+        LuceneBFloat16Tests.testRandom(this::randomSimilarity);
     }
 
     @Override
     public void testSparseVectors() throws Exception {
-        AssertionError err = expectThrows(AssertionError.class, super::testSparseVectors);
-        assertFloatsWithinBounds(err);
+        LuceneBFloat16Tests.testSparseVectors(this::randomSimilarity, this::randomVectorEncoding);
     }
 
     @Override
     public void testVectorValuesReportCorrectDocs() throws Exception {
-        AssertionError err = expectThrows(AssertionError.class, super::testVectorValuesReportCorrectDocs);
-        assertFloatsWithinBounds(err);
-    }
-
-    private static final Pattern FLOAT_ASSERTION_FAILURE = Pattern.compile(".*expected:<([0-9.-]+)> but was:<([0-9.-]+)>");
-
-    private static void assertFloatsWithinBounds(AssertionError error) {
-        Matcher m = FLOAT_ASSERTION_FAILURE.matcher(error.getMessage());
-        if (m.matches() == false) {
-            throw error;    // nothing to do with us, just rethrow
-        }
-
-        // numbers just need to be in the same vicinity
-        double expected = Double.parseDouble(m.group(1));
-        double actual = Double.parseDouble(m.group(2));
-        double allowedError = expected * 0.01;  // within 1%
-        assertThat(error.getMessage(), actual, closeTo(expected, allowedError));
+        LuceneBFloat16Tests.testVectorValuesReportCorrectDocs(
+            this::randomSimilarity,
+            this::randomVectorEncoding,
+            this::assertOffHeapByteSize
+        );
     }
 }

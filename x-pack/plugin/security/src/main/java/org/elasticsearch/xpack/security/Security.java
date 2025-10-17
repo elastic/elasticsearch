a71ca11ed100b1f8512f573f176b6f8d0d59588b
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-10-17 11:37:17 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/Security.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/Security.java
@@ -9,6 +9,7 @@ package org.elasticsearch.xpack.security;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpUtil;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.util.SetOnce;

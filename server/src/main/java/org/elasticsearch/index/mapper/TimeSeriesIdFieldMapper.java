1040ae9d84b624aa59c77c33b93d47cff6614b26
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-10-21 10:13:10 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/index/mapper/TimeSeriesIdFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/TimeSeriesIdFieldMapper.java
@@ -60,8 +60,7 @@ public class TimeSeriesIdFieldMapper extends MetadataFieldMapper {
     }
 
     public static TimeSeriesIdFieldMapper getInstance(MappingParserContext context) {
-        boolean useDocValuesSkipper = context.indexVersionCreated()
-            .onOrAfter(IndexVersions.SKIPPERS_ENABLED_BY_DEFAULT)
+        boolean useDocValuesSkipper = context.indexVersionCreated().onOrAfter(IndexVersions.SKIPPERS_ENABLED_BY_DEFAULT)
             && context.getIndexSettings().useDocValuesSkipper();
         return TimeSeriesIdFieldMapper.getInstance(useDocValuesSkipper);
     }

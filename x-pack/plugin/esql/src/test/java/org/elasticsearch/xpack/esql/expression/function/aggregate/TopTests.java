332edf0aa29bc37cc6f64ea704182da4238d369c
Przemyslaw Witek <przemyslaw.witek@elastic.co>
2025-10-21 14:25:13 +0200

    Add tests for new surrogates

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/aggregate/TopTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/aggregate/TopTests.java
@@ -14,6 +14,7 @@ import org.apache.lucene.document.InetAddressPoint;
 import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.network.InetAddresses;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
+import org.elasticsearch.xpack.esql.core.expression.FieldAttribute;
 import org.elasticsearch.xpack.esql.core.tree.Source;
 import org.elasticsearch.xpack.esql.core.type.DataType;
 import org.elasticsearch.xpack.esql.expression.function.AbstractAggregationTestCase;
@@ -88,7 +89,64 @@ public class TopTests extends AbstractAggregationTestCase {
 
         suppliers.addAll(
             List.of(
-                // Surrogates
+                // Surrogates for cases where field and outputField are effectively the same field
+                new TestCaseSupplier(
+                    List.of(DataType.INTEGER, DataType.INTEGER, DataType.KEYWORD, DataType.INTEGER),
+                    () -> new TestCaseSupplier.TestCase(
+                        List.of(
+                            TestCaseSupplier.TypedData.multiRow(List.of(5, 8, -2, 0, 200), DataType.INTEGER, "field"),
+                            new TestCaseSupplier.TypedData(3, DataType.INTEGER, "limit").forceLiteral(),
+                            new TestCaseSupplier.TypedData(new BytesRef("desc"), DataType.KEYWORD, "order").forceLiteral(),
+                            TestCaseSupplier.TypedData.multiRow(List.of(5, 8, -2, 0, 200), DataType.INTEGER, "field")
+                        ),
+                        "TopInt",
+                        DataType.INTEGER,
+                        equalTo(List.of(200, 8, 5))
+                    )
+                ),
+                new TestCaseSupplier(
+                    List.of(DataType.LONG, DataType.INTEGER, DataType.KEYWORD, DataType.LONG),
+                    () -> new TestCaseSupplier.TestCase(
+                        List.of(
+                            TestCaseSupplier.TypedData.multiRow(List.of(5L, 8L, -2L, 0L, 200L), DataType.LONG, "field"),
+                            new TestCaseSupplier.TypedData(3, DataType.INTEGER, "limit").forceLiteral(),
+                            new TestCaseSupplier.TypedData(new BytesRef("desc"), DataType.KEYWORD, "order").forceLiteral(),
+                            TestCaseSupplier.TypedData.multiRow(List.of(5L, 8L, -2L, 0L, 200L), DataType.LONG, "field")
+                        ),
+                        "TopLong",
+                        DataType.LONG,
+                        equalTo(List.of(200L, 8L, 5L))
+                    )
+                ),
+                new TestCaseSupplier(
+                    List.of(DataType.DOUBLE, DataType.INTEGER, DataType.KEYWORD, DataType.DOUBLE),
+                    () -> new TestCaseSupplier.TestCase(
+                        List.of(
+                            TestCaseSupplier.TypedData.multiRow(List.of(5., 8., -2., 0., 200.), DataType.DOUBLE, "field"),
+                            new TestCaseSupplier.TypedData(3, DataType.INTEGER, "limit").forceLiteral(),
+                            new TestCaseSupplier.TypedData(new BytesRef("desc"), DataType.KEYWORD, "order").forceLiteral(),
+                            TestCaseSupplier.TypedData.multiRow(List.of(5., 8., -2., 0., 200.), DataType.DOUBLE, "field")
+                        ),
+                        "TopDouble",
+                        DataType.DOUBLE,
+                        equalTo(List.of(200., 8., 5.))
+                    )
+                ),
+                new TestCaseSupplier(
+                    List.of(DataType.DATETIME, DataType.INTEGER, DataType.KEYWORD, DataType.DATETIME),
+                    () -> new TestCaseSupplier.TestCase(
+                        List.of(
+                            TestCaseSupplier.TypedData.multiRow(List.of(5L, 8L, 2L, 0L, 200L), DataType.DATETIME, "field"),
+                            new TestCaseSupplier.TypedData(3, DataType.INTEGER, "limit").forceLiteral(),
+                            new TestCaseSupplier.TypedData(new BytesRef("desc"), DataType.KEYWORD, "order").forceLiteral(),
+                            TestCaseSupplier.TypedData.multiRow(List.of(5L, 8L, 2L, 0L, 200L), DataType.DATETIME, "field")
+                        ),
+                        "TopLong",
+                        DataType.DATETIME,
+                        equalTo(List.of(200L, 8L, 5L))
+                    )
+                ),
+                // Surrogates for cases where limit == 1
                 new TestCaseSupplier(
                     List.of(DataType.BOOLEAN, DataType.INTEGER, DataType.KEYWORD),
                     () -> new TestCaseSupplier.TestCase(
@@ -392,7 +450,15 @@ public class TopTests extends AbstractAggregationTestCase {
 
     @Override
     protected Expression build(Source source, List<Expression> args) {
-        return new Top(source, args.get(0), args.get(1), args.size() > 2 ? args.get(2) : null, args.size() > 3 ? args.get(3) : null);
+        Expression field = args.get(0);
+        Expression outputField = args.size() > 3 ? args.get(3) : null;
+        if (field instanceof FieldAttribute f && outputField instanceof FieldAttribute of && f.fieldName().equals(of.fieldName())) {
+            // In order to avoid passing the same field twice as two different FieldAttribute objects, we use `field` as the fourth argument
+            // if `field`'s and `outputField`'s `fieldName` match. In such case TOP will use surrogate.
+            return new Top(source, field, args.get(1), args.size() > 2 ? args.get(2) : null, field);
+        } else {
+            return new Top(source, field, args.get(1), args.size() > 2 ? args.get(2) : null, outputField);
+        }
     }
 
     @SuppressWarnings("unchecked")

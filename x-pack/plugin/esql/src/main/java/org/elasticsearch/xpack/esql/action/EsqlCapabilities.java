d28c2c7d94b5ed8e7ac77edfc1e6122d7414e1b9
kanoshiou <uiaao@tuta.io>
2025-10-22 22:05:25 +0800

    ESQL: Fix lookup join filter pushdown to use semantic equality (#136818)
    Previously, when pushing down filters to the right side of a lookup
    join, we used simple equality (contains) to check for duplicate filters.
    This could result in semantically equivalent filters being added
    multiple times if they were different object instances.
    
    This commit changes the duplicate check to use `semanticEquals()`
    instead, ensuring that filters with the same semantic meaning are
    properly deduplicated regardless of object identity.
    
    Closes #136599
    
    (cherry picked from commit f54987f9690c7a2c8e40cc0c59c6aa4576dc9b96)
    
    # Conflicts:
    #	x-pack/plugin/esql/qa/server/src/main/java/org/elasticsearch/xpack/esql/qa/rest/generative/GenerativeRestTest.java
    #	x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1629,6 +1629,14 @@ public class EsqlCapabilities {
          * Temporarily forbid the use of an explicit or implicit LIMIT before INLINE STATS.
          */
         FORBID_LIMIT_BEFORE_INLINE_STATS(INLINE_STATS.enabled),
+
+        /**
+         * Fix for lookup join filter pushdown not using semantic equality.
+         * This prevents duplicate filters from being pushed down when they are semantically equivalent, causing an infinite loop where
+         * BooleanSimplification will simplify the original and duplicate filters, so they'll be pushed down again...
+         */
+        LOOKUP_JOIN_SEMANTIC_FILTER_DEDUP,
+
         // Last capability should still have a comma for fewer merge conflicts when adding new ones :)
         // This comment prevents the semicolon from being on the previous capability when Spotless formats the file.
         ;

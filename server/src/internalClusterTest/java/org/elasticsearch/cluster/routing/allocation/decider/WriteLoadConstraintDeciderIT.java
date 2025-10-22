ff29c079f1608bf401bb6fca22e331ab2946f171
zhubotang-wq <zhubo.tang@elastic.co>
2025-10-22 11:01:33 -0700

    Allocation: Include index shard counts as a criteria
    fix test regressions.

--- server/src/internalClusterTest/java/org/elasticsearch/cluster/routing/allocation/decider/WriteLoadConstraintDeciderIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/cluster/routing/allocation/decider/WriteLoadConstraintDeciderIT.java
@@ -171,7 +171,7 @@ public class WriteLoadConstraintDeciderIT extends ESIntegTestCase {
             updateClusterSettings(
                 Settings.builder()
                     .put("cluster.routing.allocation.exclude._name", harness.firstDataNodeName)
-                    .put("cluster.routing.allocation.index_balance_decider.enabled", false)
+                    .put("cluster.routing.allocation.index_balance_decider.load_skew_tolerance", 1000)
             );
 
             safeAwait(temporaryClusterStateListener);

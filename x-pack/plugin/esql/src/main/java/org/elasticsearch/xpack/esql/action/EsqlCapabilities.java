5be05c4e3606bdd07f9bbe9fa38b9e6225dd5e2d
Andrei Stefan <andrei@elastic.co>
2025-10-23 16:40:23 +0300

    Deal with equality (and implicitly the IN operator) properly when it comes to multi-valued constants (literals): they should emit a warning header, a contract set in place in the compute engine Evaluators.

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1558,6 +1558,8 @@ public class EsqlCapabilities {
          * Temporarily forbid the use of an explicit or implicit LIMIT before INLINE STATS.
          */
         FORBID_LIMIT_BEFORE_INLINE_STATS(INLINE_STATS.enabled),
+
+        FIX_MV_CONSTANT_EQUALS_FIELD,
         // Last capability should still have a comma for fewer merge conflicts when adding new ones :)
         // This comment prevents the semicolon from being on the previous capability when Spotless formats the file.
         ;

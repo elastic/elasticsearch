5f42a0cf5c171dedaaec2c2148f2c9b92348ca33
Elliot Barlas <elliot.barlas@elastic.co>
2025-10-23 14:51:41 -0700

    Add new PKC JWK settings to allowed list. Update default file polling interval. Use PrivilegedFileWatcher to address entitlement exception.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authc/jwt/JwtRealmSettings.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authc/jwt/JwtRealmSettings.java
@@ -158,7 +158,18 @@ public class JwtRealmSettings {
         final Set<Setting.AffixSetting<?>> set = new HashSet<>(RealmSettings.getStandardSettings(TYPE));
         set.add(TOKEN_TYPE);
         // JWT Issuer settings
-        set.addAll(List.of(ALLOWED_ISSUER, ALLOWED_SIGNATURE_ALGORITHMS, ALLOWED_CLOCK_SKEW, PKC_JWKSET_PATH));
+        set.addAll(
+            List.of(
+                ALLOWED_ISSUER,
+                ALLOWED_SIGNATURE_ALGORITHMS,
+                ALLOWED_CLOCK_SKEW,
+                PKC_JWKSET_PATH,
+                PKC_JWKSET_RELOAD_ENABLED,
+                PKC_JWKSET_RELOAD_FILE_INTERVAL,
+                PKC_JWKSET_RELOAD_URL_INTERVAL_MIN,
+                PKC_JWKSET_RELOAD_URL_INTERVAL_MAX
+            )
+        );
         // JWT Audience settings
         set.addAll(List.of(ALLOWED_AUDIENCES));
         // JWT End-user settings
@@ -264,7 +275,7 @@ public class JwtRealmSettings {
     public static final Setting.AffixSetting<TimeValue> PKC_JWKSET_RELOAD_URL_INTERVAL_MIN = Setting.affixKeySetting(
         RealmSettings.realmSettingPrefix(TYPE),
         "pkc_jwkset_reload_url_interval_min",
-        key -> Setting.timeSetting(key, TimeValue.timeValueMinutes(5), TimeValue.timeValueMinutes(5), Setting.Property.NodeScope)
+        key -> Setting.timeSetting(key, TimeValue.timeValueHours(1), TimeValue.timeValueMinutes(5), Setting.Property.NodeScope)
     );
 
     public static final Setting.AffixSetting<TimeValue> PKC_JWKSET_RELOAD_URL_INTERVAL_MAX = Setting.affixKeySetting(
--- x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/jwt/JwkSetLoader.java
+++ x-pack/plugin/security/src/main/java/org/elasticsearch/xpack/security/authc/jwt/JwkSetLoader.java
@@ -27,6 +27,7 @@ import org.elasticsearch.xpack.core.security.authc.RealmConfig;
 import org.elasticsearch.xpack.core.security.authc.RealmSettings;
 import org.elasticsearch.xpack.core.security.authc.jwt.JwtRealmSettings;
 import org.elasticsearch.xpack.core.ssl.SSLService;
+import org.elasticsearch.xpack.security.PrivilegedFileWatcher;
 
 import java.io.IOException;
 import java.net.URI;
@@ -375,7 +376,7 @@ public class JwkSetLoader implements Releasable {
         boolean changed = false;
 
         FileChangeWatcher(Path path) {
-            this.fileWatcher = new FileWatcher(path);
+            this.fileWatcher = new PrivilegedFileWatcher(path);
             this.fileWatcher.addListener(this);
         }
 

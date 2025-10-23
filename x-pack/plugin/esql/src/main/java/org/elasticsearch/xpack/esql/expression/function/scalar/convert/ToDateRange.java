d1eb012b016e642382c5eb0ada1fe6d1e6c57ffd
Craig Taverner <craig@amanzi.com>
2025-10-23 14:16:36 +0200

    Added identity option to TO_DATE_RANGE, plus more tests

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/convert/ToDateRange.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/convert/ToDateRange.java
@@ -13,12 +13,16 @@ import org.elasticsearch.xpack.esql.core.expression.Expression;
 import org.elasticsearch.xpack.esql.core.tree.NodeInfo;
 import org.elasticsearch.xpack.esql.core.tree.Source;
 import org.elasticsearch.xpack.esql.core.type.DataType;
+import org.elasticsearch.xpack.esql.expression.function.FunctionAppliesTo;
+import org.elasticsearch.xpack.esql.expression.function.FunctionAppliesToLifecycle;
 import org.elasticsearch.xpack.esql.expression.function.FunctionInfo;
 import org.elasticsearch.xpack.esql.expression.function.Param;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+
+import static org.elasticsearch.xpack.esql.core.type.DataType.DATE_RANGE;
 
 public class ToDateRange extends AbstractConvertFunction {
     public static final NamedWriteableRegistry.Entry ENTRY = new NamedWriteableRegistry.Entry(
@@ -27,16 +31,19 @@ public class ToDateRange extends AbstractConvertFunction {
         ToDateRange::new
     );
 
-    private static final Map<DataType, BuildFactory> EVALUATORS = Map.ofEntries(
-        // TODO
-    );
+    private static final Map<DataType, BuildFactory> EVALUATORS = Map.ofEntries(Map.entry(DATE_RANGE, (source, fieldEval) -> fieldEval));
 
-    @FunctionInfo(returnType = "DateRange", description = "TBD")
+    @FunctionInfo(
+        returnType = "date_range",
+        preview = true,
+        appliesTo = { @FunctionAppliesTo(lifeCycle = FunctionAppliesToLifecycle.PREVIEW) },
+        description = "Converts an input value to a `date_range` value."
+    )
     public ToDateRange(
         Source source,
         @Param(
             name = "field",
-            type = {},
+            type = { "date_range" },
             description = "Input value. The input can be a single- or multi-valued column or an expression."
         ) Expression field
     ) {

f4add019a38f8ca1376a205d491da10c09beafb2
Mike Pellegrini <mike.pellegrini@elastic.co>
2025-10-24 14:52:23 -0400

    Get inference results

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/TransportGetInferenceFieldsAction.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/TransportGetInferenceFieldsAction.java
@@ -9,6 +9,7 @@ package org.elasticsearch.xpack.inference;
 
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.ActionFilters;
+import org.elasticsearch.action.support.GroupedActionListener;
 import org.elasticsearch.action.support.HandledTransportAction;
 import org.elasticsearch.client.internal.Client;
 import org.elasticsearch.cluster.ClusterState;
@@ -18,17 +19,28 @@ import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.regex.Regex;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
+import org.elasticsearch.core.Tuple;
+import org.elasticsearch.inference.InferenceResults;
+import org.elasticsearch.inference.InferenceServiceResults;
+import org.elasticsearch.inference.InputType;
+import org.elasticsearch.inference.TaskType;
 import org.elasticsearch.injection.guice.Inject;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.transport.TransportService;
 import org.elasticsearch.xpack.core.inference.action.GetInferenceFieldsAction;
+import org.elasticsearch.xpack.core.inference.action.InferenceAction;
+import org.elasticsearch.xpack.core.ml.inference.results.ErrorInferenceResults;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.elasticsearch.index.IndexSettings.DEFAULT_FIELD_SETTING;
+import static org.elasticsearch.xpack.core.ClientHelper.ML_ORIGIN;
+import static org.elasticsearch.xpack.core.ClientHelper.executeAsyncWithOrigin;
 
 // TODO: Handle multi-project
 // TODO: Don't hard-code useDefaultFields
@@ -67,6 +79,7 @@ public class TransportGetInferenceFieldsAction extends HandledTransportAction<
         final List<String> indices = request.getIndices();
         final List<String> fields = request.getFields();
         final boolean resolveWildcards = request.resolveWildcards();
+        final String query = request.getQuery();
 
         Map<String, List<InferenceFieldMetadata>> inferenceFieldsMap = new HashMap<>(indices.size());
         indices.forEach(index -> {
@@ -76,7 +89,17 @@ public class TransportGetInferenceFieldsAction extends HandledTransportAction<
             }
         });
 
-        listener.onResponse(new GetInferenceFieldsAction.Response(inferenceFieldsMap, Map.of()));
+        if (query != null) {
+            Set<String> inferenceIds = inferenceFieldsMap.values()
+                .stream()
+                .flatMap(List::stream)
+                .map(InferenceFieldMetadata::getSearchInferenceId)
+                .collect(Collectors.toSet());
+
+            getInferenceResults(query, inferenceIds, inferenceFieldsMap, listener);
+        } else {
+            listener.onResponse(new GetInferenceFieldsAction.Response(inferenceFieldsMap, Map.of()));
+        }
     }
 
     private List<InferenceFieldMetadata> getInferenceFieldMetadata(
@@ -113,7 +136,72 @@ public class TransportGetInferenceFieldsAction extends HandledTransportAction<
         return inferenceFieldMetadataList;
     }
 
+    private void getInferenceResults(
+        String query,
+        Set<String> inferenceIds,
+        Map<String, List<InferenceFieldMetadata>> inferenceFieldsMap,
+        ActionListener<GetInferenceFieldsAction.Response> listener
+    ) {
+        GroupedActionListener<Tuple<String, InferenceResults>> gal = new GroupedActionListener<>(
+            inferenceIds.size(),
+            listener.delegateFailureAndWrap((l, c) -> {
+                Map<String, InferenceResults> inferenceResultsMap = new HashMap<>();
+                c.forEach(t -> inferenceResultsMap.put(t.v1(), t.v2()));
+
+                GetInferenceFieldsAction.Response response = new GetInferenceFieldsAction.Response(inferenceFieldsMap, inferenceResultsMap);
+                l.onResponse(response);
+            })
+        );
+
+        List<InferenceAction.Request> inferenceRequests = inferenceIds.stream()
+            .map(
+                i -> new InferenceAction.Request(
+                    TaskType.ANY,
+                    i,
+                    null,
+                    null,
+                    null,
+                    List.of(query),
+                    Map.of(),
+                    InputType.INTERNAL_SEARCH,
+                    null,
+                    false
+                )
+            )
+            .toList();
+
+        inferenceRequests.forEach(
+            request -> executeAsyncWithOrigin(client, ML_ORIGIN, InferenceAction.INSTANCE, request, gal.delegateFailureAndWrap((l, r) -> {
+                String inferenceId = request.getInferenceEntityId();
+                InferenceResults inferenceResults = validateAndConvertInferenceResults(r.getResults(), inferenceId);
+                l.onResponse(Tuple.tuple(inferenceId, inferenceResults));
+            }))
+        );
+    }
+
     private static List<String> getDefaultFields(Settings settings) {
         return settings.getAsList(DEFAULT_FIELD_SETTING.getKey(), DEFAULT_FIELD_SETTING.getDefault(settings));
+    }
+
+    private static InferenceResults validateAndConvertInferenceResults(
+        InferenceServiceResults inferenceServiceResults,
+        String inferenceId
+    ) {
+        List<? extends InferenceResults> inferenceResultsList = inferenceServiceResults.transformToCoordinationFormat();
+        if (inferenceResultsList.isEmpty()) {
+            return new ErrorInferenceResults(
+                new IllegalArgumentException("No inference results retrieved for inference ID [" + inferenceId + "]")
+            );
+        } else if (inferenceResultsList.size() > 1) {
+            // We don't chunk queries, so there should always be one inference result.
+            // Thus, if we receive more than one inference result, it is a server-side error.
+            return new ErrorInferenceResults(
+                new IllegalStateException(
+                    inferenceResultsList.size() + " inference results retrieved for inference ID [" + inferenceId + "]"
+                )
+            );
+        }
+
+        return inferenceResultsList.getFirst();
     }
 }

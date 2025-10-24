a552daa659496ba2aba80de7989f107f57f122f4
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2025-10-24 10:14:39 +0200

    Refactor MlAssignmentPlannerUpgradeIT to include deployment priority in startDeployment method

--- x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/MlAssignmentPlannerUpgradeIT.java
+++ x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/MlAssignmentPlannerUpgradeIT.java
@@ -187,12 +187,12 @@ public class MlAssignmentPlannerUpgradeIT extends AbstractUpgradeTestCase {
         createTrainedModel("old_memory_format", 0, 0);
         putModelDefinition("old_memory_format");
         putVocabulary(List.of("these", "are", "my", "words"), "old_memory_format");
-        startDeployment("old_memory_format");
+        startDeployment("old_memory_format", "started", "low");
 
         createTrainedModel("new_memory_format", ByteSizeValue.ofMb(300).getBytes(), ByteSizeValue.ofMb(10).getBytes());
         putModelDefinition("new_memory_format");
         putVocabulary(List.of("these", "are", "my", "words"), "new_memory_format");
-        startDeployment("new_memory_format");
+        startDeployment("new_memory_format", "started", "low");
     }
 
     private void cleanupDeployments() throws IOException {
@@ -248,10 +248,14 @@ public class MlAssignmentPlannerUpgradeIT extends AbstractUpgradeTestCase {
     }
 
     private Response startDeployment(String modelId) throws IOException {
-        return startDeployment(modelId, "started");
+        return startDeployment(modelId, "started", "normal");
     }
 
     private Response startDeployment(String modelId, String waitForState) throws IOException {
+        return startDeployment(modelId, waitForState, "normal");
+    }
+
+    private Response startDeployment(String modelId, String waitForState, String priority) throws IOException {
         String inferenceThreadParamName = "threads_per_allocation";
         String modelThreadParamName = "number_of_allocations";
         String compatibleHeader = null;
@@ -271,7 +275,8 @@ public class MlAssignmentPlannerUpgradeIT extends AbstractUpgradeTestCase {
                 + inferenceThreadParamName
                 + "=1&"
                 + modelThreadParamName
-                + "=1"
+                + "=1&priority="
+                + priority
         );
         if (compatibleHeader != null) {
             request.setOptions(request.getOptions().toBuilder().addHeader("Accept", compatibleHeader).build());

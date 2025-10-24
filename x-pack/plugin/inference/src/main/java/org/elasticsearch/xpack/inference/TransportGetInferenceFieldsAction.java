ad5b8f2ab90f09aa2a406595e76300a783490b97
Mike Pellegrini <mike.pellegrini@elastic.co>
2025-10-24 14:09:28 -0400

    Build the inference fields map

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/TransportGetInferenceFieldsAction.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/TransportGetInferenceFieldsAction.java
@@ -11,22 +11,42 @@ import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.HandledTransportAction;
 import org.elasticsearch.client.internal.Client;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.cluster.metadata.InferenceFieldMetadata;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.regex.Regex;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.injection.guice.Inject;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.transport.TransportService;
 import org.elasticsearch.xpack.core.inference.action.GetInferenceFieldsAction;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static org.elasticsearch.index.IndexSettings.DEFAULT_FIELD_SETTING;
+
+// TODO: Handle multi-project
+// TODO: Don't hard-code useDefaultFields
 
 public class TransportGetInferenceFieldsAction extends HandledTransportAction<
     GetInferenceFieldsAction.Request,
     GetInferenceFieldsAction.Response> {
 
+    private final ClusterService clusterService;
     private final Client client;
 
     @Inject
-    public TransportGetInferenceFieldsAction(TransportService transportService, ActionFilters actionFilters, Client client) {
+    public TransportGetInferenceFieldsAction(
+        TransportService transportService,
+        ActionFilters actionFilters,
+        ClusterService clusterService,
+        Client client
+    ) {
         super(
             GetInferenceFieldsAction.NAME,
             transportService,
@@ -34,6 +54,7 @@ public class TransportGetInferenceFieldsAction extends HandledTransportAction<
             GetInferenceFieldsAction.Request::new,
             EsExecutors.DIRECT_EXECUTOR_SERVICE
         );
+        this.clusterService = clusterService;
         this.client = client;
     }
 
@@ -43,6 +64,56 @@ public class TransportGetInferenceFieldsAction extends HandledTransportAction<
         GetInferenceFieldsAction.Request request,
         ActionListener<GetInferenceFieldsAction.Response> listener
     ) {
-        listener.onResponse(new GetInferenceFieldsAction.Response(Map.of(), Map.of()));
+        final List<String> indices = request.getIndices();
+        final List<String> fields = request.getFields();
+        final boolean resolveWildcards = request.resolveWildcards();
+
+        Map<String, List<InferenceFieldMetadata>> inferenceFieldsMap = new HashMap<>(indices.size());
+        indices.forEach(index -> {
+            List<InferenceFieldMetadata> inferenceFieldMetadataList = getInferenceFieldMetadata(index, fields, resolveWildcards, false);
+            if (inferenceFieldMetadataList != null) {
+                inferenceFieldsMap.put(index, inferenceFieldMetadataList);
+            }
+        });
+
+        listener.onResponse(new GetInferenceFieldsAction.Response(inferenceFieldsMap, Map.of()));
+    }
+
+    private List<InferenceFieldMetadata> getInferenceFieldMetadata(
+        String index,
+        List<String> fields,
+        boolean resolveWildcards,
+        boolean useDefaultFields
+    ) {
+        ClusterState clusterState = clusterService.state();
+        IndexMetadata indexMetadata = clusterState.getMetadata().getProject().indices().get(index);
+        if (indexMetadata == null) {
+            return null;
+        }
+
+        Map<String, InferenceFieldMetadata> inferenceFieldsMap = indexMetadata.getInferenceFields();
+        List<InferenceFieldMetadata> inferenceFieldMetadataList = new ArrayList<>();
+        List<String> effectiveFields = fields.isEmpty() && useDefaultFields ? getDefaultFields(indexMetadata.getSettings()) : fields;
+        for (String field : effectiveFields) {
+            if (inferenceFieldsMap.containsKey(field)) {
+                // No wildcards in field name
+                inferenceFieldMetadataList.add(inferenceFieldsMap.get(field));
+            } else if (resolveWildcards) {
+                if (Regex.isMatchAllPattern(field)) {
+                    inferenceFieldMetadataList.addAll(inferenceFieldsMap.values());
+                } else if (Regex.isSimpleMatchPattern(field)) {
+                    inferenceFieldsMap.values()
+                        .stream()
+                        .filter(ifm -> Regex.simpleMatch(field, ifm.getName()))
+                        .forEach(inferenceFieldMetadataList::add);
+                }
+            }
+        }
+
+        return inferenceFieldMetadataList;
+    }
+
+    private static List<String> getDefaultFields(Settings settings) {
+        return settings.getAsList(DEFAULT_FIELD_SETTING.getKey(), DEFAULT_FIELD_SETTING.getDefault(settings));
     }
 }

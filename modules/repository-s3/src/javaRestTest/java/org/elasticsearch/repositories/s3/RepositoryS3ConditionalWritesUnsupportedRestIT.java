746b7031d885ddcb5e02d16e2ca6fba9192add31
David Turner <david.turner@elastic.co>
2025-10-27 10:01:57 +0000

    Optionally make S3 writes unconditional
    As of #133030 Elasticsearch uses S3's support for conditional writes to
    protect against repository corruption. It is possible that some other
    storage system may claim to be fully S3-compatible despite rejecting
    such requests.
    
    This commit adds a repository setting that allows to make all writes
    unconditional, unsafely disabling the corruption protection, but
    allowing users some time to work with their storage supplier to address
    the incompatibility.

---
+++ modules/repository-s3/src/javaRestTest/java/org/elasticsearch/repositories/s3/RepositoryS3ConditionalWritesUnsupportedRestIT.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+package org.elasticsearch.repositories.s3;
+
+import fixture.aws.DynamicRegionSupplier;
+import fixture.s3.S3HttpFixture;
+import fixture.s3.S3HttpHandler;
+
+import com.carrotsearch.randomizedtesting.annotations.ThreadLeakFilters;
+import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
+import com.sun.net.httpserver.HttpHandler;
+
+import org.elasticsearch.ExceptionsHelper;
+import org.elasticsearch.common.ReferenceDocs;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.io.Streams;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.test.cluster.ElasticsearchCluster;
+import org.elasticsearch.test.cluster.LogType;
+import org.elasticsearch.test.fixtures.testcontainers.TestContainersThreadFilter;
+import org.hamcrest.Matchers;
+import org.junit.ClassRule;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+
+import java.io.IOException;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+import static fixture.aws.AwsCredentialsUtils.fixedAccessKey;
+
+@ThreadLeakFilters(filters = { TestContainersThreadFilter.class })
+@ThreadLeakScope(ThreadLeakScope.Scope.NONE) // https://github.com/elastic/elasticsearch/issues/102482
+public class RepositoryS3ConditionalWritesUnsupportedRestIT extends AbstractRepositoryS3RestTestCase {
+
+    private static final String PREFIX = getIdentifierPrefix("RepositoryS3BasicCredentialsRestIT");
+    private static final String BUCKET = PREFIX + "bucket";
+    private static final String BASE_PATH = PREFIX + "base_path";
+    private static final String ACCESS_KEY = PREFIX + "access-key";
+    private static final String SECRET_KEY = PREFIX + "secret-key";
+    private static final String CLIENT = "no_conditional_writes_client";
+
+    private static final Supplier<String> regionSupplier = new DynamicRegionSupplier();
+    private static final S3HttpFixture s3Fixture = new S3HttpFixture(
+        true,
+        BUCKET,
+        BASE_PATH,
+        fixedAccessKey(ACCESS_KEY, regionSupplier, "s3")
+    ) {
+        @Override
+        protected HttpHandler createHandler() {
+            final var delegateHandler = asInstanceOf(S3HttpHandler.class, super.createHandler());
+            return exchange -> {
+                if (exchange.getRequestHeaders().containsKey("if-match") || exchange.getRequestHeaders().containsKey("if-none-match")) {
+                    final var exception = new AssertionError(
+                        Strings.format(
+                            "unsupported conditional write: [%s] with headers [%s]",
+                            delegateHandler.parseRequest(exchange),
+                            exchange.getRequestHeaders()
+                        )
+                    );
+                    ExceptionsHelper.maybeDieOnAnotherThread(exception);
+                    throw exception;
+                }
+                delegateHandler.handle(exchange);
+            };
+        }
+    };
+
+    @Override
+    protected Settings extraRepositorySettings() {
+        return Settings.builder()
+            .put(super.extraRepositorySettings())
+            .put(S3Repository.UNSAFELY_INCOMPATIBLE_WITH_S3_WRITES.getKey(), true)
+            .build();
+    }
+
+    public static ElasticsearchCluster cluster = ElasticsearchCluster.local()
+        .module("repository-s3")
+        .systemProperty("aws.region", regionSupplier)
+        .keystore("s3.client." + CLIENT + ".access_key", ACCESS_KEY)
+        .keystore("s3.client." + CLIENT + ".secret_key", SECRET_KEY)
+        .setting("s3.client." + CLIENT + ".endpoint", s3Fixture::getAddress)
+        .build();
+
+    @ClassRule
+    public static TestRule ruleChain = RuleChain.outerRule(s3Fixture).around(cluster);
+
+    @Override
+    protected String getTestRestCluster() {
+        return cluster.getHttpAddresses();
+    }
+
+    @Override
+    protected String getBucketName() {
+        return BUCKET;
+    }
+
+    @Override
+    protected String getBasePath() {
+        return BASE_PATH;
+    }
+
+    @Override
+    protected String getClientName() {
+        return CLIENT;
+    }
+
+    public void testWarningLog() throws IOException {
+        final var repoName = randomIdentifier();
+        final var testRepository = new TestRepository(repoName, getClientName(), getBucketName(), getBasePath(), extraRepositorySettings());
+        try (var ignored = testRepository.register(UnaryOperator.identity()); var logStream = cluster.getNodeLog(0, LogType.SERVER)) {
+            assertThat(
+                Streams.readAllLines(logStream),
+                Matchers.hasItem(
+                    Matchers.allOf(
+                        Matchers.containsString("WARN"),
+                        Matchers.containsString(repoName),
+                        Matchers.containsString("""
+                            is configured to unsafely avoid conditional writes which may lead to repository corruption; to resolve this \
+                            warning, upgrade your storage to a system that is fully compatible with AWS S3 and then remove the \
+                            [unsafely_incompatible_with_s3_conditional_writes] repository setting"""),
+                        Matchers.containsString(ReferenceDocs.S3_COMPATIBLE_REPOSITORIES.toString())
+                    )
+                )
+            );
+        }
+    }
+}

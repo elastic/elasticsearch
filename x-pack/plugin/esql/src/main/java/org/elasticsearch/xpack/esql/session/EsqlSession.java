230456962f93bbd4a8259015ba4628d22d12b974
Alexander Spies <alexander.spies@elastic.co>
2025-10-27 10:08:59 +0100

    ESQL: Add a bit of javadoc to preanalysis methods (#137099)
    Explain where minimum transport version is determined for a given query.

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
@@ -550,9 +550,10 @@ public class EsqlSession {
             executionInfo
         );
 
-        // The main index pattern dictates on which nodes the query can be executed, so we use the minimum transport version from this field
-        // caps request.
         SubscribableListener.<PreAnalysisResult>newForked(
+            // The main index pattern dictates on which nodes the query can be executed, so we use the minimum transport version from this
+            // field
+            // caps request.
             l -> preAnalyzeMainIndices(preAnalysis.indexes().entrySet().iterator(), preAnalysis, executionInfo, result, requestFilter, l)
         ).andThenApply(r -> {
             if (r.indexResolution.isEmpty() == false // Rule out ROW case with no FROM clauses
@@ -576,6 +577,9 @@ public class EsqlSession {
             .addListener(logicalPlanListener);
     }
 
+    /**
+     * Perform a field caps request for each lookup index. Does not update the minimum transport version.
+     */
     private void preAnalyzeLookupIndices(
         Iterator<IndexPattern> lookupIndices,
         PreAnalysisResult preAnalysisResult,
@@ -796,6 +800,10 @@ public class EsqlSession {
         });
     }
 
+    /**
+     * Perform a field caps request for each index pattern and determine the minimum transport version of all clusters with matching
+     * indices.
+     */
     private void preAnalyzeMainIndices(
         Iterator<Map.Entry<IndexPattern, IndexMode>> indexPatterns,
         PreAnalyzer.PreAnalysis preAnalysis,

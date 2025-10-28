f93d906ceff372537748821234373d7ff76f5b11
Ben Chaplin <benchaplin@protonmail.com>
2025-10-28 17:48:35 -0400

    Fix

--- server/src/main/java/org/elasticsearch/action/search/SearchQueryThenFetchAsyncAction.java
+++ server/src/main/java/org/elasticsearch/action/search/SearchQueryThenFetchAsyncAction.java
@@ -599,7 +599,6 @@ public class SearchQueryThenFetchAsyncAction extends AbstractSearchAsyncAction<S
                             }
                             onPhaseFailure(getName(), "", cause);
                         }
-                        onNodeQueryFailure(e, request, routing);
                     }
                 });
         });

c02b9455b9495bbe80353b1897c0613281ccb281
Pablo Fco. Pérez Hidalgo <contact@pablofranciscoperez.info>
2025-10-29 00:18:08 +0100

    Add ASCII to capabilities list

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1529,7 +1529,12 @@ public class EsqlCapabilities {
         /**
          * Fix double release in inline stats when LocalRelation is reused
          */
-        INLINE_STATS_DOUBLE_RELEASE_FIX(INLINESTATS_V11.enabled)
+        INLINE_STATS_DOUBLE_RELEASE_FIX(INLINESTATS_V11.enabled),
+
+        /**
+         * Support for string function ASCII
+         */
+        ASCII
 
         ;
 

20b202d20db08adc9eed0f569f55c343f4b72931
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-10-30 19:09:02 +0100

    Introduce base class for Match and MatchPhrase

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/rules/logical/FoldNull.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/rules/logical/FoldNull.java
@@ -6,6 +6,7 @@
  */
 
 package org.elasticsearch.xpack.esql.optimizer.rules.logical;
+
 import org.elasticsearch.xpack.esql.core.expression.Alias;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
 import org.elasticsearch.xpack.esql.core.expression.Expressions;

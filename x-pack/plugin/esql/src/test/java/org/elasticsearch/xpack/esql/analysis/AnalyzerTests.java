97e4ff34c167757aa9cd8436caf37b07cda8b14b
Nhat Nguyen <nhat.nguyen@elastic.co>
2025-10-30 12:34:43 -0700

    Add window to aggregation function (#137344)
    This change adds a window parameter to AggregateFunction, similar to the 
    existing filter parameter. The window parameter is optional but must
    never be null. This PR also generalizes AggregateFunction to be composed
    of [source, field, filter, window, extra parameters] , with all extra
    parameters placed after filter and window.
    
    The implementation of the window function will be added in a follow-up 
    and will initially be available only for time-series aggregations such
    as rate, avg_over_time, etc.

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/analysis/AnalyzerTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/analysis/AnalyzerTests.java
@@ -1448,7 +1448,7 @@ public class AnalyzerTests extends ESTestCase {
         assertThat(output, hasSize(2));
         var aggs = agg.aggregates();
         var min = as(Alias.unwrap(aggs.get(0)), Min.class);
-        assertThat(min.arguments(), hasSize(2));    // field + filter
+        assertThat(min.arguments(), hasSize(3));    // field + filter + window
         var group = Alias.unwrap(agg.groupings().get(0));
         assertEquals(min.arguments().get(0), group);
     }
@@ -1470,7 +1470,7 @@ public class AnalyzerTests extends ESTestCase {
         assertThat(output, hasSize(2));
         var aggs = agg.aggregates();
         var min = as(Alias.unwrap(aggs.get(0)), Min.class);
-        assertThat(min.arguments(), hasSize(2));    // field + filter
+        assertThat(min.arguments(), hasSize(3));    // field + filter
         assertEquals(Expressions.attribute(min.arguments().get(0)), Expressions.attribute(agg.groupings().get(0)));
     }
 

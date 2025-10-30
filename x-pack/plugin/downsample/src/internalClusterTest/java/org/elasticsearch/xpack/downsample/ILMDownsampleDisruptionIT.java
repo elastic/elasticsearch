c32d7f75b6ca743575ca3f58f61fa4dcc0298375
gmarouli <mary.gouseti@elastic.co>
2025-10-30 14:34:34 +0200

    Nits

--- x-pack/plugin/downsample/src/internalClusterTest/java/org/elasticsearch/xpack/downsample/ILMDownsampleDisruptionIT.java
+++ x-pack/plugin/downsample/src/internalClusterTest/java/org/elasticsearch/xpack/downsample/ILMDownsampleDisruptionIT.java
@@ -25,7 +25,7 @@ import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramInterval;
 import org.elasticsearch.test.ESIntegTestCase;
 import org.elasticsearch.test.InternalTestCluster;
-import org.elasticsearch.test.junit.annotations.TestLogging;
+import org.elasticsearch.test.junit.annotations.TestIssueLogging;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentFactory;
 import org.elasticsearch.xpack.aggregatemetric.AggregateMetricMapperPlugin;
@@ -130,9 +130,9 @@ public class ILMDownsampleDisruptionIT extends DownsamplingIntegTestCase {
         assertAcked(client().execute(ILMActions.PUT, putLifecycleRequest).actionGet());
     }
 
-    @TestLogging(
-        value = "logger.org.elasticsearch.cluster.service.MasterService:TRACE",
-        reason = "https://github.com/elastic/elasticsearch/issues/136585"
+    @TestIssueLogging(
+        value = "org.elasticsearch.cluster.service.MasterService:TRACE",
+        issueUrl = "https://github.com/elastic/elasticsearch/issues/136585"
     )
     public void testILMDownsampleRollingRestart() throws Exception {
         final InternalTestCluster cluster = internalCluster();

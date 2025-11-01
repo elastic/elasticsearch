915c9f286034ea19b44189f307befa3c7fe77516
Andrei Stefan <andrei@elastic.co>
2025-11-01 19:01:22 +0200

    wip

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/rules/logical/PushDownAndCombineFilters.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/rules/logical/PushDownAndCombineFilters.java
@@ -104,7 +104,7 @@ public final class PushDownAndCombineFilters extends OptimizerRules.Parameterize
         } else if (child instanceof OrderBy orderBy) {
             // swap the filter with its child
             plan = orderBy.replaceChild(filter.with(orderBy.child(), condition));
-        } else if (child instanceof Join join && child instanceof InlineJoin == false) {
+        } else if (child instanceof Join join) {
             // TODO: could we do better here about pushing down filters for inline stats?
             // See also https://github.com/elastic/elasticsearch/issues/127497
             // Push down past INLINE STATS if the condition is on the groupings
@@ -135,6 +135,32 @@ public final class PushDownAndCombineFilters extends OptimizerRules.Parameterize
         return new ScopedFilter(rest, leftFilters, rightFilters);
     }
 
+    // split the filter condition in 2 parts:
+    // 1. filters that can be applied only to the right
+    // 2. filters that can be applied to both sides
+    // 3. filters that can be applied only to the left
+    private static ScopedFilter scopeInlineStatsFilter(List<Expression> filters, LogicalPlan left, LogicalPlan right) {
+        List<Expression> rightFilters = new ArrayList<>();
+        List<Expression> bothSides = new ArrayList<>();
+        List<Expression> leftFilters = new ArrayList<>(filters);
+
+        AttributeSet leftOutput = left.outputSet();
+        AttributeSet rightOutput = right.outputSet();
+
+        leftFilters.removeIf(f -> {
+            if (f.references().subsetOf(rightOutput)) {
+                if (f.references().subsetOf(leftOutput)) {
+                    bothSides.add(f);
+                } else {
+                    rightFilters.add(f);
+                }
+                return true;
+            }
+            return false;
+        });
+        return new ScopedFilter(bothSides, leftFilters, rightFilters);
+    }
+
     private static LogicalPlan pushDownPastJoin(Filter filter, Join join, FoldContext foldCtx) {
         LogicalPlan plan = filter;
         // pushdown only through LEFT joins
@@ -143,28 +169,41 @@ public final class PushDownAndCombineFilters extends OptimizerRules.Parameterize
             LogicalPlan left = join.left();
             LogicalPlan right = join.right();
 
-            // split the filter condition in 3 parts:
-            // 1. filter scoped to the left
-            // 2. filter scoped to the right
-            // 3. filter that requires both sides to be evaluated
-            ScopedFilter scoped = scopeFilter(Predicates.splitAnd(filter.condition()), left, right);
+            // Split the filter condition in 3 parts.
+            // For InlineJoin we use a scoping that allows pushing down filters either to right side only or to both sides.
+            // For the rest of the joins we use the standard scoping:
+            //  - filters scoped to the left
+            //  - filters scoped to the right
+            //  - filter that requires both sides to be evaluated
+            ScopedFilter scoped = join instanceof InlineJoin
+                ? scopeInlineStatsFilter(Predicates.splitAnd(filter.condition()), left, right)
+                : scopeFilter(Predicates.splitAnd(filter.condition()), left, right);
+
+            var pushableToLeftSide = join instanceof InlineJoin ? scoped.commonFilters() : scoped.leftFilters();
+            var pushableToRightSide = scoped.rightFilters();
+            var commonFilters = join instanceof InlineJoin ? scoped.leftFilters() : scoped.commonFilters();
             boolean optimizationApplied = false;
             // push the left scoped filter down to the left child
-            if (scoped.leftFilters.size() > 0) {
+            if (pushableToLeftSide.size() > 0) {
                 // push the filter down to the left child
-                left = new Filter(left.source(), left, Predicates.combineAnd(scoped.leftFilters));
+                left = new Filter(left.source(), left, Predicates.combineAnd(pushableToLeftSide));
                 // update the join with the new left child
                 join = (Join) join.replaceLeft(left);
                 // we completely applied the left filters, so we can remove them from the scoped filters
-                scoped = new ScopedFilter(scoped.commonFilters(), List.of(), scoped.rightFilters);
+                if (join instanceof InlineJoin) {
+                    // for InlineJoin the left filters are the common filters
+                    scoped = new ScopedFilter(List.of(), scoped.leftFilters, scoped.rightFilters);
+                } else {
+                    scoped = new ScopedFilter(scoped.commonFilters(), List.of(), scoped.rightFilters);
+                }
                 optimizationApplied = true;
             }
             // push the right scoped filter down to the right child
             // We check if each AND component of the filter is already part of the right side filter before we add it
             // In the future, this optimization can apply to other types of joins as well such as InlineJoin
             // but for now we limit it to LEFT joins only, till filters are supported for other join types
-            if (scoped.rightFilters().isEmpty() == false) {
-                List<Expression> rightPushableFilters = buildRightPushableFilters(scoped.rightFilters(), foldCtx);
+            if (pushableToRightSide.isEmpty() == false) {
+                List<Expression> rightPushableFilters = buildRightPushableFilters(pushableToRightSide, foldCtx);
                 if (rightPushableFilters.isEmpty() == false) {
                     if (join.right() instanceof Filter existingRightFilter) {
                         // merge the unique AND filter components from rightPushableFilters and existingRightFilter.condition()

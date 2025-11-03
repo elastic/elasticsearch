4a4c0600e12e04cfaa46a61ceda1a04de1e312aa
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-11-03 14:02:44 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/index/query/BoostingQueryBuilder.java
+++ server/src/main/java/org/elasticsearch/index/query/BoostingQueryBuilder.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Stream;
 
 /**
  * The BoostingQuery class can be used to effectively demote results that match a given query.

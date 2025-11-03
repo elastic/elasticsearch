6cb2ea3d7407b39b054e9cd9cc142fb14a2e7817
Felix Barnsteiner <felixbarny@users.noreply.github.com>
2025-11-03 19:18:51 +0100

    Support for quoted strings and ?params (#137502)

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/logical/promql/PromqlParams.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/logical/promql/PromqlParams.java
@@ -19,9 +19,9 @@ import java.time.Instant;
  * These can be specified in the {@linkplain org.elasticsearch.xpack.esql.plan.logical.promql.PromqlCommand PROMQL command} like so:
  * <pre>
  *     # instant query
- *     PROMQL time `2025-10-31T00:00:00Z` (avg(foo))
+ *     PROMQL time "2025-10-31T00:00:00Z" (avg(foo))
  *     # range query with explicit start and end
- *     PROMQL start `2025-10-31T00:00:00Z` end `2025-10-31T01:00:00Z` step 1m (avg(foo))
+ *     PROMQL start "2025-10-31T00:00:00Z" end "2025-10-31T01:00:00Z" step 1m (avg(foo))
  *     # range query with implicit time bounds, doesn't support calling {@code start()} or {@code end()} functions
  *     PROMQL step 5m (avg(foo))
  * </pre>
--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/analysis/promql/PromqlVerifierTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/analysis/promql/PromqlVerifierTests.java
@@ -32,6 +32,16 @@ public class PromqlVerifierTests extends ESTestCase {
         );
     }
 
+    public void testPromqlStepAndRangeMisaligned() {
+        assertThat(
+            error("""
+                TS test | PROMQL step 1m (
+                  avg(rate(network.bytes_in[5m]))
+                )""", tsdb),
+            equalTo("2:29: the duration for range vector selector [5m] must be equal to the query's step for range queries at this time")
+        );
+    }
+
     public void testPromqlIllegalNameLabelMatcher() {
         assertThat(
             error("TS test | PROMQL step 5m (avg({__name__=~\"*.foo.*\"}))", tsdb),
@@ -39,13 +49,15 @@ public class PromqlVerifierTests extends ESTestCase {
         );
     }
 
-    @Ignore
     public void testPromqlSubquery() {
-        // TODO doesn't parse
-        // line 1:36: Invalid query 'network.bytes_in'[ValueExpressionContext] given; expected Expression but found
-        // InstantSelector
-        assertThat(error("TS test | PROMQL step 5m (avg(rate(network.bytes_in[5m:])))", tsdb), equalTo(""));
-        assertThat(error("TS test | PROMQL step 5m (avg(rate(network.bytes_in[5m:1m])))", tsdb), equalTo(""));
+        assertThat(
+            error("TS test | PROMQL step 5m (avg(rate(network.bytes_in[5m:])))", tsdb),
+            equalTo("1:36: subqueries are not supported at this time [network.bytes_in[5m:]]")
+        );
+        assertThat(
+            error("TS test | PROMQL step 5m (avg(rate(network.bytes_in[5m:1m])))", tsdb),
+            equalTo("1:36: subqueries are not supported at this time [network.bytes_in[5m:1m]]")
+        );
     }
 
     @Ignore

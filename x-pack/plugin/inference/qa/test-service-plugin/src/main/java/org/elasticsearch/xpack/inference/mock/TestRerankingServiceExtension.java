2adfc901fb2873007fba9ab60e27d5f2c96649fd
Donal Evans <donal.evans@elastic.co>
2025-11-03 13:35:06 -0800

    [ML] Do not create inference endpoint if ID is used in existing mappings (#137055)
    When creating an inference endpoint, if the inference ID is used in
    incompatible semantic_text mappings, prevent the endpoint from
    being created.
    
    Closes #124272
    
    - Check if existing semantic text fields have compatible model settings
    - Update and expand test coverage for the new behaviour
    - Improve existing test InferenceServiceExtension implementations
    - Move SemanticTextInfoExtractor from xpack.core.ml.utils to
      xpack.inference.common
    
    (cherry picked from commit 635fda1212c386e7d21a1bcf39e97820178087c2)
    
    # Conflicts:
    #	x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/utils/SemanticTextInfoExtractor.java
    #	x-pack/plugin/inference/qa/test-service-plugin/src/main/java/org/elasticsearch/xpack/inference/mock/TestDenseInferenceServiceExtension.java
    #	x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/action/TransportDeleteInferenceEndpointAction.java
    #	x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/action/TransportPutInferenceModelAction.java
    #	x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/LocalStateInferencePlugin.java

--- x-pack/plugin/inference/qa/test-service-plugin/src/main/java/org/elasticsearch/xpack/inference/mock/TestRerankingServiceExtension.java
+++ x-pack/plugin/inference/qa/test-service-plugin/src/main/java/org/elasticsearch/xpack/inference/mock/TestRerankingServiceExtension.java
@@ -300,7 +300,10 @@ public class TestRerankingServiceExtension implements InferenceServiceExtension 
             String model = (String) map.remove("model_id");
 
             if (model == null) {
-                validationException.addValidationError("missing model");
+                model = (String) map.remove("model");
+                if (model == null) {
+                    validationException.addValidationError("missing model");
+                }
             }
 
             if (validationException.validationErrors().isEmpty() == false) {

c09e00a20ad75b94ae12d8e06e80d4bde14f55a3
Pawan Kartik <pawankartik.chitrapu@elastic.co>
2025-11-04 14:29:58 +0000

    Copy project routing during init

--- server/src/main/java/org/elasticsearch/action/search/SearchRequest.java
+++ server/src/main/java/org/elasticsearch/action/search/SearchRequest.java
@@ -209,6 +209,7 @@ public class SearchRequest extends LegacyActionRequest implements IndicesRequest
         }
         final SearchRequest request = new SearchRequest(originalSearchRequest, indices, clusterAlias, absoluteStartMillis, finalReduce);
         request.setParentTask(parentTaskId);
+        request.setProjectRouting(originalSearchRequest.getProjectRouting());
         return request;
     }
 
@@ -238,6 +239,7 @@ public class SearchRequest extends LegacyActionRequest implements IndicesRequest
         this.waitForCheckpoints = searchRequest.waitForCheckpoints;
         this.waitForCheckpointsTimeout = searchRequest.waitForCheckpointsTimeout;
         this.forceSyntheticSource = searchRequest.forceSyntheticSource;
+        this.projectRouting = searchRequest.projectRouting;
     }
 
     /**

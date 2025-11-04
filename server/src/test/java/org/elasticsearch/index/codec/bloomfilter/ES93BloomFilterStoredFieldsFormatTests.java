537d956fc28634c78dce6f6838d2d36851a3b54b
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2025-11-04 20:24:57 +0100

    Add support for merges in ES93BloomFilterStoredFieldsFormat

--- server/src/test/java/org/elasticsearch/index/codec/bloomfilter/ES93BloomFilterStoredFieldsFormatTests.java
+++ server/src/test/java/org/elasticsearch/index/codec/bloomfilter/ES93BloomFilterStoredFieldsFormatTests.java
@@ -16,11 +16,14 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.LongField;
 import org.apache.lucene.document.StringField;
+import org.apache.lucene.index.FilterMergePolicy;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.index.SegmentCommitInfo;
 import org.apache.lucene.index.SegmentInfo;
+import org.apache.lucene.index.SegmentInfos;
 import org.apache.lucene.index.SegmentReader;
 import org.apache.lucene.index.StandardDirectoryReader;
 import org.apache.lucene.store.IOContext;
@@ -91,45 +94,109 @@ public class ES93BloomFilterStoredFieldsFormatTests extends BaseStoredFieldsForm
             // We don't use the RandomIndexWriter because we want to control the settings so we get
             // deterministic test runs
             try (IndexWriter writer = new IndexWriter(directory, conf)) {
-                List<BytesRef> indexedIds = new ArrayList<>();
-                var docCount = atLeast(50);
-                for (int i = 0; i < docCount; i++) {
-                    Document doc = new Document();
-                    var id = getBytesRefFromString(UUIDs.randomBase64UUID());
-                    indexedIds.add(id);
-                    doc.add(new StringField(IdFieldMapper.NAME, id, Field.Store.YES));
-                    doc.add(new StringField("host", "host-" + i, Field.Store.YES));
-                    doc.add(new LongField("counter", i, Field.Store.YES));
-                    writer.addDocument(doc);
-                }
+                List<BytesRef> indexedIds = indexDocs(writer);
 
-                try (var directoryReader = StandardDirectoryReader.open(writer)) {
-                    for (LeafReaderContext leaf : directoryReader.leaves()) {
-                        try (ES93BloomFilterStoredFieldsFormat.BloomFilterProvider fieldReader = getBloomFilterProvider(leaf)) {
-                            var bloomFilter = fieldReader.getBloomFilter();
-                            // the bloom filter reader is null only if the _id field is not stored during indexing
-                            assertThat(bloomFilter, is(not(nullValue())));
+                assertBloomFilterTestsPositiveForExistingDocs(writer, indexedIds);
+            }
+        }
+    }
 
-                            for (BytesRef indexedId : indexedIds) {
-                                assertThat(bloomFilter.mayContainTerm(IdFieldMapper.NAME, indexedId), is(true));
+    public void testBloomFilterMerges() throws IOException {
+        try (var directory = newDirectory()) {
+            Analyzer analyzer = new MockAnalyzer(random());
+            IndexWriterConfig conf = newIndexWriterConfig(analyzer);
+            var randomBloomFilterSizes = random().nextBoolean();
+            conf.setCodec(new AssertingCodec() {
+                @Override
+                public StoredFieldsFormat storedFieldsFormat() {
+                    var bloomFilterSizeInKb = atLeast(2);
+                    return new ES93BloomFilterStoredFieldsFormat(
+                        BigArrays.NON_RECYCLING_INSTANCE,
+                        "",
+                        TestUtil.getDefaultCodec().storedFieldsFormat(),
+                        ByteSizeValue.ofKb(bloomFilterSizeInKb),
+                        IdFieldMapper.NAME
+                    ) {
+                        @Override
+                        int getBloomFilterSizeInBits() {
+                            if (randomBloomFilterSizes) {
+                                // Use different power of 2 values so we rebuild the bloom filter from the _id terms
+                                var bloomFilterSizeInBytes = ByteSizeValue.ofKb(1).getBytes() << atLeast(5);
+
+                                return ES93BloomFilterStoredFieldsFormat.closestPowerOfTwoBloomFilterSizeInBits(
+                                    ByteSizeValue.ofBytes(bloomFilterSizeInBytes)
+                                );
                             }
-                            assertThat(bloomFilter.mayContainTerm(IdFieldMapper.NAME, getBytesRefFromString("random")), is(false));
-
-                            assertThat(bloomFilter.mayContainTerm(IdFieldMapper.NAME, getBytesRefFromString("12345")), is(false));
+                            return super.getBloomFilterSizeInBits();
                         }
-                    }
-
-                    var storedFields = directoryReader.storedFields();
-                    for (int docId = 0; docId < docCount; docId++) {
-                        var document = storedFields.document(docId);
-                        // The _id field is not actually stored, just used to build the bloom filter
-                        assertThat(document.get(IdFieldMapper.NAME), nullValue());
-                        assertThat(document.get("host"), not(nullValue()));
-                        assertThat(document.get("host"), is(equalTo("host-" + docId)));
-                        assertThat(document.get("counter"), not(nullValue()));
-                        assertThat(document.getField("counter").storedValue().getLongValue(), is(equalTo((long) docId)));
-                    }
+                    };
                 }
+            });
+            conf.setMergePolicy(new FilterMergePolicy(newLogMergePolicy()) {
+                @Override
+                public boolean useCompoundFile(SegmentInfos infos, SegmentCommitInfo mergedInfo, MergeContext mergeContext) {
+                    return false;
+                }
+            });
+            conf.setMaxBufferedDocs(10);
+            // The stored fields reader that can be accessed through a StandardDirectoryReader wraps
+            // the ES93BloomFilterStoredFieldsFormat.Reader. Thus, we need to open it directly from
+            // the segment info codec and if we use compound files we would need to obtain a compound
+            // file directory. For simplicity, we just use regular files.
+            conf.setUseCompoundFile(false);
+            // We don't use the RandomIndexWriter because we want to control the settings so we get
+            // deterministic test runs
+            try (IndexWriter writer = new IndexWriter(directory, conf)) {
+                List<BytesRef> indexedIds = indexDocs(writer);
+
+                writer.forceMerge(1);
+
+                assertBloomFilterTestsPositiveForExistingDocs(writer, indexedIds);
+            }
+        }
+    }
+
+    private static List<BytesRef> indexDocs(IndexWriter writer) throws IOException {
+        List<BytesRef> indexedIds = new ArrayList<>();
+        var docCount = atLeast(50);
+        for (int i = 0; i < docCount; i++) {
+            Document doc = new Document();
+            var id = getBytesRefFromString(UUIDs.randomBase64UUID());
+            indexedIds.add(id);
+            doc.add(new StringField(IdFieldMapper.NAME, id, Field.Store.YES));
+            doc.add(new StringField("host", "host-" + i, Field.Store.YES));
+            doc.add(new LongField("counter", i, Field.Store.YES));
+            writer.addDocument(doc);
+        }
+        return indexedIds;
+    }
+
+    private void assertBloomFilterTestsPositiveForExistingDocs(IndexWriter writer, List<BytesRef> indexedIds) throws IOException {
+        try (var directoryReader = StandardDirectoryReader.open(writer)) {
+            for (LeafReaderContext leaf : directoryReader.leaves()) {
+                try (ES93BloomFilterStoredFieldsFormat.BloomFilterProvider fieldReader = getBloomFilterProvider(leaf)) {
+                    var bloomFilter = fieldReader.getBloomFilter();
+                    // the bloom filter reader is null only if the _id field is not stored during indexing
+                    assertThat(bloomFilter, is(not(nullValue())));
+
+                    for (BytesRef indexedId : indexedIds) {
+                        assertThat(bloomFilter.mayContainTerm(IdFieldMapper.NAME, indexedId), is(true));
+                    }
+                    assertThat(bloomFilter.mayContainTerm(IdFieldMapper.NAME, getBytesRefFromString("random")), is(false));
+
+                    assertThat(bloomFilter.mayContainTerm(IdFieldMapper.NAME, getBytesRefFromString("12345")), is(false));
+                }
+            }
+
+            var storedFields = directoryReader.storedFields();
+            for (int docId = 0; docId < indexedIds.size(); docId++) {
+                var document = storedFields.document(docId);
+                // The _id field is not actually stored, just used to build the bloom filter
+                assertThat(document.get(IdFieldMapper.NAME), nullValue());
+                assertThat(document.get("host"), not(nullValue()));
+                assertThat(document.get("host"), is(equalTo("host-" + docId)));
+                assertThat(document.get("counter"), not(nullValue()));
+                assertThat(document.getField("counter").storedValue().getLongValue(), is(equalTo((long) docId)));
             }
         }
     }

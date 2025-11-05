026cce73423c7c73b92f571c8c8e0d70c501109f
Adam Demjen <demjened@gmail.com>
2025-11-05 14:51:43 -0500

    Pass ES version in EIS inference request header

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/elastic/request/ElasticInferenceServiceRequest.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/elastic/request/ElasticInferenceServiceRequest.java
@@ -8,6 +8,7 @@
 package org.elasticsearch.xpack.inference.services.elastic.request;
 
 import org.apache.http.client.methods.HttpRequestBase;
+import org.elasticsearch.Version;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.xpack.inference.external.request.HttpRequest;
@@ -15,6 +16,7 @@ import org.elasticsearch.xpack.inference.external.request.Request;
 
 import java.util.Objects;
 
+import static org.elasticsearch.xpack.inference.InferencePlugin.X_ELASTIC_ES_VERSION;
 import static org.elasticsearch.xpack.inference.InferencePlugin.X_ELASTIC_PRODUCT_USE_CASE_HTTP_HEADER;
 
 public abstract class ElasticInferenceServiceRequest implements Request {
@@ -36,13 +38,18 @@ public abstract class ElasticInferenceServiceRequest implements Request {
 
         var productOrigin = metadata.productOrigin();
         var productUseCase = metadata.productUseCase();
+        var esVersion = metadata.esVersion();
 
         if (Objects.nonNull(productOrigin) && productOrigin.isEmpty() == false) {
-            request.setHeader(Task.X_ELASTIC_PRODUCT_ORIGIN_HTTP_HEADER, metadata.productOrigin());
+            request.setHeader(Task.X_ELASTIC_PRODUCT_ORIGIN_HTTP_HEADER, productOrigin);
         }
 
         if (Objects.nonNull(productUseCase) && productUseCase.isEmpty() == false) {
-            request.addHeader(X_ELASTIC_PRODUCT_USE_CASE_HTTP_HEADER, metadata.productUseCase());
+            request.addHeader(X_ELASTIC_PRODUCT_USE_CASE_HTTP_HEADER, productUseCase);
+        }
+
+        if (Objects.nonNull(esVersion) && esVersion.isEmpty() == false) {
+            request.addHeader(X_ELASTIC_ES_VERSION, esVersion);
         }
 
         return new HttpRequest(request, getInferenceEntityId());
@@ -55,7 +62,8 @@ public abstract class ElasticInferenceServiceRequest implements Request {
         // 'X-Elastic-Product-Use-Case' is Elastic Inference Service specific and is therefore not propagated through the ES-wide Task.
         return new ElasticInferenceServiceRequestMetadata(
             context.getHeader(Task.X_ELASTIC_PRODUCT_ORIGIN_HTTP_HEADER),
-            context.getHeader(X_ELASTIC_PRODUCT_USE_CASE_HTTP_HEADER)
+            context.getHeader(X_ELASTIC_PRODUCT_USE_CASE_HTTP_HEADER),
+            Version.CURRENT.toString()
         );
     }
 }

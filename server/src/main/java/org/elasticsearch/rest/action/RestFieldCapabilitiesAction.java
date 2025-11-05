9aeafd04ff8e77d548e0d5fce134bc60387d9d85
piergm <134913285+piergm@users.noreply.github.com>
2025-11-05 09:37:34 +0100

    iter

--- server/src/main/java/org/elasticsearch/rest/action/RestFieldCapabilitiesAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/RestFieldCapabilitiesAction.java
@@ -62,8 +62,9 @@ public class RestFieldCapabilitiesAction extends BaseRestHandler {
 
         final boolean crossProjectEnabled = crossProjectModeDecider.crossProjectEnabled();
         if (crossProjectModeDecider.crossProjectEnabled()) {
-            // accept project_routing param and set includeResolved to true
             fieldRequest.projectRouting(request.param("project_routing", null));
+            // Setting includeResolvedTo to always include index resolution data structure in the linked project responses,
+            // in order to allow the coordinating node to call CrossProjectIndexResolutionValidator#validate
             fieldRequest.includeResolvedTo(true);
         }
 

1ddbf15008611e71d62c5846919c2d57a672c723
Jan Kuipers <jan.kuipers@elastic.co>
2025-11-06 14:07:19 +0100

    Fix detecting stats query

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/rules/physical/local/PushStatsToSource.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/optimizer/rules/physical/local/PushStatsToSource.java
@@ -50,24 +50,23 @@ public class PushStatsToSource extends PhysicalOptimizerRules.ParameterizedOptim
             var tuple = pushableStats(aggregateExec, context);
 
             // for the moment support pushing count just for one field
-            List<EsStatsQueryExec.Stat> stats = tuple.v2();
-            if (stats.size() != 1 || stats.size() != aggregateExec.aggregates().size()) {
-                return aggregateExec;
-            }
-
             // TODO: handle case where some aggs cannot be pushed down by breaking the aggs into two sources (regular + stats) + union
             // use the stats since the attributes are larger in size (due to seen)
-            plan = new EsStatsQueryExec(
-                aggregateExec.source(),
-                queryExec.indexPattern(),
-                queryExec.query(),
-                queryExec.limit(),
-                tuple.v1(),
-                stats.get(0)
-            );
+            List<EsStatsQueryExec.Stat> stats = tuple.v2();
+            if (stats.size() == 1 && stats.size() == aggregateExec.aggregates().size()) {
+                plan = new EsStatsQueryExec(
+                    aggregateExec.source(),
+                    queryExec.indexPattern(),
+                    queryExec.query(),
+                    queryExec.limit(),
+                    tuple.v1(),
+                    stats.get(0)
+                );
+            }
         }
 
         // TODO: what is a good place to do something like this?
+        // TODO: this exception ends up in the execution info in the API response; fix that
         if (context.configuration().throwOnNonEsStatsQuery() && plan instanceof EsStatsQueryExec == false) {
             throw new UnsupportedOperationException("not executing query of type [" + plan.getClass().getSimpleName() + "]");
         }

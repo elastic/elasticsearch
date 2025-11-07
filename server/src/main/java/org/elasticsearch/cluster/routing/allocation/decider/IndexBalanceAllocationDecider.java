f6a1500c11d7868fde5ddc1674cbcfa89f2c6d99
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-11-07 18:50:31 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/cluster/routing/allocation/decider/IndexBalanceAllocationDecider.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/allocation/decider/IndexBalanceAllocationDecider.java
@@ -107,7 +107,8 @@ public class IndexBalanceAllocationDecider extends AllocationDecider {
         assert totalShards > 0;
 
         final double idealAllocation = Math.ceil((double) totalShards / eligibleNodes.size());
-        final int threshold = (totalShards + eligibleNodes.size() - 1 + indexBalanceConstraintSettings.getLoadSkewTolerance()) / eligibleNodes.size();
+        final int threshold = (totalShards + eligibleNodes.size() - 1 + indexBalanceConstraintSettings.getLoadSkewTolerance())
+            / eligibleNodes.size();
         final int currentAllocation = node.numberOfOwningShardsForIndex(index);
 
         if (currentAllocation >= threshold) {

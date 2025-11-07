691c5cd832858ecedccd92df3d8f82a4c6f69dc6
Nhat Nguyen <nhat.nguyen@elastic.co>
2025-11-07 12:10:57 -0800

    comment

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/DataNodeRequestSender.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/DataNodeRequestSender.java
@@ -546,8 +546,8 @@ abstract class DataNodeRequestSender {
                     .map(shard -> project.cluster().nodes().get(shard.currentNodeId()))
                     .toList();
             } catch (Exception ignored) {
-                // If the target index is deleted or the target shard is not found, then we skip resolving its new shard routing,
-                // and that shard won't be retried.
+                // If the target index is deleted or the target shard is not found after the query has started,
+                // we skip resolving its new shard routing, and that shard will not be retried.
                 continue;
             }
             nodes.put(shardId, allocatedNodes);

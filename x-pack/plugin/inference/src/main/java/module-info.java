f59ca21591907632aa107472f0b8aa99e2c9bd61
Pat Whelan <pat.whelan@elastic.co>
2025-11-07 10:56:45 -0500

    export module-info

--- x-pack/plugin/inference/src/main/java/module-info.java
+++ x-pack/plugin/inference/src/main/java/module-info.java
@@ -41,6 +41,7 @@ module org.elasticsearch.inference {
     exports org.elasticsearch.xpack.inference.registry;
     exports org.elasticsearch.xpack.inference.rest;
     exports org.elasticsearch.xpack.inference.services;
+    exports org.elasticsearch.xpack.inference.services.elastic.ccm;
     exports org.elasticsearch.xpack.inference;
     exports org.elasticsearch.xpack.inference.action.task;
     exports org.elasticsearch.xpack.inference.telemetry;

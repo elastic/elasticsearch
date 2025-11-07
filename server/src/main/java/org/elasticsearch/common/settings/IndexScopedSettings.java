544525f0628aa4c2e8ea8017bb2b068eba22786d
Martijn van Groningen <martijn.v.groningen@gmail.com>
2025-11-07 14:08:07 +0100

    oops

--- server/src/main/java/org/elasticsearch/common/settings/IndexScopedSettings.java
+++ server/src/main/java/org/elasticsearch/common/settings/IndexScopedSettings.java
@@ -207,7 +207,7 @@ public final class IndexScopedSettings extends AbstractScopedSettings {
                 IgnoredSourceFieldMapper.SKIP_IGNORED_SOURCE_READ_SETTING,
                 IndexSettings.INDEX_MAPPER_SOURCE_MODE_SETTING,
                 IndexSettings.RECOVERY_USE_SYNTHETIC_SOURCE_SETTING,
-                IndexSettings.TIME_SERIES_ES87TSDB_CODEC_ENABLED_SETTING,
+                IndexSettings.USE_TIME_SERIES_DOC_VALUES_CODEC_SETTING,
                 InferenceMetadataFieldsMapper.USE_LEGACY_SEMANTIC_TEXT_FORMAT,
 
                 // validate that built-in similarities don't get redefined

9a9bf3a9f74cb4fd0950080a9094bfeee6c8fe76
Simon Cooper <simon.cooper@elastic.co>
2025-11-10 17:17:35 +0000

    Revert "Add directIO and bfloat16 for bbq and unquantized HNSW vector field types (#135940)"
    This reverts commit 557aeaa72d2d3281dec5d4f7d70bbd3bf666d6f0.

--- server/src/main/java/org/elasticsearch/index/codec/vectors/es93/ES93HnswVectorsFormat.java
+++ server/src/main/java/org/elasticsearch/index/codec/vectors/es93/ES93HnswVectorsFormat.java
@@ -17,7 +17,6 @@ import org.apache.lucene.codecs.lucene99.Lucene99HnswVectorsWriter;
 import org.apache.lucene.index.SegmentReadState;
 import org.apache.lucene.index.SegmentWriteState;
 import org.elasticsearch.index.codec.vectors.AbstractHnswVectorsFormat;
-import org.elasticsearch.index.mapper.vectors.DenseVectorFieldMapper;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
@@ -33,25 +32,26 @@ public class ES93HnswVectorsFormat extends AbstractHnswVectorsFormat {
         flatVectorsFormat = new ES93GenericFlatVectorsFormat();
     }
 
-    public ES93HnswVectorsFormat(DenseVectorFieldMapper.ElementType elementType) {
+    public ES93HnswVectorsFormat(ES93GenericFlatVectorsFormat.ElementType elementType, boolean useDirectIO) {
         super(NAME);
-        flatVectorsFormat = new ES93GenericFlatVectorsFormat(elementType, false);
+        flatVectorsFormat = new ES93GenericFlatVectorsFormat(elementType, useDirectIO);
     }
 
-    public ES93HnswVectorsFormat(int maxConn, int beamWidth, DenseVectorFieldMapper.ElementType elementType) {
+    public ES93HnswVectorsFormat(int maxConn, int beamWidth, ES93GenericFlatVectorsFormat.ElementType elementType, boolean useDirectIO) {
         super(NAME, maxConn, beamWidth);
-        flatVectorsFormat = new ES93GenericFlatVectorsFormat(elementType, false);
+        flatVectorsFormat = new ES93GenericFlatVectorsFormat(elementType, useDirectIO);
     }
 
     public ES93HnswVectorsFormat(
         int maxConn,
         int beamWidth,
-        DenseVectorFieldMapper.ElementType elementType,
+        ES93GenericFlatVectorsFormat.ElementType elementType,
+        boolean useDirectIO,
         int numMergeWorkers,
         ExecutorService mergeExec
     ) {
         super(NAME, maxConn, beamWidth, numMergeWorkers, mergeExec);
-        flatVectorsFormat = new ES93GenericFlatVectorsFormat(elementType, false);
+        flatVectorsFormat = new ES93GenericFlatVectorsFormat(elementType, useDirectIO);
     }
 
     @Override

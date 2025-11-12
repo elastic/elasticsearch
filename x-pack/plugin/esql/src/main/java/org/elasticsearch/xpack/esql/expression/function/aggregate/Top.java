37ad6b71dd8f3a3c1f3f5fb9fdaafd0eea27df3e
Przemyslaw Witek <przemyslaw.witek@elastic.co>
2025-11-12 14:30:52 +0100

    Fix typo in proper file.

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/aggregate/Top.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/aggregate/Top.java
@@ -107,7 +107,7 @@ public class Top extends AggregateFunction
             name = "outputField",
             type = { "double", "integer", "long", "date" },
             description = "The extra field that, if present, will be the output of the TOP call instead of `field`."
-                + "{applies_to}`stack: ga 9.3.`"
+                + "{applies_to}`stack: ga 9.3`"
         ) Expression outputField
     ) {
         this(source, field, Literal.TRUE, NO_WINDOW, limit, order == null ? Literal.keyword(source, ORDER_ASC) : order, outputField);

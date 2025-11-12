641481b059fe8a61203573789153103c35785c55
Mridula <mridula.s@elastic.co>
2025-11-12 15:37:10 +0000

    comitting to trigger a run

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/InferenceFeatures.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/InferenceFeatures.java
@@ -55,6 +55,8 @@ public class InferenceFeatures implements FeatureSpecification {
 
     public static final NodeFeature INFERENCE_ENDPOINT_CACHE = new NodeFeature("inference.endpoint.cache");
     public static final NodeFeature SEARCH_USAGE_EXTENDED_DATA = new NodeFeature("search.usage.extended_data");
+    public static final NodeFeature TEXT_SIMILARITY_RERANKER_INFERENCE_ID_CHUNKING = 
+        new NodeFeature("text_similarity_reranker_inference_id_chunking");
 
     @Override
     public Set<NodeFeature> getFeatures() {
@@ -102,7 +104,8 @@ public class InferenceFeatures implements FeatureSpecification {
                 TEXT_SIMILARITY_RERANKER_SNIPPETS,
                 ModelStats.SEMANTIC_TEXT_USAGE,
                 SEARCH_USAGE_EXTENDED_DATA,
-                TEXT_SIMILARITY_RANK_DOC_EXPLAIN_CHUNKS
+                TEXT_SIMILARITY_RANK_DOC_EXPLAIN_CHUNKS,
+                TEXT_SIMILARITY_RERANKER_INFERENCE_ID_CHUNKING
             )
         );
         testFeatures.addAll(getFeatures());

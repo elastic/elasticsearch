3f40a5fd57de45e04e0a973e6b9b8c96d8da24d8
Simon Cooper <simon.cooper@elastic.co>
2025-11-13 16:50:48 +0000

    Add a capability

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1647,6 +1647,8 @@ public class EsqlCapabilities {
          */
         EXPONENTIAL_HISTOGRAM_PERCENTILES_SUPPORT(EXPONENTIAL_HISTOGRAM_FEATURE_FLAG),
 
+        DATE_TIME_EXCEPTIONS_HANDLED,
+
         // Last capability should still have a comma for fewer merge conflicts when adding new ones :)
         // This comment prevents the semicolon from being on the previous capability when Spotless formats the file.
         ;

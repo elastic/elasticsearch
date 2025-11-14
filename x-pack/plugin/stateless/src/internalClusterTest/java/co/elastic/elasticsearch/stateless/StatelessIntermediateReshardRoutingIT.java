8280922fc45c4af0b804320021998f9655d75fb9
Ankita Kumar <ankita.kumar@elastic.co>
2025-11-14 08:34:22 -0500

    Resharding Add Flush and Refresh functionality (#4724)
    Test for Flush and Refresh requests coming from a stale coordinator.

--- x-pack/plugin/stateless/src/internalClusterTest/java/co/elastic/elasticsearch/stateless/StatelessIntermediateReshardRoutingIT.java
+++ x-pack/plugin/stateless/src/internalClusterTest/java/co/elastic/elasticsearch/stateless/StatelessIntermediateReshardRoutingIT.java
@@ -28,6 +28,7 @@ import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.support.PlainActionFuture;
+import org.elasticsearch.action.support.broadcast.BroadcastResponse;
 import org.elasticsearch.action.support.master.MasterNodeRequestHelper;
 import org.elasticsearch.action.support.replication.TransportReplicationAction;
 import org.elasticsearch.cluster.ClusterState;
@@ -54,6 +55,9 @@ import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertNoFa
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertResponse;
 import static org.hamcrest.Matchers.equalTo;
 
+// When the coordinator that is coordinating a replication request has stale cluster state (w.r.t resharding state),
+// the replication request has to be forwarded to the target shard(s) at the primary node. This test is for
+// testing such scenarios for various replication requests such as indexing, refresh and flush.
 public class StatelessIntermediateReshardRoutingIT extends AbstractStatelessIntegTestCase {
 
     @Override
@@ -62,6 +66,8 @@ public class StatelessIntermediateReshardRoutingIT extends AbstractStatelessInte
         return super.nodeSettings().put(TransportReplicationAction.REPLICATION_RETRY_TIMEOUT.getKey(), "60s");
     }
 
+    // Test forwarding of indexing requests (by the primary shard) when the coordinator is so stale that
+    // it is not even aware of ongoing resharding.
     public void testIndexRoutingWhenCoordinatorStale() throws Exception {
         // Create master and index nodes
         String masterNode = startMasterOnlyNode();
@@ -109,11 +115,11 @@ public class StatelessIntermediateReshardRoutingIT extends AbstractStatelessInte
         // Reshard the index to two shards - send to master node (starts async)
         client(masterNode).execute(TransportReshardAction.TYPE, new ReshardIndexRequest(indexName, 2)).actionGet();
         try {
-            // Wait for all target shards to arrive at handoff point (blocked)
+            // Wait for all target shards to arrive at HANDOFF point (blocked)
             handoffTransitionBlock.await();
 
             // Send an index request to the isolated search node (which doesn't know about the reshard)
-            indexDocsToIsolatedNode(isolatedSearchNode, docsToIndex, indexName);
+            indexDocsToNode(isolatedSearchNode, docsToIndex, indexName);
 
             // Unblock reshard
             blockSplit.countDown();
@@ -128,6 +134,9 @@ public class StatelessIntermediateReshardRoutingIT extends AbstractStatelessInte
         finishReshardAndAssert(indexName, initialDocs, docsToIndex);
     }
 
+    // Test forwarding of indexing requests (by the primary shard) when the coordinator knows about
+    // resharding, but thinks that the target is still in CLONE state, and hence sends the request
+    // only to the source primary.
     public void testIndexRoutingWhenCoordinatorStaleReshardState() throws Exception {
         // Create master and index nodes
         String masterNode = startMasterOnlyNode();
@@ -182,10 +191,11 @@ public class StatelessIntermediateReshardRoutingIT extends AbstractStatelessInte
         );
 
         try {
-            // Wait for all target shards to arrive at handoff point (blocked)
+            // Wait for all target shards to arrive at HANDOFF point (blocked)
             handoffTransitionBlock.await();
 
-            indexDocsToIsolatedNode(isolatedSearchNode, docsToIndex, indexName);
+            // Send an index request to the isolated search node (which only knows about CLONE state)
+            indexDocsToNode(isolatedSearchNode, docsToIndex, indexName);
 
             // Unblock reshard
             blockSplit.countDown();
@@ -197,6 +207,161 @@ public class StatelessIntermediateReshardRoutingIT extends AbstractStatelessInte
         publishTrivialClusterStateUpdate();
 
         finishReshardAndAssert(indexName, initialDocs, docsToIndex);
+    }
+
+    // Test forwarding of flush requests by the primary shard when the coordinator is so stale that
+    // it is not even aware of ongoing resharding.
+    // Steps:
+    // A flush request is sent by a stale coordinator to just the source primary.
+    // The source and target index shards are blocked on the transition to HANDOFF.
+    // Flush request is forwarded to the new shard by the source primary.
+    // Flush response indicates the flush request was executed on both shards.
+    public void testFlushRoutingWhenCoordinatorStale() throws Exception {
+        testBroadcastRoutingWhenCoordinatorStale("flush", false);
+    }
+
+    // Similar to testFlushRoutingWhenCoordinatorStale(), except the coordinator knows about resharding.
+    // The coordinator is stale and only knows about CLONE state.
+    public void testFlushRoutingWhenCoordinatorStaleReshardingState() throws Exception {
+        testBroadcastRoutingWhenCoordinatorStale("flush", true);
+    }
+
+    // Test forwarding of refresh requests by the primary shard when the coordinator is so stale that
+    // it is not even aware of ongoing resharding.
+    public void testRefreshRoutingWhenCoordinatorStale() throws Exception {
+        testBroadcastRoutingWhenCoordinatorStale("refresh", false);
+    }
+
+    public void testRefreshRoutingWhenCoordinatorStaleReshardingState() throws Exception {
+        testBroadcastRoutingWhenCoordinatorStale("refresh", true);
+    }
+
+    private void testBroadcastRoutingWhenCoordinatorStale(String actionType, boolean waitForReshardingState) throws Exception {
+        // -- Cluster setup
+        String masterNode = startMasterOnlyNode();
+        String indexNode = startIndexNode();
+        startSearchNode();
+        ensureStableCluster(3);
+
+        // -- Index creation
+        final String indexName = randomAlphaOfLength(10).toLowerCase(Locale.ROOT);
+        createIndex(indexName, indexSettings(1, 1).build());
+        ensureGreen(indexName);
+
+        NumShards numShards = getNumShards(indexName);
+        logger.info("Initial Number of shards in index primary: {}, replica: {}", numShards.numPrimaries, numShards.numReplicas);
+
+        // -- Index initial documents
+        int initialDocs = randomIntBetween(10, 20);
+        indexDocs(indexName, initialDocs);
+        BroadcastResponse response = flush(indexName);
+        assertNoFailures(response);
+        assertThat(response.getSuccessfulShards(), equalTo(numShards.numPrimaries));
+
+        // -- Isolate a coordinator node
+        // Use the cluster.routing.allocation.exclude setting to prevent allocations on this node
+        // This simulates a coordinator only node. Otherwise, as soon as the target index shard goes to HANDOFF state,
+        // we might try to allocate search shards on this node, which will be a problem since cluster updates are blocked on this node.
+        String isolatedSearchNode = startSearchNode();
+        client().admin()
+            .cluster()
+            .prepareUpdateSettings(TEST_REQUEST_TIMEOUT, TEST_REQUEST_TIMEOUT)
+            .setPersistentSettings(Settings.builder().put("cluster.routing.allocation.exclude._name", isolatedSearchNode))
+            .get();
+        ensureStableCluster(4);
+        MockTransportService isolatedTransportService = MockTransportService.getInstance(isolatedSearchNode);
+
+        CyclicBarrier handoffTransitionBlock = new CyclicBarrier(2);
+        CyclicBarrier splitTransitionBlock = new CyclicBarrier(2);
+        CountDownLatch blockDone = new CountDownLatch(1);
+
+        // Intercept on the index node
+        MockTransportService indexTransportService = MockTransportService.getInstance(indexNode);
+        holdAtHandoffAndSplit(indexTransportService, handoffTransitionBlock, splitTransitionBlock, blockDone);
+
+        int docsToIndex = randomIntBetween(50, 100);
+        int multiple = 2;
+
+        if (waitForReshardingState) {
+            client(masterNode).execute(TransportReshardAction.TYPE, new ReshardIndexRequest(indexName, multiple)).actionGet();
+
+            waitForClusterState(isolatedSearchNode, clusterState -> {
+                IndexReshardingMetadata reshardingMetadata = clusterState.projectState()
+                    .metadata()
+                    .index(indexName)
+                    .getReshardingMetadata();
+                return reshardingMetadata != null
+                    && reshardingMetadata.getSplit().getTargetShardState(1) == IndexReshardingState.Split.TargetShardState.CLONE;
+            }).actionGet();
+
+            // Isolate the search node from getting cluster state updates by blocking publication (once it knows about CLONE state)
+            isolatedTransportService.addRequestHandlingBehavior(
+                PublicationTransportHandler.PUBLISH_STATE_ACTION_NAME,
+                (handler, request, channel, task) -> {
+                    // Block cluster state updates by sending an error response
+                    LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(50));
+                    logger.info("Blocking cluster state publication on isolated search node");
+                    channel.sendResponse(new IllegalStateException("cluster state updates blocked"));
+                }
+            );
+        } else {
+            // Block cluster state updates on the isolated node
+            isolatedTransportService.addRequestHandlingBehavior(
+                PublicationTransportHandler.PUBLISH_STATE_ACTION_NAME,
+                (handler, request, channel, task) -> {
+                    LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(50));
+                    logger.info("Blocking cluster state publication on isolated search node");
+                    channel.sendResponse(new IllegalStateException("cluster state updates blocked"));
+                }
+            );
+            // Trigger reshard
+            client(masterNode).execute(TransportReshardAction.TYPE, new ReshardIndexRequest(indexName, multiple)).actionGet();
+        }
+
+        try {
+            // Wait for shards to reach HANDOFF point
+            handoffTransitionBlock.await();
+
+            // Index more documents. This is just to add a layer of verification to this test.
+            indexDocsToNode(indexNode, docsToIndex, indexName);
+
+            // Perform broadcast action (flush or refresh) on isolated node
+            verifyBroadcastRequestExecution(isolatedSearchNode, indexName, actionType, numShards.numPrimaries * multiple);
+
+            // Wait for all target shards to arrive at SPLIT point (blocked)
+            splitTransitionBlock.await();
+
+            // Index more documents. This is just to add a layer of verification to this test.
+            indexDocsToNode(indexNode, docsToIndex, indexName);
+
+            // Perform broadcast action (flush or refresh) on isolated node
+            verifyBroadcastRequestExecution(isolatedSearchNode, indexName, actionType, numShards.numPrimaries * multiple);
+
+            // Unblock reshard
+            blockDone.countDown();
+        } finally {
+            indexTransportService.clearAllRules();
+            isolatedTransportService.clearAllRules();
+        }
+
+        numShards = getNumShards(indexName);
+        logger.info("Final Number of shards in index primary: {}, replica: {}", numShards.numPrimaries, numShards.numReplicas);
+        // Validate completion
+        finishReshardAndAssert(indexName, initialDocs, 2 * docsToIndex);
+    }
+
+    private static void verifyBroadcastRequestExecution(String isolatedSearchNode, String indexName, String actionType, int numShards) {
+        BroadcastResponse response;
+        if ("flush".equals(actionType)) {
+            response = client(isolatedSearchNode).admin().indices().prepareFlush(indexName).get();
+        } else if ("refresh".equals(actionType)) {
+            response = client(isolatedSearchNode).admin().indices().prepareRefresh(indexName).get();
+        } else {
+            throw new IllegalArgumentException("Unsupported action type: " + actionType);
+        }
+
+        assertNoFailures(response);
+        assertThat(response.getSuccessfulShards(), equalTo(numShards));
     }
 
     private static void holdAtHandoff(
@@ -227,9 +392,40 @@ public class StatelessIntermediateReshardRoutingIT extends AbstractStatelessInte
         });
     }
 
-    private void indexDocsToIsolatedNode(String isolatedSearchNode, int docsToIndex, String indexName) {
+    private static void holdAtHandoffAndSplit(
+        MockTransportService indexTransportService,
+        CyclicBarrier handoffTransitionBlock,
+        CyclicBarrier splitTransitionBlock,
+        CountDownLatch blockDone
+    ) {
+        indexTransportService.addSendBehavior((connection, requestId, action, request, options) -> {
+            try {
+                if (TransportUpdateSplitStateAction.TYPE.name().equals(action)) {
+                    // Unwrap TermOverridingMasterNodeRequest if needed
+                    TransportRequest actualRequest = MasterNodeRequestHelper.unwrapTermOverride(request);
+
+                    // Block transition to HANDOFF state, then SPLIT
+                    if (actualRequest instanceof SplitStateRequest splitStateRequest) {
+                        if (splitStateRequest.getNewTargetShardState() == IndexReshardingState.Split.TargetShardState.HANDOFF) {
+                            // Line up all target shards here
+                            handoffTransitionBlock.await();
+                        } else if (splitStateRequest.getNewTargetShardState() == IndexReshardingState.Split.TargetShardState.SPLIT) {
+                            splitTransitionBlock.await();
+                        } else if (splitStateRequest.getNewTargetShardState() == IndexReshardingState.Split.TargetShardState.DONE) {
+                            blockDone.await();
+                        }
+                    }
+                }
+                connection.sendRequest(requestId, action, request, options);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private void indexDocsToNode(String node, int docsToIndex, String indexName) {
         // Send an index request to the isolated search node (which doesn't know about the reshard)
-        BulkRequestBuilder bulkRequestBuilder = client(isolatedSearchNode).prepareBulk();
+        BulkRequestBuilder bulkRequestBuilder = client(node).prepareBulk();
         for (int i = 0; i < docsToIndex; i++) {
             bulkRequestBuilder.add(new IndexRequest(indexName).source("field", randomAlphaOfLength(10)));
         }

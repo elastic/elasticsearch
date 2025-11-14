fa08ce858e5b144ce932dc64154f113145714878
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2025-11-14 16:10:58 +0100

    remove unnecessary options

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/ComputeService.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/ComputeService.java
@@ -452,8 +452,8 @@ public class ComputeService {
                             flags,
                             configuration,
                             dataNodePlan,
+                            localOriginalIndices.indices(),
                             Set.of(localConcreteIndices.indices()),
-                            localOriginalIndices,
                             exchangeSource,
                             cancelQueryOnFailure,
                             ActionListener.wrap(r -> {

f8ea89ddc452e1ea5bff769f91ba2564c8f7f728
Jordan Powers <jordan.powers@elastic.co>
2025-11-14 12:55:59 -0800

    Address review feedback

--- server/src/internalClusterTest/java/org/elasticsearch/action/admin/indices/settings/get/GetSettingsIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/action/admin/indices/settings/get/GetSettingsIT.java
@@ -11,6 +11,9 @@ package org.elasticsearch.action.admin.indices.settings.get;
 
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.test.ESIntegTestCase;
+import org.elasticsearch.test.LambdaMatchers;
+
+import java.util.Map;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.hamcrest.Matchers.equalTo;
@@ -20,27 +23,33 @@ public class GetSettingsIT extends ESIntegTestCase {
     public void testGetDefaultValueWhenDependentOnOtherSettings() {
         final String indexName = "test-index-1";
 
-        String[][] expectedSettings = {
-            { "index.mapping.source.mode", "SYNTHETIC" },
-            { "index.mapping.total_fields.ignore_dynamic_beyond_limit", "true" },
-            { "index.recovery.use_synthetic_source", "true" },
-            { "index.use_time_series_doc_values_format", "true" },
-            { "index.mapping.ignore_above", "8191" },
-            { "index.sort.field", "[@timestamp]" },
-            { "index.sort.order", "[desc]" },
-            { "index.sort.mode", "[max]" },
-            { "index.sort.missing", "[_last]" } };
+        Settings expectedSettings = Settings.builder()
+            .put("index.mapping.source.mode", "SYNTHETIC")
+            .put("index.mapping.total_fields.ignore_dynamic_beyond_limit", true)
+            .put("index.recovery.use_synthetic_source", true)
+            .put("index.use_time_series_doc_values_format", true)
+            .put("index.mapping.ignore_above", 8191)
+            .putList("index.sort.field", "@timestamp")
+            .putList("index.sort.order", "desc")
+            .putList("index.sort.mode", "max")
+            .putList("index.sort.missing", "_last")
+            .build();
 
         assertAcked(prepareCreate(indexName).setSettings(Settings.builder().put("index.mode", "logsdb")).get());
-        for (String[] expectedSetting : expectedSettings) {
-            GetSettingsResponse unfilteredResponse = indicesAdmin().getSettings(
-                new GetSettingsRequest(TEST_REQUEST_TIMEOUT).indices(indexName).includeDefaults(true)
-            ).actionGet();
-            assertThat(unfilteredResponse.getSetting(indexName, expectedSetting[0]), equalTo(expectedSetting[1]));
+        GetSettingsResponse unfilteredResponse = indicesAdmin().getSettings(
+            new GetSettingsRequest(TEST_REQUEST_TIMEOUT).indices(indexName).includeDefaults(true)
+        ).actionGet();
+        for (String key : expectedSettings.keySet()) {
+            assertThat(unfilteredResponse.getSetting(indexName, key), equalTo(expectedSettings.get(key)));
             GetSettingsResponse filteredResponse = indicesAdmin().getSettings(
-                new GetSettingsRequest(TEST_REQUEST_TIMEOUT).indices(indexName).includeDefaults(true).names(expectedSetting[0])
+                new GetSettingsRequest(TEST_REQUEST_TIMEOUT).indices(indexName).includeDefaults(true).names(key)
             ).actionGet();
-            assertThat(filteredResponse.getSetting(indexName, expectedSetting[0]), equalTo(expectedSetting[1]));
+
+            var expectedFilteredSettingsMap = Map.of(indexName, expectedSettings.filter(key::equals));
+            assertThat(
+                filteredResponse,
+                LambdaMatchers.transformedMatch(GetSettingsResponse::getIndexToDefaultSettings, equalTo(expectedFilteredSettingsMap))
+            );
         }
 
     }

02ecb03b28bc03fd54fa9141f674f411ceedc324
AntMoraisElastic <antonio.morais@elastic.co>
2025-11-14 11:50:01 +0000

    Remove new model from Mock auth server

--- x-pack/plugin/inference/qa/inference-service-tests/src/javaRestTest/java/org/elasticsearch/xpack/inference/MockElasticInferenceServiceAuthorizationServer.java
+++ x-pack/plugin/inference/qa/inference-service-tests/src/javaRestTest/java/org/elasticsearch/xpack/inference/MockElasticInferenceServiceAuthorizationServer.java
@@ -39,10 +39,6 @@ public class MockElasticInferenceServiceAuthorizationServer implements TestRule 
                       "task_types": ["chat"]
                     },
                     {
-                      "model_name": "gp-llm-v2",
-                      "task_types": ["chat"]
-                    },
-                    {
                       "model_name": "elser_model_2",
                       "task_types": ["embed/text/sparse"]
                     },

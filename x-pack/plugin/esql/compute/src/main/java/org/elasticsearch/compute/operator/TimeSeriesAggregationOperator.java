1c433383a6f08b8212f866a78832998ed69e1b7c
Nhat Nguyen <nhat.nguyen@elastic.co>
2025-11-17 20:49:18 -0800

    Support window in more time-series aggregations

--- x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/operator/TimeSeriesAggregationOperator.java
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/operator/TimeSeriesAggregationOperator.java
@@ -8,21 +8,31 @@
 package org.elasticsearch.compute.operator;
 
 import org.elasticsearch.common.Rounding;
+import org.elasticsearch.common.util.BigArrays;
+import org.elasticsearch.common.util.IntArray;
 import org.elasticsearch.compute.Describable;
 import org.elasticsearch.compute.aggregation.AggregatorMode;
 import org.elasticsearch.compute.aggregation.GroupingAggregator;
 import org.elasticsearch.compute.aggregation.GroupingAggregatorEvaluationContext;
+import org.elasticsearch.compute.aggregation.GroupingAggregatorFunction;
 import org.elasticsearch.compute.aggregation.TimeSeriesGroupingAggregatorEvaluationContext;
+import org.elasticsearch.compute.aggregation.WindowGroupingAggregatorFunction;
 import org.elasticsearch.compute.aggregation.blockhash.BlockHash;
 import org.elasticsearch.compute.aggregation.blockhash.BytesRefLongBlockHash;
 import org.elasticsearch.compute.data.Block;
+import org.elasticsearch.compute.data.BlockFactory;
 import org.elasticsearch.compute.data.ElementType;
+import org.elasticsearch.compute.data.IntVector;
 import org.elasticsearch.compute.data.LongBlock;
+import org.elasticsearch.core.AbstractRefCounted;
+import org.elasticsearch.core.Releasable;
+import org.elasticsearch.core.Releasables;
 import org.elasticsearch.index.mapper.DateFieldMapper;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Supplier;
 
 import static java.util.stream.Collectors.joining;
@@ -69,6 +79,7 @@ public class TimeSeriesAggregationOperator extends HashAggregationOperator {
 
     private final Rounding.Prepared timeBucket;
     private final DateFieldMapper.Resolution timeResolution;
+    private ExpandingGroups expandingGroups = null;
 
     public TimeSeriesAggregationOperator(
         Rounding.Prepared timeBucket,
@@ -80,6 +91,105 @@ public class TimeSeriesAggregationOperator extends HashAggregationOperator {
         super(aggregators, blockHash, driverContext);
         this.timeBucket = timeBucket;
         this.timeResolution = timeResolution;
+    }
+
+    @Override
+    public void finish() {
+        expandWindowBuckets();
+        super.finish();
+    }
+
+    private long largestWindowMillis() {
+        long largestWindow = Long.MIN_VALUE;
+        for (GroupingAggregator aggregator : aggregators) {
+            if (aggregator.aggregatorFunction() instanceof WindowGroupingAggregatorFunction aggregatorFunction) {
+                largestWindow = Math.max(largestWindow, aggregatorFunction.window().toMillis());
+            }
+        }
+        return largestWindow;
+    }
+
+    /**
+     * Expands window buckets to ensure all required time buckets are present for time-series aggregations.
+     * This is equivalent when sliding the window over the raw input.
+     * <p>
+     * For example, if the window is 10 minutes and the bucket size is 2 minutes:
+     * If we see a bucket at 12:00, we need to ensure buckets exist for the previous 10 minutes.
+     * 02:00 -> [12:00, 13:59) // bucket=2m
+     * 00:04 -> [04:00, 05:59), [06:00, 07:59), [08:00, 09:59), [10:00, 11:59), [12:00, 13:59) // window=10m
+     */
+    private void expandWindowBuckets() {
+        for (GroupingAggregator aggregator : aggregators) {
+            if (aggregator.mode().isOutputPartial()) {
+                return;
+            }
+        }
+        final long windowMillis = largestWindowMillis();
+        if (windowMillis <= 0) {
+            return;
+        }
+        BytesRefLongBlockHash tsBlockHash = (BytesRefLongBlockHash) blockHash;
+        final long numGroups = tsBlockHash.numGroups();
+        if (numGroups == 0) {
+            return;
+        }
+        this.expandingGroups = new ExpandingGroups(driverContext.bigArrays());
+        for (long groupId = 0; groupId < numGroups; groupId++) {
+            long tsid = tsBlockHash.getBytesRefKeyFromGroup(groupId);
+            long endTimestamp = tsBlockHash.getLongKeyFromGroup(groupId);
+            long bucket = timeBucket.nextRoundingValue(endTimestamp - timeResolution.convert(largestWindowMillis()));
+            bucket = Math.max(bucket, tsBlockHash.getMinLongKey());
+            while (bucket < endTimestamp) {
+                if (tsBlockHash.addGroup(tsid, bucket) >= 0) {
+                    expandingGroups.addGroup(Math.toIntExact(groupId));
+                }
+                bucket = timeBucket.nextRoundingValue(bucket);
+            }
+        }
+    }
+
+    @Override
+    protected void evaluateAggregator(
+        GroupingAggregator aggregator,
+        Block[] blocks,
+        int offset,
+        IntVector selected,
+        GroupingAggregatorEvaluationContext evaluationContext
+    ) {
+        if (expandingGroups != null && expandingGroups.count > 0 && isValuesAggregator(aggregator.aggregatorFunction())) {
+            try (var valuesSelected = selectedForValuesAggregator(driverContext.blockFactory(), selected, expandingGroups)) {
+                super.evaluateAggregator(aggregator, blocks, offset, valuesSelected, evaluationContext);
+            }
+        } else {
+            super.evaluateAggregator(aggregator, blocks, offset, selected, evaluationContext);
+        }
+    }
+
+    private static IntVector selectedForValuesAggregator(BlockFactory blockFactory, IntVector selected, ExpandingGroups expandingGroups) {
+        try (var builder = blockFactory.newIntVectorFixedBuilder(selected.getPositionCount())) {
+            int first = selected.getPositionCount() - expandingGroups.count;
+            for (int i = 0; i < first; i++) {
+                builder.appendInt(i, selected.getInt(i));
+            }
+            for (int i = 0; i < expandingGroups.count; i++) {
+                builder.appendInt(first + i, expandingGroups.getGroup(i));
+            }
+            return builder.build();
+        }
+    }
+
+    // generated classes are not available during javadoc
+    private static final Set<String> VALUES_CLASSES = Set.of(
+        "org.elasticsearch.compute.aggregation.ValuesBooleanGroupingAggregatorFunction",
+        "org.elasticsearch.compute.aggregation.ValuesBytesRefGroupingAggregatorFunction",
+        "org.elasticsearch.compute.aggregation.ValuesIntGroupingAggregatorFunction",
+        "org.elasticsearch.compute.aggregation.ValuesLongGroupingAggregatorFunction",
+        "org.elasticsearch.compute.aggregation.ValuesDoubleGroupingAggregatorFunction",
+        "org.elasticsearch.compute.aggregation.DimensionValuesByteRefGroupingAggregatorFunction"
+    );
+
+    static boolean isValuesAggregator(GroupingAggregatorFunction aggregatorFunction) {
+        return VALUES_CLASSES.contains(aggregatorFunction.getClass().getName());
     }
 
     @Override
@@ -104,21 +214,54 @@ public class TimeSeriesAggregationOperator extends HashAggregationOperator {
 
             @Override
             public List<Integer> groupIdsFromWindow(int startingGroupId, Duration window) {
-                long ordinal = hash.getBytesRefKeyFromGroup(startingGroupId);
-                long startTimestamp = hash.getLongKeyFromGroup(startingGroupId);
+                long tsid = hash.getBytesRefKeyFromGroup(startingGroupId);
+                long bucket = hash.getLongKeyFromGroup(startingGroupId);
                 List<Integer> results = new ArrayList<>();
                 results.add(startingGroupId);
-                long endTimestamp = startTimestamp + timeResolution.convert(window.toMillis());
-                long nextTimestamp = startTimestamp;
-                while ((nextTimestamp = timeBucket.nextRoundingValue(nextTimestamp)) < endTimestamp) {
-                    long nextGroupId = hash.getGroupId(ordinal, nextTimestamp);
+                long endTimestamp = bucket + timeResolution.convert(window.toMillis());
+                while ((bucket = timeBucket.nextRoundingValue(bucket)) < endTimestamp) {
+                    long nextGroupId = hash.getGroupId(tsid, bucket);
                     if (nextGroupId != -1) {
                         results.add(Math.toIntExact(nextGroupId));
                     }
                 }
-                assert nextTimestamp == endTimestamp : "expected to end at the original timestamp bucket";
                 return results;
             }
         };
+    }
+
+    static class ExpandingGroups extends AbstractRefCounted implements Releasable {
+        private final BigArrays bigArrays;
+        private IntArray newGroups;
+        private int count;
+
+        ExpandingGroups(BigArrays bigArrays) {
+            this.bigArrays = bigArrays;
+            this.newGroups = bigArrays.newIntArray(128);
+        }
+
+        void addGroup(int groupId) {
+            newGroups = bigArrays.grow(newGroups, count + 1);
+            newGroups.set(count++, groupId);
+        }
+
+        int getGroup(int index) {
+            return newGroups.get(index);
+        }
+
+        @Override
+        protected void closeInternal() {
+            newGroups.close();
+        }
+
+        @Override
+        public void close() {
+            decRef();
+        }
+    }
+
+    @Override
+    public void close() {
+        Releasables.close(expandingGroups, super::close);
     }
 }

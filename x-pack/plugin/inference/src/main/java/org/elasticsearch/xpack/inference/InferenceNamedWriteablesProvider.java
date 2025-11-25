a4543db3547283288bf68e55bcf4e36970d66bed
Jonathan Buttner <jonathan.buttner@elastic.co>
2025-11-24 20:53:15 -0500

    Removing named writeable

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/InferenceNamedWriteablesProvider.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/InferenceNamedWriteablesProvider.java
@@ -706,19 +706,6 @@ public class InferenceNamedWriteablesProvider {
                 StreamingUnifiedChatCompletionResults.Results::new
             )
         );
-
-        /*
-        We don't really need to register this because it is never serialized across nodes and it never returned as a result.
-        The reason we have AuthorizationResponseEntity implement InferenceServiceResults is so we can use it with the sender framework
-        we have.
-         */
-        namedWriteables.add(
-            new NamedWriteableRegistry.Entry(
-                InferenceServiceResults.class,
-                AuthorizationResponseEntity.NAME,
-                AuthorizationResponseEntity::new
-            )
-        );
     }
 
     private static void addCustomElandWriteables(final List<NamedWriteableRegistry.Entry> namedWriteables) {

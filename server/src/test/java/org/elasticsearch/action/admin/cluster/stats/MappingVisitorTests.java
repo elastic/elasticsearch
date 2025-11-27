592d8fbf823c088ce722fb552db029ee9f2b8f30
gmarouli <mary.gouseti@elastic.co>
2025-11-27 11:19:57 +0200

    Add visitor infrastructure

--- server/src/test/java/org/elasticsearch/action/admin/cluster/stats/MappingVisitorTests.java
+++ server/src/test/java/org/elasticsearch/action/admin/cluster/stats/MappingVisitorTests.java
@@ -136,4 +136,61 @@ public class MappingVisitorTests extends ESTestCase {
     private static void collectRuntimeTypes(Map<String, ?> mapping, Set<String> types) {
         MappingVisitor.visitRuntimeMapping(mapping, (f, m) -> types.add(m.get("type").toString()));
     }
+
+    @SuppressWarnings("unchecked")
+    public void testConvertLongToKeyword() {
+        Map<String, Object> longType = Map.of("type", "long");
+        Map<String, Object> textType = Map.of("type", "text");
+        Map<String, Object> floatType = Map.of("type", "float", "scaling_factor", 1000);
+        Map<String, Object> multiField = Map.of("type", "keyword", "fields", Map.of("my-long", longType, "my-float", floatType));
+        Map<String, Object> objectField = Map.of("type", "keyword", "properties", Map.of("my-text", textType, "my-long", longType));
+        Map<String, Object> expectedProperties = Map.of(
+            "properties",
+            Map.of("my-long", longType, "my-float", floatType, "my-multi-field", multiField, "my-object", objectField)
+        );
+
+        HashMap<String, Object> result = new HashMap<>();
+        MappingVisitor.visitAndCopyMapping(expectedProperties, result, (ignored, source, dest) -> {
+            for (String key : source.keySet()) {
+                if (key.equals("type") && source.get(key).equals("long")) {
+                    dest.put(key, "keyword");
+                } else {
+                    dest.put(key, source.get(key));
+                }
+            }
+        });
+
+        assertTrue(result.containsKey("properties"));
+        Map<String, Object> properties = (Map<String, Object>) result.get("properties");
+
+        assertTrue(properties.containsKey("my-long"));
+        Map<String, Object> myLong = (Map<String, Object>) properties.get("my-long");
+        assertEquals("keyword", myLong.get("type"));
+
+        assertTrue(properties.containsKey("my-float"));
+        Map<String, Object> myFloat = (Map<String, Object>) properties.get("my-float");
+        assertEquals("float", myFloat.get("type"));
+        assertEquals(1000, myFloat.get("scaling_factor"));
+
+        assertTrue(properties.containsKey("my-multi-field"));
+        Map<String, Object> myMultiField = (Map<String, Object>) properties.get("my-multi-field");
+        assertEquals("keyword", myMultiField.get("type"));
+        assertTrue(myMultiField.containsKey("fields"));
+        Map<String, Object> foundFields = (Map<String, Object>) myMultiField.get("fields");
+        assertTrue(foundFields.containsKey("my-long"));
+        assertEquals("keyword", ((Map<String, Object>) foundFields.get("my-long")).get("type"));
+        assertTrue(foundFields.containsKey("my-float"));
+        assertEquals("float", ((Map<String, Object>) foundFields.get("my-float")).get("type"));
+        assertEquals(1000, ((Map<String, Object>) foundFields.get("my-float")).get("scaling_factor"));
+
+        assertTrue(properties.containsKey("my-object"));
+        Map<String, Object> myObject = (Map<String, Object>) properties.get("my-object");
+        assertEquals("keyword", myObject.get("type"));
+        assertTrue(myObject.containsKey("properties"));
+        Map<String, Object> foundSubObjects = (Map<String, Object>) myObject.get("properties");
+        assertTrue(foundSubObjects.containsKey("my-long"));
+        assertEquals("keyword", ((Map<String, Object>) foundSubObjects.get("my-long")).get("type"));
+        assertTrue(foundSubObjects.containsKey("my-text"));
+        assertEquals("text", ((Map<String, Object>) foundSubObjects.get("my-text")).get("type"));
+    }
 }

78e6e135c340e0ab0c226c4ec6578276452425a2
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-11-27 00:39:24 +0000

    [CI] Auto commit changes from spotless

--- server/src/test/java/org/elasticsearch/ingest/IngestServiceTests.java
+++ server/src/test/java/org/elasticsearch/ingest/IngestServiceTests.java
@@ -1679,8 +1679,7 @@ public class IngestServiceTests extends ESTestCase {
             XContentType.JSON
         );
 
-        ClusterState clusterState = ClusterState.builder(ClusterName.DEFAULT)
-            .build();
+        ClusterState clusterState = ClusterState.builder(ClusterName.DEFAULT).build();
         ClusterState previousClusterState = clusterState;
         clusterState = executePut(putRequest, clusterState);
         ingestService.applyClusterState(new ClusterChangedEvent("", clusterState, previousClusterState));

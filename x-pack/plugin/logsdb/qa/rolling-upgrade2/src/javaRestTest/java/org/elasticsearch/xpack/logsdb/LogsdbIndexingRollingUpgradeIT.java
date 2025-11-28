fc8689c54e3d839927815610a94baf46a351b0b1
Martijn van Groningen <martijn.v.groningen@gmail.com>
2025-11-28 10:55:35 +0100

    iter

--- x-pack/plugin/logsdb/qa/rolling-upgrade2/src/javaRestTest/java/org/elasticsearch/xpack/logsdb/LogsdbIndexingRollingUpgradeIT.java
+++ x-pack/plugin/logsdb/qa/rolling-upgrade2/src/javaRestTest/java/org/elasticsearch/xpack/logsdb/LogsdbIndexingRollingUpgradeIT.java
@@ -135,7 +135,9 @@ public class LogsdbIndexingRollingUpgradeIT extends ESRestTestCase {
 
     private void upgradeNode(int n) throws IOException {
         closeClients();
-        var upgradeVersion = Version.CURRENT;
+        var upgradeVersion = System.getProperty("tests.new_cluster_version") == null
+            ? Version.fromString(System.getProperty("tests.new_cluster_version"))
+            : Version.CURRENT;
         logger.info("Upgrading node {} to version {}", n, upgradeVersion);
         cluster.upgradeNodeToVersion(n, upgradeVersion);
         initClient();

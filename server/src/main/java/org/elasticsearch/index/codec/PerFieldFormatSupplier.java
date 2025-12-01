954afd9ffdcc1af2fca4aabb098a10f232fa14ea
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2025-12-01 14:45:58 +0100

    Fix merge posting format selection

--- server/src/main/java/org/elasticsearch/index/codec/PerFieldFormatSupplier.java
+++ server/src/main/java/org/elasticsearch/index/codec/PerFieldFormatSupplier.java
@@ -20,6 +20,7 @@ import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.IndexVersions;
 import org.elasticsearch.index.codec.bloomfilter.ES87BloomFilterPostingsFormat;
 import org.elasticsearch.index.codec.postings.ES812PostingsFormat;
+import org.elasticsearch.index.codec.tsdb.TSDBSyntheticIdPostingsFormat;
 import org.elasticsearch.index.codec.tsdb.es819.ES819TSDBDocValuesFormat;
 import org.elasticsearch.index.mapper.CompletionFieldMapper;
 import org.elasticsearch.index.mapper.IdFieldMapper;
@@ -67,11 +68,13 @@ public class PerFieldFormatSupplier {
     private final MapperService mapperService;
 
     private final PostingsFormat defaultPostingsFormat;
+    private final TSDBSyntheticIdPostingsFormat syntheticIdPostingsFormat;
 
     public PerFieldFormatSupplier(MapperService mapperService, BigArrays bigArrays) {
         this.mapperService = mapperService;
         this.bloomFilterPostingsFormat = new ES87BloomFilterPostingsFormat(bigArrays, this::internalGetPostingsFormatForField);
         this.defaultPostingsFormat = getDefaultPostingsFormat(mapperService);
+        this.syntheticIdPostingsFormat = new TSDBSyntheticIdPostingsFormat();
     }
 
     private static PostingsFormat getDefaultPostingsFormat(final MapperService mapperService) {
@@ -90,6 +93,12 @@ public class PerFieldFormatSupplier {
     }
 
     public PostingsFormat getPostingsFormatForField(String field) {
+        if (useTSDBSyntheticId(field)) {
+            // This gets called during merges where the segment merger
+            // instead of relying on the field format name attribute,
+            // it delegates that decision to the codec.
+            return syntheticIdPostingsFormat;
+        }
         if (useBloomFilter(field)) {
             return bloomFilterPostingsFormat;
         }
@@ -123,6 +132,13 @@ public class PerFieldFormatSupplier {
         } else {
             return IdFieldMapper.NAME.equals(field) && IndexSettings.BLOOM_FILTER_ID_FIELD_ENABLED_SETTING.get(indexSettings.getSettings());
         }
+    }
+
+    private boolean useTSDBSyntheticId(String field) {
+        if (mapperService == null || IdFieldMapper.NAME.equals(field) == false) {
+            return false;
+        }
+        return mapperService.getIndexSettings().useTimeSeriesSyntheticId();
     }
 
     public KnnVectorsFormat getKnnVectorsFormatForField(String field) {

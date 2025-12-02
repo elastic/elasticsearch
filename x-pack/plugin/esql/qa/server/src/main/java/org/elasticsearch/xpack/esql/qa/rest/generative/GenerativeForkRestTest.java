b7af6af0a62b0db1606a1bf6f43202d98c338032
Jan Kuipers <jan.kuipers@elastic.co>
2025-12-02 16:39:34 +0100

    wire approximate into rest test

--- x-pack/plugin/esql/qa/server/src/main/java/org/elasticsearch/xpack/esql/qa/rest/generative/GenerativeForkRestTest.java
+++ x-pack/plugin/esql/qa/server/src/main/java/org/elasticsearch/xpack/esql/qa/rest/generative/GenerativeForkRestTest.java
@@ -45,7 +45,7 @@ public abstract class GenerativeForkRestTest extends EsqlSpecTestCase {
         suffix = suffix + "| WHERE _fork == \"fork1\" | DROP _fork";
 
         String query = testCase.query + suffix;
-        doTest(query);
+        doTest(query, false);
     }
 
     @Override

9ba3b69f0717a2ec9fe9212459563551eaead2b1
Matteo Mazzola <matteo.mazzola@elastic.co>
2025-12-02 11:17:42 +0000

    Update ScopedSettingsTests.java

--- server/src/test/java/org/elasticsearch/common/settings/ScopedSettingsTests.java
+++ server/src/test/java/org/elasticsearch/common/settings/ScopedSettingsTests.java
@@ -345,7 +345,7 @@ public class ScopedSettingsTests extends ESTestCase {
             e.getMessage(),
             equalTo(
                 "unknown setting [archived.index.store.type] was archived after upgrading, and must be removed."
-                    + " See [https://www.elastic.co/docs/deploy-manage/upgrade/deployment-or-cluster/archived-settings?version=master] "
+                    + " See [https://www.elastic.co/docs/deploy-manage/upgrade/deployment-or-cluster/archived-settings?version=9.1] "
                     + "for details."
             )
         );

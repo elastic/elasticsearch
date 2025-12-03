362cea3ab085e3cbdaf614bc8a4894c37fafb375
Jack Conradson <osjdconrad@gmail.com>
2025-12-03 13:58:08 -0800

    Reapply "Remove references to V8_1_0 transport version (#136239)" (#136560) (#139008)
    This reverts commit efb3549e5ae7be92d43ba1fc478bee1aa0722da7.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/inference/assignment/AssignmentStats.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/inference/assignment/AssignmentStats.java
@@ -163,19 +163,11 @@ public class AssignmentStats implements ToXContentObject, Writeable {
             this.pendingCount = in.readOptionalVInt();
             this.routingState = in.readOptionalWriteable(RoutingStateAndReason::new);
             this.startTime = in.readOptionalInstant();
-            if (in.getTransportVersion().onOrAfter(TransportVersions.V_8_1_0)) {
-                this.threadsPerAllocation = in.readOptionalVInt();
-                this.numberOfAllocations = in.readOptionalVInt();
-                this.errorCount = in.readVInt();
-                this.rejectedExecutionCount = in.readVInt();
-                this.timeoutCount = in.readVInt();
-            } else {
-                this.threadsPerAllocation = null;
-                this.numberOfAllocations = null;
-                this.errorCount = 0;
-                this.rejectedExecutionCount = 0;
-                this.timeoutCount = 0;
-            }
+            this.threadsPerAllocation = in.readOptionalVInt();
+            this.numberOfAllocations = in.readOptionalVInt();
+            this.errorCount = in.readVInt();
+            this.rejectedExecutionCount = in.readVInt();
+            this.timeoutCount = in.readVInt();
             if (in.getTransportVersion().onOrAfter(TransportVersions.V_8_2_0)) {
                 this.peakThroughput = in.readVLong();
                 this.throughputLastPeriod = in.readVLong();
@@ -342,13 +334,11 @@ public class AssignmentStats implements ToXContentObject, Writeable {
             out.writeOptionalVInt(pendingCount);
             out.writeOptionalWriteable(routingState);
             out.writeOptionalInstant(startTime);
-            if (out.getTransportVersion().onOrAfter(TransportVersions.V_8_1_0)) {
-                out.writeOptionalVInt(threadsPerAllocation);
-                out.writeOptionalVInt(numberOfAllocations);
-                out.writeVInt(errorCount);
-                out.writeVInt(rejectedExecutionCount);
-                out.writeVInt(timeoutCount);
-            }
+            out.writeOptionalVInt(threadsPerAllocation);
+            out.writeOptionalVInt(numberOfAllocations);
+            out.writeVInt(errorCount);
+            out.writeVInt(rejectedExecutionCount);
+            out.writeVInt(timeoutCount);
             if (out.getTransportVersion().onOrAfter(TransportVersions.V_8_2_0)) {
                 out.writeVLong(peakThroughput);
                 out.writeVLong(throughputLastPeriod);

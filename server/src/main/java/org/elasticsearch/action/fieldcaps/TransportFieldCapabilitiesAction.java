31c08fd798327a77e13a94c7604430c2c34b226e
Jack Conradson <osjdconrad@gmail.com>
2025-12-03 13:38:35 -0800

    Reapply "Remove V_8_0_0 transport version from tests (#135942)" (#136578) (#138990) (#139007)
    This reverts commit 60a05a9a5c344ae9d6ff196faaf2d3e7d1b3ca15.

--- server/src/main/java/org/elasticsearch/action/fieldcaps/TransportFieldCapabilitiesAction.java
+++ server/src/main/java/org/elasticsearch/action/fieldcaps/TransportFieldCapabilitiesAction.java
@@ -432,7 +432,7 @@ public class TransportFieldCapabilitiesAction extends HandledTransportAction<Fie
                 } else {
                     subIndices = ArrayUtil.copyOfSubArray(indices, lastPendingIndex, i);
                 }
-                innerMerge(subIndices, responseMapBuilder, request, indexResponses[lastPendingIndex]);
+                innerMerge(subIndices, responseMapBuilder, indexResponses[lastPendingIndex]);
                 lastPendingIndex = i;
             }
         }
@@ -545,15 +545,9 @@ public class TransportFieldCapabilitiesAction extends HandledTransportAction<Fie
     private static void innerMerge(
         String[] indices,
         Map<String, Map<String, FieldCapabilities.Builder>> responseMapBuilder,
-        FieldCapabilitiesRequest request,
         FieldCapabilitiesIndexResponse response
     ) {
-        Map<String, IndexFieldCapabilities> fields = ResponseRewriter.rewriteOldResponses(
-            response.getOriginVersion(),
-            response.get(),
-            request.filters(),
-            request.types()
-        );
+        Map<String, IndexFieldCapabilities> fields = response.get();
         for (Map.Entry<String, IndexFieldCapabilities> entry : fields.entrySet()) {
             final String field = entry.getKey();
             final IndexFieldCapabilities fieldCap = entry.getValue();

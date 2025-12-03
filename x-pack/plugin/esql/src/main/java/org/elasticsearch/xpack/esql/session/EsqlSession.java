96b9f709c0c82e02e59e60d1d0f94716191e9257
Jan Kuipers <jan.kuipers@elastic.co>
2025-12-03 10:25:48 +0100

    add approximate as a query setting

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
@@ -253,7 +253,7 @@ public class EsqlSession {
 
                     SubscribableListener.<LogicalPlan>newForked(l -> preOptimizedPlan(plan, logicalPlanPreOptimizer, planTimeProfile, l))
                         .<LogicalPlan>andThen((l, p) -> {
-                            if (request.approximate()) {
+                            if (statement.setting(QuerySettings.APPROXIMATE)) {
                                 Approximate.verifyPlan(p);
                             }
                             l.onResponse(p);
@@ -267,6 +267,7 @@ public class EsqlSession {
                         .<Result>andThen(
                             (l, p) -> executeOptimizedPlan(
                                 request,
+                                statement,
                                 executionInfo,
                                 planRunner,
                                 p,
@@ -303,6 +304,7 @@ public class EsqlSession {
      */
     public void executeOptimizedPlan(
         EsqlQueryRequest request,
+        EsqlStatement statement,
         EsqlExecutionInfo executionInfo,
         PlanRunner planRunner,
         LogicalPlan optimizedPlan,
@@ -346,6 +348,7 @@ public class EsqlSession {
                 planRunner,
                 executionInfo,
                 request,
+                statement,
                 logicalPlanOptimizer,
                 physicalPlanOptimizer,
                 planTimeProfile,
@@ -361,6 +364,7 @@ public class EsqlSession {
         PlanRunner runner,
         EsqlExecutionInfo executionInfo,
         EsqlQueryRequest request,
+        EsqlStatement statement,
         LogicalPlanOptimizer logicalPlanOptimizer,
         PhysicalPlanOptimizer physicalPlanOptimizer,
         PlanTimeProfile planTimeProfile,
@@ -387,7 +391,7 @@ public class EsqlSession {
                 // Ensure we don't have subplan flag stuck in there on failure
                 ActionListener.runAfter(listener, executionInfo::finishSubPlans)
             );
-        } else if (request.approximate()) {
+        } else if (statement.setting(QuerySettings.APPROXIMATE)) {
             new Approximate(
                 optimizedPlan,
                 logicalPlanOptimizer,
--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/CsvTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/CsvTests.java
@@ -698,8 +698,14 @@ public class CsvTests extends ESTestCase {
         PlanTimeProfile planTimeProfile = configuration.profile() ? new PlanTimeProfile() : null;
         session.preOptimizedPlan(analyzed, logicalPlanPreOptimizer, planTimeProfile, listener.delegateFailureAndWrap((l, preOptimized) -> {
             session.executeOptimizedPlan(
+<<<<<<< HEAD
                 new EsqlQueryRequest().approximate(testCase.approximate),
                 createEsqlExecutionInfo(randomBoolean()),
+=======
+                new EsqlQueryRequest(),
+                statement,
+                new EsqlExecutionInfo(randomBoolean()),
+>>>>>>> 23f422813ec (add approximate as a query setting)
                 planRunner(bigArrays, physicalOperationProviders),
                 session.optimizedPlan(preOptimized, logicalPlanOptimizer, planTimeProfile),
                 configuration,

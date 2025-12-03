0497383f61a1a83482629d05f0b31ccc996830b4
Jack Conradson <osjdconrad@gmail.com>
2025-12-03 09:39:49 -0800

    Reapply "Remove some uses of 8.0.0 transport version constant (#135715)" (#138986) (#138988)
    This reverts commit ecc9dce9f3e8daac12e8d4ddca9e3f0060cebc7c.

--- modules/percolator/src/main/java/org/elasticsearch/percolator/PercolateQueryBuilder.java
+++ modules/percolator/src/main/java/org/elasticsearch/percolator/PercolateQueryBuilder.java
@@ -35,7 +35,6 @@ import org.apache.lucene.util.SetOnce;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ResourceNotFoundException;
 import org.elasticsearch.TransportVersion;
-import org.elasticsearch.TransportVersions;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.common.CheckedSupplier;
 import org.elasticsearch.common.bytes.BytesReference;
@@ -213,15 +212,7 @@ public class PercolateQueryBuilder extends AbstractQueryBuilder<PercolateQueryBu
         super(in);
         field = in.readString();
         name = in.readOptionalString();
-        if (in.getTransportVersion().before(TransportVersions.V_8_0_0)) {
-            String documentType = in.readOptionalString();
-            assert documentType == null;
-        }
         indexedDocumentIndex = in.readOptionalString();
-        if (in.getTransportVersion().before(TransportVersions.V_8_0_0)) {
-            String indexedDocumentType = in.readOptionalString();
-            assert indexedDocumentType == null;
-        }
         indexedDocumentId = in.readOptionalString();
         indexedDocumentRouting = in.readOptionalString();
         indexedDocumentPreference = in.readOptionalString();
@@ -255,15 +246,7 @@ public class PercolateQueryBuilder extends AbstractQueryBuilder<PercolateQueryBu
         }
         out.writeString(field);
         out.writeOptionalString(name);
-        if (out.getTransportVersion().before(TransportVersions.V_8_0_0)) {
-            // In 7x, typeless percolate queries are represented by null documentType values
-            out.writeOptionalString(null);
-        }
         out.writeOptionalString(indexedDocumentIndex);
-        if (out.getTransportVersion().before(TransportVersions.V_8_0_0)) {
-            // In 7x, typeless percolate queries are represented by null indexedDocumentType values
-            out.writeOptionalString(null);
-        }
         out.writeOptionalString(indexedDocumentId);
         out.writeOptionalString(indexedDocumentRouting);
         out.writeOptionalString(indexedDocumentPreference);

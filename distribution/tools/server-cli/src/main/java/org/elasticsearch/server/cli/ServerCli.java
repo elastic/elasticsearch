471f4d0ad76e654be3bb13c8568455e8d9d3a447
Mark Vieira <portugee@gmail.com>
2025-12-05 10:20:35 -0800

    Add clarifying comment

--- distribution/tools/server-cli/src/main/java/org/elasticsearch/server/cli/ServerCli.java
+++ distribution/tools/server-cli/src/main/java/org/elasticsearch/server/cli/ServerCli.java
@@ -118,8 +118,9 @@ class ServerCli extends EnvironmentAwareCommand {
             return;
         }
 
-        // we are running in the foreground, so wait for the server to exit
+        // Call the GC to try and free up as much heap as we can since we don't intend to do much if any more allocation after this
         System.gc();
+        // we are running in the foreground, so wait for the server to exit
         int exitCode = server.waitFor();
         onExit(exitCode);
     }

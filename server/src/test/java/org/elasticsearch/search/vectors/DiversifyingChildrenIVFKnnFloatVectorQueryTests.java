c971c74d0bfe902b1517109a859800575ba83b9c
Panagiotis Bailis <pmpailis@gmail.com>
2025-12-10 11:12:09 +0200

    fix comp

--- server/src/test/java/org/elasticsearch/search/vectors/DiversifyingChildrenIVFKnnFloatVectorQueryTests.java
+++ server/src/test/java/org/elasticsearch/search/vectors/DiversifyingChildrenIVFKnnFloatVectorQueryTests.java
@@ -18,7 +18,7 @@ public class DiversifyingChildrenIVFKnnFloatVectorQueryTests extends AbstractDiv
 
     @Override
     Query getDiversifyingChildrenKnnQuery(String fieldName, float[] queryVector, Query childFilter, int k, BitSetProducer parentBitSet) {
-        return new DiversifyingChildrenIVFKnnFloatVectorQuery(fieldName, queryVector, k, k, childFilter, parentBitSet, 0, 1.0f);
+        return new DiversifyingChildrenIVFKnnFloatVectorQuery(fieldName, queryVector, k, k, childFilter, parentBitSet, 0);
     }
 
     @Override

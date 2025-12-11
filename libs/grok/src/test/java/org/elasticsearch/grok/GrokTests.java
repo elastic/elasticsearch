5fa78079c3c843a6416f42a0db8bd59a53f9fa97
Joe Gallo <joegallo@gmail.com>
2025-12-11 15:18:35 -0500

    testExponentialExpressions still shows that this works
    but MatcherWatchdogTests is no longer valuable

--- libs/grok/src/test/java/org/elasticsearch/grok/GrokTests.java
+++ libs/grok/src/test/java/org/elasticsearch/grok/GrokTests.java
@@ -20,9 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.DoubleConsumer;
 import java.util.function.Function;
@@ -802,46 +800,12 @@ public class GrokTests extends ESTestCase {
     }
 
     private void testExponentialExpressions(boolean ecsCompatibility) {
-        AtomicBoolean run = new AtomicBoolean(true); // to avoid a lingering thread when test has completed
-
-        // keeping track of the matcher watchdog and whether it has executed at all (hunting a rare test failure)
-        AtomicBoolean hasExecuted = new AtomicBoolean(false);
-
         String grokPattern = "Bonsuche mit folgender Anfrage: Belegart->\\[%{WORD:param2},(?<param5>(\\s*%{NOTSPACE})*)\\] "
             + "Zustand->ABGESCHLOSSEN Kassennummer->%{WORD:param9} Bonnummer->%{WORD:param10} Datum->%{DATESTAMP_OTHER:param11}";
         String logLine = "Bonsuche mit folgender Anfrage: Belegart->[EINGESCHRAENKTER_VERKAUF, VERKAUF, NACHERFASSUNG] "
             + "Zustand->ABGESCHLOSSEN Kassennummer->2 Bonnummer->6362 Datum->Mon Jan 08 00:00:00 UTC 2018";
-        BiConsumer<Long, Runnable> scheduler = (delay, command) -> {
-            hasExecuted.set(true);
-            Thread t = new Thread(() -> {
-                try {
-                    Thread.sleep(delay);
-                } catch (InterruptedException e) {
-                    throw new AssertionError(e);
-                }
-                if (run.get()) {
-                    command.run();
-                }
-            });
-            t.start();
-        };
         Grok grok = new Grok(GrokBuiltinPatterns.get(ecsCompatibility), grokPattern, MatcherWatchdog.newInstance(200), logger::warn);
-
-        // hunting a rare test failure -- sometimes we get a failure in the expectThrows below, and the most
-        // logical reason for it to be hit is that the matcher watchdog just never even started up.
-        Thread t = new Thread(() -> {
-            try {
-                Thread.sleep(100); // half of max execution, 10x the interval, should be plenty
-            } catch (InterruptedException e) {
-                throw new AssertionError(e);
-            }
-            assertTrue("The MatchWatchdog scheduler should have run by now", hasExecuted.get());
-        });
-        t.setName("Quis custodiet ipsos custodes?");
-        t.start();
-
         Exception e = expectThrows(RuntimeException.class, () -> grok.captures(logLine));
-        run.set(false);
         assertThat(e.getMessage(), equalTo("grok pattern matching was interrupted after [200] ms"));
     }
 

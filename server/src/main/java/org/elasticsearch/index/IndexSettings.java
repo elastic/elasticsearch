595246901ca6807ba25fc562cebaa2294945a984
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2025-12-15 14:56:02 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/index/IndexSettings.java
+++ server/src/main/java/org/elasticsearch/index/IndexSettings.java
@@ -1245,7 +1245,8 @@ public final class IndexSettings {
         useDocValuesSkipper = scopedSettings.get(USE_DOC_VALUES_SKIPPER);
         useDocValuesSkipperForHostname = USE_DOC_VALUES_SKIPPER.exists(settings)
             ? scopedSettings.get(USE_DOC_VALUES_SKIPPER)
-            : version.onOrAfter(IndexVersions.SKIPPERS_ENABLED_BY_DEFAULT) && version.before(IndexVersions.STATELESS_SKIPPERS_ENABLED_FOR_TSDB);
+            : version.onOrAfter(IndexVersions.SKIPPERS_ENABLED_BY_DEFAULT)
+                && version.before(IndexVersions.STATELESS_SKIPPERS_ENABLED_FOR_TSDB);
         seqNoIndexOptions = scopedSettings.get(SEQ_NO_INDEX_OPTIONS_SETTING);
         useTimeSeriesDocValuesFormat = scopedSettings.get(USE_TIME_SERIES_DOC_VALUES_FORMAT_SETTING);
         useTimeSeriesDocValuesFormatLargeBlockSize = scopedSettings.get(USE_TIME_SERIES_DOC_VALUES_FORMAT_LARGE_BLOCK_SIZE);

7a5dc6ac135fcd0b73a5ec6025cc4c47efaf13ab
Mark Tozzi <mark.tozzi@gmail.com>
2025-12-17 12:05:51 -0500

    Histogram to tdigest function (#139637)
    Adds a function to convert from histogram data types to tdigest data types. This unfortunately requires decoding the histogram, even though the two encodings are the same, since we need to calculate the summary fields that don't exist for histogram. Maybe we can improve that down the line.
    
    The testing for this is very sparse. We need to work on that in the near future, but this should be enough for a prototype.
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
    Co-authored-by: Jonas Kunz <j+github@kunzj.de>

--- x-pack/plugin/esql/compute/gen/src/main/java/org/elasticsearch/compute/gen/Methods.java
+++ x-pack/plugin/esql/compute/gen/src/main/java/org/elasticsearch/compute/gen/Methods.java
@@ -51,6 +51,7 @@ import static org.elasticsearch.compute.gen.Types.LONG_BLOCK_BUILDER;
 import static org.elasticsearch.compute.gen.Types.LONG_VECTOR;
 import static org.elasticsearch.compute.gen.Types.LONG_VECTOR_BUILDER;
 import static org.elasticsearch.compute.gen.Types.LONG_VECTOR_FIXED_BUILDER;
+import static org.elasticsearch.compute.gen.Types.TDIGEST_BLOCK_BUILDER;
 
 /**
  * Finds declared methods for the code generator.
@@ -229,6 +230,9 @@ public class Methods {
         if (t.equals(TypeName.FLOAT) || t.equals(FLOAT_BLOCK_BUILDER)) {
             return "appendFloat";
         }
+        if (t.equals(Types.TDIGEST) || t.equals(Types.TDIGEST_BLOCK)) {
+            return "appendTDigest";
+        }
         throw new IllegalArgumentException("unknown append method for [" + t + "]");
     }
 
@@ -287,6 +291,9 @@ public class Methods {
         }
         if (t.equals(FLOAT_VECTOR_FIXED_BUILDER)) {
             return "newFloatVectorFixedBuilder";
+        }
+        if (t.equals(TDIGEST_BLOCK_BUILDER)) {
+            return "newTDigestBlockBuilder";
         }
         throw new IllegalArgumentException("unknown build method for [" + t + "]");
     }
--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1615,6 +1615,10 @@ public class EsqlCapabilities {
         HISTOGRAM_FIELD_SUPPORT_V0,
 
         /**
+         * histogram to tdigest conversion function
+         */
+        HISTOGRAM_TO_TDIGEST_CAST,
+        /**
          * Create new block when filtering OrdinalBytesRefBlock
          */
         FIX_FILTER_ORDINALS,

ef35613d41b30dd443068da5e4426dc4d9923416
Jonas Kunz <jonas.kunz@elastic.co>
2025-12-19 13:28:42 +0100

    Change number of cores, add script

---
+++ run-rate-query.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Sends an ES|QL query to localhost:9200 and prints row count and duration (ms)
+
+ES_URL="http://localhost:9200/_query"
+QUERY='TS metrics-hostmetricsreceiver.otel-default | STATS avg = AVG(RATE(`metrics.system.cpu.time`)) BY host.name, TBUCKET(5m) | STATS SUM(avg) by host.name | LIMIT 10000'
+
+for i in {1..30}; do
+  start_ms=$(gdate +%s%3N)
+  response=$(curl -s -H "Content-Type: application/json" -X POST "$ES_URL" -d "{\"query\":\"$QUERY\"}")
+  end_ms=$(gdate +%s%3N)
+  duration_ms=$((end_ms - start_ms))
+
+  # ES|QL tabular responses contain a top-level "values" array
+  rows=$(jq -r '.values | length' <<< "$response")
+
+  echo "run=$i rows=$rows"
+  echo "run=$i duration_ms=$duration_ms"
+done
+

499c940144c3e287807164764f49de033fb51e62
Andrei Dan <andrei.dan@elastic.co>
2025-12-19 12:46:56 +0000

    ReplicasUpdaterService: Replace the running boolean with a state machine (#5052)
    With the upcoming replicas for load balancing work we'll trigger running the replicas
    changes loop when reacting to a few events:
    1. we received the desired topology
    2. master failover
    3. node left the cluster
    4. on the timer - every 5 minutes
    5. replicas management (for instant failover or load balancing) enabled/disabled via setting
    
    The current implementation that used the `running` boolean didn't guarantee we will
    run the loop when triggered outside the loop (we even document the we might not do anything
    when triggered externally:
    https://github.com/elastic/elasticsearch-serverless/blob/9704b8c97ad5ec944732a8be1f1113a8fd92e896/modules/serverless-stateless/src/main/java/co/elastic/elasticsearch/stateless/autoscaling/search/ReplicasUpdaterService.java#L648)
    
    With replicas for load balancing we'd like to have a guranteed pending run executed when
    triggered as a response to an event as we would like to stay within the bounds of the current
    autoscaling step (i.e. when scaling from 3X14Gi to 2X8Gi we should make sure no indices have
    3 replicas configured as soon as possible, when a node leaves the cluster, so we're not delaying
    shutdowns and staying in yellow health).
    
    This PR adds an extra state to the replicas updater service, moving away from the binary
    running/not running to IDLE, RUNNING, RUNNING_WITH_PENDING.
    If a request to run the replicas updater service comes whilst in the RUNNING state already
    we'll capture it and change the state to RUNNING_WITH_PENDING. When the already running
    loop ends it will run again if the status was changed to RUNNING_WITH_PENDING.

---
+++ x-pack/plugin/stateless/src/main/java/co/elastic/elasticsearch/stateless/autoscaling/search/ReplicasUpdaterExecutionListener.java
@@ -0,0 +1,70 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ * Copyright Elasticsearch B.V. All rights reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch B.V. and its suppliers, if any.
+ * The intellectual and technical concepts contained herein
+ * are proprietary to Elasticsearch B.V. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright
+ * law.  Dissemination of this information or reproduction of
+ * this material is strictly forbidden unless prior written
+ * permission is obtained from Elasticsearch B.V.
+ */
+
+package co.elastic.elasticsearch.stateless.autoscaling.search;
+
+/**
+* Hook interface for testing and observability of the replicas updater service.
+* Production code uses {@link NoopExecutionListener}.
+* Tests can inject custom implementations to control execution timing.
+*/
+interface ReplicasUpdaterExecutionListener {
+    /**
+     * Called when the replicas updater loop starts executing
+     */
+    void onRunStart(boolean immediateScaleDown);
+
+    /**
+     * Called when the loop completes
+     */
+    void onRunComplete();
+
+    /**
+     * Called trigger to run the loop is skipped because already running
+     */
+    void onSkipped();
+
+    /**
+     * Called when the currently running loop executes a pending request to run
+     */
+    void onPendingRunExecution();
+
+    /**
+     * Called immediately after the state transitions to IDLE
+     */
+    void onStateTransitionToIdle();
+
+    class NoopExecutionListener implements ReplicasUpdaterExecutionListener {
+
+        public static final NoopExecutionListener INSTANCE = new NoopExecutionListener();
+
+        @Override
+        public void onRunStart(boolean immediateScaleDown) {}
+
+        @Override
+        public void onRunComplete() {}
+
+        @Override
+        public void onSkipped() {}
+
+        @Override
+        public void onPendingRunExecution() {}
+
+        @Override
+        public void onStateTransitionToIdle() {}
+    }
+}

f6747fdf281d0f51229fdf16d084e7d69ce5d54b
Charlotte Hoblik <sarolta@saroltah.hu>
2025-12-19 12:53:13 +0100

    Add information about text fields

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/regex/WildcardLike.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/regex/WildcardLike.java
@@ -51,6 +51,10 @@ public class WildcardLike extends RegexMatch<WildcardPattern> {
 
         // we use an inline example here because ?pattern not supported in csv-spec test
         detailedDescription = """
+            When used on `text` fields, `LIKE` treats the field as a `keyword` and does not use the analyzer.
+            This means the pattern matching is case-sensitive and must match the exact string indexed.
+            To perform full-text search, use the [`MATCH`](/reference/query-languages/esql/functions-operators/search-functions.md#esql-match) or [`QSTR`](/reference/query-languages/esql/functions-operators/search-functions.md#esql-qstr) functions.
+
             Matching the exact characters `*` and `.` will require escaping.
             The escape character is backslash `\\`. Since also backslash is a special character in string literals,
             it will require further escaping.

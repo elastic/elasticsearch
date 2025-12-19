1111a5bd8905b93299003858c6f8939db1f8cf5b
Jonathan Buttner <jonathan.buttner@elastic.co>
2025-12-19 15:09:28 -0500

    Fixing auth response

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/elastic/ElasticInferenceServiceTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/elastic/ElasticInferenceServiceTests.java
@@ -1373,15 +1373,15 @@ public class ElasticInferenceServiceTests extends ESSingleNodeTestCase {
                       "task_types": ["chat"]
                     },
                     {
-                      "model_name": "elser_model_2",
+                      "model_name": "elser-v2",
                       "task_types": ["embed/text/sparse"]
                     },
                     {
-                      "model_name": "jina-embeddings-v3",
+                      "model_name": "multilingual-embed-v1",
                       "task_types": ["embed/text/dense"]
                     },
-                    {
-                      "model_name": "elastic-rerank-v1",
+                  {
+                      "model_name": "rerank-v1",
                       "task_types": ["rerank/text/text-similarity"]
                     }
                 ]

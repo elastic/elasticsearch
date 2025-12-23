4cb8da058c68e11bcacfcd56b44ffd113242eb6f
Kathleen DeRusso <kathleen.derusso@elastic.co>
2025-12-23 09:55:57 -0500

    Update x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/TopSnippets.java
    Co-authored-by: Liam Thompson <leemthompo@gmail.com>

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/TopSnippets.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/string/TopSnippets.java
@@ -92,7 +92,7 @@ public class TopSnippets extends EsqlScalarFunction implements OptionalArgument 
             """) Expression query,
         @MapParam(
             name = "options",
-            description = "(Optional) TopSnippets additional options as <<esql-function-named-params,function named parameters>>.",
+            description = "(Optional) TopSnippets additional options as [function named parameters](/reference/query-languages/esql/esql-syntax.md#esql-function-named-params)",
             optional = true,
             params = {
                 @MapParam.MapParamEntry(

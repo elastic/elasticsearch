a96eb393b9f1c8eff0f4cbbe38380ed6ff40f752
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2026-01-02 18:23:05 +0100

    Resolve all references in PromqlCommand

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/logical/promql/PromqlCommand.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/logical/promql/PromqlCommand.java
@@ -12,6 +12,7 @@ import org.elasticsearch.xpack.esql.capabilities.PostAnalysisVerificationAware;
 import org.elasticsearch.xpack.esql.capabilities.TelemetryAware;
 import org.elasticsearch.xpack.esql.common.Failures;
 import org.elasticsearch.xpack.esql.core.expression.Attribute;
+import org.elasticsearch.xpack.esql.core.expression.AttributeSet;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
 import org.elasticsearch.xpack.esql.core.expression.Literal;
 import org.elasticsearch.xpack.esql.core.expression.NameId;
@@ -254,6 +255,14 @@ public class PromqlCommand extends UnaryPlan implements TelemetryAware, PostAnal
         sb.append(promqlPlan.toString());
         sb.append("\n<>]]");
         return sb.toString();
+    }
+
+    @Override
+    protected AttributeSet computeReferences() {
+        // Ensures field resolution is aware of all attributes used in the PromQL plan.
+        AttributeSet.Builder references = AttributeSet.builder();
+        promqlPlan().forEachDown(lp -> references.addAll(lp.references()));
+        return references.build();
     }
 
     @Override

5b41d77ee9d6207bb7986e0580a4152962318dc1
Sam Xiao <sam.xiao@elastic.co>
2026-01-05 13:45:22 -0500

    Add List Reindex API

---
+++ modules/reindex-management/src/main/java/org/elasticsearch/reindex/management/ListReindexRequest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v 3.0 only", or the "Server Side Public License, v 1".
+ */
+
+package org.elasticsearch.reindex.management;
+
+import org.elasticsearch.action.support.tasks.BaseTasksRequest;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.index.reindex.ReindexAction;
+import org.elasticsearch.tasks.Task;
+
+import java.io.IOException;
+
+/**
+ * Request to list all running reindex tasks.
+ * <p>
+ * This request automatically filters:
+ * <ul>
+ *   <li>Only reindex tasks</li>
+ *   <li>Only parent tasks</li>
+ *   <li>Only tasks for the current project (handled by TransportTasksProjectAction)</li>
+ * </ul>
+ */
+public class ListReindexRequest extends BaseTasksRequest<ListReindexRequest> {
+
+    public ListReindexRequest() {
+        super();
+    }
+
+    public ListReindexRequest(StreamInput in) throws IOException {
+        super(in);
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        super.writeTo(out);
+    }
+
+    @Override
+    public boolean match(Task task) {
+        if (super.match(task) == false) {
+            return false;
+        }
+
+        // filter for only reindex tasks
+        if (ReindexAction.NAME.equals(task.getAction()) == false) {
+            return false;
+        }
+
+        // Filter out subtasks
+        if (task.getParentTaskId().isSet()) {
+            return false;
+        }
+        return true;
+    }
+}

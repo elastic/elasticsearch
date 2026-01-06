79c56c5d916fdd0ab7974bdf61b79c2d07d3b8b6
Mikhail Berezovskiy <mikhail@elastic.co>
2026-01-05 23:19:11 -0800

    tests

--- server/src/main/java/org/elasticsearch/snapshots/SnapshotsService.java
+++ server/src/main/java/org/elasticsearch/snapshots/SnapshotsService.java
@@ -272,7 +272,9 @@ public final class SnapshotsService extends AbstractLifecycleComponent implement
         final SnapshotId snapshotId = new SnapshotId(snapshotName, request.uuid());
         Repository repository = repositoriesService.repository(projectId, request.repository());
         if (repository.isReadOnly()) {
-            listener.onFailure(new RepositoryException(repository.getMetadata().name(), "cannot create snapshot in a readonly repository"));
+            listener.onFailure(
+                new IllegalArgumentException("[" + repository.getMetadata().name() + "] cannot create snapshot in a readonly repository")
+            );
             return;
         }
         submitCreateSnapshotRequest(

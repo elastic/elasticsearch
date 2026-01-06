bc3b1f3cb4b034ba55bb79d938100f4cba2dd07d
Nick Tindall <nick.tindall@elastic.co>
2026-01-06 16:04:54 +1030

    Fix Decision.Type serialization BWC

--- server/src/test/java/org/elasticsearch/cluster/routing/allocation/decider/DecisionTests.java
+++ server/src/test/java/org/elasticsearch/cluster/routing/allocation/decider/DecisionTests.java
@@ -9,11 +9,18 @@
 
 package org.elasticsearch.cluster.routing.allocation.decider;
 
+import org.elasticsearch.TransportVersion;
+import org.elasticsearch.cluster.routing.allocation.decider.Decision.Type;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.io.stream.BytesStreamOutput;
+import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.EnumSerializationTestUtils;
 
+import java.io.IOException;
 import java.util.List;
 
+import static org.elasticsearch.cluster.routing.allocation.decider.Decision.Type.ALLOCATION_DECISION_NOT_PREFERRED;
 import static org.elasticsearch.cluster.routing.allocation.decider.Decision.Type.NO;
 import static org.elasticsearch.cluster.routing.allocation.decider.Decision.Type.NOT_PREFERRED;
 import static org.elasticsearch.cluster.routing.allocation.decider.Decision.Type.THROTTLE;
@@ -24,8 +31,10 @@ import static org.elasticsearch.cluster.routing.allocation.decider.Decision.Type
  */
 public class DecisionTests extends ESTestCase {
 
+    private static final Type PREVIOUS_YES_VALUE = Type.values()[1];
+
     public void testTypeEnumOrder() {
-        EnumSerializationTestUtils.assertEnumSerialization(Decision.Type.class, NO, NOT_PREFERRED, THROTTLE, YES);
+        EnumSerializationTestUtils.assertEnumSerialization(Type.class, NO, NOT_PREFERRED, THROTTLE, YES);
     }
 
     public void testTypeHigherThan() {
@@ -37,4 +46,43 @@ public class DecisionTests extends ESTestCase {
         List.of(NO, THROTTLE).forEach(d -> assertFalse(d.assignmentAllowed()));
     }
 
+    public void testSerializationBackwardCompatibility() throws IOException {
+        testReadWriteEnum(YES, YES, ALLOCATION_DECISION_NOT_PREFERRED);
+        testReadWriteEnum(NOT_PREFERRED, THROTTLE, ALLOCATION_DECISION_NOT_PREFERRED);
+        testReadWriteEnum(THROTTLE, NOT_PREFERRED, ALLOCATION_DECISION_NOT_PREFERRED);
+        testReadWriteEnum(NO, NO, ALLOCATION_DECISION_NOT_PREFERRED);
+
+        // Older versions, lossy serialization - remove when no longer supported
+        assertFalse(TransportVersion.minimumCompatible().supports(ALLOCATION_DECISION_NOT_PREFERRED));
+        // YES/NOT_PREFERRED turn into ordinal 1 which is YES in those versions, both round-trip to YES
+        testReadWriteEnum(YES, PREVIOUS_YES_VALUE, TransportVersion.minimumCompatible());
+        testReadWriteEnum(NOT_PREFERRED, PREVIOUS_YES_VALUE, YES, TransportVersion.minimumCompatible());
+        // THROTTLE and NO are unchanged
+        testReadWriteEnum(THROTTLE, THROTTLE, TransportVersion.minimumCompatible());
+        testReadWriteEnum(NO, NO, TransportVersion.minimumCompatible());
+    }
+
+    private void testReadWriteEnum(Decision.Type value, Decision.Type expectedSerialisation, TransportVersion remoteTransportVersion)
+        throws IOException {
+        testReadWriteEnum(value, expectedSerialisation, value, remoteTransportVersion);
+    }
+
+    private void testReadWriteEnum(
+        Decision.Type value,
+        Decision.Type expectedSerialisation,
+        Decision.Type roundTripValue,
+        TransportVersion remoteTransportVersion
+    ) throws IOException {
+        final var output = new BytesStreamOutput();
+        output.setTransportVersion(remoteTransportVersion);
+        value.writeTo(output);
+        expectValue(expectedSerialisation, TransportVersion.current(), output.bytes());
+        expectValue(roundTripValue, remoteTransportVersion, output.bytes());
+    }
+
+    private void expectValue(Decision.Type expected, TransportVersion readAsTransportVersion, BytesReference bytes) throws IOException {
+        final StreamInput currentValueInput = bytes.streamInput();
+        currentValueInput.setTransportVersion(readAsTransportVersion);
+        assertEquals(expected, Type.readFrom(currentValueInput));
+    }
 }

3da6822015a7a64541a7705f5e2e1d368be1c56b
Nick Tindall <nick.tindall@elastic.co>
2026-01-07 15:33:20 +1030

    Minimise diff

--- server/src/main/java/org/elasticsearch/cluster/routing/allocation/decider/AllocationDeciders.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/allocation/decider/AllocationDeciders.java
@@ -208,7 +208,7 @@ public class AllocationDeciders {
             Decision mostNegativeDecision = Decision.YES;
             for (AllocationDecider decider : deciders) {
                 var decision = deciderAction.apply(decider);
-                if (decision.type().compareToBetweenDecisions(mostNegativeDecision.type()) < 0) {
+                if (mostNegativeDecision.type().compareToBetweenDecisions(decision.type()) > 0) {
                     mostNegativeDecision = decision;
                     if (mostNegativeDecision.type() == Decision.Type.NO) {
                         traceNoDecisions(decider, decision, logMessageCreator);

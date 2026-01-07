6ca10aac67655600cb7e40044a5cf5f9883bff5f
Stanislav Malyshev <stas.malyshev@elastic.co>
2026-01-06 18:40:08 -0700

    Add ESQL test

---
+++ x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/EsqlQueryLogingIT.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.esql.action;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.common.logging.AccumulatingMockAppender;
+import org.elasticsearch.common.logging.ESLogMessage;
+import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.xpack.esql.VerificationException;
+import org.elasticsearch.xpack.esql.querylog.EsqlLogProducer;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import static org.elasticsearch.common.logging.action.ActionLogger.SEARCH_ACTION_LOGGER_ENABLED;
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+public class EsqlQueryLogingIT extends AbstractEsqlIntegTestCase {
+    static AccumulatingMockAppender appender;
+    static Logger queryLog = LogManager.getLogger(EsqlLogProducer.LOGGER_NAME);
+    static Level origQueryLogLevel = queryLog.getLevel();
+
+    @BeforeClass
+    public static void init() throws IllegalAccessException {
+        appender = new AccumulatingMockAppender("trace_appender");
+        appender.start();
+        Loggers.addAppender(queryLog, appender);
+
+        Loggers.setLevel(queryLog, Level.TRACE);
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        Loggers.removeAppender(queryLog, appender);
+        appender.stop();
+        Loggers.setLevel(queryLog, origQueryLogLevel);
+    }
+
+    @Before
+    public void enableLog() {
+        updateClusterSettings(Settings.builder().put(SEARCH_ACTION_LOGGER_ENABLED.getConcreteSettingForNamespace("esql").getKey(), true));
+        appender.reset();
+    }
+
+    @After
+    public void restoreLog() {
+        updateClusterSettings(
+            Settings.builder().put(SEARCH_ACTION_LOGGER_ENABLED.getConcreteSettingForNamespace("esql").getKey(), (String) null)
+        );
+    }
+
+    public void testLogging() throws Exception {
+        int numDocs1 = randomIntBetween(1, 15);
+        assertAcked(client().admin().indices().prepareCreate("index-1").setMapping("host", "type=keyword"));
+        for (int i = 0; i < numDocs1; i++) {
+            client().prepareIndex("index-1").setSource("host", "192." + i).get();
+        }
+        int numDocs2 = randomIntBetween(1, 15);
+        assertAcked(client().admin().indices().prepareCreate("index-2").setMapping("host", "type=keyword"));
+        for (int i = 0; i < numDocs2; i++) {
+            client().prepareIndex("index-2").setSource("host", "10." + i).get();
+        }
+
+        client().admin().indices().prepareRefresh("index-1", "index-2").get();
+
+        assertQuery("FROM index-* | EVAL ip = to_ip(host) | STATS s = COUNT(*) by ip | KEEP ip | LIMIT 100");
+        assertFailedQuery(
+            "FROM index-* | EVAL a = count(*) | LIMIT 100",
+            "aggregate function [count(*)] not allowed outside STATS command",
+            VerificationException.class.getName()
+        );
+    }
+
+    private void assertQuery(String query) {
+        try (var resp = run(query)) {
+            assertNotNull(appender.lastEvent());
+            var message = (ESLogMessage) appender.getLastEventAndReset().getMessage();
+            var data = message.getIndexedReadOnlyStringMap();
+            assertThat(message.get("success"), equalTo("true"));
+            assertThat(message.get("type"), equalTo("esql"));
+            assertThat(data.getValue("took"), greaterThan(0L));
+            assertThat(data.getValue("took_millis"), greaterThanOrEqualTo(0L));
+            assertThat(message.get("query"), equalTo(query));
+        }
+    }
+
+    private void assertFailedQuery(String query, String expectedMessage, String expectedException) {
+        expectThrows(VerificationException.class, () -> run(query));
+        assertNotNull(appender.lastEvent());
+        var message = (ESLogMessage) appender.getLastEventAndReset().getMessage();
+        var data = message.getIndexedReadOnlyStringMap();
+        assertThat(message.get("success"), equalTo("false"));
+        assertThat(message.get("type"), equalTo("esql"));
+        assertThat(data.getValue("took"), greaterThan(0L));
+        assertThat(data.getValue("took_millis"), greaterThanOrEqualTo(0L));
+        assertThat(message.get("query"), equalTo(query));
+        assertThat(message.get("error.message"), containsString(expectedMessage));
+        assertThat(message.get("error.type"), equalTo(expectedException));
+    }
+}

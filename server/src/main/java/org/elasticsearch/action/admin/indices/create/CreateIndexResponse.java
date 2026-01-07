30e741eea7cf5e3f8dba17589a4e48b17150cdf7
zhubotang-wq <zhubo.tang@elastic.co>
2026-01-07 01:38:26 -0800

    Initial Proposal for Index Limit

--- server/src/main/java/org/elasticsearch/action/admin/indices/create/CreateIndexResponse.java
+++ server/src/main/java/org/elasticsearch/action/admin/indices/create/CreateIndexResponse.java
@@ -12,6 +12,7 @@ package org.elasticsearch.action.admin.indices.create;
 import org.elasticsearch.action.support.master.ShardsAcknowledgedResponse;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.xcontent.ParseField;
 import org.elasticsearch.xcontent.XContentBuilder;
 
@@ -24,17 +25,28 @@ import java.util.Objects;
 public class CreateIndexResponse extends ShardsAcknowledgedResponse {
 
     public static final ParseField INDEX = new ParseField("index");
+    public static final ParseField INDEX_LIMIT_TIER_MESSAGE = new ParseField("index_limit_tier_message");
 
     private final String index;
+
+    private final IndexLimitTier indexLimitTier;
 
     public CreateIndexResponse(StreamInput in) throws IOException {
         super(in, true);
         index = in.readString();
+        indexLimitTier = in.readEnum(IndexLimitTier.class);
     }
 
     public CreateIndexResponse(boolean acknowledged, boolean shardsAcknowledged, String index) {
         super(acknowledged, shardsAcknowledged);
         this.index = Objects.requireNonNull(index);
+        this.indexLimitTier = IndexLimitTier.PASS;
+    }
+
+    public CreateIndexResponse(boolean acknowledged, boolean shardsAcknowledged, String index, IndexLimitTier indexLimitTier) {
+        super(acknowledged, shardsAcknowledged);
+        this.index = Objects.requireNonNull(index);
+        this.indexLimitTier = indexLimitTier;
     }
 
     @Override
@@ -42,6 +54,7 @@ public class CreateIndexResponse extends ShardsAcknowledgedResponse {
         super.writeTo(out);
         writeShardsAcknowledged(out);
         out.writeString(index);
+        out.writeEnum(indexLimitTier);
     }
 
     public String index() {
@@ -52,6 +65,7 @@ public class CreateIndexResponse extends ShardsAcknowledgedResponse {
     protected void addCustomFields(XContentBuilder builder, Params params) throws IOException {
         super.addCustomFields(builder, params);
         builder.field(INDEX.getPreferredName(), index());
+        builder.field(INDEX_LIMIT_TIER_MESSAGE.getPreferredName(), indexLimitTier.message);
     }
 
     @Override
@@ -66,5 +80,45 @@ public class CreateIndexResponse extends ShardsAcknowledgedResponse {
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), index);
+    }
+
+    public enum IndexLimitTier implements Writeable {
+        PASS(0, 11_249, "Success"),
+        NUDGE(11_250, 13_499, "Nudge"),
+        WARN(13_500, 14_699, "Warn"),
+        CRITICAL(14_701, 14_999, "Critical"),
+        BLOCK(15_000, Integer.MAX_VALUE, "Blocked");
+
+        private final int minInclusive;
+        private final int maxInclusive;
+        private final String message;
+
+        IndexLimitTier(int minInclusive, int maxInclusive, String message) {
+            this.minInclusive = minInclusive;
+            this.maxInclusive = maxInclusive;
+            this.message = message;
+        }
+
+        public boolean matches(int value) {
+            return value >= minInclusive && value <= maxInclusive;
+        }
+
+        public boolean matches(String name) {
+            return this.name().equals(name);
+        }
+
+        public static IndexLimitTier parse(int value) {
+            for (IndexLimitTier tier : values()) {
+                if (tier.matches(value)) {
+                    return tier;
+                }
+            }
+            throw new IllegalArgumentException("Value out of range: " + value);
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) throws IOException {
+            out.writeEnum(this);
+        }
     }
 }

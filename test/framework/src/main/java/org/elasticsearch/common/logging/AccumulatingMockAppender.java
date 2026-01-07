ebc739086a777af38903f1c9cb52e0e5045c49d1
Stanislav Malyshev <stas.malyshev@elastic.co>
2026-01-07 13:43:08 -0700

    use synchronized list - since it's a test class, it's the simplest way

--- test/framework/src/main/java/org/elasticsearch/common/logging/AccumulatingMockAppender.java
+++ test/framework/src/main/java/org/elasticsearch/common/logging/AccumulatingMockAppender.java
@@ -12,20 +12,21 @@ package org.elasticsearch.common.logging;
 import org.apache.logging.log4j.core.LogEvent;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Mock log appender class that will capture all the events and store them.
  */
 public class AccumulatingMockAppender extends MockAppender {
-    public final List<LogEvent> events = new ArrayList<>();
+    public final List<LogEvent> events = Collections.synchronizedList(new ArrayList<>());
 
     public AccumulatingMockAppender(final String name) throws IllegalAccessException {
         super(name);
     }
 
     @Override
-    public synchronized void append(LogEvent event) {
+    public void append(LogEvent event) {
         events.add(event.toImmutable());
     }
 

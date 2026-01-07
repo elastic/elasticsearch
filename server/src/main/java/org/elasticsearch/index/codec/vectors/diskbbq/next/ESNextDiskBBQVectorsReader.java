d273843c4ae24c1988492d364ce75b87731d91e2
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-01-07 20:02:00 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/index/codec/vectors/diskbbq/next/ESNextDiskBBQVectorsReader.java
+++ server/src/main/java/org/elasticsearch/index/codec/vectors/diskbbq/next/ESNextDiskBBQVectorsReader.java
@@ -56,7 +56,7 @@ public class ESNextDiskBBQVectorsReader extends IVFVectorsReader {
     }
 
     CentroidIterator getPostingListPrefetchIterator(CentroidIterator centroidIterator, IndexInput postingListSlice) throws IOException {
-        //TODO we may want to prefetch more than one postings list, however, we will likely want to place a limit
+        // TODO we may want to prefetch more than one postings list, however, we will likely want to place a limit
         // so we don't bother prefetching many lists we won't end up scoring
         return new PrefetchingCentroidIterator(centroidIterator, postingListSlice);
     }

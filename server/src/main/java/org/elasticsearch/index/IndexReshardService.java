9944fb8f1bb30b632848963e88567bb50e5037ff
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2026-01-08 15:01:28 -0800

    Fail shard snapshot if the index is/was resharded

--- server/src/main/java/org/elasticsearch/index/IndexReshardService.java
+++ server/src/main/java/org/elasticsearch/index/IndexReshardService.java
@@ -12,8 +12,10 @@ package org.elasticsearch.index;
 import org.elasticsearch.TransportVersion;
 import org.elasticsearch.cluster.ProjectState;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.index.shard.ShardId;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -36,5 +38,31 @@ public class IndexReshardService {
             }
         }
         return indices;
+    }
+
+    /// Determines if a shard snapshot is impacted by possibly happening resharding operation.
+    /// Such shard snapshot may contain data inconsistent with other shards due to metadata changes or data movement
+    /// performed in scope of resharding.
+    /// @param maximumShardIdForIndexInTheSnapshot maximum [ShardId] by [ShardId#id()] of the same index as the provided `indexShard`
+    /// that is present in the snapshot metadata. This value is used to detect previously completed resharding operations.
+    public static boolean isShardSnapshotImpactedByResharding(IndexMetadata indexMetadata, ShardId maximumShardIdForIndexInTheSnapshot) {
+        Objects.requireNonNull(maximumShardIdForIndexInTheSnapshot);
+
+        // Presence of resharding metadata obviously means that the snapshot is impacted.
+        if (indexMetadata.getReshardingMetadata() != null) {
+            return true;
+        } else if (maximumShardIdForIndexInTheSnapshot.id() < indexMetadata.getNumberOfShards() - 1) {
+            // However resharding metadata may not be present because resharding has completed already
+            // but still could have impacted this snapshot.
+            // If a snapshot doesn't contain the shard with maximum id
+            // given the number of shards in the index metadata it means that there was a resharding
+            // that increased the number of shards since the snapshot was created.
+            // E.g. with resharding split 4 -> 8 an up-to-date snapshot would contain shard id 7.
+            // But a snapshot based on a pre-resharding index metadata would only contain shard id 3.
+            // Without resharding every snapshot would always contain the maximum shard id.
+            return true;
+        } else {
+            return false;
+        }
     }
 }

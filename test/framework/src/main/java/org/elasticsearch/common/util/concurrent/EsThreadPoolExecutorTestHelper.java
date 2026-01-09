8328d5df6007e02c211f68870ad0cd9434561640
Yang Wang <yang.wang@elastic.co>
2026-01-09 15:07:29 +1100

    move unit test

--- test/framework/src/main/java/org/elasticsearch/common/util/concurrent/EsThreadPoolExecutorTestHelper.java
+++ test/framework/src/main/java/org/elasticsearch/common/util/concurrent/EsThreadPoolExecutorTestHelper.java
@@ -9,13 +9,6 @@
 
 package org.elasticsearch.common.util.concurrent;
 
-import org.elasticsearch.common.settings.Settings;
-
-import java.util.concurrent.TimeUnit;
-import java.util.function.LongSupplier;
-
-import static org.elasticsearch.test.ESTestCase.randomIdentifier;
-
 public class EsThreadPoolExecutorTestHelper {
 
     public static EsExecutors.HotThreadsOnLargeQueueConfig getHotThreadsOnLargeQueueConfig(EsThreadPoolExecutor executor) {
@@ -24,27 +17,5 @@ public class EsThreadPoolExecutorTestHelper {
 
     public static long getStartTimeMillisOfLargeQueue(EsThreadPoolExecutor executor) {
         return executor.getStartTimeMillisOfLargeQueue();
-    }
-
-    public static EsThreadPoolExecutor newEsThreadPoolExecutor(
-        String name,
-        int corePoolSize,
-        int maximumPoolSize,
-        EsExecutors.HotThreadsOnLargeQueueConfig hotThreadsOnLargeQueueConfig,
-        LongSupplier currentTimeMillisSupplier
-    ) {
-        return new EsThreadPoolExecutor(
-            name,
-            corePoolSize,
-            maximumPoolSize,
-            5,
-            TimeUnit.MINUTES,
-            new EsExecutors.ExecutorScalingQueue<>(),
-            EsExecutors.daemonThreadFactory(randomIdentifier(), name),
-            new EsExecutors.ForceQueuePolicy(false, false),
-            new ThreadContext(Settings.EMPTY),
-            hotThreadsOnLargeQueueConfig,
-            currentTimeMillisSupplier
-        );
     }
 }

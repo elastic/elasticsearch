1d4b9a6459123b91d79673f5391276e06450b671
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2026-01-09 10:07:47 +0100

    Sync default-over-time branch

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/InsertDefaultInnerTimeSeriesAggregate.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/InsertDefaultInnerTimeSeriesAggregate.java
@@ -9,7 +9,6 @@ package org.elasticsearch.xpack.esql.analysis;
 
 import org.elasticsearch.xpack.esql.core.expression.Alias;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
-import org.elasticsearch.xpack.esql.core.expression.FieldAttribute;
 import org.elasticsearch.xpack.esql.core.expression.Literal;
 import org.elasticsearch.xpack.esql.core.expression.NamedExpression;
 import org.elasticsearch.xpack.esql.core.expression.TypedAttribute;
@@ -28,13 +27,16 @@ import java.util.List;
 /**
  * Ensures that {@link TypedAttribute}s used inside a {@link TimeSeriesAggregate} are wrapped in a
  * {@link TimeSeriesAggregateFunction}.
- * For example, it rewrites
+ * Examples:
  * <pre>
- * SUM(foo + LAST_OVER_TIME(bar))
- * </pre>
- * into
- * <pre>
+ * foo + bar ->
+ * LAST_OVER_TIME(foo) + LAST_OVER_TIME(bar)
+ *
+ * SUM(foo + LAST_OVER_TIME(bar)) ->
  * SUM(LAST_OVER_TIME(foo) + LAST_OVER_TIME(bar))
+ *
+ * foo / 2 + bar * 2 ->
+ * LAST_OVER_TIME(foo) / 2 + LAST_OVER_TIME(bar) * 2
  * </pre>
  */
 public class InsertDefaultInnerTimeSeriesAggregate extends Rule<LogicalPlan, LogicalPlan> {
@@ -55,31 +57,30 @@ public class InsertDefaultInnerTimeSeriesAggregate extends Rule<LogicalPlan, Log
     }
 
     private static Expression addDefaultInnerAggs(Expression expression, Expression timestamp) {
-        return expression.transformDownSkipBranch((expr, skipBranch) -> switch (expr) {
-            case TimeSeriesAggregateFunction ts -> {
-                // if we find a TimeSeriesAggregateFunction, we can skip the branch
-                skipBranch.set(true);
-                yield ts;
-            }
-            // only transform field, not all children
-            case AggregateFunction af -> {
-                skipBranch.set(true);
-                yield af.withField(addDefaultInnerAggs(af.field(), timestamp));
-            }
-            case FilteredExpression filtered -> {
-                // avoid modifying filter conditions
-                skipBranch.set(true);
-                yield filtered.withDelegate(addDefaultInnerAggs(filtered.delegate(), timestamp));
-            }
-            case FieldAttribute field -> {
-                skipBranch.set(true);
-                yield createDefaultInnerAggregation(field, timestamp);
-            }
-            default -> expr;
+        return expression.transformDownSkipBranch((expr, skipBranch) -> {
+            // the default is to end the traversal here as we're either done or a recursive call will handle it
+            skipBranch.set(true);
+            return switch (expr) {
+                // this is already a time series aggregation, no need to go deeper
+                case TimeSeriesAggregateFunction ts -> ts;
+                // only transform field, not all children (such as inline filter or window)
+                case AggregateFunction af -> af.withField(addDefaultInnerAggs(af.field(), timestamp));
+                // avoid modifying filter conditions, just the delegate
+                case FilteredExpression filtered -> filtered.withDelegate(addDefaultInnerAggs(filtered.delegate(), timestamp));
+                // if we reach a TypedAttribute, it hasn't been wrapped in a TimeSeriesAggregateFunction yet
+                // (otherwise the traversal would have stopped earlier)
+                // so we wrap it with a default one
+                case TypedAttribute ta -> wrapWithDefaultInnerAggregation(ta, timestamp);
+                default -> {
+                    // for other expressions, continue the traversal
+                    skipBranch.set(false);
+                    yield expr;
+                }
+            };
         });
     }
 
-    private static TimeSeriesAggregateFunction createDefaultInnerAggregation(TypedAttribute attr, Expression timestamp) {
+    private static TimeSeriesAggregateFunction wrapWithDefaultInnerAggregation(TypedAttribute attr, Expression timestamp) {
         if (attr.dataType() == DataType.EXPONENTIAL_HISTOGRAM || attr.dataType() == DataType.TDIGEST) {
             return new HistogramMergeOverTime(attr.source(), attr, Literal.TRUE, AggregateFunction.NO_WINDOW);
         } else {

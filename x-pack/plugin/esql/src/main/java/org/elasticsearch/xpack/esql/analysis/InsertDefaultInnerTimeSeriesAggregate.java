9c5efba8aa0bf580f5aab51a797daf2ffcb8a307
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2026-01-09 09:43:11 +0100

    Simplify, add more examples and tests

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/InsertDefaultInnerTimeSeriesAggregate.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/InsertDefaultInnerTimeSeriesAggregate.java
@@ -8,20 +8,16 @@
 package org.elasticsearch.xpack.esql.analysis;
 
 import org.elasticsearch.xpack.esql.core.expression.Alias;
-import org.elasticsearch.xpack.esql.core.expression.Attribute;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
 import org.elasticsearch.xpack.esql.core.expression.Literal;
-import org.elasticsearch.xpack.esql.core.expression.MetadataAttribute;
 import org.elasticsearch.xpack.esql.core.expression.NamedExpression;
 import org.elasticsearch.xpack.esql.core.expression.TypedAttribute;
 import org.elasticsearch.xpack.esql.core.type.DataType;
-import org.elasticsearch.xpack.esql.core.util.Holder;
 import org.elasticsearch.xpack.esql.expression.function.aggregate.AggregateFunction;
 import org.elasticsearch.xpack.esql.expression.function.aggregate.FilteredExpression;
 import org.elasticsearch.xpack.esql.expression.function.aggregate.HistogramMergeOverTime;
 import org.elasticsearch.xpack.esql.expression.function.aggregate.LastOverTime;
 import org.elasticsearch.xpack.esql.expression.function.aggregate.TimeSeriesAggregateFunction;
-import org.elasticsearch.xpack.esql.plan.logical.EsRelation;
 import org.elasticsearch.xpack.esql.plan.logical.LogicalPlan;
 import org.elasticsearch.xpack.esql.plan.logical.TimeSeriesAggregate;
 import org.elasticsearch.xpack.esql.rule.Rule;
@@ -31,13 +27,16 @@ import java.util.List;
 /**
  * Ensures that {@link TypedAttribute}s used inside a {@link TimeSeriesAggregate} are wrapped in a
  * {@link TimeSeriesAggregateFunction}.
- * For example, it rewrites
+ * Examples:
  * <pre>
- * SUM(foo + LAST_OVER_TIME(bar))
- * </pre>
- * into
- * <pre>
+ * foo + bar ->
+ * LAST_OVER_TIME(foo) + LAST_OVER_TIME(bar)
+ *
+ * SUM(foo + LAST_OVER_TIME(bar)) ->
  * SUM(LAST_OVER_TIME(foo) + LAST_OVER_TIME(bar))
+ *
+ * foo / 2 + bar * 2 ->
+ * LAST_OVER_TIME(foo) / 2 + LAST_OVER_TIME(bar) * 2
  * </pre>
  */
 public class InsertDefaultInnerTimeSeriesAggregate extends Rule<LogicalPlan, LogicalPlan> {
@@ -47,20 +46,9 @@ public class InsertDefaultInnerTimeSeriesAggregate extends Rule<LogicalPlan, Log
     }
 
     public LogicalPlan rule(TimeSeriesAggregate aggregate) {
-        Holder<Attribute> timestamp = new Holder<>();
-        aggregate.forEachDown(EsRelation.class, (EsRelation r) -> {
-            for (Attribute attr : r.output()) {
-                if (attr.name().equals(MetadataAttribute.TIMESTAMP_FIELD)) {
-                    timestamp.set(attr);
-                }
-            }
-        });
-        if (timestamp.get() == null) {
-            throw new IllegalArgumentException("@timestamp field is missing from the time-series source");
-        }
         List<NamedExpression> newAggregates = aggregate.aggregates().stream().map(agg -> {
             if (agg instanceof Alias alias) {
-                return alias.replaceChild(addDefaultInnerAggs(alias.child(), timestamp.get()));
+                return alias.replaceChild(addDefaultInnerAggs(alias.child(), aggregate.timestamp()));
             } else {
                 return agg;
             }
@@ -68,32 +56,31 @@ public class InsertDefaultInnerTimeSeriesAggregate extends Rule<LogicalPlan, Log
         return aggregate.with(aggregate.groupings(), newAggregates);
     }
 
-    private static Expression addDefaultInnerAggs(Expression expression, Attribute timestamp) {
-        return expression.transformDownSkipBranch((expr, skipBranch) -> switch (expr) {
-            case TimeSeriesAggregateFunction ts -> {
-                // if we find a TimeSeriesAggregateFunction, we can skip the branch
-                skipBranch.set(true);
-                yield ts;
-            }
-            // only transform field, not all children
-            case AggregateFunction af -> {
-                skipBranch.set(true);
-                yield af.withField(addDefaultInnerAggs(af.field(), timestamp));
-            }
-            case FilteredExpression filtered -> {
-                // avoid modifying filter conditions
-                skipBranch.set(true);
-                yield filtered.withDelegate(addDefaultInnerAggs(filtered.delegate(), timestamp));
-            }
-            case TypedAttribute ta -> {
-                skipBranch.set(true);
-                yield createDefaultInnerAggregation(ta, timestamp);
-            }
-            default -> expr;
+    private static Expression addDefaultInnerAggs(Expression expression, Expression timestamp) {
+        return expression.transformDownSkipBranch((expr, skipBranch) -> {
+            // the default is to end the traversal here as we're either done or a recursive call will handle it
+            skipBranch.set(true);
+            return switch (expr) {
+                // this is already a time series aggregation, no need to go deeper
+                case TimeSeriesAggregateFunction ts -> ts;
+                // only transform field, not all children (such as inline filter or window)
+                case AggregateFunction af -> af.withField(addDefaultInnerAggs(af.field(), timestamp));
+                // avoid modifying filter conditions, just the delegate
+                case FilteredExpression filtered -> filtered.withDelegate(addDefaultInnerAggs(filtered.delegate(), timestamp));
+                // if we reach a TypedAttribute, it hasn't been wrapped in a TimeSeriesAggregateFunction yet
+                // (otherwise the traversal would have stopped earlier)
+                // so we wrap it with a default one
+                case TypedAttribute ta -> wrapWithDefaultInnerAggregation(ta, timestamp);
+                default -> {
+                    // for other expressions, continue the traversal
+                    skipBranch.set(false);
+                    yield expr;
+                }
+            };
         });
     }
 
-    private static TimeSeriesAggregateFunction createDefaultInnerAggregation(TypedAttribute attr, Attribute timestamp) {
+    private static TimeSeriesAggregateFunction wrapWithDefaultInnerAggregation(TypedAttribute attr, Expression timestamp) {
         if (attr.dataType() == DataType.EXPONENTIAL_HISTOGRAM || attr.dataType() == DataType.TDIGEST) {
             return new HistogramMergeOverTime(attr.source(), attr, Literal.TRUE, AggregateFunction.NO_WINDOW);
         } else {
--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/analysis/InsertDefaultInnerTimeSeriesAggregateTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/analysis/InsertDefaultInnerTimeSeriesAggregateTests.java
@@ -8,59 +8,51 @@
 package org.elasticsearch.xpack.esql.analysis;
 
 import org.elasticsearch.xpack.esql.core.expression.Alias;
-import org.elasticsearch.xpack.esql.core.expression.Expressions;
-import org.elasticsearch.xpack.esql.expression.function.aggregate.LastOverTime;
-import org.elasticsearch.xpack.esql.expression.function.aggregate.Sum;
-import org.elasticsearch.xpack.esql.expression.predicate.operator.arithmetic.Add;
+import org.elasticsearch.xpack.esql.core.expression.Expression;
 import org.elasticsearch.xpack.esql.optimizer.AbstractLogicalPlanOptimizerTests;
-import org.elasticsearch.xpack.esql.plan.logical.TimeSeriesAggregate;
 
-import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 
-import static org.elasticsearch.xpack.esql.EsqlTestUtils.as;
-import static org.hamcrest.Matchers.equalTo;
+import static org.elasticsearch.xpack.esql.EsqlTestUtils.equalToIgnoringIds;
+import static org.elasticsearch.xpack.esql.EsqlTestUtils.ignoreIds;
 
 public class InsertDefaultInnerTimeSeriesAggregateTests extends AbstractLogicalPlanOptimizerTests {
 
     private final InsertDefaultInnerTimeSeriesAggregate rule = new InsertDefaultInnerTimeSeriesAggregate();
 
-    public void testTranslateSumWithImplicitLastOverTime() {
-        var query = """
-            TS k8s | STATS sum(network.bytes_in) BY bucket(@timestamp, 1 minute)
-            | LIMIT 10
-            """;
-        var plan = rule.apply(metricsAnalyzer.analyze(parser.parseQuery(query)));
-        TimeSeriesAggregate aggsByTsid = plan.collect(TimeSeriesAggregate.class).getFirst();
-
-        Sum sum = as(Alias.unwrap(aggsByTsid.aggregates().getFirst()), Sum.class);
-        LastOverTime lastOverTime = as(sum.field(), LastOverTime.class);
-        assertThat(Expressions.attribute(lastOverTime.field()).name(), equalTo("network.bytes_in"));
+    public void testSimpleImplicitOverTime() {
+        assertStatsEqual("sum(network.bytes_in)", "sum(last_over_time(network.bytes_in))");
     }
 
-    public void testTranslateSumWithAdditionWithImplicitLastOverTime() {
-        var query = """
+    public void testBinaryWithImplicitAndExplicitOverTime() {
+        var expected = "sum(last_over_time(network.eth0.tx) + last_over_time(network.eth0.rx))";
+        assertStatsEqual("sum(network.eth0.tx + network.eth0.rx)", expected);
+        assertStatsEqual("sum(network.eth0.tx + last_over_time(network.eth0.rx))", expected);
+        assertStatsEqual("sum(last_over_time(network.eth0.tx) + network.eth0.rx)", expected);
+        assertStatsEqual(expected, expected);
+    }
+
+    public void testComplexArithmetic() {
+        var expected = "sum(last_over_time(network.eth0.tx) / 2 + last_over_time(network.eth0.rx) * 2)";
+        assertStatsEqual("sum(network.eth0.tx / 2 + network.eth0.rx * 2)", expected);
+        assertStatsEqual("sum(last_over_time(network.eth0.tx) / 2 + network.eth0.rx * 2)", expected);
+        assertStatsEqual("sum(network.eth0.tx / 2 + last_over_time(network.eth0.rx) * 2)", expected);
+        assertStatsEqual(expected, expected);
+    }
+
+    private void assertStatsEqual(String stats1, String stats2) {
+        var baseQuery = """
             TS k8s
             | STATS %s BY bucket(@timestamp, 1 minute)
             | LIMIT 10
             """;
-        for (String variant : List.of(
-            "sum(network.eth0.tx + network.eth0.rx)",
-            "sum(network.eth0.tx + last_over_time(network.eth0.rx))",
-            "sum(last_over_time(network.eth0.tx) + network.eth0.rx)",
-            "sum(last_over_time(network.eth0.tx) + last_over_time(network.eth0.rx))"
-        )) {
-            var plan = rule.apply(metricsAnalyzer.analyze(parser.parseQuery(String.format(Locale.ROOT, query, variant))));
-
-            TimeSeriesAggregate aggsByTsid = plan.collect(TimeSeriesAggregate.class).getFirst();
-            Sum sum = as(Alias.unwrap(aggsByTsid.aggregates().getFirst()), Sum.class);
-            Add add = as(sum.field(), Add.class);
-            LastOverTime lastOverTimeTx = as(add.left(), LastOverTime.class);
-            assertThat(Expressions.attribute(lastOverTimeTx.field()).name(), equalTo("network.eth0.tx"));
-
-            LastOverTime lastOverTimeRx = as(add.right(), LastOverTime.class);
-            assertThat(Expressions.attribute(lastOverTimeRx.field()).name(), equalTo("network.eth0.rx"));
-        }
+        var plan1 = rule.apply(metricsAnalyzer.analyze(parser.parseQuery(String.format(Locale.ROOT, baseQuery, stats1))));
+        Function<Alias, Expression> ignoreAliasName = (Alias a) -> new Alias(a.source(), "dummy", a.child(), a.id());
+        plan1 = plan1.transformExpressionsDown(Alias.class, ignoreAliasName);
+        var plan2 = metricsAnalyzer.analyze(parser.parseQuery(String.format(Locale.ROOT, baseQuery, stats2)));
+        plan2 = plan2.transformExpressionsDown(Alias.class, ignoreAliasName);
+        assertThat(ignoreIds(plan1), equalToIgnoringIds(plan2));
     }
 
 }

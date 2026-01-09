671c768c135632d50f941686dadbebf688d19c6d
majiayu000 <1835304752@qq.com>
2026-01-09 22:42:06 +0800

    Add dense_vector equality and inequality support in ES|QL
    Signed-off-by: majiayu000 <1835304752@qq.com>

---
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/predicate/operator/comparison/NotEqualsDenseVectorEvaluator.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.esql.expression.predicate.operator.comparison;
+
+import org.apache.lucene.util.RamUsageEstimator;
+import org.elasticsearch.compute.data.Block;
+import org.elasticsearch.compute.data.BooleanBlock;
+import org.elasticsearch.compute.data.FloatBlock;
+import org.elasticsearch.compute.data.Page;
+import org.elasticsearch.compute.operator.DriverContext;
+import org.elasticsearch.compute.operator.EvalOperator;
+import org.elasticsearch.core.Releasables;
+import org.elasticsearch.xpack.esql.core.tree.Source;
+
+/**
+ * {@link EvalOperator.ExpressionEvaluator} implementation for comparing dense_vector inequality.
+ * Two dense vectors are considered not equal if they have different dimensions or any elements differ.
+ */
+public final class NotEqualsDenseVectorEvaluator implements EvalOperator.ExpressionEvaluator {
+    private static final long BASE_RAM_BYTES_USED = RamUsageEstimator.shallowSizeOfInstance(NotEqualsDenseVectorEvaluator.class);
+
+    private final Source source;
+    private final EvalOperator.ExpressionEvaluator lhs;
+    private final EvalOperator.ExpressionEvaluator rhs;
+    private final DriverContext driverContext;
+
+    public NotEqualsDenseVectorEvaluator(
+        Source source,
+        EvalOperator.ExpressionEvaluator lhs,
+        EvalOperator.ExpressionEvaluator rhs,
+        DriverContext driverContext
+    ) {
+        this.source = source;
+        this.lhs = lhs;
+        this.rhs = rhs;
+        this.driverContext = driverContext;
+    }
+
+    @Override
+    public Block eval(Page page) {
+        try (FloatBlock lhsBlock = (FloatBlock) lhs.eval(page); FloatBlock rhsBlock = (FloatBlock) rhs.eval(page)) {
+            int positionCount = page.getPositionCount();
+            try (BooleanBlock.Builder result = driverContext.blockFactory().newBooleanBlockBuilder(positionCount)) {
+                for (int p = 0; p < positionCount; p++) {
+                    int lhsValueCount = lhsBlock.getValueCount(p);
+                    int rhsValueCount = rhsBlock.getValueCount(p);
+
+                    // If either is null, result is null
+                    if (lhsBlock.isNull(p) || rhsBlock.isNull(p)) {
+                        result.appendNull();
+                        continue;
+                    }
+
+                    // If dimensions differ, vectors are not equal
+                    if (lhsValueCount != rhsValueCount) {
+                        result.appendBoolean(true);
+                        continue;
+                    }
+
+                    // Compare all elements
+                    boolean notEqual = false;
+                    int lhsStart = lhsBlock.getFirstValueIndex(p);
+                    int rhsStart = rhsBlock.getFirstValueIndex(p);
+                    for (int i = 0; i < lhsValueCount; i++) {
+                        if (lhsBlock.getFloat(lhsStart + i) != rhsBlock.getFloat(rhsStart + i)) {
+                            notEqual = true;
+                            break;
+                        }
+                    }
+                    result.appendBoolean(notEqual);
+                }
+                return result.build();
+            }
+        }
+    }
+
+    @Override
+    public long baseRamBytesUsed() {
+        return BASE_RAM_BYTES_USED + lhs.baseRamBytesUsed() + rhs.baseRamBytesUsed();
+    }
+
+    @Override
+    public String toString() {
+        return "NotEqualsDenseVectorEvaluator[lhs=" + lhs + ", rhs=" + rhs + "]";
+    }
+
+    @Override
+    public void close() {
+        Releasables.closeExpectNoException(lhs, rhs);
+    }
+
+    public static class Factory implements EvalOperator.ExpressionEvaluator.Factory {
+        private final Source source;
+        private final EvalOperator.ExpressionEvaluator.Factory lhs;
+        private final EvalOperator.ExpressionEvaluator.Factory rhs;
+
+        public Factory(Source source, EvalOperator.ExpressionEvaluator.Factory lhs, EvalOperator.ExpressionEvaluator.Factory rhs) {
+            this.source = source;
+            this.lhs = lhs;
+            this.rhs = rhs;
+        }
+
+        @Override
+        public NotEqualsDenseVectorEvaluator get(DriverContext context) {
+            return new NotEqualsDenseVectorEvaluator(source, lhs.get(context), rhs.get(context), context);
+        }
+
+        @Override
+        public String toString() {
+            return "NotEqualsDenseVectorEvaluator[lhs=" + lhs + ", rhs=" + rhs + "]";
+        }
+    }
+}

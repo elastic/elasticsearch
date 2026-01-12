ccf8c3c5a1b4a2de731c60fc10cebfa5bf416b6d
Iván Cea Fontenla <ivancea96@outlook.com>
2026-01-12 15:35:38 +0100

    ESQL: TRange timezone support (#139911)
    - Added timezone support to `TRange(<date_period>)`. Now a `TRange(-1d)` will honor DST
    - Changed the `Configuration.now` field to be a constructor param. Currently, it was created inside, which made testing harder
      - Also changed it to be an `Instant` instead of a ZonedDateTime. It was also converted back to an instant, and there's no meaning in a zone here, as it's an absolute point in time. Plus there's a separated "zoneId" field.

--- x-pack/plugin/esql/qa/testFixtures/src/main/java/org/elasticsearch/xpack/esql/EsqlTestUtils.java
+++ x-pack/plugin/esql/qa/testFixtures/src/main/java/org/elasticsearch/xpack/esql/EsqlTestUtils.java
@@ -166,6 +166,7 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Duration;
+import java.time.Instant;
 import java.time.Period;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -614,6 +615,7 @@ public final class EsqlTestUtils {
     public static Configuration configuration(QueryPragmas pragmas, String query, EsqlStatement statement) {
         return new Configuration(
             statement.setting(QuerySettings.TIME_ZONE),
+            Instant.now(),
             Locale.US,
             null,
             null,

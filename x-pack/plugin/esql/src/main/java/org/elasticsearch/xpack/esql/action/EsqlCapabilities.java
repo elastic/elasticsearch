ab745839dc9c53be99296b50e0fe471146aac83c
Tim Brooks <tim@timbrooks.org>
2026-01-12 20:51:55 -0700

    Use the a mod number of shard function to route (#137062)
    Using hash % number of shards to routing documents. This commit
    introduces an index version to indicate which routing function
    an index is using. Since traditional split, clone, and shrink
    functions inherit index version this means that documents will
    still be routed the same after the shard is resized.

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1873,6 +1873,10 @@ public class EsqlCapabilities {
          * Dense_vector aggregation functions
          */
         DENSE_VECTOR_AGG_FUNCTIONS,
+        /**
+         * Marks the move to the hash(doc) % shard_count routing function. Added in #137062.
+         */
+        ROUTING_FUNCTION_UPDATE,
 
         // Last capability should still have a comma for fewer merge conflicts when adding new ones :)
         // This comment prevents the semicolon from being on the previous capability when Spotless formats the file.

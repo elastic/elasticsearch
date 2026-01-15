1140aa04965007ae53489f159816c993dfbd34ad
ChrisHegarty <chegar999@gmail.com>
2026-01-15 16:52:56 +0000

    refactor - prefetch is Posix only - no-op on Windows

--- libs/native/src/main/java/org/elasticsearch/nativeaccess/jdk/JdkCloseableMappedByteBuffer.java
+++ libs/native/src/main/java/org/elasticsearch/nativeaccess/jdk/JdkCloseableMappedByteBuffer.java
@@ -10,8 +10,6 @@
 package org.elasticsearch.nativeaccess.jdk;
 
 import org.elasticsearch.nativeaccess.CloseableMappedByteBuffer;
-import org.elasticsearch.nativeaccess.lib.NativeLibraryProvider;
-import org.elasticsearch.nativeaccess.lib.PosixCLibrary;
 
 import java.io.IOException;
 import java.lang.foreign.Arena;
@@ -21,22 +19,20 @@ import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
 import java.util.Objects;
 
-class JdkCloseableMappedByteBuffer implements CloseableMappedByteBuffer {
-
-    private static final PosixCLibrary LIB = NativeLibraryProvider.instance().getLibrary(PosixCLibrary.class);
-    private static final int PAGE_SIZE = LIB.getPageSize();
+public class JdkCloseableMappedByteBuffer implements CloseableMappedByteBuffer {
 
     private final Arena arena;
-    private final MemorySegment segment;
+    protected final MemorySegment segment;
     private final MappedByteBuffer bufferView;
 
-    static JdkCloseableMappedByteBuffer ofShared(FileChannel fileChannel, MapMode mode, long position, long size) throws IOException {
+    public static JdkCloseableMappedByteBuffer ofShared(FileChannel fileChannel, MapMode mode, long position, long size)
+        throws IOException {
         var arena = Arena.ofShared();
         var seg = fileChannel.map(mode, position, size, arena);
         return new JdkCloseableMappedByteBuffer(seg, arena);
     }
 
-    private JdkCloseableMappedByteBuffer(MemorySegment seg, Arena arena) {
+    protected JdkCloseableMappedByteBuffer(MemorySegment seg, Arena arena) {
         this.arena = arena;
         this.segment = seg;
         this.bufferView = (MappedByteBuffer) seg.asByteBuffer();
@@ -63,24 +59,6 @@ class JdkCloseableMappedByteBuffer implements CloseableMappedByteBuffer {
     @Override
     public void prefetch(long offset, long length) {
         Objects.checkFromIndexSize(offset, length, segment.byteSize());
-        // Align offset with the page size, this is required for madvise.
-        // Compute the offset of the current position in the OS's page.
-        final long offsetInPage = (segment.address() + offset) % PAGE_SIZE;
-        offset -= offsetInPage;
-        length += offsetInPage;
-        if (offset < 0) {
-            // start of the page is before the start of this segment, ignore the first page.
-            offset += PAGE_SIZE;
-            length -= PAGE_SIZE;
-            if (length <= 0) {
-                // This segment has no data beyond the first page.
-                return;
-            }
-        }
-        int ret = LIB.madvise(segment, offset, length, PosixCLibrary.POSIX_MADV_WILLNEED);
-        if (ret != 0) {
-            int errno = LIB.errno();
-            throw new RuntimeException("madvise failed with (error=" + errno + "): " + LIB.strerror(errno));
-        }
+        // no explicit action, override in subclass if needed.
     }
 }

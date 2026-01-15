2765a800e00f6c8b95f73233db4b1cfb59afd652
Mark Vieira <portugee@gmail.com>
2026-01-15 07:57:06 -0800

    Remove all usages of TransportVersionUtils.randomVersionBetween (#140692)
    (cherry picked from commit 628dbe3eb6b4f5002c5ffb5d98e4b8d149749ed7)
    
    # Conflicts:
    #	server/src/test/java/org/elasticsearch/action/admin/cluster/state/ClusterStateRequestTests.java
    #	server/src/test/java/org/elasticsearch/action/fieldcaps/FieldCapabilitiesNodeResponseTests.java
    #	server/src/test/java/org/elasticsearch/action/fieldcaps/FieldCapabilitiesResponseTests.java
    #	server/src/test/java/org/elasticsearch/action/index/IndexRequestTests.java
    #	server/src/test/java/org/elasticsearch/cluster/coordination/PublicationTransportHandlerTests.java
    #	server/src/test/java/org/elasticsearch/cluster/routing/allocation/allocator/NodeBalanceStatsTests.java
    #	server/src/test/java/org/elasticsearch/cluster/serialization/ClusterSerializationTests.java
    #	server/src/test/java/org/elasticsearch/cluster/version/CompatibilityVersionsTests.java
    #	server/src/test/java/org/elasticsearch/common/io/stream/DelayableWriteableTests.java
    #	server/src/test/java/org/elasticsearch/common/io/stream/VersionCheckingStreamOutputTests.java
    #	server/src/test/java/org/elasticsearch/index/translog/TranslogTests.java
    #	server/src/test/java/org/elasticsearch/persistent/BasePersistentTasksCustomMetadataTests.java
    #	server/src/test/java/org/elasticsearch/search/internal/ShardSearchRequestTests.java
    #	server/src/test/java/org/elasticsearch/search/suggest/SuggestTests.java
    #	server/src/test/java/org/elasticsearch/transport/InboundDecoderTests.java
    #	test/framework/src/main/java/org/elasticsearch/test/TransportVersionUtils.java
    #	test/framework/src/main/java/org/elasticsearch/transport/AbstractSimpleTransportTestCase.java
    #	x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/ClientHelperTests.java
    #	x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authc/AuthenticationSerializationTests.java
    #	x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authc/AuthenticationTests.java
    #	x-pack/plugin/esql/compute/src/test/java/org/elasticsearch/compute/data/BlockSerializationTests.java
    #	x-pack/plugin/esql/qa/testFixtures/src/main/java/org/elasticsearch/xpack/esql/analysis/MutableAnalyzerContext.java
    #	x-pack/plugin/identity-provider/src/test/java/org/elasticsearch/xpack/idp/action/SamlInitiateSingleSignOnRequestTests.java
    #	x-pack/plugin/identity-provider/src/test/java/org/elasticsearch/xpack/idp/saml/sp/SamlServiceProviderDocumentTests.java
    #	x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/queries/AbstractInterceptedInferenceQueryBuilderTestCase.java
    #	x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/queries/SemanticQueryBuilderTests.java

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/queries/AbstractInterceptedInferenceQueryBuilderTestCase.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/queries/AbstractInterceptedInferenceQueryBuilderTestCase.java
@@ -70,6 +70,7 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import static org.elasticsearch.xpack.core.inference.action.GetInferenceFieldsAction.GET_INFERENCE_FIELDS_ACTION_TV;
+import static org.elasticsearch.xpack.core.inference.action.GetInferenceFieldsInternalAction.GET_INFERENCE_FIELDS_ACTION_AS_INDICES_ACTION_TV;
 import static org.elasticsearch.xpack.core.ml.inference.trainedmodel.InferenceConfig.DEFAULT_RESULTS_FIELD;
 import static org.elasticsearch.xpack.inference.queries.InterceptedInferenceQueryBuilder.INFERENCE_RESULTS_MAP_WITH_CLUSTER_ALIAS;
 import static org.elasticsearch.xpack.inference.queries.SemanticQueryBuilder.SEMANTIC_SEARCH_CCS_SUPPORT;
@@ -163,13 +164,8 @@ public abstract class AbstractInterceptedInferenceQueryBuilderTestCase<T extends
     }
 
     public void testBwCSerialization() throws Exception {
-        TransportVersion minTransportVersion = TransportVersion.max(getMinimalSupportedVersion(), TransportVersion.minimumCompatible());
         for (int i = 0; i < 100; i++) {
-            TransportVersion transportVersion = TransportVersionUtils.randomVersionBetween(
-                random(),
-                minTransportVersion,
-                TransportVersionUtils.getPreviousVersion(TransportVersion.current())
-            );
+            TransportVersion transportVersion = TransportVersionUtils.randomVersionNotSupporting(random(), TransportVersion.current());
             serializationTestCase(transportVersion);
         }
     }
@@ -242,11 +238,7 @@ public abstract class AbstractInterceptedInferenceQueryBuilderTestCase<T extends
             new MockInferenceRemoteClusterClient.RemoteClusterConfig(
                 remoteInferenceEndpoints,
                 remoteIndexConfigs,
-                TransportVersionUtils.randomVersionBetween(
-                    random(),
-                    SEMANTIC_SEARCH_CCS_SUPPORT,
-                    TransportVersionUtils.getPreviousVersion(GET_INFERENCE_FIELDS_ACTION_TV)
-                )
+                TransportVersionUtils.getPreviousVersion(GET_INFERENCE_FIELDS_ACTION_AS_INDICES_ACTION_TV)
             );
 
         QueryRewriteContext preCcsRemoteClusterContext = createQueryRewriteContext(
@@ -276,11 +268,7 @@ public abstract class AbstractInterceptedInferenceQueryBuilderTestCase<T extends
         final T nonInferenceFieldQuery = createQueryBuilder("non_inference_field");
 
         for (int i = 0; i < 100; i++) {
-            TransportVersion transportVersion = TransportVersionUtils.randomVersionBetween(
-                random(),
-                TransportVersion.minimumCompatible(),
-                TransportVersionUtils.getPreviousVersion(TransportVersion.current())
-            );
+            TransportVersion transportVersion = TransportVersionUtils.randomVersionNotSupporting(random(), TransportVersion.current());
 
             QueryRewriteContext queryRewriteContext = createQueryRewriteContext(
                 Map.of("local-index", Map.of(inferenceField, SPARSE_INFERENCE_ID)),
@@ -357,11 +345,7 @@ public abstract class AbstractInterceptedInferenceQueryBuilderTestCase<T extends
         assertThat(deserializedQuery, equalTo(interceptedQuery));
 
         // Test with a transport version prior to cluster alias support, which should fail
-        TransportVersion transportVersion = TransportVersionUtils.randomVersionBetween(
-            random(),
-            NEW_SEMANTIC_QUERY_INTERCEPTORS,
-            TransportVersionUtils.getPreviousVersion(INFERENCE_RESULTS_MAP_WITH_CLUSTER_ALIAS)
-        );
+        TransportVersion transportVersion = TransportVersionUtils.getPreviousVersion(INFERENCE_RESULTS_MAP_WITH_CLUSTER_ALIAS);
         IllegalArgumentException e = assertThrows(
             IllegalArgumentException.class,
             () -> copyNamedWriteable(interceptedQuery, writableRegistry(), QueryBuilder.class, transportVersion)

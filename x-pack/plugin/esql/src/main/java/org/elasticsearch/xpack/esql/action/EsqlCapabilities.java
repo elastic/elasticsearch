8145b33670e3ec2c8ecd11dfc711f0b97bb7c385
Nhat Nguyen <nhat.nguyen@elastic.co>
2026-01-15 16:36:38 -0800

    Use sub keyword block loader with ignore_above for text fields (#140622) (#140787)
    Today, we do not use the block loader of the sub-keyword field when
    loading the text field if ignore_above is set. When ignore_above is
    configured, values exceeding the threshold are not stored for the
    keyword field, which is why we cannot load from the sub-keyword field
    alone. However, if all documents in a segment have values below the
    threshold, we can safely load values from doc_values instead of stored
    fields. If some documents exceed the threshold, we should load values
    from doc_values for those below the threshold and from stored fields for
    those above.
    
    This PR leverages the terms dictionary from the _ignored field to prefer
    loading values from doc_values of the sub-keyword field when possible.
    For any document where the sub-keyword field appears in the _ignored
    dictionary, we load from stored fields or _source; otherwise, we use
    doc_values. This improves performance when loading text fields,
    especially for logsdb.
    
    There is a bug with FLS where we blindly delegate the sub-keyword field,
    but it may be hidden by FLS. I will address this in a follow-up.
    
    Marking this as a bug fix for performance issues.
    
    (cherry picked from commit f15069a75e3576ec096879c75f1a2dfb2f01a12f)

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1832,6 +1832,11 @@ public class EsqlCapabilities {
          */
         ENRICH_DENSE_VECTOR_BUGFIX,
 
+        /**
+         * Adds a conditional block loader for text fields that prefers using the sub-keyword field whenever possible.
+         */
+        CONDITIONAL_BLOCK_LOADER_FOR_TEXT_FIELDS,
+
         // Last capability should still have a comma for fewer merge conflicts when adding new ones :)
         // This comment prevents the semicolon from being on the previous capability when Spotless formats the file.
         ;

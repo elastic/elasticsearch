dcf26e08ad3ca98e486cc022594053637eb92f42
Nhat Nguyen <nhat.nguyen@elastic.co>
2026-01-21 14:01:06 -0800

    Partition time-series source (#140475)
    Today, for rate aggregations, we can't partition a target shard by DOC
    or SEGMENT, only by SHARD. Because of this, we rely on ROUND_TO from
    time_bucket to partition the target shard into multiple slices. However,
    the number of slices can be either too many or too few. Too few large
    slices might under-utilize the CPUs and require a large amount of memory
    to buffer data points. Many smaller slices solve these issues but add
    significant overhead.
    
    This change replaces the ROUND_TO partitioning with time-based
    partitioning for time-series sources. One potential overhead of this
    method compared to ROUND_TO is that, with ROUND_TO, we do not need to
    perform round_to for the timestamp, as the query source provides this
    constant.
    
    (cherry picked from commit 54ee110805decbd6348fd9c35c0cf19653c1759e)
    
    # Conflicts:
    #	x-pack/plugin/esql/qa/testFixtures/src/main/resources/k8s-timeseries.csv-spec
    #	x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1837,6 +1837,11 @@ public class EsqlCapabilities {
          */
         CONDITIONAL_BLOCK_LOADER_FOR_TEXT_FIELDS,
 
+        /**
+         * Fixes reset calculation in rates where partitioning data into multiple slices can lead to incorrect results.
+         */
+        RATE_FIX_RESETS_MULTIPLE_SEGMENTS,
+
         // Last capability should still have a comma for fewer merge conflicts when adding new ones :)
         // This comment prevents the semicolon from being on the previous capability when Spotless formats the file.
         ;

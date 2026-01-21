bb18fe2864416fdc157dead6d619e99298aee436
Iván Cea Fontenla <ivan.cea@elastic.co>
2026-01-21 13:54:39 +0100

    Un-snapshot time_zone setting

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/QuerySettings.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plan/QuerySettings.java
@@ -54,7 +54,8 @@ public class QuerySettings {
     @Param(
         name = "time_zone",
         type = { "keyword" },
-        description = "The default timezone to be used in the query, by the functions and commands that require it. Defaults to UTC"
+        description = "The default timezone to be used in the query, by the functions and commands that require it. Defaults to UTC.\n" +
+            "See <<esql-timezones,timezones>>"
     )
     @Example(file = "tbucket", tag = "set-timezone-example")
     public static final QuerySettingDef<ZoneId> TIME_ZONE = new QuerySettingDef<>(
@@ -62,7 +63,7 @@ public class QuerySettings {
         DataType.KEYWORD,
         false,
         true,
-        true,
+        false,
         (value) -> {
             String timeZone = Foldables.stringLiteralValueOf(value, "Unexpected value");
             try {

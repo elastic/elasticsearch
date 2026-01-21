c7b2e124c4b2e4ac99bd9ccfa546f42f23b6ef84
Sergey Sidorov <sergey.sidorov@elastic.co>
2026-01-21 12:34:12 +0000

    rad

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1881,6 +1881,11 @@ public class EsqlCapabilities {
         PROMQL_DEG(PROMQL_PRE_TECH_PREVIEW_V14.isEnabled()),
 
         /**
+         * PromQL radians (deg → rad) function support.
+         */
+        PROMQL_RAD(PROMQL_PRE_TECH_PREVIEW_V14.isEnabled()),
+
+        /**
          * KNN function adds support for k and visit_percentage options
          */
         KNN_FUNCTION_OPTIONS_K_VISIT_PERCENTAGE,

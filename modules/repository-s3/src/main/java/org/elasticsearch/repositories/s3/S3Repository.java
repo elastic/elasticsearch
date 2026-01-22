49419f74065f1667ae80096f191425fc4f699100
Justin Xie <justin.xie@elastic.co>
2026-01-22 17:18:28 -0600

    Set MAX_COPY_SIZE_BEFORE_MULTIPART default to 100MB

--- modules/repository-s3/src/main/java/org/elasticsearch/repositories/s3/S3Repository.java
+++ modules/repository-s3/src/main/java/org/elasticsearch/repositories/s3/S3Repository.java
@@ -136,13 +136,18 @@ class S3Repository extends MeteredBlobStoreRepository {
     );
 
     /**
+     * Default size allowed for copy without multipart.
+     */
+    static final ByteSizeValue DEFAULT_COPY_SIZE_BEFORE_MULTIPART = ByteSizeValue.of(100, ByteSizeUnit.MB);
+
+    /**
      * Maximum size allowed for copy without multipart.
      * Objects larger than this will be copied using multipart copy. S3 enforces a minimum multipart size of 5 MiB and a maximum
-     * non-multipart copy size of 5 GiB. The default is to use the maximum allowable size in order to minimize request count.
+     * non-multipart copy size of 5 GiB. The default is 100MB in order to minimize request count without hitting the read timeout.
      */
     static final Setting<ByteSizeValue> MAX_COPY_SIZE_BEFORE_MULTIPART = Setting.byteSizeSetting(
         "max_copy_size_before_multipart",
-        MAX_FILE_SIZE,
+        DEFAULT_COPY_SIZE_BEFORE_MULTIPART,
         MIN_PART_SIZE_USING_MULTIPART,
         MAX_FILE_SIZE
     );

c2030fcc09f46d3d2711216f8b4bb0662ea6e0fd
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2026-01-23 13:20:23 +0100

    Simplify

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/conditional/Case.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/conditional/Case.java
@@ -271,17 +271,14 @@ public final class Case extends EsqlScalarFunction {
 
     @Override
     public Object fold(FoldContext ctx) {
-        DataType type = dataType();
-        if (type == DataType.DATE_PERIOD || type == DataType.TIME_DURATION) {
-            // These can't be managed by evaluators, we have to fold them manually.
-            for (Condition condition : conditions) {
-                if (Boolean.TRUE.equals(condition.condition.fold(ctx))) {
-                    return condition.value.fold(ctx);
-                }
+        // We can't rely on EvaluatorMapper.fold(), because this can contain DATE_PERIOD and TIME_DURATION
+        // that don't have a data type associated, so the creation of the evaluator would fail.
+        for (Condition condition : conditions) {
+            if (Boolean.TRUE.equals(condition.condition.fold(ctx))) {
+                return condition.value.fold(ctx);
             }
-            return elseValue.fold(ctx);
         }
-        return super.fold(ctx);
+        return elseValue.fold(ctx);
     }
 
     /**

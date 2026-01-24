a95acf62f4769e13d7d97b62d1905eadafb41944
zhubotang-wq <zhubo.tang@elastic.co>
2026-01-23 23:16:05 -0800

    ES-13866 Enforce Index count limit

--- server/src/internalClusterTest/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexServiceIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexServiceIT.java
@@ -14,17 +14,98 @@ import org.elasticsearch.action.admin.indices.create.CreateIndexClusterStateUpda
 import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
 import org.elasticsearch.action.admin.indices.get.GetIndexResponse;
 import org.elasticsearch.action.support.master.ShardsAcknowledgedResponse;
+import org.elasticsearch.common.compress.CompressedXContent;
+import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.CollectionUtils;
 import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.indices.ExecutorNames;
+import org.elasticsearch.indices.SystemDataStreamDescriptor;
+import org.elasticsearch.indices.SystemIndexDescriptor;
+import org.elasticsearch.indices.SystemIndexDescriptorUtils;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.plugins.SystemIndexPlugin;
 import org.elasticsearch.test.ESIntegTestCase;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.matchesPattern;
 
+@ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST)
 public class MetadataCreateIndexServiceIT extends ESIntegTestCase {
+
+    @Override
+    protected Collection<Class<? extends Plugin>> nodePlugins() {
+        return List.of(TestPlugin.class);
+    }
+
+    public static class TestPlugin extends Plugin implements SystemIndexPlugin {
+        @Override
+        public List<Setting<?>> getSettings() {
+            return CollectionUtils.appendToCopyNoNullElements(
+                super.getSettings(),
+                MetadataCreateIndexService.SETTING_CLUSTER_MAX_INDICES_PER_PROJECT
+            );
+        }
+
+        @Override
+        public Collection<SystemIndexDescriptor> getSystemIndexDescriptors(Settings settings) {
+            return List.of(
+                SystemIndexDescriptorUtils.createUnmanaged(".my-elasticsearch-system-*", "my elasticsearch system"),
+                SystemIndexDescriptorUtils.createUnmanaged(".test-index-limit-*", ".test index limit"),
+                SystemIndexDescriptorUtils.createUnmanaged(".system-index-exempted-*", "system index exempted")
+            );
+        }
+
+        @Override
+        public Collection<SystemDataStreamDescriptor> getSystemDataStreamDescriptors() {
+            try {
+                CompressedXContent mappings = new CompressedXContent("{\"properties\":{\"name\":{\"type\":\"keyword\"}}}");
+                return List.of(
+                    new SystemDataStreamDescriptor(
+                        ".test-data-stream",
+                        "system data stream test",
+                        SystemDataStreamDescriptor.Type.EXTERNAL,
+                        ComposableIndexTemplate.builder()
+                            .indexPatterns(
+                                List.of(
+                                    ".my-elasticsearch-system-data-stream-*",
+                                    ".test-index-limit-data-stream-*",
+                                    ".test-system-index-exempted-*"
+                                )
+                            )
+                            .template(new Template(Settings.EMPTY, mappings, null))
+                            .dataStreamTemplate(new ComposableIndexTemplate.DataStreamTemplate())
+                            .build(),
+                        Map.of(),
+                        List.of("product"),
+                        "product",
+                        ExecutorNames.DEFAULT_SYSTEM_DATA_STREAM_THREAD_POOLS
+                    )
+                );
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public String getFeatureName() {
+            return "Test-User-Indices-Limit";
+        }
+
+        @Override
+        public String getFeatureDescription() {
+            return "Test User Indices Limit";
+        }
+    }
 
     public void testRequestTemplateIsRespected() throws InterruptedException {
         /*
@@ -75,4 +156,114 @@ public class MetadataCreateIndexServiceIT extends ESIntegTestCase {
         Settings settings = response.getSettings().get(indexName);
         assertThat(settings.get("index.number_of_replicas"), equalTo(Integer.toString(numberOfReplicas)));
     }
+
+    private void testCreateIndex(
+        String indexName,
+        ActionListener<ShardsAcknowledgedResponse> listener,
+        MetadataCreateIndexService metadataCreateIndexService
+    ) {
+        CreateIndexClusterStateUpdateRequest request = new CreateIndexClusterStateUpdateRequest(
+            "testCreateIndexLimit",
+            ProjectId.DEFAULT,
+            indexName,
+            randomAlphaOfLength(20)
+        );
+        request.setMatchingTemplate(
+            ComposableIndexTemplate.builder()
+                .template(Template.builder().settings(Settings.builder().put("index.number_of_replicas", 1)))
+                .build()
+        );
+        metadataCreateIndexService.createIndex(
+            TimeValue.THIRTY_SECONDS,
+            TimeValue.THIRTY_SECONDS,
+            TimeValue.THIRTY_SECONDS,
+            request,
+            listener
+        );
+    }
+
+    private Function<CountDownLatch, ActionListener<ShardsAcknowledgedResponse>> expectSuccessListenerSupplier =
+        countDownLatch -> new ActionListener<>() {
+            @Override
+            public void onResponse(ShardsAcknowledgedResponse shardsAcknowledgedResponse) {
+                countDownLatch.countDown();
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                logger.error(e);
+                fail(e);
+            }
+        };
+
+    private Function<CountDownLatch, ActionListener<ShardsAcknowledgedResponse>> expectLimitExceededListenerSupplier =
+        countDownLatch -> new ActionListener<>() {
+            @Override
+            public void onResponse(ShardsAcknowledgedResponse shardsAcknowledgedResponse) {
+                fail("This should not succeed");
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                assertThat(
+                    e.getMessage(),
+                    matchesPattern(
+                            "This action would add an index, but this project currently has \\[\\d*]/\\[\\d*] maximum indices; for more information, "
+                            + "see https://www.elastic.co/docs/deploy-manage/production-guidance/optimize-performance/index-count-limit?"
+                            + "version=master"
+                    )
+                );
+                countDownLatch.countDown();
+            }
+        };
+
+    public void testCreateIndexLimit() throws InterruptedException {
+        final String indexName = randomAlphaOfLength(20).toLowerCase(Locale.ROOT);
+        int userIndicesLimit = randomIntBetween(5, 10);
+        final List<String> systemIndexAndDataStreamsPatterns = List.of(
+            ".my-elasticsearch-system-",
+            ".test-index-limit-",
+            ".system-index-exempted-",
+            ".my-elasticsearch-system-data-stream-",
+            ".test-index-limit-data-stream-",
+            ".test-system-index-exempted-"
+        );
+
+        final var metadataCreateIndexService = internalCluster().getCurrentMasterNodeInstance(MetadataCreateIndexService.class);
+        updateClusterSettings(
+            Settings.builder().put(MetadataCreateIndexService.SETTING_CLUSTER_MAX_INDICES_PER_PROJECT.getKey(), userIndicesLimit)
+        );
+
+        var systemIndices = userIndicesLimit + randomIntBetween(5, 10);
+        var userIndices = userIndicesLimit - 1;
+        CountDownLatch listenerCalledLatch = new CountDownLatch(systemIndices);
+
+        // System indices are exempted from limit constraint.
+        for (int i = 1; i <= systemIndices; i++) {
+            testCreateIndex(
+                randomFrom(systemIndexAndDataStreamsPatterns) + i,
+                expectSuccessListenerSupplier.apply(listenerCalledLatch),
+                metadataCreateIndexService
+            );
+        }
+        // Must wait for system indices creations to complete for IndexMetadata.isSystem to take effects.
+        listenerCalledLatch.await(10, TimeUnit.SECONDS);
+
+        listenerCalledLatch = new CountDownLatch(userIndices);
+        // System indices are exempted from limit constraint.
+        for (int i = 1; i < userIndices; i++) {
+            testCreateIndex(indexName + i, expectSuccessListenerSupplier.apply(listenerCalledLatch), metadataCreateIndexService);
+        }
+        // Within user indices limit should succeed.
+        //safeAwait()
+
+        // all subsequent attempts should fail.
+        var failedAttempts = randomIntBetween(3, 6);
+        listenerCalledLatch = new CountDownLatch(failedAttempts);
+        for (int i = 1; i <= failedAttempts; i++) {
+            //testCreateIndex(indexName + randomUUID(), expectLimitExceededListenerSupplier.apply(listenerCalledLatch), metadataCreateIndexService);
+        }
+        //listenerCalledLatch.await(10, TimeUnit.SECONDS);
+    }
+
 }

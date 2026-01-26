480c3b20053017b307922c83e4e755e7aa19f10c
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-01-26 17:14:40 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/search/aggregations/metrics/TopHitsAggregator.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/metrics/TopHitsAggregator.java
@@ -52,7 +52,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;

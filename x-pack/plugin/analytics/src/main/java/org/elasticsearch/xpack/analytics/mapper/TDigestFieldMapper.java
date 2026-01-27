3e099b85df0c7dc268c56a545365985728ceab7f
gmarouli <mary.gouseti@elastic.co>
2026-01-27 19:49:03 +0200

    Revert some changes to TDigestFieldMapper that we did not use.

--- x-pack/plugin/analytics/src/main/java/org/elasticsearch/xpack/analytics/mapper/TDigestFieldMapper.java
+++ x-pack/plugin/analytics/src/main/java/org/elasticsearch/xpack/analytics/mapper/TDigestFieldMapper.java
@@ -145,13 +145,7 @@ public class TDigestFieldMapper extends FieldMapper {
         public TDigestFieldMapper build(MapperBuilderContext context) {
             return new TDigestFieldMapper(
                 leafName(),
-                new TDigestFieldType(
-                    context.buildFullName(leafName()),
-                    meta.getValue(),
-                    this.digestType.getValue(),
-                    this.compression.getValue(),
-                    this.metric.getValue()
-                ),
+                new TDigestFieldType(context.buildFullName(leafName()), meta.getValue(), this.metric.getValue()),
                 builderParams(this, context),
                 this
             );
@@ -208,19 +202,9 @@ public class TDigestFieldMapper extends FieldMapper {
 
     public static class TDigestFieldType extends MappedFieldType {
         private final TimeSeriesParams.MetricType metricType;
-        private final TDigestState.Type tDigestType;
-        private final double compression;
 
-        public TDigestFieldType(
-            String name,
-            Map<String, String> meta,
-            TDigestState.Type tDigestType,
-            double compression,
-            TimeSeriesParams.MetricType metricType
-        ) {
+        public TDigestFieldType(String name, Map<String, String> meta, TimeSeriesParams.MetricType metricType) {
             super(name, IndexType.docValuesOnly(), false, meta);
-            this.tDigestType = tDigestType;
-            this.compression = compression;
             this.metricType = metricType;
         }
 
@@ -232,14 +216,6 @@ public class TDigestFieldMapper extends FieldMapper {
         @Override
         public TimeSeriesParams.MetricType getMetricType() {
             return metricType;
-        }
-
-        public TDigestState.Type getTDigestType() {
-            return tDigestType;
-        }
-
-        public double getCompression() {
-            return compression;
         }
 
         @Override

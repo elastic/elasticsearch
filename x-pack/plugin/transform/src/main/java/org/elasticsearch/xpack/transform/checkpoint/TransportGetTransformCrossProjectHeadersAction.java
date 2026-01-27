8bc434dd8d449bb572e7e3e6e0074ff22466f78f
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2026-01-27 10:38:08 +0100

    Clean up

--- x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/checkpoint/TransportGetTransformCrossProjectHeadersAction.java
+++ x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/checkpoint/TransportGetTransformCrossProjectHeadersAction.java
@@ -12,14 +12,12 @@ import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.TransportAction;
-import org.elasticsearch.client.internal.Client;
 import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.injection.guice.Inject;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
-import org.elasticsearch.xpack.core.ClientHelper;
 import org.elasticsearch.xpack.core.transform.transforms.TransformConfig;
 
 import java.util.HashMap;
@@ -47,28 +45,6 @@ public class TransportGetTransformCrossProjectHeadersAction extends TransportAct
         this.threadPool = threadPool;
     }
 
-    public static void executeWithCrossProjectHeaders(Client client, TransformConfig transformConfig, ActionListener<Void> listener) {
-        // force fresh token by removing any existing request-scoped credential
-        Map<String, String> currentHeaders = copyWithoutRequestScopedCredential(transformConfig.getHeaders());
-        ClientHelper.executeWithHeadersAsync(
-            currentHeaders,
-            ClientHelper.TRANSFORM_ORIGIN,
-            client,
-            GetTransformCrossProjectHeadersAction.INSTANCE,
-            new GetTransformCrossProjectHeadersAction.Request(transformConfig),
-            ActionListener.wrap(r -> listener.onResponse(null), listener::onFailure)
-        );
-    }
-
-    private static Map<String, String> copyWithoutRequestScopedCredential(Map<String, String> headers) {
-        if (headers.containsKey("_security_serverless_request_scoped_credential") == false) {
-            return headers;
-        }
-        var copy = new HashMap<>(headers);
-        copy.remove("_security_serverless_request_scoped_credential");
-        return copy;
-    }
-
     @Override
     protected void doExecute(
         Task task,
@@ -76,12 +52,6 @@ public class TransportGetTransformCrossProjectHeadersAction extends TransportAct
         ActionListener<GetTransformCrossProjectHeadersAction.Response> listener
     ) {
         final TransformConfig transformConfig = request.getTransformConfig();
-
-        if (transformConfig.getHeaders().containsKey("_security_serverless_request_scoped_credential")) {
-            logger.warn("Transform config contains serverless credential header, skipping...");
-            listener.onResponse(GetTransformCrossProjectHeadersAction.Response.INSTANCE);
-            return;
-        }
 
         final ThreadContext threadContext = threadPool.getThreadContext();
         final Map<String, String> previousHeaders = transformConfig.getHeaders();

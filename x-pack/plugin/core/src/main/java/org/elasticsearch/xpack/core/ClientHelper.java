57c3dad6249ded275c351a0f372a446b54c15118
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2026-01-28 10:01:39 +0100

    Revert "add missing code"
    This reverts commit b6c0922b7a9d1ad813c339c38050bda3345a1dd5.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ClientHelper.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ClientHelper.java
@@ -62,8 +62,7 @@ public final class ClientHelper {
     public static final Set<String> SECURITY_HEADER_FILTERS = Set.of(
         AuthenticationServiceField.RUN_AS_USER_HEADER,
         AuthenticationField.AUTHENTICATION_KEY,
-        SecondaryAuthentication.THREAD_CTX_KEY,
-        "_security_serverless_request_scoped_credential"
+        SecondaryAuthentication.THREAD_CTX_KEY
     );
 
     /**

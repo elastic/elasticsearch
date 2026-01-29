4352a43513e590eeb7f42ba3835f1814db272409
Jan Kazlouski <jan.kazlouski@elastic.co>
2026-01-29 07:12:02 +0200

    Add validation failure handling for inference model updates

--- x-pack/plugin/inference/qa/test-service-plugin/src/main/java/org/elasticsearch/xpack/inference/mock/AbstractTestInferenceService.java
+++ x-pack/plugin/inference/qa/test-service-plugin/src/main/java/org/elasticsearch/xpack/inference/mock/AbstractTestInferenceService.java
@@ -191,27 +191,29 @@ public abstract class AbstractTestInferenceService implements InferenceService {
         }
     }
 
-    public record TestTaskSettings(Integer temperature) implements TaskSettings {
+    public record TestTaskSettings(Integer temperature, Boolean shouldFailValidation) implements TaskSettings {
 
         static final String NAME = "test_task_settings";
 
         public static TestTaskSettings fromMap(Map<String, Object> map) {
             Integer temperature = (Integer) map.remove("temperature");
-            return new TestTaskSettings(temperature);
+            Boolean shouldFailValidation = (Boolean) map.remove("should_fail_validation");
+            return new TestTaskSettings(temperature, shouldFailValidation);
         }
 
         public TestTaskSettings(StreamInput in) throws IOException {
-            this(in.readOptionalVInt());
+            this(in.readOptionalVInt(), in.readOptionalBoolean());
         }
 
         @Override
         public boolean isEmpty() {
-            return temperature == null;
+            return temperature == null && shouldFailValidation == null;
         }
 
         @Override
         public void writeTo(StreamOutput out) throws IOException {
             out.writeOptionalVInt(temperature);
+            out.writeOptionalBoolean(shouldFailValidation);
         }
 
         @Override
@@ -219,6 +221,9 @@ public abstract class AbstractTestInferenceService implements InferenceService {
             builder.startObject();
             if (temperature != null) {
                 builder.field("temperature", temperature);
+            }
+            if (shouldFailValidation != null) {
+                builder.field("should_fail_validation", shouldFailValidation);
             }
             builder.endObject();
             return builder;

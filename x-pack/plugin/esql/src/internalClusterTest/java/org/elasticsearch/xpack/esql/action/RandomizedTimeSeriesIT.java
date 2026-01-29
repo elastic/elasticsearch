f2f553831f29f06e5ea7c8c530f291e77971a9f0
Kostas Krikellas <131142368+kkrik-es@users.noreply.github.com>
2026-01-29 17:33:04 +0200

    Update RandomizedTimeSeriesIT.java

--- x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/RandomizedTimeSeriesIT.java
+++ x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/RandomizedTimeSeriesIT.java
@@ -326,7 +326,7 @@ public class RandomizedTimeSeriesIT extends AbstractEsqlIntegTestCase {
                     && timeseries.getFirst().v2().v1().toEpochMilli() % (secondsInWindow * 1000L) == 0
                     && offset > 0
                     && allTimeseries.get(offset - 1).isEmpty() == false) {
-                    // Value at lower boundary is present, check if there's one in the lower boundary to use.
+                    // Value at lower boundary is present, check if there's one in the previous window to use.
                     addLastTupleFromLowerWindow(timeseries, allTimeseries.get(offset - 1), secondsInWindow);
                 }
                 if (timeseries.size() < 2) {

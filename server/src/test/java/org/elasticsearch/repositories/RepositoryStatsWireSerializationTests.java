57718405695baa3123de30fcbc98033fe787983f
Joshua Adams <joshua.adams@elastic.co>
2026-01-29 11:58:51 +0000

    Change mutateInstance

--- server/src/test/java/org/elasticsearch/repositories/RepositoryStatsWireSerializationTests.java
+++ server/src/test/java/org/elasticsearch/repositories/RepositoryStatsWireSerializationTests.java
@@ -13,8 +13,6 @@ import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.test.AbstractWireSerializingTestCase;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.elasticsearch.core.Tuple.tuple;
 import static org.elasticsearch.repositories.SnapshotStatsWireSerializationTests.randomSnapshotStats;
@@ -32,33 +30,6 @@ public class RepositoryStatsWireSerializationTests extends AbstractWireSerializi
 
     @Override
     protected RepositoriesStats mutateInstance(RepositoriesStats instance) throws IOException {
-        var original = instance.getRepositorySnapshotStats();
-        var mutated = new HashMap<>(original);
-        int mutation = randomIntBetween(0, 3);
-        switch (mutation) {
-            case 0 -> {
-                // Replace an existing value
-                String key = randomFrom(mutated.keySet());
-                mutated.put(key, randomSnapshotStats());
-            }
-            case 1 -> {
-                // Add a new entry
-                mutated.put(randomAlphaOfLength(10), randomSnapshotStats());
-            }
-            case 2 -> {
-                // Remove an entry, possibly resulting in an empty map
-                mutated.remove(randomFrom(mutated.keySet()));
-            }
-            case 3 -> {
-                // Change everything, rebuilding the map with different size
-                Map<String, RepositoriesStats.SnapshotStats> rebuilt;
-                do {
-                    rebuilt = randomMap(1, 5, () -> tuple(randomAlphaOfLength(10), randomSnapshotStats()));
-                } while (rebuilt.equals(original));
-                mutated.clear();
-                mutated.putAll(rebuilt);
-            }
-        }
-        return new RepositoriesStats(mutated);
+        return createTestInstance();
     }
 }

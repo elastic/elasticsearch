e4d21f5c02d341ab70438949c750495b298561d3
Anton Persson <anton.persson@elastic.co>
2026-01-29 16:25:46 +0100

    Rolling upgrade test for synthetic id
    Depending on cluster indexVersion we expect index with
    index.mapping.use_synthetic_id=true to either be refused by throwing
    exception on creation or be accepted and be available for indexing
    documents.

---
+++ qa/rolling-upgrade/src/javaRestTest/java/org/elasticsearch/upgrades/TSDBSyntheticIdUpgradeIT.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+package org.elasticsearch.upgrades;
+
+import com.carrotsearch.randomizedtesting.annotations.Name;
+
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseException;
+import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.IndexMode;
+import org.elasticsearch.index.IndexSettings;
+import org.elasticsearch.index.IndexVersion;
+import org.elasticsearch.index.IndexVersions;
+import org.hamcrest.Matchers;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
+import java.util.StringJoiner;
+
+public class TSDBSyntheticIdUpgradeIT extends AbstractRollingUpgradeTestCase {
+    private static final int DOC_COUNT = 10;
+
+    public TSDBSyntheticIdUpgradeIT(@Name("upgradedNodes") int upgradedNodes) {
+        super(upgradedNodes);
+    }
+
+    public void testRollingUpgrade() throws IOException {
+        IndexVersion oldClusterIndexVersion = getOldClusterIndexVersion();
+
+        if (hasSupportForSyntheticId(oldClusterIndexVersion)) {
+            if (isOldCluster()) {
+                assertWriteIndex("old-cluster-index");
+                assertIndexRead("old-cluster-index");
+            }
+
+            if (isFirstMixedCluster()) {
+                assertWriteIndex("first-mixed-cluster-index");
+                assertIndexRead("old-cluster-index");
+                assertIndexRead("first-mixed-cluster-index");
+            }
+
+            if (isFirstMixedCluster() == false && isMixedCluster()) {
+                assertWriteIndex("second-mixed-cluster-index");
+                assertIndexRead("old-cluster-index");
+                assertIndexRead("first-mixed-cluster-index");
+                assertIndexRead("second-mixed-cluster-index");
+            }
+
+            if (isUpgradedCluster()) {
+                assertWriteIndex("upgraded-cluster-index");
+                assertIndexRead("old-cluster-index");
+                assertIndexRead("first-mixed-cluster-index");
+                assertIndexRead("second-mixed-cluster-index");
+                assertIndexRead("upgraded-cluster-index");
+            }
+        } else {
+
+            if (isOldCluster()) {
+                assertNoWriteIndex("old-cluster-index");
+            }
+
+            if (isMixedCluster()) {
+                assertNoWriteIndex("mixed-cluster-index");
+            }
+
+            if (isUpgradedCluster()) {
+                assertWriteIndex("upgraded-cluster-index");
+                assertIndexRead("upgraded-cluster-index");
+            }
+        }
+    }
+
+    private static void assertWriteIndex(String indexName) throws IOException {
+        assertIndexCanBeCreated(indexName);
+        assertCanAddDocuments(indexName);
+    }
+
+    private static void assertIndexRead(String indexName) throws IOException {
+        assertTrue("Expected index [" + indexName + "] to exist, but did not", indexExists(indexName));
+        assertDocCount(client(), indexName, DOC_COUNT);
+    }
+
+    private static void assertIndexCanBeCreated(String indexName) throws IOException {
+        CreateIndexResponse response = null;
+        try {
+            response = createSyntheticIdIndex(indexName);
+            assertTrue("Expected index [" + indexName + "] to be created successfully, but was not", response.isAcknowledged());
+            assertTrue(
+                "Expected shards of index [" + indexName + "] to be created successfully, but was not",
+                response.isShardsAcknowledged()
+            );
+        } finally {
+            if (response != null) {
+                response.decRef();
+            }
+        }
+    }
+
+    private static void assertCanAddDocuments(String indexName) throws IOException {
+        StringJoiner joiner = new StringJoiner("\n", "", "\n");
+        Instant now = Instant.now();
+        for (int i = 0; i < DOC_COUNT; i++) {
+            addDocument(joiner, now.plus(i, ChronoUnit.SECONDS));
+        }
+        var request = new Request("PUT", "/" + indexName + "/_bulk");
+        request.setJsonEntity(joiner.toString());
+        request.addParameter("refresh", "true");
+        Response response = client().performRequest(request);
+        assertOK(response);
+    }
+
+    private static void addDocument(StringJoiner joiner, Instant timestamp) {
+        joiner.add("{\"create\": {}}");
+        joiner.add(String.format(Locale.ROOT, """
+            {"@timestamp": "%s", "hostname": "host", "metric": {"field": "cpu-load", "value": %d}}
+            """, timestamp, randomByte()));
+    }
+
+    private static void assertNoWriteIndex(String indexName) {
+        ResponseException e = assertThrows(ResponseException.class, () -> createSyntheticIdIndex(indexName));
+        assertThat(e.getMessage(), Matchers.containsString("unknown setting [" + IndexSettings.USE_SYNTHETIC_ID.getKey() + "]"));
+        assertThat(e.getMessage(), Matchers.containsString("illegal_argument_exception"));
+    }
+
+    private static CreateIndexResponse createSyntheticIdIndex(String indexName) throws IOException {
+        Settings settings = Settings.builder()
+            .put(IndexSettings.USE_SYNTHETIC_ID.getKey(), true)
+            .put(IndexSettings.MODE.getKey(), IndexMode.TIME_SERIES)
+            .put(IndexMetadata.INDEX_ROUTING_PATH.getKey(), "hostname")
+            .build();
+        final var mapping = """
+            {
+                "properties": {
+                    "@timestamp": {
+                        "type": "date"
+                    },
+                    "hostname": {
+                        "type": "keyword",
+                        "time_series_dimension": true
+                    },
+                    "metric": {
+                        "properties": {
+                            "field": {
+                                "type": "keyword",
+                                "time_series_dimension": true
+                            },
+                            "value": {
+                                "type": "integer",
+                                "time_series_metric": "counter"
+                            }
+                        }
+                    }
+                }
+            }
+            """;
+        return createIndex(indexName, settings, mapping);
+    }
+
+    private boolean hasSupportForSyntheticId(IndexVersion indexVersion) {
+        return indexVersion.onOrAfter(IndexVersions.TIME_SERIES_USE_SYNTHETIC_ID_94);
+    }
+}

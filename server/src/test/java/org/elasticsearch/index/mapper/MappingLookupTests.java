032bc0de9cba50ce653d011d4168bf80e84e1f6b
gmarouli <mary.gouseti@elastic.co>
2026-01-29 21:59:43 +0200

    Add index mode parameter to MappingLookup

--- server/src/test/java/org/elasticsearch/index/mapper/MappingLookupTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/MappingLookupTests.java
@@ -59,7 +59,7 @@ public class MappingLookupTests extends ESTestCase {
             new MetadataFieldMapper[0],
             Collections.emptyMap()
         );
-        return MappingLookup.fromMappers(mapping, fieldMappers, objectMappers);
+        return MappingLookup.fromMappers(mapping, fieldMappers, objectMappers, indexMode);
     }
 
     public void testOnlyRuntimeField() {

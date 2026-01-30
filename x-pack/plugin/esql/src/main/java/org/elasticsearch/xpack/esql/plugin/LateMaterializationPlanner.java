c8913014fa9e7684275eddc3d5ff7fc6166f217f
ncordon <nacho.cordon@elastic.co>
2026-01-30 12:16:44 +0100

    Extra assert to check whether we can make the condition explicit

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/LateMaterializationPlanner.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/LateMaterializationPlanner.java
@@ -131,10 +131,11 @@ class LateMaterializationPlanner {
         ExchangeSinkExec updatedDataPlan = originalPlan.replaceChild(updatedFragmentExec);
 
         // Replace the TopN child with the data driver as the source.
-        PhysicalPlan reductionPlan = toPhysical(fragmentExec.fragment(), context).transformDown(
-            TopNExec.class,
-            t -> t.replaceChild(new ExchangeSourceExec(topN.source(), expectedDataOutput, false /* isIntermediateAgg */)).withSortedInput()
-        );
+        PhysicalPlan reductionPlan = toPhysical(fragmentExec.fragment(), context).transformDown(TopNExec.class, t -> {
+            PhysicalPlan exchangeExec = new ExchangeSourceExec(topN.source(), expectedDataOutput, false /* isIntermediateAgg */);
+            assert updatedFragment.child() instanceof TopN;
+            return t.replaceChild(exchangeExec).withSortedInput();
+        });
         ExchangeSinkExec reductionPlanWithSize = originalPlan.replaceChild(
             EstimatesRowSize.estimateRowSize(updatedFragmentExec.estimatedRowSize(), reductionPlan)
         );

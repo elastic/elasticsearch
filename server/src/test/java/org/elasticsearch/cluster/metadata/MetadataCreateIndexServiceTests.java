6a0ac9348bcb71e671fd0bde38c79f092eaa49a5
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-01-31 01:37:17 +0000

    [CI] Auto commit changes from spotless

--- server/src/test/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexServiceTests.java
+++ server/src/test/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexServiceTests.java
@@ -95,7 +95,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;

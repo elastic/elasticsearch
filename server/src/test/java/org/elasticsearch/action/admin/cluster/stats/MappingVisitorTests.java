dc12f0ceb2cc28656bef0539879415d9f85f02fb
Mary Gouseti <mary.gouseti@elastic.co>
2026-02-03 09:05:40 +0100

    Fix downsampling with disabled subobjects (#138715) (#141622)
    We change the way the downsampled mapping gets created. Instead of creating a new mapping with the metric fields and then merging it with the original, we use a visitor to copy and change the original.

--- server/src/test/java/org/elasticsearch/action/admin/cluster/stats/MappingVisitorTests.java
+++ server/src/test/java/org/elasticsearch/action/admin/cluster/stats/MappingVisitorTests.java
@@ -9,6 +9,9 @@
 
 package org.elasticsearch.action.admin.cluster.stats;
 
+import org.elasticsearch.datageneration.DataGeneratorSpecification;
+import org.elasticsearch.datageneration.MappingGenerator;
+import org.elasticsearch.datageneration.TemplateGenerator;
 import org.elasticsearch.test.ESTestCase;
 
 import java.util.Arrays;
@@ -18,7 +21,11 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import static org.hamcrest.Matchers.equalTo;
+
 public class MappingVisitorTests extends ESTestCase {
+
+    private static final int MAPPING_GENERATION_ROUNDS = 500;
 
     private static void collectTypes(Map<String, ?> mapping, Set<String> types) {
         MappingVisitor.visitMapping(mapping, (f, m) -> {
@@ -138,6 +145,20 @@ public class MappingVisitorTests extends ESTestCase {
         assertEquals(new HashSet<>(Arrays.asList("keyword", "object")), fields);
     }
 
+    @SuppressWarnings("unchecked")
+    public void testVisitAndCopy() {
+        DataGeneratorSpecification specification = DataGeneratorSpecification.buildDefault();
+        var template = new TemplateGenerator(specification).generate();
+        MappingGenerator mappingGenerator = new MappingGenerator(specification);
+        for (int i = 0; i < MAPPING_GENERATION_ROUNDS; i++) {
+            var mapping = mappingGenerator.generate(template).raw();
+            var properties = (Map<String, Object>) mapping.get("_doc");
+            var updatedMapping = new HashMap<String, Object>();
+            MappingVisitor.visitPropertiesAndCopyMapping(properties, updatedMapping, (f, source, dest) -> dest.put(f, source));
+            assertThat(updatedMapping, equalTo(updatedMapping));
+        }
+    }
+
     public void testCountRuntimeFields() {
         Map<String, Object> mapping = new HashMap<>();
         Set<String> fields = new HashSet<>();
@@ -168,5 +189,63 @@ public class MappingVisitorTests extends ESTestCase {
 
     private static void collectRuntimeTypes(Map<String, ?> mapping, Set<String> types) {
         MappingVisitor.visitRuntimeMapping(mapping, (f, m) -> types.add(m.get("type").toString()));
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testConvertLongToKeyword() {
+        Map<String, Object> longType = Map.of("type", "long");
+        Map<String, Object> textType = Map.of("type", "text");
+        Map<String, Object> floatType = Map.of("type", "float", "scaling_factor", 1000);
+        Map<String, Object> multiField = Map.of("type", "keyword", "fields", Map.of("my-long", longType, "my-float", floatType));
+        Map<String, Object> objectField = Map.of("type", "object", "properties", Map.of("my-text", textType, "my-long", longType));
+        Map<String, Object> expectedProperties = Map.of(
+            "properties",
+            Map.of("my-long", longType, "my-float", floatType, "my-multi-field", multiField, "my-object", objectField)
+        );
+
+        HashMap<String, Object> result = new HashMap<>();
+        MappingVisitor.visitPropertiesAndCopyMapping(expectedProperties, result, (ignored, source, dest) -> {
+            for (String key : source.keySet()) {
+                if (key.equals("type") && source.get(key).equals("long")) {
+                    dest.put(key, "keyword");
+                } else {
+                    dest.put(key, source.get(key));
+                }
+            }
+        });
+
+        assertTrue(result.containsKey("properties"));
+        Map<String, Object> properties = (Map<String, Object>) result.get("properties");
+
+        assertTrue(properties.containsKey("my-long"));
+        Map<String, Object> myLong = (Map<String, Object>) properties.get("my-long");
+        assertEquals("keyword", myLong.get("type"));
+
+        assertTrue(properties.containsKey("my-float"));
+        Map<String, Object> myFloat = (Map<String, Object>) properties.get("my-float");
+        assertEquals("float", myFloat.get("type"));
+        assertEquals(1000, myFloat.get("scaling_factor"));
+
+        assertTrue(properties.containsKey("my-multi-field"));
+        Map<String, Object> myMultiField = (Map<String, Object>) properties.get("my-multi-field");
+        assertEquals("keyword", myMultiField.get("type"));
+        assertTrue(myMultiField.containsKey("fields"));
+        Map<String, Object> foundFields = (Map<String, Object>) myMultiField.get("fields");
+        assertTrue(foundFields.containsKey("my-long"));
+        // multi fields are not converted to keyword
+        assertEquals("long", ((Map<String, Object>) foundFields.get("my-long")).get("type"));
+        assertTrue(foundFields.containsKey("my-float"));
+        assertEquals("float", ((Map<String, Object>) foundFields.get("my-float")).get("type"));
+        assertEquals(1000, ((Map<String, Object>) foundFields.get("my-float")).get("scaling_factor"));
+
+        assertTrue(properties.containsKey("my-object"));
+        Map<String, Object> myObject = (Map<String, Object>) properties.get("my-object");
+        assertEquals("object", myObject.get("type"));
+        assertTrue(myObject.containsKey("properties"));
+        Map<String, Object> foundSubObjects = (Map<String, Object>) myObject.get("properties");
+        assertTrue(foundSubObjects.containsKey("my-long"));
+        assertEquals("keyword", ((Map<String, Object>) foundSubObjects.get("my-long")).get("type"));
+        assertTrue(foundSubObjects.containsKey("my-text"));
+        assertEquals("text", ((Map<String, Object>) foundSubObjects.get("my-text")).get("type"));
     }
 }

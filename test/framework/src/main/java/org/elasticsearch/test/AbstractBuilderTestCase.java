4c2c13135c7189457c4da9decbe42ad8ff8ac178
Jim Ferenczi <jim.ferenczi@elastic.co>
2026-02-03 11:46:27 +0100

    fix compil

--- test/framework/src/main/java/org/elasticsearch/test/AbstractBuilderTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/AbstractBuilderTestCase.java
@@ -57,6 +57,7 @@ import org.elasticsearch.index.query.CoordinatorRewriteContext;
 import org.elasticsearch.index.query.DataRewriteContext;
 import org.elasticsearch.index.query.QueryRewriteContext;
 import org.elasticsearch.index.query.SearchExecutionContext;
+import org.elasticsearch.index.search.stats.ShardSearchStats;
 import org.elasticsearch.index.shard.IndexLongFieldRange;
 import org.elasticsearch.index.shard.ShardLongFieldRange;
 import org.elasticsearch.index.similarity.SimilarityService;
@@ -615,7 +616,9 @@ public abstract class AbstractBuilderTestCase extends ESTestCase {
                 () -> true,
                 null,
                 emptyMap(),
-                MapperMetrics.NOOP
+                0,
+                MapperMetrics.NOOP,
+                ShardSearchStats.TEST_INSTANCE
             );
         }
 

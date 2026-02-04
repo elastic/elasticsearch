2c4f97512d75853e5ceee1200a09e01619a535ae
Lorenzo Dematte <lorenzo.dematte@elastic.co>
2026-02-04 19:03:00 +0100

    Fallback via temp file for huge segments

--- libs/gpu-codec/src/main/java/org/elasticsearch/gpu/codec/DatasetUtilsImpl.java
+++ libs/gpu-codec/src/main/java/org/elasticsearch/gpu/codec/DatasetUtilsImpl.java
@@ -12,9 +12,6 @@ package org.elasticsearch.gpu.codec;
 import com.nvidia.cuvs.CuVSMatrix;
 import com.nvidia.cuvs.spi.CuVSProvider;
 
-import org.apache.lucene.store.MemorySegmentAccessInput;
-
-import java.io.IOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.invoke.MethodHandle;
 
@@ -23,7 +20,6 @@ class DatasetUtilsImpl implements DatasetUtils {
     private static final DatasetUtils INSTANCE = new DatasetUtilsImpl();
 
     private static final MethodHandle createDataset$mh = CuVSProvider.provider().newNativeMatrixBuilder();
-    private static final MethodHandle createDatasetWithStrides$mh = CuVSProvider.provider().newNativeMatrixBuilderWithStrides();
 
     static DatasetUtils getInstance() {
         return INSTANCE;
@@ -43,95 +39,25 @@ class DatasetUtilsImpl implements DatasetUtils {
         }
     }
 
-    static CuVSMatrix fromMemorySegment(
-        MemorySegment memorySegment,
-        int size,
-        int dimensions,
-        int rowStride,
-        int columnStride,
-        CuVSMatrix.DataType dataType
-    ) {
-        try {
-            return (CuVSMatrix) createDatasetWithStrides$mh.invokeExact(memorySegment, size, dimensions, rowStride, columnStride, dataType);
-        } catch (Throwable e) {
-            if (e instanceof Error err) {
-                throw err;
-            } else if (e instanceof RuntimeException re) {
-                throw re;
-            } else {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
     private DatasetUtilsImpl() {}
 
     @Override
-    public CuVSMatrix fromInput(MemorySegmentAccessInput input, int numVectors, int dims, CuVSMatrix.DataType dataType) throws IOException {
+    public CuVSMatrix fromInput(MemorySegment input, int numVectors, int dims, CuVSMatrix.DataType dataType) {
         if (numVectors < 0 || dims < 0) {
             throwIllegalArgumentException(numVectors, dims);
         }
-        return createCuVSMatrix(input, 0L, input.length(), numVectors, dims, dataType);
+        return createCuVSMatrix(input, numVectors, dims, dataType);
     }
 
-    @Override
-    public CuVSMatrix fromInput(
-        MemorySegmentAccessInput input,
-        int numVectors,
-        int dims,
-        int rowStride,
-        int columnStride,
-        CuVSMatrix.DataType dataType
-    ) throws IOException {
-        if (numVectors < 0 || dims < 0) {
-            throwIllegalArgumentException(numVectors, dims);
+    private static CuVSMatrix createCuVSMatrix(MemorySegment ms, int numVectors, int dims, CuVSMatrix.DataType dataType) {
+        if (ms == null) {
+            throw new IllegalArgumentException("ms cannot be null");
         }
-        return createCuVSMatrix(input, 0L, input.length(), numVectors, dims, rowStride, columnStride, dataType);
-    }
-
-    @Override
-    public CuVSMatrix fromSlice(MemorySegmentAccessInput input, long pos, long len, int numVectors, int dims, CuVSMatrix.DataType dataType)
-        throws IOException {
-        if (pos < 0 || len < 0) {
-            throw new IllegalArgumentException("pos and len must be positive");
-        }
-        return createCuVSMatrix(input, pos, len, numVectors, dims, dataType);
-    }
-
-    private static CuVSMatrix createCuVSMatrix(
-        MemorySegmentAccessInput input,
-        long pos,
-        long len,
-        int numVectors,
-        int dims,
-        CuVSMatrix.DataType dataType
-    ) throws IOException {
-        MemorySegment ms = input.segmentSliceOrNull(pos, len);
-        assert ms != null; // TODO: this can be null if larger than 16GB or ...
         final int byteSize = dataType == CuVSMatrix.DataType.FLOAT ? Float.BYTES : Byte.BYTES;
         if (((long) numVectors * dims * byteSize) > ms.byteSize()) {
             throwIllegalArgumentException(ms, numVectors, dims);
         }
         return fromMemorySegment(ms, numVectors, dims, dataType);
-    }
-
-    private static CuVSMatrix createCuVSMatrix(
-        MemorySegmentAccessInput input,
-        long pos,
-        long len,
-        int numVectors,
-        int dims,
-        int rowStride,
-        int columnStride,
-        CuVSMatrix.DataType dataType
-    ) throws IOException {
-        MemorySegment ms = input.segmentSliceOrNull(pos, len);
-        assert ms != null;
-        final int byteSize = dataType == CuVSMatrix.DataType.FLOAT ? Float.BYTES : Byte.BYTES;
-        if (((long) numVectors * rowStride * byteSize) > ms.byteSize()) {
-            throwIllegalArgumentException(ms, numVectors, dims);
-        }
-        return fromMemorySegment(ms, numVectors, dims, rowStride, columnStride, dataType);
     }
 
     static void throwIllegalArgumentException(MemorySegment ms, int numVectors, int dims) {

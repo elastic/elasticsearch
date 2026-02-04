d9f0edeb7e51a9237622622c30ff4c8ee56b76d4
Mark Vieira <portugee@gmail.com>
2026-02-04 07:20:19 -0800

    Formatting: Keep braces for empty anonymous classes on a single line (#141683) (#141766)

--- server/src/test/java/org/elasticsearch/ingest/IngestServiceTests.java
+++ server/src/test/java/org/elasticsearch/ingest/IngestServiceTests.java
@@ -3295,8 +3295,7 @@ public class IngestServiceTests extends ESTestCase {
         });
         processors.put("remove", (factories, tag, description, config) -> {
             String field = (String) config.remove("field");
-            return new WrappingProcessorImpl("remove", tag, description, (ingestDocument -> ingestDocument.removeField(field))) {
-            };
+            return new WrappingProcessorImpl("remove", tag, description, (ingestDocument -> ingestDocument.removeField(field))) {};
         });
         return createWithProcessors(processors);
     }

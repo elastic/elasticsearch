4622160f0f3bf1ab1f1baac019d8ac6ed3f0ec2d
Ankita Kumar <ankita.kumar@elastic.co>
2026-02-04 17:35:19 -0500

    commit

--- server/src/main/java/org/elasticsearch/action/bulk/ShardBulkSplitHelper.java
+++ server/src/main/java/org/elasticsearch/action/bulk/ShardBulkSplitHelper.java
@@ -21,7 +21,6 @@ import org.elasticsearch.index.Index;
 import org.elasticsearch.index.shard.ShardId;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;

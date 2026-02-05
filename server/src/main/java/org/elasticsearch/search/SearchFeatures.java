ca55ee667f70f10dfa904d16b30f184514b75e5a
Jonas Kunz <jonas.kunz@elastic.co>
2026-02-05 15:05:25 +0100

    Implement boxplot for exponential histogram

--- server/src/main/java/org/elasticsearch/search/SearchFeatures.java
+++ server/src/main/java/org/elasticsearch/search/SearchFeatures.java
@@ -50,6 +50,9 @@ public final class SearchFeatures implements FeatureSpecification {
         "search.exponential_histogram_querydsl_percentile_ranks"
     );
     public static final NodeFeature CLOSING_INVALID_PIT_ID = new NodeFeature("closing_invalid_pit_id");
+    public static final NodeFeature EXPONENTIAL_HISTOGRAM_QUERYDSL_BOXPLOT = new NodeFeature(
+        "search.exponential_histogram_querydsl_boxplot"
+    );
 
     @Override
     public Set<NodeFeature> getTestFeatures() {
@@ -71,7 +74,8 @@ public final class SearchFeatures implements FeatureSpecification {
             EXPONENTIAL_HISTOGRAM_QUERYDSL_PERCENTILES,
             EXPONENTIAL_HISTOGRAM_QUERYDSL_PERCENTILE_RANKS,
             CLOSING_INVALID_PIT_ID,
-            FUNCTION_SCORE_NAMED_QUERIES
+            FUNCTION_SCORE_NAMED_QUERIES,
+            EXPONENTIAL_HISTOGRAM_QUERYDSL_BOXPLOT
         );
     }
 }

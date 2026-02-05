4e08760360a70bf479f1686ac4c5a75f979dc03c
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-02-05 15:56:38 +0000

    [CI] Auto commit changes from spotless

--- server/src/test/java/org/elasticsearch/search/internal/CancellableBulkScorerTests.java
+++ server/src/test/java/org/elasticsearch/search/internal/CancellableBulkScorerTests.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.lessThan;

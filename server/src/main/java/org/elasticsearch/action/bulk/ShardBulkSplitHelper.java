1450cbdb3726761e3aa6c014795cc86f9796f0bf
Ankita Kumar <ankita.kumar@elastic.co>
2026-02-05 11:39:26 -0500

    commit

--- server/src/main/java/org/elasticsearch/action/bulk/ShardBulkSplitHelper.java
+++ server/src/main/java/org/elasticsearch/action/bulk/ShardBulkSplitHelper.java
@@ -103,8 +103,7 @@ public final class ShardBulkSplitHelper {
         List<ReplicationResponse.ShardInfo.Failure> shardFailures = new ArrayList<>();
 
         Map<Integer, BulkItemResponse> itemResponsesById = new HashMap<>();
-
-        // responses.forEach((shardId, responseTuple) -> {
+        
         for (Map.Entry<ShardId, Tuple<BulkShardResponse, Exception>> entry : responses.entrySet()) {
             Tuple<BulkShardResponse, Exception> responseTuple = entry.getValue();
             ShardId shardId = entry.getKey();

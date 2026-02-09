398c2254de9e19056df4ba4a6d27ab337b747820
carlosdelest <carlos.delgado@elastic.co>
2026-02-09 18:34:33 +0100

    Fix tests

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/aggregate/SumTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/aggregate/SumTests.java
@@ -37,6 +37,10 @@ public class SumTests extends AbstractAggregationTestCase {
 
     @ParametersFactory
     public static Iterable<Object[]> parameters() {
+        return testParameters(true);
+    }
+
+    static Iterable<Object[]> testParameters(boolean includeDenseVector) {
         var suppliers = new ArrayList<TestCaseSupplier>();
 
         Stream.of(
@@ -79,24 +83,27 @@ public class SumTests extends AbstractAggregationTestCase {
                         DataType.DOUBLE,
                         equalTo(200.)
                     )
-                ),
-                new TestCaseSupplier(List.of(DataType.AGGREGATE_METRIC_DOUBLE), () -> {
-                    var value = new AggregateMetricDoubleBlockBuilder.AggregateMetricDoubleLiteral(
-                        randomDouble(),
-                        randomDouble(),
-                        randomDouble(),
-                        randomNonNegativeInt()
-                    );
-                    return new TestCaseSupplier.TestCase(
-                        List.of(TestCaseSupplier.TypedData.multiRow(List.of(value), DataType.AGGREGATE_METRIC_DOUBLE, "field")),
-                        standardAggregatorName("Sum", DataType.AGGREGATE_METRIC_DOUBLE),
-                        DataType.DOUBLE,
-                        equalTo(value.sum())
-                    );
-
-                })
+                )
             )
         );
+
+        if (includeDenseVector) {
+            suppliers.add(new TestCaseSupplier(List.of(DataType.AGGREGATE_METRIC_DOUBLE), () -> {
+                var value = new AggregateMetricDoubleBlockBuilder.AggregateMetricDoubleLiteral(
+                    randomDouble(),
+                    randomDouble(),
+                    randomDouble(),
+                    randomNonNegativeInt()
+                );
+                return new TestCaseSupplier.TestCase(
+                    List.of(TestCaseSupplier.TypedData.multiRow(List.of(value), DataType.AGGREGATE_METRIC_DOUBLE, "field")),
+                    standardAggregatorName("Sum", DataType.AGGREGATE_METRIC_DOUBLE),
+                    DataType.DOUBLE,
+                    equalTo(value.sum())
+                );
+
+            }));
+        }
 
         return parameterSuppliersFromTypedDataWithDefaultChecks(suppliers);
     }

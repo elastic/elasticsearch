24842f8d3e10f21e90e7dcedac2f5d75b30505b4
Ryan Ernst <ryan@iernst.net>
2026-02-10 12:39:42 -0800

    split into separate task

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/transport/GenerateInitialTransportVersionTask.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/transport/GenerateInitialTransportVersionTask.java
@@ -11,17 +11,13 @@ package org.elasticsearch.gradle.internal.transport;
 
 import org.elasticsearch.gradle.Version;
 import org.gradle.api.DefaultTask;
-import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.provider.Property;
 import org.gradle.api.services.ServiceReference;
 import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.options.Option;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public abstract class GenerateInitialTransportVersionTask extends DefaultTask {
@@ -29,62 +25,47 @@ public abstract class GenerateInitialTransportVersionTask extends DefaultTask {
     @ServiceReference("transportVersionResources")
     abstract Property<TransportVersionResourcesService> getResourceService();
 
-    @InputFile
-    public abstract RegularFileProperty getTransportVersionsFile();
-
     @Input
-    @Option(option = "release-version", description = "The current Elasticsearch version being released")
-    public abstract Property<String> getReleaseVersion();
-
-    @Input
-    @Option(option = "next-version", description = "The next Elasticsearch release version this transport version will be associated with")
-    public abstract Property<String> getNextVersion();
+    @Option(option = "stack-version", description = "The Elasticsearch version to generate an initial transport version for")
+    public abstract Property<String> getStackVersion();
 
     @Input
     abstract Property<Version> getCurrentVersion();
 
     @TaskAction
     public void run() throws IOException {
-        Version releaseVersion = Version.fromString(getReleaseVersion().get());
-        Version nextVersion = Version.fromString(getNextVersion().get());
-        String baseUpperBoundName = getUpperBoundName(releaseVersion);
+        Version stackVersion = Version.fromString(getStackVersion().get());
+        String baseUpperBoundName = getUpperBoundName(stackVersion);
         TransportVersionResourcesService resources = getResourceService().get();
         TransportVersionUpperBound baseUpperBound = resources.getUpperBoundFromGitBase(baseUpperBoundName);
-        String initialDefinitionName = "initial_" + nextVersion;
+        String initialDefinitionName = "initial_" + stackVersion;
         TransportVersionDefinition existingDefinition = resources.getUnreferableDefinitionFromGitBase(initialDefinitionName);
 
         // This task runs on main and release branches. In release branches we will generate the exact same
         // upper bound result because we always look at the base branch (ie upstream/main).
         if (existingDefinition == null) {
             if (baseUpperBound == null) {
-                throw new RuntimeException("Missing upper bound " + baseUpperBoundName + " for release version " + releaseVersion);
+                throw new RuntimeException("Missing upper bound " + baseUpperBoundName + " for release version " + stackVersion);
             }
 
-            // Record the current transport version for the release version before creating the new one
             int currentTransportVersionId = baseUpperBound.definitionId().complete();
-            addTransportVersionRecord(releaseVersion, currentTransportVersionId);
 
             // minors increment by 1000 to create a unique base, patches increment by 1 as other patches do
-            int increment = nextVersion.getRevision() == 0 ? 1000 : 1;
+            int increment = stackVersion.getRevision() == 0 ? 1000 : 1;
             var id = TransportVersionId.fromInt(currentTransportVersionId + increment);
             var definition = new TransportVersionDefinition(initialDefinitionName, List.of(id), false);
             resources.writeDefinition(definition);
-            String upperBoundName = getUpperBoundName(nextVersion);
+            String upperBoundName = getUpperBoundName(stackVersion);
             var newUpperBound = new TransportVersionUpperBound(upperBoundName, initialDefinitionName, id);
             resources.writeUpperBound(newUpperBound);
 
-            if (nextVersion.getRevision() == 0) {
+            if (stackVersion.getRevision() == 0) {
                 Version currentVersion = getCurrentVersion().get();
                 String currentUpperBoundName = getUpperBoundName(currentVersion);
                 var currentUpperBound = new TransportVersionUpperBound(currentUpperBoundName, initialDefinitionName, id);
                 resources.writeUpperBound(currentUpperBound);
             }
         }
-    }
-
-    private void addTransportVersionRecord(Version releaseVersion, int transportVersionId) throws IOException {
-        String newEntry = releaseVersion + "," + transportVersionId + "\n";
-        Files.writeString(getTransportVersionsFile().getAsFile().get().toPath(), newEntry, StandardOpenOption.APPEND);
     }
 
     private String getUpperBoundName(Version version) {

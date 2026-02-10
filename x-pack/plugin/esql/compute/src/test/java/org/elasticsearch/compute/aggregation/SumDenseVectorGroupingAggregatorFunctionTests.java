4594d120318645014655f3828c35cd9fecf7221d
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-02-10 17:30:25 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/esql/compute/src/test/java/org/elasticsearch/compute/aggregation/SumDenseVectorGroupingAggregatorFunctionTests.java
+++ x-pack/plugin/esql/compute/src/test/java/org/elasticsearch/compute/aggregation/SumDenseVectorGroupingAggregatorFunctionTests.java
@@ -13,8 +13,8 @@ import org.elasticsearch.compute.data.ElementType;
 import org.elasticsearch.compute.data.FloatBlock;
 import org.elasticsearch.compute.data.LongBlock;
 import org.elasticsearch.compute.data.Page;
-import org.elasticsearch.compute.test.operator.blocksource.LongFloatArrayTupleBlockSourceOperator;
 import org.elasticsearch.compute.operator.SourceOperator;
+import org.elasticsearch.compute.test.operator.blocksource.LongFloatArrayTupleBlockSourceOperator;
 import org.elasticsearch.core.Tuple;
 
 import java.util.ArrayList;

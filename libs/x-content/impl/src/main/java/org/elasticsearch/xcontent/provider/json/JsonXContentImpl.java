6db8667e6a02a4f6a962df1b9f2ae5adc66a7d5c
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-02-10 22:38:11 +0000

    [CI] Auto commit changes from spotless

--- libs/x-content/impl/src/main/java/org/elasticsearch/xcontent/provider/json/JsonXContentImpl.java
+++ libs/x-content/impl/src/main/java/org/elasticsearch/xcontent/provider/json/JsonXContentImpl.java
@@ -13,7 +13,6 @@ import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
-
 import com.fasterxml.jackson.core.json.JsonWriteFeature;
 
 import org.elasticsearch.xcontent.XContent;

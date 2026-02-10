da7bf65cc9dd54d8c4444f06494039c956823707
Moritz Mack <mmack@apache.org>
2026-02-10 09:03:04 +0100

    Update exports for stateless-sigterm

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -306,7 +306,7 @@ module org.elasticsearch.server {
     exports org.elasticsearch.monitor.os;
     exports org.elasticsearch.monitor.process;
     exports org.elasticsearch.node;
-    exports org.elasticsearch.node.internal to org.elasticsearch.internal.sigterm;
+    exports org.elasticsearch.node.internal to org.elasticsearch.xpack.stateless.sigterm;
     exports org.elasticsearch.persistent;
     exports org.elasticsearch.persistent.decider;
     exports org.elasticsearch.plugins;

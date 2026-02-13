cf1c18155db15b05be3dfa16e00f8feffd387315
Moritz Mack <mmack@apache.org>
2026-02-13 10:38:07 +0100

    fix exports

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -514,6 +514,6 @@ module org.elasticsearch.server {
     exports org.elasticsearch.index.mapper.blockloader;
     exports org.elasticsearch.index.mapper.blockloader.docvalues;
     exports org.elasticsearch.index.mapper.blockloader.docvalues.fn;
-    exports org.elasticsearch.readiness to org.elasticsearch.internal.sigterm;
+    exports org.elasticsearch.readiness to org.elasticsearch.xpack.stateless.sigterm;
     exports org.elasticsearch.search.diversification.mmr;
 }

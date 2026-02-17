73c44ff2554a2d41aa65e911aebab22d77cbf564
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2026-02-17 09:44:58 -0500

    iter

--- libs/simdvec/src/main21/java/org/elasticsearch/simdvec/internal/vectorization/PanamaESVectorizationProvider.java
+++ libs/simdvec/src/main21/java/org/elasticsearch/simdvec/internal/vectorization/PanamaESVectorizationProvider.java
@@ -43,15 +43,15 @@ final class PanamaESVectorizationProvider extends ESVectorizationProvider {
         int dataLength,
         int bulkSize
     ) throws IOException {
-        input = FilterIndexInput.unwrapOnlyTest(input);
-        input = MemorySegmentAccessInputAccess.unwrap(input);
+        IndexInput unwrappedInput = FilterIndexInput.unwrapOnlyTest(input);
+        unwrappedInput = MemorySegmentAccessInputAccess.unwrap(unwrappedInput);
         if (PanamaESVectorUtilSupport.HAS_FAST_INTEGER_VECTORS
-            && input instanceof MemorySegmentAccessInput msai
+            && unwrappedInput instanceof MemorySegmentAccessInput msai
             && queryBits == 4
             && (indexBits == 1 || indexBits == 2 || indexBits == 4)) {
-            MemorySegment ms = msai.segmentSliceOrNull(0, input.length());
+            MemorySegment ms = msai.segmentSliceOrNull(0, unwrappedInput.length());
             if (ms != null) {
-                return new MemorySegmentESNextOSQVectorsScorer(input, queryBits, indexBits, dimension, dataLength, bulkSize, ms);
+                return new MemorySegmentESNextOSQVectorsScorer(unwrappedInput, queryBits, indexBits, dimension, dataLength, bulkSize, ms);
             }
         }
         return new ESNextOSQVectorsScorer(input, queryBits, indexBits, dimension, dataLength, bulkSize);
@@ -59,12 +59,12 @@ final class PanamaESVectorizationProvider extends ESVectorizationProvider {
 
     @Override
     public ES91OSQVectorsScorer newES91OSQVectorsScorer(IndexInput input, int dimension, int bulkSize) throws IOException {
-        input = FilterIndexInput.unwrapOnlyTest(input);
-        input = MemorySegmentAccessInputAccess.unwrap(input);
-        if (PanamaESVectorUtilSupport.HAS_FAST_INTEGER_VECTORS && input instanceof MemorySegmentAccessInput msai) {
-            MemorySegment ms = msai.segmentSliceOrNull(0, input.length());
+        IndexInput unwrappedInput = FilterIndexInput.unwrapOnlyTest(input);
+        unwrappedInput = MemorySegmentAccessInputAccess.unwrap(unwrappedInput);
+        if (PanamaESVectorUtilSupport.HAS_FAST_INTEGER_VECTORS && unwrappedInput instanceof MemorySegmentAccessInput msai) {
+            MemorySegment ms = msai.segmentSliceOrNull(0, unwrappedInput.length());
             if (ms != null) {
-                return new MemorySegmentES91OSQVectorsScorer(input, dimension, bulkSize, ms);
+                return new MemorySegmentES91OSQVectorsScorer(unwrappedInput, dimension, bulkSize, ms);
             }
         }
         return new OnHeapES91OSQVectorsScorer(input, dimension, bulkSize);
@@ -72,12 +72,12 @@ final class PanamaESVectorizationProvider extends ESVectorizationProvider {
 
     @Override
     public ES92Int7VectorsScorer newES92Int7VectorsScorer(IndexInput input, int dimension, int bulkSize) throws IOException {
-        input = FilterIndexInput.unwrapOnlyTest(input);
-        input = MemorySegmentAccessInputAccess.unwrap(input);
-        if (input instanceof MemorySegmentAccessInput msai) {
-            MemorySegment ms = msai.segmentSliceOrNull(0, input.length());
+        IndexInput unwrappedInput = FilterIndexInput.unwrapOnlyTest(input);
+        unwrappedInput = MemorySegmentAccessInputAccess.unwrap(unwrappedInput);
+        if (unwrappedInput instanceof MemorySegmentAccessInput msai) {
+            MemorySegment ms = msai.segmentSliceOrNull(0, unwrappedInput.length());
             if (ms != null) {
-                return new MemorySegmentES92Int7VectorsScorer(input, dimension, bulkSize, ms);
+                return new MemorySegmentES92Int7VectorsScorer(unwrappedInput, dimension, bulkSize, ms);
             }
         }
         return new ES92Int7VectorsScorer(input, dimension, bulkSize);

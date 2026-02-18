c0cbf45f07442778fe75dce750b2ffb5dfed004a
ChrisHegarty <chegar999@gmail.com>
2026-02-18 10:55:40 +0000

    remove transitive

--- x-pack/plugin/blob-cache/src/main/java/module-info.java
+++ x-pack/plugin/blob-cache/src/main/java/module-info.java
@@ -8,7 +8,7 @@
 module org.elasticsearch.blobcache {
     requires org.elasticsearch.base;
     requires org.elasticsearch.server;
-    requires transitive org.elasticsearch.nativeaccess;
+    requires org.elasticsearch.nativeaccess;
 
     requires org.apache.logging.log4j;
     requires org.apache.lucene.core;

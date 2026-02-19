516fa5cbbaac2aa18eaa1bd30b6eae567aa66884
Alan Woodward <romseygeek@apache.org>
2026-02-19 10:16:13 +0000

    Simplify handling of empty documents against empty mappings

--- server/src/main/java/org/elasticsearch/index/mapper/ParsedDocument.java
+++ server/src/main/java/org/elasticsearch/index/mapper/ParsedDocument.java
@@ -17,7 +17,7 @@ import org.apache.lucene.document.StoredField;
 import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;
-import org.elasticsearch.index.mapper.MapperService.MergeReason;
+
 import org.elasticsearch.plugins.internal.XContentMeteringParserDecorator;
 import org.elasticsearch.xcontent.XContentType;
 
@@ -213,11 +213,6 @@ public class ParsedDocument {
     public void addDynamicMappingsUpdate(Mapping update) {
         if (dynamicMappingsUpdate == null) {
             dynamicMappingsUpdate = update;
-        } else {
-            MappingBuilder existingBuilder = MappingBuilder.fromMapping(dynamicMappingsUpdate);
-            MappingBuilder incomingBuilder = MappingBuilder.fromMapping(update);
-            existingBuilder.merge(incomingBuilder, MergeReason.MAPPING_AUTO_UPDATE, Long.MAX_VALUE);
-            dynamicMappingsUpdate = existingBuilder.build(MergeReason.MAPPING_AUTO_UPDATE);
         }
     }
 

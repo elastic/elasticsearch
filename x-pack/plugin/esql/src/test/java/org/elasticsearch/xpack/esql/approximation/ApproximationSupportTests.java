319a6d2b550f671d23a2a390fb0f8c23132e4bf7
Bogdan Pintea <bogdan.pintea@elastic.co>
2026-02-19 18:41:22 +0100

    ESQL: Reapply "Introduce pluggable external datasource framework (#141678)"

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/approximation/ApproximationSupportTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/approximation/ApproximationSupportTests.java
@@ -56,6 +56,7 @@ import org.elasticsearch.xpack.esql.plan.logical.BinaryPlan;
 import org.elasticsearch.xpack.esql.plan.logical.CompoundOutputEval;
 import org.elasticsearch.xpack.esql.plan.logical.Drop;
 import org.elasticsearch.xpack.esql.plan.logical.Explain;
+import org.elasticsearch.xpack.esql.plan.logical.ExternalRelation;
 import org.elasticsearch.xpack.esql.plan.logical.Fork;
 import org.elasticsearch.xpack.esql.plan.logical.InlineStats;
 import org.elasticsearch.xpack.esql.plan.logical.Keep;
@@ -69,6 +70,7 @@ import org.elasticsearch.xpack.esql.plan.logical.Subquery;
 import org.elasticsearch.xpack.esql.plan.logical.TimeSeriesAggregate;
 import org.elasticsearch.xpack.esql.plan.logical.UnaryPlan;
 import org.elasticsearch.xpack.esql.plan.logical.UnionAll;
+import org.elasticsearch.xpack.esql.plan.logical.UnresolvedExternalRelation;
 import org.elasticsearch.xpack.esql.plan.logical.UnresolvedRelation;
 import org.elasticsearch.xpack.esql.plan.logical.fuse.Fuse;
 import org.elasticsearch.xpack.esql.plan.logical.fuse.FuseScoreEval;
@@ -149,6 +151,7 @@ public class ApproximationSupportTests extends ESTestCase {
         Explain.class,
         ShowInfo.class,
         LocalRelation.class,
+        ExternalRelation.class,
         MetricsInfo.class,
 
         // The plans are superclasses of other plans.
@@ -161,6 +164,7 @@ public class ApproximationSupportTests extends ESTestCase {
 
         // These plans don't occur in a correct analyzed query.
         UnresolvedRelation.class,
+        UnresolvedExternalRelation.class,
         StubRelation.class,
         Drop.class,
         Keep.class,

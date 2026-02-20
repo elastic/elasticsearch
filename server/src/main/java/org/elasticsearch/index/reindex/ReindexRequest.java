ae58291f67dce397305e37dbcf6f1425d546f595
Pete Gillin <pete.gillin@elastic.co>
2026-02-20 17:25:46 +0000

    another TODO

--- server/src/main/java/org/elasticsearch/index/reindex/ReindexRequest.java
+++ server/src/main/java/org/elasticsearch/index/reindex/ReindexRequest.java
@@ -123,6 +123,7 @@ public class ReindexRequest extends AbstractBulkIndexByScrollRequest<ReindexRequ
             // - Let the reindex resilience work (relocation and PIT) merge first
             // - Check whether anything needs to be updated because of relocation
             // - Check whether anything needs to be updated because of PIT (e.g. whether we can use PIT with remote in the same way)
+            // - Fix ReindexResumeIT
             // - Consider additional integration tests e.g. ReindexFromOldRemoteIT
             // - Think about whether it's worthwhile to support ?slices=auto with a stateful remote
             if (getSlices() == AbstractBulkByScrollRequest.AUTO_SLICES) {

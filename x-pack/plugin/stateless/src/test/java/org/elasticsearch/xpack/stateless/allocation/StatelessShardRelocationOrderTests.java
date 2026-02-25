bde1337645d22d1d75222c17c4c3261578769d33
Pooya Salehi <pxsalehi@users.noreply.github.com>
2026-02-25 11:16:36 +0100

    Interleave shards with potential write load during necessary relocations (#5613)
    https://github.com/elastic/elasticsearch-serverless/pull/5431 has been
    pretty effective on the
    [cases](https://github.com/elastic/serverless-gitops/pull/59941) I've
    tried out. To enable it by default, we need to address the possibility
    that we end up with write shards at the tail of the relocation order and
    risk write unavailability since we do still occasionally hit the 60m
    timeout for node shutdown. This PRs makes sure we have a heuristic to
    address this, and enables `prioritize_write_shard_necessary_relocations`
    by default.
    
    It divides the shards into: 
    
    1. Shards to spread: data stream write shards or normal shards with non zero write load. 
    2. Data stream read shards.
    3. Normal shards with no write load. 
    
    Keeps half of 2 for the end of the returned list to make sure when
    possible we still move shards with writes with _some_ priority and they
    won't be the last. Spreads 1 across the rest of 2, and 3 for the
    beginning of the returned order.
    
    Assisted by Cursor.
    
    Closes

--- x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/allocation/StatelessShardRelocationOrderTests.java
+++ x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/allocation/StatelessShardRelocationOrderTests.java
@@ -17,26 +17,47 @@
 
 package org.elasticsearch.xpack.stateless.allocation;
 
+import org.elasticsearch.cluster.ClusterInfo;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.DataStream;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.cluster.metadata.Metadata;
+import org.elasticsearch.cluster.metadata.ProjectMetadata;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.cluster.routing.GlobalRoutingTableTestHelper;
+import org.elasticsearch.cluster.routing.IndexRoutingTable;
 import org.elasticsearch.cluster.routing.RoutingTable;
+import org.elasticsearch.cluster.routing.ShardRouting;
+import org.elasticsearch.cluster.routing.allocation.RoutingAllocation;
 import org.elasticsearch.cluster.routing.allocation.allocator.NodeAllocationOrdering;
 import org.elasticsearch.cluster.routing.allocation.allocator.OrderedShardsIterator;
 import org.elasticsearch.cluster.routing.allocation.allocator.OrderedShardsIteratorTests;
 import org.elasticsearch.cluster.routing.allocation.allocator.ShardRelocationOrder;
+import org.elasticsearch.cluster.routing.allocation.decider.AllocationDeciders;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.set.Sets;
+import org.elasticsearch.core.Strings;
+import org.elasticsearch.index.Index;
 import org.elasticsearch.index.IndexVersion;
+import org.elasticsearch.index.shard.ShardId;
+import org.elasticsearch.snapshots.SnapshotShardSizeInfo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static org.elasticsearch.cluster.routing.ShardRoutingState.STARTED;
+import static org.elasticsearch.cluster.routing.TestShardRouting.newShardRouting;
 import static org.elasticsearch.common.settings.ClusterSettings.BUILT_IN_CLUSTER_SETTINGS;
 import static org.elasticsearch.xpack.stateless.allocation.StatelessShardRelocationOrder.PRIORITIZE_WRITE_SHARD_RELOCATIONS_SETTING;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
 
 public class StatelessShardRelocationOrderTests extends OrderedShardsIteratorTests {
 
@@ -46,7 +67,7 @@ public class StatelessShardRelocationOrderTests extends OrderedShardsIteratorTes
         return new StatelessShardRelocationOrder(getClusterSettings(true));
     }
 
-    public void testNoWriteShardRelocationPriority() {
+    public void testOrderBalancingMoves() {
         ShardRelocationOrder relocationOrder = new StatelessShardRelocationOrder(getClusterSettings(false));
         var nodes = DiscoveryNodes.builder().add(newNode("node-1")).add(newNode("node-2")).build();
 
@@ -59,28 +80,18 @@ public class StatelessShardRelocationOrderTests extends OrderedShardsIteratorTes
             .build();
 
         var metadata = Metadata.builder()
-            .put(lookup, false)
-            .put(ds1, false)
-            .put(ds2, false)
-            .put(DataStream.builder("data-stream", List.of(ds1.getIndex(), ds2.getIndex())).build())
-            .build();
-
-        var routing = RoutingTable.builder()
-            .add(index(lookup.getIndex(), "node-1"))
-            .add(index(ds1.getIndex(), "node-1"))
-            .add(index(ds2.getIndex(), "node-1"))
-            .build();
-
-        // when performing necessary moves (such as preparation for the node shutdown) no prioritization should be applied
-        var routingAllocation = createRoutingAllocation(nodes, metadata, routing);
-        var shards = routingAllocation.routingNodes().node("node-1").copyShards();
-        assertThat(
-            next(3, OrderedShardsIterator.createForNecessaryMoves(routingAllocation, new NodeAllocationOrdering(), relocationOrder)),
-            contains(
-                isIndexShardAt(shards[0].getIndexName(), "node-1"),
-                isIndexShardAt(shards[1].getIndexName(), "node-1"),
-                isIndexShardAt(shards[2].getIndexName(), "node-1")
+            .put(
+                ProjectMetadata.builder(Metadata.DEFAULT_PROJECT_ID)
+                    .put(lookup, false)
+                    .put(ds1, false)
+                    .put(ds2, false)
+                    .put(DataStream.builder("data-stream", List.of(ds1.getIndex(), ds2.getIndex())).build())
             )
+            .build();
+
+        var routing = GlobalRoutingTableTestHelper.buildRoutingTable(
+            metadata,
+            (builder, index) -> builder.add(index(index.getIndex(), "node-1"))
         );
 
         // when performing rebalancing write shards should be moved last
@@ -88,7 +99,13 @@ public class StatelessShardRelocationOrderTests extends OrderedShardsIteratorTes
             next(
                 3,
                 OrderedShardsIterator.createForBalancing(
-                    createRoutingAllocation(nodes, metadata, routing),
+                    new RoutingAllocation(
+                        new AllocationDeciders(List.of()),
+                        ClusterState.builder(ClusterName.DEFAULT).nodes(nodes).metadata(metadata).routingTable(routing).build(),
+                        ClusterInfo.EMPTY,
+                        SnapshotShardSizeInfo.EMPTY,
+                        0
+                    ),
                     new NodeAllocationOrdering(),
                     relocationOrder
                 )
@@ -99,6 +116,116 @@ public class StatelessShardRelocationOrderTests extends OrderedShardsIteratorTes
                 isIndexShardAt(".ds-data-stream-2024.04.18-000002", "node-1")
             )
         );
+    }
+
+    public void testOrderNecessaryMovesWithInterleaving() {
+        var nodes = DiscoveryNodes.builder().add(newNode("node-1")).add(newNode("node-2")).build();
+
+        int numDataStreams = randomIntBetween(0, 20);
+        int numNormalIndices = randomIntBetween(0, 20);
+
+        var projectBuilder = ProjectMetadata.builder(Metadata.DEFAULT_PROJECT_ID);
+        var routingTableBuilder = RoutingTable.builder();
+        var writeLoads = new HashMap<ShardId, Double>();
+        var dsReadShardIds = new HashSet<ShardId>();
+        var shardsToSpread = new HashSet<ShardId>();
+
+        // Create data streams, each with 1 write shard (last index) and 0-20 read shards
+        for (int i = 0; i < numDataStreams; i++) {
+            String dsName = "data-stream-" + randomIdentifier() + "-" + i;
+            int numReadShards = randomIntBetween(0, 20);
+            List<Index> dsIndices = new ArrayList<>();
+
+            // Create read shard backing indices
+            for (int j = 0; j < numReadShards; j++) {
+                String indexName = Strings.format(".ds-%s-2024.04.18-%06d", dsName, j + 1);
+                IndexMetadata im = IndexMetadata.builder(indexName).settings(indexSettings(IndexVersion.current(), 1, 0)).build();
+                projectBuilder.put(im, false);
+                dsIndices.add(im.getIndex());
+                routingTableBuilder.add(index(im.getIndex(), "node-1"));
+
+                ShardId shardId = new ShardId(im.getIndex(), 0);
+                writeLoads.put(shardId, randomWriteLoad());
+                dsReadShardIds.add(shardId);
+            }
+
+            // Create write shard backing index (last in the list = write index)
+            String writeIndexName = Strings.format(".ds-%s-2024.04.18-%06d", dsName, numReadShards + 1);
+            IndexMetadata writeIm = IndexMetadata.builder(writeIndexName).settings(indexSettings(IndexVersion.current(), 1, 0)).build();
+            projectBuilder.put(writeIm, false);
+            dsIndices.add(writeIm.getIndex());
+            routingTableBuilder.add(index(writeIm.getIndex(), "node-1"));
+
+            ShardId writeShardId = new ShardId(writeIm.getIndex(), 0);
+            writeLoads.put(writeShardId, randomWriteLoad());
+            shardsToSpread.add(writeShardId);
+
+            projectBuilder.put(DataStream.builder(dsName, dsIndices).build());
+        }
+
+        // Create normal indices, each with 1-5 shards
+        for (int i = 0; i < numNormalIndices; i++) {
+            int numShards = randomIntBetween(1, 5);
+            String indexName = randomIndexName() + "-" + i;
+            IndexMetadata im = IndexMetadata.builder(indexName).settings(indexSettings(IndexVersion.current(), numShards, 0)).build();
+            projectBuilder.put(im, false);
+
+            var irtBuilder = IndexRoutingTable.builder(im.getIndex());
+            for (int s = 0; s < numShards; s++) {
+                ShardId shardId = new ShardId(im.getIndex(), s);
+                irtBuilder.addShard(newShardRouting(shardId, "node-1", true, STARTED));
+                double writeLoad = randomWriteLoad();
+                writeLoads.put(shardId, writeLoad);
+                if (writeLoad > 0.0) {
+                    shardsToSpread.add(shardId);
+                }
+            }
+            routingTableBuilder.add(irtBuilder.build());
+        }
+
+        var metadata = Metadata.builder().put(projectBuilder).build();
+        var routing = GlobalRoutingTableTestHelper.routingTable(Metadata.DEFAULT_PROJECT_ID, routingTableBuilder);
+        var clusterInfo = ClusterInfo.builder().shardWriteLoads(writeLoads).build();
+
+        var allocation = new RoutingAllocation(
+            new AllocationDeciders(List.of()),
+            ClusterState.builder(ClusterName.DEFAULT).nodes(nodes).metadata(metadata).routingTable(routing).build(),
+            clusterInfo,
+            SnapshotShardSizeInfo.EMPTY,
+            0
+        );
+
+        int totalShards = writeLoads.size();
+        ShardRelocationOrder relocationOrder = new StatelessShardRelocationOrder(getClusterSettings(false));
+        var iterator = OrderedShardsIterator.createForNecessaryMoves(allocation, new NodeAllocationOrdering(), relocationOrder);
+        var orderedShards = next(totalShards, iterator);
+
+        // 1. No duplicates
+        assertThat("no duplicate shards in ordering", orderedShards.size(), equalTo(new HashSet<>(orderedShards).size()));
+
+        // 2. All created shards are present (order doesn't matter)
+        assertThat(orderedShards.size(), equalTo(writeLoads.size()));
+        assertThat(orderedShards, containsInAnyOrder(allocation.routingNodes().node("node-1").copyShards()));
+
+        // 3. Tail of the ordered list should consist entirely of DS read shards (the "second half")
+        int expectedTailSize = Math.ceilDiv(dsReadShardIds.size(), 2); // ceil(N/2) — addToSecondHalf starts true
+        for (int i = orderedShards.size() - 1; i >= orderedShards.size() - expectedTailSize; i--) {
+            assertTrue("tail should contain half of the read shards", dsReadShardIds.contains(orderedShards.get(i).shardId()));
+        }
+
+        // 4. Make sure the shards with potential write are spread out in the head (not all at the top).
+        List<ShardRouting> head = orderedShards.subList(0, orderedShards.size() - expectedTailSize);
+        int n = shardsToSpread.size();
+        // Only check when there are enough spread shards to observe ordering (>=2) and
+        // the head contains other shards too (n < head.size), otherwise there is nothing to interleave with.
+        if (n >= 2 && n < head.size()) {
+            long spreadInTopN = head.subList(0, n).stream().filter(s -> shardsToSpread.contains(s.shardId())).count();
+            assertThat("write shards should not all be clustered at the start", spreadInTopN, lessThan((long) n));
+        }
+    }
+
+    private double randomWriteLoad() {
+        return randomBoolean() ? randomDoubleBetween(0.01, 10.0, true) : 0.0;
     }
 
     private ClusterSettings getClusterSettings(boolean prioritizeWriteShardRelocation) {

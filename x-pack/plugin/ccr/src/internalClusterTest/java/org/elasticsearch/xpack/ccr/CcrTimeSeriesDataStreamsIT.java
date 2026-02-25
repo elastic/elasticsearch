6f21cc8cae8f469fe45ecdcc9617139629fd58c5
Luke Whiting <luke.whiting@elastic.co>
2026-02-25 14:24:34 +0000

    Only set index.mapping.synthetic_id in CCR TSDB test when feature flag is on
    The test always put index.mapping.synthetic_id in the data stream template.
    When the TSDB synthetic ID feature flag is off (e.g. in CI), that setting is
    not registered in IndexScopedSettings, so template validation hits an
    assertion in validatePrivateSettingsNotExplicitlySet and the master task
    fails without notifying the client, causing the test to hang until suite
    timeout. Only add the setting to the template when the feature flag is
    enabled.

--- x-pack/plugin/ccr/src/internalClusterTest/java/org/elasticsearch/xpack/ccr/CcrTimeSeriesDataStreamsIT.java
+++ x-pack/plugin/ccr/src/internalClusterTest/java/org/elasticsearch/xpack/ccr/CcrTimeSeriesDataStreamsIT.java
@@ -276,17 +276,19 @@ public class CcrTimeSeriesDataStreamsIT extends CcrIntegTestCase {
 
     private static void putDataStreamTemplate(Client client, String dataStreamName, int primaries, int replicas, boolean useSyntheticId)
         throws IOException {
+        var settingsBuilder = indexSettings(primaries, replicas).put(IndexSettings.MODE.getKey(), IndexMode.TIME_SERIES.getName())
+            .put(IndexSettings.INDEX_REFRESH_INTERVAL_SETTING.getKey(), -1)
+            .put(IndexSettings.RECOVERY_USE_SYNTHETIC_SOURCE_SETTING.getKey(), randomBoolean());
+        if (IndexSettings.TSDB_SYNTHETIC_ID_FEATURE_FLAG) {
+            settingsBuilder.put(IndexSettings.SYNTHETIC_ID.getKey(), useSyntheticId);
+        }
         var putTemplateRequest = new TransportPutComposableIndexTemplateAction.Request(getTestClass().getName().toLowerCase(Locale.ROOT))
             .indexTemplate(
                 ComposableIndexTemplate.builder()
                     .indexPatterns(List.of(dataStreamName))
                     .template(
                         new Template(
-                            indexSettings(primaries, replicas).put(IndexSettings.MODE.getKey(), IndexMode.TIME_SERIES.getName())
-                                .put(IndexSettings.INDEX_REFRESH_INTERVAL_SETTING.getKey(), -1)
-                                .put(IndexSettings.RECOVERY_USE_SYNTHETIC_SOURCE_SETTING.getKey(), randomBoolean())
-                                .put(IndexSettings.SYNTHETIC_ID.getKey(), useSyntheticId)
-                                .build(),
+                            settingsBuilder.build(),
                             new CompressedXContent("""
                                 {
                                     "_doc": {

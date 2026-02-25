f3de3c6bd7b834afde99b112b5ead2f4a886dc61
Parker Timmins <parker.timmins@elastic.co>
2026-02-25 13:46:50 -0600

    Fix flaky multi-segment PatternText tests
    Use NoMergePolicy with a direct IndexWriter instead of
    withLuceneIndex (which uses RandomIndexWriter that can
    randomly merge segments), ensuring tests that require
    separate segments per document are deterministic.

--- x-pack/plugin/logsdb/src/test/java/org/elasticsearch/xpack/logsdb/patterntext/PatternTextFieldMapperTests.java
+++ x-pack/plugin/logsdb/src/test/java/org/elasticsearch/xpack/logsdb/patterntext/PatternTextFieldMapperTests.java
@@ -11,8 +11,11 @@ import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.DocValuesType;
 import org.apache.lucene.index.IndexOptions;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.index.IndexableFieldType;
+import org.apache.lucene.index.NoMergePolicy;
 import org.apache.lucene.queries.intervals.IntervalQuery;
 import org.apache.lucene.queries.intervals.IntervalsSource;
 import org.apache.lucene.search.FieldExistsQuery;
@@ -499,58 +502,57 @@ public class PatternTextFieldMapperTests extends MapperTestCase {
         MapperService mapperService = createMapperService(fieldMapping(b -> b.field("type", "pattern_text")));
         MappedFieldType ft = mapperService.fieldType("field");
 
-        withLuceneIndex(mapperService, iw -> {
-            LuceneDocument doc1 = mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc();
-            iw.addDocument(doc1);
-            iw.commit();
+        try (Directory dir = newDirectory()) {
+            indexDocPerSegment(
+                dir,
+                mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc(),
+                mapperService.documentMapper().parse(source(b -> {})).rootDoc()
+            );
+            try (DirectoryReader reader = DirectoryReader.open(dir)) {
+                assertEquals(2, reader.leaves().size());
 
-            LuceneDocument doc2 = mapperService.documentMapper().parse(source(b -> {})).rootDoc();
-            iw.addDocument(doc2);
-            iw.commit();
-        }, reader -> {
-            assertEquals(2, reader.leaves().size());
+                SearchExecutionContext ctx = createSearchExecutionContext(mapperService, newSearcher(reader));
+                ValueFetcher fetcher = ft.valueFetcher(ctx, null);
 
-            SearchExecutionContext ctx = createSearchExecutionContext(mapperService, newSearcher(reader));
-            ValueFetcher fetcher = ft.valueFetcher(ctx, null);
+                fetcher.setNextReader(reader.leaves().get(0));
+                List<Object> values = fetcher.fetchValues(null, 0, new ArrayList<>());
+                assertEquals(1, values.size());
+                assertEquals("abc 123", values.get(0));
 
-            fetcher.setNextReader(reader.leaves().get(0));
-            List<Object> values = fetcher.fetchValues(null, 0, new ArrayList<>());
-            assertEquals(1, values.size());
-            assertEquals("abc 123", values.get(0));
-
-            fetcher.setNextReader(reader.leaves().get(1));
-            List<Object> emptyValues = fetcher.fetchValues(null, 0, new ArrayList<>());
-            assertEquals(0, emptyValues.size());
-        });
+                fetcher.setNextReader(reader.leaves().get(1));
+                List<Object> emptyValues = fetcher.fetchValues(null, 0, new ArrayList<>());
+                assertEquals(0, emptyValues.size());
+            }
+        }
     }
 
     public void testFieldDataWithMissingFieldSegment() throws IOException {
         MapperService mapperService = createMapperService(fieldMapping(b -> b.field("type", "pattern_text")));
         MappedFieldType ft = mapperService.fieldType("field");
 
-        withLuceneIndex(mapperService, iw -> {
-            LuceneDocument doc1 = mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc();
-            iw.addDocument(doc1);
-            iw.commit();
+        try (Directory dir = newDirectory()) {
+            indexDocPerSegment(
+                dir,
+                mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc(),
+                mapperService.documentMapper().parse(source(b -> {})).rootDoc()
+            );
+            try (DirectoryReader reader = DirectoryReader.open(dir)) {
+                assertEquals(2, reader.leaves().size());
 
-            LuceneDocument doc2 = mapperService.documentMapper().parse(source(b -> {})).rootDoc();
-            iw.addDocument(doc2);
-            iw.commit();
-        }, reader -> {
-            assertEquals(2, reader.leaves().size());
+                var fieldDataContext = new FieldDataContext("", null, () -> null, Set::of, MappedFieldType.FielddataOperation.SCRIPT);
+                var fieldData = ft.fielddataBuilder(fieldDataContext)
+                    .build(new IndexFieldDataCache.None(), new NoneCircuitBreakerService());
 
-            var fieldDataContext = new FieldDataContext("", null, () -> null, Set::of, MappedFieldType.FielddataOperation.SCRIPT);
-            var fieldData = ft.fielddataBuilder(fieldDataContext).build(new IndexFieldDataCache.None(), new NoneCircuitBreakerService());
+                var leafData0 = fieldData.load(reader.leaves().get(0));
+                var bytesValues0 = leafData0.getBytesValues();
+                assertTrue(bytesValues0.advanceExact(0));
+                assertEquals("abc 123", bytesValues0.nextValue().utf8ToString());
 
-            var leafData0 = fieldData.load(reader.leaves().get(0));
-            var bytesValues0 = leafData0.getBytesValues();
-            assertTrue(bytesValues0.advanceExact(0));
-            assertEquals("abc 123", bytesValues0.nextValue().utf8ToString());
-
-            var leafData1 = fieldData.load(reader.leaves().get(1));
-            var bytesValues1 = leafData1.getBytesValues();
-            assertFalse(bytesValues1.advanceExact(0));
-        });
+                var leafData1 = fieldData.load(reader.leaves().get(1));
+                var bytesValues1 = leafData1.getBytesValues();
+                assertFalse(bytesValues1.advanceExact(0));
+            }
+        }
     }
 
     public void testValueFetcherWithDisabledTemplating() throws IOException {
@@ -560,30 +562,29 @@ public class PatternTextFieldMapperTests extends MapperTestCase {
         );
         MappedFieldType ft = mapperService.fieldType("field");
 
-        withLuceneIndex(mapperService, iw -> {
-            LuceneDocument doc1 = mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc();
-            iw.addDocument(doc1);
-            iw.commit();
+        try (Directory dir = newDirectory()) {
+            indexDocPerSegment(
+                dir,
+                mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc(),
+                mapperService.documentMapper().parse(source(b -> b.field("field", "foo 12"))).rootDoc()
+            );
+            try (DirectoryReader reader = DirectoryReader.open(dir)) {
+                assertEquals(2, reader.leaves().size());
 
-            LuceneDocument doc2 = mapperService.documentMapper().parse(source(b -> b.field("field", "foo 12"))).rootDoc();
-            iw.addDocument(doc2);
-            iw.commit();
-        }, reader -> {
-            assertEquals(2, reader.leaves().size());
+                SearchExecutionContext ctx = createSearchExecutionContext(mapperService, newSearcher(reader));
+                ValueFetcher fetcher = ft.valueFetcher(ctx, null);
 
-            SearchExecutionContext ctx = createSearchExecutionContext(mapperService, newSearcher(reader));
-            ValueFetcher fetcher = ft.valueFetcher(ctx, null);
+                fetcher.setNextReader(reader.leaves().get(0));
+                List<Object> values0 = fetcher.fetchValues(null, 0, new ArrayList<>());
+                assertEquals(1, values0.size());
+                assertEquals("abc 123", values0.get(0));
 
-            fetcher.setNextReader(reader.leaves().get(0));
-            List<Object> values0 = fetcher.fetchValues(null, 0, new ArrayList<>());
-            assertEquals(1, values0.size());
-            assertEquals("abc 123", values0.get(0));
-
-            fetcher.setNextReader(reader.leaves().get(1));
-            List<Object> values1 = fetcher.fetchValues(null, 0, new ArrayList<>());
-            assertEquals(1, values1.size());
-            assertEquals("foo 12", values1.get(0));
-        });
+                fetcher.setNextReader(reader.leaves().get(1));
+                List<Object> values1 = fetcher.fetchValues(null, 0, new ArrayList<>());
+                assertEquals(1, values1.size());
+                assertEquals("foo 12", values1.get(0));
+            }
+        }
     }
 
     public void testValueFetcherWithDisabledTemplatingAndMissingFieldSegment() throws IOException {
@@ -593,29 +594,28 @@ public class PatternTextFieldMapperTests extends MapperTestCase {
         );
         MappedFieldType ft = mapperService.fieldType("field");
 
-        withLuceneIndex(mapperService, iw -> {
-            LuceneDocument doc1 = mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc();
-            iw.addDocument(doc1);
-            iw.commit();
+        try (Directory dir = newDirectory()) {
+            indexDocPerSegment(
+                dir,
+                mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc(),
+                mapperService.documentMapper().parse(source(b -> {})).rootDoc()
+            );
+            try (DirectoryReader reader = DirectoryReader.open(dir)) {
+                assertEquals(2, reader.leaves().size());
 
-            LuceneDocument doc2 = mapperService.documentMapper().parse(source(b -> {})).rootDoc();
-            iw.addDocument(doc2);
-            iw.commit();
-        }, reader -> {
-            assertEquals(2, reader.leaves().size());
+                SearchExecutionContext ctx = createSearchExecutionContext(mapperService, newSearcher(reader));
+                ValueFetcher fetcher = ft.valueFetcher(ctx, null);
 
-            SearchExecutionContext ctx = createSearchExecutionContext(mapperService, newSearcher(reader));
-            ValueFetcher fetcher = ft.valueFetcher(ctx, null);
+                fetcher.setNextReader(reader.leaves().get(0));
+                List<Object> values = fetcher.fetchValues(null, 0, new ArrayList<>());
+                assertEquals(1, values.size());
+                assertEquals("abc 123", values.get(0));
 
-            fetcher.setNextReader(reader.leaves().get(0));
-            List<Object> values = fetcher.fetchValues(null, 0, new ArrayList<>());
-            assertEquals(1, values.size());
-            assertEquals("abc 123", values.get(0));
-
-            fetcher.setNextReader(reader.leaves().get(1));
-            List<Object> emptyValues = fetcher.fetchValues(null, 0, new ArrayList<>());
-            assertEquals(0, emptyValues.size());
-        });
+                fetcher.setNextReader(reader.leaves().get(1));
+                List<Object> emptyValues = fetcher.fetchValues(null, 0, new ArrayList<>());
+                assertEquals(0, emptyValues.size());
+            }
+        }
     }
 
     public void testFieldDataWithDisabledTemplating() throws IOException {
@@ -625,29 +625,29 @@ public class PatternTextFieldMapperTests extends MapperTestCase {
         );
         MappedFieldType ft = mapperService.fieldType("field");
 
-        withLuceneIndex(mapperService, iw -> {
-            LuceneDocument doc1 = mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc();
-            iw.addDocument(doc1);
-            iw.commit();
+        try (Directory dir = newDirectory()) {
+            indexDocPerSegment(
+                dir,
+                mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc(),
+                mapperService.documentMapper().parse(source(b -> {})).rootDoc()
+            );
+            try (DirectoryReader reader = DirectoryReader.open(dir)) {
+                assertEquals(2, reader.leaves().size());
 
-            LuceneDocument doc2 = mapperService.documentMapper().parse(source(b -> {})).rootDoc();
-            iw.addDocument(doc2);
-            iw.commit();
-        }, reader -> {
-            assertEquals(2, reader.leaves().size());
+                var fieldDataContext = new FieldDataContext("", null, () -> null, Set::of, MappedFieldType.FielddataOperation.SCRIPT);
+                var fieldData = ft.fielddataBuilder(fieldDataContext)
+                    .build(new IndexFieldDataCache.None(), new NoneCircuitBreakerService());
 
-            var fieldDataContext = new FieldDataContext("", null, () -> null, Set::of, MappedFieldType.FielddataOperation.SCRIPT);
-            var fieldData = ft.fielddataBuilder(fieldDataContext).build(new IndexFieldDataCache.None(), new NoneCircuitBreakerService());
+                var leafData0 = fieldData.load(reader.leaves().get(0));
+                var bytesValues0 = leafData0.getBytesValues();
+                assertTrue(bytesValues0.advanceExact(0));
+                assertEquals("abc 123", bytesValues0.nextValue().utf8ToString());
 
-            var leafData0 = fieldData.load(reader.leaves().get(0));
-            var bytesValues0 = leafData0.getBytesValues();
-            assertTrue(bytesValues0.advanceExact(0));
-            assertEquals("abc 123", bytesValues0.nextValue().utf8ToString());
-
-            var leafData1 = fieldData.load(reader.leaves().get(1));
-            var bytesValues1 = leafData1.getBytesValues();
-            assertFalse(bytesValues1.advanceExact(0));
-        });
+                var leafData1 = fieldData.load(reader.leaves().get(1));
+                var bytesValues1 = leafData1.getBytesValues();
+                assertFalse(bytesValues1.advanceExact(0));
+            }
+        }
     }
 
     public void testFieldDataWithDisabledTemplatingAllDocsHaveField() throws IOException {
@@ -657,30 +657,44 @@ public class PatternTextFieldMapperTests extends MapperTestCase {
         );
         MappedFieldType ft = mapperService.fieldType("field");
 
-        withLuceneIndex(mapperService, iw -> {
-            LuceneDocument doc1 = mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc();
-            iw.addDocument(doc1);
-            iw.commit();
+        try (Directory dir = newDirectory()) {
+            indexDocPerSegment(
+                dir,
+                mapperService.documentMapper().parse(source(b -> b.field("field", "abc 123"))).rootDoc(),
+                mapperService.documentMapper().parse(source(b -> b.field("field", "foo 12"))).rootDoc()
+            );
+            try (DirectoryReader reader = DirectoryReader.open(dir)) {
+                assertEquals(2, reader.leaves().size());
 
-            LuceneDocument doc2 = mapperService.documentMapper().parse(source(b -> b.field("field", "foo 12"))).rootDoc();
-            iw.addDocument(doc2);
-            iw.commit();
-        }, reader -> {
-            assertEquals(2, reader.leaves().size());
+                var fieldDataContext = new FieldDataContext("", null, () -> null, Set::of, MappedFieldType.FielddataOperation.SCRIPT);
+                var fieldData = ft.fielddataBuilder(fieldDataContext)
+                    .build(new IndexFieldDataCache.None(), new NoneCircuitBreakerService());
 
-            var fieldDataContext = new FieldDataContext("", null, () -> null, Set::of, MappedFieldType.FielddataOperation.SCRIPT);
-            var fieldData = ft.fielddataBuilder(fieldDataContext).build(new IndexFieldDataCache.None(), new NoneCircuitBreakerService());
+                var leafData0 = fieldData.load(reader.leaves().get(0));
+                var bytesValues0 = leafData0.getBytesValues();
+                assertTrue(bytesValues0.advanceExact(0));
+                assertEquals("abc 123", bytesValues0.nextValue().utf8ToString());
 
-            var leafData0 = fieldData.load(reader.leaves().get(0));
-            var bytesValues0 = leafData0.getBytesValues();
-            assertTrue(bytesValues0.advanceExact(0));
-            assertEquals("abc 123", bytesValues0.nextValue().utf8ToString());
+                var leafData1 = fieldData.load(reader.leaves().get(1));
+                var bytesValues1 = leafData1.getBytesValues();
+                assertTrue(bytesValues1.advanceExact(0));
+                assertEquals("foo 12", bytesValues1.nextValue().utf8ToString());
+            }
+        }
+    }
 
-            var leafData1 = fieldData.load(reader.leaves().get(1));
-            var bytesValues1 = leafData1.getBytesValues();
-            assertTrue(bytesValues1.advanceExact(0));
-            assertEquals("foo 12", bytesValues1.nextValue().utf8ToString());
-        });
+    /**
+     * Writes each document into its own segment with no merging, guaranteeing one leaf per doc.
+     */
+    private static void indexDocPerSegment(Directory dir, LuceneDocument... docs) throws IOException {
+        IndexWriterConfig iwc = new IndexWriterConfig();
+        iwc.setMergePolicy(NoMergePolicy.INSTANCE);
+        try (IndexWriter iw = new IndexWriter(dir, iwc)) {
+            for (LuceneDocument doc : docs) {
+                iw.addDocument(doc);
+                iw.commit();
+            }
+        }
     }
 
     @Override

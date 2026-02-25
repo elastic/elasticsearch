bb4fcb0042baa75f9831034444d5ca1b0817c814
Pooya Salehi <pxsalehi@users.noreply.github.com>
2026-02-25 11:16:36 +0100

    Interleave shards with potential write load during necessary relocations (#5613)
    https://github.com/elastic/elasticsearch-serverless/pull/5431 has been
    pretty effective on the
    [cases](https://github.com/elastic/serverless-gitops/pull/59941) I've
    tried out. To enable it by default, we need to address the possibility
    that we end up with write shards at the tail of the relocation order and
    risk write unavailability since we do still occasionally hit the 60m
    timeout for node shutdown. This PRs makes sure we have a heuristic to
    address this, and enables `prioritize_write_shard_necessary_relocations`
    by default.
    
    It divides the shards into: 
    
    1. Shards to spread: data stream write shards or normal shards with non zero write load. 
    2. Data stream read shards.
    3. Normal shards with no write load. 
    
    Keeps half of 2 for the end of the returned list to make sure when
    possible we still move shards with writes with _some_ priority and they
    won't be the last. Spreads 1 across the rest of 2, and 3 for the
    beginning of the returned order.
    
    Assisted by Cursor.
    
    Closes

--- x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/allocation/StatelessShardRelocationOrder.java
+++ x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/allocation/StatelessShardRelocationOrder.java
@@ -20,21 +20,24 @@ package org.elasticsearch.xpack.stateless.allocation;
 import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.cluster.routing.allocation.RoutingAllocation;
 import org.elasticsearch.cluster.routing.allocation.allocator.ShardRelocationOrder;
-import org.elasticsearch.common.collect.Iterators;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Setting;
 
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Objects;
 
 public class StatelessShardRelocationOrder extends ShardRelocationOrder.DefaultOrder {
 
     /**
-     * If disabled, necessary shard relocations are not ordered explicitly and simply follow the order of shards
-     * recorded in {@link org.elasticsearch.cluster.routing.RoutingNode#shards}.
+     * If enabled, necessary shard relocations follow the {@link ShardRelocationOrder.DefaultOrder} (DS write first,
+     * then normal, then DS read). If disabled, a custom ordering is applied that interleaves half of the DS read shards
+     * with DS write and normal index shards (with write-load spreading), keeping the other half of DS read at the end.
      */
     public static Setting<Boolean> PRIORITIZE_WRITE_SHARD_RELOCATIONS_SETTING = Setting.boolSetting(
         "serverless.cluster.routing.allocation.prioritize_write_shard_necessary_relocations",
-        true,
+        false,
         Setting.Property.NodeScope,
         Setting.Property.Dynamic
     );
@@ -47,9 +50,67 @@ public class StatelessShardRelocationOrder extends ShardRelocationOrder.DefaultO
 
     @Override
     public Iterator<ShardRouting> forNecessaryMoves(RoutingAllocation allocation, String nodeId) {
-        return prioritizeWriteShardRelocation
-            ? super.forNecessaryMoves(allocation, nodeId)
-            : Iterators.forArray(allocation.routingNodes().node(nodeId).copyShards());
+        if (prioritizeWriteShardRelocation) {
+            return super.forNecessaryMoves(allocation, nodeId);
+        }
+        return orderNecessaryMovesWithInterleaving(allocation, nodeId);
+    }
+
+    /**
+     * Orders necessary shard relocations by interleaving half of the DS read shards with DS write and normal index
+     * shards (with write-load spreading), keeping the other half of DS read shards at the end of the iterator.
+     */
+    static Iterator<ShardRouting> orderNecessaryMovesWithInterleaving(RoutingAllocation allocation, String nodeId) {
+        var routingNode = allocation.routingNodes().node(nodeId);
+        if (routingNode.isEmpty()) {
+            return Collections.emptyIterator();
+        }
+
+        final var result = new LinkedList<ShardRouting>();  // has two parts, the second part are just data stream read shards
+        final var shardsToSpread = new LinkedList<ShardRouting>(); // write shards or normal shards with write load
+        final var firstHalfRest = new LinkedList<ShardRouting>(); // read shards or normal shards with no write load
+        boolean addToSecondHalf = true;
+        var writeLoads = allocation.clusterInfo().getShardWriteLoads();
+
+        for (ShardRouting shard : routingNode) {
+            var index = allocation.metadata().projectFor(shard.index()).getIndicesLookup().get(shard.getIndexName());
+            if (index != null && index.getParentDataStream() != null) {
+                // data stream write shards must be always spread around. read shards are split between first and second half.
+                if (Objects.equals(index.getParentDataStream().getWriteIndex(), shard.index())) {
+                    shardsToSpread.add(shard);
+                } else if (addToSecondHalf) {
+                    result.add(shard);
+                    addToSecondHalf = false;
+                } else {
+                    firstHalfRest.add(shard);
+                    addToSecondHalf = true;
+                }
+            } else if (writeLoads.getOrDefault(shard.shardId(), 0.0) > 0.0) {
+                shardsToSpread.add(shard);
+            } else {
+                firstHalfRest.add(shard);
+            }
+        }
+
+        if (shardsToSpread.isEmpty()) {
+            firstHalfRest.forEach(result::addFirst);
+            return result.iterator();
+        }
+
+        // Interleave shardsToSpread with firstHalfRest so that the former are spread out evenly.
+        int toInterleave = Math.max(firstHalfRest.size() / shardsToSpread.size(), 1);
+        var firstHalfRestIterator = firstHalfRest.iterator();
+        for (ShardRouting shardRouting : shardsToSpread) {
+            result.addFirst(shardRouting);
+            for (int k = 0; k < toInterleave && firstHalfRestIterator.hasNext(); k++) {
+                result.addFirst(firstHalfRestIterator.next());
+            }
+        }
+        while (firstHalfRestIterator.hasNext()) {
+            result.addFirst(firstHalfRestIterator.next());
+        }
+
+        return result.iterator();
     }
 
     @Override

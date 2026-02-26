d4f6fa243a54c8df921a9225710088602df5839e
Jack Conradson <osjdconrad@gmail.com>
2026-02-26 07:19:36 -0800

    [9.3] Update x-content jackson version to 2.21.0 (#141777) (#142356)

--- test/framework/src/main/java/org/elasticsearch/datageneration/datasource/DefaultObjectGenerationHandler.java
+++ test/framework/src/main/java/org/elasticsearch/datageneration/datasource/DefaultObjectGenerationHandler.java
@@ -74,9 +74,25 @@ public class DefaultObjectGenerationHandler implements DataSourceHandler {
                 if (fieldName.startsWith(RESERVED_FIELD_NAME_PREFIX)) {
                     continue;
                 }
+                // Filter out Unicode surrogates to avoid encoding differences with
+                // JsonWriteFeature.COMBINE_UNICODE_SURROGATES_IN_UTF8 in Jackson 2.21.0+
+                if (containsSurrogates(fieldName)) {
+                    continue;
+                }
 
                 return fieldName;
             }
+        }
+
+        private boolean containsSurrogates(String str) {
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                // Check for high surrogates (U+D800 to U+DBFF) or low surrogates (U+DC00 to U+DFFF)
+                if (Character.isSurrogate(c)) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 

cab4c74b1564e3a48340944269f5c5050379f6b6
Keith Massey <keith.massey@elastic.co>
2026-02-26 14:50:59 -0600

    Moving the serverless-no-wait-for-active-shards plugin (#5633)

--- x-pack/plugin/stateless-no-wait-for-active-shards/build.gradle.kts
+++ x-pack/plugin/stateless-no-wait-for-active-shards/build.gradle.kts
@@ -1,12 +1,29 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ * Copyright Elasticsearch B.V. All rights reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch B.V. and its suppliers, if any.
+ * The intellectual and technical concepts contained herein
+ * are proprietary to Elasticsearch B.V. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright
+ * law.  Dissemination of this information or reproduction of
+ * this material is strictly forbidden unless prior written
+ * permission is obtained from Elasticsearch B.V.
+ */
+
 plugins {
     id("elasticsearch.internal-cluster-test")
     id("elasticsearch.internal-java-rest-test")
 }
 
 esplugin {
-    name = "serverless-no-wait-for-active-shards"
-    description = "Action filter to ignore ?wait_for_active_shards= on indexing requests for serverless Elasticsearch"
-    classname = "co.elastic.elasticsearch.api.waitforactiveshards.NoWaitForActiveShardsPlugin"
+    name = "stateless-no-wait-for-active-shards"
+    description = "Action filter to ignore ?wait_for_active_shards= on indexing requests for stateless Elasticsearch"
+    classname = "org.elasticsearch.xpack.stateless.waitforactiveshards.NoWaitForActiveShardsPlugin"
 }
 
 configurations {
@@ -21,5 +38,6 @@ dependencies {
     compileOnly("org.elasticsearch:server")
     internalClusterTestImplementation(testArtifact(project(":modules-self-managed:stateless"), "internalClusterTest"))
     internalClusterTestImplementation(testArtifact(project(":modules:serverless-stateless"), "internalClusterTest"))
-    javaRestTestImplementation(project(":modules:serverless-no-wait-for-active-shards"))
+    javaRestTestImplementation(project(":modules-self-managed:stateless-no-wait-for-active-shards"))
 }
+

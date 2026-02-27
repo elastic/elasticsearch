d56a445cb4bff5166cb5dd5321b8420440b0c5e4
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-02-27 15:04:42 +0000

    [CI] Auto commit changes from spotless

--- modules/reindex/src/test/java/org/elasticsearch/reindex/BulkByPaginatedSearchParallelizationHelperTests.java
+++ modules/reindex/src/test/java/org/elasticsearch/reindex/BulkByPaginatedSearchParallelizationHelperTests.java
@@ -29,7 +29,6 @@ import org.elasticsearch.threadpool.ThreadPool;
 import org.junit.After;
 import org.junit.Before;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.concurrent.atomic.AtomicReference;
 

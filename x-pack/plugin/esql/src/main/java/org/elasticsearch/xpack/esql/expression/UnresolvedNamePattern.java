fff8a50e4cdfa7312377f40e3efb737329d214c4
Alexander Spies <alexander.spies@elastic.co>
2026-02-27 12:39:36 +0100

    Make UnresolvedNamePattern less confusing

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/UnresolvedNamePattern.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/UnresolvedNamePattern.java
@@ -35,14 +35,15 @@ public class UnresolvedNamePattern extends UnresolvedNamedExpression {
     private final CharacterRunAutomaton automaton;
     private final String pattern;
     // string representation without backquotes
-    // cannot just use NamedExpression.name() because the superclass gives this a constant value of "<unresolved>"
-    private final String name;
+    // Cannot rely on NamedExpression.name: the UnresolvedNamedExpression superclass throws on name()
+    // and stores "<unresolved>" as the internal name field.
+    private final String actualName;
 
     public UnresolvedNamePattern(Source source, CharacterRunAutomaton automaton, String patternString, String name) {
         super(source, emptyList());
         this.automaton = automaton;
         this.pattern = patternString;
-        this.name = name;
+        this.actualName = name;
     }
 
     @Override
@@ -62,7 +63,7 @@ public class UnresolvedNamePattern extends UnresolvedNamedExpression {
     // override because the super class throws
     @Override
     public String name() {
-        return name;
+        return actualName;
     }
 
     public String pattern() {
@@ -76,7 +77,7 @@ public class UnresolvedNamePattern extends UnresolvedNamedExpression {
 
     @Override
     protected NodeInfo<UnresolvedNamePattern> info() {
-        return NodeInfo.create(this, UnresolvedNamePattern::new, automaton, pattern, name);
+        return NodeInfo.create(this, UnresolvedNamePattern::new, automaton, pattern, actualName);
     }
 
     @Override
@@ -101,13 +102,13 @@ public class UnresolvedNamePattern extends UnresolvedNamedExpression {
 
     @Override
     protected int innerHashCode(boolean ignoreIds) {
-        return Objects.hash(super.innerHashCode(true), pattern, name);
+        return Objects.hash(super.innerHashCode(true), pattern, actualName);
     }
 
     @Override
     protected boolean innerEquals(Object o, boolean ignoreIds) {
         var other = (UnresolvedNamePattern) o;
-        return super.innerEquals(other, true) && Objects.equals(pattern, other.pattern) && Objects.equals(name, other.name);
+        return super.innerEquals(other, true) && Objects.equals(pattern, other.pattern) && Objects.equals(actualName, other.actualName);
     }
 
     @Override

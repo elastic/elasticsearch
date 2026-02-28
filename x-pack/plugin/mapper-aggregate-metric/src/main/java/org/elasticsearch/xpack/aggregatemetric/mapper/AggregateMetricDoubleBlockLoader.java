ed97e0f26db2c24806b33938363a0d596013f602
Larisa Motova <larisa.motova@elastic.co>
2026-02-27 16:51:47 -1000

    [ES|QL] Use avg metric for AMD default metric (#141331)
    This commit changes the behavior of the default metric for
    aggregate_metric_double in ES|QL to always use the average regardless
    of what the default metric is mapped to.
    
    Note that this avg is not the same as the average you would get if you
    ran `STATS avg(agg_metric)`, because the aforementioned is the average
    of all docs, but the avg metric is an average on a per-doc basis.
    
    Part of #128356

--- x-pack/plugin/mapper-aggregate-metric/src/main/java/org/elasticsearch/xpack/aggregatemetric/mapper/AggregateMetricDoubleBlockLoader.java
+++ x-pack/plugin/mapper-aggregate-metric/src/main/java/org/elasticsearch/xpack/aggregatemetric/mapper/AggregateMetricDoubleBlockLoader.java
@@ -12,9 +12,12 @@ import org.apache.lucene.util.NumericUtils;
 import org.elasticsearch.common.breaker.CircuitBreaker;
 import org.elasticsearch.core.Releasables;
 import org.elasticsearch.index.mapper.NumberFieldMapper;
+import org.elasticsearch.index.mapper.blockloader.ConstantNull;
 import org.elasticsearch.index.mapper.blockloader.docvalues.BlockDocValuesReader;
 import org.elasticsearch.index.mapper.blockloader.docvalues.DoublesBlockLoader;
 import org.elasticsearch.index.mapper.blockloader.docvalues.IntsBlockLoader;
+import org.elasticsearch.index.mapper.blockloader.docvalues.tracking.NumericDvSingletonOrSorted;
+import org.elasticsearch.index.mapper.blockloader.docvalues.tracking.TrackingNumericDocValues;
 
 import java.io.IOException;
 import java.util.EnumMap;
@@ -138,6 +141,106 @@ public class AggregateMetricDoubleBlockLoader extends BlockDocValuesReader.DocVa
         @Override
         public void close() {
             Releasables.close(minReader, maxReader, sumReader, countReader);
+        }
+    }
+
+    public static class AvgBlockLoader extends BlockDocValuesReader.DocValuesBlockLoader {
+        NumberFieldMapper.NumberFieldType sumFieldType;
+        NumberFieldMapper.NumberFieldType countFieldType;
+
+        AvgBlockLoader(EnumMap<AggregateMetricDoubleFieldMapper.Metric, NumberFieldMapper.NumberFieldType> availableMetrics) {
+            if (availableMetrics.containsKey(AggregateMetricDoubleFieldMapper.Metric.sum) == false
+                || availableMetrics.containsKey(AggregateMetricDoubleFieldMapper.Metric.value_count) == false) {
+                sumFieldType = null;
+                countFieldType = null;
+            } else {
+                sumFieldType = availableMetrics.get(AggregateMetricDoubleFieldMapper.Metric.sum);
+                countFieldType = availableMetrics.get(AggregateMetricDoubleFieldMapper.Metric.value_count);
+            }
+        }
+
+        @Override
+        public AllReader reader(CircuitBreaker breaker, LeafReaderContext context) throws IOException {
+            if (sumFieldType == null || countFieldType == null) {
+                return ConstantNull.READER;
+            }
+            NumericDvSingletonOrSorted dvSumValues = NumericDvSingletonOrSorted.get(breaker, context, sumFieldType.name());
+            NumericDvSingletonOrSorted dvValueCountValues = NumericDvSingletonOrSorted.get(breaker, context, countFieldType.name());
+            if (dvSumValues == null || dvValueCountValues == null) {
+                return ConstantNull.READER;
+            }
+            assert dvSumValues.sorted() == null && dvValueCountValues.sorted() == null
+                : "aggregate metric doubles shouldn't have multi-values";
+
+            TrackingNumericDocValues trackingSumValues = dvSumValues.singleton();
+            TrackingNumericDocValues trackingValueCountValues = dvValueCountValues.singleton();
+            var sumValues = trackingSumValues.docValues();
+            var valueCountValues = trackingValueCountValues.docValues();
+
+            return new BlockDocValuesReader(breaker) {
+                @Override
+                public void close() {
+                    Releasables.close(trackingSumValues, trackingValueCountValues);
+                }
+
+                private int docID = -1;
+
+                @Override
+                protected int docId() {
+                    return docID;
+                }
+
+                @Override
+                public String toString() {
+                    return "BlockDocValuesReader.AggregateMetricDoubleAvg";
+                }
+
+                @Override
+                public Block read(BlockFactory factory, Docs docs, int offset, boolean nullsFiltered) throws IOException {
+                    int expectedCount = docs.count() - offset;
+                    if (sumValues == null || valueCountValues == null) {
+                        return factory.constantNulls(expectedCount);
+                    }
+                    try (DoubleBuilder builder = factory.doublesFromDocValues(expectedCount)) {
+                        int lastDoc = -1;
+
+                        for (int i = offset; i < docs.count(); i++) {
+                            int doc = docs.get(i);
+                            if (doc < lastDoc) {
+                                throw new IllegalStateException("docs within same block must be in order");
+                            }
+                            if (sumValues.advanceExact(doc) && valueCountValues.advanceExact(doc)) {
+                                this.docID = doc;
+                                lastDoc = doc;
+                                double sum = NumericUtils.sortableLongToDouble(sumValues.longValue());
+                                long count = valueCountValues.longValue();
+                                builder.appendDouble(sum / count);
+                            } else {
+                                builder.appendNull();
+                            }
+                        }
+                        return builder.build();
+                    }
+                }
+
+                @Override
+                public void read(int docId, StoredFields storedFields, Builder builder) throws IOException {
+                    DoubleBuilder blockBuilder = (DoubleBuilder) builder;
+                    if (sumValues.advanceExact(docId) && valueCountValues.advanceExact(docId)) {
+                        this.docID = docId;
+                        var sum = NumericUtils.sortableLongToDouble(sumValues.longValue());
+                        var count = valueCountValues.longValue();
+                        blockBuilder.appendDouble(sum / count);
+                    } else {
+                        blockBuilder.appendNull();
+                    }
+                }
+            };
+        }
+
+        @Override
+        public Builder builder(BlockFactory factory, int expectedCount) {
+            throw new UnsupportedOperationException("AvgBlockLoader does not have a corresponding builder");
         }
     }
 }

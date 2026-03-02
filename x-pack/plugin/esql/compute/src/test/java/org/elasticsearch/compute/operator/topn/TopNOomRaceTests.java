cc4d0ff2d146d621975e0ffaa8b9559e0e75f819
Nik Everett <nik9000@gmail.com>
2026-03-02 18:41:55 -0500

    ESQL: Don't use current memory usage in topn tests (#143424)
    The `TopNOomRaceTests` depended on free memory which made it
    inconsistent. This makes it dependent on a subset of total memory which
    should make it more consistent.
    
    Closes #140258 Closes #141471 Closes #141752 Closes #141773 Closes

--- x-pack/plugin/esql/compute/src/test/java/org/elasticsearch/compute/operator/topn/TopNOomRaceTests.java
+++ x-pack/plugin/esql/compute/src/test/java/org/elasticsearch/compute/operator/topn/TopNOomRaceTests.java
@@ -29,9 +29,6 @@ import org.elasticsearch.compute.test.MockBlockFactory;
 import org.elasticsearch.compute.test.OperatorTestCase;
 import org.elasticsearch.compute.test.TestDriverFactory;
 import org.elasticsearch.test.ESTestCase;
-import org.junit.Rule;
-import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -73,8 +70,6 @@ public class TopNOomRaceTests extends ESTestCase {
         return parameters;
     }
 
-    @Rule(order = Integer.MIN_VALUE)
-    public static final AnyFailedWatcher watcher = new AnyFailedWatcher();
     private final int repeats;
 
     public TopNOomRaceTests(int repeats) {
@@ -82,15 +77,16 @@ public class TopNOomRaceTests extends ESTestCase {
     }
 
     public void testRace() {
-        assumeFalse("previous test failed", watcher.anyFailed);
         int driverCount = 6;
         double usedByEachThread = .3;
-        ByteSizeValue limit = ByteSizeValue.ofBytes(Runtime.getRuntime().freeMemory());
+        ByteSizeValue limit = ByteSizeValue.ofBytes(Runtime.getRuntime().totalMemory() - ByteSizeValue.ofMb(30).getBytes());
         int approxObjectRefsPerFilledRow = 5;
-        int approxSizePerFilledRow = RamUsageEstimator.NUM_BYTES_OBJECT_REF * approxObjectRefsPerFilledRow + repeats * Integer.BYTES;
+        // One byte for the null/non-null marker
+        int approxSizePerFilledRow = RamUsageEstimator.NUM_BYTES_OBJECT_REF * approxObjectRefsPerFilledRow + repeats * (1 + Integer.BYTES);
         int topCount = (int) ((double) limit.getBytes() / approxSizePerFilledRow * usedByEachThread) - 1;
         int maxInput = topCount * 3;
         assertThat(topCount, greaterThan(0));
+        logger.info("limit={} approxSizePerFilledRow={} topCount={}", limit, approxSizePerFilledRow, topCount);
 
         BigArrays bigArrays = new MockBigArrays(PageCacheRecycler.NON_RECYCLING_INSTANCE, limit);
         CircuitBreaker breaker = bigArrays.breakerService().getBreaker(CircuitBreaker.REQUEST);
@@ -142,12 +138,8 @@ public class TopNOomRaceTests extends ESTestCase {
         }
     }
 
-    public static class AnyFailedWatcher extends TestWatcher {
-        private boolean anyFailed = false;
-
-        @Override
-        protected void failed(Throwable e, Description description) {
-            anyFailed = true;
-        }
+    @Override
+    protected boolean shouldFailureSkipRemainingTests() {
+        return true;
     }
 }

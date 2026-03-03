6ec8d9210141bb5644ba596bb28be4eda45aca86
Nik Everett <nik9000@gmail.com>
2026-03-03 13:47:31 -0500

    ESQL: Remaining serialization tests (#143470)
    This adds the remaining serialization tests. It makes a few production
    code changes:
    1. Registers `TO_DATE_RANGE` for serialization. It was serializable and
       not registered.
    2. Normalizes the serialization of `SCORE` and explains how it used to
       work. It was not broken but held together strangely.
    3. Replaces implementing `SurrogateExpression` with
       `OnlySurrogateExpression` in a few more cases so we don't expect
       serialization to work.
    4. Adds some package private accessors so we can refer to children by
       name in serialization tests.

---
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/scalar/spatial/StYMaxSerializationTests.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.esql.expression.function.scalar.spatial;
+
+import org.elasticsearch.xpack.esql.expression.AbstractExpressionSerializationTests;
+
+import java.io.IOException;
+
+public class StYMaxSerializationTests extends AbstractExpressionSerializationTests<StYMax> {
+    @Override
+    protected StYMax createTestInstance() {
+        return new StYMax(randomSource(), randomChild());
+    }
+
+    @Override
+    protected StYMax mutateInstance(StYMax instance) throws IOException {
+        return new StYMax(
+            instance.source(),
+            randomValueOtherThan(instance.spatialField(), AbstractExpressionSerializationTests::randomChild)
+        );
+    }
+}

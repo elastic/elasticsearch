764c6babf8a697ed10d5b50260459c6f8a0a72ae
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2026-03-03 11:03:09 +0100

    Address review feedback

--- server/src/main/java/org/elasticsearch/transport/RemoteClusterAware.java
+++ server/src/main/java/org/elasticsearch/transport/RemoteClusterAware.java
@@ -205,9 +205,9 @@ public abstract class RemoteClusterAware implements LinkedProjectConfigService.L
                         );
                     }
                 } else {
+                    everIncluded.addAll(clusters);
                     for (String clusterName : clusters) {
                         perClusterIndices.computeIfAbsent(clusterName, k -> new ArrayList<>()).add(indexName);
-                        everIncluded.add(clusterName);
                     }
                 }
             } else {

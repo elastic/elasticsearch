c4214b0e9e3957cd1b924f2b397d9a87afdb1f95
Mike Pellegrini <mike.pellegrini@elastic.co>
2026-03-04 17:53:10 -0500

    Add more failure test cases

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/vectors/EmbeddingQueryVectorBuilderFailureTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/vectors/EmbeddingQueryVectorBuilderFailureTests.java
@@ -16,11 +16,16 @@ import org.elasticsearch.inference.DataType;
 import org.elasticsearch.inference.WeightedToken;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.client.NoOpClient;
+import org.elasticsearch.threadpool.TestThreadPool;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.xpack.core.inference.action.InferenceAction;
+import org.elasticsearch.xpack.core.inference.results.EmbeddingFloatResults;
+import org.elasticsearch.xpack.core.inference.results.GenericDenseEmbeddingFloatResults;
 import org.elasticsearch.xpack.core.inference.results.SparseEmbeddingResults;
 import org.elasticsearch.xpack.core.ml.inference.results.MlDenseEmbeddingResults;
 import org.elasticsearch.xpack.core.ml.inference.results.TextExpansionResults;
+import org.junit.After;
+import org.junit.Before;
 
 import java.util.List;
 import java.util.function.Supplier;
@@ -28,6 +33,18 @@ import java.util.function.Supplier;
 import static org.hamcrest.Matchers.equalTo;
 
 public class EmbeddingQueryVectorBuilderFailureTests extends ESTestCase {
+    private ThreadPool threadPool;
+
+    @Before
+    public void setupThreadPool() {
+        threadPool = new TestThreadPool(getTestName());
+    }
+
+    @After
+    public void shutdownThreadPool() {
+        threadPool.shutdownNow();
+    }
+
     private static class MockClient extends NoOpClient {
         private final Supplier<InferenceAction.Response> responseSupplier;
 
@@ -47,29 +64,57 @@ public class EmbeddingQueryVectorBuilderFailureTests extends ESTestCase {
         }
     }
 
+    public void testEmptyResults_ThrowsIllegalStateException() {
+        var queryVectorBuilder = createTestInstance();
+        var emptyResults = new GenericDenseEmbeddingFloatResults(List.of());
+
+        final var client = new MockClient(threadPool, () -> new InferenceAction.Response(emptyResults));
+        PlainActionFuture<float[]> future = new PlainActionFuture<>();
+        queryVectorBuilder.buildVector(client, future);
+
+        var exception = expectThrows(IllegalStateException.class, () -> future.actionGet(TEST_REQUEST_TIMEOUT));
+        assertThat(exception.getMessage(), equalTo("embedding inference response contains no results"));
+    }
+
+    public void testMultipleResults_ThrowsIllegalStateException() {
+        var queryVectorBuilder = createTestInstance();
+        var multipleResults = new GenericDenseEmbeddingFloatResults(
+            List.of(
+                new EmbeddingFloatResults.Embedding(new float[] { 1.0f }),
+                new EmbeddingFloatResults.Embedding(new float[] { 2.0f }),
+                new EmbeddingFloatResults.Embedding(new float[] { 3.0f })
+            )
+        );
+
+        final var client = new MockClient(threadPool, () -> new InferenceAction.Response(multipleResults));
+        PlainActionFuture<float[]> future = new PlainActionFuture<>();
+        queryVectorBuilder.buildVector(client, future);
+
+        var exception = expectThrows(IllegalStateException.class, () -> future.actionGet(TEST_REQUEST_TIMEOUT));
+        assertThat(exception.getMessage(), equalTo("embedding inference response contains 3 results"));
+    }
+
     public void testReceivingSparseEmbeddingResults_ThrowsIllegalStateException() {
         var queryVectorBuilder = createTestInstance();
-        try (var threadPool = createThreadPool()) {
-            var sparseResults = new SparseEmbeddingResults(
-                List.of(new SparseEmbeddingResults.Embedding(List.of(new WeightedToken("token", 0.1f)), false))
-            );
+        var sparseResults = new SparseEmbeddingResults(
+            List.of(new SparseEmbeddingResults.Embedding(List.of(new WeightedToken("token", 0.1f)), false))
+        );
 
-            final var client = new MockClient(threadPool, () -> new InferenceAction.Response(sparseResults));
-            PlainActionFuture<float[]> future = new PlainActionFuture<>();
-            queryVectorBuilder.buildVector(client, future);
+        final var client = new MockClient(threadPool, () -> new InferenceAction.Response(sparseResults));
+        PlainActionFuture<float[]> future = new PlainActionFuture<>();
+        queryVectorBuilder.buildVector(client, future);
 
-            var exception = expectThrows(IllegalStateException.class, () -> future.actionGet(TEST_REQUEST_TIMEOUT));
-            assertThat(
-                exception.getMessage(),
-                equalTo(
-                    "expected inference results to be of type ["
-                        + MlDenseEmbeddingResults.NAME
-                        + "], received ["
-                        + TextExpansionResults.NAME
-                        + "]"
-                )
-            );
-        }
+        var exception = expectThrows(IllegalStateException.class, () -> future.actionGet(TEST_REQUEST_TIMEOUT));
+        assertThat(
+            exception.getMessage(),
+            equalTo(
+                "expected inference results to be of type ["
+                    + MlDenseEmbeddingResults.NAME
+                    + "], received ["
+                    + TextExpansionResults.NAME
+                    + "]"
+            )
+        );
     }
 
     private static EmbeddingQueryVectorBuilder createTestInstance() {

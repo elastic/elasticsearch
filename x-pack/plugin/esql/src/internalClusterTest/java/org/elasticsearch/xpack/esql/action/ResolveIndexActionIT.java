bc1493278901e1d1bec0e0baca87780350d5e714
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2026-03-04 10:24:10 +0100

    fix style

--- x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/ResolveIndexActionIT.java
+++ x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/ResolveIndexActionIT.java
@@ -1,12 +1,5 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the "Elastic License
  * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
  * Public License v 1"; you may not use this file except in compliance with, at

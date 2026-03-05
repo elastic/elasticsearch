9c3b3432bdc12ef5a095a805cf5c5521c52e363a
Moritz Mack <moritz.mack@elastic.co>
2026-03-05 10:13:27 +0100

    Migrate stateless-sigterm plugin for stateless on prem (#5506)
    Migrate stateless-sigterm plugin for stateless on prem.
    
    This also renames related settings from `serverless.sigterm.*` to `stateless.sigterm.*`.
    There's no override in place for any of these.
    
    Also unmutes relates tests, despite their issues already being closed:
    - https://github.com/elastic/elasticsearch-serverless/issues/3250
    - https://github.com/elastic/elasticsearch-serverless/issues/5273
    
    Relates to

--- x-pack/plugin/stateless/build.gradle.kts
+++ x-pack/plugin/stateless/build.gradle.kts
@@ -1,5 +1,20 @@
-import org.elasticsearch.gradle.internal.conventions.precommit.LicenseHeadersTask
-import org.elasticsearch.gradle.testclusters.StandaloneRestIntegTestTask
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ * Copyright Elasticsearch B.V. All rights reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch B.V. and its suppliers, if any.
+ * The intellectual and technical concepts contained herein
+ * are proprietary to Elasticsearch B.V. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright
+ * law.  Dissemination of this information or reproduction of
+ * this material is strictly forbidden unless prior written
+ * permission is obtained from Elasticsearch B.V.
+ */
+
 import org.elasticsearch.gradle.util.GradleUtils
 import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.project

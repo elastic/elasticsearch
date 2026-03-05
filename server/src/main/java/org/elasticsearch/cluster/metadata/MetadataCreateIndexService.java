51e139ff0a5797fe2b7dc74ac121e1f84d4730ea
Zhubo Tang <zhubo.tang@elastic.co>
2026-03-05 11:13:09 -0800

    ES 14291 Exclude feature associated indices from index count limit (#143126)
    * ES-14291 Exclude feature associated indices from index limit.

--- server/src/main/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexService.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/MetadataCreateIndexService.java
@@ -253,7 +253,16 @@ public class MetadataCreateIndexService {
             return;
         }
 
-        var totalUserIndices = projectMetadata.stream().filter(indexMetadata -> indexMetadata.isSystem() == false).count();
+        if (systemIndices.isFeatureAssociatedIndex(request.index())) {
+            return;
+        }
+
+        var totalUserIndices = projectMetadata.stream()
+            .filter(
+                indexMetadata -> indexMetadata.isSystem() == false
+                    && systemIndices.isFeatureAssociatedIndex(indexMetadata.getIndex().getName()) == false
+            )
+            .count();
         if (totalUserIndices >= maxIndicesPerProject) {
             throw new IndexLimitExceededException(
                 "This action would add an index, but this project currently has ["

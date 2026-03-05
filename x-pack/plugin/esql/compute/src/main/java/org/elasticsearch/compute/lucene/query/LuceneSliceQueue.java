2a041dd9540c2c0c99b58c35760f0c263f49b672
Nhat Nguyen <nhat.nguyen@elastic.co>
2026-03-05 10:27:58 -0800

    1M

--- x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/lucene/query/LuceneSliceQueue.java
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/lucene/query/LuceneSliceQueue.java
@@ -320,7 +320,7 @@ public final class LuceneSliceQueue {
             @Override
             List<List<PartialLeafReaderContext>> groups(IndexSearcher searcher, int taskConcurrency) {
                 final int totalDocCount = searcher.getIndexReader().maxDoc();
-                final int docsPerSlice = Math.max(Math.ceilDiv(totalDocCount, taskConcurrency), 1);
+                final int docsPerSlice = Math.clamp(Math.ceilDiv(totalDocCount, taskConcurrency), 1, MAX_DOCS_PER_SLICE * 4);
                 try {
                     return new TimeSeriesPartitioner().partition(searcher, docsPerSlice);
                 } catch (IOException e) {

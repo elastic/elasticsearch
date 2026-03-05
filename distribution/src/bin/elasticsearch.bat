e93f17ecbddf202185db7ba0b998c391bfe31adf
Mark Vieira <portugee@gmail.com>
2026-03-05 15:13:55 -0800

    Simplify windows batch script

--- distribution/src/bin/elasticsearch.bat
+++ distribution/src/bin/elasticsearch.bat
@@ -3,27 +3,13 @@
 setlocal enabledelayedexpansion
 setlocal enableextensions
 
-call "%~dp0elasticsearch-env.bat" || exit /b 1
+set CLI_NAME=server
+set CLI_LIBS=lib/tools/server-cli
+call "%~dp0elasticsearch-cli.bat" ^
+  %%* ^
+  || goto exit
 
-rem use a small heap size for the CLI tools, and thus the serial collector to
-rem avoid stealing many CPU cycles; a user can override by setting CLI_JAVA_OPTS
-set CLI_JAVA_OPTS=-Xms4m -Xmx64m -XX:+UseSerialGC %CLI_JAVA_OPTS%
-
-set "NATIVE_LAUNCHER=%ES_HOME%\lib\tools\server-launcher\server-launcher.exe"
-set LAUNCHER_LIBS=%ES_HOME%/lib/tools/server-launcher/*
-
-rem Prefer the native binary if present; fall back to the JVM-based launcher.
-if exist "%NATIVE_LAUNCHER%" (
-  "%NATIVE_LAUNCHER%" %*
-) else (
-  %JAVA% ^
-    %CLI_JAVA_OPTS% ^
-    -cp "%LAUNCHER_LIBS%" ^
-    org.elasticsearch.server.launcher.ServerLauncher ^
-    %*
-)
-
+endlocal
+endlocal
+:exit
 exit /b %ERRORLEVEL%
-
-endlocal
-endlocal

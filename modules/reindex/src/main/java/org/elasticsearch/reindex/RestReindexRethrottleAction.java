dd2707babbb36a42b4f8eb9f30f27096cb6e020f
Pete Gillin <pete.gillin@elastic.co>
2026-03-06 19:41:47 +0000

    Improve reindex rethrottle API in stateless
    This makes a number of improvements to the reindex rethrottle API, in
    stateless only, in preparation for making it public in serverless as
    part of the reindex managament API work.
    
    The changes are:
    
     1. The `group_by` request parameter is no longer supported. This was
        never very useful in this API, since the `ListTasksResponse` will
        only every contain one task.
    
     2. The API never groups the tasks in the response, i.e. it acts as
        though `group_by=none` (contrast with stateful, which defaults to
        `group_by=nodes`). Again, grouping is not useful in this
        case. This also means that it omits the node information which
        would be present with `group_by=nodes`, and which we do not want
        to expose in serverless.
    
     3. The `node` property of the task in the response is redacted with
        `stateless` instead of giving the node ID. (The get task API
        behaves similarly in serverless.)
    
    The API is unchanged in stateful, for backwards compatiblity reasons.
    
    Implementation note: This change is done in the REST layer rather,
    because the `group_by` parameter is only entirely implemented in that
    layer. To implement changes 1 and 2 in the transport layer would mean
    passing the requested `group_by` from the REST layer to the transport
    layer for validation, and passing the `group_by` to use from the
    transport layer back to the REST layer. Although change 3 could be
    done in the transport layer, it seems neater to keep the three changes
    together.
    
    Testing note: Adding a YAML REST test for this would require a whole
    new base class, and a whole new cluster with the stateless setting
    enabled. This seems unnecessarily heavyweight. Instead, a unit test
    for the REST action is added. This uses a real `RestController`, a
    fake `RestChannel`, and a fake `NodeClient` (i.e. the transport layer
    is faked out).

--- modules/reindex/src/main/java/org/elasticsearch/reindex/RestReindexRethrottleAction.java
+++ modules/reindex/src/main/java/org/elasticsearch/reindex/RestReindexRethrottleAction.java
@@ -9,13 +9,19 @@
 
 package org.elasticsearch.reindex;
 
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.TaskOperationFailure;
+import org.elasticsearch.action.admin.cluster.node.tasks.list.ListTasksResponse;
 import org.elasticsearch.client.internal.node.NodeClient;
+import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.Scope;
 import org.elasticsearch.rest.ServerlessScope;
 import org.elasticsearch.tasks.TaskId;
+import org.elasticsearch.tasks.TaskInfo;
 
 import java.util.List;
 import java.util.function.Supplier;
@@ -25,10 +31,15 @@ import static org.elasticsearch.rest.action.admin.cluster.RestListTasksAction.li
 
 @ServerlessScope(Scope.INTERNAL)
 public class RestReindexRethrottleAction extends BaseRestHandler {
-    private final Supplier<DiscoveryNodes> nodesInCluster;
 
-    public RestReindexRethrottleAction(Supplier<DiscoveryNodes> nodesInCluster) {
+    static final String REDACTED_NODE_ID_IN_STATELESS = "stateless";
+
+    private final Supplier<DiscoveryNodes> nodesInCluster;
+    private final boolean isStateless;
+
+    public RestReindexRethrottleAction(Supplier<DiscoveryNodes> nodesInCluster, Settings settings) {
         this.nodesInCluster = nodesInCluster;
+        this.isStateless = DiscoveryNode.isStateless(settings);
     }
 
     @Override
@@ -50,11 +61,46 @@ public class RestReindexRethrottleAction extends BaseRestHandler {
             throw new IllegalArgumentException("requests_per_second is a required parameter");
         }
         internalRequest.setRequestsPerSecond(requestsPerSecond);
-        final String groupBy = request.param("group_by", "nodes");
-        return channel -> client.execute(
-            ReindexPlugin.RETHROTTLE_ACTION,
-            internalRequest,
-            listTasksResponseListener(nodesInCluster, groupBy, channel)
+        // This ListTasksResponse will only ever contain a single task, so grouping them is not very useful.
+        // In stateful, we allow the group_by parameter and default to "nodes", for historical reasons.
+        // In stateless, we don't allow group_by, we never group, and we redact the node IDs: this minimizes the visibility of node IDs.
+        final String groupBy = isStateless ? "none" : request.param("group_by", "nodes");
+        return channel -> {
+            ActionListener<ListTasksResponse> responseListener = listTasksResponseListener(nodesInCluster, groupBy, channel);
+            client.execute(
+                ReindexPlugin.RETHROTTLE_ACTION,
+                internalRequest,
+                isStateless ? responseListener.map(RestReindexRethrottleAction::redactNodeIdsInListTasksResponse) : responseListener
+            );
+        };
+    }
+
+    private static ListTasksResponse redactNodeIdsInListTasksResponse(ListTasksResponse originalResponse) {
+        return new ListTasksResponse(
+            originalResponse.getTasks().stream().map(RestReindexRethrottleAction::redactNodeIdInTaskInfo).toList(),
+            originalResponse.getTaskFailures().stream().map(RestReindexRethrottleAction::redactNodeIdInTaskOperationFailure).toList(),
+            originalResponse.getNodeFailures()
         );
+    }
+
+    private static TaskInfo redactNodeIdInTaskInfo(TaskInfo originalTaskInfo) {
+        return new TaskInfo(
+            originalTaskInfo.taskId(),
+            originalTaskInfo.type(),
+            REDACTED_NODE_ID_IN_STATELESS,
+            originalTaskInfo.action(),
+            originalTaskInfo.description(),
+            originalTaskInfo.status(),
+            originalTaskInfo.startTime(),
+            originalTaskInfo.runningTimeNanos(),
+            originalTaskInfo.cancellable(),
+            originalTaskInfo.cancelled(),
+            originalTaskInfo.parentTaskId(),
+            originalTaskInfo.headers()
+        );
+    }
+
+    private static TaskOperationFailure redactNodeIdInTaskOperationFailure(TaskOperationFailure taskOperationFailure) {
+        return new TaskOperationFailure(REDACTED_NODE_ID_IN_STATELESS, taskOperationFailure.getTaskId(), taskOperationFailure.getCause());
     }
 }

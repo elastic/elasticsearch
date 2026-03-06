bc655f060962ef4021751128be8d3cf0ce1f27ea
Iraklis Psaroudakis <kingherc@gmail.com>
2026-03-06 13:13:26 +0200

    Fix LocalStateInferencePlugin test

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/LocalStateInferencePlugin.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/LocalStateInferencePlugin.java
@@ -56,6 +56,11 @@ public class LocalStateInferencePlugin extends LocalStateCompositeXPackPlugin {
                     TestStreamingCompletionServiceExtension.TestInferenceService::new
                 );
             }
+
+            @Override
+            public void loadExtensions(ExtensionLoader loader) {
+                // Nothing, else it would clash with super class which already loads inference services
+            }
         };
         plugins.add(inferencePlugin);
     }

5b9b89124d23128bb11cc6b95d9dd366eb93c2fa
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-03-08 11:41:17 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/node/NodeConstruction.java
+++ server/src/main/java/org/elasticsearch/node/NodeConstruction.java
@@ -749,18 +749,15 @@ class NodeConstruction {
 
         FailureStoreMetrics failureStoreMetrics = new FailureStoreMetrics(telemetryProvider.getMeterRegistry());
         MatcherWatchdog matcherWatchdog = IngestService.createGrokThreadWatchdog(environment, threadPool);
-        List<UserAgentParserRegistryProvider> userAgentParserProviders = pluginsService.filterPlugins(
-            UserAgentParserRegistryProvider.class
-        ).toList();
+        List<UserAgentParserRegistryProvider> userAgentParserProviders = pluginsService.filterPlugins(UserAgentParserRegistryProvider.class)
+            .toList();
         final UserAgentParserRegistry userAgentParserRegistry;
         if (userAgentParserProviders.isEmpty()) {
             userAgentParserRegistry = UserAgentParserRegistry.NOOP;
         } else if (userAgentParserProviders.size() == 1) {
             userAgentParserRegistry = userAgentParserProviders.getFirst().createUserAgentParserRegistry(environment);
         } else {
-            throw new IllegalStateException(
-                "Multiple UserAgentParserRegistryProvider implementations found: " + userAgentParserProviders
-            );
+            throw new IllegalStateException("Multiple UserAgentParserRegistryProvider implementations found: " + userAgentParserProviders);
         }
         final IngestService ingestService = new IngestService(
             clusterService,

9f36c6051995a70d88196fec8d9485cc62ed48a9
Ines Potier <34454302+inespot@users.noreply.github.com>
2026-03-09 08:30:28 -0400

    Another fix to StatelessPrefetchIT::testSearchShardsStarted (#5767)
    * Another fix to StatelessPrefetchIT::testSearchShardsStarted
    
    Background prefetching is enabled by default which means index refresh won't wait for the search shard prefetching to complete. Explicitly wait for the last offset to be fetched before blocking the blob store and running the search.
    
    Closes #5640
    
    * Wait for thread pool task completion instead
    
    * Spotless oops
    
    * getTotalPrefetchedBytes only counts prefetched bytes from cache misses
    
    * Search commit gen barrier, prefetchNonUploadedCommits and drain prewarm pool
    
    * Clean debugging stuff

--- x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/StatelessPrefetchIT.java
+++ x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/StatelessPrefetchIT.java
@@ -25,6 +25,9 @@ import org.elasticsearch.common.util.CollectionUtils;
 import org.elasticsearch.index.query.MatchAllQueryBuilder;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.snapshots.mockstore.MockRepository;
+import org.elasticsearch.threadpool.ThreadPool;
+import org.elasticsearch.threadpool.ThreadPoolStats;
+import org.elasticsearch.xpack.stateless.cache.SearchCommitPrefetcher;
 import org.elasticsearch.xpack.stateless.cache.StatelessOnlinePrewarmingService;
 import org.elasticsearch.xpack.stateless.objectstore.ObjectStoreService;
 
@@ -33,6 +36,8 @@ import java.util.Locale;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertHitCount;
 import static org.elasticsearch.xpack.stateless.objectstore.ObjectStoreTestUtils.getObjectStoreMockRepository;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
 
 public class StatelessPrefetchIT extends AbstractStatelessPluginIntegTestCase {
 
@@ -54,7 +59,7 @@ public class StatelessPrefetchIT extends AbstractStatelessPluginIntegTestCase {
     public void testSearchShardsStarted() throws Exception {
         startMasterAndIndexNode();
         // create large enough cache with small enough pages on the search node to allow prefetching all data
-        String searchNode = startSearchNode(
+        final String searchNode = startSearchNode(
             Settings.builder()
                 .put(
                     SharedBlobCacheService.SHARED_CACHE_SIZE_SETTING.getKey(),
@@ -64,28 +69,52 @@ public class StatelessPrefetchIT extends AbstractStatelessPluginIntegTestCase {
                     SharedBlobCacheService.SHARED_CACHE_REGION_SIZE_SETTING.getKey(),
                     ByteSizeValue.of(256, ByteSizeUnit.KB).getStringRep()
                 )
+                // The non-uploaded notification also triggers prefetchLatestCommit
+                .put(SearchCommitPrefetcher.PREFETCH_NON_UPLOADED_COMMITS_SETTING.getKey(), true)
                 .build()
         );
-        final String indexName = randomAlphaOfLength(10).toLowerCase(Locale.ROOT);
+        final var indexName = randomAlphaOfLength(10).toLowerCase(Locale.ROOT);
         createIndex(indexName, indexSettings(1, 1).build());
         ensureGreen(indexName);
         // Break the idle barrier. Otherwise, prefetching is skipped because the shard is considered idle.
-        // When max_commits is low enough that BCCs are uploaded, segment data reads route to the object store.
-        // If the data wasn't prefetched into the SharedBlobCache, the subsequent repository block causes the search to hang.
         assertHitCount(client().prepareSearch(indexName).setQuery(new MatchAllQueryBuilder()), 0);
-        int rounds = randomIntBetween(1, 4);
+
+        final ThreadPool threadPool = internalCluster().getInstance(ThreadPool.class, searchNode);
+        final long prevCompletedStats = getThreadPoolStats(threadPool, StatelessPlugin.PREWARM_THREAD_POOL).completed();
+
+        final int rounds = randomIntBetween(1, 4);
         int totalDocs = 0;
         for (int i = 0; i < rounds; i++) {
             int cnt = randomIntBetween(10, 100);
             indexDocsAndRefresh(indexName, cnt);
             totalDocs += cnt;
+            // Wait until the search shard commit generation is updated, ensuring it has received the
+            // index shard's commit notification and that prefetch has been triggered for that commit.
+            final long expectedGeneration = findIndexShard(indexName).commitStats().getGeneration();
+            assertBusy(() -> assertThat(findSearchShard(indexName).commitStats().getGeneration(), equalTo(expectedGeneration)));
         }
 
+        // Wait for the PREWARM pool to drain, to ensure prefetching has completed.
+        assertNoRunningAndQueuedTasks(threadPool, StatelessPlugin.PREWARM_THREAD_POOL, prevCompletedStats);
+
         logger.info("--> blocking repository");
-        var mockRepository = getObjectStoreMockRepository(getObjectStoreService(searchNode));
+        final var mockRepository = getObjectStoreMockRepository(getObjectStoreService(searchNode));
         mockRepository.setBlockOnAnyFiles();
         logger.info("--> running search and verifying that the repository was not accessed because prefetching warmed the cache already");
         assertHitCount(client().prepareSearch().setQuery(new MatchAllQueryBuilder()), totalDocs);
         assertFalse(mockRepository.blocked());
+    }
+
+    private static ThreadPoolStats.Stats getThreadPoolStats(ThreadPool threadPool, String poolName) {
+        return threadPool.stats().stats().stream().filter(s -> s.name().equals(poolName)).findFirst().orElseThrow();
+    }
+
+    private void assertNoRunningAndQueuedTasks(ThreadPool threadPool, String poolName, long previouslyObservedCompletedTasks)
+        throws Exception {
+        assertBusy(() -> {
+            final var stats = getThreadPoolStats(threadPool, poolName);
+            assertThat(stats.completed(), greaterThan(previouslyObservedCompletedTasks));
+            assertThat(stats.active() + stats.queue(), equalTo(0));
+        });
     }
 }

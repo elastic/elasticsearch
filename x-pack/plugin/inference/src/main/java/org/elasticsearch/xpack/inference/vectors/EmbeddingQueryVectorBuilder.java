81621f6ae7450111184465d3d723b476887cc8ef
Mike Pellegrini <mike.pellegrini@elastic.co>
2026-03-09 16:15:02 -0400

    Update EmbeddingQueryVectorBuilder to take InferenceStringGroup

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/vectors/EmbeddingQueryVectorBuilder.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/vectors/EmbeddingQueryVectorBuilder.java
@@ -15,8 +15,6 @@ import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.core.Nullable;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.features.NodeFeature;
-import org.elasticsearch.inference.DataFormat;
-import org.elasticsearch.inference.DataType;
 import org.elasticsearch.inference.EmbeddingRequest;
 import org.elasticsearch.inference.InferenceResults;
 import org.elasticsearch.inference.InferenceString;
@@ -27,17 +25,20 @@ import org.elasticsearch.search.vectors.QueryVectorBuilder;
 import org.elasticsearch.xcontent.ConstructingObjectParser;
 import org.elasticsearch.xcontent.ParseField;
 import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xcontent.XContentParseException;
 import org.elasticsearch.xcontent.XContentParser;
 import org.elasticsearch.xpack.core.inference.action.EmbeddingAction;
 import org.elasticsearch.xpack.core.inference.action.InferenceAction;
 import org.elasticsearch.xpack.core.ml.inference.results.MlDenseEmbeddingResults;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import static org.elasticsearch.xcontent.ConstructingObjectParser.constructorArg;
 import static org.elasticsearch.xcontent.ConstructingObjectParser.optionalConstructorArg;
+import static org.elasticsearch.xcontent.ObjectParser.ValueType.OBJECT_ARRAY_OR_STRING;
 import static org.elasticsearch.xpack.core.ClientHelper.INFERENCE_ORIGIN;
 import static org.elasticsearch.xpack.core.ClientHelper.executeAsyncWithOrigin;
 
@@ -45,9 +46,7 @@ public class EmbeddingQueryVectorBuilder implements QueryVectorBuilder {
     public static final String NAME = "embedding";
 
     public static final ParseField INFERENCE_ID_FIELD = new ParseField("inference_id");
-    public static final ParseField TYPE_FIELD = new ParseField("type");
-    public static final ParseField FORMAT_FIELD = new ParseField("format");
-    public static final ParseField VALUE_FIELD = new ParseField("value");
+    public static final ParseField INPUT_FIELD = new ParseField("input");
     public static final ParseField TIMEOUT_FIELD = new ParseField("timeout");
 
     public static final TimeValue DEFAULT_TIMEOUT = InferenceAction.Request.DEFAULT_TIMEOUT;
@@ -58,18 +57,14 @@ public class EmbeddingQueryVectorBuilder implements QueryVectorBuilder {
         NAME,
         args -> new EmbeddingQueryVectorBuilder(
             (String) args[0],
-            (DataType) args[1],
-            (DataFormat) args[2],
-            (String) args[3],
-            args[4] == null ? null : TimeValue.parseTimeValue((String) args[4], TIMEOUT_FIELD.getPreferredName())
+            (InferenceStringGroup) args[1],
+            args[2] == null ? null : TimeValue.parseTimeValue((String) args[2], TIMEOUT_FIELD.getPreferredName())
         )
     );
 
     static {
         PARSER.declareString(optionalConstructorArg(), INFERENCE_ID_FIELD);
-        PARSER.declareString(constructorArg(), DataType::fromString, TYPE_FIELD);
-        PARSER.declareString(optionalConstructorArg(), DataFormat::fromString, FORMAT_FIELD);
-        PARSER.declareString(constructorArg(), VALUE_FIELD);
+        PARSER.declareField(constructorArg(), (p, c) -> parseInput(p), INPUT_FIELD, OBJECT_ARRAY_OR_STRING);
         PARSER.declareString(optionalConstructorArg(), TIMEOUT_FIELD);
     }
 
@@ -77,33 +72,37 @@ public class EmbeddingQueryVectorBuilder implements QueryVectorBuilder {
         return PARSER.parse(parser, null);
     }
 
+    private static InferenceStringGroup parseInput(XContentParser parser) throws IOException {
+        var token = parser.currentToken();
+        if (token == XContentParser.Token.VALUE_STRING) {
+            return new InferenceStringGroup(parser.text());
+        } else if (token == XContentParser.Token.START_OBJECT) {
+            return new InferenceStringGroup(InferenceString.PARSER.apply(parser, null));
+        } else if (token == XContentParser.Token.START_ARRAY) {
+            List<InferenceString> inferenceStrings = new ArrayList<>();
+            while (parser.nextToken() != XContentParser.Token.END_ARRAY) {
+                inferenceStrings.add(InferenceString.PARSER.apply(parser, null));
+            }
+            return new InferenceStringGroup(inferenceStrings);
+        }
+        throw new XContentParseException(parser.getTokenLocation(), "Unsupported token [" + token + "]");
+    }
+
     private static final TransportVersion EMBEDDING_QUERY_VECTOR_BUILDER_TV = TransportVersion.fromName("embedding_query_vector_builder");
 
     private final String inferenceId;
-    private final DataType type;
-    private final DataFormat format;
-    private final String value;
+    private final InferenceStringGroup input;
     private final TimeValue timeout;
 
-    public EmbeddingQueryVectorBuilder(
-        @Nullable String inferenceId,
-        DataType type,
-        @Nullable DataFormat format,
-        String value,
-        @Nullable TimeValue timeout
-    ) {
+    public EmbeddingQueryVectorBuilder(@Nullable String inferenceId, InferenceStringGroup input, @Nullable TimeValue timeout) {
         this.inferenceId = inferenceId;
-        this.type = Objects.requireNonNull(type);
-        this.format = format;
-        this.value = Objects.requireNonNull(value);
+        this.input = Objects.requireNonNull(input);
         this.timeout = timeout;
     }
 
     public EmbeddingQueryVectorBuilder(StreamInput in) throws IOException {
         this.inferenceId = in.readOptionalString();
-        this.type = in.readEnum(DataType.class);
-        this.format = in.readOptionalEnum(DataFormat.class);
-        this.value = in.readString();
+        this.input = new InferenceStringGroup(in);
         this.timeout = in.readOptionalTimeValue();
     }
 
@@ -114,8 +113,7 @@ public class EmbeddingQueryVectorBuilder implements QueryVectorBuilder {
             return;
         }
 
-        var inferenceString = new InferenceString(type, format, value);
-        var embeddingRequest = new EmbeddingRequest(List.of(new InferenceStringGroup(inferenceString)), InputType.SEARCH, null);
+        var embeddingRequest = new EmbeddingRequest(List.of(input), InputType.SEARCH, null);
         var actualTimeout = Objects.requireNonNullElse(timeout, DEFAULT_TIMEOUT);
         var request = new EmbeddingAction.Request(inferenceId, TaskType.ANY, embeddingRequest, actualTimeout);
         executeAsyncWithOrigin(
@@ -164,9 +162,7 @@ public class EmbeddingQueryVectorBuilder implements QueryVectorBuilder {
     @Override
     public void writeTo(StreamOutput out) throws IOException {
         out.writeOptionalString(inferenceId);
-        out.writeEnum(type);
-        out.writeOptionalEnum(format);
-        out.writeString(value);
+        out.writeWriteable(input);
         out.writeOptionalTimeValue(timeout);
     }
 
@@ -176,11 +172,7 @@ public class EmbeddingQueryVectorBuilder implements QueryVectorBuilder {
         if (inferenceId != null) {
             builder.field(INFERENCE_ID_FIELD.getPreferredName(), inferenceId);
         }
-        builder.field(TYPE_FIELD.getPreferredName(), type);
-        if (format != null) {
-            builder.field(FORMAT_FIELD.getPreferredName(), format);
-        }
-        builder.field(VALUE_FIELD.getPreferredName(), value);
+        builder.field(INPUT_FIELD.getPreferredName(), input);
         if (timeout != null) {
             builder.field(TIMEOUT_FIELD.getPreferredName(), timeout.getStringRep());
         }
@@ -192,16 +184,8 @@ public class EmbeddingQueryVectorBuilder implements QueryVectorBuilder {
         return inferenceId;
     }
 
-    public DataType getType() {
-        return type;
-    }
-
-    public DataFormat getFormat() {
-        return format;
-    }
-
-    public String getValue() {
-        return value;
+    public InferenceStringGroup getInput() {
+        return input;
     }
 
     public TimeValue getTimeout() {
@@ -213,15 +197,11 @@ public class EmbeddingQueryVectorBuilder implements QueryVectorBuilder {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         EmbeddingQueryVectorBuilder that = (EmbeddingQueryVectorBuilder) o;
-        return Objects.equals(inferenceId, that.inferenceId)
-            && type == that.type
-            && format == that.format
-            && Objects.equals(value, that.value)
-            && Objects.equals(timeout, that.timeout);
+        return Objects.equals(inferenceId, that.inferenceId) && Objects.equals(input, that.input) && Objects.equals(timeout, that.timeout);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(inferenceId, type, format, value, timeout);
+        return Objects.hash(inferenceId, input, timeout);
     }
 }

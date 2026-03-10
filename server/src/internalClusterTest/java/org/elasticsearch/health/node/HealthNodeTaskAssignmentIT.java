6ab11d602e5439468f5df811b0bb26719b00ff7c
Ines Potier <ines.potier@elastic.co>
2026-03-09 22:35:04 -0400

    How did this get duplicated

--- server/src/internalClusterTest/java/org/elasticsearch/health/node/HealthNodeTaskAssignmentIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/health/node/HealthNodeTaskAssignmentIT.java
@@ -33,21 +33,10 @@ public class HealthNodeTaskAssignmentIT extends ESIntegTestCase {
 
     private static final DiskHealthInfo GREEN_DISK_HEALTH = new DiskHealthInfo(HealthStatus.GREEN, null);
 
-    @Override
-    public void tearDown() throws Exception {
-        updateClusterSettings(
-            Settings.builder()
-                .putNull(LocalHealthMonitor.POLL_INTERVAL_SETTING.getKey())
-                .putNull(HealthNodeTaskExecutor.ENABLED_SETTING.getKey())
-        );
-        super.tearDown();
-    }
-
     /**
      * Verifies that the health node task is always assigned and succeeds as expected during rolling node shutdowns.
      */
     public void testHealthNodeReassignedOnShutdown() throws Exception {
-        decreasePollingInterval();
         final var initialHealthNode = waitAndGetHealthNode(internalCluster());
         assertNotNull("health task must be assigned after cluster starts", initialHealthNode);
 
@@ -58,23 +47,7 @@ public class HealthNodeTaskAssignmentIT extends ESIntegTestCase {
         final int passes = randomIntBetween(2, 4);
         for (int pass = 0; pass < passes; pass++) {
             final String shutdownNodeId = resolveNodeId(currentHealthNodeName);
-
-            final var expectedNode = internalCluster().clusterService()
-                .state()
-                .nodes()
-                .getDataNodes()
-                .values()
-                .stream()
-                .filter(n -> n.getId().equals(shutdownNodeId) == false)
-                .findFirst()
-                .orElseThrow(() -> new AssertionError("no other data node to reassign health task to"));
-
-            logger.info(
-                "Pass {}: marking [{}] for shutdown, expecting health task on [{}]",
-                pass,
-                currentHealthNodeName,
-                expectedNode.getName()
-            );
+            logger.info("Pass {}: marking [{}] for shutdown, expecting health task to be reassigned", pass, currentHealthNodeName);
 
             NodeShutdownTestUtils.putShutdownMetadata(
                 currentHealthNodeName,
@@ -89,15 +62,15 @@ public class HealthNodeTaskAssignmentIT extends ESIntegTestCase {
                 awaitClusterState(state -> {
                     final var healthNode = HealthNode.findHealthNode(state);
                     assertNotNull("health task must never become unassigned", healthNode);
-                    return healthNode.getId().equals(expectedNode.getId());
+                    return healthNode.getId().equals(shutdownNodeId) == false;
                 });
 
                 final var newHealthNode = HealthNode.findHealthNode(internalCluster().clusterService().state());
                 assertNotNull("health task must never be unassigned", newHealthNode);
-                assertThat(
-                    "health task must be on [" + expectedNode.getName() + "] after [" + currentHealthNodeName + "] is marked for shutdown",
+                assertNotEquals(
+                    "health task must be reassigned after [" + currentHealthNodeName + "] is marked for shutdown",
                     newHealthNode.getId(),
-                    equalTo(expectedNode.getId())
+                    shutdownNodeId
                 );
 
                 NodeShutdownTestUtils.clearShutdownMetadata(internalCluster().getCurrentMasterNodeInstance(ClusterService.class));
@@ -114,7 +87,6 @@ public class HealthNodeTaskAssignmentIT extends ESIntegTestCase {
      * and that re-enabling it recreates and reassigns the task
      */
     public void testHealthNodeTaskEnabledAndDisabled() throws Exception {
-        decreasePollingInterval();
         final var initialHealthNode = waitAndGetHealthNode(internalCluster());
         assertNotNull("health task must be assigned on startup", initialHealthNode);
         waitForAllNodesToReportHealthy(initialHealthNode.getName());
@@ -133,16 +105,6 @@ public class HealthNodeTaskAssignmentIT extends ESIntegTestCase {
         } finally {
             updateClusterSettings(Settings.builder().putNull(HealthNodeTaskExecutor.ENABLED_SETTING.getKey()));
         }
-    }
-
-    private void decreasePollingInterval() {
-        updateClusterSettings(
-            Settings.builder().put(LocalHealthMonitor.POLL_INTERVAL_SETTING.getKey(), LocalHealthMonitor.MIN_POLL_INTERVAL)
-        );
-    }
-
-    private void resetPollingInterval() {
-        updateClusterSettings(Settings.builder().putNull(HealthNodeTaskExecutor.ENABLED_SETTING.getKey()));
     }
 
     private String resolveNodeId(String nodeName) {

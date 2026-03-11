5ee5f1924d6586245b087efd873927e5ca774b21
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2026-03-11 15:03:37 -0400

    updating next format as well

--- server/src/main/java/org/elasticsearch/index/codec/vectors/diskbbq/next/ESNextDiskBBQVectorsFormat.java
+++ server/src/main/java/org/elasticsearch/index/codec/vectors/diskbbq/next/ESNextDiskBBQVectorsFormat.java
@@ -76,7 +76,7 @@ public class ESNextDiskBBQVectorsFormat extends KnnVectorsFormat {
         bfloat16VectorFormat
     );
 
-    public static final int DEFAULT_VECTORS_PER_CLUSTER = 384;
+    public static final int DEFAULT_VECTORS_PER_CLUSTER = 256;
     private static final int DEFAULT_FLAT_VECTOR_THRESHOLD_MULTIPLIER = 3;
 
     /**
@@ -90,9 +90,9 @@ public class ESNextDiskBBQVectorsFormat extends KnnVectorsFormat {
 
     public static final int MIN_VECTORS_PER_CLUSTER = 64;
     public static final int MAX_VECTORS_PER_CLUSTER = 1 << 16; // 65536
-    public static final int DEFAULT_CENTROIDS_PER_PARENT_CLUSTER = 16;
+    public static final int DEFAULT_CENTROIDS_PER_PARENT_CLUSTER = 128;
     public static final int MIN_CENTROIDS_PER_PARENT_CLUSTER = 2;
-    public static final int MAX_CENTROIDS_PER_PARENT_CLUSTER = DEFAULT_VECTORS_PER_CLUSTER; // 384
+    public static final int MAX_CENTROIDS_PER_PARENT_CLUSTER = MAX_VECTORS_PER_CLUSTER;
     public static final int DEFAULT_PRECONDITIONING_BLOCK_DIMENSION = 32;
     public static final int MIN_PRECONDITIONING_BLOCK_DIMS = 8;
     public static final int MAX_PRECONDITIONING_BLOCK_DIMS = 384;

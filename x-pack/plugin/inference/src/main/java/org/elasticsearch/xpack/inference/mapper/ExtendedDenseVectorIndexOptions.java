636b8933c6bed315014e478ce5569f99fe8e1962
Mike Pellegrini <mike.pellegrini@elastic.co>
2026-03-11 15:26:32 -0400

    Implement equals and hashCode

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/mapper/ExtendedDenseVectorIndexOptions.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/mapper/ExtendedDenseVectorIndexOptions.java
@@ -13,6 +13,7 @@ import org.elasticsearch.xcontent.ParseField;
 import org.elasticsearch.xcontent.XContentBuilder;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class ExtendedDenseVectorIndexOptions implements IndexOptions {
     public static final ParseField ELEMENT_TYPE_FIELD = new ParseField("element_type");
@@ -24,13 +25,26 @@ public class ExtendedDenseVectorIndexOptions implements IndexOptions {
         DenseVectorFieldMapper.DenseVectorIndexOptions baseIndexOptions,
         DenseVectorFieldMapper.ElementType elementType
     ) {
-        this.baseIndexOptions = baseIndexOptions;
-        this.elementType = elementType;
+        this.baseIndexOptions = Objects.requireNonNull(baseIndexOptions);
+        this.elementType = Objects.requireNonNull(elementType);
     }
 
     @Override
     public void toXContentFragment(XContentBuilder builder, Params params) throws IOException {
         baseIndexOptions.toXContentFragment(builder, params);
         builder.field(ELEMENT_TYPE_FIELD.getPreferredName(), elementType.toString());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ExtendedDenseVectorIndexOptions that = (ExtendedDenseVectorIndexOptions) o;
+        return Objects.equals(baseIndexOptions, that.baseIndexOptions) && elementType == that.elementType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(baseIndexOptions, elementType);
     }
 }

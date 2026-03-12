91362c093a92862aa1c71ca2a1f5e347cbb96fda
Ioana Tagirta <ioanatia@users.noreply.github.com>
2026-03-12 11:37:03 +0100

    ES|QL: Fix MagnitudeSerializationTests release tests (#144088)

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/vector/MagnitudeSerializationTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/vector/MagnitudeSerializationTests.java
@@ -7,11 +7,19 @@
 
 package org.elasticsearch.xpack.esql.expression.function.vector;
 
+import org.elasticsearch.xpack.esql.action.EsqlCapabilities;
 import org.elasticsearch.xpack.esql.core.expression.Expression;
 import org.elasticsearch.xpack.esql.core.tree.Source;
 import org.elasticsearch.xpack.esql.expression.AbstractUnaryScalarSerializationTests;
+import org.junit.Before;
 
 public class MagnitudeSerializationTests extends AbstractUnaryScalarSerializationTests<Magnitude> {
+
+    @Before
+    public void checkCapability() {
+        assumeTrue("v_magnitude available in snapshot", EsqlCapabilities.Cap.MAGNITUDE_SCALAR_VECTOR_FUNCTION.isEnabled());
+    }
+
     @Override
     protected Magnitude create(Source source, Expression child) {
         return new Magnitude(source, child);

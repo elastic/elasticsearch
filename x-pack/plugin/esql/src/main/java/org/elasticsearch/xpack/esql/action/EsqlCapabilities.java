b1fe47e827cc1f0c67eb7b0aee50b0a1db5f6927
Lefteris Zervakis <lefteris.zervakis@elastic.co>
2026-03-12 11:32:08 +0000

    Added qstr_function_boost_fix capability

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -803,7 +803,10 @@ public class EsqlCapabilities {
          * QSTR function
          */
         QSTR_FUNCTION,
-
+        /**
+         * Guards a fix for the boost parameter in QueryString queries
+         */
+        QSTR_FUNCTION_BOOST_FIX,
         /**
          * MATCH function
          */

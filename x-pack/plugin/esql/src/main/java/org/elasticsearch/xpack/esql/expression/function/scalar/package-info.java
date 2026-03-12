e71801fea9a5822f35f3d13cb59680bb360e7a70
Nik Everett <nik9000@gmail.com>
2026-03-12 16:35:22 -0400

    ESQL: Spot check telemetry `features`
    Switches the count-based tests for ESQL's telementry's `features`
    section from a `count` based test to a spot checked one - we test for
    the things we expect to be there.
    
    Also adds examples for how to run these tests - both to the
    `FeatureMetric` spot for features and to the scalar function writing
    guide at `scalar/package-info.java`.
    
    Relates to #144086

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/package-info.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/expression/function/scalar/package-info.java
@@ -196,6 +196,16 @@
  *         function in the list and follow its link to get to the page you built. Make sure it looks ok.
  *     </li>
  *     <li>
+ *         You automatically got usage telemetry for your new function. Rejoice! Now add an assertion to
+ *         {@code 60_usage.yml} around {@snippet lang=yaml :
+ *          - exists: esql.functions.delay} to prove it.
+ *         Then run the tests with:
+ *         {@snippet lang = shell:
+ *         ./gradlew -p x-pack/plugin/esql/qa/server/single-node/ yamlRestTest \
+ *             -Dtests.method='*60*'
+ *         }
+ *     </li>
+ *     <li>
  *         Let’s finish up the code by making the tests backwards compatible. Since this is a new
  *         feature we just have to convince the tests not to run in a cluster that includes older
  *         versions of Elasticsearch. We do that with a {@link org.elasticsearch.rest.RestHandler#supportedCapabilities capability}
@@ -225,14 +235,19 @@
  *             <li>The class that implements your new functions has the right annotations.</li>
  *         </ul>
  *
- *         When it comes to the correct syntax, e.g.: annotations, groupings, etc., feel free to look at existing snapshot functions and
- *         Git history.
+ *         When it comes to the correct syntax, e.g.: annotations, groupings, etc., feel free to look at
+ *         existing snapshot functions and Git history.
  *
  *         If your function should be available in snapshot <strong>only</strong>, add it to the list in
- *         {@code x-pack/plugin/src/yamlRestTest/resources/rest-api-spec/test/esql/60_usage.yml} around
- *         {@code not_exists: esql.functions.delay}. The release build will fail if this function is
- *         available. Check out the instructions for running a release build in {@code testing.asciidoc}
- *         if you want to test this, but it's generally enough to let CI do it.
+ *         {@code 60_usage.yml} around {@snippet lang=yaml :
+ *         - not_exists: esql.functions.delay}. The release build will fail if this function is
+ *         available. Run these tests with:
+ *         {@snippet lang = shell:
+ *         ./gradlew -p x-pack/plugin/esql/qa/server/single-node/ yamlRestTest \
+ *             -Dbuild.snapshot=false -Dtests.jvm.argline=-Dbuild.snapshot=false \
+ *             -Dlicense.key="x-pack/license-tools/src/test/resources/public.key" \
+ *             -Dtests.method='*60*'
+ *         }
  *     </li>
  *     <li>
  *         Open the PR. The subject and description of the PR are important because those'll turn

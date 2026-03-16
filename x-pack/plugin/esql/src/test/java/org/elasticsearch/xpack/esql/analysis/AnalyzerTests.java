2321aaf435f95852b40aa1b9ed8ca92768fa7d94
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-03-16 14:37:13 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/analysis/AnalyzerTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/analysis/AnalyzerTests.java
@@ -7,8 +7,6 @@
 
 package org.elasticsearch.xpack.esql.analysis;
 
-import java.time.Instant;
-
 import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.Build;
 import org.elasticsearch.TransportVersion;
@@ -124,6 +122,7 @@ import org.elasticsearch.xpack.esql.session.IndexResolver;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.time.Period;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;

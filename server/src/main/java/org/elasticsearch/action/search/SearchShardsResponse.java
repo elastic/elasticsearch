eccfeef9d81c1289e6f0f63aa92197d44208e452
Eran Weiss <eran.weiss@elastic.co>
2026-03-17 15:33:31 -0400

    Revert "CanMatch returns numSkipped per cluster instead of all skipped shards (#142170)"
    This reverts commit 8d117cc41154f1fb16b170f4516e7c2e3f208cb8.

--- server/src/main/java/org/elasticsearch/action/search/SearchShardsResponse.java
+++ server/src/main/java/org/elasticsearch/action/search/SearchShardsResponse.java
@@ -36,24 +36,20 @@ import java.util.Objects;
  */
 public final class SearchShardsResponse extends ActionResponse {
     private final Collection<SearchShardsGroup> groups;
-    private final int numSkippedShards;
     private final Collection<DiscoveryNode> nodes;
     private final Map<String, AliasFilter> aliasFilters;
     private final ResolvedIndexExpressions resolvedIndexExpressions;
     public static final TransportVersion SEARCH_SHARDS_RESOLVED_INDEX_EXPRESSIONS = TransportVersion.fromName(
         "search_shards_resolved_index_expressions"
     );
-    public static final TransportVersion SEARCH_SHARDS_NUM_SKIPPED = TransportVersion.fromName("search_shards_num_skipped");
 
     public SearchShardsResponse(
         Collection<SearchShardsGroup> groups,
-        int numSkippedShards,
         Collection<DiscoveryNode> nodes,
         Map<String, AliasFilter> aliasFilters,
         @Nullable ResolvedIndexExpressions resolvedIndexExpressions
     ) {
         this.groups = groups;
-        this.numSkippedShards = numSkippedShards;
         this.nodes = nodes;
         this.aliasFilters = aliasFilters;
         this.resolvedIndexExpressions = resolvedIndexExpressions;
@@ -61,11 +57,10 @@ public final class SearchShardsResponse extends ActionResponse {
 
     public SearchShardsResponse(
         Collection<SearchShardsGroup> groups,
-        int numSkippedShards,
         Collection<DiscoveryNode> nodes,
         Map<String, AliasFilter> aliasFilters
     ) {
-        this(groups, numSkippedShards, nodes, aliasFilters, null);
+        this(groups, nodes, aliasFilters, null);
     }
 
     public SearchShardsResponse(StreamInput in) throws IOException {
@@ -77,11 +72,6 @@ public final class SearchShardsResponse extends ActionResponse {
         } else {
             this.resolvedIndexExpressions = null;
         }
-        if (in.getTransportVersion().supports(SEARCH_SHARDS_NUM_SKIPPED)) {
-            this.numSkippedShards = in.readVInt();
-        } else {
-            this.numSkippedShards = 0;
-        }
     }
 
     @Override
@@ -91,11 +81,6 @@ public final class SearchShardsResponse extends ActionResponse {
         out.writeMap(aliasFilters, StreamOutput::writeWriteable);
         if (out.getTransportVersion().supports(SEARCH_SHARDS_RESOLVED_INDEX_EXPRESSIONS)) {
             out.writeOptionalWriteable(resolvedIndexExpressions);
-        }
-        if (out.getTransportVersion().supports(SEARCH_SHARDS_NUM_SKIPPED)) {
-            out.writeVInt(numSkippedShards);
-        } else {
-            assert numSkippedShards == 0;
         }
     }
 
@@ -111,13 +96,6 @@ public final class SearchShardsResponse extends ActionResponse {
      */
     public Collection<SearchShardsGroup> getGroups() {
         return groups;
-    }
-
-    /**
-     * Number of skipped shards not listed in Groups
-     */
-    public int getNumSkippedShards() {
-        return numSkippedShards;
     }
 
     /**
@@ -154,7 +132,7 @@ public final class SearchShardsResponse extends ActionResponse {
         }
         List<SearchShardsGroup> groups = Arrays.stream(oldResp.getGroups()).map(SearchShardsGroup::new).toList();
         assert groups.stream().noneMatch(SearchShardsGroup::preFiltered) : "legacy responses must not have preFiltered set";
-        return new SearchShardsResponse(groups, 0, Arrays.asList(oldResp.getNodes()), aliasFilters, oldResp.getResolvedIndexExpressions());
+        return new SearchShardsResponse(groups, Arrays.asList(oldResp.getNodes()), aliasFilters, oldResp.getResolvedIndexExpressions());
     }
 
     @Override

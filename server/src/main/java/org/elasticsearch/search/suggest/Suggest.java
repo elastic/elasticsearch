d45254986bc357254332a65eacee884d95005898
Eran Weiss <eran.weiss@elastic.co>
2026-03-18 16:05:58 -0400

    compilation fix

--- server/src/main/java/org/elasticsearch/search/suggest/Suggest.java
+++ server/src/main/java/org/elasticsearch/search/suggest/Suggest.java
@@ -27,6 +27,7 @@ import org.elasticsearch.xcontent.XContentBuilder;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;

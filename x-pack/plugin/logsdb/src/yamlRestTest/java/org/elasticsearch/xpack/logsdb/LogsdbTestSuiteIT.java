845eb19a5a7c466a3031f8f976ed838f407cac4e
Martijn van Groningen <martijn.v.groningen@gmail.com>
2026-03-19 12:00:37 +0100

    Fix ignored_source_as_doc_values feature flag handling in tests.
    Closes #144540
    Closes #144531
    Closes #144530
    Closes #144528
    Closes #144527

--- x-pack/plugin/logsdb/src/yamlRestTest/java/org/elasticsearch/xpack/logsdb/LogsdbTestSuiteIT.java
+++ x-pack/plugin/logsdb/src/yamlRestTest/java/org/elasticsearch/xpack/logsdb/LogsdbTestSuiteIT.java
@@ -15,7 +15,9 @@ import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.core.Booleans;
+import org.elasticsearch.index.mapper.IgnoredSourceFieldMapper;
 import org.elasticsearch.test.cluster.ElasticsearchCluster;
+import org.elasticsearch.test.cluster.FeatureFlag;
 import org.elasticsearch.test.cluster.local.LocalClusterSpecBuilder;
 import org.elasticsearch.test.cluster.local.distribution.DistributionType;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
@@ -39,7 +41,8 @@ public class LogsdbTestSuiteIT extends ESClientYamlSuiteTestCase {
             .setting("xpack.security.enabled", "true")
             .user(USER, PASS)
             .keystore("bootstrap.password", "x-pack-test-password")
-            .setting("xpack.license.self_generated.type", "trial");
+            .setting("xpack.license.self_generated.type", "trial")
+            .feature(FeatureFlag.IGNORED_SOURCE_AS_DOC_VALUES_FF);
         boolean setNodes = Booleans.parseBoolean(System.getProperty("yaml.rest.tests.set_num_nodes", "true"));
         if (setNodes) {
             clusterBuilder.nodes(1);

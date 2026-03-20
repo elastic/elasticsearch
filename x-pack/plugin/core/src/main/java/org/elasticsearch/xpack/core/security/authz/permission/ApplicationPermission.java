e8569b83e50a039c34ae02f54672893352fd9425
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-03-20 22:54:51 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/permission/ApplicationPermission.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/permission/ApplicationPermission.java
@@ -23,7 +23,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;

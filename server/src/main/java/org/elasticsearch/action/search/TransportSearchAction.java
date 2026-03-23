cd3d8dca77bf65393b5af25cc8e9a3b090f58601
Eran Weiss <eran.weiss@elastic.co>
2026-03-23 08:43:16 -0400

    Reapply "Ref-counting SearchHits from InternalTopHits to SearchRespone (#142732) (#144726)
    This reverts commit a474faa8e7ab3e94e9e7abd71e24c1d5b85680d3.

--- server/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
+++ server/src/main/java/org/elasticsearch/action/search/TransportSearchAction.java
@@ -922,7 +922,8 @@ public class TransportSearchAction extends HandledTransportAction<SearchRequest,
                             timeProvider.buildTookInMillis(),
                             searchResponse.getShardFailures(),
                             clusters,
-                            searchResponse.pointInTimeId()
+                            searchResponse.pointInTimeId(),
+                            null
                         )
                     );
                 }

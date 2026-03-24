74a388b6679a13d5d7181d4b07af26357da88f83
Joshua Adams <joshua.adams@elastic.co>
2026-03-24 11:31:25 +0000

    Adds Reindexing Security Tests
    There are now dedicated reindexing APIs to replace calling the tasks
    API with get and cancel for reindexing tasks. This adds a security
    focussed test for the new dedicated endpoint, mirroring an existing test
     "Reindex wait_for_completion=false as minimal with task API".
    
     Relates: https://github.com/elastic/elasticsearch/pull/144784

--- x-pack/qa/reindex-tests-with-security/src/yamlRestTest/java/org/elasticsearch/xpack/security/ReindexWithSecurityIT.java
+++ x-pack/qa/reindex-tests-with-security/src/yamlRestTest/java/org/elasticsearch/xpack/security/ReindexWithSecurityIT.java
@@ -70,6 +70,7 @@ public class ReindexWithSecurityIT extends ESRestTestCase {
             .user("powerful_user", "x-pack-test-password", "superuser", false)
             .user("minimal_user", "x-pack-test-password", "minimal", false)
             .user("minimal_with_task_user", "x-pack-test-password", "minimal_with_task", false)
+            .user("minimal_with_reindex_get_user", "x-pack-test-password", "minimal_with_reindex_get", false)
             .user("readonly_user", "x-pack-test-password", "readonly", false)
             .user("dest_only_user", "x-pack-test-password", "dest_only", false)
             .user("can_not_see_hidden_docs_user", "x-pack-test-password", "can_not_see_hidden_docs", false)

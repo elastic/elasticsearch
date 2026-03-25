85d83efe35742c2078aaf27e1ea30b19a256c40b
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2026-03-25 09:51:49 +0100

    Capability

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -2365,6 +2365,11 @@ public class EsqlCapabilities {
          */
         AUTO_PARTITION_DOCS_THRESHOLD,
 
+        /**
+         * Added telemetry for views
+         */
+        VIEWS_TELEMETRY,
+
         // Last capability should still have a comma for fewer merge conflicts when adding new ones :)
         // This comment prevents the semicolon from being on the previous capability when Spotless formats the file.
         ;

44a139f4b633c9519c0384680ca79d3a72711966
Bogdan Pintea <bogdan.pintea@elastic.co>
2026-03-27 11:52:01 +0100

    Move datasources under a feature flag

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/parser/EsqlConfig.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/parser/EsqlConfig.java
@@ -8,6 +8,7 @@
 package org.elasticsearch.xpack.esql.parser;
 
 import org.elasticsearch.Build;
+import org.elasticsearch.xpack.core.esql.EsqlFeatureFlags;
 import org.elasticsearch.xpack.esql.expression.function.EsqlFunctionRegistry;
 
 public class EsqlConfig {
@@ -27,6 +28,13 @@ public class EsqlConfig {
 
     public boolean isDevVersion() {
         return isDevVersion;
+    }
+
+    /**
+     * Whether the EXTERNAL command and external data source grammar are enabled (feature flag; on by default in snapshot builds).
+     */
+    public boolean isExternalDataSourcesEnabled() {
+        return EsqlFeatureFlags.ESQL_EXTERNAL_DATASOURCES_FEATURE_FLAG.isEnabled();
     }
 
     public EsqlFunctionRegistry functionRegistry() {

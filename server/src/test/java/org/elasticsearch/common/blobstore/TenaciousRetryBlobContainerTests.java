597064d74a910f5d70d859ea41c1c38f92e56c2e
zhubotang-wq <zhubo.tang@elastic.co>
2026-03-27 14:23:36 -0700

    ES-13905 Infinite retry for transient CSP errors

---
+++ server/src/test/java/org/elasticsearch/common/blobstore/TenaciousRetryBlobContainerTests.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+package org.elasticsearch.common.blobstore;
+
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.common.BackoffPolicy;
+import org.elasticsearch.common.blobstore.support.TenaciousRetryBlobContainer;
+import org.elasticsearch.common.bytes.BytesArray;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.core.CheckedConsumer;
+import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.repositories.RepositoriesMetrics;
+import org.elasticsearch.telemetry.RecordingMeterRegistry;
+import org.elasticsearch.test.ESTestCase;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+
+public class TenaciousRetryBlobContainerTests extends ESTestCase {
+
+    public void testNonRetryable() throws IOException {
+
+        final var recordingMeterRegistry = new RecordingMeterRegistry();
+        final var repositoriesMetrics = new RepositoriesMetrics(recordingMeterRegistry);
+        BlobContainer blobContainer = mock(BlobContainer.class, invocationOnMock -> { throw new IOException("test"); });
+
+        TenaciousRetryBlobContainer tenaciousRetryBlobContainer = new TenaciousRetryBlobContainer(
+            blobContainer,
+            Integer.MAX_VALUE,
+            BackoffPolicy.linearBackoff(TimeValue.timeValueMillis(50), Integer.MAX_VALUE, TimeValue.ONE_MINUTE),
+            repositoriesMetrics
+        ) {
+
+            @Override
+            protected boolean isExceptionRetryable(Exception e) {
+                return true;
+            }
+
+            @Override
+            protected String getRepositoryType() {
+                return "test";
+            }
+
+            @Override
+            protected BlobContainer wrapChild(BlobContainer child) {
+                return child;
+            }
+        };
+
+        List<Method> nonRetryable = nonRetryableBlobContainerMethods();
+
+        Method method = randomFrom(nonRetryable);
+        Object[] args = buildArgs(method);
+
+        expectThrows(InvocationTargetException.class, () -> method.invoke(tenaciousRetryBlobContainer, args));
+
+    }
+
+    private Object[] buildArgs(Method method) {
+        Class<?>[] ptypes = method.getParameterTypes();
+        Object[] args = new Object[ptypes.length];
+        for (int i = 0; i < ptypes.length; i++) {
+            args[i] = sampleArgument(ptypes[i]);
+        }
+        return args;
+    }
+
+    private List<Method> nonRetryableBlobContainerMethods() {
+        List<Method> methods = new ArrayList<>();
+        for (Method m : BlobContainer.class.getDeclaredMethods()) {
+            if (m.getName().equals("listBlobs") || m.getName().equals("listBlobsByPrefix")) {
+                continue;
+            }
+            methods.add(m);
+        }
+        return Collections.unmodifiableList(methods);
+    }
+
+    private Object sampleArgument(Class<?> type) {
+        if (type == OperationPurpose.class) {
+            OperationPurpose[] values = OperationPurpose.values();
+            return randomFrom(values);
+        }
+        if (type == String.class) {
+            return randomIdentifier();
+        }
+        if (type == long.class || type == Long.class) {
+            return randomLong();
+        }
+        if (type == boolean.class || type == Boolean.class) {
+            return randomBoolean();
+        }
+        if (type == InputStream.class) {
+            return InputStream.nullInputStream();
+        }
+        if (type == BlobContainer.class) {
+            return mock(BlobContainer.class);
+        }
+        if (type == Iterator.class) {
+            return Collections.emptyIterator();
+        }
+        if (type == BytesReference.class) {
+            return BytesArray.EMPTY;
+        }
+        if (type == ActionListener.class) {
+            return ActionListener.noop();
+        }
+        if (type == CheckedConsumer.class) {
+            return (CheckedConsumer<OutputStream, IOException>) outputStream -> {};
+        }
+        if (type == BlobContainer.BlobMultiPartInputStreamProvider.class) {
+            return (BlobContainer.BlobMultiPartInputStreamProvider) (offset, length) -> new ByteArrayInputStream(
+                randomByteArrayOfLength(10)
+            );
+        }
+        throw new IllegalArgumentException("Add a sample for parameter type [" + type.getName() + "] used by BlobContainer");
+    }
+
+}

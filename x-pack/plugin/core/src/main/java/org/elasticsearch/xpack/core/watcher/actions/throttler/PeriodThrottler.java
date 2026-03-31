d55c083aced70f1d8fef560f25fa517b7ed03a2f
Michal Borek <michal.borek@elastic.co>
2026-03-31 15:10:18 +0200

    Change a logger to elasticsearch one

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/watcher/actions/throttler/PeriodThrottler.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/watcher/actions/throttler/PeriodThrottler.java
@@ -6,10 +6,10 @@
  */
 package org.elasticsearch.xpack.core.watcher.actions.throttler;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.elasticsearch.core.Nullable;
 import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.logging.LogManager;
+import org.elasticsearch.logging.Logger;
 import org.elasticsearch.xpack.core.watcher.actions.ActionStatus;
 import org.elasticsearch.xpack.core.watcher.execution.WatchExecutionContext;
 

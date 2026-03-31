5eb025a8aa777d80f7b3ecfad37ad338e233aeda
Sylvain Wallez <sylvain@bluxte.net>
2026-03-31 19:09:41 +0200

    Hide converter map in ArrowToBlockConverter

--- x-pack/plugin/esql/arrow/src/main/java/org/elasticsearch/xpack/esql/arrow/ArrowToBlockConverter.java
+++ x-pack/plugin/esql/arrow/src/main/java/org/elasticsearch/xpack/esql/arrow/ArrowToBlockConverter.java
@@ -7,27 +7,10 @@
 
 package org.elasticsearch.xpack.esql.arrow;
 
-import org.apache.arrow.vector.BitVector;
 import org.apache.arrow.vector.FieldVector;
 import org.apache.arrow.vector.types.Types;
 import org.elasticsearch.compute.data.Block;
 import org.elasticsearch.compute.data.BlockFactory;
-import org.elasticsearch.compute.data.arrow.BooleanArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.BytesRefArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.DoubleArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.Float16ArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.FloatArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.Int16ArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.Int8ArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.IntArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.LongArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.LongMul1kArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.UInt16ArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.UInt32ArrowBufBlock;
-import org.elasticsearch.compute.data.arrow.UInt8ArrowBufBlock;
-
-import java.util.EnumMap;
-import java.util.Map;
 
 /**
  * Converts Apache Arrow FieldVector to ESQL Blocks.
@@ -40,8 +23,6 @@ import java.util.Map;
 @FunctionalInterface
 public interface ArrowToBlockConverter {
 
-    Map<Types.MinorType, ArrowToBlockConverter> CONVERTERS = buildConverters();
-
     /**
      * Convert an Arrow FieldVector to an ESQL Block.
      * @param vector the Arrow vector
@@ -51,37 +32,11 @@ public interface ArrowToBlockConverter {
     Block convert(FieldVector vector, BlockFactory factory);
 
     /**
-     * Create a converter for the given Arrow type.
+     * Get a converter for the given Arrow type.
      * @param arrowType the Arrow minor type
      * @return the appropriate converter, or null if the type is not supported
      */
     static ArrowToBlockConverter forType(Types.MinorType arrowType) {
-        return CONVERTERS.get(arrowType);
-    }
-
-    private static Map<Types.MinorType, ArrowToBlockConverter> buildConverters() {
-        var map = new EnumMap<Types.MinorType, ArrowToBlockConverter>(Types.MinorType.class);
-        map.put(Types.MinorType.FLOAT2, Float16ArrowBufBlock::of);
-        map.put(Types.MinorType.FLOAT4, FloatArrowBufBlock::of);
-        map.put(Types.MinorType.FLOAT8, DoubleArrowBufBlock::of);
-        map.put(Types.MinorType.TINYINT, Int8ArrowBufBlock::of);
-        map.put(Types.MinorType.SMALLINT, Int16ArrowBufBlock::of);
-        map.put(Types.MinorType.INT, IntArrowBufBlock::of);
-        map.put(Types.MinorType.BIGINT, LongArrowBufBlock::of);
-        map.put(Types.MinorType.UINT1, UInt8ArrowBufBlock::of);
-        map.put(Types.MinorType.UINT2, UInt16ArrowBufBlock::of);
-        map.put(Types.MinorType.UINT4, UInt32ArrowBufBlock::of);
-        map.put(Types.MinorType.BIT, (vec, bf) -> BooleanArrowBufBlock.of((BitVector) vec, bf));
-        map.put(Types.MinorType.VARCHAR, BytesRefArrowBufBlock::of);
-        map.put(Types.MinorType.VARBINARY, BytesRefArrowBufBlock::of);
-        map.put(Types.MinorType.TIMESTAMPSEC, LongMul1kArrowBufBlock::of);
-        map.put(Types.MinorType.TIMESTAMPSECTZ, LongMul1kArrowBufBlock::of);
-        map.put(Types.MinorType.TIMESTAMPMILLI, LongArrowBufBlock::of);
-        map.put(Types.MinorType.TIMESTAMPMILLITZ, LongArrowBufBlock::of);
-        map.put(Types.MinorType.TIMESTAMPMICRO, LongMul1kArrowBufBlock::of);
-        map.put(Types.MinorType.TIMESTAMPMICROTZ, LongMul1kArrowBufBlock::of);
-        map.put(Types.MinorType.TIMESTAMPNANO, LongArrowBufBlock::of);
-        map.put(Types.MinorType.TIMESTAMPNANOTZ, LongArrowBufBlock::of);
-        return map;
+        return ArrowToBlockConverters.forType(arrowType);
     }
 }

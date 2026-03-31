5308fcc7e91d7cafb489946341567f4fdee565d4
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-03-31 14:02:39 +0000

    [CI] Auto commit changes from spotless

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/test/rest/compat/compat/RestCompatTestTransformTask.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/test/rest/compat/compat/RestCompatTestTransformTask.java
@@ -113,8 +113,9 @@ public abstract class RestCompatTestTransformTask extends DefaultTask {
         getTransformations().add(new InjectHeaders(headers, Sets.newHashSet(RestCompatTestTransformTask::doesNotHaveCatOperation)));
         // Prior-minor YAML still asserts length on top-level profile keys; current versions add profile.request next to
         // profile.shards (SearchProfileResults + TransportVersion include_original_query_indices_in_search_profile_results).
-        getTransformations()
-            .add(new ReplaceValueInLength("profile", SerializableJsonNode.of(2, NumericNode.class), "Test that parameters can be used"));
+        getTransformations().add(
+            new ReplaceValueInLength("profile", SerializableJsonNode.of(2, NumericNode.class), "Test that parameters can be used")
+        );
     }
 
     @Inject

00736004c84c66af05177441b7815787070b1fe8
Yang Wang <yang.wang@elastic.co>
2026-03-31 23:58:46 +1100

    remove unnecessary getter

--- server/src/main/java/org/elasticsearch/snapshots/SnapshotShardsService.java
+++ server/src/main/java/org/elasticsearch/snapshots/SnapshotShardsService.java
@@ -222,10 +222,6 @@ public final class SnapshotShardsService extends AbstractLifecycleComponent impl
         }
     }
 
-    public ThrottledTaskRunner getStartShardSnapshotTaskRunner() {
-        return startShardSnapshotTaskRunner;
-    }
-
     /**
      * The {@link SnapshotShardsService} should be activated for all nodes that contain data.
      * On stateful, since nodes share both indexing and search functionality, this is determined by whether

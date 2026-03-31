3de87d8aa8eacc9afe735b0c8e9673e7ef6bfb47
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-03-31 11:04:43 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/OptionsTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/OptionsTests.java
@@ -23,7 +23,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 public class OptionsTests extends ESTestCase {
 
     public void testNullOptions_SingleDataTypeAllowed() {

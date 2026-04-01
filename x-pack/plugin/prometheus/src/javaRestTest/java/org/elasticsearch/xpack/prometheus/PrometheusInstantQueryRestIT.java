3ff0fc1dd33cebe1ea120fe6502507c50ae0fd48
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2026-04-01 12:51:19 +0200

    Pull up generic test helpers into AbstractPrometheusRestIT
    Moves ingestTestData(String), searchDocs, searchSingleDoc, and
    dataStreamExists from the concrete IT classes into the shared abstract
    base, eliminating ~190 lines of duplication across the three REST ITs
    that needed them.

--- x-pack/plugin/prometheus/src/javaRestTest/java/org/elasticsearch/xpack/prometheus/PrometheusInstantQueryRestIT.java
+++ x-pack/plugin/prometheus/src/javaRestTest/java/org/elasticsearch/xpack/prometheus/PrometheusInstantQueryRestIT.java
@@ -7,15 +7,11 @@
 
 package org.elasticsearch.xpack.prometheus;
 
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.entity.ContentType;
-import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.test.cluster.ElasticsearchCluster;
 import org.elasticsearch.test.cluster.local.distribution.DistributionType;
 import org.elasticsearch.test.rest.ObjectPath;
-import org.elasticsearch.xpack.prometheus.proto.RemoteWrite;
 import org.junit.ClassRule;
 
 import java.io.IOException;
@@ -66,14 +62,14 @@ public class PrometheusInstantQueryRestIT extends AbstractPrometheusRestIT {
     }
 
     public void testInstantQueryWithIngestedData() throws Exception {
-        ingestTestData();
+        ingestTestData("test_gauge_iq");
 
         ObjectPath responsePath = executeInstantQuery(null);
         assertMetricResult(responsePath);
     }
 
     public void testInstantQueryWithIndexPattern() throws Exception {
-        ingestTestData();
+        ingestTestData("test_gauge_iq");
 
         ObjectPath responsePath = executeInstantQuery("metrics-generic.prometheus-*");
         assertMetricResult(responsePath);
@@ -84,7 +80,7 @@ public class PrometheusInstantQueryRestIT extends AbstractPrometheusRestIT {
      * Since test data is in the past, the result will be empty — but the request must succeed.
      */
     public void testInstantQueryWithoutTimeDefaultsToNow() throws Exception {
-        ingestTestData();
+        ingestTestData("test_gauge_iq");
 
         Request request = new Request("GET", "/_prometheus/api/v1/query");
         request.addParameter("query", "test_gauge_iq{job=\"test_job\"}");
@@ -128,60 +124,4 @@ public class PrometheusInstantQueryRestIT extends AbstractPrometheusRestIT {
         return responsePath;
     }
 
-    private void ingestTestData() throws IOException {
-        long baseTimestamp = 1767225600000L; // 2026-01-01T00:00:00Z
-
-        Request putCustomTemplate = new Request("PUT", "/_component_template/metrics-prometheus@custom");
-        putCustomTemplate.setJsonEntity("""
-            {
-              "template": {
-                "settings": {
-                  "index": {
-                    "time_series": {
-                      "start_time": "2026-01-01T00:00:00Z"
-                    }
-                  }
-                }
-              }
-            }
-            """);
-        client().performRequest(putCustomTemplate);
-
-        RemoteWrite.WriteRequest.Builder writeRequestBuilder = RemoteWrite.WriteRequest.newBuilder();
-        for (int i = 0; i < 5; i++) {
-            writeRequestBuilder.addTimeseries(
-                RemoteWrite.TimeSeries.newBuilder()
-                    .addLabels(RemoteWrite.Label.newBuilder().setName("__name__").setValue("test_gauge_iq").build())
-                    .addLabels(RemoteWrite.Label.newBuilder().setName("job").setValue("test_job").build())
-                    .addLabels(RemoteWrite.Label.newBuilder().setName("instance").setValue("localhost:9090").build())
-                    .addSamples(RemoteWrite.Sample.newBuilder().setValue(i * 10.0).setTimestamp(baseTimestamp + i * 60_000L).build())
-                    .build()
-            );
-        }
-
-        Request writeRequest = new Request("POST", "/_prometheus/api/v1/write");
-        writeRequest.setEntity(
-            new ByteArrayEntity(writeRequestBuilder.build().toByteArray(), ContentType.create("application/x-protobuf"))
-        );
-        addWriteAuth(writeRequest);
-        Response writeResponse = client().performRequest(writeRequest);
-        assertThat(writeResponse.getStatusLine().getStatusCode(), equalTo(204));
-        if (writeResponse.getEntity() != null) {
-            assertThat(EntityUtils.toString(writeResponse.getEntity()), equalTo(""));
-        }
-
-        Request refresh = new Request("POST", "/metrics-generic.prometheus-default/_refresh");
-        client().performRequest(refresh);
-
-        Request searchFailures = new Request("GET", "/metrics-generic.prometheus-default::failures/_search");
-        searchFailures.setJsonEntity("""
-            {
-              "track_total_hits": true,
-              "size": 0
-            }
-            """);
-        ObjectPath failuresSearchPath = ObjectPath.createFromResponse(client().performRequest(searchFailures));
-        Number totalFailures = failuresSearchPath.evaluate("hits.total.value");
-        assertThat(totalFailures.intValue(), equalTo(0));
-    }
 }
--- x-pack/plugin/prometheus/src/javaRestTest/java/org/elasticsearch/xpack/prometheus/PrometheusQueryRangeRestIT.java
+++ x-pack/plugin/prometheus/src/javaRestTest/java/org/elasticsearch/xpack/prometheus/PrometheusQueryRangeRestIT.java
@@ -7,15 +7,11 @@
 
 package org.elasticsearch.xpack.prometheus;
 
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.entity.ContentType;
-import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.test.cluster.ElasticsearchCluster;
 import org.elasticsearch.test.cluster.local.distribution.DistributionType;
 import org.elasticsearch.test.rest.ObjectPath;
-import org.elasticsearch.xpack.prometheus.proto.RemoteWrite;
 import org.junit.ClassRule;
 
 import java.io.IOException;
@@ -69,14 +65,14 @@ public class PrometheusQueryRangeRestIT extends AbstractPrometheusRestIT {
     }
 
     public void testQueryRangeWithIngestedData() throws Exception {
-        ingestTestData();
+        ingestTestData("test_gauge_qr");
 
         ObjectPath responsePath = executeQueryRange();
         assertMetricResults(responsePath);
     }
 
     public void testQueryRangeWithIndexPattern() throws Exception {
-        ingestTestData();
+        ingestTestData("test_gauge_qr");
 
         ObjectPath responsePath = executeQueryRangeWithIndex("metrics-generic.prometheus-*");
         assertMetricResults(responsePath);
@@ -118,64 +114,6 @@ public class PrometheusQueryRangeRestIT extends AbstractPrometheusRestIT {
         assertThat(responsePath.evaluate("status"), equalTo("success"));
         assertThat(responsePath.evaluate("data.resultType"), equalTo("matrix"));
         return responsePath;
-    }
-
-    private void ingestTestData() throws IOException {
-        long baseTimestamp = 1767225600000L; // 2026-01-01T00:00:00Z
-
-        Request putCustomTemplate = new Request("PUT", "/_component_template/metrics-prometheus@custom");
-        putCustomTemplate.setJsonEntity("""
-            {
-              "template": {
-                "settings": {
-                  "index": {
-                    "time_series": {
-                      "start_time": "2026-01-01T00:00:00Z"
-                    }
-                  }
-                }
-              }
-            }
-            """);
-        client().performRequest(putCustomTemplate);
-
-        RemoteWrite.WriteRequest.Builder writeRequestBuilder = RemoteWrite.WriteRequest.newBuilder();
-        for (int i = 0; i < 5; i++) {
-            writeRequestBuilder.addTimeseries(
-                RemoteWrite.TimeSeries.newBuilder()
-                    .addLabels(RemoteWrite.Label.newBuilder().setName("__name__").setValue("test_gauge_qr").build())
-                    .addLabels(RemoteWrite.Label.newBuilder().setName("job").setValue("test_job").build())
-                    .addLabels(RemoteWrite.Label.newBuilder().setName("instance").setValue("localhost:9090").build())
-                    .addSamples(RemoteWrite.Sample.newBuilder().setValue(i * 10.0).setTimestamp(baseTimestamp + i * 60_000L).build())
-                    .build()
-            );
-        }
-
-        Request writeRequest = new Request("POST", "/_prometheus/api/v1/write");
-        writeRequest.setEntity(
-            new ByteArrayEntity(writeRequestBuilder.build().toByteArray(), ContentType.create("application/x-protobuf"))
-        );
-        addWriteAuth(writeRequest);
-        Response writeResponse = client().performRequest(writeRequest);
-        assertThat(writeResponse.getStatusLine().getStatusCode(), equalTo(204));
-        if (writeResponse.getEntity() != null) {
-            // A non-empty body would contain partial failure details from the underlying bulk request.
-            assertThat(EntityUtils.toString(writeResponse.getEntity()), equalTo(""));
-        }
-
-        Request refresh = new Request("POST", "/metrics-generic.prometheus-default/_refresh");
-        client().performRequest(refresh);
-
-        Request searchFailures = new Request("GET", "/metrics-generic.prometheus-default::failures/_search");
-        searchFailures.setJsonEntity("""
-            {
-              "track_total_hits": true,
-              "size": 0
-            }
-            """);
-        ObjectPath failuresSearchPath = ObjectPath.createFromResponse(client().performRequest(searchFailures));
-        Number totalFailures = failuresSearchPath.evaluate("hits.total.value");
-        assertThat(totalFailures.intValue(), equalTo(0));
     }
 
 }

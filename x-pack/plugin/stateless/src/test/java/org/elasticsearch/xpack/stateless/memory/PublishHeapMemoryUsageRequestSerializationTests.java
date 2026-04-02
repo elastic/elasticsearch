16d034def0ea62e4d1489f2231a995fab75e93ea
Keith Massey <keith.massey@elastic.co>
2026-04-02 17:02:56 -0500

    Moving ShardsMappingSizeCollector and related files to modules-self-managed (#6088)

---
+++ x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/memory/PublishHeapMemoryUsageRequestSerializationTests.java
@@ -0,0 +1,40 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ * Copyright Elasticsearch B.V. All rights reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch B.V. and its suppliers, if any.
+ * The intellectual and technical concepts contained herein
+ * are proprietary to Elasticsearch B.V. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in
+ * process, and are protected by trade secret or copyright
+ * law.  Dissemination of this information or reproduction of
+ * this material is strictly forbidden unless prior written
+ * permission is obtained from Elasticsearch B.V.
+ */
+
+package org.elasticsearch.xpack.stateless.memory;
+
+import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.test.AbstractWireSerializingTestCase;
+
+import java.io.IOException;
+
+public class PublishHeapMemoryUsageRequestSerializationTests extends AbstractWireSerializingTestCase<PublishHeapMemoryMetricsRequest> {
+    @Override
+    protected Writeable.Reader<PublishHeapMemoryMetricsRequest> instanceReader() {
+        return PublishHeapMemoryMetricsRequest::new;
+    }
+
+    @Override
+    protected PublishHeapMemoryMetricsRequest createTestInstance() {
+        return new PublishHeapMemoryMetricsRequest(HeapMemoryUsageTests.randomHeapMemoryUsage());
+    }
+
+    @Override
+    protected PublishHeapMemoryMetricsRequest mutateInstance(PublishHeapMemoryMetricsRequest instance) throws IOException {
+        return new PublishHeapMemoryMetricsRequest(HeapMemoryUsageTests.mutate(instance.getHeapMemoryUsage()));
+    }
+}

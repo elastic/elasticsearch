005ebd77a900f17754669d6af8fe0305ca8cbeea
Dimitris Rempapis <dimitris.rempapis@elastic.co>
2026-04-02 16:26:09 +0300

    Add Consumer import to SearchService.java

--- server/src/main/java/org/elasticsearch/search/SearchService.java
+++ server/src/main/java/org/elasticsearch/search/SearchService.java
@@ -156,6 +156,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.LongSupplier;
 import java.util.function.Supplier;

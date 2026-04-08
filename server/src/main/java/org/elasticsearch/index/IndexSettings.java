5ce4bc9534cc80756c86764bb7aa8a15272fb2f3
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-04-08 14:04:41 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/index/IndexSettings.java
+++ server/src/main/java/org/elasticsearch/index/IndexSettings.java
@@ -25,7 +25,6 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.time.DateUtils;
 import org.elasticsearch.common.unit.ByteSizeUnit;
 import org.elasticsearch.common.unit.ByteSizeValue;
-import org.elasticsearch.common.util.FeatureFlag;
 import org.elasticsearch.core.Booleans;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.codec.CodecService;

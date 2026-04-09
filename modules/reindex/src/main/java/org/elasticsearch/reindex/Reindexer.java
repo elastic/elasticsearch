bfa042b3baeeedc2991fd217c87b52f5d456f56e
Joshua Adams <joshua.adams@elastic.co>
2026-04-09 20:08:36 +0100

    Revert "Add Reindexing PIT Unit Tests (#145803)"
    This reverts commit a8defb091b11feb240ec7e857e1a5904756aff41.

--- modules/reindex/src/main/java/org/elasticsearch/reindex/Reindexer.java
+++ modules/reindex/src/main/java/org/elasticsearch/reindex/Reindexer.java
@@ -564,9 +564,8 @@ public class Reindexer {
 
     /**
      * Closes the RestClient on the generic thread pool (to avoid closing from the client's own thread), then runs the given action.
-     * Package-private for unit tests.
      */
-    void closeRestClientAndRun(RestClient restClient, Runnable onCompletion) {
+    private void closeRestClientAndRun(RestClient restClient, Runnable onCompletion) {
         threadPool.generic().submit(() -> {
             try {
                 restClient.close();

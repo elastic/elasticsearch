509b3b4e423ba7689c72daadf7dda56f50852b31
Jan-Kazlouski-elastic <jan.kazlouski@elastic.co>
2026-04-10 15:25:27 +0300

    Refactor Contextual AI rerank model tests to simplify task settings handling and improve test coverage

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/contextualai/rerank/ContextualAiRerankModelTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/contextualai/rerank/ContextualAiRerankModelTests.java
@@ -11,16 +11,14 @@ import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.core.Nullable;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.xpack.inference.services.ConfigurationParseContext;
-import org.elasticsearch.xpack.inference.services.ServiceFields;
 import org.elasticsearch.xpack.inference.services.contextualai.ContextualAiServiceSettings;
 import org.elasticsearch.xpack.inference.services.settings.DefaultSecretSettings;
+import org.elasticsearch.xpack.inference.services.settings.DefaultSecretSettingsTests;
 import org.elasticsearch.xpack.inference.services.settings.RateLimitSettings;
 
 import java.net.URI;
 import java.util.HashMap;
-import java.util.Map;
 
-import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.DEFAULT_RERANK_URL;
 import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.INITIAL_TEST_INSTRUCTION;
 import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.INITIAL_TEST_RETURN_DOCUMENTS;
 import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.INITIAL_TEST_TOP_N;
@@ -29,20 +27,143 @@ import static org.elasticsearch.xpack.inference.services.contextualai.Contextual
 import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.NEW_TEST_TOP_N;
 import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.TEST_API_KEY;
 import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.TEST_INFERENCE_ENTITY_ID;
+import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.TEST_INSTRUCTION;
 import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.TEST_MODEL_ID;
 import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.TEST_RATE_LIMIT;
-import static org.elasticsearch.xpack.inference.services.contextualai.rerank.ContextualAiRerankTaskSettings.INSTRUCTION_FIELD;
-import static org.elasticsearch.xpack.inference.services.contextualai.rerank.ContextualAiRerankTaskSettings.RETURN_DOCUMENTS_FIELD;
-import static org.elasticsearch.xpack.inference.services.contextualai.rerank.ContextualAiRerankTaskSettings.TOP_N_FIELD;
+import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.TEST_RETURN_DOCUMENTS;
+import static org.elasticsearch.xpack.inference.services.contextualai.ContextualAiRerankTestFixtures.TEST_TOP_N;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.sameInstance;
 
 public class ContextualAiRerankModelTests extends ESTestCase {
 
-    /**
-     * Creates a {@link ContextualAiRerankModel} pointed at a custom URL (e.g. a {@code MockWebServer}),
-     * intended for use in action creator and integration-style tests.
-     */
+    public void testCreateModel_FromMap_AllFields() {
+        var model = new ContextualAiRerankModel(
+            TEST_INFERENCE_ENTITY_ID,
+            ContextualAiRerankServiceSettingsTests.buildServiceSettingsMap(TEST_MODEL_ID, TEST_RATE_LIMIT),
+            ContextualAiRerankTaskSettingsTests.buildTaskSettingsMap(TEST_RETURN_DOCUMENTS, TEST_TOP_N, TEST_INSTRUCTION),
+            DefaultSecretSettingsTests.getSecretSettingsMap(TEST_API_KEY),
+            ConfigurationParseContext.PERSISTENT
+        );
+
+        assertThat(model, is(buildModel(TEST_RETURN_DOCUMENTS, TEST_TOP_N, TEST_INSTRUCTION)));
+    }
+
+    public void testOf_EmptyMap_RequestOverridesAllValues_AllValuesUpdated() {
+        var originalModel = buildModel(null, null, null);
+
+        var mergedModel = ContextualAiRerankModel.of(
+            originalModel,
+            ContextualAiRerankTaskSettingsTests.buildTaskSettingsMap(NEW_TEST_RETURN_DOCUMENTS, NEW_TEST_TOP_N, NEW_TEST_INSTRUCTION)
+        );
+
+        assertThat(mergedModel, is(buildModel(NEW_TEST_RETURN_DOCUMENTS, NEW_TEST_TOP_N, NEW_TEST_INSTRUCTION)));
+    }
+
+    public void testOf_RequestOverridesReturnDocuments_KeepsOtherFieldsUnchanged() {
+        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
+
+        var mergedModel = ContextualAiRerankModel.of(
+            originalModel,
+            ContextualAiRerankTaskSettingsTests.buildTaskSettingsMap(NEW_TEST_RETURN_DOCUMENTS, null, null)
+        );
+
+        assertThat(mergedModel, is(buildModel(NEW_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION)));
+    }
+
+    public void testOf_RequestOverridesTopN_KeepsOtherFieldsUnchanged() {
+        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
+
+        var mergedModel = ContextualAiRerankModel.of(
+            originalModel,
+            ContextualAiRerankTaskSettingsTests.buildTaskSettingsMap(null, NEW_TEST_TOP_N, null)
+        );
+
+        assertThat(mergedModel, is(buildModel(INITIAL_TEST_RETURN_DOCUMENTS, NEW_TEST_TOP_N, INITIAL_TEST_INSTRUCTION)));
+    }
+
+    public void testOf_RequestOverridesInstruction_KeepsOtherFieldsUnchanged() {
+        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
+
+        var mergedModel = ContextualAiRerankModel.of(
+            originalModel,
+            ContextualAiRerankTaskSettingsTests.buildTaskSettingsMap(null, null, NEW_TEST_INSTRUCTION)
+        );
+
+        assertThat(mergedModel, is(buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, NEW_TEST_INSTRUCTION)));
+    }
+
+    public void testOf_EmptyTaskSettingsMap_ReturnsOriginalTaskSettingsInstance() {
+        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
+
+        var mergedModel = ContextualAiRerankModel.of(originalModel, new HashMap<>());
+
+        assertThat(mergedModel, sameInstance(originalModel));
+    }
+
+    public void testOf_SameValuesInTaskSettingsMap_ReturnsOriginalTaskSettingsInstance() {
+        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
+
+        var mergedModel = ContextualAiRerankModel.of(
+            originalModel,
+            ContextualAiRerankTaskSettingsTests.buildTaskSettingsMap(
+                INITIAL_TEST_RETURN_DOCUMENTS,
+                INITIAL_TEST_TOP_N,
+                INITIAL_TEST_INSTRUCTION
+            )
+        );
+
+        assertThat(mergedModel, sameInstance(originalModel));
+    }
+
+    public void testOf_OnlySameReturnDocumentsInTaskSettingsMap_ReturnsOriginalTaskSettingsInstance() {
+        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
+
+        var mergedModel = ContextualAiRerankModel.of(
+            originalModel,
+            ContextualAiRerankTaskSettingsTests.buildTaskSettingsMap(INITIAL_TEST_RETURN_DOCUMENTS, null, null)
+        );
+
+        assertThat(mergedModel, sameInstance(originalModel));
+    }
+
+    public void testOf_OnlySameTopNInTaskSettingsMap_ReturnsOriginalTaskSettingsInstance() {
+        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
+
+        var mergedModel = ContextualAiRerankModel.of(
+            originalModel,
+            ContextualAiRerankTaskSettingsTests.buildTaskSettingsMap(null, INITIAL_TEST_TOP_N, null)
+        );
+
+        assertThat(mergedModel, sameInstance(originalModel));
+    }
+
+    public void testOf_OnlySameInstructionInTaskSettingsMap_ReturnsOriginalTaskSettingsInstance() {
+        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
+
+        var mergedModel = ContextualAiRerankModel.of(
+            originalModel,
+            ContextualAiRerankTaskSettingsTests.buildTaskSettingsMap(null, null, INITIAL_TEST_INSTRUCTION)
+        );
+
+        assertThat(mergedModel, sameInstance(originalModel));
+    }
+
+    private static ContextualAiRerankModel buildModel(
+        @Nullable Boolean returnDocuments,
+        @Nullable Integer topN,
+        @Nullable String instruction
+    ) {
+        return new ContextualAiRerankModel(
+            TEST_INFERENCE_ENTITY_ID,
+            new ContextualAiRerankServiceSettings(
+                new ContextualAiServiceSettings.CommonSettings(TEST_MODEL_ID, new RateLimitSettings(TEST_RATE_LIMIT))
+            ),
+            new ContextualAiRerankTaskSettings(returnDocuments, topN, instruction),
+            new DefaultSecretSettings(new SecureString(TEST_API_KEY.toCharArray()))
+        );
+    }
+
     public static ContextualAiRerankModel createModel(
         String url,
         String apiKey,
@@ -58,64 +179,6 @@ public class ContextualAiRerankModelTests extends ESTestCase {
             new ContextualAiRerankTaskSettings(null, topN, instruction),
             new DefaultSecretSettings(new SecureString(apiKey.toCharArray())),
             URI.create(url)
-        );
-    }
-
-    public void testCreateModel_FromMap_NoUrl_DefaultUrl() {
-        var model = new ContextualAiRerankModel(
-            TEST_INFERENCE_ENTITY_ID,
-            new HashMap<>(Map.of(ServiceFields.MODEL_ID, TEST_MODEL_ID)),
-            new HashMap<>(),
-            null,
-            ConfigurationParseContext.PERSISTENT
-        );
-
-        assertThat(model.uri().toString(), is(DEFAULT_RERANK_URL));
-    }
-
-    public void testOf_RequestOverridesReturnDocuments_KeepsOtherFieldsUnchanged() {
-        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
-
-        var mergedModel = ContextualAiRerankModel.of(
-            originalModel,
-            new HashMap<>(Map.of(RETURN_DOCUMENTS_FIELD, NEW_TEST_RETURN_DOCUMENTS))
-        );
-
-        assertThat(mergedModel, is(buildModel(NEW_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION)));
-    }
-
-    public void testOf_RequestOverridesTopN_KeepsOtherFieldsUnchanged() {
-        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
-
-        var mergedModel = ContextualAiRerankModel.of(originalModel, new HashMap<>(Map.of(TOP_N_FIELD, NEW_TEST_TOP_N)));
-
-        assertThat(mergedModel, is(buildModel(INITIAL_TEST_RETURN_DOCUMENTS, NEW_TEST_TOP_N, INITIAL_TEST_INSTRUCTION)));
-    }
-
-    public void testOf_RequestOverridesInstruction_KeepsOtherFieldsUnchanged() {
-        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
-
-        var mergedModel = ContextualAiRerankModel.of(originalModel, new HashMap<>(Map.of(INSTRUCTION_FIELD, NEW_TEST_INSTRUCTION)));
-
-        assertThat(mergedModel, is(buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, NEW_TEST_INSTRUCTION)));
-    }
-
-    public void testOf_EmptyTaskSettingsMap_ReturnsOriginalTaskSettingsInstance() {
-        var originalModel = buildModel(INITIAL_TEST_RETURN_DOCUMENTS, INITIAL_TEST_TOP_N, INITIAL_TEST_INSTRUCTION);
-
-        var mergedModel = ContextualAiRerankModel.of(originalModel, new HashMap<>());
-
-        assertThat(mergedModel, sameInstance(originalModel));
-    }
-
-    private static ContextualAiRerankModel buildModel(boolean returnDocuments, int topN, String instruction) {
-        return new ContextualAiRerankModel(
-            TEST_INFERENCE_ENTITY_ID,
-            new ContextualAiRerankServiceSettings(
-                new ContextualAiServiceSettings.CommonSettings(TEST_MODEL_ID, new RateLimitSettings(TEST_RATE_LIMIT))
-            ),
-            new ContextualAiRerankTaskSettings(returnDocuments, topN, instruction),
-            new DefaultSecretSettings(new SecureString(TEST_API_KEY.toCharArray()))
         );
     }
 }

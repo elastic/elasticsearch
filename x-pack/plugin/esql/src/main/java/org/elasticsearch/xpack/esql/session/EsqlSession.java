c3933df7a8ed8635eec5b3c36c48b835a9dd9174
Jan Kuipers <jan.kuipers@elastic.co>
2026-04-14 11:51:16 +0200

    small fixes

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/EsqlSession.java
@@ -372,7 +372,7 @@ public class EsqlSession {
                                 p,
                                 finalConfiguration,
                                 foldContext,
-                                new Holder<>(),
+                                new Holder<Approximation>(),
                                 minimumVersion,
                                 planTimeProfile,
                                 l

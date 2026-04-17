33b449be3aa12944b88448754539815ea6160e35
Jonathan Buttner <jonathan.buttner@elastic.co>
2026-04-17 17:22:50 -0400

    Updating jina

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/jinaai/action/JinaAIActionCreator.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/jinaai/action/JinaAIActionCreator.java
@@ -9,12 +9,19 @@ package org.elasticsearch.xpack.inference.services.jinaai.action;
 
 import org.elasticsearch.xpack.inference.external.action.ExecutableAction;
 import org.elasticsearch.xpack.inference.external.action.SenderExecutableAction;
+import org.elasticsearch.xpack.inference.external.http.retry.ResponseHandler;
+import org.elasticsearch.xpack.inference.external.http.sender.EmbeddingsInput;
+import org.elasticsearch.xpack.inference.external.http.sender.GenericRequestManager;
+import org.elasticsearch.xpack.inference.external.http.sender.QueryAndDocsInputs;
 import org.elasticsearch.xpack.inference.external.http.sender.Sender;
 import org.elasticsearch.xpack.inference.services.ServiceComponents;
-import org.elasticsearch.xpack.inference.services.jinaai.JinaAIEmbeddingsRequestManager;
-import org.elasticsearch.xpack.inference.services.jinaai.JinaAIRerankRequestManager;
+import org.elasticsearch.xpack.inference.services.jinaai.JinaAIResponseHandler;
 import org.elasticsearch.xpack.inference.services.jinaai.embeddings.JinaAIEmbeddingsModel;
 import org.elasticsearch.xpack.inference.services.jinaai.rerank.JinaAIRerankModel;
+import org.elasticsearch.xpack.inference.services.jinaai.request.JinaAIEmbeddingsRequest;
+import org.elasticsearch.xpack.inference.services.jinaai.request.JinaAIRerankRequest;
+import org.elasticsearch.xpack.inference.services.jinaai.response.JinaAIEmbeddingsResponseEntity;
+import org.elasticsearch.xpack.inference.services.jinaai.response.JinaAIRerankResponseEntity;
 
 import java.util.Map;
 import java.util.Objects;
@@ -25,6 +32,17 @@ import static org.elasticsearch.xpack.inference.external.action.ActionUtils.cons
  * Provides a way to construct an {@link ExecutableAction} using the visitor pattern based on the jinaai model type.
  */
 public class JinaAIActionCreator implements JinaAIActionVisitor {
+
+    private static final ResponseHandler EMBEDDINGS_HANDLER = new JinaAIResponseHandler(
+        "jinaai text embedding",
+        JinaAIEmbeddingsResponseEntity::fromResponse
+    );
+
+    private static final ResponseHandler RERANK_HANDLER = new JinaAIResponseHandler(
+        "jinaai rerank",
+        (request, response) -> JinaAIRerankResponseEntity.fromResponse(response)
+    );
+
     private final Sender sender;
     private final ServiceComponents serviceComponents;
 
@@ -36,16 +54,32 @@ public class JinaAIActionCreator implements JinaAIActionVisitor {
     @Override
     public ExecutableAction create(JinaAIEmbeddingsModel model, Map<String, Object> taskSettings) {
         var overriddenModel = JinaAIEmbeddingsModel.of(model, taskSettings);
-        var failedToSendRequestErrorMessage = constructFailedToSendRequestMessage("JinaAI embeddings");
-        var requestCreator = JinaAIEmbeddingsRequestManager.of(overriddenModel, serviceComponents.threadPool());
-        return new SenderExecutableAction(sender, requestCreator, failedToSendRequestErrorMessage);
+        var requestManager = new GenericRequestManager<>(
+            serviceComponents.threadPool(),
+            overriddenModel,
+            EMBEDDINGS_HANDLER,
+            (embeddingsInput) -> new JinaAIEmbeddingsRequest(embeddingsInput.getInputs(), embeddingsInput.getInputType(), overriddenModel),
+            EmbeddingsInput.class
+        );
+        return new SenderExecutableAction(sender, requestManager, constructFailedToSendRequestMessage("JinaAI embeddings"));
     }
 
     @Override
     public ExecutableAction create(JinaAIRerankModel model, Map<String, Object> taskSettings) {
         var overriddenModel = JinaAIRerankModel.of(model, taskSettings);
-        var failedToSendRequestErrorMessage = constructFailedToSendRequestMessage("JinaAI rerank");
-        var requestCreator = JinaAIRerankRequestManager.of(overriddenModel, serviceComponents.threadPool());
-        return new SenderExecutableAction(sender, requestCreator, failedToSendRequestErrorMessage);
+        var requestManager = new GenericRequestManager<>(
+            serviceComponents.threadPool(),
+            overriddenModel,
+            RERANK_HANDLER,
+            (rerankInput) -> new JinaAIRerankRequest(
+                rerankInput.getQuery(),
+                rerankInput.getChunks(),
+                rerankInput.getReturnDocuments(),
+                rerankInput.getTopN(),
+                overriddenModel
+            ),
+            QueryAndDocsInputs.class
+        );
+        return new SenderExecutableAction(sender, requestManager, constructFailedToSendRequestMessage("JinaAI rerank"));
     }
 }

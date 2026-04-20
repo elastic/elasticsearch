ee7f165d20c9fb987789bbdd68adbe2f19af3e5e
Simon Cooper <simon.cooper@elastic.co>
2026-04-20 15:02:31 +0100

    sqr implementations

--- libs/simdvec/native/src/vec/c/aarch64/vec_bf16_2.cpp
+++ libs/simdvec/native/src/vec/c/aarch64/vec_bf16_2.cpp
@@ -24,73 +24,13 @@ static inline svfloat32x2_t bf16_to_f32(svbfloat16_t bf16) {
     return svcreate2(svreinterpret_f32_u32(low), svreinterpret_f32_u32(high));
 }
 
+// take the low half only - ignore the high half
 static inline svfloat32_t low_bf16_to_f32(svbfloat16_t bf16) {
     const svuint16_t u16 = svreinterpret_u16_bf16(bf16);
 
     svuint32_t low = svlsl_n_u32_x(svptrue_b32(), svunpklo_u32(u16), 16);
 
     return svreinterpret_f32_u32(low);
-}
-
-/*
-static inline float32x4_t load_bf16(const bf16_t* ptr, int elements) {
-    return bf16_to_f32(vld1_u16((const uint16_t*)(ptr + elements)));
-}
-
-static inline float32x4_t load_f32(const f32_t* ptr, int elements) {
-    return vld1q_f32(ptr + elements);
-}
-*/
-template<
-    typename TQuery,
-    typename TOp,
-    TOp(*load_d)(const bf16_t*, int element),
-    TOp(*load_q)(const TQuery*, int element),
-    svfloat32_t(*vector_op)(svfloat32_t, TOp, TOp),
-    f32_t(*scalar_op)(bf16_t, TQuery)
->
-static inline f32_t bf16_inner(const bf16_t* d, const TQuery* q, const int32_t elementCount) {
-    constexpr int batches = 8;
-    int i = 0;
-
-    svfloat32_t sum0 = svdup_f32(0.0f);
-    svfloat32_t sum1 = svdup_f32(0.0f);
-    svfloat32_t sum2 = svdup_f32(0.0f);
-    svfloat32_t sum3 = svdup_f32(0.0f);
-    svfloat32_t sum4 = svdup_f32(0.0f);
-    svfloat32_t sum5 = svdup_f32(0.0f);
-    svfloat32_t sum6 = svdup_f32(0.0f);
-    svfloat32_t sum7 = svdup_f32(0.0f);
-
-    const int elements = svcntw();
-    const int stride = elements * batches;
-    const svbool_t all = svptrue_b32();
-    for (; i + stride <= elementCount; i += stride) {
-        sum0 = vector_op(all, sum0, svld1_vnum(all, d + i, 0), svld1_vnum(all, q + i, 0));
-        sum1 = vector_op(all, sum1, svld1_vnum(all, d + i, 1), svld1_vnum(all, q + i, 1));
-        sum2 = vector_op(all, sum2, svld1_vnum(all, d + i, 2), svld1_vnum(all, q + i, 2));
-        sum3 = vector_op(all, sum3, svld1_vnum(all, d + i, 3), svld1_vnum(all, q + i, 3));
-        sum4 = vector_op(all, sum4, svld1_vnum(all, d + i, 4), svld1_vnum(all, q + i, 4));
-        sum5 = vector_op(all, sum5, svld1_vnum(all, d + i, 5), svld1_vnum(all, q + i, 5));
-        sum6 = vector_op(all, sum6, svld1_vnum(all, d + i, 6), svld1_vnum(all, q + i, 6));
-        sum7 = vector_op(all, sum7, svld1_vnum(all, d + i, 7), svld1_vnum(all, q + i, 7));
-    }
-
-    svfloat32_t sum = svadd_f32_x(all,
-        svadd_f32_x(all,
-            svadd_f32_x(all, sum0, sum1),
-            svadd_f32_x(all, sum2, sum3)),
-        svadd_f32_x(all,
-             svadd_f32_x(all, sum4, sum5),
-             svadd_f32_x(all, sum6, sum7)));
-
-    // unstrided tail
-    for (; i < elementCount; i += elements) {
-        svbool_t pg = svwhilelt_b32(i, elementCount);
-        sum = inner_op(pg, sum, svld1(pg, d + i), svld1(pg, q + i));
-    }
-
-    return svaddv_f32(all, sum);
 }
 
 static inline f32_t dotDbf16Qbf16_inner_sve(const bfloat16_t* d, const bfloat16_t* q, const int32_t elementCount) {
@@ -143,7 +83,8 @@ EXPORT f32_t vec_dotDbf16Qbf16_2(const bf16_t* a, const bf16_t* b, const int32_t
     return dotDbf16Qbf16_inner_sve((const bfloat16_t*)a, (const bfloat16_t*)b, elementCount);
 }
 
-static inline f32_t dotDbf16Qf32_inner_sve(const bfloat16_t* d, const f32_t* q, const int32_t elementCount) {
+template<svfloat32_t(*vector_op)(const svbool_t, const svfloat32_t, const svfloat32_t, const svfloat32_t)>
+static inline f32_t bf16Qf32_inner_sve(const bfloat16_t* d, const f32_t* q, const int32_t elementCount) {
     constexpr int batches = 8;
     int i = 0;
 
@@ -163,20 +104,20 @@ static inline f32_t dotDbf16Qf32_inner_sve(const bfloat16_t* d, const f32_t* q, 
     for (; i + stride <= elementCount; i += stride) {
         // load 2 sets of bf16 vectors & extend
         svfloat32x2_t bf0 = bf16_to_f32(svld1_vnum_bf16(all16, d + i, 0));
-        sum0 = svmla_f32_m(all32, sum0, svget2(bf0, 0), svld1_vnum(all32, q + i, 0));
-        sum1 = svmla_f32_m(all32, sum1, svget2(bf0, 1), svld1_vnum(all32, q + i, 1));
+        sum0 = vector_op(all32, sum0, svget2(bf0, 0), svld1_vnum(all32, q + i, 0));
+        sum1 = vector_op(all32, sum1, svget2(bf0, 1), svld1_vnum(all32, q + i, 1));
 
         svfloat32x2_t bf1 = bf16_to_f32(svld1_vnum_bf16(all16, d + i, 1));
-        sum2 = svmla_f32_m(all32, sum2, svget2(bf1, 0), svld1_vnum(all32, q + i, 2));
-        sum3 = svmla_f32_m(all32, sum3, svget2(bf1, 1), svld1_vnum(all32, q + i, 3));
+        sum2 = vector_op(all32, sum2, svget2(bf1, 0), svld1_vnum(all32, q + i, 2));
+        sum3 = vector_op(all32, sum3, svget2(bf1, 1), svld1_vnum(all32, q + i, 3));
 
         svfloat32x2_t bf2 = bf16_to_f32(svld1_vnum_bf16(all16, d + i, 2));
-        sum4 = svmla_f32_m(all32, sum4, svget2(bf2, 0), svld1_vnum(all32, q + i, 4));
-        sum5 = svmla_f32_m(all32, sum5, svget2(bf2, 1), svld1_vnum(all32, q + i, 5));
+        sum4 = vector_op(all32, sum4, svget2(bf2, 0), svld1_vnum(all32, q + i, 4));
+        sum5 = vector_op(all32, sum5, svget2(bf2, 1), svld1_vnum(all32, q + i, 5));
 
         svfloat32x2_t bf3 = bf16_to_f32(svld1_vnum_bf16(all16, d + i, 3));
-        sum6 = svmla_f32_m(all32, sum6, svget2(bf3, 0), svld1_vnum(all32, q + i, 6));
-        sum7 = svmla_f32_m(all32, sum7, svget2(bf3, 1), svld1_vnum(all32, q + i, 7));
+        sum6 = vector_op(all32, sum6, svget2(bf3, 0), svld1_vnum(all32, q + i, 6));
+        sum7 = vector_op(all32, sum7, svget2(bf3, 1), svld1_vnum(all32, q + i, 7));
     }
 
     svfloat32_t sum = svadd_f32_x(all32,
@@ -189,27 +130,146 @@ static inline f32_t dotDbf16Qf32_inner_sve(const bfloat16_t* d, const f32_t* q, 
 
     // unstrided tail
     for (; i < elementCount; i += elements) {
-        // this might end up loading more than we need
-        // TODO: is there a way to limit the load to the f32 count?
+        // this might end up loading more than we need, but anything extra
+        // will just be re-loaded next iteration
+        // TODO: is that a problem? is there a way to limit the load to the f32 count?
         svfloat32_t bf = low_bf16_to_f32(svld1(svwhilelt_b16(i, elementCount), d + i));
 
         svbool_t pg_f32 = svwhilelt_b32(i, elementCount);
-        sum = svmla_f32_m(pg_f32, sum, bf, svld1(pg_f32, q + i));
+        sum = vector_op(pg_f32, sum, bf, svld1(pg_f32, q + i));
     }
 
     return svaddv_f32(all32, sum);
 }
 
+static inline svfloat32_t dotf32_vector(svbool_t pg, svfloat32_t sum, svfloat32_t a, svfloat32_t b) {
+    return svmla_m(pg, sum, a, b);
+}
+
 EXPORT f32_t vec_dotDbf16Qf32_2(const bf16_t* a, const f32_t* b, const int32_t elementCount) {
-    return dotDbf16Qf32_inner_sve((const bfloat16_t*)a, b, elementCount);
+    return bf16Qf32_inner_sve<dotf32_vector>((const bfloat16_t*)a, b, elementCount);
 }
 
 /*
-static inline float32x4_t sqrf32_vector(float32x4_t sum, float32x4_t a, float32x4_t b) {
-    float32x4_t diff = vsubq_f32(a, b);
-    return vmlaq_f32(sum, diff, diff);
+ * Squared distance of 2 bf16 vectors using bfdot, via the identity:
+ * |a - b|^2 = a*a - 2*a*b + b*b
+ * Each term is a dot product computed natively with bfdot, avoiding
+ * the costly bf16 -> f32 conversion
+ */
+static inline f32_t sqrDbf16Qbf16_inner_sve(const bfloat16_t* d, const bfloat16_t* q, const int32_t elementCount) {
+    constexpr int batches = 8;
+    int i = 0;
+
+    svfloat32_t sum_self0 = svdup_f32(0.0f);
+    svfloat32_t sum_self1 = svdup_f32(0.0f);
+    svfloat32_t sum_self2 = svdup_f32(0.0f);
+    svfloat32_t sum_self3 = svdup_f32(0.0f);
+    svfloat32_t sum_self4 = svdup_f32(0.0f);
+    svfloat32_t sum_self5 = svdup_f32(0.0f);
+    svfloat32_t sum_self6 = svdup_f32(0.0f);
+    svfloat32_t sum_self7 = svdup_f32(0.0f);
+    svfloat32_t sum_cross0 = svdup_f32(0.0f);
+    svfloat32_t sum_cross1 = svdup_f32(0.0f);
+    svfloat32_t sum_cross2 = svdup_f32(0.0f);
+    svfloat32_t sum_cross3 = svdup_f32(0.0f);
+    svfloat32_t sum_cross4 = svdup_f32(0.0f);
+    svfloat32_t sum_cross5 = svdup_f32(0.0f);
+    svfloat32_t sum_cross6 = svdup_f32(0.0f);
+    svfloat32_t sum_cross7 = svdup_f32(0.0f);
+
+    const int elements = svcnth();
+    const int stride = elements * batches;
+    const svbool_t all16 = svptrue_b16();
+    for (; i + stride <= elementCount; i += stride) {
+        svbfloat16_t av0 = svld1_vnum(all16, d + i, 0);
+        svbfloat16_t bv0 = svld1_vnum(all16, q + i, 0);
+        sum_self0 = svbfdot_f32(sum_self0, av0, av0);
+        sum_cross0 = svbfdot_f32(sum_cross0, av0, bv0);
+        sum_self0 = svbfdot_f32(sum_self0, bv0, bv0);
+
+        svbfloat16_t av1 = svld1_vnum(all16, d + i, 1);
+        svbfloat16_t bv1 = svld1_vnum(all16, q + i, 1);
+        sum_self1 = svbfdot_f32(sum_self1, av1, av1);
+        sum_cross1 = svbfdot_f32(sum_cross1, av1, bv1);
+        sum_self1 = svbfdot_f32(sum_self1, bv1, bv1);
+
+        svbfloat16_t av2 = svld1_vnum(all16, d + i, 2);
+        svbfloat16_t bv2 = svld1_vnum(all16, q + i, 2);
+        sum_self2 = svbfdot_f32(sum_self2, av2, av2);
+        sum_cross2 = svbfdot_f32(sum_cross2, av2, bv2);
+        sum_self2 = svbfdot_f32(sum_self2, bv2, bv2);
+
+        svbfloat16_t av3 = svld1_vnum(all16, d + i, 3);
+        svbfloat16_t bv3 = svld1_vnum(all16, q + i, 3);
+        sum_self3 = svbfdot_f32(sum_self3, av3, av3);
+        sum_cross3 = svbfdot_f32(sum_cross3, av3, bv3);
+        sum_self3 = svbfdot_f32(sum_self3, bv3, bv3);
+
+        svbfloat16_t av4 = svld1_vnum(all16, d + i, 4);
+        svbfloat16_t bv4 = svld1_vnum(all16, q + i, 4);
+        sum_self4 = svbfdot_f32(sum_self4, av4, av4);
+        sum_cross4 = svbfdot_f32(sum_cross4, av4, bv4);
+        sum_self4 = svbfdot_f32(sum_self4, bv4, bv4);
+
+        svbfloat16_t av5 = svld1_vnum(all16, d + i, 5);
+        svbfloat16_t bv5 = svld1_vnum(all16, q + i, 5);
+        sum_self5 = svbfdot_f32(sum_self5, av5, av5);
+        sum_cross5 = svbfdot_f32(sum_cross5, av5, bv5);
+        sum_self5 = svbfdot_f32(sum_self5, bv5, bv5);
+
+        svbfloat16_t av6 = svld1_vnum(all16, d + i, 6);
+        svbfloat16_t bv6 = svld1_vnum(all16, q + i, 6);
+        sum_self6 = svbfdot_f32(sum_self6, av6, av6);
+        sum_cross6 = svbfdot_f32(sum_cross6, av6, bv6);
+        sum_self6 = svbfdot_f32(sum_self6, bv6, bv6);
+
+        svbfloat16_t av7 = svld1_vnum(all16, d + i, 7);
+        svbfloat16_t bv7 = svld1_vnum(all16, q + i, 7);
+        sum_self7 = svbfdot_f32(sum_self7, av7, av7);
+        sum_cross7 = svbfdot_f32(sum_cross7, av7, bv7);
+        sum_self7 = svbfdot_f32(sum_self7, bv7, bv7);
+    }
+
+    const svbool_t all32 = svptrue_b32();
+    svfloat32_t sum_self = svadd_f32_x(all32,
+        svadd_f32_x(all32,
+            svadd_f32_x(all32, sum_self0, sum_self1),
+            svadd_f32_x(all32, sum_self2, sum_self3)),
+        svadd_f32_x(all32,
+             svadd_f32_x(all32, sum_self4, sum_self5),
+             svadd_f32_x(all32, sum_self6, sum_self7)));
+    svfloat32_t sum_cross = svadd_f32_x(all32,
+        svadd_f32_x(all32,
+            svadd_f32_x(all32, sum_cross0, sum_cross1),
+            svadd_f32_x(all32, sum_cross2, sum_cross3)),
+        svadd_f32_x(all32,
+             svadd_f32_x(all32, sum_cross4, sum_cross5),
+             svadd_f32_x(all32, sum_cross6, sum_cross7)));
+
+    // unstrided tail
+    for (; i < elementCount; i += elements) {
+        svbool_t pg = svwhilelt_b16(i, elementCount);
+        svbfloat16_t av = svld1(pg, d + i);
+        svbfloat16_t bv = svld1(pg, q + i);
+        // don't need predicated vector_op (even for odd dimensions), inactive lanes are all zero anyway
+        sum_self = svbfdot_f32(sum_self, av, av);
+        sum_cross = svbfdot_f32(sum_cross, av, bv);
+        sum_self = svbfdot_f32(sum_self, bv, bv);
+    }
+
+    // |a - b|^2 = a*a - 2*a*b + b*b
+    return svaddv_f32(all32, sum_self) - 2.0f * svaddv_f32(all32, sum_cross);
 }
-EXPORT f32_t vec_sqrDbf16Qbf16(const bf16_t* a, const bf16_t* b, const int32_t elementCount) {
-    return bf16_inner<bf16_t, load_bf16, sqrf32_vector, sqr_scalar>(a, b, elementCount);
+
+EXPORT f32_t vec_sqrDbf16Qbf16_2(const bf16_t* a, const bf16_t* b, const int32_t elementCount) {
+    return sqrDbf16Qbf16_inner_sve((const bfloat16_t*)a, (const bfloat16_t*)b, elementCount);
 }
-*/
+
+static inline svfloat32_t sqrf32_vector(svbool_t pg, svfloat32_t sum, svfloat32_t a, svfloat32_t b) {
+    svfloat32_t diff = svsub_x(pg, a, b);
+    return svmla_m(pg, sum, diff, diff);
+}
+
+EXPORT f32_t vec_sqrDbf16Qf32_2(const bf16_t* a, const f32_t* b, const int32_t elementCount) {
+    return bf16Qf32_inner_sve<sqrf32_vector>((const bfloat16_t*)a, b, elementCount);
+}

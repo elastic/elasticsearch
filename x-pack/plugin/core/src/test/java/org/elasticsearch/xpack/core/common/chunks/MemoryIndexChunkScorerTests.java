a66cab866f5dc1aa4f9ea21e96f6b8e382433960
Michail Romaios <michail.romaios@elastic.co>
2026-04-20 17:19:24 +0200

    update tests

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/common/chunks/MemoryIndexChunkScorerTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/common/chunks/MemoryIndexChunkScorerTests.java
@@ -166,6 +166,12 @@ public class MemoryIndexChunkScorerTests extends ESTestCase {
     public void testOriginalIndexTracking() {
         var scorer = new MemoryIndexChunkScorer();
         List<ScoredChunk> results = scorer.scoreChunks(CHUNKS, "dogs play walk", 3, false);
+
+        assertThat(results.size(), equalTo(3));
+        assertThat(results.get(0).originalIndex(), equalTo(3));
+        assertThat(results.get(1).originalIndex(), equalTo(1));
+        assertThat(results.get(2).originalIndex(), equalTo(0));
+
         for (ScoredChunk chunk : results) {
             assertTrue(chunk.originalIndex() >= 0);
             assertThat(chunk.content(), equalTo(CHUNKS.get(chunk.originalIndex())));

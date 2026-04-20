a1421be22d3ea2c379a62fb2a2528c039b06e936
Joshua Adams <joshua.adams@elastic.co>
2026-04-20 15:12:47 +0100

    Spotless apply

--- server/src/test/java/org/elasticsearch/index/reindex/BulkByScrollWireSerializingTestUtils.java
+++ server/src/test/java/org/elasticsearch/index/reindex/BulkByScrollWireSerializingTestUtils.java
@@ -27,6 +27,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 

b685ba6df196e0373c2eb8853bc4e9924768420d
kanoshiou <uiaao@tuta.io>
2026-04-21 14:16:56 +0800

    Suggest common numeric cast for mixed numeric fields

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/type/EsqlDataTypeConverterTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/type/EsqlDataTypeConverterTests.java
@@ -283,6 +283,11 @@ public class EsqlDataTypeConverterTests extends ESTestCase {
             assertEquals(AGGREGATE_METRIC_DOUBLE, suggestedCast(subset));
         }
 
+        // numeric
+        {
+            assertEquals(LONG, DataType.suggestedCast(Set.of(BYTE, INTEGER, LONG)));
+        }
+
         // unsupported tests
         {
             assertNull(DataType.suggestedCast(Set.of()));

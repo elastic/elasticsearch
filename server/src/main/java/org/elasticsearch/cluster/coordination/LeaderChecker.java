c4304dc29e32d671550b6fdf8c26e7f08454b930
David Turner <david.turner@elastic.co>
2026-04-23 10:28:40 +0100

    Fix exception type

--- server/src/main/java/org/elasticsearch/cluster/coordination/LeaderChecker.java
+++ server/src/main/java/org/elasticsearch/cluster/coordination/LeaderChecker.java
@@ -11,6 +11,7 @@ package org.elasticsearch.cluster.coordination;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.ElasticsearchTimeoutException;
 import org.elasticsearch.ExceptionsHelper;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.ActionListenerResponseHandler;
@@ -34,7 +35,6 @@ import org.elasticsearch.threadpool.ThreadPool.Names;
 import org.elasticsearch.transport.AbstractTransportRequest;
 import org.elasticsearch.transport.ConnectTransportException;
 import org.elasticsearch.transport.NodeDisconnectedException;
-import org.elasticsearch.transport.ReceiveTimeoutTransportException;
 import org.elasticsearch.transport.TransportConnectionListener;
 import org.elasticsearch.transport.TransportRequestOptions;
 import org.elasticsearch.transport.TransportRequestOptions.Type;
@@ -269,7 +269,7 @@ public class LeaderChecker {
                     return;
                 }
 
-                if (exp instanceof ReceiveTimeoutTransportException) {
+                if (exp instanceof ElasticsearchTimeoutException) {
                     timeoutCountSinceLastSuccess += 1;
                 } else {
                     rejectedCountSinceLastSuccess += 1;

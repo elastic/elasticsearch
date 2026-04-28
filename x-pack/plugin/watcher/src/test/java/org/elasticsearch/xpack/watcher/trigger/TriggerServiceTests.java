2fba099a3f1de3b1ad86ecaf56991058d6cc34b7
Michal Borek <michal.borek@elastic.co>
2026-04-28 17:13:06 +0200

    Add missing import statement

--- x-pack/plugin/watcher/src/test/java/org/elasticsearch/xpack/watcher/trigger/TriggerServiceTests.java
+++ x-pack/plugin/watcher/src/test/java/org/elasticsearch/xpack/watcher/trigger/TriggerServiceTests.java
@@ -20,6 +20,7 @@ import org.elasticsearch.xpack.watcher.input.none.ExecutableNoneInput;
 import org.junit.Before;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;

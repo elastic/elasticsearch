d62e4838be04e6f6969ee2e597f647cc74a54f81
Mateusz Rzeszutek <mateusz.rzeszutek@elastic.co>
2026-04-29 13:39:52 +0200

    Fix OOME happening on `SearchResponse#toString()` (part 1) (#147267)
    * Fix OOME happening on `SearchResponse#toString()`
    
    This PR introduced a new `toLimitedString(ChunkedToXContent, int)`
    method to the `Strings` class. The new method, unlike the previously
    used `toString(ChunkedToXContent)` stops the serialization after a
    chosen byte threshold is reached: this might cause the resulting string
    not to be a valid JSON, but as long as it's used just for debugging it
    should be fine.
    
    The 10MiB limit in `SearchResponse#toString()` was chosen arbitrarily, w
    can make it smaller/bigger if there's any need to.
    
    There are hundreds of remaining `Strings#toString(ChunkedToXContent)`
    calls, if we want to forbid or remove them, we can do that work in the
    next PR, after this one is merged.
    
    Closes #143694
    
    * Update docs/changelog/147267.yaml
    
    * Code review comments: rename to Truncated...
    
    * Update docs/changelog/147267.yaml
    
    * Add a default limit of 1MiB
    
    * missing static
    
    * Apply suggestions from code review
    
    Co-authored-by: Moritz Mack <moritz@mackmail.net>
    
    * Append [...] only if the string was truncated
    
    * Apply code review comments
    
    ---------
    
    Co-authored-by: Moritz Mack <moritz@mackmail.net>

--- server/src/main/java/org/elasticsearch/action/search/SearchResponse.java
+++ server/src/main/java/org/elasticsearch/action/search/SearchResponse.java
@@ -501,7 +501,7 @@ public class SearchResponse extends ActionResponse implements ChunkedToXContentO
 
     @Override
     public String toString() {
-        return hasReferences() == false ? "SearchResponse[released]" : Strings.toString(this);
+        return hasReferences() == false ? "SearchResponse[released]" : Strings.toTruncatedString(this);
     }
 
     /**

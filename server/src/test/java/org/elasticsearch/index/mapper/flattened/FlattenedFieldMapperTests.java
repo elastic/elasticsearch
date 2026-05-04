b21716a1075d2f7e5c10db7e9dac21cf77568224
Jordan Powers <jordan.powers@elastic.co>
2026-05-04 10:39:01 -0700

    Fix typo

--- server/src/test/java/org/elasticsearch/index/mapper/flattened/FlattenedFieldMapperTests.java
+++ server/src/test/java/org/elasticsearch/index/mapper/flattened/FlattenedFieldMapperTests.java
@@ -339,7 +339,7 @@ public class FlattenedFieldMapperTests extends MapperTestCase {
     }
 
     public void testDisableIndex() throws Exception {
-        assumeTrue("feature under test must be present", IndexSettings.INDEX_DISABLED_BY_DEFAULT_FEATURE_FLAG.isEnabled())d;
+        assumeTrue("feature under test must be present", IndexSettings.INDEX_DISABLED_BY_DEFAULT_FEATURE_FLAG.isEnabled());
 
         DocumentMapper mapper = createDocumentMapper(fieldMapping(b -> {
             b.field("type", "flattened");

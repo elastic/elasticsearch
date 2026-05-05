85ce49ccac5fc0011fbd1a2aecaa62af533cfa30
Brian Seeders <brian.seeders@elastic.co>
2026-05-05 16:56:04 -0400

    Another test

--- .buildkite/scripts/pull-request/pipeline.sh
+++ .buildkite/scripts/pull-request/pipeline.sh
@@ -5,9 +5,12 @@ set -euo pipefail
 git config --global user.name elasticsearchmachine
 git config --global user.email 'infra-root+elasticsearchmachine@elastic.co'
 
-export VAULT_GITHUB_TOKEN="$GITHUB_TOKEN"
+# This is temporary until we work out issues with the OIDC tokens
+if [[ "${ELASTICSEARCH_MACHINE_GITHUB_TOKEN:-}" != "" ]]; then
+  export VAULT_GITHUB_TOKEN="$ELASTICSEARCH_MACHINE_GITHUB_TOKEN"
+fi
 
-TEST_BRANCH=perms-test-branch-260505-01
+TEST_BRANCH=perms-test-branch-260505-02
 
 git checkout -b "$TEST_BRANCH"
 echo "" >> README.asciidoc

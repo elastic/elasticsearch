603acd91882ef3fb48082703554c7fae251308b0
Martijn van Groningen <martijn.v.groningen@gmail.com>
2026-05-07 09:32:31 +0200

    spotless

--- server/src/main/java/org/elasticsearch/index/mapper/DocumentParserContext.java
+++ server/src/main/java/org/elasticsearch/index/mapper/DocumentParserContext.java
@@ -36,7 +36,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.BiConsumer;
-import java.util.function.BooleanSupplier;
 
 import static org.elasticsearch.index.mapper.IdFieldMapper.standardIdField;
 

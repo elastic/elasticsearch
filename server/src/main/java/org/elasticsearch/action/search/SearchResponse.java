bec9ef802040c8bb39234741560786b648307756
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-05-07 17:01:03 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/action/search/SearchResponse.java
+++ server/src/main/java/org/elasticsearch/action/search/SearchResponse.java
@@ -52,7 +52,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;

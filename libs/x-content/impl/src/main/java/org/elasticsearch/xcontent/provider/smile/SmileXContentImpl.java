702e8f007a8d59f35fd45df36a52d9920e9d6c92
daguimu <daguimu.geek@gmail.com>
2026-05-09 00:44:06 +0800

    Reject non-Smile stream content

--- libs/x-content/impl/src/main/java/org/elasticsearch/xcontent/provider/smile/SmileXContentImpl.java
+++ libs/x-content/impl/src/main/java/org/elasticsearch/xcontent/provider/smile/SmileXContentImpl.java
@@ -11,6 +11,7 @@ package org.elasticsearch.xcontent.provider.smile;
 
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.dataformat.smile.SmileConstants;
 import com.fasterxml.jackson.dataformat.smile.SmileFactory;
@@ -27,6 +28,7 @@ import org.elasticsearch.xcontent.provider.XContentImplUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PushbackInputStream;
 import java.io.Reader;
 import java.util.Set;
 
@@ -103,7 +105,24 @@ public final class SmileXContentImpl implements XContent {
 
     @Override
     public XContentParser createParser(XContentParserConfiguration config, InputStream is) throws IOException {
-        return new SmileXContentParser(config, smileFactory.createParser(is));
+        return new SmileXContentParser(config, smileFactory.createParser(validateSmileHeader(is)));
+    }
+
+    private static InputStream validateSmileHeader(InputStream is) throws IOException {
+        PushbackInputStream input = new PushbackInputStream(is, 3);
+        byte[] header = new byte[3];
+        int length = input.readNBytes(header, 0, header.length);
+        input.unread(header, 0, length);
+        if (length == 0) {
+            return input;
+        }
+        if (length < 3
+            || header[0] != SmileConstants.HEADER_BYTE_1
+            || header[1] != SmileConstants.HEADER_BYTE_2
+            || header[2] != SmileConstants.HEADER_BYTE_3) {
+            throw new JsonParseException(null, "Input does not start with Smile format header");
+        }
+        return input;
     }
 
     @Override
---
+++ libs/x-content/impl/src/test/java/org/elasticsearch/xcontent/provider/smile/SmileXContentImplTests.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with,
+ * at your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+package org.elasticsearch.xcontent.provider.smile;
+
+import com.fasterxml.jackson.core.JsonParseException;
+
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.xcontent.XContentParserConfiguration;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+public class SmileXContentImplTests extends ESTestCase {
+
+    public void testStreamParserRejectsJson() {
+        byte[] json = "{\"foo\":\"bar\"}".getBytes(StandardCharsets.UTF_8);
+        assertThrows(
+            "Input does not start with Smile format header",
+            JsonParseException.class,
+            () -> SmileXContentImpl.smileXContent().createParser(XContentParserConfiguration.EMPTY, new ByteArrayInputStream(json))
+        );
+    }
+}

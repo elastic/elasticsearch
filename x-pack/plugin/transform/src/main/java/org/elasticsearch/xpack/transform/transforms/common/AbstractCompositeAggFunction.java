8d7abc1549b6f707ccc7d21e8d460488240f5a3b
Pat Whelan <pat.whelan@elastic.co>
2026-05-13 11:29:08 -0400

    [Transform] Propagate deprecation warnings (#147934)
    Forward-porting #84040 to propagate deprecation warnings through
    Transform create and preview API so they surface to users and on the
    upgrade assistant. This is a change that went into 7.x but was never
    ported to 8.x+.
    
    Resolve #82936
    Resolve #82935

--- x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/transforms/common/AbstractCompositeAggFunction.java
+++ x-pack/plugin/transform/src/main/java/org/elasticsearch/xpack/transform/transforms/common/AbstractCompositeAggFunction.java
@@ -81,6 +81,7 @@ public abstract class AbstractCompositeAggFunction implements Function {
             ClientHelper.TRANSFORM_ORIGIN,
             client,
             TransportSearchAction.TYPE,
+            true,
             buildSearchRequestForValidation("preview", sourceConfig, timeout, numberOfBuckets),
             ActionListener.wrap(r -> {
                 try {
@@ -125,6 +126,7 @@ public abstract class AbstractCompositeAggFunction implements Function {
             ClientHelper.TRANSFORM_ORIGIN,
             client,
             TransportSearchAction.TYPE,
+            true,
             searchRequest,
             ActionListener.wrap(response -> {
                 if (response == null) {

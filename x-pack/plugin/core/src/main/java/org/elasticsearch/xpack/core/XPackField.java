d46bc4aa9bb752c5943b5ca43c694f2448af524b
Stanislav Malyshev <smalyshev@users.noreply.github.com>
2026-05-13 09:17:26 -0600

    Add logging tracking to _xpack/usage (#148087)
    * Add logging tracking to _xpack/usage
    
    (cherry picked from commit 1685f3ce2ab7fc3c5d56b916b6bd66a6c3e5d39b)
    
    # Conflicts:
    #	x-pack/plugin/core/src/main/java/module-info.java

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/XPackField.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/XPackField.java
@@ -100,6 +100,7 @@ public final class XPackField {
     /** Name for Universal Profiling. */
     public static final String UNIVERSAL_PROFILING = "universal_profiling";
     public static final String LOGSDB = "logsdb";
+    public static final String LOGGING = "logging";
 
     private XPackField() {}
 

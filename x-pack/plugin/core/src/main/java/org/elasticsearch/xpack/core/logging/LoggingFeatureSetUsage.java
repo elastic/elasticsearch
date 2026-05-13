94e7c4544e318096865ea59bf8f5066123079dbb
Stanislav Malyshev <smalyshev@users.noreply.github.com>
2026-05-13 13:24:56 -0600

    [9.3] Add logging tracking to _xpack/usage (#148087) (#149001)
    * Add logging tracking to _xpack/usage (#148087)
    
    * Add logging tracking to _xpack/usage
    
    (cherry picked from commit 1685f3ce2ab7fc3c5d56b916b6bd66a6c3e5d39b)
    
    # Conflicts:
    #	x-pack/plugin/core/src/main/java/module-info.java
    
    * fix backport
    
    * [CI] Auto commit changes from spotless
    
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

---
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/logging/LoggingFeatureSetUsage.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.core.logging;
+
+import org.elasticsearch.TransportVersion;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.xcontent.ToXContentFragment;
+import org.elasticsearch.xcontent.ToXContentObject;
+import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xpack.core.XPackFeatureUsage;
+import org.elasticsearch.xpack.core.XPackField;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
+public class LoggingFeatureSetUsage extends XPackFeatureUsage {
+
+    record LoggingConfig(boolean enabled, boolean userInfo) implements Writeable, ToXContentFragment {
+
+        LoggingConfig(StreamInput in) throws IOException {
+            this(in.readBoolean(), in.readBoolean());
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) throws IOException {
+            out.writeBoolean(enabled);
+            out.writeBoolean(userInfo);
+        }
+
+        @Override
+        public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+            builder.field("enabled", enabled);
+            builder.field("user", userInfo);
+            return builder;
+        }
+    }
+
+    record EsqlLoggingConfig(LoggingConfig base, Map<String, String> thresholds) implements Writeable, ToXContentObject {
+
+        EsqlLoggingConfig(StreamInput input) throws IOException {
+            this(new LoggingConfig(input), input.readMap(StreamInput::readString, StreamInput::readString));
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) throws IOException {
+            base.writeTo(out);
+            out.writeMap(thresholds, StreamOutput::writeString, StreamOutput::writeString);
+        }
+
+        @Override
+        public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+            builder.startObject();
+            base.toXContent(builder, params);
+            if (thresholds.isEmpty() == false) {
+                builder.field("thresholds", thresholds);
+            }
+            builder.endObject();
+            return builder;
+        }
+    }
+
+    private final EsqlLoggingConfig esqlConfig;
+
+    LoggingFeatureSetUsage(EsqlLoggingConfig esqlConfig) {
+        super(XPackField.LOGGING, true, true);
+        this.esqlConfig = esqlConfig;
+    }
+
+    EsqlLoggingConfig esqlConfig() {
+        return esqlConfig;
+    }
+
+    public LoggingFeatureSetUsage(StreamInput input) throws IOException {
+        super(input);
+        esqlConfig = new EsqlLoggingConfig(input);
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        super.writeTo(out);
+        esqlConfig.writeTo(out);
+    }
+
+    @Override
+    protected void innerXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.field("esql", esqlConfig);
+    }
+
+    @Override
+    public TransportVersion getMinimalSupportedVersion() {
+        return TransportVersion.zero();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LoggingFeatureSetUsage that = (LoggingFeatureSetUsage) o;
+        return Objects.equals(esqlConfig, that.esqlConfig);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(esqlConfig);
+    }
+}
---
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/logging/LoggingFeatureSetUsageTests.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.core.logging;
+
+import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.core.Tuple;
+import org.elasticsearch.test.AbstractWireSerializingTestCase;
+import org.elasticsearch.xpack.core.logging.LoggingFeatureSetUsage.EsqlLoggingConfig;
+import org.elasticsearch.xpack.core.logging.LoggingFeatureSetUsage.LoggingConfig;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoggingFeatureSetUsageTests extends AbstractWireSerializingTestCase<LoggingFeatureSetUsage> {
+
+    @Override
+    protected Writeable.Reader<LoggingFeatureSetUsage> instanceReader() {
+        return LoggingFeatureSetUsage::new;
+    }
+
+    @Override
+    protected LoggingFeatureSetUsage createTestInstance() {
+        return new LoggingFeatureSetUsage(randomEsqlLoggingConfig());
+    }
+
+    @Override
+    protected LoggingFeatureSetUsage mutateInstance(LoggingFeatureSetUsage instance) throws IOException {
+        var esqlConfig = instance.esqlConfig();
+        return switch (between(4, 6)) {
+            case 4 -> {
+                var base = esqlConfig.base();
+                var newBase = new LoggingConfig(!base.enabled(), base.userInfo());
+                yield new LoggingFeatureSetUsage(new EsqlLoggingConfig(newBase, esqlConfig.thresholds()));
+            }
+            case 5 -> {
+                var base = esqlConfig.base();
+                var newBase = new LoggingConfig(base.enabled(), !base.userInfo());
+                yield new LoggingFeatureSetUsage(new EsqlLoggingConfig(newBase, esqlConfig.thresholds()));
+            }
+            case 6 -> {
+                Map<String, String> thresholds = new HashMap<>(esqlConfig.thresholds());
+                if (thresholds.isEmpty() || randomBoolean()) {
+                    thresholds.put(randomAlphaOfLength(4), randomAlphaOfLength(4));
+                } else {
+                    String key = randomFrom(thresholds.keySet());
+                    thresholds.put(key, randomValueOtherThan(thresholds.get(key), () -> randomAlphaOfLength(8)));
+                }
+                yield new LoggingFeatureSetUsage(new EsqlLoggingConfig(esqlConfig.base(), thresholds));
+            }
+            default -> throw new AssertionError("unexpected branch");
+        };
+    }
+
+    private LoggingConfig randomLoggingConfig() {
+        return new LoggingConfig(randomBoolean(), randomBoolean());
+    }
+
+    private EsqlLoggingConfig randomEsqlLoggingConfig() {
+        Map<String, String> thresholds = randomMap(0, 5, () -> Tuple.tuple(randomAlphaOfLength(5), randomAlphaOfLength(5)));
+        return new EsqlLoggingConfig(randomLoggingConfig(), thresholds);
+    }
+}

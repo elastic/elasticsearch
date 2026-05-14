591f5f43f483dedfbce12c6aa54bd52034b5500c
Nacho Cordón <ncordon@users.noreply.github.com>
2026-05-14 10:01:30 +0200

    ESQL: Fixes wrong warning in expressions with unrolled multivalues (#145968)

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -2536,6 +2536,14 @@ public class EsqlCapabilities {
         FIX_TBUCKET_NUMERIC_ON_EMPTY_RANGE,
 
         /**
+         * Fix on multi-values that were unrolled and were still producing warnings in expressions
+         * that do not accept multi-values
+         *
+         * See https://github.com/elastic/elasticsearch/issues/134706
+         */
+        FIX_UNROLLED_FOLDABLE_MV_WARNING,
+
+        /**
          * Fix for SET reporting wrong line/column number (-1:-1) in validation errors.
          * see <a href="https://github.com/elastic/elasticsearch/issues/145873">ES|QL: wrong line/column number #145873</a>
          */

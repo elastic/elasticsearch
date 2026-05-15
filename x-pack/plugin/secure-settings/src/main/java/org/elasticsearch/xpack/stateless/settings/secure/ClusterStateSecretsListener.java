48b4959aaee5aa1a13682cee5f951f4213e5049b
Moritz Mack <mmack@apache.org>
2026-05-15 09:22:44 +0200

    feedback

--- x-pack/plugin/secure-settings/src/main/java/org/elasticsearch/xpack/stateless/settings/secure/ClusterStateSecretsListener.java
+++ x-pack/plugin/secure-settings/src/main/java/org/elasticsearch/xpack/stateless/settings/secure/ClusterStateSecretsListener.java
@@ -50,12 +50,10 @@ public class ClusterStateSecretsListener implements ClusterStateListener {
         assert reloadCallback != null : "Cluster state secrets listener has not been initialized";
 
         ClusterState state = event.state();
-        if (state.clusterRecovered() && state.nodes().isLocalNodeElectedMaster() && requiresCleanup.compareAndSet(true, false)) {
+        if (state.clusterRecovered() && state.nodes().isLocalNodeElectedMaster()) {
             ClusterStateSecretsMetadata legacyMetadata = state.custom(ClusterStateSecretsMetadata.TYPE);
-            if (legacyMetadata != null) {
+            if (legacyMetadata != null && requiresCleanup.compareAndSet(true, false)) {
                 removeClusterStateSecretsMetadata();
-            } else {
-                logger.info("Obsolete [{}] not found in cluster custom state, nothing to clean up", ClusterStateSecretsMetadata.TYPE);
             }
         }
 
@@ -86,18 +84,18 @@ public class ClusterStateSecretsListener implements ClusterStateListener {
                     if (currentState.custom(ClusterStateSecretsMetadata.TYPE) == null) {
                         return currentState;
                     }
-                    logger.info("Removing obsolete [{}] from cluster custom state", ClusterStateSecretsMetadata.TYPE);
+                    logger.info("Removing obsolete [file_secure_settings_metadata] from cluster custom state");
                     return ClusterState.builder(currentState).removeCustom(ClusterStateSecretsMetadata.TYPE).build();
                 }
 
                 @Override
                 public void onFailure(Exception e) {
-                    logger.warn("Failed to remove obsolete [{}] from cluster custom state", ClusterStateSecretsMetadata.TYPE, e);
+                    logger.warn("Failed to remove obsolete [file_secure_settings_metadata] from cluster custom state", e);
                     requiresCleanup.set(true);
                 }
             });
         } catch (RuntimeException e) {
-            logger.warn("Failed to remove obsolete [{}] from cluster custom state", ClusterStateSecretsMetadata.TYPE, e);
+            logger.warn("Failed to remove obsolete [file_secure_settings_metadata] from cluster custom state", e);
             requiresCleanup.set(true);
         }
     }

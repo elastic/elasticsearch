63d92533a827d012f0b0e9d6b0cddfca50e26eb9
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-05-15 13:33:25 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/objectstore/S3ObjectStoreTests.java
+++ x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/objectstore/S3ObjectStoreTests.java
@@ -530,7 +530,8 @@ public class S3ObjectStoreTests extends AbstractMockObjectStoreIntegTestCase {
 
             setReplicaCount(1, indexName);
             ensureGreen(indexName);
-            // we are setting size to 0 because we don't want to retrieve the documents (which are heavy in this test case) as it can cause suite timeouts when the cache is very small
+            // we are setting size to 0 because we don't want to retrieve the documents (which are heavy in this test case) as it can cause
+            // suite timeouts when the cache is very small
             assertHitCount(prepareSearch(indexName).setSize(0), numDocs);
         } finally {
             // Stop the node otherwise the test can fail because node tries to publish cluster state to a closed HTTP handler

8115c6649e577fca9e5fcc180635c1c7c4ae5321
Iraklis Psaroudakis <kingherc@gmail.com>
2026-05-15 16:43:52 +0300

    Add preconditions to hollowShards test function
    In #149113 we understood shards may relocate and/or become hollow
    before/while hollowShards executes.
    
    This PR ensures the preconditions of hollowShards are met.

--- x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/StatelessFieldCapabilitiesIT.java
+++ x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/StatelessFieldCapabilitiesIT.java
@@ -11,15 +11,11 @@ import org.elasticsearch.action.NoShardAvailableActionException;
 import org.elasticsearch.action.fieldcaps.FieldCapabilitiesBuilder;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
-import org.elasticsearch.cluster.routing.allocation.decider.EnableAllocationDecider.Rebalance;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.core.TimeValue;
 
 import java.util.Map;
 
-import static org.elasticsearch.cluster.InternalClusterInfoService.CLUSTER_ROUTING_ALLOCATION_ESTIMATED_HEAP_THRESHOLD_DECIDER_ENABLED;
-import static org.elasticsearch.cluster.routing.allocation.IndexBalanceConstraintSettings.INDEX_BALANCE_DECIDER_ENABLED_SETTING;
-import static org.elasticsearch.cluster.routing.allocation.decider.EnableAllocationDecider.CLUSTER_ROUTING_REBALANCE_ENABLE_SETTING;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.elasticsearch.xpack.stateless.commits.HollowShardsService.SETTING_HOLLOW_INGESTION_TTL;
 import static org.elasticsearch.xpack.stateless.commits.HollowShardsService.STATELESS_HOLLOW_INDEX_SHARDS_ENABLED;
@@ -60,37 +56,29 @@ public class StatelessFieldCapabilitiesIT extends AbstractStatelessPluginIntegTe
         assertFieldCaps(indexName);
     }
 
-    @Override
-    protected Settings.Builder nodeSettings() {
-        return super.nodeSettings()
-            // disable rebalancing to avoid spurious shard movements that might cause premature hollowing
-            .put(CLUSTER_ROUTING_REBALANCE_ENABLE_SETTING.getKey(), Rebalance.NONE)
-            // disable index balance decider to avoid spurious shard movements that might cause premature hollowing
-            .put(INDEX_BALANCE_DECIDER_ENABLED_SETTING.getKey(), false)
-            // disable estimated heap decider to avoid spurious shard movements that might cause premature hollowing
-            .put(CLUSTER_ROUTING_ALLOCATION_ESTIMATED_HEAP_THRESHOLD_DECIDER_ENABLED.getKey(), false);
-    }
-
     public void testFieldCapsForHollowShards() throws Exception {
         startMasterOnlyNode();
-        var indexNodeSettings = Settings.builder()
+        final var indexNodeSettings = Settings.builder()
             .put(STATELESS_HOLLOW_INDEX_SHARDS_ENABLED.getKey(), true)
             .put(SETTING_HOLLOW_INGESTION_TTL.getKey(), TimeValue.timeValueMillis(1))
             .build();
-        var indexNodeA = startIndexNode(indexNodeSettings);
+        final var indexNodeA = startIndexNode(indexNodeSettings);
+        final var indexNodeB = startIndexNode(indexNodeSettings);
         startSearchNode();
-        ensureStableCluster(3);
+        ensureStableCluster(4);
 
         var indexName = randomIndexName();
         int numberOfShards = randomIntBetween(1, 5);
-        assertAcked(prepareCreate(indexName).setMapping(mapping).setSettings(indexSettings(numberOfShards, 1)));
+        assertAcked(
+            prepareCreate(indexName).setMapping(mapping)
+                .setSettings(indexSettings(numberOfShards, 1).put("index.routing.allocation.exclude._name", indexNodeB))
+        );
         ensureGreen(indexName);
         insertDocs(indexName);
         flush(indexName);
 
         assertFieldCaps(indexName);
 
-        String indexNodeB = startIndexNode(indexNodeSettings);
         hollowShards(indexName, numberOfShards, indexNodeA, indexNodeB);
 
         assertFieldCaps(indexName);

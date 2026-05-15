921510bf54f0c859df895b22975f04b041dd1e57
Craig Taverner <craig@amanzi.com>
2026-05-15 12:01:46 +0200

    Backporting to 9.3 removing shape support

--- x-pack/plugin/esql/compute/src/test/java/org/elasticsearch/compute/aggregation/spatial/CentroidGroupingAggregatorTests.java
+++ x-pack/plugin/esql/compute/src/test/java/org/elasticsearch/compute/aggregation/spatial/CentroidGroupingAggregatorTests.java
@@ -13,8 +13,6 @@ import org.elasticsearch.compute.data.BlockUtils;
 import org.elasticsearch.compute.data.IntVector;
 import org.elasticsearch.compute.operator.DriverContext;
 import org.elasticsearch.compute.test.ComputeTestCase;
-import org.elasticsearch.lucene.spatial.CoordinateEncoder;
-import org.elasticsearch.lucene.spatial.DimensionalShapeType;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
@@ -32,33 +30,12 @@ public class CentroidGroupingAggregatorTests extends ComputeTestCase {
         var blockFactory = blockFactory();
         var driverContext = new DriverContext(blockFactory.bigArrays(), blockFactory, null);
         try (
-            var state = CentroidPointAggregator.initGrouping(blockFactory.bigArrays(), CoordinateEncoder.GEO);
+            var state = CentroidPointAggregator.initGrouping(blockFactory.bigArrays());
             IntVector selected = blockFactory.newIntArrayVector(new int[] { 0, 1 }, 2);
-            var evaluationContext = new GroupingAggregatorEvaluationContext(driverContext)
         ) {
+            var evaluationContext = new GroupingAggregatorEvaluationContext(driverContext);
             CentroidPointAggregator.combineIntermediate(state, 0, 10.0, 0.0, 20.0, 0.0, 1L);
             try (Block result = CentroidPointAggregator.evaluateFinal(state, selected, evaluationContext)) {
-                assertThat(result.getPositionCount(), equalTo(2));
-                assertThat("in-bounds group should have a centroid result", BlockUtils.toJavaObject(result, 0), notNullValue());
-                assertThat(
-                    "out-of-bounds group should produce null, not ArrayIndexOutOfBoundsException",
-                    BlockUtils.toJavaObject(result, 1),
-                    nullValue()
-                );
-            }
-        }
-    }
-
-    public void testShapeCentroidEvaluateFinalWithOutOfBoundsSelectedGroup() {
-        var blockFactory = blockFactory();
-        var driverContext = new DriverContext(blockFactory.bigArrays(), blockFactory, null);
-        try (
-            var state = CentroidShapeAggregator.initGrouping(blockFactory.bigArrays(), CoordinateEncoder.GEO);
-            IntVector selected = blockFactory.newIntArrayVector(new int[] { 0, 1 }, 2);
-            var evaluationContext = new GroupingAggregatorEvaluationContext(driverContext)
-        ) {
-            CentroidShapeAggregator.combineIntermediate(state, 0, 10.0, 0.0, 20.0, 0.0, 1.0, DimensionalShapeType.POINT.ordinal());
-            try (Block result = CentroidShapeAggregator.evaluateFinal(state, selected, evaluationContext)) {
                 assertThat(result.getPositionCount(), equalTo(2));
                 assertThat("in-bounds group should have a centroid result", BlockUtils.toJavaObject(result, 0), notNullValue());
                 assertThat(

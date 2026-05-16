2d338d395213496f96e27723c9fc4d8dacd3e89e
Nhat Nguyen <nhat.nguyen@elastic.co>
2026-05-16 00:30:11 -0700

    Fix alias id when drop all aggregates (#135247) (#149246)
    When dropping all aggregates, we create a synthetic aggregate referring
    to the first grouping. However, we use the ID of the first aggregate
    instead of the first grouping, which can lead to a single attribute
    referring to multiple channels when building the layout.

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1112,6 +1112,8 @@ public class EsqlCapabilities {
          * Create new block when filtering OrdinalBytesRefBlock
          */
         FIX_FILTER_ORDINALS,
+
+        FIX_ALIAS_ID_WHEN_DROP_ALL_AGGREGATES,
         // Last capability should still have a comma for fewer merge conflicts when adding new ones :)
         // This comment prevents the semicolon from being on the previous capability when Spotless formats the file.
         ;

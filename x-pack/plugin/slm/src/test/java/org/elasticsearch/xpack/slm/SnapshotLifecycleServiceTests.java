da2e684256554bd956d74789789dbf53f9806f75
Eugene Rizhkov <yevgeny.rizhkov@elastic.co>
2026-05-17 13:11:49 +0300

    Register ARS probe settings in SLM test fixture

--- x-pack/plugin/slm/src/test/java/org/elasticsearch/xpack/slm/SnapshotLifecycleServiceTests.java
+++ x-pack/plugin/slm/src/test/java/org/elasticsearch/xpack/slm/SnapshotLifecycleServiceTests.java
@@ -492,6 +492,8 @@ public class SnapshotLifecycleServiceTests extends ESTestCase {
                     Arrays.asList(
                         MasterService.MASTER_SERVICE_SLOW_TASK_LOGGING_THRESHOLD_SETTING,
                         OperationRouting.USE_ADAPTIVE_REPLICA_SELECTION_SETTING,
+                        OperationRouting.ADAPTIVE_REPLICA_SELECTION_PROBE_ENABLED_SETTING,
+                        OperationRouting.ADAPTIVE_REPLICA_SELECTION_PROBE_INFLIGHT_CAP_SETTING,
                         OperationRouting.ADAPTIVE_REPLICA_SELECTION_WARMUP_SAMPLES_SETTING,
                         ClusterService.USER_DEFINED_METADATA,
                         ClusterApplierService.CLUSTER_SERVICE_SLOW_TASK_LOGGING_THRESHOLD_SETTING,

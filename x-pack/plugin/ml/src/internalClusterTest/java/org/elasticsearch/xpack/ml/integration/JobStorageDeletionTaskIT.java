b1f09f3c5759d9dd0c8fad99425e23c6c452996e
Eugene Rizhkov <yevgeny.rizhkov@elastic.co>
2026-05-17 15:47:12 +0300

    Add probe settings to more test fixtures

--- x-pack/plugin/ml/src/internalClusterTest/java/org/elasticsearch/xpack/ml/integration/JobStorageDeletionTaskIT.java
+++ x-pack/plugin/ml/src/internalClusterTest/java/org/elasticsearch/xpack/ml/integration/JobStorageDeletionTaskIT.java
@@ -73,6 +73,8 @@ public class JobStorageDeletionTaskIT extends BaseMlIntegTestCase {
                     MasterService.MASTER_SERVICE_SLOW_TASK_LOGGING_THRESHOLD_SETTING,
                     ResultsPersisterService.PERSIST_RESULTS_MAX_RETRIES,
                     OperationRouting.USE_ADAPTIVE_REPLICA_SELECTION_SETTING,
+                    OperationRouting.ADAPTIVE_REPLICA_SELECTION_PROBE_ENABLED_SETTING,
+                    OperationRouting.ADAPTIVE_REPLICA_SELECTION_PROBE_INFLIGHT_CAP_SETTING,
                     OperationRouting.ADAPTIVE_REPLICA_SELECTION_WARMUP_SAMPLES_SETTING,
                     ClusterService.USER_DEFINED_METADATA,
                     ClusterApplierService.CLUSTER_SERVICE_SLOW_TASK_LOGGING_THRESHOLD_SETTING,

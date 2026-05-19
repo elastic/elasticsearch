5905afde38004a0a6f6fc1ae371aa8e1987e5367
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-05-19 16:53:41 +0000

    [CI] Auto commit changes from spotless

--- server/src/test/java/org/elasticsearch/indices/recovery/ThrottlingRecoveryServiceTests.java
+++ server/src/test/java/org/elasticsearch/indices/recovery/ThrottlingRecoveryServiceTests.java
@@ -22,7 +22,6 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;

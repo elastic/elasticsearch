e82ad2b7ee07e3eb5bdc577d924bdcb605015c7a
Yang Wang <yang.wang@elastic.co>
2026-05-20 10:39:45 +1000

    [Test] Ensure snapshot file deduplication between stateful and stateless snapshots
    This PR enhances the existing test to also check the snapshot files are
    deduplicated properly between snapshots taken with stateless snapshot
    disabled and enabled. While this is tested indirectly in other places,
    such as MetadataSnapshotBlobStoreDirectoryTests, this change tests it
    directly.

--- x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/snapshots/StatelessSnapshotIT.java
+++ x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/snapshots/StatelessSnapshotIT.java
@@ -7,6 +7,7 @@
 
 package org.elasticsearch.xpack.stateless.snapshots;
 
+import org.apache.lucene.index.IndexFileNames;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.support.SubscribableListener;
 import org.elasticsearch.cluster.SnapshotsInProgress;
@@ -23,10 +24,15 @@ import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.engine.EngineConfig;
 import org.elasticsearch.index.engine.EngineFactory;
 import org.elasticsearch.index.shard.ShardId;
+import org.elasticsearch.index.snapshots.blobstore.BlobStoreIndexShardSnapshot;
+import org.elasticsearch.index.snapshots.blobstore.SnapshotFiles;
 import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.plugins.IndexStorePlugin;
 import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.repositories.IndexId;
 import org.elasticsearch.repositories.RepositoriesService;
+import org.elasticsearch.repositories.RepositoryData;
+import org.elasticsearch.repositories.blobstore.BlobStoreRepository;
 import org.elasticsearch.snapshots.SnapshotException;
 import org.elasticsearch.snapshots.SnapshotState;
 import org.elasticsearch.telemetry.TestTelemetryPlugin;
@@ -62,6 +68,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
 import static org.elasticsearch.cluster.coordination.FollowersChecker.FOLLOWER_CHECK_INTERVAL_SETTING;
 import static org.elasticsearch.cluster.coordination.FollowersChecker.FOLLOWER_CHECK_RETRY_COUNT_SETTING;
@@ -99,7 +106,7 @@ public class StatelessSnapshotIT extends AbstractStatelessPluginIntegTestCase {
         return false;
     }
 
-    public void testStatelessSnapshotReadsFromObjectStore() {
+    public void testStatelessSnapshotReadsFromObjectStore() throws IOException {
         final var indexNodeName = startMasterAndIndexNode(
             Settings.builder().put(ObjectStoreService.TYPE_SETTING.getKey(), ObjectStoreService.ObjectStoreType.MOCK).build()
         );
@@ -158,6 +165,49 @@ public class StatelessSnapshotIT extends AbstractStatelessPluginIntegTestCase {
         updateClusterSettings(Settings.builder().put(STATELESS_SNAPSHOT_ENABLED_SETTING.getKey(), "disabled"));
         createSnapshot(repoName, "snap-3", List.of(indexName), List.of());
         assertFalse(snapshotReadSeen.get());
+
+        // Verify the snapshots should have expected deduplication, i.e. the later snapshot should reference files from the earlier
+        // ones without re-creating them.
+        final var repositoriesService = internalCluster().getInstance(RepositoriesService.class, indexNodeName);
+        final var repositoryData = safeAwait(
+            (ActionListener<RepositoryData> l) -> repositoriesService.getRepositoryData(ProjectId.DEFAULT, repoName, l)
+        );
+        final IndexId indexId = repositoryData.resolveIndexId(indexName);
+        final var repo = (BlobStoreRepository) repositoriesService.repository(ProjectId.DEFAULT, repoName);
+        final var blobStoreIndexShardSnapshots = repo.getBlobStoreIndexShardSnapshots(
+            indexId,
+            0,
+            repositoryData.shardGenerations().getShardGen(indexId, 0)
+        );
+
+        // Build a map of snapshot names to their corresponding file names, excluding the segment_N file since it is
+        // unique to each snapshot due to new commit being created in between.
+        final Map<String, List<String>> snapshotToFiles = blobStoreIndexShardSnapshots.snapshots()
+            .stream()
+            .collect(
+                Collectors.toUnmodifiableMap(
+                    SnapshotFiles::snapshot,
+                    snapshotFiles -> snapshotFiles.indexFiles()
+                        .stream()
+                        .map(BlobStoreIndexShardSnapshot.FileInfo::name)
+                        .filter(
+                            physicalName -> blobStoreIndexShardSnapshots.findNameFile(physicalName)
+                                .metadata()
+                                .name()
+                                .startsWith(IndexFileNames.SEGMENTS + "_") == false
+                        )
+                        .collect(Collectors.toList())
+                )
+            );
+
+        assertTrue(
+            "unmatched snapshot files: " + snapshotToFiles,
+            snapshotToFiles.get("snap-2").containsAll(snapshotToFiles.get("snap-1"))
+        );
+        assertTrue(
+            "unmatched snapshot files: " + snapshotToFiles,
+            snapshotToFiles.get("snap-3").containsAll(snapshotToFiles.get("snap-2"))
+        );
     }
 
     public void testStatelessSnapshotBasic() throws Exception {

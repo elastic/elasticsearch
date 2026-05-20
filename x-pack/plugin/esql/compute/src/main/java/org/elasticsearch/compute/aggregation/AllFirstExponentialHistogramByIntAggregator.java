205d776e8de2e5adcaed9e9797abdd03dfdb399c
Lefteris Zervakis <lefteris.zervakis@elastic.co>
2026-05-20 15:45:40 +0100

    Add AllFirstExponentialHistogramByIntAggregator for FIRST(exp_histo, int)
    Adds the hand-written aggregator source for the
    integer-sort-key branch of
    FIRST(exponential_histogram_field, int_sort_field)

---
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/aggregation/AllFirstExponentialHistogramByIntAggregator.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.compute.aggregation;
+
+import org.elasticsearch.compute.ann.Aggregator;
+import org.elasticsearch.compute.ann.GroupingAggregator;
+import org.elasticsearch.compute.ann.IntermediateState;
+import org.elasticsearch.compute.data.Block;
+import org.elasticsearch.compute.data.IntVector;
+import org.elasticsearch.compute.operator.DriverContext;
+import org.elasticsearch.exponentialhistogram.ExponentialHistogram;
+
+/**
+ * A time-series aggregation function that collects the first occurrence exponential histogram of a time series in a specified interval.
+ */
+@Aggregator(
+    processNulls = true,
+    value = {
+        @IntermediateState(name = "sortKeys", type = "LONG"),
+        @IntermediateState(name = "values", type = "EXPONENTIAL_HISTOGRAM"),
+        @IntermediateState(name = "seen", type = "BOOLEAN") }
+)
+@GroupingAggregator()
+public class AllFirstExponentialHistogramByIntAggregator {
+    public static String describe() {
+        return "all_first_ExponentialHistogram_by_int";
+    }
+
+    public static ExponentialHistogramStates.WithLongSingleState initSingle(DriverContext driverContext) {
+        return new ExponentialHistogramStates.WithLongSingleState(driverContext.breaker());
+    }
+
+    public static void combine(ExponentialHistogramStates.WithLongSingleState current, ExponentialHistogram value, int sortKey) {
+        if (current.isSeen() == false || (long) sortKey < current.longValue()) {
+            current.set((long) sortKey, value);
+        }
+    }
+
+    public static void combineIntermediate(
+        ExponentialHistogramStates.WithLongSingleState current,
+        long sortKey,
+        ExponentialHistogram value,
+        boolean seen
+    ) {
+        if (seen) {
+            if (current.isSeen()) {
+                combine(current, value, (int) sortKey);
+            } else {
+                current.set(sortKey, value);
+            }
+        }
+    }
+
+    public static Block evaluateFinal(ExponentialHistogramStates.WithLongSingleState current, DriverContext ctx) {
+        return current.evaluateFinalHistogram(ctx);
+    }
+
+    public static ExponentialHistogramStates.WithLongGroupingState initGrouping(DriverContext driverContext) {
+        return new ExponentialHistogramStates.WithLongGroupingState(driverContext.bigArrays(), driverContext.breaker());
+    }
+
+    public static void combine(
+        ExponentialHistogramStates.WithLongGroupingState current,
+        int groupId,
+        ExponentialHistogram value,
+        int sortKey
+    ) {
+        if (current.seen(groupId) == false || (long) sortKey < current.longValue(groupId)) {
+            current.set(groupId, (long) sortKey, value);
+        }
+    }
+
+    public static void combineIntermediate(
+        ExponentialHistogramStates.WithLongGroupingState current,
+        int groupId,
+        long timestamp,
+        ExponentialHistogram value,
+        boolean seen
+    ) {
+        if (seen) {
+            combine(current, groupId, value, (int) timestamp);
+        }
+    }
+
+    public static Block evaluateFinal(
+        ExponentialHistogramStates.WithLongGroupingState state,
+        IntVector selected,
+        GroupingAggregatorEvaluationContext ctx
+    ) {
+        return state.evaluateFinalHistograms(selected, ctx.driverContext());
+    }
+}

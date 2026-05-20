73c519c8d6ed5364fdcf180aac9d360a9acef7e7
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-05-20 13:20:53 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/IndexResolver.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/session/IndexResolver.java
@@ -51,7 +51,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;

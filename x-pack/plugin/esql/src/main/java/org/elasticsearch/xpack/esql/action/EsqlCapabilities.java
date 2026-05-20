a8e855c5acaf117fef73ccc7b903bb2a6498021e
Lefteris Zervakis <lefteris.zervakis@elastic.co>
2026-05-20 14:33:30 +0100

    Keeping a single capability

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -2842,12 +2842,6 @@ public class EsqlCapabilities {
          */
         FIRST_AGG_EXTENDED_TYPES,
 
-        /**
-         * Support EARLIEST aggregation on extended types: version, unsigned_long, geo_point,
-         * cartesian_point, geo_shape, cartesian_shape, geohash, geotile, geohex.
-         */
-        EARLIEST_AGG_EXTENDED_TYPES,
-
         // Last capability should still have a comma for fewer merge conflicts when adding new ones :)
         // This comment prevents the semicolon from being on the previous capability when Spotless formats the file.
         ;

cb28b90b35b00026d2b5a0ebc3a4f4fc949abf94
Bogdan Pintea <bogdan.pintea@elastic.co>
2026-05-20 17:41:44 +0200

    ESQL:DS: Add extra tests for metadata
    This adds more tests around:
    - handling of metadata attributes in the Analyzer.
    - real pruning of files by _file.modified.

--- x-pack/plugin/esql/qa/testFixtures/src/main/java/org/elasticsearch/xpack/esql/TestAnalyzer.java
+++ x-pack/plugin/esql/qa/testFixtures/src/main/java/org/elasticsearch/xpack/esql/TestAnalyzer.java
@@ -23,6 +23,7 @@ import org.elasticsearch.xpack.esql.core.querydsl.QueryDslTimestampBoundsExtract
 import org.elasticsearch.xpack.esql.core.tree.Source;
 import org.elasticsearch.xpack.esql.datasources.ExternalSourceMetadata;
 import org.elasticsearch.xpack.esql.datasources.ExternalSourceResolution;
+import org.elasticsearch.xpack.esql.datasources.ExternalSourceResolver;
 import org.elasticsearch.xpack.esql.datasources.spi.FileList;
 import org.elasticsearch.xpack.esql.enrich.ResolvedEnrichPolicy;
 import org.elasticsearch.xpack.esql.expression.function.EsqlFunctionRegistry;
@@ -397,7 +398,8 @@ public class TestAnalyzer {
     }
 
     /**
-     * Set external source resolution.
+     * Set external source resolution. Enriches the schema with {@code _file.*} metadata columns
+     * to mirror the production path in {@link ExternalSourceResolver}.
      */
     public TestAnalyzer externalSourceResolution(String path, List<Attribute> schema, FileList fileSet) {
         var metadata = new ExternalSourceMetadata() {
@@ -416,7 +418,8 @@ public class TestAnalyzer {
                 return "parquet";
             }
         };
-        var resolvedSource = new ExternalSourceResolution.ResolvedSource(metadata, fileSet, java.util.Map.of());
+        var enriched = ExternalSourceResolver.enrichSchemaWithFileMetadataColumns(metadata);
+        var resolvedSource = new ExternalSourceResolution.ResolvedSource(enriched, fileSet, java.util.Map.of());
         return externalSourceResolution(new ExternalSourceResolution(Map.of(path, resolvedSource)));
     }
 

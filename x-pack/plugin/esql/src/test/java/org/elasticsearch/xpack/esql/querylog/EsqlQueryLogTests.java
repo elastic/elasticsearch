18f6797ade849cfdaa62d8fe15bbb3e0d1e4fe20
Stanislav Malyshev <smalyshev@users.noreply.github.com>
2026-05-21 00:36:19 -0600

    [9.4] Add deprecation message for ESQL log (#149013) (#149554)
    * Add deprecation message for ESQL log (#149013)
    
    * Add deprecation message for ESQL log
    
    * fix tests

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/querylog/EsqlQueryLogTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/querylog/EsqlQueryLogTests.java
@@ -97,6 +97,7 @@ public class EsqlQueryLogTests extends ESTestCase {
 
     public void testPrioritiesOnSuccess() {
         EsqlQueryLog queryLog = new EsqlQueryLog(settings, mockLogFieldProvider());
+        assertWarnings(EsqlQueryLog.DEPRECATION_MESSAGE);
         String query = "from " + randomAlphaOfLength(10);
 
         long[] actualTook = {
@@ -148,6 +149,7 @@ public class EsqlQueryLogTests extends ESTestCase {
 
     public void testPrioritiesOnFailure() {
         EsqlQueryLog queryLog = new EsqlQueryLog(settings, mockLogFieldProvider());
+        assertWarnings(EsqlQueryLog.DEPRECATION_MESSAGE);
         String query = "from " + randomAlphaOfLength(10);
 
         long[] actualTook = {

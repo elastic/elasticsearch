64f2f9004bc2cd7302c570e86156db54e848e577
Martijn van Groningen <martijn.v.groningen@gmail.com>
2026-05-21 10:43:33 +0200

    keep parity between standard and columnar modes when it comes to norms on text fields.

--- server/src/main/java/org/elasticsearch/index/mapper/TextFieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/TextFieldMapper.java
@@ -335,8 +335,8 @@ public final class TextFieldMapper extends FieldMapper {
             IndexMode indexMode = indexSettings.getMode();
             this.norms = Parameter.normsParam(m -> ((TextFieldMapper) m).norms, () -> {
                 if (indexSettings.getIndexVersionCreated().onOrAfter(IndexVersions.DISABLE_NORMS_BY_DEFAULT_FOR_LOGSDB_AND_TSDB)) {
-                    // don't enable norms by default if the index mode is columnar.
-                    return indexMode == null || !indexMode.isColumnar();
+                    // don't enable norms by default if the index is LOGSDB or TSDB based
+                    return indexMode != IndexMode.LOGSDB && indexMode != IndexMode.TIME_SERIES && indexMode != IndexMode.LOGSDB_COLUMNAR;
                 }
                 // bwc - historically, norms were enabled by default on text fields regardless of which index mode was used
                 return true;

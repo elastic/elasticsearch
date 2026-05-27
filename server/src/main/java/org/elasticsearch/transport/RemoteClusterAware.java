13ad5af8ddfb0f1e9acf7130ede8107cd78ecb0f
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2026-05-27 10:51:58 +0200

    More

--- server/src/main/java/org/elasticsearch/transport/RemoteClusterAware.java
+++ server/src/main/java/org/elasticsearch/transport/RemoteClusterAware.java
@@ -221,7 +221,7 @@ public abstract class RemoteClusterAware implements LinkedProjectConfigService.L
                             )
                         );
                     } else {
-                        // an index exclusion is syntactic sugar for including the cluster but excluding the index,
+                        // an index exclusion like -remote:logs is syntactic sugar for including the cluster but excluding the index,
                         // so we need to add the cluster to the list of included clusters
                         everIncluded.addAll(clusters);
                         for (String clusterName : clusters) {

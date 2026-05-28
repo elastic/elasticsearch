65ccfbaa6d366170ec618a48d45489e816109f19
tballison <tim.allison@elastic.co>
2026-05-28 15:20:52 -0400

    make append a node feature

--- server/src/main/java/org/elasticsearch/rest/action/synonyms/SynonymCapabilities.java
+++ server/src/main/java/org/elasticsearch/rest/action/synonyms/SynonymCapabilities.java
@@ -21,11 +21,7 @@ public final class SynonymCapabilities {
     }
 
     private static final String SYNONYMS_REFRESH_PARAM = "synonyms_refresh_param";
-    private static final String SYNONYMS_APPEND_PARAM = "synonyms_append_param";
 
     /** Capabilities for {@code PUT /_synonyms/{id}/{ruleId}} and {@code DELETE /_synonyms/{id}/{ruleId}}. */
     public static final Set<String> CAPABILITIES = Set.of(SYNONYMS_REFRESH_PARAM);
-
-    /** Capabilities for {@code PUT /_synonyms/{id}}. */
-    public static final Set<String> PUT_SYNONYM_SET_CAPABILITIES = Set.of(SYNONYMS_REFRESH_PARAM, SYNONYMS_APPEND_PARAM);
 }

7af2b0bd2425ceb584c3f6c80786228ca1c30dcd
Bogdan Pintea <bogdan.pintea@elastic.co>
2026-05-28 15:51:39 +0200

    test fix

--- x-pack/plugin/esql-datasource-orc/src/test/java/org/elasticsearch/xpack/esql/datasource/orc/OrcFormatReaderTests.java
+++ x-pack/plugin/esql-datasource-orc/src/test/java/org/elasticsearch/xpack/esql/datasource/orc/OrcFormatReaderTests.java
@@ -1326,7 +1326,7 @@ public class OrcFormatReaderTests extends ESTestCase {
         return total;
     }
 
-    public void testCorruptOrcFileProducesIllegalArgumentException() throws Exception {
+    public void testCorruptOrcFileDoesNotProduceElasticsearchException() throws Exception {
         TypeDescription schema = TypeDescription.createStruct().addField("id", TypeDescription.createLong());
         byte[] orcData = createOrcFile(schema, batch -> {
             batch.size = 10;
@@ -1336,21 +1336,23 @@ public class OrcFormatReaderTests extends ESTestCase {
             }
         });
 
-        // ORC format: 3-byte "ORC" magic at start, then postscript at end.
-        // Corrupt the data stripe area (skip the 3-byte header, stop before the tail).
         int corruptStart = 3;
         int corruptEnd = orcData.length / 2;
         java.util.Arrays.fill(orcData, corruptStart, corruptEnd, (byte) 0xFF);
 
         StorageObject storageObject = createStorageObject(orcData);
         OrcFormatReader reader = new OrcFormatReader(blockFactory);
-        expectThrows(IllegalArgumentException.class, () -> {
+        Exception ex = expectThrows(Exception.class, () -> {
             try (CloseableIterator<Page> iterator = reader.read(storageObject, null, 100)) {
                 while (iterator.hasNext()) {
                     iterator.next().releaseBlocks();
                 }
             }
         });
+        assertFalse(
+            "Corrupt ORC data should not produce ElasticsearchException (HTTP 500), got: " + ex.getClass().getName(),
+            ex instanceof org.elasticsearch.ElasticsearchException
+        );
     }
 
     // --- ORC file creation helpers ---

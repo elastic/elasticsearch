1762f2245a150f4c0451a9f4bb9ae4f3589367a4
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2026-05-29 17:00:23 +0200

    Implement GET /_prometheus/api/v1/status/buildinfo

---
+++ x-pack/plugin/prometheus/src/main/java/org/elasticsearch/xpack/prometheus/rest/PrometheusStatusBuildInfoRestAction.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.prometheus.rest;
+
+import org.elasticsearch.Build;
+import org.elasticsearch.client.internal.node.NodeClient;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.rest.BaseRestHandler;
+import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.rest.RestResponse;
+import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.rest.Scope;
+import org.elasticsearch.rest.ServerlessScope;
+import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xcontent.json.JsonXContent;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.elasticsearch.rest.RestRequest.Method.GET;
+
+/**
+ * REST handler for {@code GET /_prometheus/api/v1/status/buildinfo}.
+ * Returns build information in the standard Prometheus buildinfo format.
+ * Grafana queries this endpoint to identify the backend type and determine available alerting features.
+ * Without it, Grafana shows an error when opening the "Plugin info" panel in Metrics Drilldown.
+ */
+@ServerlessScope(Scope.PUBLIC)
+public class PrometheusStatusBuildInfoRestAction extends BaseRestHandler {
+
+    @Override
+    public String getName() {
+        return "prometheus_status_buildinfo_action";
+    }
+
+    @Override
+    public List<Route> routes() {
+        return List.of(new Route(GET, "/_prometheus/api/v1/status/buildinfo"));
+    }
+
+    @Override
+    protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) throws IOException {
+        Build build = Build.current();
+        XContentBuilder builder = JsonXContent.contentBuilder();
+        builder.startObject();
+        {
+            builder.field("status", "success");
+            builder.startObject("data");
+            {
+                builder.field("application", "Elasticsearch");
+                builder.field("version", build.version());
+                builder.field("revision", build.hash());
+                builder.field("buildDate", build.date());
+            }
+            builder.endObject();
+        }
+        builder.endObject();
+        String body = Strings.toString(builder);
+        return channel -> channel.sendResponse(new RestResponse(RestStatus.OK, "application/json", body));
+    }
+}

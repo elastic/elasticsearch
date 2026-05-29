254c0550e5848b8d1898414e974d372e8d4f3663
Fang Xing <155562079+fang-xing-esql@users.noreply.github.com>
2026-05-29 18:57:06 -0400

    [ES|QL] Enable `TS` as a source command inside a subquery (#149485)
    * TS as subquery source command

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1331,6 +1331,11 @@ public class EsqlCapabilities {
         SUBQUERY_WITH_ROW(Build.current().isSnapshot()),
 
         /**
+         * Support TS as a source command inside subquery in the from command.
+         */
+        SUBQUERY_WITH_TS(Build.current().isSnapshot()),
+
+        /**
          * Support for views in cluster state (and REST API).
          */
         VIEWS_IN_CLUSTER_STATE,

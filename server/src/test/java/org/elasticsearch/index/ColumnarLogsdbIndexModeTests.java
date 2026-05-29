ef38e4ead3475466b4ac6a1c90f7068fb4e8d237
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-05-29 20:40:12 +0000

    [CI] Auto commit changes from spotless

--- server/src/test/java/org/elasticsearch/index/ColumnarLogsdbIndexModeTests.java
+++ server/src/test/java/org/elasticsearch/index/ColumnarLogsdbIndexModeTests.java
@@ -50,10 +50,7 @@ public class ColumnarLogsdbIndexModeTests extends ESTestCase {
     public void testColumnarLogsdbSerializationFailsOnOlderTransportVersion() throws IOException {
         try (BytesStreamOutput out = new BytesStreamOutput()) {
             out.setTransportVersion(TransportVersionUtils.getPreviousVersion(IndexMode.COLUMNAR_INDEX_MODES_ADDED));
-            IllegalStateException e = expectThrows(
-                IllegalStateException.class,
-                () -> IndexMode.writeTo(IndexMode.LOGSDB_COLUMNAR, out)
-            );
+            IllegalStateException e = expectThrows(IllegalStateException.class, () -> IndexMode.writeTo(IndexMode.LOGSDB_COLUMNAR, out));
             assertThat(e.getMessage(), containsString("[logsdb_columnar] doesn't support serialization with transport version"));
         }
     }

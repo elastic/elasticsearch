611eb7dc9cfe378ea4420bdd9a5903b234b9be0c
Martijn van Groningen <martijn.v.groningen@gmail.com>
2026-06-01 20:39:22 +0200

    Add getLongValues() to load sorted numeric doc vakues as SortedNumericLongValues and if dense the return SortedNumericLongValues wraps a DensLongValues instance.

--- server/src/main/java/org/elasticsearch/index/fielddata/SortedNumericLongValues.java
+++ server/src/main/java/org/elasticsearch/index/fielddata/SortedNumericLongValues.java
@@ -10,6 +10,8 @@
 package org.elasticsearch.index.fielddata;
 
 import org.apache.lucene.index.DocValues;
+import org.apache.lucene.index.DocValuesSkipper;
+import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.NumericDocValues;
 import org.apache.lucene.index.SortedNumericDocValues;
 import org.apache.lucene.search.DocIdSetIterator;
@@ -142,6 +144,22 @@ public abstract class SortedNumericLongValues {
     }
 
     /**
+     * Retrieves sorted numeric long values from sorted numeric doc values with specified field name.
+     * Checks if the field is dense and returns a {@link DenseLongValues} instance if so.
+     *
+     * @param fieldName the name of the field for which to retrieve the numeric values
+     * @param reader the leaf reader from which to retrieve the field's values
+     * @return a {@link SortedNumericLongValues} instance representing the sorted numeric long values of the field
+     * @throws IOException if an I/O error occurs while retrieving the values
+     */
+    public static SortedNumericLongValues getLongValues(String fieldName, LeafReader reader) throws IOException {
+        SortedNumericDocValues values = DocValues.getSortedNumeric(reader, fieldName);
+        DocValuesSkipper skipper = reader.getDocValuesSkipper(fieldName);
+        boolean isDense = skipper != null && skipper.docCount() == reader.maxDoc();
+        return wrap(values, isDense);
+    }
+
+    /**
      * Converts a {@link SortedNumericDocValues} iterator to a {@link SortedNumericLongValues}
      *
      * Note that if the wrapped iterator can be unwrapped to a singleton {@link NumericDocValues}
@@ -149,19 +167,38 @@ public abstract class SortedNumericLongValues {
      * a {@link LongValues} instance via {@link #unwrapSingleton(SortedNumericLongValues)}
      */
     public static SortedNumericLongValues wrap(SortedNumericDocValues values) {
+        return wrap(values, false);
+    }
+
+    static SortedNumericLongValues wrap(SortedNumericDocValues values, boolean isDense) {
         final NumericDocValues singleton = DocValues.unwrapSingleton(values);
         if (singleton != null) {
-            final LongValues longValues = new LongValues() {
-                @Override
-                public long longValue() throws IOException {
-                    return singleton.longValue();
-                }
+            final LongValues longValues;
+            if (isDense) {
+                longValues = new DenseLongValues() {
+                    @Override
+                    protected void doAdvanceExact(int doc) throws IOException {
+                        singleton.advanceExact(doc);
+                    }
 
-                @Override
-                public boolean advanceExact(int doc) throws IOException {
-                    return singleton.advanceExact(doc);
-                }
-            };
+                    @Override
+                    public long longValue() throws IOException {
+                        return singleton.longValue();
+                    }
+                };
+            } else {
+                longValues = new LongValues() {
+                    @Override
+                    public long longValue() throws IOException {
+                        return singleton.longValue();
+                    }
+
+                    @Override
+                    public boolean advanceExact(int doc) throws IOException {
+                        return singleton.advanceExact(doc);
+                    }
+                };
+            }
             return singleton(longValues);
         } else {
             return new SortedNumericLongValues(false, values) {

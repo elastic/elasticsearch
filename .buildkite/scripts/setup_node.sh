23e619860011886dc7951800ff340b24b378f706
Brian Seeders <brian.seeders@elastic.co>
2026-06-01 14:10:51 -0400

    refreshenv

--- .buildkite/scripts/setup_node.sh
+++ .buildkite/scripts/setup_node.sh
@@ -4,7 +4,7 @@ cd .buildkite
 
 if command -v choco > /dev/null; then # Windows
   choco install nodejs --version="24.16.0"
-  npm install -g corepack
+  call refreshenv
 else # Linux
   if ! command -v nvm > /dev/null; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash

73a816ef6d8817ffbd19a00746e884ab6a915389
Donal Evans <donal.evans@elastic.co>
2026-06-01 15:55:47 -0700

    Validate inference embedding model before checking for existing uses (#150150)
    This commit changes the order of two steps when creating an inference
    endpoint in TransportPutInferenceModelAction. The validation call is now
    performed prior to checking for existing index mappings using the
    inference endpoint ID, since if dimensions are not specified by the user
    when creating a text_embedding or embedding endpoint, then attempting to
    create a MinimalServiceSettings from the request will throw, because
    dimensions are required in the MinimalServiceSettings constructor.
    
    The validation call will populate the dimensions for the model via a
    call to updateModelWithEmbeddingDetails(), meaning that the dimensions
    are guaranteed to be non-null when TransportPutInferenceModelAction
    checks for existing uses of the inference endpoint ID.
    
    In order to properly test the fix for this bug, changes were necessary
    in TestDenseInferenceServiceExtension, to allow dimensions to be null
    in the service settings, and to implement
    updateModelWithEmbeddingDetails() so that when the dimensions are not
    specified in the request, the model can be updated with a default value,
    mimicking the behaviour of "real" providers, which have a default
    embedding size that isused when no dimensions are specified.
    
    Closes #150084
    Closes #147062
    
    (cherry picked from commit d41ba315e728318bda742430fe7ce141f26a8049)
    
    # Conflicts:
    #	x-pack/plugin/inference/qa/test-service-plugin/src/main/java/org/elasticsearch/xpack/inference/mock/TestDenseInferenceServiceExtension.java

--- x-pack/plugin/inference/qa/inference-service-tests/src/javaRestTest/java/org/elasticsearch/xpack/inference/InferenceBaseRestTest.java
+++ x-pack/plugin/inference/qa/inference-service-tests/src/javaRestTest/java/org/elasticsearch/xpack/inference/InferenceBaseRestTest.java
@@ -185,6 +185,21 @@ public class InferenceBaseRestTest extends ESRestTestCase {
             """, dimensions);
     }
 
+    static String mockTextEmbeddingServiceModelConfig_NoDimensions() {
+        return Strings.format("""
+            {
+              "task_type": "text_embedding",
+              "service": "text_embedding_test_service",
+              "service_settings": {
+                "model": "my_dense_vector_model",
+                "api_key": "abc64"
+              },
+              "task_settings": {
+              }
+            }
+            """);
+    }
+
     static String mockRerankServiceModelConfig() {
         return """
             {
@@ -217,6 +232,21 @@ public class InferenceBaseRestTest extends ESRestTestCase {
               }
             }
             """, dimensions);
+    }
+
+    static String mockEmbeddingServiceModelConfig_NoDimensions() {
+        return Strings.format("""
+            {
+              "task_type": "embedding",
+              "service": "text_embedding_test_service",
+              "service_settings": {
+                "model": "my_dense_vector_model",
+                "api_key": "abc64"
+              },
+              "task_settings": {
+              }
+            }
+            """);
     }
 
     static void deleteModel(String modelId) throws IOException {

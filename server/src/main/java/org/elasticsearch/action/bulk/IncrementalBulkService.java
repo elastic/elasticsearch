c76b9f6b313392cbbac176959e9206cbac4936e7
zhubotang-wq <zhubo.tang@elastic.co>
2026-06-02 13:30:02 -0700

    2770 Cancellable TransportWriteAction.

--- server/src/main/java/org/elasticsearch/action/bulk/IncrementalBulkService.java
+++ server/src/main/java/org/elasticsearch/action/bulk/IncrementalBulkService.java
@@ -9,7 +9,6 @@
 
 package org.elasticsearch.action.bulk;
 
-import org.elasticsearch.TransportVersion;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.DocWriteRequest;
 import org.elasticsearch.action.support.ActiveShardCount;
@@ -48,7 +47,6 @@ public class IncrementalBulkService {
     private final Client client;
     private final AtomicBoolean enabledForTests = new AtomicBoolean(true);
     private final IndexingPressure indexingPressure;
-    public static TransportVersion CANCELLABLE_BULK_OPERATIONS = TransportVersion.fromName("cancellable_bulk_operations");
 
     /* Capture in milliseconds because the APM histogram only has a range of 100,000 */
     private final LongHistogram chunkWaitTimeMillisHistogram;

f638fc5c2db0f611185a761cad47aedb0e325579
Ankur Srivastava <ankur.srivastava@elastic.co>
2026-06-02 10:38:11 -0400

    Address pr comments

--- modules/repository-s3/src/test/java/org/elasticsearch/repositories/s3/S3BlobContainerStorageClassTests.java
+++ modules/repository-s3/src/test/java/org/elasticsearch/repositories/s3/S3BlobContainerStorageClassTests.java
@@ -24,7 +24,6 @@ import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.network.InetAddresses;
 import org.elasticsearch.common.settings.MockSecureSettings;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.util.BigArrays;
 import org.elasticsearch.common.util.concurrent.DeterministicTaskQueue;
 import org.elasticsearch.core.IOUtils;
@@ -43,6 +42,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.mockito.Mockito;
 
+import software.amazon.awssdk.services.s3.model.StorageClass;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -57,19 +58,6 @@ import static org.elasticsearch.repositories.s3.S3ClientSettingsTests.DEFAULT_RE
 public class S3BlobContainerStorageClassTests extends ESTestCase {
 
     private static final String BUCKET = "bucket";
-
-    /**
-     * Minimum allowed multipart upload size in the S3 client. Any multipart upload via
-     * {@link S3BlobContainer#writeBlob} must be at least this large.
-     */
-    private static final ByteSizeValue MULTIPART_BUFFER_SIZE = S3Repository.MIN_PART_SIZE_USING_MULTIPART;
-
-    /**
-     * A buffer size small enough to trigger the multipart path inside {@link S3BlobContainer#writeMetadataBlob},
-     * which uses {@link org.elasticsearch.repositories.blobstore.ChunkedBlobOutputStream} directly and does not
-     * enforce the 5 MB per-part minimum that {@link S3BlobContainer#writeBlob} does.
-     */
-    private static final ByteSizeValue SMALL_BUFFER_SIZE = ByteSizeValue.ofBytes(100);
 
     private static HttpServer httpServer;
     private static S3Service service;
@@ -113,23 +101,10 @@ public class S3BlobContainerStorageClassTests extends ESTestCase {
         super.tearDown();
     }
 
-    private S3BlobContainer buildContainer(@Nullable String dataStorageClass, @Nullable String metadataStorageClass) {
-        return buildContainer(dataStorageClass, metadataStorageClass, null, SMALL_BUFFER_SIZE);
-    }
-
     private S3BlobContainer buildContainer(
         @Nullable String dataStorageClass,
         @Nullable String metadataStorageClass,
         @Nullable String fallbackStorageClass
-    ) {
-        return buildContainer(dataStorageClass, metadataStorageClass, fallbackStorageClass, SMALL_BUFFER_SIZE);
-    }
-
-    private S3BlobContainer buildContainer(
-        @Nullable String dataStorageClass,
-        @Nullable String metadataStorageClass,
-        @Nullable String fallbackStorageClass,
-        ByteSizeValue bufferSize
     ) {
         final String clientName = randomIdentifier();
         final Settings.Builder clientSettings = Settings.builder();
@@ -162,7 +137,7 @@ public class S3BlobContainerStorageClassTests extends ESTestCase {
             service,
             BUCKET,
             false,
-            bufferSize,
+            S3Repository.MIN_PART_SIZE_USING_MULTIPART,
             S3Repository.CANNED_ACL_SETTING.getDefault(Settings.EMPTY),
             fallbackStorageClass != null ? fallbackStorageClass : S3Repository.FALLBACK_STORAGE_CLASS_SETTING.getDefault(Settings.EMPTY),
             dataStorageClass != null ? dataStorageClass : S3Repository.DATA_STORAGE_CLASS_SETTING.getDefault(Settings.EMPTY),
@@ -182,121 +157,143 @@ public class S3BlobContainerStorageClassTests extends ESTestCase {
         return "/" + BUCKET + "/" + blobName;
     }
 
+    private void assertStorageClass(String STANDARD_IA, String blobName) {
+        assertEquals(STANDARD_IA, s3HttpHandler.blobs().get(blobKey(blobName)).storageClass());
+    }
+
+    private String getRandomAllowedStorageClassString() {
+        return randomFrom(S3BlobStore.ALLOWED_STORAGE_CLASSES.stream().map(StorageClass::toString).toList());
+    }
+
     public void testDataStorageClassSentOnSingleBlobUpload() throws IOException {
-        final S3BlobContainer container = buildContainer("standard_ia", null);
+        String dataStorageClass = getRandomAllowedStorageClassString();
+        final S3BlobContainer container = buildContainer(dataStorageClass, null, null);
         final String blobName = randomIdentifier();
 
         container.writeBlob(OperationPurpose.SNAPSHOT_DATA, blobName, new BytesArray(randomByteArrayOfLength(64)), false);
 
-        assertEquals("STANDARD_IA", s3HttpHandler.blobs().get(blobKey(blobName)).storageClass());
+        assertStorageClass(dataStorageClass, blobName);
     }
 
     public void testMetadataStorageClassSentOnSingleBlobUpload() throws IOException {
-        final S3BlobContainer container = buildContainer(null, "intelligent_tiering");
+        String metadataStorageClass = getRandomAllowedStorageClassString();
+        final S3BlobContainer container = buildContainer(null, metadataStorageClass, null);
         final String blobName = randomIdentifier();
 
         container.writeBlob(OperationPurpose.SNAPSHOT_METADATA, blobName, new BytesArray(randomByteArrayOfLength(64)), false);
 
-        assertEquals("INTELLIGENT_TIERING", s3HttpHandler.blobs().get(blobKey(blobName)).storageClass());
+        assertStorageClass(metadataStorageClass, blobName);
     }
 
     public void testFallbackStorageClassUsedForNonSnapshotPurpose() throws IOException {
-        final S3BlobContainer container = buildContainer("standard_ia", "intelligent_tiering", "reduced_redundancy");
+        String fallbackStorageClass = getRandomAllowedStorageClassString();
+        final S3BlobContainer container = buildContainer(
+            getRandomAllowedStorageClassString(),
+            getRandomAllowedStorageClassString(),
+            fallbackStorageClass);
+
         final String blobName = randomIdentifier();
 
         container.writeBlob(OperationPurpose.CLUSTER_STATE, blobName, new BytesArray(randomByteArrayOfLength(64)), false);
 
-        assertEquals("REDUCED_REDUNDANCY", s3HttpHandler.blobs().get(blobKey(blobName)).storageClass());
+        assertStorageClass(fallbackStorageClass, blobName);
     }
 
     public void testFallbackStorageClassUsedWhenDataClassNotSet() throws IOException {
-        final S3BlobContainer container = buildContainer(null, null, "reduced_redundancy");
+        String fallbackStorageClass = getRandomAllowedStorageClassString();
+        final S3BlobContainer container = buildContainer(null, null, fallbackStorageClass);
         final String blobName = randomIdentifier();
 
         container.writeBlob(OperationPurpose.SNAPSHOT_DATA, blobName, new BytesArray(randomByteArrayOfLength(64)), false);
 
-        assertEquals("REDUCED_REDUNDANCY", s3HttpHandler.blobs().get(blobKey(blobName)).storageClass());
+        assertStorageClass(fallbackStorageClass, blobName);
     }
 
     public void testFallbackStorageClassUsedWhenMetadataClassNotSet() throws IOException {
-        final S3BlobContainer container = buildContainer(null, null, "reduced_redundancy");
+        String fallbackStorageClass = getRandomAllowedStorageClassString();
+        final S3BlobContainer container = buildContainer(null, null, fallbackStorageClass);
         final String blobName = randomIdentifier();
 
         container.writeBlob(OperationPurpose.SNAPSHOT_METADATA, blobName, new BytesArray(randomByteArrayOfLength(64)), false);
 
-        assertEquals("REDUCED_REDUNDANCY", s3HttpHandler.blobs().get(blobKey(blobName)).storageClass());
+        assertStorageClass(fallbackStorageClass, blobName);
     }
 
     public void testDefaultStorageClassUsedWhenNothingConfigured() throws IOException {
-        final S3BlobContainer container = buildContainer(null, null);
+        final S3BlobContainer container = buildContainer(null, null, null);
         final String blobName = randomIdentifier();
 
         container.writeBlob(OperationPurpose.SNAPSHOT_DATA, blobName, new BytesArray(randomByteArrayOfLength(64)), false);
 
-        assertEquals("STANDARD", s3HttpHandler.blobs().get(blobKey(blobName)).storageClass());
+        assertStorageClass("STANDARD", blobName);
     }
 
     public void testDataStorageClassSentOnMultipartUpload() throws IOException {
-        final S3BlobContainer container = buildContainer("standard_ia", null, null, MULTIPART_BUFFER_SIZE);
+        String dataStorageClass = getRandomAllowedStorageClassString();
+        final S3BlobContainer container = buildContainer(dataStorageClass, null, null);
         final String blobName = randomIdentifier();
         // Write more than MULTIPART_BUFFER_SIZE to trigger multipart; writeBlob also requires >= MIN_PART_SIZE_USING_MULTIPART
-        final byte[] data = randomByteArrayOfLength((int) MULTIPART_BUFFER_SIZE.getBytes() + 1);
+        final byte[] data = randomByteArrayOfLength((int) S3Repository.MIN_PART_SIZE_USING_MULTIPART.getBytes() + 1);
 
         container.writeBlob(OperationPurpose.SNAPSHOT_DATA, blobName, new BytesArray(data), false);
 
-        assertEquals("STANDARD_IA", s3HttpHandler.blobs().get(blobKey(blobName)).storageClass());
+        assertStorageClass(dataStorageClass, blobName);
     }
 
     public void testMetadataStorageClassSentOnMultipartUpload() throws IOException {
-        final S3BlobContainer container = buildContainer(null, "intelligent_tiering", null, MULTIPART_BUFFER_SIZE);
+        String metadataStorageClass = getRandomAllowedStorageClassString();
+        final S3BlobContainer container = buildContainer(null, metadataStorageClass, null);
         final String blobName = randomIdentifier();
-        final byte[] data = randomByteArrayOfLength((int) MULTIPART_BUFFER_SIZE.getBytes() + 1);
+        final byte[] data = randomByteArrayOfLength((int) S3Repository.MIN_PART_SIZE_USING_MULTIPART.getBytes() + 1);
 
         container.writeBlob(OperationPurpose.SNAPSHOT_METADATA, blobName, new BytesArray(data), false);
 
-        assertEquals("INTELLIGENT_TIERING", s3HttpHandler.blobs().get(blobKey(blobName)).storageClass());
+        assertStorageClass(metadataStorageClass, blobName);
     }
 
     public void testMetadataStorageClassSentOnWriteMetadataBlob() throws IOException {
-        final S3BlobContainer container = buildContainer(null, "intelligent_tiering");
+        String metadataStorageClass = getRandomAllowedStorageClassString();
+        final S3BlobContainer container = buildContainer(null, metadataStorageClass, null);
         final String blobName = randomIdentifier();
-        // Write more than SMALL_BUFFER_SIZE to trigger the multipart path inside writeMetadataBlob
-        final byte[] data = randomByteArrayOfLength((int) SMALL_BUFFER_SIZE.getBytes() + 1);
+        // Write more than MIN_PART_SIZE_USING_MULTIPART to trigger the multipart path inside writeMetadataBlob
+        final byte[] data = randomByteArrayOfLength((int) S3Repository.MIN_PART_SIZE_USING_MULTIPART.getBytes() + 1);
 
         container.writeMetadataBlob(OperationPurpose.SNAPSHOT_METADATA, blobName, false, false, out -> out.write(data));
 
-        assertEquals("INTELLIGENT_TIERING", s3HttpHandler.blobs().get(blobKey(blobName)).storageClass());
+        assertStorageClass(metadataStorageClass, blobName);
     }
 
     public void testDataStorageClassSentOnCopyBlob() throws IOException {
-        final S3BlobContainer container = buildContainer("standard_ia", null);
+        String dataStorageClass = getRandomAllowedStorageClassString();
+        final S3BlobContainer container = buildContainer(dataStorageClass, null, null);
         final String sourceBlobName = randomIdentifier();
         final String destBlobName = randomIdentifier();
         final byte[] data = randomByteArrayOfLength(64);
 
         // Write source with a non-snapshot purpose so it uses the fallback (STANDARD)
         container.writeBlob(OperationPurpose.CLUSTER_STATE, sourceBlobName, new BytesArray(data), false);
-        assertEquals("STANDARD", s3HttpHandler.blobs().get(blobKey(sourceBlobName)).storageClass());
+        assertStorageClass("STANDARD", sourceBlobName);
 
         // Copy with SNAPSHOT_DATA purpose — destination should receive the configured data storage class
         container.copyBlob(OperationPurpose.SNAPSHOT_DATA, container, sourceBlobName, destBlobName, data.length);
 
-        assertEquals("STANDARD_IA", s3HttpHandler.blobs().get(blobKey(destBlobName)).storageClass());
+        assertStorageClass(dataStorageClass, destBlobName);
     }
 
     public void testMetadataStorageClassSentOnCopyBlob() throws IOException {
-        final S3BlobContainer container = buildContainer(null, "intelligent_tiering");
+        String metadataStorageClass = getRandomAllowedStorageClassString();
+        final S3BlobContainer container = buildContainer(null, metadataStorageClass, null);
         final String sourceBlobName = randomIdentifier();
         final String destBlobName = randomIdentifier();
         final byte[] data = randomByteArrayOfLength(64);
 
         // Write source with a non-snapshot purpose so it uses the fallback (STANDARD)
         container.writeBlob(OperationPurpose.CLUSTER_STATE, sourceBlobName, new BytesArray(data), false);
-        assertEquals("STANDARD", s3HttpHandler.blobs().get(blobKey(sourceBlobName)).storageClass());
+        assertStorageClass("STANDARD", sourceBlobName);
 
         // Copy with SNAPSHOT_METADATA purpose — destination should receive the configured metadata storage class
         container.copyBlob(OperationPurpose.SNAPSHOT_METADATA, container, sourceBlobName, destBlobName, data.length);
 
-        assertEquals("INTELLIGENT_TIERING", s3HttpHandler.blobs().get(blobKey(destBlobName)).storageClass());
+        assertStorageClass(metadataStorageClass, destBlobName);
     }
 }

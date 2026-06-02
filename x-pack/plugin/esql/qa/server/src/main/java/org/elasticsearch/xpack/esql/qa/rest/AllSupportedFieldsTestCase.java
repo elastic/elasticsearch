0536413b2c07713ae0589e8967cfd03feb5bffcd
Martijn van Groningen <martijn.v.groningen@gmail.com>
2026-06-02 17:33:00 +0200

    Fix AllSupportedFieldsIT for logsdb_columnar _id reader
    In strict columnar index modes, _id is read via IdDocValuesReader
    rather than falling back to stored fields.

--- x-pack/plugin/esql/qa/server/src/main/java/org/elasticsearch/xpack/esql/qa/rest/AllSupportedFieldsTestCase.java
+++ x-pack/plugin/esql/qa/server/src/main/java/org/elasticsearch/xpack/esql/qa/rest/AllSupportedFieldsTestCase.java
@@ -1487,7 +1487,9 @@ public class AllSupportedFieldsTestCase extends ESRestTestCase {
             "_id",
             indexMode == IndexMode.TIME_SERIES
                 ? matchesList().item("column_at_a_time:TsIdFieldReader")
-                : matchesList().item("column_at_a_time:null").item("row_stride:BlockStoredFieldsReader.Id")
+                : indexMode.isStrictColumnar()
+                    ? matchesList().item("column_at_a_time:IdDocValuesReader")
+                    : matchesList().item("column_at_a_time:null").item("row_stride:BlockStoredFieldsReader.Id")
         )
             .entry("_ignored", matchesList().item("column_at_a_time:constant_nulls"))
             .entry("_index_mode", matchesList().item(startsWith("column_at_a_time:constant")))

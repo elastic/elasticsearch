e966135f7eb1a9a659bcc7e8d7cc8804cfa9884e
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-06-03 11:46:35 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/indices/recovery/RecoveriesCollection.java
+++ server/src/main/java/org/elasticsearch/indices/recovery/RecoveriesCollection.java
@@ -19,7 +19,6 @@ import org.elasticsearch.index.shard.IndexShardClosedException;
 import org.elasticsearch.index.shard.ShardId;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CopyOnWriteArrayList;

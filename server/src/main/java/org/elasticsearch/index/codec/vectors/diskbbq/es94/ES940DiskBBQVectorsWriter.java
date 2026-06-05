23a77ecb307254dd38f6a417fff4aad89fd13214
Tommaso Teofili <tommaso.teofili@elastic.co>
2026-06-05 14:51:24 +0200

    minor

--- server/src/main/java/org/elasticsearch/index/codec/vectors/diskbbq/es94/ES940DiskBBQVectorsWriter.java
+++ server/src/main/java/org/elasticsearch/index/codec/vectors/diskbbq/es94/ES940DiskBBQVectorsWriter.java
@@ -836,7 +836,7 @@ public class ES940DiskBBQVectorsWriter extends IVFVectorsWriter {
 
     private CentroidAssignments calculateCentroids(
         HierarchicalKMeans<float[]> hierarchicalKMeans,
-        ClusteringFloatVectorValues floatVectorValues,
+        KMeansFloatVectorValues floatVectorValues,
         FieldInfo fieldInfo
     ) throws IOException {
         KMeansResult<float[]> kMeansResult = hierarchicalKMeans.cluster(floatVectorValues, vectorPerCluster);

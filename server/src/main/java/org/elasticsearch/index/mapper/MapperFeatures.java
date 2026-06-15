4844cbd3b73d4881972a627ee2197781e98f7251
Parker Timmins <parker.timmins@elastic.co>
2026-06-15 10:39:50 -0500

    Gate case-insensitive regexp keyword DV tests on cluster feature
    Old nodes throw "Match flags not yet implemented [256]" for regexp
    queries with case_insensitive: true on doc-values-only keyword fields.
    Add a NodeFeature and requires blocks to skip these tests in mixed
    clusters where old nodes are present.

--- server/src/main/java/org/elasticsearch/index/mapper/MapperFeatures.java
+++ server/src/main/java/org/elasticsearch/index/mapper/MapperFeatures.java
@@ -106,6 +106,9 @@ public class MapperFeatures implements FeatureSpecification {
         "mapper.analyzer-wrapper.reloadable_search_analyzer"
     );
     static final NodeFeature STORE_NOT_ALLOWED_IN_COLUMNAR_INDEX_MODE = new NodeFeature("mapper.columnar.store_not_allowed");
+    public static final NodeFeature KEYWORD_DV_CASE_INSENSITIVE_REGEXP = new NodeFeature(
+        "mapper.keyword.doc_values_case_insensitive_regexp"
+    );
 
     @Override
     public Set<NodeFeature> getTestFeatures() {
@@ -179,7 +182,8 @@ public class MapperFeatures implements FeatureSpecification {
             ANALYZER_WRAPPER_RELOADABLE_SEARCH_ANALYZER,
             ROUTING_AS_DOC_VALUES,
             ROUTING_AS_DOC_VALUES_BY_DEFAULT,
-            STORE_NOT_ALLOWED_IN_COLUMNAR_INDEX_MODE
+            STORE_NOT_ALLOWED_IN_COLUMNAR_INDEX_MODE,
+            KEYWORD_DV_CASE_INSENSITIVE_REGEXP
         );
     }
 }

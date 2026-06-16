3ba8ecbebaa09d7493ed6a4b5800b50f6fd9c26c
Fang Xing <155562079+fang-xing-esql@users.noreply.github.com>
2026-06-16 12:53:49 -0400

    [ES|QL] Enable IN subquery with view (#150322)
    * support in subquery with view
    Co-authored-by: Craig Taverner <craig@amanzi.com>

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/action/EsqlCapabilities.java
@@ -1367,6 +1367,12 @@ public class EsqlCapabilities {
         WHERE_IN_SUBQUERY_WITHOUT_VIEW(Build.current().isSnapshot()),
 
         /**
+         * Support IN non-correlated subqueries in WHERE command with View. The views can be referenced by IN subqueries, and the view
+         * definition can contain IN subqueries.
+         */
+        WHERE_IN_SUBQUERY_WITH_VIEW(Build.current().isSnapshot()),
+
+        /**
          * Support ROW as a source command inside subquery in the from command.
          */
         SUBQUERY_WITH_ROW(Build.current().isSnapshot()),

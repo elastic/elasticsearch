260461ac68bf75228e6cdb65d8de32adcad36f1f
Mike Pellegrini <mike.pellegrini@elastic.co>
2026-06-16 10:42:26 -0400

    Added SemanticFieldContentTests

---
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/mapper/SemanticFieldContentTests.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.inference.mapper;
+
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.xcontent.XContentHelper;
+import org.elasticsearch.inference.InferenceString;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.xcontent.DeprecationHandler;
+import org.elasticsearch.xcontent.NamedXContentRegistry;
+import org.elasticsearch.xcontent.ToXContent;
+import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xcontent.XContentFactory;
+import org.elasticsearch.xcontent.XContentParser;
+import org.elasticsearch.xcontent.XContentType;
+import org.elasticsearch.xcontent.support.MapXContentParser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class SemanticFieldContentTests extends ESTestCase {
+
+    public void testNullValue() {
+        SemanticFieldContent content = new SemanticFieldContent(null);
+        assertNull(content.getMapValue(randomIntBetween(0, 10)));
+        assertNull(content.getInferenceStringValue(randomIntBetween(0, 10)));
+        assertThrows(IndexOutOfBoundsException.class, () -> content.getChunkText(0, randomIntBetween(1, 10)));
+    }
+
+    public void testRandomInputs() throws IOException {
+        for (int i = 0; i < 50; i++) {
+            List<Object> inputs = randomList(1, 10, () -> SemanticTextFieldTests.randomSemanticInput(true));
+            List<Object> materialized = materialize(inputs);
+            SemanticFieldContent content = new SemanticFieldContent(materialized);
+            assertSemanticFieldContent(inputs, content);
+        }
+    }
+
+    /**
+     * Converts each {@link InferenceString} in the input list to its xContent {@link Map} representation, simulating
+     * the JSON round-trip that occurs when values are written to and read from {@code _source}.
+     */
+    private static List<Object> materialize(List<Object> inputs) throws IOException {
+        List<Object> result = new ArrayList<>(inputs.size());
+        for (Object input : inputs) {
+            if (input instanceof InferenceString infString) {
+                XContentBuilder builder = XContentFactory.jsonBuilder();
+                infString.toXContent(builder, ToXContent.EMPTY_PARAMS);
+                Map<String, Object> map = XContentHelper.convertToMap(BytesReference.bytes(builder), false, XContentType.JSON).v2();
+                result.add(map);
+            } else {
+                result.add(input);
+            }
+        }
+        return result;
+    }
+
+    private static void assertSemanticFieldContent(List<Object> inputs, SemanticFieldContent content) throws IOException {
+        int i = 0;
+        int cursor = 0;
+        for (Object input : inputs) {
+            if (input instanceof InferenceString infString) {
+                Map<?, ?> mapValue = content.getMapValue(i);
+                assertNotNull("expected map value at index " + i, mapValue);
+
+                InferenceString parsedMapValue = parseMapAsInferenceString(mapValue);
+                assertEquals(infString, parsedMapValue);
+                assertEquals(infString, content.getInferenceStringValue(i));
+            } else {
+                assertNull("expected no map value at index " + i, content.getMapValue(i));
+                assertNull("expected no inference string value at index " + i, content.getInferenceStringValue(i));
+                String expected = input.toString();
+                assertEquals(expected, content.getChunkText(cursor, cursor + expected.length()));
+                cursor += expected.length() + 1;
+            }
+
+            i++;
+        }
+    }
+
+    private static InferenceString parseMapAsInferenceString(Map<?, ?> mapValue) throws IOException {
+        @SuppressWarnings("unchecked")
+        Map<String, Object> stringKeyedMap = (Map<String, Object>) mapValue;
+
+        try (
+            XContentParser parser = new MapXContentParser(
+                NamedXContentRegistry.EMPTY,
+                DeprecationHandler.IGNORE_DEPRECATIONS,
+                stringKeyedMap,
+                XContentType.JSON
+            )
+        ) {
+            return InferenceString.PARSER.parse(parser, null);
+        }
+    }
+}

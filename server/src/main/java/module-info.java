2b6d25be952e6b867cbb183b194b3cd406f10cb8
iverase <ivera@apache.org>
2026-06-18 11:22:38 +0100

    [Slices] Add support for multiple slices in DenseVectorFieldMapper

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -57,6 +57,7 @@ module org.elasticsearch.server {
     requires org.apache.lucene.queryparser;
     requires org.apache.lucene.sandbox;
     requires org.apache.lucene.suggest;
+    requires org.jspecify;
 
     exports org.elasticsearch;
     exports org.elasticsearch.action;

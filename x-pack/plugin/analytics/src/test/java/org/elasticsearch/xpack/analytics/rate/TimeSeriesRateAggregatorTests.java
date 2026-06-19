1be3cafd8f3241476f6b87d2ab99a6a66b0340b1
Kostas Krikellas <131142368+kkrik-es@users.noreply.github.com>
2026-06-19 19:05:25 +0300

    Remove redundant parameter from TimeSeriesRateAggregatorTests

--- x-pack/plugin/analytics/src/test/java/org/elasticsearch/xpack/analytics/rate/TimeSeriesRateAggregatorTests.java
+++ x-pack/plugin/analytics/src/test/java/org/elasticsearch/xpack/analytics/rate/TimeSeriesRateAggregatorTests.java
@@ -212,7 +212,6 @@ public class TimeSeriesRateAggregatorTests extends AggregatorTestCase {
             IndexMode.TIME_SERIES,
             false,
             false,
-            true,
             true
         );
     }

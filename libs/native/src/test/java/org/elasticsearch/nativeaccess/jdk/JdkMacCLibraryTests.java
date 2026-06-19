f9d7208eb65a131236b1aedfc4b1b78c907a9ec4
Chris Hegarty <62058229+ChrisHegarty@users.noreply.github.com>
2026-06-19 17:48:54 +0100

    Update JdkMacCLibraryTests.java

--- libs/native/src/test/java/org/elasticsearch/nativeaccess/jdk/JdkMacCLibraryTests.java
+++ libs/native/src/test/java/org/elasticsearch/nativeaccess/jdk/JdkMacCLibraryTests.java
@@ -10,6 +10,7 @@
 package org.elasticsearch.nativeaccess.jdk;
 
 import org.apache.lucene.util.Constants;
+import org.elasticsearch.foreign.MemorySegmentUtil;
 import org.elasticsearch.nativeaccess.NativeAccess;
 import org.elasticsearch.nativeaccess.jdk.JdkMacCLibrary.JdkErrorReference;
 import org.elasticsearch.nativeaccess.lib.MacCLibrary;

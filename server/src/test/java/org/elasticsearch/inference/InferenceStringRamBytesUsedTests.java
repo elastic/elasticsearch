b90a55f4ad3257641b5a680fb12758c042bf1ce2
Tim Grein <grein.tim97@gmail.com>
2026-06-22 14:15:39 +0200

    Add InferenceStringRamBytesUsedTests

---
+++ server/src/test/java/org/elasticsearch/inference/InferenceStringRamBytesUsedTests.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+package org.elasticsearch.inference;
+
+import java.util.List;
+
+public class InferenceStringRamBytesUsedTests extends InferenceObjectRamBytesUsedTest<InferenceString>{
+
+    private static final String VALUE = "value";
+
+    @Override
+    public InferenceString objectToEstimate() {
+        return new InferenceString(DataType.TEXT, DataFormat.TEXT, VALUE);
+    }
+
+    @Override
+    public List<InferenceString> objectsToEstimateWithLargerInput() {
+        return List.of(
+            // Larger value
+            new InferenceString(DataType.TEXT, DataFormat.TEXT, VALUE.repeat(5))
+        );
+    }
+}

420b2a431ccab32441f13ae6565149c59932161f
Adam Demjen <demjened@gmail.com>
2026-06-23 15:56:18 +0200

    Fixing tests

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/amazonbedrock/completion/AmazonBedrockChatCompletionServiceSettingsTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/amazonbedrock/completion/AmazonBedrockChatCompletionServiceSettingsTests.java
@@ -10,10 +10,13 @@ package org.elasticsearch.xpack.inference.services.amazonbedrock.completion;
 import org.elasticsearch.TransportVersion;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.io.stream.Writeable;
+import org.elasticsearch.core.Nullable;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentFactory;
+import org.elasticsearch.xcontent.XContentParser;
 import org.elasticsearch.xcontent.XContentType;
-import org.elasticsearch.xpack.core.ml.AbstractBWCWireSerializationTestCase;
+import org.elasticsearch.xpack.inference.services.ConfigurationParseContext;
+import org.elasticsearch.xpack.inference.services.amazonbedrock.AbstractAmazonBedrockServiceSettingsTests;
 import org.elasticsearch.xpack.inference.services.amazonbedrock.AmazonBedrockProvider;
 import org.elasticsearch.xpack.inference.services.settings.RateLimitSettings;
 import org.elasticsearch.xpack.inference.services.settings.RateLimitSettingsTests;
@@ -22,17 +25,40 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.elasticsearch.xpack.inference.services.amazonbedrock.AbstractAmazonBedrockServiceSettingsTests.TEST_MODEL_ID;
-import static org.elasticsearch.xpack.inference.services.amazonbedrock.AbstractAmazonBedrockServiceSettingsTests.TEST_PROVIDER;
-import static org.elasticsearch.xpack.inference.services.amazonbedrock.AbstractAmazonBedrockServiceSettingsTests.TEST_RATE_LIMIT;
-import static org.elasticsearch.xpack.inference.services.amazonbedrock.AbstractAmazonBedrockServiceSettingsTests.TEST_REGION;
 import static org.elasticsearch.xpack.inference.services.amazonbedrock.AmazonBedrockConstants.MODEL_FIELD;
 import static org.elasticsearch.xpack.inference.services.amazonbedrock.AmazonBedrockConstants.PROVIDER_FIELD;
 import static org.elasticsearch.xpack.inference.services.amazonbedrock.AmazonBedrockConstants.REGION_FIELD;
 import static org.hamcrest.Matchers.is;
 
-public class AmazonBedrockChatCompletionServiceSettingsTests extends AbstractBWCWireSerializationTestCase<
+public class AmazonBedrockChatCompletionServiceSettingsTests extends AbstractAmazonBedrockServiceSettingsTests<
     AmazonBedrockChatCompletionServiceSettings> {
+
+    @Override
+    protected AmazonBedrockChatCompletionServiceSettings fromMap(Map<String, Object> map, ConfigurationParseContext context) {
+        return AmazonBedrockChatCompletionServiceSettings.fromMap(map, context);
+    }
+
+    @Override
+    protected Map<String, Object> buildCommonServiceSettingsMap(String region, String model, String provider, Integer rateLimit) {
+        return buildServiceSettingsMap(region, model, provider, rateLimit);
+    }
+
+    @Override
+    protected AmazonBedrockChatCompletionServiceSettings createServiceSettings(
+        String region,
+        String model,
+        String provider,
+        RateLimitSettings rateLimitSettings
+    ) {
+        return new AmazonBedrockChatCompletionServiceSettings(region, model, AmazonBedrockProvider.fromString(provider), rateLimitSettings);
+    }
+
+    @Override
+    protected AmazonBedrockChatCompletionServiceSettings doParseInstance(XContentParser parser) throws IOException {
+        return AmazonBedrockChatCompletionServiceSettings.createParser(true)
+            .apply(parser, ConfigurationParseContext.PERSISTENT)
+            .build(ConfigurationParseContext.PERSISTENT);
+    }
 
     public void testToXContent_WritesAllValues() throws IOException {
         var entity = new AmazonBedrockChatCompletionServiceSettings(
@@ -108,4 +134,27 @@ public class AmazonBedrockChatCompletionServiceSettingsTests extends AbstractBWC
             RateLimitSettingsTests.createRandom()
         );
     }
+
+    public static Map<String, Object> buildServiceSettingsMap(
+        @Nullable String region,
+        @Nullable String model,
+        @Nullable String provider,
+        @Nullable Integer rateLimit
+    ) {
+        var map = new HashMap<String, Object>();
+        if (region != null) {
+            map.put(REGION_FIELD, region);
+        }
+        if (model != null) {
+            map.put(MODEL_FIELD, model);
+        }
+        if (provider != null) {
+            map.put(PROVIDER_FIELD, provider);
+        }
+        if (rateLimit != null) {
+            map.put(RateLimitSettings.FIELD_NAME, new HashMap<>(Map.of(RateLimitSettings.REQUESTS_PER_MINUTE_FIELD, rateLimit)));
+        }
+        return map;
+    }
+
 }

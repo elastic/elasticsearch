3dec756338d6451461d3873b2a6a125a67cca9de
Kostas Krikellas <kostas.krikellas@elastic.co>
2026-06-23 13:37:52 +0300

    Restore metadataToSnapshot guard for columnar indices
    The "cannot be upgraded" validation during restore parses the stored
    mapping before the "wasn't fully snapshotted" shard check, so storing
    enabled:false at root for columnar indices triggers the columnar root
    guard. Skip the enabled:false mapping modification for columnar indices
    in metadataToSnapshot to avoid this.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/snapshots/sourceonly/SourceOnlySnapshotRepository.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/snapshots/sourceonly/SourceOnlySnapshotRepository.java
@@ -129,7 +129,9 @@ public final class SourceOnlySnapshotRepository extends FilterRepository {
             // that is valid from an operational perspective. ie. it will have all metadata fields like version/
             // seqID etc. and an indexed ID field such that we can potentially perform updates on them or delete documents.
             MappingMetadata mmd = index.mapping();
-            if (mmd != null) {
+            final String indexMode = index.getSettings().get("index.mode");
+            final boolean strictColumnar = "columnar".equals(indexMode) || "logsdb_columnar".equals(indexMode);
+            if (mmd != null && strictColumnar == false) {
                 // we don't need to obey any routing here stuff is read-only anyway and get is disabled
                 final String mapping = "{ \"_doc\" : { \"enabled\": false, \"_meta\": " + mmd.source().string() + " } }";
                 indexMetadataBuilder.putMapping(mapping);

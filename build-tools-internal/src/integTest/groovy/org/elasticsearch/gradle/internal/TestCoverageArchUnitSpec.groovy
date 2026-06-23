ec2361615c69bdb08b5cc903cfc37c72c1250256
Rene Groeschke <rene@elastic.co>
2026-06-23 13:30:07 +0200

    Fix failing test after gradle 9.6.0 work

--- build-tools-internal/src/integTest/groovy/org/elasticsearch/gradle/internal/TestCoverageArchUnitSpec.groovy
+++ build-tools-internal/src/integTest/groovy/org/elasticsearch/gradle/internal/TestCoverageArchUnitSpec.groovy
@@ -70,6 +70,7 @@ class TestCoverageArchUnitSpec extends Specification {
         "org.elasticsearch.gradle.internal.BaseInternalPluginBuildPlugin",
         "org.elasticsearch.gradle.internal.DependenciesInfoPlugin",
         "org.elasticsearch.gradle.internal.ElasticsearchJavaBasePlugin",
+        "org.elasticsearch.gradle.internal.ElasticsearchBasePlugin",
         "org.elasticsearch.gradle.internal.EmbeddedProviderPlugin",
         "org.elasticsearch.gradle.internal.InternalAvailableTcpPortProviderPlugin",
         "org.elasticsearch.gradle.internal.InternalPluginBuildPlugin",

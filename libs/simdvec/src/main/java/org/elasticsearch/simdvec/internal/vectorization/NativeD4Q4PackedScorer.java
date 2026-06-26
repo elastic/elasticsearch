bc16ed68ca5245468b1a5fff39ed18f4719e615c
Simon Cooper <simon.cooper@elastic.co>
2026-06-26 09:45:18 +0100

    Refactor D4Q4Packed and D7Q7 native scorers to use the same infrastructure

--- libs/simdvec/src/main/java/org/elasticsearch/simdvec/internal/vectorization/NativeD4Q4PackedScorer.java
+++ libs/simdvec/src/main/java/org/elasticsearch/simdvec/internal/vectorization/NativeD4Q4PackedScorer.java
@@ -8,47 +8,43 @@
  */
 package org.elasticsearch.simdvec.internal.vectorization;
 
-import org.apache.lucene.index.VectorSimilarityFunction;
 import org.apache.lucene.store.IndexInput;
-import org.elasticsearch.simdvec.ESVectorUtil;
-import org.elasticsearch.simdvec.internal.IndexInputUtils;
-import org.elasticsearch.simdvec.internal.MemorySegmentES92NativeInt7VectorsScorer;
 import org.elasticsearch.simdvec.internal.Similarities;
 
-import java.io.IOException;
 import java.lang.foreign.MemorySegment;
 
-import static org.elasticsearch.simdvec.internal.Similarities.dotProductI7uBulkWithOffsets;
+/**
+ * Packed-nibble int4 scorer that uses existing native dot-product ops.
+ * Returns sentinel values when native support is unavailable so callers can fallback.
+ */
+final class NativeD4Q4PackedScorer extends NativeMemorySegmentScorer {
 
-/** Vectorized scorer for 7-bit symmetric quantized vectors stored as a {@link MemorySegment}. */
-final class NativeD7Q7Scorer extends NativeMemorySegmentScorer {
-
-    NativeD7Q7Scorer(IndexInput in, int dimensions, int dataLength, int bulkSize) {
+    NativeD4Q4PackedScorer(IndexInput in, int dimensions, int dataLength, int bulkSize) {
         super(in, dimensions, dataLength, bulkSize);
     }
 
     @Override
     long dotProduct(MemorySegment dataset, MemorySegment query, int length) {
-        return Similarities.dotProductI7u(query, dataset, length);
+        return Similarities.dotProductI4(query, dataset, length);
     }
 
     @Override
     void dotProductBulk(MemorySegment dataset, MemorySegment query, int length, int count, MemorySegment scores) {
-        Similarities.dotProductI7uBulk(dataset, query, length, count, scores);
+        Similarities.dotProductI4Bulk(dataset, query, length, count, scores);
     }
 
     @Override
     void dotProductBulkWithOffsets(MemorySegment dataset, MemorySegment query, int dataLength, int dataStride, MemorySegment offsets, int offsetsCount, MemorySegment scores) {
-        Similarities.dotProductI7uBulkWithOffsets(dataset, query, dataLength, dataStride, offsets, offsetsCount, scores);
+        Similarities.dotProductI4BulkWithOffsets(dataset, query, dataLength, dataStride, offsets, offsetsCount, scores);
     }
 
     @Override
     float queryBitScale() {
-        return SEVEN_BIT_SCALE;
+        return FOUR_BIT_SCALE;
     }
 
     @Override
     float indexBitScale() {
-        return SEVEN_BIT_SCALE;
+        return FOUR_BIT_SCALE;
     }
 }

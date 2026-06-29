a413d5608be4db0ce6b7270a827e248e3ffcb94a
Albert Zaharovits <email+github@zalbert.me>
2026-06-29 13:24:51 +0300

    This is a backport of 40cafb1f3b3699d11f4329c3bd9c6bb88c9da11a

--- server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
+++ server/src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -523,6 +523,8 @@ public class InternalEngine extends Engine {
                 // we can access segment_stats while a shard is still in the recovering state.
                 case "segments":
                 case "segments_stats":
+                // FieldInfos can be accessed before the external reader is fully warmed up.
+                case "field_has_value":
                     break;
                 default:
                     assert externalReaderManager.isWarmedUp : "searcher was not warmed up yet for source[" + source + "]";

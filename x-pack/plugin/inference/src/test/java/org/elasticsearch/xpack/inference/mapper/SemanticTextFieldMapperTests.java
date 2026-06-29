5c05229ae1bc8366d97138d27672bca29786f30d
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-06-29 18:46:36 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/mapper/SemanticTextFieldMapperTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/mapper/SemanticTextFieldMapperTests.java
@@ -87,7 +87,6 @@ import org.elasticsearch.xpack.inference.services.elastic.ElasticInferenceServic
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;

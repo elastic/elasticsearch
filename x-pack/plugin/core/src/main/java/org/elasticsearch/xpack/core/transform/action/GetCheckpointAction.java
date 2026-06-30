7c0659d2831fac325ea4993f9a0a937e9da4d5d1
David Turner <david.turner@elastic.co>
2026-06-30 15:42:20 +0100

    Rename `LegacyActionRequest` → `UntypedActionRequest`
    It's not "legacy" yet, nor should it be deprecated. We're not yet ready
    to start adding direct (strongly-typed) subclasses of `ActionRequest`.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/transform/action/GetCheckpointAction.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/transform/action/GetCheckpointAction.java
@@ -12,7 +12,7 @@ import org.elasticsearch.action.ActionRequestValidationException;
 import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.action.ActionType;
 import org.elasticsearch.action.IndicesRequest;
-import org.elasticsearch.action.LegacyActionRequest;
+import org.elasticsearch.action.UntypedActionRequest;
 import org.elasticsearch.action.RemoteClusterActionType;
 import org.elasticsearch.action.ResolvedIndexExpressions;
 import org.elasticsearch.action.support.IndicesOptions;
@@ -51,7 +51,7 @@ public class GetCheckpointAction extends ActionType<GetCheckpointAction.Response
         super(NAME);
     }
 
-    public static class Request extends LegacyActionRequest implements IndicesRequest.Replaceable {
+    public static class Request extends UntypedActionRequest implements IndicesRequest.Replaceable {
 
         private String[] indices;
         private final IndicesOptions indicesOptions;

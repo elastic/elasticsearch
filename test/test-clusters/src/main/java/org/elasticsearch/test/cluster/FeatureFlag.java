4e46e1233af87ce0d12ea7ee3d805b2be02fbc9a
Bogdan Pintea <bogdan.pintea@elastic.co>
2026-06-30 10:29:37 +0200

    enable flags in release tests

--- test/test-clusters/src/main/java/org/elasticsearch/test/cluster/FeatureFlag.java
+++ test/test-clusters/src/main/java/org/elasticsearch/test/cluster/FeatureFlag.java
@@ -34,7 +34,11 @@ public enum FeatureFlag {
     ES95_CODEC_FEATURE_FLAG("es.es95_codec_feature_flag_enabled=true", Version.fromString("9.5.0"), null),
     SLICE_INDEXING("es.slice_indexing_feature_flag_enabled=true", Version.fromString("9.5.0"), null),
     ESQL_EXTERNAL_DATASOURCES_LOCAL("es.esql_external_datasources_local_feature_flag_enabled=true", Version.fromString("9.5.0"), null),
-    ESQL_EXTERNAL_DATASOURCES_HTTP("es.esql_external_datasources_http_feature_flag_enabled=true", Version.fromString("9.5.0"), null);
+    ESQL_EXTERNAL_DATASOURCES_HTTP("es.esql_external_datasources_http_feature_flag_enabled=true", Version.fromString("9.5.0"), null),
+    ESQL_EXTERNAL_ORC("es.esql_external_orc_feature_flag_enabled=true", Version.fromString("9.5.0"), null),
+    ESQL_EXTERNAL_GCS("es.esql_external_gcs_feature_flag_enabled=true", Version.fromString("9.5.0"), null),
+    ESQL_EXTERNAL_AZURE("es.esql_external_azure_feature_flag_enabled=true", Version.fromString("9.5.0"), null),
+    ESQL_EXTERNAL_PARQUET_RS("es.esql_external_parquet_rs_feature_flag_enabled=true", Version.fromString("9.5.0"), null);
 
     public final String systemProperty;
     public final Version from;

d2ffc9fc9d34f6e6ca1b1800a70f1ac5f5d52327
Tim Grein <grein.tim97@gmail.com>
2026-07-01 15:02:46 +0200

    Autoformat

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/external/http/StreamingHttpResultPublisherTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/external/http/StreamingHttpResultPublisherTests.java
@@ -641,7 +641,11 @@ public class StreamingHttpResultPublisherTests extends ESTestCase {
         publisher.responseReceived(mock(HttpResponse.class));
         publisher.consumeContent(contentDecoder(message), mock(IOControl.class));
 
-        assertThat("circuitBreaker should have tracked bytes after consuming content", circuitBreaker.getTracked(), equalTo((long) message.length));
+        assertThat(
+            "circuitBreaker should have tracked bytes after consuming content",
+            circuitBreaker.getTracked(),
+            equalTo((long) message.length)
+        );
 
         var subscriber = new TestSubscriber();
         testPublisher().subscribe(subscriber);
@@ -656,13 +660,21 @@ public class StreamingHttpResultPublisherTests extends ESTestCase {
         publisher.responseReceived(mock(HttpResponse.class));
         publisher.consumeContent(contentDecoder(message), mock(IOControl.class));
 
-        assertThat("circuitBreaker should have tracked bytes after consuming content", circuitBreaker.getTracked(), equalTo((long) message.length));
+        assertThat(
+            "circuitBreaker should have tracked bytes after consuming content",
+            circuitBreaker.getTracked(),
+            equalTo((long) message.length)
+        );
 
         var subscriber = new TestSubscriber();
         testPublisher().subscribe(subscriber);
         subscriber.subscription.cancel();
 
-        assertThat("circuitBreaker should have 0 tracked bytes after the subscriber was cancelled", circuitBreaker.getTracked(), equalTo(0L));
+        assertThat(
+            "circuitBreaker should have 0 tracked bytes after the subscriber was cancelled",
+            circuitBreaker.getTracked(),
+            equalTo(0L)
+        );
     }
 
     public void testPublisherCancelReleasesCircuitBreakerBytesOnDrain() throws IOException {
@@ -674,18 +686,26 @@ public class StreamingHttpResultPublisherTests extends ESTestCase {
 
         publisher.cancel();
 
-        assertThat("circuitBreaker should still track bytes immediately after Apache cancelled", circuitBreaker.getTracked(), equalTo((long) message.length));
+        assertThat(
+            "circuitBreaker should still track bytes immediately after Apache cancelled",
+            circuitBreaker.getTracked(),
+            equalTo((long) message.length)
+        );
 
         // Drains rest of the data in the queue
         subscriber.requestData();
 
-        assertThat("circuitBreaker should have 0 tracked bytes after subscriber drains the queue", circuitBreaker.getTracked(), equalTo(0L));
+        assertThat(
+            "circuitBreaker should have 0 tracked bytes after subscriber drains the queue",
+            circuitBreaker.getTracked(),
+            equalTo(0L)
+        );
     }
 
     private static class TestCircuitBreakerWithTracking extends TestCircuitBreaker {
         private long tracked;
 
-        TestCircuitBreakerWithTracking(){
+        TestCircuitBreakerWithTracking() {
             super();
             this.tracked = 0L;
         }

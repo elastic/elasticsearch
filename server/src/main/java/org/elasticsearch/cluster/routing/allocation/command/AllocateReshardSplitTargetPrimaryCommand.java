d1e7c0b68383801d83548888afb3a89e6057759c
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2026-07-02 14:40:44 -0700

    iter

--- server/src/main/java/org/elasticsearch/cluster/routing/allocation/command/AllocateReshardSplitTargetPrimaryCommand.java
+++ server/src/main/java/org/elasticsearch/cluster/routing/allocation/command/AllocateReshardSplitTargetPrimaryCommand.java
@@ -10,6 +10,7 @@
 package org.elasticsearch.cluster.routing.allocation.command;
 
 import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.cluster.metadata.Metadata;
 import org.elasticsearch.cluster.metadata.ProjectId;
 import org.elasticsearch.cluster.routing.RecoverySource;
 import org.elasticsearch.cluster.routing.RoutingNodes;
@@ -18,26 +19,65 @@ import org.elasticsearch.cluster.routing.UnassignedInfo;
 import org.elasticsearch.cluster.routing.allocation.RerouteExplanation;
 import org.elasticsearch.cluster.routing.allocation.RoutingAllocation;
 import org.elasticsearch.cluster.routing.allocation.decider.Decision;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.core.FixForMultiProject;
 import org.elasticsearch.index.IndexNotFoundException;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.index.shard.ShardNotFoundException;
+import org.elasticsearch.xcontent.ObjectParser;
+import org.elasticsearch.xcontent.ParseField;
+import org.elasticsearch.xcontent.XContentParser;
 
+import java.io.IOException;
 import java.util.Optional;
 
 public class AllocateReshardSplitTargetPrimaryCommand extends BasePrimaryAllocationCommand {
-    protected AllocateReshardSplitTargetPrimaryCommand(
-        String index,
-        int shardId,
-        String node,
-        boolean acceptDataLoss,
-        ProjectId projectId
-    ) {
+    public static final String NAME = "allocate_reshard_split_target_primary";
+    public static final ParseField COMMAND_NAME_FIELD = new ParseField(NAME);
+
+    private static final ObjectParser<AllocateReshardSplitTargetPrimaryCommand.Builder, ProjectId> PARSER = BasePrimaryAllocationCommand
+        .createAllocatePrimaryParser(NAME);
+
+    public AllocateReshardSplitTargetPrimaryCommand(String index, int shardId, String node, boolean acceptDataLoss, ProjectId projectId) {
         super(index, shardId, node, acceptDataLoss, projectId);
+    }
+
+    @FixForMultiProject(description = "Should be removed since a ProjectId must always be available")
+    @Deprecated(forRemoval = true)
+    public AllocateReshardSplitTargetPrimaryCommand(String index, int shardId, String node, boolean acceptDataLoss) {
+        this(index, shardId, node, acceptDataLoss, Metadata.DEFAULT_PROJECT_ID);
+    }
+
+    public AllocateReshardSplitTargetPrimaryCommand(StreamInput in) throws IOException {
+        super(in);
     }
 
     @Override
     public String name() {
-        return "allocate_reshard_split_primary";
+        return NAME;
+    }
+
+    @FixForMultiProject(description = "projectId should not be null once multi-project is fully in place")
+    public static AllocateReshardSplitTargetPrimaryCommand fromXContent(XContentParser parser, Object projectId) throws IOException {
+        assert projectId == null || projectId instanceof ProjectId : projectId;
+        return new AllocateReshardSplitTargetPrimaryCommand.Builder((ProjectId) projectId).parse(parser).build();
+    }
+
+    public static class Builder extends BasePrimaryAllocationCommand.Builder<AllocateReshardSplitTargetPrimaryCommand> {
+
+        Builder(ProjectId projectId) {
+            super(projectId);
+        }
+
+        private AllocateReshardSplitTargetPrimaryCommand.Builder parse(XContentParser parser) throws IOException {
+            return PARSER.parse(parser, this, null);
+        }
+
+        @Override
+        public AllocateReshardSplitTargetPrimaryCommand build() {
+            validate();
+            return new AllocateReshardSplitTargetPrimaryCommand(index, shard, node, acceptDataLoss, projectId);
+        }
     }
 
     @Override
@@ -50,7 +90,7 @@ public class AllocateReshardSplitTargetPrimaryCommand extends BasePrimaryAllocat
         }
 
         if (shardRouting.unassigned() == false || shardRouting.recoverySource().getType() != RecoverySource.Type.EMPTY_STORE) {
-            return explainOrThrowRejectedCommand(explain, allocation, "Requested shard is not in unassigned state");
+            return explainOrThrowRejectedCommand(explain, allocation, "Requested shard is not in unassigned state " + shardRouting);
         }
 
         Optional<IndexMetadata> maybeIndexMetadata = allocation.metadata().findIndex(shardRouting.index());

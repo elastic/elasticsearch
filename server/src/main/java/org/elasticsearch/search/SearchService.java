dbe6b3e0d34539d5f58e8de40e0b9b24e9841d8b
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-07-06 12:35:24 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/search/SearchService.java
+++ server/src/main/java/org/elasticsearch/search/SearchService.java
@@ -2102,7 +2102,9 @@ public class SearchService extends AbstractLifecycleComponent implements IndexEv
             threadPool.executor(Names.SEARCH),
 
             request.readerId() == null
-                ? listener.delegateFailureAndWrap((l, r) -> shard.ensureShardSearchActive(threadPool.executor(Names.SEARCH), b -> l.onResponse(request)))
+                ? listener.delegateFailureAndWrap(
+                    (l, r) -> shard.ensureShardSearchActive(threadPool.executor(Names.SEARCH), b -> l.onResponse(request))
+                )
                 : listener.safeMap(r -> request)
         );
     }

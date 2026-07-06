4d8067290bf0dd40f4de9ba5f309479f86cfe22c
Chris Berkhout <chrisberkhout@gmail.com>
2026-07-06 20:20:40 +0200

    For self reference checks in the constructor, provide a hint for the error message about what is being checked in each case.

--- server/src/test/java/org/elasticsearch/ingest/IngestDocumentTests.java
+++ server/src/test/java/org/elasticsearch/ingest/IngestDocumentTests.java
@@ -1915,7 +1915,7 @@ public class IngestDocumentTests extends ESTestCase {
             someList.add(someList); // the list contains itself
             ingestDocument.setFieldValue("someList", someList);
             Exception e = expectThrows(IllegalArgumentException.class, () -> new IngestDocument(ingestDocument));
-            assertThat(e.getMessage(), equalTo("Iterable object is self-referencing itself"));
+            assertThat(e.getMessage(), equalTo("Iterable object is self-referencing itself (source document)"));
         }
 
         {
@@ -1925,7 +1925,7 @@ public class IngestDocumentTests extends ESTestCase {
             selfReference.put("self", selfReference);
             ingestDocument.getIngestMetadata().put("self", selfReference);
             Exception e = expectThrows(IllegalArgumentException.class, () -> new IngestDocument(ingestDocument));
-            assertThat(e.getMessage(), equalTo("Iterable object is self-referencing itself"));
+            assertThat(e.getMessage(), equalTo("Iterable object is self-referencing itself (ingest metadata)"));
         }
     }
 

d2e45989b01a62e2d0d3a4ac83e347c85db988bb
Dimitris Athanasiou <dimitris@elastic.co>
2026-07-07 14:17:29 +0300

    [Inference API] Region policy for inference requests
    Region policy lets administrators restrict which geographies or
    cloud provider regions the Elastic Inference Service may use when
    handling inference requests. This drops the feature flag, making
    the feature generally available.
    
    Adds PUT, GET, and DELETE `_inference/_region_policy` APIs to
    configure an allow-list of geos (e.g. eu, us) or specific CSP and
    region pairs. The policy is persisted in the `.inference` system
    index, cached per project, and enforced on outbound requests to
    the Elastic Inference Service via the
    X-Elastic-Inference-Allowed-Geos and
    X-Elastic-Inference-Allowed-Regions headers. Endpoints the service
    denies under the policy are reported back and exposed as
    `denied_by_region_policy` on the corresponding inference endpoint.

--- x-pack/plugin/inference/qa/inference-service-tests/src/javaRestTest/java/org/elasticsearch/xpack/inference/RegionPolicyAuthorizationRefreshIT.java
+++ x-pack/plugin/inference/qa/inference-service-tests/src/javaRestTest/java/org/elasticsearch/xpack/inference/RegionPolicyAuthorizationRefreshIT.java
@@ -12,7 +12,6 @@ import org.elasticsearch.common.settings.SecureString;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.test.cluster.ElasticsearchCluster;
-import org.elasticsearch.test.cluster.FeatureFlag;
 import org.elasticsearch.test.cluster.local.distribution.DistributionType;
 import org.elasticsearch.test.rest.ESRestTestCase;
 import org.junit.Before;
@@ -37,7 +36,6 @@ public class RegionPolicyAuthorizationRefreshIT extends ESRestTestCase {
         .distribution(DistributionType.DEFAULT)
         .setting("xpack.license.self_generated.type", "trial")
         .setting("xpack.security.enabled", "true")
-        .feature(FeatureFlag.INFERENCE_REGION_POLICY)
         .setting("xpack.inference.elastic.url", mockEISServer::getUrl)
         .setting("xpack.inference.elastic.periodic_authorization_enabled", "false")
         // Disable the initial bootup authorization request so that each PUT/DELETE below triggers exactly one

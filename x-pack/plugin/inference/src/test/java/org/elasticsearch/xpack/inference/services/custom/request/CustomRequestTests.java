625ebf51f479b8f772c886589dd67bf66845ac33
Jonathan Buttner <56361221+jonathan-buttner@users.noreply.github.com>
2026-07-09 15:42:51 -0400

    Removing invalid class

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/custom/request/CustomRequestTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/custom/request/CustomRequestTests.java
@@ -399,12 +399,7 @@ public class CustomRequestTests extends ESTestCase {
 
         var exception = expectThrows(
             IllegalArgumentException.class,
-            () -> new CustomRequest(
-                RerankParameters.of(
-                    new QueryAndDocsInputs(InferenceString.ofText("query string"), InferenceString.fromStringList(List.of("abc", "123")))
-                ),
-                model
-            )
+            () -> new CustomRequest(RerankParameters.of(new QueryAndDocsInputs("query string", List.of("abc", "123"))), model)
         );
         assertThat(
             exception.getMessage(),
@@ -454,12 +449,7 @@ public class CustomRequestTests extends ESTestCase {
 
         var exception = expectThrows(
             IllegalArgumentException.class,
-            () -> new CustomRequest(
-                RerankParameters.of(
-                    new QueryAndDocsInputs(InferenceString.ofText("query string"), InferenceString.fromStringList(List.of("abc", "123")))
-                ),
-                model
-            )
+            () -> new CustomRequest(RerankParameters.of(new QueryAndDocsInputs("query string", List.of("abc", "123"))), model)
         );
         assertThat(
             exception.getMessage(),

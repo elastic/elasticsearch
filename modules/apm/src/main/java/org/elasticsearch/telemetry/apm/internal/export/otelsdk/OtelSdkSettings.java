b5b92d7d0827d8bd73ddeb31681eee822b815361
Mark Vieira <portugee@gmail.com>
2026-07-14 16:23:18 -0700

    Simplify comment

--- modules/apm/src/main/java/org/elasticsearch/telemetry/apm/internal/export/otelsdk/OtelSdkSettings.java
+++ modules/apm/src/main/java/org/elasticsearch/telemetry/apm/internal/export/otelsdk/OtelSdkSettings.java
@@ -242,9 +242,8 @@ public final class OtelSdkSettings {
     /**
      * Path to the PEM-encoded private key for the client certificate ({@link #TELEMETRY_LOGS_SSL_CERTIFICATE}).
      * Must be set together with {@link #TELEMETRY_LOGS_SSL_CERTIFICATE}.
-     * Supports PKCS#8 ({@code BEGIN PRIVATE KEY}) and PKCS#1 RSA ({@code BEGIN RSA PRIVATE KEY}) formats; encrypted keys require a
-     * {@code keystore.secure_password} entry (not currently exposed, so use unencrypted keys in practice).
-     * Path is resolved relative to the Elasticsearch config directory when not absolute.
+     * Supports PKCS#8 ({@code BEGIN PRIVATE KEY}) and PKCS#1 RSA ({@code BEGIN RSA PRIVATE KEY}) formats.
+     * Encrypted keys are not supported.
      */
     public static final Setting<String> TELEMETRY_LOGS_SSL_KEY = Setting.simpleString("telemetry.logs.ssl.key", "", NodeScope);
 

ae1619e95cc84a14393417acdafa8a2f1ee5fc6a
Anton Persson <anton.persson@elastic.co>
2026-07-15 09:56:47 +0200

    Introduce IndexEventListener.NOOP and remove usage of empty implementations

--- test/framework/src/main/java/org/elasticsearch/index/shard/IndexShardTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/index/shard/IndexShardTestCase.java
@@ -137,8 +137,6 @@ import static org.mockito.Mockito.doAnswer;
  */
 public abstract class IndexShardTestCase extends ESTestCase {
 
-    public static final IndexEventListener EMPTY_EVENT_LISTENER = new IndexEventListener() {};
-
     public static final GlobalCheckpointSyncer NOOP_GCP_SYNCER = shardId -> {};
 
     private static final AtomicBoolean failOnShardFailures = new AtomicBoolean(true);
@@ -549,7 +547,7 @@ public abstract class IndexShardTestCase extends ESTestCase {
             engineFactory,
             globalCheckpointSyncer,
             retentionLeaseSyncer,
-            EMPTY_EVENT_LISTENER,
+            IndexEventListener.NOOP,
             searchListeners,
             listeners
         );
@@ -770,7 +768,7 @@ public abstract class IndexShardTestCase extends ESTestCase {
             engineFactory,
             current.getGlobalCheckpointSyncer(),
             current.getRetentionLeaseSyncer(),
-            EMPTY_EVENT_LISTENER,
+            IndexEventListener.NOOP,
             Collections.emptyList(),
             listeners
         );

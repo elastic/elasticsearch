8c7dec7da9475ad52ce801581980a5bef56262fb
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-07-16 09:28:32 +0000

    [CI] Auto commit changes from spotless

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/test/rerun/InternalTestRerunPlugin.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/test/rerun/InternalTestRerunPlugin.java
@@ -70,7 +70,8 @@ public abstract class InternalTestRerunPlugin implements Plugin<Project> {
             return;
         }
 
-        if (test.getPath().endsWith("remote-cluster") || test.getPath().endsWith("mixed-cluster")
+        if (test.getPath().endsWith("remote-cluster")
+            || test.getPath().endsWith("mixed-cluster")
             || test.getPath().contains("rolling-upgrade-multi-cluster")) {
             test.getLogger().lifecycle("Smart retry: running all tests for {} (multi-cluster task, never skipped)", test.getPath());
             return;

78e69668a940a47c659f9227f4252facf021f152
Balaji Sridharan <balaji.sridharan@elastic.co>
2026-07-20 13:24:50 -0700

    Fix tests

--- server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
+++ server/src/test/java/org/elasticsearch/index/shard/IndexShardTests.java
@@ -3540,7 +3540,7 @@ public class IndexShardTests extends IndexShardTestCase {
                     "prepareIndex failure warning",
                     "org.elasticsearch.index.shard.IndexShard",
                     Level.WARN,
-                    "*failed to prepare index op*in batch during replay*"
+                    "failed to prepare index"
                 )
             );
             mockLog.addExpectation(

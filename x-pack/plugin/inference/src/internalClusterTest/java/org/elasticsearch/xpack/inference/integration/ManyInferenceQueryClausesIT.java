610b2cc1cf689b0d6796634bb32b7d50e98fc863
kishor-kharbas <kishor.kharbas@gmail.com>
2026-07-20 13:34:33 -0700

    review comment: add semantic field to existing ManyInferenceQueryClausesIT

--- x-pack/plugin/inference/src/internalClusterTest/java/org/elasticsearch/xpack/inference/integration/ManyInferenceQueryClausesIT.java
+++ x-pack/plugin/inference/src/internalClusterTest/java/org/elasticsearch/xpack/inference/integration/ManyInferenceQueryClausesIT.java
@@ -14,6 +14,7 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.inference.InferenceStringGroup;
 import org.elasticsearch.inference.TaskType;
 import org.elasticsearch.license.LicenseSettings;
 import org.elasticsearch.plugins.Plugin;
@@ -22,20 +23,25 @@ import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.vectors.KnnVectorQueryBuilder;
 import org.elasticsearch.test.ESIntegTestCase;
 import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xcontent.XContentFactory;
 import org.elasticsearch.xpack.core.ml.search.SparseVectorQueryBuilder;
 import org.elasticsearch.xpack.core.ml.vectors.TextEmbeddingQueryVectorBuilder;
 import org.elasticsearch.xpack.inference.FakeMlPlugin;
 import org.elasticsearch.xpack.inference.LocalStateInferencePlugin;
+import org.elasticsearch.xpack.inference.mapper.SemanticFieldMapper;
+import org.elasticsearch.xpack.inference.mapper.SemanticTextFieldMapper;
 import org.elasticsearch.xpack.inference.mock.TestInferenceServicePlugin;
 import org.elasticsearch.xpack.inference.queries.SemanticQueryBuilder;
+import org.elasticsearch.xpack.inference.vectors.EmbeddingQueryVectorBuilder;
 import org.junit.After;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
+import java.util.Set;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertResponse;
@@ -57,6 +63,23 @@ public class ManyInferenceQueryClausesIT extends ESIntegTestCase {
         "api_key",
         "my_api_key"
     );
+    private static final Map<String, Object> EMBEDDING_SERVICE_SETTINGS = Map.of(
+        "model",
+        "my_model",
+        "dimensions",
+        256,
+        "similarity",
+        "cosine",
+        "api_key",
+        "my_api_key"
+    );
+
+    private record FieldInfo(String fieldName, String inferenceId, String fieldType) {}
+
+    @FunctionalInterface
+    private interface ClauseGenerator {
+        QueryBuilder generate(FieldInfo fieldInfo, String queryText);
+    }
 
     private final Map<String, TaskType> inferenceIds = new HashMap<>();
 
@@ -79,51 +102,81 @@ public class ManyInferenceQueryClausesIT extends ESIntegTestCase {
     }
 
     public void testManySemanticQueryClauses() throws Exception {
-        manyQueryClausesTestCase(randomIntBetween(18, 24), SemanticQueryBuilder::new, TaskType.SPARSE_EMBEDDING);
+        manyQueryClausesTestCase(
+            randomIntBetween(18, 24),
+            (fi, q) -> new SemanticQueryBuilder(fi.fieldName(), q),
+            Set.of(TaskType.TEXT_EMBEDDING, TaskType.SPARSE_EMBEDDING, TaskType.EMBEDDING)
+        );
     }
 
     public void testManyMatchQueryClauses() throws Exception {
-        manyQueryClausesTestCase(randomIntBetween(18, 24), MatchQueryBuilder::new, TaskType.SPARSE_EMBEDDING);
+        manyQueryClausesTestCase(
+            randomIntBetween(18, 24),
+            (fi, q) -> new MatchQueryBuilder(fi.fieldName(), q),
+            Set.of(TaskType.TEXT_EMBEDDING, TaskType.SPARSE_EMBEDDING, TaskType.EMBEDDING)
+        );
     }
 
     public void testManySparseVectorQueryClauses() throws Exception {
-        manyQueryClausesTestCase(randomIntBetween(18, 24), (f, q) -> new SparseVectorQueryBuilder(f, null, q), TaskType.SPARSE_EMBEDDING);
+        manyQueryClausesTestCase(
+            randomIntBetween(18, 24),
+            (fi, q) -> new SparseVectorQueryBuilder(fi.fieldName(), null, q),
+            Set.of(TaskType.SPARSE_EMBEDDING)
+        );
     }
 
     public void testManyKnnQueryClauses() throws Exception {
         int clauseCount = randomIntBetween(18, 24);
-        manyQueryClausesTestCase(
-            clauseCount,
-            (f, q) -> new KnnVectorQueryBuilder(f, new TextEmbeddingQueryVectorBuilder(null, q), clauseCount, clauseCount * 10, null, null),
-            TaskType.TEXT_EMBEDDING
-        );
+        manyQueryClausesTestCase(clauseCount, (fi, q) -> {
+            if (fi.fieldType.equals(SemanticFieldMapper.CONTENT_TYPE)) {
+                return new KnnVectorQueryBuilder(
+                    fi.fieldName(),
+                    new EmbeddingQueryVectorBuilder(fi.inferenceId(), new InferenceStringGroup(q), null),
+                    clauseCount,
+                    clauseCount * 10,
+                    null,
+                    null
+                );
+            } else {
+                return new KnnVectorQueryBuilder(
+                    fi.fieldName(),
+                    new TextEmbeddingQueryVectorBuilder(null, q),
+                    clauseCount,
+                    clauseCount * 10,
+                    null,
+                    null
+                );
+            }
+        }, Set.of(TaskType.EMBEDDING, TaskType.TEXT_EMBEDDING));
     }
 
     private void manyQueryClausesTestCase(
         int clauseCount,
-        BiFunction<String, String, QueryBuilder> clauseGenerator,
-        TaskType semanticTextFieldTaskType
+        ClauseGenerator clauseGenerator,
+        Set<TaskType> taskTypesToTest
     ) throws Exception {
-        Map<String, Object> inferenceEndpointServiceSettings = getServiceSettings(semanticTextFieldTaskType);
-        Map<String, String> semanticTextFields = new HashMap<>(clauseCount);
+        List<FieldInfo> fields = new ArrayList<>(clauseCount);
         for (int i = 0; i < clauseCount; i++) {
             String fieldName = randomAlphaOfLength(10);
             String inferenceId = randomIdentifier();
-
-            createInferenceEndpoint(semanticTextFieldTaskType, inferenceId, inferenceEndpointServiceSettings);
-            semanticTextFields.put(fieldName, inferenceId);
+            TaskType taskType = randomFrom(taskTypesToTest);
+            String fieldType = taskType.equals(TaskType.EMBEDDING) && randomBoolean()
+                ? SemanticFieldMapper.CONTENT_TYPE
+                : SemanticTextFieldMapper.CONTENT_TYPE;
+            Map<String, Object> serviceSettings = getServiceSettings(taskType);
+            createInferenceEndpoint(taskType, inferenceId, serviceSettings);
+            fields.add(new FieldInfo(fieldName, inferenceId, fieldType));
         }
 
-        XContentBuilder mapping = IntegrationTestUtils.generateSemanticTextMapping(semanticTextFields);
+        XContentBuilder mapping = generateMapping(fields);
         assertAcked(prepareCreate(INDEX_NAME).setMapping(mapping));
 
         BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
-        for (String semanticTextField : semanticTextFields.keySet()) {
-            Map<String, Object> source = Map.of(semanticTextField, randomAlphaOfLength(10));
+        for (FieldInfo fieldInfo : fields) {
+            Map<String, Object> source = Map.of(fieldInfo.fieldName(), randomAlphaOfLength(10));
             DocWriteResponse docWriteResponse = client().prepareIndex(INDEX_NAME).setSource(source).get(TEST_REQUEST_TIMEOUT);
             assertThat(docWriteResponse.getResult(), is(DocWriteResponse.Result.CREATED));
-
-            boolQuery.should(clauseGenerator.apply(semanticTextField, randomAlphaOfLength(10)));
+            boolQuery.should(clauseGenerator.generate(fieldInfo, randomAlphaOfLength(10)));
         }
         client().admin().indices().prepareRefresh(INDEX_NAME).get();
 
@@ -135,6 +188,18 @@ public class ManyInferenceQueryClausesIT extends ESIntegTestCase {
         });
     }
 
+    private static XContentBuilder generateMapping(List<FieldInfo> fields) throws IOException {
+        XContentBuilder mapping = XContentFactory.jsonBuilder().startObject().startObject("properties");
+        for (FieldInfo fieldInfo : fields) {
+            mapping.startObject(fieldInfo.fieldName());
+            mapping.field("type", fieldInfo.fieldType());
+            mapping.field("inference_id", fieldInfo.inferenceId());
+            mapping.endObject();
+        }
+        mapping.endObject().endObject();
+        return mapping;
+    }
+
     private void createInferenceEndpoint(TaskType taskType, String inferenceId, Map<String, Object> serviceSettings) throws IOException {
         IntegrationTestUtils.createInferenceEndpoint(client(), taskType, inferenceId, serviceSettings);
         inferenceIds.put(inferenceId, taskType);
@@ -144,6 +209,7 @@ public class ManyInferenceQueryClausesIT extends ESIntegTestCase {
         return switch (taskType) {
             case SPARSE_EMBEDDING -> SPARSE_EMBEDDING_SERVICE_SETTINGS;
             case TEXT_EMBEDDING -> TEXT_EMBEDDING_SERVICE_SETTINGS;
+            case EMBEDDING -> EMBEDDING_SERVICE_SETTINGS;
             default -> throw new IllegalArgumentException("Unhandled task type [" + taskType + "]");
         };
     }

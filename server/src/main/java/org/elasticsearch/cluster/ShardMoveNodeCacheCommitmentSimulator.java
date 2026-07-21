6dec8eb3286d26c2edff5ea9c0e06728b36a6293
Surya Pappoppula <surya.pappoppula@elastic.co>
2026-07-21 17:51:35 -0500

    Update logging import

--- server/src/main/java/org/elasticsearch/cluster/ShardMoveNodeCacheCommitmentSimulator.java
+++ server/src/main/java/org/elasticsearch/cluster/ShardMoveNodeCacheCommitmentSimulator.java
@@ -9,10 +9,10 @@
 
 package org.elasticsearch.cluster;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.index.shard.ShardId;
+import org.elasticsearch.logging.LogManager;
+import org.elasticsearch.logging.Logger;
 
 import java.util.Collections;
 import java.util.HashMap;

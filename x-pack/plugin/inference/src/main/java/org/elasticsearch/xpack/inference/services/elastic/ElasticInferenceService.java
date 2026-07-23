917528fd3c1159e47dd7e0474c15efe8503af02e
Tim Grein <grein.tim97@gmail.com>
2026-07-23 13:37:45 +0200

    Add cache control and session id fields to chat completion input

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/elastic/ElasticInferenceService.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/elastic/ElasticInferenceService.java
@@ -260,7 +260,9 @@ public class ElasticInferenceService extends SenderService<ElasticInferenceServi
                     inputs.getRequest().toolChoice(),
                     inputs.getRequest().tools(),
                     inputs.getRequest().topP(),
-                    mergedReasoning
+                    mergedReasoning,
+                    inputs.getRequest().cacheControl(),
+                    inputs.getRequest().sessionId()
                 ),
                 inputs.stream()
             );

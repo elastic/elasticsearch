33823bde71698267d6ae77ffb69a9df7b7c7995e
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2026-07-23 13:46:23 +0200

    expand test

--- x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/CrossClusterEsqlResolveFieldsActionIT.java
+++ x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/action/CrossClusterEsqlResolveFieldsActionIT.java
@@ -8,48 +8,48 @@
 package org.elasticsearch.xpack.esql.action;
 
 import org.elasticsearch.action.fieldcaps.FieldCapabilitiesRequest;
+import org.elasticsearch.client.internal.Client;
 import org.elasticsearch.cluster.metadata.IndexAbstraction;
 import org.elasticsearch.cluster.metadata.View;
-import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.xpack.esql.datasources.dataset.PutDatasetAction;
+import org.elasticsearch.xpack.esql.datasources.datasource.PutDataSourceAction;
 import org.elasticsearch.xpack.esql.session.IndexResolver;
 import org.elasticsearch.xpack.esql.view.PutViewAction;
 import org.hamcrest.Matchers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 
 public class CrossClusterEsqlResolveFieldsActionIT extends AbstractCrossClusterTestCase {
 
-    public void testResolveIndexAbstractions() {
-        // indices
+    @Override
+    protected Collection<Class<? extends Plugin>> nodePlugins(String clusterAlias) {
+        List<Class<? extends Plugin>> plugins = new ArrayList<>(super.nodePlugins(clusterAlias));
+        plugins.add(CrossClusterDatasetIT.TestDataSourcePlugin.class);
+        return plugins;
+    }
+
+    public void testResolveIndexAbstractions() throws IOException {
+        assumeTrue("requires dataset-in-from-command capability", EsqlCapabilities.Cap.DATASET_IN_FROM_COMMAND.isEnabled());
+
         client(LOCAL_CLUSTER).admin().indices().prepareCreate("local-index").setMapping("f1", "type=keyword", "f2", "type=long").get();
         client(REMOTE_CLUSTER_1).admin().indices().prepareCreate("remote-index").setMapping("f1", "type=keyword", "f3", "type=long").get();
 
-        // views
-        assertAcked(
-            client(LOCAL_CLUSTER).execute(
-                PutViewAction.INSTANCE,
-                new PutViewAction.Request(
-                    TimeValue.THIRTY_SECONDS,
-                    TimeValue.THIRTY_SECONDS,
-                    new View("local-view", "FROM local-index | WHERE true")
-                )
-            ).actionGet(30, TimeUnit.SECONDS)
-        );
-        assertAcked(
-            client(REMOTE_CLUSTER_1).execute(
-                PutViewAction.INSTANCE,
-                new PutViewAction.Request(
-                    TimeValue.THIRTY_SECONDS,
-                    TimeValue.THIRTY_SECONDS,
-                    new View("remote-view", "FROM remote-index | WHERE true")
-                )
-            ).actionGet(30, TimeUnit.SECONDS)
-        );
+        createView(client(LOCAL_CLUSTER), "local-view", "FROM local-index | WHERE true");
+        createView(client(REMOTE_CLUSTER_1), "remote-view", "FROM remote-index | WHERE true");
 
-        // external data sets
-        // TODO
+        createExternalDataset(client(LOCAL_CLUSTER), "local-data-source", "local-data-set");
+        createExternalDataset(client(REMOTE_CLUSTER_1), "remote-data-source", "remote-data-set");
 
         // resolve schema
         var request = new EsqlResolveFieldsRequest(
@@ -64,8 +64,45 @@ public class CrossClusterEsqlResolveFieldsActionIT extends AbstractCrossClusterT
                 new EsqlResolveFieldsResponse.ResolvedIndexAbstraction("local-index", IndexAbstraction.Type.CONCRETE_INDEX),
                 new EsqlResolveFieldsResponse.ResolvedIndexAbstraction("cluster-a:remote-index", IndexAbstraction.Type.CONCRETE_INDEX),
                 new EsqlResolveFieldsResponse.ResolvedIndexAbstraction("local-view", IndexAbstraction.Type.VIEW),
-                new EsqlResolveFieldsResponse.ResolvedIndexAbstraction("cluster-a:remote-view", IndexAbstraction.Type.VIEW)
+                new EsqlResolveFieldsResponse.ResolvedIndexAbstraction("cluster-a:remote-view", IndexAbstraction.Type.VIEW),
+                new EsqlResolveFieldsResponse.ResolvedIndexAbstraction("local-data-set", IndexAbstraction.Type.DATASET),
+                new EsqlResolveFieldsResponse.ResolvedIndexAbstraction("cluster-a:remote-data-set", IndexAbstraction.Type.DATASET)
             )
+        );
+    }
+
+    private static void createView(Client client, String name, String query) {
+        assertAcked(
+            client.execute(
+                PutViewAction.INSTANCE,
+                new PutViewAction.Request(TEST_REQUEST_TIMEOUT, TEST_REQUEST_TIMEOUT, new View(name, query))
+            ).actionGet(30, TimeUnit.SECONDS)
+        );
+    }
+
+    private static void createExternalDataset(Client client, String dataSourceName, String dataSetName) throws IOException {
+        Path csvFixture = createTempFile("dataset-", ".csv");
+        Files.writeString(csvFixture, "f1:keyword,f4:long\nalice,1\nbob,2\n");
+
+        assertAcked(
+            client.execute(
+                PutDataSourceAction.INSTANCE,
+                new PutDataSourceAction.Request(TEST_REQUEST_TIMEOUT, TEST_REQUEST_TIMEOUT, dataSourceName, "test", null, new HashMap<>())
+            ).actionGet(30, TimeUnit.SECONDS)
+        );
+        assertAcked(
+            client.execute(
+                PutDatasetAction.INSTANCE,
+                new PutDatasetAction.Request(
+                    TEST_REQUEST_TIMEOUT,
+                    TEST_REQUEST_TIMEOUT,
+                    dataSetName,
+                    dataSourceName,
+                    csvFixture.toUri().toString(),
+                    null,
+                    Map.of("format", "csv")
+                )
+            ).actionGet(30, TimeUnit.SECONDS)
         );
     }
 }

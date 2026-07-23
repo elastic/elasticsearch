31d8a6bf3377ffeb31ad7094a59aee95946c570d
Jonathan Buttner <56361221+jonathan-buttner@users.noreply.github.com>
2026-07-23 12:51:35 -0400

    [Inference API] Deploy text embedding models before validation call (#153886)
    * Deploy text embedding model before validation call
    
    * Update docs/changelog/153886.yaml
    
    * Stopping deployment if it was started and a failure occurred
    
    * Add tests for stopping deployment
    
    * Fixing tests
    
    (cherry picked from commit 19f1515707c5d13408b2e607c7ce466f2dce28ea)
    
    # Conflicts:
    #	x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/action/TransportPutInferenceModelAction.java
    #	x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/validation/ChatCompletionModelValidator.java
    #	x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/validation/ModelValidatorBuilderTests.java

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/validation/ChatCompletionModelValidatorTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/validation/ChatCompletionModelValidatorTests.java
@@ -40,7 +40,7 @@ public class ChatCompletionModelValidatorTests extends ESTestCase {
     @Mock
     private Model mockModel;
     @Mock
-    private ActionListener<Model> mockActionListener;
+    private ActionListener<ModelValidationResult> mockActionListener;
 
     private ChatCompletionModelValidator underTest;
 

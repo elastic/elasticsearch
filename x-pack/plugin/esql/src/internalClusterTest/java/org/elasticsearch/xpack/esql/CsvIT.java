f4075a1f0ce470575161e504ba565f3a43e4514c
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2026-07-24 08:15:13 +0200

    fix api usage

--- x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/CsvIT.java
+++ x-pack/plugin/esql/src/internalClusterTest/java/org/elasticsearch/xpack/esql/CsvIT.java
@@ -105,6 +105,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
@@ -265,7 +266,9 @@ public class CsvIT extends ESTestCase {
         assertThat("Not enough specs found " + urls, urls, hasSize(greaterThan(0)));
 
         var specs = SpecReader.readScriptSpec(urls, CsvSpecReader::specParser);
-        Collections.shuffle(specs);
+        // forbidden aip require to pass random explicitly, however LuceneTestCase#random() is not yet initialized.
+        // Falling back to a new instance as repeatable scenario order is not esential here.
+        Collections.shuffle(specs, new Random());
         Collections.sort(specs, Comparator.comparing(spec -> GROUPS_WITH_VIEWS.contains((String) spec[1])));
         return specs;
     }

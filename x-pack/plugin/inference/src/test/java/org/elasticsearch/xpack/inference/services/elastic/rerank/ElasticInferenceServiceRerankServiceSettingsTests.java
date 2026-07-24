789d984f71adae2262b524c7bc24a89d31633042
AntMoraisElastic <antonio.morais@elastic.co>
2026-07-24 13:51:06 +0100

    Use ObjectParser for EIS rerank service settings

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/elastic/rerank/ElasticInferenceServiceRerankServiceSettingsTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/elastic/rerank/ElasticInferenceServiceRerankServiceSettingsTests.java
@@ -7,15 +7,17 @@
 
 package org.elasticsearch.xpack.inference.services.elastic.rerank;
 
+import org.elasticsearch.ElasticsearchParseException;
 import org.elasticsearch.TransportVersion;
 import org.elasticsearch.common.Strings;
-import org.elasticsearch.common.ValidationException;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.common.xcontent.XContentHelper;
+import org.elasticsearch.test.AbstractBWCSerializationTestCase;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentFactory;
+import org.elasticsearch.xcontent.XContentParseException;
+import org.elasticsearch.xcontent.XContentParser;
 import org.elasticsearch.xcontent.XContentType;
-import org.elasticsearch.xpack.core.ml.AbstractBWCWireSerializationTestCase;
 import org.elasticsearch.xpack.inference.services.ConfigurationParseContext;
 import org.elasticsearch.xpack.inference.services.ServiceFields;
 import org.elasticsearch.xpack.inference.services.settings.RateLimitSettings;
@@ -26,11 +28,26 @@ import java.util.Map;
 
 import static org.hamcrest.Matchers.anEmptyMap;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.sameInstance;
 
-public class ElasticInferenceServiceRerankServiceSettingsTests extends AbstractBWCWireSerializationTestCase<
+public class ElasticInferenceServiceRerankServiceSettingsTests extends AbstractBWCSerializationTestCase<
     ElasticInferenceServiceRerankServiceSettings> {
+
+    private final boolean ignoreUnknownFields = randomBoolean();
+
+    @Override
+    protected boolean supportsUnknownFields() {
+        return ignoreUnknownFields;
+    }
+
+    @Override
+    protected ElasticInferenceServiceRerankServiceSettings doParseInstance(XContentParser parser) throws IOException {
+        return ElasticInferenceServiceRerankServiceSettings.createParser(true, ConfigurationParseContext.PERSISTENT)
+            .apply(parser, ConfigurationParseContext.PERSISTENT)
+            .build();
+    }
 
     @Override
     protected Writeable.Reader<ElasticInferenceServiceRerankServiceSettings> instanceReader() {
@@ -51,17 +68,25 @@ public class ElasticInferenceServiceRerankServiceSettingsTests extends AbstractB
 
     public void testFromMap() {
         var modelId = "my-model-id";
+        var map = new HashMap<String, Object>(Map.of(ServiceFields.MODEL_ID, modelId));
 
-        var serviceSettings = ElasticInferenceServiceRerankServiceSettings.fromMap(
-            new HashMap<>(Map.of(ServiceFields.MODEL_ID, modelId)),
-            ConfigurationParseContext.REQUEST
-        );
+        var serviceSettings = ElasticInferenceServiceRerankServiceSettings.fromMap(map, ConfigurationParseContext.REQUEST);
 
         assertThat(serviceSettings, is(new ElasticInferenceServiceRerankServiceSettings(modelId)));
+        assertThat(map, is(anEmptyMap()));
         assertThat(serviceSettings.rateLimitSettings(), sameInstance(RateLimitSettings.DISABLED_INSTANCE));
     }
 
-    public void testFromMap_DoesNotRemoveRateLimitField_DoesNotThrowValidationException_PersistentContext() {
+    public void testFromMap_ThrowsIllegalArgumentException_WhenModelIdIsMissing() {
+        var e = expectThrows(
+            IllegalArgumentException.class,
+            () -> ElasticInferenceServiceRerankServiceSettings.fromMap(new HashMap<>(), ConfigurationParseContext.REQUEST)
+        );
+
+        assertThat(e.getMessage(), containsString("[service_settings] does not contain the required setting [model_id]"));
+    }
+
+    public void testFromMap_IgnoresRateLimitField_PersistentContext() {
         var modelId = "my-model-id";
 
         var map = new HashMap<String, Object>(
@@ -76,7 +101,6 @@ public class ElasticInferenceServiceRerankServiceSettingsTests extends AbstractB
         var serviceSettings = ElasticInferenceServiceRerankServiceSettings.fromMap(map, ConfigurationParseContext.PERSISTENT);
 
         assertThat(serviceSettings, is(new ElasticInferenceServiceRerankServiceSettings(modelId)));
-        assertThat(map, is(Map.of(RateLimitSettings.FIELD_NAME, Map.of(RateLimitSettings.REQUESTS_PER_MINUTE_FIELD, 100))));
         assertThat(serviceSettings.rateLimitSettings(), sameInstance(RateLimitSettings.DISABLED_INSTANCE));
     }
 
@@ -105,15 +129,26 @@ public class ElasticInferenceServiceRerankServiceSettingsTests extends AbstractB
         );
 
         var exception = expectThrows(
-            ValidationException.class,
+            XContentParseException.class,
             () -> ElasticInferenceServiceRerankServiceSettings.fromMap(map, ConfigurationParseContext.REQUEST)
         );
 
+        assertThat(exception.getCause(), instanceOf(ElasticsearchParseException.class));
         assertThat(
-            exception.getMessage(),
+            exception.getCause().getMessage(),
             containsString("[service_settings] rate limit settings are not permitted for service [elastic] and task type [rerank]")
         );
-        assertThat(map, is(Map.of(RateLimitSettings.FIELD_NAME, Map.of(RateLimitSettings.REQUESTS_PER_MINUTE_FIELD, 100))));
+    }
+
+    public void testFromMap_ThrowsException_WhenUnknownFieldExists_RequestContext() {
+        var map = new HashMap<String, Object>(Map.of(ServiceFields.MODEL_ID, "my-model-id", "unknown_field", "value"));
+
+        var exception = expectThrows(
+            XContentParseException.class,
+            () -> ElasticInferenceServiceRerankServiceSettings.fromMap(map, ConfigurationParseContext.REQUEST)
+        );
+
+        assertThat(exception.getMessage(), containsString("unknown field [unknown_field]"));
     }
 
     public void testToXContent_WritesAllFields() throws IOException {

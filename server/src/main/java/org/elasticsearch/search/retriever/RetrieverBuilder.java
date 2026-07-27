7c079f2c3faf9f67df175cdda689ca171a91140a
Panagiotis Bailis <pmpailis@gmail.com>
2026-07-27 12:34:23 +0300

    Update RetrieverBuilder.java

--- server/src/main/java/org/elasticsearch/search/retriever/RetrieverBuilder.java
+++ server/src/main/java/org/elasticsearch/search/retriever/RetrieverBuilder.java
@@ -61,7 +61,7 @@ public abstract class RetrieverBuilder implements Rewriteable<RetrieverBuilder>,
      * limit that guards against {@link StackOverflowError}s that would otherwise be thrown while recursively parsing a deeply
      * nested retriever tree.This is also bound by the max_rewrite_rounds available.
      */
-    public static final int MAX_NESTED_DEPTH = MAX_REWRITE_ROUNDS / 2;
+    public static final int MAX_NESTED_DEPTH = MAX_REWRITE_ROUNDS;
 
     protected static void declareBaseParserFields(AbstractObjectParser<? extends RetrieverBuilder, RetrieverParserContext> parser) {
         parser.declareObjectArray(

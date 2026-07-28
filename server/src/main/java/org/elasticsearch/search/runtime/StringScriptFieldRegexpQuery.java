3b564a1cd391223e17b816810a5b56c27e366fd0
Dimitris Rempapis <dimitris.rempapis@elastic.co>
2026-07-28 16:09:52 +0300

    Reserve regexp automaton memory on the circuit breaker before building it (#155076) (#155252)
    (cherry picked from commit 024c18b09f019a22f43407129483727d1958e863)
    
    # Conflicts:
    #	modules/mapper-extras/src/main/java/org/elasticsearch/index/mapper/extras/MatchOnlyTextFieldMapper.java
    #	modules/parent-join/src/main/java/org/elasticsearch/join/aggregations/RegexpNfaRamEstimator.java
    #	server/src/main/java/org/elasticsearch/common/lucene/search/AutomatonQueries.java
    #	server/src/main/java/org/elasticsearch/index/mapper/KeywordFieldMapper.java
    #	server/src/main/java/org/elasticsearch/index/mapper/TextFieldMapper.java
    #	server/src/main/java/org/elasticsearch/lucene/queries/ScanningBinaryDocValuesRegexpQuery.java
    #	server/src/main/java/org/elasticsearch/search/runtime/StringScriptFieldRegexpQuery.java
    #	server/src/test/java/org/elasticsearch/common/lucene/search/AutomatonQueriesTests.java
    #	server/src/test/java/org/elasticsearch/index/mapper/KeywordFieldTypeTests.java
    #	server/src/test/java/org/elasticsearch/lucene/queries/ScanningBinaryDocValuesRegexpQueryTests.java

--- server/src/main/java/org/elasticsearch/search/runtime/StringScriptFieldRegexpQuery.java
+++ server/src/main/java/org/elasticsearch/search/runtime/StringScriptFieldRegexpQuery.java
@@ -9,8 +9,9 @@
 
 package org.elasticsearch.search.runtime;
 
-import org.apache.lucene.util.automaton.ByteRunAutomaton;
-import org.apache.lucene.util.automaton.RegExp;
+import org.elasticsearch.common.breaker.CircuitBreaker;
+import org.elasticsearch.common.lucene.search.AutomatonQueries;
+import org.elasticsearch.core.Nullable;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.script.StringFieldScript;
 
@@ -28,13 +29,21 @@ public class StringScriptFieldRegexpQuery extends AbstractStringScriptFieldAutom
         String pattern,
         int syntaxFlags,
         int matchFlags,
-        int maxDeterminizedStates
+        int maxDeterminizedStates,
+        @Nullable CircuitBreaker circuitBreaker
     ) {
         super(
             script,
             leafFactory,
             fieldName,
-            new ByteRunAutomaton(new RegExp(Objects.requireNonNull(pattern), syntaxFlags, matchFlags).toAutomaton(maxDeterminizedStates))
+            AutomatonQueries.toRegexpByteRunAutomaton(
+                fieldName,
+                Objects.requireNonNull(pattern),
+                syntaxFlags,
+                matchFlags,
+                maxDeterminizedStates,
+                circuitBreaker
+            )
         );
         this.pattern = pattern;
         this.syntaxFlags = syntaxFlags;

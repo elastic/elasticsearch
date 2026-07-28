75396a1ac6100b253f929be0d820813eb42af486
Dmitry Leontyev <dmitry.leontyev@elastic.co>
2026-07-28 15:42:26 +0200

    Add an automation rule to perform a daily merge of lucene_snapshot to lucene_snapshot_11

--- .buildkite/scripts/lucene-snapshot/update-branch.sh
+++ .buildkite/scripts/lucene-snapshot/update-branch.sh
@@ -2,17 +2,25 @@
 
 set -euo pipefail
 
-if [[ "$BUILDKITE_BRANCH" != "lucene_snapshot"* ]]; then
-  echo "Error: This script should only be run on lucene_snapshot branches"
-  exit 1
-fi
+case "$BUILDKITE_BRANCH" in
+  lucene_snapshot)
+    UPSTREAM="main"
+    ;;
+  lucene_snapshot_11)
+    UPSTREAM="lucene_snapshot"
+    ;;
+  *)
+    echo "Error: no upstream branch configured for [$BUILDKITE_BRANCH]"
+    exit 1
+    ;;
+esac
 
-echo --- Updating "$BUILDKITE_BRANCH" branch with main
+echo --- Updating "$BUILDKITE_BRANCH" branch with "$UPSTREAM"
 
 git config --global user.name elasticsearchmachine
 git config --global user.email 'infra-root+elasticsearchmachine@elastic.co'
 
 git checkout "$BUILDKITE_BRANCH"
-git fetch origin main
-git merge --no-edit origin/main
+git fetch origin "$UPSTREAM"
+git merge --no-edit "origin/$UPSTREAM"
 git push origin "$BUILDKITE_BRANCH"

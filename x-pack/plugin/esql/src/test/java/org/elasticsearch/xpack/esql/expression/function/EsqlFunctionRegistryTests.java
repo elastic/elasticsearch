dd9b76f77c7a21188b9da072d9f3b4c73f6c01c1
Iván Cea Fontenla <ivancea96@outlook.com>
2026-07-30 19:46:35 +0200

    ESQL: Remove geo from MV_SORT allowed inputs and added error tests (#154417)
    Geo was allowed in MvSort, but not in MvMax/MvMin/SORT. This PR removed such support.
    
    It was never documented as allowed: https://www.elastic.co/docs/reference/query-languages/esql/functions-operators/mv-functions/mv_sort
    
    Its results are unexpected:
    
    Query:
    ```sql
    ROW p = ["POINT(1 5)", "POINT(2 5)", "POINT(1 6)", "POINT(2 6)"]::geo_point
    | EVAL p2 = MV_SORT(p)
    ```
    Result:
    ```json
          [
            "POINT (2.0 5.0)",
            "POINT (2.0 6.0)",
            "POINT (1.0 5.0)",
            "POINT (1.0 6.0)"
          ]
    ```
    
    Caught by https://github.com/elastic/elasticsearch/pull/154286

--- x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/EsqlFunctionRegistryTests.java
+++ x-pack/plugin/esql/src/test/java/org/elasticsearch/xpack/esql/expression/function/EsqlFunctionRegistryTests.java
@@ -318,7 +318,6 @@ public class EsqlFunctionRegistryTests extends ESTestCase {
                 .item("org.elasticsearch.xpack.esql.expression.function.scalar.convert.ToDenseVectorErrorTests is missing")
                 .item("org.elasticsearch.xpack.esql.expression.function.scalar.convert.ToVersionErrorTests is missing")
                 .item("org.elasticsearch.xpack.esql.expression.function.scalar.multivalue.MvIntersectionErrorTests is missing")
-                .item("org.elasticsearch.xpack.esql.expression.function.scalar.multivalue.MvSortErrorTests is missing")
                 .item("org.elasticsearch.xpack.esql.expression.function.scalar.multivalue.MvUnionErrorTests is missing")
                 .item("org.elasticsearch.xpack.esql.expression.function.scalar.nulls.CoalesceErrorTests is missing")
                 .item("org.elasticsearch.xpack.esql.expression.function.scalar.score.DecayErrorTests is missing")

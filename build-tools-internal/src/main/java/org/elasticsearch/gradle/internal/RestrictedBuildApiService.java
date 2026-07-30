5053bff4413faecb2a2d2292c901e2018d5e736d
Rene Groeschke <rene@elastic.co>
2026-07-30 17:00:41 +0200

    Migrate repository-old-versions fixture to unit rule based test cluster and test container (#154518)
    Move :x-pack:qa:repository-old-versions to the rule-based test cluster framework and extract its old Elasticsearch Docker fixture\ninto :test:fixtures:old-elasticsearch-container.
    
    The new fixture project exposes versioned prebaked images via\nelasticsearch.deploy-test-fixtures and enables fixture caching\nwith elasticsearch.cache-test-fixtures.
    
    (cherry picked from commit 6336c10219fa954367053a8d3d2190262d80468c)

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/RestrictedBuildApiService.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/RestrictedBuildApiService.java
@@ -75,7 +75,6 @@ public abstract class RestrictedBuildApiService implements BuildService<Restrict
         map.put(LegacyRestTestBasePlugin.class, ":qa:ccs-rolling-upgrade-remote-cluster");
         map.put(LegacyRestTestBasePlugin.class, ":qa:mixed-cluster");
         map.put(LegacyRestTestBasePlugin.class, ":qa:rolling-upgrade-legacy");
-        map.put(LegacyRestTestBasePlugin.class, ":x-pack:qa:repository-old-versions");
         map.put(LegacyRestTestBasePlugin.class, ":x-pack:qa:rolling-upgrade");
         map.put(LegacyRestTestBasePlugin.class, ":x-pack:qa:rolling-upgrade-basic");
         map.put(LegacyRestTestBasePlugin.class, ":x-pack:plugin:eql:qa:ccs-rolling-upgrade");

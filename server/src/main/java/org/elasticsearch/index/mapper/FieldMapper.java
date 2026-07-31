e114f72550743688245c3eac43f1cd9dda4c5fb8
Tim Brooks <tim@timbrooks.org>
2026-07-30 22:34:32 -0600

    Add support for keyword single value code path (#155551)
    This commit adds dedicated support the the binary doc values single
    value keyword path. We attempt to make the keyword path a no-op if the
    underlying column is already valid utf-8 bytes.

--- server/src/main/java/org/elasticsearch/index/mapper/FieldMapper.java
+++ server/src/main/java/org/elasticsearch/index/mapper/FieldMapper.java
@@ -282,6 +282,10 @@ public abstract class FieldMapper extends Mapper {
      * @param ctx    the batch mapping context; receives output columns via {@code addColumn}
      * @param source the Escf column holding the field's source values for the batch
      */
+    // TODO: See FieldMapper#parse. We need to migrate over multi-value and nullability restricts.
+    // This should be straightforward. We would reject array columns for multi-value and force
+    // dense columns or null replacement for no nullability. We might need to do a check if multi-value
+    // is false and there is an array column scan down the array counts because size 0 or 1 is still valid
     public void mapColumnBatch(BatchMappingContext ctx, EscfColumn source) {
         throw new UnsupportedOperationException(
             "mapColumnBatch not implemented for mapper [" + typeName() + "] on field [" + fullPath() + "]"

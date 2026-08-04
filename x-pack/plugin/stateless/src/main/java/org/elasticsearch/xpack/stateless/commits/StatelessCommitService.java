78bbdc2adacd7907386338768af4addfda1cb1e5
Albert Zaharovits <email+github@zalbert.me>
2026-08-04 10:42:25 +0300

    Nit

--- x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/commits/StatelessCommitService.java
+++ x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/commits/StatelessCommitService.java
@@ -3689,7 +3689,6 @@ public class StatelessCommitService extends AbstractLifecycleComponent implement
         return OptionalDouble.of(((double) max - (double) min) / 60_000d);
     }
 
-    // visible for testing
     public static String bccSizeBucket(long totalSizeBytes) {
         assert totalSizeBytes > 0 : "was " + totalSizeBytes;
         if (totalSizeBytes <= ByteSizeUnit.MB.toBytes(16)) {

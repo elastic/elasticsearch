cfac1f18f187222c90bd178fa23a51c375b33311
Yang Wang <yang.wang@elastic.co>
2026-08-04 15:57:34 +1000

    [Test] Inject current time for PinnedWindowPolicy (#155709)
    Relates
    https://github.com/elastic/elasticsearch/pull/155577#discussion_r3700997078

--- x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/cache/StatelessSharedBlobCacheService.java
+++ x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/cache/StatelessSharedBlobCacheService.java
@@ -225,7 +225,8 @@ public class StatelessSharedBlobCacheService extends SharedBlobCacheService<File
         );
     }
 
-    private static EvictionPolicy<FileCacheKey> createEvictionPolicy(
+    // package private for testing
+    static EvictionPolicy<FileCacheKey> createEvictionPolicy(
         Settings settings,
         ClusterService clusterService,
         IndicesService indicesService,

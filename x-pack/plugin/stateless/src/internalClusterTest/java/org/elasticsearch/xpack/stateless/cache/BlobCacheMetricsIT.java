f0c722a8d52e8df4007a7ac4e10a84030b70fec2
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-08-05 02:27:14 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/cache/BlobCacheMetricsIT.java
+++ x-pack/plugin/stateless/src/internalClusterTest/java/org/elasticsearch/xpack/stateless/cache/BlobCacheMetricsIT.java
@@ -246,7 +246,7 @@ public class BlobCacheMetricsIT extends AbstractBlobCacheMetricsIntegTestCase {
         assertThat(normalCacheBypassCount, equalTo(0L));
     }
 
-public void testSearchNodeOnlyPeriodicCacheMetrics() {
+    public void testSearchNodeOnlyPeriodicCacheMetrics() {
         final var indexNode = startMasterAndIndexNode();
         final var searchNode = startSearchNode();
 

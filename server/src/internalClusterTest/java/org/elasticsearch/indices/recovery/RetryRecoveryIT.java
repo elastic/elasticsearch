d9a791d9efcb4dccadbeb1c8060080bec31fcd30
Anton Persson <anton.persson@elastic.co>
2026-08-05 16:44:09 +0200

    Test retry race with index deletion

--- server/src/internalClusterTest/java/org/elasticsearch/indices/recovery/RetryRecoveryIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/indices/recovery/RetryRecoveryIT.java
@@ -204,18 +204,17 @@ public class RetryRecoveryIT extends AbstractIndexRecoveryIntegTestCase {
         internalCluster().startNode();
         String indexName = randomIndexName();
 
-        // Block index on the next recovery
-        RetryRecoveryTestPlugin.stateChangePostRecovery.block();
-
         // Index creation will block in recovery and then fail immediately when unblocked
         RetryRecoveryTestPlugin.armFailure();
+        RetryRecoveryTestPlugin.stateChangePostRecovery.block();
+
+        // Create index async
         prepareCreate(indexName, indexSettings(1, 0)).execute();
 
         // Wait for index creation recovery attempt
         RetryRecoveryTestPlugin.stateChangePostRecovery.await();
 
         // Delete index asynchronously while shard is blocked in recovery
-        logger.info("--> prepareDelete");
         indicesAdmin().prepareDelete(indexName).execute();
 
         // Release recovery will make retry mechanism race with index deletion
@@ -229,25 +228,199 @@ public class RetryRecoveryIT extends AbstractIndexRecoveryIntegTestCase {
         assertThat(RetryRecoveryTestPlugin.retryCounter.get(), equalTo(1));
     }
 
+    public void testRetryOnFailureOnRecoveryFromExistingStoreRaceWithIndexDeletion() throws Exception {
+        internalCluster().startNode();
+        String indexName = randomIndexName();
+
+        // Create an existing store
+        createIndex(indexName, indexSettings(1, 0).build());
+        indexDoc(indexName, "1", "f", randomAlphaOfLength(10));
+        flush(indexName);
+        ensureGreen(indexName);
+        assertAcked(indicesAdmin().prepareClose(indexName));
+
+        // Open index will block in recovery and then fail immediately when unblocked
+        RetryRecoveryTestPlugin.reset();
+        RetryRecoveryTestPlugin.armFailure();
+        RetryRecoveryTestPlugin.stateChangePostRecovery.block();
+
+        // Recover from existing store async
+        indicesAdmin().prepareOpen(indexName).execute();
+
+        // Wait for index creation recovery attempt
+        RetryRecoveryTestPlugin.stateChangePostRecovery.await();
+
+        // Delete index asynchronously while shard is blocked in recovery
+        indicesAdmin().prepareDelete(indexName).execute();
+
+        // Release recovery will make retry mechanism race with index deletion
+        RetryRecoveryTestPlugin.stateChangePostRecovery.release();
+
+        // Let all tasks complete
+        waitNoPendingTasksOnAll();
+
+        // Index should not exist, and we should have reached retry step
+        assertThat(indexExists(indexName), equalTo(false));
+        assertThat(RetryRecoveryTestPlugin.retryCounter.get(), equalTo(1));
+    }
+
+    public void testRetryOnFailureOnRecoveryFromLocalShardRaceWithIndexDeletion() throws Exception {
+        internalCluster().startNode();
+        final var sourceIndexName = randomIndexName();
+        final var targetIndexName = randomIndexName();
+
+        // Create an existing store
+        createIndex(sourceIndexName, indexSettings(1, 0).build());
+        indexDoc(sourceIndexName, "1", "f", randomAlphaOfLength(10));
+        flush(sourceIndexName);
+        ensureGreen(sourceIndexName);
+
+        // Required for clone, make the source index read-only
+        updateIndexSettings(Settings.builder().put("index.blocks.write", true), sourceIndexName);
+
+        // Cloning index will block in recovery and then fail immediately when unblocked
+        RetryRecoveryTestPlugin.reset();
+        RetryRecoveryTestPlugin.armFailure();
+        RetryRecoveryTestPlugin.stateChangePostRecovery.block();
+
+        // Recover from local shard async
+        ResizeIndexTestUtils.executeResize(ResizeType.CLONE, sourceIndexName, targetIndexName, indexSettings(1, 0));
+
+        // Wait for index creation recovery attempt
+        RetryRecoveryTestPlugin.stateChangePostRecovery.await();
+
+        // Delete index asynchronously while shard is blocked in recovery
+        indicesAdmin().prepareDelete(targetIndexName).execute();
+
+        // Release recovery will make retry mechanism race with index deletion
+        RetryRecoveryTestPlugin.stateChangePostRecovery.release();
+
+        // Let all tasks complete
+        waitNoPendingTasksOnAll();
+
+        // Index should not exist, and we should have reached retry step
+        assertThat(indexExists(targetIndexName), equalTo(false));
+        assertThat(RetryRecoveryTestPlugin.retryCounter.get(), equalTo(1));
+    }
+
+    public void testRetryOnFailureOnRecoveryFromSnapshotRaceWithIndexDeletion() throws Exception {
+        internalCluster().startNode();
+        final var indexName = randomIndexName();
+        final var repoName = "test-repo";
+
+        // Create index to snapshot
+        createIndex(indexName, indexSettings(1, 0).build());
+        indexDoc(indexName, "1", "f", randomAlphaOfLength(10));
+        flush(indexName);
+        ensureGreen(indexName);
+
+        // Snapshot the index
+        assertAcked(
+            clusterAdmin().preparePutRepository(TEST_REQUEST_TIMEOUT, TEST_REQUEST_TIMEOUT, repoName)
+                .setType("fs")
+                .setSettings(Settings.builder().put("location", randomRepoPath()))
+        );
+        clusterAdmin().prepareCreateSnapshot(TEST_REQUEST_TIMEOUT, repoName, "snap").setWaitForCompletion(true).get();
+
+        // Delete the index
+        assertAcked(indicesAdmin().prepareDelete(indexName));
+
+        // Index recovering from snapshot will block in recovery and then fail immediately when unblocked
+        RetryRecoveryTestPlugin.reset();
+        RetryRecoveryTestPlugin.armFailure();
+        RetryRecoveryTestPlugin.stateChangePostRecovery.block();
+
+        // Recover from snapshot async
+        clusterAdmin().prepareRestoreSnapshot(TEST_REQUEST_TIMEOUT, repoName, "snap").setWaitForCompletion(false).execute();
+
+        // Wait for index creation recovery attempt
+        RetryRecoveryTestPlugin.stateChangePostRecovery.await();
+
+        // Delete index asynchronously while shard is blocked in recovery
+        indicesAdmin().prepareDelete(indexName).execute();
+
+        // Release recovery will make retry mechanism race with index deletion
+        RetryRecoveryTestPlugin.stateChangePostRecovery.release();
+
+        // Let all tasks complete
+        waitNoPendingTasksOnAll();
+
+        // Index should not exist, and we should have reached retry step
+        assertThat(indexExists(indexName), equalTo(false));
+        assertThat(RetryRecoveryTestPlugin.retryCounter.get(), equalTo(1));
+    }
+
+    public void testRetryOnFailureOnRecoveryFromPeerRaceWithIndexDeletion() throws Exception {
+        String source = internalCluster().startNode();
+        final var indexName = randomIndexName();
+
+        // Create index on source
+        createIndex(indexName, indexSettings(1, 0).build());
+        indexDoc(indexName, "1", "f", randomAlphaOfLength(10));
+        flush(indexName);
+        ensureGreen(indexName);
+
+        // Start target node
+        String target = internalCluster().startNode();
+
+        // Fail next recovery attempt
+        // Target send error response back to source on data channel
+        // Source sends an error response back on the coordination channel (as response to start_recovery request)
+        // Target -> RecoveryResponseHandler -> failRecovery(..., RETRY) --> listener.onRecoveryFailure(..., RETRY)
+        AtomicInteger fileChunkCounter = new AtomicInteger(0);
+        final var targetTransport = MockTransportService.getInstance(target);
+        Gate fileChunkGate = new Gate();
+        targetTransport.addRequestHandlingBehavior(PeerRecoveryTargetService.Actions.FILE_CHUNK, (handler, request, channel, task) -> {
+            try {
+                fileChunkGate.enter();
+                if (fileChunkCounter.incrementAndGet() == 1) {
+                    throw RETRY_CAUSE; // or channel.sendResponse(RETRY_CAUSE);
+                }
+                handler.messageReceived(request, channel, task);
+            } finally {
+                fileChunkGate.exit();
+            }
+        });
+
+        // Index recovering from peer will block in recovery and then fail immediately when unblocked
+        RetryRecoveryTestPlugin.reset();
+        fileChunkGate.block();
+
+        // Recover from peer
+        ClusterRerouteUtils.reroute(client(), new MoveAllocationCommand(indexName, 0, source, target));
+
+        // Wait for peer recovery attempt
+        fileChunkGate.await();
+
+        // Delete index asynchronously while shard is blocked in recovery
+        indicesAdmin().prepareDelete(indexName).execute();
+
+        // Release recovery will make retry mechanism race with index deletion
+        fileChunkGate.release();
+
+        // Let all tasks complete
+        waitNoPendingTasksOnAll();
+
+        // Index should not exist, and we should have reached retry step
+        assertThat(indexExists(indexName), equalTo(false));
+        assertThat(RetryRecoveryTestPlugin.retryCounter.get(), equalTo(1));
+    }
+
     // Test matrix:
-    // no concurrent change
-    // x EMPTY_STORE
-    // x EXISTING_STORE
-    // x LOCAL_SHARDS
-    // x SNAPSHOT
-    // x PEER
     // concurrent with index deletion
-    // - EMPTY_STORE
-    // - EXISTING_STORE
-    // - LOCAL_SHARDS
-    // - SNAPSHOT
-    // - PEER
+    // X EMPTY_STORE
+    // X EXISTING_STORE
+    // X LOCAL_SHARDS
+    // X SNAPSHOT
+    // X PEER
     // shard deletion
     // - EMPTY_STORE
     // - EXISTING_STORE
     // - LOCAL_SHARDS
     // - SNAPSHOT
     // - PEER
+    // new allocation can't grab shard lock (shard moved away and then back while lock is held)
+    // (note that this depends on createShardWhenLockAvailable use the same retry mechanism that rest of recovery does)
     // RESHARD_SPLIT depends on x-pack plugin, so implement inside StatelessReshardIT
 
     static class Gate {

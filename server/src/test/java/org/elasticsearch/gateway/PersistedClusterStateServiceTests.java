ca2b3a963a6d62ce60bee9e9832762ad7dd0e6ca
Dianna Hohensee <dianna.hohensee@elastic.co>
2026-08-05 10:26:56 -0700

    use estimated segment terminology, and bytes

--- server/src/test/java/org/elasticsearch/gateway/PersistedClusterStateServiceTests.java
+++ server/src/test/java/org/elasticsearch/gateway/PersistedClusterStateServiceTests.java
@@ -1452,20 +1452,22 @@ public class PersistedClusterStateServiceTests extends ESTestCase {
 
                 final int indexCount = between(2, randomBoolean() ? 20 : 1000);
 
-                // The defaultMergePolicy set for the cluster state index tier is: 100 segments per tier; and merge batches of 100
-                // segments per tier into 1 segment in the next tier.
+                // The defaultMergePolicy set for the cluster state index is: 100 segments per tier; and merge will batch 100 segments per
+                // tier into 1 segment in the next tier.
                 //
-                // Roughly, an index's smallest segment is used as a heuristic to size all the segments. The cluster state index's smallest
-                // segment is its first write initializing the global metadata, which is only around half the size of a write to add index
-                // metadata. So each index metadata write will add two segments.
+                // Roughly, an index's smallest segment is used as a heuristic with which the merge algorithm estimates the number of
+                // segments in a tier and initiates merges. The merge algorithm does not actually count segments, but rather bytes. The
+                // cluster state index's smallest segment is its first write initializing the global metadata, which is only around half the
+                // size of a write to add index metadata. So while each index metadata write will add one segment, it will be counted by the
+                // heuristic as two segments.
                 //
-                // Lucene's tier system places the first 100 segments into tier-0, then tier-1 holds batches of 100 per entry, tier-2
-                // contains batches of 10000 segments per entry, etc. This test will not populate tier-2: it doesn't drive sufficient
-                // writes.
+                // Lucene's tier system places the first 100 estimated segments into tier-0, then tier-1 holds 100 batches of 100, tier-2
+                // contains 100 batches of 10000 segments, etc. This test will not populate tier-2: it doesn't drive sufficient writes.
                 //
-                // Therefore, in an example of 101 indices, there will be 203 segments (101*2 + 1 for the initial write): the first 100
-                // will go into tier 0, the next 100 will go into the first segment in tier 1, and then the last three will go into another
-                // segment in tier 1. This means the final segment count is 102: those 102 segments will not be merged further.
+                // Therefore, in an example of 101 indices, the merge algorithm will estimate that there are 203 segments (101*2 + 1 for the
+                // initial write) based on bytes: the first 100 will go into tier 0, the next 100 will go into the first segment in tier 1,
+                // and then the last three will go into another segment in tier 1. This means the final estimated segment count is 102:
+                // those 102 estimated segments will not be merged further.
                 //
                 // We'll set the upper bound of segments as if all the segments modulo 100 were in tier-1, and the remainder in tier-0 (just
                 // make it 100, don't need to be that precise).

elastic/elasticsearch

commit 360bc438d2485f4fe0a1a6432d69a9c4949eb786

Makes _terms_enum CPS compatible (#152302)
* makes _terms_enum CPS compatible

* transport changes

* [CI] Auto commit changes from spotless

* iter

* [CI] Auto commit changes from spotless

* validate should be called even if not

* fix CI flakyness

---------

Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/termsenum/action/TransportTermsEnumAction.java
@@ -10,14 +10,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.index.TermsEnum;
 import org.apache.lucene.util.PriorityQueue;
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ExceptionsHelper;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.ActionRunnable;
 import org.elasticsearch.action.OriginalIndices;
+import org.elasticsearch.action.ResolvedIndexExpressions;
 import org.elasticsearch.action.ShardOperationFailedException;
 import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.DefaultShardOperationFailedException;
 import org.elasticsearch.action.support.HandledTransportAction;
+import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.action.support.broadcast.BroadcastShardOperationFailedException;
 import org.elasticsearch.cluster.ProjectState;
 import org.elasticsearch.cluster.block.ClusterBlockException;
@@ -51,6 +54,8 @@ import org.elasticsearch.license.XPackLicenseState;
 import org.elasticsearch.script.ScriptService;
 import org.elasticsearch.search.SearchService;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.crossproject.CrossProjectIndexResolutionValidator;
+import org.elasticsearch.search.crossproject.CrossProjectModeDecider;
 import org.elasticsearch.search.internal.AliasFilter;
 import org.elasticsearch.search.internal.ShardSearchRequest;
 import org.elasticsearch.tasks.Task;
@@ -80,12 +85,14 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 
 import static org.elasticsearch.action.search.TransportSearchHelper.checkCCSVersionCompatibility;
 import static org.elasticsearch.core.Strings.format;
+import static org.elasticsearch.search.crossproject.CrossProjectIndexResolutionValidator.indicesOptionsForCrossProjectFanout;
 import static org.elasticsearch.xpack.core.security.SecurityField.DOCUMENT_LEVEL_SECURITY_FEATURE;
 
 public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRequest, TermsEnumResponse> {
@@ -100,6 +107,7 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
     private final ScriptService scriptService;
     private final ProjectResolver projectResolver;
     private final IndexNameExpressionResolver indexNameExpressionResolver;
+    private final CrossProjectModeDecider crossProjectModeDecider;
 
     final String transportShardAction;
     private final Executor coordinationExecutor;
@@ -119,7 +127,8 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
         XPackLicenseState licenseState,
         Settings settings,
         ProjectResolver projectResolver,
-        IndexNameExpressionResolver indexNameExpressionResolver
+        IndexNameExpressionResolver indexNameExpressionResolver,
+        CrossProjectModeDecider crossProjectModeDecider
     ) {
         super(
             TermsEnumAction.NAME,
@@ -134,6 +143,7 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
         this.transportService = transportService;
         this.projectResolver = projectResolver;
         this.indexNameExpressionResolver = indexNameExpressionResolver;
+        this.crossProjectModeDecider = crossProjectModeDecider;
         this.transportShardAction = actionName + "[s]";
         this.coordinationExecutor = clusterService.threadPool().executor(ThreadPool.Names.SEARCH_COORDINATION);
         this.shardExecutor = clusterService.threadPool().executor(ThreadPool.Names.AUTO_COMPLETE);
@@ -304,7 +314,16 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
         }
 
         List<String> ans = termsList.size() == 1 ? termsList.get(0) : mergeResponses(termsList, request.size());
-        return new TermsEnumResponse(ans, (failedShards + successfulShards), successfulShards, failedShards, shardFailures, complete);
+        ResolvedIndexExpressions resolvedLocally = request.includeResolvedTo() ? request.getResolvedIndexExpressions() : null;
+        return new TermsEnumResponse(
+            ans,
+            (failedShards + successfulShards),
+            successfulShards,
+            failedShards,
+            shardFailures,
+            complete,
+            resolvedLocally
+        );
     }
 
     private static List<String> mergeResponses(List<List<String>> termsList, int size) {
@@ -518,6 +537,9 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
         private final Map<String, Set<ShardId>> nodeBundles;
         private final OriginalIndices localIndices;
         private final Map<String, OriginalIndices> remoteClusterIndices;
+        private final boolean resolveCrossProject;
+        private final Map<String, Exception> remoteExceptions = new ConcurrentHashMap<>();
+        private final Map<String, ResolvedIndexExpressions> resolvedRemotely = new ConcurrentHashMap<>();
 
         protected AsyncBroadcastAction(Task task, TermsEnumRequest request, ActionListener<TermsEnumResponse> listener) {
             this.task = task;
@@ -531,7 +553,11 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
                 throw blockException;
             }
 
-            this.remoteClusterIndices = remoteClusterService.groupIndices(request.indicesOptions(), request.indices());
+            this.resolveCrossProject = crossProjectModeDecider.resolvesCrossProject(request);
+            IndicesOptions groupingOptions = resolveCrossProject
+                ? indicesOptionsForCrossProjectFanout(request.indicesOptions())
+                : request.indicesOptions();
+            this.remoteClusterIndices = remoteClusterService.groupIndices(groupingOptions, request.indices());
             this.localIndices = remoteClusterIndices.remove(RemoteClusterAware.LOCAL_CLUSTER_GROUP_KEY);
 
             // update to concrete indices
@@ -642,7 +668,9 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
 
         void performRemoteClusterOperation(final String clusterAlias, final OriginalIndices remoteIndices, final int opsIndex) {
             try {
-                TermsEnumRequest req = new TermsEnumRequest(request).indices(remoteIndices.indices());
+                TermsEnumRequest req = new TermsEnumRequest(request).indices(remoteIndices.indices())
+                    .indicesOptions(remoteIndices.indicesOptions());
+                req.includeResolvedTo(request.includeResolvedTo() || resolveCrossProject);
 
                 var remoteClient = remoteClusterService.getRemoteClusterClient(
                     clusterAlias,
@@ -665,7 +693,7 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
                     }
                 });
             } catch (Exception exc) {
-                onRemoteClusterFailure(clusterAlias, opsIndex, null);
+                onRemoteClusterFailure(clusterAlias, opsIndex, exc);
             }
         }
 
@@ -682,6 +710,9 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
         private void onRemoteClusterResponse(String clusterAlias, int opsIndex, RemoteClusterTermsEnumResponse response) {
             logger.trace("received response for cluster {}", clusterAlias);
             atomicResponses.set(opsIndex, response);
+            if (resolveCrossProject && response.resp().getResolvedLocally() != null) {
+                resolvedRemotely.put(clusterAlias, response.resp().getResolvedLocally());
+            }
             if (expectedOps == counterOps.incrementAndGet()) {
                 finishHim(true);
             } else {
@@ -699,7 +730,9 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
 
         private void onRemoteClusterFailure(String clusterAlias, int opsIndex, Exception exc) {
             logger.trace("received failure {} for cluster {}", exc, clusterAlias);
-            // TODO: Handle exceptions in the atomic response array
+            if (exc != null && resolveCrossProject) {
+                remoteExceptions.put(clusterAlias, exc);
+            }
             if (expectedOps == counterOps.incrementAndGet()) {
                 finishHim(true);
             }
@@ -711,6 +744,22 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
                 return;
             }
             try {
+                if (resolveCrossProject) {
+                    ResolvedIndexExpressions localResolved = request.getResolvedIndexExpressions();
+                    if (localResolved != null) {
+                        ElasticsearchException validationEx = CrossProjectIndexResolutionValidator.validate(
+                            request.indicesOptions(),
+                            request.getProjectRouting(),
+                            localResolved,
+                            resolvedRemotely,
+                            remoteExceptions
+                        );
+                        if (validationEx != null) {
+                            listener.onFailure(validationEx);
+                            return;
+                        }
+                    }
+                }
                 listener.onResponse(mergeResponses(request, atomicResponses, complete, nodeBundles));
             } catch (Exception e) {
                 listener.onFailure(e);

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/XPackPlugin.java
@@ -378,7 +378,7 @@ public class XPackPlugin extends XPackClientPlugin
         List<RestHandler> handlers = new ArrayList<>();
         handlers.add(new RestXPackInfoAction());
         handlers.add(new RestXPackUsageAction(DiscoveryNode.isStateless(settings)));
-        handlers.add(new RestTermsEnumAction());
+        handlers.add(new RestTermsEnumAction(restHandlersServices.crossProjectModeDecider()));
         handlers.add(new RestGetLicenseAction());
         handlers.add(new RestPutLicenseAction());
         handlers.add(new RestDeleteLicenseAction());

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/termsenum/action/TermsEnumAction.java
@@ -28,6 +28,7 @@ public class TermsEnumAction extends ActionType<TermsEnumResponse> {
 
     static final ParseField INDEX_FILTER = new ParseField("index_filter");
     static final ParseField TIMEOUT = new ParseField("timeout");
+    static final ParseField PROJECT_ROUTING = new ParseField("project_routing");
 
     private TermsEnumAction() {
         super(NAME);
@@ -53,5 +54,6 @@ public class TermsEnumAction extends ActionType<TermsEnumResponse> {
             ObjectParser.ValueType.STRING
         );
         PARSER.declareObject(TermsEnumRequest::indexFilter, (p, context) -> parseTopLevelQuery(p), INDEX_FILTER);
+        PARSER.declareString(TermsEnumRequest::projectRouting, PROJECT_ROUTING);
     }
 }

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/termsenum/action/TermsEnumRequest.java
@@ -7,7 +7,9 @@
 package org.elasticsearch.xpack.core.termsenum.action;
 
 import org.apache.lucene.util.BytesRef;
+import org.elasticsearch.TransportVersion;
 import org.elasticsearch.action.ActionRequestValidationException;
+import org.elasticsearch.action.ResolvedIndexExpressions;
 import org.elasticsearch.action.ValidateActions;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.support.IndicesOptions;
@@ -32,6 +34,8 @@ import static org.apache.lucene.index.IndexWriter.MAX_TERM_LENGTH;
  */
 public final class TermsEnumRequest extends BroadcastRequest<TermsEnumRequest> implements ToXContentObject {
 
+    static final TransportVersion TERMS_ENUM_CPS_RESOLVED_EXPRESSIONS = TransportVersion.fromName("terms_enum_cps_resolved_expressions");
+
     public static final IndicesOptions DEFAULT_INDICES_OPTIONS = SearchRequest.DEFAULT_INDICES_OPTIONS;
     public static final int DEFAULT_SIZE = 10;
     public static final TimeValue DEFAULT_TIMEOUT = new TimeValue(1000);
@@ -42,6 +46,14 @@ public final class TermsEnumRequest extends BroadcastRequest<TermsEnumRequest> i
     private int size = DEFAULT_SIZE;
     private boolean caseInsensitive;
     private QueryBuilder indexFilter;
+    // Set internally by RestTermsEnumAction, not client-supplied XContent; excluded from equals/hashCode
+    // so it doesn't break XContent round-trip equality checks.
+    private boolean includeResolvedTo = false;
+
+    @Nullable
+    private transient ResolvedIndexExpressions resolvedIndexExpressions;
+    @Nullable
+    private transient String projectRouting;
 
     public TermsEnumRequest() {
         this(Strings.EMPTY_ARRAY);
@@ -64,6 +76,8 @@ public final class TermsEnumRequest extends BroadcastRequest<TermsEnumRequest> i
         this.caseInsensitive = clone.caseInsensitive;
         this.size = clone.size;
         this.indexFilter = clone.indexFilter;
+        this.includeResolvedTo = clone.includeResolvedTo;
+        // projectRouting and resolvedIndexExpressions are not copied: they are set on the coordinator by the security layer
         indices(clone.indices);
         indicesOptions(clone.indicesOptions());
         timeout(clone.timeout());
@@ -78,6 +92,11 @@ public final class TermsEnumRequest extends BroadcastRequest<TermsEnumRequest> i
         caseInsensitive = in.readBoolean();
         size = in.readVInt();
         indexFilter = in.readOptionalNamedWriteable(QueryBuilder.class);
+        if (in.getTransportVersion().supports(TERMS_ENUM_CPS_RESOLVED_EXPRESSIONS)) {
+            includeResolvedTo = in.readBoolean();
+        } else {
+            includeResolvedTo = false;
+        }
     }
 
     @Override
@@ -89,6 +108,9 @@ public final class TermsEnumRequest extends BroadcastRequest<TermsEnumRequest> i
         out.writeBoolean(caseInsensitive);
         out.writeVInt(size);
         out.writeOptionalNamedWriteable(indexFilter);
+        if (out.getTransportVersion().supports(TERMS_ENUM_CPS_RESOLVED_EXPRESSIONS)) {
+            out.writeBoolean(includeResolvedTo);
+        }
     }
 
     @Override
@@ -108,6 +130,43 @@ public final class TermsEnumRequest extends BroadcastRequest<TermsEnumRequest> i
             builder.field("index_filter", indexFilter);
         }
         return builder.endObject();
+    }
+
+    @Override
+    public boolean allowsCrossProject() {
+        return true;
+    }
+
+    @Override
+    public void setResolvedIndexExpressions(ResolvedIndexExpressions expressions) {
+        this.resolvedIndexExpressions = expressions;
+    }
+
+    @Override
+    @Nullable
+    public ResolvedIndexExpressions getResolvedIndexExpressions() {
+        return resolvedIndexExpressions;
+    }
+
+    public void projectRouting(@Nullable String projectRouting) {
+        if (this.projectRouting != null) {
+            throw new IllegalArgumentException("project_routing is already set to [" + this.projectRouting + "]");
+        }
+        this.projectRouting = projectRouting;
+    }
+
+    @Override
+    public String getProjectRouting() {
+        return projectRouting;
+    }
+
+    public TermsEnumRequest includeResolvedTo(boolean includeResolvedTo) {
+        this.includeResolvedTo = includeResolvedTo;
+        return this;
+    }
+
+    public boolean includeResolvedTo() {
+        return includeResolvedTo;
     }
 
     @Override
@@ -133,6 +192,12 @@ public final class TermsEnumRequest extends BroadcastRequest<TermsEnumRequest> i
             if (timeout().getSeconds() > 60) {
                 validationException = ValidateActions.addValidationError("Timeout cannot be > 1 minute", validationException);
             }
+        }
+        if (projectRouting != null && indicesOptions().resolveCrossProjectIndexExpression() == false) {
+            validationException = ValidateActions.addValidationError(
+                "Unknown key for a VALUE_STRING in [project_routing]",
+                validationException
+            );
         }
         return validationException;
     }

commit 423955eb10f17c9307a033fa817128f511ee22d8

Fix _terms_enum returning 200 instead of 404 for missing index with CPS
When serverless.cross_project.enabled=true, a _terms_enum request against
a non-existent concrete index incorrectly returned 200 with an empty terms
list instead of 404. The bug was introduced by #152302.

Root cause: AsyncBroadcastAction.start() short-circuited via a direct
listener.onResponse() call when expectedOps==0 (no local shards, no remote
fan-out), bypassing finishHim() where CrossProjectIndexResolutionValidator
runs. That validator reads the CONCRETE_RESOURCE_NOT_VISIBLE result recorded
by the security layer and returns IndexNotFoundException.

Fix: replace the short-circuit branch with finishHim(true) so CPS validation
runs consistently regardless of whether there is anything to fan out to.

Adds TermsEnumCpsIT with three cases: the regression (missing concrete index
must return 404), and two controls confirming the fix does not regress
ignore_unavailable=true or wildcard-matches-nothing (both must return 200).

x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/termsenum/action/TransportTermsEnumAction.java
@@ -579,13 +579,7 @@ public class TransportTermsEnumAction extends HandledTransportAction<TermsEnumRe
 
         public void start() {
             if (expectedOps == 0) {
-                // no shards
-                try {
-                    listener.onResponse(mergeResponses(request, new AtomicReferenceArray<>(0), true, nodeBundles));
-                } catch (Exception e) {
-                    listener.onFailure(e);
-                }
-                // TODO or remove above try and instead just call finishHim() here? Helps keep return logic consistent
+                finishHim(true);
                 return;
             }
             // count the local operations, and perform the non local ones

x-pack/plugin/core/src/internalClusterTest/java/org/elasticsearch/xpack/core/termsenum/TermsEnumCpsIT.java
@@ -1,0 +1,158 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+package org.elasticsearch.xpack.core.termsenum;
+
+import org.elasticsearch.ExceptionsHelper;
+import org.elasticsearch.action.ResolvedIndexExpression;
+import org.elasticsearch.action.ResolvedIndexExpressions;
+import org.elasticsearch.action.support.IndicesOptions;
+import org.elasticsearch.common.settings.Setting;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.IndexNotFoundException;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.test.ESSingleNodeTestCase;
+import org.elasticsearch.xpack.core.LocalStateCompositeXPackPlugin;
+import org.elasticsearch.xpack.core.XPackSettings;
+import org.elasticsearch.xpack.core.termsenum.action.TermsEnumAction;
+import org.elasticsearch.xpack.core.termsenum.action.TermsEnumRequest;
+import org.elasticsearch.xpack.core.termsenum.action.TermsEnumResponse;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.instanceOf;
+
+/**
+ * Integration test for _terms_enum behavior when Cross-Project Search (CPS) is enabled
+ * ({@code serverless.cross_project.enabled=true}).
+ */
+public class TermsEnumCpsIT extends ESSingleNodeTestCase {
+
+    @Override
+    protected Collection<Class<? extends Plugin>> getPlugins() {
+        return List.of(LocalStateCompositeXPackPlugin.class, CpsPlugin.class);
+    }
+
+    @Override
+    protected Settings nodeSettings() {
+        return Settings.builder()
+            .put(super.nodeSettings())
+            .put(XPackSettings.SECURITY_ENABLED.getKey(), false)
+            .put("serverless.cross_project.enabled", true)
+            .build();
+    }
+
+    /**
+     * Regression test for the bug introduced by #152302:
+     * with CPS enabled, a _terms_enum request against a non-existent concrete index returned 200 with an empty
+     * terms list instead of 404.
+     *
+     * Root cause: {@code AsyncBroadcastAction.start()} short-circuited directly to
+     * {@code listener.onResponse} when {@code expectedOps == 0} (no local shards, no remote fan-out),
+     * bypassing {@code finishHim()} where the {@link
+     * org.elasticsearch.search.crossproject.CrossProjectIndexResolutionValidator} check runs.
+     *
+     * The fix is to call {@code finishHim(true)} in the zero-ops branch so that the validator
+     * sees the {@code CONCRETE_RESOURCE_NOT_VISIBLE} result recorded by the security layer and
+     * returns an {@link IndexNotFoundException}.
+     *
+     * Because this test runs without the security plugin (to keep it simple), the
+     * {@link ResolvedIndexExpressions} that the security layer would normally populate is
+     * pre-populated manually to exercise the same code path in
+     * {@code TransportTermsEnumAction.AsyncBroadcastAction.finishHim()}.
+     */
+    public void testMissingConcreteIndexReturnsWith404WhenCpsEnabled() {
+        // The security layer (IndicesAndAliasesResolver) records CONCRETE_RESOURCE_NOT_VISIBLE for
+        // a concrete index that does not exist when CPS-lenient options (ignoreUnavailable=true) are used.
+        // Pre-populate that result here to exercise the finishHim() validation path.
+        ResolvedIndexExpressions.Builder expressionsBuilder = ResolvedIndexExpressions.builder();
+        expressionsBuilder.addExpressions(
+            "non-existent-index",
+            new HashSet<>(),
+            ResolvedIndexExpression.LocalIndexResolutionResult.CONCRETE_RESOURCE_NOT_VISIBLE,
+            Set.of()
+        );
+        ResolvedIndexExpressions resolvedExpressions = expressionsBuilder.build();
+
+        TermsEnumRequest request = new TermsEnumRequest("non-existent-index").field("some.keyword");
+        // Replicate what RestTermsEnumAction does for CPS-enabled clusters: opt the request into
+        // cross-project resolution so that crossProjectModeDecider.resolvesCrossProject() returns true.
+        request.indicesOptions(
+            IndicesOptions.builder(request.indicesOptions())
+                .crossProjectModeOptions(new IndicesOptions.CrossProjectModeOptions(true))
+                .build()
+        );
+        request.setResolvedIndexExpressions(resolvedExpressions);
+
+        ExecutionException ex = expectThrows(ExecutionException.class, () -> client().execute(TermsEnumAction.INSTANCE, request).get());
+        assertThat(ExceptionsHelper.unwrapCause(ex.getCause()), instanceOf(IndexNotFoundException.class));
+    }
+
+    /**
+     * Control test: a concrete index that is missing but {@code ignore_unavailable=true} is set should return an empty terms list,
+     * not a 404. The {@link org.elasticsearch.search.crossproject.CrossProjectIndexResolutionValidator} respects
+     * {@code ignore_unavailable} and does not treat {@code CONCRETE_RESOURCE_NOT_VISIBLE} as a not-found failure
+     * when that flag is set.
+     */
+    public void testMissingConcreteIndexReturnsEmptyWhenIgnoreUnavailableAndCpsEnabled() {
+        ResolvedIndexExpressions.Builder expressionsBuilder = ResolvedIndexExpressions.builder();
+        expressionsBuilder.addExpressions(
+            "non-existent-index",
+            new HashSet<>(),
+            ResolvedIndexExpression.LocalIndexResolutionResult.CONCRETE_RESOURCE_NOT_VISIBLE,
+            Set.of()
+        );
+        ResolvedIndexExpressions resolvedExpressions = expressionsBuilder.build();
+
+        TermsEnumRequest request = new TermsEnumRequest("non-existent-index").field("some.keyword");
+        request.indicesOptions(
+            IndicesOptions.builder(request.indicesOptions())
+                .crossProjectModeOptions(new IndicesOptions.CrossProjectModeOptions(true))
+                .concreteTargetOptions(IndicesOptions.ConcreteTargetOptions.ALLOW_UNAVAILABLE_TARGETS)
+                .build()
+        );
+        request.setResolvedIndexExpressions(resolvedExpressions);
+
+        TermsEnumResponse response = client().execute(TermsEnumAction.INSTANCE, request).actionGet();
+        assertThat(response.getTerms(), empty());
+        assertTrue(response.isComplete());
+    }
+
+    /**
+     * Control test: a wildcard expression that matches no indices should return an empty terms list when CPS is enabled.
+     * {@code allow_no_indices=true} is the default for {@link TermsEnumRequest}, so zero wildcard matches is not an error.
+     * This confirms that routing the zero-ops case through {@code finishHim()} does not change the behaviour for wildcards.
+     */
+    public void testWildcardMatchingNoIndicesReturnsEmptyWhenCpsEnabled() {
+        TermsEnumRequest request = new TermsEnumRequest("non-existent-*").field("some.keyword");
+        request.indicesOptions(
+            IndicesOptions.builder(request.indicesOptions())
+                .crossProjectModeOptions(new IndicesOptions.CrossProjectModeOptions(true))
+                .build()
+        );
+
+        TermsEnumResponse response = client().execute(TermsEnumAction.INSTANCE, request).actionGet();
+        assertThat(response.getTerms(), empty());
+        assertTrue(response.isComplete());
+    }
+
+    /**
+     * Registers {@code serverless.cross_project.enabled} as a valid node-scoped setting.
+     * In production the setting is registered by a serverless-only module; in tests this
+     * lightweight plugin stands in so that the node does not reject it as unknown.
+     */
+    public static class CpsPlugin extends Plugin {
+        @Override
+        public List<Setting<?>> getSettings() {
+            return List.of(Setting.boolSetting("serverless.cross_project.enabled", false, Setting.Property.NodeScope));
+        }
+    }
+}
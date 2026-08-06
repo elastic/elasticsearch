e070941fc436511ec5a3759f782fc94137d3653c
Rene Groeschke <rene@elastic.co>
2026-08-06 22:03:14 +0200

    Migrate mixed-tier-cluster to JUnit rule clusters
    Replace the legacy testClusters DSL in x-pack:qa:mixed-tier-cluster
    with the JUnit rule-based ElasticsearchCluster framework.
    
    Switch the plugin to internal-java-rest-test, build the 3-node
    cluster with per-node settings in the test class, and drive the
    mixed-version state via ElasticsearchCluster#upgradeNodeToVersion.
    Drop the project from the legacy-yaml/java-rest-test whitelist.

--- x-pack/qa/mixed-tier-cluster/src/javaRestTest/java/org/elasticsearch/mixed/DataTierMixedIT.java
+++ x-pack/qa/mixed-tier-cluster/src/javaRestTest/java/org/elasticsearch/mixed/DataTierMixedIT.java
@@ -7,11 +7,47 @@
 
 package org.elasticsearch.mixed;
 
+import org.elasticsearch.test.cluster.ElasticsearchCluster;
+import org.elasticsearch.test.cluster.local.distribution.DistributionType;
+import org.elasticsearch.test.cluster.util.Version;
 import org.elasticsearch.test.rest.ESRestTestCase;
+import org.junit.ClassRule;
 
 public class DataTierMixedIT extends ESRestTestCase {
 
+    private static final String OLD_CLUSTER_VERSION = System.getProperty("tests.old_cluster_version");
+
+    @ClassRule
+    public static ElasticsearchCluster cluster = buildCluster();
+
+    private static ElasticsearchCluster buildCluster() {
+        Version oldVersion = Version.fromString(OLD_CLUSTER_VERSION);
+        // data_* roles were introduced in 7.10.0, so use 'data' for older versions
+        String dataNodeRoles = oldVersion.before("7.10.0") ? "[\"data\"]" : "[\"data_content\", \"data_hot\"]";
+
+        var builder = ElasticsearchCluster.local()
+            .distribution(DistributionType.DEFAULT)
+            .version(OLD_CLUSTER_VERSION, isOldClusterDetachedVersion())
+            .setting("xpack.security.enabled", "false")
+            .setting("xpack.watcher.enabled", "false")
+            .setting("xpack.ml.enabled", "false")
+            .setting("xpack.license.self_generated.type", "trial")
+            .withNode(node -> node.setting("node.roles", "[\"master\"]"))
+            .withNode(node -> node.setting("node.roles", dataNodeRoles))
+            .withNode(node -> node.setting("node.roles", "[\"master\"]"));
+
+        return builder.build();
+    }
+
+    @Override
+    protected String getTestRestCluster() {
+        return cluster.getHttpAddresses();
+    }
+
     public void testMixedTierCompatibility() throws Exception {
+        // Take the first node to the current version to create a mixed-version cluster.
+        cluster.upgradeNodeToVersion(0, Version.CURRENT);
+
         createIndex("test-index", indexSettings(1, 0).build());
         ensureGreen("test-index");
     }

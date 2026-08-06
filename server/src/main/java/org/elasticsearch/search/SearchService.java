4d15d606679ca2f479a1418c6bdd3620531beb97
Dimitris Rempapis <dimitris.rempapis@elastic.co>
2026-08-06 21:57:20 +0300

    Enable chunked fetch phase by default (#156044) (#156086)

--- server/src/main/java/org/elasticsearch/search/SearchService.java
+++ server/src/main/java/org/elasticsearch/search/SearchService.java
@@ -270,11 +270,9 @@ public class SearchService extends AbstractLifecycleComponent implements IndexEv
         Property.Dynamic
     );
 
-    private static final boolean CHUNKED_FETCH_PHASE_FEATURE_FLAG = new FeatureFlag("chunked_fetch_phase_enabled").isEnabled();
-
     public static final Setting<Boolean> FETCH_PHASE_CHUNKED_ENABLED = Setting.boolSetting(
         "search.fetch_phase_chunked_enabled",
-        CHUNKED_FETCH_PHASE_FEATURE_FLAG,
+        true,
         Property.NodeScope,
         Property.Dynamic
     );

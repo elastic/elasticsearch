e1f7c10d37ee0c5f3ff74326c576521a8c111a37
Kostas Krikellas <kostas.krikellas@elastic.co>
2026-08-07 12:44:12 +0300

    Fix benchmark IndexProperties calls: add explicit shard count arg

--- benchmarks/src/main/java/org/elasticsearch/benchmark/_nightly/esql/QueryPlanningBenchmark.java
+++ benchmarks/src/main/java/org/elasticsearch/benchmark/_nightly/esql/QueryPlanningBenchmark.java
@@ -108,7 +108,7 @@ public class QueryPlanningBenchmark {
             mapping.put("field" + i, new EsField("field-" + i, TEXT, emptyMap(), true, EsField.TimeSeriesFieldType.NONE));
         }
 
-        var esIndex = new EsIndex("test", mapping, Map.of("test", new IndexProperties(IndexMode.STANDARD)), Map.of(), Map.of());
+        var esIndex = new EsIndex("test", mapping, Map.of("test", new IndexProperties(IndexMode.STANDARD, 0)), Map.of(), Map.of());
 
         var functionRegistry = new EsqlFunctionRegistry();
         parser = new EsqlParser(new EsqlConfig(functionRegistry));

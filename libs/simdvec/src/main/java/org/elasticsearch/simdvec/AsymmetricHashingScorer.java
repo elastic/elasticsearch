c598db4467acf060c324d7cee8cfe358d3f80d42
Simon Cooper <simon.cooper@elastic.co>
2026-08-07 10:28:56 +0100

    Add note about float-bit comparison

--- libs/simdvec/src/main/java/org/elasticsearch/simdvec/AsymmetricHashingScorer.java
+++ libs/simdvec/src/main/java/org/elasticsearch/simdvec/AsymmetricHashingScorer.java
@@ -125,6 +125,7 @@ public final class AsymmetricHashingScorer {
         for (int j = 0; j < nDims; j++) {
             float qt = queryTransformed[j];
             sumAll += qt;
+            // TODO: this is a more general form of ESVectorUtil.ipFloatBit
             int byteIdx = j >>> 3;
             int bitIdx = 7 - (j & 7);
             for (int p = 0; p < bitsPerDim; p++) {

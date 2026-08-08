b4ef35a9d1b1eaf6d48a33e5ef85af116051b51c
Tim Brooks <tim@timbrooks.org>
2026-08-08 12:03:34 -0600

    Swap to true

--- server/src/main/java/org/elasticsearch/action/bulk/ShardBatchIndexer.java
+++ server/src/main/java/org/elasticsearch/action/bulk/ShardBatchIndexer.java
@@ -39,7 +39,7 @@ public final class ShardBatchIndexer {
     private static final Logger logger = LogManager.getLogger(ShardBatchIndexer.class);
 
     public static final FeatureFlag BATCH_INDEXING_FEATURE_FLAG = new FeatureFlag("batch_indexing");
-    public static final Setting<Boolean> BATCH_INDEXING = boolSetting("indices.batch_indexing", false, value -> {
+    public static final Setting<Boolean> BATCH_INDEXING = boolSetting("indices.batch_indexing", true, value -> {
         if (value && BATCH_INDEXING_FEATURE_FLAG.isEnabled() == false) {
             throw new IllegalArgumentException(
                 "[indices.batch_indexing] can only be enabled when the batch_indexing feature flag is enabled"

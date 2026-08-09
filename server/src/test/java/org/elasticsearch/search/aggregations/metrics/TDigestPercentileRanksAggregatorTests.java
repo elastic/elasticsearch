d8be6aa8b307a00f9a2f01e22666bcd2ef5f6339
hawk huang <hawk.huang@elastic.co>
2026-08-09 11:52:41 +0100

    revert PercentileRanks breaker tests: fix is in the abstract class, already covered

--- server/src/test/java/org/elasticsearch/search/aggregations/metrics/TDigestPercentileRanksAggregatorTests.java
+++ server/src/test/java/org/elasticsearch/search/aggregations/metrics/TDigestPercentileRanksAggregatorTests.java
@@ -11,26 +11,15 @@ package org.elasticsearch.search.aggregations.metrics;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.SortedNumericDocValuesField;
-import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.MultiReader;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.tests.index.RandomIndexWriter;
 import org.apache.lucene.util.NumericUtils;
-import org.elasticsearch.common.breaker.CircuitBreaker;
-import org.elasticsearch.common.breaker.CircuitBreakingException;
-import org.elasticsearch.common.lucene.search.Queries;
-import org.elasticsearch.common.settings.ClusterSettings;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.core.CheckedConsumer;
 import org.elasticsearch.index.mapper.MappedFieldType;
 import org.elasticsearch.index.mapper.NumberFieldMapper;
-import org.elasticsearch.indices.breaker.CircuitBreakerMetrics;
-import org.elasticsearch.indices.breaker.HierarchyCircuitBreakerService;
 import org.elasticsearch.search.aggregations.AggregationBuilder;
-import org.elasticsearch.search.aggregations.Aggregator;
 import org.elasticsearch.search.aggregations.AggregatorTestCase;
-import org.elasticsearch.search.aggregations.support.AggregationContext;
 import org.elasticsearch.search.aggregations.support.AggregationInspectionHelper;
 import org.elasticsearch.search.aggregations.support.CoreValuesSourceType;
 import org.elasticsearch.search.aggregations.support.ValuesSourceType;
@@ -39,10 +28,6 @@ import org.hamcrest.Matchers;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
-
-import static java.util.Collections.singleton;
-import static org.elasticsearch.test.InternalAggregationTestCase.DEFAULT_MAX_BUCKETS;
-import static org.hamcrest.Matchers.equalTo;
 
 public class TDigestPercentileRanksAggregatorTests extends AggregatorTestCase {
 
@@ -127,95 +112,5 @@ public class TDigestPercentileRanksAggregatorTests extends AggregatorTestCase {
         );
 
         assertThat(e.getMessage(), Matchers.equalTo("[values] must not be an empty array: [my_agg]"));
-    }
-
-    public void testBreakerBytesReleasedAfterSuccessfulAggregation() throws IOException {
-        MappedFieldType fieldType = new NumberFieldMapper.NumberFieldType("number", NumberFieldMapper.NumberType.LONG);
-        PercentileRanksAggregationBuilder aggBuilder = new PercentileRanksAggregationBuilder("ranks", new double[] { 50.0, 250.0 })
-            .field("number")
-            .percentilesConfig(new PercentilesConfig.TDigest());
-
-        HierarchyCircuitBreakerService breakerService = requestBreakerService("10mb");
-        withSequentialIndex(100, reader -> {
-            try (
-                AggregationContext context = createAggregationContext(
-                    reader,
-                    createIndexSettings(),
-                    Queries.ALL_DOCS_INSTANCE,
-                    breakerService,
-                    AggregationBuilder.DEFAULT_PREALLOCATION,
-                    DEFAULT_MAX_BUCKETS,
-                    false,
-                    false,
-                    fieldType
-                )
-            ) {
-                Aggregator aggregator = createAggregator(aggBuilder, context);
-                aggregator.preCollection();
-                context.searcher().search(Queries.ALL_DOCS_INSTANCE, aggregator.asCollector());
-                aggregator.postCollection();
-                aggregator.buildTopLevel();
-            }
-            assertThat(breakerService.getBreaker(CircuitBreaker.REQUEST).getUsed(), equalTo(0L));
-        });
-    }
-
-    public void testBreakerTripReleasesAllBytes() throws IOException {
-        HierarchyCircuitBreakerService breakerService = requestBreakerService("1kb");
-        MappedFieldType fieldType = new NumberFieldMapper.NumberFieldType("number", NumberFieldMapper.NumberType.LONG);
-        PercentileRanksAggregationBuilder aggBuilder = new PercentileRanksAggregationBuilder("ranks", new double[] { 50.0, 250.0 })
-            .field("number")
-            .percentilesConfig(new PercentilesConfig.TDigest());
-
-        withSequentialIndex(500, reader -> {
-            expectThrows(CircuitBreakingException.class, () -> {
-                try (
-                    AggregationContext context = createAggregationContext(
-                        reader,
-                        createIndexSettings(),
-                        Queries.ALL_DOCS_INSTANCE,
-                        breakerService,
-                        0,
-                        DEFAULT_MAX_BUCKETS,
-                        false,
-                        false,
-                        fieldType
-                    )
-                ) {
-                    Aggregator aggregator = createAggregator(aggBuilder, context);
-                    aggregator.preCollection();
-                    context.searcher().search(Queries.ALL_DOCS_INSTANCE, aggregator.asCollector());
-                    aggregator.postCollection();
-                    aggregator.buildTopLevel();
-                }
-            });
-            assertThat(breakerService.getBreaker(CircuitBreaker.REQUEST).getUsed(), equalTo(0L));
-        });
-    }
-
-    private void withSequentialIndex(int docCount, CheckedConsumer<DirectoryReader, IOException> body) throws IOException {
-        try (Directory directory = newDirectory()) {
-            try (RandomIndexWriter iw = new RandomIndexWriter(random(), directory)) {
-                for (int i = 0; i < docCount; i++) {
-                    iw.addDocument(singleton(new SortedNumericDocValuesField("number", i)));
-                }
-            }
-            try (DirectoryReader reader = DirectoryReader.open(directory)) {
-                body.accept(reader);
-            }
-        }
-    }
-
-    private static HierarchyCircuitBreakerService requestBreakerService(String requestLimit) {
-        Settings settings = Settings.builder()
-            .put(HierarchyCircuitBreakerService.REQUEST_CIRCUIT_BREAKER_LIMIT_SETTING.getKey(), requestLimit)
-            .put(HierarchyCircuitBreakerService.USE_REAL_MEMORY_USAGE_SETTING.getKey(), false)
-            .build();
-        return new HierarchyCircuitBreakerService(
-            CircuitBreakerMetrics.NOOP,
-            settings,
-            List.of(),
-            new ClusterSettings(settings, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS)
-        );
     }
 }

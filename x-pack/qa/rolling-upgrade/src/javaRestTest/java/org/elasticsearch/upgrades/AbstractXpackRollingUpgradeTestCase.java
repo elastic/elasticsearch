b170d1bda4982c779c26a74a9e56c140701eea97
Mateusz Rzeszutek <mateusz.rzeszutek@elastic.co>
2026-08-10 11:13:10 +0200

    Move ParameterizedRollingUpgradeTestCase to test-framework (#155762)
    * Mv ParameterizedRollingUpgradeTestCase to test-fw
    
    Continuation of #155112
    
    This PR contains several minor cleanups:
    - Moves the `ParameterizedRollingUpgradeTestCase` base class into
      `:test:framework` module
    - In `:x-pack:qa:rolling-upgrade`, move test-specific configuration into
      the tests themselves (from the abstract test base class), e.g. GeoIp
      test config
    
    * add missing public classifier
    
    * apply code review comments
    
    * apply code review comments
    
    (cherry picked from commit 284e8c329d5d397c02004e98bc59f4391370d689)

--- x-pack/qa/rolling-upgrade/src/javaRestTest/java/org/elasticsearch/upgrades/AbstractXpackRollingUpgradeTestCase.java
+++ x-pack/qa/rolling-upgrade/src/javaRestTest/java/org/elasticsearch/upgrades/AbstractXpackRollingUpgradeTestCase.java
@@ -7,60 +7,37 @@
 
 package org.elasticsearch.upgrades;
 
-import org.elasticsearch.core.SuppressForbidden;
+import org.elasticsearch.test.ParameterizedRollingUpgradeTestCase;
 import org.elasticsearch.test.cluster.ElasticsearchCluster;
+import org.elasticsearch.test.cluster.local.LocalClusterSpecBuilder;
 import org.elasticsearch.test.cluster.local.distribution.DistributionType;
 import org.elasticsearch.test.cluster.util.Version;
-import org.junit.ClassRule;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TemporaryFolder;
-import org.junit.rules.TestRule;
 
-import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public abstract class AbstractXpackRollingUpgradeTestCase extends ParameterizedRollingUpgradeTestCase {
 
-    private static final TemporaryFolder repoDirectory = new TemporaryFolder();
+    public AbstractXpackRollingUpgradeTestCase(int upgradedNodes) {
+        super(upgradedNodes);
+    }
 
-    private static final ElasticsearchCluster cluster = buildCluster();
+    protected static ElasticsearchCluster buildCluster() {
+        return buildCluster(b -> b);
+    }
 
-    @ClassRule
-    public static TestRule ruleChain = RuleChain.outerRule(repoDirectory).around(cluster);
-
-    private static ElasticsearchCluster buildCluster() {
+    protected static ElasticsearchCluster buildCluster(UnaryOperator<LocalClusterSpecBuilder<ElasticsearchCluster>> customizer) {
         var builder = ElasticsearchCluster.local()
             .distribution(DistributionType.DEFAULT)
             .version(getOldClusterVersion())
             .nodes(NODE_NUM)
             .setting("xpack.license.self_generated.type", "trial")
-            .setting("xpack.security.enabled", "false")
-            .systemProperty("ingest.geoip.downloader.enabled.default", "true")
-            .systemProperty("ingest.geoip.downloader.endpoint.default", "http://invalid.endpoint")
-            .setting("ingest.geoip.downloader.endpoint", "http://invalid.endpoint")
-            .setting("path.repo", new Supplier<>() {
-                @Override
-                @SuppressForbidden(reason = "TemporaryFolder only has io.File methods, not nio.File")
-                public String get() {
-                    return repoDirectory.getRoot().getPath();
-                }
-            })
-            .setting("xpack.searchable.snapshot.shared_cache.size", "16MB")
-            .setting("xpack.searchable.snapshot.shared_cache.region_size", "256KB");
+            .setting("xpack.security.enabled", "false");
 
         if (getOldClusterTestVersion().before(Version.fromString("8.18.0"))) {
             builder.jvmArg("-da:org.elasticsearch.index.mapper.DocumentMapper");
             builder.jvmArg("-da:org.elasticsearch.index.mapper.MapperService");
         }
 
-        return builder.build();
-    }
-
-    public AbstractXpackRollingUpgradeTestCase(int upgradedNodes) {
-        super(upgradedNodes);
-    }
-
-    @Override
-    protected ElasticsearchCluster getUpgradeCluster() {
-        return cluster;
+        return customizer.apply(builder).build();
     }
 }

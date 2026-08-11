69414f91cecc549112eb8e04b24fc933ad4e448b
Martijn van Groningen <martijn.v.groningen@gmail.com>
2026-08-11 16:52:44 +0700

    Revert "Consolidate fallback storage routing logic into central location" and unmute tests (#156367)
    This reverts commit c4dc77a2aa6b6852984c57761bdd1f284b1322a9 and unmutes tests.
    
    Closes #156349
    Closes #156350
    Closes #156351
    Closes #156352
    Closes #156353
    Closes #156354
    Closes #156355
    Closes #156361
    Closes #156361
    Closes #156361

--- server/src/main/java/org/elasticsearch/index/mapper/IgnoreMalformedStoredValues.java
+++ server/src/main/java/org/elasticsearch/index/mapper/IgnoreMalformedStoredValues.java
@@ -38,9 +38,8 @@ public abstract class IgnoreMalformedStoredValues {
 
     /**
      * Stores a malformed value in binary doc values (new indices) or in stored fields (old indices) in order to support synthetic source.
-     * Use {@link FallbackPostMapper#capture(DocumentParserContext, String, FallbackPostMapper.Reason)} from outside this package.
      */
-    static void storeMalformedValueForSyntheticSource(DocumentParserContext context, String fieldPath, XContentParser parser)
+    public static void storeMalformedValueForSyntheticSource(DocumentParserContext context, String fieldPath, XContentParser parser)
         throws IOException {
         IndexVersion indexVersion = context.indexSettings().getIndexVersionCreated();
         if (indexVersion.onOrAfter(IndexVersions.STORE_IGNORED_MALFORMED_IN_BINARY_DOC_VALUES)) {
@@ -53,11 +52,8 @@ public abstract class IgnoreMalformedStoredValues {
 
     /**
      * Stores a malformed value in binary doc values (new indices) or in stored fields (old indices) in order to support synthetic source.
-     * Use {@link FallbackPostMapper#capture(DocumentParserContext, String, FallbackPostMapper.Reason,
-     * org.elasticsearch.xcontent.XContentBuilder)}
-     * from outside this package.
      */
-    static void storeMalformedValueForSyntheticSource(DocumentParserContext context, String fieldPath, XContentBuilder builder)
+    public static void storeMalformedValueForSyntheticSource(DocumentParserContext context, String fieldPath, XContentBuilder builder)
         throws IOException {
         IndexVersion indexVersion = context.indexSettings().getIndexVersionCreated();
         if (indexVersion.onOrAfter(IndexVersions.STORE_IGNORED_MALFORMED_IN_BINARY_DOC_VALUES)) {

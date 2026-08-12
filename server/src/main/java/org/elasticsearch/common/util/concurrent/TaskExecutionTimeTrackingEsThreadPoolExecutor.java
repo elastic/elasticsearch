20e41d7554d25fc348ccee9fa29105cde5f80861
Dianna Hohensee <dianna.hohensee@elastic.co>
2026-08-12 16:02:44 -0700

    Add shard-level write thread pool utilization tracking (#155656)
    Adds a tracker to the IndexShard to count the operation time
    spent on the WRITE thread pool. Supports periodic polling.
    
    Modularizes thread utilization tracking over polling periods into a
    new class ThreadUtilizationTracker.
    
    Relates #elastic/elasticsearch-team/issues/4059

--- server/src/main/java/org/elasticsearch/common/util/concurrent/TaskExecutionTimeTrackingEsThreadPoolExecutor.java
+++ server/src/main/java/org/elasticsearch/common/util/concurrent/TaskExecutionTimeTrackingEsThreadPoolExecutor.java
@@ -12,6 +12,7 @@ package org.elasticsearch.common.util.concurrent;
 import org.elasticsearch.common.ExponentiallyWeightedMovingAverage;
 import org.elasticsearch.common.metrics.ExponentialBucketHistogram;
 import org.elasticsearch.common.metrics.ExponentiallyWeightedMovingRate;
+import org.elasticsearch.common.util.ThreadUtilizationTracker;
 import org.elasticsearch.common.util.concurrent.EsExecutors.HotThreadsOnLargeQueueConfig;
 import org.elasticsearch.common.util.concurrent.EsExecutors.TaskTrackingConfig;
 import org.elasticsearch.core.Nullable;
@@ -65,8 +66,8 @@ public final class TaskExecutionTimeTrackingEsThreadPoolExecutor extends EsThrea
         ALLOCATION,
     }
 
-    private final UtilizationTracker apmUtilizationTracker = new UtilizationTracker();
-    private final UtilizationTracker allocationUtilizationTracker = new UtilizationTracker();
+    private final ThreadUtilizationTracker apmUtilizationTracker;
+    private final ThreadUtilizationTracker allocationUtilizationTracker;
 
     TaskExecutionTimeTrackingEsThreadPoolExecutor(
         String name,
@@ -103,6 +104,8 @@ public final class TaskExecutionTimeTrackingEsThreadPoolExecutor extends EsThrea
             : null;
         this.trackOngoingTasks = trackingConfig.trackOngoingTasks();
         this.trackMaxQueueLatency = trackingConfig.trackMaxQueueLatency();
+        this.apmUtilizationTracker = new ThreadUtilizationTracker(() -> System.nanoTime(), totalExecutionTime, getMaximumPoolSize());
+        this.allocationUtilizationTracker = new ThreadUtilizationTracker(() -> System.nanoTime(), totalExecutionTime, getMaximumPoolSize());
     }
 
     public List<Instrument> setupMetrics(MeterRegistry meterRegistry, String threadPoolName) {
@@ -348,32 +351,5 @@ public final class TaskExecutionTimeTrackingEsThreadPoolExecutor extends EsThrea
     // Used for testing
     public boolean trackingMaxQueueLatency() {
         return trackMaxQueueLatency;
-    }
-
-    /**
-     * Supports periodic polling for thread pool utilization. Tracks state since the last polling request so that the average utilization
-     * since the last poll can be calculated for the next polling request.
-     *
-     * Uses the difference of {@link #totalExecutionTime} since the last polling request to determine how much activity has occurred.
-     */
-    private class UtilizationTracker {
-        long lastPollTime = System.nanoTime();
-        long lastTotalExecutionTime = 0;
-
-        public synchronized double pollUtilization() {
-            final long currentTotalExecutionTimeNanos = totalExecutionTime.sum();
-            final long currentPollTimeNanos = System.nanoTime();
-
-            final long totalExecutionTimeSinceLastPollNanos = currentTotalExecutionTimeNanos - lastTotalExecutionTime;
-            final long timeSinceLastPoll = currentPollTimeNanos - lastPollTime;
-
-            final long maximumExecutionTimeSinceLastPollNanos = timeSinceLastPoll * getMaximumPoolSize();
-            final double utilizationSinceLastPoll = (double) totalExecutionTimeSinceLastPollNanos / maximumExecutionTimeSinceLastPollNanos;
-
-            lastTotalExecutionTime = currentTotalExecutionTimeNanos;
-            lastPollTime = currentPollTimeNanos;
-
-            return utilizationSinceLastPoll;
-        }
     }
 }

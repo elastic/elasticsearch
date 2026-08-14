163206f144251770cf9035d9cc9da74cb65515cc
Kostas Krikellas <131142368+kkrik-es@users.noreply.github.com>
2026-08-14 17:05:38 +0300

    Update LuceneQueryEvaluator.java

--- x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/lucene/query/LuceneQueryEvaluator.java
+++ x-pack/plugin/esql/compute/src/main/java/org/elasticsearch/compute/lucene/query/LuceneQueryEvaluator.java
@@ -55,9 +55,7 @@ public abstract class LuceneQueryEvaluator<T extends Block.Builder> implements R
     private final LongObjectPagedHashMap<ShardState> perShardState;
 
     protected LuceneQueryEvaluator(BlockFactory blockFactory, IndexedByShardId<ShardConfig> shards) {
-        if (shards == null || shards.isEmpty()) {
-            throw new IllegalStateException("LuceneQueryEvaluator requires shard information");
-        }
+        assert shards != null && shards.isEmpty() == false : "LuceneQueryEvaluator requires shard information";
         this.blockFactory = blockFactory;
         this.shards = shards;
         this.perShardState = new LongObjectPagedHashMap<>(10, blockFactory.bigArrays());

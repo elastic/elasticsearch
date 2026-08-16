f9af694a4f7095a7a0d8c3a1197eb0af8791392c
Eugene Rizhkov <reugpro@gmail.com>
2026-08-16 17:33:38 +0300

    Backport of #122593 to `8.19`. (#156875)

--- server/src/internalClusterTest/java/org/elasticsearch/search/basic/SearchWithRandomDisconnectsIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/search/basic/SearchWithRandomDisconnectsIT.java
@@ -15,6 +15,7 @@ import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.concurrent.ListenableFuture;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.discovery.AbstractDisruptionTestCase;
 import org.elasticsearch.index.IndexModule;
@@ -71,11 +72,15 @@ public class SearchWithRandomDisconnectsIT extends AbstractDisruptionTestCase {
                     }
 
                     private void runMoreSearches() {
-                        if (done.get() == false) {
-                            prepareRandomSearch().execute(this);
-                        } else {
-                            finishFuture.onResponse(null);
+                        while (done.get() == false) {
+                            final ListenableFuture<SearchResponse> f = new ListenableFuture<>();
+                            prepareRandomSearch().execute(f);
+                            if (f.isDone() == false) {
+                                f.addListener(this);
+                                return;
+                            }
                         }
+                        finishFuture.onResponse(null);
                     }
                 });
             }

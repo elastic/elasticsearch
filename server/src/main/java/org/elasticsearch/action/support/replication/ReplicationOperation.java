013e01cf214a9dd56d42b6ea133d481e63e4c439
Albert Zaharovits <email+github@zalbert.me>
2026-08-18 15:57:50 +0300

    WIP

--- server/src/main/java/org/elasticsearch/action/support/replication/ReplicationOperation.java
+++ server/src/main/java/org/elasticsearch/action/support/replication/ReplicationOperation.java
@@ -151,6 +151,14 @@ public class ReplicationOperation<
             if (logger.isTraceEnabled()) {
                 logger.trace("[{}] op [{}] completed on primary for request [{}]", primary.routingEntry().shardId(), opType, request);
             }
+            // we have to make sure to get the global checkpoint before the replication group, to ensure that the global checkpoint
+            // is valid for the replication group. If we sample in the reverse, the global checkpoint might be based on a subset
+            // of the sampled replication group, and advanced further than what the given replication group would allow it to.
+            // This would entail that some shards could learn about a global checkpoint that would be higher than its local checkpoint.
+            final long globalCheckpoint = primary.computedGlobalCheckpoint();
+            // we have to get the replication group after successfully indexing into the primary in order to honour recovery semantics.
+            // we have to make sure that every operation indexed into the primary after recovery start will also be replicated
+            // to the recovery target. If we used an old replication group, we may miss a recovery that has started since then.
             final ReplicationGroup replicationGroup = primary.getReplicationGroup();
 
             ActionListener.run(pendingActionsListener.acquire(), primaryOperationPendingActionListener -> {
@@ -172,15 +180,6 @@ public class ReplicationOperation<
                     })
                 );
             });
-
-            // we have to get the replication group after successfully indexing into the primary in order to honour recovery semantics.
-            // we have to make sure that every operation indexed into the primary after recovery start will also be replicated
-            // to the recovery target. If we used an old replication group, we may miss a recovery that has started since then.
-            // we also have to make sure to get the global checkpoint before the replication group, to ensure that the global checkpoint
-            // is valid for this replication group. If we would sample in the reverse, the global checkpoint might be based on a subset
-            // of the sampled replication group, and advanced further than what the given replication group would allow it to.
-            // This would entail that some shards could learn about a global checkpoint that would be higher than its local checkpoint.
-            final long globalCheckpoint = primary.computedGlobalCheckpoint();
             // we have to capture the max_seq_no_of_updates after this request was completed on the primary to make sure the value of
             // max_seq_no_of_updates on replica when this request is executed is at least the value on the primary when it was executed
             // on.

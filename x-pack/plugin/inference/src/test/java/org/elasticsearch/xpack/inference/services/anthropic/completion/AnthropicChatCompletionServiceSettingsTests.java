376d155a67ced1b3455b7eb80395898ef4ae6b48
Akbar Gumbira <akbar.gumbira@elastic.co>
2026-08-18 11:39:17 +0100

    Update Anthropic service setting to use ObjectParser.

--- x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/anthropic/completion/AnthropicChatCompletionServiceSettingsTests.java
+++ x-pack/plugin/inference/src/test/java/org/elasticsearch/xpack/inference/services/anthropic/completion/AnthropicChatCompletionServiceSettingsTests.java
@@ -9,11 +9,12 @@ package org.elasticsearch.xpack.inference.services.anthropic.completion;
 
 import org.elasticsearch.TransportVersion;
 import org.elasticsearch.common.Strings;
-import org.elasticsearch.common.ValidationException;
 import org.elasticsearch.common.io.stream.Writeable;
 import org.elasticsearch.core.Nullable;
+import org.elasticsearch.inference.ModelConfigurations;
 import org.elasticsearch.xcontent.XContentBuilder;
 import org.elasticsearch.xcontent.XContentFactory;
+import org.elasticsearch.xcontent.XContentParseException;
 import org.elasticsearch.xcontent.XContentType;
 import org.elasticsearch.xpack.core.ml.AbstractBWCWireSerializationTestCase;
 import org.elasticsearch.xpack.inference.services.ConfigurationParseContext;
@@ -24,10 +25,11 @@ import org.elasticsearch.xpack.inference.services.settings.RateLimitSettingsTest
 import java.io.IOException;
 import java.net.URI;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.elasticsearch.xpack.inference.services.ServiceUtils.createUri;
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.is;
 
 public class AnthropicChatCompletionServiceSettingsTests extends AbstractBWCWireSerializationTestCase<
@@ -36,19 +38,24 @@ public class AnthropicChatCompletionServiceSettingsTests extends AbstractBWCWire
     private static final String INITIAL_TEST_MODEL_ID = "initial-test-model-id";
     private static final int TEST_RATE_LIMIT = 20;
     private static final int INITIAL_TEST_RATE_LIMIT = 30;
+    private static final int DEFAULT_RATE_LIMIT = 50;
 
     private static final String TEST_URL = "https://custom.anthropic.example.com/v1/messages";
 
-    public void testUpdateServiceSettings_AllFields_OnlyMutableFieldsAreUpdated() {
+    public void testUpdateServiceSettings_OnlyRateLimit_IsUpdated() {
+        var url = URI.create(TEST_URL);
         var originalServiceSettings = new AnthropicChatCompletionServiceSettings(
             INITIAL_TEST_MODEL_ID,
+            url,
             new RateLimitSettings(INITIAL_TEST_RATE_LIMIT)
         );
-        var updatedServiceSettings = originalServiceSettings.updateServiceSettings(getServiceSettingsMap(TEST_MODEL_ID, TEST_RATE_LIMIT));
+        var updatedServiceSettings = originalServiceSettings.updateServiceSettings(
+            new HashMap<>(Map.of(RateLimitSettings.FIELD_NAME, Map.of(RateLimitSettings.REQUESTS_PER_MINUTE_FIELD, TEST_RATE_LIMIT)))
+        );
 
         assertThat(
             updatedServiceSettings,
-            is(new AnthropicChatCompletionServiceSettings(INITIAL_TEST_MODEL_ID, new RateLimitSettings(TEST_RATE_LIMIT)))
+            is(new AnthropicChatCompletionServiceSettings(INITIAL_TEST_MODEL_ID, url, new RateLimitSettings(TEST_RATE_LIMIT)))
         );
     }
 
@@ -62,48 +69,66 @@ public class AnthropicChatCompletionServiceSettingsTests extends AbstractBWCWire
         assertThat(updatedServiceSettings, is(originalServiceSettings));
     }
 
-    public void testUpdateServiceSettings_PreservesUrl() {
-        var url = URI.create(TEST_URL);
+    public void testUpdateServiceSettings_RateLimitEmptyObject_ResetsToDefault() {
         var originalServiceSettings = new AnthropicChatCompletionServiceSettings(
             INITIAL_TEST_MODEL_ID,
-            url,
-            new RateLimitSettings(INITIAL_TEST_RATE_LIMIT)
-        );
-        var updatedServiceSettings = originalServiceSettings.updateServiceSettings(getServiceSettingsMap(TEST_MODEL_ID, TEST_RATE_LIMIT));
-
-        assertThat(
-            updatedServiceSettings,
-            is(new AnthropicChatCompletionServiceSettings(INITIAL_TEST_MODEL_ID, url, new RateLimitSettings(TEST_RATE_LIMIT)))
-        );
-    }
-
-    public void testUpdateServiceSettings_UrlCannotBeChanged() {
-        var originalUrl = URI.create(TEST_URL);
-        var originalServiceSettings = new AnthropicChatCompletionServiceSettings(
-            INITIAL_TEST_MODEL_ID,
-            originalUrl,
             new RateLimitSettings(INITIAL_TEST_RATE_LIMIT)
         );
         var updatedServiceSettings = originalServiceSettings.updateServiceSettings(
-            getServiceSettingsMap(TEST_MODEL_ID, TEST_RATE_LIMIT, "https://other.example.com/v1/messages")
+            new HashMap<>(Map.of(RateLimitSettings.FIELD_NAME, new HashMap<>()))
         );
 
         assertThat(
             updatedServiceSettings,
-            is(new AnthropicChatCompletionServiceSettings(INITIAL_TEST_MODEL_ID, originalUrl, new RateLimitSettings(TEST_RATE_LIMIT)))
+            is(new AnthropicChatCompletionServiceSettings(INITIAL_TEST_MODEL_ID, new RateLimitSettings(DEFAULT_RATE_LIMIT)))
         );
+    }
+
+    public void testUpdateServiceSettings_RateLimitNull_ResetsToDefault() {
+        var settingsMap = new HashMap<String, Object>();
+        settingsMap.put(RateLimitSettings.FIELD_NAME, null);
+        var originalServiceSettings = new AnthropicChatCompletionServiceSettings(
+            INITIAL_TEST_MODEL_ID,
+            new RateLimitSettings(INITIAL_TEST_RATE_LIMIT)
+        );
+
+        assertThat(
+            originalServiceSettings.updateServiceSettings(settingsMap),
+            is(new AnthropicChatCompletionServiceSettings(INITIAL_TEST_MODEL_ID, new RateLimitSettings(DEFAULT_RATE_LIMIT)))
+        );
+    }
+
+    public void testUpdateServiceSettings_RateLimitAbsent_KeepsExisting() {
+        var originalServiceSettings = new AnthropicChatCompletionServiceSettings(
+            INITIAL_TEST_MODEL_ID,
+            new RateLimitSettings(INITIAL_TEST_RATE_LIMIT)
+        );
+
+        var updatedServiceSettings = originalServiceSettings.updateServiceSettings(new HashMap<>());
+
+        assertThat(updatedServiceSettings.rateLimitSettings(), is(new RateLimitSettings(INITIAL_TEST_RATE_LIMIT)));
+    }
+
+    public void testUpdateServiceSettings_GivenImmutableFields_ThrowsException() {
+        var serviceSettings = new AnthropicChatCompletionServiceSettings(
+            INITIAL_TEST_MODEL_ID,
+            URI.create(TEST_URL),
+            new RateLimitSettings(INITIAL_TEST_RATE_LIMIT)
+        );
+
+        for (String immutableField : List.of(ServiceFields.MODEL_ID, ServiceFields.URL)) {
+            var e = expectThrows(
+                XContentParseException.class,
+                () -> serviceSettings.updateServiceSettings(new HashMap<>(Map.of(immutableField, "value")))
+            );
+            assertThat(
+                e.getMessage(),
+                endsWith(Strings.format("[%s] unknown field [%s]", ModelConfigurations.SERVICE_SETTINGS, immutableField))
+            );
+        }
     }
 
     public void testFromMap_AllFields_Success() {
-        var serviceSettings = AnthropicChatCompletionServiceSettings.fromMap(
-            getServiceSettingsMap(TEST_MODEL_ID, TEST_RATE_LIMIT),
-            ConfigurationParseContext.PERSISTENT
-        );
-
-        assertThat(serviceSettings, is(new AnthropicChatCompletionServiceSettings(TEST_MODEL_ID, new RateLimitSettings(TEST_RATE_LIMIT))));
-    }
-
-    public void testFromMap_WithUrl_Success() {
         var map = getServiceSettingsMap(TEST_MODEL_ID, TEST_RATE_LIMIT);
         map.put(ServiceFields.URL, TEST_URL);
 
@@ -115,9 +140,21 @@ public class AnthropicChatCompletionServiceSettingsTests extends AbstractBWCWire
         );
     }
 
-    public void testFromMap_MissingModelId_ThrowsException() {
+    public void testFromMap_OnlyMandatoryFields_UsesDefaultValues_Success() {
+        var serviceSettings = AnthropicChatCompletionServiceSettings.fromMap(
+            getServiceSettingsMap(TEST_MODEL_ID, null),
+            ConfigurationParseContext.PERSISTENT
+        );
+
+        assertThat(
+            serviceSettings,
+            is(new AnthropicChatCompletionServiceSettings(TEST_MODEL_ID, new RateLimitSettings(DEFAULT_RATE_LIMIT)))
+        );
+    }
+
+    public void testFromMap_MissingModelId_Failure() {
         var thrownException = expectThrows(
-            ValidationException.class,
+            IllegalArgumentException.class,
             () -> AnthropicChatCompletionServiceSettings.fromMap(
                 getServiceSettingsMap(null, TEST_RATE_LIMIT),
                 ConfigurationParseContext.PERSISTENT
@@ -126,17 +163,14 @@ public class AnthropicChatCompletionServiceSettingsTests extends AbstractBWCWire
 
         assertThat(
             thrownException.getMessage(),
-            containsString("Validation Failed: 1: [service_settings] does not contain the required setting [model_id];")
+            is(
+                Strings.format(
+                    "[%s] does not contain the required setting [%s]",
+                    ModelConfigurations.SERVICE_SETTINGS,
+                    ServiceFields.MODEL_ID
+                )
+            )
         );
-    }
-
-    public void testFromMap_MissingRateLimit_Success() {
-        var serviceSettings = AnthropicChatCompletionServiceSettings.fromMap(
-            getServiceSettingsMap(TEST_MODEL_ID, null),
-            ConfigurationParseContext.PERSISTENT
-        );
-
-        assertThat(serviceSettings, is(new AnthropicChatCompletionServiceSettings(TEST_MODEL_ID, null)));
     }
 
     public void testToXContent_WritesAllValues() throws IOException {
@@ -147,7 +181,7 @@ public class AnthropicChatCompletionServiceSettingsTests extends AbstractBWCWire
         String xContentResult = Strings.toString(builder);
 
         assertThat(xContentResult, is(Strings.format("""
-            {"model_id":"%s","rate_limit":{"requests_per_minute":50}}""", TEST_MODEL_ID)));
+            {"model_id":"%s","rate_limit":{"requests_per_minute":%d}}""", TEST_MODEL_ID, DEFAULT_RATE_LIMIT)));
     }
 
     public void testToXContent_WritesAllValues_WithCustomRateLimit() throws IOException {

ad9f525b2b791704da33f250deded73dfaa599cf
David Kyle <david.kyle@elastic.co>
2026-08-18 17:56:14 +0100

    Convert Enum.valueOf uses

--- server/src/test/java/org/elasticsearch/index/VectordbDocumentIndexModeTests.java
+++ server/src/test/java/org/elasticsearch/index/VectordbDocumentIndexModeTests.java
@@ -107,7 +107,6 @@ public class VectordbDocumentIndexModeTests extends ESTestCase {
         Settings userSettings = Settings.builder().put(IndexSettings.MODE.getKey(), "vectordb").build();
         IllegalArgumentException e = expectThrows(IllegalArgumentException.class, () -> runProvider(userSettings, Settings.builder()));
         assertThat(e.getMessage(), containsString("[vectordb] is an invalid index mode"));
-        assertThat(e.getMessage(), not(containsString("No enum constant")));
     }
 
     private static void runProvider(Settings userSettings, Settings.Builder additional) {

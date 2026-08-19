d769d08b090c357cc161d8adc123585422e35db4
David Turner <david.turner@elastic.co>
2026-08-19 12:33:49 +0100

    Migrate `snapshot-repo-test-kit` legacy tests
    The `snapshot-repo-test-kit:qa:rest` suite was using the legacy test
    clusters setup and also mixed up YAML tests with tests of the FS
    repository type.
    
    With this commit we migrate these tests to non-legacy infrastructure and
    move them to more appropriate places in the tree.

--- x-pack/plugin/snapshot-repo-test-kit/src/yamlRestTest/java/org/elasticsearch/repositories/blobstore/testkit/rest/SnapshotRepoTestKitClientYamlTestSuiteIT.java
+++ x-pack/plugin/snapshot-repo-test-kit/src/yamlRestTest/java/org/elasticsearch/repositories/blobstore/testkit/rest/SnapshotRepoTestKitClientYamlTestSuiteIT.java
@@ -9,13 +9,34 @@ package org.elasticsearch.repositories.blobstore.testkit.rest;
 
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 
+import org.elasticsearch.test.cluster.ElasticsearchCluster;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
 import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
+import org.junit.ClassRule;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestRule;
 
 public class SnapshotRepoTestKitClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
 
+    private static final TemporaryFolder repoDirectory = new TemporaryFolder();
+
+    private static final ElasticsearchCluster cluster = ElasticsearchCluster.local()
+        .plugin("x-pack-snapshot-repo-test-kit")
+        .setting("path.repo", () -> repoDirectory.getRoot().getPath())
+        .setting("xpack.security.enabled", "false")
+        .build();
+
+    @ClassRule
+    public static TestRule ruleChain = RuleChain.outerRule(repoDirectory).around(cluster);
+
     public SnapshotRepoTestKitClientYamlTestSuiteIT(final ClientYamlTestCandidate testCandidate) {
         super(testCandidate);
+    }
+
+    @Override
+    protected String getTestRestCluster() {
+        return cluster.getHttpAddresses();
     }
 
     @ParametersFactory

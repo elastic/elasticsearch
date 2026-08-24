e0284c4f8c61368092240d44f3873932835fcc20
Sean Handley <sean.handley@elastic.co>
2026-08-24 15:40:30 +0100

    Simplify SettingsScope: derive toString() from name() instead of storing a value

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/SettingsScope.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/SettingsScope.java
@@ -7,25 +7,21 @@
 
 package org.elasticsearch.xpack.inference.services;
 
+import java.util.Locale;
+
 /**
  * Represents the scope of a settings block, used to produce structured error messages such as
  * {@code [service_settings] does not contain the required setting [model_id]}.
  */
 public enum SettingsScope {
-    SERVICE_SETTINGS("service_settings"),
-    TASK_SETTINGS("task_settings"),
-    SECRET_SETTINGS("secret_settings"),
-    INPUT_TYPE_TRANSLATOR("input_type_translator"),
-    RATE_LIMIT("rate_limit");
-
-    private final String value;
-
-    SettingsScope(String value) {
-        this.value = value;
-    }
+    SERVICE_SETTINGS,
+    TASK_SETTINGS,
+    SECRET_SETTINGS,
+    INPUT_TYPE_TRANSLATOR,
+    RATE_LIMIT;
 
     @Override
     public String toString() {
-        return value;
+        return name().toLowerCase(Locale.ROOT);
     }
 }

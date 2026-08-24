aa2ddfa43ec3ce6a34d57d13cb87263d33972e42
Rene Groeschke <rene@elastic.co>
2026-08-24 23:10:22 +0200

    Migrate searchable-snapshots qa:rest to JUnit rule (#157414)
    * Migrate searchable-snapshots qa:rest to JUnit rule
    
    Move the searchable-snapshots REST qa project off the legacy
    Gradle testClusters framework onto the JUnit rule-based
    ElasticsearchCluster. The Java and YAML rest suites now declare
    their cluster via @ClassRule, with path.repo backed by a
    TemporaryFolder, and the project is dropped from the legacy
    RestrictedBuildApiService whitelist.
    
    * Update x-pack/plugin/searchable-snapshots/qa/rest/src/yamlRestTest/java/org/elasticsearch/xpack/searchablesnapshots/rest/SearchableSnapshotsClientYamlTestSuiteIT.java
    
    Co-authored-by: Mateusz Rzeszutek <rzeszut@pm.me>
    
    * Update x-pack/plugin/searchable-snapshots/qa/rest/src/javaRestTest/java/org/elasticsearch/xpack/searchablesnapshots/rest/FsSearchableSnapshotsIT.java
    
    Co-authored-by: Mateusz Rzeszutek <rzeszut@pm.me>
    
    ---------
    
    Co-authored-by: Mateusz Rzeszutek <rzeszut@pm.me>
    (cherry picked from commit 5fe94de148de80994142f99d7cac0f864a67e34e)
    
    # Conflicts:
    #	build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/RestrictedBuildApiService.java

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/RestrictedBuildApiService.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/RestrictedBuildApiService.java
@@ -28,7 +28,6 @@ public abstract class RestrictedBuildApiService implements BuildService<Restrict
     private static ListMultimap<Class<?>, String> createLegacyRestTestBasePluginUsage() {
         ListMultimap<Class<?>, String> map = ArrayListMultimap.create(1, 200);
         // Projects that apply LegacyRestTestBasePlugin directly via the legacy-yaml-rest-test or legacy-java-rest-test plugins.
-        map.put(LegacyRestTestBasePlugin.class, ":x-pack:plugin:searchable-snapshots:qa:rest");
         map.put(LegacyRestTestBasePlugin.class, ":x-pack:plugin:snapshot-repo-test-kit:qa:rest");
         map.put(LegacyRestTestBasePlugin.class, ":x-pack:plugin:transform:qa:multi-node-tests");
         map.put(LegacyRestTestBasePlugin.class, ":x-pack:plugin:eql:qa:mixed-node");

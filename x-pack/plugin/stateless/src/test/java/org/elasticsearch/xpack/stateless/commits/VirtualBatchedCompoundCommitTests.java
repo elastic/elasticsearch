790bc5fd48366ae5decedf233e5899c869bb536f
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-08-24 22:45:26 +0000

    [CI] Auto commit changes from spotless

--- x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/commits/VirtualBatchedCompoundCommitTests.java
+++ x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/commits/VirtualBatchedCompoundCommitTests.java
@@ -32,7 +32,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
 

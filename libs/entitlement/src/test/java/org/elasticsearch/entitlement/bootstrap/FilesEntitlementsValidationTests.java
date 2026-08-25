f30ce8ebdf5b49c34e4ec234c6e80265805b3743
Lorenzo Dematte <lorenzo.dematte@elastic.co>
2026-08-25 14:06:07 +0200

    Cleanup

--- libs/entitlement/src/test/java/org/elasticsearch/entitlement/bootstrap/FilesEntitlementsValidationTests.java
+++ libs/entitlement/src/test/java/org/elasticsearch/entitlement/bootstrap/FilesEntitlementsValidationTests.java
@@ -35,7 +35,6 @@ public class FilesEntitlementsValidationTests extends ESTestCase {
 
     private static Path TEST_PLUGINS_DIR;
     private static Path TEST_LIBS_DIR;
-    private static Path TEST_NATIVE_LIBS_DIR;
 
     @BeforeClass
     public static void beforeClass() {
@@ -44,7 +43,6 @@ public class FilesEntitlementsValidationTests extends ESTestCase {
             TEST_CONFIG_DIR = testBaseDir.resolve("config");
             TEST_PLUGINS_DIR = testBaseDir.resolve("plugins");
             TEST_LIBS_DIR = testBaseDir.resolve("libs");
-            TEST_NATIVE_LIBS_DIR = testBaseDir.resolve("libs/native");
 
             TEST_PATH_LOOKUP = new PathLookupImpl(
                 testBaseDir.resolve("user/home"),

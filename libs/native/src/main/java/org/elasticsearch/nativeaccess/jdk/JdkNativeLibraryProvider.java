656b057c0b24a7fef326b7dd713cc634442279e2
Ryan Ernst <ryan@iernst.net>
2026-08-26 08:34:11 -0700

    Remove JavaLibrary and its jdk implementation
    JavaLibrary was a single-method interface wrapping FileChannel.map().
    Each concrete NativeAccess subclass (Posix, Windows, Noop) already
    implements map() directly, making the delegation in AbstractNativeAccess
    dead code. Remove the interface, its JDK implementation, the delegation
    in AbstractNativeAccess, and the registration in JdkNativeLibraryProvider.

--- libs/native/src/main/java/org/elasticsearch/nativeaccess/jdk/JdkNativeLibraryProvider.java
+++ libs/native/src/main/java/org/elasticsearch/nativeaccess/jdk/JdkNativeLibraryProvider.java
@@ -9,7 +9,6 @@
 
 package org.elasticsearch.nativeaccess.jdk;
 
-import org.elasticsearch.nativeaccess.lib.JavaLibrary;
 import org.elasticsearch.nativeaccess.lib.NativeLibraryProvider;
 import org.elasticsearch.nativeaccess.lib.ParquetRsLibrary;
 
@@ -18,6 +17,6 @@ import java.util.Map;
 public class JdkNativeLibraryProvider extends NativeLibraryProvider {
 
     public JdkNativeLibraryProvider() {
-        super("jdk", Map.of(JavaLibrary.class, JdkJavaLibrary::new, ParquetRsLibrary.class, JdkParquetRsLibrary::new));
+        super("jdk", Map.of(ParquetRsLibrary.class, JdkParquetRsLibrary::new));
     }
 }

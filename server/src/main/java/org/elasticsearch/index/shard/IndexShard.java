7428cd620d197f6ef3af1a6b2840f3800aa485d8
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2026-08-28 10:00:03 -0700

    fix

--- server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
@@ -1421,7 +1421,11 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
     /// specifically in `ShardGetService`.
     public Engine.GetResult getForUpdate(Engine.Get get) {
         assert get.realtime() && get.isReadFromTranslog();
-        assert ThreadPool.assertCurrentThreadPool(ThreadPool.Names.WRITE, ThreadPool.Names.SYSTEM_WRITE);
+        assert ThreadPool.assertCurrentThreadPool(
+            ThreadPool.Names.WRITE,
+            ThreadPool.Names.SYSTEM_WRITE,
+            ThreadPool.Names.SYSTEM_CRITICAL_WRITE
+        );
 
         readAllowed();
         MappingLookup mappingLookup = mapperService.mappingLookup();

8ec3282dcc84fe6ec5553a7819bac55e80727806
ChrisHegarty <chegar999@gmail.com>
2026-08-28 14:15:37 +0100

    fix build

--- libs/simdjson/src/test/java/org/elasticsearch/simdjson/internal/SimdJsonLibraryTests.java
+++ libs/simdjson/src/test/java/org/elasticsearch/simdjson/internal/SimdJsonLibraryTests.java
@@ -12,7 +12,7 @@ package org.elasticsearch.simdjson.internal;
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 
 import org.elasticsearch.foreign.adapter.MemorySegmentAdapter;
-import org.elasticsearch.nativeaccess.GuardPageAllocator;
+import org.elasticsearch.simdvec.GuardPageAllocator;
 import org.elasticsearch.simdjson.SimdJsonSupport;
 import org.elasticsearch.test.ESTestCase;
 import org.junit.Before;

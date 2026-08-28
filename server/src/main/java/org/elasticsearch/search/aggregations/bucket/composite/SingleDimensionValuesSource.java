ffdbcd41b1e1eb43dda2a55854564d0b7289297c
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-08-28 17:07:05 +0000

    [CI] Auto commit changes from spotless

--- server/src/main/java/org/elasticsearch/search/aggregations/bucket/composite/SingleDimensionValuesSource.java
+++ server/src/main/java/org/elasticsearch/search/aggregations/bucket/composite/SingleDimensionValuesSource.java
@@ -144,8 +144,8 @@ abstract class SingleDimensionValuesSource<T extends Comparable<T>> implements R
      * Returns true if a {@link SortedDocsProducer} should be used to optimize the execution.
      */
     // TODO: Implementing mayDynamicallyPrune(IndexReader)/getUniqueValueCount() on other subclasses than just GlobalOrdinalValuesSource
-    //  would let CompositeAggregator#getLeafCollector return inner.competitiveIterator() for numeric and date sources too.
-    //  This matters for logsdb and tsdb, where @timestamp is always skipper backed.
+    // would let CompositeAggregator#getLeafCollector return inner.competitiveIterator() for numeric and date sources too.
+    // This matters for logsdb and tsdb, where @timestamp is always skipper backed.
     protected boolean checkIfSortedDocsIsApplicable(IndexReader reader, MappedFieldType fieldType) {
         if (fieldType == null || (missingBucket && afterValue == null) || fieldType.indexType().hasDenseIndex() == false ||
         // inverse of the natural order

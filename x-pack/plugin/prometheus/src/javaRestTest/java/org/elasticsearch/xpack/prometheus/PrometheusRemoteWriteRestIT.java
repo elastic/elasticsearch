d73a0d853a4f6723f67196ae9744b0b1f5bea3db
Johannes Freden <johannes.freden@elastic.co>
2026-09-01 10:59:17 +0200

    fixup! Use static setting for xpack.security.audit.enabled

--- x-pack/plugin/prometheus/src/javaRestTest/java/org/elasticsearch/xpack/prometheus/PrometheusRemoteWriteRestIT.java
+++ x-pack/plugin/prometheus/src/javaRestTest/java/org/elasticsearch/xpack/prometheus/PrometheusRemoteWriteRestIT.java
@@ -301,7 +301,6 @@ public class PrometheusRemoteWriteRestIT extends AbstractPrometheusRestIT {
         request.setJsonEntity("""
             {
               "persistent": {
-                "xpack.security.audit.enabled": true,
                 "xpack.security.audit.logfile.events.emit_request_body": true,
                 "xpack.security.audit.logfile.events.include": ["authentication_success"]
               }

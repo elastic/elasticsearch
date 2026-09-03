f9591c2171cc5faa3b672359b708c3b28b934aa2
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-09-03 12:52:26 +0000

    [CI] Auto commit changes from spotless

--- server/src/test/java/org/elasticsearch/cluster/routing/allocation/shards/StatefulShardsAvailabilityHealthIndicatorServiceTests.java
+++ server/src/test/java/org/elasticsearch/cluster/routing/allocation/shards/StatefulShardsAvailabilityHealthIndicatorServiceTests.java
@@ -85,7 +85,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;

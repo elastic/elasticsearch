a7f208b1763eb4604a15875f7aa9a47c6a671b97
Chris Parrinello <chris.parrinello@elastic.co>
2026-09-04 08:55:52 -0500

    add accountable interface to ProjectId

--- server/src/main/java/org/elasticsearch/cluster/metadata/ProjectMetadata.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/ProjectMetadata.java
@@ -222,7 +222,7 @@ public class ProjectMetadata implements Iterable<IndexMetadata>, Diffable<Projec
     @Override
     public long ramBytesUsed() {
         long size = BASE_RAM_BYTES_USED;
-        size += RamUsageEstimator.shallowSizeOf(id);
+        size += RamUsageEstimator.sizeOfObject(id);
         size += RamUsageEstimator.shallowSizeOf(oldestIndexVersion);
         size += MetadataRamEstimators.ramBytesUsedByIndexMetadataMap(indices);
         size += MetadataRamEstimators.ramBytesUsedByAccountableMap(templates);

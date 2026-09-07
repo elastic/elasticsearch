599d902dcf777e74764c4790e750981e6b5bd5f2
Joshua Adams <joshua.adams@elastic.co>
2026-09-07 11:37:11 +0100

    Extends ShardsCapacityHealthIndicatorServiceStatelessTests
    Extends the test coverage within ShardsCapacityHealthIndicatorServiceStatelessTests to assert on the impact and diagnosis fields
    
    Relates: https://github.com/elastic/elasticsearch-team/issues/4609

--- server/src/test/java/org/elasticsearch/health/node/ShardsCapacityHealthIndicatorServiceStatelessTests.java
+++ server/src/test/java/org/elasticsearch/health/node/ShardsCapacityHealthIndicatorServiceStatelessTests.java
@@ -21,6 +21,7 @@ import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.LoggingDeprecationHandler;
+import org.elasticsearch.health.HealthIndicatorDetails;
 import org.elasticsearch.health.HealthStatus;
 import org.elasticsearch.health.metadata.HealthMetadata;
 import org.elasticsearch.index.IndexVersion;
@@ -45,7 +46,12 @@ import java.util.Set;
 import static org.elasticsearch.cluster.metadata.IndexMetadata.SETTING_CREATION_DATE;
 import static org.elasticsearch.health.HealthStatus.RED;
 import static org.elasticsearch.health.HealthStatus.YELLOW;
+import static org.elasticsearch.health.node.ShardsCapacityHealthIndicatorService.RED_INDICATOR_IMPACTS;
+import static org.elasticsearch.health.node.ShardsCapacityHealthIndicatorService.SHARDS_MAX_CAPACITY_REACHED_DATA_NODES;
+import static org.elasticsearch.health.node.ShardsCapacityHealthIndicatorService.YELLOW_INDICATOR_IMPACTS;
 import static org.elasticsearch.indices.ShardLimitValidator.SETTING_CLUSTER_MAX_SHARDS_PER_NODE;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 public class ShardsCapacityHealthIndicatorServiceStatelessTests extends ESTestCase {
@@ -57,12 +63,12 @@ public class ShardsCapacityHealthIndicatorServiceStatelessTests extends ESTestCa
     private ClusterService clusterService;
 
     @Before
-    public void startClusterService() throws Exception {
+    public void startClusterService() {
         clusterService = ClusterServiceUtils.createClusterService(threadPool, Settings.builder().put("stateless.enabled", true).build());
     }
 
     @After
-    public void stopClusterService() throws Exception {
+    public void stopClusterService() {
         clusterService.close();
     }
 
@@ -98,46 +104,131 @@ public class ShardsCapacityHealthIndicatorServiceStatelessTests extends ESTestCa
         );
     }
 
-    public void testIndicatorYieldsColorInCaseThereIsNotEnoughRoom() {
-        final int maxShardsPerNode = randomValidMaxShards();
-        // Red health if there is no room for 5 more shards, so we take 4 out of the max
-        doTestIndicatorYieldsYellowInCaseThereIsNotEnoughRoom(RED, maxShardsPerNode, maxShardsPerNode - 4);
-        // Yellow health if there is no room for 10 more shards, so we take 9 out of the max
-        doTestIndicatorYieldsYellowInCaseThereIsNotEnoughRoom(YELLOW, maxShardsPerNode, maxShardsPerNode - 9);
+    public void testNoShardsCapacityMetadata() throws IOException {
+        int maxShardsPerNode = randomValidMaxShards();
+        var clusterService = createClusterService(
+            maxShardsPerNode,
+            1,
+            1,
+            new HealthMetadata(DISK_METADATA, null),
+            createIndex(100)
+        );
+        var indicatorResult = new ShardsCapacityHealthIndicatorService(clusterService).calculate(true, HealthInfo.EMPTY_HEALTH_INFO);
+
+        assertEquals(HealthStatus.UNKNOWN, indicatorResult.status());
+        assertTrue(indicatorResult.impacts().isEmpty());
+        assertTrue(indicatorResult.diagnosisList().isEmpty());
+        assertEquals("Unable to determine shard capacity status.", indicatorResult.symptom());
+        assertEquals(Map.of(), xContentToMap(indicatorResult.details()));
     }
 
-    public void doTestIndicatorYieldsYellowInCaseThereIsNotEnoughRoom(HealthStatus status, int maxShardsPerNode, int indexNumShards) {
+    public void testIndicatorYieldsYellowInCaseThereIsNotEnoughRoom() throws IOException {
+        int maxShardsPerNode = randomValidMaxShards();
+        // Yellow if there is no room for 10 more shards, so we take 9 out of the max
+        assertNotEnoughRoom(YELLOW, maxShardsPerNode, maxShardsPerNode - 9);
+    }
+
+    public void testIndicatorYieldsRedInCaseThereIsNotEnoughRoom() throws IOException {
+        int maxShardsPerNode = randomValidMaxShards();
+        // Red if there is no room for 5 more shards, so we take 4 out of the max
+        assertNotEnoughRoom(RED, maxShardsPerNode, maxShardsPerNode - 4);
+    }
+
+    public void testSkippingFieldsWhenVerboseIsFalse() {
+        int maxShardsPerNode = randomValidMaxShards();
+        var clusterService = createClusterService(maxShardsPerNode, 1, 1, createIndex(maxShardsPerNode - 4));
+        var indicatorResult = new ShardsCapacityHealthIndicatorService(clusterService).calculate(false, HealthInfo.EMPTY_HEALTH_INFO);
+
+        assertEquals(RED, indicatorResult.status());
+        assertEquals(
+            "Cluster is close to reaching the configured maximum number of shards for index and search nodes.",
+            indicatorResult.symptom()
+        );
+        assertThat(indicatorResult.impacts(), equalTo(RED_INDICATOR_IMPACTS));
+        assertThat(indicatorResult.diagnosisList(), hasSize(0));
+        assertThat(indicatorResult.details(), is(HealthIndicatorDetails.EMPTY));
+    }
+
+    private void assertNotEnoughRoom(HealthStatus status, int maxShardsPerNode, int indexNumShards) throws IOException {
+        var expectedImpacts = switch (status) {
+            case RED -> RED_INDICATOR_IMPACTS;
+            case YELLOW -> YELLOW_INDICATOR_IMPACTS;
+            default -> throw new AssertionError("expected RED or YELLOW but was " + status);
+        };
+
         {
-            // Only index does not have enough space
+            // Only index nodes do not have enough space
             var clusterService = createClusterService(maxShardsPerNode, 1, 2, createIndex(indexNumShards));
             var indicatorResult = new ShardsCapacityHealthIndicatorService(clusterService).calculate(true, HealthInfo.EMPTY_HEALTH_INFO);
 
-            assertEquals(indicatorResult.status(), status);
+            assertEquals(status, indicatorResult.status());
             assertEquals(
                 "Cluster is close to reaching the configured maximum number of shards for index nodes.",
                 indicatorResult.symptom()
             );
+            assertThat(indicatorResult.impacts(), equalTo(expectedImpacts));
+            assertThat(indicatorResult.diagnosisList(), hasSize(1));
+            assertThat(indicatorResult.diagnosisList().getFirst(), equalTo(SHARDS_MAX_CAPACITY_REACHED_DATA_NODES));
+            assertThat(
+                xContentToMap(indicatorResult.details()),
+                is(
+                    Map.of(
+                        "index",
+                        Map.of("max_shards_in_cluster", maxShardsPerNode, "current_used_shards", indexNumShards),
+                        "search",
+                        Map.of("max_shards_in_cluster", 2 * maxShardsPerNode)
+                    )
+                )
+            );
         }
         {
-            // Only search does not have enough space
+            // Only search nodes do not have enough space
             var clusterService = createClusterService(maxShardsPerNode, 2, 1, createIndex(indexNumShards));
             var indicatorResult = new ShardsCapacityHealthIndicatorService(clusterService).calculate(true, HealthInfo.EMPTY_HEALTH_INFO);
 
-            assertEquals(indicatorResult.status(), status);
+            assertEquals(status, indicatorResult.status());
             assertEquals(
                 "Cluster is close to reaching the configured maximum number of shards for search nodes.",
                 indicatorResult.symptom()
             );
+            assertThat(indicatorResult.impacts(), equalTo(expectedImpacts));
+            assertThat(indicatorResult.diagnosisList(), hasSize(1));
+            assertThat(indicatorResult.diagnosisList().getFirst(), equalTo(SHARDS_MAX_CAPACITY_REACHED_DATA_NODES));
+            assertThat(
+                xContentToMap(indicatorResult.details()),
+                is(
+                    Map.of(
+                        "index",
+                        Map.of("max_shards_in_cluster", 2 * maxShardsPerNode),
+                        "search",
+                        Map.of("max_shards_in_cluster", maxShardsPerNode, "current_used_shards", indexNumShards)
+                    )
+                )
+            );
         }
         {
-            // Both data and frozen nodes does not have enough space
+            // Both index and search nodes do not have enough space
             var clusterService = createClusterService(maxShardsPerNode, 1, 1, createIndex(indexNumShards));
             var indicatorResult = new ShardsCapacityHealthIndicatorService(clusterService).calculate(true, HealthInfo.EMPTY_HEALTH_INFO);
 
-            assertEquals(indicatorResult.status(), status);
+            assertEquals(status, indicatorResult.status());
             assertEquals(
                 "Cluster is close to reaching the configured maximum number of shards for index and search nodes.",
                 indicatorResult.symptom()
+            );
+            assertThat(indicatorResult.impacts(), equalTo(expectedImpacts));
+            assertThat(indicatorResult.diagnosisList(), hasSize(1));
+            assertThat(indicatorResult.diagnosisList().getFirst(), equalTo(SHARDS_MAX_CAPACITY_REACHED_DATA_NODES));
+            assertThat(
+                xContentToMap(indicatorResult.details()),
+                is(
+                    Map.of(
+                        "index",
+                        Map.of("max_shards_in_cluster", maxShardsPerNode, "current_used_shards", indexNumShards),
+                        "search",
+                        Map.of("max_shards_in_cluster", maxShardsPerNode, "current_used_shards", indexNumShards)
+                    )
+                )
             );
         }
     }
@@ -160,10 +251,26 @@ public class ShardsCapacityHealthIndicatorServiceStatelessTests extends ESTestCa
         int numSearchNodes,
         IndexMetadata.Builder... indexMetadata
     ) {
+        return createClusterService(
+            maxShardsPerNode,
+            numIndexNodes,
+            numSearchNodes,
+            new HealthMetadata(DISK_METADATA, new HealthMetadata.ShardLimits(maxShardsPerNode, 0, 10, 5)),
+            indexMetadata
+        );
+    }
+
+    private ClusterService createClusterService(
+        int maxShardsPerNode,
+        int numIndexNodes,
+        int numSearchNodes,
+        HealthMetadata healthMetadata,
+        IndexMetadata.Builder... indexMetadata
+    ) {
         final ClusterState clusterState = createClusterState(
             nodesWithIndexAndSearch(numIndexNodes, numSearchNodes),
             maxShardsPerNode,
-            new HealthMetadata(DISK_METADATA, new HealthMetadata.ShardLimits(maxShardsPerNode, 0, 10, 5)),
+            healthMetadata,
             indexMetadata
         );
         ClusterServiceUtils.setState(clusterService, clusterState);

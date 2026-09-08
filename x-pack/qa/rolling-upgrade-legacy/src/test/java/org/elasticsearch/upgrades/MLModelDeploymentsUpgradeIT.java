d80f860944d843eb165ec999ac91e33c201e8f2e
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2026-09-08 15:09:22 +0200

    [ML] Add transient-404 retry primitive for rolling-upgrade ML REST tests (#154791)
    ## Summary
    
    Adds `performRequestRetryingOnTransientStatus` / `performRequestWithRetryOnTransientStatus` to `ESRestTestCase` so `assertBusy` can retry REST reads that return transient 404s while ML indices relocate during rolling upgrade or full-cluster restart.
    
    Several ML rolling-upgrade and full-cluster-restart tests were failing intermittently with `resource_not_found_exception` on `GET /_ml/trained_models/.../_stats` or `index_not_found_exception` on ML index `/_mappings` reads, even though the models and indices had been created in an earlier cluster phase. The failures happened because `client().performRequest()` throws `ResponseException` on those transient 404s, and `assertBusy` only retries `AssertionError`, so the exception escaped the wait loop during node restarts and shard relocations.
    
    Routes four muted ML upgrade/restart tests through the primitive and unmutes them.
    
    Closes #152115
    
    (cherry picked from commit 18c77b32164aeff395263e44726806ea1d750b91)
    
    # Conflicts:
    #	muted-tests.yml
    #	x-pack/qa/full-cluster-restart/src/javaRestTest/java/org/elasticsearch/xpack/restart/MLModelDeploymentFullClusterRestartIT.java
    #	x-pack/qa/rolling-upgrade-legacy/src/test/java/org/elasticsearch/upgrades/MLModelDeploymentsUpgradeIT.java
    #	x-pack/qa/rolling-upgrade-legacy/src/test/java/org/elasticsearch/upgrades/MlAssignmentPlannerUpgradeIT.java
    #	x-pack/qa/rolling-upgrade-legacy/src/test/java/org/elasticsearch/upgrades/MlMappingsUpgradeIT.java

--- x-pack/qa/rolling-upgrade-legacy/src/test/java/org/elasticsearch/upgrades/MLModelDeploymentsUpgradeIT.java
+++ x-pack/qa/rolling-upgrade-legacy/src/test/java/org/elasticsearch/upgrades/MLModelDeploymentsUpgradeIT.java
@@ -13,6 +13,8 @@ import org.elasticsearch.client.Response;
 import org.elasticsearch.common.xcontent.support.XContentMapValues;
 import org.elasticsearch.core.RestApiVersion;
 import org.elasticsearch.core.Strings;
+import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.test.cluster.ElasticsearchCluster;
 import org.elasticsearch.xcontent.XContentType;
 import org.junit.After;
 import org.junit.Before;
@@ -272,9 +274,8 @@ public class MLModelDeploymentsUpgradeIT extends AbstractUpgradeTestCase {
 
     private Response getTrainedModelStats(String modelId) throws IOException {
         Request request = new Request("GET", "/_ml/trained_models/" + modelId + "/_stats");
-        var response = client().performRequest(request);
-        assertOK(response);
-        return response;
+        // Transient 404/503 while ML indices relocate or the plugin is still recovering during upgrade.
+        return performRequestRaisingAssertionOnTransientStatus(request, RestStatus.NOT_FOUND, RestStatus.SERVICE_UNAVAILABLE);
     }
 
     private Response infer(String input, String modelId) throws IOException {

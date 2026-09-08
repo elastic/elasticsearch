f5c20460acb806165159c65e74eb4c7bfe742ef5
Joshua Adams <joshua.adams@elastic.co>
2026-09-08 16:22:53 +0100

    Update StatelessSnapshotResiliencyTests.java

--- x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/snapshots/StatelessSnapshotResiliencyTests.java
+++ x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/snapshots/StatelessSnapshotResiliencyTests.java
@@ -217,7 +217,7 @@ public class StatelessSnapshotResiliencyTests extends SnapshotResiliencyTests {
         @Override
         public void scheduleNow(Runnable task) {
             if (task.toString().contains("processPendingDeletes[")) {
-                logger.debug("--> dropping {} to avoid wall-clock shard-lock wait on DTQ", task);
+                logger.debug("--> dropping {} to avoid possible deadlock", task);
                 return;
             }
             super.scheduleNow(task);

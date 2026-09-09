7219c9513c36e372fd18d722cb2e63d7c9a8f42e
Dimitris Rempapis <dimitris.rempapis@elastic.co>
2026-09-09 13:14:56 +0300

    update

--- server/src/main/java/org/elasticsearch/search/fetch/subphase/InnerHitsPhase.java
+++ server/src/main/java/org/elasticsearch/search/fetch/subphase/InnerHitsPhase.java
@@ -99,27 +99,26 @@ public final class InnerHitsPhase implements FetchSubPhase {
 
             fetchPhase.execute(innerHitsContext, docIdsToLoad, null);
             FetchSearchResult fetchResult = innerHitsContext.fetchResult();
-            try {
-                SearchHit[] internalHits = fetchResult.fetchResult().hits().getHits();
-                for (int j = 0; j < internalHits.length; j++) {
-                    ScoreDoc scoreDoc = topDoc.topDocs.scoreDocs[j];
-                    SearchHit searchHitFields = internalHits[j];
-                    searchHitFields.score(scoreDoc.score);
-                    if (scoreDoc instanceof FieldDoc fieldDoc) {
-                        searchHitFields.sortValues(fieldDoc.fields, innerHitsContext.sort().formats);
-                    }
-                }
-                var h = fetchResult.hits();
-                assert hit.isPooled() || h.isPooled() == false;
-                results.put(entry.getKey(), h);
-                h.mustIncRef();
-            } finally {
-                long innerHitsBreakerBytes = fetchResult.getSearchHitsSizeBytes();
-                if (innerHitsBreakerBytes > 0L) {
-                    fetchResult.releaseCircuitBreakerBytes(innerHitsContext.circuitBreaker());
-                    parentContext.chargeScriptFieldsBytes(innerHitsBreakerBytes);
+
+            long innerHitsBreakerBytes = fetchResult.getSearchHitsSizeBytes();
+            if (innerHitsBreakerBytes > 0L) {
+                fetchResult.releaseCircuitBreakerBytes(innerHitsContext.circuitBreaker());
+                parentContext.chargeScriptFieldsBytes(innerHitsBreakerBytes);
+            }
+
+            SearchHit[] internalHits = fetchResult.fetchResult().hits().getHits();
+            for (int j = 0; j < internalHits.length; j++) {
+                ScoreDoc scoreDoc = topDoc.topDocs.scoreDocs[j];
+                SearchHit searchHitFields = internalHits[j];
+                searchHitFields.score(scoreDoc.score);
+                if (scoreDoc instanceof FieldDoc fieldDoc) {
+                    searchHitFields.sortValues(fieldDoc.fields, innerHitsContext.sort().formats);
                 }
             }
+            var h = fetchResult.hits();
+            assert hit.isPooled() || h.isPooled() == false;
+            results.put(entry.getKey(), h);
+            h.mustIncRef();
         }
     }
 }

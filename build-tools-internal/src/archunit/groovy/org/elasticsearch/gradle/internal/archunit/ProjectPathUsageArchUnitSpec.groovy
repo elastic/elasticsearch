30063e26b0fc7269c0ee411268e58b0647556144
Rene Groeschke <rene@elastic.co>
2026-09-10 11:13:57 +0200

    Update ProjectPathUsageArchUnitSpec.groovy

--- build-tools-internal/src/archunit/groovy/org/elasticsearch/gradle/internal/archunit/ProjectPathUsageArchUnitSpec.groovy
+++ build-tools-internal/src/archunit/groovy/org/elasticsearch/gradle/internal/archunit/ProjectPathUsageArchUnitSpec.groovy
@@ -53,7 +53,6 @@ class ProjectPathUsageArchUnitSpec extends AbstractArchUnitSpec {
         "org.elasticsearch.gradle.internal.ElasticsearchJavaBasePlugin",
         "org.elasticsearch.gradle.internal.InternalDistributionBwcSetupPlugin",
         "org.elasticsearch.gradle.internal.InternalDistributionDownloadPlugin",
-        "org.elasticsearch.gradle.internal.JmhPlugin",
         "org.elasticsearch.gradle.internal.esql.EsqlFunctionPlugin",
         "org.elasticsearch.gradle.internal.foreign.ForeignLibraryPlugin",
         "org.elasticsearch.gradle.internal.precommit.JarHellPrecommitPlugin",

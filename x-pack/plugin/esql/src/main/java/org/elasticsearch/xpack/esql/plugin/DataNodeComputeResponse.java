2937a22940e3e3df702cf37be2ff0e972110950c
Nacho Cordón <ncordon@users.noreply.github.com>
2026-09-14 13:44:20 +0200

    ESQL: Fix BWC warnings propagation for versions not supporting DriverCompletionInfo serialization (#159155)

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/DataNodeComputeResponse.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/plugin/DataNodeComputeResponse.java
@@ -10,6 +10,7 @@ package org.elasticsearch.xpack.esql.plugin;
 import org.elasticsearch.TransportVersion;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.logging.HeaderWarning;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.compute.operator.DriverCompletionInfo;
 import org.elasticsearch.compute.operator.DriverProfile;
@@ -19,7 +20,6 @@ import org.elasticsearch.transport.TransportResponse;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * The compute result of {@link DataNodeRequest}
@@ -50,7 +50,7 @@ final class DataNodeComputeResponse extends TransportResponse {
                 0,
                 in.readCollectionAsImmutableList(DriverProfile::readFrom),
                 List.of(),
-                Set.of()
+                HeaderWarning.readWarningsFromThreadContext(threadContext)
             );
             this.shardLevelFailures = in.readMap(ShardId::new, StreamInput::readException);
             return;

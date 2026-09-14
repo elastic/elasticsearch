c8aeb39a32708ecce2f43e8dfd14923502d631af
Pooya Salehi <pooya.salehi@elastic.co>
2026-09-14 12:33:26 +0200

    Reject new persistent tasks for deleting projects

--- server/src/main/java/org/elasticsearch/persistent/UpdatePersistentTaskStatusAction.java
+++ server/src/main/java/org/elasticsearch/persistent/UpdatePersistentTaskStatusAction.java
@@ -137,7 +137,8 @@ public class UpdatePersistentTaskStatusAction {
 
         @Override
         protected ClusterBlockException checkBlock(Request request, ClusterState state) {
-            // Cluster is not affected but we look up repositories in metadata
+            // Intentionally only checks cluster-wide blocks, unlike StartPersistentTaskAction: existing tasks must be able to
+            // update their state while a project-global block (e.g. project under deletion) is in place.
             return state.blocks().globalBlockedException(ClusterBlockLevel.METADATA_WRITE);
         }
 

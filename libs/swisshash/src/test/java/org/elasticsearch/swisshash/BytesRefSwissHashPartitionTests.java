601b5de84e4b3bec1ae1c008ebcd26b32d100f98
Parker Timmins <parker.timmins@elastic.co>
2026-09-15 14:13:40 -0500

    Raise BytesRefSwissHashPartitionTests CB limit to 300mb (#159303)
    200mb was not sufficient for the absolute worst case (1000 blocks x 2048
    positions x 20-byte keys x partitionSize=128). At that extreme the
    PartitionedSum pre-allocation alone reaches ~131mb, pushing the peak
    to ~270mb. 300mb gives adequate headroom.

--- libs/swisshash/src/test/java/org/elasticsearch/swisshash/BytesRefSwissHashPartitionTests.java
+++ libs/swisshash/src/test/java/org/elasticsearch/swisshash/BytesRefSwissHashPartitionTests.java
@@ -73,7 +73,7 @@ public class BytesRefSwissHashPartitionTests extends PartitionedHashTestCase {
 
     public void testPartitionPaged() {
         var recycler = new BytesRefSwissHashTests.TestRecycler();
-        BigArrays bigArrays = new MockBigArrays(PageCacheRecycler.NON_RECYCLING_INSTANCE, ByteSizeValue.ofMb(200)).withCircuitBreaking();
+        BigArrays bigArrays = new MockBigArrays(PageCacheRecycler.NON_RECYCLING_INSTANCE, ByteSizeValue.ofMb(300)).withCircuitBreaking();
         CircuitBreaker breaker = bigArrays.breakerService().getBreaker(CircuitBreaker.REQUEST);
         runPartitionTest(recycler, bigArrays, breaker, BytesRefSwissHash.PagedBytesRefPartitionedHashKeys.class, 0L, false);
         assertThat(breaker.getUsed(), equalTo(0L));

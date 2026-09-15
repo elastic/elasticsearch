6697a6fd1b772cc88a44d7bceb867015518a7b28
Joshua Adams <joshua.adams@elastic.co>
2026-09-15 12:20:31 +0100

    Add missing import

--- server/src/test/java/org/elasticsearch/snapshots/SnapshotResiliencyTestHelper.java
+++ server/src/test/java/org/elasticsearch/snapshots/SnapshotResiliencyTestHelper.java
@@ -179,6 +179,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 

7ef4a20992c3634fdf8fa3f918d89564add485d9
Sam Xiao <sam.xiao@elastic.co>
2026-09-15 18:00:18 -0400

    [Refactor] Remove shardRecoveryStates (#159266)

--- server/src/main/java/org/elasticsearch/rest/action/cat/RestCatRecoveryAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/cat/RestCatRecoveryAction.java
@@ -9,9 +9,9 @@
 
 package org.elasticsearch.rest.action.cat;
 
-import org.apache.lucene.util.CollectionUtil;
 import org.elasticsearch.action.admin.indices.recovery.RecoveryRequest;
 import org.elasticsearch.action.admin.indices.recovery.RecoveryResponse;
+import org.elasticsearch.action.admin.indices.recovery.ShardRecoveryInfo;
 import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.cluster.routing.RecoverySource;
@@ -33,7 +33,6 @@ import java.time.Instant;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import static org.elasticsearch.rest.RestRequest.Method.GET;
 
@@ -126,18 +125,19 @@ public class RestCatRecoveryAction extends AbstractCatAction {
 
         Table t = getTableWithHeader(request);
 
-        final Map<String, List<RecoveryState>> recoveryStatesByIndex = response.shardRecoveryStates();
-        for (var entry : recoveryStatesByIndex.entrySet()) {
-            String index = entry.getKey();
-            List<RecoveryState> shardRecoveryStates = entry.getValue();
-            if (shardRecoveryStates.isEmpty()) {
+        for (var entry : response.shardRecoveryInfos().entrySet()) {
+            if (entry.getValue().isEmpty()) {
                 continue;
             }
-
+            String index = entry.getKey();
             // Sort ascending by shard id for readability
-            CollectionUtil.introSort(shardRecoveryStates, Comparator.comparingInt(o -> o.getShardId().id()));
+            List<ShardRecoveryInfo> shardRecoveryInfos = entry.getValue()
+                .stream()
+                .sorted(Comparator.comparingInt(info -> info.recoveryState().getShardId().id()))
+                .toList();
 
-            for (RecoveryState state : shardRecoveryStates) {
+            for (ShardRecoveryInfo recoveryInfo : shardRecoveryInfos) {
+                RecoveryState state = recoveryInfo.recoveryState();
                 t.startRow();
                 t.addCell(index);
                 t.addCell(state.getShardId().id());

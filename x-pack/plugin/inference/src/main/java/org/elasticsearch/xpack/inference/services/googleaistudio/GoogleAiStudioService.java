af9e0135eae8ce5bc99ee7587ae277d8bd0af5c7
Simon Cooper <simon.cooper@elastic.co>
2026-09-15 17:41:21 +0100

    [9.4] Use LimitedCharSequence for limiting the reads of chunk separator regexes (#158743) (#159350)
    * Use LimitedCharSequence for limiting the reads of chunk separator regexes (#158743)
    
    * [CI] Auto commit changes from spotless
    
    * Add parameter
    
    ---------
    
    Co-authored-by: elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>

--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/googleaistudio/GoogleAiStudioService.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/googleaistudio/GoogleAiStudioService.java
@@ -296,6 +296,7 @@ public class GoogleAiStudioService extends SenderService<GoogleAiStudioModel> {
         List<EmbeddingRequestChunker.BatchRequestAndListener> batchedRequests = new EmbeddingRequestChunker<>(
             inputs,
             EMBEDDING_MAX_BATCH_SIZE,
+            getRegexReadLimitFactor(),
             googleAiStudioModel.getConfigurations().getChunkingSettings()
         ).batchRequestsWithListeners(listener);
 
--- x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/sagemaker/SageMakerService.java
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/services/sagemaker/SageMakerService.java
@@ -35,6 +35,7 @@ import org.elasticsearch.inference.UnparsedModel;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.xpack.core.inference.chunking.EmbeddingRequestChunker;
+import org.elasticsearch.xpack.core.inference.chunking.RecursiveChunkingSettings;
 import org.elasticsearch.xpack.inference.services.ServiceUtils;
 import org.elasticsearch.xpack.inference.services.sagemaker.model.SageMakerModel;
 import org.elasticsearch.xpack.inference.services.sagemaker.model.SageMakerModelBuilder;
@@ -293,6 +294,7 @@ public class SageMakerService implements InferenceService, RerankingInferenceSer
             var batchedRequests = new EmbeddingRequestChunker<>(
                 input,
                 sageMakerModel.batchSize().orElse(DEFAULT_BATCH_SIZE),
+                clusterService.getClusterSettings().get(RecursiveChunkingSettings.REGEX_READ_LIMIT_FACTOR_SETTING),
                 sageMakerModel.getConfigurations().getChunkingSettings()
             ).batchRequestsWithListeners(listener);
 

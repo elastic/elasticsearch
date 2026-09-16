40ce4ce2dcfe8c02e5eb13d20ba164770da5acef
Alan Woodward <romseygeek@apache.org>
2026-09-16 09:46:41 +0100

    yaml

--- server/src/main/java/org/elasticsearch/index/mapper/MapperFeatures.java
+++ server/src/main/java/org/elasticsearch/index/mapper/MapperFeatures.java
@@ -167,7 +167,8 @@ public class MapperFeatures implements FeatureSpecification {
     public static final NodeFeature COLUMNAR_DROPS_DYNAMIC_FALSE_FIELDS = new NodeFeature("mapper.columnar.drops_dynamic_false_fields");
     public static final NodeFeature COLUMNAR_SUPPORTS_SHAPE_FIELDS = new NodeFeature("mapper.columnar.supports_shape_fields");
     public static final NodeFeature TSDB_METRIC_TEMPORALITY_SUPPORT = new NodeFeature("mapper.tsdb.metric_temporality_support");
-    static final NodeFeature DUPLICATE_DYNAMIC_TEMPLATE_NAMES_WARNING = new NodeFeature("mapper.dynamic_template.warn_on_duplicate_names");
+    public static final NodeFeature DUPLICATE_DYNAMIC_TEMPLATE_NAMES_WARNING = new NodeFeature("mapper.dynamic_template.warn_on_duplicate_names");
+    public static final NodeFeature PUT_MAPPING_NO_TYPES_CHECK = new NodeFeature("indices.put_mapping.no_types_check");
 
     @Override
     public Set<NodeFeature> getFeatures() {
@@ -268,7 +269,8 @@ public class MapperFeatures implements FeatureSpecification {
             BBQ_DISK_BYTE_SUPPORT,
             ASH_QUANTIZATION_TYPE_SUPPORT,
             DOC_VALUES_ON_FAILURE,
-            DISK_BBQ_STABLE_FORMAT_SELECTION
+            DISK_BBQ_STABLE_FORMAT_SELECTION,
+            PUT_MAPPING_NO_TYPES_CHECK
         );
     }
 }
--- server/src/main/java/org/elasticsearch/rest/action/admin/indices/RestPutMappingAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/admin/indices/RestPutMappingAction.java
@@ -13,21 +13,16 @@ import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
 import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.common.Strings;
-import org.elasticsearch.common.xcontent.XContentHelper;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.Scope;
 import org.elasticsearch.rest.ServerlessScope;
 import org.elasticsearch.rest.action.RestToXContentListener;
-import org.elasticsearch.xcontent.XContentType;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-import static org.elasticsearch.index.mapper.MapperService.SINGLE_MAPPING_NAME;
-import static org.elasticsearch.index.mapper.MapperService.isMappingSourceTyped;
 import static org.elasticsearch.rest.RestRequest.Method.POST;
 import static org.elasticsearch.rest.RestRequest.Method.PUT;
 import static org.elasticsearch.rest.RestUtils.getAckTimeout;

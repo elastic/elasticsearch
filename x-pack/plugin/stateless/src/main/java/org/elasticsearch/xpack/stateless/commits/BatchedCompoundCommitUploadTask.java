5a459062e71ca13189d04c00c0119f3679344967
Anantha Krishnan S R <anantha.ramu@elastic.co>
2026-09-16 14:14:49 -0700

    Name the blob a commit upload wrote to
    The upload writes to the name returned by getBlobName, and for a batch
    that name is not derivable from the contained generations. Logging it
    gives one literal string to search for, alongside the generation list
    that the deletion log can be joined on.

--- x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/commits/BatchedCompoundCommitUploadTask.java
+++ x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/commits/BatchedCompoundCommitUploadTask.java
@@ -217,11 +217,12 @@ public class BatchedCompoundCommitUploadTask extends RetryableAction<BccUploadRe
                         final var commits = virtualBcc.getPendingCompoundCommits();
                         final var newest = commits.getLast();
                         return format(
-                            "%s uuid [%s] commit [%s] uploaded in %s (%s files, %s total bytes, generations [%s], "
-                                + "local_checkpoint [%s], max_seq_no [%s])",
+                            "%s uuid [%s] commit [%s] blob [%s] uploaded in %s (%s files, %s total bytes, "
+                                + "generations [%s], local_checkpoint [%s], max_seq_no [%s])",
                             shardId,
                             shardId.getIndex().getUUID(),
                             virtualBcc.primaryTermAndGeneration(),
+                            virtualBcc.getBlobName(),
                             timing.toLogString(),
                             uploadedFileCount,
                             uploadedFileBytes,

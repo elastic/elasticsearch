e76418b8ab4089130cfb4d801d8d13c905415e07
Karolos Antoniadis <karolos.antoniadis@elastic.co>
2026-09-17 17:52:17 +0200

    nit new-line removal change

--- x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/cache/SharedBlobCacheWarmingService.java
+++ x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/cache/SharedBlobCacheWarmingService.java
@@ -1917,7 +1917,7 @@ public class SharedBlobCacheWarmingService {
 
             WarmBlobRegionTask(Type type, BlobFile blobFile, int region, ActionListener<Void> listener) {
                 super(type, warmingTaskNumber.getAndIncrement());
-                assert region >= 0: region;
+                assert region >= 0 : region;
                 this.blobFile = Objects.requireNonNull(blobFile);
                 this.region = region;
                 this.listener = listener;
@@ -1932,7 +1932,6 @@ public class SharedBlobCacheWarmingService {
                 assert warmingRun.type == Type.INDEXING_MERGE || warmingRun.type == Type.INDEXING_BCC_HEADER_PREWARM : warmingRun.type;
 
                 var releasedListener = ActionListener.releaseAfter(listener, releasable);
-
                 if (isCancelled()) {
                     releasedListener.onResponse(null);
                     return;

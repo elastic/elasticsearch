4bdf87c1d33327c8f0901260338e73dfab020c0d
Szymon Bialkowski <szybia@tuta.io>
2026-09-21 14:58:47 +0100

    [szymon] test

--- x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/cache/SearchShardRecoveryWarmingTests.java
+++ x-pack/plugin/stateless/src/test/java/org/elasticsearch/xpack/stateless/cache/SearchShardRecoveryWarmingTests.java
@@ -111,6 +111,7 @@ public class SearchShardRecoveryWarmingTests extends ESTestCase {
                 SharedBlobCacheWarmingService.UPLOAD_PREWARM_MAX_SIZE_SETTING,
                 SharedBlobCacheWarmingService.WARM_BYTE_RANGE_THROTTLE_RATIO_SETTING,
                 SharedBlobCacheWarmingService.WARM_BYTE_RANGE_PER_FILE_CONCURRENCY_SETTING,
+                SharedBlobCacheWarmingService.SMALL_WARMING_TASK_PRIORITY_SIZE_SETTING,
                 SharedBlobCacheWarmingService.PREWARM_INDEX_SHARD_FOR_ID_LOOKUPS_SETTING,
                 SharedBlobCacheWarmingService.ID_LOOKUP_PREWARM_RATIO_SETTING
             )

a783cfaebb4cdfa846c8c56d1523421d336f0940
Oleksandr Kolomiiets <oleksandr.kolomiiets@elastic.co>
2026-09-21 13:46:02 -0700

    Fix typos in UploadQueueControllerService

--- x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/commits/UploadQueueControllerService.java
+++ x-pack/plugin/stateless/src/main/java/org/elasticsearch/xpack/stateless/commits/UploadQueueControllerService.java
@@ -257,12 +257,12 @@ public class UploadQueueControllerService extends AbstractLifecycleComponent {
 
                 TimeValue ageOfTheOldestCommitPendingUpload;
                 if (oldestCommitUploadStartTime == null) {
-                    // When `oldestCommitUploadStartTime` is null it means that there are commits pending upload
+                    // When `oldestCommitUploadStartTime` is null, it means that there are no pending upload commits,
                     // and as such we can remove throttling (TimeValue.ZERO should always be smaller than `settings.deactivationThreshold`).
                     ageOfTheOldestCommitPendingUpload = TimeValue.ZERO;
                 } else {
                     ageOfTheOldestCommitPendingUpload = TimeValue.timeValueMillis(
-                        relativeTimeMillis.get() - stats.oldestCommitUploadStartTimeRelativeMillis()
+                        relativeTimeMillis.get() - oldestCommitUploadStartTime
                     );
                     oldestCommitAgeSecondsHistogram.record(ageOfTheOldestCommitPendingUpload.seconds());
                 }

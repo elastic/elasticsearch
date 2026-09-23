3eb925350e84e663f46e9ae8154c6921489dc05c
elasticsearchmachine <infra-root+elasticsearchmachine@elastic.co>
2026-09-23 18:43:58 +0000

    Bump versions after 8.19.22 release

--- x-pack/plugin/esql/src/main/generated/org/elasticsearch/xpack/esql/expression/function/scalar/spatial/StDistanceGeoPointDocValuesAndDocValuesEvaluator.java
+++ x-pack/plugin/esql/src/main/generated/org/elasticsearch/xpack/esql/expression/function/scalar/spatial/StDistanceGeoPointDocValuesAndDocValuesEvaluator.java
@@ -92,12 +92,7 @@ public final class StDistanceGeoPointDocValuesAndDocValuesEvaluator implements E
 
   private Warnings warnings() {
     if (warnings == null) {
-      this.warnings = Warnings.createWarnings(
-              driverContext.warningsMode(),
-              source.source().getLineNumber(),
-              source.source().getColumnNumber(),
-              source.text()
-          );
+      this.warnings = driverContext.createWarnings(source.source().getLineNumber(), source.source().getColumnNumber(), source.text());
     }
     return warnings;
   }

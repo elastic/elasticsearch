e9dfac90635b7f8ac79b1131bd012341636c6f06
muskan-agarwal26 <165268590+muskan-agarwal26@users.noreply.github.com>
2026-09-24 17:30:36 +0530

    [XM Cyber] - Fix Cannot execute ILM policy delete step (#160185)
    This PR focuses on the short term solution which add the `logs-xm_cyber.device-*`, `logs-xm_cyber.product-*` and `logs-xm_cyber.vulnerability_index-*` index under the `kibana_system `role with deletion privileges to prevent a failed deletion error when the index enters the [deletion phase for the ILM lifecycle](https://github.com/elastic/integrations/pull/21162). As it ships transform pipeline too hence read, write permissions are also required.
    
    (cherry picked from commit bc6c47fa391ad5aea0c2a30f9442b2deee27aa4e)

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStoreTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStoreTests.java
@@ -2102,7 +2102,7 @@ public class ReservedRolesStoreTests extends ESTestCase {
         });
 
         // Tests for third-party agent indices (ExtraHop, QualysGAV, SentinelOne, Island Browser, Cyera,
-        // IRONSCALES, Axonius, JupiterOne and PingDirectory) that
+        // IRONSCALES, Axonius, JupiterOne, PingDirectory and XM Cyber) that
         // `kibana_system` has full management access to
         // This includes read, write, create, delete, and all ILM-related management actions.
         Arrays.asList(
@@ -2130,7 +2130,10 @@ public class ReservedRolesStoreTests extends ESTestCase {
             "logs-axonius.ticket-" + randomAlphaOfLength(randomIntBetween(1, 10)),
             "logs-axonius.user-" + randomAlphaOfLength(randomIntBetween(1, 10)),
             "logs-jupiter_one.risks_and_alerts-" + randomAlphaOfLength(randomIntBetween(1, 10)),
-            "logs-ping_directory.user-" + randomAlphaOfLength(randomIntBetween(1, 10))
+            "logs-ping_directory.user-" + randomAlphaOfLength(randomIntBetween(1, 10)),
+            "logs-xm_cyber.device-" + randomAlphaOfLength(randomIntBetween(1, 10)),
+            "logs-xm_cyber.product-" + randomAlphaOfLength(randomIntBetween(1, 10)),
+            "logs-xm_cyber.vulnerability_instance-" + randomAlphaOfLength(randomIntBetween(1, 10))
         ).forEach((index_qualys_extra_hop) -> {
             final IndexAbstraction indexAbstraction = mockIndexAbstraction(index_qualys_extra_hop);
 

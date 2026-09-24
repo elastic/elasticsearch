7a467584d5679fb7e0184886bf1ad752590b2c01
Alan Woodward <romseygeek@apache.org>
2026-09-24 11:32:34 +0100

    Store mappings on PutMappingsRequest as ByteReference (#160082)
    This avoids multiple parse-to-map-and-serialize rounds on the
    co-ordinating node for PUT mappings, reducing memory pressure for large
    mapping requests.

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/job/persistence/ElasticsearchMappings.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/ml/job/persistence/ElasticsearchMappings.java
@@ -28,7 +28,6 @@ import org.elasticsearch.plugins.MapperPlugin;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.Transports;
-import org.elasticsearch.xcontent.XContentType;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -213,7 +212,7 @@ public class ElasticsearchMappings {
                 if (indicesThatRequireAnUpdate.length > 0) {
                     String mapping = mappingSupplier.get();
                     PutMappingRequest putMappingRequest = new PutMappingRequest(indicesThatRequireAnUpdate);
-                    putMappingRequest.source(mapping, XContentType.JSON);
+                    putMappingRequest.source(mapping);
                     putMappingRequest.origin(ML_ORIGIN);
                     putMappingRequest.masterNodeTimeout(masterNodeTimeout);
                     executeAsyncWithOrigin(

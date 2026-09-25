d7061d8caa170cc982ee6ecbcc478ffbbe074296
Julian Kiryakov <julian.kiryakov@elastic.co>
2026-09-24 20:31:55 -0400

    address code review comments

--- x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/Analyzer.java
+++ x-pack/plugin/esql/src/main/java/org/elasticsearch/xpack/esql/analysis/Analyzer.java
@@ -4707,19 +4707,16 @@ public class Analyzer extends ParameterizedRuleExecutor<LogicalPlan, AnalyzerCon
                 Collection<Expression> conversions = unionTypeEsField.getConversionExpressions();
                 return conversions.isEmpty() == false && conversions.stream().allMatch(e -> isConversionOf(e, convert, inputName));
             }
-            Holder<Boolean> same = new Holder<>(false);
-            branch.forEachDown(Eval.class, eval -> {
-                if (same.get()) {
-                    return;
-                }
-                for (Alias alias : eval.fields()) {
-                    if (alias.id().equals(existing.id()) && isConversionOf(alias.child(), convert, inputName)) {
-                        same.set(true);
-                        break;
+            return branch.forEachDownMayReturnEarly((plan, breakEarly) -> {
+                if (plan instanceof Eval eval) {
+                    for (Alias alias : eval.fields()) {
+                        if (alias.id().equals(existing.id()) && isConversionOf(alias.child(), convert, inputName)) {
+                            breakEarly.set(true);
+                            return;
+                        }
                     }
                 }
             });
-            return same.get();
         }
 
         /**
